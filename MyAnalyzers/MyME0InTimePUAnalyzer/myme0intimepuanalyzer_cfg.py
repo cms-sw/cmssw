18a3c8701f37f7393982ab2233c6a215cb3f997a
piet verwilligen <piet.verwilligen@cern.ch>
2015-10-16 18:09:43 +0200

    added DT and CSS matching

--- MyAnalyzers/MyME0InTimePUAnalyzer/myme0intimepuanalyzer_cfg.py
+++ MyAnalyzers/MyME0InTimePUAnalyzer/myme0intimepuanalyzer_cfg.py
@@ -5,9 +5,17 @@ process.load("FWCore.MessageService.MessageLogger_cfi")
 
 # process.load('Configuration.Geometry.GeometryExtended2015MuonGEMDevReco_cff')
 process.load('Configuration.Geometry.GeometryExtended2023HGCalMuonReco_cff')
+# CSCGeometry depends on alignment ==> necessary to provide GlobalPositionRecord
+process.load("Alignment.CommonAlignmentProducer.FakeAlignmentSource_cfi") 
+process.load("Geometry.CSCGeometry.cscGeometry_cfi")
+
+# process.load("Geometry.CSCGeometry.cscGeometry_cfi")
+# process.load("Geometry.DTGeometry.dtGeometry_cfi")
+# process.load("Geometry.GEMGeometry.gemGeometry_cfi")
+
 
 process.maxEvents = cms.untracked.PSet( 
-    input           = cms.untracked.int32(-1),
+    input           = cms.untracked.int32(1),
     # eventsToProcess = cms.untracked.VEventRange('1:26043:618522'), # Run 1 LS 26043 Evt 618522
     # eventsToProcess = cms.untracked.VEventRange('1:26044:618528'), # Run 1 LS 26044 Evt 618528
 )
@@ -22,17 +30,24 @@ process.source = cms.Source("PoolSource",
 
 process.me0timeanalyzer = cms.EDAnalyzer('MyME0InTimePUAnalyzer',
                               # ----------------------------------------------------------------------
-                              # RootFileName = cms.untracked.string("TestGEMSegmentHistograms.root"),
+                              RootFileName       = cms.untracked.string("ME0InTimeOutOfTimePUtHistograms.root"),
                               # ----------------------------------------------------------------------
-                              preDigiSmearX   = cms.untracked.double(0.05), # [in cm]
-                              preDigiSmearY   = cms.untracked.double(1.0),  # [in cm]
-                              nMatchedHits    = cms.untracked.int32(3),
+                              preDigiSmearX      = cms.untracked.double(0.0500), # [in cm] (single layer resolution)
+                              preDigiSmearY      = cms.untracked.double(1.0),    # [in cm] (single layer resolution)
+                              cscDetResX         = cms.untracked.double(0.0150), # [in cm] (chamber resolution :: 75-150um, take here 150um)
+                              cscDetResY         = cms.untracked.double(5.0),    # [in cm]
+                              dtDetResX          = cms.untracked.double(0.0125), # [in cm] (chamber resolution ::  75-125um in r-phi, take here 125um)
+                              dtDetResY          = cms.untracked.double(0.0400), # [in cm] (chamber resolution :: 150-400um in r-z  , take here 400um)
+                              nMatchedHitsME0Seg = cms.untracked.int32(3),
+                              nMatchedHitsCSCSeg = cms.untracked.int32(3),
+                              nMatchedHitsDTSeg  = cms.untracked.int32(6),
                               # ----------------------------------------------------------------------
-                              printInfoHepMC    = cms.untracked.bool(False),
-                              printInfoSignal   = cms.untracked.bool(True),
-                              printInfoPU       = cms.untracked.bool(False),
-                              printInfoAll      = cms.untracked.bool(True),
-                              printInfoME0Match = cms.untracked.bool(True),
+                              printInfoHepMC     = cms.untracked.bool(False),
+                              printInfoSignal    = cms.untracked.bool(True),
+                              printInfoPU        = cms.untracked.bool(False),
+                              printInfoAll       = cms.untracked.bool(True),
+                              printInfoME0Match  = cms.untracked.bool(False),
+                              printInfoMuonMatch = cms.untracked.bool(False),
                               # ----------------------------------------------------------------------
 
 )
