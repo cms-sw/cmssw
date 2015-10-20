d74c9397cc72653f6500928fefe5b896b4c793ca
Sebastian <thuer@physik.rwth-aachen.de>
2015-10-20 15:01:43 +0200

    removed partial pathes

--- GeneratorInterface/SherpaInterface/src/SherpackFetcher.cc
+++ GeneratorInterface/SherpaInterface/src/SherpackFetcher.cc
@@ -85,8 +85,9 @@ int SherpackFetcher::Fetch()
 
 int SherpackFetcher::CopyFile(std::string pathstring)
 {
+  //No need to backwards compatibility with the FnFileGet method, throw exception if only the relative path is given
   if (  (pathstring.find("slc6_amd64_gcc")==0) || (pathstring.find("slc5_amd64_gcc")==0) ){
-    pathstring="/afs/cern.ch/cms/generators/www/"+pathstring;
+    throw cms::Exception("SherpaInterface") << "Old method of sherpack retrieving used, please use /cvmfs to store files and specify the full path to the sherpack directory";
   }
   std::cout << "Trying to copy file " << pathstring << std::endl;
   std::string command="cp " +pathstring + " .";
