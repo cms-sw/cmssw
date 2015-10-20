bca04a0854567141f10936d954d99e0f4b3bfb29
cbravo135 <c.bravo@cern.ch>
2015-10-20 03:38:52 +0200

    Changed the geometry configuration which is loaded

--- Geometry/GEMGeometry/test/testGEMCSCStripMapping_cfg.py
+++ Geometry/GEMGeometry/test/testGEMCSCStripMapping_cfg.py
@@ -1,8 +1,8 @@
 import FWCore.ParameterSet.Config as cms
 
 process = cms.Process("Demo")
-process.load('Configuration.Geometry.GeometryExtended2023Muon_cff')
-process.load('Configuration.Geometry.GeometryExtended2023MuonReco_cff')
+process.load('Configuration.Geometry.GeometryExtended2019_cff')
+process.load('Configuration.Geometry.GeometryExtended2019Reco_cff')
 process.load('Configuration.StandardSequences.FrontierConditions_GlobalTag_cff')
 process.load('FWCore.MessageLogger.MessageLogger_cfi')
 
@@ -13,8 +13,6 @@ process.maxEvents = cms.untracked.PSet(
     input = cms.untracked.int32(1)
 )
 process.source = cms.Source("EmptySource")
-
-process.es_prefer_trackerAlignment = cms.ESPrefer("HcalTopologyIdealEP","hcalTopologyIdealSLHC")
 
 process.MessageLogger = cms.Service("MessageLogger")
 
