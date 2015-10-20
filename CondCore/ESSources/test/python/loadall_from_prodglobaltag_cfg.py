541f192372731454e5c5156db31ad9bb99d67132
Giacomo Govi <giacomo.govi@cern.ch>
2015-10-20 16:33:05 +0200

    Port of fixes to set V2 as default db source for the GTs. Also ported fixes for handling of cfg params

--- CondCore/ESSources/test/python/loadall_from_prodglobaltag_cfg.py
+++ CondCore/ESSources/test/python/loadall_from_prodglobaltag_cfg.py
@@ -7,7 +7,7 @@ options.register('runNumber',
                  VarParsing.VarParsing.varType.int,
                  "Run number; default gives latest IOV")
 options.register('globalTag',
-                 'START70_V2::All', #default value
+                 'GR_P_V50', #default value
                  VarParsing.VarParsing.multiplicity.singleton,
                  VarParsing.VarParsing.varType.string,
                  "GlobalTag")
@@ -16,10 +16,7 @@ options.parseArguments()
 
 process = cms.Process("TEST")
 
-process.add_(cms.Service("PrintEventSetupDataRetrieval", printProviders=cms.untracked.bool(True)))
-
 process.load("Configuration.StandardSequences.FrontierConditions_GlobalTag_cfi")
-#process.GlobalTag.globaltag = options.globalTag+'::All'
 process.GlobalTag.globaltag = options.globalTag
 process.GlobalTag.RefreshEachRun=cms.untracked.bool(False)
 process.GlobalTag.DumpStat=cms.untracked.bool(True)
@@ -27,19 +24,12 @@ process.GlobalTag.pfnPrefix=cms.untracked.string('')
 process.GlobalTag.pfnPostfix=cms.untracked.string('')
 
 
-# 'GR09_P_V6::All'
-#'CRAFT09_R_V9::All'
-#'MC_31X_V9::All'
-#'GR09_31X_V5P::All'
-#process.GlobalTag.pfnPrefix = "frontier://FrontierArc/"
-#process.GlobalTag.pfnPostfix = "_0911"
-#process.GlobalTag.toGet = cms.VPSet()
-#process.GlobalTag.toGet.append(
-#   cms.PSet(record = cms.string("BeamSpotObjectsRcd"),
-#            tag = cms.string("firstcollisions"),
-#             connect = cms.untracked.string("frontier://PromptProd/CMS_COND_31X_BEAMSPOT")
-#           )
-#)
+process.GlobalTag.toGet = cms.VPSet()
+process.GlobalTag.toGet.append(
+   cms.PSet(record = cms.string("BeamSpotObjectsRcd"),
+            tag = cms.string("firstcollisions"),
+           )
+)
 
 
 
