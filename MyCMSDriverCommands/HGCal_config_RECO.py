82bf4cd603684c2f0de18c0cf7a2ccfad6187883
piet verwilligen <piet.verwilligen@cern.ch>
2015-10-21 17:11:58 +0200

    bugfix: now timing window is passed correctly

--- MyCMSDriverCommands/HGCal_config_RECO.py
+++ MyCMSDriverCommands/HGCal_config_RECO.py
@@ -32,28 +32,28 @@ process.load('Configuration.StandardSequences.FrontierConditions_GlobalTag_cff')
 ### Code/Configuration with thanks to Tim Cox                   
 ### -----------------------------------------------------------
 ###############################################################
-# process.load("FWCore.MessageLogger.MessageLogger_cfi")
-# process.MessageLogger.categories.append("ME0Segment")
-# process.MessageLogger.categories.append("ME0SegmentBuilder")
-# process.MessageLogger.categories.append("ME0SegAlgoMM")   
-# process.MessageLogger.categories.append("ME0SegFit")      
-# process.MessageLogger.categories.append("ME0SegFitMatrixDetails")      
-# process.MessageLogger.debugModules = cms.untracked.vstring("*")
-# process.MessageLogger.destinations = cms.untracked.vstring("cout","junk")
-# process.MessageLogger.cout = cms.untracked.PSet(
-#     threshold = cms.untracked.string("DEBUG"),
-#     default = cms.untracked.PSet( limit = cms.untracked.int32(0) ),
-#     FwkReport = cms.untracked.PSet( limit = cms.untracked.int32(-1) ),
+process.load("FWCore.MessageLogger.MessageLogger_cfi")
+### process.MessageLogger.categories.append("ME0Segment")
+### process.MessageLogger.categories.append("ME0SegmentBuilder")
+process.MessageLogger.categories.append("ME0SegAlgoMM")   
+### process.MessageLogger.categories.append("ME0SegFit")      
+### process.MessageLogger.categories.append("ME0SegFitMatrixDetails")      
+process.MessageLogger.debugModules = cms.untracked.vstring("*")
+process.MessageLogger.destinations = cms.untracked.vstring("cout","junk")
+process.MessageLogger.cout = cms.untracked.PSet(
+    threshold = cms.untracked.string("DEBUG"),
+    default = cms.untracked.PSet( limit = cms.untracked.int32(0) ),
+    FwkReport = cms.untracked.PSet( limit = cms.untracked.int32(-1) ),
     # ME0Segment             = cms.untracked.PSet( limit = cms.untracked.int32(-1) ),
     # ME0SegmentBuilder      = cms.untracked.PSet( limit = cms.untracked.int32(-1) ),
-    # ME0SegAlgoMM             = cms.untracked.PSet( limit = cms.untracked.int32(-1) ),
+    ME0SegAlgoMM             = cms.untracked.PSet( limit = cms.untracked.int32(-1) ),
     # ME0SegFit              = cms.untracked.PSet( limit = cms.untracked.int32(-1) ),
     # ME0SegFitMatrixDetails = cms.untracked.PSet( limit = cms.untracked.int32(-1) ),
-# )
+)
 ###############################################################
 
 process.maxEvents = cms.untracked.PSet(
-    input = cms.untracked.int32(50)
+    input = cms.untracked.int32(1)
 )
 
 # Input source
@@ -62,6 +62,8 @@ process.source = cms.Source("PoolSource",
     # fileNames = cms.untracked.vstring('file:JME-TP2023SHCALDR-00001_step1.root')
     # fileNames = cms.untracked.vstring('file:out_raw_182_1_pjB.root')
     # fileNames = cms.untracked.vstring('file:out_raw_198_1_kGQ.root')
+    # fileNames = cms.untracked.vstring('file:DYToMuMu_M-20_HGCALGS_PU140_ME0_RAW_500ps_amandeep_100.root')
+    fileNames = cms.untracked.vstring('file:DYToMuMu_M-20_HGCALGS_PU140_ME0_RAW_5ns_amandeep_100.root')
 )
 
 process.options = cms.untracked.PSet(
@@ -81,10 +83,12 @@ process.FEVTDEBUGoutput = cms.OutputModule("PoolOutputModule",
     splitLevel = cms.untracked.int32(0),
     eventAutoFlushCompressedSize = cms.untracked.int32(5242880),
     outputCommands = process.FEVTDEBUGEventContent.outputCommands,
-    fileName = cms.untracked.string('out_reco.root'),
+    # fileName = cms.untracked.string('out_reco.root'),
     # fileName = cms.untracked.string('step2_RAW2DIGI_L1Reco_RECO.root'),
     # fileName = cms.untracked.string('DYToMuMu_M-20_HGCALGS_PU140_ME0_RECO_100ps_amandeep_100_v2.root'),
     # fileName = cms.untracked.string('DYToMuMu_M-20_HGCALGS_PU140_ME0_RECO_100ps_amandeep_116_v2.root'),
+    # fileName = cms.untracked.string('DYToMuMu_M-20_HGCALGS_PU140_ME0_RECO_500ps_amandeep_Random.root'),
+    fileName = cms.untracked.string('DYToMuMu_M-20_HGCALGS_PU140_ME0_RECO_5ns_amandeep_Random.root'),
     dataset = cms.untracked.PSet(
         filterName = cms.untracked.string(''),
         dataTier = cms.untracked.string('RECO')
@@ -94,7 +98,7 @@ process.FEVTDEBUGoutput = cms.OutputModule("PoolOutputModule",
 # Additional output definition
 
 # Other statements
-from Configuration.AlCa.GlobalTag import GlobalTag
+from Configuration.AlCa.GlobalTag import GlobalTag ### process.load done earlier ...
 process.GlobalTag = GlobalTag(process.GlobalTag, 'PH2_1K_FB_V6::All', '')
 
 # Path and EndPath definitions
@@ -123,4 +127,6 @@ process = addMonitoring(process)
 
 # End of customisation functions
 
-process.me0Segments.dTimeChainBoxMax = cms.double(1.0)
+# from RecoLocalMuon.GEMRecHit.me0Segments import me0Segments
+process.load('RecoLocalMuon.GEMRecHit.me0Segments_cfi')
+process.me0Segments.algo_pset.dTimeChainBoxMax = cms.double(32.0)
