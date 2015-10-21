36198e70ed9ac5c2e64cc21343ddde15c2a2fad7
Eric Vaandering <ericvaandering@gmail.com>
2015-10-21 10:56:35 +0200

    implicit tuple parameter unpacking is removed in python 3

--- Documentation/ReferenceManualScripts/python/BeautifulSoup.py
+++ Documentation/ReferenceManualScripts/python/BeautifulSoup.py
@@ -555,10 +555,10 @@ class Tag(PageElement):
         self.escapeUnrecognizedEntities = parser.escapeUnrecognizedEntities
 
         # Convert any HTML, XML, or numeric entities in the attribute values.
-        convert = lambda(k, val): (k,
+        convert = lambda k_val: (k_val[0],
                                    re.sub("&(#\d+|#x[0-9a-fA-F]+|\w+);",
                                           self._convertEntities,
-                                          val))
+                                          k_val[1]))
         self.attrs = map(convert, self.attrs)
 
     def getString(self):
@@ -1824,7 +1824,7 @@ class UnicodeDammit:
                                                       "iso-8859-1",
                                                       "iso-8859-2"):
             markup = re.compile("([\x80-\x9f])").sub \
-                     (lambda(x): self._subMSChar(x.group(1)),
+                     (lambda x: self._subMSChar(x.group(1)),
                       markup)
 
         try:
--- RecoLuminosity/LumiDB/plotdata/create_public_peakpu_plots.py
+++ RecoLuminosity/LumiDB/plotdata/create_public_peakpu_plots.py
@@ -126,10 +126,11 @@ def GetXLocator(ax):
 
 ######################################################################
 
-def TweakPlot(fig, ax, (time_begin, time_end),
+def TweakPlot(fig, ax, xxx_todo_changeme,
               add_extra_head_room=False):
 
     # Fiddle with axes ranges etc.
+    (time_begin, time_end) = xxx_todo_changeme
     ax.relim()
     ax.autoscale_view(False, True, True)
     for label in ax.get_xticklabels():
