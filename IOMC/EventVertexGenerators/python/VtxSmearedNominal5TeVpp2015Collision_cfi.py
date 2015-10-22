334b2c280b724d4f5638acef757d0be4b32d2ca8
mandrenguyen <matthew.nguyen@cern.ch>
2015-10-22 11:24:44 +0200

    Update VtxSmearedNominal5TeVpp2015Collision_cfi.py

--- IOMC/EventVertexGenerators/python/VtxSmearedNominal5TeVpp2015Collision_cfi.py
+++ IOMC/EventVertexGenerators/python/VtxSmearedNominal5TeVpp2015Collision_cfi.py
@@ -1,7 +1,7 @@
 import FWCore.ParameterSet.Config as cms
 
-from IOMC.EventVertexGenerators.VtxSmearedParameters_cfi import Nominal5TeVpp2015CollisionVtxSmearingParameters,VtxSmearedCommon
+from IOMC.EventVertexGenerators.VtxSmearedParameters_cfi import Nominal5TeVpp2015VtxSmearingParameters,VtxSmearedCommon
 VtxSmeared = cms.EDProducer("BetafuncEvtVtxGenerator",
-    Nominal5TeVpp2015CollisionVtxSmearingParameters,
+    Nominal5TeVpp2015VtxSmearingParameters,
     VtxSmearedCommon
 )
