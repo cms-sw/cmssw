fbee34c794848614c92364b0c6b63f24aaa9b923
Aaron <aaron.bundock@cern.ch>
2015-10-22 16:13:16 +0200

    Increased number of EG and Tau per link from 4->6

--- EventFilter/L1TRawToDigi/src/implementations_stage2/L1TStage2Layer2Constants.cc
+++ EventFilter/L1TRawToDigi/src/implementations_stage2/L1TStage2Layer2Constants.cc
@@ -5,7 +5,7 @@ unsigned int l1t::stage2::layer2::mp::nInputFramePerBX = 40;
 unsigned int l1t::stage2::layer2::mp::nOutputFramePerBX = 8;
 
 unsigned int l1t::stage2::layer2::demux::nOutputFramePerBX = 6;
-unsigned int l1t::stage2::layer2::demux::nEGPerLink    = 4;
-unsigned int l1t::stage2::layer2::demux::nTauPerLink   = 4;
+unsigned int l1t::stage2::layer2::demux::nEGPerLink    = 6;
+unsigned int l1t::stage2::layer2::demux::nTauPerLink   = 6;
 unsigned int l1t::stage2::layer2::demux::nJetPerLink   = 6;
 unsigned int l1t::stage2::layer2::demux::nEtSumPerLink = 4;
