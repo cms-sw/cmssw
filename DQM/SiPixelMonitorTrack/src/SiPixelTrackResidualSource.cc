0609fa27c33971c2be47944062c56ca05738a9dd
Loukas Gouskos <Loukas.Gouskos@cern.ch>
2015-10-25 20:13:45 +0100

    additional dqm plot for the pixel barrel occupancy vs module and ladder

--- DQM/SiPixelMonitorTrack/src/SiPixelTrackResidualSource.cc
+++ DQM/SiPixelMonitorTrack/src/SiPixelTrackResidualSource.cc
@@ -480,6 +480,17 @@ void SiPixelTrackResidualSource::bookHistograms(DQMStore::IBooker & iBooker, edm
     meClPosLayersOnTrack.push_back(iBooker.book2D(ss1.str(),ss2.str(),200,-30.,30.,128,-3.2,3.2));
     meClPosLayersOnTrack.at(i-1)->setAxisTitle("Global Z (cm)",1);
     meClPosLayersOnTrack.at(i-1)->setAxisTitle("Global #phi",2);
+
+    int ybins = -1; float ymin = 0.; float ymax = 0.;
+    if (i==1) { ybins = 23; ymin = -11.5; ymax = 11.5; }
+    if (i==2) { ybins = 33; ymin = -17.5; ymax = 17.5; }
+    if (i==3) { ybins = 45; ymin = -24.5; ymax = 24.5; }
+    ss1.str(std::string()); ss1 << "position_" + clustersrc_.label() + "_LadvsMod_Layer_" << i;
+    ss2.str(std::string()); ss2 << "Clusters Layer" << i << "_LadvsMod (on track)";
+    meClPosLayersLadVsModOnTrack.push_back(iBooker.book2D(ss1.str(),ss2.str(),11,-5.5,5.5,ybins,ymin,ymax));
+    meClPosLayersLadVsModOnTrack.at(i-1)->setAxisTitle("z-module",1);
+    meClPosLayersLadVsModOnTrack.at(i-1)->setAxisTitle("Ladder",2);
+
   }
   //fpix
   for (int i = 1; i <= noOfDisks; i++)
@@ -1035,10 +1046,26 @@ void SiPixelTrackResidualSource::analyze(const edm::Event& iEvent, const edm::Ev
 		DBlayer = PixelBarrelName(DetId((*hit).geographicalId()), tTopo, isUpgrade).layerName();
 		float phi = clustgp.phi(); 
 		float z = clustgp.z();
+
+                PixelBarrelName pbn(DetId((*hit).geographicalId()), tTopo, isUpgrade);
+                int ladder = pbn.ladderName();
+                int module = pbn.moduleName();
+
+		PixelBarrelName::Shell sh = pbn.shell(); //enum
+                int ladderSigned=ladder;
+                int moduleSigned=module;
+                // Shell { mO = 1, mI = 2 , pO =3 , pI =4 };
+                int shell = int(sh);
+                // change the module sign for z<0
+                if(shell==1 || shell==2) { moduleSigned = -module; }
+                // change ladeer sign for Outer )x<0)
+                if(shell==1 || shell==3) { ladderSigned = -ladder; }
+
 		for (int i = 0; i < noOfLayers; i++)
 		  {
           if (DBlayer == i + 1) {
              meClPosLayersOnTrack.at(i)->Fill(z,phi);
+	     meClPosLayersLadVsModOnTrack.at(i)->Fill(moduleSigned,ladderSigned);
              meClChargeOnTrack_layers.at(i)->Fill(corrCharge);
              meClSizeOnTrack_layers.at(i)->Fill((*clust).size());
              meClSizeXOnTrack_layers.at(i)->Fill((*clust).sizeX());
