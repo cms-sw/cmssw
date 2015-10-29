5687087a482ac7c22fd306ef2bd04e86dd27de43
Geonhee Oh <geonhee.oh@cern.ch>
2015-10-29 13:43:03 +0100

    dimuonHistograms mass, bin

--- DQMOffline/Configuration/python/DQMOfflineHeavyIons_cff.py
+++ DQMOffline/Configuration/python/DQMOfflineHeavyIons_cff.py
@@ -30,9 +30,12 @@ DQMOfflineHeavyIonsDPG = cms.Sequence( DQMOfflineHeavyIonsPreDPG *
                                        DQMMessageLogger )
 
 from DQMOffline.Muon.muonMonitors_cff import *
-diMuonHistos.etaBin = cms.int32(70)
+diMuonHistos.etaBin = cms.int32(70) #dimuonhistograms mass, bin
 diMuonHistos.etaBBin = cms.int32(70)
 diMuonHistos.etaEBin = cms.int32(35)
+diMuonHistos.etaBinLM = cms.int32(12)
+diMuonHistos.etaBBinLM = cms.int32(12)
+diMuonHistos.etaEBinLM = cms.int32(6)
 diMuonHistos.LowMassMin = cms.double(2.0)
 diMuonHistos.LowMassMax = cms.double(14.0)
 diMuonHistos.HighMassMin = cms.double(55.0)
