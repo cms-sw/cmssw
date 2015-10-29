370b1a87ec8d3b4009ed849f6b9df3edbb2e63b9
Hugo <hugo.delannoy@hotmail.com>
2015-10-29 17:11:37 +0100

    Fix the way the folder is handled in SiPixelHLT

--- DQM/SiPixelMonitorRawData/python/SiPixelMonitorHLT_cfi.py
+++ DQM/SiPixelMonitorRawData/python/SiPixelMonitorHLT_cfi.py
@@ -1,13 +1,12 @@
 import FWCore.ParameterSet.Config as cms
 
 SiPixelHLTSource = cms.EDAnalyzer("SiPixelHLTSource",
-    TopFolderName = cms.string('Pixel'),
+    DirName = cms.untracked.string('Pixel/FEDIntegrity/'),
     RawInput = cms.InputTag("rawDataCollector"),
     ErrorInput = cms.InputTag("siPixelDigis"),
     outputFile = cms.string('Pixel_DQM_HLT.root'),
     saveFile = cms.untracked.bool(False),
-    slowDown = cms.untracked.bool(False),
-    DirName = cms.untracked.string('Pixel/FEDIntegrity/')
+    slowDown = cms.untracked.bool(False)
 )
 
 
