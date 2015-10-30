34ee08d0c00d1c7d78ea9d5593ae66d37dad2e81
Matti Kortelainen <matti.kortelainen@cern.ch>
2015-10-30 13:42:41 +0100

    Use doubles in intermediate steps in liblogintpack
    Using floats causes roundings. It happens that e.g. with
      int8_t packceil(double x) { return logintpack::pack8logCeil(x, -15, 0); }
      double unpack(int8_t x)   { return logintpack::unpack8log  (x, -15, 0); }
    
    the condition "unpack(packceil(v)) >= v" does not hold for some v
    (e.g. std:exp(-15.f + 1/128.f*15.f) + 1ulp, which is in the added unit
    test).

--- DataFormats/PatCandidates/test/testlogintpack.cpp
+++ DataFormats/PatCandidates/test/testlogintpack.cpp
@@ -1,6 +1,7 @@
 #include <cppunit/extensions/HelperMacros.h>
 #include <iostream>
 #include <iomanip>
+#include <limits>
 
 #include "DataFormats/PatCandidates/interface/liblogintpack.h"
 
@@ -20,11 +21,11 @@ private:
 };
 
 namespace {
-  double pack(double x)         { return logintpack::pack8log        (x, -15, 0); }
-  double packceil(double x)     { return logintpack::pack8logCeil    (x, -15, 0); }
-  double packclosed(double x)   { return logintpack::pack8log        (x, -15, 0); }
-  double unpack(int8_t x)       { return logintpack::unpack8log      (x, -15, 0); }
-  double unpackclosed(int8_t x) { return logintpack::unpack8logClosed(x, -15, 0); }
+  int8_t pack(double x)          { return logintpack::pack8log        (x, -15, 0); }
+  int8_t packceil(double x)      { return logintpack::pack8logCeil    (x, -15, 0); }
+  int8_t packclosed(double x)    { return logintpack::pack8log        (x, -15, 0); }
+  double unpack(int8_t x)        { return logintpack::unpack8log      (x, -15, 0); }
+  double unpackclosed(int8_t x)  { return logintpack::unpack8logClosed(x, -15, 0); }
 }
 
 void testlogintpack::test() {
@@ -33,9 +34,9 @@ void testlogintpack::test() {
   constexpr int8_t largestPositive = 127;
   constexpr int8_t largestNegative = -127;
 
-  const float smallestValuePos = std::exp(-15.f);
-  const float smallestValueNeg = -std::exp(-15.f+1.f/128.f*15.f);
-  const float smallestValueNegForClosed = -std::exp(-15.f+1.f/127.f*15.f);
+  const double smallestValuePos = std::exp(-15.);
+  const double smallestValueNeg = -std::exp(-15.+1./128.*15.);
+  const double smallestValueNegForClosed = -std::exp(-15.+1./127.*15.);
   CPPUNIT_ASSERT(pack(smallestValuePos) == smallestPositive);
   CPPUNIT_ASSERT(packceil(smallestValuePos) == smallestPositive);
   CPPUNIT_ASSERT(packclosed(smallestValuePos) == smallestPositive);
@@ -49,9 +50,8 @@ void testlogintpack::test() {
   CPPUNIT_ASSERT(unpack(packceil(smallestValueNeg)) == smallestValueNeg);
   CPPUNIT_ASSERT(unpackclosed(packclosed(smallestValueNegForClosed)) == smallestValueNegForClosed);
 
-  const float largestValuePos = std::exp(-15.f+127.f/128.f*15.f);
-  const float largestValueNeg = -largestValuePos;
-  CPPUNIT_ASSERT(pack(std::exp(0.f)) == largestPositive); // this one actually overflows
+  const double largestValuePos = std::exp(-15.+127./128.*15.);
+  const double largestValueNeg = -largestValuePos;
   CPPUNIT_ASSERT(pack(largestValuePos) == largestPositive);
   CPPUNIT_ASSERT(packceil(largestValuePos) == largestPositive);
   CPPUNIT_ASSERT(unpack(largestPositive) == largestValuePos);
@@ -60,12 +60,32 @@ void testlogintpack::test() {
   CPPUNIT_ASSERT(packceil(largestValueNeg) == largestNegative);
   CPPUNIT_ASSERT(unpack(largestNegative) == largestValueNeg);
 
-  const float largestValueClosedPos = std::exp(0.f);
-  const float largestValueClosedNeg = -largestValueClosedPos;
+  const double largestValueClosedPos = std::exp(0.);
+  const double largestValueClosedNeg = -largestValueClosedPos;
   CPPUNIT_ASSERT(packclosed(largestValueClosedPos) == largestPositive);
   CPPUNIT_ASSERT(unpackclosed(largestPositive) == largestValueClosedPos);
   CPPUNIT_ASSERT(packclosed(largestValueClosedNeg) == largestNegative);
   CPPUNIT_ASSERT(unpackclosed(largestNegative) == largestValueClosedNeg);
+
+  const double someValue = std::exp(-15. + 1/128.*15.);
+  const float someValueFloat = std::exp(-15.f + 1/128.f*15.f);
+  CPPUNIT_ASSERT(unpack(packceil(someValue)) == someValue);
+  CPPUNIT_ASSERT(static_cast<float>(unpack(packceil(someValue))) == someValueFloat);
+  {
+    union { float flt; uint32_t i32; } conv;
+    conv.flt = someValueFloat;
+    conv.i32 += 1;
+    const float someValuePlus1Ulp32 = conv.flt;
+    CPPUNIT_ASSERT(static_cast<float>(unpack(packceil(someValuePlus1Ulp32))) >= someValuePlus1Ulp32);
+  }
+  {
+    union { double flt; uint64_t i64; } conv;
+    conv.flt = someValue;
+    conv.i64 += 1;
+    const float someValuePlus1Ulp64 = conv.flt;
+    CPPUNIT_ASSERT(unpack(packceil(someValuePlus1Ulp64)) >= someValuePlus1Ulp64);
+  }
+
 }
 
 CPPUNIT_TEST_SUITE_REGISTRATION(testlogintpack);
