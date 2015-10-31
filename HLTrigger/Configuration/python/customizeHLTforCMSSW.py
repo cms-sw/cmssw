da6bc8d23cc2dd0e127aa104e60c0b879ed77dd5
Andrea <perrotta@cern.ch>
2015-10-31 08:47:22 +0100

    Update customization for CMSSW_7_5_X

--- HLTrigger/Configuration/python/customizeHLTforCMSSW.py
+++ HLTrigger/Configuration/python/customizeHLTforCMSSW.py
@@ -3,7 +3,7 @@ import FWCore.ParameterSet.Config as cms
 # Update to replace old jet corrector mechanism
 from HLTrigger.Configuration.customizeHLTforNewJetCorrectors import customizeHLTforNewJetCorrectors
 
-# Possibility to put different ring dependent cut on ADC (PR #9232)                                                              
+# Possibility to put different ring dependent cut on ADC (PR #9232)
 def customiseFor9232(process):
     if hasattr(process,'hltEcalPhiSymFilter'):
         if hasattr(process.hltEcalPhiSymFilter,'ampCut_barrel'):
@@ -120,10 +120,11 @@ def customiseHLTforCMSSW(process, menuType="GRun", fastSim=False):
     import os
     cmsswVersion = os.environ['CMSSW_VERSION']
 
+    if cmsswVersion >= "CMSSW_7_6":
+        process = customiseFor8679(process)
     if cmsswVersion >= "CMSSW_7_5":
         process = customiseFor10927(process)
         process = customiseFor9232(process)
-        process = customiseFor8679(process)
         process = customiseFor8356(process)
         process = customiseFor7966(process)
         process = customiseFor7794(process)
