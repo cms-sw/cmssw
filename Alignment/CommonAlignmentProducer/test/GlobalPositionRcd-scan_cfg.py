334d6292b9522b582d7bbc71eb209330f369f4f2
Giacomo Govi <giacomo.govi@cern.ch>
2015-11-05 15:03:30 +0100

    Removed dependecies on conddb V1 packages

--- Alignment/CommonAlignmentProducer/test/GlobalPositionRcd-scan_cfg.py
+++ Alignment/CommonAlignmentProducer/test/GlobalPositionRcd-scan_cfg.py
@@ -2,7 +2,7 @@
 import FWCore.ParameterSet.Config as cms
 
 process = cms.Process("read")
-process.load("CondCore.DBCommon.CondDBSetup_cfi")
+process.load("CondCore.CondDB.CondDB_cfi")
 
 process.load("FWCore.MessageLogger.MessageLogger_cfi")
 process.MessageLogger.cerr.FwkReport.reportEvery = cms.untracked.int32(1000000)
