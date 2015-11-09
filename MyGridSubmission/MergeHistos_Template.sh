9ce2dea1acbf89c28fe222c36d25fdd99d884a20
piet verwilligen <piet.verwilligen@cern.ch>
2015-11-09 13:12:46 +0100

    updated script

--- MyGridSubmission/MergeHistos_Template.sh
+++ MyGridSubmission/MergeHistos_Template.sh
@@ -2,17 +2,17 @@
 source  /cvmfs/cms.cern.ch/cmsset_default.sh
 source /cmshome/piet/slc6_ME0Seg_SLHC_BATCH
  
-cd /lustre/home/piet/RunOnNode/ME0Analysis/ME0AnalysisDavid/Merge_SAMPLE/
+cd /lustre/home/piet/RunOnNode/ME0Analysis/ANALYSISDIR/Merge_SAMPLE/
 
 ### Copy All Files  ################################################################################
 cp /cmshome/piet/SLC6/ME0_Studies/SLHC26_InTimeOutOfTimePU/CMSSW_6_2_0_SLHC26_patch3/src/MyGridSubmission/MergeList_SAMPLE.txt MergeList.txt
 for i in `cat MergeList.txt`
 do
-  echo "copy /lustre/cms/store/user/piet/ME0Segment_Time/AnalysisDavid/DYToMuMu_M-20_TuneZ2star_14TeV-pythia6-tauola/SAMPLE/SUBMITDATE/0000/${i}"
-  cp /lustre/cms/store/user/piet/ME0Segment_Time/AnalysisDavid/DYToMuMu_M-20_TuneZ2star_14TeV-pythia6-tauola/SAMPLE/SUBMITDATE/0000/${i} ${i}
+  echo "copy /lustre/cms/store/user/piet/ME0Segment_Time/ANALYSISDIR/DYToMuMu_M-20_TuneZ2star_14TeV-pythia6-tauola/SAMPLE/SUBMITDATE/0000/${i}"
+  cp /lustre/cms/store/user/piet/ME0Segment_Time/ANALYSISDIR/DYToMuMu_M-20_TuneZ2star_14TeV-pythia6-tauola/SAMPLE/SUBMITDATE/0000/${i} ${i}
 done
 ### Merge All Files ################################################################################
-hadd ME0MuonAnalyzerOutput_SAMPLE.root `cat MergeList.txt | awk '{printf $1" "}'`
+hadd ME0Histos_SAMPLE.root `cat MergeList.txt | awk '{printf $1" "}'`
 ### Clean Files     ################################################################################
 for i in `cat MergeList.txt`
 do
@@ -20,7 +20,7 @@ do
   rm -rf ${i}
 done
 rm -rf MergeList.txt
-cp ME0MuonAnalyzerOutput_SAMPLE.root ..
+cp ME0Histos_SAMPLE.root ..
 cd ..
 rm -rf Merge_SAMPLE
 ####################################################################################################
