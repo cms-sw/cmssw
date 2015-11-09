6219d27ab8f6fc8b88802cb7ccca98c07ae30348
piet verwilligen <piet.verwilligen@cern.ch>
2015-11-09 13:12:12 +0100

    updated dasqueries

--- MyGridSubmission/DasQueries.sh
+++ MyGridSubmission/DasQueries.sh
@@ -7,6 +7,8 @@
 
 # python das_client.py --query="file dataset=/DYToMuMu_M-20_TuneZ2star_14TeV-pythia6-tauola/piet-DYToMuMu_M-20_TuneZ2star_14TeV-pythia6-tauola_HGCALGS_PU140_100ps_1p5ns_RECO-a6c1ab73bd1959e4a7fbbca874362562/USER instance=prod/phys03" --limit=100
 
+python das_client.py --query="dataset=/DYToMuMu_M-20_TuneZ2star_14TeV-pythia6-tauola/piet-DYToMuMu_M-20_TuneZ2star_14TeV-pythia6-tauola_HGCALGS_PU140_*Neutr*_RECO-*/USER instance=prod/phys03" --limit=100
+
 #### Datasets Piet ####
 #######################
 # python das_client.py --query="dataset=/DYToMuMu_M-20_TuneZ2star_14TeV-pythia6-tauola/piet-DYToMuMu_M-20_TuneZ2star_14TeV-pythia6-tauola_HGCALGS_PU140_*v2*_RECO-*/USER instance=prod/phys03" --limit=100
@@ -44,4 +46,5 @@
 # echo "First 10 files for Dataset ${DataDavid}"
 # python das_client.py --query="file dataset=${DataDavid} instance=prod/phys03" --limit=10
 
-python das_client.py --query="parent file=/store/group/upgrade/muon/ME0GlobalReco/ME0MuonReRun_DY_SLHC23patch1_SegmentReRunFullRun_ForPublish/M-20_TuneZ2star_14TeV_6_2_0_SLHC23patch1_2023/DYToMuMu_M-20_TuneZ2star_14TeV-pythia6-tauola/DYToMuMu_M-20_TuneZ2star_14TeV-pythia6-tauola_2023SHCalNoTaper_PU140_Selectors_RECO/b52ce42d5986c94dc336f39e015d825e/output_896_2_3B7.root instance=prod/phys03" --limit=10
+# python das_client.py --query="parent file=/store/group/upgrade/muon/ME0GlobalReco/ME0MuonReRun_DY_SLHC23patch1_SegmentReRunFullRun_ForPublish/M-20_TuneZ2star_14TeV_6_2_0_SLHC23patch1_2023/DYToMuMu_M-20_TuneZ2star_14TeV-pythia6-tauola/DYToMuMu_M-20_TuneZ2star_14TeV-pythia6-tauola_2023SHCalNoTaper_PU140_Selectors_RECO/b52ce42d5986c94dc336f39e015d825e/output_896_2_3B7.root instance=prod/phys03" --limit=10
+
