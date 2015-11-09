5b60f3999a10801453cdd250d19dbb44e4249243
piet verwilligen <piet.verwilligen@cern.ch>
2015-11-09 13:08:38 +0100

    reco only muons as configuration parameter

--- RecoLocalMuon/GEMRecHit/python/me0RecHits_cfi.py
+++ RecoLocalMuon/GEMRecHit/python/me0RecHits_cfi.py
@@ -2,9 +2,8 @@ import FWCore.ParameterSet.Config as cms
 
 me0RecHits = cms.EDProducer("ME0RecHitProducer",
     recAlgoConfig = cms.PSet(
-
+        recOnlyMuons = cms.bool(False),
     ),
     recAlgo = cms.string('ME0RecHitStandardAlgo'),
     me0DigiLabel = cms.InputTag("simMuonME0Digis"),
-    recOnlyMuons = cms.bool(False),
 )
