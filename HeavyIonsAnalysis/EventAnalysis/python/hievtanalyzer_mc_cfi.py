8aafc061286d361da3c3b11265c89e971f07adea
Marta <marta.verweij@cern.ch>
2015-11-10 18:23:34 +0100

    update pyton configs

--- HeavyIonsAnalysis/EventAnalysis/python/hievtanalyzer_mc_cfi.py
+++ HeavyIonsAnalysis/EventAnalysis/python/hievtanalyzer_mc_cfi.py
@@ -1,4 +1,4 @@
 import FWCore.ParameterSet.Config as cms
 from HeavyIonsAnalysis.EventAnalysis.hievtanalyzer_data_cfi import *
 hiEvtAnalyzer.doMC          = cms.bool(True)
-
+hiEvtAnalyzer.useHepMC      = cms.bool(False)
