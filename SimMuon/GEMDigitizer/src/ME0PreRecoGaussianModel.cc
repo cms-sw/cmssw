48ae8e9209ed5a0c0dc63e92e9d969ee80d58034
piet verwilligen <piet.verwilligen@cern.ch>
2015-11-11 18:53:46 +0100

    distribute neutron background uniform in time, porting to CMSSW_8_0_0 of PR 12370

--- SimMuon/GEMDigitizer/src/ME0PreRecoGaussianModel.cc
+++ SimMuon/GEMDigitizer/src/ME0PreRecoGaussianModel.cc
@@ -11,41 +11,30 @@
 #include <utility>
 #include <map>
 
+const double cspeed = 29.9792458; // [cm/ns]
+const int bxwidth   = 25;         // [ns]
+
 ME0PreRecoGaussianModel::ME0PreRecoGaussianModel(const edm::ParameterSet& config) :
-  ME0DigiPreRecoModel(config)
-, sigma_t(config.getParameter<double> ("timeResolution"))
-, sigma_u(config.getParameter<double> ("phiResolution"))
-, sigma_v(config.getParameter<double> ("etaResolution"))
-, corr(config.getParameter<bool> ("useCorrelation"))
-, etaproj(config.getParameter<bool> ("useEtaProjectiveGEO"))
-, digitizeOnlyMuons_(config.getParameter<bool> ("digitizeOnlyMuons"))
-, averageEfficiency_(config.getParameter<double> ("averageEfficiency"))
-, doBkgNoise_(config.getParameter<bool> ("doBkgNoise"))
-, simulateIntrinsicNoise_(config.getParameter<bool> ("simulateIntrinsicNoise"))
-, simulateElectronBkg_(config.getParameter<bool>("simulateElectronBkg"))
-, averageNoiseRate_(config.getParameter<double> ("averageNoiseRate"))
-, bxwidth_(config.getParameter<int> ("bxwidth"))
-, minBunch_(config.getParameter<int> ("minBunch"))
-, maxBunch_(config.getParameter<int> ("maxBunch"))
-
+  ME0DigiPreRecoModel(config), 
+  sigma_t(config.getParameter<double>("timeResolution")), 
+  sigma_u(config.getParameter<double>("phiResolution")), 
+  sigma_v(config.getParameter<double>("etaResolution")), 
+  corr(config.getParameter<bool>("useCorrelation")), 
+  etaproj(config.getParameter<bool>("useEtaProjectiveGEO")), 
+  digitizeOnlyMuons_(config.getParameter<bool>("digitizeOnlyMuons")), 
+  averageEfficiency_(config.getParameter<double>("averageEfficiency")), 
+  // simulateIntrinsicNoise_(config.getParameter<bool>("simulateIntrinsicNoise")),
+  // averageNoiseRate_(config.getParameter<double>("averageNoiseRate")), 
+  simulateElectronBkg_(config.getParameter<bool>("simulateElectronBkg")), 
+  simulateNeutralBkg_(config.getParameter<bool>("simulateNeutralBkg")), 
+  minBunch_(config.getParameter<int>("minBunch")), 
+  maxBunch_(config.getParameter<int>("maxBunch"))
 {
-  //params for the simple pol6 model of neutral and electron bkg for ME0:
-  ME0ModNeuBkgParam0 = 5.69e+06;
-  ME0ModNeuBkgParam1 = -293334;
-  ME0ModNeuBkgParam2 = 6279.6;
-  ME0ModNeuBkgParam3 = -71.2928;
-  ME0ModNeuBkgParam4 = 0.452244;
-  ME0ModNeuBkgParam5 = -0.0015191;
-  ME0ModNeuBkgParam6 = 2.1106e-06;
-
-  ME0ModElecBkgParam0 = 3.77712e+06;
-  ME0ModElecBkgParam1 = -199280;
-  ME0ModElecBkgParam2 = 4340.69;
-  ME0ModElecBkgParam3 = -49.922;
-  ME0ModElecBkgParam4 = 0.319699;
-  ME0ModElecBkgParam5 = -0.00108113;
-  ME0ModElecBkgParam6 = 1.50889e-06;
-
+  // polynomial parametrisation of neutral (n+g) and electron background
+  neuBkg.push_back(5.69e+06);     neuBkg.push_back(-293334);     neuBkg.push_back(6279.6); 
+  neuBkg.push_back(-71.2928);     neuBkg.push_back(0.452244);    neuBkg.push_back(-0.0015191);  neuBkg.push_back(2.1106e-06);
+  eleBkg.push_back(3.77712e+06);  eleBkg.push_back(-199280);     eleBkg.push_back(4340.69);
+  eleBkg.push_back(-49.922);      eleBkg.push_back(0.319699);    eleBkg.push_back(-0.00108113); eleBkg.push_back(1.50889e-06);
 }
 
 ME0PreRecoGaussianModel::~ME0PreRecoGaussianModel()
@@ -56,10 +45,11 @@ void ME0PreRecoGaussianModel::simulateSignal(const ME0EtaPartition* roll, const 
 {
 for (const auto & hit: simHits)
 {
+  // Digitize only Muons?
   if (std::abs(hit.particleType()) != 13 && digitizeOnlyMuons_) continue;
-  // GEM efficiency
+  // is GEM efficient?
   if (CLHEP::RandFlat::shoot(engine) > averageEfficiency_) continue;
-
+  // create digi
   auto entry = hit.entryPoint();
   float x=CLHEP::RandGaussQ::shoot(engine, entry.x(), sigma_u);
   float y=CLHEP::RandGaussQ::shoot(engine, entry.y(), sigma_v);
@@ -68,7 +58,6 @@ for (const auto & hit: simHits)
   float corr=0.;
   float tof=CLHEP::RandGaussQ::shoot(engine, hit.timeOfFlight(), sigma_t);
   int pdgid = hit.particleType();
-  // please keep hit time always 0 for this model
   ME0DigiPreReco digi(x,y,ex,ey,corr,tof,pdgid);
   digi_.insert(digi);
 }
@@ -76,117 +65,114 @@ for (const auto & hit: simHits)
 
 void ME0PreRecoGaussianModel::simulateNoise(const ME0EtaPartition* roll, CLHEP::HepRandomEngine* engine)
 {
-  const double cspeed = 299792458;
   double trArea(0.0);
   const ME0DetId me0Id(roll->id());
 
-  if (me0Id.region() == 0)
-  {
-    throw cms::Exception("Geometry")
-        << "GEMSynchronizer::simulateNoise() - this GEM id is from barrel, which cannot happen.";
-  }
-  const TrapezoidalStripTopology* top_(dynamic_cast<const TrapezoidalStripTopology*> (&(roll->topology())));
+  // Extract detailed information from the Strip Topology:
+  // base_bottom, base_top, height, strips, pads
+  // note that (0,0) is in the middle of the roll ==> all param are at all half length
+  if (me0Id.region() == 0) { throw cms::Exception("Geometry") << "Asking TrapezoidalStripTopology from a ME0 will fail"; } // not sure we really need this
+  const TrapezoidalStripTopology* top_(dynamic_cast<const TrapezoidalStripTopology*>(&(roll->topology())));
 
-  // base_bottom, base_top, height, strips, pads (all half length)
   auto& parameters(roll->specs()->parameters());
-  float semiBottomEdge(parameters[0]);
-  float semiTopEdge(parameters[1]);
-  float semiHeight(parameters[2]);
-  float myTanPhi = (semiTopEdge - semiBottomEdge) / (semiHeight * 2);
+  float bottomLength(parameters[0]); bottomLength = 2*bottomLength;
+  float topLength(parameters[1]);    topLength    = 2*topLength;
+  float height(parameters[2]);       height       = 2*height;
+  float myTanPhi    = (topLength - bottomLength) / (height * 2);
   double rollRadius = top_->radius();
-  const int nBxing(maxBunch_ - minBunch_ + 1);
-  trArea = 2 * semiHeight * (semiTopEdge + semiBottomEdge);
+  trArea = height * (topLength + bottomLength) / 2.0;
 
-  //simulate bkg contribution
-  if (!doBkgNoise_)
-    return;
+  // simulate intrinsic noise and background hits in all BX that are being read out
+  for(int bx=minBunch_; bx<maxBunch_+1; ++bx) {
 
-  double aveNeutrRateBotRoll = 0.;
-  double averageNoiseElectronRatePerRoll = 0.;
-  int pdgid = 0;
+    // 1) Intrinsic Noise ... Not implemented right now
+    // ------------------------------------------------
+    // if (simulateIntrinsicNoise_)
+    // {                           
+    // }                           
 
-  float myRand = CLHEP::RandFlat::shoot(engine);
-  float yy_rand = 2 * semiHeight * (myRand - 0.5);
+    // 2) Background Noise         
+    // ----------------------------
+    // 2a) electron background     
+    // -----------------------     
+    if (simulateElectronBkg_) {
 
-  double radius_rand = rollRadius + yy_rand;
+      float myRandY = CLHEP::RandFlat::shoot(engine);
+      float yy_rand = height * (myRandY - 0.5); // random Y coord in Local Coords
+      double yy_glob = rollRadius + yy_rand;    // random Y coord in Global Coords
 
-    if(simulateElectronBkg_)
-    averageNoiseElectronRatePerRoll = ME0ModElecBkgParam0
-                      + ME0ModElecBkgParam1 * radius_rand
-                      + ME0ModElecBkgParam2 * radius_rand * radius_rand
-                      + ME0ModElecBkgParam3 * radius_rand * radius_rand * radius_rand
-                      + ME0ModElecBkgParam4 * radius_rand * radius_rand * radius_rand * radius_rand
-                      + ME0ModElecBkgParam5 * radius_rand * radius_rand * radius_rand * radius_rand * radius_rand
-                      + ME0ModElecBkgParam6 * radius_rand * radius_rand * radius_rand * radius_rand * radius_rand * radius_rand;
+      // Extract / Calculate the Average Electron Rate
+      // for the given global Y coord from Parametrization
+      double averageElectronRatePerRoll = 0.0;
+      for(int j=0; j<7; ++j) { averageElectronRatePerRoll += eleBkg[j]*pow(yy_glob,j); }
 
-  const double averageNoiseElec(averageNoiseElectronRatePerRoll * nBxing * bxwidth_ * trArea * 1.0e-9);
-  const int n_elechits(CLHEP::RandPoissonQ::shoot(engine, averageNoiseElec));
+      // Rate [Hz/cm^2] * 25*10^-9 [s] * Area [cm] = # hits in this roll
+      const double averageElecRate(averageElectronRatePerRoll * (bxwidth*1.0e-9) * trArea);
+      int n_elechits(CLHEP::RandPoissonQ::shoot(engine, averageElecRate));
 
-  double xMax = semiTopEdge - (semiHeight - yy_rand) * myTanPhi;
-  for (int i = 0; i < n_elechits; ++i)
-  {
-    //calculate xx_rand at a given yy_rand
-    float myRandX = CLHEP::RandFlat::shoot(engine);
-    float xx_rand = 2 * xMax * (myRandX - 0.5);
+      // max length in x for given y coordinate (cfr trapezoidal eta partition)
+      double xMax = topLength/2.0 - (height/2.0 - yy_rand) * myTanPhi;
 
-    float ex = sigma_u;
-    float ey = sigma_v;
-    float corr = 0.;
+      // loop over amount of electron hits in this roll
+      for (int i = 0; i < n_elechits; ++i) {
+        //calculate xx_rand at a given yy_rand
+        float myRandX = CLHEP::RandFlat::shoot(engine);
+        float xx_rand = 2 * xMax * (myRandX - 0.5);
+        float ex = sigma_u;
+        float ey = sigma_v;
+        float corr = 0.;
+        // extract random time in this BX
+        float myrandT = CLHEP::RandFlat::shoot(engine);
+	float minBXtime = (bx-0.5)*bxwidth;      // float maxBXtime = (bx+0.5)*bxwidth;
+        float time = myrandT*bxwidth+minBXtime;
+        float myrandP = CLHEP::RandFlat::shoot(engine);
+        int pdgid = 0;
+        if (myrandP <= 0.5) pdgid = -11; // electron                                   
+        else                pdgid = 11;  // positron                                   
+        ME0DigiPreReco digi(xx_rand, yy_rand, ex, ey, corr, time, pdgid);
+        digi_.insert(digi);
+      }
+    }
 
-    GlobalPoint pointDigiHit = roll->toGlobal(LocalPoint(xx_rand, yy_rand));
-    //calc tof to the random estimated point
-    double stripRadius = sqrt(pointDigiHit.x() * pointDigiHit.x() + pointDigiHit.y() * pointDigiHit.y()
-        + pointDigiHit.z() * pointDigiHit.z());
-    double timeCalibrationOffset_ = (stripRadius * 1e+9) / (cspeed * 1e+2); //[ns]
-    float tof = CLHEP::RandGaussQ::shoot(engine, timeCalibrationOffset_, sigma_t);
+    // 2b) neutral (n+g) background                                                    
+    // ----------------------------                                                    
+    if (simulateNeutralBkg_) {
 
-    float myrand = CLHEP::RandFlat::shoot(engine);
-    if (myrand <= 0.5)
-      pdgid = -11;
-    else
-      pdgid = 11;
+      float myRandY = CLHEP::RandFlat::shoot(engine);
+      float yy_rand = height * (myRandY - 0.5); // random Y coord in Local Coords
+      double yy_glob = rollRadius + yy_rand;    // random Y coord in Global Coords
+      // Extract / Calculate the Average Electron Rate                            
+      // for the given global Y coord from Parametrization                        
+      double averageNeutralRatePerRoll = 0.0;
+      for(int j=0; j<7; ++j) { averageNeutralRatePerRoll += neuBkg[j]*pow(yy_glob,j); }
+      // Rate [Hz/cm^2] * 25*10^-9 [s] * Area [cm] = # hits in this roll          
+      const double averageNeutrRate(averageNeutralRatePerRoll * (bxwidth*1.0e-9) * trArea);
+      int n_hits(CLHEP::RandPoissonQ::shoot(engine, averageNeutrRate));
 
-    ME0DigiPreReco digi(xx_rand, yy_rand, ex, ey, corr, tof, pdgid);
-    digi_.insert(digi);
-  }
+      // max length in x for given y coordinate (cfr trapezoidal eta partition)   
+      double xMax = topLength/2.0 - (height/2.0 - yy_rand) * myTanPhi;
 
-  aveNeutrRateBotRoll = ME0ModNeuBkgParam0
-                      + ME0ModNeuBkgParam1 * radius_rand
-                      + ME0ModNeuBkgParam2 * radius_rand * radius_rand
-                      + ME0ModNeuBkgParam3 * radius_rand * radius_rand * radius_rand
-                      + ME0ModNeuBkgParam4 * radius_rand * radius_rand * radius_rand * radius_rand
-                      + ME0ModNeuBkgParam5 * radius_rand * radius_rand * radius_rand * radius_rand * radius_rand
-                      + ME0ModNeuBkgParam6 * radius_rand * radius_rand * radius_rand * radius_rand * radius_rand * radius_rand;
+      // loop over amount of neutral hits in this roll                            
+      for (int i = 0; i < n_hits; ++i) {
+        //calculate xx_rand at a given yy_rand                                    
+        float myRandX = CLHEP::RandFlat::shoot(engine);
+        float xx_rand = 2 * xMax * (myRandX - 0.5);
+        float ex = sigma_u;
+        float ey = sigma_v;
+        float corr = 0.;
+        // extract random time in this BX                                         
+        float myrandT = CLHEP::RandFlat::shoot(engine);
+        float minBXtime = (bx-0.5)*bxwidth;
+        float time = myrandT*bxwidth+minBXtime;
+        int pdgid = 0;
+        float myrandP = CLHEP::RandFlat::shoot(engine);
+        if (myrandP <= 0.08) pdgid = 2112; // neutrons: GEM sensitivity for neutrons: 0.08%
+        else                 pdgid = 22;   // photons:  GEM sensitivity for photons:  1.04% ==> neutron fraction = (0.08 / 1.04) = 0.077 = 0.08
+        ME0DigiPreReco digi(xx_rand, yy_rand, ex, ey, corr, time, pdgid);
+        digi_.insert(digi);
+      }
+    }
 
-  const double averageNoiseNeutral(aveNeutrRateBotRoll * nBxing * bxwidth_ * trArea * 1.0e-9);
-  const int n_hits(CLHEP::RandPoissonQ::shoot(engine, averageNoiseNeutral));
-
-  for (int i = 0; i < n_hits; ++i)
-  {
-    //calculate xx_rand at a given yy_rand
-    float myRandX = CLHEP::RandFlat::shoot(engine);
-    float xx_rand = 2 * xMax * (myRandX - 0.5);
-
-    float ex = sigma_u;
-    float ey = sigma_v;
-    float corr = 0.;
-
-    GlobalPoint pointDigiHit = roll->toGlobal(LocalPoint(xx_rand, yy_rand));
-    //calc tof to the random estimated point
-    double stripRadius = sqrt(pointDigiHit.x() * pointDigiHit.x() + pointDigiHit.y() * pointDigiHit.y()
-        + pointDigiHit.z() * pointDigiHit.z());
-    double timeCalibrationOffset_ = (stripRadius * 1e+9) / (cspeed * 1e+2); //[ns]
-    float tof = CLHEP::RandGaussQ::shoot(engine, timeCalibrationOffset_, sigma_t);
-
-    //distribute bkg between neutrons and gammas
-    float myrand = CLHEP::RandFlat::shoot(engine);
-    if (myrand <= 0.1)
-      pdgid = 2112; // neutrons
-    else
-      pdgid = 22;
-
-    ME0DigiPreReco digi(xx_rand, yy_rand, ex, ey, corr, tof, pdgid);
-    digi_.insert(digi);
-  }
+  } // end loop over bx                                  
 }
 
