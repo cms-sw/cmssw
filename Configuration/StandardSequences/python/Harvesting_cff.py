3e47aafe5dea48561c85c7a2b4da2d15c1aecb17
Arnaud <arnaud.chiron@cern.ch>
2015-11-12 16:52:39 +0100

    added electronPostValidationSequenceMiniAOD

--- Configuration/StandardSequences/python/Harvesting_cff.py
+++ Configuration/StandardSequences/python/Harvesting_cff.py
@@ -33,4 +33,4 @@ genHarvesting = cms.Path(postValidation_gen)
 
 alcaHarvesting = cms.Path()
 
-validationHarvestingMiniAOD = cms.Path(JetPostProcessor*METPostProcessorHarvesting)
+validationHarvestingMiniAOD = cms.Path(JetPostProcessor*METPostProcessorHarvesting*electronPostValidationSequenceMiniAOD)
