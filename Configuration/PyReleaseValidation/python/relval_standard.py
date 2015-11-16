6f7d0955e322c50684c7b23be5e88e3d3383f409
Hengne <Hengne.Li@CERN.CH>
2015-11-16 11:44:14 +0100

    add back accidentally removed workflow

--- Configuration/PyReleaseValidation/python/relval_standard.py
+++ Configuration/PyReleaseValidation/python/relval_standard.py
@@ -248,6 +248,7 @@ workflows[1313] = ['', ['QCD_Pt_3000_3500_13','DIGIUP15','RECOUP15','HARVESTUP15
 workflows.addOverride(1313,overridesEv5)
 workflows[1339] = ['', ['QCD_Pt_600_800_13','DIGIUP15','RECOUP15','HARVESTUP15']]
 
+workflows[1347] = ['', ['Upsilon1SToMuMu_13','DIGIUP15','RECOUP15','HARVESTUP15']]
 
 workflows[1349] = ['', ['BsToMuMu_13','DIGIUP15','RECOUP15','HARVESTUP15']]
 workflows[1350] = ['', ['JpsiMuMu_Pt-15','DIGIUP15','RECOUP15','HARVESTUP15']]
