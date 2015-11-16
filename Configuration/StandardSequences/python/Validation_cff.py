5f690c5eeee17ffdae70797cd49a38b7e49c37f2
Arnaud <arnaud.chiron@cern.ch>
2015-11-16 14:01:14 +0100

    last modification with egammaValidation file

--- Configuration/StandardSequences/python/Validation_cff.py
+++ Configuration/StandardSequences/python/Validation_cff.py
@@ -29,14 +29,14 @@ from Validation.RecoMuon.muonValidationHLT_cff import *
 from Validation.EventGenerator.BasicGenValidation_cff import *
 # miniAOD
 from Validation.RecoParticleFlow.miniAODValidation_cff import *
-#from Validation.RecoEgamma.egammaMiniAODValidation_cff import *
-from Validation.RecoEgamma.electronMiniAODValidationSequence_cff import *
+from Validation.RecoEgamma.egammaMiniAODValidation_cff import *
+#from Validation.RecoEgamma.electronMiniAODValidationSequence_cff import *
 
 prevalidation = cms.Sequence( globalPrevalidation * hltassociation * metPreValidSeq * jetPreValidSeq )
 prevalidationLiteTracking = cms.Sequence( prevalidation )
 prevalidationLiteTracking.replace(globalPrevalidation,globalPrevalidationLiteTracking)
-#prevalidationMiniAOD = cms.Sequence( genParticles1 * miniAODValidationSequence * JetValidationMiniAOD * type0PFMEtCorrectionPFCandToVertexAssociationForValidationMiniAOD * METValidationMiniAOD * egammaValidationMiniAOD )
-prevalidationMiniAOD = cms.Sequence( genParticles1 * miniAODValidationSequence * electronValidationSequenceMiniAOD * JetValidationMiniAOD * type0PFMEtCorrectionPFCandToVertexAssociationForValidationMiniAOD * METValidationMiniAOD )
+prevalidationMiniAOD = cms.Sequence( genParticles1 * miniAODValidationSequence * JetValidationMiniAOD * type0PFMEtCorrectionPFCandToVertexAssociationForValidationMiniAOD * METValidationMiniAOD * egammaValidationMiniAOD )
+#prevalidationMiniAOD = cms.Sequence( genParticles1 * miniAODValidationSequence * electronValidationSequenceMiniAOD * JetValidationMiniAOD * type0PFMEtCorrectionPFCandToVertexAssociationForValidationMiniAOD * METValidationMiniAOD )
 
 
 validation = cms.Sequence(cms.SequencePlaceholder("mix")
