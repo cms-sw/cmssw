d39c0cbee3a7e4560bd7298998a85ed5659f4f0b
Quan Wang <BetterWang@gmail.com>
2015-11-16 16:33:05 +0100

    [!] fix path names

--- DQMOffline/Trigger/python/eventshapeDQM_cfi.py
+++ DQMOffline/Trigger/python/eventshapeDQM_cfi.py
@@ -3,7 +3,7 @@ import FWCore.ParameterSet.Config as cms
 eventshapeDQM_Q2_top05_cent1030 = cms.EDAnalyzer('EventShapeDQM',
         triggerResults = cms.InputTag('TriggerResults','','HLT'),
         EPlabel = cms.InputTag("hltEvtPlaneProducer"),
-        triggerPath = cms.string('HLT_Q2Top005_L1Centrality1030_v'),
+        triggerPath = cms.string('HLT_HIQ2Top005_Centrality1030_v'),
         order = cms.int32(2),
         EPidx = cms.int32(8), #HF2
         EPlvl = cms.int32(0)
@@ -12,7 +12,7 @@ eventshapeDQM_Q2_top05_cent1030 = cms.EDAnalyzer('EventShapeDQM',
 eventshapeDQM_Q2_bottom05_cent1030 = cms.EDAnalyzer('EventShapeDQM',
         triggerResults = cms.InputTag('TriggerResults','','HLT'),
         EPlabel = cms.InputTag("hltEvtPlaneProducer"),
-        triggerPath = cms.string('HLT_Q2Bottom005_L1Centrality1030_v'),
+        triggerPath = cms.string('HLT_HIQ2Bottom005_Centrality1030_v'),
         order = cms.int32(2),
         EPidx = cms.int32(8), #HF2
         EPlvl = cms.int32(0)
@@ -21,7 +21,7 @@ eventshapeDQM_Q2_bottom05_cent1030 = cms.EDAnalyzer('EventShapeDQM',
 eventshapeDQM_Q2_top05_cent3050 = cms.EDAnalyzer('EventShapeDQM',
         triggerResults = cms.InputTag('TriggerResults','','HLT'),
         EPlabel = cms.InputTag("hltEvtPlaneProducer"),
-        triggerPath = cms.string('HLT_Q2Top005_L1Centrality3050_v'),
+        triggerPath = cms.string('HLT_HIQ2Top005_Centrality3050_v'),
         order = cms.int32(2),
         EPidx = cms.int32(8), #HF2
         EPlvl = cms.int32(0)
@@ -30,7 +30,7 @@ eventshapeDQM_Q2_top05_cent3050 = cms.EDAnalyzer('EventShapeDQM',
 eventshapeDQM_Q2_bottom05_cent3050 = cms.EDAnalyzer('EventShapeDQM',
         triggerResults = cms.InputTag('TriggerResults','','HLT'),
         EPlabel = cms.InputTag("hltEvtPlaneProducer"),
-        triggerPath = cms.string('HLT_Q2Bottom005_L1Centrality3050_v'),
+        triggerPath = cms.string('HLT_HIQ2Bottom005_Centrality3050_v'),
         order = cms.int32(2),
         EPidx = cms.int32(8), #HF2
         EPlvl = cms.int32(0)
@@ -39,7 +39,7 @@ eventshapeDQM_Q2_bottom05_cent3050 = cms.EDAnalyzer('EventShapeDQM',
 eventshapeDQM_Q2_top05_cent5070 = cms.EDAnalyzer('EventShapeDQM',
         triggerResults = cms.InputTag('TriggerResults','','HLT'),
         EPlabel = cms.InputTag("hltEvtPlaneProducer"),
-        triggerPath = cms.string('HLT_Q2Top005_L1Centrality5070_v'),
+        triggerPath = cms.string('HLT_HIQ2Top005_Centrality5070_v'),
         order = cms.int32(2),
         EPidx = cms.int32(8), #HF2
         EPlvl = cms.int32(0)
@@ -48,61 +48,7 @@ eventshapeDQM_Q2_top05_cent5070 = cms.EDAnalyzer('EventShapeDQM',
 eventshapeDQM_Q2_bottom05_cent5070 = cms.EDAnalyzer('EventShapeDQM',
         triggerResults = cms.InputTag('TriggerResults','','HLT'),
         EPlabel = cms.InputTag("hltEvtPlaneProducer"),
-        triggerPath = cms.string('HLT_Q2Bottom005_L1Centrality5070_v'),
-        order = cms.int32(2),
-        EPidx = cms.int32(8), #HF2
-        EPlvl = cms.int32(0)
-)
-
-eventshapeDQM_Q2_top10_cent1030 = cms.EDAnalyzer('EventShapeDQM',
-        triggerResults = cms.InputTag('TriggerResults','','HLT'),
-        EPlabel = cms.InputTag("hltEvtPlaneProducer"),
-        triggerPath = cms.string('HLT_Q2Top010_L1Centrality1030_v'),
-        order = cms.int32(2),
-        EPidx = cms.int32(8), #HF2
-        EPlvl = cms.int32(0)
-)
-
-eventshapeDQM_Q2_bottom10_cent1030 = cms.EDAnalyzer('EventShapeDQM',
-        triggerResults = cms.InputTag('TriggerResults','','HLT'),
-        EPlabel = cms.InputTag("hltEvtPlaneProducer"),
-        triggerPath = cms.string('HLT_Q2Bottom010_L1Centrality1030_v'),
-        order = cms.int32(2),
-        EPidx = cms.int32(8), #HF2
-        EPlvl = cms.int32(0)
-)
-
-eventshapeDQM_Q2_top10_cent3050 = cms.EDAnalyzer('EventShapeDQM',
-        triggerResults = cms.InputTag('TriggerResults','','HLT'),
-        EPlabel = cms.InputTag("hltEvtPlaneProducer"),
-        triggerPath = cms.string('HLT_Q2Top010_L1Centrality3050_v'),
-        order = cms.int32(2),
-        EPidx = cms.int32(8), #HF2
-        EPlvl = cms.int32(0)
-)
-
-eventshapeDQM_Q2_bottom10_cent3050 = cms.EDAnalyzer('EventShapeDQM',
-        triggerResults = cms.InputTag('TriggerResults','','HLT'),
-        EPlabel = cms.InputTag("hltEvtPlaneProducer"),
-        triggerPath = cms.string('HLT_Q2Bottom010_L1Centrality3050_v'),
-        order = cms.int32(2),
-        EPidx = cms.int32(8), #HF2
-        EPlvl = cms.int32(0)
-)
-
-eventshapeDQM_Q2_top10_cent5070 = cms.EDAnalyzer('EventShapeDQM',
-        triggerResults = cms.InputTag('TriggerResults','','HLT'),
-        EPlabel = cms.InputTag("hltEvtPlaneProducer"),
-        triggerPath = cms.string('HLT_Q2Top010_L1Centrality5070_v'),
-        order = cms.int32(2),
-        EPidx = cms.int32(8), #HF2
-        EPlvl = cms.int32(0)
-)
-
-eventshapeDQM_Q2_bottom10_cent5070 = cms.EDAnalyzer('EventShapeDQM',
-        triggerResults = cms.InputTag('TriggerResults','','HLT'),
-        EPlabel = cms.InputTag("hltEvtPlaneProducer"),
-        triggerPath = cms.string('HLT_Q2Bottom010_L1Centrality5070_v'),
+        triggerPath = cms.string('HLT_HIQ2Bottom005_Centrality5070_v'),
         order = cms.int32(2),
         EPidx = cms.int32(8), #HF2
         EPlvl = cms.int32(0)
@@ -110,4 +56,4 @@ eventshapeDQM_Q2_bottom10_cent5070 = cms.EDAnalyzer('EventShapeDQM',
 
 
 
-eventshapeDQMSequence = cms.Sequence(eventshapeDQM_Q2_top05_cent1030 * eventshapeDQM_Q2_bottom05_cent1030 * eventshapeDQM_Q2_top05_cent3050 * eventshapeDQM_Q2_bottom05_cent3050 * eventshapeDQM_Q2_top05_cent5070 * eventshapeDQM_Q2_bottom05_cent5070 * eventshapeDQM_Q2_top10_cent1030 * eventshapeDQM_Q2_bottom10_cent1030 * eventshapeDQM_Q2_top10_cent3050 * eventshapeDQM_Q2_bottom10_cent3050 * eventshapeDQM_Q2_top10_cent5070 * eventshapeDQM_Q2_bottom10_cent5070)
+eventshapeDQMSequence = cms.Sequence(eventshapeDQM_Q2_top05_cent1030 * eventshapeDQM_Q2_bottom05_cent1030 * eventshapeDQM_Q2_top05_cent3050 * eventshapeDQM_Q2_bottom05_cent3050 * eventshapeDQM_Q2_top05_cent5070 * eventshapeDQM_Q2_bottom05_cent5070 )
