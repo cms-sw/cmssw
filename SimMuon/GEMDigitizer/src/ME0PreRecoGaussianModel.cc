f9e6cefeb79cb0cfd883fa8a38ae7c43f43e2811
Piet <piet.verwilligen@cern.ch>
2015-11-16 13:29:24 +0100

    all calculations with double precision + remove pow overhead

--- SimMuon/GEMDigitizer/src/ME0PreRecoGaussianModel.cc
+++ SimMuon/GEMDigitizer/src/ME0PreRecoGaussianModel.cc
@@ -24,6 +24,7 @@ ME0PreRecoGaussianModel::ME0PreRecoGaussianModel(const edm::ParameterSet& config
     corr(config.getParameter<bool>("useCorrelation")), 
     etaproj(config.getParameter<bool>("useEtaProjectiveGEO")), 
     digitizeOnlyMuons_(config.getParameter<bool>("digitizeOnlyMuons")), 
+    gaussianSmearing_(config.getParameter<bool>("gaussianSmearing")),
     averageEfficiency_(config.getParameter<double>("averageEfficiency")), 
     // simulateIntrinsicNoise_(config.getParameter<bool>("simulateIntrinsicNoise")),
     // averageNoiseRate_(config.getParameter<double>("averageNoiseRate")), 
@@ -33,15 +34,10 @@ ME0PreRecoGaussianModel::ME0PreRecoGaussianModel(const edm::ParameterSet& config
     maxBunch_(config.getParameter<int>("maxBunch"))
 {
   // polynomial parametrisation of neutral (n+g) and electron background
-  // neuBkg.push_back(899644.0);     neuBkg.push_back(-30841.0);     neuBkg.push_back(441.28); 
-  // neuBkg.push_back(-3.3405);      neuBkg.push_back(0.0140588);    neuBkg.push_back(-3.11473e-05); neuBkg.push_back(2.83736e-08);
-  // eleBkg.push_back(4.68590e+05);  eleBkg.push_back(-1.63834e+04); eleBkg.push_back(2.35700e+02);
-  // eleBkg.push_back(-1.77706e+00); eleBkg.push_back(7.39960e-03);  eleBkg.push_back(-1.61448e-05); eleBkg.push_back(1.44368e-08);
-  neuBkg.push_back(5.69e+06);     neuBkg.push_back(-293334);     neuBkg.push_back(6279.6);
-  neuBkg.push_back(-71.2928);     neuBkg.push_back(0.452244);    neuBkg.push_back(-0.0015191);  neuBkg.push_back(2.1106e-06);
-  eleBkg.push_back(3.77712e+06);  eleBkg.push_back(-199280);     eleBkg.push_back(4340.69);
-  eleBkg.push_back(-49.922);      eleBkg.push_back(0.319699);    eleBkg.push_back(-0.00108113); eleBkg.push_back(1.50889e-06);
-
+  neuBkg.push_back(899644.0);     neuBkg.push_back(-30841.0);     neuBkg.push_back(441.28); 
+  neuBkg.push_back(-3.3405);      neuBkg.push_back(0.0140588);    neuBkg.push_back(-3.11473e-05); neuBkg.push_back(2.83736e-08);
+  eleBkg.push_back(4.68590e+05);  eleBkg.push_back(-1.63834e+04); eleBkg.push_back(2.35700e+02);
+  eleBkg.push_back(-1.77706e+00); eleBkg.push_back(7.39960e-03);  eleBkg.push_back(-1.61448e-05); eleBkg.push_back(1.44368e-08);
 }
 ME0PreRecoGaussianModel::~ME0PreRecoGaussianModel()
 {
@@ -67,12 +63,19 @@ void ME0PreRecoGaussianModel::simulateSignal(const ME0EtaPartition* roll, const 
     if (flat1_->fire(1) > averageEfficiency_) continue;
     // create digi
     auto entry = hit.entryPoint();
-    float x = gauss_->fire(entry.x(), sigma_u);
-    float y = gauss_->fire(entry.y(), sigma_v);
-    float ex = sigma_u;
-    float ey = sigma_v;
-    float corr = 0.;
-    float tof = gauss_->fire(hit.timeOfFlight(), sigma_t);
+    double x=0.0, y=0.0;
+    if(gaussianSmearing_) { // Gaussian Smearing
+      x=gauss_->fire(entry.x(), sigma_u);
+      y=gauss_->fire(entry.x(), sigma_u);
+    }
+    else { // Uniform Smearing ... use the sigmas as boundaries
+      x=entry.x()+(flat1_->fire(0., 1.)-0.5)*sigma_u;
+      y=entry.y()+(flat1_->fire(0., 1.)-0.5)*sigma_v;
+    }
+    double ex = sigma_u;
+    double ey = sigma_v;
+    double corr = 0.;
+    double tof = gauss_->fire(hit.timeOfFlight(), sigma_t);
     int pdgid = hit.particleType();
     ME0DigiPreReco digi(x, y, ex, ey, corr, tof, pdgid);
     digi_.insert(digi);
@@ -88,10 +91,10 @@ void ME0PreRecoGaussianModel::simulateNoise(const ME0EtaPartition* roll)
   const TrapezoidalStripTopology* top_(dynamic_cast<const TrapezoidalStripTopology*>(&(roll->topology())));
 
   auto& parameters(roll->specs()->parameters());
-  float bottomLength(parameters[0]); bottomLength = 2*bottomLength;
-  float topLength(parameters[1]);    topLength    = 2*topLength;
-  float height(parameters[2]);       height       = 2*height;
-  float myTanPhi    = (topLength - bottomLength) / (height * 2);
+  double bottomLength(parameters[0]); bottomLength = 2*bottomLength;
+  double topLength(parameters[1]);    topLength    = 2*topLength;
+  double height(parameters[2]);       height       = 2*height;
+  double myTanPhi    = (topLength - bottomLength) / (height * 2);
   double rollRadius = top_->radius();
   trArea = height * (topLength + bottomLength) / 2.0;
 
@@ -111,14 +114,15 @@ void ME0PreRecoGaussianModel::simulateNoise(const ME0EtaPartition* roll)
     // -----------------------
     if (simulateElectronBkg_) {
 
-      float myRandY = flat2_->fire(0., 1.);
-      float yy_rand = height * (myRandY - 0.5); // random Y coord in Local Coords
+      double myRandY = flat2_->fire(0., 1.);
+      double yy_rand = height * (myRandY - 0.5); // random Y coord in Local Coords
       double yy_glob = rollRadius + yy_rand;    // random Y coord in Global Coords
 
       // Extract / Calculate the Average Electron Rate 
       // for the given global Y coord from Parametrization
       double averageElectronRatePerRoll = 0.0;
-      for(int j=0; j<7; ++j) { averageElectronRatePerRoll += eleBkg[j]*pow(yy_glob,j); }
+      double yy_helper = 1.0;
+      for(int j=0; j<7; ++j) { averageElectronRatePerRoll += eleBkg[j]*yy_helper; yy_helper *= yy_glob; }
 
       // Rate [Hz/cm^2] * 25*10^-9 [s] * Area [cm] = # hits in this roll 
       const double averageElecRate(averageElectronRatePerRoll * (bxwidth*1.0e-9) * trArea); 
@@ -130,16 +134,16 @@ void ME0PreRecoGaussianModel::simulateNoise(const ME0EtaPartition* roll)
       // loop over amount of electron hits in this roll
       for (int i = 0; i < n_elechits; ++i) {
 	//calculate xx_rand at a given yy_rand
-	float myRandX = flat1_->fire(0., 1.);
-	float xx_rand = 2 * xMax * (myRandX - 0.5);
-	float ex = sigma_u;
-	float ey = sigma_v;
-	float corr = 0.;
+	double myRandX = flat1_->fire(0., 1.);
+	double xx_rand = 2 * xMax * (myRandX - 0.5);
+	double ex = sigma_u;
+	double ey = sigma_v;
+	double corr = 0.;
 	// extract random time in this BX
-	float myrandT = flat1_->fire(0., 1.);
-	float minBXtime = (bx-0.5)*bxwidth;      // float maxBXtime = (bx+0.5)*bxwidth;
-	float time = myrandT*bxwidth+minBXtime;
-	float myrandP = flat1_->fire(0., 1.);
+	double myrandT = flat1_->fire(0., 1.);
+	double minBXtime = (bx-0.5)*bxwidth;      // double maxBXtime = (bx+0.5)*bxwidth;
+	double time = myrandT*bxwidth+minBXtime;
+	double myrandP = flat1_->fire(0., 1.);
 	int pdgid = 0;
 	if (myrandP <= 0.5) pdgid = -11; // electron
 	else 	            pdgid = 11;  // positron
@@ -152,14 +156,15 @@ void ME0PreRecoGaussianModel::simulateNoise(const ME0EtaPartition* roll)
     // ----------------------------
     if (simulateNeutralBkg_) {
 
-      float myRandY = flat2_->fire(0., 1.);
-      float yy_rand = height * (myRandY - 0.5); // random Y coord in Local Coords
+      double myRandY = flat2_->fire(0., 1.);
+      double yy_rand = height * (myRandY - 0.5); // random Y coord in Local Coords
       double yy_glob = rollRadius + yy_rand;    // random Y coord in Global Coords
 
       // Extract / Calculate the Average Electron Rate 
       // for the given global Y coord from Parametrization
       double averageNeutralRatePerRoll = 0.0;
-      for(int j=0; j<7; ++j) { averageNeutralRatePerRoll += neuBkg[j]*pow(yy_glob,j); }
+      double yy_helper = 1.0;
+      for(int j=0; j<7; ++j) { averageNeutralRatePerRoll += neuBkg[j]*yy_helper; yy_helper *= yy_glob; }
 
       // Rate [Hz/cm^2] * 25*10^-9 [s] * Area [cm] = # hits in this roll
       const double averageNeutrRate(averageNeutralRatePerRoll * (bxwidth*1.0e-9) * trArea);
@@ -171,17 +176,17 @@ void ME0PreRecoGaussianModel::simulateNoise(const ME0EtaPartition* roll)
       // loop over amount of neutral hits in this roll
       for (int i = 0; i < n_hits; ++i) {
 	//calculate xx_rand at a given yy_rand
-	float myRandX = flat1_->fire(0., 1.);
-	float xx_rand = 2 * xMax * (myRandX - 0.5);
-	float ex = sigma_u;
-	float ey = sigma_v;
-	float corr = 0.;
+	double myRandX = flat1_->fire(0., 1.);
+	double xx_rand = 2 * xMax * (myRandX - 0.5);
+	double ex = sigma_u;
+	double ey = sigma_v;
+	double corr = 0.;
 	// extract random time in this BX
-        float myrandT = flat1_->fire(0., 1.);
-        float minBXtime = (bx-0.5)*bxwidth;
-	float time = myrandT*bxwidth+minBXtime;
+        double myrandT = flat1_->fire(0., 1.);
+        double minBXtime = (bx-0.5)*bxwidth;
+	double time = myrandT*bxwidth+minBXtime;
 	int pdgid = 0;
-        float myrandP = flat1_->fire(0., 1.);
+        double myrandP = flat1_->fire(0., 1.);
         if (myrandP <= 0.08) pdgid = 2112; // neutrons: GEM sensitivity for neutrons: 0.08%
         else                 pdgid = 22;   // photons:  GEM sensitivity for photons:  1.04% ==> neutron fraction = (0.08 / 1.04) = 0.077 = 0.08
         ME0DigiPreReco digi(xx_rand, yy_rand, ex, ey, corr, time, pdgid);
