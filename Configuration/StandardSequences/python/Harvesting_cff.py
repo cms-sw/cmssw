3259089d9d85eed1b25bf3f7bda96dacc4fd73a1
Lukas <lukas.vanelderen@cern.ch>
2015-11-17 16:53:13 +0100

    global validation and postvalidation: use fastsim era for fatsim customs

--- Configuration/StandardSequences/python/Harvesting_cff.py
+++ Configuration/StandardSequences/python/Harvesting_cff.py
@@ -6,8 +6,6 @@ from DQMOffline.Configuration.DQMOffline_Certification_cff import *
 from Validation.Configuration.postValidation_cff import *
 from HLTriggerOffline.Common.HLTValidationHarvest_cff import *
 
-from FastSimulation.Configuration.Harvesting_cff import *
-
 from Validation.RecoHI.HarvestingHI_cff import *
 from Validation.RecoJets.JetPostProcessor_cff import *
 from Validation.RecoMET.METPostProcessor_cff import *
@@ -25,7 +23,9 @@ validationpreprodHarvesting = cms.Path(postValidation_preprod*hltpostvalidation_
 # empty (non-hlt) postvalidation sequence here yet
 validationprodHarvesting = cms.Path(hltpostvalidation_prod*postValidation_gen)
 
-validationHarvestingFS = cms.Path(HarvestingFastSim)
+# to be removed in subsequent request
+# to avoid to many extra github signatures
+validationHarvestingFS = validationHarvesting.copy()
 
 validationHarvestingHI = cms.Path(postValidationHI)
 
