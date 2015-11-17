3cfc6ad383d360b3f0318d3989be75f9ecbabf62
Arnaud <arnaud.chiron@cern.ch>
2015-11-17 12:11:09 +0100

    new MiniAOD name

--- Validation/RecoEgamma/python/egammaPostValidationMiniAOD_cff.py
+++ Validation/RecoEgamma/python/egammaPostValidationMiniAOD_cff.py
@@ -2,4 +2,4 @@ import FWCore.ParameterSet.Config as cms
 
 from Validation.RecoEgamma.electronPostValidationSequenceMiniAOD_cff import *
  
-egammaPostValidationMiniAOD = cms.Sequence(electronPostValidationSequenceMiniAOD)
+egammaPostValidationMiniAOD = cms.Sequence( electronPostValidationSequenceMiniAOD )
