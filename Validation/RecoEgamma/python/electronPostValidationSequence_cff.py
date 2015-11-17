707ba7309b7896da86e6b5464e66f4e387b7a6bc
Arnaud <arnaud.chiron@cern.ch>
2015-11-17 11:59:48 +0100

    new MiniAOD name

--- Validation/RecoEgamma/python/electronPostValidationSequence_cff.py
+++ Validation/RecoEgamma/python/electronPostValidationSequence_cff.py
@@ -3,8 +3,6 @@ import FWCore.ParameterSet.Config as cms
 from Validation.RecoEgamma.ElectronMcSignalPostValidator_cfi import *
 from Validation.RecoEgamma.ElectronMcFakePostValidator_cfi import *
 from Validation.RecoEgamma.ElectronMcSignalPostValidatorPt1000_cfi import *
-from Validation.RecoEgamma.ElectronMcMiniAODSignalPostValidator_cfi import *
-
+ 
 electronPostValidationSequence = cms.Sequence(electronMcSignalPostValidator+electronMcFakePostValidator+electronMcSignalPostValidatorPt1000)
-electronPostValidationSequenceMiniAOD = cms.Sequence(electronMcMiniAODSignalPostValidator)
 
