0c1c634eb738652bb57389a7af8c09008ea2005d
Chiron <arnaud.chiron@cern.ch>
2015-11-18 09:13:24 +0100

    Delete egammaMiniAODValidation_cff.py

--- Validation/RecoEgamma/python/egammaMiniAODValidation_cff.py
+++
@@ -1,5 +0,0 @@
-import FWCore.ParameterSet.Config as cms
-
-from Validation.RecoEgamma.electronValidationSequence_cff import *
-
-egammaValidationMiniAOD = cms.Sequence(electronValidationSequenceMiniAOD)
