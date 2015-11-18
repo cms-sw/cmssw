701bba6c15f7a171810d5fe692806e386ebfe60d
Chris <chris.ferraioli@cern.ch>
2015-11-18 08:29:30 +0100

    updates zdc unpacker

--- EventFilter/CastorRawToDigi/src/ZdcUnpacker.cc
+++ EventFilter/CastorRawToDigi/src/ZdcUnpacker.cc
@@ -22,17 +22,12 @@ namespace ZdcUnpacker_impl {
     uint32_t zsmask=hhd.zsBunchMask()>>startSample;
     digi.setZSInfo(hhd.isUnsuppressed(),hhd.wasMarkAndPassZS(fiber,fiberchan),zsmask);
 
-    //   digi.setReadoutIds(eid);
-    //   setReadoutIds is missing in  ZdcDataFrame class  digi.setReadoutIds(eid);
     if (expectedTime>=0 && !hhd.isUnsuppressed()) {
-      // std::cout << hhd.getFibOrbMsgBCN(fiber) << " " << expectedTime << " fiber="<<fiber<< std::endl;
       digi.setFiberIdleOffset(hhd.getFibOrbMsgBCN(fiber)-expectedTime);
     }
-    // what is my sample number?
     int myFiberChan=startPoint->fiberAndChan();
     int ncurr=0,ntaken=0;
     const HcalQIESample* qie_work=startPoint;
-    //std::cout<<qie_work<<" with limit being..."<<limit<<std::endl;
     while (qie_work!=limit && qie_work->fiberAndChan()==myFiberChan) {
       if (ncurr>=startSample && ncurr<=endSample) {
         digi.setSample(ntaken,*qie_work);
@@ -66,12 +61,6 @@ ZdcUnpacker::ZdcUnpacker(int sourceIdOffset, int beg, int end) : sourceIdOffset_
   }
 }
 
-//static int slb(const HcalTriggerPrimitiveSample& theSample) { return ((theSample.raw()>>13)&0x7); }
-//static int slbChan(const HcalTriggerPrimitiveSample& theSample) { return (theSample.raw()>>11)&0x3; }
-//static int slbAndChan(const HcalTriggerPrimitiveSample& theSample) { return (theSample.raw()>>11)&0x1F; }
-
-
-
 void ZdcUnpacker::unpack(const FEDRawData& raw, const CastorElectronicsMap& emap,
                          CastorRawCollections& colls, HcalUnpackerReport& report, bool silent) {
 
@@ -83,45 +72,12 @@ void ZdcUnpacker::unpack(const FEDRawData& raw, const CastorElectronicsMap& emap
   // get the DCC header
   const HcalDCCHeader* dccHeader=(const HcalDCCHeader*)(raw.data());
   int dccid=dccHeader->getSourceId();//-sourceIdOffset_;
-  
-  std::cout<<dccid<<std::endl;
-  // check the summary status
 
   // walk through the HTR data...
   HcalHTRData htr;
   const unsigned short* daq_first, *daq_last, *tp_first, *tp_last;
   const HcalQIESample* qie_begin, *qie_end, *qie_work;
-  //  const HcalTriggerPrimitiveSample *tp_begin, *tp_end, *tp_work;
-
-
-  ///////////////////////////////////////////
-  ///////////ATTEMPT A ZDC MAP///////////////
-  ///////////////////////////////////////////
   std::map<CastorElectronicsId,uint32_t> myEMap;
-
-  //PZDC
-  /*  myEMap[HcalElectronicsId0,1,12,22,12,8,1)]=0x54000051;//PZDC EM1
-  myEMap[HcalElectronicsId(1,1,12,22,12,8,1)]=0x54000052;//PZDC EM2
-  myEMap[HcalElectronicsId(2,1,12,22,12,8,1)]=0x54000053;//PZDC EM3
-  myEMap[HcalElectronicsId(0,2,12,22,12,8,1)]=0x54000054;//PZDC EM4
-  myEMap[HcalElectronicsId(1,2,12,22,12,8,1)]=0x54000055;//PZDC EM5
-  myEMap[HcalElectronicsId(2,2,12,22,12,8,1)]=0x54000061;//PZDC HAD1
-  myEMap[HcalElectronicsId(0,3,12,22,12,8,1)]=0x54000062;//PZDC HAD2
-  myEMap[HcalElectronicsId(1,3,12,22,12,8,1)]=0x54000063;//PZDC HAD3
-  myEMap[HcalElectronicsId(2,3,12,22,12,8,1)]=0x54000064;//PZDC HAD4
-
-  //NZDC
-  myEMap[HcalElectronicsId(0,1,13,22,12,8,0)]=0x54000011;//NZDC EM1
-  myEMap[HcalElectronicsId(1,1,13,22,12,8,0)]=0x54000012;//NZDC EM2
-  myEMap[HcalElectronicsId(2,1,13,22,12,8,0)]=0x54000013;//NZDC EM3
-  myEMap[HcalElectronicsId(0,2,13,22,12,8,0)]=0x54000014;//NZDC EM4
-  myEMap[HcalElectronicsId(1,2,13,22,12,8,0)]=0x54000015;//NZDC EM5
-  myEMap[HcalElectronicsId(2,2,13,22,12,8,0)]=0x54000021;//NZDC HAD1
-  myEMap[HcalElectronicsId(0,3,13,22,12,8,0)]=0x54000022;//NZDC HAD2
-  myEMap[HcalElectronicsId(1,3,13,22,12,8,0)]=0x54000023;//NZDC HAD3
-  myEMap[HcalElectronicsId(2,3,13,22,12,8,0)]=0x54000024;//NZDC HAD4*/
-
-//CastorElectronicsId(qie_work->fiberChan(),qie_work->fiber(),spigot,22,htr_cr,htr_slot,htr_tb))->second > 0)
 
   //PZDC
   myEMap[CastorElectronicsId(0,1,0,3,18,8,1)]=0x54000051;//PZDC EM1
@@ -146,8 +102,6 @@ void ZdcUnpacker::unpack(const FEDRawData& raw, const CastorElectronicsMap& emap
   myEMap[CastorElectronicsId(2,3,1,3,18,8,0)]=0x54000024;//NZDC HAD4
 
   //slot is 17
-  
-
 
   for (int spigot=0; spigot<HcalDCCHeader::SPIGOT_COUNT; spigot++) {
     if (!dccHeader->getSpigotPresent(spigot)) continue;
@@ -203,164 +157,20 @@ void ZdcUnpacker::unpack(const FEDRawData& raw, const CastorElectronicsMap& emap
     int htr_slot=(smid>>1)&0x1F;
     int htr_cr=(smid>>6)&0x1F;
 
-    //    tp_begin=(HcalTriggerPrimitiveSample*)tp_first;
-    //tp_end=(HcalTriggerPrimitiveSample*)(tp_last+1); // one beyond last..
-
-    /// work through the samples
-    //int currFiberChan=0x3F; // invalid fiber+channel...
-    //int ncurr=0;
-    //bool valid=false;
-    //////////////////////////////////////////////
-    // bool tpgSOIbitInUse=htr.getFormatVersion()>=3; // version 3 and later
-    // bool isHOtpg=htr.getFormatVersion()>=3 && htr.getFirmwareFlavor()==0; // HO is flavor zero
-    //int npre=0;
-    /*
-      Unpack the trigger primitives
-    */
-    // lookup the right channel
-    /*            bool dotp = true;
-                  HcalElectronicsId eid(0,1,spigot,dccid);
-                  eid.setHTR(htr_cr,htr_slot,htr_tb);
-                  DetId did=emap.lookup(eid);
-                  if ( did.null() ) dotp = false;
-                  HcalZDCDetId id1(did);
-                  HcalZDCDetId id(id1.section(),(id1.zside()==0),id1.channel());
-                  if ( id1.channel() > 12 ) dotp = false;
-                  if ( dotp ) {
-                  // std::cout << " tp_first="<< tp_first << " tp_last="<< tp_last<< " tb="<<htr_tb<<" slot="<<htr_slot<<" crate="<<htr_cr<<" dccid="<< dccid<< std::endl;
-                  // regular TPs (not HO)
-                  for (tp_work=tp_begin; tp_work!=tp_end; tp_work++) {
-                  //      std::cout << "raw=0x"<<std::hex<< tp_work->raw()<<std::dec <<std::endl;
-                  if (tp_work->raw()==0xFFFF) continue; // filler word
-                  if (slbAndChan(*tp_work)!=currFiberChan) { // start new set
-                  npre=0;
-                  currFiberChan=slbAndChan(*tp_work);
-
-                  // std::cout<< " NEW SET "<<std::endl;
-                  //HcalElectronicsId eid(tp_work->slbChan(),tp_work->slb(),spigot,dccid,htr_cr,htr_slot,htr_tb);
-                  //DetId did=emap.lookupTrigger(eid);
-                  //if (did.null()) {
-                  //report.countUnmappedTPDigi(eid);
-                  //if (unknownIdsTrig_.find(eid)==unknownIdsTrig_.end()) {
-                  //if (!silent) edm::LogWarning("HCAL") << "HcalUnpacker: No trigger primitive match found for electronics id :" << eid;
-                  //unknownIdsTrig_.insert(eid);
-                  //}
-                  //valid=false;
-                  //continue;
-                  //} else if (did==HcalTrigTowerDetId::Undefined ||
-                  //(did.det()==DetId::Hcal && did.subdetId()==0)) {
-                  //// known to be unmapped
-                  //valid=false;
-                  //continue;
-                  //}
-
-                  //colls.tpCont->push_back(HcalTriggerPrimitiveDigi(id));
-                  // set the various bits
-                  //if (!tpgSOIbitInUse) colls.tpCont->back().setPresamples(nps);
-                  //colls.tpCont->back().setZSInfo(htr.isUnsuppressed(),htr.wasMarkAndPassZSTP(slb(*tp_work),slbChan(*tp_work)));
-
-                  // no hits recorded for current
-                  ncurr=0;
-                  valid=true;
-                  }
-                  // add the word (if within settings or recent firmware [recent firmware ignores startSample/endSample])
-                  if (valid && ((tpgSOIbitInUse && ncurr<10) || (ncurr>=startSample_ && ncurr<=endSample_))) {
-                  colls.tpCont->back().setSample(colls.tpCont->back().size(),*tp_work);
-                  colls.tpCont->back().setSize(colls.tpCont->back().size()+1);
-                  }
-                  // set presamples,if SOI
-                  if (valid && tpgSOIbitInUse && isTPGSOI(*tp_work)) {
-                  colls.tpCont->back().setPresamples(ncurr);
-                  }
-                  ncurr++;
-                  npre++;
-                  }
-                  }*/
-
     //////////////////////////////////////////////
     qie_begin=(HcalQIESample*)daq_first;
     qie_end=(HcalQIESample*)(daq_last+1); // one beyond last..
-    // std::cout<<qie_end<<std::endl;
-    /// work through the samples
-    //currFiberChan=0x3F; // invalid fiber+channel...
-    //ncurr=0;
-    //valid=false;
-    //int myiter=0;
-/*	int spigot0 = 0;
-	int spigot1 = 0;
-	int spigotn1 = 0;
-	int spigothold=0;
-	
-	int htr0 = 0;
-	int htr1 = 0;
-	int htrhold=0;
-	
-	int fiberchannel[18];
-	for(int x = 0; x < 18; x++){
-	  fiberchannel[x]=0;
-	}
-	int fiberchannelextra = 0;
-	
-	int fiber[18];
-	for(int x = 0; x < 18; x++){
-	  fiber[x]=0;
-	}
-	int fiberextra = 0;
-*/	
-//    for (qie_work=qie_begin; qie_work!=qie_end;qie_work++) {
+
     for (qie_work=qie_begin; qie_work!=qie_end;) {    
       if (qie_work->raw()==0xFFFF) {
         qie_work++;
         continue; // filler word
       }
-      // always at the beginning ...
-//      currFiberChan=qie_work->fiberAndChan();
-
       // lookup the right channel
-      //HcalElectronicsId eid(qie_work->fiberChan(),qie_work->fiber(),spigot,22);
-//      CastorElectronicsId eid(qie_work->fiberChan(),qie_work->fiber(),spigot,3);
    CastorElectronicsId eid(qie_work->fiberChan(),qie_work->fiber(),spigot,dccid);
       eid.setHTR(htr_cr,htr_slot,htr_tb);
-  
- /* if(spigot0%50000==0){
-    std::cout<<"spigot0 is "<<spigot0<<" and spigot1 is "<<spigot1<<" and spigotn1 is "<<spigotn1<<" and random is "<<spigothold<<std::endl;
-    std::cout<<"htr0 is "<<htr0<<" and htr1 is "<<htr1<<" and random is "<<htrhold<<std::endl;    
-    for(int x = 0; x < 18; x++){
-      std::cout<<"fiberchannel"<<x<<" is "<<fiberchannel[x]<<std::endl;
-    }
-    std::cout<<"fiberchannelextra is "<<fiberchannelextra<<std::endl;
-    for(int x = 0; x < 18; x++){
-      std::cout<<"fiber"<<x<<" is "<<fiber[x]<<std::endl;
-    }
-    std::cout<<"fiberextra is "<<fiberextra<<std::endl;
-    } 
 
-	if(spigot==0) spigot0++;
-	else if(spigot==1) spigot1++;
-	else if(spigot==-1) spigotn1++;
-	else spigothold++;
-
-	if(htr_tb==0) htr0++;
-	else if(htr_tb==1) htr1++;	
-	else htrhold++;
-	
-	for(int x = 0; x < 18; x++){
-	  if(qie_work->fiber()==x) fiber[x]++;
-	  else fiberextra++;
-	}
-	
-	for(int x = 0; x < 18; x++){
-	  if(qie_work->fiberChan()==x) fiberchannel[x]++;
-	  else fiberchannelextra++;
-	}
-	*/
-      //  std::cout<<qie_work->fiberChan()<<std::endl;
       eid.setHTR(htr_cr,htr_slot,htr_tb);
-      //CastorElectronics
-      //      std::cout<<eid<<"with fiber channel .. "<<qie_work->fiberChan()<<" and fiber ... "<<qie_work->fiber()<<std::endl;
-
-//      std::cout<<"The fiberChan is ... "<<qie_work->fiberChan()<<" the fiber is ... "<<qie_work->fiber()<<" the spigot is ... "<<spigot<<" the htr_tb is... "<<htr_tb<<std::endl;
-
 
       if (qie_work->fiberChan()==0 && qie_work->fiber()==1 && spigot==0 && htr_tb==1)
 	  {
