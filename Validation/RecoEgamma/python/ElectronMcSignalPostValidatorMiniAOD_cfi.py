adb45c736c75df92469918c773bd1081c5a945ad
Chiron <arnaud.chiron@cern.ch>
2015-11-18 09:18:33 +0100

    Update and rename ElectronMcMiniAODSignalPostValidator_cfi.py to ElectronMcSignalPostValidatorMiniAOD_cfi.py

--- Validation/RecoEgamma/python/ElectronMcSignalPostValidatorMiniAOD_cfi.py
+++ Validation/RecoEgamma/python/ElectronMcSignalPostValidatorMiniAOD_cfi.py
@@ -5,14 +5,14 @@ electronMcSignalHistosCfg = cms.PSet(
   EfficiencyFlag = cms.bool(True), StatOverflowFlag = cms.bool(False)
 )
 
-electronMcMiniAODSignalPostValidator = cms.EDAnalyzer("ElectronMcMiniAODSignalPostValidator",
+electronMcSignalPostValidatorMiniAOD = cms.EDAnalyzer("ElectronMcSignalPostValidatorMiniAOD",
 
   Verbosity = cms.untracked.int32(0),
   FinalStep = cms.string("AtJobEnd"),
   InputFile = cms.string(""),
   OutputFile = cms.string(""),
-  InputFolderName = cms.string("EgammaV/ElectronMcMiniAODSignalValidator"),
-  OutputFolderName = cms.string("EgammaV/ElectronMcMiniAODSignalValidator"),
+  InputFolderName = cms.string("EgammaV/ElectronMcSignalValidatorMiniAOD"),
+  OutputFolderName = cms.string("EgammaV/ElectronMcSignalValidatorMiniAOD"),
     
   histosCfg = cms.PSet(electronMcSignalHistosCfg)
 )
