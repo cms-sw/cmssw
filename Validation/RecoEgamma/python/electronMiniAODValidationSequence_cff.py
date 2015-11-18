7d1da1c0b933e98be2351931a0009ab80df6d104
Chiron <arnaud.chiron@cern.ch>
2015-11-18 09:11:27 +0100

    Delete electronMiniAODValidationSequence_cff.py

--- Validation/RecoEgamma/python/electronMiniAODValidationSequence_cff.py
+++
@@ -1,6 +0,0 @@
-import FWCore.ParameterSet.Config as cms
-
-from Validation.RecoEgamma.ElectronMcMiniAODSignalValidator_cfi import * 
- 
-electronValidationSequenceMiniAOD = cms.Sequence(electronMcMiniAODSignalValidator)
-
