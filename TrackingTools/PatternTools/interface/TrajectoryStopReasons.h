30d3974e126aa3f33df58b45a843d199293f7944
Marco Rovere <marco.rovere@cern.ch>
2015-11-18 15:26:25 +0100

    Explicit the values of the StopReason enum class

--- TrackingTools/PatternTools/interface/TrajectoryStopReasons.h
+++ TrackingTools/PatternTools/interface/TrajectoryStopReasons.h
@@ -2,14 +2,14 @@
 #define TRAJECTORYSTOPREASONS_H
 
 enum class StopReason {
-  MAX_HITS,
-  MAX_LOST_HITS,
-  MAX_CONSECUTIVE_LOST_HITS,
-  LOST_HIT_FRACTION,
-  MIN_PT,
-  CHARGE_SIGNIFICANCE,
-  LOOPER,
-  UNKNOWN
+  MAX_HITS = 0,
+  MAX_LOST_HITS = 1,
+  MAX_CONSECUTIVE_LOST_HITS = 2,
+  LOST_HIT_FRACTION = 3,
+  MIN_PT = 4,
+  CHARGE_SIGNIFICANCE = 5,
+  LOOPER = 6,
+  UNKNOWN=255 // this is the max allowed since it will be streames as type uint8_t
 };
 
 #endif
