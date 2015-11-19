8446f5c7142ed95b125e3946acabb608b45cb5a3
R. Alex Barbieri <luck@mit.edu>
2015-11-19 16:14:42 -0500

    guard customise with hasattr
    (cherry picked from commit 9badbab65569ff2b9bd3d766408d991d8edec362)

--- RecoHI/Configuration/python/customise_ECALsequence.py
+++ RecoHI/Configuration/python/customise_ECALsequence.py
@@ -1,14 +1,14 @@
 import FWCore.ParameterSet.Config as cms
 
 def changeHeavyIonsToUseECALGlobalFit(process) :
-    process.load('RecoLocalCalo.EcalRecProducers.ecalGlobalUncalibRecHit_cfi')
-    process.ecalUncalibRecHitSequenceHI = cms.Sequence(process.ecalGlobalUncalibRecHit*
-                                                       process.ecalDetIdToBeRecovered)
-    process.ecalLocalRecoSequenceHI     = cms.Sequence(process.ecalUncalibRecHitSequenceHI*
-                                                       process.ecalRecHitSequence)
-    process.ecalRecHit.EEuncalibRecHitCollection = cms.InputTag("ecalGlobalUncalibRecHit","EcalUncalibRecHitsEE")
-    process.ecalRecHit.EBuncalibRecHitCollection = cms.InputTag("ecalGlobalUncalibRecHit","EcalUncalibRecHitsEB")
-
-    process.caloReco.replace(process.ecalUncalibRecHitSequence, process.ecalUncalibRecHitSequenceHI)
+    if hasattr (process, "caloReco") :
+        process.load('RecoLocalCalo.EcalRecProducers.ecalGlobalUncalibRecHit_cfi')
+        process.ecalUncalibRecHitSequenceHI = cms.Sequence(process.ecalGlobalUncalibRecHit*
+                                                           process.ecalDetIdToBeRecovered)
+        process.ecalLocalRecoSequenceHI     = cms.Sequence(process.ecalUncalibRecHitSequenceHI*
+                                                           process.ecalRecHitSequence)
+        process.ecalRecHit.EEuncalibRecHitCollection = cms.InputTag("ecalGlobalUncalibRecHit","EcalUncalibRecHitsEE")
+        process.ecalRecHit.EBuncalibRecHitCollection = cms.InputTag("ecalGlobalUncalibRecHit","EcalUncalibRecHitsEB")
+        process.caloReco.replace(process.ecalUncalibRecHitSequence, process.ecalUncalibRecHitSequenceHI)
 
     return process
