ae8fcae5c8b80ea874693b2032db39465a07496c
schoefbeck <robert.schoefbeck@cern.ch>
2015-11-19 13:46:25 +0100

    consumes migration for PFSimParticleProducer

--- RecoParticleFlow/PFSimProducer/plugins/PFSimParticleProducer.h
+++ RecoParticleFlow/PFSimProducer/plugins/PFSimParticleProducer.h
@@ -12,12 +12,18 @@
 
 #include "FWCore/Framework/interface/Event.h"
 #include "FWCore/Framework/interface/MakerMacros.h"
+#include "FWCore/Utilities/interface/EDGetToken.h"
 
+#include "SimDataFormats/CaloHit/interface/PCaloHitContainer.h"
 #include "TrackingTools/TrajectoryState/interface/TrajectoryStateOnSurface.h"
 #include "TrackingTools/GeomPropagators/interface/Propagator.h"
 #include "RecoParticleFlow/PFTracking/interface/PFGeometry.h"
-
+#include "DataFormats/ParticleFlowReco/interface/PFSimParticle.h"
+#include "DataFormats/ParticleFlowReco/interface/PFSimParticleFwd.h"
+#include "SimDataFormats/Track/interface/SimTrack.h"
 #include "DataFormats/ParticleFlowReco/interface/PFRecTrackFwd.h"
+#include "DataFormats/EcalRecHit/interface/EcalRecHitCollections.h"
+#include "FastSimulation/Event/interface/FSimVertex.h"
 
 #include "RecoParticleFlow/PFProducer/interface/PFBlockAlgo.h"
 
@@ -54,17 +60,23 @@ class PFSimParticleProducer : public edm::EDProducer {
     
 
   /// module label for retrieving input simtrack and simvertex
-  edm::InputTag  inputTagSim_;  
+  edm::InputTag    inputTagSim_;
+  edm::EDGetTokenT<std::vector<SimTrack> >  tokenSim_;  
+  edm::EDGetTokenT<std::vector<SimVertex> >  tokenSimVertices_;  
 
   //MC Truth Matching 
   //modif-beg
   bool mctruthMatchingInfo_;
   edm::InputTag    inputTagFamosSimHits_;
+  edm::EDGetTokenT<edm::PCaloHitContainer>    tokenFamosSimHits_;
   //modif-end
 
-  edm::InputTag    inputTagRecTracks_;
+  edm::InputTag   inputTagRecTracks_;
+  edm::EDGetTokenT<reco::PFRecTrackCollection>   tokenRecTracks_;
   edm::InputTag    inputTagEcalRecHitsEB_;
+  edm::EDGetTokenT<EcalRecHitCollection>    tokenEcalRecHitsEB_;
   edm::InputTag    inputTagEcalRecHitsEE_;
+  edm::EDGetTokenT<EcalRecHitCollection>    tokenEcalRecHitsEE_;
 
   // parameters for retrieving true particles information --
 
