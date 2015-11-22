38ceab25b85eb658592f4532a16fc21eba5ef726
Doga <dgulhan@cern.ch>
2015-11-22 21:36:16 +0100

    adding more filters for centrality and fixed hirereco

--- HeavyIonsAnalysis/JetAnalysis/python/jets/HiReRecoJets_HI_cff.py
+++ HeavyIonsAnalysis/JetAnalysis/python/jets/HiReRecoJets_HI_cff.py
@@ -36,27 +36,27 @@ akPu6PFJets.jetPtMin = 1
 akPu6CaloJets.jetPtMin = 1
 akPu7PFJets.jetPtMin = 1
 akPu7CaloJets.jetPtMin = 1
-ak1PFJets.jetPtMin = 1
-ak1PFJets.src = cms.InputTag("particleFlowTmp")
-ak1CaloJets.jetPtMin = 1
-ak2PFJets.jetPtMin = 1
-ak2PFJets.src = cms.InputTag("particleFlowTmp")
-ak2CaloJets.jetPtMin = 1
-ak3PFJets.jetPtMin = 1
-ak3PFJets.src = cms.InputTag("particleFlowTmp")
-ak3CaloJets.jetPtMin = 1
-ak4PFJets.jetPtMin = 1
-ak4PFJets.src = cms.InputTag("particleFlowTmp")
-ak4CaloJets.jetPtMin = 1
-ak5PFJets.jetPtMin = 1
-ak5PFJets.src = cms.InputTag("particleFlowTmp")
-ak5CaloJets.jetPtMin = 1
-ak6PFJets.jetPtMin = 1
-ak6PFJets.src = cms.InputTag("particleFlowTmp")
-ak6CaloJets.jetPtMin = 1
-ak7PFJets.jetPtMin = 1
-ak7PFJets.src = cms.InputTag("particleFlowTmp")
-ak7CaloJets.jetPtMin = 1
+# ak1PFJets.jetPtMin = 1
+# ak1PFJets.src = cms.InputTag("particleFlowTmp")
+# ak1CaloJets.jetPtMin = 1
+# ak2PFJets.jetPtMin = 1
+# ak2PFJets.src = cms.InputTag("particleFlowTmp")
+# ak2CaloJets.jetPtMin = 1
+# ak3PFJets.jetPtMin = 1
+# ak3PFJets.src = cms.InputTag("particleFlowTmp")
+# ak3CaloJets.jetPtMin = 1
+# ak4PFJets.jetPtMin = 1
+# ak4PFJets.src = cms.InputTag("particleFlowTmp")
+# ak4CaloJets.jetPtMin = 1
+# ak5PFJets.jetPtMin = 1
+# ak5PFJets.src = cms.InputTag("particleFlowTmp")
+# ak5CaloJets.jetPtMin = 1
+# ak6PFJets.jetPtMin = 1
+# ak6PFJets.src = cms.InputTag("particleFlowTmp")
+# ak6CaloJets.jetPtMin = 1
+# ak7PFJets.jetPtMin = 1
+# ak7PFJets.src = cms.InputTag("particleFlowTmp")
+# ak7CaloJets.jetPtMin = 1
 akVs1PFJets.jetPtMin = 1
 akVs1PFJets.src = cms.InputTag("particleFlowTmp")
 akVs1CaloJets.jetPtMin = 1
@@ -92,27 +92,27 @@ akPu6PFJets.jetPtMin = 1
 akPu6CaloJets.jetPtMin = 1
 akPu7PFJets.jetPtMin = 1
 akPu7CaloJets.jetPtMin = 1
-ak1PFJets.jetPtMin = 1
-ak1PFJets.src = cms.InputTag("particleFlowTmp")
-ak1CaloJets.jetPtMin = 1
-ak2PFJets.jetPtMin = 1
-ak2PFJets.src = cms.InputTag("particleFlowTmp")
-ak2CaloJets.jetPtMin = 1
-ak3PFJets.jetPtMin = 1
-ak3PFJets.src = cms.InputTag("particleFlowTmp")
-ak3CaloJets.jetPtMin = 1
-ak4PFJets.jetPtMin = 1
-ak4PFJets.src = cms.InputTag("particleFlowTmp")
-ak4CaloJets.jetPtMin = 1
-ak5PFJets.jetPtMin = 1
-ak5PFJets.src = cms.InputTag("particleFlowTmp")
-ak5CaloJets.jetPtMin = 1
-ak6PFJets.jetPtMin = 1
-ak6PFJets.src = cms.InputTag("particleFlowTmp")
-ak6CaloJets.jetPtMin = 1
-ak7PFJets.jetPtMin = 1
-ak7PFJets.src = cms.InputTag("particleFlowTmp")
-ak7CaloJets.jetPtMin = 1
+# ak1PFJets.jetPtMin = 1
+# ak1PFJets.src = cms.InputTag("particleFlowTmp")
+# ak1CaloJets.jetPtMin = 1
+# ak2PFJets.jetPtMin = 1
+# ak2PFJets.src = cms.InputTag("particleFlowTmp")
+# ak2CaloJets.jetPtMin = 1
+# ak3PFJets.jetPtMin = 1
+# ak3PFJets.src = cms.InputTag("particleFlowTmp")
+# ak3CaloJets.jetPtMin = 1
+# ak4PFJets.jetPtMin = 1
+# ak4PFJets.src = cms.InputTag("particleFlowTmp")
+# ak4CaloJets.jetPtMin = 1
+# ak5PFJets.jetPtMin = 1
+# ak5PFJets.src = cms.InputTag("particleFlowTmp")
+# ak5CaloJets.jetPtMin = 1
+# ak6PFJets.jetPtMin = 1
+# ak6PFJets.src = cms.InputTag("particleFlowTmp")
+# ak6CaloJets.jetPtMin = 1
+# ak7PFJets.jetPtMin = 1
+# ak7PFJets.src = cms.InputTag("particleFlowTmp")
+# ak7CaloJets.jetPtMin = 1
 akVs1PFJets.jetPtMin = 1
 akVs1PFJets.src = cms.InputTag("particleFlowTmp")
 akVs1CaloJets.jetPtMin = 1
@@ -148,43 +148,43 @@ akPu6PFJets.jetPtMin = 1
 akPu6CaloJets.jetPtMin = 1
 akPu7PFJets.jetPtMin = 1
 akPu7CaloJets.jetPtMin = 1
-ak1PFJets.jetPtMin = 1
-ak1PFJets.src = cms.InputTag("particleFlowTmp")
-ak1CaloJets.jetPtMin = 1
-ak2PFJets.jetPtMin = 1
-ak2PFJets.src = cms.InputTag("particleFlowTmp")
-ak2CaloJets.jetPtMin = 1
-ak3PFJets.jetPtMin = 1
-ak3PFJets.src = cms.InputTag("particleFlowTmp")
-ak3CaloJets.jetPtMin = 1
-ak4PFJets.jetPtMin = 1
-ak4PFJets.src = cms.InputTag("particleFlowTmp")
-ak4CaloJets.jetPtMin = 1
-ak5PFJets.jetPtMin = 1
-ak5PFJets.src = cms.InputTag("particleFlowTmp")
-ak5CaloJets.jetPtMin = 1
-ak6PFJets.jetPtMin = 1
-ak6PFJets.src = cms.InputTag("particleFlowTmp")
-ak6CaloJets.jetPtMin = 1
-ak7PFJets.jetPtMin = 1
-ak7PFJets.src = cms.InputTag("particleFlowTmp")
-ak7CaloJets.jetPtMin = 1
+# ak1PFJets.jetPtMin = 1
+# ak1PFJets.src = cms.InputTag("particleFlowTmp")
+# ak1CaloJets.jetPtMin = 1
+# ak2PFJets.jetPtMin = 1
+# ak2PFJets.src = cms.InputTag("particleFlowTmp")
+# ak2CaloJets.jetPtMin = 1
+# ak3PFJets.jetPtMin = 1
+# ak3PFJets.src = cms.InputTag("particleFlowTmp")
+# ak3CaloJets.jetPtMin = 1
+# ak4PFJets.jetPtMin = 1
+# ak4PFJets.src = cms.InputTag("particleFlowTmp")
+# ak4CaloJets.jetPtMin = 1
+# ak5PFJets.jetPtMin = 1
+# ak5PFJets.src = cms.InputTag("particleFlowTmp")
+# ak5CaloJets.jetPtMin = 1
+# ak6PFJets.jetPtMin = 1
+# ak6PFJets.src = cms.InputTag("particleFlowTmp")
+# ak6CaloJets.jetPtMin = 1
+# ak7PFJets.jetPtMin = 1
+# ak7PFJets.src = cms.InputTag("particleFlowTmp")
+# ak7CaloJets.jetPtMin = 1
 
 hiReRecoPFJets = cms.Sequence(
-ak1PFJets
-+
-ak2PFJets
-+
-ak3PFJets
-+
-ak4PFJets
-+
-ak5PFJets
-+
-ak6PFJets
-+
-ak7PFJets
-+
+# ak1PFJets
+# +
+# ak2PFJets
+# +
+# ak3PFJets
+# +
+# ak4PFJets
+# +
+# ak5PFJets
+# +
+# ak6PFJets
+# +
+# ak7PFJets
+# +
 akPu1PFJets
 +
 akPu2PFJets
@@ -215,20 +215,20 @@ akVs7PFJets
 )
 
 hiReRecoCaloJets = cms.Sequence(
-ak1CaloJets
-+
-ak2CaloJets
-+
-ak3CaloJets
-+
-ak4CaloJets
-+
-ak5CaloJets
-+
-ak6CaloJets
-+
-ak7CaloJets
-+
+# ak1CaloJets
+# +
+# ak2CaloJets
+# +
+# ak3CaloJets
+# +
+# ak4CaloJets
+# +
+# ak5CaloJets
+# +
+# ak6CaloJets
+# +
+# ak7CaloJets
+# +
 akPu1CaloJets
 +
 akPu2CaloJets
