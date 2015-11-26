6e70f5678829688692b7a69ecdda8d92e57ea40e
Janssen Xavier <xavier.janssen@ua.ac.be>
2015-11-26 14:55:02 +0100

    Update rivetHarvesting_cfi.py

--- GeneratorInterface/RivetInterface/python/rivetHarvesting_cfi.py
+++ GeneratorInterface/RivetInterface/python/rivetHarvesting_cfi.py
@@ -4,8 +4,8 @@ rivetHarvesting = cms.EDAnalyzer('RivetHarvesting',
   AnalysisNames = cms.vstring('CMS_2010_S8808686', 'MC_DIPHOTON', 'MC_JETS', 'MC_GENERIC'),
   CrossSection = cms.double(1000),
   HepMCCollection = cms.InputTag('generatorSmeared'),
-  OutputFile = cms.string('out.aida'),
-  FilesToHarvest = cms.vstring('file1.aida', 'file2.aida'),
+  OutputFile = cms.string('out.yoda'),
+  FilesToHarvest = cms.vstring('file1.yoda', 'file2.yoda'),
   VSumOfWeights = cms.vdouble(1.0, 1.0),
   VCrossSections = cms.vdouble(1.0, 1.0)
 )
