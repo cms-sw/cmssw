1ae462066a40f645c0a305d8876d8967cabd0290
federico de guio <federico.de.guio@cern.ch>
2015-11-27 11:53:01 +0100

    adapth other sequences in the matrix and the validation sequence miniAOD as well

--- DQMOffline/Configuration/python/DQMOffline_cff.py
+++ DQMOffline/Configuration/python/DQMOffline_cff.py
@@ -133,5 +133,7 @@ DQMOfflineBTag = cms.Sequence( bTagPlotsDATA )
                                                                  
 DQMOfflineMiniAOD = cms.Sequence()
 
-DQMOfflineMiniAODEndPath = cms.Sequence(miniAODDQMSequence*jetMETDQMOfflineSourceMiniAOD*tracksDQMMiniAOD)
-DQMOfflineEndPath = cms.Sequence()
+#Post sequences are automatically placed in the EndPath by ConfigBuilder if PAT is run.
+#miniAOD DQM sequences need to access the filter results.
+PostDQMOfflineMiniAOD = cms.Sequence(miniAODDQMSequence*jetMETDQMOfflineSourceMiniAOD*tracksDQMMiniAOD)
+PostDQMOffline = cms.Sequence()
