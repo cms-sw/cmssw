5190a530cbccbc7a5b18f0d5ae14dc70260783f0
piet verwilligen <piet.verwilligen@cern.ch>
2015-12-01 16:51:01 +0100

    removed config parameter for MC truth

--- RecoLocalMuon/GEMRecHit/python/me0RecHits_cfi.py
+++ RecoLocalMuon/GEMRecHit/python/me0RecHits_cfi.py
@@ -1,9 +1,7 @@
 import FWCore.ParameterSet.Config as cms
 
 me0RecHits = cms.EDProducer("ME0RecHitProducer",
-    recAlgoConfig = cms.PSet(
-        recOnlyMuons = cms.bool(False),
-    ),
+    recAlgoConfig = cms.PSet(),
     recAlgo = cms.string('ME0RecHitStandardAlgo'),
     me0DigiLabel = cms.InputTag("simMuonME0Digis"),
 )
