35d4c78f4ea125c7db47e638f099e13b0b076e4d
Piet <piet.verwilligen@cern.ch>
2015-12-01 17:32:24 +0100

    configure the reconstruction such that you reco only muons (case 1) or muons+prompt bkg (case 2) or all (muons + prompt bkg + neutron bkg) (case 3)

--- RecoLocalMuon/GEMRecHit/python/me0RecHits_cfi.py
+++ RecoLocalMuon/GEMRecHit/python/me0RecHits_cfi.py
@@ -2,7 +2,8 @@ import FWCore.ParameterSet.Config as cms
 
 me0RecHits = cms.EDProducer("ME0RecHitProducer",
     recAlgoConfig = cms.PSet(
-
+        recOnlyMuons = cms.bool(False),
+        recOnlyPrompt = cms.bool(False),
     ),
     recAlgo = cms.string('ME0RecHitStandardAlgo'),
     me0DigiLabel = cms.InputTag("simMuonME0Digis"),
