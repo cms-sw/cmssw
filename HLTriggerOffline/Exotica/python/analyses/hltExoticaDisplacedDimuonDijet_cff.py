873dc42d59c5b34606b4036718fd8a08876c48be
Ryo Yonamine <ryo.yonamine@ulb.ac.be>
2015-12-01 13:00:47 +0100

    Optimized Dxy bins for DisplacedSUSY. Added Dxy plots for DisplacedL2Dimuon.

--- HLTriggerOffline/Exotica/python/analyses/hltExoticaDisplacedDimuonDijet_cff.py
+++ HLTriggerOffline/Exotica/python/analyses/hltExoticaDisplacedDimuonDijet_cff.py
@@ -10,6 +10,7 @@ DisplacedDimuonDijetPSet = cms.PSet(
     minCandidates = cms.uint32(2),
     # -- Analysis specific binnings
 
+    parametersDxy      = cms.vdouble(50, -50, 50),
     parametersTurnOn = cms.vdouble( 0, 5, 10, 15, 20, 25, 30, 35, 40, 45, 50,
                                     60, 70, 80, 100
                                    ),
--- HLTriggerOffline/Exotica/python/analyses/hltExoticaDisplacedL2Dimuon_cff.py
+++ HLTriggerOffline/Exotica/python/analyses/hltExoticaDisplacedL2Dimuon_cff.py
@@ -11,9 +11,7 @@ DisplacedL2DimuonPSet = cms.PSet(
     # -- Analysis specific cuts
     minCandidates = cms.uint32(2),
     # -- Analysis specific binnings
-    #parametersTurnOn = cms.vdouble( 0, 50, 100, 150, 200, 250, 300, 400, 500, 600, 700, 800, 900, 1000,
-    #                                1100, 1200, 1500
-    #                               ),
+    parametersDxy      = cms.vdouble(50, -50, 50),
     parametersTurnOn = cms.vdouble( 0, 5, 10, 15, 20, 25, 30, 35, 40, 45, 50,
                                     60, 70, 80, 100
                                    ),
