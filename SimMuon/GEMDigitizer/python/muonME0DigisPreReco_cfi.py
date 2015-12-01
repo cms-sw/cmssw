483c89fdb3037432550a98bd4f96f3ca80da0c88
Piet <piet.verwilligen@cern.ch>
2015-12-01 17:28:45 +0100

    rewrote background simulation, added prompt param, digitize signal only in [-minBx, maxBx]

--- SimMuon/GEMDigitizer/python/muonME0DigisPreReco_cfi.py
+++ SimMuon/GEMDigitizer/python/muonME0DigisPreReco_cfi.py
@@ -5,17 +5,17 @@ simMuonME0Digis = cms.EDProducer("ME0DigiPreRecoProducer",
     inputCollection = cms.string('g4SimHitsMuonME0Hits'),
     digiPreRecoModelString = cms.string('PreRecoGaussian'),
     timeResolution = cms.double(0.001), # in ns
-    phiResolution = cms.double(0.05), # in cm average resoltion along local x in case of no correlation
-    etaResolution = cms.double(1.),# in cm average resoltion along local y in case of no correlation
+    phiResolution = cms.double(0.05),   # in cm average resolution along local x in case of no correlation
+    etaResolution = cms.double(1.),     # in cm average resolution along local y in case of no correlation
     useCorrelation = cms.bool(False),
     useEtaProjectiveGEO = cms.bool(False),
     averageEfficiency = cms.double(0.98),
-    doBkgNoise = cms.bool(True), #False == No background simulation
     digitizeOnlyMuons = cms.bool(False),
-# simulateIntrinsicNoise = cms.bool(False),
-    simulateElectronBkg = cms.bool(True), #True - will simulate electron background
-    averageNoiseRate = cms.double(0.001), #intrinsic noise
-    bxwidth = cms.int32(25),
-    minBunch = cms.int32(-5), ## in terms of 25 ns
-    maxBunch = cms.int32(3)
+    gaussianSmearing = cms.bool(True),
+    # simulateIntrinsicNoise = cms.bool(False), # intrinsic noise --> not implemented
+    # averageNoiseRate = cms.double(0.001),     # intrinsic noise --> not implemented
+    simulateElectronBkg = cms.bool(True),       # True - will simulate electron background
+    simulateNeutralBkg  = cms.bool(True),       # True - will simulate neutral (n+g)  background
+    minBunch = cms.int32(-5),                   # [x 25 ns], forms the readout window together with maxBunch,
+    maxBunch = cms.int32(3),                    # we should think of shrinking this window ...
 )
