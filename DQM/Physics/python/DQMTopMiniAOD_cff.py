985414b53f353e4cb6a184c8b1f5b8b35d5d5241
Bugra <bugra.bilin@cern.ch>
2015-12-03 16:41:06 +0100

    bugfix (cherry picked from commit b717d7ed0c9a88c07ac7f9b7233673f4bd291c4b)

--- DQM/Physics/python/DQMTopMiniAOD_cff.py
+++ DQM/Physics/python/DQMTopMiniAOD_cff.py
@@ -2,6 +2,6 @@ import FWCore.ParameterSet.Config as cms
 
 from DQM.Physics.topSingleLeptonDQM_miniAOD_cfi import *
 
-topPhysicsminiAOD = cms.Sequence(    process.topSingleMuonMediumDQM_miniAOD +
-                                     process.topSingleElectronMediumDQM_miniAOD 
+topPhysicsminiAOD = cms.Sequence(    topSingleMuonMediumDQM_miniAOD 
+                                     *topSingleElectronMediumDQM_miniAOD 
                                  )
