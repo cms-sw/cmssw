caa939934f7c4f8c64512cdea76539679e3ef219
David <dnash@cern.ch>
2015-12-07 17:15:18 +0100

    Revert STAMuonAnalyzer

--- RecoMuon/StandAloneMuonProducer/test/STAMuonAnalyzer.h
+++ RecoMuon/StandAloneMuonProducer/test/STAMuonAnalyzer.h
@@ -45,7 +45,7 @@ private:
 
   // Histograms
   TH1F *hPtRec;
-  TH1F *hPtSim;
+  TH1F *hPtSim; 
   TH1F *hPres;
   TH1F *h1_Pres;
   TH1F *hPTDiff;
@@ -58,6 +58,7 @@ private:
   int numberOfRecTracks;
 
   std::string theDataType;
-
+  
 };
 #endif
+
