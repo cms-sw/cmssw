5973b2f19a50ccc3be4535dc6b2f9a0348bb3a1f
Stanislav <istaslis@gmail.com>
2015-12-07 14:45:58 +0100

    Tight tracks for b-tagging

--- HeavyIonsAnalysis/JetAnalysis/python/bTaggingTracks_cff.py
+++ HeavyIonsAnalysis/JetAnalysis/python/bTaggingTracks_cff.py
@@ -2,7 +2,7 @@ import FWCore.ParameterSet.Config as cms
 
 PureTracks = cms.EDFilter("TrackSelector",
                        src = cms.InputTag("hiGeneralTracks"),
-                       cut = cms.string('quality("highPurity")'))
+                       cut = cms.string('quality("tight")'))
 
 from RecoVertex.PrimaryVertexProducer.OfflinePrimaryVertices_cfi import offlinePrimaryVertices
 offlinePrimaryVertices.TrackLabel = 'PureTracks'
