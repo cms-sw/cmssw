4ace592da0f2213d5dfefefdeea225a333c89987
Martin Grunewald <Martin.Grunewald@cern.ch>
2015-12-09 18:48:49 +0100

    add switch also in autoHLT.py

--- Configuration/HLT/python/autoHLT.py
+++ Configuration/HLT/python/autoHLT.py
@@ -2,6 +2,10 @@
 #   cmsDiver.py hlt -s HLT:@relval
 
 autoHLT = {
+  'frozenHIon' : 'HIon_2015_v2',
+  'relvalHIon' : 'HIon',
+  'frozenPRef' : 'pp5TeV_2015_v2',
+  'relvalPRef' : 'PRef',
   'frozen25ns' : '25ns14e33_v4',
   'relval25ns' : '25ns14e33_v4',
   'frozen50ns' : '50ns_5e33_v3',
