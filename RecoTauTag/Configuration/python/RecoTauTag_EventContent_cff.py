0526183af721149a113ef8126282de5623687405
AJ Johnson <andrewj314@gmail.com>
2015-12-09 19:36:22 +0100

    Added missing keep statements, updated configs to miniAOD v2

--- RecoTauTag/Configuration/python/RecoTauTag_EventContent_cff.py
+++ RecoTauTag/Configuration/python/RecoTauTag_EventContent_cff.py
@@ -111,7 +111,10 @@ RecoTauTagRECO = cms.PSet(
         'keep *_hpsPFTauMVA3IsolationChargedIsoPtSum_*_*',
         'keep *_hpsPFTauMVA3IsolationNeutralIsoPtSum_*_*',
         'keep *_hpsPFTauMVA3IsolationPUcorrPtSum_*_*',
-        'keep *_hpsPFTauTransverseImpactParameters_*_*'
+        'keep *_hpsPFTauTransverseImpactParameters_*_*',
+        'keep *_hpsPFTauFootprintCorrection_*_*',
+        'keep *_hpsPFTauNeutralIsoPtSumWeight_*_*',
+        'keep *_hpsPFTauPhotonPtSumOutsideSignalCone_*_*'
     )
 )
 #AOD content
