1526fdf62a5788bf817556678edebd259add5a3f
Vincenzo Innocente <vincenzo.innocente@cern.ch>
2015-12-14 09:03:32 +0100

    printouts (to be removed)

--- TrackingTools/DetLayers/src/GeomDetCompatibilityChecker.cc
+++ TrackingTools/DetLayers/src/GeomDetCompatibilityChecker.cc
@@ -21,8 +21,7 @@ namespace{
 
     long long nth=0;
 
-                               // Geom checker 1337311 84696 634946 20369 243444 15267 21082 756150
-                              //Geom checker 1337311        84696 634946          20369 90758               0 5815 756150
+                            //Geom checker     1337311   84696       634946      20369      259701        241266       18435         614128
     ~Stat() { std::cout << "Geom checker " << ntot<<' '<< nf1<<' '<< nf2 <<' '<< ns1<< ' '<< ns2 << ' ' << ns11 << ' '<< ns21 << ' ' << nth << std::endl;}
 
   };
@@ -38,6 +37,8 @@ GeomDetCompatibilityChecker::isCompatible(const GeomDet* theDet,
 					  const MeasurementEstimator& est) {
   stat.ntot++;
 
+  constexpr float sagCut = 2;
+
   auto const & plane = theDet->specificSurface();
   StraightLinePlaneCrossing crossing(tsos.globalPosition().basicVector(),tsos.globalMomentum().basicVector(), prop.propagationDirection());
   auto path = crossing.pathLength(plane);
@@ -46,21 +47,23 @@ GeomDetCompatibilityChecker::isCompatible(const GeomDet* theDet,
   if unlikely ( !propSt.isValid()) { stat.nf1++; return std::make_pair( false, std::move(propSt));}
   auto isIn = path.first;
   float thresh=99999999;
+  bool close = false;
   if(!path.first) stat.ns1++;
   else {
     auto gpos =  GlobalPoint(crossing.position(path.second));
     auto tpath = (gpos-tsos.globalPosition()).perp();
-    thresh = std::abs(tpath*tsos.globalParameters().transverseCurvature());
-    if (thresh<0.2) stat.nth++;
+    thresh = std::abs(tpath*tpath*tsos.globalParameters().transverseCurvature());
+    close = thresh<sagCut;
+    if (close) stat.nth++;
     auto pos = plane.toLocal(GlobalPoint(crossing.position(path.second)));
     auto toll = LocalError(1.,0,1.);
     isIn = plane.bounds().inside(pos,toll);
-    if (thresh<0.2 && !isIn) stat.ns2++; 
+    if (close && !isIn) { stat.ns2++;}// return std::make_pair( false,TrajectoryStateOnSurface()); }
   }
   auto es = est.estimate( propSt, theDet->specificSurface());
   if (!es) stat.nf2++;
-  if (thresh<0.2 && es &&(!path.first)) stat.ns11++;
-  if (thresh<0.2 && es &&(!isIn)) { stat.ns21++; std::cout << thresh << std::endl;}
+  if (close && (!isIn) && (!es) ) stat.ns11++;
+  if (close && es &&(!isIn)) { stat.ns21++; std::cout << thresh << std::endl;}
   return std::make_pair( es, std::move(propSt));
 
 }
