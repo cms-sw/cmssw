6b8e996b76f959a86781b60077075155373ad95e
R. Alex Barbieri <luck@mit.edu>
2015-12-15 23:04:16 +0100

    fixes for bad syntax

--- HeavyIonsAnalysis/VectorBosonAnalysis/python/tupelSequence_PbPb_mc_cff.py
+++ HeavyIonsAnalysis/VectorBosonAnalysis/python/tupelSequence_PbPb_mc_cff.py
@@ -4,10 +4,8 @@ from HeavyIonsAnalysis.VectorBosonAnalysis.tupelSequence_PbPb_cff import *
 
 from PhysicsTools.PatAlgos.mcMatchLayer0.photonMatch_cfi import photonMatch
 photonMatch.src = cms.InputTag("photons")
-patPhotonSequence.remove([patPhotons])
-patPhotonSequence.append([photonMatch, patPhotons])
+patPhotonSequence = cms.Sequence(photonMatch + patPhotons)
 
 from PhysicsTools.PatAlgos.mcMatchLayer0.electronMatch_cfi import electronMatch
 electronMatch.src = cms.InputTag("gedGsfElectronsTmp")
-patElectronSequence.remove([patElectrons])
-patElectronSequence.append([electronMatch, patElectrons])
+patElectronSequence = cms.Sequence(electronMatch + patElectrons)
--- HeavyIonsAnalysis/VectorBosonAnalysis/python/tupelSequence_pp_mc_cff.py
+++ HeavyIonsAnalysis/VectorBosonAnalysis/python/tupelSequence_pp_mc_cff.py
@@ -3,9 +3,7 @@ import FWCore.ParameterSet.Config as cms
 from HeavyIonsAnalysis.VectorBosonAnalysis.tupelSequence_pp_cff import *
 
 from PhysicsTools.PatAlgos.mcMatchLayer0.photonMatch_cfi import photonMatch
-patPhotonSequence.remove([patPhotons])
-patPhotonSequence.append([photonMatch, patPhotons])
+patPhotonSequence = cms.Sequence(photonMatch + patPhotons)
 
 from PhysicsTools.PatAlgos.mcMatchLayer0.electronMatch_cfi import electronMatch
-patElectronSequence.remove([patElectrons])
-patElectronSequence.append([electronMatch, patElectrons])
+patElectronSequence = cms.Sequence(electronMatch + patElectrons)
