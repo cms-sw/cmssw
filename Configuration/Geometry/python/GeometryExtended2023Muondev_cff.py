946bcef603ddaa4b06c5a2a7faa349bbe36a7dbb
Gaelle <Gaelle.Boudoul@cern.ch>
2015-12-16 18:22:11 +0100

    Removing MuonDev

--- Configuration/Geometry/python/GeometryExtended2023Muondev_cff.py
+++
@@ -1,8 +0,0 @@
-import FWCore.ParameterSet.Config as cms
-#
-# Geometry master configuration
-#
-# Ideal geometry, needed for simulation
-from Geometry.CMSCommonData.cmsExtendedGeometry2023MuondevXML_cfi import *
-from Geometry.TrackerNumberingBuilder.trackerNumbering2023Geometry_cfi import *
-from Geometry.HcalCommonData.hcalSimNumberingInitialization_cfi import *
