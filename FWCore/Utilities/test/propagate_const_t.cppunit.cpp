fb52373c52d8c1c55ad52a927e62db9ffac716f8
Chris Jones <chrisdjones15@gmail.com>
2015-12-18 16:43:02 -0600

    Added edm::propagate_const
    propagate_const is an C++17 experimental class used to make pointers and pointer like classes have 'deep const' semantics. This is very useful for reasoning about const thread-safety.
    This is a partial implementation of the proposed standard. We will replace this class once the actual one is in the C++ standard.

---
+++ FWCore/Utilities/test/propagate_const_t.cppunit.cpp
@@ -0,0 +1,88 @@
+/*----------------------------------------------------------------------
+
+Test program for edm::propagate_const class.
+
+ ----------------------------------------------------------------------*/
+
+#include <cppunit/extensions/HelperMacros.h>
+#include <memory>
+#include "FWCore/Utilities/interface/propagate_const.h"
+
+class test_propagate_const: public CppUnit::TestFixture
+{
+CPPUNIT_TEST_SUITE(test_propagate_const);
+
+CPPUNIT_TEST(test);
+
+CPPUNIT_TEST_SUITE_END();
+public:
+  void setUp(){}
+  void tearDown(){}
+
+  void test();
+};
+
+///registration of the test so that the runner can find it
+CPPUNIT_TEST_SUITE_REGISTRATION(test_propagate_const);
+
+namespace {
+   class ConstChecker {
+   public:
+      int value() { return 0;}
+      
+      int const value() const { return 1;}
+      
+   };
+}
+
+
+void test_propagate_const::test()
+{
+   {
+      ConstChecker checker;
+      edm::propagate_const<ConstChecker*> pChecker(&checker);
+
+      CPPUNIT_ASSERT(0 == pChecker.get()->value());
+      CPPUNIT_ASSERT( pChecker.get()->value() == pChecker->value());
+      CPPUNIT_ASSERT( pChecker.get()->value() == (*pChecker).value());
+
+      const edm::propagate_const<ConstChecker*> pConstChecker(&checker);
+
+      CPPUNIT_ASSERT(1 == pConstChecker.get()->value());
+      CPPUNIT_ASSERT( pConstChecker.get()->value() == pConstChecker->value());
+      CPPUNIT_ASSERT( pConstChecker.get()->value() == (*pConstChecker).value());
+   }
+   
+   {
+      auto checker = std::make_shared<ConstChecker>();
+      edm::propagate_const<std::shared_ptr<ConstChecker>> pChecker(checker);
+      
+      CPPUNIT_ASSERT(0 == pChecker.get()->value());
+      CPPUNIT_ASSERT( pChecker.get()->value() == pChecker->value());
+      CPPUNIT_ASSERT( pChecker.get()->value() == (*pChecker).value());
+      
+      const edm::propagate_const<std::shared_ptr<ConstChecker>> pConstChecker(checker);
+      
+      CPPUNIT_ASSERT(1 == pConstChecker.get()->value());
+      CPPUNIT_ASSERT( pConstChecker.get()->value() == pConstChecker->value());
+      CPPUNIT_ASSERT( pConstChecker.get()->value() == (*pConstChecker).value());
+      
+   }
+   
+   {
+      auto checker = std::make_unique<ConstChecker>();
+      edm::propagate_const<std::unique_ptr<ConstChecker>> pChecker(std::move(checker));
+      
+      CPPUNIT_ASSERT(0 == pChecker.get()->value());
+      CPPUNIT_ASSERT( pChecker.get()->value() == pChecker->value());
+      CPPUNIT_ASSERT( pChecker.get()->value() == (*pChecker).value());
+      
+      const edm::propagate_const<std::unique_ptr<ConstChecker>> pConstChecker(std::make_unique<ConstChecker>());
+      
+      CPPUNIT_ASSERT(1 == pConstChecker.get()->value());
+      CPPUNIT_ASSERT( pConstChecker.get()->value() == pConstChecker->value());
+      CPPUNIT_ASSERT( pConstChecker.get()->value() == (*pConstChecker).value());
+      
+   }
+
+}
