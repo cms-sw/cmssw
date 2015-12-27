aafd23bab0897738752ff44709381b0f57108417
Sunanda <sunanda.banerjee@cern.ch>
2015-12-27 19:18:14 +0100

    Modify the cff's with HGCal

--- Configuration/Geometry/python/GeometryExtended2023Reco_cff.py
+++ Configuration/Geometry/python/GeometryExtended2023Reco_cff.py
@@ -17,6 +17,7 @@ from Geometry.TrackerNumberingBuilder.trackerTopology_cfi import *
 from Geometry.MuonNumbering.muonNumberingInitialization_cfi import *
 from RecoMuon.DetLayers.muonDetLayerGeometry_cfi import *
 from Geometry.GEMGeometryBuilder.gemGeometry_cfi import *
+from Geometry.GEMGeometryBuilder.me0Geometry_cfi import *
 
 #  Alignment
 from Geometry.TrackerGeometryBuilder.idealForDigiTrackerGeometry_cff import *
@@ -25,10 +26,28 @@ from Geometry.DTGeometryBuilder.idealForDigiDtGeometry_cff import *
 trackerGeometry.applyAlignment = cms.bool(False)
 
 #  Calorimeters
+from Geometry.CaloEventSetup.HGCalTopology_cfi import *
+from Geometry.HGCalGeometry.HGCalGeometryESProducer_cfi import *
 from Geometry.CaloEventSetup.CaloTopology_cfi import *
-from Geometry.CaloEventSetup.CaloGeometry_cff import *
+from Geometry.CaloEventSetup.CaloGeometryBuilder_cfi import *
+
+CaloGeometryBuilder = cms.ESProducer("CaloGeometryBuilder",
+    SelectedCalos = cms.vstring('HCAL'          , 
+                                'ZDC'           ,
+                                'CASTOR'        ,
+                                'EcalBarrel'    , 
+                                'TOWER'           )
+)
+
+from Geometry.EcalAlgo.EcalBarrelGeometry_cfi import *
+from Geometry.HcalEventSetup.HcalGeometry_cfi import *
+from Geometry.HcalEventSetup.CaloTowerGeometry_cfi import *
+from Geometry.HcalEventSetup.CaloTowerTopology_cfi import *
+from Geometry.HcalCommonData.hcalDDDRecConstants_cfi import *
+from Geometry.HcalEventSetup.hcalTopologyIdeal_cfi import *
+from Geometry.ForwardGeometry.ForwardGeometry_cfi import *
+
 from Geometry.CaloEventSetup.EcalTrigTowerConstituents_cfi import *
 from Geometry.EcalMapping.EcalMapping_cfi import *
 from Geometry.EcalMapping.EcalMappingRecord_cfi import *
-from Geometry.HcalCommonData.hcalDDDRecConstants_cfi import *
-from Geometry.HcalEventSetup.hcalTopologyIdeal_cfi import *
+
