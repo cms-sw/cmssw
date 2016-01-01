bc8521a3250d9837a61ec532e8fd736b9eb497f3
Matti Kortelainen <matti.kortelainen@cern.ch>
2016-01-01 13:45:45 +0100

    Add duplicate rate vs. DeltaR (cherry picked from commit d7dd4977a773eebdf9193fec53ed40465eb977c6)

--- Validation/RecoTrack/interface/MTVHistoProducerAlgoForTracker.h
+++ Validation/RecoTrack/interface/MTVHistoProducerAlgoForTracker.h
@@ -175,7 +175,7 @@ class MTVHistoProducerAlgoForTracker {
 
   std::vector<MonitorElement*> h_recovertpos, h_assocvertpos, h_assoc2vertpos, h_simulvertpos, h_loopervertpos, h_pileupvertpos;
   std::vector<MonitorElement*> h_recozpos, h_assoczpos, h_assoc2zpos, h_simulzpos, h_looperzpos, h_pileupzpos;
-  std::vector<MonitorElement*> h_assocdr, h_assoc2dr, h_simuldr, h_recodr, h_pileupdr;
+  std::vector<MonitorElement*> h_assocdr, h_assoc2dr, h_simuldr, h_recodr, h_looperdr, h_pileupdr;
   std::vector<MonitorElement*> h_recochi2, h_assoc2chi2, h_looperchi2, h_misidchi2, h_pileupchi2;
   std::vector<MonitorElement*> h_pt, h_eta, h_pullTheta,h_pullPhi,h_pullDxy,h_pullDz,h_pullQoverp;
   std::vector<MonitorElement*> h_assoc2_itpu_eta, h_assoc2_itpu_sig_eta, h_assoc2eta_sig;
