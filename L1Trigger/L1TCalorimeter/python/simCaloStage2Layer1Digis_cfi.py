593ae69235d0c06e7c8cfcb1ee7399ed9b6eea33
Michael Mulhearn <mulhearn@cern.ch>
2016-01-05 15:27:57 +0100

    updates for 2016 (Stage-2) L1T calorimeter trigger
    Conflicts:
    	CondFormats/L1TObjects/interface/CaloParams.h
    	L1Trigger/L1TCalorimeter/interface/CaloParamsHelper.h
    	L1Trigger/L1TCalorimeter/interface/Stage2Layer2TauAlgorithmFirmware.h
    	L1Trigger/L1TCalorimeter/plugins/L1TCaloParamsESProducer.cc
    	L1Trigger/L1TCalorimeter/plugins/L1TStage2Layer2Producer.cc
    	L1Trigger/L1TCalorimeter/src/firmware/Stage1Layer2EtSumAlgorithmImpHI.cc
    	L1Trigger/L1TCalorimeter/src/firmware/Stage2Layer2DemuxEGAlgoFirmwareImp1.cc
    	L1Trigger/L1TCalorimeter/src/firmware/Stage2Layer2DemuxSumsAlgoFirmwareImp1.cc
    	L1Trigger/L1TCalorimeter/src/firmware/Stage2Layer2DemuxTauAlgoFirmwareImp1.cc
    	L1Trigger/L1TCalorimeter/src/firmware/Stage2Layer2EGammaAlgorithmFirmwareImp1.cc
    	L1Trigger/L1TCalorimeter/src/firmware/Stage2Layer2EtSumAlgorithmFirmwareImp1.cc
    	L1Trigger/L1TCalorimeter/src/firmware/Stage2Layer2JetAlgorithmFirmwareImp1.cc
    	L1Trigger/L1TCalorimeter/src/firmware/Stage2Layer2JetSumAlgorithmFirmwareImp1.cc
    	L1Trigger/L1TCalorimeter/src/firmware/Stage2Layer2TauAlgorithmFirmwareImp1.cc
    	L1Trigger/L1TCalorimeter/src/firmware/Stage2TowerCompressAlgorithmFirmwareImp1.cc
    	L1Trigger/L1TCalorimeter/src/firmware/Stage2TowerDecompressAlgorithmFirmwareImp1.cc

--- L1Trigger/L1TCalorimeter/python/simCaloStage2Layer1Digis_cfi.py
+++ L1Trigger/L1TCalorimeter/python/simCaloStage2Layer1Digis_cfi.py
@@ -1,11 +1,11 @@
 import FWCore.ParameterSet.Config as cms
 
-caloStage2Layer1Digis = cms.EDProducer(
+simCaloStage2Layer1Digis = cms.EDProducer(
     "L1TStage2Layer1Producer",
     verbosity = cms.int32(2),
     bxFirst    = cms.int32(0),
     bxLast     = cms.int32(0),
-    ecalToken = cms.InputTag("ecalDigis:EcalTriggerPrimitives"),
-    hcalToken = cms.InputTag("hcalDigis"),
+    ecalToken = cms.InputTag("simEcalTriggerPrimitiveDigis"),
+    hcalToken = cms.InputTag("simHcalTriggerPrimitiveDigis"),
     firmware = cms.int32(1)
 )
