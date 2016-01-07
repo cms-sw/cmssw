b359a32f657340001ab82b65dbf7c9b9d3c48ad1
Arnaud <arnaud.chiron@cern.ch>
2016-01-07 16:52:02 +0100

    added CMSSW_8_0_DEVEL_X_2016-01-06-2300

--- Validation/RecoEgamma/python/egammaPostValidation_cff.py
+++ Validation/RecoEgamma/python/egammaPostValidation_cff.py
@@ -3,4 +3,5 @@ import FWCore.ParameterSet.Config as cms
 from Validation.RecoEgamma.photonPostProcessor_cff import *
 from Validation.RecoEgamma.electronPostValidationSequence_cff import *
 
-egammaPostValidation = cms.Sequence(photonPostProcessor+electronPostValidationSequence )
+egammaPostValidation = cms.Sequence(photonPostProcessor+electronPostValidationSequence)
+egammaPostValidationMiniAOD = cms.Sequence(electronPostValidationSequenceMiniAOD)
