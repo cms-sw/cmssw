fa081aa88307db7371e231b8f10bef4b8f558e66
Arnaud <arnaud.chiron@cern.ch>
2016-01-07 16:30:02 +0100

    added CMSSW_8_0_DEVEL_X_2016-01-06-2300

--- Validation/RecoEgamma/plugins/ElectronMcMiniAODSignalValidator.h
+++ Validation/RecoEgamma/plugins/ElectronMcMiniAODSignalValidator.h
@@ -36,7 +36,7 @@ class ElectronMcSignalValidatorMiniAOD : public ElectronDqmAnalyzerBase {
       explicit ElectronMcSignalValidatorMiniAOD(const edm::ParameterSet&);
       virtual ~ElectronMcSignalValidatorMiniAOD();
       bool isAncestor(const reco::Candidate * ancestor, const reco::Candidate * particle);
-       
+      
    private:
       virtual void bookHistograms( DQMStore::IBooker &, edm::Run const &, edm::EventSetup const &) ;
       virtual void analyze(const edm::Event&, const edm::EventSetup&) override;
