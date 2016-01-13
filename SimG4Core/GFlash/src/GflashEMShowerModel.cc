0eefe8d46721bc1952dbb281f77fe86dca0b9ea4
Vladimir <Vladimir.Ivantchenko@cern.ch>
2016-01-13 19:29:53 +0100

    cleanup (cherry picked from commit 94761095dfb8442af3fb8652825a3290a1169cbd)

--- SimG4Core/GFlash/src/GflashEMShowerModel.cc
+++ SimG4Core/GFlash/src/GflashEMShowerModel.cc
@@ -26,8 +26,6 @@ GflashEMShowerModel::GflashEMShowerModel(const G4String& modelName,
 					 const edm::ParameterSet& parSet)
   : G4VFastSimulationModel(modelName, envelope), theParSet(parSet) {
 
-  //theWatcherOn = parSet.getParameter<bool>("watcherOn");
-
   theProfile = new GflashEMShowerProfile(parSet);
   theRegion  = const_cast<const G4Region*>(envelope); 
 
@@ -68,22 +66,7 @@ G4bool GflashEMShowerModel::ModelTrigger(const G4FastTrack & fastTrack ) {
 
   G4LogicalVolume* lv = pCurrentVolume->GetLogicalVolume();
   if(lv->GetRegion() != theRegion) { return false; }
-  //std::cout << "GflashEMShowerModel::ModelTrigger: LV " 
-  //	    << lv->GetRegion()->GetName() << std::endl;
-
-  // The parameterization starts inside crystals
-  //std::size_t pos1 = lv->GetName().find("EBRY");
-  //std::size_t pos2 = lv->GetName().find("EFRY");
-  
-  //std::size_t pos3 = lv->GetName().find("HVQ");
-  //std::size_t pos4 = lv->GetName().find("HF");
-  //if(pos1 == std::string::npos && pos2 == std::string::npos &&
-  //   pos3 == std::string::npos && pos4 == std::string::npos) return false;
-  //@@@for now, HF is not a part of Gflash Envelopes
-  //if(pos1 == std::string::npos && pos2 == std::string::npos ) return false;
-
   return true;
-
 }
 
 // -----------------------------------------------------------------------------------
@@ -128,12 +111,6 @@ void GflashEMShowerModel::makeHits(const G4FastTrack& fastTrack) {
       theGflashNavigator->LocateGlobalPointAndUpdateTouchableHandle(spotIter->getPosition(),G4ThreeVector(0,0,0),
 								    theGflashTouchableHandle, false);
       updateGflashStep(spotIter->getPosition(),spotIter->getTime());
-
-      // if there is a watcher defined in a job and the flag is turned on
-      if(theWatcherOn) {
-      	SteppingAction* userSteppingAction = (SteppingAction*) G4EventManager::GetEventManager()->GetUserSteppingAction();
-      	userSteppingAction->m_g4StepSignal(theGflashStep);
-      }
 
       // Send G4Step information to Hit/Digi if the volume is sensitive
       // Copied from G4SteppingManager.cc
--- SimG4Core/PhysicsLists/src/UrbanMscModel93.cc
+++ SimG4Core/PhysicsLists/src/UrbanMscModel93.cc
@@ -1,31 +1,3 @@
-//
-// ********************************************************************
-// * License and Disclaimer                                           *
-// *                                                                  *
-// * The  Geant4 software  is  copyright of the Copyright Holders  of *
-// * the Geant4 Collaboration.  It is provided  under  the terms  and *
-// * conditions of the Geant4 Software License,  included in the file *
-// * LICENSE and available at  http://cern.ch/geant4/license .  These *
-// * include a list of copyright holders.                             *
-// *                                                                  *
-// * Neither the authors of this software system, nor their employing *
-// * institutes,nor the agencies providing financial support for this *
-// * work  make  any representation or  warranty, express or implied, *
-// * regarding  this  software system or assume any liability for its *
-// * use.  Please see the license in the file  LICENSE  and URL above *
-// * for the full disclaimer and the limitation of liability.         *
-// *                                                                  *
-// * This  code  implementation is the result of  the  scientific and *
-// * technical work of the GEANT4 collaboration.                      *
-// * By using,  copying,  modifying or  distributing the software (or *
-// * any work based  on the software)  you  agree  to acknowledge its *
-// * use  in  resulting  scientific  publications,  and indicate your *
-// * acceptance of all terms of the Geant4 Software license.          *
-// ********************************************************************
-//
-//
-// $Id: UrbanMscModel93.cc 69120 2013-04-18 13:41:13Z vnivanch $
-//
 // -------------------------------------------------------------------
 //   
 // GEANT4 Class file
@@ -33,24 +5,14 @@
 //
 // File name:   UrbanMscModel93
 //
-// Author:      Laszlo Urban, V.Ivanchenko copy it from G4UrbanMscModel93
-//                            geant4-09-06-ref-07a global tag
+// Original author:    Laszlo Urban, 
 //
-// Creation date: 21.08.2013 
+//    V.Ivanchenko have copied from G4UrbanMscModel93 class
+//                 of Geant4 global tag geant4-09-06-ref-07 
+//                 and have adopted to CMSSW
 //
-// Modifications:
-//
-
-// Class Description:
-//
-// Implementation of the model of multiple scattering based on
-// H.W.Lewis Phys Rev 78 (1950) 526 and others
-
 // -------------------------------------------------------------------
-// In its present form the model can be  used for simulation 
-//   of the e-/e+, muon and charged hadron multiple scattering
 //
-
 
 //....oooOO0OOooo........oooOO0OOooo........oooOO0OOooo........oooOO0OOooo......
 //....oooOO0OOooo........oooOO0OOooo........oooOO0OOooo........oooOO0OOooo......
