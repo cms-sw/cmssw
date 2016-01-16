a568db492a939139aee4f36fe9dfc555bbfcd151
dertexaner <halilsaka@gmail.com>
2016-01-16 01:25:47 +0100

    Added new pulse shape flags

--- RecoLocalCalo/HcalRecAlgos/src/HcalSeverityLevelComputer.cc
+++ RecoLocalCalo/HcalRecAlgos/src/HcalSeverityLevelComputer.cc
@@ -47,6 +47,8 @@ bool HcalSeverityLevelComputer::getRecHitFlag(HcalSeverityDefinition& mydef,
   else if (mybit == "HBHETS4TS5Noise") setBit(HcalCaloFlagLabels::HBHETS4TS5Noise, mydef.HBHEFlagMask);
   else if (mybit == "HBHENegativeNoise") setBit(HcalCaloFlagLabels::HBHENegativeNoise, mydef.HBHEFlagMask);
   else if (mybit == "HBHEPulseFitBit") setBit(HcalCaloFlagLabels::HBHEPulseFitBit, mydef.HBHEFlagMask);
+  else if (mybit == "HBHETS3TS4OOTPU") setBit(HcalCaloFlagLabels::HBHETS3TS4OOTPU, mydef.HBHEFlagMask);
+  else if (mybit == "HBHETS5TS6OOTPU") setBit(HcalCaloFlagLabels::HBHETS5TS6OOTPU, mydef.HBHEFlagMask);
 
 
   // These are multi-bit counters; we may have to revisit how to set them in the SLComputer in the future
@@ -78,8 +80,8 @@ bool HcalSeverityLevelComputer::getRecHitFlag(HcalSeverityDefinition& mydef,
   else if (mybit == "ADCSaturationBit")     setAllRHMasks(HcalCaloFlagLabels::ADCSaturationBit,    mydef);
   else if (mybit== "AddedSimHcalNoise")     setAllRHMasks(HcalCaloFlagLabels::AddedSimHcalNoise,   mydef);
 
-  else if (mybit == "UserDefinedBit0")      setAllRHMasks(HcalCaloFlagLabels::UserDefinedBit0,     mydef);
-  else if (mybit == "UserDefinedBit1")      setAllRHMasks(HcalCaloFlagLabels::UserDefinedBit1,     mydef);
+  //else if (mybit == "UserDefinedBit0")      setAllRHMasks(HcalCaloFlagLabels::UserDefinedBit0,     mydef);
+  //else if (mybit == "UserDefinedBit1")      setAllRHMasks(HcalCaloFlagLabels::UserDefinedBit1,     mydef);
     
 
   // additional defined diagnostic bits; not currently used for rejection
