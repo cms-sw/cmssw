99a06df49a993ee43bd4823042bf88e90bd78162
Lukas <lukas.vanelderen@cern.ch>
2016-01-18 11:50:04 +0100

    fastsim: re-introduce hitless seeds in TrackCandidateProducer (cherry picked from commit 6d11e92c8de145a70b81f8ace8b3a97678eb164c)

---
+++ FastSimulation/Tracking/interface/SeedMatcher.h
@@ -0,0 +1,33 @@
+#ifndef SEEDMATCHER_H
+#define SEEDMATCHER_H
+
+#include "vector"
+#include "DataFormats/TrackerRecHit2D/interface/FastTrackerRecHitCollection.h"
+
+class TrajectorySeed;
+class Propagator;
+class MagneticField;
+class TrajectoryStateOnSurface;
+class SimTrack;
+class TrackerGeometry;
+
+class SeedMatcher
+{
+public:
+    static std::vector<int> matchRecHitCombinations(
+	const TrajectorySeed & seed,
+	const FastTrackerRecHitCombinationCollection & recHitCombinationCollection,
+	const std::vector<SimTrack> & simTrackCollection,
+	double maxMatchEstimator,
+	const Propagator & propagator,
+	const MagneticField & magneticField,
+	const TrackerGeometry & trackerGeometry) ;
+
+    static double matchSimTrack(
+	const TrajectoryStateOnSurface & seedState,
+	const SimTrack & simTrack,
+	const Propagator & propagator,
+	const MagneticField & magneticField);
+};
+
+#endif
