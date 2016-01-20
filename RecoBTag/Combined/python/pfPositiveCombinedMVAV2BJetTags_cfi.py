b17f7b493efb11e38cc71aa602bd6f7ba914ff17
Joosep Pata <joosep.pata@cern.ch>
2016-01-20 10:06:14 +0100

    fix positive taginfo names

--- RecoBTag/Combined/python/pfPositiveCombinedMVAV2BJetTags_cfi.py
+++ RecoBTag/Combined/python/pfPositiveCombinedMVAV2BJetTags_cfi.py
@@ -4,8 +4,8 @@ pfPositiveCombinedMVAV2BJetTags = cms.EDProducer("JetTagProducer",
 	jetTagComputer = cms.string('candidatePositiveCombinedMVAV2Computer'),
 	tagInfos = cms.VInputTag(
 		cms.InputTag("pfImpactParameterTagInfos"),
-		cms.InputTag("pfSecondaryVertexPositiveTagInfos"),
-		cms.InputTag("pfInclusiveSecondaryVertexFinderPositiveTagInfos"),
+		cms.InputTag("pfSecondaryVertexTagInfos"),
+		cms.InputTag("pfInclusiveSecondaryVertexFinderTagInfos"),
 		cms.InputTag("softPFMuonsTagInfos"),
 		cms.InputTag("softPFElectronsTagInfos")
 	)
