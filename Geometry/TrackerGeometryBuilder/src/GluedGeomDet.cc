9f29298ab1c53084308523db0fa263cad456912e
erica brondolin <erica.brondolin@cern.ch>
2016-01-20 10:41:11 +0100

    remove commented lines

--- Geometry/TrackerGeometryBuilder/src/GluedGeomDet.cc
+++ Geometry/TrackerGeometryBuilder/src/GluedGeomDet.cc
@@ -1,9 +1,7 @@
 #include "Geometry/TrackerGeometryBuilder/interface/GluedGeomDet.h"
-//#include "DataFormats/SiStripDetId/interface/StripSubdetector.h"
 
 GluedGeomDet::GluedGeomDet( BoundPlane* sp,const GeomDetUnit* monoDet, const GeomDetUnit* stereoDet, const DetId gluedDetId) : 
   TrackerGeomDet(sp),theMonoDet(monoDet),theStereoDet(stereoDet) {
-  //StripSubdetector subdet(theMonoDet->geographicalId().rawId());
   setDetId(gluedDetId);
 }
 
