f001f408d2209c7631dd97199cb2a0f1479afbf2
Anshul Kapoor <anshul.kapoor@students.iiserpune.ac.in>
2016-01-22 14:39:38 +0530

    Update InitialStep_cff.py(cherry picked from commit 1180fba398981d75f20cc547c08c1e6f2f7a63b1)

--- FastSimulation/Tracking/python/InitialStep_cff.py
+++ FastSimulation/Tracking/python/InitialStep_cff.py
@@ -21,7 +21,6 @@ initialStepTrackCandidates = FastSimulation.Tracking.TrackCandidateProducer_cfi.
 
 # tracks
 initialStepTracks = RecoTracker.IterativeTracking.InitialStep_cff.initialStepTracks.clone(
-    #Fitter = 'FlexibleKFFittingSmoother',
     TTRHBuilder = 'WithoutRefit',
     Propagator = 'PropagatorWithMaterial'
     )
