9aad07301326f49c19a4ca51b0f94043a34e0d66
Lukas <lukas.vanelderen@cern.ch>
2016-01-22 10:11:40 +0100

    clean up FastSimulation/HighLevelTrigger

--- FastSimulation/HighLevelTrigger/python/HLTFastRecoForJetMET_cff.py
+++
@@ -1,9 +0,0 @@
-import FWCore.ParameterSet.Config as cms
-
-# Provide dummy calonoise sequences
-import FastSimulation.HighLevelTrigger.DummyModule_cfi
-# LV: probably no need to replace this with a dummy
-HLTHBHENoiseCleanerSequence = FastSimulation.HighLevelTrigger.DummyModule_cfi.dummyModule.clone()
-
-
-
