1f41ef788f558482614e6f47459100fbb797501c
erica brondolin <erica.brondolin@cern.ch>
2016-01-26 13:58:59 +0100

    Fixing TOB DetId scheme (III) - reducing bits for modules

--- Geometry/TrackerNumberingBuilder/plugins/TrackerGeometricDetESModule.cc
+++ Geometry/TrackerNumberingBuilder/plugins/TrackerGeometricDetESModule.cc
@@ -102,10 +102,10 @@ TrackerGeometricDetESModule::fillDescriptions( edm::ConfigurationDescriptions & 
   phase2Det.push_back(23); phase2Det.push_back(-1); phase2Det.push_back(18);
   // level 1
   phase2Det.push_back(20); phase2Det.push_back(18); phase2Det.push_back(14); 
-  phase2Det.push_back(18); phase2Det.push_back(21); phase2Det.push_back(14);
+  phase2Det.push_back(18); phase2Det.push_back(20); phase2Det.push_back(14);
   // level 2
   phase2Det.push_back(12); phase2Det.push_back(10); phase2Det.push_back(4); 
-  phase2Det.push_back(10) ; phase2Det.push_back(12); phase2Det.push_back(8);
+  phase2Det.push_back(10) ; phase2Det.push_back(10); phase2Det.push_back(8);
   // level 3
   phase2Det.push_back(2) ; phase2Det.push_back(2) ; phase2Det.push_back(2); 
   phase2Det.push_back(2) ; phase2Det.push_back(2) ; phase2Det.push_back(5);
