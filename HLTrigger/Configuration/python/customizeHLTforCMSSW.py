e91ac4ba10c57072aad7d13a755978271d633ea9
Mia <mia.tosi@cern.ch>
2016-01-26 11:47:53 +0100

    fix merging issue

--- HLTrigger/Configuration/python/customizeHLTforCMSSW.py
+++ HLTrigger/Configuration/python/customizeHLTforCMSSW.py
@@ -11,25 +11,6 @@ def esproducers_by_type(process, *types):
 #
 # one action function per PR - put the PR number into the name of the function
 
-# Remove hcalTopologyConstants
-def customiseFor11920(process):
-    if hasattr(process,'HcalGeometryFromDBEP'):
-        if hasattr(process.HcalGeometryFromDBEP,'hcalTopologyConstants'):
-            delattr(process.HcalGeometryFromDBEP,'hcalTopologyConstants')
-    if hasattr(process,'HcalTopologyIdealEP'):
-        if hasattr(process.HcalTopologyIdealEP,'hcalTopologyConstants'):
-            delattr(process.HcalTopologyIdealEP,'hcalTopologyConstants')
-    if hasattr(process,'CaloTowerGeometryFromDBEP'):
-        if hasattr(process.CaloTowerGeometryFromDBEP,'hcalTopologyConstants'):
-            delattr(process.CaloTowerGeometryFromDBEP,'hcalTopologyConstants')
-    return process
-
-def customiseFor12346(process):
-    if hasattr(process, 'hltMetCleanUsingJetID'):
-       if hasattr(process.hltMetCleanUsingJetID, 'usePt'):
-           delattr(process.hltMetCleanUsingJetID, 'usePt')
-    return process
-
 def customiseFor12718(process):
     for pset in process._Process__psets.values():
         if hasattr(pset,'ComponentType'):
@@ -37,13 +18,6 @@ def customiseFor12718(process):
                 if not hasattr(pset,'minGoodStripCharge'):
                     pset.minGoodStripCharge = cms.PSet(refToPSet_ = cms.string('HLTSiStripClusterChargeCutNone'))
     return process
-
-def customiseFor12387(process):
-    for producer in producers_by_type(process, 'SiStripRawToDigiModule'):
-        if not hasattr(producer,'LegacyUnpacker'):
-            setattr(producer,'LegacyUnpacker',cms.bool(False))
-    return process
-
 
 def customiseFor13062(process):
     for module in producers_by_type(process,'PixelTrackProducer'):
@@ -85,10 +59,7 @@ def customiseHLTforCMSSW(process, menuType="GRun", fastSim=False):
     cmsswVersion = os.environ['CMSSW_VERSION']
 
     if cmsswVersion >= "CMSSW_8_0":
-        process = customiseFor12346(process)
-        process = customiseFor11920(process)
         process = customiseFor12718(process)
-        process = customiseFor12387(process)
         process = customiseFor13062(process)
 
     return process
