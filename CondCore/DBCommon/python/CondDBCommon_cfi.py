9737ee2d4b722e61a02b7da2c3781f4eb8df42e5
Giacomo Govi <giacomo.govi@cern.ch>
2016-01-28 14:14:38 +0100

    Backward compatibility fix: make CondCore/DBCommon/python/CondDBCommon_cfi.py and CondCore/DBCommon/python/CondDBSetup_cfi.py deprecated ( a message is printed out ) and point to CondCore/CondDB/python/CondDB_cfi.py.

---
+++ CondCore/DBCommon/python/CondDBCommon_cfi.py
@@ -0,0 +1,8 @@
+print " ################################################################### "  
+print " # WARNING: this module is deprecated.                             # "                                                                              
+print " # Please use CondCore.CondDB.CondDB_cfi.py                        # "                                                                             
+print " ################################################################### "
+
+from CondCore.CondDB.CondDB_cfi import *
+CondDBCommon = CondDB.clone()
+
