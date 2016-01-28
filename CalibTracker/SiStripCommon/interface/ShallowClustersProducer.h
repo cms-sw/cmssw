0d1b1ecad8dbd4e7014e475eafb9a1b42bf7c4e7
Alessandro Di Mattia <dimattia@cern.ch>
2016-01-28 11:28:00 +0100

    Fix in ShallowClusterProducer.h

--- CalibTracker/SiStripCommon/interface/ShallowClustersProducer.h
+++ CalibTracker/SiStripCommon/interface/ShallowClustersProducer.h
@@ -36,7 +36,7 @@ class ShallowClustersProducer : public edm::EDProducer {
     NearDigis(const SiStripClusterInfo&);
     NearDigis(const SiStripClusterInfo&, const edm::DetSetVector<SiStripProcessedRawDigi>&);
     float max, left, right, first, last, Lleft, Rright; 
-    float etaX() const {return (left+right)/max/2.;}
+    float etaX() const {return ((left+right)/max)/2.;}
     float eta()  const {return right>left ? max/(max+right) : left/(left+max);}
     float etaasymm() const {return right>left ? (right-max)/(right+max) : (max-left)/(max+left);}
     float outsideasymm() const {return (last-first)/(last+first);}
