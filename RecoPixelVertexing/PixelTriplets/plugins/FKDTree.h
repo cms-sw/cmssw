ddfaae13aa75dfa60a6de873b7ab01646a8a7506
felicepantaleo <felice.pantaleo@cern.ch>
2016-01-28 15:55:49 +0100

    A fast/parallelizable/iterative search kdtree

---
+++ RecoPixelVertexing/PixelTriplets/plugins/FKDTree.h
@@ -0,0 +1,15 @@
+/*
+ * FKDTree.h
+ *
+ *  Created on: Jan 28, 2016
+ *      Author: fpantale
+ */
+
+#ifndef FKDTREE_H_
+#define FKDTREE_H_
+
+
+
+
+
+#endif /* CMSSW_8_0_0_PRE4_SRC_RECOPIXELVERTEXING_PIXELTRIPLETS_PLUGINS_FKDTREE_H_ */
