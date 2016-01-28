c813c6d7c0f9d791ae4a6ed8ad1b106e53b96d1a
Petra <pvmulder@cern.ch>
2016-01-28 21:36:37 +0100

    real clean up of reco sequence and RecoBTag/SecondaryVertex

--- RecoBTag/SecondaryVertex/python/candidatePositiveCombinedSecondaryVertexComputer_cfi.py
+++
@@ -1,7 +0,0 @@
-import FWCore.ParameterSet.Config as cms
-
-from RecoBTag.SecondaryVertex.candidateCombinedSecondaryVertexComputer_cfi import *
-
-candidatePositiveCombinedSecondaryVertexComputer = candidateCombinedSecondaryVertexComputer.clone()
-candidatePositiveCombinedSecondaryVertexComputer.trackSelection.sip3dSigMin = 0
-candidatePositiveCombinedSecondaryVertexComputer.trackPseudoSelection.sip3dSigMin = 0
