a3e7f1b47ab280d107b01eeaf99c7aa95f295e5d
Joosep Pata <joosep.pata@cern.ch>
2016-01-29 08:26:49 +0100

    migrated computers (cherry picked from commit 478e98ff2eb0da270f9df4e44884a8019582a760)

--- RecoBTag/Combined/python/combinedMVA_EventSetup_cff.py
+++ RecoBTag/Combined/python/combinedMVA_EventSetup_cff.py
@@ -12,6 +12,10 @@ from RecoBTag.Combined.candidatePositiveCombinedMVAComputer_cfi import *
 # CombinedMVA V2
 from RecoBTag.Combined.combinedMVAV2Computer_cfi import *
 from RecoBTag.Combined.candidateCombinedMVAV2Computer_cfi import *
+from RecoBTag.Combined.negativeCombinedMVAV2Computer_cfi import *
+from RecoBTag.Combined.positiveCombinedMVAV2Computer_cfi import *
+from RecoBTag.Combined.candidateNegativeCombinedMVAV2Computer_cfi import *
+from RecoBTag.Combined.candidatePositiveCombinedMVAV2Computer_cfi import *
 
 # Charge tagger
 from RecoBTag.Combined.candidateChargeBTagComputer_cfi import *
