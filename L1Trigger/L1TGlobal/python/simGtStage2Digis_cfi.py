054661a1e5ecd0220509d407d3be615fd6c51fff
Michael Mulhearn <mulhearn@cern.ch>
2016-01-30 20:00:42 +0100

    fix and cleanup standard L1TGlobal python configuration

--- L1Trigger/L1TGlobal/python/simGtStage2Digis_cfi.py
+++ L1Trigger/L1TGlobal/python/simGtStage2Digis_cfi.py
@@ -1,3 +1,8 @@
+#
+# WARNING: This file is in the L1T configuration critical path.
+#
+# All changes must be explicitly discussed with the L1T offline coordinator.
+#
 import FWCore.ParameterSet.Config as cms
 
 # cfi uGT emulator
