6d63182ae0be0c3b5d3c210a7198dbff5d1ac1e5
Evangelos <evangelos.paradas@cern.ch>
2016-02-01 15:06:51 +0100

    all BMTF unpacker files are added and tested

---
+++ EventFilter/L1TRawToDigi/src/implementations_stage2/BMTFTokens.cc
@@ -0,0 +1,21 @@
+#include "FWCore/Framework/interface/ConsumesCollector.h"
+#include "FWCore/ParameterSet/interface/ParameterSet.h"
+#include "FWCore/Utilities/interface/InputTag.h"
+
+#include "BMTFTokens.h"
+
+namespace l1t {
+   namespace stage2 {
+      BMTFTokens::BMTFTokens(const edm::ParameterSet& cfg, edm::ConsumesCollector& cc) : PackerTokens(cfg, cc)
+      {
+         auto ouputTag = cfg.getParameter<edm::InputTag>("InputLabel");
+         auto inputTagPh = cfg.getParameter<edm::InputTag>("InputLabel");
+         auto inputTagTh = cfg.getParameter<edm::InputTag>("InputLabel");
+         
+				 outputMuonToken_ = cc.consumes<RegionalMuonCandBxCollection>(ouputTag);
+				 inputMuonTokenPh_ = cc.consumes<L1MuDTChambPhContainer>(inputTagPh);
+				 inputMuonTokenTh_ = cc.consumes<L1MuDTChambThContainer>(inputTagTh);
+         
+      }
+   }
+}
