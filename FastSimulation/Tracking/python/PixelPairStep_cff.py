fce600c5331802cb3378081d3ca13e11a535fedf
anshul <anshul.kapoor@students.iiserpune.ac.in>
2016-02-02 13:49:14 +0100

    Complete migration to fullsim fitters (cherry picked from commit d91975363326eefc8f14a87d16b4a0bc198b52cf)

--- FastSimulation/Tracking/python/PixelPairStep_cff.py
+++ FastSimulation/Tracking/python/PixelPairStep_cff.py
@@ -25,10 +25,7 @@ pixelPairStepTrackCandidates = FastSimulation.Tracking.TrackCandidateProducer_cf
 )
 
 # tracks
-pixelPairStepTracks = RecoTracker.IterativeTracking.PixelPairStep_cff.pixelPairStepTracks.clone(
-    TTRHBuilder = 'WithoutRefit',
-    Propagator = 'PropagatorWithMaterial'
-)
+pixelPairStepTracks = RecoTracker.IterativeTracking.PixelPairStep_cff.pixelPairStepTracks.clone()
 # final Selection
 pixelPairStep = RecoTracker.IterativeTracking.PixelPairStep_cff.pixelPairStep.clone()
 pixelPairStep.vertices = "firstStepPrimaryVerticesBeforeMixing"
