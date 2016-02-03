91b0a5388c7761881e23c1cd3a982bf591dd1236
Sridhara Dasu <dasu@hep.wisc.edu>
2016-02-03 19:21:11 +0100

    Added run number and dataset selection ability at run time to test script - thanks Nick Smith

--- L1Trigger/L1TCaloLayer1/test/testL1TCaloLayer1.py
+++ L1Trigger/L1TCaloLayer1/test/testL1TCaloLayer1.py
@@ -1,5 +1,35 @@
 import FWCore.ParameterSet.Config as cms
 
+import EventFilter.L1TCaloLayer1RawToDigi.util as util
+
+from FWCore.ParameterSet.VarParsing import VarParsing
+
+options = VarParsing()
+options.register('runNumber', 0, VarParsing.multiplicity.singleton, VarParsing.varType.int, 'Run to analyze')
+options.register('lumis', '1-max', VarParsing.multiplicity.singleton, VarParsing.varType.string, 'Lumis')
+options.register('dataStream', '/ExpressPhysics/Run2015D-Express-v4/FEVT', VarParsing.multiplicity.singleton, VarParsing.varType.string, 'Dataset to look for run in')
+options.register('inputFiles', [], VarParsing.multiplicity.list, VarParsing.varType.string, 'Manual file list input, will query DAS if empty')
+options.register('inputFileList', '', VarParsing.multiplicity.singleton, VarParsing.varType.string, 'Manual file list input, will query DAS if empty')
+options.register('useORCON', False, VarParsing.multiplicity.singleton, VarParsing.varType.bool, 'Use ORCON for conditions.  This is necessary for very recent runs where conditions have not propogated to Frontier')
+options.parseArguments()
+
+def formatLumis(lumistring, run) :
+    lumis = (lrange.split('-') for lrange in lumistring.split(','))
+    runlumis = (['%d:%s' % (run,lumi) for lumi in lrange] for lrange in lumis)
+    return ['-'.join(l) for l in runlumis]
+
+print 'Getting files for run %d...' % options.runNumber
+if len(options.inputFiles) is 0 and options.inputFileList is '' :
+    inputFiles = util.getFilesForRun(options.runNumber, options.dataStream)
+elif len(options.inputFileList) > 0 :
+    with open(options.inputFileList) as f :
+        inputFiles = list((line.strip() for line in f))
+else :
+    inputFiles = cms.untracked.vstring(options.inputFiles)
+if len(inputFiles) is 0 :
+    raise Exception('No files found for dataset %s run %d' % (options.dataStream, options.runNumber))
+print 'Ok, time to analyze'
+
 process = cms.Process("L1TCaloLayer1Test")
 
 process.load("FWCore.MessageService.MessageLogger_cfi")
@@ -8,10 +38,10 @@ process.load('L1Trigger.L1TCaloLayer1.layer1EmulatorDigis_cfi')
 process.layer1EmulatorDigis.useLUT = cms.bool(True)
 process.layer1EmulatorDigis.verbose = cms.bool(False)
 
-process.maxEvents = cms.untracked.PSet( input = cms.untracked.int32(1000) )
+process.maxEvents = cms.untracked.PSet( input = cms.untracked.int32(-1) )
 
 process.source = cms.Source("PoolSource",
-    fileNames = cms.untracked.vstring('file:/data/dasu/Sept25_905539F2-5363-E511-831A-02163E013687.root')
+    fileNames = cms.untracked.vstring(inputFiles)
 )
 
 process.out = cms.OutputModule("PoolOutputModule",
