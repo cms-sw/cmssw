09b29759e23751877095785a096424a85299634f
Benjamin <bmichlin@rice.edu>
2016-02-04 16:28:34 +0100

    Added EMTFBlockRPC.cc

--- EventFilter/L1TRawToDigi/src/implementations_stage2/EMTFSetup.cc
+++ EventFilter/L1TRawToDigi/src/implementations_stage2/EMTFSetup.cc
@@ -63,7 +63,7 @@ namespace l1t {
 	auto emtf_headers_unp   = UnpackerFactory::get()->make("stage2::emtf::HeadersBlockUnpacker");  // Unpack "AMC data header" and "Event Record Header"
 	auto emtf_counters_unp  = UnpackerFactory::get()->make("stage2::emtf::CountersBlockUnpacker"); // Unpack "Block of Counters"
 	auto emtf_me_unp       = UnpackerFactory::get()->make("stage2::emtf::MEBlockUnpacker");      // Unpack "ME Data Record"
-	// auto emtf_rpc_unp      = UnpackerFactory::get()->make("stage2::emtf::RPCBlockUnpacker");     // // Unpack "RPC Data Record"
+	auto emtf_rpc_unp      = UnpackerFactory::get()->make("stage2::emtf::RPCBlockUnpacker");     // // Unpack "RPC Data Record"
 	auto emtf_sp_unp       = UnpackerFactory::get()->make("stage2::emtf::SPBlockUnpacker");      // Unpack "SP Output Data Record"
 	auto emtf_trailers_unp  = UnpackerFactory::get()->make("stage2::emtf::TrailersBlockUnpacker"); // Unpack "Event Record Trailer"
 	
@@ -71,7 +71,7 @@ namespace l1t {
 	res[511] = emtf_headers_unp;
 	res[2]   = emtf_counters_unp;
 	res[3]   = emtf_me_unp;
-	// res[4]   = emtf_rpc_unp;
+	 res[4]   = emtf_rpc_unp;
 	res[101] = emtf_sp_unp;
 	res[255] = emtf_trailers_unp;
 	
