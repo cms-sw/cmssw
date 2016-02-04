7773241fdad9728995c36453903454cf436abd03
Vincenzo Innocente <vincenzo.innocente@cern.ch>
2016-02-04 14:46:35 +0100

    use a case, not runtime ifs

--- RecoLocalTracker/SiStripRecHitConverter/src/StripCPEfromTrackAngle.cc
+++ RecoLocalTracker/SiStripRecHitConverter/src/StripCPEfromTrackAngle.cc
@@ -4,6 +4,32 @@
 
 #include "vdt/vdtMath.h"
 
+StripCPEfromTrackAngle::StripCPEfromTrackAngle( edm::ParameterSet & conf,
+                          const MagneticField& mag,
+                          const TrackerGeometry& geom,
+                          const SiStripLorentzAngle& lorentz,
+                          const SiStripBackPlaneCorrection& backPlaneCorrection,
+                          const SiStripConfObject& confObj,
+                          const SiStripLatency& latency)
+  : StripCPE(conf, mag, geom, lorentz, backPlaneCorrection, confObj, latency )
+  , useLegacyError(conf.existsAs<bool>("useLegacyError") ? conf.getParameter<bool>("useLegacyError") : true)
+  , maxChgOneMIP(conf.existsAs<float>("maxChgOneMIP") ? conf.getParameter<double>("maxChgOneMIP") : -6000.),
+    m_algo(useLegacyError ? Algo::legacy : ( maxChgOneMIP<0 ? Algo::mergeCK : Algo::chargeCK))
+  {
+    mLC_P[0] = conf.existsAs<double>("mLC_P0") ? conf.getParameter<double>("mLC_P0") : -.326;
+    mLC_P[1] = conf.existsAs<double>("mLC_P1") ? conf.getParameter<double>("mLC_P1") :  .618;
+    mLC_P[2] = conf.existsAs<double>("mLC_P2") ? conf.getParameter<double>("mLC_P2") :  .300;
+
+    mHC_P[SiStripDetId::TIB - 3][0] = conf.existsAs<double>("mTIB_P0") ? conf.getParameter<double>("mTIB_P0") : -.742  ;
+    mHC_P[SiStripDetId::TIB - 3][1] = conf.existsAs<double>("mTIB_P1") ? conf.getParameter<double>("mTIB_P1") :  .202  ;
+    mHC_P[SiStripDetId::TID - 3][0] = conf.existsAs<double>("mTID_P0") ? conf.getParameter<double>("mTID_P0") : -1.026 ;
+    mHC_P[SiStripDetId::TID - 3][1] = conf.existsAs<double>("mTID_P1") ? conf.getParameter<double>("mTID_P1") :  .253  ;
+    mHC_P[SiStripDetId::TOB - 3][0] = conf.existsAs<double>("mTOB_P0") ? conf.getParameter<double>("mTOB_P0") : -1.427 ;
+    mHC_P[SiStripDetId::TOB - 3][1] = conf.existsAs<double>("mTOB_P1") ? conf.getParameter<double>("mTOB_P1") :  .433  ;
+    mHC_P[SiStripDetId::TEC - 3][0] = conf.existsAs<double>("mTEC_P0") ? conf.getParameter<double>("mTEC_P0") : -1.885 ;
+    mHC_P[SiStripDetId::TEC - 3][1] = conf.existsAs<double>("mTEC_P1") ? conf.getParameter<double>("mTEC_P1") :  .471  ;
+  }
+
 float StripCPEfromTrackAngle::stripErrorSquared(const unsigned N, const float uProj, const SiStripDetId::SubDetector loc ) const {
   if( loc == SiStripDetId::UNKNOWN)
     throw cms::Exception("StripCPEfromTrackAngle::stripErrorSquared", "Incompatible sub-detector.");
@@ -25,8 +51,8 @@ float StripCPEfromTrackAngle::legacyStripErrorSquared(const unsigned N, const fl
   }
 }
 
-StripClusterParameterEstimator::LocalValues StripCPEfromTrackAngle::
-localParameters( const SiStripCluster& cluster, const GeomDetUnit& det, const LocalTrajectoryParameters& ltp) const {
+StripClusterParameterEstimator::LocalValues 
+StripCPEfromTrackAngle::localParameters( const SiStripCluster& cluster, const GeomDetUnit& det, const LocalTrajectoryParameters& ltp) const {
   
   StripCPE::Param const & p = param(det);
   SiStripDetId ssdid = SiStripDetId( det.geographicalId() );  
@@ -39,17 +65,23 @@ localParameters( const SiStripCluster& cluster, const GeomDetUnit& det, const Lo
 
   const unsigned N = cluster.amplitudes().size();
   const float fullProjection = p.coveredStrips( track+p.drift, ltp.position());
-  float uerr2;
-  if unlikely(useLegacyError) {
-    uerr2 = legacyStripErrorSquared(N,std::abs(fullProjection));
-  } else {
-    if unlikely(maxChgOneMIP < 0.0) {
-      uerr2 = cluster.isMerged() ? legacyStripErrorSquared(N,std::abs(fullProjection)) : stripErrorSquared( N, std::abs(fullProjection),ssdid.subDetector() );
-     } else {
-       float dQdx = siStripClusterTools::chargePerCM(ssdid, cluster, ltp);
+  float uerr2=0;
+
+  switch (m_algo) {
+    case Algo::chargeCK :
+       {
+       auto dQdx = siStripClusterTools::chargePerCM(ssdid, cluster, ltp);
        uerr2 = dQdx > maxChgOneMIP ? legacyStripErrorSquared(N,std::abs(fullProjection)) : stripErrorSquared( N, std::abs(fullProjection),ssdid.subDetector() );
-    }
+       }
+       break;
+    case Algo::legacy :
+       uerr2 = legacyStripErrorSquared(N,std::abs(fullProjection));
+       break;
+    case Algo::mergeCK :
+      uerr2 = cluster.isMerged() ? legacyStripErrorSquared(N,std::abs(fullProjection)) : stripErrorSquared( N, std::abs(fullProjection),ssdid.subDetector() );
+      break;
   }
+
   const float strip = cluster.barycenter() -  0.5f*(1.f-p.backplanecorrection) * fullProjection
     + 0.5f*p.coveredStrips(track, ltp.position());
 
