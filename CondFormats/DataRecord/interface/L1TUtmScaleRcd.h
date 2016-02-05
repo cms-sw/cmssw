f2c488da78898591618f7a559cdbe70989d7a4f9
Michael Mulhearn <mulhearn@cern.ch>
2016-02-05 15:30:08 +0100

    add records and CondCore updates for Global CondFormats

---
+++ CondFormats/DataRecord/interface/L1TUtmScaleRcd.h
@@ -0,0 +1,19 @@
+// L1TUtmScaleRcd                                                                                            
+// Description: Record for L1TUtmScale
+//
+// automatically generate by make_records.pl
+//
+#ifndef CondFormatsDataRecord_L1TUtmScale_h
+#define CondFormatsDataRecord_L1TUtmScale_h
+
+#include "FWCore/Framework/interface/EventSetupRecordImplementation.h"
+
+class L1TUtmScaleRcd : public edm::eventsetup::EventSetupRecordImplementation<L1TUtmScaleRcd> {};
+
+// Dependent record implmentation:
+//#include "FWCore/Framework/interface/DependentRecordImplementation.h"
+//#include "CondFormats/DataRecord/interface/L1TriggerKeyListRcd.h"
+//#include "CondFormats/DataRecord/interface/L1TriggerKeyRcd.h"
+//class L1TUtmScaleRcd : public edm::eventsetup::DependentRecordImplementation<L1TUtmScaleRcd, boost::mpl::vector<L1TriggerKeyListRcd,L1TriggerKeyRcd> > {};
+
+#endif
