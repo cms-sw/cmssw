5d906cb32e04c3dca1a154577631ffee2f320c17
Vladimir Rekovic <vladimir.rekovic@cern.ch>
2016-02-06 23:38:55 +0100

    Overload size() to return size of the part of vector relevant to BX = 0. Add array subscript operator, always relevant to BX = 0

--- DataFormats/L1Trigger/interface/BXVector.h
+++ DataFormats/L1Trigger/interface/BXVector.h
@@ -66,6 +66,9 @@ class BXVector  {
   // get N objects for a given BX
   unsigned size( int bx ) const;
 
+  // get N objects for a BX = 0
+  unsigned size( ) const { return size(0);}
+
   // add element with given BX index
   void push_back( int bx, T object );
  
@@ -95,6 +98,10 @@ class BXVector  {
   const_iterator end() const {return data_.end(); }
   //int bx(const_iterator & iter) const; (potentially useful)
   unsigned int key(const_iterator & iter) const { return iter - begin(); }
+
+  // array subscript operator, always referring to objects for BX=0
+  T& operator[](std::size_t i) { return at(0,i); }
+  const T& operator[](std::size_t i) const { return at(0,i); }
 
 
  private:
