3bdb66e67e558ea156ba255937dc74cff9750f31
Leonard Apanasevich <apana@fnal.gov>
2016-02-09 17:52:45 +0100

    Fix default for muonToken

--- L1Trigger/L1TNtuples/python/l1UpgradeTree_cfi.py
+++ L1Trigger/L1TNtuples/python/l1UpgradeTree_cfi.py
@@ -5,7 +5,7 @@ l1UpgradeTree = cms.EDAnalyzer(
     egToken = cms.untracked.InputTag("caloStage2Digis"),
     tauTokens = cms.untracked.VInputTag("caloStage2Digis"),
     jetToken = cms.untracked.InputTag("caloStage2Digis"),
-    muonToken = cms.untracked.InputTag("caloStage2Digis"),
+    muonToken = cms.untracked.InputTag("gmtStage2Digis"),
     sumToken = cms.untracked.InputTag("caloStage2Digis"),
     maxL1Upgrade = cms.uint32(60)
 )
