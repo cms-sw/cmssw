aa77a655c29cf1e1f6b9674ec71b830e28472c28
Christophe Delaere <christophe.delaere@cern.ch>
2016-02-10 18:07:50 +0100

    step 1: Dataformat and L1Trigger code
    at this stage, there is an error when building the dataformat library.
    In the L1Trigger code, some stub algos are commented out.
    In the L1Trigger tests, much is to be done (to be rewritten most probably).

---
+++ L1Trigger/TrackTrigger/interface/classNameFinder.h
@@ -0,0 +1,35 @@
+#ifndef classNameFinder_h
+#define classNameFinder_h
+
+#include <string>
+#include <typeinfo>
+#include <cxxabi.h>
+
+template< class T >
+std::string classNameFinder( std::string fName )
+{
+  int status2 = 0;
+  char *dm2 = abi::__cxa_demangle( typeid(T).name(), 0, 0, &status2 );
+  std::string s2 = "failed demangle";
+  if ( status2 == 0 )
+  {
+    s2 = std::string(dm2);
+  }
+  return ( fName+std::string("< ") + s2 + std::string(" >") );
+}
+
+template< class T >
+std::string templateNameFinder()
+{
+  int status2 = 0;
+  char *dm2 = abi::__cxa_demangle( typeid(T).name(), 0, 0, &status2 );
+  std::string s2 = "failed demangle";
+  if ( status2 == 0 )
+  {
+    s2 = std::string(dm2);
+  }
+  return s2;
+}
+
+#endif
+
