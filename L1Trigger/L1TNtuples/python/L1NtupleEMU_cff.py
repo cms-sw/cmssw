58ec0c6bd457caab04a6213ea0d2e11c75253a40
Leonard Apanasevich <apana@fnal.gov>
2016-02-11 13:14:49 +0100

    Fix error in stage1 collection names; update TP Token names

--- L1Trigger/L1TNtuples/python/L1NtupleEMU_cff.py
+++ L1Trigger/L1TNtuples/python/L1NtupleEMU_cff.py
@@ -9,8 +9,8 @@ l1CaloTowerEmuTree = l1CaloTowerTree.clone()
 ## l1CaloTowerEmuTree.hcalToken = cms.untracked.InputTag("none")
 
 ## used unpacked digis for now
-l1CaloTowerEmuTree.ecalToken = cms.untracked.InputTag("ecalDigis","EcalTriggerPrimitives")
-l1CaloTowerEmuTree.hcalToken = cms.untracked.InputTag("hcalDigis")
+l1CaloTowerEmuTree.ecalToken = cms.untracked.InputTag("ecalDigis:EcalTriggerPrimitives")
+l1CaloTowerEmuTree.hcalToken = cms.untracked.InputTag("simHcalTriggerPrimitiveDigis")
 
 l1CaloTowerEmuTree.l1TowerToken = cms.untracked.InputTag("simCaloStage2Layer1Digis")
 l1CaloTowerEmuTree.l1ClusterToken = cms.untracked.InputTag("simCaloStage2Digis", "MP")
@@ -23,11 +23,11 @@ l1UpgradeEmuTree.muonToken = cms.untracked.InputTag("simGmtStage2Digis")
 l1UpgradeEmuTree.sumToken = cms.untracked.InputTag("simCaloStage2Digis")
 
 if eras.stage1L1Trigger.isChosen() or eras.Run2_25ns.isChosen():
-    l1UpgradeEMUTree.egToken = "simCStage1FinalDigis"
-    l1UpgradeEMUTree.tauTokens = cms.untracked.VInputTag("simCStage1FinalDigis:rlxTaus")
-    l1UpgradeEMUTree.jetToken = "simCStage1FinalDigis"
+    l1UpgradeEMUTree.egToken = "simCaloStage1FinalDigis"
+    l1UpgradeEMUTree.tauTokens = cms.untracked.VInputTag("simCaloStage1FinalDigis:rlxTaus")
+    l1UpgradeEMUTree.jetToken = "simCaloStage1FinalDigis"
     l1UpgradeEMUTree.muonToken = "simGtDigis"
-    l1UpgradeEMUTree.sumToken = "simCStage1FinalDigis"
+    l1UpgradeEMUTree.sumToken = "simCaloStage1FinalDigis"
 
 L1NtupleEMU = cms.Sequence(
   l1CaloTowerEmuTree
