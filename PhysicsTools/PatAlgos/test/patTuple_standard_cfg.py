231e24b52652bc3fdb141edb95661a730652cff8
Roger <roger.wolf@cern.ch>
2016-02-13 12:01:58 +0100

    referting changes in PhysicsTools packages

--- PhysicsTools/PatAlgos/test/patTuple_standard_cfg.py
+++ PhysicsTools/PatAlgos/test/patTuple_standard_cfg.py
@@ -4,8 +4,6 @@ from PhysicsTools.PatAlgos.patTemplate_cfg import *
 process.options.allowUnscheduled = cms.untracked.bool(True)
 #process.Tracer = cms.Service("Tracer")
 
-process.load("RecoTauTag.Configuration.RecoPFTauTag_cff")
-process.load("RecoTauTag.Configuration.HPSPFTaus_cff")
 process.load("PhysicsTools.PatAlgos.producersLayer1.patCandidates_cff")
 process.load("PhysicsTools.PatAlgos.selectionLayer1.selectedPatCandidates_cff")
 
@@ -16,15 +14,13 @@ process.load("PhysicsTools.PatAlgos.selectionLayer1.selectedPatCandidates_cff")
 #
 #   process.GlobalTag.globaltag =  ...    ##  (according to https://twiki.cern.ch/twiki/bin/view/CMS/SWGuideFrontierConditions)
 #                                         ##
-from PhysicsTools.PatAlgos.patInputFiles_cff import filesRelValProdTTbarAODSIM, filesRelValTTbarGENSIMRECO
-process.source.fileNames = filesRelValTTbarGENSIMRECO
+from PhysicsTools.PatAlgos.patInputFiles_cff import filesRelValProdTTbarAODSIM
+process.source.fileNames = filesRelValProdTTbarAODSIM
 #                                         ##
 process.maxEvents.input = 100
 #                                         ##
 #   process.out.outputCommands = [ ... ]  ##  (e.g. taken from PhysicsTools/PatAlgos/python/patEventContent_cff.py)
 #                                         ##
 process.out.fileName = 'patTuple_standard.root'
-from  PhysicsTools.PatAlgos.patEventContent_cff import patExtraTauPOGRECOEventContent
-process.out.outputCommands += patExtraTauPOGRECOEventContent
 #                                         ##
-#   process.options.wantSummary = False   ##  (to suppress the long output at fthe end of the job)
+#   process.options.wantSummary = False   ##  (to suppress the long output at the end of the job)
