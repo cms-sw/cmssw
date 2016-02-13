2f7d66bd20b33e2fce9312fe171308d470ea44fa
Andrew Evans <aevans@physics.umn.edu>
2016-02-13 07:39:29 -0600

    More bug fixes

--- DataFormats/HcalDigi/src/HcalUHTRhistogramDigiCollection.cc
+++ DataFormats/HcalDigi/src/HcalUHTRhistogramDigiCollection.cc
@@ -1,5 +1,6 @@
 #include "DataFormats/HcalDigi/interface/HcalUHTRhistogramDigiCollection.h"
 #include <iomanip>
+#include <iostream>
 #include <algorithm>
 
 const uint32_t HcalUHTRhistogramDigiCollection::get(int capid, int bin, size_t index) const {
@@ -14,16 +15,17 @@ const int HcalUHTRhistogramDigiCollection::getSum(int bin, size_t index) const {
 }
 
 void HcalUHTRhistogramDigiCollection::fillBin(int capid, int bin, uint32_t val, size_t index) {
-  if ( abs(index) < ids_.size() )
+  if ( index < ids_.size() )
     bins_[(separateCapIds_*3+1)*binsPerHistogram_*index+capid*binsPerHistogram_+bin] = val;
 }
 const HcalDetId& HcalUHTRhistogramDigiCollection::id(size_t index) const{
   return ids_[index];
 }
 const size_t HcalUHTRhistogramDigiCollection::find(HcalDetId id) const{
+   std::cout << "Looking for: "<< id << std::endl;
    std::vector<HcalDetId>::const_iterator iter = std::find(ids_.begin(), ids_.end(), id);
    if (iter == ids_.end()) return INVALID;
-   else return *iter;
+   else return std::distance(ids_.begin() ,iter);
 }
 
 const HcalUHTRhistogramDigi HcalUHTRhistogramDigiCollection::at(size_t index) const {
@@ -36,12 +38,16 @@ const HcalUHTRhistogramDigi HcalUHTRhistogramDigiCollection::operator[](size_t i
 }
 HcalUHTRhistogramDigiMutable HcalUHTRhistogramDigiCollection::addHistogram(const HcalDetId& id) {
   size_t index = find(id);
-  if ( index < ids_.size() )
+  if ( index < ids_.size() ) {
+    std::cout << "Found id: " << id << " at: " << index << ":" << ids_[index] << std::endl;
     return HcalUHTRhistogramDigiMutable(index, *this );
+  }
   ids_.push_back(id);
   std::vector<uint32_t> res ((separateCapIds_*3+1)*binsPerHistogram_, 0);
   bins_.reserve( res.size() + bins_.size() );
   bins_.insert( bins_.end(), res.begin(), res.end() );
+  std::cout << "Did not find id " << id << std::endl;
+  std::cout << "#ids " << ids_.size() << std::endl;
   return HcalUHTRhistogramDigiMutable( ids_.size()-1, *this );
 }
 HcalUHTRhistogramDigiCollection::HcalUHTRhistogramDigiCollection() {}
--- RecoTBCalo/HcalTBObjectUnpacker/src/HcalSourcingUTCAunpacker.cc
+++ RecoTBCalo/HcalTBObjectUnpacker/src/HcalSourcingUTCAunpacker.cc
@@ -19,16 +19,19 @@ struct eventHeader {
 
   
 void HcalSourcingUTCAunpacker::unpack(const FEDRawData&  raw, const HcalElectronicsMap emap, std::auto_ptr<HcalUHTRhistogramDigiCollection>& histoDigiCollection) const {
-  std::cout << "Unpacker Time!" << std::endl; 
+  bool DEBUG = false;
+  if(DEBUG) std::cout << "Unpacker Time!" << std::endl; 
+
   if (raw.size()<4*38) {
 //    throw cms::Exception("Missing Data") << "Less than 1 histogram in event";
     std::cout << "Warning: raw size in bytes: " << raw.size() << std::endl;
   }
   const uint32_t* pData = (const uint32_t*) raw.data(); 
+  if(DEBUG) {
   int nwords=raw.size()/4;
   for (int iw=0; iw<nwords; iw++)
     printf("%04d %04x\n",iw,pData[iw]);
-  
+  }
   const struct eventHeader* eh =
     (const struct eventHeader*)(raw.data());
   
@@ -37,13 +40,13 @@ void HcalSourcingUTCAunpacker::unpack(const FEDRawData&  raw, const HcalElectron
 //  }
 //Read event header
   int numHistos  = ((eh->h3)>>16)&0xFFFF;
-  std::cout << "Number of Histograms: " << numHistos << std::endl;
+  if(DEBUG) std::cout << "Number of Histograms: " << numHistos << std::endl;
   int numBins    = ((eh->h3)>>1)&0x0000FFFE; //includes overflow and header word
-  std::cout << "Bins per Histogram: " << numBins << std::endl;
+  if(DEBUG) std::cout << "Bins per Histogram: " << numBins << std::endl;
   bool sepCapIds = eh->h3&0x00000001;
-  std::cout << "Separate CapIds: " << sepCapIds << std::endl;
+  if(DEBUG) std::cout << "Separate CapIds: " << sepCapIds << std::endl;
 
-  histoDigiCollection.reset(new HcalUHTRhistogramDigiCollection(numBins, sepCapIds));
+  histoDigiCollection.reset(new HcalUHTRhistogramDigiCollection(numBins+1, sepCapIds));
 //Set histogram word pointer to first histogram    
   int crate   = -1;
   int slot    = -1;
@@ -53,19 +56,19 @@ void HcalSourcingUTCAunpacker::unpack(const FEDRawData&  raw, const HcalElectron
 //Loop over data
   pData+=8;
   for (int iHist = 0; iHist<numHistos; iHist++) {
-    std::cout << "Histogram: " << iHist << std::endl;
+    if(DEBUG) std::cout << "Histogram: " << iHist << std::endl;
     pData+=(numBins+2);
     crate   = ((*pData)>>16)&0x00FF;
-    std::cout << "Crate: " << crate << std::endl;
+    if(DEBUG) std::cout << "Crate: " << crate << std::endl;
     slot    = ((*pData)>>12)&0x0000F;
-    std::cout << "Slot: " << slot << std::endl;
+    if(DEBUG) std::cout << "Slot: " << slot << std::endl;
     fiber   = (*pData&0x00000F80)>>7;
-    std::cout << "Fiber: " << fiber << std::endl;
+    if(DEBUG) std::cout << "Fiber: " << fiber << std::endl;
     channel = (*pData&0x0000007C)>>2;
-    std::cout << "Channel: " << channel << std::endl;
+    if(DEBUG) std::cout << "Channel: " << channel << std::endl;
     cap     = *pData&0x00000003;
-    std::cout << "Histo: " << iHist << std::endl;
-    std::cout << "CapId: " << cap << std::endl;
+    if(DEBUG) std::cout << "Histo: " << iHist << std::endl;
+    if(DEBUG) std::cout << "CapId: " << cap << std::endl;
     HcalElectronicsId eid(crate, slot, fiber, channel, false);
   //  eid.setHTR(htr_cr,htr_slot,htr_tb);
     DetId did=emap.lookup(eid);
@@ -75,14 +78,14 @@ void HcalSourcingUTCAunpacker::unpack(const FEDRawData&  raw, const HcalElectron
       }
       continue;
     }
-    std::cout << "Det Id: " << ((HcalDetId)did) << std::endl;
-    HcalUHTRhistogramDigiMutable digi = histoDigiCollection->addHistogram( did );
+    if(DEBUG) std::cout << "Det Id: " << ((HcalDetId)did) << std::endl;
+    HcalUHTRhistogramDigiMutable digi = histoDigiCollection->addHistogram( (HcalDetId)did );
     for(int iBin = 0; iBin<numBins+1; iBin++) {
       digi.fillBin(cap, iBin, pData[iBin+1]);
-      std::cout << "CapId: " << cap << "Bin: " << iBin << "Val: " << pData[iBin+1] << std::endl;
+      if(DEBUG) std::cout << "CapId: " << cap << "Bin: " << iBin << "Val: " << pData[iBin+1] << std::endl;
         
     }
 
   }
-  std::cout << "DONE" << std::endl;
+  if(DEBUG) std::cout << "DONE" << std::endl;
 }
