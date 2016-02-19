725160e178d1beb06912f9749409752f692ae537
DirtyDan88 <Max.stark88@web.de>
2016-02-19 14:24:48 +0100

    PCL-DQM-plots: implemented mp-file reader and DQMEDAnalyzer for histo booking and filling

---
+++ Alignment/MillePedeAlignmentAlgorithm/plugins/MillePedeDQMModule.cc
@@ -0,0 +1,158 @@
+/**
+ * @package   Alignment/MillePedeAlignmentAlgorithm
+ * @file      MillePedeDQMModule.cc
+ *
+ * @author    Max Stark (max.stark@cern.ch)
+ * @date      Feb 19, 2016
+ */
+
+
+/*** header-file ***/
+#include "Alignment/MillePedeAlignmentAlgorithm/plugins/MillePedeDQMModule.h"
+
+/*** ROOT objects ***/
+#include "TCanvas.h"
+#include "TH1F.h"
+
+
+
+MillePedeDQMModule
+::MillePedeDQMModule(const edm::ParameterSet& config) :
+  mpReaderConfig_(
+    config.getParameter<edm::ParameterSet>("millePedeFileReaderConfig")
+  ),
+  mpReader(mpReaderConfig_),
+
+  Xcut_       (config.getParameter<double>("Xcut")),
+  tXcut_      (config.getParameter<double>("tXcut")),
+  Ycut_       (config.getParameter<double>("Ycut")),
+  tYcut_      (config.getParameter<double>("tYcut")),
+  Zcut_       (config.getParameter<double>("Zcut")),
+  tZcut_      (config.getParameter<double>("tZcut")),
+  maxMoveCut_ (mpReaderConfig_.getParameter<double>("maxMoveCut")),
+  sigCut_     (mpReaderConfig_.getParameter<double>("sigCut"))
+{
+}
+
+MillePedeDQMModule
+::~MillePedeDQMModule()
+{
+}
+
+void MillePedeDQMModule
+::fillDescriptions(edm::ConfigurationDescriptions& descriptions) {
+  edm::ParameterSetDescription desc;
+
+  desc.add<std::string>("millePedeLogFile", "")->setComment(
+    "The .log-file of the MillePede alignment algorithm (pede-mode)"
+  );
+  desc.add<std::string>("millePedeResFile", "")->setComment(
+    "The .res-file of the MillePede alignment algorithm (pede-mode)"
+  );
+
+  descriptions.add("MillePedeDQMModule", desc);
+  descriptions.setComment("Generic .cfi-file for the MillePedeDQMModule");
+}
+
+
+
+//=============================================================================
+//===   INTERFACE IMPLEMENTATION                                            ===
+//=============================================================================
+
+void MillePedeDQMModule
+::bookHistograms(DQMStore::IBooker& booker,
+                 edm::Run const& /* run */,
+                 edm::EventSetup const& /* setup */)
+{
+  edm::LogInfo("MillePedeDQMModule") << "Booking histograms";
+
+  booker.cd();
+  booker.setCurrentFolder("AlCaReco/SiPixelAli/");
+
+  h_xPos[0] = booker.book1D("Xpos",   "#Delta X;;#mu m", 6, 0, 6.);
+  h_xPos[1] = booker.book1D("Xpos_1", "Xpos_1",          6, 0, 6.);
+  h_xPos[2] = booker.book1D("Xpos_2", "Xpos_2",          6, 0, 6.);
+  h_xPos[3] = booker.book1D("Xpos_3", "Xpos_3",          6, 0, 6.);
+
+  h_xRot[0] = booker.book1D("Xrot",   "#Delta #theta_{X};;#mu rad", 6, 0, 6.);
+  h_xRot[1] = booker.book1D("Xrot_1", "Xrot_1",                     6, 0, 6.);
+  h_xRot[2] = booker.book1D("Xrot_2", "Xrot_2",                     6, 0, 6.);
+  h_xRot[3] = booker.book1D("Xrot_3", "Xrot_3",                     6, 0, 6.);
+
+  h_yPos[0] = booker.book1D("Ypos",   "#Delta Y;;#mu m", 6, 0., 6.);
+  h_yPos[1] = booker.book1D("Ypos_1", "Ypos_1",          6, 0., 6.);
+  h_yPos[2] = booker.book1D("Ypos_2", "Ypos_2",          6, 0., 6.);
+  h_yPos[3] = booker.book1D("Ypos_3", "Ypos_3",          6, 0., 6.);
+
+  h_yRot[0] = booker.book1D("Yrot",   "#Delta #theta_{Y};;#mu rad", 6, 0, 6.);
+  h_yRot[1] = booker.book1D("Yrot_1", "Yrot_1",                     6, 0, 6.);
+  h_yRot[2] = booker.book1D("Yrot_2", "Yrot_2",                     6, 0, 6.);
+  h_yRot[3] = booker.book1D("Yrot_3", "Yrot_3",                     6, 0, 6.);
+
+  h_zPos[0] = booker.book1D("Zpos",   "#Delta Z;;#mu m", 6, 0., 6.);
+  h_zPos[1] = booker.book1D("Zpos_1", "Zpos_1",          6, 0., 6.);
+  h_zPos[2] = booker.book1D("Zpos_2", "Zpos_2",          6, 0., 6.);
+  h_zPos[3] = booker.book1D("Zpos_3", "Zpos_3",          6, 0., 6.);
+
+  h_zRot[0] = booker.book1D("Zrot",   "#Delta #theta_{Z};;#mu rad", 6, 0, 6.);
+  h_zRot[1] = booker.book1D("Zrot_1", "Zrot_1",                     6, 0, 6.);
+  h_zRot[2] = booker.book1D("Zrot_2", "Zrot_2",                     6, 0, 6.);
+  h_zRot[3] = booker.book1D("Zrot_3", "Zrot_3",                     6, 0, 6.);
+
+  booker.cd();
+}
+
+void MillePedeDQMModule
+::endRun(edm::Run const& /* run */, edm::EventSetup const& /* setup */)
+{
+  mpReader.read();
+  fillExpertHistos();
+}
+
+
+
+//=============================================================================
+//===   PRIVATE METHOD IMPLEMENTATION                                       ===
+//=============================================================================
+
+void MillePedeDQMModule
+::fillExpertHistos()
+{
+  TCanvas c("PCL_SiPixAl_Expert", "PCL_SiPixAl_Expert", 1500, 800);
+  c.Divide(3, 2);
+
+  c.cd(1); fillExpertHisto(h_xPos,  Xcut_, mpReader.getXobs(),  mpReader.getXobsErr());
+  c.cd(4); fillExpertHisto(h_xRot, tXcut_, mpReader.getTXobs(), mpReader.getTXobsErr());
+
+  c.cd(2); fillExpertHisto(h_yPos,  Ycut_, mpReader.getYobs(),  mpReader.getYobsErr());
+  c.cd(5); fillExpertHisto(h_yRot, tYcut_, mpReader.getTYobs(), mpReader.getTYobsErr());
+
+  c.cd(3); fillExpertHisto(h_zPos,  Zcut_, mpReader.getZobs(),  mpReader.getZobsErr());
+  c.cd(6); fillExpertHisto(h_zRot, tZcut_, mpReader.getTZobs(), mpReader.getTZobsErr());
+
+  c.Write();
+}
+
+void MillePedeDQMModule
+::fillExpertHisto(MonitorElement* histos[], const double cut,
+                  std::array<double, 6> obs, std::array<double, 6> obsErr)
+{
+  TH1F* histo_0 = histos[0]->getTH1F();
+  TH1F* histo_1 = histos[1]->getTH1F();
+  TH1F* histo_2 = histos[2]->getTH1F();
+  TH1F* histo_3 = histos[3]->getTH1F();
+
+  histo_0->SetMinimum(-(maxMoveCut_));
+  histo_0->SetMaximum(  maxMoveCut_);
+
+  for (size_t i = 0; i < obs.size(); ++i) {
+    histo_0->SetBinContent(i+1, obs[i]);
+    histo_1->SetBinContent(i+1, obs[i]);
+    histo_2->SetBinContent(i+1,  cut);
+    histo_3->SetBinContent(i+1, -cut);
+
+    histo_0->SetBinError(i+1, obsErr[i]);
+    histo_1->SetBinError(i+1, (histo_1->GetBinContent(i+1) / sigCut_));
+  }
+}
---
+++ Alignment/MillePedeAlignmentAlgorithm/plugins/MillePedeDQMModule.h
@@ -0,0 +1,94 @@
+#ifndef Alignment_MillePedeAlignmentAlgorithm_MillePedeDQMModule_h
+#define Alignment_MillePedeAlignmentAlgorithm_MillePedeDQMModule_h
+
+/**
+ * @package   Alignment/MillePedeAlignmentAlgorithm
+ * @file      MillePedeDQMModule.h
+ *
+ * @author    Max Stark (max.stark@cern.ch)
+ * @date      Oct 26, 2015
+ *
+ * @brief     DQM Plotter for PCL-Alignment
+ */
+
+
+
+/*** system includes ***/
+#include <array>
+//#include <memory>
+//#include <fstream>
+//#include <string>
+
+/*** core framework functionality ***/
+#include "FWCore/ParameterSet/interface/ParameterSet.h"
+#include "FWCore/ServiceRegistry/interface/Service.h"
+#include "FWCore/Framework/interface/Event.h"
+#include "FWCore/Framework/interface/EventSetup.h"
+#include "FWCore/Framework/interface/MakerMacros.h"
+#include "FWCore/MessageLogger/interface/MessageLogger.h"
+
+/*** DQM ***/
+#include "DQMServices/Core/interface/DQMEDAnalyzer.h"
+#include "DQMServices/Core/interface/MonitorElement.h"
+
+/*** MillePede ***/
+#include "Alignment/MillePedeAlignmentAlgorithm/interface/MillePedeFileReader.h"
+
+
+
+class MillePedeDQMModule : public DQMEDAnalyzer {
+
+  //========================== PUBLIC METHODS ==================================
+  public: //====================================================================
+
+    MillePedeDQMModule(const edm::ParameterSet&);
+    virtual ~MillePedeDQMModule();
+
+    static void fillDescriptions(edm::ConfigurationDescriptions& descriptions);
+
+    virtual void bookHistograms(DQMStore::IBooker&, edm::Run const&,
+                                edm::EventSetup const&) override;
+
+    virtual void analyze(edm::Event const& e, edm::EventSetup const& es) {}
+
+    virtual void endRun(edm::Run const&, edm::EventSetup const&) override;
+
+  //========================= PRIVATE METHODS ==================================
+  private: //===================================================================
+
+    void fillExpertHistos();
+
+    void fillExpertHisto(MonitorElement* histos[],
+                         const double cut,
+                         std::array<double, 6> obs,
+                         std::array<double, 6> obsErr);
+
+  //========================== PRIVATE DATA ====================================
+  //============================================================================
+
+    const edm::ParameterSet& mpReaderConfig_;
+    MillePedeFileReader mpReader;
+
+    // Cutoff in micro-meter & micro-rad
+    double Xcut_, tXcut_;
+    double Ycut_, tYcut_;
+    double Zcut_, tZcut_;
+    // maximum movement in micro-meter/rad
+    double maxMoveCut_;
+    // Signifiance of movement must be above
+    double sigCut_;
+
+    // Histograms
+    MonitorElement* h_xPos[4];
+    MonitorElement* h_xRot[4];
+    MonitorElement* h_yPos[4];
+    MonitorElement* h_yRot[4];
+    MonitorElement* h_zPos[4];
+    MonitorElement* h_zRot[4];
+
+};
+
+// define this as a plug-in
+DEFINE_FWK_MODULE(MillePedeDQMModule);
+
+#endif /* Alignment_MillePedeAlignmentAlgorithm_MillePedeDQMModule_h */
