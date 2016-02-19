cb0585d4186654fb2a9d780f24f1daae808befab
Salvatore Di Guida <salvatore.di.guida@cern.ch>
2016-02-19 10:18:40 +0100

    Improvement of the configuration stack for Condition DB access: * add a new cff, namely Configuration/StandardSequences/python/CondDBESSource_cff.py importing only CondCore.ESSources.CondDBESSource_cfi: this way, you have a standard configuration fragment as entry point for condition access, which could be eventually customised if really needed; * change Configuration/StandardSequences/python/FrontierConditions_GlobalTag_cff.py to load Configuration.StandardSequences.CondDBESSource_cff * propagate the change in the client code, in particular Configuration/AlCa/python/GlobalTag.py and two test configuration for DT.

--- Configuration/StandardSequences/python/FrontierConditions_GlobalTag_cff.py
+++ Configuration/StandardSequences/python/FrontierConditions_GlobalTag_cff.py
@@ -1,4 +1,4 @@
 import FWCore.ParameterSet.Config as cms
 
-from CondCore.ESSources.CondDBESSource_cfi import GlobalTag
+from Configuration.StandardSequences.CondDBESSource_cff import *
 from Configuration.StandardSequences.AdditionalConditions_cff import *
