fce815b6e2d853a7f8741035f0a16246f32f58fc
Slava Krutelyov <slava77@cern.ch>
2016-02-20 00:43:20 +0100

    some of it could have been era-based ... might be temporary

--- DPGAnalysis/Skims/python/logErrorSkim_cff.py
+++ DPGAnalysis/Skims/python/logErrorSkim_cff.py
@@ -1,10 +1,17 @@
 from FWCore.Modules.logErrorFilter_cfi import *
-from Configuration.StandardSequences.RawToDigi_Data_cff import gtEvmDigis
+
 stableBeam = cms.EDFilter("HLTBeamModeFilter",
                           L1GtEvmReadoutRecordTag = cms.InputTag("gtEvmDigis"),
                           AllowedBeamMode = cms.vuint32(11),
                           saveTags = cms.bool(False)
                           )
+from L1Trigger.Configuration.L1TRawToDigi_cff import L1TRawToDigi
+if not "gtEvmDigis" in L1TRawToDigi.moduleNames():
+    import EventFilter.L1GlobalTriggerRawToDigi.l1GtEvmUnpack_cfi
+    gtEvmDigis = EventFilter.L1GlobalTriggerRawToDigi.l1GtEvmUnpack_cfi.l1GtEvmUnpack.clone()
+    gtEvmDigis.EvmGtInputTag = 'rawDataCollector'
+else:
+    from L1Trigger.Configuration.L1TRawToDigi_cff import gtEvmDigis
 
 logerrorseq=cms.Sequence(gtEvmDigis+stableBeam+logErrorSkimFilter)
 logerrormonitorseq=cms.Sequence(gtEvmDigis+stableBeam+logErrorFilter)
