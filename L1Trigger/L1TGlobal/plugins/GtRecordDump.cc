e03d25ab49bb0b74761134d35032c979dd968a77
Brian Winer <brianwiner@gmail.com>
2016-02-19 12:22:11 -0500

    Add summary dump at endRun for trigger algs

--- L1Trigger/L1TGlobal/plugins/GtRecordDump.cc
+++ L1Trigger/L1TGlobal/plugins/GtRecordDump.cc
@@ -64,6 +64,7 @@ namespace l1t {
     explicit GtRecordDump(const edm::ParameterSet&);
     virtual ~GtRecordDump(){};
     virtual void analyze(const edm::Event&, const edm::EventSetup&);  
+    virtual void endRun(edm::Run const&, edm::EventSetup const&) override;
     
     EDGetToken egToken;
     EDGetToken muToken;
@@ -89,6 +90,8 @@ namespace l1t {
     unsigned int formatJet(std::vector<l1t::Jet>::const_iterator jet);
     unsigned int formatMissET(std::vector<l1t::EtSum>::const_iterator etSum);
     unsigned int formatTotalET(std::vector<l1t::EtSum>::const_iterator etSum);
+    std::map<std::string, std::vector<int> > m_algoSummary;
+    
     
     unsigned int m_absBx;
     int m_bxOffset;
@@ -167,7 +170,7 @@ namespace l1t {
   
  
 
-  if(m_dumpTriggerResults) {
+  
     
      //Fill the L1 result maps
      m_gtUtil->retrieveL1(iEvent,evSetup,uGtAlgToken);
@@ -185,29 +188,43 @@ namespace l1t {
      LogDebug("GtRecordDump") << "retrieved all event vectors " << endl;
 
      // Dump the results
-     cout << "    Bit                  Algorithm Name                  Init    PScd  Final   PS Factor     Masked    Veto " << endl;
-     cout << "============================================================================================================" << endl;
+     if(m_dumpTriggerResults) {
+       cout << "    Bit                  Algorithm Name                                      Init    PScd  Final   PS Factor     Masked    Veto " << endl;
+       cout << "================================================================================================================================" << endl;
+     }
      for(unsigned int i=0; i<initialDecisions.size(); i++) {
        
        // get the name and trigger result
        std::string name = (initialDecisions.at(i)).first;
        bool resultInit = (initialDecisions.at(i)).second;
+       
+       //  put together our map of algorithms and counts across events      
+       if(m_algoSummary.count(name)==0) {
+         std::vector<int> tst;
+	 tst.resize(3);
+	 m_algoSummary[name]=tst;
+       }    	        
+       if (resultInit) (m_algoSummary.find(name)->second).at(0) += 1;
 
        // get prescaled and final results (need some error checking here)
        bool resultPre = (prescaledDecisions.at(i)).second;
+       if (resultPre) (m_algoSummary.find(name)->second).at(1) += 1;
        bool resultFin = (finalDecisions.at(i)).second;
+       if (resultFin) (m_algoSummary.find(name)->second).at(2) += 1;
        
        // get the prescale and mask (needs some error checking here)
        int prescale = (prescales.at(i)).second;
        bool mask    = (masks.at(i)).second;
        bool veto    = (vetoMasks.at(i)).second;
        
-       if(name != "NULL") cout << std::dec << setfill(' ') << "   " << setw(5) << i << "   " << setw(40) << name.c_str() << "   " << setw(7) << resultInit << setw(7) << resultPre << setw(7) << resultFin << setw(10) << prescale << setw(11) << mask << setw(9) << veto << endl;
+       if(m_dumpTriggerResults && name != "NULL") cout << std::dec << setfill(' ') << "   " << setw(5) << i << "   " << setw(60) << name.c_str() << "   " << setw(7) << resultInit << setw(7) << resultPre << setw(7) << resultFin << setw(10) << prescale << setw(11) << mask << setw(9) << veto << endl;
      }
      bool finOR = m_gtUtil->getFinalOR();
-     cout << "                                                                FinalOR = " << finOR <<endl;
-     cout << "===========================================================================================================" << endl;
-  }
+     if(m_dumpTriggerResults) {
+       cout << "                                                                                    FinalOR = " << finOR <<endl;
+       cout << "================================================================================================================================" << endl;
+     }
+  
 
   
   if(m_dumpGTRecord) {
@@ -399,6 +416,28 @@ namespace l1t {
     
     
   }
+
+// ------------ method called when ending the processing of a run  ------------
+
+void 
+GtRecordDump::endRun(edm::Run const&, edm::EventSetup const&)
+{
+    // Dump the results
+     cout << "=========================== Global Trigger Summary Report  ==================================" << endl;
+     cout << "                       Algorithm Name                              Init      PScd     Final   " << endl;
+     cout << "=============================================================================================" << endl;
+     for (std::map<std::string, std::vector<int> >::const_iterator itAlgo = m_algoSummary.begin(); itAlgo != m_algoSummary.end(); itAlgo++) {       
+      
+        std::string name = itAlgo->first;
+	int initCnt = (itAlgo->second).at(0);
+	int initPre = (itAlgo->second).at(1);
+	int initFnl = (itAlgo->second).at(2);
+	if(name != "NULL") cout << std::dec << setfill(' ') <<  setw(60) << name.c_str() << setw(10) << initCnt << setw(10) << initPre << setw(10) << initFnl << endl; //<< "   " << setw(7) << resultInit << setw(7) << resultPre << setw(7) << resultFin << setw(10) << prescale << setw(11) << mask << setw(9) << veto
+     }
+     cout << "===========================================================================================================" << endl;   
+}
+
+
 
 void GtRecordDump::dumpTestVectors(int bx, std::ofstream& myOutFile, 
                                       Handle<BXVector<l1t::Muon>> muons,
