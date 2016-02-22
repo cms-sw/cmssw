a687730ee5a4e848183e4e4b726cd2ae2f6d1c7e
Marta <marta.verweij@cern.ch>
2016-02-23 00:07:54 +0100

    remove rho analyzer

--- HiJetBackground/HiFJRhoProducer/python/hiFJRhoAnalyzer.py
+++
@@ -1,8 +0,0 @@
-import FWCore.ParameterSet.Config as cms
-
-hiFJRhoAnalyzer = cms.EDAnalyzer('HiFJRhoAnalyzer',
-                                 etaMap    = cms.InputTag('hiFJRhoProducer','mapEtaEdges','HiForest'),
-                                 rho       = cms.InputTag('hiFJRhoProducer','mapToRho'),
-                                 rhom      = cms.InputTag('hiFJRhoProducer','mapToRhoM'),
-)
-
