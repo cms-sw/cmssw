5c153a71c967ce7c58b48ebb200c6aee6150982c
Riccardo Manzoni <riccardo.manzoni@cern.ch>
2016-02-25 16:40:29 +0100

    oops

--- RecoTauTag/HLTProducers/interface/L1HLTJetsMatching.h
+++ RecoTauTag/HLTProducers/interface/L1HLTJetsMatching.h
@@ -9,7 +9,8 @@
 #include "FWCore/ParameterSet/interface/ParameterSet.h"
 #include "FWCore/Utilities/interface/InputTag.h"
 #include "DataFormats/Common/interface/Handle.h"
-#include "DataFormats/L1Trigger/interface/Jet.h"
+#include "DataFormats/L1Trigger/interface/L1JetParticle.h"
+#include "DataFormats/L1Trigger/interface/L1JetParticleFwd.h"
 #include "DataFormats/JetReco/interface/CaloJetCollection.h"
 #include "DataFormats/HLTReco/interface/TriggerFilterObjectWithRefs.h"
 
