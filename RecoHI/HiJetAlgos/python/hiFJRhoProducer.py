e581e1957307e5867b8cf6afae397f2c136be100
Marta <marta.verweij@cern.ch>
2016-02-26 15:02:12 +0100

    doubles untracked

--- RecoHI/HiJetAlgos/python/hiFJRhoProducer.py
+++ RecoHI/HiJetAlgos/python/hiFJRhoProducer.py
@@ -3,7 +3,7 @@ import FWCore.ParameterSet.Config as cms
 hiFJRhoProducer = cms.EDProducer('HiFJRhoProducer',
                                  jetSource = cms.InputTag('kt4PFJets'),
                                  nExcl = cms.untracked.uint32(2),
-                                 etaMaxExcl = cms.double(2.),
-                                 ptMinExcl = cms.double(20.)
+                                 etaMaxExcl = cms.untracked.double(2.),
+                                 ptMinExcl = cms.untracked.double(20.)
 )
 
