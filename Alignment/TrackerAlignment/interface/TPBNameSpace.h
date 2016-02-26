6299e78cb5e5bbf0e9104ae16a50dbdd4fdd2056
DirtyDan88 <Max.stark88@web.de>
2016-02-26 18:11:07 +0100

    removed some hardcoded geometry assumptions

--- Alignment/TrackerAlignment/interface/TPBNameSpace.h
+++ Alignment/TrackerAlignment/interface/TPBNameSpace.h
@@ -10,6 +10,9 @@
  *  $Date: 2007/10/18 09:57:10 $
  *  $Revision: 1.1 $
  *  \author Chung Khim Lae
+ *
+ *  Last Update: Max Stark
+ *         Date: Fri, 05 Feb 2016 17:21:33 CET
  */
 
 #include "CondFormats/Alignment/interface/Definitions.h"
@@ -21,7 +24,7 @@ namespace align
   namespace tpb
   {
     /// Number of ladders for each quarter cylinder.
-    const unsigned int lpqc[] = {5, 8, 11};
+    std::vector<unsigned int> lpqc;
 
     /// Module number increases with z from 1 to 8.
     inline unsigned int moduleNumber(align::ID, const TrackerTopology*);
@@ -35,6 +38,9 @@ namespace align
 
     /// Half barrel number is 1 at left side (-x) and 2 at right side (+x).
     inline unsigned int halfBarrelNumber(align::ID, const TrackerTopology*);
+
+    /// Barrel number is 1 for all align::ID's which belong to this barrel
+    inline unsigned int barrelNumber(align::ID, const TrackerTopology*);
   }
 }
 
@@ -45,8 +51,6 @@ unsigned int align::tpb::moduleNumber(align::ID id, const TrackerTopology* tTopo
 
 unsigned int align::tpb::ladderNumber(align::ID id, const TrackerTopology* tTopo)
 {
-  
-
   unsigned int l = tTopo->pxbLadder(id); // increases with phi
   unsigned int c = tTopo->pxbLayer(id) - 1;
 
@@ -55,10 +59,9 @@ unsigned int align::tpb::ladderNumber(align::ID id, const TrackerTopology* tTopo
 // Ladder in 3rd quadrant: number = l - lpqc         (lpqc + 1 to 2 * lpqc)
 // Ladder in 4th quadrant: number = 5 * lpqc + 1 - l (lpqc + 1 to 2 * lpqc)
 
-  return l > 3 * lpqc[c] ? // ladder in 4th quadrant
-    5 * lpqc[c] + 1 - l :
-    (l > lpqc[c] ? // ladder not in 1st quadrant
-     l - lpqc[c] : lpqc[c] + 1 - l);
+  return l > 3 * lpqc[c] ? 5 * lpqc[c] + 1 - l :  // ladder in 4th quadrant
+           (l > lpqc[c] ? l - lpqc[c] :           // ladder not in 1st quadrant
+             lpqc[c] + 1 - l);
 }
 
 unsigned int align::tpb::layerNumber(align::ID id, const TrackerTopology* tTopo)
@@ -68,12 +71,15 @@ unsigned int align::tpb::layerNumber(align::ID id, const TrackerTopology* tTopo)
 
 unsigned int align::tpb::halfBarrelNumber(align::ID id, const TrackerTopology* tTopo)
 {
-  
-
   unsigned int l = tTopo->pxbLadder(id); // increases with phi
   unsigned int c = tTopo->pxbLayer(id) - 1;
 
   return l > lpqc[c] && l <= 3 * lpqc[c] ? 1 : 2;
+}
+
+unsigned int align::tpb::barrelNumber(align::ID, const TrackerTopology*)
+{
+  return 1;
 }
 
 #endif
