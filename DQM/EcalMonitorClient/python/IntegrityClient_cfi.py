7d6a8704ef6228e66bbe4b37fb412bfb7c20f392
Michael <michael.andrews@cern.ch>
2016-02-26 17:23:53 +0100

    Fix channel status description.

--- DQM/EcalMonitorClient/python/IntegrityClient_cfi.py
+++ DQM/EcalMonitorClient/python/IntegrityClient_cfi.py
@@ -37,7 +37,7 @@ ecalIntegrityClient = cms.untracked.PSet(
             kind = cms.untracked.string('TH2F'),
             otype = cms.untracked.string('Ecal3P'),
             btype = cms.untracked.string('Crystal'),
-            description = cms.untracked.string('Map of channel status as given by the Ecal Channel Status Record. LEGEND: 0:channel ok, 1:DAC settings problem, pedestal not in the design range, 2:channel with no laser, ok elsewhere, 3:noisy, 4:very noisy, 5-7:reserved for more categories of noisy channels, 8:channel at fixed gain 6 (or 6 and 1), 9:channel at fixed gain 1, 10:channel at fixed gain 0 (dead of type this), 11:non responding isolated channel (dead of type other), 12:channel and one or more neigbors not responding (e.g.: in a dead VFE 5x1 channel), 13:channel in TT with no data link, TP data ok, 14:channel in TT with no data link and no TP data.')
+            description = cms.untracked.string('Map of channel status as given by the Ecal Channel Status Record. LEGEND:<br/>0: Channel ok,<br/>1: DAC settings problem, pedestal not in the design range,<br/>2: Channel with no laser, ok elsewhere,<br/>3: Noisy,<br/>4: Very noisy,<br/>5-7: Reserved for more categories of noisy channels,<br/>8: Channel at fixed gain 6 (or 6 and 1),<br/>9: Channel at fixed gain 1,<br/>10: Channel at fixed gain 0 (dead of type this),<br/>11: Non-responding isolated channel (dead of type other),<br/>12: Channel and one or more neigbors not responding (e.g.: in a dead VFE 5x1 channel),<br/>13: Channel in TT with no data link, TP data ok,<br/>14: Channel in TT with no data link and no TP data.')
         )
     )
 )
