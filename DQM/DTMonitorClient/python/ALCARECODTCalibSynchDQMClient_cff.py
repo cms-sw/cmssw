8aec6d33f5cbdf0d956b4204e441dd3a45ac25e3
Javier <Javier.Fernandez@cern.ch>
2016-02-29 17:32:59 +0100

    Fixes for Prompt Offline Analysis using TwinMuX data

--- DQM/DTMonitorClient/python/ALCARECODTCalibSynchDQMClient_cff.py
+++
@@ -1,9 +0,0 @@
-import FWCore.ParameterSet.Config as cms
-
-from DQM.DTMonitorClient.dtResolutionAnalysisTest_cfi import *
-dtResolutionAnalysisTestAlca = dtResolutionAnalysisTest.clone() 
-dtResolutionAnalysisTestAlca.topHistoFolder = 'AlCaReco/DtCalibSynch/01-Calibration'
-
-ALCARECODTCalibSynchDQMClient = cms.Sequence(dtResolutionAnalysisTestAlca)
-
-
--- DQM/DTMonitorClient/python/dtDAQInfo_cfi.py
+++
@@ -1,5 +0,0 @@
-import FWCore.ParameterSet.Config as cms
-
-dtDAQInfo = cms.EDAnalyzer("DTDAQInfo")
-
-
