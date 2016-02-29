5aebdb5ea763b671cb2d9c9f66c5a433a6825627
Josh Bendavid <Josh.Bendavid@cern.ch>
2016-03-01 00:31:59 +0100

    add lhe header and weight name information to GenLumiInfoHeader

--- SimDataFormats/GeneratorProducts/interface/GenLumiInfoHeader.h
+++ SimDataFormats/GeneratorProducts/interface/GenLumiInfoHeader.h
@@ -1,6 +1,8 @@
 #ifndef SimDataFormats_GeneratorProducts_GenLumiInfoHeader_h
 #define SimDataFormats_GeneratorProducts_GenLumiInfoHeader_h
 
+#include <vector>
+#include <utility>
 #include <string>
 
 /** \class GenLumiInfoHeader
@@ -16,11 +18,19 @@ class GenLumiInfoHeader {
     void setRandomConfigIndex(int idx) { randomConfigIndex_ = idx; }
     
     const std::string &configDescription() const { return configDescription_; }
-    void setConfigDescription(const std::string &str) { configDescription_ = str; }  
+    void setConfigDescription(const std::string &str) { configDescription_ = str; }
+    
+    const std::vector<std::pair<std::string, std::string> > &lheHeaders() const { return lheHeaders_; }
+    std::vector<std::pair<std::string, std::string> > &lheHeaders() { return lheHeaders_; }
+    
+    const std::vector<std::string> &weightNames() const { return weightNames_; }
+    std::vector<std::string> &weightNames() { return weightNames_; }
 
   private:
     int randomConfigIndex_;
     std::string configDescription_;
+    std::vector<std::pair<std::string, std::string> > lheHeaders_; //header name, header content
+    std::vector<std::string> weightNames_;
 
 };
 
