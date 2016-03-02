6a59f189a74d76882b9dd41543de8d9247d33c36
sara <sara.fiorendi@cern.ch>
2016-03-03 00:18:21 +0100

    migrate to stage2 L!

--- HLTrigger/Muon/src/SealModule.cc
+++ HLTrigger/Muon/src/SealModule.cc
@@ -6,11 +6,13 @@
 #include "HLTrigger/Muon/interface/HLTMuonL1RegionalFilter.h"
 #include "HLTrigger/Muon/interface/HLTMuonL1TRegionalFilter.h"
 #include "HLTrigger/Muon/interface/HLTMuonL2PreFilter.h"
+#include "HLTrigger/Muon/interface/HLTMuonL2FromL1TPreFilter.h"
 #include "HLTrigger/Muon/interface/HLTMuonL3PreFilter.h"
 #include "HLTrigger/Muon/interface/HLTMuonL1toL3TkPreFilter.h"
 #include "HLTrigger/Muon/interface/HLTMuonL1TtoL3TkPreFilter.h"
 #include "HLTrigger/Muon/interface/HLTMuonIsoFilter.h"
 #include "HLTrigger/Muon/interface/HLTMuonDimuonL2Filter.h"
+#include "HLTrigger/Muon/interface/HLTMuonDimuonL2FromL1TFilter.h"
 #include "HLTrigger/Muon/interface/HLTMuonDimuonL3Filter.h"
 #include "HLTrigger/Muon/interface/HLTMuonTrimuonL3Filter.h"
 #include "HLTrigger/Muon/interface/HLTDiMuonGlbTrkFilter.h"
@@ -25,11 +27,13 @@ DEFINE_FWK_MODULE(HLTMuonL1Filter);
 DEFINE_FWK_MODULE(HLTMuonL1RegionalFilter);
 DEFINE_FWK_MODULE(HLTMuonL1TRegionalFilter);
 DEFINE_FWK_MODULE(HLTMuonL2PreFilter);
+DEFINE_FWK_MODULE(HLTMuonL2FromL1TPreFilter);
 DEFINE_FWK_MODULE(HLTMuonL3PreFilter);
 DEFINE_FWK_MODULE(HLTMuonL1toL3TkPreFilter);
 DEFINE_FWK_MODULE(HLTMuonL1TtoL3TkPreFilter);
 DEFINE_FWK_MODULE(HLTMuonIsoFilter);
 DEFINE_FWK_MODULE(HLTMuonDimuonL2Filter);
+DEFINE_FWK_MODULE(HLTMuonDimuonL2FromL1TFilter);
 DEFINE_FWK_MODULE(HLTMuonDimuonL3Filter);
 DEFINE_FWK_MODULE(HLTMuonTrimuonL3Filter);
 DEFINE_FWK_MODULE(HLTDiMuonGlbTrkFilter);
