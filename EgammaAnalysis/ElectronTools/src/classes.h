1fdfef3638153b01326d1bc0a399b66333d25599
Emanuele Di Marco <emanuele.dimarco@gmail.com>
2016-03-02 14:20:04 +0100

    make it working when used in python

--- EgammaAnalysis/ElectronTools/src/classes.h
+++ EgammaAnalysis/ElectronTools/src/classes.h
@@ -3,7 +3,7 @@
 #include "EgammaAnalysis/ElectronTools/interface/ElectronEnergyCalibratorRun2.h"
 
 namespace {
-  struct dictionary {
+  struct dictionaryfuffa {
     SimpleElectron fuffaElectron;
     EpCombinationTool fuffaElectronCombinator;
     ElectronEnergyCalibratorRun2 fuffaElectronCalibrator;
