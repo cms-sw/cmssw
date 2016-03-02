fcb32e23a5a8116c65aea91a6b93f8170574f200
sara <sara.fiorendi@cern.ch>
2016-03-02 20:08:16 +0100

    remove comments on old quality definitions

--- HLTrigger/Muon/interface/HLTMuonL1TFilter.h
+++ HLTrigger/Muon/interface/HLTMuonL1TFilter.h
@@ -41,23 +41,7 @@ class HLTMuonL1TFilter : public HLTFilter {
     double minPt_;
 
     /// Quality codes:
-    ///
-    /// 0  .. no muon
-    /// 1  .. beam halo muon (CSC)
-    /// 2  .. very low quality level 1 (e.g. ignore in single and di-muon trigger)
-    /// 3  .. very low quality level 2 (e.g. ignore in single muon trigger use in di-muon trigger)
-    /// 4  .. very low quality level 3 (e.g. ignore in di-muon trigger, use in single-muon trigger)
-    /// 5  .. unmatched RPC
-    /// 6  .. unmatched DT or CSC
-    /// 7  .. matched DT-RPC or CSC-RPC
-    ///
-    /// attention: try not to rely on quality codes in analysis: they may change again
-    ///
-    /// Quality bit mask:
-    ///
-    /// the eight lowest order or least significant bits correspond to the qulity codes above;
-    /// if a bit is 1, that code is accepted, otherwise not;
-    /// example: 11101000 accepts qualities 3, 5, 6, 7
+    /// to be updated with new L1 quality definitions
     int qualityBitMask_;
     
     /// min N objects
