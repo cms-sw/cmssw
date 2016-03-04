a251b78645b0865f99227c97d85fca9b219210d3
Martin Grunewald <Martin.Grunewald@cern.ch>
2016-03-04 17:48:12 +0100

    L1T xml files from CondDB

--- Configuration/HLT/python/autoCondHLT.py
+++ Configuration/HLT/python/autoCondHLT.py
@@ -1,21 +1,27 @@
 # https://cms-conddb.cern.ch/browser/#search
 
-#default value for all L1T menus
+# default value for all L1T menus
 connectionString = "frontier://FrontierProd/CMS_CONDITIONS"
+
+# L1T legacy/stage-1
 l1MenuRecord = "L1GtTriggerMenuRcd"
 l1MenuLabel = ""
+
+# L1T stage-2
+l1tMenuRecord = "L1TUtmTriggerMenuRcd"
+l1tMenuLabel = ""
 
 #The snapshot time has been set as starting point as the one of PR 12095.
 #Next time you change the customisations, change also the snapshot time in the affected tuple,
 #and leave unchanged the snapshot times for the other tuples.
 
 l1Menus= {
-    'Fake'         : ( ','.join( [ 'L1GtTriggerMenu_L1Menu_Collisions2012_v3_mc'  , l1MenuRecord, connectionString, l1MenuLabel, "2015-10-26 12:00:00.000"] ), ),
-    'FULL'         : ( ','.join( [ 'L1Menu_Collisions2015_25nsStage1_v5'          , l1MenuRecord, connectionString, l1MenuLabel, "2015-10-26 12:00:00.000"] ), ),
-    'GRun'         : ( ','.join( [ 'L1Menu_Collisions2015_25nsStage1_v5'          , l1MenuRecord, connectionString, l1MenuLabel, "2015-10-26 12:00:00.000"] ), ),
-    'HIon'         : ( ','.join( [ 'L1Menu_CollisionsHeavyIons2015_v5_mc'         , l1MenuRecord, connectionString, l1MenuLabel, "2015-11-26 20:00:00.000"] ), ),
-    'PIon'         : ( ','.join( [ 'L1Menu_Collisions2015_25nsStage1_v5'          , l1MenuRecord, connectionString, l1MenuLabel, "2015-10-26 12:00:00.000"] ), ),
-    'PRef'         : ( ','.join( [ 'L1Menu_Collisions2015_5TeV_pp_reference_v5_mc', l1MenuRecord, connectionString, l1MenuLabel, "2015-11-25 20:00:00.000"] ), ),
+    'Fake' : ( ','.join( [ 'L1GtTriggerMenu_L1Menu_Collisions2012_v3_mc'             , l1MenuRecord,connectionString, l1MenuLabel, "2015-10-26 12:00:00.000"] ), ),
+    'FULL' : ( ','.join( [ 'L1Menu_Collisions2015_25nsStage1_v7_uGT_v4_xml'          ,l1tMenuRecord,connectionString,l1tMenuLabel, "2016-03-03 17:00:00.000"] ), ),
+    'GRun' : ( ','.join( [ 'L1Menu_Collisions2015_25nsStage1_v7_uGT_v4_xml'          ,l1tMenuRecord,connectionString,l1tMenuLabel, "2016-03-03 17:00:00.000"] ), ),
+    'HIon' : ( ','.join( [ 'L1Menu_CollisionsHeavyIons2015_v5_uGT_xml'               ,l1tMenuRecord,connectionString,l1tMenuLabel, "2016-03-04 15:00:00.000"] ), ),
+    'PIon' : ( ','.join( [ 'L1Menu_Collisions2015_25nsStage1_v7_uGT_v4_xml'          ,l1tMenuRecord,connectionString,l1tMenuLabel, "2016-03-03 17:00:00.000"] ), ),
+    'PRef' : ( ','.join( [ 'L1Menu_Collisions2015_5TeV_pp_reference_v5_uGT_v2_mc_xml',l1tMenuRecord,connectionString,l1tMenuLabel, "2016-03-04 15:00:00.000"] ), ),
 }
 
 hltGTs = {
