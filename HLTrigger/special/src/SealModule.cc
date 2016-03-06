91d4922ec60e20da2f69f7d0e9765d2b2c52c7b5
Sezen Sekmen <ssekmen@cern.ch>
2016-03-06 09:59:42 +0100

    added HLTPixelIsolTrackL1TFilter as a module

--- HLTrigger/special/src/SealModule.cc
+++ HLTrigger/special/src/SealModule.cc
@@ -3,6 +3,7 @@
 #include "HLTrigger/special/interface/HLTPixlMBFilt.h"
 #include "HLTrigger/special/interface/HLTPixlMBForAlignmentFilter.h"
 #include "HLTrigger/special/interface/HLTPixelIsolTrackFilter.h"
+#include "HLTrigger/special/interface/HLTPixelIsolTrackL1TFilter.h"
 #include "HLTrigger/special/interface/HLTEcalPixelIsolTrackFilter.h"
 #include "HLTrigger/special/interface/HLTEcalIsolationFilter.h"
 #include "HLTrigger/special/interface/HLTEcalPhiSymFilter.h"
@@ -36,6 +37,7 @@
 DEFINE_FWK_MODULE(HLTPixlMBFilt);
 DEFINE_FWK_MODULE(HLTPixlMBForAlignmentFilter);
 DEFINE_FWK_MODULE(HLTPixelIsolTrackFilter);
+DEFINE_FWK_MODULE(HLTPixelIsolTrackL1TFilter);
 DEFINE_FWK_MODULE(HLTEcalPixelIsolTrackFilter);
 DEFINE_FWK_MODULE(HLTEcalIsolationFilter);
 DEFINE_FWK_MODULE(HLTEcalPhiSymFilter);
