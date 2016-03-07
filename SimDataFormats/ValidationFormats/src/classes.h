2e85d90705d4d52faa6a0c95b00ad61d78ff406d
Sunanda <sunanda.banerjee@cern.ch>
2016-03-07 16:47:59 +0100

    Add Validation code due to Maksat

--- SimDataFormats/ValidationFormats/src/classes.h
+++ SimDataFormats/ValidationFormats/src/classes.h
@@ -6,6 +6,7 @@
 #include "SimDataFormats/ValidationFormats/interface/MaterialAccountingStep.h"
 #include "SimDataFormats/ValidationFormats/interface/MaterialAccountingDetector.h"
 #include "SimDataFormats/ValidationFormats/interface/MaterialAccountingTrack.h"
+#include "SimDataFormats/ValidationFormats/interface/PHGCalValidInfo.h"
 
 namespace SimDataFormats_ValidationFormats {
   struct dictionary {
@@ -69,5 +70,8 @@ namespace SimDataFormats_ValidationFormats {
     MaterialAccountingTrack                                 matrack;
     std::vector<MaterialAccountingTrack>                    matrack_v;
     edm::Wrapper<std::vector<MaterialAccountingTrack> >     matrack_w;
+
+    PHGCalValidInfo                                         dummyHGC;
+    edm::Wrapper<PHGCalValidInfo>                           theValidData10;
   };
 }
