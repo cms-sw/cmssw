4c2cf0e1d5b1dade3968dde8890837fb3040408c
Kevin Pedro <kpedro88@gmail.com>
2016-03-14 14:19:49 -0500

    fix mantissa and overlap for QIE10

--- CondFormats/HcalObjects/interface/HcalQIEShape.h
+++ CondFormats/HcalObjects/interface/HcalQIEShape.h
@@ -22,7 +22,10 @@ class HcalQIEShape {
   float highEdge (unsigned fAdc) const;
   float center (unsigned fAdc) const;
   bool setLowEdges (unsigned int nVals, const float *fValue);
-  unsigned range (unsigned fAdc) const {return (fAdc >> 5) & 0x3;}
+  unsigned range (unsigned fAdc) const {
+    //6 bit mantissa in QIE10, 5 in QIE8
+    return (nbins_ == 32) ? (fAdc >> 5) & 0x3 : (fAdc >> 6) & 0x3;
+  }
   unsigned local (unsigned fAdc) const {
     unsigned tmp = nbins_ == 32 ? (fAdc & 0x1f) : (fAdc & 0x3f) ;
     return   tmp;
