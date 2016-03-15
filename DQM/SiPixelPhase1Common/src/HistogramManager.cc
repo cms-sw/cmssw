e37ba89d4fc88bb5e11b6c4b5d123f04f3456e51
Marcel Andre Schneider <marcel.andre.schneider@cern.ch>
2016-03-15 16:32:45 +0100

    Simple booking. Not optimal yet, hardcoded ranges etc... but generates a plot\!

--- DQM/SiPixelPhase1Common/src/HistogramManager.cc
+++ DQM/SiPixelPhase1Common/src/HistogramManager.cc
@@ -5,6 +5,13 @@
 //
 #include "DQM/SiPixelPhase1Common/interface/HistogramManager.h"
 
+#include <sstream>
+
+// Geometry stuff
+#include "FWCore/Framework/interface/ESHandle.h"
+#include "Geometry/TrackerGeometryBuilder/interface/TrackerGeometry.h"
+#include "Geometry/Records/interface/TrackerDigiGeometryRecord.h"
+
 HistogramManager::HistogramManager(const edm::ParameterSet& iconfig) :
   iConfig(iconfig),
   topFolderName(iconfig.getParameter<std::string>("TopFolderName"))
@@ -43,6 +50,43 @@ void HistogramManager::book(DQMStore::IBooker& iBooker, edm::EventSetup const& i
     geometryInterface.load(iSetup, iConfig);
   }
   iBooker.setCurrentFolder(topFolderName);
-  // TODO: more stuff here. 
+
+  // TODO: this should go into GeometryInterface?
+  edm::ESHandle<TrackerGeometry> tg;
+  iSetup.get<TrackerDigiGeometryRecord>().get(tg);
+  auto detector = tg->detIds();
+
+  // TODO: We need 2 passes, one to count the elements for EXTEND-ranges, one for actual booking.
+  for (unsigned int i = 0; i < specs.size(); i++) {
+    auto& s = specs[i];
+    auto& t = tables[i];
+    for (DetId id : detector) {
+      auto iq = GeometryInterface::InterestingQuantities{
+	id, nullptr, 0, 0 // TODO: for fine geometry, iterate over all sane values here
+      };
+      auto dimensions = this->dimensions;
+      std::stringstream name(this->name);
+      std::stringstream title(this->title);
+      std::stringstream xlabel(this->xlabel);
+      std::stringstream ylabel(this->ylabel);
+      auto significantvalues = geometryInterface.extractColumns(s.steps[0].columns, iq);
+      for (SummationStep step : s.steps) {
+	if (step.stage == SummationStep::STAGE1) {
+	  //TODO: change labels, dimensionality, range, colums as fits
+	}
+      }
+      AbstractHistogram& histo = t[significantvalues];
+      if (histo.me) continue;
+      if (dimensions == 1) {
+	title << ";" << xlabel.str();
+      	histo.me = iBooker.book1D(name.str().c_str(), title.str().c_str(), 100, 0.0, 300.0);
+      } else if (dimensions == 2) {
+	title << ";" << xlabel << ";" << ylabel;
+	histo.me = iBooker.book2D(name.str().c_str(), title.str().c_str(), 100, 0.0, 300.0, 100, 0.0, 300.0);
+      } else {
+	std::cout << "Booking " << dimensions << " dimensions not supported.\n";
+      }
+    }
+  }
 }
 
