a49d4132833abcceca9188b4e16d62bda732e90d
Marcel Schneider <marcel.andre.schneider@cern.ch>
2016-03-16 14:57:20 +0100

    WIP: Bitmap Extraction. Results for Pixel are still implausible

--- DQM/SiPixelPhase1Common/src/GeometryInterface.cc
+++ DQM/SiPixelPhase1Common/src/GeometryInterface.cc
@@ -26,6 +26,9 @@
 
 // C++ stuff
 #include <cassert>
+#include <cstdint>
+#include <iostream>
+#include <iomanip>
 
 GeometryInterface GeometryInterface::instance;
 
@@ -58,24 +61,74 @@ void GeometryInterface::loadFromAlignment(edm::EventSetup const& iSetup, const e
 
   assert(trackerGeometry);
   assert(trackerTopology);
-  std::cout << "Ptrs: " << trackerGeometry << " " << trackerTopology << "\n";
   AlignableTracker* trackerAlignables = new AlignableTracker(trackerGeometry, trackerTopology);
   assert(trackerAlignables);
 
+  struct BitInfo {
+    uint32_t characteristicBits;
+    uint32_t characteristicMask;
+    uint32_t variableBase;
+    uint32_t variableMask;
+    Alignable* currenParent;
+  };
+
+  std::map<align::StructureType, BitInfo> infos;
+
   struct {
-    void traverseAlignables(Alignable* compositeAlignable) {
+    void traverseAlignables(Alignable* compositeAlignable, std::map<align::StructureType, BitInfo>& infos) {
+      std::cout << "+++++ Alignable " << AlignableObjectId::idToString(compositeAlignable->alignableObjectId()) << "\n";
+	
+      auto alignable = compositeAlignable;
+      auto& info = infos[alignable->alignableObjectId()];
+      if (info.characteristicBits == 0) {
+	info.characteristicBits = alignable->id();
+	info.characteristicMask = 0xFFFFFFFF;
+	info.variableMask = 0x0;
+      } 
+      // climb up the hierarchy and widen characteristics.
+      for (auto alignable = compositeAlignable; alignable; alignable = alignable->mother()) {
+	auto& info = infos[alignable->alignableObjectId()];
+	// ^ gives changed bits, ~ unchanged, & to collect all always  unchanged
+	info.characteristicMask &= ~(info.characteristicBits ^ compositeAlignable->id());
+      }
+      // variable mask must be local to the hierarchy
+      if (info.currenParent != alignable->mother() || !alignable->mother()) {
+	info.currenParent = alignable->mother();
+	info.variableBase = alignable->id();
+      } else {
+	// ^ gives changed bits, | to collect all ever changed
+	info.variableMask |= info.variableBase ^ alignable->id();
+      }
+
       for (auto* alignable : compositeAlignable->components()) {
-	auto type = AlignableObjectId::idToString(alignable->alignableObjectId());
-	std::cout << "+++ Alignable: " << type << "\n";
-	if (alignable->components().size() > 0) {
-	  traverseAlignables(alignable);
-	}
+	// for e.g. full Pixel, there is no variable part. so wee need to look at 
+	// children to see where characteristic ends.
+	//info.characteristicMask &= ~(info.characteristicBits ^ alignable->id());
+	traverseAlignables(alignable, infos);
       }
     }
-  } alignabelIterator;
+  } alignableIterator;
 
-  alignabelIterator.traverseAlignables(trackerAlignables);
+  alignableIterator.traverseAlignables(trackerAlignables, infos);
 
+  for (auto el : infos) {
+    auto info = el.second;
+    auto type = AlignableObjectId::idToString(el.first);
+    // since we did not update the characteristic for all children,
+    // we have false mask bits in the lsbs.
+    // We assume a left-to right ordererd hierarchy ad kill the
+    // characteristic bits from the left.
+    // +1 flips all bits up to (including) the first 0 from the right.
+    //info.characteristicMask &= (info.characteristicMask + 1);
+    // the characteristicBits that are masked out don't matter,
+    // to normalize we set them to 0.
+    info.characteristicBits &= info.characteristicMask;
+
+    std::cout << std::hex << std::setfill('0') << std::setw(8)
+              << "+++ Type " << info.characteristicBits << " "
+                                  << info.characteristicMask << " "
+                                  << info.variableMask << " " << type << "\n";
+  }
   delete trackerAlignables;
 }
 
