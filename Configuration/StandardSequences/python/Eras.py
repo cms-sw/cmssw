f9f67e04a0ec7f6c388f2604c4429bf63e6b04c0
Sven Dildick <sven.dildick@cern.ch>
2016-03-17 22:25:07 +0100

    modifier for high pu L1CSC

--- Configuration/StandardSequences/python/Eras.py
+++ Configuration/StandardSequences/python/Eras.py
@@ -13,13 +13,15 @@ class Eras (object):
         self.run2_HI_specific = cms.Modifier()
         self.run2_HF_2016 = cms.Modifier()
         self.stage1L1Trigger = cms.Modifier()
-        self.stage2L1Trigger = cms.Modifier()
-        self.phase1Pixel = cms.Modifier()
         # Implementation note: When this was first started, stage1L1Trigger wasn't in all
         # of the eras. Now that it is, it could in theory be dropped if all changes are
         # converted to run2_common (i.e. a search and replace of "stage1L1Trigger" to
         # "run2_common" over the whole python tree). In practice, I don't think it's worth
         # it, and this also gives the flexibilty to take it out easily.
+        self.stage2L1Trigger = cms.Modifier()
+        self.phase1Pixel = cms.Modifier()
+        self.muonTrigger2017 = cms.Modifier()
+        # Implementation note: muonTrigger2017 enables the L1CSC SLHC algorithm for high-PU running
 
         # Phase 2 sub-eras for stable features
         self.phase2_common = cms.Modifier()
@@ -56,7 +58,7 @@ class Eras (object):
         self.Run2_HI = cms.ModifierChain( self.run2_common, self.run2_HI_specific, self.stage1L1Trigger )
         # Future Run 2 scenarios.
         self.Run2_2016 = cms.ModifierChain( self.run2_common, self.run2_25ns_specific, self.stage2L1Trigger, self.run2_HF_2016 )
-        self.Run2_2017 = cms.ModifierChain( self.Run2_2016, self.phase1Pixel, self.trackingPhase1 )
+        self.Run2_2017 = cms.ModifierChain( self.Run2_2016, self.phase1Pixel, self.trackingPhase1, self.muonTrigger2017 )
         # Scenarios further afield.
         # Phase2 is everything for the 2023 (2026?) detector that works so far in this release.
         self.Phase2 = cms.ModifierChain( self.phase2_common, self.phase2_tracker, self.phase2_hgc, self.phase2_muon )
