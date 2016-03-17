6f3d561b311aab98fff7f5dd69ecc9e25999c873
Marcel Schneider <marcel.andre.schneider@cern.ch>
2016-03-17 18:45:42 +0100

    Enable config for step2

--- DQM/SiPixelPhase1Common/python/SiPixelPhase1OfflineDQM_harvesting_cff.py
+++ DQM/SiPixelPhase1Common/python/SiPixelPhase1OfflineDQM_harvesting_cff.py
@@ -3,4 +3,4 @@ import FWCore.ParameterSet.Config as cms
 # Pixel Digi Monitoring
 from DQM.SiPixelPhase1Digis.SiPixelPhase1Digis_cfi import *
 
-siPixelPhase1OfflineDQM_source = cms.Sequence(SiPixelPhase1DigisAnalyzer)
+siPixelPhase1OfflineDQM_harvesting = cms.Sequence(SiPixelPhase1DigisHarvester)
