9fc1810781138342ab3390f749ddb1c56b88e495
Gaelle <Gaelle.Boudoul@cern.ch>
2016-03-17 20:45:33 +0100

    changes needed for Tilted in8X

--- Configuration/Geometry/python/GeometryExtended2023simReco_cff.py
+++ Configuration/Geometry/python/GeometryExtended2023simReco_cff.py
@@ -2,4 +2,30 @@ import FWCore.ParameterSet.Config as cms
 
 # Ideal geometry, needed for transient ECAL alignement
 from Configuration.Geometry.GeometryExtended2023sim_cff import *
-from Configuration.Geometry.GeometryReco_cff import *
+# Reconstruction geometry services
+#  Tracking Geometry
+from Geometry.CommonDetUnit.globalTrackingGeometry_cfi import *
+
+#Tracker
+from RecoTracker.GeometryESProducer.TrackerRecoGeometryESProducer_cfi import *
+from Geometry.TrackerGeometryBuilder.trackerParameters_cfi import *
+from Geometry.TrackerNumberingBuilder.trackerTopology_cfi import *
+
+#Muon
+from Geometry.MuonNumbering.muonNumberingInitialization_cfi import *
+from RecoMuon.DetLayers.muonDetLayerGeometry_cfi import *
+
+#  Alignment
+from Geometry.TrackerGeometryBuilder.idealForDigiTrackerGeometry_cff import *
+from Geometry.CSCGeometryBuilder.idealForDigiCscGeometry_cff import *
+from Geometry.DTGeometryBuilder.idealForDigiDtGeometry_cff import *
+trackerGeometry.applyAlignment = cms.bool(False)
+
+#  Calorimeters
+from Geometry.CaloEventSetup.CaloTopology_cfi import *
+from Geometry.CaloEventSetup.CaloGeometry_cff import *
+from Geometry.CaloEventSetup.EcalTrigTowerConstituents_cfi import *
+from Geometry.EcalMapping.EcalMapping_cfi import *
+from Geometry.EcalMapping.EcalMappingRecord_cfi import *
+from Geometry.HcalCommonData.hcalDDConstants_cff import *
+from Geometry.HcalEventSetup.hcalTopologyIdeal_cfi import *
