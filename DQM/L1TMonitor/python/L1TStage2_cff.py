53366e12f78dd8c310b89062c6f4f4c6206c1833
Sean-Jiun Wang <churchguitarist@gmail.com>
2016-03-17 02:04:20 +0100

    Reorganize uGMT DQM Module

--- DQM/L1TMonitor/python/L1TStage2_cff.py
+++ DQM/L1TMonitor/python/L1TStage2_cff.py
@@ -1,7 +1,7 @@
 import FWCore.ParameterSet.Config as cms
 
 from DQM.L1TMonitor.L1TStage2CaloLayer2_cfi import *
-from DQM.L1TMonitor.L1TStage2mGMT_cfi import *
+from DQM.L1TMonitor.L1TStage2uGMT_cfi import *
 from DQM.L1TMonitor.L1TStage2EMTF_cfi import *
 
 from EventFilter.L1TRawToDigi.caloStage2Digis_cfi import *
@@ -10,7 +10,7 @@ from EventFilter.L1TRawToDigi.emtfStage2Digis_cfi import *
 
 l1tStage2online = cms.Sequence(
     l1tStage2CaloLayer2 +
-    l1tStage2mGMT +
+    l1tStage2uGMT +
     l1tStage2Emtf
 )
 
