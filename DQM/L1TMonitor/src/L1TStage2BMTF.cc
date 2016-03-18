c710bd6e905050b7ec82b1797c0167296d8b02da
Sean-Jiun Wang <churchguitarist@gmail.com>
2016-03-18 06:43:05 +0100

    Reorganized BMTF DQM module, prepare for unpacker

--- DQM/L1TMonitor/src/L1TStage2BMTF.cc
+++ DQM/L1TMonitor/src/L1TStage2BMTF.cc
@@ -1,83 +1,84 @@
-/*
- * \L1TStage2BMTF.cc
- * \author Esmaeel Eskandari Tadavani
-*/
-
 #include "DQM/L1TMonitor/interface/L1TStage2BMTF.h"
 
-L1TStage2BMTF::L1TStage2BMTF(const edm::ParameterSet & ps):
-  monitorDir(ps.getUntrackedParameter<std::string>("monitorDir","")),
-  stage2bmtfSource(ps.getParameter<edm::InputTag>("stage2bmtfSource")),
-  verbose(ps.getUntrackedParameter<bool>("verbose", false))
-{
-  stage2bmtfToken=consumes<l1t::RegionalMuonCandBxCollection>(ps.getParameter<edm::InputTag>("stage2bmtfSource"));
-}
 
-L1TStage2BMTF::~L1TStage2BMTF()
-{
-}
+L1TStage2BMTF::L1TStage2BMTF(const edm::ParameterSet& ps)
+    : bmtfToken(consumes<l1t::RegionalMuonCandBxCollection>(ps.getParameter<edm::InputTag>("bmtfSource"))),
+      monitorDir(ps.getUntrackedParameter<std::string>("monitorDir", "")),
+      verbose(ps.getUntrackedParameter<bool>("verbose", false)) {}
 
-void L1TStage2BMTF::dqmBeginRun(const edm::Run& iRrun, const edm::EventSetup& eveSetup)
-{
-}
+L1TStage2BMTF::~L1TStage2BMTF() {}
 
-void L1TStage2BMTF::beginLuminosityBlock(const edm::LuminosityBlock& iLumi, const edm::EventSetup& eveSetup)
-{
-}
+void L1TStage2BMTF::dqmBeginRun(const edm::Run& r, const edm::EventSetup& c) {}
 
+void L1TStage2BMTF::beginLuminosityBlock(const edm::LuminosityBlock&, const edm::EventSetup&) {}
 
-void L1TStage2BMTF::bookHistograms(DQMStore::IBooker &ibooker, const edm::Run& iRun, const edm::EventSetup& eveSetup)
-{
+void L1TStage2BMTF::bookHistograms(DQMStore::IBooker& ibooker, const edm::Run&, const edm::EventSetup&) {
+
   ibooker.setCurrentFolder(monitorDir);
-  
-  // eta ,phi, pt 
-  eta_bmtf = ibooker.book1D("eta_bmtf", "#eta of BMTF", 447, -223.5, 223.5);
-  phi_bmtf = ibooker.book1D("phi_bmtf", "#phi of BMTF", 64, -7.5, 56.5);
-  pt_bmtf  = ibooker.book1D("pt_bmtf" , "p_{T} of BMTF", 511, -0.5, 510.5);
-  bx_bmtf  = ibooker.book1D("bx", "BX", 5, -2.5, 2.5);
-
-  etaVSphi_bmtf = ibooker.book2D("etaVSphi_bmtf", "#eta VS #phi  of BMTF",  447, -223.5, 223.5, 64 , -7.5,   56.5);
-  phiVSpt_bmtf  = ibooker.book2D("phiVSpt_bmtf" , "#phi VS p_{T} of BMTF",  64 ,   -7.5,  56.5, 511, -0.5, 510.5);
-  etaVSpt_bmtf  = ibooker.book2D("etaVSpt_bmtf" , "#eta VS p_{T} of BMTF",  447, -223.5, 223.5, 511, -0.5, 510.5);
-  etaVSbx_bmtf  = ibooker.book2D("etaVSbx_bmtf" , "#eta VS bx of BMTF", 447, -223.5, 223.5,  5, -2.5, 2.5);
-  phiVSbx_bmtf  = ibooker.book2D("phiVSbx_bmtf" , "#phi VS bx of BMTF", 64 ,   -7.5,  56.5,  5, -2.5, 2.5);
-  ptVSbx_bmtf   = ibooker.book2D("ptVSbx_bmtf"  , "p_{T} VS bx of BMTF", 511,   -0.5, 510.5,  5, -2.5, 2.5);
-
-}
-
-void L1TStage2BMTF::analyze(const edm::Event & eve, const edm::EventSetup & eveSetup)
-{
-  if (verbose) {
-    edm::LogInfo("L1TStage2BMTF") << "L1TStage2BMTF: analyze...." << std::endl;
+ 
+  bmtfBX = ibooker.book2D("bmtfBX", "BMTF BX", 12, -0.5, 11.5, 7, -3.5, 3.5);
+  bmtfBX->setAxisTitle("Wedge", 1);
+  for (int bin = 1; bin < 13; ++bin) {
+    bmtfBX->setBinLabel(bin, std::to_string(bin), 1);
   }
-  // analyze Jet
-  edm::Handle<l1t::RegionalMuonCandBxCollection> Muon;
-  eve.getByToken(stage2bmtfToken,Muon);
+  bmtfBX->setAxisTitle("BX", 2);
 
+  bmtfhwPt = ibooker.book1D("bmtfhwPt", "BMTF p_{T}", 512, -0.5, 511.5);
+  bmtfhwPt->setAxisTitle("Hardware p_{T}", 1);
 
-  for(int itBX=Muon->getFirstBX(); itBX<=Muon->getLastBX(); ++itBX)
-    {
-      for(l1t::RegionalMuonCandBxCollection::const_iterator itMuon = Muon->begin(itBX); itMuon != Muon->end(itBX); ++itMuon)
-	{  
-	  eta_bmtf->Fill(itMuon->hwEta());
-	  phi_bmtf->Fill(itMuon->hwPhi());
-	  pt_bmtf->Fill(itMuon->hwPt());
-	  bx_bmtf->Fill(itBX);
+  bmtfhwEta = ibooker.book1D("bmtfhwEta", "BMTF #eta", 461, -230.5, 230.5);
+  bmtfhwEta->setAxisTitle("Hardware #eta", 1);
 
-	  etaVSphi_bmtf->Fill(itMuon->hwEta(),itMuon->hwPhi()); 
-	  phiVSpt_bmtf->Fill(itMuon->hwPhi(),itMuon->hwPt()); 
-	  etaVSpt_bmtf->Fill(itMuon->hwEta(),itMuon->hwPt());
+  bmtfhwPhi = ibooker.book1D("bmtfhwPhi", "BMTF #phi", 201, -100.5, 100.5);
+  bmtfhwPhi->setAxisTitle("Hardware #phi", 1);
 
-	  etaVSbx_bmtf->Fill(itMuon->hwEta(),itBX); 
-	  phiVSbx_bmtf->Fill(itMuon->hwPhi(),itBX); 
-	  ptVSbx_bmtf->Fill(itMuon->hwPt()  ,itBX);  
+  bmtfhwPtvshwEta = ibooker.book2D("bmtfhwPtvshwEta", "BMTF p_{T} vs #eta", 461, -230.5, 230.5, 512, -0.5, 511.5);
+  bmtfhwPtvshwEta->setAxisTitle("Hardware #eta", 1);
+  bmtfhwPtvshwEta->setAxisTitle("Hardware p_{T}", 2);
+  
+  bmtfhwPtvshwPhi = ibooker.book2D("bmtfhwPtvshwPhi", "BMTF p_{T} vs #phi", 201, -100.5, 100.5, 512, -0.5, 511.5);
+  bmtfhwPtvshwPhi->setAxisTitle("Hardware #phi", 1);
+  bmtfhwPtvshwPhi->setAxisTitle("Hardware p_{T}", 2);
 
+  bmtfhwPhivshwEta = ibooker.book2D("bmtfhwPhivshwEta", "BMTF #phi vs #eta", 461, -230.5, 230.5, 201, -100.5, 100.5);
+  bmtfhwPhivshwEta->setAxisTitle("Hardware #eta", 1);
+  bmtfhwPhivshwEta->setAxisTitle("Hardware #phi", 2);
 
-	}
-    }
+  bmtfBXvshwPt = ibooker.book2D("bmtfBXvshwPt", "BMTF BX vs p_{T}", 512, -0.5, 511.5, 7, -3.5, 3.5);
+  bmtfBXvshwPt->setAxisTitle("Hardware p_{T}", 1);
+  bmtfBXvshwPt->setAxisTitle("BX", 2);
+
+  bmtfBXvshwEta = ibooker.book2D("bmtfBXvshwEta", "BMTF BX vs #eta", 461, -230.5, 230.5, 7, -3.5, 3.5);
+  bmtfBXvshwEta->setAxisTitle("Hardware #eta", 1);
+  bmtfBXvshwEta->setAxisTitle("BX", 2);
+
+  bmtfBXvshwPhi = ibooker.book2D("bmtfBXvshwPhi", "BMTF BX vs #phi", 201, -100.5, 100.5, 7, -3.5, 3.5);
+  bmtfBXvshwPhi->setAxisTitle("Hardware #phi", 1);
+  bmtfBXvshwPhi->setAxisTitle("BX", 2);
 }
 
+void L1TStage2BMTF::analyze(const edm::Event& e, const edm::EventSetup& c) {
 
+  if (verbose) edm::LogInfo("L1TStage2BMTF") << "L1TStage2BMTF: analyze..." << std::endl;
 
-  
+  edm::Handle<l1t::RegionalMuonCandBxCollection> BMTFBxCollection;
+  e.getByToken(bmtfToken, BMTFBxCollection);
+
+  for (int itBX = BMTFBxCollection->getFirstBX(); itBX <= BMTFBxCollection->getLastBX(); ++itBX) {
+    for (l1t::RegionalMuonCandBxCollection::const_iterator BMTF = BMTFBxCollection->begin(itBX); BMTF != BMTFBxCollection->end(itBX); ++BMTF) {
+      bmtfBX->Fill(BMTF->processor(), itBX);
+      bmtfhwPt->Fill(BMTF->hwPt());
+      bmtfhwEta->Fill(BMTF->hwEta());
+      bmtfhwPhi->Fill(BMTF->hwPhi());
+      
+      bmtfhwPtvshwEta->Fill(BMTF->hwEta(), BMTF->hwPt());
+      bmtfhwPtvshwPhi->Fill(BMTF->hwPhi(), BMTF->hwPt());
+      bmtfhwPhivshwEta->Fill(BMTF->hwEta(), BMTF->hwPhi());
+
+      bmtfBXvshwPt->Fill(BMTF->hwPt(), itBX);
+      bmtfBXvshwEta->Fill(BMTF->hwEta(), itBX);
+      bmtfBXvshwPhi->Fill(BMTF->hwPhi(), itBX);
+    }
+  }
+}
 
