bbac7761599ede86c70d59d7bf54a0d6683ff3dc
Marcel Schneider <marcel.andre.schneider@cern.ch>
2016-03-18 11:45:57 +0100

    Implement more spec steps, and move the Builder impl out of the header.

--- DQM/SiPixelPhase1Common/interface/SummationSpecification.h
+++ DQM/SiPixelPhase1Common/interface/SummationSpecification.h
@@ -16,12 +16,7 @@
 // Original Author:  Marcel Schneider
 
 #include <vector>
-#include <set>
 #include <string>
-#include <cassert>
-
-// there does not seem to be a standard string tokenizer...
-#include <boost/algorithm/string.hpp>
 
 #include "DQM/SiPixelPhase1Common/interface/GeometryInterface.h"
 
@@ -30,17 +25,15 @@ struct SummationStep {
   // to allow fill() to exectute it very quickly.
   // For step2 stuff (after the first SAVE), we can also keep strings, since 
   // step2 will only be executed once by an executor.
-  enum Type  {NO_TYPE, GROUPBY, EXTEND_X, EXTEND_Y, COUNT, SAVE};
+  enum Type  {NO_TYPE, GROUPBY, EXTEND_X, EXTEND_Y, COUNT, REDUCE, SAVE};
   Type type = NO_TYPE;
   enum Stage {NO_STAGE, FIRST, STAGE1, STAGE2};
   Stage stage = NO_STAGE;
 
   std::vector<GeometryInterface::Column> columns;
   
-  // more parameters. Burning them into the step-functor as a closure seems 
-  // nice but we might want to have them exponsed, since this is not a program
-  // that is just executed sequentially. It is a specification...
-  std::string more_args;
+  // more parameters. Not very elegant but good enough for step2.
+  std::string arg;
 };
 
 struct SummationSpecification {
@@ -59,69 +52,17 @@ struct SummationSpecificationBuilder {
 
   SummationSpecificationBuilder(SummationSpecification& s) : spec(s) {};
 
-  SummationSpecificationBuilder& groupBy(const char* cols, const char* mode = "SUM") {
-    std::vector<std::string> cs;
-    boost::split(cs, cols, boost::is_any_of("/"), boost::token_compress_on);
-    auto step = SummationStep();
-    step.stage = state;
-    step.columns = cs;
-    auto modename = std::string(mode);
-    if (modename == "SUM") {
-      step.type = SummationStep::GROUPBY;
-      activeColums.clear();
-      activeColums.insert(cs.begin(), cs.end());
-    } else if (modename == "EXTEND_X" || modename == "EXTEND_Y") {
-      assert(state != SummationStep::FIRST || !"First statement must have SUM summation.");
-      // use set diff to find removed column and store only that.
-      for (auto x : cs) activeColums.erase(x);
-      if (activeColums.size() > 1 || activeColums.size() == 0) {
-	//TODO: Better logging?
-        assert(!"groupBy with extension has to drop exactly on column");
-      }
-      step.columns.push_back(*activeColums.begin()); // precisely one el.
-      activeColums.clear();
-      activeColums.insert(cs.begin(), cs.end());
-      if (modename == "EXTEND_X") step.type = SummationStep::EXTEND_X;
-      else                    step.type = SummationStep::EXTEND_Y;
-    } else {
-      assert(!"Unsupported summation mode.");
-    }
-
-    if (state == SummationStep::FIRST) state = SummationStep::STAGE1;
-    spec.steps.push_back(step);
-    return *this;
-  }
-
-  SummationSpecificationBuilder& save() {
-    assert(state != SummationStep::FIRST || !"First statement must be groupBy.");
-    auto step = SummationStep();
-    step.stage = state;
-    step.type = SummationStep::SAVE;
-    state = SummationStep::STAGE2;
-    spec.steps.push_back(step);
-    return *this;
-  }
-
-  SummationSpecificationBuilder& count() {
-    assert(state != SummationStep::FIRST || !"First statement must be groupBy.");
-    auto step = SummationStep();
-    step.stage = state;
-    step.type = SummationStep::COUNT;
-    spec.steps.push_back(step);
-    return *this;
-  }
-  // TODO: step2 
-
-  SummationSpecificationBuilder& reduce(std::string sort) {
-    //TODO
-    return *this;
-  }
-
-  SummationSpecificationBuilder& saveAll() {
-    //TODO
-    return save();
-  }
-
+  // General grouping, pass in the columns that should remain and the mode of 
+  // histogram summation.
+  SummationSpecificationBuilder& groupBy(const char* cols, const char* mode = "SUM");
+  // Save the current state of the table as MonitorElements. Marks transition to step2.
+  SummationSpecificationBuilder& save();
+  // Reduce a higher-dimensional hisotgram to a lower (typ. single number) one.
+  SummationSpecificationBuilder& reduce(std::string sort);
+  SummationSpecificationBuilder& count(); // special case of reduce
+  // Save all parents, summed up like in the last grouping, in the hierarchy 
+  // as specified.
+  SummationSpecificationBuilder& saveAll();
 };
 
 
