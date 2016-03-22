724be40d13f1da8ed5a85e1a0f154be3bfe1d44f
Nick Smith <nick.smith@cern.ch>
2016-03-22 14:52:56 +0100

    Fix unused variable warning from llvm

--- DQM/L1TMonitor/src/L1TLayer1.cc
+++ DQM/L1TMonitor/src/L1TLayer1.cc
@@ -15,25 +15,26 @@
 
 // Binning specification
 namespace {
-  const unsigned int TPGETABINS = 57;
-  const float TPGETAMIN = -28.5;
-  const float TPGETAMAX = 28.5;
+  constexpr unsigned int TPGETABINS = 57;
+  constexpr float TPGETAMIN = -28.5;
+  constexpr float TPGETAMAX = 28.5;
 
-  const unsigned int TPETABINSHCAL = 83;
-  const float TPETAMINHCAL = -41.5;
-  const float TPETAMAXHCAL = 41.5;
+  constexpr unsigned int TPETABINSHCAL = 83;
+  constexpr float TPETAMINHCAL = -41.5;
+  constexpr float TPETAMAXHCAL = 41.5;
 
-  const unsigned int TPGPHIBINS = 72;
-  const float TPGPHIMIN = 0.5;
-  const float TPGPHIMAX = 72.5;
+  constexpr unsigned int TPGPHIBINS = 72;
+  constexpr float TPGPHIMIN = 0.5;
+  constexpr float TPGPHIMAX = 72.5;
 
-  const unsigned int TPGEtbins = 255;
-  const float TPGEtMIN = 0.0;
-  const float TPGEtMAX = 255.0;
+  constexpr unsigned int TPGEtbins = 255;
+  constexpr float TPGEtMIN = 0.0;
+  constexpr float TPGEtMAX = 255.0;
 
-  const unsigned int TPGEtbins1 = 510;
-  const float TPGEtMIN1 = -255.0;
-  const float TPGEtMAX1 = 255.0;
+  // Will be used for et difference at some point
+  // constexpr unsigned int TPGEtbins1 = 510;
+  // constexpr float TPGEtMIN1 = -255.0;
+  // constexpr float TPGEtMAX1 = 255.0;
 };
 
 L1TLayer1::L1TLayer1(const edm::ParameterSet & ps) :
