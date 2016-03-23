3b6aabbf49e534ac135c94f817ce4ca84cec0eda
Vincenzo Innocente <vincenzo.innocente@cern.ch>
2016-03-23 16:03:42 +0100

    dummy out hit etaphi estimator

--- TrackingTools/KalmanUpdators/src/EtaPhiMeasurementEstimator.cc
+++ TrackingTools/KalmanUpdators/src/EtaPhiMeasurementEstimator.cc
@@ -10,8 +10,13 @@ std::pair<bool,double>
 EtaPhiMeasurementEstimator::estimate(const TrajectoryStateOnSurface& tsos,
 				   const TrackingRecHit& aRecHit) const {
 
+  // As the center of the plane is in the region, no need to waste time to test the hit as well
+  return std::make_pair(true, 1.0);
+
+  /* HERE AS THE SECOND LINE WAS BUGGY... (AND MAY BE NEEDED IF WE MODIFY THE ESTIMATE WRT PLANE TO ACCOUNT FOR DET SPAN
+
   auto dEta = std::abs(tsos.globalPosition().eta() - aRecHit.globalPosition().eta());
-  auto dPhi = deltaPhi(tsos.globalPosition().barePhi(), aRecHit.globalPosition().barePhi());
+  auto dPhi = std::abs(deltaPhi(tsos.globalPosition().barePhi(), aRecHit.globalPosition().barePhi()));
 
   LogDebug("EtaPhiMeasurementEstimator")<< " The state to compare with is \n"<< tsos
 					<< " The hit position is:\n" << aRecHit.globalPosition()
@@ -21,13 +26,16 @@ EtaPhiMeasurementEstimator::estimate(const TrajectoryStateOnSurface& tsos,
     return std::make_pair(true, 1.0);
   else
     return std::make_pair(false, 0.0);
+
+  */
+
 }
 
 bool EtaPhiMeasurementEstimator::estimate(const TrajectoryStateOnSurface& tsos,
 				   const Plane& plane) const {
 
   auto dEta = std::abs(tsos.globalPosition().eta() - plane.eta());
-  auto dPhi = deltaPhi(tsos.globalPosition().barePhi(), plane.phi());
+  auto dPhi = std::abs(deltaPhi(tsos.globalPosition().barePhi(), plane.phi()));
 
 
   LogDebug("EtaPhiMeasurementEstimator")<< "The state to compare with is \n"<< tsos << "\n"
@@ -35,7 +43,8 @@ bool EtaPhiMeasurementEstimator::estimate(const TrajectoryStateOnSurface& tsos,
 					<< "the deta = " << thedEta << " --- the dPhi = " << thedPhi << "\n"
 					<< "deta = "<< fabs(dEta)<< " --- dPhi = "<<fabs(dPhi);
 
-  return (std::abs(dEta) < thedEta) & (std::abs(dPhi) <thedPhi);
+  // does not take into account bounds... (not sure it was intentional)
+  return (dEta < thedEta) & (dPhi <thedPhi);
 }
 
 MeasurementEstimator::Local2DVector EtaPhiMeasurementEstimator::maximalLocalDisplacement( const TrajectoryStateOnSurface& tsos,
