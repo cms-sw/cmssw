0426ab01653454c4daff24f842b92271cf8c2176
Jan Kašpar <jan.kaspar@cern.ch>
2016-03-24 14:31:34 +0100

    Import from CTPPS repository: conditions formats needed for TOTEM RP raw-to-digi.

---
+++ CondFormats/DataRecord/interface/TotemReadoutRcd.h
@@ -0,0 +1,19 @@
+/****************************************************************************
+*
+* This is a part of TOTEM offline software.
+* Authors: 
+*   Jan Kaspar (jan.kaspar@gmail.com)
+*    
+****************************************************************************/
+
+#ifndef TotemCondFormatsDataRecordTotemDAQMappingRecord_h
+#define TotemCondFormatsDataRecordTotemDAQMappingRecord_h
+
+#include "FWCore/Framework/interface/EventSetupRecordImplementation.h"
+ 
+/**
+ *\brief EventSetup record for TOTEM readout-related information.
+ **/
+class TotemReadoutRcd : public edm::eventsetup::EventSetupRecordImplementation<TotemReadoutRcd> {};
+ 
+#endif
