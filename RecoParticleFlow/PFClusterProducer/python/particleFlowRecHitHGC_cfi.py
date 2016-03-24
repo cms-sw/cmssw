1963e4a5747fd7a5c2d79b2825ceae42c7f685f5
Lindsey Gray <lindsey.gray@gmail.com>
2016-03-24 19:19:18 +0100

    Uncalibrated RecHits are now in MIPs

--- RecoParticleFlow/PFClusterProducer/python/particleFlowRecHitHGC_cfi.py
+++ RecoParticleFlow/PFClusterProducer/python/particleFlowRecHitHGC_cfi.py
@@ -21,30 +21,14 @@ particleFlowRecHitHGC = cms.EDProducer("PFRecHitProducer",
              name = cms.string("PFHGCEERecHitCreator"),
              src  = cms.InputTag("HGCalRecHit:HGCEERecHits"),
              geometryInstance = cms.string("HGCalEESensitive"),
-             qualityTests = cms.VPSet( 
-                cms.PSet(
-                  name = cms.string("PFRecHitQTestThresholdInThicknessNormalizedMIPs"),
-                  thresholdInMIPs = cms.double(0.50),
-                  mipValueInkeV = cms.double(27.55),
-                  recHitEnergyIs_keV = cms.bool(False),
-                  recHitEnergyMultiplier = cms.double(1.0),
-                  geometryInstance = cms.string("HGCalEESensitive")
-                  )
-                )
+             qualityTests = cms.VPSet(  
+                )              
            ),
            cms.PSet(
              name = cms.string("PFHGCHEFRecHitCreator"),
              src  = cms.InputTag("HGCalRecHit:HGCHEFRecHits"),
              geometryInstance = cms.string("HGCalHESiliconSensitive"),
-             qualityTests = cms.VPSet( 
-                cms.PSet(
-                  name = cms.string("PFRecHitQTestThresholdInThicknessNormalizedMIPs"),
-                  thresholdInMIPs = cms.double(0.50),
-                  mipValueInkeV = cms.double(27.55),
-                  recHitEnergyIs_keV = cms.bool(False),
-                  recHitEnergyMultiplier = cms.double(1.0),
-                  geometryInstance = cms.string("HGCalHESiliconSensitive")
-                  )                
+             qualityTests = cms.VPSet(
                 )
            )
     )          
