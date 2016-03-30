9c7b9285f4912b4c5ad5598672c27d9d48885ea0
Geng Chen <normanaachen@gmail.com>
2016-03-30 14:12:43 +0200

    CR-Stand

---
+++ Geometry/MuonCommonData/python/gembox_cfi.py
@@ -0,0 +1,15 @@
+import FWCore.ParameterSet.Config as cms
+
+XMLIdealGeometryESSource = cms.ESSource("XMLIdealGeometryESSource",
+    geomXMLFiles = cms.vstring('Geometry/CMSCommonData/data/materials.xml',
+        'Geometry/CMSCommonData/data/rotations.xml',
+        'Geometry/MuonCommonData/data/v2/gemf.xml',
+        'Geometry/MuonCommonData/data/cosmic1/cms.xml',
+        'Geometry/MuonCommonData/data/cosmic1/gembox.xml',
+        'Geometry/MuonCommonData/data/cosmic1/gem11.xml',
+        'Geometry/MuonCommonData/data/cosmic1/cms.xml',
+        'Geometry/MuonCommonData/data/cosmic1/muonNumbering.xml',
+        'Geometry/MuonCommonData/data/cosmic1/muonSens.xml',
+        'Geometry/MuonCommonData/data/cosmic1/muonProdCuts.xml'),
+    rootNodeName = cms.string('cms:OCMS')
+)
