621cc5c44b227878e7a1cb66c65d3f82a4a8dc0d
Martin Grunewald <Martin.Grunewald@cern.ch>
2016-03-30 10:28:20 +0200

    Fix HLT config

--- HLTrigger/Configuration/python/HLT_PIon_cff.py
+++ HLTrigger/Configuration/python/HLT_PIon_cff.py
@@ -1,11 +1,11 @@
-# /dev/CMSSW_8_0_0/PIon/V39 (CMSSW_8_0_3)
+# /dev/CMSSW_8_0_0/PIon/V40 (CMSSW_8_0_3)
 
 import FWCore.ParameterSet.Config as cms
 
 fragment = cms.ProcessFragment( "HLT" )
 
 fragment.HLTConfigVersion = cms.PSet(
-  tableName = cms.string('/dev/CMSSW_8_0_0/PIon/V39')
+  tableName = cms.string('/dev/CMSSW_8_0_0/PIon/V40')
 )
 
 fragment.HLTPSetInitialStepTrajectoryFilterBase = cms.PSet( 
