8954137b76590b378e9d1ee6c31052a4b75f2e39
Andrew Evans <aevans@physics.umn.edu>
2016-04-01 05:54:00 -0500

    Added unpacking of digi

--- EventFilter/HcalRawToDigi/src/HcalUnpacker.cc
+++ EventFilter/HcalRawToDigi/src/HcalUnpacker.cc
@@ -865,7 +865,10 @@ void HcalUnpacker::unpackUMNio(const FEDRawData& raw, const int& slot, HcalUMNio
   for (int iamc=0; iamc<namc; iamc++) {
     if (amc13->AMCSlot(iamc) == slot) namc = iamc;
   }
-  uint16_t* data = (uint16_t*)(amc13->AMCPayload(namc));
+  const uint16_t* data = (uint16_t*)(amc13->AMCPayload(namc));
   size_t nwords = amc13->AMCSize(namc) * ( sizeof(uint64_t) / sizeof(uint16_t) );
   std::cout << data << nwords << std::endl;
+  std::vector<uint16_t> words;
+  umnio = HcalUMNioDigi(data, nwords);
+  
 }
