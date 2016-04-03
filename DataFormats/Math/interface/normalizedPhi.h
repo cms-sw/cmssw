f105cfad546d0f2b0dcac6092acbb897c3ac2b32
Vincenzo Innocente <vincenzo.innocente@cern.ch>
2016-04-03 16:31:13 +0200

    fix phiRange

--- DataFormats/Math/interface/normalizedPhi.h
+++ DataFormats/Math/interface/normalizedPhi.h
@@ -1,6 +1,7 @@
 #ifndef Math_notmalizedPhi_h
 #define Math_notmalizedPhi_h
 #include "DataFormats/Math/interface/deltaPhi.h"
+#include <algorithm>
 
 // return a value of phi into interval [-pi,+pi]
 template<typename T>
@@ -22,9 +23,11 @@ bool checkPhiInRange(T phi, T phi1, T phi2) {
     constexpr T c1 = 2.*M_PI;
     phi1 = normalizedPhi(phi1);
     phi2 = proxim(phi2,phi1);
+    if (phi2<phi1) std::swap(phi1,phi2);
     // phi & phi1 are in [-pi,pi] range...
-    return ( (phi1 <= phi) && (phi <= phi2) ) ||
-           ( (phi1 <= phi-c1) && (phi-c1 <= phi2) );
+    return ( (phi1 <= phi) & (phi <= phi2) )
+           || ( (phi1 <= phi-c1) & (phi-c1 <= phi2) )
+           || ( (phi1 <= phi+c1) & (phi+c1 <= phi2) );
 }
 
 #endif
