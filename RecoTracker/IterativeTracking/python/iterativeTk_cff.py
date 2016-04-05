7ef63a7e169969a4bcd9188530db478c709aefd4
Matti Kortelainen <matti.kortelainen@cern.ch>
2016-04-06 01:00:22 +0200

    Use non-splitted pixel clusters in trackingLowPU

--- RecoTracker/IterativeTracking/python/iterativeTk_cff.py
+++ RecoTracker/IterativeTracking/python/iterativeTk_cff.py
@@ -40,6 +40,7 @@ iterTracking = cms.Sequence(InitialStepPreSplitting*
 
 from Configuration.StandardSequences.Eras import eras
 eras.trackingLowPU.toReplaceWith(iterTracking, cms.Sequence(
+    InitialStepPreSplitting*
     InitialStep*
     LowPtTripletStep*
     PixelPairStep*
