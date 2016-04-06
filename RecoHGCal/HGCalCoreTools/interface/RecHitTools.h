c0ca457cddf90afe8381a1c0b6d39438f05e86b4
Lindsey Gray <lindsey.gray@gmail.com>
2016-04-06 13:06:26 +0200

    baseline convenience tools for HGCal hits

--- RecoHGCal/HGCalCoreTools/interface/RecHitTools.h
+++ RecoHGCal/HGCalCoreTools/interface/RecHitTools.h
@@ -1,6 +1,7 @@
 #ifndef __RecoHGCal_HGCalCoreTools_RecHitTools_h__
 #define __RecoHGCal_HGCalCoreTools_RecHitTools_h__
 
+#include <array>
 #include <cmath>
 
 class HGCalGeometry;
@@ -15,7 +16,7 @@ namespace edm {
 namespace hgcal {
   class RecHitTools {    
   public:
-    RecHitTools() : geom_(nullptr), ddd_(nullptr) {}
+  RecHitTools() : geom_({ {nullptr,nullptr} }), ddd_({ {nullptr,nullptr} }) {}
     ~RecHitTools() {}
 
     void getEvent(const edm::Event&);
@@ -23,12 +24,12 @@ namespace hgcal {
     
     std::float_t getSiThickness(const DetId&) const;
     std::float_t getRadiusToSide(const DetId&) const;
-
+    
     bool isHalfCell(const DetId&) const;
 
   private:
-    const HGCalGeometry* geom_;
-    const HGCalDDDConstants* ddd_;
+    std::array<const HGCalGeometry*,2>     geom_;
+    std::array<const HGCalDDDConstants*,2> ddd_;
   };
 }
 
