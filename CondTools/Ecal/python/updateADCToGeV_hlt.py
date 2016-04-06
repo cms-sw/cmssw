2f6a053d68ea6136b07ae8752a290aeb477eadbc
Pierre Depasse <depasse@cern.ch>
2016-04-06 18:32:40 +0200

    new DB_Run2

---
+++ CondTools/Ecal/python/updateADCToGeV_hlt.py
@@ -0,0 +1,59 @@
+import FWCore.ParameterSet.Config as cms
+
+process = cms.Process("ProcessOne")
+
+process.MessageLogger = cms.Service("MessageLogger",
+    debugModules = cms.untracked.vstring('*'),
+    cout = cms.untracked.PSet(
+        threshold = cms.untracked.string('DEBUG')
+    ),
+    destinations = cms.untracked.vstring('cout')
+)
+
+process.source = cms.Source("EmptyIOVSource",
+    lastValue = cms.uint64(100000000000),
+    timetype = cms.string('runnumber'),
+    firstValue = cms.uint64(100000000000),
+    interval = cms.uint64(1)
+)
+
+process.load("CondCore.CondDB.CondDB_cfi")
+
+process.CondDB.DBParameters.authenticationPath = ''
+
+#process.CondDB.connect = 'sqlite_file:EcalADCToGeVConstant_V1_hlt.db'
+process.CondDB.connect = 'oracle://cms_orcon_prod/CMS_CONDITIONS'
+
+
+process.PoolDBOutputService = cms.Service("PoolDBOutputService",
+    process.CondDB, 
+    logconnect = cms.untracked.string('oracle://cms_orcon_prod/CMS_COND_31X_POPCONLOG'),
+#   logconnect = cms.untracked.string('sqlite_file:log.db'),   
+        toPut = cms.VPSet(cms.PSet(
+        record = cms.string('EcalADCToGeVConstantRcd'),
+        tag = cms.string('EcalADCToGeVConstant_V1_hlt')
+    ))
+)
+
+process.Test1 = cms.EDAnalyzer("ExTestEcalADCToGeVAnalyzer",
+    record = cms.string('EcalADCToGeVConstantRcd'),
+    loggingOn= cms.untracked.bool(True),
+    IsDestDbCheckedInQueryLog=cms.untracked.bool(True),
+    SinceAppendMode=cms.bool(True),
+    Source=cms.PSet(
+     FileLowField = cms.string('/data/O2O/Ecal/TPG//ADCToGeV_Boff.xml'),
+     FileHighField = cms.string('/data/O2O/Ecal/TPG/ADCToGeV_Bon.xml'),
+     firstRun = cms.string('207149'),
+     lastRun = cms.string('10000000'),
+     OnlineDBSID = cms.string('cms_omds_lb'),
+#     OnlineDBSID = cms.string('cms_orcon_adg'),  test on lxplus
+     OnlineDBUser = cms.string('cms_ecal_r'),
+     OnlineDBPassword = cms.string('3c4l_r34d3r'),
+     LocationSource = cms.string('P5'),
+     Location = cms.string('P5_Co'),
+     GenTag = cms.string('GLOBAL'),
+     RunType = cms.string('COSMICS')
+    )                            
+)
+
+process.p = cms.Path(process.Test1)
---
+++ CondTools/Ecal/python/updateIntercali_express.py
@@ -0,0 +1,62 @@
+import FWCore.ParameterSet.Config as cms
+
+process = cms.Process("ProcessOne")
+
+process.MessageLogger = cms.Service("MessageLogger",
+    debugModules = cms.untracked.vstring('*'),
+    cout = cms.untracked.PSet(
+        threshold = cms.untracked.string('DEBUG')
+    ),
+    destinations = cms.untracked.vstring('cout')
+)
+
+process.source = cms.Source("EmptyIOVSource",
+    lastValue = cms.uint64(100000000000),
+    timetype = cms.string('runnumber'),
+    firstValue = cms.uint64(100000000000),
+    interval = cms.uint64(1)
+)
+
+process.load("CondCore.CondDB.CondDB_cfi")
+
+process.CondDB.DBParameters.authenticationPath = ''
+
+#process.CondDB.connect = 'sqlite_file:EcalIntercalibConstants_V1_express.db'
+process.CondDB.connect = 'oracle://cms_orcon_prod/CMS_CONDITIONS'
+
+
+process.PoolDBOutputService = cms.Service("PoolDBOutputService",
+    process.CondDB, 
+    logconnect = cms.untracked.string('oracle://cms_orcon_prod/CMS_COND_31X_POPCONLOG'),
+#   logconnect = cms.untracked.string('sqlite_file:log.db'),   
+    toPut = cms.VPSet(cms.PSet(
+        record = cms.string('EcalIntercalibConstantsRcd'),
+        tag = cms.string('EcalIntercalibConstants_V1_express')
+    ))
+)
+
+process.Test1 = cms.EDAnalyzer("ExTestEcalIntercalibAnalyzer",
+    record = cms.string('EcalIntercalibConstantsRcd'),
+    loggingOn= cms.untracked.bool(True),
+    IsDestDbCheckedInQueryLog=cms.untracked.bool(True),
+    SinceAppendMode=cms.bool(True),
+    Source=cms.PSet(
+     FileLowField = cms.string('/data/O2O/Ecal/TPG/Intercalib_Boff.xml'),
+     FileHighField = cms.string('/data/O2O/Ecal/TPG/Intercalib_Bon.xml'),
+# Run1 :    Value_Bon = cms.untracked.double(0.76724),
+     Value_Bon = cms.untracked.double(0.7041),
+# March 2016 : IC value for Xtal iX="50" iY="5" iZ="-1"
+     firstRun = cms.string('207149'),
+     lastRun = cms.string('10000000'),
+     OnlineDBSID = cms.string('cms_omds_lb'),
+#     OnlineDBSID = cms.string('cms_orcon_adg'),  test on lxplus
+     OnlineDBUser = cms.string('cms_ecal_r'),
+     OnlineDBPassword = cms.string('3c4l_r34d3r'),
+     LocationSource = cms.string('P5'),
+     Location = cms.string('P5_Co'),
+     GenTag = cms.string('GLOBAL'),
+     RunType = cms.string('COSMICS')
+    )                            
+)
+
+process.p = cms.Path(process.Test1)
