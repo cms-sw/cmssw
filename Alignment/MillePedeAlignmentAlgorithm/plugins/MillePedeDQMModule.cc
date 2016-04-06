39c894870e19655ac47f9d22af710a88eb8623a0
Jan-Frederik Schulte <jschulte@physik.rwth-aachen.de>
2016-04-06 12:41:03 +0200

    modifiy MillePedeDQMModule to store information in a single histogram

--- Alignment/MillePedeAlignmentAlgorithm/plugins/MillePedeDQMModule.cc
+++ Alignment/MillePedeAlignmentAlgorithm/plugins/MillePedeDQMModule.cc
@@ -30,7 +30,8 @@ MillePedeDQMModule
   tYcut_      (mpReaderConfig_.getParameter<double>("tYcut")),
   Zcut_       (mpReaderConfig_.getParameter<double>("Zcut")),
   tZcut_      (mpReaderConfig_.getParameter<double>("tZcut")),
-  maxMoveCut_ (mpReaderConfig_.getParameter<double>("maxMoveCut"))
+  maxMoveCut_ (mpReaderConfig_.getParameter<double>("maxMoveCut")),
+  maxErrorCut_ (mpReaderConfig_.getParameter<double>("maxErrorCut"))  
 {
 }
 
@@ -51,35 +52,12 @@ void MillePedeDQMModule
   booker.cd();
   booker.setCurrentFolder("AlCaReco/SiPixelAli/");
 
-  h_xPos[0] = booker.book1D("Xpos",   "#Delta X;;#mu m", 6, 0, 6.);
-  h_xPos[1] = booker.book1D("Xpos_1", "Xpos_1",          6, 0, 6.);
-  h_xPos[2] = booker.book1D("Xpos_2", "Xpos_2",          6, 0, 6.);
-  h_xPos[3] = booker.book1D("Xpos_3", "Xpos_3",          6, 0, 6.);
-
-  h_xRot[0] = booker.book1D("Xrot",   "#Delta #theta_{X};;#mu rad", 6, 0, 6.);
-  h_xRot[1] = booker.book1D("Xrot_1", "Xrot_1",                     6, 0, 6.);
-  h_xRot[2] = booker.book1D("Xrot_2", "Xrot_2",                     6, 0, 6.);
-  h_xRot[3] = booker.book1D("Xrot_3", "Xrot_3",                     6, 0, 6.);
-
-  h_yPos[0] = booker.book1D("Ypos",   "#Delta Y;;#mu m", 6, 0., 6.);
-  h_yPos[1] = booker.book1D("Ypos_1", "Ypos_1",          6, 0., 6.);
-  h_yPos[2] = booker.book1D("Ypos_2", "Ypos_2",          6, 0., 6.);
-  h_yPos[3] = booker.book1D("Ypos_3", "Ypos_3",          6, 0., 6.);
-
-  h_yRot[0] = booker.book1D("Yrot",   "#Delta #theta_{Y};;#mu rad", 6, 0, 6.);
-  h_yRot[1] = booker.book1D("Yrot_1", "Yrot_1",                     6, 0, 6.);
-  h_yRot[2] = booker.book1D("Yrot_2", "Yrot_2",                     6, 0, 6.);
-  h_yRot[3] = booker.book1D("Yrot_3", "Yrot_3",                     6, 0, 6.);
-
-  h_zPos[0] = booker.book1D("Zpos",   "#Delta Z;;#mu m", 6, 0., 6.);
-  h_zPos[1] = booker.book1D("Zpos_1", "Zpos_1",          6, 0., 6.);
-  h_zPos[2] = booker.book1D("Zpos_2", "Zpos_2",          6, 0., 6.);
-  h_zPos[3] = booker.book1D("Zpos_3", "Zpos_3",          6, 0., 6.);
-
-  h_zRot[0] = booker.book1D("Zrot",   "#Delta #theta_{Z};;#mu rad", 6, 0, 6.);
-  h_zRot[1] = booker.book1D("Zrot_1", "Zrot_1",                     6, 0, 6.);
-  h_zRot[2] = booker.book1D("Zrot_2", "Zrot_2",                     6, 0, 6.);
-  h_zRot[3] = booker.book1D("Zrot_3", "Zrot_3",                     6, 0, 6.);
+  h_xPos = booker.book1D("Xpos",   "#Delta X;;#mu m", 10, 0, 10.);
+  h_xRot = booker.book1D("Xrot",   "#Delta #theta_{X};;#mu rad", 10, 0, 10.);
+  h_yPos = booker.book1D("Ypos",   "#Delta Y;;#mu m", 10, 0., 10.);
+  h_yRot = booker.book1D("Yrot",   "#Delta #theta_{Y};;#mu rad", 10, 0, 10.);
+  h_zPos = booker.book1D("Zpos",   "#Delta Z;;#mu m", 10, 0., 10.);
+  h_zRot = booker.book1D("Zrot",   "#Delta #theta_{Z};;#mu rad", 10, 0, 10.);
 
   booker.cd();
 }
@@ -103,36 +81,34 @@ void MillePedeDQMModule
 void MillePedeDQMModule
 ::fillExpertHistos()
 {
-  fillExpertHisto(h_xPos,  Xcut_, mpReader.getXobs(),  mpReader.getXobsErr());
-  fillExpertHisto(h_xRot, tXcut_, mpReader.getTXobs(), mpReader.getTXobsErr());
 
-  fillExpertHisto(h_yPos,  Ycut_, mpReader.getYobs(),  mpReader.getYobsErr());
-  fillExpertHisto(h_yRot, tYcut_, mpReader.getTYobs(), mpReader.getTYobsErr());
+  fillExpertHisto(h_xPos,  Xcut_, sigCut_, maxMoveCut_, maxErrorCut_, mpReader.getXobs(),  mpReader.getXobsErr());
+  fillExpertHisto(h_xRot, tXcut_, sigCut_, maxMoveCut_, maxErrorCut_, mpReader.getTXobs(), mpReader.getTXobsErr());
 
-  fillExpertHisto(h_zPos,  Zcut_, mpReader.getZobs(),  mpReader.getZobsErr());
-  fillExpertHisto(h_zRot, tZcut_, mpReader.getTZobs(), mpReader.getTZobsErr());
+  fillExpertHisto(h_yPos,  Ycut_, sigCut_, maxMoveCut_, maxErrorCut_, mpReader.getYobs(),  mpReader.getYobsErr());
+  fillExpertHisto(h_yRot, tYcut_, sigCut_, maxMoveCut_, maxErrorCut_, mpReader.getTYobs(), mpReader.getTYobsErr());
+
+  fillExpertHisto(h_zPos,  Zcut_, sigCut_, maxMoveCut_, maxErrorCut_, mpReader.getZobs(),  mpReader.getZobsErr());
+  fillExpertHisto(h_zRot, tZcut_, sigCut_, maxMoveCut_, maxErrorCut_, mpReader.getTZobs(), mpReader.getTZobsErr());
 
 }
 
 void MillePedeDQMModule
-::fillExpertHisto(MonitorElement* histos[], const double cut,
+::fillExpertHisto(MonitorElement* histo, const double cut, const double sigCut, const double maxMoveCut, const double maxErrorCut,
                   std::array<double, 6> obs, std::array<double, 6> obsErr)
 {
-  TH1F* histo_0 = histos[0]->getTH1F();
-  TH1F* histo_1 = histos[1]->getTH1F();
-  TH1F* histo_2 = histos[2]->getTH1F();
-  TH1F* histo_3 = histos[3]->getTH1F();
-
+  TH1F* histo_0 = histo->getTH1F();
+  
   histo_0->SetMinimum(-(maxMoveCut_));
   histo_0->SetMaximum(  maxMoveCut_);
 
   for (size_t i = 0; i < obs.size(); ++i) {
     histo_0->SetBinContent(i+1, obs[i]);
-    histo_1->SetBinContent(i+1, obs[i]);
-    histo_2->SetBinContent(i+1,  cut);
-    histo_3->SetBinContent(i+1, -cut);
-
     histo_0->SetBinError(i+1, obsErr[i]);
-    histo_1->SetBinError(i+1, (histo_1->GetBinContent(i+1) / sigCut_));
   }
+  histo_0->SetBinContent(8,cut);
+  histo_0->SetBinContent(9,sigCut);
+  histo_0->SetBinContent(10,maxMoveCut);
+  histo_0->SetBinContent(11,maxErrorCut);  
+
 }
