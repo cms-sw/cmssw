34eb5a9fcab6b15979b6ba1222a79188e4224034
Thomas Reis <thomas.reis@cern.ch>
2016-04-11 16:17:03 +0200

    Prevent exception if mask has not 'Rx' in the id

--- L1Trigger/L1TCommon/src/mask.cpp
+++ L1Trigger/L1TCommon/src/mask.cpp
@@ -5,14 +5,14 @@ namespace l1t{
 mask::mask(std::string id, std::string procRole)
 {
 	_id = id;
-	_port = boost::lexical_cast<int>(id.substr(id.find("Rx")+2));
+	_port = boost::lexical_cast<int>(id.substr(id.find_last_not_of("0123456789")+1));
 	_procRole = procRole;
 }
 
 void mask::setPort(std::string id)
 {
 	_id = id;
-	_port = boost::lexical_cast<int>(id.substr(id.find("Rx")+2));
+	_port = boost::lexical_cast<int>(id.substr(id.find_last_not_of("0123456789")+1));
 }
 
 }
