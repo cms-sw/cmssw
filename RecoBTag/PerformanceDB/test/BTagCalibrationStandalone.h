81eacc2d5c996345e911b7e2319cb4cbe9985860
Heiner Tholen <heiner.tholen@cern.ch>
2016-04-12 23:13:43 +0200

    BTagCalibrationReader: d-pointer implemention and min_max_pt member function

--- RecoBTag/PerformanceDB/test/BTagCalibrationStandalone.h
+++ RecoBTag/PerformanceDB/test/BTagCalibrationStandalone.h
@@ -145,43 +145,37 @@ protected:
  *
  ************************************************************/
 
-#include <map>
+#include <memory>
 #include <string>
-#include <vector>
-#include <TF1.h>
+
 
 
 class BTagCalibrationReader
 {
 public:
+  class BTagCalibrationReaderImpl;
+
   BTagCalibrationReader() {}
-  BTagCalibrationReader(const BTagCalibration* c,
-                        BTagEntry::OperatingPoint op,
-                        std::string measurementType="comb",
+  BTagCalibrationReader(BTagEntry::OperatingPoint op,
                         std::string sysType="central");
-  ~BTagCalibrationReader() {}
+
+  void load(const BTagCalibration & c,
+            BTagEntry::JetFlavor jf,
+            std::string measurementType="comb");
 
   double eval(BTagEntry::JetFlavor jf,
               float eta,
               float pt,
               float discr=0.) const;
 
-protected:
-  struct TmpEntry {
-    float etaMin;
-    float etaMax;
-    float ptMin;
-    float ptMax;
-    float discrMin;
-    float discrMax;
-    TF1 func;
-  };
-  void setupTmpData(const BTagCalibration* c);
+  std::pair<float, float> min_max_pt(BTagEntry::JetFlavor jf,
+                                     float eta,
+                                     float discr=0.) const;
 
-  BTagEntry::Parameters params;
-  std::map<BTagEntry::JetFlavor, std::vector<TmpEntry> > tmpData_;
-  std::vector<bool> useAbsEta;
+protected:
+  std::auto_ptr<BTagCalibrationReaderImpl> pimpl;
 };
+
 
 #endif  // BTagCalibrationReader_H
 
