b539b2bd90871bbb68f1620f71d776d20d2cf619
Jan Kaspar <jan.kaspar@cern.ch>
2016-04-13 10:38:12 +0200

    Added default constructor to TotemTriggerCounters.

--- DataFormats/TotemDigi/interface/TotemTriggerCounters.h
+++ DataFormats/TotemDigi/interface/TotemTriggerCounters.h
@@ -12,13 +12,23 @@
 /**
  * Trigger counters from LoneG.
 **/
-struct TotemTriggerCounters
+class TotemTriggerCounters
 {
-  unsigned char type;
-  unsigned int event_num, bunch_num, src_id;
-  unsigned int orbit_num;
-  unsigned char revision_num;
-  unsigned int run_num, trigger_num, inhibited_triggers_num, input_status_bits;
+  public:
+    unsigned char type;
+    unsigned int event_num, bunch_num, src_id;
+    unsigned int orbit_num;
+    unsigned char revision_num;
+    unsigned int run_num, trigger_num, inhibited_triggers_num, input_status_bits;
+
+    TotemTriggerCounters() :
+      type(0),
+      event_num(0), bunch_num(0), src_id(0),
+      orbit_num(0),
+      revision_num(0),
+      run_num(0), trigger_num(0), inhibited_triggers_num(0), input_status_bits(0)
+    {
+    } 
 };
 
 #endif
