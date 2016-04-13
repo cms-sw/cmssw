ccedb061dba2533c939a6be2b4f0471fba49bf69
Vincenzo Innocente <vincenzo.innocente@cern.ch>
2016-04-13 16:33:39 +0200

    speed up avoiding eta phi computations

--- RecoParticleFlow/PFClusterProducer/src/Basic2DGenericPFlowPositionCalc.cc
+++ RecoParticleFlow/PFClusterProducer/src/Basic2DGenericPFlowPositionCalc.cc
@@ -4,7 +4,9 @@
 #include "FWCore/Utilities/interface/isFinite.h"
 
 #include <cmath>
-#include <unordered_map>
+#include "CommonTools/Utils/interface/DynArray.h"
+#include<iterator>
+#include <boost/function_output_iterator.hpp>
 
 #include "vdt/vdtMath.h"
 
@@ -31,47 +33,63 @@ calculateAndSetPositionActual(reco::PFCluster& cluster) const {
   double cl_timeweight=0.0;
   double max_e = 0.0;  
   PFLayer::Layer max_e_layer = PFLayer::NONE;
-  reco::PFRecHitRef refseed;  
   // find the seed and max layer and also calculate time
   //Michalis : Even if we dont use timing in clustering here we should fill
   //the time information for the cluster. This should use the timing resolution(1/E)
   //so the weight should be fraction*E^2
   //calculate a simplistic depth now. The log weighted will be done
   //in different stage  
-  for( const reco::PFRecHitFraction& rhf : cluster.recHitFractions() ) {
-    const reco::PFRecHitRef& refhit = rhf.recHitRef();
-    if( refhit->detId() == cluster.seed() ) refseed = refhit;
-    const double rh_fraction = rhf.fraction();
-    const double rh_rawenergy = refhit->energy();
+
+
+  auto const recHitCollection = &(*cluster.recHitFractions()[0].recHitRef()) - cluster.recHitFractions()[0].recHitRef().key();
+  auto nhits = cluster.recHitFractions().size();
+  struct LHit{ reco::PFRecHit const * hit; double energy; double fraction;};
+  declareDynArray(LHit,nhits,hits);
+  for(auto i=0U; i<nhits; ++i) {
+    auto const & hf = cluster.recHitFractions()[i];
+    auto k = hf.recHitRef().key();
+    auto p = recHitCollection+k;
+    hits[i]= {p,(*p).energy(), hf.fraction()}; 
+  }
+
+  if(_posCalcNCrystals != -1) // sorted to make neighbour search faster
+    std::sort(hits.begin(),hits.end(),[](LHit const& a, LHit const& b) { return a.hit<b.hit;});
+
+  LHit mySeed={nullptr}; 
+  for( auto const & rhf : hits ) {
+    const reco::PFRecHit & refhit = *rhf.hit;
+    if( refhit.detId() == cluster.seed() ) mySeed = rhf;
+    const double rh_fraction = rhf.fraction;
+    const double rh_rawenergy = rhf.energy;
     const double rh_energy = rh_rawenergy * rh_fraction;   
     if( edm::isNotFinite(rh_energy) ) {
       throw cms::Exception("PFClusterAlgo")
-	<<"rechit " << refhit->detId() << " has a NaN energy... " 
+	<<"rechit " << refhit.detId() << " has a NaN energy... " 
 	<< "The input of the particle flow clustering seems to be corrupted.";
     }
     cl_energy += rh_energy;
     // If time resolution is given, calculated weighted average
-    if (_timeResolutionCalcBarrel && _timeResolutionCalcEndcap) {
-      double res2 = 10000.;
-      int cell_layer = (int)refhit->layer();
+    if ( bool(_timeResolutionCalcBarrel) & bool(_timeResolutionCalcEndcap) ) {
+      double res2 = 1.e-4;
+      int cell_layer = (int)refhit.layer();
       if (cell_layer == PFLayer::HCAL_BARREL1 ||
           cell_layer == PFLayer::HCAL_BARREL2 ||
           cell_layer == PFLayer::ECAL_BARREL)
-        res2 = _timeResolutionCalcBarrel->timeResolution2(rh_rawenergy);
+        res2 = 1./_timeResolutionCalcBarrel->timeResolution2(rh_rawenergy);
       else
-        res2 = _timeResolutionCalcEndcap->timeResolution2(rh_rawenergy);
-      cl_time += rh_fraction*refhit->time()/res2;
-      cl_timeweight += rh_fraction/res2;
+        res2 = 1./_timeResolutionCalcEndcap->timeResolution2(rh_rawenergy);
+      cl_time += rh_fraction*refhit.time()*res2;
+      cl_timeweight += rh_fraction*res2;
     }
     else { // assume resolution = 1/E**2
       const double rh_rawenergy2 = rh_rawenergy*rh_rawenergy;
       cl_timeweight+=rh_rawenergy2*rh_fraction;
-      cl_time += rh_rawenergy2*rh_fraction*refhit->time();
+      cl_time += rh_rawenergy2*rh_fraction*refhit.time();
     }
 
     if( rh_energy > max_e ) {
       max_e = rh_energy;
-      max_e_layer = rhf.recHitRef()->layer();
+      max_e_layer = refhit.layer();
     }    
   }
   cluster.setEnergy(cl_energy);
@@ -85,38 +103,50 @@ calculateAndSetPositionActual(reco::PFCluster& cluster) const {
   const reco::PFRecHitRefVector* seedNeighbours = nullptr;
   switch( _posCalcNCrystals ) {
   case 5:
-    seedNeighbours = &refseed->neighbours4();
+    seedNeighbours = &mySeed.hit->neighbours4();
     break;
   case 9:
-    seedNeighbours = &refseed->neighbours8();
-    break;
-  case -1:
+    seedNeighbours = &mySeed.hit->neighbours8();
     break;
   default:
-    assert(0); //bug
+    break;
   }
 
-  for( const reco::PFRecHitFraction& rhf : cluster.recHitFractions() ) {
-    const reco::PFRecHitRef& refhit = rhf.recHitRef();
-    
-    if( refhit != refseed && _posCalcNCrystals != -1 ) {
-      auto pos = std::find(seedNeighbours->begin(),seedNeighbours->end(),
-			   refhit);
-      if( pos == seedNeighbours->end() ) continue;
-    }
-    
-    const double rh_energy = refhit->energy() * ((float)rhf.fraction());
-    const double norm = ( rhf.fraction() < _minFractionInCalc ? 
+  auto compute = [&](LHit const& rhf) {
+    const reco::PFRecHit & refhit = *rhf.hit;  
+    const double rh_energy = rhf.energy * rhf.fraction;
+    const double norm = ( rhf.fraction < _minFractionInCalc ? 
 			  0.0 : 
-			  std::max(0.0,vdt::fast_log(rh_energy/_logWeightDenom)) );
-    const math::XYZPoint& rhpos_xyz = refhit->position();
+			  std::max(0.0,vdt::fast_log(rh_energy*_logWeightDenom)) );
+    const math::XYZPoint& rhpos_xyz = refhit.position();
     x += rhpos_xyz.X() * norm;
     y += rhpos_xyz.Y() * norm;
     z += rhpos_xyz.Z() * norm;
-    depth += refhit->depth()*norm;
-    
+    depth += refhit.depth()*norm;
     position_norm += norm;
+  };
+
+  if(_posCalcNCrystals == -1)
+    for( auto const & rhf : hits ) compute(rhf);
+  else {  // only seed and its neighbours
+     compute(mySeed);
+     // search seedNeighbours to find energy fraction in cluster (sic)
+     unInitDynArray(reco::PFRecHit const *,seedNeighbours->size(),nei);	  
+     for(auto k :seedNeighbours->refVector().keys()){ 
+      nei.push_back(&recHitCollection[k]);
+     }
+     std::sort(nei.begin(),nei.end());
+     struct LHitLess {
+       auto operator()(LHit const &a, reco::PFRecHit const * b) const {return a.hit<b;}
+       auto operator()(reco::PFRecHit const * b, LHit const &a) const {return b<a.hit;}
+     };
+     std::set_intersection(hits.begin(),hits.end(),nei.begin(),nei.end(), 
+       boost::make_function_output_iterator(compute),
+       LHitLess()
+     );
   }
+
+
   if( position_norm < _minAllowedNorm ) {
     edm::LogError("WeirdClusterNormalization") 
       << "PFCluster too far from seeding cell: set position to (0,0,0).";
