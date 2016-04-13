f0a06593b8f4f4653623aea63c007df2d6bfdb16
James Dolen <james.dolen@gmail.com>
2016-04-13 08:08:56 -0500

    Fixing to run 80x algo

--- RecoJets/JetProducers/python/PileupJetID_cfi.py
+++ RecoJets/JetProducers/python/PileupJetID_cfi.py
@@ -11,7 +11,7 @@ _chsalgos_74x = cms.VPSet(full_74x_chs,cutbased)
 _chsalgos_76x = cms.VPSet(full_76x_chs,cutbased)
 _chsalgos_80x = cms.VPSet(full_80x_chs,cutbased)
 
-_stdalgos    = _chsalgos_76x
+_stdalgos    = _chsalgos_80x
 
 # Calculate+store variables and run MVAs
 pileupJetId = cms.EDProducer('PileupJetIdProducer',
