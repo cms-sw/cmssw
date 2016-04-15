44426684bfb6523566d670a00817c2c2d3ed7bde
Mia <mia.tosi@cern.ch>
2016-04-15 16:20:05 +0200

    fix L1

--- DQM/SiStripMonitorClient/python/pset4GenericTriggerEventFlag_cfi.py
+++ DQM/SiStripMonitorClient/python/pset4GenericTriggerEventFlag_cfi.py
@@ -16,5 +16,6 @@ genericTriggerEventFlag4L1bd = cms.PSet(
    l1Algorithms  = cms.vstring(""), # cms.vstring( 'L1Tech_BPTX_plus_AND_minus.v0', 'L1_ZeroBias' )
    andOrL1       = cms.bool( True ),
    errorReplyL1  = cms.bool( True ),
+   l1DBKey       = cms.string("SiStrip_L1"),
    l1BeforeMask  = cms.bool( True ) # specifies, if the L1 algorithm decision should be read as before (true) or after (false) masking is applied.
 )
