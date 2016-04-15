96a425aa96ff66c2e5024314e2c4123c02013e48
Martin Grunewald <Martin.Grunewald@cern.ch>
2016-04-15 17:22:29 +0200

    Migration to L1T v5 menu

--- HLTrigger/Configuration/test/OnLine_HLT_PIon.py
+++ HLTrigger/Configuration/test/OnLine_HLT_PIon.py
@@ -1,11 +1,11 @@
-# /dev/CMSSW_8_0_0/PIon/V57 (CMSSW_8_0_4)
+# /dev/CMSSW_8_0_0/PIon/V58 (CMSSW_8_0_4)
 
 import FWCore.ParameterSet.Config as cms
 
 process = cms.Process( "HLTPIon" )
 
 process.HLTConfigVersion = cms.PSet(
-  tableName = cms.string('/dev/CMSSW_8_0_0/PIon/V57')
+  tableName = cms.string('/dev/CMSSW_8_0_0/PIon/V58')
 )
 
 process.HLTPSetInitialStepTrajectoryFilterBase = cms.PSet( 
