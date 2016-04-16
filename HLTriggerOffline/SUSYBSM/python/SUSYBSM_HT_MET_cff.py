c66bc71e4c19fb6b7e272bdd7ef1528b0bf6f262
olivito <dominick.olivito@gmail.com>
2016-04-15 21:04:00 -0700

    updating DQM configs for 30 GeV jets in HT paths

--- HLTriggerOffline/SUSYBSM/python/SUSYBSM_HT_MET_cff.py
+++ HLTriggerOffline/SUSYBSM/python/SUSYBSM_HT_MET_cff.py
@@ -8,8 +8,8 @@ SUSY_HLT_HT350_MET100 = cms.EDAnalyzer("SUSY_HLT_InclusiveHT",
   TriggerResults = cms.InputTag('TriggerResults','','HLT'),
   TriggerPath = cms.string('HLT_PFHT350_PFMET100_v'),
   TriggerPathAuxiliaryForHadronic = cms.string('HLT_IsoMu24_eta2p1_v'),
-  TriggerFilter = cms.InputTag('hltPFHT350', '', 'HLT'), #the last filter in the path
-  PtThrJet = cms.untracked.double(40.0),
+  TriggerFilter = cms.InputTag('hltPFHT350Jet30', '', 'HLT'), #the last filter in the path
+  PtThrJet = cms.untracked.double(30.0),
   EtaThrJet = cms.untracked.double(3.0)
 )
 
@@ -30,8 +30,8 @@ SUSY_HLT_HT300_MET100 = cms.EDAnalyzer("SUSY_HLT_InclusiveHT",
   TriggerResults = cms.InputTag('TriggerResults','','HLT'),
   TriggerPath = cms.string('HLT_PFHT300_PFMET100_v'),
   TriggerPathAuxiliaryForHadronic = cms.string('HLT_IsoMu24_eta2p1_v'),
-  TriggerFilter = cms.InputTag('hltPFHT300', '', 'HLT'), #the last filter in the path
-  PtThrJet = cms.untracked.double(40.0),
+  TriggerFilter = cms.InputTag('hltPFHT300Jet30', '', 'HLT'), #the last filter in the path
+  PtThrJet = cms.untracked.double(30.0),
   EtaThrJet = cms.untracked.double(3.0)
 )
 
@@ -52,8 +52,8 @@ SUSY_HLT_HT300_MET110 = cms.EDAnalyzer("SUSY_HLT_InclusiveHT",
   TriggerResults = cms.InputTag('TriggerResults','','HLT'),
   TriggerPath = cms.string('HLT_PFHT300_PFMET110_v'),
   TriggerPathAuxiliaryForHadronic = cms.string('HLT_IsoMu24_eta2p1_v'),
-  TriggerFilter = cms.InputTag('hltPFHT300', '', 'HLT'), #the last filter in the path
-  PtThrJet = cms.untracked.double(40.0),
+  TriggerFilter = cms.InputTag('hltPFHT300Jet30', '', 'HLT'), #the last filter in the path
+  PtThrJet = cms.untracked.double(30.0),
   EtaThrJet = cms.untracked.double(3.0)
 )
 
