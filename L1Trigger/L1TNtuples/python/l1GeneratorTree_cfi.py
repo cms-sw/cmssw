7aa433520f4d5315b6911b320ef4ec16e92edab6
Jim <jim.brooke@cern.ch>
2016-04-18 23:49:41 +0100

    bugfixes and naming corrections for gen info in ntuples

--- L1Trigger/L1TNtuples/python/l1GeneratorTree_cfi.py
+++
@@ -1,8 +0,0 @@
-import FWCore.ParameterSet.Config as cms
-
-l1GeneratorTree = cms.EDAnalyzer(
-    "L1EventTreeProducer",
-    genJetToken     = cms.untracked.InputTag("ak4GenJets"),
-    geParticleToken = cms.untracked.InputTag("genParticles"),
-    puInfoToken     = cms.untracked.InputTag("addPileupInfo")
-)
