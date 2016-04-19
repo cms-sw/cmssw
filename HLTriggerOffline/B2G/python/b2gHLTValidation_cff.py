a74e70891a8b9055c9242370b43500dbb0ed8e9a
Thiago <Thiago.Tomei@cern.ch>
2016-04-19 13:27:28 +0200

    DQM for B2G Ele + Single Jet paths 2016 - backport 80X

--- HLTriggerOffline/B2G/python/b2gHLTValidation_cff.py
+++ HLTriggerOffline/B2G/python/b2gHLTValidation_cff.py
@@ -7,6 +7,7 @@ from HLTriggerOffline.B2G.b2gHadronicHLTEventValidation_cfi import *
 b2gHLTriggerValidation = cms.Sequence(  
     b2gSingleMuonHLTValidation*
     b2gSingleElectronHLTValidation*
+    b2gElePlusSingleJetHLTValidation*
     b2gSingleJetHLTValidation*
     b2gDiJetHLTValidation
     )
