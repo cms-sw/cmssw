40fd7f83c38133308220931061a64d64eee7658f
Nick Smith <nick.smith@cern.ch>
2016-04-20 18:54:24 +0200

    L1 DQM Fix loose ends from merge
    Correct plot naming for EMTF emulator
    Move hardware validation sequence to L1TStage2Emulator_cff for now

--- DQM/L1TMonitor/python/L1TdeStage2CaloLayer1_cfi.py
+++ DQM/L1TMonitor/python/L1TdeStage2CaloLayer1_cfi.py
@@ -1,19 +1,7 @@
 import FWCore.ParameterSet.Config as cms
 
-from L1Trigger.L1TCaloLayer1.simCaloStage2Layer1Digis_cfi import simCaloStage2Layer1Digis
-valCaloStage2Layer1Digis = simCaloStage2Layer1Digis.clone()
-valCaloStage2Layer1Digis.ecalToken = cms.InputTag("l1tCaloLayer1Digis")
-valCaloStage2Layer1Digis.hcalToken = cms.InputTag("l1tCaloLayer1Digis")
-valCaloStage2Layer1Digis.unpackEcalMask = cms.bool(True)
-valCaloStage2Layer1Digis.unpackHcalMask = cms.bool(True)
-
 l1tdeStage2CaloLayer1 = cms.EDAnalyzer("L1TdeStage2CaloLayer1",
     dataSource = cms.InputTag("caloStage2Digis", "CaloTower"),
     emulSource = cms.InputTag("valCaloStage2Layer1Digis"),
     histFolder = cms.string('L1T2016EMU/L1TdeStage2CaloLayer1'),
 )
-
-l1tLayer1ValSequence = cms.Sequence(
-    valCaloStage2Layer1Digis +
-    l1tdeStage2CaloLayer1
-    )
--- DQM/L1TMonitor/python/L1TdeStage2EMTF_cfi.py
+++ DQM/L1TMonitor/python/L1TdeStage2EMTF_cfi.py
@@ -4,7 +4,7 @@ l1tdeStage2Emtf = cms.EDAnalyzer(
     "L1TdeStage2EMTF",
     dataProducer = cms.InputTag("emtfStage2Digis"),
     emulProducer = cms.InputTag("valEmtfStage2Digis", "EMTF"),
-    monitorDir = cms.untracked.string("L1TEMU2016/L1TdeStage2EMTF"),
+    monitorDir = cms.untracked.string("L1T2016EMU/L1TdeStage2EMTF"),
     verbose = cms.untracked.bool(False),
 )
 
