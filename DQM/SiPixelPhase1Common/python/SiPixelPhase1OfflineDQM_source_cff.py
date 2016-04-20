69a9e14bf834c216b10ba60d08a5c93d6fec326b
Marcel Andre Schneider <marcel.andre.schneider@cern.ch>
2016-04-20 17:20:00 +0200

    Enable the new Plugin
    The Harvesting config now includes the Source config, which is good to keep consistence but might cause other problems.

--- DQM/SiPixelPhase1Common/python/SiPixelPhase1OfflineDQM_source_cff.py
+++ DQM/SiPixelPhase1Common/python/SiPixelPhase1OfflineDQM_source_cff.py
@@ -4,7 +4,9 @@ from DQM.SiPixelPhase1Common.MessageLogger_cfi import *
 
 # Pixel Digi Monitoring
 from DQM.SiPixelPhase1Digis.SiPixelPhase1Digis_cfi import *
+# Cluster (track-independent) monitoring
+from DQM.SiPixelPhase1Clusters.SiPixelPhase1Clusters_cfi import *
 
 PerModule.enabled = True
 
-siPixelPhase1OfflineDQM_source = cms.Sequence(SiPixelPhase1DigisAnalyzer)
+siPixelPhase1OfflineDQM_source = cms.Sequence(SiPixelPhase1DigisAnalyzer + SiPixelPhase1ClustersAnalyzer)
