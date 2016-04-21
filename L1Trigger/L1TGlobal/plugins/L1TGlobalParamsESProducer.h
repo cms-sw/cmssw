5fb66b0c0a5275e3c49dddf877ea14b024d113eb
Michael <michaeljmulhearn@gmail.com>
2016-04-21 16:52:02 +0200

    Update L1TGlobal plugins to use Params Helper class

--- L1Trigger/L1TGlobal/plugins/L1TGlobalParamsESProducer.h
+++ L1Trigger/L1TGlobal/plugins/L1TGlobalParamsESProducer.h
@@ -30,8 +30,7 @@
 #include "FWCore/Framework/interface/ModuleFactory.h"
 #include "FWCore/Framework/interface/ESHandle.h"
 
-#include "CondFormats/L1TObjects/interface/L1TGlobalParameters.h"
-#include "CondFormats/DataRecord/interface/L1TGlobalParametersRcd.h"
+#include "L1Trigger/L1TGlobal/interface/GlobalParamsHelper.h"
 
 // forward declarations
 
@@ -49,69 +48,14 @@ public:
 
     /// public methods
 
+    typedef boost::shared_ptr<L1TGlobalParameters> ReturnType;
+
     /// L1 GT parameters
-    boost::shared_ptr<L1TGlobalParameters> produceGtStableParameters(
-        const L1TGlobalParametersRcd&);
+    ReturnType produce(const L1TGlobalParametersRcd&);
 
 private:
 
-
-    /// bx in event
-    int m_totalBxInEvent; 
-
-    /// trigger decision
-
-    /// number of physics trigger algorithms
-    unsigned int m_numberPhysTriggers;
-
-
-    /// trigger objects
-
-    /// muons
-    unsigned int m_numberL1Mu;
-
-    /// e/gamma and isolated e/gamma objects
-    unsigned int m_numberL1EG;
-
-
-    /// jets
-    unsigned int m_numberL1Jet;
-    
-    /// tau
-    unsigned int m_numberL1Tau;
-
-
-private:
-
-    /// hardware
-
-    /// number of maximum chips defined in the xml file
-    unsigned int m_numberChips;
-
-    /// number of pins on the GTL condition chips
-    unsigned int m_pinsOnChip;
-
-    /// correspondence "condition chip - GTL algorithm word" in the hardware
-    /// e.g.: chip 2: 0 - 95;  chip 1: 96 - 128 (191)
-    std::vector<int> m_orderOfChip;
-/*
-    /// number of PSB boards in GT
-    int m_numberPsbBoards;
-
-    /// number of bits for eta of calorimeter objects
-    unsigned int m_ifCaloEtaNumberBits;
-
-    /// number of bits for eta of muon objects
-    unsigned int m_ifMuEtaNumberBits;
-
-private:
-
-    /// GT DAQ record organized in words of WordLength bits
-    int m_wordLength;
-
-    /// one unit in the word is UnitLength bits
-    int m_unitLength;
-*/
+    l1t::GlobalParamsHelper data_;
 };
 
 #endif
