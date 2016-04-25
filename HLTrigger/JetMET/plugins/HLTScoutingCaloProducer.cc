25ea639141c2a4ddf38c2dd6eda5f274ff809551
Dustin <dustin.james.anderson@cern.ch>
2016-04-25 16:37:18 +0200

    Scouting calo jets can now store b-tag information

--- HLTrigger/JetMET/plugins/HLTScoutingCaloProducer.cc
+++ HLTrigger/JetMET/plugins/HLTScoutingCaloProducer.cc
@@ -29,9 +29,12 @@ Description: Producer for ScoutingCaloJets from reco::CaloJet objects
 #include "DataFormats/VertexReco/interface/VertexFwd.h"
 #include "DataFormats/METReco/interface/CaloMETCollection.h"
 #include "DataFormats/METReco/interface/CaloMET.h"
+#include "DataFormats/BTauReco/interface/JetTag.h"
 
 #include "DataFormats/Scouting/interface/ScoutingCaloJet.h"
 #include "DataFormats/Scouting/interface/ScoutingVertex.h"
+
+#include "DataFormats/Math/interface/deltaR.h"
 
 class HLTScoutingCaloProducer : public edm::global::EDProducer<> {
     public:
@@ -44,6 +47,7 @@ class HLTScoutingCaloProducer : public edm::global::EDProducer<> {
         virtual void produce(edm::StreamID sid, edm::Event & iEvent, edm::EventSetup const & setup) const override final;
 
         const edm::EDGetTokenT<reco::CaloJetCollection> caloJetCollection_;
+        const edm::EDGetTokenT<reco::JetTagCollection> caloJetTagCollection_;
         const edm::EDGetTokenT<reco::VertexCollection> vertexCollection_;
         const edm::EDGetTokenT<reco::CaloMETCollection> metCollection_;
         const edm::EDGetTokenT<double> rho_;
@@ -52,6 +56,7 @@ class HLTScoutingCaloProducer : public edm::global::EDProducer<> {
         const double caloJetEtaCut;
 
         const bool doMet;
+        const bool doJetTags;
 };
 
 //
@@ -59,12 +64,14 @@ class HLTScoutingCaloProducer : public edm::global::EDProducer<> {
 //
 HLTScoutingCaloProducer::HLTScoutingCaloProducer(const edm::ParameterSet& iConfig):
     caloJetCollection_(consumes<reco::CaloJetCollection>(iConfig.getParameter<edm::InputTag>("caloJetCollection"))),
+    caloJetTagCollection_(consumes<reco::JetTagCollection>(iConfig.getParameter<edm::InputTag>("caloJetTagCollection"))),
     vertexCollection_(consumes<reco::VertexCollection>(iConfig.getParameter<edm::InputTag>("vertexCollection"))),
     metCollection_(consumes<reco::CaloMETCollection>(iConfig.getParameter<edm::InputTag>("metCollection"))),
     rho_(consumes<double>(iConfig.getParameter<edm::InputTag>("rho"))),
     caloJetPtCut(iConfig.getParameter<double>("caloJetPtCut")),
     caloJetEtaCut(iConfig.getParameter<double>("caloJetEtaCut")),
-    doMet(iConfig.getParameter<bool>("doMet"))
+    doMet(iConfig.getParameter<bool>("doMet")),
+    doJetTags(iConfig.getParameter<bool>("doJetTags"))
 {
     //register products
     produces<ScoutingCaloJetCollection>();
@@ -87,14 +94,33 @@ HLTScoutingCaloProducer::produce(edm::StreamID sid, edm::Event & iEvent, edm::Ev
     Handle<reco::CaloJetCollection> caloJetCollection;
     std::auto_ptr<ScoutingCaloJetCollection> outCaloJets(new ScoutingCaloJetCollection());
     if(iEvent.getByToken(caloJetCollection_, caloJetCollection)){
+        //get jet tags
+        Handle<reco::JetTagCollection> caloJetTagCollection;
+        bool haveJetTags = false;
+        if(doJetTags && iEvent.getByToken(caloJetTagCollection_, caloJetTagCollection)){
+            haveJetTags = true;
+        }
+
         for(auto &jet : *caloJetCollection){
             if(jet.pt() > caloJetPtCut && fabs(jet.eta()) < caloJetEtaCut){
+                //find the jet tag corresponding to the jet
+                float tagValue = -20;
+                float minDR2 = 0.01;
+                if(haveJetTags){
+                    for(auto &tag : *caloJetTagCollection){
+                        float dR2 = reco::deltaR2(jet, *(tag.first));
+                        if(dR2 < minDR2){
+                            minDR2 = dR2;
+                            tagValue = tag.second;
+                        }
+                    }
+                }
                 outCaloJets->emplace_back(
                         jet.pt(), jet.eta(), jet.phi(), jet.mass(),
                         jet.jetArea(), jet.maxEInEmTowers(), jet.maxEInHadTowers(),
                         jet.hadEnergyInHB(), jet.hadEnergyInHE(), jet.hadEnergyInHF(),
                         jet.emEnergyInEB(), jet.emEnergyInEE(), jet.emEnergyInHF(),
-                        jet.towersArea(), 0.0
+                        jet.towersArea(), 0.0, tagValue
                         );
             }
         }
@@ -141,12 +167,14 @@ void
 HLTScoutingCaloProducer::fillDescriptions(edm::ConfigurationDescriptions& descriptions) {
     edm::ParameterSetDescription desc;
     desc.add<edm::InputTag>("caloJetCollection",edm::InputTag("hltAK4CaloJets"));
+    desc.add<edm::InputTag>("caloJetTagCollection",edm::InputTag("hltCombinedSecondaryVertexBJetTagsCalo"));
     desc.add<edm::InputTag>("vertexCollection", edm::InputTag("hltPixelVertices"));
     desc.add<edm::InputTag>("metCollection", edm::InputTag("hltMet"));
     desc.add<edm::InputTag>("rho", edm::InputTag("hltFixedGridRhoFastjetAllCalo"));
     desc.add<double>("caloJetPtCut", 20.0);
     desc.add<double>("caloJetEtaCut", 3.0);
     desc.add<bool>("doMet", true);
+    desc.add<bool>("doJetTags", false);
     descriptions.add("hltScoutingCaloProducer", desc);
 }
 
