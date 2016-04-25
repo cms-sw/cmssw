71e23468625c2dcf14a2cfe78b9f4feda08cfb3c
Vincenzo Innocente <vincenzo.innocente@cern.ch>
2016-04-25 10:07:42 +0200

    at the end this version is the fastest
    Conflicts:
    	RecoPixelVertexing/PixelTrackFitting/src/PixelTrackCleanerBySharedHits.cc

--- RecoPixelVertexing/PixelTrackFitting/src/PixelTrackCleanerBySharedHits.cc
+++ RecoPixelVertexing/PixelTrackFitting/src/PixelTrackCleanerBySharedHits.cc
@@ -4,7 +4,6 @@
 #include "DataFormats/TrackReco/interface/Track.h"
 #include "FWCore/MessageLogger/interface/MessageLogger.h"
 #include "DataFormats/TrackerCommon/interface/TrackerTopology.h"
-#include <boost/function_output_iterator.hpp>
 
 using namespace std;
 using namespace reco;
@@ -16,36 +15,6 @@ PixelTrackCleanerBySharedHits::PixelTrackCleanerBySharedHits( const edm::Paramet
 PixelTrackCleanerBySharedHits::~PixelTrackCleanerBySharedHits()
 {}
 
-namespace {
-  inline
-  bool recHitsLess(const TrackingRecHit *recHit1, const TrackingRecHit *recHit2) {
-    return recHit1->geographicalId() < recHit2->geographicalId();
-  }
-  inline 
-  bool recHitsEqu(const TrackingRecHit *recHit1, const TrackingRecHit *recHit2) {
-    return recHit1 ==recHit2;
-  }
-
-template<class InputIt1, class InputIt2, class Compare, class Equal>
-unsigned int count_intersection(InputIt1 first1, InputIt1 last1,
-                               InputIt2 first2, InputIt2 last2,
-                               Compare comp, Equal equ)
-{    
-    unsigned int n=0;
-    while (first1 != last1 && first2 != last2) {
-        if (comp(*first1, *first2)) {
-            ++first1;
-        } else {
-            if (!comp(*first2, *first1)) {
-                if (equ(*first1++,*first2)) ++n;
-            }
-            ++first2;
-        }
-    }
-    return n;
-}
-
-}
 
 void PixelTrackCleanerBySharedHits::cleanTracks(TracksWithTTRHs & trackHitPairs,
                                         const TrackerTopology *tTopo) const 
@@ -63,32 +32,21 @@ void PixelTrackCleanerBySharedHits::cleanTracks(TracksWithTTRHs & trackHitPairs,
     if (!track1) continue;
 
     auto const & recHits1 = trackHitPairs[iTrack1].second;
-    auto f1 = recHits1.data();
     auto s1 = recHits1.size();
-    auto e1 = f1+s1;
-    /*
-    {
-      auto f2 = recHits1.data();
-      auto s2 = recHits1.size();
-      auto e2 = f2+s1;
-      auto commonRecHits = 
-      count_intersection(f1,e1,f2,e2,recHitsLess,recHitsEqu);
-      assert(commonRecHits==s2);
-    }
-    */
-
-    // for (auto iRecHit1 = 1U; iRecHit1 < s1; ++iRecHit1) assert(recHitsLess(recHits1[iRecHit1-1],recHits1[iRecHit1]));
-
     for (auto iTrack2 = iTrack1 + 1U; iTrack2 < size; iTrack2++)
     {
       auto track2 = trackHitPairs[iTrack2].first;
       if (!track2) continue;
       auto const & recHits2 = trackHitPairs[iTrack2].second;
-      auto f2 = recHits2.data();
       auto s2 = recHits2.size();
-      auto e2 = f2+s2;
-      auto commonRecHits =    
-        count_intersection(f1,e1,f2,e2,recHitsLess,recHitsEqu);
+      auto f2=0U;
+      auto commonRecHits = 0U;
+      for (auto iRecHit1 = 0U; iRecHit1 < s1; ++iRecHit1) {
+        for (auto iRecHit2 = f2; iRecHit2 < s2; ++iRecHit2) {
+          if (recHits1[iRecHit1] == recHits2[iRecHit2]) { ++commonRecHits; f2=iRecHit2+1; break;} // if a hit is common, no other can be the same!
+        }
+	if (commonRecHits > 1) break;
+      }
       
       if (commonRecHits > 1) {
 	if (track1->pt() > track2->pt()) kill(iTrack2);
