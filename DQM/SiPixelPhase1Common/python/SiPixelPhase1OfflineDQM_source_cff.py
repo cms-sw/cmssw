552a6d4ccdce13d3278df1baaf6851980edbd0a8
Marcel Andre Schneider <marcel.andre.schneider@cern.ch>
2016-04-28 11:06:16 +0200

    Remove the custom MessageLogger.
    The debug log is not very useful and a performance problem anyways. This also suppresses the 'Unexpected number of APV pairs' warning.

--- DQM/SiPixelPhase1Common/python/SiPixelPhase1OfflineDQM_source_cff.py
+++ DQM/SiPixelPhase1Common/python/SiPixelPhase1OfflineDQM_source_cff.py
@@ -1,7 +1,5 @@
 import FWCore.ParameterSet.Config as cms
 
-from DQM.SiPixelPhase1Common.MessageLogger_cfi import *
-
 # Pixel Digi Monitoring
 from DQM.SiPixelPhase1Digis.SiPixelPhase1Digis_cfi import *
 # Cluster (track-independent) monitoring
