3c4593964ff3a5935445405996d7c7cf3f67fd25
Marcel Andre Schneider <marcel.andre.schneider@cern.ch>
2016-04-28 13:49:36 +0200

    Remove the Common (Code) -> Plugin -> Common (Config) dependency cycle.
    This introduces a separate package for the config, which is not yet
    common in Tracker DQM but seems like a good idea.

--- DQM/SiPixelPhase1Config/python/SiPixelPhase1OfflineDQM_harvesting_cff.py
+++ DQM/SiPixelPhase1Config/python/SiPixelPhase1OfflineDQM_harvesting_cff.py
@@ -1,5 +1,5 @@
 import FWCore.ParameterSet.Config as cms
 
-from  DQM.SiPixelPhase1Common.SiPixelPhase1OfflineDQM_source_cff import *
+from  DQM.SiPixelPhase1Config.SiPixelPhase1OfflineDQM_source_cff import *
 
 siPixelPhase1OfflineDQM_harvesting = cms.Sequence(SiPixelPhase1DigisHarvester + SiPixelPhase1ClustersHarvester)
