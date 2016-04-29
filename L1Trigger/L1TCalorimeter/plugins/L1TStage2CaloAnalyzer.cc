a8f038f99f157e52919d7d9a39d2006377d1baca
Nick Smith <nick.smith@cern.ch>
2016-04-29 17:11:37 +0200

    Disentangle CaloParams for Stage1 and Stage2 Trigger Upgrade
    Introduces new DataRecord L1TCaloStage2ParamsRcd

--- L1Trigger/L1TCalorimeter/plugins/L1TStage2CaloAnalyzer.cc
+++ L1Trigger/L1TCalorimeter/plugins/L1TStage2CaloAnalyzer.cc
@@ -30,9 +30,6 @@
 #include "FWCore/ServiceRegistry/interface/Service.h"
 #include "CommonTools/UtilAlgos/interface/TFileService.h"
 
-#include "CondFormats/L1TObjects/interface/CaloParams.h"
-#include "CondFormats/DataRecord/interface/L1TCaloParamsRcd.h"
-
 #include "DataFormats/L1TCalorimeter/interface/CaloTower.h"
 #include "DataFormats/L1TCalorimeter/interface/CaloCluster.h"
 #include "DataFormats/L1Trigger/interface/EGamma.h"
