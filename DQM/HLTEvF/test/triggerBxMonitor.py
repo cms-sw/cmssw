c90f72fa6233df3c64b7c0593dee56c9781ac93f
Andrea Bocci <fwyzard@gmail.com>
2016-05-02 22:48:49 +0200

    TriggerBxMonitor: migrate to Stage 2
    TriggerBxMonitor
      - migrate to Stage 2 L1 Trigger
      - correct the number of bx to 3564
      - add 2D plots, remove plots for unused L1 triggers
      - update test

--- DQM/HLTEvF/test/triggerBxMonitor.py
+++ DQM/HLTEvF/test/triggerBxMonitor.py
@@ -5,24 +5,32 @@ process = cms.Process("DQM")
 # DQM service
 process.load("DQMServices.Core.DQMStore_cfi")
 
+# MessageLogger
+process.load("FWCore.MessageLogger.MessageLogger_cfi")
+process.MessageLogger.cerr.FwkReport.reportEvery = 1000
+
 # Global Tag
 from Configuration.AlCa.GlobalTag_condDBv2 import GlobalTag as customiseGlobalTag
-process.GlobalTag = customiseGlobalTag(globaltag = 'auto:run2_hlt_GRun')
-process.GlobalTag.connect   = cms.string('frontier://FrontierProd/CMS_CONDITIONS')
-process.GlobalTag.pfnPrefix = cms.untracked.string('frontier://FrontierProd/')
+process.GlobalTag = customiseGlobalTag(globaltag = '80X_dataRun2_HLT_v12')
+process.GlobalTag.connect = cms.string('frontier://FrontierProd/CMS_CONDITIONS')
 
 # Source
 process.source = cms.Source("PoolSource",
   fileNames = cms.untracked.vstring(
-    '/store/data/Run2015D/L1Accept/RAW/v1/000/259/721/00000/52AFE8AC-6D78-E511-AF38-02163E0136CA.root'
+    '/store/data/Run2016B/HLTPhysics2/RAW/v1/000/272/022/00000/4CE23DEB-CB0D-E611-A6AC-02163E01181C.root'
   )
 )
+process.maxEvents = cms.untracked.PSet( input = cms.untracked.int32(1000) )
+
+# unpack L1 digis
+process.load("EventFilter.L1TRawToDigi.gtStage2Digis_cfi")
+process.gtStage2Digis.InputLabel = cms.InputTag( "rawDataCollector" )
 
 process.load("DQM.HLTEvF.triggerBxMonitor_cfi")
-process.triggerBxMonitor.l1tResults = cms.untracked.InputTag('hltGtDigis', '', 'HLT')
+process.triggerBxMonitor.l1tResults = cms.untracked.InputTag('gtStage2Digis')
 process.triggerBxMonitor.hltResults = cms.untracked.InputTag('TriggerResults', '', 'HLT')
 
 process.load('DQMServices.Components.DQMFileSaver_cfi')
 process.dqmSaver.workflow = "/HLT/TriggerBxMonitor/All"
 
-process.endp = cms.EndPath( process.triggerBxMonitor + process.dqmSaver )
+process.endp = cms.EndPath( process.gtStage2Digis + process.triggerBxMonitor + process.dqmSaver )
