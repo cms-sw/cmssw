4196ad9ce6e4c369228b6a5782a7a680e3aa6811
Slava Krutelyov <slava77@cern.ch>
2016-05-03 12:07:16 +0200

    s/ppRun2/ppEra_Run2_2016/

--- DQM/Integration/python/clients/visualization-live-secondInstance_cfg.py
+++ DQM/Integration/python/clients/visualization-live-secondInstance_cfg.py
@@ -8,8 +8,8 @@ Example configuration for online reconstruction meant for visualization clients.
 from DQM.Integration.config.inputsource_cfi import options,runType,source
 
 # this is needed to map the names of the run-types chosen by DQM to the scenarios, ideally we could converge to the same names
-#scenarios = {'pp_run': 'ppRun2','cosmic_run':'cosmicsEra_Run2_2016','hi_run':'HeavyIons'}
-#scenarios = {'pp_run': 'ppRun2','pp_run_stage1': 'ppRun2','cosmic_run':'cosmicsEra_Run2_2016','cosmic_run_stage1':'cosmicsEra_Run2_2016','hi_run':'HeavyIonsRun2'}
+#scenarios = {'pp_run': 'ppEra_Run2_2016','cosmic_run':'cosmicsEra_Run2_2016','hi_run':'HeavyIons'}
+#scenarios = {'pp_run': 'ppEra_Run2_2016','pp_run_stage1': 'ppEra_Run2_2016','cosmic_run':'cosmicsEra_Run2_2016','cosmic_run_stage1':'cosmicsEra_Run2_2016','hi_run':'HeavyIonsRun2'}
 scenarios = {'pp_run': 'ppEra_Run2_2016','cosmic_run':'cosmicsEra_Run2_2016','hi_run':'HeavyIonsRun2'}
 
 if not runType.getRunTypeName() in scenarios.keys():
