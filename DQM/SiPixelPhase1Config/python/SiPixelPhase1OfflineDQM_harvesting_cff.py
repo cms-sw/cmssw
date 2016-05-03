0bc109ebb53c148e77210e546f06ae778c25c517
Marcel Andre Schneider <marcel.andre.schneider@cern.ch>
2016-05-03 16:55:45 +0200

    Turn on the TrackClusters plugin.

--- DQM/SiPixelPhase1Config/python/SiPixelPhase1OfflineDQM_harvesting_cff.py
+++ DQM/SiPixelPhase1Config/python/SiPixelPhase1OfflineDQM_harvesting_cff.py
@@ -5,4 +5,5 @@ from  DQM.SiPixelPhase1Config.SiPixelPhase1OfflineDQM_source_cff import *
 siPixelPhase1OfflineDQM_harvesting = cms.Sequence(SiPixelPhase1DigisHarvester 
                                                 + SiPixelPhase1ClustersHarvester
                                                 + SiPixelPhase1RecHitsHarvester
-                                                + SiPixelPhase1TrackResidualsHarvester)
+                                                + SiPixelPhase1TrackResidualsHarvester
+                                                + SiPixelPhase1TrackClustersHarvester)
--- DQM/SiPixelPhase1Config/python/SiPixelPhase1OfflineDQM_source_cff.py
+++ DQM/SiPixelPhase1Config/python/SiPixelPhase1OfflineDQM_source_cff.py
@@ -8,10 +8,13 @@ from DQM.SiPixelPhase1Clusters.SiPixelPhase1Clusters_cfi import *
 from DQM.SiPixelPhase1RecHits.SiPixelPhase1RecHits_cfi import *
 # Residuals
 from DQM.SiPixelPhase1TrackResiduals.SiPixelPhase1TrackResiduals_cfi import *
+# Clusters ontrack/offtrack (also general tracks)
+from DQM.SiPixelPhase1TrackClusters.SiPixelPhase1TrackClusters_cfi import *
 
 PerModule.enabled = False
 
 siPixelPhase1OfflineDQM_source = cms.Sequence(SiPixelPhase1DigisAnalyzer
                                             + SiPixelPhase1ClustersAnalyzer
                                             + SiPixelPhase1RecHitsAnalyzer
-                                            + SiPixelPhase1TrackResidualsAnalyzer)
+                                            + SiPixelPhase1TrackResidualsAnalyzer
+                                            + SiPixelPhase1TrackClustersAnalyzer)
