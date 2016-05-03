8e2e17ab96bd5fc9c699c1059d0c75ad013b1731
Wu <mewu@cern.ch>
2016-05-03 11:17:39 +0200

    fastSim premix input added

--- Configuration/PyReleaseValidation/python/relval_steps.py
+++ Configuration/PyReleaseValidation/python/relval_steps.py
@@ -292,7 +292,7 @@ baseDataSetRelease=[
     'CMSSW_8_1_0_pre3-PU25ns_80X_mcRun2_asymptotic_v12-v1',     # 5 - fullSim PU 25ns premix for 810pre3
     'CMSSW_8_1_0_pre3-PU50ns_80X_mcRun2_startup_v12-v1',        # 6 - fullSim PU 50ns premix for 810pre3
     'CMSSW_8_1_0_pre3-80X_mcRun2_asymptotic_v12_FastSim-v1',    # 7 - fastSim MinBias for mixing for 810pre3 
-    'CMSSW_8_1_0_pre2-PU25ns_80X_mcRun2_asymptotic_v10_FastSim-v1',# 8 - fastSim premixed MinBias for 810pre2
+    'CMSSW_8_1_0_pre3-PU25ns_80X_mcRun2_asymptotic_v12_FastSim-v1',# 8 - fastSim premixed MinBias for 810pre3
     'CMSSW_7_6_0_pre6-76X_mcRun2_HeavyIon_v4-v1', 	           # 9 - Run2 HI GEN-SIM
     'CMSSW_7_6_0-76X_mcRun2_asymptotic_v11-v1',                    # 10 - 13 TeV High Stats GEN-SIM
     'CMSSW_7_6_0_pre7-76X_mcRun2_asymptotic_v9_realBS-v1',         # 11 - 13 TeV High Stats MiniBias for mixing GEN-SIM
