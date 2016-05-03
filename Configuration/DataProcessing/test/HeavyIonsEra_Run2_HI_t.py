58f32656655736714a353cdc216451871577e546
Slava Krutelyov <slava77@cern.ch>
2016-05-03 13:29:34 +0200

    s/HeavyIonsRun2/HeavyIonsEra_Run2_HI/g

--- Configuration/DataProcessing/test/HeavyIonsEra_Run2_HI_t.py
+++ Configuration/DataProcessing/test/HeavyIonsEra_Run2_HI_t.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python
 """
-_HeavyIonsRun2_
+_HeavyIonsEra_Run2_HI_
 
 Test for Collision Scenario implementation
 
@@ -25,71 +25,71 @@ def writePSetFile(name, process):
     handle.close()
 
 
-class HeavyIonsRun2ScenarioTest(unittest.TestCase):
+class HeavyIonsEra_Run2_HIScenarioTest(unittest.TestCase):
     """
-    unittest for HeavyIonsRun2 collisions scenario
+    unittest for HeavyIonsEra_Run2_HI collisions scenario
 
     """
 
     def testA(self):
         """get the scenario"""
         try:
-            scenario = getScenario("HeavyIonsRun2")
+            scenario = getScenario("HeavyIonsEra_Run2_HI")
         except Exception as ex:
-            msg = "Failed to get HeavyIonsRun2 scenario\n"
+            msg = "Failed to get HeavyIonsEra_Run2_HI scenario\n"
             msg += str(ex)
             self.fail(msg)
 
 
     def testPromptReco(self):
         """test promptReco method"""
-        scenario = getScenario("HeavyIonsRun2")
+        scenario = getScenario("HeavyIonsEra_Run2_HI")
         try:
             process = scenario.promptReco("GLOBALTAG::ALL")
             writePSetFile("testPromptReco.py", process)
         except Exception as ex:
             msg = "Failed to create Prompt Reco configuration\n"
-            msg += "for HeavyIonsRun2 Scenario\n"
+            msg += "for HeavyIonsEra_Run2_HI Scenario\n"
             msg += str(ex)
             self.fail(msg)
 
 
     def testExpressProcessing(self):
         """ test expressProcessing method"""
-        scenario = getScenario("HeavyIonsRun2")
+        scenario = getScenario("HeavyIonsEra_Run2_HI")
         try:
             process = scenario.expressProcessing("GLOBALTAG::ALL")
             writePSetFile("testExpressProcessing.py", process)
         except Exception as ex:
             msg = "Failed to create Express Processing configuration\n"
-            msg += "for HeavyIonsRun2 Scenario\n"
+            msg += "for HeavyIonsEra_Run2_HI Scenario\n"
             msg += str(ex)
             self.fail(msg)
 
 
     def testAlcaSkim(self):
         """ test alcaSkim method"""
-        scenario = getScenario("HeavyIonsRun2")
+        scenario = getScenario("HeavyIonsEra_Run2_HI")
         try:
             process = scenario.alcaSkim(["MuAlCalIsolatedMu"])
             writePSetFile("testAlcaReco.py", process)
         except Exception as ex:
            msg = "Failed to create Alca Skimming configuration\n"
-           msg += "for HeavyIonsRun2 Scenario\n"
+           msg += "for HeavyIonsEra_Run2_HI Scenario\n"
            msg += str(ex)
            self.fail(msg)
 
 
     def testDQMHarvesting(self):
         """test dqmHarvesting  method"""
-        scenario = getScenario("HeavyIonsRun2")
+        scenario = getScenario("HeavyIonsEra_Run2_HI")
         try:
             process = scenario.dqmHarvesting("dataset", 123456,
                                              "GLOBALTAG::ALL")
             writePSetFile("testDQMHarvesting.py", process)
         except Exception as ex:
             msg = "Failed to create DQM Harvesting configuration "
-            msg += "for HeavyIonsRun2 scenario:\n"
+            msg += "for HeavyIonsEra_Run2_HI scenario:\n"
             msg += str(ex)
             self.fail(msg)
 
