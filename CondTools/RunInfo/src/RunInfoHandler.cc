8d86279445b631552ceecd250f9453e88721873c
Salvatore Di Guida <salvatore.di.guida@cern.ch>
2016-05-04 11:52:02 +0200

    Fix RunInfo O2O: Improve logging messages.

--- CondTools/RunInfo/src/RunInfoHandler.cc
+++ CondTools/RunInfo/src/RunInfoHandler.cc
@@ -5,11 +5,11 @@
 #include<iostream>
 #include<vector>
 
-RunInfoHandler::RunInfoHandler(const edm::ParameterSet& pset) :
-   m_name( pset.getUntrackedParameter<std::string>( "name", "RunInfoHandler") )
+RunInfoHandler::RunInfoHandler( const edm::ParameterSet& pset ) :
+   m_name( pset.getUntrackedParameter<std::string>( "name", "RunInfoHandler" ) )
   ,m_since( pset.getParameter<unsigned long long>( "runNumber" ) )
-  ,m_connectionString( pset.getUntrackedParameter<std::string>( "connectionString", "oracle://cms_omds_adg/CMS_RUNINFO") )
-  ,m_authpath( pset.getUntrackedParameter<std::string>( "authenticationPath", "." ) )
+  ,m_connectionString( pset.getUntrackedParameter<std::string>( "connectionString", "oracle://cms_omds_adg/CMS_RUNINFO" ) )
+  ,m_authpath( pset.getUntrackedParameter<std::string>( "authenticationPath", "" ) )
   ,m_user( pset.getUntrackedParameter<std::string>( "OnlineDBUser", "CMS_RUNINFO_R" ) )
   ,m_pass( pset.getUntrackedParameter<std::string>( "OnlineDBPass", "PASSWORD") ) {
 }
@@ -18,21 +18,19 @@ RunInfoHandler::~RunInfoHandler() {}
 
 void RunInfoHandler::getNewObjects() {
   //check whats already inside of database
-  edm::LogInfo( "RunInfoHandler" ) << "------- " << m_name
-                                   << " - > getNewObjects\n"
-                                   << "got offlineInfo " << tagInfo().name
+  edm::LogInfo( "RunInfoHandler" ) << "[" << "RunInfoHandler::" << __func__ << "]:" << m_name << ": "
+                                   << "Destination Tag Info: name " << tagInfo().name
                                    << ", size " << tagInfo().size
                                    << ", last object valid since " << tagInfo().lastInterval.first
-                                   << " token " << tagInfo().lastPayloadToken << std::endl;
-  edm::LogInfo( "RunInfoHandler" ) << "runnumber/first since = " << m_since << std::endl;
+                                   << ", hash " << tagInfo().lastPayloadToken << std::endl;
+  edm::LogInfo( "RunInfoHandler" ) << "[" << "RunInfoHandler::" << __func__ << "]:" << m_name << ": runnumber/first since = " << m_since << std::endl;
 
   //check if a transfer is needed:
   //if the new run number is smaller than or equal to the latest IOV, exit.
   //This is needed as now the IOV Editor does not always protect for insertions:
   //ANY and VALIDATION sychronizations are allowed to write in the past.
   if( tagInfo().size > 0  && tagInfo().lastInterval.first >= m_since ) {
-    edm::LogWarning( "RunInfoHandler" ) << "------- " << m_name
-                                        << " - > getNewObjects\n"
+    edm::LogWarning( "RunInfoHandler" ) << "[" << "RunInfoHandler::" << __func__ << "]:" << m_name << ": "
                                         << "last IOV " << tagInfo().lastInterval.first
                                         << ( tagInfo().lastInterval.first == m_since ? " is equal to" : " is larger than" )
                                         << " the run proposed for insertion " << m_since
@@ -46,25 +44,28 @@ void RunInfoHandler::getNewObjects() {
   size_t n_empty_run = 0;
   if( tagInfo().size > 0  && (tagInfo().lastInterval.first + 1) < m_since ) {
     n_empty_run = m_since - tagInfo().lastInterval.first - 1;
-    edm::LogInfo( "RunInfoHandler" ) << "------- " << "entering fake run from "
+    edm::LogInfo( "RunInfoHandler" ) << "[" << "RunInfoHandler::" << __func__ << "]:" << m_name << ": "
+                                     << "entering fake run from "
                                      << tagInfo().lastInterval.first + 1
-                                     <<  "to " << m_since - 1 << "- > getNewObjects"
+                                     <<  " to " << m_since - 1
                                      << std::endl;
   } 
-  // transfer fake run for 1 to since for the first time
+  std::ostringstream ss;
+ // transfer fake run for 1 to since for the first time
   if( tagInfo().size == 0 && m_since != 1 ) {
     m_to_transfer.push_back( std::make_pair( (RunInfo*)(r->Fake_RunInfo()), 1 ) );
+    ss << "fake run number: " << 1 << ", ";
   }
   if ( n_empty_run != 0 ) {
     m_to_transfer.push_back(std::make_pair( (RunInfo*)(r->Fake_RunInfo()), tagInfo().lastInterval.first + 1 ) );
+    ss << "fake run number: " << tagInfo().lastInterval.first + 1 << ", ";
   }
   
   //reading from omds
   RunInfoRead rn( m_connectionString, m_user, m_pass );
   *r = rn.readData( "RUNSESSION_PARAMETER", "STRING_VALUE",(int)m_since );
   m_to_transfer.push_back( std::make_pair( (RunInfo*)r, m_since) );
-  std::ostringstream ss;
-  ss << "since =" << m_since;
-  m_userTextLog = ss.str() + ";";
-  edm::LogInfo( "RunInfoHandler" ) << "------- " << m_name << " - > getNewObjects" << std::endl;
+  ss << "run number: " << m_since << ";";
+  m_userTextLog = ss.str();
+  edm::LogInfo( "RunInfoHandler" ) << "[" << "RunInfoHandler::" << __func__ << "]:" << m_name << ": END." << std::endl;
 }
