d6024b205cef6948293b54354a65bd1f8769bdb8
Andrew Brinkerhoff <abrinke1@cern.ch>
2016-05-05 16:05:46 +0200

    Revert to proper default for simEmtfDigis_cfi.py

--- L1Trigger/L1TMuonEndCap/python/simEmtfDigis_cfi.py
+++ L1Trigger/L1TMuonEndCap/python/simEmtfDigis_cfi.py
@@ -6,5 +6,5 @@ import FWCore.ParameterSet.Config as cms
 ##   * 'csctfDigis' : real trigger primitives as received by CSCTF (legacy trigger)
 ##   * 'emtfStage2Digis' : real trigger primitives as received by EMTF, unpacked in EventFilter/L1TRawToDigi/
 simEmtfDigis = cms.EDProducer("L1TMuonEndCapTrackProducer",
-                              CSCInput = cms.InputTag('csctfDigis'),
+                              CSCInput = cms.InputTag('simCscTriggerPrimitiveDigis','MPCSORTED')
                               )
