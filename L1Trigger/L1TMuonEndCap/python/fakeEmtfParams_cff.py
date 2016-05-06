9c7bf9b023325e7d0af47e1774ca42f20b6a8573
Michael <michaeljmulhearn@gmail.com>
2016-05-06 03:34:39 +0200

    Provide CondFormats and Helper classes for EndCap DT Forest and other Parameters

--- L1Trigger/L1TMuonEndCap/python/fakeEmtfParams_cff.py
+++ L1Trigger/L1TMuonEndCap/python/fakeEmtfParams_cff.py
@@ -1,22 +1,35 @@
-#import FWCore.ParameterSet.Config as cms
+import FWCore.ParameterSet.Config as cms
+
+emtfParamsSource = cms.ESSource(
+	"EmptyESSource",
+	recordName = cms.string('L1TMuonEndCapParamsRcd'),
+	iovIsRunNotTime = cms.bool(True),
+	firstValid = cms.vuint32(1)
+)
+
+##EMTF ESProducer. Fills CondFormats from XML files.
+emtfParams = cms.ESProducer(
+	"L1TMuonEndCapParamsESProducer",
+   PtAssignVersion = cms.int32(1),
+   St1MatchWindow = cms.int32(15),
+   St2MatchWindow = cms.int32(15),
+   St3MatchWindow = cms.int32(7),
+   St4MatchWindow = cms.int32(7)
+)
 
 
-# NOT YET IMPLEMENTED ...
 
-# This file is included in the expected place, so standard sequences will find it as expected.
+emtfParamsSource = cms.ESSource(
+	"EmptyESSource",
+	recordName = cms.string('L1TMuonEndCapForestRcd'),
+	iovIsRunNotTime = cms.bool(True),
+	firstValid = cms.vuint32(1)
+)
 
-#emtfParamsSource = cms.ESSource(
-#    "EmptyESSource",
-#    recordName = cms.string('L1TMuonEndCapParamsRcd'),
-#    iovIsRunNotTime = cms.bool(True),
-#    firstValid = cms.vuint32(1)
-#)
-
-###EMTF ESProducer. Fills CondFormats from XML files.
-#fakeEmtfParams = cms.ESProducer(
-#    "L1TMuonEndCapParamsESProducer"
-#    ... local parameter setting ...
-#)
+##EMTF ESProducer. Fills CondFormats from XML files.
+emtfParams = cms.ESProducer(
+	"L1TMuonEndCapForestESProducer",
+)
 
 
 
