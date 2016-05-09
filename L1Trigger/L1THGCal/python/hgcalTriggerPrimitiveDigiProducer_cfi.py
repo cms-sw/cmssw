40206df743062b4749403ab66911f283535c63c2
Stephanie <stephanie.baffioni@llr.in2p3.fr>
2016-05-09 16:26:43 +0200

    parameters taken from digitization

--- L1Trigger/L1THGCal/python/hgcalTriggerPrimitiveDigiProducer_cfi.py
+++ L1Trigger/L1THGCal/python/hgcalTriggerPrimitiveDigiProducer_cfi.py
@@ -1,16 +1,20 @@
 import FWCore.ParameterSet.Config as cms
 
+import SimCalorimetry.HGCalSimProducers.hgcalDigitizer_cfi as digiparam
+
 fe_codec = cms.PSet( CodecName  = cms.string('HGCalBestChoiceCodec'),
                      CodecIndex = cms.uint32(1),
                      NData = cms.uint32(12),
                      DataLength = cms.uint32(8),
                      linLSB = cms.double(100./1024.),
-                     adcsaturation = cms.double(100),
-                     adcnBits =  cms.uint32(10),
-                     tdcsaturation = cms.double(10000),
-                     tdcnBits =  cms.uint32(12),
-                     tdcOnsetfC = cms.double(60)
+                     #take the following parameters from the digitization config file
+                     adcsaturation = digiparam.hgceeDigitizer.digiCfg.feCfg.adcSaturation_fC,
+                     adcnBits = digiparam.hgceeDigitizer.digiCfg.feCfg.adcNbits,
+                     tdcsaturation = digiparam.hgceeDigitizer.digiCfg.feCfg.tdcSaturation_fC,
+                     tdcnBits = digiparam.hgceeDigitizer.digiCfg.feCfg.tdcNbits,
+                     tdcOnsetfC = digiparam.hgceeDigitizer.digiCfg.feCfg.tdcOnset_fC
                    )
+
 
 cluster_algo =  cms.PSet( AlgorithmName = cms.string('FullModuleSumAlgo'),
                                  FECodec = fe_codec )
