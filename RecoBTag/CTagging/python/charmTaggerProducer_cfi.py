a6880a38e6e0fb16388a9dafd55baa153d2d5319
mauro verzetti <mverzett@cern.ch>
2016-05-10 02:22:28 -0500

    bugfix

--- RecoBTag/CTagging/python/charmTaggerProducer_cfi.py
+++ RecoBTag/CTagging/python/charmTaggerProducer_cfi.py
@@ -2,11 +2,14 @@ import FWCore.ParameterSet.Config as cms
 #use import as to mask it to process.load() 
 import RecoBTag.SecondaryVertex.candidateCombinedSecondaryVertexSoftLeptonComputer_cfi as sl_cfg 
 from RecoBTag.CTagging.training_settings import c_vs_l_vars_vpset, c_vs_b_vars_vpset
-from RecoBTag.SecondaryVertex.combinedSecondaryVertexCommon_cff import *
+#from RecoBTag.SecondaryVertex.combinedSecondaryVertexCommon_cff import *
+
+#
+# Normal tagger
+#
 
 charmTagsComputerCvsL = cms.ESProducer(
    'CharmTaggerESProducer',
-   combinedSecondaryVertexCommon,
    #clone the cfg only
    slComputerCfg = cms.PSet(
       **sl_cfg.candidateCombinedSecondaryVertexSoftLeptonComputer.parameters_()
@@ -34,37 +37,43 @@ charmTagsComputerCvsB = charmTagsComputerCvsL.clone(
    variables = c_vs_b_vars_vpset
    )
 
-#Negative Taggers
+#
+# Negative tagger
+#
+
 charmTagsNegativeComputerCvsL = charmTagsComputerCvsL.clone(
    vertexFlip = cms.bool(True),
    trackFlip = cms.bool(True)
 )
 
-charmTagsNegativeComputerCvsL.trackSelection.sip3dSigMax = 0
-charmTagsNegativeComputerCvsL.trackPseudoSelection.sip3dSigMax = 0
-charmTagsNegativeComputerCvsL.trackPseudoSelection.sip2dSigMin = -99999.9
-charmTagsNegativeComputerCvsL.trackPseudoSelection.sip2dSigMax = -2.0
+charmTagsNegativeComputerCvsL.slComputerCfg.trackSelection.sip3dSigMax = 0
+charmTagsNegativeComputerCvsL.slComputerCfg.trackPseudoSelection.sip3dSigMax = 0
+charmTagsNegativeComputerCvsL.slComputerCfg.trackPseudoSelection.sip2dSigMin = -99999.9
+charmTagsNegativeComputerCvsL.slComputerCfg.trackPseudoSelection.sip2dSigMax = -2.0
 
 charmTagsNegativeComputerCvsB = charmTagsComputerCvsB.clone(
    vertexFlip = cms.bool(True),
    trackFlip = cms.bool(True)
 )
 
-charmTagsNegativeComputerCvsB.trackSelection.sip3dSigMax = 0
-charmTagsNegativeComputerCvsB.trackPseudoSelection.sip3dSigMax = 0
-charmTagsNegativeComputerCvsB.trackPseudoSelection.sip2dSigMin = -99999.9
-charmTagsNegativeComputerCvsB.trackPseudoSelection.sip2dSigMax = -2.0
+charmTagsNegativeComputerCvsB.slComputerCfg.trackSelection.sip3dSigMax = 0
+charmTagsNegativeComputerCvsB.slComputerCfg.trackPseudoSelection.sip3dSigMax = 0
+charmTagsNegativeComputerCvsB.slComputerCfg.trackPseudoSelection.sip2dSigMin = -99999.9
+charmTagsNegativeComputerCvsB.slComputerCfg.trackPseudoSelection.sip2dSigMax = -2.0
 
-#Positive Taggers
+#
+# Positive tagger
+#
+
 charmTagsPositiveComputerCvsL = charmTagsComputerCvsL.clone(
 )
 
-charmTagsPositiveComputerCvsL.trackSelection.sip3dSigMin = 0
-charmTagsPositiveComputerCvsL.trackPseudoSelection.sip3dSigMin = 0
+charmTagsPositiveComputerCvsL.slComputerCfg.trackSelection.sip3dSigMin = 0
+charmTagsPositiveComputerCvsL.slComputerCfg.trackPseudoSelection.sip3dSigMin = 0
 
 charmTagsPositiveComputerCvsB = charmTagsComputerCvsB.clone(
 )
 
-charmTagsPositiveComputerCvsB.trackSelection.sip3dSigMin = 0
-charmTagsPositiveComputerCvsB.trackPseudoSelection.sip3dSigMin = 0
+charmTagsPositiveComputerCvsB.slComputerCfg.trackSelection.sip3dSigMin = 0
+charmTagsPositiveComputerCvsB.slComputerCfg.trackPseudoSelection.sip3dSigMin = 0
 
