c48bbe6dd3aba8daf7401208231d2865a6df2725
Steven Lowette <steven.lowette@cern.ch>
2016-05-11 13:43:50 +0200

    Correct bug in error (even though this CPE is not really usable anyway).

--- RecoLocalTracker/Phase2TrackerRecHits/src/Phase2StripCPETrivial.cc
+++ RecoLocalTracker/Phase2TrackerRecHits/src/Phase2StripCPETrivial.cc
@@ -7,7 +7,7 @@ Phase2StripCPETrivial::LocalValues Phase2StripCPETrivial::localParameters(const 
   float striplength = 5.;     // hardcoded dummy, a la 2S
   std::pair<float, float> barycenter = cluster.barycenter();
   LocalPoint lp( barycenter.second * strippitch, striplength * (barycenter.first + 1/2), 0 );
-  LocalError le( strippitch / 12, 0, striplength / 12 );
+  LocalError le( pow(strippitch,2) / 12, pow(striplength,2) / 12, 0 );
   return std::make_pair( lp, le );
 }
 
