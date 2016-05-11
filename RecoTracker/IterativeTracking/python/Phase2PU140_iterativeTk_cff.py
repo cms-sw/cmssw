2071b69db9373b3870dc6ce809a8baaffb506cfc
erica brondolin <erica.brondolin@cern.ch>
2016-05-11 11:33:55 +0200

    not spreading deprecated names around (II)

--- RecoTracker/IterativeTracking/python/Phase2PU140_iterativeTk_cff.py
+++ RecoTracker/IterativeTracking/python/Phase2PU140_iterativeTk_cff.py
@@ -1,16 +1,16 @@
 import FWCore.ParameterSet.Config as cms
 
 from RecoTracker.IterativeTracking.Phase1PU140_InitialStep_cff import *
-from RecoTracker.IterativeTracking.Phase2PU140Pixel10D_HighPtTripletStep_cff import *
-from RecoTracker.IterativeTracking.Phase2PU140Pixel10D_LowPtQuadStep_cff import *
-from RecoTracker.IterativeTracking.Phase2PU140Pixel10D_LowPtTripletStep_cff import *
-from RecoTracker.IterativeTracking.Phase2PU140Pixel10D_DetachedQuadStep_cff import *
-from RecoTracker.IterativeTracking.Phase2PU140Pixel10D_PixelPairStep_cff import *
+from RecoTracker.IterativeTracking.Phase2PU140_HighPtTripletStep_cff import *
+from RecoTracker.IterativeTracking.Phase2PU140_LowPtQuadStep_cff import *
+from RecoTracker.IterativeTracking.Phase2PU140_LowPtTripletStep_cff import *
+from RecoTracker.IterativeTracking.Phase2PU140_DetachedQuadStep_cff import *
+from RecoTracker.IterativeTracking.Phase2PU140_PixelPairStep_cff import *
 from RecoTracker.FinalTrackSelectors.Phase1PU140_earlyGeneralTracks_cfi import *
-from RecoTracker.IterativeTracking.Phase2PU140Pixel10D_MuonSeededStep_cff import *
-from RecoTracker.FinalTrackSelectors.Phase2PU140Pixel10D_preDuplicateMergingGeneralTracks_cfi import *
+from RecoTracker.IterativeTracking.Phase2PU140_MuonSeededStep_cff import *
+from RecoTracker.FinalTrackSelectors.Phase2PU140_preDuplicateMergingGeneralTracks_cfi import *
 from RecoTracker.FinalTrackSelectors.MergeTrackCollections_cff import *
-from RecoTracker.ConversionSeedGenerators.Phase2PU140Pixel10D_ConversionStep_cff import *
+from RecoTracker.ConversionSeedGenerators.Phase2PU140_ConversionStep_cff import *
 
 iterTracking = cms.Sequence(InitialStep*
                             HighPtTripletStep*
