088b5f9f873d90a6d7de46abc00d37d2791a2989
Hengne <Hengne.Li@CERN.CH>
2016-05-12 09:45:39 +0200

    update mixing and premixing inputs

--- Configuration/PyReleaseValidation/python/relval_steps.py
+++ Configuration/PyReleaseValidation/python/relval_steps.py
@@ -289,10 +289,10 @@ baseDataSetRelease=[
                                                             # THIS ABOVE IS NOT USED, AT THE MOMENT
     'CMSSW_8_1_0_pre2-80X_mcRun2_asymptotic_v10_gs810pre2-v1',     # 3 - 13 TeV samples with GEN-SIM from 810_p2; also GEN-SIM-DIGI-RAW-HLTDEBUG for id tests
     'CMSSW_7_3_0_pre1-PRE_LS172_V15_FastSim-v1',                   # 4 - fast sim GEN-SIM-DIGI-RAW-HLTDEBUG for id tests
-    'CMSSW_8_1_0_pre3-PU25ns_80X_mcRun2_asymptotic_v12-v1',     # 5 - fullSim PU 25ns premix for 810pre3
-    'CMSSW_8_1_0_pre3-PU50ns_80X_mcRun2_startup_v12-v1',        # 6 - fullSim PU 50ns premix for 810pre3
-    'CMSSW_8_1_0_pre3-80X_mcRun2_asymptotic_v12_FastSim-v1',    # 7 - fastSim MinBias for mixing for 810pre3 
-    'CMSSW_8_1_0_pre3-PU25ns_80X_mcRun2_asymptotic_v12_FastSim-v1',# 8 - fastSim premixed MinBias for 810pre3
+    'CMSSW_8_1_0_pre4-PU25ns_80X_mcRun2_asymptotic_v13-v1',     # 5 - fullSim PU 25ns premix for 810pre4
+    'CMSSW_8_1_0_pre4-PU50ns_80X_mcRun2_startup_v13-v1',        # 6 - fullSim PU 50ns premix for 810pre4
+    'CMSSW_8_1_0_pre4-80X_mcRun2_asymptotic_v13_FastSim-v1',    # 7 - fastSim MinBias for mixing for 810pre4
+    'CMSSW_8_1_0_pre4-PU25ns_80X_mcRun2_asymptotic_v13_FastSim-v1',# 8 - fastSim premixed MinBias for 810pre4
     'CMSSW_7_6_0_pre6-76X_mcRun2_HeavyIon_v4-v1', 	           # 9 - Run2 HI GEN-SIM
     'CMSSW_7_6_0-76X_mcRun2_asymptotic_v11-v1',                    # 10 - 13 TeV High Stats GEN-SIM
     'CMSSW_7_6_0_pre7-76X_mcRun2_asymptotic_v9_realBS-v1',         # 11 - 13 TeV High Stats MiniBias for mixing GEN-SIM
