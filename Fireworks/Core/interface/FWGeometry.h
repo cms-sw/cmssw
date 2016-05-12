fe8aba8deb120a41469610f0f58d5b544fe6037d
alja <amraktadel@ucsd.edu>
2016-05-12 10:54:59 -0700

    Undo change from the previous commit.

--- Fireworks/Core/interface/FWGeometry.h
+++ Fireworks/Core/interface/FWGeometry.h
@@ -111,15 +111,11 @@ public:
      return FWGeometry::find( id ) != m_idToInfo.end();
    }
 
-   IdToInfoItr mapEnd() const {return m_idToInfo.end();}
-
    void clear( void ) { m_idToInfo.clear(); m_idToMatrix.clear(); }
    IdToInfoItr find( unsigned int ) const;
    void localToGlobal( const GeomDetInfo& info, const float* local, float* global, bool translatep=true ) const;
 
    const VersionInfo& versionInfo() const { return m_versionInfo; }
-
-   TGeoShape* getShape( const GeomDetInfo& info ) const;
 
 
 private:
