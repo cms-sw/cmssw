344dfe45e75b89cda678dad1dafcf8b283b98fb0
Martin Grunewald <Martin.Grunewald@cern.ch>
2016-05-13 10:29:55 +0200

    Migration

--- HLTrigger/Configuration/python/HLT_Fake1_cff.py
+++ HLTrigger/Configuration/python/HLT_Fake1_cff.py
@@ -1,11 +1,11 @@
-# /dev/CMSSW_8_0_0/Fake1/V12 (CMSSW_8_0_7_HLT1)
+# /dev/CMSSW_8_0_0/Fake1/V13 (CMSSW_8_0_8_HLT1)
 
 import FWCore.ParameterSet.Config as cms
 
 fragment = cms.ProcessFragment( "HLT" )
 
 fragment.HLTConfigVersion = cms.PSet(
-  tableName = cms.string('/dev/CMSSW_8_0_0/Fake1/V12')
+  tableName = cms.string('/dev/CMSSW_8_0_0/Fake1/V13')
 )
 
 fragment.streams = cms.PSet(  A = cms.vstring( 'InitialPD' ) )
