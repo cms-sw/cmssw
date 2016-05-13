4fc751b3c9be3bdf34bceacb2c0dc1bc8c1a7435
Steven Lowette <steven.lowette@cern.ch>
2016-05-13 19:18:56 +0200

    Change to geometric CPE as default

--- RecoLocalTracker/Phase2TrackerRecHits/python/Phase2TrackerRecHits_cfi.py
+++ RecoLocalTracker/Phase2TrackerRecHits/python/Phase2TrackerRecHits_cfi.py
@@ -3,5 +3,5 @@ import FWCore.ParameterSet.Config as cms
 # RecHits options
 siPhase2RecHits = cms.EDProducer("Phase2TrackerRecHits",
   src = cms.InputTag("siPhase2Clusters"),
-  Phase2StripCPE = cms.ESInputTag("phase2StripCPEESProducer", "Phase2StripCPETrivial")
+  Phase2StripCPE = cms.ESInputTag("phase2StripCPEGeometricESProducer", "Phase2StripCPEGeometric")
 )
