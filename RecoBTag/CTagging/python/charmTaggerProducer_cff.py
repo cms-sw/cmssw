f6d59e64de911a9d4c88aa5d51389a70116c9c51
Kittikul Kovitanggoon <kovitang.cern@gmail.com>
2016-05-13 17:53:48 +0200

    cfi, cff, and DQM fixed

--- RecoBTag/CTagging/python/charmTaggerProducer_cff.py
+++ RecoBTag/CTagging/python/charmTaggerProducer_cff.py
@@ -1,36 +1,6 @@
 import FWCore.ParameterSet.Config as cms
-#use import as to mask it to process.load() 
-import RecoBTag.SecondaryVertex.candidateCombinedSecondaryVertexSoftLeptonComputer_cfi as sl_cfg 
-from RecoBTag.CTagging.training_settings import c_vs_l_vars_vpset, c_vs_b_vars_vpset
-#from RecoBTag.SecondaryVertex.combinedSecondaryVertexCommon_cff import *
 
-#
-# Normal tagger
-#
-
-charmTagsComputerCvsL = cms.ESProducer(
-   'CharmTaggerESProducer',
-   #clone the cfg only
-   slComputerCfg = cms.PSet(
-      **sl_cfg.candidateCombinedSecondaryVertexSoftLeptonComputer.parameters_()
-      ),
-   weightFile = cms.FileInPath('RecoBTag/CTagging/data/c_vs_udsg_sklearn.weight.xml'),
-   variables = c_vs_l_vars_vpset,
-   computer = cms.ESInputTag('combinedSecondaryVertexSoftLeptonComputer'),
-   tagInfos = cms.VInputTag(
-      cms.InputTag('pfImpactParameterTagInfos'),
-      cms.InputTag('pfInclusiveSecondaryVertexFinderCvsLTagInfos'),
-      cms.InputTag('softPFMuonsTagInfos'),
-      cms.InputTag('softPFElectronsTagInfos'),
-      ),
-   mvaName = cms.string('BDT'),
-   useCondDB = cms.bool(False),
-   gbrForestLabel = cms.string(''),
-   useGBRForest = cms.bool(True),
-   useAdaBoost = cms.bool(False)
-   )
-
-charmTagsComputerCvsL.slComputerCfg.correctVertexMass = False
+from RecoBTag.CTagging.charmTagsComputerCvsL_cfi import *
 
 charmTagsComputerCvsB = charmTagsComputerCvsL.clone(
    weightFile = cms.FileInPath('RecoBTag/CTagging/data/c_vs_b_sklearn.weight.xml'),   
@@ -52,7 +22,7 @@ charmTagsNegativeComputerCvsL.slComputerCfg.trackPseudoSelection.sip2dSigMax = -
 
 charmTagsNegativeComputerCvsB = charmTagsComputerCvsB.clone()
 
-charmTagsNegativeComputerCvsB.slComputerCfg.vertexFlip = cms.bool(True),
+charmTagsNegativeComputerCvsB.slComputerCfg.vertexFlip = cms.bool(True)
 charmTagsNegativeComputerCvsB.slComputerCfg.trackFlip = cms.bool(True)
 charmTagsNegativeComputerCvsB.slComputerCfg.trackSelection.sip3dSigMax = 0
 charmTagsNegativeComputerCvsB.slComputerCfg.trackPseudoSelection.sip3dSigMax = 0
@@ -74,4 +44,3 @@ charmTagsPositiveComputerCvsB = charmTagsComputerCvsB.clone(
 
 charmTagsPositiveComputerCvsB.slComputerCfg.trackSelection.sip3dSigMin = 0
 charmTagsPositiveComputerCvsB.slComputerCfg.trackPseudoSelection.sip3dSigMin = 0
-
