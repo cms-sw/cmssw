7cd40d9b4ba156fc4d5d1bea31719daa631bbb6d
Martin Grunewald <Martin.Grunewald@cern.ch>
2016-05-19 14:36:40 +0200

    Integration of CMSHLT-825 and preparation for CMSHLT-798

--- HLTrigger/Configuration/test/OnLine_HLT_PIon.py
+++ HLTrigger/Configuration/test/OnLine_HLT_PIon.py
@@ -1,11 +1,11 @@
-# /dev/CMSSW_8_0_0/PIon/V93 (CMSSW_8_0_8_HLT1)
+# /dev/CMSSW_8_0_0/PIon/V94 (CMSSW_8_0_8_HLT1)
 
 import FWCore.ParameterSet.Config as cms
 
 process = cms.Process( "HLTPIon" )
 
 process.HLTConfigVersion = cms.PSet(
-  tableName = cms.string('/dev/CMSSW_8_0_0/PIon/V93')
+  tableName = cms.string('/dev/CMSSW_8_0_0/PIon/V94')
 )
 
 process.HLTPSetInitialStepTrajectoryFilterBase = cms.PSet( 
