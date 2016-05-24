a4abaff8182e45b6239e7c2d8cb321350151318f
Martin Grunewald <Martin.Grunewald@cern.ch>
2016-05-24 18:05:07 +0200

    Move to L1T menu v4 - take-2

--- HLTrigger/Configuration/python/HLT_PRef_cff.py
+++ HLTrigger/Configuration/python/HLT_PRef_cff.py
@@ -1,11 +1,11 @@
-# /dev/CMSSW_8_0_0/PRef/V99 (CMSSW_8_0_8_HLT1)
+# /dev/CMSSW_8_0_0/PRef/V100 (CMSSW_8_0_8_HLT1)
 
 import FWCore.ParameterSet.Config as cms
 
 fragment = cms.ProcessFragment( "HLT" )
 
 fragment.HLTConfigVersion = cms.PSet(
-  tableName = cms.string('/dev/CMSSW_8_0_0/PRef/V99')
+  tableName = cms.string('/dev/CMSSW_8_0_0/PRef/V100')
 )
 
 fragment.HLTPSetInitialStepTrajectoryFilterBase = cms.PSet( 
--- HLTrigger/Configuration/test/OnLine_HLT_HIon.py
+++ HLTrigger/Configuration/test/OnLine_HLT_HIon.py
@@ -1,11 +1,11 @@
-# /dev/CMSSW_8_0_0/HIon/V99 (CMSSW_8_0_8_HLT1)
+# /dev/CMSSW_8_0_0/HIon/V100 (CMSSW_8_0_8_HLT1)
 
 import FWCore.ParameterSet.Config as cms
 
 process = cms.Process( "HLTHIon" )
 
 process.HLTConfigVersion = cms.PSet(
-  tableName = cms.string('/dev/CMSSW_8_0_0/HIon/V99')
+  tableName = cms.string('/dev/CMSSW_8_0_0/HIon/V100')
 )
 
 process.HLTPSetInitialStepTrajectoryFilterBase = cms.PSet( 
