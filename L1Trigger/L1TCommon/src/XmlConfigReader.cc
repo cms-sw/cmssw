de75addde033cf89820b7f333cab87849ab13f7d
Thomas Reis <thomas.reis@cern.ch>
2016-05-24 12:18:56 +0200

    Add tabs to characters to be removed

--- L1Trigger/L1TCommon/src/XmlConfigReader.cc
+++ L1Trigger/L1TCommon/src/XmlConfigReader.cc
@@ -381,8 +381,8 @@ void XmlConfigReader::appendNodesFromSubDoc(DOMNode* parentNode, DOMDocument* su
 
 void XmlConfigReader::pruneString(std::string& str)
 {
-  std::size_t alphanumBegin = str.find_first_not_of("\n ");
-  std::size_t alphanumEnd = str.find_last_not_of("\n ");
+  std::size_t alphanumBegin = str.find_first_not_of("\n\t ");
+  std::size_t alphanumEnd = str.find_last_not_of("\n\t ");
   if (alphanumBegin != std::string::npos) {
     if (alphanumEnd != std::string::npos) {
       str = str.substr(alphanumBegin, alphanumEnd - alphanumBegin + 1);
