21eb6c6837aabf80b821ebb96f54bada07163fcf
Martin Grunewald <Martin.Grunewald@cern.ch>
2016-05-25 09:21:11 +0200

    Use of new L1T v4 seeds

--- HLTrigger/Configuration/test/OnLine_HLT_GRun.py
+++ HLTrigger/Configuration/test/OnLine_HLT_GRun.py
@@ -1,11 +1,11 @@
-# /dev/CMSSW_8_0_0/GRun/V100 (CMSSW_8_0_8_HLT1)
+# /dev/CMSSW_8_0_0/GRun/V101 (CMSSW_8_0_8_HLT1)
 
 import FWCore.ParameterSet.Config as cms
 
 process = cms.Process( "HLTGRun" )
 
 process.HLTConfigVersion = cms.PSet(
-  tableName = cms.string('/dev/CMSSW_8_0_0/GRun/V100')
+  tableName = cms.string('/dev/CMSSW_8_0_0/GRun/V101')
 )
 
 process.HLTPSetInitialStepTrajectoryFilterBase = cms.PSet( 
