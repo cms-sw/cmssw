6231a48a1817fffa4ce02e031bd675b6ae52cc10
schoefbeck <robert.schoefbeck@cern.ch>
2016-05-27 22:52:54 +0200

    Adding ptError/pt on the muon inner track

--- RecoMET/METFilters/python/BadChargedCandidateFilter_cfi.py
+++ RecoMET/METFilters/python/BadChargedCandidateFilter_cfi.py
@@ -6,7 +6,7 @@ BadChargedCandidateFilter = cms.EDFilter(
     muons  = cms.InputTag("muons"),   # Collection to test
     taggingMode   = cms.bool(False),
     debug         = cms.bool(False),
-    maxDR         = cms.double(0.01),              # Maximum DR between reco::muon->innerTrack and pfCandidate 
+    maxDR         = cms.double(0.001),              # Maximum DR between reco::muon->innerTrack and pfCandidate 
     minPtDiffRel = cms.double(-0.5),               # lower threshold on difference between pt of reco::muon->innerTrack and pfCandidate
                                                    # computed as (pfcand.pt - muon.track.pt)/(0.5*(pfcand.pt + muon.track.pt))
     minMuonTrackRelErr = cms.double(0.5),          # minimum ptError/pt on muon innertrack 
