8f82eb69fb9ea56c8cfac75ab219f478b725e077
Martin Grunewald <Martin.Grunewald@cern.ch>
2016-05-27 15:20:37 +0200

    Resync

--- HLTrigger/Configuration/python/HLT_PRef_cff.py
+++ HLTrigger/Configuration/python/HLT_PRef_cff.py
@@ -1,11 +1,11 @@
-# /dev/CMSSW_8_0_0/PRef/V103 (CMSSW_8_0_8_HLT1)
+# /dev/CMSSW_8_0_0/PRef/V105 (CMSSW_8_0_8_HLT1)
 
 import FWCore.ParameterSet.Config as cms
 
 fragment = cms.ProcessFragment( "HLT" )
 
 fragment.HLTConfigVersion = cms.PSet(
-  tableName = cms.string('/dev/CMSSW_8_0_0/PRef/V103')
+  tableName = cms.string('/dev/CMSSW_8_0_0/PRef/V105')
 )
 
 fragment.HLTPSetInitialStepTrajectoryFilterBase = cms.PSet( 
