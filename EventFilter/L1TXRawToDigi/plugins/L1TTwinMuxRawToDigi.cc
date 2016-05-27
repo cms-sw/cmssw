c5f70bfe138892522e3727a00f62c00c2eccb672
Javier <Javier.Fernandez@cern.ch>
2016-05-27 17:13:21 +0200

    Fixed quality in unpacker

--- EventFilter/L1TXRawToDigi/plugins/L1TTwinMuxRawToDigi.cc
+++ EventFilter/L1TXRawToDigi/plugins/L1TTwinMuxRawToDigi.cc
@@ -464,69 +464,91 @@ void L1TTwinMuxRawToDigi::processFed( int twinMuxFed,
   
       else if ( selector == 0x3 ) { //etha word
        
-        int posALL, posBTI[7];
+        int posALL, posBTI[7], qualBTI[7];
     
         int mb3_eta    = ( dataWordSub & 0xFF );        // positions  0 -> 7
-        int mb3_eta_HQ = ( dataWordSub >> 8  ) & 0xFF;  // positions  8 -> 15
         int mb2_eta    = ( dataWordSub >> 16 ) & 0xFF;  // positions 16 -> 23
-        int mb2_eta_HQ = ( dataWordSub >> 32 ) & 0xFF;  // positions 32 -> 39
         int mb1_eta    = ( dataWordSub >> 40 ) & 0xFF;  // positions 40 -> 47
+
+        int mb3_eta_HQ = ( dataWordSub >> 8  ) & 0xFF;  // positions  8 -> 15
+        int mb2_eta_HQ = ( dataWordSub >> 32 ) & 0xFF;  // positions 32 -> 39
         int mb1_eta_HQ = ( dataWordSub >> 48 ) & 0xFF;  // positions 48 -> 55
 
         if ( debug_ ) logfile << '[' << ++lcounter << "]\t" << std::hex
                               << dataWordSub << std::dec << "\t|\t"
-                              << "mb1_eta_HQ " <<  mb1_eta_HQ   << '\t'
                               << "mb1_eta " << mb1_eta  << '\t'
-                              << "mb2_eta_HQ " <<  mb2_eta_HQ   << '\t'
                               << "mb2_eta " << mb2_eta  << '\t'
-                              << "mb3_eta_HQ " <<  mb3_eta_HQ   << '\t'
-                              << "mb3_eta " << mb3_eta  << '\n';
+                              << "mb3_eta " << mb3_eta  << '\t'
+                              << "mb1_eta_HQ " <<  mb1_eta_HQ   << '\t'
+                              << "mb2_eta_HQ " <<  mb2_eta_HQ   << '\t'
+                              << "mb3_eta_HQ " <<  mb3_eta_HQ   << '\n';
         
-        posALL    = (mb1_eta_HQ & 0x7F) + (mb1_eta & 0x7F);
-        posBTI[0] = ((mb1_eta_HQ & 0x01) + (mb1_eta & 0x01));
-        posBTI[1] = (((mb1_eta_HQ & 0x02)>>1) + ((mb1_eta & 0x02)>>1));
-        posBTI[2] = (((mb1_eta_HQ & 0x04)>>2) + ((mb1_eta & 0x04)>>2));
-        posBTI[3] = (((mb1_eta_HQ & 0x08)>>3) + ((mb1_eta & 0x08)>>3));
-        posBTI[4] = (((mb1_eta_HQ & 0x10)>>4) + ((mb1_eta & 0x10)>>4));
-        posBTI[5] = (((mb1_eta_HQ & 0x20)>>5) + ((mb1_eta & 0x20)>>5));
-        posBTI[6] = (((mb1_eta_HQ & 0x40)>>6) || ((mb1_eta_HQ & 0x80)>>7)) + (((mb1_eta & 0x40)>>6) || ((mb1_eta & 0x80)>>7));
+        //MB1
+        posALL    = (mb1_eta & 0x7F);
+        posBTI[0] = (mb1_eta & 0x01);
+        posBTI[1] = ((mb1_eta & 0x02)>>1);
+        posBTI[2] = ((mb1_eta & 0x04)>>2);
+        posBTI[3] = ((mb1_eta & 0x08)>>3);
+        posBTI[4] = ((mb1_eta & 0x10)>>4);
+        posBTI[5] = ((mb1_eta & 0x20)>>5);
+        posBTI[6] = (((mb1_eta & 0x40)>>6) || ((mb1_eta & 0x80)>>7));
+
+        qualBTI[0] = (mb1_eta_HQ & 0x01);
+        qualBTI[1] = ((mb1_eta_HQ & 0x02)>>1);
+        qualBTI[2] = ((mb1_eta_HQ & 0x04)>>2);
+        qualBTI[3] = ((mb1_eta_HQ & 0x08)>>3);
+        qualBTI[4] = ((mb1_eta_HQ & 0x10)>>4);
+        qualBTI[5] = ((mb1_eta_HQ & 0x20)>>5);
+        qualBTI[6] = (((mb1_eta_HQ & 0x40)>>6) || ((mb1_eta_HQ & 0x80)>>7));
 
         if ( posALL ) {
-            
-          theSegments.push_back( L1MuDTChambThDigi( bxNr, wheel, sector-1, 1, posBTI) );
-            
+            theSegments.push_back( L1MuDTChambThDigi( bxNr, wheel, sector-1, 1, posBTI, qualBTI) );
         }
         
-        posALL    = (mb2_eta_HQ & 0x7F) + (mb2_eta & 0x7F);
-        posBTI[0] = ((mb2_eta_HQ & 0x01) + (mb2_eta & 0x01));
-        posBTI[1] = (((mb2_eta_HQ & 0x02)>>1) + ((mb2_eta & 0x02)>>1));
-        posBTI[2] = (((mb2_eta_HQ & 0x04)>>2) + ((mb2_eta & 0x04)>>2));
-        posBTI[3] = (((mb2_eta_HQ & 0x08)>>3) + ((mb2_eta & 0x08)>>3));
-        posBTI[4] = (((mb2_eta_HQ & 0x10)>>4) + ((mb2_eta & 0x10)>>4));
-        posBTI[5] = (((mb2_eta_HQ & 0x20)>>5) + ((mb2_eta & 0x20)>>5));
-        posBTI[6] = (((mb2_eta_HQ & 0x40)>>6) || ((mb2_eta_HQ & 0x80)>>7)) + (((mb2_eta & 0x40)>>6) || ((mb2_eta & 0x80)>>7));
+        //MB2
+        posALL    = (mb2_eta & 0x7F);
+        posBTI[0] = (mb2_eta & 0x01);
+        posBTI[1] = ((mb2_eta & 0x02)>>1);
+        posBTI[2] = ((mb2_eta & 0x04)>>2);
+        posBTI[3] = ((mb2_eta & 0x08)>>3);
+        posBTI[4] = ((mb2_eta & 0x10)>>4);
+        posBTI[5] = ((mb2_eta & 0x20)>>5);
+        posBTI[6] = (((mb2_eta & 0x40)>>6) || ((mb2_eta & 0x80)>>7));
+
+        qualBTI[0] = (mb2_eta_HQ & 0x01);
+        qualBTI[1] = ((mb2_eta_HQ & 0x02)>>1);
+        qualBTI[2] = ((mb2_eta_HQ & 0x04)>>2);
+        qualBTI[3] = ((mb2_eta_HQ & 0x08)>>3);
+        qualBTI[4] = ((mb2_eta_HQ & 0x10)>>4);
+        qualBTI[5] = ((mb2_eta_HQ & 0x20)>>5);
+        qualBTI[6] = (((mb2_eta_HQ & 0x40)>>6) || ((mb2_eta_HQ & 0x80)>>7));
 
         if ( posALL ) {
-            
-          theSegments.push_back( L1MuDTChambThDigi( bxNr, wheel, sector-1, 2, posBTI) );
-            
+            theSegments.push_back( L1MuDTChambThDigi( bxNr, wheel, sector-1, 2, posBTI, qualBTI) );
         }
+        
+        //MB3
+        posALL    = (mb3_eta & 0x7F);
+        posBTI[0] = (mb3_eta & 0x01);
+        posBTI[1] = ((mb3_eta & 0x02)>>1);
+        posBTI[2] = ((mb3_eta & 0x04)>>2);
+        posBTI[3] = ((mb3_eta & 0x08)>>3);
+        posBTI[4] = ((mb3_eta & 0x10)>>4);
+        posBTI[5] = ((mb3_eta & 0x20)>>5);
+        posBTI[6] = (((mb3_eta & 0x40)>>6) || ((mb3_eta & 0x80)>>7));
 
-        posALL    = (mb3_eta_HQ & 0x7F) + (mb3_eta & 0x7F);
-        posBTI[0] = ((mb3_eta_HQ & 0x01) + (mb3_eta & 0x01));
-        posBTI[1] = (((mb3_eta_HQ & 0x02)>>1) + ((mb3_eta & 0x02)>>1));
-        posBTI[2] = (((mb3_eta_HQ & 0x04)>>2) + ((mb3_eta & 0x04)>>2));
-        posBTI[3] = (((mb3_eta_HQ & 0x08)>>3) + ((mb3_eta & 0x08)>>3));
-        posBTI[4] = (((mb3_eta_HQ & 0x10)>>4) + ((mb3_eta & 0x10)>>4));
-        posBTI[5] = (((mb3_eta_HQ & 0x20)>>5) + ((mb3_eta & 0x20)>>5));
-        posBTI[6] = (((mb3_eta_HQ & 0x40)>>6) || ((mb3_eta_HQ & 0x80)>>7)) + (((mb3_eta & 0x40)>>6) || ((mb3_eta & 0x80)>>7));
+        qualBTI[0] = (mb3_eta_HQ & 0x01);
+        qualBTI[1] = ((mb3_eta_HQ & 0x02)>>1);
+        qualBTI[2] = ((mb3_eta_HQ & 0x04)>>2);
+        qualBTI[3] = ((mb3_eta_HQ & 0x08)>>3);
+        qualBTI[4] = ((mb3_eta_HQ & 0x10)>>4);
+        qualBTI[5] = ((mb3_eta_HQ & 0x20)>>5);
+        qualBTI[6] = (((mb3_eta_HQ & 0x40)>>6) || ((mb3_eta_HQ & 0x80)>>7));
 
         if ( posALL ) {
-            
-          theSegments.push_back( L1MuDTChambThDigi( bxNr, wheel, sector-1, 3, posBTI) );
-            
+            theSegments.push_back( L1MuDTChambThDigi( bxNr, wheel, sector-1, 3, posBTI, qualBTI) );
         }
-    
+        
       }//etha word
      
       else if ( selector == 0xB ) { //MB1/2 output word
