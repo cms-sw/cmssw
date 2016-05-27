cceee9e939d6d4da2ea3704d281218576000eeeb
erica brondolin <erica.brondolin@cern.ch>
2016-05-27 16:08:47 +0200

    fix phase2 DQM and validation

--- DQM/TrackingMonitorSource/python/TrackingSourceConfig_Tier0_cff.py
+++ DQM/TrackingMonitorSource/python/TrackingSourceConfig_Tier0_cff.py
@@ -178,7 +178,7 @@ for tracks in selectedTracks :
     label = 'TrackerCollisionSelectedTrackMonCommon' + str(tracks)
     TrackingDQMSourceTier0 += locals()[label]
 # seeding monitoring
-for era in _cfg.allEras():
+for era in _cfg.allEras() + ["trackingPhase2PU140"]: # FIXME:: allEras() extension should be removed when phase2 tracking is migrated to eras
     postfix = _cfg.postfix(era)
     _seq = cms.Sequence()
     for step in locals()["selectedIterTrackingStep"+postfix]:
