2d45be43de1a86de8267b997147d9763937d946b
Jose Cupertino Ruiz Vargas <jruizvar@cern.ch>
2016-05-28 20:59:35 +0200

    Add trackless anaysis

---
+++ HLTriggerOffline/Exotica/python/analyses/hltExoticaTracklessJets_cff.py
@@ -0,0 +1,17 @@
+import FWCore.ParameterSet.Config as cms
+
+TracklessJetsPSet = cms.PSet(
+    hltPathsToCheck = cms.vstring(
+        "HLT_PFHT650_v",
+        ),
+    recPFJetLabel   = cms.InputTag("ak4PFJets"),
+    recCaloJetLabel = cms.InputTag("ak4CaloJets"),
+
+    minCandidates = cms.uint32(1),
+    # -- Analysis specific binnings
+    parametersTurnOn = cms.vdouble(0, 50, 100, 150, 200, 250, 300, 350, 400, 450, 470, 
+                                   500, 550, 600, 650, 700, 800, 900, 1000
+                                  ),
+
+    dropPt3 = cms.bool(True)
+)
