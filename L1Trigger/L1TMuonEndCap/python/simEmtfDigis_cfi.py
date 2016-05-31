b67c7ef478f55fd146dadd1f075bf8411991c489
Andrew Brinkerhoff <abrinke1@cern.ch>
2016-05-31 22:41:53 +0200

    EMTF emulator configurable BX shift for input hits

--- L1Trigger/L1TMuonEndCap/python/simEmtfDigis_cfi.py
+++ L1Trigger/L1TMuonEndCap/python/simEmtfDigis_cfi.py
@@ -5,6 +5,10 @@ import FWCore.ParameterSet.Config as cms
 ##   * 'simCscTriggerPrimitiveDigis','MPCSORTED' : simulated trigger primitives (LCTs) from re-emulating CSC digis
 ##   * 'csctfDigis' : real trigger primitives as received by CSCTF (legacy trigger)
 ##   * 'emtfStage2Digis' : real trigger primitives as received by EMTF, unpacked in EventFilter/L1TRawToDigi/
+## Two options for CSCInputBxShift
+##   * -2 when using simCscTriggerPrimitiveDigis re-emulated from data
+##   *  0 in all other cases (MC, csctfDigis or emtfStage2Digis input)
 simEmtfDigis = cms.EDProducer("L1TMuonEndCapTrackProducer",
-                              CSCInput = cms.InputTag('simCscTriggerPrimitiveDigis','MPCSORTED')
+                              CSCInput = cms.InputTag('simCscTriggerPrimitiveDigis','MPCSORTED'),
+                              CSCInputBxShift = cms.untracked.int32(0)
                               )
