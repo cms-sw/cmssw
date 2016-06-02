a04aeaa33297b4c75a73b798f546af4fd94a2539
Sunanda <sunanda.banerjee@cern.ch>
2016-06-02 16:13:14 +0200

    Missed out changes

--- Calibration/HcalIsolatedTrackReco/python/isolPixelTrackProd_cfi.py
+++ Calibration/HcalIsolatedTrackReco/python/isolPixelTrackProd_cfi.py
@@ -2,7 +2,7 @@ import FWCore.ParameterSet.Config as cms
 
 #IsolatedPixelTrackCandidateProducer default configuration
 isolPixelTrackProd    = cms.EDProducer("IsolatedPixelTrackCandidateProducer",
-    L1eTauJetsSource           = cms.InputTag( 'hltL1extraParticles','Tau' ),
+    L1eTauJetsSource           = cms.InputTag('hltCaloStage2Digis','Tau'),
     tauAssociationCone         = cms.double( 0.0 ),
     tauUnbiasCone              = cms.double( 1.2 ),
     PixelTracksSources         = cms.VInputTag( "hltPixelTracks" ),
@@ -11,7 +11,7 @@ isolPixelTrackProd    = cms.EDProducer("IsolatedPixelTrackCandidateProducer",
     L1GTSeedLabel              = cms.InputTag( "hltL1sIsoTrack" ),
     MaxVtxDXYSeed              = cms.double( 101.0 ),
     MaxVtxDXYIsol              = cms.double( 101.0 ),
-    VertexLabel                = cms.InputTag( "hltPixelVertices" ),
+    VertexLabel                = cms.InputTag( "hltTrimmedPixelVertices" ),
     MagFieldRecordName         = cms.string("VolumeBasedMagneticField"),
     minPTrack                  = cms.double( 5.0 ),
     maxPTrackForIsolation      = cms.double( 3.0 ),
