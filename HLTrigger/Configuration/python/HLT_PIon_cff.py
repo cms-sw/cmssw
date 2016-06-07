ab3f13d606035c2df275ed6d897de0447ff84cd9
Martin Grunewald <Martin.Grunewald@cern.ch>
2016-06-07 12:35:06 +0200

    Remove LookArea

--- HLTrigger/Configuration/python/HLT_PIon_cff.py
+++ HLTrigger/Configuration/python/HLT_PIon_cff.py
@@ -1,11 +1,11 @@
-# /dev/CMSSW_8_0_0/PIon/V115 (CMSSW_8_0_10)
+# /dev/CMSSW_8_0_0/PIon/V116 (CMSSW_8_0_10)
 
 import FWCore.ParameterSet.Config as cms
 
 fragment = cms.ProcessFragment( "HLT" )
 
 fragment.HLTConfigVersion = cms.PSet(
-  tableName = cms.string('/dev/CMSSW_8_0_0/PIon/V115')
+  tableName = cms.string('/dev/CMSSW_8_0_0/PIon/V116')
 )
 
 fragment.HLTPSetInitialStepTrajectoryFilterBase = cms.PSet( 
