efe85cf6be99f6b1875efe35be90499e4e4c9c49
Kai-Feng <Kai-Feng.Chen@cern.ch>
2016-06-08 14:29:16 +0200

    Remove MuonAnalysis dependence

--- Configuration/Skimming/python/PDWG_BPHSkim_cff.py
+++ Configuration/Skimming/python/PDWG_BPHSkim_cff.py
@@ -3,9 +3,25 @@ import FWCore.ParameterSet.Config as cms
 from Configuration.StandardSequences.Reconstruction_cff import *
 
 # muons with trigger info
-from MuonAnalysis.MuonAssociators.patMuonsWithTrigger_cff import *
-selectedMuons = cms.EDFilter('PATMuonSelector',
-   src = cms.InputTag('patMuonsWithTrigger'),
+import PhysicsTools.PatAlgos.producersLayer1.muonProducer_cfi
+oniaPATMuonsWithoutTrigger = PhysicsTools.PatAlgos.producersLayer1.muonProducer_cfi.patMuons.clone(
+    muonSource = 'muons',
+    embedTrack          = True,
+    embedCombinedMuon   = True,
+    embedStandAloneMuon = True,
+    embedPFCandidate    = False,
+    embedCaloMETMuonCorrs = cms.bool(False),
+    embedTcMETMuonCorrs   = cms.bool(False),
+    embedPfEcalEnergy     = cms.bool(False),
+    embedPickyMuon = False,
+    embedTpfmsMuon = False, 
+    userIsolation = cms.PSet(),   # no extra isolation beyond what's in reco::Muon itself
+    isoDeposits = cms.PSet(),     # no heavy isodeposits
+    addGenMatch = False,          # no mc
+)
+
+oniaSelectedMuons = cms.EDFilter('PATMuonSelector',
+   src = cms.InputTag('oniaPATMuonsWithoutTrigger'),
    cut = cms.string('muonID(\"TMOneStationTight\")'
                     ' && abs(innerTrack.dxy) < 0.3'
                     ' && abs(innerTrack.dz)  < 20.'
@@ -18,7 +34,7 @@ selectedMuons = cms.EDFilter('PATMuonSelector',
 )
 
 # tracks
-selectedTracks=cms.EDFilter("TrackSelector",
+oniaSelectedTracks=cms.EDFilter("TrackSelector",
      src = cms.InputTag("generalTracks"),
      cut = cms.string('pt > 0.7 && abs(eta) <= 3.0'
                       '&& charge !=0'
@@ -27,7 +43,7 @@ selectedTracks=cms.EDFilter("TrackSelector",
 
 # dimuon = Onia2MUMU
 from HeavyFlavorAnalysis.Onia2MuMu.onia2MuMuPAT_cfi import *
-onia2MuMuPAT.muons=cms.InputTag('selectedMuons')
+onia2MuMuPAT.muons=cms.InputTag('oniaSelectedMuons')
 onia2MuMuPAT.primaryVertexTag=cms.InputTag('offlinePrimaryVertices')
 onia2MuMuPAT.beamSpotTag=cms.InputTag('offlineBeamSpot')
 onia2MuMuPAT.dimuonSelection=cms.string("0.2 < mass && abs(daughter('muon1').innerTrack.dz - daughter('muon2').innerTrack.dz) < 25")
@@ -40,7 +56,7 @@ onia2MuMuPATCounter = cms.EDFilter('CandViewCountFilter',
    )
 
 # make photon candidate conversions for P-wave studies
-from HeavyFlavorAnalysis.Onia2MuMu.OniaPhotonConversionProducer_cfi import *
+from HeavyFlavorAnalysis.Onia2MuMu.OniaPhotonConversionProducer_cfi import PhotonCandidates as oniaPhotonCandidates
 
 # add v0 with tracks embed
 from HeavyFlavorAnalysis.Onia2MuMu.OniaAddV0TracksProducer_cfi import *
@@ -53,8 +69,8 @@ BPHSkim_EventContent = cms.PSet(
                      'keep *_offlineBeamSpot_*_*',
                      'keep *_TriggerResults_*_HLT',
                      'keep *_gtDigis_*_RECO',
-                     'keep *_selectedTracks_*_*',
-                     'keep *_PhotonCandidates_*_*',
+                     'keep *_oniaSelectedTracks_*_*',
+                     'keep *_oniaPhotonCandidates_*_*',
                      'keep *_onia2MuMuPAT_*_*',
                      'keep *_oniaV0Tracks_*_*',
                      'keep PileupSummaryInfos_*_*_*'
@@ -62,11 +78,11 @@ BPHSkim_EventContent = cms.PSet(
 )
 
 BPHSkimSequence = cms.Sequence(
-            patMuonsWithTriggerSequence *
-	    selectedMuons *
+            oniaPATMuonsWithoutTrigger *
+	    oniaSelectedMuons *
             onia2MuMuPAT *
 	    onia2MuMuPATCounter *
-	    PhotonCandidates *
+	    oniaPhotonCandidates *
 	    oniaV0Tracks *
-	    selectedTracks
+	    oniaSelectedTracks
 )
