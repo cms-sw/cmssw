a2f6d21714d286ea63aedaf8c23df92ddad0b689
Ernesto Migliore <migliore@to.infn.it>
2016-06-09 14:49:14 +0200

    add phase2 Inner Tracker pixel clusterizer/ v3

--- DataFormats/Phase2ITPixelCluster/src/classes.h
+++ DataFormats/Phase2ITPixelCluster/src/classes.h
@@ -1,35 +1,35 @@
 #ifndef DataFormats_Phase2ITPixelCluster_classes_h
 #define DataFormats_Phase2ITPixelCluster_classes_h
-#include "DataFormats/Phase2ITPixelCluster/interface/Phase2PixelCluster.h"
+#include "DataFormats/Phase2ITPixelCluster/interface/Phase2ITPixelCluster.h"
 #include "DataFormats/Common/interface/RefProd.h" 
 #include "DataFormats/Common/interface/Wrapper.h"
 #include "DataFormats/Common/interface/ContainerMask.h"
-#include "DataFormats/Phase2ITPixelCluster/interface/Phase2PixelClusterShapeCache.h"
+#include "DataFormats/Phase2ITPixelCluster/interface/Phase2ITPixelClusterShapeCache.h"
 
-namespace DataFormats_Phase2PixelCluster {
+namespace DataFormats_Phase2ITPixelCluster {
   struct dictionary {
-    std::vector<Phase2PixelCluster> v1;
-    edm::DetSet<Phase2PixelCluster> ds1;
-    std::vector<edm::DetSet<Phase2PixelCluster> > vds1;
-    Phase2PixelClusterCollection c1;
-    Phase2PixelClusterCollectionNew c1_new;
-    edm::Wrapper<Phase2PixelClusterCollection> w1;
-    edm::Wrapper<Phase2PixelClusterCollectionNew> w1_new;
-    Phase2PixelClusterRef r1;
-    Phase2PixelClusterRefNew r1_new;
-    Phase2PixelClusterRefVector rv1;
-    Phase2PixelClusterRefProd rp1;
-    edm::Ref<edm::DetSetVector<Phase2PixelCluster>,edm::DetSet<Phase2PixelCluster>,edm::refhelper::FindDetSetForDetSetVector<Phase2PixelCluster,edm::DetSetVector<Phase2PixelCluster> > > boguscrap;
+    std::vector<Phase2ITPixelCluster> v1;
+    edm::DetSet<Phase2ITPixelCluster> ds1;
+    std::vector<edm::DetSet<Phase2ITPixelCluster> > vds1;
+    Phase2ITPixelClusterCollection c1;
+    Phase2ITPixelClusterCollectionNew c1_new;
+    edm::Wrapper<Phase2ITPixelClusterCollection> w1;
+    edm::Wrapper<Phase2ITPixelClusterCollectionNew> w1_new;
+    Phase2ITPixelClusterRef r1;
+    Phase2ITPixelClusterRefNew r1_new;
+    Phase2ITPixelClusterRefVector rv1;
+    Phase2ITPixelClusterRefProd rp1;
+    edm::Ref<edm::DetSetVector<Phase2ITPixelCluster>,edm::DetSet<Phase2ITPixelCluster>,edm::refhelper::FindDetSetForDetSetVector<Phase2ITPixelCluster,edm::DetSetVector<Phase2ITPixelCluster> > > boguscrap;
 
-    std::vector<edm::Ref<edmNew::DetSetVector<Phase2PixelCluster>,Phase2PixelCluster,edmNew::DetSetVector<Phase2PixelCluster>::FindForDetSetVector> > dsvr_v;
-    edmNew::DetSetVector<edm::Ref<edmNew::DetSetVector<Phase2PixelCluster>,Phase2PixelCluster,edmNew::DetSetVector<Phase2PixelCluster>::FindForDetSetVector> > dsvr;
-    edm::Wrapper<edmNew::DetSetVector<edm::Ref<edmNew::DetSetVector<Phase2PixelCluster>,Phase2PixelCluster,edmNew::DetSetVector<Phase2PixelCluster>::FindForDetSetVector> > > dsvr_w;
+    std::vector<edm::Ref<edmNew::DetSetVector<Phase2ITPixelCluster>,Phase2ITPixelCluster,edmNew::DetSetVector<Phase2ITPixelCluster>::FindForDetSetVector> > dsvr_v;
+    edmNew::DetSetVector<edm::Ref<edmNew::DetSetVector<Phase2ITPixelCluster>,Phase2ITPixelCluster,edmNew::DetSetVector<Phase2ITPixelCluster>::FindForDetSetVector> > dsvr;
+    edm::Wrapper<edmNew::DetSetVector<edm::Ref<edmNew::DetSetVector<Phase2ITPixelCluster>,Phase2ITPixelCluster,edmNew::DetSetVector<Phase2ITPixelCluster>::FindForDetSetVector> > > dsvr_w;
 
-    edm::ContainerMask<Phase2PixelClusterCollectionNew> cm1;
-    edm::Wrapper<edm::ContainerMask<Phase2PixelClusterCollectionNew> > w_cm1;
+    edm::ContainerMask<Phase2ITPixelClusterCollectionNew> cm1;
+    edm::Wrapper<edm::ContainerMask<Phase2ITPixelClusterCollectionNew> > w_cm1;
 
-    Phase2PixelClusterShapeCache clusterShapeCache;
-    edm::Wrapper<Phase2PixelClusterShapeCache> wclusterShapeCache;
+    Phase2ITPixelClusterShapeCache clusterShapeCache;
+    edm::Wrapper<Phase2ITPixelClusterShapeCache> wclusterShapeCache;
   };
 }
 
