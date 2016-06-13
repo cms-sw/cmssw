6f18bf5e71088dc6e3d84b0c40ab34f25212f96d
Lorenzo <lore.bianchini@gmail.com>
2016-06-13 18:06:12 +0200

    added new MC pu

--- VHbbAnalysis/Heppy/test/Lumi/makeMChisto.py
+++ VHbbAnalysis/Heppy/test/Lumi/makeMChisto.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 
 #CHANGE THIS LINE TO CHANGE PU SCENARIO
-from SimGeneral.MixingModule import mix_2015_25ns_FallMC_matchData_PoissonOOTPU_cfi as pu
+from SimGeneral.MixingModule import mix_2016_25ns_SpringMC_PUScenarioV1_PoissonOOTPU_cfi as pu
 
 import ROOT
 n=len(pu.mix.input.nbPileupEvents.probValue )
