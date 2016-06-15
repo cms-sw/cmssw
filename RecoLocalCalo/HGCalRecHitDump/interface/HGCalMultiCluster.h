8e0e17b517690c71fb619fc9dc4d9138a5edd3ca
Emilio <Emilio.Meschi@cern.ch>
2016-06-15 09:57:37 +0200

    Additions to HGCalMultiCluster

--- RecoLocalCalo/HGCalRecHitDump/interface/HGCalMultiCluster.h
+++ RecoLocalCalo/HGCalRecHitDump/interface/HGCalMultiCluster.h
@@ -25,14 +25,66 @@ public:
   {
     return myclusters.size();
   }						
-
-  component_iterator begin(){
+  component_iterator begin() const {
     return myclusters.begin();
   }
-  component_iterator end(){
+  component_iterator end() const {
     return myclusters.end();
   }
+  double simple_z(double vz) const {
+    double acc = 0.0;
+    double totweight = 0.;
+    for(component_iterator it = begin(); it != end(); it++){
+      acc += (it->z()-vz)*it->energy()*it->size();
+      totweight+=it->energy()*it->size();
+    }
+    return acc/totweight;
+  }
 
+  double simple_slope_x(double vz) const {
+    double acc = 0.0;
+    double totweight = 0.;
+    for(component_iterator it = begin(); it != end(); it++){
+      acc += it->x()/(it->z()-vz)*it->energy()*it->size();
+      totweight+=it->energy()*it->size();
+    }
+    return acc/totweight;
+  }
+  double simple_slope_y(double vz) const {
+    double acc = 0.0;
+    double totweight = 0.;
+    for(component_iterator it = begin(); it != end(); it++){
+      acc += it->y()/(it->z()-vz)*it->energy()*it->size();
+      totweight+=it->energy()*it->size();
+    }
+    return acc/totweight;
+  }
+  double simple_eta(double vz) const {
+    return -1.*
+      simple_z(vz)/abs(simple_z(vz))*
+      log(tan(atan(sqrt(pow(simple_slope_x(vz),2)+pow(simple_slope_y(vz),2)))/2.));
+  }
+
+  double simple_phi() const {
+    double acc = 0.0;
+    int n = 0;
+    for(component_iterator it = begin(); it != end(); it++){
+      acc += atan2(it->y(),it->x());
+      n++;
+    }
+    return acc/n;
+  }
+
+  double total_uncalibrated_energy() const{
+    double acc = 0.0;
+    for(component_iterator it = begin(); it != end(); it++){
+      acc += it->energy();
+    }
+    return acc;
+  }
+  bool operator > (const HGCalMultiCluster& rhs) const { 
+    return (total_uncalibrated_energy() > rhs.total_uncalibrated_energy()); 
+  }
 private:
 
   std::vector<reco::BasicCluster> myclusters;
