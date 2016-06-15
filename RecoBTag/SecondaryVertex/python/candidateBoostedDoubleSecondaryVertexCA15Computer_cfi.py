62a484d3cd3790866b327a76e8d093f1a396707f
Dinko Ferencek <Dinko.Ferencek@cern.ch>
2016-06-15 10:10:02 -0500

    switched to v3 training for 80X

--- RecoBTag/SecondaryVertex/python/candidateBoostedDoubleSecondaryVertexCA15Computer_cfi.py
+++ RecoBTag/SecondaryVertex/python/candidateBoostedDoubleSecondaryVertexCA15Computer_cfi.py
@@ -8,7 +8,7 @@ candidateBoostedDoubleSecondaryVertexCA15Computer = cms.ESProducer("CandidateBoo
     R0 = cms.double(1.5),
     maxSVDeltaRToJet = cms.double(1.3),
     useCondDB = cms.bool(False),
-    weightFile = cms.FileInPath('RecoBTag/SecondaryVertex/data/BoostedDoubleSV_CA15_BDT_v2.weights.xml.gz'),
+    weightFile = cms.FileInPath('RecoBTag/SecondaryVertex/data/BoostedDoubleSV_CA15_BDT_v3.weights.xml.gz'),
     useGBRForest = cms.bool(True),
     useAdaBoost = cms.bool(False),
     trackPairV0Filter = cms.PSet(k0sMassWindow = cms.double(0.03))
