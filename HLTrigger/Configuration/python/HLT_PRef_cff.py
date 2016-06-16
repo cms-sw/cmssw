c1bf631ee5082d8e3b6a5bf5ebfdfbea26862e51
Martin Grunewald <Martin.Grunewald@cern.ch>
2016-06-16 10:50:04 +0200

    Development 2

--- HLTrigger/Configuration/python/HLT_PRef_cff.py
+++ HLTrigger/Configuration/python/HLT_PRef_cff.py
@@ -1,11 +1,11 @@
-# /dev/CMSSW_8_0_0/PRef/V123 (CMSSW_8_0_10)
+# /dev/CMSSW_8_0_0/PRef/V124 (CMSSW_8_0_10)
 
 import FWCore.ParameterSet.Config as cms
 
 fragment = cms.ProcessFragment( "HLT" )
 
 fragment.HLTConfigVersion = cms.PSet(
-  tableName = cms.string('/dev/CMSSW_8_0_0/PRef/V123')
+  tableName = cms.string('/dev/CMSSW_8_0_0/PRef/V124')
 )
 
 fragment.HLTPSetInitialStepTrajectoryFilterBase = cms.PSet( 
