8801a4d45b1237fb30a03543e8f9f13a4c347117
Ernesto Migliore <migliore@to.infn.it>
2016-06-17 16:43:44 +0200

    further clean-up for CXX11; wrong header in Phase2ITPixelClustersValidation.cc

--- RecoLocalTracker/Phase2ITPixelClusterizer/test/Phase2ITPixelClustersValidation.cc
+++ RecoLocalTracker/Phase2ITPixelClusterizer/test/Phase2ITPixelClustersValidation.cc
@@ -3,7 +3,7 @@
 #include <algorithm>
 
 #include "FWCore/Framework/interface/Frameworkfwd.h"
-#include "FWCore/Framework/interface/EDAnalyzer.h"
+#include "FWCore/Framework/interface/one/EDAnalyzer.h"
 #include "FWCore/Framework/interface/Event.h"
 #include "FWCore/Framework/interface/MakerMacros.h"
 #include "FWCore/ParameterSet/interface/ParameterSet.h"
@@ -64,7 +64,7 @@ struct ClusterHistos {
     TH1F* otherSimHits;
 };
 
-class Phase2ITPixelClustersValidation : public edm::one::EDAnalyzer {
+class Phase2ITPixelClustersValidation : public edm::one::EDAnalyzer<> {
 
     public:
 
