ccd3201a2debab86e32c99a511ec4bf0c4d73135
Dinko Ferencek <Dinko.Ferencek@cern.ch>
2016-06-21 06:27:00 -0500

    reduced the maxSVDeltaRToJet cut for CA15 jets

--- RecoBTag/SecondaryVertex/python/candidateBoostedDoubleSecondaryVertexCA15Computer_cfi.py
+++ RecoBTag/SecondaryVertex/python/candidateBoostedDoubleSecondaryVertexCA15Computer_cfi.py
@@ -6,7 +6,7 @@ candidateBoostedDoubleSecondaryVertexCA15Computer = cms.ESProducer("CandidateBoo
     trackSelectionBlock,
     beta = cms.double(1.0),
     R0 = cms.double(1.5),
-    maxSVDeltaRToJet = cms.double(1.3),
+    maxSVDeltaRToJet = cms.double(1.0),
     useCondDB = cms.bool(False),
     weightFile = cms.FileInPath('RecoBTag/SecondaryVertex/data/BoostedDoubleSV_CA15_BDT_v3.weights.xml.gz'),
     useGBRForest = cms.bool(True),
