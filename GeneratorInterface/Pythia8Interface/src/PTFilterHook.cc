77633cfa15b741f5c4fa2859bc2c1f1c829704b2
Alberto Sanchez Hernandez <alberto.sanchez@mail.com>
2016-06-21 21:45:08 -0500

    adding pt filter hook

---
+++ GeneratorInterface/Pythia8Interface/src/PTFilterHook.cc
@@ -0,0 +1,34 @@
+#include "Pythia8/Pythia.h"
+#include "GeneratorInterface/Pythia8Interface/interface/PTFilterHook.h"
+
+using namespace Pythia8;
+
+
+//--------------------------------------------------------------------------
+bool PTFilterHook::initAfterBeams() {
+  filter_ = settingsPtr->flag("PTFilter:filter");
+  quark_  = settingsPtr->mode("PTFilter:quarkToFilter");
+  scale_  = settingsPtr->parm("PTFilter:scaleToFilter");
+  
+  return true;
+  
+}
+
+//--------------------------------------------------------------------------
+bool PTFilterHook::checkVetoPT( int iPos, const Pythia8::Event& event) {
+
+  if (!filter_ || iPos >3 ) return false;
+  bool foundQuark = false;
+  
+  //look for quark
+  for (int i = 0; i < event.size(); ++i) {
+    if ( abs(event[i].id()) == quark_ ) {
+       foundQuark = true;
+       break;
+    }
+  }
+
+  if (!foundQuark) return true;
+  //all criteria satisfied, don't veto
+  return false;
+}
