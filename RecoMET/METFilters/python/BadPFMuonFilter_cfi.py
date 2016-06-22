da737b1558b66f0eed4799328acbff07ed1b21ac
maria <dalfonso@cern.ch>
2016-06-22 11:41:53 +0200

    update the muon filter

--- RecoMET/METFilters/python/BadPFMuonFilter_cfi.py
+++ RecoMET/METFilters/python/BadPFMuonFilter_cfi.py
@@ -5,6 +5,7 @@ BadPFMuonFilter = cms.EDFilter(
     PFCandidates  = cms.InputTag("particleFlow"),   # Collection to test
     taggingMode   = cms.bool(False),
     debug         = cms.bool(False),
+    algo          = cms.int32(14),
     minDZ         = cms.double(0.1),              # dz threshold on PF muons to consider
     minMuPt       = cms.double(100),               # pt threshold on PF muons 
     minTrkPtError  = cms.double(-1),               # threshold on inner track pt Error 
