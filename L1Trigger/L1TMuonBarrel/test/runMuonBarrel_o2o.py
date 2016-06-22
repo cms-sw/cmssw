5a209162171aca0d2673024897dadd99ec807783
gflouris <flourisj@gmail.com>
2016-06-22 18:59:32 +0200

    py original and for o2o

--- L1Trigger/L1TMuonBarrel/test/runMuonBarrel_o2o.py
+++ L1Trigger/L1TMuonBarrel/test/runMuonBarrel_o2o.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 import FWCore.ParameterSet.Config as cms
-process = cms.Process("L1TMuonEmulation")
+process = cms.Process("L1TMuonEmulationO2O")
 import os
 import sys
 import commands
@@ -11,22 +11,26 @@ process.MessageLogger.cerr.FwkReport.reportEvery = cms.untracked.int32(50)
 process.options = cms.untracked.PSet(wantSummary = cms.untracked.bool(False))
 
 process.source = cms.Source('PoolSource',
- fileNames = cms.untracked.vstring('file:/afs/cern.ch/work/g/gflouris/public/SingleMuPt6180_noanti_10k_eta1.root')
+ fileNames = cms.untracked.vstring('file:/afs/cern.ch/user/g/gflouris/public/l1tbmtf.root')
 	                    )
 
-process.maxEvents = cms.untracked.PSet( input = cms.untracked.int32(10))
+process.maxEvents = cms.untracked.PSet( input = cms.untracked.int32(-1))
 
 # PostLS1 geometry used
 process.load('Configuration.Geometry.GeometryExtended2015Reco_cff')
 process.load('Configuration.Geometry.GeometryExtended2015_cff')
 ############################
-process.load('Configuration.StandardSequences.FrontierConditions_GlobalTag_condDBv2_cff')
-from Configuration.AlCa.GlobalTag_condDBv2 import GlobalTag
-process.GlobalTag = GlobalTag(process.GlobalTag, 'auto:run2_mc', '')
+#process.load('Configuration.StandardSequences.FrontierConditions_GlobalTag_condDBv2_cff')
+#from Configuration.AlCa.GlobalTag_condDBv2 import GlobalTag
+#process.GlobalTag = GlobalTag(process.GlobalTag, 'auto:run2_mc', '')
 
 
 ####Event Setup Producer
 process.load('L1Trigger.L1TMuonBarrel.fakeBmtfParams_cff')
+process.fakeBmtfParams.configFromXML = cms.bool(True)
+process.fakeBmtfParams.hwXmlFile = cms.string('L1Trigger/L1TMuon/data/o2o/bmtf/BMTF_HW.xml')
+process.fakeBmtfParams.topCfgXmlFile = cms.string('L1Trigger/L1TMuon/data/o2o/bmtf/bmtf_top_config_p5.xml')
+
 process.esProd = cms.EDAnalyzer("EventSetupRecordDataGetter",
    toGet = cms.VPSet(
       cms.PSet(record = cms.string('L1TMuonBarrelParamsRcd'),
@@ -45,10 +49,10 @@ process.dumpED = cms.EDAnalyzer("EventContentAnalyzer")
 process.dumpES = cms.EDAnalyzer("PrintEventSetupContent")
 
 process.L1TMuonSeq = cms.Sequence( process.esProd          
-				   + process.simTwinMuxDigis 
+#				   + process.simTwinMuxDigis 
                                    + process.simBmtfDigis 
-                                   + process.dumpED
-                                   + process.dumpES
+#                                   + process.dumpED
+#                                   + process.dumpES
 )
 
 process.L1TMuonPath = cms.Path(process.L1TMuonSeq)
