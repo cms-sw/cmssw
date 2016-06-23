58d4a753ae0739c3871171c094185e4be1a5a35b
Martin Grunewald <Martin.Grunewald@cern.ch>
2016-06-23 20:58:17 +0200

    Fix Datasets extraction

--- HLTrigger/Configuration/python/HLTrigger_Datasets_PIon_cff.py
+++ HLTrigger/Configuration/python/HLTrigger_Datasets_PIon_cff.py
@@ -3,6 +3,31 @@
 import FWCore.ParameterSet.Config as cms
 
 
+# dump of the Stream PhysicsCommissioning Datasets defined in the HLT table as Stream A Datasets
+
+from HLTrigger.HLTfilters.triggerResultsFilter_cfi import triggerResultsFilter as streamA_datasetHLTPhysics_selector
+streamA_datasetHLTPhysics_selector.hltResults = cms.InputTag('TriggerResults', '', 'HLT')
+streamA_datasetHLTPhysics_selector.l1tResults = cms.InputTag('')
+streamA_datasetHLTPhysics_selector.throw      = cms.bool(False)
+streamA_datasetHLTPhysics_selector.triggerConditions = cms.vstring('HLT_Physics_v3')
+
+from HLTrigger.HLTfilters.triggerResultsFilter_cfi import triggerResultsFilter as streamA_datasetTemplates_selector
+streamA_datasetTemplates_selector.hltResults = cms.InputTag('TriggerResults', '', 'HLT')
+streamA_datasetTemplates_selector.l1tResults = cms.InputTag('')
+streamA_datasetTemplates_selector.throw      = cms.bool(False)
+streamA_datasetTemplates_selector.triggerConditions = cms.vstring('HLT_CaloJet260_v3', 
+    'HLT_Photon20_CaloIdVL_IsoL_v5')
+
+
+# dump of the Stream PhysicsEGamma Datasets defined in the HLT table as Stream A Datasets
+
+from HLTrigger.HLTfilters.triggerResultsFilter_cfi import triggerResultsFilter as streamA_datasetDoubleEG_selector
+streamA_datasetDoubleEG_selector.hltResults = cms.InputTag('TriggerResults', '', 'HLT')
+streamA_datasetDoubleEG_selector.l1tResults = cms.InputTag('')
+streamA_datasetDoubleEG_selector.throw      = cms.bool(False)
+streamA_datasetDoubleEG_selector.triggerConditions = cms.vstring('HLT_Ele17_CaloIdL_TrackIdL_IsoVL_v5')
+
+
 # dump of the Stream PhysicsHadronsTaus Datasets defined in the HLT table as Stream A Datasets
 
 from HLTrigger.HLTfilters.triggerResultsFilter_cfi import triggerResultsFilter as streamA_datasetJetHT_selector
