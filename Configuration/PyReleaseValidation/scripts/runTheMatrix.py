431714d5c6a64ee7475be76f8b19459ce0110374
Kevin Pedro <kpedro88@gmail.com>
2016-06-25 00:16:23 -0500

    add Phase2 workflows to PR tests

--- Configuration/PyReleaseValidation/scripts/runTheMatrix.py
+++ Configuration/PyReleaseValidation/scripts/runTheMatrix.py
@@ -56,7 +56,9 @@ if __name__ == '__main__':
                      136.731, #2016B Photon data
                      140.53, #2011 HI data
                      1330, #Run2 MC Zmm
-                     135.4 #Run 2 Zee ttbar
+                     135.4, #Run 2 Zee ttbar
+                     11024.0, #2023LReco ttbar (w/ HGCal)
+                     11224.0  #2023GReco ttbar (flat tracker, Run2 calo)
                      ],
         'jetmc': [5.1, 13, 15, 25, 38, 39], #MC
         'metmc' : [5.1, 15, 25, 37, 38, 39], #MC
