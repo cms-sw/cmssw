2c0a3d6d5be54768a1dd1a90c9591c59f5f2023a
erica brondolin <erica.brondolin@cern.ch>
2016-06-25 15:12:05 +0200

    moving to eras

--- RecoLocalTracker/Configuration/python/RecoLocalTracker_EventContent_cff.py
+++ RecoLocalTracker/Configuration/python/RecoLocalTracker_EventContent_cff.py
@@ -7,7 +7,6 @@ RecoLocalTrackerFEVT = cms.PSet(
     'keep DetIdedmEDCollection_siPixelDigis_*_*',
     'keep *_siPixelClusters_*_*', 
     'keep *_siStripClusters_*_*',
-    'keep *_siPhase2Clusters_*_*',
     'keep *_clusterSummaryProducer_*_*')
 )
 #RECO content
@@ -17,7 +16,6 @@ RecoLocalTrackerRECO = cms.PSet(
     'keep DetIdedmEDCollection_siPixelDigis_*_*',
     'keep *_siPixelClusters_*_*', 
     'keep *_siStripClusters_*_*',
-    'keep *_siPhase2Clusters_*_*',
     'keep ClusterSummary_clusterSummaryProducer_*_*')
 )
 #AOD content
@@ -25,3 +23,10 @@ RecoLocalTrackerAOD = cms.PSet(
     outputCommands = cms.untracked.vstring('keep ClusterSummary_clusterSummaryProducer_*_*')
 )
 
+from Configuration.StandardSequences.Eras import eras
+def _updateOutput( era, outputPSets, commands):
+   for o in outputPSets:
+      era.toModify( o, outputCommands = o.outputCommands + commands )
+
+_outputs = [RecoLocalTrackerFEVT, RecoLocalTrackerRECO]
+_updateOutput(eras.phase2_tracker, _outputs, ['keep *_siPhase2Clusters_*_*'])
