340444cb5b1b457dab5fa0e33af303a858f4e807
Slava Krutelyov <slava77@cern.ch>
2016-06-28 20:45:22 +0200

    demote to LogWarning for all

--- TrackingTools/TrackFitters/src/TrajectoryStateCombiner.cc
+++ TrackingTools/TrackFitters/src/TrajectoryStateCombiner.cc
@@ -18,7 +18,7 @@ TrajectoryStateCombiner::combine(const TSOS& Tsos1, const TSOS& Tsos2) const {
 
   if(!ok) {
     if (! (C1(0,0) == 0.0 && C2(0,0) == 0.0) )//do not make noise about obviously bad input
-      edm::LogError("MatrixInversionFailure")
+      edm::LogWarning("MatrixInversionFailure")
 	<<"the inversion of the combined error matrix failed. Impossible to get a combined state."
 	<<"\nmatrix 1:"<<C1
 	<<"\nmatrix 2:"<<C2;
