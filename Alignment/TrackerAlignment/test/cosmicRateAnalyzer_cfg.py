2416497a676ba586df97f87081e11e3d428184bb
skeshri <sumitkesri@gmail.com>
2016-07-12 14:41:56 +0200

    use auto generated cosmicrateAnalyze_cfi file

--- Alignment/TrackerAlignment/test/cosmicRateAnalyzer_cfg.py
+++ Alignment/TrackerAlignment/test/cosmicRateAnalyzer_cfg.py
@@ -1,12 +1,11 @@
 # Implementation : Description of running this tool can be find  at :
 # https://twiki.cern.ch/twiki/bin/view/CMS/TkAlCosmicsRateMonitoring
 
-
 import FWCore.ParameterSet.Config as cms
 
 import os
 
-process = cms.Process("Demo")
+process = cms.Process("cosmicRateAnalyzer")
 process.load("FWCore.MessageLogger.MessageLogger_cfi")
 
 process.load('Configuration.StandardSequences.MagneticField_cff') # B-field map
@@ -18,25 +17,20 @@ process.GlobalTag.globaltag = GlobalTag("80X_dataRun2_Prompt_v2")
 process.options = cms.untracked.PSet(
      SkipEvent= cms.untracked.vstring("ProductNotFound"), # make this exception fatal
   )
-process.maxEvents=cms.untracked.PSet(input=cms.untracked.int32(-1)) 
+process.maxEvents=cms.untracked.PSet(input=cms.untracked.int32(10000)) 
 
 import FWCore.Utilities.FileUtils as FileUtils
  
 readFiles = cms.untracked.vstring()
 
 
-readFiles = cms.untracked.vstring( FileUtils.loadListFromFile (os.environ['CMSSW_BASE']+'/src/Alignment/TrackerAlignment/test/'+'fileList.txt') )
+readFiles = cms.untracked.vstring( FileUtils.loadListFromFile (os.environ['CMSSW_BASE']+'/src/CosmicRateTool/TrackAnalyzer/test/'+'fileList.txt') )
 process.source = cms.Source("PoolSource",
 			   fileNames = readFiles,
 			   )
 
 process.TFileService = cms.Service("TFileService", fileName = cms.string("Cosmic_rate_tuple.root") )
-process.demo = cms.EDAnalyzer("CosmicRateAnalyzer",
-#				tracks = cms.InputTag("ctfWithMaterialTracksP5"),                # Track collection for prompt RECO Dataset
-				tracksInputTag = cms.InputTag("ALCARECOTkAlCosmicsCTF0T"),		 # Track collection for stream and prompt ALCARECO Dataset
-				muonsInputTag = cms.InputTag("muons1Leg"),		 # for muon Trigger timing information  
-)
 
-process.p = cms.Path(process.demo)
-
+process.load("CosmicRateTool.TrackAnalyzer.cosmicRateAnalyzer_cfi")
+process.p = cms.Path(process.cosmicRateAnalyzer)
 
