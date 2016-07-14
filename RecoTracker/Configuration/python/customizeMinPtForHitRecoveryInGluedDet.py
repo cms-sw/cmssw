9cd2396b8613b7b1204bafa64008d29996c9a060
Vincenzo Innocente <vincenzo.innocente@cern.ch>
2016-07-14 16:27:20 +0200

    change name to be explicit

--- RecoTracker/Configuration/python/customizeMinPtForHitRecoveryInGluedDet.py
+++ RecoTracker/Configuration/python/customizeMinPtForHitRecoveryInGluedDet.py
@@ -1,7 +1,11 @@
 def esproducers_by_type(process, *types):
     return (module for module in process._Process__esproducers.values() if module._TypedParameterizable__type in types)
-def customizeMinPtForHitRecoveryInGluedDet(process):
+def customizeMinPtForHitRecoveryInGluedDet(process,value):
    for esp in esproducers_by_type(process, "Chi2MeasurementEstimatorESProducer", "Chi2ChargeMeasurementEstimatorESProducer"):
        esp.MinPtForHitRecoveryInGluedDet = 100000
+def customizeHitRecoveryInGluedDetOff(process):
+   for esp in esproducers_by_type(process, "Chi2MeasurementEstimatorESProducer", "Chi2ChargeMeasurementEstimatorESProducer"):
+       customizeMinPtForHitRecoveryInGluedDet(process,1000000)
+
    return process
 
