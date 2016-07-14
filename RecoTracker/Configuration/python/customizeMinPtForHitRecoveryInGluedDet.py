be66f0b441ea4544a72355ed766b8c7364ec93df
Vincenzo Innocente <vincenzo.innocente@cern.ch>
2016-07-14 16:58:47 +0200

    fix the customize

--- RecoTracker/Configuration/python/customizeMinPtForHitRecoveryInGluedDet.py
+++ RecoTracker/Configuration/python/customizeMinPtForHitRecoveryInGluedDet.py
@@ -2,10 +2,9 @@ def esproducers_by_type(process, *types):
     return (module for module in process._Process__esproducers.values() if module._TypedParameterizable__type in types)
 def customizeMinPtForHitRecoveryInGluedDet(process,value):
    for esp in esproducers_by_type(process, "Chi2MeasurementEstimatorESProducer", "Chi2ChargeMeasurementEstimatorESProducer"):
-       esp.MinPtForHitRecoveryInGluedDet = 100000
-def customizeHitRecoveryInGluedDetOff(process):
-   for esp in esproducers_by_type(process, "Chi2MeasurementEstimatorESProducer", "Chi2ChargeMeasurementEstimatorESProducer"):
-       customizeMinPtForHitRecoveryInGluedDet(process,1000000)
-
+       esp.MinPtForHitRecoveryInGluedDet = value
    return process
+def customizeHitRecoveryInGluedDetOff(process):
+   return customizeMinPtForHitRecoveryInGluedDet(process,1000000)
+
 
