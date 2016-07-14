ad20348df18e26e0e837341d79555b2f307d1aae
skeshri <sumitkesri@gmail.com>
2016-07-14 11:01:17 +0200

    replace specific global tag to auto:run2_data

--- Alignment/TrackerAlignment/test/cosmicRateAnalyzer_cfg.py
+++ Alignment/TrackerAlignment/test/cosmicRateAnalyzer_cfg.py
@@ -12,8 +12,7 @@ process.load('Configuration.StandardSequences.MagneticField_cff') # B-field map
 process.load('Configuration.Geometry.GeometryRecoDB_cff') # Ideal geometry and interface
 process.load("Configuration.StandardSequences.FrontierConditions_GlobalTag_cff") # Global tag
 from Configuration.AlCa.GlobalTag import GlobalTag
-process.GlobalTag.globaltag = GlobalTag("80X_dataRun2_Prompt_v2")
-
+process.GlobalTag = GlobalTag(process.GlobalTag, 'auto:run2_data', '')
 process.options = cms.untracked.PSet(
      SkipEvent= cms.untracked.vstring("ProductNotFound"), # make this exception fatal
   )
