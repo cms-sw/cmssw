3681ad100013a216b09c5642c1be8f7fd2ee0355
Ivan Shvetsov <ivan.shvetsov@cern.ch>
2016-07-15 13:18:27 +0200

    changes for test

--- RecoParticleFlow/PFProducer/python/particleFlowEGamma_cff.py
+++ RecoParticleFlow/PFProducer/python/particleFlowEGamma_cff.py
@@ -8,6 +8,14 @@ from RecoParticleFlow.PFProducer.particleFlowEGamma_cfi import *
 from RecoEgamma.EgammaPhotonProducers.gedPhotonSequence_cff import *
 from RecoEgamma.EgammaElectronProducers.gedGsfElectronSequence_cff import *
 from RecoEgamma.EgammaElectronProducers.pfBasedElectronIso_cff import *
+from RecoEgamma.EgammaIsolationAlgos.particleBasedIsoProducer_cfi import *
+
+particleBasedIsolationTmp = particleBasedIsolation.clone()
+particleBasedIsolationTmp.photonProducer =  cms.InputTag("gedPhotonsTmp")
+particleBasedIsolationTmp.electronProducer = cms.InputTag("gedGsfElectronsTmp")
+particleBasedIsolationTmp.pfCandidates = cms.InputTag("particleFlowTmp")
+particleBasedIsolationTmp.valueMapPhoPFblockIso = cms.string("gedPhotons")
+particleBasedIsolationTmp.valueMapElePFblockIso = cms.string("gedGsfElectrons")
 
 particleFlowEGammaFull = cms.Sequence(particleFlowEGamma*gedGsfElectronSequenceTmp*gedPhotonSequenceTmp)
-particleFlowEGammaFinal = cms.Sequence(gedPhotonSequence*gedElectronPFIsoSequence)
+particleFlowEGammaFinal = cms.Sequence(particleBasedIsolationTmp*gedPhotonSequence*gedElectronPFIsoSequence)
