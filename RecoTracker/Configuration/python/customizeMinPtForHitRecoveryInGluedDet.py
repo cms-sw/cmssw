6c26b4e7bc4e598fb721d1e8a9c6a996dbf6cda7
Vincenzo Innocente <vincenzo.innocente@cern.ch>
2016-07-16 17:02:24 +0200

    Update customizeMinPtForHitRecoveryInGluedDet.py
    Fix it so that it is added also in non-clones

--- RecoTracker/Configuration/python/customizeMinPtForHitRecoveryInGluedDet.py
+++ RecoTracker/Configuration/python/customizeMinPtForHitRecoveryInGluedDet.py
@@ -1,8 +1,9 @@
+import FWCore.ParameterSet.Config as cms
 def esproducers_by_type(process, *types):
     return (module for module in process._Process__esproducers.values() if module._TypedParameterizable__type in types)
 def customizeMinPtForHitRecoveryInGluedDet(process,value):
    for esp in esproducers_by_type(process, "Chi2MeasurementEstimatorESProducer", "Chi2ChargeMeasurementEstimatorESProducer"):
-       esp.MinPtForHitRecoveryInGluedDet = value
+       esp.MinPtForHitRecoveryInGluedDet = cms.double(value)
    return process
 def customizeHitRecoveryInGluedDetOff(process):
    return customizeMinPtForHitRecoveryInGluedDet(process,1000000)
