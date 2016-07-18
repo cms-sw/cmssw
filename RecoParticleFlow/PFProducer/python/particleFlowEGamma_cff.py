8eb7355cd45207c6e97cbe1de52a9a287bf6da5f
Ivan Shvetsov <ivan.shvetsov@cern.ch>
2016-07-18 22:20:06 +0200

    add CITK module to egamma sequence: for test

--- RecoParticleFlow/PFProducer/python/particleFlowEGamma_cff.py
+++ RecoParticleFlow/PFProducer/python/particleFlowEGamma_cff.py
@@ -9,6 +9,7 @@ from RecoEgamma.EgammaPhotonProducers.gedPhotonSequence_cff import *
 from RecoEgamma.EgammaElectronProducers.gedGsfElectronSequence_cff import *
 from RecoEgamma.EgammaElectronProducers.pfBasedElectronIso_cff import *
 from RecoEgamma.EgammaIsolationAlgos.particleBasedIsoProducer_cfi import *
+from RecoEgamma.EgammaIsolationAlgos.egmPhotonIsolationAOD_cff import *
 
 particleBasedIsolationTmp = particleBasedIsolation.clone()
 particleBasedIsolationTmp.photonProducer =  cms.InputTag("gedPhotonsTmp")
@@ -16,6 +17,11 @@ particleBasedIsolationTmp.electronProducer = cms.InputTag("gedGsfElectronsTmp")
 particleBasedIsolationTmp.pfCandidates = cms.InputTag("particleFlowTmp")
 particleBasedIsolationTmp.valueMapPhoPFblockIso = cms.string("gedPhotons")
 particleBasedIsolationTmp.valueMapElePFblockIso = cms.string("gedGsfElectrons")
+
+egmPhotonIsolationCITK = egmPhotonIsolationAOD.clone()
+egmPhotonIsolationCITK.srcToIsolate = cms.InputTag("gedPhotonsTmp")
+egmPhotonIsolationCITK.srcForIsolationCone = cms.InputTag("particleFlowTmp")
+egmPhotonIsolationCITK.particleBasedIsolation = cms.InputTag("particleBasedIsolationTmp", "gedPhotons")
 
 particleFlowEGammaFull = cms.Sequence(particleFlowEGamma*gedGsfElectronSequenceTmp*gedPhotonSequenceTmp)
 particleFlowEGammaFinal = cms.Sequence(particleBasedIsolationTmp*gedPhotonSequence*gedElectronPFIsoSequence)
