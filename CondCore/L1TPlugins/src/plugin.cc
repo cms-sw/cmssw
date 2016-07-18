979eff5ef9a00f5870fd953684ea9ec4206a7f3b
kkotov <kkotov@cern.ch>
2016-07-18 17:14:57 +0200

    serialization plugins for L1TriggerKeyExt

--- CondCore/L1TPlugins/src/plugin.cc
+++ CondCore/L1TPlugins/src/plugin.cc
@@ -8,10 +8,20 @@
 
 REGISTER_PLUGIN(L1TriggerKeyRcd, L1TriggerKey);
 
+#include "CondFormats/DataRecord/interface/L1TriggerKeyExtRcd.h"
+#include "CondFormats/L1TObjects/interface/L1TriggerKeyExt.h"
+
+REGISTER_PLUGIN(L1TriggerKeyExtRcd, L1TriggerKeyExt);
+
 #include "CondFormats/DataRecord/interface/L1TriggerKeyListRcd.h"
 #include "CondFormats/L1TObjects/interface/L1TriggerKeyList.h"
 
 REGISTER_PLUGIN(L1TriggerKeyListRcd, L1TriggerKeyList);
+
+#include "CondFormats/DataRecord/interface/L1TriggerKeyListExtRcd.h"
+#include "CondFormats/L1TObjects/interface/L1TriggerKeyListExt.h"
+
+REGISTER_PLUGIN(L1TriggerKeyListExtRcd, L1TriggerKeyListExt);
 
 // L1 scales
 #include "CondFormats/L1TObjects/interface/L1CaloEtScale.h"
