3b1aafbf0fe9640410984d741a7d123b9515df18
Vladimir Rekovic <vladimir.rekovic@cern.ch>
2016-07-19 14:30:39 +0200

    L1REPACK update to use new feature of EMTF emulator using RPC TPs

--- Configuration/StandardSequences/python/SimL1EmulatorRepack_Full_cff.py
+++ Configuration/StandardSequences/python/SimL1EmulatorRepack_Full_cff.py
@@ -82,6 +82,7 @@ else:
 
     # EMTF
     simEmtfDigis.CSCInput            = cms.InputTag("unpackCsctf") # replace when emtfDigis availalbe 
+    simEmtfDigis.RPCInput            = cms.InputTag('unpackRPC')
 
     simCaloStage2Layer1Digis.ecalToken = cms.InputTag('unpackEcal:EcalTriggerPrimitives')
     simCaloStage2Layer1Digis.hcalToken = cms.InputTag('unpackHcal')
