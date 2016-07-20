67d76cb36753f756f6862023394cdf66846da4e0
Sunanda <sunanda.banerjee@cern.ch>
2016-07-20 23:00:28 +0200

    HGCal TB modification for July Setup

---
+++ SimG4CMS/HGCalTestBeam/python/HGCalTB160Module16XML_cfi.py
@@ -0,0 +1,17 @@
+import FWCore.ParameterSet.Config as cms
+
+XMLIdealGeometryESSource = cms.ESSource("XMLIdealGeometryESSource",
+    geomXMLFiles = cms.vstring('Geometry/CMSCommonData/data/materials.xml',
+        'Geometry/CMSCommonData/data/rotations.xml',
+        'Geometry/HGCalCommonData/data/TB160/cms.xml',
+        'Geometry/HGCalCommonData/data/TB160/hgcal.xml',
+        'Geometry/HGCalCommonData/data/TB160/16Module/hgcalEE.xml',
+        'Geometry/HGCalCommonData/data/v7/hgcalwafer.xml',
+        'Geometry/HGCalCommonData/data/TB160/hgcalBeam.xml',
+        'Geometry/HGCalCommonData/data/TB160/hgcalsense.xml',
+        'Geometry/HGCalCommonData/data/TB160/hgcProdCuts.xml',
+        'Geometry/HGCalCommonData/data/TB160/16Module/hgcalCons.xml'),
+    rootNodeName = cms.string('cms:OCMS')
+)
+
+
