9857a38cafc7b61b2a3da71d3bf9d73bb6eb9532
Thomas Reis <thomas.reis@cern.ch>
2016-07-20 11:04:49 +0200

    fill BX range plots correctly

--- DQM/L1TMonitor/src/L1TStage2MuonComp.cc
+++ DQM/L1TMonitor/src/L1TStage2MuonComp.cc
@@ -96,8 +96,13 @@ void L1TStage2MuonComp::analyze(const edm::Event& e, const edm::EventSetup& c) {
   int bxRange2 = muonBxColl2->getLastBX() - muonBxColl2->getFirstBX();
   if (bxRange1 != bxRange2) {
     summary->Fill(BXRANGEBAD);
-    muColl1BxRange->Fill(bxRange1);
-    muColl2BxRange->Fill(bxRange2);
+    int bx;
+    for (bx = muonBxColl1->getFirstBX(); bx <= muonBxColl1->getLastBX(); ++bx) {
+        muColl1BxRange->Fill(bx);
+    }
+    for (bx = muonBxColl2->getFirstBX(); bx <= muonBxColl2->getLastBX(); ++bx) {
+        muColl2BxRange->Fill(bx);
+    }
   } else {
     summary->Fill(BXRANGEGOOD);
   }
