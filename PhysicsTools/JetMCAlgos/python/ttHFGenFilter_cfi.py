271e82c6dc896e8e5abadce05386f3cea0448e2a
Andrej Saibel <andrej.saibel@cern.ch>
2016-07-23 12:27:45 +0200

    Added commentary on the code

--- PhysicsTools/JetMCAlgos/python/ttHFGenFilter_cfi.py
+++ PhysicsTools/JetMCAlgos/python/ttHFGenFilter_cfi.py
@@ -1,22 +1,12 @@
 import FWCore.ParameterSet.Config as cms
 
 ttHFGenFilter = cms.EDFilter("ttHFGenFilter",
-    # Phase space of additional jets
-    #genJetPtMin = cms.double(20.),
-    #genJetAbsEtaMax = cms.double(2.4),
-    # Input tags holding information about b/c hadron matching
-    #genJets = cms.InputTag("ak4GenJets"),
-    #genBHadJetIndex = cms.InputTag("matchGenBHadron", "genBHadJetIndex"),
+			     
     genParticles = cms.InputTag("genParticles"),
     genBHadFlavour = cms.InputTag("matchGenBHadron", "genBHadFlavour"),
     genBHadFromTopWeakDecay = cms.InputTag("matchGenBHadron", "genBHadFromTopWeakDecay"),
     genBHadPlusMothers = cms.InputTag("matchGenBHadron", "genBHadPlusMothers"),
     genBHadPlusMothersIndices = cms.InputTag("matchGenBHadron", "genBHadPlusMothersIndices"),
     genBHadIndex = cms.InputTag("matchGenBHadron", "genBHadIndex"),
-    #genBHadLeptonHadronIndex = cms.InputTag("matchGenBHadron", "genBHadLeptonHadronIndex"),
-    #genBHadLeptonViaTau = cms.InputTag("matchGenBHadron", "genBHadLeptonViaTau"),
-    #genCHadJetIndex = cms.InputTag("matchGenCHadron", "genCHadJetIndex"),
-    #genCHadFlavour = cms.InputTag("matchGenCHadron", "genCHadFlavour"),
-    #genCHadFromTopWeakDecay = cms.InputTag("matchGenCHadron", "genCHadFromTopWeakDecay"),
-    #genCHadBHadronId = cms.InputTag("matchGenCHadron", "genCHadBHadronId"),
+
 )
