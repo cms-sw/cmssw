2bfaf3862cc6147d74539643bac0d0c450d4647d
Sunanda <sunanda.banerjee@cern.ch>
2016-07-24 19:56:17 +0200

    Accept additional HLT paths for selection

--- Calibration/HcalAlCaRecoProducers/python/alcaHBHEMuonFilter_cfi.py
+++ Calibration/HcalAlCaRecoProducers/python/alcaHBHEMuonFilter_cfi.py
@@ -5,5 +5,5 @@ AlcaHBHEMuonFilter = cms.EDFilter("AlCaHBHEMuonFilter",
                                   TriggerResultLabel= cms.InputTag("TriggerResults","","HLT"),
                                   MuonLabel         = cms.InputTag("muons"),
                                   MinimumMuonP      = cms.double(10.0),
-                                  Triggers           = cms.vstring("HLT_IsoMu17","HLT_IsoMu20","HLT_IsoMu24","HLT_IsoMu27","HLT_Mu45","HLT_Mu50"),
+                                  Triggers           = cms.vstring("HLT_IsoMu","HLT_Mu"),
                                   )
