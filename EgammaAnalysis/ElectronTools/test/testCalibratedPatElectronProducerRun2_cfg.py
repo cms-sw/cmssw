52f192a70092a0b7bb36c8c61b9fb22c57edd324
Emanuele Di Marco <emanuele.dimarco@gmail.com>
2016-07-25 10:15:52 +0200

    use data from 2016 RunB

--- EgammaAnalysis/ElectronTools/test/testCalibratedPatElectronProducerRun2_cfg.py
+++ EgammaAnalysis/ElectronTools/test/testCalibratedPatElectronProducerRun2_cfg.py
@@ -4,7 +4,7 @@ from FWCore.ParameterSet.VarParsing import VarParsing
 varOptions = VarParsing('analysis')
 varOptions.register(
     "isMC",
-    True,
+    False,
     VarParsing.multiplicity.singleton,
     VarParsing.varType.bool,
     "setup MC as in/out put"
@@ -42,46 +42,7 @@ process.maxEvents = cms.untracked.PSet(
 if not varOptions.isMC :    
     process.source = cms.Source("PoolSource",
                                 fileNames = cms.untracked.vstring(
-            '/store/data/Run2015D/DoubleEG/MINIAOD/16Dec2015-v2/00000/3E38C5C7-87A6-E511-8620-002618943910.root',
-            '/store/data/Run2015D/DoubleEG/MINIAOD/16Dec2015-v2/00000/7EB36900-77A6-E511-ACCB-0CC47A4D764A.root',
-            '/store/data/Run2015D/DoubleEG/MINIAOD/16Dec2015-v2/00000/C67A894C-83A6-E511-A90D-0025905A6068.root',
-            '/store/data/Run2015D/DoubleEG/MINIAOD/16Dec2015-v2/00000/FED54E49-83A6-E511-AB65-0CC47A4C8ED8.root',
-            '/store/data/Run2015D/DoubleEG/MINIAOD/16Dec2015-v2/00000/3E65CB18-77A6-E511-A3DE-0025905A612C.root',  
-            '/store/data/Run2015D/DoubleEG/MINIAOD/16Dec2015-v2/00000/7EF48202-89A6-E511-9089-0025905A6110.root',  
-            '/store/data/Run2015D/DoubleEG/MINIAOD/16Dec2015-v2/00000/C68195C6-73A6-E511-A893-0CC47A4C8E34.root',  
-            '/store/data/Run2015D/DoubleEG/MINIAOD/16Dec2015-v2/00000/3E9B66CD-87A6-E511-943C-0025905938A4.root',  
-            '/store/data/Run2015D/DoubleEG/MINIAOD/16Dec2015-v2/00000/802ED605-79A6-E511-A555-0CC47A4D762A.root',  
-            '/store/data/Run2015D/DoubleEG/MINIAOD/16Dec2015-v2/00000/C8E870D0-61A6-E511-8D4C-0CC47A74524E.root', 
-            '/store/data/Run2015D/DoubleEG/MINIAOD/16Dec2015-v2/00000/ECA73F4E-82A6-E511-8843-0CC47A4D76A2.root',
-            '/store/data/Run2015D/DoubleEG/MINIAOD/16Dec2015-v2/00000/EE3246BE-6AA6-E511-A292-002590A2CCFE.root',
-            '/store/data/Run2015D/DoubleEG/MINIAOD/16Dec2015-v2/00000/EEC83EE3-75A6-E511-AE15-0025905A6136.root',
-            '/store/data/Run2015D/DoubleEG/MINIAOD/16Dec2015-v2/00000/EEE889E4-77A6-E511-A7D7-0025905B85D8.root',
-            '/store/data/Run2015D/DoubleEG/MINIAOD/16Dec2015-v2/00000/F066634D-83A6-E511-8874-0CC47A4C8E1E.root',
-            '/store/data/Run2015D/DoubleEG/MINIAOD/16Dec2015-v2/00000/F22451C8-87A6-E511-BEE7-0CC47A4D769A.root',
-            '/store/data/Run2015D/DoubleEG/MINIAOD/16Dec2015-v2/00000/F2515AE1-74A6-E511-9524-0CC47A74527A.root',
-            '/store/data/Run2015D/DoubleEG/MINIAOD/16Dec2015-v2/00000/F419DFC8-7AA6-E511-BCF7-0CC47A4D769E.root',
-            '/store/data/Run2015D/DoubleEG/MINIAOD/16Dec2015-v2/00000/F4D15347-66A6-E511-A73F-E41D2D08DE00.root',
-            '/store/data/Run2015D/DoubleEG/MINIAOD/16Dec2015-v2/00000/F4F4C3E9-76A6-E511-9572-0025905A6110.root',
-            '/store/data/Run2015D/DoubleEG/MINIAOD/16Dec2015-v2/00000/F60AEA20-66A6-E511-BDC4-0CC47A78A3EC.root',
-            '/store/data/Run2015D/DoubleEG/MINIAOD/16Dec2015-v2/00000/F67E523E-80A6-E511-836A-0025905964B6.root',
-            '/store/data/Run2015D/DoubleEG/MINIAOD/16Dec2015-v2/00000/F687B99A-81A6-E511-9BE4-0CC47A78A440.root',
-            '/store/data/Run2015D/DoubleEG/MINIAOD/16Dec2015-v2/00000/F6901A39-6AA6-E511-A634-0025901D4B20.root',
-            '/store/data/Run2015D/DoubleEG/MINIAOD/16Dec2015-v2/00000/F6E918C9-87A6-E511-B3D3-0CC47A4D76B2.root',
-            '/store/data/Run2015D/DoubleEG/MINIAOD/16Dec2015-v2/00000/F80C3E00-89A6-E511-8B04-0CC47A78A33E.root',
-            '/store/data/Run2015D/DoubleEG/MINIAOD/16Dec2015-v2/00000/F886D4C6-7AA6-E511-A3DF-00261894398B.root',
-            '/store/data/Run2015D/DoubleEG/MINIAOD/16Dec2015-v2/00000/F890A515-5BA6-E511-A1FD-0CC47A4C8E86.root',
-            '/store/data/Run2015D/DoubleEG/MINIAOD/16Dec2015-v2/00000/FA5697C8-7AA6-E511-A010-0CC47A78A4B8.root',
-            '/store/data/Run2015D/DoubleEG/MINIAOD/16Dec2015-v2/00000/FA78CBE0-75A6-E511-80EB-0025905AA9CC.root',
-            '/store/data/Run2015D/DoubleEG/MINIAOD/16Dec2015-v2/00000/FABF4750-7AA6-E511-BBBF-0CC47A4D762A.root',
-            '/store/data/Run2015D/DoubleEG/MINIAOD/16Dec2015-v2/00000/FAC03F87-6DA6-E511-90F5-0025901D4A0E.root',
-            '/store/data/Run2015D/DoubleEG/MINIAOD/16Dec2015-v2/00000/FC122FD8-74A6-E511-ADBA-0025905A6110.root',
-            '/store/data/Run2015D/DoubleEG/MINIAOD/16Dec2015-v2/00000/FC6E00D4-78A6-E511-BB82-003048FFD76E.root',
-            '/store/data/Run2015D/DoubleEG/MINIAOD/16Dec2015-v2/00000/FCAB2A25-57A6-E511-95F9-0CC47A4C8E16.root',
-            '/store/data/Run2015D/DoubleEG/MINIAOD/16Dec2015-v2/00000/FCC55B5B-5DA6-E511-9505-0CC47A78A33E.root',
-            '/store/data/Run2015D/DoubleEG/MINIAOD/16Dec2015-v2/00000/FE092324-64A6-E511-B90A-00266CFFA0B0.root',
-            '/store/data/Run2015D/DoubleEG/MINIAOD/16Dec2015-v2/00000/FE2483E5-77A6-E511-A182-0025905A609E.root',
-            '/store/data/Run2015D/DoubleEG/MINIAOD/16Dec2015-v2/00000/FEAD8C6B-64A6-E511-B1FC-002618943985.root',
-            '/store/data/Run2015D/DoubleEG/MINIAOD/16Dec2015-v2/00000/FED54E49-83A6-E511-AB65-0CC47A4C8ED8.root'
+            '/store/data/Run2016B/DoubleEG/MINIAOD/PromptReco-v2/000/275/073/00000/C281D9DB-5235-E611-8663-02163E0122D7.root'
             )
                                 )
 
