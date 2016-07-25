f6b1ce8d0985470c5e75593e5a02df1c5e728952
Emanuele Di Marco <emanuele.dimarco@gmail.com>
2016-07-25 10:15:18 +0200

    electron and photon scale and resolution corrections for ICHEP 2016

--- EgammaAnalysis/ElectronTools/python/calibratedElectronsRun2_cfi.py
+++ EgammaAnalysis/ElectronTools/python/calibratedElectronsRun2_cfi.py
@@ -3,7 +3,7 @@ import FWCore.ParameterSet.Config as cms
 correctionType = "80Xapproval"
 files = {"Prompt2015":"EgammaAnalysis/ElectronTools/data/ScalesSmearings/74X_Prompt_2015",
          "76XReReco" :"EgammaAnalysis/ElectronTools/data/ScalesSmearings/76X_16DecRereco_2015_Etunc",
-         "80Xapproval" : "EgammaAnalysis/ElectronTools/data/ScalesSmearings/80X_Golden22June_approval"}
+         "80Xapproval" : "EgammaAnalysis/ElectronTools/data/ScalesSmearings/80X_ichepV1_2016_ele"}
 
 calibratedElectrons = cms.EDProducer("CalibratedElectronProducerRun2",
 
