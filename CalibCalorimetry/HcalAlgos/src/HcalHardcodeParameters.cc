2f91842ae2ad364f42adcc15a8aca391a58cbbd1
Kevin Pedro <kpedro88@gmail.com>
2016-07-27 12:00:32 -0500

    make mcShape and recoShape configurable parameters for HcalHardcodeCalibrations

--- CalibCalorimetry/HcalAlgos/src/HcalHardcodeParameters.cc
+++ CalibCalorimetry/HcalAlgos/src/HcalHardcodeParameters.cc
@@ -1,14 +1,16 @@
 #include "CalibCalorimetry/HcalAlgos/interface/HcalHardcodeParameters.h"
 
 HcalHardcodeParameters::HcalHardcodeParameters(double pedestal, double pedestalWidth, std::vector<double> gain, std::vector<double> gainWidth, 
-											   int qieType, std::vector<double> qieOffset, std::vector<double> qieSlope)
+											   int qieType, std::vector<double> qieOffset, std::vector<double> qieSlope, int mcShape, int recoShape)
 :	pedestal_(pedestal),
 	pedestalWidth_(pedestalWidth),
 	gain_(gain),
 	gainWidth_(gainWidth),
 	qieType_(qieType),
 	qieOffset_(qieOffset),
-	qieSlope_(qieSlope)
+	qieSlope_(qieSlope),
+	mcShape_(mcShape),
+	recoShape_(recoShape)
 {
 }
 
@@ -19,6 +21,8 @@ HcalHardcodeParameters::HcalHardcodeParameters(const edm::ParameterSet & p)
 	gainWidth_(p.getParameter<std::vector<double>>("gainWidth")),
 	qieType_(p.getParameter<int>("qieType")),
 	qieOffset_(p.getParameter<std::vector<double>>("qieOffset")),
-	qieSlope_(p.getParameter<std::vector<double>>("qieSlope"))
+	qieSlope_(p.getParameter<std::vector<double>>("qieSlope")),
+	mcShape_(p.getParameter<int>("mcShape")),
+	recoShape_(p.getParameter<int>("recoShape"))
 {
 }
