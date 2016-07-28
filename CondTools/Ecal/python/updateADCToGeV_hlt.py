deace31ffe8aaf6982edd3450521d2f26dc1a40f
Pierre Depasse <depasse@cern.ch>
2016-07-28 11:22:20 +0200

    squashing depasse's commits: Retrieve O2O 8_0_1 version - O2O running on cms-conddb-1 in 8_0_1 -Read-only DB

--- CondTools/Ecal/python/updateADCToGeV_hlt.py
+++ CondTools/Ecal/python/updateADCToGeV_hlt.py
@@ -11,45 +11,51 @@ process.MessageLogger = cms.Service("MessageLogger",
 )
 
 process.source = cms.Source("EmptyIOVSource",
-    lastValue = cms.uint64(100000000),
+    lastValue = cms.uint64(100000000000),
     timetype = cms.string('runnumber'),
-    firstValue = cms.uint64(100000000),
+    firstValue = cms.uint64(100000000000),
     interval = cms.uint64(1)
 )
 
 #process.load("CondCore.DBCommon.CondDBCommon_cfi")
 process.load("CondCore.CondDB.CondDB_cfi")
 
-process.CondDB.connect = 'sqlite_file:EcalTPGPhysicsConst_v2_hlt.db'
-#process.CondDBCommon.connect = 'oracle://cms_orcon_prod/CMS_CONDITIONS'
 process.CondDB.DBParameters.authenticationPath = ''
+
+process.CondDB.connect = 'sqlite_file:EcalADCToGeVConstant_V1_hlt.db'
+#process.CondDB.connect = 'oracle://cms_orcon_prod/CMS_CONDITIONS'
+
 
 process.PoolDBOutputService = cms.Service("PoolDBOutputService",
     process.CondDB, 
 #    logconnect = cms.untracked.string('oracle://cms_orcon_prod/CMS_COND_31X_POPCONLOG'),
    logconnect = cms.untracked.string('sqlite_file:log.db'),   
-    toPut = cms.VPSet(cms.PSet(
-        record = cms.string('EcalTPGPhysicsConstRcd'),
-        tag = cms.string('EcalTPGPhysicsConst_v2_hlt')
+        toPut = cms.VPSet(cms.PSet(
+        record = cms.string('EcalADCToGeVConstantRcd'),
+        tag = cms.string('EcalADCToGeVConstant_V1_hlt')
     ))
 )
 
-process.Test1 = cms.EDAnalyzer("ExTestEcalTPGPhysicsConstAnalyzer",
-    record = cms.string('EcalTPGPhysicsConstRcd'),
+process.Test1 = cms.EDAnalyzer("ExTestEcalADCToGeVAnalyzer",
+    record = cms.string('EcalADCToGeVConstantRcd'),
     loggingOn= cms.untracked.bool(True),
     IsDestDbCheckedInQueryLog=cms.untracked.bool(True),
     SinceAppendMode=cms.bool(True),
     Source=cms.PSet(
-     firstRun = cms.string('200000'),
+     FileLowField = cms.string('/data/O2O/Ecal/TPG//ADCToGeV_Boff.xml'),
+     FileHighField = cms.string('/data/O2O/Ecal/TPG/ADCToGeV_Bon.xml'),
+#     FileLowField = cms.string('/afs/cern.ch/work/d/depasse/cmssw/CMSSW_8_0_1/src/CondTools/Ecal/python/ADCToGeV_hlt_current_BOFF.xml'),
+#     FileHighField = cms.string('/afs/cern.ch/work/d/depasse/cmssw/CMSSW_8_0_1/src/CondTools/Ecal/python/ADCToGeV_hlt_current_BON.xml'),
+     firstRun = cms.string('207149'),
      lastRun = cms.string('10000000'),
      OnlineDBSID = cms.string('cms_omds_lb'),
 #     OnlineDBSID = cms.string('cms_orcon_adg'),  test on lxplus
      OnlineDBUser = cms.string('cms_ecal_r'),
-     OnlineDBPassword = cms.string('***'),
+     OnlineDBPassword = cms.string('3c4l_r34d3r'),
      LocationSource = cms.string('P5'),
      Location = cms.string('P5_Co'),
      GenTag = cms.string('GLOBAL'),
-     RunType = cms.string('PHYSICS')
+     RunType = cms.string('COSMICS')
     )                            
 )
 
--- CondTools/Ecal/python/updateIntercali_express.py
+++ CondTools/Ecal/python/updateIntercali_express.py
@@ -11,45 +11,52 @@ process.MessageLogger = cms.Service("MessageLogger",
 )
 
 process.source = cms.Source("EmptyIOVSource",
-    lastValue = cms.uint64(100000000),
+    lastValue = cms.uint64(100000000000),
     timetype = cms.string('runnumber'),
-    firstValue = cms.uint64(100000000),
+    firstValue = cms.uint64(100000000000),
     interval = cms.uint64(1)
 )
 
 #process.load("CondCore.DBCommon.CondDBCommon_cfi")
 process.load("CondCore.CondDB.CondDB_cfi")
 
-process.CondDB.connect = 'sqlite_file:EcalTPGPhysicsConst_v2_hlt.db'
-#process.CondDBCommon.connect = 'oracle://cms_orcon_prod/CMS_CONDITIONS'
 process.CondDB.DBParameters.authenticationPath = ''
+
+process.CondDB.connect = 'sqlite_file:EcalIntercalibConstants_V1_express.db'
+#process.CondDB.connect = 'oracle://cms_orcon_prod/CMS_CONDITIONS'
+
 
 process.PoolDBOutputService = cms.Service("PoolDBOutputService",
     process.CondDB, 
 #    logconnect = cms.untracked.string('oracle://cms_orcon_prod/CMS_COND_31X_POPCONLOG'),
    logconnect = cms.untracked.string('sqlite_file:log.db'),   
     toPut = cms.VPSet(cms.PSet(
-        record = cms.string('EcalTPGPhysicsConstRcd'),
-        tag = cms.string('EcalTPGPhysicsConst_v2_hlt')
+        record = cms.string('EcalIntercalibConstantsRcd'),
+        tag = cms.string('EcalIntercalibConstants_V1_express')
     ))
 )
 
-process.Test1 = cms.EDAnalyzer("ExTestEcalTPGPhysicsConstAnalyzer",
-    record = cms.string('EcalTPGPhysicsConstRcd'),
+process.Test1 = cms.EDAnalyzer("ExTestEcalIntercalibAnalyzer",
+    record = cms.string('EcalIntercalibConstantsRcd'),
     loggingOn= cms.untracked.bool(True),
     IsDestDbCheckedInQueryLog=cms.untracked.bool(True),
     SinceAppendMode=cms.bool(True),
     Source=cms.PSet(
-     firstRun = cms.string('200000'),
+     FileLowField = cms.string('/data/O2O/Ecal/TPG/Intercalib_Boff.xml'),
+     FileHighField = cms.string('/data/O2O/Ecal/TPG/Intercalib_Bon.xml'),
+#     FileLowField = cms.string('/afs/cern.ch/work/d/depasse/cmssw/CMSSW_8_0_1/src/CondTools/Ecal/python/Intercalib_express_current_BOFF.xml'),
+#     FileHighField = cms.string('/afs/cern.ch/work/d/depasse/cmssw/CMSSW_8_0_1/src/CondTools/Ecal/python/Intercalib_express_current_BON.xml'),
+     Value_Bon = cms.untracked.double(0.76724),
+     firstRun = cms.string('207149'),
      lastRun = cms.string('10000000'),
      OnlineDBSID = cms.string('cms_omds_lb'),
 #     OnlineDBSID = cms.string('cms_orcon_adg'),  test on lxplus
      OnlineDBUser = cms.string('cms_ecal_r'),
-     OnlineDBPassword = cms.string('***'),
+     OnlineDBPassword = cms.string('3c4l_r34d3r'),
      LocationSource = cms.string('P5'),
      Location = cms.string('P5_Co'),
      GenTag = cms.string('GLOBAL'),
-     RunType = cms.string('PHYSICS')
+     RunType = cms.string('COSMICS')
     )                            
 )
 
