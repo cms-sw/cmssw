a98b6efc68135d5051fbfd36bf8bb89cb018bf49
Andrea <perrotta@cern.ch>
2016-07-29 16:46:45 +0200

    Update HLT menu up to /dev/CMSSW_8_0_0/HLT/V453

--- HLTrigger/Configuration/python/HLT_PRef_cff.py
+++ HLTrigger/Configuration/python/HLT_PRef_cff.py
@@ -1,11 +1,11 @@
-# /dev/CMSSW_8_0_0/PRef/V152 (CMSSW_8_0_12)
+# /dev/CMSSW_8_0_0/PRef/V153 (CMSSW_8_0_12)
 
 import FWCore.ParameterSet.Config as cms
 
 fragment = cms.ProcessFragment( "HLT" )
 
 fragment.HLTConfigVersion = cms.PSet(
-  tableName = cms.string('/dev/CMSSW_8_0_0/PRef/V152')
+  tableName = cms.string('/dev/CMSSW_8_0_0/PRef/V153')
 )
 
 fragment.HLTPSetInitialStepTrajectoryFilterBase = cms.PSet( 
