84ee5a09b2f40ef1c7ddbb331c34cfa4dfd1e0eb
Thomas Reis <thomas.reis@cern.ch>
2016-08-02 10:29:36 +0200

    Fix stop condition for loop

--- DQM/L1TMonitor/src/L1TStage2MuonComp.cc
+++ DQM/L1TMonitor/src/L1TStage2MuonComp.cc
@@ -90,8 +90,8 @@ void L1TStage2MuonComp::analyze(const edm::Event& e, const edm::EventSetup& c) {
   e.getByToken(muonToken1, muonBxColl1);
   e.getByToken(muonToken2, muonBxColl2);
 
-  int bxRange1 = muonBxColl1->getLastBX() - muonBxColl1->getFirstBX();
-  int bxRange2 = muonBxColl2->getLastBX() - muonBxColl2->getFirstBX();
+  int bxRange1 = muonBxColl1->getLastBX() - muonBxColl1->getFirstBX() + 1;
+  int bxRange2 = muonBxColl2->getLastBX() - muonBxColl2->getFirstBX() + 1;
   if (bxRange1 != bxRange2) {
     summary->Fill(BXRANGEBAD);
     int bx;
@@ -149,7 +149,7 @@ void L1TStage2MuonComp::analyze(const edm::Event& e, const edm::EventSetup& c) {
 
     muonIt1 = muonBxColl1->begin(iBx);
     muonIt2 = muonBxColl2->begin(iBx);
-    while(muonIt1 != muonBxColl1->end(iBx) && muonIt2 != muonBxColl1->end(iBx)) {
+    while(muonIt1 != muonBxColl1->end(iBx) && muonIt2 != muonBxColl2->end(iBx)) {
       summary->Fill(MUONALL);
 
       bool muonMismatch = false;
