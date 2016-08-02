09c02486e036516c9749e2a9f54e406d2040a430
Sunanda <sunanda.banerjee@cern.ch>
2016-08-02 19:16:36 +0200

    Correct code for IsoTrack and PhiSymmetry calibration

--- Calibration/HcalCalibAlgos/plugins/RecAnalyzerMinbias.cc
+++ Calibration/HcalCalibAlgos/plugins/RecAnalyzerMinbias.cc
@@ -11,13 +11,15 @@
 #include "CondFormats/DataRecord/interface/HcalRespCorrsRcd.h"
 #include "CondFormats/HcalObjects/interface/HcalRespCorrs.h"
 #include "FWCore/Common/interface/TriggerNames.h"
-#include "FWCore/Framework/interface/EDAnalyzer.h"
+#include "FWCore/Framework/interface/one/EDAnalyzer.h"
 #include "FWCore/Framework/interface/ESHandle.h"
 #include "FWCore/Framework/interface/Event.h"
 #include "FWCore/Framework/interface/EventSetup.h"
 #include "FWCore/Framework/interface/Frameworkfwd.h"
 #include "FWCore/MessageLogger/interface/MessageLogger.h"
 #include "FWCore/ParameterSet/interface/ParameterSet.h"
+#include "FWCore/ParameterSet/interface/ParameterSetDescription.h"
+#include "FWCore/ParameterSet/interface/ConfigurationDescriptions.h"
 #include "FWCore/ServiceRegistry/interface/Service.h"
 #include "CommonTools/UtilAlgos/interface/TFileService.h"
 #include "DataFormats/Common/interface/TriggerResults.h"
@@ -26,6 +28,8 @@
 #include "DataFormats/HcalRecHit/interface/HcalRecHitCollections.h"
 #include "DataFormats/L1GlobalTrigger/interface/L1GlobalTriggerObjectMap.h"
 #include "DataFormats/L1GlobalTrigger/interface/L1GlobalTriggerObjectMapRecord.h"
+#include "SimDataFormats/GeneratorProducts/interface/GenEventInfoProduct.h"
+
 #include "Geometry/CaloTopology/interface/HcalTopology.h"
 
 #include "TH1D.h"
@@ -37,19 +41,26 @@
 //#define DebugLog
 
 // class declaration
-class RecAnalyzerMinbias : public edm::EDAnalyzer {
+class RecAnalyzerMinbias : public edm::one::EDAnalyzer<edm::one::WatchRuns,edm::one::SharedResources> {
 
 public:
   explicit RecAnalyzerMinbias(const edm::ParameterSet&);
   ~RecAnalyzerMinbias();
 
-  virtual void analyze(const edm::Event&, const edm::EventSetup&) override;
-  virtual void beginJob();
-  virtual void beginRun(edm::Run const& iRun, edm::EventSetup const& iSetup);
+  static void fillDescriptions(edm::ConfigurationDescriptions& descriptions);
+
+private:
+  virtual void analyze(edm::Event const&, edm::EventSetup const&) override;
+  virtual void beginJob() ;
   virtual void endJob();
+  virtual void beginRun(edm::Run const&, edm::EventSetup const&) override;
+  virtual void endRun(edm::Run const&, edm::EventSetup const&) override {}
+  virtual void beginLuminosityBlock(edm::LuminosityBlock const&, edm::EventSetup const&) {}
+  virtual void endLuminosityBlock(edm::LuminosityBlock const&, edm::EventSetup const&) {}
     
 private:
-  void analyzeHcal(const HBHERecHitCollection&, const HFRecHitCollection&, int, bool);
+  void analyzeHcal(const HBHERecHitCollection&, const HFRecHitCollection&, int, bool, double);
+
   // ----------member data ---------------------------
   edm::Service<TFileService> fs_;
   bool                       theRecalib_, ignoreL1_, runNZS_, Noise_, fillHist_, init_;
@@ -57,7 +68,7 @@ private:
   double                     eLowHF_, eHighHF_;
   std::map<DetId,double>     corrFactor_;
   std::vector<unsigned int>  hcalID_;
-  TTree                     *myTree_;
+  TTree                     *myTree_, *myTree1_;
   TH1D                      *h_[4];
   std::vector<TH1D*>         histo_;
   std::map<HcalDetId,TH1D*>  histHC_;
@@ -73,15 +84,19 @@ private:
   double                     rnnumber;
   int                        mysubd, depth, iphi, ieta, cells, trigbit;
   float                      mom0_MB, mom1_MB, mom2_MB, mom3_MB, mom4_MB;
+  int                        HBHEsize, HFsize;
   std::map<std::pair<int,HcalDetId>,myInfo>        myMap_;
   edm::EDGetTokenT<HBHERecHitCollection>           tok_hbherecoMB_;
   edm::EDGetTokenT<HFRecHitCollection>             tok_hfrecoMB_;
   edm::EDGetTokenT<L1GlobalTriggerObjectMapRecord> tok_hltL1GtMap_;
+  edm::EDGetTokenT<GenEventInfoProduct>            tok_ew_; 
 };
 
 // constructors and destructor
 RecAnalyzerMinbias::RecAnalyzerMinbias(const edm::ParameterSet& iConfig) :
   init_(false) {
+
+  usesResource("TFileService");
 
   // get name of output file with histogramms
   runNZS_               = iConfig.getParameter<bool>("RunNZS");
@@ -104,6 +119,7 @@ RecAnalyzerMinbias::RecAnalyzerMinbias(const edm::ParameterSet& iConfig) :
   tok_hbherecoMB_   = consumes<HBHERecHitCollection>(iConfig.getParameter<edm::InputTag>("hbheInputMB"));
   tok_hfrecoMB_     = consumes<HFRecHitCollection>(iConfig.getParameter<edm::InputTag>("hfInputMB"));
   tok_hltL1GtMap_   = consumes<L1GlobalTriggerObjectMapRecord>(edm::InputTag("hltL1GtObjectMap"));
+  tok_ew_           = consumes<GenEventInfoProduct>(edm::InputTag("generator"));
 
   // Read correction factors
   std::ifstream infile(cfile.c_str());
@@ -156,6 +172,15 @@ RecAnalyzerMinbias::RecAnalyzerMinbias(const edm::ParameterSet& iConfig) :
 }
   
 RecAnalyzerMinbias::~RecAnalyzerMinbias() {}
+
+// ------------ method fills 'descriptions' with the allowed parameters for the module  ------------
+void RecAnalyzerMinbias::fillDescriptions(edm::ConfigurationDescriptions& descriptions) {
+  //The following says we do not know what parameters are allowed so do no validation
+  // Please change this to state exactly what you do use, even if it is no parameters
+  edm::ParameterSetDescription desc;
+  desc.setUnknown();
+  descriptions.addDefault(desc);
+}
   
 void RecAnalyzerMinbias::beginJob() {
 
@@ -193,9 +218,63 @@ void RecAnalyzerMinbias::beginJob() {
     myTree_->Branch("trigbit",  &trigbit,  "trigbit/I");
     myTree_->Branch("rnnumber", &rnnumber, "rnnumber/D");
   }
+  myTree1_      = fs_->make<TTree>("RecJet1","RecJet1 Tree");
+  myTree1_->Branch("rnnum_",   &rnnum_,    "rnnum_/D");
+  myTree1_->Branch("HBHEsize", &HBHEsize,  "HBHEsize/I");
+  myTree1_->Branch("HFsize",   &HFsize,    "HFsize/I");
+
   myMap_.clear();
 }
 
+//  EndJob
+//
+void RecAnalyzerMinbias::endJob() {
+
+  if (!fillHist_) {
+    cells = 0;
+    for (std::map<std::pair<int,HcalDetId>,myInfo>::const_iterator itr=myMap_.begin(); itr != myMap_.end(); ++itr) {
+      edm::LogInfo("AnalyzerMB") << "Fired trigger bit number "<<itr->first.first;
+#ifdef DebugLog
+      std::cout << "Fired trigger bit number "<<itr->first.first << std::endl;
+#endif
+      myInfo info = itr->second;
+      if (info.theMB0 > 0) { 
+	mom0_MB  = info.theMB0;
+	mom1_MB  = info.theMB1;
+	mom2_MB  = info.theMB2;
+	mom3_MB  = info.theMB3;
+	mom4_MB  = info.theMB4;
+	rnnumber = info.runcheck;
+	trigbit  = itr->first.first;
+	mysubd   = itr->first.second.subdet();
+	depth    = itr->first.second.depth();
+	iphi     = itr->first.second.iphi();
+	ieta     = itr->first.second.ieta();
+	edm::LogInfo("AnalyzerMB") << " Result=  " << trigbit << " " << mysubd 
+				   << " " << ieta << " " << iphi << " mom0  " 
+				   << mom0_MB << " mom1 " << mom1_MB << " mom2 "
+				   << mom2_MB << " mom3 " << mom3_MB << " mom4 "
+				   << mom4_MB;
+#ifdef DebugLog
+	std::cout << " Result=  " << trigbit << " " << mysubd 
+		  << " " << ieta << " " << iphi << " mom0  " 
+		  << mom0_MB << " mom1 " << mom1_MB << " mom2 " 
+		  << mom2_MB << " mom3 " << mom3_MB << " mom4 " 
+		  << mom4_MB << std::endl;
+#endif
+	myTree_->Fill();
+	cells++;
+      }
+    }
+    edm::LogInfo("AnalyzerMB") << "cells" << " " << cells;
+#ifdef DebugLog
+    std::cout << "cells" << " " << cells << std::endl;
+#endif
+  }
+#ifdef DebugLog
+  std::cout << "Exiting from RecAnalyzerMinbias::endjob" << std::endl;
+#endif
+}
   
 void RecAnalyzerMinbias::beginRun(edm::Run const&, edm::EventSetup const& iS) {
  
@@ -267,56 +346,6 @@ void RecAnalyzerMinbias::beginRun(edm::Run const&, edm::EventSetup const& iS) {
   }
 }
 
-//  EndJob
-//
-void RecAnalyzerMinbias::endJob() {
-
-  if (!fillHist_) {
-    cells = 0;
-    for (std::map<std::pair<int,HcalDetId>,myInfo>::const_iterator itr=myMap_.begin(); itr != myMap_.end(); ++itr) {
-      edm::LogInfo("AnalyzerMB") << "Fired trigger bit number "<<itr->first.first;
-#ifdef DebugLog
-      std::cout << "Fired trigger bit number "<<itr->first.first << std::endl;
-#endif
-      myInfo info = itr->second;
-      if (info.theMB0 > 0) { 
-	mom0_MB  = info.theMB0;
-	mom1_MB  = info.theMB1;
-	mom2_MB  = info.theMB2;
-	mom3_MB  = info.theMB3;
-	mom4_MB  = info.theMB4;
-	rnnumber = info.runcheck;
-	trigbit  = itr->first.first;
-	mysubd   = itr->first.second.subdet();
-	depth    = itr->first.second.depth();
-	iphi     = itr->first.second.iphi();
-	ieta     = itr->first.second.ieta();
-	edm::LogInfo("AnalyzerMB") << " Result=  " << trigbit << " " << mysubd 
-				   << " " << ieta << " " << iphi << " mom0  " 
-				   << mom0_MB << " mom1 " << mom1_MB << " mom2 "
-				   << mom2_MB << " mom3 " << mom3_MB << " mom4 "
-				   << mom4_MB;
-#ifdef DebugLog
-	std::cout << " Result=  " << trigbit << " " << mysubd 
-		  << " " << ieta << " " << iphi << " mom0  " 
-		  << mom0_MB << " mom1 " << mom1_MB << " mom2 " 
-		  << mom2_MB << " mom3 " << mom3_MB << " mom4 " 
-		  << mom4_MB << std::endl;
-#endif
-	myTree_->Fill();
-	cells++;
-      }
-    }
-    edm::LogInfo("AnalyzerMB") << "cells" << " " << cells;
-#ifdef DebugLog
-    std::cout << "cells" << " " << cells << std::endl;
-#endif
-  }
-#ifdef DebugLog
-  std::cout << "Exiting from RecAnalyzerMinbias::endjob" << std::endl;
-#endif
-}
-
 //
 // member functions
 //
@@ -324,7 +353,7 @@ void RecAnalyzerMinbias::endJob() {
   
 void RecAnalyzerMinbias::analyze(const edm::Event& iEvent, const edm::EventSetup& iSetup) {
 
-  rnnum_ = (float)iEvent.run(); 
+  rnnum_ = (double)iEvent.run(); 
   edm::Handle<HBHERecHitCollection> hbheMB;
   iEvent.getByToken(tok_hbherecoMB_, hbheMB);
   if (!hbheMB.isValid()) {
@@ -332,10 +361,11 @@ void RecAnalyzerMinbias::analyze(const edm::Event& iEvent, const edm::EventSetup
     return ;
   }
   const HBHERecHitCollection HithbheMB = *(hbheMB.product());
+  HBHEsize = HithbheMB.size();
   edm::LogInfo("AnalyzerMB") << "HBHE MB size of collection "<<HithbheMB.size();
   if (HithbheMB.size() < 5100 && runNZS_) {
     edm::LogWarning("AnalyzerMB") << "HBHE problem " << rnnum_ << " size "
-				  << HithbheMB.size();
+				  << HBHEsize;
   }
     
   edm::Handle<HFRecHitCollection> hfMB;
@@ -346,9 +376,10 @@ void RecAnalyzerMinbias::analyze(const edm::Event& iEvent, const edm::EventSetup
   }
   const HFRecHitCollection HithfMB = *(hfMB.product());
   edm::LogInfo("AnalyzerMB") << "HF MB size of collection " << HithfMB.size();
+  HFsize = HithfMB.size();
   if (HithfMB.size() < 1700 && runNZS_) {
     edm::LogWarning("AnalyzerMB") << "HF problem " << rnnum_ << " size "
-				  << HithfMB.size();
+				  << HFsize;
   }
 
   bool select(false);
@@ -372,8 +403,16 @@ void RecAnalyzerMinbias::analyze(const edm::Event& iEvent, const edm::EventSetup
     }
   }
 
+  if ((trigbit_.size() == 0) || select) myTree1_->Fill();
+
+  //event weight for FLAT sample and PU information
+  double eventWeight = 1.0;
+  edm::Handle<GenEventInfoProduct> genEventInfo;
+  iEvent.getByToken(tok_ew_, genEventInfo);
+  if (genEventInfo.isValid()) eventWeight = genEventInfo->weight();  
+
   if (ignoreL1_ || ((trigbit_.size() > 0) && select)) {
-    analyzeHcal(HithbheMB, HithfMB, 1, true);
+    analyzeHcal(HithbheMB, HithfMB, 1, true, eventWeight);
   } else if ((!ignoreL1_) && (trigbit_.size() == 0)) {
     edm::Handle<L1GlobalTriggerObjectMapRecord> gtObjectMapRecord;
     iEvent.getByToken(tok_hltL1GtMap_, gtObjectMapRecord);
@@ -385,7 +424,7 @@ void RecAnalyzerMinbias::analyze(const edm::Event& iEvent, const edm::EventSetup
 	bool resultGt = (*itMap).algoGtlResult();
 	if (resultGt) {
 	  int algoBit = (*itMap).algoBitNumber();
-	  analyzeHcal(HithbheMB, HithfMB, algoBit, (!ok));
+	  analyzeHcal(HithbheMB, HithfMB, algoBit, (!ok), eventWeight);
 	  ok          = true;
 	} 
       }
@@ -398,7 +437,7 @@ void RecAnalyzerMinbias::analyze(const edm::Event& iEvent, const edm::EventSetup
 
 void RecAnalyzerMinbias::analyzeHcal(const HBHERecHitCollection & HithbheMB,
 				     const HFRecHitCollection & HithfMB,
-				     int algoBit, bool fill) {
+				     int algoBit, bool fill, double weight) {
   // Signal part for HB HE
   for (HBHERecHitCollection::const_iterator hbheItr=HithbheMB.begin(); 
        hbheItr!=HithbheMB.end(); hbheItr++) {
@@ -436,11 +475,11 @@ void RecAnalyzerMinbias::analyzeHcal(const HBHERecHitCollection & HithbheMB,
 	  myMap_[std::pair<int,HcalDetId>(algoBit,hid)] = info;
 	  itr1 = myMap_.find(std::pair<int,HcalDetId>(algoBit,hid));
 	} 
-	itr1->second.theMB0++;
-	itr1->second.theMB1 += energyhit;
-	itr1->second.theMB2 += (energyhit*energyhit);
-	itr1->second.theMB3 += (energyhit*energyhit*energyhit);
-	itr1->second.theMB4 += (energyhit*energyhit*energyhit*energyhit);
+	itr1->second.theMB0 += weight;
+	itr1->second.theMB1 += (weight*energyhit);
+	itr1->second.theMB2 += (weight*energyhit*energyhit);
+	itr1->second.theMB3 += (weight*energyhit*energyhit*energyhit);
+	itr1->second.theMB4 += (weight*energyhit*energyhit*energyhit*energyhit);
 	itr1->second.runcheck = rnnum_;
       }
     }
@@ -487,11 +526,11 @@ void RecAnalyzerMinbias::analyzeHcal(const HBHERecHitCollection & HithbheMB,
 	  myMap_[std::pair<int,HcalDetId>(algoBit,hid)] = info;
 	  itr1 = myMap_.find(std::pair<int,HcalDetId>(algoBit,hid));
 	}
-	itr1->second.theMB0++;
-	itr1->second.theMB1 += energyhit;
-	itr1->second.theMB2 += (energyhit*energyhit);
-	itr1->second.theMB3 += (energyhit*energyhit*energyhit);
-	itr1->second.theMB4 += (energyhit*energyhit*energyhit*energyhit);
+	itr1->second.theMB0 += weight;
+	itr1->second.theMB1 += (weight*energyhit);
+	itr1->second.theMB2 += (weight*energyhit*energyhit);
+	itr1->second.theMB3 += (weight*energyhit*energyhit*energyhit);
+	itr1->second.theMB4 += (weight*energyhit*energyhit*energyhit*energyhit);
 	itr1->second.runcheck = rnnum_;
       }
     }
