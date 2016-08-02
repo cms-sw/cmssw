b718b191af4d769667e075d91b4ac022a2e28f32
Thomas Reis <thomas.reis@cern.ch>
2016-08-02 09:42:26 +0200

    Working commit. Code compiles
    Conflicts:
    	DQM/L1TMonitor/python/L1TStage2_cff.py
    	DQM/L1TMonitor/python/L1TStage2uGMT_cfi.py

--- DQM/L1TMonitor/python/L1TStage2uGMT_cfi.py
+++ DQM/L1TMonitor/python/L1TStage2uGMT_cfi.py
@@ -12,6 +12,29 @@ l1tStage2uGMT = cms.EDAnalyzer(
     verbose = cms.untracked.bool(False),
 )
 
+l1tStage2uGMTZeroSupp = cms.EDAnalyzer(
+    "L1TMP7ZeroSupp",
+    fedIds = cms.vint32(1402),
+    rawData = cms.InputTag("rawDataCollector"),
+    # mask for inputs (pt==0 defines empty muon)
+    maskCapId0 = cms.untracked.vint32(0x000001FF,
+                                      0x00000000,
+                                      0x000001FF,
+                                      0x00000000,
+                                      0x000001FF,
+                                      0x00000000),
+    # mask for outputs (pt==0 defines empty muon)
+    maskCapId1 = cms.untracked.vint32(0x0003FC00,
+                                      0x00000000,
+                                      0x0003FC00,
+                                      0x00000000,
+                                      0x0003FC00,
+                                      0x00000000),
+    # no masks defined for caption IDs 2-11
+    monitorDir = cms.untracked.string("L1T2016/L1TStage2uGMT/zeroSuppression"),
+    verbose = cms.untracked.bool(True),
+)
+
 # compares the unpacked BMTF output regional muon collection with the unpacked uGMT input regional muon collection from BMTF
 # only muons that do not match are filled in the histograms
 l1tStage2BmtfOutVsuGMTIn = cms.EDAnalyzer(
