98f93250fc1776dc734d3836b98f9fe3af18c6db
kkotov <kkotov@cern.ch>
2016-08-05 21:16:56 +0200

    XML parser from Vangelis and Thomas

--- L1Trigger/L1TCommon/src/XmlConfigReader.cc
+++ L1Trigger/L1TCommon/src/XmlConfigReader.cc
@@ -449,16 +449,3 @@ void XmlConfigReader::pruneString(std::string& str)
   }
 }
 
-void XmlConfigReader::pruneString(std::string& str)
-{
-  std::size_t alphanumBegin = str.find_first_not_of("\n\t ");
-  std::size_t alphanumEnd = str.find_last_not_of("\n\t ");
-  if (alphanumBegin != std::string::npos) {
-    if (alphanumEnd != std::string::npos) {
-      str = str.substr(alphanumBegin, alphanumEnd - alphanumBegin + 1);
-    } else {
-      str = str.substr(alphanumBegin);
-    }
-  }
-}
-
