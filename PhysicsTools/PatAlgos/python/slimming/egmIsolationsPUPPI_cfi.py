3f3162cf0be12613d90a2977fa65ccd0128a2d7d
Ivan Shvetsov <ivan.shvetsov@cern.ch>
2016-08-06 00:06:54 +0200

    Update egmIsolationsPUPPI_cfi.py

--- PhysicsTools/PatAlgos/python/slimming/egmIsolationsPUPPI_cfi.py
+++ PhysicsTools/PatAlgos/python/slimming/egmIsolationsPUPPI_cfi.py
@@ -3,6 +3,7 @@ import FWCore.ParameterSet.Config as cms
 from RecoEgamma.EgammaIsolationAlgos.egmPhotonIsolationPUPPI_cff import egmPhotonIsolationMiniAODPUPPI as _egmPhotonPUPPIIsolationForPhotons
 
 def makeInputForPUPPIIsolationEgm(process):
+	
 	process.load('RecoEgamma.EgammaIsolationAlgos.egmPhotonIsolationPUPPI_cff')
 	process.egmPhotonPUPPIIsolationForPhotons = _egmPhotonPUPPIIsolationForPhotons.clone()
 	process.egmPhotonPUPPIIsolationForPhotons.srcToIsolate = cms.InputTag("selectedPatPhotons")
