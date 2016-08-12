475a0dd8e30258d095de4080e8b6e110d447d6a0
Matti Kortelainen <matti.kortelainen@cern.ch>
2016-08-12 09:46:11 +0200

    Use standard tracking validation for phase2
    Reduces the set of histograms significantly. The additional histograms
    of trackingOnlyValidation are interesting when tuning tracking, but
    are not necessary for release validation.

--- Validation/Configuration/python/autoValidation.py
+++ Validation/Configuration/python/autoValidation.py
@@ -1,5 +1,6 @@
 autoValidation = { 'liteTracking' : ['prevalidationLiteTracking','validationLiteTracking','validationHarvesting'],
                    'trackingOnlyValidation' : ['globalPrevalidationTrackingOnly','globalValidationTrackingOnly','postValidation_trackingOnly'],
+                   'trackingValidation': ['globalPrevalidationTracking','globalValidationTrackingOnly','postValidationTracking'],
                    'muonOnlyValidation' : ['globalPrevalidationMuons','globalValidationMuons','postValidation_muons'],
                    'JetMETOnlyValidation' : ['globalPrevalidationJetMETOnly','globalValidationJetMETonly','postValidation_JetMET'],
                    'baseValidation' : ['baseCommonPreValidation','baseCommonValidation','postValidation_common'],
@@ -7,7 +8,7 @@ autoValidation = { 'liteTracking' : ['prevalidationLiteTracking','validationLite
                    'standardValidation' : ['prevalidation','validation','validationHarvesting']
                  }
 
-_phase2_allowed = ['baseValidation','trackingOnlyValidation','muonOnlyValidation','JetMETOnlyValidation']
+_phase2_allowed = ['baseValidation','trackingValidation','muonOnlyValidation','JetMETOnlyValidation']
 autoValidation['phase2Validation'] = ['','','']
 for i in range(0,3):
     autoValidation['phase2Validation'][i] = '+'.join([autoValidation[m][i] for m in _phase2_allowed])
