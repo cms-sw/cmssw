bc5ded23493d68a5612a6883bd30c85b899460c0
erica brondolin <erica.brondolin@cern.ch>
2016-08-15 18:24:25 +0200

    adding CPE as InputTag

---
+++ RecoLocalTracker/Phase2TrackerRecHits/src/module.cc
@@ -0,0 +1,5 @@
+#include "FWCore/Utilities/interface/typelookup.h"
+
+#include "RecoLocalTracker/Phase2TrackerRecHits/interface/Phase2StripCPEGeometric.h"
+
+TYPELOOKUP_DATA_REG(Phase2StripCPEGeometric);
--- RecoTracker/MeasurementDet/python/MeasurementTrackerESProducer_cfi.py
+++ RecoTracker/MeasurementDet/python/MeasurementTrackerESProducer_cfi.py
@@ -5,6 +5,7 @@ MeasurementTracker = cms.ESProducer("MeasurementTrackerESProducer",
 
     PixelCPE = cms.string('PixelCPEGeneric'),
     StripCPE = cms.string('StripCPEfromTrackAngle'),
+    Phase2StripCPE = cms.ESInputTag("phase2StripCPEGeometricESProducer", "Phase2StripCPEGeometric"),
     HitMatcher = cms.string('StandardMatcher'),
 
     SiStripQualityLabel         = cms.string(''),  ## unlabelled default SiStripQuality
