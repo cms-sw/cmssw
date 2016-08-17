fd8ad9cb4ad4871a69d4b483b7dcaa66d3a9cfc9
Sébastien <sebastien.wertz@uclouvain.be>
2016-08-17 10:08:23 +0200

    bTag validation for PhaseII reco

--- Validation/Configuration/python/autoValidation.py
+++ Validation/Configuration/python/autoValidation.py
@@ -1,12 +1,13 @@
 autoValidation = { 'liteTracking' : ['prevalidationLiteTracking','validationLiteTracking','validationHarvesting'],
                    'trackingOnlyValidation' : ['globalPrevalidationTrackingOnly','globalValidationTrackingOnly','postValidation_trackingOnly'],
                    'muonOnlyValidation' : ['globalPrevalidationMuons','globalValidationMuons','postValidation_muons'],
+                   'bTagOnlyValidation' : ['prebTagSequenceMC','bTagPlotsMCbcl','bTagCollectorSequenceMCbcl'],
                    'baseValidation' : ['baseCommonPreValidation','baseCommonValidation','postValidation_common'],
                    'miniAODValidation' : ['prevalidationMiniAOD','validationMiniAOD','validationHarvestingMiniAOD'],
                    'standardValidation' : ['prevalidation','validation','validationHarvesting']
                  }
 
-_phase2_allowed = ['baseValidation','trackingOnlyValidation','muonOnlyValidation']
+_phase2_allowed = ['baseValidation','trackingOnlyValidation','muonOnlyValidation','bTagOnlyValidation']
 autoValidation['phase2Validation'] = ['','','']
 for i in range(0,3):
     autoValidation['phase2Validation'][i] = '+'.join([autoValidation[m][i] for m in _phase2_allowed])
