e164716a29e5e293e4fab825c7a52734af7ed43f
Andrea <perrotta@cern.ch>
2016-08-18 17:39:35 +0200

    Update HLT menu up to /dev/CMSSW_8_0_0/HLT/V473

--- HLTrigger/Configuration/python/HLT_PIon_cff.py
+++ HLTrigger/Configuration/python/HLT_PIon_cff.py
@@ -1,11 +1,11 @@
-# /dev/CMSSW_8_0_0/PIon/V154 (CMSSW_8_0_17)
+# /dev/CMSSW_8_0_0/PIon/V155 (CMSSW_8_0_17)
 
 import FWCore.ParameterSet.Config as cms
 
 fragment = cms.ProcessFragment( "HLT" )
 
 fragment.HLTConfigVersion = cms.PSet(
-  tableName = cms.string('/dev/CMSSW_8_0_0/PIon/V154')
+  tableName = cms.string('/dev/CMSSW_8_0_0/PIon/V155')
 )
 
 fragment.HLTPSetInitialStepTrajectoryFilterBase = cms.PSet( 
@@ -3056,7 +3056,7 @@ fragment.hltEcalRecHit = cms.EDProducer( "EcalRecHitProducer",
       kTowerRecovered = cms.vstring( 'kDeadFE' )
     ),
     EBuncalibRecHitCollection = cms.InputTag( 'hltEcalUncalibRecHit','EcalUncalibRecHitsEB' ),
-    skipTimeCalib = cms.bool( False ),
+    skipTimeCalib = cms.bool( True ),
     algoRecover = cms.string( "EcalRecHitWorkerRecover" ),
     eeFEToBeRecovered = cms.InputTag( 'hltEcalDetIdToBeRecovered','eeFE' ),
     cleaningConfig = cms.PSet( 
--- HLTrigger/Configuration/test/OnLine_HLT_PRef.py
+++ HLTrigger/Configuration/test/OnLine_HLT_PRef.py
@@ -1,11 +1,11 @@
-# /dev/CMSSW_8_0_0/PRef/V154 (CMSSW_8_0_17)
+# /dev/CMSSW_8_0_0/PRef/V155 (CMSSW_8_0_17)
 
 import FWCore.ParameterSet.Config as cms
 
 process = cms.Process( "HLTPRef" )
 
 process.HLTConfigVersion = cms.PSet(
-  tableName = cms.string('/dev/CMSSW_8_0_0/PRef/V154')
+  tableName = cms.string('/dev/CMSSW_8_0_0/PRef/V155')
 )
 
 process.HLTPSetInitialStepTrajectoryFilterBase = cms.PSet( 
@@ -3910,7 +3910,7 @@ process.hltEcalRecHit = cms.EDProducer( "EcalRecHitProducer",
       kTowerRecovered = cms.vstring( 'kDeadFE' )
     ),
     EBuncalibRecHitCollection = cms.InputTag( 'hltEcalUncalibRecHit','EcalUncalibRecHitsEB' ),
-    skipTimeCalib = cms.bool( False ),
+    skipTimeCalib = cms.bool( True ),
     algoRecover = cms.string( "EcalRecHitWorkerRecover" ),
     eeFEToBeRecovered = cms.InputTag( 'hltEcalDetIdToBeRecovered','eeFE' ),
     cleaningConfig = cms.PSet( 
