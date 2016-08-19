a194cde744f2fb5225fb1ded0521300c79a84a5a
Ivan Shvetsov <ivan.shvetsov@cern.ch>
2016-08-19 17:31:16 +0200

    add module for electrons

--- PhysicsTools/PatAlgos/python/slimming/egmIsolationsPUPPI_cfi.py
+++ PhysicsTools/PatAlgos/python/slimming/egmIsolationsPUPPI_cfi.py
@@ -1,10 +1,17 @@
 import FWCore.ParameterSet.Config as cms
 
 from RecoEgamma.EgammaIsolationAlgos.egmPhotonIsolationPUPPI_cff import egmPhotonIsolationMiniAODPUPPI as _egmPhotonPUPPIIsolationForPhotons
+from RecoEgamma.EgammaIsolationAlgos.egmElectronIsolationPUPPI_cff import egmElectronIsolationMiniAODPUPPI as _egmElectronIsolationMiniAODPUPPI
 
 def makeInputForPUPPIIsolationEgm(process):
 	
 	process.load('RecoEgamma.EgammaIsolationAlgos.egmPhotonIsolationPUPPI_cff')
+	process.load('RecoEgamma.EgammaIsolationAlgos.egmElectronIsolationPUPPI_cff')
+
 	process.egmPhotonPUPPIIsolationForPhotons = _egmPhotonPUPPIIsolationForPhotons.clone()
 	process.egmPhotonPUPPIIsolationForPhotons.srcToIsolate = cms.InputTag("selectedPatPhotons")
 	process.egmPhotonPUPPIIsolationForPhotons.srcForIsolationCone = cms.InputTag("packedPFCandidates")
+
+	process.egmElectronPUPPIIsolationForPhotons = _egmElectronIsolationMiniAODPUPPI.clone()
+	process.egmElectronPUPPIIsolationForPhotons.srcToIsolate = cms.InputTag("selectedPatElectrons")
+	process.egmElectronPUPPIIsolationForPhotons.srcForIsolationCone = cms.InputTag("packedPFCandidates")
