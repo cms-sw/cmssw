072976e69bc6169a293061a92b0943e6ea9b533a
Philip <violatingcp@gmail.com>
2016-08-20 22:10:59 +0200

    fixed Slava's & Maria's questions

--- PhysicsTools/PatAlgos/python/slimming/puppiForMET_cff.py
+++ PhysicsTools/PatAlgos/python/slimming/puppiForMET_cff.py
@@ -1,7 +1,7 @@
 import FWCore.ParameterSet.Config as cms
 
 from CommonTools.PileupAlgos.Puppi_cff import *
-# from CommonTools.PileupAlgos.PhotonPuppi_cff import setupPuppiPhoton
+from CommonTools.PileupAlgos.PhotonPuppi_cff        import setupPuppiPhoton
 
 def makePuppies( process ):
 
@@ -15,60 +15,29 @@ def makePuppies( process ):
                                            src = cms.InputTag("particleFlow"),
                                            pdgId = cms.vint32(-11,11,-13,13),
                                            )
-
-
     process.puppiNoLep = process.puppi.clone()
     process.puppiNoLep.candName = cms.InputTag('pfNoLepPUPPI') 
 
     process.puppiMerged = cms.EDProducer("CandViewMerger",src = cms.VInputTag( 'puppiNoLep','pfLeptonsPUPPET'))
-
-## puppi met
-    process.puppiForMET = cms.EDProducer("PuppiPhoton",
-                                         candName       = cms.InputTag('packedPFCandidates'),
-                                         puppiCandName  = cms.InputTag('puppi'),
-                                         photonName     = cms.InputTag('slimmedPhotons'),
-                                         photonId       = cms.InputTag("egmPhotonIDs:cutBasedPhotonID_PHYS14_PU20bx25_V2p1-standalone-loose"),
-                                         pt             = cms.double(10),
-                                         useRefs        = cms.bool(True),
-                                         dRMatch        = cms.vdouble(10,10,10,10),
-                                         pdgids         = cms.vint32 (22,11,211,130),
-                                         weight         = cms.double(1.),
-                                         useValueMap    = cms.bool(False),
-                                         weightsName    = cms.InputTag('puppi'),
-                                         )
+    process.load('CommonTools.PileupAlgos.PhotonPuppi_cff')
+    process.puppiForMET = process.puppiPhoton.clone()
     process.puppiForMET.puppiCandName    = 'puppiMerged'
 
 
 
 def makePuppiesFromMiniAOD( process ):
-
     process.load('CommonTools.PileupAlgos.Puppi_cff')
-
     process.puppi.candName = cms.InputTag('packedPFCandidates')
     process.puppi.vertexName = cms.InputTag('offlineSlimmedPrimaryVertices')
-# kind of ugly, is there a better way to do this?
-
     process.pfNoLepPUPPI = cms.EDFilter("CandPtrSelector", src = cms.InputTag("packedPFCandidates"), cut =  cms.string("abs(pdgId) != 13 && abs(pdgId) != 11 && abs(pdgId) != 15"))
     process.pfLeptonsPUPPET   = cms.EDFilter("CandPtrSelector", src = cms.InputTag("packedPFCandidates"), cut = cms.string("abs(pdgId) == 13 || abs(pdgId) == 11 || abs(pdgId) == 15"))
-
     process.puppiNoLep = process.puppi.clone()
     process.puppiNoLep.candName = cms.InputTag('pfNoLepPUPPI') 
-
+    process.puppiNoLep.useWeightsNoLep = cms.bool(True)
     process.puppiMerged = cms.EDProducer("CandViewMerger",src = cms.VInputTag( 'puppiNoLep','pfLeptonsPUPPET'))
-
-## puppi met
-    process.puppiForMET = cms.EDProducer("PuppiPhoton",
-                                         candName       = cms.InputTag('packedPFCandidates'),
-                                         puppiCandName  = cms.InputTag('puppi'),
-                                         photonName     = cms.InputTag('slimmedPhotons'),
-                                         photonId       = cms.InputTag("egmPhotonIDs:cutBasedPhotonID_PHYS14_PU20bx25_V2p1-standalone-loose"),
-                                         pt             = cms.double(10),
-                                         useRefs        = cms.bool(True),
-                                         dRMatch        = cms.vdouble(10,10,10,10),
-                                         pdgids         = cms.vint32 (22,11,211,130),
-                                         weight         = cms.double(1.),
-                                         useValueMap    = cms.bool(False),
-                                         weightsName    = cms.InputTag('puppi'),
-                                         )
-    process.puppiForMET.puppiCandName    = 'puppiMerged'
+    process.load('CommonTools.PileupAlgos.PhotonPuppi_cff')
+    process.puppiForMET = process.puppiPhoton.clone()
+    setupPuppiPhoton(process)
+    #Line below doesn't work because of an issue with references in MiniAOD
+    #process.puppiForMET.puppiCandName    = 'puppiMerged'
     
