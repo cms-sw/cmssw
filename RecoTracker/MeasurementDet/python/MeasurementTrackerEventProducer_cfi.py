fc6627d3dfe6aeee27ff593eba649d79f012ffc3
Matti Kortelainen <matti.kortelainen@cern.ch>
2016-08-22 15:48:16 +0200

    Enable pixel digi2raw and raw2digi modules in 2017 workflows

--- RecoTracker/MeasurementDet/python/MeasurementTrackerEventProducer_cfi.py
+++ RecoTracker/MeasurementDet/python/MeasurementTrackerEventProducer_cfi.py
@@ -14,10 +14,6 @@ MeasurementTrackerEvent = cms.EDProducer("MeasurementTrackerEventProducer",
     inactiveStripDetectorLabels = cms.VInputTag(cms.InputTag('siStripDigis')),
     switchOffPixelsIfEmpty = cms.bool(True), # let's keep it like this, for cosmics                                    
 )
-# This customization will be removed once we have phase1 pixel digis
-eras.phase1Pixel.toModify(MeasurementTrackerEvent, # FIXME
-    inactivePixelDetectorLabels = []
-)
 # This customization will be removed once we have phase2 pixel digis
 # Need this line to stop error about missing siPixelDigis
 eras.phase2_tracker.toModify(MeasurementTrackerEvent, # FIXME
