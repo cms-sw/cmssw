fc1c14265750b6edbfd465b84e4b972b9cbe1a2c
Giovanni Franzoni <giovanni.franzoni@cern.ch>
2016-08-22 11:30:15 +0200

    we don't need Realistic25ns13TeV2016Collision - 76x won't be used for 2016 data

--- Configuration/StandardSequences/python/VtxSmeared.py
+++ Configuration/StandardSequences/python/VtxSmeared.py
@@ -43,7 +43,6 @@ VtxSmeared = {
     'Realistic50ns13TeVCollisionZeroTesla': 'IOMC.EventVertexGenerators.VtxSmearedRealistic50ns13TeVCollisionZeroTesla_cfi',
     'Realistic50ns13TeVCollision': 'IOMC.EventVertexGenerators.VtxSmearedRealistic50ns13TeVCollision_cfi',
     'Nominal5TeVpp2015Collision':    'IOMC.EventVertexGenerators.VtxSmearedNominal5TeVpp2015Collision_cfi',
-    'Realistic25ns13TeV2016Collision':    'IOMC.EventVertexGenerators.VtxSmearedRealistic25ns13TeV2016Collision_cfi',
     'Realistic100ns13TeVCollisionBetaStar90m' : 'IOMC.EventVertexGenerators.VtxSmearedRealistic100ns13TeVCollisionBetaStar90m_cfi',
     'Realistic100ns13TeVCollisionBetaStar90mLowBunches' : 'IOMC.EventVertexGenerators.VtxSmearedRealistic100ns13TeVCollisionBetaStar90mLowBunches_cfi'
 }
