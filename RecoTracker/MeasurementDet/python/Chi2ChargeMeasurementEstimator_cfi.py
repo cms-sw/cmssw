96979cef53109515e94b5bff161041c44f980bea
Matti Kortelainen <matti.kortelainen@cern.ch>
2016-08-23 15:12:43 +0200

    Customize MinPtForHitRecoveryInGluedDet with era

--- RecoTracker/MeasurementDet/python/Chi2ChargeMeasurementEstimator_cfi.py
+++ RecoTracker/MeasurementDet/python/Chi2ChargeMeasurementEstimator_cfi.py
@@ -1,9 +1,11 @@
 import FWCore.ParameterSet.Config as cms
+from Configuration.Eras.Modifier_tracker_apv_vfp30_2016_cff import tracker_apv_vfp30_2016 as _tracker_apv_vfp30_2016
 
 from RecoLocalTracker.SiStripClusterizer.SiStripClusterChargeCut_cfi import *
 
 from RecoTracker.MeasurementDet.Chi2ChargeMeasurementEstimatorDefault_cfi import Chi2ChargeMeasurementEstimatorDefault
 Chi2ChargeMeasurementEstimator = Chi2ChargeMeasurementEstimatorDefault.clone()
 Chi2ChargeMeasurementEstimator.clusterChargeCut = cms.PSet(refToPSet_ = cms.string('SiStripClusterChargeCutTight'))
+_tracker_apv_vfp30_2016.toModify(Chi2ChargeMeasurementEstimator, MinPtForHitRecoveryInGluedDet=0.9)
 
 
