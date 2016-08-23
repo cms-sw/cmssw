00ec0d1cb2b5ab61317b35fee2ee65c63ef85368
Marco Rovere <marco.rovere@cern.ch>
2016-08-23 21:24:44 +0200

    Comment apparently unused functions

--- SimTracker/TrackerMaterialAnalysis/plugins/ListGroups.cc
+++ SimTracker/TrackerMaterialAnalysis/plugins/ListGroups.cc
@@ -56,6 +56,7 @@ bool dddGetStringRaw(const DDFilteredView & view, const std::string & name, std:
   return false;
 }
 
+/*
 static inline
 double dddGetDouble(const std::string & s, DDFilteredView const & view) {
   std::string value;
@@ -64,6 +65,7 @@ double dddGetDouble(const std::string & s, DDFilteredView const & view) {
   else
     return NAN;
 }
+*/
 
 static inline
 std::string dddGetString(const std::string & s, DDFilteredView const & view) {
