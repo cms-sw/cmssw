0a29abee8bbd30f9433a985c0777ff575e2e304b
Christopher Brainerd <cbbrainerd@ucdavis.edu>
2016-08-23 15:53:18 -0500

    Stage 2 modifications to DQM offline. Test the sequences by running `source driver11a.sh` followed by `source driver11b.sh` in the DQMOffline/L1Trigger/test directory. This only runs over events in one file, but can easily be changed to run over the whole dataset.

--- DQMOffline/L1Trigger/test/driver11a.sh
+++ DQMOffline/L1Trigger/test/driver11a.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-numev=10
+numev=-1
 tnum=11
 DQMSEQUENCE=DQM
 step=a
@@ -8,9 +8,9 @@ step=a
 eval `scramv1 r -sh`
 
 #cmsDriver.py test_${tnum}_${step}_1 -s RAW2DIGI,RECO,${DQMSEQUENCE} -n ${numev} --eventcontent DQM --datatier DQMIO --conditions auto:com10  --filein  file:/afs/cern.ch/cms/CAF/CMSCOMM/COMM_DQM/DQMTest/MinimumBias__RAW__v1__165633__1CC420EE-B686-E011-A788-0030487CD6E8.root --data --customise DQMServices/Components/test/customDQM.py --no_exec --python_filename=test_${tnum}_${step}_1.py
-cmsDriver.py test_${tnum}_${step}_1 -s RAW2DIGI,RECO,${DQMSEQUENCE} -n ${numev} --eventcontent DQM --datatier DQMIO --conditions 80X_dataRun2_Prompt_v9 --era Run2_2016 --filein /store/data/Run2016A/ZeroBias1/RAW/v1/000/271/336/00000/00963A5A-BF0A-E611-A657-02163E0141FB.root --data --customise DQMServices/Components/test/customDQM.py --no_exec --python_filename=test_${tnum}_${step}_1.py --customise=L1Trigger/Configuration/customiseReEmul.L1TReEmulFromRAW
+cmsDriver.py test_${tnum}_${step}_1 -s RAW2DIGI,RECO,${DQMSEQUENCE} -n ${numev} --eventcontent DQM --datatier DQMIO --conditions 80X_dataRun2_Prompt_v9 --era Run2_2016 --filein /store/data/Run2016A/ZeroBias1/RAW/v1/000/271/336/00000/00963A5A-BF0A-E611-A657-02163E0141FB.root --data --customise DQMServices/Components/test/customDQM.py --no_exec --python_filename=test_${tnum}_${step}_1.py
 
-cmsRun -e test_${tnum}_${step}_1.py >& p${tnum}.1.log 
+cmsRun -e test_${tnum}_${step}_1.py 2>&1 | tee p${tnum}.1.log 
 
 if [ $? -ne 0 ]; then
   return 1
--- DQMOffline/L1Trigger/test/driver11b.sh
+++ DQMOffline/L1Trigger/test/driver11b.sh
@@ -7,8 +7,8 @@ DQMSEQUENCE=HARVESTING:dqmHarvesting
 step=b
 
 #cmsDriver.py test_${tnum}_${step}_1 -s ${DQMSEQUENCE} --conditions auto:com10 --data --filetype DQM --filein file:test_${tnum}_a_1_RAW2DIGI_RECO_DQM.root --scenario pp --customise DQMServices/Components/test/customHarvesting.py --no_exec --python_filename=test_${tnum}_${step}_1.py
-cmsDriver.py test_${tnum}_${step}_1 -s ${DQMSEQUENCE} --conditions 80X_dataRun2_Prompt_v9 --data --filetype DQM --filein file:test_${tnum}_a_1_RAW2DIGI_RECO_DQM.root --scenario pp --customise DQMServices/Components/test/customHarvesting.py --no_exec --python_filename=test_${tnum}_${step}_1.py
-cmsRun -e test_${tnum}_${step}_1.py &> q${tnum}.1.log 
+cmsDriver.py test_${tnum}_${step}_1 -s ${DQMSEQUENCE} --conditions 80X_dataRun2_Prompt_v9 --data --filetype DQM --filein file:test_${tnum}_a_1_RAW2DIGI_RECO_DQM.root --scenario pp --customise DQMServices/Components/test/customHarvesting.py --no_exec --python_filename=test_${tnum}_${step}_1.py --era Run2_2016
+cmsRun -e test_${tnum}_${step}_1.py 2>&1 | tee q${tnum}.1.log 
 
 if [ $? -ne 0 ]; then
   return 1
