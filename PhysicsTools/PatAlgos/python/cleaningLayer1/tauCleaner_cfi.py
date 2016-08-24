9c03c4cc858172f47ad5fa901dc6007d96e85f99
Roger <roger.wolf@cern.ch>
2016-08-24 19:14:51 +0200

    small fix to make the high-level run in scheduled mode possible

--- PhysicsTools/PatAlgos/python/cleaningLayer1/tauCleaner_cfi.py
+++ PhysicsTools/PatAlgos/python/cleaningLayer1/tauCleaner_cfi.py
@@ -12,7 +12,10 @@ cleanPatTaus = cms.EDProducer("PATTauCleaner",
     src = cms.InputTag("selectedPatTaus"),
 
     # preselection (any string-based cut on pat::Tau)
-    preselection = preselection,
+    preselection = 'tauID("decayModeFinding") > 0.5 &'
+                   ' tauID("byLooseCombinedIsolationDeltaBetaCorr3Hits") > 0.5 &'
+                   ' tauID("againstMuonTight3") > 0.5 &'
+                   ' tauID("againstElectronVLooseMVA6") > 0.5',
                               
     # overlap checking configurables
     checkOverlaps = cms.PSet(
