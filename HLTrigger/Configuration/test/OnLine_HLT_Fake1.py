963ea88b8c704bea26753ddb2bc893ecb725a359
Martin Grunewald <Martin.Grunewald@cern.ch>
2016-08-25 15:30:37 +0200

    Migration for and including CMSHLT-984

--- HLTrigger/Configuration/test/OnLine_HLT_Fake1.py
+++ HLTrigger/Configuration/test/OnLine_HLT_Fake1.py
@@ -1,11 +1,11 @@
-# /dev/CMSSW_8_0_0/Fake1/V17 (CMSSW_8_0_12)
+# /dev/CMSSW_8_0_0/Fake1/V19 (CMSSW_8_0_18_patch1)
 
 import FWCore.ParameterSet.Config as cms
 
 process = cms.Process( "HLTFake1" )
 
 process.HLTConfigVersion = cms.PSet(
-  tableName = cms.string('/dev/CMSSW_8_0_0/Fake1/V17')
+  tableName = cms.string('/dev/CMSSW_8_0_0/Fake1/V19')
 )
 
 process.streams = cms.PSet(  A = cms.vstring( 'InitialPD' ) )
