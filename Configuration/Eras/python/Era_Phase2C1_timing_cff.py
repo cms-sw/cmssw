fd85808aa0f522b9e37a9a398bff0fccc4227aed
lgray <lindsey.gray@gmail.com>
2016-08-26 08:28:44 -0500

    sanely use other eras

--- Configuration/Eras/python/Era_Phase2C1_timing_cff.py
+++ Configuration/Eras/python/Era_Phase2C1_timing_cff.py
@@ -1,12 +1,7 @@
 import FWCore.ParameterSet.Config as cms
 
-from Configuration.Eras.Modifier_run2_common_cff import run2_common
-from Configuration.Eras.Modifier_phase2_common_cff import phase2_common
-from Configuration.Eras.Modifier_phase2_tracker_cff import phase2_tracker
-from Configuration.Eras.Modifier_trackingPhase2PU140_cff import trackingPhase2PU140
-from Configuration.Eras.Modifier_phase2_muon_cff import phase2_muon
-from Configuration.Eras.Modifier_run3_GEM_cff import run3_GEM
+from Configuration.Eras.Era_Phase2C1_cff import Phase2C1
 from Configuration.Eras.Modifier_phase2_timing_cff import phase2_timing
 
-Phase2C1_timing = cms.ModifierChain(run2_common, phase2_common, phase2_tracker, trackingPhase2PU140, phase2_muon, run3_GEM, phase2_timing)
+Phase2C1_timing = cms.ModifierChain(Phase2C1, phase2_timing)
 
