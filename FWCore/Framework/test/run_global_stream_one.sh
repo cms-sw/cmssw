c8d3c2969a3baf0ac814560af5a3826141c346ce
Chris Jones <chrisdjones15@gmail.com>
2016-08-27 16:21:55 -0500

    Relax threshold for failure for no concurrent module test
    Since the TriggerResults producer can always run concurrently, we need to relax the fraction of running 2 modules simultaneously from <0.01 to <0.02. This should avoid a false positive without missing a true failure.

--- FWCore/Framework/test/run_global_stream_one.sh
+++ FWCore/Framework/test/run_global_stream_one.sh
@@ -14,7 +14,8 @@ F3=${LOCAL_TEST_DIR}/test_one_modules_cfg.py
 # ConcurrentModuleTimer service detailing how much time was
 # spent in 2,3 or 4 modules running simultaneously. Given the
 # only module that can run concurrently is the internal
-# TriggerResults producer, we will ignore times less then 0.01s.
+# TriggerResults producer, we will ignore times less then 0.01s
+# except for 2 at a time where we allow less than 0.02.
 touch empty_file
 
-(cmsRun ${LOCAL_TEST_DIR}/test_no_concurrent_module_cfg.py 2>&1) | tail -n 3 | grep -v ' 0.00' | grep -v ' 0 ' | grep -v 'e-' | diff - empty_file || die "Failure using test_no_concurrent_module_cfg.py" $?
+(cmsRun ${LOCAL_TEST_DIR}/test_no_concurrent_module_cfg.py 2>&1) | tail -n 3 | grep -v ' 0.00' | grep -v ' 0 ' | grep -v 'e-' | grep -v '2 0.01' | diff - empty_file || die "Failure using test_no_concurrent_module_cfg.py" $?
