b436fdefa90dacf18bc56e7f29063e6eb93c4504
Andrea <perrotta@cern.ch>
2016-08-29 13:37:49 +0200

    Update HLT menu up to /dev/CMSSW_8_0_0/HLT/V488

--- HLTrigger/Configuration/test/OnLine_HLT_PIon.py
+++ HLTrigger/Configuration/test/OnLine_HLT_PIon.py
@@ -1,11 +1,11 @@
-# /dev/CMSSW_8_0_0/PIon/V161 (CMSSW_8_0_18_patch1)
+# /dev/CMSSW_8_0_0/PIon/V162 (CMSSW_8_0_18_patch1)
 
 import FWCore.ParameterSet.Config as cms
 
 process = cms.Process( "HLTPIon" )
 
 process.HLTConfigVersion = cms.PSet(
-  tableName = cms.string('/dev/CMSSW_8_0_0/PIon/V161')
+  tableName = cms.string('/dev/CMSSW_8_0_0/PIon/V162')
 )
 
 process.HLTPSetInitialStepTrajectoryFilterBase = cms.PSet( 
