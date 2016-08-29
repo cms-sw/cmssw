afed32b37a012d810894aa8809a3be2bad734ba7
erica brondolin <erica.brondolin@cern.ch>
2016-08-29 10:42:07 +0200

    fix DigiToRaw

--- Configuration/StandardSequences/python/DigiToRaw_cff.py
+++ Configuration/StandardSequences/python/DigiToRaw_cff.py
@@ -34,7 +34,7 @@ phase2_hcal.toReplaceWith(DigiToRaw, DigiToRaw.copyAndExclude([hcalRawData]))
 # Remove siPixelRawData until we have phase1 pixel digis
 eras.phase1Pixel.toReplaceWith(DigiToRaw, DigiToRaw.copyAndExclude([siPixelRawData])) # FIXME
 # same for phase2 tracker
-eras.phase2_tracker.toReplaceWith(DigiToRaw, DigiToRaw.copyAndExclude([siPixelRawData],[rpcpacker])) # FIXME
+eras.phase2_tracker.toReplaceWith(DigiToRaw, DigiToRaw.copyAndExclude([siPixelRawData,rpcpacker])) # FIXME
 
 if eras.fastSim.isChosen() :
     for _entry in [siPixelRawData,SiStripDigiToRaw,castorRawData]:
