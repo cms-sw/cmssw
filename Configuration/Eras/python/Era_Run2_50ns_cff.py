416195a7e875514fdb6ad88985a1a4afd3e606d3
Matti Kortelainen <matti.kortelainen@cern.ch>
2016-08-29 14:30:33 +0200

    Use HIP mitigation in 2015 and 2016B RelVals

--- Configuration/Eras/python/Era_Run2_50ns_cff.py
+++ Configuration/Eras/python/Era_Run2_50ns_cff.py
@@ -2,6 +2,7 @@ import FWCore.ParameterSet.Config as cms
 
 from Configuration.Eras.Modifier_run2_common_cff import run2_common
 from Configuration.Eras.Modifier_run2_50ns_specific_cff import run2_50ns_specific
+from Configuration.Eras.Modifier_tracker_apv_vfp30_2016_cff import tracker_apv_vfp30_2016
 
-Run2_50ns = cms.ModifierChain(run2_common, run2_50ns_specific)
+Run2_50ns = cms.ModifierChain(run2_common, run2_50ns_specific, tracker_apv_vfp30_2016)
 
