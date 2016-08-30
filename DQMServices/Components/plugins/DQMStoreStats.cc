5f1ebdebca14c9b4e035a5ccefbfc042c078d30c
Deivydas <deivydas.voroneckis@cern.ch>
2016-08-30 11:24:50 +0200

    Fixed DQMStore build fail.

--- DQMServices/Components/plugins/DQMStoreStats.cc
+++ DQMServices/Components/plugins/DQMStoreStats.cc
@@ -6,9 +6,10 @@
  * Description: Print out statistics of histograms in DQMStore
 */
 
-#include "DQMServices/Components/src/DQMStoreStats.h"
+#include "DQMStoreStats.h"
 #include "FWCore/ServiceRegistry/interface/Service.h"
 #include "FWCore/MessageLogger/interface/JobReport.h"
+#include "FWCore/Framework/interface/MakerMacros.h"
 
 using namespace std;
 using namespace edm;
@@ -42,6 +43,8 @@ static unsigned int getEmptyMetric(T * array, int lenx, int leny, int lenz)
   
   return result;
 }
+
+namespace dqmservices {
 
 //==================================================================//
 //================= Constructor and Destructor =====================//
@@ -719,5 +722,6 @@ void DQMStoreStats::endJob() {
 
 }
 
-#include "FWCore/Framework/interface/MakerMacros.h"
 DEFINE_FWK_MODULE(DQMStoreStats);
+
+}
