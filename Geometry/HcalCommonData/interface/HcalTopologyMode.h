ccf763beff3c144bf2e9e331e721f5b5a4a49488
Sunanda <sunanda.banerjee@cern.ch>
2016-08-30 23:34:16 +0200

    Change TriggerMode definition

--- Geometry/HcalCommonData/interface/HcalTopologyMode.h
+++ Geometry/HcalCommonData/interface/HcalTopologyMode.h
@@ -27,10 +27,10 @@ namespace HcalTopologyMode {
   enum Mode { LHC=0, H2=1, SLHC=2, H2HE=3 };
 
   enum TriggerMode {
-    tm_LHC_RCT=0,         // HF is summed in 3x2 regions
-    tm_LHC_RCT_and_1x1=1, // HF is summed in both 3x2 and 1x1 regions 
-    tm_LHC_1x1=2          // HF is summed in 1x1 regions
+    TriggerMode_2009=0,         // HF is summed in 3x2 regions
+    TriggerMode_2016=1,         // HF is summed in both 3x2 and 1x1 regions 
+    TriggerMode_2017=2          // HF is summed in 1x1 regions
   };
 }
 
-#endif // CALO_TOPOLOGY_HCAL_TOPOLOGY_MODE_H
+#endif // Geometry_HcalCommonData_HcalTopologyMode_H
