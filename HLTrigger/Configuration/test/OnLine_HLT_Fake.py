37af29a25cc057af99b28a0c30895d3077aa6c9c
Martin Grunewald <Martin.Grunewald@cern.ch>
2016-08-30 16:00:30 +0200

    Migration / ESInputTag fix / devel

--- HLTrigger/Configuration/test/OnLine_HLT_Fake.py
+++ HLTrigger/Configuration/test/OnLine_HLT_Fake.py
@@ -1,11 +1,11 @@
-# /dev/CMSSW_8_0_0/Fake/V23 (CMSSW_8_0_18_patch1)
+# /dev/CMSSW_8_0_0/Fake/V24 (CMSSW_8_0_19)
 
 import FWCore.ParameterSet.Config as cms
 
 process = cms.Process( "HLTFake" )
 
 process.HLTConfigVersion = cms.PSet(
-  tableName = cms.string('/dev/CMSSW_8_0_0/Fake/V23')
+  tableName = cms.string('/dev/CMSSW_8_0_0/Fake/V24')
 )
 
 process.streams = cms.PSet(  A = cms.vstring( 'InitialPD' ) )
