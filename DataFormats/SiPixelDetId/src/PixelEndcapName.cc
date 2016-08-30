4bac4fa1eb3a260bfb2ae003d669b4edced0fcea
Gaelle <Gaelle.Boudoul@cern.ch>
2016-08-30 09:27:20 +0200

    set Pilot Blade to false by default

--- DataFormats/SiPixelDetId/src/PixelEndcapName.cc
+++ DataFormats/SiPixelDetId/src/PixelEndcapName.cc
@@ -7,7 +7,7 @@ using namespace std;
 
 namespace {
   //const bool phase1 = false;
-  const bool pilot_blade = true;  
+  const bool pilot_blade = false;  
 }
 
 // Decodes the pixel name from the cmssw DetID, uses tracker topology
