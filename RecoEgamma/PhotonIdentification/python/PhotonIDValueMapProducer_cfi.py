284b5bb1897be26d225bcaad51db257f8f6611ed
Giovanni <gpetruc@gmail.com>
2016-08-31 17:26:18 +0200

    photonIDValueMapProducer: don't use ES in phase2

--- RecoEgamma/PhotonIdentification/python/PhotonIDValueMapProducer_cfi.py
+++ RecoEgamma/PhotonIdentification/python/PhotonIDValueMapProducer_cfi.py
@@ -23,3 +23,9 @@ photonIDValueMapProducer = cms.EDProducer('PhotonIDValueMapProducer',
                                           # there is no need for the isolation map here, for miniAOD it is inside packedPFCandidates
                                           srcMiniAOD = cms.InputTag('slimmedPhotons',processName=cms.InputTag.skipCurrentProcess()),
                                           )
+
+from Configuration.StandardSequences.Eras import eras
+eras.phase2_common.toModify(photonIDValueMapProducer, 
+    esReducedRecHitCollection = cms.InputTag(""),
+    esReducedRecHitCollectionMiniAOD = cms.InputTag(""),
+)
