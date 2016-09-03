ba07527610c53d903c20f381dda5b20477d8fbe4
kkotov <kkotov@cern.ch>
2016-09-03 14:53:17 +0200

    modifiers for the DB authenification

--- L1TriggerConfig/L1TGlobalPrescalesVetosProducers/python/L1TGlobalPrescalesVetosOnline_cfi.py
+++ L1TriggerConfig/L1TGlobalPrescalesVetosProducers/python/L1TGlobalPrescalesVetosOnline_cfi.py
@@ -2,7 +2,7 @@ import FWCore.ParameterSet.Config as cms
 
 from L1Trigger.L1TGlobal.PrescalesVetos_cff import *
 
-L1TGlobalPrescalesVetosOnline = cms.ESProducer("L1TGlobalPrescalesVetosOnlineProd",
+L1TGlobalPrescalesVetosOnlineProd = cms.ESProducer("L1TGlobalPrescalesVetosOnlineProd",
     onlineAuthentication = cms.string('.'),
     forceGeneration = cms.bool(False),
     onlineDB = cms.string('oracle://CMS_OMDS_LB/CMS_TRG_R')
