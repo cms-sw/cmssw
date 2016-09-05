4555766d9d21a93cf428b95540456ba0ca9f7b20
Thomas Reis <thomas.reis@cern.ch>
2016-09-05 19:02:09 +0200

    Performance improvements for the L1TMuonCaloSumProducer

--- L1Trigger/L1TMuon/plugins/L1TMuonCaloSumProducer.cc
+++ L1Trigger/L1TMuon/plugins/L1TMuonCaloSumProducer.cc
@@ -114,45 +114,53 @@ L1TMuonCaloSumProducer::produce(edm::Event& iEvent, const edm::EventSetup& iSetu
   std::map<int, MuonCaloSum> sums;
   std::map<int, MuonCaloSum> regs;
 
+  int detamax = 4;
+  int dphimax = 4;
+
   if (iEvent.getByToken(m_caloTowerToken, caloTowers)) {
     for (auto it = caloTowers->begin(0); it != caloTowers->end(0); ++it) {
       const CaloTower& twr = *it;
-      if (std::abs(twr.hwEta()) > 27) {
+      int hwEta = twr.hwEta();
+      if (std::abs(hwEta) > 27) {
         continue;
       }
-      int ieta2x2 = (twr.hwEta() + 27) / 2;
-      int iphi2x2 = twr.hwPhi() / 2;
+      int hwPt = twr.hwPt();
+      if (hwPt < 1) {
+        continue;
+      }
+      int hwPhi = twr.hwPhi();
+      int ieta2x2 = (hwEta + 27) / 2;
+      int iphi2x2 = hwPhi / 2;
       int muon_idx = iphi2x2 * 28 + ieta2x2;
       if (regs.count(muon_idx) == 0) {
-        regs[muon_idx] = MuonCaloSum(twr.hwPt(), iphi2x2, ieta2x2, muon_idx);
+        regs[muon_idx] = MuonCaloSum(hwPt, iphi2x2, ieta2x2, muon_idx);
       } else {
-        regs.at(muon_idx).setEtBits(regs.at(muon_idx).etBits() + twr.hwPt());
+        regs.at(muon_idx).setEtBits(regs.at(muon_idx).etBits() + hwPt);
       }
 
-      // std::cout << "iphi; phi " << twr.hwPhi() << "; " << twr.phi() << " .. ieta; eta" << twr.hwEta() << "; " << twr.eta() << std::endl;
+      // std::cout << "iphi; phi " << hwPhi << "; " << phi << " .. ieta; eta" << hwEta << "; " << twr.eta() << std::endl;
 
-      for (int ieta = -27; ieta < 28; ++ieta) {
-        for (int iphi = 0; iphi < 72; ++iphi) {
-          int deta = std::abs(ieta - twr.hwEta());
-          int dphi = iphi - twr.hwPhi();
-          if (dphi > 36) {
-            dphi -= 72;
+      int ietamax = hwEta + detamax + 1;
+      for (int ieta = hwEta-detamax; ieta < ietamax; ++ieta) {
+        if (std::abs(ieta) > 27) {
+          continue;
+        }
+        int ietamu = (ieta + 27) / 2;
+        int iphimax = hwPhi + dphimax + 1;
+        for (int iphi = hwPhi-dphimax; iphi < iphimax; ++iphi) {
+          int iphiwrapped = iphi;
+          if (iphiwrapped < 0) {
+            iphiwrapped += 72;
+          } else if (iphiwrapped > 71) {
+            iphiwrapped -= 72;
           }
-          if (dphi < -36) {
-            dphi += 72;
+          int iphimu = iphiwrapped / 2;
+          int idxmu = iphimu * 28 + ietamu;
+          if (sums.count(idxmu) == 0) {
+            sums[idxmu] = MuonCaloSum(hwPt, iphimu, ietamu, idxmu);
+          } else {
+            sums.at(idxmu).setEtBits(sums.at(idxmu).etBits() + hwPt);
           }
-          dphi = std::abs(dphi);
-          if (deta <= 4 && dphi <= 4) {
-            int ietamu = (ieta + 27) / 2;
-            int iphimu = iphi / 2;
-            int idxmu = iphimu * 28 + ietamu;
-            if (sums.count(idxmu) == 0) {
-              sums[idxmu] = MuonCaloSum(twr.hwPt(), iphimu, ietamu, idxmu);
-            } else {
-              sums.at(idxmu).setEtBits(sums.at(idxmu).etBits() + twr.hwPt());
-            }
-          }
-
         }
       }
     }
