62c3ebb4c0ce9da1fb2bf36ea93a7b5c20e099a8
Thomas Reis <thomas.reis@cern.ch>
2016-09-07 17:41:22 +0200

    Make summary histogram title configurable for RegionalMuonCand comparison

--- DQM/L1TMonitor/python/L1TdeStage2OMTF_cfi.py
+++ DQM/L1TMonitor/python/L1TdeStage2OMTF_cfi.py
@@ -9,6 +9,7 @@ l1tdeStage2Omtf = cms.EDAnalyzer(
     monitorDir = cms.untracked.string("L1T2016EMU/L1TdeStage2OMTF"),
     regionalMuonCollection1Title = cms.untracked.string("uGMT input data from OMTF"),
     regionalMuonCollection2Title = cms.untracked.string("OMTF emulator"),
+    summaryTitle = cms.untracked.string("Summary of comparison between uGMT input muons from OMTF and OMTF emulator muons"),
     ignoreBadTrackAddress = cms.untracked.bool(True),
     verbose = cms.untracked.bool(False),
 )
