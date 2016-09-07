4764d7323aa11d25d820d18d03e0beb9df5a1bc9
maria <dalfonso@cern.ch>
2016-09-07 15:54:45 +0200

    small fix

--- RecoMET/METFilters/python/BadPFMuonFilter_cfi.py
+++ RecoMET/METFilters/python/BadPFMuonFilter_cfi.py
@@ -3,10 +3,11 @@ import FWCore.ParameterSet.Config as cms
 BadPFMuonFilter = cms.EDFilter(
     "BadPFMuonFilter",
     PFCandidates  = cms.InputTag("particleFlow"),   # Collection to test
+    muons  = cms.InputTag("muons"),   # Collection to test 
     taggingMode   = cms.bool(False),
     debug         = cms.bool(False),
     algo          = cms.int32(14),
-    minDZ         = cms.double(0.1),               # dz threshold on PF muons to consider; this is not used
+    minDZ         = cms.double(0.1),              # dz threshold on PF muons to consider; this is not used
     minMuPt       = cms.double(100),               # pt threshold on PF muons 
     minPtError    = cms.double(2.0),               # threshold on inner track pt Error
     segmentCompatibility = cms.double(0.3),        # compatibility between the inner track and the segments in the muon spectrometer
