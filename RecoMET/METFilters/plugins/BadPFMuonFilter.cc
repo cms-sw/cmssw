feb0339e1d28def1bf59133787dc0b997971e9c7
maria <dalfonso@cern.ch>
2016-09-07 13:28:44 +0200

    add akshansh version of the filters

--- RecoMET/METFilters/plugins/BadPFMuonFilter.cc
+++ RecoMET/METFilters/plugins/BadPFMuonFilter.cc
@@ -32,7 +32,7 @@ public:
 private:
   virtual bool filter(edm::StreamID iID, edm::Event&, const edm::EventSetup&) const override;
 
-      // ----------member data ---------------------------
+  // ----------member data ---------------------------
 
   edm::EDGetTokenT<edm::View<reco::Candidate> >   tokenPFCandidates_;
   edm::EDGetTokenT<edm::View<reco::Muon> >   tokenMuons_;
@@ -91,45 +91,57 @@ BadPFMuonFilter::filter(edm::StreamID iID, edm::Event& iEvent, const edm::EventS
     const reco::Muon & muon = (*muons)[i];
 
     reco::TrackRef innerMuonTrack = muon.innerTrack();
-
+    reco::TrackRef globalMuonTrack = muon.globalTrack();
+    reco::TrackRef bestMuonTrack = muon.muonBestTrack();
+    
+    if (debug_) cout << "PF filter muon:" << i << endl ;
     if ( innerMuonTrack.isNull() ) { 
       if (debug_) cout<<"Skipping this muon because it has no inner track"<<endl; 
       continue; 
     };
 
-    if ( innerMuonTrack->pt() ) {
-      if (debug_) cout<<"Skipping this muon because inner track pt."<<endl; 
-      continue;
+    if (( innerMuonTrack->pt() < minMuPt_) and (muon.pt() < minMuPt_)) {
+            if (debug_) cout<<"Skipping this muon trackPt and globalPt is less than threshold"<<endl; 
+       continue;
     }
 
-    if ( innerMuonTrack->quality(reco::TrackBase::highPurity) ) { 
-      if (debug_) cout<<"Skipping this muon because inner track is high purity."<<endl; 
-      continue;
+    if ( innerMuonTrack->quality(reco::TrackBase::highPurity) ) {
+      if (debug_) cout<<" this muon's inner track is high purity."<<endl; 
+      // continue;
     }
-
+       
     // Consider only muons with large relative pt error
     if (debug_) cout<<"Muon inner track pt rel err: "<<innerMuonTrack->ptError()/innerMuonTrack->pt()<<endl;
     if (not ( innerMuonTrack->ptError()/innerMuonTrack->pt() > minTrkPtError_ ) ) {
-      if (debug_) cout<<"Skipping this muon because seems well measured."<<endl; 
-      continue;
+      if (debug_) cout<<"this muon seems well measured."<<endl; 
+      //       continue;
     }
 
     // Consider only muons from muonSeededStepOutIn algo
     if (debug_) cout<<"Muon inner track original algo: "<<innerMuonTrack->originalAlgo() << endl;
     if (not ( innerMuonTrack->originalAlgo() == algo_  && innerMuonTrack->algo() == algo_ ) ) {
       if (debug_) cout<<"Skipping this muon because is not coming from the muonSeededStepOutIn"<<endl; 
-      continue;
+          continue;
+    }
+
+    if (debug_) cout << "SegmentCompatibility :"<< muon::segmentCompatibility(muon) << "RelPtErr:" << bestMuonTrack->ptError()/bestMuonTrack->pt() << endl;    
+    if (muon::segmentCompatibility(muon) < 0.3 && bestMuonTrack->ptError()/bestMuonTrack->pt() < 2.0) {
+      if (debug_) cout <<"Skipping this muon because segment compatiblity < 0.3 and relErr(best track) <2 " << endl;
+     continue;
     }
     
     for ( unsigned j=0; j < pfCandidates->size(); ++j ) {
       const reco::Candidate & pfCandidate = (*pfCandidates)[j];
       // look for pf muon
+      if (debug_) cout << "pf pdgID:" << abs(pfCandidate.pdgId()) << "pt:" << pfCandidate.pt() << endl;
       if ( not ( ( abs(pfCandidate.pdgId()) == 13) and (pfCandidate.pt() > minMuPt_) ) ) continue;
-      // require small dR
+      // require small  dR
       float dr = deltaR( muon.eta(), muon.phi(), pfCandidate.eta(), pfCandidate.phi() );
-      if( dr < 0.5 ) foundBadPFMuon=true;
-      cout <<"found bad muon!"<<endl; 
-      break;
+      if (dr < 0.001) {
+       	foundBadPFMuon=true;
+	if (debug_) cout <<"found bad muon! SC:" << muon::segmentCompatibility(muon) <<endl;
+	break;
+      }
     }
 
     if (foundBadPFMuon) { break; };
@@ -138,7 +150,7 @@ BadPFMuonFilter::filter(edm::StreamID iID, edm::Event& iEvent, const edm::EventS
 
   bool pass = !foundBadPFMuon;
 
-  if (debug_) cout<<"pass: "<<pass<<endl;
+  if (debug_) cout<< "badPFmuon filter"<<"pass: "<<pass<<endl;
 
   iEvent.put( std::auto_ptr<bool>(new bool(pass)) );
 
