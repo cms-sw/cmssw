4feca0c1fcdc41ab96114dc556e19b77e77aa279
aboletti <boletti@pd.infn.it>
2016-09-09 00:13:40 +0200

    added charge tagger to standard workflow

--- RecoBTag/Configuration/python/RecoBTag_EventContent_cff.py
+++ RecoBTag/Configuration/python/RecoBTag_EventContent_cff.py
@@ -25,7 +25,9 @@ RecoBTagFEVT = cms.PSet(
         'keep *_inclusiveCandidateSecondaryVerticesCvsL_*_*',
         'keep *_pfInclusiveSecondaryVertexFinderCvsLTagInfos_*_*',
         'keep *_pfCombinedCvsLJetTags_*_*',
-        'keep *_pfCombinedCvsBJetTags_*_*'
+        'keep *_pfCombinedCvsBJetTags_*_*',
+        # ChargeTag
+        'keep *_pfChargeBJetTags_*_*'
     )
 )
 #RECO content
@@ -53,7 +55,9 @@ RecoBTagRECO = cms.PSet(
         'keep *_inclusiveCandidateSecondaryVerticesCvsL_*_*',
         'keep *_pfInclusiveSecondaryVertexFinderCvsLTagInfos_*_*',
         'keep *_pfCombinedCvsLJetTags_*_*',
-        'keep *_pfCombinedCvsBJetTags_*_*'
+        'keep *_pfCombinedCvsBJetTags_*_*',
+        # ChargeTag
+        'keep *_pfChargeBJetTags_*_*'
     )
 )
 #AOD content
@@ -74,6 +78,8 @@ RecoBTagAOD = cms.PSet(
         # CTagging
         'keep *_inclusiveCandidateSecondaryVerticesCvsL_*_*',
         'keep *_pfCombinedCvsLJetTags_*_*',
-        'keep *_pfCombinedCvsBJetTags_*_*'
+        'keep *_pfCombinedCvsBJetTags_*_*',
+        # ChargeTag
+        'keep *_pfChargeBJetTags_*_*'
     )
 )
