6e362155dd9c27263a0b7ca524cce9578ed9772d
Kevin Pedro <kpedro88@gmail.com>
2016-09-12 09:50:42 -0500

    separate numbering for 2017 and 2023 workflows

--- Configuration/PyReleaseValidation/python/relval_2017.py
+++ Configuration/PyReleaseValidation/python/relval_2017.py
@@ -17,12 +17,12 @@ numWFStart=10000
 numWFSkip=200
 #2017 WFs to run in IB (TenMuE_0_200, TTbar, ZEE, MinBias, TTbar PU, ZEE PU)
 numWFIB = [10021.0,10024.0,10025.0,10026.0,10023.0,10224.0,10225.0]
-for i,key in enumerate(upgradeKeys):
+for i,key in enumerate(upgradeKeys[2017]):
     numWF=numWFStart+i*numWFSkip
     for frag in upgradeFragments:
         k=frag[:-4]+'_'+key
         stepList=[]
-        for step in upgradeProperties[key]['ScenToRun']:
+        for step in upgradeProperties[2017][key]['ScenToRun']:
             if 'Sim' in step:
                 stepList.append(k+'_'+step)
             else:
