8a09a0faa9dd8ed13db06d9107738bb41f8fd22a
Thomas Reis <thomas.reis@cern.ch>
2016-09-13 12:23:52 +0200

    Add uGMT output muon to uGT input muon comparison

--- DQM/L1TMonitor/python/L1TStage2uGMT_cfi.py
+++ DQM/L1TMonitor/python/L1TStage2uGMT_cfi.py
@@ -62,3 +62,16 @@ l1tStage2EmtfOutVsuGMTIn = cms.EDAnalyzer(
     verbose = cms.untracked.bool(False),
 )
 
+# compares the unpacked uGMT muon collection to the unpacked uGT muon collection
+# only muons that do not match are filled in the histograms
+l1tStage2uGMTOutVsuGTIn = cms.EDAnalyzer(
+    "L1TStage2MuonComp",
+    muonCollection1 = cms.InputTag("gmtStage2Digis", "Muon"),
+    muonCollection2 = cms.InputTag("gtStage2Digis", "Muon"),
+    monitorDir = cms.untracked.string("L1T2016/L1TStage2uGMT/uGMToutput_vs_uGTinput"),
+    muonCollection1Title = cms.untracked.string("uGMT output muons"),
+    muonCollection2Title = cms.untracked.string("uGT input muons"),
+    summaryTitle = cms.untracked.string("Summary of comparison between uGMT output muons and uGT input muons"),
+    verbose = cms.untracked.bool(False),
+)
+
