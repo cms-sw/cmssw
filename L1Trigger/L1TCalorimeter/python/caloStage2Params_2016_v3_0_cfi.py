8547c7beab7927f7f55d3deff704d1448a8c1163
Maxime <mguilbau@cern.ch>
2016-09-14 14:28:21 +0200

    good changes to caloStage2Param to add thereshold for tower counting

--- L1Trigger/L1TCalorimeter/python/caloStage2Params_2016_v3_0_cfi.py
+++ L1Trigger/L1TCalorimeter/python/caloStage2Params_2016_v3_0_cfi.py
@@ -113,7 +113,7 @@ caloStage2Params.jetCalibrationLUTFile    = cms.FileInPath("L1Trigger/L1TCalorim
 
 # sums: 0=ET, 1=HT, 2=MET, 3=MHT
 caloStage2Params.etSumLsb                = cms.double(0.5)
-caloStage2Params.etSumEtaMin             = cms.vint32(1, 1, 1, 1)
+caloStage2Params.etSumEtaMin             = cms.vint32(1, 1, 1, 1, 1)
 caloStage2Params.etSumEtaMax             = cms.vint32(28,  28,  28,  28, 28)
 caloStage2Params.etSumEtThreshold        = cms.vdouble(0.,  30.,  0.,  30., 0.)
 
