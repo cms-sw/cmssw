54548c0a740e8fb8b89034bebd6ec10af4f74aff
Silvio <silvio.donato@cern.ch>
2016-09-16 11:07:28 +0200

    sync with V536

--- HLTrigger/Configuration/python/HLT_PRef_cff.py
+++ HLTrigger/Configuration/python/HLT_PRef_cff.py
@@ -1,11 +1,11 @@
-# /dev/CMSSW_8_0_0/PRef/V183 (CMSSW_8_0_19_patch1)
+# /dev/CMSSW_8_0_0/PRef/V184 (CMSSW_8_0_19_patch1)
 
 import FWCore.ParameterSet.Config as cms
 
 fragment = cms.ProcessFragment( "HLT" )
 
 fragment.HLTConfigVersion = cms.PSet(
-  tableName = cms.string('/dev/CMSSW_8_0_0/PRef/V183')
+  tableName = cms.string('/dev/CMSSW_8_0_0/PRef/V184')
 )
 
 fragment.HLTPSetJetCoreStepTrajectoryFilter = cms.PSet( 
--- HLTrigger/Configuration/test/OnLine_HLT_PRef.py
+++ HLTrigger/Configuration/test/OnLine_HLT_PRef.py
@@ -1,11 +1,11 @@
-# /dev/CMSSW_8_0_0/PRef/V183 (CMSSW_8_0_19_patch1)
+# /dev/CMSSW_8_0_0/PRef/V184 (CMSSW_8_0_19_patch1)
 
 import FWCore.ParameterSet.Config as cms
 
 process = cms.Process( "HLTPRef" )
 
 process.HLTConfigVersion = cms.PSet(
-  tableName = cms.string('/dev/CMSSW_8_0_0/PRef/V183')
+  tableName = cms.string('/dev/CMSSW_8_0_0/PRef/V184')
 )
 
 process.HLTPSetJetCoreStepTrajectoryFilter = cms.PSet( 
