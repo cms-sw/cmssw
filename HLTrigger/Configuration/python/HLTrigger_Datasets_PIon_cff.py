ee5e9d2d10975ca7f38d2bd9426244ecaa3ab339
Silvio <silvio.donato@cern.ch>
2016-09-18 19:19:52 +0200

    move to 6r8 L1 menu

--- HLTrigger/Configuration/python/HLTrigger_Datasets_PIon_cff.py
+++ HLTrigger/Configuration/python/HLTrigger_Datasets_PIon_cff.py
@@ -1,47 +1,47 @@
-# getDatasets.py
+# /dev/CMSSW_8_0_0/PIon
 
 import FWCore.ParameterSet.Config as cms
 
 
-# dump of the Stream PhysicsCommissioning Datasets defined in the HLT table as Stream A Datasets
+# stream PhysicsCommissioning
 
-from HLTrigger.HLTfilters.triggerResultsFilter_cfi import triggerResultsFilter as streamA_datasetHLTPhysics_selector
-streamA_datasetHLTPhysics_selector.hltResults = cms.InputTag('TriggerResults', '', 'HLT')
-streamA_datasetHLTPhysics_selector.l1tResults = cms.InputTag('')
-streamA_datasetHLTPhysics_selector.throw      = cms.bool(False)
-streamA_datasetHLTPhysics_selector.triggerConditions = cms.vstring('HLT_Physics_v4')
+from HLTrigger.HLTfilters.triggerResultsFilter_cfi import triggerResultsFilter as streamPhysicsCommissioning_datasetHLTPhysics_selector
+streamPhysicsCommissioning_datasetHLTPhysics_selector.hltResults = cms.InputTag('TriggerResults', '', 'HLT')
+streamPhysicsCommissioning_datasetHLTPhysics_selector.l1tResults = cms.InputTag('')
+streamPhysicsCommissioning_datasetHLTPhysics_selector.throw      = cms.bool(False)
+streamPhysicsCommissioning_datasetHLTPhysics_selector.triggerConditions = cms.vstring('HLT_Physics_v4')
 
-from HLTrigger.HLTfilters.triggerResultsFilter_cfi import triggerResultsFilter as streamA_datasetTemplates_selector
-streamA_datasetTemplates_selector.hltResults = cms.InputTag('TriggerResults', '', 'HLT')
-streamA_datasetTemplates_selector.l1tResults = cms.InputTag('')
-streamA_datasetTemplates_selector.throw      = cms.bool(False)
-streamA_datasetTemplates_selector.triggerConditions = cms.vstring('HLT_CaloJet260_v4', 
+from HLTrigger.HLTfilters.triggerResultsFilter_cfi import triggerResultsFilter as streamPhysicsCommissioning_datasetTemplates_selector
+streamPhysicsCommissioning_datasetTemplates_selector.hltResults = cms.InputTag('TriggerResults', '', 'HLT')
+streamPhysicsCommissioning_datasetTemplates_selector.l1tResults = cms.InputTag('')
+streamPhysicsCommissioning_datasetTemplates_selector.throw      = cms.bool(False)
+streamPhysicsCommissioning_datasetTemplates_selector.triggerConditions = cms.vstring('HLT_CaloJet260_v4', 
     'HLT_Photon20_CaloIdVL_IsoL_v7')
 
 
-# dump of the Stream PhysicsEGamma Datasets defined in the HLT table as Stream A Datasets
+# stream PhysicsEGamma
 
-from HLTrigger.HLTfilters.triggerResultsFilter_cfi import triggerResultsFilter as streamA_datasetDoubleEG_selector
-streamA_datasetDoubleEG_selector.hltResults = cms.InputTag('TriggerResults', '', 'HLT')
-streamA_datasetDoubleEG_selector.l1tResults = cms.InputTag('')
-streamA_datasetDoubleEG_selector.throw      = cms.bool(False)
-streamA_datasetDoubleEG_selector.triggerConditions = cms.vstring('HLT_Ele17_CaloIdL_TrackIdL_IsoVL_v7')
-
-
-# dump of the Stream PhysicsHadronsTaus Datasets defined in the HLT table as Stream A Datasets
-
-from HLTrigger.HLTfilters.triggerResultsFilter_cfi import triggerResultsFilter as streamA_datasetJetHT_selector
-streamA_datasetJetHT_selector.hltResults = cms.InputTag('TriggerResults', '', 'HLT')
-streamA_datasetJetHT_selector.l1tResults = cms.InputTag('')
-streamA_datasetJetHT_selector.throw      = cms.bool(False)
-streamA_datasetJetHT_selector.triggerConditions = cms.vstring('HLT_PFJet40_v9')
+from HLTrigger.HLTfilters.triggerResultsFilter_cfi import triggerResultsFilter as streamPhysicsEGamma_datasetDoubleEG_selector
+streamPhysicsEGamma_datasetDoubleEG_selector.hltResults = cms.InputTag('TriggerResults', '', 'HLT')
+streamPhysicsEGamma_datasetDoubleEG_selector.l1tResults = cms.InputTag('')
+streamPhysicsEGamma_datasetDoubleEG_selector.throw      = cms.bool(False)
+streamPhysicsEGamma_datasetDoubleEG_selector.triggerConditions = cms.vstring('HLT_Ele17_CaloIdL_TrackIdL_IsoVL_v7')
 
 
-# dump of the Stream PhysicsMuons Datasets defined in the HLT table as Stream A Datasets
+# stream PhysicsHadronsTaus
 
-from HLTrigger.HLTfilters.triggerResultsFilter_cfi import triggerResultsFilter as streamA_datasetSingleMuon_selector
-streamA_datasetSingleMuon_selector.hltResults = cms.InputTag('TriggerResults', '', 'HLT')
-streamA_datasetSingleMuon_selector.l1tResults = cms.InputTag('')
-streamA_datasetSingleMuon_selector.throw      = cms.bool(False)
-streamA_datasetSingleMuon_selector.triggerConditions = cms.vstring('HLT_Mu50_v5')
+from HLTrigger.HLTfilters.triggerResultsFilter_cfi import triggerResultsFilter as streamPhysicsHadronsTaus_datasetJetHT_selector
+streamPhysicsHadronsTaus_datasetJetHT_selector.hltResults = cms.InputTag('TriggerResults', '', 'HLT')
+streamPhysicsHadronsTaus_datasetJetHT_selector.l1tResults = cms.InputTag('')
+streamPhysicsHadronsTaus_datasetJetHT_selector.throw      = cms.bool(False)
+streamPhysicsHadronsTaus_datasetJetHT_selector.triggerConditions = cms.vstring('HLT_PFJet40_v9')
+
+
+# stream PhysicsMuons
+
+from HLTrigger.HLTfilters.triggerResultsFilter_cfi import triggerResultsFilter as streamPhysicsMuons_datasetSingleMuon_selector
+streamPhysicsMuons_datasetSingleMuon_selector.hltResults = cms.InputTag('TriggerResults', '', 'HLT')
+streamPhysicsMuons_datasetSingleMuon_selector.l1tResults = cms.InputTag('')
+streamPhysicsMuons_datasetSingleMuon_selector.throw      = cms.bool(False)
+streamPhysicsMuons_datasetSingleMuon_selector.triggerConditions = cms.vstring('HLT_Mu50_v5')
 
