328a901ebf5cc8deb6c60b750297869ae6089154
Thomas Reis <thomas.reis@cern.ch>
2016-09-23 17:21:42 +0200

    Update list of triggers to match
    Conflicts:
    	L1Trigger/L1TMuon/data

--- L1Trigger/L1TNtuples/python/l1MuonRecoTree_cfi.py
+++ L1Trigger/L1TNtuples/python/l1MuonRecoTree_cfi.py
@@ -13,30 +13,17 @@ l1MuonRecoTree = cms.EDAnalyzer("L1Muon2RecoTreeProducer",
   # trigger to match to, may use regexp wildcard as supported by ROOT's 
   # TString; up to now the first found match (per run) is used.
   isoTriggerNames = cms.vstring(
-        "HLT_IsoMu18_v1",
-        "HLT_IsoMu18_v2",
-        "HLT_IsoMu18_v3",
-
-        "HLT_IsoMu20_v1",
-        "HLT_IsoMu20_v2",
-        "HLT_IsoMu20_v3",
-        "HLT_IsoMu20_v4",
-
-        "HLT_IsoMu22_v1",
-        "HLT_IsoMu22_v2",
-        "HLT_IsoMu22_v3",
-
-        "HLT_IsoMu24_v1",
-        "HLT_IsoMu24_v2",
-
-        "HLT_IsoMu27_v1",
-        "HLT_IsoMu27_v2",
-        "HLT_IsoMu27_v3",
-        "HLT_IsoMu27_v4",
+        "HLT_IsoMu18_v*",
+        "HLT_IsoMu20_v*",
+        "HLT_IsoMu22_v*",
+        "HLT_IsoMu24_v*",
+        "HLT_IsoMu27_v*",
         ),
   triggerNames = cms.vstring(
         "HLT_Mu30_v*",
-        "HLT_Mu40_v*"
+        "HLT_Mu40_v*",
+        "HLT_Mu50_v*",
+        "HLT_Mu55_v*",
         ),
 
   # data best guess: change for MC!
