f4f03e573d9c60a68e6a1ee860bedfa5d9d51cb8
Marco Rovere <marco.rovere@cern.ch>
2016-09-26 17:26:10 +0200

    Add PhaseII neutrino gun

---
+++ SimTracker/TrackerMaterialAnalysis/test/trackingMaterialProducer10GeVNeutrino_ForPhaseII.py
@@ -0,0 +1,81 @@
+#!/usr/bin/env python
+
+import FWCore.ParameterSet.Config as cms
+
+process = cms.Process("Geometry")
+# import of standard configurations
+process.load('Configuration.StandardSequences.Services_cff')
+#process.load('Configuration.StandardSequences.GeometryRecoDB_cff')
+process.load('Configuration.Geometry.GeometryExtended2023D1_cff')
+process.load('Configuration.StandardSequences.MagneticField_38T_cff')
+process.load('Configuration.StandardSequences.Generator_cff')
+process.load('Configuration.StandardSequences.SimIdeal_cff')
+process.load('Configuration.StandardSequences.EndOfProcess_cff')
+process.load('Configuration.StandardSequences.FrontierConditions_GlobalTag_condDBv2_cff')
+process.load('FWCore.MessageService.MessageLogger_cfi')
+process.load('Configuration.EventContent.EventContent_cff')
+process.load('SimGeneral.HepPDTESSource.pythiapdt_cfi')
+process.load('SimGeneral.MixingModule.mixNoPU_cfi')
+process.load('GeneratorInterface.Core.genFilterSummary_cff')
+process.load('IOMC.EventVertexGenerators.VtxSmearedRealistic8TeVCollision_cfi')
+process.load("SimTracker.TrackerMaterialAnalysis.single10GeVNeutrino_cfi")
+
+# gaussian Vertex Smearing
+#process.load('IOMC.EventVertexGenerators.VtxSmearedGauss_cfi')
+from Configuration.StandardSequences.VtxSmeared import VtxSmeared
+#process.load(VtxSmeared['Gauss'])
+process.load(VtxSmeared['NoSmear'])
+
+# detector simulation (Geant4-based) with tracking material accounting 
+process.load("SimTracker.TrackerMaterialAnalysis.trackingMaterialProducer_cff")
+
+
+#Global Tag
+from Configuration.AlCa.GlobalTag_condDBv2 import GlobalTag
+process.GlobalTag = GlobalTag(process.GlobalTag, 'auto:run2_mc', '')
+
+process.maxEvents = cms.untracked.PSet(
+    input = cms.untracked.int32(200000)
+)
+
+# Input source
+process.source = cms.Source("EmptySource")
+
+process.out = cms.OutputModule("PoolOutputModule",
+    outputCommands = cms.untracked.vstring(
+        'drop *',                                                       # drop all objects
+        'keep MaterialAccountingTracks_trackingMaterialProducer_*_*'),  # but the material accounting informations
+    fileName = cms.untracked.string('file:material.root')
+)
+
+process.path = cms.Path(process.generator*process.VtxSmeared*process.generatorSmeared*process.trackingMaterialProducer)
+process.outpath = cms.EndPath(process.out)
+
+def customizeMessageLogger(process):
+    ### Easy customisation of MessageLogger ###
+    # 1. Extend MessageLogger to monitor all modules: the * means any
+    #    label for all defined python modules
+    process.MessageLogger.debugModules.extend(['*'])
+    # 2. Define destination and its default logging properties
+    destination = 'debugTrackingMaterialProducer'
+    how_to_debug = cms.untracked.PSet(threshold = cms.untracked.string("DEBUG"),
+                                      DEBUG = cms.untracked.PSet(limit = cms.untracked.int32(0)),
+                                      default = cms.untracked.PSet(limit = cms.untracked.int32(0)),
+                                      )
+    # 3. Attach destination and its logging properties to the main process
+    process.MessageLogger.destinations.extend([destination])
+    process.MessageLogger._Parameterizable__addParameter(destination, how_to_debug)
+    # 4. Define and extend the categories we would like to monitor
+    log_debug_categories = ['TrackingMaterialProducer']
+    process.MessageLogger.categories.extend(log_debug_categories)
+
+    # 5. Extend the configuration of the configured destination so that it
+    #    will trace all messages coming from the list of specified
+    #    categories.
+    unlimit_debug = cms.untracked.PSet(limit = cms.untracked.int32(-1))
+    for val in log_debug_categories:
+        process.MessageLogger.debugTrackingMaterialProducer._Parameterizable__addParameter(val, unlimit_debug)
+
+    return process
+
+#process = customizeMessageLogger(process)
