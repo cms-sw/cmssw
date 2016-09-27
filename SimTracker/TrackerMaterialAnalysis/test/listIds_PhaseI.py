4bc1de93848610352ece6be6b29a7b962111b51a
Marco Rovere <marco.rovere@cern.ch>
2016-09-27 16:59:57 +0200

    Add ListIds support for PhaseI

---
+++ SimTracker/TrackerMaterialAnalysis/test/listIds_PhaseI.py
@@ -0,0 +1,29 @@
+#! /usr/bin/env cmsRun
+
+import FWCore.ParameterSet.Config as cms
+
+process = cms.Process("MaterialAnalyser")
+# import of standard configurations
+process.load('Configuration.StandardSequences.Services_cff')
+process.load('SimGeneral.HepPDTESSource.pythiapdt_cfi')
+process.load('FWCore.MessageService.MessageLogger_cfi')
+process.load('Configuration.EventContent.EventContent_cff')
+process.load('Configuration.StandardSequences.GeometryRecoDB_cff')
+process.load('Configuration.StandardSequences.GeometryDB_cff')
+process.load('Configuration.StandardSequences.MagneticField_AutoFromDBCurrent_cff')
+process.load('Configuration.StandardSequences.FrontierConditions_GlobalTag_cff')
+
+from Configuration.AlCa.GlobalTag import GlobalTag
+process.GlobalTag = GlobalTag(process.GlobalTag, 'auto:phase1_2017_realistic', '')
+
+process.maxEvents = cms.untracked.PSet(
+    input = cms.untracked.int32(1)
+)
+
+process.source = cms.Source("EmptySource",
+    numberEventsInRun = cms.untracked.uint32(1),
+        firstRun = cms.untracked.uint32(1)
+)
+
+process.listIds = cms.EDAnalyzer("ListIds")
+process.path = cms.Path(process.listIds)
