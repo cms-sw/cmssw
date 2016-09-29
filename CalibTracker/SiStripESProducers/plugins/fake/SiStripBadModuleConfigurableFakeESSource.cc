e7c83d235df7593103daa4b44eaf8a8de5050ad6
Pieter David <pieter.david@cern.ch>
2016-09-29 16:13:42 +0200

    reorganise module selection logic in CalibTracker/SiStripESProducers/plugins/fake/SiStripBadModuleConfigurableFakeESSource.cc

--- CalibTracker/SiStripESProducers/plugins/fake/SiStripBadModuleConfigurableFakeESSource.cc
+++ CalibTracker/SiStripESProducers/plugins/fake/SiStripBadModuleConfigurableFakeESSource.cc
@@ -39,7 +39,7 @@ private:
   edm::FileInPath m_file;
   bool m_printDebug;
 
-  void selectDetectors(const TrackerTopology* tTopo, const std::vector<uint32_t>& detIds, std::vector<uint32_t>& selList) const;
+  std::vector<uint32_t> selectDetectors(const TrackerTopology* tTopo, const std::vector<uint32_t>& detIds) const;
 };
 
 #include "FWCore/Framework/interface/ESHandle.h"
@@ -77,8 +77,7 @@ SiStripBadModuleConfigurableFakeESSource::produce(const SiStripBadModuleRcd& iRe
 
   SiStripDetInfoFileReader reader{m_file.fullPath()};
 
-  std::vector<uint32_t> selDetIds;
-  selectDetectors(tTopo.product(), reader.getAllDetIds(), selDetIds);
+  std::vector<uint32_t> selDetIds{selectDetectors(tTopo.product(), reader.getAllDetIds())};
   edm::LogInfo("SiStripQualityConfigurableFakeESSource")<<"[produce] number of selected dets to be removed " << selDetIds.size() <<std::endl;
 
   std::stringstream ss;
@@ -121,209 +120,112 @@ SiStripBadModuleConfigurableFakeESSource::produce(const SiStripBadModuleRcd& iRe
 }
 
 namespace {
-  bool isTIBDetector(const TrackerTopology* tTopo,
-                     const DetId & therawid,
-		     uint32_t requested_layer,
-		     uint32_t requested_bkw_frw,
-		     uint32_t requested_int_ext,
-		     uint32_t requested_string,
-		     uint32_t requested_ster,
-		     uint32_t requested_detid)
-  {
-    // check if subdetector field is a TIB, both tested numbers are int
-    return ( ( therawid.subdetId() == SiStripDetId::TIB )
-           // check if TIB is from the ones requested
-           // take everything if default value is 0
-          && ( ( tTopo->tibLayer(therawid) == requested_layer )
-            || ( requested_layer == 0 ) )
-          && ( ( tTopo->tibIsZPlusSide(therawid) && ( requested_bkw_frw == 2 ) )
-            || ( ( ! tTopo->tibIsZPlusSide(therawid) ) && ( requested_bkw_frw == 1 ) )
-            || ( requested_bkw_frw == 0 ) )
-          && ( ( tTopo->tibIsInternalString(therawid) && ( requested_int_ext == 1 ) )
-            || ( ( ! tTopo->tibIsInternalString(therawid) ) && ( requested_int_ext == 2 ) )
-            || ( requested_int_ext == 0 ) )
-          && ( ( tTopo->tibIsStereo(therawid) && ( requested_ster == 1 ) )
-            || ( tTopo->tibIsRPhi(therawid) && ( requested_ster == 2 ) )
-            || ( requested_ster == 0 ) )
-          && ( ( tTopo->tibString(therawid) == requested_string )
-            || ( requested_string == 0 ) )
-          && ( ( therawid.rawId() == requested_detid )
-            || ( requested_detid == 0 ) )
-          );
+  bool _isSel( uint32_t requested, uint32_t i )
+  { // internal helper: accept all i if requested is 0, otherwise require match
+    return ( requested == 0 ) || ( requested == i );
   }
 
-  bool isTOBDetector(const TrackerTopology* tTopo,
-                     const DetId & therawid,
-		     uint32_t requested_layer,
-		     uint32_t requested_bkw_frw,
-		     uint32_t requested_rod,
-		     uint32_t requested_ster,
-		     uint32_t requested_detid)
+  SiStripDetId::SubDetector subDetFromString( const std::string& subDetStr )
   {
-    // check if subdetector field is a TOB, both tested numbers are int
-    return ( ( therawid.subdetId() ==  SiStripDetId::TOB )
-           // check if TOB is from the ones requested
-           // take everything if default value is 0
-          && ( ( tTopo->tobLayer(therawid) == requested_layer )
-            || ( requested_layer == 0 ) )
-          && ( ( tTopo->tobIsZPlusSide(therawid) && ( requested_bkw_frw == 2 ) )
-            || ( ! tTopo->tobIsZPlusSide(therawid) && ( requested_bkw_frw == 1 ) )
-            || ( requested_bkw_frw == 0 ) )
-          && ( ( tTopo->tobIsStereo(therawid) && ( requested_ster == 1 ) )
-            || ( tTopo->tobIsRPhi(therawid) && ( requested_ster == 2 ) )
-            || ( requested_ster == 0 ) )
-          && ( ( tTopo->tobRod(therawid) == requested_rod )
-            || ( requested_rod == 0 ) )
-          && ( ( therawid.rawId() == requested_detid )
-            || ( requested_detid == 0 ) )
-          );
-  }
-
-  bool isTIDDetector(const TrackerTopology* tTopo,
-                     const DetId & therawid,
-		     uint32_t requested_side,
-		     uint32_t requested_wheel,
-		     uint32_t requested_ring,
-		     uint32_t requested_ster,
-		     uint32_t requested_detid)
-  {
-    // check if subdetector field is a TID, both tested numbers are int
-    return ( ( therawid.subdetId() ==  SiStripDetId::TID )
-           // check if TID is from the ones requested
-           // take everything if default value is 0
-          && ( ( tTopo->tidWheel(therawid) == requested_wheel )
-            || ( requested_wheel == 0 ) )
-          && ( ( tTopo->tidIsZPlusSide(therawid) && ( requested_side == 2 ) )
-            || ( ! tTopo->tidIsZPlusSide(therawid) && ( requested_side == 1 ) )
-            || ( requested_side == 0 ) )
-          && ( ( tTopo->tidIsStereo(therawid) && ( requested_ster == 1 ) )
-            || ( tTopo->tidIsRPhi(therawid) && ( requested_ster == 2 ) )
-            || ( requested_ster == 0 ) )
-          && ( ( tTopo->tidRing(therawid) == requested_ring )
-            || ( requested_ring == 0 ) )
-          && ( ( therawid.rawId() == requested_detid )
-            || ( requested_detid == 0 ) )
-          );
-  }
-
-  bool isTECDetector(const TrackerTopology* tTopo,
-                     const DetId & therawid,
-		     uint32_t requested_side,
-		     uint32_t requested_wheel,
-		     uint32_t requested_petal_bkw_frw,
-		     uint32_t requested_petal,
-		     uint32_t requested_ring,
-		     uint32_t requested_ster,
-		     uint32_t requested_detid)
-  {
-    // check if subdetector field is a TEC, both tested numbers are int
-    return ( ( therawid.subdetId()  ==   SiStripDetId::TEC )
-           // check if TEC is from the ones requested 
-           // take everything if default value is 0
-          && ( ( tTopo->tecWheel(therawid) == requested_wheel )
-            || ( requested_wheel == 0 ) )
-          && ( ( tTopo->tecIsZPlusSide(therawid) && ( requested_side == 2 ) )
-            || ( ( ! tTopo->tecIsZPlusSide(therawid) ) && ( requested_side == 1 ) )
-            || ( requested_side == 0 ) )
-          && ( ( tTopo->tecIsStereo(therawid) && ( requested_ster == 1 ) )
-            || ( ( ! tTopo->tecIsStereo(therawid) ) && ( requested_ster == 2 ) )
-            || ( requested_ster == 0 ) )
-          && ( ( tTopo->tecIsFrontPetal(therawid) && ( requested_petal_bkw_frw == 2 ) )
-            || ( ( ! tTopo->tecIsFrontPetal(therawid) ) && ( requested_petal_bkw_frw == 2 ) )
-            || ( requested_petal_bkw_frw == 0 ) )
-          && ( ( tTopo->tecPetalNumber(therawid) == requested_petal )
-            || ( requested_petal == 0 ) )
-          && ( ( tTopo->tecRing(therawid) == requested_ring )
-            || ( requested_ring == 0 ) )
-          && ( ( therawid.rawId() == requested_detid )
-            || ( requested_detid == 0 ) )
-          );
-  }
-}
-
-void SiStripBadModuleConfigurableFakeESSource::selectDetectors(const TrackerTopology* tTopo, const std::vector<uint32_t>& detIds, std::vector<uint32_t>& selList) const
-{
-  std::stringstream ss;
-  for ( const auto& badComp : m_badComponentList ) {
-    const std::string subDetStr{badComp.getParameter<std::string>("SubDet")};
-    if (m_printDebug) ss << "Bad SubDet " << subDetStr << " \t";
-
     SiStripDetId::SubDetector    subDet = SiStripDetId::UNKNOWN;
     if      ( subDetStr=="TIB" ) subDet = SiStripDetId::TIB;
     else if ( subDetStr=="TID" ) subDet = SiStripDetId::TID;
     else if ( subDetStr=="TOB" ) subDet = SiStripDetId::TOB;
     else if ( subDetStr=="TEC" ) subDet = SiStripDetId::TEC;
+    return subDet;
+  }
+}
 
-    std::vector<uint32_t> genericBadDetIds(badComp.getUntrackedParameter<std::vector<uint32_t> >("detidList", std::vector<uint32_t>()));
-    bool anySubDet{ ! genericBadDetIds.empty() };
+std::vector<uint32_t> SiStripBadModuleConfigurableFakeESSource::selectDetectors(const TrackerTopology* tTopo, const std::vector<uint32_t>& detIds) const
+{
+  std::vector<uint32_t> selList;
+  std::stringstream ss;
+  for ( const auto& badComp : m_badComponentList ) {
+    const std::string subDetStr{badComp.getParameter<std::string>("SubDet")};
+    if (m_printDebug) ss << "Bad SubDet " << subDetStr << " \t";
+    const SiStripDetId::SubDetector subDet = subDetFromString(subDetStr);
+
+    const std::vector<uint32_t> genericBadDetIds{badComp.getUntrackedParameter<std::vector<uint32_t>>("detidList", std::vector<uint32_t>())};
+    const bool anySubDet{ ! genericBadDetIds.empty() };
 
     std::cout << "genericBadDetIds.size() = " << genericBadDetIds.size() << std::endl;
 
-    uint32_t startDet{DetId{DetId::Tracker, anySubDet ? SiStripDetId::TIB   : subDet  }.rawId()};
-    uint32_t stopDet {DetId{DetId::Tracker, anySubDet ? SiStripDetId::TEC+1 : subDet+1}.rawId()};
+    using DetIdIt = std::vector<uint32_t>::const_iterator;
+    const DetIdIt beginDetIt = std::lower_bound(detIds.begin(), detIds.end(), DetId(DetId::Tracker, anySubDet ? SiStripDetId::TIB   : subDet  ).rawId());
+    const DetIdIt endDetIt   = std::lower_bound(detIds.begin(), detIds.end(), DetId(DetId::Tracker, anySubDet ? SiStripDetId::TEC+1 : subDet+1).rawId());
 
-    for ( std::vector<uint32_t>::const_iterator iter{lower_bound(detIds.begin(), detIds.end(), startDet)}
-        , iterEnd{lower_bound(detIds.begin(), detIds.end(), stopDet)}
-        ; iter != iterEnd; ++iter )
-    {
-      const DetId detectorId{*iter};
-      bool resp{false};
+    if ( anySubDet ) {
+      std::copy_if(beginDetIt, endDetIt, std::back_inserter(selList),
+          [&genericBadDetIds] ( uint32_t detId ) {
+            std::cout << "AnySubDet" << detId << std::endl;
+            return std::find(genericBadDetIds.begin(), genericBadDetIds.end(), detId) != genericBadDetIds.end();
+          });
+    } else {
       switch (subDet) {
         case SiStripDetId::TIB:
-          resp = isTIBDetector(tTopo,detectorId,
-                               badComp.getParameter<uint32_t>("layer"),
-                               badComp.getParameter<uint32_t>("bkw_frw"),
-                               badComp.getParameter<uint32_t>("int_ext"),
-                               badComp.getParameter<uint32_t>("ster"),
-                               badComp.getParameter<uint32_t>("string_"),
-                               badComp.getParameter<uint32_t>("detid")
-                               );
+          std::copy_if(beginDetIt, endDetIt, std::back_inserter(selList),
+              [tTopo,&badComp] ( uint32_t detectorId ) {
+                const DetId detId{detectorId};
+                return ( ( detId.subdetId() == SiStripDetId::TIB )
+                      && _isSel(badComp.getParameter<uint32_t>("layer")  , tTopo->tibLayer(detId))
+                      && _isSel(badComp.getParameter<uint32_t>("bkw_frw"), tTopo->tibIsZPlusSide(detId) ? 2 : 1 )
+                      && _isSel(badComp.getParameter<uint32_t>("int_ext"), tTopo->tibIsInternalString(detId) ? 1 : 2 )
+                      && _isSel(badComp.getParameter<uint32_t>("ster")   , tTopo->tibIsStereo(detId) ? 1 : ( tTopo->tibIsRPhi(detId) ? 2 : -1 ))
+                      && _isSel(badComp.getParameter<uint32_t>("string_"), tTopo->tibString(detId))
+                      && _isSel(badComp.getParameter<uint32_t>("detid")  , detId.rawId())
+                      );
+              } );
           break;
         case SiStripDetId::TID:
-          resp = isTIDDetector(tTopo,detectorId,
-                               badComp.getParameter<uint32_t>("side"),
-                               badComp.getParameter<uint32_t>("wheel"),
-                               badComp.getParameter<uint32_t>("ring"),
-                               badComp.getParameter<uint32_t>("ster"),
-                               badComp.getParameter<uint32_t>("detid")
-                               );
+          std::copy_if(beginDetIt, endDetIt, std::back_inserter(selList),
+              [tTopo,&badComp] ( uint32_t detectorId ) {
+                const DetId detId{detectorId};
+                return ( ( detId.subdetId() ==  SiStripDetId::TID )
+                      && _isSel(badComp.getParameter<uint32_t>("wheel"), tTopo->tidWheel(detId))
+                      && _isSel(badComp.getParameter<uint32_t>("side") , tTopo->tidIsZPlusSide(detId) ? 2 : 1 )
+                      && _isSel(badComp.getParameter<uint32_t>("ster") , tTopo->tidIsStereo(detId) ? 1 : ( tTopo->tidIsRPhi(detId) ? 2 : -1 ) )
+                      && _isSel(badComp.getParameter<uint32_t>("ring") , tTopo->tidRing(detId))
+                      && _isSel(badComp.getParameter<uint32_t>("detid"), detId.rawId())
+                      );
+              } );
           break;
         case SiStripDetId::TOB:
-          resp = isTOBDetector(tTopo,detectorId,
-                               badComp.getParameter<uint32_t>("layer"),
-                               badComp.getParameter<uint32_t>("bkw_frw"),
-                               badComp.getParameter<uint32_t>("rod"),
-                               badComp.getParameter<uint32_t>("ster"),
-                               badComp.getParameter<uint32_t>("detid")
-                               );
+          std::copy_if(beginDetIt, endDetIt, std::back_inserter(selList),
+              [tTopo,&badComp] ( uint32_t detectorId ) {
+                const DetId detId{detectorId};
+                return ( ( detId.subdetId() ==  SiStripDetId::TOB )
+                      && _isSel(badComp.getParameter<uint32_t>("layer")  , tTopo->tobLayer(detId))
+                      && _isSel(badComp.getParameter<uint32_t>("bkw_frw"), tTopo->tobIsZPlusSide(detId) ? 2 : 1)
+                      && _isSel(badComp.getParameter<uint32_t>("ster")   , tTopo->tobIsStereo(detId) ? 1 : ( tTopo->tobIsRPhi(detId) ? 2 : -1 ))
+                      && _isSel(badComp.getParameter<uint32_t>("rod")    , tTopo->tobRod(detId))
+                      && _isSel(badComp.getParameter<uint32_t>("detid")  , detId.rawId())
+                      );
+              } );
           break;
         case SiStripDetId::TEC:
-          resp = isTECDetector(tTopo,detectorId,
-                               badComp.getParameter<uint32_t>("side"),
-                               badComp.getParameter<uint32_t>("wheel"),
-                               badComp.getParameter<uint32_t>("petal_bkw_frw"),
-                               badComp.getParameter<uint32_t>("petal"),
-                               badComp.getParameter<uint32_t>("ring"),
-                               badComp.getParameter<uint32_t>("ster"),
-                               badComp.getParameter<uint32_t>("detid")
-                               );
+          std::copy_if(beginDetIt, endDetIt, std::back_inserter(selList),
+              [tTopo,&badComp] ( uint32_t detectorId ) {
+                const DetId detId{detectorId};
+                return ( ( detId.subdetId() == SiStripDetId::TEC )
+                      && _isSel(badComp.getParameter<uint32_t>("wheel")        , tTopo->tecWheel(detId))
+                      && _isSel(badComp.getParameter<uint32_t>("side")         , tTopo->tecIsZPlusSide(detId) ? 2 : 1 )
+                      && _isSel(badComp.getParameter<uint32_t>("ster")         , tTopo->tecIsStereo(detId) ? 1 : 2 )
+                      && _isSel(badComp.getParameter<uint32_t>("petal_bkw_frw"), tTopo->tecIsFrontPetal(detId) ? 2 : 2)
+                      && _isSel(badComp.getParameter<uint32_t>("petal")        , tTopo->tecPetalNumber(detId))
+                      && _isSel(badComp.getParameter<uint32_t>("ring")         , tTopo->tecRing(detId))
+                      && _isSel(badComp.getParameter<uint32_t>("detid")        , detId.rawId())
+                      );
+              } );
           break;
         default:
           break;
-      }
-      if ( anySubDet ) {
-        std::cout << "AnySubDet" << *iter << std::endl;
-        resp = ( std::find(genericBadDetIds.begin(), genericBadDetIds.end(), *iter) != genericBadDetIds.end() );
-      }
-
-      if ( resp ) {
-        selList.push_back(*iter);
       }
     }
   }
   if (m_printDebug) {
     edm::LogInfo("SiStripBadModuleGenerator") << ss.str();
   }
+  return selList;
 }
 
 //define this as a plug-in
