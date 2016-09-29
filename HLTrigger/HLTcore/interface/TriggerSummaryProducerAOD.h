8ca5024f53c76c3a884781e555e9eea9f7c3603e
Malik Shahzad Muzaffar <shahzad.malik.muzaffar@cern.ch>
2016-09-29 22:40:47 +0200

    Revert "Only get data products with labels beginning with 'hlt'"

--- HLTrigger/HLTcore/interface/TriggerSummaryProducerAOD.h
+++ HLTrigger/HLTcore/interface/TriggerSummaryProducerAOD.h
@@ -120,8 +120,6 @@ class TriggerSummaryProducerAOD : public edm::stream::EDProducer<edm::GlobalCach
  private:
   /// process name
   std::string pn_;
-  /// module labels which should be avoided
-  const std::vector<std::string> moduleLabelsToSkip_;
 
   /// InputTag ordering class
   struct OrderInputTag {
