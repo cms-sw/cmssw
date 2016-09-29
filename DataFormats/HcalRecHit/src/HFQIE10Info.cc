0550f175a5af9efcd8918039c8a4bb3847dd6ad0
Patrick Gartung <gartung@fnal.gov>
2016-09-29 20:40:11 +0200

    add other static HFQIE10Info::INVALID_RAW

--- DataFormats/HcalRecHit/src/HFQIE10Info.cc
+++ DataFormats/HcalRecHit/src/HFQIE10Info.cc
@@ -2,6 +2,7 @@
 #include "DataFormats/HcalRecHit/interface/HFQIE10Info.h"
 
 const unsigned HFQIE10Info::N_RAW_MAX;
+const HFQIE10Info::raw_type HFQIE10Info::INVALID_RAW;
  
 
 HFQIE10Info::HFQIE10Info()
