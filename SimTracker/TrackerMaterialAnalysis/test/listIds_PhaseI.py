3f7aa804e609d27e00cd1386bc80fd9dc47c55f0
Marco Rovere <marco.rovere@cern.ch>
2016-09-30 16:46:56 +0200

    Simplified listIds_PhaseI.py

--- SimTracker/TrackerMaterialAnalysis/test/listIds_PhaseI.py
+++ SimTracker/TrackerMaterialAnalysis/test/listIds_PhaseI.py
@@ -3,18 +3,19 @@
 import FWCore.ParameterSet.Config as cms
 
 process = cms.Process("MaterialAnalyser")
-# import of standard configurations
-process.load('Configuration.StandardSequences.Services_cff')
-process.load('SimGeneral.HepPDTESSource.pythiapdt_cfi')
-process.load('FWCore.MessageService.MessageLogger_cfi')
-process.load('Configuration.EventContent.EventContent_cff')
-process.load('Configuration.StandardSequences.GeometryRecoDB_cff')
-process.load('Configuration.StandardSequences.GeometryDB_cff')
-process.load('Configuration.StandardSequences.MagneticField_AutoFromDBCurrent_cff')
-process.load('Configuration.StandardSequences.FrontierConditions_GlobalTag_cff')
 
-from Configuration.AlCa.GlobalTag import GlobalTag
-process.GlobalTag = GlobalTag(process.GlobalTag, 'auto:phase1_2017_realistic', '')
+readGeometryFromDB = False
+
+if not readGeometryFromDB:
+  process.load('Configuration.Geometry.GeometryExtended2017NewFPixReco_cff')
+else:
+# GlobalTag and geometry via GT
+  process.load('Configuration.Geometry.GeometrySimDB_cff')
+  process.load('Configuration.StandardSequences.FrontierConditions_GlobalTag_condDBv2_cff')
+  from Configuration.AlCa.GlobalTag_condDBv2 import GlobalTag
+  process.GlobalTag = GlobalTag(process.GlobalTag, 'auto:phase1_2017_design', '')
+
+process.load('FWCore.MessageService.MessageLogger_cfi')
 
 process.maxEvents = cms.untracked.PSet(
     input = cms.untracked.int32(1)
@@ -25,7 +26,7 @@ process.source = cms.Source("EmptySource",
         firstRun = cms.untracked.uint32(1)
 )
 
-process.listIds = cms.EDAnalyzer("ListIds"
+process.listIds = cms.EDAnalyzer("ListIds",
                                 materials = cms.untracked.vstring("materials:Silicon" , "tracker:SenSi"),
                                 printMaterial = cms.untracked.bool(False)
                                 )
