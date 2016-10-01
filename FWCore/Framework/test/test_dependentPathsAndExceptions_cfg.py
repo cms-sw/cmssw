510d212f4d138c0b25cada9138dc56cbc3bec101
Chris Jones <chrisdjones15@gmail.com>
2016-10-01 14:53:04 -0500

    Add unit test for concurrent paths and exceptions
    Added a unit test for the case where a module on a path depends
    on data from a module on another path and the other module throws
    an exception.

---
+++ FWCore/Framework/test/test_dependentPathsAndExceptions_cfg.py
@@ -0,0 +1,16 @@
+import FWCore.ParameterSet.Config as cms
+
+process = cms.Process("Test")
+
+process.source = cms.Source("EmptySource")
+
+process.fail = cms.EDProducer("FailingProducer")
+
+process.readFail = cms.EDProducer("AddIntsProducer", labels = cms.vstring("fail"))
+
+process.a = cms.EDProducer("BusyWaitIntProducer", ivalue = cms.int32(5), iterations = cms.uint32(10000))
+
+process.p2 = cms.Path(process.fail)
+process.p1 = cms.Path(process.readFail+process.a)
+
+process.add_(cms.Service("Tracer"))
