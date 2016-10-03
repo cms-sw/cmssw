117f4926f3f8571da954fc2dba0fd26a0e61bebf
Jan Kašpar <jan.kaspar@cern.ch>
2016-10-03 18:31:32 +0200

    More compact event ranges.

--- EventFilter/CTPPSRawToDigi/python/totemRawToDigi_cff.py
+++ EventFilter/CTPPSRawToDigi/python/totemRawToDigi_cff.py
@@ -3,12 +3,12 @@ import FWCore.ParameterSet.Config as cms
 from CondFormats.CTPPSReadoutObjects.TotemDAQMappingESSourceXML_cfi import *
 totemDAQMappingESSourceXML.configuration = cms.untracked.VPSet(
   cms.PSet(
-    validityRange = cms.untracked.EventRange("1:1:1 - 280385:999999999:999999999999"),
+    validityRange = cms.untracked.EventRange("1:min - 280385:max"),
     mappingFileNames = cms.untracked.vstring("CondFormats/CTPPSReadoutObjects/xml/ctpps_mapping_to_fill_5288.xml"),
     maskFileNames = cms.untracked.vstring()
   ),
   cms.PSet(
-    validityRange = cms.untracked.EventRange("281601:1:1 - 999999999:999999999:999999999999"),
+    validityRange = cms.untracked.EventRange("281601:min - 999999999:max"),
     mappingFileNames = cms.untracked.vstring("CondFormats/CTPPSReadoutObjects/xml/ctpps_mapping_from_fill_5330.xml"),
     maskFileNames = cms.untracked.vstring()
   )
