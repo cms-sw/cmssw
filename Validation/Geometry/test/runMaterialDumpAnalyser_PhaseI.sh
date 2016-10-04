b152b9812b3ff0b5abf4789af9de583843c999ae
Marco Rovere <marco.rovere@cern.ch>
2016-10-04 15:37:42 +0200

    Automate PhaseI material comparison
    The runMaterialDumpAnalyser_PhaseI script has been upgraded
    to also generate the neutrino gun sample and the
    corresponding material maps derived from the simulation
    geometry. The script will proceed with the following order:
    first produce all SingleMu related files, running the real
    reconstruction and generating the reco-material map. Then
    move on to the simulation, using the neutrino particle gun
    and generating all the mandatory material maps from the
    simulation for all the Tracker components. Finally make the
    comparison between the reco-derived and simulation-derived
    material map. All steps are run iff the corresponding output
    files are not already present, otherwise they are skipped.
    If you want to regenerate a step for which the output file
    is already present, go on and remove it from the filesystem.
    The current script is tailored to use the improved
    2017NewFPix geometry.

--- Validation/Geometry/test/runMaterialDumpAnalyser_PhaseI.sh
+++ Validation/Geometry/test/runMaterialDumpAnalyser_PhaseI.sh
@@ -28,8 +28,17 @@ while getopts ":n:" opt; do
   esac
 done
 
+function checkFile {
+  [[ ! -e $1 ]] | [[ ! -s $1 ]]
+}
+
+# Wait for possible bg jobs launched in the previous loop
+function waitPendingJobs {
+  for job in $(jobs -p); do wait $job; done
+}
 
 # GEN-SIM goes first
+if checkFile SingleMuPt10_pythia8_cfi_GEN_SIM_PhaseI.root ; then
 cmsDriver.py SingleMuPt10_pythia8_cfi \
 -s GEN,SIM \
 --conditions auto:phase1_2017_realistic \
@@ -42,14 +51,16 @@ cmsDriver.py SingleMuPt10_pythia8_cfi \
 --fileout file:SingleMuPt10_pythia8_cfi_GEN_SIM_PhaseI.root \
 --python_filename SingleMuPt10_pythia8_cfi_GEN_SIM_PhaseI.py > SingleMuPt10_pythia8_cfi_GEN_SIM_PhaseI.log 2>&1
 
-if [ $? -ne 0 ]; then
-  echo "Error executing the GEN-SIM step, aborting."
-  exit 1
+    if [ $? -ne 0 ]; then
+      echo "Error executing the GEN-SIM step, aborting."
+      exit 1
+    fi
 fi
 
 # DIGI comes next
 
-cmsDriver.py step2  \
+if checkFile SingleMuPt10_step2_DIGI_L1_DIGI2RAW_HLT_PhaseI.root ; then
+  cmsDriver.py step2  \
 -s DIGI:pdigi_valid,L1,DIGI2RAW,HLT:@fake \
 --conditions auto:phase1_2017_realistic \
 -n -1 \
@@ -61,14 +72,16 @@ cmsDriver.py step2  \
 --fileout file:SingleMuPt10_step2_DIGI_L1_DIGI2RAW_HLT_PhaseI.root \
 --python_filename SingleMuPt10_step2_DIGI_L1_DIGI2RAW_HLT_PhaseI.py > SingleMuPt10_step2_DIGI_L1_DIGI2RAW_HLT_PhaseI.log 2>&1
 
-if [ $? -ne 0 ]; then
-  echo "Error executing the DIGI step, aborting."
-  exit 1
+    if [ $? -ne 0 ]; then
+      echo "Error executing the DIGI step, aborting."
+      exit 1
+    fi
 fi
 
 # Reco and special customization
 
-cmsDriver.py step3  \
+if checkFile SingleMuPt10_step3_RECO_DQM_PhaseI.root ; then
+  cmsDriver.py step3  \
 -s RAW2DIGI,L1Reco,RECO:reconstruction_trackingOnly,VALIDATION:@trackingOnlyValidation,DQM:@trackingOnlyDQM \
 --conditions auto:phase1_2017_realistic \
 -n -1 \
@@ -81,14 +94,16 @@ cmsDriver.py step3  \
 --python_filename SingleMuPt10_step2_RECO_DQM_PhaseI.py \
 --customise Validation/Geometry/customiseForDumpMaterialAnalyser.customiseForMaterialAnalyser > SingleMuPt10_step3_RECO_DQM_PhaseI.log 2>&1
 
-if [ $? -ne 0 ]; then
-  echo "Error executing the RECO step, aborting."
-  exit 1
+    if [ $? -ne 0 ]; then
+      echo "Error executing the RECO step, aborting."
+      exit 1
+    fi
 fi
 
 # HARVESTING
 
-cmsDriver.py step4  \
+if checkFile DQM_V0001_R000000001__Global__CMSSW_X_Y_Z__RECO.root ; then
+  cmsDriver.py step4  \
 -s HARVESTING:@trackingOnlyValidation+@trackingOnlyDQM  \
 --conditions auto:phase1_2017_realistic \
 -n -1   \
@@ -100,7 +115,35 @@ cmsDriver.py step4  \
 --filein file:SingleMuPt10_step3_RECO_DQM_PhaseI_inDQM.root  \
 --python_filename SingleMuPt10_step4_HARVESTING_PhaseI.py > SingleMuPt10_step4_HARVESTING_PhaseI.log 2>&1
 
-if [ $? -ne 0 ]; then
-  echo "Error executing the HARVESTING step, aborting."
-  exit 1
+    if [ $? -ne 0 ]; then
+      echo "Error executing the HARVESTING step, aborting."
+      exit 1
+    fi
 fi
+
+# Neutrino Particle gun
+
+if checkFile single_neutrino_random.root ; then
+  cmsRun ../python/single_neutrino_cfg.py
+  if [ $? -ne 0 ]; then
+    echo "Error generating single neutrino gun, aborting."
+    exit 1
+  fi
+  if [! -e Images ]; then
+    mkdir Images
+  fi
+fi
+
+# Make material map for each subdetector from simulation
+
+for t in BeamPipe Tracker PixBar PixFwdMinus PixFwdPlus TIB TOB TIDB TIDF TEC TkStrct InnerServices; do
+  if [ ! -e matbdg_${t}.root ]; then
+    cmsRun runP_Tracker_cfg.py geom=2017NewFPix label=$t >& /dev/null &
+  fi
+done
+
+waitPendingJobs
+
+# Always run the comparison at this stage, since you are guaranteed that all the ingredients are there
+
+root -b -q 'MaterialBudget_Simul_vs_Reco.C("DQM_V0001_R000000001__Global__CMSSW_X_Y_Z__RECO.root", "PhaseIDetector")'
