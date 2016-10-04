f68892ee29ec0d5e420434875a2d8f42b5245b35
SeyedMohsen <setesami@cern.ch>
2016-10-04 12:33:50 +0200

    const qualified applied for name methods and capitalization fixed

--- DataFormats/CTPPSDetId/src/CTPPSDiamondDetId.cc
+++ DataFormats/CTPPSDetId/src/CTPPSDiamondDetId.cc
@@ -14,7 +14,7 @@ using namespace std;
 
 CTPPSDiamondDetId::CTPPSDiamondDetId(uint32_t id) : CTPPSDetId(id)
 {
-  if (! Check(id))
+  if (! check(id))
     {
       throw cms::Exception("InvalidDetId") << "CTPPSDiamondDetId ctor:"
 					   << " det: " << det()
@@ -69,7 +69,7 @@ std::ostream& operator << (std::ostream& os, const CTPPSDiamondDetId& id)
 
 //----------------------------------------------------------------------------------------------------
 
-string CTPPSDiamondDetId::planeName( NameFlag flag)
+string CTPPSDiamondDetId::planeName( NameFlag flag) const
 {
   string name;
   if (flag == nFull) name = rpName(flag) + "_";
@@ -84,7 +84,7 @@ string CTPPSDiamondDetId::planeName( NameFlag flag)
 
 //----------------------------------------------------------------------------------------------------
 
-string CTPPSDiamondDetId::channelName(NameFlag flag)
+string CTPPSDiamondDetId::channelName(NameFlag flag) const
 {
   string name;
   if (flag == nFull) name = planeName(flag) + "_";
