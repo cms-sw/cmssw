59603015c86534d2f24d2f7d2152327f0a90498c
Jan Kašpar <jan.kaspar@cern.ch>
2016-10-05 12:07:36 +0200

    Important parameters changed to tracked.

--- CondFormats/CTPPSReadoutObjects/python/TotemDAQMappingESSourceXML_cfi.py
+++ CondFormats/CTPPSReadoutObjects/python/TotemDAQMappingESSourceXML_cfi.py
@@ -3,12 +3,12 @@ import FWCore.ParameterSet.Config as cms
 totemDAQMappingESSourceXML = cms.ESSource("TotemDAQMappingESSourceXML",
   verbosity = cms.untracked.uint32(0),
 
-  configuration = cms.untracked.VPSet(
+  configuration = cms.VPSet(
     # example configuration block:
     #cms.PSet(
-    #  validityRange = cms.untracked.EventRange("1:1:1 - 999999999:999999999:999999999999"),
-    #  mappingFileNames = cms.untracked.vstring("CondFormats/CTPPSReadoutObjects/xml/ctpps_mapping_to_fill_5288.xml"),
-    #  maskFileNames = cms.untracked.vstring()
+    #  validityRange = cms.EventRange("1:min - 999999999:max"),
+    #  mappingFileNames = cms.vstring("CondFormats/CTPPSReadoutObjects/xml/ctpps_mapping_to_fill_5288.xml"),
+    #  maskFileNames = cms.vstring()
     #)
   )
 )
