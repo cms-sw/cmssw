80e78fdf529ba3563e7fcda772d3485168396c36
Silvio <silvio.donato@cern.ch>
2016-10-06 01:53:26 +0200

    partial sync with /dev/CMSSW_8_0_0/HLT/V609

--- HLTrigger/Configuration/python/HLT_HIon_cff.py
+++ HLTrigger/Configuration/python/HLT_HIon_cff.py
@@ -1,11 +1,11 @@
-# /dev/CMSSW_8_0_0/HIon/V203 (CMSSW_8_0_19_patch2)
+# /dev/CMSSW_8_0_0/HIon/V205 (CMSSW_8_0_19_patch2)
 
 import FWCore.ParameterSet.Config as cms
 
 fragment = cms.ProcessFragment( "HLT" )
 
 fragment.HLTConfigVersion = cms.PSet(
-  tableName = cms.string('/dev/CMSSW_8_0_0/HIon/V203')
+  tableName = cms.string('/dev/CMSSW_8_0_0/HIon/V205')
 )
 
 fragment.HLTPSetJetCoreStepTrajectoryFilter = cms.PSet( 
