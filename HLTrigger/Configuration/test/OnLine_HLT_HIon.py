c92db540ff581a7ee32f967bdbfac56b57ac5b5e
Silvio <silvio.donato@cern.ch>
2016-10-06 02:01:05 +0200

    complete sync with /dev/CMSSW_8_0_0/HLT/V609

--- HLTrigger/Configuration/test/OnLine_HLT_HIon.py
+++ HLTrigger/Configuration/test/OnLine_HLT_HIon.py
@@ -1,11 +1,11 @@
-# /dev/CMSSW_8_0_0/HIon/V203 (CMSSW_8_0_19_patch2)
+# /dev/CMSSW_8_0_0/HIon/V205 (CMSSW_8_0_19_patch2)
 
 import FWCore.ParameterSet.Config as cms
 
 process = cms.Process( "HLTHIon" )
 
 process.HLTConfigVersion = cms.PSet(
-  tableName = cms.string('/dev/CMSSW_8_0_0/HIon/V203')
+  tableName = cms.string('/dev/CMSSW_8_0_0/HIon/V205')
 )
 
 process.HLTPSetJetCoreStepTrajectoryFilter = cms.PSet( 
