c727e57583972f35bd609e3c25a64c49ab8e466d
Alexander Morton <alexander.david.morton.cern.ch>
2016-10-06 21:07:21 +0200

    Snaphot - unstable still.

--- Validation/SiPixelPhase1ConfigV/python/SiPixelPhase1OfflineDQM_harvestingV_cff.py
+++ Validation/SiPixelPhase1ConfigV/python/SiPixelPhase1OfflineDQM_harvestingV_cff.py
@@ -1,8 +1,8 @@
 import FWCore.ParameterSet.Config as cms
 
-from Validation.SiPixelPhase1ConfigV.SiPixelPhase1OfflineDQM_sourceV_cff import *
+from  Validation.SiPixelPhase1ConfigV.SiPixelPhase1OfflineDQM_sourceV_cff import *
 
 siPixelPhase1OfflineDQM_harvestingV = cms.Sequence(SiPixelPhase1DigisHarvesterV
-                                                + SiPixelPhase1TrackClustersHarvesterV
                                                 + SiPixelPhase1RecHitsHarvesterV
+                                                + SiPixelPhase1TrackClustersHarvesterV
                                                 )
--- Validation/SiPixelPhase1ConfigV/python/SiPixelPhase1OfflineDQM_sourceV_cff.py
+++ Validation/SiPixelPhase1ConfigV/python/SiPixelPhase1OfflineDQM_sourceV_cff.py
@@ -2,14 +2,14 @@ import FWCore.ParameterSet.Config as cms
 
 # Pixel Digi Monitoring
 from Validation.SiPixelPhase1DigisV.SiPixelPhase1DigisV_cfi import *
-# Pixel Clusters
-from Validation.SiPixelPhase1TrackClustersV.SiPixelPhase1TrackClustersV_cfi import *
 # RecHit (clusters)
 from Validation.SiPixelPhase1RecHitsV.SiPixelPhase1RecHitsV_cfi import *
+# Clusters ontrack/offtrack (also general tracks)
+from Validation.SiPixelPhase1TrackClustersV.SiPixelPhase1TrackClustersV_cfi import *
 
 PerModule.enabled = False
 
 siPixelPhase1OfflineDQM_sourceV = cms.Sequence(SiPixelPhase1DigisAnalyzerV
-                                            + SiPixelPhase1TrackClustersAnalyzerV
                                             + SiPixelPhase1RecHitsAnalyzerV
+                                            + SiPixelPhase1TrackClustersAnalyzerV
                                             )
