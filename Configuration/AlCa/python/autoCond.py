106cdedfb2cae8a4bbe31abe32369ffc62b96118
Matti Kortelainen <matti.kortelainen@cern.ch>
2016-10-06 13:03:00 +0200

    Proper GTs for auto:phase1_2017_{design,realistic}, remove auto:phase1_2017_hcaldev

--- Configuration/AlCa/python/autoCond.py
+++ Configuration/AlCa/python/autoCond.py
@@ -36,11 +36,9 @@ autoCond = {
     # GlobalTag for Run2 HLT for HI: it points to the online GT
     'run2_hlt_hi'       :   '81X_dataRun2_HLTHI_frozen_v2',
     # GlobalTag for MC production with perfectly aligned and calibrated detector for Phase1 2017
-    'phase1_2017_design' :  '81X_upgrade2017_design_Candidate_2016_10_05_11_42_10',
+    'phase1_2017_design' :  '81X_upgrade2017_design_v14',
     # GlobalTag for MC production with realistic conditions for Phase1 2017 detector
-    'phase1_2017_realistic': '81X_upgrade2017_realistic_Candidate_2016_10_05_11_41_20',
-    # Global Tag for MC production with development HCAL conditions for Phase1 2017 detector
-    'phase1_2017_hcaldev'  : '81X_upgrade2017_HCALdev_Candidate_2016_10_05_11_20_17',
+    'phase1_2017_realistic': '81X_upgrade2017_realistic_v15',
     # GlobalTag for MC production with perfectly aligned and calibrated detector for Phase1 2019
     'phase1_2019_design'   : 'DES19_70_V2', # placeholder (GT not meant for standard RelVal) 
     # GlobalTag for MC production with realistic conditions for Phase2 2023
