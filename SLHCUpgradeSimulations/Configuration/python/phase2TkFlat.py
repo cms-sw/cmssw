bca846a07f461d6c085acc9a93436217d4feeeb6
erica brondolin <erica.brondolin@cern.ch>
2016-10-11 11:25:02 +0200

    cleaning up in the custom py files

--- SLHCUpgradeSimulations/Configuration/python/phase2TkFlat.py
+++ SLHCUpgradeSimulations/Configuration/python/phase2TkFlat.py
@@ -1,13 +1,8 @@
 import FWCore.ParameterSet.Config as cms
-#import SLHCUpgradeSimulations.Configuration.customise_PFlow as customise_PFlow
 
 #GEN-SIM so far...
 def customise(process):
     print "!!!You are using the SUPPORTED Flat version of the Phase2 Tracker !!!"
-    if hasattr(process,'DigiToRaw'):
-        process=customise_DigiToRaw(process)
-    if hasattr(process,'RawToDigi'):
-        process=customise_RawToDigi(process)
     n=0
     if hasattr(process,'reconstruction') or hasattr(process,'dqmoffline_step'):
         if hasattr(process,'mix'):
@@ -17,33 +12,12 @@ def customise(process):
             print 'phase1TkCustoms requires a --pileup option to cmsDriver to run the reconstruction/dqm'
             print 'Please provide one!'
             sys.exit(1)
-    if hasattr(process,'reconstruction'):
-        process=customise_Reco(process,float(n))
-    if hasattr(process,'digitisation_step'):
-        process=customise_Digi(process)
-    if hasattr(process,'validation_step'):
-        process=customise_Validation(process,float(n))
     process=customise_condOverRides(process)
 
     return process
 
-def customise_Digi(process):
-    return process
-
-
-def customise_DigiToRaw(process):
-    return process
-
-def customise_RawToDigi(process):
-    return process
-
-def customise_Reco(process,pileup):
-    return process
-
 def customise_condOverRides(process):
+    # this is a custom specific for tilted/flat .. how do we want to deal with it?
     process.load('SLHCUpgradeSimulations.Geometry.fakeConditions_phase2TkFlat_cff')
     return process
 
-
-def customise_Validation(process,pileup):
-    return process
