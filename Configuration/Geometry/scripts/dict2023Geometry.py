d3d790647563c3629aeeb7ba54426283d1c6f8f0
piet verwilligen <piet.verwilligen@cern.ch>
2016-10-13 17:08:50 +0200

    update

--- Configuration/Geometry/scripts/dict2023Geometry.py
+++ Configuration/Geometry/scripts/dict2023Geometry.py
@@ -435,6 +435,7 @@ muonDict = {
             'from RecoMuon.DetLayers.muonDetLayerGeometry_cfi import *',
             'from Geometry.GEMGeometryBuilder.gemGeometry_cfi import *',
             'from Geometry.GEMGeometryBuilder.me0Geometry_cfi import *',
+            'ME0GeometryESModule.use10EtaPart = cms.bool(True)',
             'from Geometry.CSCGeometryBuilder.idealForDigiCscGeometry_cff import *',
             'from Geometry.DTGeometryBuilder.idealForDigiDtGeometry_cff import *',
         ],
