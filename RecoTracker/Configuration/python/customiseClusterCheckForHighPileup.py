759cc5c212eba918fd32be0e7d04bfd0a2d31611
Matti Kortelainen <matti.kortelainen@cern.ch>
2016-10-14 16:51:18 +0200

    Increase also maxElement for PixelTripletLargeTipGenerator and MultiHitGeneratorFromChi2

--- RecoTracker/Configuration/python/customiseClusterCheckForHighPileup.py
+++ RecoTracker/Configuration/python/customiseClusterCheckForHighPileup.py
@@ -5,5 +5,8 @@ def customiseClusterCheckForHighPileup(process):
             # PhotonConversionTrajectorySeedProducerFromQuadruplets does not have "cut"...
             if hasattr(module.ClusterCheckPSet, "cut"):
                 module.ClusterCheckPSet.cut = "strip < 4000000 && pixel < 400000 && (strip < 500000 + 10*pixel) && (pixel < 50000 + 0.1*strip)"
+        if hasattr(module, "OrderedHitsFactoryPSet") and hasattr(module.OrderedHitsFactoryPSet, "GeneratorPSet"):
+            if module.OrderedHitsFactoryPSet.GeneratorPSet.ComponentName.value() in ["PixelTripletLargeTipGenerator", "MultiHitGeneratorFromChi2"]:
+                module.OrderedHitsFactoryPSet.GeneratorPSet.maxElement = 500000
 
     return process
