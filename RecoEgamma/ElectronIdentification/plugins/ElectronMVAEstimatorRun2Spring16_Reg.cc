53a027750b2ea05a3fe6fa0131147404f345240a
Ilya Kravchenko <ikrav@unl.edu>
2016-10-17 23:51:56 +0200

    Added 80X electron MVA implementation. Restructured location of MVA class files (including old ones) to make things work better for FWLite.

---
+++ RecoEgamma/ElectronIdentification/plugins/ElectronMVAEstimatorRun2Spring16_Reg.cc
@@ -0,0 +1,5 @@
+#include "RecoEgamma/ElectronIdentification/interface/ElectronMVAEstimatorRun2Spring16.h"
+
+DEFINE_EDM_PLUGIN(AnyMVAEstimatorRun2Factory,
+		  ElectronMVAEstimatorRun2Spring16,
+		  "ElectronMVAEstimatorRun2Spring16");
