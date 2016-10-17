1e86aa162fd8678084e58c3e46b59fbafce27066
Christopher Brainerd <cbbrainerd@ucdavis.edu>
2016-10-17 18:57:29 -0500

    Change global tag to one with the necessary conditions

--- DQMOffline/L1Trigger/test/driver11b.sh
+++ DQMOffline/L1Trigger/test/driver11b.sh
@@ -7,7 +7,7 @@ DQMSEQUENCE=HARVESTING:dqmHarvesting
 step=b
 
 #cmsDriver.py test_${tnum}_${step}_1 -s ${DQMSEQUENCE} --conditions auto:com10 --data --filetype DQM --filein file:test_${tnum}_a_1_RAW2DIGI_RECO_DQM.root --scenario pp --customise DQMServices/Components/test/customHarvesting.py --no_exec --python_filename=test_${tnum}_${step}_1.py
-cmsDriver.py test_${tnum}_${step}_1 -s ${DQMSEQUENCE} --conditions 80X_dataRun2_Prompt_v9 --data --filetype DQM --filein file:test_${tnum}_a_1_RAW2DIGI_RECO_DQM.root --scenario pp --customise DQMServices/Components/test/customHarvesting.py --no_exec --python_filename=test_${tnum}_${step}_1.py --era Run2_2016
+cmsDriver.py test_${tnum}_${step}_1 -s ${DQMSEQUENCE} --conditions 81X_upgrade2023_realistic_v1 --data --filetype DQM --filein file:test_${tnum}_a_1_RAW2DIGI_RECO_DQM.root --scenario pp --customise DQMServices/Components/test/customHarvesting.py --no_exec --python_filename=test_${tnum}_${step}_1.py --era Run2_2016
 cmsRun -e test_${tnum}_${step}_1.py 2>&1 | tee q${tnum}.1.log 
 
 if [ $? -ne 0 ]; then
