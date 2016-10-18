1e89c4b0192f05c9f10f5425b80dea841e35764d
Salvatore Di Guida <salvatore.di.guida@cern.ch>
2016-10-18 17:57:24 +0200

    Move the implementation of the PopCon handler for B transitions in CondTools/RunInfo: it is a tool manipulating tags based on a condition on the magnet current as seen in the RunInfo payloads.

--- CondTools/Ecal/plugins/EcalADCToGeVConstantPopConBTransitionAnalyzer.cc
+++ CondTools/Ecal/plugins/EcalADCToGeVConstantPopConBTransitionAnalyzer.cc
@@ -1,6 +1,6 @@
-#include "CondCore/PopCon/interface/PopConBTransitionSourceHandler.h"
 #include "CondCore/PopCon/interface/PopConAnalyzer.h"
 #include "CondFormats/EcalObjects/interface/EcalADCToGeVConstant.h"
+#include "CondTools/RunInfo/interface/PopConBTransitionSourceHandler.h"
 #include "FWCore/Framework/interface/MakerMacros.h"
 
 typedef popcon::PopConAnalyzer< popcon::PopConBTransitionSourceHandler<EcalADCToGeVConstant> > EcalADCToGeVConstantPopConBTransitionAnalyzer;
