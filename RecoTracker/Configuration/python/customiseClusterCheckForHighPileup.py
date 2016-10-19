a281db18c7556c3c7a23ea7e24ac95f67cbefe31
Matti Kortelainen <matti.kortelainen@cern.ch>
2016-10-19 09:56:16 +0200

    Increase PixelTripletLargeTipGenerator+MultiHitGeneratorFromChi2 maxElement to 1M

--- RecoTracker/Configuration/python/customiseClusterCheckForHighPileup.py
+++ RecoTracker/Configuration/python/customiseClusterCheckForHighPileup.py
@@ -7,6 +7,6 @@ def customiseClusterCheckForHighPileup(process):
                 module.ClusterCheckPSet.cut = "strip < 800000 && pixel < 80000 && (strip < 50000 + 10*pixel) && (pixel < 5000 + strip/7.)"
         if hasattr(module, "OrderedHitsFactoryPSet") and hasattr(module.OrderedHitsFactoryPSet, "GeneratorPSet"):
             if module.OrderedHitsFactoryPSet.GeneratorPSet.ComponentName.value() in ["PixelTripletLargeTipGenerator", "MultiHitGeneratorFromChi2"]:
-                module.OrderedHitsFactoryPSet.GeneratorPSet.maxElement = 500000
+                module.OrderedHitsFactoryPSet.GeneratorPSet.maxElement = 1000000
 
     return process
