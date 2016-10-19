98ce1415bf744f3ce9847872b8a44b6c18d4cbb5
piet verwilligen <piet.verwilligen@cern.ch>
2016-10-19 12:38:47 +0200

    remove short/long reference for GE2/1

--- Geometry/GEMGeometryBuilder/src/GEMGeometryBuilderFromDDD.cc
+++ Geometry/GEMGeometryBuilder/src/GEMGeometryBuilderFromDDD.cc
@@ -122,9 +122,6 @@ GEMGeometry* GEMGeometryBuilderFromDDD::buildGeometry(DDFilteredView& fv, const 
       GEMStation* station = new GEMStation(re, st);
       std::string sign( re==-1 ? "-" : "");
       std::string name("GE" + sign + std::to_string(st) + "/1");
-      // Closest (furthest) super chambers in GE2/1 are called GE2/1s (GE2/1l)
-      if (st==2) name = "GE" + sign + std::to_string(st) + "/1s";
-      if (st==3) name = "GE" + sign + std::to_string(st-1) + "/1l";
       station->setName(name);
       for (int ri=1; ri<=1; ++ri) {
 	GEMRing* ring = new GEMRing(re, st, ri);
