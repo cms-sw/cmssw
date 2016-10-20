edb4b16fce8a07c482b5d05ac48fee180ba87110
Steven Lowette <steven.lowette@cern.ch>
2016-10-20 20:53:43 +0200

    First test version of CPE with Lorentz angle correction.

--- RecoLocalTracker/Phase2TrackerRecHits/plugins/Phase2StripCPEESProducer.cc
+++ RecoLocalTracker/Phase2TrackerRecHits/plugins/Phase2StripCPEESProducer.cc
@@ -5,10 +5,13 @@
 
 #include "Geometry/TrackerGeometryBuilder/interface/TrackerGeometry.h"
 #include "DataFormats/TrackerCommon/interface/TrackerTopology.h"
+#include "MagneticField/Engine/interface/MagneticField.h"
+#include "MagneticField/Records/interface/IdealMagneticFieldRecord.h"
 
 #include "RecoLocalTracker/Records/interface/TkStripCPERecord.h"
 #include "RecoLocalTracker/ClusterParameterEstimator/interface/ClusterParameterEstimator.h"
 #include "RecoLocalTracker/Phase2TrackerRecHits/interface/Phase2StripCPE.h"
+#include "RecoLocalTracker/Phase2TrackerRecHits/interface/Phase2StripCPETrivial.h"
 #include "RecoLocalTracker/Phase2TrackerRecHits/interface/Phase2StripCPEGeometric.h"
 
 #include "DataFormats/Phase2TrackerCluster/interface/Phase2TrackerCluster1D.h"
@@ -25,7 +28,7 @@ class Phase2StripCPEESProducer: public edm::ESProducer {
 
     private:
 
-        enum CPE_t { TRIVIAL, GEOMETRIC };
+        enum CPE_t { DEFAULT, TRIVIAL, GEOMETRIC };
         std::map<std::string, CPE_t> enumMap_;
 
         CPE_t cpeNum_;
@@ -38,7 +41,8 @@ class Phase2StripCPEESProducer: public edm::ESProducer {
 Phase2StripCPEESProducer::Phase2StripCPEESProducer(const edm::ParameterSet & p) {
   std::string name = p.getParameter<std::string>("ComponentType");
 
-  enumMap_[std::string("Phase2StripCPE")]   = TRIVIAL;
+  enumMap_[std::string("Phase2StripCPE")]          = DEFAULT;
+  enumMap_[std::string("Phase2StripCPETrivial")]   = TRIVIAL;
   enumMap_[std::string("Phase2StripCPEGeometric")] = GEOMETRIC;
   if (enumMap_.find(name) == enumMap_.end())
     throw cms::Exception("Unknown StripCPE type") << name;
@@ -51,9 +55,15 @@ Phase2StripCPEESProducer::Phase2StripCPEESProducer(const edm::ParameterSet & p) 
 
 std::shared_ptr<ClusterParameterEstimator<Phase2TrackerCluster1D> > Phase2StripCPEESProducer::produce(const TkStripCPERecord & iRecord) {
 
+  edm::ESHandle<MagneticField> magfield;
+
   switch(cpeNum_) {
+    case DEFAULT:
+      iRecord.getRecord<IdealMagneticFieldRecord>().get(magfield );
+      cpe_ = std::make_shared<Phase2StripCPE>(pset_, *magfield);
+      break;
     case TRIVIAL:
-      cpe_ = std::make_shared<Phase2StripCPE>();
+      cpe_ = std::make_shared<Phase2StripCPETrivial>();
       break;
     case GEOMETRIC:
       cpe_ = std::make_shared<Phase2StripCPEGeometric>(pset_);
--- RecoLocalTracker/Phase2TrackerRecHits/src/Phase2StripCPE.cc
+++ RecoLocalTracker/Phase2TrackerRecHits/src/Phase2StripCPE.cc
@@ -1,16 +1,55 @@
+
 #include "RecoLocalTracker/Phase2TrackerRecHits/interface/Phase2StripCPE.h"
+#include "Geometry/CommonTopologies/interface/PixelTopology.h"
 
 
-Phase2StripCPE::LocalValues Phase2StripCPE::localParameters(const Phase2TrackerCluster1D & cluster, const GeomDetUnit & det) const {
-  float strippitch  = 0.0090; // hardcoded dummy, a la 2S
-  float striplength = 5.;     // hardcoded dummy, a la 2S
-  std::pair<float, float> barycenter = cluster.barycenter();
-  LocalPoint lp( barycenter.second * strippitch, striplength * (barycenter.first + 1/2), 0 ); // x, y, z
-  LocalError le( pow(strippitch,2) / 12, 0, pow(striplength,2) / 12 ); // e2_xx, e2_xy, e2_yy
+Phase2StripCPE::Phase2StripCPE(edm::ParameterSet & conf, const MagneticField & magf)
+{
+  magfield_ = &magf;
+}
+
+
+Phase2StripCPE::LocalValues Phase2StripCPE::localParameters(
+  const Phase2TrackerCluster1D & cluster,
+  const GeomDetUnit & detunit) const
+{
+  const Phase2TrackerGeomDetUnit & det = (const Phase2TrackerGeomDetUnit &) detunit;
+  const Phase2TrackerTopology * topo = &det.specificTopology();
+
+  float pitch_x = topo->pitch().first;
+  float pitch_y = topo->pitch().second;
+  float ix = cluster.center();
+  float iy = cluster.column()+0.5; // halfway the column
+
+  LocalPoint lp( topo->localX(ix), topo->localY(iy), 0 );          // x, y, z
+  LocalError le( pow(pitch_x, 2) / 12, 0, pow(pitch_y, 2) / 12);   // e2_xx, e2_xy, e2_yy
+
+  // see https://github.com/cms-sw/cmssw/blob/CMSSW_8_1_X/RecoLocalTracker/SiStripRecHitConverter/src/StripCPE.cc
+
+  float thickness = det.specificSurface().bounds().thickness();
+  std::cout << "=== THICK " << thickness << std::endl;
+  LocalVector drift = driftDirection(det) * thickness;
+  std::cout << "=== DRIFT " << drift << std::endl;
+  LocalVector lvec = drift + LocalVector(0,0,-thickness);
+  float coveredStrips = lvec.x() / pitch_x; // simplifies wrt Phase0 tracker because only rectangular modules
+  std::cout << "=== SHIFT " << coveredStrips << std::endl;
+  float strip = cluster.center() - 0.5 * coveredStrips;
+  std::cout << "=== STRIP " << strip << std::endl;
+
   return std::make_pair( lp, le );
 }
 
 
-// needed, otherwise linker misses some refs
-#include "FWCore/Utilities/interface/typelookup.h"
-TYPELOOKUP_DATA_REG(ClusterParameterEstimator<Phase2TrackerCluster1D>);
+LocalVector Phase2StripCPE::driftDirection(
+  const Phase2TrackerGeomDetUnit & det) const
+{
+  LocalVector lbfield = (det.surface()).toLocal(magfield_->inTesla(det.surface().position()));  
+  
+  float tanLorentzAnglePerTesla = 0.11; // LorentzAngleMap_.getLorentzAngle(det->geographicalId().rawId());
+  
+  float dir_x = -tanLorentzAnglePerTesla * lbfield.y();
+  float dir_y =  tanLorentzAnglePerTesla * lbfield.x();
+  float dir_z =  1.f; // E field always in z direction
+  
+  return LocalVector(dir_x,dir_y,dir_z);
+}
