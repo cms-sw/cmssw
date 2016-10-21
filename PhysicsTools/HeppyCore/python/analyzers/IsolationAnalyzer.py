b1889ceeacfb29c63d834c72967b9987d4d96718
Colin <colin.bernet@cern.ch>
2016-10-21 10:46:22 +0200

    LeptonAnalyzer becomes IsolationAnalyzer

--- PhysicsTools/HeppyCore/python/analyzers/IsolationAnalyzer.py
+++ PhysicsTools/HeppyCore/python/analyzers/IsolationAnalyzer.py
@@ -1,18 +1,19 @@
 from heppy.framework.analyzer import Analyzer
 from heppy.particles.isolation import IsolationComputer, IsolationInfo
 
+import pprint 
 
-pdgids = [211, 22, 130]
+pdgids = [211, 22, 130, 11, 13]
 
-class LeptonAnalyzer(Analyzer):
+class IsolationAnalyzer(Analyzer):
     '''Compute lepton isolation.  
 
     Example:
 
-    from heppy.analyzers.LeptonAnalyzer import LeptonAnalyzer
+    from heppy.analyzers.IsolationAnalyzer import IsolationAnalyzer
     from heppy.particles.isolation import EtaPhiCircle
     iso_leptons = cfg.Analyzer(
-    LeptonAnalyzer,
+    IsolationAnalyzer,
       leptons = 'leptons',
       particles = 'particles',
       iso_area = EtaPhiCircle(0.4)
@@ -22,8 +23,15 @@ class LeptonAnalyzer(Analyzer):
 
     * particles : collection of particles w/r to which the leptons should be isolated. 
 
-    The particles are assumed to have a pdgid equal to +- 211 (all charged hadrons), 
-    22 (photons), or 130 (neutral hadrons). 
+    The particles are assumed to have a pdgid equal to 
+    +- 11 (electrons)
+    +- 13 (muons)
+    +- 211 (all other charged particles) 
+    22 (photons)
+    130 (all other neutral particles) 
+
+    If one of the particles considered in the isolation calculation is 
+    the lepton (is the same python object), it is discarded. 
 
     For each pdgid, the isolation result is attached to the lepton.
     For example, to keep track of isolation w/r to charged hadrons, an 
@@ -39,7 +47,7 @@ class LeptonAnalyzer(Analyzer):
     '''
     
     def beginLoop(self, setup):
-        super(LeptonAnalyzer, self).beginLoop(setup)
+        super(IsolationAnalyzer, self).beginLoop(setup)
         # now using same isolation definition for all pdgids
         self.iso_computers = dict()
         for pdgid in pdgids:
@@ -53,11 +61,30 @@ class LeptonAnalyzer(Analyzer):
         leptons = getattr(event, self.cfg_ana.leptons)
         for lepton in leptons:
             isosum = IsolationInfo('all', lepton)
+            self.logger.info(str(lepton))
             for pdgid in pdgids:
-                sel_ptcs = [ptc for ptc in particles if ptc.pdgid()==pdgid]
+                sel_ptcs = [ptc for ptc in particles if abs(self.pdgid(ptc))==pdgid]
                 iso = self.iso_computers[pdgid].compute(lepton, sel_ptcs)
                 isosum += iso 
                 setattr(lepton, 'iso_{pdgid}'.format(pdgid=pdgid), iso)
+                self.logger.info(str(iso))
+                if iso.num:
+                    self.logger.info(pprint.pformat(iso.particles))             
             lepton.iso = isosum
+            self.logger.info(str(isosum))
         
-  
+    def pdgid(self, ptc): 
+        '''returns summary pdg id.
+        - e or mu -> +- 11 or +- 13
+        - charged -> +- 211
+        - photon -> 22
+        - other -> neutral hadron
+        '''
+        if ptc.pdgid() in [11,13]:
+            return ptc.pdgid() 
+        elif ptc.q(): 
+            return ptc.q() * 211
+        elif ptc.pdgid() == 22: 
+            return 22
+        else: 
+            return 130
