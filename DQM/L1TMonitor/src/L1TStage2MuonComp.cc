1ea33ac7497deb2555e92f60b44c804a15e6d01b
Christopher Brainerd <cbbrainerd@ucdavis.edu>
2016-10-20 19:58:07 -0500

    Fix bug in L1TStage2MuonComp.cc

--- DQM/L1TMonitor/src/L1TStage2MuonComp.cc
+++ DQM/L1TMonitor/src/L1TStage2MuonComp.cc
@@ -146,7 +146,7 @@ void L1TStage2MuonComp::analyze(const edm::Event& e, const edm::EventSetup& c) {
 
     muonIt1 = muonBxColl1->begin(iBx);
     muonIt2 = muonBxColl2->begin(iBx);
-    while(muonIt1 != muonBxColl1->end(iBx) && muonIt2 != muonBxColl1->end(iBx)) {
+    while(muonIt1 != muonBxColl1->end(iBx) && muonIt2 != muonBxColl2->end(iBx)) {
       summary->Fill(MUONALL);
       if (muonIt1->hwPt() != muonIt2->hwPt()) {
         muonMismatch = true;
