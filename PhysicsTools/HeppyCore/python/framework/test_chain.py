413344f496a5f930de8452639a7ff47fed9ee131
Colin <colin.bernet@cern.ch>
2016-10-21 11:02:13 +0200

    synced framework/chain

--- PhysicsTools/HeppyCore/python/framework/test_chain.py
+++ PhysicsTools/HeppyCore/python/framework/test_chain.py
@@ -2,7 +2,9 @@ import unittest
 import os
 import shutil
 
-from chain import Chain
+from ROOT import TFile
+
+from PhysicsTools.HeppyCore.framework.chain import Chain
 from PhysicsTools.HeppyCore.utils.testtree import create_tree
 
 testfname = 'test_tree.root'
@@ -10,7 +12,11 @@ testfname = 'test_tree.root'
 class ChainTestCase(unittest.TestCase):
 
     def setUp(self):
+        self.fname = create_tree()
+        rootfile = TFile(self.fname)
+        self.nevents = rootfile.Get('test_tree').GetEntries()
         self.chain = Chain(testfname, 'test_tree')
+
 
     def test_file(self):
         '''Test that the test file exists'''
@@ -22,24 +28,24 @@ class ChainTestCase(unittest.TestCase):
 
     def test_guess_treename(self):
         chain = Chain(testfname)
-        self.assertEqual(len(self.chain), 100)        
+        self.assertEqual(len(self.chain), self.nevents)        
 
     def test_load_1(self):
         '''Test that the chain has the correct number of entries'''
-        self.assertEqual(len(self.chain), 100)
+        self.assertEqual(len(self.chain), self.nevents)
 
     def test_load_2(self):
         '''Test chaining of two files.'''
         tmpfile = testfname.replace('test_tree', 'test_tree_2_tmp')
         shutil.copyfile(testfname, tmpfile)
         chain = Chain(testfname.replace('.root', '*.root'), 'test_tree')
-        self.assertEqual(len(chain), 200)
+        self.assertEqual(len(chain), self.nevents*2)
         os.remove(tmpfile)
     
     def test_load_3(self):
         '''Test LFN/root-fn loading'''
         chain = Chain(["root://{0}".format(os.path.abspath(testfname))], 'test_tree')
-        self.assertEqual(len(chain), 100)
+        self.assertEqual(len(chain), self.nevents)
 
     def test_iterate(self):
         '''Test iteration'''
@@ -54,5 +60,4 @@ class ChainTestCase(unittest.TestCase):
 
 
 if __name__ == '__main__':
-    create_tree(testfname)
     unittest.main()
