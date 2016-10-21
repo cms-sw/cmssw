13c7189766c368143134af7326c8d73c702e481a
Michael <michael.andrews@cern.ch>
2016-10-21 12:36:30 +0200

    Revert FEStatus to run based. Get LS reports from FEMap instead.

--- DQM/EcalMonitorTasks/python/RawDataTask_cfi.py
+++ DQM/EcalMonitorTasks/python/RawDataTask_cfi.py
@@ -276,7 +276,6 @@ ecalRawDataTask = cms.untracked.PSet(
             ),
             otype = cms.untracked.string('SM'),
             btype = cms.untracked.string('SuperCrystal'),
-            perLumi = cms.untracked.bool(True),
             description = cms.untracked.string('FE status counter.')
         ),
         FEStatusErrMapByLumi = cms.untracked.PSet(
@@ -284,6 +283,7 @@ ecalRawDataTask = cms.untracked.PSet(
             kind = cms.untracked.string('TH2F'),
             otype = cms.untracked.string('Ecal3P'),
             btype = cms.untracked.string('SuperCrystal'),
+            perLumi = cms.untracked.bool(True),
             description = cms.untracked.string('FE status error occupancy map for this lumisection. Nominal FE status flags such as ENABLED, SUPPRESSED, FIFOFILL, FIFOFULLL1ADESYNC, and FORCEDZS are NOT included.')
         )
     )
