b8c6e31c7f7be743fd5b9449de571791aea2c49a
Matthew <Matthew.Nguyen@cern.ch>
2016-10-24 10:25:58 +0200

    fixed label conflicts in zee skim and enabled pA skims from cmsDriver

--- Configuration/Skimming/python/PA_ZEESkim_cff.py
+++ Configuration/Skimming/python/PA_ZEESkim_cff.py
@@ -1,8 +1,5 @@
 import FWCore.ParameterSet.Config as cms
 
-#cuts
-ELECTRON_CUT=("pt > 25 && abs(eta)<1.44")
-DIELECTRON_CUT=("mass > 80 && mass < 110")
 
 # HLT dimuon trigger
 import HLTrigger.HLTfilters.hltHighLevel_cfi
@@ -13,35 +10,35 @@ hltZEEHI.andOr = True
 
 # selection of valid vertex
 primaryVertexFilterForZEE = cms.EDFilter("VertexSelector",
-    src = cms.InputTag("offlinePrimaryVertices"),
-    cut = cms.string("!isFake && abs(z) <= 25 && position.Rho <= 2"), 
-    filter = cms.bool(True),   # otherwise it won't filter the events
-    )
+                                         src = cms.InputTag("offlinePrimaryVertices"),
+                                         cut = cms.string("!isFake && abs(z) <= 25 && position.Rho <= 2"), 
+                                         filter = cms.bool(True),   # otherwise it won't filter the events
+                                         )
 
 # single lepton selector
-goodElectrons = cms.EDFilter("GsfElectronRefSelector",
-                                src = cms.InputTag("gedGsfElectrons"),
-                                cut = cms.string(ELECTRON_CUT)
-)
+goodElectronsForZEE = cms.EDFilter("GsfElectronRefSelector",
+                                   src = cms.InputTag("gedGsfElectrons"),
+                                   cut = cms.string("pt > 25 && abs(eta)<1.44")
+                                   )
 
-# dilepton selectors
-diElectrons = cms.EDProducer("CandViewShallowCloneCombiner",
-                             decay       = cms.string("goodElectrons goodElectrons"),
-                             checkCharge = cms.bool(False),
-                             cut         = cms.string(DIELECTRON_CUT)
-)
+## dilepton selectors
+diElectronsForZEE = cms.EDProducer("CandViewShallowCloneCombiner",
+                                   decay       = cms.string("goodElectronsForZEE goodElectronsForZEE"),
+                                   checkCharge = cms.bool(False),
+                                   cut         = cms.string("mass > 80 && mass < 110")
+                                   )
 
 # dilepton counter
-diElectronsFilter = cms.EDFilter("CandViewCountFilter",
-                                    src = cms.InputTag("diElectrons"),
-                                    minNumber = cms.uint32(1)
-)
+diElectronsFilterForZEE = cms.EDFilter("CandViewCountFilter",
+                                       src = cms.InputTag("diElectronsForZEE"),
+                                       minNumber = cms.uint32(1)
+                                       )
 
 # Z->ee skim sequence
 zEESkimSequence = cms.Sequence(
     hltZEEHI *
     primaryVertexFilterForZEE *
-    goodElectrons * 
-    diElectrons * 
-    diElectronsFilter
+    goodElectronsForZEE * 
+    diElectronsForZEE * 
+    diElectronsFilterForZEE
 )
