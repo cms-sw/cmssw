0adcbe76102910ebdff5ceee1051c5726d7c6e9b
lgray <lindsey.gray@gmail.com>
2016-10-24 14:59:31 -0500

    make workspaces in HGCDigitizer only depend on signal size

--- SimCalorimetry/HGCalSimProducers/interface/HGCHEbackDigitizer.h
+++ SimCalorimetry/HGCalSimProducers/interface/HGCHEbackDigitizer.h
@@ -9,7 +9,9 @@ class HGCHEbackDigitizer : public HGCDigitizerBase<HGCBHDataFrame>
  public:
 
   HGCHEbackDigitizer(const edm::ParameterSet& ps);
-  void runDigitizer(std::unique_ptr<HGCBHDigiCollection> &digiColl,hgc::HGCSimHitDataAccumulator &simData,uint32_t digitizationType, CLHEP::HepRandomEngine* engine);
+  void runDigitizer(std::unique_ptr<HGCBHDigiCollection> &digiColl,hgc::HGCSimHitDataAccumulator &simData,
+		    const CaloSubdetectorGeometry* theGeom, const std::unordered_set<DetId>& validIds,
+		    uint32_t digitizationType, CLHEP::HepRandomEngine* engine) override;
   ~HGCHEbackDigitizer();
 
  private:
@@ -17,7 +19,9 @@ class HGCHEbackDigitizer : public HGCDigitizerBase<HGCBHDataFrame>
   //calice-like digitization parameters
   float keV2MIP_, noise_MIP_;
   float nPEperMIP_, nTotalPE_, xTalk_, sdPixels_;
-  void runCaliceLikeDigitizer(std::unique_ptr<HGCBHDigiCollection> &digiColl,hgc::HGCSimHitDataAccumulator &simData, CLHEP::HepRandomEngine* engine);
+  void runCaliceLikeDigitizer(std::unique_ptr<HGCBHDigiCollection> &digiColl,hgc::HGCSimHitDataAccumulator &simData, 
+			      const CaloSubdetectorGeometry* theGeom, const std::unordered_set<DetId>& validIds,
+			      CLHEP::HepRandomEngine* engine);
 };
 
 #endif 
--- SimCalorimetry/HGCalSimProducers/src/HGCEEDigitizer.cc
+++ SimCalorimetry/HGCalSimProducers/src/HGCEEDigitizer.cc
@@ -6,7 +6,9 @@ using namespace hgc_digi;
 HGCEEDigitizer::HGCEEDigitizer(const edm::ParameterSet& ps) : HGCDigitizerBase(ps) { }
 
 //
-void HGCEEDigitizer::runDigitizer(std::unique_ptr<HGCEEDigiCollection> &digiColl,HGCSimHitDataAccumulator &simData,uint32_t digitizationType, CLHEP::HepRandomEngine* engine) {
+void HGCEEDigitizer::runDigitizer(std::unique_ptr<HGCEEDigiCollection> &digiColl,HGCSimHitDataAccumulator &simData,
+				  const CaloSubdetectorGeometry* theGeom, const std::unordered_set<DetId>& validIds,
+				  uint32_t digitizationType, CLHEP::HepRandomEngine* engine) {
 }
 
 //
