801b2c4283b26b9c800a2c6481bd5d0a6d75da37
Evangelos Paradas <evangelos.paradas@cern.ch>
2016-10-25 14:56:16 +0200

    Update BMTFUnpackerInputs.h

--- EventFilter/L1TRawToDigi/plugins/implementations_stage2/BMTFUnpackerInputs.h
+++ EventFilter/L1TRawToDigi/plugins/implementations_stage2/BMTFUnpackerInputs.h
@@ -1,28 +1,32 @@
+#include "FWCore/Framework/interface/MakerMacros.h"
 #include "EventFilter/L1TRawToDigi/interface/Unpacker.h"
 
+#include "BMTFCollections.h"
+
 namespace l1t{
-   namespace stage2{
-      struct qualityHits
-      {
-         int linkNo;
-         int hits[3][7];
-      };
+	namespace stage2{
+		struct qualityHits
+		{
+			int linkNo;
+			int hits[3][7];
+		};
+		class BMTFUnpackerInputsOldQual : public Unpacker
+		{
+			public:
+				virtual bool unpack(const Block& block, UnpackerCollections *coll) override;
+			private:
+				qualityHits linkAndQual_;
+				//std::map<int, qualityHits> linkAndQual_;
+		};
 
-      class BMTFUnpackerInputsOldQual : public Unpacker
-      {
-         public:
-            virtual bool unpack(const Block& block, UnpackerCollections *coll) override;
-         private:
-            std::map<int, qualityHits> linkAndQual_;
-      };
+		class BMTFUnpackerInputsNewQual : public Unpacker
+		{
+			public:
+				virtual bool unpack(const Block& block, UnpackerCollections *coll) override;
+			private:
+				qualityHits linkAndQual_;
+				//std::map<int, qualityHits> linkAndQual_;
+		};
 
-      class BMTFUnpackerInputsNewQual : public Unpacker
-      {
-         public:
-            virtual bool unpack(const Block& block, UnpackerCollections *coll) override;
-         private:
-            std::map<int, qualityHits> linkAndQual_;
-      };
-
-   }
+	}
 }
