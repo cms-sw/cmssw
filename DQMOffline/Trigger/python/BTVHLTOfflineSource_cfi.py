fb86faffec29fe4e75d19cf841d4a6dbb06f4119
Xavier COUBEZ <xavier.coubez@cern.ch>
2016-10-26 11:01:55 +0200

    Adding histograms to BTV DQM Offline

--- DQMOffline/Trigger/python/BTVHLTOfflineSource_cfi.py
+++ DQMOffline/Trigger/python/BTVHLTOfflineSource_cfi.py
@@ -7,14 +7,19 @@ BTVHLTOfflineSource = cms.EDAnalyzer(
     processname = cms.string("HLT"),  
     verbose = cms.untracked.bool(False),
     #
-    triggerSummaryLabel = cms.InputTag("hltTriggerSummaryAOD","","HLT"),
-    triggerResultsLabel = cms.InputTag("TriggerResults","","HLT"),
-    offlineCSVLabelPF = cms.InputTag("pfCombinedInclusiveSecondaryVertexV2BJetTags"),
+    triggerSummaryLabel    = cms.InputTag("hltTriggerSummaryAOD",                 "",    "HLT"),
+    triggerResultsLabel    = cms.InputTag("TriggerResults",                       "",    "HLT"),
+    triggerPathPF          = cms.string('HLT_QuadPFJet_BTagCSV_p016_VBF_Mqq460_v5'),
+    triggerFilterPFbfCSV   = cms.InputTag('hltBTagCaloCSVp022Single',             '',    'HLT'),
+    #triggerFilterPFbfCSV   = cms.InputTag('hltSelector6PFJets',                   '',    'HLT'),
+    triggerFilterPFafCSV   = cms.InputTag('hltBTagPFCSVp016SingleWithMatching',   '',    'HLT'),
+
+    offlineCSVLabelPF   = cms.InputTag("pfCombinedInclusiveSecondaryVertexV2BJetTags"),
     offlineCSVLabelCalo = cms.InputTag("pfCombinedInclusiveSecondaryVertexV2BJetTags"),
-    hltFastPVLabel = cms.InputTag("hltFastPrimaryVertex"),
-    hltPFPVLabel = cms.InputTag("hltVerticesPFSelector"),
-    hltCaloPVLabel = cms.InputTag("hltVerticesL3"),    
-    offlinePVLabel = cms.InputTag("offlinePrimaryVertices"),    
+    hltFastPVLabel      = cms.InputTag("hltFastPrimaryVertex"),
+    hltPFPVLabel        = cms.InputTag("hltVerticesPFSelector"),
+    hltCaloPVLabel      = cms.InputTag("hltVerticesL3"),    
+    offlinePVLabel      = cms.InputTag("offlinePrimaryVertices"),    
     
     #
     pathPairs = cms.VPSet(
