5a63b825d52ac0406d8831313a2ae16e81d5b917
kkotov <kkotov@cern.ch>
2016-10-27 16:45:25 +0200

    incrementing the tags

--- CondTools/L1TriggerExt/python/L1O2OTagsExt_cfi.py
+++ CondTools/L1TriggerExt/python/L1O2OTagsExt_cfi.py
@@ -5,14 +5,14 @@ def initL1O2OTagsExt():
 
     initL1O2OTagsExt.tagBaseVec = [None] * L1CondEnumExt.NumL1Cond
 
-    initL1O2OTagsExt.tagBaseVec[ L1CondEnumExt.L1TriggerKeyListExt ] = "Stage2v0_hlt"
-    initL1O2OTagsExt.tagBaseVec[ L1CondEnumExt.L1TriggerKeyExt ] = "Stage2v0_hlt"
+    initL1O2OTagsExt.tagBaseVec[ L1CondEnumExt.L1TriggerKeyListExt ] = "Stage2v1_hlt"
+    initL1O2OTagsExt.tagBaseVec[ L1CondEnumExt.L1TriggerKeyExt ] = "Stage2v1_hlt"
 
     initL1O2OTagsExt.tagBaseVec[ L1CondEnumExt.L1TUtmTriggerMenu ] = "Stage2v0_hlt"
     initL1O2OTagsExt.tagBaseVec[ L1CondEnumExt.L1TGlobalPrescalesVetos ] = "Stage2v0_hlt"
     initL1O2OTagsExt.tagBaseVec[ L1CondEnumExt.L1TMuonBarrelParams ] = "Stage2v0_hlt"
-    initL1O2OTagsExt.tagBaseVec[ L1CondEnumExt.L1TMuonOverlapParams ] = "Stage2v0_hlt"
-    initL1O2OTagsExt.tagBaseVec[ L1CondEnumExt.L1TMuonEndcapParams ] = "Stage2v0_hlt"
+    initL1O2OTagsExt.tagBaseVec[ L1CondEnumExt.L1TMuonOverlapParams ] = "Stage2v1_hlt"
+    initL1O2OTagsExt.tagBaseVec[ L1CondEnumExt.L1TMuonEndcapParams ] = "Stage2v1_hlt"
     initL1O2OTagsExt.tagBaseVec[ L1CondEnumExt.L1TMuonGlobalParams ] = "Stage2v0_hlt"
     initL1O2OTagsExt.tagBaseVec[ L1CondEnumExt.L1TCaloParams ] = "Stage2v0_hlt"
     
