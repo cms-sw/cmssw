eb2d1c9c11edbeb0059138625964ad079c55b305
lgray <lindsey.gray@gmail.com>
2016-10-29 11:16:23 -0500

    change D3 timing matrix test to D5

--- Configuration/PyReleaseValidation/scripts/runTheMatrix.py
+++ Configuration/PyReleaseValidation/scripts/runTheMatrix.py
@@ -60,7 +60,7 @@ if __name__ == '__main__':
                      10021.0, #2017 tenmu
                      10024.0, #2017 ttbar
                      20024.0, #2023D1 ttbar (Run2 calo)
-                     22424.0, #2023D3Timing ttbar (HGCal + timing)
+                     23224.0, #2023D5 ttbar (HGCal + timing)
                      ],
         'jetmc': [5.1, 13, 15, 25, 38, 39], #MC
         'metmc' : [5.1, 15, 25, 37, 38, 39], #MC
