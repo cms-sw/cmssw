3964d78cb0518ad7bdff9d45450f7800f9aa79be
matthieu <matthieu.marionneau@cern.ch>
2016-10-31 17:10:42 +0100

    enable update of MET sgnificance when correcting the MET
    Conflicts:
    	PhysicsTools/PatUtils/plugins/CorrectedPATMETProducer.cc
    	RecoMET/METAlgorithms/src/METSignificance.cc

--- RecoMET/METAlgorithms/src/METSignificance.cc
+++ RecoMET/METAlgorithms/src/METSignificance.cc
@@ -20,10 +20,11 @@ Implementation:
 
 metsig::METSignificance::METSignificance(const edm::ParameterSet& iConfig) {
 
-  edm::ParameterSet cfgParams = iConfig.getParameter<edm::ParameterSet>("parameters");
+  // if(iConfig.exists("parameters")) {
+    edm::ParameterSet cfgParams = iConfig.getParameter<edm::ParameterSet>("parameters");
 
-  double dRmatch = cfgParams.getParameter<double>("dRMatch");
-  dR2match_ = dRmatch*dRmatch;
+    double dRmatch = cfgParams.getParameter<double>("dRMatch");
+    dR2match_ = dRmatch*dRmatch;
   
   jetThreshold_ = cfgParams.getParameter<double>("jetThreshold");
   jetEtas_ = cfgParams.getParameter<std::vector<double> >("jeta");
