812d57e5be32d7e392b0d9324e43ac61127df8ec
Jan Kašpar <jan.kaspar@cern.ch>
2016-11-01 18:15:10 +0100

    Run-dependent mapping configuration.

--- EventFilter/CTPPSRawToDigi/python/ctppsRawToDigi_cff.py
+++ EventFilter/CTPPSRawToDigi/python/ctppsRawToDigi_cff.py
@@ -8,23 +8,55 @@ totemTriggerRawToDigi.rawDataTag = cms.InputTag("rawDataCollector")
 totemDAQMappingESSourceXML_TrackingStrip = cms.ESSource("TotemDAQMappingESSourceXML",
   verbosity = cms.untracked.uint32(0),
   subSystem = cms.untracked.string("TrackingStrip"),
-  mappingFileNames = cms.untracked.vstring("CondFormats/CTPPSReadoutObjects/xml/ctpps_210_mapping.xml"),
-  maskFileNames = cms.untracked.vstring()
+  configuration = cms.VPSet(
+    # before TS2 (2016)
+    cms.PSet(
+      validityRange = cms.EventRange("1:min - 280385:max"),
+      mappingFileNames = cms.vstring("CondFormats/CTPPSReadoutObjects/xml/mapping_tracking_strip_to_fill_5288.xml"),
+      maskFileNames = cms.vstring()
+    ),
+    # during TS2 (2016)
+    cms.PSet(
+      validityRange = cms.EventRange("280386:min - 281600:max"),
+      mappingFileNames = cms.vstring(),
+      maskFileNames = cms.vstring()
+    ),
+    # after TS2 (2016)
+    cms.PSet(
+      validityRange = cms.EventRange("281601:min - 999999999:max"),
+      mappingFileNames = cms.vstring("CondFormats/CTPPSReadoutObjects/xml/mapping_tracking_strip_from_fill_5330.xml"),
+      maskFileNames = cms.vstring()
+    )
+  )
 )
 
 from EventFilter.CTPPSRawToDigi.totemRPRawToDigi_cfi import *
 totemRPRawToDigi.rawDataTag = cms.InputTag("rawDataCollector")
 
+
 # diamonds
 totemDAQMappingESSourceXML_TimingDiamond = cms.ESSource("TotemDAQMappingESSourceXML",
   verbosity = cms.untracked.uint32(0),
   subSystem = cms.untracked.string("TimingDiamond"),
-  mappingFileNames = cms.untracked.vstring("CondFormats/CTPPSReadoutObjects/xml/ctpps_timing_diamond_215_mapping.xml"),
-  maskFileNames = cms.untracked.vstring()
+  configuration = cms.VPSet(
+    # before diamonds inserted in DAQ
+    cms.PSet(
+      validityRange = cms.EventRange("1:min - 283819:max"),
+      mappingFileNames = cms.vstring(),
+      maskFileNames = cms.vstring()
+    ),
+    # after diamonds inserted in DAQ
+    cms.PSet(
+      validityRange = cms.EventRange("283820:min - 999999999:max"),
+      mappingFileNames = cms.vstring("CondFormats/CTPPSReadoutObjects/xml/mapping_timing_diamond.xml"),
+      maskFileNames = cms.vstring()
+    )
+  )
 )
 
 from EventFilter.CTPPSRawToDigi.ctppsDiamondRawToDigi_cfi import *
 ctppsDiamondRawToDigi.rawDataTag = cms.InputTag("rawDataCollector")
+
 
 # raw-to-digi sequence
 ctppsRawToDigi = cms.Sequence(
