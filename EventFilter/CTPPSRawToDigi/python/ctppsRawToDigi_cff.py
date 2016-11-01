7514789a720a13f3d2435fc06a10aba7eacfcb70
Jan Kašpar <jan.kaspar@cern.ch>
2016-11-01 16:28:05 +0100

    Separate mapping sources for strips and diamonds.
    Conflicts:
    	EventFilter/CTPPSRawToDigi/python/ctppsRawToDigi_cff.py

--- EventFilter/CTPPSRawToDigi/python/ctppsRawToDigi_cff.py
+++ EventFilter/CTPPSRawToDigi/python/ctppsRawToDigi_cff.py
@@ -27,10 +27,24 @@ from EventFilter.CTPPSRawToDigi.totemTriggerRawToDigi_cfi import *
 totemTriggerRawToDigi.rawDataTag = cms.InputTag("rawDataCollector")
 
 # Si strips
+totemDAQMappingESSourceXML_TrackingStrip = cms.ESSource("TotemDAQMappingESSourceXML",
+  verbosity = cms.untracked.uint32(0),
+  subSystem = cms.untracked.string("TrackingStrip"),
+  mappingFileNames = cms.untracked.vstring("CondFormats/CTPPSReadoutObjects/xml/ctpps_210_mapping.xml"),
+  maskFileNames = cms.untracked.vstring()
+)
+
 from EventFilter.CTPPSRawToDigi.totemRPRawToDigi_cfi import *
 totemRPRawToDigi.rawDataTag = cms.InputTag("rawDataCollector")
 
 # diamonds
+totemDAQMappingESSourceXML_TimingDiamond = cms.ESSource("TotemDAQMappingESSourceXML",
+  verbosity = cms.untracked.uint32(0),
+  subSystem = cms.untracked.string("TimingDiamond"),
+  mappingFileNames = cms.untracked.vstring("CondFormats/CTPPSReadoutObjects/xml/ctpps_timing_diamond_215_mapping.xml"),
+  maskFileNames = cms.untracked.vstring()
+)
+
 from EventFilter.CTPPSRawToDigi.ctppsDiamondRawToDigi_cfi import *
 ctppsDiamondRawToDigi.rawDataTag = cms.InputTag("rawDataCollector")
 
