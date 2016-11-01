9664052bb0c9ee1520ad78254c2aa928921b25e1
Jan Kašpar <jan.kaspar@cern.ch>
2016-11-01 16:31:37 +0100

    Consistent sub-system names (as in CTPPSDetId).

--- EventFilter/CTPPSRawToDigi/python/ctppsDiamondRawToDigi_cfi.py
+++ EventFilter/CTPPSRawToDigi/python/ctppsDiamondRawToDigi_cfi.py
@@ -3,4 +3,4 @@ import FWCore.ParameterSet.Config as cms
 from EventFilter.CTPPSRawToDigi.totemVFATRawToDigi_cfi import totemVFATRawToDigi
 
 ctppsDiamondRawToDigi = totemVFATRawToDigi.copy()
-ctppsDiamondRawToDigi.subSystem = "diamond"
+ctppsDiamondRawToDigi.subSystem = "TimingDiamond"
