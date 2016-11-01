69da2899183b0c5c3640c308fdfd27a7292cad8b
Philipp <p.pigard@gmail.com>
2016-11-01 21:27:34 +0100

    Remove preliminary HZZ setup

--- RecoEgamma/ElectronIdentification/plugins/ElectronMVAEstimatorRun2Spring16_Reg.cc
+++
@@ -1,5 +0,0 @@
-#include "RecoEgamma/ElectronIdentification/interface/ElectronMVAEstimatorRun2Spring16.h"
-
-DEFINE_EDM_PLUGIN(AnyMVAEstimatorRun2Factory,
-		  ElectronMVAEstimatorRun2Spring16,
-		  "ElectronMVAEstimatorRun2Spring16");
