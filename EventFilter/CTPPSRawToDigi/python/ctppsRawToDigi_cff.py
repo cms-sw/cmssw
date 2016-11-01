47333df429d761b8a5365facfacee32488ba0b46
Jan Kašpar <jan.kaspar@cern.ch>
2016-11-01 16:28:05 +0100

    Separate mapping sources for strips and diamonds.

--- EventFilter/CTPPSRawToDigi/python/ctppsRawToDigi_cff.py
+++ EventFilter/CTPPSRawToDigi/python/ctppsRawToDigi_cff.py
@@ -1,18 +1,28 @@
 import FWCore.ParameterSet.Config as cms
 
-from CondFormats.CTPPSReadoutObjects.TotemDAQMappingESSourceXML_cfi import *
-TotemDAQMappingESSourceXML.mappingFileNames.append("CondFormats/CTPPSReadoutObjects/xml/ctpps_210_mapping.xml")
-TotemDAQMappingESSourceXML.mappingFileNames.append("CondFormats/CTPPSReadoutObjects/xml/ctpps_timing_diamond_215_mapping.xml")
-
-# raw trigger data
+# trigger data
 from EventFilter.CTPPSRawToDigi.totemTriggerRawToDigi_cfi import *
 totemTriggerRawToDigi.rawDataTag = cms.InputTag("rawDataCollector")
 
 # Si strips
+totemDAQMappingESSourceXML_TrackingStrip = cms.ESSource("TotemDAQMappingESSourceXML",
+  verbosity = cms.untracked.uint32(0),
+  subSystem = cms.untracked.string("TrackingStrip"),
+  mappingFileNames = cms.untracked.vstring("CondFormats/CTPPSReadoutObjects/xml/ctpps_210_mapping.xml"),
+  maskFileNames = cms.untracked.vstring()
+)
+
 from EventFilter.CTPPSRawToDigi.totemRPRawToDigi_cfi import *
 totemRPRawToDigi.rawDataTag = cms.InputTag("rawDataCollector")
 
 # diamonds
+totemDAQMappingESSourceXML_TimingDiamond = cms.ESSource("TotemDAQMappingESSourceXML",
+  verbosity = cms.untracked.uint32(0),
+  subSystem = cms.untracked.string("TimingDiamond"),
+  mappingFileNames = cms.untracked.vstring("CondFormats/CTPPSReadoutObjects/xml/ctpps_timing_diamond_215_mapping.xml"),
+  maskFileNames = cms.untracked.vstring()
+)
+
 from EventFilter.CTPPSRawToDigi.ctppsDiamondRawToDigi_cfi import *
 ctppsDiamondRawToDigi.rawDataTag = cms.InputTag("rawDataCollector")
 
