2f122bc8f098028f6d5676165cf45769ee7013f9
Jan Kašpar <jan.kaspar@cern.ch>
2016-11-02 11:39:15 +0100

    Fixed config to print the DAQ mapping.

--- CondFormats/CTPPSReadoutObjects/python/TotemDAQMappingESSourceXML_cfi.py
+++ CondFormats/CTPPSReadoutObjects/python/TotemDAQMappingESSourceXML_cfi.py
@@ -3,6 +3,8 @@ import FWCore.ParameterSet.Config as cms
 totemDAQMappingESSourceXML = cms.ESSource("TotemDAQMappingESSourceXML",
   verbosity = cms.untracked.uint32(0),
 
+  subSystem = cms.untracked.string(""),
+
   configuration = cms.VPSet(
     # example configuration block:
     #cms.PSet(
