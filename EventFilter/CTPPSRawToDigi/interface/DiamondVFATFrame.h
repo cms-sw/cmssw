d1d4a2fa74c9823c61f5ac24d8523c6086d9c328
nminafra <nicola@cern.ch>
2016-11-02 16:46:59 +0100

    test commit

--- EventFilter/CTPPSRawToDigi/interface/DiamondVFATFrame.h
+++ EventFilter/CTPPSRawToDigi/interface/DiamondVFATFrame.h
@@ -25,7 +25,7 @@ class DiamondVFATFrame : public VFATFrame
     {}
     virtual ~DiamondVFATFrame() {}
 
-    /// get time infromation
+    /// get timing infromation
     uint32_t getLeadingEdgeTime() const
     {
       uint32_t time = ((data[5]&0x1f)<<16)+data[6];
