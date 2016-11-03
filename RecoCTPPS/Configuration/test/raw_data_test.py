f9896b96c55069571c4d7ccb16ab99120ea0215b
Jan Kašpar <jan.kaspar@cern.ch>
2016-11-03 17:16:27 +0100

    Defined EventContent for diamonds.

--- RecoCTPPS/Configuration/test/raw_data_test.py
+++ RecoCTPPS/Configuration/test/raw_data_test.py
@@ -4,20 +4,27 @@ process = cms.Process("TotemStandaloneRawDataTest")
 
 # minimum of logs
 process.MessageLogger = cms.Service("MessageLogger",
-    statistics = cms.untracked.vstring(),
-    destinations = cms.untracked.vstring('cout'),
-    cout = cms.untracked.PSet(
-        threshold = cms.untracked.string('WARNING')
-    )
+  statistics = cms.untracked.vstring(),
+  destinations = cms.untracked.vstring('cout'),
+  cout = cms.untracked.PSet(
+    threshold = cms.untracked.string('WARNING')
+  )
 )
 
 # raw data source
 process.source = cms.Source("PoolSource",
-    fileNames = cms.untracked.vstring('file:/afs/cern.ch/user/j/jkaspar/public/run273062_ls0001-2_stream.root')
+  fileNames = cms.untracked.vstring(
+    'file:/afs/cern.ch/user/j/jkaspar/public/run273062_ls0001-2_stream.root',
+    '/store/express/Run2016H/ExpressPhysics/FEVT/Express-v2/000/283/877/00000/4EE44B0E-2499-E611-A155-02163E011938.root'
+  ),
+  inputCommands = cms.untracked.vstring(
+    'drop *',
+    'keep FEDRawDataCollection_*_*_*'
+  )
 )
 
 process.maxEvents = cms.untracked.PSet(
-    input = cms.untracked.int32(-1)
+  input = cms.untracked.int32(8000)
 )
 
 # raw-to-digi conversion
@@ -27,15 +34,15 @@ process.load("EventFilter.CTPPSRawToDigi.ctppsRawToDigi_cff")
 process.load("RecoCTPPS.Configuration.recoCTPPS_cff")
 
 process.p = cms.Path(
-    process.ctppsRawToDigi *
-    process.recoCTPPS
+  process.ctppsRawToDigi *
+  process.recoCTPPS
 )
 
 # output configuration
 from RecoCTPPS.Configuration.RecoCTPPS_EventContent_cff import RecoCTPPSAOD
 process.output = cms.OutputModule("PoolOutputModule",
-    fileName = cms.untracked.string("file:./AOD.root"),
-    outputCommands = RecoCTPPSAOD.outputCommands
+  fileName = cms.untracked.string("file:./AOD.root"),
+  outputCommands = RecoCTPPSAOD.outputCommands
 )
 
 process.outpath = cms.EndPath(process.output)
