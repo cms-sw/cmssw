eaf142d07742342d27f603f7663e47b69cf903bb
Silvio Donato <silvio.donato@cern.ch>
2016-11-05 17:54:34 +0100

    remove DQMHistograms from PIon (keep it only in online menu)

--- HLTrigger/Configuration/python/HLT_PIon_cff.py
+++ HLTrigger/Configuration/python/HLT_PIon_cff.py
@@ -1,11 +1,11 @@
-# /dev/CMSSW_8_0_0/PIon/V247 (CMSSW_8_0_19_patch2)
+# /dev/CMSSW_8_0_0/PIon/V249 (CMSSW_8_0_19_patch2)
 
 import FWCore.ParameterSet.Config as cms
 
 fragment = cms.ProcessFragment( "HLT" )
 
 fragment.HLTConfigVersion = cms.PSet(
-  tableName = cms.string('/dev/CMSSW_8_0_0/PIon/V247')
+  tableName = cms.string('/dev/CMSSW_8_0_0/PIon/V249')
 )
 
 fragment.HLTPSetJetCoreStepTrajectoryFilter = cms.PSet( 
