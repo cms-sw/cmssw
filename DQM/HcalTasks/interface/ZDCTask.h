8cd0b3706c853170935c40c702d3033138e6abcc
federico de guio <federico.de.guio@cern.ch>
2016-11-07 22:55:52 +0100

    make ZDCTask compatible with hardcoded emap from the unpacker

--- DQM/HcalTasks/interface/ZDCTask.h
+++ DQM/HcalTasks/interface/ZDCTask.h
@@ -19,7 +19,7 @@
 #include "DQM/HcalCommon/interface/HashFilter.h"
 #include "DQM/HcalCommon/interface/ElectronicsMap.h"
 
-class ZDCTask : public hcaldqm::DQTask
+class ZDCTask : public DQMEDAnalyzer
 {
 	public:
 		ZDCTask(edm::ParameterSet const&);
@@ -27,12 +27,9 @@ class ZDCTask : public hcaldqm::DQTask
 
 		virtual void bookHistograms(DQMStore::IBooker&,
 			edm::Run const&, edm::EventSetup const&);
-		virtual void endLuminosityBlock(edm::LuminosityBlock const&,
-			edm::EventSetup const&);
 
 	protected:
-		virtual void _process(edm::Event const&, edm::EventSetup const&);
-		virtual void _resetMonitors(hcaldqm::UpdateFreq);
+		virtual void analyze(edm::Event const&, edm::EventSetup const&);
 
 		//	tags
 		edm::InputTag	_tagQIE10;
@@ -42,30 +39,15 @@ class ZDCTask : public hcaldqm::DQTask
 		double _cut;
 		int _ped;
 
-		//	filters
-		hcaldqm::filter::HashFilter _filter_C36;
-		hcaldqm::filter::HashFilter _filter_DA;
-
-		//	Electronics Maps/Hashes
-		HcalElectronicsMap const* _emap;
-		hcaldqm::electronicsmap::ElectronicsMap _ehashmap;
 		
 		//	hcaldqm::Containers
-		hcaldqm::ContainerProf1D	_cShapeCut_EChannel;
-		hcaldqm::ContainerProf1D	_cShape_EChannel;
-		hcaldqm::Container2D	_cLETDCvsADC_EChannel[10];
-		hcaldqm::Container2D	_cTETDCvsADC_EChannel[10];
-		hcaldqm::Container1D _cLETDC_EChannel[10];
-		hcaldqm::Container1D _cADC_EChannel[10];
+		std::map<std::string,MonitorElement*>   _cShape_EChannel;
+		std::map<std::string,MonitorElement*>   _cADC_EChannel;
 
 
 		//	hcaldqm::Containers overall
-		hcaldqm::ContainerSingleProf1D	_cShapeCut;
-		hcaldqm::ContainerSingleProf1D	_cShape;
-		hcaldqm::ContainerSingle2D		_cLETDCvsADC;
-		hcaldqm::ContainerSingle2D		_cTETDCvsADC;
-		hcaldqm::ContainerSingle1D		_cLETDC;
-		hcaldqm::ContainerSingle1D		_cADC;
+		MonitorElement*   _cShape;
+		MonitorElement*   _cADC;
 };
 
 #endif
