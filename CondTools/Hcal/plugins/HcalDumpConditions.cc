32aa8169232d5b41829dc4b2cbdcc96bee378a80
Kevin Pedro <kpedro88@gmail.com>
2016-11-07 07:05:43 -0600

    add option to dump calibrations

--- CondTools/Hcal/plugins/HcalDumpConditions.cc
+++ CondTools/Hcal/plugins/HcalDumpConditions.cc
@@ -109,6 +109,9 @@ namespace edmtest
     context.get<HcalRecNumberingRecord>().get( topology );
     const HcalTopology* topo=&(*topology);
 
+    edm::ESHandle<HcalDbService> pSetup;
+    context.get<HcalDbRecord>().get( pSetup );
+
     using namespace edm::eventsetup;
     std::cout <<"HcalDumpConditions::analyze-> I AM IN RUN NUMBER "<<e.id().run() <<std::endl;
 
@@ -171,6 +174,15 @@ namespace edmtest
       dumpIt(new HcalTPChannelParameters(&(*topology)), new HcalTPChannelParametersRcd, e,context,"TPChannelParameters", topo);
     if (std::find (mDumpRequest.begin(), mDumpRequest.end(), std::string ("TPParameters")) != mDumpRequest.end())
       dumpIt(new HcalTPParameters, new HcalTPParametersRcd, e,context,"TPParameters");
+    if (std::find (mDumpRequest.begin(), mDumpRequest.end(), std::string ("CalibrationsSet")) != mDumpRequest.end()){
+      const HcalCalibrationsSet* tmp = pSetup->getHcalCalibrationsSet();
+      writeToFile(tmp,e,"CalibrationsSet");
+    }
+    if (std::find (mDumpRequest.begin(), mDumpRequest.end(), std::string ("CalibrationWidthsSet")) != mDumpRequest.end()){
+      const HcalCalibrationWidthsSet* tmp = pSetup->getHcalCalibrationWidthsSet();
+      writeToFile(tmp,e,"CalibrationWidthsSet");
+    }
+
   }
   DEFINE_FWK_MODULE(HcalDumpConditions);
 }
