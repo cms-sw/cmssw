1bca3d4dc2826cbdbf1b43b1980786595a32e808
David <David.Lange@cern.ch>
2016-11-07 17:51:56 +0100

    and other simple tests for pandas,rootpy,root_numpy

--- PhysicsTools/PythonAnalysis/test/runtests.sh
+++ PhysicsTools/PythonAnalysis/test/runtests.sh
@@ -3,3 +3,7 @@
 function die { echo $1: status $2 ;  exit $2; }
 
 python ${LOCAL_TEST_DIR}/testHistogrammar.py || die 'Failure using testHistogrammar' $?
+python ${LOCAL_TEST_DIR}/testPandas.py || die 'Failure using testPandas' $?
+python ${LOCAL_TEST_DIR}/testRootNumpy.py || die 'Failure using testRootNumpy' $?
+python ${LOCAL_TEST_DIR}/testRootpy.py || die 'Failure using testRootpy' $?
+
