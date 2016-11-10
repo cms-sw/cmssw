d6a76e9d955cfa2a3a36f0081280c688fc9ebfad
Markus Seidel <markus.seidel@cern.ch>
2016-11-10 16:06:38 +0100

    Test config

---
+++ GeneratorInterface/Pythia8Interface/test/pythia8ex12_cfg.py
@@ -0,0 +1,75 @@
+import FWCore.ParameterSet.Config as cms
+
+process = cms.Process("PROD")
+
+process.load("Configuration.StandardSequences.SimulationRandomNumberGeneratorSeeds_cff")
+
+#process.source = cms.Source("EmptySource")
+process.source = cms.Source("LHESource",
+    fileNames = cms.untracked.vstring('file:powheg-b_bbar_4l.lhe')
+)
+#    fileNames = cms.untracked.vstring('file:powheg-Zee.lhe')
+
+process.generator = cms.EDFilter("Pythia8HadronizerFilter",
+    maxEventsToPrint = cms.untracked.int32(1),
+    pythiaPylistVerbosity = cms.untracked.int32(1),
+    filterEfficiency = cms.untracked.double(1.0),
+    pythiaHepMCVerbosity = cms.untracked.bool(False),
+    comEnergy = cms.double(13000.),
+
+   #Turning on pythia8 emission veto:
+#    PythiaParameters = cms.PSet(
+#        pythia8_example07 = cms.vstring('POWHEG:nFinal = 2',
+#                                        'POWHEG:veto = 1',
+#                                        'POWHEG:vetoCount = 10000',
+#                                        'POWHEG:pThard = 1',
+#                                        'POWHEG:pTemt = 0',
+#                                        'POWHEG:emitted = 0',
+#                                        'POWHEG:pTdef = 1',
+#                                        'POWHEG:MPIveto = 0',
+#                                        'SpaceShower:pTmaxMatch = 2',
+#                                        'TimeShower:pTmaxMatch  = 2'),
+#        parameterSets = cms.vstring('pythia8_example07')
+#    )
+
+   #Turning on CMSSW Pythia8Interface emission veto:
+    PowhegRes = cms.untracked.PSet(),
+
+    PythiaParameters = cms.PSet(
+        pythia8_example12 = cms.vstring('SpaceShower:pTmaxMatch = 2',
+                                        'TimeShower:pTmaxMatch  = 2'),
+        parameterSets = cms.vstring('pythia8_example12')
+    )
+
+)
+
+#if emissionVeto is on && MPIveto > 0 add 'MultipartonInteractions:pTmaxMatch = 2'
+
+process.load("FWCore.MessageLogger.MessageLogger_cfi")
+process.MessageLogger = cms.Service("MessageLogger",
+    cout = cms.untracked.PSet(
+        default = cms.untracked.PSet(
+            limit = cms.untracked.int32(2)
+        )
+    ),
+    destinations = cms.untracked.vstring('cout')
+)
+
+process.RandomNumberGeneratorService = cms.Service("RandomNumberGeneratorService",
+    generator = cms.PSet(
+        initialSeed = cms.untracked.uint32(123456789),
+    )
+)
+
+process.maxEvents = cms.untracked.PSet(
+    input = cms.untracked.int32(100)
+)
+
+process.GEN = cms.OutputModule("PoolOutputModule",
+    fileName = cms.untracked.string('pythia8ex12.root')
+)
+
+process.p = cms.Path(process.generator)
+process.outpath = cms.EndPath(process.GEN)
+
+process.schedule = cms.Schedule(process.p, process.outpath)
