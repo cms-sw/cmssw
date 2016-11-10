fdaea3f89cb8c70b330fd0c3000e2d71abf85960
Colin <colin.bernet@cern.ch>
2016-11-10 12:32:17 +0100

    tools for context determination

---
+++ PhysicsTools/HeppyCore/python/framework/context.py
@@ -0,0 +1,26 @@
+import os 
+
+def get_name(env=None):
+    '''Returns the name of the context is which heppy is used.
+    
+    If several contexts are defined, throws a ValueError.
+
+    @return: 'cms', 'fcc' or None if no context is defined.
+    '''
+    if env is None: 
+        env = os.environ
+    contexts = dict( (key, False) for key in ['cms','fcc'])
+    reldir = env.get('CMSSW_BASE', None)
+    if reldir and os.path.isdir(reldir):
+        contexts['cms'] = True
+    fcc_envs = set(['PODIO', 'FCCPHYSICS', 'FCCEDM', 'HEPPY'])
+    if fcc_envs.issubset( env ): 
+        contexts['fcc'] = True
+    defined = [key for key,defined in contexts.iteritems() 
+               if defined is True]
+    if len(defined)>1: 
+        raise ValueError('several contexts defined: ' + str(defined) )
+    elif len(defined)==0:
+        return None
+    else: 
+        return defined.pop()
