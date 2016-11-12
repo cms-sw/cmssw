cd67fd0f0ddfd562f244ccdabb7ea71227bdac44
Ilya Kravchenko <ikrav@unl.edu>
2016-11-12 02:59:59 +0100

    Deleted automatic production of photon MVA IDs that are too old. Left are the most recent 50ns ID (V2p1), 25ns ID (V2p1), and the new 80X ID

--- RecoEgamma/PhotonIdentification/python/PhotonMVAValueMapProducer_cfi.py
+++ RecoEgamma/PhotonIdentification/python/PhotonMVAValueMapProducer_cfi.py
@@ -4,18 +4,6 @@ mvaConfigsForPhoProducer = cms.VPSet( )
 
 # Import and add all desired MVAs
 
-from RecoEgamma.PhotonIdentification.Identification.mvaPhotonID_PHYS14_PU20bx25_nonTrig_V1_cff import *
-mvaConfigsForPhoProducer.append( mvaPhoID_PHYS14_PU20bx25_nonTrig_V1_producer_config )
-
-from RecoEgamma.PhotonIdentification.Identification.mvaPhotonID_Spring15_50ns_nonTrig_V1_cff import *
-mvaConfigsForPhoProducer.append( mvaPhoID_Spring15_50ns_nonTrig_V1_producer_config )
-
-from RecoEgamma.PhotonIdentification.Identification.mvaPhotonID_Spring15_50ns_nonTrig_V2_cff import *
-mvaConfigsForPhoProducer.append( mvaPhoID_Spring15_50ns_nonTrig_V2_producer_config )
-
-from RecoEgamma.PhotonIdentification.Identification.mvaPhotonID_Spring15_25ns_nonTrig_V2_cff import *
-mvaConfigsForPhoProducer.append( mvaPhoID_Spring15_25ns_nonTrig_V2_producer_config )
-
 from RecoEgamma.PhotonIdentification.Identification.mvaPhotonID_Spring15_50ns_nonTrig_V2p1_cff import *
 mvaConfigsForPhoProducer.append( mvaPhoID_Spring15_50ns_nonTrig_V2p1_producer_config )
 
