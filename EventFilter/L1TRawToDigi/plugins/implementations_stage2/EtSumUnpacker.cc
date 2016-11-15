e983f792804ec1a863891a9d99df82cfbe441d13
Vladimir Rekovic <vladimir.rekovic@cern.ch>
2016-11-15 15:56:54 +0100

    Push the EtSum kTowerCount at the end of bx vector in the Unpacker, just as the emulator does. This facilitates unit-test comparisions between simulated EtSums and SIM+PACK+UNPACK EtSums, as the order is preserved.

--- EventFilter/L1TRawToDigi/plugins/implementations_stage2/EtSumUnpacker.cc
+++ EventFilter/L1TRawToDigi/plugins/implementations_stage2/EtSumUnpacker.cc
@@ -92,14 +92,6 @@ namespace stage2 {
        res_->push_back(bx,ht);
 
 
-       //HI-SUM
-
-       l1t::EtSum towCount = l1t::EtSum();
-       towCount.setHwPt( (raw_data>>12) & 0x1FFF );
-       towCount.setType( (l1t::EtSum::kTowerCount) );
-
-       res_->push_back(bx, towCount);
-
        //MBHFMT0
 
        l1t::EtSum mbm0 = l1t::EtSum();
@@ -186,6 +178,14 @@ namespace stage2 {
 
        res_->push_back(bx,mhthf);
        
+       //HI-SUM
+
+       l1t::EtSum towCount = l1t::EtSum();
+       towCount.setHwPt( (raw_data>>12) & 0x1FFF );
+       towCount.setType( (l1t::EtSum::kTowerCount) );
+
+       res_->push_back(bx, towCount);
+
        
      }
 
