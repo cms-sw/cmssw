1b78b53098ece0c1018b44cdf1aff324c3da1ebb
Sunanda <sunanda.banerjee@cern.ch>
2016-11-15 17:06:03 +0100

    Take out GE21 related material from the new scenario

--- Geometry/CMSCommonData/python/cmsExtendedGeometry2017MuonXML_cfi.py
+++ Geometry/CMSCommonData/python/cmsExtendedGeometry2017MuonXML_cfi.py
@@ -255,7 +255,7 @@ XMLIdealGeometryESSource = cms.ESSource("XMLIdealGeometryESSource",
         'Geometry/ForwardCommonData/data/zdc.xml',
         'Geometry/ForwardCommonData/data/zdclumi.xml',
         'Geometry/ForwardCommonData/data/cmszdc.xml')+cms.vstring(
-        'Geometry/MuonCommonData/data/v6/muonGemNumbering.xml',
+        'Geometry/MuonCommonData/data/v6/muonGemNumbering17.xml',
         'Geometry/TrackerCommonData/data/PhaseI/trackerStructureTopology.xml',
         'Geometry/TrackerSimData/data/PhaseI/trackersens.xml',
         'Geometry/TrackerRecoData/data/PhaseI/trackerRecoMaterial.xml',
