236e7ed777ef56da787d7ece6b12358edfc2f01c
Giovanni <gpetruc@gmail.com>
2016-11-15 15:08:03 +0100

    Move era changes from cfi to cff, and don't rely on indices

--- RecoParticleFlow/PFProducer/python/particleFlowBlock_cff.py
+++ RecoParticleFlow/PFProducer/python/particleFlowBlock_cff.py
@@ -9,37 +9,3 @@ from copy import deepcopy
 # include "RecoTracker/TrackProducer/data/CTFFinalFitWithMaterial.cff"
 from RecoParticleFlow.PFProducer.particleFlowBlock_cfi import *
 
-from Configuration.Eras.Modifier_phase2_hgcal_cff import phase2_hgcal
-# kill tracks in the HGCal
-phase2_hgcal.toModify(
-    particleFlowBlock,
-    elementImporters = { 5 : dict(
-        importerName = cms.string('GeneralTracksImporterWithVeto'),
-        veto = cms.InputTag('hgcalTrackCollection:TracksInHGCal')
-    ) }
-)
-### for later
-#_phase2_hgcal_Linkers.append( 
-#    cms.PSet( linkerName = cms.string("SCAndHGCalLinker"),
-#              linkType   = cms.string("SC:HGCAL"),
-#              useKDTree  = cms.bool(False),
-#              SuperClusterMatchByRef = cms.bool(True) ) 
-#)
-#_phase2_hgcal_Linkers.append(
-#    cms.PSet( linkerName = cms.string("HGCalAndBREMLinker"),
-#              linkType   = cms.string("HGCAL:BREM"),
-#              useKDTree  = cms.bool(False) )
-#)
-#_phase2_hgcal_Linkers.append(
-#    cms.PSet( linkerName = cms.string("GSFAndHGCalLinker"), 
-#                  linkType   = cms.string("GSF:HGCAL"),
-#                  useKDTree  = cms.bool(False) )
-#)
-
-
-from Configuration.Eras.Modifier_phase2_timing_cff import phase2_timing
-phase2_timing.toModify(
-    particleFlowBlock,
-    elementImporters = { 5  : dict( timeValueMap = cms.InputTag("trackTimeValueMapProducer:generalTracksConfigurableFlatResolutionModel"),
-                                timeErrorMap = cms.InputTag("trackTimeValueMapProducer:generalTracksConfigurableFlatResolutionModelResolution")) }
-)
