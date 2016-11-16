e2c3fa3631581fff512cabfa1080001b0a6a412b
kkotov <kkotov@cern.ch>
2016-11-16 15:47:50 +0100

    EndCap -> Endcap (this is one word!)

--- L1Trigger/L1TMuonEndCap/plugins/L1TMuonEndCapParamsESProducer.cc
+++ L1Trigger/L1TMuonEndCap/plugins/L1TMuonEndCapParamsESProducer.cc
@@ -8,7 +8,7 @@
 #include "FWCore/Framework/interface/ESProducts.h"
 
 #include "CondFormats/L1TObjects/interface/L1TMuonEndCapParams.h"
-#include "CondFormats/DataRecord/interface/L1TMuonEndCapParamsRcd.h"
+#include "CondFormats/DataRecord/interface/L1TMuonEndcapParamsRcd.h"
 #include "L1Trigger/L1TMuonEndCap/interface/EndCapParamsHelper.h"
 
 #include "FWCore/ParameterSet/interface/FileInPath.h"
@@ -27,7 +27,7 @@ public:
   
   typedef std::shared_ptr<L1TMuonEndCapParams> ReturnType;
 
-  ReturnType produce(const L1TMuonEndCapParamsRcd&);
+  ReturnType produce(const L1TMuonEndcapParamsRcd&);
 private:
   l1t::EndCapParamsHelper data_;
 };
@@ -61,7 +61,7 @@ L1TMuonEndCapParamsESProducer::~L1TMuonEndCapParamsESProducer()
 
 // ------------ method called to produce the data  ------------
 L1TMuonEndCapParamsESProducer::ReturnType
-L1TMuonEndCapParamsESProducer::produce(const L1TMuonEndCapParamsRcd& iRecord)
+L1TMuonEndCapParamsESProducer::produce(const L1TMuonEndcapParamsRcd& iRecord)
 {
    using namespace edm::es;
    std::shared_ptr<L1TMuonEndCapParams> pEMTFParams(data_.getWriteInstance());
