8815a41c0285819ace22c94c5aa81886027c1de7
Cesare Calabria <cesare.calabria@ba.infn.it>
2016-11-18 18:36:32 +0100

    muon1stStep fix

--- TrackingTools/TrackAssociator/python/DetIdAssociatorESProducer_cff.py
+++ TrackingTools/TrackAssociator/python/DetIdAssociatorESProducer_cff.py
@@ -50,7 +50,7 @@ from Configuration.Eras.Modifier_run3_GEM_cff import run3_GEM
 run3_GEM.toModify( muonDetIdAssociator, includeGEM = True )
 from Configuration.Eras.Modifier_phase2_muon_cff import phase2_muon
 phase2_muon.toModify( muonDetIdAssociator, includeME0 = True )
-phase2_muon.toModify( hcalDetIdAssociator, hcalRegion = 2 )
+phase2_muon.toModify( hcalDetIdAssociator, hcalRegion = 1 )
 
 preshowerDetIdAssociator = cms.ESProducer("DetIdAssociatorESProducer",
     ComponentName = cms.string('PreshowerDetIdAssociator'),
