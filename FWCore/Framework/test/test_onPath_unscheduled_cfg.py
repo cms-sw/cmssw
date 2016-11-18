be956e4542452d0ba326a45891816e5fcaafead1
W. David Dagenhart <wdd@fnal.gov>
2016-11-17 18:14:31 -0600

    Rename and fix allowUnscheduled unit tests

--- FWCore/Framework/test/test_onPath_unscheduled_cfg.py
+++ FWCore/Framework/test/test_onPath_unscheduled_cfg.py
@@ -4,19 +4,38 @@ process = cms.Process("TEST")
 
 import FWCore.Framework.test.cmsExceptionsFatalOption_cff
 process.options = cms.untracked.PSet(
-    Rethrow = FWCore.Framework.test.cmsExceptionsFatalOption_cff.Rethrow
+    Rethrow = FWCore.Framework.test.cmsExceptionsFatalOption_cff.Rethrow,
+    numberOfStreams = cms.untracked.uint32(1),
+    numberOfConcurrentRuns = cms.untracked.uint32(1),
+    numberOfConcurrentLuminosityBlocks = cms.untracked.uint32(1)
 )
 
 process.maxEvents = cms.untracked.PSet(
     input = cms.untracked.int32(3)
 )
-
 process.source = cms.Source("EmptySource",
     timeBetweenEvents = cms.untracked.uint64(10),
     firstTime = cms.untracked.uint64(1000000)
 )
 
 process.Tracer = cms.Service("Tracer")
+
+process.MessageLogger = cms.Service("MessageLogger",
+    destinations   = cms.untracked.vstring('cout',
+                                           'cerr'
+    ),
+    categories = cms.untracked.vstring(
+        'Tracer'
+    ),
+    cout = cms.untracked.PSet(
+        default = cms.untracked.PSet (
+            limit = cms.untracked.int32(0)
+        ),
+        Tracer = cms.untracked.PSet(
+            limit=cms.untracked.int32(100000000)
+        )
+    )
+)
 
 process.one = cms.EDProducer("IntProducer",
     ivalue = cms.int32(1)
@@ -36,4 +55,6 @@ process.getTwo = cms.EDAnalyzer("IntTestAnalyzer",
     moduleLabel = cms.untracked.string('two')
 )
 
-process.p = cms.Path(process.two*process.getOne+process.one*process.getTwo)
+process.p = cms.Path(process.one*process.getOne+process.two*process.getTwo)
+
+
