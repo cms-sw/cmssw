0228b287af6948bff3f72b13dae55381cfd0944f
David Lange <David.Lange@cern.ch>
2016-11-19 09:08:41 +0100

    Revert "bsunanda:Phase2-hgx67 Correct reconstruction geometry for EE and FH with full coverage"

--- Validation/HGCalValidation/test/runHGCHitAnalyzer_cfg.py
+++ Validation/HGCalValidation/test/runHGCHitAnalyzer_cfg.py
@@ -6,8 +6,7 @@ process.load('Configuration.StandardSequences.Services_cff')
 process.load('SimGeneral.HepPDTESSource.pythiapdt_cfi')    
 process.load('FWCore.MessageService.MessageLogger_cfi')
 process.load('Configuration.EventContent.EventContent_cff')
-#process.load('Configuration.Geometry.GeometryExtended2023D3Reco_cff')
-process.load('Geometry.HGCalCommonData.testHGCV8_cff')
+process.load('Configuration.Geometry.GeometryExtended2023D3Reco_cff')
 process.load('Configuration.StandardSequences.MagneticField_cff')
 process.load('Configuration.StandardSequences.FrontierConditions_GlobalTag_cff')
 
@@ -18,8 +17,8 @@ process.GlobalTag = GlobalTag(process.GlobalTag, 'auto:run2_mc', '')
 process.MessageLogger.cerr.FwkReport.reportEvery = 100
 process.source = cms.Source("PoolSource",
                             fileNames = cms.untracked.vstring(
-        'file:testHGCalSimWatcher.root',
-        #'/store/relval/CMSSW_8_1_0_pre8/RelValTTbar_14TeV/GEN-SIM-RECO/81X_mcRun2_asymptotic_v1_2023LReco-v1/10000/08E719D4-DE3D-E611-9092-003048FFD722.root',
+        #'file:testHGCalSimWatcher.root',
+        '/store/relval/CMSSW_8_1_0_pre8/RelValTTbar_14TeV/GEN-SIM-RECO/81X_mcRun2_asymptotic_v1_2023LReco-v1/10000/08E719D4-DE3D-E611-9092-003048FFD722.root',
         #'/store/relval/CMSSW_8_1_0_pre6/RelValSingleMuPt100/GEN-SIM-RECO/80X_mcRun2_asymptotic_v14_2023LReco-v1/00000/644ED025-4728-E611-A4BC-0025905A6068.root',
         #'/store/relval/CMSSW_8_1_0_pre4/RelValSingleMuPt100/GEN-SIM-RECO/80X_mcRun2_asymptotic_v13_2023tilted-v1/00000/1C09B4A5-0214-E611-98C8-0CC47A78A3F8.root'
         )
@@ -28,7 +27,7 @@ process.source = cms.Source("PoolSource",
 process.load('Validation.HGCalValidation.test.hgcHitValidation_cfi')
 
 process.TFileService = cms.Service("TFileService",
-                                   fileName = cms.string('hgcHitAnalyzer.root'),
+                                   fileName = cms.string('RelValTTbarNoPU.root'),
                                    closeFileFast = cms.untracked.bool(True)
                                    )
 
