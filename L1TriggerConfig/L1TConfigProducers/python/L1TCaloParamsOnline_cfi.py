fe82eca7535fc27f0fd7e18c596ec5eb5ff7ed48
kkotov <kkotov@cern.ch>
2016-11-19 07:58:53 +0100

    using caloStage2Params_2016_v3_3_HI_cfi as a default prototype in O2O

--- L1TriggerConfig/L1TConfigProducers/python/L1TCaloParamsOnline_cfi.py
+++ L1TriggerConfig/L1TConfigProducers/python/L1TCaloParamsOnline_cfi.py
@@ -1,7 +1,7 @@
 import FWCore.ParameterSet.Config as cms
 
 #from L1Trigger.L1TCalorimeter.caloStage2Params_cfi import *
-from L1Trigger.L1TCalorimeter.caloStage2Params_2016_v3_2_cfi import *
+from L1Trigger.L1TCalorimeter.caloStage2Params_2016_v3_3_HI_cfi import *
 
 L1TCaloParamsOnlineProd = cms.ESProducer("L1TCaloParamsOnlineProd",
     onlineAuthentication = cms.string('.'),
