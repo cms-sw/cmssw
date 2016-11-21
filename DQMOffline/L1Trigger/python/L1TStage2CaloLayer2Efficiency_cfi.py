ae0a0d33e79561e1f2a829d642b001c51424c87b
kreczko <lkreczko@googlemail.com>
2016-11-21 13:38:18 +0000

    added harvesting for l1t stage 2 calo layer 2

---
+++ DQMOffline/L1Trigger/python/L1TStage2CaloLayer2Efficiency_cfi.py
@@ -0,0 +1,48 @@
+import FWCore.ParameterSet.Config as cms
+from DQMOffline.L1Trigger import L1TStage2CaloLayer2Offline_cfi as L1TStep1
+
+variables = {
+    'jet': L1TStep1.jetEfficiencyThresholds,
+    'met': L1TStep1.metEfficiencyThresholds,
+    'mht': L1TStep1.mhtEfficiencyThresholds,
+    'ett': L1TStep1.ettEfficiencyThresholds,
+    'htt': L1TStep1.httEfficiencyThresholds,
+}
+
+plots = {
+    'jet': [
+        "efficiencyJetEt_HB", "efficiencyJetEt_HE", "efficiencyJetEt_HF",
+        "efficiencyJetEt_HB_HE"],
+    'met': ['efficiencyMET'],
+    'mht': ['efficiencyMHT'],
+    'ett': ['efficiencyETT'],
+    'htt': ['efficiencyHTT'],
+}
+
+allEfficiencyPlots = []
+add_plot = allEfficiencyPlots.append
+for variable, thresholds in variables.iteritems():
+    for plot in plots[variable]:
+        for threshold in thresholds:
+            plotName = '{0}_threshold_{1}'.format(plot, threshold)
+            add_plot(plotName)
+
+from DQMOffline.L1Trigger.L1TEfficiencyHarvesting2_cfi import l1tEfficiencyHarvesting
+l1tStage2CaloLayer2Efficiency = l1tEfficiencyHarvesting.clone(
+    plotCfgs=cms.untracked.VPSet(
+        cms.untracked.PSet(
+            numeratorDir=cms.untracked.string("L1T/L1TStage2CaloLayer2/efficiency_raw"),
+            outputDir=cms.untracked.string("L1T/L1TStage2CaloLayer2"),
+            numeratorSuffix=cms.untracked.string("_Num"),
+            denominatorSuffix=cms.untracked.string("_Den"),
+            plots=cms.untracked.vstring(allEfficiencyPlots)
+        ),
+        cms.untracked.PSet(
+            numeratorDir=cms.untracked.string("L1TEMU/L1TStage2CaloLayer2/efficiency_raw"),
+            outputDir=cms.untracked.string("L1TEMU/L1TStage2CaloLayer2"),
+            numeratorSuffix=cms.untracked.string("_Num"),
+            denominatorSuffix=cms.untracked.string("_Den"),
+            plots=cms.untracked.vstring(allEfficiencyPlots)
+        ),
+    )
+)
