adbc76f3ff2013a0e1bc030d4a0e3c09c0ef3917
esmaeeleskandari <esi.esk@gmail.com>
2016-11-22 10:16:56 +0100

    All needed inputTags are added

--- DQM/L1TMonitor/python/L1TStage2EMTFEmul_cfi.py
+++ DQM/L1TMonitor/python/L1TStage2EMTFEmul_cfi.py
@@ -2,7 +2,10 @@ import FWCore.ParameterSet.Config as cms
 
 l1tStage2EmtfEmul = cms.EDAnalyzer(
     "L1TStage2EMTF",
-    emtfSource = cms.InputTag("valEmtfStage2Digis", "EMTF"),
+    emtfSource_daq = cms.InputTag("valEmtfStage2Digis", "EMTF"),
+    emtfSource_hit = cms.InputTag("valEmtfStage2Digis", "EMTF"),
+    emtfSource_track = cms.InputTag("valEmtfStage2Digis", "EMTF"),
+    emtfSource_muon = cms.InputTag("valEmtfStage2Digis", "EMTF"),
     monitorDir = cms.untracked.string("L1T2016EMU/L1TStage2EMTFEMU"),
     isData = cms.untracked.bool(False),
     filterBX = cms.untracked.bool(True),
