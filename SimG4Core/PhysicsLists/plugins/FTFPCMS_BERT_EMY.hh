7a967c97fb636face8c4bd93e653be48e5cf9d49
Vladimir <Vladimir.Ivantchenko@cern.ch>
2016-11-23 13:06:38 +0100

    added comment

--- SimG4Core/PhysicsLists/plugins/FTFPCMS_BERT_EMY.hh
+++ SimG4Core/PhysicsLists/plugins/FTFPCMS_BERT_EMY.hh
@@ -1,6 +1,14 @@
 #ifndef SimG4Core_PhysicsLists_FTFPCMS_BERT_EMY_H
 #define SimG4Core_PhysicsLists_FTFPCMS_BERT_EMY_H
 
+// FTFP_BERT_EMY is a standard Geant4 Physics List with Option3 EM Physics
+//               configuration: forced more steps of e+- near geometry  
+//               boundary. This configuration may be used for R&D of 
+//               tracker and HGCal detector performnace. The similation
+//               is expected to be approximately two times slower then 
+//               with the CMS production Physics List
+
+
 #include "SimG4Core/Physics/interface/PhysicsList.h"
 #include "FWCore/ParameterSet/interface/ParameterSet.h"
 
