2ba0700794818f9e63917d4bf51ab7005c2d3947
Lindsey Gray <lindsey.gray@gmail.com>
2016-11-25 11:13:52 +0100

    add updates to RecHitTools from clemens

--- RecoLocalCalo/HGCalRecAlgos/src/RecHitTools.cc
+++ RecoLocalCalo/HGCalRecAlgos/src/RecHitTools.cc
@@ -16,10 +16,23 @@ namespace {
   constexpr char hgcalfh_sens[] = "HGCalHESiliconSensitive";
 
   constexpr std::float_t idx_to_thickness = std::float_t(100.0);
+  // define layer offsets
+  // https://github.com/cms-sw/cmssw/blob/CMSSW_8_1_X/DataFormats/ForwardDetId/interface/ForwardSubdetector.h
+  // HGCEE=3, HGCHEF=4, HGCHEB=5
+  const unsigned int hefOffset = 28; // number of EE layers
+  const unsigned int hebOffset = hefOffset + 12; // number of EE+FH layers
+
 
   inline void check_ddd(const HGCalDDDConstants* ddd) {
     if( nullptr == ddd ) {
-      throw cms::Exception("hgcal::RecHitTools") 
+      throw cms::Exception("hgcal::RecHitTools")
+        << "HGCalGeometry not provided yet to hgcal::RecHitTools!";
+    }
+  }
+
+  inline void check_geom(const HGCalGeometry* geom) {
+    if( nullptr == geom ) {
+      throw cms::Exception("hgcal::RecHitTools")
         << "HGCalGeometry not provided yet to hgcal::RecHitTools!";
     }
   }
@@ -38,10 +51,19 @@ void RecHitTools::getEventSetup(const edm::EventSetup& es) {
   ddd_[1]  = &(geom_[0]->topology().dddConstants());
 }
 
+GlobalPoint RecHitTools::getPosition(const DetId& id) const {
+  auto geom = id.subdetId() == HGCEE ? geom_[0] : geom_[1];
+  check_geom(geom);
+  GlobalPoint position( std::move( geom->getPosition( id ) ) );
+  return position;
+}
+
 std::float_t RecHitTools::getSiThickness(const DetId& id) const {
   auto ddd = id.subdetId() == HGCEE ? ddd_[0] : ddd_[1];
   check_ddd(ddd);
-  int tidx = ddd->waferTypeL(id);
+  const HGCalDetId hid(id);
+  unsigned int wafer = hid.wafer();
+  int tidx = ddd->waferTypeL(wafer);
   return idx_to_thickness*tidx;
 }
 
@@ -53,12 +75,87 @@ std::float_t RecHitTools::getRadiusToSide(const DetId& id) const {
   return size;
 }
 
+unsigned int RecHitTools::getLayer(const DetId& id) const {
+  auto ddd = id.subdetId() == HGCEE ? ddd_[0] : ddd_[1];
+  check_ddd(ddd);
+  const HGCalDetId hid(id);
+  unsigned int layer = hid.layer();
+  return layer;
+}
+
+unsigned int RecHitTools::getLayerWithOffset(const DetId& id) const {
+  auto ddd = id.subdetId() == HGCEE ? ddd_[0] : ddd_[1];
+  check_ddd(ddd);
+  const HGCalDetId hid(id);
+  unsigned int layer = hid.layer();
+  unsigned int offset = 0;
+  switch(id.subdetId()) {
+      case HGCHEF:
+        offset += hefOffset;
+        break;
+      case HGCHEB:
+        offset += hebOffset;
+        break;
+  }
+  layer += offset;
+  return layer;
+}
+
+unsigned int RecHitTools::getWafer(const DetId& id) const {
+  auto ddd = id.subdetId() == HGCEE ? ddd_[0] : ddd_[1];
+  check_ddd(ddd);
+  const HGCalDetId hid(id);
+  unsigned int wafer = hid.wafer();
+  return wafer;
+}
+
+unsigned int RecHitTools::getCell(const DetId& id) const {
+  auto ddd = id.subdetId() == HGCEE ? ddd_[0] : ddd_[1];
+  check_ddd(ddd);
+  const HGCalDetId hid(id);
+  unsigned int cell = hid.cell();
+  return cell;
+}
+
 bool RecHitTools::isHalfCell(const DetId& id) const {
   auto ddd = id.subdetId() == HGCEE ? ddd_[0] : ddd_[1];
   check_ddd(ddd);
   const HGCalDetId hid(id);
-  const int waferType = ddd->waferTypeT(hid.waferType());  
+  const int waferType = ddd->waferTypeT(hid.waferType());
   return ddd->isHalfCell(waferType,hid.cell());
 }
 
+float RecHitTools::getEta(const GlobalPoint& position, const float& vertex_z) const {
+  GlobalPoint corrected_position = GlobalPoint(position.x(), position.y(), position.z()-vertex_z);
+  return corrected_position.eta();
+}
 
+float RecHitTools::getEta(const DetId& id, const float& vertex_z) const {
+  GlobalPoint position = getPosition(id);
+  float eta = getEta(position, vertex_z);
+  return eta;
+}
+
+float RecHitTools::getPhi(const GlobalPoint& position) const {
+  float phi = atan2(position.y(),position.x());
+  return phi;
+}
+
+float RecHitTools::getPhi(const DetId& id) const {
+  GlobalPoint position = getPosition(id);
+  float phi = atan2(position.y(),position.x());
+  return phi;
+}
+
+float RecHitTools::getPt(const GlobalPoint& position, const float& hitEnergy, const float& vertex_z) const {
+  float eta = getEta(position, vertex_z);
+  float pt = hitEnergy / cosh(eta);
+  return pt;
+}
+
+float RecHitTools::getPt(const DetId& id, const float& hitEnergy, const float& vertex_z) const {
+  GlobalPoint position = getPosition(id);
+  float eta = getEta(position, vertex_z);
+  float pt = hitEnergy / cosh(eta);
+  return pt;
+}
