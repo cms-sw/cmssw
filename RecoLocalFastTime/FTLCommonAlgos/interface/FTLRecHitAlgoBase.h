af694537413489d8a4b84071d20c0f3cb61cca79
Lindsey Gray <lindsey.gray@gmail.com>
2016-11-28 16:34:42 +0100

    skeleton of RecHit producers

---
+++ RecoLocalFastTime/FTLCommonAlgos/interface/FTLRecHitAlgoBase.h
@@ -0,0 +1,44 @@
+#ifndef RecoLocalFastTime_FTLCommonAlgos_FTLRecHitAlgoBase_HH
+#define RecoLocalFastTime_FTLCommonAlgos_FTLRecHitAlgoBase_HH
+
+/** \class FTLRecHitAlgoBase
+  *  Template algorithm to make rechits from uncalibrated rechits
+  *
+  *  \author Lindsey Gray
+  */
+
+#include "FWCore/ParameterSet/interface/ParameterSet.h"
+#include "FWCore/Framework/interface/ConsumesCollector.h"
+
+#include "DataFormats/FTLRecHit/interface/FTLRecHit.h"
+#include "DataFormats/FTLRecHit/interface/FTLUncalibratedRecHit.h"
+
+namespace edm {
+  class Event;
+  class EventSetup;
+}
+
+class FTLRecHitAlgoBase {
+ public:
+
+  /// Constructor
+  FTLRecHitAlgoBase(const edm::ParameterSet& conf,
+                    edm::ConsumesCollector& sumes) { };
+
+  /// Destructor
+  virtual ~FTLRecHitAlgoBase() { };
+
+  /// get event and eventsetup information
+  virtual void getEvent(const edm::Event&) = 0;
+  virtual void getEventSetup(const edm::EventSetup&) = 0;
+
+  /// make rechits from dataframes
+  virtual FTLRecHit makeRecHit(const FTLUncalibratedRecHit& uncalibRH, const uint32_t &flags) const = 0;
+
+};
+
+#include "FWCore/PluginManager/interface/PluginFactory.h"
+typedef edmplugin::PluginFactory< FTLRecHitAlgoBase* (const edm::ParameterSet&, edm::ConsumesCollector&) > FTLRecHitAlgoFactory;
+
+
+#endif
