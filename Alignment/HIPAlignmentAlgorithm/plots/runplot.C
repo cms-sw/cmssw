f75f7dd12513a4b87be6a15e6fb817708a803b63
Ulascan Sarica <ulascan90@gmail.com>
2016-11-30 23:17:18 +0100

    Improve plotting scripts

--- Alignment/HIPAlignmentAlgorithm/plots/runplot.C
+++ Alignment/HIPAlignmentAlgorithm/plots/runplot.C
@@ -1,53 +1,43 @@
-void runplot()
-{
-    gROOT->ProcessLine(" .L tdrstyle.C");
-    gROOT->ProcessLine("setTDRStyle()");
-    gROOT->ProcessLine(".L HIPplots.cc++");
-    gROOT->ProcessLine(".L plotter.C++");
+#include <iostream>
+#include <vector>
+#include "TROOT.h"
+#include "TString.h"
+#include "plotter.C"
 
-    //***********DEFINED BY USER******************
+using namespace std;
 
-    char* path = "/afs/cern.ch/cms/CAF/CMSALCA/ALCA_TRACKERALIGN2/HIP/public/AlignmentCamp_2016RunB/CMSSW_8_0_7/src/Alignment/HIPAlignmentAlgorithm/";
-    char* obj = "hp1704_2";
+void runplot(TString obj, int iov, int iter){
+  //***********DEFINED BY USER******************
 
-    //starting run number of alignment
-    int iov = 272497;
+  TString path = "../";
 
-    // choose plot type from : "cov","shift","chi2","param","hitmap"
-    char* plotType[] = { "cov", "shift", "chi2", "param", "hitmap" };
+  // choose plot type from : "cov","shift","chi2","param","hitmap"
+  vector<TString> plottype;
+  //plottype.push_back("cov");
+  //plottype.push_back("shift");
+  plottype.push_back("chi2");
+  //plottype.push_back("param");
+  //plottype.push_back("hitmap");
 
-    // plot all detectors together or individually
-    bool MergeAllDet = 1;
-    
-    //the iteration to use for plotting fitted paramters
-    int Niteration =9;
+  // plot all detectors together or individually
+  bool MergeAllDet = 1;
 
-    //*******************************************
+  //*******************************************
 
-    int Nplots = sizeof(plotType) / sizeof(plotType[0]);
+  int Nplots = plottype.size();
 
-    //plotting all detectors together
+  //plotting all detectors together
 
-    if (MergeAllDet == 1) {
-        for (int i = 0; i < Nplots; i++) {
-            char arg[500];
-            sprintf(arg, "plotter(\"%s\",\"%s\",%d,\"%s\",%d)", path, obj, iov, plotType[i],Niteration);
-            cout << arg << endl;
-            gROOT->ProcessLine(arg);
-        }
+  if (MergeAllDet == 1){
+    for (int i = 0; i < Nplots; i++) plotter(path.Data(), obj.Data(), iov, plottype.at(i).Data(), iter);
+  }
+
+  // plotting each detector separately, don't use this for hit-map.
+  else{
+    // det: 0=all,1=PXB, 2=PXF, 3=TIB, 4=TID, 5=TOB, 6=TEC
+    for (int i = 0; i < Nplots; i++) {
+      for (int det = 0; det < 6; det++) plotter(path.Data(), obj.Data(), iov, plottype.at(i).Data(), iter, det);
     }
-
-    // plotting each detector separately, don't use this for hit-map.
-
-    else {
-        // det: 0=all,1=PXB, 2=PXF, 3=TIB, 4=TID, 5=TOB, 6=TEC
-        for (int i = 0; i < Nplots; i++) {
-            for (int det = 0; det < 6; det++) {
-                char arg2[500];
-                sprintf(arg2, "plotter(\"%s\",\"%s\",%d,\"%s\",%d,%d)", path, obj, iov, plotType[i],Niteration,det);
-                gROOT->ProcessLine(arg2);
-            }
-        }
-    }
+  }
 }
 
