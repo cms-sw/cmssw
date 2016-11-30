d2a3ce9110f6db26a918bf2070626a873bc71c2c
Lindsey Gray <lindsey.gray@gmail.com>
2016-11-30 11:10:50 +0100

    rename a few things

--- RecoLocalCalo/HGCalRecProducers/python/hgcalLayerClusters_cfi.py
+++ RecoLocalCalo/HGCalRecProducers/python/hgcalLayerClusters_cfi.py
@@ -3,12 +3,12 @@ import FWCore.ParameterSet.Config as cms
 #### PF CLUSTER ECAL ####
 
 #energy corrector for corrected cluster producer
-imagingClusterHGCal =  cms.EDProducer(
+hgcalLayerClusters =  cms.EDProducer(
     "HGCalClusterTestProducer",
-    detector = cms.string("EE"),
+    detector = cms.string("all"),
     doSharing = cms.bool(False),
     deltac = cms.double(2.),
-    ecut = cms.double(0.0001),
+    ecut = cms.double(0.01),
     kappa = cms.double(10.),
     verbosity = cms.untracked.uint32(3)
     )
