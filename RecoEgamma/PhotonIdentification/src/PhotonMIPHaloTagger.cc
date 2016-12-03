9aabbdaa911daecb11477d0f638ce0563af148ae
Usama Hussain <uhussain@wisc.edu>
2016-12-03 01:29:37 +0100

    giving variables the scope they need

--- RecoEgamma/PhotonIdentification/src/PhotonMIPHaloTagger.cc
+++ RecoEgamma/PhotonIdentification/src/PhotonMIPHaloTagger.cc
@@ -259,17 +259,13 @@ std::vector<double> PhotonMIPHaloTagger::GetMipTrailFit(const reco::Photon* phot
      if(debug_)std::cout<<" starting npoing = "<<Npoints<<std::endl;
 
      //defined some variable for iterative fitting the mip trail line  
-        double  res    = 0.0;                            
-        double  res_sq = 0.0;                            
-        double  wt     = 0.0;                            
         double  sx     = 0.0;    
         double  sy     = 0.0;                                   
         double  ss     = 0.0;                            
         double  sxx    = 0.0;                            
-        double  sxy    = 0.0;                            
-        double  delt   = 0.0;
+        double  sxy    = 0.0;
         double  a1     = 0.0;
-        double  b1     = 0.0;                            
+        double  b1     = 0.0;                             
         double  m_chi2 = 0.0;                            
         double  etot_cell=0.0;   
 
@@ -302,7 +298,7 @@ std::vector<double> PhotonMIPHaloTagger::GetMipTrailFit(const reco::Photon* phot
             //Fit the line to trail 
            for(int j=0; j<Npoints; j++)
             {  
-               wt = 1.0;
+               double wt = 1.0;
                ss += wt;
                sx += ieta_cell[j]*wt;                               
                sy += iphi_cell[j]; 
@@ -310,9 +306,9 @@ std::vector<double> PhotonMIPHaloTagger::GetMipTrailFit(const reco::Photon* phot
                sxy += ieta_cell[j]*iphi_cell[j]*wt;
             }
        
-            delt =  ss*sxx - (sx*sx);
-              a1 = ((sxx*sy)-(sx*sxy))/delt;   // INTERCEPT
-              b1 = ((ss*sxy)-(sx*sy))/delt;    // SLOPE
+            double delt =  ss*sxx - (sx*sx);
+            a1 = ((sxx*sy)-(sx*sxy))/delt;   // INTERCEPT
+            b1 = ((ss*sxy)-(sx*sy))/delt;    // SLOPE
 
 
           double highest_res   = 0.;
@@ -321,8 +317,8 @@ std::vector<double> PhotonMIPHaloTagger::GetMipTrailFit(const reco::Photon* phot
 
             for(int j=0; j<Npoints; j++)
                {                
-                    res = 1.0*iphi_cell[j] - a1 - b1*ieta_cell[j];
-                    res_sq = res*res;
+                    double res = 1.0*iphi_cell[j] - a1 - b1*ieta_cell[j];
+                    double res_sq = res*res;
 
                     if(TMath::Abs(res) > highest_res)
                        {             
