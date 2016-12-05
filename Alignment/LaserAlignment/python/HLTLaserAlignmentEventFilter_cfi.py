48f44593bafdf059af142b2a8ad79c28ddf878e4
Martin Grunewald <Martin.Grunewald@cern.ch>
2016-12-05 17:54:31 +0100

    Fix for too long a vector

---
+++ Alignment/LaserAlignment/python/HLTLaserAlignmentEventFilter_cfi.py
@@ -0,0 +1,18 @@
+import FWCore.ParameterSet.Config as cms
+
+
+from Alignment.LaserAlignment.LaserAlignmentDefs_cff import *
+
+LaserAlignmentEventFilter = cms.EDFilter("LaserAlignmentEventFilter",
+  FedInputTag = cms.InputTag( 'source' )
+)
+
+LaserAlignmentEventFilter.FED_IDs = cms.vint32()
+
+LaserAlignmentEventFilter.DET_IDs = cms.vint32()
+
+LaserAlignmentEventFilter.SIGNAL_IDs = cms.vint32()
+
+LaserAlignmentEventFilter.SINGLE_CHANNEL_THRESH = cms.uint32(11);
+LaserAlignmentEventFilter.CHANNEL_COUNT_THRESH = cms.uint32(8);
+
