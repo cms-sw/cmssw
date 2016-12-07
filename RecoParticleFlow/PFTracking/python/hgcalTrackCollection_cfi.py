56f382184004a77fbff120bdd5ef1b39d31c4482
Kevin Pedro <kpedro88@gmail.com>
2016-12-07 16:59:03 -0600

    restore performance with nonsense value

--- RecoParticleFlow/PFTracking/python/hgcalTrackCollection_cfi.py
+++ RecoParticleFlow/PFTracking/python/hgcalTrackCollection_cfi.py
@@ -8,7 +8,7 @@ hgcalTrackCollection = cms.EDProducer(
 
     useIterativeTracking = cms.bool(True),
     DPtOverPtCuts_byTrackAlgo = cms.vdouble(10.0,10.0,10.0,10.0,10.0,5.0),
-    NHitCuts_byTrackAlgo = cms.vuint32(3,3,3,3,3,3),
+    NHitCuts_byTrackAlgo = cms.vuint32(3,3,3,3,3,32700), # the last value is nonsense
 
     # From HGCClusterizer
     hgcalGeometryNames = cms.PSet( HGC_ECAL  = cms.string('HGCalEESensitive'),
