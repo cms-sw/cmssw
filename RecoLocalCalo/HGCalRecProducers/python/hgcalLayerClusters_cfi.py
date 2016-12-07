d30a74c8993469485acb7680aaa506dcd0a8cad2
Lindsey Gray <lindsey.gray@gmail.com>
2016-12-07 08:50:51 +0100

    move E_cut up to 0.06

--- RecoLocalCalo/HGCalRecProducers/python/hgcalLayerClusters_cfi.py
+++ RecoLocalCalo/HGCalRecProducers/python/hgcalLayerClusters_cfi.py
@@ -8,7 +8,7 @@ hgcalLayerClusters =  cms.EDProducer(
     detector = cms.string("all"),
     doSharing = cms.bool(False),
     deltac = cms.double(2.),
-    ecut = cms.double(0.01),
+    ecut = cms.double(0.06),
     kappa = cms.double(10.),
     verbosity = cms.untracked.uint32(3)
     )
