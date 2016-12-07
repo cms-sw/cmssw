9b385fd872fb9a28216c2dfb338c2d08387376cb
Marcel Andre Schneider <marcel.andre.schneider@cern.ch>
2016-12-07 15:00:15 +0100

    Make the fileinputsource more intelligent.
    To allow reprocessing StreamExpress runs. One dataset (as returned from
    DAS) can have many runs, so we allow selecting a specific run. The
    dataset always has all events and will be fully processed, which can be
    too much for the online DQM, therefore we allow selecting a subset of LS.

--- DQM/Integration/python/config/fileinputsource_cfi.py
+++ DQM/Integration/python/config/fileinputsource_cfi.py
@@ -1,44 +1,59 @@
 import FWCore.ParameterSet.Config as cms
 
-source = cms.Source(
-    "PoolSource",
-    fileNames = cms.untracked.vstring(
-        #"root://eoscms//store/express/Commissioning2016/ExpressCosmics/FEVT/Express-v1/000/266/681/00000/001AF1D0-6BE9-E511-9A8D-02163E0143FE.root", 
-        #"root://eoscms//store/express/Commissioning2016/ExpressCosmics/FEVT/Express-v1/000/266/681/00000/005BCAC3-72E9-E511-B002-02163E014310.root",
-        #"root://eoscms//store/express/Commissioning2016/ExpressCosmics/FEVT/Express-v1/000/266/681/00000/00E3C803-BBE9-E511-A99C-02163E01465A.root",
-        #"root://eoscms//store/express/Commissioning2016/ExpressCosmics/FEVT/Express-v1/000/266/681/00000/00EFE5C2-6EE9-E511-ABB3-02163E013417.root",
-        #"root://eoscms//store/express/Commissioning2016/ExpressCosmics/FEVT/Express-v1/000/266/681/00000/04159D15-95E9-E511-B73F-02163E014176.root",
-        #"root://eoscms//store/express/Commissioning2016/ExpressCosmics/FEVT/Express-v1/000/266/681/00000/04E4A62F-93E9-E511-83A3-02163E0134CD.root",
-        #"root://eoscms//store/express/Commissioning2016/ExpressCosmics/FEVT/Express-v1/000/266/681/00000/04F7F2D7-72E9-E511-9534-02163E014310.root",
-        #"root://eoscms//store/express/Commissioning2016/ExpressCosmics/FEVT/Express-v1/000/266/681/00000/06077D63-78E9-E511-9D8A-02163E014310.root",
-        #"root://eoscms//store/express/Commissioning2016/ExpressCosmics/FEVT/Express-v1/000/266/681/00000/061EFCD5-97E9-E511-8764-02163E0143FE.root",
-        #"root://eoscms//store/express/Commissioning2016/ExpressCosmics/FEVT/Express-v1/000/266/681/00000/063C23DD-82E9-E511-9020-02163E011FCE.root",        
-        "file:/afs/cern.ch/cms/CAF/CMSCOMM/COMM_DQM/DQMTest/MinimumBias__RAW__v1__165633__1CC420EE-B686-E011-A788-0030487CD6E8.root",
-        #"/store/data/Commissioning2014/Cosmics/RAW//v3/000/224/380/00000/E05943D1-1227-E411-BB8E-02163E00F0C4.root"
-        #"/store/data/Commissioning2014/Cosmics/RAW/v3/000/224/380/00000/68FDADE5-1227-E411-8AA6-02163E00A10C.root"
-    )
-)
-
-maxEvents = cms.untracked.PSet(
-    input = cms.untracked.int32(-1)
-)
-
 # Parameters for runType
 import FWCore.ParameterSet.VarParsing as VarParsing
 import sys
+import fnmatch
 from dqmPythonTypes import *
+
+from dataset_cfi import *
 
 options = VarParsing.VarParsing("analysis")
 
 options.register(
     "runkey",
-    "cosmic_run",
+    "pp_run",
     VarParsing.VarParsing.multiplicity.singleton,
     VarParsing.VarParsing.varType.string,
     "Run Keys of CMS"
 )
 
+options.register('runNumber',
+                 286520,
+                 VarParsing.VarParsing.multiplicity.singleton,
+                 VarParsing.VarParsing.varType.int,
+                 "Run number. This run number has to be present in the dataset configured in dataset_cfi.py.")
+
+options.register('maxLumi',
+                 2000,
+                 VarParsing.VarParsing.multiplicity.singleton,
+                 VarParsing.VarParsing.varType.int,
+                 "Only lumisections up to maxLumi are processed.")
+
+options.register('lumiPattern',
+                 '*0',
+                 VarParsing.VarParsing.multiplicity.singleton,
+                 VarParsing.VarParsing.varType.string,
+                 "Only lumisections with numbers matching lumiPattern are processed.")
+
 options.parseArguments()
+
+runstr = str(options.runNumber)
+runpattern = "*" + runstr[0:3] + "/" + runstr[3:] + "*"
+readFiles = cms.untracked.vstring([f for f in readFiles if fnmatch.fnmatch(f, runpattern)])
+lumirange =  cms.untracked.VLuminosityBlockRange(
+  [ str(options.runNumber) + ":" + str(ls) 
+      for ls in range(1, options.maxLumi+1) 
+      if fnmatch.fnmatch(str(ls), options.lumiPattern)
+  ]
+)
+
+print "Selected %d files and %d LS" % (len(readFiles), len(lumirange)) 
+
+source = cms.Source ("PoolSource",fileNames = readFiles, secondaryFileNames = secFiles, lumisToProcess = lumirange)
+maxEvents = cms.untracked.PSet(
+    input = cms.untracked.int32(-1)
+)
 
 # Fix to allow scram to compile
 #if len(sys.argv) > 1:
