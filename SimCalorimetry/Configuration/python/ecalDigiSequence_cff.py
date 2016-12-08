89b1b104f7d0a3a1d4029cb65444343fa49e73db
Nancy <nancy.marinelli@cern.ch>
2016-12-08 18:24:53 +0100

    Update Configuration/python/ecalDigiSequence_cff.py to add the new EB trigger primitives. Update also the event content accordignly so the new collection is saved

--- SimCalorimetry/Configuration/python/ecalDigiSequence_cff.py
+++ SimCalorimetry/Configuration/python/ecalDigiSequence_cff.py
@@ -3,12 +3,15 @@ import FWCore.ParameterSet.Config as cms
 # unsuppressed digis simulation - fast preshower
 from SimCalorimetry.EcalSimProducers.ecaldigi_cfi import *
 # ECAL Trigger Primitives (needed by SRP)
+from SimCalorimetry.EcalEBTrigPrimProducers.ecalEBTriggerPrimitiveDigis_cff import *
 from SimCalorimetry.EcalTrigPrimProducers.ecalTriggerPrimitiveDigis_cff import *
 # Selective Readout Processor producer
 from SimCalorimetry.EcalSelectiveReadoutProducers.ecalDigis_cfi import *
 # Preshower Zero suppression producer
 from SimCalorimetry.EcalZeroSuppressionProducers.ecalPreshowerDigis_cfi import *
 # simEcalUnsuppressedDigis is now done inside mixing module
-ecalDigiSequence = cms.Sequence(simEcalTriggerPrimitiveDigis*simEcalDigis*simEcalPreshowerDigis)
+ecalDigiSequence = cms.Sequence(simEcalEBTriggerPrimitiveDigis*simEcalTriggerPrimitiveDigis*simEcalDigis*simEcalPreshowerDigis)
+
+
 
 
