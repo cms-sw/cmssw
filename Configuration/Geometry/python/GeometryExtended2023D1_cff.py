53b8afe8cf59b2aea569c274a4c181621db00cbf
Gaelle <Gaelle.Boudoul@cern.ch>
2016-12-08 11:33:37 +0100

    T1 is deprecated, Replaced by T3 in the corresponding scenarios

--- Configuration/Geometry/python/GeometryExtended2023D1_cff.py
+++ Configuration/Geometry/python/GeometryExtended2023D1_cff.py
@@ -5,6 +5,6 @@ import FWCore.ParameterSet.Config as cms
 
 from Geometry.CMSCommonData.cmsExtendedGeometry2023D1XML_cfi import *
 from Geometry.TrackerNumberingBuilder.trackerNumberingGeometry_cfi import *
-from SLHCUpgradeSimulations.Geometry.fakeConditions_phase2TkTilted_cff import *
+from SLHCUpgradeSimulations.Geometry.fakeConditions_phase2TkTilted4021_cff import *
 from Geometry.HcalCommonData.hcalParameters_cfi      import *
 from Geometry.HcalCommonData.hcalDDDSimConstants_cfi import *
