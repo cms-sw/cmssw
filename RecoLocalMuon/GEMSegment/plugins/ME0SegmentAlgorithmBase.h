05446da93c4fe503eaa8f464af220232fb2ea289
Marcello <marcello.maggi@cern.ch>
2016-12-08 18:33:31 +0100

    move up typed definition

--- RecoLocalMuon/GEMSegment/plugins/ME0SegmentAlgorithmBase.h
+++ RecoLocalMuon/GEMSegment/plugins/ME0SegmentAlgorithmBase.h
@@ -15,14 +15,17 @@
 #include "DataFormats/GEMRecHit/interface/ME0RecHitCollection.h"
 #include "DataFormats/GEMRecHit/interface/ME0Segment.h"
 #include "Geometry/GEMGeometry/interface/ME0EtaPartition.h"
-#include "Geometry/GEMGeometry/interface/ME0Chamber.h"
 #include "FWCore/Framework/interface/Frameworkfwd.h"
 #include <map>
 #include <vector>
 
 class ME0SegmentAlgorithmBase {
 public:
-  typedef std::pair<const ME0Chamber*, std::map<uint32_t, const ME0EtaPartition*> >ME0Ensemble; 
+
+  /// Typedefs
+  typedef std::vector<const ME0RecHit*> EnsembleHitContainer;
+  typedef std::vector<EnsembleHitContainer> ProtoSegments;
+  typedef std::pair<const ME0EtaPartition*, std::map<uint32_t, const ME0EtaPartition*> >ME0Ensemble;
 
     /// Constructor
     explicit ME0SegmentAlgorithmBase(const edm::ParameterSet&) {};
@@ -31,9 +34,8 @@ public:
 
     /** Run the algorithm = build the segments in this chamber
     */
-    virtual std::vector<ME0Segment> run(const ME0Ensemble& ensemble, const std::vector<const ME0RecHit*>& rechits) = 0;  
+    virtual std::vector<ME0Segment> run(const ME0Ensemble& ensemble, const EnsembleHitContainer& rechits) = 0;  
 
     private:
 };
-
 #endif
