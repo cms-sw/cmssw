223aa53be9dbcc621b3bb3ab137ceff8197eb739
Patrick Gartung <gartung@fnal.gov>
2016-12-08 17:23:34 -0600

    L1Trigger: fix clang warnings about abs

--- L1Trigger/TrackTrigger/src/TTClusterAlgorithm_official.cc
+++ L1Trigger/TrackTrigger/src/TTClusterAlgorithm_official.cc
@@ -115,7 +115,7 @@ void TTClusterAlgorithm_official< Ref_Phase2TrackerDigi_ >::Cluster( std::vector
         }
 
         /// Skip non-contiguous column
-        if ( fabs( mapIter1DCbCR1->first.first - lastCol ) != 1 )
+        if ( std::abs( mapIter1DCbCR1->first.first - lastCol ) != 1 )
         {
           ++mapIter1DCbCR1;
           continue;
