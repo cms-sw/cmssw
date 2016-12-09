02535535316699fa8fac9891cf8c02ce3904399c
Patrick Gartung <gartung@fnal.gov>
2016-12-09 11:13:12 -0600

    cast unsigned long to long before abs to prevent undefined behavior

--- Geometry/CaloEventSetup/test/CaloGeometryAnalyzer.cc
+++ Geometry/CaloEventSetup/test/CaloGeometryAnalyzer.cc
@@ -217,7 +217,7 @@ CaloGeometryAnalyzer::cmpset( const CaloSubdetectorGeometry* geom ,
    }
    else
    {
-      if( 2 < std::abs( base.size() - over.size() ) ) 
+      if( 2 < std::abs( (long)(base.size()) - (long)(over.size()) ) ) 
       {
       DetSet inBaseNotOver ;
       DetSet inOverNotBase ;
