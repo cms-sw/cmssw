4d6e035dc52b4d7f5dfa3615f1fe113dccd73726
Sunanda <sunanda.banerjee@cern.ch>
2016-12-13 03:26:52 +0100

    Update all validation code in view of BH changes

--- Validation/HGCalValidation/python/digiValidationV6_cff.py
+++ Validation/HGCalValidation/python/digiValidationV6_cff.py
@@ -8,4 +8,6 @@ hgcalDigiValidationHEF = hgcalDigiValidationEE.clone(
 
 hgcalDigiValidationHEB = hgcalDigiValidationEE.clone(
     DetectorName = cms.string("HCal"),
-    DigiSource   = cms.InputTag("mix","HGCDigisHEback"))
+    DigiSource   = cms.InputTag("simHcalDigis","HBHEQIE11DigiCollection"),
+    ifHCAL       = cms.bool(True),
+    SampleIndx   = cms.untracked.int32(5))
