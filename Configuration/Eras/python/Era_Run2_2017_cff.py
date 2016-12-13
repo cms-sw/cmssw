8a86cb5394f95f539ae10ca11ccefe1c44252e45
Matti Kortelainen <matti.kortelainen@cern.ch>
2016-12-13 09:00:55 +0100

    Switch the 'old' 2017 tracking back to default, and CA seeding to the special workflows

--- Configuration/Eras/python/Era_Run2_2017_cff.py
+++ Configuration/Eras/python/Era_Run2_2017_cff.py
@@ -1,7 +1,7 @@
 import FWCore.ParameterSet.Config as cms
 
 from Configuration.Eras.ModifierChain_run2_2017_core_cff import run2_2017_core
-from Configuration.Eras.Modifier_trackingPhase1_cff import trackingPhase1
+from Configuration.Eras.Modifier_trackingPhase1QuadProp_cff import trackingPhase1QuadProp
 
-Run2_2017 = cms.ModifierChain(run2_2017_core, trackingPhase1)
+Run2_2017 = cms.ModifierChain(run2_2017_core, trackingPhase1QuadProp)
 
