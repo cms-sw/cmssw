2a43fa1a125106cbf550e75a47c095a9d716bd20
Andrea Bocci <fwyzard@gmail.com>
2016-12-13 12:00:43 +0100

    use phase1Pixel Era instead of trackingPhase1

--- HLTrigger/Configuration/python/Eras.py
+++ HLTrigger/Configuration/python/Eras.py
@@ -1,15 +1,15 @@
 import FWCore.ParameterSet.Config as cms
 
 # import the relevant eras from Configuration.Eras.*
-from Configuration.Eras.Modifier_trackingPhase1_cff import trackingPhase1
+from Configuration.Eras.Modifier_phase1Pixel_cff import phase1Pixel
 
 def modifyHLTforEras(fragment):
     """load all Eras-based customisations for the HLT configuration"""
 
     # modify the HLT configuration for the Phase I pixel geometry
     from HLTrigger.Configuration.customizeHLTTrackingForPhaseI2017 import customizeHLTPhaseIPixelGeom
-    trackingPhase1.toModify(fragment, customizeHLTPhaseIPixelGeom)
+    phase1Pixel.toModify(fragment, customizeHLTPhaseIPixelGeom)
 
     # modify the HLT configuration to run the Phase I tracking in the particle flow sequence
     from HLTrigger.Configuration.customizeHLTTrackingForPhaseI2017 import customizeHLTForPFTrackingPhaseI2017
-    trackingPhase1.toModify(fragment, customizeHLTForPFTrackingPhaseI2017)
+    phase1Pixel.toModify(fragment, customizeHLTForPFTrackingPhaseI2017)
