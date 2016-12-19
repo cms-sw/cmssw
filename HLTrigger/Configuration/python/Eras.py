2147ce5cfc35ac162e295476fe289978f70f8de3
Andrea Bocci <fwyzard@gmail.com>
2016-12-19 17:33:43 +0100

    move calls to `phase1Pixel.toModify` from Eras.py to customizeHLTTrackingForPhaseI2017.py

--- HLTrigger/Configuration/python/Eras.py
+++ HLTrigger/Configuration/python/Eras.py
@@ -7,9 +7,9 @@ def modifyHLTforEras(fragment):
     """load all Eras-based customisations for the HLT configuration"""
 
     # modify the HLT configuration for the Phase I pixel geometry
-    from HLTrigger.Configuration.customizeHLTTrackingForPhaseI2017 import customizeHLTPhaseIPixelGeom
-    phase1Pixel.toModify(fragment, customizeHLTPhaseIPixelGeom)
+    from HLTrigger.Configuration.customizeHLTTrackingForPhaseI2017 import modifyHLTPhaseIPixelGeom
+    modifyHLTPhaseIPixelGeom(fragment)
 
     # modify the HLT configuration to run the Phase I tracking in the particle flow sequence
-    from HLTrigger.Configuration.customizeHLTTrackingForPhaseI2017 import customizeHLTForPFTrackingPhaseI2017
-    phase1Pixel.toModify(fragment, customizeHLTForPFTrackingPhaseI2017)
+    from HLTrigger.Configuration.customizeHLTTrackingForPhaseI2017 import modifyHLTForPFTrackingPhaseI2017
+    modifyHLTForPFTrackingPhaseI2017(fragment)
