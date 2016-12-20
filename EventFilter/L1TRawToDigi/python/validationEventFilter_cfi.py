f3a6987698e05747346c422d8b1cf64a293210e9
Vladimir Rekovic <vladimir.rekovic@cern.ch>
2016-12-21 00:40:09 +0100

    Use getByToken for triggerCount, make it fully configurable, and use trackable parameters.

--- EventFilter/L1TRawToDigi/python/validationEventFilter_cfi.py
+++ EventFilter/L1TRawToDigi/python/validationEventFilter_cfi.py
@@ -3,6 +3,6 @@ import FWCore.ParameterSet.Config as cms
 
 validationEventFilter = cms.EDFilter(
     "L1TValidationEventFilter",
-    src       = cms.InputTag("tcdsDigis"),
-    period    = cms.untracked.int32(107)
+    src       = cms.InputTag("tcdsDigis","triggerCount"),
+    period    = cms.int32(107)
 )
