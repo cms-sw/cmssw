a6a97fd81fa7295707c234ff63b0c8a0ffdfe44d
Jan Kašpar <jan.kaspar@cern.ch>
2016-12-21 20:23:28 +0100

    CTPPSLocalTrackLite added to FEVT, RECO and AOD event content.

--- RecoCTPPS/Configuration/python/RecoCTPPS_EventContent_cff.py
+++ RecoCTPPS/Configuration/python/RecoCTPPS_EventContent_cff.py
@@ -17,7 +17,10 @@ RecoCTPPSFEVT = cms.PSet(
     # timing diamonds
     'keep TotemFEDInfos_ctppsDiamondRawToDigi_*_*',
     'keep CTPPSDiamondDigiedmDetSetVector_ctppsDiamondRawToDigi_*_*',
-    'keep TotemVFATStatusedmDetSetVector_ctppsDiamondRawToDigi_*_*'
+    'keep TotemVFATStatusedmDetSetVector_ctppsDiamondRawToDigi_*_*',
+
+    # CTPPS common
+    'keep CTPPSLocalTrackLites_ctppsLocalTrackLiteProducer_*_*'
   )
 )
 
@@ -39,7 +42,10 @@ RecoCTPPSRECO = cms.PSet(
     # timing diamonds
     'keep TotemFEDInfos_ctppsDiamondRawToDigi_*_*',
     'keep CTPPSDiamondDigiedmDetSetVector_ctppsDiamondRawToDigi_*_*',
-    'keep TotemVFATStatusedmDetSetVector_ctppsDiamondRawToDigi_*_*'
+    'keep TotemVFATStatusedmDetSetVector_ctppsDiamondRawToDigi_*_*',
+
+    # CTPPS common
+    'keep CTPPSLocalTrackLites_ctppsLocalTrackLiteProducer_*_*'
   )
 )
 
@@ -61,6 +67,9 @@ RecoCTPPSAOD = cms.PSet(
     # timing diamonds
     'keep TotemFEDInfos_ctppsDiamondRawToDigi_*_*',
     'keep CTPPSDiamondDigiedmDetSetVector_ctppsDiamondRawToDigi_*_*',
-    'keep TotemVFATStatusedmDetSetVector_ctppsDiamondRawToDigi_*_*'
+    'keep TotemVFATStatusedmDetSetVector_ctppsDiamondRawToDigi_*_*',
+
+    # CTPPS common
+    'keep CTPPSLocalTrackLites_ctppsLocalTrackLiteProducer_*_*'
   )
 )
