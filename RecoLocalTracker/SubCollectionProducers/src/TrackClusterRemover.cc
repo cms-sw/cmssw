850479592e0330fffe4f416a93d6d0f39187af41
Vincenzo Innocente <vincenzo.innocente@cern.ch>
2016-12-23 14:49:25 +0100

    use chi2 in cluster remover

--- RecoLocalTracker/SubCollectionProducers/src/TrackClusterRemover.cc
+++ RecoLocalTracker/SubCollectionProducers/src/TrackClusterRemover.cc
@@ -23,6 +23,8 @@
 
 #include "TrackingTools/PatternTools/interface/TrackCollectionTokens.h"
 
+#include "RecoTracker/TransientTrackingRecHit/interface/Traj2TrackHits.h"
+
 #include<limits>
 
 namespace {
@@ -41,7 +43,7 @@ namespace {
 
     using QualityMaskCollection = std::vector<unsigned char>;
 
-    const float maxChi2_;
+    const unsigned char maxChi2_;
     const int minNumberOfLayersWithMeasBeforeFiltering_;
     const reco::TrackBase::TrackQuality trackQuality_;
 
@@ -79,7 +81,7 @@ namespace {
   }
   
   TrackClusterRemover::TrackClusterRemover(const edm::ParameterSet& iConfig) :
-    maxChi2_(iConfig.getParameter<double>("maxChi2")),
+    maxChi2_(Traj2TrackHits::toChi2(iConfig.getParameter<double>("maxChi2"))),
     minNumberOfLayersWithMeasBeforeFiltering_(iConfig.getParameter<int>("minNumberOfLayersWithMeasBeforeFiltering")),
     trackQuality_(reco::TrackBase::qualityByName(iConfig.getParameter<std::string>("TrackQuality"))),
 
@@ -182,12 +184,15 @@ namespace {
       bool goodTk =  (pquals) ? (*pquals)[i] & qualMask : track.quality(trackQuality_);
       if ( !goodTk) continue;
       if(track.hitPattern().trackerLayersWithMeasurement() < minNumberOfLayersWithMeasBeforeFiltering_) continue;
+
+      auto const & chi2s = track.extra()->chi2s();
+      assert(chi2s.size()==track.recHitsSize());
       auto hb = track.recHitsBegin();
       for(unsigned int h=0;h<track.recHitsSize();h++){
         auto recHit = *(hb+h);
 	auto const & hit = *recHit;
 	if (!hit.isValid()) continue; 
-	// if ( tm.estimate() > maxChi2_ ) continue; // skip outliers   FIXME!!!
+	if ( chi2s[h] > maxChi2_ ) continue; // skip outliers
         auto const & thit = reinterpret_cast<BaseTrackerRecHit const&>(hit);
         auto const & cluster = thit.firstClusterRef();
 	if (cluster.isStrip()) collectedStrips[cluster.key()]=true;
