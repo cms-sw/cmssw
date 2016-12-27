06c325536012e69dafb11240905d3eb611168d39
Sunanda <sunanda.banerjee@cern.ch>
2016-12-27 15:44:50 +0100

    Bug fixes for Hcal geometry routines

--- Geometry/HcalCommonData/src/HcalDDDSimConstants.cc
+++ Geometry/HcalCommonData/src/HcalDDDSimConstants.cc
@@ -643,7 +643,7 @@ double HcalDDDSimConstants::deltaEta(int det, int etaR, int depth) const {
     }
   } else {
     if (etaR > 0 && etaR < nEta) {
-      if (etaR == hpar->noff[1]-1 && depth > 2) {
+      if (etaR == hpar->noff[1]-1 && depth > depthEta29[0]) {
 	tmp = 0.5*(hpar->etaTable[etaR+1]-hpar->etaTable[etaR-1]);
       } else if (det == static_cast<int>(HcalOuter)) {
 	if (etaR == hpar->noff[2]) {
@@ -682,7 +682,7 @@ double HcalDDDSimConstants::getEta(int det, int etaR, int zside,
     }
   } else {
     if (etaR > 0 && etaR < nEta) {
-      if (etaR == hpar->noff[1]-1 && depth > 2) {
+      if (etaR == hpar->noff[1]-1 && depth > depthEta29[0]) {
 	tmp = 0.5*(hpar->etaTable[etaR+1]+hpar->etaTable[etaR-1]);
       } else if (det == static_cast<int>(HcalOuter)) {
 	if (etaR == hpar->noff[2]) {
