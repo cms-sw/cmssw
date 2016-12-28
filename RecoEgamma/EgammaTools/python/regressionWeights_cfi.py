ea7d9b8fa00dcd3190cca74586b878a041d28892
Rafael <rclsa@fnal.gov>
2016-12-28 19:36:17 +0100

    add new regression to EGM reco sequence in 90X, requires PR 17048

--- RecoEgamma/EgammaTools/python/regressionWeights_cfi.py
+++ RecoEgamma/EgammaTools/python/regressionWeights_cfi.py
@@ -1,29 +1,82 @@
-from CondCore.DBCommon.CondDBSetup_cfi import *
+import FWCore.ParameterSet.Config as cms
 
-GBRWrapperRcd  =  cms.ESSource("PoolDBESSource",
-    CondDBSetup,
-    DumpStat=cms.untracked.bool(True),
-    toGet = cms.VPSet(
-      cms.PSet(
-        record = cms.string('GBRWrapperRcd'),
-        tag = cms.string('wgbrph_EBCorrection'),
-        label = cms.untracked.string('wgbrph_EBCorrection')
-      ),
-      cms.PSet(
-        record = cms.string('GBRWrapperRcd'),
-        tag = cms.string('wgbrph_EBUncertainty'),
-        label = cms.untracked.string('wgbrph_EBUncertainty')
-      ),    
-      cms.PSet(
-        record = cms.string('GBRWrapperRcd'),
-        tag = cms.string('wgbrph_EECorrection'),
-        label = cms.untracked.string('wgbrph_EECorrection')
-      ),
-      cms.PSet(
-        record = cms.string('GBRWrapperRcd'),
-        tag = cms.string('wgbrph_EEUncertainty'),
-        label = cms.untracked.string('wgbrph_EEUncertainty')
-      )),
-     connect = cms.string('sqlite_file:GBRWrapper.db')
-)
+def regressionWeights(process):
+    if not hasattr(process.GlobalTag,'toGet'):
+        process.GlobalTag.toGet=cms.VPSet()
+    process.GlobalTag.toGet.extend( cms.VPSet(
+        cms.PSet(
+            record = cms.string('GBRDWrapperRcd'),
+            tag = cms.string('GEDelectron_EBCorrection_80X_EGM_v2'),
+            label = cms.untracked.string('electron_eb_ECALonly'),
+            connect = cms.string('frontier://FrontierProd/CMS_CONDITIONS')
+        ),
+        cms.PSet(
+            record = cms.string('GBRDWrapperRcd'),
+            tag = cms.string('GEDelectron_EBUncertainty_80X_EGM_v2'),
+            label = cms.untracked.string('electron_eb_ECALonly_var'),
+            connect = cms.string('frontier://FrontierProd/CMS_CONDITIONS')
+        ),
+        cms.PSet(
+            record = cms.string('GBRDWrapperRcd'),
+            tag = cms.string('GEDelectron_EECorrection_80X_EGM_v2'),
+            label = cms.untracked.string('electron_ee_ECALonly'),
+            connect = cms.string('frontier://FrontierProd/CMS_CONDITIONS')
+        ),
+        cms.PSet(
+            record = cms.string('GBRDWrapperRcd'),
+            tag = cms.string('GEDelectron_EEUncertainty_80X_EGM_v2'),
+            label = cms.untracked.string('electron_ee_ECALonly_var'),
+            connect = cms.string('frontier://FrontierProd/CMS_CONDITIONS')
+        ),
+        cms.PSet(
+            record = cms.string('GBRDWrapperRcd'),
+            tag = cms.string('GEDelectron_track_EBCorrection_80X_EGM_v2'),
+            label = cms.untracked.string('electron_eb_ECALTRK'),
+            connect = cms.string('frontier://FrontierProd/CMS_CONDITIONS')
+        ),
+        cms.PSet(
+            record = cms.string('GBRDWrapperRcd'),
+            tag = cms.string('GEDelectron_track_EBUncertainty_80X_EGM_v2'),
+            label = cms.untracked.string('electron_eb_ECALTRK_var'),
+            connect = cms.string('frontier://FrontierProd/CMS_CONDITIONS')
+            ),                                              
+        cms.PSet(
+            record = cms.string('GBRDWrapperRcd'),
+            tag = cms.string('GEDelectron_track_EECorrection_80X_EGM_v2'),
+            label = cms.untracked.string('electron_ee_ECALTRK'),
+            connect = cms.string('frontier://FrontierProd/CMS_CONDITIONS')
+        ),
+        cms.PSet(
+            record = cms.string('GBRDWrapperRcd'),
+            tag = cms.string('GEDelectron_track_EEUncertainty_80X_EGM_v2'),
+            label = cms.untracked.string('electron_ee_ECALTRK_var'),
+            connect = cms.string('frontier://FrontierProd/CMS_CONDITIONS')
+        ),                                              
+        cms.PSet(
+           record = cms.string('GBRDWrapperRcd'),
+           tag = cms.string('GEDphoton_EBCorrection_80X_EGM_v2'),
+           label = cms.untracked.string('photon_eb_ECALonly'),
+           connect = cms.string('frontier://FrontierProd/CMS_CONDITIONS')
+        ),
+        cms.PSet(
+            record = cms.string('GBRDWrapperRcd'),
+            tag = cms.string('GEDphoton_EBUncertainty_80X_EGM_v2'),
+            label = cms.untracked.string('photon_eb_ECALonly_var'),
+            connect = cms.string('frontier://FrontierProd/CMS_CONDITIONS')
+        ),            
+        cms.PSet(
+            record = cms.string('GBRDWrapperRcd'),
+            tag = cms.string('GEDphoton_EECorrection_80X_EGM_v2'),
+            label = cms.untracked.string('photon_ee_ECALonly'),
+            connect = cms.string('frontier://FrontierProd/CMS_CONDITIONS')
+            ),
+        cms.PSet(
+            record = cms.string('GBRDWrapperRcd'),
+            tag = cms.string('GEDphoton_EEUncertainty_80X_EGM_v2'),
+            label = cms.untracked.string('photon_ee_ECALonly_var'),
+            connect = cms.string('frontier://FrontierProd/CMS_CONDITIONS')
+        )
+    ))
+    return process
+
 
