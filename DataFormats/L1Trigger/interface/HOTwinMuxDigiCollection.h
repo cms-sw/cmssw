64c679e0885d2ebc89f7b94017fa3c3916a5dc53
saxenapooja <pooja.saxeba@cern.ch>
2017-01-16 20:56:18 +0100

    added new HO TP collection class

---
+++ DataFormats/L1Trigger/interface/HOTwinMuxDigiCollection.h
@@ -0,0 +1,10 @@
+#ifndef TOTWINMUXDIGICOLLECTION_H
+#define TOTWINMUXDIGICOLLECTION_H
+
+#include <vector>
+#include "DataFormats/Common/interface/SortedCollection.h"
+#include "DataFormats/L1Trigger/interface/HOTPDigiTwinMux.h"
+
+typedef edm::SortedCollection<HOTPDigiTwinMux> HOTwinMuxDigiCollection;
+
+#endif
