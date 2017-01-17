3d03ae1b7ab81b8ec10c0db600c8bb898f86c71c
W. David Dagenhart <wdd@fnal.gov>
2017-01-17 21:26:28 +0100

    Clean up import statements to only import what is necessary.
    Note that the change in patPFMETCorrections_cff moves the
    import * in the right direction and allows fixing the other
    files, but leaves more work to be done elsewhere in another PR.

--- PhysicsTools/PatAlgos/python/recoLayer0/metCorrections_cff.py
+++ PhysicsTools/PatAlgos/python/recoLayer0/metCorrections_cff.py
@@ -3,7 +3,7 @@ import FWCore.ParameterSet.Config as cms
 #from PhysicsTools.PatAlgos.recoLayer0.jetCorrFactors_cfi import *
 from JetMETCorrections.Type1MET.correctionTermsPfMetType1Type2_cff import *
 from JetMETCorrections.Type1MET.correctionTermsCaloMet_cff import *
-from JetMETCorrections.Type1MET.correctedMet_cff import *
+from JetMETCorrections.Type1MET.correctedMet_cff import caloMetT1, caloMetT1T2, pfMetT1, pfMetT1T2
 #from JetMETCorrections.Configuration.JetCorrectionServicesAllAlgos_cff import *
 
 #from JetMETCorrections.Type1MET.pfMETCorrections_cff import *
