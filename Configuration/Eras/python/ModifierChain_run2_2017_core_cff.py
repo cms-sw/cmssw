c72276193d1ddff38294a58ace1d0db2e66b6f41
Kevin Pedro <kpedro88@gmail.com>
2017-01-17 11:54:48 -0600

    remove HE from 2017 Era

--- Configuration/Eras/python/ModifierChain_run2_2017_core_cff.py
+++ Configuration/Eras/python/ModifierChain_run2_2017_core_cff.py
@@ -2,9 +2,8 @@ import FWCore.ParameterSet.Config as cms
 
 from Configuration.Eras.Era_Run2_2016_cff import Run2_2016
 from Configuration.Eras.Modifier_phase1Pixel_cff import phase1Pixel
-from Configuration.Eras.Modifier_run2_HE_2017_cff import run2_HE_2017
 from Configuration.Eras.Modifier_run2_HF_2017_cff import run2_HF_2017
 from Configuration.Eras.Modifier_run2_HCAL_2017_cff import run2_HCAL_2017
 
-run2_2017_core = cms.ModifierChain(Run2_2016, phase1Pixel, run2_HE_2017, run2_HF_2017, run2_HCAL_2017)
+run2_2017_core = cms.ModifierChain(Run2_2016, phase1Pixel, run2_HF_2017, run2_HCAL_2017)
 
