1ce2ee4ee120193b3c209cd18267e07b648c986f
Giovanni <gpetruc@gmail.com>
2017-01-19 16:54:42 +0100

    Tagger modules for bad global muons in MiniAOD and AOD

---
+++ RecoMET/METFilters/python/badGlobalMuonTaggersAOD_cff.py
@@ -0,0 +1,14 @@
+import FWCore.ParameterSet.Config as cms
+
+badGlobalMuonTagger = cms.EDFilter("BadGlobalMuonTagger",
+    muons = cms.InputTag("muons"),
+    vtx   = cms.InputTag("offlinePrimaryVertices"),
+    muonPtCut = cms.double(20),
+    selectClones = cms.bool(False),
+)
+cloneGlobalMuonTagger = badGlobalMuonTagger.clone(
+    selectClones = True
+)
+
+noBadGlobalMuons = cms.Sequence(~cloneGlobalMuonTagger + ~badGlobalMuonTagger)
+    
