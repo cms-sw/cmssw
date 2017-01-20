d60f4f812c93957c0812dfdcf45229240d1faed5
Andrea Bocci <fwyzard@gmail.com>
2017-01-20 20:10:54 +0100

    implement HLT customisations for HCAL Phase I upgrades
    Migrate HBHE reconstruction from `HcalHitReconstructor`
    to `HBHEPhase1Reconstructor`.  The HLT parameters for
    HBHE for Method 3 are a guess, based on the 2016 parameters.
    
    Migrate HF reconstructioj from `HcalHitReconstructor`
    to `HFPreReconstructor` + `HFPhase1Reconstructor`.
    The HLT parameters for HF are identycal to those used by RECO,
    despite the differences that were present in 2016.

--- HLTrigger/Configuration/python/customizeHLTforMC.py
+++ HLTrigger/Configuration/python/customizeHLTforMC.py
@@ -20,14 +20,18 @@ def customizeHLTforMC(process,_fastSim=False):
 
   # customise hltHbhereco to use the Method 3 time slew parametrization and response correction for Monte Carlo (PR #11091)
   if 'hltHbhereco' in process.__dict__:
-    process.hltHbhereco.pedestalSubtractionType = cms.int32( 1 )
-    process.hltHbhereco.pedestalUpperLimit      = cms.double( 2.7 ) 
-    process.hltHbhereco.timeSlewParsType        = cms.int32( 3 )
-    # new time slew parametrisation
-    process.hltHbhereco.timeSlewPars            = cms.vdouble( 12.2999, -2.19142, 0, 12.2999, -2.19142, 0, 12.2999, -2.19142, 0 )
-    # old response correction, matching the 2015D 25ns data
-    process.hltHbhereco.respCorrM3              = cms.double( 1.0 )
-
+    if process.hltHbhereco._TypedParameterizable__type == 'HcalHitReconstructor':
+      # 2015-2016 Run 2
+      process.hltHbhereco.pedestalSubtractionType = cms.int32( 1 )
+      process.hltHbhereco.pedestalUpperLimit      = cms.double( 2.7 )
+      process.hltHbhereco.timeSlewParsType        = cms.int32( 3 )
+      # new time slew parametrisation
+      process.hltHbhereco.timeSlewPars            = cms.vdouble( 12.2999, -2.19142, 0, 12.2999, -2.19142, 0, 12.2999, -2.19142, 0 )
+      # old response correction, matching the 2015D 25ns data
+      process.hltHbhereco.respCorrM3              = cms.double( 1.0 )
+    else:
+      # 2017 Phase I
+      process.hltHbhereco.algorithm.respCorrM3    = cms.double( 1.0 )
 
   if _fastSim:
 
