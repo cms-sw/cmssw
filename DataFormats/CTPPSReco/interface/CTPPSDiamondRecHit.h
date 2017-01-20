ded3592af6c8ecd04c6659eacd77d4322902adec
Laurent Forthomme <laurent.forthomme@cern.ch>
2017-01-20 10:33:17 +0100

    Adding the HPTDC error flags to the RecHit data format

--- DataFormats/CTPPSReco/interface/CTPPSDiamondRecHit.h
+++ DataFormats/CTPPSReco/interface/CTPPSDiamondRecHit.h
@@ -10,6 +10,8 @@
 #ifndef DataFormats_CTPPSReco_CTPPSDiamondRecHit
 #define DataFormats_CTPPSReco_CTPPSDiamondRecHit
 
+#include "DataFormats/CTPPSDigi/interface/HPTDCErrorFlags.h"
+
 /// Reconstructed hit in diamond detectors.
 class CTPPSDiamondRecHit
 {
@@ -17,11 +19,13 @@ class CTPPSDiamondRecHit
     CTPPSDiamondRecHit() :
       x_( 0. ), x_width_( 0. ), y_( 0. ), y_width_( 0. ),
       t_( 0. ), tot_( 0. ),
-      ts_index_( 0 ) {;}
-    CTPPSDiamondRecHit( double x, double x_width, double y, double y_width, double t, double tot, int oot_idx ) :
+      ts_index_( 0 ), hptdc_err_( 0 )
+    {}
+    CTPPSDiamondRecHit( double x, double x_width, double y, double y_width, double t, double tot, int oot_idx, const HPTDCErrorFlags& hptdc_err ) :
       x_( x ), x_width_( x_width ), y_( y ), y_width_( y_width ),
       t_( t ), tot_( tot ),
-      ts_index_( oot_idx ) {;}
+      ts_index_( oot_idx ), hptdc_err_( hptdc_err )
+    {}
 
     inline void setX( const double& x ) { x_ = x; }
     inline double getX() const { return x_; }
@@ -44,12 +48,16 @@ class CTPPSDiamondRecHit
     inline void setOOTIndex( const int& i ) { ts_index_ = i; }
     inline int getOOTIndex() const { return ts_index_; }
 
+    inline void setHPTDCErrorFlags( const HPTDCErrorFlags& err ) { hptdc_err_ = err; }
+    inline HPTDCErrorFlags getHPTDCErrorFlags() const { return hptdc_err_; }
+
   private:
     double x_, x_width_;
     double y_, y_width_;
     double t_, tot_;
     /// Time slice index
     int ts_index_;
+    HPTDCErrorFlags hptdc_err_;
 };
 
 //----------------------------------------------------------------------------------------------------
