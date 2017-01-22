22be0fb5f67006b264b018dea776bd130fea48ea
Josh Bendavid <Josh.Bendavid@cern.ch>
2017-01-22 08:33:36 +0100

    protect against pathological case in PrimaryVertexAssignment (since filtered collections of vertices are sometimes used with PFPileUp)

--- CommonTools/RecoAlgos/src/PrimaryVertexAssignment.cc
+++ CommonTools/RecoAlgos/src/PrimaryVertexAssignment.cc
@@ -120,7 +120,7 @@ PrimaryVertexAssignment::chargedHadronVertex( const reco::VertexCollection& vert
 
   // if the track is not compatible with other PVs but is compatible with the BeamSpot, we may simply have not reco'ed the PV!
   //  we still point it to the closest in Z, but flag it as possible orphan-primary
-  if(std::abs(track->dxy(vertices[0].position()))<maxDxyForNotReconstructedPrimary_ && std::abs(track->dxy(vertices[0].position())/track->dxyError())<maxDxySigForNotReconstructedPrimary_)
+  if(vertices.size() && std::abs(track->dxy(vertices[0].position()))<maxDxyForNotReconstructedPrimary_ && std::abs(track->dxy(vertices[0].position())/track->dxyError())<maxDxySigForNotReconstructedPrimary_)
      return std::pair<int,PrimaryVertexAssignment::Quality>(vtxIdMinDist,PrimaryVertexAssignment::NotReconstructedPrimary);
  
   //FIXME: here we could better handle V0s and NucInt
