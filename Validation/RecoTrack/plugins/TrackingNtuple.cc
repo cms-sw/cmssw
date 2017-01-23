2445ca9c9a9eb9ef6d10c392cdc7f0e7dcb5c318
erica brondolin <erica.brondolin@cern.ch>
2017-01-23 12:35:21 +0100

    removing debug

--- Validation/RecoTrack/plugins/TrackingNtuple.cc
+++ Validation/RecoTrack/plugins/TrackingNtuple.cc
@@ -921,7 +921,6 @@ TrackingNtuple::TrackingNtuple(const edm::ParameterSet& iConfig):
     t->Branch("pix_bbxi"  , &pix_bbxi );
     //strips
     if(includeStripHits_){
-      LogDebug("TrackingNtuple") << "including strips";
       t->Branch("str_isBarrel"  , &str_isBarrel );
       t->Branch("str_isStereo"  , &str_isStereo );
       t->Branch("str_det"       , &str_det      );
@@ -969,7 +968,6 @@ TrackingNtuple::TrackingNtuple(const edm::ParameterSet& iConfig):
     }
     //phase2 OT
     if(includePhase2OTHits_){
-      LogDebug("TrackingNtuple") << "including phase2 ot";
       t->Branch("ph2_isBarrel"  , &ph2_isBarrel );
       t->Branch("ph2_det"       , &ph2_det      );
       t->Branch("ph2_lay"       , &ph2_lay      );
@@ -2184,7 +2182,6 @@ void TrackingNtuple::fillSeeds(const edm::Event& iEvent,
           LogTrace("TrackingNtuple") << " not pixel and not Strip detector";
         }
       }
-      LogTrace("TrackingNtuple") << " after running on hits";
       see_hitIdx   .push_back( hitIdx  );
       see_hitType  .push_back( hitType );
       see_nPixel   .push_back( std::count(hitType.begin(), hitType.end(), static_cast<int>(HitType::Pixel)) );
