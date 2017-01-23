1ff753ff7029fe9bb99fa8406239a0d504dbf5cb
Vladimir Rekovic <vladimir.rekovic@cern.ch>
2017-01-23 11:29:22 +0100

    Configure L1T to run Stage2 (not Legacy) in Phase2 era.

--- L1Trigger/L1TCalorimeter/python/simDigis_cff.py
+++ L1Trigger/L1TCalorimeter/python/simDigis_cff.py
@@ -5,7 +5,8 @@ import FWCore.ParameterSet.Config as cms
 #
 from Configuration.Eras.Modifier_stage1L1Trigger_cff import stage1L1Trigger
 from Configuration.Eras.Modifier_stage2L1Trigger_cff import stage2L1Trigger
-if not (stage1L1Trigger.isChosen() or stage2L1Trigger.isChosen()):
+from Configuration.Eras.Modifier_phase2_common_cff import phase2_common
+if not (stage1L1Trigger.isChosen() or stage2L1Trigger.isChosen() or phase2_common.isChosen()):
     print "L1TCalorimeter Sequence configured for Run1 (Legacy) trigger. "
 # -  RCT (Regional Calorimeter Trigger) emulator
     import L1Trigger.RegionalCaloTrigger.rctDigis_cfi
@@ -41,7 +42,7 @@ if stage1L1Trigger.isChosen() and not stage2L1Trigger.isChosen():
 #
 # Stage-2 Trigger
 #
-if stage2L1Trigger.isChosen():
+if (stage2L1Trigger.isChosen() or phase2_common.isChosen()):
     print "L1TCalorimeter Sequence configured for Stage-2 (2016) trigger. "
     # select one of the following two options:
     # - layer1 from L1Trigger/L1TCalorimeter package
