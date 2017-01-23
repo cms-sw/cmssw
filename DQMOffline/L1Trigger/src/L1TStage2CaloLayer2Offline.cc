9b66cd60d40cf696c2c36c415ee87d3a8ba850cf
Luke Kreczko <lkreczko@gmail.com>
2017-01-23 09:43:05 +0000

    implemented davidlange6's comments

--- DQMOffline/L1Trigger/src/L1TStage2CaloLayer2Offline.cc
+++ DQMOffline/L1Trigger/src/L1TStage2CaloLayer2Offline.cc
@@ -9,8 +9,6 @@
 #include "DataFormats/Math/interface/LorentzVector.h"
 #include "TLorentzVector.h"
 
-#include <string>
-
 //
 // -------------------------------------- Constructor --------------------------------------------
 //
