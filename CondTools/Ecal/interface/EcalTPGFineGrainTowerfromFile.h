00de4b4d9d796bed2a8eeb8a4c1e9346e449d25e
Pierre Depasse <depasse@cern.ch>
2017-01-24 17:12:34 +0100

    Reading TPG parameters from TPG_beamv6_trans_xxx_spikekill.txt file

--- CondTools/Ecal/interface/EcalTPGFineGrainTowerfromFile.h
+++ CondTools/Ecal/interface/EcalTPGFineGrainTowerfromFile.h
@@ -1,38 +1,14 @@
 #ifndef ECAL_TPGFINEGRAINTOWER_H
 #define ECAL_TPGFINEGRAINTOWER_H
 
-#include <vector>
-#include <typeinfo>
-#include <string>
-#include <map>
-#include <iostream>
-#include <time.h>
-
 #include "CondCore/PopCon/interface/PopConSourceHandler.h"
-#include "CondCore/DBOutputService/interface/PoolDBOutputService.h"
-#include "DataFormats/Common/interface/Handle.h"
-#include "FWCore/ParameterSet/interface/ParameterSetfwd.h"
-#include "FWCore/ServiceRegistry/interface/Service.h"
+
 #include "FWCore/Framework/interface/ESHandle.h"
 #include "FWCore/Framework/interface/Event.h"
 #include "FWCore/Framework/interface/MakerMacros.h"
-#include "FWCore/MessageLogger/interface/MessageLogger.h"
-#include "FWCore/ParameterSet/interface/ParameterSet.h"
-#include "FWCore/Framework/interface/EventSetup.h"
-#include "FWCore/Framework/interface/EventSetupRecordKey.h"
 
 #include "CondFormats/EcalObjects/interface/EcalTPGFineGrainTowerEE.h"
 #include "CondFormats/DataRecord/interface/EcalTPGFineGrainTowerEERcd.h"
-
-#include "OnlineDB/EcalCondDB/interface/all_monitoring_types.h"
-#include "OnlineDB/EcalCondDB/interface/all_lmf_types.h"
-
-#include "OnlineDB/Oracle/interface/Oracle.h"
-#include "OnlineDB/EcalCondDB/interface/EcalCondDBInterface.h"
-
-#include "DataFormats/EcalDetId/interface/EEDetId.h"
-#include "DataFormats/EcalDetId/interface/EBDetId.h"
-#include "DataFormats/Provenance/interface/Timestamp.h"
 
 namespace edm {
   class ParameterSet;
