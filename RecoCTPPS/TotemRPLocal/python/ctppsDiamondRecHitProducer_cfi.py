641bec03356c6289c5dd999c44a10c668ee900e1
Laurent Forthomme <laurent.forthomme@cern.ch>
2017-01-26 23:53:19 +0100

    Optimised and fixed the geometry ; RecHits are now filled and stored properly

--- RecoCTPPS/TotemRPLocal/python/ctppsDiamondRecHitProducer_cfi.py
+++ RecoCTPPS/TotemRPLocal/python/ctppsDiamondRecHitProducer_cfi.py
@@ -1,8 +1,7 @@
 import FWCore.ParameterSet.Config as cms
 
-ctppsDiamondRecHit = cms.EDProducer("CTPPSDiamondRecHitProducer",
-    verbosity = cms.int32(0),
-    digiTag = cms.InputTag("ctppsDiamondRawToDigi", 'TimingDiamond'),
+ctppsDiamondRecHit = cms.EDProducer('CTPPSDiamondRecHitProducer',
+    digiTag = cms.InputTag('ctppsDiamondRawToDigi', 'TimingDiamond'),
     timeSliceNs = cms.double(25.0/1024.0),
     timeShift = cms.int32(0), #FIXME
 )
