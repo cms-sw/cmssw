75454beb8f9ede90b6cba49bd7daaf3141dd7a2e
mauro verzetti <mverzett@cern.ch>
2017-01-26 15:26:18 +0100

    fix indentation. Fix back-port conflicts

--- PhysicsTools/PatAlgos/plugins/PATJetProducer.cc
+++ PhysicsTools/PatAlgos/plugins/PATJetProducer.cc
@@ -100,11 +100,11 @@ PATJetProducer::PATJetProducer(const edm::ParameterSet& iConfig)  :
         if ((pos !=  std::string::npos) && (pos != label.length() - 7)) {
             label.erase(pos+7); // trim a tail after "JetTags"
         }
-				if(it->instance().size()) {
-					std::stringstream name;
-					name << label << ":" << it->instance();
-					label = name.str();
-				}
+        if(it->instance().size()) {
+            std::stringstream name;
+            name << label << ":" << it->instance();
+            label = name.str();
+        }
         discriminatorLabels_.push_back(label);
     }
   }
