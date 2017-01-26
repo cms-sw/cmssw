873e5dc748ca64d33b6a0704775a8815743c5c9c
Kevin Pedro <kpedro88@gmail.com>
2017-01-26 10:06:50 -0600

    fix regression in SimCalorimetry_EventContent

--- SimCalorimetry/Configuration/python/SimCalorimetry_EventContent_cff.py
+++ SimCalorimetry/Configuration/python/SimCalorimetry_EventContent_cff.py
@@ -1,13 +1,12 @@
 import FWCore.ParameterSet.Config as cms
 
 # This object modifies the event content for different scenarios
-from Configuration.StandardSequences.Eras import eras
 
 SimCalorimetryFEVTDEBUG = cms.PSet(
     outputCommands = cms.untracked.vstring('keep *_simEcalDigis_*_*', 
         'keep *_simEcalPreshowerDigis_*_*', 
         'keep *_simEcalTriggerPrimitiveDigis_*_*', 
-        'keep *_simEcalEBTriggerPrimitiveDigis_*_*', 
+        'keep *_simEcalEBTriggerPrimitiveDigis_*_*',
         'keep *_simHcalDigis_*_*', 
         'keep ZDCDataFramesSorted_simHcalUnsuppressedDigis_*_*',
         'drop ZDCDataFramesSorted_mix_simHcalUnsuppressedDigis*_*',
@@ -29,5 +28,13 @@ SimCalorimetryAOD = cms.PSet(
 #
 # Add extra event content if running in Run 2
 #
-eras.run2_common.toModify( SimCalorimetryFEVTDEBUG.outputCommands, func=lambda outputCommands: outputCommands.append('keep *_simHcalUnsuppressedDigis_*_*') )
-eras.run2_common.toModify( SimCalorimetryRAW.outputCommands, func=lambda outputCommands: outputCommands.append('keep *_simHcalUnsuppressedDigis_*_*') )
+from Configuration.Eras.Modifier_run2_common_cff import run2_common
+run2_common.toModify( SimCalorimetryFEVTDEBUG.outputCommands, func=lambda outputCommands: outputCommands.append('keep *_simHcalUnsuppressedDigis_*_*') )
+run2_common.toModify( SimCalorimetryRAW.outputCommands, func=lambda outputCommands: outputCommands.append('keep *_simHcalUnsuppressedDigis_*_*') )
+
+from Configuration.Eras.Modifier_phase2_timing_cff import phase2_timing
+phase2_timing.toModify(SimCalorimetryFEVTDEBUG.outputCommands, func=lambda outputCommands: outputCommands.append('keep *_mix_EETimeDigi_*') )
+phase2_timing.toModify(SimCalorimetryFEVTDEBUG.outputCommands, func=lambda outputCommands: outputCommands.append('keep *_mix_EBTimeDigi_*') )
+
+phase2_timing.toModify(SimCalorimetryRAW.outputCommands, func=lambda outputCommands: outputCommands.append('keep *_mix_EETimeDigi_*') )
+phase2_timing.toModify(SimCalorimetryRAW.outputCommands, func=lambda outputCommands: outputCommands.append('keep *_mix_EBTimeDigi_*') )
