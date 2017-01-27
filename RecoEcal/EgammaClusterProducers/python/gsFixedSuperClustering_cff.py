dd8387173aab91942c2134ca0fa9590d63df990b
Giovanni <gpetruc@gmail.com>
2017-01-27 11:41:41 +0100

    Don't import modules that we don't want to run

--- RecoEcal/EgammaClusterProducers/python/gsFixedSuperClustering_cff.py
+++ RecoEcal/EgammaClusterProducers/python/gsFixedSuperClustering_cff.py
@@ -1,39 +1,34 @@
 import FWCore.ParameterSet.Config as cms
 
 
-from RecoParticleFlow.PFClusterProducer.particleFlowCluster_cff import particleFlowRecHitECAL
-from RecoParticleFlow.PFClusterProducer.particleFlowCluster_cff import particleFlowClusterECAL
-from RecoParticleFlow.PFClusterProducer.particleFlowCluster_cff import particleFlowClusterECALUncorrected
-from RecoParticleFlow.PFClusterProducer.particleFlowCluster_cff import particleFlowClusterPS
-from RecoParticleFlow.PFClusterProducer.particleFlowCluster_cff import particleFlowRecHitPS
+import RecoParticleFlow.PFClusterProducer.particleFlowCluster_cff
 
 #note I have not been careful in the slightest about the PS clustering as I dont really care about it
 #so it could be completely messed up for all I know
 #likely dont even need to remake them, will prob ditch in later versions
 #I only care about the barrel, endcap could be messed up so be warned
-particleFlowRecHitECALGSFixed=particleFlowRecHitECAL.clone()
-particleFlowRecHitPSGSFixed=particleFlowRecHitPS.clone()
+particleFlowRecHitECALGSFixed=RecoParticleFlow.PFClusterProducer.particleFlowCluster_cff.particleFlowRecHitECAL.clone()
+particleFlowRecHitPSGSFixed=RecoParticleFlow.PFClusterProducer.particleFlowCluster_cff.particleFlowRecHitPS.clone()
 
 particleFlowRecHitECALGSFixed.producers[0].src=cms.InputTag("ecalMultiAndGSWeightRecHitEB")
 particleFlowRecHitECALGSFixed.producers[1].src=cms.InputTag("reducedEcalRecHitsEE")
 particleFlowRecHitPSGSFixed.producers[0].src=cms.InputTag("reducedEcalRecHitsES")
 
 #meh, naming them gainswitched when really its just a re-run on the reduced ES rec hits
-from RecoParticleFlow.PFClusterProducer.particleFlowClusterPS_cfi import particleFlowClusterPS
-particleFlowClusterPSGSFixed = particleFlowClusterPS.clone()
+particleFlowClusterPSGSFixed = RecoParticleFlow.PFClusterProducer.particleFlowCluster_cff.particleFlowClusterPS.clone()
 particleFlowClusterPSGSFixed.recHitsSource = cms.InputTag("particleFlowRecHitPSGSFixed")
 
-particleFlowClusterECALUncorrectedGSFixed = particleFlowClusterECALUncorrected.clone()
+particleFlowClusterECALUncorrectedGSFixed = RecoParticleFlow.PFClusterProducer.particleFlowCluster_cff.particleFlowClusterECALUncorrected.clone()
 particleFlowClusterECALUncorrectedGSFixed.recHitsSource = cms.InputTag("particleFlowRecHitECALGSFixed")
 
-particleFlowClusterECALGSFixed = particleFlowClusterECAL.clone()
+particleFlowClusterECALGSFixed = RecoParticleFlow.PFClusterProducer.particleFlowCluster_cff.particleFlowClusterECAL.clone()
 particleFlowClusterECALGSFixed.energyCorrector.recHitsEBLabel=cms.InputTag("ecalMultiAndGSWeightRecHitEB")
 particleFlowClusterECALGSFixed.energyCorrector.recHitsEELabel=cms.InputTag("reducedEcalRecHitsEE")
 particleFlowClusterECALGSFixed.inputECAL = cms.InputTag("particleFlowClusterECALUncorrectedGSFixed")
 particleFlowClusterECALGSFixed.inputPS = cms.InputTag("particleFlowClusterPSGSFixed")
 
-from RecoEcal.EgammaClusterProducers.particleFlowSuperClusteringSequence_cff import particleFlowSuperClusterECAL
-particleFlowSuperClusterECALGSFixed=particleFlowSuperClusterECAL.clone()
+import RecoEcal.EgammaClusterProducers.particleFlowSuperClusteringSequence_cff
+particleFlowSuperClusterECALGSFixed=RecoEcal.EgammaClusterProducers.particleFlowSuperClusteringSequence_cff.particleFlowSuperClusterECAL.clone()
 particleFlowSuperClusterECALGSFixed.regressionConfig.ecalRecHitsEB=cms.InputTag("ecalMultiAndGSWeightRecHitEB")
 particleFlowSuperClusterECALGSFixed.regressionConfig.ecalRecHitsEE=cms.InputTag("reducedEcalRecHitsEE")
 particleFlowSuperClusterECALGSFixed.PFClusters = cms.InputTag("particleFlowClusterECALGSFixed")
--- RecoEgamma/EgammaTools/python/ecalWeightRecHitFromSelectedDigis_cff.py
+++ RecoEgamma/EgammaTools/python/ecalWeightRecHitFromSelectedDigis_cff.py
@@ -1,12 +1,13 @@
 import FWCore.ParameterSet.Config as cms
 
-from RecoLocalCalo.EcalRecProducers.ecalWeightUncalibRecHit_cfi import ecalWeightUncalibRecHit
-ecalWeightUncalibRecHitSelectedDigis = ecalWeightUncalibRecHit.clone()
+import RecoLocalCalo.EcalRecProducers.ecalWeightUncalibRecHit_cfi
+
+ecalWeightUncalibRecHitSelectedDigis = RecoLocalCalo.EcalRecProducers.ecalWeightUncalibRecHit_cfi.ecalWeightUncalibRecHit.clone()
 ecalWeightUncalibRecHitSelectedDigis.EBdigiCollection = cms.InputTag("selectDigi","selectedEcalEBDigiCollection")
 ecalWeightUncalibRecHitSelectedDigis.EEdigiCollection = cms.InputTag("selectDigi","selectedEcalEEDigiCollection")
 
-from RecoLocalCalo.EcalRecProducers.ecalRecHit_cfi import ecalRecHit
-ecalWeightRecHitSelectedDigis = ecalRecHit.clone()
+import RecoLocalCalo.EcalRecProducers.ecalRecHit_cfi
+ecalWeightRecHitSelectedDigis = RecoLocalCalo.EcalRecProducers.ecalRecHit_cfi.ecalRecHit.clone()
 ecalWeightRecHitSelectedDigis.EEuncalibRecHitCollection = cms.InputTag("ecalWeightUncalibRecHitSelectedDigis","EcalUncalibRecHitsEE")
 ecalWeightRecHitSelectedDigis.EBuncalibRecHitCollection = cms.InputTag("ecalWeightUncalibRecHitSelectedDigis","EcalUncalibRecHitsEB")
 ecalWeightRecHitSelectedDigis.recoverEBFE = cms.bool(False)
