fca2430ff6846777a5ea3d13edcf918ad427d9e3
Sam Harper <sam.j.harper@gmail.com>
2017-01-30 16:34:13 +0000

    clean up

--- RecoEcal/EgammaClusterProducers/python/gsBrokenToGSFixedSuperClustersMap_cfi.py
+++
@@ -1,8 +0,0 @@
-import FWCore.ParameterSet.Config as cms
-                                                      
-gsBrokenToGSFixedSuperClustersMap = cms.EDProducer("MapNewToOldSCs",
-                                                   oldSC=cms.InputTag("particleFlowSuperClusterECAL","particleFlowSuperClusterECALBarrel",processName=cms.InputTag.skipCurrentProcess()),
-                                                   newSC=cms.InputTag("particleFlowSuperClusterECALGSFixed","particleFlowSuperClusterECALBarrel"),
-                                                   oldRefinedSC=cms.InputTag("particleFlowEGamma",processName=cms.InputTag.skipCurrentProcess()),
-                                                   newRefinedSC=cms.InputTag("gsFixedRefinedBarrelSuperClusters")
-                                                   )
