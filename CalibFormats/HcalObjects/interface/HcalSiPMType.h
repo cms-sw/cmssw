138dac0575610ed91d6900331814539e742a3263
Kevin Pedro <kpedro88@gmail.com>
2017-01-30 07:05:55 -0600

    add HPD to SiPM type list

--- CalibFormats/HcalObjects/interface/HcalSiPMType.h
+++ CalibFormats/HcalObjects/interface/HcalSiPMType.h
@@ -3,6 +3,6 @@
 
 enum HcalSiPMType {HcalNoSiPM=0, HcalHOZecotek=1, HcalHOHamamatsu=2, 
 		   HcalHEHamamatsu1=3, HcalHEHamamatsu2=4, HcalHBHamamatsu1=5,
-		   HcalHBHamamatsu2=6};
+		   HcalHBHamamatsu2=6, HcalHPD=7};
 
 #endif
