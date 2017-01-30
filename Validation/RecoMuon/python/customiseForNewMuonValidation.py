2ccd77b4e02d0ed13760527c6a7b7b9df63f0b6b
Carlo Battilana <carlo.battilana@gmail.com>
2017-01-30 14:31:37 +0100

    Add customise on RAW-RECO

--- Validation/RecoMuon/python/customiseForNewMuonValidation.py
+++ Validation/RecoMuon/python/customiseForNewMuonValidation.py
@@ -1,6 +1,7 @@
 import FWCore.ParameterSet.Config as cms
 
 def enableNewMuonVal(process):
+    "Enable new muon validation sequence, both in step3 and harvesting"    
     
     if ( hasattr(process,"validation") or  \
          hasattr(process,"globalValidation") ) and \
@@ -18,7 +19,7 @@ def enableNewMuonVal(process):
             process.validation.replace(process.recoMuonValidation, \
                                        process.NEWrecoMuonValidation)
 
-        # CB I think probeTks need to accept loose
+    probeTracks.quality = cms.vstring('loose')
 
     if hasattr(process,"hltvalidation") and \
        hasattr(process,"recoMuonValidation") :
@@ -65,4 +66,57 @@ def enableNewMuonVal(process):
     
 
 
+
+def runMuonValForTesting(process):
+    "Customise step3 to run only muon validation (expects both step2 and step 3 files in input)"
+
+    from Validation.RecoMuon.muonValidation_cff import *
+
+    muonValidation_seq.remove(trackAssociatorByHits)
+    muonValidation_seq.remove(tpToTkmuTrackAssociation)
+    muonValidation_seq.remove(trkMuonTrackVTrackAssoc)
+    #process.muonValidation_seq.remove(process.muonValidationRMV_seq)
+
+    process.recoMuonValidationForTesting_seq = cms.Sequence( muonValidation_seq 
+                                                             + muonValidationTEV_seq 
+                                                             + muonValidationRefit_seq 
+                                                             )
+
+    process.validationForTesting = cms.Sequence( cms.SequencePlaceholder("mix") 
+                                                 + process.recoMuonValidationForTesting_seq 
+                                                 + process.recoMuonValidationHLT_seq
+                                               )
+
+    process.outputForTesting = cms.OutputModule("PoolOutputModule",
+                                                fileName = cms.untracked.string('events.root'),
+                                                splitLevel = cms.untracked.int32(0)
+                                                )
+
+    if hasattr(process,"validation_step") :
+        print "[runMuonValForTesting] : customising validation"
+        process.validation_step = cms.Path(process.validationForTesting)
+
+    # CB what to do with this?
+    #if hasattr(process,"output_step") :
+    #    print "[runMuonValForTesting] : customising output"
+    #    process.output_step = cms.EndPath(process.outputForTesting)
+
+    process.schedule = cms.Schedule(process.validation_step,process.DQMoutput_step)
+
+    return process
+    
+
+
+
+def runMuonHarvestingForTesting(process):
+    "Customise harvesting to run only muon validation"
+
+    print "[runMuonValForTesting] : customising harvesting"
+
+    process.validationHarvestingForTesting = cms.Path(process.recoMuonPostProcessors
+                                                      + process.recoMuonPostProcessorsHLT)
+
+    process.schedule = cms.Schedule(process.validationHarvestingForTesting,process.dqmsave_step)
+
+    return process
     
