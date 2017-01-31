126dab8310f9025d4276ef4555c16e8642c05a3e
Laurent Forthomme <laurent.forthomme@cern.ch>
2017-01-31 08:44:18 +0100

    Added the CTPPSDiamondLocalTrack object
    Conflicts:
    	DataFormats/CTPPSReco/interface/CTPPSDiamondRecHit.h
    	DataFormats/CTPPSReco/src/classes.h
    	DataFormats/CTPPSReco/src/classes_def.xml
    	RecoCTPPS/TotemRPLocal/plugins/CTPPSDiamondRecHitProducer.cc

---
+++ DataFormats/CTPPSReco/src/CTPPSDiamondLocalTrack.cc
@@ -0,0 +1,19 @@
+/****************************************************************************
+ *
+ * This is a part of CTPPS offline software.
+ * Authors:
+ *   Laurent Forthomme (laurent.forthomme@cern.ch)
+ *   Nicola Minafra (nicola.minafra@cern.ch)
+ *
+ ****************************************************************************/
+
+#include "DataFormats/CTPPSReco/interface/CTPPSDiamondLocalTrack.h"
+
+//----------------------------------------------------------------------------------------------------
+
+bool
+operator<( const CTPPSDiamondLocalTrack& lhs, const CTPPSDiamondLocalTrack& rhs )
+{
+  //FIXME also sort by space coordinate?
+  return ( lhs.getT() < rhs.getT() );
+}
