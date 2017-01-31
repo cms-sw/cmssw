c6926bbc50e24f3573b27ef9abc18a02277b1e55
Laurent Forthomme <laurent.forthomme@cern.ch>
2017-01-31 11:02:48 +0100

    New constructor for the track recognition algorithm object ; plugging it to the local tracks' EDProducer
    Conflicts:
    	RecoCTPPS/TotemRPLocal/python/ctppsDiamondLocalReconstruction_cff.py
    	RecoCTPPS/TotemRPLocal/test/diamonds_reco_cfg.py

--- RecoCTPPS/TotemRPLocal/python/ctppsDiamondLocalTrackFitter_cfi.py
+++ RecoCTPPS/TotemRPLocal/python/ctppsDiamondLocalTrackFitter_cfi.py
@@ -2,5 +2,10 @@ import FWCore.ParameterSet.Config as cms
 
 ctppsDiamondLocalTrack = cms.EDProducer("CTPPSDiamondLocalTrackFitter",
     verbosity = cms.int32(0),
-    recHitLabel = cms.InputTag("ctppsDiamondRecHit"),
+    recHitsTag = cms.InputTag("ctppsDiamondRecHit"),
+    trackingAlgorithmParams = cms.PSet(
+        threshold = cms.double(2.0),
+        resolution = cms.double(0.01),
+        sigma = cms.double(0.0),
+    ),
 )
