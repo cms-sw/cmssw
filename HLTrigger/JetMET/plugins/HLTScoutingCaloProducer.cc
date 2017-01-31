302d8a35345e3f65997870b26fe38e931816cda8
Swagata Mukherjee <s.mukherjee@cern.ch>
2017-01-31 11:24:06 +0100

    scouting code clean-up

--- HLTrigger/JetMET/plugins/HLTScoutingCaloProducer.cc
+++ HLTrigger/JetMET/plugins/HLTScoutingCaloProducer.cc
@@ -79,7 +79,6 @@ HLTScoutingCaloProducer::HLTScoutingCaloProducer(const edm::ParameterSet& iConfi
 {
     //register products
     produces<ScoutingCaloJetCollection>();
-    //produces<ScoutingVertexCollection>();
     produces<double>("rho");
     produces<double>("caloMetPt");
     produces<double>("caloMetPhi");
@@ -145,18 +144,6 @@ HLTScoutingCaloProducer::produce(edm::StreamID sid, edm::Event & iEvent, edm::Ev
             }
         }
     }
-
-    //get vertices
-    //    Handle<reco::VertexCollection> vertexCollection;
-    //  std::unique_ptr<ScoutingVertexCollection> outVertices(new ScoutingVertexCollection());
-    // if(iEvent.getByToken(vertexCollection_, vertexCollection)){
-        //produce vertices (only if present; otherwise return an empty collection)
-    //  for(auto &vtx : *vertexCollection){
-    //      outVertices->emplace_back(
-    //				      vtx.x(), vtx.y(), vtx.z(), vtx.zError(), vtx.xError(), vtx.yError(), vtx.tracksSize(), vtx.chi2(), vtx.ndof(), vtx.isValid()
-    //                  );
-    //  }
-    // }
 
     //get rho
     Handle<double>rho;
