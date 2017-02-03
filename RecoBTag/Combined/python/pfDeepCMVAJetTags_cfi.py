9c2159773e006008ad687d9967b90fbaf0013ae3
mauro verzetti <mverzett@cern.ch>
2017-02-03 17:34:18 +0100

    implemented PR #17247 comments

--- RecoBTag/Combined/python/pfDeepCMVAJetTags_cfi.py
+++ RecoBTag/Combined/python/pfDeepCMVAJetTags_cfi.py
@@ -1,10 +1,4 @@
 import FWCore.ParameterSet.Config as cms
-
-pfDeepCSVJetTags = cms.EDProducer(
-	'DeepFlavourJetTagsProducer',
-	src = cms.InputTag('pfDeepCSVTagInfos'),
-	NNConfig = cms.FileInPath('RecoBTag/Combined/data/DeepFlavourNoSL.json')
-	)
 	
 pfDeepCMVAJetTags = cms.EDProducer(
 	'DeepFlavourJetTagsProducer',
