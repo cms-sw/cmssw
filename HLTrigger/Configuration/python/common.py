2b76dbbe84033a79c4d6d017e9a03ef939cd2421
Andrea Bocci <fwyzard@gmail.com>
2017-02-03 15:53:59 +0100

    add the functionality of Modifier.toReplaceWith outside of Eras

--- HLTrigger/Configuration/python/common.py
+++ HLTrigger/Configuration/python/common.py
@@ -16,3 +16,30 @@ def esproducers_by_type(process, *types):
     "Find all ESProducers in the Process that are instances of the given C++ type."
     return (module for module in process._Process__esproducers.values() if module._TypedParameterizable__type in types)
 
+
+# logic from Modifier.toModify from FWCore/ParameterSet/python/Config.py
+def replace_with(fromObj, toObj):
+    """Replace one object with a different one of the same type.
+
+    This function replaces the contents of `fromObj` object with those of `toObj`,
+    so all references ot it remain valid.
+    """
+
+    if type(toObj) != type(fromObj):
+        raise TypeError('replaceWith requires both arguments to be the same type')
+
+    if isinstance(toObj, cms._ModuleSequenceType):
+        fromObj._seq = toObj._seq
+
+    elif isinstance(toObj, cms._Parameterizable):
+        # delete the old items, in case `toObj` is not a complete superset of `fromObj`
+        for p in fromObj.parameterNames_():
+            delattr(fromObj, p)
+        for p in toObj.parameterNames_():
+            setattr(fromObj, p, getattr(toObj, p))
+        if isinstance(toObj, cms._TypedParameterizable):
+            fromObj._TypedParameterizable__type = toObj._TypedParameterizable__type
+
+    else:
+        raise TypeError('replaceWith does not work with "%s" objects' % str(type(fromObj)))
+
