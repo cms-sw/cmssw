71ab1a60be183d518edb26766d1b9f9f1af49bd9
Kyle Knoepfel <knoepfel@fnal.gov>
2017-02-06 10:32:08 -0600

    Whitespace changes only.

--- DQMOffline/Trigger/src/HLTTauRefProducer.cc
+++ DQMOffline/Trigger/src/HLTTauRefProducer.cc
@@ -33,7 +33,7 @@ using namespace std;
 HLTTauRefProducer::HLTTauRefProducer(const edm::ParameterSet& iConfig)
 {
 
- 
+
   //One Parameter Set per Collection
 
   ParameterSet pfTau = iConfig.getUntrackedParameter<edm::ParameterSet>("PFTaus");
@@ -91,7 +91,7 @@ HLTTauRefProducer::HLTTauRefProducer(const edm::ParameterSet& iConfig)
 
 
   etaMax = iConfig.getUntrackedParameter<double>("EtaMax",2.5);
-  
+
 
   //recoCollections
   produces<LorentzVectorCollection>("PFTaus");
@@ -124,52 +124,52 @@ void HLTTauRefProducer::produce(edm::Event& iEvent, const edm::EventSetup& iES)
     doMET(iEvent,iES);
 }
 
-void 
+void
 HLTTauRefProducer::doPFTaus(edm::Event& iEvent,const edm::EventSetup& iES)
 {
-      unique_ptr<LorentzVectorCollection> product_PFTaus(new LorentzVectorCollection);
-      //Retrieve the collection
-      edm::Handle<PFTauCollection> pftaus;
-      if(iEvent.getByToken(PFTaus_,pftaus)) {
-        for(unsigned int i=0; i<pftaus->size(); ++i) {
-	    if((*pftaus)[i].pt()>ptMinPFTau_&&fabs((*pftaus)[i].eta())<etaMax)
-	      {
-		reco::PFTauRef thePFTau(pftaus,i);
-                bool passAll = true;
-                for(edm::EDGetTokenT<reco::PFTauDiscriminator>& token: PFTauDis_) {
-		  edm::Handle<reco::PFTauDiscriminator> pftaudis;
-		  if(iEvent.getByToken(token, pftaudis)) {
-                    if((*pftaudis)[thePFTau] < 0.5) {
-                      passAll = false;
-                      break;
-                    }
-                  }else{
-                      passAll = false;
-                      break;
-                  }
-                }
-                if(passAll) {
-                  LorentzVector vec((*pftaus)[i].px(),(*pftaus)[i].py(),(*pftaus)[i].pz(),(*pftaus)[i].energy());
-                  product_PFTaus->push_back(vec);
-                }
+  unique_ptr<LorentzVectorCollection> product_PFTaus(new LorentzVectorCollection);
+  //Retrieve the collection
+  edm::Handle<PFTauCollection> pftaus;
+  if(iEvent.getByToken(PFTaus_,pftaus)) {
+    for(unsigned int i=0; i<pftaus->size(); ++i) {
+      if((*pftaus)[i].pt()>ptMinPFTau_&&fabs((*pftaus)[i].eta())<etaMax)
+        {
+          reco::PFTauRef thePFTau(pftaus,i);
+          bool passAll = true;
+          for(edm::EDGetTokenT<reco::PFTauDiscriminator>& token: PFTauDis_) {
+            edm::Handle<reco::PFTauDiscriminator> pftaudis;
+            if(iEvent.getByToken(token, pftaudis)) {
+              if((*pftaudis)[thePFTau] < 0.5) {
+                passAll = false;
+                break;
               }
+            }else{
+              passAll = false;
+              break;
+            }
+          }
+          if(passAll) {
+            LorentzVector vec((*pftaus)[i].px(),(*pftaus)[i].py(),(*pftaus)[i].pz(),(*pftaus)[i].energy());
+            product_PFTaus->push_back(vec);
+          }
         }
-      }
-      iEvent.put(std::move(product_PFTaus),"PFTaus");
+    }
+  }
+  iEvent.put(std::move(product_PFTaus),"PFTaus");
 }
 
 
-void 
+void
 HLTTauRefProducer::doElectrons(edm::Event& iEvent,const edm::EventSetup& iES)
 {
   unique_ptr<LorentzVectorCollection> product_Electrons(new LorentzVectorCollection);
   //Retrieve the collections
-  
+
   edm::Handle<reco::ElectronIDAssociationCollection> pEleID;
   if(e_doID_){//UGLY HACK UNTIL GET ELETRON ID WORKING IN 210
-   
-      iEvent.getByToken(e_idAssocProd_, pEleID);
-    
+
+    iEvent.getByToken(e_idAssocProd_, pEleID);
+
     if (!pEleID.isValid()){
       edm::LogInfo("")<< "Error! Can't get electronIDAssocProducer by label. ";
       e_doID_ = false;
@@ -178,148 +178,148 @@ HLTTauRefProducer::doElectrons(edm::Event& iEvent,const edm::EventSetup& iES)
   edm::Handle<reco::TrackCollection> pCtfTracks;
   iEvent.getByToken(e_ctfTrackCollection_, pCtfTracks);
   if (!pCtfTracks.isValid()) {
-  edm::LogInfo("")<< "Error! Can't get " << e_ctfTrackCollectionSrc_.label() << " by label. ";
-  iEvent.put(std::move(product_Electrons),"Electrons"); 
-  return;
+    edm::LogInfo("")<< "Error! Can't get " << e_ctfTrackCollectionSrc_.label() << " by label. ";
+    iEvent.put(std::move(product_Electrons),"Electrons");
+    return;
   }
   const reco::TrackCollection * ctfTracks = pCtfTracks.product();
   edm::Handle<GsfElectronCollection> electrons;
   if(iEvent.getByToken(Electrons_,electrons))
     for(size_t i=0;i<electrons->size();++i)
       {
-	edm::Ref<reco::GsfElectronCollection> electronRef(electrons,i);
-	bool idDec=false;
-	if(e_doID_){
-	  reco::ElectronIDAssociationCollection::const_iterator tagIDAssocItr;
-	  tagIDAssocItr = pEleID->find(electronRef);
-	  const reco::ElectronIDRef& id_tag = tagIDAssocItr->val;
-	  idDec=id_tag->cutBasedDecision();
-	}else idDec=true;
-	if((*electrons)[i].pt()>ptMinElectron_&&fabs((*electrons)[i].eta())<etaMax&&idDec)
-	  {
-	    if(e_doTrackIso_){
-	      reco::TrackCollection::const_iterator tr = ctfTracks->begin();
-	      double sum_of_pt_ele=0;
-	      for(;tr != ctfTracks->end();++tr)
-		{
-		  double lip = (*electrons)[i].gsfTrack()->dz() - tr->dz();
-		  if(tr->pt() > e_trackMinPt_ && fabs(lip) < e_lipCut_){
-		    double dphi=fabs(tr->phi()-(*electrons)[i].trackMomentumAtVtx().phi());
-		    if(dphi>acos(-1.))dphi=2*acos(-1.)-dphi;
-		    double deta=fabs(tr->eta()-(*electrons)[i].trackMomentumAtVtx().eta());
-		    double dr_ctf_ele = sqrt(deta*deta+dphi*dphi);
-		    if((dr_ctf_ele>e_minIsoDR_) && (dr_ctf_ele<e_maxIsoDR_)){
-		      double cft_pt_2 = (tr->pt())*(tr->pt());
-		      sum_of_pt_ele += cft_pt_2;
-		    }
-		  }
-		}
-	      double isolation_value_ele = sum_of_pt_ele/((*electrons)[i].trackMomentumAtVtx().Rho()*(*electrons)[i].trackMomentumAtVtx().Rho());
-	      if(isolation_value_ele<e_isoMaxSumPt_){
-		LorentzVector vec((*electrons)[i].px(),(*electrons)[i].py(),(*electrons)[i].pz(),(*electrons)[i].energy());
-		product_Electrons->push_back(vec);
-	      } 
-	      
-	    }
-	    else{ 
-	      LorentzVector vec((*electrons)[i].px(),(*electrons)[i].py(),(*electrons)[i].pz(),(*electrons)[i].energy());
-	      product_Electrons->push_back(vec);
-	    }
-	  }
+        edm::Ref<reco::GsfElectronCollection> electronRef(electrons,i);
+        bool idDec=false;
+        if(e_doID_){
+          reco::ElectronIDAssociationCollection::const_iterator tagIDAssocItr;
+          tagIDAssocItr = pEleID->find(electronRef);
+          const reco::ElectronIDRef& id_tag = tagIDAssocItr->val;
+          idDec=id_tag->cutBasedDecision();
+        }else idDec=true;
+        if((*electrons)[i].pt()>ptMinElectron_&&fabs((*electrons)[i].eta())<etaMax&&idDec)
+          {
+            if(e_doTrackIso_){
+              reco::TrackCollection::const_iterator tr = ctfTracks->begin();
+              double sum_of_pt_ele=0;
+              for(;tr != ctfTracks->end();++tr)
+                {
+                  double lip = (*electrons)[i].gsfTrack()->dz() - tr->dz();
+                  if(tr->pt() > e_trackMinPt_ && fabs(lip) < e_lipCut_){
+                    double dphi=fabs(tr->phi()-(*electrons)[i].trackMomentumAtVtx().phi());
+                    if(dphi>acos(-1.))dphi=2*acos(-1.)-dphi;
+                    double deta=fabs(tr->eta()-(*electrons)[i].trackMomentumAtVtx().eta());
+                    double dr_ctf_ele = sqrt(deta*deta+dphi*dphi);
+                    if((dr_ctf_ele>e_minIsoDR_) && (dr_ctf_ele<e_maxIsoDR_)){
+                      double cft_pt_2 = (tr->pt())*(tr->pt());
+                      sum_of_pt_ele += cft_pt_2;
+                    }
+                  }
+                }
+              double isolation_value_ele = sum_of_pt_ele/((*electrons)[i].trackMomentumAtVtx().Rho()*(*electrons)[i].trackMomentumAtVtx().Rho());
+              if(isolation_value_ele<e_isoMaxSumPt_){
+                LorentzVector vec((*electrons)[i].px(),(*electrons)[i].py(),(*electrons)[i].pz(),(*electrons)[i].energy());
+                product_Electrons->push_back(vec);
+              }
+
+            }
+            else{
+              LorentzVector vec((*electrons)[i].px(),(*electrons)[i].py(),(*electrons)[i].pz(),(*electrons)[i].energy());
+              product_Electrons->push_back(vec);
+            }
+          }
       }
-  
-  iEvent.put(std::move(product_Electrons),"Electrons"); 
+
+  iEvent.put(std::move(product_Electrons),"Electrons");
 }
 
-void 
+void
 HLTTauRefProducer::doMuons(edm::Event& iEvent,const edm::EventSetup& iES)
 {
   unique_ptr<LorentzVectorCollection> product_Muons(new LorentzVectorCollection);
   //Retrieve the collection
   edm::Handle<MuonCollection> muons;
-      if(iEvent.getByToken(Muons_,muons))
-   
-      for(size_t i = 0 ;i<muons->size();++i)
-	{
-	 
-	    if((*muons)[i].pt()>ptMinMuon_&&fabs((*muons)[i].eta())<etaMax)
-	      {
-		LorentzVector vec((*muons)[i].px(),(*muons)[i].py(),(*muons)[i].pz(),(*muons)[i].energy());
-		product_Muons->push_back(vec);
-	      }
-	}
+  if(iEvent.getByToken(Muons_,muons))
+
+    for(size_t i = 0 ;i<muons->size();++i)
+      {
+
+        if((*muons)[i].pt()>ptMinMuon_&&fabs((*muons)[i].eta())<etaMax)
+          {
+            LorentzVector vec((*muons)[i].px(),(*muons)[i].py(),(*muons)[i].pz(),(*muons)[i].energy());
+            product_Muons->push_back(vec);
+          }
+      }
 
 
-      iEvent.put(std::move(product_Muons),"Muons");
- 
+  iEvent.put(std::move(product_Muons),"Muons");
+
 }
 
 
-void 
+void
 HLTTauRefProducer::doJets(edm::Event& iEvent,const edm::EventSetup& iES)
 {
-      unique_ptr<LorentzVectorCollection> product_Jets(new LorentzVectorCollection);
-      //Retrieve the collection
-      edm::Handle<CaloJetCollection> jets;
-      if(iEvent.getByToken(Jets_,jets))
-      for(size_t i = 0 ;i<jets->size();++i)
-	{
-	     if((*jets)[i].et()>ptMinJet_&&fabs((*jets)[i].eta())<etaMax)
-	      {
-		LorentzVector vec((*jets)[i].px(),(*jets)[i].py(),(*jets)[i].pz(),(*jets)[i].energy());
-		product_Jets->push_back(vec);
-	      }
-	}
-      iEvent.put(std::move(product_Jets),"Jets");
+  unique_ptr<LorentzVectorCollection> product_Jets(new LorentzVectorCollection);
+  //Retrieve the collection
+  edm::Handle<CaloJetCollection> jets;
+  if(iEvent.getByToken(Jets_,jets))
+    for(size_t i = 0 ;i<jets->size();++i)
+      {
+        if((*jets)[i].et()>ptMinJet_&&fabs((*jets)[i].eta())<etaMax)
+          {
+            LorentzVector vec((*jets)[i].px(),(*jets)[i].py(),(*jets)[i].pz(),(*jets)[i].energy());
+            product_Jets->push_back(vec);
+          }
+      }
+  iEvent.put(std::move(product_Jets),"Jets");
 }
 
-void 
+void
 HLTTauRefProducer::doTowers(edm::Event& iEvent,const edm::EventSetup& iES)
 {
-      unique_ptr<LorentzVectorCollection> product_Towers(new LorentzVectorCollection);
-      //Retrieve the collection
-      edm::Handle<CaloTowerCollection> towers;
-      if(iEvent.getByToken(Towers_,towers))
-      for(size_t i = 0 ;i<towers->size();++i)
-	{
-	     if((*towers)[i].pt()>ptMinTower_&&fabs((*towers)[i].eta())<etaMax)
-	      {
-		//calculate isolation
-		double isolET=0;
-		for(unsigned int j=0;j<towers->size();++j)
-		  {
-		    if(ROOT::Math::VectorUtil::DeltaR((*towers)[i].p4(),(*towers)[j].p4())<0.5)
-		      isolET+=(*towers)[j].pt();
-		  }
-		isolET-=(*towers)[i].pt();
-		if(isolET<towerIsol_)
-		  {
-		    LorentzVector vec((*towers)[i].px(),(*towers)[i].py(),(*towers)[i].pz(),(*towers)[i].energy());
-		    product_Towers->push_back(vec);
-		  }
-	      }
-	}
-      iEvent.put(std::move(product_Towers),"Towers");
+  unique_ptr<LorentzVectorCollection> product_Towers(new LorentzVectorCollection);
+  //Retrieve the collection
+  edm::Handle<CaloTowerCollection> towers;
+  if(iEvent.getByToken(Towers_,towers))
+    for(size_t i = 0 ;i<towers->size();++i)
+      {
+        if((*towers)[i].pt()>ptMinTower_&&fabs((*towers)[i].eta())<etaMax)
+          {
+            //calculate isolation
+            double isolET=0;
+            for(unsigned int j=0;j<towers->size();++j)
+              {
+                if(ROOT::Math::VectorUtil::DeltaR((*towers)[i].p4(),(*towers)[j].p4())<0.5)
+                  isolET+=(*towers)[j].pt();
+              }
+            isolET-=(*towers)[i].pt();
+            if(isolET<towerIsol_)
+              {
+                LorentzVector vec((*towers)[i].px(),(*towers)[i].py(),(*towers)[i].pz(),(*towers)[i].energy());
+                product_Towers->push_back(vec);
+              }
+          }
+      }
+  iEvent.put(std::move(product_Towers),"Towers");
 }
 
 
-void 
+void
 HLTTauRefProducer::doPhotons(edm::Event& iEvent,const edm::EventSetup& iES)
 {
-      unique_ptr<LorentzVectorCollection> product_Gammas(new LorentzVectorCollection);
-      //Retrieve the collection
-      edm::Handle<PhotonCollection> photons;
-      if(iEvent.getByToken(Photons_,photons))
-      for(size_t i = 0 ;i<photons->size();++i)
-	if((*photons)[i].ecalRecHitSumEtConeDR04()<photonEcalIso_)
-	{
-	     if((*photons)[i].et()>ptMinPhoton_&&fabs((*photons)[i].eta())<etaMax)
-	      {
-		LorentzVector vec((*photons)[i].px(),(*photons)[i].py(),(*photons)[i].pz(),(*photons)[i].energy());
-		product_Gammas->push_back(vec);
-	      }
-	}
-      iEvent.put(std::move(product_Gammas),"Photons");
+  unique_ptr<LorentzVectorCollection> product_Gammas(new LorentzVectorCollection);
+  //Retrieve the collection
+  edm::Handle<PhotonCollection> photons;
+  if(iEvent.getByToken(Photons_,photons))
+    for(size_t i = 0 ;i<photons->size();++i)
+      if((*photons)[i].ecalRecHitSumEtConeDR04()<photonEcalIso_)
+        {
+          if((*photons)[i].et()>ptMinPhoton_&&fabs((*photons)[i].eta())<etaMax)
+            {
+              LorentzVector vec((*photons)[i].px(),(*photons)[i].py(),(*photons)[i].pz(),(*photons)[i].energy());
+              product_Gammas->push_back(vec);
+            }
+        }
+  iEvent.put(std::move(product_Gammas),"Photons");
 }
 
 void
@@ -335,6 +335,5 @@ HLTTauRefProducer::doMET(edm::Event& iEvent,const edm::EventSetup& iES)
     LorentzVector vec(px,py,0,pt);
     product_MET->push_back(vec);
   }
-  iEvent.put(std::move(product_MET),"MET");  
+  iEvent.put(std::move(product_MET),"MET");
 }
-
