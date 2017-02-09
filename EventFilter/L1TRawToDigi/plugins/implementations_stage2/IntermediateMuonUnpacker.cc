044515dc9b8ae5467d0fa81840e160dfe6c8827b
Thomas Reis <thomas.reis@cern.ch>
2017-02-09 17:21:47 +0100

    L1 muon unpacker for 2017 RAW data format

--- EventFilter/L1TRawToDigi/plugins/implementations_stage2/IntermediateMuonUnpacker.cc
+++ EventFilter/L1TRawToDigi/plugins/implementations_stage2/IntermediateMuonUnpacker.cc
@@ -100,8 +100,10 @@ namespace l1t {
                }
 
                Muon mu;
-                   
-               MuonRawDigiTranslator::fillMuon(mu, raw_data_00_31, raw_data_32_63);
+
+               // The intermediate muons do not have coordinates estimated at the vertex in the RAW data
+               // Setting FW version to 0 makes the unpacker use the 2016 RAW format
+               MuonRawDigiTranslator::fillMuon(mu, raw_data_00_31, raw_data_32_63, 1402, 0);
 
                LogDebug("L1T") << "Mu" << nWord/2 << ": eta " << mu.hwEta() << " phi " << mu.hwPhi() << " pT " << mu.hwPt() << " iso " << mu.hwIso() << " qual " << mu.hwQual() << " charge " << mu.hwCharge() << " charge valid " << mu.hwChargeValid();
 
