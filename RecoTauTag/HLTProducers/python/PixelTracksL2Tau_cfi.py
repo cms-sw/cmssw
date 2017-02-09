95e2e20b110ac8fcacf9f28709ec5deab26b5914
Matti Kortelainen <matti.kortelainen@cern.ch>
2017-02-09 09:51:55 +0100

    Move definition of pixelTracks to its own cfi file, import that instead of pixelTracksDefault

--- RecoTauTag/HLTProducers/python/PixelTracksL2Tau_cfi.py
+++ RecoTauTag/HLTProducers/python/PixelTracksL2Tau_cfi.py
@@ -1,9 +1,9 @@
 import FWCore.ParameterSet.Config as cms
 
-from RecoPixelVertexing.PixelTrackFitting.pixelTracksDefault_cfi import *
+from RecoPixelVertexing.PixelTrackFitting.pixelTracks_cfi import pixelTracks as _pixelTracks
 from RecoTauTag.HLTProducers.TrackingRegionsFromBeamSpotAndL2Tau_cfi import *
 
-pixelTracksL2Tau = pixelTracksDefault.clone()
+pixelTracksL2Tau = _pixelTracks.clone()
 pixelTracksL2Tau.RegionFactoryPSet = cms.PSet(
     TrackingRegionsFromBeamSpotAndL2TauBlock,
     ComponentName = cms.string( "TrackingRegionsFromBeamSpotAndL2Tau" )
