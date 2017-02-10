518c1dfd7b9f1b1c90be2286e34b8146dd43e74b
Giovanni Abbiendi <Giovanni.Abbiendi@cern.ch>
2017-02-10 13:44:52 +0100

    revert back the old configuration files (to stay untouched)

--- Validation/RecoHI/python/muonValidationHeavyIons_cff.py
+++ Validation/RecoHI/python/muonValidationHeavyIons_cff.py
@@ -1,7 +1,20 @@
 import FWCore.ParameterSet.Config as cms
 
 from Validation.RecoMuon.muonValidation_cff import *
-from Validation.RecoHI.track_selectors_cff import *
+
+# pt-selection of reco tracks
+import PhysicsTools.RecoAlgos.recoTrackSelector_cfi
+cutsRecoTrkMuons = PhysicsTools.RecoAlgos.recoTrackSelector_cfi.recoTrackSelector.clone()
+cutsRecoTrkMuons.src = "hiGeneralTracks"
+cutsRecoTrkMuons.quality = []
+cutsRecoTrkMuons.ptMin = 0.0
+
+# pt-selection of tracking particles
+import PhysicsTools.RecoAlgos.trackingParticleSelector_cfi
+cutsTpMuons = PhysicsTools.RecoAlgos.trackingParticleSelector_cfi.trackingParticleSelector.clone()
+cutsTpMuons.ptMin = 0.0
+
+#----------------------------------------
 
 # MuonAssociation labels; hit-by-hit matching only,MuonAssociator
 
