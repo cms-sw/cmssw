b7b6ae4c25c5fd43b928d47823214e44865d9a9b
Ivan Shvetsov <ivan.shvetsov@cern.ch>
2017-02-12 03:09:10 +0100

    add isolation sum with map based veto for electrons

--- RecoEgamma/EgammaIsolationAlgos/python/egmGedGsfElectronPFIsolation_cfi.py
+++ RecoEgamma/EgammaIsolationAlgos/python/egmGedGsfElectronPFIsolation_cfi.py
@@ -39,3 +39,48 @@ egmGedGsfElectronPFPileUpIsolation = cms.EDProducer(
                   miniAODVertexCodes = cms.vuint32(0,1) )
         )
     )
+
+
+
+egmGedGsfElectronPFNoPileUpIsolationMapBasedVeto = cms.EDProducer(
+    "CITKPFIsolationSumProducer",
+    srcToIsolate = cms.InputTag("gedGsfElectrons"),
+    srcForIsolationCone = cms.InputTag('pfNoPileUpCandidates'),
+    isolationConeDefinitions = cms.VPSet(
+         cms.PSet( isolationAlgo = cms.string('ElectronPFIsolationWithMapBasedVeto'),
+                  coneSize = cms.double(0.3),
+                  isolateAgainst = cms.string('h+'),
+                  miniAODVertexCodes = cms.vuint32(2,3),
+                  vertexIndex = cms.int32(0),
+                  particleBasedIsolation = cms.InputTag("particleBasedIsolation", "gedGsfElectrons") ),
+        cms.PSet( isolationAlgo = cms.string('ElectronPFIsolationWithMapBasedVeto'),
+                  coneSize = cms.double(0.3),
+                  isolateAgainst = cms.string('h0'),
+                  miniAODVertexCodes = cms.vuint32(2,3),
+                  vertexIndex = cms.int32(0),
+                  particleBasedIsolation = cms.InputTag("particleBasedIsolation", "gedGsfElectrons") ),
+        cms.PSet( isolationAlgo = cms.string('ElectronPFIsolationWithMapBasedVeto'),
+                  coneSize = cms.double(0.3),
+                  isolateAgainst = cms.string('gamma'),
+                  miniAODVertexCodes = cms.vuint32(2,3),
+                  vertexIndex = cms.int32(0),
+                  particleBasedIsolation = cms.InputTag("particleBasedIsolation", "gedGsfElectrons") )
+        )
+  )
+
+
+egmGedGsfElectronPFPileUpIsolationMapBasedVeto = cms.EDProducer(
+    "CITKPFIsolationSumProducer",
+    srcToIsolate = cms.InputTag("gedGsfElectrons"),
+    srcForIsolationCone = cms.InputTag('pfPileUpAllChargedParticles'),
+    isolationConeDefinitions = cms.VPSet(
+        cms.PSet( isolationAlgo = cms.string('ElectronPFIsolationWithMapBasedVeto'),
+                  coneSize = cms.double(0.3),
+                  isolateAgainst = cms.string('h+'),
+                  miniAODVertexCodes = cms.vuint32(2,3),
+                  vertexIndex = cms.int32(0),
+                  particleBasedIsolation = cms.InputTag("particleBasedIsolation", "gedGsfElectrons") )
+          )
+    )
+
+
