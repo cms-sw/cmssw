6cc791018a80f1c1fa38c98e424b33fc1f79bb6d
Kyle Knoepfel <knoepfel@fnal.gov>
2017-02-13 12:39:49 -0600

    Remove unnecessary templating.

--- Validation/RecoTau/plugins/PVSorterBySumP.cc
+++ Validation/RecoTau/plugins/PVSorterBySumP.cc
@@ -2,7 +2,6 @@
 // Includes
 ////////////////////////////////////////////////////////////////////////////////
 #include "DataFormats/VertexReco/interface/Vertex.h"
-#include "DataFormats/VertexReco/interface/VertexFwd.h"
 #include "FWCore/Framework/interface/EDProducer.h"
 #include "FWCore/Framework/interface/Event.h"
 #include "FWCore/Framework/interface/MakerMacros.h"
@@ -17,7 +16,6 @@
 ////////////////////////////////////////////////////////////////////////////////
 // class definition
 ////////////////////////////////////////////////////////////////////////////////
-template<typename T1>
 class bestPVselector : public edm::EDProducer {
 public:
 
@@ -25,8 +23,7 @@ public:
   void produce(edm::Event&, edm::EventSetup const&) override;
 
 private:
-
-  edm::EDGetTokenT<std::vector<T1>> src_;
+  edm::EDGetTokenT<std::vector<reco::Vertex>> src_;
 };
 
 
@@ -35,11 +32,10 @@ private:
 ////////////////////////////////////////////////////////////////////////////////
 
 //______________________________________________________________________________
-template<typename T1>
-bestPVselector<T1>::bestPVselector(edm::ParameterSet const& iConfig)
-  : src_{consumes<std::vector<T1>>(iConfig.getParameter<edm::InputTag>("src"))}
+bestPVselector::bestPVselector(edm::ParameterSet const& iConfig)
+  : src_{consumes<std::vector<reco::Vertex>>(iConfig.getParameter<edm::InputTag>("src"))}
 {
-  produces<std::vector<T1>>();
+  produces<std::vector<reco::Vertex>>();
 }
 
 ////////////////////////////////////////////////////////////////////////////////
@@ -47,15 +43,14 @@ bestPVselector<T1>::bestPVselector(edm::ParameterSet const& iConfig)
 ////////////////////////////////////////////////////////////////////////////////
 
 //______________________________________________________________________________
-template<typename T1>
-void bestPVselector<T1>::produce(edm::Event& iEvent,const edm::EventSetup& iSetup)
+void bestPVselector::produce(edm::Event& iEvent, edm::EventSetup const&)
 {
-  edm::Handle<std::vector<T1>> vertices;
+  edm::Handle<std::vector<reco::Vertex>> vertices;
   iEvent.getByToken(src_,vertices);
 
-  auto theBestPV = std::make_unique<std::vector<T1>>();
+  auto theBestPV = std::make_unique<std::vector<reco::Vertex>>();
 
-  if(!vertices->empty()) {
+  if (!vertices->empty()) {
     auto sumSquarePt = [](auto const& pv) { return pv.p4().pt()*pv.p4().pt(); };
     auto bestPV = std::max_element(std::cbegin(*vertices), std::cend(*vertices),
                                    [sumSquarePt](auto const& v1, auto const& v2) {
@@ -66,5 +61,5 @@ void bestPVselector<T1>::produce(edm::Event& iEvent,const edm::EventSetup& iSetu
   iEvent.put(std::move(theBestPV));
 }
 
-using HighestSumP4PrimaryVertexSelector = bestPVselector<reco::Vertex>;
+using HighestSumP4PrimaryVertexSelector = bestPVselector;
 DEFINE_FWK_MODULE(HighestSumP4PrimaryVertexSelector);
