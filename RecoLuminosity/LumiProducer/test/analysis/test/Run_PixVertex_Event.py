5727b15a7612bac5ea41b6414406d97da6addf0b
Nicolas <nfilipov@cern.ch>
2017-02-13 03:35:35 +0100

    ntupler modifications for 90X

--- RecoLuminosity/LumiProducer/test/analysis/test/Run_PixVertex_Event.py
+++ RecoLuminosity/LumiProducer/test/analysis/test/Run_PixVertex_Event.py
@@ -16,21 +16,24 @@ process.MessageLogger.categories.append('L1GtTrigReport')
 process.options = cms.untracked.PSet( wantSummary = cms.untracked.bool(True) )
 
 # -- Database configuration
-process.load("CondCore.DBCommon.CondDBCommon_cfi")
-process.load("CondCore.DBCommon.CondDBSetup_cfi")
+#process.load("CondCore.DBCommon.CondDBCommon_cfi")
+#process.load("CondCore.DBCommon.CondDBSetup_cfi")
+process.load("CondCore.CondDB.CondDB_cfi")
 
 # -- Conditions
+
 process.load("Configuration.StandardSequences.MagneticField_38T_cff")
-process.load("Configuration.StandardSequences.GeometryRecoDB_cff") # works for MC & data
+process.load("Configuration.StandardSequences.GeometryRecoDB_cff") #
 process.load("RecoVertex.BeamSpotProducer.BeamSpot_cfi")
 
 process.load('Configuration.StandardSequences.FrontierConditions_GlobalTag_condDBv2_cff')
 from Configuration.AlCa.GlobalTag_condDBv2 import GlobalTag
-#process.GlobalTag = GlobalTag(process.GlobalTag, 'auto:run2_mc', '')
-process.GlobalTag = GlobalTag(process.GlobalTag, 'auto:run2_hlt_GRun', '')
 
-process.load("Configuration.StandardSequences.Reconstruction_cff")
+#process.GlobalTag = GlobalTag(process.GlobalTag, '80X_mcRun2_asymptotic_v14', '')
+process.GlobalTag = GlobalTag(process.GlobalTag, '90X_upgrade2023_realistic_v1', '')
 
+
+process.load("Configuration.StandardSequences.Reconstruction_cff") # 
 # -- number of events
 process.maxEvents = cms.untracked.PSet(
     input = cms.untracked.int32(-1)
@@ -57,7 +60,7 @@ process.lumi = cms.EDAnalyzer(
     vertexCollLabel              = cms.untracked.InputTag('offlinePrimaryVertices'),
     pixelClusterLabel            = cms.untracked.InputTag('siPixelClusters'),
     saveType                     = cms.untracked.string('Event'), # LumiSect, LumiNib, Event
-    sampleType                   = cms.untracked.string('DATA'), # MC, DATA
+    sampleType                   = cms.untracked.string('MC'), # MC, DATA
     includeVertexInformation     = cms.untracked.bool(True),
     includePixels                = cms.untracked.bool(True),
     splitByBX                    = cms.untracked.bool(True),
@@ -73,11 +76,13 @@ process.p = cms.Path(
     )
 
 
-outFile = 'pcc_Data_PixVtx_Event.root'
+outFile = 'pcc_Data_PixVtx_Event_90X.root'
 process.TFileService = cms.Service("TFileService",fileName = cms.string(outFile)) 
 readFiles = cms.untracked.vstring() 
 secFiles = cms.untracked.vstring() 
 process.source = cms.Source ("PoolSource",fileNames = readFiles, secondaryFileNames = secFiles) 
 readFiles.extend([
-'/store/data/Run2015A/ZeroBias1/RECO/PromptReco-v1/000/250/786/00000/B4CDEBBC-F52A-E511-808D-02163E011CE8.root',
+'/store/relval/CMSSW_9_0_0_pre2/RelValMinBias_14TeV/GEN-SIM-RECO/90X_upgrade2023_realistic_v1_2023D4Timing-v1/10000/36D3D8CD-3BC2-E611-908A-0025905A6088.root', # 80X file
+#'/store/mc/RunIISummer16DR80/MinBias_TuneCUETP8M1_13TeV-pythia8/GEN-SIM-RECO/NoPU_RECO_80X_mcRun2_asymptotic_v14-v1/100000/00150044-D075-E611-AAE8-001E67505A2D.root', # 90X file
+#'/store/data/Run2015A/ZeroBias1/RECO/PromptReco-v1/000/250/786/00000/B4CDEBBC-F52A-E511-808D-02163E011CE8.root', # data, havent tried
 ])
