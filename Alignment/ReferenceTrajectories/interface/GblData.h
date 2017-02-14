39b260d2e51c7116515d8910238ce7e7f73922e9
Gregor Mittag <Gregor.Mittag@cern.ch>
2017-02-14 16:31:28 +0100

    Move to Eigen-version of GBL developed by Claus Kleinwort.

--- Alignment/ReferenceTrajectories/interface/GblData.h
+++ Alignment/ReferenceTrajectories/interface/GblData.h
@@ -5,43 +5,64 @@
  *      Author: kleinwrt
  */
 
+/** \file
+ *  GblData definition.
+ *
+ *  \author Claus Kleinwort, DESY, 2011 (Claus.Kleinwort@desy.de)
+ *
+ *  \copyright
+ *  Copyright (c) 2011 - 2016 Deutsches Elektronen-Synchroton,
+ *  Member of the Helmholtz Association, (DESY), HAMBURG, GERMANY \n\n
+ *  This library is free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU Library General Public License as
+ *  published by the Free Software Foundation; either version 2 of the
+ *  License, or (at your option) any later version. \n\n
+ *  This library is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU Library General Public License for more details. \n\n
+ *  You should have received a copy of the GNU Library General Public
+ *  License along with this program (see the file COPYING.LIB for more
+ *  details); if not, write to the Free Software Foundation, Inc.,
+ *  675 Mass Ave, Cambridge, MA 02139, USA.
+ */
+
 #ifndef GBLDATA_H_
 #define GBLDATA_H_
 
 #include<iostream>
 #include<vector>
 #include<math.h>
-#include "VMatrix.h"
-#include "TVectorD.h"
-#include "TMatrixD.h"
-#include "TMatrixDSym.h"
+#include "Alignment/ReferenceTrajectories/interface/VMatrix.h"
 
-#include "Math/SMatrix.h"
-#include "Math/SVector.h"
-typedef ROOT::Math::SMatrix<double, 2, 5> SMatrix25;
-typedef ROOT::Math::SMatrix<double, 2, 7> SMatrix27;
-typedef ROOT::Math::SMatrix<double, 5, 5> SMatrix55;
+#include "Eigen/Core"
+typedef Eigen::Matrix<double, 5, 5> Matrix5d;
+typedef Eigen::Matrix<double, 2, 7> Matrix27d;
 
 //! Namespace for the general broken lines package
 namespace gbl {
 
+enum dataBlockType {
+	None, InternalMeasurement, InternalKink, ExternalSeed, ExternalMeasurement
+};
+
 /// Data (block) for independent scalar measurement
 /**
- * Data (block) containing value, precision and derivatives for measurements and kinks.
+ * Data (block) containing value, precision and derivatives for measurements, kinks and seeds.
  * Created from attributes of GblPoints, used to construct linear equation system for track fit.
  */
 class GblData {
 public:
-	GblData(unsigned int aLabel, double aMeas, double aPrec);
+	GblData(unsigned int aLabel, dataBlockType aType, double aMeas,
+			double aPrec, unsigned int aTraj = 0, unsigned int aPoint = 0);
 	virtual ~GblData();
 	void addDerivatives(unsigned int iRow,
-			const std::vector<unsigned int> &labDer, const SMatrix55 &matDer,
-			unsigned int iOff, const TMatrixD &derLocal,
-			const std::vector<int> &labGlobal, const TMatrixD &derGlobal,
-			unsigned int nLocal, const TMatrixD &derTrans);
+			const std::vector<unsigned int> &labDer, const Matrix5d &matDer,
+			unsigned int iOff, const Eigen::MatrixXd &derLocal, unsigned int nLocal,
+			const Eigen::MatrixXd &derTrans);
 	void addDerivatives(unsigned int iRow,
-			const std::vector<unsigned int> &labDer, const SMatrix27 &matDer,
-			unsigned int nLocal, const TMatrixD &derTrans);
+			const std::vector<unsigned int> &labDer, const Matrix27d &matDer,
+			unsigned int nLocal, const Eigen::MatrixXd &derTrans);
 	void addDerivatives(const std::vector<unsigned int> &index,
 			const std::vector<double> &derivatives);
 
@@ -49,27 +70,34 @@ public:
 	double setDownWeighting(unsigned int aMethod);
 	double getChi2() const;
 	void printData() const;
-	void getLocalData(double &aValue, double &aWeight,
-			std::vector<unsigned int>* &indLocal,
-			std::vector<double>* &derLocal);
-	void getAllData(double &aValue, double &aErr,
-			std::vector<unsigned int>* &indLocal,
-			std::vector<double>* &derLocal, std::vector<int>* &labGlobal,
-			std::vector<double>* &derGlobal);
+	unsigned int getLabel() const;
+	dataBlockType getType() const;
+	unsigned int getNumSimple() const;
+	void getLocalData(double &aValue, double &aWeight, unsigned int &numLocal,
+			unsigned int* &indLocal, double* &derLocal);
+	void getAllData(double &aValue, double &aErr, unsigned int &numLocal,
+			unsigned int* &indLocal, double* &derLocal, unsigned int &aTraj,
+			unsigned int &aPoint, unsigned int &aRow);
 	void getResidual(double &aResidual, double &aVariance, double &aDownWeight,
-			std::vector<unsigned int>* &indLocal,
-			std::vector<double>* &derLocal);
+			unsigned int &numLocal, unsigned int* &indLocal, double* &derLocal);
 
 private:
-	unsigned int theLabel; ///< Label (of measurements point)
+	unsigned int theLabel; ///< Label (of corresponding point)
+	unsigned int theRow; ///< Row number (of measurement)
+	dataBlockType theType; ///< Type (None, InternalMeasurement, InternalKink, ExternalSeed, ExternalMeasurement)
 	double theValue; ///< Value (residual)
 	double thePrecision; ///< Precision (1/sigma**2)
+	unsigned int theTrajectory; ///< Trajectory number
+	unsigned int thePoint; ///< Point number (on trajectory)
 	double theDownWeight; ///< Down-weighting factor (0-1)
 	double thePrediction; ///< Prediction from fit
-	std::vector<unsigned int> theParameters; ///< List of fit parameters (with non zero derivatives)
-	std::vector<double> theDerivatives; ///< List of derivatives for fit
-	std::vector<int> globalLabels; ///< Labels for global derivatives
-	std::vector<double> globalDerivatives; ///< Global derivatives
+	// standard local parameters (curvature, offsets), fixed size
+	unsigned int theNumLocal; ///< Number of (non zero) local derivatives (max 7 for kinks)
+	unsigned int theParameters[7]; ///< List of parameters (with non zero derivatives)
+	double theDerivatives[7]; ///< List of derivatives for fit
+	// more local parameters, dynamic size
+	std::vector<unsigned int> moreParameters; ///< List of fit parameters (with non zero derivatives)
+	std::vector<double> moreDerivatives; ///< List of derivatives for fit
 };
 }
 #endif /* GBLDATA_H_ */
--- Alignment/ReferenceTrajectories/src/GblData.cc
+++ Alignment/ReferenceTrajectories/src/GblData.cc
@@ -5,7 +5,30 @@
  *      Author: kleinwrt
  */
 
+/** \file
+ *  GblData methods.
+ *
+ *  \author Claus Kleinwort, DESY, 2011 (Claus.Kleinwort@desy.de)
+ *
+ *  \copyright
+ *  Copyright (c) 2011 - 2016 Deutsches Elektronen-Synchroton,
+ *  Member of the Helmholtz Association, (DESY), HAMBURG, GERMANY \n\n
+ *  This library is free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU Library General Public License as
+ *  published by the Free Software Foundation; either version 2 of the
+ *  License, or (at your option) any later version. \n\n
+ *  This library is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU Library General Public License for more details. \n\n
+ *  You should have received a copy of the GNU Library General Public
+ *  License along with this program (see the file COPYING.LIB for more
+ *  details); if not, write to the Free Software Foundation, Inc.,
+ *  675 Mass Ave, Cambridge, MA 02139, USA.
+ */
+
 #include "Alignment/ReferenceTrajectories/interface/GblData.h"
+using namespace Eigen;
 
 //! Namespace for the general broken lines package
 namespace gbl {
@@ -13,12 +36,17 @@ namespace gbl {
 /// Create data block.
 /**
  * \param [in] aLabel Label of corresponding point
+ * \param [in] aType Type of (scalar) measurement
  * \param [in] aValue Value of (scalar) measurement
  * \param [in] aPrec Precision of (scalar) measurement
+ * \param [in] aTraj Trajectory number
+ * \param [in] aPoint Point number
  */
-GblData::GblData(unsigned int aLabel, double aValue, double aPrec) :
-		theLabel(aLabel), theValue(aValue), thePrecision(aPrec), theDownWeight(
-				1.), thePrediction(0.), theParameters(), theDerivatives(), globalLabels(), globalDerivatives() {
+GblData::GblData(unsigned int aLabel, dataBlockType aType, double aValue,
+		double aPrec, unsigned int aTraj, unsigned int aPoint) :
+		theLabel(aLabel), theRow(0), theType(aType), theValue(aValue), thePrecision(
+				aPrec), theTrajectory(aTraj), thePoint(aPoint), theDownWeight(
+				1.), thePrediction(0.), theNumLocal(0), moreParameters(), moreDerivatives() {
 
 }
 
@@ -33,48 +61,72 @@ GblData::~GblData() {
  * \param [in] matDer Derivatives (matrix) 'measurement vs track fit parameters'
  * \param [in] iOff Offset for row index for additional parameters
  * \param [in] derLocal Derivatives (matrix) for additional local parameters
- * \param [in] labGlobal Labels for additional global (MP-II) parameters
- * \param [in] derGlobal Derivatives (matrix) for additional global (MP-II) parameters
  * \param [in] extOff Offset for external parameters
  * \param [in] extDer Derivatives for external Parameters
  */
 void GblData::addDerivatives(unsigned int iRow,
-		const std::vector<unsigned int> &labDer, const SMatrix55 &matDer,
-		unsigned int iOff, const TMatrixD &derLocal,
-		const std::vector<int> &labGlobal, const TMatrixD &derGlobal,
-		unsigned int extOff, const TMatrixD &extDer) {
+		const std::vector<unsigned int> &labDer, const Matrix5d &matDer,
+		unsigned int iOff, const MatrixXd &derLocal, unsigned int extOff,
+		const MatrixXd &extDer) {
 
-	unsigned int nParMax = 5 + derLocal.GetNcols() + extDer.GetNcols();
-	theParameters.reserve(nParMax); // have to be sorted
-	theDerivatives.reserve(nParMax);
+	unsigned int nParMax = 5 + derLocal.cols() + extDer.cols();
+	theRow = iRow - iOff;
+	if (nParMax > 7) {
+		// dynamic data block size
+		moreParameters.reserve(nParMax); // have to be sorted
+		moreDerivatives.reserve(nParMax);
 
-	for (int i = 0; i < derLocal.GetNcols(); ++i) // local derivatives
-			{
-		if (derLocal(iRow - iOff, i)) {
-			theParameters.push_back(i + 1);
-			theDerivatives.push_back(derLocal(iRow - iOff, i));
+		for (int i = 0; i < derLocal.cols(); ++i) // local derivatives
+				{
+			if (derLocal(iRow - iOff, i)) {
+				moreParameters.push_back(i + 1);
+				moreDerivatives.push_back(derLocal(iRow - iOff, i));
+			}
+		}
+
+		for (int i = 0; i < extDer.cols(); ++i) // external derivatives
+				{
+			if (extDer(iRow - iOff, i)) {
+				moreParameters.push_back(extOff + i + 1);
+				moreDerivatives.push_back(extDer(iRow - iOff, i));
+			}
+		}
+
+		for (unsigned int i = 0; i < 5; ++i) // curvature, offset derivatives
+				{
+			if (labDer[i] and matDer(iRow, i)) {
+				moreParameters.push_back(labDer[i]);
+				moreDerivatives.push_back(matDer(iRow, i));
+			}
+		}
+	} else {
+		// simple (static)  data block
+		for (int i = 0; i < derLocal.cols(); ++i) // local derivatives
+				{
+			if (derLocal(iRow - iOff, i)) {
+				theParameters[theNumLocal] = i + 1;
+				theDerivatives[theNumLocal] = derLocal(iRow - iOff, i);
+				theNumLocal++;
+			}
+		}
+
+		for (int i = 0; i < extDer.cols(); ++i) // external derivatives
+				{
+			if (extDer(iRow - iOff, i)) {
+				theParameters[theNumLocal] = extOff + i + 1;
+				theDerivatives[theNumLocal] = extDer(iRow - iOff, i);
+				theNumLocal++;
+			}
+		}
+		for (unsigned int i = 0; i < 5; ++i) // curvature, offset derivatives
+				{
+			if (labDer[i] and matDer(iRow, i)) {
+				theParameters[theNumLocal] = labDer[i];
+				theDerivatives[theNumLocal] = matDer(iRow, i);
+				theNumLocal++;
+			}
 		}
 	}
-
-	for (int i = 0; i < extDer.GetNcols(); ++i) // external derivatives
-			{
-		if (extDer(iRow - iOff, i)) {
-			theParameters.push_back(extOff + i + 1);
-			theDerivatives.push_back(extDer(iRow - iOff, i));
-		}
-	}
-
-	for (unsigned int i = 0; i < 5; ++i) // curvature, offset derivatives
-			{
-		if (labDer[i] and matDer(iRow, i)) {
-			theParameters.push_back(labDer[i]);
-			theDerivatives.push_back(matDer(iRow, i));
-		}
-	}
-
-	globalLabels = labGlobal;
-	for (int i = 0; i < derGlobal.GetNcols(); ++i) // global derivatives
-		globalDerivatives.push_back(derGlobal(iRow - iOff, i));
 }
 
 /// Add derivatives from kink.
@@ -87,26 +139,40 @@ void GblData::addDerivatives(unsigned int iRow,
  * \param [in] extDer Derivatives for external Parameters
  */
 void GblData::addDerivatives(unsigned int iRow,
-		const std::vector<unsigned int> &labDer, const SMatrix27 &matDer,
-		unsigned int extOff, const TMatrixD &extDer) {
+		const std::vector<unsigned int> &labDer, const Matrix27d &matDer,
+		unsigned int extOff, const MatrixXd &extDer) {
 
-	unsigned int nParMax = 7 + extDer.GetNcols();
-	theParameters.reserve(nParMax); // have to be sorted
-	theDerivatives.reserve(nParMax);
+	unsigned int nParMax = 7 + extDer.cols();
+	theRow = iRow;
+	if (nParMax > 7) {
+		// dynamic data block size
+		moreParameters.reserve(nParMax); // have to be sorted
+		moreDerivatives.reserve(nParMax);
 
-	for (int i = 0; i < extDer.GetNcols(); ++i) // external derivatives
-			{
-		if (extDer(iRow, i)) {
-			theParameters.push_back(extOff + i + 1);
-			theDerivatives.push_back(extDer(iRow, i));
+		for (int i = 0; i < extDer.cols(); ++i) // external derivatives
+				{
+			if (extDer(iRow, i)) {
+				moreParameters.push_back(extOff + i + 1);
+				moreDerivatives.push_back(extDer(iRow, i));
+			}
 		}
-	}
 
-	for (unsigned int i = 0; i < 7; ++i) // curvature, offset derivatives
-			{
-		if (labDer[i] and matDer(iRow, i)) {
-			theParameters.push_back(labDer[i]);
-			theDerivatives.push_back(matDer(iRow, i));
+		for (unsigned int i = 0; i < 7; ++i) // curvature, offset derivatives
+				{
+			if (labDer[i] and matDer(iRow, i)) {
+				moreParameters.push_back(labDer[i]);
+				moreDerivatives.push_back(matDer(iRow, i));
+			}
+		}
+	} else {
+		// simple (static) data block
+		for (unsigned int i = 0; i < 7; ++i) // curvature, offset derivatives
+				{
+			if (labDer[i] and matDer(iRow, i)) {
+				theParameters[theNumLocal] = labDer[i];
+				theDerivatives[theNumLocal] = matDer(iRow, i);
+				theNumLocal++;
+			}
 		}
 	}
 }
@@ -122,8 +188,8 @@ void GblData::addDerivatives(const std::vector<unsigned int> &index,
 	for (unsigned int i = 0; i < derivatives.size(); ++i) // any derivatives
 			{
 		if (derivatives[i]) {
-			theParameters.push_back(index[i]);
-			theDerivatives.push_back(derivatives[i]);
+			moreParameters.push_back(index[i]);
+			moreDerivatives.push_back(derivatives[i]);
 		}
 	}
 }
@@ -132,8 +198,15 @@ void GblData::addDerivatives(const std::vector<unsigned int> &index,
 void GblData::setPrediction(const VVector &aVector) {
 
 	thePrediction = 0.;
-	for (unsigned int i = 0; i < theDerivatives.size(); ++i) {
-		thePrediction += theDerivatives[i] * aVector(theParameters[i] - 1);
+	if (theNumLocal > 0) {
+		for (unsigned int i = 0; i < theNumLocal; ++i) {
+			thePrediction += theDerivatives[i] * aVector(theParameters[i] - 1);
+		}
+	} else {
+		for (unsigned int i = 0; i < moreDerivatives.size(); ++i) {
+			thePrediction += moreDerivatives[i]
+					* aVector(moreParameters[i] - 1);
+		}
 	}
 }
 
@@ -178,54 +251,95 @@ double GblData::getChi2() const {
 /// Print data block.
 void GblData::printData() const {
 
-	std::cout << " measurement at label " << theLabel << ": " << theValue
-			<< ", " << thePrecision << std::endl;
-	std::cout << "  param " << theParameters.size() << ":";
-	for (unsigned int i = 0; i < theParameters.size(); ++i) {
+	std::cout << " measurement at label " << theLabel << " of type " << theType
+			<< " from row " << theRow << ": " << theValue << ", "
+			<< thePrecision << std::endl;
+	std::cout << "  param " << moreParameters.size() + theNumLocal << ":";
+	for (unsigned int i = 0; i < moreParameters.size(); ++i) {
+		std::cout << " " << moreParameters[i];
+	}
+	for (unsigned int i = 0; i < theNumLocal; ++i) {
 		std::cout << " " << theParameters[i];
 	}
 	std::cout << std::endl;
-	std::cout << "  deriv " << theDerivatives.size() << ":";
-	for (unsigned int i = 0; i < theDerivatives.size(); ++i) {
+	std::cout << "  deriv " << moreDerivatives.size() + theNumLocal << ":";
+	for (unsigned int i = 0; i < moreDerivatives.size(); ++i) {
+		std::cout << " " << moreDerivatives[i];
+	}
+	for (unsigned int i = 0; i < theNumLocal; ++i) {
 		std::cout << " " << theDerivatives[i];
 	}
 	std::cout << std::endl;
+}
+
+/// Get label.
+/**
+ * \return label of corresponding point
+ */
+unsigned int GblData::getLabel() const {
+	return theLabel;
+}
+
+/// Get type.
+/**
+ * \return type
+ */
+dataBlockType GblData::getType() const {
+	return theType;
 }
 
 /// Get Data for local fit.
 /**
  * \param [out] aValue Value
  * \param [out] aWeight Weight
- * \param [out] indLocal List of labels of used (local) fit parameters
- * \param [out] derLocal List of derivatives for used (local) fit parameters
+ * \param [out] numLocal Number of local labels/derivatives
+ * \param [out] indLocal Array of labels of used (local) fit parameters
+ * \param [out] derLocal Array of derivatives for used (local) fit parameters
  */
 void GblData::getLocalData(double &aValue, double &aWeight,
-		std::vector<unsigned int>* &indLocal, std::vector<double>* &derLocal) {
+		unsigned int &numLocal, unsigned int* &indLocal, double* &derLocal) {
 
 	aValue = theValue;
 	aWeight = thePrecision * theDownWeight;
-	indLocal = &theParameters;
-	derLocal = &theDerivatives;
+	if (theNumLocal > 0) {
+		numLocal = theNumLocal;
+		indLocal = theParameters;
+		derLocal = theDerivatives;
+	} else {
+		numLocal = moreParameters.size();
+		indLocal = &moreParameters[0];
+		derLocal = &moreDerivatives[0];
+	}
 }
 
 /// Get all Data for MP-II binary record.
 /**
  * \param [out] aValue Value
  * \param [out] aErr Error
- * \param [out] indLocal List of labels of local parameters
- * \param [out] derLocal List of derivatives for local parameters
- * \param [out] labGlobal List of labels of global parameters
- * \param [out] derGlobal List of derivatives for global parameters
+ * \param [out] numLocal Number of local labels/derivatives
+ * \param [out] indLocal Array of labels of used (local) fit parameters
+ * \param [out] derLocal Array of derivatives for used (local) fit parameters
+ * \param [out] aTraj Trajectory number
+ * \param [out] aPoint Point number
+ * \param [out] aRow Row number
  */
-void GblData::getAllData(double &aValue, double &aErr,
-		std::vector<unsigned int>* &indLocal, std::vector<double>* &derLocal,
-		std::vector<int>* &labGlobal, std::vector<double>* &derGlobal) {
+void GblData::getAllData(double &aValue, double &aErr, unsigned int &numLocal,
+		unsigned int* &indLocal, double* &derLocal, unsigned int &aTraj,
+		unsigned int &aPoint, unsigned int &aRow) {
 	aValue = theValue;
 	aErr = 1.0 / sqrt(thePrecision);
-	indLocal = &theParameters;
-	derLocal = &theDerivatives;
-	labGlobal = &globalLabels;
-	derGlobal = &globalDerivatives;
+	if (theNumLocal > 0) {
+		numLocal = theNumLocal;
+		indLocal = theParameters;
+		derLocal = theDerivatives;
+	} else {
+		numLocal = moreParameters.size();
+		indLocal = &moreParameters[0];
+		derLocal = &moreDerivatives[0];
+	}
+	aTraj = theTrajectory;
+	aPoint = thePoint;
+	aRow = theRow;
 }
 
 /// Get data for residual (and errors).
@@ -233,16 +347,24 @@ void GblData::getAllData(double &aValue, double &aErr,
  * \param [out] aResidual Measurement-Prediction
  * \param [out] aVariance Variance (of measurement)
  * \param [out] aDownWeight Down-weighting factor
- * \param [out] indLocal List of labels of used (local) fit parameters
- * \param [out] derLocal List of derivatives for used (local) fit parameters
+ * \param [out] numLocal Number of local labels/derivatives
+ * \param [out] indLocal Array of labels of used (local) fit parameters
+ * \param [out] derLocal Array of derivatives for used (local) fit parameters
  */
 void GblData::getResidual(double &aResidual, double &aVariance,
-		double &aDownWeight, std::vector<unsigned int>* &indLocal,
-		std::vector<double>* &derLocal) {
+		double &aDownWeight, unsigned int &numLocal, unsigned int* &indLocal,
+		double* &derLocal) {
 	aResidual = theValue - thePrediction;
 	aVariance = 1.0 / thePrecision;
 	aDownWeight = theDownWeight;
-	indLocal = &theParameters;
-	derLocal = &theDerivatives;
+	if (theNumLocal > 0) {
+		numLocal = theNumLocal;
+		indLocal = theParameters;
+		derLocal = theDerivatives;
+	} else {
+		numLocal = moreParameters.size();
+		indLocal = &moreParameters[0];
+		derLocal = &moreDerivatives[0];
+	}
 }
 }
