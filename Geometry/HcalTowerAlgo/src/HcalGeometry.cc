8c8081a9972854083dbd77268b2413bbb780063e
Sunanda <sunanda.banerjee@cern.ch>
2017-02-17 19:15:54 +0100

    Effect of Plan1 geometry on Calibration

--- Geometry/HcalTowerAlgo/src/HcalGeometry.cc
+++ Geometry/HcalTowerAlgo/src/HcalGeometry.cc
@@ -147,9 +147,9 @@ GlobalPoint HcalGeometry::getPosition(const DetId& id) const {
   if (!m_mergePosition) {
     return (getGeometry(id)->getPosition());
   } else {
-    std::vector<HcalDetId> ids;
-    m_topology.unmergeDepthDetId(HcalDetId(id),ids);
-    return (getGeometry(ids.front())->getPosition());
+    std::vector<HcalDetId> ids_;
+    m_topology.unmergeDepthDetId(HcalDetId(id),ids_);
+    return (getGeometry(ids_.front())->getPosition());
   }
 }
 
@@ -157,9 +157,26 @@ GlobalPoint HcalGeometry::getBackPosition(const DetId& id) const {
   if (!m_mergePosition) {
     return (getGeometry(id)->getBackPoint());
   } else {
-    std::vector<HcalDetId> ids;
-    m_topology.unmergeDepthDetId(HcalDetId(id),ids);
-    return (getGeometry(ids.back())->getBackPoint());
+    std::vector<HcalDetId> ids_;
+    m_topology.unmergeDepthDetId(HcalDetId(id),ids_);
+    return (getGeometry(ids_.back())->getBackPoint());
+  }
+}
+
+CaloCellGeometry::CornersVec HcalGeometry::getCorners(const DetId& id) const {
+  if (!m_mergePosition) {
+    return (getGeometry(id)->getCorners());
+  } else {
+    std::vector<HcalDetId> ids_;
+    m_topology.unmergeDepthDetId(HcalDetId(id),ids_);
+    CaloCellGeometry::CornersVec mcorners;
+    CaloCellGeometry::CornersVec mcf = getGeometry(ids_.front())->getCorners();
+    CaloCellGeometry::CornersVec mcb = getGeometry(ids_.back())->getCorners();
+    for (unsigned int k=0; k<8; ++k) {
+      mcorners[k]   = mcf[k];
+      mcorners[k+4] = mcb[k];
+    }
+    return mcorners;
   }
 }
 
