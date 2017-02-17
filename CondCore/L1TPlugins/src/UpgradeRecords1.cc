090543f451430d9d8f4b337ecada6f4571e424ea
Shahzad Malik Muzaffar <Shahzad.Malik.Muzaffar@cern.ch>
2017-02-17 16:08:57 +0100

    split CondCore/L1TPlugins/src/plugin.cc in smaller files

---
+++ CondCore/L1TPlugins/src/UpgradeRecords1.cc
@@ -0,0 +1,13 @@
+#include "CondCore/ESSources/interface/registration_macros.h"
+#include "CondFormats/L1TObjects/interface/CaloParams.h"
+#include "CondFormats/DataRecord/interface/L1TCaloParamsRcd.h"
+#include "CondFormats/DataRecord/interface/L1TCaloStage2ParamsRcd.h"
+#include "CondFormats/DataRecord/interface/L1TCaloParamsO2ORcd.h"
+#include "CondFormats/L1TObjects/interface/CaloConfig.h"
+#include "CondFormats/DataRecord/interface/L1TCaloConfigRcd.h"
+using namespace l1t;
+REGISTER_PLUGIN(L1TCaloParamsO2ORcd, CaloParams);
+REGISTER_PLUGIN(L1TCaloParamsRcd, CaloParams);
+REGISTER_PLUGIN(L1TCaloStage2ParamsRcd, CaloParams);
+REGISTER_PLUGIN(L1TCaloConfigRcd, CaloConfig);
+
