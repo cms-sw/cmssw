b6602103f3719e2939f8aac427cb7ed240ca2623
Matti Kortelainen <matti.kortelainen@cern.ch>
2017-02-20 15:59:46 +0100

    Add optional seed stop reason

--- RecoTracker/CkfPattern/python/CkfTrackCandidates_cfi.py
+++ RecoTracker/CkfPattern/python/CkfTrackCandidates_cfi.py
@@ -31,5 +31,6 @@ ckfTrackCandidates = cms.EDProducer("CkfTrackCandidateMaker",
         numberMeasurementsForFit = cms.int32(4)
     ),
     MeasurementTrackerEvent = cms.InputTag("MeasurementTrackerEvent"),
+    produceSeedStopReasons = cms.bool(False), # useful only for ntuple/debugging
 )
 
