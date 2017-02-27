029fcc71e863c3ba9a0c417eb03a2e0eb16e4d5b
Junghwan John Goh <jhgoh@cern.ch>
2017-02-27 13:31:24 +0100

    cleanup codes, apply patch by Markus

--- GeneratorInterface/RivetInterface/plugins/MergedGenParticleProducer.hh
+++ GeneratorInterface/RivetInterface/plugins/MergedGenParticleProducer.hh
@@ -1,28 +1,25 @@
 #ifndef MergedGenParticleProducer_h
 #define MergedGenParticleProducer_h
 
-#include <vector>
-#include "FWCore/Framework/interface/EDProducer.h"
+#include "FWCore/Framework/interface/stream/EDProducer.h"
 #include "FWCore/ParameterSet/interface/ParameterSet.h"
 #include "FWCore/Framework/interface/Event.h"
-#include "FWCore/Framework/interface/EventSetup.h"
-#include "FWCore/Utilities/interface/InputTag.h"
 #include "DataFormats/HepMCCandidate/interface/GenParticle.h"
 #include "DataFormats/PatCandidates/interface/PackedGenParticle.h"
+#include <vector>
 
-class MergedGenParticleProducer : public edm::EDProducer {
+class MergedGenParticleProducer : public edm::stream::EDProducer<>
+{
  public:
-  explicit MergedGenParticleProducer(const edm::ParameterSet &);
-  ~MergedGenParticleProducer();
+  MergedGenParticleProducer(const edm::ParameterSet& pset);
+  ~MergedGenParticleProducer() {};
 
  private:
-  virtual void beginJob();
-  virtual void produce(edm::Event &, const edm::EventSetup &);
-  virtual void endJob();
+  void produce(edm::Event& event, const edm::EventSetup&) override;
+  bool isPhotonFromPrunedHadron(const pat::PackedGenParticle& pk) const;
 
   edm::EDGetTokenT<edm::View<reco::GenParticle>> input_pruned_;
   edm::EDGetTokenT<edm::View<pat::PackedGenParticle>> input_packed_;
-  reco::GenParticleRefProd ref_;
 };
 
 #endif
