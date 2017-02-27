47a863c6df345b1cf2805ba09bb1d9dacc284cd1
Luca Mastrolorenzo <luca.mastrolorenzo@cern.ch>
2017-02-27 18:53:44 +0100

    small modification to the 2D-clustering algorithm + 3D-clustering as in IC code just to debug the C2d (need the sum of the energy)

--- DataFormats/L1THGCal/src/HGCalMulticluster.cc
+++ DataFormats/L1THGCal/src/HGCalMulticluster.cc
@@ -6,17 +6,52 @@ using namespace l1t;
 HGCalMulticluster::HGCalMulticluster( const LorentzVector p4, 
                                       int pt,
                                       int eta,
-                                      int phi//,
-                                      //   ClusterCollection &basic_clusters
+                                      int phi,
+                                      ClusterCollection &basic_clusters
     ) :
     L1Candidate(p4, pt, eta, phi),
     myclusters_(basic_clusters){
     } 
 
+HGCalMulticluster::HGCalMulticluster( const l1t::HGCalCluster & clu )
+{
+
+    centre_ = clu.centreNorm();
+    hwPt_ = clu.hwPt();
+    zside_ = clu.zside();
+
+}
+
+
 HGCalMulticluster::~HGCalMulticluster() 
 {
   
 }
+
+
+bool HGCalMulticluster::isPertinent( const l1t::HGCalCluster & clu, double dR ) const
+{
+    
+    if( zside_ != clu.zside() )
+        return false;
+
+    if( ( centre_ - clu.centreNorm() ).Mag2() < dR )
+        return true;
+    
+    return false;
+
+}
+
+
+void HGCalMulticluster::addClu( const l1t::HGCalCluster & clu ) const
+{
+
+    centre_ = ( centre_*hwPt_ + clu.centreNorm()*clu.hwPt() ) / ( hwPt_+clu.hwPt() ) ;
+    
+    hwPt_ = hwPt_ + clu.hwPt();
+
+}
+
 
 bool HGCalMulticluster::operator<(const HGCalMulticluster& cl) const
 {
--- L1Trigger/L1THGCal/src/be_algorithms/HGCalMulticlusteringImpl.cc
+++ L1Trigger/L1THGCal/src/be_algorithms/HGCalMulticlusteringImpl.cc
@@ -1,6 +1,7 @@
 #include "L1Trigger/L1THGCal/interface/be_algorithms/HGCalMulticlusteringImpl.h"
 #include "DataFormats/Math/interface/deltaR.h"
 
+<<<<<<< HEAD
 //class constructor
 HGCalMulticlusteringImpl::HGCalMulticlusteringImpl(const edm::ParameterSet& conf){    
     dR_ = conf.getParameter<double>("dR_searchNeighbour");
@@ -10,14 +11,35 @@ void HGCalMulticlusteringImpl::clusterise( const l1t::HGCalClusterBxCollection &
                                            l1t::HGCalMulticlusterBxCollection & multiclusters_)
 {
    
-    edm::LogInfo("HGCclusterParameters") << "Multicluster dR for Near Neighbour search: " << dR_;  
-
-    if( clusters_.size() > 0 ){}
+    edm::LogInfo("HGCclusterParameters") << "Multicluster dR for Near Neighbour search: " << dr_;  
         
     for(l1t::HGCalClusterBxCollection::const_iterator clu = clusters_.begin(); clu != clusters_.end(); ++clu){
         
-    }
-    
+        int imclu=0;
+        vector<int> tcPertinentMulticlusters;
+        for(l1t::HGCalMulticlusterBxCollection::const_iterator mclu = multiclusters_.begin(); mclu != multiclusters_.end(); ++mclu,++imclu)
+            if( mclu->isPertinent(*clu, dr_) )
+                tcPertinentMulticlusters.push_back(imclu);
+
+        if( tcPertinentMulticlusters.size() == 0 ){
+            l1t::HGCalMulticluster obj( *clu );
+            multiclusters_.push_back( 0, obj );
+        }
+        else{
+            uint minDist = 1;
+            uint targetMulticlu = 0; 
+            for( std::vector<int>::const_iterator imclu = tcPertinentMulticlusters.begin(); imclu != tcPertinentMulticlusters.end(); ++imclu ){
+                double d = ( multiclusters_.at(0, *imclu).centre() - clu->centreNorm() ).Mag2() ;
+                if( d < minDist ){
+                    minDist = d;
+                    targetMulticlu = *imclu;
+                }
+            } 
+
+            multiclusters_.at(0, targetMulticlu).addClu( *clu );
+        
+        }            
+    }    
 }
     
    
