b5c765a900b959c513538f221a83bf1787ed92e5
Matti Kortelainen <matti.kortelainen@cern.ch>
2017-02-27 13:33:55 +0100

    Reorganize Run2_2017 eras with copyAndExclude()

--- Configuration/Eras/python/Era_Run2_2017_trackingLowPU_cff.py
+++ Configuration/Eras/python/Era_Run2_2017_trackingLowPU_cff.py
@@ -1,7 +1,7 @@
 import FWCore.ParameterSet.Config as cms
 
-from Configuration.Eras.Era_Run2_2017_cff import Run2_2017
+from Configuration.Eras.ModifierChain_run2_2017_noTrackingModifier_cff import run2_2017_noTrackingModifier
 from Configuration.Eras.Modifier_trackingLowPU_cff import trackingLowPU
 
-Run2_2017_trackingLowPU = cms.ModifierChain(Run2_2017, trackingLowPU)
+Run2_2017_trackingLowPU = cms.ModifierChain(run2_2017_noTrackingModifier, trackingLowPU)
 
