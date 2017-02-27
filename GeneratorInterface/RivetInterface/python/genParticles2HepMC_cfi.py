ecca89f611694ec5529aa4f55becc79120f5802c
Junghwan John Goh <jhgoh@cern.ch>
2017-02-27 12:43:23 +0100

    merge GenParticles2HepMCConverterHTXS into the original GenParticles2HepMCConverter

--- GeneratorInterface/RivetInterface/python/genParticles2HepMC_cfi.py
+++ GeneratorInterface/RivetInterface/python/genParticles2HepMC_cfi.py
@@ -3,4 +3,6 @@ import FWCore.ParameterSet.Config as cms
 genParticles2HepMC = cms.EDProducer("GenParticles2HepMCConverter",
     genParticles = cms.InputTag("genParticles"),
     genEventInfo = cms.InputTag("generator"),
+    signalParticlePdgIds = cms.vint32(),
+    #signalParticlePdgIds = cms.vint32(25), ## for the Higgs analysis
 )
