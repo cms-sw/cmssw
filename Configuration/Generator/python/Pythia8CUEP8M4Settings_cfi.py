17633b170154083c4146fc480ca23a216950b073
pgunnell <paolo.gunnellini@cern.ch>
2017-03-01 11:45:22 +0100

    Update Pythia8CUEP8M4Settings_cfi.py

--- Configuration/Generator/python/Pythia8CUEP8M4Settings_cfi.py
+++ Configuration/Generator/python/Pythia8CUEP8M4Settings_cfi.py
@@ -2,9 +2,9 @@ import FWCore.ParameterSet.Config as cms
 
 pythia8CUEP8M4SettingsBlock = cms.PSet(
     pythia8CUEP8M4Settings = cms.vstring(
-        'Tune:pp 14',
+              'Tune:pp 14',
 	      'Tune:ee 7',
-        'Diffraction:PomFlux=4',
+              'Diffraction:PomFlux=4',
 	      'Diffraction:PomFluxEpsilon=0.1195',
 	      'Diffraction:PomFluxAlphaPrime=0.1417',
 	      'MultipartonInteractions:ecmRef=13000.0',
