8a42ca87792fe641efe8836bb152e4836d1abb22
Martin Grunewald <Martin.Grunewald@cern.ch>
2017-03-01 13:29:06 +0100

    CMSHLT-1201

--- HLTrigger/Configuration/python/HLT_HIon_cff.py
+++ HLTrigger/Configuration/python/HLT_HIon_cff.py
@@ -1,11 +1,11 @@
-# /dev/CMSSW_9_0_0/HIon/V7 (CMSSW_9_0_0_pre5)
+# /dev/CMSSW_9_0_0/HIon/V8 (CMSSW_9_0_0_pre5)
 
 import FWCore.ParameterSet.Config as cms
 
 fragment = cms.ProcessFragment( "HLT" )
 
 fragment.HLTConfigVersion = cms.PSet(
-  tableName = cms.string('/dev/CMSSW_9_0_0/HIon/V7')
+  tableName = cms.string('/dev/CMSSW_9_0_0/HIon/V8')
 )
 
 fragment.HLTPSetJetCoreStepTrajectoryFilter = cms.PSet( 
--- HLTrigger/Configuration/test/OnLine_HLT_HIon.py
+++ HLTrigger/Configuration/test/OnLine_HLT_HIon.py
@@ -1,11 +1,11 @@
-# /dev/CMSSW_9_0_0/HIon/V7 (CMSSW_9_0_0_pre5)
+# /dev/CMSSW_9_0_0/HIon/V8 (CMSSW_9_0_0_pre5)
 
 import FWCore.ParameterSet.Config as cms
 
 process = cms.Process( "HLTHIon" )
 
 process.HLTConfigVersion = cms.PSet(
-  tableName = cms.string('/dev/CMSSW_9_0_0/HIon/V7')
+  tableName = cms.string('/dev/CMSSW_9_0_0/HIon/V8')
 )
 
 process.HLTPSetJetCoreStepTrajectoryFilter = cms.PSet( 
