f889357129e0bcfa4a5ceae96baf50738e10d464
Jean-Baptiste Sauvan <jean-baptiste.sauvan@cern.ch>
2017-03-02 22:05:40 +0100

    Add interface to retrieve neighbors with the trigger geometry

--- L1Trigger/L1THGCal/interface/HGCalTriggerGeometryGenericMapping.h
+++ L1Trigger/L1THGCal/interface/HGCalTriggerGeometryGenericMapping.h
@@ -158,6 +158,8 @@ class HGCalTriggerGeometryGenericMapping : public HGCalTriggerGeometryBase {
   virtual geom_ordered_set getOrderedCellsFromModule( const unsigned cell_det_id ) const override final;
   virtual geom_ordered_set getOrderedTriggerCellsFromModule( const unsigned trigger_cell_det_id ) const override final;
 
+  virtual geom_set getNeighborsFromTriggerCell( const unsigned trigger_cell_det_id ) const override final;
+
   virtual GlobalPoint getTriggerCellPosition(const unsigned trigger_cell_det_id) const override final;
   virtual GlobalPoint getModulePosition(const unsigned module_det_id) const override final;
 
