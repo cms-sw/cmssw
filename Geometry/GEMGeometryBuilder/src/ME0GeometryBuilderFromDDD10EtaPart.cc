9a338d1a0459a74aa589380009414a29e372ef86
Jason Lee <jason.lee@cern.ch>
2017-03-03 00:59:00 +0900

    remove me0 rotation

--- Geometry/GEMGeometryBuilder/src/ME0GeometryBuilderFromDDD10EtaPart.cc
+++ Geometry/GEMGeometryBuilder/src/ME0GeometryBuilderFromDDD10EtaPart.cc
@@ -366,7 +366,8 @@ ME0GeometryBuilderFromDDD10EtaPart::boundPlane(const DDFilteredView& fv,
   Basic3DVector<float> newX(1.,0.,0.);
   Basic3DVector<float> newY(0.,0.,1.);
   Basic3DVector<float> newZ(0.,1.,0.);
-  newY *= -1;
+  // Odd chambers are inverted in gem.xml
+  if (isOddChamber) newY *= -1;
 
   rotResult.rotateAxes(newX, newY, newZ);
 
