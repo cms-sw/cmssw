3ea96542d01e2663b9a5b85f02f19e8a3b96921c
Matti Kortelainen <matti.kortelainen@cern.ch>
2017-03-02 22:10:54 +0100

    Improve IfLogDebug and IfLogTrace unit test
    Addressing comments by Chris Jones.

--- FWCore/MessageService/test/UnitTestClient_Hd.cc
+++ FWCore/MessageService/test/UnitTestClient_Hd.cc
@@ -16,17 +16,12 @@ void
                            , edm::EventSetup const & /*unused*/
                               )
 {
-#ifdef EDM_ML_DEBUG
-       const bool debug1 = true;
-       const bool debug2 = false;
-#endif
-
        LogTrace    ("cat_A") << "LogTrace was used to send this mess" << "age";
        LogDebug    ("cat_B") << "LogDebug was used to send this other message";
-       IfLogTrace(debug1, "cat_A") << "IfLogTrace was used to send this message";
-       IfLogTrace(debug2, "cat_A") << "IfLogTrace was used to not send this message";
-       IfLogDebug(debug1, "cat_B") << "IfLogDebug was used to send this other message";
-       IfLogDebug(debug2, "cat_B") << "IfLogDebug was used to not send other this message";
+       IfLogTrace(true , "cat_A") << "IfLogTrace was used to send this message";
+       IfLogTrace(false, "cat_A") << "IfLogTrace was used to not send this message";
+       IfLogDebug(true , "cat_B") << "IfLogDebug was used to send this other message";
+       IfLogDebug(false, "cat_B") << "IfLogDebug was used to not send other this message";
   edm::LogVerbatim ("cat_A") << "LogVerbatim was us" << "ed to send this message";
   edm::LogInfo     ("cat_B") << "LogInfo was used to send this other message";
 }  // MessageLoggerClient::analyze()
