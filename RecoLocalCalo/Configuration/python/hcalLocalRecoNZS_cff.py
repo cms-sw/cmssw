9cd33f1e8957749fb333c8fe5813de5fd0f6f1a1
Kevin Pedro <kpedro88@gmail.com>
2017-03-02 18:09:13 -0600

    clone rechit merger for NZS

--- RecoLocalCalo/Configuration/python/hcalLocalRecoNZS_cff.py
+++ RecoLocalCalo/Configuration/python/hcalLocalRecoNZS_cff.py
@@ -39,8 +39,10 @@ _phase1_hfrecoMB = RecoLocalCalo.HcalRecProducers.HFPhase1Reconstructor_cfi.hfre
         rejectAllFailures = cms.bool(False)
     ),
 )
-from RecoLocalCalo.HcalRecProducers.hbheplan1_cfi import hbheplan1
-hbheplan1.hbheInput = cms.InputTag("hbheprerecoMB")
+import RecoLocalCalo.HcalRecProducers.hbheplan1_cfi
+hbheplan1MB = RecoLocalCalo.HcalRecProducers.hbheplan1_cfi.hbheplan1.clone(
+    hbheInput = cms.InputTag("hbheprerecoMB")
+)
 
 _phase1_hcalLocalRecoSequenceNZS = hcalLocalRecoSequenceNZS.copy()
 _phase1_hcalLocalRecoSequenceNZS.insert(0,hfprerecoMB)
@@ -55,5 +57,5 @@ _plan1_hcalLocalRecoSequenceNZS = _phase1_hcalLocalRecoSequenceNZS.copy()
 hbheprerecoMB = _phase1_hbherecoMB.clone()
 _plan1_hcalLocalRecoSequenceNZS.insert(0,hbheprerecoMB)
 from Configuration.Eras.Modifier_run2_HEPlan1_2017_cff import run2_HEPlan1_2017
-run2_HEPlan1_2017.toReplaceWith(hbherecoMB, hbheplan1)
+run2_HEPlan1_2017.toReplaceWith(hbherecoMB, hbheplan1MB)
 run2_HEPlan1_2017.toReplaceWith(hcalLocalRecoSequenceNZS, _plan1_hcalLocalRecoSequenceNZS)
