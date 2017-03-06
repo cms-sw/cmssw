d577303ca156aab5aef988dddc54647d94fc5813
Martin Grunewald <Martin.Grunewald@cern.ch>
2017-03-06 18:23:22 +0100

    Small bug fixes

--- HLTrigger/Configuration/test/OnLine_HLT_PIon.py
+++ HLTrigger/Configuration/test/OnLine_HLT_PIon.py
@@ -1,11 +1,11 @@
-# /dev/CMSSW_9_0_0/PIon/V8 (CMSSW_9_0_0_pre5)
+# /dev/CMSSW_9_0_0/PIon/V9 (CMSSW_9_0_0_pre5)
 
 import FWCore.ParameterSet.Config as cms
 
 process = cms.Process( "HLTPIon" )
 
 process.HLTConfigVersion = cms.PSet(
-  tableName = cms.string('/dev/CMSSW_9_0_0/PIon/V8')
+  tableName = cms.string('/dev/CMSSW_9_0_0/PIon/V9')
 )
 
 process.HLTPSetJetCoreStepTrajectoryFilter = cms.PSet( 
@@ -18297,7 +18297,7 @@ process.hltTrigReport = cms.EDAnalyzer( "HLTrigReport",
     serviceBy = cms.untracked.string( "never" ),
     resetBy = cms.untracked.string( "never" ),
     reportBy = cms.untracked.string( "job" ),
-    HLTriggerResults = cms.InputTag( 'TriggerResults','','HLTFULL' )
+    HLTriggerResults = cms.InputTag( 'TriggerResults','','HLT' )
 )
 process.hltPrePhysicsCommissioningOutput = cms.EDFilter( "HLTPrescaler",
     L1GtReadoutRecordTag = cms.InputTag( "hltGtStage2Digis" ),
