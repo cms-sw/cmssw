0786ff2d9b89ee7953e65a6606f597f04127c774
aspiezia <aniello.spiezia@cern.ch>
2017-03-08 13:21:57 +0100

    TkHistoMap to std::unique_ptr<TkHistoMap>

--- DQM/TrackerMonitorTrack/src/MonitorTrackResiduals.cc
+++ DQM/TrackerMonitorTrack/src/MonitorTrackResiduals.cc
@@ -41,7 +41,7 @@ void MonitorTrackResidualsBase<pixel_or_strip>::bookHistograms(DQMStore::IBooker
   std::string topFolderName_ = "SiStrip";
   SiStripFolderOrganizer folder_organizer;
   folder_organizer.setSiStripFolderName(topFolderName_);
-  tkhisto_ResidualsMean = new TkHistoMap(ibooker , topFolderName_ ,"TkHMap_ResidualsMean", 0.0,true);
+  tkhisto_ResidualsMean = std::unique_ptr<TkHistoMap>(new TkHistoMap(ibooker , topFolderName_ ,"TkHMap_ResidualsMean", 0.0,true)); 
 }
 
 template<TrackerType pixel_or_strip>
