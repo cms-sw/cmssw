f4056af96d502843c1225f35b99e58ba630a2437
Dmitrijus <dmitrijus@users.noreply.github.com>
2017-03-08 17:37:37 +0100

    Set addEndJob to False in cmsDriver

--- Configuration/Applications/python/cmsDriverOptions.py
+++ Configuration/Applications/python/cmsDriverOptions.py
@@ -124,7 +124,7 @@ def OptionsFromItems(items):
     options.step = options.step.replace("SIM_CHAIN","GEN,SIM,DIGI,L1,DIGI2RAW")
 
     # add on the end of job sequence...
-    addEndJob = True
+    addEndJob = False
     if ("FASTSIM" in options.step and not "VALIDATION" in options.step) or "HARVESTING" in options.step or "ALCAHARVEST" in options.step or "ALCAOUTPUT" in options.step or options.step == "": 
         addEndJob = False
     if ("SKIM" in options.step and not "RECO" in options.step):
