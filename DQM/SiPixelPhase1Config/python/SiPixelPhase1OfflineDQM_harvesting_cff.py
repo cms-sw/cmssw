8fed83db371e9e64720b11b72b944844482f6cd4
Duncan <leggat@cern.ch>
2017-03-09 16:00:25 +0100

    Updating cff files to run the qtests and summary maps

--- DQM/SiPixelPhase1Config/python/SiPixelPhase1OfflineDQM_harvesting_cff.py
+++ DQM/SiPixelPhase1Config/python/SiPixelPhase1OfflineDQM_harvesting_cff.py
@@ -4,13 +4,15 @@ from  DQM.SiPixelPhase1Config.SiPixelPhase1OfflineDQM_source_cff import *
 
 siPixelPhase1OfflineDQM_harvesting = cms.Sequence(SiPixelPhase1RawDataHarvester 
                                                 + SiPixelPhase1DigisHarvester 
-                                                + SiPixelPhase1ClustersHarvester
+                                                  + SiPixelPhase1ClustersHarvester
                                                 + SiPixelPhase1RecHitsHarvester
                                                 + SiPixelPhase1TrackResidualsHarvester
                                                 + SiPixelPhase1TrackClustersHarvester
                                                 + SiPixelPhase1TrackEfficiencyHarvester
                                                 + SiPixelPhase1RawDataHarvester
-                                                )
+                                                + RunQTests_offline
+                                                + SiPixelPhase1Summary_Offline
+                                                  )
 
 siPixelPhase1OfflineDQM_harvesting_cosmics = siPixelPhase1OfflineDQM_harvesting.copyAndExclude([
    SiPixelPhase1TrackClustersHarvester,
