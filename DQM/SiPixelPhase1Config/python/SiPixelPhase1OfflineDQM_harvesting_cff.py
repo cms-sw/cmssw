287531fe8e8a6e8047d372e21bca3d14d560927c
Duncan <leggat@cern.ch>
2017-03-09 16:06:19 +0100

    Updating indentations

--- DQM/SiPixelPhase1Config/python/SiPixelPhase1OfflineDQM_harvesting_cff.py
+++ DQM/SiPixelPhase1Config/python/SiPixelPhase1OfflineDQM_harvesting_cff.py
@@ -4,7 +4,7 @@ from  DQM.SiPixelPhase1Config.SiPixelPhase1OfflineDQM_source_cff import *
 
 siPixelPhase1OfflineDQM_harvesting = cms.Sequence(SiPixelPhase1RawDataHarvester 
                                                 + SiPixelPhase1DigisHarvester 
-                                                  + SiPixelPhase1ClustersHarvester
+                                                + SiPixelPhase1ClustersHarvester
                                                 + SiPixelPhase1RecHitsHarvester
                                                 + SiPixelPhase1TrackResidualsHarvester
                                                 + SiPixelPhase1TrackClustersHarvester
