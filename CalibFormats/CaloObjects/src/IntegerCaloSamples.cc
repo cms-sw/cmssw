4753f92904bf000dfe23b4b1ba572bab16b7f63e
Kevin Pedro <kpedro88@gmail.com>
2017-03-10 13:55:45 -0600

    allow any size CaloSamples

--- CalibFormats/CaloObjects/src/IntegerCaloSamples.cc
+++ CalibFormats/CaloObjects/src/IntegerCaloSamples.cc
@@ -1,11 +1,9 @@
 #include "CalibFormats/CaloObjects/interface/IntegerCaloSamples.h"
 
-IntegerCaloSamples::IntegerCaloSamples() : id_(), size_(0), presamples_(0) {
-  for (int i=0; i<MAXSAMPLES; i++) data_[i]=0;
+IntegerCaloSamples::IntegerCaloSamples() : id_(), size_(0), presamples_(0), data_(size_,0) {
 }
 
-IntegerCaloSamples::IntegerCaloSamples(const DetId& id, int size) : id_(id), size_(size), presamples_(0) {
-  for (int i=0; i<MAXSAMPLES; i++) data_[i]=0;
+IntegerCaloSamples::IntegerCaloSamples(const DetId& id, int size) : id_(id), size_(size), presamples_(0), data_(size_,0) {
 }
 
 void IntegerCaloSamples::setPresamples(int pre) {
