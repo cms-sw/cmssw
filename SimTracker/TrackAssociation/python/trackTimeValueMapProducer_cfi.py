5bc3d1c561ea76df10171704224ee84c53380c89
Josh Bendavid <Josh.Bendavid@cern.ch>
2017-03-14 01:24:07 +0100

    impose realistic acceptance cuts for track timing maps by default

--- SimTracker/TrackAssociation/python/trackTimeValueMapProducer_cfi.py
+++ SimTracker/TrackAssociation/python/trackTimeValueMapProducer_cfi.py
@@ -10,5 +10,8 @@ trackTimeValueMapProducer = cms.EDProducer(
                                             resolutionInNs = cms.double(0.030) ),
                                   cms.PSet( modelName = cms.string('PerfectResolutionModel') ) ),
     etaMin = cms.double(-1.0),
-    etaMax = cms.double(10.0)
+    etaMax = cms.double(3.0),
+    ptMin = cms.double(0.7),
+    pMin = cms.double(0.7),
+    etaMaxForPtThreshold = cms.double(1.5),
     )
