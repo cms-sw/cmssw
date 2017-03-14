6ee3a0e8795e338efa5a17c543de6889dd7b2292
Gregor Mittag <Gregor.Mittag@cern.ch>
2017-03-14 11:41:40 +0100

    Removed `EventSetup` from `endOfLoop` methods in alignment monitors.
     - needed for unification of alignment producers
     - in addition:
       - `EventSetup` was nowhere used
       - in case of zero processed events it would be undefined

--- Alignment/CommonAlignmentMonitor/interface/AlignmentMonitorBase.h
+++ Alignment/CommonAlignmentMonitor/interface/AlignmentMonitorBase.h
@@ -61,7 +61,7 @@ class AlignmentMonitorBase
       void duringLoop(const edm::Event &iEvent, const edm::EventSetup &iSetup, const ConstTrajTrackPairCollection &iTrajTracks);
 
       /// Called at end of loop: don't reimplement
-      void endOfLoop(const edm::EventSetup &iSetup);
+      void endOfLoop();
 
       /// Called at end of processing: don't implement
       void endOfJob() {}
@@ -76,7 +76,7 @@ class AlignmentMonitorBase
 
       /// Called after updating AlignableTracker and AlignableMuon (by
       /// "endOfLoop()"): may be reimplemented
-      virtual void afterAlignment(const edm::EventSetup &iSetup) { }
+      virtual void afterAlignment() { }
 
    protected:
       /// Use this every time you book a histogram (so that
