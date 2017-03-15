c9b929682b907d9dd2e2ec7d433ed2a15fac8bae
Emanuele Di Marco <emanuele.dimarco@gmail.com>
2017-03-15 15:48:17 +0100

    customization needed for HLT sequences for PR 17794, since the PF ECAL rechit producers now need a new input collection, which is the selective readout flags (in hltEcalDigis collection)

--- HLTrigger/Configuration/python/customizeHLTforCMSSW.py
+++ HLTrigger/Configuration/python/customizeHLTforCMSSW.py
@@ -7,21 +7,20 @@ from HLTrigger.Configuration.common import *
 # - put the PR number into the name of the function
 # - add a short comment
 
-# for example:
-
-# CCCTF tuning
-# def customiseFor12718(process):
-#     for pset in process._Process__psets.values():
-#         if hasattr(pset,'ComponentType'):
-#             if (pset.ComponentType == 'CkfBaseTrajectoryFilter'):
-#                 if not hasattr(pset,'minGoodStripCharge'):
-#                     pset.minGoodStripCharge = cms.PSet(refToPSet_ = cms.string('HLTSiStripClusterChargeCutNone'))
-#     return process
+# Matching ECAL selective readout in particle flow, need a new input with online Selective Readout Flags
+def customiseFor17794(process):
+     for edproducer in process._Process__producers.values():
+         if hasattr(edproducer,'producers'):
+             for pset in edproducer.producers:
+                 if (pset.name == 'PFEBRecHitCreator' or pset.name == 'PFEERecHitCreator'):
+                     if not hasattr(pset,'srFlags'):
+                         pset.srFlags = cms.InputTag('hltEcalDigis')
+     return process
 
 
 # CMSSW version specific customizations
 def customizeHLTforCMSSW(process, menuType="GRun"):
     # add call to action function in proper order: newest last!
-    # process = customiseFor12718(process)
+    process = customiseFor17794(process)
 
     return process
