c2edc3d19a2af7a2be9eae7945cec77ad7642fa1
Vladimir <vladimir.rekovic@cern.ch>
2017-03-15 14:23:36 +0100

    New Layer1 calibs (binned in tower eta) now in renamed caloStage2Params_2017_v1_2_inconsistent_cfi. Customisation also provided.

--- L1Trigger/Configuration/python/customiseSettings.py
+++ L1Trigger/Configuration/python/customiseSettings.py
@@ -1,12 +1,12 @@
 import os.path
 import FWCore.ParameterSet.Config as cms
 
-def L1TSettingsToCaloStage2Params_2017_v1_1(process):
-    process.load("L1Trigger.L1TCalorimeter.caloStage2Params_2017_v1_1_cfi")
+def L1TSettingsToCaloStage2Params_2017_v1_2_inconsistent(process):
+    process.load("L1Trigger.L1TCalorimeter.caloStage2Params_2017_v1_2_inconsistent_cfi")
     return process
 
-def L1TSettingsToCaloStage2Params_2017_v1_0_HI_inconsistent(process):
-    process.load("L1Trigger.L1TCalorimeter.caloStage2Params_2017_v1_0_HI_inconsistent_cfi")
+def L1TSettingsToCaloStage2Params_2017_v1_1(process):
+    process.load("L1Trigger.L1TCalorimeter.caloStage2Params_2017_v1_1_cfi")
     return process
 
 def L1TSettingsToCaloStage2Params_2017_v1_0_inconsistent(process):
