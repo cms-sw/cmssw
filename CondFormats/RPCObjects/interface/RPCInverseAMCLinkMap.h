5791261666ae122d3770f04e603d789b08649419
Filip <Filip.Thyssen@cern.ch>
2017-03-15 09:38:00 +0100

    add CondFormats for RPCTwinMuxDigiToRaw, only DependentRecords

---
+++ CondFormats/RPCObjects/interface/RPCInverseAMCLinkMap.h
@@ -0,0 +1,34 @@
+#ifndef CondFormats_RPCObjects_RPCInverseAMCLinkMap_h
+#define CondFormats_RPCObjects_RPCInverseAMCLinkMap_h
+
+#include <map>
+
+#include "CondFormats/RPCObjects/interface/RPCAMCLink.h"
+#include "CondFormats/RPCObjects/interface/RPCLBLink.h"
+
+class RPCInverseAMCLinkMap
+{
+public:
+    typedef std::multimap<RPCLBLink, RPCAMCLink> map_type;
+
+public:
+    RPCInverseAMCLinkMap();
+
+    map_type & getMap();
+    map_type const & getMap() const;
+
+protected:
+    map_type map_;
+};
+
+inline RPCInverseAMCLinkMap::map_type & RPCInverseAMCLinkMap::getMap()
+{
+    return map_;
+}
+
+inline RPCInverseAMCLinkMap::map_type const & RPCInverseAMCLinkMap::getMap() const
+{
+    return map_;
+}
+
+#endif // CondFormats_RPCObjects_RPCInverseAMCLinkMap_h
