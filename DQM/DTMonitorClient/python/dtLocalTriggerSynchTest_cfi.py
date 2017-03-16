57025390a6696fb69f35f363fad8a4ef8117e8c4
jfernan2 <Javier.Fernandez@cern.ch>
2017-03-16 16:07:36 +0100

    Update dtLocalTriggerSynchTest_cfi.py

--- DQM/DTMonitorClient/python/dtLocalTriggerSynchTest_cfi.py
+++ DQM/DTMonitorClient/python/dtLocalTriggerSynchTest_cfi.py
@@ -6,7 +6,7 @@ triggerSynchTest = cms.EDAnalyzer("DTLocalTriggerSynchTest",
     # run in online environment
     runOnline = cms.untracked.bool(True),
     # kind of trigger data processed by DTLocalTriggerTask
-    hwSources = cms.untracked.vstring('TM'),
+    hwSources = cms.untracked.vstring('TM','DDU'),
     # false if DTLocalTriggerTask used LTC digis
     localrun = cms.untracked.bool(True),                         
     # root folder for booking of histograms
@@ -27,3 +27,14 @@ triggerSynchTest = cms.EDAnalyzer("DTLocalTriggerSynchTest",
 )
 
 
+from Configuration.Eras.Modifier_run2_common_cff import run2_common
+run2_common.toModify( triggerSynchTest, hwSources = cms.untracked.vstring('TM'))
+
+from Configuration.Eras.Modifier_run2_25ns_specific_cff import run2_25ns_specific
+run2_25ns_specific.toModify( triggerSynchTest, hwSources = cms.untracked.vstring('TM'))
+
+from Configuration.Eras.Modifier_run2_HI_specific_cff import run2_HI_specific
+run2_HI_specific.toModify( triggerSynchTest, hwSources = cms.untracked.vstring('TM'))
+
+from Configuration.Eras.Modifier_pA_2016_cff import pA_2016
+pA_2016.toModify( triggerSynchTest, hwSources = cms.untracked.vstring('TM'))
