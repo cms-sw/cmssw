f363134c465fd6767eb7c652af69fe10a44cde1e
Sunanda <sunanda.banerjee@cern.ch>
2017-03-17 22:54:44 +0100

    Backport bug fix (as in PR 17773)

--- Geometry/HcalCommonData/interface/HcalDDDRecConstants.h
+++ Geometry/HcalCommonData/interface/HcalDDDRecConstants.h
@@ -117,7 +117,8 @@ public:
   bool                      isPlan1MergedId(const HcalDetId& id) const { return detIdSpR_.find(id) != detIdSpR_.end(); };
        
 private:
-  void                      getOneEtaBin(int subdet, int ieta, int zside,
+
+  void                      getOneEtaBin(HcalSubdetector subdet, int ieta, int zside,
 					 std::vector<std::pair<int,double>>& phis,
 					 std::map<int,int>& layers, bool planOne,
 					 std::vector<HcalDDDRecConstants::HcalEtaBin>& bins) const;
@@ -125,6 +126,8 @@ private:
   unsigned int              layerGroupSize(int eta) const;
   unsigned int              layerGroup(int eta, int i) const;
 
+  static const int           maxLayer_=18;
+  static const int           maxLayerHB_=16;
   const HcalParameters      *hpar;
   const HcalDDDSimConstants &hcons;
   std::vector<std::pair<int,int> > etaSimValu; // eta ranges at Sim stage
