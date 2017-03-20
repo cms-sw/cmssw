8a63641c9a4ae5f77a02ca0cd6cb0547e4dd6858
Gregor Mittag <Gregor.Mittag@cern.ch>
2017-03-20 18:40:39 +0100

    Remove tabs and trailing whitespace. Purely cosmetic and no functional changes.

--- Alignment/ReferenceTrajectories/interface/GblPoint.h
+++ Alignment/ReferenceTrajectories/interface/GblPoint.h
@@ -51,131 +51,131 @@ typedef Eigen::Matrix<double, 5, 5> Matrix5d;
 
 namespace gbl {
 
-/// Point on trajectory
-/**
- * User supplied point on (initial) trajectory.
- *
- * Must have jacobian for propagation from previous point. May have:
- *
- *   -# Measurement (1D - 5D)
- *   -# Scatterer (thin, 2D kinks)
- *   -# Additional local parameters (with derivatives). Fitted together with track parameters.
- *   -# Additional global parameters (with labels and derivatives). Not fitted, only passed
- *      on to (binary) file for fitting with Millepede-II.
- */
-class GblPoint {
-public:
-	GblPoint(const Matrix5d &aJacobian);
-	GblPoint(const GblPoint&) = default;
-	GblPoint& operator=(const GblPoint&) = default;
-	GblPoint(GblPoint&&) = default;
-	GblPoint& operator=(GblPoint&&) = default;
-	virtual ~GblPoint();
+  /// Point on trajectory
+  /**
+   * User supplied point on (initial) trajectory.
+   *
+   * Must have jacobian for propagation from previous point. May have:
+   *
+   *   -# Measurement (1D - 5D)
+   *   -# Scatterer (thin, 2D kinks)
+   *   -# Additional local parameters (with derivatives). Fitted together with track parameters.
+   *   -# Additional global parameters (with labels and derivatives). Not fitted, only passed
+   *      on to (binary) file for fitting with Millepede-II.
+   */
+  class GblPoint {
+  public:
+    GblPoint(const Matrix5d &aJacobian);
+    GblPoint(const GblPoint&) = default;
+    GblPoint& operator=(const GblPoint&) = default;
+    GblPoint(GblPoint&&) = default;
+    GblPoint& operator=(GblPoint&&) = default;
+    virtual ~GblPoint();
 #ifdef GBL_EIGEN_SUPPORT_ROOT
-	// input via ROOT
-	GblPoint(const TMatrixD &aJacobian);
-	void addMeasurement(const TMatrixD &aProjection, const TVectorD &aResiduals,
-			const TVectorD &aPrecision, double minPrecision = 0.);
-	void addMeasurement(const TMatrixD &aProjection, const TVectorD &aResiduals,
-			const TMatrixDSym &aPrecision, double minPrecision = 0.);
-	void addMeasurement(const TVectorD &aResiduals, const TVectorD &aPrecision,
-			double minPrecision = 0.);
-	void addMeasurement(const TVectorD &aResiduals,
-			const TMatrixDSym &aPrecision, double minPrecision = 0.);
-	void addScatterer(const TVectorD &aResiduals, const TVectorD &aPrecision);
-	void addScatterer(const TVectorD &aResiduals,
-			const TMatrixDSym &aPrecision);
-	void addLocals(const TMatrixD &aDerivatives);
-	void addGlobals(const std::vector<int> &aLabels,
-			const TMatrixD &aDerivatives);
+    // input via ROOT
+    GblPoint(const TMatrixD &aJacobian);
+    void addMeasurement(const TMatrixD &aProjection, const TVectorD &aResiduals,
+                        const TVectorD &aPrecision, double minPrecision = 0.);
+    void addMeasurement(const TMatrixD &aProjection, const TVectorD &aResiduals,
+                        const TMatrixDSym &aPrecision, double minPrecision = 0.);
+    void addMeasurement(const TVectorD &aResiduals, const TVectorD &aPrecision,
+                        double minPrecision = 0.);
+    void addMeasurement(const TVectorD &aResiduals,
+                        const TMatrixDSym &aPrecision, double minPrecision = 0.);
+    void addScatterer(const TVectorD &aResiduals, const TVectorD &aPrecision);
+    void addScatterer(const TVectorD &aResiduals,
+                      const TMatrixDSym &aPrecision);
+    void addLocals(const TMatrixD &aDerivatives);
+    void addGlobals(const std::vector<int> &aLabels,
+                    const TMatrixD &aDerivatives);
 #endif
-	// input via Eigen
-	template <typename Projection, typename Residuals, typename Precision,
-	          typename std::enable_if<(Residuals::ColsAtCompileTime == 1)>::type* = nullptr,
-	          typename std::enable_if<(Precision::ColsAtCompileTime != 1)>::type* = nullptr>
-	void addMeasurement(const Eigen::MatrixBase<Projection>& aProjection,
-	                    const Eigen::MatrixBase<Residuals>& aResiduals,
-	                    const Eigen::MatrixBase<Precision>& aPrecision,
-	                    double minPrecision = 0.);
-	template <typename Projection, typename Residuals, typename Precision,
-	          typename std::enable_if<(Residuals::ColsAtCompileTime == 1)>::type* = nullptr,
-	          typename std::enable_if<(Precision::ColsAtCompileTime == 1)>::type* = nullptr>
-	void addMeasurement(const Eigen::MatrixBase<Projection>& aProjection,
-	                    const Eigen::MatrixBase<Residuals>& aResiduals,
-	                    const Eigen::MatrixBase<Precision>& aPrecision,
-	                    double minPrecision = 0.);
-	template <typename Residuals, typename Precision,
-	          typename std::enable_if<(Residuals::ColsAtCompileTime == 1)>::type* = nullptr,
-	          typename std::enable_if<(Precision::ColsAtCompileTime != 1)>::type* = nullptr>
-	void addMeasurement(const Eigen::MatrixBase<Residuals>& aResiduals,
-	                    const Eigen::MatrixBase<Precision>& aPrecision,
-	                    double minPrecision = 0.);
-	template <typename Residuals, typename Precision,
-	          typename std::enable_if<(Residuals::ColsAtCompileTime == 1)>::type* = nullptr,
-	          typename std::enable_if<(Precision::ColsAtCompileTime == 1)>::type* = nullptr>
-	void addMeasurement(const Eigen::MatrixBase<Residuals>& aResiduals,
-	                    const Eigen::MatrixBase<Precision>& aPrecision,
-	                    double minPrecision = 0.);
-	template <typename Precision>
-	void addScatterer(const Eigen::Vector2d &aResiduals,
-	                  const Eigen::MatrixBase<Precision>& aPrecision);
-	void addScatterer(const Eigen::Vector2d &aResiduals,
-	                  const Eigen::Vector2d& aPrecision);
-	//
-	unsigned int hasMeasurement() const;
-	double getMeasPrecMin() const;
-	void getMeasurement(Matrix5d &aProjection, Vector5d &aResiduals,
-			Vector5d &aPrecision) const;
-	void getMeasTransformation(Eigen::MatrixXd &aTransformation) const;
-	bool hasScatterer() const;
-	void getScatterer(Eigen::Matrix2d &aTransformation, Eigen::Vector2d &aResiduals,
-			Eigen::Vector2d &aPrecision) const;
-	void getScatTransformation(Eigen::Matrix2d &aTransformation) const;
-	template <typename Derivative>
-	void addLocals(const Eigen::MatrixBase<Derivative>& aDerivatives);
-	unsigned int getNumLocals() const;
-	const Eigen::MatrixXd& getLocalDerivatives() const;
-	template <typename Derivative>
-	void addGlobals(const std::vector<int> &aLabels,
-			const Eigen::MatrixBase<Derivative>& aDerivatives);
-	unsigned int getNumGlobals() const;
-	void getGlobalLabels(std::vector<int> &aLabels) const;
-	void getGlobalDerivatives(Eigen::MatrixXd &aDerivatives) const;
-	void getGlobalLabelsAndDerivatives(unsigned int aRow,
-			std::vector<int> &aLabels, std::vector<double> &aDerivatives) const;
-	void setLabel(unsigned int aLabel);
-	unsigned int getLabel() const;
-	void setOffset(int anOffset);
-	int getOffset() const;
-	const Matrix5d& getP2pJacobian() const;
-	void addPrevJacobian(const Matrix5d &aJac);
-	void addNextJacobian(const Matrix5d &aJac);
-	void getDerivatives(int aDirection, Eigen::Matrix2d &matW, Eigen::Matrix2d &matWJ,
-			Eigen::Vector2d &vecWd) const;
-	void printPoint(unsigned int level = 0) const;
+    // input via Eigen
+    template <typename Projection, typename Residuals, typename Precision,
+              typename std::enable_if<(Residuals::ColsAtCompileTime == 1)>::type* = nullptr,
+              typename std::enable_if<(Precision::ColsAtCompileTime != 1)>::type* = nullptr>
+    void addMeasurement(const Eigen::MatrixBase<Projection>& aProjection,
+                        const Eigen::MatrixBase<Residuals>& aResiduals,
+                        const Eigen::MatrixBase<Precision>& aPrecision,
+                        double minPrecision = 0.);
+    template <typename Projection, typename Residuals, typename Precision,
+              typename std::enable_if<(Residuals::ColsAtCompileTime == 1)>::type* = nullptr,
+              typename std::enable_if<(Precision::ColsAtCompileTime == 1)>::type* = nullptr>
+    void addMeasurement(const Eigen::MatrixBase<Projection>& aProjection,
+                        const Eigen::MatrixBase<Residuals>& aResiduals,
+                        const Eigen::MatrixBase<Precision>& aPrecision,
+                        double minPrecision = 0.);
+    template <typename Residuals, typename Precision,
+              typename std::enable_if<(Residuals::ColsAtCompileTime == 1)>::type* = nullptr,
+              typename std::enable_if<(Precision::ColsAtCompileTime != 1)>::type* = nullptr>
+    void addMeasurement(const Eigen::MatrixBase<Residuals>& aResiduals,
+                        const Eigen::MatrixBase<Precision>& aPrecision,
+                        double minPrecision = 0.);
+    template <typename Residuals, typename Precision,
+              typename std::enable_if<(Residuals::ColsAtCompileTime == 1)>::type* = nullptr,
+              typename std::enable_if<(Precision::ColsAtCompileTime == 1)>::type* = nullptr>
+    void addMeasurement(const Eigen::MatrixBase<Residuals>& aResiduals,
+                        const Eigen::MatrixBase<Precision>& aPrecision,
+                        double minPrecision = 0.);
+    template <typename Precision>
+    void addScatterer(const Eigen::Vector2d &aResiduals,
+                      const Eigen::MatrixBase<Precision>& aPrecision);
+    void addScatterer(const Eigen::Vector2d &aResiduals,
+                      const Eigen::Vector2d& aPrecision);
+    //
+    unsigned int hasMeasurement() const;
+    double getMeasPrecMin() const;
+    void getMeasurement(Matrix5d &aProjection, Vector5d &aResiduals,
+                        Vector5d &aPrecision) const;
+    void getMeasTransformation(Eigen::MatrixXd &aTransformation) const;
+    bool hasScatterer() const;
+    void getScatterer(Eigen::Matrix2d &aTransformation, Eigen::Vector2d &aResiduals,
+                      Eigen::Vector2d &aPrecision) const;
+    void getScatTransformation(Eigen::Matrix2d &aTransformation) const;
+    template <typename Derivative>
+    void addLocals(const Eigen::MatrixBase<Derivative>& aDerivatives);
+    unsigned int getNumLocals() const;
+    const Eigen::MatrixXd& getLocalDerivatives() const;
+    template <typename Derivative>
+    void addGlobals(const std::vector<int> &aLabels,
+                    const Eigen::MatrixBase<Derivative>& aDerivatives);
+    unsigned int getNumGlobals() const;
+    void getGlobalLabels(std::vector<int> &aLabels) const;
+    void getGlobalDerivatives(Eigen::MatrixXd &aDerivatives) const;
+    void getGlobalLabelsAndDerivatives(unsigned int aRow,
+                                       std::vector<int> &aLabels, std::vector<double> &aDerivatives) const;
+    void setLabel(unsigned int aLabel);
+    unsigned int getLabel() const;
+    void setOffset(int anOffset);
+    int getOffset() const;
+    const Matrix5d& getP2pJacobian() const;
+    void addPrevJacobian(const Matrix5d &aJac);
+    void addNextJacobian(const Matrix5d &aJac);
+    void getDerivatives(int aDirection, Eigen::Matrix2d &matW, Eigen::Matrix2d &matWJ,
+                        Eigen::Vector2d &vecWd) const;
+    void printPoint(unsigned int level = 0) const;
 
-private:
-	unsigned int theLabel; ///< Label identifying point
-	int theOffset; ///< Offset number at point if not negative (else interpolation needed)
-	Matrix5d p2pJacobian; ///< Point-to-point jacobian from previous point
-	Matrix5d prevJacobian; ///< Jacobian to previous scatterer (or first measurement)
-	Matrix5d nextJacobian; ///< Jacobian to next scatterer (or last measurement)
-	unsigned int measDim; ///< Dimension of measurement (1-5), 0 indicates absence of measurement
-	double measPrecMin; ///< Minimal measurement precision (for usage)
-	Matrix5d measProjection; ///< Projection from measurement to local system
-	Vector5d measResiduals; ///< Measurement residuals
-	Vector5d measPrecision; ///< Measurement precision (diagonal of inverse covariance matrix)
-	bool transFlag; ///< Transformation exists?
-	Eigen::Matrix<double, Eigen::Dynamic, Eigen::Dynamic,
-	              Eigen::ColMajor /* default */, 5, 5> measTransformation; ///< Transformation of diagonalization (of meas. precision matrix)
-	bool scatFlag; ///< Scatterer present?
-	Eigen::Matrix2d scatTransformation; ///< Transformation of diagonalization (of scat. precision matrix)
-	Eigen::Vector2d scatResiduals; ///< Scattering residuals (initial kinks if iterating)
-	Eigen::Vector2d scatPrecision; ///< Scattering precision (diagonal of inverse covariance matrix)
-	Eigen::MatrixXd localDerivatives; ///< Derivatives of measurement vs additional local (fit) parameters
-	std::vector<int> globalLabels; ///< Labels of global (MP-II) derivatives
-	Eigen::MatrixXd globalDerivatives; ///< Derivatives of measurement vs additional global (MP-II) parameters
-};
+  private:
+    unsigned int theLabel; ///< Label identifying point
+    int theOffset; ///< Offset number at point if not negative (else interpolation needed)
+    Matrix5d p2pJacobian; ///< Point-to-point jacobian from previous point
+    Matrix5d prevJacobian; ///< Jacobian to previous scatterer (or first measurement)
+    Matrix5d nextJacobian; ///< Jacobian to next scatterer (or last measurement)
+    unsigned int measDim; ///< Dimension of measurement (1-5), 0 indicates absence of measurement
+    double measPrecMin; ///< Minimal measurement precision (for usage)
+    Matrix5d measProjection; ///< Projection from measurement to local system
+    Vector5d measResiduals; ///< Measurement residuals
+    Vector5d measPrecision; ///< Measurement precision (diagonal of inverse covariance matrix)
+    bool transFlag; ///< Transformation exists?
+    Eigen::Matrix<double, Eigen::Dynamic, Eigen::Dynamic,
+                  Eigen::ColMajor /* default */, 5, 5> measTransformation; ///< Transformation of diagonalization (of meas. precision matrix)
+    bool scatFlag; ///< Scatterer present?
+    Eigen::Matrix2d scatTransformation; ///< Transformation of diagonalization (of scat. precision matrix)
+    Eigen::Vector2d scatResiduals; ///< Scattering residuals (initial kinks if iterating)
+    Eigen::Vector2d scatPrecision; ///< Scattering precision (diagonal of inverse covariance matrix)
+    Eigen::MatrixXd localDerivatives; ///< Derivatives of measurement vs additional local (fit) parameters
+    std::vector<int> globalLabels; ///< Labels of global (MP-II) derivatives
+    Eigen::MatrixXd globalDerivatives; ///< Derivatives of measurement vs additional global (MP-II) parameters
+  };
 
 
   /// Add a measurement to a point.
