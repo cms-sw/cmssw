be085ac754ec44f27b1067b54cff728baf360b44
Tyler Ruggles <truggles@wisc.edu>
2017-03-20 18:08:42 +0100

    Run L1EG test on official Phase-2 l1trigger ntuple

--- L1Trigger/L1CaloTrigger/python/test_L1EGCrystalCluster_cfg.py
+++ L1Trigger/L1CaloTrigger/python/test_L1EGCrystalCluster_cfg.py
@@ -1,6 +1,8 @@
 import FWCore.ParameterSet.Config as cms
 
-process = cms.Process("L1AlgoTest")
+from Configuration.StandardSequences.Eras import eras
+
+process = cms.Process("L1AlgoTest",eras.phase2_common)
 
 process.load('Configuration.StandardSequences.Services_cff')
 process.load("FWCore.MessageService.MessageLogger_cfi")
@@ -15,7 +17,9 @@ process.maxEvents = cms.untracked.PSet( input = cms.untracked.int32(-1) )
 process.source = cms.Source("PoolSource",
     # In DAS see: file dataset=/RelValZEE_14/CMSSW_9_0_0_pre2-90X_upgrade2023_realistic_v1_2023D4-v1/GEN-SIM-DIGI-RAW
    #fileNames = cms.untracked.vstring('file:root://cmsxrootd.fnal.gov///store/relval/CMSSW_8_1_0_pre11/RelValSingleElectronPt35Extended/GEN-SIM-DIGI-RAW/PU25ns_81X_mcRun2_asymptotic_v5_2023D1PU140-v1/00000/0ADFD7B5-4277-E611-8E89-0025905A6132.root')
-   fileNames = cms.untracked.vstring('file:root://cmsxrootd.fnal.gov///store/relval/CMSSW_9_0_0_pre2/RelValZEE_14/GEN-SIM-DIGI-RAW/90X_upgrade2023_realistic_v1_2023D4-v1/10000/48F2EE36-04C2-E611-AD16-0CC47A7C360E.root')
+   #fileNames = cms.untracked.vstring('file:root://cmsxrootd.fnal.gov///store/relval/CMSSW_9_0_0_pre2/RelValZEE_14/GEN-SIM-DIGI-RAW/90X_upgrade2023_realistic_v1_2023D4-v1/10000/48F2EE36-04C2-E611-AD16-0CC47A7C360E.root')
+   #fileNames = cms.untracked.vstring('file:/data/truggles/step2_ZEE_PU200_10ev_FEVTDEBUGHLT_customHigherPtTrackParticles-RERUN_L1T_TTAssociator_EcalEBtp_HGCtp.root')
+   fileNames = cms.untracked.vstring('file:root://eoscms//eos/cms/store/group/dpg_trigger/comm_trigger/L1Trigger/rekovic/PhaseIIFall16DR82-820_backport_L1TMC_v1.2.2/step2_ZEE_PU200_100ev_FEVTDEBUGHLT_customHigherPtTrackParticles.root')
 )
 
 # All this stuff just runs the various EG algorithms that we are studying
@@ -23,22 +27,13 @@ process.source = cms.Source("PoolSource",
 # ---- Global Tag :
 process.load('Configuration.StandardSequences.FrontierConditions_GlobalTag_cff')
 from Configuration.AlCa.GlobalTag import GlobalTag
-#process.GlobalTag = GlobalTag(process.GlobalTag, 'PH2_1K_FB_V3::All', '')
-process.GlobalTag = GlobalTag(process.GlobalTag, 'auto:run2_mc', '')
+process.GlobalTag = GlobalTag(process.GlobalTag, 'auto:phase2_realistic', '')
 
 # Choose a 2023 geometry!
 process.load('Configuration.Geometry.GeometryExtended2023D7Reco_cff')
+#process.load('Configuration.Geometry.GeometryExtended2023D4Reco_cff') # Geom used by L1Trig, doesn't have Ecal EndCap, breaks CaloGeomHelper
 process.load('Configuration.StandardSequences.MagneticField_38T_PostLS1_cff')
 
-
-
-# --------------------------------------------------------------------------------------------
-#
-# ----    Produce the HCAL TPs
-# This gives errors.  It looks like I have some geometry alignment
-# issue.
-#process.load('SimCalorimetry.Configuration.hcalDigiSequence_cff')
-#process.hcalTPs = cms.Path( process.hcalDigiSequence )
 
 
 # --------------------------------------------------------------------------------------------
@@ -77,8 +72,10 @@ process.L1EGammaCrystalsProducer = cms.EDProducer("L1EGCrystalClusterProducer",
    useECalEndcap = cms.bool(False),
    useRecHits = cms.bool(False),
    ecalTPEB = cms.InputTag("EcalEBTrigPrimProducer","","L1AlgoTest"),
+   #ecalTPEB = cms.InputTag("simEcalEBTriggerPrimitiveDigis","","HLT"),
    ecalRecHitEB = cms.InputTag("ecalRecHit","EcalRecHitsEB","RECO"),
    hcalRecHit = cms.InputTag("hbhereco"),
+   hcalTP = cms.InputTag("simHcalTriggerPrimitiveDigis","","HLT"),
    useTowerMap = cms.untracked.bool(False)
 )
 
@@ -90,8 +87,12 @@ process.pL1EG = cms.Path( process.L1EGammaCrystalsProducer )
 process.Out = cms.OutputModule( "PoolOutputModule",
     fileName = cms.untracked.string( "l1egCrystalTest.root" ),
     fastCloning = cms.untracked.bool( False ),
-    outputCommands = cms.untracked.vstring( "keep *_L1EGammaCrystalsProducer_*_*",
-                    "keep *_TriggerResults_*_*")
+    outputCommands = cms.untracked.vstring(
+                    "keep *_L1EGammaCrystalsProducer_*_*",
+                    "keep *_TriggerResults_*_*",
+                    "keep *_simHcalTriggerPrimitiveDigis_*_*",
+                    "keep *_EcalEBTrigPrimProducer_*_*"
+                    )
 )
 
 process.end = cms.EndPath( process.Out )
