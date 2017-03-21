e64b07faaeb8fc5112f2ffe8edfb069d4732df7b
Seth <semoortg@vub.ac.be>
2017-03-21 11:57:27 +0100

    Add new PatJet collection with needed TagInfos

--- Validation/RecoB/python/BDHadronTrackValidation_cff.py
+++ Validation/RecoB/python/BDHadronTrackValidation_cff.py
@@ -1,9 +1,24 @@
 import FWCore.ParameterSet.Config as cms
 
+# Need to add pfImpactParameterTagInfos to the pat jets --> make personal patJet collection
+from PhysicsTools.PatAlgos.producersLayer1.jetProducer_cfi import _patJets as patJets
+patJetsBDHadron = patJets.clone(
+    tagInfoSources = cms.VInputTag(cms.InputTag('pfImpactParameterTagInfos')),
+    addTagInfos = cms.bool(True)
+)
+
+selectedPatJetsBDHadron = cms.EDFilter("PATJetSelector",
+    src = cms.InputTag("patJetsBDHadron"),
+    cut = cms.string("pt > 10.")
+)
+
+
 # my analyzer
 from Validation.RecoB.BDHadronTrackMonitoring_cfi import *
-BDHadronTrackMonitoringAnalyze.PatJetSource = cms.InputTag('selectedPatJets')
+BDHadronTrackMonitoringAnalyze.PatJetSource = cms.InputTag('selectedPatJetsBDHadron')#'selectedPatJets')
 
-bdHadronTrackValidationSeq = cms.Sequence(BDHadronTrackMonitoringAnalyze)
+
+
+bdHadronTrackValidationSeq = cms.Sequence(patJetsBDHadron * selectedPatJetsBDHadron * BDHadronTrackMonitoringAnalyze)
 
 bdHadronTrackPostProcessor = cms.Sequence(BDHadronTrackMonitoringHarvest)
