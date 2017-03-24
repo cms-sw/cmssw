2b59df3254c05dd0bc356f275f31aa2339c48f04
Fabrizio <Fabrizio.Ferro@ge.infn.it>
2017-03-24 14:25:59 +0100

    ->rawDataCollector

--- EventFilter/CTPPSRawToDigi/python/ctppsPixelRawToDigi_cfi.py
+++ EventFilter/CTPPSRawToDigi/python/ctppsPixelRawToDigi_cfi.py
@@ -3,7 +3,8 @@ import FWCore.ParameterSet.Config as cms
 
 ctppsPixelDigis = cms.EDProducer(
     "CTPPSPixelRawToDigi",
-    InputLabel = cms.InputTag("ctppsPixelRawData"),
+    #    InputLabel = cms.InputTag("ctppsPixelRawData"),
+    InputLabel = cms.InputTag("rawDataCollector"),
     mappingLabel = cms.string("RPix")
-
-)
+    
+    )
