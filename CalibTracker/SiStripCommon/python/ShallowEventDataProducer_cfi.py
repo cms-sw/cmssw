38e0ef41cdea6dbb9af9565d26b43436f2d0bf9e
mmusich <marco.musich@cern.ch>
2017-03-24 10:22:32 +0100

    Backport to 80X of #17419 #17570 and #17981

--- CalibTracker/SiStripCommon/python/ShallowEventDataProducer_cfi.py
+++ CalibTracker/SiStripCommon/python/ShallowEventDataProducer_cfi.py
@@ -2,5 +2,6 @@ import FWCore.ParameterSet.Config as cms
 
 shallowEventRun = cms.EDProducer(
    "ShallowEventDataProducer",
-   trigRecord = cms.InputTag('gtDigis')
+   trigRecord = cms.InputTag('gtDigis'),
+   lumiScalers = cms.InputTag("scalersRawToDigi") 
    )
