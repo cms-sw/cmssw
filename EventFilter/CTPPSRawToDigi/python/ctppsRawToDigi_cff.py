cda51cad7b0c6bcc4b2d3ffea402f5186bd1023d
Fabrizio <Fabrizio.Ferro@ge.infn.it>
2017-03-27 17:07:20 +0200

    added test for pixels

--- EventFilter/CTPPSRawToDigi/python/ctppsRawToDigi_cff.py
+++ EventFilter/CTPPSRawToDigi/python/ctppsRawToDigi_cff.py
@@ -59,9 +59,27 @@ from EventFilter.CTPPSRawToDigi.ctppsDiamondRawToDigi_cfi import ctppsDiamondRaw
 ctppsDiamondRawToDigi.rawDataTag = cms.InputTag("rawDataCollector")
 
 
+# pixels
+ctppsPixelDAQMappingESSourceXML = cms.ESSource("CTPPSPixelDAQMappingESSourceXML",
+  verbosity = cms.untracked.uint32(0),
+  subSystem= cms.untracked.string("RPix"),
+  configuration = cms.VPSet(
+  # example configuration block:
+  cms.PSet(
+    validityRange = cms.EventRange("1:min - 999999999:max"),
+    mappingFileNames = cms.vstring("CondFormats/CTPPSReadoutObjects/xml/rpix_mapping_220_far.xml"),
+    maskFileNames = cms.vstring("CondFormats/CTPPSReadoutObjects/xml/rpix_channel_mask_220_far.xml")
+    )
+  )
+)
+
+from EventFilter.CTPPSRawToDigi.ctppsPixelRawToDigi_cfi import ctppsPixelDigis
+ctppsPixelDigis.rawDataTag = cms.InputTag("rawDataCollector")
+
 # raw-to-digi sequence
 ctppsRawToDigi = cms.Sequence(
   totemTriggerRawToDigi *
   totemRPRawToDigi *
-  ctppsDiamondRawToDigi
+  ctppsDiamondRawToDigi*
+  ctppsPixelDigis
 )
---
+++ EventFilter/CTPPSRawToDigi/test/test_pixels_only_cfg.py
@@ -0,0 +1,50 @@
+import FWCore.ParameterSet.Config as cms
+
+process = cms.Process("CTPPSRawToDigiTestPixelsOnly")
+
+# minimum of logs
+process.MessageLogger = cms.Service("MessageLogger",
+    statistics = cms.untracked.vstring(),
+    destinations = cms.untracked.vstring('cerr'),
+    cerr = cms.untracked.PSet( threshold = cms.untracked.string('DEBUG') )
+)
+
+# raw data source
+process.source = cms.Source("PoolSource",
+  fileNames = cms.untracked.vstring(
+    #'file:/afs/cern.ch/user/j/jkaspar/public/run273062_ls0001-2_stream.root'
+   # '/store/express/Run2016H/ExpressPhysics/FEVT/Express-v2/000/283/877/00000/4EE44B0E-2499-E611-A155-02163E011938.root'
+'file:/afs/cern.ch/work/k/kas/public/PXtrees/PixelAlive_1294_151_RAW_v2.root'
+  ),
+labelRawDataLikeMC = cms.untracked.bool(False) # for testing H8 data
+)
+
+process.maxEvents = cms.untracked.PSet(
+  input = cms.untracked.int32(-1)
+)
+
+# raw-to-digi conversion
+process.load("EventFilter.CTPPSRawToDigi.ctppsRawToDigi_cff")
+ 
+process.ctppsPixelDAQMappingESSourceXML.configuration = cms.VPSet(
+    # example configuration block:
+    cms.PSet(
+        validityRange = cms.EventRange("1:min - 999999999:max"),
+        mappingFileNames = cms.vstring("CondFormats/CTPPSReadoutObjects/xml/rpix_mapping_220_far_TEST.xml"),
+        maskFileNames = cms.vstring("CondFormats/CTPPSReadoutObjects/xml/rpix_channel_mask_TEST.xml")
+        )
+    )
+
+process.p = cms.Path(
+  process.ctppsPixelDigis
+)
+
+# output configuration
+process.output = cms.OutputModule("PoolOutputModule",
+  fileName = cms.untracked.string("file:./ctpps_pixel_digi.root"),
+  outputCommands = cms.untracked.vstring(
+    'keep *'
+ )
+)
+
+process.outpath = cms.EndPath(process.output)
