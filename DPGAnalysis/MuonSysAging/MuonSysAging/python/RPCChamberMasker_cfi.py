6122e6ca32a5636d3f4755dbeea710e453b535e6
Carlo Battilana <carlo.battilana@gmail.com>
2017-03-27 15:12:03 +0200

    Removed MuonSysAging duplicate

--- DPGAnalysis/MuonSysAging/MuonSysAging/python/RPCChamberMasker_cfi.py
+++
@@ -1,6 +0,0 @@
-import FWCore.ParameterSet.Config as cms
-
-RPCChamberMasker = cms.EDProducer('RPCChamberMasker',
-                                 digiTag = cms.InputTag('preRPCDigis'),
-                                 maskedRPCIDs = cms.vint32()
-)
