cc27e5f7abaf1abcbf14bf298f30f93df21fb334
Alberto Sanchez Hernandez <alberto.sanchez@mail.com>
2017-03-29 14:19:40 -0600

    reworking the logic, in order to reduce the amount of pruners

--- PhysicsTools/PatAlgos/python/slimming/prunedGenParticles_cfi.py
+++ PhysicsTools/PatAlgos/python/slimming/prunedGenParticles_cfi.py
@@ -5,13 +5,17 @@ prunedGenParticles = cms.EDProducer("GenParticlePruner",
     select = cms.vstring(
         "drop  *", # this is the default
         "++keep abs(pdgId) == 11 || abs(pdgId) == 13 || abs(pdgId) == 15", # keep leptons, with history
-        "keep abs(pdgId) == 12 || abs(pdgId) == 14 || abs(pdgId) == 16",   # keep neutrinos
         "drop   status == 2",                                              # drop the shower part of the history
+        "keep++ (400 < abs(pdgId) < 600) || (4000 < abs(pdgId) < 6000)",   # keep decays for BPH studies
+        "drop status == 1",                                                # drop the status=1 from BPH
+        "keep+ (400 < abs(pdgId) < 600) || (4000 < abs(pdgId) < 6000)",    # but keep first daughter, to allow lifetime determinations
+        "keep abs(pdgId) == 11 || abs(pdgId) == 13 || abs(pdgId) == 15",   # keep leptons (also status1)
+        "keep abs(pdgId) == 12 || abs(pdgId) == 14 || abs(pdgId) == 16",   # keep neutrinos
         "+keep pdgId == 22 && status == 1 && (pt > 10 || isPromptFinalState())", # keep gamma above 10 GeV (or all prompt) and its first parent
         "+keep abs(pdgId) == 11 && status == 1 && (pt > 3 || isPromptFinalState())", # keep first parent of electrons above 3 GeV (or prompt)
         "keep++ abs(pdgId) == 15",                                         # but keep keep taus with decays
-	"drop  status > 30 && status < 70 ", 				   #remove pythia8 garbage
-	"drop  pdgId == 21 && pt < 5",                                    #remove pythia8 garbage
+	"drop  status > 30 && status < 70 ", 				   # remove pythia8 garbage
+	"drop  pdgId == 21 && pt < 5",                                     # remove pythia8 garbage
         "drop   status == 2 && abs(pdgId) == 21",                          # but remove again gluons in the inheritance chain
         "keep abs(pdgId) == 23 || abs(pdgId) == 24 || abs(pdgId) == 25 || abs(pdgId) == 6 || abs(pdgId) == 37 ",   # keep VIP(articles)s
         "keep abs(pdgId) == 310 && abs(eta) < 2.5 && pt > 1 ",                                                     # keep K0
@@ -20,8 +24,6 @@ prunedGenParticles = cms.EDProducer("GenParticlePruner",
 	"keep (4 <= abs(pdgId) <= 5)",
 # keep light-flavour quarks and gluons for parton-based jet flavour
 	"keep (1 <= abs(pdgId) <= 3 || pdgId = 21) & (status = 2 || status = 11 || status = 71 || status = 72) && pt>5", 
-# keep b and c hadrons for hadron-based jet flavour, and decays for BPH studies
-	"keep++ (400 < abs(pdgId) < 600) || (4000 < abs(pdgId) < 6000)",
 # keep onia states, phi, X(3872), Z(4430)+ and psi(4040)
         "keep+ abs(pdgId) == 333",
         "keep+ abs(pdgId) == 9920443 || abs(pdgId) == 9042413 || abs(pdgId) == 9000443",
