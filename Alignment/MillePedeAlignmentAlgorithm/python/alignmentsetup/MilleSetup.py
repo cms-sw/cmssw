869114ef9bc18e5514498c650cef787a50292808
Gregor Mittag <Gregor.Mittag@cern.ch>
2017-03-30 13:40:31 +0200

    Add utility to easily add EDFilter to mille step.

--- Alignment/MillePedeAlignmentAlgorithm/python/alignmentsetup/MilleSetup.py
+++ Alignment/MillePedeAlignmentAlgorithm/python/alignmentsetup/MilleSetup.py
@@ -51,3 +51,4 @@ def setup(process, input_files, collection,
     # --------------------------------------------------------------------------
     process.p = cms.Path(process.TrackRefittingSequence*
                          process.AlignmentProducer)
+    if hasattr(process, "mps_filters"): process.p.insert(0, process.mps_filters)
