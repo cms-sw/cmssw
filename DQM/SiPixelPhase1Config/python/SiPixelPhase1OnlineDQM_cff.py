cb1fc659fdc11b60ac0c277cae5bb4a0776fd4d6
Francesco Fiori <francesco.fiori@cern.ch>
2017-03-31 18:41:28 +0200

    Excluding QTest and summary by the timing client (Backport)

--- DQM/SiPixelPhase1Config/python/SiPixelPhase1OnlineDQM_cff.py
+++ DQM/SiPixelPhase1Config/python/SiPixelPhase1OnlineDQM_cff.py
@@ -60,7 +60,7 @@ StandardSpecifications1D_Num.append(
   )
 
   
-#To Configure Phase1 DQM for Phase0 data
+# To Configure Phase1 DQM for Phase0 data
 SiPixelPhase1Geometry.upgradePhase = 1
 
 # Turn on 'online' harvesting. This has to be set before other configs are 
@@ -92,6 +92,8 @@ from DQM.SiPixelPhase1RawData.SiPixelPhase1RawData_cfi import *
 
 from DQM.SiPixelPhase1Common.SiPixelPhase1GeometryDebug_cfi import *
 
+#Summary maps
+from DQM.SiPixelPhase1Summary.SiPixelPhase1Summary_cfi import *
 
 siPixelPhase1OnlineDQM_source = cms.Sequence(
    SiPixelPhase1DigisAnalyzer
@@ -104,5 +106,12 @@ siPixelPhase1OnlineDQM_harvesting = cms.Sequence(
    SiPixelPhase1DigisHarvester 
  + SiPixelPhase1ClustersHarvester
  + SiPixelPhase1RawDataHarvester
+ + RunQTests_online
+ + SiPixelPhase1Summary_Online
 # + SiPixelPhase1GeometryDebugHarvester
 )
+
+siPixelPhase1OnlineDQM_timing_harvesting = siPixelPhase1OnlineDQM_harvesting.copyAndExclude([
+ RunQTests_online,
+ SiPixelPhase1Summary_Online,
+])
