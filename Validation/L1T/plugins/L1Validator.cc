b206198b5ed06d1a938dea156c3af7cbc11dc839
Li <li.yuan@cern.ch>
2017-03-31 22:49:45 +0200

    update to new collections for filling histograms in L1TriggerVsGen

--- Validation/L1T/plugins/L1Validator.cc
+++ Validation/L1T/plugins/L1Validator.cc
@@ -55,13 +55,12 @@ L1Validator::L1Validator(const edm::ParameterSet& iConfig){
   _dirName = iConfig.getParameter<std::string>("dirName");
   _GenSource = consumes<reco::GenParticleCollection> (iConfig.getParameter<edm::InputTag>("GenSource"));
 
-  _L1ExtraIsoEGSource = consumes<l1extra::L1EmParticleCollection> (iConfig.getParameter<edm::InputTag>("L1ExtraIsoEGSource"));
-  _L1ExtraNonIsoEGSource = consumes<l1extra::L1EmParticleCollection> (iConfig.getParameter<edm::InputTag>("L1ExtraNonIsoEGSource"));
-  _L1ExtraCenJetSource = consumes<l1extra::L1JetParticleCollection> (iConfig.getParameter<edm::InputTag>("L1ExtraCenJetSource"));
-  _L1ExtraForJetSource = consumes<l1extra::L1JetParticleCollection> (iConfig.getParameter<edm::InputTag>("L1ExtraForJetSource"));
-  _L1ExtraTauJetSource = consumes<l1extra::L1JetParticleCollection> (iConfig.getParameter<edm::InputTag>("L1ExtraTauJetSource"));
-  _L1ExtraMuonSource = consumes<l1extra::L1MuonParticleCollection> (iConfig.getParameter<edm::InputTag>("L1ExtraMuonSource"));
-  //_L1ExtraMETSource = consumes<l1extra::L1EtMissParticleCollection> (iConfig.getParameter<edm::InputTag>("L1ExtraMETSource"));
+  _L1MuonBXSource = consumes<l1t::MuonBxCollection> (iConfig.getParameter<edm::InputTag>("L1MuonBXSource"));
+  _L1EGammaBXSource = consumes<l1t::EGammaBxCollection> (iConfig.getParameter<edm::InputTag>("L1EGammaBXSource"));
+  _L1TauBXSource = consumes<l1t::TauBxCollection> (iConfig.getParameter<edm::InputTag>("L1TauBXSource"));
+  _L1JetBXSource = consumes<l1t::JetBxCollection> (iConfig.getParameter<edm::InputTag>("L1JetBXSource"));
+  _srcToken = mayConsume<GenEventInfoProduct>( iConfig.getParameter<edm::InputTag>("srcToken") );
+  _L1GenJetSource = consumes<reco::GenJetCollection>( iConfig.getParameter<edm::InputTag>("L1GenJetSource"));
 
   //_fileName = iConfig.getParameter<std::string>("fileName");
 }
@@ -82,78 +81,141 @@ void L1Validator::analyze(const edm::Event& iEvent, const edm::EventSetup& iSetu
   using namespace reco;
 
   Handle<GenParticleCollection> GenParticles;
-  Handle<L1EmParticleCollection> IsoEGs;
-  Handle<L1EmParticleCollection> NonIsoEGs;
-  Handle<L1JetParticleCollection> CenJets;
-  Handle<L1JetParticleCollection> ForJets;
-  Handle<L1JetParticleCollection> Taus;
-  Handle<L1MuonParticleCollection> Muons;
-  //Handle<L1EtMissParticleCollection> METs;
+  Handle<l1t::MuonBxCollection> MuonsBX;
+  Handle<l1t::EGammaBxCollection> EGammasBX;
+  Handle<l1t::TauBxCollection> TausBX;
+  Handle<l1t::JetBxCollection> JetsBX;
+  Handle<GenEventInfoProduct> genEvtInfoProduct;
+  Handle<reco::GenJetCollection> GenJets;
 
   bool GotEverything=true;
 
   if(!iEvent.getByToken(_GenSource, GenParticles)) GotEverything=false;
-  if(!iEvent.getByToken(_L1ExtraIsoEGSource, IsoEGs)) GotEverything=false;
-  if(!iEvent.getByToken(_L1ExtraNonIsoEGSource, NonIsoEGs)) GotEverything=false;
-  if(!iEvent.getByToken(_L1ExtraCenJetSource, CenJets)) GotEverything=false;
-  if(!iEvent.getByToken(_L1ExtraForJetSource, ForJets)) GotEverything=false;
-  if(!iEvent.getByToken(_L1ExtraTauJetSource, Taus)) GotEverything=false;
-  if(!iEvent.getByToken(_L1ExtraMuonSource, Muons)) GotEverything=false;
+  if(!iEvent.getByToken(_L1MuonBXSource, MuonsBX)) GotEverything=false;
+  if(!iEvent.getByToken(_L1EGammaBXSource, EGammasBX)) GotEverything=false;
+  if(!iEvent.getByToken(_L1TauBXSource, TausBX)) GotEverything=false;
+  if(!iEvent.getByToken(_L1JetBXSource, JetsBX)) GotEverything=false;
+  if(!iEvent.getByToken(_srcToken, genEvtInfoProduct)) GotEverything=false;
+  if(!iEvent.getByToken(_L1GenJetSource, GenJets)) GotEverything=false;  
 
   if(!GotEverything) return;
 
+  /*
+  std::string moduleName = "";
+  if( genEvtInfoProduct.isValid() ) {
+	  const edm::Provenance& prov = iEvent.getProvenance(genEvtInfoProduct.id());
+	  moduleName = edm::moduleName(prov);
+	  //cout<<" generator name: "<<moduleName<<endl;
+  }
+  */ 
 
   _Hists.NEvents++;
 
-  _Hists.FillNumber(L1ValidatorHists::Type::IsoEG, IsoEGs->size());
-  _Hists.FillNumber(L1ValidatorHists::Type::NonIsoEG, NonIsoEGs->size());
-  _Hists.FillNumber(L1ValidatorHists::Type::CenJet, CenJets->size());
-  _Hists.FillNumber(L1ValidatorHists::Type::ForJet, ForJets->size());
-  _Hists.FillNumber(L1ValidatorHists::Type::TauJet, Taus->size());
-  _Hists.FillNumber(L1ValidatorHists::Type::Muon, Muons->size());
+  _Hists.FillNumber(L1ValidatorHists::Type::Muon, MuonsBX->size());
+  _Hists.FillNumber(L1ValidatorHists::Type::Egamma, EGammasBX->size());
+  _Hists.FillNumber(L1ValidatorHists::Type::Tau, TausBX->size());
+  _Hists.FillNumber(L1ValidatorHists::Type::Jet, JetsBX->size());
+
+  //For gen jet
+
+  for(uint i=0; i < GenJets->size(); i++){
+     const reco::GenJet *Genjet = &GenJets->at(i);
+     if(fabs(Genjet->eta())>4.7) continue;
+
+     double minDR = 999.0;
+     const l1t::Jet        *L1Part=NULL;
+     for(int iBx = JetsBX->getFirstBX();  iBx<=JetsBX->getLastBX(); ++iBx){
+          if(iBx>0) continue;
+          for(std::vector<l1t::Jet>::const_iterator jet = JetsBX->begin(iBx); jet != JetsBX->end(iBx); ++jet){
+                double idR = reco::deltaR(Genjet->eta(), Genjet->phi(), jet->eta(), jet->phi());
+                if( idR < minDR ){
+                         minDR = idR;
+                         L1Part = &(*jet);
+                }
+
+          }
+     }
+     _Hists.Fill(L1ValidatorHists::Type::Jet, Genjet, L1Part);
+  }
 
   for(uint i=0; i < GenParticles->size(); i++){
     const GenParticle *GenPart = &GenParticles->at(i);
 
     int pdg = GenPart->pdgId(), status = GenPart->status();
 
-    if(status==1 && (abs(pdg)==11 || pdg==22)){
-      FINDRECOPART(L1EmParticle, IsoEGs, NonIsoEGs)
+    double minDR = 999.0;
+    if(status==1 && abs(pdg)==13){  //Muon
+       if(fabs(GenPart->eta())>2.4) continue;
+       // calculate the isolation
+       double TotalIso30 = 0;
+       for(uint j=0; j<GenParticles->size(); j++){
+		const GenParticle *oGenPart = &GenParticles->at(j);
+		if(oGenPart == GenPart)continue;
+		if(oGenPart->status() !=1) continue;
+		double jdR =  reco::deltaR(GenPart->eta(), GenPart->phi(), oGenPart->eta(), oGenPart->phi()); 
+		if(jdR < 0.3) TotalIso30 += oGenPart->pt(); 
+       }
+       //if(TotalIso30/GenPart->pt() > 0.15)continue; 
+       // match L1T object
+       const l1t::Muon 	*L1Part=NULL;
+       for(int iBx = MuonsBX->getFirstBX();  iBx<=MuonsBX->getLastBX(); ++iBx){
+	  if(iBx>0) continue;
+          for(std::vector<l1t::Muon>::const_iterator mu = MuonsBX->begin(iBx); mu != MuonsBX->end(iBx); ++mu){
+	  	double idR = reco::deltaR(GenPart->eta(), GenPart->phi(), mu->eta(), mu->phi());  
+		if(idR < minDR ){
+			 minDR = idR;
+			 L1Part = &(*mu);
+		}
+	 		
+	  }
+          _Hists.Fill(L1ValidatorHists::Type::Muon, GenPart, L1Part);
+       }
 
-      if(RecoPart==NULL){
- 	_Hists.Fill(L1ValidatorHists::Type::IsoEG, GenPart, NULL);
- 	_Hists.Fill(L1ValidatorHists::Type::NonIsoEG, GenPart, NULL);
-      }else if(RecoPart->type() == L1EmParticle::EmType::kIsolated){
- 	_Hists.Fill(L1ValidatorHists::Type::IsoEG, GenPart, RecoPart);
- 	_Hists.Fill(L1ValidatorHists::Type::NonIsoEG, GenPart, NULL);
-      }else if(RecoPart->type() == L1EmParticle::EmType::kNonIsolated){
- 	_Hists.Fill(L1ValidatorHists::Type::IsoEG, GenPart, NULL);
- 	_Hists.Fill(L1ValidatorHists::Type::NonIsoEG, GenPart, RecoPart);
-      }
-    }else if(status==1 && abs(pdg)==13){
-      FINDRECOPART(L1MuonParticle, Muons, Muons)
+     }  else if(status==1 && (abs(pdg)==11 || pdg==22)){  //Egamma
+       if(fabs(GenPart->eta())>2.5) continue;
+       if(fabs(GenPart->eta())>1.4442 && fabs(GenPart->eta())<1.5660) continue;
+       // calculate the isolation
+       double TotalIso30 = 0;
+       for(uint j=0; j<GenParticles->size(); j++){
+		const GenParticle *oGenPart = &GenParticles->at(j);
+		if(oGenPart == GenPart)continue;
+		if(oGenPart->status() !=1) continue;
+		double jdR =  reco::deltaR(GenPart->eta(), GenPart->phi(), oGenPart->eta(), oGenPart->phi()); 
+		if(jdR < 0.3) TotalIso30 += oGenPart->pt(); 
+       }
+       //if(TotalIso30/GenPart->pt() > 0.15)continue; 
+       // match L1T object
+       const l1t::EGamma 	*L1Part=NULL;
+       for(int iBx = EGammasBX->getFirstBX();  iBx<=EGammasBX->getLastBX(); ++iBx){
+	  if(iBx>0) continue;
+          for(std::vector<l1t::EGamma>::const_iterator eg = EGammasBX->begin(iBx); eg != EGammasBX->end(iBx); ++eg){
+	  	double idR = reco::deltaR(GenPart->eta(), GenPart->phi(), eg->eta(), eg->phi());  
+		if(idR < minDR ){
+			 minDR = idR;
+			 L1Part = &(*eg);
+		}
+	  }
+       }
+       _Hists.Fill(L1ValidatorHists::Type::Egamma, GenPart, L1Part);
 
-      _Hists.Fill(L1ValidatorHists::Type::Muon, GenPart, RecoPart);
-    }else if(status==3 && abs(pdg)==15){
-      FINDRECOPART(L1JetParticle, Taus, Taus)
+     } else if(status==2 && abs(pdg)==15){  //Tau
+       // match L1T object
+       if(fabs(GenPart->eta())>2.4) continue;
+       const l1t::Tau 	*L1Part=NULL;
+       for(int iBx = TausBX->getFirstBX();  iBx<=TausBX->getLastBX(); ++iBx){
+	  if(iBx>0) continue;
+          for(std::vector<l1t::Tau>::const_iterator tau = TausBX->begin(iBx); tau != TausBX->end(iBx); ++tau){
+	  	double idR = reco::deltaR(GenPart->eta(), GenPart->phi(), tau->eta(), tau->phi());  
+		if(idR < minDR ){
+			 minDR = idR;
+			 L1Part = &(*tau);
+		}
+	  }
+       }
+       _Hists.Fill(L1ValidatorHists::Type::Tau, GenPart, L1Part);
 
-      _Hists.Fill(L1ValidatorHists::Type::TauJet, GenPart, RecoPart);
-    }else if(status==3 && (abs(pdg)<=5 || pdg==21)){
-      FINDRECOPART(L1JetParticle, CenJets, ForJets)
+    }
 
-      if(RecoPart==NULL){
- 	_Hists.Fill(L1ValidatorHists::Type::CenJet, GenPart, NULL);
- 	_Hists.Fill(L1ValidatorHists::Type::ForJet, GenPart, NULL);
-      }else if(RecoPart->type() == L1JetParticle::JetType::kCentral){
- 	_Hists.Fill(L1ValidatorHists::Type::CenJet, GenPart, RecoPart);
- 	_Hists.Fill(L1ValidatorHists::Type::ForJet, GenPart, NULL);
-      }else if(RecoPart->type() == L1JetParticle::JetType::kForward){
- 	_Hists.Fill(L1ValidatorHists::Type::CenJet, GenPart, NULL);
- 	_Hists.Fill(L1ValidatorHists::Type::ForJet, GenPart, RecoPart);
-      }
-    }else continue;
-
-    //cout << GenPart->pt() << '\t' << GenPart->eta() << '\t' << GenPart->phi() << '\t' << GenPart->pdgId() << endl;
   }
 }
 
