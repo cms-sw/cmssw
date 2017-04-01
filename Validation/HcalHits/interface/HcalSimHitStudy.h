7103d606d7f382117b66242717c9aed6cfd110ed
Kenichi Hatakeyama <kenichi.hatakeyama@cern.ch>
2017-03-31 23:12:36 -0500

    number of depths setting now dynamic in simhit code

--- Validation/HcalHits/interface/HcalSimHitStudy.h
+++ Validation/HcalHits/interface/HcalSimHitStudy.h
@@ -48,6 +48,22 @@ private:
   const HcalDDDRecConstants               *hcons;
   int                                      maxDepthHB_, maxDepthHE_;
   int                                      maxDepthHO_, maxDepthHF_;
+  int                                      maxDepth_;
+
+  struct HistLim {
+    HistLim(int nbin, double mini, double maxi)
+    : n(nbin), min(mini), max(maxi) {
+    }
+    int n;
+    double min;
+    double max;
+  };
+
+  std::map<std::string, MonitorElement*> *msm_;
+
+  void book2D(DQMStore::IBooker &ib, std::string name, const HistLim& limX, const HistLim& limY);
+  void fill2D(std::string name, double X, double Y, double weight = 1);
+  std::string str(int x);
 
   int iphi_bins;
   float iphi_min, iphi_max;
@@ -68,8 +84,6 @@ private:
   MonitorElement *meAllNHit_, *meBadDetHit_, *meBadSubHit_, *meBadIdHit_;
   MonitorElement *meHBNHit_, *meHENHit_, *meHONHit_, *meHFNHit_;
   MonitorElement *meDetectHit_, *meSubdetHit_, *meDepthHit_, *meEtaHit_, *meEtaPhiHit_;
-  MonitorElement *meEtaPhiHitd1_, *meEtaPhiHitd2_, *meEtaPhiHitd3_, *meEtaPhiHitd4_;
-  MonitorElement *meEtaPhiHitd5_, *meEtaPhiHitd6_, *meEtaPhiHitd7_;
   MonitorElement *mePhiHit_, *mePhiHitb_, *meEnergyHit_, *meTimeHit_, *meTimeWHit_;
   MonitorElement *meHBDepHit_, *meHEDepHit_, *meHODepHit_, *meHFDepHit_, *meHFDepHitw_;
   MonitorElement *meHBEtaHit_, *meHEEtaHit_, *meHOEtaHit_, *meHFEtaHit_;
