84636380386b219aea646941f082cf3ac527514c
mmusich <marco.musich@cern.ch>
2017-04-03 15:34:00 +0200

    introduce changes in the DQM module

--- Alignment/MillePedeAlignmentAlgorithm/plugins/MillePedeDQMModule.cc
+++ Alignment/MillePedeAlignmentAlgorithm/plugins/MillePedeDQMModule.cc
@@ -36,17 +36,7 @@ MillePedeDQMModule
 ::MillePedeDQMModule(const edm::ParameterSet& config) :
   mpReaderConfig_(
     config.getParameter<edm::ParameterSet>("MillePedeFileReader")
-  ),
-
-  sigCut_     (mpReaderConfig_.getParameter<double>("sigCut")),
-  Xcut_       (mpReaderConfig_.getParameter<double>("Xcut")),
-  tXcut_      (mpReaderConfig_.getParameter<double>("tXcut")),
-  Ycut_       (mpReaderConfig_.getParameter<double>("Ycut")),
-  tYcut_      (mpReaderConfig_.getParameter<double>("tYcut")),
-  Zcut_       (mpReaderConfig_.getParameter<double>("Zcut")),
-  tZcut_      (mpReaderConfig_.getParameter<double>("tZcut")),
-  maxMoveCut_ (mpReaderConfig_.getParameter<double>("maxMoveCut")),
-  maxErrorCut_ (mpReaderConfig_.getParameter<double>("maxErrorCut"))
+  )
 {
 }
 
@@ -114,7 +104,7 @@ void MillePedeDQMModule
   setup.get<AlignPCLThresholdsRcd>().get(thresholdHandle);
   const AlignPCLThresholds* theThresholds = thresholdHandle.product();
 
-  auto myThresholds = new AlignPCLThresholds();
+  auto myThresholds = std::unique_ptr<AlignPCLThresholds>(new AlignPCLThresholds());
   myThresholds->setAlignPCLThresholds(theThresholds->getNrecords(),theThresholds->getThreshold_Map());
 
   TrackerGeomBuilderFromGeometricDet builder;
@@ -134,7 +124,7 @@ void MillePedeDQMModule
               labelerConfig)
   };
 
-  mpReader_ = std::make_unique<MillePedeFileReader>(mpReaderConfig_, pedeLabeler, std::shared_ptr<const AlignPCLThresholds>(myThresholds)); 
+  mpReader_ = std::make_unique<MillePedeFileReader>(mpReaderConfig_, pedeLabeler, std::shared_ptr<const AlignPCLThresholds>(myThresholds.get())); 
 
 }
 
@@ -142,35 +132,102 @@ void MillePedeDQMModule
 void MillePedeDQMModule
 ::fillExpertHistos()
 {
+  std::array<double, 6> Xcut_,  sigXcut_,  maxMoveXcut_,  maxErrorXcut_; 
+  std::array<double, 6> tXcut_, sigtXcut_, maxMovetXcut_, maxErrortXcut_;
+                                                    
+  std::array<double, 6> Ycut_,  sigYcut_,  maxMoveYcut_,  maxErrorYcut_; 
+  std::array<double, 6> tYcut_, sigtYcut_, maxMovetYcut_, maxErrortYcut_;
+                                                    
+  std::array<double, 6> Zcut_,  sigZcut_,  maxMoveZcut_,  maxErrorZcut_; 
+  std::array<double, 6> tZcut_, sigtZcut_, maxMovetZcut_, maxErrortZcut_;
 
-  fillExpertHisto(h_xPos,  Xcut_, sigCut_, maxMoveCut_, maxErrorCut_, mpReader_->getXobs(),  mpReader_->getXobsErr());
-  fillExpertHisto(h_xRot, tXcut_, sigCut_, maxMoveCut_, maxErrorCut_, mpReader_->getTXobs(), mpReader_->getTXobsErr());
+  auto myMap = mpReader_->getTheThresolds();
 
-  fillExpertHisto(h_yPos,  Ycut_, sigCut_, maxMoveCut_, maxErrorCut_, mpReader_->getYobs(),  mpReader_->getYobsErr());
-  fillExpertHisto(h_yRot, tYcut_, sigCut_, maxMoveCut_, maxErrorCut_, mpReader_->getTYobs(), mpReader_->getTYobsErr());
+  std::vector<std::string> alignablesList;
+  for(auto it = myMap.begin(); it != myMap.end() ; ++it){
+    alignablesList.push_back(it->first);
+  }
 
-  fillExpertHisto(h_zPos,  Zcut_, sigCut_, maxMoveCut_, maxErrorCut_, mpReader_->getZobs(),  mpReader_->getZobsErr());
-  fillExpertHisto(h_zRot, tZcut_, sigCut_, maxMoveCut_, maxErrorCut_, mpReader_->getTZobs(), mpReader_->getTZobsErr());
+  for (auto &alignable : alignablesList){
+
+    int detIndex = getIndexFromString(alignable);
+
+    Xcut_[detIndex]          = myMap[alignable].getXcut() ;
+    sigXcut_[detIndex]       = myMap[alignable].getSigXcut() ;
+    maxMoveXcut_[detIndex]   = myMap[alignable].getMaxMoveXcut() ;
+    maxErrorXcut_[detIndex]  = myMap[alignable].getErrorXcut() ;
+
+    Ycut_[detIndex]          = myMap[alignable].getYcut() ;
+    sigYcut_[detIndex]       = myMap[alignable].getSigYcut() ;
+    maxMoveYcut_[detIndex]   = myMap[alignable].getMaxMoveYcut() ; 
+    maxErrorYcut_[detIndex]  = myMap[alignable].getErrorYcut() ;
+ 
+    Zcut_[detIndex]          = myMap[alignable].getZcut() ;
+    sigZcut_[detIndex]       = myMap[alignable].getSigZcut() ;
+    maxMoveZcut_[detIndex]   = myMap[alignable].getMaxMoveZcut() ;
+    maxErrorZcut_[detIndex]  = myMap[alignable].getErrorZcut() ;
+
+    tXcut_[detIndex]         = myMap[alignable].getThetaXcut() ;
+    sigtXcut_[detIndex]      = myMap[alignable].getSigThetaXcut() ; 
+    maxMovetXcut_[detIndex]  = myMap[alignable].getMaxMoveThetaXcut() ;
+    maxErrortXcut_[detIndex] = myMap[alignable].getErrorThetaXcut() ;
+
+    tYcut_[detIndex]         = myMap[alignable].getThetaYcut() ;
+    sigtYcut_[detIndex]      = myMap[alignable].getSigThetaYcut() ;
+    maxMovetYcut_[detIndex]  = myMap[alignable].getMaxMoveThetaYcut() ;
+    maxErrortYcut_[detIndex] = myMap[alignable].getErrorThetaYcut() ;
+ 
+    tZcut_[detIndex]         = myMap[alignable].getThetaZcut() ;
+    sigtZcut_[detIndex]      = myMap[alignable].getSigThetaYcut() ;
+    maxMovetZcut_[detIndex]  = myMap[alignable].getMaxMoveThetaYcut() ;
+    maxErrortZcut_[detIndex] = myMap[alignable].getErrorThetaYcut() ;
+
+  }
+
+  fillExpertHisto(h_xPos,  Xcut_, sigXcut_,  maxMoveXcut_,  maxErrorXcut_,  mpReader_->getXobs(),  mpReader_->getXobsErr());
+  fillExpertHisto(h_xRot, tXcut_, sigtXcut_, maxMovetXcut_, maxErrortXcut_, mpReader_->getTXobs(), mpReader_->getTXobsErr());
+
+  fillExpertHisto(h_yPos,  Ycut_, sigYcut_,  maxMoveYcut_,  maxErrorYcut_,  mpReader_->getYobs(),  mpReader_->getYobsErr());
+  fillExpertHisto(h_yRot, tYcut_, sigtYcut_, maxMovetYcut_, maxErrortYcut_, mpReader_->getTYobs(), mpReader_->getTYobsErr());
+
+  fillExpertHisto(h_zPos,  Zcut_, sigZcut_,  maxMoveZcut_,  maxErrorZcut_,  mpReader_->getZobs(),  mpReader_->getZobsErr());
+  fillExpertHisto(h_zRot, tZcut_, sigtZcut_, maxMovetZcut_, maxErrortZcut_, mpReader_->getTZobs(), mpReader_->getTZobsErr());
 
 }
 
 void MillePedeDQMModule
-::fillExpertHisto(MonitorElement* histo, const double cut, const double sigCut, const double maxMoveCut, const double maxErrorCut,
+::fillExpertHisto(MonitorElement* histo, 
+		  std::array<double, 6> cut, std::array<double, 6> sigCut, 
+		  std::array<double, 6> maxMoveCut, std::array<double, 6> maxErrorCut,
                   std::array<double, 6> obs, std::array<double, 6> obsErr)
 {
   TH1F* histo_0 = histo->getTH1F();
 
-  histo_0->SetMinimum(-(maxMoveCut_));
-  histo_0->SetMaximum(  maxMoveCut_);
+  double max_ = *std::max_element(maxMoveCut.begin(),maxMoveCut.end());
+
+  histo_0->SetMinimum(-(max_));
+  histo_0->SetMaximum(  max_);
 
   for (size_t i = 0; i < obs.size(); ++i) {
     histo_0->SetBinContent(i+1, obs[i]);
     histo_0->SetBinError(i+1, obsErr[i]);
   }
-  histo_0->SetBinContent(8,cut);
-  histo_0->SetBinContent(9,sigCut);
-  histo_0->SetBinContent(10,maxMoveCut);
-  histo_0->SetBinContent(11,maxErrorCut);
+
+  for (size_t i = obs.size(); i < obs.size()*2; ++i) {
+    histo_0->SetBinContent(i+1, cut[i]);
+  }
+
+  for (size_t i = obs.size()*2; i < obs.size()*3; ++i) {
+    histo_0->SetBinContent(i+1, sigCut[i]);
+  }
+
+  for (size_t i = obs.size()*3; i < obs.size()*4; ++i) {
+    histo_0->SetBinContent(i+1, maxMoveCut[i]);
+  }
+
+  for (size_t i = obs.size()*4; i < obs.size()*5; ++i) {
+    histo_0->SetBinContent(i+1, maxErrorCut[i]);
+  }
 
 }
 
@@ -184,4 +241,29 @@ bool MillePedeDQMModule
   if (watchPTrackerParametersRcd_.check(setup)) changed = true;
 
   return changed;
+}
+
+
+int MillePedeDQMModule
+::getIndexFromString(const std::string& alignableId)
+{
+  
+  if(alignableId == "TPBHalfBarrelXminus"){
+    return 3;
+  } else if(alignableId == "TPBHalfBarrelXplus"){
+    return 2;
+  } else if(alignableId == "TPEHalfCylinderXminusZminus") {
+    return 1;
+  } else if(alignableId == "TPEHalfCylinderXplusZminus") {
+    return 0;
+  } else if(alignableId == "TPEHalfCylinderXminusZplus") {
+    return 5;
+  } else if(alignableId == "TPEHalfCylinderXplusZplus") {
+    return 4;
+  } else{
+    throw cms::Exception("LogicError") 
+      << "@SUB=MillePedeDQMModule::getIndexFromString\n"
+      << "Retrieving conversion for not supported Alignable partition" 
+      << alignableId;
+  }
 }
