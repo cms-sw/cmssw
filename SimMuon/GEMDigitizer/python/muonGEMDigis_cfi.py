afc535d704ad8d5fe1bd27b85ddd363246eb10cc
Cesare Calabria <cesare.calabria@ba.infn.it>
2017-04-03 12:06:06 +0200

    adjust neutron background parameters

--- SimMuon/GEMDigitizer/python/muonGEMDigis_cfi.py
+++ SimMuon/GEMDigitizer/python/muonGEMDigis_cfi.py
@@ -1,13 +1,12 @@
 import FWCore.ParameterSet.Config as cms
 
-# Module to create simulated GEM digis.
-simMuonGEMDigis = cms.EDProducer("GEMDigiProducer",
+gemDigiCommonParameters = cms.PSet(
     signalPropagationSpeed = cms.double(0.66),
     cosmics = cms.bool(False),
     timeResolution = cms.double(5),
     timeJitter = cms.double(1.0),
     averageShapingTime = cms.double(50.0),
-# clsParametrization = cms.vdouble(0.455091, 0.865613, 0.945891, 0.973286, 0.986234, 0.991686, 0.996865, 0.998501, 1.),
+    #clsParametrization = cms.vdouble(0.455091, 0.865613, 0.945891, 0.973286, 0.986234, 0.991686, 0.996865, 0.998501, 1.),
     averageEfficiency = cms.double(0.98),
     averageNoiseRate = cms.double(0.001), #intrinsic noise
     bxwidth = cms.int32(25),
@@ -24,6 +23,11 @@ simMuonGEMDigis = cms.EDProducer("GEMDigiProducer",
     simulateElectronBkg = cms.bool(True),	#False=simulate only neutral Bkg
     simulateLowNeutralRate = cms.bool(False),	#True=neutral_Bkg at L=1x10^{34}, False at L=5x10^{34}cm^{-2}s^{-1}
     instLumi = cms.double(7.5), # in units of 1E34 cm^-2 s^-1. Internally the background is parametrized from FLUKA+GEANT results at 5x10^34 (PU140). We are adding a 1.5 factor for PU200
-    rateFact = cms.double(2.0), # We are adding also a safety factor of 2 to take into account the new beam pipe effect (not yet known)
+    rateFact = cms.double(2.0) # We are adding also a safety factor of 2 to take into account the new beam pipe effect (not yet known)
+)
+
+# Module to create simulated GEM digis.
+simMuonGEMDigis = cms.EDProducer("GEMDigiProducer",
+    gemDigiCommonParameters
 )
 
