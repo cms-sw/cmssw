2882cdb7a819c2f19668c8b164dc9ec6bb90311b
mmusich <marco.musich@cern.ch>
2017-04-04 13:19:56 +0200

    Improve I/O and readability

--- Alignment/MillePedeAlignmentAlgorithm/interface/MillePedeFileReader.h
+++ Alignment/MillePedeAlignmentAlgorithm/interface/MillePedeFileReader.h
@@ -102,4 +102,11 @@ class MillePedeFileReader {
   
 };
 
+#include<iostream>
+const std::array<std::string,8> coord_str = {{"X", "Y", "Z", "theta_X", "theta_Y", "theta_Z", "extra_DOF", "none"}};
+inline std::ostream & operator<<(std::ostream & os, const AlignPCLThresholds::coordType& c) {
+  if (c >= AlignPCLThresholds::endOfTypes || c < 0) return os << "unrecongnized coordinate";
+  return os << coord_str[c];
+}
+
 #endif /* ALIGNMENT_MILLEPEDEALIGNMENTALGORITHM_INTERFACE_MILLEPEDEFILEREADER_H_ */
