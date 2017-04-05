2563a6bdb0dc53b0b0b682b51245a9e130518cd6
Fabrizio <Fabrizio.Ferro@ge.infn.it>
2017-04-05 10:38:20 +0200

    using auto for iterators

--- DataFormats/CTPPSDigi/src/CTPPSPixelDigiCollection.cc
+++ DataFormats/CTPPSDigi/src/CTPPSPixelDigiCollection.cc
@@ -3,26 +3,26 @@
 #include <algorithm>
 
 void CTPPSPixelDigiCollection::put(Range input, unsigned int detID) {
-  // put in Digis of detID
+/// put in Digis of detID
 
-  // store size of vector before put
+/// store size of vector before put
   IndexRange inputRange;
 
-  // put in CTPPSPixelDigis from input
+/// put in CTPPSPixelDigis from input
   bool first = true;
 
-  // fill input in temporary vector for sorting
+/// fill input in temporary vector for sorting
   std::vector<CTPPSPixelDigi> temporary;
-  CTPPSPixelDigiCollection::ContainerIterator sort_begin = input.first;
-  CTPPSPixelDigiCollection::ContainerIterator sort_end = input.second;
+  auto sort_begin = input.first;
+  auto sort_end = input.second;
   for ( ;sort_begin != sort_end; ++sort_begin ) {
     temporary.push_back(*sort_begin);
   }
   std::sort(temporary.begin(),temporary.end());
 
-  // iterators over input
-  CTPPSPixelDigiCollection::ContainerIterator begin = temporary.begin();
-  CTPPSPixelDigiCollection::ContainerIterator end = temporary.end();
+/// iterators over input
+  auto begin = temporary.begin();
+  auto end = temporary.end();
   for ( ;begin != end; ++begin ) {
     container_.push_back(*begin);
     if ( first ) {
@@ -32,14 +32,14 @@ void CTPPSPixelDigiCollection::put(Range input, unsigned int detID) {
   }
   inputRange.second = container_.size()-1;
   
-  // fill map
+/// fill map
   map_[detID] = inputRange;
 
 }
 
 const CTPPSPixelDigiCollection::Range CTPPSPixelDigiCollection::get(unsigned int detID) const {
-  // get Digis of detID
-
+/// get Digis of detID
+  
   auto found = map_.find(detID);
   CTPPSPixelDigiCollection::IndexRange returnIndexRange{};
   if(found != map_.end()) {
@@ -54,10 +54,10 @@ const CTPPSPixelDigiCollection::Range CTPPSPixelDigiCollection::get(unsigned int
 }
 
 const std::vector<unsigned int> CTPPSPixelDigiCollection::detIDs() const {
-  // returns vector of detIDs in map
+/// returns vector of detIDs in map
 
-  CTPPSPixelDigiCollection::RegistryIterator begin = map_.begin();
-  CTPPSPixelDigiCollection::RegistryIterator end   = map_.end();
+  auto begin = map_.begin();
+  auto end   = map_.end();
 
   std::vector<unsigned int> output;
 
