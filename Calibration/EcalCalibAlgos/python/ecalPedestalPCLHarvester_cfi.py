f1c46e3ba6fa10f20f6b87eae7f2273d81fd36b1
Stefano Argiro <Stefano.Argiro@cern.ch>
2017-04-05 13:42:59 +0200

    Ecal pedestal PCL first implementation

---
+++ Calibration/EcalCalibAlgos/python/ecalPedestalPCLHarvester_cfi.py
@@ -0,0 +1,14 @@
+import FWCore.ParameterSet.Config as cms
+ECALpedestalPCLHarvester = cms.EDAnalyzer('ECALpedestalPCLHarvester',
+                                          MinEntries = cms.int(100), #skip channel if stat is low
+                                          ChannelStatusToExclude = cms.vstring('kDAC',
+                                                                               'kNoisy',
+                                                                               'kNNoisy',
+                                                                               'kFixedG6',
+                                                                               'kFixedG1',
+                                                                               'kFixedG0',
+                                                                               'kNonRespondingIsolated',
+                                                                               'kDeadVFE',
+                                                                               'kDeadFE',
+                                                                               'kNoDataNoTP',)
+                                          )
