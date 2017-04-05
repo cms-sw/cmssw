2f3cc0195bdd25f10a7659b585b2fe4115a803bb
Kevin Pedro <kpedro88@gmail.com>
2017-04-05 08:17:35 -0500

    default value for new param to satisfy HLT

--- RecoLocalCalo/HcalRecProducers/src/HBHEPhase1Reconstructor.cc
+++ RecoLocalCalo/HcalRecProducers/src/HBHEPhase1Reconstructor.cc
@@ -741,7 +741,7 @@ HBHEPhase1Reconstructor::fillDescriptions(edm::ConfigurationDescriptions& descri
     desc.add<bool>("dropZSmarkedPassed");
     desc.add<bool>("tsFromDB");
     desc.add<bool>("recoParamsFromDB");
-    desc.add<bool>("saveEffectivePedestal");
+    desc.add<bool>("saveEffectivePedestal",false);
     desc.add<bool>("setNegativeFlagsQIE8");
     desc.add<bool>("setNegativeFlagsQIE11");
     desc.add<bool>("setNoiseFlagsQIE8");
