e37baa21c0ceae46763f4bac161d09c5658f531c
Rafael <rclsa@fnal.gov>
2017-04-05 15:29:04 +0200

    code review and add auxiliary products for PS and GEN matching

--- RecoEgamma/EgammaMCTools/python/pfClusterMatchedToPhotonsSelector_cfi.py
+++ RecoEgamma/EgammaMCTools/python/pfClusterMatchedToPhotonsSelector_cfi.py
@@ -2,8 +2,12 @@ import FWCore.ParameterSet.Config as cms
 
 particleFlowClusterECALMatchedToPhotons = cms.EDProducer('PFClusterMatchedToPhotonsSelector',
                                                          pfClustersTag = cms.InputTag('particleFlowClusterECAL'),
+                                                         genParticleTag = cms.InputTag('genParticles'),
                                                          trackingParticleTag = cms.InputTag('mix', 'MergedTrackTruth'),
                                                          maxDR = cms.double(0.3),
+                                                         volumeRadius_EB = cms.double(123.8),
+                                                         volumeZ_EB = cms.double(304.5),
+                                                         volumeZ_EE = cms.double(317.0)
                                                          )
 
 
