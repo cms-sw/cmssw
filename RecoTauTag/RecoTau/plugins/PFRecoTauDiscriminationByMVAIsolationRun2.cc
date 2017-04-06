bfcbe20d9143eac4ada530f7b28c7a5042cac925
anehrkor <alexander.nehrkorn@cern.ch>
2017-04-06 11:29:19 +0200

    Small fix: decayDistMag contains already square root

--- RecoTauTag/RecoTau/plugins/PFRecoTauDiscriminationByMVAIsolationRun2.cc
+++ RecoTauTag/RecoTau/plugins/PFRecoTauDiscriminationByMVAIsolationRun2.cc
@@ -231,7 +231,7 @@ double PFRecoTauDiscriminationByMVAIsolationRun2::discriminate(const PFTauRef& t
         double mAOne = tau->p4().M();
         double pAOneMag = tau->p();
         double thetaGJmax = TMath::ASin( (TMath::Power(mTau,2) - TMath::Power(mAOne,2) ) / ( 2 * mTau * pAOneMag ) );
-        double thetaGJmeasured = TMath::ACos( ( tau->p4().px() * decayDistX + tau->p4().py() * decayDistY + tau->p4().pz() * decayDistZ ) / ( pAOneMag * TMath::Sqrt(decayDistMag) ) );
+        double thetaGJmeasured = TMath::ACos( ( tau->p4().px() * decayDistX + tau->p4().py() * decayDistY + tau->p4().pz() * decayDistZ ) / ( pAOneMag * decayDistMag ) );
         gjAngleDiff = thetaGJmeasured - thetaGJmax;
     }
 
