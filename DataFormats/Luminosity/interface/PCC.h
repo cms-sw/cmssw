aea673030ad9fee444a7f457dbff5051ef6bd1f2
Sam Higginbotham <samhiggie72@gmail.com>
2017-04-06 21:52:25 +0200

    Updates to PCC.h members and classes_def.xml

--- DataFormats/Luminosity/interface/PCC.h
+++ DataFormats/Luminosity/interface/PCC.h
@@ -28,41 +28,23 @@ static constexpr unsigned int nEmBX=3564;//Empty BX to fill with counts
             if(it == m_ModID.end()){
                 std::vector<int> m_empBX(nEmBX, 0);
                 m_ModID.push_back(mD);
-                m_counts.push_back(m_empBX); 
+                m_counts.insert(m_counts.begin(),m_empBX.begin(),m_empBX.end()); 
             }
-           m_counts[modIndex][BXid] += count; 
+            m_counts[nEmBX*modIndex+BXid] += count; 
         }
         ////////////////////////////////////////////
         void eventCounter(int BXid){
             m_events[BXid]++;
         }
         ////////////////////////////////////////////
-        std::vector<std::vector<int>> const & read_counts() const {
-                return(m_counts);
-            }
-        ////////////////////////////////////////////
-        void printVector()
-        { 
-            int irow = 0;
-            std::vector< std::vector<int> >::const_iterator row; 
-            std::vector<int>::const_iterator col; 
-
-            for (row = m_counts.begin(); row != m_counts.end(); ++row)
-            { 
-            
-               std::cout << m_ModID[irow] << " :";
-               for (col = row->begin(); col != row->end(); ++col)
-               { 
-                  std::cout << *col << " "; 
-               } 
-               irow++;
-               std::cout<<std::endl;
-            } 
-
+        std::vector<int> const & readCounts() const {
+            return(m_counts);
         }
+        ////////////////////////////////////////////
+  
 
       private:
-        std::vector<std::vector<int> > m_counts;
+        std::vector<int> m_counts;
         std::vector<int> m_events;
         std::vector<int> m_ModID;
         std::vector<int> m_empBX;
