abe1f9c04a33b6deafb3dde85c8398c761d7a59e
Sven Dildick <sven.dildick@cern.ch>
2017-04-06 02:12:52 +0200

    fix text

--- SimMuon/GEMDigitizer/python/muonME0PadDigis_cfi.py
+++ SimMuon/GEMDigitizer/python/muonME0PadDigis_cfi.py
@@ -1,6 +1,6 @@
 import FWCore.ParameterSet.Config as cms
 
-# Module to create simulated ME0-CSC trigger pad digis.
+# Module to create simulated ME0 trigger pad digis.
 simMuonME0PadDigis = cms.EDProducer("ME0PadDigiProducer",
     InputCollection = cms.InputTag('simMuonME0ReDigis'),
 )
