b13b18958ea83e8f862f0f641102538110270157
Stefano Argiro <Stefano.Argiro@cern.ch>
2017-04-06 10:53:45 +0200

    trivial fixes

--- Calibration/EcalCalibAlgos/python/ecalPedestalPCLHarvester_cfi.py
+++ Calibration/EcalCalibAlgos/python/ecalPedestalPCLHarvester_cfi.py
@@ -1,6 +1,6 @@
 import FWCore.ParameterSet.Config as cms
 ECALpedestalPCLHarvester = cms.EDAnalyzer('ECALpedestalPCLHarvester',
-                                          MinEntries = cms.int(100), #skip channel if stat is low
+                                          MinEntries = cms.int32(100), #skip channel if stat is low
                                           ChannelStatusToExclude = cms.vstring('kDAC',
                                                                                'kNoisy',
                                                                                'kNNoisy',
