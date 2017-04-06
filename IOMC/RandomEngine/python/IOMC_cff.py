9325724c2cd5baaea532fbc5a96ef48b364eb5fa
piet verwilligen <piet.verwilligen@cern.ch>
2017-04-06 19:34:25 +0200

    cesares changes for 2017 slice test

--- IOMC/RandomEngine/python/IOMC_cff.py
+++ IOMC/RandomEngine/python/IOMC_cff.py
@@ -169,6 +169,11 @@ RandomNumberGeneratorService = cms.Service("RandomNumberGeneratorService",
 
 randomEngineStateProducer = cms.EDProducer("RandomEngineStateProducer")
 
+from Configuration.Eras.Modifier_run2_GEM_2017_cff import run2_GEM_2017
+run2_GEM_2017.toModify(RandomNumberGeneratorService, simMuonGEMDigis = cms.PSet(
+        initialSeed = cms.untracked.uint32(1234567),
+        engineName = cms.untracked.string('HepJamesRandom')) )
+
 from Configuration.Eras.Modifier_run3_GEM_cff import run3_GEM
 run3_GEM.toModify(RandomNumberGeneratorService, simMuonGEMDigis = cms.PSet(
         initialSeed = cms.untracked.uint32(1234567),
