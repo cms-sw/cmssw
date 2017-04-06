f1a4e12105b394867b4eeef88db145256505c346
Dmytro Kovalskyi <dmytro@cern.ch>
2017-04-06 15:12:35 +0200

    updated configuration to the current release

--- RecoMuon/MuonIdentification/test/TestMuonIdProducer_cfg.py
+++ RecoMuon/MuonIdentification/test/TestMuonIdProducer_cfg.py
@@ -1,15 +1,31 @@
 import FWCore.ParameterSet.Config as cms
 process = cms.Process("TEST")
 
-process.load("Configuration.StandardSequences.MagneticField_cff")
-process.load("Configuration.StandardSequences.Geometry_cff")
-process.load("Configuration.StandardSequences.FrontierConditions_GlobalTag_cff")
-process.load("Configuration.StandardSequences.Reconstruction_cff")
-process.GlobalTag.globaltag = 'MC_36Y_V3::All'
+# process.load("Configuration.StandardSequences.MagneticField_cff")
+# process.load("Configuration.StandardSequences.Geometry_cff")
+# process.load("Configuration.StandardSequences.FrontierConditions_GlobalTag_cff")
+# process.load("Configuration.StandardSequences.Reconstruction_cff")
+
+# import of standard configurations
+process.load('Configuration.StandardSequences.Services_cff')
+process.load('SimGeneral.HepPDTESSource.pythiapdt_cfi')
+process.load('FWCore.MessageService.MessageLogger_cfi')
+process.load('Configuration.EventContent.EventContent_cff')
+process.load('SimGeneral.MixingModule.mixNoPU_cfi')
+process.load('Configuration.StandardSequences.GeometryRecoDB_cff')
+process.load('Configuration.StandardSequences.MagneticField_cff')
+process.load('Configuration.StandardSequences.Reconstruction_cff')
+process.load('Configuration.StandardSequences.EndOfProcess_cff')
+process.load('Configuration.StandardSequences.FrontierConditions_GlobalTag_cff')
+
+from Configuration.AlCa.GlobalTag import GlobalTag
+process.GlobalTag = GlobalTag(process.GlobalTag, 'auto:phase1_2018_realistic', '')
+# process.GlobalTag.globaltag = 'MC_36Y_V3::All'
 
 process.load("RecoMuon.MuonIdentification.links_cfi")
+# process.globalMuonLinks.inputCollection = cms.InputTag("muons","","RECO")
 process.maxEvents = cms.untracked.PSet(
-    input = cms.untracked.int32(-1)
+    input = cms.untracked.int32(10)
 )
 process.MessageLogger = cms.Service("MessageLogger",
     destinations = cms.untracked.vstring('cout'),
@@ -29,23 +45,20 @@ process.MessageLogger = cms.Service("MessageLogger",
 	#   limit = cms.untracked.int32(-1)
 	#),
     ),
-    debugModules = cms.untracked.vstring("muons")
+    debugModules = cms.untracked.vstring("muons1stStes")
 )
 
-import FWCore.Framework.test.cmsExceptionsFatalOption_cff
-options = cms.untracked.PSet(
-    Rethrow = FWCore.Framework.test.cmsExceptionsFatalOption_cff.Rethrow
-)
+# import FWCore.Framework.test.cmsExceptionsFatalOption_cff
+# options = cms.untracked.PSet(
+#     Rethrow = FWCore.Framework.test.cmsExceptionsFatalOption_cff.Rethrow
+# )
 
 process.source = cms.Source("PoolSource",
-    fileNames = cms.untracked.vstring(
-    #'/store/relval/CMSSW_3_2_5/RelValSingleMuPt10/GEN-SIM-RECO/MC_31X_V5-v1/0011/E014EF8B-588E-DE11-8D06-001617E30E28.root'
-    'file:/tmp/jribnik/12D588B7-8D37-DF11-83E5-0030487A1884.root'
-    )
+    fileNames = cms.untracked.vstring('/store/relval/CMSSW_9_1_0_pre1/RelValTTbar_13/GEN-SIM-RECO/PU25ns_90X_upgrade2018_realistic_v17-v1/00000/0CD81D1D-9715-E711-8B2D-0CC47A7452D8.root'),
 )
 
 process.out = cms.OutputModule("PoolOutputModule",
-    fileName = cms.untracked.string('mc-muonid3.root'),
+    fileName = cms.untracked.string('muonid.root'),
     outputCommands = cms.untracked.vstring("drop *",
         "keep *_genParticleCandidates_*_*",
 	"keep recoTracks_*_*_*",
@@ -57,16 +70,21 @@ process.out = cms.OutputModule("PoolOutputModule",
 	"keep *_*_*_TEST")
 )
 
-process.p = cms.Path(process.globalMuonLinks*process.muons)
+# Recreate links
+process.muonid_study = cms.Path(process.globalMuonLinks* process.muonIdProducerSequence)
 
-process.muons.inputCollectionLabels = cms.VInputTag(cms.InputTag("generalTracks"),
-						    cms.InputTag("globalMuonLinks"), 
-						    cms.InputTag("standAloneMuons","UpdatedAtVtx"))
-process.muons.inputCollectionTypes = cms.vstring('inner tracks', 
-						 'links', 
-						 'outer tracks')
+process.muons1stStep.inputCollectionLabels = cms.VInputTag(cms.InputTag("generalTracks"), 
+                                                           cms.InputTag("globalMuonLinks"), 
+                                                           cms.InputTag("standAloneMuons","UpdatedAtVtx"),
+                                                           cms.InputTag("tevMuons","firstHit"),cms.InputTag("tevMuons","picky"),cms.InputTag("tevMuons","dyt"))
+process.muons1stStep.inputCollectionTypes = cms.vstring('inner tracks', 
+                                                        'links', 
+                                                        'outer tracks',
+                                                        'tev firstHit',
+                                                        'tev picky',
+                                                        'tev dyt')
 
-process.muons.fillIsolation = False
+# process.muons.fillIsolation = False
 # process.muons.minPt = 0.
 # process.muons.minP = 0.
 
