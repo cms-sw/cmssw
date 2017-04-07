81bc0fee9d9749d4812fb00e6e3cdf2e7189898f
Jan Kašpar <jan.kaspar@cern.ch>
2017-04-07 13:01:24 +0200

    Added missing subSystem field.

--- CondFormats/CTPPSReadoutObjects/python/totemDAQMappingESSourceXML_cfi.py
+++ CondFormats/CTPPSReadoutObjects/python/totemDAQMappingESSourceXML_cfi.py
@@ -3,6 +3,9 @@ import FWCore.ParameterSet.Config as cms
 totemDAQMappingESSourceXML = cms.ESSource("TotemDAQMappingESSourceXML",
   verbosity = cms.untracked.uint32(0),
 
+  # set it to: TrackingStrip, ...
+  subSystem = cms.untracked.string(""),
+
   configuration = cms.VPSet(
     # example configuration block:
     #cms.PSet(
