bdb7106fc4dde8c10c491c8f88b59b62b3731d88
Jan Kašpar <jan.kaspar@cern.ch>
2017-04-07 14:44:24 +0200

    Booking histograms only for RPs (possibly) containing strips.

--- DQM/CTPPS/test/dqm_test_cfg.py
+++ DQM/CTPPS/test/dqm_test_cfg.py
@@ -21,8 +21,15 @@ process.dqmSaver.tag = "CTPPS"
 # raw data source
 process.source = cms.Source("PoolSource",
   fileNames = cms.untracked.vstring(
-    'file:/afs/cern.ch/user/j/jkaspar/public/run273062_ls0001-2_stream.root',
-    '/store/express/Run2016H/ExpressPhysics/FEVT/Express-v2/000/283/877/00000/4EE44B0E-2499-E611-A155-02163E011938.root'
+    # run 274199, fill 4961, 29 May 2016 (before TS2)
+    #'/store/data/Run2016B/DoubleEG/RAW/v2/000/274/199/00000/04985451-9B26-E611-BEB9-02163E013859.root',
+    #'root://eostotem.cern.ch//eos/totem/user/j/jkaspar/04C8034A-9626-E611-9B6E-02163E011F93.root'
+
+    # run 283877, fill 5442, 23 Oct 2016 (after TS2)
+    '/store/data/Run2016H/HLTPhysics/RAW/v1/000/283/877/00000/F28F8896-999B-E611-93D8-02163E013706.root',
+
+    # test file for 2017 mapping (vertical RPs only)
+    #'root://eostotem.cern.ch//eos/totem/data/ctpps/run290874.root'
   ),
   inputCommands = cms.untracked.vstring(
     'drop *',
