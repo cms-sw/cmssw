478187fe939d9378d0633d618c2dedc35a5dddb7
Jan Kašpar <jan.kaspar@cern.ch>
2017-04-07 12:23:56 +0200

    FED id range of CTPPS diamonds restricted to the FEDs actually used.

--- DataFormats/FEDRawData/interface/FEDNumbering.h
+++ DataFormats/FEDRawData/interface/FEDNumbering.h
@@ -45,7 +45,7 @@ class FEDNumbering {
      MINTotemRPFEDID = 578,
      MAXTotemRPFEDID = 581,
      MINCTPPSDiamondFEDID=582,
-     MAXCTPPSDiamondFEDID=585,
+     MAXCTPPSDiamondFEDID=583,
      MINECALFEDID = 600,
      MAXECALFEDID = 670,
      MINCASTORFEDID = 690,
