78dd79e6b412989a73bc3a3cf8ae60abe9ed908b
Carlo Battilana <carlo.battilana@gmail.com>
2017-04-07 12:54:05 +0200

    Fix era customization

--- DPGAnalysis/MuonSysAging/python/RPCChamberMasker_cfi.py
+++ DPGAnalysis/MuonSysAging/python/RPCChamberMasker_cfi.py
@@ -8,9 +8,4 @@ RPCChamberMasker = cms.EDProducer('RPCChamberMasker',
 
 from Configuration.Eras.Modifier_phase2_muon_cff import phase2_muon
 
-if phase2_muon.isChosen():
-	RPCChamberMasker = cms.EDProducer('RPCChamberMasker',
-					  digiTag = cms.InputTag('simMuonRPCDigis'),
-					  descopeRE31 = cms.bool(False),
-					  descopeRE41 =cms.bool(False)
-					  )
+phase2_muon.toModify(RPCChamberMasker, digiTag = cms.InputTag('simMuonRPCDigis'))
