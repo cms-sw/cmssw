e2670f31a9b5c57c929b257584986bc81fdaf10d
Vladimir <Vladimir.Ivantchenko@cern.ch>
2017-04-07 18:32:50 +0200

    cleanup phys interface

--- SimG4Core/PhysicsLists/src/EmParticleList.cc
+++ SimG4Core/PhysicsLists/src/EmParticleList.cc
@@ -1,24 +1,20 @@
-#ifndef SimG4Core_Application_CMSEmParticleList_h
-#define SimG4Core_Application_CMSEmParticleList_h 1
+#include "SimG4Core/PhysicsLists/interface/EmParticleList.h"
 
-// V.Ivanchenko 6 March 2017
-// List of Geant4 basic particle names used in SIM step 
-
-#include "globals.hh"
-
-static const G4int nEmParticles = 39;
-static const G4String EmPartNames[nEmParticles] = 
-{ 
+const std::vector<G4String> EmParticleList::pNames = 
+    { 
         "gamma",            "e-",           "e+",           "mu+",        "mu-",
           "pi+",           "pi-",        "kaon+",         "kaon-",     "proton",
   "anti_proton",         "alpha",          "He3",    "GenericIon",         "B+",
            "B-",            "D+",           "D-",           "Ds+",        "Ds-",
      "anti_He3",    "anti_alpha","anti_deuteron","anti_lambda_c+","anti_omega-",
 "anti_sigma_c+","anti_sigma_c++",  "anti_sigma+",   "anti_sigma-","anti_triton",
+   "anti_xi_c+",      "anti_xi-",     "deuteron",     "lambda_c+",     "omega-",
      "sigma_c+",     "sigma_c++",       "sigma+",        "sigma-",       "tau+",
          "tau-",        "triton",        "xi_c+",           "xi-"
-};
+    };
 
-
-#endif
+const std::vector<G4String>& EmParticleList::PartNames()
+{
+  return pNames;
+}
 
