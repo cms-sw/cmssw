d8fd58eaf21f7bc7c48f946f66b6c0e761eadb7e
Carlo Battilana <carlo.battilana@gmail.com>
2017-04-07 15:32:33 +0200

    fix includes

--- DPGAnalysis/MuonSysAging/plugins/GEMChamberMasker.cc
+++ DPGAnalysis/MuonSysAging/plugins/GEMChamberMasker.cc
@@ -8,7 +8,7 @@
 
 // user include files
 #include "FWCore/Framework/interface/Frameworkfwd.h"
-#include "FWCore/Framework/interface/EDProducer.h"
+#include "FWCore/Framework/interface/stream/EDProducer.h"
 #include "FWCore/Framework/interface/ConsumesCollector.h"
 #include "FWCore/Framework/interface/ESHandle.h"
 #include "FWCore/Framework/interface/Event.h"
