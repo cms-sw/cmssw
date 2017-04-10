d83de9bb0c71dcdc1d41e9def70e20c79d45fb3a
Fabrizio <Fabrizio.Ferro@ge.infn.it>
2017-04-10 14:40:30 +0200

    removing *_TEST.xml, leaving only *_200_far.xml

--- EventFilter/CTPPSRawToDigi/test/test_pixels_only_cfg.py
+++ EventFilter/CTPPSRawToDigi/test/test_pixels_only_cfg.py
@@ -15,9 +15,12 @@ process.source = cms.Source("PoolSource",
     #'file:/afs/cern.ch/user/j/jkaspar/public/run273062_ls0001-2_stream.root'
    # '/store/express/Run2016H/ExpressPhysics/FEVT/Express-v2/000/283/877/00000/4EE44B0E-2499-E611-A155-02163E011938.root'
 #'file:/afs/cern.ch/work/k/kas/public/PXtrees/PixelAlive_1294_151_RAW_v2.root'
-'root://eoscms//eos/cms/store/user/jjhollar/012017_PixelDAQTests/PixelAlive_1294_151_RAW_v2.root'
+#'root://eoscms//eos/cms/store/user/jjhollar/012017_PixelDAQTests/PixelAlive_1294_151_RAW_v2.root'
+'root://eoscms//eos/cms/store/user/jjhollar/012017_PixelDAQTests/PixelAlive_1462_2_RAW.root',
+'root://eoscms//eos/cms/store/user/jjhollar/012017_PixelDAQTests/PixelAlive_1463_2_RAW.root'
   ),
-labelRawDataLikeMC = cms.untracked.bool(False) # for testing H8 data
+labelRawDataLikeMC = cms.untracked.bool(False), # for testing H8 data
+duplicateCheckMode = cms.untracked.string("checkEachFile")
 )
 
 process.maxEvents = cms.untracked.PSet(
@@ -31,9 +34,10 @@ process.ctppsPixelDAQMappingESSourceXML.configuration = cms.VPSet(
     # example configuration block:
     cms.PSet(
         validityRange = cms.EventRange("1:min - 999999999:max"),
-        mappingFileNames = cms.vstring("CondFormats/CTPPSReadoutObjects/xml/rpix_mapping_220_far_TEST.xml"),
-        maskFileNames = cms.vstring("CondFormats/CTPPSReadoutObjects/xml/rpix_channel_mask_TEST.xml")
+        mappingFileNames = cms.vstring("CondFormats/CTPPSReadoutObjects/xml/rpix_mapping_220_far.xml"),
+        maskFileNames = cms.vstring("CondFormats/CTPPSReadoutObjects/xml/rpix_channel_mask_220_far.xml")
         )
+
     )
 
 process.p = cms.Path(
