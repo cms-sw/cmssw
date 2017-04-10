43b24015b2e49c3028020774eecabcf2a5ef39c5
arun <arun.kumar@cern.ch>
2017-04-10 16:26:29 +0200

    L1T CaloParam tag is updated with correct history of 2015 and 2016

--- Configuration/AlCa/python/autoCond.py
+++ Configuration/AlCa/python/autoCond.py
@@ -22,21 +22,21 @@ autoCond = {
     # GlobalTag for MC production (p-Pb collisions) with realistic alignment and calibrations for Run2
     'run2_mc_pa'        :   '91X_mcRun2_pA_v2',
     # GlobalTag for Run1 data reprocessing
-    'run1_data'         :   '91X_dataRun2_v2',
+    'run1_data'         :   '91X_dataRun2_v3',
     # GlobalTag for Run2 data reprocessing
-    'run2_data'         :   '91X_dataRun2_v2',
+    'run2_data'         :   '91X_dataRun2_v3',
     # GlobalTag for Run2 data relvals: allows customization to run with fixed L1 menu
-    'run2_data_relval'  :   '91X_dataRun2_relval_v2',
+    'run2_data_relval'  :   '91X_dataRun2_relval_v3',
     # GlobalTag for Run2 data 2016H relvals only: Prompt Conditions + fixed L1 menu (to be removed)
-    'run2_data_promptlike' : '91X_dataRun2_PromptLike_v2',
+    'run2_data_promptlike' : '91X_dataRun2_PromptLike_v3',
     # GlobalTag for Run1 HLT: it points to the online GT
-    'run1_hlt'          :   '91X_dataRun2_HLT_frozen_v2',
+    'run1_hlt'          :   '91X_dataRun2_HLT_frozen_v3',
     # GlobalTag for Run2 HLT: it points to the online GT
-    'run2_hlt'          :   '91X_dataRun2_HLT_frozen_v2',
+    'run2_hlt'          :   '91X_dataRun2_HLT_frozen_v3',
     # GlobalTag for Run2 HLT RelVals: customizations to run with fixed L1 Menu
-    'run2_hlt_relval'   :   '91X_dataRun2_HLT_relval_v2',
+    'run2_hlt_relval'   :   '91X_dataRun2_HLT_relval_v3',
     # GlobalTag for Run2 HLT for HI: it points to the online GT
-    'run2_hlt_hi'       :   '91X_dataRun2_HLTHI_frozen_v2',
+    'run2_hlt_hi'       :   '91X_dataRun2_HLTHI_frozen_v3',
     # GlobalTag for MC production with perfectly aligned and calibrated detector for Phase1 2017 (and 0,0,~0-centred beamspot)
     'phase1_2017_design'       :  '91X_upgrade2017_design_IdealBS_v3',
     # GlobalTag for MC production with realistic conditions for Phase1 2017 detector
