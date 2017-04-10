fb5b03f1d307f8eafb4f75a928d5df45ffe5e32c
Wolfram Erdmann <wolfram.erdmann@psi.ch>
2017-04-10 14:18:38 +0200

    first commit of clustering for 2017

--- RecoVertex/PrimaryVertexProducer/python/OfflinePrimaryVertices_cfi.py
+++ RecoVertex/PrimaryVertexProducer/python/OfflinePrimaryVertices_cfi.py
@@ -16,6 +16,7 @@ offlinePrimaryVertices = cms.EDProducer(
         minSiliconLayersWithHits = cms.int32(5),
         maxD0Significance = cms.double(5.0), 
         minPt = cms.double(0.0),
+        maxEta = cms.double(2.4),
         trackQuality = cms.string("any")
     ),
 
@@ -24,12 +25,14 @@ offlinePrimaryVertices = cms.EDProducer(
     vertexCollections = cms.VPSet(
      [cms.PSet(label=cms.string(""),
                algorithm=cms.string("AdaptiveVertexFitter"),
+               chi2cutoff = cms.double(2.5),
                minNdof=cms.double(0.0),
                useBeamConstraint = cms.bool(False),
                maxDistanceToBeam = cms.double(1.0)
                ),
       cms.PSet(label=cms.string("WithBS"),
                algorithm = cms.string('AdaptiveVertexFitter'),
+               chi2cutoff = cms.double(2.5),
                minNdof=cms.double(2.0),
                useBeamConstraint = cms.bool(True),
                maxDistanceToBeam = cms.double(1.0)
--- RecoVertex/PrimaryVertexProducer/python/TkClusParameters_cff.py
+++ RecoVertex/PrimaryVertexProducer/python/TkClusParameters_cff.py
@@ -14,10 +14,14 @@ DA2DParameters = cms.PSet(
 DA_vectParameters = cms.PSet(
     algorithm   = cms.string("DA_vect"),
     TkDAClusParameters = cms.PSet(
-        coolingFactor = cms.double(0.6),  #  moderate annealing speed
-        Tmin = cms.double(4.),            #  end of annealing
-        vertexSize = cms.double(0.01),    #  ~ resolution / sqrt(Tmin)
+        coolingFactor = cms.double(0.6),  # moderate annealing speed
+        Tmin = cms.double(2.0),           # end of vertex splitting
+        Tpurge = cms.double(2.0),         # cleaning 
+        Tstop = cms.double(0.5),          # end of annealing 
+        vertexSize = cms.double(0.005),   # added in quadrature to track-z resolutions
         d0CutOff = cms.double(3.),        # downweight high IP tracks 
-        dzCutOff = cms.double(4.)         # outlier rejection after freeze-out (T<Tmin)
+        dzCutOff = cms.double(3.),        # outlier rejection after freeze-out (T<Tmin)
+        zmerge = cms.double(1e-2),        # merge intermediat clusters separated by less than zmerge
+        uniquetrkweight = cms.double(0.8) # require at least two tracks with this weight at T=Tpurge
         )
 )
