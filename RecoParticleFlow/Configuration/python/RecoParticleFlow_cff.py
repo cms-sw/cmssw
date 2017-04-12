27c1abc1af0c44cf61e144c2660ec97445aa0072
Andreas Hinzmann <hinzmann@cern.ch>
2017-04-12 10:17:30 +0200

    charged hadron isolation in RECO

--- RecoParticleFlow/Configuration/python/RecoParticleFlow_cff.py
+++ RecoParticleFlow/Configuration/python/RecoParticleFlow_cff.py
@@ -19,6 +19,7 @@ from RecoParticleFlow.PFProducer.pfLinker_cff import *
 from CommonTools.ParticleFlow.pfParticleSelection_cff import *
 
 from RecoEgamma.EgammaIsolationAlgos.particleBasedIsoProducer_cff import *
+from RecoParticleFlow.PFProducer.chargedHadronIsolation_cfi import *
 
 from RecoJets.JetProducers.fixedGridRhoProducerFastjet_cfi import *
 fixedGridRhoFastjetAllTmp = fixedGridRhoFastjetAll.clone(pfCandidatesTag = cms.InputTag("particleFlowTmp"))
@@ -36,7 +37,7 @@ particleFlowReco = cms.Sequence( particleFlowTrackWithDisplacedVertex*
                                  particleFlowEGammaFinal*
                                  pfParticleSelectionSequence )
 
-particleFlowLinks = cms.Sequence( particleFlow*particleFlowPtrs*particleBasedIsolationSequence)
+particleFlowLinks = cms.Sequence( particleFlow*particleFlowPtrs*chargedHadronIsolation*particleBasedIsolationSequence)
 
 from RecoParticleFlow.PFTracking.hgcalTrackCollection_cfi import *
 from RecoParticleFlow.PFProducer.simPFProducer_cfi import *
---
+++ RecoParticleFlow/PFProducer/plugins/ChargedHadronIsolationProducer.cc
@@ -0,0 +1,101 @@
+/*
+ * ChargedHadronIsolationProducer
+ *
+ * Author: Andreas Hinzmann
+ *
+ * Associates PF isolation flag to charged hadron candidates
+ *
+ */
+
+#include "FWCore/Framework/interface/stream/EDProducer.h"
+#include "FWCore/Framework/interface/EventSetup.h"
+#include "FWCore/Framework/interface/Event.h"
+#include "FWCore/ParameterSet/interface/ParameterSet.h"
+#include "FWCore/ParameterSet/interface/ConfigurationDescriptions.h"
+#include "FWCore/ParameterSet/interface/ParameterSetDescription.h"
+
+#include "FWCore/Utilities/interface/Exception.h"
+#include "FWCore/MessageLogger/interface/MessageLogger.h"
+
+#include "DataFormats/ParticleFlowCandidate/interface/PFCandidate.h"
+#include "DataFormats/ParticleFlowCandidate/interface/PFCandidateFwd.h"
+#include "DataFormats/Common/interface/ValueMap.h"
+#include "DataFormats/ParticleFlowReco/interface/PFBlock.h"
+#include "DataFormats/ParticleFlowReco/interface/PFBlockElement.h"
+
+namespace edm {
+   class ConfigurationDescriptions;
+}
+
+class ChargedHadronIsolationProducer : public edm::stream::EDProducer<> 
+{
+public:
+
+  explicit ChargedHadronIsolationProducer(const edm::ParameterSet& cfg);
+  ~ChargedHadronIsolationProducer() {}
+  void produce(edm::Event& evt, const edm::EventSetup& es);
+  static void fillDescriptions(edm::ConfigurationDescriptions & descriptions);
+
+private:
+  // input collection
+  edm::InputTag srcCandidates_;
+  edm::EDGetTokenT<edm::View<reco::PFCandidate> > Candidates_token;
+  double minTrackPt_;
+  double minRawCaloEnergy_;
+
+};
+
+ChargedHadronIsolationProducer::ChargedHadronIsolationProducer(const edm::ParameterSet& cfg)
+{
+  srcCandidates_ = cfg.getParameter<edm::InputTag>("src");
+  Candidates_token = consumes<edm::View<reco::PFCandidate> >(srcCandidates_);
+  minTrackPt_ = cfg.getParameter<double>("minTrackPt");
+  minRawCaloEnergy_ = cfg.getParameter<double>("minRawCaloEnergy");
+  
+  produces<edm::ValueMap<bool> >();
+}
+
+void ChargedHadronIsolationProducer::produce(edm::Event& evt, const edm::EventSetup& es) 
+{
+  // get a view of our Candidates via the base candidates
+  typedef edm::View<reco::PFCandidate> PFCandidateView;
+  edm::Handle<PFCandidateView> Candidates;
+  evt.getByToken(Candidates_token, Candidates);
+  
+  std::vector<bool> values;
+
+  for( PFCandidateView::const_iterator c = Candidates->begin(); c!=Candidates->end(); ++c) {
+    // Check that there is only one track in the block.
+    unsigned int nTracks = 0;
+    if ((c->particleId()==1) && (c->pt()>minTrackPt_) && ((c->rawEcalEnergy()+c->rawHcalEnergy())>minRawCaloEnergy_)) {
+      const reco::PFCandidate::ElementsInBlocks& theElements = c->elementsInBlocks();
+      if( theElements.empty() ) continue;
+      const reco::PFBlockRef blockRef = theElements[0].first;
+      const edm::OwnVector<reco::PFBlockElement>& elements = blockRef->elements();
+      for(unsigned int iEle=0; iEle<elements.size(); iEle++) {	         	 // Find the tracks in the block
+         reco::PFBlockElement::Type type = elements[iEle].type();
+         if( type== reco::PFBlockElement::TRACK)
+           nTracks++;
+      }
+    }
+    values.push_back((nTracks==1));
+  }
+
+  std::unique_ptr<edm::ValueMap<bool> > out(new edm::ValueMap<bool>());
+  edm::ValueMap<bool>::Filler filler(*out);
+  filler.insert(Candidates,values.begin(),values.end());
+  filler.fill();
+  evt.put(std::move(out));
+}
+
+void ChargedHadronIsolationProducer::fillDescriptions(edm::ConfigurationDescriptions & descriptions) {
+   edm::ParameterSetDescription desc;
+   desc.add<edm::InputTag>("src", edm::InputTag("particleFlow"));
+   desc.add<double>("minTrackPt", 1);
+   desc.add<double>("minRawCaloEnergy", 0.5);
+   descriptions.add("chargedHadronIsolation", desc);
+}
+
+#include "FWCore/Framework/interface/MakerMacros.h"
+
+DEFINE_FWK_MODULE(ChargedHadronIsolationProducer);
