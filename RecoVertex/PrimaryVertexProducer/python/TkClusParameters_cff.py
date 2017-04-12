69d400b997eacb78057ea113e322cc87408ff430
Wolfram Erdmann <wolfram.erdmann@psi.ch>
2017-04-12 15:34:17 +0200

    set parameters closer to phase 0 behavior

--- RecoVertex/PrimaryVertexProducer/python/TkClusParameters_cff.py
+++ RecoVertex/PrimaryVertexProducer/python/TkClusParameters_cff.py
@@ -15,13 +15,13 @@ DA_vectParameters = cms.PSet(
     algorithm   = cms.string("DA_vect"),
     TkDAClusParameters = cms.PSet(
         coolingFactor = cms.double(0.6),  # moderate annealing speed
-        Tmin = cms.double(2.0),           # end of vertex splitting
+        Tmin = cms.double(2.4),           # end of vertex splitting
         Tpurge = cms.double(2.0),         # cleaning 
         Tstop = cms.double(0.5),          # end of annealing 
-        vertexSize = cms.double(0.005),   # added in quadrature to track-z resolutions
+        vertexSize = cms.double(0.01),    # added in quadrature to track-z resolutions
         d0CutOff = cms.double(3.),        # downweight high IP tracks 
-        dzCutOff = cms.double(3.),        # outlier rejection after freeze-out (T<Tmin)
+        dzCutOff = cms.double(4.),        # outlier rejection after freeze-out (T<Tmin)
         zmerge = cms.double(1e-2),        # merge intermediat clusters separated by less than zmerge
-        uniquetrkweight = cms.double(0.8) # require at least two tracks with this weight at T=Tpurge
+        uniquetrkweight = cms.double(0.9) # require at least two tracks with this weight at T=Tpurge
         )
 )
