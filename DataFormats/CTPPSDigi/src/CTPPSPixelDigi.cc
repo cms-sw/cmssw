b0d7eaeeb5885acdd7d543984f76c92ab85db4af
Fabrizio <Fabrizio.Ferro@ge.infn.it>
2017-04-13 13:40:56 +0200

    adding constants and comments for channel definition

--- DataFormats/CTPPSDigi/src/CTPPSPixelDigi.cc
+++ DataFormats/CTPPSDigi/src/CTPPSPixelDigi.cc
@@ -1,5 +1,6 @@
 #include "DataFormats/CTPPSDigi/interface/CTPPSPixelDigi.h"
 
+/// row_w 11, column_w 11, adc_w 10 bits
 const uint32_t CTPPSPixelDigi::row_shift = 0; 
 const uint32_t CTPPSPixelDigi::column_shift = 11; 
 const uint32_t CTPPSPixelDigi::adc_shift = 22;
@@ -13,7 +14,10 @@ const uint32_t CTPPSPixelDigi::rowcol_mask = 0x3FFFFF;
 const uint32_t CTPPSPixelDigi::max_row = 0x7FF; 
 const uint32_t CTPPSPixelDigi::max_column = 0x7FF; 
 const uint32_t CTPPSPixelDigi::max_adc = 0x3FF;
-/// row_w 11, column_w 11, adc_w 10 bits
+
+const uint32_t CTPPSPixelDigi::column_width_ch = 11; 
+const uint32_t CTPPSPixelDigi::column_mask_ch = 0x7FF;
+const uint32_t CTPPSPixelDigi::row_mask_ch = 0x7FF;
 
 void CTPPSPixelDigi::init(int row, int col, int adc) {
 
