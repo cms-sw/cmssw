016facf24e922acf17ae9264f7fea27a38c86b7e
Andrea Bocci <fwyzard@gmail.com>
2017-04-13 22:43:59 +0200

    remove obsolete FastTimer class

--- HLTrigger/Timer/interface/FastTimerService.h
+++ HLTrigger/Timer/interface/FastTimerService.h
@@ -34,7 +34,6 @@
 #include "DataFormats/Provenance/interface/ModuleDescription.h"
 #include "DQMServices/Core/interface/DQMStore.h"
 #include "DQMServices/Core/interface/MonitorElement.h"
-#include "HLTrigger/Timer/interface/FastTimer.h"
 #include "HLTrigger/Timer/interface/ProcessCallGraph.h"
 
 
--- HLTrigger/Timer/plugins/ThroughputService.cc
+++ HLTrigger/Timer/plugins/ThroughputService.cc
@@ -45,7 +45,7 @@ ThroughputService::preallocate(edm::service::SystemBounds const & bounds)
 
   m_stream_histograms.resize( bounds.maxNumberOfStreams() );
 
-  // assign a pseudo module id to the FastTimerService
+  // assign a pseudo module id to the ThroughputService
   m_module_id = edm::ModuleDescription::getUniqueID();
 }
 
