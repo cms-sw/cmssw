1dfe3a4cf93e51d87adbd093034cfe26d8a47f9b
vladimir <vladimir.popov@cern.ch>
2017-04-14 02:23:42 +0200

    ctpps pixel dqm initial code

---
+++ DQM/CTPPS/python/ctppsPixelDQMSource_cfi.py
@@ -0,0 +1,9 @@
+import FWCore.ParameterSet.Config as cms
+
+ctppsPixelDQMSource = cms.EDAnalyzer("CTPPSPixelDQMSource",
+#    tagRPixDigi = cms.InputTag("RPixDetDigitizer", ""),
+    tagRPixDigi = cms.InputTag("ctppsPixelDigis", ""),
+    tagRPixCluster = cms.InputTag("clusterProd", ""),  
+    RPStatusWord = cms.untracked.uint32(0x8000), # rpots included in readout
+    verbosity = cms.untracked.uint32(1)
+)
