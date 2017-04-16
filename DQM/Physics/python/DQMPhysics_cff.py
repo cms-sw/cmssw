460d1b0fafc5eb3321a2f3c2bc55ef00be43e0f3
sandeep <sandeep.sharma@cern.ch>
2017-04-16 16:06:38 +0200

    dqm file

--- DQM/Physics/python/DQMPhysics_cff.py
+++ DQM/Physics/python/DQMPhysics_cff.py
@@ -19,8 +19,8 @@ from DQM.Physics.CentralitypADQM_cfi import *
 from DQM.Physics.topJetCorrectionHelper_cfi import *
 
 dqmPhysics = cms.Sequence( bphysicsOniaDQM 
-                           #*ewkMuDQM # FIXME: broken after PR #14312
-                           #*ewkElecDQM
+                           *ewkMuDQM 
+                           *ewkElecDQM
                            #*ewkMuLumiMonitorDQM
                            *qcdPhotonsDQM
 			   *topSingleMuonMediumDQM
