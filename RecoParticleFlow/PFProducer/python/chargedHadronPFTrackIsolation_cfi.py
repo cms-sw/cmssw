4f68c4af86b796f7c3f1abfedb5d173f29ba35ec
Andreas Hinzmann <hinzmann@cern.ch>
2017-04-19 10:17:36 +0200

    make edm global

--- RecoParticleFlow/PFProducer/python/chargedHadronPFTrackIsolation_cfi.py
+++
@@ -1,9 +0,0 @@
-import FWCore.ParameterSet.Config as cms
-
-
-chargedHadronPFTrackIsolation = cms.EDProducer(
-    "ChargedHadronPFTrackIsolationProducer",
-    src = cms.InputTag("particleFlow"),
-    minTrackPt = cms.double(1),
-    minRawCaloEnergy = cms.double(0.5),
-    )
