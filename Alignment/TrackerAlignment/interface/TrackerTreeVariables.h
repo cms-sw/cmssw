98aab742fe3cde04003370f4f1939b2c59e7d174
Christian <cschomak@cern.ch>
2017-04-19 16:35:04 +0200

    Modernized TrackerTreeGenerator: Migrated to geometry independent code and removed obsolete variables

--- Alignment/TrackerAlignment/interface/TrackerTreeVariables.h
+++ Alignment/TrackerAlignment/interface/TrackerTreeVariables.h
@@ -9,16 +9,18 @@
 // container to hold all static module parameters, determined with ideal geometry
 struct TrackerTreeVariables{
   
-  TrackerTreeVariables(){this->clear();}
+  TrackerTreeVariables(){
+    this->clear();
+  }
   
   void clear(){
-    rawId = subdetId = layer = side = half = rod = ring = petal = blade = panel = outerInner = module = rodAl = bladeAl = nStrips = 0;
+    rawId = subdetId = layer = side = half = rod = ring = petal = blade = panel = outerInner = module = nStrips = 0;
     isDoubleSide = isRPhi = isStereo = false;
     uDirection = vDirection = wDirection = 0;
     posR = posPhi = posEta = posX = posY = posZ = -999.F;
   }
   
-  UInt_t rawId, subdetId, layer, side, half, rod, ring, petal, blade, panel, outerInner, module, rodAl, bladeAl, nStrips;
+  UInt_t rawId, subdetId, layer, side, half, rod, ring, petal, blade, panel, outerInner, module, nStrips;
   Bool_t isDoubleSide, isRPhi, isStereo;
   Int_t uDirection, vDirection, wDirection;
   Float_t posR, posPhi, posEta, posX, posY, posZ;
