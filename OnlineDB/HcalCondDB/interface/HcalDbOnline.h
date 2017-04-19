49aea5e9d402a2a5393e8622085ea4e688a072f3
Kevin Pedro <kpedro88@gmail.com>
2017-04-19 16:25:20 -0500

    remove unused functions of HcalDbOnline

--- OnlineDB/HcalCondDB/interface/HcalDbOnline.h
+++ OnlineDB/HcalCondDB/interface/HcalDbOnline.h
@@ -7,13 +7,6 @@
 #include <memory>
 
 #include "DataFormats/HcalDetId/interface/HcalDetId.h"
-#include "CondFormats/HcalObjects/interface/HcalPedestals.h"
-#include "CondFormats/HcalObjects/interface/HcalPedestalWidths.h"
-#include "CondFormats/HcalObjects/interface/HcalGains.h"
-#include "CondFormats/HcalObjects/interface/HcalGainWidths.h"
-#include "CondFormats/HcalObjects/interface/HcalQIEData.h"
-#include "CondFormats/HcalObjects/interface/HcalCalibrationQIEData.h"
-#include "CondFormats/HcalObjects/interface/HcalElectronicsMap.h"
 
 /**
 
@@ -39,16 +32,6 @@ class HcalDbOnline {
   HcalDbOnline (const std::string& fDb, bool fVerbose = false);
   ~HcalDbOnline ();
 
-  bool getObject (HcalPedestals* fObject, HcalPedestalWidths* fWidths, const std::string& fTag, IOVTime fTime);
-  bool getObject (HcalPedestals* fObject, const std::string& fTag, IOVTime fTime);
-  bool getObject (HcalGains* fObject, HcalGainWidths* fWidths, const std::string& fTag, IOVTime fTime);
-  bool getObject (HcalGains* fObject, const std::string& fTag, IOVTime fTime);
-  bool getObject (HcalPedestalWidths* fObject, const std::string& fTag, IOVTime fTime);
-  bool getObject (HcalGainWidths* fObject, const std::string& fTag, IOVTime fTime);
-  bool getObject (HcalElectronicsMap* fObject, const std::string& fTag, IOVTime fTime);
-  bool getObject (HcalQIEData* fObject, const std::string& fTag, IOVTime fTime);
-  bool getObject (HcalCalibrationQIEData* fObject, const std::string& fTag, IOVTime fTime);
-
   std::vector<std::string> metadataAllTags ();
   std::vector<IntervalOV> getIOVs (const std::string& fTag);
   
@@ -57,7 +40,6 @@ class HcalDbOnline {
   oracle::occi::Environment* mEnvironment;
   oracle::occi::Connection* mConnect;
   oracle::occi::Statement* mStatement;
-  template <class T> bool getObjectGeneric (T* fObject, const std::string& fTag);
   bool mVerbose;
 };
 #endif
