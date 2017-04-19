f711f85835573d8504e692d302e8083c26955037
Sven Dildick <sven.dildick@cern.ch>
2017-04-19 13:51:53 +0200

    remove min_endcap and min_chamber

--- L1Trigger/ME0Trigger/src/ME0TriggerBuilder.cc
+++ L1Trigger/ME0Trigger/src/ME0TriggerBuilder.cc
@@ -4,18 +4,13 @@
 #include "Geometry/Records/interface/MuonGeometryRecord.h"
 #include "Geometry/GEMGeometry/interface/ME0Geometry.h"
 
-const int ME0TriggerBuilder::min_endcap  = ME0DetId::minRegionId;
-const int ME0TriggerBuilder::max_endcap  = ME0DetId::maxRegionId;
-const int ME0TriggerBuilder::min_chamber = ME0DetId::minChamberId;
-const int ME0TriggerBuilder::max_chamber = ME0DetId::maxChamberId;
-
 ME0TriggerBuilder::ME0TriggerBuilder(const edm::ParameterSet& conf)
 {
   config_ = conf;
 
-  for (int endc = min_endcap; endc <= max_endcap; endc++)
+  for (int endc = 0; endc < 2; endc++)
   {
-    for (int cham = min_chamber; cham <= max_chamber; cham++)
+    for (int cham = 0; cham < 18; cham++)
     {
       if ((endc <= 0 || endc > MAX_ENDCAPS)    ||
 	  (cham <= 0 || cham > MAX_CHAMBERS))
@@ -27,7 +22,7 @@ ME0TriggerBuilder::ME0TriggerBuilder(const edm::ParameterSet& conf)
 	  << "]; skipping it... +++\n";
 	continue;
       }
-      tmb_[endc-1][cham-1].reset( new ME0Motherboard(endc, cham, config_) );
+      tmb_[endc][cham].reset( new ME0Motherboard(endc, cham, config_) );
     }
   }
 }
@@ -39,15 +34,16 @@ ME0TriggerBuilder::~ME0TriggerBuilder()
 void ME0TriggerBuilder::build(const ME0PadDigiCollection* me0Pads,
 			      ME0TriggerDigiCollection& oc_trig)
 {
-  for (int endc = min_endcap; endc <= max_endcap; endc++)
+  for (int endc = 0; endc < 2; endc++)
   {
-    for (int cham = min_chamber; cham <= max_chamber; cham++)
+    for (int cham = 0; cham < 18; cham++)
     {
-      ME0Motherboard* tmb = tmb_[endc-1][cham-1].get();
+      ME0Motherboard* tmb = tmb_[endc][cham].get();
       tmb->setME0Geometry(me0_g);
 
       // 0th layer means whole chamber.
-      ME0DetId detid(endc, 0, cham, 0);
+      const int region(endc == 0 ? -1 : 1);
+      ME0DetId detid(region, 0, cham, 0);
 
       // Run processors only if chamber exists in geometry.
       if (tmb == 0 || me0_g->chamber(detid) == 0) continue;
