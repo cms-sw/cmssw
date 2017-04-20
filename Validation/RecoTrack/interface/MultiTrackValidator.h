092544435c679a2202342d73d981ba460b0d2c52
Matti Kortelainen <matti.kortelainen@cern.ch>
2017-04-20 13:47:35 +0200

    Merge MultiTrackValidatorBase to MultiTrackValidator
    There are no other deriving classes, so the base class was just
    additional complication.

--- Validation/RecoTrack/interface/MultiTrackValidator.h
+++ Validation/RecoTrack/interface/MultiTrackValidator.h
@@ -6,16 +6,30 @@
  *
  *  \author cerati
  */
+#include "FWCore/Framework/interface/Event.h"
+#include "FWCore/Framework/interface/ESHandle.h"
+#include "FWCore/ParameterSet/interface/ParameterSet.h"
+#include "FWCore/Utilities/interface/EDGetToken.h"
 #include "FWCore/Framework/interface/Frameworkfwd.h"
+
 #include "DQMServices/Core/interface/DQMEDAnalyzer.h"
-#include "Validation/RecoTrack/interface/MultiTrackValidatorBase.h"
+
 #include "Validation/RecoTrack/interface/MTVHistoProducerAlgoForTracker.h"
+#include "SimDataFormats/Associations/interface/TrackToTrackingParticleAssociator.h"
 #include "SimDataFormats/Associations/interface/VertexToTrackingVertexAssociator.h"
+#include "CommonTools/RecoAlgos/interface/CosmicTrackingParticleSelector.h"
+#include "SimTracker/Common/interface/TrackingParticleSelector.h"
 #include "CommonTools/RecoAlgos/interface/RecoTrackSelectorBase.h"
 #include "SimTracker/TrackAssociation/interface/ParametersDefinerForTP.h"
 #include "CommonTools/Utils/interface/DynArray.h"
+#include "DataFormats/Common/interface/ValueMap.h"
 
-class MultiTrackValidator : public DQMEDAnalyzer, protected MultiTrackValidatorBase {
+class PileupSummaryInfo;
+namespace reco {
+class DeDxData;
+}
+
+class MultiTrackValidator : public DQMEDAnalyzer {
  public:
   /// Constructor
   MultiTrackValidator(const edm::ParameterSet& pset);
@@ -32,8 +46,30 @@ class MultiTrackValidator : public DQMEDAnalyzer, protected MultiTrackValidatorB
 
  protected:
   //these are used by MTVGenPs
-  bool UseAssociators;
+  // MTV-specific data members
+  std::vector<edm::InputTag> associators;
+  edm::EDGetTokenT<TrackingParticleCollection> label_tp_effic;
+  edm::EDGetTokenT<TrackingParticleCollection> label_tp_fake;
+  edm::EDGetTokenT<TrackingParticleRefVector> label_tp_effic_refvector;
+  edm::EDGetTokenT<TrackingParticleRefVector> label_tp_fake_refvector;
+  edm::EDGetTokenT<TrackingVertexCollection> label_tv;
+  edm::EDGetTokenT<std::vector<PileupSummaryInfo> > label_pileupinfo;
+
+  std::vector<edm::EDGetTokenT<std::vector<PSimHit> > > simHitTokens_;
+
+  std::vector<edm::InputTag> label;
+  std::vector<edm::EDGetTokenT<edm::View<reco::Track> > > labelToken;
+  std::vector<edm::EDGetTokenT<edm::View<TrajectorySeed> > > labelTokenSeed;
+  edm::EDGetTokenT<reco::BeamSpot>  bsSrc;
+
+  edm::EDGetTokenT<edm::ValueMap<reco::DeDxData> > m_dEdx1Tag;
+  edm::EDGetTokenT<edm::ValueMap<reco::DeDxData> > m_dEdx2Tag;
+
+  std::string parametersDefiner;
+
   const bool parametersDefinerIsCosmic_;
+  const bool ignoremissingtkcollection_;
+  const bool useAssociators_;
   const bool calculateDrSingleCollection_;
   const bool doPlotsOnlyForTruePV_;
   const bool doSummaryPlots_;
