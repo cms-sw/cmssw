f987d941e0b4217865e5c95e6ce013ecacdbedd4
Christopher Jones <chrisdjones15@gmail.com>
2017-04-20 18:11:17 -0500

    Fixed typo

--- GeneratorInterface/Core/interface/GeneratorFilter.h
+++ GeneratorInterface/Core/interface/GeneratorFilter.h
@@ -114,7 +114,7 @@ namespace edm
 
     produces<edm::HepMCProduct>("unsmeared");
     produces<GenEventInfoProduct>();
-    produces<GenLumiInfoHeader, edm::Transtion::BeginLuminosityBlock>();
+    produces<GenLumiInfoHeader, edm::Transition::BeginLuminosityBlock>();
     produces<GenLumiInfoProduct, edm::Transition::EndLuminosityBlock>();
     produces<GenRunInfoProduct, edm::Transition::EndRun>();
  
