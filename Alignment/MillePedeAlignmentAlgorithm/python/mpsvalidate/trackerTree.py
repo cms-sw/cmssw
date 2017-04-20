f6acf65b9209a08e8d64728c684dc4363ec0cb46
Gregor Mittag <Gregor.Mittag@cern.ch>
2017-04-20 17:58:55 +0200

    Create TrackerTree.root file for each campaign separately.
     - create it depending on the used tracker geometry
     - allows to use different geometries with the same release area
     - checking out MPS is no longer required to run 'mps_validate.py'

--- Alignment/MillePedeAlignmentAlgorithm/python/mpsvalidate/trackerTree.py
+++ Alignment/MillePedeAlignmentAlgorithm/python/mpsvalidate/trackerTree.py
@@ -8,20 +8,26 @@
 import logging
 import os
 
+from Alignment.MillePedeAlignmentAlgorithm.alignmentsetup.helper \
+    import checked_out_MPS
+
 
 def check(config):
     logger = logging.getLogger("mpsvalidate")
     logger.info("Check if TrackerTree.root file exists")
     
-    outputpath = os.path.join(os.environ['CMSSW_BASE'], "src", "Alignment", "MillePedeAlignmentAlgorithm", "python", "mpsvalidate", "TrackerTree.root")
-    print(outputpath)
+    outputpath = os.path.join(config.jobDataPath, ".TrackerTree.root")
 
     # check if file exists
-    if (not os.path.isfile(outputpath)):
+    if not os.path.isfile(outputpath):
         logger.info("TrackerTree.root file does not exist. It will be created now.")
         
-        configpath = os.path.join(os.environ["CMSSW_BASE"], "src", "Alignment", "MillePedeAlignmentAlgorithm", "test", "trackerTree_cfg.py")
+        configpath = os.path.join(config.mpspath, "test", "trackerTree_cfg.py")
         logger.info("Path to the config file: {0}".format(configpath))
         
-        
-        os.system("cmsRun {0} outputpath={1}".format(configpath, outputpath))
+        cmd = "cmsRun {0} outputFile={1}".format(configpath, outputpath)
+        if config.globalTag != None: cmd += " globalTag="+config.globalTag
+        if config.firstRun != None: cmd += " firstRun="+config.firstRun
+        os.system(cmd+" > /dev/null 2>&1")
+
+    return os.path.abspath(outputpath)
