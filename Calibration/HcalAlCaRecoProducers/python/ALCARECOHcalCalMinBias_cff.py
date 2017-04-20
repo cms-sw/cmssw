a45b71292844e3613b6096c0cdffa6e04c15a299
Sunanda <sunanda.banerjee@cern.ch>
2017-04-20 10:53:32 +0200

    Try to have part of the common specification in ALCARECOHcalCalMinBiasNoise_cff

--- Calibration/HcalAlCaRecoProducers/python/ALCARECOHcalCalMinBias_cff.py
+++ Calibration/HcalAlCaRecoProducers/python/ALCARECOHcalCalMinBias_cff.py
@@ -4,18 +4,7 @@ import FWCore.ParameterSet.Config as cms
 #AlCaReco filtering for HCAL minbias:
 #------------------------------------------------
 
-import EventFilter.HcalRawToDigi.HcalRawToDigi_cfi
-hcalDigiAlCaMB = EventFilter.HcalRawToDigi.HcalRawToDigi_cfi.hcalDigis.clone()
-
-hcalDigiAlCaMB.firstSample = 0
-hcalDigiAlCaMB.lastSample = 9
-hcalDigiAlCaMB.InputLabel = 'rawDataCollector'
-
-#add GT digi:
-import EventFilter.L1GlobalTriggerRawToDigi.l1GtUnpack_cfi
-gtDigisAlCaMB = EventFilter.L1GlobalTriggerRawToDigi.l1GtUnpack_cfi.l1GtUnpack.clone()
-
-gtDigisAlCaMB.DaqGtInputTag = 'source'
+from Calibration.HcalAlCaRecoProducers.ALCARECOHcalCalMinBiasNoise_cff import *
 
 import HLTrigger.HLTfilters.hltHighLevel_cfi
 hcalminbiasHLT =  HLTrigger.HLTfilters.hltHighLevel_cfi.hltHighLevel.clone(
@@ -48,32 +37,8 @@ hbherecoMBNZS = RecoLocalCalo.HcalRecProducers.HBHEPhase1Reconstructor_cfi.hbhep
 hbherecoMBNZS.algorithm.firstSample = 4
 hbherecoMBNZS.algorithm.samplesToAdd = 4
 
-hbherecoNoise = RecoLocalCalo.HcalRecProducers.HBHEPhase1Reconstructor_cfi.hbheprereco.clone(
-    digiLabelQIE8  = cms.InputTag("hcalDigiAlCaMB"),
-    digiLabelQIE11 = cms.InputTag("hcalDigiAlCaMB"),
-    tsFromDB = cms.bool(False),
-    dropZSmarkedPassed = cms.bool(False),
-    algorithm = dict(
-        useM2 = cms.bool(False),
-        useM3 = cms.bool(False)
-    ),
-    processQIE11 = cms.bool(False),
-    setNegativeFlagsQIE8 = cms.bool(False),
-    setNegativeFlagsQIE11 = cms.bool(False),
-    setNoiseFlagsQIE8 = cms.bool(True),
-    setNoiseFlagsQIE11 = cms.bool(False),
-    setPulseShapeFlagsQIE8 = cms.bool(False),
-    setPulseShapeFlagsQIE11 = cms.bool(False),
-    setLegacyFlagsQIE8 = cms.bool(False),
-    setLegacyFlagsQIE11 = cms.bool(False),
-)
-
-hbherecoNoise.algorithm.firstSample = 0
-hbherecoNoise.algorithm.samplesToAdd = 4
-
 import RecoLocalCalo.HcalRecProducers.HcalSimpleReconstructor_ho_cfi
 horecoMBNZS = RecoLocalCalo.HcalRecProducers.HcalSimpleReconstructor_ho_cfi.horeco.clone()
-horecoNoise = RecoLocalCalo.HcalRecProducers.HcalSimpleReconstructor_ho_cfi.horeco.clone()
 
 horecoMBNZS.firstSample = 4
 horecoMBNZS.samplesToAdd = 4
@@ -81,27 +46,14 @@ horecoMBNZS.digiLabel = 'hcalDigiAlCaMB'
 horecoMBNZS.tsFromDB = cms.bool(False)
 horecoMBNZS.dropZSmarkedPassed = cms.bool(False)
 
-horecoNoise.firstSample = 0
-horecoNoise.samplesToAdd = 4
-horecoNoise.digiLabel = 'hcalDigiAlCaMB'
-horecoNoise.tsFromDB = cms.bool(False)
-horecoNoise.dropZSmarkedPassed = cms.bool(False)
-
 import RecoLocalCalo.HcalRecProducers.HcalSimpleReconstructor_hf_cfi
 hfrecoMBNZS = RecoLocalCalo.HcalRecProducers.HcalSimpleReconstructor_hf_cfi.hfreco.clone()
-hfrecoNoise = RecoLocalCalo.HcalRecProducers.HcalSimpleReconstructor_hf_cfi.hfreco.clone()
 
 hfrecoMBNZS.firstSample = 2
 hfrecoMBNZS.samplesToAdd = 2
 hfrecoMBNZS.digiLabel = 'hcalDigiAlCaMB'
 hfrecoMBNZS.tsFromDB = cms.bool(False)
 hfrecoMBNZS.dropZSmarkedPassed = cms.bool(False)
-
-hfrecoNoise.firstSample = 0
-hfrecoNoise.samplesToAdd = 2
-hfrecoNoise.digiLabel = 'hcalDigiAlCaMB'
-hfrecoNoise.tsFromDB = cms.bool(False)
-hfrecoNoise.dropZSmarkedPassed = cms.bool(False)
 
 seqALCARECOHcalCalMinBias = cms.Sequence(hcalminbiasHLT*hcalDigiAlCaMB*gtDigisAlCaMB*hbherecoMBNZS*horecoMBNZS*hbherecoNoise*hfrecoNoise*hfrecoMBNZS*horecoNoise)
 seqALCARECOHcalCalMinBiasNoHLT = cms.Sequence(hcalDigiAlCaMB*gtDigisAlCaMB*hbherecoMBNZS*horecoMBNZS*hbherecoNoise*hfrecoNoise*hfrecoMBNZS*horecoNoise)
