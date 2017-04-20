04bdf52b381a5034a43e32650a76e6f44b291a9f
Sunanda <sunanda.banerjee@cern.ch>
2017-04-20 10:09:46 +0200

    Protect the print commands within debug protection at compilation time

--- Calibration/IsolatedParticles/src/MatchingSimTrack.cc
+++ Calibration/IsolatedParticles/src/MatchingSimTrack.cc
@@ -5,9 +5,19 @@
 
 #include <iostream>
 
+//#define EDM_ML_DEBUG
+
 namespace spr{
 
-  edm::SimTrackContainer::const_iterator matchedSimTrack(const edm::Event& iEvent, edm::Handle<edm::SimTrackContainer>& SimTk, edm::Handle<edm::SimVertexContainer>& SimVtx, const reco::Track* pTrack, TrackerHitAssociator& associate, bool debug){
+  edm::SimTrackContainer::const_iterator matchedSimTrack(const edm::Event& iEvent, 
+							 edm::Handle<edm::SimTrackContainer>& SimTk, 
+							 edm::Handle<edm::SimVertexContainer>& SimVtx, 
+							 const reco::Track* pTrack, 
+							 TrackerHitAssociator& associate, bool
+#ifdef EDM_ML_DEBUG
+							 debug
+#endif
+							 ) {
   
     edm::SimTrackContainer::const_iterator itr = SimTk->end();;
     
@@ -35,6 +45,7 @@ namespace spr{
       }
     }
 
+#ifdef EDM_ML_DEBUG
     if (debug) {
       std::cout << "Reconstructed Track with " << i << " recHits.";
       for (unsigned int isim=0; isim<trkId.size(); isim++){
@@ -42,8 +53,9 @@ namespace spr{
       } 
       std::cout << std::endl;
     }
-  
     int matchedId=0; 
+#endif
+
     unsigned int matchSimTrk=0;
     if (trkOcc.size() > 0) {
       unsigned int maxTrkOcc=0, idxMax=0;
@@ -53,18 +65,21 @@ namespace spr{
       matchSimTrk = trkId[idxMax];
       for (simTrkItr = SimTk->begin(); simTrkItr!= SimTk->end(); simTrkItr++) {
 	if ( simTrkItr->trackId() == matchSimTrk ) {
+#ifdef EDM_ML_DEBUG
 	  matchedId = simTrkItr->type();
 	  if (debug) std::cout << "matched trackId (maximum occurance) " << matchSimTrk << " type " << matchedId << std::endl;
+#endif
 	  itr = simTrkItr;
 	  break;
 	}
       }
     }
   
+#ifdef EDM_ML_DEBUG
     if (matchedId==0 && debug) {
       std::cout << "Could not find matched SimTrk and track history now " << std::endl;
     }
-
+#endif
     return itr;
   }
 
@@ -74,8 +89,9 @@ namespace spr{
     // get the matching SimTrack
     edm::SimTrackContainer::const_iterator trkInfo = spr::matchedSimTrack(iEvent, SimTk, SimVtx, pTrack,associate, debug);
     unsigned int matchSimTrk = trkInfo->trackId();
+#ifdef EDM_ML_DEBUG
     if (debug) std::cout << "matchedSimTrackId finds the SimTrk ID of the current track to be " << matchSimTrk << std::endl;
-  
+#endif  
     std::vector<int> matchTkid;
     if( trkInfo->type() != 0) {
       edm::SimTrackContainer::const_iterator simTrkItr;
@@ -98,10 +114,12 @@ namespace spr{
 	  info.charge = simTrkItr->charge();
 	} else {
 	  edm::SimTrackContainer::const_iterator parentItr =  spr::parentSimTrack(simTrkItr, SimTk, SimVtx, debug);
+#ifdef EDM_ML_DEBUG
 	  if (debug) {
 	    if (parentItr != SimTk->end() ) std::cout << "original parent of " << simTrkItr->trackId() << " " << parentItr->trackId() << ", " << parentItr->type() << std::endl;
 	    else                            std::cout << "original parent of " << simTrkItr->trackId() << " not found" << std::endl;
 	  }
+#endif
 	  if (parentItr != SimTk->end()) {
 	    info.found  = true;
 	    info.pdgId  = parentItr->type();
@@ -117,8 +135,9 @@ namespace spr{
   // Checks if this SimTrack=thisTrkItr originates from the one with trackId=simTkId
   bool validSimTrack(unsigned int simTkId, edm::SimTrackContainer::const_iterator thisTrkItr, edm::Handle<edm::SimTrackContainer>& SimTk, edm::Handle<edm::SimVertexContainer>& SimVtx, bool debug){
     
+#ifdef EDM_ML_DEBUG
     if (debug) std::cout << "Inside validSimTrack: trackId " << thisTrkItr->trackId() << " vtxIndex " <<  thisTrkItr->vertIndex() << " to be matched to " << simTkId << std::endl;
-    
+#endif    
     //This track originates from simTkId
     if (thisTrkItr->trackId() == simTkId) return true;
 
@@ -129,7 +148,9 @@ namespace spr{
     edm::SimVertexContainer::const_iterator simVtxItr= SimVtx->begin();
     for (int iv=0; iv<vertIndex; iv++) simVtxItr++;
     int parent = simVtxItr->parentIndex();
+#ifdef EDM_ML_DEBUG
     if (debug) std::cout << "validSimTrack:: parent index " << parent <<" ";
+#endif
     if (parent < 0 && simVtxItr != SimVtx->begin()) {
       const math::XYZTLorentzVectorD pos1 = simVtxItr->position();
       for (simVtxItr=SimVtx->begin(); simVtxItr!=SimVtx->end(); ++simVtxItr) {
@@ -143,7 +164,9 @@ namespace spr{
 	}
       }
     }
+#ifdef EDM_ML_DEBUG
     if (debug) std::cout << "final index " << parent << std::endl;;
+#endif
     for(edm::SimTrackContainer::const_iterator simTrkItr= SimTk->begin(); simTrkItr!= SimTk->end(); simTrkItr++){
       if ((int)simTrkItr->trackId() == parent && simTrkItr != thisTrkItr) return  validSimTrack(simTkId, simTrkItr, SimTk, SimVtx, debug) ; 
     }
@@ -158,9 +181,9 @@ namespace spr{
     edm::SimTrackContainer::const_iterator itr = SimTk->end();
 
     int vertIndex = thisTrkItr->vertIndex();
-    int type = thisTrkItr->type(); int charge = (int)thisTrkItr->charge();
-    if (debug) std::cout << "SimTrackParent " << thisTrkItr->trackId() << " Vertex " <<  vertIndex << " Type " << type << " Charge " << charge << std::endl;
-
+#ifdef EDM_ML_DEBUG
+    if (debug) std::cout << "SimTrackParent " << thisTrkItr->trackId() << " Vertex " <<  vertIndex << " Type " << thisTrkItr->type() << " Charge " << (int)thisTrkItr->charge() << std::endl;
+#endif
     if( vertIndex == -1 )                      return thisTrkItr;  
     else if (vertIndex >= (int)SimVtx->size()) return itr; 
 
