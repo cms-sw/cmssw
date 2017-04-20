5fd9ec4fc3b2c7e8a5c91fa45eff8fd8bae8ad7b
Gregor Mittag <Gregor.Mittag@cern.ch>
2017-04-20 16:59:01 +0200

    Moved template files to 'templates' directory.
    This is also a first step towards running the MPS validation w/o checking out
    'Alignment/MillePedeAlignmentAlgorithm'.

--- Alignment/MillePedeAlignmentAlgorithm/python/mpsvalidate/geometry.py
+++ Alignment/MillePedeAlignmentAlgorithm/python/mpsvalidate/geometry.py
@@ -31,7 +31,8 @@ class Alignables:
         discriminator = self.get_discriminator(objid)
         ndiscriminator = {key: [] for key in discriminator}
         # open TrackerTree.root file
-        treeFile = ROOT.TFile(os.path.join(self.config.mpspath, "TrackerTree.root"))
+        treeFile = ROOT.TFile(os.path.join(self.config.mpspath, "python",
+                                           "mpsvalidate", "TrackerTree.root"))
         tree = treeFile.Get("TrackerTreeGenerator/TrackerTree/TrackerTree")
 
         for entry in tree:
@@ -89,7 +90,8 @@ class Alignables:
         # list of all detids in the structure
         detids = []
         # open TrackerTree.root file
-        treeFile = ROOT.TFile(os.path.join(self.config.mpspath, "TrackerTree.root"))
+        treeFile = ROOT.TFile(os.path.join(self.config.mpspath, "python",
+                                           "mpsvalidate", "TrackerTree.root"))
         tree = treeFile.Get("TrackerTreeGenerator/TrackerTree/TrackerTree")
 
         for entry in tree:
