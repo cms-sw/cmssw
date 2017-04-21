d8cde0d45816bf806d77b7b235cfa8d0d6669ffc
Cesare <cesare.calabria@ba.infn.it>
2017-04-21 11:44:09 +0200

    adding also GEMMuonArbitrated to selectedPatMuons

--- PhysicsTools/PatAlgos/python/slimming/miniAOD_tools.py
+++ PhysicsTools/PatAlgos/python/slimming/miniAOD_tools.py
@@ -91,7 +91,7 @@ def miniAOD_customizeCommon(process):
     process.selectedPatMuons.cut = cms.string("pt > 5 || isPFMuon || (pt > 3 && (isGlobalMuon || isStandAloneMuon || numberOfMatches > 0 || muonID('RPCMuLoose')))")
     
     from Configuration.Eras.Modifier_phase2_muon_cff import phase2_muon
-    phase2_muon.toModify(process.selectedPatMuons, cut = "pt > 5 || isPFMuon || (pt > 3 && (isGlobalMuon || isStandAloneMuon || numberOfMatches > 0 || muonID('RPCMuLoose') || muonID('ME0MuonArbitrated')))")
+    phase2_muon.toModify(process.selectedPatMuons, cut = "pt > 5 || isPFMuon || (pt > 3 && (isGlobalMuon || isStandAloneMuon || numberOfMatches > 0 || muonID('RPCMuLoose') || muonID('ME0MuonArbitrated') || muonID('GEMMuonArbitrated')) )")
     
     process.selectedPatElectrons.cut = cms.string("")
     process.selectedPatTaus.cut = cms.string("pt > 18. && tauID('decayModeFindingNewDMs')> 0.5")
