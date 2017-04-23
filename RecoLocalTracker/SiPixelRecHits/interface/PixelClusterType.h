833f5bcdc6de66c6ce58736d1ea2dd1727069238
Vincenzo Innocente <vincenzo.innocente@cern.ch>
2017-04-23 12:41:26 +0200

    identify the type of cluster

---
+++ RecoLocalTracker/SiPixelRecHits/interface/PixelClusterType.h
@@ -0,0 +1,6 @@
+#ifndef RecoLocalTrackerSiPixelRecHitsPixelClusterType_H
+#define	RecoLocalTrackerSiPixelRecHitsPixelClusterType_H
+enum  class PixelClusterType { Multi, Big, One};
+#endif	// RecoLocalTrackerSiPixelRecHitsPixelClusterType_H
+
+
