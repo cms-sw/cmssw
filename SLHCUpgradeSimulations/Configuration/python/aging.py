cb5dd21931c9eff3565345e2a86e751af1a99c37
Kevin Pedro <kpedro88@gmail.com>
2017-04-24 10:33:56 -0500

    clean out old stuff

--- SLHCUpgradeSimulations/Configuration/python/aging.py
+++ SLHCUpgradeSimulations/Configuration/python/aging.py
@@ -1,20 +1,5 @@
 import FWCore.ParameterSet.Config as cms
 
-def agePixel(process,lumi):
-    prd=1.0
-    if lumi>299:
-        prd=1.0
-    if lumi>499:
-        prd=1.5
-    if lumi>999:
-        prd=0.
-        
-    # danger - watch for someone turning off pixel aging - if off - leave off
-    if hasattr(process,'mix') and hasattr(process.mix,'digitizers') and hasattr(process.mix.digitizers,'pixel') and not hasattr(process.mix.digitizers.pixel,'NoAging'):
-        process.mix.digitizers.pixel.PseudoRadDamage =  cms.double(float(prd*7.65))
-        process.mix.digitizers.pixel.PseudoRadDamageRadius =  cms.double(16.5)
-    return process    
-
 # handle normal mixing or premixing
 def getHcalDigitizer(process):
     if hasattr(process,'mixData'):
@@ -133,97 +118,23 @@ def ageEcal(process,lumi):
         process.g4SimHits.ECalSD.DelivLuminosity = cms.double(float(lumi))
     return process
 
-def customise_aging_100(process):
-
-    process=ageHcal(process,100)
-    process=ageEcal(process,100)
-    process=agePixel(process,100)
-    return process
-
-def customise_aging_200(process):
-
-    process=ageHcal(process,200)
-    process=ageEcal(process,200)
-    process=agePixel(process,200)
-    return process
-
 def customise_aging_300(process):
 
     process=ageHcal(process,300)
     process=ageEcal(process,300)
-    process=agePixel(process,300)
-    return process
-
-def customise_aging_400(process):
-
-    process=ageHcal(process,400)
-    process=ageEcal(process,400)
-    process=agePixel(process,400)
-    return process
-
-def customise_aging_500(process):
-
-    process=ageHcal(process,500)
-    process=ageEcal(process,500)
-    process=agePixel(process,500)
-    return process
-
-def customise_aging_600(process):
-
-    process=ageHcal(process,600)
-    process=ageEcal(process,600)
-    process=agePixel(process,600)
-    return process
-
-def customise_aging_700(process):
-
-    process=ageHcal(process,700)
-    process=ageEcal(process,700)
-    process=agePixel(process,700)
     return process
 
 def customise_aging_1000(process):
 
     process=ageHcal(process,1000)
     process=ageEcal(process,1000)
-    process=agePixel(process,1000)
     return process
 
 def customise_aging_3000(process):
 
     process=ageHcal(process,3000)
     process=ageEcal(process,3000)
-    process=agePixel(process,3000)
     return process
-
-def customise_aging_ecalonly_300(process):
-
-    process=ageEcal(process,300)
-    return process
-
-def customise_aging_ecalonly_1000(process):
-
-    process=ageEcal(process,1000)
-    return process
-
-def customise_aging_ecalonly_3000(process):
-
-    process=ageEcal(process,3000)
-    return process
-
-def customise_aging_newpixel_1000(process):
-
-    process=ageEcal(process,1000)
-    process=ageHcal(process,1000)
-    return process
-
-def customise_aging_newpixel_3000(process):
-
-    process=ageEcal(process,3000)
-    process=ageHcal(process,3000)
-    return process
-
-#no hcal 3000
 
 def ecal_complete_aging(process):
     if hasattr(process,'g4SimHits'):
@@ -231,121 +142,3 @@ def ecal_complete_aging(process):
     if hasattr(process,'ecal_digi_parameters'):    
         process.ecal_digi_parameters.UseLCcorrection = cms.untracked.bool(False)
     return process
-
-def turn_off_Pixel_aging(process):
-
-    if hasattr(process,'mix') and hasattr(process.mix,'digitizers') and hasattr(process.mix.digitizers,'hcal'):    
-        setattr(process.mix.digitizers.pixel,'NoAging',cms.double(1.))
-        process.mix.digitizers.pixel.PseudoRadDamage =  cms.double(0.)
-    return process
-
-def turn_on_Pixel_aging_1000(process):
-    # just incase we want aging afterall
-    if hasattr(process,'mix') and hasattr(process.mix,'digitizers') and hasattr(process.mix.digitizers,'hcal'):    
-        process.mix.digitizers.pixel.PseudoRadDamage =  cms.double(1.5)
-        process.mix.digitizers.pixel.PseudoRadDamageRadius =  cms.double(4.0)
-
-    return process
-
-def ecal_complete_aging_300(process):
-    process=ecal_complete_aging(process)
-
-    if not hasattr(process.GlobalTag,'toGet'):
-        process.GlobalTag.toGet=cms.VPSet()
-    process.GlobalTag.toGet.extend( cms.VPSet(
-        cms.PSet(record = cms.string("EcalPedestalsRcd"),
-                 tag = cms.string("EcalPedestals_TL300_IL1E34_mc"),
-                 connect = cms.untracked.string("frontier://FrontierProd/CMS_CONDITIONS")
-                 ),
-        ## laser D
-        cms.PSet(record = cms.string("EcalLaserAPDPNRatiosRcd"),
-                 tag = cms.string("EcalLaserAPDPNRatios_TL300_IL1E34_v2_mc"),
-                 connect = cms.untracked.string("frontier://FrontierProd/CMS_CONDITIONS")
-                 ),
-        ## L1 trigger
-        cms.PSet(record = cms.string("EcalTPGLinearizationConstRcd"),
-                 tag = cms.string("EcalTPGLinearizationConst_TL300_IL1E34_v2_mc"),
-                 connect = cms.untracked.string("frontier://FrontierProd/CMS_CONDITIONS")
-                 ),
-        cms.PSet(record = cms.string('EcalLaserAlphasRcd'),
-                 tag = cms.string('EcalLaserAlphas_EB_sic1_btcp1_EE_sic1_btcp1'),
-                 connect = cms.untracked.string('frontier://FrontierProd/CMS_CONDITIONS')
-                 ),
-        #VPT aging
-        cms.PSet(record = cms.string('EcalLinearCorrectionsRcd'),
-                 tag = cms.string('EcalLinearCorrections_mc'),
-                 connect = cms.untracked.string('frontier://FrontierProd/CMS_CONDITIONS')
-                 )
-        )
-                                    )
-    return process
-
-    
-def ecal_complete_aging_1000(process):
-    process=ecal_complete_aging(process)
-
-    if not hasattr(process.GlobalTag,'toGet'):
-        process.GlobalTag.toGet=cms.VPSet()
-    process.GlobalTag.toGet.extend( cms.VPSet(
-        cms.PSet(record = cms.string("EcalPedestalsRcd"),
-                 tag = cms.string("EcalPedestals_TL1000_IL5E34_mc"),
-                 connect = cms.untracked.string("frontier://FrontierProd/CMS_CONDITIONS")
-                 ),
-        ## laser D
-        cms.PSet(record = cms.string("EcalLaserAPDPNRatiosRcd"),
-                 tag = cms.string("EcalLaserAPDPNRatios_TL1000_IL5E34_v2_mc"),
-                 connect = cms.untracked.string("frontier://FrontierProd/CMS_CONDITIONS")
-                 ),
-        ## L1 trigger
-        cms.PSet(record = cms.string("EcalTPGLinearizationConstRcd"),
-                 tag = cms.string("EcalTPGLinearizationConst_TL1000_IL5E34_v2_mc"),
-                 connect = cms.untracked.string("frontier://FrontierProd/CMS_CONDITIONS")
-                 ),
-        cms.PSet(record = cms.string('EcalLaserAlphasRcd'),
-                 tag = cms.string('EcalLaserAlphas_EB_sic1_btcp1_EE_sic1_btcp1'),
-                 connect = cms.untracked.string('frontier://FrontierProd/CMS_CONDITIONS')
-                 ),
-        #VPT aging
-        cms.PSet(record = cms.string('EcalLinearCorrectionsRcd'),
-                 tag = cms.string('EcalLinearCorrections_mc'),
-                 connect = cms.untracked.string('frontier://FrontierProd/CMS_CONDITIONS')
-                 )
-        )
-                                    )
-    return process
-
-
-def ecal_complete_aging_3000(process):
-    process=ecal_complete_aging(process)
-
-    if not hasattr(process.GlobalTag,'toGet'):
-        process.GlobalTag.toGet=cms.VPSet()
-    process.GlobalTag.toGet.extend( cms.VPSet(
-        cms.PSet(record = cms.string("EcalPedestalsRcd"),
-                 tag = cms.string("EcalPedestals_TL3000_IL5E34_mc"),
-                 connect = cms.untracked.string("frontier://FrontierProd/CMS_CONDITIONS")
-                 ),
-        ## laser D
-        cms.PSet(record = cms.string("EcalLaserAPDPNRatiosRcd"),
-                 tag = cms.string("EcalLaserAPDPNRatios_TL3000_IL5E34_v2_mc"),
-                 connect = cms.untracked.string("frontier://FrontierProd/CMS_CONDITIONS")
-                 ),
-        ## L1 trigger
-        cms.PSet(record = cms.string("EcalTPGLinearizationConstRcd"),
-                 tag = cms.string("EcalTPGLinearizationConst_TL3000_IL5E34_v2_mc"),
-                 connect = cms.untracked.string("frontier://FrontierProd/CMS_CONDITIONS")
-                 ),
-        cms.PSet(record = cms.string('EcalLaserAlphasRcd'),
-                 tag = cms.string('EcalLaserAlphas_EB_sic1_btcp1_EE_sic1_btcp1'),
-                 connect = cms.untracked.string('frontier://FrontierProd/CMS_CONDITIONS')
-                 ),
-        #VPT aging
-        cms.PSet(record = cms.string('EcalLinearCorrectionsRcd'),
-                 tag = cms.string('EcalLinearCorrections_mc'),
-                 connect = cms.untracked.string('frontier://FrontierProd/CMS_CONDITIONS')
-                 )
-        )
-                                    )
-
-    return process
-
