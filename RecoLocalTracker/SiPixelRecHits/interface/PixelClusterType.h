3a4981bd73560d7fd0325ef909f97fe5d3f16ced
Vincenzo Innocente <vincenzo.innocente@cern.ch>
2017-04-24 19:12:04 +0200

    rollback all my changes to pixel cpe

--- RecoLocalTracker/SiPixelRecHits/interface/PixelClusterType.h
+++
@@ -1,6 +0,0 @@
-#ifndef RecoLocalTrackerSiPixelRecHitsPixelClusterType_H
-#define	RecoLocalTrackerSiPixelRecHitsPixelClusterType_H
-enum  class PixelClusterType { Multi, Big, One};
-#endif	// RecoLocalTrackerSiPixelRecHitsPixelClusterType_H
-
-
