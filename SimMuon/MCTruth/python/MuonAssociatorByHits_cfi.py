f10e7e54042e1abfe324dbbe48ca4da643def9ed
Giovanni Abbiendi <Giovanni.Abbiendi@cern.ch>
2017-04-24 09:53:38 +0200

    remove commented line

--- SimMuon/MCTruth/python/MuonAssociatorByHits_cfi.py
+++ SimMuon/MCTruth/python/MuonAssociatorByHits_cfi.py
@@ -146,4 +146,3 @@ run3_GEM.toModify( muonAssociatorByHits, useGEMs = cms.bool(True) )
 from Configuration.Eras.Modifier_phase2_tracker_cff import phase2_tracker
 phase2_tracker.toModify( muonAssociatorByHits, usePhase2Tracker = cms.bool(True) )
 phase2_tracker.toModify( muonAssociatorByHits, pixelSimLinkSrc = "simSiPixelDigis:Pixel" )
-#phase2_tracker.toModify( muonAssociatorByHits, stripSimLinkSrc = "simSiPixelDigis:Tracker" )
