560b03a56c62f8fdff89250a940499da96743d57
Sandro <sandro.fonseca@cern.ch>
2017-04-26 15:54:09 +0200

    CMS convention for class member data

--- FastSimulation/CTPPSFastGeometry/interface/CTPPSToFDetector.h
+++ FastSimulation/CTPPSFastGeometry/interface/CTPPSToFDetector.h
@@ -11,55 +11,55 @@ class CTPPSToFDetector {
             CTPPSToFDetector(int ncellx,int ncelly, double cellw,double cellh,double pitchx,double pitchy,double pos, int res);
             virtual ~CTPPSToFDetector() {};
 
-            double GetHeight()                       {return DetH;};
-            double GetWidth()                        {return DetW;};
-            double GetPosition()                     {return DetPosition;};
+            double getHeight()                       {return detH_;};
+            double getWidth()                        {return detW_;};
+            double getPosition()                     {return detPosition_;};
             int findCellId(double x, double y);   // return the cell id corresponding to the given position
             bool get_CellCenter(int cell_id, double& x, double& y); // return false if it failed
-            int get_CellId(int idx)                  {if (idx>=(int)ToFInfo.size()) return 0;
-                                                      std::map<int,std::vector<double> >::const_iterator it=ToFInfo.begin();
+            int get_CellId(int idx)                  {if (idx>=(int)theToFInfo.size()) return 0;
+                                                      std::map<int,std::vector<double> >::const_iterator it=theToFInfo.begin();
                                                       std::advance(it,idx);
                                                       return it->first;
                                                      }
-            int get_CellMultiplicity()               {return (int)ToFInfo.size();}; // return the number of cells with hit
-            int GetMultiplicityByCell(int cellid)    {if (!ToFInfo.count(cellid)) return 0;
-                                                      return (int)ToFInfo.at(cellid).size();}; // return the hit multiplicity of the given cell
+            int get_CellMultiplicity()               {return (int)theToFInfo.size();}; // return the number of cells with hit
+            int getMultiplicityByCell(int cellid)    {if (!theToFInfo.count(cellid)) return 0;
+                                                      return (int)theToFInfo.at(cellid).size();}; // return the hit multiplicity of the given cell
 
-            int get_NHits()                          {return NHits;}; // return the total hit multiplicity (full det)
-            std::vector<double> get_ToF(int cell)    {if (!ToFInfo.count(cell)) return std::vector<double>();
-                                                      return ToFInfo.at(cell);
+            int get_nHits_()                          {return nHits_;}; // return the total hit multiplicity (full det)
+            std::vector<double> get_ToF(int cell)    {if (!theToFInfo.count(cell)) return std::vector<double>();
+                                                      return theToFInfo.at(cell);
                                                      }; // return the tof of the given cell
-            int GetADC(int cell, int hit)            {
-                                                      if (!nADC.count(cell)) return 0;
-                                                      if ((int)nADC.at(cell).size()<hit) return 0;
-                                                      return nADC.at(cell).at(hit);
+            int getADC(int cell, int hit)            {
+                                                      if (!nADC_.count(cell)) return 0;
+                                                      if ((int)nADC_.at(cell).size()<hit) return 0;
+                                                      return nADC_.at(cell).at(hit);
                                                      }
 
 
             void AddHit(double x, double y,double tof);
-            void clear() {DetId=0;NHits=0;ToFInfo.clear();};
+            void clear() {detId_=0;nHits_=0;theToFInfo.clear();};
       private:
-            int                 NCellX;
-            int                 NCellY;
-            double              CellWq; // width (X, horizontal dimension in mm) 
-            std::vector<double> CellW;//move to vector - diamond geometry
-            double              CellH; // height(Y, vertical dimension in mm)
-            double              PitchX; // distance (in X) between cells
-            double              PitchY; // distance (in Y) between cells
-            int                 fToFResolution;  // in ps
-            std::vector<std::pair<double,double> > CellColumn; // lower and upper limits of cells in X
-            std::vector<std::pair<double,double> > CellRow;    // lower and upper limits of cells in Y
+            int                 nCellX_;
+            int                 nCellY_;
+            double              cellWq_; // width (X, horizontal dimension in mm) 
+            std::vector<double> cellW_;//move to vector - diamond geometry
+            double              cellH_; // height(Y, vertical dimension in mm)
+            double              pitchX_; // distance (in X) between cells
+            double              pitchY_; // distance (in Y) between cells
+            int                 fToFResolution_;  // in ps
+            std::vector<std::pair<double,double> > cellColumn_; // lower and upper limits of cells in X
+            std::vector<std::pair<double,double> > cellRow_;    // lower and upper limits of cells in Y
 //
-            double              DetW; // detector width
-            double              DetH; // detector height
-            double              DetPosition; // detector position from beam (absolute value)
+            double              detW_; // detector width
+            double              detH_; // detector height
+            double              detPosition_; // detector position from beam (absolute value)
 //
-            int                 DetId;
-            int                 NHits;
-            std::map<int,std::vector<int> >    nADC;   // fake ADC counter: in case of multiple hits in the same cell, 
+            int                 detId_;
+            int                 nHits_;
+            std::map<int,std::vector<int> >    nADC_;   // fake ADC counter: in case of multiple hits in the same cell, 
                                                     // it  counts the number of overlaps
-            std::map<int,std::vector<double> > ToFInfo;
+            std::map<int,std::vector<double> > theToFInfo;
 
-            typedef std::map<int,std::vector<double> > ToFInfo_t; // define a type for the tof info
+            typedef std::map<int,std::vector<double> > theToFInfo_t; // define a type for the tof info
 };
 #endif
