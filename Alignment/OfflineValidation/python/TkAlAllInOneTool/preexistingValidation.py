4622d635fce9115aae97a6c7417b818450bb71fc
Heshy Roskes <jroskes1@jhu.edu>
2017-04-26 19:33:54 +0200

    fix preexisting validation

--- Alignment/OfflineValidation/python/TkAlAllInOneTool/preexistingValidation.py
+++ Alignment/OfflineValidation/python/TkAlAllInOneTool/preexistingValidation.py
@@ -4,6 +4,7 @@ from geometryComparison import GeometryComparison
 from helperFunctions import getCommandOutput2, parsecolor, parsestyle
 from monteCarloValidation import MonteCarloValidation
 from offlineValidation import OfflineValidation
+from plottingOptions import PlottingOptions
 from TkAlExceptions import AllInOneError
 from trackSplittingValidation import TrackSplittingValidation
 from zMuMuValidation import ZMuMuValidation
@@ -15,25 +16,41 @@ class PreexistingValidation(GenericValidation):
     """
     defaults = {"title": ".oO[name]Oo."}
     mandatories = {"file", "color", "style"}
+    removemandatories = {"dataset", "maxevents", "trackcollection"}
     def __init__(self, valName, config):
         self.general = config.getGeneral()
         self.name = self.general["name"] = valName
         self.config = config
 
-        theUpdate = config.getResultingSection("preexisting"+self.valType+":"+self.name)
+        theUpdate = config.getResultingSection("preexisting"+self.valType+":"+self.name,
+                                               defaultDict = self.defaults,
+                                               demandPars = self.mandatories)
         self.general.update(theUpdate)
 
         self.title = self.general["title"]
         if "|" in self.title or "," in self.title or '"' in self.title:
             msg = "The characters '|', '\"', and ',' cannot be used in the alignment title!"
             raise AllInOneError(msg)
+        self.needsproxy = bool(int(self.general["needsproxy"]))
+        self.jobid = self.general["jobid"]
+        if self.jobid:
+            try:  #make sure it's actually a valid jobid
+                output = getCommandOutput2("bjobs %(jobid)s 2>&1"%self.general)
+                if "is not found" in output: raise RuntimeError
+            except RuntimeError:
+                raise AllInOneError("%s is not a valid jobid.\nMaybe it finished already?"%self.jobid)
 
-        knownOpts = self.defaults.keys()+self.mandatories
+        knownOpts = set(self.defaults.keys())|self.mandatories|self.optionals
         ignoreOpts = []
         config.checkInput("preexisting"+self.valType+":"+self.name,
                           knownSimpleOptions = knownOpts,
                           ignoreOptions = ignoreOpts)
         self.jobmode = None
+
+        try:  #initialize plotting options for this validation type
+            result = PlottingOptions(self.config, self.valType)
+        except KeyError:
+            pass
 
     @property
     def filesToCompare(self):
@@ -41,7 +58,11 @@ class PreexistingValidation(GenericValidation):
 
     def getRepMap(self):
         #do not call super
-        result = self.general.copy()
+        try:
+            result = PlottingOptions(self.config, self.valType)
+        except KeyError:
+            result = {}
+        result.update(self.general)
         result.update({
                        "color": str(parsecolor(result["color"])),
                        "style": str(parsestyle(result["style"])),
@@ -65,7 +86,7 @@ class PreexistingOfflineValidation(PreexistingValidation, OfflineValidation):
             "OfflineTreeBaseDir":"",
             "SurfaceShapes":""
             }
-    defaults = deprecateddefaults
+    defaults = deprecateddefaults.copy()
     def __init__(self, valName, config):
         super(PreexistingOfflineValidation, self).__init__(valName, config)
         for option in self.deprecateddefaults:
@@ -75,8 +96,9 @@ class PreexistingOfflineValidation(PreexistingValidation, OfflineValidation):
     def getRepMap(self):
         result = super(PreexistingOfflineValidation, self).getRepMap()
         result.update({
-                       "filetoplot": self.getCompareStrings("OfflineValidation", plain=True),
+                       "filetoplot": self.general["file"],
                      })
+        return result
 
     def appendToMerge(self, *args, **kwargs):
         raise AllInOneError("Shouldn't be here...")
