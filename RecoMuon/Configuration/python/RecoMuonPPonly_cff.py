b13a91f1aac971f2dbc56b2da38047b091362f2f
Cesare Calabria <cesare.calabria@ba.infn.it>
2017-04-26 10:50:36 +0200

    moving era modifiers

--- RecoMuon/Configuration/python/RecoMuonPPonly_cff.py
+++ RecoMuon/Configuration/python/RecoMuonPPonly_cff.py
@@ -83,30 +83,6 @@ muonGlobalReco = cms.Sequence(globalmuontracking*muonIdProducerSequence*muonSele
 
 ########################################################
 
-_enableGEMMeasurement = dict( EnableGEMMeasurement = cms.bool(True) )
-from Configuration.Eras.Modifier_run3_GEM_cff import run3_GEM
-run3_GEM.toModify( standAloneMuons, STATrajBuilderParameters = dict(
-    FilterParameters = _enableGEMMeasurement, 
-    BWFilterParameters = _enableGEMMeasurement ) )
-run3_GEM.toModify( displacedStandAloneMuons, STATrajBuilderParameters = dict(
-    FilterParameters = _enableGEMMeasurement, 
-    BWFilterParameters = _enableGEMMeasurement ) )
-run3_GEM.toModify( refittedStandAloneMuons, STATrajBuilderParameters = dict(
-    FilterParameters = _enableGEMMeasurement,
-    BWFilterParameters = _enableGEMMeasurement ) )
-
-_enableME0Measurement = dict( EnableME0Measurement = cms.bool(True) )
-from Configuration.Eras.Modifier_phase2_muon_cff import phase2_muon
-phase2_muon.toModify( standAloneMuons, STATrajBuilderParameters = dict(
-    FilterParameters = _enableME0Measurement,
-    BWFilterParameters = _enableME0Measurement ) )
-phase2_muon.toModify( displacedStandAloneMuons, STATrajBuilderParameters = dict(
-    FilterParameters = _enableME0Measurement,
-    BWFilterParameters = _enableME0Measurement ) )
-phase2_muon.toModify( refittedStandAloneMuons, STATrajBuilderParameters = dict(
-    FilterParameters = _enableME0Measurement,
-    BWFilterParameters = _enableME0Measurement ) )
-
 from RecoMuon.MuonIdentification.me0MuonReco_cff import *
 _phase2_muonGlobalReco = muonGlobalReco.copy()
 _phase2_muonGlobalReco += me0MuonReco
