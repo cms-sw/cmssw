c0f54d3fdb41c2036b03c6af8a5141e78b890266
Saba Sehrish <ssehrish@fnal.gov>
2017-04-27 15:50:27 -0500

    more consts

--- EventFilter/ESDigiToRaw/interface/ESDataFormatter.h
+++ EventFilter/ESDigiToRaw/interface/ESDataFormatter.h
@@ -36,40 +36,19 @@ class ESDataFormatter {
   typedef uint64_t Word64;
 
   ESDataFormatter(const edm::ParameterSet& ps) : 
-    pset_(ps)
-//, run_number_(0), orbit_number_(0), bx_(0), lv1_(0), trgtype_(0),
-  //  kchip_bc_(0), kchip_ec_(0) 
-{ 
-    debug_ = pset_.getUntrackedParameter<bool>("debugMode", false);
-    printInHex_ = pset_.getUntrackedParameter<bool>("printInHex", false);
-  };
+    pset_(ps), trgtype_(0), 
+    debug_(pset_.getUntrackedParameter<bool>("debugMode", false)),
+    printInHex_(pset_.getUntrackedParameter<bool>("printInHex", false))
+ { };
   virtual ~ESDataFormatter() {};
 
   virtual void DigiToRaw(int fedId, Digis & digis, FEDRawData& fedRawData, const Meta_Data& meta_data) const = 0;
-/*
-  virtual void setRunNumber(int i) {run_number_ = i;};
-  virtual void setOrbitNumber(int i) {orbit_number_ = i;};
-  virtual void setBX(int i) {bx_ = i;};
-  virtual void setLV1(int i) {lv1_ = i;};
-  virtual void setTriggerType(int i) {trgtype_ = i;};
-  virtual void setKchipBC(int i) {kchip_bc_ = i;};
-  virtual void setKchipEC(int i) {kchip_ec_ = i;};
-*/
   protected :    
     
   const edm::ParameterSet pset_;
-/*
-  int run_number_;
-  int orbit_number_;
-  int bx_;
-  int lv1_;
-  int trgtype_;
-  int kchip_bc_; 
-  int kchip_ec_;
-*/
-  int trgtype_;
-  bool debug_;
-  bool printInHex_; 
+  const int trgtype_;
+  const bool debug_;
+  const bool printInHex_; 
 
   int formatMajor_; 
   int formatMinor_;
