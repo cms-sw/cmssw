aab26b71ae4ccf6a9f13e17df62cb8dd6511064f
Thomas Reis <thomas.reis@cern.ch>
2017-04-28 10:11:37 +0200

    Switch EMTF cancel mode to coordinate based.

--- L1Trigger/L1TMuon/python/simGmtStage2Digis_cfi.py
+++ L1Trigger/L1TMuon/python/simGmtStage2Digis_cfi.py
@@ -15,8 +15,8 @@ simGmtStage2Digis = cms.EDProducer('L1TMuonProducer',
     autoBxRange = cms.bool(True), # if True the output BX range is calculated from the inputs and 'bxMin' and 'bxMax' are ignored
     bxMin = cms.int32(-2),
     bxMax = cms.int32(2),
-    autoCancelMode = cms.bool(True), # if True the cancel out methods are configured depending on the FW version number and 'emtfCancelMode' is ignored
-    emtfCancelMode = cms.string("tracks") # 'tracks' or 'coordinate'
+    autoCancelMode = cms.bool(False), # if True the cancel out methods are configured depending on the FW version number and 'emtfCancelMode' is ignored
+    emtfCancelMode = cms.string("coordinate") # 'tracks' or 'coordinate'
 )
 
 from CondCore.CondDB.CondDB_cfi import CondDB
