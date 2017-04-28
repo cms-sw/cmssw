d9f6b5418c6cb2ae7dcc47305c97dcdb60730675
Felice <felice.pantaleo@cern.ch>
2017-04-28 18:00:33 +0200

    removing internal vector and convert tabs to spaces

--- CommonTools/RecoAlgos/interface/FQueue.h
+++ CommonTools/RecoAlgos/interface/FQueue.h
@@ -3,148 +3,119 @@
 
 #include <vector>
 
-
 template<class T>
 
 class FQueue
 {
-public:
-	FQueue()
-	{
-		theSize = 0;
-		theFront = 0;
-		theTail = 0;
-		theCapacity = 0;
+    public:
+        FQueue()
+        {
+            theSize = 0;
+            theFront = 0;
+            theTail = 0;
+            theCapacity = 0;
+        }
 
-	}
+        FQueue(unsigned int initialCapacity)
+        {
+            theBuffer.resize(initialCapacity);
+            theSize = 0;
+            theFront = 0;
+            theTail = 0;
+            theCapacity = initialCapacity;
+        }
 
-	FQueue(unsigned int initialCapacity)
-	{
+        unsigned int size() const
+        {
+            return theSize;
+        }
 
-		theBuffer.resize(initialCapacity);
+        bool empty() const
+        {
+            return theSize == 0;
+        }
 
-		theSize = 0;
-		theFront = 0;
-		theTail = 0;
+        T front() const
+        {
+            return theBuffer[theFront];
+        }
 
-		theCapacity = initialCapacity;
-	}
+        T & tail()
+        {
+            return theBuffer[theTail];
+        }
 
+        constexpr unsigned int wrapIndex(unsigned int i)
+        {
+            return i & (theBuffer.size() - 1);
+        }
 
+        void push_back(const T & value)
+        {
+            if (theSize >= theCapacity)
+            {
+                theBuffer.resize(theCapacity * 2);
+                if (theFront != 0)
+                {
+                    std::copy(theBuffer.begin(), theBuffer.begin() + theTail,
+                            theBuffer.begin() + theCapacity);
 
-	unsigned int size() const
-	{
-		return theSize;
-	}
+                    theTail += theSize;
 
+                }
+                else
+                {
+                    theTail += theCapacity;
+                }
+                theCapacity *= 2;
+            }
+            theBuffer[theTail] = value;
+            theTail = wrapIndex(theTail + 1);
+            theSize++;
 
-	bool empty() const
-	{
-		return theSize == 0;
-	}
+        }
 
+        void pop_front()
+        {
+            if (theSize > 0)
+            {
+                theFront = wrapIndex(theFront + 1);
+                theSize--;
+            }
+        }
 
-	T front() const
-	{
+        void pop_front(const unsigned int numberOfElementsToPop)
+        {
+            unsigned int elementsToErase =
+                    theSize > numberOfElementsToPop ? numberOfElementsToPop : theSize;
+            theSize -= elementsToErase;
+            theFront = wrapIndex(theFront + elementsToErase);
+        }
 
-		return theBuffer[theFront];
+        void reserve(unsigned int capacity)
+        {
+            theBuffer.reserve(capacity);
+        }
 
-	}
-	T & tail()
-	{
-		return theBuffer[theTail];
-	}
+        T & operator[](unsigned int index)
+        {
+            return theBuffer[wrapIndex(theFront + index)];
+        }
 
-  	constexpr unsigned int wrapIndex(unsigned int i) {
-    	return i & (theBuffer.size()-1);
- 	}
+        void clear()
+        {
+            theBuffer.clear();
+            theSize = 0;
+            theFront = 0;
+            theTail = 0;
+        }
 
-
-	void push_back(const T & value)
-	{
-
-		if (theSize >= theCapacity)
-		{
-			theBuffer.resize(theCapacity *2);
-			if (theFront != 0)
-			{
-				std::copy(theBuffer.begin(), theBuffer.begin() + theTail, theBuffer.begin() + theCapacity);
-
-				theTail += theSize;
-
-
-			}
-			else
-			{
-
-				theTail += theCapacity;
-
-			}
-			theCapacity *=2;
-
-		}
-
-
-		theBuffer[theTail] = value;
-		theTail = wrapIndex(theTail + 1);
-
-		theSize++;
-
-
-	}
-
-
-
-	void pop_front()
-	{
-
-		if (theSize > 0)
-		{
-			theFront = wrapIndex(theFront + 1);
-			theSize--;
-
-
-		}
-	}
-
-	void pop_front(const unsigned int numberOfElementsToPop)
-	{
-		unsigned int elementsToErase =
-				theSize > numberOfElementsToPop ?
-						numberOfElementsToPop : theSize;
-		theSize -= elementsToErase;
-		theFront = wrapIndex(theFront + elementsToErase);
-	}
-
-	void reserve(unsigned int capacity)
-	{
-
-		theBuffer.reserve(capacity);
-
-	}
-
-	T & operator[](unsigned int index)
-	{
-		return theBuffer[wrapIndex(theFront + index)];
-	}
-
-	void clear()
-	{
-
-		theBuffer.clear();
-
-		theSize = 0;
-		theFront = 0;
-		theTail = 0;
-	}
-private:
-	unsigned int theSize;
-	unsigned int theFront;
-	unsigned int theTail;
-
-	std::vector<T> theBuffer;
-
-	unsigned int theCapacity;
+    private:
+        unsigned int theSize;
+        unsigned int theFront;
+        unsigned int theTail;
+        std::vector<T> theBuffer;
+        unsigned int theCapacity;
 
 };
 
