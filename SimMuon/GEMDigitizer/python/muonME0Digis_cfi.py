86ed4e8fb6578a7c87b062a1ed7cc8d78521349a
Sven Dildick <sven.dildick@cern.ch>
2017-04-30 05:53:37 +0200

    ME0 realistic digi updates
    Conflicts:
    	Validation/MuonME0Validation/python/MuonME0Digis_cfi.py

---
+++ SimMuon/GEMDigitizer/python/muonME0Digis_cfi.py
@@ -0,0 +1,33 @@
+import FWCore.ParameterSet.Config as cms
+
+me0DigiCommonParameters = cms.PSet(
+    signalPropagationSpeed = cms.double(0.66),
+    cosmics = cms.bool(False),
+    timeResolution = cms.double(5),
+    timeJitter = cms.double(1.0),
+    averageShapingTime = cms.double(50.0),
+    #clsParametrization = cms.vdouble(0.455091, 0.865613, 0.945891, 0.973286, 0.986234, 0.991686, 0.996865, 0.998501, 1.),
+    averageEfficiency = cms.double(0.98),
+    averageNoiseRate = cms.double(0.001), #intrinsic noise
+    bxwidth = cms.int32(25),
+    minBunch = cms.int32(-5), ## in terms of 25 ns
+    maxBunch = cms.int32(3),
+    mixLabel = cms.string('mix'),	# added by A.Sharma
+    inputCollection = cms.string('g4SimHitsMuonME0Hits'),
+    digiModelString = cms.string('Simple'),
+    digitizeOnlyMuons = cms.bool(False),
+    doBkgNoise = cms.bool(False), #False == No background simulation
+    doNoiseCLS = cms.bool(True),
+    fixedRollRadius = cms.bool(True), #Uses fixed radius in the center of the roll
+    simulateIntrinsicNoise = cms.bool(False),
+    simulateElectronBkg = cms.bool(True),	#False=simulate only neutral Bkg
+    simulateLowNeutralRate = cms.bool(False),	#True=neutral_Bkg at L=1x10^{34}, False at L=5x10^{34}cm^{-2}s^{-1}
+    instLumi = cms.double(7.5), # in units of 1E34 cm^-2 s^-1. Internally the background is parametrized from FLUKA+GEANT results at 5x10^34 (PU140). We are adding a 1.5 factor for PU200
+    rateFact = cms.double(2.0) # We are adding also a safety factor of 2 to take into account the new beam pipe effect (not yet known)
+)
+
+# Module to create simulated ME0 digis.
+simMuonME0Digis = cms.EDProducer("ME0DigiProducer",
+    me0DigiCommonParameters
+)
+
