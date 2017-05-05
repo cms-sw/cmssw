ceeca6679b1018e47464a2320c26d2c8e244553d
Chris Jones <chrisdjones15@gmail.com>
2017-05-05 08:07:09 -0500

    Revert "Revert "Run modules concurrently during global begin transitions""
    This reverts commit e26292386353ecdbf44ff6042e3bace69ecb08d8.

--- FWCore/Framework/src/Principal.cc
+++ FWCore/Framework/src/Principal.cc
@@ -114,7 +114,8 @@ namespace edm {
     reader_(),
     branchType_(bt),
     historyAppender_(historyAppender),
-    cacheIdentifier_(nextIdentifier())
+    cacheIdentifier_(nextIdentifier()),
+    atEndTransition_(false)
   {
     productResolvers_.resize(reg->getNextIndexValue(bt));
     //Now that these have been set, we can create the list of Branches we need.
@@ -306,6 +307,11 @@ namespace edm {
   }
 
   void
+  Principal::setAtEndTransition(bool iAtEnd) {
+    atEndTransition_ = iAtEnd;
+  }
+  
+  void
   Principal::deleteProduct(BranchID const& id) const {
     auto phb = getExistingProduct(id);
     assert(nullptr != phb);
@@ -319,6 +325,7 @@ namespace edm {
                            DelayedReader* reader) {
     //increment identifier here since clearPrincipal isn't called for Run/Lumi
     cacheIdentifier_=nextIdentifier();
+    atEndTransition_=false;
     if(reader) {
       reader_ = reader;
     }
