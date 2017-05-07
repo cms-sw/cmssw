3b33b827a5fa0e7455bd8cd1bd11b097bcf6944c
Vincenzo Innocente <vincenzo.innocente@cern.ch>
2017-05-07 13:49:39 +0200

    force const, clean not needed planes...

--- TrackingTools/TrajectoryState/interface/BasicTrajectoryState.h
+++ TrackingTools/TrajectoryState/interface/BasicTrajectoryState.h
@@ -97,6 +97,19 @@ public:
 			const SurfaceSide side = SurfaceSideDefinition::atCenterOfSurface);
 
 
+  /** Constructor from FTS: just a wrapper
+   */
+  explicit BasicTrajectoryState( const FreeTrajectoryState& fts):
+    theFreeState(fts),
+    theLocalError(InvalidError()),
+    theLocalParameters(),
+    theLocalParametersValid(false),
+    theValid(true),
+    theWeight(1.)
+    {}
+
+
+
 
   /** Constructor from local parameters, errors and surface. For surfaces 
    *  with material the side of the surface should be specified explicitely. 
