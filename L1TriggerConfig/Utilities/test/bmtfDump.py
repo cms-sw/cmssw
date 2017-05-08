2f7ff87e4672a78072fef44d003fa7fc2939cd3c
kkotov <kkotov@cern.ch>
2017-05-08 12:51:12 +0200

    Dumping BMTF xml configs

--- L1TriggerConfig/Utilities/test/bmtfDump.py
+++ L1TriggerConfig/Utilities/test/bmtfDump.py
@@ -25,8 +25,13 @@ myre = re.compile(r'(ID)|(-{80})')
 # if no arguments are given, query the top level keys only and exit
 if len(sys.argv) == 1:
     sqlplus = subprocess.Popen(sqlplusCmd, shell=False, stdout=subprocess.PIPE, stdin=subprocess.PIPE)
-    print 'No args specified, querying and printing only top-level keys:'
-    for line in re.split('\n',sqlplus.communicate('select unique ID from CMS_TRG_L1_CONF.EMTF_KEYS;')[0]):
+    print 'No args specified, querying and printing only top-level config keys:'
+    for line in re.split('\n',sqlplus.communicate('select unique ID from CMS_TRG_L1_CONF.BMTF_KEYS;')[0]):
+        if myre.search(line) == None :
+            print line
+    sqlplus = subprocess.Popen(sqlplusCmd, shell=False, stdout=subprocess.PIPE, stdin=subprocess.PIPE)
+    print 'No args specified, querying and printing only top-level Run Settings keys:'
+    for line in re.split('\n',sqlplus.communicate('select unique ID from CMS_TRG_L1_CONF.BMTF_RS_KEYS;')[0]):
         if myre.search(line) == None :
             print line
     print 'Pick any of these keys as an argument next time you run this script'
@@ -34,46 +39,40 @@ if len(sys.argv) == 1:
 
 # if an argument is given query the whole content of the key
 key = sys.argv[1]
+rsKey = sys.argv[2]
 
-sqlplus = subprocess.Popen(sqlplusCmd,
-                           shell=False,
-                           stdout=subprocess.PIPE,
-                           stdin=subprocess.PIPE
-                          )
+# if the argument is the very top level key, querry the BMTF specific subkey
+queryKey   = "select BMTF_KEY    from CMS_TRG_L1_CONF.L1_TRG_CONF_KEYS where ID='{0}'".format(key)
+queryRsKey = "select BMTF_RS_KEY from CMS_TRG_L1_CONF.L1_TRG_RS_KEYS   where ID='{0}'".format(rsKey)
 
-# if the argument is the very top level key, querry the EMTF specific subkey
-queryKey = "select EMTF_KEY from CMS_TRG_L1_CONF.L1_TRG_CONF_KEYS where ID='{0}'".format(key)
-
+sqlplus = subprocess.Popen(sqlplusCmd, shell=False, stdout=subprocess.PIPE, stdin=subprocess.PIPE)
 for line in re.split('\n',sqlplus.communicate(queryKey+';')[0]):
     print line
     if re.search('/v',line) :
         key=line
 
-print key
+sqlplus = subprocess.Popen(sqlplusCmd, shell=False, stdout=subprocess.PIPE, stdin=subprocess.PIPE)
+for line in re.split('\n',sqlplus.communicate(queryRsKey+';')[0]):
+    print line
+    if re.search('/v',line) :
+        rsKey=line
+
+print key+":"+rsKey
 
 queryKeys = """
-        select
-            INFRA_KEYS.MTF7 as MTF7, INFRA_KEYS.AMC13 as AMC13, TOP_KEYS.HW as HW, TOP_KEYS.ALGO as ALGO
-        from
-            CMS_TRG_L1_CONF.EMTF_INFRA_KEYS INFRA_KEYS
-            inner join
-            (
             select
-                HW, INFRA, ALGO
+                CONF.HW as HW, CONF.ALGO as ALGO, RS.MP7 as MP7, RS.AMC13 as AMC13
             from
-                CMS_TRG_L1_CONF.EMTF_KEYS
+                CMS_TRG_L1_CONF.BMTF_KEYS CONF, CMS_TRG_L1_CONF.BMTF_RS_KEYS RS
             where
-                ID = '{0}'
-            ) TOP_KEYS
-            on 
-                TOP_KEYS.INFRA = INFRA_KEYS.ID
-""".format(key)
+                CONF.ID = '{0}' and RS.ID = '{1}'
+            """.format(key,rsKey)
 
 # write results for specific configs to the following files
 batch = {
          'HW'     : 'hw.xml',
          'ALGO'   : 'algo.xml',
-         'MTF7'   : 'mtf7.xml',
+         'MP7'    : 'mp7.xml',
          'AMC13'  : 'amc13.xml'
         }
 
@@ -86,7 +85,7 @@ for config,fileName in batch.iteritems():
                 select
                     CLOBS.CONF
                 from
-                    CMS_TRG_L1_CONF.EMTF_CLOBS CLOBS, ({0}) KEY
+                    CMS_TRG_L1_CONF.BMTF_CLOBS CLOBS, ({0}) KEY
                 where
                     CLOBS.ID = KEY.{1}
                 """.format(queryKeys, config)
@@ -96,11 +95,11 @@ for config,fileName in batch.iteritems():
             f.write(line)
         f.close()
 
+
 sqlplus = subprocess.Popen(sqlplusCmd, shell=False, stdout=subprocess.PIPE, stdin=subprocess.PIPE)
 print 'Following keys were found:'
 for line in re.split('\n',sqlplus.communicate(queryKeys+';')[0]):
     print line
-
 
 print 'Results are saved in ' + ' '.join(batch.values()) + ' files'
 
