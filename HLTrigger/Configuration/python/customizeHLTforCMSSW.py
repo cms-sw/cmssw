1d56996e6c079fa50a9d184f50b2f605c911e22a
Afiq <afiq.anuar@cern.ch>
2017-05-09 11:32:22 +0200

    Removing dupe customization v2

--- HLTrigger/Configuration/python/customizeHLTforCMSSW.py
+++ HLTrigger/Configuration/python/customizeHLTforCMSSW.py
@@ -151,31 +151,6 @@ def customiseFor18330(process):
                     pset.chi2cutoff = cms.double(3.0)
      return process
 
-
-# Add new parameters required by RecoTauBuilderConePlugin
-def customiseFor18429(process):
-     for producer in producers_by_type(process, "RecoTauProducer"):
-          if hasattr(producer,'builders'):
-               for pset in producer.builders:
-                    if not hasattr(pset,'minAbsPhotonSumPt_insideSignalCone'):
-                         pset.minAbsPhotonSumPt_insideSignalCone = cms.double(2.5)
-                    if not hasattr(pset,'minRelPhotonSumPt_insideSignalCone'):
-                         pset.minRelPhotonSumPt_insideSignalCone = cms.double(0.10)
-
-     return process
-
-# Add new parameters required by RecoTauBuilderConePlugin
-def customiseFor18429(process):
-     for producer in producers_by_type(process, "RecoTauProducer"):
-          if hasattr(producer,'builders'):
-               for pset in producer.builders:
-                    if not hasattr(pset,'minAbsPhotonSumPt_insideSignalCone'):
-                         pset.minAbsPhotonSumPt_insideSignalCone = cms.double(2.5)
-                    if not hasattr(pset,'minRelPhotonSumPt_insideSignalCone'):
-                         pset.minRelPhotonSumPt_insideSignalCone = cms.double(0.10)
-
-     return process
-
 # CMSSW version specific customizations
 def customizeHLTforCMSSW(process, menuType="GRun"):
     # add call to action function in proper order: newest last!
