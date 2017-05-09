23a7084927d9a47f2847ad0c0c12758fab9a54a7
Kevin Pedro <kpedro88@gmail.com>
2017-05-09 09:48:52 -0500

    add fastsim digi aliases for HCAL upgrades

--- FastSimulation/Configuration/python/DigiAliases_cff.py
+++ FastSimulation/Configuration/python/DigiAliases_cff.py
@@ -86,7 +86,9 @@ def loadDigiAliases(premix=False):
                cms.VPSet(
                 cms.PSet(type = cms.string("HBHEDataFramesSorted")),
                 cms.PSet(type = cms.string("HFDataFramesSorted")),
-                cms.PSet(type = cms.string("HODataFramesSorted"))
+                cms.PSet(type = cms.string("HODataFramesSorted")),
+                cms.PSet(type = cms.string('QIE10DataFrameHcalDataFrameContainer')),
+                cms.PSet(type = cms.string('QIE11DataFrameHcalDataFrameContainer'))
                 )
            }
           )
