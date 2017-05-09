03d72da5ec9d3d4e36722effb01aef3302980f81
piet verwilligen <piet.verwilligen@cern.ch>
2017-05-09 23:06:20 +0200

    GEM Slice Test Geometry + related Validation Package Changes

--- Geometry/GEMGeometryBuilder/src/GEMGeometryBuilderFromCondDB.h
+++ Geometry/GEMGeometryBuilder/src/GEMGeometryBuilderFromCondDB.h
@@ -2,34 +2,44 @@
 #define Geometry_GEMGeometry_GEMGeometryBuilderFromCondDB_H
 
 /** \class  GEMGeometryBuilderFromCondDB
- *  Build the GEMGeometry from the DDD description stored in Condition DB 
+ *  Build the GEMGeometry from the RecoIdealGeometry description stored in Condition DB 
  *
  *  \author M. Maggi - INFN Bari
  *
  */
 
-#include <CondFormats/GeometryObjects/interface/RecoIdealGeometry.h>
+#include "CondFormats/GeometryObjects/interface/RecoIdealGeometry.h"
+#include "DataFormats/GeometrySurface/interface/Plane.h"
 #include <string>
 #include <map>
 #include <list>
 
-
 class GEMGeometry;
 class GEMDetId;
 class GEMEtaPartition;
+class GEMSuperChamber;
+class GEMChamber;
 
 class GEMGeometryBuilderFromCondDB 
-{ 
+{
  public:
 
   GEMGeometryBuilderFromCondDB();
 
   ~GEMGeometryBuilderFromCondDB();
-
+  
   GEMGeometry* build(const RecoIdealGeometry& rgeo);
-
+  
  private:
-  //  std::map<GEMDetId,std::list<GEMEtaPartition *> > chids;
+  typedef ReferenceCountingPointer<Plane> RCPPlane;
+  
+  GEMSuperChamber* buildSuperChamber( const GEMDetId id, const RecoIdealGeometry& rig,
+				      size_t idt ) const;
+  
+  GEMChamber* buildChamber( const GEMDetId id,  const RecoIdealGeometry& rig,
+			    size_t idt ) const;
+  
+  std::map<GEMDetId, std::list<GEMEtaPartition *> > chids;
 };
 
 #endif
