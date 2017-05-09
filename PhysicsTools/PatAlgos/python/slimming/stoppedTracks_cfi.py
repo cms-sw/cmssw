f52421fe0c865f4b1433e520cedb53fa22c57f6e
Bennett Marsh <bmarsh@physics.ucsb.edu>
2017-05-09 11:26:45 -0700

    updating w/ PFIsolation

--- PhysicsTools/PatAlgos/python/slimming/stoppedTracks_cfi.py
+++ PhysicsTools/PatAlgos/python/slimming/stoppedTracks_cfi.py
@@ -9,7 +9,13 @@ stoppedTracks = cms.EDProducer("PATStoppedTrackProducer",
     pT_cut = cms.double(20.0),
     dR_cut = cms.double(0.3),
     dZ_cut = cms.double(0.1),
+    miniIsoParams = cms.vdouble(0.05, 0.2, 10.0), # (minDR, maxDR, kT)
+                                                  # dR for miniiso is max(minDR, min(maxDR, kT/pT))
     absIso_cut = cms.double(5.0),
     relIso_cut = cms.double(0.2),
     miniRelIso_cut = cms.double(0.2),
+
+    # absIso_cut = cms.double(999999.0),
+    # relIso_cut = cms.double(999999.0),
+    # miniRelIso_cut = cms.double(999999.0),
 )
