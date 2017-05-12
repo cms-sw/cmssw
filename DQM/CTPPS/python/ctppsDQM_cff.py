3501c78200535d866e53e5ccc95dac26a77cbe0f
Jan Kašpar <jan.kaspar@cern.ch>
2017-05-12 12:05:53 +0200

    Returning diamond DQM modules.

--- DQM/CTPPS/python/ctppsDQM_cff.py
+++ DQM/CTPPS/python/ctppsDQM_cff.py
@@ -5,10 +5,15 @@ from DQM.CTPPS.totemDAQTriggerDQMSource_cfi import *
 from DQM.CTPPS.totemRPDQMHarvester_cfi import *
 from DQM.CTPPS.totemRPDQMSource_cfi import *
 
+from DQM.CTPPS.ctppsDiamondDQMSource_cfi import *
+
 from DQM.CTPPS.ctppsPixelDQMSource_cfi import *
 
 ctppsDQM = cms.Sequence(
-  totemDAQTriggerDQMSource
- *(totemRPDQMSource + ctppsPixelDQMSource)
- *totemRPDQMHarvester
+    totemDAQTriggerDQMSource *
+    totemRPDQMSource *
+    ctppsDiamondDQMSource *
+    ctppsPixelDQMSource *
+
+    totemRPDQMHarvester
 )
