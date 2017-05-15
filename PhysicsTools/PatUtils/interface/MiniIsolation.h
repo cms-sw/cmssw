97aaac602399c4262442fd219bf1f6209f46fc8c
Bennett Marsh <bmarsh@physics.ucsb.edu>
2017-05-14 21:20:47 -0700

    fixes for David's comments

--- PhysicsTools/PatUtils/interface/MiniIsolation.h
+++ PhysicsTools/PatUtils/interface/MiniIsolation.h
@@ -8,19 +8,18 @@
   of packed PF candidates
 
   Mini-Isolation reference: https://hypernews.cern.ch/HyperNews/CMS/get/susy/1991.html
-
+    typedef ROOT::Math::LorentzVector<ROOT::Math::PxPyPzE4D<double> > LorentzVector;
 */
 
 #include "DataFormats/Candidate/interface/Candidate.h"
 #include "DataFormats/PatCandidates/interface/PackedCandidate.h"
 #include "DataFormats/PatCandidates/interface/PFIsolation.h"
+#include "DataFormats/Math/interface/LorentzVector.h"
 
 namespace pat{
 
-    typedef ROOT::Math::LorentzVector<ROOT::Math::PxPyPzE4D<double> > LorentzVector;
-
     // see src file for definitions of parameters
-    PFIsolation getMiniPFIsolation(const pat::PackedCandidateCollection *pfcands, const LorentzVector& p4,
+    PFIsolation getMiniPFIsolation(const pat::PackedCandidateCollection *pfcands, const math::XYZTLorentzVector& p4,
                                    float mindr=0.05, float maxdr=0.2, float kt_scale=10.0,
                                    float ptthresh=0.5, float deadcone_ch=0.0001,
                                    float deadcone_pu=0.01, float deadcone_ph=0.01, float deadcone_nh=0.01,
