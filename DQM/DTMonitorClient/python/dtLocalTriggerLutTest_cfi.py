b37e27d66128be0c422983ca4d5a9fd7a6d177c2
Patrick Gartung <gartung@fnal.gov>
2017-05-15 13:48:04 -0500

    Missed some mistakes in editing.

--- DQM/DTMonitorClient/python/dtLocalTriggerLutTest_cfi.py
+++ DQM/DTMonitorClient/python/dtLocalTriggerLutTest_cfi.py
@@ -1,4 +1,5 @@
 import FWCore.ParameterSet.Config as cms
+from DQMServices.Core.DQMEDHarvester import DQMEDHarvester
 
 triggerLutTest = DQMEDHarvester("DTLocalTriggerLutTest",
     # prescale factor (in luminosity blocks) to perform client analysis
--- DQMOffline/CalibCalo/python/PostProcessorHcalIsoTrack_cfi.py
+++ DQMOffline/CalibCalo/python/PostProcessorHcalIsoTrack_cfi.py
@@ -1,6 +1,6 @@
 import FWCore.ParameterSet.Config as cms
 from DQMServices.Core.DQMEDHarvester import DQMEDHarvester
 
-PostProcessorHcalIsoTrack = cms.DQMDQMEDProducer("DQMHcalIsoTrackPostProcessor",
+PostProcessorHcalIsoTrack = cms.DQMEDHarvester("DQMHcalIsoTrackPostProcessor",
      subDir = cms.untracked.string("AlCaReco/HcalIsoTrack"),
 )
