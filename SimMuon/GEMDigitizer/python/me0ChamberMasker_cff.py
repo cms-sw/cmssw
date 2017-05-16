810e97df85564fdc362104a6b0e8eda0cdc97536
Sven Dildick <sven.dildick@cern.ch>
2017-05-16 18:12:13 -0500

    use new ME0 digi module names

--- SimMuon/GEMDigitizer/python/me0ChamberMasker_cff.py
+++ SimMuon/GEMDigitizer/python/me0ChamberMasker_cff.py
@@ -2,7 +2,7 @@ import FWCore.ParameterSet.Config as cms
 import sys
 
 from SimMuon.GEMDigitizer.me0ChamberMasker_cfi import me0ChamberMasker as _me0ChamberMasker
-from SimMuon.GEMDigitizer.muonME0ReDigis_cfi import simMuonME0ReDigis
+from SimMuon.GEMDigitizer.muonME0PseudoReDigis_cfi import simMuonME0PseudoReDigis
 
 def appendME0ChamberMaskerAtReco(process):
 
@@ -10,16 +10,14 @@ def appendME0ChamberMaskerAtReco(process):
 
         sys.stderr.write("[appendME0ChamberMasker] : Found RawToDigi, appending filter\n")
 
-        process.simMuonME0Digis = _me0ChamberMasker.clone()
-        process.simMuonME0ReDigis = simMuonME0ReDigis.clone()
-        process.simMuonME0Digis.digiTag =  cms.InputTag("simMuonME0Digis", \
+        process.simMuonME0PseudoDigis = _me0ChamberMasker.clone()
+        process.simMuonME0PseudoReDigis = simMuonME0PseudoReDigis.clone()
+        process.simMuonME0PseudoDigis.digiTag =  cms.InputTag("simMuonME0PseudoDigis", \
                                                         processName = cms.InputTag.skipCurrentProcess())
 
-        process.filteredME0DigiSequence = cms.Sequence( process.simMuonME0Digis 
-                                                        + process.simMuonME0ReDigis )
+        process.filteredME0DigiSequence = cms.Sequence( process.simMuonME0PseudoDigis 
+                                                        + process.simMuonME0PseudoReDigis )
 
         process.RawToDigi += process.filteredME0DigiSequence
 
     return process
-
-
