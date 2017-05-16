13631156acd382a76e4f46664de9f84aea3853ee
Vincenzo Innocente <vincenzo.innocente@cern.ch>
2017-05-16 10:01:54 +0200

    allow parsing integers of any size

--- RecoPixelVertexing/PixelTriplets/python/quadrupletseedmerging_cff.py
+++ RecoPixelVertexing/PixelTriplets/python/quadrupletseedmerging_cff.py
@@ -47,7 +47,9 @@ _layerListForPhase2 = ['BPix1+BPix2+BPix3+BPix4',
                        'FPix5_pos+FPix6_pos+FPix7_pos+FPix8_pos', 'FPix5_neg+FPix6_neg+FPix7_neg+FPix8_neg',
 #  removed as redunant and covering effectively only eta>4   (here for documentation, to be optimized after TDR)
 #                       'FPix5_pos+FPix6_pos+FPix7_pos+FPix9_pos', 'FPix5_neg+FPix6_neg+FPix7_neg+FPix9_neg',
-#                       'FPix6_pos+FPix7_pos+FPix8_pos+FPix9_pos', 'FPix6_neg+FPix7_neg+FPix8_neg+FPix9_neg'
+#                       'FPix6_pos+FPix7_pos+FPix8_pos+FPix9_pos', 'FPix6_neg+FPix7_neg+FPix8_neg+FPix9_neg',
+#                       'FPix8_pos+FPix9_pos+FPix10_pos+FPix11_pos', 'FPix8_neg+FPix9_neg+FPix10_neg+FPix11_neg',
+#                        'FPix11_pos'FPix9_pos+FPix10_pos+FPix12_pos', 'FPix9_neg+FPix10_neg+FPix11_neg+FPix12_neg'
 ]
 
 # Needed to have pixelTracks to not to look like depending
