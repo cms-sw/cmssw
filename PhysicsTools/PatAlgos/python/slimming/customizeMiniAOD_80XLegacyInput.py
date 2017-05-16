d147343a22e73fecb7449e0861d3db2dd090c100
mauro verzetti <mverzett@cern.ch>
2017-05-16 09:33:26 +0200

    run only deepCSV and not all the taggers

--- PhysicsTools/PatAlgos/python/slimming/customizeMiniAOD_80XLegacyInput.py
+++ PhysicsTools/PatAlgos/python/slimming/customizeMiniAOD_80XLegacyInput.py
@@ -4,12 +4,21 @@ from pdb  import set_trace
 def customize4BTV(process, verbose=False):
    ' -- Add DeepCSV on miniAOD only -- '
    #set_trace()
-   process.load('RecoBTag.Configuration.RecoBTag_cff')
    if not hasattr(process, 'patJets'):
       raise RuntimeError('The custimization from BTV requires patJets to be loaded in the process')
    
+   process.load('RecoBTag.Combined.deepFlavour_cff')
    #task black magic to force the modules to be run
-   process.pfBTaggingTask.add(process.patJets)
-   process.makePatJetsTask.replace(process.patJets, process.pfBTaggingTask)
+   process.pfDeepFlavourTask.add(process.patJets)
+   if not hasattr(process, 'pfImpactParameterTagInfos'):
+      process.load('RecoBTag.ImpactParameter.pfImpactParameterTagInfos_cfi')
+      process.pfDeepFlavourTask.add(process.pfImpactParameterTagInfos)
+   if not hasattr(process, 'pfSecondaryVertexTagInfos'):
+      process.load('RecoBTag.SecondaryVertex.pfSecondaryVertexTagInfos_cfi')
+      process.pfDeepFlavourTask.add(process.pfSecondaryVertexTagInfos)
+   if not hasattr( process, 'pfInclusiveSecondaryVertexFinderTagInfos'):
+      process.load('RecoBTag.SecondaryVertex.pfInclusiveSecondaryVertexFinderTagInfos_cfi')
+      process.pfDeepFlavourTask.add(process.pfInclusiveSecondaryVertexFinderTagInfos)
+   process.makePatJetsTask.replace(process.patJets, process.pfDeepFlavourTask)
    
    return process
