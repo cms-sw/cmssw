b23dc5add3931fb9d92b58385ce3a2b87f72a378
Giovanni Franzoni <giovanni.franzoni@cern.ch>
2017-05-17 15:34:19 +0200

    add EcalPedestalsRcd

--- CondFormats/Common/test/ProduceDropBoxMetadata.py
+++ CondFormats/Common/test/ProduceDropBoxMetadata.py
@@ -50,10 +50,14 @@ SiStripApvGainRcdAfterAbortGap_prep_str = encodeJsonInString("SiStripApvGainRcdA
 SiPixelAliRcd_prod_str = encodeJsonInString("SiPixelAliRcd_prod.json")
 SiPixelAliRcd_prep_str = encodeJsonInString("SiPixelAliRcd_prep.json")
 
+#EcalPedestalsRcd
+EcalPedestalsRcd_prod_str = encodeJsonInString("EcalPedestal_prod.json")
+EcalPedestalsRcd_prep_str = encodeJsonInString("EcalPedestal_prep.json")
+
 # given a set of .json files in the current dir, ProduceDropBoxMetadata produces a sqlite containign the payload with the prod/and/prep metadata
 process.mywriter = cms.EDAnalyzer("ProduceDropBoxMetadata",
                                   # set to True if you want to write out a sqlite.db translating the json's into a payload
-                                  write = cms.untracked.bool(False),
+                                  write = cms.untracked.bool(True),
 
                                   # toWrite holds a list of Pset's, one for each workflow you want to produce DropBoxMetadata for; you need to have 2 .json files for each PSet
                                   toWrite = cms.VPSet(cms.PSet(record              = cms.untracked.string("BeamSpotObjectsRcdByRun"), 
@@ -94,12 +98,18 @@ process.mywriter = cms.EDAnalyzer("ProduceDropBoxMetadata",
                                                                prodMetaData        = cms.untracked.string(SiStripApvGainRcdAfterAbortGap_prod_str),
                                                                prepMetaData        = cms.untracked.string(SiStripApvGainRcdAfterAbortGap_prep_str),
                                                                ),
+                                                      cms.PSet(record              = cms.untracked.string('EcalPedestalsRcd'),
+                                                               Source              = cms.untracked.string("AlcaHarvesting"),
+                                                               FileClass           = cms.untracked.string("ALCA"),
+                                                               prodMetaData        = cms.untracked.string(EcalPedestalsRcd_prod_str),
+                                                               prepMetaData        = cms.untracked.string(EcalPedestalsRcd_prep_str),
+                                                               ),
                                                       ),
                                   # this boolean will read the content of whichever payload is available and print its content to stoutput
-                                  read = cms.untracked.bool(True),
+                                  read = cms.untracked.bool(False),
 
                                   # toRead lists of record naemes to be sought inside the DropBoxMetadataRcd payload avaialble to the ProduceDropBoxMetadata; for instance, if write is True, you're reading back the metadata you've just entered in the payload from the .json files
-                                  toRead = cms.untracked.vstring('BeamSpotObjectsRcdByRun','BeamSpotObjectsRcdByLumi','SiStripBadStripRcd','SiStripApvGainRcd','TrackerAlignmentRcd','SiStripApvGainRcdAfterAbortGap') # same strings as fType
+                                  toRead = cms.untracked.vstring('BeamSpotObjectsRcdByRun','BeamSpotObjectsRcdByLumi','SiStripBadStripRcd','SiStripApvGainRcd','TrackerAlignmentRcd','EcalPedestalsRcd','SiStripApvGainRcdAfterAbortGap') # same strings as fType
                                   )
 
 process.p = cms.Path(process.mywriter)
@@ -107,7 +117,7 @@ process.p = cms.Path(process.mywriter)
 if process.mywriter.write:
 
     from CondCore.CondDB.CondDB_cfi import CondDB
-    CondDB.connect = "sqlite_file:DropBoxMetadata.db"
+    CondDB.connect = "sqlite_file:DropBoxMetadata_Wed.db"
 
     process.PoolDBOutputService = cms.Service("PoolDBOutputService",
                                               CondDB,
@@ -122,18 +132,18 @@ if process.mywriter.write:
                                               )
     
 process.load('Configuration.StandardSequences.FrontierConditions_GlobalTag_cff')
-process.GlobalTag.globaltag = '80X_dataRun2_Express_Queue'
+process.GlobalTag.globaltag = '91X_dataRun2_Express_Queue'
 
 #process.GlobalTag.connect   = 'frontier://PromptProd/CMS_CONDITIONS'
 #process.GlobalTag.connect   = 'sqlite_file:/data/franzoni/92x/CMSSW_9_2_X_2017-05-16-1100_metadata/src/CondFormats/Common/test/last-iov-DropBoxMetadata_v5.1_express.db'
 
 # Set to True if you want to read a DropBoxMetadata payload from a local sqlite
 # specify the name of the sqlitefile.db and the tag name; the payload loaded will be for run 300000
-readsqlite = True
+readsqlite = False
 if readsqlite:
     process.GlobalTag.toGet = cms.VPSet(
         cms.PSet(record = cms.string("DropBoxMetadataRcd"),
-                 tag = cms.string("DropBoxMetadata_v5.1_express"),
-                 connect = cms.string("sqlite_file:last-iov-DropBoxMetadata_v5.1_express.db")
+                 tag = cms.string("DropBoxMetadata"),
+                 connect = cms.string("sqlite_file:DropBoxMetadata_Wed.db")
                 )
         )
