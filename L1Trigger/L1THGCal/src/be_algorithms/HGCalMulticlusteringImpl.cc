3fd5e78cc2304166430b8edf4da06aa9051be32c
Luca Mastrolorenzo <luca.mastrolorenzo@cern.ch>
2017-05-19 15:17:46 +0200

    added C3d-dedicated global calibration scale factor

--- L1Trigger/L1THGCal/src/be_algorithms/HGCalMulticlusteringImpl.cc
+++ L1Trigger/L1THGCal/src/be_algorithms/HGCalMulticlusteringImpl.cc
@@ -4,10 +4,13 @@
 
 HGCalMulticlusteringImpl::HGCalMulticlusteringImpl( const edm::ParameterSet& conf ) :
     dr_(conf.getParameter<double>("dR_multicluster")),
-    ptC3dThreshold_(conf.getParameter<double>("minPt_multicluster"))
+    ptC3dThreshold_(conf.getParameter<double>("minPt_multicluster")),
+    calibSF_(conf.getParameter<double>("calibSF_multicluster"))
 {    
     edm::LogInfo("HGCalMulticlusterParameters") << "Multicluster dR for Near Neighbour search: " << dr_;  
     edm::LogInfo("HGCalMulticlusterParameters") << "Multicluster minimum transverse-momentum: " << ptC3dThreshold_;
+    edm::LogInfo("HGCalMulticlusterParameters") << "Multicluster minimum transverse-momentum: " << calibSF_;
+
 }
 
 
@@ -67,6 +70,12 @@ void HGCalMulticlusteringImpl::clusterize( const edm::PtrVector<l1t::HGCalCluste
 
     /* making the collection of multiclusters */
     for( unsigned i(0); i<multiclustersTmp.size(); ++i ){
+//        math::PtEtaPhiMLorentzVector calibP4(  multiclustersTmp.at(i).pt() * calibSF_, 
+//                                               multiclustersTmp.at(i).eta(), 
+//                                               multiclustersTmp.at(i).phi(), 
+//                                               multiclustersTmp.at(i).p4().M() );
+//        // overwriting the 4p with the calibrated 4p     
+//        multiclustersTmp.at(i).setP4( calibP4 );
         if( multiclustersTmp.at(i).pt() > ptC3dThreshold_ ){
             multiclusters.push_back( 0, multiclustersTmp.at(i));  
         }
