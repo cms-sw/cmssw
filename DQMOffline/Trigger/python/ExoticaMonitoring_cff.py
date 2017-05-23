fd713b3c07c3b3ea5dc456b1173ffb30849f7593
sudhaahuja <sudha.ahuja@cern.ch>
2017-05-23 06:36:35 +0200

    edited METmonitor with HT plots

--- DQMOffline/Trigger/python/ExoticaMonitoring_cff.py
+++ DQMOffline/Trigger/python/ExoticaMonitoring_cff.py
@@ -1,7 +1,9 @@
 import FWCore.ParameterSet.Config as cms
 
 from DQMOffline.Trigger.METMonitor_cff import *
+from DQMOffline.Trigger.HTMonitor_cff import *
 
 exoticaMonitorHLT = cms.Sequence(
     exoHLTMETmonitoring
+    + exoHLTHTmonitoring
 )
