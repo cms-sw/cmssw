6720367d474bf62b7cc7e5760f56353f9993d0d0
Marco Rovere <marco.rovere@cern.ch>
2017-05-23 13:27:52 +0200

    Use HGCal MultiClusters as input to PF

---
+++ RecoParticleFlow/PFClusterProducer/plugins/PFClusterFromHGCalMultiCluster.h
@@ -0,0 +1,36 @@
+#ifndef __RecoParticleFlow_PFClusterProducer_PFClusterFromHGCalMultiCluster_H__
+#define __RecoParticleFlow_PFClusterProducer_PFClusterFromHGCalMultiCluster_H__
+
+#include "DataFormats/ParticleFlowReco/interface/HGCalMultiCluster.h"
+#include "DataFormats/ParticleFlowReco/interface/PFRecHitFraction.h"
+#include "RecoParticleFlow/PFClusterProducer/interface/InitialClusteringStepBase.h"
+
+class PFClusterFromHGCalMultiCluster : public InitialClusteringStepBase {
+  typedef PFClusterFromHGCalMultiCluster B2DGT;
+
+ public:
+  PFClusterFromHGCalMultiCluster(const edm::ParameterSet& conf,
+                                 edm::ConsumesCollector& sumes)
+      : InitialClusteringStepBase(conf, sumes) {
+    _clusterToken = sumes.consumes<std::vector<reco::HGCalMultiCluster> >(
+        conf.getParameter<edm::InputTag>("clusterSrc"));
+  }
+  virtual ~PFClusterFromHGCalMultiCluster() {}
+  PFClusterFromHGCalMultiCluster(const B2DGT&) = delete;
+  B2DGT& operator=(const B2DGT&) = delete;
+
+  virtual void updateEvent(const edm::Event&) override final;
+
+  void buildClusters(const edm::Handle<reco::PFRecHitCollection>&,
+                     const std::vector<bool>&, const std::vector<bool>&,
+                     reco::PFClusterCollection&) override;
+
+ private:
+  edm::EDGetTokenT<std::vector<reco::HGCalMultiCluster> > _clusterToken;
+  edm::Handle<std::vector<reco::HGCalMultiCluster> > _clusterH;
+};
+
+DEFINE_EDM_PLUGIN(InitialClusteringStepFactory, PFClusterFromHGCalMultiCluster,
+                  "PFClusterFromHGCalMultiCluster");
+
+#endif
--- RecoParticleFlow/PFClusterProducer/python/particleFlowClusterHGC_cfi.py
+++ RecoParticleFlow/PFClusterProducer/python/particleFlowClusterHGC_cfi.py
@@ -26,12 +26,19 @@ _positionCalcPCA_HGCal = cms.PSet(
         minFractionInCalc = cms.double(1e-9)
 )
 
+_hgcalMultiClusterMapper_HGCal = cms.PSet(
+    algoName = cms.string("PFClusterFromHGCalMultiCluster"),
+    thresholdsByDetector = cms.VPSet(
+    ),
+    clusterSrc = cms.InputTag("hgcalLayerClusters")
+)
+
 particleFlowClusterHGCal = cms.EDProducer(
     "PFClusterProducer",
     recHitsSource = cms.InputTag("particleFlowRecHitHGC"),
     recHitCleaners = cms.VPSet(),
     seedFinder = _passThruSeeds_HGCal,
-    initialClusteringStep = _simClusterMapper_HGCal,
+    initialClusteringStep = _hgcalMultiClusterMapper_HGCal,
     pfClusterBuilder = cms.PSet(),
     positionReCalc = _positionCalcPCA_HGCal,
     energyCorrector = cms.PSet()
