727b485caa91c051f2d926e687478a579d49d323
Jean-Baptiste Sauvan <jean-baptiste.sauvan@cern.ch>
2017-05-23 14:46:16 +0200

    Fix configs

--- L1Trigger/L1THGCal/test/testHGCalL1T_RelVal_cfg.py
+++ L1Trigger/L1THGCal/test/testHGCalL1T_RelVal_cfg.py
@@ -54,9 +54,6 @@ process.TFileService = cms.Service(
 from Configuration.AlCa.GlobalTag import GlobalTag
 process.GlobalTag = GlobalTag(process.GlobalTag, 'auto:phase2_realistic', '')
 
-process.SimpleMemoryCheck = cms.Service("SimpleMemoryCheck",
-    ignoreTotal = cms.untracked.int32(1)
-)
 
 # load HGCAL TPG simulation
 process.load('L1Trigger.L1THGCal.hgcalTriggerPrimitives_cff')
