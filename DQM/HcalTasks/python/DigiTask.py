4994b8fbc0d815ba9ade3b8a0dbc30c67aac3e5f
David <david.renhwa.yu@gmail.com>
2017-05-24 19:36:34 +0200

    Add Run2_2016_HIPM to eras for QIE10 skipping

--- DQM/HcalTasks/python/DigiTask.py
+++ DQM/HcalTasks/python/DigiTask.py
@@ -31,6 +31,9 @@ digiTask = cms.EDAnalyzer(
 from Configuration.Eras.Modifier_Run2_2016 import Run2_2016
 Run2_2016.toModify(digiTask, qie10InConditions=cms.untracked.bool(False))
 
+from Configuration.Eras.Modifier_Run2_2016_HIPM import Run2_2016_HIPM
+Run2_2016_HIPM.toModify(digiTask, qie10InConditions=cms.untracked.bool(False))
+
 
 
 
