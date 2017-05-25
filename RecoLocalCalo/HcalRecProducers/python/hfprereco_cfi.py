d0bf6273c6fdca111927a16adba94a944c99c5c5
Kevin Pedro <kpedro88@gmail.com>
2017-05-25 17:44:07 -0500

    more specific input tags

--- RecoLocalCalo/HcalRecProducers/python/hfprereco_cfi.py
+++ RecoLocalCalo/HcalRecProducers/python/hfprereco_cfi.py
@@ -1,7 +1,7 @@
 import FWCore.ParameterSet.Config as cms
 
 hfprereco = cms.EDProducer("HFPreReconstructor",
-    digiLabel = cms.InputTag("hcalDigis"),
+    digiLabel = cms.InputTag("hcalDigis","HFQIE10DigiCollection"),
     dropZSmarkedPassed = cms.bool(True),
     tsFromDB = cms.bool(False),
     sumAllTimeSlices = cms.bool(False)
