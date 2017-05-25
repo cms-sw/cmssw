9ba6d1509f3a81034623b9b890ef5d6b418c6dc0
matteo.defranchis <matteo.defranchis@cern.ch>
2017-05-25 12:57:28 +0200

    UNFINISHED code

--- DQMOffline/Trigger/python/TopMonitoring_Client_cff.py
+++ DQMOffline/Trigger/python/TopMonitoring_Client_cff.py
@@ -1,4 +1,21 @@
 import FWCore.ParameterSet.Config as cms
 
+topEfficiency = cms.EDAnalyzer("DQMGenericClient",
+    subDirs        = cms.untracked.vstring("HLT/TOP/*"),
+    verbose        = cms.untracked.uint32(0), # Set to 2 for all messages
+    resolution     = cms.vstring(),
+    efficiency     = cms.vstring(
+        "effic_muPhi       'mu efficiency vs phi; mu phi [rad]; efficiency' muPhi_numerator       muPhi_denominator",
+        "effic_muEta       'mu efficiency vs eta; mu eta [rad]; efficiency' muEta_numerator       muEta_denominator",
+        "effic_muPt       'mu efficiency vs pt; mu pt [GeV]; efficiency' muPt_numerator       muPt_denominator",
+        "effic_elePhi       'electron efficiency vs phi; electron phi ; efficiency' elePhi_numerator       elePhi_denominator",
+        "effic_eleEta       'electron efficiency vs eta; electron eta ; efficiency' eleEta_numerator       eleEta_denominator",
+        "effic_elePt       'electron efficiency vs pt; electron pt [GeV]; efficiency' elePt_numerator       elePt_denominator",
+        "effic_jetPhi       'jet efficiency vs phi; jet phi ; efficiency' jetPhi_numerator       jetPhi_denominator",
+        "effic_jetEta       'jet efficiency vs eta; jet eta ; efficiency' jetEta_numerator       jetEta_denominator",
+        "effic_jetPt       'jet efficiency vs pt; jet pt [GeV]; efficiency' jetPt_numerator       jetPt_denominator",
+    ),
+)
+
 topClient = cms.Sequence(
 )
--- DQMOffline/Trigger/python/TopMonitoring_cff.py
+++ DQMOffline/Trigger/python/TopMonitoring_cff.py
@@ -1,4 +1,18 @@
 import FWCore.ParameterSet.Config as cms
 
+from DQMOffline.Trigger.TopMonitor_cfi import hltTOPmonitoring
+
+dummy = hltTOPmonitoring.clone()
+dummy.FolderName = cms.string('HLT/TOP/dummy/')
+dummy.nmuons = cms.int32(0)
+dummy.nelectrons = cms.int32(1)
+dummy.njets = cms.int32(1)
+dummy.eleSelection = cms.string('pt>15 & abs(eta)<2.4')
+dummy.jetSelection = cms.string('pt>20 & abs(eta)<2.4')
+dummy.numGenericTriggerEventPSet.hltPaths = cms.vstring('HLT_Ele30_eta2p1_WPTight_Gsf_CentralPFJet35_EleCleaned')
+dummy.denGenericTriggerEventPSet.hltPaths = cms.vstring('HLT_Ele30_eta2p1_WPTight_Gsf_CentralPFJet35_EleCleaned')
+
+
 topMonitorHLT = cms.Sequence(
+    dummy
 )
