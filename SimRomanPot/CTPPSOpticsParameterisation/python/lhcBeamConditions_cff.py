3cb47311b3071bc78746f4309909319802b1bb3a
Laurent Forthomme <laurent.forthomme@cern.ch>
2017-05-25 13:44:05 +0200

    Reverted .gitignore ; repository cleanup

--- SimRomanPot/CTPPSOpticsParameterisation/python/lhcBeamConditions_cff.py
+++ SimRomanPot/CTPPSOpticsParameterisation/python/lhcBeamConditions_cff.py
@@ -6,9 +6,9 @@ lhcBeamConditions_2016PreTS2 = cms.PSet(
 
     # vertex vertical offset in both sectors
     yOffsetSector45 = cms.double(300.e-6), # in m
-    yOffsetSector56 = cms.double(200.e-6),
+    yOffsetSector56 = cms.double(200.e-6), # in m
 
-    # crossing angles
+    # crossing angle
     halfCrossingAngleSector45 = cms.double(179.394e-6), # in rad
     halfCrossingAngleSector56 = cms.double(191.541e-6), # in rad
 )
