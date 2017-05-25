a761e1073cec20180efd81337f9d25d4c47a6254
gomber <bhawna.gomber@cern.ch>
2017-05-25 06:28:41 +0200

    Added prompt monitoring code for HLT Singlephoton paths

--- DQMOffline/Trigger/python/ExoticaMonitoring_Client_cff.py
+++ DQMOffline/Trigger/python/ExoticaMonitoring_Client_cff.py
@@ -16,6 +16,22 @@ metEfficiency = DQMEDHarvester("DQMGenericClient",
   
 )
 
+photonEfficiency = cms.EDAnalyzer("DQMGenericClient",
+    subDirs        = cms.untracked.vstring("HLT/Photon/*"),
+    verbose        = cms.untracked.uint32(0), # Set to 2 for all messages
+    resolution     = cms.vstring(),
+    efficiency     = cms.vstring(
+        "effic_photon         'Photon turnON;            Photon pt [GeV]; efficiency'     photon_pt_numerator          photon_pt_denominator",
+        "effic_photon_variable 'Photon turnON;            Photon pt [GeV]; efficiency'     photon_pt_variable_numerator photon_pt_variable_denominator",
+    ),
+    efficiencyProfile = cms.untracked.vstring(
+        "effic_photon_vs_LS 'Photon pt efficiency vs LS; LS; PF MET efficiency' photonVsLS_numerator photonVsLS_denominator"
+    ),
+  
+)
+
+
 exoticaClient = cms.Sequence(
     metEfficiency
+    + photonEfficiency
 )
