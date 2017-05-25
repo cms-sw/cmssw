b2d707e3bab1e3458ff5e2b28dbdf5e8e7d55e8d
Michael <mplaner@cern.ch>
2017-05-25 19:52:09 +0200

    updated for higgs diphoton HLT dqm

---
+++ DQMOffline/Trigger/python/HigDiphotonHLTOfflineSource_cfi.py
@@ -0,0 +1,21 @@
+import FWCore.ParameterSet.Config as cms
+
+higDiphotonHLTOfflineSource = cms.EDAnalyzer(
+    "HigDiphotonHLTOfflineSource",
+    # Used when fetching triggerSummary and triggerResults
+    hltProcessName = cms.string("HLT"),
+    # HLT paths passing any one of these regular expressions will be included
+    hltPathsToCheck = cms.vstring(
+        "HLT_Diphoton30_22_R9Id_OR_IsoCaloId_AND_HE_R9Id_Mass95_v",
+    ),
+    # Location of plots in DQM
+    dirname = cms.untracked.string("HLT/Higgs/Diphoton"), 
+    verbose = cms.untracked.bool(False), # default: False
+    triggerAccept = cms.untracked.bool(True), # default: True 
+    triggerResultsToken = cms.InputTag("TriggerResults","","HLT"),
+    pvToken = cms.InputTag("offlinePrimaryVertices"),
+    photonsToken = cms.InputTag("gedPhotons"),
+    # cuts
+    photonMinPt = cms.untracked.double(20.0), #GeV
+    
+)
