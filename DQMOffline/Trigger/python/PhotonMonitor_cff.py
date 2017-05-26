4fc84eacc4053e9c22090d4855f5d1dad03e7599
gomber <bhawna.gomber@cern.ch>
2017-05-26 16:23:30 +0200

    Added displaced photon paths

--- DQMOffline/Trigger/python/PhotonMonitor_cff.py
+++ DQMOffline/Trigger/python/PhotonMonitor_cff.py
@@ -14,9 +14,31 @@ SinglePhoton175_monitoring.FolderName = cms.string('HLT/Photon/Photon175/')
 SinglePhoton175_monitoring.numGenericTriggerEventPSet.hltPaths = cms.vstring("HLT_Photon175_v*")
 
 
+Photon60_monitoring = hltPhotonmonitoring.clone()
+Photon60_monitoring.FolderName = cms.string('HLT/Photon/Photon60/')
+hltPhotonmonitoring.denGenericTriggerEventPSet.hltPaths = cms.vstring()
+hltPhotonmonitoring.denGenericTriggerEventPSet.andOrHlt_ = cms.bool(True)
+Photon60_monitoring.numGenericTriggerEventPSet.hltPaths = cms.vstring("HLT_Photon60_R9Id90_CaloIdL_IsoL_v*")
+Photon60_monitoring.photonSelection = cms.string("pt > 20 && r9() < 0.1 && ((eta<1.4442 && hadTowOverEm<0.0597 && full5x5_sigmaIetaIeta()<0.01031 && chargedHadronIso<1.295) || (eta<2.5 && eta>1.566 && hadTowOverEm<0.0481 && full5x5_sigmaIetaIeta()<0.03013 && chargedHadronIso<1.011))")
+
+Photon60_DisplacedIdL_monitoring = Photon60_monitoring.clone()
+Photon60_DisplacedIdL_monitoring.FolderName = cms.string('HLT/Photon/Photon60_DisplacedIdL/')
+Photon60_DisplacedIdL_monitoring.denGenericTriggerEventPSet.hltPaths = cms.vstring("HLT_Photon60_R9Id90_CaloIdL_IsoL_v*")
+Photon60_DisplacedIdL_monitoring.numGenericTriggerEventPSet.hltPaths = cms.vstring("HLT_Photon60_R9Id90_CaloIdL_IsoL_DisplacedIdL_v*")
+
+Photon60_DisplacedIdL_PFJet350MinPFJet15_monitoring = Photon60_DisplacedIdL_monitoring.clone()
+Photon60_DisplacedIdL_PFJet350MinPFJet15_monitoring.FolderName = cms.string('HLT/Photon/Photon60_DisplacedIdL_PFJet350MinPFJet15/')
+Photon60_DisplacedIdL_PFJet350MinPFJet15_monitoring.denGenericTriggerEventPSet.hltPaths = cms.vstring("HLT_Photon60_R9Id90_CaloIdL_IsoL_v*","HLT_PFHT350MinPFJet15_v*")
+Photon60_DisplacedIdL_PFJet350MinPFJet15_monitoring.numGenericTriggerEventPSet.hltPaths = cms.vstring("HLT_Photon60_R9Id90_CaloIdL_IsoL_DisplacedIdL_PFHT350MinPFJet15_v*")
+
+
 exoHLTPhotonmonitoring = cms.Sequence(
     SinglePhoton300_monitoring
     + SinglePhoton175_monitoring
+    + Photon60_monitoring
+    + Photon60_DisplacedIdL_monitoring
+    + Photon60_DisplacedIdL_PFJet350MinPFJet15_monitoring
+    
 )
 
 
