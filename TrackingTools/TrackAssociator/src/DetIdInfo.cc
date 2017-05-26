ed4b4bba2b93636c55bcb94e80d65c2a10c73ac6
Pieter David <pieter.david@gmail.com>
2017-05-26 15:52:07 +0200

    Remove unnecessary T**DetId includes

--- TrackingTools/TrackAssociator/src/DetIdInfo.cc
+++ TrackingTools/TrackAssociator/src/DetIdInfo.cc
@@ -16,10 +16,6 @@
 #include "DataFormats/SiStripDetId/interface/StripSubdetector.h"
 #include "DataFormats/SiPixelDetId/interface/PXBDetId.h"
 #include "DataFormats/SiPixelDetId/interface/PXFDetId.h"
-#include "DataFormats/SiStripDetId/interface/TIBDetId.h"
-#include "DataFormats/SiStripDetId/interface/TOBDetId.h"
-#include "DataFormats/SiStripDetId/interface/TIDDetId.h"
-#include "DataFormats/SiStripDetId/interface/TECDetId.h"
 
 #include "DataFormats/CaloTowers/interface/CaloTowerDetId.h"
 
