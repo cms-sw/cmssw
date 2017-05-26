eddc9891b1703f3f41d5c9193f40b0d7c1802664
gomber <bhawna.gomber@cern.ch>
2017-05-26 08:15:15 +0200

    Added 2016 HLT pahts for jets and photon to make it run

--- DQMOffline/Trigger/python/PhotonMonitor_cfi.py
+++ DQMOffline/Trigger/python/PhotonMonitor_cfi.py
@@ -18,14 +18,14 @@ hltPhotonmonitoring.numGenericTriggerEventPSet.andOr         = cms.bool( False )
 #hltPhotonmonitoring.numGenericTriggerEventPSet.dbLabel       = cms.string("ExoDQMTrigger") # it does not exist yet, we should consider the possibility of using the DB, but as it is now it will need a label per path !
 hltPhotonmonitoring.numGenericTriggerEventPSet.andOrHlt      = cms.bool(True)# True:=OR; False:=AND
 hltPhotonmonitoring.numGenericTriggerEventPSet.hltInputTag   = cms.InputTag( "TriggerResults::HLT" )
-hltPhotonmonitoring.numGenericTriggerEventPSet.hltPaths      = cms.vstring("HLT_Photon200_v*") # HLT_ZeroBias_v*
+hltPhotonmonitoring.numGenericTriggerEventPSet.hltPaths      = cms.vstring("HLT_Photon175_v*") # HLT_ZeroBias_v*
 #hltPhotonmonitoring.numGenericTriggerEventPSet.hltDBKey      = cms.string("EXO_HLT_MET")
 hltPhotonmonitoring.numGenericTriggerEventPSet.errorReplyHlt = cms.bool( False )
 hltPhotonmonitoring.numGenericTriggerEventPSet.verbosityLevel = cms.uint32(1)
 
 hltPhotonmonitoring.denGenericTriggerEventPSet.andOr         = cms.bool( True )
 hltPhotonmonitoring.numGenericTriggerEventPSet.hltInputTag   = cms.InputTag( "TriggerResults::HLT" )
-hltPhotonmonitoring.numGenericTriggerEventPSet.hltPaths      = cms.vstring("HLT_PFJetFwd40_v*,HLT_PFJetFwd60_v*,HLT_PFJetFwd80__v*") # HLT_ZeroBias_v*
+hltPhotonmonitoring.numGenericTriggerEventPSet.hltPaths      = cms.vstring("HLT_PFJet40_v*,HLT_PFJet60_v*,HLT_PFJet80__v*") # HLT_ZeroBias_v*
 hltPhotonmonitoring.numGenericTriggerEventPSet.errorReplyHlt = cms.bool( False )
 hltPhotonmonitoring.denGenericTriggerEventPSet.dcsInputTag   = cms.InputTag( "scalersRawToDigi" )
 hltPhotonmonitoring.denGenericTriggerEventPSet.dcsPartitions = cms.vint32 ( 24, 25, 26, 27, 28, 29 ) # 24-27: strip, 28-29: pixel, we should add all other detectors !
