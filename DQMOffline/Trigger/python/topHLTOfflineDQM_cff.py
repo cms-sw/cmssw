78a763e297ab757dd87249c75e4d7a873247d24c
matteo.defranchis <matteo.defranchis@cern.ch>
2017-05-26 14:36:57 +0200

    commenting out topEleJetsHLTOfflineDQM

--- DQMOffline/Trigger/python/topHLTOfflineDQM_cff.py
+++ DQMOffline/Trigger/python/topHLTOfflineDQM_cff.py
@@ -14,6 +14,6 @@ topHLTriggerOfflineDQM = cms.Sequence(
         *topSingleElectronHLTOfflineDQM
         *SingleTopSingleMuonHLTOfflineDQM
         *SingleTopSingleElectronHLTOfflineDQM	
-        *topEleJetsHLTOfflineDQM
+        # *topEleJetsHLTOfflineDQM
         )
 
