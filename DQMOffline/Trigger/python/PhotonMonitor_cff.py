fbb29a6b4095fa4117fc3bc2ce330367abab5eff
gomber <bhawna.gomber@cern.ch>
2017-05-26 08:15:09 +0200

    Added 2016 HLT pahts for jets and photon to make it run

--- DQMOffline/Trigger/python/PhotonMonitor_cff.py
+++ DQMOffline/Trigger/python/PhotonMonitor_cff.py
@@ -2,10 +2,10 @@ import FWCore.ParameterSet.Config as cms
 
 from DQMOffline.Trigger.PhotonMonitor_cfi import hltPhotonmonitoring
 
-# HLT_SinglePhoton200_IDTight
-SinglePhoton200_monitoring = hltPhotonmonitoring.clone()
-SinglePhoton200_monitoring.FolderName = cms.string('HLT/Photon/Photon200/')
-SinglePhoton200_monitoring.numGenericTriggerEventPSet.hltPaths = cms.vstring("HLT_Photon200_v*")
+#HLT_SinglePhoton200_IDTight
+SinglePhoton300_monitoring = hltPhotonmonitoring.clone()
+SinglePhoton300_monitoring.FolderName = cms.string('HLT/Photon/Photon300/')
+SinglePhoton300_monitoring.numGenericTriggerEventPSet.hltPaths = cms.vstring("HLT_Photon300_NoHE_v")
 
 
 # HLT_SinglePhoton175_IDTight
@@ -15,7 +15,11 @@ SinglePhoton175_monitoring.numGenericTriggerEventPSet.hltPaths = cms.vstring("HL
 
 
 exoHLTPhotonmonitoring = cms.Sequence(
-    SinglePhoton200_monitoring
+    SinglePhoton300_monitoring
     + SinglePhoton175_monitoring
 )
+
+
+
+
 
