3aabcaf9a8f456d86125fa87d704a3ff79ebcedc
matteo.defranchis <matteo.defranchis@cern.ch>
2017-05-26 20:05:44 +0200

    monitor for ele+jets and ele+HT paths

--- DQMOffline/Trigger/python/TopMonitoring_cff.py
+++ DQMOffline/Trigger/python/TopMonitoring_cff.py
@@ -2,37 +2,83 @@ import FWCore.ParameterSet.Config as cms
 
 from DQMOffline.Trigger.TopMonitor_cfi import hltTOPmonitoring
 
-dummy = hltTOPmonitoring.clone()
-dummy.FolderName = cms.string('HLT/TopHLTOffline/TopMonitor/dummy/')
-dummy.nmuons = cms.uint32(0)
-dummy.nelectrons = cms.uint32(1)
-dummy.njets = cms.uint32(2)
-dummy.eleSelection = cms.string('pt>15 & abs(eta)<2.4')
-dummy.jetSelection = cms.string('pt>20 & abs(eta)<2.4')
-dummy.numGenericTriggerEventPSet.hltPaths = cms.vstring('HLT_Ele30_eta2p1_WPTight_Gsf_CentralPFJet35_EleCleaned')
-dummy.denGenericTriggerEventPSet.hltPaths = cms.vstring('HLT_Ele30_eta2p1_WPTight_Gsf_CentralPFJet35_EleCleaned')
+eleJet_jet = hltTOPmonitoring.clone()
+eleJet_jet.FolderName = cms.string('HLT/TopHLTOffline/TopMonitor/EleJet/JetMonitor')
+eleJet_jet.nmuons = cms.uint32(0)
+eleJet_jet.nelectrons = cms.uint32(1)
+eleJet_jet.njets = cms.uint32(2)
+eleJet_jet.eleSelection = cms.string('pt>50 & abs(eta)<2.1')
+eleJet_jet.jetSelection = cms.string('pt>30 & abs(eta)<2.4')
+eleJet_jet.numGenericTriggerEventPSet.hltPaths = cms.vstring('HLT_Ele30_eta2p1_WPTight_Gsf_CentralPFJet35_EleCleaned_v*')
+eleJet_jet.denGenericTriggerEventPSet.hltPaths = cms.vstring('HLT_Ele30_eta2p1_WPTight_Gsf_v*',
+                                                             'HLT_Ele35_WPTight_Gsf_v*',
+                                                             'HLT_Ele38_WPTight_Gsf_v*',
+                                                             'HLT_Ele40_WPTight_Gsf_v*',)
 
-test = hltTOPmonitoring.clone()
-test.FolderName = cms.string('HLT/TopHLTOffline/TopMonitor/test/')
-test.nmuons = cms.uint32(0)
-test.nelectrons = cms.uint32(1)
-test.njets = cms.uint32(2)
-test.eleSelection = cms.string('pt>15 & abs(eta)<2.4')
-test.jetSelection = cms.string('pt>20 & abs(eta)<2.4')
-test.numGenericTriggerEventPSet.hltPaths = cms.vstring('HLT_Ele30_eta2p1_WPTight_Gsf_CentralPFJet35_EleCleaned')
-test.denGenericTriggerEventPSet.hltPaths = cms.vstring('HLT_Ele30_eta2p1_WPTight_Gsf_v*')
+eleJet_ele = hltTOPmonitoring.clone()
+eleJet_ele.FolderName = cms.string('HLT/TopHLTOffline/TopMonitor/EleJet/ElectronMonitor')
+eleJet_ele.nmuons = cms.uint32(0)
+eleJet_ele.nelectrons = cms.uint32(1)
+eleJet_ele.njets = cms.uint32(2)
+eleJet_ele.eleSelection = cms.string('pt>25 & abs(eta)<2.1')
+eleJet_ele.jetSelection = cms.string('pt>50 & abs(eta)<2.4')
+eleJet_ele.numGenericTriggerEventPSet.hltPaths = cms.vstring('HLT_Ele30_eta2p1_WPTight_Gsf_CentralPFJet35_EleCleaned_v*')
+eleJet_ele.denGenericTriggerEventPSet.hltPaths = cms.vstring('HLT_PFJet60_v*')
 
-test2 = hltTOPmonitoring.clone()
-test2.FolderName = cms.string('HLT/TopHLTOffline/TopMonitor/test2/')
-test2.nmuons = cms.uint32(0)
-test2.nelectrons = cms.uint32(1)
-test2.njets = cms.uint32(2)
-test2.eleSelection = cms.string('pt>15 & abs(eta)<2.4')
-test2.jetSelection = cms.string('pt>20 & abs(eta)<2.4')
-test2.numGenericTriggerEventPSet.hltPaths = cms.vstring('HLT_Ele30_eta2p1_WPTight_Gsf_CentralPFJet35_EleCleaned')
+eleJet_all = hltTOPmonitoring.clone()
+eleJet_all.FolderName = cms.string('HLT/TopHLTOffline/TopMonitor/EleJet/GlobalMonitor')
+eleJet_all.nmuons = cms.uint32(0)
+eleJet_all.nelectrons = cms.uint32(1)
+eleJet_all.njets = cms.uint32(2)
+eleJet_all.eleSelection = cms.string('pt>25 & abs(eta)<2.1')
+eleJet_all.jetSelection = cms.string('pt>30 & abs(eta)<2.4')
+eleJet_all.numGenericTriggerEventPSet.hltPaths = cms.vstring('HLT_Ele30_eta2p1_WPTight_Gsf_CentralPFJet35_EleCleaned_v*')
+# eleJet_all.denGenericTriggerEventPSet.hltPaths = cms.vstring('HLT_IsoMu24_v*')
+
+eleHT_ht = hltTOPmonitoring.clone()
+eleHT_ht.FolderName = cms.string('HLT/TopHLTOffline/TopMonitor/EleHT/HTMonitor')
+eleHT_ht.nmuons = cms.uint32(0)
+eleHT_ht.nelectrons = cms.uint32(1)
+eleHT_ht.njets = cms.uint32(2)
+eleHT_ht.eleSelection = cms.string('pt>50 & abs(eta)<2.1')
+eleHT_ht.jetSelection = cms.string('pt>30 & abs(eta)<2.4')
+eleHT_ht.HTcut = cms.double(200)
+eleHT_ht.numGenericTriggerEventPSet.hltPaths = cms.vstring('HLT_Ele28_eta2p1_WPTight_Gsf_HT150_v*')
+eleHT_ht.denGenericTriggerEventPSet.hltPaths = cms.vstring('HLT_Ele30_eta2p1_WPTight_Gsf_v*',
+                                                           'HLT_Ele35_WPTight_Gsf_v*',
+                                                           'HLT_Ele38_WPTight_Gsf_v*',
+                                                           'HLT_Ele40_WPTight_Gsf_v*',)
+
+eleHT_ele = hltTOPmonitoring.clone()
+eleHT_ele.FolderName = cms.string('HLT/TopHLTOffline/TopMonitor/EleHT/ElectronMonitor')
+eleHT_ele.nmuons = cms.uint32(0)
+eleHT_ele.nelectrons = cms.uint32(1)
+eleHT_ele.njets = cms.uint32(2)
+eleHT_ele.eleSelection = cms.string('pt>25 & abs(eta)<2.1')
+eleHT_ele.jetSelection = cms.string('pt>30 & abs(eta)<2.4')
+eleHT_ele.HTcut = cms.double(200)
+eleHT_ele.numGenericTriggerEventPSet.hltPaths = cms.vstring('HLT_Ele28_eta2p1_WPTight_Gsf_HT150_v*')
+eleHT_ele.denGenericTriggerEventPSet.hltPaths = cms.vstring('HLT_HT200_v*',
+                                                            'HLT_HT275_v*',)
+
+eleHT_all = hltTOPmonitoring.clone()
+eleHT_all.FolderName = cms.string('HLT/TopHLTOffline/TopMonitor/EleHT/GlobalMonitor')
+eleHT_all.nmuons = cms.uint32(0)
+eleHT_all.nelectrons = cms.uint32(1)
+eleHT_all.njets = cms.uint32(2)
+eleHT_all.eleSelection = cms.string('pt>25 & abs(eta)<2.1')
+eleHT_all.jetSelection = cms.string('pt>30 & abs(eta)<2.4')
+eleHT_all.numGenericTriggerEventPSet.hltPaths = cms.vstring('HLT_Ele28_eta2p1_WPTight_Gsf_HT150_v*')
+# eleHT_all.denGenericTriggerEventPSet.hltPaths = cms.vstring('HLT_IsoMu24_v*')
+
 
 topMonitorHLT = cms.Sequence(
-    dummy
-    + test
-    + test2
+
+    eleJet_ele
+    + eleJet_jet
+    + eleJet_all
+    + eleHT_ele
+    + eleHT_ht
+    + eleHT_all
+
 )
