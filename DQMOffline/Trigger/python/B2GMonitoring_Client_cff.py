0bf7c6df37810dcf43e0332e740480aac2199092
UBParker <ash.marie.parker@gmail.com>
2017-05-29 03:37:21 -0500

    Adding first B2G HLT path for AK8s

--- DQMOffline/Trigger/python/B2GMonitoring_Client_cff.py
+++ DQMOffline/Trigger/python/B2GMonitoring_Client_cff.py
@@ -1,4 +1,28 @@
 import FWCore.ParameterSet.Config as cms
 
-b2gClient = cms.Sequence(
+
+b2gjetEfficiency = cms.EDAnalyzer("DQMGenericClient",
+    subDirs        = cms.untracked.vstring("HLT/B2GMonitor/*"),
+    verbose        = cms.untracked.uint32(0), # Set to 2 for all messages                                                                                    
+    resolution     = cms.vstring(),
+    efficiency     = cms.vstring(
+        "effic_pfjetpT          'Jet pT turnON;            PFJet(pT) [GeV]; efficiency'     pfjetpT_numerator          pfjetpT_denominator",
+        "effic_pfjetpT_variable 'Jet pT turnON;            PFJet(pT) [GeV]; efficiency'     pfjetpT_variable_numerator pfjetpT_variable_denominator",
+        "effic_pfjetPhi         'Jet efficiency vs #phi; PF Jet #phi [rad]; efficiency'     pfjetPhi_numerator         pfjetPhi_denominator",
+        "effic_pfjetEta         'Jet efficiency vs #eta; PF Jet #eta [rad]; efficiency'     pfjetEta_numerator         pfjetEta_denominator",
+    ),
+
+    efficiencyProfile = cms.untracked.vstring(
+        "effic_pfjetpT_vs_LS 'JET efficiency vs LS; LS; PF JET efficiency' jetpTVsLS_numerator jetpTVsLS_denominator"
+    ),
 )
+
+
+
+b2gClient = cms.Sequence(
+    b2gjetEfficiency
+)
+
+
+
+
--- DQMOffline/Trigger/python/B2GMonitoring_cff.py
+++ DQMOffline/Trigger/python/B2GMonitoring_cff.py
@@ -1,4 +1,18 @@
 import FWCore.ParameterSet.Config as cms
 
+from DQMOffline.Trigger.JetMETMonitor_cfi import hltJetMETmonitoring
+
+AK8PFJet360_TrimMass30_PromptMonitoring = hltJetMETmonitoring.clone()
+AK8PFJet360_TrimMass30_PromptMonitoring.FolderName = cms.string('HLT/B2GMonitor/AK8PFJet360_TrimMass30')
+AK8PFJet360_TrimMass30_PromptMonitoring.ptcut = cms.double(360)
+AK8PFJet360_TrimMass30_PromptMonitoring.numGenericTriggerEventPSet.hltPaths = cms.vstring("HLT_AK8PFJet360_TrimMass30_v*")
+AK8PFJet360_TrimMass30_PromptMonitoring.pfjets      = cms.InputTag("ak8PFJetsCHS")
+
+
 b2gMonitorHLT = cms.Sequence(
+    AK8PFJet360_TrimMass30_PromptMonitoring
 )
+
+
+
+
