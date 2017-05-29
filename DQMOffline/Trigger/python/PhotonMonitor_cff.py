f9f6db44289f077e431ac596efd5cbb0179ee860
Michael <mplaner@cern.ch>
2017-05-29 20:38:41 +0200

    updated mass window for low mass

--- DQMOffline/Trigger/python/PhotonMonitor_cff.py
+++ DQMOffline/Trigger/python/PhotonMonitor_cff.py
@@ -37,7 +37,9 @@ DiphotonMass55AND_monitoring.FolderName = cms.string('HLT/Photon/diphotonMass55A
 DiphotonMass55AND_monitoring.numGenericTriggerEventPSet.hltPaths = cms.vstring("HLT_Diphoton30PV_18PV_R9Id_AND_IsoCaloId_AND_HE_R9Id_PixelVeto_Mass55_v*")
 DiphotonMass55AND_monitoring.nphotons = cms.int32(2)
 DiphotonMass55AND_monitoring.photonSelection = cms.string("(pt > 20 && eta<1.4442 && hadTowOverEm<0.12 && full5x5_sigmaIetaIeta()<0.015 && full5x5_r9>.5)||(pt > 20 && eta<2.5 && eta>1.5556 && hadTowOverEm<0.12 && full5x5_sigmaIetaIeta()<0.035 && full5x5_r9>.8)")
-DiphotonMass55AND_monitoring.massbins = cms.vint32(50.,51.,52.,53.,54.,55.,56.,57.,58.,59.,60.,61.,62.,63.,64.,65.,66.,67.,68.,69.,70.,75.,80.,90.,110.,150.)
+DiphotonMass55AND_monitoring.histoPSet.massBinning = cms.vdouble(50.,51.,52.,53.,54.,55.,56.,57.,58.,59.,60.,61.,62.,63.,64.,65.,66.,67.,68.,69.,70.,75.,80.,90.,110.,150.)
+
+#DiphotonMass55AND_monitoring.massBinning = cms.vdouble(50.,51.,52.,53.,54.,55.,56.,57.,58.,59.,60.,61.,62.,63.,64.,65.,66.,67.,68.,69.,70.,75.,80.,90.,110.,150.)
 
 higgsHLTDiphotonMonitoring = cms.Sequence(
     DiphotonMass90_monitoring
