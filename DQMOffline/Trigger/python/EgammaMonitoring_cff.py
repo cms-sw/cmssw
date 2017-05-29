29263ce9b42de1c077c26b586fc16f3bc066663f
Sam Harper <sam.j.harper@gmail.com>
2017-05-29 14:15:59 +0100

    updating to use VID and generic trigger event

--- DQMOffline/Trigger/python/EgammaMonitoring_cff.py
+++ DQMOffline/Trigger/python/EgammaMonitoring_cff.py
@@ -1,8 +1,8 @@
 import FWCore.ParameterSet.Config as cms
 
-from DQMOffline.Trigger.HLTEGTnPMonitor_cfi import egHLTDQMSelection,egHLTDQMOfflineTnPSource
+from DQMOffline.Trigger.HLTEGTnPMonitor_cfi import egmGsfElectronIDsForDQM,egHLTDQMOfflineTnPSource
 
 egammaMonitorHLT = cms.Sequence(
-    egHLTDQMSelection*
+    egmGsfElectronIDsForDQM*
     egHLTDQMOfflineTnPSource
 )
