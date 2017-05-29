98d406870764c11e17481b62e21a8463834a5650
mplaner <mplaner@cern.ch>
2017-05-29 15:32:41 -0400

    Update HiggsMonitoring_Client_cff.py
    missed a chunk of text in the merge conflict for this file.

--- DQMOffline/Trigger/python/HiggsMonitoring_Client_cff.py
+++ DQMOffline/Trigger/python/HiggsMonitoring_Client_cff.py
@@ -15,19 +15,6 @@ diphotonEfficiency = cms.EDAnalyzer("DQMGenericClient",
         "eff_photon_vs_LS 'Photon pt efficiency vs LS; LS' photonVsLS_numerator photonVsLS_denominator"
     ),
 )
-
-<<<<<<< HEAD
-diphotonEfficiency = cms.EDAnalyzer("DQMGenericClient",
-    subDirs        = cms.untracked.vstring("HLT/Higgs/*"),
-    verbose        = cms.untracked.uint32(0), # Set to 2 for all messages
-    resolution     = cms.vstring(),
-    efficiency     = cms.vstring(
-        "eff_diphoton         'Photon turnON;             Photon pt [GeV]; efficiency'     photon_pt_numerator          photon_pt_denominator",
-        "eff_diphoton_variable 'Photon turnON;            Photon pt [GeV]; efficiency'     photon_pt_variable_numerator photon_pt_variable_denominator",
-    ),
-    efficiencyProfile = cms.untracked.vstring(
-        "eff_photon_vs_LS 'Photon pt efficiency vs LS; LS' photonVsLS_numerator photonVsLS_denominator"
-    )
                                     )
 
 higgsClient = cms.Sequence(
