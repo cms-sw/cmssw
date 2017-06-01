a304c83e334552982c6fa6118326b7bc67bf4c95
Enoch Chen <enochnotsocool@gmail.com>
2017-06-01 15:13:57 +0200

    Tweaked plot ranges for more descriptive axis title and binning

--- DQM/SiPixelPhase1Common/python/HistogramManager_cfi.py
+++ DQM/SiPixelPhase1Common/python/HistogramManager_cfi.py
@@ -28,7 +28,7 @@ SiPixelPhase1Geometry = cms.PSet(
   n_onlineblocks = cms.int32(100),  # #blocks to keep for histograms with history
 
   # lumiblock -  for coarse temporal splitting 
-  lumiblock = cms.int32(5),       # Number of LS to include in a block
+  lumiblock = cms.int32(10),       # Number of LS to include in a block
 
   # other geometry parameters (n_layers, n_ladders per layer, etc.) are inferred.
   # there are lots of geometry assuptions in the code.
