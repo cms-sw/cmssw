9fb7eed01f254dec60d5e873c5d1cf42587c7992
Vincenzo Innocente <vincenzo.innocente@cern.ch>
2017-06-02 15:09:42 +0200

    producer made

---
+++ RecoTracker/Record/src/TkMSParameterizationRecord.cc
@@ -0,0 +1,4 @@
+#include "RecoTracker/Record/interface/TkMSParameterizationRecord.h"
+#include "FWCore/Framework/interface/eventsetuprecord_registration_macro.h"
+
+EVENTSETUP_RECORD_REG(TkMSParameterizationRecord);
