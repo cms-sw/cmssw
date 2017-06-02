3b7bdb53ba439a94f8e98da1df33591748810e1e
Seungkyu Ha <physicist87@gmail.com>
2017-06-02 06:08:48 +0200

    include PromptDQM for HLTJETMET

--- DQMOffline/Trigger/python/DQMOffline_Trigger_cff.py
+++ DQMOffline/Trigger/python/DQMOffline_Trigger_cff.py
@@ -77,6 +77,8 @@ from DQMOffline.Trigger.BTaggingMonitoring_cff import *
 from DQMOffline.Trigger.BPHMonitor_cff import *
 # remove quadJetAna
 from DQMOffline.Trigger.topHLTOfflineDQM_cff import *
+# JETMET
+from DQMOffline.Trigger.JetMETPromptMonitor_cff import *
 offlineHLTSource = cms.Sequence(
     hltResults *
     lumiMonitorHLTsequence *
@@ -101,7 +103,8 @@ offlineHLTSource = cms.Sequence(
     smpMonitorHLT *
     topMonitorHLT *
     btagMonitorHLT *
-    bphMonitorHLT
+    bphMonitorHLT *
+    jetmetMonitorHLT
     )
 
 # offline DQM for the HLTMonitoring stream
