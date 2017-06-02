cf99cd64cb642f4a3ce1eda5e7fc1a4bacf83079
nvoytish <nvoytish@cern.ch>
2017-06-02 16:46:48 +0300

    Update CSCSegmentAlgorithmRU_cfi.py
    unused parameter removed

--- RecoLocalMuon/CSCSegment/python/CSCSegmentAlgorithmRU_cfi.py
+++ RecoLocalMuon/CSCSegment/python/CSCSegmentAlgorithmRU_cfi.py
@@ -8,7 +8,6 @@ RU_ME1A = cms.PSet(
     dPhiIntMax = cms.double(0.005),
     dPhiMax = cms.double(0.006),
     wideSeg = cms.double(3.0),
-    minLayersApart = cms.int32(1),
     dRIntMax = cms.double(2.0),
     dRMax = cms.double(1.5)
 )
@@ -20,7 +19,6 @@ RU_ME1B = cms.PSet(
     dPhiIntMax = cms.double(0.004),
     dPhiMax = cms.double(0.005),
     wideSeg = cms.double(3.0),
-    minLayersApart = cms.int32(1),
     dRIntMax = cms.double(2.0),
     dRMax = cms.double(1.5)
 )
@@ -32,7 +30,6 @@ RU_ME12 = cms.PSet(
     dPhiIntMax = cms.double(0.003),
     dPhiMax = cms.double(0.004),
     wideSeg = cms.double(3.0),
-    minLayersApart = cms.int32(1),
     dRIntMax = cms.double(2.0),
     dRMax = cms.double(1.5)
 )
@@ -44,7 +41,6 @@ RU_ME13 = cms.PSet(
     dPhiIntMax = cms.double(0.002),
     dPhiMax = cms.double(0.003),
     wideSeg = cms.double(3.0),
-    minLayersApart = cms.int32(1),
     dRIntMax = cms.double(2.0),
     dRMax = cms.double(1.5)
 )
@@ -56,7 +52,6 @@ RU_MEX1 = cms.PSet(
     dPhiIntMax = cms.double(0.005),
     dPhiMax = cms.double(0.007),
     wideSeg = cms.double(3.0),
-    minLayersApart = cms.int32(1),
     dRIntMax = cms.double(2.0),
     dRMax = cms.double(1.5)
 )
@@ -68,7 +63,6 @@ RU_MEX2 = cms.PSet(
     dPhiIntMax = cms.double(0.004),
     dPhiMax = cms.double(0.006),
     wideSeg = cms.double(3.0),
-    minLayersApart = cms.int32(1),
     dRIntMax = cms.double(2.0),
     dRMax = cms.double(1.5)
 )
