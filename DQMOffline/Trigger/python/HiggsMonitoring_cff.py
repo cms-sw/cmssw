20a32a21fcb7078ee7627c3c77db8bb5db0affac
Suvankar Roy Chowdhury <suvankar.roy.chowdhury@cern.ch>
2017-06-02 12:32:44 +0200

    higgs hlt dqm 92X

--- DQMOffline/Trigger/python/HiggsMonitoring_cff.py
+++ DQMOffline/Trigger/python/HiggsMonitoring_cff.py
@@ -1,4 +1,141 @@
+
 import FWCore.ParameterSet.Config as cms
 
+from DQMOffline.Trigger.HiggsMonitoring_cfi import hltHIGmonitoring
+
+##############################DiLepton cross triggers######################################################
+mu23TrkIsoVVLEle8CaloIdLTrackIdLIsoVL_muref = hltHIGmonitoring.clone()
+mu23TrkIsoVVLEle8CaloIdLTrackIdLIsoVL_muref.nmuons = cms.uint32(1)
+mu23TrkIsoVVLEle8CaloIdLTrackIdLIsoVL_muref.nelectrons = cms.uint32(1)
+mu23TrkIsoVVLEle8CaloIdLTrackIdLIsoVL_muref.FolderName = cms.string('HLT/Higgs/DiLepton/HLT_Mu23_TrkIsoVVL_Ele8_CaloIdL_TrackIdL_IsoVL_muref/')
+mu23TrkIsoVVLEle8CaloIdLTrackIdLIsoVL_muref.numGenericTriggerEventPSet.hltPaths = cms.vstring("HLT_Mu23_TrkIsoVVL_Ele8_CaloIdL_TrackIdL_IsoVL_v*") 
+mu23TrkIsoVVLEle8CaloIdLTrackIdLIsoVL_muref.denGenericTriggerEventPSet.hltPaths = cms.vstring(
+	"HLT_Mu20_v*","HLT_TkMu20_v*",
+	"HLT_IsoMu24_eta2p1_v*",
+	"HLT_IsoMu24_v*",
+	"HLT_IsoMu27_v*",
+	"HLT_IsoMu20_v*",
+	"HLT_IsoTkMu24_eta2p1_v*",
+	"HLT_IsoTkMu24_v*",
+	"HLT_IsoTkMu27_v*",
+	"HLT_IsoTkMu20_v*"
+)
+
+mu23TrkIsoVVLEle8CaloIdLTrackIdLIsoVL_eleref = hltHIGmonitoring.clone()
+mu23TrkIsoVVLEle8CaloIdLTrackIdLIsoVL_eleref.nmuons = cms.uint32(1)
+mu23TrkIsoVVLEle8CaloIdLTrackIdLIsoVL_eleref.nelectrons = cms.uint32(1)
+mu23TrkIsoVVLEle8CaloIdLTrackIdLIsoVL_eleref.FolderName = cms.string('HLT/Higgs/DiLepton/HLT_Mu23_TrkIsoVVL_Ele8_CaloIdL_TrackIdL_IsoVL_eleref/')
+mu23TrkIsoVVLEle8CaloIdLTrackIdLIsoVL_eleref.numGenericTriggerEventPSet.hltPaths = cms.vstring("HLT_Mu23_TrkIsoVVL_Ele8_CaloIdL_TrackIdL_IsoVL_v*")
+mu23TrkIsoVVLEle8CaloIdLTrackIdLIsoVL_eleref.denGenericTriggerEventPSet.hltPaths = cms.vstring(
+        "HLT_Ele27_WPTight_Gsf_v*",
+	"HLT_Ele35_WPTight_Gsf_v*"
+)
+
+#####HLT_Mu8_TrkIsoVVL_Ele23_CaloIdL_TrackIdL_IsoVL_v#####
+mu8TrkIsoVVLEle23CaloIdLTrackIdLIsoVL_muref = hltHIGmonitoring.clone()
+mu8TrkIsoVVLEle23CaloIdLTrackIdLIsoVL_muref.nmuons = cms.uint32(1)
+mu8TrkIsoVVLEle23CaloIdLTrackIdLIsoVL_muref.nelectrons = cms.uint32(1)
+mu8TrkIsoVVLEle23CaloIdLTrackIdLIsoVL_muref.FolderName = cms.string('HLT/Higgs/DiLepton/HLT_Mu8_TrkIsoVVL_Ele23_CaloIdL_TrackIdL_IsoVL_muref/')
+mu8TrkIsoVVLEle23CaloIdLTrackIdLIsoVL_muref.numGenericTriggerEventPSet.hltPaths = cms.vstring("HLT_Mu8_TrkIsoVVL_Ele23_CaloIdL_TrackIdL_IsoVL_v*") # 
+mu8TrkIsoVVLEle23CaloIdLTrackIdLIsoVL_muref.denGenericTriggerEventPSet.hltPaths = cms.vstring(
+	"HLT_Mu20_v*",
+	"HLT_TkMu20_v*",
+	"HLT_IsoMu24_eta2p1_v*",
+	"HLT_IsoMu24_v*",
+	"HLT_IsoMu27_v*",
+	"HLT_IsoMu20_v*",
+	"HLT_IsoTkMu24_eta2p1_v*",
+	"HLT_IsoTkMu24_v*",
+	"HLT_IsoTkMu27_v*",
+	"HLT_IsoTkMu20_v*"
+)
+
+mu8TrkIsoVVLEle23CaloIdLTrackIdLIsoVL_eleref = hltHIGmonitoring.clone()
+mu8TrkIsoVVLEle23CaloIdLTrackIdLIsoVL_eleref.nmuons = cms.uint32(1)
+mu8TrkIsoVVLEle23CaloIdLTrackIdLIsoVL_eleref.nelectrons = cms.uint32(1)
+mu8TrkIsoVVLEle23CaloIdLTrackIdLIsoVL_eleref.FolderName = cms.string('HLT/Higgs/DiLepton/HLT_Mu8_TrkIsoVVL_Ele23_CaloIdL_TrackIdL_IsoVL_eleref/')
+mu8TrkIsoVVLEle23CaloIdLTrackIdLIsoVL_eleref.numGenericTriggerEventPSet.hltPaths = cms.vstring("HLT_Mu8_TrkIsoVVL_Ele23_CaloIdL_TrackIdL_IsoVL_v*")
+mu8TrkIsoVVLEle23CaloIdLTrackIdLIsoVL_eleref.denGenericTriggerEventPSet.hltPaths = cms.vstring(
+        "HLT_Ele27_WPTight_Gsf_v*",
+	"HLT_Ele35_WPTight_Gsf_v*"
+)
+
+###############################same flavour trilepton monitor####################################
+########TripleMuon########
+higgsTrimumon = hltHIGmonitoring.clone()
+higgsTrimumon.FolderName = cms.string('HLT/Higgs/TriLepton/HLT_TripleMu_12_10_5/')
+higgsTrimumon.nmuons = cms.uint32(3)
+higgsTrimumon.numGenericTriggerEventPSet.hltPaths = cms.vstring("HLT_TripleMu_12_10_5_v*") # 
+higgsTrimumon.denGenericTriggerEventPSet.hltPaths = cms.vstring("HLT_Mu17_TrkIsoVVL_Mu8_TrkIsoVVL_v*","HLT_Mu17_TrkIsoVVL_TkMu8_TrkIsoVVL_v*")
+
+#######TripleElectron####
+higgsTrielemon = hltHIGmonitoring.clone()
+higgsTrielemon.FolderName = cms.string('HLT/Higgs/TriLepton/HLT_Ele16_Ele12_Ele8_CaloIdL_TrackIdL/')
+higgsTrielemon.nelectrons = cms.uint32(3)
+higgsTrielemon.numGenericTriggerEventPSet.hltPaths = cms.vstring("HLT_Ele16_Ele12_Ele8_CaloIdL_TrackIdL_v*") # 
+higgsTrielemon.denGenericTriggerEventPSet.hltPaths = cms.vstring("HLT_Ele23_Ele12_CaloIdL_TrackIdL_IsoVL_DZ_v*")
+
+###############################cross flavour trilepton monitor####################################
+#########DiMuon+Single Ele Trigger###################
+diMu9Ele9CaloIdLTrackIdL_muegref = hltHIGmonitoring.clone()
+diMu9Ele9CaloIdLTrackIdL_muegref.FolderName = cms.string('HLT/Higgs/TriLepton/HLT_DiMu9_Ele9_CaloIdL_TrackIdL_muegRef/')
+diMu9Ele9CaloIdLTrackIdL_muegref.nelectrons = cms.uint32(1)
+diMu9Ele9CaloIdLTrackIdL_muegref.nmuons = cms.uint32(2)
+diMu9Ele9CaloIdLTrackIdL_muegref.numGenericTriggerEventPSet.hltPaths = cms.vstring("HLT_DiMu9_Ele9_CaloIdL_TrackIdL_v*") # HLT_ZeroBias_v*
+diMu9Ele9CaloIdLTrackIdL_muegref.denGenericTriggerEventPSet.hltPaths = cms.vstring(
+	"HLT_Mu23_TrkIsoVVL_Ele8_CaloIdL_TrackIdL_IsoVL_DZ_v*", 
+	"HLT_Mu8_TrkIsoVVL_Ele23_CaloIdL_TrackIdL_IsoVL_DZ_v*",
+	"HLT_Mu23_TrkIsoVVL_Ele8_CaloIdL_TrackIdL_IsoVL_v*", 
+        "HLT_Mu8_TrkIsoVVL_Ele23_CaloIdL_TrackIdL_IsoVL_v*"
+)
+
+diMu9Ele9CaloIdLTrackIdL_dimuref = hltHIGmonitoring.clone()
+diMu9Ele9CaloIdLTrackIdL_dimuref.FolderName = cms.string('HLT/Higgs/TriLepton/HLT_DiMu9_Ele9_CaloIdL_TrackIdL_dimuRef/')
+diMu9Ele9CaloIdLTrackIdL_dimuref.nelectrons = cms.uint32(1)
+diMu9Ele9CaloIdLTrackIdL_dimuref.nmuons = cms.uint32(2)
+diMu9Ele9CaloIdLTrackIdL_dimuref.numGenericTriggerEventPSet.hltPaths = cms.vstring("HLT_DiMu9_Ele9_CaloIdL_TrackIdL_v*") # HLT_ZeroBias_v*
+diMu9Ele9CaloIdLTrackIdL_dimuref.denGenericTriggerEventPSet.hltPaths = cms.vstring(
+	"HLT_Mu17_TrkIsoVVL_Mu8_TrkIsoVVL_v*",
+	"HLT_Mu17_TrkIsoVVL_Mu8_TrkIsoVVL_DZ_v*",
+	"HLT_Mu17_TrkIsoVVL_TkMu8_TrkIsoVVL_v*",
+	"HLT_Mu17_TrkIsoVVL_TkMu8_TrkIsoVVL_DZ_v*",
+	"HLT_TkMu17_TrkIsoVVL_TkMu8_TrkIsoVVL_v*",
+	"HLT_TkMu17_TrkIsoVVL_TkMu8_TrkIsoVVL_DZ_v*"
+)
+
+#################DiElectron+Single Muon Trigger##################
+mu8diEle12CaloIdLTrackIdL_muegref = hltHIGmonitoring.clone()
+mu8diEle12CaloIdLTrackIdL_muegref.FolderName = cms.string('HLT/Higgs/TriLepton/HLT_Mu8_DiEle12_CaloIdL_TrackIdL_muegRef/')
+mu8diEle12CaloIdLTrackIdL_muegref.nelectrons = cms.uint32(2)
+mu8diEle12CaloIdLTrackIdL_muegref.nmuons = cms.uint32(1)
+mu8diEle12CaloIdLTrackIdL_muegref.numGenericTriggerEventPSet.hltPaths = cms.vstring("HLT_Mu8_DiEle12_CaloIdL_TrackIdL_v*") # HLT_ZeroBias_v*
+mu8diEle12CaloIdLTrackIdL_muegref.denGenericTriggerEventPSet.hltPaths = cms.vstring(
+	"HLT_Mu23_TrkIsoVVL_Ele8_CaloIdL_TrackIdL_IsoVL_DZ_v*", 
+	"HLT_Mu8_TrkIsoVVL_Ele23_CaloIdL_TrackIdL_IsoVL_DZ_v*",
+	"HLT_Mu23_TrkIsoVVL_Ele8_CaloIdL_TrackIdL_IsoVL_v*", 
+        "HLT_Mu8_TrkIsoVVL_Ele23_CaloIdL_TrackIdL_IsoVL_v*"
+)
+
+mu8diEle12CaloIdLTrackIdL_dieleref = hltHIGmonitoring.clone()
+mu8diEle12CaloIdLTrackIdL_dieleref.FolderName = cms.string('HLT/Higgs/TriLepton/HLT_Mu8_DiEle12_CaloIdL_TrackIdL_dieleRef/')
+mu8diEle12CaloIdLTrackIdL_dieleref.nelectrons = cms.uint32(2)
+mu8diEle12CaloIdLTrackIdL_dieleref.nmuons = cms.uint32(1)
+mu8diEle12CaloIdLTrackIdL_dieleref.numGenericTriggerEventPSet.hltPaths = cms.vstring("HLT_Mu8_DiEle12_CaloIdL_TrackIdL_v*") # HLT_ZeroBias_v*
+mu8diEle12CaloIdLTrackIdL_dieleref.denGenericTriggerEventPSet.hltPaths = cms.vstring(
+	"HLT_Ele23_Ele12_CaloIdL_TrackIdL_IsoVL_DZ_v*"
+)
+
+
+###############################Higgs Monitor HLT##############################################
 higgsMonitorHLT = cms.Sequence(
+ higgsTrielemon
+ + higgsTrimumon
+ + mu23TrkIsoVVLEle8CaloIdLTrackIdLIsoVL_muref
+ + mu23TrkIsoVVLEle8CaloIdLTrackIdLIsoVL_eleref
+ + mu8TrkIsoVVLEle23CaloIdLTrackIdLIsoVL_muref
+ + mu8TrkIsoVVLEle23CaloIdLTrackIdLIsoVL_eleref
+ + mu8diEle12CaloIdLTrackIdL_muegref
+ + mu8diEle12CaloIdLTrackIdL_dieleref
+ + diMu9Ele9CaloIdLTrackIdL_dimuref
+ + diMu9Ele9CaloIdLTrackIdL_muegref
 )
---
+++ DQMOffline/Trigger/python/HiggsMonitoring_cfi.py
@@ -0,0 +1,96 @@
+import FWCore.ParameterSet.Config as cms
+
+from DQMOffline.Trigger.topMonitoring_cfi import topMonitoring
+
+hltHIGmonitoring = topMonitoring.clone()
+hltHIGmonitoring.FolderName = cms.string('HLT/Higgs/default/')
+hltHIGmonitoring.histoPSet.metPSet = cms.PSet(
+  nbins = cms.int32 (  30   ),
+  xmin  = cms.double(   0   ),
+  xmax  = cms.double(  300  ),
+)
+hltHIGmonitoring.histoPSet.ptPSet = cms.PSet(
+  nbins = cms.int32 (  60   ),
+  xmin  = cms.double(   0   ),
+  xmax  = cms.double(  300  ),
+)
+hltHIGmonitoring.histoPSet.phiPSet = cms.PSet(
+  nbins = cms.int32 (  32  ),
+  xmin  = cms.double( -3.2 ),
+  xmax  = cms.double(  3.2 ),
+)
+hltHIGmonitoring.histoPSet.etaPSet = cms.PSet(
+  nbins = cms.int32 (  30  ),
+  xmin  = cms.double( -3.0 ),
+  xmax  = cms.double(  3.0 ),
+)
+hltHIGmonitoring.histoPSet.htPSet = cms.PSet(
+  nbins = cms.int32 (   60  ),
+  xmin  = cms.double(   0   ),
+  xmax  = cms.double(  600  ),
+)
+
+hltHIGmonitoring.applyleptonPVcuts = cms.bool(True)
+hltHIGmonitoring.leptonPVcuts = cms.PSet(
+  dxy = cms.double(   0.5  ),
+  dz  = cms.double(   1.   ),
+)
+
+
+#MET and HT binning
+hltHIGmonitoring.histoPSet.metBinning = cms.vdouble(0,20,40,60,80,100,125,150,175,200)
+hltHIGmonitoring.histoPSet.HTBinning  = cms.vdouble(0,20,40,60,80,100,125,150,175,200,300,400,500,700)
+#Eta binning
+hltHIGmonitoring.histoPSet.eleEtaBinning = cms.vdouble(-2.4,-2.1,-1.7,-1.2,-0.9,-0.6,-0.3,-0.1,0,0.1,0.3,0.6,0.9,1.2,1.7,2.1,2.4)
+hltHIGmonitoring.histoPSet.jetEtaBinning = cms.vdouble(-2.4,-2.1,-1.7,-1.2,-0.9,-0.6,-0.3,-0.1,0,0.1,0.3,0.6,0.9,1.2,1.7,2.1,2.4)
+hltHIGmonitoring.histoPSet.muEtaBinning  = cms.vdouble(-2.4,-2.1,-1.7,-1.2,-0.9,-0.6,-0.3,-0.1,0,0.1,0.3,0.6,0.9,1.2,1.7,2.1,2.4)
+#pt binning
+hltHIGmonitoring.histoPSet.elePtBinning = cms.vdouble(0,3,5,8,10,15,20,25,30,40,50,60,80,120,200,400)
+hltHIGmonitoring.histoPSet.jetPtBinning = cms.vdouble(0,3,5,8,10,15,20,25,30,40,50,60,80,120,200,400)
+hltHIGmonitoring.histoPSet.muPtBinning  = cms.vdouble(0,3,5,8,10,15,20,25,30,40,50,60,80,120,200,400)
+#Eta binning 2D
+hltHIGmonitoring.histoPSet.eleEtaBinning2D = cms.vdouble(-2.4,-1.7,-1.2,-0.6,-0.3,0,0.3,0.6,1.2,1.7,2.4)
+hltHIGmonitoring.histoPSet.jetEtaBinning2D = cms.vdouble(-2.4,-1.7,-1.2,-0.6,-0.3,0,0.3,0.6,1.2,1.7,2.4)
+hltHIGmonitoring.histoPSet.muEtaBinning2D  = cms.vdouble(-2.4,-1.7,-1.2,-0.6,-0.3,0,0.3,0.6,1.2,1.7,2.4)
+#pt binning 2D
+hltHIGmonitoring.histoPSet.elePtBinning2D = cms.vdouble(0,15,20,30,40,60,80,100,200,400)
+hltHIGmonitoring.histoPSet.jetPtBinning2D = cms.vdouble(0,15,20,30,40,60,80,100,200,400)
+hltHIGmonitoring.histoPSet.muPtBinning2D  = cms.vdouble(0,15,20,30,40,60,80,100,200,400)
+#HT and phi binning 2D
+hltHIGmonitoring.histoPSet.HTBinning2D  = cms.vdouble(0,20,40,70,100,150,200,400,700)
+hltHIGmonitoring.histoPSet.phiBinning2D = cms.vdouble(-3.1416,-2.5132,-1.8849,-1.2566,-0.6283,0,0.6283,1.2566,1.8849,2.5132,3.1416)
+
+
+hltHIGmonitoring.met       = cms.InputTag("pfMetEI") # pfMet
+hltHIGmonitoring.jets      = cms.InputTag("pfJetsEI") # ak4PFJets, ak4PFJetsCHS
+hltHIGmonitoring.electrons = cms.InputTag("gedGsfElectrons") # while pfIsolatedElectronsEI are reco::PFCandidate !
+hltHIGmonitoring.muons     = cms.InputTag("muons") # while pfIsolatedMuonsEI are reco::PFCandidate !
+hltHIGmonitoring.vertices  = cms.InputTag("offlinePrimaryVertices")
+
+hltHIGmonitoring.HTdefinition = cms.string('pt>30 & abs(eta)<2.5')
+hltHIGmonitoring.leptJetDeltaRmin = cms.double(0.4)
+hltHIGmonitoring.eleSelection =  cms.string('pt > 7. && abs(eta) < 2.5') 
+hltHIGmonitoring.muoSelection =  cms.string('pt > 5 &&  abs(eta) < 2.4 && (isGlobalMuon || (isTrackerMuon && numberOfMatches>0)) && muonBestTrackType != 2')
+hltHIGmonitoring.vertexSelection = cms.string('!isFake && ndof > 4 && abs(z) <= 24 && position.Rho <= 2')
+
+hltHIGmonitoring.nmuons     = cms.uint32(0)
+hltHIGmonitoring.nelectrons = cms.uint32(0)
+hltHIGmonitoring.njets      = cms.uint32(0)
+
+
+hltHIGmonitoring.numGenericTriggerEventPSet.andOr         = cms.bool( False )
+hltHIGmonitoring.numGenericTriggerEventPSet.andOrHlt      = cms.bool(True)# True:=OR; False:=AND
+hltHIGmonitoring.numGenericTriggerEventPSet.hltInputTag   = cms.InputTag( "TriggerResults::HLT" )
+hltHIGmonitoring.numGenericTriggerEventPSet.errorReplyHlt = cms.bool( False )
+hltHIGmonitoring.numGenericTriggerEventPSet.verbosityLevel = cms.uint32(1)
+
+hltHIGmonitoring.denGenericTriggerEventPSet.andOr         = cms.bool( False )
+hltHIGmonitoring.numGenericTriggerEventPSet.andOrHlt      = cms.bool(True)# True:=OR; False:=AND
+hltHIGmonitoring.numGenericTriggerEventPSet.hltInputTag   = cms.InputTag( "TriggerResults::HLT" )
+hltHIGmonitoring.numGenericTriggerEventPSet.errorReplyHlt = cms.bool( False )
+hltHIGmonitoring.denGenericTriggerEventPSet.dcsInputTag   = cms.InputTag( "scalersRawToDigi" )
+hltHIGmonitoring.denGenericTriggerEventPSet.dcsPartitions = cms.vint32 ( 24, 25, 26, 27, 28, 29 ) # 24-27: strip, 28-29: pixel, we should add all other detectors !
+hltHIGmonitoring.denGenericTriggerEventPSet.andOrDcs      = cms.bool( False )
+hltHIGmonitoring.denGenericTriggerEventPSet.errorReplyDcs = cms.bool( True )
+hltHIGmonitoring.denGenericTriggerEventPSet.verbosityLevel = cms.uint32(1)
+
