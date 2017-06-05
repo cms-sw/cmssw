9eb090e30198dc7dcf2d12dd7b44047f78e04351
Ianna Osborne <ianna.osborne@cern.ch>
2017-06-05 10:37:17 +0200

    moved headers

--- DetectorDescription/Algorithm/plugins/module.cc
+++ DetectorDescription/Algorithm/plugins/module.cc
@@ -1,8 +1,8 @@
 #include <string>
 
 #include "DetectorDescription/Algorithm/interface/DDAlgorithmFactory.h"
-#include "DetectorDescription/Algorithm/interface/DDAngular.h"
-#include "DetectorDescription/Algorithm/interface/DDLinear.h"
+#include "DDAngular.h"
+#include "DDLinear.h"
 #include "FWCore/PluginManager/interface/PluginFactory.h"
 
 DEFINE_EDM_PLUGIN( DDAlgorithmFactory, DDAngular, "global:DDAngular");
