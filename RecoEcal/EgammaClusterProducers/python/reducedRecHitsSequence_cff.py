c728cedf09e81aad2486e0cb4df3c17e9ed8a73d
Kevin <kpm82@cornell.edu>
2017-06-06 20:01:51 +0200

    simple clones for interesting ES and HCAL recHit producers

--- RecoEcal/EgammaClusterProducers/python/reducedRecHitsSequence_cff.py
+++ RecoEcal/EgammaClusterProducers/python/reducedRecHitsSequence_cff.py
@@ -95,6 +95,7 @@ reducedEcalRecHitsEB = cms.EDProducer("ReducedRecHitCollectionProducer",
             # egamma
             cms.InputTag("interestingGedEleIsoDetIdEB"),
             cms.InputTag("interestingGedGamIsoDetIdEB"),
+            cms.InputTag("interestingOotGamIsoDetIdEB"),
             cms.InputTag("interestingGamIsoDetIdEB"),
             # tau
             #cms.InputTag("caloRecoTauProducer"),
@@ -119,6 +120,7 @@ reducedEcalRecHitsEE = cms.EDProducer("ReducedRecHitCollectionProducer",
             # egamma
             cms.InputTag("interestingGedEleIsoDetIdEE"),
             cms.InputTag("interestingGedGamIsoDetIdEE"),
+            cms.InputTag("interestingOotGamIsoDetIdEE"),
             cms.InputTag("interestingGamIsoDetIdEE"),
             # tau
             #cms.InputTag("caloRecoTauProducer"),
@@ -142,6 +144,7 @@ reducedEcalRecHitsES = cms.EDProducer("ReducedESRecHitCollectionProducer",
                                       ),
                                       interestingDetIdsNotToClean = cms.VInputTag(
                                           cms.InputTag("interestingGedEgammaIsoESDetId"),
+                                          cms.InputTag("interestingOotEgammaIsoESDetId"),
                                       )
 )
 
