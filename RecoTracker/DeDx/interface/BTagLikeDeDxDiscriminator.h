e410bf0ca1918c95c3439b6b68b642718ea9668c
Raphael Isemann <teemperor@gmail.com>
2017-06-07 08:50:30 +0200

    Add missing include for BaseDeDxEstimator.h to RecoTracker/DeDx headers.
    Those headers all have classes who inherit from BaseDeDxEstimator, so
    they also need to include the appropriate header.

--- RecoTracker/DeDx/interface/BTagLikeDeDxDiscriminator.h
+++ RecoTracker/DeDx/interface/BTagLikeDeDxDiscriminator.h
@@ -1,6 +1,7 @@
 #ifndef RecoTrackerDeDx_BTagLikeDeDxDiscriminator_h
 #define RecoTrackerDeDx_BTagLikeDeDxDiscriminator_h
 
+#include "RecoTracker/DeDx/interface/BaseDeDxEstimator.h"
 #include "RecoTracker/DeDx/interface/DeDxTools.h"
 #include "DataFormats/TrackReco/interface/DeDxHit.h"
 
