05f61e51fbf9f23567162e6c276c45cb834680b9
mmusich <marco.musich@cern.ch>
2017-06-08 16:41:11 +0200

    add averages per barrel layer

--- CondCore/SiStripPlugins/plugins/SiStripApvGain_PayloadInspector.cc
+++ CondCore/SiStripPlugins/plugins/SiStripApvGain_PayloadInspector.cc
@@ -19,7 +19,7 @@ namespace {
     
   public:
     SiStripApvGainsValue() : cond::payloadInspector::Histogram1D<SiStripApvGain>("SiStripApv Gains values",
-										 "SiStripApv Gains values", 200,0.,2.){
+										 "SiStripApv Gains values", 200,0.2,1.8){
       Base::setSingleIov( true );
     }
     
@@ -47,13 +47,67 @@ namespace {
   };
 
   /************************************************
+    1d histogram of means of SiStripApvGains
+    for Tracker Barrel of 1 IOV 
+  *************************************************/
+
+  // inherit from one of the predefined plot class: Histogram1D
+  class SiStripApvBarrelGainsByLayer : public cond::payloadInspector::Histogram1D<SiStripApvGain> {
+    
+  public:
+    SiStripApvBarrelGainsByLayer() : cond::payloadInspector::Histogram1D<SiStripApvGain>("SiStripApv Gains averages by Barrel layer",
+											 "SiStripApv Gains averages by Barrel layer",10,0,10){
+      Base::setSingleIov( true );
+    }
+    
+    bool fill( const std::vector<std::tuple<cond::Time_t,cond::Hash> >& iovs ){
+      for ( auto const & iov: iovs) {
+	std::shared_ptr<SiStripApvGain> payload = Base::fetchPayload( std::get<1>(iov) );
+	if( payload.get() ){
+	 
+	  std::vector<uint32_t> detid;
+	  payload->getDetIds(detid);
+	  
+	  std::map<int,std::pair<float,float> > sumOfGainsByLayer;
+
+	  for (size_t id=0;id<detid.size();id++){
+
+	    int layer=-1;
+	    int subid = (id >> 25)&0x7;
+	    if(subid!=3 && subid!=5) continue;
+	    if(subid==3){
+	      layer= int((id >> 14)& 0x7);
+	    } else {
+	      layer+=4;
+	    }
+
+	    SiStripApvGain::Range range=payload->getRange(detid[id]);
+	    for(int it=0;it<range.second-range.first;it++){
+	      sumOfGainsByLayer[layer].first+=payload->getApvGain(it,range);
+	      sumOfGainsByLayer[layer].second+=1.;
+		}// loop over APVs
+	  } // loop over detIds
+
+	  // loop on the map to fill the plot
+	  for (auto& data : sumOfGainsByLayer){
+	    fillWithBinAndValue(data.first,(data.second.first/data.second.second));
+	  }
+	  
+	}// payload
+      }// iovs
+      return true;
+    }// fill
+  };
+
+
+  /************************************************
     time history histogram of SiStripApvGains 
   *************************************************/
 
-  class SiStripApvGainTimeMeans : public cond::payloadInspector::HistoryPlot<SiStripApvGain,float> {
+  class SiStripApvGainByRunMeans : public cond::payloadInspector::HistoryPlot<SiStripApvGain,float> {
   public:
-    SiStripApvGainTimeMeans() : cond::payloadInspector::HistoryPlot<SiStripApvGain,float>( "SiStripApv Gains average","average Strip APV gain value"){}
-    virtual ~SiStripApvGainTimeMeans() = default;
+    SiStripApvGainByRunMeans() : cond::payloadInspector::HistoryPlot<SiStripApvGain,float>( "SiStripApv Gains average","average Strip APV gain value"){}
+    virtual ~SiStripApvGainByRunMeans() = default;
 
     float getFromPayload( SiStripApvGain& payload ){
      
@@ -80,10 +134,10 @@ namespace {
     time history histogram of TIB SiStripApvGains 
   *************************************************/
 
-  class SiStripApvTIBGainTimeMeans : public cond::payloadInspector::HistoryPlot<SiStripApvGain,float> {
+  class SiStripApvTIBGainByRunMeans : public cond::payloadInspector::HistoryPlot<SiStripApvGain,float> {
   public:
-    SiStripApvTIBGainTimeMeans() : cond::payloadInspector::HistoryPlot<SiStripApvGain,float>( "SiStripApv Gains average","average Tracker Inner Barrel APV gain value"){}
-    virtual ~SiStripApvTIBGainTimeMeans() = default;
+    SiStripApvTIBGainByRunMeans() : cond::payloadInspector::HistoryPlot<SiStripApvGain,float>( "SiStripApv Gains average","average Tracker Inner Barrel APV gain value"){}
+    virtual ~SiStripApvTIBGainByRunMeans() = default;
 
     float getFromPayload( SiStripApvGain& payload ){
      
@@ -114,10 +168,10 @@ namespace {
     time history histogram of TOB SiStripApvGains 
   *************************************************/
 
-  class SiStripApvTOBGainTimeMeans : public cond::payloadInspector::HistoryPlot<SiStripApvGain,float> {
+  class SiStripApvTOBGainByRunMeans : public cond::payloadInspector::HistoryPlot<SiStripApvGain,float> {
   public:
-    SiStripApvTOBGainTimeMeans() : cond::payloadInspector::HistoryPlot<SiStripApvGain,float>( "SiStripApv Gains average","average Tracker Outer Barrel gain value"){}
-    virtual ~SiStripApvTOBGainTimeMeans() = default;
+    SiStripApvTOBGainByRunMeans() : cond::payloadInspector::HistoryPlot<SiStripApvGain,float>( "SiStripApv Gains average","average Tracker Outer Barrel gain value"){}
+    virtual ~SiStripApvTOBGainByRunMeans() = default;
 
     float getFromPayload( SiStripApvGain& payload ){
      
@@ -148,10 +202,10 @@ namespace {
     time history histogram of TID SiStripApvGains 
   *************************************************/
 
-  class SiStripApvTIDGainTimeMeans : public cond::payloadInspector::HistoryPlot<SiStripApvGain,float> {
+  class SiStripApvTIDGainByRunMeans : public cond::payloadInspector::HistoryPlot<SiStripApvGain,float> {
   public:
-    SiStripApvTIDGainTimeMeans() : cond::payloadInspector::HistoryPlot<SiStripApvGain,float>( "SiStripApv Gains average","average Tracker Inner Disks APV gain value"){}
-    virtual ~SiStripApvTIDGainTimeMeans() = default;
+    SiStripApvTIDGainByRunMeans() : cond::payloadInspector::HistoryPlot<SiStripApvGain,float>( "SiStripApv Gains average","average Tracker Inner Disks APV gain value"){}
+    virtual ~SiStripApvTIDGainByRunMeans() = default;
 
     float getFromPayload( SiStripApvGain& payload ){
      
@@ -182,10 +236,10 @@ namespace {
     time history histogram of TEC SiStripApvGains 
   *************************************************/
 
-  class SiStripApvTECGainTimeMeans : public cond::payloadInspector::HistoryPlot<SiStripApvGain,float> {
+  class SiStripApvTECGainByRunMeans : public cond::payloadInspector::HistoryPlot<SiStripApvGain,float> {
   public:
-    SiStripApvTECGainTimeMeans() : cond::payloadInspector::HistoryPlot<SiStripApvGain,float>( "SiStripApv Gains average in TEC","average Tracker Endcaps APV gain value"){}
-    virtual ~SiStripApvTECGainTimeMeans() = default;
+    SiStripApvTECGainByRunMeans() : cond::payloadInspector::HistoryPlot<SiStripApvGain,float>( "SiStripApv Gains average in TEC","average Tracker Endcaps APV gain value"){}
+    virtual ~SiStripApvTECGainByRunMeans() = default;
 
     float getFromPayload( SiStripApvGain& payload ){
      
@@ -218,9 +272,10 @@ namespace {
 // Register the classes as boost python plugin
 PAYLOAD_INSPECTOR_MODULE(SiStripApvGain){
   PAYLOAD_INSPECTOR_CLASS(SiStripApvGainsValue);
-  PAYLOAD_INSPECTOR_CLASS(SiStripApvGainTimeMeans);
-  PAYLOAD_INSPECTOR_CLASS(SiStripApvTIBGainTimeMeans);
-  PAYLOAD_INSPECTOR_CLASS(SiStripApvTIDGainTimeMeans);
-  PAYLOAD_INSPECTOR_CLASS(SiStripApvTOBGainTimeMeans);
-  PAYLOAD_INSPECTOR_CLASS(SiStripApvTECGainTimeMeans);
+  PAYLOAD_INSPECTOR_CLASS(SiStripApvBarrelGainsByLayer);
+  PAYLOAD_INSPECTOR_CLASS(SiStripApvGainByRunMeans);
+  PAYLOAD_INSPECTOR_CLASS(SiStripApvTIBGainByRunMeans);
+  PAYLOAD_INSPECTOR_CLASS(SiStripApvTIDGainByRunMeans);
+  PAYLOAD_INSPECTOR_CLASS(SiStripApvTOBGainByRunMeans);
+  PAYLOAD_INSPECTOR_CLASS(SiStripApvTECGainByRunMeans);
 }
