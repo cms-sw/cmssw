91f0c6ff1644260ad78680db45ea12e615c633b7
Jay <jlawhorn@mit.edu>
2017-06-11 01:54:20 +0200

    remove hard-coded values, un-needed collections; currently has runtime error

--- DQMOffline/Trigger/plugins/RazorMonitor.h
+++ DQMOffline/Trigger/plugins/RazorMonitor.h
@@ -28,14 +28,6 @@
 #include "DataFormats/JetReco/interface/PFJetCollection.h"
 #include "DataFormats/JetReco/interface/CaloJet.h"
 #include "DataFormats/JetReco/interface/CaloJetCollection.h"
-#include "DataFormats/MuonReco/interface/Muon.h"
-#include "DataFormats/MuonReco/interface/MuonFwd.h"
-#include "DataFormats/TrackReco/interface/Track.h"
-#include "DataFormats/TrackReco/interface/TrackFwd.h"
-#include "DataFormats/EgammaCandidates/interface/GsfElectron.h"
-#include "DataFormats/EgammaCandidates/interface/GsfElectronFwd.h"
-#include "DataFormats/EgammaCandidates/interface/Photon.h"
-#include "DataFormats/EgammaCandidates/interface/PhotonFwd.h"
 
 //Hemispheres                                                                                                                                                                  
 #include "HLTrigger/JetMET/interface/HLTRHemisphere.h"
@@ -65,8 +57,8 @@ public:
   RazorMonitor( const edm::ParameterSet& );
   ~RazorMonitor();
   static void fillDescriptions(edm::ConfigurationDescriptions & descriptions);
-  //static void fillHistoPSetDescription(edm::ParameterSetDescription & pset);
-  //static void fillHistoLSPSetDescription(edm::ParameterSetDescription & pset);
+  static void fillHistoPSetDescription(edm::ParameterSetDescription & pset);
+  static void fillHistoLSPSetDescription(edm::ParameterSetDescription & pset);
 
   static double CalcMR(TLorentzVector ja,TLorentzVector jb);
   static double CalcR(double MR, TLorentzVector ja,TLorentzVector jb, edm::Handle<std::vector<reco::PFMET> > met, const std::vector<math::XYZTLorentzVector>& muons);
@@ -84,6 +76,7 @@ protected:
   void analyze(edm::Event const& iEvent, edm::EventSetup const& iSetup) override;
 
 private:
+  static MEbinning getHistoPSet    (edm::ParameterSet pset);
   static MEbinning getHistoLSPSet  (edm::ParameterSet pset);
 
   std::string folderName_;
@@ -91,37 +84,34 @@ private:
 
   edm::EDGetTokenT<reco::PFMETCollection>       metToken_;
   edm::EDGetTokenT<reco::PFJetCollection>       jetToken_;
-  edm::EDGetTokenT<reco::GsfElectronCollection> eleToken_;
-  edm::EDGetTokenT<reco::MuonCollection>        muoToken_;
   edm::EDGetTokenT<std::vector<math::XYZTLorentzVector> > theHemispheres_;
-
-  MEbinning           mr_binning_;
-  MEbinning           rsq_binning_;
-  MEbinning           dphiR_binning_;
+  
   MEbinning           ls_binning_;
+  std::vector<double> rsq_binning_;
+  std::vector<double> mr_binning_;
+  std::vector<double> dphiR_binning_;
 
   RazorME MR_ME_;
-  RazorME MR_Jet120_ME_;
+  RazorME MR_Tight_ME_;
   RazorME MRVsLS_;
   RazorME Rsq_ME_;
-  RazorME Rsq_Jet120_ME_;
+  RazorME Rsq_Tight_ME_;
   RazorME RsqVsLS_;
   RazorME dPhiR_ME_;
   RazorME dPhiRVsLS_;
 
   RazorME MRVsRsq_ME_;
-  RazorME MRVsRsq_Jet120_ME_;
+  RazorME MRVsRsq_Tight_ME_;
 
   GenericTriggerEventFlag* num_genTriggerEventFlag_;
   GenericTriggerEventFlag* den_genTriggerEventFlag_;
 
   StringCutObjectSelector<reco::MET,true>         metSelection_;
   StringCutObjectSelector<reco::PFJet,true   >    jetSelection_;
-  StringCutObjectSelector<reco::GsfElectron,true> eleSelection_;
-  StringCutObjectSelector<reco::Muon,true>        muoSelection_;
+  StringCutObjectSelector<reco::PFJet,true   >    tightJetSelection_;
   int njets_;
-  int nelectrons_;
-  int nmuons_;
+  float rsqCut_;
+  float mrCut_;
 
 };
 
