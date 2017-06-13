71b0393bc851db239d2909ef41a79c9068ecda0e
Roberval Walsh <roberval.walsh@gmail.com>
2017-06-13 13:09:36 +0200

    dqm online btag T&P plots for several monitoring semileptonic and all-hadronic paths, pt dependent

--- DQMOffline/Trigger/python/MssmHbbMonitor_cff.py
+++ DQMOffline/Trigger/python/MssmHbbMonitor_cff.py
@@ -3,5 +3,12 @@ import FWCore.ParameterSet.Config as cms
 from DQMOffline.Trigger.MssmHbbMonitor_cfi import *
 
 mssmHbbMonitor = cms.Sequence(
-    mssmHbbBtagTriggerMonitor
+    mssmHbbBtagTriggerMonitorSL40noMu +
+    mssmHbbBtagTriggerMonitorSL40 +
+    mssmHbbBtagTriggerMonitorSL100 +
+    mssmHbbBtagTriggerMonitorSL200 +
+    mssmHbbBtagTriggerMonitorSL350 +
+    mssmHbbBtagTriggerMonitorAH100 +
+    mssmHbbBtagTriggerMonitorAH200 +
+    mssmHbbBtagTriggerMonitorAH350
 )
