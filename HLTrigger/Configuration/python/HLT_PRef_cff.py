19d80d9a2532a15a2965789b90722c7b43f78adf
silviodonato <silvio.donato@cern.ch>
2017-06-14 16:16:03 +0200

    sync with /dev/CMSSW_9_2_0/HLT/V46

--- HLTrigger/Configuration/python/HLT_PRef_cff.py
+++ HLTrigger/Configuration/python/HLT_PRef_cff.py
@@ -1,11 +1,11 @@
-# /dev/CMSSW_9_2_0/PRef/V14 (CMSSW_9_2_2)
+# /dev/CMSSW_9_2_0/PRef/V15 (CMSSW_9_2_2)
 
 import FWCore.ParameterSet.Config as cms
 
 fragment = cms.ProcessFragment( "null" )
 
 fragment.HLTConfigVersion = cms.PSet(
-  tableName = cms.string('/dev/CMSSW_9_2_0/PRef/V14')
+  tableName = cms.string('/dev/CMSSW_9_2_0/PRef/V15')
 )
 
 fragment.transferSystem = cms.PSet( 
