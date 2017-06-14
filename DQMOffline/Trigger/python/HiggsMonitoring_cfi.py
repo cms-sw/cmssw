11fe0e02bff0e226e4d892b6f869c16e465660ef
Suvankar Roy Chowdhury <suvankar.roy.chowdhury@cern.ch>
2017-06-14 06:06:30 +0200

    updates to take into account changes made in Top PR

--- DQMOffline/Trigger/python/HiggsMonitoring_cfi.py
+++ DQMOffline/Trigger/python/HiggsMonitoring_cfi.py
@@ -37,6 +37,12 @@ hltHIGmonitoring.histoPSet.csvPSet = cms.PSet(
   xmax  = cms.double( 1.0  ),
 )
 
+hltHIGmonitoring.histoPSet.DRPSet = cms.PSet(
+   nbins = cms.int32 ( 60  ),
+   xmin  = cms.double( 0.0 ),
+   xmax  = cms.double( 6.0 ),
+)
+
 hltHIGmonitoring.applyleptonPVcuts = cms.bool(True)
 hltHIGmonitoring.leptonPVcuts = cms.PSet(
   dxy = cms.double(   0.5  ),
