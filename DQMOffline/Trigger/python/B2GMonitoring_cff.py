212e24a3bf54ccfa5364609b63828532d2f02310
Ashley Marie Parker <ash.marie.parker@gmail.com>
2017-06-15 12:30:07 +0200

    Adding eta cut on jets and raising JetHT pt cut

--- DQMOffline/Trigger/python/B2GMonitoring_cff.py
+++ DQMOffline/Trigger/python/B2GMonitoring_cff.py
@@ -7,8 +7,8 @@ from DQMOffline.Trigger.HTMonitor_cfi import hltHTmonitoring
 AK8PFHT700_HTmonitoring = hltHTmonitoring.clone()
 AK8PFHT700_HTmonitoring.FolderName = cms.string('HLT/B2GMonitor/AK8PFHT700_TrimR0p1PT0p03Mass50')
 AK8PFHT700_HTmonitoring.numGenericTriggerEventPSet.hltPaths = cms.vstring("HLT_AK8PFHT700_TrimR0p1PT0p03Mass50_v*")
-AK8PFHT700_HTmonitoring.jetSelection      = cms.string("pt > 0")
-AK8PFHT700_HTmonitoring.jetSelection_HT = cms.string("pt > 10 && eta < 2.5")
+AK8PFHT700_HTmonitoring.jetSelection      = cms.string("pt > 0 && eta < 2.5")
+AK8PFHT700_HTmonitoring.jetSelection_HT = cms.string("pt > 30 && eta < 2.5")
 
 # HLT_AK8PFJet360_TrimMass30                                                                                                                                                  
 AK8PFJet360_TrimMass30_PromptMonitoring = hltJetMETmonitoring.clone()
@@ -16,23 +16,23 @@ AK8PFJet360_TrimMass30_PromptMonitoring.FolderName = cms.string('HLT/B2GMonitor/
 AK8PFJet360_TrimMass30_PromptMonitoring.ptcut = cms.double(360)
 AK8PFJet360_TrimMass30_Promptonitoring.numGenericTriggerEventPSet.hltPaths = cms.vstring("HLT_AK8PFJet360_TrimMass30_v*")
 AK8PFJet360_TrimMass30_PromptMonitoring.pfjets      = cms.InputTag("ak8PFJetsCHS")
-AK8PFJet360_TrimMass30_PromptMonitoring.jetSelection      = cms.string("pt > 0")
-AK8PFJet360_TrimMass30_PromptMonitoring.jetSelection_HT= cms.string("pt > 10 && eta < 2.5")
+AK8PFJet360_TrimMass30_PromptMonitoring.jetSelection      = cms.string("pt > 0 && eta < 2.5")
+AK8PFJet360_TrimMass30_PromptMonitoring.jetSelection_HT= cms.string("pt > 30 && eta < 2.5")
 
 
 # HLT_PFHT650_WideJetMJJ900DEtaJJ1p5                                                                                                                   
 PFHT650_WideJetMJJ900DEtaJJ1p5_HTmonitoring = hltHTmonitoring.clone()                        
 PFHT650_WideJetMJJ900DEtaJJ1p5_HTmonitoring.FolderName = cms.string('HLT/B2GMonitor/PFHT650_WideJetMJJ900DEtaJJ1p5')
 PFHT650_WideJetMJJ900DEtaJJ1p5_HTmonitoring.numGenericTriggerEventPSet.hltPaths = cms.vstring("HLT_PFHT650_WideJetMJJ900DEtaJJ1p5_v*")
-PFHT650_WideJetMJJ900DEtaJJ1p5_HTmonitoring.jetSelection = cms.string("pt > 0")
-PFHT650_WideJetMJJ900DEtaJJ1p5_HTmonitoring.jetSelection_HT =  cms.string("pt > 10 && eta < 2.5")
+PFHT650_WideJetMJJ900DEtaJJ1p5_HTmonitoring.jetSelection = cms.string("pt > 0 && eta < 2.5")
+PFHT650_WideJetMJJ900DEtaJJ1p5_HTmonitoring.jetSelection_HT =  cms.string("pt > 30 && eta < 2.5")
 
 # HLT_PFHT800                                                                                                                                          
 PFHT800_HTmonitoring = hltHTmonitoring.clone()
 PFHT800_HTmonitoring.FolderName = cms.string('HLT/B2GMonitor/PFHT800')
 PFHT800_HTmonitoring.numGenericTriggerEventPSet.hltPaths = cms.vstring("HLT_PFHT800_v*")
-PFHT800_HTmonitoring.jetSelection = cms.string("pt > 0") 
-PFHT800_HTmonitoring.jetSelection_HT =  cms.string("pt > 10 && eta < 2.5")
+PFHT800_HTmonitoring.jetSelection = cms.string("pt > 0 && eta < 2.5") 
+PFHT800_HTmonitoring.jetSelection_HT =  cms.string("pt > 30 && eta < 2.5")
 
 b2gMonitorHLT = cms.Sequence(
     AK8PFHT700_HTmonitoring
