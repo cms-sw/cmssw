f0402d439c3e794e7da4af1db6b27edee1bf7e54
David <David.Lange@cern.ch>
2017-06-19 10:53:49 +0200

    adding some python tests

--- PhysicsTools/PythonAnalysis/test/runtests.sh
+++ PhysicsTools/PythonAnalysis/test/runtests.sh
@@ -7,3 +7,9 @@ python ${LOCAL_TEST_DIR}/testPandas.py || die 'Failure using testPandas' $?
 python ${LOCAL_TEST_DIR}/testRootNumpy.py || die 'Failure using testRootNumpy' $?
 python ${LOCAL_TEST_DIR}/test_pycurl.py || die 'Failure using test_pycurl' $?
 #python ${LOCAL_TEST_DIR}/testRootpy.py || die 'Failure using testRootpy' $?
+
+python ${LOCAL_TEST_DIR}/testBottleneck.py || die 'Failure using testBottleneck' $?
+python ${LOCAL_TEST_DIR}/testDeepDish.py || die 'Failure using testDeepDish' $?
+python ${LOCAL_TEST_DIR}/testNumExpr.py || die 'Failure using testNumExpr' $?
+python ${LOCAL_TEST_DIR}/testNumba.py || die 'Failure using testNumba' $?
+python ${LOCAL_TEST_DIR}/testTables.py || die 'Failure using testTables' $?
