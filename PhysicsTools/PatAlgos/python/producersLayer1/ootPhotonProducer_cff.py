23832eacb7255b56229b5bcd14af51459e3d30d3
Kevin <kpm82@cornell.edu>
2017-06-22 14:49:10 +0200

    OOT photon sequence (RECO+PAT) for 2016 legacy 80X reminiAOD from AOD

--- PhysicsTools/PatAlgos/python/producersLayer1/ootPhotonProducer_cff.py
+++ PhysicsTools/PatAlgos/python/producersLayer1/ootPhotonProducer_cff.py
@@ -49,3 +49,44 @@ makePatOOTPhotonsTask = cms.Task(
     )
 
 makePatOOTPhotons = cms.Sequence(makePatOOTPhotonsTask)
+
+## For legacy reprocessing
+## Standard Sequences
+from RecoJets.Configuration.CaloTowersES_cfi import *
+from RecoLocalCalo.EcalRecAlgos.EcalSeverityLevelESProducer_cfi import *
+from RecoParticleFlow.PFClusterProducer.particleFlowRecHitPS_cfi import *
+from RecoParticleFlow.PFClusterProducer.particleFlowClusterPS_cfi import *
+## OOT Sequences
+from RecoParticleFlow.PFClusterProducer.particleFlowRecHitOOTECAL_cff import *
+from RecoParticleFlow.PFClusterProducer.particleFlowClusterOOTECALUncorrected_cff import *
+from RecoParticleFlow.PFClusterProducer.particleFlowClusterOOTECAL_cff import *
+from RecoEcal.EgammaClusterProducers.particleFlowSuperClusterOOTECAL_cff import *
+from RecoEgamma.EgammaPhotonProducers.ootPhotonCore_cff import *
+from RecoEgamma.EgammaPhotonProducers.ootPhotons_cff import *
+from RecoEgamma.EgammaIsolationAlgos.pfClusterIsolation_cfi import *
+
+_makePatOOTPhotonsTask = makePatOOTPhotonsTask.copy()
+_makePatOOTPhotonsTask.add(
+    CaloTowerConstituentsMapBuilder,
+    essourceEcalSev,
+    ecalSeverityLevel,
+    particleFlowRecHitPS,
+    particleFlowClusterPS,
+    particleFlowRecHitOOTECAL,
+    particleFlowClusterOOTECALUncorrected,
+    particleFlowClusterOOTECAL,
+    particleFlowSuperClusterOOTECAL,
+    ootPhotonCore,
+    ootPhotons,
+    ootPhotonEcalPFClusterIsolationProducer
+)
+
+from Configuration.Eras.Modifier_run2_miniAOD_80XLegacy_cff import run2_miniAOD_80XLegacy
+run2_miniAOD_80XLegacy.toModify(
+    patOOTPhotons, 
+    hcalPFClusterIsoMap = ""
+)
+
+run2_miniAOD_80XLegacy.toReplaceWith(
+    makePatOOTPhotonsTask, _makePatOOTPhotonsTask
+)
