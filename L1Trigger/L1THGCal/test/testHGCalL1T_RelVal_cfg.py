a1c61cc42d814d721b0dde80c835ee77a8b90e9e
Jean-Baptiste Sauvan <jean-baptiste.sauvan@cern.ch>
2017-06-23 11:00:55 +0200

    Add trigger geometry customization

--- L1Trigger/L1THGCal/test/testHGCalL1T_RelVal_cfg.py
+++ L1Trigger/L1THGCal/test/testHGCalL1T_RelVal_cfg.py
@@ -57,6 +57,9 @@ process.GlobalTag = GlobalTag(process.GlobalTag, 'auto:phase2_realistic', '')
 # load HGCAL TPG simulation
 process.load('L1Trigger.L1THGCal.hgcalTriggerPrimitives_cff')
 process.hgcl1tpg_step = cms.Path(process.hgcalTriggerPrimitives)
+# Change to V7 trigger geometry for older samples
+#  from L1Trigger.L1THGCal.customTriggerGeometry import custom_geometry_V7
+#  process = custom_geometry_V7(process)
 
 # load ntuplizer
 process.load('L1Trigger.L1THGCal.hgcalTriggerNtuples_cff')
