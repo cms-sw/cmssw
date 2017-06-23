e405489a084e3c43d319b47c0235618a0c9b5ef9
Jean-Baptiste Sauvan <jean-baptiste.sauvan@cern.ch>
2017-06-23 10:14:27 +0200

    Update configs

--- L1Trigger/L1THGCal/python/hgcalTriggerGeometryESProducer_cfi.py
+++ L1Trigger/L1THGCal/python/hgcalTriggerGeometryESProducer_cfi.py
@@ -3,9 +3,9 @@ import FWCore.ParameterSet.Config as cms
 
 geometry = cms.PSet( TriggerGeometryName = cms.string('HGCalTriggerGeometryHexImp2'),
                      L1TCellsMapping = cms.FileInPath("L1Trigger/L1THGCal/data/triggercell_mapping.txt"),
-                     L1TModulesMapping = cms.FileInPath("L1Trigger/L1THGCal/data/module_mapping_PairsRing_V7_0.txt"),
+                     L1TModulesMapping = cms.FileInPath("L1Trigger/L1THGCal/data/module_mapping_PairsRing_V8_0.txt"),
                      L1TCellNeighborsMapping = cms.FileInPath("L1Trigger/L1THGCal/data/triggercell_neighbor_mapping_1.txt"),
-                     L1TWaferNeighborsMapping = cms.FileInPath("L1Trigger/L1THGCal/data/wafer_neighbor_mapping_V7_0.txt"),
+                     L1TWaferNeighborsMapping = cms.FileInPath("L1Trigger/L1THGCal/data/wafer_neighbor_mapping_V8_0.txt"),
                      eeSDName = cms.string('HGCalEESensitive'),
                      fhSDName = cms.string('HGCalHESiliconSensitive'),
                      bhSDName = cms.string('HGCalHEScintillatorSensitive'),
