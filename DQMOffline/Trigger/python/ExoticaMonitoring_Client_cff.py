f27d3652d18a064d0043f6779c2993ee6ce39ee1
bpf6qc <bfrancis@gmail.com>
2017-06-29 15:52:04 -0400

    Add offline DQM for MET plus track EXO triggers

--- DQMOffline/Trigger/python/ExoticaMonitoring_Client_cff.py
+++ DQMOffline/Trigger/python/ExoticaMonitoring_Client_cff.py
@@ -35,7 +35,27 @@ NoBPTXEfficiency = DQMEDHarvester("DQMGenericClient",
     ), 
 )
 
+METplusTrackEfficiency = DQMEDHarvester("DQMGenericClient",
+    subDirs = cms.untracked.vstring("HLT/MET/MET105_IsoTrk50/", "HLT/MET/MET120_IsoTrk50/"),
+    verbose = cms.untracked.uint32(0), # Set to 2 for all messages
+    resolution = cms.vstring(),
+    efficiency = cms.vstring(
+        "effic_met             'MET leg turnON;              CaloMET [GeV]; efficiency'     met_numerator             met_denominator",
+        "effic_met_variable    'MET leg turnON;              CaloMET [GeV]; efficiency'     met_variable_numerator    met_variable_denominator",
+        "effic_metPhi          'MET leg efficiency vs phi;   CaloMET phi [rad]; efficiency' metPhi_numerator          metPhi_denominator",
+        "effic_muonPt          'Track leg turnON;            Muon p_{T} [GeV]; efficiency'  muonPt_numerator          muonPt_denominator",
+        "effic_muonPt_variable 'Track leg turnON;            Muon p_{T} [GeV]; efficiency'  muonPt_variable_numerator muonPt_variable_denominator",
+        "effic_muonEta         'Track leg efficiency vs eta; Muon #eta; efficiency'         muonEta_numerator         muonEta_denominator",
+    ),
+    efficiencyProfile = cms.untracked.vstring(
+        "effic_met_vs_LS     'MET leg efficiency vs LS; LS; CaloMET leg efficiency' metVsLS_numerator metVsLS_denominator",
+        "effic_muonPt_vs_LS 'Track leg efficiency vs LS; LS; Track leg efficiency'  muonPtVsLS_numerator muonPtVsLS_denominator",
+    ),
+
+)
+
 exoticaClient = cms.Sequence(
     metEfficiency
     + NoBPTXEfficiency
+    + METplusTrackEfficiency
 )
