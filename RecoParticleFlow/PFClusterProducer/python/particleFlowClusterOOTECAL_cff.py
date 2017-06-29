81dbe39ec6af01e83984929afeb70c2c8abf3445
Rafael <rclsa@fnal.gov>
2017-06-29 17:43:52 +0200

    use same correction for OOT and normal ECAL clusters

--- RecoParticleFlow/PFClusterProducer/python/particleFlowClusterOOTECAL_cff.py
+++ RecoParticleFlow/PFClusterProducer/python/particleFlowClusterOOTECAL_cff.py
@@ -2,8 +2,6 @@ import FWCore.ParameterSet.Config as cms
 from RecoParticleFlow.PFClusterProducer.particleFlowClusterECAL_cff import *
 
 particleFlowClusterOOTECAL = particleFlowClusterECAL.clone()
-particleFlowClusterOOTECAL.energyCorrector.applyMVACorrections = True
-particleFlowClusterOOTECAL.energyCorrector.maxPtForMVAEvaluation = 90.
 particleFlowClusterOOTECAL.inputECAL = cms.InputTag("particleFlowClusterOOTECALUncorrected")
 
 from Configuration.Eras.Modifier_run2_miniAOD_80XLegacy_cff import run2_miniAOD_80XLegacy
