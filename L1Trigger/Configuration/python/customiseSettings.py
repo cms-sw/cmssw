907829eed29d615945353ae2b4b3fbfb3247bae5
Aaron <aaron.bundock@cern.ch>
2017-06-29 17:54:00 +0200

    MET PUS v6 add v1_8 with 0.7 HF SF in low pT bins and v6 MET

--- L1Trigger/Configuration/python/customiseSettings.py
+++ L1Trigger/Configuration/python/customiseSettings.py
@@ -37,6 +37,10 @@ def L1TSettingsToCaloStage2Params_2017_v1_8_2_updateHFSF(process):
     process.load("L1Trigger.L1TCalorimeter.caloStage2Params_2017_v1_8_2_updateHFSF_cfi")
     return process
 
+def L1TSettingsToCaloStage2Params_2017_v1_8_updateHFSF_v6MET(process):
+    process.load("L1Trigger.L1TCalorimeter.caloStage2Params_2017_v1_8_updateHFSF_v6MET_cfi")
+    return process
+
 def L1TSettingsToCaloStage2Params_2017_v1_8_updateHFSF(process):
     process.load("L1Trigger.L1TCalorimeter.caloStage2Params_2017_v1_8_updateHFSF_cfi")
     return process
