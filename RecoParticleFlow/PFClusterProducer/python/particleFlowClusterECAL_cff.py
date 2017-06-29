77db7604daa83cfc9948a80374318f25aa091139
Rafael <rclsa@fnal.gov>
2017-06-29 13:44:49 +0200

    remove cfi file and move config to cff

--- RecoParticleFlow/PFClusterProducer/python/particleFlowClusterECAL_cff.py
+++ RecoParticleFlow/PFClusterProducer/python/particleFlowClusterECAL_cff.py
@@ -1,10 +1,11 @@
 import FWCore.ParameterSet.Config as cms
 
 from RecoParticleFlow.PFClusterProducer.particleFlowClusterECAL_cfi import particleFlowClusterECAL
-particleFlowClusterECAL.energyCorrector.applyMVACorrections = True
-particleFlowClusterECAL.energyCorrector.maxPtForMVAEvaluation = 90
+particleFlowClusterECAL.energyCorrector.applyMVACorrections = cms.bool(True)
+particleFlowClusterECAL.energyCorrector.maxPtForMVAEvaluation = cms.double(90.)
 
 from Configuration.Eras.Modifier_run2_ECAL_2017_cff import run2_ECAL_2017
 run2_ECAL_2017.toModify(particleFlowClusterECAL,
-                        energyCorrector = dict(srfAwareCorrection = True, maxPtForMVAEvaluation = 300))
+                        energyCorrector = dict(srfAwareCorrection = cms.bool(True), maxPtForMVAEvaluation = cms.double(300.)))
 
+particleFlowClusterECALSequence = cms.Sequence(particleFlowClusterECAL)
