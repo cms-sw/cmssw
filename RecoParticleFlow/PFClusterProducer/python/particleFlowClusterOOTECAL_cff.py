1ddb334debc9ea5d483e5499ac533082037e255c
Rafael <rclsa@fnal.gov>
2017-06-29 17:16:36 +0200

    do not specify types in the OOTECAL files either

--- RecoParticleFlow/PFClusterProducer/python/particleFlowClusterOOTECAL_cff.py
+++ RecoParticleFlow/PFClusterProducer/python/particleFlowClusterOOTECAL_cff.py
@@ -2,6 +2,6 @@ import FWCore.ParameterSet.Config as cms
 from RecoParticleFlow.PFClusterProducer.particleFlowClusterECAL_cff import *
 
 particleFlowClusterOOTECAL = particleFlowClusterECAL.clone()
-particleFlowClusterOOTECAL.energyCorrector.applyMVACorrections = cms.bool(True)
-particleFlowClusterOOTECAL.energyCorrector.maxPtForMVAEvaluation = cms.double(90.)
+particleFlowClusterOOTECAL.energyCorrector.applyMVACorrections = True
+particleFlowClusterOOTECAL.energyCorrector.maxPtForMVAEvaluation = 90.
 particleFlowClusterOOTECAL.inputECAL = cms.InputTag("particleFlowClusterOOTECALUncorrected")
