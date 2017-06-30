7aabfe75e18934bf7d091c9cc9dd08b57b14a829
Ashley Marie Parker <ash.marie.parker@gmail.com>
2017-06-30 15:13:26 +0200

    Removing  PFMETNoMu90_PFMHTNoMu90 , MET200 & MonoCentralPFJet80_PFMETNoMu90_PFMHTNoMu90

--- DQMOffline/Trigger/python/HTMonitor_cff.py
+++ DQMOffline/Trigger/python/HTMonitor_cff.py
@@ -2,20 +2,10 @@ import FWCore.ParameterSet.Config as cms
 
 from DQMOffline.Trigger.HTMonitor_cfi import hltHTmonitoring
 
-# HLT_PFMETNoMu90_PFMHTNoMu90_IDTight
-PFMETNoMu90_PFMHTNoMu90_HTmonitoring = hltHTmonitoring.clone()
-PFMETNoMu90_PFMHTNoMu90_HTmonitoring.FolderName = cms.string('HLT/HT/PFMETNoMu90/')
-PFMETNoMu90_PFMHTNoMu90_HTmonitoring.numGenericTriggerEventPSet.hltPaths = cms.vstring("HLT_PFMETNoMu90_PFMHTNoMu90_IDTight_v*")
-
 # HLT_PFMETNoMu120_PFMHTNoMu120_IDTight
 PFMETNoMu120_PFMHTNoMu120_HTmonitoring = hltHTmonitoring.clone()
 PFMETNoMu120_PFMHTNoMu120_HTmonitoring.FolderName = cms.string('HLT/HT/PFMETNoMu120/')
 PFMETNoMu120_PFMHTNoMu120_HTmonitoring.numGenericTriggerEventPSet.hltPaths = cms.vstring("HLT_PFMETNoMu120_PFMHTNoMu120_IDTight_v*")
-
-# HLT_MET200
-MET200_HTmonitoring = hltHTmonitoring.clone()
-MET200_HTmonitoring.FolderName = cms.string('HLT/HT/MET200/')
-MET200_HTmonitoring.numGenericTriggerEventPSet.hltPaths = cms.vstring("HLT_MET200_v*")
 
 # HLT_MonoCentralPFJet80_PFMETNoMu110_PFMHTNoMu110_IDTight
 MonoCentralPFJet80_PFMETNoMu110_PFMHTNoMu110_HTmonitoring = hltHTmonitoring.clone()
@@ -44,12 +34,6 @@ MonoCentralPFJet80_PFMETNoMu140_PFMHTNoMu140_HTmonitoring.FolderName = cms.strin
 MonoCentralPFJet80_PFMETNoMu140_PFMHTNoMu140_HTmonitoring.numGenericTriggerEventPSet.hltPaths = cms.vstring("HLT_MonoCentralPFJet80_PFMETNoMu140_PFMHTNoMu140_IDTight_v*")
 MonoCentralPFJet80_PFMETNoMu140_PFMHTNoMu140_HTmonitoring.jetSelection      = cms.string("pt > 100 && eta < 2.5 && neutralHadronEnergyFraction < 0.8 && chargedHadronEnergyFraction > 0.1")
 MonoCentralPFJet80_PFMETNoMu140_PFMHTNoMu140_HTmonitoring.jetSelection_HT   = cms.string("pt > 30 && eta < 2.5")
-
-# HLT_MonoCentralPFJet80_PFMETNoMu90_PFMHTNoMu90_IDTight
-MonoCentralPFJet80_PFMETNoMu90_PFMHTNoMu90_HTmonitoring = hltHTmonitoring.clone()
-MonoCentralPFJet80_PFMETNoMu90_PFMHTNoMu90_HTmonitoring.FolderName = cms.string('HLT/HT/MonoCentralPFJet80_PFMETNoMu90/')
-MonoCentralPFJet80_PFMETNoMu90_PFMHTNoMu90_HTmonitoring.numGenericTriggerEventPSet.hltPaths = cms.vstring("HLT_MonoCentralPFJet80_PFMETNoMu110_PFMHTNoMu110_IDTight_v*")
-MonoCentralPFJet80_PFMETNoMu90_PFMHTNoMu90_HTmonitoring.jetSelection_HT   = cms.string("pt > 30 && eta < 2.5")
 
 # HLT_PFHT350MinPFJet15
 PFHT350MinPFJet15_HTmonitoring = hltHTmonitoring.clone()
@@ -196,11 +180,8 @@ PFMETTypeOne140_PFMHT140_HTmonitoring.jetSelection      = cms.string("pt > 100 &
 PFMETTypeOne140_PFMHT140_HTmonitoring.jetSelection_HT   = cms.string("pt > 30 && eta < 2.5")
 
 exoHLTHTmonitoring = cms.Sequence(
-    PFMETNoMu90_PFMHTNoMu90_HTmonitoring
-    + PFMETNoMu120_PFMHTNoMu120_HTmonitoring
-    + MET200_HTmonitoring
+    PFMETNoMu120_PFMHTNoMu120_HTmonitoring
     + MonoCentralPFJet80_PFMETNoMu120_PFMHTNoMu120_HTmonitoring
-    + MonoCentralPFJet80_PFMETNoMu90_PFMHTNoMu90_HTmonitoring
     + PFHT350MinPFJet15_HTmonitoring
     + PFHT500_PFMET100_PFMHT100_HTmonitoring
     + PFHT500_PFMET110_PFMHT110_HTmonitoring
