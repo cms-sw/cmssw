d25bbf45fa0ad1c96c6ce76594264fb9b4e76762
Roberval Walsh <roberval.walsh@gmail.com>
2017-06-30 11:12:04 +0200

    removed calling modules for physics paths

--- DQMOffline/Trigger/python/MssmHbbMonitor_cff.py
+++ DQMOffline/Trigger/python/MssmHbbMonitor_cff.py
@@ -3,13 +3,6 @@ import FWCore.ParameterSet.Config as cms
 from DQMOffline.Trigger.MssmHbbMonitor_cfi import *
 
 mssmHbbMonitor = cms.Sequence(
-    msssHbbAllHadronic100 +
-    msssHbbAllHadronic116 +
-    msssHbbAllHadronic128 +
-    msssHbbSemileptonic40 +
-    msssHbbSemileptonic54 +
-    msssHbbSemileptonic62 +
-    msssHbbSemileptonicNoBtag +
     mssmHbbBtagTriggerMonitorSL40noMu +
     mssmHbbBtagTriggerMonitorSL40 +
     mssmHbbBtagTriggerMonitorSL100 +
