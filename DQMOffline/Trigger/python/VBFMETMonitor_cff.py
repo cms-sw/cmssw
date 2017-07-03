230c492ed1953bc7fd246a9da9d2c5a0f665f632
amagnan <a.magnan@imperial.ac.uk>
2017-07-03 15:50:18 +0200

    fix typo in cff

--- DQMOffline/Trigger/python/VBFMETMonitor_cff.py
+++ DQMOffline/Trigger/python/VBFMETMonitor_cff.py
@@ -9,7 +9,7 @@ DiJetVBFmonitoring.numGenericTriggerEventPSet.hltInputTag   = cms.InputTag( "Tri
 DiJetVBFmonitoring.numGenericTriggerEventPSet.hltPaths = cms.vstring("HLT_DiJet110_35_Mjj650_PFMET110_v*","HLT_DiJet110_35_Mjj650_PFMET120_v*","HLT_DiJet110_35_Mjj650_PFMET130_v*")
 #DiJetVBFmonitoring.metSelection = cms.string("")
 DiJetVBFmonitoring.jetSelection = cms.string("pt>40 & abs(eta)<4.7")
-EXO_VBF_METmonitoring.jetId = cms.string("loose")
+DiJetVBFmonitoring.jetId = cms.string("loose")
 DiJetVBFmonitoring.njets = cms.int32(2)
 #DiJetVBFmonitoring.metSelection = cms.string("pt>150")
 
