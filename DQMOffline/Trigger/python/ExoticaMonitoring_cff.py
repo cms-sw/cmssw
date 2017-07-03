6d8416e7bdbe156a961ff314289df13a2d2b89e8
gomber <bhawna.gomber@cern.ch>
2017-07-03 15:46:50 +0200

    Dark SUSY and displaced lepton monitoring

--- DQMOffline/Trigger/python/ExoticaMonitoring_cff.py
+++ DQMOffline/Trigger/python/ExoticaMonitoring_cff.py
@@ -2,8 +2,10 @@ import FWCore.ParameterSet.Config as cms
 
 from DQMOffline.Trigger.METMonitor_cff import *
 from DQMOffline.Trigger.NoBPTXMonitor_cff import *
+from DQMOffline.Trigger.MuonMonitor_cff import *
 
 exoticaMonitorHLT = cms.Sequence(
     exoHLTMETmonitoring
     + exoHLTNoBPTXmonitoring
+    + exoHLTMuonmonitoring
 )
