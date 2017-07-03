f565ca64f1ad5b361cb1d0593a940f1ceb4e44ed
Roberval Walsh <roberval.walsh@gmail.com>
2017-07-03 17:00:13 +0200

    modifying module name

--- DQMOffline/Trigger/python/HiggsMonitoring_Client_cff.py
+++ DQMOffline/Trigger/python/HiggsMonitoring_Client_cff.py
@@ -1,6 +1,6 @@
 import FWCore.ParameterSet.Config as cms
 
-from DQMOffline.Trigger.MssmHbbMonitor_Client_cfi import *
+from DQMOffline.Trigger.MssmHbbBtagTriggerMonitor_Client_cfi import *
 
 higgsClient = cms.Sequence(
    mssmHbbBtagTriggerEfficiency
