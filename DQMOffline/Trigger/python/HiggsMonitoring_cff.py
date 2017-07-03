e56e0719ebd2d243bb5aadea7243b7553fac916b
Roberval Walsh <roberval.walsh@gmail.com>
2017-07-03 15:53:36 +0200

    imported module renamed

--- DQMOffline/Trigger/python/HiggsMonitoring_cff.py
+++ DQMOffline/Trigger/python/HiggsMonitoring_cff.py
@@ -1,8 +1,7 @@
 import FWCore.ParameterSet.Config as cms
 
-from DQMOffline.Trigger.MssmHbbMonitor_cff import *
-
+from DQMOffline.Trigger.MssmHbbBtagTriggerMonitor_cff import *
 
 higgsMonitorHLT = cms.Sequence(
-   mssmHbbMonitor
+   mssmHbbBtagTriggerMonitor
 )
