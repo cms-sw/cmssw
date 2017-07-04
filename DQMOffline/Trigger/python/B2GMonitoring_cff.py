681da97d56d6a71cca4090c91d7b4aa2f7fa4291
UBParker <ash.marie.parker@gmail.com>
2017-07-04 07:25:09 -0500

    Implement changes for after PR 19178 is merged and added HLT_AK8PFJet360_TrimMass30_v* monitoring

--- DQMOffline/Trigger/python/B2GMonitoring_cff.py
+++ DQMOffline/Trigger/python/B2GMonitoring_cff.py
@@ -1,7 +1,7 @@
 import FWCore.ParameterSet.Config as cms
 
-# THIS WILL BE UPDATED WHEN 19178 IS MERGED:
-#from DQMOffline.Trigger.JetMETMonitor_cfi import hltJetMETmonitoring
+
+from DQMOffline.Trigger.JetMETMonitor_cfi import hltJetMETmonitoring
 from DQMOffline.Trigger.HTMonitor_cfi import hltHTmonitoring
 from DQMOffline.Trigger.B2GTnPMonitor_cfi import B2GegmGsfElectronIDsForDQM,B2GegHLTDQMOfflineTnPSource
 
@@ -42,9 +42,14 @@ AK8PFHT900_TrimMass50_HTmonitoring.jets = cms.InputTag("ak8PFJetsCHS")
 AK8PFHT900_TrimMass50_HTmonitoring.jetSelection      = cms.string("pt > 0 && eta < 2.5")
 AK8PFHT900_TrimMass50_HTmonitoring.jetSelection_HT = cms.string("pt > 200 && eta < 2.5")
 
+AK8PFJet360_TrimMass30_PromptMonitoring = hltJetMETmonitoring.clone()
+AK8PFJet360_TrimMass30_PromptMonitoring.FolderName = cms.string('HLT/B2GMonitor/AK8PFJet360_TrimMass30')
+AK8PFJet360_TrimMass30_PromptMonitoring.ptcut = cms.double(360)
+AK8PFJet360_TrimMass30_Promptonitoring.numGenericTriggerEventPSet.hltPaths = cms.vstring("HLT_AK8PFJet360_TrimMass30_v*")
+AK8PFJet360_TrimMass30_PromptMonitoring.pfjets      = cms.InputTag("ak8PFJetsCHS")
+AK8PFJet360_TrimMass30_PromptMonitoring.jetSelection      = cms.string("pt > 0 && eta < 2.5")
+AK8PFJet360_TrimMass30_PromptMonitoring.jetSelection_HT= cms.string("pt > 200 && eta < 2.5")
 
-# THE FOLLOWING TO BE ADDED WHEN PR 19178 is merged:
-'''               
 AK8PFJet380_TrimMass30_PromptMonitoring = hltJetMETmonitoring.clone()
 AK8PFJet380_TrimMass30_PromptMonitoring.FolderName = cms.string('HLT/B2GMonitor/AK8PFJet380_TrimMass30')
 AK8PFJet380_TrimMass30_PromptMonitoring.ptcut = cms.double(380)
@@ -68,17 +73,18 @@ AK8PFJet420_TrimMass30_Promptonitoring.numGenericTriggerEventPSet.hltPaths = cms
 AK8PFJet420_TrimMass30_PromptMonitoring.pfjets      = cms.InputTag("ak8PFJetsCHS")
 AK8PFJet420_TrimMass30_PromptMonitoring.jetSelection      = cms.string("pt > 0 && eta < 2.5")
 AK8PFJet420_TrimMass30_PromptMonitoring.jetSelection_HT= cms.string("pt > 200 && eta < 2.5")
-'''
+
 
 
 b2gMonitorHLT = cms.Sequence(
     AK8PFHT750_TrimMass50_HTmonitoring +
     AK8PFHT800_TrimMass50_HTmonitoring + 
     AK8PFHT850_TrimMass50_HTmonitoring + 
-    AK8PFHT900_TrimMass50_HTmonitoring# +
-    #AK8PFJet380_TrimMass30 + 
-    #AK8PFJet400_TrimMass30 + 
-    #AK8PFJet420_TrimMass30
+    AK8PFHT900_TrimMass50_HTmonitoring +
+    AK8PFJet360_TrimMass30_PromptMonitoring +
+    AK8PFJet380_TrimMass30_PromptMonitoring + 
+    AK8PFJet400_TrimMass30_PromptMonitoring + 
+    AK8PFJet420_TrimMass30_PromptMonitoring
 )
 
 
