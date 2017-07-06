5eab2efa190b7a1bc30876e40fdb19c5b279ff16
Minsuk Kim <minsuk@cern.ch>
2017-07-06 14:39:33 +0200

    Update SusyMonitoring

--- DQMOffline/Trigger/python/SusyMonitoring_cff.py
+++ DQMOffline/Trigger/python/SusyMonitoring_cff.py
@@ -1,7 +1,9 @@
 import FWCore.ParameterSet.Config as cms
 
 from DQMOffline.Trigger.RazorMonitor_cff import *
+from DQMOffline.Trigger.VBFSUSYMonitor_cff import *
 
 susyMonitorHLT = cms.Sequence(
     susyHLTRazorMonitoring
+    *susyHLTVBFMonitoring
 )
