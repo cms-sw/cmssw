5956c25d453a1477b1bc0c0b84210dd32ed330a5
Fabrizio <Fabrizio.Ferro@ge.infn.it>
2017-07-07 14:27:53 +0200

    renaming DDDTotemRPCommon to CTPPSDDDNames

---
+++ Geometry/VeryForwardGeometryBuilder/interface/CTPPSDDDNames.h
@@ -0,0 +1,22 @@
+/****************************************************************************
+*
+* Authors:
+*	Jan Kaspar (jan.kaspar@gmail.com) 
+*
+****************************************************************************/
+
+#ifndef Geometry_VeryForwardGeometryBuilder_CTPPSDDDNames_H
+#define Geometry_VeryForwardGeometryBuilder_CTPPSDDDNames_H
+
+
+/// DDD name of RP detector
+const char DDD_TOTEM_RP_DETECTOR_NAME[] = "RP_Silicon_Detector";
+
+/// DDD name of RP
+const char DDD_TOTEM_RP_PRIMARY_VACUUM_NAME[] = "RP_box_primary_vacuum";
+
+const char DDD_CTPPS_DIAMONDS_PLANE_NAME[] = "CTPPS_Diamond_Plane";
+const char DDD_CTPPS_DIAMONDS_DETECTOR_NAME[] = "CTPPS_Diamond_Segment";
+const char DDD_CTPPS_PIXELS_DETECTOR_NAME[] = "RPixWafer";
+
+#endif 
