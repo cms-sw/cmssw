d8f2d7d947446f2dcc2af47ee7899d6eaead8be3
Norraphat <srimanob@mail.cern.ch>
2017-07-07 10:58:08 +0200

    Cleanup monopole skimming code

--- Configuration/Skimming/python/PDWG_EXOMONOPOLE_cff.py
+++ Configuration/Skimming/python/PDWG_EXOMONOPOLE_cff.py
@@ -12,52 +12,20 @@ hltMonopole.HLTPaths = cms.vstring(
 hltMonopole.throw = False
 hltMonopole.andOr = True
 
-# selection of valid vertex
-#primaryVertexFilterForZMM = cms.EDFilter("VertexSelector",
-#    src = cms.InputTag("offlinePrimaryVertices"),
-#    cut = cms.string("!isFake && abs(z) <= 25 && position.Rho <= 2"), 
-#    filter = cms.bool(True),   # otherwise it won't filter the events
-#    )
-
-#Data
 from Configuration.EventContent.EventContent_cff import AODEventContent
-EXOMonopoleAODContent = AODEventContent.clone()
-# Barrel Cluster
-EXOMonopoleAODContent.outputCommands.append('keep *_hybridSuperClusters_*_*')
-# Endcap Cluster
-EXOMonopoleAODContent.outputCommands.append('keep *_multi5x5SuperClusters_multi5x5EndcapSuperClusters_*')
-EXOMonopoleAODContent.outputCommands.append('keep *_multi5x5SuperClusters_uncleanOnlyMulti5x5EndcapBasicClusters_*')
-EXOMonopoleAODContent.outputCommands.append('keep *_multi5x5SuperClusters_uncleanOnlyMulti5x5EndcapSuperClusters_*')
-#
-EXOMonopoleAODContent.outputCommands.append('keep *_siStripClusters_*_*')
-EXOMonopoleAODContent.outputCommands.append('keep *_siPixelClusters_*_*')
-EXOMonopoleAODContent.outputCommands.append('drop *_generalTracks_*_*')
-EXOMonopoleAODContent.outputCommands.append('keep *_generalTracks_*_*')
-EXOMonopoleAODContent.outputCommands.append('drop *_generalTracks_QualityMasks_*')
-EXOMonopoleAODContent.outputCommands.append('keep *_ecalRecHit_EcalRecHitsEB_*')
-EXOMonopoleAODContent.outputCommands.append('keep *_ecalRecHit_EcalRecHitsEE_*')
-EXOMonopoleAODContent.outputCommands.append('keep *_hbhereco_*_*')
-
-##MC
-#from Configuration.EventContent.EventContent_cff import AODSIMEventContent
-#EXOMonopoleAODSIMContent = AODSIMEventContent.clone()
-## Barrel Cluster
-#EXOMonopoleAODSIMContent.outputCommands.append('keep *_hybridSuperClusters_*_*')
-## Endcap Cluster
-#EXOMonopoleAODSIMContent.outputCommands.append('keep *_multi5x5SuperClusters_multi5x5EndcapSuperClusters_*')
-#EXOMonopoleAODSIMContent.outputCommands.append('keep *_multi5x5SuperClusters_uncleanOnlyMulti5x5EndcapBasicClusters_*')
-#EXOMonopoleAODSIMContent.outputCommands.append('keep *_multi5x5SuperClusters_uncleanOnlyMulti5x5EndcapSuperClusters_*')
-##
-#EXOMonopoleAODSIMContent.outputCommands.append('keep *_siStripClusters_*_*')
-#EXOMonopoleAODSIMContent.outputCommands.append('keep *_siPixelClusters_*_*')
-#EXOMonopoleAODSIMContent.outputCommands.append('drop *_generalTracks_*_*')
-#EXOMonopoleAODSIMContent.outputCommands.append('keep *_generalTracks_*_*')
-#EXOMonopoleAODSIMContent.outputCommands.append('drop *_generalTracks_QualityMasks_*')
-#EXOMonopoleAODSIMContent.outputCommands.append('keep *_ecalRecHit_EcalRecHitsEB_*')
-#EXOMonopoleAODSIMContent.outputCommands.append('keep *_ecalRecHit_EcalRecHitsEE_*')
-#EXOMonopoleAODSIMContent.outputCommands.append('keep *_hbhereco_*_*')
-#EXOMonopoleAODSIMContent.outputCommands.append('keep *_generatorSmeared_*_*')
-#EXOMonopoleAODSIMContent.outputCommands.append('keep *_g4SimHits_*_*')
+EXOMonopoleSkimContent = AODEventContent.clone()
+EXOMonopoleSkimContent.outputCommands.append('keep *_hybridSuperClusters_*_*')
+EXOMonopoleSkimContent.outputCommands.append('keep *_multi5x5SuperClusters_multi5x5EndcapSuperClusters_*')
+EXOMonopoleSkimContent.outputCommands.append('keep *_multi5x5SuperClusters_uncleanOnlyMulti5x5EndcapBasicClusters_*')
+EXOMonopoleSkimContent.outputCommands.append('keep *_multi5x5SuperClusters_uncleanOnlyMulti5x5EndcapSuperClusters_*')
+EXOMonopoleSkimContent.outputCommands.append('keep *_siStripClusters_*_*')
+EXOMonopoleSkimContent.outputCommands.append('keep *_siPixelClusters_*_*')
+EXOMonopoleSkimContent.outputCommands.append('drop *_generalTracks_*_*')
+EXOMonopoleSkimContent.outputCommands.append('keep *_generalTracks_*_*')
+EXOMonopoleSkimContent.outputCommands.append('drop *_generalTracks_QualityMasks_*')
+EXOMonopoleSkimContent.outputCommands.append('keep *_ecalRecHit_EcalRecHitsEB_*')
+EXOMonopoleSkimContent.outputCommands.append('keep *_ecalRecHit_EcalRecHitsEE_*')
+EXOMonopoleSkimContent.outputCommands.append('keep *_hbhereco_*_*')
 
 # monopole skim sequence
 EXOMonopoleSkimSequence = cms.Sequence(
