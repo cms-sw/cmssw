95584e63cd2ed3824db62ea350a31a114c3c8bf2
Vincenzo Innocente <vincenzo.innocente@cern.ch>
2017-07-07 11:08:01 +0200

    first cleanup

--- DQM/SiPixelPhase1TrackClusters/src/SiPixelPhase1TrackClusters.cc
+++ DQM/SiPixelPhase1TrackClusters/src/SiPixelPhase1TrackClusters.cc
@@ -26,19 +26,13 @@
 
 namespace {
 
-class SiPixelPhase1TrackClusters : public SiPixelPhase1Base {
+class SiPixelPhase1TrackClusters final : public SiPixelPhase1Base {
   enum {
     ONTRACK_CHARGE,
     ONTRACK_SIZE,
     ONTRACK_NCLUSTERS,
     ONTRACK_POSITION_B,
     ONTRACK_POSITION_F,
-
-    OFFTRACK_CHARGE,
-    OFFTRACK_SIZE,
-    OFFTRACK_NCLUSTERS,
-    OFFTRACK_POSITION_B,
-    OFFTRACK_POSITION_F,
 
     NTRACKS,
     NTRACKS_VOLUME,
@@ -51,40 +45,46 @@ public:
   void analyze(const edm::Event&, const edm::EventSetup&) override;
 
 private:
-  edm::EDGetTokenT<edmNew::DetSetVector<SiPixelCluster> > clustersToken_;
-  edm::EDGetTokenT<reco::TrackCollection> tracksToken_;
-  edm::EDGetTokenT<reco::VertexCollection> offlinePrimaryVerticesToken_;
+  const bool applyVertexCut_;
 
-  bool applyVertexCut_;
+  const edm::EDGetTokenT<edmNew::DetSetVector<SiPixelCluster> > clustersToken_;
+  const edm::EDGetTokenT<reco::TrackCollection> tracksToken_;
+  const edm::EDGetTokenT<reco::VertexCollection> offlinePrimaryVerticesToken_;
 };
 
 
 
 SiPixelPhase1TrackClusters::SiPixelPhase1TrackClusters(const edm::ParameterSet& iConfig) :
-  SiPixelPhase1Base(iConfig) 
-{
-  clustersToken_ = consumes<edmNew::DetSetVector<SiPixelCluster>>(iConfig.getParameter<edm::InputTag>("clusters"));
-
-  tracksToken_ = consumes<reco::TrackCollection>(iConfig.getParameter<edm::InputTag>("tracks"));
-
-  offlinePrimaryVerticesToken_ = consumes<reco::VertexCollection>(std::string("offlinePrimaryVertices"));
-
-  applyVertexCut_=iConfig.getUntrackedParameter<bool>("VertexCut",true);
-}
+  SiPixelPhase1Base(iConfig),
+  applyVertexCut_(iConfig.getUntrackedParameter<bool>("VertexCut",true)),
+  tracksToken_(consumes<reco::TrackCollection>(iConfig.getParameter<edm::InputTag>("tracks"))),
+  offlinePrimaryVerticesToken_(applyVertexCut_ ?
+                              consumes<reco::VertexCollection>(std::string("offlinePrimaryVertices")) :
+                              edm::EDGetTokenT<reco::VertexCollection>())
+{}
 
 void SiPixelPhase1TrackClusters::analyze(const edm::Event& iEvent, const edm::EventSetup& iSetup) {
 
-  if (! (histo.size() > ONTRACK_SIZE_VS_ETA)) return;
+  if (histo.size() != ONTRACK_SIZE_VS_ETA+1) {
+    edm::LogError("SiPixelPhase1TrackClusters") << "incompatible configuration " << histo.size()
+         << '<' << ONTRACK_SIZE_VS_ETA+1 << std::endl;
+    return;
+  }
 
   // get geometry
   edm::ESHandle<TrackerGeometry> tracker;
   iSetup.get<TrackerDigiGeometryRecord>().get(tracker);
   assert(tracker.isValid());
-  
-  edm::Handle<reco::VertexCollection> vertices;
-  iEvent.getByToken(offlinePrimaryVerticesToken_, vertices);
 
-  if (applyVertexCut_ && (!vertices.isValid() || vertices->size() == 0)) return;
+   edm::ESHandle<TrackerTopology> tTopoHandle;
+   iSetup.get<TrackerTopologyRcd>().get(tTopoHandle);
+   // auto const & tkTpl = *tTopoHandle;
+
+  edm::Handle<reco::VertexCollection> vertices;
+  if(applyVertexCut_) {
+    iEvent.getByToken(offlinePrimaryVerticesToken_, vertices);
+    if (!vertices.isValid() || vertices->empty()) return;
+  }
 
   //get the map
   edm::Handle<reco::TrackCollection> tracks;
@@ -95,32 +95,18 @@ void SiPixelPhase1TrackClusters::analyze(const edm::Event& iEvent, const edm::Ev
     return;
   }
   
-  // get clusters
-  edm::Handle< edmNew::DetSetVector<SiPixelCluster> >  clusterColl;
-  iEvent.getByToken( clustersToken_, clusterColl );
-
-  if ( !clusterColl.isValid() ) {
-    edm::LogWarning("SiPixelPhase1TrackClusters")  << "pixel cluster collection is not valid";
-    return;
-  }
-  
-  // we need to store some per-cluster data. Instead of a map, we use a vector,
-  // exploiting the fact that all custers live in the DetSetVector and we can 
-  // use the same indices to refer to them.
-  // corr_charge is not strictly needed but cleaner to have it.
-  std::vector<bool>  ontrack    (clusterColl->data().size(), false);
-  std::vector<float> corr_charge(clusterColl->data().size(), -1.0f);
-  std::vector<float> etatk(clusterColl->data().size(), -1.0f);
-
   for (auto const & track : *tracks) {
 
-    if (applyVertexCut_ && (track.pt() < 0.75 || std::abs( track.dxy(vertices->at(0).position()) ) > 5*track.dxyError())) continue;
+    if (applyVertexCut_ && (track.pt() < 0.75 || std::abs( track.dxy((*vertices)[0].position()) ) > 5*track.dxyError())) continue;
 
     bool isBpixtrack = false, isFpixtrack = false, crossesPixVol=false;
 
     // find out whether track crosses pixel fiducial volume (for cosmic tracks)
-    double d0 = track.d0(), dz = track.dz(); 
+    auto d0 = track.d0(), dz = track.dz(); 
     if(std::abs(d0)<15 && std::abs(dz)<50) crossesPixVol = true;
+
+    auto etatk = track.eta();
+
 
     auto const & trajParams = track.extra()->trajParams();
     assert(trajParams.size()==track.recHitsSize());
@@ -128,33 +114,39 @@ void SiPixelPhase1TrackClusters::analyze(const edm::Event& iEvent, const edm::Ev
     for(unsigned int h=0;h<track.recHitsSize();h++){
       auto hit = *(hb+h);
       if (!hit->isValid()) continue;
-      DetId id = hit->geographicalId();
+      auto id = hit->geographicalId();
 
       // check that we are in the pixel
-      uint32_t subdetid = (id.subdetId());
+      auto subdetid = (id.subdetId());
       if (subdetid == PixelSubdetector::PixelBarrel) isBpixtrack = true;
       if (subdetid == PixelSubdetector::PixelEndcap) isFpixtrack = true;
       if (subdetid != PixelSubdetector::PixelBarrel && subdetid != PixelSubdetector::PixelEndcap) continue;
       auto pixhit = dynamic_cast<const SiPixelRecHit*>(hit->hit());
       if (!pixhit) continue;
-        
-      // get the cluster
-      auto clust = pixhit->cluster();
-      if (clust.isNull()) continue; 
-      ontrack[clust.key()] = true; // mark cluster as ontrack
 
+      // auto geomdetunit = dynamic_cast<const PixelGeomDetUnit*> (pixhit->detUnit());
+      // auto const & topol = geomdetunit->specificTopology();
+      
+      // get the cluster
+      auto clustp = pixhit->cluster();
+      if (clustp.isNull()) continue; 
+      auto const & cluster = *clustp;
+
+      auto const & ltp = trajParams[h];
+      
+      // LocalVector localDir = ltp.momentum()/ltp.momentum().mag();
 
       // correct charge for track impact angle
-      auto const & ltp = trajParams[h];
-      LocalVector localDir = ltp.momentum()/ltp.momentum().mag();
-     
-      float clust_alpha = atan2(localDir.z(), localDir.x());
-      float clust_beta  = atan2(localDir.z(), localDir.y());
-      double corrCharge = clust->charge() * sqrt( 1.0 / ( 1.0/pow( tan(clust_alpha), 2 ) + 
-                                                          1.0/pow( tan(clust_beta ), 2 ) + 
-                                                          1.0 ));
-      corr_charge[clust.key()] = (float) corrCharge;
-      etatk[clust.key()]=(float) track.eta();
+      auto charge = cluster.charge()*ltp.absdz();
+
+      auto clustgp =  pixhit->globalPosition();  // from rechit
+
+      histo[ONTRACK_NCLUSTERS ].fill(id, &iEvent);
+      histo[ONTRACK_CHARGE    ].fill(charge, id, &iEvent);
+      histo[ONTRACK_SIZE	].fill(cluster.size(), id, &iEvent);
+      histo[ONTRACK_POSITION_B].fill(clustgp.z(),   clustgp.phi(),   id, &iEvent);
+      histo[ONTRACK_POSITION_F].fill(clustgp.x(),   clustgp.y(),     id, &iEvent);
+      histo[ONTRACK_SIZE_VS_ETA].fill(etatk, cluster.sizeY(), id, &iEvent);
     }
 
     // statistics on tracks
@@ -174,42 +166,7 @@ void SiPixelPhase1TrackClusters::analyze(const edm::Event& iEvent, const edm::Ev
     }
   }
 
-  edmNew::DetSetVector<SiPixelCluster>::const_iterator it;
-  for (it = clusterColl->begin(); it != clusterColl->end(); ++it) {
-    auto id = DetId(it->detId());
-
-    const PixelGeomDetUnit* geomdetunit = dynamic_cast<const PixelGeomDetUnit*> ( tracker->idToDet(id) );
-    const PixelTopology& topol = geomdetunit->specificTopology();
-
-    for(auto subit = it->begin(); subit != it->end(); ++subit) {
-      // we could do subit-...->data().front() as well, but this seems cleaner.
-      auto key = edmNew::makeRefTo(clusterColl, subit).key(); 
-      bool is_ontrack = ontrack[key];
-      float corrected_charge = corr_charge[key];
-      SiPixelCluster const& cluster = *subit;
-
-      LocalPoint clustlp = topol.localPosition(MeasurementPoint(cluster.x(), cluster.y()));
-      GlobalPoint clustgp = geomdetunit->surface().toGlobal(clustlp);
-
-      if (is_ontrack) {
-        histo[ONTRACK_NCLUSTERS ].fill(id, &iEvent);
-        histo[ONTRACK_CHARGE    ].fill(double(corrected_charge), id, &iEvent);
-        histo[ONTRACK_SIZE      ].fill(double(cluster.size()  ), id, &iEvent);
-        histo[ONTRACK_POSITION_B].fill(clustgp.z(),   clustgp.phi(),   id, &iEvent);
-        histo[ONTRACK_POSITION_F].fill(clustgp.x(),   clustgp.y(),     id, &iEvent);
-	histo[ONTRACK_SIZE_VS_ETA].fill(etatk[key], cluster.sizeY(), id, &iEvent);
-      } else {
-        histo[OFFTRACK_NCLUSTERS ].fill(id, &iEvent);
-        histo[OFFTRACK_CHARGE    ].fill(double(cluster.charge()), id, &iEvent);
-        histo[OFFTRACK_SIZE      ].fill(double(cluster.size()  ), id, &iEvent);
-        histo[OFFTRACK_POSITION_B].fill(clustgp.z(),   clustgp.phi(),   id, &iEvent);
-        histo[OFFTRACK_POSITION_F].fill(clustgp.x(),   clustgp.y(),     id, &iEvent);
-      }
-    }
-  }
-
   histo[ONTRACK_NCLUSTERS].executePerEventHarvesting(&iEvent);
-  histo[OFFTRACK_NCLUSTERS].executePerEventHarvesting(&iEvent);
 }
 
 }// namespace
