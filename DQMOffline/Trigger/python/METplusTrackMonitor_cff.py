cdce30d010f44c69a549aae37906caa10dd6cec2
bpf6qc <bfrancis@gmail.com>
2017-07-10 17:39:16 -0400

    Get online MET from TriggerEvent instead of transient collections, use TriggerDQMBase

--- DQMOffline/Trigger/python/METplusTrackMonitor_cff.py
+++ DQMOffline/Trigger/python/METplusTrackMonitor_cff.py
@@ -5,16 +5,16 @@ from DQMOffline.Trigger.METplusTrackMonitor_cfi import hltMETplusTrackMonitoring
 # HLT_MET105_IsoTrk50
 MET105_IsoTrk50monitoring = hltMETplusTrackMonitoring.clone()
 MET105_IsoTrk50monitoring.FolderName = cms.string('HLT/MET/MET105_IsoTrk50/')
-MET105_IsoTrk50monitoring.numGenericTriggerEventPSet.hltPaths = cms.vstring("HLT_MET105_IsoTrk50_v")
-MET105_IsoTrk50monitoring.hltMetSelection = cms.string("pt > 105")
-MET105_IsoTrk50monitoring.hltMetCleanSelection = cms.string("pt > 65")
+MET105_IsoTrk50monitoring.numGenericTriggerEventPSet.hltPaths = cms.vstring("HLT_MET105_IsoTrk50_v*")
+MET105_IsoTrk50monitoring.hltMetFilter = cms.InputTag('hltMET105', '', 'HLT')
+MET105_IsoTrk50monitoring.hltMetCleanFilter = cms.InputTag('hltMETClean65', '', 'HLT')
 
 # HLT_MET120_IsoTrk50
 MET120_IsoTrk50monitoring = hltMETplusTrackMonitoring.clone()
 MET120_IsoTrk50monitoring.FolderName = cms.string('HLT/MET/MET120_IsoTrk50/')
-MET120_IsoTrk50monitoring.numGenericTriggerEventPSet.hltPaths = cms.vstring("HLT_MET120_IsoTrk50_v")
-MET105_IsoTrk50monitoring.hltMetSelection = cms.string("pt > 120")
-MET105_IsoTrk50monitoring.hltMetCleanSelection = cms.string("pt > 65")
+MET120_IsoTrk50monitoring.numGenericTriggerEventPSet.hltPaths = cms.vstring("HLT_MET120_IsoTrk50_v*")
+MET120_IsoTrk50monitoring.hltMetFilter = cms.InputTag('hltMET120', '', 'HLT')
+MET120_IsoTrk50monitoring.hltMetCleanFilter = cms.InputTag('hltMETClean65', '', 'HLT')
 
 exoHLTMETplusTrackMonitoring = cms.Sequence(
     MET105_IsoTrk50monitoring
