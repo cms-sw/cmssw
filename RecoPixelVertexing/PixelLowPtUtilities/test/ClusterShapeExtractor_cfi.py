b6b25d704c445b82ef1430dd12641e6202284e25
Vincenzo Innocente <vincenzo.innocente@cern.ch>
2017-07-10 17:34:58 +0200

    introduce a new faster concurrent extractor

--- RecoPixelVertexing/PixelLowPtUtilities/test/ClusterShapeExtractor_cfi.py
+++ RecoPixelVertexing/PixelLowPtUtilities/test/ClusterShapeExtractor_cfi.py
@@ -1,11 +1,12 @@
 import FWCore.ParameterSet.Config as cms
 
-clusterShapeExtractor = cms.EDAnalyzer("ClusterShapeExtractor",
-    trackProducer  = cms.string('allTracks'),
+clusterShapeExtractor = cms.EDAnalyzer("PixelClusterShapeExtractor",
+    tracks = cms.InputTag("generalTracks"),
     clusterShapeCacheSrc = cms.InputTag('siPixelClusterShapeCache'),
     pixelSimLinkSrc = cms.InputTag('simSiPixelDigis', 'Pixel'),
     hasSimHits     = cms.bool(True),
     hasRecTracks   = cms.bool(False),
+# for the associator
     associateStrip      = cms.bool(False),
     associatePixel      = cms.bool(True),
     associateRecoTracks = cms.bool(False),
