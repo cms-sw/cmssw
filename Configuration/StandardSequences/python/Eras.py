7ecc5733dce4714d4e38cb749d61ff61ef1d51c4
Kevin Pedro <kpedro88@gmail.com>
2017-07-12 12:02:44 -0500

    introduce hcalSkipPacker Era and HcalRawToDigiFake

--- Configuration/StandardSequences/python/Eras.py
+++ Configuration/StandardSequences/python/Eras.py
@@ -42,7 +42,7 @@ class Eras (object):
                            'phase2_timing_layer','phase2_hcal',
                            'trackingLowPU', 'trackingPhase1', 'trackingPhase1QuadProp', 'ctpps_2016', 'trackingPhase2PU140',
                            'tracker_apv_vfp30_2016', 'run2_miniAOD_80XLegacy',
-                           'hcalHardcodeConditions']
+                           'hcalHardcodeConditions', 'hcalSkipPacker']
         internalUseModChains = ['run2_2017_noTrackingModifier']
 
 
