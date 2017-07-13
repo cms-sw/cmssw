4af95edcd5fc0c831686a32ef210c0570b862a52
Jan Kaspar <jan.kaspar@cern.ch>
2017-07-13 17:14:41 +0200

    Standardised names.

--- Geometry/VeryForwardGeometryBuilder/interface/CTPPSDDDNames.h
+++ Geometry/VeryForwardGeometryBuilder/interface/CTPPSDDDNames.h
@@ -9,14 +9,16 @@
 #define Geometry_VeryForwardGeometryBuilder_CTPPSDDDNames_H
 
 
-/// DDD name of RP detector
-const char DDD_TOTEM_RP_DETECTOR_NAME[] = "RP_Silicon_Detector";
+/// DDD names of sensors
+const char DDD_TOTEM_RP_SENSOR_NAME[] = "RP_Silicon_Detector";
+const char DDD_CTPPS_PIXELS_SENSOR_NAME[] = "RPixWafer";
+//const char DDD_CTPPS_DIAMONDS_SENSOR_NAME[] = "CTPPS_Diamond_Plane";
+const char DDD_CTPPS_DIAMONDS_SEGMENT_NAME[] = "CTPPS_Diamond_Segment";
 
-/// DDD name of RP
-const char DDD_TOTEM_RP_PRIMARY_VACUUM_NAME[] = "RP_box_primary_vacuum";
 
-const char DDD_CTPPS_DIAMONDS_PLANE_NAME[] = "CTPPS_Diamond_Plane";
-const char DDD_CTPPS_DIAMONDS_DETECTOR_NAME[] = "CTPPS_Diamond_Segment";
-const char DDD_CTPPS_PIXELS_DETECTOR_NAME[] = "RPixWafer";
+/// DDD names of RP volumes
+const char DDD_TOTEM_RP_RP_NAME[] = "RP_box_primary_vacuum";
+const char DDD_CTPPS_PIXELS_RP_NAME[] = "RP_box_primary_vacuum"; // TODO
+const char DDD_CTPPS_DIAMONDS_RP_NAME[] = "CTPPS_Diamond_Main_Box";
 
 #endif 
