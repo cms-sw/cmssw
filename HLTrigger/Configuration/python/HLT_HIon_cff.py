a83586a546e511ad94c98bec4258445c29fa4969
silviodonato <silvio.donato@cern.ch>
2017-07-13 22:00:49 +0200

    move to HLT V2 menu (/dev/CMSSW_9_2_0/HLT/V274)

--- HLTrigger/Configuration/python/HLT_HIon_cff.py
+++ HLTrigger/Configuration/python/HLT_HIon_cff.py
@@ -1,11 +1,11 @@
-# /dev/CMSSW_9_2_0/HIon/V73 (CMSSW_9_2_5_patch1)
+# /dev/CMSSW_9_2_0/HIon/V79 (CMSSW_9_2_5_patch1)
 
 import FWCore.ParameterSet.Config as cms
 
 fragment = cms.ProcessFragment( "HLT" )
 
 fragment.HLTConfigVersion = cms.PSet(
-  tableName = cms.string('/dev/CMSSW_9_2_0/HIon/V73')
+  tableName = cms.string('/dev/CMSSW_9_2_0/HIon/V79')
 )
 
 fragment.transferSystem = cms.PSet( 
--- HLTrigger/Configuration/test/OnLine_HLT_HIon.py
+++ HLTrigger/Configuration/test/OnLine_HLT_HIon.py
@@ -1,11 +1,11 @@
-# /dev/CMSSW_9_2_0/HIon/V73 (CMSSW_9_2_5_patch1)
+# /dev/CMSSW_9_2_0/HIon/V79 (CMSSW_9_2_5_patch1)
 
 import FWCore.ParameterSet.Config as cms
 
 process = cms.Process( "HLTHIon" )
 
 process.HLTConfigVersion = cms.PSet(
-  tableName = cms.string('/dev/CMSSW_9_2_0/HIon/V73')
+  tableName = cms.string('/dev/CMSSW_9_2_0/HIon/V79')
 )
 
 process.transferSystem = cms.PSet( 
