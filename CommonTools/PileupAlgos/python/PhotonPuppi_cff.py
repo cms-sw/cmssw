633ea8f27aff3b44f76f01ab3f5fbcf9fb22bc17
Andreas Hinzmann <hinzmann@cern.ch>
2017-07-13 16:44:21 +0200

    remove puppi tails

--- CommonTools/PileupAlgos/python/PhotonPuppi_cff.py
+++ CommonTools/PileupAlgos/python/PhotonPuppi_cff.py
@@ -6,10 +6,11 @@ puppiPhoton = cms.EDProducer("PuppiPhoton",
                              puppiCandName  = cms.InputTag('puppi'),
                              photonName     = cms.InputTag('reducedEgamma','reducedGedPhotons'),
 			     recoToPFMap    = cms.InputTag("reducedEgamma","reducedPhotonPfCandMap"),
-                             photonId       = cms.InputTag("egmPhotonIDs:cutBasedPhotonID-Spring16-V2p2-loose"), 
-                             pt             = cms.double(10),
+                             photonId       = cms.InputTag(""), # Add once available and validated (Note that Spring16 cut based photon ID fails for high pT photons in 93X)
+                             pt             = cms.double(20),
                              eta            = cms.double(2.5),
-			     runOnMiniAOD   = cms.bool(False),
+                             usePFphotons   = cms.bool(True), # Use PF photons and not GED photons
+			     runOnMiniAOD   = cms.bool(False), # If set to true photon ID is taken from pat::Photon, otherwise from the value map at RECO/AOD level
                              useRefs        = cms.bool(True),
                              dRMatch        = cms.vdouble(0.005,0.005,0.005,0.005),
                              pdgids         = cms.vint32 (22,11,211,130),
@@ -20,14 +21,14 @@ puppiPhoton = cms.EDProducer("PuppiPhoton",
 
 
 def setupPuppiPhoton(process):
-    my_id_modules = ['RecoEgamma.PhotonIdentification.Identification.cutBasedPhotonID_Spring16_V2p2_cff']
+    my_id_modules = [] # Add photon ID used by puppiPhoton producer here
     switchOnVIDPhotonIdProducer(process, DataFormat.AOD)
     for idmod in my_id_modules:
         setupAllVIDIdsInModule(process,idmod,setupVIDPhotonSelection)
 
 
 def setupPuppiPhotonMiniAOD(process):
-    my_id_modules = ['RecoEgamma.PhotonIdentification.Identification.cutBasedPhotonID_Spring16_V2p2_cff']
+    my_id_modules = [] # Add photon ID used by puppiPhoton producer here
     switchOnVIDPhotonIdProducer(process, DataFormat.MiniAOD)
     for idmod in my_id_modules:
         setupAllVIDIdsInModule(process,idmod,setupVIDPhotonSelection)
