d69adf316bacfbdb34e72eca21a0d607592604e1
Jan Kaspar <jan.kaspar@cern.ch>
2017-07-13 17:17:36 +0200

    Uses standardised DDD names, processes sensors and strips.

--- Geometry/VeryForwardGeometryBuilder/src/DDDTotemRPConstruction.cc
+++ Geometry/VeryForwardGeometryBuilder/src/DDDTotemRPConstruction.cc
@@ -59,13 +59,13 @@ void DDDTotemRPContruction::buildDetGeomDesc(DDFilteredView *fv, DetGeomDesc *gd
     // create new DetGeomDesc node and add it to the parent's (gd) list
     DetGeomDesc* newGD = new DetGeomDesc(fv);
 
-    // add ID (only for detectors/sensors)
-    if (fv->logicalPart().name().name().compare(DDD_TOTEM_RP_DETECTOR_NAME) == 0)
+    // strip sensors
+    if (fv->logicalPart().name().name().compare(DDD_TOTEM_RP_SENSOR_NAME) == 0)
     {
       const vector<int> &cN = fv->copyNumbers();
       // check size of copy numubers array
       if (cN.size() < 3)
-        throw cms::Exception("DDDTotemRPContruction") << "size of copyNumbers for RP_Silicon_Detector is "
+        throw cms::Exception("DDDTotemRPContruction") << "size of copyNumbers for strip sensor is "
           << cN.size() << ". It must be >= 3." << endl;
 
       // extract information
@@ -77,14 +77,24 @@ void DDDTotemRPContruction::buildDetGeomDesc(DDFilteredView *fv, DetGeomDesc *gd
       newGD->setGeographicalID(TotemRPDetId(arm, station, rp, detector));
     }
 
-// for 3d pixels
-    if (fv->logicalPart().name().name().compare(DDD_CTPPS_PIXELS_DETECTOR_NAME) == 0)
+    // strip RPs
+    if (fv->logicalPart().name().name().compare(DDD_TOTEM_RP_RP_NAME) == 0)
     {
+      const uint32_t decRPId = fv->copyno();
+      const uint32_t armIdx = (decRPId / 100) % 10;
+      const uint32_t stIdx = (decRPId / 10) % 10;
+      const uint32_t rpIdx = decRPId % 10;
 
-     const vector<int> &cN = fv->copyNumbers();
+      newGD->setGeographicalID(TotemRPDetId(armIdx, stIdx, rpIdx));
+    }
+
+    // pixel sensors
+    if (fv->logicalPart().name().name().compare(DDD_CTPPS_PIXELS_SENSOR_NAME) == 0)
+    {
+      const vector<int> &cN = fv->copyNumbers();
       // check size of copy numubers array
-     if (cN.size() < 4)
-        throw cms::Exception("DDDTotemRPContruction") << "size of copyNumbers for Wafer is "
+      if (cN.size() < 4)
+        throw cms::Exception("DDDTotemRPContruction") << "size of copyNumbers for pixel sensor is "
           << cN.size() << ". It must be >= 4." << endl;
 
       // extract information
@@ -92,23 +102,18 @@ void DDDTotemRPContruction::buildDetGeomDesc(DDFilteredView *fv, DetGeomDesc *gd
       const unsigned int arm = A / 100;
       const unsigned int station = (A % 100) / 10;
       const unsigned int rp = A % 10;
-      const unsigned int detector = cN[cN.size() - 2]-1;
+      const unsigned int detector = cN[cN.size() - 2] - 1;
       newGD->setGeographicalID(CTPPSPixelDetId(arm, station, rp, detector));
     }
 
-    if (fv->logicalPart().name().name().compare(DDD_TOTEM_RP_PRIMARY_VACUUM_NAME) == 0)
-    {
-      const uint32_t decRPId = fv->copyno();
-      const uint32_t armIdx = (decRPId / 100) % 10;
-      const uint32_t stIdx = (decRPId / 10) % 10;
-      const uint32_t rpIdx = decRPId % 10;
-      
-      newGD->setGeographicalID(TotemRPDetId(armIdx, stIdx, rpIdx));
-    }
+    // pixel RPs
+    // TODO
 
-    if (fv->logicalPart().name().name().compare(DDD_CTPPS_DIAMONDS_DETECTOR_NAME) == 0)
+    // diamond sensors
+    if (fv->logicalPart().name().name().compare(DDD_CTPPS_DIAMONDS_SEGMENT_NAME) == 0)
     {
       const vector<int>& copy_num = fv->copyNumbers();
+
       const unsigned int id = copy_num[copy_num.size()-1],
                          arm = copy_num[1]-1,
                          station = 1,
@@ -118,6 +123,24 @@ void DDDTotemRPContruction::buildDetGeomDesc(DDFilteredView *fv, DetGeomDesc *gd
       newGD->setGeographicalID( CTPPSDiamondDetId( arm, station, rp, plane, channel ) );
     }
 
+    // diamond RPs
+    if (fv->logicalPart().name().name().compare(DDD_CTPPS_DIAMONDS_RP_NAME) == 0)
+    {
+      const vector<int>& copy_num = fv->copyNumbers();
+
+      // check size of copy numubers array
+      if (copy_num.size() < 2)
+        throw cms::Exception("DDDTotemRPContruction") << "size of copyNumbers for diamond RP is "
+          << copy_num.size() << ". It must be >= 2." << endl;
+
+      const unsigned int arm = copy_num[1] - 1;
+      const unsigned int station = 1;
+      const unsigned int rp = 6;
+
+      newGD->setGeographicalID(CTPPSDiamondDetId(arm, station, rp));
+    }
+
+    // add component
     gd->addComponent(newGD);
 
     // recursion
