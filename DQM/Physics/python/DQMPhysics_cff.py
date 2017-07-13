0305eff96b59887b8e3a8a5bfe96ca646bb9ac8f
Kevin Pedro <kpedro88@gmail.com>
2017-07-13 14:31:15 -0500

    remove errant Eras.py imports

--- DQM/Physics/python/DQMPhysics_cff.py
+++ DQM/Physics/python/DQMPhysics_cff.py
@@ -41,10 +41,10 @@ phase1Pixel.toReplaceWith(dqmPhysics, dqmPhysics.copyAndExclude([ # FIXME
     ewkElecDQM,          # Excessive printouts because 2017 doesn't have HLT yet
     ewkMuLumiMonitorDQM, # Excessive printouts because 2017 doesn't have HLT yet
 ]))
-from Configuration.StandardSequences.Eras import eras
+from Configuration.Eras.Modifier_pA_2016_cff import pA_2016
 dqmPhysicspA  =  dqmPhysics.copy()
 dqmPhysicspA += CentralitypADQM
-eras.pA_2016.toReplaceWith(dqmPhysics, dqmPhysicspA)
+pA_2016.toReplaceWith(dqmPhysics, dqmPhysicspA)
 
 bphysicsOniaDQMHI = bphysicsOniaDQM.clone(vertex=cms.InputTag("hiSelectedVertex"))
 dqmPhysicsHI = cms.Sequence(bphysicsOniaDQMHI+CentralityDQM)
--- DQMOffline/L1Trigger/python/L1TriggerDqmOffline_cff.py
+++ DQMOffline/L1Trigger/python/L1TriggerDqmOffline_cff.py
@@ -268,7 +268,6 @@ from DQMOffline.L1Trigger.L1TEGammaOffline_cfi import *
 l1tEGammaOfflineDQMEmu.stage2CaloLayer2EGammaSource=cms.InputTag("valCaloStage2Layer2Digis")
 from DQMOffline.L1Trigger.L1TEfficiencyMuons_Offline_cfi import *
 
-from Configuration.StandardSequences.Eras import eras
 from DQM.L1TMonitor.L1TStage2_cff import *
 from DQMOffline.L1Trigger.L1TriggerDqmOffline_SecondStep_cff import *
 from DQMOffline.L1Trigger.L1TEfficiencyHarvesting_cfi import *
