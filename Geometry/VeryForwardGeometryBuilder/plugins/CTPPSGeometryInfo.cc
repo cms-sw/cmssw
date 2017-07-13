58f508b6331c63797f0b5b5a9ce8565fbbf4a7f0
Jan Kaspar <jan.kaspar@cern.ch>
2017-07-13 15:57:43 +0200

    Updated for new CTPPSDetId scheme, printf replaced with cout.

--- Geometry/VeryForwardGeometryBuilder/plugins/CTPPSGeometryInfo.cc
+++ Geometry/VeryForwardGeometryBuilder/plugins/CTPPSGeometryInfo.cc
@@ -19,92 +19,49 @@
 #include "Geometry/Records/interface/VeryForwardMisalignedGeometryRecord.h"
 #include "Geometry/VeryForwardGeometryBuilder/interface/TotemRPGeometry.h"
 
+#include "DataFormats/CTPPSDetId/interface/CTPPSDetId.h"
+#include "DataFormats/CTPPSDetId/interface/TotemRPDetId.h"
+#include "DataFormats/CTPPSDetId/interface/CTPPSPixelDetId.h"
+#include "DataFormats/CTPPSDetId/interface/CTPPSDiamondDetId.h"
+
 //----------------------------------------------------------------------------------------------------
 
 /**
- * \ingroup TotemRPGeometry
  * \brief Class to print out information on current geometry.
- *
- * See schema of \ref TotemRPGeometry "TOTEM RP geometry classes"
  **/
 class CTPPSGeometryInfo : public edm::one::EDAnalyzer<>
 {
   public:
     explicit CTPPSGeometryInfo(const edm::ParameterSet&);
-    ~CTPPSGeometryInfo();
-
-    struct MeanRPData
-    {
-      unsigned int N, N_u, N_v;
-      double Sx, Sy, Sz, Sdx_u, Sdx_v, Sdy_u, Sdy_v;
-    
-      MeanRPData() : N(0), N_u(0), N_v(0), Sx(0.), Sy(0.), Sz(0.), Sdx_u(0.), Sdx_v(0.), Sdy_u(0.), Sdy_v(0.) {}
-
-      void Fill(double x, double y, double z, bool uDir, double dx, double dy)
-      {
-        N++;
-        Sx += x;
-        Sy += y;
-        Sz += z;
-
-        if (uDir)
-        {
-          N_u++;
-          Sdx_u += dx;
-          Sdy_u += dy;
-        } else {
-          N_v++;
-          Sdx_v += dx;
-          Sdy_v += dy;
-        }
-      }
-    };
 
   private: 
     std::string geometryType;
 
-    bool printRPInfo, printSensorInfo, printMeanSensorInfo;
+    bool printRPInfo, printSensorInfo;
 
     edm::ESWatcher<IdealGeometryRecord> watcherIdealGeometry;
     edm::ESWatcher<VeryForwardRealGeometryRecord> watcherRealGeometry;
     edm::ESWatcher<VeryForwardMisalignedGeometryRecord> watcherMisalignedGeometry;
 
-    virtual void beginRun(edm::Run const&, edm::EventSetup const&);
-    virtual void analyze(const edm::Event&, const edm::EventSetup&);
-    virtual void endJob();
+    virtual void analyze(const edm::Event&, const edm::EventSetup&) override;
+
+    static void PrintDetId(const CTPPSDetId &id, bool printDetails = true);
 
     void PrintGeometry(const TotemRPGeometry &, const edm::Event&);
 };
 
+//----------------------------------------------------------------------------------------------------
+
 using namespace edm;
 using namespace std;
-
+using namespace CLHEP;
 
 //----------------------------------------------------------------------------------------------------
 
 CTPPSGeometryInfo::CTPPSGeometryInfo(const edm::ParameterSet& ps) :
   geometryType(ps.getUntrackedParameter<string>("geometryType", "real")),
   printRPInfo(ps.getUntrackedParameter<bool>("printRPInfo", true)),
-  printSensorInfo(ps.getUntrackedParameter<bool>("printSensorInfo", true)),
-  printMeanSensorInfo(ps.getUntrackedParameter<bool>("printMeanSensorInfo", true))
-{
-}
-
-//----------------------------------------------------------------------------------------------------
-
-CTPPSGeometryInfo::~CTPPSGeometryInfo()
-{
-}
-
-//----------------------------------------------------------------------------------------------------
-
-void CTPPSGeometryInfo::beginRun(edm::Run const&, edm::EventSetup const& iSetup)
-{
-}
-
-//----------------------------------------------------------------------------------------------------
-
-void CTPPSGeometryInfo::endJob()
+  printSensorInfo(ps.getUntrackedParameter<bool>("printSensorInfo", true))
 {
 }
 
@@ -149,92 +106,97 @@ void CTPPSGeometryInfo::analyze(const edm::Event& event, const edm::EventSetup& 
 
 //----------------------------------------------------------------------------------------------------
 
+void CTPPSGeometryInfo::PrintDetId(const CTPPSDetId &id, bool printDetails)
+{
+  cout << id.rawId();
+
+  const unsigned int rpDecId = id.arm()*100 + id.station()*10 + id.rp();
+
+  if (id.subdetId() == CTPPSDetId::sdTrackingStrip)
+  {
+    TotemRPDetId fid(id);
+    cout << " (strip RP " << rpDecId;
+    if (printDetails)
+      cout <<  ", plane " << fid.plane();
+    cout << ")";
+  }
+
+  if (id.subdetId() == CTPPSDetId::sdTrackingPixel)
+  {
+    CTPPSPixelDetId fid(id);
+    cout << " (pixel RP " << rpDecId;
+    if (printDetails)
+      cout <<  ", plane " << fid.plane();
+    cout << ")";
+  }
+
+  if (id.subdetId() == CTPPSDetId::sdTimingDiamond)
+  {
+    CTPPSDiamondDetId fid(id);
+    cout << " (diamd RP " << rpDecId;
+    if (printDetails)
+      cout <<  ", plane " << fid.plane() << ", channel " << fid.channel();
+    cout << ")";
+  }
+
+}
+
+//----------------------------------------------------------------------------------------------------
+
 void CTPPSGeometryInfo::PrintGeometry(const TotemRPGeometry &geometry, const edm::Event& event)
 {
   time_t unixTime = event.time().unixTime();
   char timeStr[50];
   strftime(timeStr, 50, "%F %T", localtime(&unixTime));
-  printf(">> CTPPSGeometryInfo::PrintGeometry\n\tnew %s geometry found in run=%u, event=%llu, UNIX timestamp=%lu (%s)\n",
-    geometryType.c_str(), event.id().run(), event.id().event(), unixTime, timeStr);
+
+  cout << ">> CTPPSGeometryInfo::PrintGeometry > new " << geometryType << "geometry found in run="
+    << event.id().run() << ", event=" << event.id().event() << ", UNIX timestamp=" << unixTime
+    << " (" << timeStr << ")";
 
   // RP geometry
   if (printRPInfo)
   {
-    printf("\n* RPs:\n");
-    printf("  ID |   x (mm)    |   y (mm)    |   z  (m)  |\n");
-    for (TotemRPGeometry::RPDeviceMapType::const_iterator it = geometry.beginRP(); it != geometry.endRP(); ++it)
+    cout << endl << "* RPs:" << endl;
+    cout << "  ID |   center x (mm)    |   center y (mm)    |   center z  (m)" << endl;
+    for (auto it = geometry.beginRP(); it != geometry.endRP(); ++it)
     {
       const DDTranslation &t = it->second->translation();
-      printf(" %3i | %+11.3f | %+11.3f | %+9.4f |\n", it->first, t.x(), t.y(), t.z() * 1E-3);
+
+      PrintDetId(CTPPSDetId(it->first), false);
+      cout << " | " << fixed << setprecision(3) << t.x() << " | " << t.y() << " | " << t.z() * 1E-3 << endl;
     }
   }
   
   // sensor geometry
   if (printSensorInfo)
   {
-    printf("\n* silicon sensors:\n");
-    printf("DetId |            detector center           |  readout direction  |\n");
-    printf("      |   x (mm)   |   y (mm)   |   z  (m)   |    dx    |    dy    |\n");
-    for (TotemRPGeometry::mapType::const_iterator it = geometry.beginDet(); it != geometry.endDet(); ++it)
+    cout << endl << "* sensors:" << endl;
+    cout << "    center: sensor center in global coordinates, in mm" << endl;
+    cout << "    axis1: local axis (1, 0, 0) in global coordinates" << endl;
+    cout << "    axis2: local axis (0, 1, 0) in global coordinates" << endl;
+    cout << "    axis3: local axis (0, 0, 1) in global coordinates" << endl;
+
+    for (auto it = geometry.beginDet(); it != geometry.endDet(); ++it)
     {
-      TotemRPDetId id(it->first);
+      CTPPSDetId detId(it->first);
   
-      double x = it->second->translation().x();
-      double y = it->second->translation().y();
-      double z = it->second->translation().z() * 1E-3;
-  
-      double dx = 0., dy = 0.;
-      geometry.GetReadoutDirection(it->first, dx, dy);
-  
-      printf("%4i  |  %8.3f  |  %8.3f  |  %9.4f | %8.3f | %8.3f |\n", id.getPlaneDecimalId(), x, y, z, dx, dy);
-    }
-  }
-  
-  // sensor geometry averaged over 1 RP
-  if (printMeanSensorInfo)
-  {
-    printf("\n* average over silicon sensors of 1 RP:\n");
+      Hep3Vector gl_o = geometry.LocalToGlobal(detId, Hep3Vector(0, 0, 0));
+      Hep3Vector gl_a1 = geometry.LocalToGlobal(detId, Hep3Vector(1, 0, 0)) - gl_o;
+      Hep3Vector gl_a2 = geometry.LocalToGlobal(detId, Hep3Vector(0, 1, 0)) - gl_o;
+      Hep3Vector gl_a3 = geometry.LocalToGlobal(detId, Hep3Vector(0, 0, 1)) - gl_o;
 
-    map<unsigned int, MeanRPData> data; // map: RP Id --> sums of geometrical info
+      PrintDetId(detId);
 
-    for (TotemRPGeometry::mapType::const_iterator it = geometry.beginDet(); it != geometry.endDet(); ++it)
-    {
-      TotemRPDetId plId(it->first);
-      unsigned int rpDecId = plId.getRPDecimalId();
-      bool uDirection = plId.isStripsCoordinateUDirection();
-  
-      double x = it->second->translation().x();
-      double y = it->second->translation().y();
-      double z = it->second->translation().z() * 1E-3;
-  
-      double dx = 0., dy = 0.;
-      geometry.GetReadoutDirection(it->first, dx, dy);
-  
-      data[rpDecId].Fill(x, y, z, uDirection, dx, dy);
-    }
-
-    printf("RPId |                center                |      U direction    |      V direction    |\n");
-    printf("     |   x (mm)   |   y (mm)   |   z  (m)   |    dx    |    dy    |    dx    |    dy    |\n");
-    
-    for (map<unsigned int, MeanRPData>::iterator it = data.begin(); it != data.end(); ++it)
-    {
-      const MeanRPData &d = it->second;
-  
-      double mx = (d.N > 0) ? d.Sx / d.N : 0.;
-      double my = (d.N > 0) ? d.Sy / d.N : 0.;
-      double mz = (d.N > 0) ? d.Sz / d.N : 0.;
-
-      double mdx_u = (d.N_u > 0) ? d.Sdx_u / d.N_u : 0.;
-      double mdy_u = (d.N_u > 0) ? d.Sdy_u / d.N_u : 0.;
-
-      double mdx_v = (d.N_v > 0) ? d.Sdx_v / d.N_v : 0.;
-      double mdy_v = (d.N_v > 0) ? d.Sdy_v / d.N_v : 0.;
-
-      
-      printf(" %3i |  %8.3f  |  %8.3f  |  %9.4f | %8.3f | %8.3f | %8.3f | %8.3f |\n", it->first, mx, my, mz, mdx_u, mdy_u, mdx_v, mdy_v);
+      cout
+        << " | center=(" << gl_o.x() << ", " << gl_o.y() << ", " << gl_o.z() << ")"
+        << " | axis1=(" << gl_a1.x() << ", " << gl_a1.y() << ", " << gl_a1.z() << ")"
+        << " | axis2=(" << gl_a2.x() << ", " << gl_a2.y() << ", " << gl_a2.z() << ")"
+        << " | axis3=(" << gl_a3.x() << ", " << gl_a3.y() << ", " << gl_a3.z() << ")"
+        << endl;
     }
   }
 }
 
-DEFINE_FWK_MODULE(CTPPSGeometryInfo);
+//----------------------------------------------------------------------------------------------------
 
+DEFINE_FWK_MODULE(CTPPSGeometryInfo);
