a9e823c8b53ef0b9dea5677192042adb4a818562
Andrea Bocci <fwyzard@gmail.com>
2017-07-13 17:46:56 +0200

    cleaned up by clang-format 4.0.0

--- DQMOffline/Trigger/plugins/BPHMonitor.h
+++ DQMOffline/Trigger/plugins/BPHMonitor.h
@@ -1,59 +1,58 @@
-#ifndef BPHMONITOR_H
-#define BPHMONITOR_H
+#ifndef DQMOffline_Trigger_BPHMonitor_h
+#define DQMOffline_Trigger_BPHMonitor_h
 
+#include <map>
 #include <string>
 #include <vector>
-#include <map>
 
-#include "FWCore/Utilities/interface/EDGetToken.h"
-#include "FWCore/Framework/interface/Frameworkfwd.h"
-#include "FWCore/Framework/interface/Event.h"
-#include "FWCore/Framework/interface/MakerMacros.h"
-#include "FWCore/ParameterSet/interface/ParameterSet.h"
-#include "FWCore/ServiceRegistry/interface/Service.h"
-#include "DQMServices/Core/interface/MonitorElement.h"
-#include <DQMServices/Core/interface/DQMEDAnalyzer.h>
-#include "FWCore/Framework/interface/ESHandle.h"
-#include "FWCore/ParameterSet/interface/ParameterSetDescription.h"
-#include "FWCore/ParameterSet/interface/ConfigurationDescriptions.h"
-#include "FWCore/ParameterSet/interface/Registry.h"
-#include "MagneticField/Records/interface/IdealMagneticFieldRecord.h"
 #include "CommonTools/Utils/interface/StringCutObjectSelector.h"
-#include "DataFormats/Math/interface/deltaR.h"
-//DataFormats
+#include "DQMServices/Core/interface/DQMEDAnalyzer.h"
+#include "DQMServices/Core/interface/MonitorElement.h"
+#include "DataFormats/BeamSpot/interface/BeamSpot.h"
+#include "DataFormats/Common/interface/TriggerResults.h"
 #include "DataFormats/EgammaCandidates/interface/Photon.h"
-#include "DataFormats/EgammaCandidates/interface/PhotonFwd.h" 
-#include "DataFormats/VertexReco/interface/Vertex.h" 
-#include "DataFormats/VertexReco/interface/VertexFwd.h" 
-#include "DataFormats/MuonReco/interface/Muon.h"
-#include "DataFormats/MuonReco/interface/MuonFwd.h"
-#include "RecoVertex/VertexPrimitives/interface/TransientVertex.h"
-#include "RecoVertex/KalmanVertexFit/interface/KalmanVertexFitter.h"
-#include "TrackingTools/TransientTrack/interface/TransientTrack.h"
-#include "TrackingTools/PatternTools/interface/ClosestApproachInRPhi.h"
-#include "TrackingTools/TrajectoryState/interface/TrajectoryStateClosestToPoint.h"
-#include "DataFormats/TrackReco/interface/Track.h"
-#include "MagneticField/Engine/interface/MagneticField.h"
-#include "DataFormats/TrackReco/interface/TrackFwd.h"
-#include "DataFormats/TrackReco/interface/TrackExtra.h"
-#include "DataFormats/TrackReco/interface/TrackExtraFwd.h"
-
+#include "DataFormats/EgammaCandidates/interface/PhotonFwd.h"
 #include "DataFormats/HLTReco/interface/TriggerEvent.h"
 #include "DataFormats/HLTReco/interface/TriggerEventWithRefs.h"
-#include "DataFormats/Common/interface/TriggerResults.h"
 #include "DataFormats/HLTReco/interface/TriggerObject.h"
-#include "DataFormats/BeamSpot/interface/BeamSpot.h"
-
+#include "DataFormats/Math/interface/deltaR.h"
+#include "DataFormats/MuonReco/interface/Muon.h"
+#include "DataFormats/MuonReco/interface/MuonFwd.h"
+#include "DataFormats/TrackReco/interface/Track.h"
+#include "DataFormats/TrackReco/interface/TrackExtra.h"
+#include "DataFormats/TrackReco/interface/TrackExtraFwd.h"
+#include "DataFormats/TrackReco/interface/TrackFwd.h"
+#include "DataFormats/VertexReco/interface/Vertex.h"
+#include "DataFormats/VertexReco/interface/VertexFwd.h"
+#include "FWCore/Framework/interface/ESHandle.h"
+#include "FWCore/Framework/interface/Event.h"
+#include "FWCore/Framework/interface/Frameworkfwd.h"
+#include "FWCore/Framework/interface/MakerMacros.h"
+#include "FWCore/ParameterSet/interface/ConfigurationDescriptions.h"
+#include "FWCore/ParameterSet/interface/ParameterSet.h"
+#include "FWCore/ParameterSet/interface/ParameterSetDescription.h"
+#include "FWCore/ParameterSet/interface/Registry.h"
+#include "FWCore/ServiceRegistry/interface/Service.h"
+#include "FWCore/Utilities/interface/EDGetToken.h"
+#include "MagneticField/Engine/interface/MagneticField.h"
+#include "MagneticField/Records/interface/IdealMagneticFieldRecord.h"
+#include "RecoVertex/KalmanVertexFit/interface/KalmanVertexFitter.h"
+#include "RecoVertex/VertexPrimitives/interface/TransientVertex.h"
+#include "TrackingTools/PatternTools/interface/ClosestApproachInRPhi.h"
+#include "TrackingTools/TrajectoryState/interface/TrajectoryStateClosestToPoint.h"
+#include "TrackingTools/TransientTrack/interface/TransientTrack.h"
 
 class GenericTriggerEventFlag;
 
-struct MEbinning {
+struct MEbinning
+{
   int nbins;
   double xmin;
   double xmax;
 };
 
-struct  METME{
+struct METME
+{
   MonitorElement* numerator;
   MonitorElement* denominator;
 };
@@ -62,54 +61,51 @@ struct  METME{
 // class declaration
 //
 
-class BPHMonitor : public DQMEDAnalyzer 
+class BPHMonitor : public DQMEDAnalyzer
 {
 public:
-  BPHMonitor( const edm::ParameterSet& );
+  BPHMonitor(const edm::ParameterSet&);
   ~BPHMonitor();
-  static void fillDescriptions(edm::ConfigurationDescriptions & descriptions);
-  static void fillHistoPSetDescription(edm::ParameterSetDescription & pset);
-  static void fillHistoLSPSetDescription(edm::ParameterSetDescription & pset);
+  static void fillDescriptions(edm::ConfigurationDescriptions& descriptions);
+  static void fillHistoPSetDescription(edm::ParameterSetDescription& pset);
+  static void fillHistoLSPSetDescription(edm::ParameterSetDescription& pset);
 
 protected:
-
-  void bookHistograms(DQMStore::IBooker &, edm::Run const &, edm::EventSetup const &) override;
-  void bookME(DQMStore::IBooker &, METME& me, std::string& histname, std::string& histtitle, int& nbins, double& xmin, double& xmax);
-  void bookME(DQMStore::IBooker &, METME& me, std::string& histname, std::string& histtitle, std::vector<double> binningX);
-  void bookME(DQMStore::IBooker &, METME& me, std::string& histname, std::string& histtitle, int& nbinsX, double& xmin, double& xmax, double& ymin, double& ymax);
-  void bookME(DQMStore::IBooker &, METME& me, std::string& histname, std::string& histtitle, int& nbinsX, double& xmin, double& xmax, int& nbinsY, double& ymin, double& ymax);
-  void bookME(DQMStore::IBooker &, METME& me, std::string& histname, std::string& histtitle, std::vector<double> binningX, std::vector<double> binningY);
+  void bookHistograms(DQMStore::IBooker&, edm::Run const&, edm::EventSetup const&) override;
+  void bookME(DQMStore::IBooker&, METME& me, std::string& histname, std::string& histtitle, int& nbins, double& xmin, double& xmax);
+  void bookME(DQMStore::IBooker&, METME& me, std::string& histname, std::string& histtitle, std::vector<double> binningX);
+  void bookME(DQMStore::IBooker&, METME& me, std::string& histname, std::string& histtitle, int& nbinsX, double& xmin, double& xmax, double& ymin, double& ymax);
+  void bookME(DQMStore::IBooker&, METME& me, std::string& histname, std::string& histtitle, int& nbinsX, double& xmin, double& xmax, int& nbinsY, double& ymin, double& ymax);
+  void bookME(DQMStore::IBooker&, METME& me, std::string& histname, std::string& histtitle, std::vector<double> binningX, std::vector<double> binningY);
   void setMETitle(METME& me, std::string titleX, std::string titleY);
 
   void analyze(edm::Event const& iEvent, edm::EventSetup const& iSetup) override;
   template <typename T>
-  bool matchToTrigger(std::string theTriggerName,T t, edm::Handle<trigger::TriggerEvent> handleTriggerEvent);
-  //bool matchToTrigger(std::string theTriggerName,T t, edm::Handle<trigger::TriggerEventWithRefs> handleTriggerEvent);
-
+  bool matchToTrigger(std::string theTriggerName, T t, edm::Handle<trigger::TriggerEvent> handleTriggerEvent);
 
 private:
-  static MEbinning getHistoPSet    (edm::ParameterSet pset);
-  static MEbinning getHistoLSPSet  (edm::ParameterSet pset);
+  static MEbinning getHistoPSet(edm::ParameterSet pset);
+  static MEbinning getHistoLSPSet(edm::ParameterSet pset);
 
   std::string folderName_;
   std::string histoSuffix_;
 
-  edm::EDGetTokenT<reco::MuonCollection>        muoToken_;
-  edm::EDGetTokenT<reco::BeamSpot>        bsToken_;
-  edm::EDGetTokenT<reco::TrackCollection>       trToken_;
-  edm::EDGetTokenT<reco::PhotonCollection>       phToken_;
+  edm::EDGetTokenT<reco::MuonCollection> muoToken_;
+  edm::EDGetTokenT<reco::BeamSpot> bsToken_;
+  edm::EDGetTokenT<reco::TrackCollection> trToken_;
+  edm::EDGetTokenT<reco::PhotonCollection> phToken_;
 
-  MEbinning           phi_binning_;
-  MEbinning           pt_binning_;
-  MEbinning           eta_binning_;
-  MEbinning           d0_binning_;
-  MEbinning           z0_binning_;
-  MEbinning           dR_binning_;
-  MEbinning           mass_binning_;
-  MEbinning           dca_binning_;
-  MEbinning           ds_binning_;
-  MEbinning           cos_binning_;
-  MEbinning           prob_binning_;
+  MEbinning phi_binning_;
+  MEbinning pt_binning_;
+  MEbinning eta_binning_;
+  MEbinning d0_binning_;
+  MEbinning z0_binning_;
+  MEbinning dR_binning_;
+  MEbinning mass_binning_;
+  MEbinning dca_binning_;
+  MEbinning ds_binning_;
+  MEbinning cos_binning_;
+  MEbinning prob_binning_;
 
   METME muPhi_;
   METME muEta_;
@@ -133,30 +129,25 @@ private:
   METME mu3d0_;
   METME mu3z0_;
 
-
-///
-METME  phPhi_   ;
-METME  phEta_   ;
-METME  phPt_   ;
-METME  DiMuPhi_   ;
-METME  DiMuEta_   ;
-METME  DiMuPt_   ;
-METME  DiMuPVcos_   ;
-METME  DiMuProb_   ;
-METME  DiMuDS_   ;
-METME  DiMuDCA_   ;
-METME  DiMuMass_   ;
-METME  DiMudR_   ;
-
-
-//
+  METME phPhi_;
+  METME phEta_;
+  METME phPt_;
+  METME DiMuPhi_;
+  METME DiMuEta_;
+  METME DiMuPt_;
+  METME DiMuPVcos_;
+  METME DiMuProb_;
+  METME DiMuDS_;
+  METME DiMuDCA_;
+  METME DiMuMass_;
+  METME DiMudR_;
 
   GenericTriggerEventFlag* num_genTriggerEventFlag_;
   GenericTriggerEventFlag* den_genTriggerEventFlag_;
-  StringCutObjectSelector<reco::Muon,true>        muoSelection_;
-  StringCutObjectSelector<reco::Muon,true>        muoSelection_ref;
-  StringCutObjectSelector<reco::Muon,true>        muoSelection_tag;
-  StringCutObjectSelector<reco::Muon,true>        muoSelection_probe;
+  StringCutObjectSelector<reco::Muon, true> muoSelection_;
+  StringCutObjectSelector<reco::Muon, true> muoSelection_ref;
+  StringCutObjectSelector<reco::Muon, true> muoSelection_tag;
+  StringCutObjectSelector<reco::Muon, true> muoSelection_probe;
   int nmuons_;
   int tnp_;
   int L3_;
@@ -176,15 +167,12 @@ METME  DiMudR_   ;
   double minprob;
   double mincos;
   double minDS;
-  edm::EDGetTokenT<trigger::TriggerEvent>  hltInputTag_;
-  //edm::EDGetTokenT<edm::TriggerResults>  hltInputTag_;
-  //edm::InputTag  hltInputTag_;
+  edm::EDGetTokenT<trigger::TriggerEvent> hltInputTag_;
   std::vector<std::string> hltpaths_num;
   std::vector<std::string> hltpaths_den;
-  StringCutObjectSelector<reco::Track,true>        trSelection_;
-  StringCutObjectSelector<reco::Track,true>        trSelection_ref;
-  StringCutObjectSelector<reco::Candidate::LorentzVector,true>        DMSelection_ref;
-
+  StringCutObjectSelector<reco::Track, true> trSelection_;
+  StringCutObjectSelector<reco::Track, true> trSelection_ref;
+  StringCutObjectSelector<reco::Candidate::LorentzVector, true> DMSelection_ref;
 };
 
-#endif // METMONITOR_H
+#endif  // DQMOffline_Trigger_BPHMonitor_h
