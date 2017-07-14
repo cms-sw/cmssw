8f9bdf1c9a7226a588364a42d6de1399391b453d
Ulascan Sarica <ulascan90@gmail.com>
2017-07-14 17:41:46 +0200

    Re-organize hit monitor implementation as well

--- Alignment/HIPAlignmentAlgorithm/interface/HIPMonitorConfig.h
+++ Alignment/HIPAlignmentAlgorithm/interface/HIPMonitorConfig.h
@@ -5,6 +5,7 @@
 #include <string>
 #include "TTree.h"
 #include <FWCore/ParameterSet/interface/ParameterSet.h>
+#include <CondFormats/Alignment/interface/Definitions.h>
 
 struct HIPTrackMonitorVariables{
   const int maxNEvents;
@@ -23,59 +24,45 @@ struct HIPTrackMonitorVariables{
     m_Pt.clear(); m_Eta.clear(); m_Phi.clear(); m_Chi2n.clear(); m_P.clear(); m_d0.clear(); m_dz.clear(); m_wt.clear();
     m_Nhits.clear(); m_nhPXB.clear(); m_nhPXF.clear(); m_nhTIB.clear(); m_nhTOB.clear(); m_nhTID.clear(); m_nhTEC.clear();
   }
+  void resizeVectors(int NewSize){
+    m_Pt.resize(NewSize); m_Eta.resize(NewSize); m_Phi.resize(NewSize); m_Chi2n.resize(NewSize); m_P.resize(NewSize); m_d0.resize(NewSize); m_dz.resize(NewSize); m_wt.resize(NewSize);
+    m_Nhits.resize(NewSize); m_nhPXB.resize(NewSize); m_nhPXF.resize(NewSize); m_nhTIB.resize(NewSize); m_nhTOB.resize(NewSize); m_nhTID.resize(NewSize); m_nhTEC.resize(NewSize);
+  }
 
   void setTree(TTree* tree_){ tree=tree_; }
-  void bookBranches(){
-    if (tree!=0){
-      tree->Branch("Ntracks", &m_Ntracks);
-      tree->Branch("Nhits", &m_Nhits);
-      //tree->Branch("DataType", &m_datatype); This is done in HIPAlignmentAlgorithm
-      tree->Branch("nhPXB", &m_nhPXB);
-      tree->Branch("nhPXF", &m_nhPXF);
-      tree->Branch("nhTIB", &m_nhTIB);
-      tree->Branch("nhTOB", &m_nhTOB);
-      tree->Branch("nhTID", &m_nhTID);
-      tree->Branch("nhTEC", &m_nhTEC);
-      tree->Branch("Pt", &m_Pt);
-      tree->Branch("P", &m_P);
-      tree->Branch("Eta", &m_Eta);
-      tree->Branch("Phi", &m_Phi);
-      tree->Branch("Chi2n", &m_Chi2n);
-      tree->Branch("d0", &m_d0);
-      tree->Branch("dz", &m_dz);
-      tree->Branch("wt", &m_wt);
-    }
+  void bookBranches();
+  void fill();
+
+};
+
+struct HIPHitMonitorVariables{
+  const int maxHitsRcd;
+  int nHits;
+
+  bool m_hasHitProb;
+  float m_sinTheta, m_hitwt, m_angle, m_probXY, m_probQ;
+  unsigned int m_rawQualityWord;
+  align::ID m_detId;
+
+  TTree* tree;
+
+  void resetPerHit(){
+    m_hasHitProb=false;
+    m_sinTheta=0;
+    m_hitwt=1;
+    m_angle=0;
+    m_probXY=-1;
+    m_probQ=-1;
+    m_rawQualityWord=9999;
+    m_detId=0;
   }
-  void fill(){
-    if (tree==0) return;
-    if (maxNEvents>=0 && nEvents>=maxNEvents) return;
 
-    bool doFill=false;
-    int OldSize = m_Ntracks-(int)m_Pt.size();
-    if (m_Ntracks==OldSize) return;
+  HIPHitMonitorVariables(int maxHitsRcd_=-1) : maxHitsRcd(maxHitsRcd_), nHits(0), tree(0){ resetPerHit(); }
 
-    if (maxTracksRcd<0) doFill=true;
-    else if (OldSize<maxTracksRcd){
-      if (m_Ntracks<maxTracksRcd) doFill=true;
-      else{
-        int NewSize = maxTracksRcd-OldSize;
-        if ((int)m_Pt.size()<NewSize) NewSize=m_Pt.size();
+  void setTree(TTree* tree_){ tree=tree_; }
+  void bookBranches();
+  void fill();
 
-        // Do not touch m_Ntracks, just resize these vectors
-        m_Pt.resize(NewSize); m_Eta.resize(NewSize); m_Phi.resize(NewSize); m_Chi2n.resize(NewSize); m_P.resize(NewSize); m_d0.resize(NewSize); m_dz.resize(NewSize); m_wt.resize(NewSize);
-        m_Nhits.resize(NewSize); m_nhPXB.resize(NewSize); m_nhPXF.resize(NewSize); m_nhTIB.resize(NewSize); m_nhTOB.resize(NewSize); m_nhTID.resize(NewSize); m_nhTEC.resize(NewSize);
-
-        doFill=true;
-      }
-    }
-
-    if (doFill){
-      tree->Fill();
-      nEvents++;
-    }
-
-    resetPerEvent();
-  }
 };
 
 struct HIPMonitorConfig{
@@ -91,6 +78,7 @@ struct HIPMonitorConfig{
 
   const bool fillTrackHitMonitoring;
   const int maxHits;
+  HIPHitMonitorVariables hitmonitorvars;
 
   std::string outfile;
 
