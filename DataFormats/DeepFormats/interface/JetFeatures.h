440d56478c307521b1b947acb851e0badb01fc3a
Pablo de Castro <pablodecm@gmail.com>
2017-07-14 11:15:58 +0200

    add new DataFormats for DeepFlavour

---
+++ DataFormats/DeepFormats/interface/JetFeatures.h
@@ -0,0 +1,23 @@
+#ifndef DataFormats_DeepFormats_JetFeatures_h
+#define DataFormats_DeepFormats_JetFeatures_h
+
+namespace deep {
+
+class JetFeatures {
+
+  public:
+
+    float pt;
+    float corr_pt;
+    float eta;
+    float phi;
+    float mass;
+    float energy;
+
+    float looseId;
+
+};
+
+}
+
+#endif //DataFormats_DeepFormats_JetFeatures_h
