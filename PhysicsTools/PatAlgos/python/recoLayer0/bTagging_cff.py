fe1d10d83a6c4a7d390cc52d33b37fbc629f6855
Pablo de Castro <pablodecm@gmail.com>
2017-07-14 11:25:08 +0200

    modify PatAlgos and BTag conf so tests can run

--- PhysicsTools/PatAlgos/python/recoLayer0/bTagging_cff.py
+++ PhysicsTools/PatAlgos/python/recoLayer0/bTagging_cff.py
@@ -38,6 +38,7 @@ supportedBtagInfos = [
   , 'pfDeepCMVAPositiveTagInfos'
     # TopTagInfos (unrelated to b tagging)
   , 'caTopTagInfos'
+  , 'pfDeepTagInfos'
   ]
 # extend for "internal use" in PAT/MINIAOD (renaming)
 supportedBtagInfos.append( 'caTopTagInfosPAT' )
@@ -170,6 +171,7 @@ supportedBtagDiscr = {
   , 'pfPositiveDeepCMVAJetTags:probcc'                     : [["pfDeepCMVAPositiveTagInfos"], ["pfImpactParameterTagInfos","softPFMuonsTagInfos","softPFElectronsTagInfos","pfInclusiveSecondaryVertexFinderTagInfos"]]    
     # ChargeTagging
   , 'pfChargeBJetTags'                                      : [["pfImpactParameterTagInfos", "pfInclusiveSecondaryVertexFinderTagInfos", "softPFMuonsTagInfos", "softPFElectronsTagInfos"]]
+  , 'pfDeepFlavourJetTags:probb'                                      : [["pfDeepFlavourTagInfos"], ['pfDeepCSVTagInfos']]
   }
 
 
