a9c1529ef8c43d299d5a2e44fc3bd74f85bb18da
Andrew Brinkerhoff <abrinke1@cern.ch>
2017-07-14 12:57:46 +0200

    EMTF added 2017 data formats - not yet used

--- DataFormats/L1TMuon/src/classes.h
+++ DataFormats/L1TMuon/src/classes.h
@@ -6,6 +6,9 @@
 #include "DataFormats/L1TMuon/interface/RegionalMuonCandFwd.h"
 #include "DataFormats/L1TMuon/interface/RegionalMuonCand.h"
 #include "DataFormats/L1TMuon/interface/EMTFDaqOut.h"
+#include "DataFormats/L1TMuon/interface/EMTFHit.h"
+#include "DataFormats/L1TMuon/interface/EMTFRoad.h"
+#include "DataFormats/L1TMuon/interface/EMTFTrack.h"
 #include "DataFormats/L1TMuon/interface/EMTFTrack2016.h"
 #include "DataFormats/L1TMuon/interface/EMTFHit2016.h"
 #include "DataFormats/L1TMuon/interface/EMTFTrack2016Extra.h"
@@ -24,17 +27,26 @@ namespace {
     l1t::EMTFDaqOutCollection emtfOutput;
     edm::Wrapper<l1t::EMTFDaqOutCollection> emtfOutputWrap;
    
-    l1t::EMTFTrack2016Collection emtfTrack;
-    edm::Wrapper<l1t::EMTFTrack2016Collection> emtfTrackWrap;
+    l1t::EMTFHitCollection emtfHit;
+    edm::Wrapper<l1t::EMTFHitCollection> emtfHitWrap;
    
-    l1t::EMTFHit2016Collection emtfHit;
-    edm::Wrapper<l1t::EMTFHit2016Collection> emtfHitWrap;
+    l1t::EMTFRoadCollection emtfRoad;
+    edm::Wrapper<l1t::EMTFRoadCollection> emtfRoadWrap;
    
-    l1t::EMTFTrack2016ExtraCollection emtfTrackExtra;
-    edm::Wrapper<l1t::EMTFTrack2016ExtraCollection> emtfTrackExtraWrap;
+    l1t::EMTFTrackCollection emtfTrack;
+    edm::Wrapper<l1t::EMTFTrackCollection> emtfTrackWrap;
    
-    l1t::EMTFHit2016ExtraCollection emtfHitExtra;
-    edm::Wrapper<l1t::EMTFHit2016ExtraCollection> emtfHitExtraWrap;
+    l1t::EMTFTrack2016Collection emtfTrack2016;
+    edm::Wrapper<l1t::EMTFTrack2016Collection> emtfTrack2016Wrap;
+   
+    l1t::EMTFHit2016Collection emtfHit2016;
+    edm::Wrapper<l1t::EMTFHit2016Collection> emtfHit2016Wrap;
+   
+    l1t::EMTFTrack2016ExtraCollection emtfTrack2016Extra;
+    edm::Wrapper<l1t::EMTFTrack2016ExtraCollection> emtfTrack2016ExtraWrap;
+   
+    l1t::EMTFHit2016ExtraCollection emtfHit2016Extra;
+    edm::Wrapper<l1t::EMTFHit2016ExtraCollection> emtfHit2016ExtraWrap;
    
   };
 }
