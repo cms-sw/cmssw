bf08810656cc5961b45fa8c62d03c2cbb2c2402a
Sunanda <sunanda.banerjee@cern.ch>
2017-07-17 17:21:47 +0200

    Update Pedestal

--- Calibration/HcalAlCaRecoProducers/python/ALCARECOHcalCalPedestal_Output_cff.py
+++ Calibration/HcalAlCaRecoProducers/python/ALCARECOHcalCalPedestal_Output_cff.py
@@ -11,8 +11,8 @@ OutALCARECOHcalCalPedestal_noDrop = cms.PSet(
     outputCommands = cms.untracked.vstring(
         'keep *_hltTriggerSummaryAOD_*_*',
         'keep *_TriggerResults_*_*',
-        'keep FEDRawDataCollection_rawDataCollector_*_*',
-        'keep FEDRawDataCollection_source_*_*',
+        'keep FEDRawDataCollection_hltEcalCalibrationRaw_*_*',
+        'keep FEDRawDataCollection_hltHcalCalibrationRaw_*_*',
         'keep HBHERecHitsSorted_hbherecoPedestal_*_*',
         'keep HORecHitsSorted_horecoPedestal_*_*',
         'keep HFRecHitsSorted_hfrecoPedestal_*_*')
