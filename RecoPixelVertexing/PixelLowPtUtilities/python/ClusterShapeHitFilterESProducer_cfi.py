7c95d2dfc748222d341caf166864ec67086832d4
Vincenzo Innocente <vincenzo.innocente@cern.ch>
2017-07-17 14:34:36 +0200

    allow the use of a different calib for BPIX1

--- RecoPixelVertexing/PixelLowPtUtilities/python/ClusterShapeHitFilterESProducer_cfi.py
+++ RecoPixelVertexing/PixelLowPtUtilities/python/ClusterShapeHitFilterESProducer_cfi.py
@@ -3,11 +3,12 @@ import FWCore.ParameterSet.Config as cms
 ClusterShapeHitFilterESProducer = cms.ESProducer("ClusterShapeHitFilterESProducer",
                                                         ComponentName = cms.string('ClusterShapeHitFilter'),
                                                         PixelShapeFile= cms.string('RecoPixelVertexing/PixelLowPtUtilities/data/pixelShapePhase0.par'),
+                                                        PixelShapeFileL1= cms.string('RecoPixelVertexing/PixelLowPtUtilities/data/pixelShapePhase0.par'),
                                                         clusterChargeCut = cms.PSet(refToPSet_ = cms.string('SiStripClusterChargeCutNone'))
                                                         )
 from Configuration.Eras.Modifier_phase1Pixel_cff import phase1Pixel
 phase1Pixel.toModify(ClusterShapeHitFilterESProducer,
-#    PixelShapeFile = 'RecoPixelVertexing/PixelLowPtUtilities/data/clusterShapePhase1MinBias90Kall.par'
+    PixelShapeFileL1 = 'RecoPixelVertexing/PixelLowPtUtilities/data/clusterShapePhase1MinBias90Kall.par',
     PixelShapeFile = 'RecoPixelVertexing/PixelLowPtUtilities/data/clusterShapePhase1MinBias90KnoL1.par'
 )
 from Configuration.Eras.Modifier_phase2_tracker_cff import phase2_tracker
