f5b1bb7588c645cede488281741eeb2926660fa7
Pablo de Castro <pablodecm@gmail.com>
2017-07-18 14:48:06 +0200

    add SVs and corresponding converter/helpers

--- RecoBTag/DeepFlavour/python/DeepFlavourTagInfos_cfi.py
+++ RecoBTag/DeepFlavour/python/DeepFlavourTagInfos_cfi.py
@@ -3,6 +3,8 @@ import FWCore.ParameterSet.Config as cms
 pfDeepFlavourTagInfos = cms.EDProducer(
 	'DeepFlavourTagInfoProducer',
   jets = cms.InputTag("slimmedJets"),
+  vertices = cms.InputTag("offlineSlimmedPrimaryVertices"),
   secondary_vertices = cms.InputTag("slimmedSecondaryVertices"),
-  shallow_tag_infos = cms.InputTag('pfDeepCSVTagInfos')
+  shallow_tag_infos = cms.InputTag('pfDeepCSVTagInfos'),
+  jet_radius = cms.double(0.4)
 )
