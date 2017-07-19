cc5519ba581fb3c3befd35ace55500d30103e984
Jan Kaspar <jan.kaspar@cern.ch>
2017-07-19 11:30:59 +0200

    Track data converted mm --> m, beam position subracted.

--- RecoCTPPS/ProtonReconstruction/src/ProtonReconstructionAlgorithm.cc
+++ RecoCTPPS/ProtonReconstruction/src/ProtonReconstructionAlgorithm.cc
@@ -48,6 +48,9 @@ ProtonReconstructionAlgorithm::ProtonReconstructionAlgorithm(const std::string &
     { TotemRPDetId(1, 0, 3), "ip5_to_station_150_h_2_lhcb1" }
   };
 
+  // TODO: debug only
+  TFile *f_debug = new TFile("debug.root", "recreate");
+
   // build optics data for each object
   for (const auto &it : idNameMap)
   {
@@ -78,39 +81,54 @@ ProtonReconstructionAlgorithm::ProtonReconstructionAlgorithm(const std::string &
     }
 
     const bool check_appertures = false;
-    const bool invert_beam_coord_sytems = true;
+    const bool invert_beam_coord_systems = true;
 
     TGraph *g_xi_vs_x = new TGraph();
     TGraph *g_y0_vs_xi = new TGraph();
     TGraph *g_v_y_vs_xi = new TGraph();
     TGraph *g_L_y_vs_xi = new TGraph();
 
+    // input: all zero
+    double kin_in_zero[5] = { 0., crossing_angle, vtx0_y, 0., 0. };
+    double kin_out_zero[5] = { 0., 0., 0., 0., 0. };
+    rpod.optics->Transport(kin_in_zero, kin_out_zero, check_appertures, invert_beam_coord_systems);
+
     for (double xi = 0.; xi <= 0.201; xi += 0.005)
     {
       // input: only xi
       double kin_in_xi[5] = { 0., crossing_angle * (1. - xi), vtx0_y, 0., -xi };
       double kin_out_xi[5];
-        rpod.optics->Transport(kin_in_xi, kin_out_xi, check_appertures, invert_beam_coord_sytems);
+        rpod.optics->Transport(kin_in_xi, kin_out_xi, check_appertures, invert_beam_coord_systems);
 
       // input: xi and vtx_y
       const double vtx_y = 10E-6;  // m
       double kin_in_xi_vtx_y[5] = { 0., crossing_angle * (1. - xi), vtx0_y + vtx_y, 0., -xi };
       double kin_out_xi_vtx_y[5];
-        rpod.optics->Transport(kin_in_xi_vtx_y, kin_out_xi_vtx_y, check_appertures, invert_beam_coord_sytems);
+        rpod.optics->Transport(kin_in_xi_vtx_y, kin_out_xi_vtx_y, check_appertures, invert_beam_coord_systems);
 
       // input: xi and th_y
       const double th_y = 20E-6;  // rad
       double kin_in_xi_th_y[5] = { 0., crossing_angle * (1. - xi), vtx0_y, th_y * (1. - xi), -xi };
       double kin_out_xi_th_y[5];
-        rpod.optics->Transport(kin_in_xi_th_y, kin_out_xi_th_y, check_appertures, invert_beam_coord_sytems);
+        rpod.optics->Transport(kin_in_xi_th_y, kin_out_xi_th_y, check_appertures, invert_beam_coord_systems);
 
       // fill graphs
       int idx = g_xi_vs_x->GetN();
-      g_xi_vs_x->SetPoint(idx, kin_out_xi[0], xi);
-      g_y0_vs_xi->SetPoint(idx, xi, kin_out_xi[2]);
+      g_xi_vs_x->SetPoint(idx, kin_out_xi[0] - kin_out_zero[0], xi);
+      g_y0_vs_xi->SetPoint(idx, xi, kin_out_xi[2] - kin_out_zero[2]);
       g_v_y_vs_xi->SetPoint(idx, xi, (kin_out_xi_vtx_y[2] - kin_out_xi[2]) / vtx_y);
       g_L_y_vs_xi->SetPoint(idx, xi, (kin_out_xi_th_y[2] - kin_out_xi[2]) / th_y);
     }
+
+    // TODO: debug only
+    char buf[100];
+    unsigned int decRPId = rpId.arm()*100 + rpId.station()*10 + rpId.rp();
+    sprintf(buf, "%u", decRPId);
+    gDirectory = f_debug->mkdir(buf);
+    g_xi_vs_x->Write("g_xi_vs_x");
+    g_y0_vs_xi->Write("g_y0_vs_xi");
+    g_v_y_vs_xi->Write("g_v_y_vs_xi");
+    g_L_y_vs_xi->Write("g_L_y_vs_xi");
 
     rpod.s_xi_vs_x = make_shared<TSpline3>("", g_xi_vs_x->GetX(), g_xi_vs_x->GetY(), g_xi_vs_x->GetN());
     delete g_xi_vs_x;
@@ -131,6 +149,9 @@ ProtonReconstructionAlgorithm::ProtonReconstructionAlgorithm(const std::string &
   // initialise fitter
   double pStart[] = { 0, 0, 0, 0 };
   fitter_->SetFCN( 4, *chiSquareCalculator_, pStart, 0, true );
+
+  // TODO: debug only
+  delete f_debug;
 }
 
 //----------------------------------------------------------------------------------------------------
@@ -159,6 +180,7 @@ double ProtonReconstructionAlgorithm::ChiSquareCalculator::operator() (const dou
   {
     const CTPPSDetId rpId(track->getRPId());
 
+    // determine beam beam parameters
     double crossing_angle = 0., vtx0_y = 0.;
 
     if (rpId.arm() == 0)
@@ -170,20 +192,27 @@ double ProtonReconstructionAlgorithm::ChiSquareCalculator::operator() (const dou
       vtx0_y = yOffsetSector56_;
     }
 
-    // transport proton to the RP
+    const bool invert_beam_coord_systems = true;
+
+    // get beam position at the RP, TODO: this can be pre-calculated for performace optimisation
     auto oit = m_rp_optics->find(rpId);
 
+    double kin_in_zero[5] = { 0., crossing_angle, vtx0_y, 0., 0. };
+    double kin_out_zero[5];
+    oit->second.optics->Transport(kin_in_zero, kin_out_zero, check_apertures, invert_beam_coord_systems);
+
+    // transport proton to the RP
     double kin_in[5] = { vtx_x, (th_x + crossing_angle) * (1. - xi), vtx0_y + vtx_y, th_y * (1. - xi), -xi };
     double kin_out[5];
-    const bool invert_beam_coord_sytems = true;
-    oit->second.optics->Transport(kin_in, kin_out, check_apertures, invert_beam_coord_sytems);
+    oit->second.optics->Transport(kin_in, kin_out, check_apertures, invert_beam_coord_systems);
 
-    const double& x = kin_out[0];
-    const double& y = kin_out[2];
+    // proton position wrt. beam
+    const double& x = kin_out[0] - kin_out_zero[0];
+    const double& y = kin_out[2] - kin_out_zero[2];
 
-    // calculate chi^2 contributions
-    const double x_diff_norm = (x - track->getX()) / track->getXUnc();
-    const double y_diff_norm = (y - track->getY()) / track->getYUnc();
+    // calculate chi^2 contributions, convert track data mm --> m
+    const double x_diff_norm = (x - track->getX()*1E-3) / (track->getXUnc()*1E-3);
+    const double y_diff_norm = (y - track->getY()*1E-3) / (track->getYUnc()*1E-3);
 
     // increase chi^2
     S2 += x_diff_norm*x_diff_norm + y_diff_norm*y_diff_norm;
@@ -220,7 +249,7 @@ void ProtonReconstructionAlgorithm::reconstructFromMultiRP(const vector<const CT
   for (const auto &track : tracks)
   {
     auto oit = m_rp_optics_.find(track->getRPId());
-    double xi = oit->second.s_xi_vs_x->Eval(track->getX());
+    double xi = oit->second.s_xi_vs_x->Eval(track->getX() * 1E-3);  // mm --> m
 
     S_1 += 1.;
     S_xi0 += xi;
@@ -238,7 +267,7 @@ void ProtonReconstructionAlgorithm::reconstructFromMultiRP(const vector<const CT
 
     auto oit = m_rp_optics_.find(track->getRPId());
 
-    y[y_idx] = track->getY() - oit->second.s_y0_vs_xi->Eval(xi_0);
+    y[y_idx] = track->getY()*1E-3 - oit->second.s_y0_vs_xi->Eval(xi_0); // track y: mm --> m
     v_y[y_idx] = oit->second.s_v_y_vs_xi->Eval(xi_0);
     L_y[y_idx] = oit->second.s_L_y_vs_xi->Eval(xi_0);
 
@@ -248,6 +277,9 @@ void ProtonReconstructionAlgorithm::reconstructFromMultiRP(const vector<const CT
   const double det_y = v_y[0] * L_y[1] - L_y[0] * v_y[1];
   const double vtx_y_0 = (L_y[1] * y[0] - L_y[0] * y[1]) / det_y;
   const double th_y_0 = (v_y[0] * y[1] - v_y[1] * y[0]) / det_y;
+
+  printf("* ProtonReconstructionAlgorithm::reconstructFromMultiRP\n");
+  printf("    initial estimate: xi_0 = %f, th_y_0 = %E, vtx_y_0 = %E\n", xi_0, th_y_0, vtx_y_0);
 
   // minimisation
   fitter_->Config().ParSettings(0).Set("xi", xi_0, 0.005);
@@ -272,6 +304,8 @@ void ProtonReconstructionAlgorithm::reconstructFromMultiRP(const vector<const CT
     << "theta_y=" << params[2] << ", "
     << "vertex_y=" << params[3] << "\n";
 
+  printf("    fit: xi = %f, th_x = %E, th_y = %E, vtx_y = %E\n", params[0], params[1], params[2], params[3]);
+
   reco::ProtonTrack pt;
   pt.method = reco::ProtonTrack::rmMultiRP;
   pt.setValid(result.IsValid());
@@ -281,9 +315,9 @@ void ProtonReconstructionAlgorithm::reconstructFromMultiRP(const vector<const CT
 
   for (const auto &track : tracks)
     pt.contributingRPIds.insert(track->getRPId());
-  
+
   pt.fitChiSq = result.Chi2();
-  pt.fitNDF = tracks.size() - 4;
+  pt.fitNDF = 2.*tracks.size() - 4;
   pt.lhcSector = (CTPPSDetId(tracks[0]->getRPId()).arm() == 0) ? reco::ProtonTrack::sector45 : reco::ProtonTrack::sector56;
 
   out.push_back(move(pt));
@@ -305,8 +339,15 @@ void ProtonReconstructionAlgorithm::reconstructFromSingleRP(const vector<const C
   // rough estimate of xi from each track
   for (const auto &track : tracks)
   {
+    CTPPSDetId rpId(track->getRPId());
+    unsigned int decRPId = rpId.arm()*100 + rpId.station()*10 + rpId.rp();
+
+    printf("* reconstructFromSingleRP(%u)\n", decRPId);
+
     auto oit = m_rp_optics_.find(track->getRPId());
-    double xi = oit->second.s_xi_vs_x->Eval(track->getX());
+    double xi = oit->second.s_xi_vs_x->Eval(track->getX() * 1E-3); // mm --> m
+
+    printf("    x = %f mm, xi = %f\n", track->getX(), xi);
 
     reco::ProtonTrack pt;
     pt.method = reco::ProtonTrack::rmSingleRP;
