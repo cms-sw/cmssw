bea183fbe2240a14d429109afd3ff41b5b922714
Pablo de Castro <pablodecm@gmail.com>
2017-07-19 19:48:07 +0200

    add PF cand. to TagInfoProducer

--- RecoBTag/DeepFlavour/plugins/DeepFlavourTagInfoProducer.cc
+++ RecoBTag/DeepFlavour/plugins/DeepFlavourTagInfoProducer.cc
@@ -9,8 +9,12 @@
 #include "FWCore/Utilities/interface/StreamID.h"
 
 #include "DataFormats/PatCandidates/interface/Jet.h"
+#include "DataFormats/PatCandidates/interface/PackedCandidate.h"
 
 #include "DataFormats/BTauReco/interface/ShallowTagInfo.h"
+
+#include "TrackingTools/TransientTrack/interface/TransientTrackBuilder.h"
+#include "TrackingTools/Records/interface/TransientTrackRecord.h"
 
 #include "DataFormats/DeepFormats/interface/DeepFlavourTagInfo.h"
 #include "DataFormats/DeepFormats/interface/DeepFlavourFeatures.h"
@@ -18,6 +22,11 @@
 #include "RecoBTag/DeepFlavour/interface/jet_features_converter.h"
 #include "RecoBTag/DeepFlavour/interface/btag_features_converter.h"
 #include "RecoBTag/DeepFlavour/interface/sv_features_converter.h"
+#include "RecoBTag/DeepFlavour/interface/n_pf_features_converter.h"
+#include "RecoBTag/DeepFlavour/interface/c_pf_features_converter.h"
+
+#include "RecoBTag/DeepFlavour/interface/TrackInfoBuilder.h"
+#include "RecoBTag/DeepFlavour/interface/sorting_modules.h"
 
 class DeepFlavourTagInfoProducer : public edm::stream::EDProducer<> {
 
@@ -43,6 +52,8 @@ class DeepFlavourTagInfoProducer : public edm::stream::EDProducer<> {
     edm::EDGetTokenT<VertexCollection> vtx_token_;
     edm::EDGetTokenT<SVCollection> sv_token_;
     edm::EDGetTokenT<ShallowTagInfoCollection> shallow_tag_info_token_;
+    
+
 
 };
 
@@ -84,7 +95,8 @@ void DeepFlavourTagInfoProducer::produce(edm::Event& iEvent, const edm::EventSet
   edm::Handle<ShallowTagInfoCollection> shallow_tag_infos;
   iEvent.getByToken(shallow_tag_info_token_, shallow_tag_infos);
 
-
+  edm::ESHandle<TransientTrackBuilder> track_builder; 
+  iSetup.get<TransientTrackRecord>().get("TransientTrackBuilder", track_builder);
 
   for (const auto & tag_info : *shallow_tag_infos) {
 
@@ -118,13 +130,69 @@ void DeepFlavourTagInfoProducer::produce(edm::Event& iEvent, const edm::EventSet
         deep::sv_features_converter(sv, pv, jet, sv_features);
       }
     }
+
+    // stuff required for dealing with pf candidates 
+    math::XYZVector jet_dir = jet.momentum().Unit();
+    GlobalVector jet_ref_track_dir(jet.px(),jet.py(),jet.pz());
+
+    std::vector<sorting::sortingClass<pat::PackedCandidate>> sortedall;
+
+    deep::TrackInfoBuilder trackinfo(track_builder);
+
+    // unsorted reference to sv
+    const auto & svs_unsorted = *svs;     
+    // fill collection, from DeepTNtuples plus some styling
+    for (unsigned int i = 0; i <  jet.numberOfDaughters(); i++){
+        auto packed_cand = dynamic_cast<const pat::PackedCandidate*>(jet.daughter(i));
+        if(packed_cand){
+
+            trackinfo.buildTrackInfo(packed_cand,jet_dir,jet_ref_track_dir,pv);
+
+            sortedall.emplace_back(packed_cand, trackinfo.getTrackSip2dSig(),
+                    -deep::mindrsvpfcand(svs_unsorted,packed_cand), packed_cand->pt()/jet.pt());
+
+        }
+    }
+
+    // sort collection (open the black-box if you please) 
+    std::sort(sortedall.begin(),sortedall.end(),
+              sorting::sortingClass<pat::PackedCandidate>::compareByABCInv);
+
+  for (const auto& s : sortedall) {
+
+    // get ref and check that is correct
+    const auto& packed_cand =s.get();
+    if(!packed_cand) continue;
+
+    float drminpfcandsv = deep::mindrsvpfcand(svs_unsorted, packed_cand);
     
+    if (packed_cand->charge() != 0) {
+      // is charged candidate, add with default values
+      features.c_pf_features.emplace_back();
+      // build track info
+      trackinfo.buildTrackInfo(packed_cand,jet_dir,jet_ref_track_dir,pv);
+      // in C++17 could just get from emplace_back output
+      auto & c_pf_features = features.c_pf_features.back();
+      // fill feature structure 
+      deep::c_pf_features_converter(packed_cand, jet, trackinfo, 
+                                    drminpfcandsv, c_pf_features);
+    } else {
+      // is neutral candidate, add with default values
+      features.n_pf_features.emplace_back();
+      // in C++17 could just get from emplace_back output
+      auto & n_pf_features = features.n_pf_features.back();
+      // fill feature structure 
+      deep::n_pf_features_converter(packed_cand, jet, drminpfcandsv, 
+                                   n_pf_features);
+    }
+
     
-    output_tag_infos->emplace_back(features, jet_ref);
   }
 
-  
+    
 
+    output_tag_infos->emplace_back(features, jet_ref);
+  }
 
   iEvent.put(std::move(output_tag_infos));
 
