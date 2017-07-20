bb5620ebac4ad32e2c22d940167edc8e350638ab
Fabrizio <Fabrizio.Ferro@ge.infn.it>
2017-07-20 10:39:44 +0200

    cleaning of pixel topology

--- Geometry/VeryForwardGeometry/interface/CTPPSPixelSimTopology.h
+++ Geometry/VeryForwardGeometry/interface/CTPPSPixelSimTopology.h
@@ -1,9 +1,7 @@
-#ifndef Geometry_CTPPSPixelDetTopology_RPix_DET_SIM_TOPOLOGY_H
-#define Geometry_CTPPSPixelDetTopology_RPix_DET_SIM_TOPOLOGY_H
+#ifndef Geometry_VeryForwardGeometry_CTPPSPixelSimTopology_H
+#define Geometry_VeryForwardGeometry_CTPPSPixelSimTopology_H
 
 #include "TMath.h"
-#include "FWCore/ParameterSet/interface/ParameterSet.h"
-#include "DataFormats/GeometryVector/interface/LocalVector.h"
 #include "Geometry/VeryForwardGeometry/interface/CTPPSPixelTopology.h"
 
 /* simX and simY are the coordinates as in the simulation: 
@@ -18,31 +16,32 @@
 
 class pixel_info
 {
-public:
-pixel_info(double lower_simX_border, double higher_simX_border, double lower_simY_border, double higher_simY_border, double eff_factor, 
-	   unsigned short pixel_row_no, unsigned short pixel_col_no) : lower_simX_border_(lower_simX_border), 
-    higher_simX_border_(higher_simX_border), lower_simY_border_(lower_simY_border), 
-    higher_simY_border_(higher_simY_border), eff_factor_(eff_factor), pixel_row_no_(pixel_row_no), pixel_col_no_(pixel_col_no),pixel_index_(pixel_col_no*160+pixel_row_no){}
-  inline double & HigherSimXBorder() {return higher_simX_border_;}
-  inline double & LowerSimXBorder() {return lower_simX_border_;}
-  inline double & HigherSimYBorder() {return higher_simY_border_;}
-  inline double & LowerSimYBorder() {return lower_simY_border_;}
-  inline double & EffFactor() {return eff_factor_;}
-  inline unsigned short & PixelRowNo() {return pixel_row_no_;}
-  inline unsigned short & PixelColNo() {return pixel_col_no_;}
-  inline unsigned short &PixelIndex() {return pixel_index_;}
 
-  inline double HigherSimXBorder() const {return higher_simX_border_;}
-  inline double LowerSimXBorder() const {return lower_simX_border_;}
-  inline double HigherSimYBorder() const {return higher_simY_border_;}
-  inline double LowerSimYBorder() const {return lower_simY_border_;}
-  inline double EffFactor() const {return eff_factor_;}
-  inline unsigned short PixelRowNo() const {return pixel_row_no_;}
-  inline unsigned short PixelColNo() const {return pixel_col_no_;}
-  inline unsigned short PixelIndex() const {return pixel_index_;}
+public:
+
+pixel_info(double lower_simX_border, double higher_simX_border, double lower_simY_border, double higher_simY_border, double eff_factor, unsigned short pixel_row_no, unsigned short pixel_col_no) : 
+  lower_simX_border_(lower_simX_border), 
+    higher_simX_border_(higher_simX_border), 
+    lower_simY_border_(lower_simY_border), 
+    higher_simY_border_(higher_simY_border), 
+    eff_factor_(eff_factor), 
+    pixel_row_no_(pixel_row_no), 
+    pixel_col_no_(pixel_col_no),
+    pixel_index_(pixel_col_no*CTPPSPixelTopology::no_of_pixels_simX_+pixel_row_no)
+    {}
+
+  inline double higherSimXBorder() const {return higher_simX_border_;}
+  inline double lowerSimXBorder() const {return lower_simX_border_;}
+  inline double higherSimYBorder() const {return higher_simY_border_;}
+  inline double lowerSimYBorder() const {return lower_simY_border_;}
+  inline double effFactor() const {return eff_factor_;}
+  inline unsigned short pixelRowNo() const {return pixel_row_no_;}
+  inline unsigned short pixelColNo() const {return pixel_col_no_;}
+  inline unsigned short pixelIndex() const {return pixel_index_;}
     
 
 private:
+
   double lower_simX_border_;
   double higher_simX_border_;
   double lower_simY_border_;
@@ -55,84 +54,87 @@ private:
 
 class CTPPSPixelSimTopology : public CTPPSPixelTopology
 {
+
 public:
-  CTPPSPixelSimTopology(const edm::ParameterSet &params);
-  std::vector<pixel_info> GetPixelsInvolved(double x, double y, double sigma, double &hit_pos_x, double &hit_pos_y);
-  inline void PixelRange(unsigned int row, unsigned int col, double &lower_x, double &higher_x, double &lower_y, double &higher_y){
+  CTPPSPixelSimTopology();
+  ~CTPPSPixelSimTopology(){};
+
+  std::vector<pixel_info> getPixelsInvolved(double x, double y, double sigma, double &hit_pos_x, double &hit_pos_y);
+
+  inline void pixelRange(unsigned int arow, unsigned int acol, double &lower_x, double &higher_x, double &lower_y, double &higher_y){
        
 // x and y in the system  of Geant4 SIMULATION
 
-    row = 159 - row;
+    arow = 159 - arow;
 
-    assert(row<=159 &&  col <=155);
+    if(!(arow<=159 && acol <=155))
+      throw cms::Exception("CTPPSPixelSimTopology")<< " rows or columns exceeding limits";
  
 // rows (x segmentation)
-
-    if(row == 0) {
+    
+    if(arow == 0) {
       lower_x = 0;
       higher_x = 0.3;
     }
-    if( row > 0 && row <= 78 ){
-      lower_x = 0.3 + (row-1)*pitch_simX_;
-      higher_x = 0.3 + row*pitch_simX_;
+    if( arow > 0 && arow <= 78 ){
+      lower_x = 0.3 + (arow-1)*pitch_simX_;
+      higher_x = 0.3 + arow*pitch_simX_;
     } 
-    if(row == 79) {
-      lower_x = 0.3 + (row-1)*pitch_simX_;
-      higher_x = 0.3 + (row+1)*pitch_simX_;
+    if(arow == 79) {
+      lower_x = 0.3 + (arow-1)*pitch_simX_;
+      higher_x = 0.3 + (arow+1)*pitch_simX_;
     }
-    if(row == 80) {
-      lower_x = 0.3 + (row)*pitch_simX_;
-      higher_x = 0.3 + (row+2)*pitch_simX_;
+    if(arow == 80) {
+      lower_x = 0.3 + (arow)*pitch_simX_;
+      higher_x = 0.3 + (arow+2)*pitch_simX_;
     }
-    if( row > 80 && row <= 158 ){
-      lower_x = 0.3 + (row+1)*pitch_simX_;
-      higher_x = 0.3 + (row+2)*pitch_simX_;
+    if( arow > 80 && arow <= 158 ){
+      lower_x = 0.3 + (arow+1)*pitch_simX_;
+      higher_x = 0.3 + (arow+2)*pitch_simX_;
     } 
-    if( row == 159){
-      lower_x = 0.3 + (row+1)*pitch_simX_;
-      higher_x = 0.3 + (row+4)*pitch_simX_;
+    if( arow == 159){
+      lower_x = 0.3 + (arow+1)*pitch_simX_;
+      higher_x = 0.3 + (arow+4)*pitch_simX_;
     } 
 
 // columns (y segmentation)
 
-
-
-    if(col == 0) {
+    if(acol == 0) {
       lower_y = 0;
       higher_y = 0.35;
     }
-    if( col > 0 && col <= 50 ){
-      lower_y = 0.35 + (col-1)*pitch_simY_;
-      higher_y = 0.35 + col*pitch_simY_;
+    if( acol > 0 && acol <= 50 ){
+      lower_y = 0.35 + (acol-1)*pitch_simY_;
+      higher_y = 0.35 + acol*pitch_simY_;
     } 
-    if(col == 51) {
-      lower_y = 0.35 + (col-1)*pitch_simY_;
-      higher_y = 0.35 + (col+1)*pitch_simY_;
+    if(acol == 51) {
+      lower_y = 0.35 + (acol-1)*pitch_simY_;
+      higher_y = 0.35 + (acol+1)*pitch_simY_;
     }
-    if(col == 52) {
-      lower_y = 0.35 + (col)*pitch_simY_;
-      higher_y = 0.35 + (col+2)*pitch_simY_;
+    if(acol == 52) {
+      lower_y = 0.35 + (acol)*pitch_simY_;
+      higher_y = 0.35 + (acol+2)*pitch_simY_;
     }
-    if( col > 52 && col <= 102 ){
-      lower_y = 0.35 + (col+1)*pitch_simY_;
-      higher_y = 0.35 + (col+2)*pitch_simY_;
+    if( acol > 52 && acol <= 102 ){
+      lower_y = 0.35 + (acol+1)*pitch_simY_;
+      higher_y = 0.35 + (acol+2)*pitch_simY_;
     } 
 
-    if(col == 103) {
-      lower_y = 0.35 + (col+1)*pitch_simY_;
-      higher_y = 0.35 + (col+3)*pitch_simY_;
+    if(acol == 103) {
+      lower_y = 0.35 + (acol+1)*pitch_simY_;
+      higher_y = 0.35 + (acol+3)*pitch_simY_;
     }
-    if(col == 104) {
-      lower_y = 0.35 + (col+2)*pitch_simY_;
-      higher_y = 0.35 + (col+4)*pitch_simY_;
+    if(acol == 104) {
+      lower_y = 0.35 + (acol+2)*pitch_simY_;
+      higher_y = 0.35 + (acol+4)*pitch_simY_;
     }
-    if( col > 104 && col <= 154 ){
-      lower_y = 0.35 + (col+3)*pitch_simY_;
-      higher_y = 0.35 + (col+4)*pitch_simY_;
+    if( acol > 104 && acol <= 154 ){
+      lower_y = 0.35 + (acol+3)*pitch_simY_;
+      higher_y = 0.35 + (acol+4)*pitch_simY_;
     } 
-    if(col == 155) {
-      lower_y = 0.35 + (col+3)*pitch_simY_;
-      higher_y = 0.35 + (col+4)*pitch_simY_ + 0.2;
+    if(acol == 155) {
+      lower_y = 0.35 + (acol+3)*pitch_simY_;
+      higher_y = 0.35 + (acol+4)*pitch_simY_ + 0.2;
     }
  
     lower_x = lower_x - simX_width_/2.;
@@ -148,134 +150,125 @@ private:
   double active_edge_x_;
   double active_edge_y_;
     
-
-  double active_edge_sigma_;
-    
-  int verbosity_;
-    
-
-  
-  inline double ActiveEdgeFactor(double x, double y)
+  inline double activeEdgeFactor(double x, double y)
   {
-    double TopEdgeFactor=TMath::Erf(-DistanceFromTopActiveEdge(x, y)/active_edge_sigma_)/2 + 0.5;
-    double BottomEdgeFactor=TMath::Erf(-DistanceFromBottomActiveEdge(x, y)/active_edge_sigma_)/2 + 0.5;
-    double RightEdgeFactor=TMath::Erf(-DistanceFromRightActiveEdge(x, y)/active_edge_sigma_)/2 + 0.5;
-    double LeftEdgeFactor=TMath::Erf(-DistanceFromLeftActiveEdge(x, y)/active_edge_sigma_)/2 + 0.5;
+    double topEdgeFactor=TMath::Erf(-distanceFromTopActiveEdge(x, y)/active_edge_sigma_)/2 + 0.5;
+    double bottomEdgeFactor=TMath::Erf(-distanceFromBottomActiveEdge(x, y)/active_edge_sigma_)/2 + 0.5;
+    double rightEdgeFactor=TMath::Erf(-distanceFromRightActiveEdge(x, y)/active_edge_sigma_)/2 + 0.5;
+    double leftEdgeFactor=TMath::Erf(-distanceFromLeftActiveEdge(x, y)/active_edge_sigma_)/2 + 0.5;
 
-    double AEF = TopEdgeFactor*BottomEdgeFactor*RightEdgeFactor*LeftEdgeFactor;
+    double aEF = topEdgeFactor*bottomEdgeFactor*rightEdgeFactor*leftEdgeFactor;
 
-    assert(AEF<=1);
+    if(aEF>1)
+      throw cms::Exception("CTPPSPixelSimTopology")<< " active edge factor > 1";
 
-    return AEF;
+    return aEF;
   }
-    
-
- 
-  inline double DistanceFromTopActiveEdge(double x, double y)
+     
+  inline double distanceFromTopActiveEdge(double x, double y)
   {
     double d=y-active_edge_y_;
     return d;
   }
-  inline double DistanceFromBottomActiveEdge(double x, double y)
+
+  inline double distanceFromBottomActiveEdge(double x, double y)
   {
     double d=-y-active_edge_y_;
     return d;
   }
-  inline double DistanceFromRightActiveEdge(double x, double y)
+
+  inline double distanceFromRightActiveEdge(double x, double y)
   {
     double d=x-active_edge_x_;
     return d;
   }
-  inline double DistanceFromLeftActiveEdge(double x, double y)
+
+  inline double distanceFromLeftActiveEdge(double x, double y)
   {
     double d=-x-active_edge_x_;
     return d;
   }
 
-
-  inline unsigned int Row(double x){
+  inline unsigned int row(double x){
 // x in the G4 simulation system
 
-x = x + simX_width_/2.;
+    x = x + simX_width_/2.;
  
 // now x in the system centered in the bottom left corner of the sensor (sensor view, rocs behind)
-    assert(x>=0 && x<=simX_width_);
-    unsigned int row;
+    if(!(x>=0 && x<=simX_width_))
+      throw cms::Exception("CTPPSPixelSimTopology")<< " out of reference frame";
+    unsigned int arow;
 // rows (x segmentation)
 
-    if(x <= (dead_edge_width_+pitch_simX_) ) row = 0;
+    if(x <= (dead_edge_width_+pitch_simX_) ) arow = 0;
 
     if(x > (dead_edge_width_+pitch_simX_) && x <= (dead_edge_width_+79*pitch_simX_)   ){
-      row = int((x - dead_edge_width_-pitch_simX_)/pitch_simX_)+1 ;
+      arow = int((x - dead_edge_width_-pitch_simX_)/pitch_simX_)+1 ;
     }      
 
-    if(x >  (dead_edge_width_+79*pitch_simX_) &&  x<= (dead_edge_width_+81*pitch_simX_)) row = 79;
+    if(x >  (dead_edge_width_+79*pitch_simX_) &&  x<= (dead_edge_width_+81*pitch_simX_)) arow = 79;
 
-    if(x >  (dead_edge_width_+81*pitch_simX_) &&  x<= (dead_edge_width_+83*pitch_simX_)) row = 80;
+    if(x >  (dead_edge_width_+81*pitch_simX_) &&  x<= (dead_edge_width_+83*pitch_simX_)) arow = 80;
 
     if(x > (dead_edge_width_+83*pitch_simX_) && x <= (dead_edge_width_+162*pitch_simX_)   ){
-      row = int((x - dead_edge_width_-pitch_simX_)/pitch_simX_)-1;
+      arow = int((x - dead_edge_width_-pitch_simX_)/pitch_simX_)-1;
     }      
-    if(x > (dead_edge_width_+162*pitch_simX_) ) row = 159;
+    if(x > (dead_edge_width_+162*pitch_simX_) ) arow = 159;
 
-    row=159-row;
-    assert(row<=159);
-
+    arow=159-arow;
+    if(arow>159)
+      throw cms::Exception("CTPPSPixelSimTopology")<< " row number exceeding limit";
  
-    return row;
-       
-
+    return arow;
   }
 
-  inline unsigned int Col(double y){
-    unsigned int col;
+  inline unsigned int col(double y){
+    unsigned int column;
 // y in the G4 simulation system
 
 // columns (y segmentation)
 // now y in the system centered in the bottom left corner of the sensor (sensor view, rocs behind)
     y = y + simY_width_/2.;
-    assert(y>=0 && y <=simY_width_);
+    if(!(y>=0 && y <=simY_width_))
+      throw cms::Exception("CTPPSPixelSimTopology")<< " out of reference frame";
 
-    if(y <= (dead_edge_width_+pitch_simY_) ) col = 0;
+    if(y <= (dead_edge_width_+pitch_simY_) ) column = 0;
 
     if(y > (dead_edge_width_+pitch_simY_) && y <= (dead_edge_width_+51*pitch_simY_)   ){
-      col = int((y - dead_edge_width_-pitch_simY_)/pitch_simY_)+1 ;
+      column = int((y - dead_edge_width_-pitch_simY_)/pitch_simY_)+1 ;
     }      
 
-    if(y >  (dead_edge_width_+51*pitch_simY_) &&  y<= (dead_edge_width_+53*pitch_simY_)) col = 51;
+    if(y >  (dead_edge_width_+51*pitch_simY_) &&  y<= (dead_edge_width_+53*pitch_simY_)) column = 51;
 
-    if(y >  (dead_edge_width_+53*pitch_simY_) &&  y<= (dead_edge_width_+55 *pitch_simY_)) col = 52;
+    if(y >  (dead_edge_width_+53*pitch_simY_) &&  y<= (dead_edge_width_+55 *pitch_simY_)) column = 52;
 
     if(y >  (dead_edge_width_+55*pitch_simY_) &&  y<= (dead_edge_width_+105 *pitch_simY_)) {
-      col = int((y - dead_edge_width_-pitch_simY_)/pitch_simY_)-1 ;
+      column = int((y - dead_edge_width_-pitch_simY_)/pitch_simY_)-1 ;
     }
 
 
+    if(y >  (dead_edge_width_+105*pitch_simY_) &&  y<= (dead_edge_width_+107*pitch_simY_)) column = 103;
 
-    if(y >  (dead_edge_width_+105*pitch_simY_) &&  y<= (dead_edge_width_+107*pitch_simY_)) col = 103;
-
-    if(y >  (dead_edge_width_+107*pitch_simY_) &&  y<= (dead_edge_width_+109 *pitch_simY_)) col = 104;
+    if(y >  (dead_edge_width_+107*pitch_simY_) &&  y<= (dead_edge_width_+109 *pitch_simY_)) column = 104;
 
     if(y >  (dead_edge_width_+109*pitch_simY_) &&  y<= (dead_edge_width_+159 *pitch_simY_)) {
-      col = int((y - dead_edge_width_-pitch_simY_)/pitch_simY_)-3 ;
+      column = int((y - dead_edge_width_-pitch_simY_)/pitch_simY_)-3 ;
     }
 
-    if(y >  (dead_edge_width_+159*pitch_simY_) ) col = 155;
+    if(y >  (dead_edge_width_+159*pitch_simY_) ) column = 155;
 
-
-    return col;
-
+    return column;
   }
 
-
-  inline void RowCol2Index(unsigned int row, unsigned int col, unsigned int &index){
-    index = col*160+row;
+  inline void rowCol2Index(unsigned int arow, unsigned int acol, unsigned int &index){
+    index = acol*no_of_pixels_simX_+arow;
   }
-  inline void Index2RowCol(unsigned int &row, unsigned int &col, unsigned int index){
-    col = index / 160;
-    row = index % 160;
+
+  inline void index2RowCol(unsigned int &arow, unsigned int &acol, unsigned int index){
+    acol = index / no_of_pixels_simX_ ;
+    arow = index % no_of_pixels_simX_;
   }
 
 };
 
-#endif  //Geometry_CTPPSPixelDetTopology_RPix_DET_SIM_TOPOLOGY_H
+#endif
--- Geometry/VeryForwardGeometry/interface/CTPPSPixelTopology.h
+++ Geometry/VeryForwardGeometry/interface/CTPPSPixelTopology.h
@@ -1,57 +1,44 @@
-#ifndef Geometry_Veryforwardgeometry_RPix_DET_TOPOLOGY_H
-#define Geometry_Veryforwardgeometry_RPix_DET_TOPOLOGY_H
+#ifndef Geometry_VeryForwardGeometry_CTPPSPixelTopology_H
+#define Geometry_VeryForwardGeometry_CTPPSPixelTopology_H
 
 #include "TMath.h"
-#include "DataFormats/GeometryVector/interface/LocalVector.h"
-#include "DataFormats/Common/interface/Ref.h"
-#include "SimDataFormats/GeneratorProducts/interface/HepMCProduct.h"
 #include "CondFormats/CTPPSReadoutObjects/interface/CTPPSPixelIndices.h"
-
-using namespace std; 
-namespace HepMC {
-	class ThreeVector;
-}
-
 /**
  *\brief Geometrical and topological information on RPix silicon detector.
  * Uses coordinate a frame with origin in the center of the wafer.
  **/
+
 class CTPPSPixelTopology
 {
   public:
     CTPPSPixelTopology();
     ~CTPPSPixelTopology();
 
+    static constexpr double pitch_simY_ = 150E-3;
+    static constexpr double pitch_simX_ = 100E-3;
+    static constexpr double thickness_ = 0.23;
+    static constexpr unsigned short no_of_pixels_simX_ = 160;  
+    static constexpr unsigned short no_of_pixels_simY_ = 156;  
+    static constexpr unsigned short no_of_pixels_ = 160*156;  
+    static constexpr double simX_width_ = 16.6;
+    static constexpr double simY_width_ = 24.4;
+    static constexpr double dead_edge_width_ = 200E-3;
+    static constexpr double active_edge_sigma_ = 0.02;
+    static constexpr double phys_active_edge_dist_ = 0.150;
 
- 
+    inline double detPitchSimX() const {return pitch_simX_;}
+    inline double detPitchSimY() const {return pitch_simY_;}
+    inline double detThickness() const {return thickness_;}
+    inline unsigned short detPixelSimXNo() const {return no_of_pixels_simX_;}
+    inline unsigned short detPixelSimYNo() const {return no_of_pixels_simY_;}
+    inline unsigned short detPixelNo() const {return no_of_pixels_;}
+    inline double detXWidth() const {return simX_width_;}
+    inline double detYWidth() const {return simY_width_;}
+    inline double detDeadEdgeWidth() const {return dead_edge_width_;}
+    inline double activeEdgeSigma() const {return active_edge_sigma_;}
+    inline double physActiveEdgeDist() const {return phys_active_edge_dist_;}
 
-    inline double DetXWidth() const {return simX_width_;}
-    inline double DetYWidth() const {return simY_width_;}
-//   inline double DetEdgeLength() const {return phys_edge_lenght_;}
-    inline double DetDeadEdgeWidth() const {return dead_edge_width_;}
-    inline double DetThickness() const {return thickness_;}
-    inline double DetPitchSimX() const {return pitch_simX_;}
-    inline double DetPitchSimY() const {return pitch_simY_;}
-    inline unsigned short DetPixelSimXNo() const {return no_of_pixels_simX_;}
-    inline unsigned short DetPixelSimYNo() const {return no_of_pixels_simY_;}
-    inline unsigned short DetPixelNo() const {return no_of_pixels_;}
-    CTPPSPixelIndices *Indici() const {return indici_;}
-
-      
-  public:
-
-    static double pitch_simY_;
-    static double pitch_simX_;
-    static double thickness_;
-    static unsigned short no_of_pixels_simX_;  
-    static unsigned short no_of_pixels_simY_;  
-    static unsigned short no_of_pixels_;  
-    static double simX_width_;
-    static double simY_width_;
-    static double dead_edge_width_;
-
-    CTPPSPixelIndices *indici_;
-
+    CTPPSPixelIndices indici_;    
 };
 
 #endif 
