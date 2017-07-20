a94e3d4bb1d350114559832604ebf542182d38da
bobaekim <bokim@cern.ch>
2017-07-20 17:50:37 +0200

    update for running dijetdqm code

--- DQMOffline/Trigger/python/JetMETPromptMonitor_cff.py
+++ DQMOffline/Trigger/python/JetMETPromptMonitor_cff.py
@@ -1,7 +1,9 @@
 import FWCore.ParameterSet.Config as cms
 
 from DQMOffline.Trigger.JetMonitor_cff import *
+from DQMOffline.Trigger.DiJetMonitor_cff import *
 
 jetmetMonitorHLT = cms.Sequence(
     HLTJetmonitoring
+    *HLTDiJetmonitoring
 )
