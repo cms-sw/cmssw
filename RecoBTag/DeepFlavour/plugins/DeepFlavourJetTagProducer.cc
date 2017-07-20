2862a754ec3f6d19dbba7659d55c0f6a80d5a09c
Pablo de Castro <pablodecm@gmail.com>
2017-07-20 17:25:55 +0200

    load TF graph and prepare inputs/outpus

--- RecoBTag/DeepFlavour/plugins/DeepFlavourJetTagProducer.cc
+++ RecoBTag/DeepFlavour/plugins/DeepFlavourJetTagProducer.cc
@@ -14,6 +14,9 @@
 
 #include "DataFormats/DeepFormats/interface/DeepFlavourTagInfo.h"
 
+#include "DNN/Tensorflow/interface/Graph.h"
+#include "DNN/Tensorflow/interface/Tensor.h"
+
 
 class DeepFlavourJetTagProducer : public edm::stream::EDProducer<> {
 
@@ -33,20 +36,49 @@ class DeepFlavourJetTagProducer : public edm::stream::EDProducer<> {
 	  virtual void endStream() override {}
 
 	  const edm::EDGetTokenT< TagInfoCollection > src_;
+    std::string graph_path_;
     std::vector<std::string> outputs_;
+    std::vector<std::string> input_names_;
+    std::vector<std::string> output_names_;
+
+    // graph for TF evaluation
+    dnn::tf::Graph graph_;
+    // not owing vector of pointers for inputs and outputs
+    std::vector<dnn::tf::Tensor *> dnn_inputs_;
+    std::vector<dnn::tf::Tensor *> dnn_outputs_;
+
 
 };
 
 DeepFlavourJetTagProducer::DeepFlavourJetTagProducer(const edm::ParameterSet& iConfig) :
   src_(consumes<TagInfoCollection>(iConfig.getParameter<edm::InputTag>("src"))),
-  outputs_(iConfig.getParameter<std::vector<std::string>>("outputs"))
+  graph_path_(iConfig.getParameter<std::string>("graph_path")),
+  outputs_(iConfig.getParameter<std::vector<std::string>>("outputs")),
+  input_names_(iConfig.getParameter<std::vector<std::string>>("input_names")),
+  output_names_(iConfig.getParameter<std::vector<std::string>>("output_names")),
+  graph_(graph_path_)
 {
-
-  edm::LogInfo("DeepFlavourJetTags") << "outputs size: " << outputs_.size() << " " <<  outputs_.at(0);
 
   for (const auto & output : outputs_) {
     produces<JetTagCollection>(output);
   }
+
+  for (const auto & input_name : input_names_) {
+    dnn_inputs_.emplace_back(graph_.defineInput(new dnn::tf::Tensor(input_name)));
+  }
+
+  // required because of batch norm
+  auto learning_phase_name = "cpf_batchnorm0/keras_learning_phase:0";
+  auto learning_phase = graph_.defineInput(new dnn::tf::Tensor(learning_phase_name));
+  learning_phase->setArray(0, nullptr);
+  learning_phase->setValue<bool>(false);
+
+  for (const auto & output_name : output_names_) {
+    dnn_outputs_.emplace_back(graph_.defineOutput(new dnn::tf::Tensor(output_name)));
+  }
+
+
+  
 
 }
 
@@ -67,6 +99,24 @@ void DeepFlavourJetTagProducer::produce(edm::Event& iEvent, const edm::EventSetu
 
   std::vector<std::unique_ptr<JetTagCollection>> output_tags;
 
+  auto n_jets = dnn::tf::Shape(tag_infos->size());
+  std::vector<std::vector<dnn::tf::Shape>> input_sizes {
+    {n_jets, 15},         // input_1 - global jet features
+    {n_jets, 25, 17},     // input_2 - charged pf
+    {n_jets, 25, 6},      // input_3 - neutral pf
+    {n_jets, 4, 12},      // input_4 - vertices 
+    {n_jets, 1}           // input_5 - jet pt for reg 
+  };
+  
+  // initalize inputs
+  for (std::size_t i=0; i < input_sizes.size(); i++) {
+    auto & input_shape = input_sizes.at(i);
+    dnn_inputs_.at(i)->setArray(input_shape.size(),
+                                &input_shape[0]);
+  }
+
+  graph_.eval();
+
   // create output collection
   for (std::size_t i=0; i < outputs_.size(); i++) {
     if (tag_infos->size() > 0) {
@@ -77,6 +127,8 @@ void DeepFlavourJetTagProducer::produce(edm::Event& iEvent, const edm::EventSetu
       output_tags.emplace_back(std::make_unique<JetTagCollection>());
     }
   }
+
+  
 
   for (std::size_t i=0; i < outputs_.size(); i++) {
     iEvent.put(std::move(output_tags.at(i)), outputs_.at(i));
--- RecoBTag/DeepFlavour/python/DeepFlavourJetTags_cfi.py
+++ RecoBTag/DeepFlavour/python/DeepFlavourJetTags_cfi.py
@@ -1,7 +1,14 @@
 import FWCore.ParameterSet.Config as cms
 
+
+input_names = ["input_{}:0".format(i) for i in range(1,6)]
+output_names = ["ID_pred/Softmax:0", "regression_pred/BiasAdd:0"]
+
 pfDeepFlavourJetTags = cms.EDProducer(
     'DeepFlavourJetTagProducer',
     src = cms.InputTag('DeepFlavourTagInfos'),
+    graph_path = cms.string('/afs/cern.ch/work/p/pdecastr/public/Deep/models_19072017/KERAS_model.h5_tfsession/tf'),
     outputs = cms.vstring(["probb"]),
+    input_names = cms.vstring(input_names),
+    output_names = cms.vstring(output_names)
 )
