dd85024b071d8f197461b12de3e7e37908c4e800
bcourbon <benoit.courbon@cern.ch>
2017-07-21 16:17:44 +0200

    new structure for HGCalShowerShape

--- L1Trigger/L1THGCal/interface/be_algorithms/HGCalShowerShape.h
+++ L1Trigger/L1THGCal/interface/be_algorithms/HGCalShowerShape.h
@@ -9,6 +9,7 @@
 #include "DataFormats/GeometryVector/interface/GlobalPoint.h"
 #include "DataFormats/GeometryVector/interface/GlobalVector.h"
 #include "DataFormats/L1THGCal/interface/HGCalCluster.h"
+#include "DataFormats/L1THGCal/interface/HGCalMulticluster.h"
 
    
     class HGCalShowerShape{
@@ -19,37 +20,27 @@
 
     ~HGCalShowerShape(){}
 
-    //void Init2D(const edm::PtrVector<l1t::HGCalTriggerCell> & triggerCellsPtrs);
-
-    //void Init3D(const edm::PtrVector<l1t::HGCalCluster> & clustersPtr);
-
-    void make2DshowerShape(const edm::PtrVector<l1t::HGCalTriggerCell> & triggerCellsPtrs);
+    int firstLayer(const l1t::HGCalMulticluster& c3d);
+	int nLayers(const l1t::HGCalMulticluster& c3d);
   
-    void makeHGCalProfile(const edm::PtrVector<l1t::HGCalCluster> & clustersPtrs);
-
-    int firstLayer() const {return firstLayer_;} 
-	int nLayers() const {return nLayers_;} 
+    float EMax(const l1t::HGCalMulticluster& c3d);  
+    //float E0(const l1t::HGCalMulticluster& c3d);   
+    //int EMaxLayer(const l1t::HGCalMulticluster& c3d);
   
-    float EMax() const {return EMax_;}    
-    float E0() const {return E0_;}    
-    int EMaxLayer() const {return EMaxLayer_;} 
-  
-    float SigmaZZ() const {return SigmaZZ_;}
-    float Zmean() const {return Zmean_;}          
+    float SigmaZZ(const l1t::HGCalMulticluster& c3d);
+    //float Zmean(const l1t::HGCalMulticluster& c3d);        
 
-    float SigmaEtaEta() const {return SigmaEtaEta_;}      
-    float SigmaEtaEtaMax() const {return SigmaEtaEtaMax_;}    
-    float SigmaEtaEta0() const {return SigmaEtaEta0_;}    
-    int SigmaEtaEtaMaxLayer() const {return SigmaEtaEtaMaxLayer_;} 
+    float SigmaEtaEtaTot(const l1t::HGCalMulticluster& c3d);
+    float SigmaEtaEta2D(const l1t::HGCalCluster& c2d);       
+    float SigmaEtaEtaMax(const l1t::HGCalMulticluster& c3d);   
+    //float SigmaEtaEta0(const l1t::HGCalMulticluster& c3d);   
+    //int SigmaEtaEtaMaxLayer(const l1t::HGCalMulticluster& c3d);
 
-    float SigmaPhiPhi() const {return SigmaPhiPhi_;}      
-    float SigmaPhiPhiMax() const {return SigmaPhiPhiMax_;}    
-    float SigmaPhiPhi0() const {return SigmaPhiPhi0_;}    
-    int SigmaPhiPhiMaxLayer() const {return SigmaPhiPhiMaxLayer_;} 
-
-    float dEtaMax() const {return dEtaMax_;}  
-    float dPhiMax() const {return dPhiMax_;}  
-
+    float SigmaPhiPhiTot(const l1t::HGCalMulticluster& c3d);
+    float SigmaPhiPhi2D(const l1t::HGCalCluster& c2d);       
+    float SigmaPhiPhiMax(const l1t::HGCalMulticluster& c3d);   
+    //float SigmaPhiPhi0(const l1t::HGCalMulticluster& c3d);   
+    //int SigmaPhiPhiMaxLayer(const l1t::HGCalMulticluster& c3d);
 
 
     private: 
@@ -57,34 +48,9 @@
     float SigmaEtaEta(std::vector<float> energy, std::vector<float> eta);
     float SigmaPhiPhi(std::vector<float> energy, std::vector<float> phi);   
     float SigmaZZ(std::vector<float> energy, std::vector<float> z);
-    float Zmean(std::vector<float> energy, std::vector<float> z);
+    //float Zmean(std::vector<float> energy, std::vector<float> z);
 
-    float SigmaZZ_; 
-    float Zmean_; 
 
-    float EMax_;
-    float E0_;
-    int EMaxLayer_;
-
-    float SigmaEtaEta_;
-    float SigmaEtaEtaMax_;
-    float SigmaEtaEta0_;
-    int SigmaEtaEtaMaxLayer_;
-
-    float SigmaPhiPhi_;
-    float SigmaPhiPhiMax_;
-    float SigmaPhiPhi0_;
-    int SigmaPhiPhiMaxLayer_;
-
-    float dEtaMax_;
-    float dPhiMax_;
-
-    float dEta(std::vector<float> energy, std::vector<float> eta);
-    float dPhi(std::vector<float> energy, std::vector<float> phi);  
-
-    float deltaR(float eta1,float phi1, float eta2, float phi2);
-   
-    int firstLayer_, lastLayer_, nLayers_;
 
     };
 
