8078e743c2a42dd85f1c9e44a276e7496f162c9c
perrozzi <perrozzi@cern.ch>
2017-07-22 00:39:06 +0200

    restore files used in runTheMatrix.py

---
+++ Configuration/Generator/python/PhotonJet_Pt_10_8TeV_TuneCUETP8M1_cfi.py
@@ -0,0 +1,23 @@
+import FWCore.ParameterSet.Config as cms
+from Configuration.Generator.Pythia8CommonSettings_cfi import *
+from Configuration.Generator.Pythia8CUEP8M1Settings_cfi import *
+
+generator = cms.EDFilter("Pythia8GeneratorFilter",
+                         pythiaHepMCVerbosity = cms.untracked.bool(False),
+                         maxEventsToPrint = cms.untracked.int32(0),
+                         pythiaPylistVerbosity = cms.untracked.int32(0),
+                         filterEfficiency = cms.untracked.double(1.0),
+                         comEnergy = cms.double(8000.0),
+                         PythiaParameters = cms.PSet(
+        pythia8CommonSettingsBlock,
+        pythia8CUEP8M1SettingsBlock,
+        processParameters = cms.vstring(
+            'PromptPhoton:all = on',
+            'PhaseSpace:pTHatMin = 10.',
+            ),
+        parameterSets = cms.vstring('pythia8CommonSettings',
+                                    'pythia8CUEP8M1Settings',
+                                    'processParameters',
+                                    )
+        )
+                         )
