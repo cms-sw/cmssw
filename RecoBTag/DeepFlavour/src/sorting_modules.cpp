72bc152c2dbea227203736648fa90cf5f0df7433
Pablo de Castro <pablodecm@gmail.com>
2017-07-23 13:23:54 +0200

    fix new sorting integration

--- RecoBTag/DeepFlavour/src/sorting_modules.cpp
+++ RecoBTag/DeepFlavour/src/sorting_modules.cpp
@@ -5,8 +5,8 @@
 #include <iostream>
 namespace sorting{
 
-std::vector<size_t> invertSortingVector(const std::vector<sortingClass<size_t> > & in){
-    size_t max=0;
+std::vector<std::size_t> invertSortingVector(const std::vector<sortingClass<std::size_t> > & in){
+    std::size_t max=0;
     for(const auto& s:in){
         if(s.get()>max)max=s.get();
     }
@@ -18,8 +18,8 @@ std::vector<size_t> invertSortingVector(const std::vector<sortingClass<size_t> >
         throw std::runtime_error("sorting vector size more than 1k ");
         
     }
-    std::vector<size_t> out(max+1,0);
-    for(size_t i=0;i<in.size();i++){
+    std::vector<std::size_t> out(max+1,0);
+    for(std::size_t i=0;i<in.size();i++){
         out.at(in.at(i).get())=i;
     }
 
