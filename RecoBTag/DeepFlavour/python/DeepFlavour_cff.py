5cdad26f6444b7b9ebe5b879a4c8457f493a5b22
Pablo de Castro <pablodecm@gmail.com>
2017-07-24 16:15:07 +0200

    fix default model label and create a new DeepFlavour cms.Task

--- RecoBTag/DeepFlavour/python/DeepFlavour_cff.py
+++ RecoBTag/DeepFlavour/python/DeepFlavour_cff.py
@@ -2,3 +2,6 @@ import FWCore.ParameterSet.Config as cms
 
 from RecoBTag.DeepFlavour.DeepFlavourTagInfos_cfi import pfDeepFlavourTagInfos
 from RecoBTag.DeepFlavour.DeepFlavourJetTags_cfi import pfDeepFlavourJetTags
+
+
+pfDeepFlavourTaskNew = cms.Task(pfDeepFlavourTagInfos, pfDeepFlavourJetTags)
