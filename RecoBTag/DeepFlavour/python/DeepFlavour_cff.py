d46adf1fd7a70455cae2cb2d6acc23f12bbb3c7b
Pablo de Castro <pablodecm@gmail.com>
2017-07-24 22:48:27 +0200

    add pv association quatily to producer and also sequence

--- RecoBTag/DeepFlavour/python/DeepFlavour_cff.py
+++ RecoBTag/DeepFlavour/python/DeepFlavour_cff.py
@@ -2,6 +2,9 @@ import FWCore.ParameterSet.Config as cms
 
 from RecoBTag.DeepFlavour.DeepFlavourTagInfos_cfi import pfDeepFlavourTagInfos
 from RecoBTag.DeepFlavour.DeepFlavourJetTags_cfi import pfDeepFlavourJetTags
+from CommonTools.PileupAlgos.Puppi_cff import puppi
+from PhysicsTools.PatAlgos.slimming.primaryVertexAssociation_cfi import primaryVertexAssociation
 
 
-pfDeepFlavourTaskNew = cms.Task(pfDeepFlavourTagInfos, pfDeepFlavourJetTags)
+pfDeepFlavourTaskNew = cms.Task(puppi, primaryVertexAssociation,
+                                pfDeepFlavourTagInfos, pfDeepFlavourJetTags)
