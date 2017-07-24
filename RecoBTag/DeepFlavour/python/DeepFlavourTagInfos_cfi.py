b7cebb0efc25b6913f9a6f6647fb59cfa85d5535
Pablo de Castro <pablodecm@gmail.com>
2017-07-24 12:53:39 +0200

    revert back to use only uncorrected

--- RecoBTag/DeepFlavour/python/DeepFlavourTagInfos_cfi.py
+++ RecoBTag/DeepFlavour/python/DeepFlavourTagInfos_cfi.py
@@ -3,7 +3,6 @@ import FWCore.ParameterSet.Config as cms
 pfDeepFlavourTagInfos = cms.EDProducer(
 	'DeepFlavourTagInfoProducer',
   jets = cms.InputTag("slimmedJets"),
-  jets_uncorrected = cms.InputTag("slimmedJets"),
   vertices = cms.InputTag("offlineSlimmedPrimaryVertices"),
   secondary_vertices = cms.InputTag("slimmedSecondaryVertices"),
   shallow_tag_infos = cms.InputTag('pfDeepCSVTagInfos'),
