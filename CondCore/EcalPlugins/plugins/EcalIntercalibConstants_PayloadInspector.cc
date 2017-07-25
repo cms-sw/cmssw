1985681c7eaafcb8a7d35db4d619c1d7dfdb63a1
Pierre Depasse <pierre.depasse@cern.ch>
2017-07-25 14:44:36 +0200

    Ecal Payload Inspector with Plot Image method (v2)

--- CondCore/EcalPlugins/plugins/EcalIntercalibConstants_PayloadInspector.cc
+++ CondCore/EcalPlugins/plugins/EcalIntercalibConstants_PayloadInspector.cc
@@ -17,8 +17,6 @@
 #include <memory>
 #include <sstream>
 
-
-
 namespace {
   enum {kEBChannels = 61200, kEEChannels = 14648};
   enum {MIN_IETA = 1, MIN_IPHI = 1, MAX_IETA = 85, MAX_IPHI = 360, EBhistEtaMax = 171};   // barrel lower and upper bounds on eta and phi
@@ -193,11 +191,123 @@ namespace {
     }// fill method
   };
 
+  /*****************************************************************
+     2d plot of ECAL IntercalibConstants difference between 2 IOVs
+  *****************************************************************/
+  class EcalIntercalibConstantsDiff : public cond::payloadInspector::PlotImage<EcalIntercalibConstants> {
+
+  public:
+    EcalIntercalibConstantsDiff() : cond::payloadInspector::PlotImage<EcalIntercalibConstants>("ECAL Intercalib Constants difference ") {
+      setSingleIov(false);
+    }
+
+    bool fill( const std::vector<std::tuple<cond::Time_t,cond::Hash> >& iovs ){
+      TH2F* barrel = new TH2F("EB", "mean EB", MAX_IPHI, 0, MAX_IPHI, 2 * MAX_IETA, -MAX_IETA, MAX_IETA);
+      TH2F* endc_p = new TH2F("EE+", "mean EE+", IX_MAX, IX_MIN, IX_MAX + 1, IY_MAX, IY_MIN, IY_MAX + 1);
+      TH2F* endc_m = new TH2F("EE-", "mean EE-", IX_MAX, IX_MIN, IX_MAX + 1, IY_MAX, IY_MIN, IY_MAX + 1);
+      float pEBmin, pEEmin, pEBmax, pEEmax;
+	pEBmin = 10.;
+	pEEmin = 10.;
+	pEBmax = -10.;
+	pEEmax = -10.;
+
+      unsigned int run[2], irun = 0;
+      float pEB[kEBChannels], pEE[kEEChannels];
+      for ( auto const & iov: iovs) {
+	std::shared_ptr<EcalIntercalibConstants> payload = fetchPayload( std::get<1>(iov) );
+	run[irun] = std::get<0>(iov);
+	if( payload.get() ){
+	  if (!payload->barrelItems().size()) return false;
+	  for(int cellid = EBDetId::MIN_HASH; cellid < EBDetId::kSizeForDenseIndexing; ++cellid) {
+	    uint32_t rawid = EBDetId::unhashIndex(cellid);
+	    EcalFloatCondObjectContainer::const_iterator value_ptr =  payload->find(rawid);
+	    if (value_ptr == payload->end()) continue; // cell absent from payload
+	    float weight = (float)(*value_ptr);
+	    if(irun == 0) 
+	      pEB[cellid] = weight;
+	    else {
+	      Double_t phi = (Double_t)(EBDetId(rawid)).iphi() - 0.5;
+	      Double_t eta = (Double_t)(EBDetId(rawid)).ieta();
+	      if(eta > 0.) eta = eta - 0.5;   //   0.5 to 84.5
+	      else eta  = eta + 0.5;         //  -84.5 to -0.5
+	      double diff = weight - pEB[cellid];
+	      std::cout << " channel " << cellid << " new " << weight << " old " << pEB[cellid] << " diff " << diff << std::endl;
+	      //	      if(diff < pEBmin) pEBmin = diff;
+	      //	      if(diff > pEBmax) pEBmax = diff;
+	      if(diff < pEBmin) {
+		pEBmin = diff;
+		std::cout << " new min " << pEBmin << std::endl;
+	      }
+	      if(diff > pEBmax) {
+		pEBmax = diff;
+		std::cout << " new max " << pEBmax << std::endl;
+	      }
+	      barrel->Fill(phi, eta, diff);
+	    }
+	  }// loop over cellid
+
+	  if (!payload->endcapItems().size()) return false;
+	  // looping over the EE channels
+	  for(int iz = -1; iz < 2; iz = iz + 2)   // -1 or +1
+	    for(int iy = IY_MIN; iy < IY_MAX+IY_MIN; iy++)
+	      for(int ix = IX_MIN; ix < IX_MAX+IX_MIN; ix++)
+		if(EEDetId::validDetId(ix, iy, iz)) {
+		  EEDetId myEEId = EEDetId(ix, iy, iz, EEDetId::XYMODE);
+		  uint32_t cellid = myEEId.hashedIndex();
+		  uint32_t rawid = myEEId.rawId();
+		  EcalFloatCondObjectContainer::const_iterator value_ptr =  payload->find(rawid);
+		  if (value_ptr == payload->end()) continue; // cell absent from payload
+		  float weight = (float)(*value_ptr);
+		  if(irun == 0) 
+		    pEE[cellid] = weight;
+		  else {
+		    double diff = weight - pEE[cellid];
+		    if(diff < pEEmin) pEEmin = diff;
+		    if(diff > pEEmax) pEEmax = diff;
+		    if(iz == 1)
+		      endc_p->Fill(ix, iy, diff);
+		    else
+		      endc_m->Fill(ix, iy, diff);
+		  }
+		}  // validDetId 
+	}    // payload
+	irun++;
+      }      // loop over IOVs
+
+      gStyle->SetPalette(1);
+      gStyle->SetOptStat(0);      
+      TCanvas canvas("CC map","CC map", 1600, 450);
+      TLatex t1;
+      t1.SetNDC();
+      t1.SetTextAlign(26);
+      t1.SetTextSize(0.05);
+      t1.DrawLatex(0.5, 0.96, Form("Ecal IntercalibConstants, IOV %i - %i", run[1], run[0]));
+
+      float xmi[3] = {0.0 , 0.24, 0.76};
+      float xma[3] = {0.24, 0.76, 1.00};
+      TPad** pad = new TPad*;
+      for (int obj = 0; obj < 3; obj++) {
+	pad[obj] = new TPad(Form("p_%i", obj),Form("p_%i", obj), xmi[obj], 0.0, xma[obj], 0.94);
+	pad[obj]->Draw();
+      }
+      pad[0]->cd();
+      DrawEE(endc_m, pEEmin, pEEmax);
+      pad[1]->cd();
+      DrawEB(barrel, pEBmin, pEBmax);
+      pad[2]->cd();
+      DrawEE(endc_p, pEEmin, pEEmax);
+      canvas.SaveAs("ecalICdiff.png");
+
+      return true;
+    }// fill method
+  };
+
 } // close namespace
 
 // Register the classes as boost python plugin
 PAYLOAD_INSPECTOR_MODULE( EcalIntercalibConstants ){
-  PAYLOAD_INSPECTOR_CLASS( EcalIntercalibConstantsEBMap );
-  PAYLOAD_INSPECTOR_CLASS( EcalIntercalibConstantsEEMap );
-  PAYLOAD_INSPECTOR_CLASS( EcalIntercalibConstantsPlot );
+  PAYLOAD_INSPECTOR_CLASS( EcalIntercalibConstantsEBMap);
+  PAYLOAD_INSPECTOR_CLASS( EcalIntercalibConstantsEEMap);
+  PAYLOAD_INSPECTOR_CLASS( EcalIntercalibConstantsPlot);
+  PAYLOAD_INSPECTOR_CLASS( EcalIntercalibConstantsDiff);
 }
