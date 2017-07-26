6db8c4370e7abf4c0a5d8a41d4de51421c4de6b8
Marco Rovere <marco.rovere@cern.ch>
2017-07-26 21:19:49 +0200

    electronGsfTracking w/ HGCal Multiclusters

--- TrackingTools/GsfTracking/python/GsfElectronTracking_cff.py
+++ TrackingTools/GsfTracking/python/GsfElectronTracking_cff.py
@@ -5,8 +5,22 @@ from RecoEgamma.EgammaElectronProducers.ecalDrivenElectronSeeds_cfi import *
 from RecoParticleFlow.PFTracking.mergedElectronSeeds_cfi import *
 
 electronSeeds = cms.Sequence(trackerDrivenElectronSeeds*ecalDrivenElectronSeeds*electronMergedSeeds) 
+_electronSeedsFromMC = electronSeeds.copy()
+_electronSeedsFromMC += cms.Sequence(ecalDrivenElectronSeedsFromMC*electronMergedSeedsFromMC)
+
+from Configuration.Eras.Modifier_phase2_hgcal_cff import phase2_hgcal
+phase2_hgcal.toReplaceWith(
+  electronSeeds, _electronSeedsFromMC
+)
 
 
 from TrackingTools.GsfTracking.CkfElectronCandidateMaker_cff import *
 from TrackingTools.GsfTracking.GsfElectronGsfFit_cff import *
 electronGsfTracking = cms.Sequence(electronSeeds*electronCkfTrackCandidates*electronGsfTracks)
+_electronGsfTracking = electronGsfTracking.copy()
+_electronGsfTracking += cms.Sequence(electronCkfTrackCandidatesFromMC*electronGsfTracksFromMC)
+
+from Configuration.Eras.Modifier_phase2_hgcal_cff import phase2_hgcal
+phase2_hgcal.toReplaceWith(
+  electronGsfTracking, _electronGsfTracking
+)
