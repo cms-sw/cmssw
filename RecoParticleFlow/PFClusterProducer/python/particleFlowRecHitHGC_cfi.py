8c5e66459b93951a762714b27a0239828f7dfe34
Felice Pantaleo <felicepantaleo@users.noreply.github.com>
2017-07-26 09:44:25 +0200

    adding comment to configuration

--- RecoParticleFlow/PFClusterProducer/python/particleFlowRecHitHGC_cfi.py
+++ RecoParticleFlow/PFClusterProducer/python/particleFlowRecHitHGC_cfi.py
@@ -22,6 +22,8 @@ particleFlowRecHitHGC = cms.EDProducer("PFRecHitProducer",
              src  = cms.InputTag("HGCalRecHit:HGCEERecHits"),
              geometryInstance = cms.string("HGCalEESensitive"),
              qualityTests = cms.VPSet(
+# Enabling PFRecHitQTestHGCalThresholdSNR will filter out of the PFRecHits, all the HGCRecHits with energy not exceeding
+# 5 sigma noise                 
 #                 cms.PSet(
 #                     name = cms.string("PFRecHitQTestHGCalThresholdSNR"),
 #                     thresholdSNR = cms.double(5.0),
@@ -33,6 +35,8 @@ particleFlowRecHitHGC = cms.EDProducer("PFRecHitProducer",
              src  = cms.InputTag("HGCalRecHit:HGCHEFRecHits"),
              geometryInstance = cms.string("HGCalHESiliconSensitive"),
              qualityTests = cms.VPSet(
+# Enabling PFRecHitQTestHGCalThresholdSNR will filter out of the PFRecHits, all the HGCRecHits with energy not exceeding
+# 5 sigma noise                     
 #                 cms.PSet(
 #                     name = cms.string("PFRecHitQTestHGCalThresholdSNR"),
 #                     thresholdSNR = cms.double(5.0),
@@ -44,6 +48,8 @@ particleFlowRecHitHGC = cms.EDProducer("PFRecHitProducer",
              src  = cms.InputTag("HGCalRecHit:HGCHEBRecHits"),
              geometryInstance = cms.string(""),
              qualityTests = cms.VPSet(
+# Enabling PFRecHitQTestHGCalThresholdSNR will filter out of the PFRecHits, all the HGCRecHits with energy not exceeding
+# 5 sigma noise                     
 #                 cms.PSet(
 #                     name = cms.string("PFRecHitQTestHGCalThresholdSNR"),
 #                     thresholdSNR = cms.double(5.0),
