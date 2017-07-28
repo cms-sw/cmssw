e46bb15386485d3be4cd628aef8826a89e02b5dc
Jan Kaspar <jan.kaspar@cern.ch>
2017-07-28 16:40:07 +0200

    Added script to run all tests.

---
+++ Validation/CTPPS/test/run_all_tests.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+function RunOne()
+{
+	local tag="$1"
+
+	cmsRun "${tag}_cfg.py" &> "${tag}.log" &
+}
+
+
+cd "optics_parametrisation_validation"
+RunOne "plot_optical_functions"
+cd - &> /dev/null
+
+
+cd "fast_simu_validation"
+RunOne "test_acceptance_shape"
+RunOne "test_reco_simu_diff_with_det_sm"
+RunOne "test_reco_simu_diff_without_det_sm"
+RunOne "test_y_vs_x_profile"
+cd - &> /dev/null
+
+
+cd "fast_simu_with_phys_generator/qgsjet/global"
+RunOne "test"
+cd - &> /dev/null
+
+cd "fast_simu_with_phys_generator/qgsjet/relative_to_beam"
+RunOne "test"
+cd - &> /dev/null
