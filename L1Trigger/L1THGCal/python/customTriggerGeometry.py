9212edff799a6db2619af31f309b6c4d7cc47b0a
Jean-Baptiste Sauvan <jean-baptiste.sauvan@cern.ch>
2017-07-28 10:48:48 +0200

    Customize MaxCellsInModule in geometry custom functions

--- L1Trigger/L1THGCal/python/customTriggerGeometry.py
+++ L1Trigger/L1THGCal/python/customTriggerGeometry.py
@@ -7,6 +7,8 @@ def custom_geometry_ZoltanSplit_V8(process):
     process.hgcalTriggerGeometryESProducer.TriggerGeometry.L1TCellNeighborsMapping = cms.FileInPath("L1Trigger/L1THGCal/data/triggercell_neighbor_mapping_8inch_aligned_192_432_0.txt")
     process.hgcalTriggerGeometryESProducer.TriggerGeometry.L1TCellsBHMapping = cms.FileInPath("L1Trigger/L1THGCal/data/triggercell_mapping_BH_3x3_30deg_0.txt")
     process.hgcalTriggerGeometryESProducer.TriggerGeometry.L1TCellNeighborsBHMapping = cms.FileInPath("L1Trigger/L1THGCal/data/triggercell_neighbor_mapping_BH_3x3_30deg_0.txt")
+    process.hgcalTriggerPrimitiveDigiProducer.FECodec.MaxCellsInModule = cms.uint32(288)
+    process.hgcalTriggerPrimitiveDigiProducer.BEConfiguration.algorithms[0].FECodec.MaxCellsInModule = cms.uint32(288)
     return process
 
 
@@ -17,6 +19,8 @@ def custom_geometry_ZoltanSplit_V7(process):
     process.hgcalTriggerGeometryESProducer.TriggerGeometry.L1TCellNeighborsMapping = cms.FileInPath("L1Trigger/L1THGCal/data/triggercell_neighbor_mapping_8inch_aligned_192_432_0.txt")
     process.hgcalTriggerGeometryESProducer.TriggerGeometry.L1TCellsBHMapping = cms.FileInPath("L1Trigger/L1THGCal/data/triggercell_mapping_BH_3x3_30deg_0.txt")
     process.hgcalTriggerGeometryESProducer.TriggerGeometry.L1TCellNeighborsBHMapping = cms.FileInPath("L1Trigger/L1THGCal/data/triggercell_neighbor_mapping_BH_3x3_30deg_0.txt")
+    process.hgcalTriggerPrimitiveDigiProducer.FECodec.MaxCellsInModule = cms.uint32(288)
+    process.hgcalTriggerPrimitiveDigiProducer.BEConfiguration.algorithms[0].FECodec.MaxCellsInModule = cms.uint32(288)
     return process
 
 def custom_geometry_6inch_V8(process):
@@ -25,6 +29,8 @@ def custom_geometry_6inch_V8(process):
     process.hgcalTriggerGeometryESProducer.TriggerGeometry.L1TModulesMapping = cms.FileInPath("L1Trigger/L1THGCal/data/module_mapping_PairsRing_V8_0.txt")
     process.hgcalTriggerGeometryESProducer.TriggerGeometry.L1TCellNeighborsMapping = cms.FileInPath("L1Trigger/L1THGCal/data/triggercell_neighbor_mapping_1.txt")
     process.hgcalTriggerGeometryESProducer.TriggerGeometry.L1TWaferNeighborsMapping = cms.FileInPath("L1Trigger/L1THGCal/data/wafer_neighbor_mapping_V8_0.txt")
+    process.hgcalTriggerPrimitiveDigiProducer.FECodec.MaxCellsInModule = cms.uint32(116)
+    process.hgcalTriggerPrimitiveDigiProducer.BEConfiguration.algorithms[0].FECodec.MaxCellsInModule = cms.uint32(116)
     return process
 
 def custom_geometry_6inch_V7(process):
@@ -33,4 +39,6 @@ def custom_geometry_6inch_V7(process):
     process.hgcalTriggerGeometryESProducer.TriggerGeometry.L1TModulesMapping = cms.FileInPath("L1Trigger/L1THGCal/data/module_mapping_PairsRing_V7_0.txt")
     process.hgcalTriggerGeometryESProducer.TriggerGeometry.L1TCellNeighborsMapping = cms.FileInPath("L1Trigger/L1THGCal/data/triggercell_neighbor_mapping_1.txt")
     process.hgcalTriggerGeometryESProducer.TriggerGeometry.L1TWaferNeighborsMapping = cms.FileInPath("L1Trigger/L1THGCal/data/wafer_neighbor_mapping_V7_0.txt")
+    process.hgcalTriggerPrimitiveDigiProducer.FECodec.MaxCellsInModule = cms.uint32(116)
+    process.hgcalTriggerPrimitiveDigiProducer.BEConfiguration.algorithms[0].FECodec.MaxCellsInModule = cms.uint32(116)
     return process
--- L1Trigger/L1THGCal/python/hgcalTriggerPrimitiveDigiProducer_cfi.py
+++ L1Trigger/L1THGCal/python/hgcalTriggerPrimitiveDigiProducer_cfi.py
@@ -30,7 +30,6 @@ thicknessCorrection_200 = thicknessCorrection[1]
 
 fe_codec = cms.PSet( CodecName  = cms.string('HGCalTriggerCellThresholdCodec'),
                      CodecIndex = cms.uint32(2),
-                     #  MaxCellsInModule = cms.uint32(116),
                      MaxCellsInModule = cms.uint32(288),
                      DataLength = cms.uint32(16),
                      linLSB = cms.double(triggerCellLsbBeforeCompression),
