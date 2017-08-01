9a80eee27bb88dba7d25551a8979cd884d31a039
Andrej Saibel <andrej.saibel@cern.ch>
2017-08-01 16:21:17 +0200

    removed unnecessary lines

--- PhysicsTools/JetMCAlgos/python/AK4GenJetFlavourInfos_cfi.py
+++ PhysicsTools/JetMCAlgos/python/AK4GenJetFlavourInfos_cfi.py
@@ -1,6 +1,5 @@
 import FWCore.ParameterSet.Config as cms
 from PhysicsTools.JetMCAlgos.AK4PFJetsMCFlavourInfos_cfi import ak4JetFlavourInfos
-#from PhysicsTools.PatAlgos.slimmedGenJets_cfi import slimmedGenJets
 
 ak4GenJetFlavourInfos = ak4JetFlavourInfos.clone(jets = "ak4GenJetsNoNu")
 
