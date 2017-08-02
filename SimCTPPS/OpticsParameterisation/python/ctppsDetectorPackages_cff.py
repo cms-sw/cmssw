e46d4de023cb034397c9aa086c63ba326ad4cf1d
Jan Kaspar <jan.kaspar@cern.ch>
2017-08-02 12:24:54 +0200

    Directory cleanup ; RPs z-position matched

--- SimCTPPS/OpticsParameterisation/python/ctppsDetectorPackages_cff.py
+++ SimCTPPS/OpticsParameterisation/python/ctppsDetectorPackages_cff.py
@@ -13,22 +13,22 @@ detectorPackages_2016PreTS2 = cms.VPSet(
     genericStripsPackage.clone(
         potId = cms.uint32(0x76100000), # 002
         interpolatorName = cms.string('ip5_to_station_150_h_1_lhcb2'),
-        zPosition = cms.double(-215.077), # z coordinate, m
+        zPosition = cms.double(-203.826), # z coordinate, m
     ),
     genericStripsPackage.clone(
         potId = cms.uint32(0x76180000), # 003
         interpolatorName = cms.string('ip5_to_station_150_h_2_lhcb2'),
-        zPosition = cms.double(-215.077), # z coordinate, m
+        zPosition = cms.double(-212.551), # z coordinate, m
     ),
     #----- sector 56
     genericStripsPackage.clone(
         potId = cms.uint32(0x77100000), # 102
         interpolatorName = cms.string('ip5_to_station_150_h_1_lhcb1'),
-        zPosition = cms.double(+215.077), # z coordinate, m
+        zPosition = cms.double(+203.826), # z coordinate, m
     ),
     genericStripsPackage.clone(
         potId = cms.uint32(0x77180000), # 103
         interpolatorName = cms.string('ip5_to_station_150_h_2_lhcb1'),
-        zPosition = cms.double(+215.077), # z coordinate, m
+        zPosition = cms.double(+212.551), # z coordinate, m
     ),
 )
