8c650127100a57207b4617fe5a42e325add3f08d
Antonio <antonio.vilela.pereira@cern.ch>
2017-08-03 00:19:34 +0200

    Remove configuration customisation functions for genPUProtons.

--- SimGeneral/DataMixingModule/python/addGenPUProtonsDataMixing.py
+++
@@ -1,6 +0,0 @@
-import FWCore.ParameterSet.Config as cms
-
-def customiseGenPUProtonsDataMixing(process):
-
-    process.mixData.GenPUProtonsInputTags = cms.VInputTag("genPUProtonsBx0", "genPUProtonsBxm1", "genPUProtonsBxp1")
-    return process
