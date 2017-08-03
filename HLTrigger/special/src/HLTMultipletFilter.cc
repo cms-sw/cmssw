152b18f9482141c1a4500efc1ba1facafe7acae2
Sunanda <sunanda.banerjee@cern.ch>
2017-08-03 16:56:41 +0200

    Add a range in BX

--- HLTrigger/special/src/HLTMultipletFilter.cc
+++ HLTrigger/special/src/HLTMultipletFilter.cc
@@ -20,6 +20,8 @@ HLTMultipletFilter::HLTMultipletFilter(const edm::ParameterSet& iConfig) : HLTFi
   hltMuonSeedLabel_  = iConfig.getParameter<edm::InputTag>("L1MuonInputTag");
   hltTauSeedLabel_   = iConfig.getParameter<edm::InputTag>("L1TauInputTag");
   minN_              = iConfig.getParameter<int>    ("MinN");
+  ibxMin_            = iConfig.getParameter<int>    ("IBxMin");
+  ibxMax_            = iConfig.getParameter<int>    ("IBxMax");
   minEta_            = iConfig.getParameter<double> ("MinEta");
   maxEta_            = iConfig.getParameter<double> ("MaxEta");
   minPhi_            = iConfig.getParameter<double> ("MinPhi");
@@ -57,6 +59,7 @@ HLTMultipletFilter::HLTMultipletFilter(const edm::ParameterSet& iConfig) : HLTFi
     hltTauToken_ = consumes<l1t::TauBxCollection>(hltTauSeedLabel_);
   }
   edm::LogVerbatim("Report") << "Input Parameters:: minN = " << minN_
+			     << " Bx Range = " << ibxMin_ << ":" << ibxMax_
 			     << " minPt = " << minPt_ << " Eta " << minEta_
 			     << ":" << maxEta_ << " Phi " << minPhi_ << ":" 
 			     << maxPhi_ << " GT Seed for EGamma " 
@@ -79,6 +82,8 @@ HLTMultipletFilter::fillDescriptions(edm::ConfigurationDescriptions& description
   desc.add<edm::InputTag>("L1MuonInputTag",edm::InputTag());
   desc.add<edm::InputTag>("L1TauInputTag",edm::InputTag("hltCaloStage2Digis:Tau"));
   desc.add<int>("MinN",1);
+  desc.add<int>("IBxMin",0);
+  desc.add<int>("IBxMax",0);
   desc.add<double>("MinEta",1.305);
   desc.add<double>("MaxEta",3.000);
   desc.add<double>("MinPhi",5.4105);
@@ -99,28 +104,27 @@ bool HLTMultipletFilter::hltFilter(edm::Event& iEvent, const edm::EventSetup& iS
   }
 
   bool      accept(false);
-  const int ibx(0);
   int       nobj(0);
 
   if ((!accept) && flag_[EGamma]) {
-    nobj += objects(iEvent,hltEGammaToken_,hltEGammaSeedLabel_,EGamma,ibx);
-    if (nobj > minN_) accept = true;
+    nobj += objects(iEvent,hltEGammaToken_,hltEGammaSeedLabel_,EGamma);
+    if (nobj >= minN_) accept = true;
   }
   if ((!accept) && flag_[EtSum]) {
-    nobj += objects(iEvent,hltEtSumToken_,hltEtSumSeedLabel_,EtSum,ibx);
-    if (nobj > minN_) accept = true;
+    nobj += objects(iEvent,hltEtSumToken_,hltEtSumSeedLabel_,EtSum);
+    if (nobj >= minN_) accept = true;
   }
   if ((!accept) && flag_[Jet]) {
-    nobj += objects(iEvent,hltJetToken_,hltJetSeedLabel_,Jet,ibx);
-    if (nobj > minN_) accept = true;
+    nobj += objects(iEvent,hltJetToken_,hltJetSeedLabel_,Jet);
+    if (nobj >= minN_) accept = true;
   }
   if ((!accept) && flag_[Muon]) {
-    nobj += objects(iEvent,hltMuonToken_,hltMuonSeedLabel_,Muon,ibx);
-    if (nobj > minN_) accept = true;
+    nobj += objects(iEvent,hltMuonToken_,hltMuonSeedLabel_,Muon);
+    if (nobj >= minN_) accept = true;
   }
   if ((!accept) && flag_[Tau]) {
-    nobj += objects(iEvent,hltTauToken_,hltTauSeedLabel_,Tau,ibx);
-    if (nobj > minN_) accept = true;
+    nobj += objects(iEvent,hltTauToken_,hltTauSeedLabel_,Tau);
+    if (nobj >= minN_) accept = true;
   }
       
   edm::LogVerbatim("Report") << "Selection flag " << accept;
@@ -131,7 +135,7 @@ template<typename T1>
 int HLTMultipletFilter::objects(edm::Event& iEvent, 
 				edm::EDGetTokenT<T1> const& hltToken,
 				edm::InputTag const& hltSeedLabel,
-				HLTMultipletFilter::Types type,int ibx) const {
+				HLTMultipletFilter::Types type) const {
   int nobj(0);
   edm::Handle<T1> objs;
   iEvent.getByToken(hltToken, objs);
@@ -140,16 +144,16 @@ int HLTMultipletFilter::objects(edm::Event& iEvent,
 			      << " requested, but not found in the event.";
   } else {
     edm::LogVerbatim("Report") << "Collection for type " << type 
-			       << " has " << objs->size() 
-			       << " BX's and " << objs->size(ibx)
-			       << " candidates in BX " << ibx;
-    for (typename T1::const_iterator p=objs->begin(ibx); 
-	 p!=objs->end(ibx); ++p) {
-      if (p->pt() > minPt_) {
-	if (p->eta() > minEta_ && p->eta() < maxEta_) {
-	  double phi = p->phi();
-	  if (phi < 0) phi += 2*M_PI;
-	  if (phi > minPhi_ && phi < maxPhi_) ++nobj;
+			       << " has " << objs->size() << " in "
+			       << ibxMin_ << ":" << ibxMax_ << " BX's";
+    for (int ibx=ibxMin_; ibx<=ibxMax_; ++ibx) {
+      for (auto p = objs->begin(ibx); p != objs->end(ibx); ++p) {
+	if (p->pt() > minPt_) {
+	  if (p->eta() > minEta_ && p->eta() < maxEta_) {
+	    double phi = p->phi();
+	    if (phi < 0) phi += 2*M_PI;
+	    if (phi > minPhi_ && phi < maxPhi_) ++nobj;
+	  }
 	}
       }
     }
