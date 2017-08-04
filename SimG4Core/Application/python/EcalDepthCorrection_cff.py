e24d77c50b54f484db9f3a72d7d47b449871181a
Vladimir <Vladimir.Ivantchenko@cern.ch>
2017-08-04 11:54:53 +0200

    added fragment

---
+++ SimG4Core/Application/python/EcalDepthCorrection_cff.py
@@ -0,0 +1,10 @@
+import FWCore.ParameterSet.Config as cms
+
+def customise(process):
+
+  # fragment allowing to simulate correct Ecal hit depth 
+
+  if hasattr(process,'g4SimHits'):
+    process.g4SimHits.ECalSD.IgnoreDepthCorr = cms.bool(False)
+
+    return(process)
