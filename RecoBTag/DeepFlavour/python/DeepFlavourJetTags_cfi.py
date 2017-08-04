4290ffed4932de36b80210751e7931a06644e559
Pablo de Castro <pablodecm@gmail.com>
2017-08-04 10:35:36 +0200

    fix paths, operation names, avoid dynamical tf::Tensors

--- RecoBTag/DeepFlavour/python/DeepFlavourJetTags_cfi.py
+++ RecoBTag/DeepFlavour/python/DeepFlavourJetTags_cfi.py
@@ -1,14 +1,14 @@
 import FWCore.ParameterSet.Config as cms
 
 
-input_names = ["input_{}:0".format(i) for i in range(1,6)]
-output_names = ["ID_pred/Softmax:0", "regression_pred/BiasAdd:0"]
+input_names = ["input_{}".format(i) for i in range(1,6)]
+output_names = ["ID_pred/Softmax", "regression_pred/BiasAdd"]
 
 pfDeepFlavourJetTags = cms.EDProducer(
     'DeepFlavourJetTagProducer',
     src = cms.InputTag('pfDeepFlavourTagInfos'),
     # path to .meta file (will be stripped)
-    graph_path = cms.FileInPath('RecoBTag/Combined/data/DeepFlavourV01/tf.meta'),
+    graph_path = cms.FileInPath('RecoBTag/Combined/data/DeepFlavourV01_C/saved_model.pb'),
     flav_table = cms.PSet(
                       probb = cms.vuint32([0]),
                       probbb = cms.vuint32([1]),
@@ -19,5 +19,5 @@ pfDeepFlavourJetTags = cms.EDProducer(
                       ),
     input_names = cms.vstring(input_names),
     output_names = cms.vstring(output_names),
-    lp_names = cms.vstring('cpf_input_batchnorm/keras_learning_phase:0')
+    lp_names = cms.vstring('cpf_input_batchnorm/keras_learning_phase')
 )
