6b4983f4a204af4a6db244eccbc36dc6c1230307
thomas madlener <thomas.madlener@cern.ch>
2017-08-09 10:30:54 +0200

    Add 2017 paths and remove obsolete 2016 paths.

--- HLTriggerOffline/HeavyFlavor/python/heavyFlavorValidationSequence_cff.py
+++ HLTriggerOffline/HeavyFlavor/python/heavyFlavorValidationSequence_cff.py
@@ -20,19 +20,6 @@ hfv5 = hfv1.clone(
     TriggerPathName = cms.untracked.string("HLT_DoubleMu4_LowMassNonResonantTrk_Displaced_v")
 )
 
-hfvQuadmu1 = hfv1.clone(
-    TriggerPathName = cms.untracked.string("HLT_Dimuon0_Jpsi_Muon_v")
-)
-hfvQuadmu2 = hfv1.clone(
-    TriggerPathName = cms.untracked.string("HLT_Dimuon0_Upsilon_Muon_v")
-)
-hfvQuadmu3 = hfv1.clone(
-    TriggerPathName = cms.untracked.string("HLT_QuadMuon0_Dimuon0_Jpsi_v")
-)
-hfvQuadmu4 = hfv1.clone(
-    TriggerPathName = cms.untracked.string("HLT_QuadMuon0_Dimuon0_Upsilon_v")
-)
-
 hfvTnP1 = hfv1.clone(
     TriggerPathName = cms.untracked.string("HLT_Mu7p5_L2Mu2_Jpsi_v")
 )
@@ -57,70 +44,80 @@ hfvTnP7 = hfv1.clone(
 hfvTnP8 = hfv1.clone(
     TriggerPathName = cms.untracked.string("HLT_Mu7p5_Track7_Upsilon_v")
 )
-hfvTnP9 = hfv1.clone(
-    TriggerPathName = cms.untracked.string("HLT_Dimuon0er16_Jpsi_NoOS_NoVertexing_v")
-)
-hfvTnP10 = hfv1.clone(
-    TriggerPathName = cms.untracked.string("HLT_Dimuon0er16_Jpsi_NoVertexing_v")
-)
-hfvTnP11 = hfv1.clone(
-    TriggerPathName = cms.untracked.string("HLT_Dimuon6_Jpsi_NoVertexing_v")
+
+## 2017 additions
+hfjpsiMuon = hfv1.clone(
+    TriggerPathName = cms.untracked.string("HLT_Dimuon0_Jpsi3p5_Muon2_v")
 )
 
-### Quarkonia 7E33
-hfvQ1 = hfv1.clone(
-    TriggerPathName = cms.untracked.string("HLT_Dimuon10_Jpsi_Barrel_v")
-)
-hfvQ2 = hfv1.clone(
-    TriggerPathName = cms.untracked.string("HLT_Dimuon16_Jpsi_v")
-)
-hfvQ3 = hfv1.clone(
-    TriggerPathName = cms.untracked.string("HLT_Dimuon8_PsiPrime_Barrel_v")
-)
-hfvQ4 = hfv1.clone(
-    TriggerPathName = cms.untracked.string("HLT_Dimuon8_Upsilon_Barrel_v")
-)
-hfvQ5 = hfv1.clone(
-    TriggerPathName = cms.untracked.string("HLT_Dimuon0_Phi_Barrel_v")
-)
-hfvQ6 = hfv1.clone(
-    TriggerPathName = cms.untracked.string("HLT_Mu16_TkMu0_dEta18_Onia_v")
-)
-hfvQ7 = hfv1.clone(
-    TriggerPathName = cms.untracked.string("HLT_Mu16_TkMu0_dEta18_Phi_v")
+hfjpsiNoVertexing = hfv1.clone(
+    TriggerPathName = cms.untracked.string("HLT_Dimuon0_Jpsi_NoVertexing_v")
 )
 
-hfv6 = hfv1.clone(
-    TriggerPathName = cms.untracked.string("HLT_Dimuon20_Jpsi_v"),
-    MuonPtBins = cms.untracked.vdouble(7.5, 10., 12.5, 15., 17.5, 20., 22.5, 25., 27.5, 30.),
-    DimuonPtBins = cms.untracked.vdouble(10., 12.5, 15., 17.5, 20., 22.5, 25., 27.5, 30.)
-)
-hfv7 = hfv6.clone(
-    TriggerPathName = cms.untracked.string("HLT_Dimuon13_PsiPrime_v"),
-)
-hfv8 = hfv6.clone(
-    TriggerPathName = cms.untracked.string("HLT_Dimuon13_Upsilon_v"),
-)
-hfv9 = hfv6.clone(
-    TriggerPathName = cms.untracked.string("HLT_Mu25_TkMu0_dEta18_Onia_v"),
+hfjpsiNoVertexingNoOS = hfv1.clone(
+    TriggerPathName = cms.untracked.string("HLT_Dimuon0_Jpsi_NoVertexing_NoOS_v")
 )
 
-### tau3mu
-hfvTau3mu = hfv1.clone(
-    TriggerPathName = cms.untracked.string("HLT_DoubleMu3_Trk_tau3mu_v")
+hfjpsiBarrel = hfv1.clone(
+    TriggerPathName = cms.untracked.string("HLT_Dimuon20_Jpsi_Barrel_Seagulls_v")
 )
 
-### DoubleMu0
-hfvDoubleMu0 = hfv1.clone(
-    TriggerPathName = cms.untracked.string("HLT_DoubleMu0")
+hfpsiBarrel = hfv1.clone(
+    TriggerPathName = cms.untracked.string("HLT_Dimuon10_PsiPrime_Barrel_Seagulls_v")
 )
+
+hfupsilonBarrel = hfv1.clone(
+    TriggerPathName = cms.untracked.string("HLT_Dimuon10_Upsilon_Barrel_Seagulls_v")
+)
+
+hfphiBarrel = hfv1.clone(
+    TriggerPathName = cms.untracked.string("HLT_Dimuon14_Phi_Barrel_Seagulls_v")
+)
+
+hfmu25Onia = hfv1.clone(
+    TriggerPathName = cms.untracked.string("HLT_Mu25_TkMu0_Onia_v")
+)
+
+hfmu30Onia = hfv1.clone(
+    TriggerPathName = cms.untracked.string("HLT_Mu30_TkMu0_Onia_v")
+)
+
+hfmu25Phi = hfv1.clone(
+    TriggerPathName = cms.untracked.string("HLT_Mu25_TkMu0_Phi_v")
+)
+
+hfmu20Phi = hfv1.clone(
+    TriggerPathName = cms.untracked.string("HLT_Mu20_TkMu0_Phi_v")
+)
+
+hfjpsi = hfv1.clone(
+    TriggerPathName = cms.untracked.string("HLT_Dimuon25_Jpsi_v")
+)
+
+hfpsi = hfv1.clone(
+    TriggerPathName = cms.untracked.string("HLT_Dimuon18_PsiPrime_v")
+)
+
+hfupsilon = hfv1.clone(
+    TriggerPathName = cms.untracked.string("HLT_Dimuon12_Upsilon_eta1p5_v")
+)
+
 
 heavyFlavorValidationSequence = cms.Sequence(
-  hfv1+hfv2+hfv3+hfv4+hfv5 
-  +hfvQuadmu1+hfvQuadmu2+hfvQuadmu3+hfvQuadmu4
-  +hfvTnP1+hfvTnP2+hfvTnP3+hfvTnP4+hfvTnP5+hfvTnP6+hfvTnP7+hfvTnP8+hfvTnP9+hfvTnP10+hfvTnP11
-  +hfv6+hfv7+hfv8+hfv9
-  +hfvTau3mu
-  +hfvDoubleMu0
-  +hfvQ1+hfvQ2+hfvQ3+hfvQ4+hfvQ5+hfvQ6+hfvQ7
+    hfv1+hfv2+hfv3+hfv4+hfv5
+    +hfvTnP1+hfvTnP2+hfvTnP3+hfvTnP4+hfvTnP5+hfvTnP6+hfvTnP7+hfvTnP8
+    + hfjpsiMuon
+    + hfjpsiNoVertexing
+    + hfjpsiNoVertexingNoOS
+    + hfjpsiBarrel
+    + hfpsiBarrel
+    + hfupsilonBarrel
+    + hfphiBarrel
+    + hfmu25Onia
+    + hfmu30Onia
+    + hfmu25Phi
+    + hfmu20Phi
+    + hfjpsi
+    + hfpsi
+    + hfupsilon
 )
