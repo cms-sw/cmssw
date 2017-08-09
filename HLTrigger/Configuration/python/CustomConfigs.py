2aa0f320b6c0835cfa219f0daea71dddf1fec93a
Martin Grunewald <Martin.Grunewald@cern.ch>
2017-08-09 09:18:47 +0200

    More L1T compat work for TSG use

--- HLTrigger/Configuration/python/CustomConfigs.py
+++ HLTrigger/Configuration/python/CustomConfigs.py
@@ -134,9 +134,11 @@ def L1REPACK(process,sequence="Full"):
         getattr(process,path).insert(0,process.SimL1Emulator)
 
     # special L1T cleanup
-    for obj in ('SimL1TCalorimeter','SimL1TMuonCommon','SimL1TMuon','SimL1TechnicalTriggers','SimL1EmulatorCore'):
+    for obj in ('SimL1TCalorimeter','SimL1TMuonCommon','SimL1TMuon','SimL1TechnicalTriggers','SimL1EmulatorCore','ecalDigiSequence','hcalDigiSequence','calDigi'):
         if hasattr(process,obj):
             delattr(process,obj)
+    if (hasattr(process,'StableParameters') and hasattr(process,'GlobalParameters')):
+        delattr(process,'GlobalParameters')
 
     return process
 
