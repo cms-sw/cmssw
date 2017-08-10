657795b41591b750db7de610b1c481ab18790835
Pablo de Castro <pablodecm@gmail.com>
2017-08-10 15:11:02 +0200

    fix quality in reco to reproduce u_int of qualityMask in pat

--- RecoBTag/DeepFlavour/interface/c_pf_features_converter.h
+++ RecoBTag/DeepFlavour/interface/c_pf_features_converter.h
@@ -9,6 +9,13 @@ namespace deep {
   // conversion map from quality flags used in PV association and miniAOD one
   constexpr int qualityMap[8]  = {1,0,1,1,4,4,5,6};
 
+  enum qualityFlagsShiftsAndMasks {
+        assignmentQualityMask = 0x7, assignmentQualityShift = 0,
+        trackHighPurityMask  = 0x8, trackHighPurityShift=3,
+        lostInnerHitsMask = 0x30, lostInnerHitsShift=4,
+        muonFlagsMask = 0x0600, muonFlagsShift=9
+  };
+
 
   template <typename CandidateType, typename JetType,
             typename TrackInfoBuilderType,
@@ -226,13 +233,18 @@ namespace deep {
             c_pf_features.chi2 = deep::catch_infs_and_bound(std::floor(pseudo_track.normalizedChi2()),300,-1,300);
 
             // conditions from PackedCandidate producer
-            auto isHighPurity = c_pf->trackRef().isNonnull() && pseudo_track.quality(reco::Track::highPurity);
+            bool highPurity = c_pf->trackRef().isNonnull() && pseudo_track.quality(reco::Track::highPurity);
+            // do same bit operations than in PackedCandidate
+            uint16_t qualityFlags = 0;
+            qualityFlags = (qualityFlags & ~trackHighPurityMask) | ((highPurity << trackHighPurityShift) & trackHighPurityMask);
+            bool isHighPurity = (qualityFlags & trackHighPurityMask)>>trackHighPurityShift;
+            // to do as in TrackBase
+            uint8_t quality = (1 << reco::TrackBase::loose);
             if (isHighPurity) {
-              c_pf_features.quality = reco::TrackBase::highPurity;
-            } else {
-              c_pf_features.quality = reco::TrackBase::loose;
-            }
+              quality |= (1 << reco::TrackBase::highPurity);
+            } 
 
+            c_pf_features.quality = quality; 
             c_pf_features.drminsv = deep::catch_infs_and_bound(drminpfcandsv,0,-0.4,0,-0.4);
 
   } 
