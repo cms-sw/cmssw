63b70eda863bb0f91ca4b0253b3f2af12c314905
Felice <felice.pantaleo@cern.ch>
2017-08-11 13:38:43 +0200

    adding calibrations

---
+++ RecoParticleFlow/PFClusterProducer/python/particleFlowRealisticSimClusterHGCCalibrations_cfi.py
@@ -0,0 +1,6 @@
+import FWCore.ParameterSet.Config as cms
+
+minEtaCorrection = cms.double(1.4)
+maxEtaCorrection = cms.double(3.0)
+hadronCorrections = cms.vdouble(1.15, 1.15, 1.14, 1.14, 1.15, 1.17, 1.20, 1.20)  
+egammaCorrections = cms.vdouble(1.00, 1.00, 1.00, 1.00, 1.02, 1.03, 1.03, 1.03)  
