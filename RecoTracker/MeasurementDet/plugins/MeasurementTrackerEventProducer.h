e5430eb3333e42a38e69d04c6bcf6daa7d5ea721
Viktor Veszpremi <veszpv@cern.ch>
2017-08-14 10:42:15 +0200

    Populate PxMeasurementDetSet from PixelFEDChannelCollection per event. Feature is turned on by including badPixelFEDChannelCollectionLabels in MeasurementTrackerEventProducer_cfi.py; if so, also must add pixelCablingMapLabel

--- RecoTracker/MeasurementDet/plugins/MeasurementTrackerEventProducer.h
+++ RecoTracker/MeasurementDet/plugins/MeasurementTrackerEventProducer.h
@@ -10,6 +10,8 @@
 #include "RecoTracker/MeasurementDet/src/TkMeasurementDetSet.h"
 #include "DataFormats/Common/interface/ContainerMask.h"
 #include "DataFormats/DetId/interface/DetIdCollection.h"
+#include "DataFormats/SiPixelDetId/interface/PixelFEDChannel.h"
+#include "CondFormats/SiPixelObjects/interface/SiPixelFedCablingMap.h"
 
 class dso_hidden MeasurementTrackerEventProducer final : public edm::stream::EDProducer<> {
 public:
@@ -19,7 +21,8 @@ private:
       virtual void produce(edm::Event&, const edm::EventSetup&) override;
 
 protected:
-      void updatePixels( const edm::Event&, PxMeasurementDetSet & thePxDets, std::vector<bool> & pixelClustersToSkip ) const;
+      void updatePixels( const edm::Event&, PxMeasurementDetSet & thePxDets, std::vector<bool> & pixelClustersToSkip, 
+			 const TrackerGeometry& trackerGeom, const SiPixelFedCablingMap& cablingMap) const;
       void updateStrips( const edm::Event&, StMeasurementDetSet & theStDets, std::vector<bool> & stripClustersToSkip ) const;
       void updatePhase2OT( const edm::Event&, Phase2OTMeasurementDetSet & thePh2OTDets ) const;
       //FIXME:: going to be updated soon
@@ -36,6 +39,8 @@ protected:
       edm::EDGetTokenT<edm::ContainerMask<edmNew::DetSetVector<SiStripCluster>>> theStripClusterMask;
 
       std::vector<edm::EDGetTokenT<DetIdCollection>>      theInactivePixelDetectorLabels;
+      std::vector<edm::EDGetTokenT<PixelFEDChannelCollection> > theBadPixelFEDChannelsLabels;
+      std::string pixelCablingMapLabel_;
       std::vector<edm::EDGetTokenT<DetIdCollection>>      theInactiveStripDetectorLabels;
 
       bool selfUpdateSkipClusters_;
