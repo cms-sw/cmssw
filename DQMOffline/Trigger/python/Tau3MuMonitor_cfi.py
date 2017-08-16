e8268132d6c55a5dd972d6d2df468135faca9c31
Riccardo Manzoni <riccardo.manzoni@cern.ch>
2017-08-16 10:36:04 +0200

    fix tau3mu pset attribute

--- DQMOffline/Trigger/python/Tau3MuMonitor_cfi.py
+++ DQMOffline/Trigger/python/Tau3MuMonitor_cfi.py
@@ -38,7 +38,7 @@ hltTau3Mumonitoring.GenericTriggerEventPSet.andOrHlt       = cms.bool( True  ) #
 hltTau3Mumonitoring.GenericTriggerEventPSet.hltPaths       = cms.vstring("HLT_Tau3Mu_Mu7_Mu1_TkMu1_IsoTau15_v*")
 # determine the DCS partitions to be active https://github.com/cms-sw/cmssw/blob/b767924e38a6b75340e6e120ece95b648bd11d2d/DataFormats/Scalers/interface/DcsStatus.h#L35
 # RPC (12) + DT (13-15) + CSC (16-17) + TRK (24-27) + PIX (28-29)
-hltTau3Mumonitoring.denGenericTriggerEventPSet.dcsPartitions = cms.vint32(12, 13, 14, 15, 16, 17, 24, 25, 26, 27, 28, 29) 
+hltTau3Mumonitoring.GenericTriggerEventPSet.dcsPartitions = cms.vint32(12, 13, 14, 15, 16, 17, 24, 25, 26, 27, 28, 29) 
 # hltTau3Mumonitoring.GenericTriggerEventPSet.verbosityLevel = cms.uint32(2) # set to 2 for debugging
 # hltTau3Mumonitoring.GenericTriggerEventPSet.hltInputTag    = cms.InputTag("TriggerResults::reHLT") # change the process name to reHLT when running tests (if the process used to rerun the HLT is reHLT, of course)
 
