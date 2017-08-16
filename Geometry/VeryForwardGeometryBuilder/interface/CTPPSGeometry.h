390ea0f8ce89fb8f66030b93bb711848a8f44fef
Laurent Forthomme <laurent.forthomme@cern.ch>
2017-08-16 08:32:31 +0200

    CTPPSGeometry refactoring

--- Geometry/VeryForwardGeometryBuilder/interface/CTPPSGeometry.h
+++ Geometry/VeryForwardGeometryBuilder/interface/CTPPSGeometry.h
@@ -41,77 +41,65 @@ class CTPPSGeometry
     ~CTPPSGeometry() {}
 
     /// build up from DetGeomDesc
-    CTPPSGeometry(const DetGeomDesc * gd)
-    {
-      build(gd);
-    }
+    CTPPSGeometry( const DetGeomDesc* gd ) { build( gd ); }
 
-    /// build up from DetGeomDesc structure, return 0 = success
-    char build(const DetGeomDesc *);          
+    /// build up from DetGeomDesc structure
+    void build( const DetGeomDesc* );
+
+    //----- setters and getters
 
     ///\brief adds an item to the map (detector ID --> DetGeomDesc)
-    /// performs necessary checks, returns 0 if succesful
-    char addSensor(unsigned int, const DetGeomDesc * &);
+    /// performs necessary checks
+    /// \return true if successful, false if the sensor is already present
+    bool addSensor( unsigned int, const DetGeomDesc*& );
 
     ///\brief adds a RP box to a map
-    char addRP(unsigned int id, const DetGeomDesc * &det_geom_desc);
-
+    /// \return true if successful, false if the RP is already present
+    bool addRP( unsigned int id, const DetGeomDesc*& );
 
     ///\brief returns geometry of a detector
     /// performs necessary checks, returns NULL if fails
-    const DetGeomDesc *getSensor(unsigned int id) const;
+    const DetGeomDesc* getSensor( unsigned int id ) const;
 
     /// returns geometry of a RP box
-    const DetGeomDesc *getRP(unsigned int id) const;
+    const DetGeomDesc* getRP( unsigned int id ) const;
 
+    //----- objects iterators
 
     /// begin iterator over sensors
-    mapType::const_iterator beginSensor() const
-    {
-      return theMap.begin();
-    }
-
+    mapType::const_iterator beginSensor() const { return sensors_map_.begin(); }
     /// end iterator over sensors
-    mapType::const_iterator endSensor() const
-    {
-      return theMap.end();
-    }
+    mapType::const_iterator endSensor() const { return sensors_map_.end(); }
 
     /// begin iterator over RPs
-    RPDeviceMapType::const_iterator beginRP() const
-    {
-      return theRomanPotMap.begin();
-    }
-
+    RPDeviceMapType::const_iterator beginRP() const { return rps_map_.begin(); }
     /// end iterator over RPs
-    RPDeviceMapType::const_iterator endRP() const
-    {
-      return theRomanPotMap.end();
-    }
+    RPDeviceMapType::const_iterator endRP() const { return rps_map_.end(); }
 
+    //----- translators
 
     /// coordinate transformations between local<-->global reference frames
     /// dimensions in mm
     /// sensor id expected
-    CLHEP::Hep3Vector localToGlobal(const DetGeomDesc *gd, const CLHEP::Hep3Vector& r) const;
-    CLHEP::Hep3Vector globalToLocal(const DetGeomDesc *gd, const CLHEP::Hep3Vector& r) const;
-    CLHEP::Hep3Vector localToGlobal(unsigned int id, const CLHEP::Hep3Vector& r) const;
-    CLHEP::Hep3Vector globalToLocal(unsigned int id, const CLHEP::Hep3Vector& r) const;
+    CLHEP::Hep3Vector localToGlobal( const DetGeomDesc*, const CLHEP::Hep3Vector& ) const;
+    CLHEP::Hep3Vector globalToLocal( const DetGeomDesc*, const CLHEP::Hep3Vector& ) const;
+    CLHEP::Hep3Vector localToGlobal( unsigned int, const CLHEP::Hep3Vector& ) const;
+    CLHEP::Hep3Vector globalToLocal( unsigned int, const CLHEP::Hep3Vector& ) const;
 
-    /// direction transformations between local<-->global reference frames
-    /// sensor id expected
-    CLHEP::Hep3Vector localToGlobalDirection(unsigned int id, const CLHEP::Hep3Vector& dir) const;
-    CLHEP::Hep3Vector globalToLocalDirection(unsigned int id, const CLHEP::Hep3Vector& dir) const;
-
+    /// direction transformations between local and global reference frames
+    /// \param id sensor id
+    CLHEP::Hep3Vector localToGlobalDirection( unsigned int id, const CLHEP::Hep3Vector& ) const;
+    /// direction transformations between global and local reference frames
+    /// \param id sensor id
+    CLHEP::Hep3Vector globalToLocalDirection( unsigned int id, const CLHEP::Hep3Vector& ) const;
 
     /// returns translation (position) of a detector
-    /// sensor id expected
-    CLHEP::Hep3Vector getSensorTranslation(unsigned int id) const;
+    /// \param id sensor id
+    CLHEP::Hep3Vector getSensorTranslation( unsigned int id ) const;
 
     /// returns position of a RP box
-    /// RP id expected
-    CLHEP::Hep3Vector getRPTranslation(unsigned int id) const;
-
+    /// \param id RP id
+    CLHEP::Hep3Vector getRPTranslation( unsigned int id ) const;
 
     /// after checks returns set of station ids corresponding to the given arm id
     const std::set<unsigned int>& getStationsInArm( unsigned int ) const;
@@ -124,14 +112,14 @@ class CTPPSGeometry
 
   protected:
     /// map: sensor id --> DetGeomDesc
-    mapType theMap;
+    mapType sensors_map_;
     
     /// map: rp id --> DetGeomDesc
-    RPDeviceMapType theRomanPotMap;           
+    RPDeviceMapType rps_map_;
 
     ///\brief map: parent ID -> set of subelements
-    /// E.g. stationsInArm is map of arm ID -> set of stations (in that arm)
-    mapSetType stationsInArm, rpsInStation, detsInRP;
+    /// E.g. \a stations_in_arm_ is map of arm ID -> set of stations (in that arm)
+    mapSetType stations_in_arm_, rps_in_station_, dets_in_rp_;
 };
 
 #endif
