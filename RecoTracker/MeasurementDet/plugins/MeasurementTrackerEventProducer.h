6b03d62a33796268962d6c9a4df906e43b1e5f54
Matti Kortelainen <matti.kortelainen@cern.ch>
2017-08-17 15:39:35 +0200

    Add fillDescriptions to MeasurementTrackerEventProducer

--- RecoTracker/MeasurementDet/plugins/MeasurementTrackerEventProducer.h
+++ RecoTracker/MeasurementDet/plugins/MeasurementTrackerEventProducer.h
@@ -17,6 +17,8 @@ class dso_hidden MeasurementTrackerEventProducer final : public edm::stream::EDP
 public:
       explicit MeasurementTrackerEventProducer(const edm::ParameterSet &iConfig) ;
       ~MeasurementTrackerEventProducer() override {}
+
+      static void fillDescriptions(edm::ConfigurationDescriptions& descriptions);
 private:
       void produce(edm::Event&, const edm::EventSetup&) override;
 
