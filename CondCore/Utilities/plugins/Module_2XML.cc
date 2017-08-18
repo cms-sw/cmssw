4e3f4c705bfc52b9b4c8ce69cad2d5fb7c603881
Giacomo Govi <giacomo.govi@cern.ch>
2017-08-18 17:58:28 +0200

    Improvements for:  handling of xml converters from built-in libraries; re-use of locally generated libraries with xml converters

---
+++ CondCore/Utilities/plugins/Module_2XML.cc
@@ -0,0 +1,6 @@
+#include "CondCore/Utilities/interface/Payload2XMLModule.h"
+#include "CondCore/Utilities/src/CondFormats.h"
+
+PAYLOAD_2XML_MODULE( pluginUtilities_payload2xml ){
+  PAYLOAD_2XML_CLASS( RunInfo );
+}
