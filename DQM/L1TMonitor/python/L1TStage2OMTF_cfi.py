b50c696499cb599aafa0db9b239022b36fe95925
Thomas Reis <thomas.reis@cern.ch>
2017-08-21 17:42:12 +0200

    Use the OMTF unpacker for the OMTF online DQM.

--- DQM/L1TMonitor/python/L1TStage2OMTF_cfi.py
+++ DQM/L1TMonitor/python/L1TStage2OMTF_cfi.py
@@ -2,7 +2,7 @@ import FWCore.ParameterSet.Config as cms
 
 l1tStage2Omtf = cms.EDAnalyzer(
     "L1TStage2OMTF",
-    omtfSource = cms.InputTag("omtfStage2Digis", "OMTF"),
+    omtfSource = cms.InputTag("omtfStage2Digis", ""),
     monitorDir = cms.untracked.string("L1T/L1TStage2OMTF"),
     verbose = cms.untracked.bool(False),
 )
