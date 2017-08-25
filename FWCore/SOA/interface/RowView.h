d6d7dd39ba86f097100d82ba39ecb134fb7b6522
Chris Jones <chrisdjones15@gmail.com>
2017-08-25 11:13:38 -0500

    Initial commit of edm::soa::Table
    The edm::soa::Table is a 'structure of arrays' which uses a spreadsheet table metaphor.

---
+++ FWCore/SOA/interface/RowView.h
@@ -0,0 +1,79 @@
+#ifndef FWCore_SOA_RowView_h
+#define FWCore_SOA_RowView_h
+// -*- C++ -*-
+//
+// Package:     FWCore/SOA
+// Class  :     RowView
+// 
+/**\class RowView RowView.h "RowView.h"
+
+ Description: [one line class summary]
+
+ Usage:
+    <usage>
+
+*/
+//
+// Original Author:  root
+//         Created:  Thu, 24 Aug 2017 16:49:17 GMT
+//
+
+// system include files
+#include <tuple>
+#include <array>
+
+// user include files
+#include "FWCore/SOA/interface/tablehelpers.h"
+
+// forward declarations
+
+namespace edm {
+namespace soa {
+  
+template <typename... Args>
+class RowView {
+  using Layout = std::tuple<Args...>;
+  std::array<void*, sizeof...(Args)> m_values;
+  
+public:
+  explicit RowView( std::array<void*, sizeof...(Args)> const& iValues):
+  m_values{iValues} {}
+  
+  template<typename U>
+  typename U::type const& get() const {
+    return *(static_cast<typename U::type*>(columnAddress<U>()));
+  }
+  
+  template<typename U>
+  void * columnAddress() const {
+    return m_values[impl::GetIndex<0,U,Layout>::index];
+  }
+  
+};
+
+template <typename... Args>
+class MutableRowView {
+  using Layout = std::tuple<Args...>;
+  std::array<void*, sizeof...(Args)> m_values;
+  
+public:
+  explicit MutableRowView( std::array<void*, sizeof...(Args)>& iValues):
+  m_values{iValues} {}
+  
+  template<typename U>
+  typename U::type& get() const {
+    return *(static_cast<typename U::type*>(columnAddress<U>()));
+  }
+  
+  template<typename U>
+  void * columnAddress() const {
+    return m_values[impl::GetIndex<0,U,Layout>::index];
+  }
+  
+};
+
+}
+}
+
+
+#endif
