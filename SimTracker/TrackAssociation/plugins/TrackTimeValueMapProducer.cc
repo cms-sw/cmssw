2678690976e66bff4c2f25556acfa58eb642a7ce
Josh Bendavid <Josh.Bendavid@cern.ch>
2017-08-25 19:19:30 +0200

    fix for time assignment of displaced tracks

--- SimTracker/TrackAssociation/plugins/TrackTimeValueMapProducer.cc
+++ SimTracker/TrackAssociation/plugins/TrackTimeValueMapProducer.cc
@@ -12,6 +12,7 @@
 
 #include "DataFormats/Common/interface/ValueMap.h"
 #include "DataFormats/Common/interface/View.h"
+#include "DataFormats/Math/interface/deltaPhi.h"
 
 #include "DataFormats/TrackReco/interface/Track.h"
 #include "DataFormats/TrackReco/interface/TrackFwd.h"
@@ -25,6 +26,10 @@
 #include "SimDataFormats/TrackingAnalysis/interface/TrackingVertexContainer.h"
 #include "SimDataFormats/PileupSummaryInfo/interface/PileupSummaryInfo.h"
 #include "SimDataFormats/Associations/interface/TrackToTrackingParticleAssociator.h"
+#include "TrackingTools/TransientTrack/interface/TransientTrackBuilder.h"
+#include "TrackingTools/Records/interface/TransientTrackRecord.h"
+#include "MagneticField/Engine/interface/MagneticField.h"
+
 
 #include <memory>
 
@@ -59,13 +64,14 @@ private:
   // options
   std::vector<std::unique_ptr<const ResolutionModel> > resolutions_;
   // functions
-  std::pair<float,float> extractTrackVertexTime(const TrackingParticle&) const;
+  float extractTrackVertexTime(const TrackingParticle&, const reco::TransientTrack&) const;
 };
 
 DEFINE_FWK_MODULE(TrackTimeValueMapProducer);
 
 namespace {
   constexpr float fakeBeamSpotTimeWidth = 0.300f; // ns
+  constexpr float m_pion = 139.57061e-3;
   const std::string resolution("Resolution");
 
   template<typename ParticleType, typename T>
@@ -142,6 +148,10 @@ void TrackTimeValueMapProducer::produce(edm::StreamID sid, edm::Event& evt, cons
   edm::Handle<std::vector<PileupSummaryInfo> > pileupSummaryH;
   evt.getByToken(pileupSummaryInfo_, pileupSummaryH);
   
+  //transient track builder
+  edm::ESHandle<TransientTrackBuilder> theB;
+  es.get<TransientTrackRecord>().get("TransientTrackBuilder",theB);
+  
   // associate the reco tracks / gsf Tracks
   std::vector<reco::RecoToSimCollection> associatedTracks;  
   for( auto associator : associators ) {
@@ -178,8 +188,9 @@ void TrackTimeValueMapProducer::produce(edm::StreamID sid, edm::Event& evt, cons
     }
 
     if (track_tps != associatedTracks.back().end() && track_tps->val.size() == 1) {
-      const std::pair<float,float> time_info = extractTrackVertexTime(*track_tps->val[0].first);
-      generalTrackTimes.push_back(time_info.first);
+      reco::TransientTrack tt = theB->build(*tkref);
+      float time = extractTrackVertexTime(*track_tps->val[0].first,tt);
+      generalTrackTimes.push_back(time);
     }
     else {
       float rndtime = CLHEP::RandGauss::shoot(rng_engine, meanSimTime, rmsSimTime);
@@ -201,6 +212,7 @@ void TrackTimeValueMapProducer::produce(edm::StreamID sid, edm::Event& evt, cons
       const reco::Track& tk = TrackCollection[i];
       const float absEta = std::abs(tk.eta());
       bool inAcceptance = absEta < etaMax_ && absEta >= etaMin_ && tk.p()>pMin_ && (absEta>etaMaxForPtThreshold_ || tk.pt()>ptMin_);
+//       bool inAcceptance = absEta < etaMax_ && absEta >= etaMin_ && tk.pt()>pMin_ && (absEta>etaMaxForPtThreshold_ || tk.pt()>ptMin_);
       if (inAcceptance) {
         const float resolution = reso->getTimeResolution(tk);
         times.push_back( CLHEP::RandGauss::shoot(rng_engine, generalTrackTimes[i], resolution) );
@@ -217,24 +229,40 @@ void TrackTimeValueMapProducer::produce(edm::StreamID sid, edm::Event& evt, cons
   }
 }
 
-std::pair<float,float> TrackTimeValueMapProducer::
-extractTrackVertexTime( const TrackingParticle &tp ) const {
-  float result = 0.f;
-  float result_z = 0.f;
+float TrackTimeValueMapProducer::
+extractTrackVertexTime( const TrackingParticle &tp, const reco::TransientTrack &tt ) const {
+  int pdgid = tp.pdgId();
   const auto& tvertex = tp.parentVertex();
-  result = tvertex->position().T()*CLHEP::second; // convert into nano-seconds
-  result_z = tvertex->position().Z();
+  math::XYZTLorentzVectorD result = tvertex->position();
+//   result = tvertex->position().T()*CLHEP::second; // convert into nano-seconds
   // account for secondary vertices...
   
-  if( tvertex->nSourceTracks() ) {
+  if( tvertex->nSourceTracks() && tvertex->sourceTracks()[0]->pdgId()==pdgid ) {
     auto pvertex = tvertex->sourceTracks()[0]->parentVertex();
-    result = pvertex->position().T()*CLHEP::second;
-    result_z = pvertex->position().Z();
-    while( pvertex->nSourceTracks() ) {
+    result = pvertex->position();
+    while( pvertex->nSourceTracks() && pvertex->sourceTracks()[0]->pdgId()==pdgid ) {
       pvertex = pvertex->sourceTracks()[0]->parentVertex();
-      result = pvertex->position().T()*CLHEP::second;
-      result_z = pvertex->position().Z();
+      result = pvertex->position();
     }
   }
-  return std::make_pair(result,result_z);
+  
+  float time = result.T()*CLHEP::second;
+  //correct for time of flight from track reference position
+  GlobalPoint result_pos(result.x(),result.y(),result.z());
+  const auto &tkstate = tt.trajectoryStateClosestToPoint(result_pos);
+  float tkphi = tkstate.momentum().phi();
+  float tkz = tkstate.position().z();
+  float dphi = reco::deltaPhi(tkphi,tt.track().phi());
+  float dz = tkz - tt.track().vz();
+  
+  float radius = 100.*tt.track().pt()/(0.3*tt.field()->inTesla(GlobalPoint(0,0,0)).z());
+  float pathlengthrphi = tt.track().charge()*dphi*radius;
+  
+  float pathlength = std::sqrt(pathlengthrphi*pathlengthrphi + dz*dz);
+  float p = tt.track().p();
+  
+  float speed = std::sqrt(1./(1.+m_pion/p))*CLHEP::c_light*CLHEP::s/CLHEP::m;
+  float dt = 1e9*pathlength/speed;
+  
+  return time-dt;
 }
