9753ca4ae0e26711bd9892560e69fb7845730289
Andrew Brinkerhoff <abrinke1@cern.ch>
2017-08-26 15:32:48 +0200

    EMTF moved L1TMuon files out of deprecate

--- L1Trigger/L1TMuonEndCap/interface/MuonInternalTrack.h
+++ L1Trigger/L1TMuonEndCap/interface/MuonInternalTrack.h
@@ -18,7 +18,7 @@
 
 #include <iostream>
 
-#include "L1Trigger/L1TMuon/interface/deprecate/MuonTriggerPrimitive.h"
+#include "L1Trigger/L1TMuon/interface/MuonTriggerPrimitive.h"
 #include "DataFormats/L1GlobalMuonTrigger/interface/L1MuRegionalCand.h"
 #include "L1Trigger/L1TMuonEndCap/interface/MuonInternalTrackFwd.h"
 
