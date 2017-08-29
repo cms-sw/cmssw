b5a08d12cc627940e68c32bd965119275be7c091
bobaekim <bokim@cern.ch>
2017-08-29 16:26:30 +0200

    update for dijet monitoring

--- DQMOffline/Trigger/plugins/DiJetMonitor.h
+++ DQMOffline/Trigger/plugins/DiJetMonitor.h
@@ -71,10 +71,8 @@ private:
   edm::EDGetTokenT<reco::MuonCollection>        muoToken_;
   edm::EDGetTokenT<reco::PFJetCollection>  dijetSrc_; // test for Jet
 
-  std::vector<double> dijetpT_variable_binning_;
   MEbinning           dijetpt_binning_;
   MEbinning           dijetptThr_binning_;
-  MEbinning           ls_binning_;
 
   ObjME jetpt1ME_;
   ObjME jetpt2ME_;
@@ -95,8 +93,6 @@ private:
 
   int nmuons_;
   double ptcut_;
-  double etacut_;
-  double phicut_;
 
   std::vector<double> v_jetpt;
   std::vector<double> v_jeteta;
