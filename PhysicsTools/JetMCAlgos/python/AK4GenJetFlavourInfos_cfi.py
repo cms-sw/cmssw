ad01d381c63d129fd28783cbcf015e2dcd18c378
Andrej Saibel <andrej.saibel@cern.ch>
2017-08-30 11:32:40 +0200

    fixed indentation

--- PhysicsTools/JetMCAlgos/python/AK4GenJetFlavourInfos_cfi.py
+++ PhysicsTools/JetMCAlgos/python/AK4GenJetFlavourInfos_cfi.py
@@ -2,10 +2,10 @@ import FWCore.ParameterSet.Config as cms
 from PhysicsTools.JetMCAlgos.AK4PFJetsMCFlavourInfos_cfi import ak4JetFlavourInfos
 
 ak4GenJetFlavourInfos = ak4JetFlavourInfos.clone(
-											jets = "ak4GenJetsNoNu",
-											bHadrons = cms.InputTag("selectedHadronsAndPartonsForGenJetsFlavourInfos","bHadrons"),
-    										cHadrons = cms.InputTag("selectedHadronsAndPartonsForGenJetsFlavourInfos","cHadrons"),
-  											partons  = cms.InputTag("selectedHadronsAndPartonsForGenJetsFlavourInfos","physicsPartons"),
-    										leptons  = cms.InputTag("selectedHadronsAndPartonsForGenJetsFlavourInfos","leptons"),
+                         jets = "ak4GenJetsNoNu",
+                         bHadrons = cms.InputTag("selectedHadronsAndPartonsForGenJetsFlavourInfos","bHadrons"),
+                         cHadrons = cms.InputTag("selectedHadronsAndPartonsForGenJetsFlavourInfos","cHadrons"),
+                         partons  = cms.InputTag("selectedHadronsAndPartonsForGenJetsFlavourInfos","physicsPartons"),
+                         leptons  = cms.InputTag("selectedHadronsAndPartonsForGenJetsFlavourInfos","leptons"),
 )
 
