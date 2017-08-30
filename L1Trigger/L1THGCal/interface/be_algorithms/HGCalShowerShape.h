b89fdcde20790f89089451eb44929f14e57f0754
Jean-Baptiste Sauvan <jean-baptiste.sauvan@cern.ch>
2017-08-30 12:16:43 +0200

    Remove useless comments

--- L1Trigger/L1THGCal/interface/be_algorithms/HGCalShowerShape.h
+++ L1Trigger/L1THGCal/interface/be_algorithms/HGCalShowerShape.h
@@ -39,11 +39,3 @@ class HGCalShowerShape{
 
 #endif
 
-// -----------------------------------
-// Local Variables:
-// mode:c++
-// indent-tabs-mode:nil
-// tab-width:4
-// c-basic-offset:4
-// End:
-// vim: tabstop=4 expandtab shiftwidth=4 softtabstop=4
