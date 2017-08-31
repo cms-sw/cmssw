20716ae3fe72e128d11d18f8a8db095ec31e3732
osahin <ozgur.sahin@cern.ch>
2017-08-31 17:28:35 +0200

    fixing minor bugs and correcting the parameters

--- L1Trigger/L1THGCal/python/hgcalTriggerPrimitiveDigiProducer_cfi.py
+++ L1Trigger/L1THGCal/python/hgcalTriggerPrimitiveDigiProducer_cfi.py
@@ -66,9 +66,9 @@ C2d_parValues = cms.PSet( seeding_threshold_silicon = cms.double(5), # MipT
 C3d_parValues = cms.PSet( dR_multicluster = cms.double(0.01), # dR in normalized plane used to clusterize C2d
                           minPt_multicluster = cms.double(0.5), # minimum pt of the multicluster (GeV)
                           calibSF_multicluster = cms.double(1.084),
-                          type_multicluster = cms.string('DBSCAN'), #ConedR for the cone algorithm 
+                          type_multicluster = cms.string('DBSCANC3d'), # 'dRC3d' for the cone algorithm 
                           dist_dbscan_multicluster = cms.double(0.03),
-                          minN_dbscan_multicluster = cms.uint32(3)
+                          minN_dbscan_multicluster = cms.uint32(2)
 
                           )
 cluster_algo =  cms.PSet( AlgorithmName = cms.string('HGCClusterAlgoThreshold'),
