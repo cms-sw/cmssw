d8dc0d54a9c3261c82b52c98a88391394ac15e13
osahin <ozgur.sahin@cern.ch>
2017-08-31 11:18:58 +0200

    The DBSCAN algorithm is implemented for the 3D clusters

--- L1Trigger/L1THGCal/python/hgcalTriggerPrimitiveDigiProducer_cfi.py
+++ L1Trigger/L1THGCal/python/hgcalTriggerPrimitiveDigiProducer_cfi.py
@@ -65,7 +65,11 @@ C2d_parValues = cms.PSet( seeding_threshold_silicon = cms.double(5), # MipT
 
 C3d_parValues = cms.PSet( dR_multicluster = cms.double(0.01), # dR in normalized plane used to clusterize C2d
                           minPt_multicluster = cms.double(0.5), # minimum pt of the multicluster (GeV)
-                          calibSF_multicluster = cms.double(1.084)
+                          calibSF_multicluster = cms.double(1.084),
+                          type_multicluster = cms.string('DBSCAN'), #ConedR for the cone algorithm 
+                          dist_dbscan_multicluster = cms.double(0.03),
+                          minN_dbscan_multicluster = cms.uint32(3)
+
                           )
 cluster_algo =  cms.PSet( AlgorithmName = cms.string('HGCClusterAlgoThreshold'),
                           FECodec = fe_codec.clone(),
