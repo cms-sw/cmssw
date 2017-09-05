4844932fd0182006ddf7a8741d1f73e14008d8cd
Remi <remigius.mommsen@cern.ch>
2017-09-05 11:29:05 +0200

    Use TCDSRaw for temporary accesses to TCDS information

--- EventFilter/Utilities/src/AuxiliaryMakers.cc
+++ EventFilter/Utilities/src/AuxiliaryMakers.cc
@@ -1,21 +1,22 @@
 #include <sys/time.h>
 
+#include "EventFilter/FEDInterface/interface/fed_header.h"
 #include "EventFilter/Utilities/interface/AuxiliaryMakers.h"
 #include "FWCore/MessageLogger/interface/MessageLogger.h"
 
 namespace evf{
   namespace evtn{
 
-      edm::EventAuxiliary makeEventAuxiliary(TCDSRecord *record,
+	edm::EventAuxiliary makeEventAuxiliary(const tcds::Raw_v1* tcds,
 					     unsigned int runNumber,
                                              unsigned int lumiSection,
 					     std::string const &processGUID,
                                              bool verifyLumiSection){
 	edm::EventID eventId(runNumber, // check that runnumber from record is consistent
                              lumiSection,
-			     record->getEventNumber());
+			     tcds->header.eventNumber);
 
-        edm::TimeValue_t time = static_cast<edm::TimeValue_t> (record->getBST().getGpsTime());
+        edm::TimeValue_t time = static_cast<edm::TimeValue_t>(((uint64_t)tcds->bst.gpstimehigh << 32) | tcds->bst.gpstimelow);
         if (time == 0) {
           timeval stv;
           gettimeofday(&stv,0);
@@ -23,8 +24,8 @@ namespace evf{
           time = (time << 32) + stv.tv_usec;
         }
 
-        const uint64_t orbitnr = record->getOrbitNr();
-        const uint32_t recordLumiSection = record->getLumiSection();
+        const uint64_t orbitnr = ((uint64_t)tcds->header.orbitHigh << 16) | tcds->header.orbitLow;
+        const uint32_t recordLumiSection = tcds->header.lumiSection;
 
         if (verifyLumiSection && recordLumiSection != lumiSection)
           edm::LogWarning("AuxiliaryMakers") << "Lumisection mismatch, external : " << lumiSection << ", record : " << recordLumiSection;
@@ -35,9 +36,9 @@ namespace evf{
 				   processGUID,
 				   edm::Timestamp(time),
 				   true,
-                                   (edm::EventAuxiliary::ExperimentType)(record->getEventType()),
-				   (int)record->getBXID(),
-				   edm::EventAuxiliary::invalidStoreNumber,
+                                   (edm::EventAuxiliary::ExperimentType)FED_EVTY_EXTRACT(tcds->fedHeader.eventid),
+				   (int)tcds->header.bxid,
+				   ((uint32_t)(tcds->bst.lhcFillHigh)<<16)|tcds->bst.lhcFillLow,
 				   (int)(orbitnr&0x7fffffffU));//framework supports only 32-bit signed
       }
   }
