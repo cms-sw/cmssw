e860f4373cd3fa401606ac8ecc6a7caa527f1c10
Remi <remigius.mommsen@cern.ch>
2017-09-07 15:09:27 +0200

    Move TCDSRecord into dictionary struct

--- DataFormats/TCDS/src/classes.h
+++ DataFormats/TCDS/src/classes.h
@@ -3,10 +3,10 @@
 #include "DataFormats/Common/interface/Ref.h"
 #include "DataFormats/Common/interface/RefProd.h"
 
-TCDSRecord tcdsRecord;
-
 namespace DataFormats_TCDS {
   struct dictionary {
+
+    TCDSRecord tcdsRecord;
 
     edm::Wrapper<TCDSRecord> w_tcdsRecord;
 
