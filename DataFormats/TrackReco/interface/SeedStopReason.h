5f6ada4b12e7e4ede80febfa8a6502f7bf46dfb3
Matti Kortelainen <matti.kortelainen@cern.ch>
2017-09-08 13:58:25 +0200

    Seeding region rebuild may return empty set of trajectories

--- DataFormats/TrackReco/interface/SeedStopReason.h
+++ DataFormats/TrackReco/interface/SeedStopReason.h
@@ -8,19 +8,21 @@ enum class SeedStopReason {
   NOT_STOPPED = 1,
   SEED_CLEANING = 2,
   NO_TRAJECTORY = 3,
-  FINAL_CLEAN = 4,
-  SMOOTHING_FAILED = 5,
-  SIZE = 6
+  SEED_REGION_REBUILD = 4,
+  FINAL_CLEAN = 5,
+  SMOOTHING_FAILED = 6,
+  SIZE = 7
 };
 
 namespace SeedStopReasonName {
   static const std::string SeedStopReasonName[] = {
-    "UNINITIALIZED",    //  0
-    "NOT_STOPPED",      //  1
-    "SEED_CLEANING",    //  2
-    "NO_TRAJECTORY",    //  3
-    "FINAL_CLEAN",      //  4
-    "SMOOTHING_FAILED"  //  5
+    "UNINITIALIZED",       // 0
+    "NOT_STOPPED",         // 1
+    "SEED_CLEANING",       // 2
+    "NO_TRAJECTORY",       // 3
+    "SEED_REGION_REBUILD", // 4
+    "FINAL_CLEAN",         // 5
+    "SMOOTHING_FAILED"     // 6
   };
 }
 
