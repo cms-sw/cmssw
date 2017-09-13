4b1a2afa536456adda76900dc25257c7c3db4417
Swagata Mukherjee <s.mukherjee@cern.ch>
2017-09-13 20:32:36 +0200

    clear disp-vtx vector and other changes

--- HLTrigger/Muon/src/HLTScoutingMuonProducer.cc
+++ HLTrigger/Muon/src/HLTScoutingMuonProducer.cc
@@ -145,7 +145,8 @@ void HLTScoutingMuonProducer::produce(edm::StreamID sid, edm::Event & iEvent,
       
       double hcalisopf=-1.0;
       if ( HcalPFClusterIsoMap.isValid()) { hcalisopf = (*HcalPFClusterIsoMap)[muonRef]; }
-      
+
+      vtxInd.reserve(vtxMuPair.size());
       for (unsigned int i=0; i<vtxMuPair.size(); i++) {
 	float dr2_1 = reco::deltaR2( ((vtxMuPair[i]).first),muon );
 	float dr2_2 = reco::deltaR2( ((vtxMuPair[i]).second),muon );
@@ -176,6 +177,7 @@ void HLTScoutingMuonProducer::produce(edm::StreamID sid, edm::Event & iEvent,
 			     track->dszError(),
 			     vtxInd
 			     );
+      vtxInd.clear();
     }
     
     // Put output
