f094c5354af4aa7f78e440fd13985f82e29b8ebd
Gregor Mittag <Gregor.Mittag@cern.ch>
2017-09-13 17:39:46 +0200

    Add missing linebreak.
    This way one avoids that the warning "hides" other output which was
    previously printed on the same line.

--- Configuration/AlCa/python/GlobalTag.py
+++ Configuration/AlCa/python/GlobalTag.py
@@ -69,7 +69,7 @@ def GlobalTag(essource = None, globaltag = None, conditions = None):
             if globaltag not in autoCond:
                 raise Exception('no correspondence for '+globaltag+'\navailable keys are\n'+','.join(autoCond.keys()))
             if 'phase1_2017_design' == globaltag:
-                sys.stderr.write('Warning: %s now points to %s. This has reco- Beamspot centered to (0,0,0)'%(globaltag,autoCond[globaltag]))
+                sys.stderr.write('Warning: %s now points to %s. This has reco-Beamspot centered to (0,0,0)\n'%(globaltag,autoCond[globaltag]))
             autoKey = autoCond[globaltag]
             if isinstance(autoKey, tuple) or isinstance(autoKey, list):
                 globaltag = autoKey[0]
