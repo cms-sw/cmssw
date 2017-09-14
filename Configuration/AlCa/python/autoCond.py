c30cba8dd52c2a4c44e7d001c7b903374b1aadd7
Vladimir <vladimir.rekovic@cern.ch>
2017-09-14 18:23:33 +0200

    Introduce alias run2_mc_l1stage1 and adjust configuration used by HLT AddOn tests to use it.

--- Configuration/AlCa/python/autoCond.py
+++ Configuration/AlCa/python/autoCond.py
@@ -15,6 +15,8 @@ autoCond = {
     'run2_mc_50ns'      :   '93X_mcRun2_startup_v1',
     #GlobalTag for MC production with optimistic alignment and calibrations for Run2
     'run2_mc'           :   '93X_mcRun2_asymptotic_v2',
+    # GlobalTag for MC production (L1 Trigger Stage1) with starup-like alignment and calibrations for Run2, L1 trigger in Stage1 mode
+    'run2_mc_l1stage1'  :   '93X_mcRun2_asymptotic_v1',
     # GlobalTag for MC production (cosmics) with starup-like alignment and calibrations for Run2, Strip tracker in peak mode
     'run2_mc_cosmics'   :   '93X_mcRun2cosmics_startup_deco_v1',
     # GlobalTag for MC production (Heavy Ions collisions) with optimistic alignment and calibrations for Run2
