077cc51f472d48b2f789e5d1d5e471d1d223f6f0
Sunanda <sunanda.banerjee@cern.ch>
2017-09-15 14:45:32 +0200

    Take care of code checker

--- RecoTauTag/RecoTau/plugins/TauDiscriminationAgainstCaloMuon.cc
+++ RecoTauTag/RecoTau/plugins/TauDiscriminationAgainstCaloMuon.cc
@@ -179,7 +179,7 @@ void TauDiscriminationAgainstCaloMuon<TauType, TauDiscriminator>::beginEvent(con
   edm::Handle<reco::VertexCollection> vertices;
   evt.getByLabel(srcVertex_, vertices);
   eventVertexPosition_ = GlobalPoint(0., 0., 0.);
-  if ( vertices->size() >= 1 ) {
+  if (!vertices->empty()) {
     const reco::Vertex& thePrimaryEventVertex = (*vertices->begin());
     eventVertexPosition_ = GlobalPoint(thePrimaryEventVertex.x(), thePrimaryEventVertex.y(), thePrimaryEventVertex.z());
   }
