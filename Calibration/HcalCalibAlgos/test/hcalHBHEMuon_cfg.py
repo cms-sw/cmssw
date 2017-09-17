c9520567ac9d8ceeb92ed6fe942360319babfd35
Sunanda <sunanda.banerjee@cern.ch>
2017-09-18 01:01:53 +0200

    Modify the MIP analyzer in view of new requirements

--- Calibration/HcalCalibAlgos/test/hcalHBHEMuon_cfg.py
+++ Calibration/HcalCalibAlgos/test/hcalHBHEMuon_cfg.py
@@ -12,10 +12,14 @@ process.GlobalTag.globaltag=autoCond['run2_mc']
 process.load("RecoLocalCalo.EcalRecAlgos.EcalSeverityLevelESProducer_cfi")
 process.load("Calibration.HcalCalibAlgos.hcalHBHEMuon_cfi")
 
+if 'MessageLogger' in process.__dict__:
+    process.MessageLogger.categories.append('HBHEMuon')
+
 process.maxEvents = cms.untracked.PSet( input = cms.untracked.int32(-1) )
 process.source = cms.Source("PoolSource",
     fileNames = cms.untracked.vstring(
-        'root://xrootd.unl.edu//store/mc/Phys14DR/DYToMuMu_M-50_Tune4C_13TeV-pythia8/GEN-SIM-RECO/PU20bx25_tsg_castor_PHYS14_25_V1-v1/10000/184C1AC9-A775-E411-9196-002590200824.root'
+        'root://xrootd.unl.edu//store/data/Run2017B/SingleMuon/RECO/PromptReco-v2/000/298/678/00000/C0C0C0B0-A466-E711-AE46-02163E019E8C.root'
+#        'root://xrootd.unl.edu//store/mc/Phys14DR/DYToMuMu_M-50_Tune4C_13TeV-pythia8/GEN-SIM-RECO/PU20bx25_tsg_castor_PHYS14_25_V1-v1/10000/184C1AC9-A775-E411-9196-002590200824.root'
         )
                             )
 
@@ -23,4 +27,15 @@ process.TFileService = cms.Service("TFileService",
                                    fileName = cms.string("Validation.root")
 )
 
-process.p = cms.Path(process.HcalHBHEMuonAnalyzer)
+process.hcalHBHEMuon.UseRaw = False
+process.hcalHBHEMuon.UnCorrect = True
+process.hcalHBHEMuon.GetCharge = True
+process.hcalHBHEMuon.CollapseDepth = False
+process.hcalHBHEMuon.IsItPlan1 = True
+process.hcalHBHEMuon.IgnoreHECorr = False
+process.hcalHBHEMuon.IsItPreRecHit = True
+process.hcalHBHEMuon.MaxDepth = 7
+process.hcalHBHEMuon.LabelHBHERecHit = cms.InputTag("hbheprereco")
+process.hcalHBHEMuon.Verbosity = 1111
+
+process.p = cms.Path(process.hcalHBHEMuon)
