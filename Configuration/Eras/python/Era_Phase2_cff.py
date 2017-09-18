272709c01920a52fd206d4c6f5c620d4c94a2b93
Kevin Pedro <kpedro88@gmail.com>
2017-09-18 11:41:29 -0500

    use real emap for HB upgrade

--- Configuration/Eras/python/Era_Phase2_cff.py
+++ Configuration/Eras/python/Era_Phase2_cff.py
@@ -16,9 +16,8 @@ from Configuration.Eras.Modifier_phase2_muon_cff import phase2_muon
 from Configuration.Eras.Modifier_run3_GEM_cff import run3_GEM
 from Configuration.Eras.Modifier_stage2L1Trigger_cff import stage2L1Trigger
 from Configuration.Eras.Modifier_hcalHardcodeConditions_cff import hcalHardcodeConditions
-from Configuration.Eras.Modifier_hcalSkipPacker_cff import hcalSkipPacker
 
 Phase2 = cms.ModifierChain(run2_common, run3_common, phase2_common, phase2_tracker, trackingPhase2PU140, phase2_ecal,
     run2_HE_2017, run2_HF_2017, run2_HCAL_2017, run3_HB, phase2_hcal, phase2_hgcal, phase2_muon, run3_GEM, stage2L1Trigger,
-    hcalHardcodeConditions, hcalSkipPacker,
+    hcalHardcodeConditions,
 )
