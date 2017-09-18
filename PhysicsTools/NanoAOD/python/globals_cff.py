edae6c2315f840972f3184f425338c618d174bc1
Giovanni <gpetruc@gmail.com>
2017-09-18 16:47:12 +0200

    Import NanoAOD prototype code @ https://github.com/gpetruc/NanoAOD.git tag v1.0 except data files

---
+++ PhysicsTools/NanoAOD/python/globals_cff.py
@@ -0,0 +1,13 @@
+import FWCore.ParameterSet.Config as cms
+from PhysicsTools.NanoAOD.common_cff import *
+
+rhoTable = cms.EDProducer("GlobalVariablesTableProducer",
+    variables = cms.PSet(
+        fixedGridRhoFastjetAll = ExtVar( cms.InputTag("fixedGridRhoFastjetAll"), "double", doc = "rho from all PF Candidates, used e.g. for JECs" ),
+        fixedGridRhoFastjetCentralNeutral = ExtVar( cms.InputTag("fixedGridRhoFastjetCentralNeutral"), "double", doc = "rho from neutral PF Candidates with |eta| < 2.5, used e.g. for rho corrections of some lepton isolations" ),
+        fixedGridRhoFastjetCentralCalo = ExtVar( cms.InputTag("fixedGridRhoFastjetCentralCalo"), "double", doc = "rho from calo towers with |eta| < 2.5, used e.g. egamma PFCluster isolation" ),
+    )
+)
+
+globalTables = cms.Sequence(rhoTable)
+
