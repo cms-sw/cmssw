6ff3ab03dfe36db6df93c5059e55dbc5d90a2ed6
fravera <fabio.ravera89@gmail.com>
2017-09-21 14:44:41 +0200

    Removed include and comments

--- RecoCTPPS/PixelLocal/python/ctppsPixelLocalReconstruction_cff.py
+++ RecoCTPPS/PixelLocal/python/ctppsPixelLocalReconstruction_cff.py
@@ -2,7 +2,6 @@ import FWCore.ParameterSet.Config as cms
 
 # geometry
 from Geometry.VeryForwardGeometry.geometryRP_cfi import *
-#from Geometry.VeryForwardGeometry.geometry_CTPPS_alaTotem_RECO_cfi import *
 
 # local clusterizer
 from RecoCTPPS.PixelLocal.ctppsPixelClusters_cfi import ctppsPixelClusters
