9659d71ec4e5a3f703df10317620bb3cc99ccd6d
Ianna Osborne <ianna.osborne@cern.ch>
2017-09-22 12:07:25 +0200

    Cleanup dependent files

--- DataFormats/MuonDetId/test/CSCDetIdAnalyzer.cc
+++ DataFormats/MuonDetId/test/CSCDetIdAnalyzer.cc
@@ -89,10 +89,10 @@ void CSCDetIdAnalyzer::analyze( const edm::Event& iEvent, const edm::EventSetup&
    int icountAll = 0;
 
    // Iterate over the DetUnits in the CSCGeometry
-   for( CSCGeometry::DetUnitContainer::const_iterator it = pDD->detUnits().begin(); it != pDD->detUnits().end(); ++it ){
+   for( const auto& it : pDD->detUnits()) {
 
      // Check each DetUnit really is a CSC layer
-     auto layer = dynamic_cast<CSCLayer const*>( *it );
+     auto layer = dynamic_cast<CSCLayer const*>( it );
      
       if( layer ) {
         ++icountAll; // how many layers we see
