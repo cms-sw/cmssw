520b96231dcc45d8f37c36ae450c222c3d91cea9
Remi <remigius.mommsen@cern.ch>
2017-09-25 19:05:12 +0200

    Reshuffle member variables to reduce padding

--- DataFormats/TCDS/src/L1aInfo.cc
+++ DataFormats/TCDS/src/L1aInfo.cc
@@ -2,17 +2,17 @@
 #include "DataFormats/TCDS/interface/TCDSRaw.h"
 
 L1aInfo::L1aInfo() :
-  index_(0),
   orbitNr_(0),
   bxid_(0),
+  index_(0),
   eventType_(0)
 {}
 
 
 L1aInfo::L1aInfo(const tcds::L1aInfo_v1& l1Info) :
-  index_(-l1Info.ind0 - 1),
   orbitNr_(((uint64_t)(l1Info.orbithigh)<<32)|l1Info.orbitlow),
   bxid_(l1Info.bxid),
+  index_(-l1Info.ind0 - 1),
   eventType_(l1Info.eventtype)
 {}
 
