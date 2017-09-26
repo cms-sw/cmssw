5dd23f130d243f7c7721ec49fe3798c4873b3121
Simon Kurz <Simon.Kurz@desy.de>
2017-09-26 14:56:16 +0200

    remove test files

--- FastSimulation/SimplifiedGeometryPropagator/test/gen.sh
+++
@@ -1,1 +0,0 @@
-cmsDriver.py SingleMuPt10_pythia8_cfi  --conditions auto:run2_mc --fast  -n 100 --era Run2_2016 --eventcontent FEVTDEBUGHLT,DQM --relval 27000,3000 -s GEN --datatier GEN-SIM-DIGI-RECO,DQMIO --beamspot Realistic50ns13TeVCollision --fileout gen_muGun.root
