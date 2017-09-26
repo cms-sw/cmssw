5ec58ea48ddea71c43c4c7e0a0d664953904bed9
Giovanni <gpetruc@gmail.com>
2017-09-26 17:02:54 +0200

    Cleanup MET naming conventions

--- PhysicsTools/NanoAOD/python/met_cff.py
+++ PhysicsTools/NanoAOD/python/met_cff.py
@@ -6,8 +6,7 @@ from  PhysicsTools.NanoAOD.common_cff import *
 ##################### User floats producers, selectors ##########################
 ## this can be merged with chsFor soft activity if we keep the same selection
 chsForTkMet = cms.EDFilter("CandPtrSelector", src = cms.InputTag("packedPFCandidates"), cut = cms.string('charge()!=0 && pvAssociationQuality()>=5 && vertexRef().key()==0'))
-tkMet = cms.EDProducer(
-    "PFMETProducer",
+tkMet = cms.EDProducer("PFMETProducer",
     src = cms.InputTag("chsForTkMet"),
     alias = cms.string('tkMet'),
     globalThreshold = cms.double(0.0),
@@ -25,7 +24,6 @@ metTable = cms.EDProducer("SimpleCandidateFlatTableProducer",
     extension = cms.bool(False), # this is the main table for the MET
     variables = cms.PSet(PTVars,
        sumEt = Var("sumEt()", float, doc="scalar sum of Et",precision=10),
-       rawEt = Var("uncorPt", float, doc="raw (uncorrected) scalar sum of Et",precision=10),
        covXX = Var("getSignificanceMatrix().At(0,0)",float,doc="xx element of met covariance matrix", precision=8),
        covXY = Var("getSignificanceMatrix().At(0,1)",float,doc="xy element of met covariance matrix", precision=8),
        covYY = Var("getSignificanceMatrix().At(1,1)",float,doc="yy element of met covariance matrix", precision=8),
@@ -34,22 +32,37 @@ metTable = cms.EDProducer("SimpleCandidateFlatTableProducer",
     ),
 )
 
-caloMetTable = cms.EDProducer("SimpleCandidateFlatTableProducer",
+
+rawMetTable = cms.EDProducer("SimpleCandidateFlatTableProducer",
     src = metTable.src,
-    name = cms.string("METCalo"),
-    doc = cms.string("Calo MET"),
+    name = cms.string("RawMET"),
+    doc = cms.string("raw PF MET"),
     singleton = cms.bool(True),  # there's always exactly one MET per event
     extension = cms.bool(False), # this is the main table for the MET
     variables = cms.PSet(#NOTA BENE: we don't copy PTVars here!
-       pt  = Var("caloMETPt",  float, precision=10),
-       phi = Var("caloMETPhi", float, precision=10),
+       pt  = Var("uncorPt",  float, doc="pt", precision=10),
+       phi = Var("uncorPhi", float, doc="phi", precision=10),
+       sumEt = Var("uncorSumEt", float, doc="scalar sum of Et", precision=10),
+    ),
+)
+
+
+caloMetTable = cms.EDProducer("SimpleCandidateFlatTableProducer",
+    src = metTable.src,
+    name = cms.string("CaloMET"),
+    doc = cms.string("CaloMET"),
+    singleton = cms.bool(True),  # there's always exactly one MET per event
+    extension = cms.bool(False), # this is the main table for the MET
+    variables = cms.PSet(#NOTA BENE: we don't copy PTVars here!
+       pt  = Var("caloMETPt",  float, doc="pt", precision=10),
+       phi = Var("caloMETPhi", float, doc="phi", precision=10),
        sumEt = Var("caloMETSumEt", float, doc="scalar sum of Et", precision=10),
     ),
 )
 
 puppiMetTable = cms.EDProducer("SimpleCandidateFlatTableProducer",
     src = cms.InputTag("slimmedMETsPuppi"),
-    name = cms.string("METpuppi"),
+    name = cms.string("PuppiMET"),
     doc = cms.string("PUPPI  MET"),
     singleton = cms.bool(True),  # there's always exactly one MET per event
     extension = cms.bool(False), # this is the main table for the MET
@@ -63,7 +76,7 @@ tkMetTable = cms.EDProducer("SimpleCandidateFlatTableProducer",
     name = cms.string("TkMET"),
     doc = cms.string("Track MET computed with tracks from PV0 ( pvAssociationQuality()>=5 ) "),
     singleton = cms.bool(True),  # there's always exactly one MET per event
-    extension = cms.bool(True), # this is the main table for the MET
+    extension = cms.bool(False), # this is the main table for the TkMET
     variables = cms.PSet(PTVars,
        sumEt = Var("sumEt()", float, doc="scalar sum of Et",precision=10),
     ),
@@ -72,19 +85,19 @@ tkMetTable = cms.EDProducer("SimpleCandidateFlatTableProducer",
 
 metMCTable = cms.EDProducer("SimpleCandidateFlatTableProducer",
     src = metTable.src,
-    name = cms.string("MET"),
-    doc = cms.string("Calo MET"),
-    singleton = cms.bool(True),  # there's always exactly one MET per event
-    extension = cms.bool(True),  # we add variables to the MET
+    name = cms.string("GenMET"),
+    doc = cms.string("Gen MET"),
+    singleton = cms.bool(True),  
+    extension = cms.bool(False),
     variables = cms.PSet(
-       genPt  = Var("genMET.pt",  float, precision=10),
-       genPhi = Var("genMET.phi", float, precision=10),
+       pt  = Var("genMET.pt",  float, doc="pt", precision=10),
+       phi = Var("genMET.phi", float, doc="phi", precision=10),
     ),
 )
 
 
 
 metSequence = cms.Sequence(chsForTkMet+tkMet)
-metTables = cms.Sequence( metTable + caloMetTable + puppiMetTable +tkMetTable)
+metTables = cms.Sequence( metTable + rawMetTable + caloMetTable + puppiMetTable + tkMetTable)
 metMC = cms.Sequence( metMCTable )
 
