3d7e4991114d678a97a6567a0876250a18c267aa
Zeynep Demiragli <zeynep.demiragli@gmail.com>
2017-09-27 18:23:08 +0200

    adding a simple producer rather than the full slimmedMETs collections

--- PhysicsTools/PatAlgos/python/slimming/miniAOD_tools.py
+++ PhysicsTools/PatAlgos/python/slimming/miniAOD_tools.py
@@ -179,24 +179,15 @@ def miniAOD_customizeCommon(process):
                                     )
     task.add(process.CHSCands)
 
-    runMetCorAndUncForMiniAODProduction(process,
-                                        pfCandColl=cms.InputTag("CHSCands"),
-                                        recoMetFromPFCs=True,
-                                        postfix="CHS"
-                                        )
+    process.pfMetCHS = cms.EDProducer("PFMETProducer",
+                                      src = cms.InputTag("CHSCands"),
+                                      alias = cms.string('pfMet'),
+                                      globalThreshold = cms.double(0.0),
+                                      calculateSignificance = cms.bool(False),
+                                      )
 
-    addToProcessAndTask('slimmedMETsCHS', process.slimmedMETs.clone(), process, task)
-    process.slimmedMETsCHS.src = cms.InputTag("patMETsCHS")
-    process.slimmedMETsCHS.rawVariation =  cms.InputTag("patPFMetCHS")
-    process.slimmedMETsCHS.t1Uncertainties = cms.InputTag("patPFMetT1%sCHS") 
-    process.slimmedMETsCHS.t01Variation = cms.InputTag("patPFMetT0pcT1CHS")
-    process.slimmedMETsCHS.t1SmearedVarsAndUncs = cms.InputTag("patPFMetT1Smear%sCHS")
-    process.slimmedMETsCHS.tXYUncForRaw = cms.InputTag("patPFMetTxyCHS")
-    process.slimmedMETsCHS.tXYUncForT1 = cms.InputTag("patPFMetT1TxyCHS")
-    process.slimmedMETsCHS.tXYUncForT01 = cms.InputTag("patPFMetT0pcT1TxyCHS")
-    process.slimmedMETsCHS.tXYUncForT1Smear = cms.InputTag("patPFMetT1SmearTxyCHS")
-    process.slimmedMETsCHS.tXYUncForT01Smear = cms.InputTag("patPFMetT0pcT1SmearTxyCHS")
-    del process.slimmedMETsCHS.caloMET
+    task.add(process.pfMetCHS)    
+
     #  ==================  CHSMET 
 
     #  ==================  TrkMET 
@@ -206,24 +197,14 @@ def miniAOD_customizeCommon(process):
                                     )
     task.add(process.TrkCands)
 
-    runMetCorAndUncForMiniAODProduction(process,
-                                        pfCandColl=cms.InputTag("TrkCands"),
-                                        recoMetFromPFCs=True,
-                                        postfix="Trk"
-                                        )
+    process.pfMetTrk = cms.EDProducer("PFMETProducer",
+                                      src = cms.InputTag("TrkCands"),
+                                      alias = cms.string('pfMet'),
+                                      globalThreshold = cms.double(0.0),
+                                      calculateSignificance = cms.bool(False),
+                                      )
 
-    addToProcessAndTask('slimmedMETsTrk', process.slimmedMETs.clone(), process, task)
-    process.slimmedMETsTrk.src = cms.InputTag("patMETsTrk")
-    process.slimmedMETsTrk.rawVariation =  cms.InputTag("patPFMetTrk")
-    process.slimmedMETsTrk.t1Uncertainties = cms.InputTag("patPFMetT1%sTrk") 
-    process.slimmedMETsTrk.t01Variation = cms.InputTag("patPFMetT0pcT1Trk")
-    process.slimmedMETsTrk.t1SmearedVarsAndUncs = cms.InputTag("patPFMetT1Smear%sTrk")
-    process.slimmedMETsTrk.tXYUncForRaw = cms.InputTag("patPFMetTxyTrk")
-    process.slimmedMETsTrk.tXYUncForT1 = cms.InputTag("patPFMetT1TxyTrk")
-    process.slimmedMETsTrk.tXYUncForT01 = cms.InputTag("patPFMetT0pcT1TxyTrk")
-    process.slimmedMETsTrk.tXYUncForT1Smear = cms.InputTag("patPFMetT1SmearTxyTrk")
-    process.slimmedMETsTrk.tXYUncForT01Smear = cms.InputTag("patPFMetT0pcT1SmearTxyTrk")
-    del process.slimmedMETsTrk.caloMET
+    task.add(process.pfMetTrk)
     #  ==================  TrkMET 
 
     ## PU JetID
