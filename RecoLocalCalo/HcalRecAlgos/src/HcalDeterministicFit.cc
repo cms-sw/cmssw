09830cf54d848923f7726cc4910393178af2de8c
Christopher_Madrid <Chris_Madrid@Baylor.edu>
2017-09-27 14:54:54 -0500

    Tracking down where TimeSlew is used "HcalDeterministicFit"

--- RecoLocalCalo/HcalRecAlgos/src/HcalDeterministicFit.cc
+++ RecoLocalCalo/HcalRecAlgos/src/HcalDeterministicFit.cc
@@ -11,7 +11,8 @@ constexpr float HcalDeterministicFit::rCorrSiPM[2];
 
 using namespace std;
 
-HcalDeterministicFit::HcalDeterministicFit() {
+HcalDeterministicFit::HcalDeterministicFit(const HcalTimeSlew* hcalTimeSlew_delay) : hcalTimeSlew_delay_(hcalTimeSlew_delay) 
+{
 }
 
 HcalDeterministicFit::~HcalDeterministicFit() { 
@@ -121,25 +122,20 @@ void HcalDeterministicFit::phase1Apply(const HBHEChannelInfo& channelData,
 
   const HcalDetId& cell = channelData.id();
 
-  //!!!!!!!!!!!!!!!!!!!!!!!!!!!
-  //DEAR HLT EXPERTS FORGIVE US
-  //WE KNOW NOT WHAT WE DO     
-  //--C.Madrid J.Pastika       
-  //!!!!!!!!!!!!!!!!!!!!!!!!!!!
-  //double fpar0, fpar1, fpar2;
-  //if(std::abs(cell.ieta())<HcalRegion[0]){
-  //  fpar0 = fpars[0];
-  //  fpar1 = fpars[1];
-  //  fpar2 = fpars[2];
-  //}else if(std::abs(cell.ieta())==HcalRegion[0]||std::abs(cell.ieta())==HcalRegion[1]){
-  //  fpar0 = fpars[3];
-  //  fpar1 = fpars[4];
-  //  fpar2 = fpars[5];
-  //}else{
-  //  fpar0 = fpars[6];
-  //  fpar1 = fpars[7];
-  //  fpar2 = fpars[8];
-  //}
+  double fpar0, fpar1, fpar2;
+  if(std::abs(cell.ieta())<HcalRegion[0]){
+    fpar0 = fpars[0];
+    fpar1 = fpars[1];
+    fpar2 = fpars[2];
+  }else if(std::abs(cell.ieta())==HcalRegion[0]||std::abs(cell.ieta())==HcalRegion[1]){
+    fpar0 = fpars[3];
+    fpar1 = fpars[4];
+    fpar2 = fpars[5];
+  }else{
+    fpar0 = fpars[6];
+    fpar1 = fpars[7];
+    fpar2 = fpars[8];
+  }
 
   if (fTimeSlew==0)respCorr=1.0;
   else if (fTimeSlew==1) channelData.hasTimeInfo()?respCorr=rCorrSiPM[0]:respCorr=rCorr[0];
@@ -151,14 +147,9 @@ void HcalDeterministicFit::phase1Apply(const HBHEChannelInfo& channelData,
 
   if(applyTimeSlew_) {
 
-    //!!!!!!!!!!!!!!!!!!!!!!!!!!!
-    //DEAR HLT EXPERTS FORGIVE US
-    //WE KNOW NOT WHAT WE DO     
-    //--C.Madrid J.Pastika       
-    //!!!!!!!!!!!!!!!!!!!!!!!!!!!
-    tsShift3=0;//HcalTimeSlew::delay(inputCharge[soi-1], fTimeSlew, fTimeSlewBias, fpar0, fpar1 ,fpar2,!channelData.hasTimeInfo());
-    tsShift4=0;//HcalTimeSlew::delay(inputCharge[soi], fTimeSlew, fTimeSlewBias, fpar0, fpar1 ,fpar2,!channelData.hasTimeInfo());
-    tsShift5=0;//HcalTimeSlew::delay(inputCharge[soi+1], fTimeSlew, fTimeSlewBias, fpar0, fpar1 ,fpar2,!channelData.hasTimeInfo());
+    tsShift3=hcalTimeSlew_delay_->delay(inputCharge[soi-1], fTimeSlew, fTimeSlewBias, fpar0, fpar1 ,fpar2,!channelData.hasTimeInfo());
+    tsShift4=hcalTimeSlew_delay_->delay(inputCharge[soi], fTimeSlew, fTimeSlewBias, fpar0, fpar1 ,fpar2,!channelData.hasTimeInfo());
+    tsShift5=hcalTimeSlew_delay_->delay(inputCharge[soi+1], fTimeSlew, fTimeSlewBias, fpar0, fpar1 ,fpar2,!channelData.hasTimeInfo());
 
   }
 
