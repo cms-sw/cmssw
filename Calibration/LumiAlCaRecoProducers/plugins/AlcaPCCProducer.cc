6a58ec2199da992130e7646f85919d169e0562c7
Christopher Jones <chrisdjones15@gmail.com>
2017-09-27 16:21:18 -0500

    Applied clang-tidy changes

--- Calibration/LumiAlCaRecoProducers/plugins/AlcaPCCProducer.cc
+++ Calibration/LumiAlCaRecoProducers/plugins/AlcaPCCProducer.cc
@@ -33,13 +33,13 @@ ________________________________________________________________**/
 class AlcaPCCProducer : public edm::one::EDProducer<edm::EndLuminosityBlockProducer,edm::one::WatchLuminosityBlocks>{
   public:
     explicit AlcaPCCProducer(const edm::ParameterSet&);
-    ~AlcaPCCProducer();
+    ~AlcaPCCProducer() override;
 
   private:
-    virtual void beginLuminosityBlock     (edm::LuminosityBlock const& lumiSeg, const edm::EventSetup& iSetup) ;
-    virtual void endLuminosityBlock       (edm::LuminosityBlock const& lumiSeg, const edm::EventSetup& iSetup) ;
-    virtual void endLuminosityBlockProduce(edm::LuminosityBlock& lumiSeg, const edm::EventSetup& iSetup) ;
-    virtual void produce                  (edm::Event& iEvent, const edm::EventSetup& iSetup) ;
+    void beginLuminosityBlock     (edm::LuminosityBlock const& lumiSeg, const edm::EventSetup& iSetup) override ;
+    void endLuminosityBlock       (edm::LuminosityBlock const& lumiSeg, const edm::EventSetup& iSetup) override ;
+    void endLuminosityBlockProduce(edm::LuminosityBlock& lumiSeg, const edm::EventSetup& iSetup) override ;
+    void produce                  (edm::Event& iEvent, const edm::EventSetup& iSetup) override ;
  
     edm::EDGetTokenT<edmNew::DetSetVector<SiPixelCluster> >  pixelToken;
     edm::InputTag   fPixelClusterLabel;
