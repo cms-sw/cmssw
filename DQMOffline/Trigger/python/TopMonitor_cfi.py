ac2fd8165660fa5bd00220aba1eb14a245fccbcd
Mohsan Waseem Ather <mather@cern.ch>
2017-09-28 14:18:53 +0200

    Missing Config Parameters in TopMonitor_cfi for backport

--- DQMOffline/Trigger/python/TopMonitor_cfi.py
+++ DQMOffline/Trigger/python/TopMonitor_cfi.py
@@ -14,6 +14,9 @@ hltTOPmonitoring.histoPSet.ptPSet = cms.PSet(
   xmin  = cms.double(   0   ),
   xmax  = cms.double(  300  ),
 )
+hltTOPmonitoring.histoPSet.lsPSet = cms.PSet(
+  nbins = cms.uint32( 2500 ),
+)
 hltTOPmonitoring.histoPSet.phiPSet = cms.PSet(
   nbins = cms.uint32(  32  ),
   xmin  = cms.double( -3.2 ),
@@ -54,12 +57,14 @@ hltTOPmonitoring.histoPSet.invMassPSet = cms.PSet(
   xmin  = cms.double( 0.0 ),
   xmax  = cms.double( 80.0  ),
 )
-#hltTOPmonitoring.histoPSet.MHTPSet = cms.PSet(
-#  nbins = cms.uint32(   80  ),
-#  xmin  = cms.double(   60   ),
-#  xmax  = cms.double(  300  ),
-#)
+hltTOPmonitoring.histoPSet.MHTPSet = cms.PSet(
+ nbins = cms.uint32(   80  ),
+ xmin  = cms.double(   60   ),
+ xmax  = cms.double(  300  ),
+)
 
+
+hltTOPmonitoring.enablePhotonPlot = cms.bool(False)
 
 #MET and HT binning
 hltTOPmonitoring.histoPSet.metBinning = cms.vdouble(0,20,40,60,80,100,125,150,175,200)
@@ -76,10 +81,12 @@ hltTOPmonitoring.histoPSet.muPtBinning  = cms.vdouble(0,5,10,20,30,40,50,70,100,
 hltTOPmonitoring.histoPSet.eleEtaBinning2D = cms.vdouble(-2.5,-1.5,-0.6,0.,0.6,1.5,2.5)
 hltTOPmonitoring.histoPSet.jetEtaBinning2D = cms.vdouble(-2.5,-1.5,-0.6,0.,0.6,1.5,2.5)
 hltTOPmonitoring.histoPSet.muEtaBinning2D  = cms.vdouble(-2.5,-1.5,-0.6,0.,0.6,1.5,2.5)
+hltTOPmonitoring.histoPSet.phoEtaBinning2D = cms.vdouble(-2.5,-1.5,-0.6,0.,0.6,1.5,2.5)
 #pt binning 2D
 hltTOPmonitoring.histoPSet.elePtBinning2D = cms.vdouble(0,20,30,50,100,200,400)
 hltTOPmonitoring.histoPSet.jetPtBinning2D = cms.vdouble(0,20,30,50,100,200,400)
 hltTOPmonitoring.histoPSet.muPtBinning2D  = cms.vdouble(0,20,30,50,100,200,400)
+hltTOPmonitoring.histoPSet.phoPtBinning2D = cms.vdouble(0,20,30,50,100,200,400)
 #HT and phi binning 2D
 hltTOPmonitoring.histoPSet.HTBinning2D  = cms.vdouble(0,20,40,70,100,150,200,400,700)
 hltTOPmonitoring.histoPSet.phiBinning2D = cms.vdouble(-3.1416,-1.8849,-0.6283,0.6283,1.8849,3.1416)
@@ -92,6 +99,7 @@ hltTOPmonitoring.electrons = cms.InputTag("gedGsfElectrons") # while pfIsolatedE
 hltTOPmonitoring.elecID    = cms.InputTag("egmGsfElectronIDsForDQM:cutBasedElectronID-Summer16-80X-V1-medium") #Electron ID
 
 hltTOPmonitoring.muons     = cms.InputTag("muons") # while pfIsolatedMuonsEI are reco::PFCandidate !
+hltTOPmonitoring.photons   = cms.InputTag("photons") #reco::Photon 
 #Suvankar
 hltTOPmonitoring.vertices  = cms.InputTag("offlinePrimaryVertices")
 
