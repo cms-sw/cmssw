73130a542667e8c24e71fd74138a1604e5272444
georgios karathanasis <georgios.karathanasis@cern.ch>
2017-10-03 13:48:02 +0200

    fixing lines

--- DQMOffline/Trigger/python/SusyMonitoring_Client_cff.py
+++ DQMOffline/Trigger/python/SusyMonitoring_Client_cff.py
@@ -103,16 +103,16 @@ triple_muon_mupt_efficiency = DQMEDHarvester("DQMGenericClient",
     verbose        = cms.untracked.uint32(0), # Set to 2 for all messages 
     resolution     = cms.vstring(),
     efficiency     = cms.vstring(
-"effic_muPt_1       'efficiency vs muon pt; muon pt [GeV]; efficiency' muPt_1_numerator       muPt_1_variableBinning_denominator",
-   "effic_muEta_1       'efficiency vs muon eta; muon eta ; efficiency' muEta_1_variableBinning_numerator       muEta_1_variableBinning_denominator",
-     "effic_muPhi_1       'efficiency vs muon phi; muon phi ; efficiency' muPhi_1_numerator       muPhi_1_denominator",
+    "effic_muPt_1       'efficiency vs muon pt; muon pt [GeV]; efficiency' muPt_1_numerator       muPt_1_variableBinning_denominator",
+    "effic_muEta_1       'efficiency vs muon eta; muon eta ; efficiency' muEta_1_variableBinning_numerator       muEta_1_variableBinning_denominator",
+    "effic_muPhi_1       'efficiency vs muon phi; muon phi ; efficiency' muPhi_1_numerator       muPhi_1_denominator",
 
-     "effic_muPt_2       'efficiency vs muon pt; muon pt [GeV]; efficiency' muPt_2_variableBinning_numerator       muPt_2_variableBinning_denominator",
-      "effic_muEta_2       'efficiency vs muon eta; muon eta ; efficiency' muEta_2_variableBinning_numerator       muEta_2_variableBinning_denominator",
-      "effic_muPhi_2       'efficiency vs muon phi; muon phi ; efficiency' muPhi_2_numerator       muPhi_2_denominator",
-"effic_muPt_3       'efficiency vs muon pt; muon pt [GeV]; efficiency' muPt_3_variableBinning_numerator       muPt_3_variableBinning_denominator",
-      "effic_muEta_3       'efficiency vs muon eta; muon eta ; efficiency' muEta_3_variableBinning_numerator       muEta_3_variableBinning_denominator",
-  "effic_muPhi_2       'efficiency vs muon phi; muon phi ; efficiency' muPhi_3_numerator       muPhi_3_denominator",
+    "effic_muPt_2       'efficiency vs muon pt; muon pt [GeV]; efficiency' muPt_2_variableBinning_numerator       muPt_2_variableBinning_denominator",
+    "effic_muEta_2       'efficiency vs muon eta; muon eta ; efficiency' muEta_2_variableBinning_numerator       muEta_2_variableBinning_denominator",
+    "effic_muPhi_2       'efficiency vs muon phi; muon phi ; efficiency' muPhi_2_numerator       muPhi_2_denominator",
+    "effic_muPt_3       'efficiency vs muon pt; muon pt [GeV]; efficiency' muPt_3_variableBinning_numerator       muPt_3_variableBinning_denominator",
+    "effic_muEta_3       'efficiency vs muon eta; muon eta ; efficiency' muEta_3_variableBinning_numerator       muEta_3_variableBinning_denominator",
+    "effic_muPhi_2       'efficiency vs muon phi; muon phi ; efficiency' muPhi_3_numerator       muPhi_3_denominator",
         ),
 )
 
