0c8d2c82e80f3cb48ad8949d9f02aefb537dcd51
Markus Seidel <markus.seidel@cern.ch>
2017-10-04 11:20:04 +0200

    Merged RivetJets with GenJets (no lepton cleaning), removed RivetTags, renamed RivetFatJets/DressedLeptons->GenFatJets/DressedLeptons

--- PhysicsTools/NanoAOD/python/particlelevel_cff.py
+++ PhysicsTools/NanoAOD/python/particlelevel_cff.py
@@ -20,23 +20,23 @@ particleLevel = cms.EDProducer("ParticleLevelProducer",
     src = cms.InputTag("genParticles2HepMC:unsmeared"),
     
     usePromptFinalStates = cms.bool(True), # for leptons, photons, neutrinos
-    excludePromptLeptonsFromJetClustering = cms.bool(True),
+    excludePromptLeptonsFromJetClustering = cms.bool(False),
     excludeNeutrinosFromJetClustering = cms.bool(True),
     
     particleMinPt  = cms.double(0.),
-    particleMaxEta = cms.double(5.), # HF range. Maximum 6.0 on MiniAOD
+    particleMaxEta = cms.double(999.), # HF range. Maximum 6.0 on MiniAOD
     
     lepConeSize = cms.double(0.1), # for photon dressing
     lepMinPt    = cms.double(15.),
     lepMaxEta   = cms.double(2.5),
     
     jetConeSize = cms.double(0.4),
-    jetMinPt    = cms.double(20.),
-    jetMaxEta   = cms.double(2.4),
+    jetMinPt    = cms.double(10.),
+    jetMaxEta   = cms.double(999.),
     
     fatJetConeSize = cms.double(0.8),
-    fatJetMinPt    = cms.double(200.),
-    fatJetMaxEta   = cms.double(2.4),
+    fatJetMinPt    = cms.double(170.),
+    fatJetMaxEta   = cms.double(999.),
 )
 
 
@@ -44,8 +44,8 @@ particleLevel = cms.EDProducer("ParticleLevelProducer",
 ##################### Tables for final output and docs ##########################
 rivetLeptonTable = cms.EDProducer("SimpleCandidateFlatTableProducer",
     src = cms.InputTag("particleLevel:leptons"),
-    cut = cms.string(""), #we should not filter after pruning
-    name= cms.string("RivetDressedLepton"),
+    cut = cms.string(""),
+    name= cms.string("GenDressedLepton"),
     doc = cms.string("Dressed leptons from Rivet-based ParticleLevelProducer"),
     singleton = cms.bool(False), # the number of entries is variable
     extension = cms.bool(False), # this is the main table
@@ -57,46 +57,47 @@ rivetLeptonTable = cms.EDProducer("SimpleCandidateFlatTableProducer",
 
 rivetJetTable = cms.EDProducer("SimpleCandidateFlatTableProducer",
     src = cms.InputTag("particleLevel:jets"),
-    cut = cms.string(""), #we should not filter after pruning
-    name= cms.string("RivetJet"),
+    cut = cms.string(""),
+    name= cms.string("GenJet"),
     doc = cms.string("AK4 jets from Rivet-based ParticleLevelProducer"),
     singleton = cms.bool(False), # the number of entries is variable
-    extension = cms.bool(False), # this is the main table
+    extension = cms.bool(True),
     variables = cms.PSet(
-        P4Vars,
-        pdgId = Var("pdgId", int, doc="PDG id"), 
+        # Identical to GenJets, so we just extend their flavor information
+        #P4Vars,
+        hadronFlavour = Var("pdgId", int, doc="PDG id"), 
     )
 )
 
 rivetFatJetTable = cms.EDProducer("SimpleCandidateFlatTableProducer",
     src = cms.InputTag("particleLevel:fatjets"),
-    cut = cms.string(""), #we should not filter after pruning
-    name= cms.string("RivetFatJet"),
+    cut = cms.string(""),
+    name= cms.string("GenFatJet"),
     doc = cms.string("AK8 jets from Rivet-based ParticleLevelProducer"),
     singleton = cms.bool(False), # the number of entries is variable
     extension = cms.bool(False), # this is the main table
     variables = cms.PSet(
         P4Vars,
-        pdgId = Var("pdgId", int, doc="PDG id"), 
+        hadronFlavour = Var("pdgId", int, doc="PDG id"), 
     )
 )
 
-rivetTagTable = cms.EDProducer("SimpleCandidateFlatTableProducer",
-    src = cms.InputTag("particleLevel:tags"),
-    cut = cms.string(""), #we should not filter after pruning
-    name= cms.string("RivetTag"),
-    doc = cms.string("Tag particles from Rivet-based ParticleLevelProducer, momenta scaled down by 10e-20"),
-    singleton = cms.bool(False), # the number of entries is variable
-    extension = cms.bool(False), # this is the main table
-    variables = cms.PSet(
-        P4Vars,
-        pdgId = Var("pdgId", int, doc="PDG id"), 
-    )
-)
+#rivetTagTable = cms.EDProducer("SimpleCandidateFlatTableProducer",
+#    src = cms.InputTag("particleLevel:tags"),
+#    cut = cms.string(""),
+#    name= cms.string("RivetTag"),
+#    doc = cms.string("Tag particles from Rivet-based ParticleLevelProducer, momenta scaled down by 10e-20"),
+#    singleton = cms.bool(False), # the number of entries is variable
+#    extension = cms.bool(False), # this is the main table
+#    variables = cms.PSet(
+#        P4Vars,
+#        pdgId = Var("pdgId", int, doc="PDG id"), 
+#    )
+#)
 
 rivetMetTable = cms.EDProducer("SimpleCandidateFlatTableProducer",
     src = cms.InputTag("particleLevel:mets"),
-    name = cms.string("RivetMET"),
+    name = cms.string("RivetGenMET"),
     doc = cms.string("MET from Rivet-based ParticleLevelProducer"),
     singleton = cms.bool(True),  # there's always exactly one MET per event
     extension = cms.bool(False), # this is the main table
@@ -107,5 +108,4 @@ rivetMetTable = cms.EDProducer("SimpleCandidateFlatTableProducer",
 )
 
 particleLevelSequence = cms.Sequence(mergedGenParticles + genParticles2HepMC + particleLevel)
-particleLevelTables = cms.Sequence(rivetLeptonTable + rivetJetTable + rivetFatJetTable + rivetTagTable + rivetMetTable)
-
+particleLevelTables = cms.Sequence(rivetLeptonTable + rivetJetTable + rivetFatJetTable + rivetMetTable)
