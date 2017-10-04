6401dd7e0f768d59da4fcf285f83fceb0d2687f5
Chris Palmer <capalmer@cern.ch>
2017-10-04 21:48:25 +0200

    change Set, Get to set, get

--- CondFormats/Luminosity/interface/LumiCorrections.h
+++ CondFormats/Luminosity/interface/LumiCorrections.h
@@ -15,16 +15,16 @@
 
 class LumiCorrections {
     public:
-        void  SetOverallCorrection(float overallCorrection){m_overallCorrection=overallCorrection;}
-        void  SetType1Fraction(float type1frac){m_type1Fraction=type1frac;}
-        void  SetType1Residual(float type1res){m_type1Residual=type1res;}
-        void  SetType2Residual(float type2res){m_type2Residual=type2res;}
-        void  SetCorrectionsBX(std::vector<float>& correctBX){m_correctionsBX.assign(correctBX.begin(),correctBX.end());} 
-        float GetOverallCorrection(){return m_overallCorrection;}
-        float GetType1Fraction(){return m_type1Fraction;}
-        float GetType1Residual(){return m_type1Residual;}
-        float GetType2Residual(){return m_type2Residual;}
-        const std::vector<float>& GetCorrectionsBX() const{return m_correctionsBX;} 
+        void  setOverallCorrection(float overallCorrection){m_overallCorrection=overallCorrection;}
+        void  setType1Fraction(float type1frac){m_type1Fraction=type1frac;}
+        void  setType1Residual(float type1res){m_type1Residual=type1res;}
+        void  setType2Residual(float type2res){m_type2Residual=type2res;}
+        void  setCorrectionsBX(std::vector<float>& correctBX){m_correctionsBX.assign(correctBX.begin(),correctBX.end());} 
+        float getOverallCorrection(){return m_overallCorrection;}
+        float getType1Fraction(){return m_type1Fraction;}
+        float getType1Residual(){return m_type1Residual;}
+        float getType2Residual(){return m_type2Residual;}
+        const std::vector<float>& getCorrectionsBX() const{return m_correctionsBX;} 
     private:
         float m_overallCorrection;
         float m_type1Fraction;
