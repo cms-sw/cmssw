52d6a9d1620653a2b186430dbffc18fc18915d96
Zeynep Demiragli <zeynep.demiragli@gmail.com>
2017-10-05 12:39:45 +0200

    fixing indentation

--- DataFormats/PatCandidates/interface/MET.h
+++ DataFormats/PatCandidates/interface/MET.h
@@ -149,21 +149,21 @@ namespace pat {
 
       // ---- members for MET corrections ----
       enum METUncertainty {
-	JetResUp=0, JetResDown=1, JetEnUp=2, JetEnDown=3,
-        MuonEnUp=4, MuonEnDown=5, ElectronEnUp=6, ElectronEnDown=7,
-	TauEnUp=8, TauEnDown=9, UnclusteredEnUp=10, UnclusteredEnDown=11,
-	PhotonEnUp=12, PhotonEnDown=13, NoShift=14, METUncertaintySize=15,
-	JetResUpSmear=16, JetResDownSmear=17, METFullUncertaintySize=18
+       JetResUp=0, JetResDown=1, JetEnUp=2, JetEnDown=3,
+       MuonEnUp=4, MuonEnDown=5, ElectronEnUp=6, ElectronEnDown=7,
+       TauEnUp=8, TauEnDown=9, UnclusteredEnUp=10, UnclusteredEnDown=11,
+       PhotonEnUp=12, PhotonEnDown=13, NoShift=14, METUncertaintySize=15,
+       JetResUpSmear=16, JetResDownSmear=17, METFullUncertaintySize=18
       };
       enum METCorrectionLevel {
-        Raw=0, Type1=1, Type01=2, TypeXY=3, Type1XY=4, Type01XY=5,
-	Type1Smear=6, Type01Smear=7, Type1SmearXY=8, 
-    Type01SmearXY=9, RawCalo=10, RawChs=11, RawTrk=12, METCorrectionLevelSize=13
+       Raw=0, Type1=1, Type01=2, TypeXY=3, Type1XY=4, Type01XY=5,
+       Type1Smear=6, Type01Smear=7, Type1SmearXY=8, 
+       Type01SmearXY=9, RawCalo=10, RawChs=11, RawTrk=12, METCorrectionLevelSize=13
       };
       enum METCorrectionType {
-        None=0, T1=1, T0=2, TXY=3, TXYForRaw=4,
-	TXYForT01=5, TXYForT1Smear=6, TXYForT01Smear=7,
-    Smear=8, Calo=9, Chs=10, Trk=11, METCorrectionTypeSize=12
+       None=0, T1=1, T0=2, TXY=3, TXYForRaw=4,
+       TXYForT01=5, TXYForT1Smear=6, TXYForT01Smear=7,
+       Smear=8, Calo=9, Chs=10, Trk=11, METCorrectionTypeSize=12
       };
 
       struct Vector2 { 
