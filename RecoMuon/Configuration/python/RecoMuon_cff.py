02520c333ba72a02e54e69eb830192e9915d44b5
Dmytro Kovalskyi <dmytro@cern.ch>
2017-10-06 15:45:52 +0200

    add muon simhit based classifier

--- RecoMuon/Configuration/python/RecoMuon_cff.py
+++ RecoMuon/Configuration/python/RecoMuon_cff.py
@@ -58,4 +58,5 @@ muoncosmichighlevelreco = cms.Sequence((muoncosmicreco2legsHighLevel+muoncosmicr
 #### High level sequence (i.e., post PF reconstruction) ###
 from RecoMuon.MuonIdentification.muons_cfi import *
 from RecoMuon.MuonIsolation.muonPFIsolation_cff import *
-muonshighlevelreco = cms.Sequence(muonPFIsolationSequence*muons) 
+from RecoMuon.SimMuonAlgos.muonSimClassificationByHits_cff import *
+muonshighlevelreco = cms.Sequence(muonPFIsolationSequence*muons*muonSimClassificationByHitsSequence) 
