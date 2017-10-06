52c9d2e3b49b3ea5058528be6f0c91a760a0b8a0
Matthew <Matthew.Nguyen@cern.ch>
2017-10-06 13:46:12 +0200

    More style changes for Matti

--- RecoTracker/TkTrackingRegions/python/globalTrackingRegionWithVertices_cff.py
+++ RecoTracker/TkTrackingRegions/python/globalTrackingRegionWithVertices_cff.py
@@ -5,14 +5,14 @@ from Configuration.Eras.Modifier_pp_on_XeXe_2017_cff import pp_on_XeXe_2017
 
 pp_on_XeXe_2017.toModify(globalTrackingRegionWithVertices, RegionPSet = dict(
        VertexCollection = "firstStepPrimaryVertices",
-       beamSpot = cms.InputTag("offlineBeamSpot"),
-       maxNVertices = cms.int32(-1),
-       nSigmaZ = cms.double(4.),
-       precise = cms.bool(True),
-       sigmaZVertex = cms.double(4.),
-       useFakeVertices = cms.bool(False),
-       useFixedError = cms.bool(True),
-       useFoundVertices = cms.bool(True),
-       useMultipleScattering = cms.bool(False)                                                  
+       beamSpot = "offlineBeamSpot",
+       maxNVertices = -1,
+       nSigmaZ = 4.,
+       precise = True,
+       sigmaZVertex = 4.,
+       useFakeVertices = False,
+       useFixedError = True,
+       useFoundVertices = True,
+       useMultipleScattering = False                                                  
        )
 )
