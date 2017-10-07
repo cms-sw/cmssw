4ee871bf9fdf239c4bb8467ba2a9ea7803bfc27b
Vladimir <vladimir.rekovic@cern.ch>
2017-10-07 17:40:06 +0200

    Allow new EMTF to configure from statatic config (still using DB), till we push Forests in CMSSW.

--- L1Trigger/Configuration/python/SimL1Emulator_cff.py
+++ L1Trigger/Configuration/python/SimL1Emulator_cff.py
@@ -52,6 +52,10 @@ SimL1Emulator = cms.Sequence( SimL1EmulatorCore )
 #from L1Trigger.L1TCalorimeter.hackConditions_cff import *
 #from L1Trigger.L1TMuon.hackConditions_cff import *
 # from L1Trigger.L1TGlobal.hackConditions_cff import *
+
+# 2017 EMTF emulator uses payloads and forests from DB, but not yet in GT
+from L1Trigger.L1TMuonEndCap.fakeEmtfParams_2017_data_cff import *
+
 from L1Trigger.L1TGlobal.GlobalParameters_cff import *
 
 # Customisation for the phase2_hgcal era. Includes the HGCAL L1 trigger
