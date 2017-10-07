21414ae0f1076d006e4ca3457bac914c7dd0c021
David <David.Lange@cern.ch>
2017-10-07 17:01:04 +0200

    clang check RecoRomanPot

--- RecoRomanPot/RecoFP420/interface/TrackerizerFP420.h
+++ RecoRomanPot/RecoFP420/interface/TrackerizerFP420.h
@@ -34,12 +34,12 @@ namespace cms
     explicit TrackerizerFP420(const edm::ParameterSet& conf);
     //TrackerizerFP420();
     
-    virtual ~TrackerizerFP420();
+    ~TrackerizerFP420() override;
     
-    virtual void beginJob();
+    void beginJob() override;
     
     //  virtual void produce(ClusterCollectionFP420 &, TrackCollectionFP420 &);
-    virtual void produce(edm::Event& e, const edm::EventSetup& c);
+    void produce(edm::Event& e, const edm::EventSetup& c) override;
     
   private:
     typedef std::vector<std::string> vstring;
