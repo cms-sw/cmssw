055c01ddd03d0fc4b421baab311e21af32229f41
Vladimir <vladimir.rekovic@cern.ch>
2017-10-09 01:21:44 +0200

    Add RPCTwinMuxRawToDigi to the L1TRawToDigi_Stage2 sequence.  Also  include the ESSource RPCLinkMapSource till it makes it into the GT.

--- CondTools/RPC/python/RPCLinkMap_CondDB_cff.py
+++ CondTools/RPC/python/RPCLinkMap_CondDB_cff.py
@@ -27,29 +27,3 @@ RPCLinkMapSource = cms.ESSource('PoolDBESSource'
                                 )
                                 , connect = cms.string("frontier://FrontierProd/CMS_CONDITIONS")
 )
-
-twinmuxParamsSource = cms.ESSource("EmptyESSource",
-    recordName = cms.string('L1TTwinMuxParamsRcd'),
-    iovIsRunNotTime = cms.bool(True),
-    firstValid = cms.vuint32(1)
-)
-#ego
-bmbtfParamsSource = cms.ESSource(
-    "EmptyESSource",
-    recordName = cms.string('L1TMuonBarrelParamsRcd'),
-    iovIsRunNotTime = cms.bool(True),
-    firstValid = cms.vuint32(1)
-)
-
-#here
-fakeTwinMuxParams = cms.ESProducer('L1TTwinMuxParamsESProducer',
-    fwVersion = cms.uint32(1),
-    useRpcBxForDtBelowQuality = cms.uint32(4),#correct bx only if dt ts is below
-    dphiWindowBxShift         = cms.uint32(9999),
-    useOnlyRPC                = cms.bool(False),#use trigger segments (ts)  from rpc hit
-    useOnlyDT                 = cms.bool(False),# use ts from dt
-    useLowQDT                 = cms.bool(False),
-    CorrectDTBxwRPC           = cms.bool(True),#correct bx of dt ts based on rpc
-    verbose                   = cms.bool(False),
-
-)
