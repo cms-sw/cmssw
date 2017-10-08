aa8b4833f22acf648aeb9c3fc0c0b8daa8fc0546
Vladimir <vladimir.rekovic@cern.ch>
2017-10-08 20:45:37 +0200

    Rename caloParams file to the next version 2017_v1_8_4.

--- L1Trigger/Configuration/python/customiseSettings.py
+++ L1Trigger/Configuration/python/customiseSettings.py
@@ -41,8 +41,8 @@ def L1TSettingsToCaloStage2Params_2017_v1_8_2_updateHFSF_v7MET(process):
     process.load("L1Trigger.L1TCalorimeter.caloStage2Params_2017_v1_8_2_updateHFSF_v7MET_cfi")
     return process
 
-def L1TSettingsToCaloStage2Params_2017_v1_8_3_updateHFSF_v7MET(process):
-    process.load("L1Trigger.L1TCalorimeter.caloStage2Params_2017_v1_8_3_updateHFSF_v7MET_cfi")
+def L1TSettingsToCaloStage2Params_2017_v1_8_4(process):
+    process.load("L1Trigger.L1TCalorimeter.caloStage2Params_2017_v1_8_4_cfi")
     return process
 
 def L1TSettingsToCaloStage2Params_2017_v1_8_2_updateHFSF_v6MET(process):
