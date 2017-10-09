8c41f4b289c6828dab5a5adc79ac4dc90761274b
Vladimir <vladimir.rekovic@cern.ch>
2017-10-09 03:15:40 +0200

    Rename BMTF packer to bmtfStage2Raw, as per convention.

--- EventFilter/L1TRawToDigi/python/bmtfStage2Raw_cfi.py
+++ EventFilter/L1TRawToDigi/python/bmtfStage2Raw_cfi.py
@@ -3,7 +3,7 @@ import FWCore.ParameterSet.Config as cms
 print "\n!!!!! WARNING: The BMTF Packer returns the entire detector information in one fed (# 1376), instead of the real BMTF which owns 2 feds (1376, 1377)  !!!!!\n"
 
 
-BMTFStage2Digis = cms.EDProducer(
+bmtfStage2Raw = cms.EDProducer(
     "L1TDigiToRaw",
     Setup = cms.string("stage2::BMTFSetup"),
     InputLabel = cms.InputTag("BMTFStage2Digis","BMTF"),
