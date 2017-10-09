30fa9d1db3ba1f0514797e174a512cbd3ed41cf0
Giovanni <gpetruc@gmail.com>
2017-10-09 15:15:45 +0200

    Get DQM running on top of current master branch

--- PhysicsTools/NanoAOD/python/nanoDQM_cff.py
+++ PhysicsTools/NanoAOD/python/nanoDQM_cff.py
@@ -3,12 +3,9 @@ import FWCore.ParameterSet.Config as cms
 from PhysicsTools.NanoAOD.nanoDQM_cfi import nanoDQM
 
 nanoDQMMC = nanoDQM.clone()
-nanoDQMMC.vplots.Electron.sels.Prompt = cms.string("mcMatchFlav == 1")
-nanoDQMMC.vplots.Muon.sels.Prompt = cms.string("mcMatchFlav == 1")
-nanoDQMMC.vplots.Photon.sels.Prompt = cms.string("mcMatchFlav == 1")
-nanoDQMMC.vplots.Tau.sels.Prompt = cms.string("mcMatchFlav == 5")
+nanoDQMMC.vplots.Electron.sels.Prompt = cms.string("genPartFlav == 1")
+nanoDQMMC.vplots.Muon.sels.Prompt = cms.string("genPartFlav == 1")
+nanoDQMMC.vplots.Photon.sels.Prompt = cms.string("genPartFlav == 1")
+nanoDQMMC.vplots.Tau.sels.Prompt = cms.string("genPartFlav == 5")
 nanoDQMMC.vplots.Jet.sels.Prompt = cms.string("genJetIdx != 1")
 nanoDQMMC.vplots.Jet.sels.PromptB = cms.string("genJetIdx != 1 && hadronFlavour == 5")
-
-nanoDQMSequence = cms.Sequence(nanoDQM)
-nanoDQMSequenceMC = cms.Sequence(nanoDQMMC)
