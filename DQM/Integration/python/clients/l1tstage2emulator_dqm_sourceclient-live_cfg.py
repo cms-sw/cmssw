ff15f07a2b70f991653632d99d648ec122ece30b
Vladimir <vladimir.rekovic@cern.ch>
2017-10-10 09:42:28 +0200

    Allow new EMTF emulator to configure (for MC) from DB via Frontier, as GT not yet updated. Turn-off hackConditions in Offline DQM.

--- DQM/Integration/python/clients/l1tstage2emulator_dqm_sourceclient-live_cfg.py
+++ DQM/Integration/python/clients/l1tstage2emulator_dqm_sourceclient-live_cfg.py
@@ -83,6 +83,9 @@ process.l1tEmulatorMonitorPath = cms.Path(
 #process.load("L1Trigger.L1TGlobal.hackConditions_cff")
 process.load("L1Trigger.L1TGlobal.GlobalParameters_cff")
 
+# 2017 EMTF emulator uses payloads and forests from DB, but not yet in GT
+process.load("L1Trigger.L1TMuonEndCap.fakeEmtfParams_2017_MC_cff")
+
 # To get CaloTPGTranscoder
 process.load('SimCalorimetry.HcalTrigPrimProducers.hcaltpdigi_cff')
 process.HcalTPGCoderULUT.LUTGenerationMode = cms.bool(False)
