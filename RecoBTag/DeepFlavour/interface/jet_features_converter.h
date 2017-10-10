feadf170ce97f280374abed50c279b545ba99bc7
Pablo de Castro <pablodecm@gmail.com>
2017-10-10 22:42:36 +0200

    make converters classes with static members (modify commit)

--- RecoBTag/DeepFlavour/interface/jet_features_converter.h
+++ RecoBTag/DeepFlavour/interface/jet_features_converter.h
@@ -1,21 +1,23 @@
-#ifndef RecoBTag_DeepFlavour_jet_features_converter_h
-#define RecoBTag_DeepFlavour_jet_features_converter_h
+#ifndef RecoBTag_DeepFlavour_JetConverter_h
+#define RecoBTag_DeepFlavour_JetConverter_h
 
 namespace btagbtvdeep {
 
-  template <typename JetType, typename JetFeaturesType>
-  void jet_features_converter( const JetType & jet, JetFeaturesType & jet_features) {
+  class JetConverter {
+    public:
 
-    jet_features.pt = jet.pt(); // uncorrected was used on DeepNtuples
-    jet_features.eta = jet.eta();
-    jet_features.phi = jet.phi();
-    jet_features.corr_pt = jet.pt();
-    jet_features.mass = jet.mass();
-    jet_features.energy = jet.energy();
-
-  } 
+      template <typename JetType, typename JetFeaturesType>
+      static void JetToFeatures( const JetType & jet, JetFeaturesType & jet_features) {
+        jet_features.pt = jet.pt(); // uncorrected was used on DeepNtuples
+        jet_features.eta = jet.eta();
+        jet_features.phi = jet.phi();
+        jet_features.corr_pt = jet.pt();
+        jet_features.mass = jet.mass();
+        jet_features.energy = jet.energy();
+      }
+  }; 
 
 }
 
-#endif
+#endif //RecoBTag_DeepFlavour_JetConverter_h
 
