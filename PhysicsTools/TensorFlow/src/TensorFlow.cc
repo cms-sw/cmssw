4301b647a9d42eae9368764ecac2752ea2cc82b3
Marcel Rieger <marcel.rieger@cern.ch>
2017-10-11 01:27:00 +0200

    Refactor TF interface, use C++ API.

---
+++ PhysicsTools/TensorFlow/src/TensorFlow.cc
@@ -0,0 +1,162 @@
+/*
+ * TensorFlow interface helpers.
+ * Based on TensorFlow C++ API 1.3.
+ * For more info, see https://gitlab.cern.ch/mrieger/CMSSW-DNN.
+ *
+ * Author: Marcel Rieger
+ */
+
+#include "PhysicsTools/TensorFlow/interface/TensorFlow.h"
+
+namespace tf
+{
+
+void setLogging(const std::string& level)
+{
+    setenv("TF_CPP_MIN_LOG_LEVEL", level.c_str(), 0);
+}
+
+void setThreading(SessionOptions& sessionOptions, int nThreads)
+{
+    sessionOptions.config.set_intra_op_parallelism_threads(nThreads);
+    sessionOptions.config.set_inter_op_parallelism_threads(nThreads);
+}
+
+MetaGraphDef* loadMetaGraph(const std::string& exportDir, bool multiThreaded,
+    const std::string& tag)
+{
+    // objects to load the graph
+    Status status;
+    SessionOptions sessionOptions;
+    RunOptions runOptions;
+    SavedModelBundle bundle;
+
+    // set the number of threads
+    setThreading(sessionOptions, multiThreaded ? 0 : 1);
+
+    // load the model
+    status = LoadSavedModel(sessionOptions, runOptions, exportDir, { tag }, &bundle);
+    if (!status.ok())
+    {
+        throw cms::Exception("InvalidGraph")
+            << "error while loading graph: " << status.ToString();
+    }
+
+    // return and return a copy
+    return new MetaGraphDef(bundle.meta_graph_def);
+}
+
+Session* createSession(bool multiThreaded)
+{
+    // objects to create the session
+    Status status;
+    SessionOptions sessionOptions;
+
+    // set the number of threads
+    setThreading(sessionOptions, multiThreaded ? 0 : 1);
+
+    // create a new, empty session
+    tf::Session* session = nullptr;
+    status = tf::NewSession(sessionOptions, &session);
+    if (!status.ok())
+    {
+        throw cms::Exception("InvalidSession")
+            << "error while creating session: " << status.ToString();
+    }
+
+    return session;
+}
+
+Session* createSession(MetaGraphDef* metaGraph, const std::string& exportDir, bool multiThreaded)
+{
+    Session* session = createSession(multiThreaded);
+
+    // add the graph def from the meta graph
+    Status status;
+    status = session->Create(metaGraph->graph_def());
+    if (!status.ok())
+    {
+        throw cms::Exception("InvalidSession")
+            << "error while attaching graph to session: " << status.ToString();
+    }
+
+    // restore variables using the variable and index files in the export directory
+    // first, find names and paths
+    std::string varFileTensorName = metaGraph->saver_def().filename_tensor_name();
+    std::string restoreOpName = metaGraph->saver_def().restore_op_name();
+    std::string varDir = io::JoinPath(exportDir, kSavedModelVariablesDirectory);
+    std::string indexFile = io::JoinPath(varDir, MetaFilename(kSavedModelVariablesFilename));
+    std::string varFile = io::JoinPath(varDir, kSavedModelVariablesFilename);
+
+    // when the index file is missing, there's nothing to do
+    if (!Env::Default()->FileExists(indexFile).ok())
+    {
+        return session;
+    }
+
+    // create a tensor to store the variable file
+    Tensor varFileTensor(DT_STRING, TensorShape({}));
+    varFileTensor.scalar<std::string>()() = varFile;
+
+    status = session->Run({ { varFileTensorName, varFileTensor } }, {}, { restoreOpName }, nullptr);
+
+    if (!status.ok())
+    {
+        throw cms::Exception("InvalidSession")
+            << "error while restoring variables in session: " << status.ToString();
+    }
+
+    return session;
+}
+
+void run(Session* session, const NamedTensorList& inputs,
+    const std::vector<std::string>& outputNames, const std::vector<std::string>& targetNodes,
+    std::vector<Tensor>* outputs)
+{
+    if (session == nullptr)
+    {
+        throw cms::Exception("InvalidSession") << "cannot run empty session";
+    }
+
+    // run and check the status
+    Status status;
+    status = session->Run(inputs, outputNames, targetNodes, outputs);
+    if (!status.ok())
+    {
+        throw cms::Exception("InvalidRun")
+            << "error while running session: " << status.ToString();
+    }
+}
+
+void run(Session* session, const std::vector<std::string>& inputNames,
+    const std::vector<Tensor>& inputTensors, const std::vector<std::string>& outputNames,
+    const std::vector<std::string>& targetNodes, std::vector<Tensor>* outputs)
+{
+    if (inputNames.size() != inputTensors.size())
+    {
+        throw cms::Exception("InvalidInput") << "numbers of input names and tensors not equal";
+    }
+
+    NamedTensorList inputs;
+    for (size_t i = 0; i < inputNames.size(); i++)
+    {
+        inputs.push_back(NamedTensor(inputNames[i], inputTensors[i]));
+    }
+
+    run(session, inputs, outputNames, targetNodes, outputs);
+}
+
+void run(Session* session, const NamedTensorList& inputs,
+    const std::vector<std::string>& outputNames, std::vector<Tensor>* outputs)
+{
+    run(session, inputs, outputNames, {}, outputs);
+}
+
+void run(Session* session, const std::vector<std::string>& inputNames,
+    const std::vector<Tensor>& inputTensors, const std::vector<std::string>& outputNames,
+    std::vector<Tensor>* outputs)
+{
+    run(session, inputNames, inputTensors, outputNames, {}, outputs);
+}
+
+} // namespace tf
---
+++ PhysicsTools/TensorFlow/test/testLoading.cc
@@ -0,0 +1,123 @@
+/*
+ * Graph and session loading tests.
+ * Based on TensorFlow C++ API 1.3.
+ * For more info, see https://gitlab.cern.ch/mrieger/CMSSW-DNN.
+ *
+ * Author: Marcel Rieger
+ */
+
+#include <boost/filesystem.hpp>
+#include <cppunit/extensions/HelperMacros.h>
+#include <stdexcept>
+
+#include "PhysicsTools/TensorFlow/interface/TensorFlow.h"
+
+class testLoading : public CppUnit::TestFixture
+{
+    CPPUNIT_TEST_SUITE(testLoading);
+    CPPUNIT_TEST(checkAll);
+    CPPUNIT_TEST_SUITE_END();
+
+public:
+    std::string dataPath;
+
+    void setUp();
+    void tearDown();
+    void checkAll();
+};
+
+CPPUNIT_TEST_SUITE_REGISTRATION(testLoading);
+
+void testLoading::setUp()
+{
+    dataPath = std::string(getenv("CMSSW_BASE")) + "/test/" + std::string(getenv("SCRAM_ARCH"))
+        + "/" + boost::filesystem::unique_path().string();
+
+    // create the graph
+    std::string testPath = std::string(getenv("CMSSW_BASE")) + "/src/PhysicsTools/TensorFlow/test";
+    std::string cmd = "python " + testPath + "/creategraph.py " + dataPath;
+    std::array<char, 128> buffer;
+    std::string result;
+    std::shared_ptr<FILE> pipe(popen(cmd.c_str(), "r"), pclose);
+    if (!pipe)
+    {
+        throw std::runtime_error("popen() failed!");
+    }
+    while (!feof(pipe.get()))
+    {
+        if (fgets(buffer.data(), 128, pipe.get()) != NULL)
+        {
+            result += buffer.data();
+        }
+    }
+    std::cout << std::endl
+              << result << std::endl;
+}
+
+void testLoading::tearDown()
+{
+    if (boost::filesystem::exists(dataPath))
+    {
+        boost::filesystem::remove_all(dataPath);
+    }
+}
+
+void testLoading::checkAll()
+{
+    std::string exportDir = dataPath + "/simplegraph";
+
+    // load the graph
+    tf::setLogging();
+    tf::MetaGraphDef* metaGraph = tf::loadMetaGraph(exportDir);
+    CPPUNIT_ASSERT(metaGraph != nullptr);
+
+    // create a new, empty session
+    tf::Session* session1 = tf::createSession();
+    CPPUNIT_ASSERT(session1 != nullptr);
+
+    // create a new session, using the meta graph
+    tf::Session* session2 = tf::createSession(metaGraph, exportDir);
+    CPPUNIT_ASSERT(session2 != nullptr);
+
+    // example evaluation
+    tf::Tensor input(tf::DT_FLOAT, { 1, 10 });
+    float* d = input.flat<float>().data();
+    for (size_t i = 0; i < 10; i++, d++)
+    {
+        *d = float(i);
+    }
+    tf::Tensor scale(tf::DT_FLOAT, {});
+    scale.scalar<float>()() = 1.0;
+
+    std::vector<tf::Tensor> outputs;
+    tf::Status status = session2->Run({ { "input", input }, { "scale", scale } }, { "output" },
+        {}, &outputs);
+    if (!status.ok())
+    {
+        std::cout << status.ToString() << std::endl;
+        CPPUNIT_ASSERT(false);
+    }
+
+    // check the output
+    CPPUNIT_ASSERT(outputs.size() == 1);
+    std::cout << outputs[0].DebugString() << std::endl;
+    CPPUNIT_ASSERT(outputs[0].matrix<float>()(0, 0) == 46.);
+
+    // run again using the convenience helper
+    outputs.clear();
+    tf::run(session2, { { "input", input }, { "scale", scale } }, { "output" }, &outputs);
+    CPPUNIT_ASSERT(outputs.size() == 1);
+    std::cout << outputs[0].DebugString() << std::endl;
+    CPPUNIT_ASSERT(outputs[0].matrix<float>()(0, 0) == 46.);
+
+    // check for exception
+    CPPUNIT_ASSERT_THROW(tf::run(session2, { { "foo", input } }, { "output" }, &outputs),
+        cms::Exception);
+
+    // cleanup
+    session1->Close();
+    session2->Close();
+    delete session1;
+    delete session2;
+    delete metaGraph;
+}
