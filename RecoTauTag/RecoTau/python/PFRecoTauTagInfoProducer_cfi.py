83f1b990d6ad6416678da7604a3ce09dbb12a7f2
Michal Bluj <michal.bluj@cern.ch>
2017-10-10 16:10:57 +0200

    Increase Gamma Et threshold for tau reco and id to 1GeV

--- RecoTauTag/RecoTau/python/PFRecoTauTagInfoProducer_cfi.py
+++ RecoTauTag/RecoTau/python/PFRecoTauTagInfoProducer_cfi.py
@@ -9,7 +9,7 @@ pfRecoTauTagInfoProducer = cms.EDProducer("PFRecoTauTagInfoProducer",
     ChargedHadrCand_tkminPt    = cms.double(0.5),  # charged PF objects
     tkminPt                    = cms.double(0.5),  # track (non-PF) objects
     NeutrHadrCand_HcalclusMinEt = cms.double(1.0),  # PF neutral hadrons (HCAL)
-    GammaCand_EcalclusMinEt     = cms.double(0.5),  # PF gamma candidates (ECAL)
+    GammaCand_EcalclusMinEt     = cms.double(1.0),  # PF gamma candidates (ECAL)
 
     # The size of the delta R cone used to collect objects from the jet
     ChargedHadrCand_AssociationCone   = cms.double(0.8),
