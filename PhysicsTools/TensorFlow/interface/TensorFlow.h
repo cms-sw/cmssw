522b609bd250291df2650b0ce290a78661d4ab2b
Marcel Rieger <marcel.rieger@cern.ch>
2017-10-11 01:27:00 +0200

    Refactor TF interface, use C++ API.

--- PhysicsTools/TensorFlow/interface/TensorFlow.h
+++ PhysicsTools/TensorFlow/interface/TensorFlow.h
@@ -1,17 +1,77 @@
 /*
- * TensorFlow interface header.
- * Based on TensorFlow C API 1.1.
+ * TensorFlow interface helpers.
+ * Based on TensorFlow C++ API 1.3.
  * For more info, see https://gitlab.cern.ch/mrieger/CMSSW-DNN.
  *
  * Author: Marcel Rieger
  */
 
-#ifndef PHYSICSTOOLS_TENSORFLOW_H
-#define PHYSICSTOOLS_TENSORFLOW_H
+#ifndef PHYSICSTOOLS_TENSORFLOW_TENSORFLOW_H
+#define PHYSICSTOOLS_TENSORFLOW_TENSORFLOW_H
 
-#include "PhysicsTools/TensorFlow/interface/Session.h"
-#include "PhysicsTools/TensorFlow/interface/Graph.h"
-#include "PhysicsTools/TensorFlow/interface/Tensor.h"
-#include "PhysicsTools/TensorFlow/interface/IO.h"
+#include "tensorflow/core/public/session.h"
+#include "tensorflow/core/framework/tensor.h"
+#include "tensorflow/cc/saved_model/loader.h"
+#include "tensorflow/cc/saved_model/tag_constants.h"
+#include "tensorflow/cc/saved_model/constants.h"
+#include "tensorflow/core/lib/io/path.h"
+#include "tensorflow/core/util/tensor_bundle/naming.h"
 
-#endif // PHYSICSTOOLS_TENSORFLOW_H
+#include "FWCore/Utilities/interface/Exception.h"
+
+namespace tf
+{
+
+using namespace tensorflow;
+
+typedef std::pair<std::string, Tensor> NamedTensor;
+typedef std::vector<NamedTensor> NamedTensorList;
+
+// set the tensorflow log level
+void setLogging(const std::string& level = "3");
+
+// updates the config of sessionOptions so that it uses a single thread
+void setThreading(SessionOptions& sessionOptions, int nThreads);
+
+// loads a meta graph definition saved at exportDir using the SavedModel interface for a tag
+// transfers ownership
+MetaGraphDef* loadMetaGraph(const std::string& exportDir, bool multiThreaded = false,
+    const std::string& tag = kSavedModelTagServe);
+
+// return a new, empty session
+// transfers ownership
+Session* createSession(bool multiThreaded = false);
+
+// return a new session that contains an already loaded meta graph whose exportDir must be given in
+// order to load and initialize the variables within the session
+// transfers ownership
+Session* createSession(MetaGraphDef* metaGraph, const std::string& exportDir,
+    bool multiThreaded = false);
+
+// run the session with inputs, outputNames and targetNodes, and store output tensors
+// throws a cms exception when not successful
+void run(Session* session, const NamedTensorList& inputs,
+    const std::vector<std::string>& outputNames, const std::vector<std::string>& targetNodes,
+    std::vector<Tensor>* outputs);
+
+// run the session with inputNames, inputTensors, outputNames and targetNodes, and store output
+// tensors
+// throws a cms exception when not successful
+void run(Session* session, const std::vector<std::string>& inputNames,
+    const std::vector<Tensor>& inputTensors, const std::vector<std::string>& outputNames,
+    const std::vector<std::string>& targetNodes, std::vector<Tensor>* outputs);
+
+// run the session with inputs and outputNames, and store output tensors
+// throws a cms exception when not successful
+void run(Session* session, const NamedTensorList& inputs,
+    const std::vector<std::string>& outputNames, std::vector<Tensor>* outputs);
+
+// run the session with inputNames, inputTensors and outputNames, and store output tensors
+// throws a cms exception when not successful
+void run(Session* session, const std::vector<std::string>& inputNames,
+    const std::vector<Tensor>& inputTensors, const std::vector<std::string>& outputNames,
+    std::vector<Tensor>* outputs);
+
+} // namespace tf
+
+#endif // PHYSICSTOOLS_TENSORFLOW_TENSORFLOW_H
