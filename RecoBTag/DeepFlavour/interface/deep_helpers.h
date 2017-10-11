e44c95a7c19792c0e0107ca5d0fe6f1b6c8e8dfc
Pablo de Castro <pablodecm@gmail.com>
2017-10-11 10:06:37 +0200

    improve and add comments in helpers

--- RecoBTag/DeepFlavour/interface/deep_helpers.h
+++ RecoBTag/DeepFlavour/interface/deep_helpers.h
@@ -9,10 +9,10 @@
 
 namespace btagbtvdeep {
 
-  // adapted from DeepNTuples
-  inline const float& catch_infs(const float& in,
-                                 const float& replace_value) {
-    if(in==in){ // why is this ?
+  // remove infs and NaNs with value  (adapted from DeepNTuples)
+  inline const float catch_infs(const float in,
+                                 const float replace_value) {
+    if(in==in){ // check if NaN 
       if(std::isinf(in))
         return replace_value;
       else if(in < -1e32 || in > 1e32)
@@ -22,13 +22,13 @@ namespace btagbtvdeep {
     return replace_value;
   } 
 
-  // adapted from DeepNTuples
-  inline const float catch_infs_and_bound(const float& in,
-                                          const float& replace_value,
-                                          const float& lowerbound,
-                                          const float& upperbound,
+  // remove infs/NaN and bound (adapted from DeepNTuples)
+  inline const float catch_infs_and_bound(const float in,
+                                          const float replace_value,
+                                          const float lowerbound,
+                                          const float upperbound,
                                           const float offset=0.,
-                                          const bool& use_offsets = true){
+                                          const bool use_offsets = true){
         float withoutinfs=catch_infs(in,replace_value);
         if(withoutinfs+offset<lowerbound) return lowerbound;
         if(withoutinfs+offset>upperbound) return upperbound;
@@ -38,7 +38,7 @@ namespace btagbtvdeep {
   }
 
 
-  // adapted from DeepNTuples 
+  // 2D distance between SV and PV (adapted from DeepNTuples)
   Measurement1D vertexDxy(const reco::VertexCompositePtrCandidate &svcand, const reco::Vertex &pv)  {
     VertexDistanceXY dist;
     reco::Vertex::CovarianceMatrix csv; svcand.fillVertexCovariance(csv);
@@ -46,7 +46,7 @@ namespace btagbtvdeep {
     return dist.distance(svtx, pv);
   }
 
-  // adapted from DeepNTuples 
+  //3D distance between SV and PV (adapted from DeepNTuples)
   Measurement1D vertexD3d(const reco::VertexCompositePtrCandidate &svcand, const reco::Vertex &pv)  {
     VertexDistance3D dist;
     reco::Vertex::CovarianceMatrix csv; svcand.fillVertexCovariance(csv);
@@ -54,42 +54,44 @@ namespace btagbtvdeep {
     return dist.distance(svtx, pv);
   }
 
-  // adapted from DeepNTuples 
+  // dot product between SV and PV (adapted from DeepNTuples)
   float vertexDdotP(const reco::VertexCompositePtrCandidate &sv, const reco::Vertex &pv)  {
     reco::Candidate::Vector p = sv.momentum();
     reco::Candidate::Vector d(sv.vx() - pv.x(), sv.vy() - pv.y(), sv.vz() - pv.z());
     return p.Unit().Dot(d.Unit());
   }
 
-  // adapted from DeepNTuples 
+  // helper to order vertices by significance (adapted from DeepNTuples)
   template < typename SVType, typename PVType>
   bool sv_vertex_comparator(const SVType & sva, const SVType & svb, const PVType & pv) {
-    float adxy= vertexDxy(sva,pv).value();
-    float bdxy= vertexDxy(svb,pv).value();
-    float aerr= vertexDxy(sva,pv).error();
-    float berr= vertexDxy(svb,pv).error();
+    auto adxy = vertexDxy(sva,pv); 
+    auto bdxy = vertexDxy(svb,pv); 
+    float aval= adxy.value();
+    float bval= bdxy.value();
+    float aerr= adxy.error();
+    float berr= bdxy.error();
 
-    float asig= catch_infs(adxy/aerr,0.);
-    float bsig= catch_infs(bdxy/berr,0.);
+    float asig= catch_infs(aval/aerr,0.);
+    float bsig= catch_infs(bval/berr,0.);
     return bsig<asig;
   }
 
-  // adapted from DeepNTuples 
+  // write tagging variables to vector (adapted from DeepNTuples)
   template <typename T>
   int dump_vector(reco::TaggingVariableList& from, T* to,
-                  reco::btau::TaggingVariableName name, const size_t& max) {
+                  reco::btau::TaggingVariableName name, const size_t max) {
     std::vector<T> vals = from.getList(name ,false);
     size_t size=std::min(vals.size(),max);
     if(size > 0){
       for(size_t i=0;i<vals.size();i++){
-        to[i]=catch_infs(vals.at(i),-0.1);
+        to[i]=catch_infs(vals[i],-0.1);
       }
     }
     return size;
   }
 
-  // adapted from DeepNtuples (now polymorphic)
-  float mindrsvpfcand(const std::vector<reco::VertexCompositePtrCandidate> svs, 
+  // compute minimum dr between SVs and a candidate (from DeepNTuples, now polymorphic)
+  float mindrsvpfcand(const std::vector<reco::VertexCompositePtrCandidate> & svs, 
                       const reco::Candidate* cand, float mindr=0.4) {
 
     for (unsigned int i0=0; i0<svs.size(); ++i0) {
