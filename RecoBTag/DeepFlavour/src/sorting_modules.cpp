a0ae8b845585839898a1748f05652acd1bebd105
Pablo de Castro <pablodecm@gmail.com>
2017-10-11 14:25:26 +0200

    remove unused functions and refactor sorting modules

--- RecoBTag/DeepFlavour/src/sorting_modules.cpp
+++ RecoBTag/DeepFlavour/src/sorting_modules.cpp
@@ -3,20 +3,16 @@
 
 #include "../interface/sorting_modules.h"
 #include <iostream>
-namespace sorting{
+namespace btagbtvdeep{
 
-std::vector<std::size_t> invertSortingVector(const std::vector<sortingClass<std::size_t> > & in){
+std::vector<std::size_t> invertSortingVector(const std::vector<SortingClass<std::size_t> > & in){
     std::size_t max=0;
     for(const auto& s:in){
         if(s.get()>max)max=s.get();
     }
 
     if(max>1e3){
-        for(const auto& s:in){
-          std::cout << s.get() << std::endl;
-        }
         throw std::runtime_error("sorting vector size more than 1k ");
-        
     }
     std::vector<std::size_t> out(max+1,0);
     for(std::size_t i=0;i<in.size();i++){
