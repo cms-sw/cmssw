f7ccf6cb4619dba89f55a947234f4463e601e8a6
Gregor Mittag <Gregor.Mittag@cern.ch>
2017-10-12 13:47:54 +0200

    Clean up 'mps_splice.py'.

--- Alignment/MillePedeAlignmentAlgorithm/scripts/mps_splice.py
+++ Alignment/MillePedeAlignmentAlgorithm/scripts/mps_splice.py
@@ -7,8 +7,8 @@ import fileinput
 # Set up argrument parser
 helpEpilog = ''''''
 
-parser = argparse.ArgumentParser(description='Take card file, blank all INFI directives and insert the INFI directives from the modifier file instead.', 
-                                 epilog=helpEpilog, 
+parser = argparse.ArgumentParser(description='Take card file, blank all INFI directives and insert the INFI directives from the modifier file instead.',
+                                 epilog=helpEpilog,
                                  formatter_class=argparse.RawDescriptionHelpFormatter)
 
 parser.add_argument('inCfg', action='store',
@@ -30,24 +30,24 @@ isn    = args.isn
 
 # open input file
 with open(inCfg, 'r') as INFILE:
-	body = INFILE.read()
+    body = INFILE.read()
 
 # read modifier file
 with open(modCfg, 'r') as MODFILE:
-	mods = MODFILE.read()
+    mods = MODFILE.read()
 mods = mods.strip()
 
 # prepare the new fileNames directive. Delete first line if necessary.
 fileNames = mods.split('\n')
 if 'CastorPool=' in fileNames[0]:
-	del fileNames[0]
+    del fileNames[0]
 
 # replace ISN number (input is a string of three digit number with leading zeros though)
 body = re.sub(re.compile('ISN',re.M), isn, body)
 
 # print to outCfg
 with open(outCfg, 'w') as OUTFILE:
-	OUTFILE.write(body)
+    OUTFILE.write(body)
 
 # Number of total files and number of extends for cms.vstring are needed
 numberOfFiles   = len(fileNames)
@@ -55,28 +55,20 @@ numberOfExtends = int(math.ceil(numberOfFiles/255.))
 
 # Create and insert the readFile.extend lines
 for j in xrange(numberOfExtends):
-	insertBlock = "readFiles.extend([\n    "	
-	i=0
-	currentStart = j*255
-	while (i<255) and ((currentStart+i)<numberOfFiles):
-		entry = fileNames[currentStart+i].strip()
-		if (i==254) or ((currentStart+i+1)==numberOfFiles):
-		    insertBlock += "\'"+entry+"\'])\n"
-		else:
-			insertBlock += "\'"+entry+"\',\n    "
-		i+=1
-	
-	for line in fileinput.input(outCfg, inplace=1):
-		if re.match('readFiles\s*=\s*cms.untracked.vstring()',line):
-			print line,
-			print insertBlock,
-		else:
-			print line,
+    insertBlock = "readFiles.extend([\n    "
+    i=0
+    currentStart = j*255
+    while (i<255) and ((currentStart+i)<numberOfFiles):
+        entry = fileNames[currentStart+i].strip()
+        if (i==254) or ((currentStart+i+1)==numberOfFiles):
+            insertBlock += "\'"+entry+"\'])\n"
+        else:
+            insertBlock += "\'"+entry+"\',\n    "
+        i+=1
 
-
-
-
-
-
-
-
+    for line in fileinput.input(outCfg, inplace=1):
+        if re.match('readFiles\s*=\s*cms.untracked.vstring()',line):
+            print line,
+            print insertBlock,
+        else:
+            print line,
