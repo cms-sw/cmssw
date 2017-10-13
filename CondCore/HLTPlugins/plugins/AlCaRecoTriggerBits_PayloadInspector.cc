7675571ee5013eddc2aa50a8798bdccee0dc0051
mmusich <marco.musich@cern.ch>
2017-10-13 18:22:19 +0200

    more graphics tuning

--- CondCore/HLTPlugins/plugins/AlCaRecoTriggerBits_PayloadInspector.cc
+++ CondCore/HLTPlugins/plugins/AlCaRecoTriggerBits_PayloadInspector.cc
@@ -27,15 +27,12 @@ namespace {
 
       std::string IOVsince  = std::to_string(std::get<0>(iov));
 
-      
       // Get map of strings to concatenated list of names of HLT paths:
       typedef std::map<std::string, std::string> TriggerMap;
       const TriggerMap &triggerMap = payload->m_alcarecoToTrig;
 
       unsigned int mapsize = triggerMap.size();
       float pitch = 1./(mapsize*1.20);
-
-      //std::cout<<"map size:" << mapsize << " pitch:" << pitch << "ratio: " << 1000./mapsize << std::endl;
 
       TCanvas canvas("AlCaRecoTriggerBits","AlCaRecoTriggerBits",2000,40*mapsize); 
 
@@ -140,10 +137,11 @@ namespace {
       std::set_difference(last_keys.begin(),last_keys.end(),first_keys.begin(),first_keys.end(), 
 			  std::inserter(not_in_first_keys, not_in_first_keys.begin()));
 
-      TCanvas canvas1("AlCaRecoTriggerBits0","AlCaRecoTriggerBits0",2000,1000); 
+      TCanvas canvas("AlCaRecoTriggerBits","AlCaRecoTriggerBits",2500,std::max(f_mapsize,l_mapsize)*40); 
+      float pitch = 0.013;
 
       TLatex l;
-      l.SetTextSize(0.018);
+      l.SetTextSize(0.012);
 
       // Draw the columns titles
       l.SetTextAlign(12);
@@ -151,33 +149,44 @@ namespace {
       float y, x1, x2, x3;
       y  = 1.0; 
       x1 = 0.02; 
-      x2 = x1+0.15; 
-      x3 = x2+0.35;
+      x2 = x1+0.20; 
+      x3 = x2+0.30;
 
-      y -= 0.017; 
+      y -= pitch; 
       l.DrawLatexNDC(x1, y,"#scale[1.2]{Key}"); 
       l.DrawLatexNDC(x2, y,("#scale[1.2]{in IOV: "+firstIOVsince+"}").c_str()); 
       l.DrawLatexNDC(x3, y,("#scale[1.2]{in IOV: "+lastIOVsince+"}").c_str()); 
-      y -= 0.005;
+      y -= pitch/3;
 
       // print the ones missing in the last key
       for(const auto& key : not_in_last_keys ) {
 	//std::cout<< key ;
-	y -= 0.017; l.DrawLatexNDC(x1, y,key.c_str()); 
+	y -= pitch; l.DrawLatexNDC(x1, y,key.c_str()); 
 	const std::vector<std::string> missing_in_last_paths = first_payload->decompose(first_triggerMap.at(key));
 	
 	std::map<int,std::string> output;
 	int count=0;
 	for (unsigned int iPath = 0; iPath < missing_in_last_paths.size(); ++iPath) {
 	  //std::cout << missing_in_last_paths[iPath] << " ; " ;
+
+	  if((output[count]+missing_in_last_paths[iPath]).length()<60){
+	    output[count]+=missing_in_last_paths[iPath];
+	    output[count]+=";";
+	  } else {
+	    count++;
+	    output[count]+=missing_in_last_paths[iPath];
+	  }
+
+	  /* old logic
 	  output[count]+=missing_in_last_paths[iPath];
 	  output[count]+=";";
-	  if(output[count].length()>60) count++;
+	  if(output[count].length()>30) count++;
+	  */
 	}
 	
 	for (unsigned int br=0; br<output.size();br++){
 	  l.DrawLatexNDC(x2,y,("#color[2]{"+output[br]+"}").c_str());  
-	  if(br!=output.size()-1) y -=0.017;
+	  if(br!=output.size()-1) y -=pitch;
 	}
 	//std::cout << " |||||| not in last";
 	//std::cout << std::endl;
@@ -191,7 +200,7 @@ namespace {
       // print the ones missing in the first key
       for(const auto& key : not_in_first_keys ) {
 	//std::cout<< key ;
-	y -= 0.017; l.DrawLatexNDC(x1, y,key.c_str()); 
+	y -= pitch; l.DrawLatexNDC(x1, y,key.c_str()); 
 	const std::vector<std::string> missing_in_first_paths = last_payload->decompose(last_triggerMap.at(key));
 
 	//std::cout << " not in first ||||||";
@@ -199,14 +208,25 @@ namespace {
 	int count=0;
 	for (unsigned int iPath = 0; iPath < missing_in_first_paths.size(); ++iPath) {
 	  //std::cout << missing_in_first_paths[iPath] << " ; " ;
+
+	  if((output[count]+missing_in_first_paths[iPath]).length()<60){
+	    output[count]+=missing_in_first_paths[iPath];
+	    output[count]+=";";
+	  } else {
+	    count++;
+	    output[count]+=missing_in_first_paths[iPath];
+	  }
+
+	  /* old logic
 	  output[count]+=missing_in_first_paths[iPath];
 	  output[count]+=";";
-	  if(output[count].length()>60) count++;
+	  if(output[count].length()>30) count++;
+	  */
 	}
 
 	for (unsigned int br=0; br<output.size();br++){
 	  l.DrawLatexNDC(x3,y,("#color[4]{"+output[br]+"}").c_str());  	    
-	  if(br!=output.size()-1) y -= 0.017;
+	  if(br!=output.size()-1) y -= pitch;
 	}
 	//std::cout << std::endl;
 	
@@ -237,19 +257,30 @@ namespace {
       	  if(not_in_last.size()!=0 || not_in_first.size()!=0) {
 
       	    //std::cout<< element.first << " : "  ;
-      	    y -= 0.017; l.DrawLatexNDC(x1, y, element.first.c_str()); 
+      	    y -= pitch; l.DrawLatexNDC(x1, y, element.first.c_str()); 
 	    
       	    std::map<int,std::string> output; 
       	    int count(0);
       	    for (unsigned int iPath = 0; iPath < not_in_last.size(); ++iPath) {
       	      //std::cout << not_in_last[iPath] << " ; " ;
+	      
+	      if((output[count]+not_in_last[iPath]).length()<60){
+		output[count]+=not_in_last[iPath];
+		output[count]+=";";
+	      } else {
+		count++;
+		output[count]+=not_in_last[iPath];
+	      }
+
+	      /* old logic
       	      output[count]+= not_in_last[iPath];
       	      output[count]+="; ";
       	      if(output[count].length()>60) count++;
+	      */
       	    }
 
       	    for (unsigned int br=0; br<output.size();br++){
-      	      l.DrawLatexNDC(x2,y-(br*0.017),("#color[6]{"+output[br]+"}").c_str());
+      	      l.DrawLatexNDC(x2,y-(br*pitch),("#color[6]{"+output[br]+"}").c_str());
       	    }  
       	    //std::cout << " ||||||";
 	    
@@ -257,22 +288,31 @@ namespace {
       	    int count1=0;
       	    for (unsigned int jPath = 0; jPath < not_in_first.size(); ++jPath) {
       	      //std::cout << not_in_first[jPath] << " ; " ;
+
+	      if((output[count]+not_in_first[jPath]).length()<60){
+		output[count]+=not_in_first[jPath];
+		output[count]+=";";
+	      } else {
+		count++;
+		output[count]+=not_in_first[jPath];
+	      }
+
+	      /* old logic
       	      output[count1]+= not_in_first[jPath];
       	      output[count1]+=";";
       	      if(output[count1].length()>60) count1++;
+	      */
       	    }
 
       	    for (unsigned int br=0; br<output.size();br++){
-      	      l.DrawLatexNDC(x3,y-(br*0.017),("#color[8]{"+output[br]+"}").c_str());  	    
+      	      l.DrawLatexNDC(x3,y-(br*pitch),("#color[8]{"+output[br]+"}").c_str());  	    
       	    }
 
-	    y-=std::max(count,count1)*0.017;
+	    // decrease the y position to the maximum of the two lists
+	    y-=std::max(count,count1)*pitch;
       	    TLine *line3 = new TLine(gPad->GetUxmin(),y-0.008,gPad->GetUxmax(),y-0.008);
       	    line3->Draw();
 	    
-	    // decrease the y position to the maximum of the two lists
-
-
       	    //std::cout << std::endl;
  
       	  } // close if there is at least a difference 
@@ -282,7 +322,7 @@ namespace {
 
       //canvas.SetCanvasSize(2000,(1-y)*1000);
       std::string fileName(m_imageFileName);
-      canvas1.SaveAs(fileName.c_str());
+      canvas.SaveAs(fileName.c_str());
       return true;
     }
   };
