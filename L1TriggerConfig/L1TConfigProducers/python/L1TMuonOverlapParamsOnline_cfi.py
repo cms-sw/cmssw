f8b74c6b27fd68c74fb98833170bc10fe83c1b5e
kkotov <kkotov@cern.ch>
2017-10-13 12:57:46 +0200

    Adding a transactionSafe parameter to the configs

--- L1TriggerConfig/L1TConfigProducers/python/L1TMuonOverlapParamsOnline_cfi.py
+++ L1TriggerConfig/L1TConfigProducers/python/L1TMuonOverlapParamsOnline_cfi.py
@@ -2,6 +2,7 @@ import FWCore.ParameterSet.Config as cms
 
 L1TMuonOverlapParamsOnlineProd = cms.ESProducer("L1TMuonOverlapParamsOnlineProd",
     onlineAuthentication = cms.string('.'),
-    forceGeneration = cms.bool(False),
-    onlineDB = cms.string('oracle://CMS_OMDS_LB/CMS_TRG_R')
+    forceGeneration      = cms.bool(False),
+    onlineDB             = cms.string('oracle://CMS_OMDS_LB/CMS_TRG_R'),
+    transactionSafe      = cms.bool(True) # nothrow guarantee if set to False: carry on no matter what
 )
