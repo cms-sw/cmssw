8b2098d8c9ab586ec030c0b58569f27ae667850e
Dmitrijus Bugelskis <dmitrijus.bugelskis@cern.ch>
2017-10-18 21:31:25 +0200

    Introduce DQM memory usage script
    Due to *popular* demand, this adds dqmMemoryStats.py script.
    
    This won't draw a chart or anything, just a simple text breakdown of
    histograms in DQM.
    
    Usage:
    
    `dqmMemoryStats.py -x -i step3_inDQM.root` will output the memory usage
    for that file. It will output: `Total bytes: 251.78 MiB`.
    
    `--summary` Will also output a summary table will output biggest offenders.
    
    ```
    dqmMemoryStats.py -x -i step3_inDQM.root --summary
    
    ************************* DQM level 2 folder breakdown *************************
    * Size cutoff: 1.00 MiB                                                        *
    ********************************************************************************
    31.92 MiB        TrackerHitsV/TrackerHit
    12.91 MiB        Muons/RecoMuonV
    12.81 MiB        Vertexing/PrimaryVertexV
    11.40 MiB        HLT/JME
    ...
    ```
    
    Setting `-r/--ref <file>` flag will compare two files.
    
    ```
    dqmMemoryStats.py -x -i step3_inDQM.root -r DQM_*_RECO.root --summary
    
    ************************* DQM level 1 folder breakdown *************************
    ********************************************************************************
    -2721.23 KiB     EcalBarrel
    -1364.68 KiB     EcalEndcap
    -1255.52 KiB     HLT
    ...
    ```
    
    Both `-i` and `-r` can be either DQM IO or DQM root files or any
    combination of those. The script autodetects format.

---
+++ DQMServices/FileIO/python/DQM.py
@@ -0,0 +1,106 @@
+#!/bin/env python
+
+import ROOT as R
+import os, re
+
+class DQMReader(object):
+    """
+    Reader for  DQM IO and DQM root files.
+    """
+
+    #defined DQMIO types, index is important!
+    DQMIO_TYPES = ["Ints","Floats","Strings",
+         "TH1Fs","TH1Ss","TH1Ds",
+         "TH2Fs", "TH2Ss", "TH2Ds",
+         "TH3Fs", "TProfiles","TProfile2Ds", "kNIndicies"]
+
+    def __init__(self, input_filename):
+        self._root_file = R.TFile.Open(input_filename)
+
+        ioTest = self._root_file.Get("Indices")
+        if bool(ioTest):
+            self.type = "DQMIO"
+        else:
+            self.type = "ROOT"
+
+    def read_objects(self):
+        if (self.type == "DQMIO"):
+            return self.read_objects_dqmio()
+        else:
+            return self.read_objects_root()
+
+    def read_objects_dqmio(self):
+        indices = self._root_file.Get("Indices")
+
+        for y in xrange(indices.GetEntries()):
+            indices.GetEntry(y)
+            # print indices.Run, indices.Lumi, indices.Type
+
+            object_type = self.DQMIO_TYPES[indices.Type]
+            t_tree = self._root_file.Get(object_type)
+
+            for i in xrange(indices.FirstIndex, indices.LastIndex + 1):
+                t_tree.GetEntry(i)
+
+                fullname = str(t_tree.FullName)
+                yield (fullname, t_tree.Value, )
+
+    def read_objects_root(self):
+        xml_re = re.compile(r"^<(.+)>(.+)=(.+)<\/\1>$")
+        def parse_directory(di):
+            directory = self._root_file.GetDirectory(di)
+            for key in directory.GetListOfKeys():
+                entry = key.GetName()
+                rtype = key.GetClassName()
+                fullpath = "%s/%s" % (di, entry)
+
+                if (rtype == "TDirectoryFile"):
+                    for k, v in parse_directory(fullpath):
+                        yield (k, v, )
+                else:
+                    obj = self._root_file.Get(fullpath)
+                    if obj:
+                        yield (fullpath, obj, )
+                    else:
+                        # special case to parse the xml abomination
+                        m = xml_re.search(entry)
+                        if m:
+                            name = m.group(1)
+                            typecode = m.group(2)
+                            value = m.group(3)
+
+                            fp = "%s/%s" % (di, name)
+                            yield (fp, value, )
+                        else:
+                            raise Exception("Invalid xml:" + entry)
+
+
+        path_fix = re.compile(r"^\/Run \d+")
+        for fullname, obj in parse_directory(""):
+            f = fullname.replace("/DQMData", "")
+            f = f.replace("/Run summary", "")
+            f = path_fix.sub(r"", f)
+            if f[0] == "/":
+                f = f[1:]
+
+            yield f, obj
+
+    def close(self):
+        self._root_file.Close()
+
+if __name__ == '__main__':
+    import argparse
+
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-i", "--input", help = "Input DQMIO ROOT file")
+    args = parser.parse_args()
+
+    reader = DQMReader(args.input)
+
+    for (fn, v) in reader.read_objects():
+        if (hasattr(v, "ClassName")):
+            print fn, v.ClassName()
+        else:
+            print fn, type(v)
+
+    reader.close()
