cab16da93ac8b7d0df707aa41f00e7a3ae9798cb
Heshy Roskes <jroskes1@jhu.edu>
2017-10-18 22:33:55 +0200

    first quick implementation to write validation and hippy dataset files

---
+++ Alignment/CommonAlignment/python/tools/utilities.py
@@ -0,0 +1,13 @@
+def cache(function):
+    cache = {}
+    def newfunction(*args, **kwargs):
+        try:
+            return cache[args, tuple(sorted(kwargs.iteritems()))]
+        except TypeError:
+            print args, tuple(sorted(kwargs.iteritems()))
+            raise
+        except KeyError:
+            cache[args, tuple(sorted(kwargs.iteritems()))] = function(*args, **kwargs)
+            return newfunction(*args, **kwargs)
+    newfunction.__name__ = function.__name__
+    return newfunction
