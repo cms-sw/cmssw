e486fd1598e45967c608fb5db36fd23db6c70064
Mia <mia.tosi@cern.ch>
2017-10-19 13:08:54 +0200

    adjust hit efficiency vs PU plots binning for muon->innerTrack

--- DQM/TrackingMonitor/python/MonitorTrackInnerTrackMuons_cff.py
+++ DQM/TrackingMonitor/python/MonitorTrackInnerTrackMuons_cff.py
@@ -25,6 +25,9 @@ muonInnerTrack.TkSizeBin = 10
 muonInnerTrack.TkSizeMax = 10.
 muonInnerTrack.phiErrMax = 0.001
 muonInnerTrack.etaErrMax = 0.001
+muonInnerTrack.PVBin =  40
+muonInnerTrack.PVMin =   0.5
+muonInnerTrack.PVMax = 120.5
 
 import DQM.TrackingMonitor.TrackingMonitor_cfi
 MonitorTrackMuonsInnerTrack = DQM.TrackingMonitor.TrackingMonitor_cfi.TrackMon.clone()
