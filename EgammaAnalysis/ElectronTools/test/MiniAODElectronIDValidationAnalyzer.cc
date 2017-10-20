ef38594f7b43f133c012f061d30023fea942149d
Vincenzo Innocente <vincenzo.innocente@cern.ch>
2017-10-20 16:15:47 +0200

    two more migration to Lost

--- EgammaAnalysis/ElectronTools/test/MiniAODElectronIDValidationAnalyzer.cc
+++ EgammaAnalysis/ElectronTools/test/MiniAODElectronIDValidationAnalyzer.cc
@@ -239,7 +239,7 @@ MiniAODElectronIDValidationAnalyzer::analyze(const edm::Event& iEvent, const edm
     // Conversion rejection
     constexpr reco::HitPattern::HitCategory missingHitType =
       reco::HitPattern::MISSING_INNER_HITS;
-    expectedMissingInnerHits_ = el->gsfTrack()->hitPattern().numberOfHits(missingHitType);
+    expectedMissingInnerHits_ = el->gsfTrack()->hitPattern().numberOfLostHits(missingHitType);
     passConversionVeto_ = false;
     if( thebs.isValid() && convs.isValid() ) {
       passConversionVeto_ = !ConversionTools::hasMatchedConversion(*el,convs,
