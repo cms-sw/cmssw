519363bbd8158e585006fee99f78c51108bb0cfa
Vladimir <vladimir.rekovic@cern.ch>
2017-10-20 12:53:12 +0200

    Add omtf unpacker to the L1T unpacker sequence. Fix DQM configuration for ignoreBins parametar.

--- DQM/L1TMonitor/python/L1TdeStage2OMTF_cfi.py
+++ DQM/L1TMonitor/python/L1TdeStage2OMTF_cfi.py
@@ -1,4 +1,6 @@
 import FWCore.ParameterSet.Config as cms
+# List of bins to ignore
+ignoreBinsDeStage2Omtf = [1, 14]
 
 # compares the unpacked OMTF regional muon collection to the emulated OMTF regional muon collection
 # only muons that do not match are filled in the histograms
@@ -11,7 +13,7 @@ l1tdeStage2Omtf = cms.EDAnalyzer(
     regionalMuonCollection2Title = cms.untracked.string("OMTF emulator"),
     summaryTitle = cms.untracked.string("Summary of comparison between OMTF muons and OMTF emulator muons"),
     ignoreBadTrackAddress = cms.untracked.bool(True),
-    ignoreBin = cms.untracked.vint32(ignoreBins),
+    ignoreBin = cms.untracked.vint32(ignoreBinsDeStage2Omtf),
     verbose = cms.untracked.bool(False),
 )
 
