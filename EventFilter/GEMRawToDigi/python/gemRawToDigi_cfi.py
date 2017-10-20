45ad3f6a911fc7c3635dd901d4fb3f05ee2c229c
Jason Lee <jason.lee@cern.ch>
2017-10-20 15:38:57 +0900

    cleanup

--- EventFilter/GEMRawToDigi/python/gemRawToDigi_cfi.py
+++ EventFilter/GEMRawToDigi/python/gemRawToDigi_cfi.py
@@ -1,5 +1,5 @@
 import FWCore.ParameterSet.Config as cms
 
-gemRawToDigi = cms.EDProducer("GEMRawToDigiModule",
+gemDigis = cms.EDProducer("GEMRawToDigiModule",
     InputObjects = cms.InputTag("rawDataCollector"),
 )
