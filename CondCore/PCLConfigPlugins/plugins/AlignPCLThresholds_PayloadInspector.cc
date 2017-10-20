0ea9ae3009d7c37556413b37a25a685951880776
mmusich <marco.musich@cern.ch>
2017-10-20 13:39:04 +0200

    simplify &cleanup

--- CondCore/PCLConfigPlugins/plugins/AlignPCLThresholds_PayloadInspector.cc
+++ CondCore/PCLConfigPlugins/plugins/AlignPCLThresholds_PayloadInspector.cc
@@ -13,12 +13,10 @@
 // the data format of the condition to be inspected
 #include "CondFormats/PCLConfig/interface/AlignPCLThresholds.h"
 
-// auxilliary functions
-
 #include <memory>
 #include <sstream>
 #include <iostream>
-#include <regex>
+#include <functional>
 
 // include ROOT 
 #include "TH2F.h"
@@ -26,11 +24,14 @@
 #include "TCanvas.h"
 #include "TLine.h"
 #include "TStyle.h"
-#include "TLatex.h"
-#include "TPave.h"
-#include "TPaveStats.h"
 
 namespace {
+
+  enum types {DELTA,
+	      SIG,
+	      MAXMOVE,
+	      MAXERR,
+	      END_OF_TYPES};
   
   /************************************************
     Display of AlignPCLThresholds
@@ -44,49 +45,58 @@ namespace {
     bool fill( const std::vector<std::tuple<cond::Time_t,cond::Hash> >& iovs ) override{
       auto iov = iovs.front();
       std::shared_ptr<AlignPCLThresholds> payload = fetchPayload( std::get<1>(iov) );
-      AlignPCLThresholds::threshold_map m_thresholds = payload->getThreshold_Map();
+      auto alignables = payload->getAlignableList();
       
-      TCanvas canvas("Alignment PCL thresholds summary","Alignment PCL thresholds summary",1000,1000); 
+      TCanvas canvas("Alignment PCL thresholds summary","Alignment PCL thresholds summary",1500,800); 
       canvas.cd();
 
       canvas.SetTopMargin(0.07);
-      canvas.SetBottomMargin(0.25);
-      canvas.SetLeftMargin(0.18);
+      canvas.SetBottomMargin(0.06);
+      canvas.SetLeftMargin(0.11);
       canvas.SetRightMargin(0.05);
       canvas.Modified();
       canvas.SetGrid();
 
-      auto Thresholds = std::unique_ptr<TH2F>(new TH2F("Thresholds","Alignment parameter thresholds",m_thresholds.size(),0,m_thresholds.size(),24,0,24));
+      auto Thresholds = std::unique_ptr<TH2F>(new TH2F("Thresholds","Alignment parameter thresholds",alignables.size(),0,alignables.size(),24,0,24));
       Thresholds->SetStats(false);
+      
+      std::function<float(types,std::string,AlignPCLThresholds::coordType)> cutFunctor = [&payload](types my_type,std::string alignable,AlignPCLThresholds::coordType coord) {	
+	float ret(-999.);
+	switch(my_type){
+	case DELTA   : return payload->getCut(alignable,coord);
+	case SIG     : return payload->getSigCut(alignable,coord);
+	case MAXMOVE : return payload->getMaxMoveCut(alignable,coord);
+	case MAXERR  : return payload->getMaxErrorCut(alignable,coord);
+	case END_OF_TYPES : return ret;
+	default : return ret;
+	}
+      };
 
-      std::array<std::string,24> ylabels = {{"X","sig X","maxMove X","Error X","Y","sig Y","maxMove Y","Error Y","Z","sig Z","maxMove Z", "Error Z","#theta_{X}","sig #theta_{X}","maxMove #theta_{X}","Error #theta_{X}","#theta_{Y}","sig #theta_{Y}","maxMove #theta_{Y}","Error #theta_{Y}","#theta_{Z}","sig #theta_{Z}","maxMove #theta_{Z}","Error #theta_{Z}"}};
-
-      unsigned int xBin=0;
-      for(const auto &entry : m_thresholds){
+      unsigned int xBin=0;	    
+      for(const auto &alignable : alignables){
 	xBin++;
- 	Thresholds->GetXaxis()->SetBinLabel(xBin,(entry.first).c_str());
 
-	// fill the labels on y-axis
-	unsigned int yBin=25;
-	for (const std::string &text : ylabels ){
-	  yBin--;
-	  if(xBin==1){
-	    Thresholds->GetYaxis()->SetBinLabel(yBin,text.c_str());
-	  }
-	}
-
+	auto xLabel = replaceAll(replaceAll(alignable,"minus","(-)"),"plus","(+)");
+ 	Thresholds->GetXaxis()->SetBinLabel(xBin,(xLabel).c_str());
+	unsigned int yBin=24;
 	for(int foo = AlignPCLThresholds::X; foo != AlignPCLThresholds::extra_DOF; foo++ ){
-	  AlignPCLThresholds::coordType type = static_cast<AlignPCLThresholds::coordType>(foo);  
+	  AlignPCLThresholds::coordType coord = static_cast<AlignPCLThresholds::coordType>(foo);  
+	  for(int bar = types::DELTA; bar != types::END_OF_TYPES; bar++ ){
+	    types type = static_cast<types>(bar);
+	    std::string theLabel = getStringFromTypeEnum(type)+getStringFromCoordEnum(coord);
+	    if(xBin==1){
+	      Thresholds->GetYaxis()->SetBinLabel(yBin,theLabel.c_str());
+	    }
+ 
+	    Thresholds->SetBinContent(xBin,yBin,cutFunctor(type,alignable,coord)); 
 
-	  Thresholds->SetBinContent(xBin,24-(foo*4),payload->getCut(entry.first,type));
-	  Thresholds->SetBinContent(xBin,24-((foo*4)+1),payload->getSigCut(entry.first,type));
-	  Thresholds->SetBinContent(xBin,24-((foo*4)+2),payload->getMaxMoveCut(entry.first,type));
-	  Thresholds->SetBinContent(xBin,24-((foo*4)+3),payload->getMaxErrorCut(entry.first,type));
-	}
-	
-      }
+	    yBin--;
+	  }// loop on types
+	}// loop on coordinates
+      } // loop on alignables
 
-      Thresholds->GetXaxis()->LabelsOption("v");
+
+      Thresholds->GetXaxis()->LabelsOption("h");
       Thresholds->Draw("TEXT");
       
       std::string fileName(m_imageFileName);
@@ -95,18 +105,44 @@ namespace {
       return true;
     }
     
-    std::string getStringFromCoordEnum (const AlignPCLThresholds::coordType &type){
-      switch(type){
+    /************************************************/
+    std::string getStringFromCoordEnum (const AlignPCLThresholds::coordType &coord){
+      switch(coord){
       case AlignPCLThresholds::X : return "X";
       case AlignPCLThresholds::Y : return "Y";
-      case AlignPCLThresholds::Z : return "Y";
-      case AlignPCLThresholds::theta_X : return "theta_X";
-      case AlignPCLThresholds::theta_Y : return "theta_Y";
-      case AlignPCLThresholds::theta_Z : return "theta_Z";
+      case AlignPCLThresholds::Z : return "Z";
+      case AlignPCLThresholds::theta_X : return "#theta_{X}";
+      case AlignPCLThresholds::theta_Y : return "#theta_{Y}";
+      case AlignPCLThresholds::theta_Z : return "#theta_{Z}";
       default : return "should never be here";
       }
     }
-    
+
+    /************************************************/
+    std::string getStringFromTypeEnum (const types &type){
+      switch(type){
+      case types::DELTA   : return "#Delta";
+      case types::SIG     : return "#Delta/#sigma ";
+      case types::MAXMOVE : return "max. move ";
+      case types::MAXERR  : return "max. err ";
+      default: return "should never be here";
+      }
+    }
+
+    /************************************************/
+    std::string replaceAll(const std::string& str, const std::string& from, const std::string& to) {
+      std::string out(str);
+  
+      if(from.empty())
+        return out;
+      size_t start_pos = 0;
+      while((start_pos = out.find(from, start_pos)) != std::string::npos) {
+        out.replace(start_pos, from.length(), to);
+        start_pos += to.length(); // In case 'to' contains 'from', like replacing 'x' with 'yx'
+      }
+      return out;
+    }
+   
   };
 } // close namespace
 
