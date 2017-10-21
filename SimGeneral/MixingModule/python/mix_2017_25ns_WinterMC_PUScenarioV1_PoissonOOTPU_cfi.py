d4f15664f881eb79818f158dca1d97e546aa2008
David Lange <David.Lange@cern.ch>
2017-10-21 19:20:38 +0200

    Update mix_2017_25ns_WinterMC_PUScenarioV1_PoissonOOTPU_cfi.py
    use probFunction_25ns_PoissonOOTPU_cfi as basis instead of repeating lots of config

--- SimGeneral/MixingModule/python/mix_2017_25ns_WinterMC_PUScenarioV1_PoissonOOTPU_cfi.py
+++ SimGeneral/MixingModule/python/mix_2017_25ns_WinterMC_PUScenarioV1_PoissonOOTPU_cfi.py
@@ -1,28 +1,15 @@
 import FWCore.ParameterSet.Config as cms
-
-# configuration to model pileup for initial physics phase
-from SimGeneral.MixingModule.mixObjects_cfi import theMixObjects
-from SimGeneral.MixingModule.mixPoolSource_cfi import *
-from SimGeneral.MixingModule.digitizers_cfi import *
-
-mix = cms.EDProducer("MixingModule",
-    digitizers = cms.PSet(theDigitizers),
-    LabelPlayback = cms.string(''),
-    maxBunch = cms.int32(3),
-    minBunch = cms.int32(-12), ## in terms of 25 nsec
-
-    bunchspace = cms.int32(25), ##ns
-    mixProdStep1 = cms.bool(False),
-    mixProdStep2 = cms.bool(False),
-
-    playback = cms.untracked.bool(False),
-    useCurrentProcessOnly = cms.bool(False),
-
-    input = cms.SecSource("EmbeddedRootSource",
-        type = cms.string('probFunction'),
-        nbPileupEvents = cms.PSet(
-          probFunctionVariable = cms.vint32(1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32, 33, 34, 35, 36, 37, 38, 39, 40, 41, 42, 43, 44, 45, 46, 47, 48, 49, 50, 51, 52, 53, 54, 55, 56, 57, 58, 59, 60, 61, 62, 63, 64, 65, 66, 67, 68, 69, 70, 71, 72, 73, 74, 75, 76, 77, 78, 79, 80, 81, 82, 83, 84, 85, 86, 87, 88, 89, 90, 91, 92, 93, 94, 95, 96, 97, 98, 99),
-	        probValue = cms.vdouble(
+from SimGeneral.MixingModule.mix_probFunction_25ns_PoissonOOTPU_cfi import *
+mix.input.nbPileupEvents.probFunctionVariable = cms.vint32(1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 
+							   15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 
+							   27, 28, 29, 30, 31, 32, 33, 34, 35, 36, 37, 38, 
+							   39, 40, 41, 42, 43, 44, 45, 46, 47, 48, 49, 50, 
+							   51, 52, 53, 54, 55, 56, 57, 58, 59, 60, 61, 62, 
+							   63, 64, 65, 66, 67, 68, 69, 70, 71, 72, 73, 74, 
+							   75, 76, 77, 78, 79, 80, 81, 82, 83, 84, 85, 86, 
+							   87, 88, 89, 90, 91, 92, 93, 94, 95, 96, 97, 98, 
+							   99)
+mix.input.nbPileupEvents.probValue = cms.vdouble(
               3.60995527865e-05,
               6.99001006951e-06,
               1.94611883714e-05,
@@ -121,21 +108,4 @@ mix = cms.EDProducer("MixingModule",
               7.49676241012e-10,
               4.68678999934e-10,
               2.92830678318e-10,
-              1.82815648903e-10),
-          histoFileName = cms.untracked.string('histProbFunction.root'),
-        ),
-	sequential = cms.untracked.bool(False),
-        manage_OOT = cms.untracked.bool(True),  ## manage out-of-time pileup
-        ## setting this to True means that the out-of-time pileup
-        ## will have a different distribution than in-time, given
-        ## by what is described on the next line:
-        OOT_type = cms.untracked.string('Poisson'),  ## generate OOT with a Poisson matching the number chosen for in-time
-        #OOT_type = cms.untracked.string('fixed'),  ## generate OOT with a fixed distribution
-        #intFixed_OOT = cms.untracked.int32(2),
-        fileNames = FileNames
-    ),
-    mixObjects = cms.PSet(theMixObjects)
-)
-
-
-
+              1.82815648903e-10)
