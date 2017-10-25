7d38e46ae4192b75fa024405248d013dd7ea7eac
Kaya Tatar <tatark@mit.edu>
2017-10-25 02:25:39 +0200

    rename HI-specific islandSuperClusters modules to remove the "same name" ambiguity with the modules under RecoEcal/EgammaClusterProducers/

--- RecoHI/HiEgammaAlgos/python/HiEgamma_cff.py
+++ RecoHI/HiEgammaAlgos/python/HiEgamma_cff.py
@@ -11,15 +11,15 @@ from RecoEcal.EgammaClusterProducers.particleFlowSuperClusteringSequence_cff imp
 
 particleFlowSuperClusterECAL.regressionConfig.vertexCollection = 'hiSelectedVertex'
 
-hiEcalClusteringSequence = cms.Sequence(islandClusteringSequence*hybridClusteringSequence*multi5x5ClusteringSequence*multi5x5PreshowerClusteringSequence*preshowerClusteringSequence*particleFlowSuperClusteringSequence)
+hiEcalClusteringSequence = cms.Sequence(hiIslandClusteringSequence*hybridClusteringSequence*multi5x5ClusteringSequence*multi5x5PreshowerClusteringSequence*preshowerClusteringSequence*particleFlowSuperClusteringSequence)
 
 
 # reco photon producer
 from RecoEgamma.EgammaPhotonProducers.photonSequence_cff import *
 
 # use island for the moment
-photonCore.scHybridBarrelProducer = cms.InputTag("correctedIslandBarrelSuperClusters")
-photonCore.scIslandEndcapProducer = cms.InputTag("correctedIslandEndcapSuperClusters")
+photonCore.scHybridBarrelProducer = cms.InputTag("hiCorrectedIslandBarrelSuperClusters")
+photonCore.scIslandEndcapProducer = cms.InputTag("hiCorrectedIslandEndcapSuperClusters")
 photonCore.minSCEt = cms.double(8.0)
 photons.minSCEtBarrel = cms.double(5.0)
 photons.minSCEtEndcap = cms.double(15.0)
--- RecoHI/HiEgammaAlgos/python/hiSpikeCleaner_cfi.py
+++ RecoHI/HiEgammaAlgos/python/hiSpikeCleaner_cfi.py
@@ -3,7 +3,7 @@ import FWCore.ParameterSet.Config as cms
 hiSpikeCleaner = cms.EDProducer("HiSpikeCleaner",
                                 recHitProducerBarrel = cms.InputTag("ecalRecHit","EcalRecHitsEB"),
                                 recHitProducerEndcap = cms.InputTag("ecalRecHit","EcalRecHitsEE"),
-                                originalSuperClusterProducer = cms.InputTag("correctedIslandBarrelSuperClusters"),
+                                originalSuperClusterProducer = cms.InputTag("hiCorrectedIslandBarrelSuperClusters"),
                                 outputColl  = cms.string( "" ),
                                 etCut          = cms.double(10),
                                 TimingCut    = cms.untracked.double(4.0),
