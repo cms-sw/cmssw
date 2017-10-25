42d501c99dd004abe93bdf8736ff23613c9c317e
George Stephans <gsfs@mit.edu>
2017-10-25 19:59:27 +0200

    Standardized code

--- IOMC/EventVertexGenerators/python/VtxSmearedRealistic5TeVppCollision2017_cfi.py
+++ IOMC/EventVertexGenerators/python/VtxSmearedRealistic5TeVppCollision2017_cfi.py
@@ -1,6 +1,6 @@
 import FWCore.ParameterSet.Config as cms
 
-from IOMC.EventVertexGenerators.VtxSmearedParameters_cfi import *
+from IOMC.EventVertexGenerators.VtxSmearedParameters_cfi import Realistic5TeVppCollision2017VtxSmearingParameters,VtxSmearedCommon
 VtxSmeared = cms.EDProducer("BetafuncEvtVtxGenerator",
     Realistic5TeVppCollision2017VtxSmearingParameters,
     VtxSmearedCommon
