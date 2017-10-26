3356502da987408c067cf6d15f1d165884f69f28
Nick Smith <nick.smith@cern.ch>
2017-10-26 22:11:19 +0200

    code checks

--- RecoEgamma/EgammaTools/plugins/HGCalPhotonIDValueMapProducer.cc
+++ RecoEgamma/EgammaTools/plugins/HGCalPhotonIDValueMapProducer.cc
@@ -41,14 +41,14 @@
 class HGCalPhotonIDValueMapProducer : public edm::stream::EDProducer<> {
   public:
     explicit HGCalPhotonIDValueMapProducer(const edm::ParameterSet&);
-    ~HGCalPhotonIDValueMapProducer();
+    ~HGCalPhotonIDValueMapProducer() override;
 
     static void fillDescriptions(edm::ConfigurationDescriptions& descriptions);
 
   private:
-    virtual void beginStream(edm::StreamID) override;
-    virtual void produce(edm::Event&, const edm::EventSetup&) override;
-    virtual void endStream() override;
+    void beginStream(edm::StreamID) override;
+    void produce(edm::Event&, const edm::EventSetup&) override;
+    void endStream() override;
 
     // ----------member data ---------------------------
     edm::EDGetTokenT<edm::View<reco::Photon>> photonsToken_;
