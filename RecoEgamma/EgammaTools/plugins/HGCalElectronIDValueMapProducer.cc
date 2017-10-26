845544ce39cce7c4025333963c0e02410ad5fd06
Nick Smith <nick.smith@cern.ch>
2017-10-26 22:11:19 +0200

    code checks

--- RecoEgamma/EgammaTools/plugins/HGCalElectronIDValueMapProducer.cc
+++ RecoEgamma/EgammaTools/plugins/HGCalElectronIDValueMapProducer.cc
@@ -42,14 +42,14 @@
 class HGCalElectronIDValueMapProducer : public edm::stream::EDProducer<> {
   public:
     explicit HGCalElectronIDValueMapProducer(const edm::ParameterSet&);
-    ~HGCalElectronIDValueMapProducer();
+    ~HGCalElectronIDValueMapProducer() override;
 
     static void fillDescriptions(edm::ConfigurationDescriptions& descriptions);
 
   private:
-    virtual void beginStream(edm::StreamID) override;
-    virtual void produce(edm::Event&, const edm::EventSetup&) override;
-    virtual void endStream() override;
+    void beginStream(edm::StreamID) override;
+    void produce(edm::Event&, const edm::EventSetup&) override;
+    void endStream() override;
 
     // ----------member data ---------------------------
     edm::EDGetTokenT<edm::View<reco::GsfElectron>> ElectronsToken_;
