3efd478538d6b06dcc08d8451d74b80a722253b1
Gaelle <Gaelle.Boudoul@cern.ch>
2017-10-26 17:35:46 +0200

    removing comments

--- Configuration/PyReleaseValidation/python/upgradeWorkflowComponents.py
+++ Configuration/PyReleaseValidation/python/upgradeWorkflowComponents.py
@@ -37,7 +37,7 @@ numWFStart={
 }
 numWFSkip=200
 # temporary measure to keep other WF numbers the same
-numWFConflict = [[11000,11200],[11400,11600],[11800,12000],[12200,12400],[25000,26000],[50000,51000]]    ### HAAAAA LOOK HERE
+numWFConflict = [[11000,11200],[11400,11600],[11800,12000],[12200,12400],[25000,26000],[50000,51000]]
 numWFAll={
     2017: [],
     2023: []
