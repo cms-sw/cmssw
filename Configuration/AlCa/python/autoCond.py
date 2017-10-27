eeef459386b34822344631142b2baa145435e0ae
arun <arun.kumar@cern.ch>
2017-10-28 01:32:31 +0200

    prompt GT updated with revert of AlCaReco trigger bits

--- Configuration/AlCa/python/autoCond.py
+++ Configuration/AlCa/python/autoCond.py
@@ -28,7 +28,7 @@ autoCond = {
     # GlobalTag for Run2 data relvals: allows customization to run with fixed L1 menu
     'run2_data_relval'  :   '92X_dataRun2_relval_v8',
     # GlobalTag for Run2 data 2016H relvals only: Prompt Conditions + fixed L1 menu (to be removed)
-    'run2_data_promptlike' : '92X_dataRun2_PromptLike_v7',
+    'run2_data_promptlike' : '92X_dataRun2_PromptLike_v8',
 
     # GlobalTag for Run1 HLT: it points to the online GT
     'run1_hlt'          :   '92X_dataRun2_HLT_frozen_v7',
