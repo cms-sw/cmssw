3d32e91deda13ef2fdd206049a853b0486d2616e
Vincenzo Innocente <vincenzo.innocente@cern.ch>
2017-10-30 16:22:29 +0100

    fix error in trap

--- DataFormats/GeometrySurface/src/TrapezoidalPlaneBounds.cc
+++ DataFormats/GeometrySurface/src/TrapezoidalPlaneBounds.cc
@@ -47,7 +47,7 @@ bool TrapezoidalPlaneBounds::inside( const Local2DPoint& p, const LocalError& er
 float TrapezoidalPlaneBounds::howMuchInside(const Local3DPoint& p, const LocalError& err) const {
    return std::max( 
                    (std::abs(p.y())-hapothem)/std::sqrt(err.yy()),
-                   (std::abs(p.x())-tan_a*std::abs(p.y()+offset))/std::sqrt(err.yy())
+                   (std::abs(p.x())-tan_a*std::abs(p.y()+offset))/std::sqrt(err.xx())
                   );
 }
 
