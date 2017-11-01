56efbd88bcb7dcf70871ebcd51d858f3781f7763
Nick Smith <nick.smith@cern.ch>
2017-11-01 22:27:01 +0100

    Explain hgcal shower depth

--- RecoEgamma/EgammaTools/interface/ShowerDepth.h
+++ RecoEgamma/EgammaTools/interface/ShowerDepth.h
@@ -1,29 +1,57 @@
-#ifndef SHOWERDEPTH_H
-#define SHOWERDEPTH_H
+#ifndef RecoEgamma_EgammaTools_ShowerDepth_h
+#define RecoEgamma_EgammaTools_ShowerDepth_h
 
-// Code copied from C. Charlot's
+/* ShowerDepth: computes expected EM shower mean depth
+ * in the HGCal, and compares it to measured depth
+ *
+ * Code copied from C. Charlot's
+ * Hopefully correct explanations by N. Smith
+ *
+ * Based on gamma distribution description of electromagnetic cascades [PDG2016, ss33.5]
+ * Basic equation:
+ *   D_exp = X_0 t_max alpha / (alpha-1)
+ * where D_exp is expectedDepth, X_0 the radiation length in HGCal material, t_max
+ * the shower maximum and alpha a parameter of the gamma distribution. (beta := (a-1)/t_max)
+ * Both t_max and alpha are approximated as first order polynomials of ln y = E/E_c, where E_c
+ * is the critical energy, and presumably are extracted from fits to simulation, along with their
+ * corresponding uncertainties.
+ *
+ * sigma(D_exp) then follows from error propagation and we can compare to
+ * measured depth (distance between first hit and shower barycenter)
+ *
+ * The parameterization is for electrons, although photons will give similar results
+ */
 
 class ShowerDepth {
 public:
-    ShowerDepth();
-    ~ShowerDepth() {;}
-    float getClusterDepthCompatibility(float length,float emEnergy,float & expectedDepth, float & expectedSigma) const;
+    ShowerDepth() {}
+    ~ShowerDepth() {}
+
+    float getClusterDepthCompatibility(float measuredDepth, float emEnergy, float & expectedDepth, float & expectedSigma) const;
 
 private:
-    // longitudinal parametrisation
-    double criticalEnergy_;
-    double radiationLength_;
-    double meant0_;
-    double meant1_;
-    double meanalpha0_;
-    double meanalpha1_;
-    double sigmalnt0_;
-    double sigmalnt1_;
-    double sigmalnalpha0_;
-    double sigmalnalpha1_;
-    double corrlnalphalnt0_;
-    double corrlnalphalnt1_;
-    bool debug_;
+    // HGCAL average medium
+    static constexpr double criticalEnergy_ = 0.00536; // in GeV
+    static constexpr double radiationLength_ = 0.968; // in cm
+
+    // mean values
+    // shower max <t_max> = t0 + t1*lny
+    static constexpr double meant0_{ -1.396 };
+    static constexpr double meant1_{ 1.007 };
+    // <alpha> = alpha0 + alpha1*lny
+    static constexpr double meanalpha0_{ -0.0433 };
+    static constexpr double meanalpha1_{ 0.540 };
+    // sigmas (relative uncertainty)
+    // sigma(ln(t_max)) = 1 / (sigmalnt0 + sigmalnt1*lny);
+    static constexpr double sigmalnt0_{ -2.506 };
+    static constexpr double sigmalnt1_{ 1.245 };
+    // sigma(ln(alpha)) = 1 / (sigmalnt0 + sigmalnt1*lny);
+    static constexpr double sigmalnalpha0_{ -0.08442 };
+    static constexpr double sigmalnalpha1_{ 0.7904 };
+    // correlation coefficient
+    // corr(ln(alpha), ln(t_max)) = corrlnalpha0_+corrlnalphalnt1_*lny
+    static constexpr double corrlnalphalnt0_{ 0.7858 };
+    static constexpr double corrlnalphalnt1_{ -0.0232 };
 };
 
 #endif
