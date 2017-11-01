910f7b01a92372fe240c3e0ecfe05de7b9865014
Nick Smith <nick.smith@cern.ch>
2017-11-01 18:34:07 +0100

    more code review

--- RecoEgamma/EgammaTools/src/LongDeps.cc
+++ RecoEgamma/EgammaTools/src/LongDeps.cc
@@ -1,14 +1,18 @@
 #include "RecoEgamma/EgammaTools/interface/LongDeps.h"
 
 
-LongDeps::LongDeps(float radius, const std::vector<float>& energyPerLayer, float energyEE,float energyFH,float energyBH,const std::set<int> layers):
+
+LongDeps::LongDeps(float radius, const std::vector<float>& energyPerLayer, float energyEE,float energyFH,float energyBH,const std::set<int>& layers):
 energyPerLayer_(energyPerLayer),radius_(radius),energyEE_(energyEE),energyFH_(energyFH),
 energyBH_(energyBH),layers_(layers) {
     lay_Efrac10_ = 0;
     lay_Efrac90_ = 0;
     float lay_energy = 0;
-    for (unsigned lay = 1; lay < 52; ++lay) {
+    float e4 = 0.;
+    // NB: energyPerLayer_ is 1-indexed
+    for (unsigned lay = 1; lay < energyPerLayer_.size(); ++lay) {
         lay_energy += energyPerLayer_[lay];
+        if (lay<5) e4 += energyPerLayer_[lay];
         if (lay_Efrac10_ == 0 && lay_energy > 0.1 * energyEE_){
             lay_Efrac10_ = lay;
         }
@@ -16,7 +20,6 @@ energyBH_(energyBH),layers_(layers) {
             lay_Efrac90_ = lay;
         }
     }
-    float e4 = energyPerLayer_[1]+energyPerLayer_[2]+energyPerLayer_[3]+energyPerLayer_[4];
     float etot = energyEE_ + energyFH_ + energyBH_;
     e4oEtot_ =  (etot > 0.) ? e4/etot : -1. ;
 }
