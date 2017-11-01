c7a7bbc7a1115e66c89359164f46e8f9f2219b59
Christopher_Madrid <Chris_Madrid@Baylor.edu>
2017-11-01 03:33:16 -0500

    Changing the phase back to how it was: new shape 206

--- RecoLocalCalo/HcalRecProducers/python/HBHEMethod0Parameters_cfi.py
+++ RecoLocalCalo/HcalRecProducers/python/HBHEMethod0Parameters_cfi.py
@@ -5,10 +5,5 @@ m0Parameters = cms.PSet(
     firstSample = cms.int32(4),
     samplesToAdd = cms.int32(2),
     correctForPhaseContainment = cms.bool(True),
-
-    #HPD Setting
-    #correctionPhaseNS = cms.double(6.0),
-
-    #SiPM Setting
-    correctionPhaseNS = cms.double(-0.3),
+    correctionPhaseNS = cms.double(6.0),
 )
