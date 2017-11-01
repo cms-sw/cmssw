4e77f481fb2be6848312015f6f22b15f20a98ff5
Dinko Ferencek <Dinko.Ferencek@cern.ch>
2017-11-01 14:59:26 +0100

    added protection for invalid GhostTrackStates

--- RecoVertex/GhostTrackFitter/src/PositiveSideGhostTrackFitter.cc
+++ RecoVertex/GhostTrackFitter/src/PositiveSideGhostTrackFitter.cc
@@ -35,13 +35,15 @@ GhostTrackPrediction PositiveSideGhostTrackFitter::fit(
 			GhostTrackState testState = state;
 			testState.linearize(pred, 2. * origin - lambda);
 			double ndof, chi2;
-
-			updater.contribution(prior, testState, ndof, chi2, true);
-			if (ndof > 0. && chi2 < 10.) {
-				state = testState;
-				if (state.weight() != 1.)
-					state.setWeight(3.);
-				done = false;
+			
+			if (testState.isValid()) {
+				updater.contribution(prior, testState, ndof, chi2, true);
+				if (ndof > 0. && chi2 < 10.) {
+					state = testState;
+					if (state.weight() != 1.)
+						state.setWeight(3.);
+					done = false;
+				}
 			}
 		}
 	}
