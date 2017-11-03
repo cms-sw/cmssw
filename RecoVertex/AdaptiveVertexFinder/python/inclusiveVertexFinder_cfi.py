0d6df110c3b71be26c4ac5f6c5a2a8322bc5f1b7
Lindsey Gray <lindsey.gray@gmail.com>
2017-11-03 21:34:08 +0100

    fix typo in python file

--- RecoVertex/AdaptiveVertexFinder/python/inclusiveVertexFinder_cfi.py
+++ RecoVertex/AdaptiveVertexFinder/python/inclusiveVertexFinder_cfi.py
@@ -1,6 +1,6 @@
 import FWCore.ParameterSet.Config as cms
 
-from RecoVertex.AdaptiveVertexFinder.inclusiveVertexFinderDefault import inclusiveVertexFinderDefault
+from RecoVertex.AdaptiveVertexFinder.inclusiveVertexFinderDefault_cfi import inclusiveVertexFinderDefault
 
 inclusiveVertexFinder  = inclusiveVertexFinderDefault.clone()
 
