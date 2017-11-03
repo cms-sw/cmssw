e1591b59be81aeb8bf3479051708c2d8b7f05658
Jan Kaspar <jan.kaspar@cern.ch>
2017-11-03 20:42:34 +0100

    Added diagonal plots.

--- DQM/CTPPS/test/elastic_dqm_test_cfg.py
+++ DQM/CTPPS/test/elastic_dqm_test_cfg.py
@@ -5,8 +5,8 @@ process = cms.Process('RECODQM')
 # minimum of logs
 process.MessageLogger = cms.Service("MessageLogger",
   statistics = cms.untracked.vstring(),
-  destinations = cms.untracked.vstring('cerr'),
-  cerr = cms.untracked.PSet(
+  destinations = cms.untracked.vstring('cout'),
+  cout = cms.untracked.PSet(
       threshold = cms.untracked.string('WARNING')
   )
 )
@@ -28,7 +28,7 @@ process.source = cms.Source("PoolSource",
 )
 
 process.maxEvents = cms.untracked.PSet(
-  input = cms.untracked.int32(1000)
+  input = cms.untracked.int32(-1)
 )
 
 # needed for geometry declaration
