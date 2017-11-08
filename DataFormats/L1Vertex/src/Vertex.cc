b90b5bab7c4059ea87af89f068d3741c198a9316
Tom Williams <tom.williams@cern.ch>
2017-11-08 13:41:22 +0000

    Implemented first iteration of L1 vertex dataformat

---
+++ DataFormats/L1Vertex/src/Vertex.cc
@@ -0,0 +1,36 @@
+
+#include "DataFormats/L1Vertex/interface/Vertex.h"
+
+
+
+namespace l1t {
+
+Vertex::Vertex() :
+  z0_(0.0)
+{
+}
+
+Vertex::Vertex(float z0, const std::vector<edm::Ptr<Track_t>>& tracks) :
+  z0_(z0),
+  tracks_(tracks)
+{
+}
+
+
+Vertex::~Vertex()
+{
+}
+
+
+float Vertex::z0() const
+{
+  return z0_;
+}
+
+
+const std::vector<edm::Ptr<Vertex::Track_t>>& Vertex::tracks() const
+{
+  return tracks_;
+}
+
+}
