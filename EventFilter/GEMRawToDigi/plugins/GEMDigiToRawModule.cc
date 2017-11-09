4049ff1cc6f465e13a2b0ce92b735273f1d7872e
Jason Lee <jason.lee@cern.ch>
2017-11-10 05:32:21 +0900

    fixed dummy mapping for packer

--- EventFilter/GEMRawToDigi/plugins/GEMDigiToRawModule.cc
+++ EventFilter/GEMRawToDigi/plugins/GEMDigiToRawModule.cc
@@ -59,10 +59,122 @@ void GEMDigiToRawModule::produce( edm::Event & e, const edm::EventSetup& c )
   int ndigis = 0;
 
   std::vector<AMC13Event*> amc13Events;
-  
+
   // currently only one FEDRaw
   {
     AMC13Event * amc13Event = new AMC13Event();
+    
+    for (auto amc : m_gemROMap->getAMCs()){	
+      AMCdata * amcData = new AMCdata();
+      uint16_t amcId = amc;
+
+      for (auto geb : m_gemROMap->getAMC2GEBs(amcId)){
+	
+	uint16_t gebId = geb;
+	uint32_t chamberId = (amcId << 5) | gebId;
+	
+	GEMDetId chamDetId = m_gemROMap->gebPosition(chamberId);
+
+	GEBdata * gebData = new GEBdata();
+	gebData->setInputID(gebId);
+	
+	// 1 GEB per chamber
+	// making vfats
+	for (uint16_t bc = 0; bc < 50; ++bc){
+	  std::map<int, std::vector<int> > vFatToStripMap;
+	  bool hasDigi = false;
+	
+	  for (int roll = 1; roll<=8; ++roll){
+	  
+	    GEMDetId gemId(chamDetId.region(), 1, chamDetId.station(), chamDetId.layer(), chamDetId.chamber(), roll);
+	  
+	    GEMDigiCollection::Range range = gemDigis->get(gemId);
+	    for (GEMDigiCollection::const_iterator digiIt = range.first; digiIt!=range.second; ++digiIt){
+
+	      const GEMDigi & digi = (*digiIt);
+	      if (digi.bx() != bc-25) continue;
+	
+	      GEMROmap::dCoord dc;
+	      dc.gemDetId = gemId;
+	      dc.stripId = digi.strip();
+
+	      GEMROmap::eCoord ec = m_gemROMap->hitPosition(dc);
+	      uint32_t vFatID = ec.vfatId;
+	      int channelId = ec.channelId;
+		
+	      vFatToStripMap[vFatID].push_back(channelId);	
+	      hasDigi = true;
+
+	      std::cout <<"GEMDigiToRawModule vfatId "<<ec.vfatId
+			<<" gemDetId "<< gemId
+			<<" chan "<< ec.channelId
+			<<" strip "<< dc.stripId
+			<<" bx "<< digi.bx()
+			<<std::endl;
+	      ndigis++;
+
+	    }
+	  }
+	  
+	  if (!hasDigi) continue;
+	  
+	  // fill in vFat
+	  std::map<int, std::vector<int> >::const_iterator vFatStrIt = vFatToStripMap.begin();    
+	  for (; vFatStrIt != vFatToStripMap.end(); ++vFatStrIt) {
+
+	    if (vFatStrIt->second.size() == 0) continue;
+      
+	    uint8_t  b1010      =0xA;           ///<1010:4 Control bits, shoud be 1010
+	    uint16_t BC         =bc;             ///<Bunch Crossing number, 12 bits
+	    uint8_t  b1100      =0xC;           ///<1100:4, Control bits, shoud be 1100
+	    uint8_t  EC         =0;             ///<Event Counter, 8 bits
+	    uint8_t  Flag       =0;             ///<Control Flags: 4 bits, Hamming Error/AFULL/SEUlogic/SUEI2C
+	    uint8_t  b1110      =0xE;           ///<1110:4 Control bits, shoud be 1110
+	    //uint16_t crc        =0;             ///<Check Sum value, 16 bits
+	    uint16_t crc_calc   =0;             ///<Check Sum value recalculated, 16 bits
+	    int      SlotNumber =0;             ///<Calculated chip position
+	    bool     isBlockGood=1;             ///<Shows if block is good (control bits, chip ID and CRC checks)
+
+	    uint16_t ChipID = 0xFFF & vFatStrIt->first; ///<Chip ID, 12 bits
+	    uint64_t lsData     =0;             ///<channels from 1to64 
+	    uint64_t msData     =0;             ///<channels from 65to128
+	  
+	    for (auto chan : vFatStrIt->second){
+	      //std::cout <<"chan "<< chan<< std::endl;
+	      uint64_t oneBit = 0x1;
+	      if (chan < 64) lsData = lsData | (oneBit << chan);
+	      else msData = msData | (oneBit << (chan-64));
+	    }
+
+	    uint16_t crc = checkCRC(b1010, BC, b1100,
+				    EC, Flag, b1110,
+				    ChipID, msData, lsData);
+	    
+	    VFATdata * vfatData =
+	      new VFATdata(b1010, BC, b1100, EC, Flag, b1110, ChipID, lsData, msData,
+			   crc, crc_calc, SlotNumber, isBlockGood);
+	  
+	    gebData->v_add(*vfatData);
+	    delete vfatData;
+	  }
+	}
+	
+	if (gebData->vfats().size()){
+	  gebData->setInputID(gebId);
+	  gebData->setVwh(gebData->vfats().size()*3);
+	  amcData->g_add(*gebData);
+	}
+	delete gebData;	
+      }
+
+      if (amcData->gebs().size()){
+	amcData->setGDcount(amcData->gebs().size());
+	amcData->setBID(amcId);
+	amc13Event->addAMCpayload(*amcData);
+      }
+      delete amcData;
+    }
+
     // CDFHeader
     uint8_t cb5 = 0x5;// control bit, should be 0x5 bits 60-63
     uint8_t Evt_ty = event_type_;
@@ -73,7 +185,7 @@ void GEMDigiToRawModule::produce( edm::Event & e, const edm::EventSetup& c )
 
     // AMC13header
     uint8_t CalTyp = 1;
-    uint8_t nAMC = 1; // currently only one AMC13Event
+    uint8_t nAMC = amc13Event->getAMCpayload().size(); // currently only one AMC13Event
     uint32_t OrN = 2;
     uint8_t cb0  = 0b0000;// control bit, should be 0b0000
     amc13Event->setAMC13header(CalTyp, nAMC, OrN, cb0);
@@ -85,131 +197,12 @@ void GEMDigiToRawModule::produce( edm::Event & e, const edm::EventSetup& c )
       uint16_t BoardID = 0;
       amc13Event->addAMCheader(AMC_size, Blk_No, AMC_No, BoardID);
     }
-
-    // Now make AMC payloads
-    for (unsigned short i = 0; i < amc13Event->nAMC(); ++i){
-      uint8_t GDcount = 0; // count GEBs
-      AMCdata * amcData = new AMCdata();
-
     
-      for (int re = -1; re <= 1; re = re+2) {
-	for (int st = GEMDetId::minStationId; st<=GEMDetId::maxStationId; ++st) {
-	  for (int ch = GEMDetId::minChamberId; ch<=GEMDetId::maxChamberId; ++ch) {
-
-	    // 1 GEB per superChamber
-	    GEMDetId gebDetId(re, 1, st, 0, ch, 0);
-	  
-	    std::map<int, std::vector<int> > vFatToStripMap;
-	    bool hasDigi = false;
-	    for (int ly = 1; ly<=GEMDetId::maxLayerId; ++ly) {
-	      for (int roll = 1; roll<=GEMDetId::maxRollId; ++roll) {
-		GEMDetId gemId(re, 1, st, ly, ch, roll);
-	      
-		GEMDigiCollection::Range range = gemDigis->get(gemId);
-		for (GEMDigiCollection::const_iterator digiIt = range.first; digiIt!=range.second; ++digiIt){
-		  const GEMDigi & digi = (*digiIt);
-		
-		  GEMROmap::dCoord dc;
-		  dc.gemDetId = gemId;
-		  dc.stripId = digi.strip();
-
-		  GEMROmap::eCoord ec = m_gemROMap->hitPosition(dc);
-		  int vFatID = ec.vfatId;
-		  int channelId = ec.channelId;
-		
-		  vFatToStripMap[vFatID].push_back(channelId);	
-		  hasDigi = true;
-
-		  std::cout <<"GEMDigiToRawModule ChipID "<<ec.vfatId
-			    <<" gemDetId "<< gemId
-			    <<" chan "<< ec.channelId
-			    <<" strip "<< dc.stripId
-			    <<std::endl;
-		  ndigis++;
-
-		}
-	      }
-	    }
-	    if (!hasDigi) continue;
-	  
-	    // count no. of superChambers with hits
-	    ++GDcount;
-	    uint16_t Vwh = 0;
-	    // const uint32_t &ZeroSup = 0;
-	    // const uint8_t &InputID = 0;
-	    // const uint16_t &ErrorC = 0;
-	    // const uint16_t &OHCRC = 0;
-	    // const uint16_t &Vwt = 0;
-	    // const uint8_t &InFu = 0;
-	    // const uint8_t &Stuckd = 0;
-	    // GEBdata * gebData = new GEBdata(ZeroSup, InputID, Vwt, ErrorC,
-	    // 				  OHCRC, Vwt, InFu, Stuckd);
-	    GEBdata * gebData = new GEBdata();
-	    int gebID = m_gemROMap->gebPosition(gebDetId.superChamberId());
-	    gebData->setInputID(gebID);
-
-	    // fill in vFat
-	    std::map<int, std::vector<int> >::const_iterator vFatStrIt = vFatToStripMap.begin();    
-	    for (; vFatStrIt != vFatToStripMap.end(); ++vFatStrIt) {
-
-	      if (vFatStrIt->second.size() == 0) continue;
-      
-	      uint8_t  b1010      =0xA;           ///<1010:4 Control bits, shoud be 1010
-	      uint16_t BC         =0;             ///<Bunch Crossing number, 12 bits
-	      uint8_t  b1100      =0xC;           ///<1100:4, Control bits, shoud be 1100
-	      uint8_t  EC         =0;             ///<Event Counter, 8 bits
-	      uint8_t  Flag       =0;             ///<Control Flags: 4 bits, Hamming Error/AFULL/SEUlogic/SUEI2C
-	      uint8_t  b1110      =0xE;           ///<1110:4 Control bits, shoud be 1110
-	      //uint16_t crc        =0;             ///<Check Sum value, 16 bits
-	      uint16_t crc_calc   =0;             ///<Check Sum value recalculated, 16 bits
-	      int      SlotNumber =0;             ///<Calculated chip position
-	      bool     isBlockGood=1;             ///<Shows if block is good (control bits, chip ID and CRC checks)
-
-	      uint16_t ChipID = 0xFFF & vFatStrIt->first; ///<Chip ID, 12 bits
-	      uint64_t lsData     =0;             ///<channels from 1to64 
-	      uint64_t msData     =0;             ///<channels from 65to128
-	    
-	      for (auto chan : vFatStrIt->second){
-		//std::cout <<"chan "<< chan<< std::endl;
-		uint64_t oneBit = 0x1;
-		if (chan < 64) lsData = lsData | (oneBit << chan);
-		else msData = msData | (oneBit << (chan-64));
-	      }
-
-	      uint16_t crc = checkCRC(b1010, BC, b1100,
-				      EC, Flag, b1110,
-				      ChipID, msData, lsData);
-	    
-	      VFATdata * vfatData =
-		new VFATdata(b1010, BC, b1100, EC, Flag, b1110, ChipID, lsData, msData,
-			     crc, crc_calc, SlotNumber, isBlockGood);
-
-	      ++Vwh;
-	      ++Vwh;
-	      ++Vwh;
-	      gebData->v_add(*vfatData);
-	      delete vfatData;
-	    }
-
-	    gebData->setVwh(Vwh);
-	    amcData->g_add(*gebData);
-	    delete gebData;
-	  
-	  }
-	}
-      }
-    
-      amcData->setGDcount(GDcount);    
-      amc13Event->addAMCpayload(*amcData);
-      delete amcData;
-
-    }
-  
     //AMC13 trailer
     uint32_t CRC_amc13 = 0;
     uint8_t Blk_NoT = 0;
     uint8_t LV1_idT = 0;
-    uint16_t BX_idT = 0;
+    uint16_t BX_idT = BX_id;
     amc13Event->setAMC13trailer(CRC_amc13, Blk_NoT, LV1_idT, BX_idT);
     //CDF trailer
     uint8_t cbA = 0xA; // control bit, should be 0xA bits 60-63
