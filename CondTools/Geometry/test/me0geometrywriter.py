52898f5392e964a86262042ec15521af7c1371ae
Jason Lee <jason.lee@cern.ch>
2017-11-15 03:42:06 +0900

    add in super chamber and chamber dim

--- CondTools/Geometry/test/me0geometrywriter.py
+++ CondTools/Geometry/test/me0geometrywriter.py
@@ -1,8 +1,8 @@
 import FWCore.ParameterSet.Config as cms
 
-process = cms.Process("ME0GeometryWriter")
+process = cms.Process("GEMGeometryWriter")
 process.load('CondCore.CondDB.CondDB_cfi')
-process.load('Configuration.Geometry.GeometryExtended2023D3_cff')
+process.load('Configuration.Geometry.GeometryExtended2023D20_cff')
 process.load('Geometry.MuonNumbering.muonNumberingInitialization_cfi')
 
 process.source = cms.Source("EmptyIOVSource",
@@ -12,18 +12,22 @@ process.source = cms.Source("EmptyIOVSource",
                             interval = cms.uint64(1)
                             )
 
+process.GEMGeometryWriter = cms.EDAnalyzer("GEMRecoIdealDBLoader")
 process.ME0GeometryWriter = cms.EDAnalyzer("ME0RecoIdealDBLoader")
 
 process.CondDB.timetype = cms.untracked.string('runnumber')
 process.CondDB.connect = cms.string('sqlite_file:myfile.db')
 process.PoolDBOutputService = cms.Service("PoolDBOutputService",
                                           process.CondDB,
-                                          toPut = cms.VPSet(cms.PSet(record = cms.string('ME0RecoGeometryRcd'),tag = cms.string('ME0RECO_Geometry_Test01'))
-                                                            )
+                                          toPut = cms.VPSet(cms.PSet(record = cms.string('GEMRecoGeometryRcd'),
+                                                                        tag = cms.string('GEMRECO_Geometry_TagXX')),
+                                                            cms.PSet(record = cms.string('ME0RecoGeometryRcd'),
+                                                                        tag = cms.string('ME0RECO_Geometry_TagXX'))
+                                                            ),
                                           )
 
 process.maxEvents = cms.untracked.PSet(
     input = cms.untracked.int32(1)
     )
 
-process.p1 = cms.Path(process.ME0GeometryWriter)
+process.p1 = cms.Path(process.GEMGeometryWriter+process.ME0GeometryWriter)
--- Geometry/GEMGeometryBuilder/src/ME0GeometryParsFromDD.cc
+++ Geometry/GEMGeometryBuilder/src/ME0GeometryParsFromDD.cc
@@ -8,75 +8,226 @@
 #include "Geometry/MuonNumbering/interface/MuonBaseNumber.h"
 #include "Geometry/MuonNumbering/interface/ME0NumberingScheme.h"
 #include "DataFormats/GeometryVector/interface/Basic3DVector.h"
+#include "CLHEP/Units/GlobalSystemOfUnits.h"
+
+#include <boost/lexical_cast.hpp>
 
 void
 ME0GeometryParsFromDD::build( const DDCompactView* cview, 
 			      const MuonDDDConstants& muonConstants,
 			      RecoIdealGeometry& rgeo )
 {
-  std::string attribute = "ReadOutName";
-  std::string value     = "MuonME0Hits";
+  std::string attribute = "MuStructure";
+  std::string value     = "MuonEndCapME0";
 
   // Asking only for the MuonME0's
   DDSpecificsMatchesValueFilter filter{DDValue(attribute, value, 0.0)};
-  DDFilteredView fview( *cview, filter );
+  DDFilteredView fview(*cview,filter);
 
   this->buildGeometry( fview, muonConstants, rgeo );
 }
 
 void
-ME0GeometryParsFromDD::buildGeometry( DDFilteredView& fview,
-				      const MuonDDDConstants& muonConstants,
-				      RecoIdealGeometry& rgeo )
+ME0GeometryParsFromDD::buildGeometry(DDFilteredView& fv,
+				     const MuonDDDConstants& muonConstants,
+				     RecoIdealGeometry& rgeo)
+{  
+  LogDebug("ME0GeometryParsFromDD") <<"Building the geometry service";
+  LogDebug("ME0GeometryParsFromDD") << "About to run through the ME0 structure\n" 
+				    <<" First logical part "
+				    <<fv.logicalPart().name().name(); 
+  
+  bool doChambers = fv.firstChild();
+  LogDebug("ME0GeometryParsFromDD") << "doSuperChamber = " << doChambers;
+  // loop over superchambers
+  while (doChambers){
+
+    // getting chamber id from eta partitions
+    fv.firstChild();fv.firstChild();
+    MuonDDDNumbering mdddnumCh(muonConstants);
+    ME0NumberingScheme me0NumCh(muonConstants);
+    int rawidCh = me0NumCh.baseNumberToUnitNumber(mdddnumCh.geoHistoryToBaseNumber(fv.geoHistory()));
+    ME0DetId detIdCh = ME0DetId(rawidCh);
+    // back to chambers
+    fv.parent();fv.parent();
+
+    buildChamber(fv, detIdCh, rgeo);
+
+    // loop over chambers
+    // only 1 chamber
+    bool doLayers = fv.firstChild();
+    while (doLayers){
+      
+
+      fv.firstChild();
+      MuonDDDNumbering mdddnum(muonConstants);
+      ME0NumberingScheme me0Num(muonConstants);
+      int rawId = me0Num.baseNumberToUnitNumber(mdddnum.geoHistoryToBaseNumber(fv.geoHistory()));
+      ME0DetId detId = ME0DetId(rawId);
+      ME0DetId detIdLa = detId.layerId();
+      fv.parent();
+
+      // build layer
+      buildLayer(fv, detIdLa, rgeo);
+      
+      // loop over ME0EtaPartitions
+      bool doEtaPart = fv.firstChild();
+      while (doEtaPart){
+
+	MuonDDDNumbering mdddnum(muonConstants);
+	ME0NumberingScheme me0Num(muonConstants);
+	int rawid = me0Num.baseNumberToUnitNumber(mdddnum.geoHistoryToBaseNumber(fv.geoHistory()));
+	ME0DetId detId = ME0DetId(rawid);
+
+	buildEtaPartition(fv, detId, rgeo);
+	
+	doEtaPart = fv.nextSibling();
+      }
+      fv.parent();
+      doLayers = fv.nextSibling();
+    }
+    fv.parent();
+    doChambers = fv.nextSibling();
+  }  
+}
+
+void
+ME0GeometryParsFromDD::buildChamber(DDFilteredView& fv, ME0DetId detId, RecoIdealGeometry& rgeo)
 {
-  bool doSubDets = fview.firstChild();
+  LogDebug("ME0GeometryParsFromDD") << "buildChamber "<<fv.logicalPart().name().name()
+				    <<" "<< detId <<std::endl;
+  
+  DDBooleanSolid solid = (DDBooleanSolid)(fv.logicalPart().solid());
+  std::vector<double> dpar = solid.parameters(); 
+  
+  double dy = dpar[0]/cm;//length is along local Y
+  double dz = dpar[3]/cm;// thickness is long local Z
+  double dx1= dpar[4]/cm;// bottom width is along local X
+  double dx2= dpar[8]/cm;// top width is along local X
+  //dpar = solid.solidB().parameters();
+  //dz += dpar[3]/cm;// chamber thickness
 
-  while( doSubDets )
-  {
-    // Get the Base Muon Number
-    MuonDDDNumbering mdddnum( muonConstants );
-    MuonBaseNumber mbn = mdddnum.geoHistoryToBaseNumber( fview.geoHistory());
+  ME0DetId me0id = detId.chamberId();
+  std::vector<std::string> strpars;
+  std::string name = fv.logicalPart().name().name();
+  strpars.emplace_back(name);
+  
+  std::vector<double> pars;  
+  pars.emplace_back(dx1); //b/2; bottom width is along local X
+  pars.emplace_back(dx2); //B/2; top width is along local X
+  pars.emplace_back(dy); //h/2; length is along local Y
+  pars.emplace_back(dz); // thickness is long local Z
 
-    // Get the The ME0 det Id 
-    ME0NumberingScheme me0num( muonConstants );
-    ME0DetId rollDetId( me0num.baseNumberToUnitNumber( mbn ));
+  std::vector<double> vtra = getTranslation(fv);
+  std::vector<double> vrot = getRotation(fv);
 
-    std::vector<double> dpar = fview.logicalPart().solid().parameters();
-    std::vector<double> pars;
-    pars.emplace_back( dpar[4]); // half bottom edge
-    pars.emplace_back( dpar[8]); // half top edge
-    pars.emplace_back( dpar[0]); // half apothem
-    pars.emplace_back( 0.4 ); // half thickness
-    pars.emplace_back( 0.0 ); // nStrips
-    pars.emplace_back( 0.0 ); // nPads
+  rgeo.insert(me0id.rawId(), vtra, vrot, pars, strpars);
+}
 
-    std::string name = fview.logicalPart().name().name();
-    std::vector<std::string> strpars;
-    strpars.emplace_back( name );
+void
+ME0GeometryParsFromDD::buildLayer(DDFilteredView& fv, ME0DetId detId, RecoIdealGeometry& rgeo)
+{
+  LogDebug("ME0GeometryParsFromDD") << "buildLayer "<<fv.logicalPart().name().name()
+				    <<" "<< detId <<std::endl;
+  
+  DDBooleanSolid solid = (DDBooleanSolid)(fv.logicalPart().solid());
+  std::vector<double> dpar = solid.parameters(); 
+  
+  double dy = dpar[0]/cm;//length is along local Y
+  double dz = dpar[3]/cm;// thickness is long local Z
+  double dx1= dpar[4]/cm;// bottom width is along local X
+  double dx2= dpar[8]/cm;// top width is along local X
+  //dpar = solid.solidB().parameters();
+  //dz += dpar[3]/cm;// chamber thickness
 
-    DDRotationMatrix rota = fview.rotation();
+  ME0DetId me0id = detId.chamberId();
+  std::vector<std::string> strpars;
+  std::string name = fv.logicalPart().name().name();
+  strpars.emplace_back(name);
+  
+  std::vector<double> pars;  
+  pars.emplace_back(dx1); //b/2; bottom width is along local X
+  pars.emplace_back(dx2); //B/2; top width is along local X
+  pars.emplace_back(dy); //h/2; length is along local Y
+  pars.emplace_back(dz); // thickness is long local Z
 
-    // ROOT::Math way
-    DD3Vector x, y, z;
-    rota.GetComponents( x, y, z );
-    std::vector<double> vrot(9);
-    vrot[0] = x.X();
-    vrot[1] = x.Y();
-    vrot[2] = x.Z();
-    vrot[3] = y.X();
-    vrot[4] = y.Y();
-    vrot[5] = y.Z();
-    vrot[6] = z.X();
-    vrot[7] = z.Y();
-    vrot[8] = z.Z();
- 
-    DDTranslation tran = fview.translation();
-    std::vector<double> vtra(3);
-    vtra[0] = tran.x();
-    vtra[1] = tran.y();
-    vtra[2] = tran.z();
+  std::vector<double> vtra = getTranslation(fv);
+  std::vector<double> vrot = getRotation(fv);
 
-    rgeo.insert( rollDetId.rawId(), vtra, vrot, pars, strpars );
-    doSubDets = fview.nextSibling(); // go to next layer
+  rgeo.insert(me0id.rawId(), vtra, vrot, pars, strpars);
+}
+
+void
+ME0GeometryParsFromDD::buildEtaPartition(DDFilteredView& fv, ME0DetId detId, RecoIdealGeometry& rgeo)
+{
+  LogDebug("ME0GeometryParsFromDD") << "buildEtaPartition "<<fv.logicalPart().name().name()
+				    <<" "<< detId <<std::endl;
+  
+  // EtaPartition specific parameter (nstrips and npads) 
+  DDValue numbOfStrips("nStrips");
+  DDValue numbOfPads("nPads");
+  std::vector<const DDsvalues_type* > specs(fv.specifics());
+  std::vector<const DDsvalues_type* >::iterator is = specs.begin();
+  double nStrips = 0., nPads = 0.;
+  for (;is != specs.end(); is++){
+    if (DDfetch( *is, numbOfStrips)) nStrips = numbOfStrips.doubles()[0];
+    if (DDfetch( *is, numbOfPads))   nPads = numbOfPads.doubles()[0];
   }
+  LogDebug("ME0GeometryParsFromDD") 
+    << ((nStrips == 0. ) ? ("No nStrips found!!") : ("Number of strips: " + boost::lexical_cast<std::string>(nStrips))); 
+  LogDebug("ME0GeometryParsFromDD") 
+    << ((nPads == 0. ) ? ("No nPads found!!") : ("Number of pads: " + boost::lexical_cast<std::string>(nPads)));
+  
+  // EtaPartition specific parameter (size) 
+  std::vector<double> dpar = fv.logicalPart().solid().parameters();
+
+  double dy = dpar[0]/cm;//length is along local Y
+  double dz = dpar[3]/cm;// thickness is long local Z
+  double dx1= dpar[4]/cm;// bottom width is along local X
+  double dx2= dpar[8]/cm;// top width is along local X
+
+  std::vector<std::string> strpars;
+  std::string name = fv.logicalPart().name().name();
+  strpars.emplace_back(name);
+  
+  std::vector<double> pars;  
+  pars.emplace_back(dx1); //b/2; bottom width is along local X
+  pars.emplace_back(dx2); //B/2; top width is along local X
+  pars.emplace_back(dy); //h/2; length is along local Y
+  pars.emplace_back(dz); // thickness is long local Z
+  pars.emplace_back(nStrips);
+  pars.emplace_back(nPads);
+
+  std::vector<double> vtra = getTranslation(fv);
+  std::vector<double> vrot = getRotation(fv);
+
+  rgeo.insert(detId.rawId(), vtra, vrot, pars, strpars);
+}
+
+std::vector<double> ME0GeometryParsFromDD::getTranslation(DDFilteredView& fv)
+{
+  DDTranslation tran = fv.translation();
+  std::vector<double> vtra(3);
+  vtra[0]=(float) 1.0 * (tran.x());
+  vtra[1]=(float) 1.0 * (tran.y());
+  vtra[2]=(float) 1.0 * (tran.z());
+  return vtra;  
+}
+
+std::vector<double> ME0GeometryParsFromDD::getRotation(DDFilteredView& fv)
+{
+  DDRotationMatrix rota = fv.rotation();//.Inverse();
+  DD3Vector x, y, z;
+  rota.GetComponents(x,y,z);  
+  std::vector<double> vrot(9);
+  vrot[0]=(float) 1.0 * x.X();
+  vrot[1]=(float) 1.0 * x.Y();
+  vrot[2]=(float) 1.0 * x.Z();
+  vrot[3]=(float) 1.0 * y.X();
+  vrot[4]=(float) 1.0 * y.Y();
+  vrot[5]=(float) 1.0 * y.Z();
+  vrot[6]=(float) 1.0 * z.X();
+  vrot[7]=(float) 1.0 * z.Y();
+  vrot[8]=(float) 1.0 * z.Z();
+  return vrot;
 }
