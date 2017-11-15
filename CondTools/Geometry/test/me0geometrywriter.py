a16ab29dc83e0d204d1060c858656a70b0d5e4ea
Jason Lee <jason.lee@cern.ch>
2017-11-16 02:21:34 +0900

    init vectors

--- CondTools/Geometry/test/me0geometrywriter.py
+++ CondTools/Geometry/test/me0geometrywriter.py
@@ -1,8 +1,8 @@
 import FWCore.ParameterSet.Config as cms
 
-process = cms.Process("GEMGeometryWriter")
+process = cms.Process("ME0GeometryWriter")
 process.load('CondCore.CondDB.CondDB_cfi')
-process.load('Configuration.Geometry.GeometryExtended2023D20_cff')
+process.load('Configuration.Geometry.GeometryExtended2023D3_cff')
 process.load('Geometry.MuonNumbering.muonNumberingInitialization_cfi')
 
 process.source = cms.Source("EmptyIOVSource",
@@ -12,22 +12,18 @@ process.source = cms.Source("EmptyIOVSource",
                             interval = cms.uint64(1)
                             )
 
-process.GEMGeometryWriter = cms.EDAnalyzer("GEMRecoIdealDBLoader")
 process.ME0GeometryWriter = cms.EDAnalyzer("ME0RecoIdealDBLoader")
 
 process.CondDB.timetype = cms.untracked.string('runnumber')
 process.CondDB.connect = cms.string('sqlite_file:myfile.db')
 process.PoolDBOutputService = cms.Service("PoolDBOutputService",
                                           process.CondDB,
-                                          toPut = cms.VPSet(cms.PSet(record = cms.string('GEMRecoGeometryRcd'),
-                                                                        tag = cms.string('GEMRECO_Geometry_TagXX')),
-                                                            cms.PSet(record = cms.string('ME0RecoGeometryRcd'),
-                                                                        tag = cms.string('ME0RECO_Geometry_TagXX'))
-                                                            ),
+                                          toPut = cms.VPSet(cms.PSet(record = cms.string('ME0RecoGeometryRcd'),tag = cms.string('ME0RECO_Geometry_Test01'))
+                                                            )
                                           )
 
 process.maxEvents = cms.untracked.PSet(
     input = cms.untracked.int32(1)
     )
 
-process.p1 = cms.Path(process.GEMGeometryWriter+process.ME0GeometryWriter)
+process.p1 = cms.Path(process.ME0GeometryWriter)
