54d395a7fa723638bd20433930f945e9d7ed169e
Thomas Reis <thomas.reis@cern.ch>
2017-11-15 17:19:26 +0100

    Remove custom unpacking for L1T DQM modules and use the standard rawtodigi instead.

--- DQM/Integration/python/clients/l1tstage2emulator_dqm_sourceclient-live_cfg.py
+++ DQM/Integration/python/clients/l1tstage2emulator_dqm_sourceclient-live_cfg.py
@@ -66,12 +66,10 @@ process.selfFatEventFilter = cms.EDFilter("HLTL1NumberFilter",
 process.load("DQM.L1TMonitor.L1TStage2Emulator_cff")
 
 process.l1tEmulatorMonitorPath = cms.Path(
-    process.l1tStage2Unpack  +
     process.Stage2L1HardwareValidation +
     process.l1tStage2EmulatorOnlineDQM +
     process.hltFatEventFilter +
 #    process.selfFatEventFilter +
-    process.l1tStage2UnpackValidationEvents  +
     process.Stage2L1HardwareValidationForValidationEvents +
     process.l1tStage2EmulatorOnlineDQMValidationEvents
     )
