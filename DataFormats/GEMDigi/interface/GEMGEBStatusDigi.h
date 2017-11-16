14204dad351e2ded6933cffcac70619fa1c96cc6
Jason Lee <jason.lee@cern.ch>
2017-11-16 21:11:43 +0900

    clean up

--- DataFormats/GEMDigi/interface/GEMGEBStatusDigi.h
+++ DataFormats/GEMDigi/interface/GEMGEBStatusDigi.h
@@ -1,10 +1,9 @@
 #ifndef DataFormats_GEMDigi_GEMGEBStatusDigi_H
 #define DataFormats_GEMDigi_GEMGEBStatusDigi_H
 
-
 #include <cstdint>
-#include <iosfwd>
 #include <vector>
+
 class GEMGEBStatusDigi {
 
  public:
@@ -18,20 +17,17 @@ class GEMGEBStatusDigi {
 			    uint8_t Stuckd,
 			    std::vector<uint8_t> v_GEBflags);
 
-  GEMGEBStatusDigi();
-   
+  GEMGEBStatusDigi(){}
+  
   uint32_t getZeroSup() const {return ZeroSup_;}   ///<Returns Zero Suppression flags
   uint8_t  getInputID() const {return InputID_;}   ///<Returns GLIB input ID
   uint16_t getVwh() const     {return Vwh_;}       ///<Returns VFAT word count (size of VFAT payload)
   uint16_t getErrorC() const  {return ErrorC_;}    ///<Returns thirteen flags in GEM Chamber Header
-
   uint16_t getOHCRC() const   {return OHCRC_;}     ///<Returns OH CRC 
   uint16_t getVwt() const     {return Vwt_;}       ///<Returns VFAT word count
   uint8_t  getInFu() const    {return InFu_;}      ///<Returns InFIFO underflow flag
   uint8_t  getStuckd() const  {return Stuckd_;}    ///<Returns Stuck data flag
   const std::vector<uint8_t> & get_GEBflags() const {return v_GEBflags_;}
-
-
 
  private:
 
@@ -45,8 +41,5 @@ class GEMGEBStatusDigi {
   uint8_t  Stuckd_; 
   std::vector<uint8_t> v_GEBflags_;
 
-
 };
-
-
 #endif
