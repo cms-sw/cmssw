74126c7d8ba4001f4f9b1a7293936695dc2051fc
Thomas Reis <thomas.reis@cern.ch>
2017-11-16 16:39:22 +0100

    Ignore BX range mismatches in BMTF and EMTF data vs. emulator summary.

--- DQM/L1TMonitorClient/python/L1TStage2EMTFEmulatorClient_cff.py
+++ DQM/L1TMonitorClient/python/L1TStage2EMTFEmulatorClient_cff.py
@@ -1,5 +1,6 @@
 import FWCore.ParameterSet.Config as cms
 from DQMServices.Core.DQMEDHarvester import DQMEDHarvester
+from DQM.L1TMonitor.L1TdeStage2EMTF_cff import ignoreBinsDeStage2Emtf
 
 # RegionalMuonCands
 l1tStage2EMTFEmulatorCompRatioClient = DQMEDHarvester("L1TStage2RatioClient",
@@ -9,7 +10,8 @@ l1tStage2EMTFEmulatorCompRatioClient = DQMEDHarvester("L1TStage2RatioClient",
     ratioName = cms.untracked.string('mismatchRatio'),
     ratioTitle = cms.untracked.string('Summary of mismatch rates between EMTF muons and EMTF emulator muons'),
     yAxisTitle = cms.untracked.string('# mismatch / # total'),
-    binomialErr = cms.untracked.bool(True)
+    binomialErr = cms.untracked.bool(True),
+    ignoreBin = cms.untracked.vint32(ignoreBinsDeStage2Emtf)
 )
 
 # sequences
