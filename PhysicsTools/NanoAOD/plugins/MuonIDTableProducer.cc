e4f0fccf5d27721cbdbdc2b15fe1125b4c3858a3
peruzzim <peruzzim@users.noreply.github.com>
2017-11-18 13:08:09 +0100

    Fix isSoftMuonHIP cutflow

--- PhysicsTools/NanoAOD/plugins/MuonIDTableProducer.cc
+++ PhysicsTools/NanoAOD/plugins/MuonIDTableProducer.cc
@@ -97,7 +97,6 @@ bool MuonIDTableProducer::isSoftMuonHIP(const pat::Muon& mu, const reco::Vertex&
     return muon::isGoodMuon(mu, muon::TMOneStationTight) && 
            mu.innerTrack()->hitPattern().trackerLayersWithMeasurement() > 5 &&
            mu.innerTrack()->hitPattern().pixelLayersWithMeasurement() > 0 && 
-           mu.innerTrack()->quality(reco::Track::highPurity) && 
            std::abs(mu.innerTrack()->dxy(vtx.position())) < 0.3 && 
            std::abs(mu.innerTrack()->dz(vtx.position())) < 20.;
 }
