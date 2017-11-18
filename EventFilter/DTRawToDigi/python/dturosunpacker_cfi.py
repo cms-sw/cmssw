4c98030065033f9b629589380cf15b915fd6dcbb
Carlo Battilana <carlo.battilana@gmail.com>
2017-11-18 22:20:26 +0100

    make 2018 workflow run

--- EventFilter/DTRawToDigi/python/dturosunpacker_cfi.py
+++ EventFilter/DTRawToDigi/python/dturosunpacker_cfi.py
@@ -1,7 +1,7 @@
 import FWCore.ParameterSet.Config as cms
 
 dturosunpacker = cms.EDProducer("DTuROSRawToDigi",
-                                  DTuROS_FED_Source = cms.InputTag("source"),
+                                  inputLabel = cms.InputTag("source"),
                                   feds     = cms.untracked.vint32( 1368, 1369, 1370,),
                                   debug = cms.untracked.bool(False),
                                )
