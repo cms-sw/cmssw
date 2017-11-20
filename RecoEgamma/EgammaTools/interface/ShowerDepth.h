3960a2166a54c8707fd4d8480b38ed09163b725c
Nick Smith <nick.smith@cern.ch>
2017-11-20 16:50:09 +0100

    indentation

--- RecoEgamma/EgammaTools/interface/ShowerDepth.h
+++ RecoEgamma/EgammaTools/interface/ShowerDepth.h
@@ -1,8 +1,6 @@
 #ifndef RecoEgamma_EgammaTools_ShowerDepth_h
 #define RecoEgamma_EgammaTools_ShowerDepth_h
 
-namespace hgcal {
-
 /* ShowerDepth: computes expected EM shower mean depth
  * in the HGCal, and compares it to measured depth
  *
@@ -24,37 +22,39 @@ namespace hgcal {
  * The parameterization is for electrons, although photons will give similar results
  */
 
-class ShowerDepth {
-public:
-    ShowerDepth() {}
-    ~ShowerDepth() {}
+namespace hgcal {
 
-    float getClusterDepthCompatibility(float measuredDepth, float emEnergy, float & expectedDepth, float & expectedSigma) const;
+  class ShowerDepth {
+    public:
+      ShowerDepth() {}
+      ~ShowerDepth() {}
 
-private:
-    // HGCAL average medium
-    static constexpr double criticalEnergy_ = 0.00536; // in GeV
-    static constexpr double radiationLength_ = 0.968; // in cm
+      float getClusterDepthCompatibility(float measuredDepth, float emEnergy, float & expectedDepth, float & expectedSigma) const;
 
-    // mean values
-    // shower max <t_max> = t0 + t1*lny
-    static constexpr double meant0_{ -1.396 };
-    static constexpr double meant1_{ 1.007 };
-    // <alpha> = alpha0 + alpha1*lny
-    static constexpr double meanalpha0_{ -0.0433 };
-    static constexpr double meanalpha1_{ 0.540 };
-    // sigmas (relative uncertainty)
-    // sigma(ln(t_max)) = 1 / (sigmalnt0 + sigmalnt1*lny);
-    static constexpr double sigmalnt0_{ -2.506 };
-    static constexpr double sigmalnt1_{ 1.245 };
-    // sigma(ln(alpha)) = 1 / (sigmalnt0 + sigmalnt1*lny);
-    static constexpr double sigmalnalpha0_{ -0.08442 };
-    static constexpr double sigmalnalpha1_{ 0.7904 };
-    // correlation coefficient
-    // corr(ln(alpha), ln(t_max)) = corrlnalpha0_+corrlnalphalnt1_*lny
-    static constexpr double corrlnalphalnt0_{ 0.7858 };
-    static constexpr double corrlnalphalnt1_{ -0.0232 };
-};
+    private:
+      // HGCAL average medium
+      static constexpr double criticalEnergy_ = 0.00536; // in GeV
+      static constexpr double radiationLength_ = 0.968; // in cm
+
+      // mean values
+      // shower max <t_max> = t0 + t1*lny
+      static constexpr double meant0_{ -1.396 };
+      static constexpr double meant1_{ 1.007 };
+      // <alpha> = alpha0 + alpha1*lny
+      static constexpr double meanalpha0_{ -0.0433 };
+      static constexpr double meanalpha1_{ 0.540 };
+      // sigmas (relative uncertainty)
+      // sigma(ln(t_max)) = 1 / (sigmalnt0 + sigmalnt1*lny);
+      static constexpr double sigmalnt0_{ -2.506 };
+      static constexpr double sigmalnt1_{ 1.245 };
+      // sigma(ln(alpha)) = 1 / (sigmalnt0 + sigmalnt1*lny);
+      static constexpr double sigmalnalpha0_{ -0.08442 };
+      static constexpr double sigmalnalpha1_{ 0.7904 };
+      // correlation coefficient
+      // corr(ln(alpha), ln(t_max)) = corrlnalpha0_+corrlnalphalnt1_*lny
+      static constexpr double corrlnalphalnt0_{ 0.7858 };
+      static constexpr double corrlnalphalnt1_{ -0.0232 };
+  };
 
 }
 
