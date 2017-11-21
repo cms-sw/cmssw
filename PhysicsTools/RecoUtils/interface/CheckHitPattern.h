990dbfbc090e8f547751a32a1e3d18ba70bb98b2
Vincenzo Innocente <vincenzo.innocente@cern.ch>
2017-11-21 13:47:31 +0100

    make CheckHitPattern inefficient but Thread safe

--- PhysicsTools/RecoUtils/interface/CheckHitPattern.h
+++ PhysicsTools/RecoUtils/interface/CheckHitPattern.h
@@ -6,15 +6,7 @@
  * Also determine if it misses hits between its assumed production point and its innermost hit.
  */
 
-// standard EDAnalyser include files
-#include <memory>
-#include "FWCore/Framework/interface/Frameworkfwd.h"
-#include "FWCore/Framework/interface/EDAnalyzer.h"
-#include "FWCore/Framework/interface/Event.h"
-#include "FWCore/Framework/interface/MakerMacros.h"
-#include "FWCore/ParameterSet/interface/ParameterSet.h"
 #include "FWCore/Framework/interface/ESHandle.h"
-
 #include "FWCore/Framework/interface/EventSetup.h"
 #include "DataFormats/TrackReco/interface/Track.h"
 #include "RecoVertex/VertexPrimitives/interface/VertexState.h"
@@ -27,6 +19,7 @@ class DetId;
 
 class TrackerTopology;
 
+
 class CheckHitPattern {
 
 public:
@@ -37,10 +30,6 @@ public:
     // Number of missing hits between the vertex position and the innermost valid hit on the track.
     unsigned int missHitsAfterVert;
   };
-
-  CheckHitPattern() : geomInitDone_(false) {}
-  
-  ~CheckHitPattern() {}
 
   // Check if hit pattern of this track is consistent with it being produced
   // at given vertex. See comments above for "Result" struct for details of returned information.
@@ -66,12 +55,12 @@ private:
 
 private:
   // Note if geometry info is already initialized.
-  bool geomInitDone_;
+  bool geomInitDone_=false;
 
   // For a given subdetector & layer number, this stores the minimum and maximum
   // r (or z) values if it is barrel (or endcap) respectively.
   typedef std::map< DetInfo, std::pair< double, double> > RZrangeMap;
-  static RZrangeMap rangeRorZ_;
+  RZrangeMap rangeRorZ_;
 
  // Makes TransientTracks needed for vertex fitting.
   edm::ESHandle<TransientTrackBuilder> trkTool_;
