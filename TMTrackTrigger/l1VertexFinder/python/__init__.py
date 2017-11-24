7b69b6cee50165ea88ca8a4217ff2d0519318421
Tom Williams <tom.williams@cern.ch>
2017-11-24 16:24:05 +0000

    Clean up : Remove accidentally-committed files

--- TMTrackTrigger/l1VertexFinder/python/__init__.py
+++
@@ -1,3 +0,0 @@
-#Automatically created by SCRAM
-import os
-__path__.append(os.path.dirname(os.path.abspath(__file__).rsplit('/TMTrackTrigger/VertexFinder/',1)[0])+'/cfipython/slc6_amd64_gcc530/TMTrackTrigger/VertexFinder')
