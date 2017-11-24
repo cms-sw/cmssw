23139b88b1cfd8f3d056e627c34b9c5509e5468b
Simon Kurz <Simon.Kurz@desy.de>
2017-11-24 16:00:58 +0100

    Fixed ESProducer

--- FastSimulation/SimplifiedGeometryPropagator/python/FastSimGeometryESProducer_cfi.py
+++ FastSimulation/SimplifiedGeometryPropagator/python/FastSimGeometryESProducer_cfi.py
@@ -2,12 +2,12 @@ import FWCore.ParameterSet.Config as cms
 
 # With an ideal geometry (for the reconstruction)
 from FastSimulation.TrackerSetup.TrackerMaterial_cfi import *
-GeometryESProducer = cms.ESProducer("GeometryESProducer",
+FastSimGeometryESProducer = cms.ESProducer("FastSimGeometryESProducer",
     TrackerMaterialBlock
 )
 
 # The same as above but with a misaligned tracker geometry (for the simulation)
-misalignedGeometry = cms.ESProducer("GeometryESProducer",
+misalignedGeometry = cms.ESProducer("FastSimGeometryESProducer",
     TrackerMaterialBlock,
     trackerGeometryLabel = cms.untracked.string('MisAligned'),
     appendToDataLabel = cms.string('MisAligned')
