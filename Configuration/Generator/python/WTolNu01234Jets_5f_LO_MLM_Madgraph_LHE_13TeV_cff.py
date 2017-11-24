79026e334789a57d71de98f0928d7fc7662c676e
joonblee <joon.bin.lee@cern.ch>
2017-11-24 19:29:46 +0900

    update_WTolNu

--- Configuration/Generator/python/WTolNu01234Jets_5f_LO_MLM_Madgraph_LHE_13TeV_cff.py
+++ Configuration/Generator/python/WTolNu01234Jets_5f_LO_MLM_Madgraph_LHE_13TeV_cff.py
@@ -1,9 +1,9 @@
 import FWCore.ParameterSet.Config as cms
 
 externalLHEProducer = cms.EDProducer("ExternalLHEProducer",
-    args = cms.vstring('/cvmfs/cms.cern.ch/phys_generator/gridpacks/slc6_amd64_gcc481/13TeV/madgraph/V5_2.4.2/lv_bwcutoff_WJetsToLNu_HT-incl/v1/lv_bwcutoff_WJetsToLNu_HT-incl_tarball.tar.xz'),
+    args = cms.vstring('/cvmfs/cms.cern.ch/phys_generator/gridpacks/slc6_amd64_gcc481/13TeV/madgraph/V5_2.4.2/lv_bwcutoff_WJetsToLNu_HT-incl/v1/lv_bwcutoff_WJetsToLNu_HT-incl_tarball.tar.xz','false','slc6_amd64_gcc481','CMSSW_7_1_28'),
     nEvents = cms.untracked.uint32(5000),
-    numberOfParameters = cms.uint32(1),
+    numberOfParameters = cms.uint32(4),
     outputFile = cms.string('cmsgrid_final.lhe'),
     scriptName = cms.FileInPath('GeneratorInterface/LHEInterface/data/run_generic_tarball_cvmfs.sh')
 )
