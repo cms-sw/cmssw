070b36a656355be0a07db7461f2befc686864e98
ocolegro <ocolegro@u.rochester.edu>
2017-11-24 16:48:04 +0100

    Update L1TMuonEndcapParamsRcd.h

--- CondFormats/DataRecord/interface/L1TMuonEndcapParamsRcd.h
+++ CondFormats/DataRecord/interface/L1TMuonEndcapParamsRcd.h
@@ -1,19 +1,23 @@
-// L1TMuonEndCapParamsRcd                                                                                            
-// Description: Record for L1TMuonEndCapParams
+// Temporary copy to avoid crashes - AWB 28.08.17
+// Required to satisfy current convention for "Record" in "Global Tag Entries"
+// https://cms-conddb-prod.cern.ch/cmsDbBrowser/search/Prod/L1TMuonEndCapParams
+
+// L1TMuonEndcapParamsRcd                                                                                            
+// Description: Record for L1TMuonEndcapParams
 //
 // automatically generate by make_records.pl
 //
-#ifndef CondFormatsDataRecord_L1TMuonEndCapParams_h
-#define CondFormatsDataRecord_L1TMuonEndCapParams_h
+#ifndef CondFormatsDataRecord_L1TMuonEndcapParams_h
+#define CondFormatsDataRecord_L1TMuonEndcapParams_h
 
 #include "FWCore/Framework/interface/EventSetupRecordImplementation.h"
 
-class L1TMuonEndCapParamsRcd : public edm::eventsetup::EventSetupRecordImplementation<L1TMuonEndCapParamsRcd> {};
+class L1TMuonEndcapParamsRcd : public edm::eventsetup::EventSetupRecordImplementation<L1TMuonEndcapParamsRcd> {};
 
 // Dependent record implmentation:
 //#include "FWCore/Framework/interface/DependentRecordImplementation.h"
 //#include "CondFormats/DataRecord/interface/L1TriggerKeyListRcd.h"
 //#include "CondFormats/DataRecord/interface/L1TriggerKeyRcd.h"
-//class L1TMuonEndCapParamsRcd : public edm::eventsetup::DependentRecordImplementation<L1TMuonEndCapParamsRcd, boost::mpl::vector<L1TriggerKeyListRcd,L1TriggerKeyRcd> > {};
+//class L1TMuonEndcapParamsRcd : public edm::eventsetup::DependentRecordImplementation<L1TMuonEndcapParamsRcd, boost::mpl::vector<L1TriggerKeyListRcd,L1TriggerKeyRcd> > {};
 
 #endif
