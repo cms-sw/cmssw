470b241c0f49b5eb4eb7cbbb90956263a7cc2ae4
Matti Kortelainen <matti.kortelainen@cern.ch>
2017-11-28 12:53:52 +0100

    Add edm::defaultModuleLabel() to convert plugin name to a default module label

---
+++ FWCore/ParameterSet/interface/defaultModuleLabel.h
@@ -0,0 +1,30 @@
+#ifndef FWCore_ParameterSet_defaultModuleLabel_h
+#define FWCore_ParameterSet_defaultModuleLabel_h
+
+#include <string>
+#include <algorithm>
+
+namespace edm {
+  std::string defaultModuleLabel(std::string label) { // take by value because we'll copy it anyway
+    // remove all colons (module type may contain namespace)
+    label.erase(std::remove(label.begin(), label.end(), ':'), label.end());
+
+    // if the label is all uppercase, change it to all lowercase
+    // if the label starts with more than one uppercase letter, change n-1 to lowercase
+    // otherwise, change the first letter to lowercase
+    unsigned int ups = 0;
+    for (char c: label)
+      if (std::isupper(c))
+        ++ups;
+      else
+        break;
+    if (ups > 1 and ups != label.size())
+      --ups;
+    for (unsigned int i = 0; i < ups; ++i)
+      label[i] = std::tolower(label[i]);
+
+    return label;
+  }
+}
+
+#endif
