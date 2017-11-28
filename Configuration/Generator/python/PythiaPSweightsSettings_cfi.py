9e2679acf817538fcca479d8bb207bb18aa66162
Javier <Javier.Fernandez@cern.ch>
2017-11-28 17:20:17 +0100

    Right file now

--- Configuration/Generator/python/PythiaPSweightsSettings_cfi.py
+++ Configuration/Generator/python/PythiaPSweightsSettings_cfi.py
@@ -1,25 +1,20 @@
 import FWCore.ParameterSet.Config as cms
 
-pythia8CP5SettingsBlock = cms.PSet(
-    pythia8CP5Settings = cms.vstring(
-    'Tune:pp 14',
-        'Tune:ee 7',
-        'MultipartonInteractions:ecmPow=0.03344',
-        'PDF:pSet=20',
-        'MultipartonInteractions:bProfile=2',
-        'MultipartonInteractions:pT0Ref=1.41',
-        'MultipartonInteractions:coreRadius=0.7634',
-        'MultipartonInteractions:coreFraction=0.63',
-        'ColourReconnection:range=5.176',
-        'SigmaTotal:zeroAXB=off',
-        'SpaceShower:alphaSorder=2',
-        'SpaceShower:alphaSvalue=0.118',
-        'SigmaProcess:alphaSvalue=0.118',
-        'SigmaProcess:alphaSorder=2',
-        'MultipartonInteractions:alphaSvalue=0.118',
-        'MultipartonInteractions:alphaSorder=2',
-        'TimeShower:alphaSorder=2',
-        'TimeShower:alphaSvalue=0.118',
+pythia8PSweightsSettingsBlock = cms.PSet(
+    pythia8PSweightsSettings = cms.vstring(
+        'UncertaintyBands:doVariations = on',
+# 3 sets of variations for ISR&FSR up/down
+# Reduced sqrt(2)/(1/sqrt(2)), Default 2/0.5 and Conservative 4/0.25 variations
+        'UncertaintyBands:List = {\
+isrRedHi isr:muRfac=0.707,fsrRedHi fsr:muRfac=0.707,isrRedLo isr:muRfac=1.414,fsrRedLo fsr:muRfac=1.414,\
+isrDefHi isr:muRfac=0.5, fsrDefHi fsr:muRfac=0.5,isrDefLo isr:muRfac=2.0,fsrDefLo fsr:muRfac=2.0,\
+isrConHi isr:muRfac=0.25, fsrConHi fsr:muRfac=0.25,isrConLo isr:muRfac=4.0,fsrConLo fsr:muRfac=4.0}',
+
+        'UncertaintyBands:MPIshowers = on',
+        'UncertaintyBands:overSampleFSR = 10.0',
+        'UncertaintyBands:overSampleISR = 10.0',
+        'UncertaintyBands:FSRpTmin2Fac = 20',
+        'UncertaintyBands:ISRpTmin2Fac = 1'
         )
 )
 
