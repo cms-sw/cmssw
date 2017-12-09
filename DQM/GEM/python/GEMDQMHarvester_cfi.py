05c3e2093720d3777ee19bcedef36346c706425d
Jason Lee <jason.lee@cern.ch>
2017-12-10 03:54:04 +0900

    update PR

--- DQM/GEM/python/GEMDQMHarvester_cfi.py
+++
@@ -1,6 +0,0 @@
-import FWCore.ParameterSet.Config as cms
-
-GEMDQMHarvester = cms.EDProducer("GEMDQMHarvester", 
-  Name = cms.untracked.string('HarvestingAnalyzer'),
-
-)
--- DQM/GEM/python/GEMDQMSource_cfi.py
+++
@@ -1,6 +0,0 @@
-import FWCore.ParameterSet.Config as cms
-
-GEMDQMSource = cms.EDAnalyzer("GEMDQMSource",
-    recHitsInputLabel = cms.InputTag("gemRecHits", "", "RECO"),
-
-)
