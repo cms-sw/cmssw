d22c84e184b5de7a49da0001dd66eea17422e131
Martin Grunewald <Martin.Grunewald@cern.ch>
2017-12-10 13:24:23 +0100

    HI-PRef related fixes in GRun

--- HLTrigger/Configuration/test/OnLine_HLT_PRef.py
+++ HLTrigger/Configuration/test/OnLine_HLT_PRef.py
@@ -1,13 +1,13 @@
 # hltGetConfiguration --full --data /dev/CMSSW_10_0_0/PRef --type PRef --unprescale --process HLTPRef --globaltag auto:run2_hlt_PRef --input file:RelVal_Raw_PRef_DATA.root
 
-# /dev/CMSSW_10_0_0/PRef/V1 (CMSSW_10_0_0_pre2)
+# /dev/CMSSW_10_0_0/PRef/V2 (CMSSW_10_0_0_pre2)
 
 import FWCore.ParameterSet.Config as cms
 
 process = cms.Process( "HLTPRef" )
 
 process.HLTConfigVersion = cms.PSet(
-  tableName = cms.string('/dev/CMSSW_10_0_0/PRef/V1')
+  tableName = cms.string('/dev/CMSSW_10_0_0/PRef/V2')
 )
 
 process.transferSystem = cms.PSet( 
