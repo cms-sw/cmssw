6abf4c9ed9a4691cd24990d9de5bb0e80730f83a
vpalladi <v.palladino@imperial.ac.uk>
2017-12-11 15:16:13 +0100

    All comments implemnted except the PtrVector to be replaces with vector<Ptr>

--- L1Trigger/L1THGCal/interface/be_algorithms/HGCalClusteringImpl.h
+++ L1Trigger/L1THGCal/interface/be_algorithms/HGCalClusteringImpl.h
@@ -72,9 +72,9 @@ private:
     void triggerCellReshuffling( const edm::PtrVector<l1t::HGCalTriggerCell> & triggerCellsPtrs, 
                                  std::array<std::array<std::vector<edm::Ptr<l1t::HGCalTriggerCell>>, kLayers_>, kNSides_> & reshuffledTriggerCells );
 
-    bool areTCneighbour_( uint32_t detIDa, uint32_t detIDb, const HGCalTriggerGeometryBase & triggerGeometry );
+    bool areTCneighbour( uint32_t detIDa, uint32_t detIDb, const HGCalTriggerGeometryBase & triggerGeometry );
     
-    void removeUnconnectedTCinCluster_( l1t::HGCalCluster* cluster,
+    void removeUnconnectedTCinCluster( l1t::HGCalCluster & cluster,
                                         const HGCalTriggerGeometryBase & triggerGeometry
         );
 
