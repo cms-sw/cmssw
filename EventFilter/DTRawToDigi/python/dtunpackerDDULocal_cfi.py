f37c30e9e218dee47a0ec9ecd8c9caa5e6e2b711
Javier <Javier.Fernandez@cern.ch>
2017-12-12 17:24:20 +0100

    Full path for headers and unneeded Eras removal

--- EventFilter/DTRawToDigi/python/dtunpackerDDULocal_cfi.py
+++ EventFilter/DTRawToDigi/python/dtunpackerDDULocal_cfi.py
@@ -23,8 +23,3 @@ dtunpacker.readOutParameters = cms.PSet(
     performDataIntegrityMonitor = cms.untracked.bool(False)
 )
 
-import EventFilter.DTRawToDigi.dturosunpacker_cfi
-_dturosunpacker = EventFilter.DTRawToDigi.dturosunpacker_cfi.dturosunpacker.clone()
-from Configuration.Eras.Modifier_run2_DT_2018_cff import run2_DT_2018
-run2_DT_2018.toReplaceWith(dtunpacker, _dturosunpacker)
-
