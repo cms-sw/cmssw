774de7f9e06e7b8b85581fa3933d8eec8825c189
Pablo de Castro <pablodecm@gmail.com>
2017-12-13 15:30:18 +0100

    add Holders and clone/destructor for DeepFlavourTagInfo

--- DataFormats/BTauReco/interface/DeepFlavourTagInfo.h
+++ DataFormats/BTauReco/interface/DeepFlavourTagInfo.h
@@ -24,6 +24,11 @@ template<class Features> class FeaturesTagInfo : public BaseTagInfo {
 
     const Features & features() const { return features_; }
 
+    ~FeaturesTagInfo() override {}
+    // without overidding clone from base class will be store/retrieved
+    FeaturesTagInfo* clone(void) const override { return new FeaturesTagInfo(*this); }
+
+
     CMS_CLASS_VERSION(3)
 
   private:
