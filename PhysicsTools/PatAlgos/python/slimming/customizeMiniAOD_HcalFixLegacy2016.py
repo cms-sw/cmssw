26a8d3c3f558ad1f942b8d3678b73ea794884554
federico de guio <federico.de.guio@cern.ch>
2017-12-13 11:04:53 +0100

    attempt to handle refs properly

--- PhysicsTools/PatAlgos/python/slimming/customizeMiniAOD_HcalFixLegacy2016.py
+++ PhysicsTools/PatAlgos/python/slimming/customizeMiniAOD_HcalFixLegacy2016.py
@@ -3,6 +3,35 @@ import FWCore.ParameterSet.Config as cms
 from PhysicsTools.PatAlgos.tools.helpers import MassSearchReplaceAnyInputTagVisitor, addKeepStatement
 from PhysicsTools.PatAlgos.tools.helpers import getPatAlgosToolsTask
 
+def loadJetMETBTag(process):
+
+    task = getPatAlgosToolsTask(process)
+
+    import RecoJets.Configuration.RecoPFJets_cff
+    process.ak4PFJetsCHS = RecoJets.Configuration.RecoPFJets_cff.ak4PFJetsCHS.clone()
+    task.add(process.ak4PFJetsCHS)
+    process.ak8PFJetsCHS = RecoJets.Configuration.RecoPFJets_cff.ak8PFJetsCHS.clone()
+    task.add(process.ak8PFJetsCHS)
+    process.load("RecoMET.METProducers.PFMET_cfi")
+    task.add(process.pfMet)
+    process.load("RecoBTag.ImpactParameter.impactParameter_cff")
+    task.add(process.impactParameterTask)
+    process.load("RecoBTag.SecondaryVertex.secondaryVertex_cff")
+    task.add(process.secondaryVertexTask)
+    process.load("RecoBTag.SoftLepton.softLepton_cff")
+    task.add(process.softLeptonTask)
+    process.load("RecoBTag.Combined.combinedMVA_cff")
+    task.add(process.combinedMVATask)
+    process.load("RecoBTag.Combined.deepFlavour_cff")
+    task.add(process.pfDeepFlavourTask)
+    process.load("RecoBTag.CTagging.cTagging_cff")
+    task.add(process.cTaggingTask)
+    process.load("RecoVertex.AdaptiveVertexFinder.inclusiveVertexing_cff")
+    task.add(process.inclusiveVertexingTask)
+    task.add(process.inclusiveCandidateVertexingTask)
+    task.add(process.inclusiveCandidateVertexingCvsLTask)
+
+
 def cleanPfCandidates(process, verbose=False):
     task = getPatAlgosToolsTask(process)
 
@@ -16,6 +45,14 @@ def cleanPfCandidates(process, verbose=False):
             if obj != process.pfCandidatesBadHadRecalibrated:
                 replacePFCandidates.doIt(obj, name)
 
+
+    process.load("CommonTools.ParticleFlow.pfEGammaToCandidateRemapper_cfi")
+    task.add(process.pfEGammaToCandidateRemapper)
+    process.pfEGammaToCandidateRemapper.pf2pf = cms.InputTag("pfCandidatesBadHadRecalibrated")
+    process.reducedEgamma.gsfElectronsPFValMap = cms.InputTag("pfEGammaToCandidateRemapper","electrons")
+    process.reducedEgamma.photonsPFValMap      = cms.InputTag("pfEGammaToCandidateRemapper","photons")
+
+
     #add bugged conditions to GT for comparison
     process.GlobalTag.toGet.append(cms.PSet(
         record = cms.string("HcalRespCorrsRcd"),
@@ -24,17 +61,45 @@ def cleanPfCandidates(process, verbose=False):
         )
     )
 
+def addDiscardedPFCandidates(process, inputCollection, verbose=False):
+
+    task = getPatAlgosToolsTask(process)
+
+    process.primaryVertexAssociationDiscardedCandidates = process.primaryVertexAssociation.clone(
+        particles = inputCollection,
+        )
+    task.add(process.primaryVertexAssociationDiscardedCandidates)
+
+    process.packedPFCandidatesDiscarded = process.packedPFCandidates.clone(
+        inputCollection = inputCollection,
+        PuppiNoLepSrc = cms.InputTag(""),
+        PuppiSrc = cms.InputTag(""),
+        secondaryVerticesForWhiteList = cms.VInputTag(),
+        vertexAssociator = cms.InputTag("primaryVertexAssociationDiscardedCandidates","original")
+        )
+    task.add(process.packedPFCandidatesDiscarded)
+
+    addKeepStatement(process, "keep patPackedCandidates_packedPFCandidates_*_*",
+                             ["keep patPackedCandidates_packedPFCandidatesDiscarded_*_*"],
+                              verbose=verbose)
+    # Now make the mixed map for rekeying
+    from PhysicsTools.PatAlgos.slimming.packedPFCandidateRefMixer_cfi import packedPFCandidateRefMixer
+    process.oldPFCandToPackedOrDiscarded = packedPFCandidateRefMixer.clone(
+        pf2pf = cms.InputTag(inputCollection.moduleLabel),
+        pf2packed = cms.VInputTag(cms.InputTag("packedPFCandidates"), cms.InputTag("packedPFCandidatesDiscarded"))
+    )
+    task.add(process.oldPFCandToPackedOrDiscarded)
 
 
-def customizeAll(process, verbose=False):
-    print "===>>> customizing the process for legacy rereco 2016"
+def customizeAll(process, verbose=True): #change to false by default
+
+    if verbose:
+        print "===>>> customizing the process for legacy rereco 2016"
+
+    loadJetMETBTag(process)
 
     cleanPfCandidates(process, verbose)
+    addDiscardedPFCandidates(process, cms.InputTag("pfCandidatesBadHadRecalibrated","discarded"), verbose=verbose)
 
-    addKeepStatement(process,
-                     "keep *_pfCandidatesBadHadRecalibrated_*_*",
-                     ["keep *_pfCandidatesBadHadRecalibrated_discarded_*"],
-                     verbose=verbose)
-    
 
     return process
