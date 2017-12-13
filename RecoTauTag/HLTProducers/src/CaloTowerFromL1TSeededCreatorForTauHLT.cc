51c9213bdd1090671cb612283f6976013472d539
Thomas Strebler <thomas.strebler@llr.in2p3.fr>
2017-12-13 11:24:35 +0100

    Added CaloTower producer for regional PFlow based on fired L1 objects

---
+++ RecoTauTag/HLTProducers/src/CaloTowerFromL1TSeededCreatorForTauHLT.cc
@@ -0,0 +1,98 @@
+// makes CaloTowerCandidates from CaloTowers
+// original author: L.Lista INFN, modifyed by: F.Ratnikov UMd 
+// Author for regionality A. Nikitenko
+// Modified by S. Gennai + T. Strebler
+
+#include "DataFormats/RecoCandidate/interface/RecoCaloTowerCandidate.h"
+#include "DataFormats/Common/interface/Handle.h"
+#include "FWCore/Framework/interface/Event.h"
+#include "FWCore/ParameterSet/interface/ParameterSet.h"
+#include "FWCore/MessageLogger/interface/MessageLogger.h"
+#include "RecoTauTag/HLTProducers/interface/CaloTowerFromL1TSeededCreatorForTauHLT.h"
+// Math
+#include "Math/GenVector/VectorUtil.h"
+#include <cmath>
+
+using namespace edm ;
+using namespace reco;
+using namespace std ;
+
+CaloTowerFromL1TSeededCreatorForTauHLT::CaloTowerFromL1TSeededCreatorForTauHLT( const ParameterSet & p ) 
+  :
+  mVerbose                                            (p.getUntrackedParameter<int> ("verbose"        , 0) ),
+  mtowers_token     (consumes<CaloTowerCollection>    (p.getParameter<InputTag>     ("towers"            ))),
+  mCone                                               (p.getParameter<double>       ("UseTowersInCone"   ) ),
+  mTauTrigger_token (consumes<trigger::TriggerFilterObjectWithRefs>   (p.getParameter<InputTag>     ("TauTrigger"        ))),
+  mEtThreshold                                        (p.getParameter<double>       ("minimumEt"         ) ),
+  mEThreshold                                         (p.getParameter<double>       ("minimumE"          ) )
+{
+  produces<CaloTowerCollection>();
+}
+
+CaloTowerFromL1TSeededCreatorForTauHLT::~CaloTowerFromL1TSeededCreatorForTauHLT() {
+}
+
+void CaloTowerFromL1TSeededCreatorForTauHLT::produce( StreamID sid, Event& evt, const EventSetup& stp ) const {
+  edm::Handle<CaloTowerCollection> caloTowers;
+  evt.getByToken( mtowers_token, caloTowers );
+
+  // L1 seeds
+  edm::Handle<trigger::TriggerFilterObjectWithRefs> l1TriggeredTaus;
+  evt.getByToken( mTauTrigger_token, l1TriggeredTaus );
+
+  std::unique_ptr<CaloTowerCollection> cands( new CaloTowerCollection );
+  cands->reserve( caloTowers->size() );
+
+  l1t::TauVectorRef tauCandRefVec;
+  l1TriggeredTaus->getObjects( trigger::TriggerL1Tau,tauCandRefVec);
+
+  for(unsigned int iL1Tau = 0; iL1Tau < tauCandRefVec.size(); iL1Tau++){  
+
+    unsigned idx   = 0 ;
+    for (; idx < caloTowers->size(); idx++) {
+      const CaloTower* cal = &((*caloTowers) [idx]);
+      bool isAccepted = false;
+      if (mVerbose == 2) {
+	edm::LogInfo("JetDebugInfo") << "CaloTowerFromL1TSeededCreatorForTauHLT::produce-> " << idx 
+				     << " tower et/eta/phi/e: "                        << cal->et()  << '/' 
+				     << cal->eta() << '/' 
+				     << cal->phi() << '/' 
+				     << cal->energy() 
+				     << " is...";
+      }
+      if (cal->et() >= mEtThreshold && cal->energy() >= mEThreshold ) {
+	math::PtEtaPhiELorentzVector p( cal->et(), cal->eta(), cal->phi(), cal->energy() );
+	double delta  = ROOT::Math::VectorUtil::DeltaR((tauCandRefVec[iL1Tau]->p4()).Vect(), p);
+	if(delta < mCone) {
+	  isAccepted = true;
+	  cands->push_back( *cal );
+	}
+      }
+      if (mVerbose == 2){
+	if (isAccepted) edm::LogInfo("JetDebugInfo") << "accepted \n";
+	else edm::LogInfo("JetDebugInfo") << "rejected \n";
+      }
+
+    }
+
+  } 
+  
+  evt.put(std::move(cands));
+  
+}
+
+void CaloTowerFromL1TSeededCreatorForTauHLT::fillDescriptions( edm::ConfigurationDescriptions & desc ) {
+
+  edm::ParameterSetDescription aDesc;
+
+  aDesc.add<edm::InputTag>("TauTrigger"     , edm::InputTag("hltL1sDoubleIsoTau40er"))->setComment("Name of trigger filter for L1 seeds"                                );
+  aDesc.add<edm::InputTag>("towers"         , edm::InputTag("towerMaker"     ))->setComment("Input tower collection"                                       );
+  aDesc.add<double>       ("UseTowersInCone", 0.8                             )->setComment("Radius of cone around seed"                                   );
+  aDesc.add<double>       ("minimumE"       , 0.8                             )->setComment("Minimum tower energy"                                         );
+  aDesc.add<double>       ("minimumEt"      , 0.5                             )->setComment("Minimum tower ET"                                             );
+  aDesc.addUntracked<int> ("verbose"        , 0                               )->setComment("Verbosity level; 0=silent"                                    );
+
+  desc.add                ("CaloTowerFromL1TSeededCreatorForTauHLT", aDesc);
+  desc.setComment         ("Produce tower collection around L1 particle seed.");
+
+}
