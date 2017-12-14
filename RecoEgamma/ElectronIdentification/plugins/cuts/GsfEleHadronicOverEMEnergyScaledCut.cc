79a1011da007b12a3020fb97ecd6d11ac93a3dac
livia <livia.soffi@cern.ch>
2017-12-14 16:59:42 +0100

    Added New 2017 Electron cutbased ID

---
+++ RecoEgamma/ElectronIdentification/plugins/cuts/GsfEleHadronicOverEMEnergyScaledCut.cc
@@ -0,0 +1,69 @@
+#include "PhysicsTools/SelectorUtils/interface/CutApplicatorWithEventContentBase.h"
+#include "DataFormats/EgammaCandidates/interface/GsfElectron.h"
+
+class GsfEleHadronicOverEMEnergyScaledCut : public CutApplicatorWithEventContentBase {
+public:
+  GsfEleHadronicOverEMEnergyScaledCut(const edm::ParameterSet& c) :
+    CutApplicatorWithEventContentBase(c),
+    _barrelC0(c.getParameter<double>("barrelC0")),
+    _barrelCE(c.getParameter<double>("barrelCE")),
+    _barrelCr(c.getParameter<double>("barrelCr")),
+    _endcapC0(c.getParameter<double>("endcapC0")),
+    _endcapCE(c.getParameter<double>("endcapCE")),
+    _endcapCr(c.getParameter<double>("endcapCr")),
+    _barrelCutOff(c.getParameter<double>("barrelCutOff")) 
+  {
+    edm::InputTag rhoTag = c.getParameter<edm::InputTag>("rho");    
+    contentTags_.emplace("rho",rhoTag);  
+    
+  }
+  
+  result_type operator()(const reco::GsfElectronPtr&) const override final;
+
+  void setConsumes(edm::ConsumesCollector&) override final;
+  void getEventContent(const edm::EventBase&) override final;
+
+  double value(const reco::CandidatePtr& cand) const override final;
+
+  CandidateType candidateType() const override final { 
+    return ELECTRON; 
+  }
+
+private:
+  const float _barrelC0, _barrelCE, _barrelCr, _endcapC0, _endcapCE, _endcapCr, _barrelCutOff;  
+  edm::Handle<double> rhoHandle_;
+};
+
+DEFINE_EDM_PLUGIN(CutApplicatorFactory,
+		  GsfEleHadronicOverEMEnergyScaledCut,
+		  "GsfEleHadronicOverEMEnergyScaledCut");
+
+void GsfEleHadronicOverEMEnergyScaledCut::setConsumes(edm::ConsumesCollector& cc) {
+  auto rho = cc.consumes<double>(contentTags_["rho"]);
+  contentTokens_.emplace("rho",rho);
+}
+
+void GsfEleHadronicOverEMEnergyScaledCut::getEventContent(const edm::EventBase& ev) {  
+  ev.getByLabel(contentTags_["rho"],rhoHandle_);
+}
+
+
+CutApplicatorBase::result_type GsfEleHadronicOverEMEnergyScaledCut::operator()(const reco::GsfElectronPtr& cand) const { 
+  std::cout<<"here1"<<std::endl;
+  const double rho = rhoHandle_.isValid() ? (float)(*rhoHandle_) : 0;
+  std::cout<<"here2"<<std::endl;
+  const float energy = cand->superCluster()->energy();
+  std::cout<<"here3"<<std::endl;
+  const float C0 = (std::abs(cand->superCluster()->position().eta()) < _barrelCutOff ? _barrelC0 : _endcapC0);
+  std::cout<<"here4"<<std::endl;
+  const float CE = (std::abs(cand->superCluster()->position().eta()) < _barrelCutOff ? _barrelCE : _endcapCE);
+  std::cout<<"here5"<<std::endl;
+  const float Cr = (std::abs(cand->superCluster()->position().eta()) < _barrelCutOff ? _barrelCr : _endcapCr);
+  std::cout<<"here6"<<std::endl;
+  return cand->hadronicOverEm() < C0 + CE/energy + Cr*rho/energy;
+}
+
+double GsfEleHadronicOverEMEnergyScaledCut::value(const reco::CandidatePtr& cand) const {
+  reco::GsfElectronPtr ele(cand);  
+  return ele->hadronicOverEm();
+}
