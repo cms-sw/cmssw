5841fd0afed5d7a7d142f00ab59c70c4934eee1b
Vladimir <Vladimir.Ivantchenko@cern.ch>
2017-12-17 19:15:46 +0100

    cleanup sensitive tracker

--- SimG4CMS/Tracker/src/TrackerFrameRotation.cc
+++ SimG4CMS/Tracker/src/TrackerFrameRotation.cc
@@ -1,6 +1,6 @@
 #include "SimG4CMS/Tracker/interface/TrackerFrameRotation.h"
 #include "G4SystemOfUnits.hh"
 
-Local3DPoint TrackerFrameRotation::transformPoint(Local3DPoint & point,G4VPhysicalVolume * v=nullptr) const {
+Local3DPoint TrackerFrameRotation::transformPoint(const Local3DPoint & point,const G4VPhysicalVolume*) const {
   return Local3DPoint(point.x()/cm,point.y()/cm,point.z()/cm);
 }
