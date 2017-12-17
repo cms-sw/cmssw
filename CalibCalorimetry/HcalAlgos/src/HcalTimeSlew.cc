ea74f080ec325e1cefbe05f7327b64786ad8cca0
Christopher_Madrid <Chris_Madrid@Baylor.edu>
2017-12-16 20:39:41 -0600

    Adding default parameters

--- CalibCalorimetry/HcalAlgos/src/HcalTimeSlew.cc
+++ CalibCalorimetry/HcalAlgos/src/HcalTimeSlew.cc
@@ -10,17 +10,36 @@ void HcalTimeSlew::addM3ParameterSet(double cap, double tspar0, double tspar1, d
   parametersM3_.emplace_back(cap, tspar0, tspar1, tspar2, tspar0_siPM, tspar1_siPM, tspar2_siPM);
 }
 
-// used by M2
-double HcalTimeSlew::delay(double fC, BiasSetting bias) const {
-  double rawDelay = parametersM2_[bias].tzero + parametersM2_[bias].slope*log(fC);
-  return (rawDelay < 0)?(0):((rawDelay > parametersM2_[bias].tmax)?(parametersM2_[bias].tmax):(rawDelay));
+// Used by M2/Simulation
+double HcalTimeSlew::delay(double fC, BiasSetting bias) const {  
+  if(!parametersM2_.empty()){ 
+    double rawDelay = parametersM2_[bias].tzero + parametersM2_[bias].slope*log(fC);
+    return (rawDelay < 0)?(0):((rawDelay > parametersM2_[bias].tmax)?(parametersM2_[bias].tmax):(rawDelay));
+  }
+  else{//Default parameters
+    double rawDelay = 13.307784 + -1.556668*log(fC);
+    return (rawDelay < 0)?(0):((rawDelay > 10.00)?(10.00):(rawDelay));
+  }
 }
 
-// used by M3
+// Used by M3
 double HcalTimeSlew::delay(double fC, ParaSource source, bool isHPD) const {
-  if(fC < 0) return 0;
-  else{
-    if(isHPD) return std::fmin(parametersM3_[source].cap,parametersM3_[source].tspar0+parametersM3_[source].tspar1*log(fC+parametersM3_[source].tspar2));
-    return parametersM3_[source].cap+parametersM3_[source].tspar0_siPM;
+  double rawDelay = 0.0;
+  if(!parametersM3_.empty()){
+    if(isHPD){
+      rawDelay = std::fmin(parametersM3_[source].cap,parametersM3_[source].tspar0+parametersM3_[source].tspar1*log(fC+parametersM3_[source].tspar2));
+    }
+    else{
+      rawDelay = parametersM3_[source].cap+parametersM3_[source].tspar0_siPM;  
+    }
   }
+  else{//Default parameters
+    if(isHPD){
+      rawDelay = std::fmin(6.0,12.2999-2.19142*log(fC));
+    }
+    else{
+      rawDelay = 6.0;
+    }
+  }
+  return (rawDelay < 0)?(0):(rawDelay);
 }
