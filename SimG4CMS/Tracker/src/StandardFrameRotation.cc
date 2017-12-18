e9f05a70ab6ac69563cd3324305ccf601f389111
Vladimir <Vladimir.Ivantchenko@cern.ch>
2017-12-18 18:56:51 +0100

    removed obsolete files

--- SimG4CMS/Tracker/src/StandardFrameRotation.cc
+++
@@ -1,6 +0,0 @@
-#include "SimG4CMS/Tracker/interface/StandardFrameRotation.h"
-#include "G4SystemOfUnits.hh"
-
-Local3DPoint StandardFrameRotation::transformPoint(const Local3DPoint & point, const G4VPhysicalVolume *) const {
-  return Local3DPoint(point.x()*invcm,point.y()*invcm,point.z()*invcm);
-}
