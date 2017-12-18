1aaf14a10c6f3391d48cc52f7e40a122b53411cb
maria <dalfonso@cern.ch>
2017-12-18 20:28:18 +0100

    fix pedestal threshould

--- RecoLocalCalo/HcalRecAlgos/src/PedestalSub.cc
+++ RecoLocalCalo/HcalRecAlgos/src/PedestalSub.cc
@@ -30,12 +30,12 @@ double PedestalSub::getCorrection(const std::vector<double> & inputCharge, const
   double baseline=0;
 
     for (auto i=0; i<nTS; i++) {
-      if (i==soi||i==soi+1) continue;
+      if (i==soi || i==(soi+1)) continue;
       if ( (inputCharge[i]-inputPedestal[i])<3*inputNoise[i]) {
 	baseline+=(inputCharge[i]-inputPedestal[i]);
       }
       else {
-	baseline+=fThreshold;
+	baseline+=3*inputNoise[i];
       }
     }
     baseline/=(nTS-2);
