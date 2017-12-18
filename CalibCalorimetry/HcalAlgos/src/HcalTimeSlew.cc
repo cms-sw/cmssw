8f91288eab38feadfb588f4db4dcf464bd672483
Christopher_Madrid <Chris_Madrid@Baylor.edu>
2017-12-18 10:15:13 -0600

    Removed default parameters and fixed HLT customize to remove time slew parameters

--- CalibCalorimetry/HcalAlgos/src/HcalTimeSlew.cc
+++ CalibCalorimetry/HcalAlgos/src/HcalTimeSlew.cc
@@ -12,34 +12,19 @@ void HcalTimeSlew::addM3ParameterSet(double cap, double tspar0, double tspar1, d
 
 // Used by M2/Simulation
 double HcalTimeSlew::delay(double fC, BiasSetting bias) const {  
-  if(!parametersM2_.empty()){ 
-    double rawDelay = parametersM2_[bias].tzero + parametersM2_[bias].slope*log(fC);
-    return (rawDelay < 0)?(0):((rawDelay > parametersM2_[bias].tmax)?(parametersM2_[bias].tmax):(rawDelay));
-  }
-  else{//Default parameters
-    double rawDelay = 13.307784 + -1.556668*log(fC);
-    return (rawDelay < 0)?(0):((rawDelay > 10.00)?(10.00):(rawDelay));
-  }
+  double rawDelay = parametersM2_[bias].tzero + parametersM2_[bias].slope*log(fC);
+  return (rawDelay < 0)?(0):((rawDelay > parametersM2_[bias].tmax)?(parametersM2_[bias].tmax):(rawDelay));
 }
 
 // Used by M3
 double HcalTimeSlew::delay(double fC, ParaSource source, bool isHPD) const {
   double rawDelay = 0.0;
-  if(!parametersM3_.empty()){
-    if(isHPD){
-      rawDelay = std::fmin(parametersM3_[source].cap,parametersM3_[source].tspar0+parametersM3_[source].tspar1*log(fC+parametersM3_[source].tspar2));
-    }
-    else{
-      rawDelay = parametersM3_[source].cap+parametersM3_[source].tspar0_siPM;  
-    }
+  if(isHPD){
+    rawDelay = std::fmin( parametersM3_[source].cap, 
+			  parametersM3_[source].tspar0+parametersM3_[source].tspar1*log(fC+parametersM3_[source].tspar2 ));
   }
-  else{//Default parameters
-    if(isHPD){
-      rawDelay = std::fmin(6.0,12.2999-2.19142*log(fC));
-    }
-    else{
-      rawDelay = 6.0;
-    }
+  else{
+    rawDelay = parametersM3_[source].cap+parametersM3_[source].tspar0_siPM;  
   }
   return (rawDelay < 0)?(0):(rawDelay);
 }
