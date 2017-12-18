24bfb065f5545d1bf950601149a2b7df9e2f8a28
vpalladi <v.palladino@imperial.ac.uk>
2017-12-18 11:19:40 +0100

    Indentation fixed in DataFormats/L1THGCal/interface/HGCalClusterT.h; dRNNC2D method is set as not the default

--- DataFormats/L1THGCal/interface/HGCalClusterT.h
+++ DataFormats/L1THGCal/interface/HGCalClusterT.h
@@ -3,7 +3,6 @@
 
 /* CMSSW */
 #include "DataFormats/Common/interface/Ptr.h"
-#include "DataFormats/Common/interface/PtrVector.h"
 #include "DataFormats/GeometryVector/interface/GlobalPoint.h"
 #include "DataFormats/L1Trigger/interface/L1Candidate.h"
 #include "DataFormats/L1THGCal/interface/HGCalTriggerCell.h"
@@ -17,268 +16,266 @@ namespace l1t
   template <class C> class HGCalClusterT : public L1Candidate 
   {
 
-  public:
-    typedef typename std::vector<edm::Ptr<C>>::const_iterator const_iterator;
+    public:
+      typedef typename std::vector<edm::Ptr<C>>::const_iterator const_iterator;
 
-  public:
-    HGCalClusterT(){}
-    HGCalClusterT( const LorentzVector p4,
-             int pt=0,
-             int eta=0,
-             int phi=0
-    )
-    : L1Candidate(p4, pt, eta, phi),
-      valid_(true),
-      detId_(0),
-      centre_(0, 0, 0),
-      centreProj_(0., 0., 0.),
-      mipPt_(0),
-      seedMipPt_(0){}
-    
-    HGCalClusterT( const edm::Ptr<C>& c ):
-    valid_(true),
-      detId_( c->detId() ),
-      centre_(0., 0., 0.),
-      centreProj_(0., 0., 0.),
-      mipPt_(0.),
-      seedMipPt_(0.)
+    public:
+      HGCalClusterT(){}
+      HGCalClusterT( const LorentzVector p4,
+          int pt=0,
+          int eta=0,
+          int phi=0
+          )
+        : L1Candidate(p4, pt, eta, phi),
+        valid_(true),
+        detId_(0),
+        centre_(0, 0, 0),
+        centreProj_(0., 0., 0.),
+        mipPt_(0),
+        seedMipPt_(0){}
+
+      HGCalClusterT( const edm::Ptr<C>& c ):
+        valid_(true),
+        detId_( c->detId() ),
+        centre_(0., 0., 0.),
+        centreProj_(0., 0., 0.),
+        mipPt_(0.),
+        seedMipPt_(0.)
       {
-        addConstituent( c );
-      }
-    
-    ~HGCalClusterT() {};
-    
-    const std::vector<edm::Ptr<C>>& constituents() const { return constituents_; }  
-    const_iterator constituents_begin() const { return constituents_.begin(); }
-    const_iterator constituents_end() const { return constituents_.end(); }
-    unsigned size() const { return constituents_.size(); }
-    
-    void addConstituent( const edm::Ptr<C>& c, bool updateCentre=true, float fraction=1. )
-    {
-      
-//      if( fraction > 1. ) edm::LogInfo("HGCalClusterT::addConstituent: fraction greater than 1!!!") ; 
-
-      double cMipt = c->mipPt()*fraction;
-
-      if( constituents_.empty() )
-      { 
-        detId_ = HGCalDetId( c->detId() );
-        seedMipPt_ = cMipt;
-        /* if the centre will not be dynamically calculated 
-           the seed centre is considere as cluster centre */
-        if( !updateCentre )
-        {
-          centre_ = c->position(); 
-        }
+        addConstituent(c);
       }
 
-      /* update cluster positions (IF requested) */
-      if( updateCentre ){
-        Basic3DVector<float> constituentCentre( c->position() );
-        Basic3DVector<float> clusterCentre( centre_ );
-      
-        clusterCentre = clusterCentre*mipPt_ + constituentCentre*cMipt;
-        if( (mipPt_ + cMipt ) > 0 ) 
-        {
-          clusterCentre /= ( mipPt_ +  cMipt );
-        }
-        centre_ = GlobalPoint( clusterCentre );
-      
-        if( clusterCentre.z()!=0 ) 
-        {
-          centreProj_= GlobalPoint( clusterCentre / clusterCentre.z() );
-        }
-      }
+      ~HGCalClusterT() {};
 
-      /* update cluster energies */
-      mipPt_ += cMipt;
+      const std::vector<edm::Ptr<C>>& constituents() const { return constituents_; }
+      const_iterator constituents_begin() const { return constituents_.begin(); }
+      const_iterator constituents_end() const { return constituents_.end(); }
+      unsigned size() const { return constituents_.size(); }
 
-      int updatedPt = hwPt() + (int)(c->hwPt()*fraction);
-      setHwPt( updatedPt );
-
-      math::PtEtaPhiMLorentzVector updatedP4 ( p4() );
-      updatedP4 += (c->p4()*fraction); 
-      setP4( updatedP4 );
-
-      constituents_.push_back( c );
-      constituentsFraction_.push_back( fraction );
-
-    }
-    
-    void removeConstituent( const edm::Ptr<C>& c, bool updateCentre=true ){
-      
-      /* remove the pointer to c from the std::vector */
-      double fraction=0;
-      bool constituentRemoved=false;
-      for( unsigned i=0; i<constituents_.size(); i++ )
+      void addConstituent( const edm::Ptr<C>& c, bool updateCentre=true, float fraction=1. )
       {
-        if( constituents_[i] == c )
+
+        double cMipt = c->mipPt()*fraction;
+
+        if( constituents_.empty() )
         {
-          // remove constituent and get its fraction in the cluster
-          constituents_.erase( constituents_.begin()+i );
-          fraction = constituentsFraction_.at(i);
-          constituentsFraction_.erase( constituentsFraction_.begin()+i );
-          constituentRemoved=true;
-          break;
+          detId_ = HGCalDetId( c->detId() );
+          seedMipPt_ = cMipt;
+          /* if the centre will not be dynamically calculated
+             the seed centre is considere as cluster centre */
+          if( !updateCentre )
+          {
+            centre_ = c->position();
+          }
         }
-      }
-      
-      /* if a constituent has been removed update cluster info */
-      if( constituentRemoved ) { 
 
         /* update cluster positions (IF requested) */
-        double cMipt = c->mipPt()*fraction;
         if( updateCentre ){
           Basic3DVector<float> constituentCentre( c->position() );
           Basic3DVector<float> clusterCentre( centre_ );
-          
-          clusterCentre = clusterCentre*mipPt_ - constituentCentre*cMipt;
-          if( (mipPt_ - cMipt ) > 0 ) 
+
+          clusterCentre = clusterCentre*mipPt_ + constituentCentre*cMipt;
+          if( (mipPt_ + cMipt ) > 0 )
           {
-            clusterCentre /= ( mipPt_ - cMipt ) ;
+            clusterCentre /= ( mipPt_ +  cMipt );
           }
           centre_ = GlobalPoint( clusterCentre );
-          
-          if( clusterCentre.z() != 0 ) 
+
+          if( clusterCentre.z()!=0 )
           {
             centreProj_= GlobalPoint( clusterCentre / clusterCentre.z() );
-          }  
-        
+          }
         }
-      
+
         /* update cluster energies */
-        mipPt_ -= cMipt;
-        
-        int updatedPt = hwPt() - ( c->hwPt()*fraction );
+        mipPt_ += cMipt;
+
+        int updatedPt = hwPt() + (int)(c->hwPt()*fraction);
         setHwPt( updatedPt );
-        
+
         math::PtEtaPhiMLorentzVector updatedP4 ( p4() );
-        updatedP4 -= ( c->p4()*fraction ); 
+        updatedP4 += (c->p4()*fraction);
         setP4( updatedP4 );
-      
+
+        constituents_.push_back( c );
+        constituentsFraction_.push_back( fraction );
+
       }
 
-    }
+      void removeConstituent( const edm::Ptr<C>& c, bool updateCentre=true ){
 
-    bool valid() const { return valid_; }
-    void setValid(bool valid) { valid_ = valid; }
-    
-    double mipPt() const { return mipPt_; }
-    double seedMipPt() const { return seedMipPt_; }
-    uint32_t detId() const { return detId_.rawId(); }
-
-
-    /* distance in 'cm' */
-    double distance( const l1t::HGCalTriggerCell &tc ) const { return ( tc.position() - centre_ ).mag(); }
-
-    const GlobalPoint& position() const { return centre_; } 
-    const GlobalPoint& centre() const { return centre_; }
-    const GlobalPoint& centreProj() const { return centreProj_; }
-
-    // FIXME: will need to fix places where the shapes are directly accessed
-    // Right now keep shapes() getter as non-const 
-    ClusterShapes& shapes() {return shapes_;}
-    double hOverE() const
-    {
-      double pt_em = 0.;
-      double pt_had = 0.;
-      double hOe = 0.;
-
-      for(const auto& constituent : constituents())
-      {
-        switch( constituent->subdetId() )
+        /* remove the pointer to c from the std::vector */
+        double fraction=0;
+        bool constituentRemoved=false;
+        for( unsigned i=0; i<constituents_.size(); i++ )
         {
-        case HGCEE:
-          pt_em += constituent->pt();
-          break;
-        case HGCHEF:
-          pt_had += constituent->pt();
-          break;
-        case HGCHEB:
-          pt_had += constituent->pt();
-          break;
-        default:
-          break;
+          if( constituents_[i] == c )
+          {
+            // remove constituent and get its fraction in the cluster
+            constituents_.erase( constituents_.begin()+i );
+            fraction = constituentsFraction_.at(i);
+            constituentsFraction_.erase( constituentsFraction_.begin()+i );
+            constituentRemoved=true;
+            break;
+          }
         }
+
+        /* if a constituent has been removed update cluster info */
+        if( constituentRemoved ) {
+
+          /* update cluster positions (IF requested) */
+          double cMipt = c->mipPt()*fraction;
+          if( updateCentre ){
+            Basic3DVector<float> constituentCentre( c->position() );
+            Basic3DVector<float> clusterCentre( centre_ );
+
+            clusterCentre = clusterCentre*mipPt_ - constituentCentre*cMipt;
+            if( (mipPt_ - cMipt ) > 0 )
+            {
+              clusterCentre /= ( mipPt_ - cMipt ) ;
+            }
+            centre_ = GlobalPoint( clusterCentre );
+
+            if( clusterCentre.z() != 0 )
+            {
+              centreProj_= GlobalPoint( clusterCentre / clusterCentre.z() );
+            }
+
+          }
+
+          /* update cluster energies */
+          mipPt_ -= cMipt;
+
+          int updatedPt = hwPt() - ( c->hwPt()*fraction );
+          setHwPt( updatedPt );
+
+          math::PtEtaPhiMLorentzVector updatedP4 ( p4() );
+          updatedP4 -= ( c->p4()*fraction );
+          setP4( updatedP4 );
+
+        }
+
       }
-      if(pt_em>0) hOe = pt_had / pt_em ;
-      else hOe = -1.;
-      return hOe;
-    }
 
-    uint32_t subdetId() const {return detId_.subdetId();} 
-    uint32_t layer() const {return detId_.layer();}
-    int32_t zside() const {return detId_.zside();}
+      bool valid() const { return valid_; }
+      void setValid(bool valid) { valid_ = valid; }
 
-
-    //shower shape
-
-    int showerLength() const { return showerLength_; }
-    int coreShowerLength() const { return coreShowerLength_; }
-    int firstLayer() const { return firstLayer_; }
-    int maxLayer() const { return maxLayer_; }
-    float eMax() const { return eMax_; }
-    float sigmaEtaEtaMax() const { return sigmaEtaEtaMax_; }
-    float sigmaPhiPhiMax() const { return sigmaPhiPhiMax_; }
-    float sigmaEtaEtaTot() const { return sigmaEtaEtaTot_; }
-    float sigmaPhiPhiTot() const { return sigmaPhiPhiTot_; }
-    float sigmaZZ() const { return sigmaZZ_; }
-    float sigmaRRTot() const { return sigmaRRTot_; }
-    float sigmaRRMax() const { return sigmaRRMax_; }
-    float sigmaRRMean() const { return sigmaRRMean_; }
-
-    void set_showerLength(int showerLength) { showerLength_ = showerLength;}
-    void set_coreShowerLength(int coreShowerLength) { coreShowerLength_ = coreShowerLength;}
-    void set_firstLayer(int firstLayer) { firstLayer_ = firstLayer;}
-    void set_maxLayer(int maxLayer) { maxLayer_ = maxLayer;}
-    void set_eMax(float eMax) { eMax_ = eMax;}
-    void set_sigmaEtaEtaMax(float sigmaEtaEtaMax) { sigmaEtaEtaMax_ = sigmaEtaEtaMax;}
-    void set_sigmaEtaEtaTot(float sigmaEtaEtaTot) { sigmaEtaEtaTot_ = sigmaEtaEtaTot;}
-    void set_sigmaPhiPhiMax(float sigmaPhiPhiMax) { sigmaPhiPhiMax_ = sigmaPhiPhiMax;}
-    void set_sigmaPhiPhiTot(float sigmaPhiPhiTot) { sigmaPhiPhiTot_ = sigmaPhiPhiTot;}
-    void set_sigmaRRMax(float sigmaRRMax) { sigmaRRMax_ = sigmaRRMax;}
-    void set_sigmaRRTot(float sigmaRRTot) { sigmaRRTot_ = sigmaRRTot;}
-    void set_sigmaRRMean(float sigmaRRMean) { sigmaRRMean_ = sigmaRRMean;}
-    void set_sigmaZZ(float sigmaZZ) { sigmaZZ_ = sigmaZZ;}
-    
-    /* operators */
-    bool operator<(const HGCalClusterT<C>& cl) const {return mipPt() < cl.mipPt();}
-    bool operator>(const HGCalClusterT<C>& cl) const  { return  cl<*this;   }
-    bool operator<=(const HGCalClusterT<C>& cl) const { return !(cl>*this); }
-    bool operator>=(const HGCalClusterT<C>& cl) const { return !(cl<*this); }
+      double mipPt() const { return mipPt_; }
+      double seedMipPt() const { return seedMipPt_; }
+      uint32_t detId() const { return detId_.rawId(); }
 
 
-  private:
-    
-    bool valid_;
-    HGCalDetId detId_;   
-    
-    std::vector<edm::Ptr<C>> constituents_;  /* ???? possibly change this in something like     */
-    std::vector<double> constituentsFraction_;  /*    vector<pair<edm::Ptr<C>,float>>    ????  */
+      /* distance in 'cm' */
+      double distance( const l1t::HGCalTriggerCell &tc ) const { return ( tc.position() - centre_ ).mag(); }
 
-    GlobalPoint centre_;
-    GlobalPoint centreProj_; // centre projected onto the first HGCal layer
+      const GlobalPoint& position() const { return centre_; } 
+      const GlobalPoint& centre() const { return centre_; }
+      const GlobalPoint& centreProj() const { return centreProj_; }
 
-    double mipPt_;
-    double seedMipPt_;
+      // FIXME: will need to fix places where the shapes are directly accessed
+      // Right now keep shapes() getter as non-const 
+      ClusterShapes& shapes() {return shapes_;}
+      double hOverE() const
+      {
+        double pt_em = 0.;
+        double pt_had = 0.;
+        double hOe = 0.;
 
-    //shower shape
+        for(const auto& constituent : constituents())
+        {
+          switch( constituent->subdetId() )
+          {
+            case HGCEE:
+              pt_em += constituent->pt();
+              break;
+            case HGCHEF:
+              pt_had += constituent->pt();
+              break;
+            case HGCHEB:
+              pt_had += constituent->pt();
+              break;
+            default:
+              break;
+          }
+        }
+        if(pt_em>0) hOe = pt_had / pt_em ;
+        else hOe = -1.;
+        return hOe;
+      }
 
-    int showerLength_;
-    int coreShowerLength_;
-    int firstLayer_;
-    int maxLayer_;
-    float eMax_;
-    float sigmaEtaEtaMax_;
-    float sigmaPhiPhiMax_;
-    float sigmaRRMax_;
-    float sigmaEtaEtaTot_;
-    float sigmaPhiPhiTot_;
-    float sigmaRRTot_;
-    float sigmaRRMean_;
-    float sigmaZZ_;
+      uint32_t subdetId() const {return detId_.subdetId();} 
+      uint32_t layer() const {return detId_.layer();}
+      int32_t zside() const {return detId_.zside();}
 
-    ClusterShapes shapes_;
+
+      //shower shape
+
+      int showerLength() const { return showerLength_; }
+      int coreShowerLength() const { return coreShowerLength_; }
+      int firstLayer() const { return firstLayer_; }
+      int maxLayer() const { return maxLayer_; }
+      float eMax() const { return eMax_; }
+      float sigmaEtaEtaMax() const { return sigmaEtaEtaMax_; }
+      float sigmaPhiPhiMax() const { return sigmaPhiPhiMax_; }
+      float sigmaEtaEtaTot() const { return sigmaEtaEtaTot_; }
+      float sigmaPhiPhiTot() const { return sigmaPhiPhiTot_; }
+      float sigmaZZ() const { return sigmaZZ_; }
+      float sigmaRRTot() const { return sigmaRRTot_; }
+      float sigmaRRMax() const { return sigmaRRMax_; }
+      float sigmaRRMean() const { return sigmaRRMean_; }
+
+      void set_showerLength(int showerLength) { showerLength_ = showerLength;}
+      void set_coreShowerLength(int coreShowerLength) { coreShowerLength_ = coreShowerLength;}
+      void set_firstLayer(int firstLayer) { firstLayer_ = firstLayer;}
+      void set_maxLayer(int maxLayer) { maxLayer_ = maxLayer;}
+      void set_eMax(float eMax) { eMax_ = eMax;}
+      void set_sigmaEtaEtaMax(float sigmaEtaEtaMax) { sigmaEtaEtaMax_ = sigmaEtaEtaMax;}
+      void set_sigmaEtaEtaTot(float sigmaEtaEtaTot) { sigmaEtaEtaTot_ = sigmaEtaEtaTot;}
+      void set_sigmaPhiPhiMax(float sigmaPhiPhiMax) { sigmaPhiPhiMax_ = sigmaPhiPhiMax;}
+      void set_sigmaPhiPhiTot(float sigmaPhiPhiTot) { sigmaPhiPhiTot_ = sigmaPhiPhiTot;}
+      void set_sigmaRRMax(float sigmaRRMax) { sigmaRRMax_ = sigmaRRMax;}
+      void set_sigmaRRTot(float sigmaRRTot) { sigmaRRTot_ = sigmaRRTot;}
+      void set_sigmaRRMean(float sigmaRRMean) { sigmaRRMean_ = sigmaRRMean;}
+      void set_sigmaZZ(float sigmaZZ) { sigmaZZ_ = sigmaZZ;}
+
+      /* operators */
+      bool operator<(const HGCalClusterT<C>& cl) const {return mipPt() < cl.mipPt();}
+      bool operator>(const HGCalClusterT<C>& cl) const  { return  cl<*this;   }
+      bool operator<=(const HGCalClusterT<C>& cl) const { return !(cl>*this); }
+      bool operator>=(const HGCalClusterT<C>& cl) const { return !(cl<*this); }
+
+
+    private:
+
+      bool valid_;
+      HGCalDetId detId_;
+
+      std::vector<edm::Ptr<C>> constituents_;  /* ???? possibly change this in something like     */
+      std::vector<double> constituentsFraction_;  /*    vector<pair<edm::Ptr<C>,float>>    ????  */
+
+      GlobalPoint centre_;
+      GlobalPoint centreProj_; // centre projected onto the first HGCal layer
+
+      double mipPt_;
+      double seedMipPt_;
+
+      //shower shape
+
+      int showerLength_;
+      int coreShowerLength_;
+      int firstLayer_;
+      int maxLayer_;
+      float eMax_;
+      float sigmaEtaEtaMax_;
+      float sigmaPhiPhiMax_;
+      float sigmaRRMax_;
+      float sigmaEtaEtaTot_;
+      float sigmaPhiPhiTot_;
+      float sigmaRRTot_;
+      float sigmaRRMean_;
+      float sigmaZZ_;
+
+      ClusterShapes shapes_;
 
   };
 
