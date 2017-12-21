341640f981feb7b6d5e2f4f2eae4925b0feb852d
jfernan2 <Javier.Fernandez@cern.ch>
2017-12-21 10:36:21 +0100

    Update dturospacker_cfi.py

--- EventFilter/DTRawToDigi/python/dturospacker_cfi.py
+++ EventFilter/DTRawToDigi/python/dturospacker_cfi.py
@@ -2,6 +2,6 @@ import FWCore.ParameterSet.Config as cms
 
 dturospacker = cms.EDProducer("DTuROSDigiToRaw",
                                 DTDigi_Source = cms.InputTag("simMuonDTDigis"),
-                                feds     = cms.untracked.vint32( 1368, 1369, 1370,),
+                                feds     = cms.untracked.vint32( 1369, 1370, 1371,),
                                 debug = cms.untracked.bool(True),
                              )
