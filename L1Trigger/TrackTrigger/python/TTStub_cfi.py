90f8155bf0d4f4ee5e6986c655eb5e5ecf6be6bc
Seb <viret@in2p3.fr>
2017-12-22 12:57:03 +0100

    Addressing PR comments

--- L1Trigger/TrackTrigger/python/TTStub_cfi.py
+++ L1Trigger/TrackTrigger/python/TTStub_cfi.py
@@ -8,7 +8,9 @@ TTStubsFromPhase2TrackerDigis = cms.EDProducer("TTStubBuilder_Phase2TrackerDigi_
     MPAlimit      = cms.uint32(5),   # MPA chip limit (in stubs/chip/2BX)
     SS5GCIClimit  = cms.uint32(16),  # 2S 5G chip limit (in stubs/CIC/8BX)
     PS5GCIClimit  = cms.uint32(17),  # PS 5G chip limit (in stubs/CIC/8BX)
-    PS10GCIClimit = cms.uint32(35)   # PS 10G chip limit (in stubs/CIC/8BX)
+    PS10GCIClimit = cms.uint32(35),  # PS 10G chip limit (in stubs/CIC/8BX)
+    TEDD1Max10GRing = cms.uint32(3), # How many rings are PS10G in TEDD1 disks (1/2)? 
+    TEDD2Max10GRing = cms.uint32(0)  # How many rings are PS10G in TEDD2 disks (3/4/5)? 
 )
 
 
