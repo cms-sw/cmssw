b08da57b1aaf0faab4362824baf38b48e81fcffe
Sunanda <sunanda.banerjee@cern.ch>
2017-12-28 17:08:14 +0100

    Make a plugin application updated with latest convention

--- Calibration/HcalCalibAlgos/macros/CalibSort.C
+++ Calibration/HcalCalibAlgos/macros/CalibSort.C
@@ -67,6 +67,21 @@ struct recordLess {
   }
 };
 
+struct recordEvent {
+  recordEvent(unsigned int ser=0, unsigned int ent=0, unsigned int r=0,
+	      unsigned int ev=0) : serial_(ser), entry_(ent), run_(r),
+				   event_(ev) {}
+
+  unsigned int serial_, entry_, run_, event_;
+};
+
+struct recordEventLess {
+  bool operator() (const recordEvent& a, const recordEvent& b) {
+    return ((a.run_ < b.run_) || 
+	    ((a.run_ == b.run_) && (a.event_ <  b.event_)));
+  }
+};
+
 class CalibSort {
 public :
   CalibSort(std::string fname, std::string dirname="HcalIsoTrkAnalyzer",
@@ -81,8 +96,8 @@ public :
   virtual void               Show(Long64_t entry = -1);
 private:
 
-  TTree          *fChain;   //!pointer to the analyzed TTree or TChain
-  Int_t           fCurrent; //!current Tree number in a TChain
+  TTree                     *fChain;  //!pointer to the analyzed TTree or TChain
+  Int_t                      fCurrent; //!current Tree number in a TChain
 
   // Declaration of leaf types
   Int_t                      t_Run;
@@ -362,6 +377,217 @@ void CalibSort::Loop() {
  	    << runHigh << std::endl;
 }
 
+class CalibSortEvent {
+public :
+  TTree                     *fChain;   //!pointer to the analyzed TTree/TChain
+  Int_t                      fCurrent; //!current Tree number in a TChain
+
+  // Declaration of leaf types
+  UInt_t                     t_RunNo;
+  UInt_t                     t_EventNo;
+  Int_t                      t_Tracks;
+  Int_t                      t_TracksProp;
+  Int_t                      t_TracksSaved;
+  Int_t                      t_TracksLoose;
+  Int_t                      t_TracksTight;
+  Int_t                      t_allvertex;
+  Bool_t                     t_TrigPass;
+  Bool_t                     t_TrigPassSel;
+  Bool_t                     t_L1Bit;
+  std::vector<Bool_t>       *t_hltbits;
+  std::vector<int>          *t_ietaAll;
+  std::vector<int>          *t_ietaGood;
+  std::vector<int>          *t_trackType;
+
+  // List of branches
+  TBranch                   *b_t_RunNo;         //!
+  TBranch                   *b_t_EventNo;       //!
+  TBranch                   *b_t_Tracks;        //!
+  TBranch                   *b_t_TracksProp;    //!
+  TBranch                   *b_t_TracksSaved;   //!
+  TBranch                   *b_t_TracksLoose;   //!
+  TBranch                   *b_t_TracksTight;   //!
+  TBranch                   *b_t_allvertex;     //!
+  TBranch                   *b_t_TrigPass;      //!
+  TBranch                   *b_t_TrigPassSel;   //!
+  TBranch                   *b_t_L1Bit;         //!
+  TBranch                   *b_t_hltbits;       //!
+  TBranch                   *b_t_ietaAll;       //!
+  TBranch                   *b_t_ietaGood;      //!
+  TBranch                   *b_t_trackType;     //!
+
+  CalibSortEvent(std::string fname, std::string dirname, std::string prefix="",
+		 bool append=false);
+  virtual ~CalibSortEvent();
+  virtual Int_t    Cut(Long64_t entry);
+  virtual Int_t    GetEntry(Long64_t entry);
+  virtual Long64_t LoadTree(Long64_t entry);
+  virtual void     Init(TTree *tree);
+  virtual void     Loop();
+  virtual Bool_t   Notify();
+  virtual void     Show(Long64_t entry = -1);
+
+private:
+  std::string      fname_, dirnm_, prefix_;
+  bool             append_;
+};
+
+CalibSortEvent::CalibSortEvent(std::string fname, std::string dirnm,
+			       std::string prefix, bool append) :
+  fname_(fname), dirnm_(dirnm), prefix_(prefix), append_(append) {
+
+  TFile      *file = new TFile(fname.c_str());
+  TDirectory *dir  = (TDirectory*)file->FindObjectAny(dirnm.c_str());
+  std::cout << fname << " file " << file << " " << dirnm << " " << dir << "\n";
+  TTree      *tree = (TTree*)dir->Get("EventInfo");
+  std::cout << "CalibSortEvent " << tree << std::endl;
+  Init(tree);
+}
+
+CalibSortEvent::~CalibSortEvent() {
+  if (!fChain) return;
+  delete fChain->GetCurrentFile();
+}
+
+Int_t CalibSortEvent::GetEntry(Long64_t entry) {
+  // Read contents of entry.
+  if (!fChain) return 0;
+  return fChain->GetEntry(entry);
+}
+
+Long64_t CalibSortEvent::LoadTree(Long64_t entry) {
+  // Set the environment to read one entry
+  if (!fChain) return -5;
+  Long64_t centry = fChain->LoadTree(entry);
+  if (centry < 0) return centry;
+  if (!fChain->InheritsFrom(TChain::Class()))  return centry;
+  TChain *chain = (TChain*)fChain;
+  if (chain->GetTreeNumber() != fCurrent) {
+    fCurrent = chain->GetTreeNumber();
+    Notify();
+  }
+  return centry;
+}
+
+void CalibSortEvent::Init(TTree *tree) {
+  // The Init() function is called when the selector needs to initialize
+  // a new tree or chain. Typically here the branch addresses and branch
+  // pointers of the tree will be set.
+  // It is normally not necessary to make changes to the generated
+  // code, but the routine can be extended by the user if needed.
+  // Init() will be called many times when running on PROOF
+  // (once per file to be processed).
+
+  // Set branch addresses and branch pointers
+  // Set object pointer
+  t_hltbits      = 0;
+  t_ietaAll      = 0;
+  t_ietaGood     = 0;
+  t_trackType    = 0;
+  t_L1Bit        = false;
+  fChain         = tree;
+  fCurrent       = -1;
+  if (!tree) return;
+  fChain->SetMakeClass(1);
+  fChain->SetBranchAddress("t_RunNo",       &t_RunNo,       &b_t_RunNo);
+  fChain->SetBranchAddress("t_EventNo",     &t_EventNo,     &b_t_EventNo);
+  fChain->SetBranchAddress("t_Tracks",      &t_Tracks,      &b_t_Tracks);
+  fChain->SetBranchAddress("t_TracksProp",  &t_TracksProp,  &b_t_TracksProp);
+  fChain->SetBranchAddress("t_TracksSaved", &t_TracksSaved, &b_t_TracksSaved);
+  fChain->SetBranchAddress("t_TracksLoose", &t_TracksLoose, &b_t_TracksLoose);
+  fChain->SetBranchAddress("t_TracksTight", &t_TracksTight, &b_t_TracksTight);
+  fChain->SetBranchAddress("t_allvertex",   &t_allvertex,   &b_t_allvertex);
+  fChain->SetBranchAddress("t_TrigPass",    &t_TrigPass,    &b_t_TrigPass);
+  fChain->SetBranchAddress("t_TrigPassSel", &t_TrigPassSel, &b_t_TrigPassSel);
+  fChain->SetBranchAddress("t_L1Bit",       &t_L1Bit,       &b_t_L1Bit);
+  fChain->SetBranchAddress("t_hltbits",     &t_hltbits,     &b_t_hltbits);
+  fChain->SetBranchAddress("t_ietaAll",     &t_ietaAll,     &b_t_ietaAll);
+  fChain->SetBranchAddress("t_ietaGood",    &t_ietaGood,    &b_t_ietaGood);
+  fChain->SetBranchAddress("t_trackType",   &t_trackType,   &b_t_trackType);
+  Notify();
+}
+
+Bool_t CalibSortEvent::Notify() {
+  // The Notify() function is called when a new file is opened. This
+  // can be either for a new TTree in a TChain or when when a new TTree
+  // is started when using PROOF. It is normally not necessary to make changes
+  // to the generated code, but the routine can be extended by the
+  // user if needed. The return value is currently not used.
+  
+  return kTRUE;
+}
+
+void CalibSortEvent::Show(Long64_t entry) {
+  // Print contents of entry.
+  // If entry is not specified, print current entry
+  if (!fChain) return;
+  fChain->Show(entry);
+}
+
+Int_t CalibSortEvent::Cut(Long64_t) {
+  // This function may be called from Loop.
+  // returns  1 if entry is accepted.
+  // returns -1 otherwise.
+  return 1;
+}
+
+void CalibSortEvent::Loop() {
+  //   In a ROOT session, you can do:
+  //      Root > .L CalibMonitor.C+g
+  //      Root > CalibSortEvent t
+  //      Root > t.GetEntry(12); // Fill t data members with entry number 12
+  //      Root > t.Show();       // Show values of entry 12
+  //      Root > t.Show(16);     // Read and show values of entry 16
+  //      Root > t.Loop();       // Loop on all entries
+  //
+
+  //     This is the loop skeleton where:
+  //    jentry is the global entry number in the chain
+  //    ientry is the entry number in the current Tree
+  //  Note that the argument to GetEntry must be:
+  //    jentry for TChain::GetEntry
+  //    ientry for TTree::GetEntry and TBranch::GetEntry
+  //
+  //       To read only selected branches, Insert statements like:
+  // METHOD1:
+  //    fChain->SetBranchStatus("*",0);  // disable all branches
+  //    fChain->SetBranchStatus("branchname",1);  // activate branchname
+  // METHOD2: replace line
+  //    fChain->GetEntry(jentry);       //read all branches
+  //by  b_branchname->GetEntry(ientry); //read only this branch
+  if (fChain == 0) return;
+
+  std::ofstream fileout;
+  if (!append_) {
+    fileout.open("runevents.txt", std::ofstream::out);
+    std::cout << "Opens runevents.txt in output mode\n";
+  } else {
+    fileout.open("runevents.txt", std::ofstream::app);
+    std::cout << "Opens runevents.txt in append mode\n";
+  }
+  fileout << "Input file: " << fname_ << " Directory: " << dirnm_ 
+	  << " Prefix: " << prefix_ << "\n";
+  UInt_t   runLow(99999999), runHigh(0);
+  Long64_t nbytes(0), nb(0), good(0);
+  Long64_t nentries = fChain->GetEntriesFast();
+  for (Long64_t jentry=0; jentry<nentries;jentry++) {
+    Long64_t ientry = LoadTree(jentry);
+    if (ientry < 0) break;
+    nb = fChain->GetEntry(jentry);   nbytes += nb;
+    if (t_RunNo > 200000 && t_RunNo < 800000) {
+      if (t_RunNo < runLow)  runLow  = t_RunNo;
+      if (t_RunNo > runHigh) runHigh = t_RunNo;
+    }
+    good++;
+    fileout << good << " " << jentry << " " << t_RunNo  << " " << t_EventNo 
+	    << std::endl;
+  }
+  fileout.close();
+  std::cout << "Writes " << good << " events in the file events.txt from "
+	    << nentries << " entries in run range " << runLow << ":"
+ 	    << runHigh << std::endl;
+}
+
 void readRecords(std::string fname, std::vector<record>& records, bool debug) {
   records.clear();
   ifstream infile (fname.c_str());
@@ -443,4 +669,83 @@ void findDuplicate(std::string infile, std::string outfile, bool debug=false) {
   readRecords(infile, records, debug);
   sort(records,debug);
   duplicate(outfile, records, debug);
+}
+
+void readRecordEvents(std::string fname, std::vector<recordEvent>& records,
+		      bool debug) {
+  records.clear();
+  ifstream infile (fname.c_str());
+  if (!infile.is_open()) {
+    std::cout << "Cannot open " << fname << std::endl;
+  } else {
+    while (1) {
+      unsigned int ser, ent, r, ev;
+      infile >> ser >> ent >> r >> ev;
+      if (!infile.good()) break;
+      recordEvent rec(ser,ent,r,ev);
+      records.push_back(rec);
+    }
+    infile.close();
+  }
+  std::cout << "Reads " << records.size() << " records from " << fname << "\n";
+  if (debug) {
+    for (unsigned int k=0; k<records.size(); ++k) {
+      if (k%100 == 0) 
+	std::cout << "[" << records[k].serial_ << ":" << records[k].entry_ 
+		  << "] " << records[k].run_ << ":" << records[k].event_ 
+		  << std::endl;
+    }
+  }
+}
+
+void sortEvent(std::vector<recordEvent>& records, bool debug) {
+  // Use std::sort
+  std::sort(records.begin(), records.end(), recordEventLess());
+  if (debug) {
+    for (unsigned int k=0; k<records.size(); ++k) {
+      std::cout << "[" << k << ":" << records[k].serial_ << ":" 
+		<< records[k].entry_ << "] " << records[k].run_ << ":" 
+		<< records[k].event_ << std::endl;
+    }
+  }
+}
+
+
+void duplicateEvent(std::string fname, std::vector<recordEvent>& records,
+		    bool debug) {
+  std::ofstream file;
+  file.open(fname.c_str(), std::ofstream::out);
+  std::cout << "List of entry names of duplicate events" << std::endl;
+  int duplicate(0);
+  for (unsigned int k=1; k<records.size(); ++k) {
+    if ((records[k].run_ == records[k-1].run_) &&
+	(records[k].event_ == records[k-1].event_)) {
+      // This is a duplicate event - reject the one with larger serial #
+      if (records[k].entry_ < records[k-1].entry_) {
+	recordEvent swap = records[k-1];
+	records[k-1]= records[k];
+	records[k]  = swap;
+      }
+      if (debug)
+	std::cout << "Serial " << records[k-1].serial_ << ":"  
+		  << records[k].serial_ << " Entry "  
+		  << records[k-1].entry_ << ":" << records[k].entry_ << " Run "
+		  << records[k-1].run_ << ":"  << records[k].run_ << " Event "
+		  << records[k-1].event_ << " " << records[k].event_
+		  << std::endl;
+      file << records[k].entry_ << std::endl;
+      duplicate++;
+    }
+  }
+  file.close();
+  std::cout << "Total # of duplcate events " << duplicate << std::endl;
+}
+
+void findDuplicateEvent(std::string infile, std::string outfile,
+			bool debug=false) {
+
+  std::vector<recordEvent> records;
+  readRecordEvents(infile, records, debug);
+  sortEvent(records,debug);
+  duplicateEvent(outfile, records, debug);
 }
