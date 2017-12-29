c1fb989c24f2ac7b15493bb25c8b5031f3cf0fd3
Dimitrios Tsitsonis <dimitrios.tsitsonis@cern.ch>
2017-12-29 22:08:00 +0100

    Additional Options for uGT & New Timing Plots using eta/phi map

--- DQM/L1TMonitor/python/L1TStage2uGT_cfi.py
+++ DQM/L1TMonitor/python/L1TStage2uGT_cfi.py
@@ -3,5 +3,7 @@ import FWCore.ParameterSet.Config as cms
 l1tStage2uGt = cms.EDAnalyzer("L1TStage2uGT",
     l1tStage2uGtSource = cms.InputTag("gtStage2Digis"),    
     monitorDir = cms.untracked.string("L1T/L1TStage2uGT"),
-    verbose = cms.untracked.bool(False)
+    verbose = cms.untracked.bool(False),
+    firstBXInTrainAlgo = cms.untracked.string("L1_FirstCollisionInTrain"),
+    lastBXInTrainAlgo = cms.untracked.string("L1_LastCollisionInTrain")
 )
