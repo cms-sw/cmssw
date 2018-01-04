3689006f048e71b0ff9a1f02acb76aa74dc1e319
Kevin Pedro <kpedro88@gmail.com>
2018-01-04 13:16:31 -0600

    exclude phase1 tk eras

--- Configuration/Eras/python/Era_Phase2_cff.py
+++ Configuration/Eras/python/Era_Phase2_cff.py
@@ -8,5 +8,7 @@ from Configuration.Eras.Modifier_trackingPhase2PU140_cff import trackingPhase2PU
 from Configuration.Eras.Modifier_phase2_hcal_cff import phase2_hcal
 from Configuration.Eras.Modifier_phase2_hgcal_cff import phase2_hgcal
 from Configuration.Eras.Modifier_phase2_muon_cff import phase2_muon
+from Configuration.Eras.Modifier_phase1Pixel_cff import phase1Pixel
+from Configuration.Eras.Modifier_trackingPhase1_cff import trackingPhase1
 
-Phase2 = cms.ModifierChain(Run3, phase2_common, phase2_tracker, trackingPhase2PU140, phase2_ecal, phase2_hcal, phase2_hgcal, phase2_muon)
+Phase2 = cms.ModifierChain(Run3.copyAndExclude([phase1Pixel,trackingPhase1]), phase2_common, phase2_tracker, trackingPhase2PU140, phase2_ecal, phase2_hcal, phase2_hgcal, phase2_muon)
