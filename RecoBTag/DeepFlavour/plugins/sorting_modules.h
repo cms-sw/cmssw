2b064a39bb2d007669474d76c53241ddc5defe4a
Pablo de Castro <pablodecm@gmail.com>
2018-01-10 11:40:18 +0100

    fix candidate hierarchical sort and remove 1k Exception

--- RecoBTag/DeepFlavour/plugins/sorting_modules.h
+++ RecoBTag/DeepFlavour/plugins/sorting_modules.h
@@ -14,70 +14,72 @@ namespace btagbtvdeep{
 
 template <class T>
 class SortingClass{
+
   public:
+
     SortingClass():sortValA(0),sortValB(0),sortValC(0),t_(0) {}
 
     SortingClass(const T& t, float sortA, float sortB=0, float sortC=0) :
-      sortValA(sortA), sortValB(sortB), sortValC(sortC), t_(t) { }
+      t_(t), sortValA(sortA), sortValB(sortB), sortValC(sortC) {}
 
-    const T& get() const{return t_;}
+    const T& get() const {return t_;}
+ 
+    enum compareResult{cmp_smaller,cmp_greater,cmp_invalid};
 
-    // hierarchical sort
-    static bool compareByABC(SortingClass a, SortingClass b){
-        if(std::isnormal(a.sortValA) && std::isnormal(b.sortValA) && a.sortValA!=b.sortValA){
-            return CompareA(a,b);
-        }
-        else if(!std::isnormal(a.sortValA) && std::isnormal(b.sortValA)){
-            return true;
-        }
-        else if(std::isnormal(a.sortValA) && !std::isnormal(b.sortValA)){
-            return false;
-        }
-        else{
-            if(std::isnormal(a.sortValB) && std::isnormal(b.sortValB) && a.sortValB!=b.sortValB){
-                return CompareB(a,b);
-            }
-            else if(!std::isnormal(a.sortValB) && std::isnormal(b.sortValB)){
-                return true;
-            }
-            else if(std::isnormal(a.sortValB) && !std::isnormal(b.sortValB)){
-                return false;
-            }
-            else{
-                if(std::isnormal(a.sortValC) && std::isnormal(b.sortValC) && a.sortValC!=b.sortValC){
-                    return CompareC(a,b);
-                }
-                else if(!std::isnormal(a.sortValC) && std::isnormal(b.sortValC)){
-                    return true;
-                }
-                else if(std::isnormal(a.sortValC) && !std::isnormal(b.sortValC)){
-                    return false;
-                }
-                else{
-                    return true;
-                }
-            }
-        }
-        return true; //never reached
+    static inline bool isPhysValue(const float& val){
+        if (val!=val)return false;
+        if (std::isinf(val)) return false;
+        return true;
     }
 
-    static bool compareByABCInv(SortingClass a, SortingClass b){
-        return !compareByABC(a,b);
+    static inline compareResult compare(const SortingClass& a, const SortingClass& b,int validx=0){
+        float vala=a.sortValA;
+        float valb=b.sortValA;
+        if(validx==1){
+            vala=a.sortValB;
+            valb=b.sortValB;
+        }else if(validx==2){
+            vala=a.sortValC;
+            valb=b.sortValC;
+        }
+        if(isPhysValue(vala) && isPhysValue(valb) && valb!=vala){
+            if(vala>valb) return cmp_greater;
+            else return cmp_smaller;
+        }
+        if(isPhysValue(vala) && !isPhysValue(valb))
+            return cmp_greater;
+        if(!isPhysValue(vala) && isPhysValue(valb))
+            return cmp_smaller;
+        return cmp_invalid;
+    }
+
+       //hierarchical sort
+    static bool compareByABC(const SortingClass& a, const SortingClass& b){
+
+        compareResult tmpres=compare(a,b,0);
+        if(tmpres==cmp_smaller) return true;
+        if(tmpres==cmp_greater) return false;
+
+        tmpres=compare(a,b,1);
+        if(tmpres==cmp_smaller) return true;
+        if(tmpres==cmp_greater) return false;
+
+        tmpres=compare(a,b,2);
+        if(tmpres==cmp_smaller) return true;
+        if(tmpres==cmp_greater) return false;
+
+        return false;
+
+    }
+
+    static bool compareByABCInv(const SortingClass& a, const SortingClass& b){
+			return compareByABC(b,a);
     }
 
  private:
-    float sortValA,sortValB,sortValC;
-    
-    static bool CompareA(SortingClass a, SortingClass b){
-        return a.sortValA<b.sortValA;
-    }
-    static bool CompareB(SortingClass a, SortingClass b){
-        return a.sortValB<b.sortValB;
-    }
-    static bool CompareC(SortingClass a, SortingClass b){
-        return a.sortValC<b.sortValC;
-    }
+
     T t_;
+    float sortValA,sortValB,sortValC;
 };
 
 std::vector<std::size_t> invertSortingVector(const std::vector<SortingClass<std::size_t> > & in);
