e1b222eb7e29b618eeac1fb7f6bda6d2a0925916
Thomas Reis <thomas.reis@cern.ch>
2018-01-10 15:00:00 +0100

    L1T muon efficiencies for TF regions and ptcut/quality combinations from cfg file.

--- DQMOffline/L1Trigger/python/L1TMuonDQMEfficiency_cff.py
+++ DQMOffline/L1Trigger/python/L1TMuonDQMEfficiency_cff.py
@@ -32,6 +32,59 @@ l1tMuonDQMEfficiency = l1tEfficiencyHarvesting.clone(
     )
 )
 
+from DQMServices.Core.DQMEDHarvester import DQMEDHarvester
+
+numDenDir = "numerators_and_denominators/"
+
+l1tMuonDQMEfficiencyGeneric = DQMEDHarvester("DQMGenericClient",
+    subDirs = cms.untracked.vstring("L1T/L1TMuon/"),
+    efficiency = cms.vstring(),
+    efficiencyProfile = cms.untracked.vstring(
+       "eff_eta_3_qualOpen    'L1 muon efficiency vs. #eta' "+numDenDir+"effNum_eta_3_qualOpen    "+numDenDir+"effDen_eta_3",
+       "eff_eta_7_qualDouble  'L1 muon efficiency vs. #eta' "+numDenDir+"effNum_eta_7_qualDouble  "+numDenDir+"effDen_eta_7",
+       "eff_eta_15_qualDouble 'L1 muon efficiency vs. #eta' "+numDenDir+"effNum_eta_15_qualDouble "+numDenDir+"effDen_eta_15",
+       "eff_eta_25_qualSingle 'L1 muon efficiency vs. #eta' "+numDenDir+"effNum_eta_25_qualSingle "+numDenDir+"effDen_eta_25",
+
+       "eff_phi_3_etaMin0_etaMax2p4_qualOpen 'L1 muon efficiency vs. #phi' "+numDenDir+"effNum_phi_3_etaMin0_etaMax2p4_qualOpen "+numDenDir+"effDen_phi_3_etaMin0_etaMax2p4",
+       "eff_phi_3_etaMin0_etaMax0p83_qualOpen 'L1 muon efficiency vs. #phi' "+numDenDir+"effNum_phi_3_etaMin0_etaMax0p83_qualOpen "+numDenDir+"effDen_phi_3_etaMin0_etaMax0p83",
+       "eff_phi_3_etaMin0p83_etaMax1p24_qualOpen 'L1 muon efficiency vs. #phi' "+numDenDir+"effNum_phi_3_etaMin0p83_etaMax1p24_qualOpen "+numDenDir+"effDen_phi_3_etaMin0p83_etaMax1p24",
+       "eff_phi_3_etaMin1p24_etaMax2p4_qualOpen 'L1 muon efficiency vs. #phi' "+numDenDir+"effNum_phi_3_etaMin1p24_etaMax2p4_qualOpen "+numDenDir+"effDen_phi_3_etaMin1p24_etaMax2p4",
+       "eff_phi_7_etaMin0_etaMax2p4_qualDouble 'L1 muon efficiency vs. #phi' "+numDenDir+"effNum_phi_7_etaMin0_etaMax2p4_qualDouble "+numDenDir+"effDen_phi_7_etaMin0_etaMax2p4",
+       "eff_phi_7_etaMin0_etaMax0p83_qualDouble 'L1 muon efficiency vs. #phi' "+numDenDir+"effNum_phi_7_etaMin0_etaMax0p83_qualDouble "+numDenDir+"effDen_phi_7_etaMin0_etaMax0p83",
+       "eff_phi_7_etaMin0p83_etaMax1p24_qualDouble 'L1 muon efficiency vs. #phi' "+numDenDir+"effNum_phi_7_etaMin0p83_etaMax1p24_qualDouble "+numDenDir+"effDen_phi_7_etaMin0p83_etaMax1p24",
+       "eff_phi_7_etaMin1p24_etaMax2p4_qualDouble 'L1 muon efficiency vs. #phi' "+numDenDir+"effNum_phi_7_etaMin1p24_etaMax2p4_qualDouble "+numDenDir+"effDen_phi_7_etaMin1p24_etaMax2p4",
+       "eff_phi_15_etaMin0_etaMax2p4_qualDouble 'L1 muon efficiency vs. #phi' "+numDenDir+"effNum_phi_15_etaMin0_etaMax2p4_qualDouble "+numDenDir+"effDen_phi_15_etaMin0_etaMax2p4",
+       "eff_phi_15_etaMin0_etaMax0p83_qualDouble 'L1 muon efficiency vs. #phi' "+numDenDir+"effNum_phi_15_etaMin0_etaMax0p83_qualDouble "+numDenDir+"effDen_phi_15_etaMin0_etaMax0p83",
+       "eff_phi_15_etaMin0p83_etaMax1p24_qualDouble 'L1 muon efficiency vs. #phi' "+numDenDir+"effNum_phi_15_etaMin0p83_etaMax1p24_qualDouble "+numDenDir+"effDen_phi_15_etaMin0p83_etaMax1p24",
+       "eff_phi_15_etaMin1p24_etaMax2p4_qualDouble 'L1 muon efficiency vs. #phi' "+numDenDir+"effNum_phi_15_etaMin1p24_etaMax2p4_qualDouble "+numDenDir+"effDen_phi_15_etaMin1p24_etaMax2p4",
+       "eff_phi_25_etaMin0_etaMax2p4_qualSingle 'L1 muon efficiency vs. #phi' "+numDenDir+"effNum_phi_25_etaMin0_etaMax2p4_qualSingle "+numDenDir+"effDen_phi_25_etaMin0_etaMax2p4",
+       "eff_phi_25_etaMin0_etaMax0p83_qualSingle 'L1 muon efficiency vs. #phi' "+numDenDir+"effNum_phi_25_etaMin0_etaMax0p83_qualSingle "+numDenDir+"effDen_phi_25_etaMin0_etaMax0p83",
+       "eff_phi_25_etaMin0p83_etaMax1p24_qualSingle 'L1 muon efficiency vs. #phi' "+numDenDir+"effNum_phi_25_etaMin0p83_etaMax1p24_qualSingle "+numDenDir+"effDen_phi_25_etaMin0p83_etaMax1p24",
+       "eff_phi_25_etaMin1p24_etaMax2p4_qualSingle 'L1 muon efficiency vs. #phi' "+numDenDir+"effNum_phi_25_etaMin1p24_etaMax2p4_qualSingle "+numDenDir+"effDen_phi_25_etaMin1p24_etaMax2p4",
+
+       "eff_pt_3_etaMin0_etaMax2p4_qualOpen 'L1 muon efficiency vs. p_{T}' "+numDenDir+"effNum_pt_3_etaMin0_etaMax2p4_qualOpen "+numDenDir+"effDen_pt_etaMin0_etaMax2p4",
+       "eff_pt_3_etaMin0_etaMax0p83_qualOpen 'L1 muon efficiency vs. p_{T}' "+numDenDir+"effNum_pt_3_etaMin0_etaMax0p83_qualOpen "+numDenDir+"effDen_pt_etaMin0_etaMax0p83",
+       "eff_pt_3_etaMin0p83_etaMax1p24_qualOpen 'L1 muon efficiency vs. p_{T}' "+numDenDir+"effNum_pt_3_etaMin0p83_etaMax1p24_qualOpen "+numDenDir+"effDen_pt_etaMin0p83_etaMax1p24",
+       "eff_pt_3_etaMin1p24_etaMax2p4_qualOpen 'L1 muon efficiency vs. p_{T}' "+numDenDir+"effNum_pt_3_etaMin1p24_etaMax2p4_qualOpen "+numDenDir+"effDen_pt_etaMin1p24_etaMax2p4",
+       "eff_pt_7_etaMin0_etaMax2p4_qualDouble 'L1 muon efficiency vs. p_{T}' "+numDenDir+"effNum_pt_7_etaMin0_etaMax2p4_qualDouble "+numDenDir+"effDen_pt_etaMin0_etaMax2p4",
+       "eff_pt_7_etaMin0_etaMax0p83_qualDouble 'L1 muon efficiency vs. p_{T}' "+numDenDir+"effNum_pt_7_etaMin0_etaMax0p83_qualDouble "+numDenDir+"effDen_pt_etaMin0_etaMax0p83",
+       "eff_pt_7_etaMin0p83_etaMax1p24_qualDouble 'L1 muon efficiency vs. p_{T}' "+numDenDir+"effNum_pt_7_etaMin0p83_etaMax1p24_qualDouble "+numDenDir+"effDen_pt_etaMin0p83_etaMax1p24",
+       "eff_pt_7_etaMin1p24_etaMax2p4_qualDouble 'L1 muon efficiency vs. p_{T}' "+numDenDir+"effNum_pt_7_etaMin1p24_etaMax2p4_qualDouble "+numDenDir+"effDen_pt_etaMin1p24_etaMax2p4",
+       "eff_pt_15_etaMin0_etaMax2p4_qualDouble 'L1 muon efficiency vs. p_{T}' "+numDenDir+"effNum_pt_15_etaMin0_etaMax2p4_qualDouble "+numDenDir+"effDen_pt_etaMin0_etaMax2p4",
+       "eff_pt_15_etaMin0_etaMax0p83_qualDouble 'L1 muon efficiency vs. p_{T}' "+numDenDir+"effNum_pt_15_etaMin0_etaMax0p83_qualDouble "+numDenDir+"effDen_pt_etaMin0_etaMax0p83",
+       "eff_pt_15_etaMin0p83_etaMax1p24_qualDouble 'L1 muon efficiency vs. p_{T}' "+numDenDir+"effNum_pt_15_etaMin0p83_etaMax1p24_qualDouble "+numDenDir+"effDen_pt_etaMin0p83_etaMax1p24",
+       "eff_pt_15_etaMin1p24_etaMax2p4_qualDouble 'L1 muon efficiency vs. p_{T}' "+numDenDir+"effNum_pt_15_etaMin1p24_etaMax2p4_qualDouble "+numDenDir+"effDen_pt_etaMin1p24_etaMax2p4",
+       "eff_pt_25_etaMin0_etaMax2p4_qualSingle 'L1 muon efficiency vs. p_{T}' "+numDenDir+"effNum_pt_25_etaMin0_etaMax2p4_qualSingle "+numDenDir+"effDen_pt_etaMin0_etaMax2p4",
+       "eff_pt_25_etaMin0_etaMax0p83_qualSingle 'L1 muon efficiency vs. p_{T}' "+numDenDir+"effNum_pt_25_etaMin0_etaMax0p83_qualSingle "+numDenDir+"effDen_pt_etaMin0_etaMax0p83",
+       "eff_pt_25_etaMin0p83_etaMax1p24_qualSingle 'L1 muon efficiency vs. p_{T}' "+numDenDir+"effNum_pt_25_etaMin0p83_etaMax1p24_qualSingle "+numDenDir+"effDen_pt_etaMin0p83_etaMax1p24",
+       "eff_pt_25_etaMin1p24_etaMax2p4_qualSingle 'L1 muon efficiency vs. p_{T}' "+numDenDir+"effNum_pt_25_etaMin1p24_etaMax2p4_qualSingle "+numDenDir+"effDen_pt_etaMin1p24_etaMax2p4",
+
+    ),
+    resolution = cms.vstring(),
+    outputFileName = cms.untracked.string(""),
+    verbose = cms.untracked.uint32(0)
+)
+
 # modifications for the pp reference run
 from Configuration.Eras.Modifier_ppRef_2017_cff import ppRef_2017
 ppRef_2017.toModify(l1tMuonDQMEfficiency,
