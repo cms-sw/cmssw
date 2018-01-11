139338dfab5abce55fd45e072406f8d0b8bb7c9b
Fabrizio <Fabrizio.Ferro@ge.infn.it>
2018-01-11 15:14:05 +0100

    removing unneeded set methods

--- DataFormats/CTPPSDigi/interface/CTPPSPixelDataError.h
+++ DataFormats/CTPPSDigi/interface/CTPPSPixelDataError.h
@@ -25,16 +25,13 @@ class CTPPSPixelDataError {
     /// Destructor
     ~CTPPSPixelDataError();
 
-    void setWord32(uint32_t errorWord32);		// function to allow user to input the error word (if 32-bit) after instantiation
-    void setWord64(uint64_t errorWord64);		// function to allow user to input the error word (if 64-bit) after instantiation
-    void setType(int errorType);			// function to allow user to input the error type after instantiation
-    void setFedId(int fedId);			        // function to allow user to input the fedID after instantiation
     void setMessage();				        // function to create an error message based on errorType
 
     inline uint32_t getWord32() const {return errorWord32_;} // the 32-bit word that contains the error information
     inline uint64_t getWord64() const {return errorWord64_;} // the 64-bit word that contains the error information
     inline int getType() const {return errorType_;} 	         // the number associated with the error type (26-31 for ROC number errors, 32-33 for calibration errors)
     inline int getFedId() const {return fedId_;} 	         // the fedId where the error occured
+
     inline const std::string & getMessage() const {return errorMessage_;}     // the error message to be displayed with the error	
 
   private:
