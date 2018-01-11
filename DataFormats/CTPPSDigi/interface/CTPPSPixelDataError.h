05f973e20cdc8979c7107455df3146e53a64a649
Fabrizio <Fabrizio.Ferro@ge.infn.it>
2018-01-11 15:38:46 +0100

    renaming get methods and adding doxygen style comments

--- DataFormats/CTPPSDigi/interface/CTPPSPixelDataError.h
+++ DataFormats/CTPPSDigi/interface/CTPPSPixelDataError.h
@@ -14,28 +14,32 @@
 #include <string>
 
 class CTPPSPixelDataError {
-  public:
+public:
 
-    /// Default constructor
-    CTPPSPixelDataError();
-    /// Constructor for 32-bit error word
-    CTPPSPixelDataError(uint32_t errorWord32, const int errorType, int fedId);
-    /// Constructor with 64-bit error word and type included (header or trailer word)
-    CTPPSPixelDataError(uint64_t errorWord64, const int errorType, int fedId);
-    /// Destructor
-    ~CTPPSPixelDataError();
-
-    void setMessage();				        // function to create an error message based on errorType
-
-    inline uint32_t getWord32() const {return errorWord32_;} // the 32-bit word that contains the error information
-    inline uint64_t getWord64() const {return errorWord64_;} // the 64-bit word that contains the error information
-    inline int getType() const {return errorType_;} 	         // the number associated with the error type (26-31 for ROC number errors, 32-33 for calibration errors)
-    inline int getFedId() const {return fedId_;} 	         // the fedId where the error occured
-
-    inline const std::string & getMessage() const {return errorMessage_;}     // the error message to be displayed with the error	
-
-  private:
-
+  /// Default constructor
+  CTPPSPixelDataError();
+  /// Constructor for 32-bit error word
+  CTPPSPixelDataError(uint32_t errorWord32, const int errorType, int fedId);
+  /// Constructor with 64-bit error word and type included (header or trailer word)
+  CTPPSPixelDataError(uint64_t errorWord64, const int errorType, int fedId);
+  /// Destructor
+  ~CTPPSPixelDataError();
+  
+/// create an error message based on errorType
+  void setMessage();				        
+/// the 32-bit word that contains the error information
+  inline uint32_t errorWord32() const {return errorWord32_;} 
+/// the 64-bit word that contains the error information
+  inline uint64_t errorWord64() const {return errorWord64_;} 
+  /// the number associated with the error type (26-31 for ROC number errors, 32-33 for calibration errors)
+  inline int errorType() const {return errorType_;} 	    
+  /// the fedId where the error occured    
+  inline int fedId() const {return fedId_;} 	        
+  /// the error message to be displayed with the error	
+  inline const std::string & errorMessage() const {return errorMessage_;}    
+  
+private:
+	
     uint64_t errorWord64_;
     uint32_t errorWord32_; 
     int errorType_;
@@ -46,7 +50,7 @@ class CTPPSPixelDataError {
 
 // Comparison operators
 inline bool operator<( const CTPPSPixelDataError& one, const CTPPSPixelDataError& other) {
-  return one.getFedId() < other.getFedId();
+  return one.fedId() < other.fedId();
 }
 
 #endif
