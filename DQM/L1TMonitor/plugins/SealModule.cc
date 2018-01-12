e90e5671cb8574028f632104dacc72d91d57cc53
Dimitrios Tsitsonis <dimitrios.tsitsonis@cern.ch>
2018-01-12 12:02:55 +0100

    New Additional Options for uGT & New Timing Plots using eta/phi map

--- DQM/L1TMonitor/plugins/SealModule.cc
+++ DQM/L1TMonitor/plugins/SealModule.cc
@@ -45,6 +45,9 @@ DEFINE_FWK_MODULE(L1TStage2MuonComp);
 #include <DQM/L1TMonitor/interface/L1TStage2RegionalMuonCandComp.h>
 DEFINE_FWK_MODULE(L1TStage2RegionalMuonCandComp);
 
+#include <DQM/L1TMonitor/interface/L1TObjectsTiming.h>
+DEFINE_FWK_MODULE(L1TObjectsTiming);
+
 #include <DQM/L1TMonitor/interface/L1TStage2uGT.h>
 DEFINE_FWK_MODULE(L1TStage2uGT);
 
@@ -110,8 +113,8 @@ DEFINE_FWK_MODULE(L1TRate);
 #include "DQM/L1TMonitor/interface/L1TBPTX.h"
 DEFINE_FWK_MODULE(L1TBPTX);
 
-#include "DQM/L1TMonitor/interface/L1GtHwValidation.h"
-DEFINE_FWK_MODULE( L1GtHwValidation);
+//#include "DQM/L1TMonitor/interface/L1GtHwValidation.h"
+//DEFINE_FWK_MODULE( L1GtHwValidation);
 
 #include "DQM/L1TMonitor/interface/L1ExtraDQM.h"
 DEFINE_FWK_MODULE(L1ExtraDQM);
