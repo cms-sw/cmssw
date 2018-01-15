b148f8bac98ab51128feea6f94571b8b9095f46a
Antoni Shtipliyski <a.shtipliyski@nullone.com>
2018-01-15 14:22:07 +0100

    Add ASCII file output for l1t-ebec when disagreements are found.

--- L1Trigger/L1TCalorimeter/python/l1tStage2CaloLayer2Comp_cfi.py
+++ L1Trigger/L1TCalorimeter/python/l1tStage2CaloLayer2Comp_cfi.py
@@ -2,6 +2,8 @@ import FWCore.ParameterSet.Config as cms
 
 l1tStage2CaloLayer2Comp = cms.EDProducer(
     'L1TStage2CaloLayer2Comp',
+    dumpTowers = cms.bool(False),
+    dumpWholeEvent = cms.bool(False),
     calol2JetCollectionData    = cms.InputTag("caloStage2Digis", "MP"),
     calol2JetCollectionEmul    = cms.InputTag("simCaloStage2Digis", "MP"),
     calol2EGammaCollectionData = cms.InputTag("caloStage2Digis", "MP"),
