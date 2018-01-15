7d78592d7887baf0536ad90682c5261c61c3b16d
Sam Harper <sam.j.harper@gmail.com>
2018-01-15 16:47:37 +0000

    adapting producing modules to store pf cluster isol in the reco ele/pho

--- Configuration/StandardSequences/python/Eras.py
+++ Configuration/StandardSequences/python/Eras.py
@@ -43,7 +43,7 @@ class Eras (object):
                            'phase2_hgcal', 'phase2_muon', 'phase2_timing',
                            'phase2_timing_layer','phase2_hcal',
                            'trackingLowPU', 'trackingPhase1', 'ctpps_2016', 'trackingPhase2PU140',
-                           'tracker_apv_vfp30_2016', 'run2_miniAOD_80XLegacy', 'run2_nanoAOD_92X',
+                           'tracker_apv_vfp30_2016', 'run2_miniAOD_80XLegacy','run2_miniAOD_9X', 'run2_nanoAOD_92X',
                            'hcalHardcodeConditions', 'hcalSkipPacker']
         internalUseModChains = ['run2_2017_noTrackingModifier']
 
