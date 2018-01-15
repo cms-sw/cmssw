ba6b5e6df49eb84309cf5100d685c23a06fba552
federico de guio <federico.de.guio@cern.ch>
2018-01-15 21:11:27 +0100

    fix import

--- HLTrigger/Configuration/python/customizeHLTforCMSSW.py
+++ HLTrigger/Configuration/python/customizeHLTforCMSSW.py
@@ -18,7 +18,7 @@ from HLTrigger.Configuration.common import *
 #     return process
 
 def customiseFor21810(process):
-    from RecoLocalCalo.CaloTowersCreator.calotowermaker_cfi.py import calotowermaker
+    from RecoLocalCalo.CaloTowersCreator.calotowermaker_cfi import calotowermaker
     for producer in producers_by_type(process, "CaloTowersCreator"):
         producer.HcalPhase = cms.int32(1),
         producer.HESThreshold1 = cms.double(0.8),
