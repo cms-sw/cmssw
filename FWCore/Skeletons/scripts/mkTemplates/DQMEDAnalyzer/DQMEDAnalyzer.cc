8c239fec31083148e14802e73f1f3317e44a2c6c
Marco Rovere <marco.rovere@cern.ch>
2018-01-17 12:02:06 +0100

    Add support for DQMGlobalEDAnalyzer

--- FWCore/Skeletons/scripts/mkTemplates/DQMEDAnalyzer/DQMEDAnalyzer.cc
+++ FWCore/Skeletons/scripts/mkTemplates/DQMEDAnalyzer/DQMEDAnalyzer.cc
@@ -20,20 +20,26 @@
 
 // user include files
 #include "FWCore/Framework/interface/Frameworkfwd.h"
-#include "DQMServices/Core/interface/DQMEDAnalyzer.h"
+@example_stream#include "DQMServices/Core/interface/DQMEDAnalyzer.h"
+@example_global#include "DQMServices/Core/interface/DQMGlobalEDAnalyzer.h"
 
 #include "FWCore/Framework/interface/Event.h"
 #include "FWCore/Framework/interface/MakerMacros.h"
 
 #include "FWCore/ParameterSet/interface/ParameterSet.h"
 
-#include "DQMServices/Core/interface/MonitorElement.h"
+@example_stream#include "DQMServices/Core/interface/MonitorElement.h"
 
 //
 // class declaration
 //
 
-class __class__ : public DQMEDAnalyzer {
+@example_globalstruct Histograms___class__ {
+@example_global ConcurrentMonitorElement histo_;
+@example_global};
+
+@example_streamclass __class__ : public DQMEDAnalyzer {
+@example_globalclass __class__ : public DQMGlobalEDAnalyzer<Histograms___class__> {
    public:
       explicit __class__(const edm::ParameterSet&);
       ~__class__();
@@ -42,14 +48,22 @@ class __class__ : public DQMEDAnalyzer {
 
 
    private:
-      virtual void bookHistograms(DQMStore::IBooker &,
-                                  edm::Run const&,
-                                  edm::EventSetup const&) override;
-      virtual void analyze(const edm::Event&, const edm::EventSetup&) override;
+@example_stream      virtual void bookHistograms(DQMStore::IBooker &,
+@example_stream                                  edm::Run const&,
+@example_stream                                  edm::EventSetup const&) override;
+@example_global      virtual void bookHistograms(DQMStore::ConcurrentBooker &,
+@example_global                                  edm::Run const&,
+@example_global                                  edm::EventSetup const&,
+@example_global                                  Histograms___class__&) const override;
+
+@example_stream      virtual void analyze(const edm::Event&, const edm::EventSetup&) override;
+@example_global      virtual void dqmAnalyze(edm::Event const&,
+@example_global                              edm::EventSetup const&,
+@example_global                              Histograms___class__ const&) const override;
 
       // ----------member data ---------------------------
       std::string folder_;
-      MonitorElement * example_;
+@example_stream      MonitorElement * example_;
 };
 
 //
@@ -82,21 +96,38 @@ __class__::~__class__()
 //
 
 // ------------ method called for each event  ------------
-void
-__class__::analyze(const edm::Event& iEvent, const edm::EventSetup& iSetup)
-{
-   using namespace edm;
-}
+@example_streamvoid
+@example_stream__class__::analyze(const edm::Event& iEvent, const edm::EventSetup& iSetup)
+@example_stream{
+@example_stream   using namespace edm;
+@example_stream}
+@example_stream
+@example_streamvoid
+@example_stream__class__::bookHistograms(DQMStore::IBooker & ibook,
+@example_stream                          edm::Run const& run,
+@example_stream                          edm::EventSetup const & iSetup)
+@example_stream{
+@example_stream  ibook.setCurrentFolder(folder_);
+@example_stream
+@example_stream  ibook.book1D("EXAMPLE", "EXAMPLE", 10, 0., 10.);
+@example_stream}
 
-void
-__class__::bookHistograms(DQMStore::IBooker & ibook,
-                          edm::Run const& run,
-                          edm::EventSetup const & iSetup)
-{
-  ibook.setCurrentFolder(folder_);
+@example_globalvoid
+@example_global__class__::dqmAnalyze(edm::Event const& iEvent, edm::EventSetup const& iSetup,
+@example_global                      Histograms___class__ const & histos) const
+@example_global{
+@example_global}
+@example_global
 
-  ibook.book1D("EXAMPLE", "EXAMPLE", 10, 0., 10.);
-}
+@example_globalvoid
+@example_global__class__::bookHistograms(DQMStore::ConcurrentBooker & ibook,
+@example_global                          edm::Run const & run,
+@example_global                          edm::EventSetup const & iSetup,
+@example_global                          Histograms___class__ & histos) const
+@example_global{
+@example_global  ibook.setCurrentFolder(folder_);
+@example_global  histos.histo_ = ibook.book1D("EXAMPLE", "EXAMPLE", 10, 0., 10.);
+@example_global}
 
 // ------------ method fills 'descriptions' with the allowed parameters for the module  ------------
 void
