4f982777ca789475cfdf65dc9f0976785e90e469
Martin Grunewald <Martin.Grunewald@cern.ch>
2018-01-23 15:03:02 +0100

    HLT DTuROS unpacking

--- HLTrigger/Configuration/python/HLT_HIon_cff.py
+++ HLTrigger/Configuration/python/HLT_HIon_cff.py
@@ -1,13 +1,13 @@
 # hltGetConfiguration --cff --data /dev/CMSSW_10_0_0/HIon --type HIon
 
-# /dev/CMSSW_10_0_0/HIon/V7 (CMSSW_10_0_0_HLT1)
+# /dev/CMSSW_10_0_0/HIon/V8 (CMSSW_10_0_0_HLT1)
 
 import FWCore.ParameterSet.Config as cms
 
 fragment = cms.ProcessFragment( "HLT" )
 
 fragment.HLTConfigVersion = cms.PSet(
-  tableName = cms.string('/dev/CMSSW_10_0_0/HIon/V7')
+  tableName = cms.string('/dev/CMSSW_10_0_0/HIon/V8')
 )
 
 fragment.transferSystem = cms.PSet( 
