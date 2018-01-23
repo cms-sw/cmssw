ccd8b572ff7f257013734e797308245b09dc2f1c
Thomas Reis <thomas.reis@cern.ch>
2018-01-23 11:50:02 +0100

    Convert the L1TStage2CaloLayer2 to being a DQMGlobalEDAnalyzer. Remove unused MEs.

--- DQM/L1TMonitor/interface/L1TStage2CaloLayer2.h
+++ DQM/L1TMonitor/interface/L1TStage2CaloLayer2.h
@@ -7,30 +7,103 @@
 #include "FWCore/MessageLogger/interface/MessageLogger.h"
 
 // DQM
-#include "DQMServices/Core/interface/MonitorElement.h"
-#include "DQMServices/Core/interface/DQMEDAnalyzer.h"
+#include "DQMServices/Core/interface/DQMGlobalEDAnalyzer.h"
+#include "DQMServices/Core/interface/ConcurrentMonitorElement.h"
 
 // stage2 collection
-
 #include "DataFormats/L1Trigger/interface/EGamma.h"
 #include "DataFormats/L1Trigger/interface/Jet.h"
 #include "DataFormats/L1Trigger/interface/EtSum.h"
 #include "DataFormats/L1Trigger/interface/Tau.h"
 
-class L1TStage2CaloLayer2 : public DQMEDAnalyzer {
+namespace calolayer2dqm {
+  struct Histograms {
+    ConcurrentMonitorElement stage2CaloLayer2CenJetEtEtaPhi;
+    ConcurrentMonitorElement stage2CaloLayer2CenJetEta;
+    ConcurrentMonitorElement stage2CaloLayer2CenJetPhi;
+    ConcurrentMonitorElement stage2CaloLayer2CenJetRank;
+    ConcurrentMonitorElement stage2CaloLayer2CenJetOcc;
+    ConcurrentMonitorElement stage2CaloLayer2CenJetBxOcc;
+    ConcurrentMonitorElement stage2CaloLayer2CenJetQual;
+
+    ConcurrentMonitorElement stage2CaloLayer2ForJetEtEtaPhi;
+    ConcurrentMonitorElement stage2CaloLayer2ForJetEta;
+    ConcurrentMonitorElement stage2CaloLayer2ForJetPhi;
+    ConcurrentMonitorElement stage2CaloLayer2ForJetRank;
+    ConcurrentMonitorElement stage2CaloLayer2ForJetOcc;
+    ConcurrentMonitorElement stage2CaloLayer2ForJetBxOcc;
+    ConcurrentMonitorElement stage2CaloLayer2ForJetQual;
+
+    ConcurrentMonitorElement stage2CaloLayer2EGIso;
+
+    ConcurrentMonitorElement stage2CaloLayer2IsoEGEtEtaPhi;
+    ConcurrentMonitorElement stage2CaloLayer2IsoEGEta;
+    ConcurrentMonitorElement stage2CaloLayer2IsoEGPhi;
+    ConcurrentMonitorElement stage2CaloLayer2IsoEGRank;
+    ConcurrentMonitorElement stage2CaloLayer2IsoEGOcc;
+    ConcurrentMonitorElement stage2CaloLayer2IsoEGBxOcc;
+    ConcurrentMonitorElement stage2CaloLayer2IsoEGQual;
+
+    ConcurrentMonitorElement stage2CaloLayer2NonIsoEGEtEtaPhi;
+    ConcurrentMonitorElement stage2CaloLayer2NonIsoEGEta;
+    ConcurrentMonitorElement stage2CaloLayer2NonIsoEGPhi;
+    ConcurrentMonitorElement stage2CaloLayer2NonIsoEGRank;
+    ConcurrentMonitorElement stage2CaloLayer2NonIsoEGOcc;
+    ConcurrentMonitorElement stage2CaloLayer2NonIsoEGBxOcc;
+    ConcurrentMonitorElement stage2CaloLayer2NonIsoEGQual;
+
+    ConcurrentMonitorElement stage2CaloLayer2TauIso;
+
+    ConcurrentMonitorElement stage2CaloLayer2IsoTauEtEtaPhi;
+    ConcurrentMonitorElement stage2CaloLayer2IsoTauEta;
+    ConcurrentMonitorElement stage2CaloLayer2IsoTauPhi;
+    ConcurrentMonitorElement stage2CaloLayer2IsoTauRank;
+    ConcurrentMonitorElement stage2CaloLayer2IsoTauOcc;
+    ConcurrentMonitorElement stage2CaloLayer2IsoTauBxOcc;
+    ConcurrentMonitorElement stage2CaloLayer2IsoTauQual;
+
+    ConcurrentMonitorElement stage2CaloLayer2TauEtEtaPhi;
+    ConcurrentMonitorElement stage2CaloLayer2TauEta;
+    ConcurrentMonitorElement stage2CaloLayer2TauPhi;
+    ConcurrentMonitorElement stage2CaloLayer2TauRank;
+    ConcurrentMonitorElement stage2CaloLayer2TauOcc;
+    ConcurrentMonitorElement stage2CaloLayer2TauBxOcc;
+    ConcurrentMonitorElement stage2CaloLayer2TauQual;
+
+    ConcurrentMonitorElement stage2CaloLayer2EtSumBxOcc;
+    ConcurrentMonitorElement stage2CaloLayer2METRank;
+    ConcurrentMonitorElement stage2CaloLayer2METPhi;
+    ConcurrentMonitorElement stage2CaloLayer2ETTRank;
+    ConcurrentMonitorElement stage2CaloLayer2MHTRank;
+    ConcurrentMonitorElement stage2CaloLayer2MHTPhi;
+    ConcurrentMonitorElement stage2CaloLayer2HTTRank;
+    ConcurrentMonitorElement stage2CaloLayer2METHFRank;
+    ConcurrentMonitorElement stage2CaloLayer2METHFPhi;
+    // ConcurrentMonitorElement stage2CaloLayer2ETTHFRank;
+    ConcurrentMonitorElement stage2CaloLayer2MHTHFRank;
+    ConcurrentMonitorElement stage2CaloLayer2MHTHFPhi;
+    // ConcurrentMonitorElement stage2CaloLayer2HTTHFRank;
+    ConcurrentMonitorElement stage2CaloLayer2MinBiasHFP0;
+    ConcurrentMonitorElement stage2CaloLayer2MinBiasHFM0;
+    ConcurrentMonitorElement stage2CaloLayer2MinBiasHFP1;
+    ConcurrentMonitorElement stage2CaloLayer2MinBiasHFM1;
+    ConcurrentMonitorElement stage2CaloLayer2ETTEMRank;
+    ConcurrentMonitorElement stage2CaloLayer2TowCount;
+  };
+}
+
+class L1TStage2CaloLayer2 : public DQMGlobalEDAnalyzer<calolayer2dqm::Histograms> {
   
  public:
   
   L1TStage2CaloLayer2(const edm::ParameterSet & ps);
-
   ~L1TStage2CaloLayer2() override;
 
  protected:
 
-  void analyze(const edm::Event& e, const edm::EventSetup& c) override;
-  void dqmBeginRun(const edm::Run&, const edm::EventSetup&) override;
-  void bookHistograms(DQMStore::IBooker&, const edm::Run&, const edm::EventSetup&) override ;
-  void beginLuminosityBlock(const edm::LuminosityBlock&, const edm::EventSetup&) override;
+  void dqmAnalyze(const edm::Event& e, const edm::EventSetup& c, calolayer2dqm::Histograms const&) const override;
+  void dqmBeginRun(const edm::Run&, const edm::EventSetup&, calolayer2dqm::Histograms&) const override;
+  void bookHistograms(DQMStore::ConcurrentBooker&, const edm::Run&, const edm::EventSetup&, calolayer2dqm::Histograms&) const override ;
 
  private:
 
@@ -42,86 +115,6 @@ class L1TStage2CaloLayer2 : public DQMEDAnalyzer {
   edm::EDGetTokenT<l1t::EtSumBxCollection> stage2CaloLayer2EtSumToken_;
 
   bool verbose_;
-
-  MonitorElement* stage2CaloLayer2CenJetEtEtaPhi_;
-  MonitorElement* stage2CaloLayer2CenJetEta_;
-  MonitorElement* stage2CaloLayer2CenJetPhi_;
-  MonitorElement* stage2CaloLayer2CenJetRank_;
-  MonitorElement* stage2CaloLayer2CenJetOcc_;
-  MonitorElement* stage2CaloLayer2CenJetBxOcc_;
-  MonitorElement* stage2CaloLayer2CenJetQual_;
-
-  MonitorElement* stage2CaloLayer2ForJetEtEtaPhi_;
-  MonitorElement* stage2CaloLayer2ForJetEta_;
-  MonitorElement* stage2CaloLayer2ForJetPhi_;
-  MonitorElement* stage2CaloLayer2ForJetRank_;
-  MonitorElement* stage2CaloLayer2ForJetOcc_;
-  MonitorElement* stage2CaloLayer2ForJetBxOcc_;
-  MonitorElement* stage2CaloLayer2ForJetQual_;
-
-  MonitorElement* stage2CaloLayer2EGIso_;
-
-  MonitorElement* stage2CaloLayer2IsoEGEtEtaPhi_;
-  MonitorElement* stage2CaloLayer2IsoEGEta_;
-  MonitorElement* stage2CaloLayer2IsoEGPhi_;
-  MonitorElement* stage2CaloLayer2IsoEGRank_;
-  MonitorElement* stage2CaloLayer2IsoEGOcc_;
-  MonitorElement* stage2CaloLayer2IsoEGBxOcc_;
-  MonitorElement* stage2CaloLayer2IsoEGQual_;
-
-  MonitorElement* stage2CaloLayer2NonIsoEGEtEtaPhi_;
-  MonitorElement* stage2CaloLayer2NonIsoEGEta_;
-  MonitorElement* stage2CaloLayer2NonIsoEGPhi_;
-  MonitorElement* stage2CaloLayer2NonIsoEGRank_;
-  MonitorElement* stage2CaloLayer2NonIsoEGOcc_;
-  MonitorElement* stage2CaloLayer2NonIsoEGBxOcc_;
-  MonitorElement* stage2CaloLayer2NonIsoEGQual_;
-
-  MonitorElement* stage2CaloLayer2TauIso_;
-
-  MonitorElement* stage2CaloLayer2IsoTauEtEtaPhi_;
-  MonitorElement* stage2CaloLayer2IsoTauEta_;
-  MonitorElement* stage2CaloLayer2IsoTauPhi_;
-  MonitorElement* stage2CaloLayer2IsoTauRank_;
-  MonitorElement* stage2CaloLayer2IsoTauOcc_;
-  MonitorElement* stage2CaloLayer2IsoTauBxOcc_;
-  MonitorElement* stage2CaloLayer2IsoTauQual_;
-
-  MonitorElement* stage2CaloLayer2TauEtEtaPhi_;
-  MonitorElement* stage2CaloLayer2TauEta_;
-  MonitorElement* stage2CaloLayer2TauPhi_;
-  MonitorElement* stage2CaloLayer2TauRank_;
-  MonitorElement* stage2CaloLayer2TauOcc_;
-  MonitorElement* stage2CaloLayer2TauBxOcc_;
-  MonitorElement* stage2CaloLayer2TauQual_;
-
-  MonitorElement* stage2CaloLayer2EtSumBxOcc_;
-  MonitorElement* stage2CaloLayer2METRank_;
-  MonitorElement* stage2CaloLayer2METPhi_;
-  MonitorElement* stage2CaloLayer2ETTRank_;
-  MonitorElement* stage2CaloLayer2MHTRank_;
-  MonitorElement* stage2CaloLayer2MHTPhi_;
-  MonitorElement* stage2CaloLayer2HTTRank_;
-  MonitorElement* stage2CaloLayer2METHFRank_;
-  MonitorElement* stage2CaloLayer2METHFPhi_;
-  // MonitorElement* stage2CaloLayer2ETTHFRank_;
-  MonitorElement* stage2CaloLayer2MHTHFRank_;
-  MonitorElement* stage2CaloLayer2MHTHFPhi_;
-  // MonitorElement* stage2CaloLayer2HTTHFRank_;
-  MonitorElement* stage2CaloLayer2MinBiasHFP0_;
-  MonitorElement* stage2CaloLayer2MinBiasHFM0_;
-  MonitorElement* stage2CaloLayer2MinBiasHFP1_;
-  MonitorElement* stage2CaloLayer2MinBiasHFM1_;
-  MonitorElement* stage2CaloLayer2ETTEMRank_;
-  MonitorElement* stage2CaloLayer2TowCount_;
-
-  MonitorElement* timingStage2CaloLayer2CenJetBxOcc_;
-  MonitorElement* timingStage2CaloLayer2ForJetBxOcc_;
-  MonitorElement* timingStage2CaloLayer2IsoEGBxOcc_;
-  MonitorElement* timingStage2CaloLayer2NonIsoEGBxOcc_;
-  MonitorElement* timingStage2CaloLayer2IsoTauBxOcc_;
-  MonitorElement* timingStage2CaloLayer2TauBxOcc_;
-  MonitorElement* timingStage2CaloLayer2EtSumBxOcc_;
 };
 
 #endif 
