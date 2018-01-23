12c69c1b3882a9dcd3ca01981e2ac1e2c255ffd2
Sven Dildick <sven.dildick@cern.ch>
2018-01-23 22:31:36 +0100

    extend GEM and ME0 DB object interface

--- CondFormats/GEMObjects/src/headers.h
+++ CondFormats/GEMObjects/src/headers.h
@@ -1,2 +1,8 @@
 #include "CondFormats/GEMObjects/interface/GEMEMap.h"
 #include "CondFormats/GEMObjects/interface/ME0EMap.h"
+
+#include "CondFormats/GEMObjects/interface/GEMDeadStrips.h"
+#include "CondFormats/GEMObjects/interface/GEMMaskedStrips.h"
+
+#include "CondFormats/GEMObjects/interface/ME0DeadStrips.h"
+#include "CondFormats/GEMObjects/interface/ME0MaskedStrips.h"
