b2cc42dc321f1563f16308bd281b571b7afd1d78
amassiro <massironi.andrea@gmail.com>
2018-01-23 20:10:37 +0100

    era 2017 updated with zs thresholds

--- RecoParticleFlow/PFClusterProducer/python/particleFlowZeroSuppressionECAL_cff.py
+++ RecoParticleFlow/PFClusterProducer/python/particleFlowZeroSuppressionECAL_cff.py
@@ -19,22 +19,19 @@ particle_flow_zero_suppression_ECAL = cms.PSet(
     )
 
 _particle_flow_zero_suppression_ECAL_2017 = cms.PSet(
-    thresholds = cms.vdouble(_pfZeroSuppressionThresholds_EB_2017 + _pfZeroSuppressionThresholds_EEminus_2017 + _pfZeroSuppressionThresholds_EEplus_2017
+    thresholds = cms.vdouble(pfZeroSuppressionThresholds_EB + pfZeroSuppressionThresholds_EEminus + pfZeroSuppressionThresholds_EEplus
         )
     )
 
-# 
-# The following commented lines were used in 2017 to set SR@PF eta dependent thresholds
-# as defined in _particle_flow_zero_suppression_ECAL_2017
 #
-# For 2018 the thresholds have been temporarily removed (lowered to 80 MeV in EB and 300 MeV in EE,
+# The thresholds have been temporarily removed (lowered to 80 MeV in EB and 300 MeV in EE,
 # then overseeded by the gathering and seeding PF cluster thresholds)
 # Later, we may need to reintroduce eta dependent thresholds
 # to mitigate the effect of the noise
 #
 
-#from Configuration.Eras.Modifier_run2_ECAL_2017_cff import run2_ECAL_2017
-#run2_ECAL_2017.toReplaceWith(particle_flow_zero_suppression_ECAL, _particle_flow_zero_suppression_ECAL_2017)
+from Configuration.Eras.Modifier_run2_ECAL_2017_cff import run2_ECAL_2017
+run2_ECAL_2017.toReplaceWith(particle_flow_zero_suppression_ECAL, _particle_flow_zero_suppression_ECAL_2017)
 
-#from Configuration.Eras.Modifier_phase2_ecal_cff import phase2_ecal
-#phase2_ecal.toReplaceWith(particle_flow_zero_suppression_ECAL, _particle_flow_zero_suppression_ECAL_2017)
+from Configuration.Eras.Modifier_phase2_ecal_cff import phase2_ecal
+phase2_ecal.toReplaceWith(particle_flow_zero_suppression_ECAL, _particle_flow_zero_suppression_ECAL_2017)
