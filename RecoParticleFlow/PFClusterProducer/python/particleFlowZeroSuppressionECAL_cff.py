9fe6d4a691c0997ad92b92314bdbcaf7c76079b9
amassiro <massironi.andrea@gmail.com>
2018-01-24 18:18:38 +0100

    cleaning

--- RecoParticleFlow/PFClusterProducer/python/particleFlowZeroSuppressionECAL_cff.py
+++ RecoParticleFlow/PFClusterProducer/python/particleFlowZeroSuppressionECAL_cff.py
@@ -5,11 +5,12 @@ pfZeroSuppressionThresholds_EEminus = [0.300]*39
 pfZeroSuppressionThresholds_EEplus = pfZeroSuppressionThresholds_EEminus
 
 #
-# The thresholds have been temporarily removed (lowered to 80 MeV in EB and 300 MeV in EE,
-# then overseeded by the gathering and seeding PF cluster thresholds)
-# Later, we may need to reintroduce eta dependent thresholds
-# to mitigate the effect of the noise
+# These are expected to be adjusted soon, while the thresholds for older setups will remain unchanged.
 #
+_pfZeroSuppressionThresholds_EB_2017 = pfZeroSuppressionThresholds_EB
+_pfZeroSuppressionThresholds_EEminus_2017 = pfZeroSuppressionThresholds_EEminus
+_pfZeroSuppressionThresholds_EEplus_2017 = _pfZeroSuppressionThresholds_EEminus_2017
+
 
 
 particle_flow_zero_suppression_ECAL = cms.PSet(
@@ -17,3 +18,20 @@ particle_flow_zero_suppression_ECAL = cms.PSet(
         )
     )
 
+_particle_flow_zero_suppression_ECAL_2017 = cms.PSet(
+    thresholds = cms.vdouble(_pfZeroSuppressionThresholds_EB_2017 + _pfZeroSuppressionThresholds_EEminus_2017 + _pfZeroSuppressionThresholds_EEplus_2017
+        )
+    )
+
+#
+# The thresholds have been temporarily removed (lowered to 80 MeV in EB and 300 MeV in EE,
+# then overseeded by the gathering and seeding PF cluster thresholds)
+# Later, we may need to reintroduce eta dependent thresholds
+# to mitigate the effect of the noise
+#
+
+from Configuration.Eras.Modifier_run2_ECAL_2017_cff import run2_ECAL_2017
+run2_ECAL_2017.toReplaceWith(particle_flow_zero_suppression_ECAL, _particle_flow_zero_suppression_ECAL_2017)
+
+from Configuration.Eras.Modifier_phase2_ecal_cff import phase2_ecal
+phase2_ecal.toReplaceWith(particle_flow_zero_suppression_ECAL, _particle_flow_zero_suppression_ECAL_2017)
