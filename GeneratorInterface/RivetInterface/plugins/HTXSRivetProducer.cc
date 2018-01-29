98522fc7884da45ac4079b4d01423a67f4335343
perrozzi <perrozzi@cern.ch>
2018-01-29 21:53:37 +0100

    Update HTXSRivetProducer.cc

--- GeneratorInterface/RivetInterface/plugins/HTXSRivetProducer.cc
+++ GeneratorInterface/RivetInterface/plugins/HTXSRivetProducer.cc
@@ -6,7 +6,7 @@
  *
  */
 
-#include "FWCore/Framework/interface/stream/EDProducer.h"
+#include "FWCore/Framework/interface/EDProducer.h"
 #include "FWCore/Framework/interface/Event.h"
 #include "FWCore/ParameterSet/interface/ParameterSet.h"
 #include "FWCore/Framework/interface/Run.h"
