0d2c55f66e8d45771626e10b389bc1e8f53d94a3
Marcel Andre Schneider <marcel.andre.schneider@cern.ch>
2018-01-30 16:57:00 +0100

    Add more method dummies

--- DQMServices/Core/interface/DQMEDAnalyzer.h
+++ DQMServices/Core/interface/DQMEDAnalyzer.h
@@ -5,6 +5,10 @@
 #include "FWCore/Framework/interface/one/EDProducer.h"
 #include "FWCore/Framework/interface/moduleAbilities.h" 
 #include "DQMServices/Core/interface/DQMStore.h"
+
+// TODO: For derived modules that need it
+#include "FWCore/Framework/interface/Run.h"
+#include "FWCore/Framework/interface/LuminosityBlock.h"
 //<<<<<< PUBLIC DEFINES                                                 >>>>>>
 //<<<<<< PUBLIC CONSTANTS                                               >>>>>>
 //<<<<<< PUBLIC TYPES                                                   >>>>>>
@@ -19,6 +23,7 @@ namespace dqmDetails {struct NoCache {};}
 
 class DQMEDAnalyzer
     : public edm::one::EDProducer<edm::one::WatchRuns,
+                                  edm::one::WatchLuminosityBlocks,
                                   edm::Accumulator>
 {
 public:
@@ -26,10 +31,17 @@ public:
   // implicit copy constructor
   // implicit assignment operator
   // implicit destructor
+  virtual void beginJob() {};
   virtual void beginRun(edm::Run const &, edm::EventSetup const&) final;
-  virtual void   endRun(edm::Run const &, edm::EventSetup const&) final {};
+  virtual void   endRun(edm::Run const &, edm::EventSetup const&) {};
   virtual void analyze(const edm::Event&, const edm::EventSetup&) = 0;
-  void accumulate(edm::Event const&, edm::EventSetup const&) override final;
+  virtual void accumulate(edm::Event const&, edm::EventSetup const&) override final;
+  virtual void endJob() {};
+  // TODO: Make sure those get called.
+  virtual void beginLuminosityBlock(edm::LuminosityBlock const&, edm::EventSetup const&) {};
+  virtual void endLuminosityBlock(edm::LuminosityBlock const&, edm::EventSetup const&) {};
+
+
 
   /* (never called, only for stream-modules */ 
   virtual void endRunSummary(edm::Run const&,
