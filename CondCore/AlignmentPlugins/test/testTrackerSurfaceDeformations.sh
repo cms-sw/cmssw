494431852a9a23a11279cba3fbfb82f4fda3e806
mmusich <marco.musich@cern.ch>
2018-02-06 18:19:52 +0100

    improve the surface deformation plugin

--- CondCore/AlignmentPlugins/test/testTrackerSurfaceDeformations.sh
+++ CondCore/AlignmentPlugins/test/testTrackerSurfaceDeformations.sh
@@ -53,7 +53,7 @@ do
  	--plot plot_${i}SurfaceDeformationsComparison \
  	--tag TrackerSurafceDeformations_v1_express \
  	--time_type Run \
-	--iovs '{"start_iov": "298757", "end_iov": "299685"}' \
+	--iovs '{"start_iov": "283024", "end_iov": "299685"}' \
   	--db Prod \
  	--test;
     
@@ -75,5 +75,24 @@ do
  	--test;
     
     mv *.png $W_DIR/results_surfaces/SurfaceDeformationTrackerMapParameter_${i}.png
+
+done
+
+
+#*************************************************************************#
+for i in {0..12}
+do 
+    echo "Processing: $i parameter"
+    
+    getPayloadData.py  \
+ 	--plugin pluginTrackerSurfaceDeformations_PayloadInspector \
+ 	--plot plot_SurfaceDeformationParameter${i}TkMapDelta \
+ 	--tag TrackerSurafceDeformations_v1_express \
+ 	--time_type Run \
+	--iovs '{"start_iov": "283024", "end_iov": "299685"}' \
+  	--db Prod \
+ 	--test;
+    
+    mv *.png $W_DIR/results_surfaces/SurfaceDeformationParameter${i}TkMapDelta.png
 
 done
---
+++ CondCore/AlignmentPlugins/test/testTrackerSurfaceDeformationsDeltaMap.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+# Save current working dir so img can be outputted there later
+W_DIR=$(pwd);
+
+# Set SCRAM architecture var
+SCRAM_ARCH=slc6_amd64_gcc530; 
+export SCRAM_ARCH;
+
+cd $W_DIR;
+source /afs/cern.ch/cms/cmsset_default.sh;
+eval `scram run -sh`;
+
+mkdir -p $W_DIR/results_surfaces
+
+#*************************************************************************#
+for i in {0..12}
+do 
+    echo "Processing: $i parameter"
+    
+    getPayloadData.py  \
+ 	--plugin pluginTrackerSurfaceDeformations_PayloadInspector \
+ 	--plot plot_SurfaceDeformationParameter${i}TkMapDelta \
+ 	--tag TrackerSurafceDeformations_v1_express \
+ 	--time_type Run \
+	--iovs '{"start_iov": "283024", "end_iov": "299685"}' \
+  	--db Prod \
+ 	--test;
+    
+    mv *.png $W_DIR/results_surfaces/SurfaceDeformationParameter${i}TkMapDelta.png
+
+done
