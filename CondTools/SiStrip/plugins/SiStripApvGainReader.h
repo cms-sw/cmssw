e34eca78b69d974a69813b0ff8371ab2a2a0842d
jpriscia <jessica.prisciandaro@cern.ch>
2018-02-06 14:07:13 +0100

    Strip APV gains dump in TTree

--- CondTools/SiStrip/plugins/SiStripApvGainReader.h
+++ CondTools/SiStrip/plugins/SiStripApvGainReader.h
@@ -13,8 +13,14 @@
 #include "FWCore/ServiceRegistry/interface/Service.h"
 #include "FWCore/MessageLogger/interface/MessageLogger.h"
 #include "FWCore/ParameterSet/interface/ParameterSet.h"
+#include "CommonTools/UtilAlgos/interface/TFileService.h"
 
-
+// root objects
+#include "TROOT.h"
+#include "TSystem.h"
+#include "TFile.h"
+#include "TDirectory.h"
+#include "TTree.h"
 
 class SiStripApvGainReader : public edm::EDAnalyzer {
 
@@ -28,6 +34,10 @@ class SiStripApvGainReader : public edm::EDAnalyzer {
   bool printdebug_;
   std::string formatedOutput_;
   uint32_t gainType_;
+  edm::Service<TFileService> fs_;
+  TTree *tree_;
+  int id_=0,  detId_=0, apvId_=0;
+  double gain_=0;
 
 };
 #endif
