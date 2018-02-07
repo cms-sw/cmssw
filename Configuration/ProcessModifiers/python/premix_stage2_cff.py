d5cbfb20e80c1bf41da30f943c5bdf8f939db0d9
Matti Kortelainen <matti.kortelainen@cern.ch>
2018-02-07 09:59:15 +0100

    Add premix_stage1 and premix_stage2 process modifiers

---
+++ Configuration/ProcessModifiers/python/premix_stage2_cff.py
@@ -0,0 +1,5 @@
+import FWCore.ParameterSet.Config as cms
+
+# This modifier is for the premixing stage2, i.e. mixing the premixed pileup library with the signal event
+
+premix_stage2 = cms.Modifier()
