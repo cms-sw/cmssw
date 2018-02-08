ca13c40dcf8f30a67e6bd668bd3325627aca63d4
Giacomo Govi <giacomo.govi@cern.ch>
2018-02-08 15:29:39 +0100

    debug printout for tnsnames.ora

--- CondTools/RunInfo/test/RunInfoStart_O2O_test.sh
+++ CondTools/RunInfo/test/RunInfoStart_O2O_test.sh
@@ -3,5 +3,6 @@ conddb --yes copy runinfo_start_31X_hlt --destdb runinfo_O2O_test.db --o2oTest
 lastRun=`conddb listRuns | tail -2 | awk '{print $1}'`
 echo "last run = $lastRun"
 echo "TNS_ADMIN: $TNS_ADMIN"
+cat /etc/tnsnames.ora | grep cms_orcon_adg
 cmsRun ./src/CondTools/RunInfo/python/RunInfoPopConAnalyzer.py runNumber=$lastRun destinationConnection=sqlite_file:runinfo_O2O_test.db tag=runinfo_start_31X_hlt
 conddb --db runinfo_O2O_test.db list runinfo_start_31X_hlt
