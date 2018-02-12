6e61238eae8aba3300dca17b5d8bba9b6b4abb18
Vincenzo Innocente <vincenzo.innocente@cern.ch>
2018-02-12 12:18:26 +0100

    fix cpu version

--- RecoLocalTracker/SiPixelRecHits/python/SiPixelRecHits_cfi.py
+++ RecoLocalTracker/SiPixelRecHits/python/SiPixelRecHits_cfi.py
@@ -2,7 +2,7 @@ import FWCore.ParameterSet.Config as cms
 
 siPixelRecHits = cms.EDProducer("SiPixelRecHitConverter",
     src = cms.InputTag("siPixelClusters"),
-    CPE = cms.string('Generic'),
+    CPE = cms.string('PixelCPEGeneric'),
     VerboseLevel = cms.untracked.int32(0),
 
 )
