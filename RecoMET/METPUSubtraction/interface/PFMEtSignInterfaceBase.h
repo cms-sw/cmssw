e18ae0629fca33895d78211162db7c7dc9f635c4
Jan Steggemann <jan.steggemann@cern.ch>
2018-02-12 12:55:02 +0100

    Adaptions for changes in PFTau data format

--- RecoMET/METPUSubtraction/interface/PFMEtSignInterfaceBase.h
+++ RecoMET/METPUSubtraction/interface/PFMEtSignInterfaceBase.h
@@ -101,11 +101,27 @@ class PFMEtSignInterfaceBase
       if ( dynamic_cast<const pat::Tau*>(particle) != nullptr ) {
 	const pat::Tau* pfTau = dynamic_cast<const pat::Tau*>(particle);
 	//std::cout << "tau: pt = " << pt << ", eta = " << eta << ", phi = " << phi << std::endl;
-	return pfMEtResolution_->evalPFJet(pfTau->pfJetRef().get());
+	const reco::PFJet* pfJet = dynamic_cast<const reco::PFJet*>(pfTau->pfJetRef().get());
+	if (pfJet != nullptr)
+	  return pfMEtResolution_->evalPFJet(pfJet);
+
+	const pat::Jet* patJet = dynamic_cast<const pat::Jet*>(pfTau->pfJetRef().get());
+	if (patJet != nullptr) {
+	  reco::PFJet pfJet(patJet->p4(), patJet->vertex(), patJet->pfSpecific(), patJet->getJetConstituents());
+	  return pfMEtResolution_->evalPFJet(&pfJet);
+	}
+	else throw cms::Exception("addPFMEtSignObjects")
+	    << "Neither PAT jet nor PF Jet in tau object !!\n";
       } else if ( dynamic_cast<const reco::PFTau*>(particle) != nullptr  ) {
 	const reco::PFTau* pfTau = dynamic_cast<const reco::PFTau*>(particle);
 	//std::cout << "tau: pt = " << pt << ", eta = " << eta << ", phi = " << phi << std::endl;
-	return pfMEtResolution_->evalPFJet(pfTau->jetRef().get());
+	const pat::Jet* patJet = dynamic_cast<const pat::Jet*>(pfTau->jetRef().get());
+	if (patJet != nullptr) {
+	  reco::PFJet pfJet(patJet->p4(), patJet->vertex(), patJet->pfSpecific(), patJet->getJetConstituents());
+	  return pfMEtResolution_->evalPFJet(&pfJet);
+	}
+	else throw cms::Exception("addPFMEtSignObjects")
+	    << "Neither PAT jet nor PF Jet in tau object !!\n";
       } else assert(0);
     } else if ( dynamic_cast<const reco::PFJet*>(particle) != nullptr ||
 		dynamic_cast<const pat::Jet*>(particle) != nullptr ) {
