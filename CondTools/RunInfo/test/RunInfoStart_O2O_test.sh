ce65631f69de323279b057cc95bf57a98573cfba
Giacomo Govi <giacomo.govi@cern.ch>
2018-02-12 16:23:24 +0100

    Added return code

--- CondTools/RunInfo/test/RunInfoStart_O2O_test.sh
+++ CondTools/RunInfo/test/RunInfoStart_O2O_test.sh
@@ -2,7 +2,8 @@
 conddb --yes copy runinfo_start_31X_hlt --destdb runinfo_O2O_test.db --o2oTest
 lastRun=`conddb listRuns | tail -2 | awk '{print $1}'`
 echo "last run = $lastRun"
-echo "TNS_ADMIN: $TNS_ADMIN"
-cat /etc/tnsnames.ora | grep cms_orcon_adg
 cmsRun ./src/CondTools/RunInfo/python/RunInfoPopConAnalyzer.py runNumber=$lastRun destinationConnection=sqlite_file:runinfo_O2O_test.db tag=runinfo_start_31X_hlt
+ret=$?
 conddb --db runinfo_O2O_test.db list runinfo_start_31X_hlt
+echo "return code is $ret"
+exit $ret
