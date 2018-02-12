4bdaf37b3e874c34ef46833b86c1695a34e10dce
Fabrizio <Fabrizio.Ferro@ge.infn.it>
2018-02-12 15:34:09 +0100

    using ideal geometry from DB

---
+++ RecoCTPPS/Configuration/python/recoCTPPS_DB_cff.py
@@ -0,0 +1,13 @@
+import FWCore.ParameterSet.Config as cms
+
+from RecoCTPPS.TotemRPLocal.totemRPLocalReconstruction_DB_cff import *
+from RecoCTPPS.TotemRPLocal.ctppsDiamondLocalReconstruction_DB_cff import *
+from RecoCTPPS.TotemRPLocal.ctppsLocalTrackLiteProducer_cff import ctppsLocalTrackLiteProducer
+from RecoCTPPS.PixelLocal.ctppsPixelLocalReconstruction_DB_cff import *
+
+recoCTPPS = cms.Sequence(
+    totemRPLocalReconstruction *
+    ctppsDiamondLocalReconstruction *
+    ctppsPixelLocalReconstruction *
+    ctppsLocalTrackLiteProducer
+)
