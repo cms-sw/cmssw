2af530d37d4620098b9b13a7ee107191fcee40d2
Shervin <shervin@cern.ch>
2018-02-13 12:54:02 +0100

    python

--- EgammaAnalysis/ElectronTools/python/calibratedElectronsRun2_cfi.py
+++ EgammaAnalysis/ElectronTools/python/calibratedElectronsRun2_cfi.py
@@ -1,41 +1,28 @@
 import FWCore.ParameterSet.Config as cms
 
-correctionType = "80Xapproval"
-files = {"Prompt2015":"EgammaAnalysis/ElectronTools/data/ScalesSmearings/74X_Prompt_2015",
-         "76XReReco" :"EgammaAnalysis/ElectronTools/data/ScalesSmearings/76X_16DecRereco_2015_Etunc",
-         "80Xapproval" : "EgammaAnalysis/ElectronTools/data/ScalesSmearings/80X_ichepV1_2016_ele"}
+from EgammaAnalysis.ElectronTools.calibrationTablesRun2 import correctionType
+from EgammaAnalysis.ElectronTools.calibrationTablesRun2 import files
 
 calibratedElectrons = cms.EDProducer("CalibratedElectronProducerRun2",
 
                                      # input collections
                                      electrons = cms.InputTag('gedGsfElectrons'),
-                                     gbrForestName = cms.string("gedelectron_p4combination_25ns"),
+                                     gbrForestName = cms.vstring('electron_eb_ECALTRK_lowpt', 'electron_eb_ECALTRK',
+                                                                 'electron_ee_ECALTRK_lowpt', 'electron_ee_ECALTRK',
+                                                                 'electron_eb_ECALTRK_lowpt_var', 'electron_eb_ECALTRK_var',
+                                                                 'electron_ee_ECALTRK_lowpt_var', 'electron_ee_ECALTRK_var'),
 
                                      # data or MC corrections
                                      # if isMC is false, data corrections are applied
                                      isMC = cms.bool(False),
+                                     autoDataType = cms.bool(True),
     
                                      # set to True to get special "fake" smearing for synchronization. Use JUST in case of synchronization
                                      isSynchronization = cms.bool(False),
                                      
-                                     correctionFile = cms.string(files[correctionType])
+                                     correctionFile = cms.string(files[correctionType]),
+                                     recHitCollectionEB = cms.InputTag('reducedEcalRecHitsEB'),
+                                     recHitCollectionEE = cms.InputTag('reducedEcalRecHitsEE')
+                                     
                                      )
-
-
-calibratedPatElectrons = cms.EDProducer("CalibratedPatElectronProducerRun2",
-                                        
-                                        # input collections
-                                        electrons = cms.InputTag('slimmedElectrons'),
-                                        gbrForestName = cms.string("gedelectron_p4combination_25ns"),
-                                        
-                                        # data or MC corrections
-                                        # if isMC is false, data corrections are applied
-                                        isMC = cms.bool(False),
-                                        
-                                        # set to True to get special "fake" smearing for synchronization. Use JUST in case of synchronization
-                                        isSynchronization = cms.bool(False),
-
-                                        correctionFile = cms.string(files[correctionType])
-                                       )
-
 
