8a9ef7288be49404372ed7149eaa6b0d12adf773
Shervin <shervin@cern.ch>
2018-02-13 12:54:02 +0100

    python

--- EgammaAnalysis/ElectronTools/python/calibratedPhotonsRun2_cfi.py
+++ EgammaAnalysis/ElectronTools/python/calibratedPhotonsRun2_cfi.py
@@ -1,25 +1,7 @@
-
 import FWCore.ParameterSet.Config as cms
 
-correctionType = "80Xapproval"
-files = {"Prompt2015":"EgammaAnalysis/ElectronTools/data/ScalesSmearings/74X_Prompt_2015",
-         "76XReReco" :"EgammaAnalysis/ElectronTools/data/ScalesSmearings/76X_16DecRereco_2015_Etunc",
-         "80Xapproval" : "EgammaAnalysis/ElectronTools/data/ScalesSmearings/80X_ichepV2_2016_pho"}
-
-calibratedPatPhotons = cms.EDProducer("CalibratedPatPhotonProducerRun2",
-
-                                      # input collections
-                                      photons = cms.InputTag('slimmedPhotons'),
-                                      
-                                      # data or MC corrections
-                                      # if isMC is false, data corrections are applied
-                                      isMC = cms.bool(False),
-                                      
-                                      # set to True to get special "fake" smearing for synchronization. Use JUST in case of synchronization
-                                      isSynchronization = cms.bool(False),
-
-                                      correctionFile = cms.string(files[correctionType])
-                                      )
+from EgammaAnalysis.ElectronTools.calibrationTablesRun2 import correctionType
+from EgammaAnalysis.ElectronTools.calibrationTablesRun2 import files
 
 calibratedPhotons = cms.EDProducer("CalibratedPhotonProducerRun2",
 
@@ -29,11 +11,15 @@ calibratedPhotons = cms.EDProducer("CalibratedPhotonProducerRun2",
                                    # data or MC corrections
                                    # if isMC is false, data corrections are applied
                                    isMC = cms.bool(False),
+                                   autoDataType = cms.bool(True),
                                    
                                    # set to True to get special "fake" smearing for synchronization. Use JUST in case of synchronization
                                    isSynchronization = cms.bool(False),
 
-                                   correctionFile = cms.string(files[correctionType])
+                                   correctionFile = cms.string(files[correctionType]),
+                                   recHitCollectionEB = cms.InputTag('reducedEcalRecHitsEB'),
+                                   recHitCollectionEE = cms.InputTag('reducedEcalRecHitsEE')
+                                     
                                    )
 
 
