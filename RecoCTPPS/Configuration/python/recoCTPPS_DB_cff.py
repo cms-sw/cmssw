d04630d9bf3d88d895950a1523564fade17aa527
Fabrizio <Fabrizio.Ferro@ge.infn.it>
2018-02-14 16:21:11 +0100

    fix names

--- RecoCTPPS/Configuration/python/recoCTPPS_DB_cff.py
+++ RecoCTPPS/Configuration/python/recoCTPPS_DB_cff.py
@@ -4,10 +4,10 @@ from Geometry.VeryForwardGeometry.geometryRPFromDB_cfi import *
 from Geometry.VeryForwardGeometryBuilder.ctppsIncludeAlignments_cfi import *
 ctppsIncludeAlignments.RealFiles = cms.vstring("Alignment/CTPPS/data/RPixGeometryCorrections.xml")
 
-from RecoCTPPS.TotemRPLocal.totemRPLocalReconstruction_DB_cff import *
-from RecoCTPPS.TotemRPLocal.ctppsDiamondLocalReconstruction_DB_cff import *
+from RecoCTPPS.TotemRPLocal.totemRPLocalReconstruction_cff import *
+from RecoCTPPS.TotemRPLocal.ctppsDiamondLocalReconstruction_cff import *
 from RecoCTPPS.TotemRPLocal.ctppsLocalTrackLiteProducer_cff import ctppsLocalTrackLiteProducer
-from RecoCTPPS.PixelLocal.ctppsPixelLocalReconstruction_DB_cff import *
+from RecoCTPPS.PixelLocal.ctppsPixelLocalReconstruction_cff import *
 
 recoCTPPS = cms.Sequence(
     totemRPLocalReconstruction *
