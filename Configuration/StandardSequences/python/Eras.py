9c560fe4bb48819f7cc3764f48eb0aad1fd1c9ea
Roger <roger.wolf@cern.ch>
2018-02-14 16:43:09 +0100

    restoring exact formof file on request

--- Configuration/StandardSequences/python/Eras.py
+++ Configuration/StandardSequences/python/Eras.py
@@ -44,8 +44,8 @@ class Eras (object):
                            'phase2_hgcal', 'phase2_muon', 'phase2_timing',
                            'phase2_timing_layer','phase2_hcal',
                            'trackingLowPU', 'trackingPhase1', 'ctpps_2016', 'trackingPhase2PU140',
-                           'tracker_apv_vfp30_2016', 'run2_miniAOD_80XLegacy', 'run2_miniAOD_94XFall17',
-                           'run2_nanoAOD_92X', 'hcalHardcodeConditions', 'hcalSkipPacker']
+                           'tracker_apv_vfp30_2016', 'run2_miniAOD_80XLegacy','run2_miniAOD_94XFall17', 'run2_nanoAOD_92X',
+                           'hcalHardcodeConditions', 'hcalSkipPacker']
         internalUseModChains = ['run2_2017_noTrackingModifier']
 
 
