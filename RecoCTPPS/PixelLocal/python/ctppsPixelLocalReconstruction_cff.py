824ceab83f6cf6de6b65050a5d28a6d74751d42a
Fabrizio <Fabrizio.Ferro@ge.infn.it>
2018-02-14 16:06:03 +0100

    cleaning

--- RecoCTPPS/PixelLocal/python/ctppsPixelLocalReconstruction_cff.py
+++ RecoCTPPS/PixelLocal/python/ctppsPixelLocalReconstruction_cff.py
@@ -9,9 +9,6 @@ from RecoCTPPS.PixelLocal.ctppsPixelRecHits_cfi import ctppsPixelRecHits
 # local track producer
 from RecoCTPPS.PixelLocal.ctppsPixelLocalTracks_cfi import ctppsPixelLocalTracks
 
-from Geometry.VeryForwardGeometryBuilder.ctppsIncludeAlignments_cfi import *
-ctppsIncludeAlignments.RealFiles = cms.vstring("Alignment/CTPPS/data/RPixGeometryCorrections.xml")
-
 ctppsPixelLocalReconstruction = cms.Sequence(
     ctppsPixelClusters*ctppsPixelRecHits*ctppsPixelLocalTracks
 )
