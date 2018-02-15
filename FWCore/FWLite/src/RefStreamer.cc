a0539ac9e35da4b1e709d1277c294f77a269da05
W. David Dagenhart <wdd@fnal.gov>
2018-02-15 16:10:25 -0600

    Remove setting the RefCore streamer where unnecessary

--- FWCore/FWLite/src/RefStreamer.cc
+++ FWCore/FWLite/src/RefStreamer.cc
@@ -9,20 +9,6 @@ class TBuffer;
 
 namespace fwlite {
   edm::EDProductGetter const* setRefStreamer(edm::EDProductGetter const* ep) {
-    {
-      TClass* cl = TClass::GetClass("edm::RefCore");
-      TClassStreamer* st = cl->GetStreamer();
-      if (st == nullptr) {
-        cl->AdoptStreamer(new edm::RefCoreStreamer());
-      }
-    }
-    {
-      TClass* cl = TClass::GetClass("edm::RefCoreWithIndex");
-      TClassStreamer* st = cl->GetStreamer();
-      if (st == nullptr) {
-        cl->AdoptStreamer(new edm::RefCoreWithIndexStreamer());
-      }
-    }
     return edm::EDProductGetter::switchProductGetter(ep);
   }
 }
