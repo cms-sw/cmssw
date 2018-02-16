488a1b5455ad1098bbc804a211d8aec4ffe93150
Thomas Strebler <thomas.strebler@llr.in2p3.fr>
2018-02-16 13:05:56 +0100

    New implementation of HGC towers

---
+++ DataFormats/L1THGCal/interface/HGCalTowerMap.h
@@ -0,0 +1,60 @@
+#ifndef DataFormats_L1TCalorimeter_HGCalTowerMap_h
+#define DataFormats_L1TCalorimeter_HGCalTowerMap_h
+
+
+#include "DataFormats/L1THGCal/interface/HGCalTower.h"
+#include "DataFormats/L1Trigger/interface/BXVector.h"
+
+namespace l1t {
+  
+  class HGCalTowerMap;
+  typedef BXVector<HGCalTowerMap> HGCalTowerMapBxCollection;
+
+  class HGCalTowerMap {
+
+  public:
+
+  HGCalTowerMap(): nEtaBins_(0), nPhiBins_(0), layer_(0) {}
+    
+    HGCalTowerMap( int& nEtaBins, int& nPhiBins );    
+
+    HGCalTowerMap( std::vector<double>& etaBins, std::vector<double>& phiBins );
+
+    ~HGCalTowerMap();
+
+    void setLayer( const unsigned layer ) { layer_ = layer; }
+
+
+    int nEtaBins() const { return nEtaBins_; }
+    int nPhiBins() const { return nPhiBins_; }
+    vector<double> etaBins() const { return etaBins_; }
+    vector<double> phiBins() const { return phiBins_; }
+    l1t::HGCalTower* tower(int iEta, int iPhi) { return &(towerMap_[iEta][iPhi]); }
+    int iEta(const double eta) const;
+    int iPhi(const double phi) const;
+    int layer() const { return layer_;}
+
+    HGCalTowerMap& operator+=(HGCalTowerMap map);
+
+
+  private:
+
+    static constexpr double kEtaMin_ = 1.479;
+    static constexpr double kEtaMax_ = 3.;
+    static constexpr double kPhiMin_ = -M_PI;
+    static constexpr double kPhiMax_ = +M_PI;  
+    
+    int nEtaBins_;
+    int nPhiBins_;
+    vector<double> etaBins_;
+    vector<double> phiBins_;
+    std::map<int,std::vector<l1t::HGCalTower>>  towerMap_;
+    unsigned layer_;
+
+  };
+
+}
+
+#endif
+
+
---
+++ DataFormats/L1THGCal/src/HGCalTowerMap.cc
@@ -0,0 +1,141 @@
+#include "DataFormats/L1THGCal/interface/HGCalTowerMap.h"
+#include "DataFormats/Math/interface/normalizedPhi.h"
+#include "FWCore/Utilities/interface/EDMException.h"
+#include "FWCore/MessageLogger/interface/MessageLogger.h"
+
+using namespace l1t;
+
+constexpr double HGCalTowerMap::kEtaMax_;
+constexpr double HGCalTowerMap::kEtaMin_;
+constexpr double HGCalTowerMap::kPhiMax_;
+constexpr double HGCalTowerMap::kPhiMin_;
+
+
+HGCalTowerMap::HGCalTowerMap( int& nEtaBins, int& nPhiBins )
+{
+
+  nEtaBins_ = nEtaBins;
+  nPhiBins_ = nPhiBins;
+
+  double eta_step = (kEtaMax_ - kEtaMin_)/nEtaBins_;
+  for(int i=0; i<nEtaBins_; i++) etaBins_.emplace_back( kEtaMin_ + eta_step*i );
+  etaBins_.emplace_back( kEtaMax_ );
+  
+  double phi_step = (kPhiMax_ - kPhiMin_)/nPhiBins_;
+  for(int i=0; i<nPhiBins_; i++) phiBins_.emplace_back( kPhiMin_ + phi_step*i );
+  phiBins_.emplace_back( kPhiMax_ );
+
+  vector<l1t::HGCalTower> towerRing( nPhiBins_ );
+  for(int iEta=-nEtaBins_; iEta<=nEtaBins_; iEta++){
+    if(iEta==0) continue;
+    towerMap_[iEta] = towerRing;
+  }
+
+  for(int iEta=-nEtaBins_; iEta<=nEtaBins_; iEta++){
+    if(iEta==0) continue;
+    for(int iPhi=0; iPhi<nPhiBins_; iPhi++){
+      towerMap_[iEta][iPhi].setHwEta(iEta);
+      towerMap_[iEta][iPhi].setHwPhi(iPhi);
+    }
+  }
+
+
+}
+
+
+HGCalTowerMap::HGCalTowerMap( vector<double>& etaBins, vector<double>& phiBins )
+{
+
+  nEtaBins_ = etaBins.size()-1;
+  nPhiBins_ = phiBins.size()-1;
+  etaBins_ = etaBins;
+  phiBins_ = phiBins;
+
+  vector<l1t::HGCalTower> towerRing( nPhiBins_ );
+  for(int iEta=-nEtaBins_; iEta<=nEtaBins_; iEta++){
+    if(iEta==0) continue;
+    towerMap_[iEta] = towerRing;
+  }
+
+  for(int iEta=-nEtaBins_; iEta<=nEtaBins_; iEta++){
+    if(iEta==0) continue;
+    for(int iPhi=0; iPhi<nPhiBins_; iPhi++){
+      towerMap_[iEta][iPhi].setHwEta(iEta);
+      towerMap_[iEta][iPhi].setHwPhi(iPhi);
+    }
+  }
+
+}
+
+
+HGCalTowerMap::~HGCalTowerMap()
+{
+
+}
+
+
+int HGCalTowerMap::iEta(const double eta) const
+{
+
+  double absEta = fabs(eta);
+  int signEta = eta>0 ? 1 : -1;
+  
+  if(absEta<kEtaMin_){
+    absEta = kEtaMin_;    
+    edm::LogWarning("HGCalTowerMap") << "WARNING: trying to access towers below min eta range of tower maps eta="<<eta;
+    return signEta;
+  }
+  else if(absEta>kEtaMax_){
+    absEta = kEtaMax_;
+    edm::LogWarning("HGCalTowerMap") << "WARNING: trying to access towers above max eta range of tower maps eta="<<eta;
+    return signEta*nEtaBins_;
+  }
+
+  for(int i = 0; i<nEtaBins_; i++){
+    if(absEta>=etaBins_[i] && absEta<etaBins_[i+1]){      
+      return signEta*(i+1);
+    }
+  }
+  
+  return 0;
+
+}
+
+
+int HGCalTowerMap::iPhi(const double phi) const
+{
+
+  double phi_norm = normalizedPhi(phi);
+ 
+  for(int i = 0; i<nPhiBins_; i++){
+    if(phi_norm>=phiBins_[i] && phi_norm<phiBins_[i+1]){
+      return i;
+    }
+  }
+  
+  return -1;
+
+}
+
+
+HGCalTowerMap& HGCalTowerMap::operator+=(HGCalTowerMap map){
+
+  if(etaBins_!=map.etaBins() || phiBins_!=map.phiBins()){
+    throw edm::Exception(edm::errors::StdException, "StdException")
+      << "HGCalTowerMap: Trying to add HGCalTowerMaps with different bins"<<endl;
+  }
+
+  for(int iEta=-nEtaBins_; iEta<=nEtaBins_; iEta++){
+    if(iEta==0) continue;
+    for(int iPhi=0; iPhi<nPhiBins_; iPhi++){
+      towerMap_[iEta][iPhi] += *(map.tower(iEta,iPhi));
+    }
+  }
+
+  return *this;
+
+}
+
+
+
+
