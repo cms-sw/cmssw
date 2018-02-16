e6dc8e4b4a18b40e29082142f4a3b5015749b6ce
Fabrizio <Fabrizio.Ferro@ge.infn.it>
2018-02-16 14:16:46 +0100

    adding sequences

--- RecoCTPPS/Configuration/python/recoCTPPS_cff.py
+++ RecoCTPPS/Configuration/python/recoCTPPS_cff.py
@@ -2,17 +2,8 @@ import FWCore.ParameterSet.Config as cms
 
 #from Geometry.VeryForwardGeometry.geometryRPFromDD_2018_cfi import *
 from Geometry.VeryForwardGeometry.geometryRPFromDD_2017_cfi import *
-from Geometry.VeryForwardGeometryBuilder.ctppsIncludeAlignments_cfi import *
-ctppsIncludeAlignments.RealFiles = cms.vstring("Alignment/CTPPS/data/RPixGeometryCorrections.xml")
 
-from RecoCTPPS.TotemRPLocal.totemRPLocalReconstruction_cff import *
-from RecoCTPPS.TotemRPLocal.ctppsDiamondLocalReconstruction_cff import *
-from RecoCTPPS.TotemRPLocal.ctppsLocalTrackLiteProducer_cff import ctppsLocalTrackLiteProducer
-from RecoCTPPS.PixelLocal.ctppsPixelLocalReconstruction_cff import *
+from recoCTPPS_sequences_cff import *
 
-recoCTPPS = cms.Sequence(
-    totemRPLocalReconstruction *
-    ctppsDiamondLocalReconstruction *
-    ctppsPixelLocalReconstruction *
-    ctppsLocalTrackLiteProducer
-)
+recoCTPPS = cms.Sequence(recoCTPPSdets)
+
