6f08e897b54a31a021bb820d5a6f45f732c219a4
Giacomo Govi <giacomo.govi@cern.ch>
2018-02-17 12:00:16 +0100

    Completed test coverage for Ecal O2Os

---
+++ CondTools/Ecal/test/EcalTPGFineGrainEBGroup_O2O_test.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+conddb --yes copy EcalTPGFineGrainEBGroup_v2_hlt --destdb EcalTPGFineGrainEBGroup_v2_hlt_O2OTEST.db --o2oTest
+cmsRun ./src/CondTools/Ecal/python/copyFgrGroup_cfg.py destinationDatabase=sqlite_file:EcalTPGFineGrainEBGroup_v2_hlt_O2OTEST.db destinationTag=EcalTPGFineGrainEBGroup_v2_hlt
+ret=$?
+conddb --db EcalTPGFineGrainEBGroup_v2_hlt_O2OTEST.db list EcalTPGFineGrainEBGroup_v2_hlt
+echo "return code is $ret"
+exit $ret
