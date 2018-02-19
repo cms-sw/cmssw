27bdfe023d3a7a19ffc4752742783be0a61113bb
Fabrizio <Fabrizio.Ferro@ge.infn.it>
2018-02-19 13:22:21 +0100

    using DB as default for Geometry

--- RecoCTPPS/Configuration/python/recoCTPPS_cff.py
+++ RecoCTPPS/Configuration/python/recoCTPPS_cff.py
@@ -1,9 +1,7 @@
 import FWCore.ParameterSet.Config as cms
 
-#from Geometry.VeryForwardGeometry.geometryRPFromDD_2018_cfi import *
-from Geometry.VeryForwardGeometry.geometryRPFromDD_2017_cfi import *
+from Geometry.VeryForwardGeometry.geometryRPFromDB_cfi import *
 
 from recoCTPPS_sequences_cff import *
 
 recoCTPPS = cms.Sequence(recoCTPPSdets)
-
