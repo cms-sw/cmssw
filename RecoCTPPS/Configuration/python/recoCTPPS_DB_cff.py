9c1215cf614ac7dca309363cea7895d063f4d389
Fabrizio <Fabrizio.Ferro@ge.infn.it>
2018-02-20 11:55:26 +0100

    using full path

--- RecoCTPPS/Configuration/python/recoCTPPS_DB_cff.py
+++ RecoCTPPS/Configuration/python/recoCTPPS_DB_cff.py
@@ -2,6 +2,6 @@ import FWCore.ParameterSet.Config as cms
 
 from Geometry.VeryForwardGeometry.geometryRPFromDB_cfi import *
 
-from recoCTPPS_sequences_cff import *
+from RecoCTPPS.Configuration.recoCTPPS_sequences_cff import *
 
 recoCTPPS = cms.Sequence(recoCTPPSdets)
