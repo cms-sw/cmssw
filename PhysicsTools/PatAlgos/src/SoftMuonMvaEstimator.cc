0981c43266a1021e8500137a72ebab5aa171db52
AlbertoBragagnolo <alberto.bragagnolo@pd.infn.it>
2018-02-21 10:21:00 +0100

    Fix identation

--- PhysicsTools/PatAlgos/src/SoftMuonMvaEstimator.cc
+++ PhysicsTools/PatAlgos/src/SoftMuonMvaEstimator.cc
@@ -13,25 +13,25 @@ SoftMuonMvaEstimator::SoftMuonMvaEstimator():
 void SoftMuonMvaEstimator::initialize(std::string weightsfile)
 {
 	if (initialized_) return;
-	tmvaReader_.AddVariable("segComp",											&segmentCompatibility_);
-	tmvaReader_.AddVariable("chi2LocMom",										&chi2LocalMomentum_);
-	tmvaReader_.AddVariable("chi2LocPos",										&chi2LocalPosition_);
-	tmvaReader_.AddVariable("glbTrackTailProb",							&glbTrackProbability_);
-	tmvaReader_.AddVariable("iValFrac",											&iValidFraction_);
-	tmvaReader_.AddVariable("LWH",													&layersWithMeasurement_);
-	tmvaReader_.AddVariable("kinkFinder",										&trkKink_);
+	tmvaReader_.AddVariable("segComp",			&segmentCompatibility_);
+	tmvaReader_.AddVariable("chi2LocMom",			&chi2LocalMomentum_);
+	tmvaReader_.AddVariable("chi2LocPos",			&chi2LocalPosition_);
+	tmvaReader_.AddVariable("glbTrackTailProb",		&glbTrackProbability_);
+	tmvaReader_.AddVariable("iValFrac",			&iValidFraction_);
+	tmvaReader_.AddVariable("LWH",				&layersWithMeasurement_);
+	tmvaReader_.AddVariable("kinkFinder",			&trkKink_);
 	tmvaReader_.AddVariable("TMath::Log(2+glbKinkFinder)",	&log2PlusGlbKink_);
-	tmvaReader_.AddVariable("timeAtIpInOutErr",							&timeAtIpInOutErr_);
-	tmvaReader_.AddVariable("outerChi2",										&outerChi2_);
-	tmvaReader_.AddVariable("innerChi2",										&innerChi2_);
-	tmvaReader_.AddVariable("trkRelChi2",										&trkRelChi2_);
-	tmvaReader_.AddVariable("vMuonHitComb",									&vMuonHitComb_);
-	tmvaReader_.AddVariable("Qprod",												&Qprod_);
+	tmvaReader_.AddVariable("timeAtIpInOutErr",		&timeAtIpInOutErr_);
+	tmvaReader_.AddVariable("outerChi2",			&outerChi2_);
+	tmvaReader_.AddVariable("innerChi2",			&innerChi2_);
+	tmvaReader_.AddVariable("trkRelChi2",			&trkRelChi2_);
+	tmvaReader_.AddVariable("vMuonHitComb",			&vMuonHitComb_);
+	tmvaReader_.AddVariable("Qprod",			&Qprod_);
 
-	tmvaReader_.AddSpectator("pID",													&pID_);
-	tmvaReader_.AddSpectator("pt",													&pt_);
-	tmvaReader_.AddSpectator("eta",													&eta_);
-	tmvaReader_.AddSpectator("MomID",												&MomID_);
+	tmvaReader_.AddSpectator("pID",			       	&pID_);
+	tmvaReader_.AddSpectator("pt",				&pt_);
+	tmvaReader_.AddSpectator("eta",				&eta_);
+	tmvaReader_.AddSpectator("MomID",			&MomID_);
 
 	tmvaReader_.BookMVA("BDT", weightsfile);
 	initialized_ = true;	
@@ -41,6 +41,18 @@ void SoftMuonMvaEstimator::computeMva(const pat::Muon& muon)
 {
 	if (not initialized_)
 		throw cms::Exception("FatalError") << "SoftMuonMVA is not initialized";
+
+	reco::TrackRef gTrack = muon.globalTrack();
+	reco::TrackRef iTrack = muon.innerTrack();
+	reco::TrackRef oTrack = muon.outerTrack();
+
+	if(!(muon.innerTrack().isNonnull() and 
+	     muon.outerTrack().isNonnull() and 
+	     muon.globalTrack().isNonnull()))
+	{
+	  mva_ = -1; 
+	  return;
+	}
 
 	//VARIABLE EXTRACTION
 	pt_ = muon.pt();
@@ -62,27 +74,6 @@ void SoftMuonMvaEstimator::computeMva(const pat::Muon& muon)
 	timeAtIpInOutErr_ = muon.time().timeAtIpInOutErr;
 
 	//TRACK RELATED VARIABLES
-	reco::TrackRef gTrack = muon.globalTrack();
-	reco::TrackRef iTrack = muon.innerTrack();
-	reco::TrackRef oTrack = muon.outerTrack();
-
-	iValidFraction_ = -1;
-	innerChi2_ = -1;
-	layersWithMeasurement_ = -1;
-	outerChi2_ = -1;
-	Qprod_ = -1;
-	vMuonHitComb_ = -1;
-	glbTrackProbability_ = -1;
-	log2PlusGlbKink_ = -1;
-	outerChi2_ = -1;
-
-	if(!(muon.innerTrack().isNonnull() and 
-			muon.outerTrack().isNonnull() and 
-	    muon.globalTrack().isNonnull()))
-	{
-		mva_ = -1; 
-		return;
-	}
 	
 	iValidFraction_ =  iTrack->validFraction();
 	innerChi2_ =  iTrack->normalizedChi2();
@@ -100,11 +91,11 @@ void SoftMuonMvaEstimator::computeMva(const pat::Muon& muon)
 	vMuonHitComb_ = (vDT/2 + vCSCmax + vRPC);
 
 	if(chi2LocalMomentum_ < 5000 and chi2LocalPosition_ < 2000 and
-    glbTrackProbability_ < 5000 and trkKink_ < 900 and
-    log2PlusGlbKink_ < 50 and timeAtIpInOutErr_ < 4 and
-    outerChi2_ < 1000 and innerChi2_ < 10 and trkRelChi2_ < 3) 
+	   glbTrackProbability_ < 5000 and trkKink_ < 900 and
+	   log2PlusGlbKink_ < 50 and timeAtIpInOutErr_ < 4 and
+	   outerChi2_ < 1000 and innerChi2_ < 10 and trkRelChi2_ < 3) 
 	{
-		mva_ = tmvaReader_.EvaluateMVA("BDT");
+	  mva_ = tmvaReader_.EvaluateMVA("BDT");
 	}else{ mva_ = -1; }
 
 };
