a93b1f2fcf3621ccd06b5b7ff109fe1cfcbcaa78
Aaron <aaron.bundock@cern.ch>
2018-02-22 13:02:05 +0100

    Add options for hw emu validation to use static or db emu conf & add make L1Ntuple options

--- L1Trigger/L1TCalorimeter/python/simCaloStage2Digis_cfi.py
+++ L1Trigger/L1TCalorimeter/python/simCaloStage2Digis_cfi.py
@@ -3,5 +3,6 @@ import FWCore.ParameterSet.Config as cms
 simCaloStage2Digis = cms.EDProducer(
     "L1TStage2Layer2Producer",
     towerToken = cms.InputTag("simCaloStage2Layer1Digis"),
-    firmware = cms.int32(1)
+    firmware = cms.int32(1),
+    useStaticConfig = cms.bool(False)
 )
