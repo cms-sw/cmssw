10a837fdbebdad68daab356ce0708d2f29c37d69
Amey Noolkar <anoolkar@cern.ch>
2018-02-26 15:46:25 +0100

    Added the FillInfo.h interface in CondCore/Utilities.

--- CondCore/Utilities/src/CondFormats.h
+++ CondCore/Utilities/src/CondFormats.h
@@ -128,6 +128,7 @@
 #include "CondFormats/RecoMuonObjects/interface/DYTThrObject.h"
 #include "CondFormats/RecoMuonObjects/interface/MuScleFitDBobject.h"
 #include "CondFormats/RecoMuonObjects/interface/DYTParamObject.h"
+#include "CondFormats/RunInfo/interface/FillInfo.h"
 #include "CondFormats/RunInfo/interface/RunInfo.h"
 #include "CondFormats/RunInfo/interface/MixingModuleConfig.h"
 #include "CondFormats/SiPixelObjects/interface/SiPixelCalibConfiguration.h"
