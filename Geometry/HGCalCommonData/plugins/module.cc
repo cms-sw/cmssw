3fb72b467dd941e5ee3687dd156e1db3f390ab94
Sunanda <sunanda.banerjee@cern.ch>
2018-02-26 22:39:20 +0100

    Start with new TDR geometry

--- Geometry/HGCalCommonData/plugins/module.cc
+++ Geometry/HGCalCommonData/plugins/module.cc
@@ -1,5 +1,6 @@
 #include "Geometry/HGCalCommonData/plugins/DDHGCalEEAlgo.h"
 #include "Geometry/HGCalCommonData/plugins/DDHGCalHEAlgo.h"
+#include "Geometry/HGCalCommonData/plugins/DDHGCalCell.h"
 #include "Geometry/HGCalCommonData/plugins/DDHGCalWaferAlgo.h"
 #include "Geometry/HGCalCommonData/plugins/DDHGCalWafer.h"
 #include "Geometry/HGCalCommonData/plugins/DDHGCalModule.h"
@@ -11,6 +12,7 @@
 
 DEFINE_EDM_PLUGIN (DDAlgorithmFactory, DDHGCalEEAlgo, "hgcal:DDHGCalEEAlgo");
 DEFINE_EDM_PLUGIN (DDAlgorithmFactory, DDHGCalHEAlgo, "hgcal:DDHGCalHEAlgo");
+DEFINE_EDM_PLUGIN (DDAlgorithmFactory, DDHGCalCell,   "hgcal:DDHGCalCell");
 DEFINE_EDM_PLUGIN (DDAlgorithmFactory, DDHGCalWaferAlgo, "hgcal:DDHGCalWaferAlgo");
 DEFINE_EDM_PLUGIN (DDAlgorithmFactory, DDHGCalWafer, "hgcal:DDHGCalWafer");
 DEFINE_EDM_PLUGIN (DDAlgorithmFactory, DDHGCalModule, "hgcal:DDHGCalModule");
