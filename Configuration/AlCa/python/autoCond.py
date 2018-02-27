f7018b41b5b96cefa2bd13f40abeddf47b4b2d15
Luca <luca.pernie@cern.ch>
2018-02-27 23:22:17 +0100

    add JEC to data

--- Configuration/AlCa/python/autoCond.py
+++ Configuration/AlCa/python/autoCond.py
@@ -24,11 +24,11 @@ autoCond = {
     # GlobalTag for MC production (p-Pb collisions) with realistic alignment and calibrations for Run2
     'run2_mc_pa'        :   '94X_mcRun2_pA_v2',
     # GlobalTag for Run1 data reprocessing
-    'run1_data'         :   '94X_dataRun2_v4',
+    'run1_data'         :   '94X_dataRun2_v6',
     # GlobalTag for Run2 data reprocessing
-    'run2_data'         :   '94X_dataRun2_v4',
+    'run2_data'         :   '94X_dataRun2_v6',
     # GlobalTag for Run2 data relvals: allows customization to run with fixed L1 menu
-    'run2_data_relval'  :   '94X_dataRun2_relval_v10',
+    'run2_data_relval'  :   '94X_dataRun2_relval_v11',
     # GlobalTag for Run2 data 2016H relvals only: Prompt Conditions + fixed L1 menu (to be removed)
     'run2_data_promptlike' : '94X_dataRun2_PromptLike_v9',
     # GlobalTag for Run1 HLT: it points to the online GT
