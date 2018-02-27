c8610851a798ed32861cac6a5f9d23923ed08a57
Vladimir <vladimir.rekovic@cern.ch>
2018-02-27 09:50:40 +0100

    Apply code-checks fixes.

--- L1Trigger/L1TNtuples/plugins/L1MetFilterRecoTreeProducer.cc
+++ L1Trigger/L1TNtuples/plugins/L1MetFilterRecoTreeProducer.cc
@@ -143,7 +143,7 @@ void L1MetFilterRecoTreeProducer::analyze(const edm::Event& iEvent, const edm::E
   
   if (trigRes.isValid()) {
     // get trigger names
-    const edm::TriggerNames trigNames = iEvent.triggerNames(*trigRes);
+    const edm::TriggerNames& trigNames = iEvent.triggerNames(*trigRes);
     doMetFilters(trigRes, trigNames);
   }
   else {
