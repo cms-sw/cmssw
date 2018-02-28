a9524fa0b7478f8b5ec4499e0ad658bf0ff1ef86
Jason Lee <jason.lee@cern.ch>
2018-02-28 16:11:35 +0100

    rm ME0DeadStrips/ME0MaskedStrips

--- CondCore/GEMPlugins/src/plugins.cc
+++ CondCore/GEMPlugins/src/plugins.cc
@@ -1,5 +1,19 @@
-#include "CondCore/ESSources/interface/registration_macros.h"
+/*
+ *  plugin.cc
+ *  CMSSW
+ *
+ *  Created by Sven Dildick --TAMU
+ *
+ */
+
+#include "CondCore/PluginSystem/interface/registration_macros.h"
 #include "CondFormats/GEMObjects/interface/GEMELMap.h"
 #include "CondFormats/DataRecord/interface/GEMELMapRcd.h"
+#include "CondFormats/GEMObjects/interface/GEMMaskedStrips.h"
+#include "CondFormats/DataRecord/interface/GEMMaskedStripsRcd.h"
+#include "CondFormats/GEMObjects/interface/GEMDeadStrips.h"
+#include "CondFormats/DataRecord/interface/GEMDeadStripsRcd.h"
+REGISTER_PLUGIN(GEMELMapRcd,GEMELMap);
+REGISTER_PLUGIN(GEMMaskedStripsRcd, GEMMaskedStrips);
+REGISTER_PLUGIN(GEMDeadStripsRcd, GEMDeadStrips);
 
-REGISTER_PLUGIN(GEMELMapRcd,GEMELMap); 
