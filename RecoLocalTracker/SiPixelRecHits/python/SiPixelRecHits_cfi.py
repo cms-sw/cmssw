e5801c410e5bbd9a312f7d0609faf9de99291fd9
Andrea Bocci <fwyzard@gmail.com>
2018-02-28 19:41:17 +0100

    Move the import next to the GPU modifier

--- RecoLocalTracker/SiPixelRecHits/python/SiPixelRecHits_cfi.py
+++ RecoLocalTracker/SiPixelRecHits/python/SiPixelRecHits_cfi.py
@@ -1,6 +1,5 @@
 import FWCore.ParameterSet.Config as cms
 
-from RecoLocalTracker.SiPixelRecHits.SiPixelRecHitsGPU_cfi import siPixelRecHits as _siPixelRecHitsGPU
 siPixelRecHits = cms.EDProducer("SiPixelRecHitConverter",
     src = cms.InputTag("siPixelClusters"),
     CPE = cms.string('PixelCPEGeneric'),
@@ -8,6 +7,7 @@ siPixelRecHits = cms.EDProducer("SiPixelRecHitConverter",
 
 )
 
+from RecoLocalTracker.SiPixelRecHits.SiPixelRecHitsGPU_cfi import siPixelRecHits as _siPixelRecHitsGPU
 from Configuration.ProcessModifiers.gpu_cff import gpu
 gpu.toReplaceWith(siPixelRecHits, _siPixelRecHitsGPU)
 
