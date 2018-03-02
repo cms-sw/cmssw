f1aea173ce5d3ecfcd995c8f83d0a246aca6c700
Sam Harper <sam.j.harper@gmail.com>
2018-03-02 20:42:40 +0000

    adding 2016/2017 IDs to MiniAOD and removing old ones

--- RecoEgamma/PhotonIdentification/python/PhotonMVAValueMapProducer_cfi.py
+++ RecoEgamma/PhotonIdentification/python/PhotonMVAValueMapProducer_cfi.py
@@ -4,12 +4,6 @@ mvaConfigsForPhoProducer = cms.VPSet( )
 
 # Import and add all desired MVAs
 
-from RecoEgamma.PhotonIdentification.Identification.mvaPhotonID_Spring15_50ns_nonTrig_V2p1_cff import *
-mvaConfigsForPhoProducer.append( mvaPhoID_Spring15_50ns_nonTrig_V2p1_producer_config )
-
-from RecoEgamma.PhotonIdentification.Identification.mvaPhotonID_Spring15_25ns_nonTrig_V2p1_cff import *
-mvaConfigsForPhoProducer.append( mvaPhoID_Spring15_25ns_nonTrig_V2p1_producer_config )
-
 from RecoEgamma.PhotonIdentification.Identification.mvaPhotonID_Spring16_nonTrig_V1_cff \
     import mvaPhoID_Spring16_nonTrig_V1_producer_config
 mvaConfigsForPhoProducer.append( mvaPhoID_Spring16_nonTrig_V1_producer_config )
