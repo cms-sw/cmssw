8eece768d4c6ec334356aa722f1b554b95c150e6
Jean-Baptiste Sauvan <jean-baptiste.sauvan@cern.ch>
2018-03-02 11:12:21 +0100

    Switch back default clustering in test cfg

--- L1Trigger/L1THGCal/test/testHGCalL1T_RelVal_cfg.py
+++ L1Trigger/L1THGCal/test/testHGCalL1T_RelVal_cfg.py
@@ -69,8 +69,6 @@ process.hgcl1tpg_step = cms.Path(process.hgcalTriggerPrimitives)
 # Change to V7 trigger geometry for older samples
 #  from L1Trigger.L1THGCal.customTriggerGeometry import custom_geometry_ZoltanSplit_V7
 #  process = custom_geometry_ZoltanSplit_V7(process)
-from L1Trigger.L1THGCal.customClustering import custom_2dclustering_constrainedtopological
-process = custom_2dclustering_constrainedtopological(process)
 
 # load ntuplizer
 process.load('L1Trigger.L1THGCal.hgcalTriggerNtuples_cff')
