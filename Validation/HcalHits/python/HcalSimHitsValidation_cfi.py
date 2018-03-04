677abe654936868487dc259e06286355738f0724
brooks <brooks_mcmaster@baylor.edu>
2018-03-04 08:23:50 +0100

    further fixes to EEHitCollection

--- Validation/HcalHits/python/HcalSimHitsValidation_cfi.py
+++ Validation/HcalHits/python/HcalSimHitsValidation_cfi.py
@@ -11,7 +11,10 @@ from Configuration.Eras.Modifier_fastSim_cff import fastSim
 fastSim.toModify(    HcalSimHitsAnalyser, ModuleLabel = cms.untracked.string("fastSimProducer") )
 
 from Configuration.Eras.Modifier_run2_HCAL_2017_cff import run2_HCAL_2017
-run2_HCAL_2017.toModify( HcalSimHitsAnalyser, TestNumber = cms.untracked.bool(True), EEHitCollection = cms.untracked.string("HGCHitsEE") )
+run2_HCAL_2017.toModify( HcalSimHitsAnalyser, TestNumber = cms.untracked.bool(True) )
+
+from Configuration.Eras.Modifier_phase2_hcal_cff import phase2_hcal 
+phase2_hcal.toModify( HcalSimHitsAnalyser, EEHitCollection = cms.untracked.string("") )
 
 # post-LS1 switch for sampling factors
 from Configuration.Eras.Modifier_run2_common_cff import run2_common
