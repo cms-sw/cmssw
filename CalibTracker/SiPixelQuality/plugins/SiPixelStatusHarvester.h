38046579cc278297886e344de0e7955a8a405868
tocheng <tocheng.cms@gmail.com>
2018-03-05 10:20:06 +0100

    Add SiPixelStatusHarvester to use the data from SiPixelStatusProducer to produce payloads for different sources of SiPixel Quality

---
+++ CalibTracker/SiPixelQuality/plugins/SiPixelStatusHarvester.h
@@ -0,0 +1,56 @@
+#ifndef SiPixelStatusHarvester_H
+#define SiPixelStatusHarvester_H
+
+/** \class SiPixelStatusHarvester
+ *  harvest per-lumi prduced SiPixelDetector status and make the payload for SiPixelQualityFromDB
+ *
+ *  \author 
+ */
+#include "FWCore/Framework/interface/EDAnalyzer.h"
+#include "FWCore/Framework/interface/Frameworkfwd.h"
+#include "FWCore/ParameterSet/interface/ParameterSet.h"
+
+#include "DQM/SiPixelPhase1Common/interface/SiPixelCoordinates.h"
+
+class SiPixelStatusHarvester : public edm::EDAnalyzer {
+ public:
+
+  // Constructor
+  SiPixelStatusHarvester(const edm::ParameterSet&);
+
+  // Destructor
+  ~SiPixelStatusHarvester() override;
+  
+  // Operations
+  void beginJob            () override;
+  void endJob              () override;  
+  void analyze             (const edm::Event&          , const edm::EventSetup&) override;
+  void beginRun            (const edm::Run&            , const edm::EventSetup&) override;
+  void endRun              (const edm::Run&            , const edm::EventSetup&) override;
+  void beginLuminosityBlock(const edm::LuminosityBlock&, const edm::EventSetup&) override;
+  void endLuminosityBlock  (const edm::LuminosityBlock&, const edm::EventSetup&) override;
+
+ protected:
+
+ private:
+  // Parameters
+  std::string 	      outputBase_;
+  int aveDigiOcc_;
+  int nLumi_;
+  std::string moduleName_;
+  std::string label_;  
+  // harvest helper classs that setup the IOV structure
+  SiPixelStatusManager siPixelStatusManager_;
+
+  std::string         recordName_;
+  bool                dumpTxt_;
+  std::string 	      outTxtFileName_;
+
+  // permanent known bad components
+  const SiPixelQuality* badPixelInfo_;
+
+  SiPixelCoordinates coord_;
+
+};
+
+#endif
---
+++ CalibTracker/SiPixelQuality/python/SiPixelStatusHarvester_cff.py
@@ -0,0 +1,4 @@
+import FWCore.ParameterSet.Config as cms
+
+from CalibTracker.SiPixelQuality.SiPixelStatusHarvester_cfi import *
+siPixelStatusHarvesting = cms.Sequence( siPixelStatusHarvester )
