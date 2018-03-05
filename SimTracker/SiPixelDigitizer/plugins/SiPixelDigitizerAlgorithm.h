ddec31e0d70ea8a5f488cfaa2d5aa8d5397fabfb
Paul Schuetze <paul.schuetze@desy.de>
2018-03-05 11:06:53 +0100

    Change the SiPixelDigitizer to depend on the Template on the SiPixelRecHits

--- SimTracker/SiPixelDigitizer/plugins/SiPixelDigitizerAlgorithm.h
+++ SimTracker/SiPixelDigitizer/plugins/SiPixelDigitizerAlgorithm.h
@@ -14,8 +14,8 @@
 #include "DataFormats/Math/interface/approx_exp.h"
 #include "SimDataFormats/PileupSummaryInfo/interface/PileupMixingContent.h"
 #include "Geometry/CommonTopologies/interface/PixelTopology.h"
-#include "SiPixelTemplate2D.h"
-#include "CondFormats/SiPixelObjects/interface/SiPixel2DTemplateDBObject.h" 
+#include "RecoLocalTracker/SiPixelRecHits/interface/SiPixelTemplate2D.h"
+#include "CondFormats/SiPixelObjects/interface/SiPixel2DTemplateDBObject.h"
 #include "boost/multi_array.hpp"
 
 typedef boost::multi_array<float, 2> array_2d;
