f6f63cc6fd19fdcd7e94f399c307011f38842892
Chris West <christopher.alan.west@gmail.com>
2018-03-06 14:27:37 -0600

    Set weight and pthat variables

--- L1Trigger/L1TNtuples/python/l1GeneratorTree_cfi.py
+++ L1Trigger/L1TNtuples/python/l1GeneratorTree_cfi.py
@@ -4,5 +4,6 @@ l1GeneratorTree = cms.EDAnalyzer(
     "L1GenTreeProducer",
     genJetToken     = cms.untracked.InputTag("ak4GenJets"),
     genParticleToken = cms.untracked.InputTag("genParticles"),
-    pileupInfoToken     = cms.untracked.InputTag("addPileupInfo")
+    pileupInfoToken     = cms.untracked.InputTag("addPileupInfo"),
+    genInfoToken     = cms.InputTag("generator")
 )
