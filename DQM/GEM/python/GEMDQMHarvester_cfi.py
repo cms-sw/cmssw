d86db5346342c898110a4f52b5a61ce0cca21f63
hyunyong <antilogy33@gmail.com>
2018-03-07 16:38:09 +0100

    remove white space

--- DQM/GEM/python/GEMDQMHarvester_cfi.py
+++
@@ -1,7 +0,0 @@
-import FWCore.ParameterSet.Config as cms
-from DQMServices.Core.DQMEDHarvester import DQMEDHarvester
-
-GEMDQMHarvester = DQMEDHarvester("GEMDQMHarvester", 
-  Name = cms.untracked.string('HarvestingAnalyzer'),
-
-)
--- DQM/GEM/python/GEMDQMStatusDigi_cfi.py
+++
@@ -1,8 +0,0 @@
-import FWCore.ParameterSet.Config as cms
-from DQMServices.Core.DQMEDAnalyzer import DQMEDAnalyzer
-
-GEMDQMStatusDigi = DQMEDAnalyzer("GEMDQMStatusDigi",
-    VFATInputLabel = cms.InputTag("muonGEMDigis", "vfatStatus"),
-    AMCInputLabel = cms.InputTag("muonGEMDigis", "AMCStatus"),     
-    GEBInputLabel = cms.InputTag("muonGEMDigis", "GEBStatus"), 
-)
