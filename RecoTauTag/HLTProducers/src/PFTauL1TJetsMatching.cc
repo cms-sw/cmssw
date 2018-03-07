65dc2de8fd019fced532b16491ea38cbb8cbf091
Albert <dow.albert@gmail.com>
2018-03-07 14:37:05 +0100

    changed name of module and added comments

--- RecoTauTag/HLTProducers/src/PFTauL1TJetsMatching.cc
+++ RecoTauTag/HLTProducers/src/PFTauL1TJetsMatching.cc
@@ -1,4 +1,4 @@
-#include "RecoTauTag/HLTProducers/interface/L1TLeadingJetTauMatching.h"
+#include "RecoTauTag/HLTProducers/interface/PFTauL1TJetsMatching.h"
 #include "Math/GenVector/VectorUtil.h"
 #include "DataFormats/HLTReco/interface/TriggerTypeDefs.h"
 #include "FWCore/Utilities/interface/EDMException.h"
@@ -8,20 +8,20 @@
 //
 // class declaration
 //
-L1TLeadingJetTauMatching::L1TLeadingJetTauMatching(const edm::ParameterSet& iConfig):
+PFTauL1TJetsMatching::PFTauL1TJetsMatching(const edm::ParameterSet& iConfig):
     tauSrc_    ( consumes<reco::PFTauCollection>(iConfig.getParameter<edm::InputTag>("TauSrc"      ) ) ),
     L1JetSrc_  ( consumes<trigger::TriggerFilterObjectWithRefs>(iConfig.getParameter<edm::InputTag>("L1JetSrc") ) ),
     matchingR2_ ( iConfig.getParameter<double>("MatchingdR")*iConfig.getParameter<double>("MatchingdR") ),
     minTauPt_ (iConfig.getParameter<double>("MinTauPt") ),
-    minJetPt_ (iConfig.getParameter<double>("MinJetPt") )
+    minL1TPt_ (iConfig.getParameter<double>("MinL1TPt") )
 {  
     produces<reco::PFTauCollection>();
 }
-L1TLeadingJetTauMatching::~L1TLeadingJetTauMatching(){ }
+PFTauL1TJetsMatching::~PFTauL1TJetsMatching(){ }
 
-void L1TLeadingJetTauMatching::produce(edm::StreamID iSId, edm::Event& iEvent, const edm::EventSetup& iES) const
+void PFTauL1TJetsMatching::produce(edm::StreamID iSId, edm::Event& iEvent, const edm::EventSetup& iES) const
 {
-    std::unique_ptr<reco::PFTauCollection> L1matchedPFTau(new reco::PFTauCollection);
+    std::unique_ptr<reco::PFTauCollection> L1TmatchedPFTau(new reco::PFTauCollection);
     
     edm::Handle<reco::PFTauCollection> taus;
     iEvent.getByToken(tauSrc_, taus);
@@ -32,11 +32,16 @@ void L1TLeadingJetTauMatching::produce(edm::StreamID iSId, edm::Event& iEvent, c
     l1t::JetVectorRef jetCandRefVec;
     L1Jets->getObjects(trigger::TriggerL1Jet,jetCandRefVec);
 
+    /* Loop over taus that must pass a certain minTauPt_ cut */ 
+    /* then loop over L1T jets that must pass minL1TPt_ */
+    /* and check whether they match, if yes -> include the taus in */ 
+    /* the new L1T matched PFTau collection */
+
     for(unsigned int iTau = 0; iTau < taus->size(); iTau++){  
         bool isMatched = false;
         if ((*taus)[iTau].pt() > minTauPt_){
             for (unsigned int iJet = 0; iJet < jetCandRefVec.size(); iJet++) {
-                if (jetCandRefVec[iJet]->pt() > minJetPt_){
+                if (jetCandRefVec[iJet]->pt() > minL1TPt_){
 
                     if(reco::deltaR2((*taus)[iTau].p4(), jetCandRefVec[iJet]->p4()) < matchingR2_){
                         isMatched = true;
@@ -45,18 +50,19 @@ void L1TLeadingJetTauMatching::produce(edm::StreamID iSId, edm::Event& iEvent, c
                 }
             }
         }
-        if(isMatched == true) L1matchedPFTau->push_back((*taus)[iTau]);
+        if(isMatched == true) L1TmatchedPFTau->push_back((*taus)[iTau]);
     }
-    iEvent.put(std::move(L1matchedPFTau));
+    iEvent.put(std::move(L1TmatchedPFTau));
 }
 
-void L1TLeadingJetTauMatching::fillDescriptions(edm::ConfigurationDescriptions& descriptions) 
+void PFTauL1TJetsMatching::fillDescriptions(edm::ConfigurationDescriptions& descriptions) 
 {
     edm::ParameterSetDescription desc;
     desc.add<edm::InputTag>("L1JetSrc", edm::InputTag("hltL1VBFDiJetOR"))->setComment("Input filter objects passing L1 seed"    );
     desc.add<edm::InputTag>("TauSrc", edm::InputTag("hltSelectedPFTausTrackFindingLooseChargedIsolationAgainstMuon"))->setComment("Input collection of PFTaus");
     desc.add<double>       ("MatchingdR",0.5)->setComment("Maximum dR for matching between PFTaus and L1 filter jets");
     desc.add<double>       ("MinTauPt",20.0)->setComment("PFTaus above this pt will be considered");
+    desc.add<double>       ("MinL1TPt",115.0)->setComment("L1T Objects above this pt will be considered");
     descriptions.setComment("This module produces a collection of PFTaus matched to the leading jet passing the L1 seed filter.");
-    descriptions.add       ("L1TLeadingJetTauMatching",desc);
+    descriptions.add       ("PFTauL1TJetsMatching",desc);
 }
