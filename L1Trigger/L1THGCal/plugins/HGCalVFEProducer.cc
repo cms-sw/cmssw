54088d0a53e9563211bda67a1d02786e086dc7a9
Emilia Becheva <emilia.becheva@llr.in2p3.fr>
2018-03-09 14:31:01 +0100

    New producer which takes VFE part of HGCalTriggerDigiReproducer.cc

--- L1Trigger/L1THGCal/plugins/HGCalVFEProducer.cc
+++ L1Trigger/L1THGCal/plugins/HGCalVFEProducer.cc
@@ -1,73 +1,62 @@
 #include "FWCore/Framework/interface/Frameworkfwd.h"
-#include "FWCore/Framework/interface/stream/EDProducer.h"
+#include "FWCore/Framework/interface/EDProducer.h"
 
 #include "FWCore/Framework/interface/Event.h"
 #include "FWCore/Framework/interface/MakerMacros.h"
-#include "FWCore/Framework/interface/ConsumesCollector.h"
 
-#include "DataFormats/L1THGCal/interface/HGCFETriggerDigi.h"
-#include "DataFormats/L1THGCal/interface/HGCFETriggerDigiDefs.h"
+#include "DataFormats/L1THGCal/interface/HGCalTriggerCell.h"
+#include "DataFormats/L1THGCal/interface/HGCalTriggerSums.h"
 #include "DataFormats/HGCDigi/interface/HGCDigiCollections.h"
 
-#include "Geometry/Records/interface/CaloGeometryRecord.h"
-
 #include "L1Trigger/L1THGCal/interface/HGCalTriggerGeometryBase.h"
-#include "L1Trigger/L1THGCal/interface/HGCalTriggerFECodecBase.h"
-#include "L1Trigger/L1THGCal/interface/HGCalTriggerBackendProcessor.h"
 
+#include "L1Trigger/L1THGCal/interface/HGCalVFEProcessorBase.h"
+
+#include <sstream>
 #include <memory>
 
-class HGCalTriggerDigiProducer : public edm::stream::EDProducer<> {  
+
+class HGCalVFEProducer : public edm::stream::EDProducer<>  {  
  public:    
-  HGCalTriggerDigiProducer(const edm::ParameterSet&);
-  ~HGCalTriggerDigiProducer() override { }
+  HGCalVFEProducer(const edm::ParameterSet&);
+  ~HGCalVFEProducer() { }
   
-  void beginRun(const edm::Run&, 
-                        const edm::EventSetup&) override;
-  void produce(edm::Event&, const edm::EventSetup&) override;
+  virtual void beginRun(const edm::Run&, 
+                        const edm::EventSetup&);
+  virtual void produce(edm::Event&, const edm::EventSetup&);
   
  private:
   // inputs
   edm::EDGetToken inputee_, inputfh_, inputbh_;
   edm::ESHandle<HGCalTriggerGeometryBase> triggerGeometry_;
-  // algorithm containers
-  std::unique_ptr<HGCalTriggerFECodecBase> codec_;
-  std::unique_ptr<HGCalTriggerBackendProcessor> backEndProcessor_;
+  
+  std::unique_ptr<HGCalVFEProcessorBase> vfeProcess_;
 };
 
-DEFINE_FWK_MODULE(HGCalTriggerDigiProducer);
+DEFINE_FWK_MODULE(HGCalVFEProducer);
 
-HGCalTriggerDigiProducer::
-HGCalTriggerDigiProducer(const edm::ParameterSet& conf):
+HGCalVFEProducer::
+HGCalVFEProducer(const edm::ParameterSet& conf):
   inputee_(consumes<HGCEEDigiCollection>(conf.getParameter<edm::InputTag>("eeDigis"))),
-  inputfh_(consumes<HGCHEDigiCollection>(conf.getParameter<edm::InputTag>("fhDigis"))), 
-  inputbh_(consumes<HGCBHDigiCollection>(conf.getParameter<edm::InputTag>("bhDigis"))), 
-  backEndProcessor_(new HGCalTriggerBackendProcessor(conf.getParameterSet("BEConfiguration"),consumesCollector()) )
-{
-  //setup FE codec
-  const edm::ParameterSet& feCodecConfig = conf.getParameterSet("FECodec");
-  const std::string& feCodecName = feCodecConfig.getParameter<std::string>("CodecName");
-  HGCalTriggerFECodecBase* codec = HGCalTriggerFECodecFactory::get()->create(feCodecName,feCodecConfig);
-  codec_.reset(codec);
-  codec_->unSetDataPayload();
-  
-  produces<l1t::HGCFETriggerDigiCollection>();
-  // register backend processor products
-  backEndProcessor_->setProduces(*this);
+  inputfh_(consumes<HGCHEDigiCollection>(conf.getParameter<edm::InputTag>("fhDigis"))),
+  inputbh_(consumes<HGCBHDigiCollection>(conf.getParameter<edm::InputTag>("bhDigis")))
+{   
+  //setup VFE parameters
+  const edm::ParameterSet& vfeParamConfig = conf.getParameterSet("VFEparam");
+  const std::string& vfeProcessorName = vfeParamConfig.getParameter<std::string>("VFEProcessorName");
+  HGCalVFEProcessorBase* vfeProc = HGCalVFEProcessorBaseFactory::get()->create(vfeProcessorName, vfeParamConfig);
+  vfeProcess_.reset(vfeProc);
+    
+  vfeProcess_->setProduces(*this);
 }
 
-void HGCalTriggerDigiProducer::beginRun(const edm::Run& /*run*/, 
-                                          const edm::EventSetup& es) {
-  es.get<CaloGeometryRecord>().get(triggerGeometry_);
-  codec_->setGeometry(triggerGeometry_.product());
-  backEndProcessor_->setGeometry(triggerGeometry_.product());
-
+void HGCalVFEProducer::beginRun(const edm::Run& /*run*/, 
+                                          const edm::EventSetup& es) {					  				  
+  es.get<IdealGeometryRecord>().get(triggerGeometry_);
+  vfeProcess_->setGeometry(triggerGeometry_.product());
 }
 
-void HGCalTriggerDigiProducer::produce(edm::Event& e, const edm::EventSetup& es) {
-  std::unique_ptr<l1t::HGCFETriggerDigiCollection> 
-    fe_output( new l1t::HGCFETriggerDigiCollection );
-  
+void HGCalVFEProducer::produce(edm::Event& e, const edm::EventSetup& es) {
   edm::Handle<HGCEEDigiCollection> ee_digis_h;
   edm::Handle<HGCHEDigiCollection> fh_digis_h;
   edm::Handle<HGCBHDigiCollection> bh_digis_h;
@@ -103,41 +92,21 @@ void HGCalTriggerDigiProducer::produce(edm::Event& e, const edm::EventSetup& es)
     auto itr_insert = hit_modules_bh.emplace(module, HGCBHDigiCollection());
     itr_insert.first->second.push_back(bhdata);
   }
-  // loop on modules containing hits and call front-end processing
-  // we produce one output trigger digi per module in the FE
-  fe_output->reserve(hit_modules_ee.size() + hit_modules_fh.size() + hit_modules_bh.size());
-  for( const auto& module_hits : hit_modules_ee ) {        
-    fe_output->push_back(l1t::HGCFETriggerDigi());
-    l1t::HGCFETriggerDigi& digi = fe_output->back();
-    codec_->setDataPayload(module_hits.second,HGCHEDigiCollection(),HGCBHDigiCollection());
-    codec_->encode(digi);
-    digi.setDetId( DetId(module_hits.first) );
-    codec_->unSetDataPayload(); 
-  } //end loop on EE modules
-  for( const auto& module_hits : hit_modules_fh ) {        
-    fe_output->push_back(l1t::HGCFETriggerDigi());
-    l1t::HGCFETriggerDigi& digi = fe_output->back();
-    codec_->setDataPayload(HGCEEDigiCollection(),module_hits.second,HGCBHDigiCollection());
-    codec_->encode(digi);
-    digi.setDetId( DetId(module_hits.first) );
-    codec_->unSetDataPayload();
-  } //end loop on FH modules
-  for( const auto& module_hits : hit_modules_bh ) {
-    fe_output->push_back(l1t::HGCFETriggerDigi());
-    l1t::HGCFETriggerDigi& digi = fe_output->back();
-    codec_->setDataPayload(HGCEEDigiCollection(),HGCHEDigiCollection(),module_hits.second);
-    codec_->encode(digi);
-    digi.setDetId( DetId(module_hits.first) );
-    codec_->unSetDataPayload();
-  } //end loop on BH modules
-
-
-  // get the orphan handle and fe digi collection
-  auto fe_digis_handle = e.put(std::move(fe_output));
-  auto fe_digis_coll = *fe_digis_handle;
   
-  //now we run the emulation of the back-end processor
-  backEndProcessor_->reset();
-  backEndProcessor_->run(fe_digis_coll,es,e);
-  backEndProcessor_->putInEvent(e);
+  vfeProcess_->reset();
+  for( const auto& module_hits : hit_modules_ee ) {
+    vfeProcess_->vfeProcessing(module_hits.second, HGCHEDigiCollection(), HGCBHDigiCollection(), es);        
+  } //end loop on EE modules
+  
+      
+  for( const auto& module_hits : hit_modules_fh ) {
+    vfeProcess_->vfeProcessing(HGCEEDigiCollection(), module_hits.second, HGCBHDigiCollection(), es);   
+  } //end loop on FH modules
+
+  for( const auto& module_hits : hit_modules_bh ) {
+    vfeProcess_->vfeProcessing(HGCEEDigiCollection(), HGCHEDigiCollection(), module_hits.second, es);   
+  } //end loop on FH modules
+     
+  vfeProcess_->putInEvent(e);
+
 }
