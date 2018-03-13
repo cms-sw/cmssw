f1ff1f1639360418fca816cc7a97d030ecc0f68c
arun <arun.kumar@cern.ch>
2018-03-13 21:40:18 +0100

    switch the reading of GEM electronics map from DB

--- EventFilter/GEMRawToDigi/python/gemUnpacker_cfi.py
+++ EventFilter/GEMRawToDigi/python/gemUnpacker_cfi.py
@@ -3,5 +3,5 @@ import FWCore.ParameterSet.Config as cms
 muonGEMDigis = cms.EDProducer("GEMRawToDigiModule",
     InputLabel = cms.InputTag("rawDataCollector"),
     UnpackStatusDigis = cms.bool(False),
-    useDBEMap = cms.bool(False),
+    useDBEMap = cms.bool(True),
 )
