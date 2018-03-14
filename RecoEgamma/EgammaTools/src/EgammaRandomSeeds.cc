d3ff613a4f6fcb81ec36809ebc610e53159e1821
Sam Harper <sam.j.harper@gmail.com>
2018-03-14 17:59:24 +0000

    electron energy setting bug and making EG random seed more robust

--- RecoEgamma/EgammaTools/src/EgammaRandomSeeds.cc
+++ RecoEgamma/EgammaTools/src/EgammaRandomSeeds.cc
@@ -4,8 +4,10 @@
 
 uint32_t egamma::getRandomSeedFromSC(const edm::Event& iEvent,const reco::SuperClusterRef scRef)
 {
+  const int offset=0; //for future expansion
   std::seed_seq seeder = {int(iEvent.id().event()), int(iEvent.id().luminosityBlock()), int(iEvent.id().run()),
-			  int(scRef->seed()->seed().rawId()),int(scRef->hitsAndFractions().size())};
+			  int(scRef->seed()->seed().rawId()),int(scRef->seed()->hitsAndFractions().size()),
+			  offset};
   uint32_t seed = 0, tries = 10;
   do {
     seeder.generate(&seed,&seed+1); tries++;
