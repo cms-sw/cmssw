f36981b91b92e0184e21ae1dc6f9dcf81d714f7d
Jay <jlawhorn@mit.edu>
2018-03-15 21:15:10 +0100

    getting around whatever went wrong with the time slew

--- RecoLocalCalo/HcalRecAlgos/test/MahiFitDebugger_cfg.py
+++ RecoLocalCalo/HcalRecAlgos/test/MahiFitDebugger_cfg.py
@@ -21,7 +21,7 @@ process.load('Configuration.StandardSequences.EndOfProcess_cff')
 process.load('Configuration.StandardSequences.FrontierConditions_GlobalTag_cff')
 
 process.maxEvents = cms.untracked.PSet(
-    input = cms.untracked.int32(10)
+    input = cms.untracked.int32(100)
 )
 
 # Input source
