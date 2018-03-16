01b17ddcd38e42cddb2cde59906937100422cd45
Emilia Becheva <emilia.becheva@llr.in2p3.fr>
2018-03-16 11:30:41 +0100

    new class HGCalVFECompressionImpl

---
+++ L1Trigger/L1THGCal/interface/veryfrontend/HGCalVFECompressionImpl.h
@@ -0,0 +1,20 @@
+#ifndef __L1Trigger_L1THGCal_HGCalVFECompressionImpl_h__
+#define __L1Trigger_L1THGCal_HGCalVFECompressionImpl_h__
+
+#include "FWCore/ParameterSet/interface/ParameterSet.h"
+#include "L1Trigger/L1THGCal/interface/HGCalTriggerGeometryBase.h"
+
+#include <array>
+#include <vector>
+
+class HGCalVFECompressionImpl
+{
+
+ public:
+  HGCalVFECompressionImpl(const edm::ParameterSet& conf);
+     
+ private:
+     
+};
+
+#endif
