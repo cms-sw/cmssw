b177b42c84ac9018d16711dc7b5e8b6b8b1c4386
Emilia Becheva <emilia.becheva@llr.in2p3.fr>
2018-03-16 11:23:45 +0100

    first commit of HGCalVFEProcessor.h

---
+++ L1Trigger/L1THGCal/interface/veryfrontend/HGCalVFEProcessor.h
@@ -0,0 +1,73 @@
+#ifndef __L1Trigger_L1THGCal_HGCalVFEProcessor_h__
+#define __L1Trigger_L1THGCal_HGCalVFEProcessor_h__
+
+#include "L1Trigger/L1THGCal/interface/HGCalVFEProcessorBase.h"
+
+#include "L1Trigger/L1THGCal/interface/veryfrontend/HGCalVFELinearizationImpl.h"
+#include "L1Trigger/L1THGCal/interface/veryfrontend/HGCalVFESummationImpl.h"
+
+#include "DataFormats/L1THGCal/interface/HGCalTriggerCell.h"
+#include "DataFormats/L1THGCal/interface/HGCalTriggerSums.h"
+
+#include "L1Trigger/L1THGCal/interface/HGCalTriggerGeometryBase.h"
+#include "L1Trigger/L1THGCal/interface/be_algorithms/HGCalTriggerCellCalibration.h"
+
+#include "DataFormats/L1THGCal/interface/HGCalCluster.h"
+
+
+class HGCalVFEProcessor : public HGCalVFEProcessorBase
+{
+    
+  public:
+    
+    HGCalVFEProcessor(const edm::ParameterSet& conf);
+    
+    void vfeProcessing(const HGCEEDigiCollection& ee,
+                	   const HGCHEDigiCollection& fh,
+                	   const HGCHEDigiCollection& bh, const edm::EventSetup& es);
+    
+    void putInEvent(edm::Event& evt);
+    
+    virtual void setProduces(edm::stream::EDProducer<>& prod) const override final 
+    { 
+      prod.produces<l1t::HGCalTriggerCellBxCollection>("calibratedTriggerCells");
+      prod.produces<l1t::HGCalTriggerSumsBxCollection>("calibratedTriggerCells");
+    }
+    
+    virtual void reset() override final 
+    {
+      triggerCell_product_.reset( new l1t::HGCalTriggerCellBxCollection );
+      triggerSums_product_.reset( new l1t::HGCalTriggerSumsBxCollection );
+    }
+             
+    typedef std::unique_ptr<HGCalVFEProcessorBase> vfeProcessing_ptr;
+    
+    // Retrieve parameters
+    double   linLSB()        const {return linLSB_;}
+    uint32_t triggerCellTruncationBits() const {return triggerCellTruncationBits_;}
+    
+  private:
+    std::vector<vfeProcessing_ptr> vfeProcessing_;
+    double   linLSB_;
+    uint32_t triggerCellTruncationBits_;
+        
+    HGCalVFELinearizationImpl vfeLinearizationImpl_;
+    HGCalVFESummationImpl vfeSummationImpl_; 
+    HGCalTriggerCellCalibration calibration_;
+    
+    std::vector<l1t::HGCalTriggerCell> vecTrigCell_;
+    std::unique_ptr<l1t::HGCalTriggerCellBxCollection> triggerCell_product_;
+    std::unique_ptr<l1t::HGCalTriggerSumsBxCollection> triggerSums_product_;
+    
+     
+    /* lables of sensitive detector (geometry record) */
+    std::string HGCalEESensitive_;
+    std::string HGCalHESiliconSensitive_;
+
+    /* handles to the detector topologies */
+    edm::ESHandle<HGCalTopology> hgceeTopoHandle_;
+    edm::ESHandle<HGCalTopology> hgchefTopoHandle_;    
+ 
+};    
+    
+#endif
