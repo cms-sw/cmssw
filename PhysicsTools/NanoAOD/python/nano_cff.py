dcccc9e0cbcbd7dcd8930266c0b158708cd01d39
mondalspandan <mondalspandan@yahoo.co.in>
2018-03-19 10:40:22 +0100

    Added era switches, added default output of 1.0 for the eras where SFs are unavailable

--- PhysicsTools/NanoAOD/python/nano_cff.py
+++ PhysicsTools/NanoAOD/python/nano_cff.py
@@ -16,6 +16,8 @@ from PhysicsTools.NanoAOD.triggerObjects_cff import *
 from PhysicsTools.NanoAOD.isotracks_cff import *
 from PhysicsTools.NanoAOD.NanoAODEDMEventContent_cff import *
 
+from Configuration.Eras.Modifier_run2_miniAOD_80XLegacy_cff import run2_miniAOD_80XLegacy
+from Configuration.Eras.Modifier_run2_nanoAOD_92X_cff import run2_nanoAOD_92X
 
 nanoMetadata = cms.EDProducer("UniqueStringProducer",
     strings = cms.PSet(
@@ -48,30 +50,53 @@ simpleCleanerTable = cms.EDProducer("NanoAODSimpleCrossCleaner",
    tauName=cms.string("Tau"),photonName=cms.string("Photon")
 )
 
+btagSFdir="/src/PhysicsTools/NanoAOD/data/btagSF/"
+
 btagWeightTable = cms.EDProducer("BTagSFProducer",
     src = cms.InputTag("linkedObjects","jets"),
-    cut = cms.string("pt > 25. && abs(eta) < 2.4"),
+    cut = cms.string("pt > 25. && abs(eta) < 2.5"),
     discNames = cms.vstring(
         "pfCombinedInclusiveSecondaryVertexV2BJetTags",
-        "pfCombinedMVAV2BJetTags",
-        "pfDeepCSVJetTags:probb+pfDeepCSVJetTags:probbb"        #if multiple MiniAOD branches need to be summed up (e.g., DeepCSV b+bb), separate them using '+' delimiter
+        "pfDeepCSVJetTags:probb+pfDeepCSVJetTags:probbb",       #if multiple MiniAOD branches need to be summed up (e.g., DeepCSV b+bb), separate them using '+' delimiter
+        "pfCombinedMVAV2BJetTags"        
     ),
     discShortNames = cms.vstring(
         "CSVV2",
-        "CMVA",
-        "DeepCSVB"
+        "DeepCSVB",
+        "CMVA"
     ),
-    weightFiles = cms.vstring(
-        "/src/PhysicsTools/NanoAOD/data/btagSF/CSVv2_94XSF_V1_B_F.csv",
-        "/src/PhysicsTools/NanoAOD/data/btagSF/btagSF_cMVAv2_ichep2016.csv",
-        "/src/PhysicsTools/NanoAOD/data/btagSF/DeepCSV_94XSF_V1_B_F.csv"
+    weightFiles = cms.vstring(                                  #default settings are for 2017 94X. toModify function is called later for other eras.
+        btagSFdir+"CSVv2_94XSF_V1_B_F.csv",
+        btagSFdir+"DeepCSV_94XSF_V1_B_F.csv",                    
+        "unavailable"                                           #if SFs for an algorithm in an era is unavailable, all weights will be stored as 1.0
     ),
-    operatingPoints = cms.vstring("1","1","1"),                  #loose = 0, medium = 1, tight = 2, reshaping = 3
-    measurementTypesB = cms.vstring("comb","ttbar","comb"),      #e.g. "ttbar", "comb", "incl", "iterativefit"
-    measurementTypesC = cms.vstring("comb","ttbar","comb"),
+    operatingPoints = cms.vstring("1","1","1"),                 #loose = 0, medium = 1, tight = 2, reshaping = 3
+    measurementTypesB = cms.vstring("comb","comb","ttbar"),     #e.g. "comb", "incl", "ttbar", "iterativefit"
+    measurementTypesC = cms.vstring("comb","comb","ttbar"),
     measurementTypesUDSG = cms.vstring("incl","incl","incl"),
     sysTypes = cms.vstring("central","central","central")
 )
+
+#run2_miniAOD_80XLegacy.toModify(btagWeightTable,                
+    #cut = cms.string("pt > 25. && abs(eta) < 2.4"),
+#)
+
+run2_miniAOD_80XLegacy.toModify(btagWeightTable,                
+    cut = cms.string("pt > 25. && abs(eta) < 2.4"),             #80X corresponds to 2016, |eta| < 2.4
+    weightFiles = cms.vstring(                                  #80X corresponds to 2016 SFs
+        btagSFdir+"CSVv2_Moriond17_B_H.csv",            
+        btagSFdir+"DeepCSV_Moriond17_B_H.csv",                    
+        btagSFdir+"cMVAv2_Moriond17_B_H.csv"                                            
+    )
+)
+
+run2_nanoAOD_92X.toModify(btagWeightTable,                      #92X corresponds to MCv1, for which SFs are unavailable
+    weightFiles = cms.vstring(
+        "unavailable",
+        "unavailable",                    
+        "unavailable"                                            
+    )
+)                    
 
 genWeightsTable = cms.EDProducer("GenWeightsTableProducer",
     genEvent = cms.InputTag("generator"),
