6fef73fc2253432c3510db58f91d4eb470c4b10f
masegura <manuel.segura@cern.ch>
2018-03-20 19:24:57 +0100

    CPPFDigis emulator, Path for LUT @maseguracern

--- L1Trigger/L1TMuonCPPF/python/emulatorCppfDigis_cfi.py
+++ L1Trigger/L1TMuonCPPF/python/emulatorCppfDigis_cfi.py
@@ -8,14 +8,14 @@ emulatorCppfDigis = cms.EDProducer("L1TMuonCPPFDigiProducer",
                                    #  cppfSource = cms.string('Geo'), #'File' for Look up table and 'Geo' for CMSSW Geometry 
                                    cppfSource = cms.string('File'), #'File' for Look up table and 'Geo' for CMSSW Geometry 
                                    
-                                   cppfvecfile = cms.FileInPath('L1Trigger/L1TMuonCPPF/data/angleScale_RPC_CPPFmerged.txt')                     
-                                   #    cppfvecfile = cms.FileInPath('L1Trigger/L1TMuonCPPF/data/angleScale_RPC_CPPFn1.txt')                    
-                                   #    cppfvecfile = cms.FileInPath('L1Trigger/L1TMuonCPPF/data/angleScale_RPC_CPPFn2.txt')                    
-                                   #    cppfvecfile = cms.FileInPath('L1Trigger/L1TMuonCPPF/data/angleScale_RPC_CPPFn3.txt')                    
-                                   #    cppfvecfile = cms.FileInPath('L1Trigger/L1TMuonCPPF/data/angleScale_RPC_CPPFn4.txt')                    
-                                   #    cppfvecfile = cms.FileInPath('L1Trigger/L1TMuonCPPF/data/angleScale_RPC_CPPFp1.txt')                    
-                                   #    cppfvecfile = cms.FileInPath('L1Trigger/L1TMuonCPPF/data/angleScale_RPC_CPPFp2.txt')                    
-                                   #    cppfvecfile = cms.FileInPath('L1Trigger/L1TMuonCPPF/data/angleScale_RPC_CPPFp3.txt')                   
-                                   #    cppfvecfile = cms.FileInPath('L1Trigger/L1TMuonCPPF/data/angleScale_RPC_CPPFp4.txt')                    
+                                   cppfvecfile = cms.FileInPath('L1Trigger/L1TMuon/data/cppf/angleScale_RPC_CPPFmerged.txt')                     
+                                   #    cppfvecfile = cms.FileInPath('L1Trigger/L1TMuon/data/cppf/angleScale_RPC_CPPFn1.txt')                    
+                                   #    cppfvecfile = cms.FileInPath('L1Trigger/L1TMuon/data/cppf/angleScale_RPC_CPPFn2.txt')                    
+                                   #    cppfvecfile = cms.FileInPath('L1Trigger/L1TMuon/data/cppf/angleScale_RPC_CPPFn3.txt')                    
+                                   #    cppfvecfile = cms.FileInPath('L1Trigger/L1TMuon/data/cppf/angleScale_RPC_CPPFn4.txt')                    
+                                   #    cppfvecfile = cms.FileInPath('L1Trigger/L1TMuon/data/cppf/angleScale_RPC_CPPFp1.txt')                    
+                                   #    cppfvecfile = cms.FileInPath('L1Trigger/L1TMuon/data/cppf/angleScale_RPC_CPPFp2.txt')                    
+                                   #    cppfvecfile = cms.FileInPath('L1Trigger/L1TMuon/data/cppf/angleScale_RPC_CPPFp3.txt')                   
+                                   #    cppfvecfile = cms.FileInPath('L1Trigger/L1TMuon/data/cppf/angleScale_RPC_CPPFp4.txt')                    
                                    )
 
