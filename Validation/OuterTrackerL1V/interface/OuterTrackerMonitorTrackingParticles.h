bc3b31ce17dbee4bfb9c38aed58ef3ff1015ab59
Emily <emacdonald16@my.whitworth.edu>
2018-03-20 17:22:24 -0500

    Reverted to previous to commit to fix compile error; re-added BuildFiles and modified; modified syntax suggested by code-checks

--- Validation/OuterTrackerL1V/interface/OuterTrackerMonitorTrackingParticles.h
+++ Validation/OuterTrackerL1V/interface/OuterTrackerMonitorTrackingParticles.h
@@ -32,74 +32,74 @@ public:
   void bookHistograms(DQMStore::IBooker &, edm::Run const &, edm::EventSetup const &) override;
 
   // Tracking particle distributions
-  MonitorElement* trackParts_Eta = 0;
-  MonitorElement* trackParts_Phi = 0;
-  MonitorElement* trackParts_Pt = 0;
+  MonitorElement* trackParts_Eta = nullptr;
+  MonitorElement* trackParts_Phi = nullptr;
+  MonitorElement* trackParts_Pt = nullptr;
 
   // Plots for correctly matched tracks
-  MonitorElement* Track_MatchedChi2 = 0; //Chi2 for only tracks correctly matched to truth level
-  MonitorElement* Track_MatchedChi2Red = 0; //Chi2/dof for only tracks correctly matched to truth level
+  MonitorElement* Track_MatchedChi2 = nullptr; //Chi2 for only tracks correctly matched to truth level
+  MonitorElement* Track_MatchedChi2Red = nullptr; //Chi2/dof for only tracks correctly matched to truth level
 
   // pT and eta for efficiency plots
-  MonitorElement* tp_pt = 0;  // denominator
-  MonitorElement* tp_eta = 0; // denominator
-  MonitorElement* tp_d0 = 0; // denominator
-  MonitorElement* tp_VtxR = 0; // denominator (also known as vxy)
-  MonitorElement* tp_VtxZ = 0; // denominator
-  MonitorElement* match_tp_pt = 0; // numerator
-  MonitorElement* match_tp_eta = 0; // numerator
-  MonitorElement* match_tp_d0 = 0; // numerator
-  MonitorElement* match_tp_VtxR = 0; // numerator (also known as vxy)
-  MonitorElement* match_tp_VtxZ = 0; // numerator
+  MonitorElement* tp_pt = nullptr;  // denominator
+  MonitorElement* tp_eta = nullptr; // denominator
+  MonitorElement* tp_d0 = nullptr; // denominator
+  MonitorElement* tp_VtxR = nullptr; // denominator (also known as vxy)
+  MonitorElement* tp_VtxZ = nullptr; // denominator
+  MonitorElement* match_tp_pt = nullptr; // numerator
+  MonitorElement* match_tp_eta = nullptr; // numerator
+  MonitorElement* match_tp_d0 = nullptr; // numerator
+  MonitorElement* match_tp_VtxR = nullptr; // numerator (also known as vxy)
+  MonitorElement* match_tp_VtxZ = nullptr; // numerator
 
   // 1D intermediate resolution plots (pT and eta)
-  MonitorElement* res_eta = 0; // for all eta and pT
-  MonitorElement* res_pt = 0; // for all eta and pT
-  MonitorElement* res_ptRel = 0; // for all eta and pT (delta(pT)/pT)
-  MonitorElement* respt_eta0to0p7_pt2to3 = 0;
-  MonitorElement* respt_eta0p7to1_pt2to3 = 0;
-  MonitorElement* respt_eta1to1p2_pt2to3 = 0;
-  MonitorElement* respt_eta1p2to1p6_pt2to3 = 0;
-  MonitorElement* respt_eta1p6to2_pt2to3 = 0;
-  MonitorElement* respt_eta2to2p4_pt2to3 = 0;
-  MonitorElement* respt_eta0to0p7_pt3to8 = 0;
-  MonitorElement* respt_eta0p7to1_pt3to8 = 0;
-  MonitorElement* respt_eta1to1p2_pt3to8 = 0;
-  MonitorElement* respt_eta1p2to1p6_pt3to8 = 0;
-  MonitorElement* respt_eta1p6to2_pt3to8 = 0;
-  MonitorElement* respt_eta2to2p4_pt3to8 = 0;
-  MonitorElement* respt_eta0to0p7_pt8toInf = 0;
-  MonitorElement* respt_eta0p7to1_pt8toInf = 0;
-  MonitorElement* respt_eta1to1p2_pt8toInf = 0;
-  MonitorElement* respt_eta1p2to1p6_pt8toInf = 0;
-  MonitorElement* respt_eta1p6to2_pt8toInf = 0;
-  MonitorElement* respt_eta2to2p4_pt8toInf = 0;
-  MonitorElement* reseta_eta0to0p7 = 0;
-  MonitorElement* reseta_eta0p7to1 = 0;
-  MonitorElement* reseta_eta1to1p2 = 0;
-  MonitorElement* reseta_eta1p2to1p6 = 0;
-  MonitorElement* reseta_eta1p6to2 = 0;
-  MonitorElement* reseta_eta2to2p4 = 0;
-  MonitorElement* resphi_eta0to0p7 = 0;
-  MonitorElement* resphi_eta0p7to1 = 0;
-  MonitorElement* resphi_eta1to1p2 = 0;
-  MonitorElement* resphi_eta1p2to1p6 = 0;
-  MonitorElement* resphi_eta1p6to2 = 0;
-  MonitorElement* resphi_eta2to2p4 = 0;
-  MonitorElement* resVtxZ_eta0to0p7 = 0;
-  MonitorElement* resVtxZ_eta0p7to1 = 0;
-  MonitorElement* resVtxZ_eta1to1p2 = 0;
-  MonitorElement* resVtxZ_eta1p2to1p6 = 0;
-  MonitorElement* resVtxZ_eta1p6to2 = 0;
-  MonitorElement* resVtxZ_eta2to2p4 = 0;
+  MonitorElement* res_eta = nullptr; // for all eta and pT
+  MonitorElement* res_pt = nullptr; // for all eta and pT
+  MonitorElement* res_ptRel = nullptr; // for all eta and pT (delta(pT)/pT)
+  MonitorElement* respt_eta0to0p7_pt2to3 = nullptr;
+  MonitorElement* respt_eta0p7to1_pt2to3 = nullptr;
+  MonitorElement* respt_eta1to1p2_pt2to3 = nullptr;
+  MonitorElement* respt_eta1p2to1p6_pt2to3 = nullptr;
+  MonitorElement* respt_eta1p6to2_pt2to3 = nullptr;
+  MonitorElement* respt_eta2to2p4_pt2to3 = nullptr;
+  MonitorElement* respt_eta0to0p7_pt3to8 = nullptr;
+  MonitorElement* respt_eta0p7to1_pt3to8 = nullptr;
+  MonitorElement* respt_eta1to1p2_pt3to8 = nullptr;
+  MonitorElement* respt_eta1p2to1p6_pt3to8 = nullptr;
+  MonitorElement* respt_eta1p6to2_pt3to8 = nullptr;
+  MonitorElement* respt_eta2to2p4_pt3to8 = nullptr;
+  MonitorElement* respt_eta0to0p7_pt8toInf = nullptr;
+  MonitorElement* respt_eta0p7to1_pt8toInf = nullptr;
+  MonitorElement* respt_eta1to1p2_pt8toInf = nullptr;
+  MonitorElement* respt_eta1p2to1p6_pt8toInf = nullptr;
+  MonitorElement* respt_eta1p6to2_pt8toInf = nullptr;
+  MonitorElement* respt_eta2to2p4_pt8toInf = nullptr;
+  MonitorElement* reseta_eta0to0p7 = nullptr;
+  MonitorElement* reseta_eta0p7to1 = nullptr;
+  MonitorElement* reseta_eta1to1p2 = nullptr;
+  MonitorElement* reseta_eta1p2to1p6 = nullptr;
+  MonitorElement* reseta_eta1p6to2 = nullptr;
+  MonitorElement* reseta_eta2to2p4 = nullptr;
+  MonitorElement* resphi_eta0to0p7 = nullptr;
+  MonitorElement* resphi_eta0p7to1 = nullptr;
+  MonitorElement* resphi_eta1to1p2 = nullptr;
+  MonitorElement* resphi_eta1p2to1p6 = nullptr;
+  MonitorElement* resphi_eta1p6to2 = nullptr;
+  MonitorElement* resphi_eta2to2p4 = nullptr;
+  MonitorElement* resVtxZ_eta0to0p7 = nullptr;
+  MonitorElement* resVtxZ_eta0p7to1 = nullptr;
+  MonitorElement* resVtxZ_eta1to1p2 = nullptr;
+  MonitorElement* resVtxZ_eta1p2to1p6 = nullptr;
+  MonitorElement* resVtxZ_eta1p6to2 = nullptr;
+  MonitorElement* resVtxZ_eta2to2p4 = nullptr;
 
   // For d0
-  MonitorElement* resd0_eta0to0p7 = 0;
-  MonitorElement* resd0_eta0p7to1 = 0;
-  MonitorElement* resd0_eta1to1p2 = 0;
-  MonitorElement* resd0_eta1p2to1p6 = 0;
-  MonitorElement* resd0_eta1p6to2 = 0;
-  MonitorElement* resd0_eta2to2p4 = 0;
+  MonitorElement* resd0_eta0to0p7 = nullptr;
+  MonitorElement* resd0_eta0p7to1 = nullptr;
+  MonitorElement* resd0_eta1to1p2 = nullptr;
+  MonitorElement* resd0_eta1p2to1p6 = nullptr;
+  MonitorElement* resd0_eta1p6to2 = nullptr;
+  MonitorElement* resd0_eta2to2p4 = nullptr;
 
  private:
    edm::ParameterSet conf_;
