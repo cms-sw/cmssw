8849730040943958c785bfe64dcd63ebd7bad65a
Jason Lee <jason.lee@cern.ch>
2018-03-24 09:37:47 +0900

    remove gemPadValidation,gemCoPadValidation for run2_GEM_2017 GEMDigiMatcher to only need gemDigis

--- Validation/MuonGEMDigis/python/MuonGEMDigis_cff.py
+++ Validation/MuonGEMDigis/python/MuonGEMDigis_cff.py
@@ -57,3 +57,6 @@ gemGeometryChecker = DQMEDAnalyzer('GEMCheckGeometry',
 )
 
 gemDigiValidation = cms.Sequence( gemStripValidation+gemPadValidation+gemCoPadValidation+gemDigiTrackValidation+gemGeometryChecker)
+ 
+from Configuration.Eras.Modifier_run2_GEM_2017_cff import run2_GEM_2017
+run2_GEM_2017.toReplaceWith(gemDigiValidation, gemDigiValidation.copyAndExclude([gemPadValidation,gemCoPadValidation]))
