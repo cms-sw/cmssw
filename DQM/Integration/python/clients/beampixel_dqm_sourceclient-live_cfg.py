02d9d68ff8c4457e1c84165ec45a5c19837a752b
Mauro <mauro.dinardo@cern.ch>
2018-03-28 21:06:02 +0200

    Update beampixel_dqm_sourceclient-live_cfg.py
    Updated tracking region for pixel tracks

--- DQM/Integration/python/clients/beampixel_dqm_sourceclient-live_cfg.py
+++ DQM/Integration/python/clients/beampixel_dqm_sourceclient-live_cfg.py
@@ -132,10 +132,10 @@ if (process.runType.getRunType() == process.runType.pp_run or process.runType.ge
     process.recopixelvertexing = cms.Sequence(process.pixelTracksSequence + process.pixelVertices)
     process.pixelVertices.TkFilterParameters.minPt = process.pixelTracksTrackingRegions.RegionPSet.ptMin
     process.pixelTracksTrackingRegions.RegionPSet.originRadius = 0.4
-    process.pixelTracksTrackingRegions.RegionPSet.originHalfLength = 6
+    process.pixelTracksTrackingRegions.RegionPSet.originHalfLength = 12
     process.pixelTracksTrackingRegions.RegionPSet.originXPos = 0.08
     process.pixelTracksTrackingRegions.RegionPSet.originYPos = -0.03
-    process.pixelTracksTrackingRegions.RegionPSet.originZPos = 1
+    process.pixelTracksTrackingRegions.RegionPSet.originZPos = 0.
 
 
     #----------------------------
