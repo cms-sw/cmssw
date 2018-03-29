07cd919ad5ea9e3836b5d31a1e28237d39751410
Laurent Forthomme <laurent.forthomme@cern.ch>
2018-03-29 15:28:09 +0200

    Stripped the TOTEM timing DQM subpart from the CTPPS DQM

--- DQM/CTPPS/python/ctppsDQM_cff.py
+++ DQM/CTPPS/python/ctppsDQM_cff.py
@@ -7,14 +7,11 @@ from DQM.CTPPS.totemRPDQMSource_cfi import *
 
 from DQM.CTPPS.ctppsDiamondDQMSource_cfi import *
 
-from DQM.CTPPS.totemTimingDQMSource_cfi import *
-
 from DQM.CTPPS.ctppsPixelDQMSource_cfi import *
 
 ctppsDQM = cms.Sequence(
     totemDAQTriggerDQMSource
     + (totemRPDQMSource * totemRPDQMHarvester)
     + ctppsDiamondDQMSource
-    + totemTimingDQMSource
     + ctppsPixelDQMSource
 )
