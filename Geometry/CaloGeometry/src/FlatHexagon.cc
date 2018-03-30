7f6ce4ef3e8d5a1338f700f33be3a60133d6e9a9
Sunanda <sunanda.banerjee@cern.ch>
2018-03-30 16:30:04 +0200

    Add a new shape for hexgonal geometry

--- Geometry/CaloGeometry/src/FlatHexagon.cc
+++ Geometry/CaloGeometry/src/FlatHexagon.cc
@@ -1,15 +1,15 @@
 #include "FWCore/MessageLogger/interface/MessageLogger.h"
-#include "Geometry/CaloGeometry/interface/FlatTrd.h"
+#include "Geometry/CaloGeometry/interface/FlatHexagon.h"
 #include "Geometry/CaloGeometry/interface/TruncatedPyramid.h"
 #include <algorithm>
 #include <iostream>
 
 //#define EDM_ML_DEBUG
 
-typedef FlatTrd::CCGFloat CCGFloat ;
-typedef FlatTrd::Pt3D     Pt3D     ;
-typedef FlatTrd::Pt3DVec  Pt3DVec  ;
-typedef FlatTrd::Tr3D     Tr3D     ;
+typedef FlatHexagon::CCGFloat CCGFloat ;
+typedef FlatHexagon::Pt3D     Pt3D     ;
+typedef FlatHexagon::Pt3DVec  Pt3DVec  ;
+typedef FlatHexagon::Tr3D     Tr3D     ;
 
 typedef HepGeom::Vector3D<CCGFloat> FVec3D   ;
 typedef HepGeom::Plane3D<CCGFloat>  Plane3D ;
@@ -20,16 +20,16 @@ typedef HepGeom::Point3D<double>  DPt3D ;
 
 //----------------------------------------------------------------------
 
-FlatTrd::FlatTrd() : CaloCellGeometry(),  m_axis ( 0., 0., 0. ),
-		     m_corOne ( 0., 0., 0. ), m_local (0., 0., 0.),
-		     m_global ( 0., 0., 0. ) {
+FlatHexagon::FlatHexagon() : CaloCellGeometry(),  m_axis ( 0., 0., 0. ),
+			     m_corOne ( 0., 0., 0. ), m_local (0., 0., 0.),
+			     m_global ( 0., 0., 0. ) {
 }
 
-FlatTrd::FlatTrd( const FlatTrd& tr ) : CaloCellGeometry( tr ) {
+FlatHexagon::FlatHexagon( const FlatHexagon& tr ) : CaloCellGeometry( tr ) {
   *this = tr ; 
 }
 
-FlatTrd& FlatTrd::operator=( const FlatTrd& tr ) {
+FlatHexagon& FlatHexagon::operator=( const FlatHexagon& tr ) {
   CaloCellGeometry::operator=( tr ) ;
   if ( this != &tr ) {
     m_axis   = tr.m_axis ;
@@ -39,20 +39,20 @@ FlatTrd& FlatTrd::operator=( const FlatTrd& tr ) {
     m_tr     = tr.m_tr;
   }
 #ifdef EDM_ML_DEBUG
-  edm::LogVerbatim("CaloGeometry") << "FlatTrd(Copy): Local " << m_local 
-				   << " Global " << m_global << " eta " 
+  edm::LogVerbatim("CaloGeometry") << "FlatHexagon(Copy): Local " << m_local 
+				   << " Global " << m_global  << " eta " 
 				   << etaPos() << " phi " << phiPos() 
-				   << " Translation " << m_tr.getTranslation()
+				   << " Translation " << m_tr.getTranslation() 
 				   << " and rotation " << m_tr.getRotation();
 #endif
   return *this ; 
 }
 
-FlatTrd::FlatTrd( CornersMgr*  cMgr ,
-		  const GlobalPoint& fCtr ,
-		  const GlobalPoint& bCtr ,
-		  const GlobalPoint& cor1 ,
-		  const CCGFloat*    parV ) :
+FlatHexagon::FlatHexagon( CornersMgr*  cMgr ,
+			  const GlobalPoint& fCtr ,
+			  const GlobalPoint& bCtr ,
+			  const GlobalPoint& cor1 ,
+			  const CCGFloat*    parV ) :
   CaloCellGeometry ( fCtr, cMgr, parV ) ,
   m_axis           ( ( bCtr - fCtr ).unit() ) ,
   m_corOne         ( cor1.x(), cor1.y(), cor1.z() ),
@@ -61,16 +61,16 @@ FlatTrd::FlatTrd( CornersMgr*  cMgr ,
   Pt3D glb = m_tr*m_local;
   m_global = GlobalPoint(glb.x(),glb.y(),glb.z());
 #ifdef EDM_ML_DEBUG
-  edm::LogVerbatim("CaloGeometry") << "FlatTrd: Local " << m_local 
+  edm::LogVerbatim("CaloGeometry") << "FlatHexagon: Local " << m_local 
 				   << " Global " << glb << " eta "
 				   << etaPos() << " phi " << phiPos() 
-				   << " Translation " << m_tr.getTranslation() 
+				   << " Translation " << m_tr.getTranslation()
 				   << " and rotation " << m_tr.getRotation();
 #endif
 } 
 
-FlatTrd::FlatTrd( const CornersVec& corn ,
-		  const CCGFloat*   par    ) :
+FlatHexagon::FlatHexagon( const CornersVec& corn ,
+			  const CCGFloat*   par    ) :
   CaloCellGeometry ( corn, par  ) , 
   m_corOne         ( corn[0].x(), corn[0].y(), corn[0].z() ),
   m_local          (0., 0., 0.) {
@@ -79,90 +79,90 @@ FlatTrd::FlatTrd( const CornersVec& corn ,
   Pt3D glb = m_tr*m_local;
   m_global = GlobalPoint(glb.x(),glb.y(),glb.z());
 #ifdef EDM_ML_DEBUG
-  edm::LogVerbatim("CaloGeometry") << "FlatTrd: Local " << m_local
+  edm::LogVerbatim("CaloGeometry") << "FlatHexagon: Local " << m_local 
 				   << " Global " << glb << " eta "
-				   << etaPos() << " phi " << phiPos()
+				   << etaPos() << " phi " << phiPos() 
 				   << " Translation " << m_tr.getTranslation()
 				   << " and rotation " << m_tr.getRotation();
 #endif
 } 
 
-FlatTrd::FlatTrd( const FlatTrd& tr, const Pt3D & local ) : 
+FlatHexagon::FlatHexagon( const FlatHexagon& tr, const Pt3D & local ) : 
   CaloCellGeometry( tr ),  m_local(local) {
   *this = tr;
   Pt3D glb = m_tr*m_local;
   m_global = GlobalPoint(glb.x(),glb.y(),glb.z());
 #ifdef EDM_ML_DEBUG
-  edm::LogVerbatim("CaloGeometry") << "FlatTrd: Local " << m_local 
+  edm::LogVerbatim("CaloGeometry") << "FlatHexagon: Local " << m_local 
 				   << " Global " << glb << " eta "
-				   << etaPos() << " phi " << phiPos()
+				   << etaPos() << " phi " << phiPos() 
 				   << " Translation " << m_tr.getTranslation()
 				   << " and rotation " << m_tr.getRotation();
 #endif
 }
 
-FlatTrd::~FlatTrd() {}
+FlatHexagon::~FlatHexagon() {}
 
-GlobalPoint FlatTrd::getPosition(const Pt3D& local ) const {
+GlobalPoint FlatHexagon::getPosition(const Pt3D& local ) const {
   Pt3D glb = m_tr*local;
 #ifdef EDM_ML_DEBUG
-  edm::LogVerbatim("CaloGeometry") << "FlatTrd::Local " << local.x() << ":" 
+  edm::LogVerbatim("CaloGeometry") << "FlatHexagon::Local " << local.x() << ":"
 				   << local.y() << ":" << local.z() 
-				   << " Global " << glb.x() << ":" << glb.y()
-				   << ":" << glb.z() << " TR " << m_tr.xx()
-				   << ":" << m_tr.xy() << ":" << m_tr.xz()
+				   << " Global " << glb.x() << ":" << glb.y() 
+				   << ":" << glb.z() << " TR " << m_tr.xx() 
+				   << ":" << m_tr.xy() << ":" << m_tr.xz() 
 				   << ":" << m_tr.yx() << ":" << m_tr.yy() 
 				   << ":" << m_tr.yz() << ":" << m_tr.zx() 
 				   << ":" << m_tr.zy() << ":" << m_tr.zz() 
-				   << ":" << m_tr.dx() << ":" << m_tr.dy()
+				   << ":" << m_tr.dx() << ":" << m_tr.dy() 
 				   << ":" << m_tr.dz();
 #endif
   return GlobalPoint(glb.x(),glb.y(),glb.z());
 }
 
-Pt3D FlatTrd::getLocal(const GlobalPoint& global) const {
+Pt3D FlatHexagon::getLocal(const GlobalPoint& global) const {
   Pt3D local = m_tr.inverse()*Pt3D(global.x(),global.y(),global.z());
 #ifdef EDM_ML_DEBUG
-  edm::LogVerbatim("CaloGeometry") << "FlatTrd::Global " << global.x() << ":" 
-				   << global.y() << ":" << global.z()
+  edm::LogVerbatim("CaloGeometry") << "FlatHexagon::Global " << global.x() 
+				   << ":" << global.y() << ":" << global.z()
 				   << " Local " << local.x() << ":" 
 				   << local.y() << ":" << local.z();
 #endif
   return local;
 }
 
-CCGFloat FlatTrd::getThetaAxis() const { 
+CCGFloat FlatHexagon::getThetaAxis() const { 
   return m_axis.theta() ; 
 } 
 
-CCGFloat FlatTrd::getPhiAxis() const {
+CCGFloat FlatHexagon::getPhiAxis() const {
   return m_axis.phi() ; 
 } 
 
-const GlobalVector& FlatTrd::axis() const { 
+const GlobalVector& FlatHexagon::axis() const { 
   return m_axis ; 
 }
 
-void FlatTrd::vocalCorners( Pt3DVec&        vec ,
-			    const CCGFloat* pv  ,
-			    Pt3D&           ref  ) const { 
+void FlatHexagon::vocalCorners( Pt3DVec&        vec ,
+				const CCGFloat* pv  ,
+				Pt3D&           ref  ) const { 
   localCorners( vec, pv, ref ) ; 
 }
 
-void  FlatTrd::createCorners( const std::vector<CCGFloat>&  pv ,
-			      const Tr3D&                   tr ,
-			      std::vector<GlobalPoint>&     co   ) {
+void  FlatHexagon::createCorners( const std::vector<CCGFloat>&  pv ,
+				  const Tr3D&                   tr ,
+				  std::vector<GlobalPoint>&     co   ) {
 
-  assert( 11 <= pv.size() ) ;
-  assert( 8 == co.size() ) ;
+  assert(  3 == pv.size() ) ;
+  assert( 12 == co.size() ) ;
 
-  Pt3DVec        ko ( 8, Pt3D(0,0,0) ) ;
+  Pt3DVec        ko ( 12, Pt3D(0,0,0) ) ;
 
   Pt3D    tmp ;
-  Pt3DVec to ( 8, Pt3D(0,0,0) ) ;
+  Pt3DVec to ( 12, Pt3D(0,0,0) ) ;
   localCorners( to, &pv.front(), tmp ) ;
 
-  for( unsigned int i ( 0 ) ; i != 8 ; ++i ) {
+  for( unsigned int i ( 0 ) ; i != 12 ; ++i ) {
     ko[i] = tr * to[i] ; // apply transformation
     const Pt3D & p ( ko[i] ) ;
     co[ i ] = GlobalPoint( p.x(), p.y(), p.z() ) ;
@@ -172,51 +172,53 @@ void  FlatTrd::createCorners( const std::vector<CCGFloat>&  pv ,
   }
 }
 
-void FlatTrd::localCorners( Pt3DVec&        lc  ,
-			    const CCGFloat* pv  ,
-			    Pt3D&           ref   ) {
+void FlatHexagon::localCorners( Pt3DVec&        lc  ,
+				const CCGFloat* pv  ,
+				Pt3D&           ref   ) {
    assert( nullptr != pv ) ;
-   assert( 8 == lc.size() ) ;
+   assert( 12 == lc.size() ) ;
 
    const CCGFloat dz ( pv[0] ) ;
-   const CCGFloat h  ( pv[3] ) ;
-   const CCGFloat bl ( pv[4] ) ;
-   const CCGFloat tl ( pv[5] ) ;
-   const CCGFloat a1 ( pv[6] ) ;
-  
-   const CCGFloat ta1 ( tan( a1 ) ) ;
+   const CCGFloat r  ( pv[1] ) ;
+   const CCGFloat R  ( pv[2] ) ;
 
-   lc[0] = Pt3D ( - h*ta1 - bl, - h , -dz ); // (-,-,-)
-   lc[1] = Pt3D ( + h*ta1 - tl, + h , -dz ); // (-,+,-)
-   lc[2] = Pt3D ( + h*ta1 + tl, + h , -dz ); // (+,+,-)
-   lc[3] = Pt3D ( - h*ta1 + bl, - h , -dz ); // (+,-,-)
-   lc[4] = Pt3D ( - h*ta1 - bl, - h ,  dz ); // (-,-,+)
-   lc[5] = Pt3D ( + h*ta1 - tl, + h ,  dz ); // (-,+,+)
-   lc[6] = Pt3D ( + h*ta1 + tl, + h ,  dz ); // (+,+,+)
-   lc[7] = Pt3D ( - h*ta1 + bl, - h ,  dz ); // (+,-,+)
+   lc[0] = Pt3D (   0 ,  - 2*R , -dz ); // (0,-,-)
+   lc[1] = Pt3D ( - r ,  -   R , -dz ); // (-,-,-)
+   lc[2] = Pt3D ( - r ,      R , -dz ); // (-,+,-)
+   lc[3] = Pt3D (   0 ,    2*R , -dz ); // (0,+,-)
+   lc[4] = Pt3D (   r ,      R , -dz ); // (+,+,-)
+   lc[5] = Pt3D (   r ,  -   R , -dz ); // (+,-,-)
+   lc[6] = Pt3D (   0 ,  - 2*R ,  dz ); // (0,-,+)
+   lc[7] = Pt3D ( - r ,  -   R ,  dz ); // (-,-,+)
+   lc[8] = Pt3D ( - r ,      R ,  dz ); // (-,+,+)
+   lc[9] = Pt3D (   0 ,    2*R ,  dz ); // (0,+,+)
+   lc[10]= Pt3D (   r ,      R ,  dz ); // (+,+,+)
+   lc[11]= Pt3D (   r ,  -   R ,  dz ); // (+,-,+)
 
-   ref   = 0.25*( lc[0] + lc[1] + lc[2] + lc[3] ) ;
+   ref   = ( lc[0] + lc[1] + lc[2] + lc[3] + lc[4] + lc[5] ) / 6.0;
 #ifdef EDM_ML_DEBUG
    edm::LogVerbatim("CaloGeometry") << "Ref " << ref << " Local Corners " 
 				    << lc[0] << "|" << lc[1] << "|" << lc[2] 
 				    << "|" << lc[3] << "|" << lc[4] << "|" 
-				    << lc[5] << "|" << lc[6] << "|" << lc[7];
+				    << lc[5] << "|" << lc[6] << "|" << lc[7] 
+				    << "|" << lc[8] << "|" << lc[9] << "|"
+				    << lc[10] << "|" << lc[11];
 #endif
 }
 
-void FlatTrd::getTransform( Tr3D& tr, Pt3DVec* lptr ) const {
+void FlatHexagon::getTransform( Tr3D& tr, Pt3DVec* lptr ) const {
   const GlobalPoint& p ( CaloCellGeometry::getPosition() ) ;
   const Pt3D         gFront ( p.x(), p.y(), p.z() ) ;
   const DPt3D        dgFront ( p.x(), p.y(), p.z() ) ;
 
   Pt3D  lFront ;
   assert( nullptr != param() ) ;
-  std::vector<Pt3D > lc( 8, Pt3D(0,0,0) ) ;
+  std::vector<Pt3D > lc( 12, Pt3D(0,0,0) ) ;
   localCorners( lc, param(), lFront ) ;
 
   // figure out if reflction volume or not
 
-  Pt3D  lBack  ( 0.25*(lc[4]+lc[5]+lc[6]+lc[7]) ) ;
+  Pt3D  lBack  ( (lc[6]+lc[7]+lc[8]+lc[9]+lc[10]+lc[11]) / 6.0 ) ;
 
   const DPt3D dlFront ( lFront.x(), lFront.y(), lFront.z() ) ;
   const DPt3D dlBack  ( lBack.x() , lBack.y() , lBack.z()  ) ;
@@ -249,7 +251,7 @@ void FlatTrd::getTransform( Tr3D& tr, Pt3DVec* lptr ) const {
   if( nullptr != lptr ) (*lptr) = lc ;
 }
 
-void FlatTrd::initCorners(CaloCellGeometry::CornersVec& co) {
+void FlatHexagon::initCorners(CaloCellGeometry::CornersVec& co) {
 
   if( co.uninitialized() ) {
     CornersVec& corners ( co ) ;
@@ -257,19 +259,19 @@ void FlatTrd::initCorners(CaloCellGeometry::CornersVec& co) {
     Tr3D tr ;
     getTransform( tr, &lc ) ;
 
-    for (unsigned int i ( 0 ) ; i != 8 ; ++i ) {
+    for (unsigned int i ( 0 ) ; i != 12 ; ++i ) {
       const Pt3D corn ( tr*lc[i] ) ;
       corners[i] = GlobalPoint( corn.x(), corn.y(), corn.z() ) ;
     }
   }
 }
 
-GlobalVector FlatTrd::makeAxis() { 
+GlobalVector FlatHexagon::makeAxis() { 
   return GlobalVector( backCtr() - getPosition() ).unit() ;
 }
 
-GlobalPoint FlatTrd::backCtr() const {
-  float dz = (getCorners()[4].z() > getCorners()[0].z()) ? 
+GlobalPoint FlatHexagon::backCtr() const {
+  float dz = (getCorners()[6].z() > getCorners()[0].z()) ? 
     param()[0] : -param()[0];
   Pt3D local_b(m_local.x(),m_local.y(),m_local.z()+dz);
   Pt3D global_b = m_tr*local_b;
@@ -280,7 +282,7 @@ GlobalPoint FlatTrd::backCtr() const {
 //----------------------------------------------------------------------
 //----------------------------------------------------------------------
 
-std::ostream& operator<<( std::ostream& s, const FlatTrd& cell ) {
+std::ostream& operator<<( std::ostream& s, const FlatHexagon& cell ) {
   s << "Center: " << cell.getPosition() << " eta " << cell.etaPos()
     << " phi " << cell.phiPos() << std::endl;
   s << "Axis: " << cell.getThetaAxis() << " " << cell.getPhiAxis() <<std::endl;
