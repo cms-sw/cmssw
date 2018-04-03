c3d35d038f54ac0a02558c66292800345d12b923
tocheng <tocheng.cms@gmail.com>
2018-04-03 23:45:10 +0200

    Reduce SiPixelDetectorStatus data size and change the SiPixelStatusProducer accordingly

--- CalibTracker/SiPixelQuality/interface/SiPixelRocStatus.h
+++ CalibTracker/SiPixelQuality/interface/SiPixelRocStatus.h
@@ -8,35 +8,20 @@ class SiPixelRocStatus {
 public:
   SiPixelRocStatus();
   ~SiPixelRocStatus();
-  void fillDIGI(int idc);
-  void updateDIGI(int idc, unsigned long int hits);
-
-  void fillStuckTBM(unsigned int fed, unsigned int link, std::time_t time);
-  void updateStuckTBM(unsigned int fed, unsigned int link, std::time_t time, unsigned long int freq);
+  void fillDIGI();
+  void updateDIGI(unsigned int hits);
+  void fillStuckTBM();
 
   // stuckTBM
   bool isStuckTBM(){ return isStuckTBM_; }
-  unsigned int getBadFed(){ return badFed_; }
-  unsigned int getBadLink(){ return badLink_; }
-  std::time_t getStartBadTime(){ return startBadTime_; }
-  unsigned long int getBadFreq(){ return badFreq_; }
 
   // occpancy
-  unsigned long int digiOccDC(int idc);
-  unsigned long int digiOccROC();
-
-  int nDC(){ return nDC_;}
-   
+  unsigned int digiOccROC();
 
 private:
-  const int nDC_ = 26;
-  unsigned long int fDC[26];
 
+  unsigned int fDC;
   bool isStuckTBM_;
-  unsigned int badFed_;
-  unsigned int badLink_;
-  std::time_t startBadTime_;
-  unsigned long int badFreq_;
  
 };
 
