1c8b8036425e96103b304a751d4a4bcb595a1a3d
Giovanni <gpetruc@gmail.com>
2018-04-04 11:10:31 +0200

    Change L1EGCrystalCluster to inherit from l1t::L1Candidate, add cfi file

--- DataFormats/Phase2L1CaloTrig/interface/L1EGCrystalCluster.h
+++ DataFormats/Phase2L1CaloTrig/interface/L1EGCrystalCluster.h
@@ -5,15 +5,15 @@
 #include <map>
 #include <string>
 #include <algorithm>
-#include "DataFormats/Candidate/interface/LeafCandidate.h"
+#include "DataFormats/L1Trigger/interface/L1Candidate.h"
 #include "DataFormats/DetId/interface/DetId.h"
 
 namespace l1slhc
 {
 
-  class L1EGCrystalCluster : public reco::LeafCandidate {
+  class L1EGCrystalCluster : public l1t::L1Candidate {
     public:
-      L1EGCrystalCluster() : LeafCandidate(), calibratedPt_(0.), hovere_(0.), iso_(0.), PUcorrPt_(0.), bremStrength_(0.),
+      L1EGCrystalCluster() : l1t::L1Candidate(), calibratedPt_(0.), hovere_(0.), iso_(0.), PUcorrPt_(0.), bremStrength_(0.),
             e2x2_(0.), e2x5_(0.), e3x5_(0.), e5x5_(0.), electronWP98_(0.), photonWP80_(0.), electronWP90_(0.),
             looseL1TkMatchWP_(0.), stage2effMatch_(0.) {};
 
@@ -21,7 +21,7 @@ namespace l1slhc
             float PUcorrPt = 0., float bremStrength = 0., float e2x2 = 0., float e2x5 = 0.,
             float e3x5 = 0., float e5x5 = 0., bool electronWP98 = false, bool photonWP80 = false,
             bool electronWP90 = false, bool looseL1TkMatchWP = false, bool stage2effMatch = false ) :
-                    LeafCandidate(0, p4), calibratedPt_(calibratedPt), hovere_(hovere), iso_(iso), seedCrystal_(seedCrystal),
+                    l1t::L1Candidate(p4), calibratedPt_(calibratedPt), hovere_(hovere), iso_(iso), seedCrystal_(seedCrystal),
                     PUcorrPt_(PUcorrPt), bremStrength_(bremStrength), e2x2_(e2x2), e2x5_(e2x5),
                     e3x5_(e3x5), e5x5_(e5x5), electronWP98_(electronWP98), photonWP80_(photonWP80),
                     electronWP90_(electronWP90), looseL1TkMatchWP_(looseL1TkMatchWP),
