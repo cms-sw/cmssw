d5ab2762fb9b438c88ad0ee1f8b919b78172941c
Gaelle <Gaelle.Boudoul@cern.ch>
2018-04-06 08:34:15 +0200

    Rename

--- SLHCUpgradeSimulations/Geometry/python/fakeConditions_phase2TkT5_cff.py
+++ SLHCUpgradeSimulations/Geometry/python/fakeConditions_phase2TkT5_cff.py
@@ -8,6 +8,6 @@ es_prefer_fake_gain = cms.ESPrefer("SiPixelFakeGainOfflineESSource","siPixelFake
 
 siPixelFakeLorentzAngleESSource = cms.ESSource("SiPixelFakeLorentzAngleESSource",
         file = 
-cms.FileInPath('SLHCUpgradeSimulations/Geometry/data/PhaseII/Tilted/PixelSkimmedGeometry_base.txt')
+cms.FileInPath('SLHCUpgradeSimulations/Geometry/data/PhaseII/Tilted/PixelSkimmedGeometryT5.txt')
         )
 es_prefer_fake_lorentz = cms.ESPrefer("SiPixelFakeLorentzAngleESSource","siPixelFakeLorentzAngleESSource")
--- SLHCUpgradeSimulations/Geometry/python/fakeConditions_phase2TkT6_cff.py
+++ SLHCUpgradeSimulations/Geometry/python/fakeConditions_phase2TkT6_cff.py
@@ -8,6 +8,6 @@ es_prefer_fake_gain = cms.ESPrefer("SiPixelFakeGainOfflineESSource","siPixelFake
 
 siPixelFakeLorentzAngleESSource = cms.ESSource("SiPixelFakeLorentzAngleESSource",
         file = 
-cms.FileInPath('SLHCUpgradeSimulations/Geometry/data/PhaseII/Tilted/PixelSkimmedGeometry.txt')
+cms.FileInPath('SLHCUpgradeSimulations/Geometry/data/PhaseII/Tilted/PixelSkimmedGeometryT6.txt')
         )
 es_prefer_fake_lorentz = cms.ESPrefer("SiPixelFakeLorentzAngleESSource","siPixelFakeLorentzAngleESSource")
