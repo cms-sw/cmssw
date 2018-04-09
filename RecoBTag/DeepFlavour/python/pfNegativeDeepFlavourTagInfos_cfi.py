e42d3b03878ed8b9dbae1dd180c2d77f6d762cd2
Emil Bols <emilbols@hotmail.com>
2018-04-09 16:53:18 +0200

    Added correct vertex collection to DeepCSV+DeepFlavour

--- RecoBTag/DeepFlavour/python/pfNegativeDeepFlavourTagInfos_cfi.py
+++ RecoBTag/DeepFlavour/python/pfNegativeDeepFlavourTagInfos_cfi.py
@@ -4,5 +4,6 @@ from RecoBTag.DeepFlavour.pfDeepFlavourTagInfos_cfi import pfDeepFlavourTagInfos
 
 pfNegativeDeepFlavourTagInfos = pfDeepFlavourTagInfos.clone(
         shallow_tag_infos = cms.InputTag('pfDeepCSVNegativeTagInfos'),
+        secondary_vertices = cms.InputTag('inclusiveCandidateNegativeSecondaryVertices'),
         flip = cms.bool(True)
         )
