a0f2c127bd6f1b8a5a92f3bcaf547ede325d3513
nminafra <nicola@cern.ch>
2018-04-09 19:04:23 +0200

    DQM using TotemTimingDigi

---
+++ DQM/CTPPS/test/timing_dqm_test_cfg.py
@@ -0,0 +1,64 @@
+import FWCore.ParameterSet.Config as cms
+import string
+
+process = cms.Process('RECODQM')
+
+process.maxEvents = cms.untracked.PSet( input = cms.untracked.int32(1000) )
+process.verbosity = cms.untracked.PSet( input = cms.untracked.int32(-1) )
+
+# minimum of logs
+process.MessageLogger = cms.Service("MessageLogger",
+    statistics = cms.untracked.vstring(),
+    destinations = cms.untracked.vstring('cerr'),
+    cerr = cms.untracked.PSet(
+        threshold = cms.untracked.string('WARNING')
+    )
+)
+    
+# import of standard configurations
+process.load('Configuration.StandardSequences.Services_cff')
+process.load('FWCore.MessageService.MessageLogger_cfi')
+process.load('Configuration.EventContent.EventContent_cff')
+process.load('Configuration.StandardSequences.FrontierConditions_GlobalTag_cff')
+
+# load DQM framework
+process.load("DQM.Integration.config.environment_cfi")
+process.dqmEnv.subSystemFolder = "CTPPS"
+process.dqmEnv.eventInfoFolder = "EventInfo"
+process.dqmSaver.path = ""
+process.dqmSaver.tag = "CTPPS"
+
+process.source = cms.Source("NewEventStreamFileReader",
+  fileNames = cms.untracked.vstring(
+    'file:/eos/cms/store/group/dpg_ctpps/comm_ctpps/TotemTiming/Minidaq/860/run312860_ls0002_streamA_StorageManager.dat'
+  )
+)
+
+
+from Configuration.AlCa.GlobalTag import GlobalTag
+process.GlobalTag = GlobalTag(process.GlobalTag, 'auto:run2_hlt_relval', '')
+
+# raw-to-digi conversion
+process.load("EventFilter.CTPPSRawToDigi.ctppsRawToDigi_cff")
+
+# local RP reconstruction chain with standard settings
+process.load("RecoCTPPS.Configuration.recoCTPPS_cff")
+
+# CTPPS DQM modules
+process.load("DQM.CTPPS.ctppsDQM_cff")
+
+process.path = cms.Path(
+    process.ctppsRawToDigi *
+    process.recoCTPPS *
+    process.ctppsDQM
+)
+
+process.end_path = cms.EndPath(
+    process.dqmEnv +
+    process.dqmSaver
+)
+
+process.schedule = cms.Schedule(
+    process.path,
+    process.end_path
+)
