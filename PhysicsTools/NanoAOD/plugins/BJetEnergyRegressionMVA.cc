52019f4d7d157aba20897f35755bd3078f146570
Andrea <andrea.rizzi@cern.ch>
2018-04-10 10:44:27 +0200

    nadya's regression

--- PhysicsTools/NanoAOD/plugins/BJetEnergyRegressionMVA.cc
+++ PhysicsTools/NanoAOD/plugins/BJetEnergyRegressionMVA.cc
@@ -33,7 +33,8 @@ class BJetEnergyRegressionMVA : public BaseMVAValueMapProducer<pat::Jet> {
 	  explicit BJetEnergyRegressionMVA(const edm::ParameterSet &iConfig):
 		BaseMVAValueMapProducer<pat::Jet>(iConfig),
     		pvsrc_(edm::stream::EDProducer<>::consumes<std::vector<reco::Vertex>>(iConfig.getParameter<edm::InputTag>("pvsrc"))),
-    		svsrc_(edm::stream::EDProducer<>::consumes<edm::View<reco::VertexCompositePtrCandidate>> (iConfig.getParameter<edm::InputTag>("svsrc")))
+    		svsrc_(edm::stream::EDProducer<>::consumes<edm::View<reco::VertexCompositePtrCandidate>> (iConfig.getParameter<edm::InputTag>("svsrc"))),
+    		rhosrc_(edm::stream::EDProducer<>::consumes<double> (iConfig.getParameter<edm::InputTag>("rhosrc")))
 
 	  {
 
@@ -42,66 +43,94 @@ class BJetEnergyRegressionMVA : public BaseMVAValueMapProducer<pat::Jet> {
 	  void readAdditionalCollections(edm::Event&iEvent, const edm::EventSetup&) override {
 		iEvent.getByToken(pvsrc_, pvs_);
 		iEvent.getByToken(svsrc_, svs_);
-	  }
+        iEvent.getByToken(rhosrc_,rho_);
 
-          void fillAdditionalVariables(const pat::Jet&j)  override {
-		this->setValue("nPVs",(pvs_->size()-19.1268252508)*0.138597756712);
+      }
 
-        /*		BaseMVAValueMapProducer<pat::Jet>::setValue("Jet_leptonPtRel",0);
+    void fillAdditionalVariables(const pat::Jet&j)  override {
 
-		if(!j.overlaps("muons").empty()) { 
-			const auto *lep=dynamic_cast<const pat::Muon *>(&*j.overlaps("muons")[0]);
-			if(lep!=nullptr) {BaseMVAValueMapProducer<pat::Jet>::setValue("Jet_leptonPtRel",lep->userFloat("ptRel"));}
-		}
-		else if(!j.overlaps("electrons").empty()) {
-			const auto *lep=dynamic_cast<const pat::Electron *>(&*j.overlaps("electrons")[0]);
-			if(lep!=nullptr) {BaseMVAValueMapProducer<pat::Jet>::setValue("Jet_leptonPtRel",lep->userFloat("ptRel"));}
-		}
-		
-		float ptMax=0;
-		for(const auto & d : j.daughterPtrVector()){if(d->pt()>ptMax) ptMax=d->pt();}
-		BaseMVAValueMapProducer<pat::Jet>::setValue("Jet_leadTrackPt",ptMax);
+    this->setValue("nPVs",(pvs_->size()-19.1268252508)*0.138597756712);
+    this->setValue("rho",*(rho_.product()));
 
-		//Fill vertex properties
-		VertexDistance3D vdist;
-		float maxFoundSignificance=0;
-		const auto & pv = (*pvs_)[0];
-	  	this->setValue("Jet_vtxPt",0);
-	   	this->setValue("Jet_vtxMass",0);
-    		this->setValue("Jet_vtx3dL",0);
-    		this->setValue("Jet_vtx3deL",0);
-    		this->setValue("Jet_vtxNtrk",0);
+    
+    float cone_boundaries[] = { 0.05, 0.1, 0.2, 0.3,0.4 }; 
+    size_t ncone_boundaries = sizeof(cone_boundaries)/sizeof(float);
+    std::vector<float> EmFractionEnergyRings(ncone_boundaries+1);
+    std::vector<float> ChFractionEnergyRings(ncone_boundaries+1);
+    std::vector<float> NeFractionEnergyRings(ncone_boundaries+1);
+    std::vector<float> MuFractionEnergyRings(ncone_boundaries+1);
+    float jetRawEnergy=j.p4().E()*j.jecFactor("Uncorrected");
+    int numDaughtersPt03=0;
+        for (unsigned int ijcone = 0; ijcone<ncone_boundaries; ijcone++){
+            EmFractionEnergyRings[ijcone] = 0;
+            MuFractionEnergyRings[ijcone] = 0;
+            ChFractionEnergyRings[ijcone] = 0;
+            NeFractionEnergyRings[ijcone] = 0;
+        }
+        for(const auto & d : j.daughterPtrVector()){
+           float candDr   = Geom::deltaR(d->p4(),j.p4());
+           size_t icone = std::lower_bound(&cone_boundaries[0],&cone_boundaries[ncone_boundaries],candDr) - &cone_boundaries[0];
+           float candEnergy = d->energy()/jetRawEnergy;
+           int pdgid = abs(d->pdgId()) ;
+           if( pdgid == 22 || pdgid == 11 ) {
+               EmFractionEnergyRings[icone] += candEnergy;
+            } else if ( pdgid == 13 ) { 
+               MuFractionEnergyRings[icone] += candEnergy;
+           } else if ( d->charge() != 0 ) {
+               ChFractionEnergyRings[icone] += candEnergy;
+           } else {
+               NeFractionEnergyRings[icone] += candEnergy;
+           } 
+           if(d->pt()>0.3) numDaughtersPt03+=1;
+         } // end of jet daughters loop
 
-		for(const auto &sv: *svs_){
-	      	GlobalVector flightDir(sv.vertex().x() - pv.x(), sv.vertex().y() - pv.y(),sv.vertex().z() - pv.z());
-	            GlobalVector jetDir(j.px(),j.py(),j.pz());
-                    if( Geom::deltaR2( flightDir, jetDir ) < 0.09 ){
-		        Measurement1D dl= vdist.distance(pv,VertexState(RecoVertex::convertPos(sv.position()),RecoVertex::convertError(sv.error())));
-			if(dl.significance() > maxFoundSignificance){
-				 maxFoundSignificance=dl.significance();
-				 this->setValue("Jet_vtxPt",sv.pt());
-				 this->setValue("Jet_vtxMass",sv.p4().M());
-				 this->setValue("Jet_vtx3dL",dl.value());
-				 this->setValue("Jet_vtx3deL",dl.error());
-				 this->setValue("Jet_vtxNtrk",sv.numberOfSourceCandidatePtrs());
-			}	
-		    } 
-		}
-*/
+    this->setValue("Jet_energyRing_dR0_em_Jet_rawEnergy", EmFractionEnergyRings[0]);
+    this->setValue("Jet_energyRing_dR1_em_Jet_rawEnergy", EmFractionEnergyRings[1]);
+    this->setValue("Jet_energyRing_dR2_em_Jet_rawEnergy", EmFractionEnergyRings[2]);
+    this->setValue("Jet_energyRing_dR3_em_Jet_rawEnergy", EmFractionEnergyRings[3]);
+    this->setValue("Jet_energyRing_dR4_em_Jet_rawEnergy", EmFractionEnergyRings[4]);
+  //  this->setValue("Jet_energyRing_dR5_em_Jet_rawEnergy", EmFractionEnergyRings[5]);
+    
+    this->setValue("Jet_energyRing_dR0_ch_Jet_rawEnergy", ChFractionEnergyRings[0]);
+    this->setValue("Jet_energyRing_dR1_ch_Jet_rawEnergy", ChFractionEnergyRings[1]);
+    this->setValue("Jet_energyRing_dR2_ch_Jet_rawEnergy", ChFractionEnergyRings[2]);
+    this->setValue("Jet_energyRing_dR3_ch_Jet_rawEnergy", ChFractionEnergyRings[3]);
+    this->setValue("Jet_energyRing_dR4_ch_Jet_rawEnergy", ChFractionEnergyRings[4]);
+  //  this->setValue("Jet_energyRing_dR5_ch_Jet_rawEnergy", ChFractionEnergyRings[5]);
+    
+    this->setValue("Jet_energyRing_dR0_mu_Jet_rawEnergy", MuFractionEnergyRings[0]);
+    this->setValue("Jet_energyRing_dR1_mu_Jet_rawEnergy", MuFractionEnergyRings[1]);
+    this->setValue("Jet_energyRing_dR2_mu_Jet_rawEnergy", MuFractionEnergyRings[2]);
+    this->setValue("Jet_energyRing_dR3_mu_Jet_rawEnergy", MuFractionEnergyRings[3]);
+    this->setValue("Jet_energyRing_dR4_mu_Jet_rawEnergy", MuFractionEnergyRings[4]);
+  //  this->setValue("Jet_energyRing_dR5_mu_Jet_rawEnergy", MuFractionEnergyRings[5]);
+    
+    this->setValue("Jet_energyRing_dR0_neut_Jet_rawEnergy", NeFractionEnergyRings[0]);
+    this->setValue("Jet_energyRing_dR1_neut_Jet_rawEnergy", NeFractionEnergyRings[1]);
+    this->setValue("Jet_energyRing_dR2_neut_Jet_rawEnergy", NeFractionEnergyRings[2]);
+    this->setValue("Jet_energyRing_dR3_neut_Jet_rawEnergy", NeFractionEnergyRings[3]);
+    this->setValue("Jet_energyRing_dR4_neut_Jet_rawEnergy", NeFractionEnergyRings[4]);
+//    this->setValue("Jet_energyRing_dR5_neut_Jet_rawEnergy", NeFractionEnergyRings[5]);
+
+
 	  }
 
           static void fillDescriptions(edm::ConfigurationDescriptions& descriptions) {
             edm::ParameterSetDescription desc = BaseMVAValueMapProducer<pat::Jet>::getDescription();
             desc.add<edm::InputTag>("pvsrc")->setComment("primary vertices input collection");
             desc.add<edm::InputTag>("svsrc")->setComment("secondary vertices input collection");
+            desc.add<edm::InputTag>("rhosrc")->setComment("rho  input collection");
             descriptions.add("BJetEnergyRegressionMVA",desc);
           }
 
         private:
 	  const edm::EDGetTokenT<std::vector<reco::Vertex>> pvsrc_;
  	  edm::Handle<std::vector<reco::Vertex>> pvs_;
-          const edm::EDGetTokenT<edm::View<reco::VertexCompositePtrCandidate> > svsrc_;
+      const edm::EDGetTokenT<edm::View<reco::VertexCompositePtrCandidate> > svsrc_;
  	  edm::Handle<edm::View<reco::VertexCompositePtrCandidate>> svs_;
+      edm::EDGetTokenT<double> rhosrc_;
+      edm::Handle<double> rho_;
+
 	  
 };
 
