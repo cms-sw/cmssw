a6b9aaacd4ffac1842dd93f7a2bbb152ab807edd
amassiro <massironi.andrea@gmail.com>
2018-04-12 20:16:57 +0200

    ecal pf rechit eta dependent in 94x

--- RecoParticleFlow/PFClusterProducer/python/particleFlowRecHitECAL_cfi.py
+++ RecoParticleFlow/PFClusterProducer/python/particleFlowRecHitECAL_cfi.py
@@ -16,7 +16,7 @@ particleFlowRecHitECAL = cms.EDProducer("PFRecHitProducer",
            cms.PSet(
              name = cms.string("PFEBRecHitCreator"),
              src  = cms.InputTag("ecalRecHit","EcalRecHitsEB"),
-             srFlags = cms.InputTag("ecalDigis"),
+             srFlags = cms.InputTag(""),
              qualityTests = cms.VPSet(
                   cms.PSet(
                   name = cms.string("PFRecHitQTestECALMultiThreshold"),
@@ -35,7 +35,7 @@ particleFlowRecHitECAL = cms.EDProducer("PFRecHitProducer",
           cms.PSet(
             name = cms.string("PFEERecHitCreator"),
             src  = cms.InputTag("ecalRecHit","EcalRecHitsEE"),
-            srFlags = cms.InputTag("ecalDigis"),
+            srFlags = cms.InputTag(""),
             qualityTests = cms.VPSet(
                  cms.PSet(
                  name = cms.string("PFRecHitQTestECALMultiThreshold"),
