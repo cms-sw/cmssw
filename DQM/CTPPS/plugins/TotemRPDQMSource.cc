e0a11af5d4ae3389ee79e4fb8c4bb4fdb4733dc4
Jan Kaspar <jan.kaspar@cern.ch>
2018-04-16 19:53:58 +0200

    Removed global and arm plots+code (now in common module).

--- DQM/CTPPS/plugins/TotemRPDQMSource.cc
+++ DQM/CTPPS/plugins/TotemRPDQMSource.cc
@@ -55,32 +55,6 @@ class TotemRPDQMSource: public DQMEDAnalyzer
     edm::EDGetTokenT< edm::DetSetVector<TotemRPUVPattern> > tokenUVPattern;
     edm::EDGetTokenT< edm::DetSetVector<TotemRPLocalTrack> > tokenLocalTrack;
 
-    /// plots related to the whole system
-    struct GlobalPlots
-    {
-      MonitorElement *events_per_bx = nullptr, *events_per_bx_short = nullptr;
-      MonitorElement *h_trackCorr_hor = nullptr;
-
-      void Init(DQMStore::IBooker &ibooker);
-    };
-
-    GlobalPlots globalPlots;
-
-    /// plots related to one arm
-    struct ArmPlots
-    {
-      int id;
-
-      MonitorElement *h_numRPWithTrack_top=nullptr, *h_numRPWithTrack_hor=nullptr, *h_numRPWithTrack_bot=nullptr;
-      MonitorElement *h_trackCorr=nullptr, *h_trackCorr_overlap=nullptr;
-
-      ArmPlots(){}
-
-      ArmPlots(DQMStore::IBooker &ibooker, int _id);
-    };
-
-    std::map<unsigned int, ArmPlots> armPlots;
-
     /// plots related to one RP
     struct PotPlots
     {
@@ -122,74 +96,6 @@ class TotemRPDQMSource: public DQMEDAnalyzer
 
 using namespace std;
 using namespace edm;
-
-//----------------------------------------------------------------------------------------------------
-
-void TotemRPDQMSource::GlobalPlots::Init(DQMStore::IBooker &ibooker)
-{
-  ibooker.setCurrentFolder("CTPPS");
-
-  events_per_bx = ibooker.book1D("events per BX", "rp;Event.BX", 4002, -1.5, 4000. + 0.5);
-  events_per_bx_short = ibooker.book1D("events per BX (short)", "rp;Event.BX", 102, -1.5, 100. + 0.5);
-
-  ibooker.setCurrentFolder("CTPPS/TrackingStrip");
-
-  h_trackCorr_hor = ibooker.book2D("track correlation RP-210-hor", "rp, 210, hor", 4, -0.5, 3.5, 4, -0.5, 3.5);
-  TH2F *hist = h_trackCorr_hor->getTH2F();
-  TAxis *xa = hist->GetXaxis(), *ya = hist->GetYaxis();
-  xa->SetBinLabel(1, "45, 210, near"); ya->SetBinLabel(1, "45, 210, near");
-  xa->SetBinLabel(2, "45, 210, far"); ya->SetBinLabel(2, "45, 210, far");
-  xa->SetBinLabel(3, "56, 210, near"); ya->SetBinLabel(3, "56, 210, near");
-  xa->SetBinLabel(4, "56, 210, far"); ya->SetBinLabel(4, "56, 210, far");
-}
-
-//----------------------------------------------------------------------------------------------------
-
-TotemRPDQMSource::ArmPlots::ArmPlots(DQMStore::IBooker &ibooker, int _id) : id(_id)
-{
-  string path;
-  TotemRPDetId(id).armName(path, TotemRPDetId::nPath);
-  ibooker.setCurrentFolder(path);
-
-  string title;
-  TotemRPDetId(id).armName(title, TotemRPDetId::nFull);
-
-  h_numRPWithTrack_top = ibooker.book1D("number of top RPs with tracks", title+";number of top RPs with tracks", 5, -0.5, 4.5);
-  h_numRPWithTrack_hor = ibooker.book1D("number of hor RPs with tracks", title+";number of hor RPs with tracks", 5, -0.5, 4.5);
-  h_numRPWithTrack_bot = ibooker.book1D("number of bot RPs with tracks", title+";number of bot RPs with tracks", 5, -0.5, 4.5);
-
-  h_trackCorr = ibooker.book2D("track RP correlation", title, 13, -0.5, 12.5, 13, -0.5, 12.5);
-  TH2F *h_trackCorr_h = h_trackCorr->getTH2F();
-  TAxis *xa = h_trackCorr_h->GetXaxis(), *ya = h_trackCorr_h->GetYaxis();
-  xa->SetBinLabel(1, "210, near, top"); ya->SetBinLabel(1, "210, near, top");
-  xa->SetBinLabel(2, "bot"); ya->SetBinLabel(2, "bot");
-  xa->SetBinLabel(3, "hor"); ya->SetBinLabel(3, "hor");
-  xa->SetBinLabel(4, "far, hor"); ya->SetBinLabel(4, "far, hor");
-  xa->SetBinLabel(5, "top"); ya->SetBinLabel(5, "top");
-  xa->SetBinLabel(6, "bot"); ya->SetBinLabel(6, "bot");
-  xa->SetBinLabel(8, "220, near, top"); ya->SetBinLabel(8, "220, near, top");
-  xa->SetBinLabel(9, "bot"); ya->SetBinLabel(9, "bot");
-  xa->SetBinLabel(10, "hor"); ya->SetBinLabel(10, "hor");
-  xa->SetBinLabel(11, "far, hor"); ya->SetBinLabel(11, "far, hor");
-  xa->SetBinLabel(12, "top"); ya->SetBinLabel(12, "top");
-  xa->SetBinLabel(13, "bot"); ya->SetBinLabel(13, "bot");
-
-  h_trackCorr_overlap = ibooker.book2D("track RP correlation hor-vert overlaps", title, 13, -0.5, 12.5, 13, -0.5, 12.5);
-  h_trackCorr_h = h_trackCorr_overlap->getTH2F();
-  xa = h_trackCorr_h->GetXaxis(); ya = h_trackCorr_h->GetYaxis();
-  xa->SetBinLabel(1, "210, near, top"); ya->SetBinLabel(1, "210, near, top");
-  xa->SetBinLabel(2, "bot"); ya->SetBinLabel(2, "bot");
-  xa->SetBinLabel(3, "hor"); ya->SetBinLabel(3, "hor");
-  xa->SetBinLabel(4, "far, hor"); ya->SetBinLabel(4, "far, hor");
-  xa->SetBinLabel(5, "top"); ya->SetBinLabel(5, "top");
-  xa->SetBinLabel(6, "bot"); ya->SetBinLabel(6, "bot");
-  xa->SetBinLabel(8, "220, near, top"); ya->SetBinLabel(8, "220, near, top");
-  xa->SetBinLabel(9, "bot"); ya->SetBinLabel(9, "bot");
-  xa->SetBinLabel(10, "hor"); ya->SetBinLabel(10, "hor");
-  xa->SetBinLabel(11, "far, hor"); ya->SetBinLabel(11, "far, hor");
-  xa->SetBinLabel(12, "top"); ya->SetBinLabel(12, "top");
-  xa->SetBinLabel(13, "bot"); ya->SetBinLabel(13, "bot");
-}
 
 //----------------------------------------------------------------------------------------------------
 
@@ -284,14 +190,9 @@ void TotemRPDQMSource::bookHistograms(DQMStore::IBooker &ibooker, edm::Run const
   ibooker.cd();
   ibooker.setCurrentFolder("CTPPS");
 
-  // global plots
-  globalPlots.Init(ibooker);
-
   // loop over arms
   for (unsigned int arm = 0; arm < 2; arm++)
   {
-    TotemRPDetId armId(arm, 0);
-    armPlots[armId] = ArmPlots(ibooker, armId);
 
     // loop over stations
     for (unsigned int st = 0; st < 3; st += 2)
@@ -376,48 +277,6 @@ void TotemRPDQMSource::analyze(edm::Event const& event, edm::EventSetup const& e
     }
 
     return;
-  }
-
-  //------------------------------
-  // Global Plots
-
-  globalPlots.events_per_bx->Fill(event.bunchCrossing());
-  globalPlots.events_per_bx_short->Fill(event.bunchCrossing());
-
-  for (auto &ds1 : *tracks)
-  {
-    for (auto &tr1 : ds1)
-    {
-      if (! tr1.isValid())
-        continue;
-  
-      CTPPSDetId rpId1(ds1.detId());
-      unsigned int arm1 = rpId1.arm();
-      unsigned int stNum1 = rpId1.station();
-      unsigned int rpNum1 = rpId1.rp();
-      if (stNum1 != 0 || (rpNum1 != 2 && rpNum1 != 3))
-        continue;
-      unsigned int idx1 = arm1*2 + rpNum1-2;
-
-      for (auto &ds2 : *tracks)
-      {
-        for (auto &tr2 : ds2)
-        {
-          if (! tr2.isValid())
-            continue;
-        
-          CTPPSDetId rpId2(ds2.detId());
-          unsigned int arm2 = rpId2.arm();
-          unsigned int stNum2 = rpId2.station();
-          unsigned int rpNum2 = rpId2.rp();
-          if (stNum2 != 0 || (rpNum2 != 2 && rpNum2 != 3))
-            continue;
-          unsigned int idx2 = arm2*2 + rpNum2-2;
-  
-          globalPlots.h_trackCorr_hor->Fill(idx1, idx2); 
-        }
-      }
-    }
   }
 
   //------------------------------
@@ -740,91 +599,6 @@ void TotemRPDQMSource::analyze(edm::Event const& event, edm::EventSetup const& e
       pp.track_v_profile->Fill(V);
     }
   }
-  
-  //------------------------------
-  // Arm Plots
-  {
-    map<unsigned int, unsigned int> mTop, mHor, mBot;
-
-    for (auto p : armPlots)
-    {
-      mTop[p.first] = 0;
-      mHor[p.first] = 0;
-      mBot[p.first] = 0;
-    }
-
-    for (auto &ds : *tracks)
-    {
-      CTPPSDetId rpId(ds.detId());
-      unsigned int rpNum = rpId.rp();
-      CTPPSDetId armId = rpId.getArmId();
-
-      for (auto &tr : ds)
-      {
-        if (! tr.isValid())
-          continue;
-  
-        if (rpNum == 0 || rpNum == 4)
-          mTop[armId]++;
-        if (rpNum == 2 || rpNum == 3)
-          mHor[armId]++;
-        if (rpNum == 1 || rpNum == 5)
-          mBot[armId]++;
-      }
-    }
-
-    for (auto &p : armPlots)
-    {
-      p.second.h_numRPWithTrack_top->Fill(mTop[p.first]);
-      p.second.h_numRPWithTrack_hor->Fill(mHor[p.first]);
-      p.second.h_numRPWithTrack_bot->Fill(mBot[p.first]);
-    }
-
-    // track RP correlation
-    for (auto &ds1 : *tracks)
-    {
-      for (auto &tr1 : ds1)
-      {
-        if (! tr1.isValid())
-          continue;
-  
-        CTPPSDetId rpId1(ds1.detId());
-        unsigned int arm1 = rpId1.arm();
-        unsigned int stNum1 = rpId1.station();
-        unsigned int rpNum1 = rpId1.rp();
-        unsigned int idx1 = stNum1/2 * 7 + rpNum1;
-        bool hor1 = (rpNum1 == 2 || rpNum1 == 3);
-  
-        CTPPSDetId armId = rpId1.getArmId();
-        ArmPlots &ap = armPlots[armId];
-  
-        for (auto &ds2 : *tracks)
-        {
-          for (auto &tr2 : ds2)
-          {
-            if (! tr2.isValid())
-              continue;
-          
-            CTPPSDetId rpId2(ds2.detId());
-            unsigned int arm2 = rpId2.arm();
-            unsigned int stNum2 = rpId2.station();
-            unsigned int rpNum2 = rpId2.rp();
-            unsigned int idx2 = stNum2/2 * 7 + rpNum2;
-            bool hor2 = (rpNum2 == 2 || rpNum2 == 3);
-    
-            if (arm1 != arm2)
-              continue;
-    
-            ap.h_trackCorr->Fill(idx1, idx2); 
-            
-            if (hor1 != hor2)
-              ap.h_trackCorr_overlap->Fill(idx1, idx2); 
-          }
-        }
-      }
-    }
-  }
-  
 }
 
 //----------------------------------------------------------------------------------------------------
