a446efec01c1f67c7a98bd34bd0eb42fa383e09b
Heshy Roskes <jroskes1@jhu.edu>
2018-04-17 23:03:01 +0200

    more flexibility in writing the dataset file

--- Alignment/CommonAlignment/scripts/writedatasetfile.py
+++ Alignment/CommonAlignment/scripts/writedatasetfile.py
@@ -10,6 +10,7 @@ parser.add_argument("--hippy", help="output file name for HipPy")
 parser.add_argument("-v", "--validation", help="output file name for validation")
 parser.add_argument("-f", "--first-run", default=0, type=int)
 parser.add_argument("-l", "--last-run", default=float("inf"), type=int)
+parser.add_argument("-r", "--run", action="append", type=int)
 parser.add_argument("-m", "--max-events", default=float("inf"), type=int)
 parser.add_argument("-j", "--events-per-job", type=int)
 
@@ -22,6 +23,6 @@ if args.validation and not args.hippy and args.events_per_job:
   raise RuntimeError("--events-per-job is only used for HipPy")
 
 if args.hippy:
-  dataset.writefilelist_hippy(firstrun=args.first_run, lastrun=args.last_run, maxevents=args.max_events, eventsperjob=args.events_per_job, outputfile=args.hippy)
+  dataset.writefilelist_hippy(firstrun=args.first_run, lastrun=args.last_run, runs=args.run, maxevents=args.max_events, eventsperjob=args.events_per_job, outputfile=args.hippy)
 if args.validation:
-  dataset.writefilelist_validation(firstrun=args.first_run, lastrun=args.last_run, maxevents=args.max_events, outputfile=args.validation)
+  dataset.writefilelist_validation(firstrun=args.first_run, lastrun=args.last_run, runs=args.run, maxevents=args.max_events, outputfile=args.validation)
