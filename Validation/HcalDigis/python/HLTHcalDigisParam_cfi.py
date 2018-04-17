ffc5f2ddf25cedf3eddd6d3ca52a8d4cb352a6a8
Kevin Pedro <kpedro88@gmail.com>
2018-04-17 05:23:46 -0500

    clean up hcal phase2 validation settings

--- Validation/HcalDigis/python/HLTHcalDigisParam_cfi.py
+++ Validation/HcalDigis/python/HLTHcalDigisParam_cfi.py
@@ -31,11 +31,4 @@ from Configuration.Eras.Modifier_run2_HEPlan1_2017_cff import run2_HEPlan1_2017
 run2_HEPlan1_2017.toModify(hltHCALdigisAnalyzer,
     hep17 = cms.bool(True)
 )
-    
-#from Configuration.Eras.Modifier_phase2_hcal_cff import phase2_hcal
-#phase2_hcal.toModify(hltHCALdigisAnalyzer,
-#    dataTPs      = cms.InputTag(""),
-#    digiTag      = cms.InputTag("simHcalDigis"),
-#    QIE10digiTag = cms.InputTag("simHcalDigis","HFQIE10DigiCollection"),
-#    QIE11digiTag = cms.InputTag("simHcalDigis","HBHEQIE11DigiCollection"),
-#)
+
