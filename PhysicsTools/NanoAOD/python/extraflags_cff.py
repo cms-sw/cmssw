c74456a1ed5b8f6e595f4d7e9b129979b82252fb
Shane Breeze <s.breeze15@imperial.ac.uk>
2018-04-18 15:57:55 +0100

    Add a flag for the BadPFMuonFilter module in 80X nanoAOD

--- PhysicsTools/NanoAOD/python/extraflags_cff.py
+++ PhysicsTools/NanoAOD/python/extraflags_cff.py
@@ -11,6 +11,13 @@ cloneGlobalMuonTagger = cloneGlobalMuonTaggerMAOD.clone(
     taggingMode = True
 )
 
+from RecoMET.METFilters.BadPFMuonFilter_cfi import BadPFMuonFilter
+BadPFMuonTagger = BadPFMuonFilter.clone(
+    PFCandidates = cms.InputTag("packedPFCandidates"),
+    muons = cms.InputTag("slimmedMuons"),
+    taggingMode = True,
+)
+
 # Bad charge hadron
 from RecoMET.METFilters.BadChargedCandidateFilter_cfi import BadChargedCandidateFilter
 BadChargedCandidateTagger = BadChargedCandidateFilter.clone(
@@ -23,8 +30,9 @@ extraFlagsTable = cms.EDProducer("GlobalVariablesTableProducer",
     variables = cms.PSet(
         Flag_BadGlobalMuon = ExtVar(cms.InputTag("badGlobalMuonTagger:notBadEvent"), bool, doc = "Bad muon flag"),
         Flag_CloneGlobalMuon = ExtVar(cms.InputTag("cloneGlobalMuonTagger:notBadEvent"), bool, doc = "Clone muon flag"),
+        Flag_BadPFMuonFilter = ExtVar(cms.InputTag("BadPFMuonTagger"), bool, doc = "Bad PF muon flag"),
         Flag_BadChargedCandidateFilter = ExtVar(cms.InputTag("BadChargedCandidateTagger"), bool, doc = "Bad charged hadron flag"),
     )
 )
 
-extraFlagsProducers = cms.Sequence(badGlobalMuonTagger + cloneGlobalMuonTagger + BadChargedCandidateTagger)
+extraFlagsProducers = cms.Sequence(badGlobalMuonTagger + cloneGlobalMuonTagger + BadPFMuonTagger + BadChargedCandidateTagger)
