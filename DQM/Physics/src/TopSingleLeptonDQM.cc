adf7e98057e199d63655946d80d6797d8b4d6974
Fabio Cossutti <fabio.cossutti@ts.infn.it>
2018-04-18 11:46:20 +0200

    Temporary protection against missing jet corrector collection

--- DQM/Physics/src/TopSingleLeptonDQM.cc
+++ DQM/Physics/src/TopSingleLeptonDQM.cc
@@ -537,6 +537,7 @@ void MonitorEnsemble::fill(const edm::Event& event,
 
 	edm::Handle<reco::JetCorrector> corrector;
 	event.getByToken(mJetCorrector, corrector);
+    if (!event.getByToken(mJetCorrector, corrector)) return;
 
   // loop jet collection
   std::vector<reco::Jet> correctedJets;
