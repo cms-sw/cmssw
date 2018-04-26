cb379aa7f9844ddc30c8c74c28a8999cebd33381
Nicola Minafra <nicola.minafra@cern.ch>
2018-04-26 15:24:34 +0200

    clang-format applied to new files

--- RecoCTPPS/TotemRPLocal/plugins/TotemTimingRecHitProducer.cc
+++ RecoCTPPS/TotemRPLocal/plugins/TotemTimingRecHitProducer.cc
@@ -9,17 +9,17 @@
 
 #include <memory>
 
-#include "FWCore/Framework/interface/Frameworkfwd.h"
-#include "FWCore/Framework/interface/stream/EDProducer.h"
-#include "FWCore/Framework/interface/Event.h"
-#include "FWCore/Framework/interface/MakerMacros.h"
 #include "FWCore/Framework/interface/ESHandle.h"
+#include "FWCore/Framework/interface/Event.h"
+#include "FWCore/Framework/interface/Frameworkfwd.h"
+#include "FWCore/Framework/interface/MakerMacros.h"
+#include "FWCore/Framework/interface/stream/EDProducer.h"
 
 #include "FWCore/ParameterSet/interface/ParameterSet.h"
 #include "FWCore/Utilities/interface/StreamID.h"
 
-#include "DataFormats/Common/interface/DetSetVector.h"
 #include "DataFormats/Common/interface/DetSet.h"
+#include "DataFormats/Common/interface/DetSetVector.h"
 
 #include "DataFormats/CTPPSDetId/interface/TotemTimingDetId.h"
 #include "DataFormats/CTPPSDigi/interface/TotemTimingDigi.h"
@@ -30,76 +30,74 @@
 #include "Geometry/Records/interface/VeryForwardRealGeometryRecord.h"
 #include "Geometry/VeryForwardGeometryBuilder/interface/CTPPSGeometry.h"
 
-class TotemTimingRecHitProducer : public edm::stream::EDProducer<>
-{
-  public:
-    explicit TotemTimingRecHitProducer( const edm::ParameterSet& );
-    ~TotemTimingRecHitProducer() override;
+class TotemTimingRecHitProducer : public edm::stream::EDProducer<> {
+public:
+  explicit TotemTimingRecHitProducer(const edm::ParameterSet &);
+  ~TotemTimingRecHitProducer() override;
 
-    static void fillDescriptions( edm::ConfigurationDescriptions& );
+  static void fillDescriptions(edm::ConfigurationDescriptions &);
 
-  private:
-    void produce( edm::Event&, const edm::EventSetup& ) override;
+private:
+  void produce(edm::Event &, const edm::EventSetup &) override;
 
-    edm::EDGetTokenT<edm::DetSetVector<TotemTimingDigi> > digiToken_;
+  edm::EDGetTokenT<edm::DetSetVector<TotemTimingDigi>> digiToken_;
 
-    /// A watcher to detect geometry changes.
-    //edm::ESWatcher<VeryForwardRealGeometryRecord> geometryWatcher_;
+  /// A watcher to detect geometry changes.
+  // edm::ESWatcher<VeryForwardRealGeometryRecord> geometryWatcher_;
 
-    TotemTimingRecHitProducerAlgorithm algo_;
+  TotemTimingRecHitProducerAlgorithm algo_;
 };
 
-TotemTimingRecHitProducer::TotemTimingRecHitProducer( const edm::ParameterSet& iConfig ) :
-  digiToken_( consumes<edm::DetSetVector<TotemTimingDigi> >( iConfig.getParameter<edm::InputTag>( "digiTag" ) ) ),
-  algo_( iConfig )
-{
-  produces<edm::DetSetVector<TotemTimingRecHit> >();
+TotemTimingRecHitProducer::TotemTimingRecHitProducer(
+    const edm::ParameterSet &iConfig)
+    : digiToken_(consumes<edm::DetSetVector<TotemTimingDigi>>(
+          iConfig.getParameter<edm::InputTag>("digiTag"))),
+      algo_(iConfig) {
+  produces<edm::DetSetVector<TotemTimingRecHit>>();
 }
 
-TotemTimingRecHitProducer::~TotemTimingRecHitProducer()
-{}
+TotemTimingRecHitProducer::~TotemTimingRecHitProducer() {}
 
-void
-TotemTimingRecHitProducer::produce( edm::Event& iEvent, const edm::EventSetup& iSetup )
-{
-  std::unique_ptr<edm::DetSetVector<TotemTimingRecHit> > pOut( new edm::DetSetVector<TotemTimingRecHit> );
+void TotemTimingRecHitProducer::produce(edm::Event &iEvent,
+                                        const edm::EventSetup &iSetup) {
+  std::unique_ptr<edm::DetSetVector<TotemTimingRecHit>> pOut(
+      new edm::DetSetVector<TotemTimingRecHit>);
 
   // get the digi collection
-  edm::Handle<edm::DetSetVector<TotemTimingDigi> > digis;
-  iEvent.getByToken( digiToken_, digis );
+  edm::Handle<edm::DetSetVector<TotemTimingDigi>> digis;
+  iEvent.getByToken(digiToken_, digis);
 
   // get the geometry
   edm::ESHandle<CTPPSGeometry> geometry;
-  iSetup.get<VeryForwardRealGeometryRecord>().get( geometry );
+  iSetup.get<VeryForwardRealGeometryRecord>().get(geometry);
 
   // produce the rechits collection
-  algo_.build( geometry.product(), *( digis ), *( pOut ) );
+  algo_.build(geometry.product(), *(digis), *(pOut));
 
-  iEvent.put( std::move( pOut ) );
+  iEvent.put(std::move(pOut));
 }
 
-void
-TotemTimingRecHitProducer::fillDescriptions( edm::ConfigurationDescriptions& descr )
-{
+void TotemTimingRecHitProducer::fillDescriptions(
+    edm::ConfigurationDescriptions &descr) {
   edm::ParameterSetDescription desc;
 
-  desc.add<edm::InputTag>( "digiTag", edm::InputTag( "totemTimingRawToDigi", "TotemTiming" ) )
-    ->setComment( "input digis collection to retrieve" );
-  desc.add<unsigned int>( "baselinePoints", 8 )
-    ->setComment( "number of points to be used for the baseline" );
-  desc.add<unsigned int>( "risingEdgePointsBeforeTh", 4 )
-    ->setComment( "number of points to be used for the rising edge, before the threshold" );
-  desc.add<unsigned int>( "risingEdgePoints", 8 )
-    ->setComment( "number of points to be used for the rising edge" );
-  desc.add<double>( "threholdFactor", 6. )
-    ->setComment( "the threshold will be threholdFactor*RMS of noise" );
-  desc.add<double>( "cfdFraction", 0.5 )
-    ->setComment( "fraction of the CFD" );
-  desc.add<double>( "hysteresis", 25e-3 )
-    ->setComment( "hysteresis of the discriminator" );
+  desc.add<edm::InputTag>("digiTag",
+                          edm::InputTag("totemTimingRawToDigi", "TotemTiming"))
+      ->setComment("input digis collection to retrieve");
+  desc.add<unsigned int>("baselinePoints", 8)
+      ->setComment("number of points to be used for the baseline");
+  desc.add<unsigned int>("risingEdgePointsBeforeTh", 4)
+      ->setComment("number of points to be used for the rising edge, before "
+                   "the threshold");
+  desc.add<unsigned int>("risingEdgePoints", 8)
+      ->setComment("number of points to be used for the rising edge");
+  desc.add<double>("threholdFactor", 6.)
+      ->setComment("the threshold will be threholdFactor*RMS of noise");
+  desc.add<double>("cfdFraction", 0.5)->setComment("fraction of the CFD");
+  desc.add<double>("hysteresis", 25e-3)
+      ->setComment("hysteresis of the discriminator");
 
-
-  descr.add( "totemTimingRecHits", desc );
+  descr.add("totemTimingRecHits", desc);
 }
 
-DEFINE_FWK_MODULE( TotemTimingRecHitProducer );
+DEFINE_FWK_MODULE(TotemTimingRecHitProducer);
