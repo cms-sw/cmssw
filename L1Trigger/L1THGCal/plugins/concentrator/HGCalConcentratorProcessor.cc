6e5f5ef347a7601ca89cbdda9c25f5ae9fb7d212
Emilia Becheva <emilia.becheva@llr.in2p3.fr>
2018-04-26 17:30:41 +0200

    in addition to previous small fixes

--- L1Trigger/L1THGCal/plugins/concentrator/HGCalConcentratorProcessor.cc
+++ L1Trigger/L1THGCal/plugins/concentrator/HGCalConcentratorProcessor.cc
@@ -9,7 +9,7 @@ DEFINE_EDM_PLUGIN(HGCalConcentratorFactory,
 
 HGCalConcentratorProcessor::HGCalConcentratorProcessor(const edm::ParameterSet& conf)  : 
   HGCalConcentratorProcessorBase(conf),
-  ConcentratorProcImpl_(conf)
+  concentratorProcImpl_(conf)
 { 
 }
 
@@ -19,12 +19,12 @@ void HGCalConcentratorProcessor::putInEvent(edm::Event& evt)
   evt.put(std::move(triggerSumsConc_product_), name());
 }
 
-std::vector<l1t::HGCalTriggerCell> HGCalConcentratorProcessor::trigCellCollectionToVector(int ibx, const l1t::HGCalTriggerCellBxCollection& coll){
+std::vector<l1t::HGCalTriggerCell> HGCalConcentratorProcessor::trigCellCollectionToVector(const l1t::HGCalTriggerCellBxCollection& coll){
     
   std::vector<l1t::HGCalTriggerCell> trigCellVec;
       
   //loop over HGCalTriggerCellBxCollection for a given bx and put the HGCalTriggerCell objects into a std::vector
-  for( std::vector<l1t::HGCalTriggerCell>::const_iterator it = coll.begin(ibx) ; it != coll.end(ibx) ; ++it )
+  for( std::vector<l1t::HGCalTriggerCell>::const_iterator it = coll.begin() ; it != coll.end() ; ++it )
   { 
     trigCellVec.push_back(*it); 
   }
@@ -33,45 +33,39 @@ std::vector<l1t::HGCalTriggerCell> HGCalConcentratorProcessor::trigCellCollectio
 }
 
 // Transform std::vector<l1t::HGCalTriggerCell> to BXVector<HGCalTriggerCell> HGCalTriggerCellBxCollection
-void HGCalConcentratorProcessor::trigCellVectorToCollection(int ibx, const std::vector<l1t::HGCalTriggerCell> trigCellVec)
+void HGCalConcentratorProcessor::trigCellVectorToCollection(const std::vector<l1t::HGCalTriggerCell> trigCellVec)
 {
   for( auto trigCell = trigCellVec.begin(); trigCell != trigCellVec.end(); ++trigCell){
-    triggerCellConc_product_->push_back(ibx, *trigCell);
+    triggerCellConc_product_->push_back(0,*trigCell);//bx=0
   }
 }
 
 void HGCalConcentratorProcessor::bestChoiceSelect(const l1t::HGCalTriggerCellBxCollection& coll)
 {
-  int bxFirst = coll.getFirstBX();
-  int bxLast = coll.getLastBX();
-
   //we consider here only Bx=0
   std::vector<l1t::HGCalTriggerCell> trigCellVec;
 
   // Convert vector to collection
-  trigCellVec = trigCellCollectionToVector(ibx, coll);
+  trigCellVec = trigCellCollectionToVector(coll);
 
   // Selection in implementation class
-  ConcentratorProcImpl_.bestChoiceSelectImpl(trigCellVec);
+  concentratorProcImpl_.bestChoiceSelectImpl(trigCellVec);
 
   // Convert std::vector to collection (BXVector), fill into the final product
-  trigCellVectorToCollection(ibx, trigCellVec);
+  trigCellVectorToCollection(trigCellVec);
 }
 
 void 
 HGCalConcentratorProcessor::
 thresholdSelect(const l1t::HGCalTriggerCellBxCollection& coll)
-{  
-  int bxFirst = coll.getFirstBX();
-  int bxLast = coll.getLastBX();
-    
+{      
   //we consider here only Bx=0
   std::vector<l1t::HGCalTriggerCell> trigCellVec;
     
   // Convert vector to collection
-  trigCellVec = trigCellCollectionToVector(ibx, coll);
+  trigCellVec = trigCellCollectionToVector(coll);
   // Select in the implementation class
-  ConcentratorProcImpl_.thresholdSelectImpl(trigCellVec);
+  concentratorProcImpl_.thresholdSelectImpl(trigCellVec);
   // Convert std::vector to collection (BXVector), fill into the final product 
-  trigCellVectorToCollection(ibx, trigCellVec);    
+  trigCellVectorToCollection(trigCellVec);    
 }
