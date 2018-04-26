1c5aecbb641fe9d881deb9b1f225accccc06de7a
Josh Kunkle <jkunkle@cern.ch>
2018-04-26 13:26:01 +0200

    clone hcalLaserEventFilter

--- RecoMET/METFilters/python/hcalLaserEventFilter_cfi.py
+++ RecoMET/METFilters/python/hcalLaserEventFilter_cfi.py
@@ -6,9 +6,4 @@ except:
     print "<hcalLaserEventFilter_cfi> WARNING!  No explicit list 'badEvents.py' containing bad HCAL laser run/events was found!"
     badEvents=[]
 
-#hcalLaserEventFilter = cms.EDFilter("HcalLaserEventFilter",)
-hcalLaserEventFilter = cms.EDFilter("HcalLaserEventFilter",
-                                    BadRunEventNumbers=cms.vuint32(badEvents
-                                    # badEvents file contains 218 bad events, as of 28 Nov. 2011
-                                                                  ),
-                                    )
+hcalLaserEventFilter = hcallaserevent.clone(BadRunEventNumbers=cms.vuint32(badEvents) )
