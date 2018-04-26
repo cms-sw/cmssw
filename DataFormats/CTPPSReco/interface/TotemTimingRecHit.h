ca9d82d0570af1abcb3e5cc1279cf7691b079100
Nicola Minafra <nicola.minafra@cern.ch>
2018-04-26 15:24:34 +0200

    clang-format applied to new files

--- DataFormats/CTPPSReco/interface/TotemTimingRecHit.h
+++ DataFormats/CTPPSReco/interface/TotemTimingRecHit.h
@@ -1,11 +1,11 @@
 /****************************************************************************
-*
-* This is a part of CTPPS offline software.
-* Authors:
-*   Laurent Forthomme (laurent.forthomme@cern.ch)
-*   Nicola Minafra (nicola.minafra@cern.ch)
-*
-****************************************************************************/
+ *
+ * This is a part of CTPPS offline software.
+ * Authors:
+ *   Laurent Forthomme (laurent.forthomme@cern.ch)
+ *   Nicola Minafra (nicola.minafra@cern.ch)
+ *
+ ****************************************************************************/
 
 #ifndef DataFormats_CTPPSReco_TotemTimingRecHit
 #define DataFormats_CTPPSReco_TotemTimingRecHit
@@ -13,24 +13,21 @@
 #include "DataFormats/CTPPSReco/interface/CTPPSTimingRecHit.h"
 
 /// Reconstructed hit in totem ufsd detectors.
-class TotemTimingRecHit : public CTPPSTimingRecHit
-{
-  public:
-    TotemTimingRecHit() :
-      CTPPSTimingRecHit(),
-      amplitude_(0)
-    {}
-    TotemTimingRecHit( float x, float x_width, float y, float y_width, float z, float z_width, float t, float tot, float t_precision, float amplitude ) :
-      CTPPSTimingRecHit( x, x_width, y, y_width, z, z_width, t, tot, t_precision ),
-      amplitude_(amplitude)
-    {}
+class TotemTimingRecHit : public CTPPSTimingRecHit {
+public:
+  TotemTimingRecHit() : CTPPSTimingRecHit(), amplitude_(0) {}
+  TotemTimingRecHit(float x, float x_width, float y, float y_width, float z,
+                    float z_width, float t, float tot, float t_precision,
+                    float amplitude)
+      : CTPPSTimingRecHit(x, x_width, y, y_width, z, z_width, t, tot,
+                          t_precision),
+        amplitude_(amplitude) {}
 
-    inline void setAmplitude( const float& amplitude ) { amplitude_ = amplitude; }
-    inline float getAmplitude() const { return amplitude_; }
+  inline void setAmplitude(const float &amplitude) { amplitude_ = amplitude; }
+  inline float getAmplitude() const { return amplitude_; }
 
-  private:
-    float amplitude_;
-
+private:
+  float amplitude_;
 };
 
 #endif
--- RecoCTPPS/TotemRPLocal/interface/TotemTimingRecHitProducerAlgorithm.h
+++ RecoCTPPS/TotemRPLocal/interface/TotemTimingRecHitProducerAlgorithm.h
@@ -1,11 +1,11 @@
 /****************************************************************************
-*
-* This is a part of CTPPS offline software.
-* Authors:
-*   Laurent Forthomme (laurent.forthomme@cern.ch)
-*   Nicola Minafra
-*
-****************************************************************************/
+ *
+ * This is a part of CTPPS offline software.
+ * Authors:
+ *   Laurent Forthomme (laurent.forthomme@cern.ch)
+ *   Nicola Minafra
+ *
+ ****************************************************************************/
 
 #ifndef RecoCTPPS_TotemRPLocal_TotemTimingRecHitProducerAlgorithm
 #define RecoCTPPS_TotemRPLocal_TotemTimingRecHitProducerAlgorithm
@@ -20,45 +20,50 @@
 
 #include <algorithm>
 
-#include "Geometry/VeryForwardRPTopology/interface/RPTopology.h"
 #include "Geometry/VeryForwardGeometryBuilder/interface/CTPPSGeometry.h"
+#include "Geometry/VeryForwardRPTopology/interface/RPTopology.h"
 
-class TotemTimingRecHitProducerAlgorithm
-{
-  public:
-    TotemTimingRecHitProducerAlgorithm( const edm::ParameterSet& conf );
+class TotemTimingRecHitProducerAlgorithm {
+public:
+  TotemTimingRecHitProducerAlgorithm(const edm::ParameterSet &conf);
 
-    void build( const CTPPSGeometry*, const edm::DetSetVector<TotemTimingDigi>&, edm::DetSetVector<TotemTimingRecHit>& );
+  void build(const CTPPSGeometry *, const edm::DetSetVector<TotemTimingDigi> &,
+             edm::DetSetVector<TotemTimingRecHit> &);
 
-    struct RegressionResults{
-      float m;
-      float q;
-      float rms;
-      RegressionResults() : m(0), q(0), rms(0) {};
-    };
+  struct RegressionResults {
+    float m;
+    float q;
+    float rms;
+    RegressionResults() : m(0), q(0), rms(0){};
+  };
 
-  private:
-    static const double SAMPIC_SAMPLING_PERIOD_NS;
-    static const double SAMPIC_MAX_NUMBER_OF_SAMPLES;
-    static const double SAMPIC_ADC_V;
+private:
+  static const double SAMPIC_SAMPLING_PERIOD_NS;
+  static const double SAMPIC_MAX_NUMBER_OF_SAMPLES;
+  static const double SAMPIC_ADC_V;
 
-    unsigned int baselinePoints_;
-    unsigned int risingEdgePointsBeforeTh_;
-    unsigned int risingEdgePoints_;
-    double threholdFactor_;
-    double cfdFraction_;
-    double hysteresis_;
+  unsigned int baselinePoints_;
+  unsigned int risingEdgePointsBeforeTh_;
+  unsigned int risingEdgePoints_;
+  double threholdFactor_;
+  double cfdFraction_;
+  double hysteresis_;
 
-    float tmp_1_;
-    float tmp_2_;
+  float tmp_1_;
+  float tmp_2_;
 
-    RegressionResults SimplifiedLinearRegression( const std::vector<float>& time, const std::vector<float>& data, const unsigned int start_at, const unsigned int points ) const;
+  RegressionResults SimplifiedLinearRegression(const std::vector<float> &time,
+                                               const std::vector<float> &data,
+                                               const unsigned int start_at,
+                                               const unsigned int points) const;
 
-    int FastDiscriminator( const std::vector<float>& data, const float& threshold ) const;
+  int FastDiscriminator(const std::vector<float> &data,
+                        const float &threshold) const;
 
-    float SmartTimeOfArrival(const std::vector<float>& time, const std::vector<float>& data );
-    float ConstantFractionDiscriminator(const std::vector<float>& time, const std::vector<float>& data );
-
+  float SmartTimeOfArrival(const std::vector<float> &time,
+                           const std::vector<float> &data);
+  float ConstantFractionDiscriminator(const std::vector<float> &time,
+                                      const std::vector<float> &data);
 };
 
 #endif
