497e9ed9f15f616d7745b5a18b659e1d42c73eb3
mmusich <marco.musich@cern.ch>
2018-04-27 16:31:09 +0200

    update configuration, add full-blown example for all layers

--- CondTools/SiStrip/test/SiStripNoiseFromDBMiscalibrator_cfg.py
+++ CondTools/SiStrip/test/SiStripNoiseFromDBMiscalibrator_cfg.py
@@ -1,5 +1,6 @@
 import FWCore.ParameterSet.Config as cms
 import FWCore.ParameterSet.VarParsing as VarParsing
+import copy 
 
 process = cms.Process("Demo")
 
@@ -8,7 +9,7 @@ process = cms.Process("Demo")
 options = VarParsing.VarParsing("analysis")
 
 options.register ('globalTag',
-                  "100X_dataRun2_Express_v2",
+                  "auto:run2_data",
                   VarParsing.VarParsing.multiplicity.singleton, # singleton or list
                   VarParsing.VarParsing.varType.string,          # string, int, or float
                   "GlobalTag")
@@ -43,6 +44,11 @@ process.load('Configuration.StandardSequences.FrontierConditions_GlobalTag_cff')
 from Configuration.AlCa.GlobalTag import GlobalTag
 process.GlobalTag = GlobalTag(process.GlobalTag,options.globalTag, '')
 
+print "Using Global Tag:", process.GlobalTag.globaltag._value
+
+##
+## Empty Source
+##
 process.source = cms.Source("EmptySource",
                             firstRun = cms.untracked.uint32(options.runNumber),
                             numberEventsInRun = cms.untracked.uint32(1),
@@ -54,266 +60,6 @@ process.maxEvents = cms.untracked.PSet( input = cms.untracked.int32(1) )
 ##
 ## Example smearing configurations
 ##
-
-# Layer: Layer.TIB1 ratio of ratios: 1.02533166915
-# Layer: Layer.TIB2 ratio of ratios: 1.01521093183
-# Layer: Layer.TIB3 ratio of ratios: 1.01552419364
-# Layer: Layer.TIB4 ratio of ratios: 0.95224779507
-# Layer: Layer.TOB1 ratio of ratios: 1.01219411074
-# Layer: Layer.TOB2 ratio of ratios: 1.00835168635
-# Layer: Layer.TOB3 ratio of ratios: 0.996159099354
-# Layer: Layer.TOB4 ratio of ratios: 0.997676926445
-# Layer: Layer.TOB5 ratio of ratios: 0.993886888572
-# Layer: Layer.TOB6 ratio of ratios: 0.997490411188
-# Layer: Layer.TIDP1 ratio of ratios: 1.0314881072
-# Layer: Layer.TIDP2 ratio of ratios: 1.02853114088
-# Layer: Layer.TIDP3 ratio of ratios: 1.0518768914
-# Layer: Layer.TIDM1 ratio of ratios: 1.03421675878
-# Layer: Layer.TIDM2 ratio of ratios: 1.04546785025
-# Layer: Layer.TIDM3 ratio of ratios: 1.0311586591
-# Layer: Layer.TECP1 ratio of ratios: 1.04989866792
-# Layer: Layer.TECP2 ratio of ratios: 1.03711260343
-# Layer: Layer.TECP3 ratio of ratios: 1.04297992451
-# Layer: Layer.TECP4 ratio of ratios: 1.04669045804
-# Layer: Layer.TECP5 ratio of ratios: 1.03838249025
-# Layer: Layer.TECP6 ratio of ratios: 1.04727471357
-# Layer: Layer.TECP7 ratio of ratios: 1.03632636024
-# Layer: Layer.TECP8 ratio of ratios: 1.04860504406
-# Layer: Layer.TECP9 ratio of ratios: 1.03398568113
-# Layer: Layer.TECM1 ratio of ratios: 1.04750199121
-# Layer: Layer.TECM2 ratio of ratios: 1.03771633506
-# Layer: Layer.TECM3 ratio of ratios: 1.0409554129
-# Layer: Layer.TECM4 ratio of ratios: 1.03630204118
-# Layer: Layer.TECM5 ratio of ratios: 1.0417988699
-# Layer: Layer.TECM6 ratio of ratios: 1.03864754217
-# Layer: Layer.TECM7 ratio of ratios: 1.03868976393
-# Layer: Layer.TECM8 ratio of ratios: 1.03942709841
-# Layer: Layer.TECM9 ratio of ratios: 1.03678940814
-
-byLayer = cms.VPSet(
-
-    ################## TIB ##################
-
-    cms.PSet(partition = cms.string("TIB_1"),
-             doScale   = cms.bool(True),
-             doSmear   = cms.bool(False),
-             scaleFactor = cms.double(1.02533166915),
-             smearFactor = cms.double(0.0)
-             ),
-    cms.PSet(partition = cms.string("TIB_2"),
-             doScale   = cms.bool(True),
-             doSmear   = cms.bool(False),
-             scaleFactor = cms.double(1.01521093183),
-             smearFactor = cms.double(0.0)
-             ),
-    cms.PSet(partition = cms.string("TIB_3"),
-             doScale   = cms.bool(True),
-             doSmear   = cms.bool(False),
-             scaleFactor = cms.double(1.01552419364),
-             smearFactor = cms.double(0.0)
-             ),
-    cms.PSet(partition = cms.string("TIB_4"),
-             doScale   = cms.bool(True),
-             doSmear   = cms.bool(False),
-             scaleFactor = cms.double(0.95224779507),
-             smearFactor = cms.double(0.0)
-             ),
-
-    ################## TOB ##################
-
-    cms.PSet(partition = cms.string("TOB_1"),
-             doScale   = cms.bool(True),
-             doSmear   = cms.bool(False),
-             scaleFactor = cms.double(1.01219411074),
-             smearFactor = cms.double(0.0)
-             ),
-    cms.PSet(partition = cms.string("TOB_2"),
-             doScale   = cms.bool(True),
-             doSmear   = cms.bool(False),
-             scaleFactor = cms.double(1.00835168635),
-             smearFactor = cms.double(0.0)
-             ),
-    cms.PSet(partition = cms.string("TOB_3"),
-             doScale   = cms.bool(True),
-             doSmear   = cms.bool(False),
-             scaleFactor = cms.double(0.996159099354),
-             smearFactor = cms.double(0.0)
-             ),
-    cms.PSet(partition = cms.string("TOB_4"),
-             doScale   = cms.bool(True),
-             doSmear   = cms.bool(False),
-             scaleFactor = cms.double(0.997676926445),
-             smearFactor = cms.double(0.0)
-             ),
-    cms.PSet(partition = cms.string("TOB_5"),
-             doScale   = cms.bool(True),
-             doSmear   = cms.bool(False),
-             scaleFactor = cms.double(0.993886888572),
-             smearFactor = cms.double(0.0)
-             ),
-    cms.PSet(partition = cms.string("TOB_6"),
-             doScale   = cms.bool(True),
-             doSmear   = cms.bool(False),
-             scaleFactor = cms.double(0.997490411188),
-             smearFactor = cms.double(0.0)
-             ),
-
-    ################## TID Plus ##################
-
-    cms.PSet(partition = cms.string("TIDP_1"),
-             doScale   = cms.bool(True),
-             doSmear   = cms.bool(False),
-             scaleFactor = cms.double(1.0314881072),
-             smearFactor = cms.double(0.0)
-             ),
-    cms.PSet(partition = cms.string("TIDP_2"),
-             doScale   = cms.bool(True),
-             doSmear   = cms.bool(False),
-             scaleFactor = cms.double(1.02853114088),
-             smearFactor = cms.double(0.0)
-             ),
-    cms.PSet(partition = cms.string("TIDP_3"),
-             doScale   = cms.bool(True),
-             doSmear   = cms.bool(False),
-             scaleFactor = cms.double(1.0518768914),
-             smearFactor = cms.double(0.0)
-             ),
-
-    ################## TID Minus ##################
-
-    cms.PSet(partition = cms.string("TIDM_1"),
-             doScale   = cms.bool(True),
-             doSmear   = cms.bool(False),
-             scaleFactor = cms.double(1.03421675878),
-             smearFactor = cms.double(0.0)
-             ),
-    cms.PSet(partition = cms.string("TIDM_2"),
-             doScale   = cms.bool(True),
-             doSmear   = cms.bool(False),
-             scaleFactor = cms.double(1.04546785025),
-             smearFactor = cms.double(0.0)
-             ),
-    cms.PSet(partition = cms.string("TIDM_3"),
-             doScale   = cms.bool(True),
-             doSmear   = cms.bool(False),
-             scaleFactor = cms.double(1.0311586591),
-             smearFactor = cms.double(0.0)
-             ),
-    
-    ################## TEC plus ##################
-
-    cms.PSet(partition = cms.string("TECP_1"),
-             doScale   = cms.bool(True),
-             doSmear   = cms.bool(False),
-             scaleFactor = cms.double(1.04989866792),
-             smearFactor = cms.double(0.0)
-             ),
-    cms.PSet(partition = cms.string("TECP_2"),
-             doScale   = cms.bool(True),
-             doSmear   = cms.bool(False),
-             scaleFactor = cms.double(1.03711260343),
-             smearFactor = cms.double(0.0)
-             ),
-    cms.PSet(partition = cms.string("TECP_3"),
-             doScale   = cms.bool(True),
-             doSmear   = cms.bool(False),
-             scaleFactor = cms.double(1.04297992451),
-             smearFactor = cms.double(0.0)
-             ),
-    cms.PSet(partition = cms.string("TECP_4"),
-             doScale   = cms.bool(True),
-             doSmear   = cms.bool(False),
-             scaleFactor = cms.double(1.04669045804),
-             smearFactor = cms.double(0.0)
-             ),
-    cms.PSet(partition = cms.string("TECP_5"),
-             doScale   = cms.bool(True),
-             doSmear   = cms.bool(False),
-             scaleFactor = cms.double(1.03838249025),
-             smearFactor = cms.double(0.0)
-             ),
-    cms.PSet(partition = cms.string("TECP_6"),
-             doScale   = cms.bool(True),
-             doSmear   = cms.bool(False),
-             scaleFactor = cms.double(1.04727471357),
-             smearFactor = cms.double(0.0)
-             ),
-    cms.PSet(partition = cms.string("TECP_7"),
-             doScale   = cms.bool(True),
-             doSmear   = cms.bool(False),
-             scaleFactor = cms.double(1.03632636024),
-             smearFactor = cms.double(0.0)
-             ),
-    cms.PSet(partition = cms.string("TECP_8"),
-             doScale   = cms.bool(True),
-             doSmear   = cms.bool(False),
-             scaleFactor = cms.double(1.04860504406),
-             smearFactor = cms.double(0.0)
-             ),
-    cms.PSet(partition = cms.string("TECP_9"),
-             doScale   = cms.bool(True),
-             doSmear   = cms.bool(False),
-             scaleFactor = cms.double(1.03398568113),
-             smearFactor = cms.double(0.0)
-             ),
-
-    ################## TEC Minus ##################
-    cms.PSet(partition = cms.string("TECM_1"),
-             doScale   = cms.bool(True),
-             doSmear   = cms.bool(False),
-             scaleFactor = cms.double(1.04750199121),
-             smearFactor = cms.double(0.0)
-             ),
-    cms.PSet(partition = cms.string("TECM_2"),
-             doScale   = cms.bool(True),
-             doSmear   = cms.bool(False),
-             scaleFactor = cms.double(1.03771633506),
-             smearFactor = cms.double(0.0)
-             ),
-    cms.PSet(partition = cms.string("TECM_3"),
-             doScale   = cms.bool(True),
-             doSmear   = cms.bool(False),
-             scaleFactor = cms.double(1.0409554129),
-             smearFactor = cms.double(0.0)
-             ),
-    cms.PSet(partition = cms.string("TECM_4"),
-             doScale   = cms.bool(True),
-             doSmear   = cms.bool(False),
-             scaleFactor = cms.double(1.03630204118),
-             smearFactor = cms.double(0.0)
-             ),
-    cms.PSet(partition = cms.string("TECM_5"),
-             doScale   = cms.bool(True),
-             doSmear   = cms.bool(False),
-             scaleFactor = cms.double(1.0417988699),
-             smearFactor = cms.double(0.0)
-             ),    
-    cms.PSet(partition = cms.string("TECM_6"),
-             doScale   = cms.bool(True),
-             doSmear   = cms.bool(False),
-             scaleFactor = cms.double(1.03864754217),
-             smearFactor = cms.double(0.0)
-             ),
-    cms.PSet(partition = cms.string("TECM_7"),
-             doScale   = cms.bool(True),
-             doSmear   = cms.bool(False),
-             scaleFactor = cms.double(1.0386897639),
-             smearFactor = cms.double(0.0)
-             ),
-    cms.PSet(partition = cms.string("TECM_8"),
-             doScale   = cms.bool(True),
-             doSmear   = cms.bool(False),
-             scaleFactor = cms.double(1.03942709841),
-             smearFactor = cms.double(0.0)
-             ),
-    cms.PSet(partition = cms.string("TECM_9"),
-             doScale   = cms.bool(True),
-             doSmear   = cms.bool(False),
-             scaleFactor = cms.double(1.03678940814),
-             smearFactor = cms.double(0.0)
-             )
-    ) 
-
 
 ########### Noise ##########
 # TEC: new = 5.985 old = 5.684  => ratio: 1.052
@@ -364,7 +110,11 @@ wholeTracker = cms.VPSet(
              )
     )
 
-byLayer2 = cms.VPSet(
+##
+## by Layer only one partition
+##
+
+byLayerOnlyTIB = cms.VPSet(
 
     ################## TIB ##################
 
@@ -394,6 +144,10 @@ byLayer2 = cms.VPSet(
              )
     )
 
+##
+## hierarchies
+##
+
 subsets =  cms.VPSet(
     cms.PSet(partition = cms.string("Tracker"),
              doScale   = cms.bool(True),
@@ -421,7 +175,9 @@ subsets =  cms.VPSet(
              )
     )
 
-
+#
+# just a silly example
+#
 autoparams=[]
 listOfLayers=["TIB_1","TIB_2","TIB_3","TIB_4","TOB_1","TOB_2","TOB_3","TOB_4","TOB_5","TOB_6","TIDM_1","TIDM_2","TIDM_3","TECM_1","TECM_2","TECM_3","TECM_4","TECM_5","TECM_6","TECM_7","TECM_8","TECM_9","TIDP_1","TIDP_2","TIDP_3","TECP_1","TECP_2","TECP_3","TECP_4","TECP_5","TECP_6","TECP_7","TECP_8","TECP_9"]
 
@@ -436,20 +192,27 @@ for i,ll in enumerate(listOfLayers):
             )
         )
 
-
-# process.demo = cms.EDAnalyzer('SiStripChannelGainFromDBMiscalibrator',
-#                               record = cms.untracked.string("SiStripApvGainRcd"),
-#                               gainType = cms.untracked.uint32(1), #0 for G1, 1 for G2
-#                               params = subsets # as a cms.VPset
+# process.demo = cms.EDAnalyzer('SiStripNoisesFromDBMiscalibrator',
+#                               params = subsets, # as a cms.VPset
+#                               fillDefaults = cms.bool(False),
+#                               saveMaps = cms.bool(True)      
 #                               )
 
+
+##
+## Impot the thresholds configuration
+##
+import CondTools.SiStrip.MiscalibrationSettings_cff as Settings
+eachLayer = copy.deepcopy(Settings.byLayer)
+
 process.load("CondTools.SiStrip.scaleAndSmearSiStripNoises_cfi")
-#process.scaleAndSmearSiStripNoises.params  = wholeTracker  # as a cms.VPset
-#process.scaleAndSmearSiStripNoises.params  = byPartition   # as a cms.VPset
-#process.scaleAndSmearSiStripNoises.params = subsets        # as a cms.VPset
-#process.scaleAndSmearSiStripNoises.params = byLayer2       # as a cms.VPset
+#process.scaleAndSmearSiStripNoises.params  = eachLayer      # as a cms.VPset
+#process.scaleAndSmearSiStripNoises.params  = wholeTracker   # as a cms.VPset
+#process.scaleAndSmearSiStripNoises.params  = byPartition    # as a cms.VPset
+#process.scaleAndSmearSiStripNoises.params  = subsets        # as a cms.VPset
+#process.scaleAndSmearSiStripNoises.params  = byLayerOnlyTIB # as a cms.VPset
 process.scaleAndSmearSiStripNoises.params  = autoparams
-process.scaleAndSmearSiStripNoises.fillDefaults = True     # to fill uncabled DetIds with default
+process.scaleAndSmearSiStripNoises.fillDefaults = False     # to fill uncabled DetIds with default
 
 ##
 ## Database output service
@@ -459,7 +222,7 @@ process.load("CondCore.CondDB.CondDB_cfi")
 ##
 ## Output database (in this case local sqlite file)
 ##
-process.CondDB.connect = 'sqlite_file:modifiedNoise_'+options.globalTag+'_IOV_'+str(options.runNumber)+".db"
+process.CondDB.connect = 'sqlite_file:modifiedNoise_'+ process.GlobalTag.globaltag._value+'_IOV_'+str(options.runNumber)+".db"
 process.PoolDBOutputService = cms.Service("PoolDBOutputService",
                                           process.CondDB,
                                           timetype = cms.untracked.string('runnumber'),
