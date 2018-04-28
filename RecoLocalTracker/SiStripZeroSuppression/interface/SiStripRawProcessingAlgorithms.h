ff87f333300a2d8e516237e1b4fa06bd05061f0c
Pieter David <pieter.david@cern.ch>
2018-04-28 15:55:08 +0200

    SiStripRawProcessingAlgorithms: uint32_t and uint16_t in interfaces

--- RecoLocalTracker/SiStripZeroSuppression/interface/SiStripRawProcessingAlgorithms.h
+++ RecoLocalTracker/SiStripZeroSuppression/interface/SiStripRawProcessingAlgorithms.h
@@ -12,33 +12,33 @@
 #include "DataFormats/Common/interface/DetSet.h"
 #include "DataFormats/Common/interface/DetSetVector.h"
 
-class SiStripRawProcessingAlgorithms {
+class SiStripRawProcessingAlgorithms
+{
   friend class SiStripRawProcessingFactory;
 
- public: 
+public:
   void initialize(const edm::EventSetup&);
   void initialize(const edm::EventSetup&, const edm::Event&);
-  
-  uint16_t SuppressHybridData(const edm::DetSet<SiStripDigi>&, edm::DetSet<SiStripDigi>&,  std::vector<int16_t>& );
-  uint16_t SuppressHybridData(const uint32_t&, const uint16_t&, std::vector<int16_t>&, edm::DetSet<SiStripDigi>&);
+
+  uint16_t SuppressHybridData(const edm::DetSet<SiStripDigi>& inDigis, edm::DetSet<SiStripDigi>& suppressedDigis, std::vector<int16_t>& rawDigis);
+  uint16_t SuppressHybridData(uint32_t detId, uint16_t firstAPV, std::vector<int16_t>& processedRawDigis, edm::DetSet<SiStripDigi>& suppressedDigis);
+
+  uint16_t SuppressVirginRawData(uint32_t detId, uint16_t firstAPV, std::vector<int16_t>& procRawDigis, edm::DetSet<SiStripDigi>& output);
+  uint16_t SuppressVirginRawData(const edm::DetSet<SiStripRawDigi>& rawDigis, edm::DetSet<SiStripDigi>& output);
+
+  uint16_t SuppressProcessedRawData(uint32_t detId, uint16_t firstAPV, std::vector<int16_t>& procRawDigis, edm::DetSet<SiStripDigi>& output);
+  uint16_t SuppressProcessedRawData(const edm::DetSet<SiStripRawDigi>& rawDigis, edm::DetSet<SiStripDigi>& output);
 
 
-  uint16_t SuppressVirginRawData(const uint32_t&, const uint16_t&, std::vector<int16_t>&, edm::DetSet<SiStripDigi>&);
-  uint16_t SuppressVirginRawData(const edm::DetSet<SiStripRawDigi>&, edm::DetSet<SiStripDigi>& );
-  
-  uint16_t SuppressProcessedRawData(const uint32_t&, const uint16_t&, std::vector<int16_t>&, edm::DetSet<SiStripDigi>&);
-  uint16_t SuppressProcessedRawData(const edm::DetSet<SiStripRawDigi>&, edm::DetSet<SiStripDigi>&  );
-  
-  
-  uint16_t ConvertVirginRawToHybrid(const uint32_t&, const uint16_t&, std::vector<int16_t>&, edm::DetSet<SiStripDigi>&);
-  uint16_t ConvertVirginRawToHybrid(const edm::DetSet<SiStripRawDigi>&, edm::DetSet<SiStripDigi>& );
-  
-  void ConvertHybridDigiToRawDigiVector(const uint32_t&, const edm::DetSet<SiStripDigi>&, std::vector<int16_t>&);
-  
-  inline std::vector<bool>& GetAPVFlags(){return restorer->GetAPVFlags();}
-  inline std::map<uint16_t, std::vector < int16_t> >& GetBaselineMap(){return restorer->GetBaselineMap();}
-  inline std::map< uint16_t, std::map< uint16_t, int16_t> >& GetSmoothedPoints(){return restorer->GetSmoothedPoints();}
-  inline const std::vector< std::pair<short,float> >& getAPVsCM(){return subtractorCMN->getAPVsCM();}
+  uint16_t ConvertVirginRawToHybrid(uint32_t detId, uint16_t firstAPV, std::vector<int16_t>& inDigis, edm::DetSet<SiStripDigi>& rawDigis);
+  uint16_t ConvertVirginRawToHybrid(const edm::DetSet<SiStripRawDigi>& rawDigis, edm::DetSet<SiStripDigi>& suppressedDigis);
+
+  void ConvertHybridDigiToRawDigiVector(uint32_t detId, const edm::DetSet<SiStripDigi>& inDigis, std::vector<int16_t>& rawDigis);
+
+  inline std::vector<bool>& GetAPVFlags() { return restorer->GetAPVFlags(); }
+  inline std::map<uint16_t, std::vector<int16_t>>& GetBaselineMap() { return restorer->GetBaselineMap(); }
+  inline std::map<uint16_t, std::map<uint16_t,int16_t>>& GetSmoothedPoints() { return restorer->GetSmoothedPoints(); }
+  inline const std::vector<std::pair<short,float>>& getAPVsCM() { return subtractorCMN->getAPVsCM(); }
 
   const std::unique_ptr<SiStripPedestalsSubtractor> subtractorPed;
   const std::unique_ptr<SiStripCommonModeNoiseSubtractor> subtractorCMN;
