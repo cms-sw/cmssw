d4991362da746cd88d42ff228724ff3ab8bea775
Nicola Minafra <nicola.minafra@cern.ch>
2018-04-29 11:20:42 +0200

    CFD for non saturated signals

--- RecoCTPPS/TotemRPLocal/interface/TotemTimingRecHitProducerAlgorithm.h
+++ RecoCTPPS/TotemRPLocal/interface/TotemTimingRecHitProducerAlgorithm.h
@@ -1,11 +1,11 @@
 /****************************************************************************
- *
- * This is a part of CTPPS offline software.
- * Authors:
- *   Laurent Forthomme (laurent.forthomme@cern.ch)
- *   Nicola Minafra
- *
- ****************************************************************************/
+*
+* This is a part of CTPPS offline software.
+* Authors:
+*   Laurent Forthomme (laurent.forthomme@cern.ch)
+*   Nicola Minafra
+*
+****************************************************************************/
 
 #ifndef RecoCTPPS_TotemRPLocal_TotemTimingRecHitProducerAlgorithm
 #define RecoCTPPS_TotemRPLocal_TotemTimingRecHitProducerAlgorithm
@@ -19,51 +19,46 @@
 #include "DataFormats/CTPPSReco/interface/TotemTimingRecHit.h"
 
 #include <algorithm>
+#include "TGraph.h"
 
-#include "Geometry/VeryForwardGeometryBuilder/interface/CTPPSGeometry.h"
 #include "Geometry/VeryForwardRPTopology/interface/RPTopology.h"
+#include "Geometry/VeryForwardGeometryBuilder/interface/CTPPSGeometry.h"
 
-class TotemTimingRecHitProducerAlgorithm {
-public:
-  TotemTimingRecHitProducerAlgorithm(const edm::ParameterSet &conf);
+class TotemTimingRecHitProducerAlgorithm
+{
+  public:
+    TotemTimingRecHitProducerAlgorithm( const edm::ParameterSet& conf );
 
-  void build(const CTPPSGeometry *, const edm::DetSetVector<TotemTimingDigi> &,
-             edm::DetSetVector<TotemTimingRecHit> &);
+    void build( const CTPPSGeometry*, const edm::DetSetVector<TotemTimingDigi>&, edm::DetSetVector<TotemTimingRecHit>& );
 
-  struct RegressionResults {
-    float m;
-    float q;
-    float rms;
-    RegressionResults() : m(0), q(0), rms(0){};
-  };
+    struct RegressionResults{
+      float m;
+      float q;
+      float rms;
+      RegressionResults() : m(0), q(0), rms(0) {};
+    };
 
-private:
-  static const double SAMPIC_SAMPLING_PERIOD_NS;
-  static const double SAMPIC_MAX_NUMBER_OF_SAMPLES;
-  static const double SAMPIC_ADC_V;
+  private:
+    static const double SAMPIC_SAMPLING_PERIOD_NS;
+    static const double SAMPIC_MAX_NUMBER_OF_SAMPLES;
+    static const double SAMPIC_ADC_V;
 
-  unsigned int baselinePoints_;
-  unsigned int risingEdgePointsBeforeTh_;
-  unsigned int risingEdgePoints_;
-  double threholdFactor_;
-  double cfdFraction_;
-  double hysteresis_;
+    int baselinePoints_;
+    int risingEdgePointsBeforeTh_;
+    int risingEdgePoints_;
+    double saturationLimit_;
+    double thresholdFactor_;
+    double cfdFraction_;
+    double hysteresis_;
+    TotemTimingRecHit::TimingAlgorithm mode_;
 
-  float tmp_1_;
-  float tmp_2_;
+    RegressionResults SimplifiedLinearRegression( const std::vector<float>& time, const std::vector<float>& data, const unsigned int start_at, const unsigned int points ) const;
 
-  RegressionResults SimplifiedLinearRegression(const std::vector<float> &time,
-                                               const std::vector<float> &data,
-                                               const unsigned int start_at,
-                                               const unsigned int points) const;
+    int FastDiscriminator( const std::vector<float>& data, const float& threshold ) const;
 
-  int FastDiscriminator(const std::vector<float> &data,
-                        const float &threshold) const;
+    float SmartTimeOfArrival(const std::vector<float>& time, const std::vector<float>& data, const float threshold );
+    float ConstantFractionDiscriminator(const std::vector<float>& time, const std::vector<float>& data );
 
-  float SmartTimeOfArrival(const std::vector<float> &time,
-                           const std::vector<float> &data);
-  float ConstantFractionDiscriminator(const std::vector<float> &time,
-                                      const std::vector<float> &data);
 };
 
 #endif
