0262991dde293f70517cc263eed55d7b28b4fa7a
Vladimir <Vladimir.Ivantchenko@cern.ch>
2018-04-30 15:36:18 +0200

    updated monopole simulation according to recent updates in Geant4

--- SimG4Core/Physics/interface/Monopole.h
+++ SimG4Core/Physics/interface/Monopole.h
@@ -1,5 +1,5 @@
-#ifndef SimG4Core_Physics_G4Monopole_h
-#define SimG4Core_Physics_G4Monopole_h 1
+#ifndef SimG4Core_Physics_Monopole_h
+#define SimG4Core_Physics_Monopole_h 1
 
 #include "G4ParticleDefinition.hh"
 #include "globals.hh"
@@ -9,20 +9,18 @@
 // ###                       Monopole                                 ###
 // ######################################################################
 
-class G4Monopole : public G4ParticleDefinition {
+class Monopole : public G4ParticleDefinition {
 
 public: 
   
-  G4Monopole (const G4String name="Monopole", G4int pdgEncoding= 0, 
-	      G4double mass_=100.*GeV, G4int magCharge_=1, G4int elCharge_ =0);
+  Monopole (const G4String& name="Monopole", G4int pdgEncoding= 0, 
+            G4double mass_=100.*CLHEP::GeV, G4int magCharge_=1, G4int elCharge_ =0);
 
   G4double MagneticCharge() const {return magCharge;};
 
 private:
 
-  ~G4Monopole() override;
-
-private:
+  ~Monopole() override;
 
   G4double magCharge;
 };
