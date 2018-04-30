8736d885bc76f2c52ae7ae14e0dbfce6b1e5c848
federico de guio <federico.de.guio@cern.ch>
2018-04-30 18:00:44 +0200

    more fixes

--- CommonTools/ParticleFlow/python/pfCandidatesBadHadRecalibrated_cfi.py
+++ CommonTools/ParticleFlow/python/pfCandidatesBadHadRecalibrated_cfi.py
@@ -1,5 +1,7 @@
 import FWCore.ParameterSet.Config as cms
 
 pfCandidatesBadHadRecalibrated = cms.EDProducer("PFCandidateRecalibrator",
-    pfcandidates = cms.InputTag("particleFlow")
-)
+                                                pfcandidates = cms.InputTag("particleFlow"),
+                                                shortFibreThr = cms.double(1.4),  #taking it from particleFlowClusterHF_cfi.py
+                                                longFibreThr = cms.double(1.4)    #taking it from particleFlowClusterHF_cfi.py
+                                                )
