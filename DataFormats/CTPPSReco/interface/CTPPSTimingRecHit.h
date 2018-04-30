bdf9f5658d5c2186ee91abd3df46ed4248115363
Laurent Forthomme <laurent.forthomme@cern.ch>
2018-04-30 12:03:57 +0200

    Cleanup of the timing rechits

--- DataFormats/CTPPSReco/interface/CTPPSTimingRecHit.h
+++ DataFormats/CTPPSReco/interface/CTPPSTimingRecHit.h
@@ -11,41 +11,40 @@
 #define DataFormats_CTPPSReco_CTPPSTimingRecHit
 
 /// Reconstructed hit in timing detectors.
-class CTPPSTimingRecHit {
-public:
-  CTPPSTimingRecHit()
-      : x_(0.), xWidth_(0.), y_(0.), yWidth_(0.), z_(0.), zWidth_(0.), t_(0.) {}
-  CTPPSTimingRecHit(float x, float xWidth, float y, float yWidth, float z,
-                    float zWidth, float t)
-      : x_(x), xWidth_(xWidth), y_(y), yWidth_(yWidth), z_(z), zWidth_(zWidth),
-        t_(t) {}
+class CTPPSTimingRecHit
+{
+  public:
+    CTPPSTimingRecHit() :
+      x_( 0. ), xWidth_( 0. ), y_( 0. ), yWidth_( 0. ), z_( 0. ), zWidth_( 0. ), t_( 0. ) {}
+    CTPPSTimingRecHit( float x, float xWidth, float y, float yWidth, float z, float zWidth, float t ) :
+      x_( x ), xWidth_( xWidth ), y_( y ), yWidth_( yWidth ), z_( z ), zWidth_( zWidth ), t_( t ) {}
 
-  inline void setX(const float &x) { x_ = x; }
-  inline float getX() const { return x_; }
+    inline void setX( float x ) { x_ = x; }
+    inline float getX() const { return x_; }
 
-  inline void setY(const float &y) { y_ = y; }
-  inline float getY() const { return y_; }
+    inline void setY( float y ) { y_ = y; }
+    inline float getY() const { return y_; }
 
-  inline void setZ(const float &z) { z_ = z; }
-  inline float getZ() const { return z_; }
+    inline void setZ( float z ) { z_ = z; }
+    inline float getZ() const { return z_; }
 
-  inline void setXWidth(const float &xWidth) { xWidth_ = xWidth; }
-  inline float getXWidth() const { return xWidth_; }
+    inline void setXWidth( float xWidth ) { xWidth_ = xWidth; }
+    inline float getXWidth() const { return xWidth_; }
 
-  inline void setYWidth(const float &yWidth) { yWidth_ = yWidth; }
-  inline float getYWidth() const { return yWidth_; }
+    inline void setYWidth( float yWidth ) { yWidth_ = yWidth; }
+    inline float getYWidth() const { return yWidth_; }
 
-  inline void setZWidth(const float &zWidth) { zWidth_ = zWidth; }
-  inline float getZWidth() const { return zWidth_; }
+    inline void setZWidth( float zWidth ) { zWidth_ = zWidth; }
+    inline float getZWidth() const { return zWidth_; }
 
-  inline void setT(const float &t) { t_ = t; }
-  inline float getT() const { return t_; }
+    inline void setT( float t ) { t_ = t; }
+    inline float getT() const { return t_; }
 
-protected:
-  float x_, xWidth_;
-  float y_, yWidth_;
-  float z_, zWidth_;
-  float t_;
+  protected:
+    float x_, xWidth_;
+    float y_, yWidth_;
+    float z_, zWidth_;
+    float t_;
 };
 
 //----------------------------------------------------------------------------------------------------
@@ -56,3 +55,4 @@ inline bool operator<(const CTPPSTimingRecHit &l, const CTPPSTimingRecHit &r) {
 }
 
 #endif
+
