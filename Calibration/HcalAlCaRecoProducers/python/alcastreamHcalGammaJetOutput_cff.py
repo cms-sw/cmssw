de33b9bb0733e154cef58e755111ee04f1c5f0bb
Sunanda <sunanda.banerjee@cern.ch>
2018-05-03 23:10:10 +0200

    Backport PR 23134 to update AlCaReco o/p collection

--- Calibration/HcalAlCaRecoProducers/python/alcastreamHcalGammaJetOutput_cff.py
+++ Calibration/HcalAlCaRecoProducers/python/alcastreamHcalGammaJetOutput_cff.py
@@ -5,6 +5,7 @@ import FWCore.ParameterSet.Config as cms
 #  module alcastreamHcalGammaJetOutput = PoolOutputModule
 alcastreamHcalGammaJetOutput = cms.PSet(
     outputCommands = cms.untracked.vstring('drop *', 
+        'keep HcalNoiseSummary_hcalnoise_*_*',
         'keep *_GammaJetProd_*_*')
 )
 
