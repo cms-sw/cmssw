344b209440a531c9b41702e14d28601d77bfeb18
Pieter David <pieter.david@cern.ch>
2018-05-03 09:55:01 +0200

    Update configs

--- RecoLocalTracker/SiStripZeroSuppression/python/SiStripZeroSuppression_cfi.py
+++ RecoLocalTracker/SiStripZeroSuppression/python/SiStripZeroSuppression_cfi.py
@@ -10,22 +10,14 @@ siStripZeroSuppression = cms.EDProducer("SiStripZeroSuppression",
                                           cms.InputTag('siStripDigis','ScopeMode')),
                                        #   cms.InputTag('siStripDigis','ZeroSuppressed')),
 
-
-    DigisToMergeZS = cms.InputTag('siStripDigis','ZeroSuppressed'),
-    DigisToMergeVR = cms.InputTag('siStripVRDigis','VirginRaw'),
-                                    
-
     storeCM = cms.bool(True), 
     fixCM= cms.bool(False),                # put -999 into CM collection for "inspected" APV
 
-    produceRawDigis = cms.bool(True),     # if mergeCollection is True, produceRawDigi is not considered
+    produceRawDigis = cms.bool(True),
     produceCalculatedBaseline = cms.bool(False),
     produceBaselinePoints = cms.bool(False),
     storeInZScollBadAPV = cms.bool(True), # it selects if in the ZS collection the bad APVs are written. To be kept for ZS
-    mergeCollections = cms.bool(False),
     produceHybridFormat = cms.bool(False)
-    
-    
 )
 
 # The SiStripClusters are not used anymore in phase2 tracking
