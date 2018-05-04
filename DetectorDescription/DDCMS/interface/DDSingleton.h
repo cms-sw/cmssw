767d553374f76a16ffb326596c0337b5c7c93f1e
Ianna Osborne <ianna.osborne@cern.ch>
2018-05-04 16:03:05 +0200

    Generic Singleton pattern

---
+++ DetectorDescription/DDCMS/interface/DDSingleton.h
@@ -0,0 +1,38 @@
+#ifndef DETECTOR_DESCRIPTION_DD_SINGLETON_H
+#define DETECTOR_DESCRIPTION_DD_SINGLETON_H
+
+namespace cms {
+
+  template< typename T, typename CONTEXT >
+    class DDSingleton
+  {
+  public:
+    T* operator->() { return m_instance; }
+    const T* operator->() const { return m_instance; }
+    T& operator*() { return *m_instance; }
+    const T& operator*() const { return *m_instance; }
+    
+  protected:
+    DDSingleton() {
+      static bool static_init __attribute__(( unused )) = []()->bool {
+	m_instance = new T;
+	return true;
+      }();
+    }
+
+    DDSingleton( int ) {
+      static bool static_init __attribute__(( unused )) = []()->bool {
+	m_instance = CONTEXT::init();
+	return true;
+      }();
+    }
+    
+  private:
+    static T *m_instance;
+  };
+}
+
+template < typename T, typename CONTEXT >
+  T *cms::DDSingleton< T, CONTEXT >::m_instance;
+  
+#endif
