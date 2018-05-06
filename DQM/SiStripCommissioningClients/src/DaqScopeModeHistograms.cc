e4458a8db27dbc44cc79e729b2f345061751c5cc
Raffaele <raffaele.gerosa@cern.ch>
2018-05-06 10:59:07 +0200

    DaqScopeMode analysis for spy data + other fixes

--- DQM/SiStripCommissioningClients/src/DaqScopeModeHistograms.cc
+++ DQM/SiStripCommissioningClients/src/DaqScopeModeHistograms.cc
@@ -2,6 +2,7 @@
 #include "CondFormats/SiStripObjects/interface/DaqScopeModeAnalysis.h"
 #include "DataFormats/SiStripCommon/interface/SiStripConstants.h"
 #include "DataFormats/SiStripCommon/interface/SiStripEnumsAndStrings.h"
+#include "DQM/SiStripCommissioningSummary/interface/DaqScopeModeSummaryFactory.h"
 #include "DQM/SiStripCommissioningAnalysis/interface/DaqScopeModeAlgorithm.h"
 #include "DQM/SiStripCommissioningSummary/interface/SummaryGenerator.h"
 #include "FWCore/MessageLogger/interface/MessageLogger.h"
@@ -18,94 +19,118 @@ DaqScopeModeHistograms::DaqScopeModeHistograms( const edm::ParameterSet& pset,
                                                 DQMStore* bei ) 
   : CommissioningHistograms( pset.getParameter<edm::ParameterSet>("DaqScopeModeParameters"),
                              bei,
-                             sistrip::DAQ_SCOPE_MODE ),
-    factory_( new Factory )
+                             sistrip::DAQ_SCOPE_MODE )
 {
-  cout << endl // LogTrace(mlDqmClient_) 
-       << "[DaqScopeModeHistograms::" << __func__ << "]"
-       << " Constructing object...";
+
+  factory_ = auto_ptr<DaqScopeModeSummaryFactory>( new DaqScopeModeSummaryFactory );
+  LogTrace(mlDqmClient_)
+    << "[DaqScopeModeHistograms::" << __func__ << "]"
+    << " Constructing object...";
+
 }
 
 // -----------------------------------------------------------------------------
 /** */
 DaqScopeModeHistograms::~DaqScopeModeHistograms() {
-  cout << endl // LogTrace(mlDqmClient_) 
-       << "[DaqScopeModeHistograms::" << __func__ << "]"
-       << " Constructing object...";
+  LogTrace(mlDqmClient_)
+    << "[DaqScopeModeHistograms::" << __func__ << "]"
+    << " Destructing object...";
 }
 
 // -----------------------------------------------------------------------------	 
 /** */	 
 void DaqScopeModeHistograms::histoAnalysis( bool debug ) {
 
-  // Clear std::map holding analysis objects
-  data_.clear();
+  LogTrace(mlDqmClient_)
+    << "[DaqScopeModeHistograms::" << __func__ << "]";
+
+  // Some initialisation                                                                                                                                                                            
+  uint16_t valid = 0;
+  HistosMap::const_iterator iter;
+  Analyses::iterator ianal;
+  std::map<std::string,uint16_t> errors;
+
+  // Clear map holding analysis objects                                                                                                                                                   
+  for ( ianal = data().begin(); ianal != data().end(); ianal++ ) {
+    if ( ianal->second ) { delete ianal->second; }
+  }
+  data().clear();
+
+  // Iterate through map containing histograms                                                                                                                                                        
+  for ( iter = histos().begin();
+        iter != histos().end(); iter++ ) {
+    
+    // Check vector of histos is not empty                                                                                                                                                            
+    if ( iter->second.empty() ) {
+      edm::LogWarning(mlDqmClient_)
+        << "[DaqScopeModeHistograms::" << __func__ << "]"
+        << " Zero histograms found!";
+      continue;
+    }
+    
+    // Retrieve pointers to profile histos                                                                                                                                                             
+    std::vector<TH1*> profs;
+    Histos::const_iterator ihis = iter->second.begin();
+    for ( ; ihis != iter->second.end(); ihis++ ) {
+      TProfile* prof = ExtractTObject<TProfile>().extract( (*ihis)->me_ );
+      if ( prof ) 
+	profs.push_back(prof);
+    }
+
+    // Perform histo analysis                                                                                                                                                                          
+    DaqScopeModeAnalysis* anal = new DaqScopeModeAnalysis( iter->first );
+    DaqScopeModeAlgorithm algo( this->pset(), anal );
+    algo.analysis( profs );
+    data()[iter->first] = anal;
   
-//   // Iterate through std::map containing std::vectors of profile histograms
-//   CollationsMap::const_iterator iter = collations().begin();
-//   for ( ; iter != collations().end(); iter++ ) {
-    
-//     // Check std::vector of histos is not empty (should be 1 histo)
-//     if ( iter->second.empty() ) {
-//       edm::LogWarning(mlDqmClient_) 
-// 	<< "[DaqScopeModeHistograms::" << __func__ << "]"
-// 	<< " Zero collation histograms found!";
-//       continue;
-//     }
-    
-//     // Retrieve pointers to profile histos for this FED channel 
-//     std::vector<TH1*> histos;
-//     Collations::const_iterator ihis = iter->second.begin(); 
-//     for ( ; ihis != iter->second.end(); ihis++ ) {
-//       TH1F* his = ExtractTObject<TH1F>().extract( ihis->second->getMonitorElement() );
-//       if ( his ) { histos.push_back(his); }
-//     } 
-    
-//     // Perform histo analysis
-//     DaqScopeModeAnalysis anal( iter->first );
-//     DaqScopeModeAlgorithm algo( &anal );
-//     algo.analysis( histos );
-//     data_[iter->first] = anal; 
-//     if ( debug ) {
-//       std::stringstream ss;
-//       anal.print( ss ); 
-//       cout << ss.str() << endl;
-//     }
-    
-//   }
+    if ( anal->isValid() ) { valid++; }
+    if ( !anal->getErrorCodes().empty() ) {
+      errors[anal->getErrorCodes()[0]]++;
+    }
+  }
   
-//   cout << endl // LogTrace(mlDqmClient_) 
-//        << "[DaqScopeModeHistograms::" << __func__ << "]"
-//        << " Analyzed histograms for " 
-//        << collations().size() 
-//        << " FED channels";
-  
+  if ( !histos().empty() ) {
+    edm::LogVerbatim(mlDqmClient_)
+      << "[DaqScopeModeHistograms::" << __func__ << "]"
+      << " Analyzed histograms for " << histos().size()
+      << " FED channels, of which " << valid
+      << " (" << 100 * valid / histos().size()
+      << "%) are valid.";      
+    if ( !errors.empty() ) {	
+      uint16_t count = 0;
+      std::stringstream ss;
+      ss << std::endl;
+      std::map<std::string,uint16_t>::const_iterator ii;
+      for ( ii = errors.begin(); ii != errors.end(); ++ii ) {
+	ss << " " << ii->first << ": " << ii->second << std::endl;
+	count += ii->second;
+      }
+      edm::LogWarning(mlDqmClient_)
+	<< "[DaqScopeModeHistograms::" << __func__ << "]"
+	<< " Found " << count << " errors ("
+	<< 100 * count / histos().size() << "%): "
+	<< ss.str();
+    }      
+  } else {
+    edm::LogWarning(mlDqmClient_)
+      << "[DaqScopeModeHistograms::" << __func__ << "]"
+      << " No histograms to analyze!";
+  }
 }
 
-// -----------------------------------------------------------------------------
-/** */
-void DaqScopeModeHistograms::createSummaryHisto( const sistrip::Monitorable& histo, 
-						 const sistrip::Presentation& type, 
-						 const std::string& directory,
-						 const sistrip::Granularity& gran ) {
-  cout << endl // LogTrace(mlDqmClient_)
-       << "[DaqScopeModeHistograms::" << __func__ << "]";
-  
-  // Check view 
-  sistrip::View view = SiStripEnumsAndStrings::view(directory);
-  if ( view == sistrip::UNKNOWN_VIEW ) { return; }
-
-  // Analyze histograms
-  histoAnalysis( false );
-
-  // Extract data to be histogrammed
-  factory_->init( histo, type, view, directory, gran );
-  uint32_t xbins = factory_->extract( data_ );
-
-  // Create summary histogram (if it doesn't already exist)
-  TH1* summary = histogram( histo, type, view, directory, xbins );
-
-  // Fill histogram with data
-  factory_->fill( *summary );
-  
+// -----------------------------------------------------------------------------                                                                                                              
+void DaqScopeModeHistograms::printAnalyses() {
+  Analyses::iterator ianal = data().begin();
+  Analyses::iterator janal = data().end();
+  for ( ; ianal != janal; ++ianal ) {
+    if ( ianal->second ) {
+      std::stringstream ss;
+      ianal->second->print( ss, 1 );
+      ianal->second->print( ss, 2 );
+      if ( ianal->second->isValid() ) { LogTrace(mlDqmClient_) << ss.str();
+      }
+      else { edm::LogWarning(mlDqmClient_) << ss.str(); }
+    }
+  }
 }
+
