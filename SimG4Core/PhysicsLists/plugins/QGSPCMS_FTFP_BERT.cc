b059dc48da310c2ae218197d81d299b43b54af36
Vladimir <Vladimir.Ivantchenko@cern.ch>
2018-05-07 20:16:07 +0200

    CMS physics lists

--- SimG4Core/PhysicsLists/plugins/QGSPCMS_FTFP_BERT.cc
+++ SimG4Core/PhysicsLists/plugins/QGSPCMS_FTFP_BERT.cc
@@ -1,5 +1,4 @@
 #include "QGSPCMS_FTFP_BERT.h"
-#include "SimG4Core/PhysicsLists/interface/CMSMonopolePhysics.h"
 #include "FWCore/MessageLogger/interface/MessageLogger.h"
 
 #include "G4EmStandardPhysics.hh"
@@ -14,10 +13,8 @@
 #include "G4DataQuestionaire.hh"
 #include "G4HadronPhysicsQGSP_FTFP_BERT.hh"
 
-QGSPCMS_FTFP_BERT::QGSPCMS_FTFP_BERT(G4LogicalVolumeToDDLogicalPartMap& map, 
-			   const HepPDT::ParticleDataTable * table_,
-			   sim::ChordFinderSetter *chordFinderSetter_, 
-			   const edm::ParameterSet & p) : PhysicsList(map, table_, chordFinderSetter_, p) {
+QGSPCMS_FTFP_BERT::QGSPCMS_FTFP_BERT(const edm::ParameterSet & p) 
+  : PhysicsList(p) {
 
   G4DataQuestionaire it(photon);
   
@@ -66,8 +63,5 @@ QGSPCMS_FTFP_BERT::QGSPCMS_FTFP_BERT(G4LogicalVolumeToDDLogicalPartMap& map,
       RegisterPhysics(ncut);
     }
   }
-
-  // Monopoles
-  RegisterPhysics( new CMSMonopolePhysics(table_,chordFinderSetter_,p));
 }
 
--- SimG4Core/PhysicsLists/plugins/QGSPCMS_FTFP_BERT_EMM.cc
+++ SimG4Core/PhysicsLists/plugins/QGSPCMS_FTFP_BERT_EMM.cc
@@ -1,6 +1,5 @@
 #include "QGSPCMS_FTFP_BERT_EMM.h"
 #include "SimG4Core/PhysicsLists/interface/CMSEmStandardPhysicsLPM.h"
-#include "SimG4Core/PhysicsLists/interface/CMSMonopolePhysics.h"
 #include "FWCore/MessageLogger/interface/MessageLogger.h"
 
 #include "G4DecayPhysics.hh"
@@ -14,12 +13,8 @@
 #include "G4DataQuestionaire.hh"
 #include "G4HadronPhysicsQGSP_FTFP_BERT.hh"
 
-QGSPCMS_FTFP_BERT_EMM::QGSPCMS_FTFP_BERT_EMM(
-                       G4LogicalVolumeToDDLogicalPartMap& map, 
-                       const HepPDT::ParticleDataTable * table_,
-                       sim::ChordFinderSetter *chordFinderSetter_, 
-                       const edm::ParameterSet & p) 
-  : PhysicsList(map, table_, chordFinderSetter_, p) {
+QGSPCMS_FTFP_BERT_EMM::QGSPCMS_FTFP_BERT_EMM(const edm::ParameterSet & p) 
+  : PhysicsList(p) {
 
   G4DataQuestionaire it(photon);
   
@@ -68,8 +63,5 @@ QGSPCMS_FTFP_BERT_EMM::QGSPCMS_FTFP_BERT_EMM(
       RegisterPhysics(ncut);
     }
   }
-
-  // Monopoles
-  RegisterPhysics( new CMSMonopolePhysics(table_,chordFinderSetter_,p));
 }
 
