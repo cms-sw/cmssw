4c383834acb367806ef3e2a42a8fa403c0563b59
Fabio Cossutti <fabio.cossutti@ts.infn.it>
2018-05-12 18:29:05 +0200

    remove obsolete geometry configuration files, restore FTL data in FTLDigi

--- DataFormats/FTLDigi/src/classes.h
+++ DataFormats/FTLDigi/src/classes.h
@@ -7,6 +7,11 @@ namespace DataFormats_FTLDigi {
     FTLSample anFTLsample;
     std::vector<FTLSample> vFTLsample;
 
+    FTLDataFrame anFTLDataFrame;
+    std::vector<FTLDataFrame> vFTLDataFrames;
+    edm::SortedCollection< FTLDataFrame > scFTLDataFrames;
+    edm::Wrapper< edm::SortedCollection< FTLDataFrame > > prodFTlDataFrames;
+
     BTLDataFrame anBTLDataFrame;
     std::vector<BTLDataFrame> vBTLDataFrames;
     edm::SortedCollection< BTLDataFrame > scBTLDataFrames;
