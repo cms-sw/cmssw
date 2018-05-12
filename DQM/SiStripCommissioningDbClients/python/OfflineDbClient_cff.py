4c69d32ae998d11b7869639979438902272b23b5
Raffaele <raffaele.gerosa@cern.ch>
2018-05-12 18:48:21 +0200

    updates for calibration scan and run analysis

--- DQM/SiStripCommissioningDbClients/python/OfflineDbClient_cff.py
+++ DQM/SiStripCommissioningDbClients/python/OfflineDbClient_cff.py
@@ -17,7 +17,16 @@ db_client = cms.EDAnalyzer("SiStripCommissioningOfflineDbClient",
     TargetDelay = cms.int32(-1)       # -1: latest tick (old default), otherwise target delay for all ticks' rising edge
   ),
   CalibrationParameters    = cms.PSet(),
-  DaqScopeModeParameters   = cms.PSet(),
+  DaqScopeModeParameters   = cms.PSet(
+        HighThreshold       = cms.double(5),
+        LowThreshold        = cms.double(2),
+        DisableBadStrips    = cms.bool(False),
+        DeadStripMax        = cms.double(10),
+        NoisyStripMin       = cms.double(10),
+        KeepsStripsDisabled = cms.bool(False),
+        SkipPedestalUpdate  = cms.bool(False),
+        SkipTickUpdate  = cms.bool(False)
+        ),
   FastFedCablingParameters = cms.PSet(),
   FedCablingParameters     = cms.PSet(),
   FedTimingParameters      = cms.PSet(),
