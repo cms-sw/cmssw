8e5fae8e1ff25c844dcfb3e5295b2572088cdfdc
Antonio Vagnerini <antonio.vagnerini@desy.de>
2018-05-14 11:56:14 +0200

    correcting inputTag for btagalgo

--- DQMOffline/Trigger/python/MssmHbbMonitoring_cfi.py
+++ DQMOffline/Trigger/python/MssmHbbMonitoring_cfi.py
@@ -105,7 +105,7 @@ mssmHbbMonitoring.muons     = cms.InputTag("muons") # while pfIsolatedMuonsEI ar
 mssmHbbMonitoring.vertices  = cms.InputTag("offlinePrimaryVertices")
 
 #Antonio
-mssmHbbMonitoring.btagalgo        = cms.InputTag("pfDeepCSVJetTags")
+mssmHbbMonitoring.btagalgo        = cms.InputTag("pfDeepCSVDiscriminatorsJetTags:BvsAll")
 mssmHbbMonitoring.workingpoint    = cms.double(0.45) # medium
 
 mssmHbbMonitoring.HTdefinition = cms.string('pt>30 & abs(eta)<2.5')
