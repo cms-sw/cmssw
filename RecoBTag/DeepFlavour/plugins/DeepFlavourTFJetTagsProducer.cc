13d6c65fcabb3e3baa27e192c5155403d4e654f9
Emil Bols <emilbols@hotmail.com>
2018-05-15 10:55:35 +0200

    Updated the DeepFlavour model to the newest training.

--- RecoBTag/DeepFlavour/plugins/DeepFlavourTFJetTagsProducer.cc
+++ RecoBTag/DeepFlavour/plugins/DeepFlavourTFJetTagsProducer.cc
@@ -136,9 +136,9 @@ void DeepFlavourTFJetTagsProducer::fillDescriptions(edm::ConfigurationDescriptio
   desc.add<std::vector<std::string>>("input_names", 
     { "input_1", "input_2", "input_3", "input_4", "input_5" });
   desc.add<edm::FileInPath>("graph_path",
-    edm::FileInPath("RecoBTag/Combined/data/DeepFlavourV01_GraphDef_PtCut/constant_graph.pb"));
+    edm::FileInPath("RecoBTag/Combined/data/DeepFlavourV02_realistic_training/constant_graph.pb"));
   desc.add<std::vector<std::string>>("lp_names",
-    { "globals_input_batchnorm/keras_learning_phase" });
+    { "cpf_input_batchnorm/keras_learning_phase" });
   desc.add<std::vector<std::string>>("output_names",
     { "ID_pred/Softmax", "regression_pred/BiasAdd" });
   {
