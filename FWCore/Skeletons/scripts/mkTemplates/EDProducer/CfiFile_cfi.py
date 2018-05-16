d697e59b69bd51134b38edb5e927343cc637acd4
Chris Jones <chrisdjones15@gmail.com>
2018-05-16 10:27:12 -0500

    Removed obsolete python files from skeletons

--- FWCore/Skeletons/scripts/mkTemplates/EDProducer/CfiFile_cfi.py
+++
@@ -1,6 +0,0 @@
-import FWCore.ParameterSet.Config as cms
-
-demo = cms.EDProducer('__class__'
-@example_myparticle     , muons = cms.InputTag('muons')
-@example_myparticle     , electrons = cms.InputTag('pixelMatchGsfElectrons')
-)
