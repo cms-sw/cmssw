e09114abffa83b20a2e097231f9e27d7ec3ba384
Fabio Cossutti <fabio.cossutti@ts.infn.it>
2018-05-17 18:34:29 +0200

    Add phase2_timing_layer_new modifier to Reco configurations

--- RecoLocalFastTime/Configuration/python/RecoLocalFastTime_cff.py
+++ RecoLocalFastTime/Configuration/python/RecoLocalFastTime_cff.py
@@ -5,3 +5,9 @@ from RecoLocalFastTime.FTLRecProducers.ftlRecHits_cfi import ftlRecHits
 
 fastTimingLocalReco = cms.Sequence(ftlUncalibratedRecHits*ftlRecHits)
 
+from RecoLocalFastTime.FTLRecProducers.mtdUncalibratedRecHits_cfi import mtdUncalibratedRecHits
+_phase2_timing_layer_new_fastTimingLocalReco = cms.Sequence(mtdUncalibratedRecHits*ftlRecHits)
+from Configuration.Eras.Modifier_phase2_timing_layer_new_cff import phase2_timing_layer_new
+phase2_timing_layer_new.toReplaceWith(fastTimingLocalReco, _phase2_timing_layer_new_fastTimingLocalReco)
+phase2_timing_layer_new.toModify(ftlRecHits, barrelUncalibratedRecHits = cms.InputTag('mtdUncalibratedRecHits:FTLBarrel'), 
+                                 endcapUncalibratedRecHits = cms.InputTag('mtdUncalibratedRecHits:FTLEndcap') )
