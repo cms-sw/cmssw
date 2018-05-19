e6c447ed5cea386617b47011f6c5ea2762eeb143
Vincenzo Innocente <vincenzo.innocente@cern.ch>
2018-05-19 10:46:21 +0200

    make it constexpr

--- DataFormats/Math/interface/PtEtaPhiMass.h
+++ DataFormats/Math/interface/PtEtaPhiMass.h
@@ -2,9 +2,9 @@
 #define DataFormats_Math_PtEtaPhiMass_h
 
 #include<cmath>
-inline float __attribute__((always_inline)) __attribute__ ((pure))
+constexpr float __attribute__((always_inline)) __attribute__ ((pure))
 etaFromXYZ(float x, float y, float z) { float t(z/std::sqrt(x*x+y*y)); return ::asinhf(t);} 
-inline float __attribute__((always_inline)) __attribute__ ((pure))
+constexpr float __attribute__((always_inline)) __attribute__ ((pure))
 etaFromRZ(float r, float z) { float t(z/r); return ::asinhf(t);} 
 
 
@@ -16,20 +16,20 @@ private:
   float pt_, eta_, phi_, mass_;
 public:
   // default constructor (unitialized)
-  PtEtaPhiMass() {}
+  constexpr PtEtaPhiMass() {}
 
   //positional constructor (still compatible with Root, c++03)
-  PtEtaPhiMass(float ipt, float ieta, float iphi, float imass):
+  constexpr PtEtaPhiMass(float ipt, float ieta, float iphi, float imass):
     pt_(ipt), eta_(ieta), phi_(iphi), mass_(imass){}
 
   /// transverse momentum                                                               
-  float pt() const { return pt_;}
+  constexpr float pt() const { return pt_;}
   /// momentum pseudorapidity                                                           
-  float eta() const { return eta_; }
+  constexpr float eta() const { return eta_; }
   /// momentum azimuthal angle                                                          
-  float phi() const { return phi_; }
+  constexpr float phi() const { return phi_; }
     /// mass                                                                              
-  float mass() const { return mass_; }
+  constexpr float mass() const { return mass_; }
 
 };
 
