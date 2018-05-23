2ed4b5fae01f3d429448599ddeba3c7ede873c96
Thomas Reis <thomas.reis@cern.ch>
2018-05-23 14:23:45 +0200

    Update L1T muon pT resolution calculation.

--- DQMOffline/L1Trigger/src/L1TMuonDQMOffline.cc
+++ DQMOffline/L1Trigger/src/L1TMuonDQMOffline.cc
@@ -91,8 +91,8 @@ L1TMuonDQMOffline::EtaRegion MuonGmtPair::etaRegion() const {
 
 double MuonGmtPair::getDeltaVar(const L1TMuonDQMOffline::ResType type) const {
     if (type == L1TMuonDQMOffline::kResPt)      return (gmtPt() - pt()) / pt();
-    if (type == L1TMuonDQMOffline::kRes1OverPt) return 1/gmtPt() - 1/pt();
-    if (type == L1TMuonDQMOffline::kResQOverPt) return gmtCharge()/gmtPt() - charge()/pt();
+    if (type == L1TMuonDQMOffline::kRes1OverPt) return (pt() - gmtPt()) / gmtPt(); // (1/gmtPt - 1/pt) / (1/pt)
+    if (type == L1TMuonDQMOffline::kResQOverPt) return (gmtCharge()*charge()*pt() - gmtPt()) / gmtPt(); // (gmtCharge/gmtPt - charge/pt) / (charge/pt) with gmtCharge/charge = gmtCharge*charge
     if (type == L1TMuonDQMOffline::kResPhi)     return reco::deltaPhi(gmtPhi(), phi());
     if (type == L1TMuonDQMOffline::kResEta)     return gmtEta() - eta();
     if (type == L1TMuonDQMOffline::kResCh)      return gmtCharge() - charge();
@@ -153,7 +153,7 @@ L1TMuonDQMOffline::L1TMuonDQMOffline(const ParameterSet & ps) :
     m_effStrings({ {kEffPt, "pt"}, {kEffPhi, "phi"}, {kEffEta, "eta"}, {kEffVtx, "vtx"} }),
     m_effLabelStrings({ {kEffPt, "p_{T} (GeV)"}, {kEffPhi, "#phi"}, {kEffEta, "#eta"}, {kEffVtx, "# vertices"} }),
     m_resStrings({ {kResPt, "pt"}, {kRes1OverPt, "1overpt"}, {kResQOverPt, "qoverpt"}, {kResPhi, "phi"}, {kResEta, "eta"}, {kResCh, "charge"} }),
-    m_resLabelStrings({ {kResPt, "(p_{T}^{L1} - p_{T}^{reco})/p_{T}^{reco}"}, {kRes1OverPt, "1/p_{T}^{L1} - 1/p_{T}^{reco}"}, {kResQOverPt, "q^{L1}/p_{T}^{L1} - q^{reco}/p_{T}^{reco}"}, {kResPhi, "#phi_{L1} - #phi_{reco}"}, {kResEta, "#eta_{L1} - #eta_{reco}"}, {kResCh, "charge^{L1} - charge^{reco}"} }),
+    m_resLabelStrings({ {kResPt, "(p_{T}^{L1} - p_{T}^{reco})/p_{T}^{reco}"}, {kRes1OverPt, "(p_{T}^{reco} - p_{T}^{L1})/p_{T}^{L1}"}, {kResQOverPt, "(q^{L1}*q^{reco}*p_{T}^{reco} - p_{T}^{L1})/p_{T}^{L1}"}, {kResPhi, "#phi_{L1} - #phi_{reco}"}, {kResEta, "#eta_{L1} - #eta_{reco}"}, {kResCh, "charge^{L1} - charge^{reco}"} }),
     m_etaStrings({ {kEtaRegionAll, "etaMin0_etaMax2p4"}, {kEtaRegionBmtf, "etaMin0_etaMax0p83"}, {kEtaRegionOmtf, "etaMin0p83_etaMax1p24"}, {kEtaRegionEmtf, "etaMin1p24_etaMax2p4"} }),
     m_qualStrings({ {kQualAll, "qualAll"}, {kQualOpen, "qualOpen"}, {kQualDouble, "qualDouble"}, {kQualSingle, "qualSingle"} }),
     m_verbose(ps.getUntrackedParameter<bool>("verbose")),
@@ -648,9 +648,9 @@ std::vector<float> L1TMuonDQMOffline::getHistBinsEff(EffType eff) {
 }
 
 std::tuple<int, double, double> L1TMuonDQMOffline::getHistBinsRes(ResType res){
-    if (res == kResPt)      return {100, -50., 50.};
-    if (res == kRes1OverPt) return {50, -0.05, 0.05};
-    if (res == kResQOverPt) return {50, -0.05, 0.05};
+    if (res == kResPt)      return {50, -2.5, 2.5};
+    if (res == kRes1OverPt) return {50, -2., 2.};
+    if (res == kResQOverPt) return {50, -2., 2.};
     if (res == kResPhi)     return {96, -0.2, 0.2};
     if (res == kResEta)     return {100, -0.1, 0.1};
     if (res == kResCh)      return {5, -2, 3};
