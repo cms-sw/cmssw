d6e0e31fbbf04ce95b6ae04637bc2ee28e2aad4e
Slava Krutelyov <slava77@cern.ch>
2018-05-27 16:16:31 +0200

    add pp, hcalnzs, and trackingOnly scenarios for Era_Run2_2018_highBetaStar

---
+++ Configuration/DataProcessing/python/Impl/ppEra_Run2_2018_highBetaStar.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python
+"""
+_ppEra_Run2_2018_highBetaStar_
+
+Scenario supporting proton collisions
+
+"""
+
+import os
+import sys
+
+from Configuration.DataProcessing.Reco import Reco
+import FWCore.ParameterSet.Config as cms
+from Configuration.Eras.Era_Run2_2018_highBetaStar_cff import Run2_2018_highBetaStar
+
+from Configuration.DataProcessing.Impl.pp import pp
+
+class ppEra_Run2_2018_highBetaStar(pp):
+    def __init__(self):
+        pp.__init__(self)
+        self.recoSeq=''
+        self.cbSc='pp'
+        self.addEI=True
+        self.eras=Run2_2018_highBetaStar
+        #keep post-era parts the same as in the default 2018 era
+        self.promptCustoms += [ 'Configuration/DataProcessing/RecoTLR.customisePostEra_Run2_2018' ]
+        self.expressCustoms += [ 'Configuration/DataProcessing/RecoTLR.customisePostEra_Run2_2018' ]
+        self.visCustoms += [ 'Configuration/DataProcessing/RecoTLR.customisePostEra_Run2_2018' ]
+    """
+    _ppEra_Run2_2018_highBetaStar_
+
+    Implement configuration building for data processing for proton
+    collision data taking for Run2 2018 highBetaStar data taking
+
+    """
---
+++ Configuration/DataProcessing/python/Impl/trackingOnlyEra_Run2_2018_highBetaStar.py
@@ -0,0 +1,38 @@
+#!/usr/bin/env python
+"""
+_trackingOnlyEra_Run2_2018_highBetaStar
+
+Scenario supporting proton collisions and tracking only reconstruction for HP beamspot
+
+"""
+
+import os
+import sys
+
+from   Configuration.DataProcessing.Impl.trackingOnly import trackingOnly
+import FWCore.ParameterSet.Config as cms
+from   Configuration.Eras.Era_Run2_2018_highBetaStar_cff import Run2_2018_highBetaStar
+
+from   Configuration.DataProcessing.Impl.pp import pp
+
+class trackingOnlyEra_Run2_2018_highBetaStar(trackingOnly):
+    def __init__(self):
+        trackingOnly.__init__(self)
+        # tracking only RECO is sufficient, to run high performance BS at PCL;
+        # some dedicated customization are required, though: customisePostEra_Run2_2018_trackingOnly
+        self.recoSeq=':reconstruction_trackingOnly'
+        self.cbSc='pp'
+        self.addEI=False
+        self.eras=Run2_2018_highBetaStar
+        #keep post-era parts the same as in the default 2018 era
+        self.promptCustoms += [ 'Configuration/DataProcessing/RecoTLR.customisePostEra_Run2_2018' ]
+        self.expressCustoms += [ 'Configuration/DataProcessing/RecoTLR.customisePostEra_Run2_2018_express_trackingOnly' ]
+        self.visCustoms += [ 'Configuration/DataProcessing/RecoTLR.customisePostEra_Run2_2018' ]
+
+    """
+    _trackingOnlyEra_Run2_2018_highBetaStar
+
+    Implement configuration building for data processing for proton
+    collision data taking for Run2, 2018 high performance beamspot in highBetaStar data taking
+
+    """
