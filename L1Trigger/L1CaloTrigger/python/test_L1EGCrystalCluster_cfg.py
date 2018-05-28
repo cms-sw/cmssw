a21fec1c383d701115866dad1fdae7b9ea3376e0
Tyler Ruggles <truggles@wisc.edu>
2018-05-28 14:06:57 +0200

    Updating example script

--- L1Trigger/L1CaloTrigger/python/test_L1EGCrystalCluster_cfg.py
+++ L1Trigger/L1CaloTrigger/python/test_L1EGCrystalCluster_cfg.py
@@ -16,7 +16,13 @@ process.maxEvents = cms.untracked.PSet( input = cms.untracked.int32(100) )
 
 process.source = cms.Source("PoolSource",
    # Set to do test run on official Phase-2 L1T Ntuples
-   fileNames = cms.untracked.vstring('file:/eos/cms/store/group/upgrade/sandhya/SMP-PhaseIIFall17D-00001.root')
+   fileNames = cms.untracked.vstring('file:root://cmsxrootd.fnal.gov///store/mc/PhaseIISpring17D/SingleE_FlatPt-8to100/GEN-SIM-DIGI-RAW/PU200_90X_upgrade2023_realistic_v9-v1/120000/04B4BF1D-1E2C-E711-BE1C-7845C4FC39D1.root'),
+   inputCommands = cms.untracked.vstring(
+                    "keep *",
+                    "drop l1tEMTFHitExtras_simEmtfDigis_CSC_HLT",
+                    "drop l1tEMTFHitExtras_simEmtfDigis_RPC_HLT",
+                    "drop l1tEMTFTrackExtras_simEmtfDigis__HLT",
+   )
 )
 
 # All this stuff just runs the various EG algorithms that we are studying
@@ -24,7 +30,7 @@ process.source = cms.Source("PoolSource",
 # ---- Global Tag :
 process.load('Configuration.StandardSequences.FrontierConditions_GlobalTag_cff')
 from Configuration.AlCa.GlobalTag import GlobalTag
-process.GlobalTag = GlobalTag(process.GlobalTag, '90X_upgrade2023_realistic_v9', '')
+process.GlobalTag = GlobalTag(process.GlobalTag, '100X_upgrade2023_realistic_v1', '')
 
 # Choose a 2023 geometry!
 process.load('Configuration.Geometry.GeometryExtended2023D17Reco_cff')
