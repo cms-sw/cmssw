61a18391d28fdc1604455e91a726a8712fad8781
Thomas Reis <thomas.reis@cern.ch>
2018-05-29 17:27:05 +0200

    Add OMTF quality tests and move some uGMT tests to new muon test configuration.

---
+++ DQM/L1TMonitorClient/python/L1TStage2MuonQualityTests_cff.py
@@ -0,0 +1,18 @@
+# quality tests for stage 2 L1T muons 
+
+import FWCore.ParameterSet.Config as cms
+
+l1TStage2MuonQualityTests = cms.EDAnalyzer("QualityTester",
+    qtList = cms.untracked.FileInPath('DQM/L1TMonitorClient/data/L1TStage2MuonQualityTests.xml'),
+    QualityTestPrescaler = cms.untracked.int32(1),
+    getQualityTestsFromFile = cms.untracked.bool(True),
+    testInEventloop = cms.untracked.bool(False),
+    qtestOnEndLumi = cms.untracked.bool(True),
+    qtestOnEndRun = cms.untracked.bool(True),
+    qtestOnEndJob = cms.untracked.bool(False),
+    reportThreshold = cms.untracked.string(""),
+    verboseQT = cms.untracked.bool(True)
+)
+
+l1TStage2MuonQualityTestsCollisions = l1TStage2MuonQualityTests.clone()
+l1TStage2MuonQualityTestsCollisions.qtList = cms.untracked.FileInPath('DQM/L1TMonitorClient/data/L1TStage2MuonQualityTestsCollisions.xml')
