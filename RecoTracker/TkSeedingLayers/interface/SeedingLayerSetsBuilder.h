e77256839e6280f5e9c00dd583e414284546a091
Angira Rastogi <rastogiangira0908@gmail.com>
2018-05-29 07:54:43 +0200

    Making SeedingLayerSetsHits pointer for FastSim seeding

--- RecoTracker/TkSeedingLayers/interface/SeedingLayerSetsBuilder.h
+++ RecoTracker/TkSeedingLayers/interface/SeedingLayerSetsBuilder.h
@@ -8,10 +8,11 @@
 #include "FWCore/ParameterSet/interface/ParameterSetDescription.h"
 #include "FWCore/Utilities/interface/InputTag.h"
 #include "FWCore/Framework/interface/ESWatcher.h"
-
+#include "FWCore/Utilities/interface/EDGetToken.h"
 #include "Geometry/CommonDetUnit/interface/GeomDetEnumerators.h"
 #include "DataFormats/TrackerCommon/interface/TrackerDetSide.h"
-
+#include "DataFormats/TrackerRecHit2D/interface/FastTrackerRecHit.h"
+#include "DataFormats/TrackerRecHit2D/interface/FastTrackerRecHitCollection.h"
 #include <string>
 #include <vector>
 namespace edm { class Event; class EventSetup; class ConsumesCollector;}
@@ -41,6 +42,8 @@ public:
   const std::vector<SeedingLayerSetsHits::LayerSetIndex>& layerSetIndices() const { return theLayerSetIndices; }
 
   std::unique_ptr<SeedingLayerSetsHits> hits(const edm::Event& ev, const edm::EventSetup& es);
+  //new function for FastSim only
+  std::unique_ptr<SeedingLayerSetsHits> makeSeedingLayerSetsHitsforFastSim(const edm::Event& ev, const edm::EventSetup& es);
 
   static SeedingLayerId nameToEnumId(const std::string& name);
   static std::vector<std::vector<std::string> > layerNamesInSets(const std::vector<std::string> & namesPSet) ;
@@ -51,7 +54,7 @@ private:
 
   edm::ESWatcher<TrackerRecoGeometryRecord> geometryWatcher_;
   edm::ESWatcher<TransientRecHitRecord> trhWatcher_;
-
+  edm::EDGetTokenT<FastTrackerRecHitCollection> fastSimrecHitsToken_;
   struct LayerSpec { 
     LayerSpec(unsigned short index, const std::string& layerName, const edm::ParameterSet& cfgLayer, edm::ConsumesCollector& iC);
     ~LayerSpec() = default;
@@ -77,6 +80,7 @@ private:
   std::vector<const DetLayer *> theLayerDets;
   std::vector<const TransientTrackingRecHitBuilder *> theTTRHBuilders;
   std::vector<LayerSpec> theLayers;
+  bool isFastSim = false;
 };
 
 #endif
