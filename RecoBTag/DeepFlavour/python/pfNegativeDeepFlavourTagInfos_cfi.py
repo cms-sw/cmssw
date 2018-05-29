3fd39f0f281dd1aeb7e990fc591897e5364de432
Emil Bols <emilbols@hotmail.com>
2018-05-29 19:55:02 +0200

    Remove DeepFlavour Negative Tagger

--- RecoBTag/DeepFlavour/python/pfNegativeDeepFlavourTagInfos_cfi.py
+++ RecoBTag/DeepFlavour/python/pfNegativeDeepFlavourTagInfos_cfi.py
@@ -3,7 +3,5 @@ import FWCore.ParameterSet.Config as cms
 from RecoBTag.DeepFlavour.pfDeepFlavourTagInfos_cfi import pfDeepFlavourTagInfos
 
 pfNegativeDeepFlavourTagInfos = pfDeepFlavourTagInfos.clone(
-        shallow_tag_infos = cms.InputTag('pfDeepCSVNegativeTagInfos'),
-        secondary_vertices = cms.InputTag('inclusiveCandidateNegativeSecondaryVertices'),
-        flip = cms.bool(True)
+        shallow_tag_infos = cms.InputTag('pfDeepCSVNegativeTagInfos')
         )
