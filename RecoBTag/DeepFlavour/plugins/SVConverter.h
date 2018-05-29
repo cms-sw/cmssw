407eec4d1f5028a0bb222c1e9b124d43f9580fae
Emil Bols <emilbols@hotmail.com>
2018-05-29 19:55:02 +0200

    Remove DeepFlavour Negative Tagger

--- RecoBTag/DeepFlavour/plugins/SVConverter.h
+++ RecoBTag/DeepFlavour/plugins/SVConverter.h
@@ -15,13 +15,12 @@ namespace btagbtvdeep {
 
       static void SVToFeatures( const reco::VertexCompositePtrCandidate & sv,
                                 const reco::Vertex & pv, const reco::Jet & jet,
-                                SecondaryVertexFeatures & sv_features,
-				const bool flip) {
-
-	math::XYZVector jet_dir = jet.momentum().Unit();
+                                SecondaryVertexFeatures & sv_features) {
+    
         sv_features.pt = sv.pt();
-        sv_features.deltaR = catch_infs_and_bound(std::fabs(Geom::deltaR(sv.position() - pv.position(), flip ? -jet_dir : jet_dir))-0.5,
-						  0,-2,0);
+        sv_features.deltaR = catch_infs_and_bound(
+                               std::fabs(reco::deltaR(sv,jet))-0.5,
+                               0,-2,0);
         sv_features.mass = sv.mass();
         sv_features.ntracks = sv.numberOfDaughters();
         sv_features.chi2 = sv.vertexChi2();
@@ -35,11 +34,7 @@ namespace btagbtvdeep {
         sv_features.d3d = d3d_meas.value();
         sv_features.d3dsig = catch_infs_and_bound(d3d_meas.value()/d3d_meas.error(),
                                                   0,-1,800);
-	float costhetasvpv = vertexDdotP(sv,pv);
-	if(flip){
-	  costhetasvpv = -1.0*costhetasvpv;
-	}
-        sv_features.costhetasvpv = costhetasvpv;
+        sv_features.costhetasvpv = vertexDdotP(sv,pv);
         sv_features.enratio = sv.energy()/jet.energy();
     
       } 
