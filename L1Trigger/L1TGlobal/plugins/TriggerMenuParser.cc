4281594ae494aaf45261d249bd60284f42faa7f3
Christopher Jones <chrisdjones15@gmail.com>
2018-05-30 13:34:45 -0500

    Apply code-check patch for string comparison

--- L1Trigger/L1TGlobal/plugins/TriggerMenuParser.cc
+++ L1Trigger/L1TGlobal/plugins/TriggerMenuParser.cc
@@ -3380,7 +3380,7 @@ bool l1t::TriggerMenuParser::parseAlgorithm( tmeventsetup::esAlgorithm algorithm
     std::string algAlias = algorithm.getName();
     const std::string& algName  = algorithm.getName();
 
-    if (algAlias == "") {
+    if (algAlias.empty()) {
         algAlias = algName;
         LogDebug("TriggerMenuParser")
                 << "\n    No alias defined for algorithm. Alias set to algorithm name."
