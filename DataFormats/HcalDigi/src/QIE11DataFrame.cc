21137905baa83066295755511015c86175b590c9
Viktor Khristenko <vdkhristenko1991@gmail.com>
2018-05-30 11:35:35 +0200

    Make HcalDetId and HcalDigi dataformats constexpr
    Similar to DataFormats/DetId updates for HcalDetid and HcalDigi
      - move the implementation to the header files
      - mark methods as `constexpr`

--- DataFormats/HcalDigi/src/QIE11DataFrame.cc
+++ DataFormats/HcalDigi/src/QIE11DataFrame.cc
@@ -1,38 +1,6 @@
 #include "DataFormats/HcalDigi/interface/QIE11DataFrame.h"
 #include "DataFormats/HcalDetId/interface/HcalGenericDetId.h"
 
-void QIE11DataFrame::setCapid0(int cap0) {
-  m_data[0]&=0xFCFF; // inversion of the capid0 mask
-  m_data[0]|=((cap0&Sample::MASK_CAPID)<<Sample::OFFSET_CAPID);  
-}
-
-void QIE11DataFrame::setFlags(uint16_t v) {
-  m_data[size()-1]=v;
-}
-
-void QIE11DataFrame::copyContent(const QIE11DataFrame& digi) {
-  for (edm::DataFrame::size_type i=0; i<size() && i<digi.size();i++){
-    Sample sam = digi[i];
-	setSample(i,sam.adc(),sam.tdc(),sam.soi());
-  }
-}
-
-int QIE11DataFrame::presamples() const {
-  for (int i=0; i<samples(); i++) {
-    if ((*this)[i].soi()) return i;
-  }
-  return -1;
-}
-
-void QIE11DataFrame::setZSInfo(bool markAndPass){
-	if(markAndPass) m_data[0] |= (markAndPass&MASK_FLAVOR)<<OFFSET_FLAVOR;
-}
-
-void QIE11DataFrame::setSample(edm::DataFrame::size_type isample, int adc, int tdc, bool soi) {
-  if (isample>=size()) return;
-  m_data[isample+1]=(adc&Sample::MASK_ADC)|(soi?(Sample::MASK_SOI):(0))|((tdc&Sample::MASK_TDC)<<Sample::OFFSET_TDC);
-}
-
 std::ostream& operator<<(std::ostream& s, const QIE11DataFrame& digi) {
   if (digi.detid().det()==DetId::Hcal) {
     s << HcalGenericDetId(digi.detid());
