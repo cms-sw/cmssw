a6718be14a0737a445bb84f93cbc0753369174bf
Emil Bols <emilbols@hotmail.com>
2018-05-30 06:20:25 +0200

    move arguments inside .clone

--- RecoVertex/AdaptiveVertexFinder/python/inclusiveNegativeVertexing_cff.py
+++ RecoVertex/AdaptiveVertexFinder/python/inclusiveNegativeVertexing_cff.py
@@ -6,22 +6,26 @@ from RecoVertex.AdaptiveVertexFinder.candidateVertexArbitrator_cfi import *
 
 
 inclusiveCandidateNegativeVertexFinder = inclusiveCandidateVertexFinder.clone(
-   vertexMinAngleCosine = cms.double(-0.95) 
+   vertexMinAngleCosine = -0.95, 
+   clusterizer = inclusiveCandidateVertexFinder.clusterizer.clone(
+        clusterMinAngleCosine = -0.5
+        )
 )
-inclusiveCandidateNegativeVertexFinder.clusterizer.clusterMinAngleCosine = cms.double(-0.5)
 
-candidateNegativeVertexMerger = candidateVertexMerger.clone()
-candidateNegativeVertexMerger.secondaryVertices = cms.InputTag("inclusiveCandidateNegativeVertexFinder")
+candidateNegativeVertexMerger = candidateVertexMerger.clone(
+   secondaryVertices = "inclusiveCandidateNegativeVertexFinder"
+)
 
-candidateNegativeVertexArbitrator = candidateVertexArbitrator.clone()
-candidateNegativeVertexArbitrator.secondaryVertices = cms.InputTag("candidateNegativeVertexMerger")
-candidateNegativeVertexArbitrator.dRCut = cms.double(-0.4)
+candidateNegativeVertexArbitrator = candidateVertexArbitrator.clone(
+   secondaryVertices = "candidateNegativeVertexMerger",
+   dRCut = -0.4
+)
 
-inclusiveCandidateNegativeSecondaryVertices = candidateVertexMerger.clone()
-inclusiveCandidateNegativeSecondaryVertices.secondaryVertices = cms.InputTag("candidateNegativeVertexArbitrator")
-inclusiveCandidateNegativeSecondaryVertices.maxFraction = 0.2
-inclusiveCandidateNegativeSecondaryVertices.minSignificance = 10.
-
+inclusiveCandidateNegativeSecondaryVertices = candidateVertexMerger.clone(
+   secondaryVertices = "candidateNegativeVertexArbitrator",
+   maxFraction = 0.2,
+   minSignificance = 10.
+)
 
 
 inclusiveCandidateNegativeVertexingTask = cms.Task(inclusiveCandidateNegativeVertexFinder,
@@ -33,25 +37,27 @@ inclusiveCandidateNegativeVertexing = cms.Sequence(inclusiveCandidateNegativeVer
 
 
 inclusiveCandidateNegativeVertexFinderCvsL = inclusiveCandidateVertexFinder.clone(
-   vertexMinDLen2DSig = cms.double(1.25),
-   vertexMinDLenSig = cms.double(0.25),
-   vertexMinAngleCosine = cms.double(-0.95)
+   vertexMinDLen2DSig = 1.25,
+   vertexMinDLenSig = 0.25,
+   vertexMinAngleCosine = -0.95,
+   clusterizer = inclusiveCandidateVertexFinder.clusterizer.clone(
+      clusterMinAngleCosine = -0.5
+      )
 )
-inclusiveCandidateNegativeVertexFinderCvsL.clusterizer.clusterMinAngleCosine = cms.double(-0.5)
 
 candidateNegativeVertexMergerCvsL = candidateVertexMerger.clone(
-   secondaryVertices = cms.InputTag("inclusiveCandidateNegativeVertexFinderCvsL")
+   secondaryVertices = "inclusiveCandidateNegativeVertexFinderCvsL"
 )
 
 candidateNegativeVertexArbitratorCvsL = candidateVertexArbitrator.clone(
-   secondaryVertices = cms.InputTag("candidateNegativeVertexMergerCvsL"),
-   dRCut = cms.double(-0.4)
+   secondaryVertices = "candidateNegativeVertexMergerCvsL",
+   dRCut = -0.4
 )
 
 inclusiveCandidateNegativeSecondaryVerticesCvsL = candidateVertexMerger.clone(
-   secondaryVertices = cms.InputTag("candidateNegativeVertexArbitratorCvsL"),
-   maxFraction = cms.double(0.2),
-   minSignificance = cms.double(10.)
+   secondaryVertices = "candidateNegativeVertexArbitratorCvsL",
+   maxFraction = 0.2,
+   minSignificance = 10.
 )
 
 
