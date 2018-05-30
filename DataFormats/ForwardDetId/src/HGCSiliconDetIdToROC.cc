c94ba8d668047f35c7de39ca4e29db99cd255f9d
Sunanda <sunanda.banerjee@cern.ch>
2018-05-30 18:43:47 +0200

    Update in view of review

--- DataFormats/ForwardDetId/src/HGCSiliconDetIdToROC.cc
+++ DataFormats/ForwardDetId/src/HGCSiliconDetIdToROC.cc
@@ -4,18 +4,18 @@ HGCSiliconDetIdToROC::HGCSiliconDetIdToROC() {
   const int rocMax(6), cellRows(4), cellColumn(2);
   const int type[rocMax]               = {0,0,1,1,2,2};
   const int start1[rocMax][cellColumn] = {{3,2},{1,0},{1,2},{3,4},{7,6},{5,4}};
-  const int start2[rocMax]             = {0,0,0,0,4,4};
+  const int start2[rocMax]             = {0,0,0,1,4,4};
 
   // key = ROC #; value = vector of trigger cell U.V
   for (int roc=0; roc<rocMax; ++roc) {
     std::vector<std::pair<int,int> > cells;
     if (type[roc] == 1) {
       for (int i1=0; i1<cellColumn; ++i1) {
-	int v = start2[roc];
 	int u = start1[roc][i1];
+	int v = start2[roc] + u/2;
 	for (int i2=0; i2<cellRows; ++i2) {
 	  cells.emplace_back(u,v);
-	  ++u; ++v;
+	  ++u;
 	}
       }
     } else if (type[roc] == 2) {
@@ -44,6 +44,33 @@ HGCSiliconDetIdToROC::HGCSiliconDetIdToROC() {
   for (auto const & itr : triggerIdFromROC_) {
     for (auto cell : itr.second)
       triggerIdToROC_[cell] = itr.first;
+  }
+}
+
+int HGCSiliconDetIdToROC::getROCNumber(int triggerCellU, int triggerCellV,
+				       int type) const {
+  auto itr = triggerIdToROC_.find(std::make_pair(triggerCellU,triggerCellV));
+  int  roc = ((itr == triggerIdToROC_.end()) ? -1 : 
+	      ((type == 0) ? itr->second : (1+itr->second)/2));
+  return roc;
+}
+
+std::vector<std::pair<int,int> > HGCSiliconDetIdToROC::getTriggerId(int roc, int type) const {
+  
+  std::vector<std::pair<int,int> > list;
+  if (type == 0) {
+    auto itr = triggerIdFromROC_.find(roc);
+    return ((itr == triggerIdFromROC_.end()) ? list : itr->second);
+  } else {
+    for (int k=0; k<2; ++k) {
+      int rocx = 2*roc + k - 1;
+      auto itr = triggerIdFromROC_.find(rocx);
+      if (itr != triggerIdFromROC_.end()) {
+	for (auto cell : itr->second)
+	  list.emplace_back(cell);
+      }
+    }
+    return list;
   }
 }
 
