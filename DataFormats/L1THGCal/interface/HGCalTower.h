b8ea25260b47360caf5e1f0a850da942d707c907
Gianluca <gianluca.cerminara@cern.ch>
2018-05-31 17:13:13 +0200

    restore hw quantities in Tower interface

--- DataFormats/L1THGCal/interface/HGCalTower.h
+++ DataFormats/L1THGCal/interface/HGCalTower.h
@@ -14,8 +14,13 @@ namespace l1t {
   class HGCalTower : public L1Candidate {
 
   public:
-    
-    HGCalTower(): etEm_(0.),etHad_(0.),id_(0) {}
+
+    HGCalTower(): etEm_(0.),
+                  etHad_(0.),
+                  id_(0),
+                  hwEtEm_(0),
+                  hwEtHad_(0),
+                  hwEtRatio_(0) {}
 
     HGCalTower(double etEm,
                double etHad,
@@ -25,7 +30,10 @@ namespace l1t {
                int hwpt=0,
                int hweta=0,
                int hwphi=0,
-               int qual=0);
+               int qual=0,
+               int hwEtEm=0,
+               int hwEtHad=0,
+               int hwEtRatio=0);
 
     ~HGCalTower() override;
 
@@ -42,6 +50,14 @@ namespace l1t {
     HGCalTowerID id() const { return id_;}
     short zside() const { return id_.zside(); }
 
+    void setHwEtEm(int et) { hwEtEm_ = et; }
+    void setHwEtHad(int et) { hwEtHad_ = et; }
+    void setHwEtRatio(int ratio) { hwEtRatio_ = ratio; }
+
+    int hwEtEm() const { return hwEtEm_; }
+    int hwEtHad() const { return hwEtHad_; }
+    int hwEtRatio() const { return hwEtRatio_; }
+
   private:
 
     void addEt(double et);
@@ -50,6 +66,10 @@ namespace l1t {
     double etEm_;
     double etHad_;
     HGCalTowerID id_;
+
+    int hwEtEm_;
+    int hwEtHad_;
+    int hwEtRatio_;
 
   };
 
