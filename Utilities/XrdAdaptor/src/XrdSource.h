c200743202edaf17e2ec53be66793e1d793872cd
Mircho Rodozov <mrodozov@cern.ch>
2018-06-01 10:34:37 +0200

    Revert once more

--- Utilities/XrdAdaptor/src/XrdSource.h
+++ Utilities/XrdAdaptor/src/XrdSource.h
@@ -1,6 +1,7 @@
 #ifndef Utilities_XrdAdaptor_XrdSource_h
 #define Utilities_XrdAdaptor_XrdSource_h
 
+#include "XrdCl/XrdClXRootDResponses.hh"
 #include "FWCore/Utilities/interface/get_underlying_safe.h"
 
 #include <memory>
@@ -11,7 +12,6 @@
 #include "QualityMetric.h"
 
 namespace XrdCl {
-    class File;
     class HostList;
 }
 
