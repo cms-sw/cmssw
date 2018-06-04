5c9effdcb64d5ab60eb4f18e97846bb32996d5c1
Thomas Reis <thomas.reis@cern.ch>
2018-06-04 11:57:52 +0200

    Add uGT data vs. emulator quality tests and summary in L1TEMU.

---
+++ DQM/L1TMonitorClient/python/L1TStage2uGTDEQualityTests_cfi.py
@@ -0,0 +1,15 @@
+# quality tests for L1T Stage2 uGT Data vs Emulator 
+ 
+import FWCore.ParameterSet.Config as cms
+
+l1TStage2uGTDEQualityTests = cms.EDAnalyzer("QualityTester",
+    qtList=cms.untracked.FileInPath('DQM/L1TMonitorClient/data/L1TStage2uGTDEQualityTests.xml'),
+    QualityTestPrescaler=cms.untracked.int32(1),
+    getQualityTestsFromFile=cms.untracked.bool(True),
+    testInEventloop=cms.untracked.bool(False),
+    qtestOnEndLumi=cms.untracked.bool(True),
+    qtestOnEndRun=cms.untracked.bool(True),
+    qtestOnEndJob=cms.untracked.bool(False),
+    reportThreshold=cms.untracked.string(""),
+    verboseQT=cms.untracked.bool(True)
+)
