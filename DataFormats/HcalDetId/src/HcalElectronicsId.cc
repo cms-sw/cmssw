30594f67e6f3abc450e23e19e32260fd8f547a2f
Andrea Bocci <andrea.bocci@cern.ch>
2018-06-04 15:41:24 +0200

    Make HcalDetId and HcalDigi dataformats constexpr (#63)
    Update HcalDetids and HcalDigis to support CUDA similar to DataFormats/DetId:
      - move the implementation to the header files
      - mark methods as `constexpr`
    
    Add CUDA tests for HcalDetId and HcalDigi dataformats:
      - create an `HBHEDigiCollection`/`HFDigiCollection`/`HODigiCollection` on the host side and fill it
      - allocate space on the gpu
      - transfer to the gpu
      - run the kernel to compute some func per channel
      - transfer the results back to host
      - assert for each channel

--- DataFormats/HcalDetId/src/HcalElectronicsId.cc
+++ DataFormats/HcalDetId/src/HcalElectronicsId.cc
@@ -1,32 +1,5 @@
 #include "DataFormats/HcalDetId/interface/HcalElectronicsId.h"
 
-HcalElectronicsId::HcalElectronicsId() {
-  hcalElectronicsId_=0xffffffffu;
-}
-
-HcalElectronicsId::HcalElectronicsId(uint32_t id) {
-  hcalElectronicsId_=id;
-}
-
-HcalElectronicsId::HcalElectronicsId(int fiberChan, int fiberIndex, int spigot, int dccid) {
-  hcalElectronicsId_=(fiberChan&0x3) | (((fiberIndex-1)&0x7)<<2) |
-    ((spigot&0xF)<<5) | ((dccid&0x1F)<<9);
-}
-
-HcalElectronicsId::HcalElectronicsId(int slbChan, int slbSite, int spigot, int dccid, int crate, int slot, int tb) {
-  hcalElectronicsId_=(slbChan&0x3) | (((slbSite)&0x7)<<2) |
-    ((spigot&0xF)<<5) | ((dccid&0x1F)<<9);
-  hcalElectronicsId_|=((tb&0x1)<<19) | ((slot&0x1f)<<14) | ((crate&0x3f)<<20);
-  hcalElectronicsId_|=0x02000000;
-}
-
-HcalElectronicsId::HcalElectronicsId(int crate, int slot, int fiber, int fc, bool isTrigger) {
-  hcalElectronicsId_=(fc&0xF) | (((fiber)&0x1F)<<4) |
-    ((slot&0xF)<<9) | ((crate&0x3F)<<13);
-  if (isTrigger)   hcalElectronicsId_|=0x02000000;
-  hcalElectronicsId_|=0x04000000;
-}
-
 std::string HcalElectronicsId::slbChannelCode() const {
   std::string retval;
   if (isTriggerChainId() && isVMEid()) {
@@ -49,12 +22,6 @@ std::string HcalElectronicsId::slbChannelCode() const {
   return retval;
 }
 
-void HcalElectronicsId::setHTR(int crate, int slot, int tb) {
-  if (isUTCAid()) return; // cannot do this for uTCA
-  hcalElectronicsId_&=0x3FFF; // keep the readout chain info
-  hcalElectronicsId_|=((tb&0x1)<<19) | ((slot&0x1f)<<14) | ((crate&0x3f)<<20);
-}
-
 std::ostream& operator<<(std::ostream& os,const HcalElectronicsId& id) {
   if (id.isUTCAid()) {
     if (id.isTriggerChainId()) os << "UTCA(trigger): ";
@@ -71,5 +38,3 @@ std::ostream& operator<<(std::ostream& os,const HcalElectronicsId& id) {
     }
   }
 }
-
-
--- DataFormats/HcalDigi/interface/HBHEDataFrame.h
+++ DataFormats/HcalDigi/interface/HBHEDataFrame.h
@@ -4,7 +4,6 @@
 #include "DataFormats/HcalDetId/interface/HcalDetId.h"
 #include "DataFormats/HcalDetId/interface/HcalElectronicsId.h"
 #include "DataFormats/HcalDigi/interface/HcalQIESample.h"
-#include <vector>
 #include <ostream>
 
 /** \class HBHEDataFrame
@@ -16,40 +15,79 @@ class HBHEDataFrame {
 public:
   typedef HcalDetId key_type; ///< For the sorted collection
 
-  HBHEDataFrame(); // for persistence
-  explicit HBHEDataFrame(const HcalDetId& id);
+  constexpr HBHEDataFrame() 
+      : id_(0), size_(0), hcalPresamples_(0)
+  {}
+  constexpr explicit HBHEDataFrame(const HcalDetId& id) : 
+    id_(id), size_(0), hcalPresamples_(0)
+  {
+    // TODO : test id for HcalBarrel or HcalEndcap
+  }
   
-  const HcalDetId& id() const { return id_; }
-  const HcalElectronicsId& elecId() const { return electronicsId_; }
+  constexpr const HcalDetId& id() const { return id_; }
+  constexpr const HcalElectronicsId& elecId() const { return electronicsId_; }
   
   /// total number of samples in the digi 
-  int size() const { return size_&0xF; }
+  constexpr int size() const { return size_&0xF; }
   /// number of samples before the sample from the triggered beam crossing (according to the hardware)
-  int presamples() const { return hcalPresamples_&0xF; }
+  constexpr int presamples() const { return hcalPresamples_&0xF; }
    /// was ZS MarkAndPass?
-  bool zsMarkAndPass() const { return (hcalPresamples_&0x10); }
+  constexpr bool zsMarkAndPass() const { return (hcalPresamples_&0x10); }
   /// was ZS unsuppressed?
-  bool zsUnsuppressed() const { return (hcalPresamples_&0x20); }
+  constexpr bool zsUnsuppressed() const { return (hcalPresamples_&0x20); }
   /// zs crossing mask (which sums considered)
-  uint32_t zsCrossingMask() const { return (hcalPresamples_&0x3FF000)>>12; }
+  constexpr uint32_t zsCrossingMask() const { return (hcalPresamples_&0x3FF000)>>12; }
  
   /// access a sample
-  const HcalQIESample& operator[](int i) const { return data_[i]; }
+  constexpr HcalQIESample const& operator[](int i) const { return data_[i]; }
   /// access a sample
-  const HcalQIESample& sample(int i) const { return data_[i]; }
+  constexpr HcalQIESample const& sample(int i) const { return data_[i]; }
 
   /// offset of bunch number for this channel relative to nominal set in the unpacker (range is +7->-7.  -1000 indicates the data is invalid/unavailable)
-  int fiberIdleOffset() const;
+  constexpr int fiberIdleOffset() const {
+    int val=(hcalPresamples_&0xF00)>>8;
+    return (val==0)?(-1000):(((val&0x8)==0)?(-(val&0x7)):(val&0x7));
+  }
   
   /// validate appropriate DV and ER bits as well as capid rotation for the specified samples (default is all)
-  bool validate(int firstSample=0, int nSamples=100) const;
+  constexpr bool validate(int firstSample=0, int nSamples=100) const {
+    int capid=-1;
+    bool ok=true;
+    for (int i=0; ok && i<nSamples && i+firstSample<size_; i++) {
+      if (data_[i+firstSample].er() || !data_[i+firstSample].dv()) ok=false;
+      if (i==0) capid=data_[i+firstSample].capid();
+      if (capid!=data_[i+firstSample].capid()) ok=false;
+      capid=(capid+1)%4;
+    }
+    return ok;
+  }
   
-  void setSize(int size);
-  void setPresamples(int ps);
-  void setZSInfo(bool unsuppressed, bool markAndPass, uint32_t crossingMask=0);
-  void setSample(int i, const HcalQIESample& sam) { data_[i]=sam; }
-  void setReadoutIds(const HcalElectronicsId& eid);
-  void setFiberIdleOffset(int offset);
+  constexpr void setSize(int size) {
+    if (size>MAXSAMPLES) size_=MAXSAMPLES;
+    else if (size<=0) size_=0;
+    else size_=size;
+  }
+  constexpr void setPresamples(int ps) {
+    hcalPresamples_|=ps&0xF;
+  }
+  constexpr void setZSInfo(bool unsuppressed, bool markAndPass, 
+                 uint32_t crossingMask=0) {
+    hcalPresamples_&=0x7FC00F0F; // preserve actual presamples and fiber idle offset
+    if (markAndPass) hcalPresamples_|=0x10;
+    if (unsuppressed) hcalPresamples_|=0x20;
+    hcalPresamples_|=(crossingMask&0x3FF)<<12; 
+  }
+  constexpr void setSample(int i, const HcalQIESample& sam) { data_[i]=sam; }
+  constexpr void setReadoutIds(const HcalElectronicsId& eid) {
+    electronicsId_=eid;
+  }
+  constexpr void setFiberIdleOffset(int offset) {
+    hcalPresamples_&=0x7FFFF0FF;
+    if (offset>=7) hcalPresamples_|=0xF00;
+    else if (offset>=0) hcalPresamples_|=(0x800)|(offset<<8);
+    else if (offset>=-7) hcalPresamples_|=((-offset)<<8);
+    else hcalPresamples_|=0x700;
+  }
   
   static const int MAXSAMPLES = 10;
 private:
