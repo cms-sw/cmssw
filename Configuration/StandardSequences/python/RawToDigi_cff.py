8f3fc6296ac819e795129fb5d983c829b29047af
Matti Kortelainen <matti.kortelainen@cern.ch>
2018-06-04 11:26:15 +0200

    Implement a Heterogeneous version of Raw2Cluster and RecHit (cms-patatrack#62)
      - reorganize `SiPixelRawToDigi` as `SiPixelRawToDigiHeterogeneous` using `HeterogeneousEDProducer`
          - output a `HeterogeneousEvent`
          - use `PixelThresholdClusterizer`
          - add `SiPixelDigiHeterogeneousConverter`
          - make cabling and gain transfers asynchronous
      - reorganize `SiPixelRecHits` as `SiPixelRecHitHeterogeneous`
      - move `PixelThresholdClusterizer` (back?) to interface+src in order to use it outside of RecoLocalTracker/SiPixelClusterizer
      - replace __host__ __device__ with constexpr to avoid weird compilation failures
      - split clusters to their own converter

--- Configuration/StandardSequences/python/RawToDigi_cff.py
+++ Configuration/StandardSequences/python/RawToDigi_cff.py
@@ -4,12 +4,14 @@ import FWCore.ParameterSet.Config as cms
 # scenarios. In this case it makes changes for Run 2.
 
 from EventFilter.SiPixelRawToDigi.SiPixelRawToDigi_cfi import *
+from EventFilter.SiPixelRawToDigi.siPixelDigisHeterogeneous_cfi import *
 
 from EventFilter.SiStripRawToDigi.SiStripDigis_cfi import *
 
 from SimCalorimetry.EcalTrigPrimProducers.ecalTriggerPrimitiveDigis_cff import *
 
-from EventFilter.EcalRawToDigi.ecalDigis_cff import *
+import EventFilter.EcalRawToDigi.EcalUnpackerData_cfi
+ecalDigis = EventFilter.EcalRawToDigi.EcalUnpackerData_cfi.ecalEBunpacker.clone()
 
 import EventFilter.ESRawToDigi.esRawToDigi_cfi
 ecalPreshowerDigis = EventFilter.ESRawToDigi.esRawToDigi_cfi.esRawToDigi.clone()
@@ -23,8 +25,8 @@ muonCSCDigis = EventFilter.CSCRawToDigi.cscUnpacker_cfi.muonCSCDigis.clone()
 import EventFilter.DTRawToDigi.dtunpacker_cfi
 muonDTDigis = EventFilter.DTRawToDigi.dtunpacker_cfi.muonDTDigis.clone()
 
-import EventFilter.RPCRawToDigi.RPCRawToDigi_cfi 
-muonRPCDigis = EventFilter.RPCRawToDigi.RPCRawToDigi_cfi.muonRPCDigis.clone()
+import EventFilter.RPCRawToDigi.rpcUnpacker_cfi
+muonRPCDigis = EventFilter.RPCRawToDigi.rpcUnpacker_cfi.rpcunpacker.clone()
 
 import EventFilter.GEMRawToDigi.muonGEMDigis_cfi
 muonGEMDigis = EventFilter.GEMRawToDigi.muonGEMDigis_cfi.muonGEMDigis.clone()
@@ -44,36 +46,44 @@ from L1Trigger.Configuration.L1TRawToDigi_cff import *
 
 from EventFilter.CTPPSRawToDigi.ctppsRawToDigi_cff import *
 
-RawToDigiTask = cms.Task(L1TRawToDigiTask,
-                         siPixelDigis,
-                         siStripDigis,
-                         ecalDigisTask,
-                         ecalPreshowerDigis,
-                         hcalDigis,
-                         muonCSCDigis,
-                         muonDTDigis,
-                         muonRPCDigis,
-                         castorDigis,
-                         scalersRawToDigi,
-                         tcdsDigis,
-                         onlineMetaDataDigis,
+RawToDigi = cms.Sequence(L1TRawToDigi
+                         +siPixelDigis
+                         +siStripDigis
+                         +ecalDigis
+                         +ecalPreshowerDigis
+                         +hcalDigis
+                         +muonCSCDigis
+                         +muonDTDigis
+                         +muonRPCDigis
+                         +castorDigis
+                         +scalersRawToDigi
+                         +tcdsDigis
+                         +onlineMetaDataDigis
                          )
-RawToDigi = cms.Sequence(RawToDigiTask)
+from Configuration.ProcessModifiers.gpu_cff import gpu
+_RawToDigi_gpu = RawToDigi.copy()
+_RawToDigi_gpu.replace(siPixelDigis, siPixelDigisHeterogeneous+siPixelDigis)
+gpu.toReplaceWith(RawToDigi, _RawToDigi_gpu)
 
-RawToDigiTask_noTk = RawToDigiTask.copyAndExclude([siPixelDigis, siStripDigis])
-RawToDigi_noTk = cms.Sequence(RawToDigiTask_noTk)
+RawToDigi_noTk = cms.Sequence(L1TRawToDigi
+                              +ecalDigis
+                              +ecalPreshowerDigis
+                              +hcalDigis
+                              +muonCSCDigis
+                              +muonDTDigis
+                              +muonRPCDigis
+                              +castorDigis
+                              +scalersRawToDigi
+                              +tcdsDigis
+                              +onlineMetaDataDigis
+                              )
 
-RawToDigiTask_pixelOnly = cms.Task(siPixelDigis)
-RawToDigi_pixelOnly = cms.Sequence(RawToDigiTask_pixelOnly)
-
-RawToDigiTask_ecalOnly = cms.Task(ecalDigisTask, ecalPreshowerDigis, scalersRawToDigi)
-RawToDigi_ecalOnly = cms.Sequence(RawToDigiTask_ecalOnly)
-
-RawToDigiTask_hcalOnly = cms.Task(hcalDigis)
-RawToDigi_hcalOnly = cms.Sequence(RawToDigiTask_hcalOnly)
+RawToDigi_pixelOnly = cms.Sequence(siPixelDigisHeterogeneous+siPixelDigis)
 
 scalersRawToDigi.scalersInputTag = 'rawDataCollector'
-siPixelDigis.InputLabel = 'rawDataCollector'
+siPixelDigisHeterogeneous.InputLabel = 'rawDataCollector'
+(~gpu).toModify(siPixelDigis, InputLabel = 'rawDataCollector')
+#false by default anyways ecalDigis.DoRegional = False
 ecalDigis.InputLabel = 'rawDataCollector'
 ecalPreshowerDigis.sourceTag = 'rawDataCollector'
 hcalDigis.InputLabel = 'rawDataCollector'
@@ -83,43 +93,30 @@ muonRPCDigis.InputLabel = 'rawDataCollector'
 castorDigis.InputLabel = 'rawDataCollector'
 
 from Configuration.Eras.Modifier_run3_common_cff import run3_common
-run3_common.toReplaceWith(RawToDigiTask, RawToDigiTask.copyAndExclude([castorDigis]))
+run3_common.toReplaceWith(RawToDigi, RawToDigi.copyAndExclude([castorDigis]))
 
 from Configuration.Eras.Modifier_phase2_tracker_cff import phase2_tracker
 # Remove siPixelDigis until we have phase1 pixel digis
-phase2_tracker.toReplaceWith(RawToDigiTask, RawToDigiTask.copyAndExclude([siPixelDigis])) # FIXME
+phase2_tracker.toReplaceWith(RawToDigi, RawToDigi.copyAndExclude([siPixelDigis])) # FIXME
 
 
 # add CTPPS 2016 raw-to-digi modules
 from Configuration.Eras.Modifier_ctpps_2016_cff import ctpps_2016
 
-_ctpps_2016_RawToDigiTask = RawToDigiTask.copy()
-_ctpps_2016_RawToDigiTask.add(ctppsRawToDigiTask)
-ctpps_2016.toReplaceWith(RawToDigiTask, _ctpps_2016_RawToDigiTask)
+_ctpps_2016_RawToDigi = RawToDigi.copy()
+_ctpps_2016_RawToDigi += ctppsRawToDigi
+ctpps_2016.toReplaceWith(RawToDigi, _ctpps_2016_RawToDigi)
 
-_ctpps_2016_RawToDigiTask_noTk = RawToDigiTask_noTk.copy()
-_ctpps_2016_RawToDigiTask_noTk.add(ctppsRawToDigiTask)
-ctpps_2016.toReplaceWith(RawToDigiTask_noTk, _ctpps_2016_RawToDigiTask_noTk)
+_ctpps_2016_RawToDigi_noTk = RawToDigi_noTk.copy()
+_ctpps_2016_RawToDigi_noTk += ctppsRawToDigi
+ctpps_2016.toReplaceWith(RawToDigi_noTk, _ctpps_2016_RawToDigi_noTk)
 
 # GEM settings
-_gem_RawToDigiTask = RawToDigiTask.copy()
-_gem_RawToDigiTask.add(muonGEMDigis)
+_gem_RawToDigi = RawToDigi.copy()
+_gem_RawToDigi.insert(-1,muonGEMDigis)
 
 from Configuration.Eras.Modifier_run2_GEM_2017_cff import run2_GEM_2017
-run2_GEM_2017.toReplaceWith(RawToDigiTask, _gem_RawToDigiTask)
+run2_GEM_2017.toReplaceWith(RawToDigi, _gem_RawToDigi)
 
 from Configuration.Eras.Modifier_run3_GEM_cff import run3_GEM
-run3_GEM.toReplaceWith(RawToDigiTask, _gem_RawToDigiTask)
-
-from EventFilter.HGCalRawToDigi.HGCalRawToDigi_cfi import *
-_hgcal_RawToDigiTask = RawToDigiTask.copy()
-_hgcal_RawToDigiTask.add(hgcalDigis)
-from Configuration.Eras.Modifier_phase2_hgcal_cff import phase2_hgcal
-phase2_hgcal.toReplaceWith(RawToDigiTask,_hgcal_RawToDigiTask)
-
-_hfnose_RawToDigiTask = RawToDigiTask.copy()
-_hfnose_RawToDigiTask.add(hfnoseDigis)
-
-from Configuration.Eras.Modifier_phase2_hfnose_cff import phase2_hfnose
-phase2_hfnose.toReplaceWith(RawToDigiTask,_hfnose_RawToDigiTask)
-
+run3_GEM.toReplaceWith(RawToDigi, _gem_RawToDigi)
