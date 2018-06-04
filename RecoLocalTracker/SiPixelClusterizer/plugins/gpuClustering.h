999ec32d4105c7d655b9e0376b88c54d9afc550a
Vincenzo Innocente <vincenzo.innocente@cern.ch>
2018-06-04 14:50:37 +0200

    Faster clustering, now does not requires to know number of modules (cms-patatrack#68)

--- RecoLocalTracker/SiPixelClusterizer/plugins/gpuClustering.h
+++ RecoLocalTracker/SiPixelClusterizer/plugins/gpuClustering.h
@@ -1,165 +1,169 @@
 #ifndef RecoLocalTracker_SiPixelClusterizer_plugins_gpuClustering_h
 #define RecoLocalTracker_SiPixelClusterizer_plugins_gpuClustering_h
 
-#include "gpuClusteringConstants.h"
-
 #include <cassert>
 #include <cstdint>
 #include <cstdio>
 
+#include "gpuClusteringConstants.h"
+
 namespace gpuClustering {
+
   __global__ void countModules(uint16_t const * id,
-			       uint32_t * moduleStart,
-			       int32_t * clus,
-			       int numElements){
-    
+                               uint32_t * moduleStart,
+                               int32_t * clus,
+                               int numElements)
+  {
     int i = blockDim.x * blockIdx.x + threadIdx.x;
-    if (i >= numElements) return;
-    clus[i]=i;
-    if (InvId==id[i]) return;
-    auto j=i-1;
-    while(j>=0 && id[j]==InvId) --j;
-    if(j<0 || id[j]!=id[i]) {
+    if (i >= numElements)
+      return;
+    clus[i] = i;
+    if (InvId == id[i])
+      return;
+    auto j = i - 1;
+    while (j >= 0 and id[j] == InvId)
+      --j;
+    if (j < 0 or id[j] != id[i]) {
       // boundary...
-      auto loc = atomicInc(moduleStart,MaxNumModules);
-      moduleStart[loc+1]=i;
+      auto loc = atomicInc(moduleStart, MaxNumModules);
+      moduleStart[loc + 1] = i;
     }
   }
-  
-  
+
   __global__ void findClus(uint16_t const * id,
-			   uint16_t const * x,
-			   uint16_t const * y,
-			   uint16_t const * adc,
-			   uint32_t const * moduleStart,
-			   uint32_t * clusInModule, uint32_t * moduleId,
-			   int32_t * clus, uint32_t * debug, 
-			   int numElements){
-    
+                           uint16_t const * x,
+                           uint16_t const * y,
+                           uint16_t const * adc,
+                           uint32_t const * moduleStart,
+                           uint32_t * clusInModule, uint32_t * moduleId,
+                           int32_t * clus, uint32_t * debug,
+                           int numElements)
+  {
     __shared__ bool go;
     __shared__ int nclus;
+    __shared__ int msize;
 
-    __shared__  int msize;    
+    if (blockIdx.x >= moduleStart[0])
+      return;
 
-    auto first = moduleStart[1 + blockIdx.x];  
-    
+    auto first = moduleStart[1 + blockIdx.x];
     auto me = id[first];
 
-    assert(me<MaxNumModules);    
-
-    // auto debugme = (me==31 && threadIdx.x==127);
+    assert(me < MaxNumModules);
 
 #ifdef GPU_DEBUG
-    if (me%100==1)
-      if (threadIdx.x==0) printf("start clusterizer for module %d in block %d\n",me,blockIdx.x);
+    if (me%100 == 1)
+      if (threadIdx.x == 0)
+        printf("start clusterizer for module %d in block %d\n", me, blockIdx.x);
 #endif
 
-    first+=threadIdx.x;
-   
-    if (first>=numElements) return;
-    
-    go=true;
-    nclus=0;
+    first += threadIdx.x;
+    if (first>= numElements)
+      return;
 
-    msize=numElements;
+    go = true;
+    nclus = 0;
+
+    msize = numElements;
     __syncthreads();
 
-    for (int i=first; i<numElements; i+=blockDim.x) {
-      if (id[i]==InvId) continue;  // not valid
-      if (id[i]!=me) { atomicMin(&msize,i); break;}  // end of module
-     }
+    for (int i = first; i < numElements; i += blockDim.x) {
+      if (id[i] == InvId)               // not valid
+        continue;
+      if (id[i] != me) {                // end of module
+        atomicMin(&msize, i);
+        break;
+      }
+    }
     __syncthreads();
 
-    assert(msize<=numElements);    
-    if (first>=msize) return;
+    assert(msize<= numElements);
+    if (first>= msize)
+      return;
 
     int jmax[10];
-    auto niter = (msize-first)/blockDim.x;
-    assert(niter<10);
-    for (int i=0; i<niter+1; ++i) jmax[i]=msize;    
+    auto niter = (msize - first) / blockDim.x;
+    assert(niter < 10);
+    for (int k = 0; k < niter + 1; ++k)
+      jmax[k] = msize;
 
     while (go) {
       __syncthreads();
-      go=false;
+      go = false;
+
       __syncthreads();
-
-      int k=-1;
-      for (int i=first; i<msize; i+=blockDim.x) {
+     int k = -1;
+      for (int i = first; i < msize; i += blockDim.x) {
         ++k;
-	if (id[i]==InvId) continue;  // not valid
-	assert(id[i]==me); //  break;  // end of module
-	++debug[i];
+        if (id[i] == InvId)             // not valid
+          continue;
+        assert(id[i] == me); // break;  // end of module
+        ++debug[i];
+        auto js = i + 1;
         auto jm = jmax[k];
-	for (int j=i+1; j<jm; ++j) {
- 	  if (id[j]==InvId) continue;  // not valid
-          if (std::abs(int(x[j])-int(x[i]))>1) continue;
-	  if (std::abs(int(y[j])-int(y[i]))>1) continue;
-	  auto old = atomicMin(&clus[j],clus[i]);
-	  if (old!=clus[i]) go=true;
-	  atomicMin(&clus[i],old);
-          jmax[k]=j+1;
-	}
+        jmax[k] = i + 1;
+        for (int j = js; j < jm; ++j) {
+          if (id[j] == InvId)           // not valid
+            continue;
+          if (std::abs(int(x[j]) - int(x[i])) > 1 |
+              std::abs(int(y[j]) - int(y[i])) > 1)
+            continue;
+          auto old = atomicMin(&clus[j], clus[i]);
+          if (old != clus[i]) go = true;
+          atomicMin(&clus[i], old);
+          jmax[k] = j + 1;
+        }
       }
-      assert (k<=niter);
-    __syncthreads();
+      assert (k<= niter);
+      __syncthreads();
     }
-    
-    /*
-    // fast count (nice but not much useful)
-    auto laneId = threadIdx.x & 0x1f;
-    
-    for (int i=first; i<numElements; i+=blockDim.x) {
-    if (id[i]==InvId) continue;  // not valid
-    if (id[i]!=me) break;  // end of module
-    auto value = clus[i]^i;
-    auto mask = __ballot_sync(0xffffffff,value==0);
-    if (laneId==0) atomicAdd(&nclus,__popc(mask));
-    }
-    
-    __syncthreads();
-    if (me<30)
-    if (laneId==0) printf("%d clusters in module %d\n",nclus,me);
-    */
 
-    nclus=0;
+    nclus = 0;
     __syncthreads();
-    for (int i=first; i<numElements; i+=blockDim.x) {
-      if (id[i]==InvId) continue;  // not valid
-      if (id[i]!=me) break;  // end of module
-      if (clus[i]==i) {
-	auto old = atomicAdd(&nclus,1);
-	clus[i]=-(old+1);
+    for (int i = first; i < numElements; i += blockDim.x) {
+      if (id[i] == InvId)               // not valid
+        continue;
+      if (id[i] != me)                  // end of module
+        break;
+      if (clus[i] == i) {
+        auto old = atomicAdd(&nclus, 1);
+        clus[i] = -(old + 1);
       }
     }
-    
+
     __syncthreads();
-    
-    for (int i=first; i<numElements; i+=blockDim.x) {
-      if (id[i]==InvId) continue;  // not valid
-      if (id[i]!=me) break;  // end of module
-      if (clus[i]>=0) clus[i]=clus[clus[i]];
+    for (int i = first; i < numElements; i += blockDim.x) {
+      if (id[i] == InvId)               // not valid
+        continue;
+      if (id[i] != me)                  // end of module
+        break;
+      if (clus[i]>= 0) clus[i] = clus[clus[i]];
     }
-    
+
     __syncthreads();
-    for (int i=first; i<numElements; i+=blockDim.x) {
-      if (id[i]==InvId) {clus[i]=-9999; continue; } // not valid
-      if (id[i]!=me) break;  // end of module
-      clus[i] = -clus[i] -1;
+    for (int i = first; i < numElements; i += blockDim.x) {
+      if (id[i] == InvId) {             // not valid
+        clus[i] = -9999;
+        continue;
+      }
+      if (id[i] != me)                  // end of module
+        break;
+      clus[i] = - clus[i] - 1;
     }
-    
-  
+
     __syncthreads();
-    if (threadIdx.x==0) {
-      clusInModule[me]=nclus;
-      moduleId[blockIdx.x]=me;
+    if (threadIdx.x == 0) {
+      clusInModule[me] = nclus;
+      moduleId[blockIdx.x] = me;
     }
-    
-#ifdef GPU_DEBUG    
-    if (me%100==1)
-      if (threadIdx.x==0) printf("%d clusters in module %d\n",nclus,me);
-#endif    
+
+#ifdef GPU_DEBUG
+    if (me % 100 == 1)
+      if (threadIdx.x == 0)
+        printf("%d clusters in module %d\n", nclus, me);
+#endif
   }
-  
-} //namespace gpuClustering
+
+} // namespace gpuClustering
 
 #endif // RecoLocalTracker_SiPixelClusterizer_plugins_gpuClustering_h
