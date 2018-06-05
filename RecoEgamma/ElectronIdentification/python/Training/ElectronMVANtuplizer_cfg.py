c0194396971a505cab9563a465f26cd67a770353
Jonas Rembser <jonas.rembser@cern.ch>
2018-06-06 00:09:35 +0200

    Changed egammaObjectModificationsInMiniAOD_cff.py to comply with new config files

--- RecoEgamma/ElectronIdentification/python/Training/ElectronMVANtuplizer_cfg.py
+++ RecoEgamma/ElectronIdentification/python/Training/ElectronMVANtuplizer_cfg.py
@@ -19,8 +19,7 @@ process.maxEvents = cms.untracked.PSet( input = cms.untracked.int32(-1) )
 
 process.source = cms.Source("PoolSource",
     fileNames = cms.untracked.vstring(
-        # /DYJetsToLL_M-50_TuneCP5_13TeV-madgraphMLM-pythia8/RunIIFall17MiniAOD-RECOSIMstep_94X_mc2017_realistic_v10-v1/MINIAODSIM
-        '/store/mc/RunIIFall17MiniAOD/DYJetsToLL_M-50_TuneCP5_13TeV-madgraphMLM-pythia8/MINIAODSIM/RECOSIMstep_94X_mc2017_realistic_v10-v1/00000/0293A280-B5F3-E711-8303-3417EBE33927.root',
+         '/store/mc/RunIIFall17MiniAOD/DYJetsToLL_M-50_TuneCP5_13TeV-madgraphMLM-pythia8/MINIAODSIM/RECOSIMstep_94X_mc2017_realistic_v10-v1/00000/0293A280-B5F3-E711-8303-3417EBE33927.root'
     )
 )
 
@@ -124,5 +123,4 @@ process.TFileService = cms.Service("TFileService",
                                    fileName = cms.string( outputFile )
                                    )
 
-process.electronMVAVariableHelper = electronMVAVariableHelper
 process.p = cms.Path(process.egmGsfElectronIDSequence * process.ntuplizer)
