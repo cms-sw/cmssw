06f849ec9f978ea3226a7047fc523c1105c187eb
Sunanda <sunanda.banerjee@cern.ch>
2018-06-06 09:28:19 +0200

    Adopt validation package to new geometry

--- Validation/HGCalValidation/plugins/HGCalDigiValidation.cc
+++ Validation/HGCalValidation/plugins/HGCalDigiValidation.cc
@@ -134,20 +134,25 @@ void HGCalDigiValidation::analyze(const edm::Event& iEvent,
   
   const HGCalGeometry* geom0(nullptr);
   const CaloGeometry*  geom1(nullptr);
+  int geomType(0);
   if (nameDetector_ == "HCal") {
     edm::ESHandle<CaloGeometry> geom;
     iSetup.get<CaloGeometryRecord>().get(geom);
     if (!geom.isValid()) 
-      edm::LogWarning("HGCalValidation") << "Cannot get valid HGCalGeometry "
-					 << "Object for " << nameDetector_;
+      edm::LogVerbatim("HGCalValidation") << "HGCalDigiValidation: Cannot get valid "
+					  << "HGCalGeometry Object for " << nameDetector_;
     geom1 = geom.product();
   } else {
     edm::ESHandle<HGCalGeometry> geom;
     iSetup.get<IdealGeometryRecord>().get(nameDetector_, geom);
     if (!geom.isValid()) 
-      edm::LogWarning("HGCalValidation") << "Cannot get valid HGCalGeometry "
-					 << "Object for " << nameDetector_;
+      edm::LogVerbatim("HGCalValidation") << "HGCalDigiValidation: Cannot get valid "
+					  << "HGCalGeometry Object for " << nameDetector_;
     geom0 = geom.product();
+    HGCalGeometryMode::GeometryMode mode = geom0->topology().geomMode();
+    if ((mode == HGCalGeometryMode::Hexagon8) ||
+	(mode == HGCalGeometryMode::Hexagon8Full)) geomType = 1;
+    else if (mode == HGCalGeometryMode::Trapezoid) geomType = 2;
   }
 
   unsigned int ntot(0), nused(0);
@@ -163,8 +168,9 @@ void HGCalDigiValidation::analyze(const edm::Event& iEvent,
       
       for (const auto & it: *(theHGCEEDigiContainers.product())) {
 	ntot++; nused++;
-	HGCalDetId detId     = it.id();
-	int        layer     = detId.layer();
+	DetId      detId     = it.id();
+	int        layer     = ((geomType == 0) ? HGCalDetId(detId).layer() :
+				HGCSiliconDetId(detId).layer());
 	const HGCSample&  hgcSample = it.sample(SampleIndx_);
 	uint16_t   gain      = hgcSample.toa();
 	uint16_t   adc       = hgcSample.data();
@@ -173,8 +179,8 @@ void HGCalDigiValidation::analyze(const edm::Event& iEvent,
       }
       fillDigiInfo();
     } else {
-      edm::LogWarning("HGCalValidation") << "DigiCollection handle does not "
-					 << "exist for HGCEE!!!";
+      edm::LogVerbatim("HGCalValidation") << "DigiCollection handle does not "
+					  << "exist for HGCEE!!!";
     }
   } else if ((nameDetector_ == "HGCalHESiliconSensitive") || 
 	     (nameDetector_ == "HGCalHEScintillatorSensitive")) {
@@ -189,8 +195,10 @@ void HGCalDigiValidation::analyze(const edm::Event& iEvent,
       
       for (const auto & it: *(theHGCHEDigiContainers.product())) {
 	ntot++; nused++;
-	HGCalDetId detId     = it.id();
-	int        layer     = detId.layer();
+	DetId      detId     = it.id();
+	int        layer     = ((geomType == 0) ? HGCalDetId(detId).layer() :
+				((geomType == 1) ? HGCSiliconDetId(detId).layer() :
+				 HGCScintillatorDetId(detId).layer()));
 	const HGCSample&  hgcSample = it.sample(SampleIndx_);
 	uint16_t   gain      = hgcSample.toa();
 	uint16_t   adc       = hgcSample.data();
@@ -199,8 +207,8 @@ void HGCalDigiValidation::analyze(const edm::Event& iEvent,
       }
       fillDigiInfo();
     } else {
-      edm::LogWarning("HGCalValidation") << "DigiCollection handle does not "
-					 << "exist for HGCFH!!!";
+      edm::LogVerbatim("HGCalValidation") << "DigiCollection handle does not "
+					  << "exist for HGCFH!!!";
     }
   } else if ((nameDetector_ == "HCal") && (!ifHCAL_)) {
     //HGCalBH
@@ -276,7 +284,9 @@ template<class T1, class T2>
 void HGCalDigiValidation::digiValidation(const T1& detId, const T2* geom, 
 					 int layer, uint16_t adc, double charge) {
   
-  if (verbosity_>1) edm::LogVerbatim("HGCalValidation") << detId;
+  if (verbosity_>1) edm::LogVerbatim("HGCalValidation") << std::hex 
+							<< detId.rawId()
+							<< std::dec;
   DetId id1 = DetId(detId.rawId());
   const GlobalPoint& global1 = geom->getPosition(id1);
   
