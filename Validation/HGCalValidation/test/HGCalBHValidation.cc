5124512a2f9384a68852f55da32d29cea164f2a8
Sunanda <sunanda.banerjee@cern.ch>
2018-06-08 14:36:59 +0200

    Update the validation codes in the test area

--- Validation/HGCalValidation/test/HGCalBHValidation.cc
+++ Validation/HGCalValidation/test/HGCalBHValidation.cc
@@ -26,6 +26,7 @@
 #include "SimDataFormats/CaloHit/interface/PCaloHit.h"
 #include "SimDataFormats/CaloHit/interface/PCaloHitContainer.h"
 #include "DataFormats/HcalDetId/interface/HcalTestNumbering.h"
+#include "DataFormats/ForwardDetId/interface/HGCScintillatorDetId.h"
 
 // Root objects
 #include "TROOT.h"
@@ -47,18 +48,19 @@ protected:
   virtual void beginRun(edm::Run const&, edm::EventSetup const&) override;
   virtual void endRun(edm::Run const&, edm::EventSetup const&) override {}
   virtual void analyze(edm::Event const&, edm::EventSetup const&) override;
-  void analyzeDigi(HcalDetId const& , double const& , unsigned int &);
+  template<class T>
+  void analyzeDigi(const T&, double const&, bool const&, int const& , unsigned int &);
 private:
 
   edm::Service<TFileService>                  fs_;
-  std::string                                 g4Label_, hcalHits_;
-  edm::InputTag                               hcalDigis_;
-  int                                         iSample_;
-  double                                      threshold_;
+  const std::string                           g4Label_, hcalHits_;
+  const edm::InputTag                         hcalDigis_;
+  const int                                   iSample_, geomType_;
+  const double                                threshold_;
+  const bool                                  ifHCAL_;
   edm::EDGetTokenT<edm::PCaloHitContainer>    tok_hits_;
   edm::EDGetToken                             tok_hbhe_;
   int                                         etaMax_;
-  bool                                        ifHCAL_;
 
   TH1D                                       *hsimE1_, *hsimE2_, *hsimTm_;
   TH1D                                       *hsimLn_, *hdigEn_, *hdigLn_;
@@ -67,22 +69,26 @@ private:
 };
 
 
-HGCalBHValidation::HGCalBHValidation(const edm::ParameterSet& ps) {
+HGCalBHValidation::HGCalBHValidation(const edm::ParameterSet& ps) :
+  g4Label_(ps.getUntrackedParameter<std::string>("ModuleLabel","g4SimHits")),
+  hcalHits_((ps.getUntrackedParameter<std::string>("HitCollection","HcalHits"))),
+  hcalDigis_(ps.getUntrackedParameter<edm::InputTag>("DigiCollection")),
+  iSample_(ps.getUntrackedParameter<int>("Sample",5)),
+  geomType_(ps.getUntrackedParameter<int>("GeometryType",0)),
+  threshold_(ps.getUntrackedParameter<double>("Threshold",12.0)),
+  ifHCAL_(ps.getUntrackedParameter<bool>("ifHCAL",false)), etaMax_(0) {
 
   usesResource(TFileService::kSharedResource);
 
-  g4Label_  = ps.getUntrackedParameter<std::string>("ModuleLabel","g4SimHits");
-  hcalHits_ = ps.getUntrackedParameter<std::string>("HitCollection","HcalHits");
-  hcalDigis_= ps.getUntrackedParameter<edm::InputTag>("DigiCollection");
-  iSample_  = ps.getUntrackedParameter<int>("Sample",5);
-  threshold_= ps.getUntrackedParameter<double>("Threshold",12.0);
-  ifHCAL_   = ps.getUntrackedParameter<bool>("ifHCAL",false);
-
   tok_hits_ = consumes<edm::PCaloHitContainer>(edm::InputTag(g4Label_,hcalHits_));
-  if (ifHCAL_) 
-    tok_hbhe_ = consumes<QIE11DigiCollection>(hcalDigis_);
-  else
+  if (geomType_ == 0) {
+    if (ifHCAL_) 
+      tok_hbhe_ = consumes<QIE11DigiCollection>(hcalDigis_);
+    else
+      tok_hbhe_ = consumes<HGCBHDigiCollection>(hcalDigis_);
+  } else {
     tok_hbhe_ = consumes<HGCBHDigiCollection>(hcalDigis_);
+  }
   edm::LogVerbatim("HGCalValidation") << "HGCalBHValidation::Input for SimHit:"
 				      << edm::InputTag(g4Label_,hcalHits_) 
 				      << "  Digits:" << hcalDigis_ 
@@ -96,19 +102,21 @@ void HGCalBHValidation::fillDescriptions(edm::ConfigurationDescriptions& descrip
   desc.addUntracked<std::string>("HitCollection","HcalHits");
   desc.addUntracked<edm::InputTag>("DigiCollection",edm::InputTag("mix","HGCDigisHEback"));
   desc.addUntracked<int>("Sample",5);
+  desc.addUntracked<int>("GeometryType",0);
   desc.addUntracked<double>("Threshold",15.0);
   desc.addUntracked<bool>("ifHCAL",false);
   descriptions.add("hgcalBHValidation",desc);
 }
 
 void HGCalBHValidation::beginRun(edm::Run const&, edm::EventSetup const& es) {
-  
-  std::string                        label;
-  edm::ESHandle<HcalParameters>      parHandle;
-  es.get<HcalParametersRcd>().get(label, parHandle);
-  const HcalParameters* hpar = &(*parHandle);
-  const std::vector<int> etaM = hpar->etaMax;
-  etaMax_ = etaM[1];
+  if (geomType_ == 0) {
+    std::string                        label;
+    edm::ESHandle<HcalParameters>      parHandle;
+    es.get<HcalParametersRcd>().get(label, parHandle);
+    const HcalParameters* hpar = &(*parHandle);
+    const std::vector<int> etaM = hpar->etaMax;
+    etaMax_ = etaM[1];
+  }
   edm::LogVerbatim("HGCalValidation") << "HGCalBHValidation::Maximum Number of"
 				      << " eta sectors:" << etaMax_
 				      << "\nHitsValidationHcal::Booking the Histograms";
@@ -152,12 +160,23 @@ void HGCalBHValidation::analyze(const edm::Event& e, const edm::EventSetup& ) {
       double time      = it->time();
       unsigned int id  = it->id();
       int subdet, z, depth, eta, phi, lay;
-      HcalTestNumbering::unpackHcalIndex(id, subdet, z, depth, eta, phi, lay);
-      if (z==0) eta = -eta;
-      if ((subdet == static_cast<int>(HcalEndcap)) ||
-	  (subdet == static_cast<int>(HcalBarrel))) 
-	hsi2Oc_->Fill((eta+0.1),(phi-0.1),energy);
-      if (subdet == static_cast<int>(HcalEndcap)) {
+      bool hbhe, bh;
+      if (geomType_ == 0) {
+	HcalTestNumbering::unpackHcalIndex(id, subdet, z, depth, eta, phi, lay);
+	if (z==0) eta = -eta;
+	hbhe = ((subdet == static_cast<int>(HcalEndcap)) ||
+		(subdet == static_cast<int>(HcalBarrel)));
+	bh   = (subdet == static_cast<int>(HcalEndcap));
+      } else {
+	hbhe = bh = (DetId(id).det() == DetId::HGCalHSc);
+	if (bh) {
+	  eta = HGCScintillatorDetId(id).ieta();
+	  phi = HGCScintillatorDetId(id).iphi();
+	  lay = HGCScintillatorDetId(id).layer();
+	}
+      }
+      if (hbhe) hsi2Oc_->Fill((eta+0.1),(phi-0.1),energy);
+      if (bh) {
 	hsimE1_->Fill(energy);
 	hsimTm_->Fill(time,energy);
 	hsimOc_->Fill((eta+0.1),(phi-0.1),energy);
@@ -185,7 +204,7 @@ void HGCalBHValidation::analyze(const edm::Event& e, const edm::EventSetup& ) {
 
   //Digits
   unsigned int kount(0);
-  if (ifHCAL_) {
+  if ((geomType_ == 0) && ifHCAL_) {
     edm::Handle<QIE11DigiCollection> hbhecoll;
     e.getByToken(tok_hbhe_, hbhecoll);
     edm::LogVerbatim("HGCalValidation") << "HGCalBHValidation.: "
@@ -198,8 +217,14 @@ void HGCalBHValidation::analyze(const edm::Event& e, const edm::EventSetup& ) {
 	   it != hbhecoll->end(); ++it) {
 	QIE11DataFrame df(*it);
 	HcalDetId cell(df.id());
-	double energy = df[iSample_].adc();
-	analyzeDigi(cell, energy, kount);
+	bool hbhe = ((cell.subdetId() == static_cast<int>(HcalEndcap)) ||
+		     (cell.subdetId() == static_cast<int>(HcalBarrel)));
+	if (hbhe) {
+	  bool   bh     = (cell.subdetId() == static_cast<int>(HcalEndcap));
+	  int    depth  = cell.depth();
+	  double energy = df[iSample_].adc();
+	  analyzeDigi(cell, energy, bh, depth, kount);
+	}
       }
     }
   } else {
@@ -214,23 +239,38 @@ void HGCalBHValidation::analyze(const edm::Event& e, const edm::EventSetup& ) {
       for (HGCBHDigiCollection::const_iterator it=hbhecoll->begin(); 
 	   it != hbhecoll->end(); ++it) {
 	HGCBHDataFrame df(*it);
-	HcalDetId cell(df.id());
 	double energy = df[iSample_].data();
-	analyzeDigi(cell, energy, kount);
+	if (geomType_ == 0) {
+	  HcalDetId cell(df.id());
+	  bool hbhe = ((cell.subdetId() == static_cast<int>(HcalEndcap)) ||
+		       (cell.subdetId() == static_cast<int>(HcalBarrel)));
+	  if (hbhe) {
+	    bool   bh     = (cell.subdetId() == static_cast<int>(HcalEndcap));
+	    int    depth  = cell.depth();
+	    analyzeDigi(cell, energy, bh, depth, kount);
+	  }
+	} else {
+	  bool bh = (DetId(df.id()).det() == DetId::HGCalHSc);
+	  if (bh) {
+	    HGCScintillatorDetId cell(df.id());
+	    int    depth  = cell.layer();
+	    analyzeDigi(cell, energy, bh, depth, kount);
+	  }
+	}
       }
     }
   }
 }
 
-void HGCalBHValidation::analyzeDigi(HcalDetId const& cell,double const& energy,
+template<class T>
+void HGCalBHValidation::analyzeDigi(const T& cell,double const& energy,
+				    bool const& bh, int const& depth, 
 				    unsigned int &kount) {
   if (energy > threshold_) {
     int    eta    = cell.ieta();
     int    phi    = cell.iphi();
-    int    depth  = cell.depth();
-    if ((cell.subdet() == HcalEndcap) || (cell.subdet() == HcalBarrel)) 
-      hdi2Oc_->Fill((eta+0.1),(phi-0.1));
-    if (cell.subdet() == HcalEndcap) {
+    hdi2Oc_->Fill((eta+0.1),(phi-0.1));
+    if (bh) {
       hdigEn_->Fill(energy);
       hdigOc_->Fill((eta+0.1),(phi-0.1));
       hdigLn_->Fill(depth);
