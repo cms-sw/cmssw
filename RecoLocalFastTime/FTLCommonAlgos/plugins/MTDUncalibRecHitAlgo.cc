c49289d814c84aa6a012cf27e601e6045c49557f
Massimo Casarsa <massimo.casarsa@ts.infn.it>
2018-06-12 19:30:50 +0200

    Added parameters to fix the ETL reconstruction

--- RecoLocalFastTime/FTLCommonAlgos/plugins/MTDUncalibRecHitAlgo.cc
+++ RecoLocalFastTime/FTLCommonAlgos/plugins/MTDUncalibRecHitAlgo.cc
@@ -7,13 +7,17 @@ class MTDUncalibRecHitAlgo : public MTDUncalibratedRecHitAlgoBase {
   MTDUncalibRecHitAlgo( const edm::ParameterSet& conf,
                               edm::ConsumesCollector& sumes ) : 
   MTDUncalibratedRecHitAlgoBase( conf, sumes ) { 
-    uint32_t nBits     = conf.getParameter<uint32_t>("adcNbits");
-    double saturation  = conf.getParameter<double>("adcSaturation");
-    adcLSB_            = saturation/(1<<nBits);    
-    
-    toaLSBToNS_        = conf.getParameter<double>("toaLSB_ns");
+    uint32_t BTL_nBits     = conf.getParameter<uint32_t>("BTLadcNbits");
+    double BTL_saturation  = conf.getParameter<double>("BTLadcSaturation");
+    BTL_adcLSB_            = BTL_saturation/(1<<BTL_nBits);
+    BTL_toaLSBToNS_        = conf.getParameter<double>("BTLtoaLSB_ns");
+    BTL_timeError_         = conf.getParameter<double>("BTLtimeResolutionInNs");
 
-    timeError_ = conf.getParameter<double>("timeResolutionInNs");
+    uint32_t ETL_nBits     = conf.getParameter<uint32_t>("ETLadcNbits");
+    double ETL_saturation  = conf.getParameter<double>("ETLadcSaturation");
+    ETL_adcLSB_            = ETL_saturation/(1<<ETL_nBits);
+    ETL_toaLSBToNS_        = conf.getParameter<double>("ETLtoaLSB_ns");
+    ETL_timeError_         = conf.getParameter<double>("ETLtimeResolutionInNs");
   }
 
   /// Destructor
@@ -28,7 +32,8 @@ class MTDUncalibRecHitAlgo : public MTDUncalibratedRecHitAlgoBase {
   FTLUncalibratedRecHit makeRecHit(const ETLDataFrame& dataFrame ) const final;
 
  private:  
-  double adcLSB_, toaLSBToNS_, timeError_;
+  double BTL_adcLSB_, BTL_toaLSBToNS_, BTL_timeError_;
+  double ETL_adcLSB_, ETL_toaLSBToNS_, ETL_timeError_;
 };
 
 FTLUncalibratedRecHit 
@@ -36,35 +41,35 @@ MTDUncalibRecHitAlgo::makeRecHit(const BTLDataFrame& dataFrame ) const {
   constexpr int iSample=2; //only in-time sample
   const auto& sample = dataFrame.sample(iSample);
   
-  double amplitude = double(sample.data()) * adcLSB_;
-  double time    = double(sample.toa()) * toaLSBToNS_;
+  double amplitude = double(sample.data()) * BTL_adcLSB_;
+  double time      = double(sample.toa()) * BTL_toaLSBToNS_;
 
   unsigned char flag = 0;
   
   LogDebug("MTDUncalibRecHit") << "ADC+: set the charge to: " << amplitude << ' ' << sample.data() 
-                                     << ' ' << adcLSB_ << ' ' << std::endl;    
+                                     << ' ' << BTL_adcLSB_ << ' ' << std::endl;
   LogDebug("MTDUncalibRecHit") << "ADC+: set the time to: " << time << ' ' << sample.toa() 
-                                     << ' ' << toaLSBToNS_ << ' ' << std::endl;    
+                                     << ' ' << BTL_toaLSBToNS_ << ' ' << std::endl;
   LogDebug("MTDUncalibRecHit") << "Final uncalibrated amplitude : " << amplitude << std::endl;
   
-  return FTLUncalibratedRecHit( dataFrame.id(), amplitude, time, timeError_, flag);
+  return FTLUncalibratedRecHit( dataFrame.id(), amplitude, time, BTL_timeError_, flag);
 }
 FTLUncalibratedRecHit 
 MTDUncalibRecHitAlgo::makeRecHit(const ETLDataFrame& dataFrame ) const { 
   constexpr int iSample=2; //only in-time sample
   const auto& sample = dataFrame.sample(iSample);
   
-  double amplitude = double(sample.data()) * adcLSB_;
-  double time    = double(sample.toa()) * toaLSBToNS_;
+  double amplitude = double(sample.data()) * ETL_adcLSB_;
+  double time      = double(sample.toa()) * ETL_toaLSBToNS_;
   unsigned char flag = 0;
-  
+
   LogDebug("MTDUncalibRecHit") << "ADC+: set the charge to: " << amplitude << ' ' << sample.data() 
-                                     << ' ' << adcLSB_ << ' ' << std::endl;    
+                                     << ' ' << ETL_adcLSB_ << ' ' << std::endl;
   LogDebug("MTDUncalibRecHit") << "ADC+: set the time to: " << time << ' ' << sample.toa() 
-                                     << ' ' << toaLSBToNS_ << ' ' << std::endl;    
+                                     << ' ' << ETL_toaLSBToNS_ << ' ' << std::endl;
   LogDebug("MTDUncalibRecHit") << "Final uncalibrated amplitude : " << amplitude << std::endl;
   
-  return FTLUncalibratedRecHit( dataFrame.id(), amplitude, time, timeError_, flag);
+  return FTLUncalibratedRecHit( dataFrame.id(), amplitude, time, ETL_timeError_, flag);
 }
 
 #include "FWCore/Framework/interface/MakerMacros.h"
--- RecoLocalFastTime/FTLCommonAlgos/python/mtdRecHitAlgo_cff.py
+++ RecoLocalFastTime/FTLCommonAlgos/python/mtdRecHitAlgo_cff.py
@@ -3,6 +3,8 @@ import FWCore.ParameterSet.Config as cms
 # MTD rechit producer from uncalibrated rechits
 mtdRecHitAlgo = cms.PSet(
     algoName = cms.string("MTDRecHitAlgo"),
-    thresholdToKeep = cms.double(1.),
-    calibrationConstant = cms.double(0.026041667) # MeV/pC
+    BTLthresholdToKeep = cms.double(1.),
+    BTLcalibrationConstant = cms.double(0.026041667), # MeV/pC
+    ETLthresholdToKeep = cms.double(0.5),
+    ETLcalibrationConstant = cms.double(1.)
 )
