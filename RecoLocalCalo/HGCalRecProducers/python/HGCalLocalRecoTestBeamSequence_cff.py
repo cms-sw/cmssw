5d40412e0569858abc0810f4ba0b33615e7348a5
Felice <felice.pantaleo@cern.ch>
2018-06-13 15:38:13 +0200

    rename to be explicitly used for test beams

---
+++ RecoLocalCalo/HGCalRecProducers/python/HGCalLocalRecoTestBeamSequence_cff.py
@@ -0,0 +1,10 @@
+import FWCore.ParameterSet.Config as cms
+
+#HGCAL reconstruction
+from RecoLocalCalo.HGCalRecProducers.HGCalUncalibRecHit_cfi import *
+from RecoLocalCalo.HGCalRecProducers.HGCalRecHit_cfi import *
+from RecoLocalCalo.HGCalRecProducers.hgcalLayerClusters_cfi import *
+from RecoLocalCalo.HGCalRecProducers.hgcalMultiClusters_cfi import *
+
+
+HGCalLocalRecoTestBeamSequence = cms.Sequence(HGCalUncalibRecHit*HGCalRecHit*hgcalLayerClusters*hgcalMultiClusters)
