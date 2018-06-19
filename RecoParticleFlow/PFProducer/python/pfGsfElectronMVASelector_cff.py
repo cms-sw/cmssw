133e2e032b749505848ff6f6f4b204100f544184
Andrea <perrotta@cern.ch>
2018-06-19 12:13:07 +0200

    make ElectronIdMVABased and EDProducer instead of a EDFilter

--- RecoParticleFlow/PFProducer/python/pfGsfElectronMVASelector_cff.py
+++ RecoParticleFlow/PFProducer/python/pfGsfElectronMVASelector_cff.py
@@ -1,6 +1,13 @@
 import FWCore.ParameterSet.Config as cms
 
-from RecoParticleFlow.PFProducer.pfGsfElectronMVASelector_cfi import *
+from RecoEgamma.ElectronIdentification.electronIdMVABased_cfi import *
+
+electronsWithPresel = cms.EDFilter("GsfElectronSelector",
+                                   src = cms.InputTag("ecalDrivenGsfElectrons"),
+                                   cut = cms.string("pt > 5 && ecalDrivenSeed && passingCutBasedPreselection"),
+                                   )
+
+mvaElectrons.electronTag = cms.InputTag('electronsWithPresel')
 
 pfGsfElectronMVASelectionSequence = cms.Sequence(
     cms.ignore(electronsWithPresel)+
