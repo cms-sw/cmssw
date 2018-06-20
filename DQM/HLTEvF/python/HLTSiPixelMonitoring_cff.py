f5cca0a22b86c219f28e48db617a2a4d301327b4
Jan-Frederik <jschulte@cern.ch>
2018-06-20 22:35:47 +0200

    add DQM for 90m run observables backport

--- DQM/HLTEvF/python/HLTSiPixelMonitoring_cff.py
+++ DQM/HLTEvF/python/HLTSiPixelMonitoring_cff.py
@@ -2,6 +2,23 @@ import FWCore.ParameterSet.Config as cms
 
 from DQMOffline.Trigger.SiPixel_OfflineMonitoring_cff import *
 
+hltSiPixelPhase1ClustersAnalyzer.pixelSrc = cms.InputTag("hltSiPixelClustersForHighBeta")
+hltSiPixelPhase1TrackClustersAnalyzer.clusters = cms.InputTag("hltSiPixelClustersForHighBeta")
+hltSiPixelPhase1TrackClustersAnalyzer.tracks = cms.InputTag("hltPixelTracksForHighBeta")
+
+hltSiPixelPhase1TrackClustersOnTrackNClusters.range_max = 50
+hltSiPixelPhase1TrackClustersOnTrackNClusters.range_nbins = 50
+
+hltSiPixelPhase1ClustersNClusters.range_max = 50
+hltSiPixelPhase1ClustersNClusters.range_nbins = 50
+
+hltSiPixelPhase1ClustersNClustersInclusive.range_max = 50
+hltSiPixelPhase1ClustersNClustersInclusive.range_nbins = 50
+
+hltSiPixelPhase1ClustersReadoutNClusters.range_max = 50
+hltSiPixelPhase1ClustersReadoutNClusters.range_nbins = 50
+
+
 pixelOnlineMonitorHLTsequence = cms.Sequence(
     sipixelMonitorHLTsequence
 )
--- DQM/HLTEvF/python/HLTTrackingMonitoring_cff.py
+++ DQM/HLTEvF/python/HLTTrackingMonitoring_cff.py
@@ -9,8 +9,21 @@ pixelTracksMonitoringHLT.beamSpot = cms.InputTag("hltOnlineBeamSpot")
 from TrackingTools.TrackFitters.KFTrajectoryFitter_cfi import *
 from TrackingTools.TransientTrack.TransientTrackBuilder_cfi import *
 
+pixelTracksForHighBetaMonitoringHLT = pixelTracksMonitoringHLT.clone()
+pixelTracksForHighBetaMonitoringHLT.FolderName = "HLT/Tracking/pixelTracksForHighBeta"
+pixelTracksForHighBetaMonitoringHLT.TrackProducer = "hltPixelTracksForHighBeta"
+pixelTracksForHighBetaMonitoringHLT.allTrackProducer = "hltPixelTracksForHighBeta"
+
+pixelTracksForHighBetaBPixMonitoringHLT = pixelTracksMonitoringHLT.clone()
+pixelTracksForHighBetaBPixMonitoringHLT.FolderName = "HLT/Tracking/pixelTracksForHighBetaBPix"
+pixelTracksForHighBetaBPixMonitoringHLT.TrackProducer = "hltPixelTracksForHighBetaBPix"
+pixelTracksForHighBetaBPixMonitoringHLT.allTrackProducer = "hltPixelTracksForHighBetaBPix"
+
+
 trackingMonitoringHLTsequence = cms.Sequence(
     pixelTracksMonitoringHLT # hltPixel tracks monitoring
+    * pixelTracksForHighBetaMonitoringHLT # hltPixelTrackHighBeta monitoring for 90m beta star
+    * pixelTracksForHighBetaBPixMonitoringHLT # hltPixelTrackHighBetaBPix monitoring for 90m beta star
     * iter2MergedTracksMonitoringHLT # hltIter2Merged tracks monitoring    
     * iterHLTTracksMonitoringHLT # hltTracksMerged tracks monitoring
 )
