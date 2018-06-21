eb073bb2b978713a0244a5c2e62cd49ded51c346
Javier Duarte <jduarte1@fnal.gov>
2018-06-21 07:15:18 -0500

    add boolean (false by default) to control whether to apply deep double-b

--- PhysicsTools/PatAlgos/python/slimming/applyDeepBtagging_cff.py
+++ PhysicsTools/PatAlgos/python/slimming/applyDeepBtagging_cff.py
@@ -43,34 +43,36 @@ def applyDeepBtagging( process, postfix="" ) :
     delattr(process, 'selectedUpdatedPatJetsSlimmedDeepFlavour'+postfix)
 
 
-
-    ## update slimmed jets to include DeepFlavour (keep same name)
-    ## make clone for DeepDoubleB-less slimmed AK8 jets, so output name is preserved
-    #addToProcessAndTask('slimmedJetsAK8NoDeepDoubleB', process.slimmedJetsAK8.clone(), process, task)
-    #updateJetCollection(
-    #   process,
-    #   jetSource = cms.InputTag('slimmedJetsAK8NoDeepDoubleB'),
-    #   # updateJetCollection defaults to MiniAOD inputs but
-    #   # here it is made explicit (as in training or MINIAOD redoing)
-    #   pvSource = cms.InputTag('offlineSlimmedPrimaryVertices'),
-    #   pfCandidates = cms.InputTag('packedPFCandidates'),
-    #   svSource = cms.InputTag('slimmedSecondaryVertices'),
-    #   muSource = cms.InputTag('slimmedMuons'),
-    #   elSource = cms.InputTag('slimmedElectrons'),
-    #   rParam = 0.8,
-    #   jetCorrections = ('AK8PFPuppi', cms.vstring(['L1FastJet', 'L2Relative', 'L3Absolute']), 'None'),
-    #   btagDiscriminators = [
-    #      'pfDeepDoubleBJetTags:probQ',
-    #      'pfDeepDoubleBJetTags:probH',
-    #   ],
-    #   postfix = 'SlimmedAK8DeepDoubleB'+postfix,
-    #   printWarning = False
-    #)
-
-    ## slimmedJetsAK8 with DeepDoubleB (remove DeepDoubleB-less)
-    #delattr(process, 'slimmedJetsAK8')
-    #addToProcessAndTask('slimmedJetsAK8', getattr(process,'selectedUpdatedPatJetsSlimmedAK8DeepDoubleB'+postfix).clone(), process, task)
-    ## delete module not used anymore (slimmedJetsAK8 substitutes)
-    #delattr(process, 'selectedUpdatedPatJetsSlimmedAK8DeepDoubleB'+postfix)
-    
-
+    #switch to True if needed for private tests
+    enableDoubleB = False
+    if enableDoubleB:
+        # update slimmed jets to include DeepFlavour (keep same name)
+        # make clone for DeepDoubleB-less slimmed AK8 jets, so output name is preserved
+        addToProcessAndTask('slimmedJetsAK8NoDeepDoubleB', process.slimmedJetsAK8.clone(), process, task)
+        updateJetCollection(
+            process,
+            jetSource = cms.InputTag('slimmedJetsAK8NoDeepDoubleB'),
+            # updateJetCollection defaults to MiniAOD inputs but
+            # here it is made explicit (as in training or MINIAOD redoing)
+            pvSource = cms.InputTag('offlineSlimmedPrimaryVertices'),
+            pfCandidates = cms.InputTag('packedPFCandidates'),
+            svSource = cms.InputTag('slimmedSecondaryVertices'),
+            muSource = cms.InputTag('slimmedMuons'),
+            elSource = cms.InputTag('slimmedElectrons'),
+            rParam = 0.8,
+            jetCorrections = ('AK8PFPuppi', cms.vstring(['L1FastJet', 'L2Relative', 'L3Absolute']), 'None'),
+            btagDiscriminators = [
+                'pfDeepDoubleBJetTags:probQ',
+                'pfDeepDoubleBJetTags:probH',
+                ],
+            postfix = 'SlimmedAK8DeepDoubleB'+postfix,
+            printWarning = False
+            )
+        
+        # slimmedJetsAK8 with DeepDoubleB (remove DeepDoubleB-less)
+        delattr(process, 'slimmedJetsAK8')
+        addToProcessAndTask('slimmedJetsAK8', getattr(process,'selectedUpdatedPatJetsSlimmedAK8DeepDoubleB'+postfix).clone(), process, task)
+        # delete module not used anymore (slimmedJetsAK8 substitutes)
+        delattr(process, 'selectedUpdatedPatJetsSlimmedAK8DeepDoubleB'+postfix)
+        
+        
