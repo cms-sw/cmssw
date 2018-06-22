02955fddd58c0cc1a66fd2d168fdf016e73d7502
Shahzad Malik Muzaffar <Shahzad.Malik.Muzaffar@cern.ch>
2018-06-22 16:21:43 +0200

    [10.2.X][ANALYSIS] Fix include statements relative paths

--- EgammaAnalysis/ElectronTools/src/EnergyScaleCorrection_class.cc
+++ EgammaAnalysis/ElectronTools/src/EnergyScaleCorrection_class.cc
@@ -1,4 +1,4 @@
-#include "../interface/EnergyScaleCorrection_class.h"
+#include "EgammaAnalysis/ElectronTools/interface/EnergyScaleCorrection_class.h"
 #include "FWCore/ParameterSet/interface/FileInPath.h"
 #include <RooDataSet.h>
 #include <RooArgSet.h>
