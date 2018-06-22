c11a471519661cbb36eadfaf087321cc31dde006
Evan Coleman <ecoleman@fnal.gov>
2018-06-22 06:12:41 +0200

    Add warning for NaN values in DeepFlavourJetTagsProducer for DeepCSV

--- RecoBTag/Combined/plugins/DeepFlavourJetTagsProducer.cc
+++ RecoBTag/Combined/plugins/DeepFlavourJetTagsProducer.cc
@@ -246,6 +246,12 @@ DeepFlavourJetTagsProducer::produce(edm::Event& iEvent, const edm::EventSetup& i
 				else {
 					inputs_[var.name] = vars.get(var.id, var.default_value);
 				}
+
+				//warn if the input is nan
+				if(std::isnan(inputs_[var.name])) {
+					edm::LogWarning("ValueError") 	<< "The required output, " 
+													<< var.name << ", encountered an nan value during TagInfo processing." << endl;
+				}
 			}
 
 			//compute NN output(s)
@@ -254,6 +260,13 @@ DeepFlavourJetTagsProducer::produce(edm::Event& iEvent, const edm::EventSetup& i
 			//merge outputs
 			for(auto const& entry : toadd) {
 				nnout[entry.second] += nnout[entry.first];
+			}
+
+			//warn if the input is nan
+			for(auto entry : nnout) {
+				if(std::isnan(entry.second)) {
+					edm::LogWarning("ValueError") << "The NN output is nan for an event." << endl;
+				}
 			}
 		}
 
