73659881125fd7edc743a7119a6c926556a3b191
Mateusz Zarucki <mateusz.zarucki@cern.ch>
2018-06-26 17:04:50 +0200

    Generalisation of L1TStage2uGTCaloLayer2Comp DQM module input parameters

--- DQM/L1TMonitor/python/L1TStage2uGTCaloLayer2Comp_cfi.py
+++ DQM/L1TMonitor/python/L1TStage2uGTCaloLayer2Comp_cfi.py
@@ -3,13 +3,15 @@ import FWCore.ParameterSet.Config as cms
 from DQMServices.Core.DQMEDAnalyzer import DQMEDAnalyzer
 l1tStage2uGTCaloLayer2Comp = DQMEDAnalyzer(
     "L1TStage2uGTCaloLayer2Comp",
-    calol2JetCollection    = cms.InputTag("caloStage2Digis", "Jet"),
-    uGTJetCollection       = cms.InputTag("gtStage2Digis", "Jet"),
-    calol2EGammaCollection = cms.InputTag("caloStage2Digis", "EGamma"),
-    uGTEGammaCollection    = cms.InputTag("gtStage2Digis", "EGamma"),
-    calol2TauCollection    = cms.InputTag("caloStage2Digis", "Tau"),
-    uGTTauCollection       = cms.InputTag("gtStage2Digis", "Tau"),
-    calol2EtSumCollection  = cms.InputTag("caloStage2Digis", "EtSum"),
-    uGTEtSumCollection     = cms.InputTag("gtStage2Digis", "EtSum"),
+    collection1Title = cms.untracked.string("CaloLayer2"),
+    collection2Title = cms.untracked.string("uGT"),
+    JetCollection1    = cms.InputTag("caloStage2Digis", "Jet"),
+    JetCollection2    = cms.InputTag("gtStage2Digis",   "Jet"),
+    EGammaCollection1 = cms.InputTag("caloStage2Digis", "EGamma"),
+    EGammaCollection2 = cms.InputTag("gtStage2Digis",   "EGamma"),
+    TauCollection1    = cms.InputTag("caloStage2Digis", "Tau"),
+    TauCollection2    = cms.InputTag("gtStage2Digis",   "Tau"),
+    EtSumCollection1  = cms.InputTag("caloStage2Digis", "EtSum"),
+    EtSumCollection2  = cms.InputTag("gtStage2Digis",   "EtSum"),
     monitorDir = cms.untracked.string("L1T/L1TStage2uGT/calol2ouput_vs_uGTinput")
 )
