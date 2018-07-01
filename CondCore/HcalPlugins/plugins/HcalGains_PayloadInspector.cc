e1a979ef8c683460f9ec21f8228b49deefcaac5e
Jacob Jackson <jacob_jackson@brown.edu>
2018-07-01 20:36:12 +0200

    GainsPI class prints empty maps; still need to import data from Payload

--- CondCore/HcalPlugins/plugins/HcalGains_PayloadInspector.cc
+++ CondCore/HcalPlugins/plugins/HcalGains_PayloadInspector.cc
@@ -3,7 +3,7 @@
 #include "CondCore/CondDB/interface/Time.h"
 //#include "DataFormats/EcalDetId/interface/EBDetId.h"
 //#include "DataFormats/EcalDetId/interface/EEDetId.h"
-#include "DataFormats/HcalDetId/interface/HcalCalibDetId.h"
+#include "DataFormats/HcalDetId/interface/HcalDetId.h"
 
 
 //#include "CondCore/HcalPlugins/plugins/HcalDrawUtils.h"
@@ -23,6 +23,8 @@
 
 namespace {
 
+  //TODO: Check these
+  enum{ HBmaxAbsEta = 19, maxPhi = 72 }
 
   /******************************************
      2d plot of ECAL GainRatios of 1 IOV
@@ -34,15 +36,40 @@ namespace {
     }
 
     bool fill( const std::vector<std::tuple<cond::Time_t,cond::Hash> >& iovs ) override{
+      //TODO: Shift binning by 0.5
+      TH2F *hHB_d1 = new TH2F("HB_d1", "          HB_d1", 83, -42, 42, 71, 1, 72);
+      
+
+      auto iov = iovs.front();
+      std::shared_ptr<HcalGains> payload = fetchPayload( std::get<1>(iov) );
+      unsigned int run = std::get<0>(iov);
+      
+
 
       gStyle->SetPalette(1);
+
       gStyle->SetOptStat(0);      
       TCanvas canvas("CC map","CC map",1680,1320);
       TLatex t1;
       t1.SetNDC();
       t1.SetTextAlign(26);
       t1.SetTextSize(0.05);
-      t1.DrawLatex(0.5, 0.96, Form("Hcal Gains, IOV %i", -1));
+      if( payload.get() ){
+        for(int iEta = -1 * HBmaxAbsEta; iEta < HBmaxAbsEta +1; iEta++) {
+          for(int iPhi = 1; iPhi < maxPhi+1; iPhi++) {
+            HcalDetId detId = HcalDetId(HcalBarrel,iEta,iPhi,0);
+            uint32_t rawId = detId.rawId();
+           
+            hHB_d1->Fill(detId.ieta(), detId.iphi(), (*payload)[rawId].gain12Over6());
+          } 
+        }
+          //canvas->cd()
+          hHB_d1->Draw();
+        }
+        else{
+          t1.DrawLatex(0.5, 0.96, Form("COULDN'T FIND PAYLOAD %i", -1));
+        } 
+     
       std::string ImageName(m_imageFileName);
       canvas.SaveAs(ImageName.c_str());
       return true;
