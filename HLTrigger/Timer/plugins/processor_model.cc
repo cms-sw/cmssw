f5bdc88ce7e8bf50e80accd5e77e2565076c7958
Andrea Bocci <andrea.bocci@cern.ch>
2018-07-02 18:59:45 +0200

    Add support for reading the processor model on BSD and OS X

--- HLTrigger/Timer/plugins/processor_model.cc
+++ HLTrigger/Timer/plugins/processor_model.cc
@@ -1,12 +1,24 @@
+#include <string>
+#include <boost/predef/os.h>
+
+#if BOOST_OS_LINUX
+// Linux
 #include <fstream>
 #include <regex>
-#include <string>
+#endif  // BOOST_OS_LINUX
+
+#if BOOST_OS_BSD || BOOST_OS_MACOS 
+// OSX or BSD
+#include <sys/types.h>
+#include <sys/sysctl.h>
+#endif // BOOST_OS_BSD || BOOST_OS_MACOS
 
 #include "processor_model.h"
 
 std::string read_processor_model()
 {
-#ifdef __linux__
+#if BOOST_OS_LINUX
+  // on Linux, read the processor  model from /proc/cpuinfo
   static const std::regex pattern("^model name\\s*:\\s*(.*)", std::regex::optimize);
   std::smatch match;
 
@@ -18,7 +30,17 @@ std::string read_processor_model()
       return match[1];
     }
   }
-#endif // __linux__
+#endif // BOOST_OS_LINUX
+
+#if BOOST_OS_BSD || BOOST_OS_MACOS
+  // on BSD and OS X, read the processor  model via sysctlbyname("machdep.cpu.brand_string", ...)
+  std::string result;
+  size_t len;
+  sysctlbyname("machdep.cpu.brand_string", nullptr, & len, NULL, 0);
+  result.resize(len);
+  sysctlbyname("machdep.cpu.brand_string", result.data(), & len, NULL, 0);
+  return result;
+#endif // BOOST_OS_BSD || BOOST_OS_MACOS
 
   return "unknown";
 }
