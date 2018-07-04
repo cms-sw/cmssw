5fe82d32eaac548d84889d27f880ca584b85661c
Vincenzo Innocente <vincenzo.innocente@cern.ch>
2018-07-04 15:33:04 +0200

    Add GPU utilities (#69)
    Add an implementation of
      - radix-sort
      - histogram
    that work on indices in a contiguous array
    
    Add a file containing simplified versions of std algorithms that compile with cuda (mostly constexpr and no fancy templating).

---
+++ HeterogeneousCore/CUDAUtilities/interface/cudastdAlgorithm.h
@@ -0,0 +1,76 @@
+#ifndef HeterogeneousCore_CUDAUtilities_cudastdAlgorithm_h
+#define HeterogeneousCore_CUDAUtilities_cudastdAlgorithm_h
+
+#include <utility>
+
+// reimplementation of std algorithms able to compile with CUDA and run on GPUs,
+// mostly by declaringthem constexpr
+
+namespace cuda_std  {
+
+  template<typename T = void>
+  struct less {
+    constexpr bool operator()(const T &lhs, const T &rhs) const {
+      return lhs < rhs;
+    }
+  };
+
+  template<>
+  struct less<void> {
+    template<typename T, typename U>
+    constexpr bool operator()(const T &lhs, const U &rhs ) const { return lhs < rhs;}
+  };
+
+  template<typename RandomIt, typename T, typename Compare = less<T>>
+  constexpr
+  RandomIt lower_bound(RandomIt first, RandomIt last, const T& value, Compare comp={})
+  {
+    auto count = last - first;
+
+    while (count > 0) {
+        auto it = first;
+        auto step = count / 2;
+        it += step;
+        if (comp(*it, value)) {
+            first = ++it;
+            count -= step + 1;
+        }
+        else {
+            count = step;
+        }
+    }
+    return first;
+  }
+
+  template<typename RandomIt, typename T, typename Compare = less<T>>
+  constexpr
+  RandomIt upper_bound(RandomIt first, RandomIt last, const T& value, Compare comp={})
+  {
+    auto count = last - first;
+
+    while (count > 0) {
+        auto it = first;
+        auto step = count / 2;
+        it+=step;
+        if (!comp(value,*it)) {
+            first = ++it;
+            count -= step + 1;
+        }
+        else {
+            count = step;
+        }
+    }
+    return first;
+  }
+
+  template<typename RandomIt, typename T, typename Compare = cuda_std::less<T>>
+  constexpr
+  RandomIt binary_find(RandomIt first, RandomIt last, const T& value, Compare comp={})
+  {
+    first = cuda_std::lower_bound(first, last, value, comp);
+    return first != last && !comp(value, *first) ? first : last;
+  }
+
+}
+
+#endif // HeterogeneousCore_CUDAUtilities_cudastdAlgorithm_h
