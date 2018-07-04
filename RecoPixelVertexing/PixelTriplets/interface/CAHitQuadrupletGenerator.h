d75f8545439d7315f2fa35008facae467550d294
Andrea Bocci <andrea.bocci@cern.ch>
2018-07-04 23:47:58 +0200

    Synchronise with CMSSW_10_2_0_pre6

--- RecoPixelVertexing/PixelTriplets/interface/CAHitQuadrupletGenerator.h
+++ RecoPixelVertexing/PixelTriplets/interface/CAHitQuadrupletGenerator.h
@@ -16,120 +16,118 @@
 
 #include "RecoTracker/TkHitPairs/interface/IntermediateHitDoublets.h"
 #include "RecoPixelVertexing/PixelTriplets/interface/OrderedHitSeeds.h"
-#include "RecoPixelVertexing/PixelTriplets/src/CACut.h"
 
 class TrackingRegion;
 class SeedingLayerSetsHits;
 
 namespace edm {
-  class Event;
-  class EventSetup;
-  class ParameterSetDescription;
-}  // namespace edm
+    class Event;
+    class EventSetup;
+    class ParameterSetDescription;
+}
 
 class CAHitQuadrupletGenerator {
 public:
-  typedef LayerHitMapCache LayerCacheType;
+    typedef LayerHitMapCache LayerCacheType;
 
-  static constexpr unsigned int minLayers = 4;
-  typedef OrderedHitSeeds ResultType;
+    static constexpr unsigned int minLayers = 4;
+    typedef OrderedHitSeeds ResultType;
 
 public:
-  CAHitQuadrupletGenerator(const edm::ParameterSet& cfg, edm::ConsumesCollector&& iC)
-      : CAHitQuadrupletGenerator(cfg, iC) {}
-  CAHitQuadrupletGenerator(const edm::ParameterSet& cfg, edm::ConsumesCollector& iC);
 
-  ~CAHitQuadrupletGenerator() = default;
+    CAHitQuadrupletGenerator(const edm::ParameterSet& cfg, edm::ConsumesCollector&& iC): CAHitQuadrupletGenerator(cfg, iC) {}
+    CAHitQuadrupletGenerator(const edm::ParameterSet& cfg, edm::ConsumesCollector& iC);
 
-  static void fillDescriptions(edm::ParameterSetDescription& desc);
-  static const char* fillDescriptionsLabel() { return "caHitQuadruplet"; }
+    ~CAHitQuadrupletGenerator() = default;
 
-  void initEvent(const edm::Event& ev, const edm::EventSetup& es);
+    static void fillDescriptions(edm::ParameterSetDescription& desc);
+    static const char *fillDescriptionsLabel() { return "caHitQuadrupletDefault"; }
 
-  void hitNtuplets(const IntermediateHitDoublets& regionDoublets,
-                   std::vector<OrderedHitSeeds>& result,
-                   const edm::EventSetup& es,
-                   const SeedingLayerSetsHits& layers);
+    void initEvent(const edm::Event& ev, const edm::EventSetup& es);
+
+    void hitNtuplets(const IntermediateHitDoublets& regionDoublets,
+                     std::vector<OrderedHitSeeds>& result,
+                     const edm::EventSetup& es,
+                     const SeedingLayerSetsHits& layers);
 
 private:
-  LayerCacheType theLayerCache;
+    LayerCacheType theLayerCache;
 
-  std::unique_ptr<SeedComparitor> theComparitor;
+    std::unique_ptr<SeedComparitor> theComparitor;
 
-  class QuantityDependsPtEval {
-  public:
-    QuantityDependsPtEval(float v1, float v2, float c1, float c2)
-        : value1_(v1), value2_(v2), curvature1_(c1), curvature2_(c2) {}
+    class QuantityDependsPtEval {
+    public:
 
-    float value(float curvature) const {
-      if (value1_ == value2_)  // not enabled
-        return value1_;
+        QuantityDependsPtEval(float v1, float v2, float c1, float c2) :
+        value1_(v1), value2_(v2), curvature1_(c1), curvature2_(c2) {
+        }
 
-      if (curvature1_ < curvature)
-        return value1_;
-      if (curvature2_ < curvature && curvature <= curvature1_)
-        return value2_ + (curvature - curvature2_) / (curvature1_ - curvature2_) * (value1_ - value2_);
-      return value2_;
-    }
+        float value(float curvature) const {
+            if (value1_ == value2_) // not enabled
+                return value1_;
 
-  private:
-    const float value1_;
-    const float value2_;
-    const float curvature1_;
-    const float curvature2_;
-  };
+            if (curvature1_ < curvature)
+                return value1_;
+            if (curvature2_ < curvature && curvature <= curvature1_)
+                return value2_ + (curvature - curvature2_) / (curvature1_ - curvature2_) * (value1_ - value2_);
+            return value2_;
+        }
 
-  // Linear interpolation (in curvature) between value1 at pt1 and
-  // value2 at pt2. If disabled, value2 is given (the point is to
-  // allow larger/smaller values of the quantity at low pt, so it
-  // makes more sense to have the high-pt value as the default).
+    private:
+        const float value1_;
+        const float value2_;
+        const float curvature1_;
+        const float curvature2_;
+    };
 
-  class QuantityDependsPt {
-  public:
-    explicit QuantityDependsPt(const edm::ParameterSet& pset)
-        : value1_(pset.getParameter<double>("value1")),
-          value2_(pset.getParameter<double>("value2")),
-          pt1_(pset.getParameter<double>("pt1")),
-          pt2_(pset.getParameter<double>("pt2")),
-          enabled_(pset.getParameter<bool>("enabled")) {
-      if (enabled_ && pt1_ >= pt2_)
-        throw cms::Exception("Configuration") << "PixelQuadrupletGenerator::QuantityDependsPt: pt1 (" << pt1_
-                                              << ") needs to be smaller than pt2 (" << pt2_ << ")";
-      if (pt1_ <= 0)
-        throw cms::Exception("Configuration")
-            << "PixelQuadrupletGenerator::QuantityDependsPt: pt1 needs to be > 0; is " << pt1_;
-      if (pt2_ <= 0)
-        throw cms::Exception("Configuration")
-            << "PixelQuadrupletGenerator::QuantityDependsPt: pt2 needs to be > 0; is " << pt2_;
-    }
+    // Linear interpolation (in curvature) between value1 at pt1 and
+    // value2 at pt2. If disabled, value2 is given (the point is to
+    // allow larger/smaller values of the quantity at low pt, so it
+    // makes more sense to have the high-pt value as the default).
 
-    QuantityDependsPtEval evaluator(const edm::EventSetup& es) const {
-      if (enabled_) {
-        return QuantityDependsPtEval(value1_,
-                                     value2_,
-                                     PixelRecoUtilities::curvature(1.f / pt1_, es),
-                                     PixelRecoUtilities::curvature(1.f / pt2_, es));
-      }
-      return QuantityDependsPtEval(value2_, value2_, 0.f, 0.f);
-    }
+    class QuantityDependsPt {
+    public:
 
-  private:
-    const float value1_;
-    const float value2_;
-    const float pt1_;
-    const float pt2_;
-    const bool enabled_;
-  };
+        explicit QuantityDependsPt(const edm::ParameterSet& pset) :
+        value1_(pset.getParameter<double>("value1")),
+        value2_(pset.getParameter<double>("value2")),
+        pt1_(pset.getParameter<double>("pt1")),
+        pt2_(pset.getParameter<double>("pt2")),
+        enabled_(pset.getParameter<bool>("enabled")) {
+            if (enabled_ && pt1_ >= pt2_)
+                throw cms::Exception("Configuration") << "PixelQuadrupletGenerator::QuantityDependsPt: pt1 (" << pt1_ << ") needs to be smaller than pt2 (" << pt2_ << ")";
+            if (pt1_ <= 0)
+                throw cms::Exception("Configuration") << "PixelQuadrupletGenerator::QuantityDependsPt: pt1 needs to be > 0; is " << pt1_;
+            if (pt2_ <= 0)
+                throw cms::Exception("Configuration") << "PixelQuadrupletGenerator::QuantityDependsPt: pt2 needs to be > 0; is " << pt2_;
+        }
 
-  const float extraHitRPhitolerance;
+        QuantityDependsPtEval evaluator(const edm::EventSetup& es) const {
+            if (enabled_) {
+                return QuantityDependsPtEval(value1_, value2_,
+                        PixelRecoUtilities::curvature(1.f / pt1_, es),
+                        PixelRecoUtilities::curvature(1.f / pt2_, es));
+            }
+            return QuantityDependsPtEval(value2_, value2_, 0.f, 0.f);
+        }
 
-  const QuantityDependsPt maxChi2;
-  const bool fitFastCircle;
-  const bool fitFastCircleChi2Cut;
-  const bool useBendingCorrection;
+    private:
+        const float value1_;
+        const float value2_;
+        const float pt1_;
+        const float pt2_;
+        const bool enabled_;
+    };
 
-  CACut caThetaCut;
-  CACut caPhiCut;
-  const float caHardPtCut = 0.f;
+    const float extraHitRPhitolerance;
+
+    const QuantityDependsPt maxChi2;
+    const bool fitFastCircle;
+    const bool fitFastCircleChi2Cut;
+    const bool useBendingCorrection;
+
+    const float caThetaCut = 0.00125f;
+    const float caPhiCut = 0.1f;
+    const float caHardPtCut = 0.f;
 };
 #endif
