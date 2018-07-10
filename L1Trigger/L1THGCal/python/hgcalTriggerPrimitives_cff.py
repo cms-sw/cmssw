762cbcacf7dba04ae4118be1f3e9b118162a3650
Emilia Becheva <emilia.becheva@llr.in2p3.fr>
2018-07-10 15:51:34 +0200

    update configuration files

--- L1Trigger/L1THGCal/python/hgcalTriggerPrimitives_cff.py
+++ L1Trigger/L1THGCal/python/hgcalTriggerPrimitives_cff.py
@@ -1,9 +1,12 @@
 import FWCore.ParameterSet.Config as cms
 
 from L1Trigger.L1THGCal.hgcalTriggerGeometryESProducer_cfi import *
-from L1Trigger.L1THGCal.hgcalTriggerPrimitiveDigiProducer_cfi import *
+from L1Trigger.L1THGCal.hgcalVFEProducer_cfi import *
+from L1Trigger.L1THGCal.hgcalConcentratorProducer_cfi import *
+from L1Trigger.L1THGCal.hgcalBackEndLayer1Producer_cfi import *
+from L1Trigger.L1THGCal.hgcalBackEndLayer2Producer_cfi import *
+from L1Trigger.L1THGCal.hgcalTowerMapProducer_cfi import *
+from L1Trigger.L1THGCal.hgcalTowerProducer_cfi import *
 
 
-hgcalTriggerPrimitives = cms.Sequence(hgcalTriggerPrimitiveDigiProducer)
-
-hgcalTriggerPrimitives_reproduce = cms.Sequence(hgcalTriggerPrimitiveDigiFEReproducer)
+hgcalTriggerPrimitives = cms.Sequence(hgcalVFEProducer*hgcalConcentratorProducer*hgcalBackEndLayer1Producer*hgcalBackEndLayer2Producer*hgcalTowerMapProducer*hgcalTowerProducer)
