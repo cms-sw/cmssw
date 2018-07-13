a5467fabda5c9dff5dbf6393d28bf2f27a57c3f9
Petar <petar@jhu.edu>
2018-07-13 17:15:03 -0500

    Final tweaks before the PR; proper Era-dependent config of pixel plugins

--- FastSimulation/TrackingRecHitProducer/python/PixelPluginsPhase0_cfi.py
+++ FastSimulation/TrackingRecHitProducer/python/PixelPluginsPhase0_cfi.py
@@ -1,10 +1,7 @@
 import FWCore.ParameterSet.Config as cms
 
 
-fastTrackerRecHits = cms.EDProducer("TrackingRecHitProducer",
-    simHits = cms.InputTag("fastSimProducer","TrackerHits"),
-    plugins=cms.VPSet()
-)
+pixelPluginsPhase0=cms.VPSet()
 
 #=====================================================================================
 #--- Phase 0 Pixel Barrel
@@ -14,7 +11,7 @@ fastTrackerRecHits = cms.EDProducer("TrackingRecHitProducer",
 #      *      5611       template_events_d39921.out.gz    pixel_histos39921_5611_6.root
 #
 #
-fastTrackerRecHits.plugins.append(
+pixelPluginsPhase0.append(
     cms.PSet(
         select = cms.string("subdetId==BPX"),
         isBarrel = cms.bool(True),
@@ -41,7 +38,7 @@ fastTrackerRecHits.plugins.append(
 #       *       6415      template_events_d40722.out.gz   pixel_histos40722_6415_6.root
 #
 #
-fastTrackerRecHits.plugins.append(
+pixelPluginsPhase0.append(
     cms.PSet(
         select=cms.string("subdetId==FPX"),
         isBarrel = cms.bool(False),
@@ -60,46 +57,3 @@ fastTrackerRecHits.plugins.append(
 )
 
 
-
-trackerStripGaussianResolutions={
-    "TIB": {
-        1: cms.double(0.00195),
-        2: cms.double(0.00191),
-        3: cms.double(0.00325),
-        4: cms.double(0.00323)
-    },
-    "TID": {
-        1: cms.double(0.00262),
-        2: cms.double(0.00354),
-        3: cms.double(0.00391)
-    },
-    "TOB": {
-        1: cms.double(0.00461),
-        2: cms.double(0.00458),
-        3: cms.double(0.00488),
-        4: cms.double(0.00491),
-        5: cms.double(0.00293),
-        6: cms.double(0.00299)
-    },
-    "TEC": {
-        1: cms.double(0.00262),
-        2: cms.double(0.00354),
-        3: cms.double(0.00391),
-        4: cms.double(0.00346),
-        5: cms.double(0.00378),
-        6: cms.double(0.00508),
-        7: cms.double(0.00422),
-        8: cms.double(0.00434),
-        9: cms.double(0.00432),
-    }
-}
-
-for subdetId,trackerLayers in trackerStripGaussianResolutions.iteritems():
-    for trackerLayer, resolutionX in trackerLayers.iteritems():
-        pluginConfig = cms.PSet(
-            name = cms.string(subdetId+str(trackerLayer)),
-            type=cms.string("TrackingRecHitStripGSPlugin"),
-            resolutionX=resolutionX,
-            select=cms.string("(subdetId=="+subdetId+") && (layer=="+str(trackerLayer)+")"),
-        )
-        fastTrackerRecHits.plugins.append(pluginConfig)
