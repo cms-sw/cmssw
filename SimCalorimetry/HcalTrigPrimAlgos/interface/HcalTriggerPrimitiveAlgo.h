2d99f808aa4858de79a5de02341605eca9be5fd6
Chris West <christopher.alan.west@gmail.com>
2018-07-16 23:47:26 +0200

    Ignore TDC information when computing min-bias feature bit

--- SimCalorimetry/HcalTrigPrimAlgos/interface/HcalTriggerPrimitiveAlgo.h
+++ SimCalorimetry/HcalTrigPrimAlgos/interface/HcalTriggerPrimitiveAlgo.h
@@ -27,7 +27,7 @@ public:
   HcalTriggerPrimitiveAlgo(bool pf, const std::vector<double>& w, int latency,
                            uint32_t FG_threshold, uint32_t FG_HF_threshold, uint32_t ZS_threshold,
                            int numberOfSamples,   int numberOfPresamples,
-                           int numberOfSamplesHF, int numberOfPresamplesHF,
+                           int numberOfSamplesHF, int numberOfPresamplesHF, bool useTDCInMinBiasBits,
                            uint32_t minSignalThreshold=0, uint32_t PMT_NoiseThreshold=0);
   ~HcalTriggerPrimitiveAlgo();
 
@@ -83,6 +83,7 @@ public:
   void addFG(const HcalTrigTowerDetId& id, std::vector<bool>& msb);
   void addUpgradeFG(const HcalTrigTowerDetId& id, int depth, const std::vector<std::bitset<2>>& bits);
 
+  bool passTDC(const QIE10DataFrame& digi, int ts) const;
   bool validUpgradeFG(const HcalTrigTowerDetId& id, int depth) const;
   bool validChannel(const QIE10DataFrame& digi, int ts) const;
   bool needLegacyFG(const HcalTrigTowerDetId& id) const;
@@ -124,6 +125,7 @@ public:
   int numberOfPresamples_;
   int numberOfSamplesHF_;
   int numberOfPresamplesHF_;
+  bool useTDCInMinBiasBits_;
   uint32_t minSignalThreshold_;
   uint32_t PMT_NoiseThreshold_; 
   int NCTScaleShift;
@@ -157,6 +159,7 @@ public:
      QIE10DataFrame digi;
      std::vector<bool> validity;
      std::vector<bool> fgbit;
+     std::vector<bool> passTDC;
   };
   typedef std::map<HcalTrigTowerDetId, std::map<uint32_t, std::array<HFUpgradeDetails, 4>>> HFUpgradeDetailMap;
   HFUpgradeDetailMap theHFUpgradeDetailMap;
