26b07dc256f6822decc1347f2e483bc04d267234
Michail <bachtis@cern.ch>
2018-07-21 09:44:59 +0200

    Updates for new version

--- L1Trigger/L1TMuonBarrel/interface/L1TMuonBarrelKalmanStubProcessor.h
+++ L1Trigger/L1TMuonBarrel/interface/L1TMuonBarrelKalmanStubProcessor.h
@@ -22,7 +22,8 @@ class L1TMuonBarrelKalmanStubProcessor {
 
 
   L1MuKBMTCombinedStubCollection makeStubs(const L1MuDTChambPhContainer*,const L1MuDTChambThContainer*,const L1TMuonBarrelParams&);
-  void printWord(const L1MuDTChambPhContainer* phiContainer,const L1MuDTChambThContainer* etaContainer,int,int);
+  void makeInputPattern(const L1MuDTChambPhContainer* phiContainer,const L1MuDTChambThContainer* etaContainer,int sector);
+
   
  private:
   bool isGoodPhiStub(const L1MuDTChambPhDigi*); 
@@ -48,84 +49,7 @@ class L1TMuonBarrelKalmanStubProcessor {
 
 
 
-  //For Making Patterns
-  typedef struct {
-    int ts1_st1_phi;
-    int ts1_st1_phib;
-    int ts1_st1_q;
-    int ts1_st1_rpc;
-    int ts1_st1_cal;
 
-    int ts1_st2_phi;
-    int ts1_st2_phib;
-    int  ts1_st2_q;
-    int ts1_st2_rpc;
-    int ts1_st2_cal;
-
-    int ts1_st3_phi;
-    int ts1_st3_phib;
-    int ts1_st3_q;
-    int ts1_st3_rpc;
-    int ts1_st3_cal;
-
-    int ts1_st4_phi;
-    int ts1_st4_phib;
-    int ts1_st4_q;
-    int ts1_st4_rpc;
-    int ts1_st4_cal;
-
-    int eta_hit_st1;
-    int eta_hit_st2;
-    int eta_hit_st3;
-
-    int bcnt_1a;
-    int bcnt_1b;
-    int bcnt_1c;
-    int bcnt_1d;
-    int bcnt_1e;
-    int bcnt_1f;
-
-    int bc0_1;
-
-
-    int ts2_st1_phi;
-    int ts2_st1_phib;
-    int ts2_st1_q;
-    int ts2_st1_rpc;
-    int ts2_st1_cal;
-
-    int ts2_st2_phi;
-    int ts2_st2_phib;
-    int ts2_st2_q;
-    int ts2_st2_rpc;
-    int ts2_st2_cal;
-
-    int ts2_st3_phi;
-    int ts2_st3_phib;
-    int ts2_st3_q;
-    int ts2_st3_rpc;
-    int ts2_st3_cal;
-
-    int ts2_st4_phi;
-    int ts2_st4_phib;
-    int ts2_st4_q;
-    int ts2_st4_rpc;
-    int ts2_st4_cal;
-
-    int eta_qbit_st1;
-    int eta_qbit_st2;
-    int eta_qbit_st3;
-
-    int bcnt_2a;
-    int bcnt_2b;
-    int bcnt_2c;
-    int bcnt_2d;
-    int bcnt_2e;
-    int bcnt_2f;
-    int bc0_2;
-  }bmtf_in;
-
-  bmtf_in makePattern(const L1MuDTChambPhContainer* phiContainer,const L1MuDTChambThContainer* etaContainer,int sector, int wheel);
 
 };
 
