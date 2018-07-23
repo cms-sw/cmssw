5d344f0b37868db2e9c6f5294a10513c20f4c3f9
Petar <petar@jhu.edu>
2018-07-23 18:08:20 -0500

    Fixing EdmFileInPath().c_str() possible issue with GCC 7

--- FastSimulation/TrackingRecHitProducer/interface/PixelResolutionHistograms.h
+++ FastSimulation/TrackingRecHitProducer/interface/PixelResolutionHistograms.h
@@ -25,9 +25,9 @@ public:
   //    We make empty histograms (which we own), but generator pointers 
   //    remain null.
   //
-  PixelResolutionHistograms( const char * filename,    // ROOT file for histograms
-			     const char * rootdir,     // Subdirectory in the file, "" if none
-			     const char * descTitle,   // Descriptive title	     
+  PixelResolutionHistograms( std::string filename,    // ROOT file for histograms
+			     std::string rootdir,     // Subdirectory in the file, "" if none
+			     std::string descTitle,   // Descriptive title	     
 			     unsigned int detType,     // Where we are... (&&& do we need this?)
 			     double cotbetaBinWidth,   // cot(beta) : bin width,
 			     double cotbetaLowEdge,    //           : low endpoint,
@@ -44,8 +44,8 @@ public:
   //    ROOT file, and we do *not* own them.  But we do own the
   //    generators.
   //
-  PixelResolutionHistograms( const char * filename,      // ROOT file for histograms
-			     const char * rootdir = "",  // ROOT dir, "" if none
+  PixelResolutionHistograms( std::string filename,      // ROOT file for histograms
+			     std::string rootdir = "",  // ROOT dir, "" if none
 			     int   detType = -1,         // default: read from ROOT file.
 			     bool  ignore_multi = false, // Forward Big is always single
 			     bool  ignore_single = false,   // Edge does not need single pixels
