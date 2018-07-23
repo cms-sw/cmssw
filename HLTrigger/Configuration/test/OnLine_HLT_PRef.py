154ea36108bc54641ad6dcbc08e07d6e608fc3e0
silviodonato <silvio.donato@cern.ch>
2018-07-23 20:04:17 +0200

    Fix L1 seed

--- HLTrigger/Configuration/test/OnLine_HLT_PRef.py
+++ HLTrigger/Configuration/test/OnLine_HLT_PRef.py
@@ -1,13 +1,13 @@
 # hltGetConfiguration --full --data /dev/CMSSW_10_1_0/PRef --type PRef --unprescale --process HLTPRef --globaltag auto:run2_hlt_PRef --input file:RelVal_Raw_PRef_DATA.root
 
-# /dev/CMSSW_10_1_0/PRef/V57 (CMSSW_10_1_7)
+# /dev/CMSSW_10_1_0/PRef/V62 (CMSSW_10_1_7)
 
 import FWCore.ParameterSet.Config as cms
 
 process = cms.Process( "HLTPRef" )
 
 process.HLTConfigVersion = cms.PSet(
-  tableName = cms.string('/dev/CMSSW_10_1_0/PRef/V57')
+  tableName = cms.string('/dev/CMSSW_10_1_0/PRef/V62')
 )
 
 process.transferSystem = cms.PSet( 
