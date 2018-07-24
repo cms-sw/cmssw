686e9e382181c177635607494fd2f6769c6e9b74
Martin Grunewald <Martin.Grunewald@cern.ch>
2018-07-24 11:33:03 +0200

    Move frozen menu from V2.2 to V3.2

--- HLTrigger/Configuration/python/Tools/options.py
+++ HLTrigger/Configuration/python/Tools/options.py
@@ -5,7 +5,7 @@ globalTag = {
   'Fake2': 'auto:run2_mc_Fake2',
   'FULL' : 'auto:run2_mc_FULL',
   'GRun' : 'auto:run2_mc_GRun',       # used as default
-  '2018v22' : 'auto:run2_mc_2018v22',
+  '2018v32' : 'auto:run2_mc_2018v32',
   'HIon' : 'auto:run2_mc_HIon',
   'PIon' : 'auto:run2_mc_PIon',
   'PRef' : 'auto:run2_mc_PRef',
