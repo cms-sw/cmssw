8555c653ef5b9e701c02ff77c64fae2c67d2ccbe
David <David.Lange@cern.ch>
2018-07-25 15:09:01 +0200

    python print migration Alignment/MuonAlignmentAlgorithms

--- Alignment/MuonAlignmentAlgorithms/python/gather_cfg.py
+++ Alignment/MuonAlignmentAlgorithms/python/gather_cfg.py
@@ -1,3 +1,4 @@
+from __future__ import print_function
 import os
 import FWCore.ParameterSet.Config as cms
 
@@ -8,7 +9,7 @@ except:
     try:
         import simplejson as json
     except:
-        print "Please use lxplus or set an environment (for example crab) with json lib available"
+        print("Please use lxplus or set an environment (for example crab) with json lib available")
         sys.exit(1)
 
 inputfiles = os.environ["ALIGNMENT_INPUTFILES"].split(" ")
