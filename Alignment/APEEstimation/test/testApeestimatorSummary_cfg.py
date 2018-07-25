da3d2e426bc3f5e89d726e825b00b086ad2502f9
David <David.Lange@cern.ch>
2018-07-25 15:08:00 +0200

    python print migration Alignment/APEEstimation

--- Alignment/APEEstimation/test/testApeestimatorSummary_cfg.py
+++ Alignment/APEEstimation/test/testApeestimatorSummary_cfg.py
@@ -1,3 +1,4 @@
+from __future__ import print_function
 import os
 
 import FWCore.ParameterSet.Config as cms
@@ -24,8 +25,8 @@ if( hasattr(sys, "argv") ):
             if(len(val)==2):
                 setattr(options,val[0], val[1])
 
-print "Input sample: ", options.sample
-print "Test run: ", options.isTest
+print("Input sample: ", options.sample)
+print("Test run: ", options.isTest)
 
 
 
@@ -96,7 +97,7 @@ elif options.sample == 'zmumu20':
     isZmumu20 = True
     isMc = True
 else:
-    print 'ERROR --- incorrect data sammple: ', options.sample
+    print('ERROR --- incorrect data sammple: ', options.sample)
     exit(8888)
 
 
