66dfb73ba54ec939b5f3ff370d2ac4f8066e9809
David <David.Lange@cern.ch>
2018-07-26 18:02:04 +0200

    python print migration PhysicsTools/TagAndProbe

--- PhysicsTools/TagAndProbe/test/utilities/dumpPileup.py
+++ PhysicsTools/TagAndProbe/test/utilities/dumpPileup.py
@@ -1,7 +1,8 @@
 #!/usr/bin/env python
+from __future__ import print_function
 import sys
 import ROOT
 fin=ROOT.TFile.Open(sys.argv[1])
 pu = fin.Get("pileup")
 pileup = map( pu.GetBinContent, xrange(1,pu.GetNbinsX()+1) )
-print ",".join(map(lambda x: "%1.3g"%x, pileup))
+print(",".join(map(lambda x: "%1.3g"%x, pileup)))
