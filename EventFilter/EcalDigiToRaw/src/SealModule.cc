b444a9ba40da620492ccddd6aef357b88a3a47bd
Christopher Jones <chrisdjones15@gmail.com>
2018-07-26 16:26:22 -0500

    Made EcalDigiToRaw a global module

--- EventFilter/EcalDigiToRaw/src/SealModule.cc
+++
@@ -1,9 +0,0 @@
-
-#include "FWCore/PluginManager/interface/ModuleDef.h"
-#include "FWCore/Framework/interface/MakerMacros.h"
-#include "EventFilter/EcalDigiToRaw/interface/EcalDigiToRaw.h"
-
-
-DEFINE_FWK_MODULE(EcalDigiToRaw);
-
-
