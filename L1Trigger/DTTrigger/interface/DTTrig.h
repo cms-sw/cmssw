0945d94f2fe44f89844bb9c4ef394b6e6041d31b
Christopher Jones <chrisdjones15@gmail.com>
2018-07-27 15:04:49 -0500

    Modernized DTTrigProd
    Refactored DTTrig and DTTrigProd to use modern C++ and modern
    CMSSW constructs. This will easy the migration of DTTrigProd
    to a thread friendly module type.

--- L1Trigger/DTTrigger/interface/DTTrig.h
+++ L1Trigger/DTTrigger/interface/DTTrig.h
@@ -43,6 +43,7 @@
 
 #include "FWCore/ParameterSet/interface/ParameterSet.h"
 
+#include <memory>
 
 class InputTag;
 
@@ -54,10 +55,10 @@ class DTTrig {
 
   public:
 
-    typedef std::map< DTChamberId,DTSCTrigUnit*,std::less<DTChamberId> > TUcontainer;
+    typedef std::map< DTChamberId,std::unique_ptr<DTSCTrigUnit>,std::less<DTChamberId> > TUcontainer;
     typedef TUcontainer::iterator TU_iterator;
     typedef TUcontainer::const_iterator TU_const_iterator;
-    typedef std::map< DTSectCollId,DTSectColl*,std::less<DTSectCollId> > SCcontainer;
+    typedef std::map< DTSectCollId,std::unique_ptr<DTSectColl>,std::less<DTSectCollId> > SCcontainer;
     typedef SCcontainer::iterator SC_iterator;
     typedef SCcontainer::const_iterator SC_const_iterator;
     typedef std::pair<TU_iterator,TU_iterator> Range;
@@ -87,7 +88,7 @@ class DTTrig {
     void clear();
 
     //! Size of the trigger units store
-    int size() { return _cache.size(); }
+    int size() const { return _cache.size(); }
 
     //! Begin of the trigger units store
     TU_iterator begin() { /*check();*/ return _cache.begin(); }
@@ -104,7 +105,7 @@ class DTTrig {
     // ------------ do the same for Sector Collector
     
     //! Size of the sector collector store
-    int size1() { /*check();*/ return _cache1.size(); }
+    int size1() const { /*check();*/ return _cache1.size(); }
 
     //! Begin of the sector collector store
     SC_iterator begin1() { /*check();*/ return _cache1.begin(); }
@@ -174,33 +175,33 @@ class DTTrig {
     // end sector collector
 
     //! Dump the geometry
-    void dumpGeom();
+    void dumpGeom() const;
 
     //! Dump the LUT files
-    void dumpLuts(short int lut_btic, const DTConfigManager *conf);
+    void dumpLuts(short int lut_btic, const DTConfigManager *conf) const;
 
     //! Get BX Offset
-    int getBXOffset() { return _conf_manager->getBXOffset(); }
+    int getBXOffset() const { return _conf_manager->getBXOffset(); }
 
     // Methods to access intermediate results
 
     //! Return a copy of all the BTI triggers
-    std::vector<DTBtiTrigData> BtiTrigs();
+    std::vector<DTBtiTrigData> BtiTrigs() const;
 
     //! Return a copy of all the TRACO triggers
-    std::vector<DTTracoTrigData> TracoTrigs();
+    std::vector<DTTracoTrigData> TracoTrigs() const;
 
     //! Return a copy of all the Trigger Server (Phi) triggers
-    std::vector<DTChambPhSegm> TSPhTrigs();
+    std::vector<DTChambPhSegm> TSPhTrigs() const;
 
     //! Return a copy of all the Trigger Server (Theta) triggers
-    std::vector<DTChambThSegm> TSThTrigs();
+    std::vector<DTChambThSegm> TSThTrigs() const;
 
     //! Return a copy of all the Sector Collector (Phi) triggers
-    std::vector<DTSectCollPhSegm> SCPhTrigs();
+    std::vector<DTSectCollPhSegm> SCPhTrigs() const;
 
     //! Return a copy of all the Sector Collector (Theta) triggers
-    std::vector<DTSectCollThSegm> SCThTrigs();
+    std::vector<DTSectCollThSegm> SCThTrigs() const;
 
     //! Coordinate of a trigger-data object in chamber frame
     LocalPoint localPosition(const DTTrigData* trig) const {
