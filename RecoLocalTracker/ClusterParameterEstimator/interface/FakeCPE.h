911b957ff9d1e7c6c662613421cc85b74e7f7e34
Vincenzo Innocente <vincenzo.innocente@cern.ch>
2018-07-31 10:29:24 +0200

    add a fakecpe

---
+++ RecoLocalTracker/ClusterParameterEstimator/interface/FakeCPE.h
@@ -0,0 +1,64 @@
+#ifndef RecoLocalTracker_Cluster_Parameter_Estimator_Fake_H
+#define RecoLocalTracker_Cluster_Parameter_Estimator_Fake_H
+
+#include "DataFormats/GeometrySurface/interface/LocalError.h"
+#include "DataFormats/GeometryVector/interface/LocalPoint.h"
+
+#include "Geometry/CommonDetUnit/interface/GeomDet.h"
+
+#include "DataFormats/SiPixelCluster/interface/SiPixelCluster.h"
+#include "DataFormats/SiStripCluster/interface/SiStripCluster.h"
+
+
+
+#include<map>
+
+class FakeCPE {
+public:
+
+  using LocalValues = std::pair<LocalPoint,LocalError>;
+
+  // store position and error for each cluster...
+  class Map {
+     public: 
+     using LocalValues = std::pair<LocalPoint,LocalError>;
+     void clear() {m_map.clear());
+     void error() const;
+     template<typename Cluster>
+     void add(const Cluster& cluster, const GeomDetUnit& gd,LocalValues const & lv) { m_map[encode(cluster,gd) = lv; }
+     template<typename Cluster>
+     LocalValues const & get(const Cluster& cluster, const GeomDetUnit& gd) const {
+       auto p = m_map.find(encode(cluster,gd));
+       if (p!=m_map.end) return (*p).second;
+       error();
+       return dummy;
+     }
+
+     static uint64_t encode(const SiPixelCluster& cluster, const GeomDetUnit& gd) {
+          uint64_t u1 = det.geographicalId().rawId();
+          uint64_t u2 = cluster.minPixelRow();
+          uint64_t u3 = cluster.minPixelCol();
+          return (u1<<32) & (u2<<16) & u3;
+     }
+     static uint64_t encode(const SaiStripCluster& cluster, const GeomDetUnit& gd) {
+          uint64_t u1 = det.geographicalId().rawId();
+          uint64_t u2 = cluster.firstStrip();
+       	  return (u1<<32) & u2;
+     }
+
+
+     std::unordered_map<uint64_t,LocalValues> m_map;
+     LocalValues dummy;
+  };
+ 
+
+  m_map & map() { return m_map;}
+  m_map const & map() const { return m_map;}
+
+private:
+
+  Map m_map;
+
+};
+
+#endif
