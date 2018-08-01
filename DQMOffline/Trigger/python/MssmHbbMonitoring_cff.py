c176866a997e6c0bac6229aef1060cada133836f
Mia <mia.tosi@cern.ch>
2018-08-01 13:51:38 +0200

    fix naming (HLT/Higgs --> HLT/HIG)

--- DQMOffline/Trigger/python/MssmHbbMonitoring_cff.py
+++ DQMOffline/Trigger/python/MssmHbbMonitoring_cff.py
@@ -11,7 +11,8 @@ hltMssmHbbmonitoring.bJetMuDeltaRmax = cms.double(0.4)   # dR(mu,nbjet) cone; on
 
 # Fully-hadronic MssmHbb
 hltMssmHbbmonitoringAL100 = hltMssmHbbmonitoring.clone()
-hltMssmHbbmonitoringAL100.FolderName = cms.string('HLT/Higgs/MssmHbb/fullhadronic/pt100')
+#hltMssmHbbmonitoringAL100.FolderName = cms.string('HLT/Higgs/MssmHbb/fullhadronic/pt100')
+hltMssmHbbmonitoringAL100.FolderName = cms.string('HLT/HIG/MssmHbb/fullhadronic/pt100')
 hltMssmHbbmonitoringAL100.nmuons = cms.uint32(0)
 hltMssmHbbmonitoringAL100.nbjets = cms.uint32(2)
 hltMssmHbbmonitoringAL100.bjetSelection = cms.string('pt>110 & abs(eta)<2.2')
@@ -19,7 +20,8 @@ hltMssmHbbmonitoringAL100.numGenericTriggerEventPSet.hltPaths = cms.vstring('HLT
 hltMssmHbbmonitoringAL100.histoPSet.jetPtBinning = cms.vdouble(0,250,280,300,320,360,400,700,1000,1500)
 
 hltMssmHbbmonitoringAL116 = hltMssmHbbmonitoring.clone()
-hltMssmHbbmonitoringAL116.FolderName = cms.string('HLT/Higgs/MssmHbb/fullhadronic/pt116')
+#hltMssmHbbmonitoringAL116.FolderName = cms.string('HLT/Higgs/MssmHbb/fullhadronic/pt116')
+hltMssmHbbmonitoringAL116.FolderName = cms.string('HLT/HIG/MssmHbb/fullhadronic/pt116')
 hltMssmHbbmonitoringAL116.nmuons = cms.uint32(0)
 hltMssmHbbmonitoringAL116.nbjets = cms.uint32(2)
 hltMssmHbbmonitoringAL116.bjetSelection = cms.string('pt>116 & abs(eta)<2.2')
@@ -27,7 +29,8 @@ hltMssmHbbmonitoringAL116.numGenericTriggerEventPSet.hltPaths = cms.vstring('HLT
 hltMssmHbbmonitoringAL116.histoPSet.jetPtBinning = cms.vdouble(0,250,280,300,320,360,400,700,1000,1500)
 
 hltMssmHbbmonitoringAL128 = hltMssmHbbmonitoring.clone()
-hltMssmHbbmonitoringAL128.FolderName = cms.string('HLT/Higgs/MssmHbb/fullhadronic/pt128')
+#hltMssmHbbmonitoringAL128.FolderName = cms.string('HLT/Higgs/MssmHbb/fullhadronic/pt128')
+hltMssmHbbmonitoringAL128.FolderName = cms.string('HLT/HIG/MssmHbb/fullhadronic/pt128')
 hltMssmHbbmonitoringAL128.nmuons = cms.uint32(0)
 hltMssmHbbmonitoringAL128.nbjets = cms.uint32(2)
 hltMssmHbbmonitoringAL128.bjetSelection = cms.string('pt>128 & abs(eta)<2.2')
@@ -36,7 +39,8 @@ hltMssmHbbmonitoringAL128.histoPSet.jetPtBinning = cms.vdouble(0,250,280,300,320
 
 # Semi-leptonic MssmHbb(mu)
 hltMssmHbbmonitoringSL40 = hltMssmHbbmonitoring.clone()
-hltMssmHbbmonitoringSL40.FolderName = cms.string('HLT/Higgs/MssmHbb/semileptonic/pt40')
+#hltMssmHbbmonitoringSL40.FolderName = cms.string('HLT/Higgs/MssmHbb/semileptonic/pt40')
+hltMssmHbbmonitoringSL40.FolderName = cms.string('HLT/HIG/MssmHbb/semileptonic/pt40')
 hltMssmHbbmonitoringSL40.nmuons = cms.uint32(1)
 hltMssmHbbmonitoringSL40.nbjets = cms.uint32(2)
 hltMssmHbbmonitoringSL40.muoSelection = cms.string('pt>12 & abs(eta)<2.2 & isPFMuon & isGlobalMuon  & innerTrack.hitPattern.trackerLayersWithMeasurement>5 & innerTrack.hitPattern.numberOfValidPixelHits>0 & globalTrack.hitPattern.numberOfValidMuonHits>0 & globalTrack.normalizedChi2<10')
@@ -45,7 +49,8 @@ hltMssmHbbmonitoringSL40.numGenericTriggerEventPSet.hltPaths = cms.vstring('HLT_
 hltMssmHbbmonitoringSL40.histoPSet.jetPtBinning = cms.vdouble(0,250,280,300,320,360,400,700,1000,1500)
 
 hltMssmHbbmonitoringSL54 = hltMssmHbbmonitoring.clone()
-hltMssmHbbmonitoringSL54.FolderName = cms.string('HLT/Higgs/MssmHbb/semileptonic/pt54')
+#hltMssmHbbmonitoringSL54.FolderName = cms.string('HLT/Higgs/MssmHbb/semileptonic/pt54')
+hltMssmHbbmonitoringSL54.FolderName = cms.string('HLT/HIG/MssmHbb/semileptonic/pt54')
 hltMssmHbbmonitoringSL54.nmuons = cms.uint32(1)
 hltMssmHbbmonitoringSL54.nbjets = cms.uint32(2)
 hltMssmHbbmonitoringSL54.muoSelection = cms.string('pt>12 & abs(eta)<2.2 & isPFMuon & isGlobalMuon  & innerTrack.hitPattern.trackerLayersWithMeasurement>5 & innerTrack.hitPattern.numberOfValidPixelHits>0 & globalTrack.hitPattern.numberOfValidMuonHits>0 & globalTrack.normalizedChi2<10')
@@ -54,7 +59,8 @@ hltMssmHbbmonitoringSL54.numGenericTriggerEventPSet.hltPaths = cms.vstring('HLT_
 hltMssmHbbmonitoringSL54.histoPSet.jetPtBinning = cms.vdouble(0,250,280,300,320,360,400,700,1000,1500)
 
 hltMssmHbbmonitoringSL62 = hltMssmHbbmonitoring.clone()
-hltMssmHbbmonitoringSL62.FolderName = cms.string('HLT/Higgs/MssmHbb/semileptonic/pt62')
+#hltMssmHbbmonitoringSL62.FolderName = cms.string('HLT/Higgs/MssmHbb/semileptonic/pt62')
+hltMssmHbbmonitoringSL62.FolderName = cms.string('HLT/HIG/MssmHbb/semileptonic/pt62')
 hltMssmHbbmonitoringSL62.nmuons = cms.uint32(1)
 hltMssmHbbmonitoringSL62.nbjets = cms.uint32(2)
 hltMssmHbbmonitoringSL62.muoSelection = cms.string('pt>12 & abs(eta)<2.2 & isPFMuon & isGlobalMuon  & innerTrack.hitPattern.trackerLayersWithMeasurement>5 & innerTrack.hitPattern.numberOfValidPixelHits>0 & globalTrack.hitPattern.numberOfValidMuonHits>0 & globalTrack.normalizedChi2<10')
@@ -64,7 +70,8 @@ hltMssmHbbmonitoringSL62.histoPSet.jetPtBinning = cms.vdouble(0,250,280,300,320,
 
 #control b-tagging 
 hltMssmHbbmonitoringControl = hltMssmHbbmonitoring.clone()
-hltMssmHbbmonitoringControl.FolderName = cms.string('HLT/Higgs/MssmHbb/control/mu12_pt30_nobtag')
+#hltMssmHbbmonitoringControl.FolderName = cms.string('HLT/Higgs/MssmHbb/control/mu12_pt30_nobtag')
+hltMssmHbbmonitoringControl.FolderName = cms.string('HLT/HIG/MssmHbb/control/mu12_pt30_nobtag')
 hltMssmHbbmonitoringControl.nmuons = cms.uint32(1)
 hltMssmHbbmonitoringControl.nbjets = cms.uint32(0)
 hltMssmHbbmonitoringControl.njets = cms.uint32(1)
