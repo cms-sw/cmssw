e47d2a62a208db255d32cb34b1e14eacd4296eb9
Mia <mia.tosi@cern.ch>
2018-08-01 12:03:47 +0200

    (re)enable HLTDQM + fix naming scheme

--- DQMOffline/Trigger/python/B2GMonitoring_cff.py
+++ DQMOffline/Trigger/python/B2GMonitoring_cff.py
@@ -143,14 +143,16 @@ AK8PFJet420_TrimMass30_PromptMonitoring.numGenericTriggerEventPSet.hltPaths = cm
 
 
 b2gDileptonHLTOfflineDQM = topDiLeptonHLTOfflineDQM.clone()
-b2gDileptonHLTOfflineDQM.setup.directory = cms.string('HLT/B2GHLTOffline/Dileptonic/CrossTriggers')
+#b2gDileptonHLTOfflineDQM.setup.directory = cms.string('HLT/B2GHLTOffline/Dileptonic/CrossTriggers')
+b2gDileptonHLTOfflineDQM.setup.directory = cms.string('HLT/B2G/Dileptonic/CrossTriggers')
 b2gDileptonHLTOfflineDQM.setup.triggerExtras.pathsELECMU = cms.vstring(['HLT_Mu37_Ele27_CaloIdL_MW_v','HLT_Mu27_Ele37_CaloIdL_MW_v'])
 b2gDileptonHLTOfflineDQM.setup.triggerExtras.pathsDIMUON = cms.vstring([''])
 b2gDileptonHLTOfflineDQM.setup.triggerExtras.pathsDIELEC = cms.vstring([''])
 b2gDileptonHLTOfflineDQM.preselection.trigger.select = cms.vstring(['HLT_Mu37_Ele27_CaloIdL_MW_v','HLT_Mu27_Ele37_CaloIdL_MW_v'])
 
 b2gDimuonHLTOfflineDQM = topDiLeptonHLTOfflineDQM.clone()
-b2gDimuonHLTOfflineDQM.setup.directory = cms.string('HLT/B2GHLTOffline/Dileptonic/Dimuon')
+#b2gDimuonHLTOfflineDQM.setup.directory = cms.string('HLT/B2GHLTOffline/Dileptonic/Dimuon')
+b2gDimuonHLTOfflineDQM.setup.directory = cms.string('HLT/B2G/Dileptonic/Dimuon')
 b2gDimuonHLTOfflineDQM.setup.triggerExtras.pathsELECMU = cms.vstring([''])
 b2gDimuonHLTOfflineDQM.setup.triggerExtras.pathsDIMUON = cms.vstring(['HLT_Mu37_TkMu27_v'])
 b2gDimuonHLTOfflineDQM.setup.triggerExtras.pathsDIELEC = cms.vstring([''])
@@ -159,9 +161,6 @@ b2gDimuonHLTOfflineDQM.preselection.trigger.select = cms.vstring(['HLT_Mu37_TkMu
 
 
 b2gMonitorHLT = cms.Sequence(
-)
-
-b2gHLTDQMSourceExtra = cms.Sequence(
     PFHT1050_Mjjmonitoring +
     PFHT1050_Softdropmonitoring +
 
@@ -194,6 +193,8 @@ b2gHLTDQMSourceExtra = cms.Sequence(
     b2gDileptonHLTOfflineDQM*
     b2gDimuonHLTOfflineDQM,
 
-    cms.Task(B2GegmGsfElectronIDsForDQM)
+    cms.Task(B2GegmGsfElectronIDsForDQM) ## unschedule execution [Use of electron VID requires this module being executed first]
+)
 
+b2gHLTDQMSourceExtra = cms.Sequence(
 )
