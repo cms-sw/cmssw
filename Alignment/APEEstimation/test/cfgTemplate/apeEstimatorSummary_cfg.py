569f4e9202ca024714c9a02a159f7f15dc45e502
mteroerd <marius.teroerde@cern.ch>
2018-08-03 13:25:44 +0200

    Enabled parallel APE measurements

--- Alignment/APEEstimation/test/cfgTemplate/apeEstimatorSummary_cfg.py
+++ Alignment/APEEstimation/test/cfgTemplate/apeEstimatorSummary_cfg.py
@@ -13,17 +13,11 @@ import sys
 options = VarParsing.VarParsing ('standard')
 options.register('iterNumber', 0, VarParsing.VarParsing.multiplicity.singleton, VarParsing.VarParsing.varType.string, "Iteration number")
 options.register('setBaseline', False, VarParsing.VarParsing.multiplicity.singleton, VarParsing.VarParsing.varType.bool, "Set baseline")
-
-
+options.register('measurementName', "workingArea", VarParsing.VarParsing.multiplicity.singleton, VarParsing.VarParsing.varType.string, "Folder in which to store results")
+options.register('baselineName', "Design", VarParsing.VarParsing.multiplicity.singleton, VarParsing.VarParsing.varType.string, "Folder in which baseline-trees are found or stored")
 
 # get and parse the command line arguments
-if( hasattr(sys, "argv") ):
-    for args in sys.argv :
-        arg = args.split(',')
-        for val in arg:
-            val = val.split('=')
-            if(len(val)==2):
-                setattr(options,val[0], val[1])
+options.parseArguments()
 
 print("Iteration number: ", options.iterNumber)
 print("Set baseline: ", options.setBaseline)
@@ -62,9 +56,9 @@ process.options = cms.untracked.PSet(
 ## Input Files
 ##
 process.source = cms.Source("EmptySource",
-					numberEventsInRun = cms.untracked.uint32(1),
-					firstRun = cms.untracked.uint32(246994)
-					)
+                    numberEventsInRun = cms.untracked.uint32(1),
+                    firstRun = cms.untracked.uint32(246994)
+                    )
 
 
 
@@ -78,18 +72,20 @@ process.load("Configuration.StandardSequences.FrontierConditions_GlobalTag_cff")
 from Configuration.AlCa.GlobalTag import GlobalTag
 from CondCore.CondDB.CondDB_cfi import *
 
-process.GlobalTag = GlobalTag(process.GlobalTag, 'auto:phase1_2017_design', '')
 
+process.GlobalTag = GlobalTag(process.GlobalTag, 'auto:phase1_2018_design', '')
+
+# Load a certain APE payload to write into allData_defaultApe.root 
 CondDBAlignmentError = CondDB.clone(connect = cms.string('frontier://FrontierProd/CMS_CONDITIONS'))
 process.myTrackerAlignmentErr = cms.ESSource("PoolDBESSource",
-	CondDBAlignmentError,
-	timetype = cms.string("runnumber"),
-	toGet = cms.VPSet(
-		cms.PSet(
-			record = cms.string('TrackerAlignmentErrorExtendedRcd'),
-			tag = cms.string('TrackerAlignmentExtendedErr_2009_v2_express_IOVs')
-		)
-	)
+    CondDBAlignmentError,
+    timetype = cms.string("runnumber"),
+    toGet = cms.VPSet(
+        cms.PSet(
+            record = cms.string('TrackerAlignmentErrorExtendedRcd'),
+            tag = cms.string('TrackerAlignmentExtendedErr_2009_v2_express_IOVs')
+        )
+    )
 )
 process.es_prefer_trackerAlignmentErr = cms.ESPrefer("PoolDBESSource","myTrackerAlignmentErr")
 
@@ -102,33 +98,33 @@ process.ApeEstimatorSummarySequence = cms.Sequence()
 if options.setBaseline:
   process.ApeEstimatorSummary1 = ApeEstimatorSummaryBaseline.clone(
     # baseline will be set
-    BaselineFile = os.environ['CMSSW_BASE'] + '/src/Alignment/APEEstimation/hists/Design/baseline/allData_baselineApe.root',
-    DefaultFile = os.environ['CMSSW_BASE'] + '/src/Alignment/APEEstimation/hists/Design/baseline/allData_defaultApe.root',
-    InputFile = os.environ['CMSSW_BASE'] + '/src/Alignment/APEEstimation/hists/Design/baseline/allData.root',
-    ResultsFile = os.environ['CMSSW_BASE'] + '/src/Alignment/APEEstimation/hists/Design/baseline/allData_resultsFile.root',
+    BaselineFile = os.environ['CMSSW_BASE'] + '/src/Alignment/APEEstimation/hists/'+options.baselineName+'/baseline/allData_baselineApe.root',
+    DefaultFile = os.environ['CMSSW_BASE'] + '/src/Alignment/APEEstimation/hists/'+options.baselineName+'/baseline/allData_defaultApe.root',
+    InputFile = os.environ['CMSSW_BASE'] + '/src/Alignment/APEEstimation/hists/'+options.baselineName+'/baseline/allData.root',
+    ResultsFile = os.environ['CMSSW_BASE'] + '/src/Alignment/APEEstimation/hists/'+options.baselineName+'/baseline/allData_resultsFile.root',
   )
   process.ApeEstimatorSummary2 = ApeEstimatorSummaryIter.clone(
-    BaselineFile = os.environ['CMSSW_BASE'] + '/src/Alignment/APEEstimation/hists/Design/baseline/allData_baselineApe.root',
-    InputFile = os.environ['CMSSW_BASE'] + '/src/Alignment/APEEstimation/hists/Design/baseline/allData.root',
-    ResultsFile = os.environ['CMSSW_BASE'] + '/src/Alignment/APEEstimation/hists/Design/baseline/allData_resultsFile.root',
+    BaselineFile = os.environ['CMSSW_BASE'] + '/src/Alignment/APEEstimation/hists/'+options.baselineName+'/baseline/allData_baselineApe.root',
+    InputFile = os.environ['CMSSW_BASE'] + '/src/Alignment/APEEstimation/hists/'+options.baselineName+'/baseline/allData.root',
+    ResultsFile = os.environ['CMSSW_BASE'] + '/src/Alignment/APEEstimation/hists/'+options.baselineName+'/baseline/allData_resultsFile.root',
     # files are not in use in baseline mode
-    IterationFile = os.environ['CMSSW_BASE'] + '/src/Alignment/APEEstimation/hists/Design/baseline/allData_iterationApe.root',
-    DefaultFile = os.environ['CMSSW_BASE'] + '/src/Alignment/APEEstimation/hists/Design/baseline/allData_defaultApe.root',
-    ApeOutputFile = os.environ['CMSSW_BASE'] + '/src/Alignment/APEEstimation/hists/Design/baseline/allData_apeOutput.txt',
+    IterationFile = os.environ['CMSSW_BASE'] + '/src/Alignment/APEEstimation/hists/'+options.baselineName+'/baseline/allData_iterationApe.root',
+    DefaultFile = os.environ['CMSSW_BASE'] + '/src/Alignment/APEEstimation/hists/'+options.baselineName+'/baseline/allData_defaultApe.root',
+    ApeOutputFile = os.environ['CMSSW_BASE'] + '/src/Alignment/APEEstimation/hists/'+options.baselineName+'/baseline/allData_apeOutput.txt',
   )
   process.ApeEstimatorSummarySequence *= process.ApeEstimatorSummary1
   process.ApeEstimatorSummarySequence *= process.ApeEstimatorSummary2
 else:
   process.ApeEstimatorSummary1 = ApeEstimatorSummaryIter.clone(
     # keep the same for all jobs
-    BaselineFile = os.environ['CMSSW_BASE'] + '/src/Alignment/APEEstimation/hists/Design/baseline/allData_baselineApe.root',
+    BaselineFile = os.environ['CMSSW_BASE'] + '/src/Alignment/APEEstimation/hists/'+options.baselineName+'/baseline/allData_baselineApe.root',
     # keep the first one on misaligned geometry for iterations on same geometry (or better use copy of it)
-    IterationFile = os.environ['CMSSW_BASE'] + '/src/Alignment/APEEstimation/hists/workingArea/iter'+str(options.iterNumber)+'/allData_iterationApe.root',
+    IterationFile = os.environ['CMSSW_BASE'] + '/src/Alignment/APEEstimation/hists/'+options.measurementName+'/iter'+str(options.iterNumber)+'/allData_iterationApe.root',
     # change iteration number for these
-    InputFile = os.environ['CMSSW_BASE'] + '/src/Alignment/APEEstimation/hists/workingArea/iter'+str(options.iterNumber)+'/allData.root',
-    ResultsFile = os.environ['CMSSW_BASE'] + '/src/Alignment/APEEstimation/hists/workingArea/iter'+str(options.iterNumber)+'/allData_resultsFile.root',
-    ApeOutputFile = os.environ['CMSSW_BASE'] + '/src/Alignment/APEEstimation/hists/workingArea/iter'+str(options.iterNumber)+'/allData_apeOutput.txt',
-    DefaultFile = os.environ['CMSSW_BASE'] + '/src/Alignment/APEEstimation/hists/workingArea/iter'+str(options.iterNumber)+'/allData_defaultApe.root',
+    InputFile = os.environ['CMSSW_BASE'] + '/src/Alignment/APEEstimation/hists/'+options.measurementName+'/iter'+str(options.iterNumber)+'/allData.root',
+    ResultsFile = os.environ['CMSSW_BASE'] + '/src/Alignment/APEEstimation/hists/'+options.measurementName+'/iter'+str(options.iterNumber)+'/allData_resultsFile.root',
+    ApeOutputFile = os.environ['CMSSW_BASE'] + '/src/Alignment/APEEstimation/hists/'+options.measurementName+'/iter'+str(options.iterNumber)+'/allData_apeOutput.txt',
+    DefaultFile = os.environ['CMSSW_BASE'] + '/src/Alignment/APEEstimation/hists/'+options.measurementName+'/iter'+str(options.iterNumber)+'/allData_defaultApe.root',
   )
   process.ApeEstimatorSummarySequence *= process.ApeEstimatorSummary1
 
