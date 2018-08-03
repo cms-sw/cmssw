41332dae43f60f18663de17678f49571a55e5f7e
Ianna Osborne <ianna.osborne@cern.ch>
2018-08-03 10:29:07 +0200

    Use namespace and CMS style

--- DetectorDescription/Core/interface/DDUnits.h
+++ DetectorDescription/Core/interface/DDUnits.h
@@ -3,29 +3,36 @@
 
 #include <cmath>
 
-#define ConvertTo(_x, _y) (_x)/(1.0_##_y)
+#define CONVERT_TO(_x, _y) (_x)/(1.0_##_y)
 
-constexpr long double _pi(M_PI);
+namespace dd {
+  
+  constexpr long double _pi(M_PI);
+  
+  namespace operators {
 
-constexpr long double operator"" _deg( long double deg )
-{
-    return deg*_pi/180;
+    constexpr long double operator"" _deg( long double deg )
+    {
+      return deg*_pi/180;
+    }
+    
+    constexpr long double operator"" _deg( unsigned long long int deg )
+    {
+      return deg*_pi/180;
+    }
+    
+    constexpr long double operator"" _mm( long double length )
+    {
+      return length*1.;
+    }
+    
+    constexpr long double operator "" _pi( long double x ) 
+    { return x * _pi; }
+    
+    constexpr long double operator "" _pi( unsigned long long int x ) 
+    { return x * _pi; } 
+    
+  }
 }
-
-constexpr long double operator"" _deg( unsigned long long int deg )
-{
-    return deg*_pi/180;
-}
-
-constexpr long double operator"" _mm( long double length )
-{
-    return length*1.;
-}
-
-constexpr long double operator "" _pi( long double x ) 
-{ return x * _pi; }
-
-constexpr long double operator "" _pi( unsigned long long int x ) 
-{ return x * _pi; } 
-
+  
 #endif
