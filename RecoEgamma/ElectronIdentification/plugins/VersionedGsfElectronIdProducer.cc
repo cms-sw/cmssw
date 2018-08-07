97b29c8e583d72c05a16d72e6cde0862f32621c2
Jonas Rembser <jonas.rembser@cern.ch>
2018-08-08 01:14:12 +0200

    Remove unneeded includes

--- RecoEgamma/ElectronIdentification/plugins/VersionedGsfElectronIdProducer.cc
+++ RecoEgamma/ElectronIdentification/plugins/VersionedGsfElectronIdProducer.cc
@@ -1,21 +1,5 @@
-// system include files
-#include <memory>
-
-// user include files
-#include "FWCore/Framework/interface/Frameworkfwd.h"
-#include "FWCore/Framework/interface/EDProducer.h"
-
-#include "FWCore/Framework/interface/Event.h"
-#include "FWCore/Framework/interface/MakerMacros.h"
-
-#include "FWCore/ParameterSet/interface/ParameterSet.h"
-
-#include "DataFormats/Common/interface/View.h"
-
-#include "PhysicsTools/SelectorUtils/interface/VersionedIdProducer.h"
-
 #include "DataFormats/EgammaCandidates/interface/GsfElectron.h"
-#include "DataFormats/EgammaCandidates/interface/GsfElectronFwd.h"
+#include "PhysicsTools/SelectorUtils/interface/VersionedIdProducer.h"
 
 typedef VersionedIdProducer<reco::GsfElectronPtr> VersionedGsfElectronIdProducer;
 
