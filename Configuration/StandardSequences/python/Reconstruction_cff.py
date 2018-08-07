1edd63ae2de21aa87e2efe2112a462712c19cfba
Matthew <Matthew.Nguyen@cern.ch>
2018-08-07 14:10:31 +0200

    Exclude taus, IVF and PF Met from Run2_2018_pp_on_AA era

--- Configuration/StandardSequences/python/Reconstruction_cff.py
+++ Configuration/StandardSequences/python/Reconstruction_cff.py
@@ -158,6 +158,7 @@ _highlevelreco_HI = highlevelreco.copy()
 _highlevelreco_HI += hiCentrality
 _highlevelreco_HI += hiClusterCompatibility
 (pp_on_XeXe_2017 | pp_on_AA_2018).toReplaceWith(highlevelreco, _highlevelreco_HI)
+pp_on_AA_2018.toReplaceWith(highlevelreco,highlevelreco.copyAndExclude([PFTau]))
 
 # not commisoned and not relevant in FastSim (?):
 _fastSim_highlevelreco = highlevelreco.copyAndExclude([cosmicDCTracksSeq,muoncosmichighlevelreco])
--- RecoVertex/V0Producer/python/generalV0Candidates_cff.py
+++ RecoVertex/V0Producer/python/generalV0Candidates_cff.py
@@ -9,3 +9,5 @@ import FWCore.ParameterSet.Config as cms
 # ctfV0Producer
 from RecoVertex.V0Producer.generalV0Candidates_cfi import *
 
+from Configuration.Eras.Modifier_pp_on_AA_2018_cff import pp_on_AA_2018
+pp_on_AA_2018.toModify(generalV0Candidates, tkPtCut = 999.)
