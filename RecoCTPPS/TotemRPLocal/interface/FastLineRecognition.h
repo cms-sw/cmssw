460f8bc48b93df6e6e447c41d8c821c21976541a
W. David Dagenhart <wdd@fnal.gov>
2018-08-07 13:51:12 -0500

    GCC 8 Compiler Warnings, Minor Reco Fixes
    Two fixes included here and neither should affect
    behavior. One modifies the extent of an unnamed
    namespace. It was maybe inconsistent before, but
    not a real problem. The other deletes some
    uninitialized variables which were also not used
    for anything.

--- RecoCTPPS/TotemRPLocal/interface/FastLineRecognition.h
+++ RecoCTPPS/TotemRPLocal/interface/FastLineRecognition.h
@@ -83,12 +83,11 @@ class FastLineRecognition
     {
       double Saw, Sbw, Sw, S1;
       double weight;
-      double min_a, max_a, min_b, max_b;
 
       std::vector<const Point *> contents;
-      
+
       Cluster() : Saw(0.), Sbw(0.), Sw(0.), S1(0.), weight(0.) {}
-      
+
       void add(const Point *p1, const Point *p2, double a, double b, double w);
 
       bool operator<(const Cluster &c) const
