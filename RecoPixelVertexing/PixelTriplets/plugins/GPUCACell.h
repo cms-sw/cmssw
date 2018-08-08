ecd14656a68c029c883c5faf492612da0a8db68b
Vincenzo Innocente <vincenzo.innocente@cern.ch>
2018-08-08 19:17:46 +0200

    Pixel doublets on GPU (cms-patatrack#118)
    Pixel doublets (actually CACells) are created on GPU and fed to CA.
    The whole workflow up to quadruplets candidates is now fully on GPU.

--- RecoPixelVertexing/PixelTriplets/plugins/GPUCACell.h
+++ RecoPixelVertexing/PixelTriplets/plugins/GPUCACell.h
@@ -5,42 +5,42 @@
 #define GPU_CACELL_H_
 
 #include "GPUHitsAndDoublets.h"
+#include "RecoLocalTracker/SiPixelRecHits/plugins/siPixelRecHitsHeterogeneousProduct.h"
 #include "HeterogeneousCore/CUDAUtilities/interface/GPUVecArray.h"
 #include "HeterogeneousCore/CUDAUtilities/interface/GPUSimpleVector.h"
 #include <cuda.h>
+
 struct Quadruplet {
-  int2 layerPairsAndCellId[3];
+  int hitId[4];
 };
+
 
 class GPUCACell {
 public:
   __host__ __device__  GPUCACell() {}
 
-  __host__ __device__ void init(const GPULayerDoublets *doublets,
-                                const GPULayerHits *hitsOnLayer,
-                                int layerPairId, int doubletId, int innerHitId,
-                                int outerHitId, float regionX, float regionY) {
 
+__host__ __device__ void init(siPixelRecHitsHeterogeneousProduct::HitsOnGPU const & hh,
+                              int layerPairId, int doubletId, int innerHitId,int outerHitId) {
     theInnerHitId = innerHitId;
     theOuterHitId = outerHitId;
     theDoubletId = doubletId;
     theLayerPairId = layerPairId;
 
-    auto innerLayerId = doublets->innerLayerId;
-    auto outerLayerId = doublets->outerLayerId;
+    theInnerX = hh.xg_d[innerHitId];
+    theOuterX = hh.xg_d[outerHitId];
 
-    theInnerX = hitsOnLayer[innerLayerId].x[innerHitId];
-    theOuterX = hitsOnLayer[outerLayerId].x[outerHitId];
+    theInnerY = hh.yg_d[innerHitId];
+    theOuterY = hh.yg_d[outerHitId];
 
-    theInnerY = hitsOnLayer[innerLayerId].y[innerHitId];
-    theOuterY = hitsOnLayer[outerLayerId].y[outerHitId];
-
-    theInnerZ = hitsOnLayer[innerLayerId].z[innerHitId];
-    theOuterZ = hitsOnLayer[outerLayerId].z[outerHitId];
-    theInnerR = hypot(theInnerX - regionX, theInnerY - regionY);
-    theOuterR = hypot(theOuterX - regionX, theOuterY - regionY);
+    theInnerZ = hh.zg_d[innerHitId];
+    theOuterZ = hh.zg_d[outerHitId];
+    theInnerR = hh.rg_d[innerHitId];
+    theOuterR = hh.rg_d[outerHitId];
     theOuterNeighbors.reset();
   }
+
+
 
   constexpr float get_inner_x() const { return theInnerX; }
   constexpr float get_outer_x() const { return theOuterX; }
@@ -201,57 +201,31 @@ public:
     // the ntuplets is then saved if the number of hits it contains is greater
     // than a threshold
 
+    tmpNtuplet.push_back_unsafe(theInnerHitId);
+    assert(tmpNtuplet.size()<=3);
 
-    if ((unsigned int)(tmpNtuplet.size()) >= minHitsPerNtuplet - 1) {
+    if ((unsigned int)(tmpNtuplet.size()) >= minHitsPerNtuplet-1) {
       Quadruplet tmpQuadruplet;
-      for (unsigned int i = 0; i < minHitsPerNtuplet - 1; ++i) {
-        tmpQuadruplet.layerPairsAndCellId[i].x = cells[tmpNtuplet[i]].theLayerPairId;
-        tmpQuadruplet.layerPairsAndCellId[i].y = tmpNtuplet[i];
+      for (unsigned int i = 0; i < minHitsPerNtuplet-1; ++i) {
+        tmpQuadruplet.hitId[i] = tmpNtuplet[i];
       }
+      tmpQuadruplet.hitId[minHitsPerNtuplet-1] = theOuterHitId;
       foundNtuplets->push_back(tmpQuadruplet);
     }
     else {
       for (int j = 0; j < theOuterNeighbors.size(); ++j) {
         auto otherCell = theOuterNeighbors[j];
-        tmpNtuplet.push_back_unsafe(otherCell);
         cells[otherCell].find_ntuplets(cells, foundNtuplets, tmpNtuplet,
                                        minHitsPerNtuplet);
-        tmpNtuplet.pop_back();
       }
     }
+    tmpNtuplet.pop_back();
+    assert(tmpNtuplet.size()<3);
   }
 
 #endif
-  template <int maxNumberOfQuadruplets>
-  __host__ inline void find_ntuplets_host(
-      const GPUCACell *cells,
-      GPU::VecArray<Quadruplet, maxNumberOfQuadruplets> *foundNtuplets,
-      GPU::VecArray<unsigned int, 3> &tmpNtuplet,
-      const unsigned int minHitsPerNtuplet) const {
 
-    Quadruplet tmpQuadruplet;
-    if (tmpNtuplet.size() >= minHitsPerNtuplet - 1) {
-      for (int i = 0; i < minHitsPerNtuplet - 1; ++i) {
-        tmpQuadruplet.layerPairsAndCellId[i].x =
-            cells[tmpNtuplet[i]].theLayerPairId;
 
-        tmpQuadruplet.layerPairsAndCellId[i].y = tmpNtuplet[i];
-      }
-      foundNtuplets->push_back(tmpQuadruplet);
-
-    }
-
-    else {
-      for (int j = 0; j < theOuterNeighbors.size(); ++j) {
-        auto otherCell = theOuterNeighbors[j];
-        tmpNtuplet.push_back_unsafe(otherCell);
-        cells[otherCell].find_ntuplets_host(cells, foundNtuplets, tmpNtuplet,
-                                            minHitsPerNtuplet);
-
-        tmpNtuplet.pop_back();
-      }
-    }
-  }
   GPU::VecArray< unsigned int, 40> theOuterNeighbors;
 
   int theDoubletId;
