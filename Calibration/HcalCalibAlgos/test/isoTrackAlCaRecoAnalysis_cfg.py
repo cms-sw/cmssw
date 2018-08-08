dcb3b9b1f9ec79753383f88fc6ed919fa9aaf525
Sunanda <sunanda.banerjee@cern.ch>
2018-08-08 02:06:46 +0200

    Include variables to study effect of isolation in ECAL

--- Calibration/HcalCalibAlgos/test/isoTrackAlCaRecoAnalysis_cfg.py
+++ Calibration/HcalCalibAlgos/test/isoTrackAlCaRecoAnalysis_cfg.py
@@ -1,6 +1,7 @@
 import FWCore.ParameterSet.Config as cms
 process = cms.Process('ANALYSIS')
 
+process.load("FWCore.MessageService.MessageLogger_cfi")
 process.load('Configuration.StandardSequences.Services_cff')
 process.load("Configuration.StandardSequences.GeometryRecoDB_cff")
 process.load("Configuration.StandardSequences.MagneticField_cff")
@@ -9,79 +10,36 @@ from Configuration.AlCa.autoCond import autoCond
 process.GlobalTag.globaltag=autoCond['run2_mc']
 #process.GlobalTag.globaltag = 'START53_V15::All'
 
-process.MessageLogger = cms.Service("MessageLogger",
-    destinations = cms.untracked.vstring('warnings','errors',
-                                         'cout','cerr'),
-    categories = cms.untracked.vstring('HcalIsoTrack'), 
-    debugModules = cms.untracked.vstring('*'),
-    warnings = cms.untracked.PSet(
-        placeholder = cms.untracked.bool(True)
-    ),
-    default = cms.untracked.PSet(
+if 'MessageLogger' in process.__dict__:
+    process.MessageLogger.categories.append('HcalIsoTrack')
 
-    ),
-    errors = cms.untracked.PSet(
-        placeholder = cms.untracked.bool(True)
-    ),
-    cerr = cms.untracked.PSet(
-        optionalPSet = cms.untracked.bool(True),
-        INFO = cms.untracked.PSet(
-            limit = cms.untracked.int32(0)
-        ),
-        noTimeStamps = cms.untracked.bool(False),
-        FwkReport = cms.untracked.PSet(
-            optionalPSet = cms.untracked.bool(True),
-            reportEvery = cms.untracked.int32(500),
-            limit = cms.untracked.int32(10000000)
-        ),
-        default = cms.untracked.PSet(
-            limit = cms.untracked.int32(10000000)
-        ),
-        Root_NoDictionary = cms.untracked.PSet(
-            optionalPSet = cms.untracked.bool(True),
-            limit = cms.untracked.int32(0)
-        ),
-        FwkJob = cms.untracked.PSet(
-            optionalPSet = cms.untracked.bool(True),
-            limit = cms.untracked.int32(0)
-        ),
-        FwkSummary = cms.untracked.PSet(
-            optionalPSet = cms.untracked.bool(True),
-            reportEvery = cms.untracked.int32(1),
-            limit = cms.untracked.int32(10000000)
-        ),
-        threshold = cms.untracked.string('INFO')
-     ),
-     cout = cms.untracked.PSet(
-        threshold = cms.untracked.string('INFO'),
-        noTimeStamps = cms.untracked.bool(True),
-        INFO = cms.untracked.PSet(
-            limit = cms.untracked.int32(0)
-        ),
-        DEBUG = cms.untracked.PSet(
-            limit = cms.untracked.int32(0)
-        ),
-        HcalIsoTrack = cms.untracked.PSet(
-            limit = cms.untracked.int32(-1)
-       )
-    )
-)
+process.MessageLogger.cerr.FwkReport.reportEvery = 1
+process.options = cms.untracked.PSet( wantSummary = cms.untracked.bool(True) )
+
+process.load('RecoLocalCalo.CaloTowersCreator.calotowermaker_cfi')
+process.towerMakerAll = process.calotowermaker.clone()
+process.towerMakerAll.hbheInput = cms.InputTag("hbhereco")
+process.towerMakerAll.hoInput = cms.InputTag("none")
+process.towerMakerAll.hfInput = cms.InputTag("none")
+process.towerMakerAll.ecalInputs = cms.VInputTag(cms.InputTag("ecalRecHit","EcalRecHitsEB"), cms.InputTag("ecalRecHit","EcalRecHitsEE"))
+process.towerMakerAll.AllowMissingInputs = True
 
 process.load('Calibration.HcalCalibAlgos.HcalIsoTrkAnalyzer_cfi')
-process.HcalIsoTrkAnalyzer.ProcessName  = 'HLTNew1'
-process.HcalIsoTrkAnalyzer.ProducerName = 'ALCAISOTRACK'
-process.HcalIsoTrkAnalyzer.ModuleName   = 'IsoProd'
+process.HcalIsoTrkAnalyzer.processName  = 'HLTNew1'
+process.HcalIsoTrkAnalyzer.producerName = 'ALCAISOTRACK'
+process.HcalIsoTrkAnalyzer.moduleName   = 'IsoProd'
+
 process.source = cms.Source("PoolSource", 
                             fileNames = cms.untracked.vstring(
         'file:/afs/cern.ch/work/g/gwalia/calib/dqm/test_29_04/CMSSW_7_5_0_pre2/src/Calibration/HcalCalibAlgos/test/PoolOutput.root'
     )
 )
+
 process.maxEvents = cms.untracked.PSet( input = cms.untracked.int32(-1) )
-process.MessageLogger.cerr.FwkReport.reportEvery = 1
-process.options = cms.untracked.PSet( wantSummary = cms.untracked.bool(True) )
 
 process.TFileService = cms.Service("TFileService",
    fileName = cms.string('output_alca.root')
 )
+
 process.p = cms.Path(process.HcalIsoTrkAnalyzer)
 
--- Calibration/HcalCalibAlgos/test/isoTrackRecoAnalysis_cfg.py
+++ Calibration/HcalCalibAlgos/test/isoTrackRecoAnalysis_cfg.py
@@ -1,70 +1,21 @@
 import FWCore.ParameterSet.Config as cms
 process = cms.Process('ANALYSIS')
 
+process.load("FWCore.MessageService.MessageLogger_cfi")
 process.load('Configuration.StandardSequences.Services_cff')
 process.load("Configuration.StandardSequences.GeometryRecoDB_cff")
 process.load("Configuration.StandardSequences.MagneticField_cff")
+process.load("RecoLocalCalo.EcalRecAlgos.EcalSeverityLevelESProducer_cfi")
 process.load('Configuration.StandardSequences.FrontierConditions_GlobalTag_cff')
+
 from Configuration.AlCa.autoCond import autoCond
-process.GlobalTag.globaltag='92X_dataRun2_Prompt_v4'
+process.GlobalTag.globaltag='101X_dataRun2_Prompt_v10'
 
-process.MessageLogger = cms.Service("MessageLogger",
-    destinations = cms.untracked.vstring('warnings','errors',
-                                         'cout','cerr'),
-    categories = cms.untracked.vstring('HcalIsoTrack'), 
-    debugModules = cms.untracked.vstring('*'),
-    warnings = cms.untracked.PSet(
-        placeholder = cms.untracked.bool(True)
-    ),
-    default = cms.untracked.PSet(
+if 'MessageLogger' in process.__dict__:
+    process.MessageLogger.categories.append('HcalIsoTrack')
 
-    ),
-    errors = cms.untracked.PSet(
-        placeholder = cms.untracked.bool(True)
-    ),
-    cerr = cms.untracked.PSet(
-        optionalPSet = cms.untracked.bool(True),
-        INFO = cms.untracked.PSet(
-            limit = cms.untracked.int32(0)
-        ),
-        noTimeStamps = cms.untracked.bool(False),
-        FwkReport = cms.untracked.PSet(
-            optionalPSet = cms.untracked.bool(True),
-            reportEvery = cms.untracked.int32(500),
-            limit = cms.untracked.int32(10000000)
-        ),
-        default = cms.untracked.PSet(
-            limit = cms.untracked.int32(10000000)
-        ),
-        Root_NoDictionary = cms.untracked.PSet(
-            optionalPSet = cms.untracked.bool(True),
-            limit = cms.untracked.int32(0)
-        ),
-        FwkJob = cms.untracked.PSet(
-            optionalPSet = cms.untracked.bool(True),
-            limit = cms.untracked.int32(0)
-        ),
-        FwkSummary = cms.untracked.PSet(
-            optionalPSet = cms.untracked.bool(True),
-            reportEvery = cms.untracked.int32(1),
-            limit = cms.untracked.int32(10000000)
-        ),
-        threshold = cms.untracked.string('INFO')
-     ),
-     cout = cms.untracked.PSet(
-        threshold = cms.untracked.string('INFO'),
-        noTimeStamps = cms.untracked.bool(True),
-        INFO = cms.untracked.PSet(
-            limit = cms.untracked.int32(0)
-        ),
-        DEBUG = cms.untracked.PSet(
-            limit = cms.untracked.int32(0)
-        ),
-        HcalIsoTrack = cms.untracked.PSet(
-            limit = cms.untracked.int32(0)
-       )
-    )
-)
+process.MessageLogger.cerr.FwkReport.reportEvery = 1
+process.options = cms.untracked.PSet( wantSummary = cms.untracked.bool(True) )
 
 process.load('RecoLocalCalo.CaloTowersCreator.calotowermaker_cfi')
 process.towerMakerAll = process.calotowermaker.clone()
@@ -77,16 +28,20 @@ process.towerMakerAll.AllowMissingInputs = True
 process.load('Calibration.HcalCalibAlgos.HcalIsoTrkAnalyzer_cfi')
 process.source = cms.Source("PoolSource", 
                             fileNames = cms.untracked.vstring(
-        '/store/data/Run2017A/JetHT/RECO/PromptReco-v2/000/296/168/00000/CE1DFEC8-444C-E711-AB09-02163E019CB1.root'
+        '/store/data/Run2018B/JetHT/ALCARECO/HcalCalIsoTrkFilter-PromptReco-v1/000/317/696/00000/D60EC93B-9870-E811-BAF3-FA163E8DA20D.root',
+#       'file:/afs/cern.ch/work/s/sdey/public/forsunandada/C2F61205-A366-E711-9AFA-02163E01A2B0.root',
     )
 )
 
 process.maxEvents = cms.untracked.PSet( input = cms.untracked.int32(-1) )
-process.MessageLogger.cerr.FwkReport.reportEvery = 1
-process.options = cms.untracked.PSet( wantSummary = cms.untracked.bool(True) )
+
 process.TFileService = cms.Service("TFileService",
    fileName = cms.string('output.root')
 )
-process.HcalIsoTrkAnalyzer.Triggers = []
+
+process.HcalIsoTrkAnalyzer.triggers = []
+process.HcalIsoTrkAnalyzer.dataType = 0 #0 for jetHT else 1
+#process.HcalIsoTrkAnalyzer.useRaw = 2
+
 process.p = cms.Path(process.HcalIsoTrkAnalyzer)
 
