3ea8cb856c202e00ca3b571bd65716f51e4943e3
Giovanni <gpetruc@gmail.com>
2018-08-10 17:43:02 +0200

    PFBadHcalPseudoClusterProducer: activate protection if more than half of the depths are bad (to be tested)

--- RecoParticleFlow/PFClusterProducer/plugins/PFBadHcalPseudoClusterProducer.cc
+++ RecoParticleFlow/PFClusterProducer/plugins/PFBadHcalPseudoClusterProducer.cc
@@ -51,7 +51,6 @@ class PFBadHcalPseudoClusterProducer : public edm::stream::EDProducer<> {
         void produce(edm::Event&, const edm::EventSetup&) override;
 
         bool enabled_;
-        int  thresholdHB_, thresholdHE_;
         bool debug_;
 
         edm::ESHandle<HcalChannelQuality> hQuality_;
@@ -64,10 +63,8 @@ class PFBadHcalPseudoClusterProducer : public edm::stream::EDProducer<> {
 
 PFBadHcalPseudoClusterProducer::PFBadHcalPseudoClusterProducer(const edm::ParameterSet& ps) :
    enabled_(ps.getParameter<bool>("enable")),
-   thresholdHB_(ps.getParameter<int32_t>("thresholdHB")),
-   thresholdHE_(ps.getParameter<int32_t>("thresholdHE")),
-    debug_(ps.getUntrackedParameter<bool>("debug",false)),
-    cacheId_quality_(0), cacheId_geom_(0)
+   debug_(ps.getUntrackedParameter<bool>("debug",false)),
+   cacheId_quality_(0), cacheId_geom_(0)
 {
     produces<std::vector<reco::PFCluster>>();
     produces<std::vector<reco::PFRecHit>>("hits");
@@ -86,7 +83,6 @@ void PFBadHcalPseudoClusterProducer::init(const EventSetup& iSetup)
     edm::ESHandle<HcalChannelQuality> hQuality_;
     iSetup.get<HcalChannelQualityRcd>().get("withTopo",hQuality_);
     const HcalChannelQuality & chanquality = *hQuality_;
-    const HcalTopology & topo = * chanquality.topo();
 
     edm::ESHandle<CaloGeometry> hGeom_;
     iSetup.get<CaloGeometryRecord>().get(hGeom_);
@@ -96,31 +92,29 @@ void PFBadHcalPseudoClusterProducer::init(const EventSetup& iSetup)
 
     int statusMask = ((1<<HcalChannelStatus::HcalCellOff) | (1<<HcalChannelStatus::HcalCellMask) | (1<<HcalChannelStatus::HcalCellDead));
     // histogram the number of bad depths at each ieta, iphi
-    std::map<std::pair<int,int>, int> bads;
+    std::map<std::pair<int,int>, int> good, bads;
     for (const DetId & i : chanquality.getAllChannels()) {
         if (i.det()!=DetId::Hcal) continue; // not an hcal cell
         HcalDetId id = HcalDetId(i);
         if (id.subdet() != HcalBarrel && id.subdet() != HcalEndcap) continue; // we don't deal with HO and HF here
         int status = chanquality.getValues(id)->getValue();
-        if (status == 0) continue;
         if (status & statusMask) {
             bads[std::make_pair(id.ieta(), id.iphi())]++;
             if (debug_) std::cout << "Channel " << i() << " (subdet " << id.subdet() << ", zside " << id.zside() << ", ieta " << id.ieta() << ", iphi " << id.iphi() << " depth " << id.depth() << " has status " << status << "  masked " << (status & statusMask) << std::endl;
+        } else {
+            good[std::make_pair(id.ieta(), id.iphi())]++;
         }
     }
 
-    // determine ieta,iphi locations and which all depths are bad, and make a PFCluster there
-    int maxHE = std::min(topo.maxDepthHE(), thresholdHE_);
-    int maxHB = std::min(topo.maxDepthHB(), thresholdHB_);
-
     const float dummyEnergy = 1e-5; // non-zero, but small (even if it shouldn't ever be used)
     for (const auto & rec : bads) {
-        int ieta = rec.first.first, iphi = rec.first.second, ndepths = rec.second;
-        bool barrel = std::abs(ieta) < 16;
+        int ieta = rec.first.first, iphi = rec.first.second, nbad = rec.second, ngood = good[rec.first];
+        bool barrel = std::abs(ieta) <= 16; // 16 is the largest ieta value for which the first depths are in the barrel.
         HcalSubdetector  subdet = barrel ? HcalBarrel : HcalEndcap;
         HcalDetId id(subdet, ieta, iphi, 1);
-        if (debug_) std::cout << "At ieta " << id.ieta() << ", iphi " << id.iphi() << " I have " << ndepths << " bad depths: " << (ndepths >= (barrel ? maxHB : maxHE) ? " MARK BAD": " ignore") << std::endl;
-        if (ndepths < (barrel ? maxHB : maxHE)) continue;
+        bool isBad = (nbad > 0 && nbad >= ngood);
+        if (debug_) std::cout << "At ieta " << id.ieta() << ", iphi " << id.iphi() << " I have " << nbad << " bad depths, " << ngood << " good depths. " << (isBad ? " MARK BAD": " ignore") << std::endl;
+        if (!isBad) continue;
         const GlobalPoint & pos = caloGeom.getPosition(id);
         PFLayer::Layer layer = (barrel ? PFLayer::HCAL_BARREL1 : PFLayer::HCAL_ENDCAP);
         // make a PF RecHit
@@ -163,8 +157,6 @@ PFBadHcalPseudoClusterProducer::produce(edm::Event& iEvent, const edm::EventSetu
 void PFBadHcalPseudoClusterProducer::fillDescriptions(edm::ConfigurationDescriptions & descriptions) {
     edm::ParameterSetDescription desc;
     desc.add<bool>("enable", true);
-    desc.add<int32_t>("thresholdHB",  1)->setComment("minimum required number of bad depths in HB (not including HO)");
-    desc.add<int32_t>("thresholdHE",  2)->setComment("minimum required number of bad depths in HE");
     desc.addUntracked<bool>("debug", false);
     descriptions.add("particleFlowBadHcalPseudoCluster", desc);
 }
--- RecoParticleFlow/PFClusterProducer/python/particleFlowBadHcalPseudoCluster_cff.py
+++ RecoParticleFlow/PFClusterProducer/python/particleFlowBadHcalPseudoCluster_cff.py
@@ -1,12 +1,8 @@
 from RecoParticleFlow.PFClusterProducer.particleFlowBadHcalPseudoCluster_cfi import *
 
 # OFF by default, turned on via modifier for the moment
-particleFlowBadHcalPseudoCluster.enable = cms.bool(False)
+particleFlowBadHcalPseudoCluster.enable = False
 
-from Configuration.Eras.Modifier_run2_HE_2018_cff import run2_HE_2018
-run2_HE_2018.toModify(particleFlowBadHcalPseudoCluster, thresholdHE = 4)
 from Configuration.Eras.Modifier_PF_badHcalMitigation_cff import PF_badHcalMitigation
-PF_badHcalMitigation.toModify(particleFlowBadHcalPseudoCluster, enable = cms.bool(True))
+PF_badHcalMitigation.toModify(particleFlowBadHcalPseudoCluster, enable = True)
 
-from Configuration.Eras.Modifier_run3_HB_cff import run3_HB
-run3_HB.toModify(particleFlowBadHcalPseudoCluster, thresholdHB = 4, thresholdHE = 4)
