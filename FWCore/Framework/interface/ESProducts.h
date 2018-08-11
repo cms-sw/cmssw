4f7abb5b4f465ed97ce48e8abab72adb710d4536
Chris Jones <chrisdjones15@gmail.com>
2018-08-11 09:02:12 -0500

    Removed '<<' operations for creating ESProducts
    The ability to use variadic templates with the edm::es::products
    function means it can now handle arbitrary numbers of arguments.
    The '<<' mechanism required lots of additional classes to function
    and is not as efficient as the new version of edm::es::products.

--- FWCore/Framework/interface/ESProducts.h
+++ FWCore/Framework/interface/ESProducts.h
@@ -12,7 +12,7 @@
  Usage:
     This is used as a return type from the produce method of an ESProducer.  Users are not anticipated to ever need
  to directly use this class.  Instead, to create an instance of the class at method return time, they should call
- the helper function es::products or 'output' the values to es::produced via the '<<' operator.
+ the helper function es::products.
 
 */
 //
@@ -30,85 +30,16 @@
 namespace edm {
    namespace eventsetup {      
       namespace produce {
-         struct Produce {
-            Produce() {}
-         };
-         
-         template< typename T> struct OneHolder {
-            OneHolder() {}
-            OneHolder(T iValue) : value_(std::move(iValue)) {}
-
-            template<typename S>
-            void setFromRecursive(S& iGiveValues) {
-               iGiveValues.setFrom(value_);
-            }
-            
-            void moveTo(T& oValue) { oValue = std::move(value_);}
-            T value_;
-            typedef T tail_type;
-            typedef Null head_type;
-         };
-
-         template<typename T> struct OneHolder<std::unique_ptr<T>> {
-            typedef std::unique_ptr<T> Type;
-            OneHolder() {}
-            OneHolder(OneHolder<T> const& ) = delete;
-            OneHolder(OneHolder<Type>&& iOther): value_(std::move(iOther.value_)) {}
-            OneHolder(Type iPtr): value_(std::move(iPtr)) {}
-            
-            
-            OneHolder<Type> const& operator=(OneHolder<Type> iRHS) { value_ = std::move(iRHS.value_); return *this; }
-            template<typename S>
-            void setFromRecursive(S& iGiveValues) {
-               iGiveValues.setFrom(value_);
-            }
-            
-            void moveTo(Type& oValue) { oValue = std::move(value_);}
-            mutable Type value_; //mutable needed for std::unique_ptr
-            typedef Type tail_type;
-            typedef Null head_type;
-         };
-         
-        template<typename T> OneHolder<std::remove_reference_t<T>> operator<<(const Produce&, T&& iValue) {
-            return OneHolder<std::remove_reference_t<T>>(std::forward<T>(iValue));
-         }
-
-         template<typename T, typename U> struct MultiHolder {
-            MultiHolder(const T& iT, U iValue): value_(std::move(iValue)), head_(iT) {
-            }
-            
-            template<typename TTaker>
-            void setFromRecursive(TTaker& iGiveValues) {
-               iGiveValues.setFrom(value_);
-               head_.setFromRecursive(iGiveValues);
-            }
-            
-            
-            void moveTo(U& oValue) { oValue = std::move(value_);}
-            U value_;
-            T head_;
-            
-            typedef U tail_type;
-            typedef T head_type;
-         };
-
-         template<typename T, typename S>
-            MultiHolder<OneHolder<T>, std::remove_reference_t<S>> operator<<(OneHolder<T>&& iHolder,
-                                                    S&& iValue) {
-              return MultiHolder<OneHolder<T>, std::remove_reference_t<S>>(std::forward<OneHolder<T>>(iHolder), std::forward<S>(iValue));
-            }
-         template< typename T, typename U, typename V>
-            MultiHolder< MultiHolder<T, U>, std::remove_reference_t<V> >
-            operator<<(MultiHolder<T,U>&& iHolder, V&& iValue) {
-              return MultiHolder< MultiHolder<T, U>, std::remove_reference_t<V>> (std::forward<MultiHolder<T,U>>(iHolder), std::forward<V>(iValue));
-            }
-        
          template<typename T1, typename... TArgs>
             struct ProductHolder : public ProductHolder<TArgs...> {
-               typedef ProductHolder<TArgs...> parent_type;
+              using parent_type = ProductHolder<TArgs...>;
               
               ProductHolder() : value() {}
-               
+              ProductHolder(ProductHolder<T1,TArgs...>&&) = default;
+              ProductHolder(ProductHolder<T1,TArgs...> const&) = default;
+              ProductHolder<T1, TArgs...>& operator=(ProductHolder<T1,TArgs...>&&) = default;
+              ProductHolder<T1, TArgs...>& operator=(ProductHolder<T1,TArgs...> const&) = default;
+
                template<typename T>
                   void setAllValues(T& iValuesFrom) {
                      iValuesFrom.setFromRecursive(*this);
@@ -118,6 +49,7 @@ namespace edm {
 
                using parent_type::setFrom;
                void setFrom(T1& iValue) { value = iValue ; }
+               void setFrom(T1&& iValue) { value = std::move(iValue) ; }
 
                template<typename T>
                   void setFromRecursive(T& iValuesTo) {
@@ -132,14 +64,18 @@ namespace edm {
                   }
                T1 value;
                
-               typedef T1 tail_type;
-               typedef parent_type head_type;
+               using tail_type = T1;
+               using head_type = parent_type;
             };
          
          template<typename T1>
             struct ProductHolder<T1> {
                
               ProductHolder() : value() {}
+              ProductHolder(ProductHolder<T1>&&) = default;
+              ProductHolder(ProductHolder<T1> const&) = default;
+              ProductHolder<T1>& operator=(ProductHolder<T1>&&) = default;
+              ProductHolder<T1>& operator=(ProductHolder<T1> const&) = default;
 
               template<typename T>
                void setAllValues(T& iValuesFrom) {
@@ -147,6 +83,7 @@ namespace edm {
                }
                void moveTo(T1& oValue) { oValue = std::move(value); }
                void setFrom(T1& iValue) { value = iValue ; }
+               void setFrom(T1&& iValue) { value = std::move(iValue) ; }
                template<typename T>
                void moveToRecursive(T& iValuesTo) {
                   iValuesTo.moveTo(value);
@@ -157,8 +94,8 @@ namespace edm {
                }
                T1 value;
                
-               typedef T1 tail_type;
-               typedef Null head_type;
+               using tail_type = T1;
+               using head_type = Null;
             };
          
       }
@@ -170,25 +107,27 @@ namespace edm {
       typedef eventsetup::produce::ProductHolder<TArgs...> parent_type;
       template<typename... S>
       ESProducts(ESProducts<S...>&& iProducts) {
-        parent_type::setAllValues(iProducts);
+         parent_type::setAllValues(iProducts);
       }
       template<typename T>
       /*explicit*/ ESProducts(T&& iValues) {
-        parent_type::setAllValues(iValues);
+         parent_type::setAllValues(iValues);
       }
       template<typename ...Vars>
-      ESProducts(ESFillDirectly, Vars... vars) {
-        (this->setFrom(vars), ...);
+      ESProducts(ESFillDirectly, Vars&&... vars) {
+         (this->setFrom(std::forward<Vars>(vars)), ...);
       }
-     
+
+      ESProducts(ESProducts<TArgs...> const&) = default;
+      ESProducts(ESProducts<TArgs...>&&) = default;
+      ESProducts<TArgs...>& operator=(ESProducts<TArgs...> const&) = default;
+      ESProducts<TArgs...>& operator=(ESProducts<TArgs...>&&) = default;
    };
 
    namespace es {
-      extern const eventsetup::produce::Produce produced;
-
       template<typename ...TArgs>
       ESProducts<std::remove_reference_t<TArgs>...> products(TArgs&&... args) {
-        return ESProducts<std::remove_reference_t<TArgs>...>(edm::ESFillDirectly{}, std::forward<TArgs>(args)...);
+         return ESProducts<std::remove_reference_t<TArgs>...>(edm::ESFillDirectly{}, std::forward<TArgs>(args)...);
       }
    }
 
