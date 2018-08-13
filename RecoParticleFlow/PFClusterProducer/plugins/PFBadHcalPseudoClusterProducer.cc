20486e0f389205cb40e3ec31c439135462986253
Giovanni <gpetruc@gmail.com>
2018-08-13 11:25:37 +0200

    PFBadHcalPseudoClusterProduce: figure out first depth and subdetector from ieta, iphi

--- RecoParticleFlow/PFClusterProducer/plugins/PFBadHcalPseudoClusterProducer.cc
+++ RecoParticleFlow/PFClusterProducer/plugins/PFBadHcalPseudoClusterProducer.cc
@@ -93,32 +93,39 @@ void PFBadHcalPseudoClusterProducer::init(const EventSetup& iSetup)
     int statusMask = ((1<<HcalChannelStatus::HcalCellOff) | (1<<HcalChannelStatus::HcalCellMask) | (1<<HcalChannelStatus::HcalCellDead));
     // histogram the number of bad depths at each ieta, iphi
     std::map<std::pair<int,int>, int> good, bads;
+    std::map<std::pair<int,int>, std::pair<int,HcalSubdetector>> minDepths;
     for (const DetId & i : chanquality.getAllChannels()) {
         if (i.det()!=DetId::Hcal) continue; // not an hcal cell
         HcalDetId id = HcalDetId(i);
         if (id.subdet() != HcalBarrel && id.subdet() != HcalEndcap) continue; // we don't deal with HO and HF here
         int status = chanquality.getValues(id)->getValue();
+        auto tower = std::make_pair(id.ieta(), id.iphi());
         if (status & statusMask) {
-            bads[std::make_pair(id.ieta(), id.iphi())]++;
+            bads[tower]++;
             if (debug_) std::cout << "Channel " << i() << " (subdet " << id.subdet() << ", zside " << id.zside() << ", ieta " << id.ieta() << ", iphi " << id.iphi() << " depth " << id.depth() << " has status " << status << "  masked " << (status & statusMask) << std::endl;
         } else {
-            good[std::make_pair(id.ieta(), id.iphi())]++;
+            good[tower]++;
+        }
+        auto & minD = minDepths[tower];
+        if (minD.second == HcalEmpty || minD.first > id.depth()) {
+            minD.first  = id.depth();
+            minD.second = id.subdet();
         }
     }
 
     const float dummyEnergy = 1e-5; // non-zero, but small (even if it shouldn't ever be used)
     for (const auto & rec : bads) {
         int ieta = rec.first.first, iphi = rec.first.second, nbad = rec.second, ngood = good[rec.first];
-        bool barrel = std::abs(ieta) <= 16; // 16 is the largest ieta value for which the first depths are in the barrel.
-        HcalSubdetector  subdet = barrel ? HcalBarrel : HcalEndcap;
-        HcalDetId id(subdet, ieta, iphi, 1);
+        auto minDepth = minDepths[rec.first];   
+        bool barrel = minDepth.second == HcalBarrel;
+        HcalDetId id(minDepth.second, ieta, iphi, minDepth.first);
         bool isBad = (nbad > 0 && nbad >= ngood);
-        if (debug_) std::cout << "At ieta " << id.ieta() << ", iphi " << id.iphi() << " I have " << nbad << " bad depths, " << ngood << " good depths. " << (isBad ? " MARK BAD": " ignore") << std::endl;
+        if (debug_) std::cout << "At ieta " << id.ieta() << ", iphi " << id.iphi() << " I have " << nbad << " bad depths, " << ngood << " good depths. First depth is in " << (barrel ? "HB" : "HE") << " depth " << minDepth.first <<"; " << (isBad ? " MARK BAD": " ignore") << std::endl;
         if (!isBad) continue;
-        const GlobalPoint & pos = caloGeom.getPosition(id);
         PFLayer::Layer layer = (barrel ? PFLayer::HCAL_BARREL1 : PFLayer::HCAL_ENDCAP);
         // make a PF RecHit
         std::shared_ptr<const CaloCellGeometry> thisCell = (barrel ? hbGeom : heGeom)->getGeometry(id);
+        const GlobalPoint & pos = thisCell->getPosition();
         badAreasRH_.emplace_back( thisCell, id(), layer, dummyEnergy );
         // make a PF cluster (but can't add the rechit, as for that I need an edm::Ref)
         badAreasC_.emplace_back( layer, dummyEnergy, pos.x(), pos.y(), pos.z() );
