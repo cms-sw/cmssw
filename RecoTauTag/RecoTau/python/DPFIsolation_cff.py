32dd010f6eb30119235b291bf9378f8770c1fbf4
Owen <ocolegro@physics.ucsb.edu>
2018-08-13 06:57:17 -0500

    Building dpf isolation module

---
+++ RecoTauTag/RecoTau/python/DPFIsolation_cff.py
@@ -0,0 +1,10 @@
+
+import FWCore.ParameterSet.Config as cms
+
+DPFIsolation = cms.EDProducer("DPFIsolation",
+    pfcands     = cms.InputTag('packedPFCandidates'),
+    taus 	= cms.InputTag('slimmedTaus'),
+    graph_file  = cms.string('RecoTauTag/RecoTau/data/DPFIsolation_2017v0.pb')
+)
+
+DPFIsolationSeq = cms.Sequence(DPFIsolation)
