8b1fc2f874121098a68fb921376fa319c2e9c240
abaty <abaty@mit.edu>
2018-08-13 21:31:44 +0200

    Changed back to keeping pixelTracks in pp_on_AA era

--- Configuration/Eras/python/Era_Run2_2018_pp_on_AA_wPixelTrk_cff.py
+++
@@ -1,7 +0,0 @@
-import FWCore.ParameterSet.Config as cms
-
-from Configuration.Eras.Era_Run2_2018_cff import Run2_2018
-from Configuration.Eras.Modifier_pp_on_AA_2018_cff import pp_on_AA_2018
-from Configuration.Eras.Modifier_pp_on_AA_2018_wPixelTrk_cff import pp_on_AA_2018_wPixelTrk
-
-Run2_2018_pp_on_AA_wPixelTrk = cms.ModifierChain(Run2_2018, pp_on_AA_2018, pp_on_AA_2018_wPixelTrk)
