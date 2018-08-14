c0a655e80ae22c91b5e6d7ae8f90f5a7e896da94
Andrea Bocci <andrea.bocci@cern.ch>
2018-08-14 17:30:35 +0200

    Clean up some non-GPU-related pixel tracking code
    Cleanup whitespaces and indenttion, plugin definitions, includes, and file names.
    
    Backport from the Patatrack fork: cms-patatrack#49, cms-patatrack#122, cms-patatrack#134.

--- RecoLocalTracker/SiPixelRecHits/python/SiPixelRecHits_cfi.py
+++ RecoLocalTracker/SiPixelRecHits/python/SiPixelRecHits_cfi.py
@@ -1,10 +1,9 @@
 import FWCore.ParameterSet.Config as cms
-from Configuration.ProcessModifiers.gpu_cff import gpu
 
 siPixelRecHits = cms.EDProducer("SiPixelRecHitConverter",
     src = cms.InputTag("siPixelClusters"),
     CPE = cms.string('PixelCPEGeneric'),
-    VerboseLevel = cms.untracked.int32(0),
+    VerboseLevel = cms.untracked.int32(0)
 )
 
 siPixelRecHitsPreSplitting = siPixelRecHits.clone(
