bb9238132c134f51981a9ca7158729c13ae3345e
David <David.Lange@cern.ch>
2018-08-15 17:09:13 +0200

    python3 compiling cleanup

--- PhysicsTools/PythonAnalysis/examples/MCTruth.py
+++ PhysicsTools/PythonAnalysis/examples/MCTruth.py
@@ -1,8 +1,9 @@
+from __future__ import print_function
 from PhysicsTools.PythonAnalysis import *
 from ROOT import *
 # prepare the FWLite autoloading mechanism
 gSystem.Load("libFWCoreFWLite.so")
-FWLiteEnabler::enable()
+ROOT.FWLiteEnabler.enable()
 
 # load the file with the generator output
 theFile = TFile("generatorOutput.root")
@@ -31,4 +32,4 @@ for index in all(events):
 
     # do something with the data
     genEvent = source.GetEvent();
-    print genEvent.event_number()
+    print(genEvent.event_number())
