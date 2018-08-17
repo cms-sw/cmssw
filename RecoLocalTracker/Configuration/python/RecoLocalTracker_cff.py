6d7863458006caab23b2cf2f230b9adde7ca1c46
Andrea Bocci <andrea.bocci@cern.ch>
2018-08-17 13:18:10 +0200

    Cleanup after merging with CMSSW 10.2.2 (cms-patatrack#134)
    Clean up unnecessary changes, whitespaces, defines and include directives.

--- RecoLocalTracker/Configuration/python/RecoLocalTracker_cff.py
+++ RecoLocalTracker/Configuration/python/RecoLocalTracker_cff.py
@@ -17,18 +17,15 @@ pixeltrackerlocalreco = cms.Sequence(siPixelClustersPreSplitting*siPixelRecHitsP
 striptrackerlocalreco = cms.Sequence(siStripZeroSuppression*siStripClusters*siStripMatchedRecHits)
 trackerlocalreco = cms.Sequence(pixeltrackerlocalreco*striptrackerlocalreco*clusterSummaryProducer)
 
-
 from RecoLocalTracker.SiPixelClusterizer.siPixelClustersHeterogeneous_cfi import *
 from RecoLocalTracker.SiPixelClusterizer.siPixelFedCablingMapGPUWrapper_cfi import *
 from CalibTracker.SiPixelESProducers.siPixelGainCalibrationForHLTGPU_cfi import *
 
+from Configuration.ProcessModifiers.gpu_cff import gpu
 from RecoLocalTracker.SiPixelRecHits.siPixelRecHitHeterogeneous_cfi import *
 from RecoLocalTracker.SiPixelRecHits.siPixelRecHitHeterogeneousConverter_cfi import siPixelRecHitHeterogeneousConverter as _siPixelRecHitHeterogeneousConverter
 gpu.toReplaceWith(siPixelRecHitsPreSplitting, _siPixelRecHitHeterogeneousConverter.clone())
 
-
-
-from Configuration.ProcessModifiers.gpu_cff import gpu
 _pixeltrackerlocalreco_gpu = pixeltrackerlocalreco.copy()
 _pixeltrackerlocalreco_gpu.replace(siPixelClustersPreSplitting, siPixelClustersHeterogeneous+siPixelClustersPreSplitting)
 _pixeltrackerlocalreco_gpu.replace(siPixelRecHitsPreSplitting, siPixelRecHitHeterogeneous+siPixelRecHitsPreSplitting)
