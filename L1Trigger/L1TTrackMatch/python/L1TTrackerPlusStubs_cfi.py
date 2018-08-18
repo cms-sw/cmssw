64901333666a91541fa185582142999f59d283aa
Michail <bachtis@cern.ch>
2018-08-17 17:23:58 -0700

    Fixing python file

--- L1Trigger/L1TTrackMatch/python/L1TTrackerPlusStubs_cfi.py
+++ L1Trigger/L1TTrackMatch/python/L1TTrackerPlusStubs_cfi.py
@@ -2,7 +2,7 @@ import FWCore.ParameterSet.Config as cms
 
 l1StubMatchedMuons = cms.EDProducer("L1TTrackerPlusStubsProducer",
     srcStubs = cms.InputTag("simKBmtfStubs"),
-    srcTracks = cms.InputTag("TTTracksFromTracklet"),
+    srcTracks = cms.InputTag("TTTracksFromTracklet:Level1TTTracks"),
 
     trackMatcherSettings = cms.PSet(
         sectorsToProcess = cms.vint32(0,1,2,3,4,5,6,7,8,9,10,11),
