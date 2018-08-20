f5401dc50f8cc7c34c5d3057a19add77487f3ccb
Christopher Jones <chrisdjones15@gmail.com>
2018-08-20 14:06:41 -0500

    Separate options for checking and throwing

--- DataFormats/TrackerRecHit2D/interface/BaseTrackerRecHit.h
+++ DataFormats/TrackerRecHit2D/interface/BaseTrackerRecHit.h
@@ -8,8 +8,8 @@
 #include "DataFormats/GeometrySurface/interface/Surface.h" 
 
 
-
-// #define VI_DEBUG
+//#define DO_INTERNAL_CHECKS_BTR
+//#define VI_DEBUG
 
 class OmniClusterRef;
 
@@ -134,7 +134,7 @@ private:
 
 #ifdef VI_DEBUG
   void check() const { assert(det());}
-#elif DO_THROW_UNINITIALIZED
+#elif defined(DO_INTERNAL_CHECKS_BTR)
   void check() const;
 #else 
   static void check(){}
