4978ce6ba508912f8f882158bcd935fd0c40babf
Luca <luca.pernie@cern.ch>
2018-08-22 18:18:51 +0200

    JEC MC

--- Configuration/AlCa/python/autoCond.py
+++ Configuration/AlCa/python/autoCond.py
@@ -40,19 +40,19 @@ autoCond = {
     # GlobalTag for Run2 HLT for HI: it points to the online GT
     'run2_hlt_hi'       :   '101X_dataRun2_HLTHI_frozen_v7',
     # GlobalTag for MC production with perfectly aligned and calibrated detector for Phase1 2017 (and 0,0,~0-centred beamspot)
-    'phase1_2017_design'       :  '102X_mc2017_design_IdealBS_v3',
+    'phase1_2017_design'       :  '102X_mc2017_design_IdealBS_v4',
     # GlobalTag for MC production with realistic conditions for Phase1 2017 detector
-    'phase1_2017_realistic'    : '102X_mc2017_realistic_v3',
+    'phase1_2017_realistic'    : '102X_mc2017_realistic_v4',
     # GlobalTag for MC production (cosmics) with realistic alignment and calibrations for Phase1 2017 detector, Strip tracker in DECO mode
-    'phase1_2017_cosmics'      : '102X_mc2017cosmics_realistic_deco_v3',
+    'phase1_2017_cosmics'      : '102X_mc2017cosmics_realistic_deco_v4',
     # GlobalTag for MC production (cosmics) with realistic alignment and calibrations for Phase1 2017 detector, Strip tracker in PEAK mode
-    'phase1_2017_cosmics_peak' : '102X_mc2017cosmics_realistic_peak_v3',
+    'phase1_2017_cosmics_peak' : '102X_mc2017cosmics_realistic_peak_v4',
     # GlobalTag for MC production with perfectly aligned and calibrated detector for full Phase1 2018 (and 0,0,0-centred beamspot)
-    'phase1_2018_design'       : '102X_upgrade2018_design_v6',
+    'phase1_2018_design'       : '102X_upgrade2018_design_v7',
     # GlobalTag for MC production with realistic conditions for full Phase1 2018 detector
-    'phase1_2018_realistic'    : '102X_upgrade2018_realistic_v11',
+    'phase1_2018_realistic'    : '102X_upgrade2018_realistic_v12',
     # GlobalTag for MC production (cosmics) with realistic conditions for full Phase1 2018 detector,  Strip tracker in DECO mode
-    'phase1_2018_cosmics'      :   '102X_upgrade2018cosmics_realistic_deco_v10',
+    'phase1_2018_cosmics'      :   '102X_upgrade2018cosmics_realistic_deco_v11',
     # GlobalTag for MC production with perfectly aligned and calibrated detector for Phase1 2019
     'phase1_2019_design'       : '102X_postLS2_design_v5', # GT containing design conditions for postLS2
     # GlobalTag for MC production with perfectly aligned and calibrated detector for Phase1 2019
