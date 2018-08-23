43367a20a917d50264adfc991b700ac2f810ed4f
mmusich <marco.musich@cern.ch>
2018-08-23 16:12:44 +0200

    change to non-verbose

--- Calibration/TkAlCaRecoProducers/python/CalibrationTrackSelectorFromDetIdList_cfi.py
+++ Calibration/TkAlCaRecoProducers/python/CalibrationTrackSelectorFromDetIdList_cfi.py
@@ -2,7 +2,7 @@ import FWCore.ParameterSet.Config as cms
 
 CalibrationTrackSelectorFromDetIdList = cms.EDProducer("CalibrationTrackSelectorFromDetIdList",
                                                        Input = cms.InputTag("generalTracks"),
-                                                       verbose = cms.untracked.bool(True),
+                                                       verbose = cms.untracked.bool(False),
                                                        selections = cms.VPSet(cms.PSet(selection=cms.untracked.vstring("0x1e0c0000-0x1c040000")),    # TEC minus
                                                                               cms.PSet(selection=cms.untracked.vstring("0x1e0c0000-0x1c080000")),    # TEC plus
                                                                               cms.PSet(selection=cms.untracked.vstring("0x1e000000-0x1a000000")),    # TOB
