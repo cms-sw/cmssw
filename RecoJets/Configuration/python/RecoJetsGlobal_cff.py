709e303e56b06c22a0fedd8f24eddbc3d98ba56a
Stepan <stepobr@cern.ch>
2018-08-24 18:57:20 +0200

    remove pa_2016 modifier for hiFJGridEmptyAreaCalculator

--- RecoJets/Configuration/python/RecoJetsGlobal_cff.py
+++ RecoJets/Configuration/python/RecoJetsGlobal_cff.py
@@ -11,13 +11,10 @@ from JetMETCorrections.Configuration.JetCorrectorsForReco_cff import *
 jetGlobalReco = cms.Sequence(recoJets*recoJetIds*recoTrackJets)
 jetHighLevelReco = cms.Sequence(recoPFJets*jetCorrectorsForReco*recoJetAssociations*recoJetAssociationsExplicit*recoJPTJets)
 
-from Configuration.Eras.Modifier_pA_2016_cff import pA_2016
-#HI-specific algorithms needed in pp scenario special configurations
 from RecoHI.HiJetAlgos.hiFJGridEmptyAreaCalculator_cff import hiFJGridEmptyAreaCalculator
-pA_2016.toModify(hiFJGridEmptyAreaCalculator, doCentrality = False)
-
 from RecoHI.HiJetAlgos.hiFJRhoProducer import hiFJRhoProducer
 from RecoHI.HiJetAlgos.HiRecoPFJets_cff import kt4PFJetsForRho
+from Configuration.Eras.Modifier_pA_2016_cff import pA_2016
 from RecoHI.HiCentralityAlgos.pACentrality_cfi import pACentrality
 pA_2016.toModify(pACentrality, producePixelTracks = False)
 
