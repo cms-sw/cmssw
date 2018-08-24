a2822ade7ea2b4e1daac19bf47addcda35acdbcf
tocheng <tocheng.cms@gmail.com>
2018-08-25 00:27:36 +0200

    correct JEC/JER and CTPPS geometry for data GTs

--- Configuration/AlCa/python/autoCond.py
+++ Configuration/AlCa/python/autoCond.py
@@ -24,11 +24,11 @@ autoCond = {
     # GlobalTag for MC production (p-Pb collisions) with realistic alignment and calibrations for Run2
     'run2_mc_pa'        :   '103X_mcRun2_pA_v1',
     # GlobalTag for Run1 data reprocessing
-    'run1_data'         :   '103X_dataRun2_v1',
+    'run1_data'         :   '103X_dataRun2_v2',
     # GlobalTag for Run2 data reprocessing
-    'run2_data'         :   '103X_dataRun2_v1',
+    'run2_data'         :   '103X_dataRun2_v2',
     # GlobalTag for Run2 data relvals: allows customization to run with fixed L1 menu
-    'run2_data_relval'  :   '103X_dataRun2_relval_v3',
+    'run2_data_relval'  :   '103X_dataRun2_relval_v4',
     # GlobalTag for Run2 data 2016H relvals only: Prompt Conditions + fixed L1 menu (to be removed)
     'run2_data_promptlike' : '103X_dataRun2_PromptLike_v3',
     # GlobalTag for Run1 HLT: it points to the online GT
@@ -36,7 +36,7 @@ autoCond = {
     # GlobalTag for Run2 HLT: it points to the online GT
     'run2_hlt'          :   '101X_dataRun2_HLT_frozen_v6',
     # GlobalTag for Run2 HLT RelVals: customizations to run with fixed L1 Menu
-    'run2_hlt_relval'   :   '103X_dataRun2_HLT_relval_v2',
+    'run2_hlt_relval'   :   '103X_dataRun2_HLT_relval_v3',
     # GlobalTag for Run2 HLT for HI: it points to the online GT
     'run2_hlt_hi'       :   '101X_dataRun2_HLTHI_frozen_v7',
     # GlobalTag for MC production with perfectly aligned and calibrated detector for Phase1 2017 (and 0,0,~0-centred beamspot)
