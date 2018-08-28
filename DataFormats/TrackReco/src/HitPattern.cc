95dbe756a9bbee8cbb6bdcd4e27bee1ba30c92a1
lgray <lindsey.gray@gmail.com>
2018-08-28 09:51:56 -0500

    extend the hit pattern class to include MTD hits

--- DataFormats/TrackReco/src/HitPattern.cc
+++ DataFormats/TrackReco/src/HitPattern.cc
@@ -5,6 +5,8 @@
 #include "DataFormats/MuonDetId/interface/RPCDetId.h"
 #include "DataFormats/MuonDetId/interface/GEMDetId.h"
 #include "DataFormats/MuonDetId/interface/ME0DetId.h"
+#include "DataFormats/ForwardDetId/interface/BTLDetId.h"
+#include "DataFormats/ForwardDetId/interface/ETLDetId.h"
 
 #include "DataFormats/TrackerCommon/interface/TrackerTopology.h"
 
@@ -133,6 +135,29 @@ namespace {
         }
         return layer;
     }
+  uint16_t encodeTimingLayer(const DetId& id) {
+    uint16_t detid = id.det();
+    uint16_t subdet = id.subdetId();    
+    uint16_t layer = 0x0;
+    if( detid == DetId::Forward && subdet == FastTime ) {
+      MTDDetId mtdid(id);
+      switch( mtdid.mtdSubDetector() ) {
+      case MTDDetId::BTL:
+	layer = BTLDetId(id).modType();
+	break;
+      case MTDDetId::ETL:
+	layer = ETLDetId(id).mtdRR();
+	break;
+      default:
+	throw cms::Exception("HitPattern")
+	  << "Invalid MTD Subdetector " << mtdid.mtdSubDetector() << "!";
+      }	
+    } else {
+      throw cms::Exception("HitPattern")
+	<< "Invalid DetId for FastTime det= " <<detid << " subdet= " << subdet  << "!";
+    }    
+    return layer;
+  }
 }
 
 uint16_t HitPattern::encode(const DetId &id, TrackingRecHit::Type hitType, const TrackerTopology& ttopo)
@@ -146,14 +171,24 @@ uint16_t HitPattern::encode(const DetId &id, TrackingRecHit::Type hitType, const
         layer = ttopo.layer(id);
     } else if (detid == DetId::Muon) {
         layer = encodeMuonLayer(id);
+    } else if( detid == DetId::Forward && subdet == FastTime ) {
+        layer = encodeTimingLayer(id);
     }
 
     // adding mono/stereo bit
     uint16_t side = 0x0;
     if (detid == DetId::Tracker) {
         side = isStereo(id, ttopo);
-    } else if (detid == DetId::Muon) {
+    } else if (detid == DetId::Muon || (detid == DetId::Forward && subdet == FastTime) ) {
         side = 0x0;
+    }
+
+    // juggle the detid around to deal with the fact the bitwidth is larger
+    // DetId::Muon is 2 and DetId::Forward is 6, must map to 0 and 2 respectively
+    if( detid == DetId::Muon ) {
+      detid = 0; // DetId::Muon is 2 and needs to be reordered to match old encoding where it got masked
+    } else if( detid == DetId::Forward && subdet == FastTime ) {
+      detid = 2; // since DetId::Forward is some other number, reorder it here
     }
 
     return encode(detid, subdet, layer, side, hitType);
@@ -162,13 +197,13 @@ uint16_t HitPattern::encode(const DetId &id, TrackingRecHit::Type hitType, const
 uint16_t HitPattern::encode(uint16_t det, uint16_t subdet, uint16_t layer, uint16_t side, TrackingRecHit::Type hitType) {
     uint16_t pattern = HitPattern::EMPTY_PATTERN;
 
-    // adding tracker/muon detector bit
+    // adding tracker/muon/mtd detector bits
     pattern |= (det & SubDetectorMask) << SubDetectorOffset;
 
-    // adding substructure (PXB, PXF, TIB, TID, TOB, TEC, or DT, CSC, RPC,GEM) bits
+    // adding substructure (PXB, PXF, TIB, TID, TOB, TEC, or DT, CSC, RPC,GEM, or BTL, ETL) bits
     pattern |= (subdet & SubstrMask) << SubstrOffset;
 
-    // adding layer/disk/wheel bits
+    // adding layer/disk/wheel/ring/modType bits
     pattern |= (layer & LayerMask) << LayerOffset;
 
     // adding mono/stereo bit
@@ -839,6 +874,8 @@ void HitPattern::printHitPattern(HitCategory category, int position, std::ostrea
         stream << "muon";
     } else if (trackerHitFilter(pattern)) {
         stream << "tracker";
+    } else if (timingHitFilter(pattern)) {
+        stream << "timing";
     }
 
     stream << "\tsubstructure " << getSubStructure(pattern);
@@ -860,6 +897,8 @@ void HitPattern::printHitPattern(HitCategory category, int position, std::ostrea
 	  stream << "(UNKNOWN Muon SubStructure!) \tsubsubstructure "
 		 << getSubStructure(pattern);
         }
+    } else if( timingHitFilter(pattern) ) {
+        stream << "\tdetector " << getSubStructure(pattern);
     } else {
         stream << "\tlayer " << getLayer(pattern);
     }
