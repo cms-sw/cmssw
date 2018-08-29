a3eb03b490f77b3066a098f18de213e679c584c0
Vladimir <vladimir.rekovic@cern.ch>
2018-08-29 14:54:07 +0200

    Revert "Change LogWarning to LogDebug for EMTF emulator in case in finds more then 2 CSC TPs in the same chamber."
    This reverts commit c2a1a11602400d1a84ad56aea76c3acd5cb7c12f.

--- L1Trigger/L1TMuonEndCap/src/PrimitiveSelection.cc
+++ L1Trigger/L1TMuonEndCap/src/PrimitiveSelection.cc
@@ -83,17 +83,17 @@ void PrimitiveSelection::process(
         selected_csc_map[selected_csc].push_back(new_tp);
       }
       else {
-        LogDebug("L1T") << "\n******************* EMTF EMULATOR: SUPER-BIZZARE CASE *******************";
-        LogDebug("L1T") << "Found 3 CSC trigger primitives in the same chamber";
+        edm::LogWarning("L1T") << "\n******************* EMTF EMULATOR: SUPER-BIZZARE CASE *******************";
+        edm::LogWarning("L1T") << "Found 3 CSC trigger primitives in the same chamber";
         for (int ii = 0; ii < 3; ii++) {
           TriggerPrimitive tp_err = (ii < 2 ? selected_csc_map[selected_csc].at(ii) : new_tp);
-          LogDebug("L1T") << "LCT #" << ii+1 << ": BX " << tp_err.getBX()
+          edm::LogWarning("L1T") << "LCT #" << ii+1 << ": BX " << tp_err.getBX()
                     << ", endcap " << tp_err.detId<CSCDetId>().endcap() << ", sector " << tp_err.detId<CSCDetId>().triggerSector()
                     << ", station " << tp_err.detId<CSCDetId>().station() << ", ring " << tp_err.detId<CSCDetId>().ring()
                     << ", chamber " << tp_err.detId<CSCDetId>().chamber() << ", CSC ID " << tp_err.getCSCData().cscID
                     << ": strip " << tp_err.getStrip() << ", wire " << tp_err.getWire();
         }
-        LogDebug("L1T") << "************************* ONLY KEEP FIRST TWO *************************\n\n";
+        edm::LogWarning("L1T") << "************************* ONLY KEEP FIRST TWO *************************\n\n";
       }
 
     } // End conditional: if (selected_csc >= 0)
