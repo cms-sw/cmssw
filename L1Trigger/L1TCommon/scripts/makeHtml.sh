17c0aeb296b62aead6f6b8a1a058a86fc2614820
danielguerrero <daniel.guerrero@cern.ch>
2018-08-29 10:44:47 +0200

    Updating root macro and script for plotting and making html page

--- L1Trigger/L1TCommon/scripts/makeHtml.sh
+++ L1Trigger/L1TCommon/scripts/makeHtml.sh
@@ -25,6 +25,8 @@ compDir2=$2
 #compStr2=sed -e 's/.*.\///' $compDir2
 
 directory=`pwd`
+agreeDirectory=`pwd`/inAgrement
+mkdir $agreeDirectory
 
 ## convert all the .pdf files to .png
 find . -type f -name '*.pdf' -print0 |
@@ -32,9 +34,43 @@ find . -type f -name '*.pdf' -print0 |
       do convert -verbose "${file}" "${file%.*}.png"
   done
 
+# move all the plots in agreement in separate directory
+mv agree* $agreeDirectory
+
 sleep 1
 
 tempDate=`date "+%Y-%m-%d"`;
+
+# Make webpage for plots in agreement
+outputFileAgreement=inAgrement/inAgreement_$tempDate.htm
+>$outputFileAgreement
+cat <<EOF >>$outputFileAgreement
+
+<HTML>
+
+<HEAD><TITLE> Result WebPages for $tempDate</TITLE></HEAD>
+
+<BODY link="Red">
+<FONT color="Black">
+<h2><A name="EB"><FONT color="Black"> Results  Web Pages for $tempDate</FONT></A><BR></h2>
+<h3><A name="EB"><FONT color="Blue">Result Agreeing Histograms: </FONT></A><BR></h3>
+<h3><A name="EB"><FONT color="Blue">comparing </FONT><FONT color="Black">$compDir1 (line)</FONT> <FONT color="Blue"> and </FONT> <FONT color="Black">$compDir2 (points)</FONT></A><BR></h3>
+
+EOF
+
+for file in `ls $agreeDirectory | grep png` ; 
+#for file in "$( find $directory -name "*.png" )"   # -type l = symbolic links
+do
+  echo "$file"
+cat <<EOF >>$outputFileAgreement
+<A HREF=$file> <img height="300" src="$file"> </A>
+<h3><A name="EB"><FONT color="Black">Plot: $file </FONT></A><BR></h3>
+<h3><A name="EB"><FONT color="Black">----------------------</FONT></A><BR></h3>
+
+EOF
+done | sort
+
+# Make main webpage for plots with discrepancies
 outputFile=plots_$tempDate.htm
 >$outputFile
 
@@ -47,10 +83,18 @@ cat <<EOF >>$outputFile
 <BODY link="Red">
 <FONT color="Black">
 <h2><A name="EB"><FONT color="Black"> Results  Web Pages for $tempDate</FONT></A><BR></h2>
-<h3><A name="EB"><FONT color="Blue">Result Histograms comparing $compDir1 (line) and $compDir2 (points)</FONT></A><BR></h3>
+<h2><A name="EB"><FONT color="Blue">Compare </FONT><FONT color="Black">$compDir1</FONT> <FONT color="Blue"> and </FONT> <FONT color="Black">$compDir2</FONT></A><BR></h3>
 
 <A HREF=diff_menu_a_vs_menu_b.txt> diff_menu_a_vs_menu_b.txt </A>
 <h3><A name="EB"><FONT color="Black">----------------------</FONT></A><BR></h3>
+
+<A HREF=$outputFileAgreement> Plots In Agreement </A>
+<h3><A name="EB"><FONT color="Blue">----------------------</FONT></A><BR></h3>
+
+<A> Plots In Disagrement </A>
+<h3><A name="EB"><FONT color="Blue">----------------------</FONT></A><BR></h3>
+<h3><A name="EB"><FONT color="Blue">Result Discrepancy Histograms: </FONT></A><BR></h3>
+<h3><A name="EB"><FONT color="Blue">comparing </FONT><FONT color="Black">$compDir1 (line)</FONT> <FONT color="Blue"> and </FONT> <FONT color="Black">$compDir2 (points)</FONT></A><BR></h3>
 EOF
 
 for file in `ls $directory | grep png` ; 
