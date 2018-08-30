c359f430b6d255238fa6eca78323258a2607df28
Vladimir <vladimir.rekovic@cern.ch>
2018-08-30 08:46:28 +0200

    Corrected post-rebase fix provided in tag l1t-integration-v99.0-CMSSW_10_2_1.

--- L1Trigger/L1TCalorimeter/plugins/L1TStage2CaloLayer2Comp.cc
+++ L1Trigger/L1TCalorimeter/plugins/L1TStage2CaloLayer2Comp.cc
@@ -713,9 +713,6 @@ bool L1TStage2CaloLayer2Comp::compareSums(
   double dataEt = 0;
   double emulEt = 0;
 
-  l1t::EtSumBxCollection::const_iterator dataIt = dataCol->begin(currBx);
-  l1t::EtSumBxCollection::const_iterator emulIt = emulCol->begin(currBx);
-
   // if either data or emulator collections are empty, or they have different
   // size, mark the event as bad (this should never occur in normal running)
   if ((dataCol->isEmpty(currBx)) || (emulCol->isEmpty(currBx)) ||
