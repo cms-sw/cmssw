eac8530e1c6afb8832f5f69111190c8fcb538d13
Stepan <stepobr@cern.ch>
2018-08-31 20:08:26 +0200

    remove part of HI jet algorithms

--- RecoJets/Configuration/python/RecoJets_cff.py
+++ RecoJets/Configuration/python/RecoJets_cff.py
@@ -29,19 +29,12 @@ recoAllJetsPUOffsetCorr=cms.Sequence(fixedGridRhoFastjetAllCalo+
                                      fixedGridRhoFastjetCentralCalo+
                                      ak4CaloJetsPUCorr)
 
-from Configuration.Eras.Modifier_pp_on_AA_2018_cff import pp_on_AA_2018
-
-from RecoHI.HiJetAlgos.HiRecoJets_cff import caloTowersRec, caloTowers, akPu3CaloJets, akPu4CaloJets, akPu5CaloJets
+from RecoHI.HiJetAlgos.HiRecoJets_cff import caloTowersRec, caloTowers, akPu4CaloJets
 
 recoJetsHI =cms.Sequence(fixedGridRhoFastjetAllCalo+
                          fixedGridRhoFastjetCentralCalo+
                          ak4CaloJets+
                          caloTowersRec+
                          caloTowers+
-                         akPu3CaloJets+
-                         akPu4CaloJets+
-                         akPu5CaloJets
+                         akPu4CaloJets
                          )
-
-for e in [pp_on_AA_2018]:
- e.toReplaceWith(recoJets, recoJetsHI)
--- RecoJets/Configuration/python/RecoPFJets_cff.py
+++ RecoJets/Configuration/python/RecoPFJets_cff.py
@@ -106,9 +106,7 @@ recoPFJetsWithSubstructureTask=cms.Task(
     )
 recoPFJetsWithSubstructure=cms.Sequence(recoPFJetsWithSubstructureTask)
 
-from Configuration.Eras.Modifier_pp_on_AA_2018_cff import pp_on_AA_2018
-
-from RecoHI.HiJetAlgos.HiRecoPFJets_cff import PFTowers, akPu3PFJets, akPu4PFJets, akPu5PFJets, kt4PFJetsForRho, hiFJRhoProducer, akCs3PFJets, akCs4PFJets
+from RecoHI.HiJetAlgos.HiRecoPFJets_cff import PFTowers, akPu3PFJets, akPu4PFJets, kt4PFJetsForRho, hiFJRhoProducer, akCs4PFJets
 
 recoPFJetsHITask =cms.Task(fixedGridRhoAll,
                            fixedGridRhoFastjetAll,
@@ -121,16 +119,9 @@ recoPFJetsHITask =cms.Task(fixedGridRhoAll,
                            PFTowers,			   
                            akPu3PFJets,
                            akPu4PFJets,
-                           akPu5PFJets,
                            kt4PFJetsForRho,
                            hiFJRhoProducer,
-                           akCs3PFJets,
                            akCs4PFJets
 
    )
 recoPFJetsHI   = cms.Sequence(recoPFJetsHITask)
-
-
-for e in [pp_on_AA_2018]:
- e.toReplaceWith(recoPFJets, recoPFJetsHI)
-
