b4336c5560938ad5ff03aa511e63c23cfdf0ee2a
Huilin Qu <huilin.qu@cern.ch>
2018-09-03 17:48:24 +0200

    Style and performance improvements.

--- DataFormats/BTauReco/interface/DeepBoostedJetFeatures.h
+++ DataFormats/BTauReco/interface/DeepBoostedJetFeatures.h
@@ -20,6 +20,10 @@ public:
     feature_map_[name];
   }
 
+  void reserve(const std::string& name, unsigned capacity){
+    feature_map_[name].reserve(capacity);
+  }
+
   void fill(const std::string& name, float value){
     auto item = feature_map_.find(name);
     if (item != feature_map_.end()){
@@ -32,6 +36,17 @@ public:
 
   void set(const std::string& name, const std::vector<float>& vec){
     feature_map_[name] = vec;
+  }
+
+  void check_consistency(const std::vector<std::string> &names) const {
+    if (names.empty()) return;
+    const auto ref_len = get(names.front()).size();
+    for (unsigned i=1; i<names.size(); ++i){
+      if (get(names[i]).size() != ref_len){
+        throw cms::Exception("InvalidArgument") << "[DeepBoostedJetFeatures::check_consistency()] Inconsistent variable length "
+            << get(names[i]).size() << " for " << names[i] << ", should be " << ref_len;
+      }
+    }
   }
 
   const std::vector<float>& get(const std::string& name) const {
