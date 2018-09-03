858724214db57ca59bc36ab788c432e5ac14cb66
Jean-Baptiste Sauvan <jean-baptiste.sauvan@cern.ch>
2018-09-03 17:11:43 +0200

    Revert null changes

--- L1Trigger/L1THGCal/src/HGCalTriggerTools.cc
+++ L1Trigger/L1THGCal/src/HGCalTriggerTools.cc
@@ -131,7 +131,7 @@ layer(const DetId& id) const {
 unsigned 
 HGCalTriggerTools::
 layerWithOffset(const DetId& id) const {
-  unsigned int l = layer(id);  
+  unsigned int l = layer(id);
   if( id.det() == DetId::Forward && id.subdetId() == HGCHEF ) {
     l += eeLayers_;
   } else if( (id.det() == DetId::Hcal && id.subdetId() == HcalEndcap) ||
