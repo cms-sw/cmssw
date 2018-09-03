788e161e86cb88576806c23c81ce10cb1e86ba46
Huilin Qu <huilin.qu@cern.ch>
2018-09-03 18:26:23 +0200

    Reorganize TagInfo producers and JetTags producers.

--- RecoBTag/FeatureTools/python/pfNegativeDeepFlavourTagInfos_cfi.py
+++ RecoBTag/FeatureTools/python/pfNegativeDeepFlavourTagInfos_cfi.py
@@ -1,6 +1,6 @@
 import FWCore.ParameterSet.Config as cms
 
-from RecoBTag.TensorFlow.pfDeepFlavourTagInfos_cfi import pfDeepFlavourTagInfos
+from RecoBTag.FeatureTools.pfDeepFlavourTagInfos_cfi import pfDeepFlavourTagInfos
 
 pfNegativeDeepFlavourTagInfos = pfDeepFlavourTagInfos.clone(
     shallow_tag_infos = 'pfDeepCSVNegativeTagInfos',
