44021717be0683e1a678f9222b279f0b05508672
Pieter David <pieter.david@cern.ch>
2018-09-04 11:25:41 +0200

    rename hybrid emulation customize method to addBeforeRepackZS10

--- RecoLocalTracker/SiStripZeroSuppression/python/customise_EmulateHybrid.py
+++ RecoLocalTracker/SiStripZeroSuppression/python/customise_EmulateHybrid.py
@@ -2,7 +2,7 @@ import FWCore.ParameterSet.Config as cms
 
 ## Add the ZS algorithm (in hybrid emulation mode) before repacking, to produce emulated hybrid samples with
 ##   cmsDriver --step RAW2DIGI,REPACK:DigiToZS10RawRepack --customiseRecoLocalTracker/SiStripZeroSuppression/customise_EmulateHybrid.py ...
-def customise(process):
+def addBeforeRepackZS10(process):
     process.load("RecoLocalTracker.SiStripZeroSuppression.SiStripZeroSuppression_cfi")
     zs = process.siStripZeroSuppression
     zs.produceRawDigis = False
@@ -13,7 +13,7 @@ def customise(process):
     zs.Algorithms.MeanCM = 512
     zs.Algorithms.DeltaCMThreshold = 20
     zs.Algorithms.Use10bitsTruncation = True
-    zs.RawDigiProducersList = cms.VInputTag(cms.InputTag("siStripDigis","VirginRaw"))
+    zs.RawDigiProducersList = cms.VInputTag(cms.InputTag("siStripDigis", "VirginRaw"))
 
     process.DigiToZS10RawRepack.insert(0, zs) ## insert before repacking
     return process
