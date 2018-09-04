d286ac168e7fe48886dd4fc58e1bd14f891d208d
Slava Krutelyov <slava77@cern.ch>
2018-09-04 14:28:50 +0200

    pp-like and full HI scenarios for 2018 HI data taking

--- Configuration/DataProcessing/python/RecoTLR.py
+++ Configuration/DataProcessing/python/RecoTLR.py
@@ -85,6 +85,15 @@ def customisePostEra_Run2_2018_harvesting_trackingOnly(process):
     customisePostEra_Run2_2017_harvesting_trackingOnly(process)
     return process
 
+def customise_HI_PostEra_Run2_2018(process):
+    customisePostEra_Run2_2018(process)
+    return process
+
+def customisePostEra_Run2_2018_pp_on_AA(process):
+    customisePostEra_Run2_2018(process)
+    return process
+
+
 
 ##############################################################################
 def customisePPData(process):
