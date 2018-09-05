2983b63892032a5e9b9ee8ff70ae276229ff43c6
zhen <zhen.hu@cern.ch>
2018-09-05 22:21:31 +0200

    update GT to fix conflict with PR 24383

--- Configuration/AlCa/python/autoCond.py
+++ Configuration/AlCa/python/autoCond.py
@@ -32,7 +32,7 @@ autoCond = {
     # GlobalTag for Run2 data 2016H relvals only: Prompt Conditions + fixed L1 menu (to be removed)
     'run2_data_promptlike' : '103X_dataRun2_PromptLike_v5',
     # GlobalTag for Run2 data 2018B relvals only: HEM-15-16 fail
-    'run2_data_promptlike_HEfail' : '103X_dataRun2_PromptLike_HEfail_v1',
+    'run2_data_promptlike_HEfail' : '103X_dataRun2_PromptLike_HEfail_v3',
     # GlobalTag for Run1 HLT: it points to the online GT
     'run1_hlt'          :   '101X_dataRun2_HLT_frozen_v6',
     # GlobalTag for Run2 HLT: it points to the online GT
