3dd249fdca65312a167290359d997f147c01026d
Luca Pernie <mrluca87@gmail.com>
2018-09-10 15:47:47 -0500

    Fix conflicts

--- Configuration/AlCa/python/autoCond.py
+++ Configuration/AlCa/python/autoCond.py
@@ -55,6 +55,8 @@ autoCond = {
     'phase1_2018_realistic_hi' : '103X_upgrade2018_realistic_HI_v1',
     # GlobalTag for MC production (cosmics) with realistic conditions for full Phase1 2018 detector,  Strip tracker in DECO mode
     'phase1_2018_cosmics'      :   '103X_upgrade2018cosmics_realistic_deco_v4',
+    # GlobalTag for MC production with realistic conditions for full Phase1 2018 detector: HEM-15-16 fail
+    'phase1_2018_realistic_HEfail'    : '103X_upgrade2018_realistic_HEfail_v1',    
     # GlobalTag for MC production with perfectly aligned and calibrated detector for Phase1 2019
     'phase1_2019_design'       : '103X_postLS2_design_v2', # GT containing design conditions for postLS2
     # GlobalTag for MC production with perfectly aligned and calibrated detector for Phase1 2019
