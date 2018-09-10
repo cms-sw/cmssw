be1352adc0260d6813df476f29bd59e3a20b3342
Michail <bachtis@cern.ch>
2018-09-10 14:51:53 -0700

    Adding test file"

--- L1Trigger/L1TTrackMatch/test/runTrackerPlusStubs_cfg.py
+++ L1Trigger/L1TTrackMatch/test/runTrackerPlusStubs_cfg.py
@@ -6,23 +6,13 @@ process.load('Configuration.StandardSequences.FrontierConditions_GlobalTag_cff')
 process.load('Configuration.StandardSequences.Services_cff')
 process.load('Configuration.StandardSequences.GeometryRecoDB_cff')   
 
-
-
-process.maxEvents = cms.untracked.PSet( input = cms.untracked.int32(10) )
-
+process.maxEvents = cms.untracked.PSet( input = cms.untracked.int32(100) )
 
 process.source = cms.Source("PoolSource",
     fileNames = cms.untracked.vstring(
         'file:/scratch3/MTF/data/singleMu140_PhaseII.root'
     ),
 )
-
-
-
-
-
-
-
 
 process.load('Configuration.StandardSequences.FrontierConditions_GlobalTag_condDBv2_cff')
 from Configuration.AlCa.GlobalTag import GlobalTag
@@ -47,7 +37,6 @@ process.fakeBmtfParams.mask_phtf_st1        = cms.vstring(maskdisable, maskenabl
 process.fakeBmtfParams.mask_phtf_st2        = cms.vstring(maskenable,  maskenable, maskenable, maskenable, maskenable, maskenable, maskenable)
 process.fakeBmtfParams.mask_phtf_st3        = cms.vstring(maskenable,  maskenable, maskenable, maskenable, maskenable, maskenable, maskenable)
 process.fakeBmtfParams.mask_phtf_st4        = cms.vstring(maskenable,  maskenable, maskenable, maskenable, maskenable, maskenable, maskenable)
-
 process.fakeBmtfParams.mask_ettf_st1        = cms.vstring(maskdisable, maskenable, maskenable, maskenable, maskenable, maskenable, maskdisable)
 process.fakeBmtfParams.mask_ettf_st2        = cms.vstring(maskenable,  maskenable, maskenable, maskenable, maskenable, maskenable, maskenable)
 process.fakeBmtfParams.mask_ettf_st3        = cms.vstring(maskenable,  maskenable, maskenable, maskenable, maskenable, maskenable, maskenable)
@@ -57,23 +46,29 @@ process.fakeBmtfParams.mask_ettf_st3        = cms.vstring(maskenable,  maskenabl
 
 # Load Muon Stubs
 process.load('L1Trigger.L1TMuonBarrel.simKBmtfStubs_cfi')
+#print('Loaded muon stubs')
+
+
 # Load Tracker Plus Stubs
 process.load('L1Trigger.L1TTrackMatch.L1TTrackerPlusStubs_cfi')
+#print('Loaded tracker plus stubs')
 
 
 #Configure for Phase 2
 
 
 
-
+#print('Configuring for phase 2')
 process.out = cms.OutputModule("PoolOutputModule",
-    fileName = cms.untracked.string('test.root'),
+    fileName = cms.untracked.string('Mu140PU.root'),
 )
 
   
 #Path that makes stubs and then runs Tracker+Stubs
+#print('Running tracker+stubs')
 process.p = cms.Path(process.simKBmtfStubs*process.l1StubMatchedMuons)
 
 process.e = cms.EndPath(process.out)
 process.schedule = cms.Schedule(process.p,process.e)
 
+#print('End of python script')
