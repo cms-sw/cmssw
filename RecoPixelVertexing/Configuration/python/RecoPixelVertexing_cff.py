5807acbead16346aa8496f7d0487eb5c1cd2dad8
Vincenzo Innocente <vincenzo.innocente@cern.ch>
2018-09-12 09:47:46 +0200

    Add new concurrent vertex algo (cms-patatrack#158)
    Add a new concurrent vertex algorithm based on DBSCAN (that reuses parts of the pixel cluster algorithm).
    It still needs to be tuned to reach at least the performance of current DivisiveClustering algorithm used at HLT.

--- RecoPixelVertexing/Configuration/python/RecoPixelVertexing_cff.py
+++ RecoPixelVertexing/Configuration/python/RecoPixelVertexing_cff.py
@@ -4,7 +4,6 @@ from RecoPixelVertexing.PixelTrackFitting.PixelTracks_cff import *
 #
 # for STARTUP ONLY use try and use Offline 3D PV from pixelTracks, with adaptive vertex
 #
-#from RecoPixelVertexing.PixelVertexFinding.PixelVertexes_cff import *
-from RecoVertex.PrimaryVertexProducer.OfflinePixel3DPrimaryVertices_cfi import *
-recopixelvertexingTask = cms.Task(pixelTracksTask,pixelVertices)
-recopixelvertexing = cms.Sequence(recopixelvertexingTask)
+from RecoPixelVertexing.PixelVertexFinding.PixelVertexes_cff import *
+# from RecoVertex.PrimaryVertexProducer.OfflinePixel3DPrimaryVertices_cfi import *
+recopixelvertexing = cms.Sequence(pixelTracksSequence*pixelVertices)
