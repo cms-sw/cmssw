852e734a8db7d083456afdca160822210bccd3e3
Efe Yazgan <efe.yazgan@cern.ch>
2018-09-13 14:28:37 +0200

    Update PythiaCP5TuneUpSettings_cfi.py

--- Configuration/Generator/python/MCTunes2017/PythiaCP5TuneUpSettings_cfi.py
+++ Configuration/Generator/python/MCTunes2017/PythiaCP5TuneUpSettings_cfi.py
@@ -1,7 +1,7 @@
 import FWCore.ParameterSet.Config as cms
 
-pythia8CP5SettingsBlock = cms.PSet(
-    pythia8CP5Settings = cms.vstring(
+pythia8CP5TuneUpSettingsBlock = cms.PSet(
+    pythia8CP5TuneUpSettings = cms.vstring(
     'Tune:pp 14',
         'Tune:ee 7',
         'MultipartonInteractions:ecmPow=0.03344',
