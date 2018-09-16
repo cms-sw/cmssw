aef27ffec5b64db8a39f1a771c367702bce1154c
kpanos <panos.katsoulis@cern.ch>
2018-09-16 20:11:08 +0200

    branch to PR the DQM fix to the CMSSW master

--- DQM/L1TMonitor/python/L1TdeStage2BMTF_cfi.py
+++ DQM/L1TMonitor/python/L1TdeStage2BMTF_cfi.py
@@ -9,12 +9,14 @@ from DQMServices.Core.DQMEDAnalyzer import DQMEDAnalyzer
 l1tdeStage2Bmtf = DQMEDAnalyzer(
     "L1TStage2RegionalMuonCandComp",
     regionalMuonCollection1 = cms.InputTag("bmtfDigis", "BMTF"),
-    regionalMuonCollection2 = cms.InputTag("valBmtfDigis", "BMTF"),
+    # regionalMuonCollection2 = cms.InputTag("valBmtfDigis", "BMTF"), # didn't remove the default config
+    regionalMuonCollection2 = cms.InputTag("valBmtfAlgoSel", "BMTF"),
     monitorDir = cms.untracked.string("L1TEMU/L1TdeStage2BMTF"),
     regionalMuonCollection1Title = cms.untracked.string("BMTF data"),
     regionalMuonCollection2Title = cms.untracked.string("BMTF emulator"),
     summaryTitle = cms.untracked.string("Summary of comparison between BMTF muons and BMTF emulator muons"),
     ignoreBin = cms.untracked.vint32(ignoreBinsDeStage2Bmtf),
     verbose = cms.untracked.bool(False),
+    isBmtf = cms.untracked.bool(True)
 )
 
