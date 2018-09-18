00da8e9e1206543960fbe57bf17fe8109dd26609
tocheng <tocheng.cms@gmail.com>
2018-09-18 16:27:10 +0200

    fix class name error, use customisePostEra_Run2_2018_pp_on_AA, remove superfluous class initialization; add two scenarios to Configuration/DataProcessing/test/run_CfgTest.sh

--- Configuration/DataProcessing/python/Impl/hcalnzsEra_Run2_2018_pp_on_AA.py
+++ Configuration/DataProcessing/python/Impl/hcalnzsEra_Run2_2018_pp_on_AA.py
@@ -12,18 +12,14 @@ import sys
 from Configuration.DataProcessing.Impl.hcalnzs import hcalnzs
 from Configuration.Eras.Era_Run2_2018_pp_on_AA_cff import Run2_2018_pp_on_AA
 
-class hcalnzsEra_Run2_2018_highBetaStar(hcalnzs):
+class hcalnzsEra_Run2_2018_pp_on_AA(hcalnzs):
     def __init__(self):
-        hcalnzs.__init__(self)
-        self.recoSeq=':reconstruction_HcalNZS'
-        self.cbSc='pp'
-        self.addEI=True
         self.isRepacked=True
         self.eras=Run2_2018_pp_on_AA
         #keep post-era parts the same as in the default 2018 era
-        self.promptCustoms += [ 'Configuration/DataProcessing/RecoTLR.customisePostEra_Run2_2018' ]
-        self.expressCustoms += [ 'Configuration/DataProcessing/RecoTLR.customisePostEra_Run2_2018' ]
-        self.visCustoms += [ 'Configuration/DataProcessing/RecoTLR.customisePostEra_Run2_2018' ]
+        self.promptCustoms += [ 'Configuration/DataProcessing/RecoTLR.customisePostEra_Run2_2018_pp_on_AA' ]
+        self.expressCustoms += [ 'Configuration/DataProcessing/RecoTLR.customisePostEra_Run2_2018_pp_on_AA' ]
+        self.visCustoms += [ 'Configuration/DataProcessing/RecoTLR.customisePostEra_Run2_2018_pp_on_AA' ]
     """
     _hcalnzsEra_Run2_2018_pp_on_AA_
 
