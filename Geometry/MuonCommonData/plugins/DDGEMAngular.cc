35cb1ee23086c1908cbe7d0d0867e356c6c8a7ae
Carl Vuosalo <cvuosalo@cern.ch>
2018-09-19 01:23:53 +0200

    Try rounding to nearest degree to eliminate GEM volume placement differences

--- Geometry/MuonCommonData/plugins/DDGEMAngular.cc
+++ Geometry/MuonCommonData/plugins/DDGEMAngular.cc
@@ -67,7 +67,12 @@ void DDGEMAngular::execute(DDCompactView& cpv) {
 
   for (int ii=0; ii<n; ii++) {
 
-    double phitmp = phi;
+    double phitmp = CONVERT_TO( phi, deg );
+    int factor = 1;
+    if (phitmp < 0.0) factor = -1;
+    // Round angle to nearest degree
+    phitmp = (double) factor * int(std::abs(phitmp) + 0.1);
+    phitmp *= _pi / 180.0;
     if (phitmp >= 2._pi) phitmp -= 2._pi;
     DDRotation rotation;
     std::string rotstr("RG");
