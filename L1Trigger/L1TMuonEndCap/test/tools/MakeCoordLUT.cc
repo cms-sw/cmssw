9940018980613fba84786dd93ffd5061c9fde16c
Andrew Brinkerhoff <abrinke1@cern.ch>
2018-09-19 16:01:32 +0200

    Added branches to MakeCoordLUT.cc

--- L1Trigger/L1TMuonEndCap/test/tools/MakeCoordLUT.cc
+++ L1Trigger/L1TMuonEndCap/test/tools/MakeCoordLUT.cc
@@ -541,6 +541,15 @@ void MakeCoordLUT::validateLUTs() {
   int es          = 0;
   int st          = 0;
   int ch          = 0;
+
+  int endcap      = 0;
+  int station     = 0;
+  int sector      = 0;
+  int subsector   = 0;
+  int ring        = 0;
+  int chamber     = 0;
+  int CSC_ID      = 0;
+
   int strip       = 0;  // it is half-strip, despite the name
   int wire        = 0;  // it is wiregroup, despite the name
   int fph_int     = 0;
@@ -555,6 +564,15 @@ void MakeCoordLUT::validateLUTs() {
   ttree->Branch("es"     , &es     );
   ttree->Branch("st"     , &st     );
   ttree->Branch("ch"     , &ch     );
+
+  ttree->Branch("endcap"   , &endcap    );
+  ttree->Branch("station"  , &station   );
+  ttree->Branch("sector"   , &sector    );
+  ttree->Branch("subsector", &subsector );
+  ttree->Branch("ring"     , &ring      );
+  ttree->Branch("chamber"  , &chamber   );
+  ttree->Branch("CSC_ID"   , &CSC_ID    );
+
   ttree->Branch("strip"  , &strip  );
   ttree->Branch("wire"   , &wire   );
   ttree->Branch("fph_int", &fph_int);
@@ -563,6 +581,8 @@ void MakeCoordLUT::validateLUTs() {
   ttree->Branch("fth_emu", &fth_emu);
   ttree->Branch("fph_sim", &fph_sim);
   ttree->Branch("fth_sim", &fth_sim);
+
+
 
 
   for (es = 0; es < 12; ++es) {
@@ -588,11 +608,11 @@ void MakeCoordLUT::validateLUTs() {
       assert(es < 12 && st < 5 && ch < 16);
 
       // Retrieve endcap, sector, subsector, station, chamber
-      int endcap      = (es/6) + 1;
-      int sector      = (es%6) + 1;
-      int subsector   = (st <= 1) ? st + 1 : 0;
-      int station     = (st <= 1) ? 1 : st;
-      int chamber     = ch + 1;
+      endcap      = (es/6) + 1;
+      sector      = (es%6) + 1;
+      subsector   = (st <= 1) ? st + 1 : 0;
+      station     = (st <= 1) ? 1 : st;
+      chamber     = ch + 1;
 
       bool is_me11a = false;
       bool is_neighbor = false;
@@ -638,12 +658,14 @@ void MakeCoordLUT::validateLUTs() {
         }
       }
 
+      CSC_ID = rcscid;
+
       // Set maxWire, maxStrip
       const CSCDetId cscDetId = getCSCDetId(endcap, rsector, rsubsector, station, rcscid, is_me11a);
       const CSCChamber* chamb = theCSCGeometry_->chamber(cscDetId);
       const CSCLayerGeometry* layerGeom = chamb->layer(CSCConstants::KEY_CLCT_LAYER)->geometry();
 
-      const int ring     = cscDetId.ring();
+      ring               = cscDetId.ring();
       const int maxWire  = layerGeom->numberOfWireGroups();
       const int maxStrip = layerGeom->numberOfStrips();
 
---
+++ L1Trigger/L1TMuonEndCap/test/tools/make_coordlut_MC.py
@@ -0,0 +1,41 @@
+import FWCore.ParameterSet.Config as cms
+
+process = cms.Process("Whatever")
+
+#process.load('Configuration.Geometry.GeometryExtended2016Reco_cff')
+process.load("Configuration.StandardSequences.GeometryDB_cff")  # load from DB
+process.load('Configuration.StandardSequences.MagneticField_cff')
+process.load("Configuration.StandardSequences.FrontierConditions_GlobalTag_cff")
+
+from Configuration.AlCa.GlobalTag import GlobalTag
+process.GlobalTag = GlobalTag(process.GlobalTag, 'auto:run2_mc', '')
+# process.GlobalTag = GlobalTag(process.GlobalTag, '102X_dataRun2_Sep2018Rereco_v1')
+print "Using GlobalTag: %s" % process.GlobalTag.globaltag.value()
+
+# Fake alignment is/should be ideal geometry
+# ==========================================
+process.load("Alignment.CommonAlignmentProducer.FakeAlignmentSource_cfi")
+process.preferFakeAlign = cms.ESPrefer("FakeAlignmentSource")
+
+process.source = cms.Source("EmptySource")
+# process.source = cms.Source("EmptyIOVSource",
+#     timetype = cms.string('runnumber'),
+#     firstValue = cms.uint64(321988),
+#     lastValue = cms.uint64(321988),
+#     interval = cms.uint64(1)
+# )
+
+process.maxEvents = cms.untracked.PSet(input = cms.untracked.int32(1))
+
+process.analyzer1 = cms.EDAnalyzer("MakeCoordLUT",
+    # Verbosity level
+    verbosity = cms.untracked.int32(1),
+
+    # Output diectory
+    outdir = cms.string("./pc_luts/firmware_mc/"),
+
+    # Produce "validate.root" to validate the LUTs
+    please_validate = cms.bool(True),
+)
+
+process.path1 = cms.Path(process.analyzer1)
