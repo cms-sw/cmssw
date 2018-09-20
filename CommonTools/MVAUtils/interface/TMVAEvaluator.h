fc3b00b612566ec8d9631adbc5cace7f7ca7aaf5
Jonas Rembser <jonas.rembser@cern.ch>
2018-09-20 10:48:18 +0200

    Code formatting

--- CommonTools/MVAUtils/interface/TMVAEvaluator.h
+++ CommonTools/MVAUtils/interface/TMVAEvaluator.h
@@ -1,35 +1,39 @@
-#ifndef CommonTools_Utils_TMVAEvaluator_h
-#define CommonTools_Utils_TMVAEvaluator_h
+#ifndef CommonTools_MVAUtils_TMVAEvaluator_h
+#define CommonTools_MVAUtils_TMVAEvaluator_h
 
+#include <map>
 #include <memory>
+#include <mutex>
 #include <string>
 #include <vector>
-#include <map>
-#include <mutex>
 
-#include "TMVA/Reader.h"
-#include "TMVA/IMethod.h"
 #include "CondFormats/EgammaObjects/interface/GBRForest.h"
 #include "FWCore/Framework/interface/EventSetup.h"
 #include "FWCore/Utilities/interface/thread_safety_macros.h"
-
+#include "TMVA/IMethod.h"
+#include "TMVA/Reader.h"
 
 class TMVAEvaluator {
 
-  public:
+public:
     TMVAEvaluator();
 
-    void initialize(const std::string & options, const std::string & method, const std::string & weightFile,
-                    const std::vector<std::string> & variables, const std::vector<std::string> & spectators, bool useGBRForest=false, bool useAdaBoost=false);
-    void initializeGBRForest(const GBRForest* gbrForest, const std::vector<std::string> & variables,
-                             const std::vector<std::string> & spectators, bool useAdaBoost=false);
-    void initializeGBRForest(const edm::EventSetup &iSetup, const std::string & label,
-                             const std::vector<std::string> & variables, const std::vector<std::string> & spectators, bool useAdaBoost=false);
-    float evaluateTMVA(const std::map<std::string,float> & inputs, bool useSpectators) const;
-    float evaluateGBRForest(const std::map<std::string,float> & inputs) const;
-    float evaluate(const std::map<std::string,float> & inputs, bool useSpectators=false) const;
+    void initialize(const std::string& options, const std::string& method, const std::string& weightFile,
+        const std::vector<std::string>& variables, const std::vector<std::string>& spectators,
+        bool useGBRForest = false, bool useAdaBoost = false);
 
-  private:
+    void initializeGBRForest(const GBRForest* gbrForest, const std::vector<std::string>& variables,
+        const std::vector<std::string>& spectators, bool useAdaBoost = false);
+
+    void initializeGBRForest(const edm::EventSetup& iSetup, const std::string& label,
+        const std::vector<std::string>& variables, const std::vector<std::string>& spectators,
+        bool useAdaBoost = false);
+
+    float evaluateTMVA(const std::map<std::string, float>& inputs, bool useSpectators) const;
+    float evaluateGBRForest(const std::map<std::string, float>& inputs) const;
+    float evaluate(const std::map<std::string, float>& inputs, bool useSpectators = false) const;
+
+private:
     bool mIsInitialized;
     bool mUsingGBRForest;
     bool mUseAdaBoost;
@@ -39,9 +43,8 @@ class TMVAEvaluator {
     CMS_THREAD_GUARD(m_mutex) std::unique_ptr<TMVA::Reader> mReader;
     std::shared_ptr<const GBRForest> mGBRForest;
 
-    CMS_THREAD_GUARD(m_mutex) mutable std::map<std::string,std::pair<size_t,float>> mVariables;
-    CMS_THREAD_GUARD(m_mutex) mutable std::map<std::string,std::pair<size_t,float>> mSpectators;
+    CMS_THREAD_GUARD(m_mutex) mutable std::map<std::string, std::pair<size_t, float>> mVariables;
+    CMS_THREAD_GUARD(m_mutex) mutable std::map<std::string, std::pair<size_t, float>> mSpectators;
 };
 
 #endif // CommonTools_Utils_TMVAEvaluator_h
-
