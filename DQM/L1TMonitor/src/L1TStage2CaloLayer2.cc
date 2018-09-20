564d55db48d0fc09897f58fc285b3ee3aa8e34ee
vmilosev <vukasin.milosevic@cern.ch>
2018-09-20 18:00:53 +0200

    Typo fix and binning change for the centrality plot

--- DQM/L1TMonitor/src/L1TStage2CaloLayer2.cc
+++ DQM/L1TMonitor/src/L1TStage2CaloLayer2.cc
@@ -103,8 +103,8 @@ void L1TStage2CaloLayer2::bookHistograms(DQMStore::IBooker &ibooker, edm::Run co
   // stage2CaloLayer2HTTHFRank_ = ibooker.book1D("HTTHFRank", "HTTHF E_{T}", 4096, -0.5, 4095.5);
   stage2CaloLayer2ETTEMRank_ = ibooker.book1D("ETTEMRank", "ETTEM E_{T}; ETTEM iE_{T}; Counts", 4096, -0.5, 4095.5);
   
-  stage2CaloLayer2Asymmetry_ = ibooker.book1D("Assymetry", "Assymetry; Assymetry; Counts", 256, -0.5, 255.5);
-  stage2CaloLayer2Centrality_ = ibooker.book1D("Centrality", "Centrality; Centrality; Counts", 8, 0, 8);
+  stage2CaloLayer2Asymmetry_ = ibooker.book1D("Asymmetry", "Asymmetry; Assymmetry; Counts", 256, -0.5, 255.5);
+  stage2CaloLayer2Centrality_ = ibooker.book1D("Centrality", "Centrality; Centrality; Counts", 8, 0.5, 7.5);
  
   stage2CaloLayer2MinBiasHFP0_ = ibooker.book1D("MinBiasHFP0", "HF Min Bias Sum Threshold 0, Positive #eta; N_{towers}; Events", 16, -0.5, 15.5);
   stage2CaloLayer2MinBiasHFM0_ = ibooker.book1D("MinBiasHFM0", "HF Min Bias Sum Threshold 1, Nevagive #eta; N_{towers}; Events", 16, -0.5, 15.5);
@@ -299,10 +299,10 @@ void L1TStage2CaloLayer2::analyze(const edm::Event & e, const edm::EventSetup & 
 	} else if (l1t::EtSum::EtSumType::kTotalHt == itEtSum->getType()){
 	  stage2CaloLayer2HTTRank_->Fill(itEtSum->hwPt());
 	} else if (l1t::EtSum::EtSumType::kAsymEt == itEtSum->getType()){     //Asym ET
-          stage2CaloLayer2Asymmetry_->Fill(itEtSum->hwPt());
+	  stage2CaloLayer2Asymmetry_->Fill(itEtSum->hwPt());
         } else if (l1t::EtSum::EtSumType::kCentrality == itEtSum->getType()){     //Centrality
           for (int i=0; i<8; i++)
-              if (((itEtSum->hwPt()>>i)&1)==1) stage2CaloLayer2Centrality_->Fill(i);
+		if (((itEtSum->hwPt()>>i)&1)==1) stage2CaloLayer2Centrality_->Fill(i);
         }
       }
     }
