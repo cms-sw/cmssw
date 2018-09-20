ecf3f89e0f74a9d98e6779cbbe8402d3d5c2cb55
Vladimir <vladimir.rekovic@cern.ch>
2018-09-20 09:05:22 +0200

    Add customisation for excluding hgcTps from sequence and complete printout statment for customisation of Droping.

--- L1Trigger/Configuration/python/customiseUtils.py
+++ L1Trigger/Configuration/python/customiseUtils.py
@@ -168,6 +168,15 @@ def DropOutputProducts(process):
     print "drop TrackingParticles_mix_MergedTrackTruth_*"
     print "drop PixelDigiSimLinkedmDetSetVector_simSiPixelDigis_Tracker_*"
     print "drop TrackingVertexs_mix_MergedTrackTruth_*"
+    print "drop HGCalDetIdHGCSampleHGCDataFramesSorted_mix_HGCDigisEE_HLT"
+    print "drop l1tHGCalTriggerCellBXVector_hgcalTriggerPrimitiveDigiProducer_calibratedTriggerCellsTower_L1"
+    print "drop PixelDigiSimLinkedmDetSetVector_simSiPixelDigis_Pixel_HLT"
+    print "drop l1tHGCalTowerMapBXVector_hgcalTriggerPrimitiveDigiProducer_towerMap_L1"
+    print "drop SimClusters_mix_MergedCaloTruth_HLT"
+    print "drop Phase2TrackerDigiedmDetSetVectorPhase2TrackerDigiPhase2TrackerDigiedmrefhelperFindForDetSetVectoredmRefTTClusteredmNewDetSetVector_TTClustersFromPhase2TrackerDigis_ClusterInclusive_HLT"
+    print "drop Phase2TrackerDigiedmDetSetVectorPhase2TrackerDigiPhase2TrackerDigiedmrefhelperFindForDetSetVectoredmRefTTClusterAssociationMap_TTClusterAssociatorFromPixelDigis_ClusterInclusive_HLT"
+    print "drop PixelDigiedmDetSetVector_simSiPixelDigis_Pixel_HLT"
+    print "drop Phase2TrackerDigiedmDetSetVector_mix_Tracker_HLT"
     process.FEVTDEBUGHLToutput.outputCommands = cms.untracked.vstring('keep *' 
               ,'drop TrackingParticles_mix_MergedTrackTruth_*'
               ,'drop PixelDigiSimLinkedmDetSetVector_simSiPixelDigis_Tracker_*'
@@ -200,6 +209,11 @@ def L1TrackTriggerTMTT(process):
     process.schedule.insert(2,process.L1TrackTriggerTMTT_step)
     return process
 
+def L1TTurnOffHGCalTPs(process):
+    cutlist=['hgcalTriggerPrimitiveDigiProducer']
+    for b in cutlist:
+        process.SimL1Emulator.remove(getattr(process,b))
+    return process
 
 def appendDTChamberAgingAtL1Trigger(process):
 # #############################################################################
