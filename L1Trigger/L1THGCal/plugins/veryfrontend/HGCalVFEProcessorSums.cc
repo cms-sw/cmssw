5e04e6459977e7c474677416a0576b6e4942c20d
Jean-Baptiste Sauvan <jean-baptiste.sauvan@cern.ch>
2018-09-21 14:38:36 +0200

    Fix detid checks for v9 geometry by Jean-Baptiste

--- L1Trigger/L1THGCal/plugins/veryfrontend/HGCalVFEProcessorSums.cc
+++ L1Trigger/L1THGCal/plugins/veryfrontend/HGCalVFEProcessorSums.cc
@@ -60,8 +60,8 @@ HGCalVFEProcessorSums::run(const HGCalDigiCollection& ee,
   else if(!bh.empty())
   {  
      for(const auto& bhdata : bh)
-     { 
-       if(DetId(bhdata.id()).det()!=DetId::HGCalHSc && HcalDetId(bhdata.id()).subdetId()!=HcalEndcap) continue;
+     {
+       if(DetId(bhdata.id()).det()==DetId::Hcal && HcalDetId(bhdata.id()).subdetId()!=HcalEndcap) continue;
        uint32_t module = geometry_->getModuleFromCell(bhdata.id());
        if(geometry_->disconnectedModule(module)) continue;
        dataframes.emplace_back(bhdata.id());
