dfd88b822ea8088156dbdf003cdd9ccf406b22bc
Dilson de Jesus Damiao <dilson@cern.ch>
2018-09-21 15:18:25 +0200

    add random number engine

--- SimPPS/Configuration/python/SimPPS_cff.py
+++ SimPPS/Configuration/python/SimPPS_cff.py
@@ -5,4 +5,8 @@ import FWCore.ParameterSet.Config as cms
 from SimPPS.PPSPixelDigiProducer.RPixDetDigitizer_cfi import *
 from SimPPS.RPDigiProducer.RPSiDetConf_cfi import *
 
+from IOMC.RandomEngine.IOMC_cff import *
+RandomNumberGeneratorService.RPixDetDigitizer = cms.PSet(initialSeed =cms.untracked.uint32(137137))
+RandomNumberGeneratorService.RPSiDetDigitizer = cms.PSet(initialSeed =cms.untracked.uint32(137137))
+
 ppsDigi = cms.Sequence(RPixDetDigitizer+RPSiDetDigitizer)
