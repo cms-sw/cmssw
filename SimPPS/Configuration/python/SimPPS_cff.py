ec7ac4a8e2344ad756ab39d38f6d3968629527fc
Dilson de Jesus Damiao <dilson@cern.ch>
2018-09-21 15:18:25 +0200

    add random number engine

--- SimPPS/Configuration/python/SimPPS_cff.py
+++ SimPPS/Configuration/python/SimPPS_cff.py
@@ -5,11 +5,29 @@ import FWCore.ParameterSet.Config as cms
 from SimPPS.PPSPixelDigiProducer.RPixDetDigitizer_cfi import *
 from SimPPS.RPDigiProducer.RPSiDetConf_cfi import *
 
-<<<<<<< HEAD
 from IOMC.RandomEngine.IOMC_cff import *
 RandomNumberGeneratorService.RPixDetDigitizer = cms.PSet(initialSeed =cms.untracked.uint32(137137))
 RandomNumberGeneratorService.RPSiDetDigitizer = cms.PSet(initialSeed =cms.untracked.uint32(137137))
 
-=======
->>>>>>> PPS Full Simulation branch to PR
-ppsDigi = cms.Sequence(RPixDetDigitizer+RPSiDetDigitizer)
+ppsDigi = cms.Sequence()
+
+# add PPS 2016 digi modules
+from Configuration.Eras.Modifier_pps_2016_cff import pps_2016
+_pps_2016_Digi = ppsDigi.copy()
+_pps_2016_Digi = cms.Sequence(RPSiDetDigitizer)
+pps_2016.toReplaceWith(ppsDigi,_pps_2016_Digi)
+
+# add PPS 2017 digi modules
+from Configuration.Eras.Modifier_pps_2017_cff import pps_2017
+_pps_2017_Digi = ppsDigi.copy()
+_pps_2017_Digi = cms.Sequence(RPixDetDigitizer+RPSiDetDigitizer)
+pps_2017.toReplaceWith(ppsDigi,_pps_2017_Digi)
+
+# add PPS 2018 digi modules
+from Configuration.Eras.Modifier_pps_2018_cff import pps_2018
+_pps_2018_Digi = ppsDigi.copy()
+_pps_2018_Digi = cms.Sequence(RPixDetDigitizer)
+pps_2018.toReplaceWith(ppsDigi,_pps_2018_Digi)
+
+#from Configuration.Eras.Modifier_fastSim_cff import fastSim
+#fastSim.toReplaceWith(doAllDigi,doAllDigi.copyAndExclude([RPixDetDigitizer,RPSiDetDigitizer]))
