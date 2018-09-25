c439bfe88d2a51eb0b5ee2b207703961c0788295
Michal Bluj <michal.bluj@cern.ch>
2018-09-25 17:57:29 +0200

    Reorganisation of configuration files: cff split to cfi and cff

--- RecoTauTag/RecoTau/python/DeepTauId_cff.py
+++ RecoTauTag/RecoTau/python/DeepTauId_cff.py
@@ -1,11 +1,7 @@
 
 import FWCore.ParameterSet.Config as cms
 
-deepTauIdraw = cms.EDProducer("DeepTauId",
-    electrons = cms.InputTag('slimmedElectrons'),
-    muons = cms.InputTag('slimmedMuons'),
-    taus = cms.InputTag('slimmedTaus'),
-    graph_file = cms.string('RecoTauTag/RecoTau/data/deepTau_2017v1_20L1024N.pb')
-)
+from RecoTauTag.RecoTau.DeepTauId_cfi import *
 
-deepTauIdSeq = cms.Sequence(deepTauIdraw)
+deepTauIdTask = cms.Task(deepTauIdraw)
+deepTauIdSeq = cms.Sequence(deepTauIdTask)
