8db87e14325850be293c87fc353d45bf4d8fcedd
Vladimir <Vladimir.Ivantchenko@cern.ch>
2018-09-25 18:50:50 +0200

    use namespace

--- SimG4CMS/Calo/interface/HFFibreFiducial.h
+++ SimG4CMS/Calo/interface/HFFibreFiducial.h
@@ -7,10 +7,8 @@
 
 #include "G4ThreeVector.hh"
 
-class HFFibreFiducial {
-
-public:
-  static int PMTNumber(const G4ThreeVector& pe_effect); // M.K. HF acceptance
+namespace HFFibreFiducial {
+  int PMTNumber(const G4ThreeVector& pe_effect); // M.K. HF acceptance
 };
 
 #endif
