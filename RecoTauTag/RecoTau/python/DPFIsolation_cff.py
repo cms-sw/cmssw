f14eba2983409cc0bd81ab1cfbef530831b11561
Michal Bluj <michal.bluj@cern.ch>
2018-09-25 17:57:29 +0200

    Reorganisation of configuration files: cff split to cfi and cff

--- RecoTauTag/RecoTau/python/DPFIsolation_cff.py
+++ RecoTauTag/RecoTau/python/DPFIsolation_cff.py
@@ -1,11 +1,7 @@
 
 import FWCore.ParameterSet.Config as cms
 
-DPFIsolation = cms.EDProducer("DPFIsolation",
-    pfcands     = cms.InputTag('packedPFCandidates'),
-    taus 	= cms.InputTag('slimmedTaus'),
-    vertices               = cms.InputTag('offlineSlimmedPrimaryVertices'),
-    graph_file  = cms.string('RecoTauTag/RecoTau/data/DPFIsolation_2017v0.pb')
-)
+from RecoTauTag.RecoTau.DPFIsolation_cff import *
 
-DPFIsolationSeq = cms.Sequence(DPFIsolation)
+DPFIsolationTask = cms.Task(DPFIsolation)
+DPFIsolationSeq = cms.Sequence(DPFIsolationTask)
