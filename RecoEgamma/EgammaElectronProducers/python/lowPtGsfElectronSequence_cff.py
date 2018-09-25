3c0f01626427eb6f266f3bfc848b46f4f557a962
Robert Bainbridge <bainbrid@users.noreply.github.com>
2018-09-25 08:51:32 +0100

    add SuperClusters to Core, other tweaks

--- RecoEgamma/EgammaElectronProducers/python/lowPtGsfElectronSequence_cff.py
+++ RecoEgamma/EgammaElectronProducers/python/lowPtGsfElectronSequence_cff.py
@@ -8,10 +8,13 @@ import FWCore.ParameterSet.Config as cms
 
 # Tracker-driven seeds
 from RecoParticleFlow.PFTracking.trackerDrivenElectronSeeds_cfi import *
-trackerDrivenElectronSeedsOpen = trackerDrivenElectronSeeds.copy()
+trackerDrivenElectronSeedsOpen = trackerDrivenElectronSeeds.clone()
 trackerDrivenElectronSeedsOpen.PassThrough = True
 trackerDrivenElectronSeedsOpen.PtThresholdSavePreId = 0.
 trackerDrivenElectronSeedsOpen.MinPt = 0.5
+
+# Above relies on the following default configurations !
+# (Whatever module produces generalTracks ...)
 
 # Electron (KF) track candidates
 from TrackingTools.GsfTracking.CkfElectronCandidateMaker_cff import *
@@ -19,27 +22,44 @@ TrajectoryFilterForElectronsOpen = TrajectoryFilterForElectrons.clone()
 TrajectoryFilterForElectronsOpen.minPt = 0.5
 TrajectoryBuilderForElectronsOpen = TrajectoryBuilderForElectrons.clone()
 TrajectoryBuilderForElectronsOpen.trajectoryFilter.refToPSet_ = "TrajectoryFilterForElectronsOpen"
-electronCkfTrackCandidatesOpen = electronCkfTrackCandidates.copy()
+electronCkfTrackCandidatesOpen = electronCkfTrackCandidates.clone()
 electronCkfTrackCandidatesOpen.TrajectoryBuilderPSet.refToPSet_ = "TrajectoryBuilderForElectronsOpen"
 electronCkfTrackCandidatesOpen.src = cms.InputTag("trackerDrivenElectronSeedsOpen","SeedsForGsf")
 
 # GSF tracks
 from TrackingTools.GsfTracking.GsfElectronGsfFit_cff import * 
-electronGsfTracksOpen = electronGsfTracks.copy()
+electronGsfTracksOpen = electronGsfTracks.clone()
 electronGsfTracksOpen.src = "electronCkfTrackCandidatesOpen"
 
 # PFTracks
 from RecoParticleFlow.PFTracking.pfTrack_cfi import *
-pfTrackOpen = pfTrack.copy()
+pfTrackOpen = pfTrack.clone()
 pfTrackOpen.GsfTrackModuleLabel = "electronGsfTracksOpen"
 
 # PFGSFTracks
 from RecoParticleFlow.PFTracking.pfTrackElec_cfi import *
-pfTrackElecOpen = pfTrackElec.copy()
+pfTrackElecOpen = pfTrackElec.clone()
 pfTrackElecOpen.applyGsfTrackCleaning = False
 pfTrackElecOpen.GsfTrackModuleLabel = "electronGsfTracksOpen"
 pfTrackElecOpen.PFRecTrackLabel = "pfTrackOpen"
+# Are the following necessary? 
 #pfTrackElecOpen.PFEcalClusters  = "particleFlowClusterECAL"
+pfTrackElecOpen.TrajInEvents = False
+pfTrackElecOpen.ModeMomentum = True
+pfTrackElecOpen.applyEGSelection = False
+pfTrackElecOpen.applyGsfTrackCleaning = False
+pfTrackElecOpen.applyAlsoGsfAngularCleaning = False
+pfTrackElecOpen.useFifthStepForTrackerDrivenGsf = True
+pfTrackElecOpen.useFifthStepForEcalDrivenGsf = True
+
+# SuperCluster generator and matching to GSF tracks
+from RecoEgamma.EgammaElectronProducers.lowPtGsfElectronSuperClusters_cfi import *
+lowPtGsfElectronSuperClusters.gsfPfRecTracks = "pfTrackElecOpen"
+
+# Above relies on the following default configurations !
+# RecoParticleFlow/PFClusterProducer/python/particleFlowClusterECALUncorrected_cfi.py
+# RecoParticleFlow/PFClusterProducer/python/particleFlowClusterECAL_cff.py
+# (particleFlowClusterECAL_cfi is generated automatically)
 
 # Low pT electron cores
 from RecoEgamma.EgammaElectronProducers.lowPtGsfElectronCores_cfi import *
@@ -59,6 +79,7 @@ lowPtGsfElectronSequence = cms.Sequence(trackerDrivenElectronSeedsOpen+
                                         electronGsfTracksOpen+
                                         pfTrackOpen+
                                         pfTrackElecOpen+
+                                        lowPtGsfElectronSuperClusters+
                                         lowPtGsfElectronCores+
                                         lowPtGsfElectrons
                                         )
