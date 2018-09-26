2b92089957f1b22d57dfe4992378b6bce616fafd
Jonas Rembser <jonas.rembser@cern.ch>
2018-09-27 01:13:14 +0200

    Fixes

--- TrackingTools/DetLayers/src/DetLessZ.h
+++ TrackingTools/DetLayers/src/DetLessZ.h
@@ -4,8 +4,22 @@
 /** Comparison operator for Dets based on the Z.
  */
 
-class GeometricSearchDet;
+#include "TrackingTools/DetLayers/src/DetLessZ.h"
+#include "TrackingTools/DetLayers/interface/GeometricSearchDet.h"
 
-bool isDetLessZ( const GeometricSearchDet* a, const GeometricSearchDet* b);
+inline bool isDetLessZ( const GeometricSearchDet* a, const GeometricSearchDet* b) {
+
+  // multiply by 1+epsilon to make it numericaly stable
+  // the epsilon should depend on the scalar precision,
+  // this is just a quick fix!
+  if (a->position().z() > 0) {
+    return a->position().z()*1.000001 < b->position().z();
+  }
+  else if (b->position().z() < 0) {
+    return a->position().z() < b->position().z()*1.000001;
+  }
+  else return true;
+}
+
 
 #endif 
