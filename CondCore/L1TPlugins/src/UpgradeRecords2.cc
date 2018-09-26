37d15ea5b8ede461fb25bf98b88ad0338f4e84ee
Vladimir <vladimir.rekovic@cern.ch>
2018-09-26 11:52:18 +0200

    Revert "Revert "L1T EMTF O2O 2018""
    This reverts commit c421a78d88bf70648b7c3a087c0bdfc043b6cc92.

--- CondCore/L1TPlugins/src/UpgradeRecords2.cc
+++ CondCore/L1TPlugins/src/UpgradeRecords2.cc
@@ -1,9 +1,5 @@
 #include "CondCore/ESSources/interface/registration_macros.h"
 
-#include "CondFormats/DataRecord/interface/L1TMuonEndcapParamsRcd.h" // Temporary copy to avoid crashes - AWB 28.08.17
-// Required to satisfy current convention for "Record" in "Global Tag Entries" 
-// https://cms-conddb-prod.cern.ch/cmsDbBrowser/search/Prod/L1TMuonEndCapParams
-
 #include "CondFormats/L1TObjects/interface/L1TMuonEndCapParams.h"
 #include "CondFormats/DataRecord/interface/L1TMuonEndCapParamsRcd.h"
 #include "CondFormats/DataRecord/interface/L1TMuonEndCapParamsO2ORcd.h"
@@ -25,8 +21,6 @@
 #include "CondFormats/L1TObjects/interface/L1TMuonGlobalParams.h"
 #include "CondFormats/DataRecord/interface/L1TMuonGlobalParamsRcd.h"
 #include "CondFormats/DataRecord/interface/L1TMuonGlobalParamsO2ORcd.h"
-
-REGISTER_PLUGIN(L1TMuonEndcapParamsRcd,  L1TMuonEndCapParams); // Temporary copy to avoid crashes - AWB 28.08.17
 
 REGISTER_PLUGIN(L1TMuonEndCapParamsRcd,  L1TMuonEndCapParams);
 REGISTER_PLUGIN(L1TMuonEndCapForestRcd,  L1TMuonEndCapForest);
