8f7f9fbf7962355d79eb0cba0131ebee85c2c168
Jonas Rembser <jonas.rembser@cern.ch>
2018-09-27 01:13:14 +0200

    Fixes

--- RecoEcal/EgammaCoreTools/interface/ClusterEtLess.h
+++ RecoEcal/EgammaCoreTools/interface/ClusterEtLess.h
@@ -1,12 +1,13 @@
 #ifndef RecoECAL_ECALClusters_ClusterEtLess_h
 #define RecoECAL_ECALClusters_ClusterEtLess_h
 
-namespace reco {
-    class CaloCluster;
-}
+#include "DataFormats/CaloRecHit/interface/CaloCluster.h"
 
 // Less than operator for sorting EcalRecHits according to energy.
-bool isClusterEtLess(const reco::CaloCluster& x, const reco::CaloCluster& y);
+inline bool isClusterEtLess(const reco::CaloCluster& x, const reco::CaloCluster& y)
+{
+  return ( (x.energy() * sin(x.position().theta())) < (y.energy() * sin(y.position().theta())) ) ;
+}
 
 #endif
 
--- RecoEcal/EgammaCoreTools/src/ClusterEtLess.cc
+++
@@ -1,8 +0,0 @@
-#include "RecoEcal/EgammaCoreTools/interface/ClusterEtLess.h"
-#include "DataFormats/CaloRecHit/interface/CaloCluster.h"
-
-// Less than operator for sorting EcalRecHits according to energy.
-bool isClusterEtLess(const reco::CaloCluster& x, const reco::CaloCluster& y)
-{
-  return ( (x.energy() * sin(x.position().theta())) < (y.energy() * sin(y.position().theta())) ) ;
-}
