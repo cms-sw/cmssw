58a5ecb2c5e34a114c2729de80e514625aad4b49
Vincenzo Innocente <vincenzo.innocente@cern.ch>
2018-09-30 21:52:39 +0200

    Fix the thread mask in the warp prefix scan (#179)
    Fix the concurrent behaviour on Volta GPUs.

--- HeterogeneousCore/CUDAUtilities/interface/prefixScan.h
+++ HeterogeneousCore/CUDAUtilities/interface/prefixScan.h
@@ -8,12 +8,12 @@ template<typename T>
 __device__
 void 
 __forceinline__
-warpPrefixScan(T * c, uint32_t i) {
+warpPrefixScan(T * c, uint32_t i, uint32_t mask) {
    auto x = c[i];
    auto laneId = threadIdx.x & 0x1f;
    #pragma unroll
    for( int offset = 1 ; offset < 32 ; offset <<= 1 ) {
-     auto y = __shfl_up_sync(0xffffffff,x, offset);
+     auto y = __shfl_up_sync(mask,x, offset);
      if(laneId >= offset) x += y;
    }
    c[i] = x;
@@ -29,17 +29,19 @@ blockPrefixScan(T * c, uint32_t size, T* ws) {
   assert(0==blockDim.x%32);
 
   auto first = threadIdx.x;
+  auto mask = __ballot_sync(0xffffffff,first<size);
 
   for (auto i=first; i<size; i+=blockDim.x) {
-    warpPrefixScan(c,i);
+    warpPrefixScan(c,i,mask);
     auto laneId = threadIdx.x & 0x1f;
     auto warpId = i/32;
     assert(warpId<32);
     if (31==laneId) ws[warpId]=c[i];
+    mask = __ballot_sync(mask,i+blockDim.x<size);
   }
   __syncthreads();
   if (size<=32) return;
-  if (threadIdx.x<32) warpPrefixScan(ws,threadIdx.x);
+  if (threadIdx.x<32) warpPrefixScan(ws,threadIdx.x,0xffffffff);
   __syncthreads();
   for (auto i=first+32; i<size; i+=blockDim.x) {
     auto warpId = i/32;
