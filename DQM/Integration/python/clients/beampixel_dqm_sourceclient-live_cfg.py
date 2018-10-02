85be0857ebcab31fd11a69c85138b6c42e979007
Mauro <mauro.dinardo@cern.ch>
2018-10-02 16:53:15 +0200

    Updated BeamPixel DQM module configuration file

--- DQM/Integration/python/clients/beampixel_dqm_sourceclient-live_cfg.py
+++ DQM/Integration/python/clients/beampixel_dqm_sourceclient-live_cfg.py
@@ -1,3 +1,4 @@
+from __future__ import print_function
 import FWCore.ParameterSet.Config as cms
 from Configuration.StandardSequences.Eras import eras
 
@@ -93,11 +94,11 @@ process.siPixelClusterShapeCachePreSplitting = siPixelClusterShapeCache.clone(sr
 process.load("RecoLocalTracker.SiPixelRecHits.PixelCPEGeneric_cfi")
 process.load("RecoPixelVertexing.Configuration.RecoPixelVertexing_cff")
 process.pixelVertices.TkFilterParameters.minPt = process.pixelTracksTrackingRegions.RegionPSet.ptMin
-process.pixelTracksTrackingRegions.RegionPSet.originRadius     = 0.4
-process.pixelTracksTrackingRegions.RegionPSet.originHalfLength = 15.
-process.pixelTracksTrackingRegions.RegionPSet.originXPos       = 0.08
-process.pixelTracksTrackingRegions.RegionPSet.originYPos       = -0.03
-process.pixelTracksTrackingRegions.RegionPSet.originZPos       = 0.
+process.pixelTracksTrackingRegions.RegionPSet.originRadius     = cms.double(0.4)
+process.pixelTracksTrackingRegions.RegionPSet.originHalfLength = cms.double(15.)
+process.pixelTracksTrackingRegions.RegionPSet.originXPos       = cms.double(0.08)
+process.pixelTracksTrackingRegions.RegionPSet.originYPos       = cms.double(-0.03)
+process.pixelTracksTrackingRegions.RegionPSet.originZPos       = cms.double(0.)
 
 
 #----------------------------
@@ -106,7 +107,7 @@ process.pixelTracksTrackingRegions.RegionPSet.originZPos       = 0.
 if (process.runType.getRunType() == process.runType.pp_run or process.runType.getRunType() == process.runType.pp_run_stage1 or 
     process.runType.getRunType() == process.runType.cosmic_run or process.runType.getRunType() == process.runType.cosmic_run_stage1 or 
     process.runType.getRunType() == process.runType.hpu_run):
-    print "[beampixel_dqm_sourceclient-live_cfg]::running pp"
+    print("[beampixel_dqm_sourceclient-live_cfg]::running pp")
 
 
     #----------------------------
@@ -140,7 +141,7 @@ if (process.runType.getRunType() == process.runType.pp_run or process.runType.ge
 # Heavy Ion Specific Section
 #----------------------------
 if (process.runType.getRunType() == process.runType.hi_run):
-    print "[beampixel_dqm_sourceclient-live_cfg]::running HI"
+    print("[beampixel_dqm_sourceclient-live_cfg]::running HI")
 
 
     #----------------------------
@@ -170,40 +171,6 @@ if (process.runType.getRunType() == process.runType.hi_run):
                                             fileName           = cms.string("/nfshome0/dqmdev/BeamMonitorDQM/BeamPixelResults.txt"))
 
 
-    #----------------------------
-    # Pixel-Tracks&Vertices Config
-    #----------------------------
-    from RecoVertex.PrimaryVertexProducer.TkClusParameters_cff import DA_vectParameters
-    offlinePrimaryVertices = cms.EDProducer(
-        "PrimaryVertexProducer",
-        verbose       = cms.untracked.bool(False),
-        TrackLabel    = cms.InputTag("generalTracks"),
-        beamSpotLabel = cms.InputTag("offlineBeamSpot"),
-        TkFilterParameters           = cms.PSet(
-            algorithm                = cms.string('filter'),
-            maxNormalizedChi2        = cms.double(10.0),
-            minPixelLayersWithHits   = cms.int32(2),
-            minSiliconLayersWithHits = cms.int32(5),
-            maxD0Significance        = cms.double(4.0), 
-            minPt                    = cms.double(0.0),
-            maxEta                   = cms.double(2.4),
-            trackQuality             = cms.string("any")),
-        TkClusParameters  = DA_vectParameters,
-        vertexCollections = cms.VPSet(
-            [cms.PSet(label             = cms.string(""),
-                      algorithm         = cms.string("AdaptiveVertexFitter"),
-                      chi2cutoff        = cms.double(2.5),
-                      minNdof           = cms.double(0.0),
-                      useBeamConstraint = cms.bool(False),
-                      maxDistanceToBeam = cms.double(1.0)),
-             cms.PSet(label             = cms.string("WithBS"),
-                      algorithm         = cms.string('AdaptiveVertexFitter'),
-                      chi2cutoff        = cms.double(2.5),
-                      minNdof           = cms.double(2.0),
-                      useBeamConstraint = cms.bool(True),
-                      maxDistanceToBeam = cms.double(1.0))]))
-
-
 #----------------------------
 # File to save beamspot info
 #----------------------------
@@ -211,7 +178,7 @@ if process.dqmRunConfig.type.value() is "production":
     process.pixelVertexDQM.fileName = cms.string("/nfshome0/dqmpro/BeamMonitorDQM/BeamPixelResults.txt")
 else:
     process.pixelVertexDQM.fileName = cms.string("/nfshome0/dqmdev/BeamMonitorDQM/BeamPixelResults.txt")
-print "[beampixel_dqm_sourceclient-live_cfg]::saving DIP file into " + str(process.pixelVertexDQM.fileName)
+print("[beampixel_dqm_sourceclient-live_cfg]::saving DIP file into " + str(process.pixelVertexDQM.fileName))
 
 
 #----------------------------
