dab749fde17be928aee11ed09359076979179042
Ivan <icali@cern.ch>
2018-10-09 20:49:55 +0200

    PR requests

--- EventFilter/RawDataCollector/src/SealModule.cc
+++ EventFilter/RawDataCollector/src/SealModule.cc
@@ -2,8 +2,10 @@
 #include "FWCore/Framework/interface/MakerMacros.h"
 #include "FWCore/ServiceRegistry/interface/ServiceMaker.h"
 #include "EventFilter/RawDataCollector/src/RawDataCollectorByLabel.h"
+#include "EventFilter/RawDataCollector/interface/RawDataMapperByLabel.h"
 
 using namespace edm::serviceregistry;
 
 DEFINE_FWK_MODULE(RawDataCollectorByLabel);
+DEFINE_FWK_MODULE(RawDataMapperByLabel);
 
