d39446161255039dc6db583002bdc1b2d2d8212e
Chris West <christopher.alan.west@gmail.com>
2018-10-11 17:36:00 +0200

    Fix typo

--- SimCalorimetry/HcalTrigPrimAlgos/src/HcalTriggerPrimitiveAlgo.cc
+++ SimCalorimetry/HcalTrigPrimAlgos/src/HcalTriggerPrimitiveAlgo.cc
@@ -653,7 +653,7 @@ void HcalTriggerPrimitiveAlgo::analyzeHFQIE10(
                }
             }
 	    // the EM bit is only used if the "second" FG bit is disabled
-            if (embit != nullptr and FG_HF_thresholds.at(1) != 255) {
+            if (embit != nullptr and FG_HF_thresholds_.at(1) != 255) {
                finegrain[ibin][0] = embit->fineGrainbit(
                      details[1].digi, details[3].digi,
                      details[0].digi, details[2].digi,
