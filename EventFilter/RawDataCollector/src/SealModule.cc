04ca2673183f836e4866d9a2f49bd44e47d7af96
Ivan <icali@cern.ch>
2018-10-12 09:11:18 +0200

    fillDescription, headers, class definition and some logoc

--- EventFilter/RawDataCollector/src/SealModule.cc
+++ EventFilter/RawDataCollector/src/SealModule.cc
@@ -2,10 +2,9 @@
 #include "FWCore/Framework/interface/MakerMacros.h"
 #include "FWCore/ServiceRegistry/interface/ServiceMaker.h"
 #include "EventFilter/RawDataCollector/src/RawDataCollectorByLabel.h"
-#include "EventFilter/RawDataCollector/interface/RawDataMapperByLabel.h"
+
 
 using namespace edm::serviceregistry;
 
 DEFINE_FWK_MODULE(RawDataCollectorByLabel);
-DEFINE_FWK_MODULE(RawDataMapperByLabel);
 
