aed2fd12c705bb162e781f35cd7d29f34cb3a640
Ivan <icali@cern.ch>
2018-10-14 08:40:35 +0200

    removed cfi, fixed fillDescritpion

--- EventFilter/RawDataCollector/python/rawDataMapperByLabel_cfi.py
+++
@@ -1,9 +0,0 @@
-import FWCore.ParameterSet.Config as cms
-
-rawDataMapper = cms.EDProducer("RawDataMapperByLabel",
-    rawCollectionList = cms.VInputTag( cms.InputTag('rawDataCollector'),
-                                       cms.InputTag('rawDataRepacker'),
-                                       cms.InputTag('rawDataReducedFormat')),
-    mainCollection= cms.InputTag('rawDataCollector')                          
-)
-
