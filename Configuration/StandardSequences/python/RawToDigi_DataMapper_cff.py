e8f614e6b8fb0580fbdbe660e157559f80a3eb35
Ivan <icali@cern.ch>
2018-10-18 01:37:33 +0200

    renaming module

--- Configuration/StandardSequences/python/RawToDigi_DataMapper_cff.py
+++ Configuration/StandardSequences/python/RawToDigi_DataMapper_cff.py
@@ -3,7 +3,7 @@ import FWCore.ParameterSet.Config as cms
 from Configuration.StandardSequences.RawToDigi_Data_cff import *
 from EventFilter.RawDataCollector.rawDataMapperByLabel_cfi import rawDataMapperByLabel
 
-RawToDigi.insert(0, rawDataRemapperByLabel)
-RawToDigi_noTk.insert(0, rawDataRemapperByLabel)
-RawToDigi_pixelOnly.insert(0, rawDataRemapperByLabel)
+RawToDigi.insert(0, rawDataMapperByLabel)
+RawToDigi_noTk.insert(0, rawDataMapperByLabel)
+RawToDigi_pixelOnly.insert(0, rawDataMapperByLabel)
 
