58f71dbace3d886ad818a69fa67c69fc981dd7a1
Ivan <icali@cern.ch>
2018-10-18 01:14:03 +0200

    Remapper + cff

--- Configuration/StandardSequences/python/RawToDigi_DataMapper_cff.py
+++ Configuration/StandardSequences/python/RawToDigi_DataMapper_cff.py
@@ -1,11 +1,9 @@
 import FWCore.ParameterSet.Config as cms
 
-from Configuration.StandardSequences.RawToDigi_cff import *
+from Configuration.StandardSequences.RawToDigi_Data_cff import *
+from EventFilter.RawDataCollector.rawDataMapperByLabel_cfi import rawDataMapperByLabel
 
 RawToDigi.insert(0, rawDataRemapperByLabel)
 RawToDigi_noTk.insert(0, rawDataRemapperByLabel)
 RawToDigi_pixelOnly.insert(0, rawDataRemapperByLabel)
-
-ecalDigis.DoRegional = False
-#False by default ecalDigis.DoRegional = False
 
