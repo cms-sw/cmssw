cc876f4d51742c5dd75b8955e8e652b883bce431
Ivan <icali@cern.ch>
2018-10-18 01:20:25 +0200

    changing sequence

--- Configuration/Applications/python/ConfigBuilder.py
+++ Configuration/Applications/python/ConfigBuilder.py
@@ -929,6 +929,7 @@ class ConfigBuilder(object):
         self.HLTDefaultCFF="Configuration/StandardSequences/HLTtable_cff"
         self.RAW2DIGIDefaultCFF="Configuration/StandardSequences/RawToDigi_Data_cff"
         self.RAW2DIGIDataRemapperCFF="Configuration/StandardSequences/RawToDigi_DataRemapper_cff"
+        if self._options.isRepacked: self.RAW2DIGIDefaultCFF="Configuration/StandardSequences/RawToDigi_DataMapper_cff"
         self.L1RecoDefaultCFF="Configuration/StandardSequences/L1Reco_cff"
         self.L1TrackTriggerDefaultCFF="Configuration/StandardSequences/L1TrackTrigger_cff"
         self.RECODefaultCFF="Configuration/StandardSequences/Reconstruction_Data_cff"
@@ -1548,12 +1549,9 @@ class ConfigBuilder(object):
         return
 
     def prepare_RAW2DIGI(self, sequence = "RawToDigi"):
-        if self._options.isRepacked:
-            self.loadDefaultOrSpecifiedCFF(sequence,self.RAW2DIGIDataRemapperCFF)
-        else:
-            self.loadDefaultOrSpecifiedCFF(sequence,self.RAW2DIGIDefaultCFF)
-        
-        self.scheduleSequence(sequence,'raw2digi_step')	
+        self.loadDefaultOrSpecifiedCFF(sequence,self.RAW2DIGIDefaultCFF)
+        self.scheduleSequence(sequence,'raw2digi_step')
+         #          if self._options.isRepacked:
         #self.renameInputTagsInSequence(sequence)
         return
 
@@ -2223,8 +2221,8 @@ class ConfigBuilder(object):
         if self._options.isRepacked:
             self.pythonCfgCode +="\n"
             self.pythonCfgCode +="from Configuration.Applications.ConfigBuilder import MassReplaceInputTag\n"
-            self.pythonCfgCode +="MassReplaceInputTag(process)\n"
-            MassReplaceInputTag(self.process, "rawDataRemapperByLabel", "rawDataCollector")
+            self.pythonCfgCode +="MassReplaceInputTag(process, new=\"rawDataMapperByLabel\", old=\"rawDataCollector\")\n"
+            MassReplaceInputTag(self.process, new="rawDataMapperByLabel", old="rawDataCollector")
 
         # special treatment in case of production filter sequence 2/2
         if self.productionFilterSequence:
