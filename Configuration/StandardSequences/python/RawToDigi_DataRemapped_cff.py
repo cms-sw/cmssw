2c33df323d320e575ed229b6259c2f7ecc165772
Ivan <icali@cern.ch>
2018-10-17 16:09:21 +0200

    the born of the remapper sequence

---
+++ Configuration/StandardSequences/python/RawToDigi_DataRemapped_cff.py
@@ -0,0 +1,8 @@
+import FWCore.ParameterSet.Config as cms
+
+from Configuration.StandardSequences.RawToDigi_cff import *
+
+
+ecalDigis.DoRegional = False
+#False by default ecalDigis.DoRegional = False
+
