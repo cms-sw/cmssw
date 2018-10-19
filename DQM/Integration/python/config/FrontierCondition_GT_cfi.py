1d9665e98778a1e22d3fb5fb3260fa688dc81f8d
threus <threus@cern.ch>
2018-10-19 14:59:33 +0200

    GT update to 10_3_X

--- DQM/Integration/python/config/FrontierCondition_GT_cfi.py
+++ DQM/Integration/python/config/FrontierCondition_GT_cfi.py
@@ -1,3 +1,3 @@
 import FWCore.ParameterSet.Config as cms
 from Configuration.StandardSequences.FrontierConditions_GlobalTag_cff import *
-GlobalTag.globaltag = "101X_dataRun2_HLT_v7"
+GlobalTag.globaltag = "103X_dataRun2_HLT_v1"
