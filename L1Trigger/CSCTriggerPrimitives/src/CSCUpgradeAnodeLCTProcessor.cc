23022d67272943884fc09e40406787a2d7f69168
tahuang1991 <taohuang@physics.tamu.edu>
2018-10-19 18:51:02 +0200

    AnodeLCTProcessor: fix a bug in ghost cancellation logic

--- L1Trigger/CSCTriggerPrimitives/src/CSCUpgradeAnodeLCTProcessor.cc
+++ L1Trigger/CSCTriggerPrimitives/src/CSCUpgradeAnodeLCTProcessor.cc
@@ -122,6 +122,7 @@ void CSCUpgradeAnodeLCTProcessor::ghostCancellationLogic()
 
 void CSCUpgradeAnodeLCTProcessor::ghostCancellationLogicOneWire(const int key_wire)
 {
+  if (key_wire == 0) return;//ignore ghost cancellation for wire=0
   int ghost_cleared[2];
 
     for (int i_pattern = 0; i_pattern < 2; i_pattern++) {
@@ -135,12 +136,22 @@ void CSCUpgradeAnodeLCTProcessor::ghostCancellationLogicOneWire(const int key_wi
           qual_this = (qual_this & 0x03);
         // Previous wire.
         int dt = -1;
-        int qual_prev = (key_wire > 0) ? quality[key_wire-1][i_pattern] : 0;
-        if (qual_prev > 0) {
+        for (auto& p : lct_list){
+          if (not (p.isValid() and p.getKeyWG() == key_wire -1 and 1-p.getAccelerator() == i_pattern)) continue;
+            
+          bool ghost_cleared_prev = false;
+          int qual_prev = p.getQuality();
+          int first_bx_prev = p.getBX();
+          if (infoV > 1) LogTrace("CSCAnodeLCTProcessor")
+            << "ghost concellation logic " << ((i_pattern == 0) ? "Accelerator" : "Collision")
+            << " key_wire "<< key_wire <<" quality "<< qual_this <<" bx " <<  first_bx[key_wire]
+            << " previous key_wire "<< key_wire -1 <<" quality "<< qual_prev <<" bx " <<  first_bx[key_wire-1];
+
+          //int dt = first_bx[key_wire] - first_bx[key_wire-1];
           if (use_corrected_bx)
-            dt = first_bx_corrected[key_wire] - first_bx_corrected[key_wire-1];
+            dt = first_bx_corrected[key_wire] - first_bx_prev;
           else
-            dt = first_bx[key_wire] - first_bx[key_wire-1];
+            dt = first_bx[key_wire] - first_bx_prev;
           // hack to run the Phase-II ME2/1, ME3/1 and ME4/1 ILT
           if (isSLHC_ and (runME21ILT_ or runME3141ILT_))
             qual_prev = (qual_prev & 0x03);
@@ -159,7 +170,11 @@ void CSCUpgradeAnodeLCTProcessor::ghostCancellationLogicOneWire(const int key_wi
                 (qual_prev > qual_this) )
               ghost_cleared[i_pattern] = 1;
           }
-        }
+          else if (dt < 0 && dt*(-1) <= ghost_cancellation_bx_depth){
+            if ((!ghost_cancellation_side_quality) ||
+                (qual_prev < qual_this) )
+              ghost_cleared_prev = true;
+          }
 
         // Skip this step if this wire is already declared "ghost".
         if (ghost_cleared[i_pattern] == 1) {
@@ -167,7 +182,15 @@ void CSCUpgradeAnodeLCTProcessor::ghostCancellationLogicOneWire(const int key_wi
             << ((i_pattern == 0) ? "Accelerator" : "Collision")
             << " pattern ghost cancelled on key_wire " << key_wire <<" q="<<qual_this
             << "  by wire " << key_wire-1<<" q="<<qual_prev<<"  dt="<<dt;
-          continue;
+        }
+        if (ghost_cleared_prev) {
+          if (infoV > 1) LogTrace("CSCAnodeLCTProcessor")
+            << ((i_pattern == 0) ? "Accelerator" : "Collision")
+            << " pattern ghost cancelled on key_wire " << key_wire - 1 <<" q="<<qual_prev
+            << "  by wire " << key_wire <<" q="<<qual_this;
+            p.setValid(0);//clean prev ALCT
+        }
+
         }
 
     }
