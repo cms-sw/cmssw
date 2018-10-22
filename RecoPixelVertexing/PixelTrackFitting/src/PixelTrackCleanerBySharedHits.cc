fb699c5a0aaab950937ccafd2af7ca1267d36055
Vincenzo Innocente <vincenzo.innocente@cern.ch>
2018-10-22 16:11:30 +0200

    simplify

--- RecoPixelVertexing/PixelTrackFitting/src/PixelTrackCleanerBySharedHits.cc
+++ RecoPixelVertexing/PixelTrackFitting/src/PixelTrackCleanerBySharedHits.cc
@@ -5,6 +5,7 @@
 #include "FWCore/MessageLogger/interface/MessageLogger.h"
 #include "DataFormats/TrackerCommon/interface/TrackerTopology.h"
 #include <cassert>
+#include <cstdint>
 
 using namespace std;
 using namespace reco;
@@ -27,78 +28,68 @@ void PixelTrackCleanerBySharedHits::cleanTracks(TracksWithTTRHs & trackHitPairs)
   if (size <= 1) return;
 
   // sort (stabilize cleaning)
-  float pt[size]; 
+  uint16_t score[size]; 
   unsigned int ind[size];
-  for (auto i = 0U; i < size; ++i) {ind[i]=i; pt[i]=trackHitPairs[i].first->pt();}
-  std::sort(ind,ind+size,[&](unsigned int i, unsigned int j){return pt[i]>pt[j];});
-  assert(pt[ind[0]]>=pt[ind[size-1]]);
+  for (auto i = 0U; i < size; ++i) {
+     ind[i]=i; 
+     score[i]= 32000-std::min(32000, int(trackHitPairs[i].first->chi2()*100.f));  // chi2: smaller is better
+     if (trackHitPairs[i].second.size()==4) score[i]+=32001; //  s4 always better than s3
+   }
+   std::sort(ind,ind+size,[&](unsigned int i, unsigned int j){return score[i]>score[j];});
 
   int killed=0;
   auto kill = [&](unsigned int k) { assert(trackHitPairs[k].first); killed++; delete trackHitPairs[k].first; trackHitPairs[k].first=nullptr;};
 
-  auto iTrack1 = 0U;
-  auto iTrack2 = 0U;
-  auto track1 = trackHitPairs[iTrack1].first;
-  auto track2 = trackHitPairs[iTrack1].first;
-  auto cleanTrack = [&](){
-    auto mpt = track1->pt(); // larger pt as sorted
-    if (mpt<2.) { // tuned
-      kill(iTrack2); return false;  // lower pt
-    }else{
-      if (track1->chi2() < track2->chi2()) { kill(iTrack2); return false; }
-    }
-    kill(iTrack1);
-    return true;
-  };
+  // sorted: first is always better!
 
   // first loop: only first    two hits....
   for (auto i = 0U; i < size; ++i) {
-    iTrack1 = ind[i];
-    track1 = trackHitPairs[iTrack1].first;
+    auto iTrack1 = ind[i];
+    auto track1 = trackHitPairs[iTrack1].first;
     if (!track1) continue;
     auto const & recHits1 = trackHitPairs[iTrack1].second;
     for (auto j = i+1; j < size; ++j) {
-      iTrack2 = ind[j];
-      track2 = trackHitPairs[iTrack2].first;
+      auto iTrack2 = ind[j];
+      auto track2 = trackHitPairs[iTrack2].first;
       if (!track2) continue;
       auto const & recHits2 = trackHitPairs[iTrack2].second;
       if (recHits1[0] != recHits2[0]) continue;	
       if (recHits1[1] != recHits2[1]) continue;
-      if(cleanTrack()) break;	
+      kill(iTrack2);	
     }  // tk2
   } // tk1
 
 
   // second loop: first and third hits....
   for (auto i = 0U; i < size; ++i) {
-    iTrack1 = ind[i];
-    track1 = trackHitPairs[iTrack1].first;
+    auto iTrack1 = ind[i];
+    auto track1 = trackHitPairs[iTrack1].first;
     if (!track1) continue;
     auto const & recHits1 = trackHitPairs[iTrack1].second;
     if (recHits1.size()<3) continue;
     for (auto j = i+1; j < size; ++j) {
-      iTrack2 = ind[j];
-      track2 = trackHitPairs[iTrack2].first;
+      auto iTrack2 = ind[j];
+      auto track2 = trackHitPairs[iTrack2].first;
       if (!track2) continue;
       auto const & recHits2 = trackHitPairs[iTrack2].second;
       if (recHits2.size()<3) continue;
       if (recHits1[0] != recHits2[0]) continue;
       if (recHits1[2] != recHits2[2]) continue;
-      if(cleanTrack()) break;
+      kill(iTrack2);
     }  // tk2
   } // tk1
 
 
   // final loop: all the rest
   for (auto i = 0U; i < size; ++i) {
-    iTrack1 = ind[i];
-    track1 = trackHitPairs[iTrack1].first;
+    auto iTrack1 = ind[i];
+    auto track1 = trackHitPairs[iTrack1].first;
     if (!track1) continue;
     auto const & recHits1 = trackHitPairs[iTrack1].second;
     auto s1 = recHits1.size();
     for (auto j = i+1; j < size; ++j) {
-      iTrack2 = ind[j];
-      track2 = trackHitPairs[iTrack2].first;
+      auto iTrack2 = ind[j];
+      auto track2 = trackHitPairs[iTrack2].first;
       if (!track2) continue;
       auto const & recHits2 = trackHitPairs[iTrack2].second;
       auto s2 = recHits1.size();
@@ -112,14 +103,11 @@ void PixelTrackCleanerBySharedHits::cleanTracks(TracksWithTTRHs & trackHitPairs)
       }
       if(useQuadrupletAlgo_) {
         if(commonRecHits >= 1) {
-          if     (s1 > s2) kill(iTrack2);
-          else if(s1 < s2) { kill(iTrack1); break;}
-          else if(s1 == 3) { if(cleanTrack()) break; } // same number of hits
-          else if(commonRecHits > 1) { if(cleanTrack()) break; }// same number of hits, size != 3 (i.e. == 4)
+          if(s1 == 3 || commonRecHits > 1) { kill(iTrack2); }
         }
       }
       else if (commonRecHits > 1) {
-        if(cleanTrack()) break;
+       kill(iTrack2);
       }
     } // tk2
   }  //tk1
