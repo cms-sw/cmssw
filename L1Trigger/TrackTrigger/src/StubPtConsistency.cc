7923f154987bbdbae6d95f507a2cf6c9e144be3c
Emily MacDonald <macdonald.e17@gmail.com>
2018-10-23 16:27:00 +0200

    Addressing Kevin's suggestions

--- L1Trigger/TrackTrigger/src/StubPtConsistency.cc
+++ L1Trigger/TrackTrigger/src/StubPtConsistency.cc
@@ -6,42 +6,38 @@
 namespace StubPtConsistency {
 
   float getConsistency(TTTrack< Ref_Phase2TrackerDigi_ > aTrack, const TrackerGeometry* theTrackerGeom, const TrackerTopology* tTopo, double mMagneticFieldStrength, int nPar) {
+
+    if( !(nPar==4 || nPar==5) ) throw cms::Exception("IncorrectInput") << "Not a valid nPar option!";
+
     double trk_bendchi2 = 0.0;
-
-    if ( !(nPar==4 || nPar==5)) {
-      std::cerr << "Not a valid nPar option!" << std::endl;
-      return trk_bendchi2;
-    }
-
     double bend_resolution = 0.483;
+    float speedOfLight = 3.0E8; //in m/s
+    float UnitConversion = 1.0E11;
+
     // Need the pT signed in order to determine if bend is positive or negative
-    float trk_signedPt = 0.3*3.811202/100.0/(aTrack.getRInv(nPar));
+    float trk_signedPt = (speedOfLight/UnitConversion)*mMagneticFieldStrength/aTrack.getRInv(nPar); // P(MeV/c) = (c/10^9)·Q·B(kG)·R(cm)
 
     // loop over stubs
     std::vector< edm::Ref< edmNew::DetSetVector< TTStub< Ref_Phase2TrackerDigi_ > >, TTStub< Ref_Phase2TrackerDigi_ > > > stubRefs = aTrack.getStubRefs();
     int nStubs = stubRefs.size();
 
-    for (int is=0; is<nStubs; is++) {
-      DetId detIdStub = theTrackerGeom->idToDet( (stubRefs.at(is)->getClusterRef(0))->getDetId() )->geographicalId();
-      MeasurementPoint coords = stubRefs.at(is)->getClusterRef(0)->findAverageLocalCoordinatesCentered();
+    for ( const auto& stubRef : stubRefs) {
+      DetId detIdStub = theTrackerGeom->idToDet( (stubRef->getClusterRef(0))->getDetId() )->geographicalId();
+      MeasurementPoint coords = stubRef->getClusterRef(0)->findAverageLocalCoordinatesCentered();
       const GeomDet* theGeomDet = theTrackerGeom->idToDet(detIdStub);
       Global3DPoint posStub = theGeomDet->surface().toGlobal( theGeomDet->topology().localPosition(coords) );
 
       float stub_r = posStub.perp();
       float stub_z = posStub.z();
 
-      bool isBarrel = false;
-      if ( detIdStub.subdetId()==StripSubdetector::TOB ) {
-        isBarrel = true;
-      }
+      bool isBarrel = (detIdStub.subdetId()==StripSubdetector::TOB);
 
-      //input tilted module correction
       const GeomDetUnit* det0 = theTrackerGeom->idToDetUnit( detIdStub );
       const GeomDetUnit* det1 = theTrackerGeom->idToDetUnit( tTopo->partnerDetId( detIdStub ) );
       const PixelGeomDetUnit* unit = reinterpret_cast<const PixelGeomDetUnit*>( det0 );
       const PixelTopology& topo = unit->specificTopology();
 
-      //Calculation of snesor spacing obtained from TMTT: https://github.com/CMS-TMTT/cmssw/blob/TMTT_938/L1Trigger/TrackFindingTMTT/src/Stub.cc#L138-L146
+      // Calculation of snesor spacing obtained from TMTT: https://github.com/CMS-TMTT/cmssw/blob/TMTT_938/L1Trigger/TrackFindingTMTT/src/Stub.cc#L138-L146
       float stripPitch = topo.pitch().first;
 
       float modMinR = std::min(det0->position().perp(),det1->position().perp());
@@ -50,16 +46,20 @@ namespace StubPtConsistency {
       float modMaxZ = std::max(det0->position().z(),det1->position().z());
       float sensorSpacing = sqrt((modMaxR-modMinR)*(modMaxR-modMinR) + (modMaxZ-modMinZ)*(modMaxZ-modMinZ));
 
-      //Approximation of phiOverBendCorrection, from TMTT: https://github.com/CMS-TMTT/cmssw/blob/TMTT_938/L1Trigger/TrackFindingTMTT/src/Stub.cc#L440-L448
+      // Approximation of phiOverBendCorrection, from TMTT: https://github.com/CMS-TMTT/cmssw/blob/TMTT_938/L1Trigger/TrackFindingTMTT/src/Stub.cc#L440-L448
       bool tiltedBarrel = (isBarrel && tTopo->tobSide(detIdStub)!=3);
+      float gradient = 0.886454;
+      float intercept = 0.504148;
       float correction;
-      if (tiltedBarrel) correction= 0.886454*fabs(stub_z)/stub_r+0.504148;
-      else if (isBarrel) correction=1;
-      else correction= fabs(stub_z)/stub_r;
+      if (tiltedBarrel) correction = gradient*fabs(stub_z)/stub_r + intercept;
+      else if (isBarrel) correction = 1;
+      else correction = fabs(stub_z)/stub_r;
 
-      float stubBend = stubRefs.at(is)->getTriggerBend();
-      if (!isBarrel && stub_z<0.0) stubBend=-stubBend;
-      float trackBend = -(sensorSpacing*stub_r*mMagneticFieldStrength*(3.0E8/2.0E11))/(stripPitch*trk_signedPt*correction);
+      float stubBend = stubRef->getTriggerBend();
+      if (!isBarrel && stub_z<0.0) stubBend=-stubBend; // flip sign of bend if in negative end cap
+
+      // B*c/2E11 - converts q/pt to track angle at some radius from beamline
+      float trackBend = -(sensorSpacing*stub_r*mMagneticFieldStrength*(speedOfLight/UnitConversion/2))/(stripPitch*trk_signedPt*correction);
       float bendDiff = trackBend-stubBend;
 
       trk_bendchi2 += (bendDiff*bendDiff)/(bend_resolution*bend_resolution);
