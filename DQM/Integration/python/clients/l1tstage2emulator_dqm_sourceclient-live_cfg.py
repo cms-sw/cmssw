eb573d7e759767672fbc48b0f0d1ec86a3949e0a
Quan Wang <BetterWang@gmail.com>
2018-10-23 13:25:22 +0200

    [+] adding HIPhysics to the HLT

--- DQM/Integration/python/clients/l1tstage2emulator_dqm_sourceclient-live_cfg.py
+++ DQM/Integration/python/clients/l1tstage2emulator_dqm_sourceclient-live_cfg.py
@@ -65,7 +65,7 @@ from HLTrigger.HLTfilters.hltHighLevel_cfi import hltHighLevel
 process.hltFatEventFilter = hltHighLevel.clone()
 process.hltFatEventFilter.throw = cms.bool(False)
 # HLT_Physics now has the event % 107 filter as well as L1FatEvents
-process.hltFatEventFilter.HLTPaths = cms.vstring('HLT_L1FatEvents_v*', 'HLT_Physics_v*')
+process.hltFatEventFilter.HLTPaths = cms.vstring('HLT_L1FatEvents_v*', 'HLT_Physics_v*', 'HLT_HIPhysics_v*')
 
 # This can be used if HLT filter not available in a run
 process.selfFatEventFilter = cms.EDFilter("HLTL1NumberFilter",
