6c387069df4415efa30b40fb3059624708f6c354
Fabio Cossutti <fabio.cossutti@ts.infn.it>
2018-10-24 14:34:51 +0200

    Scenario D34 for crystal bars along z flat no staggering in blocks of 16

--- Configuration/PyReleaseValidation/python/relval_steps.py
+++ Configuration/PyReleaseValidation/python/relval_steps.py
@@ -2721,6 +2721,7 @@ defaultDataSets['2023D29']=''
 defaultDataSets['2023D30']=''
 defaultDataSets['2023D31']=''
 defaultDataSets['2023D33']=''
+defaultDataSets['2023D34']=''
 
 keys=defaultDataSets.keys()
 for key in keys:
