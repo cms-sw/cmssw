e4866ea53d18e97414a0b986aefa3f40dca3a9c2
Patrick Gartung <gartung@fnal.gov>
2018-10-24 09:00:41 -0500

    Move postprocess-scan-build.py to Utilities/StaticAnalyzers

---
+++ Utilities/StaticAnalyzers/scripts/postprocess-scan-build.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python
+import lxml
+from bs4 import BeautifulSoup
+url=r'index-full.html'
+page=open(url)
+soup=BeautifulSoup(page.read(),features="lxml")
+seen=set()
+tables=soup.find_all('table',recursive=True)
+rows = tables[2].findChildren('tr')
+for row in rows:
+    cells=row.findChildren('td')
+    key=str(cells[2])+str(cells[4])+str(cells[5])
+    if key in seen:
+        row.decompose()
+    else:
+        seen.add(key)
+print soup.prettify("latin1")
