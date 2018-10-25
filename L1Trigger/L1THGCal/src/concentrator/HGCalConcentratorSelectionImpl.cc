268de5f7507ca9fdff709033b910be9f8a7e6a31
Samuel Webb <samuel.webb@cern.ch>
2018-10-25 12:31:58 +0200

    Update add SuperTriggerCell algorithm to concentrator

--- L1Trigger/L1THGCal/src/concentrator/HGCalConcentratorSelectionImpl.cc
+++ L1Trigger/L1THGCal/src/concentrator/HGCalConcentratorSelectionImpl.cc
@@ -53,3 +53,77 @@ bestChoiceSelectImpl(const std::vector<l1t::HGCalTriggerCell>& trigCellVecInput,
   
 }
 
+l1t::HGCalSuperTriggerCellMap*
+HGCalConcentratorSelectionImpl::
+getSuperTriggerCell_(uint32_t module_id, l1t::HGCalTriggerCell TC)
+{
+
+  HGCalDetId TC_id( TC.detId() );
+  int TC_wafer = TC_id.wafer();
+
+  int TC_12th = ( TC_id.cell() & 0x3a );
+
+  
+  long SuperTriggerCellMap_id = 0;
+  if(TC_id.subdetId()==HGCHEB) SuperTriggerCellMap_id = TC_id.cell(); //scintillator
+
+  else SuperTriggerCellMap_id = TC_wafer<<6 | TC_12th;
+
+  return &mapSuperTriggerCellMap_[module_id][SuperTriggerCellMap_id];
+
+}
+
+void 
+HGCalConcentratorSelectionImpl::
+clearSuperTriggerCellMap(){
+  mapSuperTriggerCellMap_.clear();
+}
+
+
+void 
+HGCalConcentratorSelectionImpl::
+superTriggerCellSelectImpl(uint32_t module_id ,const std::vector<l1t::HGCalTriggerCell>& trigCellVecInput, std::vector<l1t::HGCalTriggerCell>& trigCellVecOutput)
+{ 
+
+  //Clear SuperTriggerCell Map
+  this->clearSuperTriggerCellMap();
+
+  for (size_t i = 0; i<trigCellVecInput.size();i++){
+
+    getSuperTriggerCell_( module_id, trigCellVecInput.at(i) )->addTriggerCell( trigCellVecInput.at(i) );
+
+  }
+    
+  for (size_t i = 0; i<trigCellVecInput.size();i++){
+
+    //If scintillator use a simple threshold cut
+    if ( HGCalDetId(trigCellVecInput[i].detId()).subdetId()==HGCHEB ){
+
+      if  ( ( trigCellVecInput[i].hwPt() >= TCThresholdBH_ADC_ ) && (trigCellVecInput[i].mipPt() >= triggercell_threshold_scintillator_ ) ){
+
+	trigCellVecOutput.push_back( trigCellVecInput.at(i) );
+
+      }
+
+    }
+
+    else{
+   
+      l1t::HGCalSuperTriggerCellMap* TCmap = getSuperTriggerCell_( module_id, trigCellVecInput.at(i) );    
+
+      
+      //Check if TC is the most energetic in superTC and assign the full hwPt of the superTC
+      //Else zeroed
+      
+      if(TCmap->maxTriggerCell().detId() == trigCellVecInput.at(i).detId()) {
+		
+	trigCellVecOutput.push_back( trigCellVecInput.at(i) );
+	trigCellVecOutput.back().setHwPt(TCmap->hwPt());
+	
+      }
+
+    }
+    
+  }  
+  
+}
