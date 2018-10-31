8c3fb204fa365dcdd673414346aca52e4febaeeb
Maria Di Domenico <didomenico13@gmail.com>
2018-10-31 13:05:07 +0100

    - Implemented on runTauIdMVA the option to work with new training files quantized - Added a new parameter 'version' on runTauIdMVA, used on DPFIsolation - Changes on DeepTauId to reduce memory consumption

--- RecoTauTag/RecoTau/test/runDeepTauIDsOnMiniAOD.py
+++ RecoTauTag/RecoTau/test/runDeepTauIDsOnMiniAOD.py
@@ -9,7 +9,7 @@ import FWCore.ParameterSet.Config as cms
 updatedTauName = "slimmedTausNewID"
 minimalOutput = True
 eventsToProcess = 1000
-nThreads = 2
+nThreads = 1
 
 process = cms.Process('TauID')
 process.load('Configuration.StandardSequences.MagneticField_cff')
@@ -22,7 +22,7 @@ process.GlobalTag.globaltag = '94X_mc2017_realistic_v14'
 # Input source
 process.source = cms.Source('PoolSource', fileNames = cms.untracked.vstring(
     # File from dataset /GluGluHToTauTau_M125_13TeV_powheg_pythia8/RunIIFall17MiniAODv2-PU2017_12Apr2018_94X_mc2017_realistic_v14-v1/MINIAODSIM
-    '/store/mc/RunIIFall17MiniAODv2/GluGluHToTauTau_M125_13TeV_powheg_pythia8/MINIAODSIM/PU2017_12Apr2018_94X_mc2017_realistic_v14-v1/90000/0498CD6A-CC42-E811-95D3-008CFA1CB8A8.root'
+     '/store/mc/RunIIFall17MiniAODv2/GluGluHToTauTau_M125_13TeV_powheg_pythia8/MINIAODSIM/PU2017_12Apr2018_94X_mc2017_realistic_v14-v1/90000/0498CD6A-CC42-E811-95D3-008CFA1CB8A8.root'
 ))
 
 process.maxEvents = cms.untracked.PSet( input = cms.untracked.int32(eventsToProcess) )
@@ -34,8 +34,11 @@ tauIdEmbedder = tauIdConfig.TauIDEmbedder(process, cms, debug = False,
                     toKeep = [ "2017v2", "dR0p32017v2", "newDM2017v2",
                                "deepTau2017v1",
                                "DPFTau_2016_v0",
-                               #"DPFTau_2016_v1"
-                               ])
+                               "DPFTau_2016_v1",
+                               "deepTau2017v1Q",
+                               #"DPFTau_2016_v0Q",
+                               #"DPFTau_2016_v1Q",
+                               ]) #DPF quantized files are not working for release 94X
 tauIdEmbedder.runTauID()
 
 # Output definition
