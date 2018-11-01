717fbd7ce334785ffeea4c6809e4a0c476bebf8e
Andres Vargas <vargasa@cua.edu>
2018-11-01 20:46:14 +0100

    CleanUp
    	* None of this removed code was being executed due to matSize == 0

--- Validation/Geometry/src/MaterialBudgetCategorizer.cc
+++ Validation/Geometry/src/MaterialBudgetCategorizer.cc
@@ -30,46 +30,7 @@ void MaterialBudgetCategorizer::buildMaps()
   //----- Build map material name - volume index
   const G4MaterialTable* matTable = G4Material::GetMaterialTable();
   G4int matSize = matTable->size();
-  for( ii = 0; ii < matSize; ii++ ) {
-    theMaterialMap[ (*matTable)[ii]->GetName()] = ii+1;
-  }
-  
-  //----- Build map material name
-  for( int ii = 0; ii < matSize; ii++ ) {
-    float sup,sen,cab,col,ele,oth,air;
-    sup=sen=cab=col=ele=0.;
-    oth=1.;
-    air=0;
-    edm::LogInfo("MaterialBudget") << "MaterialBudgetCategorizer: Material " << (*matTable)[ii]->GetName() << " prepared";
-    if((*matTable)[ii]->GetName()=="Air"
-       ||
-       (*matTable)[ii]->GetName()=="Vacuum"
-       ) {
-      air=1.000; 
-      oth=0.000;
-    }
-    // actually this class does not work if there are spaces into material names --> Recover material properties
-    if((*matTable)[ii]->GetName()=="Carbon fibre str.") { 
-      sup=1.000; 
-      oth=0.000;
-    }
-    // X0
-    theX0Map[ (*matTable)[ii]->GetName() ].push_back(sup); // sup
-    theX0Map[ (*matTable)[ii]->GetName() ].push_back(sen); // sen
-    theX0Map[ (*matTable)[ii]->GetName() ].push_back(cab); // cab
-    theX0Map[ (*matTable)[ii]->GetName() ].push_back(col); // col
-    theX0Map[ (*matTable)[ii]->GetName() ].push_back(ele); // ele
-    theX0Map[ (*matTable)[ii]->GetName() ].push_back(oth); // oth
-    theX0Map[ (*matTable)[ii]->GetName() ].push_back(air); // air
-    // L0
-    theL0Map[ (*matTable)[ii]->GetName() ].push_back(sup); // sup
-    theL0Map[ (*matTable)[ii]->GetName() ].push_back(sen); // sen
-    theL0Map[ (*matTable)[ii]->GetName() ].push_back(cab); // cab
-    theL0Map[ (*matTable)[ii]->GetName() ].push_back(col); // col
-    theL0Map[ (*matTable)[ii]->GetName() ].push_back(ele); // ele
-    theL0Map[ (*matTable)[ii]->GetName() ].push_back(oth); // oth
-    theL0Map[ (*matTable)[ii]->GetName() ].push_back(air); // air
-  }
+  edm::LogInfo("MaterialBudget") << "MaterialBudgetCategorizer: matSize:" << matSize;
   
   //----- Build map material name - X0 contributes
   edm::LogInfo("MaterialBudget") << "MaterialBudgetCategorizer: Fill X0 Map";
@@ -88,39 +49,6 @@ void MaterialBudgetCategorizer::buildMaps()
   // summary of all the materials loaded
   edm::LogInfo("MaterialBudget") << "MaterialBudgetCategorizer: Material Summary Starts";
   G4EmCalculator calc;
-  for( ii = 0; ii < matSize; ii++ ) {
-    edm::LogInfo("MaterialBudget") 
-      << "MaterialBudgetCateogorizer: Material " << (*matTable)[ii]->GetName()
-      << std::endl
-      << "\t density = " << G4BestUnit((*matTable)[ii]->GetDensity(),"Volumic Mass")
-      << std::endl
-      << "\t X0 = "      << (*matTable)[ii]->GetRadlen()             << " mm"
-      << std::endl
-      << "\t Energy threshold for photons for 100 mm range = "
-      << G4BestUnit(calc.ComputeEnergyCutFromRangeCut(100, G4String("gamma"), (*matTable)[ii]->GetName()) , "Energy")
-      << std::endl
-      << " SUP " << theX0Map[ (*matTable)[ii]->GetName() ][0] 
-      << " SEN " << theX0Map[ (*matTable)[ii]->GetName() ][1]
-      << " CAB " << theX0Map[ (*matTable)[ii]->GetName() ][2]
-      << " COL " << theX0Map[ (*matTable)[ii]->GetName() ][3]
-      << " ELE " << theX0Map[ (*matTable)[ii]->GetName() ][4]
-      << " OTH " << theX0Map[ (*matTable)[ii]->GetName() ][5]
-      << " AIR " << theX0Map[ (*matTable)[ii]->GetName() ][6]
-      << std::endl
-      << "\t Lambda0 = " << (*matTable)[ii]->GetNuclearInterLength() << " mm"
-      << std::endl
-      << " SUP " << theL0Map[ (*matTable)[ii]->GetName() ][0] 
-      << " SEN " << theL0Map[ (*matTable)[ii]->GetName() ][1]
-      << " CAB " << theL0Map[ (*matTable)[ii]->GetName() ][2]
-      << " COL " << theL0Map[ (*matTable)[ii]->GetName() ][3]
-      << " ELE " << theL0Map[ (*matTable)[ii]->GetName() ][4]
-      << " OTH " << theL0Map[ (*matTable)[ii]->GetName() ][5]
-      << " AIR " << theL0Map[ (*matTable)[ii]->GetName() ][6]
-      << std::endl;
-    if( theX0Map[ (*matTable)[ii]->GetName() ][5] == 1 || theL0Map[ (*matTable)[ii]->GetName() ][5] == 1 )
-      edm::LogWarning("MaterialBudget") 
-	<< "MaterialBudgetCategorizer Material with no category: " << (*matTable)[ii]->GetName();
-  }
 }
 
 void MaterialBudgetCategorizer::buildCategoryMap(std::string theMaterialFileName, std::map<std::string,std::vector<float> >& theMap) {
@@ -138,6 +66,7 @@ void MaterialBudgetCategorizer::buildCategoryMap(std::string theMaterialFileName
   while(theMaterialFile) {
     theMaterialFile >> materialName;
     theMaterialFile >> sup >> sen >> cab >> col >> ele;
+
     oth = 0.000;
     air = 0.000;
     theMap[materialName].clear();        // clear before re-filling
@@ -148,7 +77,7 @@ void MaterialBudgetCategorizer::buildCategoryMap(std::string theMaterialFileName
     theMap[materialName].push_back(ele); // ele
     theMap[materialName].push_back(oth); // oth
     theMap[materialName].push_back(air); // air
-    LogDebug("MaterialBudget") 
+    edm::LogInfo("MaterialBudget") 
       << "MaterialBudgetCategorizer: Material " << materialName << " filled " 
       << " SUP " << sup 
       << " SEN " << sen 
