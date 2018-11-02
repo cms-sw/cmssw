6e83f882efe827bd26a2c15640ae80c37b39ca36
Felice <felice.pantaleo@cern.ch>
2018-11-02 19:44:49 +0100

    changing interface

--- RecoHGCal/TICL/plugins/PatternRecognitionbyMultiClusters.cc
+++ RecoHGCal/TICL/plugins/PatternRecognitionbyMultiClusters.cc
@@ -6,7 +6,7 @@ void PatternRecognitionbyMultiClusters::makeTracksters(
       const edm::Event& ev,
       const edm::EventSetup& es,
       const std::vector<reco::CaloCluster>& layerClusters,
-      const std::vector<std::pair<unsigned int, float> >& mask, std::vector<Trackster>& result) const
+      const std::vector<std::pair<unsigned int, float> >& mask, std::vector<Trackster>& result) 
       {
 
 
