85d3a9684519d188506d3e8c64572e1c1a2f750a
Felice <felice.pantaleo@cern.ch>
2018-11-02 19:43:41 +0100

    add constant

--- RecoHGCal/TICL/interface/TICLConstants.h
+++ RecoHGCal/TICL/interface/TICLConstants.h
@@ -1,11 +1,12 @@
 #ifndef RecoHGCal_TICL_interface_TICLConstants_h
 #define RecoHGCal_TICL_interface_TICLConstants_h
 
-// #include <cstdint>
+#include <cstdint>
 
 namespace ticlConstants {
   constexpr float minEta  = 1.5f;
-  constexpr float maxEta  = 3.0;
+  constexpr float maxEta  = 3.0f;
+  constexpr uint8_t maxNumberOfLayers = 52;
 }
 
 #endif // RecoHGCal_TICL_interface_TICLConstants_h
