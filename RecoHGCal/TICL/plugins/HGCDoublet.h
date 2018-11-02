0ab292fc5462bcffac74e26b375caa48bc0e067d
Felice <felice.pantaleo@cern.ch>
2018-11-02 14:21:23 +0100

    preparing the Cellular Automaton Graph

---
+++ RecoHGCal/TICL/plugins/HGCDoublet.h
@@ -0,0 +1,10 @@
+// Author: Felice Pantaleo - felice.pantaleo@cern.ch
+// Date: 11/2018
+// Copyright CERN
+
+#ifndef __RecoHGCal_TICL_HGCDoublet_H__
+#define __RecoHGCal_TICL_HGCDoublet_H__
+
+
+
+#endif
