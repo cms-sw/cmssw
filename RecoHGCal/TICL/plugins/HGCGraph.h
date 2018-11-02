f6baa3d081d5f7cce365a4bcdd3e92d12f92c2ac
Felice <felice.pantaleo@cern.ch>
2018-11-02 19:44:27 +0100

    doublet generator

--- RecoHGCal/TICL/plugins/HGCGraph.h
+++ RecoHGCal/TICL/plugins/HGCGraph.h
@@ -5,6 +5,24 @@
 #ifndef __RecoHGCal_TICL_HGCGraph_H__
 #define __RecoHGCal_TICL_HGCGraph_H__
 
+#include <vector>
+#include "HGCDoublet.h"
 
+class HGCGraph
+{
+  public:
+    void makeAndConnectDoublets(const std::vector<std::vector<std::vector<unsigned int>>> &h, int nEtaBins,
+    int nPhiBins, const std::vector<reco::CaloCluster>& layerClusters, int nClusters, int deltaIEta, int deltaIPhi, float minCosTheta);
+
+    std::vector<HGCDoublet> &getAllDoublets() { return allDoublets_; }
+
+  private:
+    std::vector<HGCDoublet> allDoublets_;
+
+    std::vector<unsigned int> theRootDoublets_;
+    std::vector<std::vector<HGCDoublet *>> theNtuplets_;
+    std::vector<std::vector<int> > isOuterClusterOfDoublets_;
+
+};
 
 #endif
--- RecoHGCal/TICL/plugins/PatternRecognitionbyCA.cc
+++ RecoHGCal/TICL/plugins/PatternRecognitionbyCA.cc
@@ -1,28 +1,40 @@
-#include "PatternRecognitionbyCA.h"
-#include "RecoHGCal/TICL/interface/TICLConstants.h"
 #include <vector>
+#include "RecoHGCal/TICL/interface/TICLConstants.h"
+#include "PatternRecognitionbyCA.h"
+
 
 void PatternRecognitionbyCA::fillHistogram(const std::vector<reco::CaloCluster>& layerClusters,
             const std::vector<std::pair<unsigned int, float> >& mask)
 {
-    const auto nClusters = layerClusters.size();
-    for(uint i=0; i<nClusters; ++i)
+    std::cout << "filling eta/phi histogram per Layer" << std::endl;
+    for(auto& m : mask)
     {
-        
+        auto lcId = m.first;
+        const auto& lc = layerClusters[lcId];
+        //getting the layer Id from the detId of the first hit of the layerCluster
+        const auto firstHitDetId = lc.hitsAndFractions()[0].first;
+        int layer = rhtools_.getLayerWithOffset(firstHitDetId) + ticlConstants::maxNumberOfLayers*((rhtools_.zside(firstHitDetId)+1)>>1)-1;
+        assert(layer>=0);
+        auto etaBin = getEtaBin(lc.eta());
+        auto phiBin = getPhiBin(lc.phi());
+        histogram_[layer][globalBin(etaBin,phiBin)].push_back(lcId);
     }
-
 }
+
+
 
 
 void PatternRecognitionbyCA::makeTracksters(
       const edm::Event& ev,
       const edm::EventSetup& es,
       const std::vector<reco::CaloCluster>& layerClusters,
-      const std::vector<std::pair<unsigned int, float> >& mask, std::vector<Trackster>& result) const
-      {
+      const std::vector<std::pair<unsigned int, float> >& mask, std::vector<Trackster>& result) 
+{
+    rhtools_.getEventSetup(es);
 
+    clearHistogram();
+    std::cout << "making Tracksters with CA" << std::endl;
 
-          std::cout << "making Tracksters" << std::endl;
-      }
-
-
+    fillHistogram(layerClusters,mask);
+    theGraph_.makeAndConnectDoublets(histogram_, nEtaBins_, nPhiBins_, layerClusters, mask.size(), 2, 2, 0.9);
+}
