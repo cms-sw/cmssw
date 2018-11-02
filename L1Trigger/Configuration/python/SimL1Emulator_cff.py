7a9b64c39c3d8571da8b0d7576e67a4e549fb789
Vladimir <vladimir.rekovic@cern.ch>
2018-11-02 19:01:14 +0100

    Add a cloned version of PFProducer specially configured for PF Met and Adjust PF Met producer to use it as input.

--- L1Trigger/Configuration/python/SimL1Emulator_cff.py
+++ L1Trigger/Configuration/python/SimL1Emulator_cff.py
@@ -139,7 +139,8 @@ from L1Trigger.Phase2L1ParticleFlow.l1ParticleFlow_cff import *
 #    pfClustersFromHGC3DClustersEM +
 #    pfClustersFromL1EGClusters +
 #    pfClustersFromCombinedCalo +
-#    l1pfProducer
+#    l1pfProducer +
+#    l1pfProducerForMET     
 #)
 l1pfProducerTightTK = l1pfProducer.clone(trkMinStubs = 6)
 l1ParticleFlow += l1pfProducerTightTK
@@ -155,7 +156,7 @@ l1MetTK      = pfMet.clone(src = "l1pfProducer:TK")
 l1MetTKV     = pfMet.clone(src = "l1pfProducer:TKVtx")
 l1MetTightTK      = pfMet.clone(src = "l1pfProducerTightTK:TK")
 l1MetTightTKV     = pfMet.clone(src = "l1pfProducerTightTK:TKVtx")
-l1MetPF      = pfMet.clone(src = "l1pfProducer:PF")
+l1MetPF      = pfMet.clone(src = "l1pfProducerForMET:PF")
 l1MetPuppi   = pfMet.clone(src = "l1pfProducer:Puppi")
 l1PFMets = cms.Sequence( l1MetCalo + l1MetTK + l1MetTKV + l1MetPF + l1MetPuppi
                         + l1MetTightTK + l1MetTightTKV)
--- L1Trigger/Phase2L1ParticleFlow/python/l1ParticleFlow_cff.py
+++ L1Trigger/Phase2L1ParticleFlow/python/l1ParticleFlow_cff.py
@@ -8,11 +8,18 @@ from L1Trigger.Phase2L1ParticleFlow.pfClustersFromL1EGClusters_cfi import *
 from L1Trigger.Phase2L1ParticleFlow.pfClustersFromCombinedCalo_cfi import *
 from L1Trigger.Phase2L1ParticleFlow.l1pfProducer_cfi import *
 
+l1pfProducerForMET = l1pfProducer.clone(
+  puppiEtaCuts = cms.vdouble(1.5, 2.5, 2.85, 3.0, 5.5),
+  puppiPtCuts = cms.vdouble(0.0, 20., 40., 999., 30.),
+  puppiPtCutsPhotons = cms.vdouble(0.0, 20., 40., 999., 30.),
+)
+
 l1ParticleFlow = cms.Sequence(
     l1EGammaCrystalsProducer + 
     pfTracksFromL1Tracks +
     pfClustersFromHGC3DClustersEM +
     pfClustersFromL1EGClusters +
     pfClustersFromCombinedCalo +
-    l1pfProducer
+    l1pfProducer +
+    l1pfProducerForMET
 )
