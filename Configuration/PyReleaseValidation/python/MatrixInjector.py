8b627b92b624d6a07b96ed75e70307d8934e8620
Gaelle <Gaelle.Boudoul@cern.ch>
2018-11-07 11:16:37 +0100

    Moving to 2018 Cosmic WFs (103_X)

--- Configuration/PyReleaseValidation/python/MatrixInjector.py
+++ Configuration/PyReleaseValidation/python/MatrixInjector.py
@@ -249,6 +249,8 @@ class MatrixInjector(object):
             wmsplit['RECOUP17_PU25']=1
             wmsplit['DIGICOS_UP17']=1
             wmsplit['RECOCOS_UP17']=1
+            wmsplit['DIGICOS_UP18']=1
+            wmsplit['RECOCOS_UP18']=1
             wmsplit['HYBRIDRepackHI2015VR']=1
             wmsplit['HYBRIDZSHI2015']=1
             wmsplit['RECOHID15']=1
