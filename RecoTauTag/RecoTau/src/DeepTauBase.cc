c55a8692b06d414e470154c7ce874419cea79ba6
Maria Di Domenico <didomenico13@gmail.com>
2018-11-08 02:45:00 +0100

    Applied style comments

--- RecoTauTag/RecoTau/src/DeepTauBase.cc
+++ RecoTauTag/RecoTau/src/DeepTauBase.cc
@@ -12,13 +12,13 @@
 
 namespace deep_tau {
 
-TauWPThreshold::TauWPThreshold(const std::string& cut_str)
+TauWPThreshold::TauWPThreshold(const std::string& cutStr)
 {
     bool simple_value = false;
     try {
         size_t pos = 0;
-        value = std::stod(cut_str, &pos);
-        simple_value = pos == cut_str.size();
+        value_ = std::stod(cutStr, &pos);
+        simple_value = pos == cutStr.size();
     } catch(std::invalid_argument&) {
     } catch(std::out_of_range&) {
     }
@@ -28,32 +28,32 @@ TauWPThreshold::TauWPThreshold(const std::string& cut_str)
         static const int n_params = 3;
         static const auto handler = [](int, Bool_t, const char*, const char*) -> void {};
 
-        const std::string fn_str = prefix + cut_str + "}";
+        const std::string fn_str = prefix + cutStr + "}";
         auto old_handler = SetErrorHandler(handler);
-        fn = std::make_unique<TF1>("fn", fn_str.c_str(), 0, 1, n_params);
+        fn_ = std::make_unique<TF1>("fn_", fn_str.c_str(), 0, 1, n_params);
         SetErrorHandler(old_handler);
-        if(!fn->IsValid())
-            throw cms::Exception("TauWPThreshold: invalid formula") << "Invalid WP cut formula = '" << cut_str << "'.";
+        if(!fn_->IsValid())
+            throw cms::Exception("TauWPThreshold: invalid formula") << "Invalid WP cut formula = '" << cutStr << "'.";
     }
 }
 
 double TauWPThreshold::operator()(const pat::Tau& tau) const
 {
-    if(!fn)
-        return value;
-    fn->SetParameter(0, tau.decayMode());
-    fn->SetParameter(1, tau.pt());
-    fn->SetParameter(2, tau.eta());
-    return fn->Eval(0);
+    if(!fn_)
+        return value_;
+    fn_->SetParameter(0, tau.decayMode());
+    fn_->SetParameter(1, tau.pt());
+    fn_->SetParameter(2, tau.eta());
+    return fn_->Eval(0);
 }
 
 DeepTauBase::Output::ResultMap DeepTauBase::Output::get_value(const edm::Handle<TauCollection>& taus,
                                                               const tensorflow::Tensor& pred,
-                                                              const WPMap& working_points) const
+                                                              const WPMap& workingPoints_) const
 {
     ResultMap output;
     output[""] = std::make_unique<TauDiscriminator>(TauRefProd(taus));
-    for(const auto& wp : working_points)
+    for(const auto& wp : workingPoints_)
         output[wp.first] = std::make_unique<TauDiscriminator>(TauRefProd(taus));
 
     for(size_t tau_index = 0; tau_index < taus->size(); ++tau_index) {
@@ -67,7 +67,7 @@ DeepTauBase::Output::ResultMap DeepTauBase::Output::get_value(const edm::Handle<
             x = den_val != 0 ? x / den_val : std::numeric_limits<float>::max();
         }
         output[""]->setValue(tau_index, x);
-        for(const auto& wp : working_points) {
+        for(const auto& wp : workingPoints_) {
             const auto& tau = taus->at(tau_index);
             const bool pass = x > (*wp.second)(tau);
             output[wp.first]->setValue(tau_index, pass);
@@ -76,17 +76,17 @@ DeepTauBase::Output::ResultMap DeepTauBase::Output::get_value(const edm::Handle<
     return output;
 }
 
-DeepTauBase::DeepTauBase(const edm::ParameterSet& cfg, const OutputCollection& outputCollection, const DeepTauCache* _cache) :
-    taus_token(consumes<TauCollection>(cfg.getParameter<edm::InputTag>("taus"))),
-    outputs(outputCollection),
-    cache(_cache)
+DeepTauBase::DeepTauBase(const edm::ParameterSet& cfg, const OutputCollection& outputCollection, const DeepTauCache* Cache) :
+    tausToken_(consumes<TauCollection>(cfg.getParameter<edm::InputTag>("taus"))),
+    outputs_(outputCollection),
+    cache_(Cache)
 {
-    for(const auto& output_desc : outputs) {
+    for(const auto& output_desc : outputs_) {
         produces<TauDiscriminator>(output_desc.first);
         const auto& cut_pset = cfg.getParameter<edm::ParameterSet>(output_desc.first + "WP");
         for(const std::string& wp_name : cut_pset.getParameterNames()) {
-            const auto& cut_str = cut_pset.getParameter<std::string>(wp_name);
-            working_points[output_desc.first][wp_name] = std::make_unique<Cutter>(cut_str);
+            const auto& cutStr = cut_pset.getParameter<std::string>(wp_name);
+            workingPoints_[output_desc.first][wp_name] = std::make_unique<Cutter>(cutStr);
             produces<TauDiscriminator>(output_desc.first + wp_name);
         }
     }
@@ -95,7 +95,7 @@ DeepTauBase::DeepTauBase(const edm::ParameterSet& cfg, const OutputCollection& o
 void DeepTauBase::produce(edm::Event& event, const edm::EventSetup& es)
 {
     edm::Handle<TauCollection> taus;
-    event.getByToken(taus_token, taus);
+    event.getByToken(tausToken_, taus);
 
     const tensorflow::Tensor& pred = getPredictions(event, es, taus);
     createOutputs(event, pred, taus);
@@ -103,8 +103,8 @@ void DeepTauBase::produce(edm::Event& event, const edm::EventSetup& es)
 
 void DeepTauBase::createOutputs(edm::Event& event, const tensorflow::Tensor& pred, edm::Handle<TauCollection> taus)
 {
-    for(const auto& output_desc : outputs) {
-        auto result_map = output_desc.second.get_value(taus, pred, working_points.at(output_desc.first));
+    for(const auto& output_desc : outputs_) {
+        auto result_map = output_desc.second.get_value(taus, pred, workingPoints_.at(output_desc.first));
         for(auto& result : result_map)
             event.put(std::move(result.second), output_desc.first + result.first);
     }
@@ -114,10 +114,8 @@ std::unique_ptr<DeepTauCache> DeepTauBase::initializeGlobalCache(const edm::Para
 {
     std::string graphName = edm::FileInPath(cfg.getParameter<std::string>("graph_file")).fullPath();
     bool memMapped = cfg.getParameter<bool>("memMapped");
-    return std::make_unique<DeepTauCache>(graphName, memMapped );
+    return std::make_unique<DeepTauCache>(graphName, memMapped);
 }
-
-// void DeepTauBase::globalEndJob(const DeepTauCache* cache) { }
 
 DeepTauCache::DeepTauCache(const std::string& graphName, const bool& memMapped)
 {
@@ -125,33 +123,33 @@ DeepTauCache::DeepTauCache(const std::string& graphName, const bool& memMapped)
     tensorflow::setThreading(options, 1, "no_threads");
 
     if(memMapped) {
-        memmapped_env = std::make_unique<tensorflow::MemmappedEnv>(tensorflow::Env::Default());
-        const tensorflow::Status mmap_status = memmapped_env.get()->InitializeFromFile(graphName);
+        memmappedEnv_ = std::make_unique<tensorflow::MemmappedEnv>(tensorflow::Env::Default());
+        const tensorflow::Status mmap_status = memmappedEnv_.get()->InitializeFromFile(graphName);
         if(!mmap_status.ok())
             throw cms::Exception("DeepTauCache: unable to initalize memmapped environment for ") << graphName << ". \n"
                                                                                                  << mmap_status.ToString();
 
-        graph = std::make_unique<tensorflow::GraphDef>();
-        const tensorflow::Status load_graph_status = ReadBinaryProto(memmapped_env.get(),
+        graph_ = std::make_unique<tensorflow::GraphDef>();
+        const tensorflow::Status load_graph_status = ReadBinaryProto(memmappedEnv_.get(),
                                                                      tensorflow::MemmappedFileSystem::kMemmappedPackageDefaultGraphDef,
-                                                                     graph.get());
+                                                                     graph_.get());
         if(!load_graph_status.ok())
-            throw cms::Exception("DeepTauCache: unable to load graph from ") << graphName << ". \n"
+            throw cms::Exception("DeepTauCache: unable to load graph_ from ") << graphName << ". \n"
                                                                              << mmap_status.ToString();
         options.config.mutable_graph_options()->mutable_optimizer_options()->set_opt_level(::tensorflow::OptimizerOptions::L0);
-        options.env = memmapped_env.get();
+        options.env = memmappedEnv_.get();
 
-        session = tensorflow::createSession(graph.get(), options);
+        session_ = tensorflow::createSession(graph_.get(), options);
 
     } else {
-        graph.reset(tensorflow::loadGraphDef(graphName));
-        session = tensorflow::createSession(graph.get(), options);
+        graph_.reset(tensorflow::loadGraphDef(graphName));
+        session_ = tensorflow::createSession(graph_.get(), options);
       }
 }
 
 DeepTauCache::~DeepTauCache()
 {
-    tensorflow::closeSession(session);
+    tensorflow::closeSession(session_);
 }
 
 } // namespace deep_tau
