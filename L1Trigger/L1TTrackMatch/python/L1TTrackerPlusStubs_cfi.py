3cbe5173a57f957020392db8c8ae7016d39dde62
David Hamilton <dwahamilton@physics.ucla.edu>
2018-11-07 16:17:59 -0800

    fixed simKBmtfStubs

--- L1Trigger/L1TTrackMatch/python/L1TTrackerPlusStubs_cfi.py
+++ L1Trigger/L1TTrackMatch/python/L1TTrackerPlusStubs_cfi.py
@@ -34,7 +34,7 @@ l1StubMatchedMuons = cms.EDProducer("L1TTrackerPlusStubsProducer",
 	    #alphaB = cms.vdouble(4.66511e-5,7.61571e-5,1.31778e-4,1.33713e-4),
 	    #betaB = cms.vdouble(1.42130e1,1.31157e1,1.04929e1,1.17798e1)
 	    alpha = cms.vdouble(2.50016e-2,3.10230e-2,3.65267e-2,3.46650e-2),
-	    beta = cms.vdouble(2.13888e1,2.15320e1,2.09371e1,2.98381e01),
+	    beta = cms.vdouble(2.13888e1,2.15320e1,2.09371e1,2.98381e1),
 	    alphaB = cms.vdouble(5.56536e-3,6.74712e-3,8.22339e-3,1.04466e-2),
 	    betaB = cms.vdouble(1.68262e0,1.61702e0,1.52894e0,1.25928e0)
         )
