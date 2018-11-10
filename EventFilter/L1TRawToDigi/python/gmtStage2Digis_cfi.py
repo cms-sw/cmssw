44e3114bf69c9c0824036e62d234e8cb156b0e05
Matthew <Matthew.Nguyen@cern.ch>
2018-11-11 00:25:12 +0100

    Add explicit raw collection label for l1traw2digi

--- EventFilter/L1TRawToDigi/python/gmtStage2Digis_cfi.py
+++ EventFilter/L1TRawToDigi/python/gmtStage2Digis_cfi.py
@@ -2,6 +2,7 @@ import FWCore.ParameterSet.Config as cms
 
 gmtStage2Digis = cms.EDProducer(
     "L1TRawToDigi",
+    InputLabel = cms.InputTag("rawDataCollector"),
     Setup = cms.string("stage2::GMTSetup"),
     FedIds = cms.vint32(1402),
 )
