3b94f5cc4a121186773199b40dc87f5f4c69f284
Andrea Bocci <andrea.bocci@cern.ch>
2018-11-14 23:56:26 +0100

    Synchronise with CMSSW_10_4_0_pre2

--- RecoLocalCalo/Configuration/python/hcalLocalReco_cff.py
+++ RecoLocalCalo/Configuration/python/hcalLocalReco_cff.py
@@ -1,61 +1,56 @@
 import FWCore.ParameterSet.Config as cms
 
 from RecoLocalCalo.HcalRecAlgos.hcalRecAlgoESProd_cfi import *
-from RecoLocalCalo.HcalRecAlgos.hcalChannelPropertiesESProd_cfi import *
 hcalOOTPileupESProducer = cms.ESProducer('OOTPileupDBCompatibilityESProducer')
 
 from RecoLocalCalo.HcalRecProducers.HBHEPhase1Reconstructor_cfi import hbheprereco as _phase1_hbheprereco
 hbheprereco = _phase1_hbheprereco.clone(
-    processQIE11 = False,
-    tsFromDB = True,
+    processQIE11 = cms.bool(False),
+    tsFromDB = cms.bool(True),
     pulseShapeParametersQIE8 = dict(
-        TrianglePeakTS = 4,
+        TrianglePeakTS = cms.uint32(4),
     )
 )
 
 from RecoLocalCalo.HcalRecProducers.HcalHitReconstructor_ho_cfi import *
 from RecoLocalCalo.HcalRecProducers.HcalHitReconstructor_hf_cfi import *
 from RecoLocalCalo.HcalRecProducers.HcalHitReconstructor_zdc_cfi import *
-hcalLocalRecoTask = cms.Task(hbheprereco,hfreco,horeco,zdcreco)
-hcalLocalRecoSequence = cms.Sequence(hcalLocalRecoTask)
+hcalLocalRecoSequence = cms.Sequence(hbheprereco+hfreco+horeco+zdcreco)
 
 from RecoLocalCalo.HcalRecProducers.hfprereco_cfi import hfprereco
 from RecoLocalCalo.HcalRecProducers.HFPhase1Reconstructor_cfi import hfreco as _phase1_hfreco
 from RecoLocalCalo.HcalRecProducers.hbheplan1_cfi import hbheplan1
 
-#--- for HCALonly wf
-hcalOnlyLocalRecoTask = cms.Task(hbheprereco,hfprereco,hfreco,horeco)
-
 # copy for cosmics
 _default_hfreco = hfreco.clone()
 
-#--- Phase1 
-_phase1_hcalLocalRecoTask = hcalLocalRecoTask.copy()
-_phase1_hcalLocalRecoTask.add(hfprereco)
+_phase1_hcalLocalRecoSequence = hcalLocalRecoSequence.copy()
+_phase1_hcalLocalRecoSequence.insert(0,hfprereco)
 
 from Configuration.Eras.Modifier_run2_HF_2017_cff import run2_HF_2017
-run2_HF_2017.toReplaceWith( hcalLocalRecoTask, _phase1_hcalLocalRecoTask )
+run2_HF_2017.toReplaceWith( hcalLocalRecoSequence, _phase1_hcalLocalRecoSequence )
 run2_HF_2017.toReplaceWith( hfreco, _phase1_hfreco )
 from Configuration.Eras.Modifier_run2_HCAL_2017_cff import run2_HCAL_2017
 run2_HCAL_2017.toReplaceWith( hbheprereco, _phase1_hbheprereco )
 
-_plan1_hcalLocalRecoTask = _phase1_hcalLocalRecoTask.copy()
-_plan1_hcalLocalRecoTask.add(hbheplan1)
+_plan1_hcalLocalRecoSequence = _phase1_hcalLocalRecoSequence.copy()
+_plan1_hcalLocalRecoSequence += hbheplan1
 from Configuration.Eras.Modifier_run2_HEPlan1_2017_cff import run2_HEPlan1_2017
-run2_HEPlan1_2017.toReplaceWith(hcalLocalRecoTask, _plan1_hcalLocalRecoTask)
+run2_HEPlan1_2017.toReplaceWith(hcalLocalRecoSequence, _plan1_hcalLocalRecoSequence)
 
 hbhecollapse = hbheplan1.clone()
-_collapse_hcalLocalRecoTask = _phase1_hcalLocalRecoTask.copy()
-_collapse_hcalLocalRecoTask.add(hbhecollapse)
+_collapse_hcalLocalRecoSequence = _phase1_hcalLocalRecoSequence.copy()
+_collapse_hcalLocalRecoSequence += hbhecollapse
 from Configuration.ProcessModifiers.run2_HECollapse_2018_cff import run2_HECollapse_2018
-run2_HECollapse_2018.toReplaceWith(hcalLocalRecoTask, _collapse_hcalLocalRecoTask)
+run2_HECollapse_2018.toReplaceWith(hcalLocalRecoSequence, _collapse_hcalLocalRecoSequence)
 
-#--- from >=Run3
-_run3_hcalLocalRecoTask = _phase1_hcalLocalRecoTask.copy()
-_run3_hcalLocalRecoTask.remove(hbheprereco)
-from Configuration.Eras.Modifier_run3_HB_cff import run3_HB
-run3_HB.toReplaceWith( hcalLocalRecoTask, _run3_hcalLocalRecoTask )
+_phase2_hcalLocalRecoSequence = hcalLocalRecoSequence.copy()
+_phase2_hcalLocalRecoSequence.remove(hbheprereco)
 
-_fastSim_hcalLocalRecoTask = hcalLocalRecoTask.copyAndExclude([zdcreco])
+from Configuration.Eras.Modifier_phase2_hcal_cff import phase2_hcal
+phase2_hcal.toReplaceWith( hcalLocalRecoSequence, _phase2_hcalLocalRecoSequence )
+
+
+_fastSim_hcalLocalRecoSequence = hcalLocalRecoSequence.copyAndExclude([zdcreco])
 from Configuration.Eras.Modifier_fastSim_cff import fastSim
-fastSim.toReplaceWith( hcalLocalRecoTask, _fastSim_hcalLocalRecoTask )
+fastSim.toReplaceWith( hcalLocalRecoSequence, _fastSim_hcalLocalRecoSequence )
