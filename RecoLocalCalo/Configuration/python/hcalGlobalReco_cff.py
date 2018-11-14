260b70aace3386b5b49003456a2e9747c1244605
Andrea Bocci <andrea.bocci@cern.ch>
2018-11-14 23:56:26 +0100

    Synchronise with CMSSW_10_4_0_pre2

--- RecoLocalCalo/Configuration/python/hcalGlobalReco_cff.py
+++ RecoLocalCalo/Configuration/python/hcalGlobalReco_cff.py
@@ -1,10 +1,9 @@
 import FWCore.ParameterSet.Config as cms
 
 from RecoLocalCalo.HcalRecProducers.HBHEIsolatedNoiseReflagger_cfi import *
-hcalGlobalRecoTask = cms.Task(hbhereco)
-hcalGlobalRecoSequence = cms.Sequence(hcalGlobalRecoTask)
+hcalGlobalRecoSequence = cms.Sequence(hbhereco)
 
 from RecoLocalCalo.HcalRecProducers.HBHEPhase1Reconstructor_cfi import hbheprereco as _phase1_hbheprereco
 
-from Configuration.Eras.Modifier_run3_HB_cff import run3_HB
-run3_HB.toReplaceWith( hbhereco, _phase1_hbheprereco ) # >=Run3
+from Configuration.Eras.Modifier_phase2_hcal_cff import phase2_hcal
+phase2_hcal.toReplaceWith( hbhereco, _phase1_hbheprereco )
