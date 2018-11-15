28453873843dd765d1be3e62aa836b63165b2d9d
peruzzim <peruzzim@users.noreply.github.com>
2018-11-15 09:55:51 +0100

    Use 2016 b-tagging SF for run2_nanoAOD_94X2016 modifier (temporary for consistency)

--- PhysicsTools/NanoAOD/python/nano_cff.py
+++ PhysicsTools/NanoAOD/python/nano_cff.py
@@ -81,14 +81,15 @@ btagWeightTable = cms.EDProducer("BTagSFProducer",
     sysTypes = cms.vstring("central","central","central")
 )
 
-run2_miniAOD_80XLegacy.toModify(btagWeightTable,                
-    cut = cms.string("pt > 25. && abs(eta) < 2.4"),             #80X corresponds to 2016, |eta| < 2.4
-    weightFiles = cms.vstring(                                  #80X corresponds to 2016 SFs
-        btagSFdir+"CSVv2_Moriond17_B_H.csv",            
-        "unavailable",                    
-        btagSFdir+"cMVAv2_Moriond17_B_H.csv"                                            
+for modifier in run2_miniAOD_80XLegacy, run2_nanoAOD_94X2016: # to be updated when SF for Summer16MiniAODv3 MC will be available
+    modifier.toModify(btagWeightTable,                
+        cut = cms.string("pt > 25. && abs(eta) < 2.4"),             #80X corresponds to 2016, |eta| < 2.4
+        weightFiles = cms.vstring(                                  #80X corresponds to 2016 SFs
+            btagSFdir+"CSVv2_Moriond17_B_H.csv",            
+            "unavailable",                    
+            btagSFdir+"cMVAv2_Moriond17_B_H.csv"                                            
+        )
     )
-)
 
 run2_nanoAOD_92X.toModify(btagWeightTable,                      #92X corresponds to MCv1, for which SFs are unavailable
     weightFiles = cms.vstring(
