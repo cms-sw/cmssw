cc0e24223ffb89b60a05abd3c9cf848ab5010efa
Jean-Baptiste Sauvan <jean-baptiste.sauvan@cern.ch>
2018-11-22 14:40:17 +0100

    Add threshold parameter in selection custom

--- L1Trigger/L1THGCal/python/customTriggerCellSelect.py
+++ L1Trigger/L1THGCal/python/customTriggerCellSelect.py
@@ -2,26 +2,25 @@ import FWCore.ParameterSet.Config as cms
 import SimCalorimetry.HGCalSimProducers.hgcalDigitizer_cfi as digiparam
 
 def custom_triggercellselect_supertriggercell(process):
-    
     process.hgcalConcentratorProducer.ProcessorParameters.Method = cms.string('superTriggerCellSelect')
     return process
 
 
-def custom_triggercellselect_threshold(process):
-
+def custom_triggercellselect_threshold(process,
+        threshold=2. # in mipT
+        ):
     adcSaturationBH_MIP = digiparam.hgchebackDigitizer.digiCfg.feCfg.adcSaturation_fC
     adcNbitsBH = digiparam.hgchebackDigitizer.digiCfg.feCfg.adcNbits
 
     parameters = process.hgcalConcentratorProducer.ProcessorParameters
     parameters.Method = cms.string('thresholdSelect')
     parameters.NData = cms.uint32(999)
-    parameters.MaxCellsInModule = cms.uint32(288)
     parameters.linLSB = cms.double(100./1024.)
     parameters.adcsaturationBH = adcSaturationBH_MIP
     parameters.adcnBitsBH = adcNbitsBH
     parameters.TCThreshold_fC = cms.double(0.)
     parameters.TCThresholdBH_MIP = cms.double(0.)
-    parameters.triggercell_threshold_silicon = cms.double(2.) # MipT
-    parameters.triggercell_threshold_scintillator = cms.double(2.) # MipT
+    parameters.triggercell_threshold_silicon = cms.double(threshold) # MipT
+    parameters.triggercell_threshold_scintillator = cms.double(threshold) # MipT
 
     return process
