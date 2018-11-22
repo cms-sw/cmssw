1b8c151711e6fea60c70c429c1fb40110862a44a
Martin Grunewald <Martin.Grunewald@cern.ch>
2018-11-22 10:00:20 +0100

    Toward HIon HLT V2.0

--- HLTrigger/Configuration/python/HLT_PIon_cff.py
+++ HLTrigger/Configuration/python/HLT_PIon_cff.py
@@ -1,13 +1,13 @@
 # hltGetConfiguration --cff --data /dev/CMSSW_10_3_0/PIon --type PIon
 
-# /dev/CMSSW_10_3_0/PIon/V40 (CMSSW_10_3_0)
+# /dev/CMSSW_10_3_0/PIon/V42 (CMSSW_10_3_0)
 
 import FWCore.ParameterSet.Config as cms
 
 fragment = cms.ProcessFragment( "HLT" )
 
 fragment.HLTConfigVersion = cms.PSet(
-  tableName = cms.string('/dev/CMSSW_10_3_0/PIon/V40')
+  tableName = cms.string('/dev/CMSSW_10_3_0/PIon/V42')
 )
 
 fragment.transferSystem = cms.PSet( 
