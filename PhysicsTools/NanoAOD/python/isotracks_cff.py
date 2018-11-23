fdc29c737570aa953900cefb4482e6607088f1e4
Maria Giulia Ratti <maria.giulia.ratti@cern.ch>
2018-11-23 11:14:03 +0100

    restored original selection and changed name for fromPV

--- PhysicsTools/NanoAOD/python/isotracks_cff.py
+++ PhysicsTools/NanoAOD/python/isotracks_cff.py
@@ -3,8 +3,7 @@ from PhysicsTools.NanoAOD.common_cff import *
 
 finalIsolatedTracks = cms.EDProducer("IsolatedTrackCleaner",
     tracks = cms.InputTag("isolatedTracks"),
-#    cut = cms.string("((pt>5 && (abs(pdgId) == 11 || abs(pdgId) == 13)) || pt > 10) && (abs(pdgId) < 15 || abs(eta) < 2.5) && abs(dxy) < 0.2 && abs(dz) < 0.1 && ((pfIsolationDR03().chargedHadronIso < 5 && pt < 25) || pfIsolationDR03().chargedHadronIso/pt < 0.2)"), 
-    cut =  cms.string("(1)"),
+    cut = cms.string("((pt>5 && (abs(pdgId) == 11 || abs(pdgId) == 13)) || pt > 10) && (abs(pdgId) < 15 || abs(eta) < 2.5) && abs(dxy) < 0.2 && abs(dz) < 0.1 && ((pfIsolationDR03().chargedHadronIso < 5 && pt < 25) || pfIsolationDR03().chargedHadronIso/pt < 0.2)"), 
     finalLeptons = cms.VInputTag(
         cms.InputTag("finalElectrons"),
         cms.InputTag("finalMuons"),
@@ -37,7 +36,7 @@ isoTrackTable = cms.EDProducer("SimpleCandidateFlatTableProducer",
         pfRelIso03_chg = Var("pfIsolationDR03().chargedHadronIso/pt",float,doc="PF relative isolation dR=0.3, charged component",precision=10),
         pfRelIso03_all = Var("(pfIsolationDR03().chargedHadronIso + max(pfIsolationDR03().neutralHadronIso + pfIsolationDR03().photonIso - pfIsolationDR03().puChargedHadronIso/2,0.0))/pt",float,doc="PF relative isolation dR=0.3, total (deltaBeta corrections)",precision=10),
         isPFcand = Var("packedCandRef().isNonnull()",bool,doc="if isolated track is a PF candidate"),
-        isFromPV = Var("fromPV", int, doc="if isolated track comes from PV"),
+        fromPV = Var("fromPV", int, doc="isolated track comes from PV"),
         pdgId = Var("pdgId",int,doc="PDG id of PF cand"),
         isHighPurityTrack = Var("isHighPurityTrack",bool,doc="track is high purity"),
     ),
