265630fc851b3eaf543cb729b7ccbcd81fcd7581
Fabio Cossutti <fabio.cossutti@ts.infn.it>
2018-12-03 23:32:27 +0100

    Dump topology infomration and compare with detId

--- Geometry/MTDNumberingBuilder/test/MTDTopologyAnalyzer.cc
+++ Geometry/MTDNumberingBuilder/test/MTDTopologyAnalyzer.cc
@@ -8,13 +8,25 @@
 #include "FWCore/Framework/interface/Event.h"
 #include "FWCore/Framework/interface/EventSetup.h"
 #include "FWCore/Framework/interface/ESHandle.h"
+#include "FWCore/Framework/interface/ESTransientHandle.h"
 #include "FWCore/Framework/interface/MakerMacros.h"
 
 #include "FWCore/ParameterSet/interface/ParameterSet.h"
 #include "Geometry/Records/interface/MTDTopologyRcd.h"
 #include "Geometry/MTDNumberingBuilder/interface/MTDTopology.h"
 
+#include "DetectorDescription/Core/interface/DDFilteredView.h"
+
+#include "Geometry/MTDCommonData/interface/MTDBaseNumber.h"
+#include "Geometry/MTDCommonData/interface/BTLNumberingScheme.h"
+#include "Geometry/MTDCommonData/interface/ETLNumberingScheme.h"
+
+#include "DataFormats/ForwardDetId/interface/BTLDetId.h"
+#include "DataFormats/ForwardDetId/interface/ETLDetId.h"
+
 #include "FWCore/MessageLogger/interface/MessageLogger.h"
+
+//#define EDM_ML_DEBUG
 
 //
 // class declaration
@@ -28,20 +40,125 @@ public:
   
   void analyze(edm::Event const& iEvent, edm::EventSetup const&) override;
 
+  void theBaseNumber( const DDGeoHistory& gh );
+
+  MTDBaseNumber thisN_;
+  BTLNumberingScheme btlNS_;
+  ETLNumberingScheme etlNS_;
+
 };
 
-MTDTopologyAnalyzer::MTDTopologyAnalyzer( const edm::ParameterSet& iConfig )
+MTDTopologyAnalyzer::MTDTopologyAnalyzer( const edm::ParameterSet& iConfig ) :
+  thisN_(),btlNS_(),etlNS_()
 {}
 
 // ------------ method called to produce the data  ------------
 void
-MTDTopologyAnalyzer::analyze( const edm::Event& iEvent, const edm::EventSetup& iSetup )
-{
+MTDTopologyAnalyzer::analyze( const edm::Event& iEvent, const edm::EventSetup& iSetup ){
 
   edm::ESHandle<MTDTopology> mtdTopo;
   iSetup.get<MTDTopologyRcd>().get( mtdTopo );     
   edm::LogInfo("MTDTopologyAnalyzer") << "MTD topology mode = " << mtdTopo->getMTDTopologyMode();
     
+  // Build DetIds based on DDD description, then extract information from topology and compare
+
+  std::string label_;
+
+  edm::ESTransientHandle<DDCompactView> pDD;
+  iSetup.get<IdealGeometryRecord>().get(label_, pDD );
+  if (pDD.description()) {
+    edm::LogInfo("MTDTopologyAnalyzer") << pDD.description()->type_ << " label: " << pDD.description()->label_;
+  } else {
+    edm::LogWarning("MTDTopologyAnalyzer") << "NO label found pDD.description() returned false.";
+  }
+  if (!pDD.isValid()) {
+    edm::LogError("MTDTopologyAnalyzer") << "ESTransientHandle<DDCompactView> pDD is not valid!";
+  }
+  
+  DDPassAllFilter filter;
+  DDFilteredView fv(*pDD, filter);
+
+  edm::LogInfo("MTDTopologyAnalyzer") << "Top Most LogicalPart = " << fv.logicalPart();
+
+  using nav_type =  DDFilteredView::nav_type;
+  using id_type = std::map<nav_type,int>;
+  id_type idMap;
+  int id=0;
+  std::string ddtop_("");
+  size_t limit = 0;
+
+  bool isBarrel = false;
+  
+  do {
+    nav_type pos = fv.navPos();
+    idMap[pos]=id;
+
+    size_t num = fv.geoHistory().size();
+
+    if ( fv.geoHistory()[num-1].logicalPart().name() == "btl:BarrelTimingLayer" ) { 
+      isBarrel = true;
+      limit = num;
+      ddtop_ = "btl:BarrelTimingLayer";
+    }
+    else if ( fv.geoHistory()[num-1].logicalPart().name() == "etl:EndcapTimingLayer" ) {
+      isBarrel = false;
+      limit = num;
+      ddtop_ = "etl:EndcapTimingLayer";
+    }
+
+    if ( num <= limit && fv.geoHistory()[num-1].logicalPart().name().fullname() != ddtop_ ) { ddtop_.clear(); }
+
+    if ( !ddtop_.empty() ) { 
+
+      // Actions for MTD volumes: searchg for sensitive detectors
+    
+      bool isSens = false; 
+    
+      if ( fv.geoHistory()[num-1].logicalPart().specifics().size() > 0 ) { 
+        for ( auto elem : *(fv.geoHistory()[num-1].logicalPart().specifics()[0]) ) {
+          if ( elem.second.name() == "SensitiveDetector" ) { isSens = true; break; }
+        }
+      }
+    
+      // Check of numbering scheme for sensitive detectors
+    
+      if ( isSens ) { 
+     
+        theBaseNumber( fv.geoHistory() );
+  
+        edm::LogInfo("MTDTopologyAnalyzer") << fv.geoHistory();
+
+        if ( isBarrel ) { 
+          BTLDetId theId(btlNS_.getUnitID(thisN_)); 
+          DetId localId_( theId.rawId() );
+          edm::LogInfo("MTDTopologAnalyzer") << mtdTopo->print(localId_) << "\n" << theId;;
+        }
+        else { 
+          ETLDetId theId(etlNS_.getUnitID(thisN_)); 
+          DetId localId_( theId.rawId() );
+          edm::LogInfo("MTDTopologAnalyzer") << mtdTopo->print(localId_) << "\n" << theId;;
+        }
+      }
+    }
+    
+    ++id;
+  } while (fv.next());
+}
+
+void MTDTopologyAnalyzer::theBaseNumber( const DDGeoHistory& gh ) {
+
+  thisN_.reset();
+  thisN_.setSize( gh.size() );
+
+  for ( uint i = gh.size(); i-- > 0; ) {
+    std::string name(gh[i].logicalPart().name().fullname());
+    int copyN(gh[i].copyno());
+    thisN_.addLevel( name, copyN );
+#ifdef EDM_ML_DEBUG
+    edm::LogInfo("MTDTopologyAnalyzer") << name << " " << copyN;
+#endif
+  }
+
 }
 
 //define this as a plug-in
