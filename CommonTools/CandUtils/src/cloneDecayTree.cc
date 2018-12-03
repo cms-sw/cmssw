0a0c077dbef2c2c254ff0936eb35a46384ae671b
Christopher Jones <chrisdjones15@gmail.com>
2018-12-03 12:52:18 -0600

    Remove std::move from return statements
    Doing a std::move stops the compiler from doing copy elision.
    This was found by the clang compiler.

--- CommonTools/CandUtils/src/cloneDecayTree.cc
+++ CommonTools/CandUtils/src/cloneDecayTree.cc
@@ -8,9 +8,8 @@ unique_ptr<Candidate> cloneDecayTree(const Candidate & c) {
   if (n == 1) return unique_ptr<Candidate>(c.clone());
   // pass a particle, not a candidate, to avoid cloning daughters 
   const Candidate &p = c;
-  CompositeCandidate * cmp(new CompositeCandidate(p));
-  unique_ptr<Candidate> cmpPtr(cmp);
+  auto cmp = std::make_unique<CompositeCandidate>(p);
   for(size_t i = 0; i < n; ++ i)
     cmp->addDaughter(cloneDecayTree(* c.daughter(i)));
-  return std::move(cmpPtr);
+  return cmp;
 }
