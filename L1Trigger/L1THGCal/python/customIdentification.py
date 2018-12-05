f01e56b7dfd949c362cbe2353d3d5d88a83186ea
Jean-Baptiste Sauvan <jean-baptiste.sauvan@cern.ch>
2018-12-05 11:43:05 +0100

    Add information in exceptions

--- L1Trigger/L1THGCal/python/customIdentification.py
+++ L1Trigger/L1THGCal/python/customIdentification.py
@@ -6,7 +6,8 @@ def custom_identification_drnn_cone(process,
         working_points=tight_wp
         ):
     if len(working_points)!=len(working_points_drnn_cone):
-        raise RuntimeError('HGC TPG ID: Number of working points not compatible with number of categories')
+        raise RuntimeError('HGC TPG ID: Number of working points ({0}) not compatible with number of categories ({1})'.format(
+                    len(working_points), len(working_points_drnn_cone)))
     for wp,cat in zip(working_points,working_points_drnn_cone):
         if not wp in cat:
             raise KeyError('HGC TPG ID: Cannot find a cut corresponding to the working point {}'.format(wp))
@@ -23,7 +24,8 @@ def custom_identification_drnn_dbscan(process,
         working_points=tight_wp
         ):
     if len(working_points)!=len(working_points_drnn_dbscan):
-        raise RuntimeError('HGC TPG ID: Number of working points not compatible with number of categories')
+        raise RuntimeError('HGC TPG ID: Number of working points ({0}) not compatible with number of categories ({1})'.format(
+                    len(working_points), len(working_points_drnn_dbscan)))
     for wp,cat in zip(working_points,working_points_drnn_dbscan):
         if not wp in cat:
             raise KeyError('HGC TPG ID: Cannot find a cut corresponding to the working point {}'.format(wp))
