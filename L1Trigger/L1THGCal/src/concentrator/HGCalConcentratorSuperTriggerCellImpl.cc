ede7fabf79b4e7cbd138be7762a85ff06ec2a602
Samuel Webb <samuel.webb@cern.ch>
2018-12-05 17:45:03 +0100

    Small updates

--- L1Trigger/L1THGCal/src/concentrator/HGCalConcentratorSuperTriggerCellImpl.cc
+++ L1Trigger/L1THGCal/src/concentrator/HGCalConcentratorSuperTriggerCellImpl.cc
@@ -13,9 +13,9 @@ HGCalConcentratorSuperTriggerCellImpl::getSuperTriggerCellId(int detid) const {
     return TC_id.cell(); //scintillator
   } else {
     int TC_wafer = TC_id.wafer();
-    int split = 0x3a;
+    static const int split = 0x3a;
     int TC_12th = ( TC_id.cell() & split );
-    int wafer_offset = 6;
+    static const  wafer_offset = 6;
     return TC_wafer<<wafer_offset | TC_12th;
   }
   
