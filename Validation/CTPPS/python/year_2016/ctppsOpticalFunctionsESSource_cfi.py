bf6b3b9717b730b130d548115ff23d15a4a2e6b1
Jan Kaspar <jan.kaspar@cern.ch>
2018-12-07 22:03:20 +0100

    All 2016-test files moved to one package.

--- Validation/CTPPS/python/year_2016/ctppsOpticalFunctionsESSource_cfi.py
+++ Validation/CTPPS/python/year_2016/ctppsOpticalFunctionsESSource_cfi.py
@@ -1,11 +1,11 @@
 import FWCore.ParameterSet.Config as cms
 
 ctppsOpticalFunctionsESSource = cms.ESSource("CTPPSOpticalFunctionsESSource",
-    xangle1 = cms.double(185),
-    fileName1 = cms.FileInPath("CondFormats/CTPPSReadoutObjects/data/year_2016/optical_functions.root"),
+  xangle1 = cms.double(185),
+  fileName1 = cms.FileInPath("Validation/CTPPS/test/year_2016/optical_functions.root"),
 
-    xangle2 = cms.double(185),
-    fileName2 = cms.FileInPath("CondFormats/CTPPSReadoutObjects/data/year_2016/optical_functions.root"),
+  xangle2 = cms.double(185),
+  fileName2 = cms.FileInPath("Validation/CTPPS/test/year_2016/optical_functions.root"),
 
     scoringPlanes = cms.VPSet(
       cms.PSet( rpId = cms.uint32(0x76100000), dirName = cms.string("XRPH_C6L5_B2"), z = cms.double(-203.826) ),  # RP 002
