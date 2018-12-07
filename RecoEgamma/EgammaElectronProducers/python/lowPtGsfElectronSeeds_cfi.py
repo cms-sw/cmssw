f558b5a5cfd874905f9c4124535252b1171eaa38
Robert Bainbridge <bainbrid@users.noreply.github.com>
2018-12-07 18:07:37 +0100

    GsfTracks using LowPtGsfElectronSeedProducer
    moved low pT ele sequence into highlevelreco

---
+++ RecoEgamma/EgammaElectronProducers/python/lowPtGsfElectronSeeds_cfi.py
@@ -0,0 +1,32 @@
+import FWCore.ParameterSet.Config as cms
+
+lowPtGsfElectronSeeds = cms.EDProducer(
+    "LowPtGsfElectronSeedProducer",
+    tracks = cms.InputTag("generalTracks"),
+    pfTracks = cms.InputTag("lowPtGsfElePfTracks"),
+    ecalClusters = cms.InputTag("particleFlowClusterECAL"),
+    hcalClusters = cms.InputTag("particleFlowClusterHCAL"),
+    EBRecHits = cms.InputTag('reducedEcalRecHitsEB'), 
+    EERecHits = cms.InputTag('reducedEcalRecHitsEE'),
+    rho = cms.InputTag('fixedGridRhoFastjetAllTmp'),
+    BeamSpot = cms.InputTag("offlineBeamSpot"),
+    Fitter = cms.string('GsfTrajectoryFitter_forPreId'),
+    Smoother = cms.string('GsfTrajectorySmoother_forPreId'),
+    TTRHBuilder = cms.string('WithAngleAndTemplate'),
+    ModelNames = cms.vstring([
+            'unbiased',
+            'ptbiased',
+            ]),
+    ModelWeights = cms.vstring([
+            'RecoEgamma/ElectronIdentification/data/LowPtElectrons/2018Nov01_improvedfullseeding_formatted.xml.gz',
+            'RecoEgamma/ElectronIdentification/data/LowPtElectrons/2018Nov01_displaced_improvedfullseeding_formatted.xml.gz',
+            ]),
+    ModelThresholds = cms.vdouble([
+            {"L": 1.03,"M":1.75,"T":2.61}["L"], # unbiased
+            {"L":-0.48,"M":0.76,"T":1.83}["L"], # ptbiased
+            ]),
+    PassThrough = cms.bool(False),
+    UsePfTracks = cms.bool(True),
+    MinPtThreshold = cms.double(0.5),
+    MaxPtThreshold = cms.double(15.),
+    )
