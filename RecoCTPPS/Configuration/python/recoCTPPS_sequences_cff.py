2ae343ad192fb70c7b7cb18d0022e8e6701798fd
Jan Kaspar <jan.kaspar@cern.ch>
2018-12-07 22:08:40 +0100

    Proton reconstruction added to the standard PPS sequence.

--- RecoCTPPS/Configuration/python/recoCTPPS_sequences_cff.py
+++ RecoCTPPS/Configuration/python/recoCTPPS_sequences_cff.py
@@ -3,8 +3,11 @@ import FWCore.ParameterSet.Config as cms
 from RecoCTPPS.TotemRPLocal.totemRPLocalReconstruction_cff import *
 from RecoCTPPS.TotemRPLocal.ctppsDiamondLocalReconstruction_cff import *
 from RecoCTPPS.TotemRPLocal.totemTimingLocalReconstruction_cff import *
-from RecoCTPPS.TotemRPLocal.ctppsLocalTrackLiteProducer_cff import ctppsLocalTrackLiteProducer
 from RecoCTPPS.PixelLocal.ctppsPixelLocalReconstruction_cff import *
+
+from RecoCTPPS.TotemRPLocal.ctppsLocalTrackLiteProducer_cff import ctppsLocalTrackLiteProducer
+
+from RecoCTPPS.ProtonReconstruction.ctppsProtonReconstruction_cfi import *
 
 from CondFormats.CTPPSReadoutObjects.ctppsRPAlignmentCorrectionsDataESSourceXML_cfi import *
 ctppsRPAlignmentCorrectionsDataESSourceXML.RealFiles = cms.vstring("Alignment/CTPPS/data/RPixGeometryCorrections.xml")
@@ -14,5 +17,6 @@ recoCTPPSdets = cms.Sequence(
     ctppsDiamondLocalReconstruction *
     totemTimingLocalReconstruction *
     ctppsPixelLocalReconstruction *
-    ctppsLocalTrackLiteProducer
+    ctppsLocalTrackLiteProducer *
+    ctppsProtonReconstruction
 )
