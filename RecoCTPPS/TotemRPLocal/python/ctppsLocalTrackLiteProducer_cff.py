9e0571b92a4e09bcb3fbfc0a74582b7294c99a56
Jan Kaspar <jan.kaspar@cern.ch>
2018-12-11 15:39:18 +0100

    doNothing mechanism replaced by the use of includeXYZ flags.

--- RecoCTPPS/TotemRPLocal/python/ctppsLocalTrackLiteProducer_cff.py
+++ RecoCTPPS/TotemRPLocal/python/ctppsLocalTrackLiteProducer_cff.py
@@ -8,7 +8,9 @@ ctppsLocalTrackLiteProducer = ctppsLocalTrackLiteDefaultProducer.clone()
 from Configuration.Eras.Modifier_ctpps_2016_cff import ctpps_2016
 ctpps_2016.toModify(
     ctppsLocalTrackLiteProducer,
-    doNothing = cms.bool(False)
+    includeStrips = cms.bool(True),
+    includeDiamonds = cms.bool(True),
+    includePixels = cms.bool(True)
 )
 
 from Configuration.Eras.Modifier_run2_miniAOD_80XLegacy_cff import run2_miniAOD_80XLegacy
