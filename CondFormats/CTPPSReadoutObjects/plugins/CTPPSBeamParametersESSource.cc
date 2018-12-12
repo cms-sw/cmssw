af2cb9b14fdf01d53ef84575f9b2242cdc1a38b4
Laurent Forthomme <laurent.forthomme@cern.ch>
2018-12-12 11:47:35 +0100

    Cleanup of the CondFormats/CTPPSReadoutObjects sub-package

--- CondFormats/CTPPSReadoutObjects/plugins/CTPPSBeamParametersESSource.cc
+++ CondFormats/CTPPSReadoutObjects/plugins/CTPPSBeamParametersESSource.cc
@@ -2,11 +2,11 @@
  *
  *  CondFormats/CTPPSReadoutObjects/plugins/CTPPSBeamParametersESSource.cc
  *
- *  Description :  - Loads CTPPSBeamParameters from the CTPPSBeamParametersESSource_cfi.py 
- *                   config file. 
- *                 - Currently, one single set of beam parameters is provided. Just to be 
+ *  Description :  - Loads CTPPSBeamParameters from the CTPPSBeamParametersESSource_cfi.py
+ *                   config file.
+ *                 - Currently, one single set of beam parameters is provided. Just to be
  *                   ready in time for 10_4_0 and allow simple tests.
- *                 - To be further developed to provide multiple sets of parameters 
+ *                 - To be further developed to provide multiple sets of parameters
  *                 - Needed while CTPPSBeamParameters is not available in database
  *
  *
@@ -43,24 +43,30 @@ class CTPPSBeamParametersESSource: public edm::ESProducer, public edm::EventSetu
 {
 public:
 
-  CTPPSBeamParametersESSource(const edm::ParameterSet &);
-  ~CTPPSBeamParametersESSource() override;
+  CTPPSBeamParametersESSource(const edm::ParameterSet&);
+  ~CTPPSBeamParametersESSource() override = default;
 
-  std::unique_ptr<CTPPSBeamParameters> produce(const CTPPSBeamParametersRcd &);
+  std::unique_ptr<CTPPSBeamParameters> produce(const CTPPSBeamParametersRcd&);
+  static void fillDescriptions(edm::ConfigurationDescriptions&);
 
 private:
-  
-  bool setBeamPars;
-  
-  void initializeBeamParameters();    //  Initialize beam parameters (BP) to zero 
-  void setBeamParameters(const edm::ParameterSet &);    //  Set BP to their values from config
-  CTPPSBeamParameters* fillBeamParameters();   //  Fill CTPPSBeamParameters object with BP 
-  
-  // Beam parameters 
-  double beamMom45, beamMom56, betaStarX45, betaStarX56, betaStarY45, betaStarY56, 
-         beamDivX45, beamDivX56, beamDivY45, beamDivY56, halfXangleX45, halfXangleX56, 
-         halfXangleY45, halfXangleY56, vtxOffsetX45, vtxOffsetX56, vtxOffsetY45, vtxOffsetY56, 
-         vtxOffsetZ45, vtxOffsetZ56, vtxStddevX, vtxStddevY, vtxStddevZ ; 
+
+  bool setBeamPars_;
+
+  /// Set BP to their values from config
+  void setBeamParameters(const edm::ParameterSet &);
+  ///  Fill CTPPSBeamParameters object with BP
+  std::unique_ptr<CTPPSBeamParameters> fillBeamParameters();
+
+  // Beam parameters
+  double beamMom45_, beamMom56_;
+  double betaStarX45_, betaStarY45_, betaStarX56_, betaStarY56_;
+  double beamDivX45_, beamDivY45_, beamDivX56_, beamDivY56_;
+  double halfXangleX45_, halfXangleY45_;
+  double halfXangleX56_, halfXangleY56_;
+  double vtxOffsetX45_, vtxOffsetY45_, vtxOffsetZ45_;
+  double vtxOffsetX56_, vtxOffsetY56_, vtxOffsetZ56_;
+  double vtxStddevX_, vtxStddevY_, vtxStddevZ_;
 
 protected:
 
@@ -70,142 +76,163 @@ protected:
 
 //----------------------------------------------------------------------------------------------------
 
-using namespace std;
-using namespace edm;
-
-CTPPSBeamParametersESSource::CTPPSBeamParametersESSource(const edm::ParameterSet& conf) :
-  setBeamPars(conf.getUntrackedParameter<bool>("setBeamPars","False"))
-  
+CTPPSBeamParametersESSource::CTPPSBeamParametersESSource(const edm::ParameterSet& iConfig) :
+  setBeamPars_( iConfig.getParameter<bool>( "setBeamPars" ) ),
+  beamMom45_( 0. ), beamMom56_( 0. ),
+  betaStarX45_( 0. ), betaStarY45_( 0. ), betaStarX56_( 0. ), betaStarY56_( 0. ),
+  beamDivX45_( 0. ), beamDivY45_( 0. ), beamDivX56_( 0. ), beamDivY56_( 0. ),
+  halfXangleX45_( 0. ), halfXangleY45_( 0. ),
+  halfXangleX56_( 0. ), halfXangleY56_( 0. ),
+  vtxOffsetX45_( 0. ), vtxOffsetY45_( 0. ), vtxOffsetZ45_( 0. ),
+  vtxOffsetX56_( 0. ), vtxOffsetY56_( 0. ), vtxOffsetZ56_( 0. ),
+  vtxStddevX_( 0. ), vtxStddevY_( 0. ), vtxStddevZ_( 0. )
 {
+  if ( setBeamPars_ )
+    setBeamParameters( iConfig );
 
-  initializeBeamParameters();
-  if(setBeamPars) setBeamParameters(conf);
-  
   setWhatProduced(this);
   findingRecord<CTPPSBeamParametersRcd>();
-
 }
 
 //----------------------------------------------------------------------------------------------------
 
-CTPPSBeamParametersESSource::~CTPPSBeamParametersESSource()
+std::unique_ptr<CTPPSBeamParameters>
+CTPPSBeamParametersESSource::produce( const CTPPSBeamParametersRcd & )
 {
-}
-
-//----------------------------------------------------------------------------------------------------
-
-std::unique_ptr<CTPPSBeamParameters> CTPPSBeamParametersESSource::produce( const CTPPSBeamParametersRcd & )
-{
-  
-  CTPPSBeamParameters *bp;
-  bp = new CTPPSBeamParameters();
-  
   // If beam parameters are available from the config file, fill their values into CTPPSBeamParameters object
-  if(setBeamPars) bp = fillBeamParameters();
-  
-  edm::LogInfo("CTPPSBeamParametersESSource::produce") << "\n" << *bp << "\n" ;
+  auto bp = ( setBeamPars_ )
+    ? fillBeamParameters()
+    : std::make_unique<CTPPSBeamParameters>();
 
-  return std::unique_ptr<CTPPSBeamParameters>(bp);
-  
+  edm::LogInfo("CTPPSBeamParametersESSource::produce") << "\n" << *bp;
+
+  return std::move( bp );
 }
 
 //----------------------------------------------------------------------------------------------------
 
-void CTPPSBeamParametersESSource::initializeBeamParameters() 
+void
+CTPPSBeamParametersESSource::setBeamParameters( const edm::ParameterSet& iConfig )
 {
-  beamMom45 = beamMom56 = betaStarX45 = betaStarX56 = betaStarY45 = betaStarY56 = 
-  beamDivX45 = beamDivX56 = beamDivY45 = beamDivY56 = halfXangleX45 = halfXangleX56 = 
-  halfXangleY45 = halfXangleY56 = vtxOffsetX45 = vtxOffsetX56 = vtxOffsetY45 = vtxOffsetY56 = 
-  vtxOffsetZ45 = vtxOffsetZ56 = vtxStddevX = vtxStddevY = vtxStddevZ = 0.0 ;
+  beamMom45_ = iConfig.getParameter<double>( "beamMom45" );
+  beamMom56_ = iConfig.getParameter<double>( "beamMom56" );
+  betaStarX45_ = iConfig.getParameter<double>( "betaStarX45" );
+  betaStarX56_ = iConfig.getParameter<double>( "betaStarX56" );
+  betaStarY45_ = iConfig.getParameter<double>( "betaStarY45" );
+  betaStarY56_ = iConfig.getParameter<double>( "betaStarY56" );
+  beamDivX45_ = iConfig.getParameter<double>( "beamDivX45" );
+  beamDivX56_ = iConfig.getParameter<double>( "beamDivX56" );
+  beamDivY45_ = iConfig.getParameter<double>( "beamDivY45" );
+  beamDivY56_ = iConfig.getParameter<double>( "beamDivY56" );
+  halfXangleX45_ = iConfig.getParameter<double>( "halfXangleX45" );
+  halfXangleX56_ = iConfig.getParameter<double>( "halfXangleX56" );
+  halfXangleY45_ = iConfig.getParameter<double>( "halfXangleY45" );
+  halfXangleY56_ = iConfig.getParameter<double>( "halfXangleY56" );
+  vtxOffsetX45_ = iConfig.getParameter<double>( "vtxOffsetX45" );
+  vtxOffsetY45_ = iConfig.getParameter<double>( "vtxOffsetY45" );
+  vtxOffsetZ45_ = iConfig.getParameter<double>( "vtxOffsetZ45" );
+  vtxOffsetX56_ = iConfig.getParameter<double>( "vtxOffsetX56" );
+  vtxOffsetY56_ = iConfig.getParameter<double>( "vtxOffsetY56" );
+  vtxOffsetZ56_ = iConfig.getParameter<double>( "vtxOffsetZ56" );
+  vtxStddevX_ = iConfig.getParameter<double>( "vtxStddevX" );
+  vtxStddevY_ = iConfig.getParameter<double>( "vtxStddevY" );
+  vtxStddevZ_ = iConfig.getParameter<double>( "vtxStddevZ" );
 }
 
 //----------------------------------------------------------------------------------------------------
 
-void CTPPSBeamParametersESSource::setBeamParameters(const edm::ParameterSet& conf)
+std::unique_ptr<CTPPSBeamParameters>
+CTPPSBeamParametersESSource::fillBeamParameters()
 {
-    beamMom45 = conf.getParameter<double>("beamMom45");
-    beamMom56 = conf.getParameter<double>("beamMom56");
-    betaStarX45 = conf.getParameter<double>("betaStarX45");
-    betaStarX56 = conf.getParameter<double>("betaStarX56");
-    betaStarY45 = conf.getParameter<double>("betaStarY45");
-    betaStarY56 = conf.getParameter<double>("betaStarY56");
-    beamDivX45 = conf.getParameter<double>("beamDivX45");
-    beamDivX56 = conf.getParameter<double>("beamDivX56");
-    beamDivY45 = conf.getParameter<double>("beamDivY45");
-    beamDivY56 = conf.getParameter<double>("beamDivY56");
-    halfXangleX45 = conf.getParameter<double>("halfXangleX45");
-    halfXangleX56 = conf.getParameter<double>("halfXangleX56");
-    halfXangleY45 = conf.getParameter<double>("halfXangleY45");
-    halfXangleY56 = conf.getParameter<double>("halfXangleY56");
-    vtxOffsetX45 = conf.getParameter<double>("vtxOffsetX45");
-    vtxOffsetY45 = conf.getParameter<double>("vtxOffsetY45");
-    vtxOffsetZ45 = conf.getParameter<double>("vtxOffsetZ45");
-    vtxOffsetX56 = conf.getParameter<double>("vtxOffsetX56");
-    vtxOffsetY56 = conf.getParameter<double>("vtxOffsetY56");
-    vtxOffsetZ56 = conf.getParameter<double>("vtxOffsetZ56");
-    vtxStddevX = conf.getParameter<double>("vtxStddevX");
-    vtxStddevY = conf.getParameter<double>("vtxStddevY");
-    vtxStddevZ = conf.getParameter<double>("vtxStddevZ");
+  auto p = std::make_unique<CTPPSBeamParameters>();
+
+  p->setBeamMom45( beamMom45_ );
+  p->setBeamMom56( beamMom56_ );
+
+  p->setBetaStarX45( betaStarX45_ );
+  p->setBetaStarY45( betaStarY45_ );
+  p->setBetaStarX56( betaStarX56_ );
+  p->setBetaStarY56( betaStarY56_ );
+
+  p->setBeamDivergenceX45( beamDivX45_ );
+  p->setBeamDivergenceY45( beamDivY45_ );
+  p->setBeamDivergenceX56( beamDivX56_ );
+  p->setBeamDivergenceY56( beamDivY56_ );
+
+  p->setHalfXangleX45( halfXangleX45_ );
+  p->setHalfXangleY45( halfXangleY45_ );
+  p->setHalfXangleX56( halfXangleX56_ );
+  p->setHalfXangleY56( halfXangleY56_ );
+
+  p->setVtxOffsetX45( vtxOffsetX45_ );
+  p->setVtxOffsetY45( vtxOffsetY45_ );
+  p->setVtxOffsetZ45( vtxOffsetZ45_ );
+  p->setVtxOffsetX56( vtxOffsetX56_ );
+  p->setVtxOffsetY56( vtxOffsetY56_ );
+  p->setVtxOffsetZ56( vtxOffsetZ56_ );
+
+  p->setVtxStddevX( vtxStddevX_ );
+  p->setVtxStddevY( vtxStddevY_ );
+  p->setVtxStddevZ( vtxStddevZ_ );
+
+  return p;
 }
 
 //----------------------------------------------------------------------------------------------------
 
-CTPPSBeamParameters* CTPPSBeamParametersESSource::fillBeamParameters() 
+void
+CTPPSBeamParametersESSource::setIntervalFor( const edm::eventsetup::EventSetupRecordKey& key,
+                                             const edm::IOVSyncValue& iosv, edm::ValidityInterval& oValidity )
 {
-  CTPPSBeamParameters* p = new CTPPSBeamParameters();
-  
-  p->setBeamMom45( beamMom45 );
-  p->setBeamMom56( beamMom56 );
-  
-  p->setBetaStarX45( betaStarX45 );
-  p->setBetaStarY45( betaStarY45 );
-  p->setBetaStarX56( betaStarX56 );
-  p->setBetaStarY56( betaStarY56 );
-  
-  p->setBeamDivergenceX45( beamDivX45 );
-  p->setBeamDivergenceY45( beamDivY45 );
-  p->setBeamDivergenceX56( beamDivX56 );
-  p->setBeamDivergenceY56( beamDivY56 );
-  
-  p->setHalfXangleX45( halfXangleX45 );
-  p->setHalfXangleY45( halfXangleY45 );
-  p->setHalfXangleX56( halfXangleX56 );
-  p->setHalfXangleY56( halfXangleY56 );
-  
-  p->setVtxOffsetX45( vtxOffsetX45 );
-  p->setVtxOffsetY45( vtxOffsetY45 );
-  p->setVtxOffsetZ45( vtxOffsetZ45 );
-  p->setVtxOffsetX56( vtxOffsetX56 );
-  p->setVtxOffsetY56( vtxOffsetY56 );
-  p->setVtxOffsetZ56( vtxOffsetZ56 );
-  
-  p->setVtxStddevX( vtxStddevX );
-  p->setVtxStddevY( vtxStddevY );
-  p->setVtxStddevZ( vtxStddevZ );
-  
-  // edm::LogInfo("CTPPSBeamParametersESSource::fillBeamParameters") << "\n" ;
-  
-  return p ;
-}
-
-//----------------------------------------------------------------------------------------------------
-
-void CTPPSBeamParametersESSource::setIntervalFor(const edm::eventsetup::EventSetupRecordKey &key,
-						     const edm::IOVSyncValue& iosv, edm::ValidityInterval& oValidity)
-{
-  /*
-  LogVerbatim("CTPPSBeamParametersESSource")
-    << ">> CTPPSBeamParametersESSource::setIntervalFor(" << key.name() << ")";
-
-  LogVerbatim("CTPPSBeamParametersESSource")
+  LogDebug("CTPPSBeamParametersESSource")
+    << ">> CTPPSBeamParametersESSource::setIntervalFor(" << key.name() << ")\n"
     << "    run=" << iosv.eventID().run() << ", event=" << iosv.eventID().event();
-  */
-  
+
   edm::ValidityInterval infinity( iosv.beginOfTime(), iosv.endOfTime() );
   oValidity = infinity;
-  
 }
 
 //----------------------------------------------------------------------------------------------------
 
-DEFINE_FWK_EVENTSETUP_SOURCE(CTPPSBeamParametersESSource);
+void
+CTPPSBeamParametersESSource::fillDescriptions( edm::ConfigurationDescriptions& descriptions )
+{
+  edm::ParameterSetDescription desc;
+  desc.add<bool>( "setBeamPars", true );
+  // beam momentum (GeV)
+  desc.add<double>( "beamMom45", 6500. );
+  desc.add<double>( "beamMom56", 6500. );
+  // beta* (cm)
+  desc.add<double>( "betaStarX45", 30. );
+  desc.add<double>( "betaStarY45", 30. );
+  desc.add<double>( "betaStarX56", 30. );
+  desc.add<double>( "betaStarY56", 30. );
+  // beam divergence (rad)
+  desc.add<double>( "beamDivX45", 0.1 );
+  desc.add<double>( "beamDivY45", 0.1 );
+  desc.add<double>( "beamDivX56", 0.1 );
+  desc.add<double>( "beamDivY56", 0.1 );
+  // half crossing angle (rad)
+  desc.add<double>( "halfXangleX45", 80.e-6 );
+  desc.add<double>( "halfXangleY45", 80.e-6 );
+  desc.add<double>( "halfXangleX56", 80.e-6 );
+  desc.add<double>( "halfXangleY56", 80.e-6 );
+  // vertex offset (cm)
+  desc.add<double>( "vtxOffsetX45", 1.e-2 );
+  desc.add<double>( "vtxOffsetY45", 1.e-2 );
+  desc.add<double>( "vtxOffsetZ45", 1.e-2 );
+  desc.add<double>( "vtxOffsetX56", 1.e-2 );
+  desc.add<double>( "vtxOffsetY56", 1.e-2 );
+  desc.add<double>( "vtxOffsetZ56", 1.e-2 );
+  // vertex sigma (cm)
+  desc.add<double>( "vtxStddevX", 2.e-2 );
+  desc.add<double>( "vtxStddevY", 2.e-2 );
+  desc.add<double>( "vtxStddevZ", 2.e-2 );
+
+  descriptions.add( "ctppsBeamParametersESSource", desc );
+}
+
+//----------------------------------------------------------------------------------------------------
+
+DEFINE_FWK_EVENTSETUP_SOURCE( CTPPSBeamParametersESSource );
+
--- Validation/CTPPS/python/year_2016/ctppsOpticalFunctionsESSource_cfi.py
+++ Validation/CTPPS/python/year_2016/ctppsOpticalFunctionsESSource_cfi.py
@@ -1,16 +1,15 @@
 import FWCore.ParameterSet.Config as cms
+from CondFormats.CTPPSReadoutObjects.ctppsOpticalFunctionsESSource_cfi import ctppsOpticalFunctionsESSource as _tmp
 
-ctppsOpticalFunctionsESSource = cms.ESSource("CTPPSOpticalFunctionsESSource",
-  xangle1 = cms.double(185),
-  fileName1 = cms.FileInPath("Validation/CTPPS/test/year_2016/optical_functions.root"),
-
-  xangle2 = cms.double(185),
-  fileName2 = cms.FileInPath("Validation/CTPPS/test/year_2016/optical_functions.root"),
-
+ctppsOpticalFunctionsESSource = _tmp.clone(
+    xangle1 = cms.double(185),
+    fileName1 = cms.FileInPath("Validation/CTPPS/test/year_2016/optical_functions.root"),
+    xangle2 = cms.double(185),
+    fileName2 = cms.FileInPath("Validation/CTPPS/test/year_2016/optical_functions.root"),
     scoringPlanes = cms.VPSet(
-      cms.PSet( rpId = cms.uint32(0x76100000), dirName = cms.string("XRPH_C6L5_B2"), z = cms.double(-203.826) ),  # RP 002
-      cms.PSet( rpId = cms.uint32(0x76180000), dirName = cms.string("XRPH_D6L5_B2"), z = cms.double(-212.551) ),  # RP 003
-      cms.PSet( rpId = cms.uint32(0x77100000), dirName = cms.string("XRPH_C6R5_B1"), z = cms.double(+203.826) ),  # RP 102
-      cms.PSet( rpId = cms.uint32(0x77180000), dirName = cms.string("XRPH_D6R5_B1"), z = cms.double(+212.551) ),  # RP 103
+        cms.PSet( rpId = cms.uint32(0x76100000), dirName = cms.string("XRPH_C6L5_B2"), z = cms.double(-203.826) ),  # RP 002
+        cms.PSet( rpId = cms.uint32(0x76180000), dirName = cms.string("XRPH_D6L5_B2"), z = cms.double(-212.551) ),  # RP 003
+        cms.PSet( rpId = cms.uint32(0x77100000), dirName = cms.string("XRPH_C6R5_B1"), z = cms.double(+203.826) ),  # RP 102
+        cms.PSet( rpId = cms.uint32(0x77180000), dirName = cms.string("XRPH_D6R5_B1"), z = cms.double(+212.551) ),  # RP 103
     )
 )
