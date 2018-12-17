8c9756c7f09fe2324175bb6404b6b10afe5d0d22
Vladimir <Vladimir.Ivantchenko@cern.ch>
2018-12-17 20:18:48 +0100

    fixed build error

--- SimG4Core/SensitiveDetector/interface/SensitiveDetectorMakerBase.h
+++ SimG4Core/SensitiveDetector/interface/SensitiveDetectorMakerBase.h
@@ -9,6 +9,8 @@
 //         Created:  Mon Nov 14 11:50:24 EST 2005
 //
 
+#include "SimG4Core/SensitiveDetector/interface/SensitiveDetector.h"
+
 // system include files
 #include <string>
 
@@ -16,8 +18,7 @@
 class SimActivityRegistry;
 class DDCompactView;
 class SimTrackManager;
-class SensitiveCaloDetector;
-class SensitiveTkDetector;
+class SensitiveDetectorCatalog;
 
 namespace edm{
   class ParameterSet;
@@ -25,7 +26,6 @@ namespace edm{
 
 class SensitiveDetectorMakerBase
 {
-
 public:
   explicit SensitiveDetectorMakerBase() {};
   virtual ~SensitiveDetectorMakerBase() {};
