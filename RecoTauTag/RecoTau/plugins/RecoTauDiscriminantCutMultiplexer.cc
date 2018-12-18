797472e2a8b6bae7db4a503fe6b150c1fd2dcfa3
Michal Bluj <michal.bluj@cern.ch>
2018-12-18 13:50:44 +0100

    followup on code-checks

--- RecoTauTag/RecoTau/plugins/RecoTauDiscriminantCutMultiplexer.cc
+++ RecoTauTag/RecoTau/plugins/RecoTauDiscriminantCutMultiplexer.cc
@@ -195,7 +195,7 @@ void RecoTauDiscriminantCutMultiplexer::beginEvent(const edm::Event& evt, const 
     //Only open the file once and we can close it when this routine is done
     // since all objects gotten from the file will have been copied
     std::unique_ptr<TFile> inputFile;
-    if ( mvaOutputNormalizationName_ != "" ) {
+    if ( !mvaOutputNormalizationName_.empty() ) {
       if ( !loadMVAfromDB_ ) {
 	inputFile = openInputFile(inputFileName_);
 	mvaOutput_normalization_ = loadObjectFromFile<TFormula>(*inputFile, mvaOutputNormalizationName_);
