f8a16f0ef14a63c6264d382186a12a22bb8cc952
Vladimir <Vladimir.Ivantchenko@cern.ch>
2018-12-18 14:14:07 +0100

    clean-up build

--- SimG4CMS/Tracker/plugins/module.cc
+++ SimG4CMS/Tracker/plugins/module.cc
@@ -1,5 +1,4 @@
 #include "SimG4CMS/Tracker/interface/TkAccumulatingSensitiveDetector.h"
-#include "SimG4Core/SensitiveDetector/interface/SensitiveDetectorPluginFactory.h"
 #include "FWCore/PluginManager/interface/ModuleDef.h"
   
 
