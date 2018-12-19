9c8ce90f99d542e4be5537cd5c390ff7a3e3756c
Laurent Forthomme <laurent.forthomme@cern.ch>
2018-12-19 10:02:47 +0100

    Splitted the timing calibration class into a header/implementation files pair

--- CondFormats/CTPPSReadoutObjects/interface/PPSTimingCalibration.h
+++ CondFormats/CTPPSReadoutObjects/interface/PPSTimingCalibration.h
@@ -22,24 +22,13 @@ class PPSTimingCalibration
     /// Helper structure for storing calibration data
     struct CalibrationKey
     {
-      int db, sampic, channel, cell;
+      CalibrationKey( int db = -1, int sampic = -1, int channel = -1, int cell = -1 ) :
+        db( db ), sampic( sampic ), channel( channel ), cell( cell ) {}
       /// Comparison operator
-      bool operator<(const CalibrationKey& rhs) const {
-        if (db == rhs.db) {
-          if (sampic == rhs.sampic) {
-            if (channel == rhs.channel)
-              return cell < rhs.cell;
-            return channel < rhs.channel;
-          }
-          return sampic < rhs.sampic;
-        }
-        return db < rhs.db;
-      }
-      CalibrationKey(int db = -1, int sampic = -1, int channel = -1, int cell = -1) :
-        db(db), sampic(sampic), channel(channel), cell(cell) {}
-      friend std::ostream& operator<<(std::ostream& os, const CalibrationKey& key) {
-        return os << key.db << " " << key.sampic << " " << key.channel << " " << key.cell;
-      }
+      bool operator<( const CalibrationKey& rhs ) const;
+      friend std::ostream& operator<<( std::ostream& os, const CalibrationKey& key );
+
+      int db, sampic, channel, cell;
     };
     //--------------------------------------------------------------------------
 
@@ -48,47 +37,15 @@ class PPSTimingCalibration
 
     PPSTimingCalibration() = default;
     PPSTimingCalibration( const std::string& formula, const ParametersMap& params, const TimingMap& timeinfo ) :
-      formula_(formula), parameters_(params), timeInfo_(timeinfo) {}
+      formula_( formula ), parameters_( params ), timeInfo_( timeinfo ) {}
     ~PPSTimingCalibration() = default;
 
-    inline std::vector<double> getParameters(int db, int sampic, int channel, int cell) const {
-      CalibrationKey key = CalibrationKey(db, sampic, channel, cell);
-      auto out = parameters_.find(key);
-      if (out == parameters_.end())
-        return {};
-      else
-        return out->second;
-    }
+    std::vector<double> getParameters( int db, int sampic, int channel, int cell ) const;
     inline const std::string& getFormula() const { return formula_; }
-    inline double getTimeOffset(int db, int sampic, int channel) const {
-      CalibrationKey key = CalibrationKey(db, sampic, channel);
-      auto out = timeInfo_.find(key);
-      if (out == timeInfo_.end())
-        return 0.;
-      else
-        return out->second.first;
-    }
-    inline double getTimePrecision(int db, int sampic, int channel) const {
-      CalibrationKey key = CalibrationKey(db, sampic, channel);
-      auto out = timeInfo_.find(key);
-      if (out == timeInfo_.end())
-        return 0.;
-      else
-        return out->second.second;
-    }
+    double getTimeOffset( int db, int sampic, int channel ) const;
+    double getTimePrecision( int db, int sampic, int channel ) const;
 
-    friend std::ostream& operator<<(std::ostream& os, const PPSTimingCalibration& data) {
-      os << "FORMULA: "<< data.formula_ << "\nDB SAMPIC CHANNEL CELL PARAMETERS TIME_OFFSET\n";
-      for (const auto& kv : data.parameters_) {
-        os << kv.first <<" [";
-        for (size_t i = 0; i < kv.second.size(); ++i)
-          os << (i > 0 ? ", " : "") << kv.second.at(i);
-        CalibrationKey k = kv.first;
-        k.cell = -1;
-        os << "] " << data.timeInfo_.at(k).first << " " <<  data.timeInfo_.at(k).second << "\n";
-      }
-      return os;
-    }
+    friend std::ostream& operator<<( std::ostream& os, const PPSTimingCalibration& data );
 
   private:
     friend PPSTimingCalibrationESSource;
