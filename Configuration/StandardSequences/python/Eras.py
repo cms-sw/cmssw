704939f35352a03d1691fdb2bdc283db38116ce2
Robert Bainbridge <bainbrid@users.noreply.github.com>
2018-12-19 21:00:39 +0100

    modifiers for BParking + triv tidy up

--- Configuration/StandardSequences/python/Eras.py
+++ Configuration/StandardSequences/python/Eras.py
@@ -53,7 +53,8 @@ class Eras (object):
                            'tracker_apv_vfp30_2016', 'pf_badHcalMitigation', 'run2_miniAOD_80XLegacy','run2_miniAOD_94XFall17', 'run2_nanoAOD_92X',
                            'run2_nanoAOD_94XMiniAODv1', 'run2_nanoAOD_94XMiniAODv2', 'run2_nanoAOD_94X2016',
                            'run2_miniAOD_devel', 'run2_nanoAOD_102Xv1',
-                           'hcalHardcodeConditions', 'hcalSkipPacker']
+                           'hcalHardcodeConditions', 'hcalSkipPacker',
+                           'BParking']
         internalUseModChains = ['run2_2017_noTrackingModifier']
 
 
