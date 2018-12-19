5e9d1b7f153d77bdc27de9aca329446e60458a20
Laurent Forthomme <laurent.forthomme@cern.ch>
2018-12-19 11:26:17 +0100

    First fully working version (see the test)

--- RecoCTPPS/TotemRPLocal/python/totemTimingLocalReconstruction_cff.py
+++ RecoCTPPS/TotemRPLocal/python/totemTimingLocalReconstruction_cff.py
@@ -1,5 +1,8 @@
 import FWCore.ParameterSet.Config as cms
 
+from CondFormats.CTPPSReadoutObjects.timingCalibrationESSource_cfi import timingCalibrationESSource
+timingCalibrationESSource.calibrationFile = cms.FileInPath('RecoCTPPS/TotemRPLocal/data/timing_offsets_ufsd_2018.dec18.cal.json')
+
 # reco hit production
 from RecoCTPPS.TotemRPLocal.totemTimingRecHits_cfi import totemTimingRecHits
 
@@ -10,3 +13,4 @@ totemTimingLocalReconstruction = cms.Sequence(
     totemTimingRecHits *
     totemTimingLocalTracks
 )
+
