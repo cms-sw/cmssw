21b76217a1a5c9a1aea14c727e8983928cc28fea
Robert Bainbridge <bainbrid@users.noreply.github.com>
2018-12-19 21:00:39 +0100

    modifiers for BParking + triv tidy up

--- RecoEgamma/EgammaElectronProducers/python/lowPtGsfElectronSeeds_cfi.py
+++ RecoEgamma/EgammaElectronProducers/python/lowPtGsfElectronSeeds_cfi.py
@@ -1,5 +1,9 @@
 import FWCore.ParameterSet.Config as cms
 
+def thresholds( wp ) :
+    return cms.vdouble([{"L": 1.03,"M":1.75,"T":2.61}.get(wp,1.e6),  # unbiased
+                        {"L":-0.48,"M":0.76,"T":1.83}.get(wp,1.e6)]) # ptbiased
+
 lowPtGsfElectronSeeds = cms.EDProducer(
     "LowPtGsfElectronSeedProducer",
     tracks = cms.InputTag("generalTracks"),
@@ -21,18 +25,14 @@ lowPtGsfElectronSeeds = cms.EDProducer(
             'RecoEgamma/ElectronIdentification/data/LowPtElectrons/RunII_Fall17_LowPtElectrons_unbiased.xml.gz',
             'RecoEgamma/ElectronIdentification/data/LowPtElectrons/RunII_Fall17_LowPtElectrons_displaced_pt_eta_biased.xml.gz',
             ]),
-    ModelThresholds = cms.vdouble([
-            {"L": 1.03,"M":1.75,"T":2.61}["L"], # unbiased
-            {"L":-0.48,"M":0.76,"T":1.83}["L"], # ptbiased
-            ]),
+    ModelThresholds = thresholds("T"),
     PassThrough = cms.bool(False),
     UsePfTracks = cms.bool(True),
     MinPtThreshold = cms.double(0.5),
     MaxPtThreshold = cms.double(15.),
     )
-# copying from RecoParticleFlow/PFTracking/python/trackerDrivenElectronSeeds_cfi.py
-# inFastSim jobs, trajectories are only available for the 'before mixing' track collections
-# Therefore we let the seeds depend on the 'before mixing' generalTracks collection
+
+# Modifiers for FastSim
 from Configuration.Eras.Modifier_fastSim_cff import fastSim
 lowPtGsfElectronSeedsTmp = lowPtGsfElectronSeeds.clone(tracks = cms.InputTag("generalTracksBeforeMixing"))
 import FastSimulation.Tracking.ElectronSeedTrackRefFix_cfi
@@ -43,5 +43,10 @@ _fastSim_lowPtGsfElectronSeeds.PreIdLabel = cms.vstring("","HCAL")
 _fastSim_lowPtGsfElectronSeeds.PreGsfLabel = cms.string("")
 fastSim.toReplaceWith(lowPtGsfElectronSeeds,_fastSim_lowPtGsfElectronSeeds)
 
+# Modifiers for Phase2
 from Configuration.Eras.Modifier_phase2_tracker_cff import phase2_tracker
 phase2_tracker.toModify(lowPtGsfElectronSeeds, TTRHBuilder  = 'WithTrackAngle')
+
+# Modifiers for BParking
+from Configuration.Eras.Modifier_BParking_cff import BParking
+BParking.toModify(lowPtGsfElectronSeeds, ModelThresholds = thresholds("L") )
