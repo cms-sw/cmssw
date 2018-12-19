839023131ee958e3aeefa23ce2d6f4d8e7a8efc4
Jan Kaspar <jan.kaspar@cern.ch>
2018-12-19 13:49:35 +0100

    Added validity interval.

--- Validation/CTPPS/plugins/CTPPSLHCInfoESSource.cc
+++ Validation/CTPPS/plugins/CTPPSLHCInfoESSource.cc
@@ -26,8 +26,11 @@ class CTPPSLHCInfoESSource: public edm::ESProducer, public edm::EventSetupRecord
     edm::ESProducts<std::unique_ptr<LHCInfo>> produce(const LHCInfoRcd &);
 
   private:
+    edm::EventRange m_validityRange;
     double m_beamEnergy;
     double m_xangle;
+
+    bool m_insideValidityRange;
 
     void setIntervalFor(const edm::eventsetup::EventSetupRecordKey&, const edm::IOVSyncValue&, edm::ValidityInterval&) override;
 };
@@ -36,8 +39,10 @@ class CTPPSLHCInfoESSource: public edm::ESProducer, public edm::EventSetupRecord
 //----------------------------------------------------------------------------------------------------
 
 CTPPSLHCInfoESSource::CTPPSLHCInfoESSource(const edm::ParameterSet& conf) :
+  m_validityRange(conf.getParameter<edm::EventRange>("validityRange")),
   m_beamEnergy(conf.getParameter<double>("beamEnergy")),
-  m_xangle(conf.getParameter<double>("xangle"))
+  m_xangle(conf.getParameter<double>("xangle")),
+  m_insideValidityRange(false)
 {
   setWhatProduced(this);
   findingRecord<LHCInfoRcd>();
@@ -48,7 +53,28 @@ CTPPSLHCInfoESSource::CTPPSLHCInfoESSource(const edm::ParameterSet& conf) :
 void CTPPSLHCInfoESSource::setIntervalFor(const edm::eventsetup::EventSetupRecordKey &key,
   const edm::IOVSyncValue& iosv, edm::ValidityInterval& oValidity)
 {
-  oValidity = edm::ValidityInterval(edm::IOVSyncValue::beginOfTime(), edm::IOVSyncValue::endOfTime());
+  if (edm::contains(m_validityRange, iosv.eventID()))
+  {
+    m_insideValidityRange = true;
+    oValidity = edm::ValidityInterval(edm::IOVSyncValue(m_validityRange.startEventID()), edm::IOVSyncValue(m_validityRange.endEventID()));
+  } else {
+    m_insideValidityRange = false;
+
+    if (iosv.eventID() < m_validityRange.startEventID())
+    {
+      edm::RunNumber_t run = m_validityRange.startEventID().run();
+      edm::LuminosityBlockNumber_t lb = m_validityRange.startEventID().luminosityBlock();
+      edm::EventID endEvent = (lb > 1) ? edm::EventID(run, lb-1, 0) : edm::EventID(run-1, edm::EventID::maxLuminosityBlockNumber(), 0);
+
+      oValidity = edm::ValidityInterval(edm::IOVSyncValue::beginOfTime(), edm::IOVSyncValue(endEvent));
+    } else {
+      edm::RunNumber_t run = m_validityRange.startEventID().run();
+      edm::LuminosityBlockNumber_t lb = m_validityRange.startEventID().luminosityBlock();
+      edm::EventID beginEvent = (lb < edm::EventID::maxLuminosityBlockNumber()-1) ? edm::EventID(run, lb+1, 0) : edm::EventID(run+1, 0, 0);
+
+      oValidity = edm::ValidityInterval(edm::IOVSyncValue(beginEvent), edm::IOVSyncValue::endOfTime());
+    }
+  }
 }
 
 //----------------------------------------------------------------------------------------------------
@@ -57,8 +83,14 @@ edm::ESProducts<std::unique_ptr<LHCInfo>> CTPPSLHCInfoESSource::produce(const LH
 {
   auto output = std::make_unique<LHCInfo>();
 
-  output->setEnergy(m_beamEnergy);
-  output->setCrossingAngle(m_xangle);
+  if (m_insideValidityRange)
+  {
+    output->setEnergy(m_beamEnergy);
+    output->setCrossingAngle(m_xangle);
+  } else {
+    output->setEnergy(0.);
+    output->setCrossingAngle(0.);
+  }
 
   return edm::es::products(std::move(output));
 }
--- Validation/CTPPS/python/year_2016/ctppsLHCInfoESSource_cfi.py
+++ Validation/CTPPS/python/year_2016/ctppsLHCInfoESSource_cfi.py
@@ -1,6 +1,7 @@
 import FWCore.ParameterSet.Config as cms
 
 ctppsLHCInfoESSource = cms.ESSource("CTPPSLHCInfoESSource",
+  validityRange = cms.EventRange("270293:min - 290872:max"),
   beamEnergy = cms.double(6500),  # GeV
   xangle = cms.double(185)  # murad
 )
