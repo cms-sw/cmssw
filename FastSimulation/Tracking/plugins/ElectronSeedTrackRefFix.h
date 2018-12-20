95d47fadf8e0084afdb380934bccfccb5a686029
Robert Bainbridge <bainbrid@users.noreply.github.com>
2018-12-20 12:53:00 +0100

    fix white space, tidy up

--- FastSimulation/Tracking/plugins/ElectronSeedTrackRefFix.h
+++ FastSimulation/Tracking/plugins/ElectronSeedTrackRefFix.h
@@ -1,37 +1,34 @@
-// system include files
 #ifndef FastSimulation_Tracking_ElectronSeedTrackRefFix_h
 #define FastSimulation_Tracking_ElectronSeedTrackRefFix_h
 
-
-#include <memory>
-
-// user include files
-#include "FWCore/Framework/interface/Frameworkfwd.h"
+#include "DataFormats/Common/interface/ValueMap.h"
+#include "DataFormats/EgammaReco/interface/ElectronSeedFwd.h"
 #include "DataFormats/ParticleFlowReco/interface/PreIdFwd.h"
 #include "DataFormats/TrackReco/interface/TrackFwd.h"
-#include "DataFormats/EgammaReco/interface/ElectronSeedFwd.h"
-
+#include "FWCore/Framework/interface/Frameworkfwd.h"
 #include "FWCore/Framework/interface/stream/EDProducer.h"
-#include "DataFormats/Common/interface/ValueMap.h"
-
+#include <memory>
 
 class ElectronSeedTrackRefFix : public edm::stream::EDProducer<> {
-public:
+
+ public:
+ 
   explicit ElectronSeedTrackRefFix(const edm::ParameterSet&);
   ~ElectronSeedTrackRefFix() override;
-  
+
   static void fillDescriptions(edm::ConfigurationDescriptions& descriptions);
   
-private:
+ private:
+ 
   void produce(edm::Event&, const edm::EventSetup&) override;
     
-  // ----------member data ---------------------------
+  // member data
+
   edm::EDGetTokenT<reco::TrackCollection > newTracksToken_;
   edm::EDGetTokenT<reco::TrackCollection > oldTracksToken_;
   edm::EDGetTokenT<reco::ElectronSeedCollection > seedsToken_;
   std::vector<edm::EDGetTokenT<reco::PreIdCollection > > idsToken_;
   std::vector<edm::EDGetTokenT<edm::ValueMap<reco::PreIdRef>  > > idMapToken_;
-
   std::string preidgsfLabel_;
   std::vector<std::string> preidLabel_;
   edm::InputTag oldTracksTag_;
@@ -41,4 +38,4 @@ private:
   
 };
 
-#endif
+#endif // FastSimulation_Tracking_ElectronSeedTrackRefFix_h
