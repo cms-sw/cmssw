454a6264270d55337c035d5256190d9253ad4cc6
Christopher Jones <chrisdjones15@gmail.com>
2018-12-21 15:33:32 -0600

    code-check change

--- HLTrigger/HLTcore/src/HLTConfigData.cc
+++ HLTrigger/HLTcore/src/HLTConfigData.cc
@@ -216,7 +216,7 @@ void HLTConfigData::extract()
    } else if ( processPSet_->existsAs<ParameterSet>(preT,true)) {
      prescaleName=preT;
    }
-   if (prescaleName=="") {
+   if (prescaleName.empty()) {
      hltPrescaleTable_=HLTPrescaleTable();
    } else {
      const ParameterSet& iPS(processPSet_->getParameterSet(prescaleName));
