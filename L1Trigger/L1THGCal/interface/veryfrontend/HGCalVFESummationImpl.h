f0feebcaae04a057c2a816cae88bd4321a6c33d6
Shahzad Malik Muzaffar <Shahzad.Malik.Muzaffar@cern.ch>
2019-01-04 12:41:54 +0100

    [HGCAL trigger] New algorithms + fixes

--- L1Trigger/L1THGCal/interface/veryfrontend/HGCalVFESummationImpl.h
+++ L1Trigger/L1THGCal/interface/veryfrontend/HGCalVFESummationImpl.h
@@ -3,6 +3,7 @@
 
 #include "FWCore/ParameterSet/interface/ParameterSet.h"
 #include "L1Trigger/L1THGCal/interface/HGCalTriggerGeometryBase.h"
+#include "L1Trigger/L1THGCal/interface/HGCalTriggerTools.h"
 
 #include "DataFormats/L1THGCal/interface/HGCalTriggerCell.h"
 #include "DataFormats/L1THGCal/interface/HGCalTriggerSums.h"
@@ -16,13 +17,15 @@ class HGCalVFESummationImpl
  public:
   HGCalVFESummationImpl(const edm::ParameterSet& conf);
   
+  void eventSetup(const edm::EventSetup& es) {triggerTools_.eventSetup(es);}
   void triggerCellSums(const HGCalTriggerGeometryBase& ,
                        const std::vector<std::pair<DetId, uint32_t > >&,
-                       std::map<HGCalDetId, uint32_t>& payload);
+                       std::unordered_map<uint32_t, uint32_t>& payload);
   const std::vector<double>& thicknessCorrections() const {return thickness_corrections_;} 
    
  private:
   std::vector<double> thickness_corrections_;   
+  HGCalTriggerTools triggerTools_;
 };
 
 #endif
--- L1Trigger/L1THGCal/python/customTriggerGeometry.py
+++ L1Trigger/L1THGCal/python/customTriggerGeometry.py
@@ -3,12 +3,12 @@ import FWCore.ParameterSet.Config as cms
 
 def custom_geometry_V9(process):
     process.hgcalTriggerGeometryESProducer.TriggerGeometry.TriggerGeometryName = cms.string('HGCalTriggerGeometryV9Imp1')
-    process.hgcalTriggerGeometryESProducer.TriggerGeometry.L1TCellsMapping = cms.FileInPath("L1Trigger/L1THGCal/data/triggercell_mapping_8inch_aligned_192_432_V9_0.txt")
-    process.hgcalTriggerGeometryESProducer.TriggerGeometry.L1TWafersMapping = cms.FileInPath("L1Trigger/L1THGCal/data/wafer_mapping_V9_0.txt")
+    process.hgcalTriggerGeometryESProducer.TriggerGeometry.L1TCellsMapping = cms.FileInPath("L1Trigger/L1THGCal/data/triggercell_mapping_8inch_aligned_192_432_V9_1.txt")
+    process.hgcalTriggerGeometryESProducer.TriggerGeometry.L1TWafersMapping = cms.FileInPath("L1Trigger/L1THGCal/data/wafer_mapping_V9_1.txt")
     process.hgcalTriggerGeometryESProducer.TriggerGeometry.L1TModulesMapping = cms.FileInPath("L1Trigger/L1THGCal/data/panel_mapping_tdr_0.txt")
     process.hgcalTriggerGeometryESProducer.TriggerGeometry.L1TCellNeighborsMapping = cms.FileInPath("L1Trigger/L1THGCal/data/triggercell_neighbor_mapping_8inch_aligned_192_432_0.txt")
-    process.hgcalTriggerGeometryESProducer.TriggerGeometry.L1TCellsSciMapping = cms.FileInPath("L1Trigger/L1THGCal/data/triggercell_mapping_sci_2x2_V9_0.txt")
-    process.hgcalTriggerGeometryESProducer.TriggerGeometry.L1TCellNeighborsSciMapping = cms.FileInPath("L1Trigger/L1THGCal/data/triggercell_neighbor_mapping_sci_2x2_V9_0.txt")
+    process.hgcalTriggerGeometryESProducer.TriggerGeometry.L1TCellsSciMapping = cms.FileInPath("L1Trigger/L1THGCal/data/triggercell_mapping_sci_2x2_V9_1.txt")
+    process.hgcalTriggerGeometryESProducer.TriggerGeometry.L1TCellNeighborsSciMapping = cms.FileInPath("L1Trigger/L1THGCal/data/triggercell_neighbor_mapping_sci_2x2_V9_1.txt")
     process.hgcalTriggerGeometryESProducer.TriggerGeometry.DisconnectedModules = cms.vuint32(0)
     process.hgcalConcentratorProducer.ProcessorParameters.MaxCellsInModule = cms.uint32(288)
     return process
