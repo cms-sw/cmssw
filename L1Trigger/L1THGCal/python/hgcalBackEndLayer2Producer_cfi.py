fa0cd8504638a9ffa685bf0fa7771dd12fdeb85f
Shahzad Malik Muzaffar <Shahzad.Malik.Muzaffar@cern.ch>
2019-01-04 12:41:54 +0100

    [HGCAL trigger] New algorithms + fixes

--- L1Trigger/L1THGCal/python/hgcalBackEndLayer2Producer_cfi.py
+++ L1Trigger/L1THGCal/python/hgcalBackEndLayer2Producer_cfi.py
@@ -5,12 +5,22 @@ import RecoLocalCalo.HGCalRecProducers.HGCalUncalibRecHit_cfi as recoparam
 import RecoLocalCalo.HGCalRecProducers.HGCalRecHit_cfi as recocalibparam 
 
 from L1Trigger.L1THGCal.egammaIdentification import egamma_identification_drnn_cone
+from L1Trigger.L1THGCal.customClustering import binSums
+
 C3d_parValues = cms.PSet( type_multicluster = cms.string('dRC3d'),
                           dR_multicluster = cms.double(0.01),
                           minPt_multicluster = cms.double(0.5), # minimum pt of the multicluster (GeV)
                           dist_dbscan_multicluster=cms.double(0.),
                           minN_dbscan_multicluster=cms.uint32(0),
-                          EGIdentification=egamma_identification_drnn_cone.clone()
+                          nBins_R_histo_multicluster = cms.uint32(36),
+                          nBins_Phi_histo_multicluster = cms.uint32(216),
+                          binSumsHisto = binSums,
+                          threshold_histo_multicluster = cms.double(20.),
+                          EGIdentification=egamma_identification_drnn_cone.clone(),
+                          neighbour_weights=cms.vdouble(  0    , 0.25, 0   ,
+                                                          0.25 , 0  ,  0.25,
+                                                          0    , 0.25, 0
+                                                          )
  )
 
 be_proc = cms.PSet( ProcessorName  = cms.string('HGCalBackendLayer2Processor3DClustering'),
