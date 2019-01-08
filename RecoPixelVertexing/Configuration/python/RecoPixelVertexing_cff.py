65544d06568f40642e7a18f17e89fc511588402f
Vincenzo Innocente <vincenzo.innocente@cern.ch>
2019-01-08 18:29:08 +0100

    Full workflow from raw data to pixel tracks and vertices on GPUs (cms-patatrack#216)
    Port and optimise the full workflow from pixel raw data to pixel tracks and vertices to GPUs.
    Clean the pixel n-tuplets with the "fishbone" algorithm (only on GPUs).
    
    Other changes:
      - recover the Riemann fit updates lost during the merge with CMSSW 10.4.x;
      - speed up clustering and track fitting;
      - minor bug fix to avoid trivial regression with the optimized fit.

--- RecoPixelVertexing/Configuration/python/RecoPixelVertexing_cff.py
+++ RecoPixelVertexing/Configuration/python/RecoPixelVertexing_cff.py
@@ -4,7 +4,7 @@ from RecoPixelVertexing.PixelTrackFitting.PixelTracks_cff import *
 #
 # for STARTUP ONLY use try and use Offline 3D PV from pixelTracks, with adaptive vertex
 #
-#from RecoPixelVertexing.PixelVertexFinding.PixelVertexes_cff import *
-from RecoVertex.PrimaryVertexProducer.OfflinePixel3DPrimaryVertices_cfi import *
+from RecoPixelVertexing.PixelVertexFinding.PixelVertexes_cff import *
+#from RecoVertex.PrimaryVertexProducer.OfflinePixel3DPrimaryVertices_cfi import *
 recopixelvertexingTask = cms.Task(pixelTracksTask,pixelVertices)
 recopixelvertexing = cms.Sequence(recopixelvertexingTask)
