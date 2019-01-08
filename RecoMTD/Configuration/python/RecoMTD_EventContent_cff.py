f15383c0d6d8a20105f21ffc86f2f0dd41e40a2d
lgray <lindsey.gray@gmail.com>
2019-01-07 21:27:12 -0600

    more code review and bug fixes

--- RecoMTD/Configuration/python/RecoMTD_EventContent_cff.py
+++ RecoMTD/Configuration/python/RecoMTD_EventContent_cff.py
@@ -1,26 +1,14 @@
 import FWCore.ParameterSet.Config as cms
 
-#FEVT
-RecoMTDFEVT = cms.PSet(
+#AOD
+RecoMTDAOD = cms.PSet(
     outputCommands = cms.untracked.vstring(
+        'keep *_trackExtenderWithMTD_*_*'
         )
 )
 
 #RECO content
-RecoMTDRECO = RecoMTDFEVT.copy()
+RecoMTDRECO = RecoMTDAOD.copy()
 
-#AOD content
-RecoMTDAOD = RecoMTDFEVT.copy()
-
-_phase2TimingLayer_EventContent = cms.untracked.vstring('keep *_trackExtenderWithMTD_*_*')
-
-from Configuration.Eras.Modifier_phase2_timing_layer_tile_cff import phase2_timing_layer_tile
-from Configuration.Eras.Modifier_phase2_timing_layer_bar_cff import phase2_timing_layer_bar
-
-def modifyDataTier(DataTier):
-    added = DataTier.outputCommands + _phase2TimingLayer_EventContent
-    (phase2_timing_layer_tile | phase2_timing_layer_bar).toModify(DataTier, outputCommands = added)
-
-modifyDataTier(RecoMTDFEVT)
-modifyDataTier(RecoMTDRECO)
-modifyDataTier(RecoMTDAOD)
+#FEVT content
+RecoMTDFEVT = RecoMTDRECO.copy()
