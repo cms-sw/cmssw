ba0a0be725711c53fb5cadd6a9d72a2c47858ffc
Jan Kaspar <jan.kaspar@cern.ch>
2019-01-10 15:48:22 +0100

    Eliminated uncessary BeamDivergenceVtxGenerator.h.

--- IOMC/EventVertexGenerators/src/module.cc
+++ IOMC/EventVertexGenerators/src/module.cc
@@ -9,7 +9,6 @@
 #include "IOMC/EventVertexGenerators/interface/BetafuncEvtVtxGenerator.h"
 #include "IOMC/EventVertexGenerators/interface/GaussianZBeamSpotFilter.h"
 #include "IOMC/EventVertexGenerators/interface/HLLHCEvtVtxGenerator.h"
-#include "IOMC/EventVertexGenerators/interface/BeamDivergenceVtxGenerator.h"
 
 #include "FWCore/PluginManager/interface/ModuleDef.h"
 #include "FWCore/Framework/interface/MakerMacros.h"
@@ -25,4 +24,3 @@ DEFINE_FWK_MODULE(BeamProfileVtxGenerator) ;
 DEFINE_FWK_MODULE(BetafuncEvtVtxGenerator) ;
 DEFINE_FWK_MODULE(GaussianZBeamSpotFilter);
 DEFINE_FWK_MODULE(HLLHCEvtVtxGenerator);
-DEFINE_FWK_MODULE(BeamDivergenceVtxGenerator);
