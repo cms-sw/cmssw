4e1581872a7cb8d4b9fe1e36bb775da2af8a5af7
Andrea Bocci <andrea.bocci@cern.ch>
2019-01-14 13:33:34 +0100

    Synchronise with CMSSW_10_4_0

--- EventFilter/SiPixelRawToDigi/python/SiPixelRawToDigi_cfi.py
+++ EventFilter/SiPixelRawToDigi/python/SiPixelRawToDigi_cfi.py
@@ -1,6 +1,5 @@
 import FWCore.ParameterSet.Config as cms
 import EventFilter.SiPixelRawToDigi.siPixelRawToDigi_cfi
-import RecoLocalTracker.SiPixelClusterizer.siPixelDigiHeterogeneousConverter_cfi
 
 siPixelDigis = EventFilter.SiPixelRawToDigi.siPixelRawToDigi_cfi.siPixelRawToDigi.clone()
 siPixelDigis.Timing = cms.untracked.bool(False)
@@ -21,10 +20,12 @@ siPixelDigis.CablingMapLabel = cms.string("")
 
 from Configuration.Eras.Modifier_phase1Pixel_cff import phase1Pixel
 phase1Pixel.toModify(siPixelDigis, UsePhase1=True)
+from Configuration.ProcessModifiers.premix_stage2_cff import premix_stage2
+premix_stage2.toModify(siPixelDigis, BadPixelFEDChannelsInputLabel = "mixData")
 
+import RecoLocalTracker.SiPixelClusterizer.siPixelDigiHeterogeneousConverter_cfi
 _siPixelDigis_gpu = RecoLocalTracker.SiPixelClusterizer.siPixelDigiHeterogeneousConverter_cfi.siPixelDigiHeterogeneousConverter.clone()
 _siPixelDigis_gpu.includeErrors = cms.bool(True)
 
 from Configuration.ProcessModifiers.gpu_cff import gpu
 gpu.toReplaceWith(siPixelDigis, _siPixelDigis_gpu)
-
