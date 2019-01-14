027f20eb5a913a0dda6b929c56abf24437aedee5
Paolo Meridiani <paolo.meridiani@cern.ch>
2019-01-14 20:23:13 +0100

    fillDescription

--- RecoLocalFastTime/FTLClusterizer/plugins/MTDClusterProducer.cc
+++ RecoLocalFastTime/FTLClusterizer/plugins/MTDClusterProducer.cc
@@ -106,10 +106,7 @@ MTDClusterProducer::fillDescriptions(edm::ConfigurationDescriptions& description
   desc.add<std::string>("BarrelClusterName", "FTLBarrel");
   desc.add<std::string>("EndcapClusterName", "FTLEndcap");
   desc.add<std::string>("ClusterMode","MTDThresholdClusterizer");
-  desc.add<double>("HitThreshold", 0.);
-  desc.add<double>("SeedThreshold", 0.);
-  desc.add<double>("ClusterThreshold", 0.);
-  desc.add<double>("TimeThreshold", 10.);
+  MTDThresholdClusterizer::fillDescriptions(desc);
   descriptions.add("mtdClusterProducer", desc);
 }
   
