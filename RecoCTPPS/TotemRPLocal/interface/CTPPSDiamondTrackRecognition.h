76d72f0bc8b9dc9efddcf582b2e7d2d6c0195f9b
Laurent Forthomme <laurent.forthomme@cern.ch>
2019-01-15 18:07:35 +0100

    Separated CTPPSDiamondTrackRecognition implementation file from header

--- RecoCTPPS/TotemRPLocal/interface/CTPPSDiamondTrackRecognition.h
+++ RecoCTPPS/TotemRPLocal/interface/CTPPSDiamondTrackRecognition.h
@@ -11,13 +11,13 @@
 #ifndef RecoCTPPS_TotemRPLocal_CTPPSDiamondTrackRecognition
 #define RecoCTPPS_TotemRPLocal_CTPPSDiamondTrackRecognition
 
-#include "DataFormats/Common/interface/DetSetVector.h"
+#include "DataFormats/Common/interface/DetSet.h"
 #include "DataFormats/CTPPSReco/interface/CTPPSDiamondRecHit.h"
 #include "DataFormats/CTPPSReco/interface/CTPPSDiamondLocalTrack.h"
 
 #include "RecoCTPPS/TotemRPLocal/interface/CTPPSTimingTrackRecognition.h"
 
-#include <vector>
+#include <unordered_map>
 
 /**
  * \brief Class performing smart reconstruction for PPS Diamond Detectors.
@@ -26,8 +26,7 @@
 class CTPPSDiamondTrackRecognition : public CTPPSTimingTrackRecognition<CTPPSDiamondLocalTrack, CTPPSDiamondRecHit>
 {
   public:
-    CTPPSDiamondTrackRecognition( const edm::ParameterSet& iConfig ) :
-      CTPPSTimingTrackRecognition<CTPPSDiamondLocalTrack, CTPPSDiamondRecHit>( iConfig ) {}
+    CTPPSDiamondTrackRecognition( const edm::ParameterSet& iConfig );
 
     void clear() override;
     /// Feed a new hit to the tracks recognition algorithm
@@ -38,76 +37,6 @@ class CTPPSDiamondTrackRecognition : public CTPPSTimingTrackRecognition<CTPPSDia
   private:
     std::unordered_map<int,int> mhMap_;
 };
-
-/****************************************************************************
- * Implementation
- ****************************************************************************/
-
-void
-CTPPSDiamondTrackRecognition::clear()
-{
-  CTPPSTimingTrackRecognition<CTPPSDiamondLocalTrack, CTPPSDiamondRecHit>::clear();
-  mhMap_.clear();
-}
-
-//----------------------------------------------------------------------------------------------------
-
-void
-CTPPSDiamondTrackRecognition::addHit( const CTPPSDiamondRecHit& recHit )
-{
-  // store hit parameters
-  hitVectorMap_[recHit.getOOTIndex()].emplace_back( recHit );
-}
-
-//----------------------------------------------------------------------------------------------------
-
-int
-CTPPSDiamondTrackRecognition::produceTracks( edm::DetSet<CTPPSDiamondLocalTrack>& tracks )
-{
-  int numberOfTracks = 0;
-  DimensionParameters param;
-
-  for ( const auto& hitBatch: hitVectorMap_ ) {
-    const auto& oot = hitBatch.first;
-    const auto& hits = hitBatch.second;
-
-    auto hitRange = getHitSpatialRange( hits );
-
-    std::vector<CTPPSDiamondLocalTrack> xPartTracks;
-    auto getX = []( const CTPPSDiamondRecHit& hit ){ return hit.getX(); };
-    auto getXWidth = []( const CTPPSDiamondRecHit& hit ){ return hit.getXWidth(); };
-    auto setX = []( CTPPSDiamondLocalTrack& track, float x ){ track.setPosition( math::XYZPoint( x, 0., 0. ) ); };
-    auto setXSigma = []( CTPPSDiamondLocalTrack& track, float sigma ){ track.setPositionSigma( math::XYZPoint( sigma, 0., 0. ) ); };
-
-    // Produces tracks in x dimension
-    param.rangeBegin = hitRange.xBegin;
-    param.rangeEnd = hitRange.xEnd;
-    producePartialTracks( hits, param, getX, getXWidth, setX, setXSigma, xPartTracks );
-
-    if ( xPartTracks.empty() )
-      continue;
-
-    const float yRangeCenter = 0.5f*( hitRange.yBegin + hitRange.yEnd );
-    const float zRangeCenter = 0.5f*( hitRange.zBegin + hitRange.zEnd );
-    const float ySigma = 0.5f*( hitRange.yEnd - hitRange.yBegin );
-    const float zSigma = 0.5f*( hitRange.zEnd - hitRange.zBegin );
-
-    for ( const auto& xTrack: xPartTracks ) {
-      math::XYZPoint position( xTrack.getX0(), yRangeCenter, zRangeCenter );
-      math::XYZPoint positionSigma( xTrack.getX0Sigma(), ySigma, zSigma );
-
-      const int multipleHits = ( mhMap_.find(oot) != mhMap_.end() )
-        ? mhMap_[oot]
-        : 0;
-      CTPPSDiamondLocalTrack newTrack( position, positionSigma, 0.f, 0.f, oot, multipleHits );
-      newTrack.setValid( true );
-
-      tracks.push_back( newTrack );
-    }
-  }
-
-  return numberOfTracks;
-}
 
 #endif
 
