762c0992bd5d573bbb8aac8d619a24b464b823a4
Laurent Forthomme <laurent.forthomme@cern.ch>
2019-01-15 14:10:13 +0100

    Renaming of ProtonTrack to ForwardProton

--- DataFormats/ProtonReco/src/ForwardProton.cc
+++ DataFormats/ProtonReco/src/ForwardProton.cc
@@ -4,20 +4,20 @@
  *   Laurent Forthomme
  ****************************************************************************/
 
-#include "DataFormats/ProtonReco/interface/ProtonTrack.h"
+#include "DataFormats/ProtonReco/interface/ForwardProton.h"
 
 #include <set>
 
 using namespace reco;
 
-ProtonTrack::ProtonTrack() :
+ForwardProton::ForwardProton() :
   t_( 0. ), t_err_( 0. ), xi_( 0. ), chi2_( 0. ), ndof_( 0 ),
   valid_fit_( false ), method_( ReconstructionMethod::invalid )
 {}
 
-ProtonTrack::ProtonTrack( double chi2, double ndof, const Point& vtx, const Vector& momentum, float xi,
-                          const CovarianceMatrix& cov, ReconstructionMethod method,
-                          const CTPPSLocalTrackLiteRefVector& local_tracks, bool valid ) :
+ForwardProton::ForwardProton( double chi2, double ndof, const Point& vtx, const Vector& momentum, float xi,
+                              const CovarianceMatrix& cov, ReconstructionMethod method,
+                              const CTPPSLocalTrackLiteRefVector& local_tracks, bool valid ) :
   vertex_( vtx ), momentum_( momentum ),
   t_( 0. ), t_err_( 0. ), xi_( xi ),
   covariance_( cov ), chi2_( chi2 ), ndof_( ndof ),
@@ -25,7 +25,7 @@ ProtonTrack::ProtonTrack( double chi2, double ndof, const Point& vtx, const Vect
 {}
 
 float
-ProtonTrack::calculateT( double beam_mom, double proton_mom, double theta )
+ForwardProton::calculateT( double beam_mom, double proton_mom, double theta )
 {
   const double t0 = 2.*( massSquared_+beam_mom*proton_mom-sqrt( ( massSquared_+beam_mom*beam_mom ) * ( massSquared_+proton_mom*proton_mom ) ) );
   const double S = sin(theta/2.);
@@ -33,7 +33,7 @@ ProtonTrack::calculateT( double beam_mom, double proton_mom, double theta )
 }
 
 float
-ProtonTrack::t() const
+ForwardProton::t() const
 {
   const double beam_mom = p() / (1.-xi());
   const double theta = std::hypot( thetaX(), thetaY() );
