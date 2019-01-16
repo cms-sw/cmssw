c2c4b29daaf5494f50fb5638fb0a39d2a00015eb
Salavat <abdullin@mail.cern.ch>
2019-01-16 12:53:56 +0100

    fixing omission of the mergingjob config

--- Validation/CaloTowers/test/CaloScan/merging_cfg.py
+++ Validation/CaloTowers/test/CaloScan/merging_cfg.py
@@ -3,18 +3,12 @@ import FWCore.ParameterSet.Config as cms
 from DQMServices.Core.DQMEDHarvester import DQMEDHarvester
 
 process = cms.Process("CONV")
-process.load("Configuration.StandardSequences.Reconstruction_cff")
-process.load("Configuration.StandardSequences.GeometryRecoDB_cff")
-
-process.load("Configuration.StandardSequences.FrontierConditions_GlobalTag_cff")
-from Configuration.AlCa.autoCond import autoCond
-process.GlobalTag.globaltag = autoCond['mc']
 
 process.load("FWCore.MessageLogger.MessageLogger_cfi")
 process.MessageLogger.cerr.FwkReport.reportEvery = 1000
 
-process.load("DQMServices.Core.DQM_cfg")
-process.DQM.collectorHost = ''
+process.load("DQMServices.Core.DQMStore_cfi")
+process.load("DQMServices.Components.MEtoEDMConverter_cfi")
 
 process.maxEvents = cms.untracked.PSet(
     input = cms.untracked.int32(-1)
@@ -46,7 +40,32 @@ process.source = cms.Source("PoolSource",
        'file:pi50_22.root',
        'file:pi50_23.root',
        'file:pi50_24.root',
-       'file:pi50_25.root'
+       'file:pi50_25.root',
+       'file:pi50_26.root',
+       'file:pi50_27.root',
+       'file:pi50_28.root',
+       'file:pi50_29.root',
+       'file:pi50_30.root',
+       'file:pi50_31.root',
+       'file:pi50_32.root',
+       'file:pi50_33.root',
+       'file:pi50_34.root',
+       'file:pi50_35.root',
+       'file:pi50_36.root',
+       'file:pi50_37.root',
+       'file:pi50_38.root',
+       'file:pi50_39.root',
+       'file:pi50_40.root',
+       'file:pi50_41.root',
+       'file:pi50_42.root',
+       'file:pi50_43.root',
+       'file:pi50_44.root',
+       'file:pi50_45.root',
+       'file:pi50_46.root',
+       'file:pi50_47.root',
+       'file:pi50_48.root',
+       'file:pi50_49.root',
+       'file:pi50_50.root'
       )
 )
 
@@ -56,6 +75,11 @@ process.dqmSaver.referenceHandling = cms.untracked.string('all')
 cmssw_version = os.environ.get('CMSSW_VERSION','CMSSW_X_Y_Z')
 Workflow = '/HcalValidation/'+'Harvesting/'+str(cmssw_version)
 process.dqmSaver.workflow = Workflow
+
+process.hcaldigisClient = DQMEDHarvester("HcalDigisClient",
+     outputFile	= cms.untracked.string('HcalDigisHarvestingME.root'),
+     DQMDirName	= cms.string("/") # root directory
+)
 
 process.calotowersClient = DQMEDHarvester("CaloTowersClient", 
      outputFile = cms.untracked.string('CaloTowersHarvestingME.root'),
@@ -70,4 +94,5 @@ process.p = cms.Path(
 process.EDMtoME * 
 process.calotowersClient * 
 process.hcalrechitsClient *
+process.hcaldigisClient *
 process.dqmSaver)
