64d401ff426d828746e2cf54d2d5adc70d2ff2d2
Chris Jones <chrisdjones15@gmail.com>
2019-01-16 11:24:40 -0600

    Added ValidHandle
    The new handle type is guaranteed to be valid so no additional checks are done internally when it is being used.

---
+++ DataFormats/Common/src/ValidHandle.cc
@@ -0,0 +1,11 @@
+#include "DataFormats/Common/interface/ValidHandle.h"
+#include "FWCore/Utilities/interface/Exception.h"
+
+namespace edm::vhhelper {
+  void
+  throwIfNotValid(const void* iProduct) noexcept(false) {
+    if(nullptr == iProduct) {
+      throw cms::Exception("Invalid Product")<<"Attempted to fill a edm::ValidHandle with an invalid product";
+    }
+  }
+}
