75d66ace425f3644b9eb97f6fb868b3a738189ad
Josh Bendavid <Josh.Bendavid@cern.ch>
2019-01-18 13:50:28 +0000

    split 4D vertices into separate cfi file

--- PhysicsTools/PatAlgos/python/slimming/slimming_cff.py
+++ PhysicsTools/PatAlgos/python/slimming/slimming_cff.py
@@ -4,6 +4,7 @@ from PhysicsTools.PatAlgos.slimming.packedPFCandidates_cff import *
 from PhysicsTools.PatAlgos.slimming.isolatedTracks_cfi import *
 from PhysicsTools.PatAlgos.slimming.lostTracks_cfi import *
 from PhysicsTools.PatAlgos.slimming.offlineSlimmedPrimaryVertices_cfi import *
+from PhysicsTools.PatAlgos.slimming.offlineSlimmedPrimaryVertices4D_cfi import *
 from PhysicsTools.PatAlgos.slimming.primaryVertexAssociation_cfi import *
 from PhysicsTools.PatAlgos.slimming.genParticles_cff import *
 from PhysicsTools.PatAlgos.slimming.selectedPatTrigger_cfi import *
