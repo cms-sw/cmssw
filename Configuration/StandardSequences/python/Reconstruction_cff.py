9abb7e8edf125befee78c243f20f251fbd15deaf
Robert Bainbridge <bainbrid@users.noreply.github.com>
2019-01-21 16:08:15 +0100

    execute lowPtGsfElectronSequence only for bParking era

--- Configuration/StandardSequences/python/Reconstruction_cff.py
+++ Configuration/StandardSequences/python/Reconstruction_cff.py
@@ -50,9 +50,6 @@ from RecoLocalCalo.CastorReco.CastorSimpleReconstructor_cfi import *
 # Cosmic During Collisions
 from RecoTracker.SpecialSeedGenerators.cosmicDC_cff import *
 
-# Low pT electrons
-from RecoEgamma.EgammaElectronProducers.lowPtGsfElectronSequence_cff import *
-
 localreco = cms.Sequence(bunchSpacingProducer+trackerlocalreco+muonlocalreco+calolocalreco+castorreco)
 localreco_HcalNZS = cms.Sequence(bunchSpacingProducer+trackerlocalreco+muonlocalreco+calolocalrecoNZS+castorreco)
 
@@ -149,9 +146,15 @@ highlevelreco = cms.Sequence(egammaHighLevelRecoPrePF*
                              recoPFMET*
                              PFTau*
                              reducedRecHits*
-                             cosmicDCTracksSeq*
-                             lowPtGsfElectronSequence
+                             cosmicDCTracksSeq
                              )
+
+# Low pT electrons for bParking
+from Configuration.Eras.Modifier_bParking_cff import bParking
+from RecoEgamma.EgammaElectronProducers.lowPtGsfElectronSequence_cff import *
+_highlevelreco_bParking = highlevelreco.copy()
+_highlevelreco_bParking += lowPtGsfElectronSequence
+bParking.toReplaceWith(highlevelreco, _highlevelreco_bParking)
 
 # XeXe data with pp reco
 from Configuration.Eras.Modifier_pp_on_XeXe_2017_cff import pp_on_XeXe_2017
