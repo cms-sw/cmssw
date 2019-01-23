14a6a99b344ba67d856bf8edc48e79cccf17eae7
Andrea Bellora <andrea.bellora@cern.ch>
2019-01-23 14:55:06 +0100

    Added mantissa rounding for tracklites and small review-prompted changes

--- DataFormats/CTPPSReco/interface/CTPPSPixelLocalTrackRecoInfo.h
+++ DataFormats/CTPPSReco/interface/CTPPSPixelLocalTrackRecoInfo.h
@@ -14,6 +14,7 @@
   /// reco_info = noShiftedPlanes  -> Track reconstructed in a bx-shifted ROC with non-bx-shifted planes only
   /// reco_info = mixedPlanes      -> Track reconstructed in a bx-shifted ROC both with bx-shifted and non-bx-shifted planes
   /// reco_info = invalid          -> Dummy value. Assigned when reco_info is not computed
-  enum class CTPPSReconstructionInfo {notShiftedRun = 0, allShiftedPlanes = 1, noShiftedPlanes = 2, mixedPlanes = 3, invalid = 5};
+  enum class CTPPSpixelLocalTrackReconstructionInfo {notShiftedRun = 0, allShiftedPlanes = 1, noShiftedPlanes = 2, mixedPlanes = 3, invalid = 5};
 
 #endif
+  
