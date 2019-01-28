5a77d602de8202fc56b00287f8617473e592bba3
Andrea Bocci <andrea.bocci@cern.ch>
2019-01-28 11:37:35 +0100

    Synchronise with CMSSW_10_5_0_pre1

--- Validation/RecoTrack/python/TrackValidation_cff.py
+++ Validation/RecoTrack/python/TrackValidation_cff.py
@@ -836,7 +836,6 @@ tracksValidationLite = cms.Sequence(
 ## customization for timing
 from Configuration.Eras.Modifier_phase2_timing_layer_cff import phase2_timing_layer
 phase2_timing_layer.toModify( generalTracksFromPV, 
-                              vertexTag = cms.InputTag('offlinePrimaryVertices4D'),
                               timesTag  = cms.InputTag('trackTimeValueMapProducer:generalTracksConfigurableFlatResolutionModel'), 
                               timeResosTag = cms.InputTag('trackTimeValueMapProducer:generalTracksConfigurableFlatResolutionModelResolution'), 
                               nSigmaDtVertex = cms.double(3) )
@@ -850,3 +849,9 @@ phase2_timing_layer.toModify( trackValidatorConversionStandalone,
                               label_vertex = cms.untracked.InputTag('offlinePrimaryVertices4D') )
 phase2_timing_layer.toModify( trackValidatorGsfTracks,
                               label_vertex = cms.untracked.InputTag('offlinePrimaryVertices4D') )
+
+from Configuration.Eras.Modifier_phase2_timing_layer_tile_cff import phase2_timing_layer_tile
+from Configuration.Eras.Modifier_phase2_timing_layer_bar_cff import phase2_timing_layer_bar
+(phase2_timing_layer_tile | phase2_timing_layer_bar).toModify( generalTracksFromPV, 
+                              timesTag  = cms.InputTag('tofPID:t0'), 
+                              timeResosTag = cms.InputTag('tofPID:sigmat0') )
