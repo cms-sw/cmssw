b5a0506b0c4576f51ade21dcf65d0957a4b87799
Nancy <nancy.marinelli@cern.ch>
2019-01-29 14:32:20 +0100

    cleaning

--- RecoEgamma/EgammaPhotonProducers/python/conversionOpenTrackSequence_cff.py
+++ RecoEgamma/EgammaPhotonProducers/python/conversionOpenTrackSequence_cff.py
@@ -3,7 +3,7 @@ import FWCore.ParameterSet.Config as cms
 import RecoEgamma.EgammaPhotonProducers.conversionTrackProducer_cfi
 
 #producer from lowPt electronTracksOpen
-gsfTracksOpenConversionTrackProducer = RecoEgamma.EgammaPhotonProducers.conversionTrackProducer_cfi.conversionTrackProducer.clone(
+gsfTracksOpenConversionTrackProducer = conversionTrackProducer.clone(
    TrackProducer = cms.string('lowPtGsfEleGsfTracks'),
    setIsGsfTrackOpen = cms.bool(True),
    setArbitratedMergedEcalGeneral  = cms.bool(False),
