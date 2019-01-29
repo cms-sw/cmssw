bd13e022daa1538f48bbf738a971254eae996de7
Emilia Becheva <emilia.becheva@llr.in2p3.fr>
2019-01-29 16:59:24 +0100

    Delete old package HGCalTriggerPrimitives

--- Validation/HGCalTriggerPrimitives/python/hgcalValidationTPG_cff.py
+++
@@ -1,6 +0,0 @@
-import FWCore.ParameterSet.Config as cms
-
-from L1Trigger.L1THGCal.hgcalTriggerGeometryESProducer_cfi import *
-from Validation.HGCalTriggerPrimitives.hgcalValidationTPG_cfi import *
-
-runHGCALValidationTPG = cms.Sequence(hgcalTrigPrimValidation)
