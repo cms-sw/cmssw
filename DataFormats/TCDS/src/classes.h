a723e8263566a56af3e41441d825038ae21370d9
Christopher Jones <chrisdjones15@gmail.com>
2019-01-31 09:51:43 -0600

    Removed struct dictionary from classes.h files
    ROOT no longer requires forcing the instantiation of templates
    in the .h file. The declaration in classes_def.xml is sufficient.

--- DataFormats/TCDS/src/classes.h
+++ DataFormats/TCDS/src/classes.h
@@ -3,13 +3,3 @@
 #include "DataFormats/Common/interface/Ref.h"
 #include "DataFormats/Common/interface/RefProd.h"
 
-namespace DataFormats_TCDS {
-  struct dictionary {
-
-    TCDSRecord tcdsRecord;
-
-    edm::Wrapper<TCDSRecord> w_tcdsRecord;
-
-    edm::RefProd<TCDSRecord> tcdsRecordRef;
-  };
-}
