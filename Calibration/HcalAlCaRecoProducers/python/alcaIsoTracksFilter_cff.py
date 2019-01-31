0543c5b0f0f999f970f1a5540ff5918dd5b550b2
Sunanda <sunanda.banerjee@cern.ch>
2019-01-31 04:59:56 +0100

    define era modifications within cff in right sequence

--- Calibration/HcalAlCaRecoProducers/python/alcaIsoTracksFilter_cff.py
+++ Calibration/HcalAlCaRecoProducers/python/alcaIsoTracksFilter_cff.py
@@ -2,7 +2,18 @@ import FWCore.ParameterSet.Config as cms
 
 from Calibration.HcalAlCaRecoProducers.alcaIsoTracksFilter_cfi import *
 
-from Configuration.Eras.Modifier_run2_HCAL_2017_cff import run2_HCAL_2017
+from Configuration.Eras.Modifier_run2_ECAL_2017_cff import run2_ECAL_2017
+
+run2_ECAL_2017.toModify(alcaIsoTracksFilter,
+  EBHitEnergyThreshold    = cms.double(0.18),
+  EEHitEnergyThreshold0   = cms.double(-206.074),
+  EEHitEnergyThreshold1   = cms.double(357.671),
+  EEHitEnergyThreshold2   = cms.double(-204.978),
+  EEHitEnergyThreshold3   = cms.double(39.033),
+  EEHitEnergyThresholdLow = cms.double(1.25),
+  EEHitEnergyThresholdHigh= cms.double(10.0),
+)
+
 from Configuration.Eras.Modifier_run2_HCAL_2018_cff import run2_HCAL_2018
 
 run2_HCAL_2018.toModify(alcaIsoTracksFilter,
@@ -13,14 +24,4 @@ run2_HCAL_2018.toModify(alcaIsoTracksFilter,
   EEHitEnergyThreshold3   = cms.double(7.04303),
   EEHitEnergyThresholdLow = cms.double(0.11),
   EEHitEnergyThresholdHigh= cms.double(15.4),
-)
-
-run2_HCAL_2017.toModify(alcaIsoTracksFilter,
-  EBHitEnergyThreshold    = cms.double(0.18),
-  EEHitEnergyThreshold0   = cms.double(-206.074),
-  EEHitEnergyThreshold1   = cms.double(357.671),
-  EEHitEnergyThreshold2   = cms.double(-204.978),
-  EEHitEnergyThreshold3   = cms.double(39.033),
-  EEHitEnergyThresholdLow = cms.double(1.25),
-  EEHitEnergyThresholdHigh= cms.double(10.0),
 )
