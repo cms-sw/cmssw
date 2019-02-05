4844966868916ce926e688a6ff4835761ec73d10
Laurent Forthomme <laurent.forthomme@cern.ch>
2019-02-05 14:25:32 +0100

    Workaround while the db is not yet populated with calibration payload

--- RecoCTPPS/Configuration/python/recoCTPPS_sequences_cff.py
+++ RecoCTPPS/Configuration/python/recoCTPPS_sequences_cff.py
@@ -9,6 +9,11 @@ from RecoCTPPS.PixelLocal.ctppsPixelLocalReconstruction_cff import *
 from CondFormats.CTPPSReadoutObjects.ctppsRPAlignmentCorrectionsDataESSourceXML_cfi import *
 ctppsRPAlignmentCorrectionsDataESSourceXML.RealFiles = cms.vstring("Alignment/CTPPS/data/RPixGeometryCorrections.xml")
 
+#--- temporary workaround while calibration conditions are integrated within the DB
+from CondFormats.CTPPSReadoutObjects.ppsTimingCalibrationESSource_cfi import ppsTimingCalibrationESSource
+ppsTimingCalibrationESSource.calibrationFile = cms.FileInPath('RecoCTPPS/TotemRPLocal/data/timing_offsets_ufsd_2018.dec18.cal.json')
+#--- to be removed before the integration
+
 recoCTPPSdets = cms.Sequence(
     totemRPLocalReconstruction *
     ctppsDiamondLocalReconstruction *
