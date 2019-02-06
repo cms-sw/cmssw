e9904b45ebf107d33c679f3bceb081ad5dd887af
Laurent Forthomme <laurent.forthomme@cern.ch>
2019-02-06 12:20:20 +0100

    Added import to print_function from __future__ wherever necessary for Python 3

--- CondTools/CTPPS/test/retrieve_sqlite_PixGainCalib_cfg.py
+++ CondTools/CTPPS/test/retrieve_sqlite_PixGainCalib_cfg.py
@@ -1,6 +1,6 @@
 import FWCore.ParameterSet.Config as cms
 import sys,os
-
+from __future__ import print_function
 
 arguments=sys.argv
 sqlitename="ctppspixgain.db"
@@ -15,13 +15,13 @@ else:
     if len(arguments)>3: tagname= arguments[3]
     if len(arguments)>4: runnumber=int(arguments[4])
     if len(arguments)>5: outfile=arguments[5]
-    
+
 process = cms.Process("ProcessOne")
 
 process.source = cms.Source("EmptyIOVSource",
     timetype = cms.string('runnumber'),
-    firstValue = cms.uint64(runnumber),  
-    lastValue = cms.uint64(runnumber),   
+    firstValue = cms.uint64(runnumber),
+    lastValue = cms.uint64(runnumber),
     interval = cms.uint64(1)
 )
 
@@ -29,7 +29,7 @@ process.source = cms.Source("EmptyIOVSource",
 process.load("CondCore.CondDB.CondDB_cfi")
 
 # input database (in this case local sqlite file)
-process.CondDB.connect = 'sqlite_file:'+sqlitename  
+process.CondDB.connect = 'sqlite_file:'+sqlitename
 
 
 process.PoolDBESSource = cms.ESSource("PoolDBESSource",
@@ -45,8 +45,8 @@ process.PoolDBESSource = cms.ESSource("PoolDBESSource",
 
 process.myprodtest = cms.EDAnalyzer("CTPPSPixGainCalibsESAnalyzer",
                                     outputrootfile=cms.untracked.string(outfile)
-                                    ) 
- 
+                                    )
+
 process.p = cms.Path(process.myprodtest)
 
 
