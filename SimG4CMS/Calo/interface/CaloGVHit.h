85dfa05a1bb41a03cc0997e83f5fe2890c687b65
Sunanda Banerjee <sunanda@cmsdev14.cern.ch>
2019-02-07 11:05:21 +0100

    Try to remove dependency of CLHEP in a few places and streamline the SteppingAction code for transporting to GeantV application

--- SimG4CMS/Calo/interface/CaloGVHit.h
+++ SimG4CMS/Calo/interface/CaloGVHit.h
@@ -39,33 +39,36 @@ public:
   
 public:
   
-  double         getEM() const                 {return elem;}
-  void           setEM (double e)              {elem = e;}
+  double         getEM() const                 {return elem_;}
+  void           setEM (double e)              {elem_ = e;}
   
-  double         getHadr() const               {return hadr;}
-  void           setHadr (double e)            {hadr = e;}
+  double         getHadr() const               {return hadr_;}
+  void           setHadr (double e)            {hadr_ = e;}
   
-  int            getTrackID() const            {return hitID.trackID();}
-  uint32_t       getUnitID() const             {return hitID.unitID();}
-  double         getTimeSlice() const          {return hitID.timeSlice();}
-  int            getTimeSliceID() const        {return hitID.timeSliceID();}
-  uint16_t       getDepth() const              {return hitID.depth();}
+  int            getEventID() const            {return eventID_;}
+  void           setEventID(int id)            {eventID_ = id;}
 
-  CaloHitID      getID() const                 {return hitID;}
+  int            getTrackID() const            {return hitID_.trackID();}
+  uint32_t       getUnitID() const             {return hitID_.unitID();}
+  double         getTimeSlice() const          {return hitID_.timeSlice();}
+  int            getTimeSliceID() const        {return hitID_.timeSliceID();}
+  uint16_t       getDepth() const              {return hitID_.depth();}
+
+  CaloHitID      getID() const                 {return hitID_;}
   void           setID (uint32_t i, double d, int j, uint16_t k=0) {
-    hitID.setID(i,d,j,k);}
-  void           setID (const CaloHitID& id)   {hitID = id;}
+    hitID_.setID(i,d,j,k);}
+  void           setID (const CaloHitID& id)   {hitID_ = id;}
   
   void           addEnergyDeposit(double em, double hd);
   void           addEnergyDeposit(const CaloGVHit& aHit);
   
-  double         getEnergyDeposit() const      {return (elem+hadr);}
+  double         getEnergyDeposit() const      {return (elem_+hadr_);}
   
 private:
-  
-  double           elem;              //EnergyDeposit of EM particles
-  double           hadr;              //EnergyDeposit of HD particles
-  CaloHitID        hitID;             //Identification number of the hit given 
+  int              eventID_;          //Event ID
+  double           elem_;             //EnergyDeposit of EM particles
+  double           hadr_;             //EnergyDeposit of HD particles
+  CaloHitID        hitID_;            //Identification number of the hit given 
                                       //by primary particle, Cell ID, Time of 
                                       //the hit
 };
@@ -73,7 +76,9 @@ private:
 class CaloGVHitLess {
 public:
   bool operator()( const CaloGVHit* a, const CaloGVHit* b) {
-    if (a->getTrackID() != b->getTrackID()) {
+    if (a->getEventID() != b->getEventID()) {
+      return (a->getEventID() < b->getEventID());
+    } else if (a->getTrackID() != b->getTrackID()) {
       return (a->getTrackID() < b->getTrackID());
     } else if (a->getUnitID() != b->getUnitID()) {
       return (a->getUnitID() < b->getUnitID());
@@ -88,7 +93,8 @@ public:
 class CaloGVHitEqual {
 public:
   bool operator()( const CaloGVHit* a, const CaloGVHit* b) {
-    return (a->getTrackID()     == b->getTrackID() && 
+    return (a->getEventID()     == b->getEventID() && 
+	    a->getTrackID()     == b->getTrackID() && 
 	    a->getUnitID()      == b->getUnitID()  && 
 	    a->getDepth()       == b->getDepth()   &&
 	    a->getTimeSliceID() == b->getTimeSliceID());
