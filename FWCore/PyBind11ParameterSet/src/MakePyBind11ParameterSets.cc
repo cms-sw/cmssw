a8c91f7ed39a1c38df0bacff6786f264738a6e38
David <David.Lange@cern.ch>
2019-02-08 13:12:43 +0100

    pybind11 based parameter set reading tools - working for python2 at least

---
+++ FWCore/PyBind11ParameterSet/src/MakePyBind11ParameterSets.cc
@@ -0,0 +1,77 @@
+#include "FWCore/PyBind11ParameterSet/interface/MakePyBind11ParameterSets.h"
+#include "FWCore/ParameterSet/interface/ParameterSet.h"
+
+#include "FWCore/PyBind11ParameterSet/interface/Python11ParameterSet.h"
+#include "FWCore/PyBind11ParameterSet/interface/PyBind11ProcessDesc.h"
+#include "initializePyBind11Module.h"
+#include <pybind11/embed.h>
+
+static
+void
+makePSetsFromFile(std::string const& fileName) { 
+  std::string initCommand("from FWCore.ParameterSet.Types import makeCppPSet\n"
+                          "execfile('");
+  initCommand += fileName + "')";
+
+  pybind11::exec(initCommand);
+  pybind11::exec("makeCppPSet(locals(), topPSet)");
+}
+
+static
+void
+makePSetsFromString(std::string const& module) { 
+  std::string command = module;
+  command += "\nfrom FWCore.ParameterSet.Types import makeCppPSet\nmakeCppPSet(locals(), topPSet)";
+  pybind11::exec(command);
+}
+
+namespace edm {
+  namespace cmspybind11 {
+    std::unique_ptr<ParameterSet>
+    readConfig(std::string const& config) {
+      PyBind11ProcessDesc pythonProcessDesc(config);
+      return pythonProcessDesc.parameterSet();
+    }
+
+    std::unique_ptr<ParameterSet>
+    readConfig(std::string const& config, int argc, char* argv[]) {
+      PyBind11ProcessDesc pythonProcessDesc(config, argc, argv);
+      return pythonProcessDesc.parameterSet();
+    }
+
+    void
+    makeParameterSets(std::string const& configtext,
+		      std::unique_ptr<ParameterSet>& main) {
+      PyBind11ProcessDesc pythonProcessDesc(configtext);
+      main = pythonProcessDesc.parameterSet();
+    }
+
+    std::unique_ptr<ParameterSet>
+    readPSetsFrom(std::string const& module) {
+
+      pybind11::scoped_interpreter guard{};
+      python::initializePyBind11Module();
+      Python11ParameterSet theProcessPSet; //scope ok?
+      {
+	pybind11::object mainModule = pybind11::module::import("__main__");
+	mainModule.attr("topPSet") = pybind11::cast(&theProcessPSet);
+	
+	try {
+	  // if it ends with py, it's a file
+	  if(module.substr(module.size()-3) == ".py") {
+	    makePSetsFromFile(module);
+	  } else {
+	    makePSetsFromString(module);
+	  }
+	}
+	catch( pybind11::error_already_set const& ) {
+	  pythonToCppException("Configuration");
+	  //pybind11::finalize_interpreter();
+	  //Py_Finalize();
+	}
+	//theProcessPSet.pset().swap(*returnValue);
+      }
+      return std::make_unique<edm::ParameterSet>(ParameterSet(theProcessPSet.pset()));
+    }
+  }
+} // namespace edm
