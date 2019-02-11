d39be48e56363727b69a5c453e6239249e54835a
Jason Lee <jason.lee@cern.ch>
2019-02-11 15:45:30 +0100

    GEM vfatv3 improved dataformat for HW classes backward compatible with v2 Removed struct dictionary from classes.h files

---
+++ EventFilter/GEMRawToDigi/src/AMCdata.cc
@@ -0,0 +1,31 @@
+#include <cstdint>
+#include "EventFilter/GEMRawToDigi/interface/AMCdata.h"
+
+using namespace gem;
+
+void AMCdata::setAMCheader1(uint32_t dataLength, uint16_t bxID, uint32_t l1AID, uint8_t AMCnum)
+{
+  AMCheader1 u;  
+  u.dataLength = dataLength;
+  u.bxID = bxID;
+  u.l1AID = l1AID;
+  u.AMCnum = AMCnum;
+  amch1_ = u.word;
+}
+
+void AMCdata::setAMCheader2(uint16_t boardID, uint16_t orbitNum, uint8_t runType)
+{
+  AMCheader2 u;  
+  u.boardID = boardID;
+  u.orbitNum = orbitNum;
+  u.runType = runType;
+  amch2_ = u.word;
+}
+
+void AMCdata::setGEMeventHeader(uint8_t davCnt, uint32_t davList)
+{
+  EventHeader u;  
+  u.davCnt = davCnt;
+  u.davList = davList;
+  eh_ = u.word;
+}
