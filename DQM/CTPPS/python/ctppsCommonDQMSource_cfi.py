1c9f19f7fe29bb780af6f7fdcf96060a2acb1fbd
Jan Kaspar <jan.kaspar@cern.ch>
2019-02-12 16:06:30 +0100

    Added plots for SW tests and RelVals (lite tracks and proton reco).

--- DQM/CTPPS/python/ctppsCommonDQMSource_cfi.py
+++ DQM/CTPPS/python/ctppsCommonDQMSource_cfi.py
@@ -3,7 +3,8 @@ import FWCore.ParameterSet.Config as cms
 from DQMServices.Core.DQMEDAnalyzer import DQMEDAnalyzer
 
 ctppsCommonDQMSource = DQMEDAnalyzer('CTPPSCommonDQMSource',
-    tagLocalTrackLite = cms.InputTag('ctppsLocalTrackLiteProducer'),
     ctppsmetadata = cms.untracked.InputTag("onlineMetaDataDigis"),
+    tagLocalTrackLite = cms.InputTag('ctppsLocalTrackLiteProducer'),
+    tagRecoProtons = cms.InputTag("ctppsProtons", "multiRP"),
     verbosity = cms.untracked.uint32(0),
 )
