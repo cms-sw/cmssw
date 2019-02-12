36d2189cfcd87a957f45a49a64ed7e0dbcdc8ba1
Robert Bainbridge <bainbrid@users.noreply.github.com>
2019-02-12 16:57:04 +0100

    store Seed BDT outputs in ValueMaps keyed by GsfTrackRef instead of GsfElectronRef

--- RecoEgamma/EgammaElectronProducers/plugins/LowPtGsfElectronSeedValueMapsProducer.cc
+++ RecoEgamma/EgammaElectronProducers/plugins/LowPtGsfElectronSeedValueMapsProducer.cc
@@ -1,11 +1,10 @@
 #include "DataFormats/Common/interface/Handle.h"
-#include "DataFormats/EgammaCandidates/interface/GsfElectron.h"
-#include "DataFormats/EgammaCandidates/interface/GsfElectronCore.h"
 #include "DataFormats/EgammaReco/interface/ElectronSeed.h"
 #include "DataFormats/EgammaReco/interface/ElectronSeedFwd.h"
 #include "DataFormats/GsfTrackReco/interface/GsfTrack.h"
 #include "DataFormats/GsfTrackReco/interface/GsfTrackExtra.h"
 #include "DataFormats/ParticleFlowReco/interface/PreId.h"
+#include "FWCore/MessageLogger/interface/MessageLogger.h"
 #include "FWCore/ParameterSet/interface/ParameterSet.h"
 #include "FWCore/ParameterSet/interface/ParameterSetDescription.h"
 #include "FWCore/Utilities/interface/InputTag.h"
@@ -14,7 +13,7 @@
 ////////////////////////////////////////////////////////////////////////////////
 //
 LowPtGsfElectronSeedValueMapsProducer::LowPtGsfElectronSeedValueMapsProducer( const edm::ParameterSet& conf ) :
-  gsfElectrons_(consumes<reco::GsfElectronCollection>(conf.getParameter<edm::InputTag>("electrons"))),
+  gsfTracks_(consumes<reco::GsfTrackCollection>(conf.getParameter<edm::InputTag>("gsfTracks"))),
   preIdsValueMap_(consumes< edm::ValueMap<reco::PreIdRef> >(conf.getParameter<edm::InputTag>("preIdsValueMap"))),
   names_(conf.getParameter< std::vector<std::string> >("ModelNames"))
 {
@@ -29,33 +28,32 @@ LowPtGsfElectronSeedValueMapsProducer::~LowPtGsfElectronSeedValueMapsProducer() 
 //
 void LowPtGsfElectronSeedValueMapsProducer::produce( edm::Event& event, const edm::EventSetup& setup ) {
 
-  // Retrieve GsfElectrons from Event
-  edm::Handle<reco::GsfElectronCollection> gsfElectrons;
-  event.getByToken(gsfElectrons_,gsfElectrons);
-  if ( !gsfElectrons.isValid() ) { edm::LogError("Problem with gsfElectrons handle"); }
+  // Retrieve GsfTracks from Event
+  edm::Handle<reco::GsfTrackCollection> gsfTracks;
+  event.getByToken(gsfTracks_,gsfTracks);
+  if ( !gsfTracks.isValid() ) { edm::LogError("Problem with gsfTracks handle"); }
 
   // Retrieve PreIds from Event
   edm::Handle< edm::ValueMap<reco::PreIdRef> > preIdsValueMap;
   event.getByToken(preIdsValueMap_,preIdsValueMap);
   if ( !preIdsValueMap.isValid() ) { edm::LogError("Problem with preIdsValueMap handle"); }
   
-  // Iterate through Electrons, extract BDT output, and store result in ValueMap for each model
+  // Iterate through GsfTracks, extract BDT output, and store result in ValueMap for each model
   std::vector< std::vector<float> > output;
   for ( unsigned int iname = 0; iname < names_.size(); ++iname ) { 
-    output.push_back( std::vector<float>(gsfElectrons->size(),-999.) );
+    output.push_back( std::vector<float>(gsfTracks->size(),-999.) );
   }
-  for ( unsigned int iele = 0; iele < gsfElectrons->size(); iele++ ) {
-    reco::GsfElectronRef ele(gsfElectrons,iele);
-    if ( ele->core().isNonnull() && 
-	 ele->core()->gsfTrack().isNonnull() && 
-	 ele->core()->gsfTrack()->extra().isNonnull() && 
-	 ele->core()->gsfTrack()->extra()->seedRef().isNonnull() ) {
-      reco::ElectronSeedRef seed = ele->core()->gsfTrack()->extra()->seedRef().castTo<reco::ElectronSeedRef>();
+  for ( unsigned int igsf = 0; igsf < gsfTracks->size(); igsf++ ) {
+    reco::GsfTrackRef gsf(gsfTracks,igsf);
+    if ( gsf.isNonnull() && 
+	 gsf->extra().isNonnull() && 
+	 gsf->extra()->seedRef().isNonnull() ) {
+      reco::ElectronSeedRef seed = gsf->extra()->seedRef().castTo<reco::ElectronSeedRef>();
       if ( seed.isNonnull() && seed->ctfTrack().isNonnull() ) {
 	const reco::PreIdRef preid = (*preIdsValueMap)[seed->ctfTrack()];
 	if ( preid.isNonnull() ) {
 	  for ( unsigned int iname = 0; iname < names_.size(); ++iname ) {
-	    output[iname][iele] = preid->mva(iname);
+	    output[iname][igsf] = preid->mva(iname);
 	  }
 	}
       }
@@ -66,7 +64,7 @@ void LowPtGsfElectronSeedValueMapsProducer::produce( edm::Event& event, const ed
   for ( unsigned int iname = 0; iname < names_.size(); ++iname ) {
     auto ptr = std::make_unique< edm::ValueMap<float> >( edm::ValueMap<float>() );
     edm::ValueMap<float>::Filler filler(*ptr);
-    filler.insert(gsfElectrons, output[iname].begin(), output[iname].end());
+    filler.insert(gsfTracks, output[iname].begin(), output[iname].end());
     filler.fill();
     event.put(std::move(ptr),names_[iname]);
   }
@@ -78,9 +76,9 @@ void LowPtGsfElectronSeedValueMapsProducer::produce( edm::Event& event, const ed
 void LowPtGsfElectronSeedValueMapsProducer::fillDescriptions( edm::ConfigurationDescriptions& descriptions )
 {
   edm::ParameterSetDescription desc;
-  desc.add<edm::InputTag>("electrons",edm::InputTag("lowPtGsfElectrons"));
+  desc.add<edm::InputTag>("gsfTracks",edm::InputTag("lowPtGsfEleGsfTracks"));
   desc.add<edm::InputTag>("preIdsValueMap",edm::InputTag("lowPtGsfElectronSeeds"));
-  desc.add< std::vector<std::string> >("ModelNames",std::vector<std::string>({"default"}));
+  desc.add< std::vector<std::string> >("ModelNames",std::vector<std::string>());
   descriptions.add("defaultLowPtGsfElectronSeedValueMaps",desc);
 }
 
