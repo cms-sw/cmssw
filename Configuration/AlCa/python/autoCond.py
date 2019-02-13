a41b1f7e2e5bddfde88fb00465c33d450a15b81d
tocheng <tocheng.cms@gmail.com>
2019-02-13 21:56:35 +0100

    Removing PPS alignment which conflicts with the GT update and use the 2017 MC alignment with weak mode fixed for 2017/Run-3 simualtion

--- Configuration/AlCa/python/autoCond.py
+++ Configuration/AlCa/python/autoCond.py
@@ -24,15 +24,15 @@ autoCond = {
     # GlobalTag for MC production (p-Pb collisions) with realistic alignment and calibrations for Run2
     'run2_mc_pa'        :   '103X_mcRun2_pA_v3',
     # GlobalTag for Run1 data reprocessing
-    'run1_data'         :   '105X_dataRun2_v5',
+    'run1_data'         :   '105X_dataRun2_v6',
     # GlobalTag for Run2 data reprocessing
-    'run2_data'         :   '105X_dataRun2_v5',
+    'run2_data'         :   '105X_dataRun2_v6',
     # GlobalTag for Run2 data relvals: allows customization to run with fixed L1 menu
-    'run2_data_relval'  :   '105X_dataRun2_relval_v4',
+    'run2_data_relval'  :   '105X_dataRun2_relval_v5',
     # GlobalTag for Run2 data 2018B relvals only: HEM-15-16 fail
-    'run2_data_promptlike_HEfail' : '105X_dataRun2_PromptLike_HEfail_v2',
+    'run2_data_promptlike_HEfail' : '105X_dataRun2_PromptLike_HEfail_v3',
     # GlobalTag for Run2 data 2016H relvals only: Prompt Conditions + fixed L1 menu (to be removed)
-    'run2_data_promptlike'    : '105X_dataRun2_PromptLike_v3',
+    'run2_data_promptlike'    : '105X_dataRun2_PromptLike_v4',
     'run2_data_promptlike_hi' : '103X_dataRun2_PromptLike_HI_v3',
     # GlobalTag for Run1 HLT: it points to the online GT
     'run1_hlt'          :   '101X_dataRun2_HLT_frozen_v6',
@@ -46,11 +46,11 @@ autoCond = {
     # GlobalTag for MC production with perfectly aligned and calibrated detector for Phase1 2017 (and 0,0,~0-centred beamspot)
     'phase1_2017_design'       :  '105X_mc2017_design_IdealBS_v3',
     # GlobalTag for MC production with realistic conditions for Phase1 2017 detector
-    'phase1_2017_realistic'    :  '105X_mc2017_realistic_v3',
+    'phase1_2017_realistic'    :  '105X_mc2017_realistic_v4',
     # GlobalTag for MC production (cosmics) with realistic alignment and calibrations for Phase1 2017 detector, Strip tracker in DECO mode
-    'phase1_2017_cosmics'      :  '105X_mc2017cosmics_realistic_deco_v3',
+    'phase1_2017_cosmics'      :  '105X_mc2017cosmics_realistic_deco_v4',
     # GlobalTag for MC production (cosmics) with realistic alignment and calibrations for Phase1 2017 detector, Strip tracker in PEAK mode
-    'phase1_2017_cosmics_peak' :  '105X_mc2017cosmics_realistic_peak_v4',
+    'phase1_2017_cosmics_peak' :  '105X_mc2017cosmics_realistic_peak_v5',
     # GlobalTag for MC production with perfectly aligned and calibrated detector for full Phase1 2018 (and 0,0,0-centred beamspot)
     'phase1_2018_design'       :  '105X_upgrade2018_design_v2',
     # GlobalTag for MC production with realistic conditions for full Phase1 2018 detector
@@ -65,8 +65,8 @@ autoCond = {
     'phase1_2018_cosmics_peak' :   '105X_upgrade2018cosmics_realistic_peak_v3',
     # GlobalTag for MC production with perfectly aligned and calibrated detector for Phase1 2019
     'phase1_2019_design'       : '105X_postLS2_design_v1', # GT containing design conditions for postLS2
-    # GlobalTag for MC production with perfectly aligned and calibrated detector for Phase1 2019
-    'phase1_2019_realistic'    : '105X_postLS2_realistic_v2', # GT containing realistic conditions for postLS2
+    # GlobalTag for MC production with realistic conditions for Phase1 2019
+    'phase1_2019_realistic'    : '105X_postLS2_realistic_v3', # GT containing realistic conditions for postLS2
     # GlobalTag for MC production with realistic conditions for Phase2 2023
     'phase2_realistic'         : '105X_upgrade2023_realistic_v2'
 }
