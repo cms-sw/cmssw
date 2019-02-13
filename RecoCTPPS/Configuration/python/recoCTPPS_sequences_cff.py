e5e62de2ee0fe1da83ed0b54c9f417c41957acdb
Jan Kaspar <jan.kaspar@cern.ch>
2019-02-13 10:06:12 +0100

    Eliminate dependence of RecoCTPPS on Validation.

--- RecoCTPPS/Configuration/python/recoCTPPS_sequences_cff.py
+++ RecoCTPPS/Configuration/python/recoCTPPS_sequences_cff.py
@@ -24,10 +24,30 @@ PoolDBESSource = cms.ESSource("PoolDBESSource",
 )
 
 # TODO: load these data from DB
-from Validation.CTPPS.year_2016.ctppsLHCInfoESSource_cfi import *
+# NB: this example only works for 2016 data
+ctppsLHCInfoESSource = cms.ESSource("CTPPSLHCInfoESSource",
+  validityRange = cms.EventRange("270293:min - 290872:max"),
+  beamEnergy = cms.double(6500),  # GeV
+  xangle = cms.double(185)  # murad
+)
 
 # TODO: load these data from DB
-from Validation.CTPPS.year_2016.ctppsOpticalFunctionsESSource_cfi import *
+# NB: this example only works for 2016 data
+import FWCore.ParameterSet.Config as cms
+from CalibPPS.ESProducers.ctppsOpticalFunctionsESSource_cfi import ctppsOpticalFunctionsESSource as _optics_tmp
+ctppsOpticalFunctionsESSource = _optics_tmp.clone(
+    xangle1 = cms.double(185),
+    xangle2 = cms.double(185),
+    fileName1 = cms.FileInPath("RecoCTPPS/TotemRPLocal/data/optical_functions_2016.root"),
+    fileName2 = cms.FileInPath("RecoCTPPS/TotemRPLocal/data/optical_functions_2016.root"),
+    scoringPlanes = cms.VPSet(
+        # z in cm
+        cms.PSet( rpId = cms.uint32(0x76100000), dirName = cms.string("XRPH_C6L5_B2"), z = cms.double(-20382.6) ),  # RP 002
+        cms.PSet( rpId = cms.uint32(0x76180000), dirName = cms.string("XRPH_D6L5_B2"), z = cms.double(-21255.1) ),  # RP 003
+        cms.PSet( rpId = cms.uint32(0x77100000), dirName = cms.string("XRPH_C6R5_B1"), z = cms.double(+20382.6) ),  # RP 102
+        cms.PSet( rpId = cms.uint32(0x77180000), dirName = cms.string("XRPH_D6R5_B1"), z = cms.double(+21255.1) ),  # RP 103
+    )
+)
 
 recoCTPPSdets = cms.Sequence(
     totemRPLocalReconstruction *
