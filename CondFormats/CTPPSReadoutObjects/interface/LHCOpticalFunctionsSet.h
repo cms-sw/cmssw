d7bb687ca3131030edd6180bd152241cbe2bec68
Jan Kaspar <jan.kaspar@cern.ch>
2019-02-13 15:55:27 +0100

    Updated CondFormats related to optical functions.

--- CondFormats/CTPPSReadoutObjects/interface/LHCOpticalFunctionsSet.h
+++ CondFormats/CTPPSReadoutObjects/interface/LHCOpticalFunctionsSet.h
@@ -1,7 +1,7 @@
+// Original Author:  Jan Kašpar
+
 #ifndef CondFormats_CTPPSReadoutObjects_LHCOpticalFunctionsSet_h
 #define CondFormats_CTPPSReadoutObjects_LHCOpticalFunctionsSet_h
-
-// Original Author:  Jan Kašpar
 
 #include "CondFormats/Serialization/interface/Serializable.h"
 
@@ -9,54 +9,32 @@
 #include <string>
 #include <memory>
 
-#include "TSpline.h"
-
 /// Set of optical functions corresponding to one scoring plane along LHC.
 class LHCOpticalFunctionsSet
 {
   public:
     /// indices for m_fcn_values and m_splines data members
-    enum { evx, eLx, e14, exd, evpx, eLpx, e24, expd, e32, evy, eLy, eyd, e42, evpy, eLpy, eypd };
+    enum { evx, eLx, e14, exd, evpx, eLpx, e24, expd, e32, evy, eLy, eyd, e42, evpy, eLpy, eypd, nFunctions };
 
     LHCOpticalFunctionsSet() = default;
+
     /// fills m_*_values fields from a ROOT file
     LHCOpticalFunctionsSet(const std::string &fileName, const std::string &directoryName, double z);
+
     ~LHCOpticalFunctionsSet() = default;
 
     /// returns the position of the scoring plane (LHC/TOTEM convention)
     double getScoringPlaneZ() const { return m_z; }
 
     const std::vector<double>& getXiValues() const { return m_xi_values; }
-    const std::array<std::vector<double>, 16>& getFcnValues() const { return m_fcn_values; }
-    const std::array<std::shared_ptr<const TSpline3>, 16>& getSplines() const { return m_splines; }
+    const std::vector<std::vector<double>>& getFcnValues() const { return m_fcn_values; }
 
-    /// builds splines from m_*_values fields
-    void initializeSplines();
-
-    /// proton kinematics description
-    struct Kinematics
-    {
-      double x;     // physics vertex position (beam offset subtracted), cm
-      double th_x;  // physics scattering angle (crossing angle subtracted), rad
-      double y;     // physics vertex position, cm
-      double th_y;  // physics scattering angle, rad
-      double xi;    // relative momentum loss (positive for diffractive protons)
-    };
-
-    /// transports proton according to the splines
-    void transport(const Kinematics &input, Kinematics &output, bool calculateAngles = false) const;
-
-    /// interpolates optical functions from (xangle1, of1) and (xangle2, of2) to xangle
-    static LHCOpticalFunctionsSet* interpolate(double xangle1, const LHCOpticalFunctionsSet &of1,
-      double xangle2, const LHCOpticalFunctionsSet &of2, double xangle);
-
-  private:
+  protected:
     /// position of the scoring plane, in LHC/TOTEM convention, cm
     double m_z;
 
     std::vector<double> m_xi_values;
-    std::array<std::vector<double>, 16> m_fcn_values; ///< length unit cm
-    std::array<std::shared_ptr<const TSpline3>, 16> m_splines;
+    std::vector<std::vector<double>> m_fcn_values; ///< length unit cm
 
     COND_SERIALIZABLE;
 };
