3bd5cbc0308b67df180b59c7ac8b884491741da0
Srecko Morovic <srecko.morovic@cern.ch>
2019-02-13 14:01:49 +0100

    ForOutput LB and Run can now be used in EvFOutputModule

--- EventFilter/Utilities/interface/EvFOutputModule.h
+++ EventFilter/Utilities/interface/EvFOutputModule.h
@@ -24,15 +24,16 @@ namespace evf {
     static void fillDescriptions(edm::ConfigurationDescriptions& descriptions);
     
   private:
+    void write(edm::EventForOutput const& e) override;
+
     //pure in parent class but unused here
     void writeLuminosityBlock(edm::LuminosityBlockForOutput const&) override {}
-    void globalEndRun(edm::Run const&, edm::EventSetup const&) const override {}
     void writeRun(edm::RunForOutput const&) override {}
+    void globalEndRun(edm::RunForOutput const&) const override {}
 
-    std::shared_ptr<EvFOutputJSONWriter> globalBeginRun(edm::Run const& run, edm::EventSetup const& setup) const override;
-    std::shared_ptr<EvFOutputEventWriter> globalBeginLuminosityBlock(edm::LuminosityBlock const& iLB, edm::EventSetup const&) const override;
-    void write(edm::EventForOutput const& e) override;
-    void globalEndLuminosityBlock(edm::LuminosityBlock const& iLB, edm::EventSetup const&) const override;
+    std::shared_ptr<EvFOutputJSONWriter> globalBeginRun(edm::RunForOutput const& run) const override;
+    std::shared_ptr<EvFOutputEventWriter> globalBeginLuminosityBlock(edm::LuminosityBlockForOutput const& iLB) const override;
+    void globalEndLuminosityBlock(edm::LuminosityBlockForOutput const& iLB) const override;
 
     Trig getTriggerResults(edm::EDGetTokenT<edm::TriggerResults> const& token, edm::EventForOutput const& e) const;
 
