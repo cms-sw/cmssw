93f0b0f2190034f560bd5f3ace3dad1494e1bfb9
Jean-Baptiste Sauvan <jean-baptiste.sauvan@cern.ch>
2019-02-19 12:17:01 +0100

    Clean V9 test config files

--- L1Trigger/L1THGCalUtilities/test/testHGCalL1T_RelValV9_cfg.py
+++ L1Trigger/L1THGCalUtilities/test/testHGCalL1T_RelValV9_cfg.py
@@ -70,11 +70,8 @@ process.GlobalTag = GlobalTag(process.GlobalTag, 'auto:phase2_realistic', '')
 process.load('L1Trigger.L1THGCal.hgcalTriggerPrimitives_cff')
 process.hgcl1tpg_step = cms.Path(process.hgcalTriggerPrimitives)
 # To test V9Imp2
-from L1Trigger.L1THGCal.customTriggerGeometry import custom_geometry_V9
-process = custom_geometry_V9(process, implementation=2)
-from L1Trigger.L1THGCal.customClustering import custom_2dclustering_dummy, custom_3dclustering_histoMax
-process = custom_2dclustering_dummy(process)
-process = custom_3dclustering_histoMax(process)
+#from L1Trigger.L1THGCal.customTriggerGeometry import custom_geometry_V9
+#process = custom_geometry_V9(process, implementation=2)
 
 
 # load ntuplizer
