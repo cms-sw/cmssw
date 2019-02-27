90346d40aef4ed101811b9070bea9e17e5d283f0
Christopher Jones <chrisdjones15@gmail.com>
2019-02-27 07:44:56 -0600

    Fix failing tests in FWCore/ParameterSet
    Fixed tests that had been in the works during the initial transition
    to using pythone 3 syntax.

--- FWCore/ParameterSet/python/Types.py
+++ FWCore/ParameterSet/python/Types.py
@@ -1,6 +1,7 @@
 from __future__ import absolute_import
 from .Mixins import PrintOptions, _SimpleParameterTypeBase, _ParameterTypeBase, _Parameterizable, _ConfigureComponent, _Labelable, _TypedParameterizable, _Unlabelable, _modifyParametersFromDict
 from .Mixins import _ValidatingParameterListBase, specialImportRegistry
+from .Mixins import saveOrigin
 from .ExceptionHandling import format_typename, format_outerframe
 
 
