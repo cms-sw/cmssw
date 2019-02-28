18df6a1f489a9e97d445197da49b8173365f4dbd
Marina Toumazou <mtoumazo@cern.ch>
2019-02-28 12:04:05 +0100

    [Phase2L1Taus]: Added HGCal EGs input tag in the configuaration file of L1TkEGTauParticleProducer (not used yet).

--- L1Trigger/Phase2L1Taus/python/L1TkEGTauParticleProducer_cfi.py
+++ L1Trigger/Phase2L1Taus/python/L1TkEGTauParticleProducer_cfi.py
@@ -14,10 +14,11 @@ L1TkEGTaus = cms.EDProducer("L1TkEGTauParticleProducer",
         tk_minStubs     = cms.uint32( 5 ),
                             
         # L1 EGammas
-        L1EGammaInputTag = cms.InputTag("L1EGammaClusterEmuProducer", "L1EGammaCollectionBXVEmulator"), 
-        eg_minEt         = cms.double( 1.5 ),
-        eg_minEta        = cms.double( 0.0 ),
-        eg_maxEta        = cms.double( 1.5 ),
+        L1EGammaInputTag      = cms.InputTag("L1EGammaClusterEmuProducer", "L1EGammaCollectionBXVEmulator"), 
+        #L1EGammaHGCalInputTag = cms.InputTag("l1EGammaEEProducer","L1EGammaCollectionBXVWithCuts"),
+        eg_minEt              = cms.double( 1.5 ),
+        eg_minEta             = cms.double( 0.0 ),
+        eg_maxEta             = cms.double( 1.5 ),
 
         # Seed-tracks parameters
         seedtk_minPt     = cms.double( 5.0 ),
