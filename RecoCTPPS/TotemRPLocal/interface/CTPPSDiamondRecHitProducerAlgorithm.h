b25f63252605c126b6aa60467e50db3e3346889b
Laurent Forthomme <laurent.forthomme@cern.ch>
2019-03-04 11:38:02 +0100

    First matches to the algorithm, calibration retrieval from db in test

--- RecoCTPPS/TotemRPLocal/interface/CTPPSDiamondRecHitProducerAlgorithm.h
+++ RecoCTPPS/TotemRPLocal/interface/CTPPSDiamondRecHitProducerAlgorithm.h
@@ -10,6 +10,7 @@
 #define RecoCTPPS_TotemRPLocal_CTPPSDiamondRecHitProducerAlgorithm
 
 #include "FWCore/ParameterSet/interface/ParameterSet.h"
+#include "CommonTools/Utils/interface/FormulaEvaluator.h"
 
 #include "DataFormats/Common/interface/DetSetVector.h"
 
@@ -31,9 +32,12 @@ class CTPPSDiamondRecHitProducerAlgorithm
     void build( const CTPPSGeometry&, const edm::DetSetVector<CTPPSDiamondDigi>&, edm::DetSetVector<CTPPSDiamondRecHit>& );
 
   private:
+    static constexpr unsigned short MAX_CHANNEL = 20;
     /// Conversion constant between HPTDC time slice and absolute time (in ns)
     double ts_to_ns_;
     PPSTimingCalibration calib_;
+    std::unique_ptr<reco::FormulaEvaluator> calib_fct_;
 };
 
 #endif
+
