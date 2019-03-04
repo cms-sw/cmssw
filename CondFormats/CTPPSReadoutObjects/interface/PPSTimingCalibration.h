ba96f7afa95bdda3f3901cec86dd5ba4dbab648b
Laurent Forthomme <laurent.forthomme@cern.ch>
2019-03-04 10:16:05 +0100

    Reverted back key naming thanks to hidden boost backward compatibility feature

--- CondFormats/CTPPSReadoutObjects/interface/PPSTimingCalibration.h
+++ CondFormats/CTPPSReadoutObjects/interface/PPSTimingCalibration.h
@@ -21,7 +21,7 @@ class PPSTimingCalibration
     /// Helper structure for indexing calibration data
     struct Key
     {
-      int key1, key2, key3, key4;
+      int db, sampic, channel, cell;
 
       /// Comparison operator
       bool operator<( const Key& rhs ) const;
