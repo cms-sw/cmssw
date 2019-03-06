5f33c74fa916874701e3a65ae0acbfe9dc1d5f84
Giovanni <gpetruc@gmail.com>
2019-03-06 17:43:21 +0100

    Move old PFlow config to l1ParticleFlow_old_cff

--- L1Trigger/Phase2L1ParticleFlow/python/l1ParticleFlow_old_cff.py
+++ L1Trigger/Phase2L1ParticleFlow/python/l1ParticleFlow_old_cff.py
@@ -6,10 +6,12 @@ from L1Trigger.Phase2L1ParticleFlow.pfTracksFromL1Tracks_cfi import *
 from L1Trigger.Phase2L1ParticleFlow.pfClustersFromHGC3DClustersEM_cfi import *
 from L1Trigger.Phase2L1ParticleFlow.pfClustersFromL1EGClusters_cfi import *
 from L1Trigger.Phase2L1ParticleFlow.pfClustersFromCombinedCalo_cfi import *
-from L1Trigger.Phase2L1ParticleFlow.l1pfProducer_cfi import *
+import L1Trigger.Phase2L1ParticleFlow.l1pfProducer_cfi 
 
-l1PuppiForMET = cms.EDFilter("L1TPFCandSelector", 
-    src = cms.InputTag("l1pfProducer:Puppi"), 
+l1pfProducerOld = L1Trigger.Phase2L1ParticleFlow.l1pfProducer_cfi.l1pfProducer.clone()
+
+l1OldPuppiForMET = cms.EDFilter("L1TPFCandSelector", 
+    src = cms.InputTag("l1pfProducerOld:Puppi"), 
     cut = cms.string("charge != 0 ||"+
                      "abs(eta) < 1.5 ||"+
                      "(pt > 20 && abs(eta) < 2.5) ||"+
@@ -17,12 +19,12 @@ l1PuppiForMET = cms.EDFilter("L1TPFCandSelector",
                      "(pt > 30 && abs(eta) > 3.0)")
 )
 
-l1ParticleFlow = cms.Sequence(
+l1ParticleFlowOld = cms.Sequence(
     l1EGammaCrystalsProducer + 
     pfTracksFromL1Tracks +
     pfClustersFromHGC3DClustersEM +
     pfClustersFromL1EGClusters +
     pfClustersFromCombinedCalo +
-    l1pfProducer +
-    l1PuppiForMET
+    l1pfProducerOld +
+    l1OldPuppiForMET
 )
