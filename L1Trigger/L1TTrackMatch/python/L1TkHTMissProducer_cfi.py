62f4de702ca3a1d2d358b86cb3d9d22be6a5e2fc
rpatel <rpatel@cern.ch>
2019-03-06 08:10:36 -0700

    Fix for L1TkHTMissProducer_cfi

--- L1Trigger/L1TTrackMatch/python/L1TkHTMissProducer_cfi.py
+++ L1Trigger/L1TTrackMatch/python/L1TkHTMissProducer_cfi.py
@@ -5,22 +5,23 @@ L1TkCaloHTMiss = cms.EDProducer("L1TkHTMissProducer",
      L1VertexInputTag = cms.InputTag("L1TkPrimaryVertex"),
      jet_maxEta = cms.double(2.2),          # maximum eta of jets for HT
      jet_minPt = cms.double(15.0),          # minimum pt of jets for HT [GeV]
-     jet_minNtracksHighPt=cms.int(0),       #Add track jet quality criteria pT>100
-     jet_minNtracksLowPt=cms.int(0),        #Add track jet quality criteria pT>50
-     DoVtxConstrain = cms.bool(False),      # turn on/off applying any vertex constraint
+     jet_minNtracksHighPt=cms.int32(0),       #Add track jet quality criteria pT>100
+     jet_minNtracksLowPt=cms.int32(0),        #Add track jet quality criteria pT>50
+     DoVtxConstrain = cms.bool(False),      # turn on/off applying any vertex constraint32
      DeltaZ = cms.double(1.0),              # require jets to have |z_jet - z_ref| below DeltaZ [cm]
      PrimaryVtxConstrain = cms.bool(False), # use primary vertex instead of leading jet as reference z position
      UseCaloJets = cms.bool(True)        # determines whether matched jets or standalone jets are used for MHT
 
 )
 
-L1TkCaloHTMissVtx = L1TkCaloHTMiss.clone()
-L1TrackerHTMiss = L1TkCaloHTMiss.clone()
-L1TrackerHTMiss.L1TkJetInputTag = cms.InputTag("L1TrackerJets","L1TrackerJets")
-L1TrackerHTMiss.jet_maxEta = cms.double(2.4)
-L1TrackerHTMiss.jet_minPt = cms.double(5.0)
-L1TrackerHTMiss.jet_minNtracksLowPt=cms.int(2)
-L1TrackerHTMiss.jet_minNtracksHighPt=cms.int(3)
-L1TrackerHTMiss.UseCaloJets = cms.bool(False)
-L1TrackerHTMiss.DoVtxConstrain = cms.bool(False)      # turn on/off applying any vertex constraint
-L1TrackerHTMiss.PrimaryVtxConstrain = cms.bool(False) # primary vertex already applied to track jet collections
+L1TrackerHTMiss = cms.EDProducer("L1TkHTMissProducer",
+	L1TkJetInputTag = cms.InputTag("L1TrackerJets","L1TrackerJets"),
+	L1VertexInputTag = cms.InputTag("VertexProducer","l1vertextdr"),
+	jet_maxEta = cms.double(2.4),
+	jet_minPt = cms.double(5.0),
+	jet_minNtracksLowPt=cms.int32(2),
+	jet_minNtracksHighPt=cms.int32(3), 
+        UseCaloJets = cms.bool(False),
+        DoVtxConstrain = cms.bool(False),      # turn on/off applying any vertex constraint32
+        PrimaryVtxConstrain = cms.bool(False) # primary vertex already applied to track jet collections
+)
