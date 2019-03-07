3729bb96160f644481cc4b2bccea78017ac60149
Matti Kortelainen <matti.kortelainen@cern.ch>
2019-03-07 11:48:25 -0800

    Add a flag to disable strip rechit matching

--- RecoLocalTracker/SiStripRecHitConverter/plugins/SiStripRecHitConverter.cc
+++ RecoLocalTracker/SiStripRecHitConverter/plugins/SiStripRecHitConverter.cc
@@ -5,15 +5,18 @@ SiStripRecHitConverter::SiStripRecHitConverter(edm::ParameterSet const& conf)
     : recHitConverterAlgorithm(conf),
       matchedRecHitsTag(conf.getParameter<std::string>("matchedRecHits")),
       rphiRecHitsTag(conf.getParameter<std::string>("rphiRecHits")),
-      stereoRecHitsTag(conf.getParameter<std::string>("stereoRecHits")) {
+      stereoRecHitsTag(conf.getParameter<std::string>("stereoRecHits")),
+      doMatching(conf.getParameter<bool>("doMatching")) {
   clusterProducer =
       consumes<edmNew::DetSetVector<SiStripCluster> >(conf.getParameter<edm::InputTag>("ClusterProducer"));
 
-  produces<SiStripMatchedRecHit2DCollection>(matchedRecHitsTag);
   produces<SiStripRecHit2DCollection>(rphiRecHitsTag);
   produces<SiStripRecHit2DCollection>(stereoRecHitsTag);
-  produces<SiStripRecHit2DCollection>(rphiRecHitsTag + "Unmatched");
-  produces<SiStripRecHit2DCollection>(stereoRecHitsTag + "Unmatched");
+  if (doMatching) {
+    produces<SiStripMatchedRecHit2DCollection>(matchedRecHitsTag);
+    produces<SiStripRecHit2DCollection>(rphiRecHitsTag + "Unmatched");
+    produces<SiStripRecHit2DCollection>(stereoRecHitsTag + "Unmatched");
+  }
 }
 
 void SiStripRecHitConverter::produce(edm::Event& e, const edm::EventSetup& es) {
@@ -28,9 +31,11 @@ void SiStripRecHitConverter::produce(edm::Event& e, const edm::EventSetup& es) {
                                      << output.rphi->dataSize() << "  clusters in mono detectors\n"
                                      << output.stereo->dataSize() << "  clusters in partners stereo detectors\n";
 
-  e.put(std::move(output.matched), matchedRecHitsTag);
   e.put(std::move(output.rphi), rphiRecHitsTag);
   e.put(std::move(output.stereo), stereoRecHitsTag);
-  e.put(std::move(output.rphiUnmatched), rphiRecHitsTag + "Unmatched");
-  e.put(std::move(output.stereoUnmatched), stereoRecHitsTag + "Unmatched");
+  if (doMatching) {
+    e.put(std::move(output.matched), matchedRecHitsTag);
+    e.put(std::move(output.rphiUnmatched), rphiRecHitsTag + "Unmatched");
+    e.put(std::move(output.stereoUnmatched), stereoRecHitsTag + "Unmatched");
+  }
 }
