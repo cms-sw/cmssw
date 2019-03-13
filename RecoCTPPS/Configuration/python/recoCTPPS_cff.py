0706711cfe2e1a014a033cfb6c0bf82cfdd36769
Jan Kaspar <jan.kaspar@cern.ch>
2019-03-13 12:45:22 +0100

    LHCInfo and optics source specs moved from main reco config to dedicated cff files.

--- RecoCTPPS/Configuration/python/recoCTPPS_cff.py
+++ RecoCTPPS/Configuration/python/recoCTPPS_cff.py
@@ -10,38 +10,13 @@ from RecoCTPPS.ProtonReconstruction.ctppsProtons_cfi import *
 
 from Geometry.VeryForwardGeometry.geometryRPFromDB_cfi import *
 
-# TODO: load these data from DB
-# NB: this example only works for 2016 data
-ctppsLHCInfoESSource = cms.ESSource("CTPPSLHCInfoESSource",
-  validityRange = cms.EventRange("270293:min - 290872:max"),
-  beamEnergy = cms.double(6500),  # GeV
-  xangle = cms.double(185)  # murad
-)
+# TODO: remove these lines once LHCInfo available in DB
+from CalibPPS.ESProducers.ctppsLHCInfo_cff import *
+ctppsProtons.lhcInfoLabel = ctppsLHCInfoESSource.label
 
-# TODO: remove this line once LHCInfo is loaded from DB
-es_prefer_LHCInfo = cms.ESPrefer("CTPPSLHCInfoESSource", "ctppsLHCInfoESSource")
-
-# TODO: load these data from DB
-# NB: this example only works for 2016 data
-import FWCore.ParameterSet.Config as cms
-from CalibPPS.ESProducers.ctppsOpticalFunctionsESSource_cfi import ctppsOpticalFunctionsESSource as _optics_tmp
-ctppsOpticalFunctionsESSource = _optics_tmp.clone(
-  validityRange = cms.EventRange("270293:min - 290872:max"),
-
-  opticalFunctions = cms.VPSet(
-      cms.PSet( xangle = cms.double(185), fileName = cms.FileInPath("CalibPPS/ESProducers/data/optical_functions_2016.root") )
-  ),
-
-  scoringPlanes = cms.VPSet(
-      # z in cm
-      cms.PSet( rpId = cms.uint32(0x76100000), dirName = cms.string("XRPH_C6L5_B2"), z = cms.double(-20382.6) ),  # RP 002
-      cms.PSet( rpId = cms.uint32(0x76180000), dirName = cms.string("XRPH_D6L5_B2"), z = cms.double(-21255.1) ),  # RP 003
-      cms.PSet( rpId = cms.uint32(0x77100000), dirName = cms.string("XRPH_C6R5_B1"), z = cms.double(+20382.6) ),  # RP 102
-      cms.PSet( rpId = cms.uint32(0x77180000), dirName = cms.string("XRPH_D6R5_B1"), z = cms.double(+21255.1) ),  # RP 103
-  )
-)
-
-ctppsInterpolatedOpticalFunctionsESSource = cms.ESProducer("CTPPSInterpolatedOpticalFunctionsESSource")
+# TODO: remove these lines once optical functions available in DB
+from CalibPPS.ESProducers.ctppsOpticalFunctions_cff import *
+ctppsInterpolatedOpticalFunctionsESSource.lhcInfoLabel = ctppsLHCInfoESSource.label
 
 recoCTPPS = cms.Sequence(
     totemRPLocalReconstruction *
