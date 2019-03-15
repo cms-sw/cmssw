7ecf9a0c529b342a5db9e5a605129aeb08359f31
Igor <i.volobouev@ttu.edu>
2019-03-15 08:07:09 +0100

    Framework for implementing HFStripFilter in CMSSW

---
+++ RecoLocalCalo/HcalRecAlgos/interface/HFStripFilter.h
@@ -0,0 +1,45 @@
+#ifndef RecoLocalCalo_HcalRecAlgos_HFStripFilter_h_
+#define RecoLocalCalo_HcalRecAlgos_HFStripFilter_h_
+
+#include <memory>
+
+#include "DataFormats/HcalRecHit/interface/HcalRecHitCollections.h"
+#include "FWCore/ParameterSet/interface/ParameterSetDescription.h"
+
+class HFStripFilter
+{
+public:
+    // Construct this object with all necessary parameters
+    HFStripFilter(double energyMax1, double energyMax2,
+                  double stripThreshold, double maxThreshold,
+                  double timeMax, double maxStripTime,
+                  double wedgeCut, int gap,
+                  int lstrips, int verboseLevel);
+
+    // Destructor
+    ~HFStripFilter();
+
+    // The actual rechit tagging is performed by the following function
+    void runFilter(HFRecHitCollection& rec) const;
+
+    // Parser function to create this object from a parameter set
+    static std::unique_ptr<HFStripFilter> parseParameterSet(
+        const edm::ParameterSet& ps);
+
+    // Standard parameter values
+    static edm::ParameterSetDescription fillDescription();
+
+private:
+    double energyMax1_;
+    double energyMax2_;
+    double stripThreshold_;
+    double maxThreshold_;
+    double timeMax_;
+    double maxStripTime_;
+    double wedgeCut_;
+    int gap_;
+    int lstrips_;
+    int verboseLevel_;
+};
+
+#endif // RecoLocalCalo_HcalRecAlgos_HFStripFilter_h_
---
+++ RecoLocalCalo/HcalRecAlgos/src/HFStripFilter.cc
@@ -0,0 +1,85 @@
+#include "FWCore/ParameterSet/interface/ParameterSet.h"
+#include "FWCore/MessageLogger/interface/MessageLogger.h"
+
+#include "DataFormats/METReco/interface/HcalPhase1FlagLabels.h"
+
+#include "RecoLocalCalo/HcalRecAlgos/interface/HFStripFilter.h"
+
+
+HFStripFilter::HFStripFilter(const double energyMax1, const double energyMax2,
+                             const double stripThreshold, const double maxThreshold,
+                             const double timeMax, const double maxStripTime,
+                             const double wedgeCut, const int gap,
+                             const int lstrips, const int verboseLevel)
+    : energyMax1_(energyMax1),
+      energyMax2_(energyMax2),
+      stripThreshold_(stripThreshold),
+      maxThreshold_(maxThreshold),
+      timeMax_(timeMax),
+      maxStripTime_(maxStripTime),
+      wedgeCut_(wedgeCut),
+      gap_(gap),
+      lstrips_(lstrips),
+      verboseLevel_(verboseLevel)
+{
+    // For the description of CMSSW message logging, see
+    // https://twiki.cern.ch/twiki/bin/view/CMSPublic/SWGuideMessageLogger
+    if (verboseLevel_ >= 20)
+        edm::LogInfo("HFStripFilter") << "constructor called";
+}
+
+HFStripFilter::~HFStripFilter()
+{
+    if (verboseLevel_ >= 20)
+        edm::LogInfo("HFStripFilter") << "destructor called";
+}
+
+void HFStripFilter::runFilter(HFRecHitCollection& rec) const
+{
+    if (verboseLevel_ >= 20)
+        edm::LogInfo("HFStripFilter") << "runFilter called";
+
+    // Cycle over the rechit collection
+    for (HFRecHitCollection::iterator it = rec.begin(); it != rec.end(); ++it)
+    {
+        // Figure out which rechits need to be tagged
+
+        // To tag a rechit with the anomalous hit flag, do the following
+        it->setFlagField(1U, HcalPhase1FlagLabels::HFAnomalousHit);
+    }
+}
+
+std::unique_ptr<HFStripFilter> HFStripFilter::parseParameterSet(
+    const edm::ParameterSet& ps)
+{
+    return std::make_unique<HFStripFilter>(
+        ps.getParameter<double>("energyMax1"),
+        ps.getParameter<double>("energyMax2"),
+        ps.getParameter<double>("stripThreshold"),
+        ps.getParameter<double>("maxThreshold"),
+        ps.getParameter<double>("timeMax"),
+        ps.getParameter<double>("maxStripTime"),
+        ps.getParameter<double>("wedgeCut"),
+        ps.getParameter<int>("gap"),
+        ps.getParameter<int>("lstrips"),
+        ps.getParameter<int>("verboseLevel")
+    );
+}
+
+edm::ParameterSetDescription HFStripFilter::fillDescription()
+{
+    edm::ParameterSetDescription desc;
+
+    desc.add<double>("energyMax1", -10.0);
+    desc.add<double>("energyMax2", -10.0);
+    desc.add<double>("stripThreshold", 40.0);
+    desc.add<double>("maxThreshold", 100.0);
+    desc.add<double>("timeMax", 6.0);
+    desc.add<double>("maxStripTime", 10.0);
+    desc.add<double>("wedgeCut", 0.05);
+    desc.add<int>("gap", 2);
+    desc.add<int>("lstrips", 2);
+    desc.add<int>("verboseLevel", 0);
+
+    return desc;
+}
