4bed8d41ae89cad3ec1638252f6ce7b1d7f8b69b
Jan Kaspar <jan.kaspar@cern.ch>
2019-03-17 13:03:27 +0100

    classes.h contains headers only.

--- DataFormats/ProtonReco/src/classes.h
+++ DataFormats/ProtonReco/src/classes.h
@@ -7,10 +7,3 @@
 
 #include <vector>
 #include <set>
-
-reco::ForwardProton fp;
-std::vector<reco::ForwardProton> vec_fp;
-edm::Wrapper<std::vector<reco::ForwardProton> > wrp_vec_fp;
-edm::RefProd<std::vector<reco::ForwardProton> > rp_vec_fp;
-edm::Ref<std::vector<reco::ForwardProton>,reco::ForwardProton,edm::refhelper::FindUsingAdvance<std::vector<reco::ForwardProton>,reco::ForwardProton> > ref_vec_fp;
-edm::RefVector<std::vector<reco::ForwardProton>,reco::ForwardProton,edm::refhelper::FindUsingAdvance<std::vector<reco::ForwardProton>,reco::ForwardProton> > rv_vec_fp;
