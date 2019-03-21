29f6c857a228901259d309ed905b8b106d2e06a9
Marcel Andre Schneider <marcel.andre.schneider@cern.ch>
2019-03-21 17:56:19 +0100

    Force per-lumi saving.

--- DQMServices/Core/interface/MonitorElement.h
+++ DQMServices/Core/interface/MonitorElement.h
@@ -144,7 +144,7 @@ public:
 
   /// true if ME is meant to be stored for each luminosity section
   bool getLumiFlag() const
-    { return data_.flags & DQMNet::DQM_PROP_LUMI; }
+    { return true; }
 
   /// this ME is meant to be stored for each luminosity section
   void setLumiFlag()
