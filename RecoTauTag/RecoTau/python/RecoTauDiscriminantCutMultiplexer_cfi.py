35df801da8170b0c3f38e4949bbf6e7ccb31090e
Andrea <perrotta@cern.ch>
2019-03-25 18:31:44 +0100

    Fix TauDiscriminatorProducer

--- RecoTauTag/RecoTau/python/RecoTauDiscriminantCutMultiplexer_cfi.py
+++ RecoTauTag/RecoTau/python/RecoTauDiscriminantCutMultiplexer_cfi.py
@@ -9,22 +9,11 @@ MVA output depending on what the category is.
 '''
 
 import FWCore.ParameterSet.Config as cms
+from RecoTauTag.RecoTau.recoTauDiscriminantCutMultiplexerDefault_cfi import recoTauDiscriminantCutMultiplexerDefault
 
-recoTauDiscriminantCutMultiplexer = cms.EDProducer(
-    "RecoTauDiscriminantCutMultiplexer",
+recoTauDiscriminantCutMultiplexer = recoTauDiscriminantCutMultiplexerDefault.clone(
     PFTauProducer = cms.InputTag("fixme"),
     toMultiplex = cms.InputTag("fixme"),
-    Prediscriminants = cms.PSet(
-        BooleanOperator = cms.string("and"),
-        decayMode = cms.PSet(
-            Producer = cms.InputTag("fixme"),
-            cut = cms.double(0.)
-            )
-        ),
-    key = cms.InputTag("fixme"), # a discriminator
-    loadMVAfromDB = cms.bool(True),
-    inputFileName = cms.FileInPath("RecoTauTag/RecoTau/data/emptyMVAinputFile"), # the filename for MVA if it is not loaded from DB
-    mvaOutput_normalization = cms.string(""), # empty basicaly does nothing: it rewrites the attribute which is initialized as empty string
     mapping = cms.VPSet(
         cms.PSet(
             category = cms.uint32(0),
@@ -34,6 +23,5 @@ recoTauDiscriminantCutMultiplexer = cms.EDProducer(
             category = cms.uint32(1),
             cut = cms.double(0.2),
         ),
-    ),
-    verbosity = cms.int32(0)
+    )
 )
