6876e75d41e7f5e47fc4bb8dc865b95ae811f585
Jason Fan <jfan20@jhu.edu>
2019-03-27 19:08:26 +0100

    mean added on plots, TEC TID plot range  changed

--- Alignment/OfflineValidation/python/overlapValidationPlot.py
+++ Alignment/OfflineValidation/python/overlapValidationPlot.py
@@ -5,11 +5,11 @@ from TkAlStyle import TkAlStyle
 def hist(tree_file_name, hist_name,subdet_id,module_direction,overlap_direction):
     f = ROOT.TFile(tree_file_name)
     t = f.Get("analysis/Overlaps")
-
-    h = ROOT.TH1F(hist_name, hist_name, 100, -300, 300)
-
+    if (subdet_id==4 or subdet_id==6):
+	    h = ROOT.TH1F(hist_name, hist_name, 100, -5000, 5000)
+    else:
+    	h = ROOT.TH1F(hist_name, hist_name, 100, -300, 300)
     h.SetDirectory(0)
-	
     for entry in t:
         if not ((t.subdetID == subdet_id)):
             continue
@@ -19,12 +19,12 @@ def hist(tree_file_name, hist_name,subdet_id,module_direction,overlap_direction)
 	    continue 
         modulePhi0 = math.atan2(t.moduleY[0], t.moduleX[0]) 
         modulePhi1 = math.atan2(t.moduleY[1], t.moduleX[1])
-	phidiff = min(abs(modulePhi0-modulePhi1), math.pi - abs(modulePhi0-modulePhi1))
+	phidiff = min(abs(modulePhi0-modulePhi1), abs(math.pi - abs(modulePhi0-modulePhi1)))
 	moduleR0 = math.sqrt(t.moduleY[0]**2+t.moduleX[0]**2)
 	moduleR1 = math.sqrt(t.moduleY[1]**2+t.moduleX[1]**2)
 	if module_direction !="r" and subdet_id%2 == 0 and (abs(moduleR0 - moduleR1)<1):
             continue	
-	if module_direction !="phi" and ((moduleR0*phidiff)<2):
+	if module_direction !="phi" and ((moduleR0*phidiff)<1):
 	    continue
 	if overlap_direction == "phi":
              if modulePhi0 > modulePhi1:
@@ -85,7 +85,7 @@ def hist(tree_file_name, hist_name,subdet_id,module_direction,overlap_direction)
 
 def plot(file_name,subdet_id,module_direction,overlap_direction, *filesTitlesColorsStyles):
     hstack = ROOT.THStack("hstack","")
-    legend = TkAlStyle.legend(len(filesTitlesColorsStyles), 0.3)
+    legend = TkAlStyle.legend(len(filesTitlesColorsStyles), 1)
     legend.SetBorderSize(0)
     legend.SetFillStyle(0)
     hs = []
@@ -94,10 +94,13 @@ def plot(file_name,subdet_id,module_direction,overlap_direction, *filesTitlesCol
         h = hist(files,files.replace("/",""),subdet_id,module_direction,overlap_direction)
         h.SetLineColor(color)
         h.SetLineStyle(style)
-        legend.AddEntry(h, title, "l")
+	hMean = h.GetMean(1)
+	hMeanError = h.GetMeanError(1)
+#        legend.AddEntry(h, title, "l")
+        legend.AddEntry(h, title + ", mean = {0}\pm {1}\mu m ".format(round(hMean,3),round(hMeanError,3)), "l")
         hstack.Add(h)
         hs.append(h)
-    
+    hstack.SetMaximum(hstack.GetMaximum("nostack") * 1.2)    
     c = ROOT.TCanvas()
     hstack.Draw("nostack")
     legend.Draw()
