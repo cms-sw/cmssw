808263c6b1aa8a2774a2639e3381fd15512e5d03
Tyler Ruggles <truggles@wisc.edu>
2019-03-28 09:05:21 +0100

    Adding ability to do Jet HTT for gen jets if requested

--- L1Trigger/L1CaloTrigger/plugins/L1CaloJetHTTProducer.cc
+++ L1Trigger/L1CaloTrigger/plugins/L1CaloJetHTTProducer.cc
@@ -32,6 +32,9 @@ Implementation:
 // Run2/PhaseI output formats
 #include "DataFormats/L1Trigger/interface/L1JetParticleFwd.h"
 #include "DataFormats/L1Trigger/interface/Jet.h"
+// GenJets if needed
+#include "DataFormats/JetReco/interface/GenJet.h"
+#include "DataFormats/JetReco/interface/GenJetCollection.h"
 
 
 class L1CaloJetHTTProducer : public edm::EDProducer {
@@ -47,17 +50,24 @@ class L1CaloJetHTTProducer : public edm::EDProducer {
         edm::EDGetTokenT<BXVector<l1t::Jet>> bxvCaloJetsToken_;
         edm::Handle<BXVector<l1t::Jet>> bxvCaloJetsHandle;        
 
+        // Gen jet collections are only loaded and used if requested
+        // (use_gen_jets == true)
+        edm::EDGetTokenT<std::vector<reco::GenJet>> genJetsToken_;
+        edm::Handle<std::vector<reco::GenJet>> genJetsHandle;
 
         bool debug;
 
-        // Add CaloJets FIXME XXX
+        bool use_gen_jets;
+
 };
 
 L1CaloJetHTTProducer::L1CaloJetHTTProducer(const edm::ParameterSet& iConfig) :
     EtaMax(iConfig.getParameter<double>("EtaMax")),
     PtMin(iConfig.getParameter<double>("PtMin")),
     bxvCaloJetsToken_(consumes<BXVector<l1t::Jet>>(iConfig.getParameter<edm::InputTag>("BXVCaloJetsInputTag"))),
-    debug(iConfig.getParameter<bool>("debug"))
+    genJetsToken_(consumes<std::vector<reco::GenJet>>(iConfig.getParameter<edm::InputTag>("genJets"))),
+    debug(iConfig.getParameter<bool>("debug")),
+    use_gen_jets(iConfig.getParameter<bool>("use_gen_jets"))
 
 {
 
@@ -70,37 +80,60 @@ L1CaloJetHTTProducer::L1CaloJetHTTProducer(const edm::ParameterSet& iConfig) :
 void L1CaloJetHTTProducer::produce(edm::Event& iEvent, const edm::EventSetup& iSetup)
 {
 
-    //printf("begin L1CaloJetHTTProducer\n");
-
 
     // Output collections
     std::unique_ptr< float > CaloJetHTT(new float);
 
-
-
-    iEvent.getByToken(bxvCaloJetsToken_,bxvCaloJetsHandle);
-
-
     *CaloJetHTT = 0.;
-    if (bxvCaloJetsHandle.isValid())
+
+
+    // CaloJet HTT for L1 collections
+    if (!use_gen_jets)
     {
-        for(const auto& caloJet : *bxvCaloJetsHandle.product())
+        iEvent.getByToken(bxvCaloJetsToken_,bxvCaloJetsHandle);
+
+
+        if (bxvCaloJetsHandle.isValid())
         {
-            if (caloJet.pt() < PtMin) continue;
-            if ( fabs(caloJet.eta()) > EtaMax) continue;
-            *CaloJetHTT += float(caloJet.pt());
+            for(const auto& caloJet : *bxvCaloJetsHandle.product())
+            {
+                if (caloJet.pt() < PtMin) continue;
+                if ( fabs(caloJet.eta()) > EtaMax) continue;
+                *CaloJetHTT += float(caloJet.pt());
+            }
+        }
+
+        if (debug)
+        {
+            printf("BXV L1CaloJetCollection JetHTT = %f   for PtMin %f   and EtaMax %f\n", *CaloJetHTT, PtMin, EtaMax);
         }
     }
 
-    if (debug)
+    // CaloJet HTT for gen jets
+    if (use_gen_jets)
     {
-        printf("BXV L1CaloJetCollection JetHTT = %f   for PtMin %f   and EtaMax %f\n", *CaloJetHTT, PtMin, EtaMax);
+        iEvent.getByToken(genJetsToken_,genJetsHandle);
+
+
+        if (genJetsHandle.isValid())
+        {
+            for(const auto& genJet : *genJetsHandle.product())
+            {
+                if (genJet.pt() < PtMin) continue;
+                if ( fabs(genJet.eta()) > EtaMax) continue;
+                *CaloJetHTT += float(genJet.pt());
+            }
+        }
+
+        if (debug)
+        {
+            printf("Gen Jets HTT = %f   for PtMin %f   and EtaMax %f\n", *CaloJetHTT, PtMin, EtaMax);
+        }
     }
 
 
     iEvent.put(std::move(CaloJetHTT),"CaloJetHTT");
 
-    //printf("end L1CaloJetHTTProducer\n");
 }
 
 
--- L1Trigger/L1CaloTrigger/python/L1CaloJetHTTProducer_cfi.py
+++ L1Trigger/L1CaloTrigger/python/L1CaloJetHTTProducer_cfi.py
@@ -1,8 +1,10 @@
 import FWCore.ParameterSet.Config as cms
 
 L1CaloJetHTTProducer = cms.EDProducer("L1CaloJetHTTProducer",
-    debug = cms.bool(False),
-    PtMin = cms.double(30.0),
     EtaMax = cms.double(2.4),
-    BXVCaloJetsInputTag = cms.InputTag("L1CaloJetProducer","L1CaloJetCollectionBXV")
+    PtMin = cms.double(30.0),
+    BXVCaloJetsInputTag = cms.InputTag("L1CaloJetProducer","L1CaloJetCollectionBXV"),
+    genJets = cms.InputTag("ak4GenJetsNoNu", "", "HLT"),
+    debug = cms.bool(False),
+    use_gen_jets = cms.bool(False),
 )
