22c32c5386722fd2fe7a555dd9332c44f6ca1149
Marco Rovere <marco.rovere@cern.ch>
2019-03-28 17:30:58 +0100

    Add and use filter by size

--- RecoHGCal/TICL/plugins/filters.cc
+++ RecoHGCal/TICL/plugins/filters.cc
@@ -6,6 +6,8 @@
 
 #include "RecoHGCal/TICL/interface/ClusterFilterByAlgo.h"
 #include "RecoHGCal/TICL/interface/ClusterFilterByAlgoOrSize.h"
+#include "RecoHGCal/TICL/interface/ClusterFilterBySize.h"
 
 DEFINE_EDM_PLUGIN(ClusterFilterFactory, ClusterFilterByAlgo, "ClusterFilterByAlgo");
 DEFINE_EDM_PLUGIN(ClusterFilterFactory, ClusterFilterByAlgoOrSize, "ClusterFilterByAlgoOrSize");
+DEFINE_EDM_PLUGIN(ClusterFilterFactory, ClusterFilterBySize, "ClusterFilterBySize");
