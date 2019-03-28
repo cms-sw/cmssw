14c5d2c8a7962822b7e1752f55ce241eccfeaacc
Carl Vuosalo <cvuosalo@cern.ch>
2019-03-28 21:51:09 +0100

    Move generic angle utilities out of GeantUnits.h and into deltaPhi.h

--- DataFormats/Math/interface/deltaPhi.h
+++ DataFormats/Math/interface/deltaPhi.h
@@ -59,4 +59,132 @@ struct DeltaPhi {
   }
 };
 
+
+namespace angle_units {
+  
+  constexpr long double piRadians(M_PIl);  // M_PIl is long double version of pi
+  constexpr long double degPerRad = 180. / piRadians; // Degrees per radian
+  
+  namespace operators {
+
+    // Angle
+    constexpr long double operator "" _pi( long double x ) 
+    { return x * piRadians; }
+    constexpr long double operator "" _pi( unsigned long long int x ) 
+    { return x * piRadians; }
+    constexpr long double operator"" _deg( long double deg )
+    {
+      return deg / degPerRad;
+    }
+    constexpr long double operator"" _deg( unsigned long long int deg )
+    {
+      return deg / degPerRad;
+    }
+    constexpr long double operator"" _rad( long double rad )
+    {
+      return rad * 1.;
+    }
+
+    template <class NumType>
+    inline constexpr NumType convertRadToDeg(NumType radians) // Radians -> degrees
+    {
+      return (radians * degPerRad);
+    }
+
+    template <class NumType>
+    inline constexpr long double convertDegToRad(NumType degrees) // Degrees -> radians
+    {
+      return (degrees / degPerRad);
+    }
+  }
+}
+
+namespace angle0to2pi {
+
+  using namespace angle_units::operators;
+
+  // make0to2pi constrains an angle to be >= 0 and < 2pi.
+	// This function is a faster version of reco::reduceRange.
+	// In timing tests, it takes about half the time of reco::reduceRange.
+	// It also protects against floating-point value drift over repeated calculations.
+
+  template <class valType>
+  inline constexpr valType make0to2pi(valType angle) {
+    constexpr valType twoPi = 2._pi;
+    constexpr valType epsilon = 1.e-13;
+
+    if ((std::abs(angle) <= epsilon) || (std::abs(twoPi - std::abs(angle)) <= epsilon))
+      return (0.);
+      
+    // if statements arranged to promote faster performance
+    if (angle < 0.) {
+      if (angle >= -twoPi)
+        angle += twoPi;
+      else angle = fmod(angle, twoPi) + twoPi;
+    }
+    else if (angle >= twoPi) {
+      angle = fmod(angle, twoPi);
+    }
+    return (angle);
+  }
+
+
+  // Angle0to2pi class keep its  value as >= 0 and < 2pi.
+  //
+  // nearZero() tells whether the angle is close enough to 0 to be considered 0.
+  // The default tolerance is 1 degree.
+  //
+  // degrees() returns the angle in degrees.
+
+  template <class valType>
+  class Angle0to2pi {
+  public:
+
+    Angle0to2pi(valType ang) :
+    ang0to2pi (make0to2pi(ang))
+    {
+    }
+
+    inline operator valType() const {
+      return (ang0to2pi);
+    }
+
+    inline bool nearZero(float tolerance = 1.0_deg) const {
+      return (std::abs(ang0to2pi) - tolerance <= 0.0);
+    }
+
+    inline bool nearEqual(const Angle0to2pi<valType> &angle, float tolerance = 0.001) const {
+      return (std::abs(ang0to2pi - angle) - tolerance <= 0.0);
+    }
+
+    inline valType degrees() const {
+      return (convertRadToDeg(ang0to2pi));
+    }
+
+    inline Angle0to2pi & operator +=(valType ang) {
+      ang0to2pi = make0to2pi(ang0to2pi + ang);
+      return (*this);
+    }
+
+    inline Angle0to2pi & operator -=(valType ang) {
+      ang0to2pi = make0to2pi(ang0to2pi - ang );
+      return (*this);
+    }
+
+    inline Angle0to2pi & operator *=(valType val) {
+      ang0to2pi = make0to2pi(ang0to2pi * val);
+      return (*this);
+    }
+
+    inline Angle0to2pi & operator /=(valType val) {
+      ang0to2pi = make0to2pi(ang0to2pi / val);
+      return (*this);
+    }
+
+  private:
+    valType ang0to2pi;
+  };
+
+}
+
 #endif
