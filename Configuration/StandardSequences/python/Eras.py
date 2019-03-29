5d9a266bd45513f250541a24755b0cafa836f9df
Sunanda <sunanda.banerjee@cern.ch>
2019-03-30 00:17:33 +0100

    Add specifics for HGCal V10 geometry

--- Configuration/StandardSequences/python/Eras.py
+++ Configuration/StandardSequences/python/Eras.py
@@ -42,6 +42,9 @@ class Eras (object):
                  'Phase2C6',
                  'Phase2C6_timing',
                  'Phase2C4_timing_layer_bar',
+                 'Phase2C8',
+                 'Phase2C8_timing',
+                 'Phase2C8_timing_layer_bar',
         ]
 
         internalUseMods = ['run2_common', 'run2_25ns_specific',
@@ -54,7 +57,7 @@ class Eras (object):
                            'phase1Pixel', 'run3_GEM', 'run2_GEM_2017',
                            'run2_CSC_2018',
                            'phase2_common', 'phase2_tracker',
-                           'phase2_hgcal', 'phase2_muon', 'phase2_timing', 'phase2_hgcalV9', 'phase2_hfnose', 
+                           'phase2_hgcal', 'phase2_muon', 'phase2_timing', 'phase2_hgcalV9', 'phase2_hfnose', 'phase2_hgcalV10',
                            'phase2_timing_layer','phase2_timing_layer_tile','phase2_timing_layer_bar','phase2_hcal',
                            'trackingLowPU', 'trackingPhase1', 'ctpps_2016', 'trackingPhase2PU140','highBetaStar_2018',
                            'tracker_apv_vfp30_2016', 'pf_badHcalMitigation', 'run2_miniAOD_80XLegacy','run2_miniAOD_94XFall17', 'run2_nanoAOD_92X',
