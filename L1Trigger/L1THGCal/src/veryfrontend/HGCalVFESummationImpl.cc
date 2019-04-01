7f6dd912f9c0d54425ec11c203e7c8792c910552
Jean-Baptiste Sauvan <jean-baptiste.sauvan@cern.ch>
2019-04-01 09:05:41 +0200

    Add cell threshold

--- L1Trigger/L1THGCal/src/veryfrontend/HGCalVFESummationImpl.cc
+++ L1Trigger/L1THGCal/src/veryfrontend/HGCalVFESummationImpl.cc
@@ -1,7 +1,24 @@
 #include "L1Trigger/L1THGCal/interface/veryfrontend/HGCalVFESummationImpl.h"
 
-HGCalVFESummationImpl::HGCalVFESummationImpl(const edm::ParameterSet& conf)
-    : thickness_corrections_(conf.getParameter<std::vector<double>>("ThicknessCorrections")) {}
+HGCalVFESummationImpl::
+HGCalVFESummationImpl(const edm::ParameterSet& conf):
+  thickness_corrections_(conf.getParameter<std::vector<double>>("ThicknessCorrections")),
+  LSB_silicon_fC_(conf.getParameter<double>("siliconCellLSB_fC")),
+  LSB_scintillator_MIP_(conf.getParameter<double>("scintillatorCellLSB_MIP")),
+  thresholds_silicon_(conf.getParameter<std::vector<double>>("thresholdsSilicon")),
+  threshold_scintillator_(conf.getParameter<double>("thresholdScintillator"))
+{
+  if(thickness_corrections_.size()!=3)
+  {
+    throw cms::Exception("Configuration") <<
+      thickness_corrections_.size() << " thickness corrections are given instead of 3 (the number of sensor thicknesses)";
+  }
+  if(thresholds_silicon_.size()!=3)
+  {
+    throw cms::Exception("Configuration") <<
+      thresholds_silicon_.size() << " silicon thresholds are given instead of 3 (the number of sensor thicknesses)";
+  }
+}
 
 void HGCalVFESummationImpl::triggerCellSums(const HGCalTriggerGeometryBase& geometry,
                                             const std::vector<std::pair<DetId, uint32_t>>& linearized_dataframes,
@@ -11,14 +28,28 @@ void HGCalVFESummationImpl::triggerCellSums(const HGCalTriggerGeometryBase& geom
   // sum energies in trigger cells
   for (const auto& frame : linearized_dataframes) {
     DetId cellid(frame.first);
+    uint32_t value = frame.second;
+
+    // Apply noise threshold before summing into trigger cells
+    if(triggerTools_.isSilicon(cellid))
+    {
+      int thickness = triggerTools_.thicknessIndex(cellid);
+      double threshold = thresholds_silicon_.at(thickness);
+      value = ( value*LSB_silicon_fC_ > threshold ? value : 0 );
+    }
+    else if(triggerTools_.isScintillator(cellid))
+    {
+      value = ( value*LSB_scintillator_MIP_ > threshold_scintillator_ ? value : 0 );
+    }
+    if(value==0) continue;
 
     // find trigger cell associated to cell
     uint32_t tcid = geometry.getTriggerCellFromCell(cellid);
-    payload.emplace(tcid, 0);  // do nothing if key exists already
-    uint32_t value = frame.second;
-    unsigned det = cellid.det();
+    payload.emplace(tcid, 0); // do nothing if key exists already
+
     // equalize value among cell thicknesses for Silicon parts
-    if (det == DetId::Forward || det == DetId::HGCalEE || det == DetId::HGCalHSi) {
+    if(triggerTools_.isSilicon(cellid))
+    {
       int thickness = triggerTools_.thicknessIndex(cellid);
       double thickness_correction = thickness_corrections_.at(thickness);
       value = (double)value * thickness_correction;
