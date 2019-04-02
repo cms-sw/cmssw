3fcb86dcad9dffb40dd506bdf198408f45b9af07
leonardo <leonardo.giannini@cern.ch>
2019-04-02 12:30:06 +0200

    minor changes

--- RecoBTag/FeatureTools/interface/TrackPairInfoBuilder.h
+++ RecoBTag/FeatureTools/interface/TrackPairInfoBuilder.h
@@ -70,31 +70,35 @@ public:
         VertexDistance3D distanceComputer;
         TwoTrackMinimumDistance dist;
         
-        if(dist.calculate(tt->impactPointState(),it->impactPointState())) {
+        auto const& iImpactState = it->impactPointState();
+        auto const& tImpactState = tt->impactPointState();
+        
+        if(dist.calculate(tImpactState,iImpactState)) {
             
             GlobalPoint ttPoint          = dist.points().first;
-            GlobalError ttPointErr       = tt->impactPointState().cartesianError().position();
+            GlobalError ttPointErr       = tImpactState.cartesianError().position();
             GlobalPoint seedPosition     = dist.points().second;
-            GlobalError seedPositionErr  = it->impactPointState().cartesianError().position();
+            GlobalError seedPositionErr  = iImpactState.cartesianError().position();
             
             Measurement1D m = distanceComputer.distance(VertexState(seedPosition,seedPositionErr), VertexState(ttPoint, ttPointErr));
             
             GlobalPoint cp(dist.crossingPoint()); 
 
-            GlobalVector PairMomentum(it->track().px()+tt->track().px(), it->track().py()+tt->track().py(), it->track().pz()+tt->track().pz());
+            GlobalVector PairMomentum((Basic3DVector<float>) (it->track().momentum()+tt->track().momentum()));
+//             it->track().px()+tt->track().px(), it->track().py()+tt->track().py(), it->track().pz()+tt->track().pz()
             GlobalVector  PCA_pv(cp-pvp);
 
             float PCAseedFromPV =  (dist.points().second-pvp).mag();
             float PCAtrackFromPV =  (dist.points().first-pvp).mag();               
             float distance = dist.distance();
 
-            GlobalVector trackDir2D(tt->impactPointState().globalDirection().x(),tt->impactPointState().globalDirection().y(),0.); 
-            GlobalVector seedDir2D(it->impactPointState().globalDirection().x(),it->impactPointState().globalDirection().y(),0.); 
+            GlobalVector trackDir2D(tImpactState.globalDirection().x(),tImpactState.globalDirection().y(),0.); 
+            GlobalVector seedDir2D(iImpactState.globalDirection().x(),iImpactState.globalDirection().y(),0.); 
             GlobalVector trackPCADir2D(dist.points().first.x()-pvp.x(),dist.points().first.y()-pvp.y(),0.); 
             GlobalVector seedPCADir2D(dist.points().second.x()-pvp.x(),dist.points().second.y()-pvp.y(),0.); 
 
-            float dotprodTrack = (dist.points().first-pvp).unit().dot(tt->impactPointState().globalDirection().unit());
-            float dotprodSeed = (dist.points().second-pvp).unit().dot(it->impactPointState().globalDirection().unit());                    
+            float dotprodTrack = (dist.points().first-pvp).unit().dot(tImpactState.globalDirection().unit());
+            float dotprodSeed = (dist.points().second-pvp).unit().dot(iImpactState.globalDirection().unit());                    
     
             std::pair<bool,Measurement1D> t_ip = IPTools::absoluteImpactParameter3D(*tt,pv);        
             std::pair<bool,Measurement1D> t_ip2d = IPTools::absoluteTransverseImpactParameter(*tt,pv);             
@@ -141,7 +145,7 @@ public:
             track_ip3dSig_=t_ip.second.significance();
 
             dotprodTrackSeed2D_=trackDir2D.unit().dot(seedDir2D.unit());
-            dotprodTrackSeed3D_=it->impactPointState().globalDirection().unit().dot(tt->impactPointState().globalDirection().unit());
+            dotprodTrackSeed3D_=iImpactState.globalDirection().unit().dot(tImpactState.globalDirection().unit());
             dotprodTrackSeed2DV_=trackPCADir2D.unit().dot(seedPCADir2D.unit());
             dotprodTrackSeed3DV_=(dist.points().second-pvp).unit().dot((dist.points().first-pvp).unit());
 
@@ -155,42 +159,42 @@ public:
     }
 
    
-    const float get_track_pt() const {return track_pt_;}
-    const float get_track_eta() const {return track_eta_;}
-    const float get_track_phi() const {return track_phi_;}
-    const float get_track_dz() const {return track_dz_;}
-    const float get_track_dxy() const {return track_dxy_;}
-    const float get_pca_distance() const {return pca_distance_;}
-    const float get_pca_significance() const {return pca_significance_; }   
-    const float get_pcaSeed_x() const {return pcaSeed_x_;}
-    const float get_pcaSeed_y() const {return pcaSeed_y_;}
-    const float get_pcaSeed_z() const {return pcaSeed_z_;}
-    const float get_pcaSeed_xerr() const {return pcaSeed_xerr_;}
-    const float get_pcaSeed_yerr() const {return pcaSeed_yerr_;}
-    const float get_pcaSeed_zerr() const {return pcaSeed_zerr_;}
-    const float get_pcaTrack_x() const {return pcaTrack_x_;}
-    const float get_pcaTrack_y() const {return pcaTrack_y_;}
-    const float get_pcaTrack_z() const {return pcaTrack_z_;}
-    const float get_pcaTrack_xerr() const {return pcaTrack_xerr_;}
-    const float get_pcaTrack_yerr() const {return pcaTrack_yerr_;}
-    const float get_pcaTrack_zerr() const {return pcaTrack_zerr_;}    
-    const float get_dotprodTrack() const {return dotprodTrack_;}
-    const float get_dotprodSeed() const {return dotprodSeed_;}
-    const float get_pcaSeed_dist() const {return pcaSeed_dist_;}
-    const float get_pcaTrack_dist() const {return pcaTrack_dist_;}    
-    const float get_track_candMass() const {return track_candMass_;}
-    const float get_track_ip2d() const {return track_ip2d_;}
-    const float get_track_ip2dSig() const {return track_ip2dSig_;}
-    const float get_track_ip3d() const {return track_ip3d_;}
-    const float get_track_ip3dSig() const {return track_ip3dSig_; }   
-    const float get_dotprodTrackSeed2D() const {return dotprodTrackSeed2D_;}
-    const float get_dotprodTrackSeed2DV() const {return dotprodTrackSeed2DV_;}
-    const float get_dotprodTrackSeed3D() const {return dotprodTrackSeed3D_;}
-    const float get_dotprodTrackSeed3DV() const {return dotprodTrackSeed3DV_;}    
-    const float get_pca_jetAxis_dist() const {return pca_jetAxis_dist_;}
-    const float get_pca_jetAxis_dotprod() const {return pca_jetAxis_dotprod_;}
-    const float get_pca_jetAxis_dEta() const {return pca_jetAxis_dEta_;}
-    const float get_pca_jetAxis_dPhi() const {return pca_jetAxis_dPhi_;}
+    const float track_pt() const {return track_pt_;}
+    const float track_eta() const {return track_eta_;}
+    const float track_phi() const {return track_phi_;}
+    const float track_dz() const {return track_dz_;}
+    const float track_dxy() const {return track_dxy_;}
+    const float pca_distance() const {return pca_distance_;}
+    const float pca_significance() const {return pca_significance_; }   
+    const float pcaSeed_x() const {return pcaSeed_x_;}
+    const float pcaSeed_y() const {return pcaSeed_y_;}
+    const float pcaSeed_z() const {return pcaSeed_z_;}
+    const float pcaSeed_xerr() const {return pcaSeed_xerr_;}
+    const float pcaSeed_yerr() const {return pcaSeed_yerr_;}
+    const float pcaSeed_zerr() const {return pcaSeed_zerr_;}
+    const float pcaTrack_x() const {return pcaTrack_x_;}
+    const float pcaTrack_y() const {return pcaTrack_y_;}
+    const float pcaTrack_z() const {return pcaTrack_z_;}
+    const float pcaTrack_xerr() const {return pcaTrack_xerr_;}
+    const float pcaTrack_yerr() const {return pcaTrack_yerr_;}
+    const float pcaTrack_zerr() const {return pcaTrack_zerr_;}    
+    const float dotprodTrack() const {return dotprodTrack_;}
+    const float dotprodSeed() const {return dotprodSeed_;}
+    const float pcaSeed_dist() const {return pcaSeed_dist_;}
+    const float pcaTrack_dist() const {return pcaTrack_dist_;}    
+    const float track_candMass() const {return track_candMass_;}
+    const float track_ip2d() const {return track_ip2d_;}
+    const float track_ip2dSig() const {return track_ip2dSig_;}
+    const float track_ip3d() const {return track_ip3d_;}
+    const float track_ip3dSig() const {return track_ip3dSig_; }   
+    const float dotprodTrackSeed2D() const {return dotprodTrackSeed2D_;}
+    const float dotprodTrackSeed2DV() const {return dotprodTrackSeed2DV_;}
+    const float dotprodTrackSeed3D() const {return dotprodTrackSeed3D_;}
+    const float dotprodTrackSeed3DV() const {return dotprodTrackSeed3DV_;}    
+    const float pca_jetAxis_dist() const {return pca_jetAxis_dist_;}
+    const float pca_jetAxis_dotprod() const {return pca_jetAxis_dotprod_;}
+    const float pca_jetAxis_dEta() const {return pca_jetAxis_dEta_;}
+    const float pca_jetAxis_dPhi() const {return pca_jetAxis_dPhi_;}
     
     
 
