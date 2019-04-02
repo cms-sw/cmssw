e9a30cc8ad5c43b34636a97d601cf169f5c7e087
yechanKang <yck.kang@gmail.com>
2019-04-02 16:23:14 +0900

    rename files and romove GEMDigiModelFactory

--- SimMuon/GEMDigitizer/src/SealModule.cc
+++ SimMuon/GEMDigitizer/src/SealModule.cc
@@ -4,12 +4,6 @@
 #include "SimMuon/GEMDigitizer/interface/ME0DigiPreRecoModelFactory.h"
 #include "SimMuon/GEMDigitizer/interface/ME0DigiModelFactory.h"
 
-#include "SimMuon/GEMDigitizer/interface/GEMDigiProducer.h"
-DEFINE_FWK_MODULE(GEMDigiProducer);
-
-#include "SimMuon/GEMDigitizer/interface/GEMSimpleModel.h"
-DEFINE_EDM_PLUGIN(GEMDigiModelFactory, GEMSimpleModel, "GEMSimpleModel");
-
 #include "SimMuon/GEMDigitizer/interface/GEMPadDigiProducer.h"
 DEFINE_FWK_MODULE(GEMPadDigiProducer);
 
