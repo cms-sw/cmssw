459ef47f814fa96184a134ef4959c2b16649b7f4
Laurent Forthomme <laurent.forthomme@cern.ch>
2019-04-02 15:48:12 +0200

    Also compute the ToT if no calibration function is present

--- RecoCTPPS/TotemRPLocal/src/CTPPSDiamondRecHitProducerAlgorithm.cc
+++ RecoCTPPS/TotemRPLocal/src/CTPPSDiamondRecHitProducerAlgorithm.cc
@@ -63,12 +63,15 @@ CTPPSDiamondRecHitProducerAlgorithm::build( const CTPPSGeometry& geom,
         : CTPPSDiamondRecHit::TIMESLICE_WITHOUT_LEADING;
 
       double tot = -1., t_mean = 0.;
-      if ( calib_fct_ && t_lead != 0 && t_trail != 0 ) {
+      if ( t_lead != 0 && t_trail != 0 ) {
         tot = ( t_trail-t_lead )*ts_to_ns_;
-        t_mean = calib_fct_->evaluate( std::vector<double>{ tot }, ch_params );
-        if ( std::isnan( t_mean ) )
-          t_mean = 0.;
+        if ( calib_fct_ ) {
+          t_mean = calib_fct_->evaluate( std::vector<double>{ tot }, ch_params );
+          if ( std::isnan( t_mean ) )
+            t_mean = 0.;
+        }
       }
+      // calibrated time of arrival
       const double t0 = ( t_lead % 1024 )*ts_to_ns_+ ch_t_offset-t_mean;
 
       rec_hits.emplace_back(
