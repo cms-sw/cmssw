1257fec5e2646a62ac69b6589dab0a2af21c4a32
leonardo <leonardo.giannini@cern.ch>
2019-04-03 16:46:35 +0200

    changes for testing

--- RecoBTag/ONNXRuntime/python/pfDeepFlavour_cff.py
+++ RecoBTag/ONNXRuntime/python/pfDeepFlavour_cff.py
@@ -6,6 +6,8 @@ from RecoBTag.FeatureTools.pfDeepDoubleXTagInfos_cfi import pfDeepDoubleXTagInfo
 
 from RecoBTag.ONNXRuntime.pfDeepFlavourJetTags_cfi import pfDeepFlavourJetTags
 from RecoBTag.ONNXRuntime.pfNegativeDeepFlavourJetTags_cfi import pfNegativeDeepFlavourJetTags
+from RecoBTag.TensorFlow.pfDeepVertexJetTags_cfi import pfDeepVertexJetTags
+
 from CommonTools.PileupAlgos.Puppi_cff import puppi
 from PhysicsTools.PatAlgos.slimming.primaryVertexAssociation_cfi import primaryVertexAssociation
 
