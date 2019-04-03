afc33a0652d700995282406ddb3d9300bd77ca48
Jean-Baptiste Sauvan <jean-baptiste.sauvan@cern.ch>
2019-04-03 13:32:59 +0200

    Add cluster selector to TPG chain

--- L1Trigger/L1THGCalUtilities/python/selectors.py
+++ L1Trigger/L1THGCalUtilities/python/selectors.py
@@ -3,8 +3,7 @@ import FWCore.ParameterSet.Config as cms
 def create_genmatch(process, inputs,
         distance=0.3
         ):
-    producer = process.hgc3DClusterGenMatchSelector.clone(
-            dR = cms.double(distance),
-            src = cms.InputTag('{}:HGCalBackendLayer2Processor3DClustering'.format(inputs))
-            )
+    producer = process.hgc3DClusterGenMatchSelector.clone() 
+    producer.dR = cms.double(distance)
+    producer.src = cms.InputTag('{}:HGCalBackendLayer2Processor3DClustering'.format(inputs))
     return producer
