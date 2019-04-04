ede13584a703a7bb4fa095c6690e34ce76630a89
tocheng <tocheng.cms@gmail.com>
2019-04-04 21:33:19 +0200

    solve conflicts with old local conditions

--- Configuration/AlCa/python/autoCond.py
+++ Configuration/AlCa/python/autoCond.py
@@ -24,11 +24,11 @@ autoCond = {
     # GlobalTag for MC production (p-Pb collisions) with realistic alignment and calibrations for Run2
     'run2_mc_pa'        :   '105X_mcRun2_pA_v2',
     # GlobalTag for Run1 data reprocessing
-    'run1_data'         :   '106X_dataRun2_v1',
+    'run1_data'         :   '106X_dataRun2_v4',
     # GlobalTag for Run2 data reprocessing
-    'run2_data'         :   '106X_dataRun2_v1',
+    'run2_data'         :   '106X_dataRun2_v4',
     # GlobalTag for Run2 data relvals: allows customization to run with fixed L1 menu
-    'run2_data_relval'  :   '106X_dataRun2_relval_v1',
+    'run2_data_relval'  :   '106X_dataRun2_relval_v2',
     # GlobalTag for Run2 data 2018B relvals only: HEM-15-16 fail
     'run2_data_promptlike_HEfail' : '106X_dataRun2_PromptLike_HEfail_v1',
     # GlobalTag for Run2 data 2016H relvals only: Prompt Conditions + fixed L1 menu (to be removed)
