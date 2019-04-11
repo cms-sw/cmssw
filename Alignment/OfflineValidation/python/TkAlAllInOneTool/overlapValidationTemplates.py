96ad02ab5ef00461cb96fe8e393c3d6cf076484d
Jason Fan <jfan20@jhu.edu>
2019-04-11 21:08:11 +0200

    restructed overlapvalidation.py for efficiency

--- Alignment/OfflineValidation/python/TkAlAllInOneTool/overlapValidationTemplates.py
+++ Alignment/OfflineValidation/python/TkAlAllInOneTool/overlapValidationTemplates.py
@@ -35,23 +35,14 @@ try:
   os.makedirs(".oO[datadir]Oo./.oO[PlotsDirName]Oo./")
 except OSError:
   pass
+try:
+  os.makedirs(".oO[datadir]Oo./.oO[PlotsDirName]Oo./Profiles")
+except OSError:
+  pass
 
 from Alignment.OfflineValidation.overlapValidationPlot import plot
-detNameList = ("BPIX", "FPIX", "TIB", "TID", "TOB", "TEC")
-for subdetid in (1,2,3,4,5,6):
-	for moduledirection in ("phi","z","r"):
-		for overlapdirection in ("phi","z","r"):
-			if subdetid == 1 and (moduledirection == "r" or overlapdirection =="r"):
-				continue
-			if subdetid == 2 and (moduledirection == "z" or overlapdirection == "z"):
-				continue
-			if (subdetid == 3 or subdetid == 5) and (overlapdirection != "phi" or moduledirection == "r"):
-				continue
-			if (subdetid == 4 or subdetid == 6) and (overlapdirection != "phi" or moduledirection == "z"):
-                                continue
-			plot(".oO[datadir]Oo./.oO[PlotsDirName]Oo./{0}_{1}_{2}".format(moduledirection,overlapdirection,detNameList[subdetid-1]),subdetid,moduledirection,overlapdirection,None, .oO[PlottingInstantiation]Oo.)
 
-plot(".oO[datadir]Oo./.oO[PlotsDirName]Oo./profile_phi_phi_TOB_z", 5, "phi", "phi", "z", .oO[PlottingInstantiation]Oo.)
+plot(".oO[datadir]Oo./.oO[PlotsDirName]Oo./", True,.oO[PlottingInstantiation]Oo.)
 
 
 """
--- Alignment/OfflineValidation/python/overlapValidationPlot.py
+++ Alignment/OfflineValidation/python/overlapValidationPlot.py
@@ -3,41 +3,67 @@ from __future__ import print_function
 import math
 import ROOT
 from TkAlStyle import TkAlStyle
-
-def hist(tree_file_name, hist_name,subdet_id,module_direction,overlap_direction,profile_direction):
+dirNameList=["z","r","phi"]
+detNameList = ("BPIX", "FPIX", "TIB", "TID", "TOB", "TEC")
+def hist(tree_file_name, hist_name,profiles):
     f = ROOT.TFile(tree_file_name)
     t = f.Get("analysis/Overlaps")
-    if profile_direction is None:
-        if (subdet_id==4 or subdet_id==6):
-	    h = ROOT.TH1F(hist_name, hist_name, 100, -5000, 5000)
-        else:
-    	    h = ROOT.TH1F(hist_name, hist_name, 100, -300, 300)
-
-    else: h = ROOT.TProfile(hist_name, hist_name, 10, -100, 100) 
-
-    h.SetDirectory(0)
-
+    if profiles==False:
+		l = 1
+    if profiles == True:
+		l = 4
+    dimList=[[35,15],[60,15],[70,60],[100,55],[100,110],[280,110]] #Dimension of tracker in cm
+    h = []
+    for subdet in range(6):#Creates a 4-D list of empty histograms for permutations of subdetector, overlap direction, module direction and profile direction.
+		h.append([])
+		for module in range(3):
+			h[subdet].append([])
+			for overlap in range(3):
+				h[subdet][module].append([])
+				for profile in range(l):
+					if (subdet+1 == 1 and (module == 1 or overlap ==1))or (subdet+1== 2 and (module == 0 or overlap == 0))or ((subdet+1== 3 or subdet+1== 5) and (overlap != 2 or module == 1))or ((subdet+1== 4 or subdet+1== 6) and (overlap != 2 or module == 0)):
+                        			h[subdet][module][overlap].append(0)
+						continue
+					name = hist_name + "{0}_{1}_{2}".format(dirNameList[module],dirNameList[overlap],detNameList[subdet])
+					if (profile>0):
+					    name = name + "{0}Profile".format(dirNameList[profile-1])
+					    if profile==3:
+					    	h[subdet][module][overlap].append(ROOT.TProfile(name, name, 10, -3.5, 3.5))
+					    elif profile==1:
+					    	h[subdet][module][overlap].append(ROOT.TProfile(name, name, 20, -dimList[subdet][profile-1],dimList[subdet][profile-1]))
+					    elif profile==2:
+					    	h[subdet][module][overlap].append(ROOT.TProfile(name, name, 20, 0,dimList[subdet][profile-1]))
+					elif subdet+1==4 or subdet+1==6:
+					    h[subdet][module][overlap].append(ROOT.TH1F(name, name, 100, -5000, 5000))
+					else:
+					    h[subdet][module][overlap].append(ROOT.TH1F(name, name, 100, -300, 300))						
+					h[subdet][module][overlap][profile].SetDirectory(0)
     nentries = t.GetEntries()
 
-    for i, entry in enumerate(t, start=1):
+    for i, entry in enumerate(t, start=1):#loops through the tree, filling in relevant histograms for each event
         if i % 10000 == 0 or i == nentries:
             print(i, "/", nentries)
-        if not ((t.subdetID == subdet_id)):
-            continue
-	if module_direction not in ("r" ,"phi", "z"): 
-	    raise ValueError("Invalid module direction")
-	if module_direction != "z" and subdet_id%2 == 1 and ((abs(t.moduleZ[0] - t.moduleZ[1]) < 1)):
-	    continue 
-        modulePhi0 = math.atan2(t.moduleY[0], t.moduleX[0]) 
-        modulePhi1 = math.atan2(t.moduleY[1], t.moduleX[1])
+	
+        subdet_id = t.subdetID
+	modulePhi0 = math.atan2(t.moduleY[0], t.moduleX[0]) 
+	modulePhi1 = math.atan2(t.moduleY[1], t.moduleX[1])
 	phidiff = min(abs(modulePhi0-modulePhi1), abs(math.pi - abs(modulePhi0-modulePhi1)))
 	moduleR0 = math.sqrt(t.moduleY[0]**2+t.moduleX[0]**2)
 	moduleR1 = math.sqrt(t.moduleY[1]**2+t.moduleX[1]**2)
-	if module_direction !="r" and subdet_id%2 == 0 and (abs(moduleR0 - moduleR1)<1):
-            continue	
-	if module_direction !="phi" and ((moduleR0*phidiff)<1):
+
+	#determines the direction the modules are in respect to each other for each event
+	if subdet_id%2 == 1 and ((abs(t.moduleZ[0] - t.moduleZ[1]) > 1)): 
+	    module_direction = 0 #0 refers to Z, 1 to R and 2 to phi
+        elif subdet_id%2 == 0 and (abs(moduleR0 - moduleR1)>1):
+            module_direction = 1	
+	elif ((moduleR0*phidiff)>1):
+	    module_direction = 2
+	else:
 	    continue
-	if overlap_direction == "phi":
+
+	avgList=[(t.moduleZ[0]+t.moduleZ[1])/2,(moduleR0+moduleR1)/2,(modulePhi0+modulePhi1)/2]
+	if True:
+	     overlap_direction = 2
              if modulePhi0 > modulePhi1:
                  hitXA = t.hitX[1]
                  hitXB = t.hitX[0]
@@ -52,7 +78,21 @@ def hist(tree_file_name, hist_name,subdet_id,module_direction,overlap_direction,
                  predXB = t.predX[1]
                  overlapSignA = t.localxdotglobalphi[0]
                  overlapSignB = t.localxdotglobalphi[1]
-	if overlap_direction == "z":
+	     residualA = hitXA - predXA
+             residualB = hitXB - predXB
+             if overlapSignA < 0:
+        	 residualA *= -1
+             if overlapSignB < 0:
+                 residualB *= -1
+             A = 10000*(residualA - residualB)
+	     h[subdet_id-1][module_direction][overlap_direction][0].Fill(A)
+	     if profiles == True:
+		for profile in range(3):
+			h[subdet_id-1][module_direction][overlap_direction][profile+1].Fill(avgList[profile],A)
+	         
+	         	
+	if subdet_id==1 and module_direction != 1:
+	     overlap_direction = 0
              if t.moduleZ[0] > t.moduleZ[1]:
                  hitXA = t.hitY[1]
                  hitXB = t.hitY[0]
@@ -67,7 +107,20 @@ def hist(tree_file_name, hist_name,subdet_id,module_direction,overlap_direction,
                  predXB = t.predY[1]
                  overlapSignA = t.localydotglobalz[0]
                  overlapSignB = t.localydotglobalz[1]
-	if overlap_direction == "r":
+	     residualA = hitXA - predXA
+             residualB = hitXB - predXB
+             if overlapSignA < 0:
+        	residualA *= -1
+             if overlapSignB < 0:
+                residualB *= -1
+             A = 10000*(residualA - residualB)
+	     h[subdet_id-1][module_direction][overlap_direction][0].Fill(A)
+             if profiles == True:
+                for profile in range(3):
+                        h[subdet_id-1][module_direction][overlap_direction][profile+1].Fill(avgList[profile],A)
+
+	if subdet_id==2 and module_direction !=0:
+	     overlap_direction = 1
              if moduleR0 > moduleR1:
                  hitXA = t.hitY[1]
                  hitXB = t.hitY[0]
@@ -82,54 +135,89 @@ def hist(tree_file_name, hist_name,subdet_id,module_direction,overlap_direction,
                  predXB = t.predY[1]
                  overlapSignA = t.localydotglobalr[0]
                  overlapSignB = t.localydotglobalr[1]
-
-        residualA = hitXA - predXA
-        residualB = hitXB - predXB
-        if overlapSignA < 0:
-            residualA *= -1
-        if overlapSignB < 0:
-            residualB *= -1
-
-        A = 10000*(residualA - residualB)
-        if profile_direction is None:
-            h.Fill(A)
-        elif profile_direction == "z":
-            h.Fill((t.moduleZ[0]+t.moduleZ[1])/2, A)
-
+	     
+             residualA = hitXA - predXA
+             residualB = hitXB - predXB
+             if overlapSignA < 0:
+                 residualA *= -1
+             if overlapSignB < 0:
+                 residualB *= -1
+             A = 10000*(residualA - residualB)
+	     h[subdet_id-1][module_direction][overlap_direction][0].Fill(A)
+             if profiles == True:
+                for profile in range(3):
+                        h[subdet_id-1][module_direction][overlap_direction][profile+1].Fill(avgList[profile],A)
     return h
 
-def plot(file_name,subdet_id,module_direction,overlap_direction,profile_direction,*filesTitlesColorsStyles):
-    hstack = ROOT.THStack("hstack","")
-    legend = TkAlStyle.legend(len(filesTitlesColorsStyles), 1)
-    legend.SetBorderSize(0)
-    legend.SetFillStyle(0)
-    hs = []
-    
-    for files, title, color, style in filesTitlesColorsStyles:
-        h = hist(files,files.replace("/",""),subdet_id,module_direction,overlap_direction,profile_direction)
-        h.SetLineColor(color)
-        h.SetLineStyle(style)
-	hMean = h.GetMean(1)
-	hMeanError = h.GetMeanError(1)
-#        legend.AddEntry(h, title, "l")
-        legend.AddEntry(h, title + ", mean = {0}\pm {1}\mu m ".format(round(hMean,3),round(hMeanError,3)), "l")
-        hstack.Add(h)
-        hs.append(h)
-    hstack.SetMaximum(hstack.GetMaximum("nostack") * 1.2)    
-    c = ROOT.TCanvas()
-    hstack.Draw("nostack")
-    legend.Draw()
-    xTitle = "hit_{A} - pred_{A} - (hit_{B} - pred_{B}) (#mum)"
-
-    hstack.GetXaxis().SetTitle(xTitle)
-    hstack.GetYaxis().SetTitle("number of events")
-    hstack.GetXaxis().SetNdivisions(404)
-
-    TkAlStyle.drawStandardTitle()
-        
-    save_as_file_name = file_name
-
-
-    for ext in "png", "eps", "root", "pdf":
-        c.SaveAs(save_as_file_name+"." +ext)
+def plot(file_name,profiles,*filesTitlesColorsStyles):
+	legend=[]
+	hstack=[]
+	if profiles == False:
+		l = 1
+	else:
+		l=4
+	for subdet in range(6):#creates lists of empty THStacks and legends to be filled later
+		hstack.append([])
+		legend.append([])
+		for module in range(3):
+			hstack[subdet].append([])
+			legend[subdet].append([])
+			for overlap in range(3):
+				hstack[subdet][module].append([])
+				legend[subdet][module].append([])
+				for profile in range(l):
+					if (subdet+1== 1 and (module == 1 or overlap ==1))or (subdet+1== 2 and (module == 0 or overlap == 0))or ((subdet+1== 3 or subdet+1== 5) and (overlap != 2 or module == 1))or ((subdet+1== 4 or subdet+1== 6) and (overlap != 2 or module == 0)):
+                        			hstack[subdet][module][overlap].append(0)
+						legend[subdet][module][overlap].append(0)
+						continue
+					else:
+						hstack[subdet][module][overlap].append(ROOT.THStack("hstack",""))
+						legend[subdet][module][overlap].append(TkAlStyle.legend(len(filesTitlesColorsStyles), 1))
+						legend[subdet][module][overlap][profile].SetBorderSize(0)
+						legend[subdet][module][overlap][profile].SetFillStyle(0)    
+	for files, title, color, style in filesTitlesColorsStyles:
+		h = hist(files,files.replace("/",""),profiles)
+		for subdet in range(6):
+			for module in range(3):
+				for overlap in range(3):
+					if (subdet+1== 1 and (module == 1 or overlap ==1))or (subdet+1== 2 and (module == 0 or overlap == 0))or ((subdet+1== 3 or subdet+1== 5) and (overlap != 2 or module == 1))or ((subdet+1== 4 or subdet+1== 6) and (overlap != 2 or module == 0)):
+							continue
+					for profile in range(l):						
+						g = h[subdet][module][overlap][profile]
+						g.SetLineColor(color)
+						g.SetLineStyle(style)
+						hMean = g.GetMean(1)
+						hMeanError = g.GetMeanError(1)
+						if (profile==0):
+							legend[subdet][module][overlap][profile].AddEntry(g, title + ", mean = {0}\pm {1}\mu m ".format(round(hMean,3),round(hMeanError,3)), "l")
+					        else:
+							legend[subdet][module][overlap][profile].AddEntry(g, title, "l")
+						hstack[subdet][module][overlap][profile].Add(g)
+	for subdet in range(6):
+			for module in range(3):
+				for overlap in range(3):
+						if (subdet+1== 1 and (module == 1 or overlap ==1))or (subdet+1== 2 and (module == 0 or overlap == 0))or ((subdet+1== 3 or subdet+1== 5) and (overlap != 2 or module == 1))or ((subdet+1== 4 or subdet+1== 6) and (overlap != 2 or module == 0)):
+							continue
+						for profile in range(l):
+							currLegend = legend[subdet][module][overlap][profile]
+							currhstack = hstack[subdet][module][overlap][profile]
+							currhstack.SetMaximum(currhstack.GetMaximum("nostack") * 1.2)    
+							c = ROOT.TCanvas()
+							currhstack.Draw("nostack")
+							currLegend.Draw()
+							xTitle = "hit_{A} - pred_{A} - (hit_{B} - pred_{B}) (#mum)"
+							yTitle="number of events"
+							save_as_file_name = file_name +  "{0}_{1}_{2}".format(dirNameList[module],dirNameList[overlap],detNameList[subdet])
+							if profile>0:
+								save_as_file_name = file_name +"Profiles/profile_{0}_{1}_{2}_{3}".format(dirNameList[module],dirNameList[overlap],detNameList[subdet],dirNameList[profile-1])
+								yTitle= xTitle
+								xTitle= dirNameList[profile-1]
+							currhstack.GetXaxis().SetTitle(xTitle)
+							currhstack.GetYaxis().SetTitle(yTitle)
+							if profile==0:
+								currhstack.GetXaxis().SetNdivisions(404)
+							TkAlStyle.drawStandardTitle()        
+							
+							for ext in "png", "eps", "root", "pdf":
+								c.SaveAs(save_as_file_name+"." +ext)
 
