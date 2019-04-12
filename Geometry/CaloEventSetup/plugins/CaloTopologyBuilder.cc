8dfebe90e0d1d29228df33031e9d6b84b2b04e5a
Matti Kortelainen <matti.kortelainen@cern.ch>
2019-04-12 23:48:45 +0200

    Pass and store subdetector topologies as unique_ptr's

--- Geometry/CaloEventSetup/plugins/CaloTopologyBuilder.cc
+++ Geometry/CaloEventSetup/plugins/CaloTopologyBuilder.cc
@@ -38,12 +38,12 @@ CaloTopologyBuilder::produceCalo( const CaloTopologyRecord& iRecord )
    //ECAL parts      
    ct->setSubdetTopology( DetId::Ecal,
 			  EcalBarrel,
-			  new EcalBarrelTopology( *theGeometry ) ) ;
+			  std::make_unique<EcalBarrelTopology>( *theGeometry ) ) ;
    ct->setSubdetTopology( DetId::Ecal,
 			  EcalEndcap,
-			  new EcalEndcapTopology( *theGeometry ) ) ;
+			  std::make_unique<EcalEndcapTopology>( *theGeometry ) ) ;
    ct->setSubdetTopology( DetId::Ecal,
 			  EcalPreshower,
-			  new EcalPreshowerTopology());
+			  std::make_unique<EcalPreshowerTopology>());
    return ct ;
 }
