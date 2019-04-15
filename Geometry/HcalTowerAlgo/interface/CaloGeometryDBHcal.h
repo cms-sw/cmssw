fa59003eeb466f659d50caa6c91515aade203244
Matti Kortelainen <matti.kortelainen@cern.ch>
2019-04-15 18:39:23 +0200

    Migrate CaloGeometryDBEP to EventSetup consumes
    There are specializations of CaloGeometryDBEP::produceAligned() that
    ask for additional ESProducts. I added a mechanism for such
    specializations to call consumes() to get ESGetTokens for such
    products.

---
+++ Geometry/HcalTowerAlgo/interface/CaloGeometryDBHcal.h
@@ -0,0 +1,16 @@
+#ifndef Geometry_HcalTowerAlgo_CaloGeometryDBHcal_h
+#define Geometry_HcalTowerAlgo_CaloGeometryDBHcal_h
+
+#include "Geometry/CaloEventSetup/interface/CaloGeometryDBEP.h"
+
+namespace calogeometryDBEPimpl {
+  template <>
+  struct AdditionalTokens<HcalGeometry> {
+    void makeTokens(edm::ESConsumesCollectorT<HcalGeometry::AlignedRecord>& cc) {
+      topology = cc.consumesFrom<HcalTopology, HcalRecNumberingRecord>(edm::ESInputTag{});
+    }
+    edm::ESGetToken<HcalTopology, HcalRecNumberingRecord> topology;
+  };
+}
+
+#endif
