f8c857c5b6465deb45c04a908e53eec77339a90f
Matti Kortelainen <matti.kortelainen@cern.ch>
2019-04-16 15:00:25 -0500

    Add get() and getHandle() to DependentRecordImplementation

--- FWCore/Framework/interface/DependentRecordImplementation.h
+++ FWCore/Framework/interface/DependentRecordImplementation.h
@@ -76,6 +76,33 @@ class DependentRecordImplementation : public EventSetupRecordImplementation<Reco
         return eventSetupT.tryToGet<DepRecordT>();
       }
 
+      using EventSetupRecordImplementation<RecordT>::getHandle;
+
+      template<typename ProductT, typename DepRecordT>
+      ESHandle<ProductT> getHandle(ESGetToken<ProductT, DepRecordT> const& iToken) const {
+        //Make sure that DepRecordT is a type in ListT
+        using EndItrT = typename boost::mpl::end< ListT >::type;
+        using FoundItrT = typename boost::mpl::find< ListT, DepRecordT >::type;
+        static_assert(! std::is_same<FoundItrT, EndItrT>::value, "Trying to get a a product with an ESGetToken specifying a Record from another Record where the second Record is not dependent on the first Record.");
+        return getRecord<DepRecordT>().getHandle(iToken);
+      }
+
+      using EventSetupRecordImplementation<RecordT>::get;
+
+      template<typename ProductT, typename DepRecordT>
+      ProductT const& get(ESGetToken<ProductT, DepRecordT> const& iToken) const {
+        //Make sure that DepRecordT is a type in ListT
+        using EndItrT = typename boost::mpl::end< ListT >::type;
+        using FoundItrT = typename boost::mpl::find< ListT, DepRecordT >::type;
+        static_assert(! std::is_same<FoundItrT, EndItrT>::value, "Trying to get a a product with an ESGetToken specifying a Record from another Record where the second Record is not dependent on the first Record.");
+        return getRecord<DepRecordT>().get(iToken);
+      }
+
+      template<typename ProductT, typename DepRecordT>
+      ProductT const& get(ESGetToken<ProductT, DepRecordT>& iToken) const {
+        return get(const_cast<ESGetToken<ProductT, DepRecordT> const&>(iToken));
+      }
+
       // ---------- static member functions --------------------
 
       // ---------- member functions ---------------------------
