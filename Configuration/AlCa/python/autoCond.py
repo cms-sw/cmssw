b7886d54bd218593ed43d60ffeedfaa7a6b013c3
tocheng <tocheng.cms@gmail.com>
2019-04-16 16:58:00 +0200

    Revert "Fix PPS Geometry and add LHC optical function"
    This reverts commit de91ae6152a0fbfd2d29d1a269cd3c4fe28a6b24.

--- Configuration/AlCa/python/autoCond.py
+++ Configuration/AlCa/python/autoCond.py
@@ -24,16 +24,16 @@ autoCond = {
     # GlobalTag for MC production (p-Pb collisions) with realistic alignment and calibrations for Run2
     'run2_mc_pa'        :   '106X_mcRun2_pA_v2',
     # GlobalTag for Run1 data reprocessing
-    'run1_data'         :   '106X_dataRun2_v7',
+    'run1_data'         :   '106X_dataRun2_v6',
     # GlobalTag for Run2 data reprocessing
-    'run2_data'         :   '106X_dataRun2_v7',
+    'run2_data'         :   '106X_dataRun2_v6',
     # GlobalTag for Run2 data relvals: allows customization to run with fixed L1 menu
-    'run2_data_relval'  :   '106X_dataRun2_relval_v5',
+    'run2_data_relval'  :   '106X_dataRun2_relval_v4',
     # GlobalTag for Run2 data 2018B relvals only: HEM-15-16 fail
-    'run2_data_promptlike_HEfail' : '106X_dataRun2_PromptLike_HEfail_v3',
+    'run2_data_promptlike_HEfail' : '106X_dataRun2_PromptLike_HEfail_v2',
     # GlobalTag for Run2 data 2016H relvals only: Prompt Conditions + fixed L1 menu (to be removed)
-    'run2_data_promptlike'    : '106X_dataRun2_PromptLike_v3',
-    'run2_data_promptlike_hi' : '106X_dataRun2_PromptLike_HI_v3',
+    'run2_data_promptlike'    : '106X_dataRun2_PromptLike_v2',
+    'run2_data_promptlike_hi' : '106X_dataRun2_PromptLike_HI_v2',
     # GlobalTag for Run1 HLT: it points to the online GT
     'run1_hlt'          :   '101X_dataRun2_HLT_frozen_v9',
     # GlobalTag for Run2 HLT: it points to the online GT
