4ea7a7cc4d59561c452d2af3815c7607ac831e8c
Massimo <massimo.casarsa@ts.infn.it>
2019-04-16 16:52:28 +0200

    clang-formatted

--- Validation/MtdValidation/plugins/BtlRecHitsValidation.cc
+++ Validation/MtdValidation/plugins/BtlRecHitsValidation.cc
@@ -25,7 +25,6 @@
 #include "DataFormats/ForwardDetId/interface/BTLDetId.h"
 #include "DataFormats/FTLRecHit/interface/FTLRecHitCollections.h"
 
-
 #include "Geometry/Records/interface/MTDDigiGeometryRecord.h"
 #include "Geometry/Records/interface/MTDTopologyRcd.h"
 #include "Geometry/MTDGeometryBuilder/interface/MTDGeometry.h"
@@ -34,20 +33,15 @@
 #include "Geometry/MTDGeometryBuilder/interface/ProxyMTDTopology.h"
 #include "Geometry/MTDGeometryBuilder/interface/RectangularMTDTopology.h"
 
-
 class BtlRecHitsValidation : public DQMEDAnalyzer {
-
 public:
   explicit BtlRecHitsValidation(const edm::ParameterSet&);
   ~BtlRecHitsValidation() override;
 
   static void fillDescriptions(edm::ConfigurationDescriptions& descriptions);
 
-
 private:
-  void bookHistograms(DQMStore::IBooker &,
-		      edm::Run const&,
-		      edm::EventSetup const&) override;
+  void bookHistograms(DQMStore::IBooker&, edm::Run const&, edm::EventSetup const&) override;
 
   void analyze(const edm::Event&, const edm::EventSetup&) override;
 
@@ -79,28 +73,21 @@ private:
   MonitorElement* meHitTvsPhi_;
   MonitorElement* meHitTvsEta_;
   MonitorElement* meHitTvsZ_;
-
 };
 
-
 // ------------ constructor and destructor --------------
-BtlRecHitsValidation::BtlRecHitsValidation(const edm::ParameterSet& iConfig):
-  folder_(iConfig.getParameter<std::string>("folder")) {
-
+BtlRecHitsValidation::BtlRecHitsValidation(const edm::ParameterSet& iConfig)
+    : folder_(iConfig.getParameter<std::string>("folder")) {
   const std::string infoLabel = iConfig.getParameter<std::string>("moduleLabel");
   const std::string btlRecHitsCollection = iConfig.getParameter<std::string>("btlRecHitsCollection");
 
   btlRecHitsToken_ = consumes<FTLRecHitCollection>(iConfig.getParameter<edm::InputTag>("inputTag"));
-
 }
 
-BtlRecHitsValidation::~BtlRecHitsValidation() {
-}
-
+BtlRecHitsValidation::~BtlRecHitsValidation() {}
 
 // ------------ method called for each event  ------------
 void BtlRecHitsValidation::analyze(const edm::Event& iEvent, const edm::EventSetup& iSetup) {
-
   using namespace edm;
 
   edm::ESHandle<MTDGeometry> geometryHandle;
@@ -117,26 +104,24 @@ void BtlRecHitsValidation::analyze(const edm::Event& iEvent, const edm::EventSet
 
   unsigned int n_reco_btl = 0;
 
-  for (const auto& recHit: *btlRecHitsHandle) {
-
+  for (const auto& recHit : *btlRecHitsHandle) {
     BTLDetId detId = recHit.id();
-    DetId geoId = detId.geographicalId( static_cast<BTLDetId::CrysLayout>(topology->getMTDTopologyMode()) );
+    DetId geoId = detId.geographicalId(static_cast<BTLDetId::CrysLayout>(topology->getMTDTopologyMode()));
     const MTDGeomDet* thedet = geom->idToDet(geoId);
-    if( thedet == nullptr )
-      throw cms::Exception("BtlRecHitsValidation") << "GeographicalID: " << std::hex << geoId.rawId()
-						   << " (" << detId.rawId()<< ") is invalid!" << std::dec
-						   << std::endl;
+    if (thedet == nullptr)
+      throw cms::Exception("BtlRecHitsValidation") << "GeographicalID: " << std::hex << geoId.rawId() << " ("
+                                                   << detId.rawId() << ") is invalid!" << std::dec << std::endl;
     const ProxyMTDTopology& topoproxy = static_cast<const ProxyMTDTopology&>(thedet->topology());
     const RectangularMTDTopology& topo = static_cast<const RectangularMTDTopology&>(topoproxy.specificTopology());
 
-    Local3DPoint local_point(0.,0.,0.);
-    local_point = topo.pixelToModuleLocalPoint(local_point,detId.row(topo.nrows()),detId.column(topo.nrows()));
+    Local3DPoint local_point(0., 0., 0.);
+    local_point = topo.pixelToModuleLocalPoint(local_point, detId.row(topo.nrows()), detId.column(topo.nrows()));
     const auto& global_point = thedet->toGlobal(local_point);
 
     meHitEnergy_->Fill(recHit.energy());
     meHitTime_->Fill(recHit.time());
 
-    meOccupancy_->Fill(global_point.z(),global_point.phi());
+    meOccupancy_->Fill(global_point.z(), global_point.phi());
 
     meHitX_->Fill(global_point.x());
     meHitY_->Fill(global_point.y());
@@ -144,76 +129,67 @@ void BtlRecHitsValidation::analyze(const edm::Event& iEvent, const edm::EventSet
     meHitPhi_->Fill(global_point.phi());
     meHitEta_->Fill(global_point.eta());
 
-    meHitTvsE_->Fill(recHit.energy(),recHit.time());
-    meHitEvsPhi_->Fill(global_point.phi(),recHit.energy());
-    meHitEvsEta_->Fill(global_point.eta(),recHit.energy());
-    meHitEvsZ_->Fill(global_point.z(),recHit.energy());
-    meHitTvsPhi_->Fill(global_point.phi(),recHit.time());
-    meHitTvsEta_->Fill(global_point.eta(),recHit.time());
-    meHitTvsZ_->Fill(global_point.z(),recHit.time());
+    meHitTvsE_->Fill(recHit.energy(), recHit.time());
+    meHitEvsPhi_->Fill(global_point.phi(), recHit.energy());
+    meHitEvsEta_->Fill(global_point.eta(), recHit.energy());
+    meHitEvsZ_->Fill(global_point.z(), recHit.energy());
+    meHitTvsPhi_->Fill(global_point.phi(), recHit.time());
+    meHitTvsEta_->Fill(global_point.eta(), recHit.time());
+    meHitTvsZ_->Fill(global_point.z(), recHit.time());
 
     n_reco_btl++;
 
-  } // recHit loop
+  }  // recHit loop
 
   meNhits_->Fill(n_reco_btl);
-
 }
 
-
 // ------------ method for histogram booking ------------
-void BtlRecHitsValidation::bookHistograms(DQMStore::IBooker & ibook,
-                               edm::Run const& run,
-                               edm::EventSetup const & iSetup) {
-
+void BtlRecHitsValidation::bookHistograms(DQMStore::IBooker& ibook,
+                                          edm::Run const& run,
+                                          edm::EventSetup const& iSetup) {
   ibook.setCurrentFolder(folder_);
 
   // --- histograms booking
 
-  meNhits_     = ibook.book1D("BtlNhits", "Number of BTL RECO hits;N_{RECO}", 100, 0., 5000.);
+  meNhits_ = ibook.book1D("BtlNhits", "Number of BTL RECO hits;N_{RECO}", 100, 0., 5000.);
 
   meHitEnergy_ = ibook.book1D("BtlHitEnergy", "BTL RECO hits energy;E_{RECO} [MeV]", 100, 0., 20.);
-  meHitTime_   = ibook.book1D("BtlHitTime", "BTL RECO hits ToA;ToA_{RECO} [ns]", 100, 0., 25.);
+  meHitTime_ = ibook.book1D("BtlHitTime", "BTL RECO hits ToA;ToA_{RECO} [ns]", 100, 0., 25.);
 
-  meOccupancy_ = ibook.book2D("BtlOccupancy","BTL RECO hits occupancy;Z_{RECO} [cm]; #phi_{RECO} [rad]",
-			      65, -260., 260., 126, -3.15, 3.15 );
+  meOccupancy_ = ibook.book2D(
+      "BtlOccupancy", "BTL RECO hits occupancy;Z_{RECO} [cm]; #phi_{RECO} [rad]", 65, -260., 260., 126, -3.15, 3.15);
 
-  meHitX_      = ibook.book1D("BtlHitX", "BTL RECO hits X;X_{RECO} [cm]", 60, -120., 120.);
-  meHitY_      = ibook.book1D("BtlHitY", "BTL RECO hits Y;Y_{RECO} [cm]", 60, -120., 120.);
-  meHitZ_      = ibook.book1D("BtlHitZ", "BTL RECO hits Z;Z_{RECO} [cm]", 100, -260., 260.);
-  meHitPhi_    = ibook.book1D("BtlHitPhi", "BTL RECO hits #phi;#phi_{RECO} [rad]", 126, -3.15, 3.15);
-  meHitEta_    = ibook.book1D("BtlHitEta", "BTL RECO hits #eta;#eta_{RECO}", 100, -1.55, 1.55);
+  meHitX_ = ibook.book1D("BtlHitX", "BTL RECO hits X;X_{RECO} [cm]", 60, -120., 120.);
+  meHitY_ = ibook.book1D("BtlHitY", "BTL RECO hits Y;Y_{RECO} [cm]", 60, -120., 120.);
+  meHitZ_ = ibook.book1D("BtlHitZ", "BTL RECO hits Z;Z_{RECO} [cm]", 100, -260., 260.);
+  meHitPhi_ = ibook.book1D("BtlHitPhi", "BTL RECO hits #phi;#phi_{RECO} [rad]", 126, -3.15, 3.15);
+  meHitEta_ = ibook.book1D("BtlHitEta", "BTL RECO hits #eta;#eta_{RECO}", 100, -1.55, 1.55);
 
-  meHitTvsE_   = ibook.bookProfile("BtlHitTvsE", "BTL RECO ToA vs energy;E_{RECO} [MeV];ToA_{RECO} [ns]",
-				   50, 0., 20., 0., 100.);
-  meHitEvsPhi_ = ibook.bookProfile("BtlHitEvsPhi", "BTL RECO energy vs #phi;#phi_{RECO} [rad];E_{RECO} [MeV]",
-				   50, -3.15, 3.15, 0., 100.);
-  meHitEvsEta_ = ibook.bookProfile("BtlHitEvsEta","BTL RECO energy vs #eta;#eta_{RECO};E_{RECO} [MeV]",
-				   50, -1.55, 1.55, 0., 100.);
-  meHitEvsZ_   = ibook.bookProfile("BtlHitEvsZ","BTL RECO energy vs Z;Z_{RECO} [cm];E_{RECO} [MeV]",
-				   50, -260., 260., 0., 100.);
-  meHitTvsPhi_ = ibook.bookProfile("BtlHitTvsPhi", "BTL RECO ToA vs #phi;#phi_{RECO} [rad];ToA_{RECO} [ns]",
-				   50, -3.15, 3.15, 0., 100.);
-  meHitTvsEta_ = ibook.bookProfile("BtlHitTvsEta","BTL RECO ToA vs #eta;#eta_{RECO};ToA_{RECO} [ns]",
-				   50, -1.6, 1.6, 0., 100.);
-  meHitTvsZ_   = ibook.bookProfile("BtlHitTvsZ","BTL RECO ToA vs Z;Z_{RECO} [cm];ToA_{RECO} [ns]",
-				   50, -260., 260., 0., 100.);
-
-
+  meHitTvsE_ =
+      ibook.bookProfile("BtlHitTvsE", "BTL RECO ToA vs energy;E_{RECO} [MeV];ToA_{RECO} [ns]", 50, 0., 20., 0., 100.);
+  meHitEvsPhi_ = ibook.bookProfile(
+      "BtlHitEvsPhi", "BTL RECO energy vs #phi;#phi_{RECO} [rad];E_{RECO} [MeV]", 50, -3.15, 3.15, 0., 100.);
+  meHitEvsEta_ = ibook.bookProfile(
+      "BtlHitEvsEta", "BTL RECO energy vs #eta;#eta_{RECO};E_{RECO} [MeV]", 50, -1.55, 1.55, 0., 100.);
+  meHitEvsZ_ =
+      ibook.bookProfile("BtlHitEvsZ", "BTL RECO energy vs Z;Z_{RECO} [cm];E_{RECO} [MeV]", 50, -260., 260., 0., 100.);
+  meHitTvsPhi_ = ibook.bookProfile(
+      "BtlHitTvsPhi", "BTL RECO ToA vs #phi;#phi_{RECO} [rad];ToA_{RECO} [ns]", 50, -3.15, 3.15, 0., 100.);
+  meHitTvsEta_ =
+      ibook.bookProfile("BtlHitTvsEta", "BTL RECO ToA vs #eta;#eta_{RECO};ToA_{RECO} [ns]", 50, -1.6, 1.6, 0., 100.);
+  meHitTvsZ_ =
+      ibook.bookProfile("BtlHitTvsZ", "BTL RECO ToA vs Z;Z_{RECO} [cm];ToA_{RECO} [ns]", 50, -260., 260., 0., 100.);
 }
-
-
 
 // ------------ method fills 'descriptions' with the allowed parameters for the module  ------------
 void BtlRecHitsValidation::fillDescriptions(edm::ConfigurationDescriptions& descriptions) {
-
   edm::ParameterSetDescription desc;
 
   desc.add<std::string>("folder", "MTD/BTL/RecHits");
   desc.add<edm::InputTag>("inputTag", edm::InputTag("mtdRecHits", "FTLBarrel"));
 
   descriptions.add("btlRecHits", desc);
-
 }
 
 DEFINE_FWK_MODULE(BtlRecHitsValidation);
--- Validation/MtdValidation/plugins/BtlSimHitsValidation.cc
+++ Validation/MtdValidation/plugins/BtlSimHitsValidation.cc
@@ -35,7 +35,6 @@
 #include "Geometry/MTDGeometryBuilder/interface/ProxyMTDTopology.h"
 #include "Geometry/MTDGeometryBuilder/interface/RectangularMTDTopology.h"
 
-
 struct MTDHit {
   float energy;
   float time;
@@ -44,20 +43,15 @@ struct MTDHit {
   float z;
 };
 
-
 class BtlSimHitsValidation : public DQMEDAnalyzer {
-
 public:
   explicit BtlSimHitsValidation(const edm::ParameterSet&);
   ~BtlSimHitsValidation() override;
 
   static void fillDescriptions(edm::ConfigurationDescriptions& descriptions);
 
-
 private:
-  void bookHistograms(DQMStore::IBooker &,
-		      edm::Run const&,
-		      edm::EventSetup const&) override;
+  void bookHistograms(DQMStore::IBooker&, edm::Run const&, edm::EventSetup const&) override;
 
   void analyze(const edm::Event&, const edm::EventSetup&) override;
 
@@ -96,26 +90,19 @@ private:
   MonitorElement* meHitTvsPhi_;
   MonitorElement* meHitTvsEta_;
   MonitorElement* meHitTvsZ_;
-
 };
 
-
 // ------------ constructor and destructor --------------
-BtlSimHitsValidation::BtlSimHitsValidation(const edm::ParameterSet& iConfig):
-  folder_(iConfig.getParameter<std::string>("folder")),
-  hitMinEnergy_( iConfig.getParameter<double>("hitMinimumEnergy") ) {
-
+BtlSimHitsValidation::BtlSimHitsValidation(const edm::ParameterSet& iConfig)
+    : folder_(iConfig.getParameter<std::string>("folder")),
+      hitMinEnergy_(iConfig.getParameter<double>("hitMinimumEnergy")) {
   btlSimHitsToken_ = consumes<edm::PSimHitContainer>(iConfig.getParameter<edm::InputTag>("inputTag"));
-
 }
 
-BtlSimHitsValidation::~BtlSimHitsValidation() {
-}
-
+BtlSimHitsValidation::~BtlSimHitsValidation() {}
 
 // ------------ method called for each event  ------------
 void BtlSimHitsValidation::analyze(const edm::Event& iEvent, const edm::EventSetup& iSetup) {
-
   using namespace edm;
   using namespace geant_units::operators;
 
@@ -133,34 +120,31 @@ void BtlSimHitsValidation::analyze(const edm::Event& iEvent, const edm::EventSet
   std::unordered_map<uint32_t, std::set<int> > m_btlTrkPerCell;
 
   // --- Loop over the BLT SIM hits
-  for (auto const& simHit: *btlSimHitsHandle) {
-
+  for (auto const& simHit : *btlSimHitsHandle) {
     // --- Use only hits compatible with the in-time bunch-crossing
-    if ( simHit.tof() < 0 || simHit.tof() > 25. ) continue;
+    if (simHit.tof() < 0 || simHit.tof() > 25.)
+      continue;
 
     DetId id = simHit.detUnitId();
 
     m_btlTrkPerCell[id.rawId()].insert(simHit.trackId());
 
-    auto simHitIt = m_btlHits.emplace(id.rawId(),MTDHit()).first;
+    auto simHitIt = m_btlHits.emplace(id.rawId(), MTDHit()).first;
 
     // --- Accumulate the energy (in MeV) of SIM hits in the same detector cell
     (simHitIt->second).energy += convertUnitsTo(0.001_MeV, simHit.energyLoss());
 
     // --- Get the time of the first SIM hit in the cell
-    if( (simHitIt->second).time==0 || simHit.tof()<(simHitIt->second).time ) {
-
+    if ((simHitIt->second).time == 0 || simHit.tof() < (simHitIt->second).time) {
       (simHitIt->second).time = simHit.tof();
 
       auto hit_pos = simHit.entryPoint();
       (simHitIt->second).x = hit_pos.x();
       (simHitIt->second).y = hit_pos.y();
       (simHitIt->second).z = hit_pos.z();
-
     }
 
-  } // simHit loop
-
+  }  // simHit loop
 
   // ==============================================================================
   //  Histogram filling
@@ -168,28 +152,27 @@ void BtlSimHitsValidation::analyze(const edm::Event& iEvent, const edm::EventSet
 
   meNhits_->Fill(m_btlHits.size());
 
-  for (auto const& hit: m_btlTrkPerCell)
+  for (auto const& hit : m_btlTrkPerCell)
     meNtrkPerCell_->Fill((hit.second).size());
 
-
-  for (auto const& hit: m_btlHits) {
-
-    if ( (hit.second).energy < hitMinEnergy_ ) continue;
+  for (auto const& hit : m_btlHits) {
+    if ((hit.second).energy < hitMinEnergy_)
+      continue;
 
     // --- Get the SIM hit global position
-    BTLDetId detId(hit.first); 
-    DetId geoId = detId.geographicalId( static_cast<BTLDetId::CrysLayout>(topology->getMTDTopologyMode()) );
+    BTLDetId detId(hit.first);
+    DetId geoId = detId.geographicalId(static_cast<BTLDetId::CrysLayout>(topology->getMTDTopologyMode()));
     const MTDGeomDet* thedet = geom->idToDet(geoId);
-    if( thedet == nullptr )
-      throw cms::Exception("BtlSimHitsValidation") << "GeographicalID: " << std::hex << geoId.rawId()
-						   << " (" << detId.rawId()<< ") is invalid!" << std::dec
-						   << std::endl;
+    if (thedet == nullptr)
+      throw cms::Exception("BtlSimHitsValidation") << "GeographicalID: " << std::hex << geoId.rawId() << " ("
+                                                   << detId.rawId() << ") is invalid!" << std::dec << std::endl;
     const ProxyMTDTopology& topoproxy = static_cast<const ProxyMTDTopology&>(thedet->topology());
     const RectangularMTDTopology& topo = static_cast<const RectangularMTDTopology&>(topoproxy.specificTopology());
 
-    Local3DPoint local_point(convertMmToCm((hit.second).x),convertMmToCm((hit.second).y),convertMmToCm((hit.second).z));
+    Local3DPoint local_point(
+        convertMmToCm((hit.second).x), convertMmToCm((hit.second).y), convertMmToCm((hit.second).z));
 
-    local_point = topo.pixelToModuleLocalPoint(local_point,detId.row(topo.nrows()),detId.column(topo.nrows()));
+    local_point = topo.pixelToModuleLocalPoint(local_point, detId.row(topo.nrows()), detId.column(topo.nrows()));
     const auto& global_point = thedet->toGlobal(local_point);
 
     // --- Fill the histograms
@@ -200,84 +183,77 @@ void BtlSimHitsValidation::analyze(const edm::Event& iEvent, const edm::EventSet
     meHitYlocal_->Fill((hit.second).y);
     meHitZlocal_->Fill((hit.second).z);
 
-    meOccupancy_->Fill(global_point.z(),global_point.phi());
+    meOccupancy_->Fill(global_point.z(), global_point.phi());
 
-    meHitX_->Fill(global_point.x()); 
-    meHitY_->Fill(global_point.y()); 
-    meHitZ_->Fill(global_point.z()); 
-    meHitPhi_->Fill(global_point.phi()); 
-    meHitEta_->Fill(global_point.eta()); 
+    meHitX_->Fill(global_point.x());
+    meHitY_->Fill(global_point.y());
+    meHitZ_->Fill(global_point.z());
+    meHitPhi_->Fill(global_point.phi());
+    meHitEta_->Fill(global_point.eta());
 
-    meHitTvsE_->Fill((hit.second).energy,(hit.second).time);  
-    meHitEvsPhi_->Fill(global_point.phi(),(hit.second).energy);
-    meHitEvsEta_->Fill(global_point.eta(),(hit.second).energy);
-    meHitEvsZ_->Fill(global_point.z(),(hit.second).energy);
-    meHitTvsPhi_->Fill(global_point.phi(),(hit.second).time);
-    meHitTvsEta_->Fill(global_point.eta(),(hit.second).time);
-    meHitTvsZ_->Fill(global_point.z(),(hit.second).time);
+    meHitTvsE_->Fill((hit.second).energy, (hit.second).time);
+    meHitEvsPhi_->Fill(global_point.phi(), (hit.second).energy);
+    meHitEvsEta_->Fill(global_point.eta(), (hit.second).energy);
+    meHitEvsZ_->Fill(global_point.z(), (hit.second).energy);
+    meHitTvsPhi_->Fill(global_point.phi(), (hit.second).time);
+    meHitTvsEta_->Fill(global_point.eta(), (hit.second).time);
+    meHitTvsZ_->Fill(global_point.z(), (hit.second).time);
 
-  } // hit loop
-
+  }  // hit loop
 }
 
-
 // ------------ method for histogram booking ------------
-void BtlSimHitsValidation::bookHistograms(DQMStore::IBooker & ibook,
-					  edm::Run const& run,
-					  edm::EventSetup const & iSetup) {
-
+void BtlSimHitsValidation::bookHistograms(DQMStore::IBooker& ibook,
+                                          edm::Run const& run,
+                                          edm::EventSetup const& iSetup) {
   ibook.setCurrentFolder(folder_);
 
   // --- histograms booking
 
-  meNhits_       = ibook.book1D("BtlNhits", "Number of BTL cells with SIM hits;N_{BTL cells}", 100, 0., 5000.);
+  meNhits_ = ibook.book1D("BtlNhits", "Number of BTL cells with SIM hits;N_{BTL cells}", 100, 0., 5000.);
   meNtrkPerCell_ = ibook.book1D("BtlNtrkPerCell", "Number of tracks per BTL cell;N_{trk}", 10, 0., 10.);
 
-  meHitEnergy_   = ibook.book1D("BtlHitEnergy", "BTL SIM hits energy;E_{SIM} [MeV]", 100, 0., 20.);
-  meHitTime_     = ibook.book1D("BtlHitTime", "BTL SIM hits ToA;ToA_{SIM} [ns]", 100, 0., 25.);
+  meHitEnergy_ = ibook.book1D("BtlHitEnergy", "BTL SIM hits energy;E_{SIM} [MeV]", 100, 0., 20.);
+  meHitTime_ = ibook.book1D("BtlHitTime", "BTL SIM hits ToA;ToA_{SIM} [ns]", 100, 0., 25.);
 
-  meHitXlocal_   = ibook.book1D("BtlHitXlocal", "BTL SIM local X;X_{SIM}^{LOC} [mm]", 100, -1.65, 1.65);
-  meHitYlocal_   = ibook.book1D("BtlHitYlocal", "BTL SIM local Y;Y_{SIM}^{LOC} [mm]", 100, -30., 30.);
-  meHitZlocal_   = ibook.book1D("BtlHitZlocal", "BTL SIM local z;z_{SIM}^{LOC} [mm]", 100, -2., 2.);
+  meHitXlocal_ = ibook.book1D("BtlHitXlocal", "BTL SIM local X;X_{SIM}^{LOC} [mm]", 100, -1.65, 1.65);
+  meHitYlocal_ = ibook.book1D("BtlHitYlocal", "BTL SIM local Y;Y_{SIM}^{LOC} [mm]", 100, -30., 30.);
+  meHitZlocal_ = ibook.book1D("BtlHitZlocal", "BTL SIM local z;z_{SIM}^{LOC} [mm]", 100, -2., 2.);
 
-  meOccupancy_   = ibook.book2D("BtlOccupancy","BTL SIM hits occupancy;z_{SIM} [cm];#phi_{SIM} [rad]",
-				130, -260., 260., 200, -3.15, 3.15 );
+  meOccupancy_ = ibook.book2D(
+      "BtlOccupancy", "BTL SIM hits occupancy;z_{SIM} [cm];#phi_{SIM} [rad]", 130, -260., 260., 200, -3.15, 3.15);
 
-  meHitX_        = ibook.book1D("BtlHitX", "BTL SIM hits X;X_{SIM} [cm]", 100, -120., 120.);
-  meHitY_        = ibook.book1D("BtlHitY", "BTL SIM hits Y;Y_{SIM} [cm]", 100, -120., 120.);
-  meHitZ_        = ibook.book1D("BtlHitZ", "BTL SIM hits Z;Z_{SIM} [cm]", 100, -260., 260.);
-  meHitPhi_      = ibook.book1D("BtlHitPhi", "BTL SIM hits #phi;#phi_{SIM} [rad]", 200, -3.15, 3.15);
-  meHitEta_      = ibook.book1D("BtlHitEta", "BTL SIM hits #eta;#eta_{SIM}", 100, -1.55, 1.55);
+  meHitX_ = ibook.book1D("BtlHitX", "BTL SIM hits X;X_{SIM} [cm]", 100, -120., 120.);
+  meHitY_ = ibook.book1D("BtlHitY", "BTL SIM hits Y;Y_{SIM} [cm]", 100, -120., 120.);
+  meHitZ_ = ibook.book1D("BtlHitZ", "BTL SIM hits Z;Z_{SIM} [cm]", 100, -260., 260.);
+  meHitPhi_ = ibook.book1D("BtlHitPhi", "BTL SIM hits #phi;#phi_{SIM} [rad]", 200, -3.15, 3.15);
+  meHitEta_ = ibook.book1D("BtlHitEta", "BTL SIM hits #eta;#eta_{SIM}", 100, -1.55, 1.55);
 
-  meHitTvsE_     = ibook.bookProfile("BtlHitTvsE", "BTL SIM time vs energy;E_{SIM} [MeV];T_{SIM} [ns]",
-				     50, 0., 20., 0., 100.);
-  meHitEvsPhi_   = ibook.bookProfile("BtlHitEvsPhi", "BTL SIM energy vs #phi;#phi_{SIM} [rad];E_{SIM} [MeV]",
-				     50, -3.15, 3.15, 0., 100.);
-  meHitEvsEta_   = ibook.bookProfile("BtlHitEvsEta","BTL SIM energy vs #eta;#eta_{SIM};E_{SIM} [MeV]",
-				     50, -1.55, 1.55, 0., 100.);
-  meHitEvsZ_     = ibook.bookProfile("BtlHitEvsZ","BTL SIM energy vs Z;Z_{SIM} [cm];E_{SIM} [MeV]",
-				     50, -260., 260., 0., 100.);
-  meHitTvsPhi_   = ibook.bookProfile("BtlHitTvsPhi", "BTL SIM time vs #phi;#phi_{SIM} [rad];T_{SIM} [ns]",
-				     50, -3.15, 3.15, 0., 100.);
-  meHitTvsEta_   = ibook.bookProfile("BtlHitTvsEta","BTL SIM time vs #eta;#eta_{SIM};T_{SIM} [ns]",
-				     50, -1.55, 1.55, 0., 100.);
-  meHitTvsZ_     = ibook.bookProfile("BtlHitTvsZ","BTL SIM time vs Z;Z_{SIM} [cm];T_{SIM} [ns]",
-				     50, -260., 260., 0., 100.);
-
+  meHitTvsE_ =
+      ibook.bookProfile("BtlHitTvsE", "BTL SIM time vs energy;E_{SIM} [MeV];T_{SIM} [ns]", 50, 0., 20., 0., 100.);
+  meHitEvsPhi_ = ibook.bookProfile(
+      "BtlHitEvsPhi", "BTL SIM energy vs #phi;#phi_{SIM} [rad];E_{SIM} [MeV]", 50, -3.15, 3.15, 0., 100.);
+  meHitEvsEta_ =
+      ibook.bookProfile("BtlHitEvsEta", "BTL SIM energy vs #eta;#eta_{SIM};E_{SIM} [MeV]", 50, -1.55, 1.55, 0., 100.);
+  meHitEvsZ_ =
+      ibook.bookProfile("BtlHitEvsZ", "BTL SIM energy vs Z;Z_{SIM} [cm];E_{SIM} [MeV]", 50, -260., 260., 0., 100.);
+  meHitTvsPhi_ = ibook.bookProfile(
+      "BtlHitTvsPhi", "BTL SIM time vs #phi;#phi_{SIM} [rad];T_{SIM} [ns]", 50, -3.15, 3.15, 0., 100.);
+  meHitTvsEta_ =
+      ibook.bookProfile("BtlHitTvsEta", "BTL SIM time vs #eta;#eta_{SIM};T_{SIM} [ns]", 50, -1.55, 1.55, 0., 100.);
+  meHitTvsZ_ =
+      ibook.bookProfile("BtlHitTvsZ", "BTL SIM time vs Z;Z_{SIM} [cm];T_{SIM} [ns]", 50, -260., 260., 0., 100.);
 }
-
 
 // ------------ method fills 'descriptions' with the allowed parameters for the module  ------------
 void BtlSimHitsValidation::fillDescriptions(edm::ConfigurationDescriptions& descriptions) {
-
   edm::ParameterSetDescription desc;
 
   desc.add<std::string>("folder", "MTD/BTL/SimHits");
   desc.add<edm::InputTag>("inputTag", edm::InputTag("g4SimHits", "FastTimerHitsBarrel"));
-  desc.add<double>("hitMinimumEnergy",1.); // [MeV]
+  desc.add<double>("hitMinimumEnergy", 1.);  // [MeV]
 
   descriptions.add("btlSimHits", desc);
-
 }
 
 DEFINE_FWK_MODULE(BtlSimHitsValidation);
