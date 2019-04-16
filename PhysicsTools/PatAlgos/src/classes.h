cffc2aa999449ef95b6b53b2baeea9a898510f8a
Kenichi Hatakeyama <kenichi.hatakeyama@cern.ch>
2019-04-16 04:43:52 -0500

    Use HcalDepthEnergyFractionProd

--- PhysicsTools/PatAlgos/src/classes.h
+++ PhysicsTools/PatAlgos/src/classes.h
@@ -1,14 +1,23 @@
 #include "DataFormats/Common/interface/Wrapper.h"
 #include "DataFormats/Common/interface/ValueMap.h"
+#include "PhysicsTools/PatAlgos/interface/PATAuxiliaryProd.h"
 
 namespace PhysicsTools_PatAlgos {
-
+  
   struct dictionary {
     
     edm::Wrapper<edm::ValueMap<std::vector<uint8_t>> > dummy01;
     edm::Wrapper<edm::ValueMap<std::vector<unsigned char>> > dummy02;
-    //edm::Wrapper<edm::ValueMap<std::vector<SampleProd>> > dummy03;
+    edm::Wrapper<edm::ValueMap<pat::SampleProd> > dummy03;
+    edm::Wrapper<edm::ValueMap<pat::HcalDepthEnergyFractionProd> > dummy04;
 
+    vector<pat::HcalDepthEnergyFractionProd> dummy05;
+    vector<pat::SampleProd> dummy06;
+
+    edm::ValueMap<pat::HcalDepthEnergyFractionProd> dummy07;
+    edm::ValueMap<pat::SampleProd> dummy08;
+  
+    
   };
 
 }
