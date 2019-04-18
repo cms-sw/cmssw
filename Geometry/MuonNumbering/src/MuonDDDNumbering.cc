6fd1863f5eb470ca745a6822a65b1e70481e8559
Sunanda <sunanda.banerjee@cern.ch>
2019-04-18 20:38:39 +0200

    Use edm::Log in the code rather than cout

--- Geometry/MuonNumbering/src/MuonDDDNumbering.cc
+++ Geometry/MuonNumbering/src/MuonDDDNumbering.cc
@@ -1,8 +1,7 @@
 #include "Geometry/MuonNumbering/interface/MuonDDDNumbering.h"
 #include "Geometry/MuonNumbering/interface/MuonBaseNumber.h"
 #include "Geometry/MuonNumbering/interface/MuonDDDConstants.h"
-
-#include <iostream>
+#include "FWCore/MessageLogger/interface/MessageLogger.h"
 
 //#define LOCAL_DEBUG
 
@@ -16,28 +15,28 @@ MuonDDDNumbering::MuonDDDNumbering( const MuonDDDConstants& muonConstants ){
   // some consistency checks
 
   if (theBasePart!=1) {
-    std::cout << "MuonDDDNumbering finds unusual base constant:"
-	 <<theBasePart<<std::endl;
+    edm::LogWarning("Geometry") 
+      << "MuonDDDNumbering finds unusual base constant:" << theBasePart;
   }
   if (theSuperPart<100) {
-    std::cout << "MuonDDDNumbering finds unusual super constant:"
-	 <<theSuperPart<<std::endl;
+    edm::LogWarning("Geometry") 
+      << "MuonDDDNumbering finds unusual super constant:" << theSuperPart;
   }
   if (theLevelPart<10*theSuperPart) {
-    std::cout << "MuonDDDNumbering finds unusual level constant:"
-	 <<theLevelPart<<std::endl;
+    edm::LogWarning("Geometry") 
+      << "MuonDDDNumbering finds unusual level constant:" << theLevelPart;
   }
   if ((theStartCopyNo!=0)&&(theStartCopyNo!=1)) {
-    std::cout << "MuonDDDNumbering finds unusual start value for copy numbers:"
-	 <<theStartCopyNo<<std::endl;
+    edm::LogWarning("Geometry") 
+      << "MuonDDDNumbering finds unusual start value for copy numbers:"
+      << theStartCopyNo;
   }
 
 #ifdef LOCAL_DEBUG
-  std::cout << "MuonDDDNumbering configured with"<<std::endl;
-  std::cout << "Level = "<<theLevelPart<<" ";
-  std::cout << "Super = "<<theSuperPart<<" ";
-  std::cout << "Base = "<<theBasePart<<" ";
-  std::cout << "StartCopyNo = "<<theStartCopyNo<<std::endl;
+  edm::LogVerbatim("Geometry")
+    << "MuonDDDNumbering configured with"
+    << " Level = " << theLevelPart << " Super = " << theSuperPart
+    << " Base = " << theBasePart << " StartCopyNo = " << theStartCopyNo;
 #endif
 
 }
@@ -46,8 +45,8 @@ MuonBaseNumber MuonDDDNumbering::geoHistoryToBaseNumber(const DDGeoHistory & his
   MuonBaseNumber num;
 
 #ifdef LOCAL_DEBUG
-  std::cout << "MuonDDDNumbering create MuonBaseNumber for"<<std::endl;
-  std::cout << history <<std::endl;
+  edm::LogVerbatim("Geometry") 
+    << "MuonDDDNumbering create MuonBaseNumber for " << history;
 #endif
 
   //loop over all parents and check
@@ -66,9 +65,10 @@ MuonBaseNumber MuonDDDNumbering::geoHistoryToBaseNumber(const DDGeoHistory & his
   }
 
 #ifdef LOCAL_DEBUG
-  std::cout << num.getLevels() <<std::endl;
+  edm::LogVerbatim("Geometry") << "MuonDDDNumbering::" <<  num.getLevels();
   for (int i=1;i<=num.getLevels();i++) {
-    std::cout << num.getSuperNo(i)<<" "<<num.getBaseNo(i)<<std::endl;
+    edm::LogVerbatim("Geometry") 
+      << "[" << i << "] " << num.getSuperNo(i) << " " << num.getBaseNo(i);
   }
 #endif
  
@@ -91,7 +91,9 @@ int MuonDDDNumbering::getInt(const std::string & s, const DDLogicalPart & part)
       std::vector<double> temp = val.doubles();
       if (temp.size() != 1)
       {
-	std::cout << " ERROR: I need only 1 " << s << " in DDLogicalPart " << part.name() << std::endl;
+	edm::LogError("Geometry") 
+	  << "MuonDDDNumbering:: ERROR: I need only 1 " << s 
+	  << " in DDLogicalPart " << part.name();
 	 abort();
       }      
       return int(temp[0]);
