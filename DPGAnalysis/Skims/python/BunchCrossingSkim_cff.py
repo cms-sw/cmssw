b2918c214b5eb583acd2b5b7ffce8168e5f18775
Mohamed <mohamed.anwar@cern.ch>
2019-04-24 22:54:34 +0200

    Add skim configuration depend on bunch crossing number

---
+++ DPGAnalysis/Skims/python/BunchCrossingSkim_cff.py
@@ -0,0 +1,23 @@
+import FWCore.ParameterSet.Config as cms
+
+from FWCore.Modules.bunchCrossingFilter_cfi import bunchCrossingFilter as _bunchCrossingFilter
+selectRange1 = _bunchCrossingFilter.clone(                                                                                                                                                           
+    bunches = cms.vuint32(range(757,759)) 
+)                                                                                                                                                                                                          
+
+selectSingle = _bunchCrossingFilter.clone(
+    bunches = cms.vuint32(755)
+)
+
+selectRange2 = _bunchCrossingFilter.clone(
+    bunches = cms.vuint32(range(1646,1651)) 
+)
+
+BXSeq1 = cms.Sequence( selectRange1 )
+BXSeq2 = cms.Sequence( selectSingle )
+BXSeq3 = cms.Sequence( selectRange2 )
+
+
+
+
+
