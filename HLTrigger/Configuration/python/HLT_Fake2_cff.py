dfc592dccad8bf68c096c67d4dc0345edc18418a
Martin Grunewald <Martin.Grunewald@cern.ch>
2019-04-26 14:55:05 +0200

    Migration of HLT to CMSSW_10_6_0_pre4 template

--- HLTrigger/Configuration/python/HLT_Fake2_cff.py
+++ HLTrigger/Configuration/python/HLT_Fake2_cff.py
@@ -1,13 +1,13 @@
 # hltGetConfiguration --cff --data /dev/CMSSW_10_6_0/Fake2 --type Fake2
 
-# /dev/CMSSW_10_6_0/Fake2/V2 (CMSSW_10_6_0_pre3_HLT1)
+# /dev/CMSSW_10_6_0/Fake2/V3 (CMSSW_10_6_0_pre4)
 
 import FWCore.ParameterSet.Config as cms
 
 fragment = cms.ProcessFragment( "HLT" )
 
 fragment.HLTConfigVersion = cms.PSet(
-  tableName = cms.string('/dev/CMSSW_10_6_0/Fake2/V2')
+  tableName = cms.string('/dev/CMSSW_10_6_0/Fake2/V3')
 )
 
 fragment.streams = cms.PSet(  A = cms.vstring( 'InitialPD' ) )
