8b0932a1edcb64d522cfd8b5c8ae7cf0099e0252
Marco Rovere <marco.rovere@cern.ch>
2019-04-26 10:58:33 +0200

    Modernize classes* files

--- RecoHGCal/TICL/src/classes.h
+++ RecoHGCal/TICL/src/classes.h
@@ -2,10 +2,3 @@
 #include "FWCore/Common/interface/EventBase.h"
 #include "RecoHGCal/TICL/interface/Trackster.h"
 
-namespace RecoHGCal_TICL {
-struct dictionary {
-  Trackster tr;
-  std::vector<Trackster> vtr;
-  edm::Wrapper<std::vector<Trackster> > edmwrtp;
-};
-}  // namespace RecoHGCal_TICL
