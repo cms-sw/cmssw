2259e45f362ce97c43adffd8dbcd9398e3830822
Jonas Rembser <jonas.rembser@cern.ch>
2019-04-27 13:44:09 +0200

    new FlaggedPtr class

---
+++ RecoParticleFlow/PFProducer/interface/FlaggedPtr.h
@@ -0,0 +1,14 @@
+template <typename T>
+class FlaggedPtr {
+public:
+  FlaggedPtr(T* pointer, bool flag) : pointer_(pointer), flag_(flag) {}
+  T& operator*() const { return *pointer_; }
+  T* operator->() const { return pointer_; }
+  T* get() const { return pointer_; }
+  bool flag() const { return flag_; }
+  void setFlag(bool flag) { flag_ = flag; }
+
+private:
+  T* pointer_;
+  bool flag_;
+};
