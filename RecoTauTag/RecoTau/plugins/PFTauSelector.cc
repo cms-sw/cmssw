d1d58d7f5a7a3ff497ce52c598cd03b0d4cfa04a
Jan Steggemann <jan.steggemann@cern.ch>
2019-04-30 10:39:18 +0200

    Move TauSelector to RecoTauTag/RecoTau

---
+++ RecoTauTag/RecoTau/plugins/PFTauSelector.cc
@@ -0,0 +1,10 @@
+#include "FWCore/PluginManager/interface/ModuleDef.h"
+#include "FWCore/Framework/interface/MakerMacros.h"
+
+#include "CommonTools/UtilAlgos/interface/ObjectSelectorStream.h"
+#include "RecoTauTag/RecoTau/plugins/PFTauSelectorDefinition.h"
+
+typedef ObjectSelectorStream<PFTauSelectorDefinition> PFTauSelector;
+
+DEFINE_FWK_MODULE(PFTauSelector);
+
