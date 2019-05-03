83fcc11d60fcca4abecbb856825a8685ff2c8eec
Cms Build <cmsbuild@cern.ch>
2019-05-04 00:08:56 +0200

    Clang-Format

--- SimCalorimetry/CastorSim/src/CastorElectronicsSim.h
+++ SimCalorimetry/CastorSim/src/CastorElectronicsSim.h
@@ -1,10 +1,10 @@
 #ifndef CastorSim_CastorElectronicsSim_h
 #define CastorSim_CastorElectronicsSim_h
-  
-  /** This class turns a CaloSamples, representing the analog
-      signal input to the readout electronics, into a
-      digitized data frame
-   */
+
+/** This class turns a CaloSamples, representing the analog
+    signal input to the readout electronics, into a
+    digitized data frame
+ */
 #include "CalibFormats/CaloObjects/interface/CaloSamples.h"
 
 class CastorDataFrame;
@@ -13,29 +13,29 @@ class CastorAmplifier;
 class CastorCoderFactory;
 
 namespace CLHEP {
-  class HepRandomEngine;
+class HepRandomEngine;
 }
 
 class CastorElectronicsSim {
 public:
-  CastorElectronicsSim(CastorAmplifier * amplifier, 
-                     const CastorCoderFactory * coderFactory);
+  CastorElectronicsSim(CastorAmplifier *amplifier,
+                       const CastorCoderFactory *coderFactory);
   ~CastorElectronicsSim();
 
-  void analogToDigital(CLHEP::HepRandomEngine*, CaloSamples & linearFrame, CastorDataFrame & result);
+  void analogToDigital(CLHEP::HepRandomEngine *, CaloSamples &linearFrame,
+                       CastorDataFrame &result);
 
   /// Things that need to be initialized every event
-  void newEvent(CLHEP::HepRandomEngine*);
+  void newEvent(CLHEP::HepRandomEngine *);
 
 private:
-  template<class Digi> void convert(CaloSamples & frame, Digi & result, CLHEP::HepRandomEngine*);
+  template <class Digi>
+  void convert(CaloSamples &frame, Digi &result, CLHEP::HepRandomEngine *);
 
-  CastorAmplifier * theAmplifier;
-  const CastorCoderFactory * theCoderFactory;
+  CastorAmplifier *theAmplifier;
+  const CastorCoderFactory *theCoderFactory;
 
   int theStartingCapId;
 };
 
-  
 #endif
-  
