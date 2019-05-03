aa58e4c2bbf005ba5db725aab8155090487e56b6
Ianna Osborne <ianna.osborne@cern.ch>
2019-05-03 12:58:36 +0200

    Review and fixes

--- Geometry/TrackerCommonData/plugins/dd4hep/DDTIDAxialCableAlgo.cc
+++ Geometry/TrackerCommonData/plugins/dd4hep/DDTIDAxialCableAlgo.cc
@@ -6,24 +6,27 @@
 #include <cmath>
 #include <algorithm>
 
-#include "FWCore/MessageLogger/interface/MessageLogger.h"
 #include "DD4hep/DetFactoryHelper.h"
 #include "DataFormats/Math/interface/GeantUnits.h"
 #include "DetectorDescription/DDCMS/interface/DDPlugins.h"
+#include "FWCore/MessageLogger/interface/MessageLogger.h"
 
+using namespace edm;
 using namespace dd4hep;
+using namespace cms;
 using namespace geant_units::operators;
 
 namespace {
-  static long algorithm(Detector& description,
+  static long algorithm(dd4hep::Detector& description,
                         cms::DDParsingContext& ctxt,
-                        xml_h e) {
-
+                        xml_h e,
+			SensitiveDetector& /* sens */) {
+    
     cms::DDNamespace ns(ctxt,e,true);
     cms::DDAlgoArguments args(ctxt,e);
-
-    LogDebug("TIDGeom") << "DDTIDAxialCableAlgo info: Creating an instance";
-
+    
+    LogVerbatim("TIDGeom") << "DDTIDAxialCableAlgo info: Creating an instance...";
+     
     double zBend = args.value<double>("ZBend");  //Start z (at bending)........
     double zEnd = args.value<double>("ZEnd");    //End   z             ........
     double rMin = args.value<double>("RMin");    //Minimum radius      ........
@@ -39,7 +42,7 @@ namespace {
     std::string matIn = args.value<std::string>("MaterialIn"); //Material name (for inner parts)
     std::string matOut = args.value<std::string>("MaterialOut"); //Material name (for outer part)
 
-    LogDebug("TIDGeom") << "DDTIDAxialCableAlgo debug: Parameters for creating "
+    LogVerbatim("TIDGeom") << "DDTIDAxialCableAlgo debug: Parameters for creating "
                         << (zposWheel.size()+2) << " axial cables and positioning"
                         << angles.size() << "copies in Service volume"
                         << "\nzBend " << zBend
@@ -50,31 +53,31 @@ namespace {
                         << "\nthickness " << thick << " with Angles";
 
     for ( int i=0; i < (int)(angles.size()); i++)
-      LogDebug("TIDGeom") << "\n\tangles[" << i << "] = "
+      LogVerbatim("TIDGeom") << "\n\tangles[" << i << "] = "
                           << convertRadToDeg(angles[i]);
 
-    LogDebug("TIDGeom") << "\nWheels "
+    LogVerbatim("TIDGeom") << "\nWheels "
                         << zposWheel.size() << " at Z";
 
     for ( int i=0; i<(int)(zposWheel.size()); i++)
-      LogDebug("TIDGeom") << "\n\tzposWheel[" << i <<"] = "
+      LogVerbatim("TIDGeom") << "\n\tzposWheel[" << i <<"] = "
                           << zposWheel[i];
 
-    LogDebug("TIDGeom") << "\neach with "
+    LogVerbatim("TIDGeom") << "\neach with "
                         << zposRing.size() << " Rings at Z";
 
     for (int i=0; i<(int)(zposRing.size()); i++)
-      LogDebug("TIDGeom") << "\tzposRing[" << i <<"] = " << zposRing[i];
+      LogVerbatim("TIDGeom") << "\tzposRing[" << i <<"] = " << zposRing[i];
 
-    Volume mother = ns.addVolume(args.parentName());
+    Volume mother = ns.volume(args.parentName());
 
-    LogDebug("TIDGeom") << "DDTIDAxialCableAlgo debug: Parent " << mother.name()
+    LogVerbatim("TIDGeom") << "DDTIDAxialCableAlgo debug: Parent " << mother.name()
                         << "\tChild " << childName
                         << " NameSpace " << ns.name()
                         << "\tMaterial " << matIn << " and " << matOut;
 
 
-    std::vector<std::string> logs;
+    std::vector<Volume> logs;
     double thk = thick/zposRing.size();
     double r = rMin;
     double thktot = 0;
@@ -127,7 +130,7 @@ namespace {
                                          pconRmin, pconRmax, pconZ)
                                 );
 
-      LogDebug("TIDGeom") << "DDTIDAxialCableAlgo test: "
+      LogVerbatim("TIDGeom") << "DDTIDAxialCableAlgo test: "
                           << solid.name()
                           << " Polycone made of " << matIn
                           << " from " << convertRadToDeg(-0.5*width)
@@ -136,19 +139,20 @@ namespace {
 
 
       for (int i = 0; i <(int)(pconZ.size()); i++)
-        LogDebug("TIDGeom") <<  "\t[" << i  << "]\tZ = " << pconZ[i]
+        LogVerbatim("TIDGeom") <<  "\t[" << i  << "]\tZ = " << pconZ[i]
                             << "\tRmin = "<< pconRmin[i]
                             << "\tRmax = " << pconRmax[i];
 
-      logs.emplace_back(solid.name());
-
+      logs.emplace_back(Volume(solid.name(),
+			       solid,
+			       ns.material("materials:Air")));
     }
 
     //Cable in the vertical part
     std::vector<double> pconZ, pconRmin, pconRmax;
     r = thktot*rMax/rTop;
     z = zBend - thktot;
-    LogDebug("TIDGeom") << "DDTIDAxialCableAlgo test: Thk " << thk
+    LogVerbatim("TIDGeom") << "DDTIDAxialCableAlgo test: Thk " << thk
                         << " Total " << thktot << " rMax " << rMax
                         << " rTop " << rTop << " dR " << r << " z " << z;
 
@@ -174,7 +178,7 @@ namespace {
                                        pconRmin, pconRmax, pconZ)
                               );
 
-    LogDebug("TIDGeom") << "DDTIDAxialCableAlgo test: "
+    LogVerbatim("TIDGeom") << "DDTIDAxialCableAlgo test: "
                         << solid.name()
                         << " Polycone made of "<< matIn
                         << " from " << convertRadToDeg(-0.5*width)
@@ -183,17 +187,13 @@ namespace {
 
 
     for (int i = 0; i < (int)(pconZ.size()); i++)
-      LogDebug("TIDGeom") << "\t[" << i << "]\tZ = " << pconZ[i]
+      LogVerbatim("TIDGeom") << "\t[" << i << "]\tZ = " << pconZ[i]
                           << "\tRmin = " << pconRmin[i]
                           << "\tRmax = " << pconRmax[i];
 
-
-    Volume genLogic = ns.addVolume(Volume(solid.name(),
-                                          solid,
-                                          ns.material(matIn))
-                                   );
-
-    logs.emplace_back(solid.name());
+    logs.emplace_back(Volume(solid.name(),
+			     solid,
+			     ns.material(matIn)));
 
     //Cable in the outer part
     name = childName + std::to_string(zposWheel.size()+1);
@@ -204,7 +204,7 @@ namespace {
                              -0.5*width,width)
                         );
 
-    LogDebug("TIDGeom") << "DDTIDAxialCableAlgo test: "
+    LogVerbatim("TIDGeom") << "DDTIDAxialCableAlgo test: "
                         << solid.name()
                         << " Tubs made of " << matOut
                         << " from " << convertRadToDeg(-0.5*width)
@@ -213,12 +213,9 @@ namespace {
                         << " Rout " << rTop
                         << " ZHalf " << 0.5*(zEnd-zBend);
 
-    genLogic = ns.addVolume(Volume(solid.name(),
-                                   solid,
-                                   ns.material(matOut))
-                            );
-
-    logs.emplace_back(solid.name());
+    logs.emplace_back(Volume(solid.name(),
+			     solid,
+			     ns.material(matOut)));
 
     //Position the cables
     double theta = 90_deg;
@@ -226,36 +223,32 @@ namespace {
     for ( int i=0; i<(int)(angles.size()); i++) {
       double phix = angles[i];
       double phiy = phix + 90_deg;
-      double phideg = convertRadToDeg(phix);
 
-      Rotation3D rotation;
-      if (phideg != 0) {
-        std::string rotstr = childName + std::to_string(phideg*10.);
-        rotation = Rotation3D();
-        LogDebug("TIDGeom") << "DDTIDAxialCableAlgo test: Creating a new "
-                            << "rotation: " << rotstr << " "
-                            << convertRadToDeg(theta) << ", "
-                            << convertRadToDeg(phix) << ", "
-                            << convertRadToDeg(theta) << ", "
-                            << convertRadToDeg(phiy) << ", 0, 0";
-
-        rotation = cms::makeRotation3D( theta, phix, theta, phiy, 0., 0. );
-      }
+      Rotation3D rotation = cms::makeRotation3D( theta, phix, theta, phiy, 0., 0. );
+      LogVerbatim("TIDGeom") << "DDTIDAxialCableAlgo test: Creating a new "
+			     << "rotation: "
+			     << convertRadToDeg(theta) << ", "
+			     << convertRadToDeg(phix) << ", "
+			     << convertRadToDeg(theta) << ", "
+			     << convertRadToDeg(phiy) << ", 0, 0";
 
       for (int k=0; k<(int)(logs.size()); k++) {
-
         Position tran = Position(0.,0.,0.);
-
+	
         if (k == ((int)(logs.size())-1))
           tran = Position(0.,0.,0.5*(zEnd+zBend));
+	
+	mother.placeVolume(logs[k], i+1, Transform3D(rotation, tran));
 
-        LogDebug("TIDGeom") << "DDTIDAxialCableAlgo test " << logs[k]
-                            << " number " << i+1
-                            << " positioned in " << mother << " at " << tran
-                            << " with " << rotation;
+	LogVerbatim("TIDGeom") << "DDTIDAxialCableAlgo test " << logs[k].name()
+			       << " number " << i+1
+			       << " positioned in " << mother.name() << " at " << tran
+			       << " with " << rotation;
       }
-    }
-
-    return 1;
+    } 
+    return s_executed;
   }
 }
+
+// first argument is the type from the xml file
+DECLARE_DDCMS_DETELEMENT(DDCMS_track_DDTIDAxialCableAlgo,algorithm)
