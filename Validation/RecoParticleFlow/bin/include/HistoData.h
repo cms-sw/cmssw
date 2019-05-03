34bb058071fceafcd0f219ceadf679849ae15e85
Cms Build <cmsbuild@cern.ch>
2019-05-04 00:48:18 +0200

    Clang-Format

--- Validation/RecoParticleFlow/bin/include/HistoData.h
+++ Validation/RecoParticleFlow/bin/include/HistoData.h
@@ -1,22 +1,22 @@
 #ifndef HISTO_DATA__H
 #define HISTO_DATA__H
 
-#include <string>
-#include <cstring>
-#include <TH1.h>
 #include <TFile.h>
-
+#include <TH1.h>
+#include <cstring>
+#include <string>
 
 class HistoData {
 public:
-
-  HistoData(std::string Name, int PlotType, int Bin, std::string NewPath, TFile *NewFile, std::string RefPath, TFile *RefFile);
-  HistoData(std::string Name, int PlotType, int Bin, TH1 *NewHisto, TH1 *RefHisto);
+  HistoData(std::string Name, int PlotType, int Bin, std::string NewPath,
+            TFile *NewFile, std::string RefPath, TFile *RefFile);
+  HistoData(std::string Name, int PlotType, int Bin, TH1 *NewHisto,
+            TH1 *RefHisto);
   virtual ~HistoData() {}
 
   // Get General Information
   std::string getName() const { return name; }
-  //PlotType getType() const { return type; }
+  // PlotType getType() const { return type; }
   int getType() const { return type; }
   int getBin() const { return bin; }
   TH1 *getNewHisto() const { return newHisto; }
@@ -45,16 +45,20 @@ public:
 
   // Get Visual Attributes
   bool getLineUseFillColor() const { return lineUseFillColor; }
-  int getSolidLineColor() const { return lineUseFillColor ? solidFillColor : solidLineColor; }
+  int getSolidLineColor() const {
+    return lineUseFillColor ? solidFillColor : solidLineColor;
+  }
   int getSolidFillColor() const { return solidFillColor; }
   int getSolidFillStyle() const { return solidFillStyle; }
-  int getShadedLineColor() const { return lineUseFillColor ? shadedFillColor : shadedLineColor; }
+  int getShadedLineColor() const {
+    return lineUseFillColor ? shadedFillColor : shadedLineColor;
+  }
   int getShadedFillColor() const { return shadedFillColor; }
   int getShadedFillStyle() const { return shadedFillStyle; }
 
   // Set General Information
   void setName(std::string Name) { name = Name; }
-  //void setType(PlotType Type) { type = Type; }
+  // void setType(PlotType Type) { type = Type; }
   void setType(int PlotType) { type = PlotType; }
   void setBin(int Bin) { bin = Bin; }
   void setResultImage(std::string Image) { resultImage = Image; }
@@ -67,7 +71,10 @@ public:
   void setDoAllow2DRebinningY(bool Toggle) { doAllow2DRebinningY = Toggle; }
   void setDoProjectionsX(bool Toggle) { doProjectionsX = Toggle; }
   void setDoProjectionsY(bool Toggle) { doProjectionsY = Toggle; }
-  void setMaxProjections(int Num) { maxProjectionsX = Num; maxProjectionsY = Num; }
+  void setMaxProjections(int Num) {
+    maxProjectionsX = Num;
+    maxProjectionsY = Num;
+  }
   void setMaxProjectionsX(bool Num) { maxProjectionsX = Num; }
   void setMaxProjectionsY(bool Num) { maxProjectionsY = Num; }
 
@@ -90,14 +97,16 @@ public:
 
   // Misc Functions
   void drawResult(TH1 *Summary, bool Vertical = true, bool SetLabels = false);
-  void clear(){ newHisto->Clear(); refHisto->Clear(); };
+  void clear() {
+    newHisto->Clear();
+    refHisto->Clear();
+  };
   inline void dump();
 
 private:
-
   // Misc. Data
   std::string name;
-  //PlotType type;
+  // PlotType type;
   int type;
   int bin;
   TH1 *newHisto;
@@ -131,7 +140,6 @@ private:
 
   // Implementation Function
   void initialize();
-
 };
 
 #endif // HISTO_DATA__H
