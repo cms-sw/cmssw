0d99d64f558b26dbad8f8866e46b55b594e486ee
Cms Build <cmsbuild@cern.ch>
2019-05-04 09:54:09 +0200

    Clang-Format

--- CalibFormats/CaloObjects/src/CaloTSamples.cc
+++ CalibFormats/CaloObjects/src/CaloTSamples.cc
@@ -1,7 +1,7 @@
-#include "CalibFormats/CaloObjects/interface/CaloTSamplesBase.icc"
 #include "CalibFormats/CaloObjects/interface/CaloTSamples.icc"
+#include "CalibFormats/CaloObjects/interface/CaloTSamplesBase.icc"
 
-template class CaloTSamplesBase<float> ;
+template class CaloTSamplesBase<float>;
 
-template class CaloTSamples<float,10> ;
-template class CaloTSamples<float, 3> ;
+template class CaloTSamples<float, 10>;
+template class CaloTSamples<float, 3>;
--- CalibMuon/CSCCalibration/test/stubs/CSCBadChambersPopConAnalyzer.cc
+++ CalibMuon/CSCCalibration/test/stubs/CSCBadChambersPopConAnalyzer.cc
@@ -1,7 +1,8 @@
-#include "CondCore/PopCon/interface/PopConAnalyzer.h"
 #include "CSCBadChambersHandler.h"
+#include "CondCore/PopCon/interface/PopConAnalyzer.h"
 #include "FWCore/Framework/interface/MakerMacros.h"
 
-typedef popcon::PopConAnalyzer<popcon::CSCBadChambersImpl> CSCBadChambersPopConAnalyzer;
+typedef popcon::PopConAnalyzer<popcon::CSCBadChambersImpl>
+    CSCBadChambersPopConAnalyzer;
 
 DEFINE_FWK_MODULE(CSCBadChambersPopConAnalyzer);
