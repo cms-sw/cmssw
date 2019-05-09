4137f6cf2e3aaf0955dd807633e3d6663159c380
Cms Build <cmsbuild@cern.ch>
2019-05-09 06:16:28 +0200

    Clang-Format

--- CalibTracker/SiPixelConnectivity/interface/TRange.h
+++ CalibTracker/SiPixelConnectivity/interface/TRange.h
@@ -7,36 +7,35 @@
 #include <utility>
 #include <algorithm>
 
-
-template<class T> class TRange : public std::pair<T,T> {
+template <class T>
+class TRange : public std::pair<T, T> {
 public:
+  TRange() {}
 
-  TRange() { }
+  TRange(const T& aMin, const T& aMax) : std::pair<T, T>(aMin, aMax) {}
 
-  TRange(const T & aMin, const T & aMax) 
-      : std::pair<T,T> (aMin,aMax) { }
+  TRange(const std::pair<T, T>& apair) : std::pair<T, T>(apair) {}
 
-  TRange(const std::pair<T,T> & apair ) 
-      : std::pair<T,T> (apair) { }  
-
-  /// lower edge of range 
-  const T & min() const { return this->first; }
+  /// lower edge of range
+  const T& min() const { return this->first; }
 
   /// upper edge of range
-  const T & max() const { return this->second; }
+  const T& max() const { return this->second; }
 
-  T mean() const { return (this->first+this->second)/2.; }
+  T mean() const { return (this->first + this->second) / 2.; }
 
   /// true for empty region. note that region [x,x] is not empty
   bool empty() const { return (this->second < this->first); }
 
   /// check if object is inside region
-  bool inside(const T & value) const {
-    if (value < this->first || this->second < value)  return false; 
-    else  return true;
+  bool inside(const T& value) const {
+    if (value < this->first || this->second < value)
+      return false;
+    else
+      return true;
   }
 
-/*
+  /*
   /// check if ranges overlap
   bool hasIntersection( const TRange<T> & r) const {
     return rangesIntersect(*this,r); 
@@ -50,19 +49,23 @@ public:
 */
 
   /// sum of overlaping regions. the overlapping should be checked before.
-  TRange<T> sum(const TRange<T> & r) const {
-   if( this->empty()) return r;
-   else if( r.empty()) return *this;
-   else return TRange( (min() < r.min()) ? min() : r.min(), 
-      (max() < r.max()) ? r.max() : max()); 
+  TRange<T> sum(const TRange<T>& r) const {
+    if (this->empty())
+      return r;
+    else if (r.empty())
+      return *this;
+    else
+      return TRange((min() < r.min()) ? min() : r.min(), (max() < r.max()) ? r.max() : max());
   }
 
-  void sort() { if (empty() ) std::swap(this->first,this->second); }
+  void sort() {
+    if (empty())
+      std::swap(this->first, this->second);
+  }
 };
 
-template <class T> std::ostream & operator<<( 
-    std::ostream& out, const TRange<T>& r) 
-{
-  return out << "("<<r.min()<<","<<r.max()<<")";
+template <class T>
+std::ostream& operator<<(std::ostream& out, const TRange<T>& r) {
+  return out << "(" << r.min() << "," << r.max() << ")";
 }
 #endif
