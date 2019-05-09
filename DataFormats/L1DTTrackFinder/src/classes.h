7293a844f8c3abfb3d5797584bdf4f2e965ab40d
Santiago <santiago.folgueras@cern.ch>
2019-05-09 12:12:24 +0200

    Incorporating DataFormat for slice-test

--- DataFormats/L1DTTrackFinder/src/classes.h
+++ DataFormats/L1DTTrackFinder/src/classes.h
@@ -6,5 +6,6 @@
 #include <DataFormats/L1DTTrackFinder/interface/L1MuDTTrackContainer.h>
 #include <DataFormats/L1DTTrackFinder/interface/L1Phase2MuDTPhDigi.h>
 #include <DataFormats/L1DTTrackFinder/interface/L1Phase2MuDTPhContainer.h>
+#include <DataFormats/L1DTTrackFinder/interface/L1MuDTChambContainer.h>
 #include <DataFormats/Common/interface/Wrapper.h>
 
