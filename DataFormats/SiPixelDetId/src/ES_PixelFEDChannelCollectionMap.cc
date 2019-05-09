32b7f54eb9b641fb0698787175494843896db857
Patrick Gartung <gartung@fnal.gov>
2019-05-08 19:16:15 -0500

    DataFormats/SiPixelDetId: fix for undefined unordered_map when compiling with clang 4.0.1 and macos10.9.X SDK under Conda

--- DataFormats/SiPixelDetId/src/ES_PixelFEDChannelCollectionMap.cc
+++ DataFormats/SiPixelDetId/src/ES_PixelFEDChannelCollectionMap.cc
@@ -1,6 +1,6 @@
 #include "DataFormats/SiPixelDetId/interface/PixelFEDChannel.h"
 #include "FWCore/Utilities/interface/typelookup.h"
-
+#include <unordered_map>
 typedef std::unordered_map<std::string,PixelFEDChannelCollection> PixelFEDChannelCollectionMap;
 
 TYPELOOKUP_DATA_REG(PixelFEDChannelCollectionMap);
