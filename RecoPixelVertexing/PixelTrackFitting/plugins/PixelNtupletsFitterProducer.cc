9262bbfcd483af3963aec2da838cfbea785a993d
Andrea Bocci <andrea.bocci@cern.ch>
2019-05-14 23:31:50 +0200

    Clean up by clang-format (cms-patatrack#338)

--- RecoPixelVertexing/PixelTrackFitting/plugins/PixelNtupletsFitterProducer.cc
+++ RecoPixelVertexing/PixelTrackFitting/plugins/PixelNtupletsFitterProducer.cc
@@ -1,27 +1,22 @@
-#include "FWCore/Framework/interface/Frameworkfwd.h"
-#include "FWCore/Framework/interface/global/EDProducer.h"
-
-#include "FWCore/Framework/interface/Event.h"
-#include "FWCore/Framework/interface/MakerMacros.h"
-#include "FWCore/Framework/interface/EventSetup.h"
 #include "FWCore/Framework/interface/ESHandle.h"
-
-#include "FWCore/ParameterSet/interface/ParameterSet.h"
+#include "FWCore/Framework/interface/Event.h"
+#include "FWCore/Framework/interface/EventSetup.h"
+#include "FWCore/Framework/interface/Frameworkfwd.h"
+#include "FWCore/Framework/interface/MakerMacros.h"
+#include "FWCore/Framework/interface/global/EDProducer.h"
 #include "FWCore/ParameterSet/interface/ConfigurationDescriptions.h"
+#include "FWCore/ParameterSet/interface/ParameterSet.h"
 #include "FWCore/ParameterSet/interface/ParameterSetDescription.h"
-
-#include "RecoPixelVertexing/PixelTrackFitting/interface/PixelFitter.h"
-#include "RecoPixelVertexing/PixelTrackFitting/interface/PixelNtupletsFitter.h"
-
 #include "MagneticField/Engine/interface/MagneticField.h"
 #include "MagneticField/Records/interface/IdealMagneticFieldRecord.h"
+#include "RecoPixelVertexing/PixelTrackFitting/interface/PixelFitter.h"
+#include "RecoPixelVertexing/PixelTrackFitting/interface/PixelNtupletsFitter.h"
 #include "RecoTracker/TkMSParametrization/interface/PixelRecoUtilities.h"
 
-class PixelNtupletsFitterProducer: public edm::global::EDProducer<> {
+class PixelNtupletsFitterProducer : public edm::global::EDProducer<> {
 public:
   explicit PixelNtupletsFitterProducer(const edm::ParameterSet& iConfig)
-    : useRiemannFit_(iConfig.getParameter<bool>("useRiemannFit"))
-  {
+      : useRiemannFit_(iConfig.getParameter<bool>("useRiemannFit")) {
     produces<PixelFitter>();
   }
   ~PixelNtupletsFitterProducer() override {}
@@ -37,13 +32,11 @@ private:
   void produce(edm::StreamID, edm::Event& iEvent, const edm::EventSetup& iSetup) const override;
 };
 
-
 void PixelNtupletsFitterProducer::produce(edm::StreamID, edm::Event& iEvent, const edm::EventSetup& iSetup) const {
   edm::ESHandle<MagneticField> fieldESH;
   iSetup.get<IdealMagneticFieldRecord>().get(fieldESH);
   float bField = 1 / PixelRecoUtilities::fieldInInvGev(iSetup);
-  auto impl = std::make_unique<PixelNtupletsFitter>(bField,
-      fieldESH.product(), useRiemannFit_);
+  auto impl = std::make_unique<PixelNtupletsFitter>(bField, fieldESH.product(), useRiemannFit_);
   auto prod = std::make_unique<PixelFitter>(std::move(impl));
   iEvent.put(std::move(prod));
 }
