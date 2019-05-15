08cd101cd8035f5ad27e3b28e301e746a3d2c1c6
Cms Build <cmsbuild@cern.ch>
2019-05-15 23:00:07 +0200

    Clang-Format

--- Utilities/StorageFactory/interface/StorageAccountProxy.h
+++ Utilities/StorageFactory/interface/StorageAccountProxy.h
@@ -1,10 +1,10 @@
 #ifndef STORAGE_FACTORY_STORAGE_ACCOUNT_PROXY_H
-# define STORAGE_FACTORY_STORAGE_ACCOUNT_PROXY_H
+#define STORAGE_FACTORY_STORAGE_ACCOUNT_PROXY_H
 
-# include "Utilities/StorageFactory/interface/StorageAccount.h"
-# include "Utilities/StorageFactory/interface/Storage.h"
-# include "FWCore/Utilities/interface/get_underlying_safe.h"
-# include <string>
+#include "Utilities/StorageFactory/interface/StorageAccount.h"
+#include "Utilities/StorageFactory/interface/Storage.h"
+#include "FWCore/Utilities/interface/get_underlying_safe.h"
+#include <string>
 #include <memory>
 
 /** Proxy class that wraps SEAL's #Storage class with one that ticks
@@ -15,32 +15,31 @@
     Future improvement would be to implement more methods so that the
     wrapper itself doesn't cause peroformance degradation if the base
     storage does actually implement "sophisticated" features.  */
-class StorageAccountProxy : public Storage
-{
+class StorageAccountProxy : public Storage {
 public:
-  StorageAccountProxy (const std::string &storageClass, std::unique_ptr<Storage> baseStorage);
-  ~StorageAccountProxy (void) override;
+  StorageAccountProxy(const std::string &storageClass, std::unique_ptr<Storage> baseStorage);
+  ~StorageAccountProxy(void) override;
 
   using Storage::read;
   using Storage::write;
 
-  bool		prefetch (const IOPosBuffer *what, IOSize n) override;
-  IOSize	read (void *into, IOSize n) override;
-  IOSize	read (void *into, IOSize n, IOOffset pos) override;
-  IOSize	readv (IOBuffer *into, IOSize n) override;
-  IOSize	readv (IOPosBuffer *into, IOSize n) override;
-  IOSize	write (const void *from, IOSize n) override;
-  IOSize	write (const void *from, IOSize n, IOOffset pos) override;
-  IOSize	writev (const IOBuffer *from, IOSize n) override;
-  IOSize	writev (const IOPosBuffer *from, IOSize n) override;
+  bool prefetch(const IOPosBuffer *what, IOSize n) override;
+  IOSize read(void *into, IOSize n) override;
+  IOSize read(void *into, IOSize n, IOOffset pos) override;
+  IOSize readv(IOBuffer *into, IOSize n) override;
+  IOSize readv(IOPosBuffer *into, IOSize n) override;
+  IOSize write(const void *from, IOSize n) override;
+  IOSize write(const void *from, IOSize n, IOOffset pos) override;
+  IOSize writev(const IOBuffer *from, IOSize n) override;
+  IOSize writev(const IOPosBuffer *from, IOSize n) override;
 
-  IOOffset	position (IOOffset offset, Relative whence = SET) override;
-  void		resize (IOOffset size) override;
-  void		flush (void) override;
-  void		close (void) override;
+  IOOffset position(IOOffset offset, Relative whence = SET) override;
+  void resize(IOOffset size) override;
+  void flush(void) override;
+  void close(void) override;
 
 protected:
-  void releaseStorage() {get_underlying_safe(m_baseStorage).release();}
+  void releaseStorage() { get_underlying_safe(m_baseStorage).release(); }
 
   edm::propagate_const<std::unique_ptr<Storage>> m_baseStorage;
 
@@ -53,4 +52,4 @@ protected:
   StorageAccount::Counter &m_statsPrefetch;
 };
 
-#endif // STORAGE_FACTORY_STORAGE_ACCOUNT_PROXY_H
+#endif  // STORAGE_FACTORY_STORAGE_ACCOUNT_PROXY_H
