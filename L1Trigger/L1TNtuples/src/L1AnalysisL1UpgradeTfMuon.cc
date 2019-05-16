767973a47148c1839bc446cc74db361c1b924599
Dinyar Rabady <dinyar.rabady@cern.ch>
2019-05-16 15:53:36 +0200

    Put L1 TF track addresses into map in L1TNtuple May make the interface to get them more clear.

--- L1Trigger/L1TNtuples/src/L1AnalysisL1UpgradeTfMuon.cc
+++ L1Trigger/L1TNtuples/src/L1AnalysisL1UpgradeTfMuon.cc
@@ -24,6 +24,7 @@ void L1Analysis::L1AnalysisL1UpgradeTfMuon::SetTfMuon(const l1t::RegionalMuonCan
         l1upgradetfmuon_.tfMuonTrackFinderType.push_back(it->trackFinderType());
         l1upgradetfmuon_.tfMuonHwHF.push_back(it->hwHF());
         l1upgradetfmuon_.tfMuonBx.push_back(ibx);
+        std::map<std::string, int> decoded_track_address;
         if (it->trackFinderType() == l1t::tftype::bmtf) {
           int detSide = it->trackSubAddress(l1t::RegionalMuonCand::kWheelSide);
           int wheelNum = it->trackSubAddress(l1t::RegionalMuonCand::kWheelNum);
@@ -38,7 +39,28 @@ void L1Analysis::L1AnalysisL1UpgradeTfMuon::SetTfMuon(const l1t::RegionalMuonCan
           l1upgradetfmuon_.tfMuonTrAdd.push_back(stat2);
           l1upgradetfmuon_.tfMuonTrAdd.push_back(stat3);
           l1upgradetfmuon_.tfMuonTrAdd.push_back(stat4);
+
+          decoded_track_address["wheel"] = wheel;
+          decoded_track_address["station1"] = stat1;
+          decoded_track_address["station2"] = stat2;
+          decoded_track_address["station3"] = stat3;
+          decoded_track_address["station4"] = stat4;
+        } else if (it->trackFinderType() == l1t::omtf_neg || it->trackFinderType() == l1t::omtf_pos) {
+          decoded_track_address["kLayers"] = it->trackSubAddress(l1t::RegionalMuonCand::kLayers);
+          decoded_track_address["kWeight"] = it->trackSubAddress(l1t::RegionalMuonCand::kWeight);
+        } else if (it->trackFinderType() == l1t::emtf_neg || it->trackFinderType() == l1t::emtf_pos) {
+          decoded_track_address["kME1Seg"] = it->trackSubAddress(l1t::RegionalMuonCand::kME1Seg);
+          decoded_track_address["kME1Ch"] = it->trackSubAddress(l1t::RegionalMuonCand::kME1Ch);
+          decoded_track_address["kME2Seg"] = it->trackSubAddress(l1t::RegionalMuonCand::kME2Seg);
+          decoded_track_address["kME2Ch"] = it->trackSubAddress(l1t::RegionalMuonCand::kME2Ch);
+          decoded_track_address["kME3Seg"] = it->trackSubAddress(l1t::RegionalMuonCand::kME3Seg);
+          decoded_track_address["kME3Ch"] = it->trackSubAddress(l1t::RegionalMuonCand::kME3Ch);
+          decoded_track_address["kME4Seg"] = it->trackSubAddress(l1t::RegionalMuonCand::kME4Seg);
+          decoded_track_address["kME4Ch"] = it->trackSubAddress(l1t::RegionalMuonCand::kME4Ch);
+          decoded_track_address["kTrkNum"] = it->trackSubAddress(l1t::RegionalMuonCand::kTrkNum);
+          decoded_track_address["kBX"] = it->trackSubAddress(l1t::RegionalMuonCand::kBX);
         }
+        l1upgradetfmuon_.tfMuonDecodedTrAdd.push_back(decoded_track_address);
         l1upgradetfmuon_.tfMuonRawTrAdd.push_back(l1t::RegionalMuonRawDigiTranslator::generateRawTrkAddress(*it));
 
         l1upgradetfmuon_.nTfMuons++;
