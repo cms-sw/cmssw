bcb68cca101e07d7667a673437af5af86c0ae6a8
Efe <efe.yazgan@cern.ch>
2019-05-16 14:50:19 +0200

    new settings needed for pythia8v240 all tunes

--- Configuration/Generator/python/MCTunes2017/PythiaCP3TuneUpSettings_cfi.py
+++ Configuration/Generator/python/MCTunes2017/PythiaCP3TuneUpSettings_cfi.py
@@ -4,7 +4,6 @@ pythia8CP3TuneUpSettingsBlock = cms.PSet(
     pythia8CP3TuneUpSettings = cms.vstring(
     'Tune:pp 14',
     'Tune:ee 7',	
-    'PDF:pSet=19',
     'MultipartonInteractions:bProfile=2',
     'MultipartonInteractions:ecmPow=0.02266',
     'MultipartonInteractions:pT0Ref=1.478',
@@ -21,6 +20,10 @@ pythia8CP3TuneUpSettingsBlock = cms.PSet(
     'TimeShower:alphaSorder=2',
     'TimeShower:alphaSvalue=0.118',
     'SpaceShower:rapidityOrder=off', 
+    'SigmaTotal:mode = 0',
+    'SigmaTotal:sigmaEl = 21.89',
+    'SigmaTotal:sigmaTot = 100.309',
+    'PDF:pSet=LHAPDF6:NNPDF31_nlo_as_0118',
         )
 )
 
