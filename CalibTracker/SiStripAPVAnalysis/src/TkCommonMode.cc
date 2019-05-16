c463e282e824303217630cab01eea07e9e691256
Cms Build <cmsbuild@cern.ch>
2019-05-16 14:48:18 +0200

    Clang-Format

--- CalibTracker/SiStripAPVAnalysis/src/TkCommonMode.cc
+++ CalibTracker/SiStripAPVAnalysis/src/TkCommonMode.cc
@@ -2,7 +2,7 @@
 using namespace std;
 vector<float> TkCommonMode::toVector() const {
   vector<float> temp;
-  for (int i=0; i<myTkCommonModeTopology->numberOfStrips(); i++){
+  for (int i = 0; i < myTkCommonModeTopology->numberOfStrips(); i++) {
     temp.push_back(returnAsVector()[myTkCommonModeTopology->setOfStrip(i)]);
   }
   return temp;
