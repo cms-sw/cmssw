74714d3a2a1b536f8130cc97b21a80957d9277f0
Yacine <yhaddad@cern.ch>
2019-05-17 22:21:06 +0200

    updating HTXS::HiggsClassification class

--- SimDataFormats/HTXS/interface/HiggsTemplateCrossSections.h
+++ SimDataFormats/HTXS/interface/HiggsTemplateCrossSections.h
@@ -297,6 +297,10 @@ namespace Stage1_1 {
       HTXS::Stage0::Category stage0_cat;
       HTXS::Stage1::Category stage1_cat_pTjet25GeV;
       HTXS::Stage1::Category stage1_cat_pTjet30GeV;
+      STXS::Stage1_1::Category stage1_1_cat_pTjet25GeV;
+      STXS::Stage1_1::Category stage1_1_cat_pTjet30GeV;
+      STXS::Stage1_1_Fine::Category stage1_1_fine_cat_pTjet25GeV;
+      STXS::Stage1_1_Fine::Category stage1_1_fine_cat_pTjet30GeV;
       // Error code :: classification was succesful or some error occured
       HTXS::ErrorCode errorCode;
     };
@@ -315,6 +319,10 @@ namespace Stage1_1 {
       cat.stage0_cat = htxs_cat_rivet.stage0_cat;
       cat.stage1_cat_pTjet25GeV = htxs_cat_rivet.stage1_cat_pTjet25GeV;
       cat.stage1_cat_pTjet30GeV = htxs_cat_rivet.stage1_cat_pTjet30GeV;
+      cat.stage1_1_cat_pTjet25GeV = stxs_cat_rivet.stage1_1_cat_pTjet25GeV;
+      cat.stage1_1_cat_pTjet30GeV = stxs_cat_rivet.stage1_1_cat_pTjet30GeV;
+      cat.stage1_1_fine_cat_pTjet25GeV = stxs_cat_rivet.stage1_1_fine_cat_pTjet25GeV;
+      cat.stage1_1_fine_cat_pTjet30GeV = stxs_cat_rivet.stage1_1_fine_cat_pTjet30GeV;
       return cat;    
     }
     
