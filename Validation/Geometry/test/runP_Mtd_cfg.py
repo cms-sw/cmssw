a81f7c7b6c689dd37dfa7bd74306ce973e9f4d65
Fabio Cossutti <fabio.cossutti@ts.infn.it>
2019-05-19 19:45:25 +0200

    Add macro to produce plots

--- Validation/Geometry/test/runP_Mtd_cfg.py
+++ Validation/Geometry/test/runP_Mtd_cfg.py
@@ -54,7 +54,7 @@ process.source = cms.Source("PoolSource",
 
 process.maxEvents = cms.untracked.PSet(
 #    input = cms.untracked.int32(-1)
-    input = cms.untracked.int32(10)
+    input = cms.untracked.int32(10000)
 )
 
 process.p1 = cms.Path(process.g4SimHits)
