a27185a4dfbe14d2e92915ba9e95954aea33b4a9
Cms Build <cmsbuild@cern.ch>
2019-05-20 12:01:22 +0200

    Clang-Format

--- CondFormats/CastorObjects/interface/CastorPedestalWidths.h
+++ CondFormats/CastorObjects/interface/CastorPedestalWidths.h
@@ -15,25 +15,23 @@ POOL container to store PedestalWidth values 4xCapId, using template
 
 //typedef CastorCondObjectContainer<CastorPedestalWidth> CastorPedestalWidths;
 
-class CastorPedestalWidths: public CastorCondObjectContainer<CastorPedestalWidth>
-{
- public:
-  //constructor definition: has to contain 
-  CastorPedestalWidths():CastorCondObjectContainer<CastorPedestalWidth>(), unitIsADC(false) {}
-  CastorPedestalWidths(bool isADC):CastorCondObjectContainer<CastorPedestalWidth>(), unitIsADC(isADC) {}
+class CastorPedestalWidths : public CastorCondObjectContainer<CastorPedestalWidth> {
+public:
+  //constructor definition: has to contain
+  CastorPedestalWidths() : CastorCondObjectContainer<CastorPedestalWidth>(), unitIsADC(false) {}
+  CastorPedestalWidths(bool isADC) : CastorCondObjectContainer<CastorPedestalWidth>(), unitIsADC(isADC) {}
 
   // are the units ADC ? (true=ADC, false=fC)
-  bool isADC() const {return unitIsADC;}
+  bool isADC() const { return unitIsADC; }
   // set unit boolean
-  void setUnitADC(bool isADC) {unitIsADC = isADC;}
+  void setUnitADC(bool isADC) { unitIsADC = isADC; }
 
-  std::string const myname() {return (std::string)"CastorPedestalWidths";}
+  std::string const myname() { return (std::string) "CastorPedestalWidths"; }
 
- private:
+private:
   bool unitIsADC;
 
-
- COND_SERIALIZABLE;
+  COND_SERIALIZABLE;
 };
 
 #endif
