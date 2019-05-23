b24163c855e3be0dee4f5e27c141be4e8fa29e9c
Jaime Leon <jleonhol@gmail.com>
2019-05-23 15:00:23 +0200

    CalibratedDigis fix

--- Phase2L1Trigger/CalibratedDigis/plugins/CalibratedDigis.cc
+++ Phase2L1Trigger/CalibratedDigis/plugins/CalibratedDigis.cc
@@ -161,8 +161,12 @@ CalibratedDigis::produce(edm::Event& iEvent, const edm::EventSetup& iSetup)
 	  float newTime =0;
 	  if(flat_calib_!=0)
 	      newTime = digiTime - 325                     + 25.0*iEvent.eventAuxiliary().bunchCrossing() + float(timeOffset_);
-	  else 
-	      newTime = digiTime - theSync->offset(wireId) + 25.0*iEvent.eventAuxiliary().bunchCrossing() + float(timeOffset_);
+	  else {
+	      if (iEvent.eventAuxiliary().bunchCrossing() == -1) //FIX MC 
+	          newTime = digiTime - theSync->offset(wireId) + 25.0*400 + float(timeOffset_);
+	      else 
+	          newTime = digiTime - theSync->offset(wireId) + 25.0*iEvent.eventAuxiliary().bunchCrossing() + float(timeOffset_); 
+	  }
 	  DTDigi newDigi(wire, newTime, number);
 	  mydigis.insertDigi(layerId,newDigi);
       }
