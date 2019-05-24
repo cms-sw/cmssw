e0a1c161b2b3c8c0ae7d805770fe3873acb8301e
Oz Amram <oamram1@jhu.edu>
2019-05-24 10:54:31 -0500

    Turn on CR only if not running at 0T

--- RecoTracker/TransientTrackingRecHit/python/TTRHBuilderWithTemplate_cfi.py
+++ RecoTracker/TransientTrackingRecHit/python/TTRHBuilderWithTemplate_cfi.py
@@ -11,8 +11,8 @@ TTRHBuilderAngleAndTemplate = cms.ESProducer("TkTransientTrackingRecHitBuilderES
 from Configuration.Eras.Modifier_trackingPhase2PU140_cff import trackingPhase2PU140
 trackingPhase2PU140.toModify(TTRHBuilderAngleAndTemplate, Phase2StripCPE = cms.string('Phase2StripCPE'))
 
-# uncomment these two lines to turn on Cluster Repair CPE
-from Configuration.StandardSequences.MagneticField_cff import *
+#Turn on cluster repair if not running at 0T
+from Configuration.StandardSequences.MagneticField_cff import ParabolicParametrizedMagneticFieldProducer
 if ParabolicParametrizedMagneticFieldProducer.valueOverride != 0:
     from Configuration.Eras.Modifier_phase1Pixel_cff import phase1Pixel
     phase1Pixel.toModify(TTRHBuilderAngleAndTemplate, PixelCPE = cms.string('PixelCPEClusterRepair'))
