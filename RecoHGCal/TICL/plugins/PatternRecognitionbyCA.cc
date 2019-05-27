f79669a01a400d10e708b35c1d5ebaeb73979d2d
Marco Rovere <marco.rovere@cern.ch>
2019-05-27 14:42:54 +0200

    Remove HGCGraph from within PatternRecognitionbyCA

--- RecoHGCal/TICL/plugins/PatternRecognitionbyCA.cc
+++ RecoHGCal/TICL/plugins/PatternRecognitionbyCA.cc
@@ -6,8 +6,20 @@
 
 #include "FWCore/MessageLogger/interface/MessageLogger.h"
 #include "PatternRecognitionbyCA.h"
+#include "HGCGraph.h"
 
 using namespace ticl;
+
+PatternRecognitionbyCA::PatternRecognitionbyCA(const edm::ParameterSet& conf)
+  : PatternRecognitionAlgoBase(conf) {
+        theGraph_ = std::make_unique<HGCGraph>();
+        min_cos_theta_ = (float)conf.getParameter<double>("min_cos_theta");
+        min_cos_pointing_ = (float)conf.getParameter<double>("min_cos_pointing");
+        missing_layers_ = conf.getParameter<int>("missing_layers");
+        min_clusters_per_ntuplet_ = conf.getParameter<int>("min_clusters_per_ntuplet");
+      }
+
+PatternRecognitionbyCA::~PatternRecognitionbyCA(){};
 
 void PatternRecognitionbyCA::fillHistogram(
     const std::vector<reco::CaloCluster> &layerClusters,
@@ -41,19 +53,19 @@ void PatternRecognitionbyCA::makeTracksters(const edm::Event &ev, const edm::Eve
   rhtools_.getEventSetup(es);
 
   clearHistogram();
-  theGraph_.setVerbosity(algo_verbosity_);
-  theGraph_.clear();
+  theGraph_->setVerbosity(algo_verbosity_);
+  theGraph_->clear();
   if (algo_verbosity_ > None) {
     LogDebug("HGCPatterRecoByCA") << "Making Tracksters with CA" << std::endl;
   }
   std::vector<HGCDoublet::HGCntuplet> foundNtuplets;
   fillHistogram(layerClusters, mask);
-  theGraph_.makeAndConnectDoublets(tile_, patternbyca::nEtaBins, patternbyca::nPhiBins, layerClusters, 2, 2,
+  theGraph_->makeAndConnectDoublets(tile_, patternbyca::nEtaBins, patternbyca::nPhiBins, layerClusters, 2, 2,
                                    min_cos_theta_, min_cos_pointing_, missing_layers_,
                                    rhtools_.lastLayerFH());
-  theGraph_.findNtuplets(foundNtuplets, min_clusters_per_ntuplet_);
+  theGraph_->findNtuplets(foundNtuplets, min_clusters_per_ntuplet_);
   //#ifdef FP_DEBUG
-  const auto &doublets = theGraph_.getAllDoublets();
+  const auto &doublets = theGraph_->getAllDoublets();
   int tracksterId = 0;
   for (auto const &ntuplet : foundNtuplets) {
     std::set<unsigned int> effective_cluster_idx;
