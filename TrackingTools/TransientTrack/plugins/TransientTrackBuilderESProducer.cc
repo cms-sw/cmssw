d3c8c6ffdec24e446f0934997d9b9ca2fde100e9
Christopher Jones <chrisdjones15@gmail.com>
2019-05-29 13:43:26 -0500

    Added fillDescriptions to TransientTrackBuilderESProducer

--- TrackingTools/TransientTrack/plugins/TransientTrackBuilderESProducer.cc
+++ TrackingTools/TransientTrack/plugins/TransientTrackBuilderESProducer.cc
@@ -21,3 +21,11 @@ TransientTrackBuilderESProducer::TransientTrackBuilderESProducer(edm::ESConsumes
 std::unique_ptr<TransientTrackBuilder> TransientTrackBuilderESProducer::produce(const TransientTrackRecord& iRecord) {
   return std::make_unique<TransientTrackBuilder>(&iRecord.get(magToken_), iRecord.getHandle(geomToken_));
 }
+
+void TransientTrackBuilderESProducer::fillDescriptions(edm::ConfigurationDescriptions& descriptions) {
+  edm::ParameterSetDescription desc;
+  desc.add<std::string>("ComponentName", "TransientTrackBuilder")
+      ->setComment("data label to use when getting the data product");
+
+  descriptions.addDefault(desc);
+}
