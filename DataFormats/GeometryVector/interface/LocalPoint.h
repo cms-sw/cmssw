6ff2e625aa4dccca2fa427db963c330a985b8198
Cms Build <cmsbuild@cern.ch>
2019-05-29 07:34:34 +0200

    Clang-Format

--- DataFormats/GeometryVector/interface/LocalPoint.h
+++ DataFormats/GeometryVector/interface/LocalPoint.h
@@ -5,9 +5,9 @@
 #include "DataFormats/GeometryVector/interface/Point2DBase.h"
 #include "DataFormats/GeometryVector/interface/Point3DBase.h"
 
-typedef Point2DBase< float, LocalTag>    Local2DPoint;
-typedef Point3DBase< float, LocalTag>    Local3DPoint;
+typedef Point2DBase<float, LocalTag> Local2DPoint;
+typedef Point3DBase<float, LocalTag> Local3DPoint;
 
-typedef Local3DPoint                     LocalPoint;
+typedef Local3DPoint LocalPoint;
 
-#endif // GeometryVector_LocalPoint_h
+#endif  // GeometryVector_LocalPoint_h
--- DataFormats/GeometryVector/interface/Phi.h
+++ DataFormats/GeometryVector/interface/Phi.h
@@ -7,7 +7,7 @@
 
 namespace Geom {
 
-/** \class Phi
+  /** \class Phi
  *  A class for azimuthal angle represantation and algebra.
  *  The use of Phi<T> is tranparant due to the implicit conversion to T
  *  Constructs like cos(phi) work as with float or double.
@@ -22,35 +22,34 @@ namespace Geom {
   using angle_units::operators::convertRadToDeg;
 
   struct MinusPiToPi {};  // Dummy struct to indicate -pi to pi range
-  struct ZeroTo2pi{};  // Dummy struct to indicate 0 to 2pi range
+  struct ZeroTo2pi {};    // Dummy struct to indicate 0 to 2pi range
 
   template <typename T1, typename Range>
   class NormalizeWrapper {};
 
   template <typename T1>
   class NormalizeWrapper<T1, MinusPiToPi> {
-    public:
-    static void normalize(T1 &value) { // Reduce range to -pi to pi
-      if( value > twoPi() || value < -twoPi()) {
+  public:
+    static void normalize(T1& value) {  // Reduce range to -pi to pi
+      if (value > twoPi() || value < -twoPi()) {
         value = std::fmod(value, static_cast<T1>(twoPi()));
       }
-      if (value <= -pi()) value += twoPi();
-      if (value >  pi()) value -= twoPi();
+      if (value <= -pi())
+        value += twoPi();
+      if (value > pi())
+        value -= twoPi();
     }
   };
 
   template <typename T1>
-  class NormalizeWrapper<T1, ZeroTo2pi> { // Reduce range to 0 to 2pi
+  class NormalizeWrapper<T1, ZeroTo2pi> {  // Reduce range to 0 to 2pi
   public:
-    static void normalize(T1 &value) {
-      value = angle0to2pi::make0To2pi(value);
-    }
+    static void normalize(T1& value) { value = angle0to2pi::make0To2pi(value); }
   };
 
   template <typename T1, typename Range = MinusPiToPi>
   class Phi {
   public:
-
     /// Default constructor does not initialise - just as double.
     Phi() {}
 
@@ -60,59 +59,73 @@ namespace Geom {
     // conversions, in which case explicit casts must be used.
     // The constructor provides range checking and normalization,
     // e.g. the value of Phi(2*pi()+1) is 1
-    Phi( const T1& val) : theValue(val) { normalize(theValue);}
+    Phi(const T1& val) : theValue(val) { normalize(theValue); }
 
     /// conversion operator makes transparent use possible.
-    operator T1() const { return theValue;}
+    operator T1() const { return theValue; }
 
     /// Template argument conversion
-    template <typename T2, typename Range1> operator Phi<T2, Range1>() { return Phi<T2, Range1>(theValue);}
+    template <typename T2, typename Range1>
+    operator Phi<T2, Range1>() {
+      return Phi<T2, Range1>(theValue);
+    }
 
     /// Explicit access to value in case implicit conversion not OK
-    T1 value() const { return theValue;}
+    T1 value() const { return theValue; }
 
     // so that template classes expecting phi() works! (deltaPhi)
-    T1 phi() const { return theValue;}
+    T1 phi() const { return theValue; }
 
-    /// Standard arithmetics 
-    Phi& operator+=(const T1& a) {theValue+=a; normalize(theValue); return *this;}
-    Phi& operator+=(const Phi& a) {return operator+=(a.value());}
+    /// Standard arithmetics
+    Phi& operator+=(const T1& a) {
+      theValue += a;
+      normalize(theValue);
+      return *this;
+    }
+    Phi& operator+=(const Phi& a) { return operator+=(a.value()); }
 
-    Phi& operator-=(const T1& a) {theValue-=a; normalize(theValue); return *this;}
-    Phi& operator-=(const Phi& a) {return operator-=(a.value());}
+    Phi& operator-=(const T1& a) {
+      theValue -= a;
+      normalize(theValue);
+      return *this;
+    }
+    Phi& operator-=(const Phi& a) { return operator-=(a.value()); }
 
-    Phi& operator*=(const T1& a) {theValue*=a; normalize(theValue); return *this;}
+    Phi& operator*=(const T1& a) {
+      theValue *= a;
+      normalize(theValue);
+      return *this;
+    }
 
-    Phi& operator/=(const T1& a) {theValue/=a; normalize(theValue); return *this;}
+    Phi& operator/=(const T1& a) {
+      theValue /= a;
+      normalize(theValue);
+      return *this;
+    }
 
     T1 degrees() const { return convertRadToDeg(theValue); }
-    
+
     // nearZero() tells whether the angle is close enough to 0 to be considered 0.
     // The default tolerance is 1 degree.
-    inline bool nearZero(float tolerance = 1.0_deg) const {
-      return (std::abs(theValue) - tolerance <= 0.0);
-    }
+    inline bool nearZero(float tolerance = 1.0_deg) const { return (std::abs(theValue) - tolerance <= 0.0); }
 
     // nearEqual() tells whether two angles are close enough to be considered equal.
     // The default tolerance is 0.001 radian.
-    inline bool nearEqual(const Phi<T1, Range> &angle, float tolerance = 0.001) const {
+    inline bool nearEqual(const Phi<T1, Range>& angle, float tolerance = 0.001) const {
       return (std::abs(theValue - angle) - tolerance <= 0.0);
     }
 
   private:
-
     T1 theValue;
 
-    void normalize(T1 &value) {
-      NormalizeWrapper<T1, Range>::normalize(value);
-    }
+    void normalize(T1& value) { NormalizeWrapper<T1, Range>::normalize(value); }
   };
-
 
   /// - operator
   template <typename T1, typename Range>
-  inline Phi<T1, Range> operator-(const Phi<T1, Range>& a) {return Phi<T1, Range>(-a.value());}
-
+  inline Phi<T1, Range> operator-(const Phi<T1, Range>& a) {
+    return Phi<T1, Range>(-a.value());
+  }
 
   /// Addition
   template <typename T1, typename Range>
@@ -130,23 +143,21 @@ namespace Geom {
     return Phi<T1, Range>(b) += a;
   }
 
-
   /// Subtraction
   template <typename T1, typename Range>
-  inline Phi<T1, Range> operator-(const Phi<T1, Range>& a, const Phi<T1, Range>& b) { 
+  inline Phi<T1, Range> operator-(const Phi<T1, Range>& a, const Phi<T1, Range>& b) {
     return Phi<T1, Range>(a) -= b;
   }
   /// Subtraction with scalar, does not change the precision
   template <typename T1, typename Range, typename Scalar>
-  inline Phi<T1, Range> operator-(const Phi<T1, Range>& a, const Scalar& b) { 
+  inline Phi<T1, Range> operator-(const Phi<T1, Range>& a, const Scalar& b) {
     return Phi<T1, Range>(a) -= b;
   }
   /// Subtraction with scalar, does not change the precision
   template <typename T1, typename Range, typename Scalar>
-  inline Phi<T1, Range> operator-(const Scalar& a, const Phi<T1, Range>& b) { 
+  inline Phi<T1, Range> operator-(const Scalar& a, const Phi<T1, Range>& b) {
     return Phi<T1, Range>(a - b.value());
   }
-
 
   /// Multiplication with scalar, does not change the precision
   template <typename T1, typename Range, typename Scalar>
@@ -159,10 +170,9 @@ namespace Geom {
     return Phi<T1, Range>(b) *= a;
   }
 
-
   /// Division
   template <typename T1, typename Range>
-  inline T1 operator/(const Phi<T1, Range>& a, const Phi<T1, Range>& b) { 
+  inline T1 operator/(const Phi<T1, Range>& a, const Phi<T1, Range>& b) {
     return a.value() / b.value();
   }
   /// Division by scalar
@@ -172,9 +182,9 @@ namespace Geom {
   }
 
   // For convenience
-  template<typename T>
+  template <typename T>
   using Phi0To2pi = Phi<T, ZeroTo2pi>;
-}
+}  // namespace Geom
 
 /*
 // this a full mess with the above that is a mess in itself
@@ -205,14 +215,3 @@ namespace reco {
 */
 
 #endif
-
-
-
-
-
-
-
-
-
-
-
