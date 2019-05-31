bb71fee1d4b9c06e131825546ffca3ae0c70e2e0
Ankush <ankushreddy_kanugan1@baylor.edu>
2019-05-31 04:49:09 +0200

    Changed the DQM output folder name

--- Validation/RecoParticleFlow/plugins/PFClusterValidation.cc
+++ Validation/RecoParticleFlow/plugins/PFClusterValidation.cc
@@ -53,7 +53,7 @@ void PFClusterValidation::bookHistograms(DQMStore::IBooker & ibooker, edm::Run c
 
   Char_t histo[100];
 
-  ibooker.setCurrentFolder("PFClusterV/PFClusterTask");
+  ibooker.setCurrentFolder("ParticleFlow/PFClusterV");
   Double_t etaBinsOffset[83] = {-5.191, -4.889, -4.716, -4.538, -4.363, -4.191, -4.013, -3.839, -3.664, -3.489, -3.314, -3.139, -2.964, -2.853, -2.65,
 				-2.5, -2.322, -2.172, -2.043, -1.93, -1.83, -1.74, -1.653, -1.566, -1.479, -1.392, -1.305, -1.218, -1.131, -1.044, -0.957,
 				-0.879, -0.783, -0.696, -0.609, -0.522, -0.435, -0.348, -0.261, -0.174, -0.087, 0,
