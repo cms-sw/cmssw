fcac96bab546c207840d9a9d9eed24a9b9472b84
Cms Build <cmsbuild@cern.ch>
2019-05-31 07:35:03 +0200

    Clang-Format

--- CondFormats/HLTObjects/test/testSerializationHLTObjects.cpp
+++ CondFormats/HLTObjects/test/testSerializationHLTObjects.cpp
@@ -2,11 +2,10 @@
 
 #include "CondFormats/HLTObjects/src/headers.h"
 
-int main()
-{
-    testSerialization<AlCaRecoTriggerBits>();
-    testSerialization<std::pair<const std::string,std::vector<unsigned int>>>();
-    //testSerialization<trigger::HLTPrescaleTableCond>(); never serialized in the old DB
+int main() {
+  testSerialization<AlCaRecoTriggerBits>();
+  testSerialization<std::pair<const std::string, std::vector<unsigned int>>>();
+  //testSerialization<trigger::HLTPrescaleTableCond>(); never serialized in the old DB
 
-    return 0;
+  return 0;
 }
