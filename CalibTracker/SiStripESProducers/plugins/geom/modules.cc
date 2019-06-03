5863d39f9db8e537a72045b868de12ff332773c2
Matti Kortelainen <matti.kortelainen@cern.ch>
2019-06-03 19:58:01 +0200

    Modernize SiStripRegionConnectivity
    -  move contents of header to source
    - use EDGetToken
    - use make_unique
    - use range for
    - remove unnecessary vector copy

--- CalibTracker/SiStripESProducers/plugins/geom/modules.cc
+++
@@ -1,9 +0,0 @@
-#include "FWCore/Framework/interface/MakerMacros.h"
-#include "FWCore/Framework/interface/ModuleFactory.h"
-#include "FWCore/Framework/interface/SourceFactory.h"
-
-
-
-#include "CalibTracker/SiStripESProducers/plugins/geom/SiStripRegionConnectivity.h"
-DEFINE_FWK_EVENTSETUP_MODULE(SiStripRegionConnectivity);
-
