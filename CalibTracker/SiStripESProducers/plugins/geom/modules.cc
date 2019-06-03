edfcbe2d4f9a2992fcaea73f71c30f10f28d1745
Matti Kortelainen <matti.kortelainen@cern.ch>
2019-06-03 18:47:19 +0200

    Modernize SiStripConnectivity
    - move contents of header to source
    - use ESGetToken and  make_unique

--- CalibTracker/SiStripESProducers/plugins/geom/modules.cc
+++ CalibTracker/SiStripESProducers/plugins/geom/modules.cc
@@ -7,9 +7,6 @@
 #include "CalibTracker/SiStripESProducers/plugins/geom/SiStripHashedDetIdESModule.h"
 DEFINE_FWK_EVENTSETUP_MODULE(SiStripHashedDetIdESModule);
 
-#include "CalibTracker/SiStripESProducers/plugins/geom/SiStripConnectivity.h"
-DEFINE_FWK_EVENTSETUP_MODULE(SiStripConnectivity);
-
 #include "CalibTracker/SiStripESProducers/plugins/geom/SiStripRegionConnectivity.h"
 DEFINE_FWK_EVENTSETUP_MODULE(SiStripRegionConnectivity);
 
