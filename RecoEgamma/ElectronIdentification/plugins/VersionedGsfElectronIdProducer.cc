47b071043fc565007f3c3eef4724f6cab892dcae
Jonas Rembser <jonas.rembser@cern.ch>
2019-06-04 17:55:23 +0200

    plugins.cc for RecoEgamma/ElectronIdentification

--- RecoEgamma/ElectronIdentification/plugins/VersionedGsfElectronIdProducer.cc
+++
@@ -1,7 +0,0 @@
-#include "DataFormats/EgammaCandidates/interface/GsfElectron.h"
-#include "PhysicsTools/SelectorUtils/interface/VersionedIdProducer.h"
-
-typedef VersionedIdProducer<reco::GsfElectronPtr> VersionedGsfElectronIdProducer;
-
-//define this as a plug-in
-DEFINE_FWK_MODULE(VersionedGsfElectronIdProducer);
