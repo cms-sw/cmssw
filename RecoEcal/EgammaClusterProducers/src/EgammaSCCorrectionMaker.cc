1bdeb394429e3ae236bd118137a61ca59e380948
Matti Kortelainen <matti.kortelainen@cern.ch>
2019-06-05 17:09:40 -0500

    Remove redundant unique_ptr constructor calls from RecoEcal

--- RecoEcal/EgammaClusterProducers/src/EgammaSCCorrectionMaker.cc
+++ RecoEcal/EgammaClusterProducers/src/EgammaSCCorrectionMaker.cc
@@ -78,17 +78,14 @@ EgammaSCCorrectionMaker::EgammaSCCorrectionMaker(const edm::ParameterSet& ps) {
 
   // energy correction class
   if (applyEnergyCorrection_)
-    energyCorrectionFunction_ = std::unique_ptr<EcalClusterFunctionBaseClass>(
-        EcalClusterFunctionFactory::get()->create(energyCorrectorName_, ps));
+    energyCorrectionFunction_ = EcalClusterFunctionFactory::get()->create(energyCorrectorName_, ps);
   //energyCorrectionFunction_ = EcalClusterFunctionFactory::get()->create("EcalClusterEnergyCorrection", ps);
 
   if (applyCrackCorrection_)
-    crackCorrectionFunction_ = std::unique_ptr<EcalClusterFunctionBaseClass>(
-        EcalClusterFunctionFactory::get()->create(crackCorrectorName_, ps));
+    crackCorrectionFunction_ = EcalClusterFunctionFactory::get()->create(crackCorrectorName_, ps);
 
   if (applyLocalContCorrection_)
-    localContCorrectionFunction_ = std::unique_ptr<EcalClusterFunctionBaseClass>(
-        EcalClusterFunctionFactory::get()->create(localContCorrectorName_, ps));
+    localContCorrectionFunction_ = EcalClusterFunctionFactory::get()->create(localContCorrectorName_, ps);
 }
 
 EgammaSCCorrectionMaker::~EgammaSCCorrectionMaker() = default;
