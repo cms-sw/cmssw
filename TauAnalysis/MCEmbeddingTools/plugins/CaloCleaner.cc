d9789a64ac1ca878960ee9bd80ee3c0e4cb33300
Cms Build <cmsbuild@cern.ch>
2019-06-05 12:25:33 +0200

    Clang-Format

--- TauAnalysis/MCEmbeddingTools/plugins/CaloCleaner.cc
+++ TauAnalysis/MCEmbeddingTools/plugins/CaloCleaner.cc
@@ -14,75 +14,66 @@ typedef CaloCleaner<HORecHit> HORecHitColCleaner;
 typedef CaloCleaner<CastorRecHit> CastorRecHitColCleaner;
 typedef CaloCleaner<ZDCRecHit> ZDCRecHitColCleaner;
 
-
 //-------------------------------------------------------------------------------
 // define 'buildRecHit' functions used for different types of recHits
 //-------------------------------------------------------------------------------
-  
-
 
 template <typename T>
-void  CaloCleaner<T>::fill_correction_map(TrackDetMatchInfo *,  std::map<uint32_t, float> *)
-{
-  assert(0); // CV: make sure general function never gets called;
-             //     always use template specializations
+void CaloCleaner<T>::fill_correction_map(TrackDetMatchInfo *, std::map<uint32_t, float> *) {
+  assert(0);  // CV: make sure general function never gets called;
+              //     always use template specializations
 }
 
 template <>
-void  CaloCleaner<EcalRecHit>::fill_correction_map(TrackDetMatchInfo * info,  std::map<uint32_t, float> * cor_map)
-{
-  if (is_preshower_){
-     for ( std::vector<DetId>::const_iterator detId = info->crossedPreshowerIds.begin(); detId != info->crossedPreshowerIds.end(); ++detId ) {
-       (*cor_map) [detId->rawId()] = 9999999; // just remove all energy (Below 0 is not possible)  
-      }
-    } 
-  else {
-    for(std::vector<const EcalRecHit*>::const_iterator hit=info->crossedEcalRecHits.begin(); hit!=info->crossedEcalRecHits.end(); hit++){
+void CaloCleaner<EcalRecHit>::fill_correction_map(TrackDetMatchInfo *info, std::map<uint32_t, float> *cor_map) {
+  if (is_preshower_) {
+    for (std::vector<DetId>::const_iterator detId = info->crossedPreshowerIds.begin();
+         detId != info->crossedPreshowerIds.end();
+         ++detId) {
+      (*cor_map)[detId->rawId()] = 9999999;  // just remove all energy (Below 0 is not possible)
+    }
+  } else {
+    for (std::vector<const EcalRecHit *>::const_iterator hit = info->crossedEcalRecHits.begin();
+         hit != info->crossedEcalRecHits.end();
+         hit++) {
       //    (*cor_map) [(*hit)->detid().rawId()] +=(*hit)->energy();
-      (*cor_map) [(*hit)->detid().rawId()] =(*hit)->energy(); 
+      (*cor_map)[(*hit)->detid().rawId()] = (*hit)->energy();
     }
   }
 }
 
-
 template <>
-void  CaloCleaner<HBHERecHit>::fill_correction_map(TrackDetMatchInfo * info,  std::map<uint32_t, float> * cor_map)
-{
-  for(std::vector<const HBHERecHit*>::const_iterator hit = info->crossedHcalRecHits.begin(); hit != info->crossedHcalRecHits.end(); hit++) {
-    (*cor_map) [(*hit)->detid().rawId()] =(*hit)->energy(); 
+void CaloCleaner<HBHERecHit>::fill_correction_map(TrackDetMatchInfo *info, std::map<uint32_t, float> *cor_map) {
+  for (std::vector<const HBHERecHit *>::const_iterator hit = info->crossedHcalRecHits.begin();
+       hit != info->crossedHcalRecHits.end();
+       hit++) {
+    (*cor_map)[(*hit)->detid().rawId()] = (*hit)->energy();
   }
 }
 
-
 template <>
-void  CaloCleaner<HORecHit>::fill_correction_map(TrackDetMatchInfo * info,  std::map<uint32_t, float> * cor_map)
-{
-  for(std::vector<const HORecHit*>::const_iterator hit = info->crossedHORecHits.begin(); hit != info->crossedHORecHits.end(); hit++) {
-    (*cor_map) [(*hit)->detid().rawId()] =(*hit)->energy(); 
+void CaloCleaner<HORecHit>::fill_correction_map(TrackDetMatchInfo *info, std::map<uint32_t, float> *cor_map) {
+  for (std::vector<const HORecHit *>::const_iterator hit = info->crossedHORecHits.begin();
+       hit != info->crossedHORecHits.end();
+       hit++) {
+    (*cor_map)[(*hit)->detid().rawId()] = (*hit)->energy();
   }
 }
 
-
 template <>
-void  CaloCleaner<HFRecHit>::fill_correction_map(TrackDetMatchInfo * info,  std::map<uint32_t, float> * cor_map)
-{
- return; // No corrections for HF 
+void CaloCleaner<HFRecHit>::fill_correction_map(TrackDetMatchInfo *info, std::map<uint32_t, float> *cor_map) {
+  return;  // No corrections for HF
 }
 
 template <>
-void  CaloCleaner<CastorRecHit>::fill_correction_map(TrackDetMatchInfo * info,  std::map<uint32_t, float> * cor_map)
-{
- return;// No corrections for Castor
+void CaloCleaner<CastorRecHit>::fill_correction_map(TrackDetMatchInfo *info, std::map<uint32_t, float> *cor_map) {
+  return;  // No corrections for Castor
 }
 
 template <>
-void  CaloCleaner<ZDCRecHit>::fill_correction_map(TrackDetMatchInfo * info,  std::map<uint32_t, float> * cor_map)
-{
- return;// No corrections for Castor
+void CaloCleaner<ZDCRecHit>::fill_correction_map(TrackDetMatchInfo *info, std::map<uint32_t, float> *cor_map) {
+  return;  // No corrections for Castor
 }
-
-
-
 
 DEFINE_FWK_MODULE(EcalRecHitColCleaner);
 DEFINE_FWK_MODULE(HBHERecHitColCleaner);
@@ -91,6 +82,3 @@ DEFINE_FWK_MODULE(HORecHitColCleaner);
 DEFINE_FWK_MODULE(HFRecHitColCleaner);
 DEFINE_FWK_MODULE(CastorRecHitColCleaner);
 DEFINE_FWK_MODULE(ZDCRecHitColCleaner);
-
-
-
