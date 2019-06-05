6786217864f87a152788a024d8f64257b839db75
Andreas Psallidas <andreas.psallidas@cern.ch>
2019-06-05 16:35:15 +0200

    make adjustable the histos parameters

--- Validation/Geometry/interface/MaterialBudgetHGCalHistos.h
+++ Validation/Geometry/interface/MaterialBudgetHGCalHistos.h
@@ -9,6 +9,15 @@ public:
   MaterialBudgetHGCalHistos(std::shared_ptr<MaterialBudgetData> data,
                             std::shared_ptr<TestHistoMgr> mgr,
                             const std::string& fileName);
+  MaterialBudgetHGCalHistos(std::shared_ptr<MaterialBudgetData> data,
+                            std::shared_ptr<TestHistoMgr> mgr,
+                            const std::string& fileName, 
+			    double minZ, double maxZ, int nintZ,
+			    double rMin, double rMax, int nrbin,
+			    double etaMin, double etaMax, int netabin,
+			    double phiMin, double phiMax, int nphibin,
+			    double RMin, double RMax, int nRbin
+			    );
   ~MaterialBudgetHGCalHistos() override {}
   void fillStartTrack() override;
   void fillPerStep() override;
@@ -25,6 +34,13 @@ private:
   double* theMateId;
 
   std::shared_ptr<TestHistoMgr> hmgr;
+
+  double zMin_, zMax_; int nzbin_;
+  double rMin_, rMax_; int nrbin_;
+  double etaMin_, etaMax_; int netabin_;
+  double phiMin_, phiMax_; int nphibin_;
+  double RMin_, RMax_; int nRbin_;
+
 };
 
 #endif
