751242d189014fc280c12a9d8d135b95a31b429b
Jan Steggemann <jan.steggemann@cern.ch>
2019-06-06 10:13:19 +0200

    Protect against having histogram with no bins

--- Validation/RecoTau/src/TauTagValidation.cc
+++ Validation/RecoTau/src/TauTagValidation.cc
@@ -127,9 +127,10 @@ void TauTagValidation::bookHistograms(DQMStore::IBooker& ibooker,
   MonitorElement *ptTemp, *etaTemp, *phiTemp, *pileupTemp, *tmpME, *summaryTemp;
 
   ibooker.setCurrentFolder("RecoTauV/" + TauProducer_ + extensionName_ + "_Summary");
+  auto n_disc = discriminators_.size() ? discriminators_.size() : 21;
   hinfo summaryHinfo = (histoSettings_.exists("summary"))
                            ? hinfo(histoSettings_.getParameter<edm::ParameterSet>("summary"))
-                           : hinfo(discriminators_.size(), -0.5, discriminators_.size() - 0.5);
+                           : hinfo(n_disc, -0.5, n_disc - 0.5);
   summaryTemp =
       ibooker.book1D("summaryPlotNum", "summaryPlotNum", summaryHinfo.nbins, summaryHinfo.min, summaryHinfo.max);
   summaryMap.insert(std::make_pair(refCollection_ + "Num", summaryTemp));
