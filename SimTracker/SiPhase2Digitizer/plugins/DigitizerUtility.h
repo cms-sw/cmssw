23c0a0b55118fade0945114bdbebd8fd3ed9a1db
Cms Build <cmsbuild@cern.ch>
2019-06-07 08:36:54 +0200

    Clang-Format

--- SimTracker/SiPhase2Digitizer/plugins/DigitizerUtility.h
+++ SimTracker/SiPhase2Digitizer/plugins/DigitizerUtility.h
@@ -14,30 +14,33 @@ namespace DigitizerUtility {
   class Amplitude {
   public:
     Amplitude() : _amp(0.0) {}
-    Amplitude( float amp, const PSimHit* hitp, float frac=0, size_t hitIndex=0, unsigned int tofBin=0) :
-      _amp(amp) {
+    Amplitude(float amp, const PSimHit* hitp, float frac = 0, size_t hitIndex = 0, unsigned int tofBin = 0)
+        : _amp(amp) {
       if (frac > 0) {
-	if (hitp != nullptr) _simInfoList.push_back({frac, std::make_unique<SimHitInfoForLinks>(hitp,hitIndex,tofBin)});
-        else _simInfoList.push_back({frac, nullptr});
+        if (hitp != nullptr)
+          _simInfoList.push_back({frac, std::make_unique<SimHitInfoForLinks>(hitp, hitIndex, tofBin)});
+        else
+          _simInfoList.push_back({frac, nullptr});
       }
     }
 
     // can be used as a float by convers.
-    operator float() const {return _amp;}
-    float ampl() const {return _amp;}
-    const std::vector<std::pair<float, std::unique_ptr<SimHitInfoForLinks> > >& simInfoList() const {return _simInfoList;}
+    operator float() const { return _amp; }
+    float ampl() const { return _amp; }
+    const std::vector<std::pair<float, std::unique_ptr<SimHitInfoForLinks> > >& simInfoList() const {
+      return _simInfoList;
+    }
 
-    void operator+= (const Amplitude& other) {
+    void operator+=(const Amplitude& other) {
       _amp += other._amp;
       // in case of digi from the noise, the MC information need not be there
       for (auto const& ic : other.simInfoList()) {
-	if (ic.first > -0.5) _simInfoList.push_back({ic.first, std::make_unique<SimHitInfoForLinks>(*ic.second)});
+        if (ic.first > -0.5)
+          _simInfoList.push_back({ic.first, std::make_unique<SimHitInfoForLinks>(*ic.second)});
       }
     }
-    void operator+= (const float& amp) {
-      _amp += amp;
-    }
-    void set (const float amplitude) { // Used to reset the amplitude
+    void operator+=(const float& amp) { _amp += amp; }
+    void set(const float amplitude) {  // Used to reset the amplitude
       _amp = amplitude;
     }
     // void setind (const float indamplitude) { // Used to reset the amplitude
@@ -54,15 +57,14 @@ namespace DigitizerUtility {
   //*********************************************************
   class EnergyDepositUnit {
   public:
-    EnergyDepositUnit(): _energy(0),_position(0,0,0) {}
-    EnergyDepositUnit(float energy,float x, float y, float z):
-      _energy(energy),_position(x,y,z) {}
-    EnergyDepositUnit(float energy, Local3DPoint position):
-      _energy(energy),_position(position) {}
-    float x() const{return _position.x();}
-    float y() const{return _position.y();}
-    float z() const{return _position.z();}
-    float energy() const { return _energy;}
+    EnergyDepositUnit() : _energy(0), _position(0, 0, 0) {}
+    EnergyDepositUnit(float energy, float x, float y, float z) : _energy(energy), _position(x, y, z) {}
+    EnergyDepositUnit(float energy, Local3DPoint position) : _energy(energy), _position(position) {}
+    float x() const { return _position.x(); }
+    float y() const { return _position.y(); }
+    float z() const { return _position.z(); }
+    float energy() const { return _energy; }
+
   private:
     float _energy;
     Local3DPoint _position;
@@ -73,34 +75,33 @@ namespace DigitizerUtility {
   //**********************************************************
   class SignalPoint {
   public:
-    SignalPoint(): _pos(0,0), _time(0), _amplitude(0), 
-      _sigma_x(1.), _sigma_y(1.), _hitp(nullptr) {}
-    
-    SignalPoint(float x, float y, float sigma_x, float sigma_y,
-		float t, float a=1.0):
-      _pos(x,y), _time(t), _amplitude(a), _sigma_x(sigma_x), 
-      _sigma_y(sigma_y), _hitp(nullptr) {}
-    
-    SignalPoint(float x, float y, float sigma_x, float sigma_y,
-		float t, const PSimHit& hit, float a=1.0):
-      _pos(x,y), _time(t), _amplitude(a), _sigma_x(sigma_x), 
-      _sigma_y(sigma_y),_hitp(&hit) {}
-    
-    const LocalPoint& position() const {return _pos;}
-    float x()         const {return _pos.x();}
-    float y()         const {return _pos.y();}
-    float sigma_x()   const {return _sigma_x;}
-    float sigma_y()   const {return _sigma_y;}
-    float time()      const {return _time;}
-    float amplitude() const {return _amplitude;}
-    const PSimHit& hit() {return *_hitp;}
-    SignalPoint& set_amplitude(float amp) {_amplitude = amp; return *this;}
+    SignalPoint() : _pos(0, 0), _time(0), _amplitude(0), _sigma_x(1.), _sigma_y(1.), _hitp(nullptr) {}
+
+    SignalPoint(float x, float y, float sigma_x, float sigma_y, float t, float a = 1.0)
+        : _pos(x, y), _time(t), _amplitude(a), _sigma_x(sigma_x), _sigma_y(sigma_y), _hitp(nullptr) {}
+
+    SignalPoint(float x, float y, float sigma_x, float sigma_y, float t, const PSimHit& hit, float a = 1.0)
+        : _pos(x, y), _time(t), _amplitude(a), _sigma_x(sigma_x), _sigma_y(sigma_y), _hitp(&hit) {}
+
+    const LocalPoint& position() const { return _pos; }
+    float x() const { return _pos.x(); }
+    float y() const { return _pos.y(); }
+    float sigma_x() const { return _sigma_x; }
+    float sigma_y() const { return _sigma_y; }
+    float time() const { return _time; }
+    float amplitude() const { return _amplitude; }
+    const PSimHit& hit() { return *_hitp; }
+    SignalPoint& set_amplitude(float amp) {
+      _amplitude = amp;
+      return *this;
+    }
+
   private:
-    LocalPoint     _pos;
-    float          _time;
-    float          _amplitude;
-    float          _sigma_x;   // gaussian sigma in the x direction (cm)
-    float          _sigma_y;   //    "       "          y direction (cm) */
+    LocalPoint _pos;
+    float _time;
+    float _amplitude;
+    float _sigma_x;  // gaussian sigma in the x direction (cm)
+    float _sigma_y;  //    "       "          y direction (cm) */
     const PSimHit* _hitp;
   };
   struct DigiSimInfo {
@@ -108,5 +109,5 @@ namespace DigitizerUtility {
     bool ot_bit;
     std::vector<std::pair<float, SimHitInfoForLinks*> > simInfoList;
   };
-}
+}  // namespace DigitizerUtility
 #endif
