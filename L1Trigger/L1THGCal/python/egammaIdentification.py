4ec82633101f03908a427bc8957fbadd9a536fe6
Jonathon Langford <jl2117@ic.ac.uk>
2019-06-10 15:26:53 +0200

    updating egid for v9 geometry, trained on v3.9.4 clusters

--- L1Trigger/L1THGCal/python/egammaIdentification.py
+++ L1Trigger/L1THGCal/python/egammaIdentification.py
@@ -1,5 +1,7 @@
 import FWCore.ParameterSet.Config as cms
 
+from Configuration.Eras.Modifier_phase2_hgcalV9_cff import phase2_hgcalV9
+
 
 class Category:
     def __init__(self, eta_min, eta_max, pt_min, pt_max):
@@ -67,30 +69,84 @@ working_points_drnn_dbscan = [
         },
         ]
 
-# Identification for 3D HistoMax clustering (as in 3.5.2)
-bdt_weights_histomax = [
-        # Low eta
-        'L1Trigger/L1THGCal/data/egamma_id_histomax_352_loweta_v0.xml',
-        # High eta
-        'L1Trigger/L1THGCal/data/egamma_id_histomax_352_higheta_v0.xml',
-        ]
+# Identification for 3D HistoMax clustering: switch between configurations for v8 and v9 geometry
+input_features_histomax = {
+        "v8_352":['cl3d_firstlayer', 'cl3d_coreshowerlength', 'cl3d_maxlayer', 'cl3d_srrmean'],
+        "v9_370":['cl3d_coreshowerlength', 'cl3d_showerlength', 'cl3d_firstlayer', 'cl3d_maxlayer', 'cl3d_szz', 'cl3d_srrmean', 'cl3d_srrtot', 'cl3d_seetot', 'cl3d_spptot'],
+        "v9_394":['cl3d_coreshowerlength', 'cl3d_showerlength', 'cl3d_firstlayer', 'cl3d_maxlayer', 'cl3d_szz', 'cl3d_srrmean', 'cl3d_srrtot', 'cl3d_seetot', 'cl3d_spptot']
+        }
 
-working_points_histomax = [
-        # Low eta
-        {
-        '900':0.19146989,
-        '950':0.1379665 ,
-        '975':0.03496629,
-        '995':-0.24383164,
-        },
-        # High eta
-        {
-        '900':0.13347613,
-        '950':0.04267797,
-        '975':-0.03698097,
-        '995':-0.23077505,
-        },
-        ]
+bdt_weights_histomax = {
+        "v8_352":[ #trained using TPG software version 3.5.2
+                # Low eta
+                'L1Trigger/L1THGCal/data/egamma_id_histomax_352_loweta_v0.xml',
+                # High eta
+                'L1Trigger/L1THGCal/data/egamma_id_histomax_352_higheta_v0.xml'
+                ],
+        "v9_370":[ #trained using TPG software version 3.7.0
+                # Low eta
+                'L1Trigger/L1THGCal/data/egamma_id_histomax_370_loweta_v0.xml',
+                # High eta
+                'L1Trigger/L1THGCal/data/egamma_id_histomax_370_higheta_v0.xml'
+                ],
+        "v9_394":[ #trained using TPG software version 3.9.4
+                # Low eta
+                'L1Trigger/L1THGCal/data/egamma_id_histomax_394_loweta_v0.xml',
+                # High eta
+                'L1Trigger/L1THGCal/data/egamma_id_histomax_394_higheta_v0.xml'
+                ]
+        }
+
+working_points_histomax = {
+        "v8_352":[
+                # Low eta
+                {
+                '900':0.19146989,
+                '950':0.1379665,
+                '975':0.03496629,
+                '995':-0.24383164,
+                },
+                # High eta
+                {
+                '900':0.13347613,
+                '950':0.04267797,
+                '975':-0.03698097,
+                '995':-0.23077505,
+                }
+             ],
+        "v9_370":[
+                # Low eta
+                {
+                '900':0.8815851, # epsilon_b = 2.5%
+                '950':0.5587649, # epsilon_b = 4.0%
+                '975':-0.1937952, # epsilon_b = 5.5%
+                '995':-0.9394884, # epsilon_b = 10.1%
+                },
+                # High eta
+                {
+                '900':0.7078400, #epsilon_b = 3.5%
+                '950':-0.0239623, #epsilon_b = 6.9%
+                '975':-0.7045071, #epsilon_b = 11.6% 
+                '995':-0.9811426, #epsilon_b = 26.1%
+                }
+             ],
+        "v9_394":[
+                # Low eta
+                {
+                '900':0.9794103, # epsilon_b = 1.9%
+                '950':0.9052764, # epsilon_b = 3.9%
+                '975':0.5276631, # epsilon_b = 6.6%
+                '995':-0.9153535, # epsilon_b = 20.4%
+                },
+                # High eta
+                {
+                '900':0.8825340, #epsilon_b = 1.0%
+                '950':0.2856039, #epsilon_b = 1.7%
+                '975':-0.5274948, #epsilon_b = 2.8% 
+                '995':-0.9864445, #epsilon_b = 7.6%
+                }
+             ]
+        }
 
 tight_wp = ['975', '900']
 loose_wp = ['995', '950']
@@ -117,11 +173,20 @@ egamma_identification_drnn_dbscan = cms.PSet(
         )
 
 egamma_identification_histomax = cms.PSet(
-        Inputs=cms.vstring('cl3d_firstlayer', 'cl3d_coreshowerlength', 'cl3d_maxlayer', 'cl3d_srrmean'),
+        Inputs=cms.vstring(input_features_histomax['v8_352']),
         CategoriesEtaMin=cms.vdouble([cat.eta_min for cat in categories]),
         CategoriesEtaMax=cms.vdouble([cat.eta_max for cat in categories]),
         CategoriesPtMin=cms.vdouble([cat.pt_min for cat in categories]),
         CategoriesPtMax=cms.vdouble([cat.pt_max for cat in categories]),
-        Weights=cms.vstring(bdt_weights_histomax),
-        WorkingPoints=cms.vdouble([wps[eff] for wps,eff in zip(working_points_histomax,tight_wp)]),
+        Weights=cms.vstring(bdt_weights_histomax['v8_352']),
+        WorkingPoints=cms.vdouble([wps[eff] for wps,eff in zip(working_points_histomax['v8_352'],tight_wp)]),
+        )
+
+# Era Modification for HGCal v9: use correct training and WPs
+phase2_hgcalV9.toModify(egamma_identification_histomax,
+        Inputs=cms.vstring(input_features_histomax['v9_394']),
+        Weights=cms.vstring(bdt_weights_histomax['v9_394']),
+        WorkingPoints=cms.vdouble(
+                [wps[eff] for wps,eff in zip(working_points_histomax['v9_394'],tight_wp)]
+                )
         )
