bc5c0da317dd95f88ddcab9efd63e00e86457c4f
Pieter David <pieter.david@cern.ch>
2019-06-13 10:42:33 +0200

    Add a helper method to get the list of SiStrip DetIds from a GeometricDet (to replace some SiStripDetInfoFileReader uses)

---
+++ Geometry/TrackerNumberingBuilder/interface/utils.h
@@ -0,0 +1,14 @@
+#ifndef GEOMETRY_TRACKERNUMBERINGBUILDER_UTILS_H
+#define GEOMETRY_TRACKERNUMBERINGBUILDER_UTILS_H
+
+#include "Geometry/TrackerNumberingBuilder/interface/GeometricDet.h"
+
+/**
+ * A helper method to get the full list of SiStrip DetIds from the GeometricDet
+ *
+ * The DetIds are sorted by subdetector, but otherwise keep the ordering from
+ * GeometricDet::deepComponents (for compatibility with SiStripDetInfoFileReader)
+ */
+std::vector<uint32_t> getSiStripDetIds(const GeometricDet& geomDet);
+
+#endif // GEOMETRY_TRACKERNUMBERINGBUILDER_UTILS_H
