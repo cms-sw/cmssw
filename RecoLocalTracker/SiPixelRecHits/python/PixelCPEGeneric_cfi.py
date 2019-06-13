2548caca0d0daadced95ade5e671676367ac46ad
mmusich <marco.musich@cern.ch>
2019-06-13 22:59:41 +0200

    drop type specifications for all parameters which already exist

--- RecoLocalTracker/SiPixelRecHits/python/PixelCPEGeneric_cfi.py
+++ RecoLocalTracker/SiPixelRecHits/python/PixelCPEGeneric_cfi.py
@@ -62,7 +62,7 @@ PixelCPEGenericESProducer = cms.ESProducer("PixelCPEGenericESProducer",
 # This customizes the Run3 Pixel CPE generic reconstruction in order to activate the IrradiationBiasCorrection
 # because of the expected resolution loss due to radiation damage
 from Configuration.Eras.Modifier_run3_common_cff import run3_common
-run3_common.toModify(PixelCPEGenericESProducer, IrradiationBiasCorrection = cms.bool(True))
+run3_common.toModify(PixelCPEGenericESProducer, IrradiationBiasCorrection = True)
 
 # This customization will be removed once we get the templates for phase2 pixel
 # FIXME::Is the Upgrade variable actually used?
@@ -76,5 +76,3 @@ phase2_tracker.toModify(PixelCPEGenericESProducer,
   DoCosmics = False,
   Upgrade = cms.bool(True)
 )
-
-
