9f617129e54f8f950eb7d43007e7cf27723fca02
Ankush <ankushreddy_kanugan1@baylor.edu>
2019-06-17 18:06:03 +0200

    Code format

--- Validation/RecoParticleFlow/plugins/PFClusterValidation.h
+++ Validation/RecoParticleFlow/plugins/PFClusterValidation.h
@@ -1,6 +1,5 @@
 #ifndef PFClusterValidation_h
 #define PFClusterValidation_h
- 
 
 #include "FWCore/Framework/interface/Frameworkfwd.h"
 #include "FWCore/Framework/interface/EDAnalyzer.h"
@@ -13,7 +12,6 @@
 #include "FWCore/MessageLogger/interface/MessageLogger.h"
 #include "FWCore/ServiceRegistry/interface/Service.h"
 
-
 #include "DataFormats/DetId/interface/DetId.h"
 #include "DataFormats/CaloTowers/interface/CaloTowerDetId.h"
 #include "DataFormats/Math/interface/Vector3D.h"
@@ -21,7 +19,7 @@
 #include "DataFormats/CaloTowers/interface/CaloTowerCollection.h"
 #include "SimDataFormats/GeneratorProducts/interface/HepMCProduct.h"
 
-// including PFCluster 
+// including PFCluster
 #include "DataFormats/ParticleFlowReco/interface/PFBlock.h"
 #include "DataFormats/ParticleFlowReco/interface/PFBlockElementTrack.h"
 #include "DataFormats/ParticleFlowReco/interface/PFBlockElementCluster.h"
@@ -50,13 +48,13 @@
 #include "DQMServices/Core/interface/DQMEDAnalyzer.h"
 
 class PFClusterValidation : public DQMEDAnalyzer {
- public:
+public:
   PFClusterValidation(edm::ParameterSet const& conf);
   ~PFClusterValidation() override;
   void analyze(edm::Event const& e, edm::EventSetup const& c) override;
-  void bookHistograms(DQMStore::IBooker &, edm::Run const &, edm::EventSetup const &) override;
+  void bookHistograms(DQMStore::IBooker&, edm::Run const&, edm::EventSetup const&) override;
 
- private:
+private:
   double dR(double eta1, double phi1, double eta2, double phi2);
   double sumEnergy(edm::Handle<reco::PFClusterCollection> pfCluster1);
   std::string outputFile_;
@@ -71,43 +69,43 @@ class PFClusterValidation : public DQMEDAnalyzer {
   edm::EDGetTokenT<reco::PFClusterCollection> PFClusterHFTok_;
 
   int imc;
-  
+
   // this acts throught the entire class
   // no reinitialization required
-  const double partR = 0.3; // dr cutoff
-  double eta_MC, phi_MC,energy_MC = 9999.;
+  const double partR = 0.3;  // dr cutoff
+  double eta_MC, phi_MC, energy_MC = 9999.;
 
   //************Modules
 
-  MonitorElement*  emean_vs_eta_E;
-  MonitorElement*  emean_vs_eta_H;
-  MonitorElement*  emean_vs_eta_EH;
+  MonitorElement* emean_vs_eta_E;
+  MonitorElement* emean_vs_eta_H;
+  MonitorElement* emean_vs_eta_EH;
 
-  MonitorElement*  emean_vs_eta_HF;
-  MonitorElement*  emean_vs_eta_HO;
-  MonitorElement*  emean_vs_eta_EHF;
-  MonitorElement*  emean_vs_eta_EHFO;
+  MonitorElement* emean_vs_eta_HF;
+  MonitorElement* emean_vs_eta_HO;
+  MonitorElement* emean_vs_eta_EHF;
+  MonitorElement* emean_vs_eta_EHFO;
 
   MonitorElement* Ratio_Esummed_ECAL_0;
   MonitorElement* Ratio_Esummed_HCAL_0;
   MonitorElement* Ratio_Esummed_HO_0;
- 
+
   MonitorElement* Ratio_Esummed_ECAL_1;
   MonitorElement* Ratio_Esummed_HCAL_1;
   MonitorElement* Ratio_Esummed_HO_1;
- 
+
   MonitorElement* Ratio_Esummed_ECAL_2;
   MonitorElement* Ratio_Esummed_HCAL_2;
   MonitorElement* Ratio_Esummed_HO_2;
- 
+
   MonitorElement* Ratio_Esummed_ECAL_3;
   MonitorElement* Ratio_Esummed_HCAL_3;
   MonitorElement* Ratio_Esummed_HO_3;
- 
+
   MonitorElement* Ratio_Esummed_ECAL_4;
   MonitorElement* Ratio_Esummed_HCAL_4;
   MonitorElement* Ratio_Esummed_HO_4;
- 
+
   MonitorElement* Ratio_Esummed_HF_5;
   MonitorElement* Ratio_Esummed_HF_6;
 
@@ -123,7 +121,6 @@ class PFClusterValidation : public DQMEDAnalyzer {
   MonitorElement* Ratio_Esummed_ECAL_HCAL_HO_4;
 
   MonitorElement* Egen_MC;
-
 };
 
 #endif
