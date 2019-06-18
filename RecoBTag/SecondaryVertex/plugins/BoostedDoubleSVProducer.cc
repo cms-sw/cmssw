2addeb4b05421573a713355ca060073449bba24a
anovak10 <novak.andrzej@gmail.com>
2019-06-18 17:26:07 +0200

    Fix for better merging

--- RecoBTag/SecondaryVertex/plugins/BoostedDoubleSVProducer.cc
+++ RecoBTag/SecondaryVertex/plugins/BoostedDoubleSVProducer.cc
@@ -674,12 +674,13 @@ BoostedDoubleSVProducer::calcNsubjettiness(const reco::JetBaseRef & jet, float &
             edm::LogWarning("MissingJetConstituent") << "Jet constituent required for N-subjettiness computation is missing!";
         }
       }
-      else{
+      else
+	{
 	// Check if any values were nan or inf
 	float valcheck = daughter->px() + daughter->py() +  daughter->pz() + daughter->energy();
 	if (std::isnan(valcheck) || std::isinf(valcheck)) continue;
         fjParticles.push_back( fastjet::PseudoJet( daughter->px(), daughter->py(), daughter->pz(), daughter->energy() ) );
-      }
+	}
     }
     else
       edm::LogWarning("MissingJetConstituent") << "Jet constituent required for N-subjettiness computation is missing!";
