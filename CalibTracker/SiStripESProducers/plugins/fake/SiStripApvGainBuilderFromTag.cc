5aa1385191723bc36ff46ded8345bb3d36a8f799
Pieter David <pieter.david@cern.ch>
2019-06-19 15:10:12 +0200

    Fake noise and gain builders: use TrackerGeometry instead of SiStripDetInfoFileReader

--- CalibTracker/SiStripESProducers/plugins/fake/SiStripApvGainBuilderFromTag.cc
+++ CalibTracker/SiStripESProducers/plugins/fake/SiStripApvGainBuilderFromTag.cc
@@ -1,9 +1,8 @@
 #include "CalibTracker/SiStripESProducers/plugins/fake/SiStripApvGainBuilderFromTag.h"
-#include "CalibTracker/SiStripCommon/interface/SiStripDetInfoFileReader.h"
 #include "CondFormats/DataRecord/interface/SiStripCondDataRecords.h"
 #include "DataFormats/TrackerCommon/interface/TrackerTopology.h"
-#include "Geometry/Records/interface/IdealGeometryRecord.h"
-#include "FWCore/Framework/interface/EventSetup.h"
+#include "Geometry/TrackerGeometryBuilder/interface/TrackerGeometry.h"
+#include "Geometry/TrackerGeometryBuilder/interface/StripGeomDetUnit.h"
 #include <iostream>
 #include <fstream>
 #include <vector>
@@ -14,15 +13,16 @@
 SiStripApvGainBuilderFromTag::SiStripApvGainBuilderFromTag( const edm::ParameterSet& iConfig ):
   printdebug_(iConfig.getUntrackedParameter<uint32_t>("printDebug",1)),
   pset_(iConfig)
-{}
+{
+  tTopoToken_ = esConsumes<TrackerTopology, TrackerTopologyRcd>();
+  tGeomToken_ = esConsumes<TrackerGeometry, TrackerDigiGeometryRecord>();
+}
 
 void SiStripApvGainBuilderFromTag::analyze(const edm::Event& evt, const edm::EventSetup& iSetup)
 {
-  //Retrieve tracker topology from geometry
-  edm::ESHandle<TrackerTopology> tTopoHandle;
-  iSetup.get<IdealGeometryRecord>().get(tTopoHandle);
-  const TrackerTopology* const tTopo = tTopoHandle.product();
- 
+  const auto& tTopo = iSetup.get<TrackerTopologyRcd>().get(tTopoToken_);
+  const auto& tGeom = iSetup.get<TrackerDigiGeometryRecord>().get(tGeomToken_);
+
   //   unsigned int run=evt.id().run();
 
   std::string genMode = pset_.getParameter<std::string>("genMode");
@@ -46,63 +46,63 @@ void SiStripApvGainBuilderFromTag::analyze(const edm::Event& evt, const edm::Eve
   // Prepare the new object
   SiStripApvGain* obj = new SiStripApvGain();
 
-  const edm::Service<SiStripDetInfoFileReader> reader;
   uint32_t count = 0;
-  const std::map<uint32_t, SiStripDetInfoFileReader::DetInfo >& DetInfos = reader->getAllData();
-  for(std::map<uint32_t, SiStripDetInfoFileReader::DetInfo >::const_iterator it = DetInfos.begin(); it != DetInfos.end(); it++) {
+  for ( const auto det : tGeom.detUnits() ) {
+    const StripGeomDetUnit* stripDet = dynamic_cast<const StripGeomDetUnit*>(det);
+    if ( stripDet != nullptr ) {
+      const DetId detid = stripDet->geographicalId();
+      // Find if this DetId is in the input tag and if so how many are the Apvs for which it contains information
+      SiStripApvGain::Range inputRange;
+      size_t inputRangeSize = 0;
+      if( find( inputDetIds.begin(), inputDetIds.end(), detid ) != inputDetIds.end() ) {
+        inputRange = inputApvGain->getRange(detid);
+        inputRangeSize = distance(inputRange.first, inputRange.second);
+      }
 
-    // Find if this DetId is in the input tag and if so how many are the Apvs for which it contains information
-    SiStripApvGain::Range inputRange;
-    size_t inputRangeSize = 0;
-    if( find( inputDetIds.begin(), inputDetIds.end(), it->first ) != inputDetIds.end() ) {
-      inputRange = inputApvGain->getRange(it->first);
-      inputRangeSize = distance(inputRange.first, inputRange.second);
+      std::vector<float> theSiStripVector;
+      for(unsigned short j=0; j<(stripDet->specificTopology().nstrips()/128); j++){
+
+        double gainValue = meanGain_;
+
+        if( j < inputRangeSize ) {
+          gainValue = inputApvGain->getApvGain(j, inputRange);
+          // cout << "Gain = " << gainValue <<" from input tag for DetId = " << detid.rawId() << " and apv = " << j << endl;
+        }
+        // else {
+        //   cout << "No gain in input tag for DetId = " << detid << " and apv = " << j << " using value from cfg = " << gainValue << endl;
+        // }
+
+
+        // corrections at layer/disk level:
+        SiStripFakeAPVParameters::index sl = SiStripFakeAPVParameters::getIndex(&tTopo, detid);
+        //unsigned short nApvs = (stripDet->specificTopology().nstrips()/128);
+        if (applyTuning) {
+          double correction = correct.get(sl);
+          gainValue *= correction;
+        }
+
+        // smearing:
+        if (genMode == "gaussian") {
+          gainValue = CLHEP::RandGauss::shoot(gainValue, sigmaGain_);
+          if(gainValue<=minimumPosValue_) gainValue=minimumPosValue_;
+        }
+        else if( genMode != "default" ) {
+          LogDebug("SiStripApvGain") << "ERROR: wrong genMode specifier : " << genMode << ", please select one of \"default\" or \"gaussian\"" << std::endl;
+          exit(1);
+        }
+          
+        if (count<printdebug_) {
+          edm::LogInfo("SiStripApvGainGeneratorFromTag") << "detid: " << detid.rawId()  << " Apv: " << j <<  " gain: " << gainValue  << std::endl;
+        }
+        theSiStripVector.push_back(gainValue);
+      }
+      count++;
+      SiStripApvGain::Range range(theSiStripVector.begin(),theSiStripVector.end());
+      if ( ! obj->put(detid, range) )
+        edm::LogError("SiStripApvGainGeneratorFromTag")<<" detid already exists"<<std::endl;
     }
-
-    std::vector<float> theSiStripVector;
-    for(unsigned short j=0; j<it->second.nApvs; j++){
-
-      double gainValue = meanGain_;
-
-      if( j < inputRangeSize ) {
-        gainValue = inputApvGain->getApvGain(j, inputRange);
-        // cout << "Gain = " << gainValue <<" from input tag for DetId = " << it->first << " and apv = " << j << endl;
-      }
-      // else {
-      //   cout << "No gain in input tag for DetId = " << it->first << " and apv = " << j << " using value from cfg = " << gainValue << endl;
-      // }
-
-
-      // corrections at layer/disk level:
-      uint32_t detId = it->first;
-      SiStripFakeAPVParameters::index sl = SiStripFakeAPVParameters::getIndex(tTopo, detId);
-      //unsigned short nApvs = it->second.nApvs;
-      if (applyTuning) {
-	double correction = correct.get(sl);
-	gainValue *= correction;
-      }
-
-      // smearing:
-      if (genMode == "gaussian") {
-	gainValue = CLHEP::RandGauss::shoot(gainValue, sigmaGain_);
-	if(gainValue<=minimumPosValue_) gainValue=minimumPosValue_;
-      }
-      else if( genMode != "default" ) {
-        LogDebug("SiStripApvGain") << "ERROR: wrong genMode specifier : " << genMode << ", please select one of \"default\" or \"gaussian\"" << std::endl;
-        exit(1);
-      }
-	
-      if (count<printdebug_) {
-	edm::LogInfo("SiStripApvGainGeneratorFromTag") << "detid: " << it->first  << " Apv: " << j <<  " gain: " << gainValue  << std::endl; 	    
-      }
-      theSiStripVector.push_back(gainValue);
-    }
-    count++;
-    SiStripApvGain::Range range(theSiStripVector.begin(),theSiStripVector.end());
-    if ( ! obj->put(it->first,range) )
-      edm::LogError("SiStripApvGainGeneratorFromTag")<<" detid already exists"<<std::endl;
   }
-  
+
   //End now write data in DB
   edm::Service<cond::service::PoolDBOutputService> mydbservice;
   
