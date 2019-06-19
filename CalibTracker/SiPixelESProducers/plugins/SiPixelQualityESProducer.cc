7ada7fdfdeb013059db93e79438d98a27a65d024
Cms Build <cmsbuild@cern.ch>
2019-06-19 13:14:51 +0200

    Clang-Format

--- CalibTracker/SiPixelESProducers/plugins/SiPixelQualityESProducer.cc
+++ CalibTracker/SiPixelESProducers/plugins/SiPixelQualityESProducer.cc
@@ -2,7 +2,7 @@
 //
 // Package:    SiPixelQualityESProducer
 // Class:      SiPixelQualityESProducer
-// 
+//
 /**\class SiPixelQualityESProducer SiPixelQualityESProducer.h CalibTracker/SiPixelESProducer/src/SiPixelQualityESProducer.cc
 
  Description: <one line class summary>
@@ -39,25 +39,25 @@
 
 using namespace edm;
 
-class SiPixelQualityESProducer : public edm::ESProducer, public edm::EventSetupRecordIntervalFinder  {
- public:
-  SiPixelQualityESProducer(const edm::ParameterSet & iConfig);
+class SiPixelQualityESProducer : public edm::ESProducer, public edm::EventSetupRecordIntervalFinder {
+public:
+  SiPixelQualityESProducer(const edm::ParameterSet& iConfig);
   ~SiPixelQualityESProducer() override;
-  
-  std::unique_ptr<SiPixelQuality> produce(const SiPixelQualityRcd & iRecord) ;
-  std::unique_ptr<SiPixelQuality> produceWithLabel(const SiPixelQualityRcd & iRecord);
-  
- private:
-  void setIntervalFor( const edm::eventsetup::EventSetupRecordKey&,
-			       const edm::IOVSyncValue&,
-			       edm::ValidityInterval& ) override;
-  
+
+  std::unique_ptr<SiPixelQuality> produce(const SiPixelQualityRcd& iRecord);
+  std::unique_ptr<SiPixelQuality> produceWithLabel(const SiPixelQualityRcd& iRecord);
+
+private:
+  void setIntervalFor(const edm::eventsetup::EventSetupRecordKey&,
+                      const edm::IOVSyncValue&,
+                      edm::ValidityInterval&) override;
+
   struct Tokens {
     edm::ESGetToken<SiStripDetVOff, SiPixelDetVOffRcd> voffToken_;
     edm::ESGetToken<SiPixelQuality, SiPixelQualityFromDbRcd> dbobjectToken_;
   };
 
-  std::unique_ptr<SiPixelQuality> get_pointer(const SiPixelQualityRcd & iRecord, const Tokens& tokens);
+  std::unique_ptr<SiPixelQuality> get_pointer(const SiPixelQualityRcd& iRecord, const Tokens& tokens);
 
   Tokens defaultTokens_;
   Tokens labelTokens_;
@@ -67,11 +67,11 @@ class SiPixelQualityESProducer : public edm::ESProducer, public edm::EventSetupR
 // constructors and destructor
 //
 
-SiPixelQualityESProducer::SiPixelQualityESProducer(const edm::ParameterSet& conf_) 
-{
+SiPixelQualityESProducer::SiPixelQualityESProducer(const edm::ParameterSet& conf_) {
   edm::LogInfo("SiPixelQualityESProducer::SiPixelQualityESProducer");
 
-  auto label = conf_.exists("siPixelQualityLabel")?conf_.getParameter<std::string>("siPixelQualityLabel"):std::string{};
+  auto label =
+      conf_.exists("siPixelQualityLabel") ? conf_.getParameter<std::string>("siPixelQualityLabel") : std::string{};
   auto setConsumes = [](edm::ESConsumesCollector&& cc, Tokens& tokens, const std::string& label) {
     cc.setConsumes(tokens.voffToken_).setConsumes(tokens.dbobjectToken_, edm::ESInputTag{"", label});
   };
@@ -79,33 +79,32 @@ SiPixelQualityESProducer::SiPixelQualityESProducer(const edm::ParameterSet& conf
   //the following line is needed to tell the framework what
   // data is being produced
   setConsumes(setWhatProduced(this), defaultTokens_, "");
-  if (label == "forDigitizer"){
-    setConsumes(setWhatProduced(this, &SiPixelQualityESProducer::produceWithLabel, edm::es::Label(label)), labelTokens_, label);
+  if (label == "forDigitizer") {
+    setConsumes(
+        setWhatProduced(this, &SiPixelQualityESProducer::produceWithLabel, edm::es::Label(label)), labelTokens_, label);
   }
-  findingRecord<SiPixelQualityRcd>();  
+  findingRecord<SiPixelQualityRcd>();
 }
 
-SiPixelQualityESProducer::~SiPixelQualityESProducer()
-{
- 
-   // do anything here that needs to be done at desctruction time
-   // (e.g. close files, deallocate resources etc.)
-
+SiPixelQualityESProducer::~SiPixelQualityESProducer() {
+  // do anything here that needs to be done at desctruction time
+  // (e.g. close files, deallocate resources etc.)
 }
 
-std::unique_ptr<SiPixelQuality> SiPixelQualityESProducer::get_pointer(const SiPixelQualityRcd & iRecord, const Tokens& tokens) {
+std::unique_ptr<SiPixelQuality> SiPixelQualityESProducer::get_pointer(const SiPixelQualityRcd& iRecord,
+                                                                      const Tokens& tokens) {
   ///////////////////////////////////////////////////////
   //  errortype "whole" = int 0 in DB  BadRocs = 65535 //
   //  errortype "tbmA" = int 1 in DB  BadRocs = 255    //
   //  errortype "tbmB" = int 2 in DB  Bad Rocs = 65280 //
   //  errortype "none" = int 3 in DB                   //
   ///////////////////////////////////////////////////////
-  
+
   //if I have understood this is the one got from the DB or file, but in any case the ORIGINAL(maybe i need to get the record for it)
   //SiPixelQuality * obj = new SiPixelQuality();
   //SiPixelQuality::disabledModuleType BadModule;
   //BadModule.DetID = 1; BadModule.errorType = 0; BadModule.BadRocs = 65535; obj->addDisabledModule(BadModule);
-  
+
   //now the dbobject is the one copied from the db
   auto dbptr = std::make_unique<SiPixelQuality>(iRecord.get(tokens.dbobjectToken_));
 
@@ -114,21 +113,18 @@ std::unique_ptr<SiPixelQuality> SiPixelQualityESProducer::get_pointer(const SiPi
   return dbptr;
 }
 
-
-std::unique_ptr<SiPixelQuality> SiPixelQualityESProducer::produce(const SiPixelQualityRcd & iRecord)
-{
+std::unique_ptr<SiPixelQuality> SiPixelQualityESProducer::produce(const SiPixelQualityRcd& iRecord) {
   return get_pointer(iRecord, defaultTokens_);
 }
-std::unique_ptr<SiPixelQuality> SiPixelQualityESProducer::produceWithLabel(const SiPixelQualityRcd & iRecord)
-{
+std::unique_ptr<SiPixelQuality> SiPixelQualityESProducer::produceWithLabel(const SiPixelQualityRcd& iRecord) {
   return get_pointer(iRecord, labelTokens_);
 }
 
-void SiPixelQualityESProducer::setIntervalFor( const edm::eventsetup::EventSetupRecordKey&, 
-						const edm::IOVSyncValue& iosv, 
-						edm::ValidityInterval& oValidity ) {
-  edm::ValidityInterval infinity( iosv.beginOfTime(), iosv.endOfTime() );
-  oValidity = infinity;  
+void SiPixelQualityESProducer::setIntervalFor(const edm::eventsetup::EventSetupRecordKey&,
+                                              const edm::IOVSyncValue& iosv,
+                                              edm::ValidityInterval& oValidity) {
+  edm::ValidityInterval infinity(iosv.beginOfTime(), iosv.endOfTime());
+  oValidity = infinity;
 }
 
 DEFINE_FWK_EVENTSETUP_MODULE(SiPixelQualityESProducer);
