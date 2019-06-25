95079c1c545050161e6f03c23db872d788f7f4b9
juska <juska@cern.ch>
2019-06-25 10:26:40 +0200

    Apply automatic code formattingÃ

--- Validation/RecoParticleFlow/bin/offsetStack.cc
+++ Validation/RecoParticleFlow/bin/offsetStack.cc
@@ -11,13 +11,12 @@
 #include <iostream>
 #include <map>
 
-bool getHists( std::map<TString, TH1D*>& hists, TFile*& file, TString var, int var_val, float r );
+bool getHists(std::map<TString, TH1D*>& hists, TFile*& file, TString var, int var_val, float r);
 void setStyle();
-void setStack( THStack*& stack, std::map<TString, TH1D*>& hists );
-void split( const TString& str, TString& sub1, TString& sub2 );
+void setStack(THStack*& stack, std::map<TString, TH1D*>& hists);
+void split(const TString& str, TString& sub1, TString& sub2);
 
 int main(int argc, char* argv[]) {
-
   if (argc < 5) {
     std::cout << "Please provide a label:filename, variable, deltaR value, and an out directory." << std::endl;
     std::cout << "You may also include a second file at the end." << std::endl;
@@ -27,76 +26,91 @@ int main(int argc, char* argv[]) {
   }
 
   TString label1, fname1;
-  split( argv[1], label1, fname1 );
+  split(argv[1], label1, fname1);
   TString label = label1;
 
   TString var = argv[2];
-  float r = std::stof( argv[3] );
+  float r = std::stof(argv[3]);
   TString outdir = argv[4];
 
-  TFile* file1 = TFile::Open( fname1 );
-  if (!file1) {std::cout << "Invalid file1: " << fname1 << std::endl; return -1;}
+  TFile* file1 = TFile::Open(fname1);
+  if (!file1) {
+    std::cout << "Invalid file1: " << fname1 << std::endl;
+    return -1;
+  }
 
-  TH1F* h = (TH1F*) file1->FindObjectAny( var );
-  if (!h) {std::cout << "Could not find " + var + " hist" << std::endl;  return -1;}
-  int avg = h->GetMean()+0.5;
+  TH1F* h = (TH1F*)file1->FindObjectAny(var);
+  if (!h) {
+    std::cout << "Could not find " + var + " hist" << std::endl;
+    return -1;
+  }
+  int avg = h->GetMean() + 0.5;
 
-  std::map<TString, TH1D*> hists1 = { {"chm",0}, {"chu",0}, {"nh",0}, {"ne",0}, {"hfh",0}, {"hfe",0}, {"lep",0} };
-  if (!getHists( hists1, file1, var, avg, r )) return -1;
+  std::map<TString, TH1D*> hists1 = {{"chm", 0}, {"chu", 0}, {"nh", 0}, {"ne", 0}, {"hfh", 0}, {"hfe", 0}, {"lep", 0}};
+  if (!getHists(hists1, file1, var, avg, r))
+    return -1;
 
   setStyle();
   TCanvas c("c", "c", 600, 600);
 
-  THStack* stack1 = new THStack( "stack1", Form(";#eta;<Offset Energy_{T}(#pi%.1f^{2})>/<%s> [GeV]", r, var=="npv"?"N_{PV}":"#mu") );
-  setStack( stack1, hists1 );
+  THStack* stack1 = new THStack(
+      "stack1", Form(";#eta;<Offset Energy_{T}(#pi%.1f^{2})>/<%s> [GeV]", r, var == "npv" ? "N_{PV}" : "#mu"));
+  setStack(stack1, hists1);
   stack1->Draw("hist");
   TString legPF = "F";
 
-  TLegend* leg = new TLegend(.4,.67,.65,.92);
+  TLegend* leg = new TLegend(.4, .67, .65, .92);
 
   //file2 included//
   if (argc > 5) {
     TString label2, fname2;
-    split( argv[5], label2, fname2 );
+    split(argv[5], label2, fname2);
 
-    TFile* file2 = TFile::Open( fname2 );
-    if (!file2) {std::cout << "Invalid file2: " << fname2 << std::endl; return -1;}
+    TFile* file2 = TFile::Open(fname2);
+    if (!file2) {
+      std::cout << "Invalid file2: " << fname2 << std::endl;
+      return -1;
+    }
 
-    TH1F* h2 = (TH1F*) file2->FindObjectAny( var );
-    if (!h2) {std::cout << "Could not find " + var + " hist in file2" << std::endl;  return -1;}
-    int avg2 = h2->GetMean()+0.5;
+    TH1F* h2 = (TH1F*)file2->FindObjectAny(var);
+    if (!h2) {
+      std::cout << "Could not find " + var + " hist in file2" << std::endl;
+      return -1;
+    }
+    int avg2 = h2->GetMean() + 0.5;
 
     std::map<TString, TH1D*> hists2 = hists1;
-    if (!getHists( hists2, file2, var, avg2, r )) return -1;
+    if (!getHists(hists2, file2, var, avg2, r))
+      return -1;
 
-    THStack* stack2 = new THStack( "stack2", "stack2" );
-    setStack( stack2, hists2 );
+    THStack* stack2 = new THStack("stack2", "stack2");
+    setStack(stack2, hists2);
     stack2->Draw("samepe");
 
     legPF = "PF";
-    leg->SetHeader( Form("#bf{Markers: %s, Histograms: %s}", label2.Data(), label1.Data()) );
+    leg->SetHeader(Form("#bf{Markers: %s, Histograms: %s}", label2.Data(), label1.Data()));
     label = label1 + "_vs_" + label2;
 
     //Draw Markers for EM Deposits and Hadronic Deposits in two separate regions//
-    TH1D* hfe_clone = (TH1D*) hists2["hfe"]->Clone("hfe_clone");
-    TH1D* hfh_clone = (TH1D*) hists2["hfh"]->Clone("hfh_clone");
+    TH1D* hfe_clone = (TH1D*)hists2["hfe"]->Clone("hfe_clone");
+    TH1D* hfh_clone = (TH1D*)hists2["hfh"]->Clone("hfh_clone");
 
-    THStack* cloneStack = new THStack( "cloneStack", "cloneStack" );
+    THStack* cloneStack = new THStack("cloneStack", "cloneStack");
     cloneStack->Add(hists2["ne"]);
     cloneStack->Add(hfe_clone);
     cloneStack->Add(hists2["nh"]);
     cloneStack->Add(hfh_clone);
     cloneStack->Draw("samepe");
 
-    hists2["ne"] ->SetAxisRange(-2.9,2.9);
-    hists2["hfe"]->SetAxisRange(-5,-2.6);
-    hists2["nh"] ->SetAxisRange(-2.9,2.9);
-    hists2["hfh"]->SetAxisRange(-5,-2.6);
-    hists2["chu"]->SetAxisRange(-2.9,2.9);
-    hists2["chm"]->SetAxisRange(-2.9,2.9);
+    hists2["ne"]->SetAxisRange(-2.9, 2.9);
+    hists2["hfe"]->SetAxisRange(-5, -2.6);
+    hists2["nh"]->SetAxisRange(-2.9, 2.9);
+    hists2["hfh"]->SetAxisRange(-5, -2.6);
+    hists2["chu"]->SetAxisRange(-2.9, 2.9);
+    hists2["chm"]->SetAxisRange(-2.9, 2.9);
 
-    hfe_clone        ->SetAxisRange(2.6,5);
-    hfh_clone        ->SetAxisRange(2.6,5);
+    hfe_clone->SetAxisRange(2.6, 5);
+    hfh_clone->SetAxisRange(2.6, 5);
   }
 
   leg->SetBorderSize(0);
@@ -105,12 +119,12 @@ int main(int argc, char* argv[]) {
   leg->SetTextSize(0.04);
   leg->SetTextFont(42);
 
-  leg->AddEntry( hists1["ne"],  "Photons",                  legPF );
-  leg->AddEntry( hists1["hfe"], "EM Deposits",              legPF );
-  leg->AddEntry( hists1["nh"],  "Neutral Hadrons",          legPF );
-  leg->AddEntry( hists1["hfh"], "Hadronic Deposits",        legPF );
-  leg->AddEntry( hists1["chu"], "Unassoc. Charged Hadrons", legPF );
-  leg->AddEntry( hists1["chm"], "Assoc. Charged Hadrons",   legPF );
+  leg->AddEntry(hists1["ne"], "Photons", legPF);
+  leg->AddEntry(hists1["hfe"], "EM Deposits", legPF);
+  leg->AddEntry(hists1["nh"], "Neutral Hadrons", legPF);
+  leg->AddEntry(hists1["hfh"], "Hadronic Deposits", legPF);
+  leg->AddEntry(hists1["chu"], "Unassoc. Charged Hadrons", legPF);
+  leg->AddEntry(hists1["chm"], "Assoc. Charged Hadrons", legPF);
 
   leg->Draw();
 
@@ -123,25 +137,26 @@ int main(int argc, char* argv[]) {
 
   text.SetTextSize(0.045);
   text.SetTextFont(42);
-  text.DrawLatex(1-label.Length()/41., 0.96, label);
+  text.DrawLatex(1 - label.Length() / 41., 0.96, label);
 
-  c.Print( outdir + "/stack_" + label + ".pdf" );
+  c.Print(outdir + "/stack_" + label + ".pdf");
 }
 
-bool getHists( std::map<TString, TH1D*>& hists, TFile*& file, TString var, int var_val, float r ) {
-
-  for ( auto& pair : hists ) {
-
+bool getHists(std::map<TString, TH1D*>& hists, TFile*& file, TString var, int var_val, float r) {
+  for (auto& pair : hists) {
     TString name = Form("p_offset_eta_%s%i_%s", var.Data(), var_val, pair.first.Data());
-    TProfile* p = (TProfile*) file->FindObjectAny(name);
-    if (!p) {std::cout << "Could not find " + name << std::endl;  return false;}
-    pair.second = p->ProjectionX( pair.first );
-    pair.second->Scale( r*r / 2 / var_val );
+    TProfile* p = (TProfile*)file->FindObjectAny(name);
+    if (!p) {
+      std::cout << "Could not find " + name << std::endl;
+      return false;
+    }
+    pair.second = p->ProjectionX(pair.first);
+    pair.second->Scale(r * r / 2 / var_val);
 
     const double* xbins = p->GetXaxis()->GetXbins()->GetArray();
-    for (int i=1, n=p->GetNbinsX(); i<=n; i++) {
-      pair.second->SetBinContent( i, pair.second->GetBinContent(i) / (xbins[i]-xbins[i-1]) );
-      pair.second->SetBinError( i, pair.second->GetBinError(i) / (xbins[i]-xbins[i-1]) );
+    for (int i = 1, n = p->GetNbinsX(); i <= n; i++) {
+      pair.second->SetBinContent(i, pair.second->GetBinContent(i) / (xbins[i] - xbins[i - 1]));
+      pair.second->SetBinError(i, pair.second->GetBinError(i) / (xbins[i] - xbins[i - 1]));
     }
   }
   return true;
@@ -169,39 +184,38 @@ void setStyle() {
   gStyle->SetPadTickY(1);
 }
 
-void setStack( THStack*& stack, std::map<TString, TH1D*>& hists ) {
+void setStack(THStack*& stack, std::map<TString, TH1D*>& hists) {
+  stack->Add(hists["ne"]);
+  stack->Add(hists["hfe"]);
+  stack->Add(hists["nh"]);
+  stack->Add(hists["hfh"]);
+  stack->Add(hists["chu"]);
+  stack->Add(hists["chm"]);
 
-  stack->Add( hists["ne"] );
-  stack->Add( hists["hfe"] );
-  stack->Add( hists["nh"] );
-  stack->Add( hists["hfh"] );
-  stack->Add( hists["chu"] );
-  stack->Add( hists["chm"] );
-
-  hists["ne"] ->SetMarkerStyle(kMultiply);
+  hists["ne"]->SetMarkerStyle(kMultiply);
   hists["hfe"]->SetMarkerStyle(kOpenStar);
-  hists["nh"] ->SetMarkerStyle(kOpenDiamond);
+  hists["nh"]->SetMarkerStyle(kOpenDiamond);
   hists["hfh"]->SetMarkerStyle(kOpenTriangleUp);
   hists["chu"]->SetMarkerStyle(kOpenCircle);
   hists["chm"]->SetMarkerStyle(kOpenCircle);
 
-  hists["ne"] ->SetFillColor(kBlue);
-  hists["hfe"]->SetFillColor(kViolet+2);
-  hists["nh"] ->SetFillColor(kGreen);
-  hists["hfh"]->SetFillColor(kPink+6);
-  hists["chu"]->SetFillColor(kRed-9);
+  hists["ne"]->SetFillColor(kBlue);
+  hists["hfe"]->SetFillColor(kViolet + 2);
+  hists["nh"]->SetFillColor(kGreen);
+  hists["hfh"]->SetFillColor(kPink + 6);
+  hists["chu"]->SetFillColor(kRed - 9);
   hists["chm"]->SetFillColor(kRed);
 
-  hists["ne"] ->SetLineColor(kBlack);
+  hists["ne"]->SetLineColor(kBlack);
   hists["hfe"]->SetLineColor(kBlack);
-  hists["nh"] ->SetLineColor(kBlack);
+  hists["nh"]->SetLineColor(kBlack);
   hists["hfh"]->SetLineColor(kBlack);
   hists["chu"]->SetLineColor(kBlack);
   hists["chm"]->SetLineColor(kBlack);
 }
 
-void split( const TString& str, TString& sub1, TString& sub2 ) {
+void split(const TString& str, TString& sub1, TString& sub2) {
   int pos = str.First(':');
   sub1 = str(0, pos);
-  sub2 = str(pos+1, str.Length());
+  sub2 = str(pos + 1, str.Length());
 }
