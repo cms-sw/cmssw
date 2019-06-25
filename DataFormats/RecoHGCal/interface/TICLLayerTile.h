05f226de494e4ff4a1df2c2f8fe72f36e9a3f927
Marco Rovere <marco.rovere@cern.ch>
2019-06-25 14:23:19 +0200

    Rename accessor methods

--- DataFormats/RecoHGCal/interface/TICLLayerTile.h
+++ DataFormats/RecoHGCal/interface/TICLLayerTile.h
@@ -10,10 +10,10 @@
 class TICLLayerTile {
 public:
   void fill(double eta, double phi, unsigned int layerClusterId) {
-    tile_[getGlobalBin(eta, phi)].push_back(layerClusterId);
+    tile_[globalBin(eta, phi)].push_back(layerClusterId);
   }
 
-  int getEtaBin(float eta) const {
+  int etaBin(float eta) const {
     constexpr float etaRange = ticl::constants::maxEta - ticl::constants::minEta;
     static_assert(etaRange >= 0.f);
     float r = ticl::constants::nEtaBins / etaRange;
@@ -22,7 +22,7 @@ public:
     return etaBin;
   }
 
-  int getPhiBin(float phi) const {
+  int phiBin(float phi) const {
     auto normPhi = normalizedPhi(phi);
     float r = ticl::constants::nPhiBins * M_1_PI * 0.5f;
     int phiBin = (normPhi + M_PI) * r;
@@ -30,9 +30,9 @@ public:
     return phiBin;
   }
 
-  int getGlobalBin(int etaBin, int phiBin) const { return phiBin + etaBin * ticl::constants::nPhiBins; }
+  int globalBin(int etaBin, int phiBin) const { return phiBin + etaBin * ticl::constants::nPhiBins; }
 
-  int getGlobalBin(double eta, double phi) const { return getPhiBin(phi) + getEtaBin(eta) * ticl::constants::nPhiBins; }
+  int globalBin(double eta, double phi) const { return phiBin(phi) + etaBin(eta) * ticl::constants::nPhiBins; }
 
   void clear() {
     auto nBins = ticl::constants::nEtaBins * ticl::constants::nPhiBins;
