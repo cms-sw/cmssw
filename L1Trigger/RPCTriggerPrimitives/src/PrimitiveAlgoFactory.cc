8e3ed42122cf011d346125dde04ee93732ed9bae
Manuel Segura <manuel.segura@cern.ch>
2019-06-27 16:17:17 +0200

    Trigger primitive phase2 @maseguracern plugin

--- L1Trigger/RPCTriggerPrimitives/src/PrimitiveAlgoFactory.cc
+++
@@ -1,4 +0,0 @@
-#include "FWCore/PluginManager/interface/PluginFactory.h"
-#include "L1Trigger/RPCTriggerPrimitives/interface/PrimitiveAlgoFactory.h"
-
-EDM_REGISTER_PLUGINFACTORY(PrimitiveAlgoFactory, "PrimitiveAlgoFactory");
