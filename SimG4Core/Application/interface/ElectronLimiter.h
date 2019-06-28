6733ba3769190618a321c2e56ebeacbc243eb0b6
Vladimir <Vladimir.Ivantchenko@cern.ch>
2019-06-28 12:15:12 +0200

    applied code format

--- SimG4Core/Application/interface/ElectronLimiter.h
+++ SimG4Core/Application/interface/ElectronLimiter.h
@@ -22,43 +22,44 @@ class CMSTrackingCutModel;
 
 class ElectronLimiter : public G4VEmProcess {
 public:
-  explicit ElectronLimiter(const edm::ParameterSet&, const G4ParticleDefinition*);
+  explicit ElectronLimiter(const edm::ParameterSet &, const G4ParticleDefinition *);
 
   ~ElectronLimiter() override;
 
-  G4bool IsApplicable(const G4ParticleDefinition&) override;
+  G4bool IsApplicable(const G4ParticleDefinition &) override;
 
-  void InitialiseProcess(const G4ParticleDefinition*) override;
+  void InitialiseProcess(const G4ParticleDefinition *) override;
 
-  void StartTracking(G4Track*) override;
+  void StartTracking(G4Track *) override;
 
   G4double PostStepGetPhysicalInteractionLength(const G4Track &track,
                                                 G4double previousStepSize,
                                                 G4ForceCondition *condition) override;
 
-  G4VParticleChange* PostStepDoIt(const G4Track&, const G4Step&) override;
+  G4VParticleChange *PostStepDoIt(const G4Track &, const G4Step &) override;
 
-  void SetTrackingCutPerRegion(std::vector<const G4Region*>&, std::vector<G4double>&, 
-                               std::vector<G4double>&, std::vector<G4double>&);
+  void SetTrackingCutPerRegion(std::vector<const G4Region *> &,
+                               std::vector<G4double> &,
+                               std::vector<G4double> &,
+                               std::vector<G4double> &);
 
   inline void SetRangeCheckFlag(G4bool);
 
   inline void SetFieldCheckFlag(G4bool);
 
 private:
-
   G4VEnergyLossProcess *ionisation_;
   CMSTrackingCutModel *trcut_;
-  const G4ParticleDefinition* particle_;
+  const G4ParticleDefinition *particle_;
 
-  std::vector<const G4Region*> regions_;
+  std::vector<const G4Region *> regions_;
   std::vector<G4double> energyLimits_;
   std::vector<G4double> factors_;
   std::vector<G4double> rms_;
 
   G4double minStepLimit_;
 
-  G4int  nRegions_;
+  G4int nRegions_;
   G4bool rangeCheckFlag_;
   G4bool fieldCheckFlag_;
   G4bool killTrack_;
--- SimG4Core/Application/src/ElectronLimiter.cc
+++ SimG4Core/Application/src/ElectronLimiter.cc
@@ -16,35 +16,37 @@
 #include "G4SystemOfUnits.hh"
 #include "G4TransportationProcessType.hh"
 
-ElectronLimiter::ElectronLimiter(const edm::ParameterSet &p, const G4ParticleDefinition* part) 
-  : G4VEmProcess("eLimiter", fGeneral), ionisation_(nullptr), particle_(part), nRegions_(0), 
-    rangeCheckFlag_(false), fieldCheckFlag_(false), killTrack_(false) {
+ElectronLimiter::ElectronLimiter(const edm::ParameterSet &p, const G4ParticleDefinition *part)
+    : G4VEmProcess("eLimiter", fGeneral),
+      ionisation_(nullptr),
+      particle_(part),
+      nRegions_(0),
+      rangeCheckFlag_(false),
+      fieldCheckFlag_(false),
+      killTrack_(false) {
   // set Process Sub Type
   SetProcessSubType(static_cast<int>(STEP_LIMITER));
-  minStepLimit_ = p.getParameter<double>("MinStepLimit") * CLHEP::mm; 
+  minStepLimit_ = p.getParameter<double>("MinStepLimit") * CLHEP::mm;
   trcut_ = new CMSTrackingCutModel(part);
 }
 
-ElectronLimiter::~ElectronLimiter() { delete trcut_;}
+ElectronLimiter::~ElectronLimiter() { delete trcut_; }
 
-void ElectronLimiter::InitialiseProcess(const G4ParticleDefinition*) {
-
-  G4LossTableManager* lManager = G4LossTableManager::Instance();
-  if(rangeCheckFlag_) { 
+void ElectronLimiter::InitialiseProcess(const G4ParticleDefinition *) {
+  G4LossTableManager *lManager = G4LossTableManager::Instance();
+  if (rangeCheckFlag_) {
     ionisation_ = lManager->GetEnergyLossProcess(particle_);
-    if(!ionisation_) {
+    if (!ionisation_) {
       rangeCheckFlag_ = false;
     }
   }
   AddEmModel(0, new G4DummyModel());
 
-  if(lManager->IsMaster()) {
+  if (lManager->IsMaster()) {
     edm::LogVerbatim("SimG4CoreApplication")
-      << "ElectronLimiter::BuildPhysicsTable for " 
-      << particle_->GetParticleName()
-      << " ioni: " << ionisation_ << " rangeCheckFlag: " << rangeCheckFlag_
-      << " fieldCheckFlag: " << fieldCheckFlag_ 
-      << " " << nRegions_ << " regions for tracking cuts\n";
+        << "ElectronLimiter::BuildPhysicsTable for " << particle_->GetParticleName() << " ioni: " << ionisation_
+        << " rangeCheckFlag: " << rangeCheckFlag_ << " fieldCheckFlag: " << fieldCheckFlag_ << " " << nRegions_
+        << " regions for tracking cuts\n";
   }
 }
 
@@ -57,21 +59,21 @@ G4double ElectronLimiter::PostStepGetPhysicalInteractionLength(const G4Track &aT
 
   G4double kinEnergy = aTrack.GetKineticEnergy();
   if (0 < nRegions_) {
-    if(regions_[0] == nullptr) {
-      if(kinEnergy < energyLimits_[0]) {
-	killTrack_ = true;
+    if (regions_[0] == nullptr) {
+      if (kinEnergy < energyLimits_[0]) {
+        killTrack_ = true;
         trcut_->InitialiseForStep(factors_[0], rms_[0]);
         limit = 0.0;
       }
-    } else {      
-      const G4Region* reg = aTrack.GetVolume()->GetLogicalVolume()->GetRegion();
-      for(G4int i=0; i<nRegions_; ++i) {
-	if(reg == regions_[i] && kinEnergy < energyLimits_[i]) {
-	  killTrack_ = true;
-	  trcut_->InitialiseForStep(factors_[i], rms_[i]);
-	  limit = 0.0;
-	  break;
-	}
+    } else {
+      const G4Region *reg = aTrack.GetVolume()->GetLogicalVolume()->GetRegion();
+      for (G4int i = 0; i < nRegions_; ++i) {
+        if (reg == regions_[i] && kinEnergy < energyLimits_[i]) {
+          killTrack_ = true;
+          trcut_->InitialiseForStep(factors_[i], rms_[i]);
+          limit = 0.0;
+          break;
+        }
       }
     }
   }
@@ -91,7 +93,7 @@ G4double ElectronLimiter::PostStepGetPhysicalInteractionLength(const G4Track &aT
   return limit;
 }
 
-G4VParticleChange* ElectronLimiter::PostStepDoIt(const G4Track& aTrack, const G4Step&) {
+G4VParticleChange *ElectronLimiter::PostStepDoIt(const G4Track &aTrack, const G4Step &) {
   fParticleChange.Initialize(aTrack);
   if (killTrack_) {
     fParticleChange.ProposeTrackStatus(fStopAndKill);
@@ -102,18 +104,16 @@ G4VParticleChange* ElectronLimiter::PostStepDoIt(const G4Track& aTrack, const G4
   return &fParticleChange;
 }
 
-G4bool ElectronLimiter::IsApplicable(const G4ParticleDefinition&) {
-  return true;
-}
+G4bool ElectronLimiter::IsApplicable(const G4ParticleDefinition &) { return true; }
 
-void ElectronLimiter::StartTracking(G4Track*) {}
+void ElectronLimiter::StartTracking(G4Track *) {}
 
-void ElectronLimiter::SetTrackingCutPerRegion(std::vector<const G4Region*> &reg, 
+void ElectronLimiter::SetTrackingCutPerRegion(std::vector<const G4Region *> &reg,
                                               std::vector<G4double> &cut,
                                               std::vector<G4double> &fac,
                                               std::vector<G4double> &rms) {
   nRegions_ = reg.size();
-  if(nRegions_ > 0) {
+  if (nRegions_ > 0) {
     regions_ = reg;
     energyLimits_ = cut;
     factors_ = fac;
