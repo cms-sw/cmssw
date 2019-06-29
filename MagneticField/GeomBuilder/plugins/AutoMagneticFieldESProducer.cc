94cdb14590f927e464d74673e3d8eb910d08aee1
Matti Kortelainen <matti.kortelainen@cern.ch>
2019-06-29 23:18:39 +0200

    Modernize AutoMagneticFieldESProducer
    - Merge header to source
    - Remove ParameterSet member
    - Make members const, set them in constructor initializer list
    - Use trailing underscore for member variables
    - Reduce copying of strings
    - Use ESGetToken

--- MagneticField/GeomBuilder/plugins/AutoMagneticFieldESProducer.cc
+++ MagneticField/GeomBuilder/plugins/AutoMagneticFieldESProducer.cc
@@ -1,78 +1,91 @@
-/** \file
+/** \class AutoMagneticFieldESProducer
+ *
+ *  Produce a magnetic field map corresponding to the current
+ *  recorded in the condidtion DB.
  *
  *  \author Nicola Amapane 11/08
  */
 
-#include "MagneticField/GeomBuilder/plugins/AutoMagneticFieldESProducer.h"
+#include "FWCore/Framework/interface/ESProducer.h"
+#include "FWCore/ParameterSet/interface/ParameterSet.h"
+#include "FWCore/Utilities/interface/ESProductTag.h"
+#include "FWCore/Utilities/interface/Exception.h"
+#include "FWCore/MessageLogger/interface/MessageLogger.h"
 
+#include "MagneticField/Engine/interface/MagneticField.h"
 #include "MagneticField/GeomBuilder/src/MagGeoBuilderFromDDD.h"
-
 #include "MagneticField/Records/interface/IdealMagneticFieldRecord.h"
 
 #include "CondFormats/RunInfo/interface/RunInfo.h"
 #include "CondFormats/DataRecord/interface/RunSummaryRcd.h"
-
-#include "FWCore/Framework/interface/ESHandle.h"
-#include "FWCore/Framework/interface/EventSetup.h"
-#include "FWCore/Utilities/interface/Exception.h"
-#include <FWCore/MessageLogger/interface/MessageLogger.h>
 
 #include <string>
 #include <sstream>
 #include <iostream>
 #include <vector>
 
+namespace magneticfield {
+  class AutoMagneticFieldESProducer : public edm::ESProducer {
+  public:
+    AutoMagneticFieldESProducer(const edm::ParameterSet&);
+    ~AutoMagneticFieldESProducer() override;
+
+    std::unique_ptr<MagneticField> produce(const IdealMagneticFieldRecord&);
+
+  private:
+    const std::string& closerModel(float current) const;
+    const std::vector<int> nominalCurrents_;
+    const std::vector<std::string> maps_;
+    const int overrideCurrent_;
+    edm::ESGetToken<MagneticField, IdealMagneticFieldRecord> magFieldToken_;
+  };
+}  // namespace magneticfield
+
 using namespace std;
 using namespace edm;
 using namespace magneticfield;
 
-AutoMagneticFieldESProducer::AutoMagneticFieldESProducer(const edm::ParameterSet& iConfig) : pset(iConfig) {
-  setWhatProduced(this, pset.getUntrackedParameter<std::string>("label", ""));
-  nominalCurrents = pset.getUntrackedParameter<vector<int> >("nominalCurrents");
-  maps = pset.getUntrackedParameter<vector<string> >("mapLabels");
+AutoMagneticFieldESProducer::AutoMagneticFieldESProducer(const edm::ParameterSet& iConfig)
+    : nominalCurrents_{iConfig.getUntrackedParameter<vector<int>>("nominalCurrents")},
+      maps_{iConfig.getUntrackedParameter<vector<string>>("mapLabels")},
+      overrideCurrent_{iConfig.getParameter<int>("valueOverride")} {
+  auto cc = setWhatProduced(this, iConfig.getUntrackedParameter<std::string>("label", ""));
 
-  if (maps.empty() || (maps.size() != nominalCurrents.size())) {
+  if (maps_.empty() || (maps_.size() != nominalCurrents_.size())) {
     throw cms::Exception("InvalidParameter") << "Invalid values for parameters \"nominalCurrents\" and \"maps\"";
+  }
+
+  if (overrideCurrent_ < 0) {
+    cc.setMayConsume(
+        magFieldToken_,
+        [this](const auto& get, edm::ESTransientHandle<RunInfo> runInfo) {
+          const auto current = runInfo->m_avg_current;
+          const auto& model = closerModel(current);
+          edm::LogInfo("MagneticField|AutoMagneticField")
+              << "Current: " << current << "(from RunInfo DB); using map with label: " << model;
+          return get("", model);
+        },
+        edm::ESProductTag<RunInfo, RunInfoRcd>("", ""));
+  } else {
+    const auto& model = closerModel(overrideCurrent_);
+    edm::LogInfo("MagneticField|AutoMagneticField")
+        << "Current: " << overrideCurrent_ << "(from valueOverride card); using map with label: " << model;
+    cc.setConsumes(magFieldToken_, edm::ESInputTag{"", model});
   }
 }
 
 AutoMagneticFieldESProducer::~AutoMagneticFieldESProducer() {}
 
 std::unique_ptr<MagneticField> AutoMagneticFieldESProducer::produce(const IdealMagneticFieldRecord& iRecord) {
-  float current = pset.getParameter<int>("valueOverride");
-
-  string message;
-
-  if (current < 0) {
-    ESHandle<RunInfo> rInfo;
-    iRecord.getRecord<RunInfoRcd>().get(rInfo);
-    current = rInfo->m_avg_current;
-    message = " (from RunInfo DB)";
-  } else {
-    message = " (from valueOverride card)";
-  }
-
-  string model = closerModel(current);
-
-  edm::LogInfo("MagneticField|AutoMagneticField")
-      << "Current: " << current << message << "; using map with label: " << model;
-
-  edm::ESHandle<MagneticField> map;
-
-  iRecord.get(model, map);
-
-  MagneticField* result = map.product()->clone();
-
-  return std::unique_ptr<MagneticField>(result);
+  return std::unique_ptr<MagneticField>(iRecord.get(magFieldToken_).clone());
 }
-
-std::string AutoMagneticFieldESProducer::closerModel(float current) {
+const std::string& AutoMagneticFieldESProducer::closerModel(float current) const {
   int i = 0;
-  for (; i < (int)maps.size() - 1; i++) {
-    if (2 * current < nominalCurrents[i] + nominalCurrents[i + 1])
-      return maps[i];
+  for (; i < (int)maps_.size() - 1; i++) {
+    if (2 * current < nominalCurrents_[i] + nominalCurrents_[i + 1])
+      return maps_[i];
   }
-  return maps[i];
+  return maps_[i];
 }
 
 #include "FWCore/Framework/interface/ModuleFactory.h"
