d7ef3514316b7d9622a505cfe180297bf1839657
Carl Vuosalo <cvuosalo@cern.ch>
2019-07-03 01:20:47 +0200

    Condensed commit for mf geom builder migration to DD4hep

--- MagneticField/GeomBuilder/src/bSlab.h
+++ MagneticField/GeomBuilder/src/bSlab.h
@@ -9,37 +9,51 @@
  *  \author N. Amapane - INFN Torino
  */
 
-#include "MagneticField/GeomBuilder/src/MagGeoBuilderFromDDD.h"
-#include "MagneticField/GeomBuilder/src/volumeHandle.h"
-#include "DataFormats/GeometryVector/interface/Pi.h"
+#include "DataFormats/GeometryVector/interface/Phi.h"
+
+#include "MagneticField/VolumeGeometry/interface/MagVolume6Faces.h"
+#include "MagneticField/Layers/interface/MagBSlab.h"
+#include "MagneticField/Layers/interface/MagVerbosity.h"
+#include "MagneticField/GeomBuilder/src/BaseVolumeHandle.h"
+
+#include "Utilities/General/interface/precomputed_value_sort.h"
+
+#include <iostream>
+#include <vector>
 
 class MagBSlab;
 
-class MagGeoBuilderFromDDD::bSlab {
-public:
-  /// Constructor from list of volumes
-  bSlab(handles::const_iterator begin, handles::const_iterator end);
+namespace magneticfield {
 
-  /// Destructor
-  ~bSlab();
+  using std::vector;
 
-  /// Distance from center along sector normal.
-  const float RN() const { return volumes.front()->RN(); }
+  class bSlab {
+  public:
+    /// Constructor from list of volumes
+    bSlab(handles::const_iterator startIter, handles::const_iterator endIter, bool debugVal = false);
 
-  /// Boundary in phi.
-  // FIXME: use volumeHandle [max|min]Phi, which returns phi at median of
-  // phi plane (not absolute limits). Used by: bRod ctor (only for dphi)
-  Geom::Phi<float> minPhi() const;
+    /// Destructor
+    ~bSlab() = default;
 
-  /// Boundary in phi.
-  Geom::Phi<float> maxPhi() const;
+    /// Distance from center along sector normal.
+    const float RN() const { return volumes.front()->RN(); }
 
-  /// Construct the MagBSlab upon request.
-  MagBSlab* buildMagBSlab() const;
+    /// Boundary in phi.
+    // FIXME: use volumeHandle [max|min]Phi, which returns phi at median of
+    // phi plane (not absolute limits). Used by: bRod ctor (only for dphi)
+    Geom::Phi<float> minPhi() const;
 
-private:
-  handles volumes;  // pointers to all volumes in the slab
-  mutable MagBSlab* mslab;
-};
+    /// Boundary in phi.
+    Geom::Phi<float> maxPhi() const;
+
+    /// Construct the MagBSlab upon request.
+    MagBSlab* buildMagBSlab() const;
+
+  private:
+    handles volumes;  // pointers to all volumes in the slab
+    mutable MagBSlab* mslab;
+    bool debug;  // Allow assignment
+  };
+}  // namespace magneticfield
 
 #endif
