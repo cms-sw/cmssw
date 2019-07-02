af84164bcf19e7578a677c0402a49b00c5e22854
Jason Lee <jason.lee@cern.ch>
2019-07-02 11:34:37 +0200

    code-format

--- EventFilter/GEMRawToDigi/interface/AMCdata.h
+++ EventFilter/GEMRawToDigi/interface/AMCdata.h
@@ -8,38 +8,39 @@ namespace gem {
   union AMCheader1 {
     uint64_t word;
     struct {
-      uint64_t dataLength : 20; // Always 0xfffff, use trailer dataLengthT
-      uint64_t bxID : 12;       // Bunch crossing ID
-      uint64_t l1AID : 24;      // L1A number – basically this is like event number, but reset by resync
-      uint64_t AMCnum : 4;      // Slot number of the AMC
-      uint64_t reserved : 4;    // not used
+      uint64_t dataLength : 20;  // Always 0xfffff, use trailer dataLengthT
+      uint64_t bxID : 12;        // Bunch crossing ID
+      uint64_t l1AID : 24;       // L1A number – basically this is like event number, but reset by resync
+      uint64_t AMCnum : 4;       // Slot number of the AMC
+      uint64_t reserved : 4;     // not used
     };
   };
   union AMCheader2 {
     uint64_t word;
     struct {
-      uint64_t boardID : 16;    // 8bit long GLIB serial number 
-      uint64_t orbitNum : 16;   // Orbit number, Reset by EC0
-      uint64_t param3 : 8;      // RunPar - Controlled by software, normally used only for calibrations
-      uint64_t param2 : 8;      // RunPar - Controlled by software, normally used only for calibrations
-      uint64_t param1 : 8;      // RunPar - Controlled by software, normally used only for calibrations
-      uint64_t runType : 4;     // run types like physics, cosmics, threshold scan, latency scan, etc..
-      uint64_t formatVer : 4;   // Current format version = 0x0
+      uint64_t boardID : 16;   // 8bit long GLIB serial number
+      uint64_t orbitNum : 16;  // Orbit number, Reset by EC0
+      uint64_t param3 : 8;     // RunPar - Controlled by software, normally used only for calibrations
+      uint64_t param2 : 8;     // RunPar - Controlled by software, normally used only for calibrations
+      uint64_t param1 : 8;     // RunPar - Controlled by software, normally used only for calibrations
+      uint64_t runType : 4;    // run types like physics, cosmics, threshold scan, latency scan, etc..
+      uint64_t formatVer : 4;  // Current format version = 0x0
     };
   };
   union AMCTrailer {
     uint64_t word;
     struct {
-      uint64_t dataLength : 20; // Number of 64bit words in this event 
-      uint64_t l1AIDT : 12;     // 8bit long GLIB serial number (first 8 bits)
-      uint64_t crc : 32;        // CRC added by the AMC13
+      uint64_t dataLength : 20;  // Number of 64bit words in this event
+      uint64_t l1AIDT : 12;      // 8bit long GLIB serial number (first 8 bits)
+      uint64_t crc : 32;         // CRC added by the AMC13
     };
   };
   union EventHeader {
     uint64_t word;
     struct {
-      uint64_t ttsState : 4;    // GLIB TTS state at the moment when this event was built.
-      uint64_t pType : 4;       // Payload type: can be one refering to different zero suppression schemes in normal data taking or calibration type. Note in calibration type the entire GCT (GEM Chamber Trailer) is skipped
+      uint64_t ttsState : 4;  // GLIB TTS state at the moment when this event was built.
+      uint64_t
+          pType : 4;  // Payload type: can be one refering to different zero suppression schemes in normal data taking or calibration type. Note in calibration type the entire GCT (GEM Chamber Trailer) is skipped
       uint64_t pVer : 3;        // Version of the payload type
       uint64_t davCnt : 5;      // Number of chamber blocks in this event
       uint64_t buffState : 24;  // Buffer status, Always 0 in current fw
@@ -49,19 +50,21 @@ namespace gem {
   union EventTrailer {
     uint64_t word;
     struct {
-      uint64_t BCL : 4;         // 1st bit, BC0 locked - If 0, this is a bad condition indicating a problem in the clock or TTC command stream (critical condition)
-      uint64_t DR : 1;          // DAQ Ready - If 0, this means that AMC13 is not ready to take data (critical condition)
-      uint64_t CL : 1;          // DAQ clock locked- If 0, this indicates a problem in the DAQ clock (critical condition)
-      uint64_t ML : 1;          // MMCM locked - Should always be 1
-      uint64_t BP : 1;          // Backpressure - If this is 1, it means that we are receiving backpressure from AMC13
-      uint64_t oosGlib : 40;    // GLIB is out‐of‐sync (critical): L1A ID is different for different chambers in this event (1 bit)
-      uint64_t linkTo : 24;     // Link timeout flags (one bit for each link indicating timeout condition)
+      uint64_t
+          BCL : 4;  // 1st bit, BC0 locked - If 0, this is a bad condition indicating a problem in the clock or TTC command stream (critical condition)
+      uint64_t DR : 1;  // DAQ Ready - If 0, this means that AMC13 is not ready to take data (critical condition)
+      uint64_t CL : 1;  // DAQ clock locked- If 0, this indicates a problem in the DAQ clock (critical condition)
+      uint64_t ML : 1;  // MMCM locked - Should always be 1
+      uint64_t BP : 1;  // Backpressure - If this is 1, it means that we are receiving backpressure from AMC13
+      uint64_t
+          oosGlib : 40;  // GLIB is out‐of‐sync (critical): L1A ID is different for different chambers in this event (1 bit)
+      uint64_t linkTo : 24;  // Link timeout flags (one bit for each link indicating timeout condition)
     };
   };
 
   class AMCdata {
   public:
-  AMCdata() : amch1_(0), amch2_(0), amct_(0), eh_(0), et_(0) {};
+    AMCdata() : amch1_(0), amch2_(0), amct_(0), eh_(0), et_(0){};
     ~AMCdata() { gebd_.clear(); }
 
     void setAMCheader1(uint64_t word) { amch1_ = word; }
@@ -94,7 +97,7 @@ namespace gem {
     uint8_t param1() const { return AMCheader2{amch2_}.param1; }
     uint8_t runType() const { return AMCheader2{amch2_}.runType; }
     uint8_t formatVer() const { return AMCheader2{amch2_}.formatVer; }
- 
+
     uint16_t ttsState() const { return EventHeader{eh_}.ttsState; }
     uint8_t davCnt() const { return EventHeader{eh_}.davCnt; }
     uint32_t buffState() const { return EventHeader{eh_}.buffState; }
@@ -107,7 +110,7 @@ namespace gem {
     uint8_t backPressure() const { return EventTrailer{et_}.BP; }
     uint8_t oosGlib() const { return EventTrailer{et_}.oosGlib; }
     uint32_t linkTo() const { return EventTrailer{et_}.linkTo; }
- 
+
     //!Adds GEB data to vector
     void addGEB(GEBdata g) { gebd_.push_back(g); }
     //!Returns a vector of GEB data
@@ -120,7 +123,7 @@ namespace gem {
     uint64_t eh_;
     uint64_t et_;
 
-    std::vector<GEBdata> gebd_; ///<Vector of GEB data
+    std::vector<GEBdata> gebd_;  ///<Vector of GEB data
   };
-} // namespace gem
+}  // namespace gem
 #endif
--- EventFilter/GEMRawToDigi/src/AMCdata.cc
+++ EventFilter/GEMRawToDigi/src/AMCdata.cc
@@ -3,9 +3,8 @@
 
 using namespace gem;
 
-void AMCdata::setAMCheader1(uint32_t dataLength, uint16_t bxID, uint32_t l1AID, uint8_t AMCnum)
-{
-  AMCheader1 u{0};  
+void AMCdata::setAMCheader1(uint32_t dataLength, uint16_t bxID, uint32_t l1AID, uint8_t AMCnum) {
+  AMCheader1 u{0};
   u.dataLength = dataLength;
   u.bxID = bxID;
   u.l1AID = l1AID;
@@ -18,8 +17,7 @@ void AMCdata::setAMCheader1(uint32_t dataLength, uint16_t bxID, uint32_t l1AID, 
   amct_ = ut.word;
 }
 
-void AMCdata::setAMCheader2(uint16_t boardID, uint16_t orbitNum, uint8_t runType)
-{
+void AMCdata::setAMCheader2(uint16_t boardID, uint16_t orbitNum, uint8_t runType) {
   AMCheader2 u{0};
   u.boardID = boardID;
   u.orbitNum = orbitNum;
@@ -27,17 +25,16 @@ void AMCdata::setAMCheader2(uint16_t boardID, uint16_t orbitNum, uint8_t runType
   amch2_ = u.word;
 }
 
-void AMCdata::setGEMeventHeader(uint8_t davCnt, uint32_t davList)
-{
+void AMCdata::setGEMeventHeader(uint8_t davCnt, uint32_t davList) {
   EventHeader u{0};
   u.davCnt = davCnt;
   u.davList = davList;
   eh_ = u.word;
 
   EventTrailer ut{0};
-  ut.BCL = 1;  
-  ut.DR = 1;  
-  ut.CL = 1;  
-  ut.ML = 1;  
+  ut.BCL = 1;
+  ut.DR = 1;
+  ut.CL = 1;
+  ut.ML = 1;
   et_ = ut.word;
 }
