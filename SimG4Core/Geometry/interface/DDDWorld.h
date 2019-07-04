20f2e8ec2a276a8b3323ea59f1ca0c9a18d60c5c
Vladimir <Vladimir.Ivantchenko@cern.ch>
2019-07-04 17:42:28 +0200

    cleanup initialisation of Geant4

--- SimG4Core/Geometry/interface/DDDWorld.h
+++ SimG4Core/Geometry/interface/DDDWorld.h
@@ -17,17 +17,11 @@ public:
   DDDWorld(const DDCompactView *, G4LogicalVolumeToDDLogicalPartMap &, SensitiveDetectorCatalog &, bool check = false);
   DDDWorld(const cms::DDDetector *, dd4hep::sim::Geant4GeometryMaps::VolumeMap &);
   ~DDDWorld();
-  static void WorkerSetAsWorld(G4VPhysicalVolume *pv);
-  const G4VPhysicalVolume *GetWorldVolume() const { return m_world; }
-
-  // In order to share the world volume with the worker threads, we
-  // need a non-const pointer. Thread-safety is handled inside Geant4
-  // with TLS. Should we consider a friend declaration here in order
-  // to avoid misuse?
-  G4VPhysicalVolume *GetWorldVolumeForWorker() const { return m_world; }
+  G4VPhysicalVolume *GetWorldVolume() const { return m_world; }
 
 private:
   void SetAsWorld(G4VPhysicalVolume *pv);
+
   G4VPhysicalVolume *m_world;
 };
 
