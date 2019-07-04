2237467c71efeaffbee9e8f1c79bc9cc32fe70de
Kenichi Hatakeyama <kenichi.hatakeyama@cern.ch>
2019-07-03 21:38:08 -0500

    first attempt to produce PF blocks with tracks and HF clusters, then make charged hadrons from them.

--- DataFormats/ParticleFlowReco/src/PFTrack.cc
+++ DataFormats/ParticleFlowReco/src/PFTrack.cc
@@ -57,7 +57,9 @@ void PFTrack::calculatePositionREP() {
 
 const reco::PFTrajectoryPoint& PFTrack::extrapolatedPoint(unsigned layerid) const {
   const unsigned offset_layerid = nTrajectoryMeasurements() + layerid;
-  if (layerid >= reco::PFTrajectoryPoint::NLayers || offset_layerid >= trajectoryPoints_.size()) {
+  //if (layerid >= reco::PFTrajectoryPoint::NLayers || offset_layerid >= trajectoryPoints_.size()) {
+  //KH hack to make tracks extrapolated to HF work
+  if (layerid >= reco::PFTrajectoryPoint::NLayers) {
     throw cms::Exception("SizeError") << "PFRecTrack::extrapolatedPoint: cannot access " << layerid
                                       << " #traj meas = " << nTrajectoryMeasurements()
                                       << " #traj points = " << trajectoryPoints_.size() << endl
