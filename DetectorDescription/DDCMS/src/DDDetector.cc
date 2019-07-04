ee9d6308a6c8438e5140138106b25aeb2485b8c4
Ianna Osborne <ianna.osborne@cern.ch>
2019-07-04 11:00:37 +0200

    Update Ecal Barrel New algorithm

--- DetectorDescription/DDCMS/src/DDDetector.cc
+++ DetectorDescription/DDCMS/src/DDDetector.cc
@@ -19,6 +19,18 @@ DDDetector::~DDDetector() { Detector::destroyInstance(m_tag); }
 
 void DDDetector::process(const string& fileName) {
   std::string name("DD4hep_CompactLoader");
+//   ClhepEvaluator::ClhepEvaluator() {
+// 0012   // enable standard mathematical funtions
+// 0013   evaluator_.setStdMath();
+// 0014 
+// 0015   // set Geant4 compatible units
+// 0016   evaluator_.setSystemOfUnits(1.e+3, 1. / 1.60217733e-25, 1.e+9, 1. / 1.60217733e-10, 1.0, 1.0, 1.0);
+// 0017 
+// 0018   // set some global vars, which are in fact known by Clhep::SystemOfUnits
+// 0019   // but are NOT set in CLHEP::Evaluator ...
+// 0020   evaluator_.setVariable("mum", "1.e-3*mm");
+// 0021   evaluator_.setVariable("fm", "1.e-15*meter");
+// 0022 }
   const char* files[] = {fileName.c_str(), nullptr};
   m_description->apply(name.c_str(), 2, (char**)files);
 }
