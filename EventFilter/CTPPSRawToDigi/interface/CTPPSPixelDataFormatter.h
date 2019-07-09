608eb59b0aee9c97c9a13df5422e1e0f3471e435
Luiz Mundim <luiz.mundim@cern.ch>
2019-07-09 14:14:56 +0200

    Modification needed in CTPPSRawToDigi in order to implemente PPS Full simulation

--- EventFilter/CTPPSRawToDigi/interface/CTPPSPixelDataFormatter.h
+++ EventFilter/CTPPSRawToDigi/interface/CTPPSPixelDataFormatter.h
@@ -30,57 +30,87 @@
  * The CTPPSPixelDataFormatter interpret/format ONLY detector data words
  * (not FED headers or trailer, which are treated elsewhere).
  */
-//
 
-#include "DataFormats/CTPPSDigi/interface/CTPPSPixelDigi.h"
-#include "DataFormats/CTPPSDigi/interface/CTPPSPixelDataError.h"
-#include "CondFormats/CTPPSReadoutObjects/interface/CTPPSPixelDAQMapping.h"
+#include "DataFormats/CTPPSDigi/interface/CTPPSPixelDigi.h" 
+#include "DataFormats/CTPPSDigi/interface/CTPPSPixelDataError.h" 
+#include "CondFormats/CTPPSReadoutObjects/interface/CTPPSPixelDAQMapping.h" 
 #include "DataFormats/Common/interface/DetSetVector.h"
 
 #include "EventFilter/CTPPSRawToDigi/interface/RPixErrorChecker.h"
 
+#include "CondFormats/CTPPSReadoutObjects/interface/CTPPSPixelIndices.h"
+#include "EventFilter/CTPPSRawToDigi/interface/CTPPSElectronicIndex.h"
+#include "FWCore/Utilities/interface/typedefs.h"
+
 #include <cstdint>
 #include <vector>
 #include <map>
+#include <unordered_map>
 
 class FEDRawData;
 class RPixErrorChecker;
 
 class CTPPSPixelDataFormatter {
-public:
-  typedef edm::DetSetVector<CTPPSPixelDigi> Collection;
 
-  typedef std::map<int, FEDRawData> RawData;
-  typedef std::vector<CTPPSPixelDigi> DetDigis;
+  public:
 
-  typedef std::vector<CTPPSPixelDataError> DetErrors;
-  typedef std::map<uint32_t, DetErrors> Errors;
+    typedef edm::DetSetVector<CTPPSPixelDigi> Collection;
 
-  typedef uint32_t Word32;
-  typedef uint64_t Word64;
+    typedef std::unordered_map<int, FEDRawData> RawData;
+    typedef std::vector<CTPPSPixelDigi> DetDigis;
 
-  CTPPSPixelDataFormatter(std::map<CTPPSPixelFramePosition, CTPPSPixelROCInfo> const& mapping);
+    typedef std::vector<CTPPSPixelDataError> DetErrors;
+    typedef std::map<uint32_t, DetErrors> Errors;
 
-  void setErrorStatus(bool theErrorStatus);
+    typedef uint32_t Word32;
+    typedef uint64_t Word64;
 
-  int nWords() const { return m_WordCounter; }
+    typedef std::unordered_map<cms_uint32_t,DetDigis> Digis;
 
-  void interpretRawData(bool& errorsInEvent, int fedId, const FEDRawData& data, Collection& digis, Errors& errors);
+    CTPPSPixelDataFormatter(std::map<CTPPSPixelFramePosition, CTPPSPixelROCInfo> const &mapping);
 
-private:
-  mutable int m_WordCounter;
+    void setErrorStatus(bool theErrorStatus);
 
-  bool m_IncludeErrors;
-  RPixErrorChecker m_ErrorCheck;
+    int nWords() const { return m_WordCounter; }
 
-  int m_ADC_shift, m_PXID_shift, m_DCOL_shift, m_ROC_shift, m_LINK_shift;
-  Word32 m_LINK_mask, m_ROC_mask, m_DCOL_mask, m_PXID_mask, m_ADC_mask;
+    void interpretRawData( bool& errorsInEvent, int fedId,  const FEDRawData & data, Collection & digis, Errors & errors);
 
-  int checkError(const Word32& data) const;
+    int nDigis() const { return m_DigiCounter; }
 
-  std::string print(const Word64& word) const;
+    struct PPSPixelIndex { 
+      uint32_t id; 
+      unsigned int roc; 
+      short unsigned int rocch; 
+      short unsigned int fedid; 
+      short unsigned int fedch;
+    };
 
-  const std::map<CTPPSPixelFramePosition, CTPPSPixelROCInfo>& m_Mapping;
+    void formatRawData( unsigned int lvl1_ID, RawData & fedRawData, const Digis & digis, std::vector<PPSPixelIndex> v_iDdet2fed);
+
+    static bool compare(const PPSPixelIndex& a, const PPSPixelIndex& b) {
+      return a.id < b.id || (a.id == b.id && a.roc < b.roc);
+    }
+
+  private:
+
+    int m_WordCounter;
+
+    bool m_IncludeErrors;
+    RPixErrorChecker m_ErrorCheck;
+
+    int m_ADC_shift, m_PXID_shift, m_DCOL_shift, m_ROC_shift, m_LINK_shift;
+    Word32 m_LINK_mask, m_ROC_mask, m_DCOL_mask, m_PXID_mask, m_ADC_mask;
+
+    int checkError(const Word32& data) const;
+
+    std::string print(const Word64& word) const;
+
+    const std::map<CTPPSPixelFramePosition, CTPPSPixelROCInfo> &m_Mapping;
+
+    int m_DigiCounter;
+    int m_allDetDigis;
+    int m_hasDetDigis;
+    CTPPSPixelIndices theIndices;
 };
 
 #endif
