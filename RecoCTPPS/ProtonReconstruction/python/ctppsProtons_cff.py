885bfc01ad83638fe8f09f3a6fa0617cf1e4f020
Jan Kašpar <jan.kaspar@cern.ch>
2019-07-09 19:00:10 +0200

    Review suggestions implemented.

--- RecoCTPPS/ProtonReconstruction/python/ctppsProtons_cff.py
+++ RecoCTPPS/ProtonReconstruction/python/ctppsProtons_cff.py
@@ -14,7 +14,7 @@ from Configuration.Eras.Modifier_ctpps_2016_cff import ctpps_2016
 from Configuration.Eras.Modifier_ctpps_2017_cff import ctpps_2017
 from Configuration.Eras.Modifier_ctpps_2018_cff import ctpps_2018
 
-def ApplyDefaultSettings(ctppsProtons):
+def applyDefaultSettings(ctppsProtons):
   ctppsProtons.association_cuts_45.x_cut_apply = False
   ctppsProtons.association_cuts_45.y_cut_apply = False
   ctppsProtons.association_cuts_45.xi_cut_apply = True
@@ -27,15 +27,15 @@ def ApplyDefaultSettings(ctppsProtons):
   ctppsProtons.association_cuts_56.xi_cut_value = 0.015
   ctppsProtons.association_cuts_56.th_y_cut_apply = False
 
-ctpps_2016.toModify(ctppsProtons, ApplyDefaultSettings) # applied for all Run2 years (2016, 2017 and 2018)
+ctpps_2016.toModify(ctppsProtons, applyDefaultSettings) # applied for all Run2 years (2016, 2017 and 2018)
 
-def Apply2017Settings(ctppsProtons):
+def apply2017Settings(ctppsProtons):
   ctppsProtons.association_cuts_45.xi_cut_value = 0.010
   ctppsProtons.association_cuts_56.xi_cut_value = 0.015
 
-ctpps_2017.toModify(ctppsProtons, Apply2017Settings)
+ctpps_2017.toModify(ctppsProtons, apply2017Settings)
 
-def Apply2018Settings(ctppsProtons):
+def apply2018Settings(ctppsProtons):
   ctppsProtons.association_cuts_45.xi_cut_value = 0.013
   ctppsProtons.association_cuts_45.th_y_cut_apply = True
   ctppsProtons.association_cuts_45.th_y_cut_value = 30E-6
@@ -44,4 +44,4 @@ def Apply2018Settings(ctppsProtons):
   ctppsProtons.association_cuts_56.th_y_cut_apply = True
   ctppsProtons.association_cuts_56.th_y_cut_value = 20E-6
 
-ctpps_2018.toModify(ctppsProtons, Apply2018Settings)
+ctpps_2018.toModify(ctppsProtons, apply2018Settings)
