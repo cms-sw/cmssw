edeca28da718897ce0e083717aa5920deb5d8551
Fabio Cossutti <fabio.cossutti@ts.infn.it>
2019-07-10 21:43:41 +0200

    Revert "Primitive RPC trigger"

--- L1Trigger/RPCTriggerPrimitives/plugins/PrimitiveAlgoFactory.cc
+++
@@ -1,4 +0,0 @@
-#include "FWCore/PluginManager/interface/PluginFactory.h"
-#include "L1Trigger/RPCTriggerPrimitives/plugins/PrimitiveAlgoFactory.h"
-
-EDM_REGISTER_PLUGINFACTORY(PrimitiveAlgoFactory, "PrimitiveAlgoFactory");
