b4caff34397dbf51880c5143b35535b8893e7c94
Yechan Kang <yechan.kang@cern.ch>
2019-07-12 20:28:20 +0200

    remove PSet from muonGEMDigis_cfi

--- SimMuon/GEMDigitizer/python/muonGEMDigis_cfi.py
+++ SimMuon/GEMDigitizer/python/muonGEMDigis_cfi.py
@@ -1,11 +1,5 @@
 import FWCore.ParameterSet.Config as cms
 
-gemDigiCommonParameters = cms.PSet(
-    instLumi = cms.double(7.5), # in units of 1E34 cm^-2 s^-1. Internally the background is parametrized from FLUKA+GEANT results at 5x10^34 (PU140). We are adding a 1.5 factor for PU200
-    rateFact = cms.double(1.0), # Set this factor to 1 since the new background model includes the new beam pipe and the relevant effects, so no need of higher safety factor. keeping is here is just for backward compatibiliy
-    referenceInstLumi = cms.double(5.), #In units of 10^34 Hz/cm^2. Internally the functions based on the FLUKA+GEANT simulation are normalized to 5x10^34 Hz/cm^2, this is needed to rescale them properly
-)
-
 from SimMuon.GEMDigitizer.simMuonGEMDigisDef_cfi import *
 simMuonGEMDigis = simMuonGEMDigisDef.clone()
 
