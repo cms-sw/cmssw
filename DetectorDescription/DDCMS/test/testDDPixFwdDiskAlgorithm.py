473c9431590e77fb84b91f4c2e981ff6268cd155
Andres Vargas <vargasa@cua.edu>
2019-07-13 17:17:27 +0200

    Test DDPixFwdDiskAlgo

---
+++ DetectorDescription/DDCMS/test/testDDPixFwdDiskAlgorithm.py
@@ -0,0 +1,51 @@
+import FWCore.ParameterSet.Config as cms
+
+process = cms.Process("DDCMSAlgorithmTest")
+
+process.source = cms.Source("EmptySource")
+
+process.maxEvents = cms.untracked.PSet( input = cms.untracked.int32(1) )
+
+process.MessageLogger = cms.Service(
+    "MessageLogger",
+    statistics = cms.untracked.vstring('cout', 'testDDPixFwdAlgo'),
+    categories = cms.untracked.vstring('PixFwDGeom'),
+    cout = cms.untracked.PSet(
+        threshold = cms.untracked.string('WARNING'),
+        noLineBreaks = cms.untracked.bool(True)
+        ),
+    testDDPixFwdAlgo = cms.untracked.PSet(
+        INFO = cms.untracked.PSet(
+            limit = cms.untracked.int32(0)
+            ),
+        noLineBreaks = cms.untracked.bool(True),
+        DEBUG = cms.untracked.PSet(
+            limit = cms.untracked.int32(0)
+            ),
+        WARNING = cms.untracked.PSet(
+            limit = cms.untracked.int32(0)
+            ),
+        ERROR = cms.untracked.PSet(
+            limit = cms.untracked.int32(0)
+            ),
+        threshold = cms.untracked.string('INFO'),
+        PixFwdGeom = cms.untracked.PSet(
+            limit = cms.untracked.int32(-1)
+            )
+        ),
+    destinations = cms.untracked.vstring('cout',
+                                         'testDDPixFwdAlgo')
+    )
+
+process.DDDetectorESProducer = cms.ESSource("DDDetectorESProducer",
+    confGeomXMLFiles = cms.FileInPath(
+        'DetectorDescription/DDCMS/data/cms-test-pixfwd-disk-algorithm.xml'
+    ),
+    appendToDataLabel = cms.string('TestDDPixFwdDiskAlgo')
+)
+
+process.testDump = cms.EDAnalyzer("DDTestDumpFile",
+    DDDetector = cms.ESInputTag('TestDDPixFwdDiskAlgo')
+)
+
+process.p = cms.Path(process.testDump)
