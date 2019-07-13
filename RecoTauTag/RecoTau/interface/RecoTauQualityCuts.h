7219dff735702e8d8c8ff7337dd8ff7546c1d43b
Slava Krutelyov <slava77@cern.ch>
2019-07-13 15:31:35 +0200

    make setLeadTrack methods non-const as they are and avoid mutable leadTrack_. This constness is not used after all.

--- RecoTauTag/RecoTau/interface/RecoTauQualityCuts.h
+++ RecoTauTag/RecoTau/interface/RecoTauQualityCuts.h
@@ -46,12 +46,12 @@ namespace reco {
       void setPV(const reco::VertexRef& vtx) const { pv_ = vtx; }
 
       /// Update the leading track
-      void setLeadTrack(const reco::Track& leadTrack) const;
-      void setLeadTrack(const reco::Candidate& leadCand) const;
+      void setLeadTrack(const reco::Track& leadTrack);
+      void setLeadTrack(const reco::Candidate& leadCand);
 
       /// Update the leading track (using reference)
       /// If null, this will set the lead track ref null.
-      void setLeadTrack(const reco::CandidateRef& leadCand) const;
+      void setLeadTrack(const reco::CandidateRef& leadCand);
 
       /// Filter a single Track
       bool filterTrack(const reco::TrackBaseRef& track) const;
@@ -100,7 +100,7 @@ namespace reco {
       // The current primary vertex
       mutable reco::VertexRef pv_;
       // The current lead track references
-      mutable const reco::Track* leadTrack_;
+      const reco::Track* leadTrack_;
 
       double minTrackPt_;
       double maxTrackChi2_;
