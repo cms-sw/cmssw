4590f5d4ba0a20f4fc13295e2d376820cab49d9e
mmusich <marco.musich@cern.ch>
2019-07-13 20:39:36 +0200

    make actual use of T15 fake conditions for detector D45

--- Configuration/Geometry/python/GeometryExtended2023D45_cff.py
+++ Configuration/Geometry/python/GeometryExtended2023D45_cff.py
@@ -5,7 +5,7 @@ import FWCore.ParameterSet.Config as cms
 
 from Geometry.CMSCommonData.cmsExtendedGeometry2023D45XML_cfi import *
 from Geometry.TrackerNumberingBuilder.trackerNumberingGeometry_cfi import *
-from SLHCUpgradeSimulations.Geometry.fakeConditions_phase2TkT14_cff import *
+from SLHCUpgradeSimulations.Geometry.fakeConditions_phase2TkT15_cff import *
 from Geometry.HcalCommonData.hcalParameters_cfi      import *
 from Geometry.HcalCommonData.hcalDDDSimConstants_cfi import *
 from Geometry.HGCalCommonData.hgcalParametersInitialization_cfi import *
