1a81e07fdab4982bb715131c613531b8d5a105db
Andrius Kirilovas <akirilov@fu-c2f11-15-02.cms>
2019-07-17 10:21:36 +0200

    Added final GT and era related changes for ED

--- DQM/Integration/python/config/FrontierCondition_GT_cfi.py
+++ DQM/Integration/python/config/FrontierCondition_GT_cfi.py
@@ -1,3 +1,3 @@
 import FWCore.ParameterSet.Config as cms
 from Configuration.StandardSequences.FrontierConditions_GlobalTag_cff import *
-GlobalTag.globaltag = "106X_dataRun3_HLT_Candidate_2019_07_11_15_48_53"
+GlobalTag.globaltag = "106X_dataRun3_HLT_v3"
