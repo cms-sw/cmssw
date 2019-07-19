6484894d01449d2925dae56a0a965569160e1e84
Pieter David <pieter.david@cern.ch>
2019-07-19 13:23:46 +0200

    SiStripQualityStatistics: merge FedErr contribution later
    - change to DQMEDHarvester, construct FedErr SiStripQuality in dqmEndRun
    - copy SiStripBadModuleFedErrESSource logic to shared library
    - update bad channel merging config files

--- CalibTracker/SiStripQuality/plugins/SiStripQualityStatistics.h
+++ CalibTracker/SiStripQuality/plugins/SiStripQualityStatistics.h
@@ -6,38 +6,30 @@
 
 // user include files
 #include "FWCore/Framework/interface/Frameworkfwd.h"
-#include "FWCore/Framework/interface/EDAnalyzer.h"
-#include "DQMServices/Core/interface/DQMEDAnalyzer.h"
-#include "FWCore/Framework/interface/Event.h"
-#include "FWCore/Framework/interface/EventSetup.h"
-#include "FWCore/Framework/interface/ESHandle.h"
-#include "FWCore/ServiceRegistry/interface/Service.h"
-#include "FWCore/MessageLogger/interface/MessageLogger.h"
-#include "FWCore/ParameterSet/interface/ParameterSet.h"
+#include "DQMServices/Core/interface/DQMEDHarvester.h"
 #include "CalibFormats/SiStripObjects/interface/SiStripQuality.h"
-#include "CommonTools/TrackerMap/interface/TrackerMap.h"
-#include "CalibTracker/SiStripCommon/interface/SiStripDetInfoFileReader.h"
-#include "FWCore/ParameterSet/interface/FileInPath.h"
-#include "DQM/SiStripCommon/interface/TkHistoMap.h"
 
-#include <sstream>
+class TrackerMap;
+class TkHistoMap;
+class SiStripDetInfoFileReader;
+class SiStripFedCabling;
 
-class SiStripQualityStatistics : public DQMEDAnalyzer {
+class SiStripQualityStatistics : public DQMEDHarvester {
 public:
   explicit SiStripQualityStatistics(const edm::ParameterSet&);
   ~SiStripQualityStatistics() override;
 
-  void bookHistograms(DQMStore::IBooker&, edm::Run const&, edm::EventSetup const&) override;
-  void analyze(const edm::Event&, const edm::EventSetup&) override;
+  void endRun(edm::Run const&, edm::EventSetup const&) override;
+  void dqmEndJob(DQMStore::IBooker&, DQMStore::IGetter&) override;
 
 private:
+  void updateAndSave(const SiStripQuality* siStripQuality);
   void SetBadComponents(int, int, SiStripQuality::BadComponent&);
-  void EndJob();
 
   unsigned long long m_cacheID_;
+  edm::RunID run_;
   std::string dataLabel_;
   std::string TkMapFileName_;
-  edm::FileInPath fp_;
   bool saveTkHistoMap_;
   //Global Info
   int NTkBadComponent[4];  //k: 0=BadModule, 1=BadFiber, 2=BadApv, 3=BadStrips
@@ -50,5 +42,10 @@ private:
   TrackerMap *tkMap, *tkMapFullIOVs;
   SiStripDetInfoFileReader* reader;
   std::unique_ptr<TkHistoMap> tkhisto;
+  bool addBadCompFromFedErr_;
+  float fedErrCutoff_;
+  const TrackerTopology* tTopo_;
+  const SiStripFedCabling* fedCabling_;
+  const SiStripQuality* siStripQuality_;
 };
 #endif
