48efdf20dc57897d0b7ba2c133e4372fd510153b
Arnaud <arnaud.chiron@cern.ch>
2019-07-19 10:33:11 +0200

    change # bins and # vertices showed in recOfflineVertices histo after code-format patch.

--- Validation/RecoEgamma/plugins/ElectronMcSignalValidator.cc
+++ Validation/RecoEgamma/plugins/ElectronMcSignalValidator.cc
@@ -575,13 +575,8 @@ void ElectronMcSignalValidator::bookHistograms(DQMStore::IBooker &iBooker, edm::
   h1_recCoreNum = bookH1(iBooker, "recCoreNum", "# rec electron cores", 21, -0.5, 20.5, "N_{core}");
   h1_recTrackNum = bookH1(iBooker, "recTrackNum", "# rec gsf tracks", 41, -0.5, 40.5, "N_{track}");
   h1_recSeedNum = bookH1(iBooker, "recSeedNum", "# rec electron seeds", 101, -0.5, 100.5, "N_{seed}");
-  h1_recOfflineVertices = bookH1(iBooker, 
-                                 "recOfflineVertices", 
-                                 "# rec Offline Primary Vertices", 
-                                 81, 
-                                 -0.5, 
-                                 161.5, 
-                                 "N_{Vertices}");
+  h1_recOfflineVertices =
+      bookH1(iBooker, "recOfflineVertices", "# rec Offline Primary Vertices", 81, -0.5, 161.5, "N_{Vertices}");
   h2_scl_EoEtrueVsrecOfflineVertices = bookH2(iBooker,
                                               "scl_EoEtrueVsrecOfflineVertices",
                                               "E/Etrue vs number of primary vertices",
