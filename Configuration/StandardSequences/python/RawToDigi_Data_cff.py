fa725fa25beb2e918360f301c13b55308944400a
Dilson de Jesus Damiao <dilson@cern.ch>
2019-07-21 15:49:35 +0200

    correcting absence of MC rechits in RPCDigiMerger

--- Configuration/StandardSequences/python/RawToDigi_Data_cff.py
+++ Configuration/StandardSequences/python/RawToDigi_Data_cff.py
@@ -6,3 +6,7 @@ from Configuration.StandardSequences.RawToDigi_cff import *
 ecalDigis.DoRegional = False
 #False by default ecalDigis.DoRegional = False
 
+# RPC Merged Digis 
+muonRPCNewDigis.inputTagTwinMuxDigis = cms.InputTag('rpcTwinMuxRawToDigi')
+muonRPCNewDigis.inputTagOMTFDigis = cms.InputTag('omtfStage2Digis')
+muonRPCNewDigis.inputTagCPPFDigis = cms.InputTag('rpcCPPFRawToDigi')
