7fb01a6fb152e63ae71e77d74e4e7c1c365409b1
Laurent Forthomme <laurent.forthomme@cern.ch>
2019-07-23 14:46:07 +0200

    Matched selection to #27586 to recover full 1-to-1 correspondence

--- DQM/CTPPS/plugins/CTPPSDiamondDQMSource.cc
+++ DQM/CTPPS/plugins/CTPPSDiamondDQMSource.cc
@@ -911,8 +911,8 @@ void CTPPSDiamondDQMSource::analyze(const edm::Event& event, const edm::EventSet
           continue;
         for (const auto& lt : ds) {
           if (lt.isValid() && pixId.arm() == detId_pot.arm()) {
-            if (rechit.getOOTIndex() != CTPPSDiamondRecHit::TIMESLICE_WITHOUT_LEADING &&
-                potPlots_[detId_pot].pixelTomographyAll.count(rechit.getOOTIndex()) && rechit.getOOTIndex() >= 0 &&
+            if (rechit.getOOTIndex() != CTPPSDiamondRecHit::TIMESLICE_WITHOUT_LEADING && rechit.getOOTIndex() >= 0 &&
+                potPlots_[detId_pot].pixelTomographyAll.count(rechit.getOOTIndex()) > 0 &&
                 lt.getX0() - horizontalShiftBwDiamondPixels_ < 24)
               potPlots_[detId_pot]
                   .pixelTomographyAll.at(rechit.getOOTIndex())
