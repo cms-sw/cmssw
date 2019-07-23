03c4e07be029299f2e7a83e33c21cce19e614359
arabella <arabella.martelli@cern.ch>
2019-07-23 10:55:30 +0200

    code checks

--- RecoHGCal/TICL/plugins/SeedingRegionGlobal.h
+++ RecoHGCal/TICL/plugins/SeedingRegionGlobal.h
@@ -22,7 +22,7 @@ namespace ticl {
     SeedingRegionGlobal(const edm::ParameterSet& conf, edm::ConsumesCollector& sumes);
     ~SeedingRegionGlobal() override;
 
-    void Initialize(const edm::EventSetup& es) override {};
+    void Initialize(const edm::EventSetup& es) override{};
 
     void makeRegions(const edm::Event& ev, const edm::EventSetup& es, std::vector<TICLSeedingRegion>& result) override;
   };
