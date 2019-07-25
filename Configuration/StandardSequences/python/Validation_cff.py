d3017d2c91f7db84e1ec59bce36052917891fae5
Tanmay Mudholkar <tkmudholkar@gmail.com>
2019-07-25 18:37:02 +0200

    Moved ECAL validation sequence for phase 2 to more conventional location.

--- Configuration/StandardSequences/python/Validation_cff.py
+++ Configuration/StandardSequences/python/Validation_cff.py
@@ -103,23 +103,3 @@ for _entry in [hltvalidation_prod]:
 from Configuration.Eras.Modifier_fastSim_cff import fastSim
 fastSim.toReplaceWith(validation_prod,_validation_prod_fastsim)
 
-from Validation.EcalClusters.ecalClustersValidationSequence_cff import ecalClustersValidationSequence
-from DQM.EcalMonitorTasks.EcalMonitorTask_cfi import *
-from DQM.EcalMonitorTasks.EcalFEDMonitor_cfi import *
-from DQMOffline.Ecal.EcalZmassTask_cfi import *
-from DQMOffline.Ecal.EcalPileUpDepMonitor_cfi import *
-
-ecalDQMSequencePhase2 = cms.Sequence(
-    ecalMonitorTask +
-    ecalFEDMonitor +
-    ecalzmasstask +
-    ecalPileUpDepMonitor
-)
-
-validationECALPhase2 = cms.Sequence(
-    ecalSimHitsValidationSequence*
-    ecalDigisValidationSequence*
-    ecalRecHitsValidationSequencePhase2*
-    ecalClustersValidationSequence*
-    ecalDQMSequencePhase2
-)
