1979bc1384a7824f657df492b64e4c4fefe9c5ce
Luiz Mundim <luiz.mundim@cern.ch>
2019-07-25 15:39:09 +0200

    Initial commit for SimPPS/RPDigiProducer, cleaning the code, BuildFiles, cms code conventions adaptations, etc

---
+++ SimPPS/RPDigiProducer/plugins/RPGaussianTailNoiseAdder.h
@@ -0,0 +1,20 @@
+#ifndef SimPPS_RPDigiProducer_RP_GAUSSIAN_TAIL_NOISE_ADDER_H
+#define SimPPS_RPDigiProducer_RP_GAUSSIAN_TAIL_NOISE_ADDER_H
+
+#include "SimPPS/RPDigiProducer/plugins/RPHitChargeConverter.h"
+#include "SimPPS/RPDigiProducer/interface/RPSimTypes.h"
+
+class RPGaussianTailNoiseAdder {
+public:
+  RPGaussianTailNoiseAdder(int numStrips, double theNoiseInElectrons, double theStripThresholdInE, int verbosity);
+  simromanpot::strip_charge_map addNoise(const simromanpot::strip_charge_map &theSignal);
+
+private:
+  int numStrips_;
+  double theNoiseInElectrons;
+  double theStripThresholdInE;
+  double strips_above_threshold_prob_;
+  int verbosity_;
+};
+
+#endif  //SimPPS_RPDigiProducer_RP_GAUSSIAN_TAIL_NOISE_ADDER_H
