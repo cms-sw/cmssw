74bdd090fed4907a608da1bf34e7fb6c4512ac9f
mmusich <marco.musich@cern.ch>
2019-07-29 19:31:27 +0200

    change Global Tags to full-fledged GTs

--- Configuration/AlCa/python/autoCond.py
+++ Configuration/AlCa/python/autoCond.py
@@ -14,15 +14,15 @@ autoCond = {
     # GlobalTag for MC production (L1 Trigger Stage1) with starup-like alignment and calibrations for Run2, L1 trigger in Stage1 mode
     'run2_mc_l1stage1'  :   '110X_mcRun2_asymptotic_l1stage1_v1',
     # GlobalTag for MC production with perfectly aligned and calibrated detector for Run2
-    'run2_design'       :   '110X_mcRun2_design_Candidate_2019_07_24_16_36_56',
+    'run2_design'       :   '110X_mcRun2_design_v2',
     #GlobalTag for MC production with optimistic alignment and calibrations for Run2
-    'run2_mc'           :   '110X_mcRun2_asymptotic_Candidate_2019_07_24_12_20_37',
+    'run2_mc'           :   '110X_mcRun2_asymptotic_v2',
     # GlobalTag for MC production (cosmics) with starup-like alignment and calibrations for Run2, Strip tracker in peak mode
-    'run2_mc_cosmics'   :   '110X_mcRun2cosmics_startup_deco_Candidate_2019_07_24_12_21_07',
+    'run2_mc_cosmics'   :   '110X_mcRun2cosmics_startup_deco_v2',
     # GlobalTag for MC production (Heavy Ions collisions) with optimistic alignment and calibrations for Run2
     'run2_mc_hi'        :   '110X_mcRun2_HeavyIon_v1',
     # GlobalTag for MC production (p-Pb collisions) with realistic alignment and calibrations for Run2
-    'run2_mc_pa'        :   '110X_mcRun2_pA_Candidate_2019_07_24_12_21_09',
+    'run2_mc_pa'        :   '110X_mcRun2_pA_v2',
     # GlobalTag for Run1 data reprocessing
     'run1_data'         :   '110X_dataRun2_v1',
     # GlobalTag for Run2 data reprocessing
