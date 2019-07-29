b7d7959e7c60775f63e13acb56169da4b71b271b
nancy <nancy.marinelli@cern.ch>
2019-07-29 12:21:41 +0200

    Final (?) fix to the chi2 problem. In the end the 'source' of the different chi2 was residing in the old recovery code which has never been turned on hence the problem never spot. With this commit the recovery with the new BDT is still on. Will be switched off with the next one

--- RecoLocalCalo/EcalRecProducers/plugins/EcalRecHitProducer.cc
+++ RecoLocalCalo/EcalRecProducers/plugins/EcalRecHitProducer.cc
@@ -308,6 +308,13 @@ void EcalRecHitProducer::fillDescriptions(edm::ConfigurationDescriptions& descri
   desc.add<edm::InputTag>("eeFEToBeRecovered", edm::InputTag("ecalDetIdToBeRecovered", "eeFE"));
   desc.add<edm::InputTag>("ebDetIdToBeRecovered", edm::InputTag("ecalDetIdToBeRecovered", "ebDetId"));
   desc.add<double>("singleChannelRecoveryThreshold", 8);
+  desc.add<double>("sum8ChannelRecoveryThreshold", 0.);
+  desc.add<edm::FileInPath>("bdtWeightFileNoCracks",
+                            edm::FileInPath("RecoLocalCalo/EcalDeadChannelRecoveryAlgos/data/BDTWeights/"
+                                            "bdtgAllRH_8GT700MeV_noCracks_ZskimData2017_v1.xml"));
+  desc.add<edm::FileInPath>("bdtWeightFileCracks",
+                            edm::FileInPath("RecoLocalCalo/EcalDeadChannelRecoveryAlgos/data/BDTWeights/"
+                                            "bdtgAllRH_8GT700MeV_onlyCracks_ZskimData2017_v1.xml"));
   {
     std::vector<std::string> temp1;
     temp1.reserve(9);
