06482d456ed200f5f4a3a7b44343bfcc7ed2ce44
Ianna Osborne <ianna.osborne@cern.ch>
2019-07-30 10:04:08 +0200

    Define and use cms units

--- Geometry/EcalCommonData/plugins/dd4hep/DDEcalPreshowerAlgo.cc
+++ Geometry/EcalCommonData/plugins/dd4hep/DDEcalPreshowerAlgo.cc
@@ -1,6 +1,7 @@
 #include "DD4hep/DetFactoryHelper.h"
 #include "DetectorDescription/DDCMS/interface/DDPlugins.h"
 #include "DetectorDescription/DDCMS/interface/BenchmarkGrd.h"
+#include "DataFormats/Math/interface/CMSUnits.h"
 #include "DD4hep/Shapes.h"
 
 #include <string>
@@ -9,20 +10,7 @@
 using namespace std;
 using namespace cms;
 using namespace dd4hep;
-
-// FIXME: use local definition until dd4hep fixes the uniits
-// using namespace geant_units::operators;
-
-constexpr long double piRadians(M_PIl);              // M_PIl is long double version of pi
-constexpr long double degPerRad = 180. / piRadians;  // Degrees per radian
-constexpr double operator"" _deg(long double deg) { return deg / degPerRad; }
-constexpr double operator"" _deg(unsigned long long int deg) { return deg / degPerRad; }
-
-// Length
-constexpr double operator"" _mm(long double length) { return length * 0.1; }
-constexpr double operator"" _mm(unsigned long long int length) { return length * 0.1; }
-constexpr double operator"" _cm(long double length) { return length * 1.; }
-constexpr double operator"" _cm(unsigned long long int length) { return length * 1; }
+using namespace cms_units::operators;
 
 namespace {
   struct EcalPreshower {
