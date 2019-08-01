01aa7b1ee06b2ca4d832e70afcc607fd633a5821
Ernesto Migliore <ernesto.migliore@cern.ch>
2019-08-01 12:34:40 +0200

    restore dummy version of ListGroupsMaterialDifference.h

--- SimTracker/TrackerMaterialAnalysis/plugins/ListGroupsMaterialDifference.h
+++ SimTracker/TrackerMaterialAnalysis/plugins/ListGroupsMaterialDifference.h
@@ -1,63 +1,8 @@
-
+// This file will be replaced with that created automatically when running
+// python dumpFullXML.py -p -c
 #ifndef SIMTRACKER_TRACKERMATERIALANALYSIS_LISTGROUPS_MATERIALDIFFERENCE_H
 #define SIMTRACKER_TRACKERMATERIALANALYSIS_LISTGROUPS_MATERIALDIFFERENCE_H
 
-void ListGroups::fillMaterialDifferences() {
-  m_diff["TrackerRecMaterialPhase2PixelForwardDisk11"] = std::make_pair<float, float>(75.574681, 33.016020);
-  m_diff["TrackerRecMaterialPhase2PixelForwardDisk10"] = std::make_pair<float, float>(97.977203, 61.368586);
-  m_diff["TrackerRecMaterialPhase2PixelForwardDisk12"] = std::make_pair<float, float>(0.716092, -38.004999);
-  m_diff["TrackerRecMaterialPhase2OTForwardDisk3"] = std::make_pair<float, float>(76.896023, 74.147857);
-  m_diff["TrackerRecMaterialPhase2OTForwardDisk2"] = std::make_pair<float, float>(149.463433, 172.554451);
-  m_diff["TrackerRecMaterialPhase2OTForwardDisk1"] = std::make_pair<float, float>(186.806926, 194.746940);
-  m_diff["TrackerRecMaterialPhase2OTForwardDisk5"] = std::make_pair<float, float>(30.414345, 49.141122);
-  m_diff["TrackerRecMaterialPhase2OTForwardDisk4"] = std::make_pair<float, float>(114.487740, 127.786693);
-  m_diff["TrackerRecMaterialPhase2PixelForwardDisk5"] = std::make_pair<float, float>(273.347878, 298.458434);
-  m_diff["TrackerRecMaterialPhase2PixelForwardDisk4"] = std::make_pair<float, float>(250.421588, 259.827565);
-  m_diff["TrackerRecMaterialPhase2PixelForwardDisk7"] = std::make_pair<float, float>(288.198798, 276.009749);
-  m_diff["TrackerRecMaterialPhase2PixelForwardDisk6"] = std::make_pair<float, float>(347.178600, 409.090194);
-  m_diff["TrackerRecMaterialPhase2PixelForwardDisk1"] = std::make_pair<float, float>(217.692958, 179.270426);
-  m_diff["TrackerRecMaterialPhase2PixelForwardDisk3"] = std::make_pair<float, float>(236.696869, 227.856286);
-  m_diff["TrackerRecMaterialPhase2PixelForwardDisk2"] = std::make_pair<float, float>(213.467901, 192.945026);
-  m_diff["TrackerRecMaterialPhase2OTBarrelLayer2"] = std::make_pair<float, float>(27.796548, 44.337953);
-  m_diff["TrackerRecMaterialPhase2OTBarrelLayer3"] = std::make_pair<float, float>(40.907160, 66.624743);
-  m_diff["TrackerRecMaterialPhase2OTBarrelLayer1"] = std::make_pair<float, float>(81.463884, 125.543571);
-  m_diff["TrackerRecMaterialPhase2OTBarrelLayer6"] = std::make_pair<float, float>(68.033904, 96.467072);
-  m_diff["TrackerRecMaterialPhase2PixelForwardDisk8"] = std::make_pair<float, float>(128.185171, 66.128462);
-  m_diff["TrackerRecMaterialPhase2OTBarrelLayer4"] = std::make_pair<float, float>(84.396341, 117.966916);
-  m_diff["TrackerRecMaterialPhase2OTBarrelLayer5"] = std::make_pair<float, float>(67.429350, 87.757205);
-  m_diff["TrackerRecMaterialPhase2PixelBarrelLayer1"] = std::make_pair<float, float>(42.919324, 23.146110);
-  m_diff["TrackerRecMaterialPhase2PixelBarrelLayer2"] = std::make_pair<float, float>(36.385849, -0.433207);
-  m_diff["TrackerRecMaterialPhase2PixelBarrelLayer3"] = std::make_pair<float, float>(32.708045, -6.223594);
-  m_diff["TrackerRecMaterialPhase2PixelBarrelLayer4"] = std::make_pair<float, float>(74.617151, 57.548498);
-  m_diff["TrackerRecMaterialPhase2PixelForwardDisk9"] = std::make_pair<float, float>(380.210886, 418.005722);
-  m_values["TrackerRecMaterialPhase2PixelForwardDisk11"] = std::make_pair<float, float>(0.026725, 0.000040);
-  m_values["TrackerRecMaterialPhase2PixelForwardDisk10"] = std::make_pair<float, float>(0.030135, 0.000049);
-  m_values["TrackerRecMaterialPhase2PixelForwardDisk12"] = std::make_pair<float, float>(0.015331, 0.000019);
-  m_values["TrackerRecMaterialPhase2OTForwardDisk3"] = std::make_pair<float, float>(0.043155, 0.000085);
-  m_values["TrackerRecMaterialPhase2OTForwardDisk2"] = std::make_pair<float, float>(0.063578, 0.000139);
-  m_values["TrackerRecMaterialPhase2OTForwardDisk1"] = std::make_pair<float, float>(0.073096, 0.000150);
-  m_values["TrackerRecMaterialPhase2OTForwardDisk5"] = std::make_pair<float, float>(0.031906, 0.000073);
-  m_values["TrackerRecMaterialPhase2OTForwardDisk4"] = std::make_pair<float, float>(0.052326, 0.000111);
-  m_values["TrackerRecMaterialPhase2PixelForwardDisk5"] = std::make_pair<float, float>(0.056987, 0.000122);
-  m_values["TrackerRecMaterialPhase2PixelForwardDisk4"] = std::make_pair<float, float>(0.053487, 0.000110);
-  m_values["TrackerRecMaterialPhase2PixelForwardDisk7"] = std::make_pair<float, float>(0.059254, 0.000115);
-  m_values["TrackerRecMaterialPhase2PixelForwardDisk6"] = std::make_pair<float, float>(0.068256, 0.000155);
-  m_values["TrackerRecMaterialPhase2PixelForwardDisk1"] = std::make_pair<float, float>(0.048492, 0.000085);
-  m_values["TrackerRecMaterialPhase2PixelForwardDisk3"] = std::make_pair<float, float>(0.051392, 0.000100);
-  m_values["TrackerRecMaterialPhase2PixelForwardDisk2"] = std::make_pair<float, float>(0.047847, 0.000089);
-  m_values["TrackerRecMaterialPhase2OTBarrelLayer2"] = std::make_pair<float, float>(0.050043, 0.000113);
-  m_values["TrackerRecMaterialPhase2OTBarrelLayer3"] = std::make_pair<float, float>(0.054057, 0.000128);
-  m_values["TrackerRecMaterialPhase2OTBarrelLayer1"] = std::make_pair<float, float>(0.074629, 0.000186);
-  m_values["TrackerRecMaterialPhase2OTBarrelLayer6"] = std::make_pair<float, float>(0.027239, 0.000064);
-  m_values["TrackerRecMaterialPhase2PixelForwardDisk8"] = std::make_pair<float, float>(0.034829, 0.000051);
-  m_values["TrackerRecMaterialPhase2OTBarrelLayer4"] = std::make_pair<float, float>(0.029891, 0.000071);
-  m_values["TrackerRecMaterialPhase2OTBarrelLayer5"] = std::make_pair<float, float>(0.027141, 0.000061);
-  m_values["TrackerRecMaterialPhase2PixelBarrelLayer1"] = std::make_pair<float, float>(0.025480, 0.000044);
-  m_values["TrackerRecMaterialPhase2PixelBarrelLayer2"] = std::make_pair<float, float>(0.023108, 0.000034);
-  m_values["TrackerRecMaterialPhase2PixelBarrelLayer3"] = std::make_pair<float, float>(0.020919, 0.000030);
-  m_values["TrackerRecMaterialPhase2PixelBarrelLayer4"] = std::make_pair<float, float>(0.027526, 0.000050);
-  m_values["TrackerRecMaterialPhase2PixelForwardDisk9"] = std::make_pair<float, float>(0.073095, 0.000158);
+void ListGroups::fillMaterialDifferences() {}
 
-}
-
-#endif //  SIMTRACKER_TRACKERMATERIALANALYSIS_LISTGROUPS_MATERIALDIFFERENCE_H
+#endif  //  SIMTRACKER_TRACKERMATERIALANALYSIS_LISTGROUPS_MATERIALDIFFERENCE_H
