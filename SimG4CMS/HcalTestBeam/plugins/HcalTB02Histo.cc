7ed207b15401d25f6886bf0c1a5041d947aeb292
Sunanda <sunanda.banerjee@cern.ch>
2019-08-01 17:57:59 +0200

    Move all files from interface/src to plugins for TB simulation

--- SimG4CMS/HcalTestBeam/plugins/HcalTB02Histo.cc
+++ SimG4CMS/HcalTestBeam/plugins/HcalTB02Histo.cc
@@ -15,10 +15,10 @@
 #include <cmath>
 
 // user include files
-#include "SimG4CMS/HcalTestBeam/interface/HcalTB02Histo.h"
 #include "FWCore/MessageLogger/interface/MessageLogger.h"
 #include "FWCore/ServiceRegistry/interface/Service.h"
 #include "CommonTools/UtilAlgos/interface/TFileService.h"
+#include "HcalTB02Histo.h"
 
 //
 // constructors and destructor
