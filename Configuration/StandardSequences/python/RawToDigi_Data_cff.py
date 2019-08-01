ba52657d677b55799457d4abf68d7d7ebdb39201
Dilson de Jesus Damiao <dilson@cern.ch>
2019-08-01 22:47:10 +0200

    applying suggestions

--- Configuration/StandardSequences/python/RawToDigi_Data_cff.py
+++ Configuration/StandardSequences/python/RawToDigi_Data_cff.py
@@ -7,9 +7,9 @@ ecalDigis.DoRegional = False
 #False by default ecalDigis.DoRegional = False
 
 # RPC Merged Digis 
-_muonRPCDigis = muonRPCDigis.copy()
 from Configuration.Eras.Modifier_run3_RPC_cff import run3_RPC
-_muonRPCDigis.inputTagTwinMuxDigis = cms.InputTag('rpcTwinMuxRawToDigi')
-_muonRPCDigis.inputTagOMTFDigis = cms.InputTag('omtfStage2Digis')
-_muonRPCDigis.inputTagCPPFDigis = cms.InputTag('rpcCPPFRawToDigi')
-run3_RPC.toReplaceWith(muonRPCDigis,_muonRPCDigis)
+run3_RPC.toModify(muonRPCDigis,
+    inputTagTwinMuxDigis = 'rpcTwinMuxRawToDigi',
+    inputTagOMTFDigis = 'omtfStage2Digis',
+    inputTagCPPFDigis = 'rpcCPPFRawToDigi'
+)
