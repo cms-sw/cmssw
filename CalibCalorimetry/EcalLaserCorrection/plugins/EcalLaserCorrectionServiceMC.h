c9cf67d99672f8cf1a79438de0d20b0a74862b80
amassiro <massironi.andrea@gmail.com>
2019-08-03 20:14:12 +0200

    new files

---
+++ CalibCalorimetry/EcalLaserCorrection/plugins/EcalLaserCorrectionServiceMC.h
@@ -0,0 +1,53 @@
+//
+// Toyoko Orimoto (Caltech), 10 July 2007
+// Andrea Massironi, 3 Aug 2019
+//
+
+// system include files
+#include <memory>
+
+// user include files
+#include "FWCore/Framework/interface/ModuleFactory.h"
+#include "FWCore/Framework/interface/ESProducer.h"
+#include "FWCore/Framework/interface/ESProductHost.h"
+#include "FWCore/Utilities/interface/ReusableObjectHolder.h"
+
+class EcalLaserDbServiceMC;
+class EcalLaserDbRecordMC;
+
+#include "CondFormats/DataRecord/interface/EcalLaserAlphasRcd.h"
+#include "CondFormats/DataRecord/interface/EcalLaserAPDPNRatiosRefRcd.h"
+#include "CondFormats/DataRecord/interface/EcalLaserAPDPNRatiosMCRcd.h"
+#include "CondFormats/DataRecord/interface/EcalLinearCorrectionsRcd.h"
+
+
+class EcalLaserCorrectionServiceMC : public edm::ESProducer {
+public:
+  EcalLaserCorrectionServiceMC( const edm::ParameterSet& );
+  ~EcalLaserCorrectionServiceMC() override;
+  
+  std::shared_ptr<EcalLaserDbServiceMC> produce( const EcalLaserDbRecordMC& );
+
+private:
+
+  using HostType = edm::ESProductHost<EcalLaserDbServiceMC,
+                                      EcalLaserAlphasRcd,
+                                      EcalLaserAPDPNRatiosRefRcd,
+                                      EcalLaserAPDPNRatiosMCRcd,
+                                      EcalLinearCorrectionsRcd>;
+
+  void setupAlpha(const EcalLaserAlphasRcd& fRecord,
+                  EcalLaserDbServiceMC& service);
+  void setupApdpnRef(const EcalLaserAPDPNRatiosRefRcd& fRecord,
+                     EcalLaserDbServiceMC& service);
+  void setupApdpn(const EcalLaserAPDPNRatiosMCRcd& fRecord,
+                  EcalLaserDbServiceMC& service);
+  void setupLinear(const EcalLinearCorrectionsRcd& fRecord,
+                   EcalLaserDbServiceMC& service);
+
+  // ----------member data ---------------------------
+  edm::ReusableObjectHolder<HostType> holder_;
+
+  //  std::vector<std::string> mDumpRequest;
+  //  std::ostream* mDumpStream;
+};
---
+++ CondFormats/EcalObjects/interface/EcalLaserAPDPNRatiosMC.h
@@ -0,0 +1,53 @@
+#ifndef CondFormats_EcalObjects_EcalLaserAPDPNRatiosMC_H 
+#define CondFormats_EcalObjects_EcalLaserAPDPNRatiosMC_H 
+/**
+ *Author: Vladlen Timciuc, Caltech
+ * Created: 10 July 2007
+ * $Id: EcalLaserAPDPNRatiosMC.h,v 1.5 2007/09/27 09:42:55 ferriff Exp $
+ **/
+#include "CondFormats/Serialization/interface/Serializable.h"
+
+#include "CondFormats/EcalObjects/interface/EcalCondObjectContainer.h"
+#include "DataFormats/Provenance/interface/Timestamp.h"
+#include <vector>
+
+class EcalLaserAPDPNRatiosMC {
+ public:
+  struct EcalLaserAPDPNpair{
+    EcalLaserAPDPNpair() : p1(0), p2(0), p3(0) {}
+    float p1;
+    float p2;
+    float p3;
+  
+  COND_SERIALIZABLE;
+};
+  struct EcalLaserTimeStamp{
+    EcalLaserTimeStamp() : t1(), t2(), t3() {}
+    edm::Timestamp t1;
+    edm::Timestamp t2;
+    edm::Timestamp t3;
+  
+  COND_SERIALIZABLE;
+};
+  
+  typedef EcalCondObjectContainer<EcalLaserAPDPNpair> EcalLaserAPDPNRatiosMap;
+  typedef std::vector<EcalLaserTimeStamp> EcalLaserTimeStampMap;
+
+  EcalLaserAPDPNRatiosMC() : time_map(92) {}; // FIXME
+  ~EcalLaserAPDPNRatiosMC() {};
+   
+  void  setValue(uint32_t rawId, const EcalLaserAPDPNpair& value) { laser_map[rawId] = value; };
+  const EcalLaserAPDPNRatiosMap& getLaserMap() const { return laser_map; }
+  
+  void setTime(int hashedIndex, const EcalLaserTimeStamp& value) { time_map[hashedIndex] = value; };
+  const EcalLaserTimeStampMap& getTimeMap() const { return time_map; }  
+
+ private:
+  EcalLaserAPDPNRatiosMap laser_map;
+  EcalLaserTimeStampMap time_map;
+   
+
+ COND_SERIALIZABLE;
+};
+
+#endif
