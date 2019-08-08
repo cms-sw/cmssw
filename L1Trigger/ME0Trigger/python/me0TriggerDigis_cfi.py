b4ea79f8f56f4ca1cbaeab01c71410e7b5460e5c
Sven Dildick <sven.dildick@cern.ch>
2019-08-08 19:13:07 +0200

    Allow for ME0 triggers to be produced from pads

--- L1Trigger/ME0Trigger/python/me0TriggerDigis_cfi.py
+++ L1Trigger/ME0Trigger/python/me0TriggerDigis_cfi.py
@@ -1,6 +1,8 @@
 import FWCore.ParameterSet.Config as cms
 
 me0TriggerDigis = cms.EDProducer("ME0TriggerProducer",
-    ME0PadDigiClusterProducer = cms.InputTag("simMuonME0PadDigiClusters"),
+    ME0PadDigiClusters = cms.InputTag("simMuonME0PadDigiClusters"),
+    ME0PadDigis = cms.InputTag("simMuonME0PadDigis"),
+    useClusters = cms.bool(False),
     tmbParam = cms.PSet()
 )
--- L1Trigger/ME0Trigger/src/ME0TriggerBuilder.cc
+++ L1Trigger/ME0Trigger/src/ME0TriggerBuilder.cc
@@ -1,8 +1,4 @@
 #include "L1Trigger/ME0Trigger/interface/ME0TriggerBuilder.h"
-#include "L1Trigger/ME0Trigger/interface/ME0Motherboard.h"
-#include "FWCore/MessageLogger/interface/MessageLogger.h"
-#include "Geometry/Records/interface/MuonGeometryRecord.h"
-#include "Geometry/GEMGeometry/interface/ME0Geometry.h"
 
 ME0TriggerBuilder::ME0TriggerBuilder(const edm::ParameterSet& conf) {
   config_ = conf;
@@ -21,31 +17,3 @@ ME0TriggerBuilder::ME0TriggerBuilder(const edm::ParameterSet& conf) {
 }
 
 ME0TriggerBuilder::~ME0TriggerBuilder() {}
-
-void ME0TriggerBuilder::build(const ME0PadDigiClusterCollection* me0Pads, ME0TriggerDigiCollection& oc_trig) {
-  for (int endc = 0; endc < 2; endc++) {
-    for (int cham = 0; cham < 18; cham++) {
-      ME0Motherboard* tmb = tmb_[endc][cham].get();
-      tmb->setME0Geometry(me0_g);
-
-      // 0th layer means whole chamber.
-      const int region(endc == 0 ? -1 : 1);
-      ME0DetId detid(region, 0, cham, 0);
-
-      // Run processors only if chamber exists in geometry.
-      if (tmb == nullptr || me0_g->chamber(detid) == nullptr)
-        continue;
-
-      tmb->run(me0Pads);
-
-      std::vector<ME0TriggerDigi> trigV = tmb->readoutTriggers();
-
-      if (!trigV.empty()) {
-        LogTrace("L1ME0Trigger") << "ME0TriggerBuilder got results in " << detid << std::endl
-                                 << "Put " << trigV.size() << " Trigger digi" << ((trigV.size() > 1) ? "s " : " ")
-                                 << "in collection\n";
-        oc_trig.put(std::make_pair(trigV.begin(), trigV.end()), detid);
-      }
-    }
-  }
-}
