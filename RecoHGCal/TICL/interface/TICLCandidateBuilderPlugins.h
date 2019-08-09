8406d8cc55b016898eb6eca3cd895dad04f80385
Jan Steggemann <jan.steggemann@cern.ch>
2019-08-09 16:20:00 +0200

    Working version of TICL -> PF

--- RecoHGCal/TICL/interface/TICLCandidateBuilderPlugins.h
+++ RecoHGCal/TICL/interface/TICLCandidateBuilderPlugins.h
@@ -3,18 +3,34 @@
 
 #include "DataFormats/Candidate/interface/Candidate.h"
 #include "DataFormats/VertexReco/interface/Vertex.h"
+#include "DataFormats/CaloRecHit/interface/CaloCluster.h"
 #include "DataFormats/HGCalReco/interface/Trackster.h"
+#include "FWCore/Framework/interface/ConsumesCollector.h"
+#include "FWCore/Framework/interface/Event.h"
 #include "FWCore/PluginManager/interface/PluginFactory.h"
 
 namespace ticl {
-  class TracksterMomentumBase {
+  class TracksterMomentumPluginBase {
   public:
+    // TracksterMomentumPluginBase() {}
+    TracksterMomentumPluginBase(const edm::ParameterSet&, edm::ConsumesCollector&& iC) {}
     typedef reco::Candidate::LorentzVector LorentzVector;
-    virtual ~TracksterMomentumBase(){};
-    virtual LorentzVector calcP4(const ticl::Trackster& trackster, const reco::Vertex& vertex) const = 0;
+    virtual ~TracksterMomentumPluginBase(){};
+    virtual LorentzVector calcP4(const ticl::Trackster& trackster) const = 0;
+
+    edm::Event& evt() { return *evt_; }
+    
+    // To be 
+    void beginEvent(edm::Event& evt) {
+       evt_ = &evt;
+       this->beginEvt();
+     }
+  private:
+    virtual void beginEvt() = 0;
+    edm::Event* evt_;
   };
 } // namespace
 
-typedef edmplugin::PluginFactory<ticl::TracksterMomentumBase*()> TracksterMomentumPluginFactory;
+typedef edmplugin::PluginFactory<ticl::TracksterMomentumPluginBase*(const edm::ParameterSet&, edm::ConsumesCollector&& iC)> TracksterMomentumPluginFactory;
 
 #endif
