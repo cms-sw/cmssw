a7ca47373bd56c4370c29ec87be224b1c4de0565
Ianna Osborne <ianna.osborne@cern.ch>
2019-08-11 22:41:37 +0200

    code format

--- Geometry/MTDNumberingBuilder/src/CmsMTDStringToEnum.cc
+++ Geometry/MTDNumberingBuilder/src/CmsMTDStringToEnum.cc
@@ -10,10 +10,9 @@ CmsMTDStringToEnum::Impl::Impl() {
                                                                                   GeometricTimingDet::BTL));
   _map.insert(
       std::pair<std::string, GeometricTimingDet::GeometricTimingEnumType>("Layer1", GeometricTimingDet::BTLLayer));
+  _map.insert(std::pair<std::string, GeometricTimingDet::GeometricTimingEnumType>("Rod1", GeometricTimingDet::BTLTray));
   _map.insert(
-      std::pair<std::string, GeometricTimingDet::GeometricTimingEnumType>("Rod1", GeometricTimingDet::BTLTray));
-  _map.insert(std::pair<std::string, GeometricTimingDet::GeometricTimingEnumType>("BModule",
-                                                                                  GeometricTimingDet::BTLModule));
+      std::pair<std::string, GeometricTimingDet::GeometricTimingEnumType>("BModule", GeometricTimingDet::BTLModule));
   _map.insert(std::pair<std::string, GeometricTimingDet::GeometricTimingEnumType>("SensorPackage",
                                                                                   GeometricTimingDet::BTLSensor));
   _map.insert(
@@ -23,10 +22,9 @@ CmsMTDStringToEnum::Impl::Impl() {
                                                                                   GeometricTimingDet::ETL));
   _map.insert(
       std::pair<std::string, GeometricTimingDet::GeometricTimingEnumType>("Disc1", GeometricTimingDet::ETLDisc));
+  _map.insert(std::pair<std::string, GeometricTimingDet::GeometricTimingEnumType>("Ring", GeometricTimingDet::ETLRing));
   _map.insert(
-      std::pair<std::string, GeometricTimingDet::GeometricTimingEnumType>("Ring", GeometricTimingDet::ETLRing));
-  _map.insert(std::pair<std::string, GeometricTimingDet::GeometricTimingEnumType>("EModule",
-                                                                                  GeometricTimingDet::ETLModule));
+      std::pair<std::string, GeometricTimingDet::GeometricTimingEnumType>("EModule", GeometricTimingDet::ETLModule));
   _map.insert(
       std::pair<std::string, GeometricTimingDet::GeometricTimingEnumType>("Sensor", GeometricTimingDet::ETLSensor));
 
@@ -71,7 +69,7 @@ GeometricTimingDet::GeometricTimingEnumType CmsMTDStringToEnum::type(std::string
   std::string_view v = s;
   auto first = v.find_first_of(":");
   v.remove_prefix(std::min(first + 1, v.size()));
-  MapEnumType::const_iterator p = map().find({v.data(),v.size()});
+  MapEnumType::const_iterator p = map().find({v.data(), v.size()});
   if (p != map().end())
     return p->second;
   return GeometricTimingDet::unknown;
