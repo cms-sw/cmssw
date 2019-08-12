08ad8a65163e026c1b3ec5eadf26f4b5d9745641
mmusich <marco.musich@cern.ch>
2019-08-12 15:43:14 +0200

    use range-based loop

--- CondTools/SiPixel/test/SiPixelBadModuleByHandBuilder.cc
+++ CondTools/SiPixel/test/SiPixelBadModuleByHandBuilder.cc
@@ -102,31 +102,31 @@ std::unique_ptr<SiPixelQuality> SiPixelBadModuleByHandBuilder::getNewObject() {
       //std::cout<<"New module read "<<name<<" "<<roc<<" --> "<<detId<<" "<<std::bitset<32>(it->second)<<std::endl;
     }
 
-    for (std::map<uint32_t, uint32_t>::iterator it = disabledModules.begin(); it != disabledModules.end(); it++) {
+    for (const auto& it : disabledModules) {
       SiPixelQuality::disabledModuleType BadModule;
-      BadModule.DetID = it->first;
-      if (it->second == 65535) {  // "whole"
+      BadModule.DetID = it.first;
+      if (it.second == 65535) {  // "whole"
         BadModule.errorType = 0;
         BadModule.BadRocs = 65535;
-      }                              //corresponds to all rocs being bad
-      else if (it->second == 255) {  // "tbmA"
+      }                             //corresponds to all rocs being bad
+      else if (it.second == 255) {  // "tbmA"
         BadModule.errorType = 1;
         BadModule.BadRocs = 255;
-      }                                //corresponds to Rocs 0-7 being bad
-      else if (it->second == 65280) {  // "tbmB"
+      }                               //corresponds to Rocs 0-7 being bad
+      else if (it.second == 65280) {  // "tbmB"
         BadModule.errorType = 2;
         BadModule.BadRocs = 65280;
       }       //corresponds to Rocs 8-15 being bad
       else {  // "none"
         BadModule.errorType = 3;
-        BadModule.BadRocs = it->second;
+        BadModule.BadRocs = it.second;
       }
 
       obj->addDisabledModule(BadModule);
       if (printdebug_) {
         edm::LogVerbatim("SiPixelBadModuleByHandBuilder")
             << "New module added: " << tTopo_->print(BadModule.DetID) << ", errorType: " << BadModule.errorType
-            << ", BadRocs: " << std::bitset<16>(it->second) << std::endl;
+            << ", BadRocs: " << std::bitset<16>(it.second) << std::endl;
       }
     }
   }
