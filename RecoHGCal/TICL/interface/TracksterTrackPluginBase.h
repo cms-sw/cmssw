6d500173743bde5b1db35274f9f7e2835d12bb62
Jan Steggemann <jan.steggemann@cern.ch>
2019-08-19 13:15:20 +0200

    Add track plugin to TICL candidate producer

--- RecoHGCal/TICL/interface/TracksterTrackPluginBase.h
+++ RecoHGCal/TICL/interface/TracksterTrackPluginBase.h
@@ -1,25 +1,26 @@
-#ifndef RecoHGCal_TICL_TracksterMomentumPluginBase_H__
-#define RecoHGCal_TICL_TracksterMomentumPluginBase_H__
+#ifndef RecoHGCal_TICL_TracksterTrackPluginBase_H__
+#define RecoHGCal_TICL_TracksterTrackPluginBase_H__
 
 #include "DataFormats/Candidate/interface/Candidate.h"
 #include "DataFormats/VertexReco/interface/Vertex.h"
 #include "DataFormats/CaloRecHit/interface/CaloCluster.h"
 #include "DataFormats/HGCalReco/interface/Trackster.h"
+#include "DataFormats/HGCalReco/interface/TICLCandidate.h"
 #include "FWCore/Framework/interface/ConsumesCollector.h"
 #include "FWCore/Framework/interface/Event.h"
 #include "FWCore/PluginManager/interface/PluginFactory.h"
 
 namespace ticl {
-  class TracksterMomentumPluginBase {
+  class TracksterTrackPluginBase {
   public:
-    TracksterMomentumPluginBase(const edm::ParameterSet&, edm::ConsumesCollector&& iC) {}
+    TracksterTrackPluginBase(const edm::ParameterSet&, edm::ConsumesCollector&& iC) {}
     typedef reco::Candidate::LorentzVector LorentzVector;
-    virtual ~TracksterMomentumPluginBase() {}
-    virtual LorentzVector calcP4(const ticl::Trackster& trackster) const = 0;
+    virtual ~TracksterTrackPluginBase() {}
+    virtual void setTrack(const ticl::Trackster& trackster, ticl::TICLCandidate& ticl_cand) const = 0;
 
     // Allow access to event so data products can be read once at beginning of event but four-vector calculation can 
     // be run on single tracksters
-    edm::Event& evt() { return *evt_; }
+    edm::Event& evt() const { return *evt_; }
     
     // Needs to be called by CMSSW plugins using this plugin before being able to use other methods
     void beginEvent(edm::Event& evt) {
@@ -32,6 +33,6 @@ namespace ticl {
   };
 } // namespace
 
-typedef edmplugin::PluginFactory<ticl::TracksterMomentumPluginBase*(const edm::ParameterSet&, edm::ConsumesCollector&& iC)> TracksterMomentumPluginFactory;
+typedef edmplugin::PluginFactory<ticl::TracksterTrackPluginBase*(const edm::ParameterSet&, edm::ConsumesCollector&& iC)> TracksterTrackPluginFactory;
 
 #endif
