d8f1bff40d1717c4cfb8ddfd0bed32083a02774e
Sunanda <sunanda.banerjee@cern.ch>
2019-08-19 20:59:49 +0200

    Move remaining geometry parameters for SIM step to HcalSimulationParameters

--- Geometry/HcalCommonData/interface/HcalSimParametersFromDD.h
+++ Geometry/HcalCommonData/interface/HcalSimParametersFromDD.h
@@ -5,6 +5,7 @@
 #include <string>
 
 class DDCompactView;
+class DDFilteredView;
 class HcalSimulationParameters;
 
 class HcalSimParametersFromDD {
@@ -15,6 +16,9 @@ public:
   bool build(const DDCompactView*, HcalSimulationParameters&);
 
 private:
+  void fillNameVector(const DDCompactView*, const std::string&, const std::string&, std::vector<std::string>&);
+  bool isItHF(const std::string&, const HcalSimulationParameters&);
+  std::vector<std::string> getNames(DDFilteredView& fv);
   std::vector<double> getDDDArray(const std::string& str, const DDsvalues_type& sv, int& nmin);
 };
 
