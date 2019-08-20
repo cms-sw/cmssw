35e65de89ea6ff96b346a05cb550d5c2ec7853a3
Sunanda <sunanda.banerjee@cern.ch>
2019-08-20 23:19:31 +0200

    Code Check

--- Geometry/HcalAlgo/plugins/dd4hep/DDHCalForwardAlgo.cc
+++ Geometry/HcalAlgo/plugins/dd4hep/DDHCalForwardAlgo.cc
@@ -13,15 +13,15 @@ static long algorithm(dd4hep::Detector& /* description */,
   cms::DDAlgoArguments args(ctxt, e);
   // Header section
   std::string parentName = DDSplit(args.parentName()).first;
-  std::string cellMat = args.value<std::string>("CellMaterial"); //Cell material
-  double cellDx = args.value<double>("CellDx");       //Cell size (x)
-  double cellDy = args.value<double>("CellDy");       //Cell size (y)
-  double cellDz = args.value<double>("CellDz");       //Cell size (z)
-  double startY = args.value<double>("StartY");       //Starting Y for Cell
-  std::vector<std::string> childName = args.value<std::vector<std::string> >("Child"); //Children name
-  std::vector<int> number = args.value<std::vector<int> >("Number"); //Number of cells
-  std::vector<int> size = args.value<std::vector<int> >("Size");     //Number of children
-  std::vector<int> type = args.value<std::vector<int> >("Type");     //First child
+  std::string cellMat = args.value<std::string>("CellMaterial");                        //Cell material
+  double cellDx = args.value<double>("CellDx");                                         //Cell size (x)
+  double cellDy = args.value<double>("CellDy");                                         //Cell size (y)
+  double cellDz = args.value<double>("CellDz");                                         //Cell size (z)
+  double startY = args.value<double>("StartY");                                         //Starting Y for Cell
+  std::vector<std::string> childName = args.value<std::vector<std::string> >("Child");  //Children name
+  std::vector<int> number = args.value<std::vector<int> >("Number");                    //Number of cells
+  std::vector<int> size = args.value<std::vector<int> >("Size");                        //Number of children
+  std::vector<int> type = args.value<std::vector<int> >("Type");                        //First child
 #ifdef EDM_ML_DEBUG
   edm::LogVerbatim("HCalGeom") << "DDHCalForwardAlgo: Cell material " << cellMat << "\tCell Size " << cellDx << ", "
                                << cellDy << ", " << cellDz << "\tStarting Y " << startY << "\tChildren " << childName[0]
@@ -29,7 +29,7 @@ static long algorithm(dd4hep::Detector& /* description */,
                                << "          Cell positioning done for " << number.size() << " times";
   for (unsigned int i = 0; i < number.size(); ++i)
     edm::LogVerbatim("HCalGeom") << "\t" << i << " Number of children " << size[i] << " occurence " << number[i]
-				 << " first child index " << type[i];
+                                 << " first child index " << type[i];
   edm::LogVerbatim("HCalGeom") << "DDHCalForwardAlgo debug: Parent " << args.parentName() << " NameSpace " << ns.name();
 #endif
 
@@ -47,8 +47,8 @@ static long algorithm(dd4hep::Detector& /* description */,
       dd4hep::Solid solid = dd4hep::Box(dx, cellDy, cellDz);
       ns.addSolidNS(ns.prepend(name), solid);
 #ifdef EDM_ML_DEBUG
-      edm::LogVerbatim("HCalGeom") << "DDHCalForwardAlgo: " << solid.name() << " Box made of " << cellMat
-                                   << " of Size " << dx << ", " << cellDy << ", " << cellDz;
+      edm::LogVerbatim("HCalGeom") << "DDHCalForwardAlgo: " << solid.name() << " Box made of " << cellMat << " of Size "
+                                   << dx << ", " << cellDy << ", " << cellDz;
 #endif
       dd4hep::Volume glog(solid.name(), solid, matter);
       ns.addVolumeNS(glog);
@@ -67,10 +67,10 @@ static long algorithm(dd4hep::Detector& /* description */,
 
       for (int k = 0; k < size[i]; ++k) {
         dd4hep::Position r1(xpos, 0.0, 0.0);
-	glog.placeVolume(ns.volume(childName[indx]), k + 1, dd4hep::Transform3D(rot, r1));
+        glog.placeVolume(ns.volume(childName[indx]), k + 1, dd4hep::Transform3D(rot, r1));
 #ifdef EDM_ML_DEBUG
-        edm::LogVerbatim("HCalGeom") << "DDHCalForwardAlgo: " << childName[indx] << " number " << k + 1 << " positioned in "
-                                     << glog.name() << " at " << r1 << " with " << rot;
+        edm::LogVerbatim("HCalGeom") << "DDHCalForwardAlgo: " << childName[indx] << " number " << k + 1
+                                     << " positioned in " << glog.name() << " at " << r1 << " with " << rot;
 #endif
         xpos += 2 * cellDx;
       }
