e3b1082d5481112d53bd5c37cf77e8f28046e444
Sunanda <sunanda.banerjee@cern.ch>
2019-08-21 21:26:53 +0200

    Code check

--- Geometry/HcalAlgo/plugins/dd4hep/DDHCalXtalAlgo.cc
+++ Geometry/HcalAlgo/plugins/dd4hep/DDHCalXtalAlgo.cc
@@ -14,15 +14,15 @@ static long algorithm(dd4hep::Detector& /* description */,
   cms::DDNamespace ns(ctxt, e, true);
   cms::DDAlgoArguments args(ctxt, e);
   // Header section
-  std::string idNameSpace = static_cast<std::string>(ns.name());                   //Namespace of this and ALL sub-parts
-  double radius = args.value<double>("Radius");                                    //Pointing distance from front surface
-  double offset = args.value<double>("Offset");                                    //Offset along Z
-  double dx = args.value<double>("Dx");                                            //Half size along x
-  double dz = args.value<double>("Dz");                                            //Half size along z
-  double angwidth = args.value<double>("AngWidth");                                //Angular width
-  int iaxis = args.value<int>("Axis");                                             //Axis of rotation
-  std::vector<std::string> names = args.value<std::vector<std::string> >("Names"); //Names for rotation matrices
-  std::string idName = args.value<std::string>("ChildName");                       //Children name
+  std::string idNameSpace = static_cast<std::string>(ns.name());  //Namespace of this and ALL sub-parts
+  double radius = args.value<double>("Radius");                   //Pointing distance from front surface
+  double offset = args.value<double>("Offset");                   //Offset along Z
+  double dx = args.value<double>("Dx");                           //Half size along x
+  double dz = args.value<double>("Dz");                           //Half size along z
+  double angwidth = args.value<double>("AngWidth");               //Angular width
+  int iaxis = args.value<int>("Axis");                            //Axis of rotation
+  std::vector<std::string> names = args.value<std::vector<std::string> >("Names");  //Names for rotation matrices
+  std::string idName = args.value<std::string>("ChildName");                        //Children name
 #ifdef EDM_ML_DEBUG
   edm::LogVerbatim("HCalGeom") << "DDHCalXtalAlgo::Parameters for positioning:"
                                << " Axis " << iaxis << "\tRadius " << radius << "\tOffset " << offset << "\tDx " << dx
@@ -30,8 +30,8 @@ static long algorithm(dd4hep::Detector& /* description */,
                                << names.size();
   for (unsigned int i = 0; i < names.size(); i++)
     edm::LogVerbatim("HCalGeom") << "\tnames[" << i << "] = " << names[i];
-  edm::LogVerbatim("HCalGeom") << "DDHCalXtalAlgo: Parent " << args.parentName() << "\tChild " << idName << " NameSpace "
-                               << idNameSpace;
+  edm::LogVerbatim("HCalGeom") << "DDHCalXtalAlgo: Parent " << args.parentName() << "\tChild " << idName
+                               << " NameSpace " << idNameSpace;
 #endif
 
   dd4hep::Volume parent = ns.volume(args.parentName());
@@ -64,17 +64,17 @@ static long algorithm(dd4hep::Detector& /* description */,
     static const double tol = 0.01_deg;  // 0.01 degree
     if (std::abs(angle) > tol) {
 #ifdef EDM_ML_DEBUG
-      edm::LogVerbatim("HCalGeom") << "DDHCalXtalAlgo: Creating a rotation \t"
-                                   << convertRadToDeg(theta[0]) << "," << convertRadToDeg(phi[0]) << ","
-                                   << convertRadToDeg(theta[1]) << "," << convertRadToDeg(phi[1]) << ","
-                                   << convertRadToDeg(theta[2]) << "," << convertRadToDeg(phi[2]);
+      edm::LogVerbatim("HCalGeom") << "DDHCalXtalAlgo: Creating a rotation \t" << convertRadToDeg(theta[0]) << ","
+                                   << convertRadToDeg(phi[0]) << "," << convertRadToDeg(theta[1]) << ","
+                                   << convertRadToDeg(phi[1]) << "," << convertRadToDeg(theta[2]) << ","
+                                   << convertRadToDeg(phi[2]);
 #endif
       rotation = cms::makeRotation3D(theta[0], phi[0], theta[1], phi[1], theta[2], phi[2]);
     }
     parent.placeVolume(glog, i + 1, dd4hep::Transform3D(rotation, tran));
 #ifdef EDM_ML_DEBUG
-    edm::LogVerbatim("HCalGeom") << "DDHCalXtalAlgo: " << glog.name() << " number " << i + 1
-                                 << " positioned in " << parent.name() << " at " << tran << " with " << rotation;
+    edm::LogVerbatim("HCalGeom") << "DDHCalXtalAlgo: " << glog.name() << " number " << i + 1 << " positioned in "
+                                 << parent.name() << " at " << tran << " with " << rotation;
 #endif
   }
 
