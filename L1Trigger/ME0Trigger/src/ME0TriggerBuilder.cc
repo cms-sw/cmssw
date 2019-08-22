22207b0e7f9cde556c5db9192b9bc18748fb13c6
Sven Dildick <sven.dildick@cern.ch>
2019-08-22 19:15:49 +0200

    Use enums and use const ref

--- L1Trigger/ME0Trigger/src/ME0TriggerBuilder.cc
+++ L1Trigger/ME0Trigger/src/ME0TriggerBuilder.cc
@@ -3,14 +3,8 @@
 ME0TriggerBuilder::ME0TriggerBuilder(const edm::ParameterSet& conf) {
   config_ = conf;
 
-  for (int endc = 0; endc < 2; endc++) {
-    for (int cham = 0; cham < 18; cham++) {
-      if ((endc <= 0 || endc > MAX_ENDCAPS) || (cham <= 0 || cham > MAX_CHAMBERS)) {
-        edm::LogError("L1ME0TPEmulatorSetupError")
-            << "+++ trying to instantiate TMB of illegal ME0 id ["
-            << " endcap = " << endc << " chamber = " << cham << "]; skipping it... +++\n";
-        continue;
-      }
+  for (int endc = 0; endc < MAX_ENDCAPS; endc++) {
+    for (int cham = 0; cham < MAX_CHAMBERS; cham++) {
       tmb_[endc][cham].reset(new ME0Motherboard(endc, cham, config_));
     }
   }
