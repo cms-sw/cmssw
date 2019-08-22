c22c6a2f2be99c5e8ff67ab04c9f7ac225bb2498
arabella <arabella.martelli@cern.ch>
2019-08-22 17:59:59 +0200

    revert const back

--- RecoHGCal/TICL/plugins/PatternRecognitionbyMultiClusters.cc
+++ RecoHGCal/TICL/plugins/PatternRecognitionbyMultiClusters.cc
@@ -1,7 +1,7 @@
 #include "PatternRecognitionbyMultiClusters.h"
 #include "FWCore/MessageLogger/interface/MessageLogger.h"
 
-const void ticl::PatternRecognitionbyMultiClusters::makeTracksters(const PatternRecognitionAlgoBase::Inputs& input,
-                                                                   std::vector<Trackster>& result) {
+void ticl::PatternRecognitionbyMultiClusters::makeTracksters(const PatternRecognitionAlgoBase::Inputs& input,
+                                                             std::vector<Trackster>& result) {
   LogDebug("HGCPatterRecoTrackster") << "making Tracksters" << std::endl;
 }
