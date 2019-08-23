6c985ca4cd52c22b0f316902d432cb829206dec8
Clemens Lange <clemens.lange@cern.ch>
2019-08-23 11:53:55 +0200

    Re-add hgcalValidatorSequence

--- Validation/Configuration/python/hgcalSimValid_cff.py
+++ Validation/Configuration/python/hgcalSimValid_cff.py
@@ -5,8 +5,10 @@ from Validation.HGCalValidation.digiValidation_cff      import *
 from Validation.HGCalValidation.rechitValidation_cff    import *
 from Validation.HGCalValidation.hgcalHitValidation_cfi  import *
 
+from Validation.HGCalValidation.HGCalValidator_cfi import hgcalValidator
 from Validation.RecoParticleFlow.PFJetValidation_cff import pfJetValidation1 as _hgcalPFJetValidation
 
+hgcalValidatorSequence = cms.Sequence(hgcalValidator)
 hgcalPFJetValidation = _hgcalPFJetValidation.clone(BenchmarkLabel = 'PFJetValidation/HGCAlCompWithGenJet',
     VariablePtBins=[10., 30., 80., 120., 250., 600.],
     DeltaPtOvPtHistoParameter = dict(EROn=True,EREtaMax=3.0, EREtaMin=1.6, slicingOn=True))
