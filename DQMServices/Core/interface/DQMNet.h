f10d1b5b53457de27e6b8123533bf755c6c2f451
Mircho Rodozov <mrodozov@cern.ch>
2019-08-23 13:19:01 +0200

    Add fallthrough instead of breaks

--- DQMServices/Core/interface/DQMNet.h
+++ DQMServices/Core/interface/DQMNet.h
@@ -266,40 +266,40 @@ public:
     switch (keylen) {
       case 12:
         c += ((uint32_t)k[11]) << 24;
-        break;
+        [[fallthrough]];
       case 11:
         c += ((uint32_t)k[10]) << 16;
-        break;
+        [[fallthrough]];
       case 10:
         c += ((uint32_t)k[9]) << 8;
-        break;
+        [[fallthrough]];
       case 9:
         c += k[8];
-        break;
+        [[fallthrough]];
       case 8:
         b += ((uint32_t)k[7]) << 24;
-        break;
+        [[fallthrough]];
       case 7:
         b += ((uint32_t)k[6]) << 16;
-        break;
+        [[fallthrough]];
       case 6:
         b += ((uint32_t)k[5]) << 8;
-        break;
+        [[fallthrough]];
       case 5:
         b += k[4];
-        break;
+        [[fallthrough]];
       case 4:
         a += ((uint32_t)k[3]) << 24;
-        break;
+        [[fallthrough]];
       case 3:
         a += ((uint32_t)k[2]) << 16;
-        break;
+        [[fallthrough]];
       case 2:
         a += ((uint32_t)k[1]) << 8;
-        break;
+        [[fallthrough]];
       case 1:
         a += k[0];
-        break;
+        [[fallthrough]];
       case 0:
         return c;
     }
