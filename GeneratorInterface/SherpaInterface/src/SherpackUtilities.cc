08421a22d2da0ccbcea21e78ef96afd7de500b46
Mircho Rodozov <mrodozov@cern.ch>
2019-08-24 13:06:14 +0200

    Remove fallthrough/no break comments as agreed

--- GeneratorInterface/SherpaInterface/src/SherpackUtilities.cc
+++ GeneratorInterface/SherpaInterface/src/SherpackUtilities.cc
@@ -103,7 +103,7 @@ namespace spu {
         assert(ret != Z_STREAM_ERROR); /* state not clobbered */
         switch (ret) {
           case Z_NEED_DICT:
-            ret = Z_DATA_ERROR; /* and fall through */
+            ret = Z_DATA_ERROR;
             [[fallthrough]];
           case Z_DATA_ERROR:
           case Z_MEM_ERROR:
