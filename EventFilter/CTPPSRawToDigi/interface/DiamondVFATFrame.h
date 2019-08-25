ff34c5eea8ee764d1c627903ab3ed180a988ca01
Laurent Forthomme <laurent.forthomme@cern.ch>
2019-08-25 14:24:17 +0200

    Diamond VFAT frame not inheriting from base VFAT frame anymore

--- EventFilter/CTPPSRawToDigi/interface/DiamondVFATFrame.h
+++ EventFilter/CTPPSRawToDigi/interface/DiamondVFATFrame.h
@@ -1,8 +1,9 @@
 /****************************************************************************
 *
-* This is a part of the TOTEM offline software.
+* This is a part of the PPS offline software.
 * Authors:
 *   Seyed Mohsen Etesami (setesami@cern.ch)
+*   Laurent Forthomme
 *   Nicola Minafra
 *
 ****************************************************************************/
@@ -16,34 +17,39 @@
 
 #include "EventFilter/CTPPSRawToDigi/interface/VFATFrame.h"
 
-/** 
+/**
  * This class intended to handle the timing infromation of diamond VFAT frame
 **/
-class DiamondVFATFrame : public VFATFrame {
+class DiamondVFATFrame {
 public:
-  DiamondVFATFrame(const word* inputData = nullptr) {}
-  ~DiamondVFATFrame() override {}
+  DiamondVFATFrame(const VFATFrame::word* inputData = nullptr) {
+    std::copy_n(inputData, 9, data_.begin());
+  }
+  ~DiamondVFATFrame() = default;
 
-  /// get timing infromation
+  /// get timing information for leading single edge
   uint32_t getLeadingEdgeTime() const {
-    uint32_t time = ((data[7] & 0x1f) << 16) + data[8];
-    time = (time & 0xFFE7FFFF) << 2 |
-           (time & 0x00180000) >> 19;  //HPTDC inperpolation bits are MSB but should be LSB.... ask HPTDC designers...
-    return time;
+    uint32_t time = ((data_[7] & 0x1f) << 16) + data_[8];
+    return correctTime(time);
   }
-
+  /// get timing information for trailing single edge
   uint32_t getTrailingEdgeTime() const {
-    uint32_t time = ((data[5] & 0x1f) << 16) + data[6];
-    time = (time & 0xFFE7FFFF) << 2 |
-           (time & 0x00180000) >> 19;  //HPTDC inperpolation bits are MSB but should be LSB.... ask HPTDC designers...
-    return time;
+    uint32_t time = ((data_[5] & 0x1f) << 16) + data_[6];
+    return correctTime(time);
   }
+  /// retrieve the threshold voltage for this channel
+  uint32_t getThresholdVoltage() const { return ((data_[3] & 0x7ff) << 16) + data_[4]; }
+  /// flag stating whether the HPTDC channel encountered multiple hits
+  VFATFrame::word getMultihit() const { return data_[2] & 0x01; }
+  /// retrieve the list of error/status flags for the HPTDC when the frame was recorded
+  VFATFrame::word getHptdcErrorFlag() const { return data_[1] & 0xFFFF; }
 
-  uint32_t getThresholdVoltage() const { return ((data[3] & 0x7ff) << 16) + data[4]; }
-
-  VFATFrame::word getMultihit() const { return data[2] & 0x01; }
-
-  VFATFrame::word getHptdcErrorFlag() const { return data[1] & 0xFFFF; }
+private:
+  /// Account for MSB/LSB "HW feature" reversal in HPTDC interpolation bits
+  uint32_t correctTime(uint32_t& time) const {
+    return (time & 0xFFE7FFFF) << 2 | (time & 0x00180000) >> 19;
+  }
+  std::array<VFATFrame::word,9> data_;
 };
 
 #endif
