7329c23c5ec7d00464d3011a041cc52d44b87aa0
Alberto Zucchetta <a.zucchetta@cern.ch>
2019-08-26 11:28:15 +0200

    Moved modifiers from RecoBTag_cff and partially from miniAOD_tools.py to pixelClusterTagInfos_cfi

--- RecoBTag/PixelCluster/python/pixelClusterTagInfos_cfi.py
+++ RecoBTag/PixelCluster/python/pixelClusterTagInfos_cfi.py
@@ -12,5 +12,17 @@ pixelClusterTagInfos = cms.EDProducer("PixelClusterTagInfoProducer",
     hadronMass = cms.double(12.), # indicative mass to derive the shrinking cone radius
 )
 
+# Use 3 layers instead of 4 in Phase-0 pixel
 from Configuration.Eras.Modifier_pixel_2016_cff import pixel_2016
 pixel_2016.toModify(pixelClusterTagInfos, isPhase1 = False )
+
+## FastSim modifier (do not run pixelClusterTagInfos because the siPixelCluster input collection is not produced in FastSim)
+from Configuration.Eras.Modifier_fastSim_cff import fastSim
+fastSim.toModify(pixelClusterTagInfos, minJetPtCut = 1.e+9 )
+
+from Configuration.Eras.Modifier_run2_miniAOD_80XLegacy_cff import run2_miniAOD_80XLegacy
+run2_miniAOD_80XLegacy.toModify(pixelClusterTagInfos, minJetPtCut = 1.e+9 )
+    
+from Configuration.Eras.Modifier_run2_miniAOD_94XFall17_cff import run2_miniAOD_94XFall17
+run2_miniAOD_94XFall17.toModify(pixelClusterTagInfos, minJetPtCut = 1.e+9 )
+
