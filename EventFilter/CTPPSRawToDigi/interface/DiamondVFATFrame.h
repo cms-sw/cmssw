13f9dd8978b3e8e9f0c81672bd7eb7999935751c
Laurent Forthomme <laurent.forthomme@cern.ch>
2019-08-26 17:35:53 +0200

    Removed class, only using free (namespaced) functions

--- EventFilter/CTPPSRawToDigi/interface/DiamondVFATFrame.h
+++ EventFilter/CTPPSRawToDigi/interface/DiamondVFATFrame.h
@@ -13,38 +13,38 @@
 
 #include "EventFilter/CTPPSRawToDigi/interface/VFATFrame.h"
 
-#include <array>
 #include <cstdint>
 
 /**
- * This class intended to handle the timing infromation of diamond VFAT frame
+ * Utilitary namespace to retrieve timing/status information from diamond VFAT frame
 **/
-class DiamondVFATFrame {
-public:
-  DiamondVFATFrame(const VFATFrame::word* inputData = nullptr) { std::copy_n(inputData, 9, data_.begin()); }
-  ~DiamondVFATFrame() = default;
+namespace ppsdiamondvfat
+{
+  /// Account for MSB/LSB "HW feature" reversal in HPTDC interpolation bits
+  uint32_t correctTime(const uint32_t& time) { return (time & 0xFFE7FFFF) << 2 | (time & 0x00180000) >> 19; }
 
-  /// get timing information for leading single edge
-  uint32_t getLeadingEdgeTime() const {
-    uint32_t time = ((data_[7] & 0x1f) << 16) + data_[8];
+  /// get timing information for single leading edge
+  uint32_t getLeadingEdgeTime(const VFATFrame& frame) {
+    uint32_t time = ((frame.getData()[7] & 0x1f) << 16) + frame.getData()[8];
     return correctTime(time);
   }
-  /// get timing information for trailing single edge
-  uint32_t getTrailingEdgeTime() const {
-    uint32_t time = ((data_[5] & 0x1f) << 16) + data_[6];
+  /// get timing information for single trailing edge
+  uint32_t getTrailingEdgeTime(const VFATFrame& frame) {
+    uint32_t time = ((frame.getData()[5] & 0x1f) << 16) + frame.getData()[6];
     return correctTime(time);
   }
   /// retrieve the threshold voltage for this channel
-  uint32_t getThresholdVoltage() const { return ((data_[3] & 0x7ff) << 16) + data_[4]; }
+  uint32_t getThresholdVoltage(const VFATFrame& frame) {
+    return ((frame.getData()[3] & 0x7ff) << 16) + frame.getData()[4];
+  }
   /// flag stating whether the HPTDC channel encountered multiple hits
-  VFATFrame::word getMultihit() const { return data_[2] & 0x01; }
+  VFATFrame::word getMultihit(const VFATFrame& frame) {
+    return frame.getData()[2] & 0x01;
+  }
   /// retrieve the list of error/status flags for the HPTDC when the frame was recorded
-  VFATFrame::word getHptdcErrorFlag() const { return data_[1] & 0xFFFF; }
-
-private:
-  /// Account for MSB/LSB "HW feature" reversal in HPTDC interpolation bits
-  uint32_t correctTime(uint32_t& time) const { return (time & 0xFFE7FFFF) << 2 | (time & 0x00180000) >> 19; }
-  std::array<VFATFrame::word, 9> data_;
-};
+  VFATFrame::word getHptdcErrorFlag(const VFATFrame& frame) {
+    return frame.getData()[1] & 0xFFFF;
+  }
+}
 
 #endif
