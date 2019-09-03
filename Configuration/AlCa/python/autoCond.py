5be46310f6aeac9304cd73bbf4e9a3274866bb0e
Chris West <christopher.alan.west@gmail.com>
2019-09-03 23:24:08 +0200

    Updates to beamspot, tracker alignment, muon alignment, ECAL calibrations and a fix for pixel conditions for a single fill.

--- Configuration/AlCa/python/autoCond.py
+++ Configuration/AlCa/python/autoCond.py
@@ -24,11 +24,11 @@ autoCond = {
     # GlobalTag for MC production (p-Pb collisions) with realistic alignment and calibrations for Run2
     'run2_mc_pa'        :   '110X_mcRun2_pA_v2',
     # GlobalTag for Run1 data reprocessing
-    'run1_data'         :   '110X_dataRun2_v5',
+    'run1_data'         :   '110X_dataRun2_v6',
     # GlobalTag for Run2 data reprocessing
-    'run2_data'         :   '110X_dataRun2_v5',
+    'run2_data'         :   '110X_dataRun2_v6',
     # GlobalTag for Run2 data relvals: allows customization to run with fixed L1 menu
-    'run2_data_relval'  :   '110X_dataRun2_relval_v5',
+    'run2_data_relval'  :   '110X_dataRun2_relval_v6',
     # GlobalTag for Run2 data 2018B relvals only: HEM-15-16 fail
     'run2_data_promptlike_HEfail' : '110X_dataRun2_PromptLike_HEfail_v3',
     # GlobalTag for Run2 data 2016H relvals only: Prompt Conditions + fixed L1 menu (to be removed)
