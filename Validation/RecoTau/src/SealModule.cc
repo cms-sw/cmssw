4ac647bdf0032b34c1a9e7791274fe196e108db9
aspiezia <aniello.spiezia@cern.ch>
2019-09-04 10:20:02 +0200

    TAU miniaod validation/dqm

--- Validation/RecoTau/src/SealModule.cc
+++ Validation/RecoTau/src/SealModule.cc
@@ -1,5 +1,9 @@
 #include "FWCore/PluginManager/interface/ModuleDef.h"
 #include "FWCore/Framework/interface/MakerMacros.h"
 #include "Validation/RecoTau/interface/TauTagValidation.h"
+#include "Validation/RecoTau/interface/TauValidationMiniAOD.h"
 
-DEFINE_FWK_MODULE(TauTagValidation);
+
+DEFINE_FWK_MODULE( TauTagValidation );
+DEFINE_FWK_MODULE( TauValidationMiniAOD );
+
