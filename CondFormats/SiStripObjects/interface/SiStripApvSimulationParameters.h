5d7d8267d82a2408c297083c7a9eabf6cef9f4ea
Pieter David <pieter.david@cern.ch>
2019-09-10 17:21:11 +0200

    SiStripApvSimulationParameters: add members for TID and TEC partitions

--- CondFormats/SiStripObjects/interface/SiStripApvSimulationParameters.h
+++ CondFormats/SiStripObjects/interface/SiStripApvSimulationParameters.h
@@ -19,9 +19,12 @@ public:
   using layerid = unsigned int;
   using LayerParameters = PhysicsTools::Calibration::HistogramF3D;
 
-  SiStripApvSimulationParameters(layerid nTIB, layerid nTOB) : m_nTIB(nTIB), m_nTOB(nTOB) {
+  SiStripApvSimulationParameters(layerid nTIB, layerid nTOB, layerid nTID, layerid nTEC)
+      : m_nTIB(nTIB), m_nTOB(nTOB), m_nTID(nTID), m_nTEC(nTEC) {
     m_barrelParam.resize(m_nTIB + m_nTOB);
     m_barrelParam_xInt.resize(m_nTIB + m_nTOB);
+    m_endcapParam.resize(m_nTID + m_nTEC);
+    m_endcapParam_xInt.resize(m_nTID + m_nTEC);
   }
   SiStripApvSimulationParameters() {}
   ~SiStripApvSimulationParameters() {}
@@ -34,8 +37,16 @@ public:
   bool putTOB(layerid layer, const LayerParameters& params) { return putTOB(layer, LayerParameters(params)); }
   bool putTOB(layerid layer, LayerParameters&& params);
 
+  bool putTID(layerid ring, const LayerParameters& params) { return putTID(ring, LayerParameters(params)); }
+  bool putTID(layerid ring, LayerParameters&& params);
+
+  bool putTEC(layerid ring, const LayerParameters& params) { return putTEC(ring, LayerParameters(params)); }
+  bool putTEC(layerid ring, LayerParameters&& params);
+
   const LayerParameters& getTIB(layerid layer) const { return m_barrelParam[layer - 1]; }
   const LayerParameters& getTOB(layerid layer) const { return m_barrelParam[m_nTIB + layer - 1]; }
+  const LayerParameters& getTID(layerid ring) const { return m_endcapParam[ring - 1]; }
+  const LayerParameters& getTEC(layerid ring) const { return m_endcapParam[m_nTID + ring - 1]; }
 
   float sampleTIB(layerid layer, float z, float pu, CLHEP::HepRandomEngine* engine) const {
     return sampleBarrel(layer - 1, z, pu, engine);
@@ -43,13 +54,22 @@ public:
   float sampleTOB(layerid layer, float z, float pu, CLHEP::HepRandomEngine* engine) const {
     return sampleBarrel(m_nTIB + layer - 1, z, pu, engine);
   };
+  float sampleTID(layerid ring, float z, float pu, CLHEP::HepRandomEngine* engine) const {
+    return sampleEndcap(ring - 1, z, pu, engine);
+  }
+  float sampleTEC(layerid ring, float z, float pu, CLHEP::HepRandomEngine* engine) const {
+    return sampleEndcap(m_nTID + ring - 1, z, pu, engine);
+  }
 
 private:
-  layerid m_nTIB, m_nTOB;
+  layerid m_nTIB, m_nTOB, m_nTID, m_nTEC;
   std::vector<PhysicsTools::Calibration::HistogramF3D> m_barrelParam;
   std::vector<PhysicsTools::Calibration::HistogramF2D> m_barrelParam_xInt;
+  std::vector<PhysicsTools::Calibration::HistogramF3D> m_endcapParam;
+  std::vector<PhysicsTools::Calibration::HistogramF2D> m_endcapParam_xInt;
 
   float sampleBarrel(layerid layerIdx, float z, float pu, CLHEP::HepRandomEngine* engine) const;
+  float sampleEndcap(layerid ringIdx, float z, float pu, CLHEP::HepRandomEngine* engine) const;
 
   COND_SERIALIZABLE;
 };
