c23a571b2e51e4100eb51ca2194153c8b6b744b5
Andrea Bocci <andrea.bocci@cern.ch>
2019-09-12 00:22:05 +0200

    Apply clang-format style formatting

--- CUDADataFormats/Vertex/interface/ZVertexHeterogeneous.h
+++ CUDADataFormats/Vertex/interface/ZVertexHeterogeneous.h
@@ -5,11 +5,10 @@
 #include "CUDADataFormats/Common/interface/HeterogeneousSoA.h"
 #include "CUDADataFormats/Track/interface/PixelTrackHeterogeneous.h"
 
-
 using ZVertexHeterogeneous = HeterogeneousSoA<ZVertexSoA>;
 #ifndef __CUDACC__
 #include "CUDADataFormats/Common/interface/CUDAProduct.h"
-using ZVertexCUDAProduct =  CUDAProduct<ZVertexHeterogeneous>;
+using ZVertexCUDAProduct = CUDAProduct<ZVertexHeterogeneous>;
 #endif
 
 #endif
