6081ba3807bc299e735a3d1be216916c59e66a37
Michal <michal.bluj@cern.ch>
2019-09-11 10:40:37 +0200

    New era modifiers for tauID Run2 UL processing chained for 2016 and 2018 eras

--- Configuration/Eras/python/Era_Run2_2018_cff.py
+++ Configuration/Eras/python/Era_Run2_2018_cff.py
@@ -14,10 +14,11 @@ from Configuration.Eras.Modifier_run2_muon_2018_cff import run2_muon_2018
 from Configuration.Eras.Modifier_run2_muon_2017_cff import run2_muon_2017
 from Configuration.Eras.Modifier_run2_egamma_2018_cff import run2_egamma_2018
 from Configuration.Eras.Modifier_run2_egamma_2017_cff import run2_egamma_2017
+from Configuration.Eras.Modifier_run2_tau_ul_2018_cff import run2_tau_ul_2018
 from Configuration.Eras.Modifier_run2_L1prefiring_cff import run2_L1prefiring
 from Configuration.Eras.Modifier_ctpps_2017_cff import ctpps_2017
 from Configuration.Eras.Modifier_ctpps_2018_cff import ctpps_2018
 
 Run2_2018 = cms.ModifierChain(Run2_2017.copyAndExclude([run2_HEPlan1_2017, run2_muon_2017, run2_L1prefiring, run2_HLTconditions_2017, run2_egamma_2017, ctpps_2017]),
 run2_CSC_2018, run2_HCAL_2018, run2_HB_2018, run2_HE_2018,run2_DT_2018, run2_SiPixel_2018, 
-run2_HLTconditions_2018, run2_muon_2018, run2_egamma_2018, ctpps_2018)
+run2_HLTconditions_2018, run2_muon_2018, run2_egamma_2018, run2_tau_ul_2018, ctpps_2018)
