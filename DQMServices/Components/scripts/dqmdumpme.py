32dd6cf7c87220359849a67788703e9507c6736f
Marcel Andre Schneider <marcel.andre.schneider@cern.ch>
2019-09-16 15:51:22 +0200

    Extend dqmdumpme for DQMIO data, though uproot can't read most ME types.

--- DQMServices/Components/scripts/dqmdumpme.py
+++ DQMServices/Components/scripts/dqmdumpme.py
@@ -18,8 +18,51 @@ args = parser.parse_args()
 f = uproot.open(args.filename)
 things = f.keys()
 if 'Indices;1' in things:
-  # this is DQMIO data, TODO
-  pass
+  # this is DQMIO data
+
+  treenames = [ # order matters!
+    "Ints",
+    "Floats",
+    "Strings",
+    "TH1Fs",
+    "TH1Ss",
+    "TH1Ds",
+    "TH2Fs",
+    "TH2Ss",
+    "TH2Ds",
+    "TH3Fs",
+    "TProfiles",
+    "TProfile2Ds"
+  ]
+  trees = [{"FullName": f[name]["FullName"].array(), "Value": f[name]["Value"].lazyarray()} for name in treenames]
+
+  def binsearch(a, key, lower, upper):
+    n = upper - lower
+    if n <= 1: return lower
+    mid = int(n / 2) + lower
+    if a[mid] < key: return binsearch(a, key, mid, upper)
+    else: return binsearch(a, key, lower, mid)
+  def linsearch(a, key, lower, upper):
+    for k in range(lower, upper):
+      if a[k] == key: return k
+    return 0
+
+  indices = f['Indices'].lazyarrays()
+  for idx in range(len(indices["Run"])):
+    run = indices["Run"][idx]
+    lumi = indices["Lumi"][idx]
+    type = indices["Type"][idx]
+    first = int(indices["FirstIndex"][idx])
+    last = int(indices["LastIndex"][idx])
+    if type == 1000: continue # no MEs here
+    names = trees[type]["FullName"]
+    for me in args.mepaths:
+      k = linsearch(names, me, first, last+1)
+      if names[k] == me:
+        meobj = trees[type]["Value"][k]
+        print("ME for run %d, lumi %d" % (run, lumi), meobj)
+        # uproot can't read most TH1 types from trees right now.
+
 elif 'DQMData;1' in things:
   basedir = f['DQMData']
   for run in basedir.keys():
