a8685c8055253657afa18e8045f5406fbb809dae
Marcel Andre Schneider <marcel.andre.schneider@cern.ch>
2019-09-16 12:01:35 +0200

    Fix re-harvesting script based on actual prod logs.

--- DQMServices/Components/test/test_reharvest_from_dqmio.sh
+++ DQMServices/Components/test/test_reharvest_from_dqmio.sh
@@ -1,5 +1,5 @@
 # DQMIO dataset to take data from and run to look at
-RUN=302570
+RUN=302663
 DATASET=/SingleElectron/Run2017D-09Aug2019_UL2017-v1/DQMIO
 # Workflow to take the HARVESTING step from
 WORKFLOW=136.834
@@ -12,7 +12,7 @@ pythonname=$(echo step*_HARVESTING.py)
 ./dqmiofilecopy.sh $DATASET $RUN >> $pythonname
 
 # no idea where this is injected in the prod setup, but it must be somewhere.
-echo "process.source.filterOnRun = cms.untracked.uint32($RUN)" >> $pythonname
+echo "process.source.lumisToProcess = cms.untracked.VLuminosityBlockRange('$RUN:1-$(($RUN+1)):0')" >> $pythonname
 
 echo '### Got all data, starting cmsRun!'
 
