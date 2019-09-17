605138f2bca6d06d0835e50d94d9a5484884b317
Javier <Javier.Fernandez@cern.ch>
2019-09-17 16:47:18 +0200

    Code format

--- DQM/DTMonitorClient/src/DTLocalTriggerSynchTest.cc
+++ DQM/DTMonitorClient/src/DTLocalTriggerSynchTest.cc
@@ -150,7 +150,7 @@ void DTLocalTriggerSynchTest::dqmEndJob(DQMStore::IBooker& ibooker, DQMStore::IG
                             << endl;
 
     DTTPGParameters* delayMap = new DTTPGParameters();
-    hwSource = "TM" ;
+    hwSource = "TM";
     std::vector<const DTChamber*>::const_iterator chambIt = muonGeom->chambers().begin();
     std::vector<const DTChamber*>::const_iterator chambEnd = muonGeom->chambers().end();
     for (; chambIt != chambEnd; ++chambIt) {
