157161a6c294f9f062eaeac1c2978c3502b8954c
mmusich <marco.musich@cern.ch>
2019-09-18 13:48:42 +0200

    use frontier connection for SiStripApvSimulationParameters DB object

--- SimTracker/SiStripDigitizer/python/customizeSiStripAPVDynamicGainSimulation.py
+++ SimTracker/SiStripDigitizer/python/customizeSiStripAPVDynamicGainSimulation.py
@@ -13,9 +13,8 @@ def activateSiStripAPVDynamicGain(process):
         process.GlobalTag.toGet=cms.VPSet()
 
     process.GlobalTag.toGet.extend(cms.VPSet(cms.PSet(record = cms.string('SiStripApvSimulationParametersRcd'),
-                                                      tag = cms.string('SiStripApvSimulationParameters_2016preVFP_v1'),
-                                                      #connect = cms.string('frontier://FrontierPrep/CMS_CONDITIONS'))
-                                                      connect = cms.string('sqlite_file:/tmp/musich/CMSSW_11_0_X_2019-09-16-2300/src/CondTools/SiStrip/test/dbfile.db'))
+                                                      tag = cms.string('SiStripApvSimulationParameters_2016preVFP_v0'),
+                                                      connect = cms.string('frontier://FrontierPrep/CMS_CONDITIONS'))
                                          )
                                    )            
     return process
