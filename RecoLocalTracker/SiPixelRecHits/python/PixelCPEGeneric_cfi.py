b98b0cac2ccd9f4bece923c47875d67d54b3a935
Oz Amram <oamram1@jhu.edu>
2019-09-18 10:09:27 -0500

    Copied marcos commits

--- RecoLocalTracker/SiPixelRecHits/python/PixelCPEGeneric_cfi.py
+++ RecoLocalTracker/SiPixelRecHits/python/PixelCPEGeneric_cfi.py
@@ -8,14 +8,3 @@ PixelCPEGenericESProducer = _generic_default.clone()
 from Configuration.Eras.Modifier_run3_common_cff import run3_common
 run3_common.toModify(PixelCPEGenericESProducer, IrradiationBiasCorrection = True)
 
-# This customization will be removed once we get the templates for phase2 pixel
-# FIXME::Is the Upgrade variable actually used?
-from Configuration.Eras.Modifier_phase2_tracker_cff import phase2_tracker
-phase2_tracker.toModify(PixelCPEGenericESProducer, 
-  UseErrorsFromTemplates = False,
-  LoadTemplatesFromDB = False,
-  TruncatePixelCharge = False,
-  IrradiationBiasCorrection = False,
-  DoCosmics = False,
-  Upgrade = cms.bool(True)
-)
