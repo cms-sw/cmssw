1cd25c7da76eb0211f1a2f0663bd97147c377a71
Sunanda <sunanda.banerjee@cern.ch>
2019-09-25 14:06:00 +0200

    Add a throw for a wrong constructor

--- Geometry/HcalAlgo/plugins/dd4hep/DDHCalEndcapAlgo.cc
+++ Geometry/HcalAlgo/plugins/dd4hep/DDHCalEndcapAlgo.cc
@@ -88,7 +88,10 @@ struct HCalEndcapAlgo {
   int idOffset;                        // Geant4 ID's...    = 4000;
   double tolPos, tolAbs;               //Tolerances
 
-  HCalEndcapAlgo() {}
+  HCalEndcapAlgo() {
+    throw cms::Exception("HCalGeom") << "Wrong initialization to HcalEndcapAlgo";
+  }
+
   HCalEndcapAlgo(cms::DDParsingContext& ctxt, xml_h e) {
     cms::DDNamespace ns(ctxt, e, true);
     cms::DDAlgoArguments args(ctxt, e);
