e1df85eb4fdb9fd3febe6150eb01a6481dc1aab9
Sunanda <sunanda.banerjee@cern.ch>
2019-09-26 21:33:29 +0200

    Add possibility of mapping DetId/TriggerId to ModuleId and viceversa

---
+++ DataFormats/ForwardDetId/src/HGCSiliconDetIdToModule.cc
@@ -0,0 +1,17 @@
+#include "DataFormats/ForwardDetId/interface/HGCSiliconDetIdToModule.h"
+
+HGCSiliconDetIdToModule::HGCSiliconDetIdToModule() {}
+
+std::vector<HGCSiliconDetId> HGCSiliconDetIdToModule::getDetIds(HGCSiliconDetId const& id) const {
+  std::vector<HGCSiliconDetId> ids;
+  int nCells = (id.type() == 0) ? HGCSiliconDetId::HGCalFineN : HGCSiliconDetId::HGCalCoarseN;
+  for (int u = 0; u < 2 * nCells; ++u) {
+    for (int v = 0; v < 2 * nCells; ++v) {
+      if (((v - u) < nCells) && (u - v) <= nCells) {
+	HGCSiliconDetId newId(id.det(), id.zside(), id.type(), id.layer(), id.waferU(), id.waferV(), u, v);
+	ids.emplace_back(newId);
+      }
+    }
+  }
+  return ids;
+}
