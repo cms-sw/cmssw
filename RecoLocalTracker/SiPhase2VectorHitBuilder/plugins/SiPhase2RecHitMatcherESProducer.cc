09136ad90ad80dc611c09cf5a3e9c6897c246dc8
Jan-Frederik <jschulte@cern.ch>
2019-10-02 01:45:07 +0200

    address Iannas comments part 1

--- RecoLocalTracker/SiPhase2VectorHitBuilder/plugins/SiPhase2RecHitMatcherESProducer.cc
+++ RecoLocalTracker/SiPhase2VectorHitBuilder/plugins/SiPhase2RecHitMatcherESProducer.cc
@@ -1,4 +1,3 @@
-#include "RecoLocalTracker/SiPhase2VectorHitBuilder/plugins/SiPhase2RecHitMatcherESProducer.h"
 
 #include "FWCore/Framework/interface/EventSetup.h"
 #include "FWCore/Framework/interface/ESHandle.h"
@@ -6,15 +5,32 @@
 #include "FWCore/Framework/interface/ESProducer.h"
 #include "RecoLocalTracker/Records/interface/TkPhase2OTCPERecord.h"
 
+#include "FWCore/Framework/interface/ESProducer.h"
+#include "FWCore/ParameterSet/interface/ParameterSet.h"
+#include "RecoLocalTracker/SiPhase2VectorHitBuilder/interface/VectorHitBuilderEDProducer.h"
+#include <memory>
+
+class SiPhase2RecHitMatcherESProducer : public edm::ESProducer {
+public:
+  SiPhase2RecHitMatcherESProducer(const edm::ParameterSet&);
+  static void fillDescriptions(edm::ConfigurationDescriptions& descriptions);
+  std::shared_ptr<VectorHitBuilderEDProducer> produce(const TkPhase2OTCPERecord&);
+
+private:
+  std::string name_;
+  std::shared_ptr<VectorHitBuilderEDProducer> matcher_;
+  edm::ParameterSet pset_;
+};
+
 SiPhase2RecHitMatcherESProducer::SiPhase2RecHitMatcherESProducer(const edm::ParameterSet& p) {
-  name = p.getParameter<std::string>("ComponentName");
+  name_ = p.getParameter<std::string>("ComponentName");
   pset_ = p;
-  setWhatProduced(this, name);
+  setWhatProduced(this, name_);
 }
 
 std::shared_ptr<VectorHitBuilderEDProducer> SiPhase2RecHitMatcherESProducer::produce(
     const TkPhase2OTCPERecord& iRecord) {
-  if (name == "SiPhase2VectorHitMatcher") {
+  if (name_ == "SiPhase2VectorHitMatcher") {
     matcher_ = std::make_shared<VectorHitBuilderEDProducer>(pset_);
 
     edm::ESHandle<TrackerGeometry> tGeomHandle;
@@ -28,3 +44,36 @@ std::shared_ptr<VectorHitBuilderEDProducer> SiPhase2RecHitMatcherESProducer::pro
   }
   return matcher_;
 }
+
+void
+SiPhase2RecHitMatcherESProducer::fillDescriptions(edm::ConfigurationDescriptions& descriptions) {
+  edm::ParameterSetDescription desc;
+  desc.add<std::string>("offlinestubs", "vectorHits");
+  desc.add<int>("maxVectorHits", 999999999);
+  desc.add<std::string>("Algorithm", "VectorHitBuilderAlgorithm");
+  desc.add<std::string>("ComponentName", "SiPhase2VectorHitMatcher");
+  desc.add<edm::ESInputTag>("CPE", edm::ESInputTag("phase2StripCPEESProducer","Phase2StripCPE"));
+  desc.add<std::vector<double>>("BarrelCut", {
+    0.0,
+    0.05,
+    0.06,
+    0.08,
+    0.09,
+    0.12,
+    0.2,
+  });
+  desc.add<std::string>("Phase2CPE_name", "Phase2StripCPE");
+  desc.add<std::string>("Clusters", "siPhase2Clusters");
+  desc.add<int>("maxVectorHitsInAStack", 999);
+  desc.add<std::vector<double>>("EndcapCut", {
+    0.0,
+    0.1,
+    0.1,
+    0.1,
+    0.1,
+    0.1,
+  });
+  descriptions.add("SiPhase2RecHitMatcherESProducer", desc);
+}
+
+DEFINE_FWK_EVENTSETUP_MODULE(SiPhase2RecHitMatcherESProducer);
