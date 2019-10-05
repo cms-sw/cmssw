aad2c52e852d8e102f93cbb247a6bad81d946c4c
peruzzim <peruzzim@users.noreply.github.com>
2019-10-05 10:01:01 +0200

    code format

--- MuonAnalysis/MuonAssociators/plugins/MuonFSRAssociator.cc
+++ MuonAnalysis/MuonAssociators/plugins/MuonFSRAssociator.cc
@@ -25,42 +25,36 @@
 //
 
 class MuonFSRAssociator : public edm::global::EDProducer<> {
-
 public:
-  
-  explicit MuonFSRAssociator(const edm::ParameterSet &iConfig):
+  explicit MuonFSRAssociator(const edm::ParameterSet& iConfig)
+      :
 
-    photons_ {consumes<pat::GenericParticleCollection>(iConfig.getParameter<edm::InputTag>("photons"))},
-    muons_   {consumes<edm::View<reco::Muon>>(iConfig.getParameter<edm::InputTag>("muons"))}
+        photons_{consumes<pat::GenericParticleCollection>(iConfig.getParameter<edm::InputTag>("photons"))},
+        muons_{consumes<edm::View<reco::Muon>>(iConfig.getParameter<edm::InputTag>("muons"))}
 
   {
-      
-      produces<edm::Association<std::vector<pat::GenericParticle>>>();
-      produces<edm::ValueMap<int>>("fsrIndex");
+    produces<edm::Association<std::vector<pat::GenericParticle>>>();
+    produces<edm::ValueMap<int>>("fsrIndex");
+  }
 
-    }
-
-   static void fillDescriptions(edm::ConfigurationDescriptions& descriptions) {
+  static void fillDescriptions(edm::ConfigurationDescriptions& descriptions) {
     edm::ParameterSetDescription desc;
     desc.add<edm::InputTag>("photons")->setComment("FSR photon collection to associate with muons");
     desc.add<edm::InputTag>("muons")->setComment("collection of muons to associate with FSR photons");
 
     descriptions.add("MuonFSRAssociator", desc);
-  } 
+  }
   ~MuonFSRAssociator() override {}
-  
+
 private:
-  
   void produce(edm::StreamID, edm::Event&, const edm::EventSetup&) const override;
 
-// ----------member data ---------------------------
+  // ----------member data ---------------------------
   const edm::EDGetTokenT<pat::GenericParticleCollection> photons_;
   const edm::EDGetTokenT<edm::View<reco::Muon>> muons_;
-
 };
 
 void MuonFSRAssociator::produce(edm::StreamID streamID, edm::Event& iEvent, const edm::EventSetup& iSetup) const {
-
   using namespace std;
 
   edm::Handle<pat::GenericParticleCollection> photons;
@@ -68,11 +62,13 @@ void MuonFSRAssociator::produce(edm::StreamID streamID, edm::Event& iEvent, cons
   edm::Handle<edm::View<reco::Muon>> muons;
   iEvent.getByToken(muons_, muons);
 
-  std::vector<int> muonMapping(muons->size(),-1);
+  std::vector<int> muonMapping(muons->size(), -1);
   // loop over all muons
-  for (auto muon = muons->begin(); muon != muons->end(); ++muon){
-    for(auto iter_pho = photons->begin(); iter_pho != photons->end(); iter_pho++){
-      if (iter_pho->hasUserCand("associatedMuon") and iter_pho->userCand("associatedMuon")==reco::CandidatePtr(muons,muon-muons->begin())) muonMapping[muon-muons->begin()] = (iter_pho - photons->begin());
+  for (auto muon = muons->begin(); muon != muons->end(); ++muon) {
+    for (auto iter_pho = photons->begin(); iter_pho != photons->end(); iter_pho++) {
+      if (iter_pho->hasUserCand("associatedMuon") and
+          iter_pho->userCand("associatedMuon") == reco::CandidatePtr(muons, muon - muons->begin()))
+        muonMapping[muon - muons->begin()] = (iter_pho - photons->begin());
     }
   }
 
@@ -86,10 +82,8 @@ void MuonFSRAssociator::produce(edm::StreamID streamID, edm::Event& iEvent, cons
   edm::ValueMap<int>::Filler fillerBareIdx(*bareIdx);
   fillerBareIdx.insert(muons, muonMapping.begin(), muonMapping.end());
   fillerBareIdx.fill();
-  iEvent.put(std::move(bareIdx),"fsrIndex");
-
+  iEvent.put(std::move(bareIdx), "fsrIndex");
 }
 
 //define this as a plug-in
 DEFINE_FWK_MODULE(MuonFSRAssociator);
-
