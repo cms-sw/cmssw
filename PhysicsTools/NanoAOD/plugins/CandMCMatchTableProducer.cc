319a05c6afffd55759e733373eb88c60384c7c58
Andrew Levin <amlevin@cern.ch>
2019-10-07 23:21:07 +0200

    use 11 instead of 13 for electron match

--- PhysicsTools/NanoAOD/plugins/CandMCMatchTableProducer.cc
+++ PhysicsTools/NanoAOD/plugins/CandMCMatchTableProducer.cc
@@ -47,7 +47,7 @@ public:
             "5 = electron from b, 4 = electron from c, 3 = electron from light or unknown, 0 = unmatched";
         break;
       case MPhoton:
-        flavDoc_ = "1 = prompt photon, 13 = prompt electron, 0 = unknown or unmatched";
+        flavDoc_ = "1 = prompt photon, 11 = prompt electron, 0 = unknown or unmatched";
         break;
       case MTau:
         flavDoc_ =
@@ -117,7 +117,7 @@ public:
           if (match->isPromptFinalState() && match->pdgId() == 22)
             flav[i] = 1;  // prompt photon
           else if ((match->isPromptFinalState() || match->isDirectPromptTauDecayProductFinalState()) && abs(match->pdgId()) == 11)
-            flav[i] = 13; // prompt electron
+            flav[i] = 11; // prompt electron
           break;
         case MTau:
           // CV: assignment of status codes according to https://twiki.cern.ch/twiki/bin/viewauth/CMS/HiggsToTauTauWorking2016#MC_Matching
