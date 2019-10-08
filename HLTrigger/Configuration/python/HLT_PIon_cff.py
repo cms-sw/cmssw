f3e7d24cd831ba3d10b6b910c741ba2f30c1625d
Martin Grunewald <Martin.Grunewald@cern.ch>
2019-10-08 10:33:58 +0200

    Adding cosmics path for validation - CMSHLT-2097

--- HLTrigger/Configuration/python/HLT_PIon_cff.py
+++ HLTrigger/Configuration/python/HLT_PIon_cff.py
@@ -1,13 +1,13 @@
 # hltGetConfiguration --cff --data /dev/CMSSW_11_0_0/PIon --type PIon
 
-# /dev/CMSSW_11_0_0/PIon/V7 (CMSSW_11_0_0_pre9)
+# /dev/CMSSW_11_0_0/PIon/V8 (CMSSW_11_0_0_pre9)
 
 import FWCore.ParameterSet.Config as cms
 
 fragment = cms.ProcessFragment( "HLT" )
 
 fragment.HLTConfigVersion = cms.PSet(
-  tableName = cms.string('/dev/CMSSW_11_0_0/PIon/V7')
+  tableName = cms.string('/dev/CMSSW_11_0_0/PIon/V8')
 )
 
 fragment.transferSystem = cms.PSet( 
