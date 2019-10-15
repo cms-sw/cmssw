c5a2ead1c39573031c8c2cb65c410cddb37c6a14
Sven Dildick <sven.dildick@cern.ch>
2019-10-15 23:35:48 +0200

    Enable Converted ME0 trigger pseudo digis

--- L1Trigger/ME0Trigger/python/me0TriggerConvertedPseudoDigis_cfi.py
+++ L1Trigger/ME0Trigger/python/me0TriggerConvertedPseudoDigis_cfi.py
@@ -1,6 +1,6 @@
 import FWCore.ParameterSet.Config as cms
 
-me0TriggerPseudoDigis = cms.EDProducer("ME0TriggerPseudoProducer",
+me0TriggerConvertedPseudoDigis = cms.EDProducer("ME0TriggerPseudoProducer",
     ME0SegmentProducer = cms.InputTag("me0Segments"),
     info = cms.untracked.int32(0),
     DeltaPhiResolution = cms.untracked.double(0.25)# in term of trigger pad
