a4a87cffcb53a566ec5bb07075878dca6ec79db2
Huilin Qu <huilin.qu@cern.ch>
2019-10-19 14:30:08 +0200

    ONNXRuntime implementation of DeepDoubleX.

--- RecoBTag/Configuration/python/RecoBTag_cff.py
+++ RecoBTag/Configuration/python/RecoBTag_cff.py
@@ -8,7 +8,7 @@ from RecoBTag.Combined.combinedMVA_cff import *
 from RecoBTag.CTagging.RecoCTagging_cff import *
 from RecoBTag.Combined.deepFlavour_cff import *
 from RecoBTag.ONNXRuntime.pfDeepFlavour_cff import *
-from RecoBTag.TensorFlow.pfDeepDoubleX_cff import *
+from RecoBTag.ONNXRuntime.pfDeepDoubleX_cff import *
 from RecoBTag.ONNXRuntime.pfDeepBoostedJet_cff import *
 from RecoVertex.AdaptiveVertexFinder.inclusiveVertexing_cff import *
 from RecoBTag.PixelCluster.pixelClusterTagInfos_cfi import *
