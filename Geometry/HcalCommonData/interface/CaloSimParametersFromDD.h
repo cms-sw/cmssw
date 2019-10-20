5df1a87f5b1147cf1f69537064f8b97fde20b606
Sunanda <sunanda.banerjee@cern.ch>
2019-10-20 19:15:30 +0200

    Try to make CaloSimParameters object from dd4hep

--- Geometry/HcalCommonData/interface/CaloSimParametersFromDD.h
+++ Geometry/HcalCommonData/interface/CaloSimParametersFromDD.h
@@ -2,24 +2,34 @@
 #define HcalCommonData_CaloSimParametersFromDD_h
 
 #include "DetectorDescription/Core/interface/DDsvalues.h"
+#include "DetectorDescription/Core/interface/DDCompactView.h"
+#include <DetectorDescription/DDCMS/interface/DDCompactView.h>
+#include "FWCore/MessageLogger/interface/MessageLogger.h"
 #include <string>
+#include <vector>
 
-class DDCompactView;
 class DDFilteredView;
 class CaloSimulationParameters;
 
 class CaloSimParametersFromDD {
 public:
-  CaloSimParametersFromDD(bool fromDD4Hep);
+  CaloSimParametersFromDD() {}
   virtual ~CaloSimParametersFromDD() {}
 
   bool build(const DDCompactView*, CaloSimulationParameters&);
+  bool build(const cms::DDCompactView*, CaloSimulationParameters&);
 
 private:
+  bool buildParameters(const CaloSimulationParameters&);
   std::vector<std::string> getNames(const std::string&, const DDsvalues_type&, bool);
   std::vector<int> getNumbers(const std::string&, const DDsvalues_type&, bool);
 
-  bool fromDD4Hep_;
+  template <typename T>
+  void myPrint(std::string value, const std::vector<T>& vec) {
+    edm::LogVerbatim("HCalGeom") << "CaloSimParametersFromDD: " << vec.size() << " entries for " << value << ":";
+    for (unsigned int i = 0; i < vec.size(); i++)
+      edm::LogVerbatim("HCalGeom") << " (" << i << ") " << vec[i];
+  }
 };
 
 #endif
