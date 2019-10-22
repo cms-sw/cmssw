6ced0384987d034bffaab1cdc9adbf33f58605cc
Kevin Pedro <kpedro88@gmail.com>
2019-10-22 14:49:19 -0500

    include dedx producers for fastsim

--- RecoTracker/Configuration/python/RecoTracker_cff.py
+++ RecoTracker/Configuration/python/RecoTracker_cff.py
@@ -38,5 +38,6 @@ trackingGlobalRecoTask = cms.Task(ckftracksTask, trackExtrapolator)
 trackingGlobalReco = cms.Sequence(trackingGlobalRecoTask)
 
 from Configuration.Eras.Modifier_fastSim_cff import fastSim
-_fastSim_trackingGlobalReco = cms.Sequence(trackExtrapolator)
+ckftracks_fast = cms.Sequence(doAlldEdXEstimators)
+_fastSim_trackingGlobalReco = cms.Sequence(ckftracks_fast*trackExtrapolator)
 fastSim.toReplaceWith(trackingGlobalReco,_fastSim_trackingGlobalReco)
