308de009bd30daf35e8860ee7f885abc2318fea2
Alicia Calderon <calderon@ifca.unican.es>
2019-10-22 13:05:38 +0200

    customized MuonRecoAnalyzer

---
+++ DQM/MuonMonitor/src/SealModule.cc
@@ -0,0 +1,6 @@
+#include "FWCore/PluginManager/interface/ModuleDef.h"
+#include "FWCore/Framework/interface/MakerMacros.h"
+
+#include "DQM/MuonMonitor/interface/CosmicMuonRecoAnalyzer.h"
+
+DEFINE_FWK_MODULE(CosmicMuonRecoAnalyzer);
