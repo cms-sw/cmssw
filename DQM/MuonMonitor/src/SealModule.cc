56d9753c37c1f47f5cd18a7a18753ebd19bec380
Alicia Calderon <calderon@ifca.unican.es>
2019-10-23 21:13:33 +0200

    reverte

--- DQM/MuonMonitor/src/SealModule.cc
+++ DQM/MuonMonitor/src/SealModule.cc
@@ -1,6 +1,6 @@
 #include "FWCore/PluginManager/interface/ModuleDef.h"
 #include "FWCore/Framework/interface/MakerMacros.h"
 
-#include "DQM/MuonMonitor/interface/CosmicMuonRecoAnalyzer.h"
+#include "DQM/MuonMonitor/interface/MuonRecoAnalyzer.h"
 
-DEFINE_FWK_MODULE(CosmicMuonRecoAnalyzer);
+DEFINE_FWK_MODULE(MuonRecoAnalyzer);
