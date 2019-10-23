00748b03a12953341e5fb47b618882ed021543a2
Alicia Calderon <calderon@ifca.unican.es>
2019-10-23 12:40:50 +0200

    code-format

--- DQM/MuonMonitor/src/CosmicMuonRecoAnalyzer.cc
+++ DQM/MuonMonitor/src/CosmicMuonRecoAnalyzer.cc
@@ -2,7 +2,7 @@
 
 #include "DataFormats/Common/interface/Handle.h"
 #include "DataFormats/MuonReco/interface/Muon.h"
-#include "DataFormats/MuonReco/interface/MuonFwd.h" 
+#include "DataFormats/MuonReco/interface/MuonFwd.h"
 #include "DataFormats/MuonReco/interface/MuonEnergy.h"
 #include "DataFormats/TrackReco/interface/Track.h"
 #include "DataFormats/TrackReco/interface/TrackBase.h"
@@ -18,18 +18,16 @@
 using namespace std;
 using namespace edm;
 
-
-
 CosmicMuonRecoAnalyzer::CosmicMuonRecoAnalyzer(const edm::ParameterSet& pSet) {
   parameters = pSet;
 
   // the services:
   theService = new MuonServiceProxy(parameters.getParameter<ParameterSet>("ServiceParameters"));
-  theMuonCollectionLabel_ = consumes<edm::View<reco::Track> >  (parameters.getParameter<edm::InputTag>("MuonCollection"));
+  theMuonCollectionLabel_ = consumes<edm::View<reco::Track> >(parameters.getParameter<edm::InputTag>("MuonCollection"));
 
-  nTrkBin= parameters.getParameter<int>("nTrkBin");
-  nTrkMax= parameters.getParameter<int>("nTrkMax");  
-  nTrkMin= parameters.getParameter<int>("nTrkMin");  
+  nTrkBin = parameters.getParameter<int>("nTrkBin");
+  nTrkMax = parameters.getParameter<int>("nTrkMax");
+  nTrkMin = parameters.getParameter<int>("nTrkMin");
   ptBin = parameters.getParameter<int>("ptBin");
   ptMin = parameters.getParameter<double>("ptMin");
   ptMax = parameters.getParameter<double>("ptMax");
@@ -52,122 +50,113 @@ CosmicMuonRecoAnalyzer::CosmicMuonRecoAnalyzer(const edm::ParameterSet& pSet) {
   pResMin = parameters.getParameter<double>("pResMin");
   pResMax = parameters.getParameter<double>("pResMax");
 
-  hitsBin= parameters.getParameter<int>("hitsBin");
-  hitsMin= parameters.getParameter<int>("hitsMin");    
-  hitsMax= parameters.getParameter<int>("hitsMax");    
-  
+  hitsBin = parameters.getParameter<int>("hitsBin");
+  hitsMin = parameters.getParameter<int>("hitsMin");
+  hitsMax = parameters.getParameter<int>("hitsMax");
+
   theFolder = parameters.getParameter<string>("folder");
 }
 
+CosmicMuonRecoAnalyzer::~CosmicMuonRecoAnalyzer() { delete theService; }
 
-CosmicMuonRecoAnalyzer::~CosmicMuonRecoAnalyzer() {
-  delete theService;
-}
-
-void CosmicMuonRecoAnalyzer::bookHistograms(DQMStore::IBooker & ibooker,
-				      edm::Run const & /*iRun*/,
-				      edm::EventSetup const & /* iSetup */){
-
+void CosmicMuonRecoAnalyzer::bookHistograms(DQMStore::IBooker& ibooker,
+                                            edm::Run const& /*iRun*/,
+                                            edm::EventSetup const& /* iSetup */) {
   ibooker.cd();
   ibooker.setCurrentFolder(theFolder);
-  
 
   /////////////////////////////////////////////////////
   // monitoring of eta parameter
   /////////////////////////////////////////////////////
   std::string histname = "cosmicMuon";
 
-  nTracksSta = ibooker.book1D(histname+"_traks", "#tracks", nTrkBin, nTrkMin, nTrkMax);    
+  nTracksSta = ibooker.book1D(histname + "_traks", "#tracks", nTrkBin, nTrkMin, nTrkMax);
 
+  etaStaTrack = ibooker.book1D(histname + "_eta", "#eta_{STA}", etaBin, etaMin, etaMax);
 
-  etaStaTrack =           ibooker.book1D(histname+"_eta",   "#eta_{STA}",   etaBin, etaMin, etaMax);
-   
   //////////////////////////////////////////////////////
   // monitoring of theta parameter
   /////////////////////////////////////////////////////
 
-  thetaStaTrack = ibooker.book1D(histname+"_theta", "#theta_{STA}", thetaBin, thetaMin, thetaMax);
+  thetaStaTrack = ibooker.book1D(histname + "_theta", "#theta_{STA}", thetaBin, thetaMin, thetaMax);
   thetaStaTrack->setAxisTitle("rad");
 
   // monitoring of phi paramater
- 
-  phiStaTrack = ibooker.book1D(histname+"_phi", "#phi_{STA}", phiBin, phiMin, phiMax);
-  phiStaTrack->setAxisTitle("rad"); 
 
+  phiStaTrack = ibooker.book1D(histname + "_phi", "#phi_{STA}", phiBin, phiMin, phiMax);
+  phiStaTrack->setAxisTitle("rad");
 
   // monitoring of the chi2 parameter
-  
-  chi2OvDFStaTrack = ibooker.book1D(histname+"_chi2OverDf", "#chi_{2}OverDF_{STA}", chi2Bin, chi2Min, chi2Max);
+
+  chi2OvDFStaTrack = ibooker.book1D(histname + "_chi2OverDf", "#chi_{2}OverDF_{STA}", chi2Bin, chi2Min, chi2Max);
 
   //--------------------------
- 
-  probchi2StaTrack=ibooker.book1D(histname+"_probchi", "Prob #chi_{STA}", 120, chi2Min, 1.20);
 
+  probchi2StaTrack = ibooker.book1D(histname + "_probchi", "Prob #chi_{STA}", 120, chi2Min, 1.20);
 
   // monitoring of the momentum
 
-  pStaTrack = ibooker.book1D(histname+"_p", "p_{STA}", pBin, pMin, pMax);
-  pStaTrack->setAxisTitle("GeV"); 
+  pStaTrack = ibooker.book1D(histname + "_p", "p_{STA}", pBin, pMin, pMax);
+  pStaTrack->setAxisTitle("GeV");
 
-  qOverPStaTrack = ibooker.book1D(histname+"_qoverp", "qoverp_{STA}", pResBin, pResMin, pResMax ); 
+  qOverPStaTrack = ibooker.book1D(histname + "_qoverp", "qoverp_{STA}", pResBin, pResMin, pResMax);
   qOverPStaTrack->setAxisTitle("1/GeV");
 
-  qOverPStaTrack_p = ibooker.book2D(histname+"_qoverp_p","qoverp_p_{STA}", pBin, pMin, pMax, pResBin, pResMin, pResMax );                                       
+  qOverPStaTrack_p =
+      ibooker.book2D(histname + "_qoverp_p", "qoverp_p_{STA}", pBin, pMin, pMax, pResBin, pResMin, pResMax);
 
   // monitoring of the transverse momentum
- 
-  ptStaTrack = ibooker.book1D(histname+"_pt", "pt_{STA}", ptBin, ptMin, pMax);
-  ptStaTrack->setAxisTitle("GeV"); 
 
+  ptStaTrack = ibooker.book1D(histname + "_pt", "pt_{STA}", ptBin, ptMin, pMax);
+  ptStaTrack->setAxisTitle("GeV");
 
   // monitoring of the muon charge
- 
-  qStaTrack = ibooker.book1D(histname+"_q", "q_{STA}", 5, -2.5, 2.5);
 
-  
+  qStaTrack = ibooker.book1D(histname + "_q", "q_{STA}", 5, -2.5, 2.5);
+
   //////////////////////////////////////////////////////////////
   // monitoring of the phi-eta
 
-  phiVsetaStaTrack = ibooker.book2D(histname+"_phiVseta",      "#phi vs #eta (STA)", etaBin/2, etaMin, etaMax, phiBin/2, phiMin, phiMax);
-phiVsetaStaTrack->setAxisTitle("eta",1);
-phiVsetaStaTrack->setAxisTitle("phi",2);    
- 
-// monitoring the hits  
-nValidHitsStaTrack = ibooker.book1D(histname+"_nValidHits", "#valid hits (STA)", hitsBin, hitsMin, hitsMax);
+  phiVsetaStaTrack = ibooker.book2D(
+      histname + "_phiVseta", "#phi vs #eta (STA)", etaBin / 2, etaMin, etaMax, phiBin / 2, phiMin, phiMax);
+  phiVsetaStaTrack->setAxisTitle("eta", 1);
+  phiVsetaStaTrack->setAxisTitle("phi", 2);
 
-nValidHitsStaTrack_eta = ibooker.book2D(histname+"_nValidHits_eta", "#valid hits vs eta (STA)", etaBin, etaMin, etaMax, hitsBin, hitsMin, hitsMax);                         
-                                                                                                                 
-nValidHitsStaTrack_phi = ibooker.book2D(histname+"_nValidHits_phi", "#valid hits vs phi (STA)", phiBin, phiMin, phiMax, hitsBin, hitsMin, hitsMax);  
+  // monitoring the hits
+  nValidHitsStaTrack = ibooker.book1D(histname + "_nValidHits", "#valid hits (STA)", hitsBin, hitsMin, hitsMax);
 
+  nValidHitsStaTrack_eta = ibooker.book2D(
+      histname + "_nValidHits_eta", "#valid hits vs eta (STA)", etaBin, etaMin, etaMax, hitsBin, hitsMin, hitsMax);
+
+  nValidHitsStaTrack_phi = ibooker.book2D(
+      histname + "_nValidHits_phi", "#valid hits vs phi (STA)", phiBin, phiMin, phiMax, hitsBin, hitsMin, hitsMax);
 }
 
-
-void CosmicMuonRecoAnalyzer::analyze(const edm::Event& iEvent, const edm::EventSetup& iSetup){
-  LogTrace(metname)<<"[MuonRecoAnalyzer] Analyze the mu";
+void CosmicMuonRecoAnalyzer::analyze(const edm::Event& iEvent, const edm::EventSetup& iSetup) {
+  LogTrace(metname) << "[MuonRecoAnalyzer] Analyze the mu";
   theService->update(iSetup);
-  
+
   // Take the muon container
-  edm::Handle<edm::View<reco::Track> > muons; 
-  iEvent.getByToken(theMuonCollectionLabel_,muons);
-  
-  int nTracks_=0; 
- 
-  
-  if(!muons.isValid()) return;
+  edm::Handle<edm::View<reco::Track> > muons;
+  iEvent.getByToken(theMuonCollectionLabel_, muons);
 
-  for (edm::View<reco::Track>::const_iterator muon = muons->begin(); muon != muons->end(); ++muon){
+  int nTracks_ = 0;
 
-    nTracks_++; 
+  if (!muons.isValid())
+    return;
+
+  for (edm::View<reco::Track>::const_iterator muon = muons->begin(); muon != muons->end(); ++muon) {
+    nTracks_++;
 
     //Needed for MVA soft muon
-    
+
     // get the track using only the mu spectrometer data
-    
+
     etaStaTrack->Fill(muon->eta());
     thetaStaTrack->Fill(muon->theta());
     phiStaTrack->Fill(muon->phi());
     chi2OvDFStaTrack->Fill(muon->normalizedChi2());
-    probchi2StaTrack->Fill(TMath::Prob(muon->chi2(),muon->ndof()));
+    probchi2StaTrack->Fill(TMath::Prob(muon->chi2(), muon->ndof()));
     pStaTrack->Fill(muon->p());
     ptStaTrack->Fill(muon->pt());
     qStaTrack->Fill(muon->charge());
@@ -175,12 +164,10 @@ void CosmicMuonRecoAnalyzer::analyze(const edm::Event& iEvent, const edm::EventS
     qOverPStaTrack_p->Fill(muon->p(), muon->qoverp());
     phiVsetaStaTrack->Fill(muon->eta(), muon->phi());
 
-    nValidHitsStaTrack->Fill(muon->numberOfValidHits());    
+    nValidHitsStaTrack->Fill(muon->numberOfValidHits());
     nValidHitsStaTrack_eta->Fill(muon->eta(), muon->numberOfValidHits());
     nValidHitsStaTrack_phi->Fill(muon->phi(), muon->numberOfValidHits());
+  }
 
-  }  
- 
-  nTracksSta->Fill(nTracks_);   
-
-}    
+  nTracksSta->Fill(nTracks_);
+}
