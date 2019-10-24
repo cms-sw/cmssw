3ffdf28f2abedbd21f68fcf03942307fb916b50e
Kevin Pedro <kpedro88@gmail.com>
2019-10-24 11:55:52 -0500

    simplify tasks

--- RecoTracker/Configuration/python/RecoTracker_cff.py
+++ RecoTracker/Configuration/python/RecoTracker_cff.py
@@ -38,6 +38,4 @@ trackingGlobalRecoTask = cms.Task(ckftracksTask, trackExtrapolator)
 trackingGlobalReco = cms.Sequence(trackingGlobalRecoTask)
 
 from Configuration.Eras.Modifier_fastSim_cff import fastSim
-ckftracks_fast = cms.Sequence(doAlldEdXEstimators)
-_fastSim_trackingGlobalReco = cms.Sequence(ckftracks_fast*trackExtrapolator)
-fastSim.toReplaceWith(trackingGlobalReco,_fastSim_trackingGlobalReco)
+fastSim.toReplaceWith(trackingGlobalRecoTask, cms.Task(doAlldEdXEstimatorsTask, trackExtrapolator))
