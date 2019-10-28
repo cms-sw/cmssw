5180a11e407889b9f79651074bd56d3b7d847c96
Jakob <jakob.salfeld@cern.ch>
2019-10-28 09:29:10 +0100

    added (removed) trackQuality (debug) parameter from hgcalTrackCollection_cfi.py, added trackQuality parameter to particleFlowBlock_cfi.py

--- RecoParticleFlow/PFTracking/python/hgcalTrackCollection_cfi.py
+++ RecoParticleFlow/PFTracking/python/hgcalTrackCollection_cfi.py
@@ -3,9 +3,8 @@ import FWCore.ParameterSet.Config as cms
 hgcalTrackCollection = cms.EDProducer(
     "HGCalTrackCollectionProducer",
     src = cms.InputTag("pfTrack"),
-    debug = cms.bool(False),
+    trackQuality = cms.string("highPurity"),
     # From GeneralTracksImporter
-
     useIterativeTracking = cms.bool(True),
     DPtOverPtCuts_byTrackAlgo = cms.vdouble(10.0,10.0,10.0,10.0,10.0,5.0),
     NHitCuts_byTrackAlgo = cms.vuint32(3,3,3,3,3,32700), # the last value is nonsense
