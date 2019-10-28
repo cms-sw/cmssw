0e7a41169c6abf3f3e6a7abaa6499de17ecc42af
Marcel Andre Schneider <marcel.andre.schneider@cern.ch>
2019-10-28 18:18:05 +0100

    Remove DQMServices/Diagonstic.
    This looks like an old variation of Historic DQM. Since the new HDQM
    managed by the DQM group since more than a year lives outside CMSSW,
    and Tracker HDQM did for a long time before, I assume this is unused.
    
    I can't tell if anybody is using maybe some of the funcitonality in that
    package, but removing it is probably the best way to find out.

--- DQMServices/Diagnostic/scripts/Database/Python/PopulateDB.py
+++
@@ -1,8 +0,0 @@
-from __future__ import print_function
-import os
-
-class PopulateDB:
-    def run(self):
-        os.system("cat "+self.TemplatesDir+"/template_"+self.DetName+"HistoryDQMService_cfg.py | sed -e \"s@RUNNUMBER@"+self.RunNumber+"@g\" -e \"s@FILENAME@"+self.FileName+"@\" -e \"s@TAGNAME@"+self.TagName+"@g\" -e \"s@DATABASE@"+self.Database+"@\" -e \"s@AUTHENTICATIONPATH@"+self.AuthenticationPath+"@\" > "+self.Dir+"Run_"+self.DetName+"_"+self.RunNumber+".py")
-        print("cd "+self.CMSSW_Version+"; eval `scramv1 r -sh`; cd "+self.Dir+"; cmsRun "+self.Dir+"Run_"+self.DetName+"_"+self.RunNumber+".py > "+self.Dir+"Run_"+self.DetName+"_"+self.RunNumber+".log")
-        os.system("cd "+self.CMSSW_Version+"; eval `scramv1 r -sh`; cd "+self.Dir+"; cmsRun "+self.Dir+"Run_"+self.DetName+"_"+self.RunNumber+".py > "+self.Dir+"Run_"+self.DetName+"_"+self.RunNumber+".log")
