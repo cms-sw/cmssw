eb7424349771d7448014a16db94b1fd3223ca34c
Giovanni Franzoni <giovanni.franzoni@cern.ch>
2019-10-29 23:02:23 +0100

    readibilit

--- DataFormats/HGCDigi/interface/HGCSample.h
+++ DataFormats/HGCDigi/interface/HGCSample.h
@@ -20,7 +20,7 @@ public:
 
   /**
      @short CTOR
-   */
+  */
 
  HGCSample() : value_(0) { }
  HGCSample(uint32_t value) : value_(value) { }
@@ -28,17 +28,17 @@ public:
 
   /**
      @short setters
-   */
+  */
 
-  // GF indentation
-  void setThreshold(bool thr) { setWord(thr, kThreshMask, kThreshShift); }
-  void setMode(bool mode) { setWord(mode, kModeMask, kModeShift); }
-  void setGain(uint16_t gain) { setWord(gain, kGainMask, kToGainShift); }
-  void setToA(uint16_t toa) { setWord(toa, kToAMask, kToAShift); }
-  void setData(uint16_t data) { setWord(data, kDataMask, kDataShift); }
+  void setThreshold(bool thr)     { setWord(thr, kThreshMask, kThreshShift); }
+  void setMode(bool mode)         { setWord(mode, kModeMask, kModeShift); }
+  void setGain(uint16_t gain)     { setWord(gain, kGainMask, kToGainShift); }
+  void setToA(uint16_t toa)       { setWord(toa, kToAMask, kToAShift); }
+  void setData(uint16_t data)     { setWord(data, kDataMask, kDataShift); }
   void setToAValid(bool toaFired) { setWord(toaFired, kToAValidMask, kToAValidShift); }
 
-  void set(bool thr, bool mode, uint16_t gain, uint16_t toa, uint16_t data) {
+  void set(bool thr, bool mode, uint16_t gain,
+	   uint16_t toa, uint16_t data) {
     setThreshold(thr);
     setMode(mode);
     setGain(gain);
@@ -47,27 +47,28 @@ public:
   }
 
   void print(std::ostream& out = std::cout) {
-    out << "THR: " << threshold() << " Mode: " << mode() << " ToA: " << toa() << " Data: " << data() << " Raw=0x"
+    out << "THR: " << threshold() << " Mode: " << mode() 
+	<< " ToA: " << toa() << " Data: " << data() << " Raw=0x"
         << std::hex << raw() << std::dec << std::endl;
   }
 
   /**
      @short getters
   */
-  uint32_t raw() const { return value_; }
-  bool     threshold() const { return getWord(kThreshMask, kThreshShift); }
-  bool     mode() const { return getWord(kModeMask, kModeShift); }
-  uint32_t gain() const { return getWord(kGainMask, kToGainShift); }
-  uint32_t toa() const { return getWord(kToAMask, kToAShift); }
-  uint32_t data() const { return getWord(kDataMask, kDataShift); }
+  uint32_t raw()         const { return value_; }
+  bool     threshold()   const { return getWord(kThreshMask, kThreshShift); }
+  bool     mode()        const { return getWord(kModeMask, kModeShift); }
+  uint32_t gain()        const { return getWord(kGainMask, kToGainShift); }
+  uint32_t toa()         const { return getWord(kToAMask, kToAShift); }
+  uint32_t data()        const { return getWord(kDataMask, kDataShift); }
   bool     getToAValid() const { return getWord(kToAValidMask, kToAValidShift); }
   uint32_t operator()() { return value_; }
 
 
-private:
+ private:
   /**
      @short wrapper to reset words at a given position
-   */
+  */
   void setWord(uint32_t word, uint32_t mask, uint32_t shift) {
 
     // deal with saturation: set to mask 
