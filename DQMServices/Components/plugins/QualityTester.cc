5c935311c53b84d47901339b7902519f41828a06
Marcel Andre Schneider <marcel.andre.schneider@cern.ch>
2019-10-30 14:57:31 +0100

    Add remaining parameters and types.
    The parameter names are terribly inconsistent, I extracted the mappings
    form the old ClientConfig/src/QTestConfigure.cc. I set very few
    defaults, lte's see how many errors that causes.

--- DQMServices/Components/plugins/QualityTester.cc
+++ DQMServices/Components/plugins/QualityTester.cc
@@ -171,24 +171,123 @@ void QualityTester::performTests() {
 }
 
 std::unique_ptr<QCriterion> QualityTester::makeQCriterion(boost::property_tree::ptree const& config) {
+  // For whatever reasons the compiler needs the "template" keyword on ptree::get.
+  // To save some noise in the code we have the preprocessor add it everywhere.
+#define get template get
+  // TODO: make the parameter names more consistent.
+  // TODO: add defaults where it makes sense.
   std::map<std::string,
            std::function<std::unique_ptr<QCriterion>(boost::property_tree::ptree const&, std::string& name)>>
-      qtestmakers = {
-          {ContentsXRange::getAlgoName(),
-           [](auto const& config, std::string& name) {
-             auto test = std::make_unique<ContentsXRange>(name);
-             test->setAllowedXRange(config.template get<double>("xmin"), config.template get<double>("xmax"));
-             return test;
-           }},
-          {ContentsYRange::getAlgoName(),
-           [](auto const& config, std::string& name) {
-             auto test = std::make_unique<ContentsYRange>(name);
-             test->setAllowedYRange(config.template get<double>("ymin"), config.template get<double>("ymax"));
-             test->setUseEmptyBins(config.template get<bool>("useEmptyBins"));
-             return test;
-           }}
-          // TODO: add more types
-      };
+      qtestmakers = {{CheckVariance::getAlgoName(),
+                      [](auto const& config, std::string& name) {
+                        auto test = std::make_unique<CheckVariance>(name);
+                        test->setWarningProb(config.get<float>("warning", QCriterion::WARNING_PROB_THRESHOLD));
+                        test->setErrorProb(config.get<float>("error", QCriterion::ERROR_PROB_THRESHOLD));
+                        return test;
+                      }},
+                     {CompareLastFilledBin::getAlgoName(),
+                      [](auto const& config, std::string& name) {
+                        auto test = std::make_unique<CompareLastFilledBin>(name);
+                        test->setWarningProb(config.get<float>("warning", QCriterion::WARNING_PROB_THRESHOLD));
+                        test->setErrorProb(config.get<float>("error", QCriterion::ERROR_PROB_THRESHOLD));
+                        test->setAverage(config.get<float>("AvVal"));
+                        test->setMin(config.get<float>("MinVal"));
+                        test->setMax(config.get<float>("MaxVal"));
+                        return test;
+                      }},
+                     {CompareToMedian::getAlgoName(),
+                      [](auto const& config, std::string& name) {
+                        auto test = std::make_unique<CompareToMedian>(name);
+                        test->setWarningProb(config.get<float>("warning", QCriterion::WARNING_PROB_THRESHOLD));
+                        test->setErrorProb(config.get<float>("error", QCriterion::ERROR_PROB_THRESHOLD));
+                        test->setMin(config.get<float>("MinRel"));
+                        test->setMax(config.get<float>("MaxRel"));
+                        test->setMinMedian(config.get<float>("MinAbs"));
+                        test->setMaxMedian(config.get<float>("MaxAbs"));
+                        test->setEmptyBins(config.get<int>("UseEmptyBins"));
+                        test->setStatCut(config.get<float>("StatCut", 0));
+                        return test;
+                      }},
+                     {ContentSigma::getAlgoName(),
+                      [](auto const& config, std::string& name) {
+                        auto test = std::make_unique<ContentSigma>(name);
+                        test->setWarningProb(config.get<float>("warning", QCriterion::WARNING_PROB_THRESHOLD));
+                        test->setErrorProb(config.get<float>("error", QCriterion::ERROR_PROB_THRESHOLD));
+                        test->setNumXblocks(config.get<int>("Xblocks"));
+                        test->setNumYblocks(config.get<int>("Yblocks"));
+                        test->setNumNeighborsX(config.get<int>("neighboursX"));
+                        test->setNumNeighborsY(config.get<int>("neighboursY"));
+                        test->setToleranceNoisy(config.get<double>("toleranceNoisy"));
+                        test->setToleranceDead(config.get<double>("toleranceDead"));
+                        test->setNoisy(config.get<double>("noisy"));
+                        test->setDead(config.get<double>("dead"));
+                        test->setXMin(config.get<int>("xMin"));
+                        test->setXMax(config.get<int>("xMax"));
+                        test->setYMin(config.get<int>("yMin"));
+                        test->setYMax(config.get<int>("yMax"));
+                        return test;
+                      }},
+                     {ContentsWithinExpected::getAlgoName(),
+                      [](auto const& config, std::string& name) {
+                        auto test = std::make_unique<ContentsWithinExpected>(name);
+                        test->setWarningProb(config.get<float>("warning", QCriterion::WARNING_PROB_THRESHOLD));
+                        test->setErrorProb(config.get<float>("error", QCriterion::ERROR_PROB_THRESHOLD));
+                        test->setUseEmptyBins(config.get<bool>("useEmptyBins"));
+                        test->setMinimumEntries(config.get<int>("minEntries"));
+                        test->setMeanTolerance(config.get<float>("toleranceMean"));
+                        test->setMeanRange(config.get<double>("minMean"), config.get<double>("maxMean"));
+                        test->setRMSRange(config.get<double>("minRMS"), config.get<double>("maxRMS"));
+                        return test;
+                      }},
+                     {ContentsXRange::getAlgoName(),
+                      [](auto const& config, std::string& name) {
+                        auto test = std::make_unique<ContentsXRange>(name);
+                        test->setWarningProb(config.get<float>("warning", QCriterion::WARNING_PROB_THRESHOLD));
+                        test->setErrorProb(config.get<float>("error", QCriterion::ERROR_PROB_THRESHOLD));
+                        test->setAllowedXRange(config.get<double>("xmin"), config.get<double>("xmax"));
+                        return test;
+                      }},
+                     {ContentsYRange::getAlgoName(),
+                      [](auto const& config, std::string& name) {
+                        auto test = std::make_unique<ContentsYRange>(name);
+                        test->setWarningProb(config.get<float>("warning", QCriterion::WARNING_PROB_THRESHOLD));
+                        test->setErrorProb(config.get<float>("error", QCriterion::ERROR_PROB_THRESHOLD));
+                        test->setAllowedYRange(config.get<double>("ymin"), config.get<double>("ymax"));
+                        test->setUseEmptyBins(config.get<bool>("useEmptyBins"));
+                        return test;
+                      }},
+                     {DeadChannel::getAlgoName(),
+                      [](auto const& config, std::string& name) {
+                        auto test = std::make_unique<DeadChannel>(name);
+                        test->setWarningProb(config.get<float>("warning", QCriterion::WARNING_PROB_THRESHOLD));
+                        test->setErrorProb(config.get<float>("error", QCriterion::ERROR_PROB_THRESHOLD));
+                        test->setThreshold(config.get<int>("threshold"));
+                        return test;
+                      }},
+                     {MeanWithinExpected::getAlgoName(),
+                      [](auto const& config, std::string& name) {
+                        auto test = std::make_unique<MeanWithinExpected>(name);
+                        test->setWarningProb(config.get<float>("warning", QCriterion::WARNING_PROB_THRESHOLD));
+                        test->setErrorProb(config.get<float>("error", QCriterion::ERROR_PROB_THRESHOLD));
+                        test->setMinimumEntries(config.get<int>("minEntries", 0));
+                        test->setExpectedMean(config.get<double>("mean"));
+                        if (config.get<double>("useSigma", 0))
+                          test->useSigma(config.get<double>("useSigma"));
+                        if (config.get<int>("useRMS", 0))
+                          test->useRMS();
+                        if (config.get<int>("useRange", 0))
+                          test->useRange(config.get<float>("xmin"), config.get<float>("xmax"));
+                        return test;
+                      }},
+                     {NoisyChannel::getAlgoName(), [](auto const& config, std::string& name) {
+                        auto test = std::make_unique<NoisyChannel>(name);
+                        test->setWarningProb(config.get<float>("warning", QCriterion::WARNING_PROB_THRESHOLD));
+                        test->setErrorProb(config.get<float>("error", QCriterion::ERROR_PROB_THRESHOLD));
+                        test->setTolerance(config.get<double>("tolerance"));
+                        test->setNumNeighbors(config.get<double>("neighbours"));
+                        return test;
+                      }}};
+#undef get
 
   auto maker = qtestmakers.find(config.get<std::string>("TYPE"));
   // Check if the type is known, error out otherwise.
@@ -231,7 +330,7 @@ void QualityTester::configureTests(std::string const& file) {
   }
   auto& testconfig = it->second;
   for (auto& kv : testconfig) {
-    // QTEST describes a QTest object (atually QCriterion) with its parameters
+    // QTEST describes a QTest object (actually QCriterion) with its parameters
     if (kv.first == "QTEST") {
       auto& qtestconfig = kv.second;
       auto name = qtestconfig.get<std::string>("<xmlattr>.name");
