5fdcc54ff5f14c7ff6c0201c26815082adbcb59b
Marcel Andre Schneider <marcel.andre.schneider@cern.ch>
2019-10-30 11:55:25 +0100

    Change datastructure in QualityTester.

--- DQMServices/Components/plugins/QualityTester.cc
+++ DQMServices/Components/plugins/QualityTester.cc
@@ -67,12 +67,13 @@ private:
 
   DQMStore* bei;
 
-  struct TestItem {
-    std::unique_ptr<QCriterion> qtest;
-    std::vector<std::string> pathpatterns;
-  };
-
-  std::map<std::string, TestItem> qtests;
+  // this vector holds and owns all the QTest objects. mostly for memory management.
+  std::vector<std::unique_ptr<QCriterion>> qtestobjects;
+  // we use this structure to check which tests to run on which MEs. The first
+  // part is the pattern with wildcards matching MEs that the test should be
+  // applied to, the second points to an object in qtestobjects. We cannot own
+  // the objects here since more than one pattern can use the same test.
+  std::vector<std::pair<std::string, QCriterion*>> qtestpatterns;
 
   void configureTests(std::string const& file);
   void attachTests();
@@ -141,6 +142,12 @@ void QualityTester::performTests() {
   edm::LogVerbatim("QualityTester") << "Running the Quality Test";
 
   // TODO: runQTests() on each ME
+  // for (auto& kv : this->qtests) {
+  //   std::cout << "+++ Qtest " << kv.first << " " << kv.second.qtest.get() <<  "\n";
+  //   for (auto& p : kv.second.pathpatterns) {
+  //     std::cout << "   +++ at " << p << "\n";
+  //   }
+  // }
 
   if (!reportThreshold.empty()) {
     // map {red, orange, black} -> [QReport message, ...]
@@ -190,7 +197,7 @@ std::unique_ptr<QCriterion> QualityTester::makeQCriterion(boost::property_tree::
 
   // The QTest XML format has structure
   // <QTEST><TYPE>QTestClass</TYPE><PARAM name="thing">value</PARAM>...</QTEST>
-  // but that is a pain to read with property_tree. Se we reorder the structure
+  // but that is a pain to read with property_tree. So we reorder the structure
   // and add a child "thing" with data "value" for each param to a new tree.
   // Then the qtestmakers can just config.get<type>("thing").
   boost::property_tree::ptree reordered;
@@ -206,6 +213,15 @@ std::unique_ptr<QCriterion> QualityTester::makeQCriterion(boost::property_tree::
 }
 
 void QualityTester::configureTests(std::string const& file) {
+  // We read QTests from the config file into this structure, before
+  // transforming into the final [(pattern, *qtest), ...] structure.
+  struct TestItem {
+    std::unique_ptr<QCriterion> qtest;
+    std::vector<std::string> pathpatterns;
+  };
+  std::map<std::string, TestItem> qtestmap;
+
+  // read XML using property tree. Should throw on error.
   boost::property_tree::ptree xml;
   boost::property_tree::read_xml(file, xml);
 
@@ -213,32 +229,47 @@ void QualityTester::configureTests(std::string const& file) {
   if (it == xml.not_found()) {
     throw cms::Exception("QualityTester") << "QTest XML needs to have a TESTSCONFIGURATION node.";
   }
-  auto& testconfig = xml.find("TESTSCONFIGURATION")->second;
-  for (auto kv : testconfig) {
-    // TODO: check tag, do thing
+  auto& testconfig = it->second;
+  for (auto& kv : testconfig) {
+    // QTEST describes a QTest object (atually QCriterion) with its parameters
     if (kv.first == "QTEST") {
       auto& qtestconfig = kv.second;
       auto name = qtestconfig.get<std::string>("<xmlattr>.name");
       auto value = makeQCriterion(qtestconfig);
       // LINK and QTEST can be in any order, so this element may or may not exist
-      this->qtests[name].qtest = std::move(value);
+      qtestmap[name].qtest = std::move(value);
     }  // else
+    // LINK associates one ore more QTest referenced by name to a ME path pattern.
     if (kv.first == "LINK") {
       auto& linkconfig = kv.second;
-      auto objname = linkconfig.get<std::string>("<xmlattr>.name");
-      for (auto subkv : linkconfig) {
+      auto pathpattern = linkconfig.get<std::string>("<xmlattr>.name");
+      for (auto& subkv : linkconfig) {
         if (subkv.first == "TestName") {
           std::string testname = subkv.second.data();
           bool enabled = subkv.second.get<bool>("<xmlattr>.activate");
           if (enabled) {
             // LINK and QTEST can be in any order, so this element may or may not exist
-            this->qtests[testname].pathpatterns.push_back(objname);
+            qtestmap[testname].pathpatterns.push_back(pathpattern);
           }
         }
       }
     }
     // else: unknown tag, but that is fine, its XML
   }
+
+  // now that we have all the objects created and references resolved, flatten
+  // the structure into something more useful for evaluating tests.
+  this->qtestobjects.clear();
+  this->qtestpatterns.clear();
+  for (auto& kv : qtestmap) {
+    QCriterion* bareptr = kv.second.qtest.get();
+    for (auto& p : kv.second.pathpatterns) {
+      this->qtestpatterns.push_back(std::make_pair(p, bareptr));
+    }
+    this->qtestobjects.push_back(std::move(kv.second.qtest));
+  }
+  // we could sort the patterns here to allow more performant matching
+  // (using a suffix-array to handle the "*" in the beginning)
 }
 
 void QualityTester::attachTests() {}
