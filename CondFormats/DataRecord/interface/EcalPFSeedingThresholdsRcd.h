de005bbaa9003238efe004443c877919917eaacb
argiro <Stefano.Argiro@cern.ch>
2019-10-31 10:53:45 +0100

    record for xtal-dependent seeding thresholds

---
+++ CondFormats/DataRecord/interface/EcalPFSeedingThresholdsRcd.h
@@ -0,0 +1,6 @@
+#ifndef CondFormats_DataRecord_EcalPFSeedingThresholdsRcd_h
+#define CondFormats_DataRecord_EcalPFSeedingThresholdsRcd_h
+
+#include "FWCore/Framework/interface/EventSetupRecordImplementation.h"
+class EcalPFSeedingThresholdsRcd : public edm::eventsetup::EventSetupRecordImplementation<EcalPFSeedingThresholdsRcd> {};
+#endif
---
+++ CondFormats/EcalObjects/interface/EcalPFSeedingThresholds.h
@@ -0,0 +1,15 @@
+#ifndef CondFormats_EcalObjects_EcalPFSeedingThresholds_H
+#define CondFormats_EcalObjects_EcalPFSeedingThresholds_H
+/**
+ * Author: Maria Giulia Ratti, ETH Zurich
+ * Created: 05 Sep 2019
+ * $Id: EcalPFSeedingThresholds.h, 2019/09/05 20:21:42 mratti Exp $
+ **/
+#include "CondFormats/EcalObjects/interface/EcalCondObjectContainer.h"
+
+typedef float EcalPFSeedingThreshold;
+typedef EcalFloatCondObjectContainer EcalPFSeedingThresholdsMap;
+typedef EcalPFSeedingThresholdsMap EcalPFSeedingThresholds;
+
+
+#endif
