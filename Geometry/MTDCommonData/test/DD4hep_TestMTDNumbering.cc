2e4c9c1209b12131b90b7c02e57a6db0b48b7766
Fabio Cossutti <fabio.cossutti@ts.infn.it>
2019-11-05 14:09:17 +0100

    Reorganize scenario D46 as D50 with new xml structure but same content, prepare for DD4hep migration

---
+++ Geometry/MTDCommonData/test/DD4hep_TestMTDNumbering.cc
@@ -0,0 +1,215 @@
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <utility>
+#include <vector>
+
+#include "FWCore/Framework/interface/Frameworkfwd.h"
+#include "FWCore/Framework/interface/one/EDAnalyzer.h"
+#include "FWCore/Framework/interface/Event.h"
+#include "FWCore/Framework/interface/EventSetup.h"
+#include "FWCore/Framework/interface/ESTransientHandle.h"
+#include "FWCore/Framework/interface/MakerMacros.h"
+#include "FWCore/ParameterSet/interface/ParameterSet.h"
+#include "FWCore/Utilities/interface/Exception.h"
+
+#include "Geometry/Records/interface/IdealGeometryRecord.h"
+
+#include "DetectorDescription/DDCMS/interface/DDFilteredView.h"
+
+#include "Geometry/MTDCommonData/interface/MTDBaseNumber.h"
+#include "Geometry/MTDCommonData/interface/BTLNumberingScheme.h"
+#include "Geometry/MTDCommonData/interface/ETLNumberingScheme.h"
+
+#include "DataFormats/ForwardDetId/interface/BTLDetId.h"
+#include "DataFormats/ForwardDetId/interface/ETLDetId.h"
+
+//#define EDM_ML_DEBUG
+
+using namespace cms;
+
+class DD4hep_TestMTDNumbering : public edm::one::EDAnalyzer<> {
+public:
+  explicit DD4hep_TestMTDNumbering(const edm::ParameterSet&);
+  ~DD4hep_TestMTDNumbering() override;
+
+  void beginJob() override {}
+  void analyze(edm::Event const&, edm::EventSetup const&) override;
+  void endJob() override {}
+
+  // void theBaseNumber(const DDGeoHistory& gh);
+
+  void checkMTD(const DDCompactView& cpv,
+                std::string fname = "GeoHistory",
+                int nVols = 0,
+                std::string ddtop_ = "mtd:BarrelTimingLayer");
+
+private:
+  const edm::ESInputTag tag_;
+  std::string fname_;
+  int nNodes_;
+  std::string ddTopNodeName_;
+  uint32_t theLayout_;
+
+  MTDBaseNumber thisN_;
+  BTLNumberingScheme btlNS_;
+  ETLNumberingScheme etlNS_;
+};
+
+DD4hep_TestMTDNumbering::DD4hep_TestMTDNumbering(const edm::ParameterSet& iConfig)
+  : tag_(iConfig.getParameter<edm::ESInputTag>("DDDetector")),
+    fname_(iConfig.getUntrackedParameter<std::string>("outFileName", "GeoHistory")),
+    nNodes_(iConfig.getUntrackedParameter<uint32_t>("numNodesToDump", 0)),
+    ddTopNodeName_(iConfig.getUntrackedParameter<std::string>("ddTopNodeName", "btl:BarrelTimingLayer")),
+    theLayout_(iConfig.getUntrackedParameter<uint32_t>("theLayout", 1)),
+    thisN_(),
+    btlNS_(),
+    etlNS_() {}
+
+DD4hep_TestMTDNumbering::~DD4hep_TestMTDNumbering() {}
+
+void DD4hep_TestMTDNumbering::analyze(const edm::Event& iEvent, const edm::EventSetup& iSetup) {
+
+  edm::ESTransientHandle<DDCompactView> pDD;
+  iSetup.get<IdealGeometryRecord>().get(tag_, pDD);
+
+  if (ddTopNodeName_ != "btl:BarrelTimingLayer" && ddTopNodeName_ != "etl:EndcapTimingLayer") {
+    edm::LogWarning("DD4hep_TestMTDNumbering") << ddTopNodeName_ << "Not valid top MTD volume";
+    return;
+  }
+
+  if (!pDD.isValid()) {
+    edm::LogError("DD4hep_TestMTDNumbering") << "ESTransientHandle<DDCompactView> pDD is not valid!";
+    return;
+  }
+  if (pDD.description()) {
+    edm::LogInfo("DD4hep_TestMTDNumbering") << pDD.description()->type_ << " label: " << pDD.description()->label_;
+  } else {
+    edm::LogWarning("DD4hep_TestMTDNumbering") << "NO label found pDD.description() returned false.";
+  }
+
+  //  checkMTD(*pDD, fname_, nNodes_, ddTopNodeName_);
+}
+
+void DD4hep_TestMTDNumbering::checkMTD(const DDCompactView& cpv, std::string fname, int nVols, std::string ddtop_) {
+  fname = "dump" + fname;
+
+  DDFilteredView fv(cpv);
+
+  edm::LogInfo("DD4hep_TestMTDNumbering") << "Top Most LogicalPart = " << fv.name();
+
+//   using nav_type = DDFilteredView::nav_type;
+//   using id_type = std::map<nav_type, int>;
+//   id_type idMap;
+//   int id = 0;
+//   std::ofstream dump(fname.c_str());
+//   bool notReachedDepth(true);
+
+//   bool write = false;
+//   bool isBarrel = true;
+//   size_t limit = 0;
+
+//   do {
+//     nav_type pos = fv.navPos();
+//     idMap[pos] = id;
+
+//     size_t num = fv.geoHistory().size();
+
+//     if (num <= limit) {
+//       write = false;
+//     }
+//     if (fv.geoHistory()[num - 1].logicalPart().name() == "btl:BarrelTimingLayer") {
+//       isBarrel = true;
+//       limit = num;
+//       write = true;
+// #ifdef EDM_ML_DEBUG
+//       edm::LogInfo("DD4hep_TestMTDNumbering") << "isBarrel = " << isBarrel;
+// #endif
+//     } else if (fv.geoHistory()[num - 1].logicalPart().name() == "etl:EndcapTimingLayer") {
+//       isBarrel = false;
+//       limit = num;
+//       write = true;
+// #ifdef EDM_ML_DEBUG
+//       edm::LogInfo("DD4hep_TestMTDNumbering") << "isBarrel = " << isBarrel;
+// #endif
+//     }
+
+//     // Actions for MTD volumes: searchg for sensitive detectors
+
+//     if (write && fv.geoHistory()[limit - 1].logicalPart().name() == ddtop_) {
+//       dump << " - " << fv.geoHistory();
+//       dump << "\n";
+
+//       bool isSens = false;
+
+//       if (fv.geoHistory()[num - 1].logicalPart().specifics().size() > 0) {
+//         for (auto elem : *(fv.geoHistory()[num - 1].logicalPart().specifics()[0])) {
+//           if (elem.second.name() == "SensitiveDetector") {
+//             isSens = true;
+//             break;
+//           }
+//         }
+//       }
+
+//       // Check of numbering scheme for sensitive detectors
+
+//       if (isSens) {
+//         theBaseNumber(fv.geoHistory());
+
+//         if (isBarrel) {
+//           BTLDetId::CrysLayout lay = static_cast<BTLDetId::CrysLayout>(theLayout_);
+//           BTLDetId theId(btlNS_.getUnitID(thisN_));
+//           int hIndex = theId.hashedIndex(lay);
+//           BTLDetId theNewId(theId.getUnhashedIndex(hIndex, lay));
+//           dump << theId;
+//           dump << "\n layout type = " << static_cast<int>(lay);
+//           dump << "\n ieta        = " << theId.ieta(lay);
+//           dump << "\n iphi        = " << theId.iphi(lay);
+//           dump << "\n hashedIndex = " << theId.hashedIndex(lay);
+//           dump << "\n BTLDetId hI = " << theNewId;
+//           if (theId.mtdSide() != theNewId.mtdSide()) {
+//             dump << "\n DIFFERENCE IN SIDE";
+//           }
+//           if (theId.mtdRR() != theNewId.mtdRR()) {
+//             dump << "\n DIFFERENCE IN ROD";
+//           }
+//           if (theId.module() != theNewId.module()) {
+//             dump << "\n DIFFERENCE IN MODULE";
+//           }
+//           if (theId.modType() != theNewId.modType()) {
+//             dump << "\n DIFFERENCE IN MODTYPE";
+//           }
+//           if (theId.crystal() != theNewId.crystal()) {
+//             dump << "\n DIFFERENCE IN CRYSTAL";
+//           }
+//           dump << "\n";
+//         } else {
+//           ETLDetId theId(etlNS_.getUnitID(thisN_));
+//           dump << theId;
+//         }
+//         dump << "\n";
+//       }
+//     }
+//     ++id;
+//     if (nVols != 0 && id > nVols)
+//       notReachedDepth = false;
+//   } while (fv.next() && notReachedDepth);
+//   dump << std::flush;
+//   dump.close();
+}
+
+// void DD4hep_TestMTDNumbering::theBaseNumber(const DDGeoHistory& gh) {
+//   thisN_.reset();
+//   thisN_.setSize(gh.size());
+
+//   for (uint i = gh.size(); i-- > 0;) {
+//     std::string name(gh[i].logicalPart().name().name());
+//     int copyN(gh[i].copyno());
+//     thisN_.addLevel(name, copyN);
+// #ifdef EDM_ML_DEBUG
+//     edm::LogInfo("DD4hep_TestMTDNumbering") << name << " " << copyN;
+// #endif
+//   }
+// }
+
+DEFINE_FWK_MODULE(DD4hep_TestMTDNumbering);
