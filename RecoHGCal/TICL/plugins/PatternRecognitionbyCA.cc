68b10533c3cee92e41ca679d4f4482e082e1db27
Marco Rovere <marco.rovere@cern.ch>
2019-11-05 11:26:08 +0100

    Add rudimentary PCA computation

--- RecoHGCal/TICL/plugins/PatternRecognitionbyCA.cc
+++ RecoHGCal/TICL/plugins/PatternRecognitionbyCA.cc
@@ -10,6 +10,8 @@
 #include "HGCGraph.h"
 #include "RecoLocalCalo/HGCalRecProducers/interface/ComputeClusterTime.h"
 
+#include "TPrincipal.h"
+
 using namespace ticl;
 
 PatternRecognitionbyCA::PatternRecognitionbyCA(const edm::ParameterSet &conf, const CacheBase *cache)
@@ -134,17 +136,47 @@ void PatternRecognitionbyCA::makeTracksters(const PatternRecognitionAlgoBase::In
     tracksterId++;
   }
 
+  TPrincipal pca(3, "");
+  std::cout << "-------" << std::endl;
   for (auto &trackster : result) {
+    pca.Clear();
     assert(trackster.vertices.size() <= trackster.vertex_multiplicity.size());
     for (size_t i = 0; i < trackster.vertices.size(); ++i) {
       trackster.vertex_multiplicity[i] = layer_cluster_usage[trackster.vertices[i]];
       LogDebug("HGCPatterRecoByCA") << "LayerID: " << trackster.vertices[i]
                                     << " count: " << (int)trackster.vertex_multiplicity[i] << std::endl;
+      double point[3] = {
+        input.layerClusters[trackster.vertices[i]].x(),
+        input.layerClusters[trackster.vertices[i]].y(),
+        input.layerClusters[trackster.vertices[i]].z()
+      };
+      pca.AddRow(&point[0]);
     }
+    pca.MakePrincipals();
+    const auto & mean = *(pca.GetMeanValues());
+    const auto & eigenvectors = *(pca.GetEigenVectors());
+    const auto & eigenvalues = *(pca.GetEigenValues());
+    const auto & sigmas = *(pca.GetSigmas());
+    std::cout << "Trackster characteristics: " << std::endl;
+    std::cout << "Size: " << trackster.vertices.size() << std::endl;
+    std::cout << "Mean: " << mean[0] << ", " << mean[1] << ", " << mean[2] << std::endl;
+    std::cout << "EigenValues: " << eigenvalues[0] << ", " << eigenvalues[1] << ", " << eigenvalues[2]  << std::endl;
+    std::cout << "EigeVectors 1: " << eigenvectors(0, 0) << ", " << eigenvectors(0, 1) << ", " << eigenvectors(0, 2) <<std::endl;
+    std::cout << "EigeVectors 2: " << eigenvectors(1, 0) << ", " << eigenvectors(1, 1) << ", " << eigenvectors(1, 2) <<std::endl;
+    std::cout << "EigeVectors 3: " << eigenvectors(2, 0) << ", " << eigenvectors(2, 1) << ", " << eigenvectors(2, 2) <<std::endl;
+    std::cout << "Sigmas: " << sigmas[0] << ", " << sigmas[1] << ", " << sigmas[2] << std::endl;
   }
 
   // run energy regression and ID
   energyRegressionAndID(input.layerClusters, result);
+  for (auto &trackster : result) {
+    std::cout << "Trackster characteristics: " << std::endl;
+    std::cout << "Size: " << trackster.vertices.size() << std::endl;
+    auto counter = 0;
+    for (auto const & p : trackster.id_probabilities) {
+      std::cout << counter++ << ": " << p << std::endl;
+    }
+  }
 }
 
 void PatternRecognitionbyCA::energyRegressionAndID(const std::vector<reco::CaloCluster> &layerClusters,
