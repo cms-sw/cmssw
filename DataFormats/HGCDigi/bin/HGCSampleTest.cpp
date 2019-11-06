f26d9b016e6d76709b3d6aae6fd2aad4fea4c9ca
Giovanni Franzoni <giovanni.franzoni@cern.ch>
2019-11-06 14:07:24 +0100

    KP comment:more modern C++ random number library

--- DataFormats/HGCDigi/bin/HGCSampleTest.cpp
+++ DataFormats/HGCDigi/bin/HGCSampleTest.cpp
@@ -2,6 +2,8 @@
 #include <iostream>
 #include <cassert>
 #include <string>
+#include <chrono>
+#include <random>
 
 // run for instance with:
 //
@@ -12,7 +14,7 @@
 // for a measureble amount of time taken
 
 int main(int argc, char** argv) {
-  std::cout << "Basic performance tests for HGCSample\n" << std::endl;
+  std::cout << "Basic performance tests for HGCSample (pseudo-random seed set according to local time)\n" << std::endl;
   std::cout << "num parameters entered: " << argc << std::endl;
 
   // first command line argument is the number of trials
@@ -28,23 +30,27 @@ int main(int argc, char** argv) {
   // init static values
   uint32_t adc = 124;
   uint32_t gain = 15;
-  uint32_t thrADC = 900;
+  const uint32_t thrADC = 900;
   bool thr = adc > thrADC;
   uint32_t toa = 0;
   bool mode = false;
   bool toaFired = true;
+
+  // http://www.cplusplus.com/reference/random/linear_congruential_engine/
+  unsigned seed1 = std::chrono::system_clock::now().time_since_epoch().count();
+  std::minstd_rand0 myrand(seed1);
 
   // do the trials: time/performance test and exploit randomisation to check
   unsigned long int u = 0;
   for (; u < repetitions; u++) {
     // randomise all inputs, if chosen at the command line
     if (generateRandomValues) {
-      adc = rand() % 4096;
-      toa = rand() % 1024;
-      gain = rand() % 16;
-      mode = rand() % 2;
-      thr = rand() % 2;
-      toaFired = rand() % 2;
+      adc = myrand() % 4096;
+      toa = myrand() % 1024;
+      gain = myrand() % 16;
+      mode = myrand() % 2;
+      thr = myrand() % 2;
+      toaFired = myrand() % 2;
     }
 
     HGCSample aSample;
@@ -60,8 +66,8 @@ int main(int argc, char** argv) {
     assert(thr == aSample.threshold());
     assert(toaFired == aSample.getToAValid());
 
-    std::cout << aSample.raw() << "\t" << thr << "\t" << mode << "\t" << toaFired << "\t" << gain << "\t" << toa << "\t"
-              << adc << std::endl;
+    // std::cout << aSample.raw() << "\t" << thr << "\t" << mode << "\t" << toaFired << "\t" << gain << "\t" << toa << "\t"
+    //          << adc << std::endl;
 
     HGCSample ASample;
     ASample.setThreshold(thr);
