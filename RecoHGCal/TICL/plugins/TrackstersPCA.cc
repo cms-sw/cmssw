a382951850ec14363851466c5f8c14096defc5a5
Marco Rovere <marco.rovere@cern.ch>
2019-11-08 00:23:49 +0100

    Fix Eigenvalues indices

--- RecoHGCal/TICL/plugins/TrackstersPCA.cc
+++ RecoHGCal/TICL/plugins/TrackstersPCA.cc
@@ -27,13 +27,13 @@ void ticl::assignPCAtoTracksters(std::vector<Trackster> & tracksters,
     trackster.eigenvalues[1] = (float)(*(pca.GetEigenValues()))[1];
     trackster.eigenvalues[2] = (float)(*(pca.GetEigenValues()))[2];
     trackster.eigenvectors[0] = ticl::Trackster::Vector((*(pca.GetEigenVectors()))[0][0],
-        (*(pca.GetEigenVectors()))[0][1],
-        (*(pca.GetEigenVectors()))[0][2] );
-    trackster.eigenvectors[1] = ticl::Trackster::Vector((*(pca.GetEigenVectors()))[1][0],
+        (*(pca.GetEigenVectors()))[1][0],
+        (*(pca.GetEigenVectors()))[2][0] );
+    trackster.eigenvectors[1] = ticl::Trackster::Vector((*(pca.GetEigenVectors()))[0][1],
         (*(pca.GetEigenVectors()))[1][1],
-        (*(pca.GetEigenVectors()))[1][2] );
-    trackster.eigenvectors[2] = ticl::Trackster::Vector((*(pca.GetEigenVectors()))[2][0],
-        (*(pca.GetEigenVectors()))[2][1],
+        (*(pca.GetEigenVectors()))[2][1] );
+    trackster.eigenvectors[2] = ticl::Trackster::Vector((*(pca.GetEigenVectors()))[0][2],
+        (*(pca.GetEigenVectors()))[1][2],
         (*(pca.GetEigenVectors()))[2][2] );
     trackster.sigmas[0] = (float)(*(pca.GetSigmas()))[0];
     trackster.sigmas[1] = (float)(*(pca.GetSigmas()))[1];
@@ -46,9 +46,9 @@ void ticl::assignPCAtoTracksters(std::vector<Trackster> & tracksters,
     std::cout << "Size: " << trackster.vertices.size() << std::endl;
     std::cout << "Mean: " << mean[0] << ", " << mean[1] << ", " << mean[2] << std::endl;
     std::cout << "EigenValues: " << eigenvalues[0] << ", " << eigenvalues[1] << ", " << eigenvalues[2]  << std::endl;
-    std::cout << "EigeVectors 1: " << eigenvectors(0, 0) << ", " << eigenvectors(0, 1) << ", " << eigenvectors(0, 2) <<std::endl;
-    std::cout << "EigeVectors 2: " << eigenvectors(1, 0) << ", " << eigenvectors(1, 1) << ", " << eigenvectors(1, 2) <<std::endl;
-    std::cout << "EigeVectors 3: " << eigenvectors(2, 0) << ", " << eigenvectors(2, 1) << ", " << eigenvectors(2, 2) <<std::endl;
+    std::cout << "EigeVectors 1: " << eigenvectors(0, 0) << ", " << eigenvectors(1, 0) << ", " << eigenvectors(2, 0) <<std::endl;
+    std::cout << "EigeVectors 2: " << eigenvectors(0, 1) << ", " << eigenvectors(1, 1) << ", " << eigenvectors(2, 1) <<std::endl;
+    std::cout << "EigeVectors 3: " << eigenvectors(0, 2) << ", " << eigenvectors(1, 2) << ", " << eigenvectors(2, 2) <<std::endl;
     std::cout << "Sigmas: " << sigmas[0] << ", " << sigmas[1] << ", " << sigmas[2] << std::endl;
   }
 }
