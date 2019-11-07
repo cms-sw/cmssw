598dd0443627068fd67da06fcc42b754403da144
Sven Dildick <sven.dildick@cern.ch>
2019-11-07 23:35:02 +0100

    New producers

---
+++ L1Trigger/L1TGEM/python/simMuonGEMPadDigiConvClusters_cfi.py
@@ -0,0 +1,4 @@
+import FWCore.ParameterSet.Config as cms
+
+from L1Trigger.L1TGEM.simMuonGEMPadDigiConvClustersDef_cfi import *
+simMuonGEMPadDigiConvClusters = simMuonGEMPadDigiConvClustersDef.clone()
