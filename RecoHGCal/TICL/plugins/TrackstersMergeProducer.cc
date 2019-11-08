1dc2fa433c6a0c1470175df32566dd878855a48a
Marco Rovere <marco.rovere@cern.ch>
2019-11-08 14:53:19 +0100

    Add tracksters from Merge step

--- RecoHGCal/TICL/plugins/TrackstersMergeProducer.cc
+++ RecoHGCal/TICL/plugins/TrackstersMergeProducer.cc
@@ -14,6 +14,8 @@
 #include "DataFormats/HGCalReco/interface/Trackster.h"
 #include "DataFormats/HGCalReco/interface/TICLSeedingRegion.h"
 
+#include "TrackstersPCA.h"
+
 using namespace ticl;
 
 class TrackstersMergeProducer : public edm::stream::EDProducer<> {
@@ -30,13 +32,15 @@ private:
   const edm::EDGetTokenT<std::vector<Trackster>> tracksterstrk_token_;
   const edm::EDGetTokenT<std::vector<Trackster>> trackstershad_token_;
   const edm::EDGetTokenT<std::vector<TICLSeedingRegion>> seedingTrk_token_;
+  const edm::EDGetTokenT<std::vector<reco::CaloCluster>> clusters_token_;
 };
 
 TrackstersMergeProducer::TrackstersMergeProducer(const edm::ParameterSet &ps) :
   trackstersem_token_(consumes<std::vector<Trackster>>(ps.getParameter<edm::InputTag>("trackstersem"))),
   tracksterstrk_token_(consumes<std::vector<Trackster>>(ps.getParameter<edm::InputTag>("tracksterstrk"))),
   trackstershad_token_(consumes<std::vector<Trackster>>(ps.getParameter<edm::InputTag>("trackstershad"))),
-  seedingTrk_token_(consumes<std::vector<TICLSeedingRegion>>(ps.getParameter<edm::InputTag>("seedingTrk"))) {
+  seedingTrk_token_(consumes<std::vector<TICLSeedingRegion>>(ps.getParameter<edm::InputTag>("seedingTrk"))),
+    clusters_token_(consumes<std::vector<reco::CaloCluster>>(ps.getParameter<edm::InputTag>("layer_clusters"))) {
   produces<std::vector<Trackster>>();
 }
 
@@ -59,22 +63,34 @@ void TrackstersMergeProducer::produce(edm::Event &evt, const edm::EventSetup &) 
   auto result = std::make_unique<std::vector<Trackster>>();
 
   TICLTracksterTiles tracksterTile;
+  std::vector<bool> usedTrackstersEM;
+  std::vector<bool> usedTrackstersTRK;
+  std::vector<bool> usedTrackstersHAD;
+  std::vector<bool> usedSeeds;
+
+  edm::Handle<std::vector<reco::CaloCluster>> cluster_h;
+  evt.getByToken(clusters_token_, cluster_h);
+  const auto& layerClusters = *cluster_h;
 
   edm::Handle<std::vector<Trackster>> trackstersem_h;
   evt.getByToken(trackstersem_token_, trackstersem_h);
   const auto &trackstersEM = *trackstersem_h;
+  usedTrackstersEM.resize(trackstersEM.size(), 0);
 
   edm::Handle<std::vector<Trackster>> tracksterstrk_h;
   evt.getByToken(tracksterstrk_token_, tracksterstrk_h);
   const auto &trackstersTRK = *tracksterstrk_h;
+  usedTrackstersTRK.resize(trackstersTRK.size(), 0);
 
   edm::Handle<std::vector<Trackster>> trackstershad_h;
   evt.getByToken(trackstershad_token_, trackstershad_h);
   const auto &trackstersHAD = *trackstershad_h;
+  usedTrackstersHAD.resize(trackstersHAD.size(), 0);
 
   edm::Handle<std::vector<TICLSeedingRegion>> seedingTrk_h;
   evt.getByToken(seedingTrk_token_, seedingTrk_h);
   const auto & seedingTrk = * seedingTrk_h;
+  usedSeeds.resize(seedingTrk.size(), 0);
 
   int tracksterIteration = 0;
   fillTile(tracksterTile, trackstersEM, tracksterIteration++);
@@ -86,6 +102,7 @@ void TrackstersMergeProducer::produce(edm::Event &evt, const edm::EventSetup &) 
     tracksterTile.fill(tracksterIteration, s.origin.eta(), s.origin.phi(), seedId++);
   }
 
+  int tracksterTRK_idx = 0;
   for (auto const & t : trackstersTRK) {
     int bin = tracksterTile[3].globalBin(t.barycenter.eta(), t.barycenter.phi());
       std::cout << "TrackstersMergeProducer Tracking obj: " << t.barycenter
@@ -94,24 +111,70 @@ void TrackstersMergeProducer::produce(edm::Event &evt, const edm::EventSetup &) 
         << std::endl;
     auto const & seeds = tracksterTile[3][bin];
     auto const & ems = tracksterTile[0][bin];
-    for (auto const & s : seeds) {
-      auto const & seed = seedingTrk[s];
-      std::cout << " linked to seed obj: " << seed.origin
-        << ", " << seed.directionAtOrigin.mag()
-        << " abs(alignemnt): " << std::abs(t.eigenvectors[0].Dot(seed.directionAtOrigin.unit()))
-        << std::endl;
+    auto const & original_seed = seedingTrk[t.seedIndex];
+    std::cout << "Original seed: " << original_seed.origin
+      << ", " << original_seed.directionAtOrigin.mag()
+      << " trk/seed: " << t.raw_energy/original_seed.directionAtOrigin.mag()
+      << " abs(alignemnt): " << std::abs(t.eigenvectors[0].Dot(original_seed.directionAtOrigin.unit()))
+      << std::endl;
+    if (t.raw_energy/original_seed.directionAtOrigin.mag() < 0.6) {
+      std::cout << "Trying to recover energy..." << std::endl;
+      auto tracksterEM_idx = 0;
+      for (auto const & e : ems) {
+        auto const & em = trackstersEM[e];
+        auto cos_angle = std::abs(t.eigenvectors[0].Dot(em.eigenvectors[0]));
+        if (cos_angle > 0.9945) {
+          usedTrackstersTRK[tracksterTRK_idx] = 1;
+          usedTrackstersEM[tracksterEM_idx] = 1;
+          auto combined = t;
+          std::copy(std::begin(em.vertices), std::end(em.vertices),
+              std::back_inserter(combined.vertices));
+          std::copy(std::begin(em.vertex_multiplicity), std::end(em.vertex_multiplicity),
+              std::back_inserter(combined.vertex_multiplicity));
+          std::cout << " linked to em obj: " << em.barycenter
+            << " abs(alignemnt): " << std::abs(t.eigenvectors[0].Dot(em.eigenvectors[0]))
+            << std::endl
+            << " regressed energy: " << em.regressed_energy
+            << " raw_energy: " << em.raw_energy
+            << " cumulative: " << (t.raw_energy+em.raw_energy)
+            << " (trk+em)/seed: " << (t.raw_energy+em.raw_energy)/original_seed.directionAtOrigin.mag()
+            << std::endl;
+          result->push_back(combined);
+        }
+        tracksterEM_idx++;
+      }
+    } else {
+      result->push_back(t);
+      usedTrackstersTRK[tracksterTRK_idx] = 1;
     }
-    for (auto const & e : ems) {
-      auto const & em = trackstersEM[e];
-      std::cout << " linked to em obj: " << em.barycenter
-        << " abs(alignemnt): " << std::abs(t.eigenvectors[0].Dot(em.eigenvectors[0]))
-        << " regressed energy: " << em.regressed_energy
-        << " raw_energy: " << em.raw_energy
-        << " cumulative: " << (t.raw_energy+em.raw_energy)
-        << std::endl;
-    }
+    tracksterTRK_idx++;
   }
 
+  tracksterTRK_idx = 0;
+  for (auto const & t : trackstersTRK) {
+    if (! usedTrackstersTRK[tracksterTRK_idx]) {
+      result->push_back(t);
+    }
+    tracksterTRK_idx++;
+  }
+
+  auto tracksterEM_idx = 0;
+  for (auto const & t : trackstersEM) {
+    if (! usedTrackstersEM[tracksterEM_idx]) {
+      result->push_back(t);
+    }
+    tracksterEM_idx++;
+  }
+
+  auto tracksterHAD_idx = 0;
+  for (auto const & t : trackstersHAD) {
+    if (! usedTrackstersHAD[tracksterHAD_idx]) {
+      result->push_back(t);
+    }
+    tracksterHAD_idx++;
+  }
+
+  assignPCAtoTracksters(*result, layerClusters);
 
   evt.put(std::move(result));
 }
@@ -122,6 +185,7 @@ void TrackstersMergeProducer::fillDescriptions(edm::ConfigurationDescriptions &d
   desc.add<edm::InputTag>("tracksterstrk", edm::InputTag("trackstersTrk"));
   desc.add<edm::InputTag>("trackstershad", edm::InputTag("trackstersHAD"));
   desc.add<edm::InputTag>("seedingTrk", edm::InputTag("ticlSeedingTrk"));
+  desc.add<edm::InputTag>("layer_clusters", edm::InputTag("hgcalLayerClusters"));
   descriptions.add("trackstersMergeProducer", desc);
 }
 
--- RecoHGCal/TICL/python/iterativeTICL_cff.py
+++ RecoHGCal/TICL/python/iterativeTICL_cff.py
@@ -7,12 +7,17 @@ from RecoHGCal.TICL.HADStep_cff import *
 from RecoHGCal.TICL.ticlLayerTileProducer_cfi import ticlLayerTileProducer
 from RecoHGCal.TICL.ticlCandidateFromTrackstersProducer_cfi import ticlCandidateFromTrackstersProducer
 from RecoHGCal.TICL.pfTICLProducer_cfi import pfTICLProducer
-from RecoHGCal.TICL.trackstersMergeProducer_cfi import trackstersMergeProducer
+from RecoHGCal.TICL.trackstersMergeProducer_cfi import trackstersMergeProducer as _trackstersMergeProducer
+from RecoHGCal.TICL.multiClustersFromTrackstersProducer_cfi import multiClustersFromTrackstersProducer as _multiClustersFromTrackstersProducer
 
 
 ticlLayerTileTask = cms.Task(ticlLayerTileProducer)
 ticlPFTask = cms.Task(ticlCandidateFromTrackstersProducer, pfTICLProducer)
-ticlTracksterMergeTask = cms.Task(trackstersMergeProducer)
+trackstersMerge = _trackstersMergeProducer.clone()
+multiClustersFromTrackstersMerge = _multiClustersFromTrackstersProducer.clone(
+    Tracksters = "trackstersMerge"
+)
+ticlTracksterMergeTask = cms.Task(trackstersMerge, multiClustersFromTrackstersMerge)
 
 iterTICLTask = cms.Task(ticlLayerTileTask
     ,MIPStepTask
