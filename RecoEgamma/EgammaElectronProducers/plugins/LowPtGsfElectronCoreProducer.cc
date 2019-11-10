0930308dbc90a315953bac99d7be3d27d84b3313
Jonas Rembser <jonas.rembser@cern.ch>
2019-11-10 18:00:49 +0100

    further modernization of electron core producers

--- RecoEgamma/EgammaElectronProducers/plugins/LowPtGsfElectronCoreProducer.cc
+++ RecoEgamma/EgammaElectronProducers/plugins/LowPtGsfElectronCoreProducer.cc
@@ -20,56 +20,52 @@ public:
 
 private:
   const edm::EDGetTokenT<reco::GsfPFRecTrackCollection> gsfPfRecTracksToken_;
-  const edm::EDGetTokenT<reco::GsfTrackCollection> gsfTracksToken_;
   const edm::EDGetTokenT<reco::TrackCollection> ctfTracksToken_;
   const edm::EDGetTokenT<edm::ValueMap<reco::SuperClusterRef> > superClusterRefs_;
+  const edm::EDPutTokenT<reco::GsfElectronCoreCollection> putToken_;
 };
 
+using reco::GsfPFRecTrackCollection;
+using reco::SuperClusterRef;
+
 LowPtGsfElectronCoreProducer::LowPtGsfElectronCoreProducer(const edm::ParameterSet& config)
-    : gsfPfRecTracksToken_(
-          mayConsume<reco::GsfPFRecTrackCollection>(config.getParameter<edm::InputTag>("gsfPfRecTracks"))),
-      gsfTracksToken_(consumes<reco::GsfTrackCollection>(config.getParameter<edm::InputTag>("gsfTracks"))),
+    : gsfPfRecTracksToken_(mayConsume<GsfPFRecTrackCollection>(config.getParameter<edm::InputTag>("gsfPfRecTracks"))),
       ctfTracksToken_(consumes<reco::TrackCollection>(config.getParameter<edm::InputTag>("ctfTracks"))),
-      superClusterRefs_(
-          consumes<edm::ValueMap<reco::SuperClusterRef> >(config.getParameter<edm::InputTag>("superClusters"))) {
-  produces<reco::GsfElectronCoreCollection>();
-}
+      superClusterRefs_(consumes<edm::ValueMap<SuperClusterRef> >(config.getParameter<edm::InputTag>("superClusters"))),
+      putToken_(produces<reco::GsfElectronCoreCollection>()) {}
 
 void LowPtGsfElectronCoreProducer::produce(edm::StreamID, edm::Event& event, const edm::EventSetup& setup) const {
   // Output collection
-  auto electrons = std::make_unique<reco::GsfElectronCoreCollection>();
+  reco::GsfElectronCoreCollection electrons;
 
   // Init
   auto gsfPfRecTracksHandle = event.getHandle(gsfPfRecTracksToken_);
-  auto gsfTracksHandle = event.getHandle(gsfTracksToken_);
   auto ctfTracksHandle = event.getHandle(ctfTracksToken_);
-  auto superClusterRefs = event.getHandle(superClusterRefs_);
+  auto const& superClusterRefs = event.get(superClusterRefs_);
 
   // Create ElectronCore objects
   for (size_t ipfgsf = 0; ipfgsf < gsfPfRecTracksHandle->size(); ++ipfgsf) {
     // Refs to GSF(PF) objects and SC
     reco::GsfPFRecTrackRef pfgsf(gsfPfRecTracksHandle, ipfgsf);
-    reco::GsfTrackRef gsf = pfgsf->gsfTrackRef();
-    const reco::SuperClusterRef sc = (*superClusterRefs)[pfgsf];
 
     // Use GsfElectronCore(gsf) constructor and store object via emplace
-    electrons->emplace_back(gsf);
+    electrons.emplace_back(pfgsf->gsfTrackRef());
 
     // Do not consider ECAL-driven objects
-    if (electrons->back().ecalDrivenSeed()) {
-      electrons->pop_back();
+    if (electrons.back().ecalDrivenSeed()) {
+      electrons.pop_back();
       continue;
     }
 
     // Add GSF(PF) track information
-    auto ctfpair = egamma::getClosestCtfToGsf(electrons->back().gsfTrack(), ctfTracksHandle);
-    electrons->back().setCtfTrack(ctfpair.first, ctfpair.second);
+    auto ctfpair = egamma::getClosestCtfToGsf(electrons.back().gsfTrack(), ctfTracksHandle);
+    electrons.back().setCtfTrack(ctfpair.first, ctfpair.second);
 
     // Add super cluster information
-    electrons->back().setSuperCluster(sc);
+    electrons.back().setSuperCluster(superClusterRefs[pfgsf]);
   }
 
-  event.put(std::move(electrons));
+  event.emplace(putToken_, electrons);
 }
 
 //////////////////////////////////////////////////////////////////////////////////////////
@@ -77,7 +73,6 @@ void LowPtGsfElectronCoreProducer::produce(edm::StreamID, edm::Event& event, con
 void LowPtGsfElectronCoreProducer::fillDescriptions(edm::ConfigurationDescriptions& descriptions) {
   edm::ParameterSetDescription desc;
   desc.add<edm::InputTag>("gsfPfRecTracks", {"lowPtGsfElePfGsfTracks"});
-  desc.add<edm::InputTag>("gsfTracks", {"lowPtGsfEleGsfTracks"});
   desc.add<edm::InputTag>("ctfTracks", {"generalTracks"});
   desc.add<edm::InputTag>("superClusters", edm::InputTag("lowPtGsfElectronSuperClusters"));
   descriptions.add("lowPtGsfElectronCores", desc);
