47a510b88def06dc6d7be3ee41e27ff3177fe69c
Jonas Rembser <jonas.rembser@cern.ch>
2019-11-12 15:57:40 +0100

    removing old pre-GED GsfElectronProducers

--- RecoEgamma/EgammaElectronProducers/python/gsfElectronSequence_cff.py
+++ RecoEgamma/EgammaElectronProducers/python/gsfElectronSequence_cff.py
@@ -7,31 +7,14 @@ import FWCore.ParameterSet.Config as cms
 #==============================================================================
 
 from RecoEgamma.EgammaElectronProducers.ecalDrivenGsfElectronCores_cfi import ecalDrivenGsfElectronCores
-from RecoEgamma.EgammaElectronProducers.gsfElectronCores_cfi import gsfElectronCores
 from RecoEgamma.EgammaElectronProducers.ecalDrivenGsfElectronCoresFromMultiCl_cff import ecalDrivenGsfElectronCoresFromMultiCl
 from RecoEgamma.EgammaElectronProducers.gsfElectrons_cfi import *
-gsfElectronTask = cms.Task(ecalDrivenGsfElectronCores,ecalDrivenGsfElectrons,gsfElectronCores,gsfElectrons)
-gsfElectronSequence = cms.Sequence(gsfElectronTask)
 
 gsfEcalDrivenElectronTask = cms.Task(ecalDrivenGsfElectronCores,ecalDrivenGsfElectrons)
 gsfEcalDrivenElectronSequence = cms.Sequence(gsfEcalDrivenElectronTask)
 
 _gsfEcalDrivenElectronTaskFromMultiCl = gsfEcalDrivenElectronTask.copy()
 _gsfEcalDrivenElectronTaskFromMultiCl.add(cms.Task(ecalDrivenGsfElectronCoresFromMultiCl,ecalDrivenGsfElectronsFromMultiCl))
-_gsfEcalDrivenElectronSequenceFromMultiCl = cms.Sequence(_gsfEcalDrivenElectronTaskFromMultiCl)
-
-
-from RecoEgamma.EgammaElectronProducers.edBasedElectronIso_cff import *
-from RecoEgamma.EgammaElectronProducers.pfBasedElectronIso_cff import *
-
-electronIsoTask = cms.Task(
-        edBasedElectronIsoTask,
-        pfBasedElectronIsoTask
-     )
-electronIsoSequence = cms.Sequence(electronIsoTask)
-
-gsfElectronMergingTask = cms.Task(electronIsoTask,gsfElectronCores,gsfElectrons)
-gsfElectronMergingSequence = cms.Sequence(gsfElectronMergingTask)
 
 from Configuration.Eras.Modifier_phase2_hgcal_cff import phase2_hgcal
 phase2_hgcal.toReplaceWith(
