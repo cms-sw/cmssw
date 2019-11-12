e2225aa576ef50248fc7e4a9ae636ac135a0a168
Sunanda <sunanda.banerjee@cern.ch>
2019-11-12 16:05:15 +0100

    Attempt to make HcalSimulationParameters to be read in for dd4hep

--- Geometry/HcalCommonData/interface/HcalSimParametersFromDD.h
+++ Geometry/HcalCommonData/interface/HcalSimParametersFromDD.h
@@ -2,23 +2,29 @@
 #define HcalCommonData_HcalSimParametersFromDD_h
 
 #include "DetectorDescription/Core/interface/DDsvalues.h"
+#include "DetectorDescription/Core/interface/DDCompactView.h"
+#include "DetectorDescription/Core/interface/DDFilteredView.h"
+#include "DetectorDescription/DDCMS/interface/DDCompactView.h"
+#include "DetectorDescription/DDCMS/interface/DDFilteredView.h"
 #include <string>
 
-class DDCompactView;
-class DDFilteredView;
 class HcalSimulationParameters;
 
 class HcalSimParametersFromDD {
 public:
-  HcalSimParametersFromDD() {}
-  virtual ~HcalSimParametersFromDD() {}
+  HcalSimParametersFromDD() = default;
 
   bool build(const DDCompactView*, HcalSimulationParameters&);
+  bool build(const cms::DDCompactView*, HcalSimulationParameters&);
 
 private:
+  bool buildParameters(const HcalSimulationParameters&);
   void fillNameVector(const DDCompactView*, const std::string&, const std::string&, std::vector<std::string>&);
+  void fillNameVector(const cms::DDCompactView*, const std::string&, std::vector<std::string>&);
+  void fillPMTs(const std::vector<double>&, bool, HcalSimulationParameters&);
   bool isItHF(const std::string&, const HcalSimulationParameters&);
   std::vector<std::string> getNames(DDFilteredView& fv);
+  std::vector<std::string> getNames(cms::DDFilteredView& fv);
   std::vector<double> getDDDArray(const std::string& str, const DDsvalues_type& sv, int& nmin);
 };
 
