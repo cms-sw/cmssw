8f454349d936c2c91d5b1273fc5784fd0ac0a54b
Sunanda <sunanda.banerjee@cern.ch>
2019-11-13 18:38:40 +0100

    Fix the test beam runs in view of present changes

---
+++ Geometry/HcalTestBeamData/python/hcalDDDSimConstants_cff.py
@@ -0,0 +1,6 @@
+import FWCore.ParameterSet.Config as cms
+
+from Geometry.HcalCommonData.hcalParameters_cfi             import *
+from Geometry.HcalCommonData.hcalDDDSimConstants_cfi        import *
+from Geometry.HcalCommonData.hcalDDDSimulationConstants_cfi import *
+from Geometry.HcalCommonData.caloSimulationParameters_cff   import *
