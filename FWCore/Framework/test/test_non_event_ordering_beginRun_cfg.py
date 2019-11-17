994da9ad1c95e1b8e514dfc1577e7c0220427697
Chris Jones <chrisdjones15@gmail.com>
2019-11-16 22:44:08 -0600

    Check expected value in test

--- FWCore/Framework/test/test_non_event_ordering_beginRun_cfg.py
+++ FWCore/Framework/test/test_non_event_ordering_beginRun_cfg.py
@@ -7,7 +7,11 @@ process.source = cms.Source("EmptySource")
 process.maxEvents.input = 3
 
 process.a = cms.EDProducer("NonEventIntProducer", ivalue = cms.int32(1))
-process.b = cms.EDProducer("NonEventIntProducer", ivalue = cms.int32(2), consumesBeginRun = cms.InputTag("c","beginRun") )
-process.c = cms.EDProducer("NonEventIntProducer", ivalue = cms.int32(3), consumesBeginRun = cms.InputTag("a", "beginRun"))
+process.b = cms.EDProducer("NonEventIntProducer", ivalue = cms.int32(2),
+                           consumesBeginRun = cms.InputTag("c","beginRun"),
+                           expectBeginRun = cms.untracked.int32(3) )
+process.c = cms.EDProducer("NonEventIntProducer", ivalue = cms.int32(3),
+                           consumesBeginRun = cms.InputTag("a", "beginRun"),
+                           expectBeginRun = cms.untracked.int32(1))
 
 process.schedule = cms.Schedule(tasks=cms.Task(process.a,process.b,process.c))
