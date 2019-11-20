a6fe32a5a5674ba3e1875c02b32bd3222d5d7eb0
jeongsumin <yami4515@naver.com>
2019-11-20 15:54:40 +0900

    Removing the RPCEfficiency area which are not used

--- DQM/RPCMonitorClient/python/RPCEfficiencyPerRingLayer_cfi.py
+++
@@ -1,8 +0,0 @@
-import FWCore.ParameterSet.Config as cms
-from DQMServices.Core.DQMEDHarvester import DQMEDHarvester
-
-rpcEfficiencyPerRingLayer = DQMEDHarvester("RPCEfficiencyPerRingLayer",
-                                           GlobalFolder = cms.untracked.string('RPC/RPCEfficiency/'),
-                                           NumberOfEndcapDisks  = cms.untracked.int32(4),
-                                           NumberOfInnermostEndcapRings  = cms.untracked.int32(2)
-                                           )
--- DQM/RPCMonitorClient/python/RPCTier0Client_cff.py
+++ DQM/RPCMonitorClient/python/RPCTier0Client_cff.py
@@ -13,10 +13,6 @@ from DQM.RPCMonitorClient.RPCRecHitProbabilityClient_cfi import *
 from  DQM.RPCMonitorClient.RPCChamberQuality_cfi import *
 
                                    
-from  DQM.RPCMonitorClient.RPCEfficiencySecondStep_cfi import *
-
-from  DQM.RPCMonitorClient.RPCEfficiencyShiftHisto_cfi import *
-from  DQM.RPCMonitorClient.RPCEfficiencyPerRingLayer_cfi import *
 
 
 qTesterRPC = cms.EDAnalyzer("QualityTester",
@@ -29,7 +25,7 @@ qTesterRPC = cms.EDAnalyzer("QualityTester",
 from DQM.RPCMonitorClient.RPCDcsInfoClient_cfi import *
 
 if (muonFlag):
-    rpcTier0Client = cms.Sequence(qTesterRPC*rpcdqmclient*rpcdqmMuonclient*rpcrechitprobabilityclient*rpcChamberQuality*rpcMuonChamberQuality*rpcDcsInfoClient*rpcefficiencysecond*rpcEventSummary*rpcEfficiencyShiftHisto*rpcEfficiencyPerRingLayer)
+    rpcTier0Client = cms.Sequence(qTesterRPC*rpcdqmclient*rpcdqmMuonclient*rpcrechitprobabilityclient*rpcChamberQuality*rpcMuonChamberQuality*rpcDcsInfoClient*rpcEventSummary)
 else:
-    rpcTier0Client = cms.Sequence(qTesterRPC*rpcdqmclient*rpcChamberQuality*rpcDcsInfoClient*rpcefficiencysecond*rpcEventSummary*rpcEfficiencyShiftHisto*rpcEfficiencyPerRingLayer)
+    rpcTier0Client = cms.Sequence(qTesterRPC*rpcdqmclient*rpcChamberQuality*rpcDcsInfoClient*rpcEventSummary)
 
