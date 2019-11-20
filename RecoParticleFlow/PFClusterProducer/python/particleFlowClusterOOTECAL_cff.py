857cfe181f59be0d50acbd52cbe0bff7d6866da9
Shilpi <shilpi.jain@cern.ch>
2019-11-20 01:54:22 +0100

    maximum # characters in a line

--- RecoParticleFlow/PFClusterProducer/python/particleFlowClusterOOTECAL_cff.py
+++ RecoParticleFlow/PFClusterProducer/python/particleFlowClusterOOTECAL_cff.py
@@ -5,7 +5,9 @@ particleFlowClusterOOTECAL = particleFlowClusterECAL.clone()
 particleFlowClusterOOTECAL.inputECAL = cms.InputTag("particleFlowClusterOOTECALUncorrected")
 
 from Configuration.Eras.Modifier_run2_miniAOD_80XLegacy_cff import run2_miniAOD_80XLegacy
-run2_miniAOD_80XLegacy.toModify(
-    particleFlowClusterOOTECAL,
-    energyCorrector = dict(applyMVACorrections = True, maxPtForMVAEvaluation = 90., srfAwareCorrection = False, recHitsEBLabel = "reducedEcalRecHitsEB", recHitsEELabel = "reducedEcalRecHitsEE")
-)
+run2_miniAOD_80XLegacy.toModify(particleFlowClusterOOTECAL,
+                                energyCorrector = dict( 
+                                    maxPtForMVAEvaluation = 90., srfAwareCorrection = False, 
+                                    recHitsEBLabel = "reducedEcalRecHitsEB", recHitsEELabel = "reducedEcalRecHitsEE"
+                                )
+                            )
