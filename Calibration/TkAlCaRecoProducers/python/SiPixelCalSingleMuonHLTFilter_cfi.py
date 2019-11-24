144c1430c1265759f31241280d8d12a8d6424f89
Tamas <vami.tamas@wigner.mta.hu>
2019-11-25 00:10:06 +0100

    Removing unused files

--- Calibration/TkAlCaRecoProducers/python/SiPixelCalSingleMuonHLTFilter_cfi.py
+++
@@ -1,6 +0,0 @@
-import HLTrigger.HLTfilters.hltHighLevel_cfi
-
-SiPixelCalSingleMuonHLTFilter = HLTrigger.HLTfilters.hltHighLevel_cfi.hltHighLevel.clone(
-    HLTPaths = ['HLT_*'],
-    throw = False #dont throw except on unknown path name 
-)
