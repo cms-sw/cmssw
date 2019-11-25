2ab5f00009c8ac74bff2c79c7bb4e5e99383a585
Andrea Bellora <andrea.bellora@cern.ch>
2019-11-25 16:42:35 +0100

    Added efficiency plots and online/offline flags

--- DQM/CTPPS/python/ctppsPixelDQMSource_cfi.py
+++ DQM/CTPPS/python/ctppsPixelDQMSource_cfi.py
@@ -7,5 +7,7 @@ ctppsPixelDQMSource = DQMEDAnalyzer('CTPPSPixelDQMSource',
     tagRPixCluster = cms.InputTag("ctppsPixelClusters", ""),  
     tagRPixLTrack = cms.InputTag("ctppsPixelLocalTracks", ""),  
     RPStatusWord = cms.untracked.uint32(0x8008), # rpots in readout:220_fr_hr; 210_fr_hr
-    verbosity = cms.untracked.uint32(0)
+    verbosity = cms.untracked.uint32(0),
+    offlinePlots = cms.untracked.bool(True),
+    onlinePlots = cms.untracked.bool(False)
 )
