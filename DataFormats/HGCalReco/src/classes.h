ad4ba27dd6cd7a7d1c30cbfaa3972b7174efc26d
maria <dalfonso@cern.ch>
2019-11-25 18:16:53 +0100

    modification to TICL unnecessary for cluster2D

--- DataFormats/HGCalReco/src/classes.h
+++ DataFormats/HGCalReco/src/classes.h
@@ -4,5 +4,4 @@
 #include "DataFormats/HGCalReco/interface/TICLLayerTile.h"
 #include "DataFormats/HGCalReco/interface/TICLSeedingRegion.h"
 #include "DataFormats/HGCalReco/interface/TICLCandidate.h"
-#include "DataFormats/HGCalReco/interface/TICLHFNoseLayerTile.h"
 #include "DataFormats/Common/interface/Wrapper.h"
