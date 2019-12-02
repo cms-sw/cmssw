cb2084ed7d0e79f1baafae5c1bb4452c78bb6e4d
Christopher Jones <cdj@fnal.gov>
2019-12-02 23:38:49 +0100

    Adjust to new function APIs
    moduleName and parameterSet now require a edm::ProcessHistory.

--- PhysicsTools/JetMCAlgos/plugins/HadronAndPartonSelector.cc
+++ PhysicsTools/JetMCAlgos/plugins/HadronAndPartonSelector.cc
@@ -141,7 +141,7 @@ void HadronAndPartonSelector::produce(edm::Event& iEvent, const edm::EventSetup&
     std::string moduleName = "";
     if (genEvtInfoProduct.isValid()) {
       const edm::Provenance& prov = iEvent.getProvenance(genEvtInfoProduct.id());
-      moduleName = edm::moduleName(prov);
+      moduleName = edm::moduleName(prov, iEvent.processHistory());
     }
 
     if (moduleName.find("Pythia6") != std::string::npos)
