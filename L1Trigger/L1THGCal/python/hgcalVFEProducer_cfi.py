60117d99bc67092ab4311fbaec7d35d34ae0631f
Jean-Baptiste Sauvan <jean-baptiste.sauvan@cern.ch>
2019-12-02 10:03:45 +0100

    Add OOT correction

--- L1Trigger/L1THGCal/python/hgcalVFEProducer_cfi.py
+++ L1Trigger/L1THGCal/python/hgcalVFEProducer_cfi.py
@@ -59,6 +59,7 @@ vfe_proc = cms.PSet( ProcessorName = cms.string('HGCalVFEProcessorSums'),
                      adcnBits_sc = adcNbits_sc,
                      tdcsaturation_sc = tdcSaturation_sc,
                      linnBits = cms.uint32(16),
+                     oot_coefficients = cms.vdouble(0., 0.), # (bx-2, bx-1)
                      siliconCellLSB_fC =  cms.double( triggerCellLsbBeforeCompression*(2**triggerCellTruncationBits) ),
                      scintillatorCellLSB_MIP = cms.double(float(adcSaturation_sc.value())/(2**float(adcNbits_sc.value()))),
                      noiseSilicon = cms.PSet(),
