ac57c2e3e4df20c076d70a4bf6fbc844da8752ac
Chris West <christopher.alan.west@gmail.com>
2019-12-06 19:39:32 +0100

    Add new key run2_mc_pre_vfp for 2016 pre-VFP change era

--- Configuration/AlCa/python/autoCond.py
+++ Configuration/AlCa/python/autoCond.py
@@ -15,7 +15,9 @@ autoCond = {
     'run2_mc_l1stage1'  :   '110X_mcRun2_asymptotic_l1stage1_v2',
     # GlobalTag for MC production with perfectly aligned and calibrated detector for Run2
     'run2_design'       :   '110X_mcRun2_design_v3',
-    #GlobalTag for MC production with optimistic alignment and calibrations for Run2
+    #GlobalTag for MC production with optimistic alignment and calibrations for 2016, prior to VFP change
+    'run2_mc_pre_vfp'           :   '110X_mcRun2_asymptotic_preVFP_v1',
+    #GlobalTag for MC production with optimistic alignment and calibrations for 2016, after VFP change
     'run2_mc'           :   '110X_mcRun2_asymptotic_v5',
     # GlobalTag for MC production (cosmics) with starup-like alignment and calibrations for Run2, Strip tracker in peak mode
     'run2_mc_cosmics'   :   '110X_mcRun2cosmics_startup_deco_v4',
