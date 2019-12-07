1f5696608eec5594e1a8df5aef81b94c95852bfd
Matti Kortelainen <matti.kortelainen@cern.ch>
2019-12-07 03:14:47 +0100

    Take parameter via string_view as well
    To avoid creating temporary strings from const char* arguments.

--- FWCore/Utilities/src/stemFromPath.cc
+++ FWCore/Utilities/src/stemFromPath.cc
@@ -1,11 +1,11 @@
 #include "FWCore/Utilities/interface/stemFromPath.h"
 
 namespace edm {
-  std::string_view stemFromPath(const std::string& path) {
+  std::string_view stemFromPath(std::string_view path) {
     auto begin = path.rfind("/");
-    if (begin == std::string::npos) {
+    if (begin == std::string_view::npos) {
       begin = path.rfind(":");
-      if (begin == std::string::npos) {
+      if (begin == std::string_view::npos) {
         // shouldn't really happen?
         begin = 0;
       } else {
@@ -15,6 +15,6 @@ namespace edm {
       begin += 1;
     }
     auto end = path.find(".", begin);
-    return std::string_view(path).substr(begin, end - begin);
+    return path.substr(begin, end - begin);
   }
 }  // namespace edm
