a823cd5aa8c69ce3c1e5cd8d31f1d91b49afcc22
mmusich <marco.musich@cern.ch>
2019-12-09 19:16:39 +0100

    add DCS filter for Tracker Patrition and define cuts for cosmics

--- Calibration/TkAlCaRecoProducers/python/ALCARECOSiPixelCalCosmics_cff.py
+++ Calibration/TkAlCaRecoProducers/python/ALCARECOSiPixelCalCosmics_cff.py
@@ -1,6 +1,18 @@
 import FWCore.ParameterSet.Config as cms
 import copy
 
+# DCS partitions
+# "EBp","EBm","EEp","EEm","HBHEa","HBHEb","HBHEc","HF","HO","RPC"
+# "DT0","DTp","DTm","CSCp","CSCm","CASTOR","TIBTID","TOB","TECp","TECm"
+# "BPIX","FPIX","ESp","ESm"
+import DPGAnalysis.Skims.skim_detstatus_cfi
+ALCARECOSiPixelCalCosmicsDCSFilter = DPGAnalysis.Skims.skim_detstatus_cfi.dcsstatus.clone(
+    DetectorType = cms.vstring('TIBTID','TOB','TECp','TECm','BPIX','FPIX'),
+    ApplyFilter  = cms.bool(True),
+    AndOr        = cms.bool(True),
+    DebugOn      = cms.untracked.bool(False)
+)
+
 from ALCARECOSiPixelCalSingleMuon_cff import ALCARECOSiPixelCalSingleMuonHLTFilter 
 ALCARECOSiPixelCalCosmicsHLTFilter = ALCARECOSiPixelCalSingleMuonHLTFilter.clone(
     HLTPaths = ["HLT_*"],
@@ -9,9 +21,21 @@ ALCARECOSiPixelCalCosmicsHLTFilter = ALCARECOSiPixelCalSingleMuonHLTFilter.clone
 
 from ALCARECOSiPixelCalSingleMuon_cff import ALCARECOSiPixelCalSingleMuon 
 ALCARECOSiPixelCalCosmics = ALCARECOSiPixelCalSingleMuon.clone(
-    ptMin = 0.0, #GeV
-    src = 'ctfWithMaterialTracksP5'
+    filter = True,
+    applyBasicCuts = True,
+    ptMin = 0.,
+    ptMax = 99999.,
+    pMin = 0.,
+    pMax = 99999.,
+    etaMin = -99., ##-2.4 keep also what is going through...
+    etaMax = 99., ## 2.4 ...both TEC with flat slope
+    chi2nMax = 999999.,
+    applyMultiplicityFilter = False,
+    applyNHighestPt = True, ## select only highest pT track
+    nHighestPt = 1,
+    src = 'ctfWithMaterialTracksP5',
 )
+ALCARECOSiPixelCalCosmics.minHitsPerSubDet.inPIXEL = 1
 
 # Sequence #
 seqALCARECOSiPixelCalCosmics = cms.Sequence(ALCARECOSiPixelCalCosmicsHLTFilter*ALCARECOSiPixelCalCosmics)
