4d4e59a6a61fab45d676f76f996f44f56261de16
Gabrielle Hugo <gabrielle.hugo83@gmail.com>
2019-12-10 17:55:11 +0100

    PixelGeomDetUnit was moved to Geometry/CommonDetUnit in recent change.

--- SimTracker/SiPhase2Digitizer/plugins/Pixel3DDigitizerAlgorithm.cc
+++ SimTracker/SiPhase2Digitizer/plugins/Pixel3DDigitizerAlgorithm.cc
@@ -38,7 +38,7 @@
 
 // Geometry
 #include "Geometry/Records/interface/TrackerDigiGeometryRecord.h"
-#include "Geometry/TrackerGeometryBuilder/interface/PixelGeomDetUnit.h"
+#include "Geometry/CommonDetUnit/interface/PixelGeomDetUnit.h"
 //#include "Geometry/TrackerGeometryBuilder/interface/TrackerGeometry.h"
 //#include "Geometry/CommonTopologies/interface/PixelTopology.h"
 //#include "Geometry/Records/interface/IdealGeometryRecord.h"
