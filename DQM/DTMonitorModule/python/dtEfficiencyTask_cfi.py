b78bd098b3736985bfa0a42226993f59715cd6cc
Javier <Javier.Fernandez@cern.ch>
2019-12-10 19:07:42 +0100

    Move stuff to new class

--- DQM/DTMonitorModule/python/dtEfficiencyTask_cfi.py
+++ DQM/DTMonitorModule/python/dtEfficiencyTask_cfi.py
@@ -1,19 +1,14 @@
 import FWCore.ParameterSet.Config as cms
 
 from DQMServices.Core.DQMEDAnalyzer import DQMEDAnalyzer
-dtOccupancyMonitor = DQMEDAnalyzer('DTEfficiencyTask',
+dtEfficiencyMonitor = DQMEDAnalyzer('DTEfficiencyTask',
     # switch for verbosity
     debug = cms.untracked.bool(False),
-    # label for dtDigis
-    digiLabel = cms.string('muonDTDigis'),
     # labels of 4D and 1D hits
     recHits4DLabel = cms.string('dt4DSegments'),
     recHitLabel = cms.string('dt1DRecHits'),
     # interval of lumi block after which we reset the histos
-    ResetCycle = cms.untracked.int32(10000),
-    # do all Layer vy Layer  plots or just summary ones
-    doDetailedPlots = cms.untracked.bool(False)
+    ResetCycle = cms.untracked.int32(10000)
 )
-
 
 
