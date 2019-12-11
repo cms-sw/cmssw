aa5f533ce85aa5a133759c389d77ce6ac54b4530
Marcel Andre Schneider <marcel.andre.schneider@cern.ch>
2019-12-11 18:42:03 +0100

    Add some Harvesting tests.
    No Harvesters yet.

---
+++ DQMServices/Demo/test/run_harvesters_cfg.py
@@ -0,0 +1,62 @@
+import FWCore.ParameterSet.Config as cms
+
+import FWCore.ParameterSet.VarParsing as VarParsing
+parser = VarParsing.VarParsing('python')
+one = VarParsing.VarParsing.multiplicity.singleton
+bool = VarParsing.VarParsing.varType.bool
+string = VarParsing.VarParsing.varType.string
+parser.register('nolegacy',             False, one, bool, "Don't run legacy harvesters")
+parser.register('nomodules',             False, one, bool, "Don't run any harvesters")
+parser.register('legacyoutput',         False, one, bool, "Use DQMFileSaver for output instead of DQMIO.")
+parser.register('protobufinput',         False, one, bool, "Use DQMProtobufReader for input instead of DQMIO.")
+parser.register('outfile',              "dqm.root", one, string, "Output file name.")
+parser.parseArguments()
+args = parser
+
+process = cms.Process("HARVESTING")
+process.add_(cms.Service("DQMStore"))
+
+print args.inputFiles
+
+if args.protobufinput:
+  infile = args.inputFiles[0]
+  runnr = int(infile[-6:])
+  indir = "/".join(infile.split("/")[:-1])
+  process.source = cms.Source("DQMStreamerReader",
+    SelectEvents = cms.untracked.vstring("*"),
+    runNumber = cms.untracked.uint32(runnr),
+    runInputDir = cms.untracked.string(indir),
+    streamLabel = cms.untracked.string("streamDQMHistograms"),
+    scanOnce = cms.untracked.bool(True),
+    datafnPosition = cms.untracked.uint32(4),
+    minEventsPerLumi = cms.untracked.int32(1),
+    delayMillis = cms.untracked.uint32(500),
+    nextLumiTimeoutMillis = cms.untracked.int32(-1),
+    skipFirstLumis = cms.untracked.bool(False),
+    deleteDatFiles = cms.untracked.bool(False),
+    endOfRunKills  = cms.untracked.bool(False),
+  )
+
+else:
+  process.source = cms.Source("DQMRootSource",
+                              fileNames = cms.untracked.vstring(*["file://" + f for f in args.inputFiles]))
+
+# legacy output
+process.dqmSaver = cms.EDAnalyzer("DQMFileSaver",
+  convention = cms.untracked.string('Offline'),
+  fileFormat = cms.untracked.string('ROOT'),
+  producer = cms.untracked.string('DQM'),
+  workflow = cms.untracked.string('/Harvesting/DQMTests/DQMIO'),
+  dirName = cms.untracked.string('.'),
+  saveByRun = cms.untracked.int32(-1),
+  saveAtJobEnd = cms.untracked.bool(True),
+)
+
+# dqmio ouptut
+process.out = cms.OutputModule("DQMRootOutputModule",
+                               fileName = cms.untracked.string(args.outfile))
+if args.legacyoutput:
+  process.e = cms.EndPath(process.dqmSaver)
+else:
+  process.e = cms.EndPath(process.out)
+
--- DQMServices/Demo/test/runtests.sh
+++ DQMServices/Demo/test/runtests.sh
@@ -15,10 +15,10 @@ cmsRun run_analyzers_cfg.py outfile=nolegacy-mt.root numberEventsInRun=1000 numb
 cmsRun run_analyzers_cfg.py outfile=nolegacy-cl.root numberEventsInRun=1000 numberEventsInLuminosityBlock=200 nEvents=1000 nolegacy=True nThreads=10 nConcurrent=10
 
 # Validate 2 and 3: Dump DQMIO into plain text and compare
-dumpdqmio() { root2sqlite.py -o $1.sqlite $1 ; echo '.dump' | sqlite3 $1.sqlite > $1.sqldump ; rm $1.sqlite ; }
-dumpdqmio nolegacy.root
-dumpdqmio nolegacy-mt.root
-dumpdqmio nolegacy-cl.root
+dumproot() { root2sqlite.py -o $1.sqlite $1 ; echo '.dump' | sqlite3 $1.sqlite > $1.sqldump ; rm $1.sqlite ; }
+dumproot nolegacy.root
+dumproot nolegacy-mt.root
+dumproot nolegacy-cl.root
 
 # TODO: if out DQM was correct, this would succeed!
 cmp nolegacy.root.sqldump nolegacy-mt.root.sqldump || true
@@ -27,6 +27,7 @@ cmp nolegacy.root.sqldump nolegacy-cl.root.sqldump || true
 
 # 4. Try crossing a run boundary.
 cmsRun run_analyzers_cfg.py outfile=multirun.root numberEventsInRun=300 numberEventsInLuminosityBlock=100 nEvents=1200
+dqmiodumpmetadata.py multirun.root | grep -q '4 runs, 12 lumisections'
 
 # 5. Now, make some chopped up files to try harvesting.
 cmsRun run_analyzers_cfg.py outfile=part1.root numberEventsInRun=300 numberEventsInLuminosityBlock=100 nEvents=50               # 1st half of 1st lumi
@@ -34,8 +35,12 @@ cmsRun run_analyzers_cfg.py outfile=part2.root numberEventsInRun=300 numberEvent
 cmsRun run_analyzers_cfg.py outfile=part3.root numberEventsInRun=300 numberEventsInLuminosityBlock=100 nEvents=200 firstEvent=100 firstLuminosityBlock=2 # lumi 2 and 3
 cmsRun run_analyzers_cfg.py outfile=part4.root numberEventsInRun=300 numberEventsInLuminosityBlock=100 nEvents=900 firstRun=2   # 3 more runs
 
-# TODO: merge these and compare to multirun.root.
-# ATM, harvesting is tested in DQMSevices/FwkIO.
+cmsRun run_harvesters_cfg.py inputFiles=part1.root inputFiles=part2.root inputFiles=part3.root inputFiles=part4.root outfile=merged.root nomodules=True
+dqmiodumpmetadata.py merged.root | grep -q '4 runs, 12 lumisections'
+dumproot multirun.root
+dumproot merged.root
+# these are unlikely to ever fully argee, though the histograms should. They do not, for now.
+#cmp multirun.root.sqldump merged.root.sqldump
 
 # 6. A load test. 
 ( ulimit -v 4000000 # limit available virtual memory
@@ -45,12 +50,19 @@ cmsRun run_analyzers_cfg.py outfile=part4.root numberEventsInRun=300 numberEvent
 # 7. Try writing a TDirectory file. This is only safe for a single run for now.
 cmsRun run_analyzers_cfg.py numberEventsInRun=100 numberEventsInLuminosityBlock=20 nEvents=100 legacyoutput=True
 
-# TODO: harvest and compare to alltypes.root
+cmsRun run_harvesters_cfg.py inputFiles=alltypes.root nomodules=True legacyoutput=True
+
+dumproot DQM_V0001_R000000001__EmptySource__DQMTests__DQMIO.root
+dumproot DQM_V0001_R000000001__Harvesting__DQMTests__DQMIO.root
+# These disagree due to the werid handling of per-lumi MEs in the current DQMStore.
+cmp DQM_V0001_R000000001__EmptySource__DQMTests__DQMIO.root.sqldump DQM_V0001_R000000001__Harvesting__DQMTests__DQMIO.root.sqldump || true
 
 # 8. Try writing ProtoBuf files.
 cmsRun run_analyzers_cfg.py outfile=multirun.root numberEventsInRun=300 numberEventsInLuminosityBlock=100 nEvents=1200 protobufoutput=True
 
-# TODO: try reading them in again, like harvesting.
+# This does not work, something in the file format seems to be not right.
+cmsRun run_harvesters_cfg.py inputFiles=./run000001 outfile=pbdata.root nomodules=True protobufinput=True || true
+
 # TODO: maybe also try fastHadd.
 
 
