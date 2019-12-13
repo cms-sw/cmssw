555852a0393c45a4ff071012773f5307dfcd72b4
maria <dalfonso@cern.ch>
2019-12-13 13:31:27 +0100

    add reco::ceil

--- RecoLocalCalo/HGCalRecProducers/interface/HFNoseTilesConstants.h
+++ RecoLocalCalo/HGCalRecProducers/interface/HFNoseTilesConstants.h
@@ -1,7 +1,7 @@
 #ifndef __RecoLocalCalo_HGCalRecAlgos_HFNoseTilesConstants_h__
 #define __RecoLocalCalo_HGCalRecAlgos_HFNoseTilesConstants_h__
 
-#include "RecoLocalCalo/HGCalRecProducers/interface/HGCalTilesConstants.h"
+#include "DataFormats/Math/interface/constexpr_cmath.h"
 
 #include <cstdint>
 #include <array>
@@ -13,16 +13,16 @@ struct HFNoseTilesConstants {
   static constexpr float maxX = 110.f;
   static constexpr float minY = -110.f;
   static constexpr float maxY = 110.f;
-  static constexpr int nColumns = static_cast<int>(std::ceil((maxX - minX) / tileSize));
-  static constexpr int nRows = static_cast<int>(std::ceil((maxY - minY) / tileSize));
+  static constexpr int nColumns = reco::ceil((maxX - minX) / tileSize);
+  static constexpr int nRows = reco::ceil((maxY - minY) / tileSize);
   static constexpr float tileSizeEtaPhi = 0.15f;
   static constexpr float minEta = -4.2f;
   static constexpr float maxEta = 4.2f;
   //To properly construct search box for cells in phi=[-3.15,-3.] and [3.,3.15], cells in phi=[3.,3.15] are copied to the first bin and cells in phi=[-3.15,-3.] are copied to the last bin
   static constexpr float minPhi = -3.3f;
   static constexpr float maxPhi = 3.3f;
-  static constexpr int nColumnsEta = static_cast<int>(std::ceil((maxEta - minEta) / tileSizeEtaPhi));
-  static constexpr int nRowsPhi = static_cast<int>(std::ceil((maxPhi - minPhi) / tileSizeEtaPhi));
+  static constexpr int nColumnsEta = reco::ceil((maxEta - minEta) / tileSizeEtaPhi);
+  static constexpr int nRowsPhi = reco::ceil((maxPhi - minPhi) / tileSizeEtaPhi);
   static constexpr int nTiles = nColumns * nRows + nColumnsEta * nRowsPhi;
 };
 
--- RecoLocalCalo/HGCalRecProducers/interface/HGCalTilesConstants.h
+++ RecoLocalCalo/HGCalRecProducers/interface/HGCalTilesConstants.h
@@ -4,6 +4,7 @@
 #ifndef __RecoLocalCalo_HGCalRecAlgos_HGCalTilesConstants_h__
 #define __RecoLocalCalo_HGCalRecAlgos_HGCalTilesConstants_h__
 
+#include "DataFormats/Math/interface/constexpr_cmath.h"
 #include <cstdint>
 #include <array>
 
@@ -13,16 +14,16 @@ struct HGCalTilesConstants {
   static constexpr float maxX = 285.f;
   static constexpr float minY = -285.f;
   static constexpr float maxY = 285.f;
-  static constexpr int nColumns = static_cast<int>(std::ceil((maxX - minX) / tileSize));
-  static constexpr int nRows = static_cast<int>(std::ceil((maxY - minY) / tileSize));
+  static constexpr int nColumns = reco::ceil((maxX - minX) / tileSize);
+  static constexpr int nRows = reco::ceil((maxY - minY) / tileSize);
   static constexpr float tileSizeEtaPhi = 0.15f;
   static constexpr float minEta = -3.f;
   static constexpr float maxEta = 3.f;
   //To properly construct search box for cells in phi=[-3.15,-3.] and [3.,3.15], cells in phi=[3.,3.15] are copied to the first bin and cells in phi=[-3.15,-3.] are copied to the last bin
   static constexpr float minPhi = -3.3f;
   static constexpr float maxPhi = 3.3f;
-  static constexpr int nColumnsEta = static_cast<int>(std::ceil((maxEta - minEta) / tileSizeEtaPhi));
-  static constexpr int nRowsPhi = static_cast<int>(std::ceil((maxPhi - minPhi) / tileSizeEtaPhi));
+  static constexpr int nColumnsEta = reco::ceil((maxEta - minEta) / tileSizeEtaPhi);
+  static constexpr int nRowsPhi = reco::ceil((maxPhi - minPhi) / tileSizeEtaPhi);
   static constexpr int nTiles = nColumns * nRows + nColumnsEta * nRowsPhi;
 };
 
