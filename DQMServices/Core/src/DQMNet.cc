fae7feb49e6548c10466b5d33d535ca70aecb78c
Marcel Andre Schneider <marcel.andre.schneider@cern.ch>
2019-12-13 19:24:45 +0100

    Clang says these are unused.
    They also don't seem very important.

--- DQMServices/Core/src/DQMNet.cc
+++ DQMServices/Core/src/DQMNet.cc
@@ -38,15 +38,10 @@ static const Regexp s_rxmeval("<(.*)>(i|f|s|qr)=(.*)</\\1>");
 // TODO: Can't include the header file since that leads to ambiguities.
 namespace dqm {
   namespace qstatus {
-    static const int OTHER = 30;        //< Anything but 'ok','warning' or 'error'.
-    static const int DISABLED = 50;     //< Test has been disabled.
-    static const int INVALID = 60;      //< Problem preventing test from running.
-    static const int INSUF_STAT = 70;   //< Insufficient statistics.
-    static const int DID_NOT_RUN = 90;  //< Algorithm did not run.
-    static const int STATUS_OK = 100;   //< Test was succesful.
-    static const int WARNING = 200;     //< Test had some problems.
-    static const int ERROR = 300;       //< Test has failed.
-  }                                     // namespace qstatus
+    static const int STATUS_OK = 100;  //< Test was succesful.
+    static const int WARNING = 200;    //< Test had some problems.
+    static const int ERROR = 300;      //< Test has failed.
+  }                                    // namespace qstatus
 }  // namespace dqm
 
 //////////////////////////////////////////////////////////////////////
