b325639023a77c3648c7e7c3ad48bbf7ef18f309
maria <dalfonso@cern.ch>
2019-12-13 20:56:20 +0100

    fix header

--- DataFormats/Math/interface/constexpr_cmath.h
+++ DataFormats/Math/interface/constexpr_cmath.h
@@ -1,6 +1,8 @@
 #ifndef DataFormats_Math_constexpr_cmath_h
 #define DataFormats_Math_constexpr_cmath_h
 
+#include <cstdint>
+
 namespace reco {
   constexpr int32_t ceil(float num) {
     return (static_cast<float>(static_cast<int32_t>(num)) == num) ? static_cast<int32_t>(num)
