8d4163972a2e08aff042d45036d68e21b41ab2b0
Matti Kortelainen <matti.kortelainen@cern.ch>
2019-12-30 16:47:56 +0100

    Reset prefetchRequested and WaitingTaskList only if reset does not originate from early delete
    Add tests for consuming (but not reading) a product after early
    deletion. The tests show that resetting these members also for
    early-delete-resets leads to another round of prefetching. In case the
    producer is in a Task (i.e. UnscheduledProductResolver is used for the
    product), an exception gets thrown even if the consumer does not read
    the product.
    
    Also update class name name in the early deletion exception message.

--- FWCore/Framework/test/test_deleteEarly.sh
+++ FWCore/Framework/test/test_deleteEarly.sh
@@ -9,6 +9,8 @@ F3=${LOCAL_TEST_DIR}/test_readAfterEarlyDelete_fail_cfg.py
 F4=${LOCAL_TEST_DIR}/test_multiPathEarlyDelete_cfg.py
 F5=${LOCAL_TEST_DIR}/test_multiPathMultiModuleEarlyDelete_cfg.py
 F6=${LOCAL_TEST_DIR}/test_subProcessDeleteEarly_cfg.py
+F7=${LOCAL_TEST_DIR}/test_consumeAfterEarlyDeleteTask_cfg.py
+F8=${LOCAL_TEST_DIR}/test_consumeAfterEarlyDeletePath_cfg.py
 
 (cmsRun $F1 ) || die "Failure using $F1" $?
 (cmsRun $F2 ) || die "Failure using $F2" $?
@@ -16,5 +18,7 @@ F6=${LOCAL_TEST_DIR}/test_subProcessDeleteEarly_cfg.py
 (cmsRun $F4 ) || die "Failure using $F4" $?
 (cmsRun $F5 ) || die "Failure using $F5" $?
 (cmsRun $F6 ) || die "Failure using $F6" $?
+(cmsRun $F7 ) || die "Failure using $F7" $?
+(cmsRun $F8 ) || die "Failure using $F8" $?
 
 
