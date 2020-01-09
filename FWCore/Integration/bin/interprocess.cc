4d5d8ca01c2deb9bbbab479d0230bb995fb10c1c
Chris Jones <chrisdjones15@gmail.com>
2020-01-09 14:31:12 -0600

    Full serialize/deserialize using shared memory

--- FWCore/Integration/bin/interprocess.cc
+++ FWCore/Integration/bin/interprocess.cc
@@ -6,9 +6,14 @@
 #include <boost/interprocess/sync/named_condition.hpp>
 #include <boost/interprocess/sync/scoped_lock.hpp>
 
+#include "TClass.h"
+#include "TBufferFile.h"
 
 #include <string>
 #include <iostream>
+
+#include "FWCore/TestProcessor/interface/TestProcessor.h"
+#include "DataFormats/TestObjects/interface/ToyProducts.h"
 
 static char const* const kMemoryNameOpt = "memory-name";
 static char const* const kMemoryNameCommandOpt = "memory-name,m";
@@ -17,102 +22,175 @@ static char const* const kUniqueIDCommandOpt = "unique-id,i";
 static char const* const kHelpOpt = "help";
 static char const* const kHelpCommandOpt = "help,h";
 
+//NOTE: Can use TestProcessor as the harness for the worker
+
 namespace {
   std::string unique_name(std::string iBase, std::string_view ID) {
     iBase.append(ID);
     return iBase;
   }
-}
+
+  template <typename T>
+  class Serializer {
+  public:
+    Serializer(boost::interprocess::managed_shared_memory& iSM,
+               std::string iSMName,
+               std::string const& iBase,
+               std::string_view ID)
+        : managed_shm_(&iSM),
+          smName_{std::move(iSMName)},
+          name_{unique_name(iBase, ID)},
+          class_{TClass::GetClass(typeid(T))},
+          bufferFile_{TBuffer::kWrite} {
+      buffer_ = managed_shm_->find<char>(name_.c_str());
+      assert(buffer_.first);
+      std::pair<bool*, std::size_t> sm_buffer_resized =
+          managed_shm_->find<bool>(unique_name(iBase + "_resize", ID).c_str());
+      buffer_resized_ = sm_buffer_resized.first;
+      assert(buffer_resized_);
+    }
+
+    void serialize(T& iValue) {
+      bufferFile_.Reset();
+      class_->WriteBuffer(bufferFile_, &iValue);
+
+      if (static_cast<unsigned long>(bufferFile_.Length()) > buffer_.second) {
+        managed_shm_->destroy<char>(name_.c_str());
+        auto diff = bufferFile_.Length() - buffer_.second;
+        auto success = managed_shm_->grow(smName_.c_str(), diff);
+        assert(success);
+
+        buffer_.first = managed_shm_->construct<char>(name_.c_str())[bufferFile_.Length()](0);
+        buffer_.second = bufferFile_.Length();
+        *buffer_resized_ = true;
+      }
+      std::copy(bufferFile_.Buffer(), bufferFile_.Buffer() + bufferFile_.Length(), buffer_.first);
+    }
+
+  private:
+    boost::interprocess::managed_shared_memory* const managed_shm_;
+    std::string smName_;
+    std::string name_;
+    std::pair<char*, std::size_t> buffer_;
+    bool* buffer_resized_;
+    TClass* const class_;
+    TBufferFile bufferFile_;
+  };
+}  // namespace
+
+class Harness {
+public:
+  Harness(std::string const& iConfig) : tester_(edm::test::TestProcessor::Config{iConfig}) {}
+
+  edmtest::IntProduct getNextValue() {
+    auto event = tester_.test();
+    return *event.get<edmtest::IntProduct>();
+  }
+
+private:
+  edm::test::TestProcessor tester_;
+};
 
 int main(int argc, char* argv[]) {
-  
   std::string descString(argv[0]);
-  descString += " [--" ;
-  descString += kMemoryNameOpt ;
+  descString += " [--";
+  descString += kMemoryNameOpt;
   descString += "] memory_name";
   boost::program_options::options_description desc(descString);
-  
+
   desc.add_options()(kHelpCommandOpt, "produce help message")(
-    kMemoryNameCommandOpt, boost::program_options::value<std::string>(), "memory name")(
-   kUniqueIDCommandOpt, boost::program_options::value<std::string>(), "unique id");
-  
+      kMemoryNameCommandOpt, boost::program_options::value<std::string>(), "memory name")(
+      kUniqueIDCommandOpt, boost::program_options::value<std::string>(), "unique id");
+
   boost::program_options::positional_options_description p;
   p.add(kMemoryNameOpt, 1);
   p.add(kUniqueIDOpt, 2);
-  
+
   boost::program_options::options_description all_options("All Options");
   all_options.add(desc);
-  
+
   boost::program_options::variables_map vm;
   try {
     store(boost::program_options::command_line_parser(argc, argv).options(all_options).positional(p).run(), vm);
     notify(vm);
   } catch (boost::program_options::error const& iException) {
-    std::cout
-        << argv[0] <<": Error while trying to process command line arguments:\n"
-        << iException.what() << "\nFor usage and an options list, please do 'cmsRun --help'.";
+    std::cout << argv[0] << ": Error while trying to process command line arguments:\n"
+              << iException.what() << "\nFor usage and an options list, please do 'cmsRun --help'.";
     return 1;
   }
-  
+
   if (vm.count(kHelpOpt)) {
     std::cout << desc << std::endl;
     return 0;
   }
-  
+
   if (!vm.count(kMemoryNameOpt)) {
-    std::cout <<" no argument given"<<std::endl;
+    std::cout << " no argument given" << std::endl;
     return 1;
   }
 
   if (!vm.count(kUniqueIDOpt)) {
-    std::cout <<" no second argument given"<<std::endl;
+    std::cout << " no second argument given" << std::endl;
     return 1;
   }
-  
+
   std::string const memoryName(vm[kMemoryNameOpt].as<std::string>());
   std::string const uniqueID(vm[kUniqueIDOpt].as<std::string>());
   {
     using namespace boost::interprocess;
-    managed_shared_memory managed_shm{open_only, unique_name(memoryName, uniqueID).c_str()};
-    std::pair<long double *, std::size_t> sm_sum = managed_shm.find<long double>(unique_name("sum", uniqueID).c_str());
-    
-    const auto s_pi{std::acos(-1)};
-  
-    constexpr unsigned int iterations = 100000000;
-    
+    auto memoryNameUnique = unique_name(memoryName, uniqueID);
+    managed_shared_memory managed_shm{open_only, memoryNameUnique.c_str()};
+
     named_mutex named_mtx{open_or_create, unique_name("mtx", uniqueID).c_str()};
     named_condition named_cndFromMain{open_or_create, unique_name("cndFromMain", uniqueID).c_str()};
-    std::pair<bool *, std::size_t> sm_stop = managed_shm.find<bool>(unique_name("stop", uniqueID).c_str());
-    
-    //named_mutex named_mtxToMain{open_or_create, "mtxToMain"};
+    std::pair<bool*, std::size_t> sm_stop = managed_shm.find<bool>(unique_name("stop", uniqueID).c_str());
+
     named_condition named_cndToMain{open_or_create, unique_name("cndToMain", uniqueID).c_str()};
-  
+
     int counter = 0;
+
     scoped_lock<named_mutex> lock(named_mtx);
-    while(true) {
+    std::cerr << uniqueID << " process: initializing " << std::endl;
+    int nlines;
+    std::cin >> nlines;
+
+    std::string configuration;
+    for (int i = 0; i < nlines; ++i) {
+      std::string c;
+      std::getline(std::cin, c);
+      std::cerr << c << "\n";
+      configuration += c + "\n";
+    }
+
+    Harness harness(configuration);
+
+    Serializer<edmtest::IntProduct> serializer(managed_shm, memoryNameUnique, "buffer", uniqueID);
+
+    std::cerr << uniqueID << " process: done initializing" << std::endl;
+    named_cndToMain.notify_all();
+    while (true) {
       {
         ++counter;
-        std::cerr <<uniqueID<<" process: waiting "<<counter<<std::endl;
+        std::cerr << uniqueID << " process: waiting " << counter << std::endl;
         named_cndFromMain.wait(lock);
-        if(*sm_stop.first) { break;}
+        if (*sm_stop.first) {
+          break;
+        }
       }
-  
-      std::cerr <<uniqueID <<" process: integrating "<<counter<<std::endl;
-      long double sum = 0.;
-      const long double stepSize = s_pi / iterations;
-      for (unsigned int i = 0; i < iterations; ++i) {
-        sum += stepSize * cos(i * stepSize);
-      }
-      std::cerr <<uniqueID<<" process: integrated "<<counter<<std::endl;
+
+      std::cerr << uniqueID << " process: integrating " << counter << std::endl;
+      auto value = harness.getNextValue();
+
+      std::cerr << uniqueID << " process: integrated " << counter << std::endl;
 
       {
-        *sm_sum.first = sum;
-        std::cerr <<uniqueID<<" process: notifying "<<counter<<std::endl;
+        serializer.serialize(value);
+        std::cerr << uniqueID << " process: notifying " << counter << std::endl;
         named_cndToMain.notify_all();
       }
-      std::cerr <<uniqueID<<" process: "<<sum<<" "<<counter<<std::endl;
+      std::cerr << uniqueID << " process: " << value.value << " "
+                << " " << counter << std::endl;
     }
-  }  
+  }
   return 0;
-  
 }
--- FWCore/Integration/test/TestInterProcessProd.cc
+++ FWCore/Integration/test/TestInterProcessProd.cc
@@ -2,8 +2,12 @@
 #include "FWCore/Utilities/interface/EDPutToken.h"
 #include "FWCore/Framework/interface/MakerMacros.h"
 
+#include "DataFormats/TestObjects/interface/ToyProducts.h"
+
 #include <stdio.h>
 #include <iostream>
+
+#include "TBufferFile.h"
 
 #include "boost/interprocess/shared_memory_object.hpp"
 #include "boost/interprocess/managed_shared_memory.hpp"
@@ -11,80 +15,147 @@
 #include <boost/interprocess/sync/named_condition.hpp>
 #include <boost/interprocess/sync/scoped_lock.hpp>
 
-
 using namespace boost::interprocess;
 
 namespace testinter {
   struct Cache {
     mutable int id = 0;
   };
-}
+
+  template <typename T>
+  class Deserializer {
+  public:
+    Deserializer(boost::interprocess::managed_shared_memory& iSM, std::string iBase, int ID)
+        : managed_shm_(&iSM),
+          name_{unique_name(iBase, ID)},
+          resizeName_{unique_name(iBase + "_resize", ID)},
+          class_{TClass::GetClass(typeid(T))},
+          bufferFile_(TBuffer::kRead) {
+      managed_shm_->destroy<char>(name_.c_str());
+      managed_shm_->destroy<char>(resizeName_.c_str());
+
+      constexpr unsigned int bufferInitialSize = 5;
+      buffer_.first = managed_shm_->construct<char>(name_.c_str())[bufferInitialSize](0);
+      buffer_.second = bufferInitialSize;
+      assert(buffer_.first);
+      buffer_resized_ = managed_shm_->construct<bool>(resizeName_.c_str())(false);
+      assert(buffer_resized_);
+
+      bufferFile_.SetBuffer(buffer_.first, buffer_.second, kFALSE);
+    }
+
+    ~Deserializer() {
+      managed_shm_->destroy<char>(name_.c_str());
+      managed_shm_->destroy<bool>(resizeName_.c_str());
+    }
+
+    T deserialize() {
+      T value;
+      if (*buffer_resized_) {
+        buffer_ = managed_shm_->find<char>(name_.c_str());
+        bufferFile_.SetBuffer(buffer_.first, buffer_.second, kFALSE);
+        *buffer_resized_ = false;
+      }
+
+      class_->ReadBuffer(bufferFile_, &value);
+      bufferFile_.Reset();
+      return value;
+    }
+
+  private:
+    std::string unique_name(std::string const& iBase, int ID) {
+      auto pid = getpid();
+      return iBase + std::to_string(pid) + "_" + std::to_string(ID);
+    }
+
+    boost::interprocess::managed_shared_memory* const managed_shm_;
+    const std::string name_;
+    const std::string resizeName_;
+    std::pair<char*, std::size_t> buffer_;
+    bool* buffer_resized_;
+    TClass* const class_;
+    TBufferFile bufferFile_;
+  };
+}  // namespace testinter
 
 class TestInterProcessProd : public edm::stream::EDProducer<edm::GlobalCache<testinter::Cache>> {
-
 public:
-  TestInterProcessProd( edm::ParameterSet const&, testinter::Cache const* );
+  TestInterProcessProd(edm::ParameterSet const&, testinter::Cache const*);
   ~TestInterProcessProd();
-  
-  void produce(edm::Event& , edm::EventSetup const&);
+
+  void produce(edm::Event&, edm::EventSetup const&);
 
   static std::unique_ptr<testinter::Cache> initializeGlobalCache(edm::ParameterSet const&);
 
   static void globalEndJob(testinter::Cache*);
 
 private:
-
   int id_;
-  edm::EDPutTokenT<double> token_;
+  edm::EDPutTokenT<edmtest::IntProduct> token_;
   FILE* pipe_;
 
   managed_shared_memory managed_sm_;
-  
+
   named_mutex named_mtx_;
   named_condition named_cndFromMain_;
 
-  //named_mutex named_mtxToMain_;
   named_condition named_cndToMain_;
-  
-  long double* sm_sum_;
+
+  testinter::Deserializer<edmtest::IntProduct> deserializer_;
   bool* stop_;
 
   std::string unique_name(std::string iBase) {
     auto pid = getpid();
     iBase += std::to_string(pid);
-    iBase +="_";
-    iBase +=std::to_string(id_);
-    
+    iBase += "_";
+    iBase += std::to_string(id_);
+
     return iBase;
   }
-
 };
 
-TestInterProcessProd::TestInterProcessProd( edm::ParameterSet const&, testinter::Cache const* cache ):
-id_{ ++cache->id},
-managed_sm_{ open_or_create, unique_name("testProd").c_str(), 1024},
-named_mtx_{open_or_create, unique_name("mtx").c_str()},
-named_cndFromMain_{open_or_create, unique_name("cndFromMain").c_str()},
-//named_mtxToMain_{open_or_create, "mtxToMain"},
-named_cndToMain_{open_or_create, unique_name("cndToMain").c_str()}
+TestInterProcessProd::TestInterProcessProd(edm::ParameterSet const&, testinter::Cache const* cache)
+    : id_{++cache->id},
+      managed_sm_{open_or_create, unique_name("testProd").c_str(), 1024},
+      named_mtx_{open_or_create, unique_name("mtx").c_str()},
+      named_cndFromMain_{open_or_create, unique_name("cndFromMain").c_str()},
+      named_cndToMain_{open_or_create, unique_name("cndToMain").c_str()},
+      deserializer_{managed_sm_, "buffer", id_} {
+  token_ = produces<edmtest::IntProduct>();
 
- {
-  token_ = produces<double>();
-
-  managed_sm_.destroy<long double>(unique_name("sum").c_str());
   managed_sm_.destroy<bool>(unique_name("stop").c_str());
-
-  sm_sum_ = managed_sm_.construct<long double>(unique_name("sum").c_str())(0);
   stop_ = managed_sm_.construct<bool>(unique_name("stop").c_str())(false);
+  assert(stop_);
 
   //make sure output is flushed before popen does any writing
   fflush(stdout);
   fflush(stderr);
+
+  scoped_lock<named_mutex> lock(named_mtx_);
+  std::cout << id_ << " starting external process" << std::endl;
   pipe_ = popen(unique_name("cmsInterProcess testProd ").c_str(), "w");
-  
-  if(NULL == pipe_) {
+
+  if (NULL == pipe_) {
     abort();
   }
+
+  {
+    auto v = R"_(from FWCore.TestProcessor.TestProcess import *
+process = TestProcess()
+process.gen = cms.EDProducer("BusyWaitIntProducer", ivalue = cms.int32(6), iterations=cms.uint32(10000000))
+process.moduleToTest(process.gen)
+)_";
+    auto length = strlen(v);
+    auto nlines = std::to_string(std::count(v, v + length, '\n'));
+    auto result = fwrite(nlines.data(), sizeof(char), nlines.size(), pipe_);
+    assert(result = nlines.size());
+    result = fwrite(v, sizeof(char), strlen(v), pipe_);
+    assert(result == strlen(v));
+    fflush(pipe_);
+  }
+  std::cout << id_ << " waiting for external process" << std::endl;
+  named_cndToMain_.wait(lock);
+  std::cout << id_ << " done waiting for external process" << std::endl;
 }
 
 std::unique_ptr<testinter::Cache> TestInterProcessProd::initializeGlobalCache(edm::ParameterSet const&) {
@@ -93,37 +164,35 @@ std::unique_ptr<testinter::Cache> TestInterProcessProd::initializeGlobalCache(ed
 
 void TestInterProcessProd::globalEndJob(testinter::Cache*) {}
 
-
-
 TestInterProcessProd::~TestInterProcessProd() {
   {
     scoped_lock<named_mutex> lock(named_mtx_);
     *stop_ = true;
     named_cndFromMain_.notify_all();
   }
-  
+
   pclose(pipe_);
-  managed_sm_.destroy<long double>(unique_name("sum").c_str());
   managed_sm_.destroy<bool>(unique_name("stop").c_str());
+
+  named_mutex::remove(unique_name("mtx").c_str());
+  named_condition::remove(unique_name("cndFromMain").c_str());
+  named_condition::remove(unique_name("cndToMain").c_str());
 }
 
-
 void TestInterProcessProd::produce(edm::Event& iEvent, edm::EventSetup const&) {
-  std::cout <<id_<<" taking from lock"<<std::endl;
+  std::cout << id_ << " taking from lock" << std::endl;
   scoped_lock<named_mutex> lock(named_mtx_);
   {
-    std::cout <<id_ <<" notifying"<<std::endl;
-    named_cndFromMain_.notify_all();    
+    std::cout << id_ << " notifying" << std::endl;
+    named_cndFromMain_.notify_all();
   }
-  
-  long double value;
-  {
-    std::cout <<id_<< " waiting"<<std::endl;
-    named_cndToMain_.wait(lock);
-    value = *sm_sum_;
-  }
-  std::cout <<id_ << " from shared memory "<<value<<std::endl;
-  
+
+  std::cout << id_ << " waiting" << std::endl;
+  named_cndToMain_.wait(lock);
+
+  auto value = deserializer_.deserialize();
+  std::cout << id_ << " from shared memory " << value.value << std::endl;
+
   iEvent.emplace(token_, value);
 }
 
