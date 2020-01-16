a68d9b8b3ee490aff68b9f1dae25e22290ad3267
Matti Kortelainen <matti.kortelainen@cern.ch>
2020-01-16 04:20:12 -0600

    Load CUDAService from Services_cff, and only if gpu modifier is active (#432)
    Include offlineBeamSpotCUDA only if gpu modifier is active

--- RecoVertex/BeamSpotProducer/python/BeamSpot_cff.py
+++ RecoVertex/BeamSpotProducer/python/BeamSpot_cff.py
@@ -3,9 +3,10 @@ import FWCore.ParameterSet.Config as cms
 from RecoVertex.BeamSpotProducer.BeamSpot_cfi import *
 from RecoVertex.BeamSpotProducer.beamSpotToCUDA_cfi import beamSpotToCUDA as _beamSpotToCUDA
 
-offlineBeamSpotCUDA = _beamSpotToCUDA.clone()
+offlineBeamSpotTask = cms.Task(offlineBeamSpot)
 
-offlineBeamSpotTask = cms.Task(
-    offlineBeamSpot,
-    offlineBeamSpotCUDA
-)
+from Configuration.ProcessModifiers.gpu_cff import gpu
+offlineBeamSpotCUDA = _beamSpotToCUDA.clone()
+_offlineBeamSpotTask_gpu = offlineBeamSpotTask.copy()
+_offlineBeamSpotTask_gpu.add(offlineBeamSpotCUDA)
+gpu.toReplaceWith(offlineBeamSpotTask, _offlineBeamSpotTask_gpu)
