7cd3e8e48036d5ed1a11bf936b0a3e588781c614
Matti Kortelainen <matti.kortelainen@cern.ch>
2020-01-17 09:10:53 -0600

    Implement changes from the CUDA framework review (cms-patatrack#429)
    Rename the cudautils namespace to cms::cuda or cms::cudatest, and drop the CUDA prefix from the symbols defined there.
    
    Always record and query the CUDA event, to minimize need for error checking in CUDAScopedContextProduce destructor.
    
    Add comments to highlight the pieces in CachingDeviceAllocator that have been changed wrt. cub.
    
    Various other updates and clean up:
      - enable CUDA for compute capability 3.5.
      - clean up CUDAService, CUDA tests and plugins.
      - add CUDA existence protections to BuildFiles.
      - mark thread-safe static variables with CMS_THREAD_SAFE.

--- RecoLocalCalo/EcalRecAlgos/interface/EcalTimeBiasCorrectionsGPU.h
+++ RecoLocalCalo/EcalRecAlgos/interface/EcalTimeBiasCorrectionsGPU.h
@@ -5,7 +5,7 @@
 
 #ifndef __CUDACC__
 #include "HeterogeneousCore/CUDAUtilities/interface/CUDAHostAllocator.h"
-#include "HeterogeneousCore/CUDACore/interface/CUDAESProduct.h"
+#include "HeterogeneousCore/CUDACore/interface/ESProduct.h"
 #endif
 
 #include <cuda_runtime.h>
@@ -44,7 +44,7 @@ private:
   std::vector<float> const& EETimeCorrShiftBins_;
 
 #ifndef __CUDACC__
-  CUDAESProduct<Product> product_;
+  cms::cuda::ESProduct<Product> product_;
 #endif
 };
 
