e88ebac7f6c210dcee8d6c8505d69ab19a996441
Norraphat <srimanob@mail.cern.ch>
2020-01-24 22:12:17 +0100

    add 2 workflows for PR test, target for PPS plot

--- Configuration/PyReleaseValidation/scripts/runTheMatrix.py
+++ Configuration/PyReleaseValidation/scripts/runTheMatrix.py
@@ -62,7 +62,9 @@ if __name__ == '__main__':
                      136.7611, #2016E JetHT reMINIAOD from 80X legacy
                      136.8311, #2017F JetHT reMINIAOD from 94X reprocessing
                      136.788, #2017B Photon data
+                     136.801, #2017C ZeroBias
                      136.85, #2018A Egamma data
+                     136.880, #2018C ZeroBias
                      140.53, #2011 HI data
                      140.56, #2018 HI data
                      158.0, #2018 HI MC with pp-like reco
