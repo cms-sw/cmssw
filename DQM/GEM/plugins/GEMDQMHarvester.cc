91eca0d451ff56cf41a70f3de1626de7d616c856
Byeonghak Ko <quark2930@hotmail.com>
2020-01-28 19:50:12 +0900

    clang-format

--- DQM/GEM/plugins/GEMDQMHarvester.cc
+++ DQM/GEM/plugins/GEMDQMHarvester.cc
@@ -29,11 +29,11 @@ public:
 
 protected:
   void endRun(edm::Run const &, edm::EventSetup const &) override;
-  void dqmEndJob(DQMStore::IBooker &, DQMStore::IGetter &) override {};  // Cannot use; it is called after dqmSaver
-  
+  void dqmEndJob(DQMStore::IBooker &, DQMStore::IGetter &) override{};  // Cannot use; it is called after dqmSaver
+
   void refineSummaryHistogram(edm::Service<DQMStore> &);
   void refineSummaryHistogramCore(TH3F *, std::string &, TH2F *&, std::string strTmpPrefix = "tmp_");
-  
+
   void fillUnderOverflowBunchCrossing(edm::Service<DQMStore> &, std::string);
 
   std::string strOutFile_;
@@ -52,10 +52,10 @@ void GEMDQMHarvester::fillDescriptions(edm::ConfigurationDescriptions &descripti
 void GEMDQMHarvester::endRun(edm::Run const &, edm::EventSetup const &) {
   edm::Service<DQMStore> store;
   refineSummaryHistogram(store);
-  
+
   store->setCurrentFolder("GEM/StatusDigi");
   auto listME = store->getMEs();
-  
+
   for (auto strName : listME) {
     if (strName.find("vfatStatus_BC_") != std::string::npos) {
       fillUnderOverflowBunchCrossing(store, strName);
@@ -67,41 +67,42 @@ void GEMDQMHarvester::refineSummaryHistogram(edm::Service<DQMStore> &store) {
   std::string strDirCurr = "GEM/EventInfo";
   std::string strNameSrc = "reportSummaryMapPreliminary";
   std::string strNewName = "reportSummaryMap";
-  
+
   store->setCurrentFolder(strDirCurr.c_str());
-  
-  MonitorElement *h3Curr = store->get(( strDirCurr + "/" + strNameSrc ).c_str());
+
+  MonitorElement *h3Curr = store->get((strDirCurr + "/" + strNameSrc).c_str());
   TH2F *h2New = NULL;
-  
+
   refineSummaryHistogramCore(h3Curr->getTH3F(), strNewName, h2New);
   store->book2D(strNewName, h2New);
 }
 
-void GEMDQMHarvester::refineSummaryHistogramCore(TH3F *h3Src, 
-                                                 std::string &strNewName, 
-                                                 TH2F *&h2New, 
-                                                 std::string strTmpPrefix) 
-{
+void GEMDQMHarvester::refineSummaryHistogramCore(TH3F *h3Src,
+                                                 std::string &strNewName,
+                                                 TH2F *&h2New,
+                                                 std::string strTmpPrefix) {
   Int_t i, j;
-  
+
   Int_t nNBinX = h3Src->GetNbinsX();
   Int_t nNBinY = h3Src->GetNbinsY();
-  
-  Float_t arrfBinX[ 128 ], arrfBinY[ 32 ];
-  
-  for ( i = 0 ; i <= nNBinX ; i++ ) arrfBinX[ i ] = h3Src->GetXaxis()->GetBinLowEdge(i + 1);
-  for ( i = 0 ; i <= nNBinY ; i++ ) arrfBinY[ i ] = h3Src->GetYaxis()->GetBinLowEdge(i + 1);
-  
+
+  Float_t arrfBinX[128], arrfBinY[32];
+
+  for (i = 0; i <= nNBinX; i++)
+    arrfBinX[i] = h3Src->GetXaxis()->GetBinLowEdge(i + 1);
+  for (i = 0; i <= nNBinY; i++)
+    arrfBinY[i] = h3Src->GetYaxis()->GetBinLowEdge(i + 1);
+
   h2New = new TH2F(strNewName.c_str(), h3Src->GetTitle(), nNBinX, arrfBinX, nNBinY, arrfBinY);
-  
-  for ( i = 0 ; i < nNBinX ; i++ ) {
+
+  for (i = 0; i < nNBinX; i++) {
     h2New->GetXaxis()->SetBinLabel(i + 1, h3Src->GetXaxis()->GetBinLabel(i + 1));
-    for ( j = 0 ; j < nNBinY ; j++ ) {
+    for (j = 0; j < nNBinY; j++) {
       h2New->GetYaxis()->SetBinLabel(j + 1, h3Src->GetYaxis()->GetBinLabel(j + 1));
-      
-      if ( h3Src->GetBinContent(i + 1, j + 1, 2) != 0 ) {
+
+      if (h3Src->GetBinContent(i + 1, j + 1, 2) != 0) {
         h2New->SetBinContent(i + 1, j + 1, 2);
-      } else if ( h3Src->GetBinContent(i + 1, j + 1, 1) != 0 ) {
+      } else if (h3Src->GetBinContent(i + 1, j + 1, 1) != 0) {
         h2New->SetBinContent(i + 1, j + 1, 1);
       }
     }
@@ -110,15 +111,15 @@ void GEMDQMHarvester::refineSummaryHistogramCore(TH3F *h3Src,
 
 void GEMDQMHarvester::fillUnderOverflowBunchCrossing(edm::Service<DQMStore> &store, std::string strNameSrc) {
   std::string strDirCurr = "GEM/StatusDigi";
-  
+
   store->setCurrentFolder(strDirCurr.c_str());
-  MonitorElement *h2Curr = store->get(( strDirCurr + "/" + strNameSrc ).c_str());
-  
+  MonitorElement *h2Curr = store->get((strDirCurr + "/" + strNameSrc).c_str());
+
   Int_t nNBinX = h2Curr->getNbinsX();
   Int_t nNBinY = h2Curr->getNbinsY();
-  
+
   for (Int_t i = 0; i < nNBinY; i++) {
-    h2Curr->setBinContent(1,      i, h2Curr->getBinContent(0, i)      + h2Curr->getBinContent(1, i));
+    h2Curr->setBinContent(1, i, h2Curr->getBinContent(0, i) + h2Curr->getBinContent(1, i));
     h2Curr->setBinContent(nNBinX, i, h2Curr->getBinContent(nNBinX, i) + h2Curr->getBinContent(nNBinX + 1, i));
   }
 }
