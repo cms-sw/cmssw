4efd66a776bc7295dc949bb4eee4c0b0fd1ab032
Paul Asmuss <paul.asmuss@cern.ch>
2020-02-03 14:23:59 +0100

    Adjusted color code

--- GeneratorInterface/Pythia8Interface/plugins/Py8EGun.cc
+++ GeneratorInterface/Pythia8Interface/plugins/Py8EGun.cc
@@ -38,6 +38,8 @@ namespace gen {
   bool Py8EGun::generatePartonsAndHadronize() {
     fMasterGen->event.reset();
 
+    int colorindex = 101;
+
     for (size_t i = 0; i < fPartIDs.size(); i++) {
       int particleID = fPartIDs[i];  // this is PDG - need to convert to Py8 ???
 
@@ -53,14 +55,12 @@ namespace gen {
       double py = pp * sin(the) * sin(phi);
       double pz = pp * cos(the);
 
-      int colorindex = 101 + i;
-
       if (!((fMasterGen->particleData).isParticle(particleID))) {
         particleID = std::fabs(particleID);
       }
-      if (1 <= std::abs(particleID) && std::abs(particleID) <= 6)  // quarks
+      if (1 <= std::abs(particleID) && std::abs(particleID) <= 6) // quarks
         (fMasterGen->event).append(particleID, 23, colorindex, 0, px, py, pz, ee, mass);
-      else if (std::abs(particleID) == 21)  // gluons
+      else if (std::abs(particleID) == 21) // gluons
         (fMasterGen->event).append(21, 23, colorindex, colorindex+1, px, py, pz, ee, mass);
       // other
       else {
@@ -78,8 +78,10 @@ namespace gen {
       if (fAddAntiParticle) {
         if (1 <= std::abs(particleID) && std::abs(particleID) <= 6) {  // quarks
           (fMasterGen->event).append(-particleID, 23, 0, colorindex, -px, -py, -pz, ee, mass);
+	  colorindex ++;
         } else if (std::abs(particleID) == 21) {  // gluons
           (fMasterGen->event).append(21, 23, colorindex+1, colorindex, -px, -py, -pz, ee, mass);
+	  colorindex ++; colorindex++;
         } else {
           if ((fMasterGen->particleData).isParticle(-particleID)) {
             (fMasterGen->event).append(-particleID, 1, 0, 0, -px, -py, -pz, ee, mass);
