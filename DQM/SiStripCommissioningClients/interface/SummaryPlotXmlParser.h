b1c0af60a5385ecea2dcccaf31181ee84a4efb52
Marcel Andre Schneider <marcel.andre.schneider@cern.ch>
2020-02-05 15:41:22 +0100

    Changes by @rgerosa to SiStripCommissioningClients
    ... to make it independent from DQM/ClientConfig.

--- DQM/SiStripCommissioningClients/interface/SummaryPlotXmlParser.h
+++ DQM/SiStripCommissioningClients/interface/SummaryPlotXmlParser.h
@@ -8,14 +8,14 @@
 #include <string>
 #include <vector>
 #include <map>
-
+#include <cassert>
 #include <boost/property_tree/xml_parser.hpp>
 #include <boost/property_tree/ptree.hpp>
 
 class SummaryPlotXmlParser;
 
 /** Debug information. */
-std::ostream& operator<<(std::ostream&, const SummaryPlotXmlParser&);
+std::ostream& operator<< ( std::ostream&, const SummaryPlotXmlParser& );
 
 /** 
     @class SummaryPlotXmlParser
@@ -24,28 +24,52 @@ std::ostream& operator<<(std::ostream&, const SummaryPlotXmlParser&);
     @brief Parses the "summary plot" xml configuration file
 */
 class SummaryPlotXmlParser {
-public:
+  
+ public:
+
   // ---------- Co(de)nstructors and consts ----------
 
   /** Default constructor. */
   SummaryPlotXmlParser();
 
+  /** Default destructor. */
+  ~SummaryPlotXmlParser(){;}
+  
   // ---------- Public interface ----------
 
+  
   /** Fill the map with the required tag/names and values */
-  void parseXML(const std::string& xml_file);
-
+  void parseXML( const std::string& xml_file );
+  
   /** Returns SummaryPlot objects for given commissioning task. */
-  std::vector<SummaryPlot> summaryPlots(const sistrip::RunType&);
-
+  std::vector<SummaryPlot> summaryPlots( const sistrip::RunType& );
+  
   /** Debug print method. */
-  void print(std::stringstream&) const;
+  void print( std::stringstream& ) const;
+  
+ private:
 
-private:
   // ---------- Private member data ----------
 
   /** Container holding the SummaryPlot objects. */
-  std::map<sistrip::RunType, std::vector<SummaryPlot> > plots_;
+  std::map< sistrip::RunType, std::vector<SummaryPlot> > plots_;
+
+  // RunType tags and attributes
+  static const std::string rootTag_;
+  static const std::string runTypeTag_;
+  static const std::string runTypeAttr_;
+
+  // SummaryPlot tags and attributes
+  static const std::string summaryPlotTag_;
+  static const std::string monitorableAttr_;
+  static const std::string presentationAttr_;
+  static const std::string viewAttr_;
+  static const std::string levelAttr_;
+  static const std::string granularityAttr_;
+  
 };
 
-#endif  // DQM_SiStripCommissioningClients_SummaryPlotXmlParser_H
+#endif // DQM_SiStripCommissioningClients_SummaryPlotXmlParser_H
+
+
+    
--- DQM/SiStripCommissioningClients/src/SummaryPlotXmlParser.cc
+++ DQM/SiStripCommissioningClients/src/SummaryPlotXmlParser.cc
@@ -2,38 +2,105 @@
 #include "DataFormats/SiStripCommon/interface/SiStripEnumsAndStrings.h"
 #include "FWCore/MessageLogger/interface/MessageLogger.h"
 #include <stdexcept>
-#include <cassert>
 
 using namespace sistrip;
 
-SummaryPlotXmlParser::SummaryPlotXmlParser() {}
+// -----------------------------------------------------------------------------
+//
+const std::string SummaryPlotXmlParser::rootTag_ = "root";
+const std::string SummaryPlotXmlParser::runTypeTag_ = "RunType";
+const std::string SummaryPlotXmlParser::runTypeAttr_ = "name";
+const std::string SummaryPlotXmlParser::summaryPlotTag_ = "SummaryPlot";
+const std::string SummaryPlotXmlParser::monitorableAttr_ = "monitorable";
+const std::string SummaryPlotXmlParser::presentationAttr_ = "presentation";
+const std::string SummaryPlotXmlParser::viewAttr_ = "view";
+const std::string SummaryPlotXmlParser::levelAttr_ = "level";
+const std::string SummaryPlotXmlParser::granularityAttr_ = "granularity";
 
 // -----------------------------------------------------------------------------
 //
-std::vector<SummaryPlot> SummaryPlotXmlParser::summaryPlots(const sistrip::RunType& run_type) {
-  if (plots_.empty()) {
-    edm::LogWarning(mlDqmClient_) << "[SummaryPlotXmlParser" << __func__ << "]"
-                                  << " You have not called the parseXML function,"
-                                  << " or your XML file is erronious" << std::endl;
-  }
-  if (plots_.find(run_type) != plots_.end()) {
-    return plots_[run_type];
-  } else {
-    return std::vector<SummaryPlot>();
-  }
-}
-
-// -----------------------------------------------------------------------------
-//
-void SummaryPlotXmlParser::parseXML(const std::string& f) {
+SummaryPlotXmlParser::SummaryPlotXmlParser() {
   plots_.clear();
-  // TODO: implement parser based on property tree.
-  assert(!"No longer implemented.");
 }
 
 // -----------------------------------------------------------------------------
 //
-std::ostream& operator<<(std::ostream& os, const SummaryPlotXmlParser& parser) {
+std::vector<SummaryPlot> SummaryPlotXmlParser::summaryPlots( const sistrip::RunType& run_type ) {
+  if( plots_.empty() ) {
+    edm::LogWarning(mlDqmClient_)
+      << "[SummaryPlotXmlParser" << __func__ << "]"
+      << " You have not called the parseXML function,"
+      << " or your XML file is erronious" << std::endl;
+  }
+  if( plots_.find( run_type ) != plots_.end() ) {
+    return plots_[run_type];
+  } else { return std::vector<SummaryPlot>(); }
+  
+}
+
+// -----------------------------------------------------------------------------
+//
+void SummaryPlotXmlParser::parseXML( const std::string& filename ) {
+  
+  plots_.clear();
+  
+  boost::property_tree::ptree xmltree;
+  boost::property_tree::read_xml(filename, xmltree);
+  
+  auto runs = xmltree.find(rootTag_);
+  if(runs == xmltree.not_found()){
+  }
+  
+  // Iterate through nodes
+  for(auto & xml: xmltree){
+    if(xml.first == rootTag_){// find main root
+      for(auto & runtype: xml.second){
+	if(runtype.first == runTypeTag_){ // enter in the run type
+	  sistrip::RunType run_type = SiStripEnumsAndStrings::runType(runtype.second.get<std::string>("<xmlattr>."+runTypeAttr_)); 
+	  for(auto & sumplot: runtype.second){
+	    if(sumplot.first == summaryPlotTag_){
+	      std::string mon  = sumplot.second.get<std::string>("<xmlattr>."+monitorableAttr_);
+	      std::string pres = sumplot.second.get<std::string>("<xmlattr>."+presentationAttr_);
+	      std::string level = sumplot.second.get<std::string>("<xmlattr>."+levelAttr_);
+	      std::string gran = sumplot.second.get<std::string>("<xmlattr>."+granularityAttr_);
+	      SummaryPlot plot(mon,pres,gran,level);
+	      plots_[run_type].push_back( plot );		
+	    }
+	  }
+	  if(plots_[run_type].empty()){
+	    std::stringstream ss;
+	    ss << "[SummaryPlotXmlParser::" << __func__ << "]"
+	       << " Unable to find any summary plot for "<<runTypeTag_<<" nodes!"
+	       << " Empty xml summary histo block?";
+	    throw( std::runtime_error( ss.str() ) ); 
+	    return;	      
+	  }
+	}
+	else{
+	  std::stringstream ss;
+	  ss << "[SummaryPlotXmlParser::" << __func__ << "]"
+	     << " Unable to find any "<<runTypeTag_<<" nodes!"
+	     << " Empty xml run-type block?";
+	  throw( std::runtime_error( ss.str() ) ); 
+	  return;
+	}	
+      }
+    }
+    else{
+      std::stringstream ss;
+	ss << "[SummaryPlotXmlParser::" << __func__ << "]"
+	   << " Did not find \"" << rootTag_ << "\" tag! " 
+	   << " Tag name is "
+	   << rootTag_;
+	throw( std::runtime_error( ss.str() ) );
+	return;
+    }
+  }
+}
+
+// -----------------------------------------------------------------------------
+//
+std::ostream& operator<< ( std::ostream& os, const SummaryPlotXmlParser& parser ) {
   std::stringstream ss;
   parser.print(ss);
   os << ss.str();
@@ -41,21 +108,23 @@ std::ostream& operator<<(std::ostream& os, const SummaryPlotXmlParser& parser) {
 }
 
 // -----------------------------------------------------------------------------
-//
-void SummaryPlotXmlParser::print(std::stringstream& ss) const {
-  ss << "[SummaryPlotXmlParser::SummaryPlot::" << __func__ << "]"
+// 
+void SummaryPlotXmlParser::print( std::stringstream& ss ) const {
+  ss << "[SummaryPlotXmlParser::SummaryPlot::" << __func__ << "]" 
      << " Dumping contents of parsed XML file: " << std::endl;
   using namespace sistrip;
   typedef std::vector<SummaryPlot> Plots;
-  std::map<RunType, Plots>::const_iterator irun = plots_.begin();
-  for (; irun != plots_.end(); irun++) {
-    ss << " RunType=\"" << SiStripEnumsAndStrings::runType(irun->first) << "\"" << std::endl;
-    if (irun->second.empty()) {
+  std::map<RunType,Plots>::const_iterator irun = plots_.begin();
+  for ( ; irun != plots_.end(); irun++ ) {
+    ss << " RunType=\"" 
+       << SiStripEnumsAndStrings::runType( irun->first )
+       << "\"" << std::endl;
+    if ( irun->second.empty() ) {
       ss << " No summary plots for this RunType!";
     } else {
       Plots::const_iterator iplot = irun->second.begin();
-      for (; iplot != irun->second.end(); iplot++) {
-        ss << *iplot << std::endl;
+      for ( ; iplot != irun->second.end(); iplot++ ) {
+	ss << *iplot << std::endl;
       }
     }
   }
