1df752ab04af526f838c86e05a1ff1bb7daada8c
Massimo Casarsa <massimo.casarsa@ts.infn.it>
2020-02-06 17:41:03 +0100

    Removed the RECO code of the obsolete simple FTL device

--- RecoLocalFastTime/Configuration/python/RecoLocalFastTime_EventContent_cff.py
+++ RecoLocalFastTime/Configuration/python/RecoLocalFastTime_EventContent_cff.py
@@ -3,8 +3,6 @@ import FWCore.ParameterSet.Config as cms
 #FEVT
 RecoLocalFastTimeFEVT = cms.PSet(
     outputCommands = cms.untracked.vstring(
-        'keep *_ftlUncalibratedRecHits_*_*',
-        'keep *_ftlRecHits_*_*',        
         'keep *_mtdUncalibratedRecHits_*_*',
         'keep *_mtdRecHits_*_*',
         'keep *_mtdClusters_*_*',
@@ -14,7 +12,6 @@ RecoLocalFastTimeFEVT = cms.PSet(
 #RECO content
 RecoLocalFastTimeRECO = cms.PSet(
     outputCommands = cms.untracked.vstring(
-        'keep *_ftlRecHits_*_*',
         'keep *_mtdRecHits_*_*', 
         'keep *_mtdClusters_*_*',       
         )
