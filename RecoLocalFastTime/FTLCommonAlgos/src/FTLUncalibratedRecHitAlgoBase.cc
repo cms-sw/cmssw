e3e900221e5ad816a1d11b74510cdaab59d583c6
Massimo Casarsa <massimo.casarsa@ts.infn.it>
2020-02-06 17:41:03 +0100

    Removed the RECO code of the obsolete simple FTL device

--- RecoLocalFastTime/FTLCommonAlgos/src/FTLUncalibratedRecHitAlgoBase.cc
+++
@@ -1,3 +0,0 @@
-#include "RecoLocalFastTime/FTLCommonAlgos/interface/FTLUncalibratedRecHitAlgoBase.h"
-
-EDM_REGISTER_PLUGINFACTORY(FTLUncalibratedRecHitAlgoFactory, "FTLUncalibratedRecHitAlgoFactory");
