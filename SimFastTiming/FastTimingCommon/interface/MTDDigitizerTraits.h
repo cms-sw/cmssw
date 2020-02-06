f97befd11f651ab51cc325b699ab42bd6c2cfc28
Massimo Casarsa <massimo.casarsa@ts.infn.it>
2020-02-06 15:05:57 +0100

    Removed the BTL tile device and the simple FTL device

--- SimFastTiming/FastTimingCommon/interface/MTDDigitizerTraits.h
+++ SimFastTiming/FastTimingCommon/interface/MTDDigitizerTraits.h
@@ -3,23 +3,10 @@
 
 #include "DataFormats/FTLDigi/interface/FTLDigiCollections.h"
 
-#include "SimFastTiming/FastTimingCommon/interface/BTLTileDeviceSim.h"
 #include "SimFastTiming/FastTimingCommon/interface/BTLBarDeviceSim.h"
 #include "SimFastTiming/FastTimingCommon/interface/BTLElectronicsSim.h"
 #include "SimFastTiming/FastTimingCommon/interface/ETLDeviceSim.h"
 #include "SimFastTiming/FastTimingCommon/interface/ETLElectronicsSim.h"
-
-class BTLTileDigitizerTraits {
-public:
-  // --- The BTL DIGI collection
-  typedef BTLDigiCollection DigiCollection;
-
-  // --- The BTL sensor response simulation for the tile geometry
-  typedef BTLTileDeviceSim DeviceSim;
-
-  // --- The BTL electronics simulation
-  typedef BTLElectronicsSim ElectronicsSim;
-};
 
 class BTLBarDigitizerTraits {
 public:
