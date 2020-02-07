9c4ade4bae78a28430d4c38dd298ee6b27c6c6e2
Patrick Gartung <gartung@fnal.gov>
2020-02-07 13:35:12 -0600

    Code formatting

--- CondFormats/Serialization/interface/eos/portable_iarchive.hpp
+++ CondFormats/Serialization/interface/eos/portable_iarchive.hpp
@@ -456,23 +456,23 @@ BOOST_SERIALIZATION_REGISTER_ARCHIVE(eos::polymorphic_portable_iarchive)
 // define NO_EXPLICIT_TEMPLATE_INSTANTIATION before every include but one
 // or you move the instantiation section into an implementation file
 // #ifndef NO_EXPLICIT_TEMPLATE_INSTANTIATION
-// 
+//
 // #include <boost/archive/impl/basic_binary_iarchive.ipp>
 // #include <boost/archive/impl/basic_binary_iprimitive.ipp>
-// 
+//
 // #if BOOST_VERSION < 104000
 // #include <boost/archive/impl/archive_pointer_iserializer.ipp>
 // #elif !defined BOOST_ARCHIVE_SERIALIZER_INCLUDED
 // #include <boost/archive/impl/archive_serializer_map.ipp>
 // #define BOOST_ARCHIVE_SERIALIZER_INCLUDED
 // #endif
-// 
+//
 // namespace boost {
 //   namespace archive {
-// 
+//
 //     // explicitly instantiate for this type of binary stream
 //     template class basic_binary_iarchive<eos::portable_iarchive>;
-// 
+//
 //     template class basic_binary_iprimitive<eos::portable_iarchive
 // #if BOOST_VERSION < 103400
 //                                            ,
@@ -483,15 +483,15 @@ BOOST_SERIALIZATION_REGISTER_ARCHIVE(eos::polymorphic_portable_iarchive)
 //                                            std::istream::traits_type
 // #endif
 //                                            >;
-// 
+//
 // #if BOOST_VERSION < 104000
 //     template class detail::archive_pointer_iserializer<eos::portable_iarchive>;
 // #else
 //     template class detail::archive_serializer_map<eos::portable_iarchive>;
 //     //template class detail::archive_serializer_map<eos::polymorphic_portable_iarchive>;
 // #endif
-// 
+//
 //   }  // namespace archive
 // }  // namespace boost
-// 
+//
 // #endif
