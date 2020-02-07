79ac48c526249dd86bd317eafe66ffe4e3e7ecf5
Cms Build <cmsbuild@cern.ch>
2020-02-07 15:08:34 +0100

    Clang-Format

--- RecoEgamma/EgammaMCTools/interface/ElectronMCTruth.h
+++ RecoEgamma/EgammaMCTools/interface/ElectronMCTruth.h
@@ -16,37 +16,32 @@
 */
 
 class ElectronMCTruth {
+public:
+  ElectronMCTruth();
+  ElectronMCTruth(CLHEP::HepLorentzVector& v,
+                  int vertIndex,
+                  std::vector<CLHEP::Hep3Vector>& bremPos,
+                  std::vector<CLHEP::HepLorentzVector>& pBrem,
+                  std::vector<float>& xbrem,
+                  CLHEP::HepLorentzVector& pV,
+                  SimTrack& eTrack);
 
-  public:
-    ElectronMCTruth();
-    ElectronMCTruth( CLHEP::HepLorentzVector&  v, 
-		     int vertIndex,
-		     std::vector<CLHEP::Hep3Vector>& bremPos, 
-		     std::vector<CLHEP::HepLorentzVector>& pBrem, 
-		     std::vector<float>& xbrem, 
-		     CLHEP::HepLorentzVector& pV,  
-		     SimTrack& eTrack );
+  CLHEP::HepLorentzVector fourMomentum() const { return theElectron_; }
+  CLHEP::HepLorentzVector primaryVertex() const { return thePrimaryVertex_; }
+  std::vector<CLHEP::Hep3Vector> bremVertices() const { return theBremPosition_; }
+  std::vector<CLHEP::HepLorentzVector> bremMomentum() const { return theBremMomentum_; }
+  std::vector<float> eloss() const { return theELoss_; }
+  SimTrack simTracks() const { return eTrack_; }
+  int vertexInd() const { return theVertexIndex_; }
 
-
-    CLHEP::HepLorentzVector fourMomentum() const {return theElectron_;} 
-    CLHEP::HepLorentzVector primaryVertex() const {return thePrimaryVertex_;} 
-    std::vector<CLHEP::Hep3Vector> bremVertices() const { return theBremPosition_;}
-    std::vector<CLHEP::HepLorentzVector> bremMomentum() const { return theBremMomentum_;}
-    std::vector<float>  eloss() const {return theELoss_;}
-    SimTrack simTracks() const {return eTrack_;}     
-    int vertexInd() const {return theVertexIndex_;}
-
- private:
-    CLHEP::HepLorentzVector theElectron_;
-    int theVertexIndex_;
-    std::vector<CLHEP::Hep3Vector> theBremPosition_;
-    std::vector<CLHEP::HepLorentzVector> theBremMomentum_;
-    std::vector<float> theELoss_;
-    CLHEP::HepLorentzVector thePrimaryVertex_;
-    SimTrack eTrack_;
-
-
-
+private:
+  CLHEP::HepLorentzVector theElectron_;
+  int theVertexIndex_;
+  std::vector<CLHEP::Hep3Vector> theBremPosition_;
+  std::vector<CLHEP::HepLorentzVector> theBremMomentum_;
+  std::vector<float> theELoss_;
+  CLHEP::HepLorentzVector thePrimaryVertex_;
+  SimTrack eTrack_;
 };
 
 #endif
