c7beb24d8033a79b0eb25ae68f7b0da570f472ea
Sergio Lo Meo <sergio.lomeo@cern.ch>
2020-02-11 14:33:47 +0100

    test again

--- Geometry/MuonNumbering/src/MuonBaseNumber.cc
+++ Geometry/MuonNumbering/src/MuonBaseNumber.cc
@@ -1,7 +1,7 @@
 #include "Geometry/MuonNumbering/interface/MuonBaseNumber.h"
 #include "FWCore/MessageLogger/interface/MessageLogger.h"
 
-//#define LOCAL_DEBUG
+#define LOCAL_DEBUG
 
 void MuonBaseNumber::addBase(const LevelBaseNumber& num) {
   basenumber_type::iterator cur = sortedBaseNumber.begin();
@@ -29,7 +29,7 @@ void MuonBaseNumber::addBase(const LevelBaseNumber& num) {
   }
   sortedBaseNumber.insert(cur, num);
 
-  //#ifdef LOCAL_DEBUG
+  #ifdef LOCAL_DEBUG
   cur = sortedBaseNumber.begin();
   end = sortedBaseNumber.end();
   edm::LogVerbatim("Geometry") << "MuonBaseNumber::AddBase ";
@@ -39,7 +39,7 @@ void MuonBaseNumber::addBase(const LevelBaseNumber& num) {
     cur++;
     ++k;
   }
-  //#endif
+  #endif
 }
 
 void MuonBaseNumber::addBase(const int level, const int super, const int base) {
