420317894dd9e586f8af3f0eba9b0dc9efa32916
Duong <nhduongvn@gmail.com>
2020-02-13 13:16:43 -0600

    fix style

--- IOPool/Input/src/RootSecondaryFileSequence.cc
+++ IOPool/Input/src/RootSecondaryFileSequence.cc
@@ -26,12 +26,10 @@ namespace edm {
         orderedProcessHistoryIDs_(),
         enablePrefetching_(false),
         enforceGUIDInFileName_(pset.getUntrackedParameter<bool>("enforceGUIDInFileName")),
-        useMultipleDataCatalogs_(false) { 
+        useMultipleDataCatalogs_(false) {
     if (useMultipleDataCatalogs_ && !catalog.hasMultipleDataCatalogs()) {
       LogWarning("RootPrimaryFileSequence")
-          << "Want to use multiple data catalogs, but they are not available. Please, change the setting in "
-             "InputFileCatalog (setUseMultipleDataCatalogs = true). I use the default setting for now, ie. a primary "
-             "and fallback data catalogs.\n";
+          << "Want to use multiple data catalogs, but they are not available. Please, change the setting in InputFileCatalog (setUseMultipleDataCatalogs = true). I use the default setting for now, ie. a primary and fallback data catalogs.\n";
       useMultipleDataCatalogs_ = false;
     }
 
