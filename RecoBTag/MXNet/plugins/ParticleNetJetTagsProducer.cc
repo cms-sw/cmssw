4ef37a3a775f474da203ce247c365c79c314954b
Huilin Qu <huilin.qu@cern.ch>
2020-02-13 18:54:24 +0100

    Apply code checks.

--- RecoBTag/MXNet/plugins/ParticleNetJetTagsProducer.cc
+++ RecoBTag/MXNet/plugins/ParticleNetJetTagsProducer.cc
@@ -148,10 +148,10 @@ void ParticleNetJetTagsProducer::fillDescriptions(edm::ConfigurationDescriptions
   edm::ParameterSetDescription preprocessParams;
   preprocessParams.setAllowAnything();
   desc.add<edm::ParameterSetDescription>("preprocessParams", preprocessParams);
-  desc.add<edm::FileInPath>("model_path",
-                            edm::FileInPath("RecoBTag/Combined/data/ParticleNetAK8/General/V00/ParticleNet-symbol.json"));
-  desc.add<edm::FileInPath>("param_path",
-                            edm::FileInPath("RecoBTag/Combined/data/ParticleNetAK8/General/V00/ParticleNet-0000.params"));
+  desc.add<edm::FileInPath>(
+      "model_path", edm::FileInPath("RecoBTag/Combined/data/ParticleNetAK8/General/V00/ParticleNet-symbol.json"));
+  desc.add<edm::FileInPath>(
+      "param_path", edm::FileInPath("RecoBTag/Combined/data/ParticleNetAK8/General/V00/ParticleNet-0000.params"));
   desc.add<std::vector<std::string>>(
       "flav_names",
       std::vector<std::string>{
