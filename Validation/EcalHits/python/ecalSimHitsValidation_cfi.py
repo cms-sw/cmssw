93870ecce0437fcc4e9ec9dcfc896fd866a9800b
Matti Kortelainen <matti.kortelainen@cern.ch>
2020-02-13 22:29:57 +0100

    Migrate EcalSimHitsValidation to DQMEDAnalyzer

--- Validation/EcalHits/python/ecalSimHitsValidation_cfi.py
+++ Validation/EcalHits/python/ecalSimHitsValidation_cfi.py
@@ -1,8 +1,8 @@
 import FWCore.ParameterSet.Config as cms
 
-ecalSimHitsValidation = cms.EDAnalyzer("EcalSimHitsValidation",
+from DQMServices.Core.DQMEDAnalyzer import DQMEDAnalyzer
+ecalSimHitsValidation = DQMEDAnalyzer("EcalSimHitsValidation",
     ESHitsCollection = cms.string('EcalHitsES'),
-    outputFile = cms.untracked.string(''),
     verbose = cms.untracked.bool(False),
     moduleLabelMC = cms.string('generatorSmeared'),
     EBHitsCollection = cms.string('EcalHitsEB'),
