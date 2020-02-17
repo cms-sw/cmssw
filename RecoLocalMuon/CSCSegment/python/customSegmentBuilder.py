96b53cab9e6403480e169b74d514a6b9d9e916eb
nvoytish <nvoytish@cern.ch>
2020-02-18 00:07:33 +0300

    shortened the code

--- RecoLocalMuon/CSCSegment/python/customSegmentBuilder.py
+++ RecoLocalMuon/CSCSegment/python/customSegmentBuilder.py
@@ -2,12 +2,7 @@ import FWCore.ParameterSet.Config as cms
 
 def widenRoads(process):
    if hasattr(process,'cscSegments'):
-      process.cscSegments.algo_psets[4].algo_psets[0].enlarge = cms.bool(True)
-      process.cscSegments.algo_psets[4].algo_psets[1].enlarge = cms.bool(True)
-      process.cscSegments.algo_psets[4].algo_psets[2].enlarge = cms.bool(True)
-      process.cscSegments.algo_psets[4].algo_psets[3].enlarge = cms.bool(True)
-      process.cscSegments.algo_psets[4].algo_psets[4].enlarge = cms.bool(True)
-      process.cscSegments.algo_psets[4].algo_psets[5].enlarge = cms.bool(True)
-
+      for ps in process.cscSegments.algo_psets[4].algo_psets: ps.enlarge = True
+     
       return process
 
