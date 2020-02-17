9776ca4f20171b6cd84500aca4e31e429e15b770
nvoytish <nvoytish@cern.ch>
2020-02-18 00:06:24 +0300

    type specifications dropped

--- RecoLocalMuon/CSCRecHitD/python/customRecHitBuilder.py
+++ RecoLocalMuon/CSCRecHitD/python/customRecHitBuilder.py
@@ -2,7 +2,7 @@ import FWCore.ParameterSet.Config as cms
 
 def tightenAnodeTimes(process):
     if hasattr(process,'csc2DRecHits'):
-        process.csc2DRecHits.CSCUseReducedWireTimeWindow = cms.bool(True)
-        process.csc2DRecHits.CSCWireTimeWindowLow = cms.int32(5)
-        process.csc2DRecHits.CSCWireTimeWindowHigh = cms.int32(11)
+        process.csc2DRecHits.CSCUseReducedWireTimeWindow = True
+        process.csc2DRecHits.CSCWireTimeWindowLow = 5
+        process.csc2DRecHits.CSCWireTimeWindowHigh = 11
         return process
