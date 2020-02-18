90541a3b3d9a0f5ccd58c4fdc1b4fa33400dfb9b
nvoytish <nvoytish@cern.ch>
2020-02-18 20:34:04 +0300

    Update customSegmentBuilder.py

--- RecoLocalMuon/CSCSegment/python/customSegmentBuilder.py
+++ RecoLocalMuon/CSCSegment/python/customSegmentBuilder.py
@@ -2,4 +2,5 @@ import FWCore.ParameterSet.Config as cms
 
 def widenRoads(process):
    if hasattr(process,'cscSegments'):
-      for ps in process.cscSegments.algo_psets[4].algo_psets: ps.enlarge = True     
+      for ps in process.cscSegments.algo_psets[4].algo_psets: ps.enlarge = True   
+      return process
