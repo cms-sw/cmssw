553f783be107b5dbca91929328f9901c49abe627
Vladimir <Vladimir.Ivantchenko@cern.ch>
2020-02-20 18:43:33 +0100

    update phys_lists

--- SimG4Core/PhysicsLists/interface/CMSHadronPhysicsFTFP_BERT.h
+++ SimG4Core/PhysicsLists/interface/CMSHadronPhysicsFTFP_BERT.h
@@ -1,65 +1,37 @@
-//---------------------------------------------------------------------------
-// Author: Vladimir Ivanchenko
-// Date:   March 2018
 //
-// Hadron physics for the new CMS physics list FTFP_BERT_EMM_TRK.
-// The hadron physics of FTFP_BERT has the transition between Bertini
-// (BERT) intra-nuclear cascade model and Fritiof (FTF) string model in the
-// energy region [4, 5] GeV (instead of the default for Geant4 10.4).
-//---------------------------------------------------------------------------
-//
-#ifndef SimG4Core_PhysicsLists_CMSHadronPhysicsFTFP_BERT_h
-#define SimG4Core_PhysicsLists_CMSHadronPhysicsFTFP_BERT_h
+#ifndef CMSHadronPhysicsFTFP_BERT_h
+#define CMSHadronPhysicsFTFP_BERT_h 1
 
 #include "globals.hh"
 #include "G4ios.hh"
 
 #include "G4VPhysicsConstructor.hh"
 
-#include "G4Cache.hh"
-
-class G4ComponentGGHadronNucleusXsc;
-class G4VCrossSectionDataSet;
-
-class CMSHadronPhysicsFTFP_BERT : public G4VPhysicsConstructor {
-public:
-  explicit CMSHadronPhysicsFTFP_BERT(G4int verbose = 1);
+class CMSHadronPhysicsFTFP_BERT : public G4VPhysicsConstructor
+{
+public: 
+  explicit CMSHadronPhysicsFTFP_BERT(G4int verb);
+  explicit CMSHadronPhysicsFTFP_BERT(G4double e1, G4double e2);
   ~CMSHadronPhysicsFTFP_BERT() override;
-
+ 
   void ConstructParticle() override;
-  //This will call in order:
-  // DumpBanner (for master)
-  // CreateModels
-  // ExtraConfiguation
   void ConstructProcess() override;
 
-  void TerminateWorker() override;
-
-protected:
-  G4bool QuasiElastic;
+private:
   //This calls the specific ones for the different particles in order
-  virtual void CreateModels();
-  virtual void Neutron();
-  virtual void Proton();
-  virtual void Pion();
-  virtual void Kaon();
-  virtual void Others();
-  virtual void DumpBanner();
+  void CreateModels();
+  void Neutron();
+  void Proton();
+  void Pion();
+  void Kaon();
+  void Others();
+  void DumpBanner();
   //This contains extra configurataion specific to this PL
-  virtual void ExtraConfiguration();
+  void ExtraConfiguration();
 
-  G4double minFTFP_pion;
-  G4double maxBERT_pion;
-  G4double minFTFP_kaon;
-  G4double maxBERT_kaon;
-  G4double minFTFP_proton;
-  G4double maxBERT_proton;
-  G4double minFTFP_neutron;
-  G4double maxBERT_neutron;
-
-  //Thread-private data write them here to delete them
-  G4VectorCache<G4VCrossSectionDataSet*> xs_ds;
-  G4Cache<G4ComponentGGHadronNucleusXsc*> xs_k;
+  G4double minFTFP_;
+  G4double maxBERT_;
 };
 
 #endif
+
