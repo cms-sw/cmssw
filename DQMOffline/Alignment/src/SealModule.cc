8ef8899ee131e5fcc73232309a81c61737ed3856
Javier <Javier.Fernandez@cern.ch>
2020-02-21 13:08:09 +0100

    Deleting DQM MuonAlignment

--- DQMOffline/Alignment/src/SealModule.cc
+++ DQMOffline/Alignment/src/SealModule.cc
@@ -1,9 +1,3 @@
 #include "FWCore/Framework/interface/MakerMacros.h"
 #include "FWCore/PluginManager/interface/ModuleDef.h"
 
-// the clients
-#include "DQMOffline/Alignment/interface/MuonAlignment.h"
-#include "DQMOffline/Alignment/interface/MuonAlignmentSummary.h"
-
-DEFINE_FWK_MODULE(MuonAlignment);
-DEFINE_FWK_MODULE(MuonAlignmentSummary);
