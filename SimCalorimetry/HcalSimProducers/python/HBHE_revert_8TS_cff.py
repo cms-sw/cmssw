fe604c17c98c5079630d00193c969d94091f0cd2
Salavat <abdullin@mail.cern.ch>
2020-02-25 09:05:38 +0100

    minor editing

--- SimCalorimetry/HcalSimProducers/python/HBHE_revert_8TS_cff.py
+++ SimCalorimetry/HcalSimProducers/python/HBHE_revert_8TS_cff.py
@@ -6,6 +6,6 @@ import FWCore.ParameterSet.Config as cms
 def customise(process):
     process.mix.digitizers.hcal.hb.readoutFrameSize = 8
     process.mix.digitizers.hcal.he.readoutFrameSize = 8
-    process.mix.digitizers.hcal.hb.binOfMaximum = cms.int32(4)
-    process.mix.digitizers.hcal.he.binOfMaximum = cms.int32(4)
+    process.mix.digitizers.hcal.hb.binOfMaximum = 4
+    process.mix.digitizers.hcal.he.binOfMaximum = 4
     return(process)
