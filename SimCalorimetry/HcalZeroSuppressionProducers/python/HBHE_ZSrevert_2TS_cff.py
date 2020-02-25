58e9725e487e25bb307a50363725c0c7df8effe8
Salavat <abdullin@mail.cern.ch>
2020-02-25 13:46:17 +0100

    renaming customisation snippets

--- SimCalorimetry/HcalZeroSuppressionProducers/python/HBHE_ZSrevert_2TS_cff.py
+++ SimCalorimetry/HcalZeroSuppressionProducers/python/HBHE_ZSrevert_2TS_cff.py
@@ -3,7 +3,7 @@ import FWCore.ParameterSet.Config as cms
 #--- reset HB/HE ZS to 2TS 
 #--- NB: may need appropriate HcalZSThresholds update
   
-def customise(process):
+def customise_2TS(process):
     process.simHcalDigis.HBregion = (2,5)
     process.simHcalDigis.HEregion = (2,5)
     process.simHcalDigis.use1ts = False
