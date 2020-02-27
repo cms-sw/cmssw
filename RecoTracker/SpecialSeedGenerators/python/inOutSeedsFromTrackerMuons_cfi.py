3c44d6e9d6f07598ffa9b3517d31d1161fc4ea8c
Jordi Duarte Campderros <Jordi.Duarte.Campderros@cern.ch>
2020-02-28 00:05:26 +0100

    Remove unnecessary fixme comments

--- RecoTracker/SpecialSeedGenerators/python/inOutSeedsFromTrackerMuons_cfi.py
+++ RecoTracker/SpecialSeedGenerators/python/inOutSeedsFromTrackerMuons_cfi.py
@@ -23,4 +23,4 @@ inOutSeedsFromTrackerMuons = cms.EDProducer("MuonReSeeder",
 )
 
 from Configuration.ProcessModifiers.run4_PixelCPEGeneric_cff import run4_PixelCPEGeneric
-run4_PixelCPEGeneric.toModify(inOutSeedsFromTrackerMuons, TrackerRecHitBuilder = 'WithTrackAngle') # FIXME
+run4_PixelCPEGeneric.toModify(inOutSeedsFromTrackerMuons, TrackerRecHitBuilder = 'WithTrackAngle')
