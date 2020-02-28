663d37957024efad193f3b378b1ebb3673d506b0
Matti Kortelainen <matti.kortelainen@cern.ch>
2020-02-28 23:26:52 +0100

    Rename numberOfCUDADevices() to cms::cuda::numberOfDevices

--- HeterogeneousCore/CUDAServices/src/numberOfCUDADevices.cc
+++
@@ -1,8 +0,0 @@
-#include "HeterogeneousCore/CUDAServices/interface/numberOfCUDADevices.h"
-#include "HeterogeneousCore/CUDAServices/interface/CUDAService.h"
-#include "FWCore/ServiceRegistry/interface/Service.h"
-
-int numberOfCUDADevices() {
-  edm::Service<CUDAService> cs;
-  return cs->enabled() ? cs->numberOfDevices() : 0;
-}
