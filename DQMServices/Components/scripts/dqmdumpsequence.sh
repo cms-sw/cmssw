0ff8305ec3e56bd931605926ac92f127e610fc3d
Marcel Andre Schneider <marcel.andre.schneider@cern.ch>
2020-03-02 17:59:15 +0100

    Merge some logic from edmCheckMultithreading into dqmdumpsequence.

--- DQMServices/Components/scripts/dqmdumpsequence.sh
+++ DQMServices/Components/scripts/dqmdumpsequence.sh
@@ -3,14 +3,23 @@ SEQUENCE=""
 ERA="Run2_2018"
 SCENARIO="pp"
 STEP="DQM"
+ANALYZE=""
 
 while [[ $# -gt 0 ]]; do
   case $1 in
     --help)
-    echo "Usage: $0 [ --era ERA ] [ --scenario SCENARIO ] [ --step STEP ] { SEQUENCNAME | @AUTODQMSEQUENCE }"
+    echo "Usage: $0 [--analyze-modules] [ --era ERA ] [ --scenario SCENARIO ] [ --step STEP ] { SEQUENCNAME | @AUTODQMSEQUENCE }"
     echo "STEP can be 'DQM', 'VALIDATION' or 'HARVESTING' (others may or may not work) (default '$STEP')"
     echo "SCENARIO can be pp, cosmics, or HeavyIons (default '$SCENARIO')"
     echo "ERA can be any era (default '$ERA')"
+    echo "If --analyze-modules is passed, run the modules through the equivalent of edmCheckMultithreading."
+    echo
+    echo "Example:"
+    cat <<'EOF'
+    # Analyze modules on all DQM and VALIDATION sequences.
+    for seq in $(cat Validation/Configuration/python/autoValidation.py | grep -oE "'\w+' ?:" | tr -d "':" | sort | uniq); do (mkdir -p validation/$seq; cd validation/$seq; dqmdumpsequence.sh --analyze-modules --step VALIDATION @$seq > list ) & done
+    for seq in $(cat DQMOffline/Configuration/python/autoDQM.py | grep -oE "'\w+' ?:" | tr -d "':" | sort | uniq); do (mkdir -p dqm/$seq; cd dqm/$seq; dqmdumpsequence.sh --analyze-modules --step DQM @$seq > list ) & done
+EOF
     exit ;;
     --era)
     ERA="$2"; shift 2 ;;
@@ -18,6 +27,8 @@ while [[ $# -gt 0 ]]; do
     SCENARIO="$2"; shift 2 ;;
     --step)
     STEP="$2"; shift 2 ;;
+    --analyze-modules)
+    ANALYZE="yes"; shift ;;
     --*)
     echo "Option '$1' not recognized." 1>&2
     exit 1 ;;
@@ -60,8 +71,67 @@ echo Running cmsDriver.py "${cmsdriverargs[@]}" 1>&2
 
 # Finally run cmssw and select the modules names out of the tracer output.
 # Remove some modules that are there for technical reasons but did not come from the DQM sequences.
+
 # Sorted output for easier diff'ing, ordering should not matter anyways.
-cmsDriver.py "${cmsdriverargs[@]}"  2>&1 \
+cmsDriver.py --python_filename cmssw_cfg.py "${cmsdriverargs[@]}" --no_exec 1>&2
+cmsRun cmssw_cfg.py 2>&1 \
   | grep "++++ starting: constructing module with label" | grep -oE "'[^']*'" | tr -d "'" \
   | grep -vE "$BLACKLIST" \
-  | sort
+  | sort | tee modlist
+
+if [[ -z $ANALYZE ]]; then
+  exit 0
+fi
+
+# with the --analyze-modules option, we now run a tuned edmCheckMultithreading on the list of modules.
+# The hard part is that we have module labels, but we need module class names. So, we still need to 
+# run edmConfigDump (as edmCheckMultithreading usually does), but filter down the output using our 
+# list of modules.
+
+# use colors only if the output is sent to a terminal
+if [ -t 1 ] ; then
+  SETCOLOR_SUCCESS='\e[0;32m'
+  SETCOLOR_WARNING='\e[1;33m'
+  SETCOLOR_FAILURE='\e[0;31m'
+  SETCOLOR_NORMAL_='\e[0m'
+else
+  SETCOLOR_SUCCESS=''
+  SETCOLOR_WARNING=''
+  SETCOLOR_FAILURE=''
+  SETCOLOR_NORMAL_=''
+fi
+
+edmConfigDump cmssw_cfg.py | fgrep -f modlist | grep 'cms\.EDAnalyzer\|cms\.EDFilter\|cms\.EDProducer\|cms\.OutputModule' | cut -d'"' -f2 | sort -u | while read MODULE
+do
+  edmPluginHelp -p $MODULE | {
+    read N CLASS TYPE P
+    read
+    if [ ! "$CLASS" ]; then
+      FILL="--"
+      TYPE="--"
+      MT="--"
+      ED="--"
+    else
+      if grep -q "not implemented the function"; then
+        FILL="no"
+        SETCOLOR_FILL="$SETCOLOR_WARNING"
+      else
+        FILL="yes"
+        SETCOLOR_FILL="$SETCOLOR_SUCCESS"
+      fi
+      TYPE=`echo $TYPE | sed -e's/[()]//g'`
+      MT=`echo $TYPE | cut -s -d: -f1`
+      ED=`echo $TYPE | cut -s -d: -f3`
+      if ! [ "$MT" ]; then
+        MT="legacy"
+        ED="$TYPE"
+        SETCOLOR_MT="$SETCOLOR_FAILURE"
+      elif [ "$MT" == "one" ]; then
+        SETCOLOR_MT="$SETCOLOR_WARNING"
+      else
+        SETCOLOR_MT="$SETCOLOR_SUCCESS"
+      fi
+    fi
+    printf "%-64s%-16s%b%-16s%b%s$SETCOLOR_NORMAL_\n" "$MODULE" "$ED" "$SETCOLOR_MT" "$MT" "$SETCOLOR_FILL" "$FILL"
+  }
+done
