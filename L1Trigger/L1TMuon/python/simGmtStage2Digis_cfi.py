a17631ebae6e927b941306a17c80fd06ac9aeb71
Dinyar Rabady <dinyar@cern.ch>
2020-03-05 17:14:54 +0100

    Enabled switching of cancel-out algo based on FW version

--- L1Trigger/L1TMuon/python/simGmtStage2Digis_cfi.py
+++ L1Trigger/L1TMuon/python/simGmtStage2Digis_cfi.py
@@ -15,7 +15,7 @@ simGmtStage2Digis = cms.EDProducer('L1TMuonProducer',
     autoBxRange = cms.bool(True), # if True the output BX range is calculated from the inputs and 'bxMin' and 'bxMax' are ignored
     bxMin = cms.int32(-2),
     bxMax = cms.int32(2),
-    autoCancelMode = cms.bool(False), # if True the cancel out methods are configured depending on the FW version number and 'bmtfCancelMode'&'emtfCancelMode' are ignored
+    autoCancelMode = cms.bool(True), # if True the cancel out methods are configured depending on the FW version number and 'bmtfCancelMode'&'emtfCancelMode' are ignored
     bmtfCancelMode = cms.string("kftracks"), # 'tracks' or 'kftracks' (when using the Run-3 BMTF)
     emtfCancelMode = cms.string("coordinate") # 'tracks' or 'coordinate'
 )
