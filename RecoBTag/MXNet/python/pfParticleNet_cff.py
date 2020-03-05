3c9ce7f25fce7f9fd68393f8480cd84f9600774d
Huilin Qu <huilin.qu@cern.ch>
2020-03-05 02:06:42 +0100

    Merge TagInfo producers.

--- RecoBTag/MXNet/python/pfParticleNet_cff.py
+++ RecoBTag/MXNet/python/pfParticleNet_cff.py
@@ -1,10 +1,14 @@
 import FWCore.ParameterSet.Config as cms
 
-from RecoBTag.FeatureTools.pfParticleNetTagInfos_cfi import pfParticleNetTagInfos
+from RecoBTag.FeatureTools.pfDeepBoostedJetTagInfos_cfi import pfDeepBoostedJetTagInfos
 from RecoBTag.MXNet.boostedJetMXNetJetTagsProducer_cfi import boostedJetMXNetJetTagsProducer
 from RecoBTag.MXNet.Parameters.ParticleNet.V00.pfParticleNetPreprocessParams_cfi import pfParticleNetPreprocessParams, pfMassDecorrelatedParticleNetPreprocessParams
 from RecoBTag.MXNet.pfParticleNetDiscriminatorsJetTags_cfi import pfParticleNetDiscriminatorsJetTags
 from RecoBTag.MXNet.pfMassDecorrelatedParticleNetDiscriminatorsJetTags_cfi import pfMassDecorrelatedParticleNetDiscriminatorsJetTags
+
+pfParticleNetTagInfos = pfDeepBoostedJetTagInfos.clone(
+    use_puppiP4 = False
+)
 
 pfParticleNetJetTags = boostedJetMXNetJetTagsProducer.clone(
     preprocessParams = pfParticleNetPreprocessParams,
