349b7a46b868a745b923e9aba874a88ee26dd8f6
Dinyar Rabady <dinyar@cern.ch>
2020-03-05 15:37:05 +0100

    Unpackers for BMTF, uGMT, and uGT adapted for Kalman muons

--- L1Trigger/L1TMuon/interface/MuonRawDigiTranslator.h
+++ L1Trigger/L1TMuon/interface/MuonRawDigiTranslator.h
@@ -6,30 +6,44 @@
 namespace l1t {
   class MuonRawDigiTranslator {
   public:
-    static void fillMuon(Muon&, uint32_t, uint32_t, int, unsigned int);
-    static void fillMuon(Muon&, uint64_t, int, unsigned int);
+    static void fillMuon(Muon&, uint32_t, uint32_t, uint32_t, int, unsigned int, int, bool);
+    static void fillMuon(Muon&, uint32_t, uint64_t, int, unsigned int, int, bool);
+    static void fillMuonCoordinates2016(Muon& mu, uint32_t raw_data_00_31, uint32_t raw_data_32_63);
+    static void fillMuonCoordinatesFrom2017(Muon& mu, uint32_t raw_data_00_31, uint32_t raw_data_32_63);
+    static void fillMuonCoordinatesRun3(
+        Muon& mu, uint32_t raw_data_spare, uint32_t raw_data_00_31, uint32_t raw_data_32_63, int muInBx);
+    static void fillIntermediateMuonCoordinatesRun3(Muon& mu, uint32_t raw_data_00_31, uint32_t raw_data_32_63);
     static void generatePackedDataWords(const Muon&, uint32_t&, uint32_t&);
     static uint64_t generate64bitDataWord(const Muon&);
     static int calcHwEta(const uint32_t&, const unsigned, const unsigned);
 
-    static const unsigned ptMask_ = 0x1FF;
-    static const unsigned ptShift_ = 10;
-    static const unsigned qualMask_ = 0xF;
-    static const unsigned qualShift_ = 19;
-    static const unsigned absEtaMask_ = 0xFF;
-    static const unsigned absEtaShift_ = 21;
-    static const unsigned absEtaAtVtxShift_ = 23;
-    static const unsigned etaSignShift_ = 29;
-    static const unsigned etaAtVtxSignShift_ = 31;
-    static const unsigned phiMask_ = 0x3FF;
-    static const unsigned phiShift_ = 11;
-    static const unsigned phiAtVtxShift_ = 0;
-    static const unsigned chargeShift_ = 2;
-    static const unsigned chargeValidShift_ = 3;
-    static const unsigned tfMuonIndexMask_ = 0x7F;
-    static const unsigned tfMuonIndexShift_ = 4;
-    static const unsigned isoMask_ = 0x3;
-    static const unsigned isoShift_ = 0;
+    static constexpr unsigned ptMask_ = 0x1FF;
+    static constexpr unsigned ptShift_ = 10;
+    static constexpr unsigned qualMask_ = 0xF;
+    static constexpr unsigned qualShift_ = 19;
+    static constexpr unsigned absEtaMask_ = 0xFF;
+    static constexpr unsigned absEtaShift_ = 21;
+    static constexpr unsigned absEtaAtVtxShift_ = 23;
+    static constexpr unsigned etaSignShift_ = 29;
+    static constexpr unsigned etaAtVtxSignShift_ = 31;
+    static constexpr unsigned phiMask_ = 0x3FF;
+    static constexpr unsigned phiShift_ = 11;
+    static constexpr unsigned phiAtVtxShift_ = 0;
+    static constexpr unsigned chargeShift_ = 2;
+    static constexpr unsigned chargeValidShift_ = 3;
+    static constexpr unsigned tfMuonIndexMask_ = 0x7F;
+    static constexpr unsigned tfMuonIndexShift_ = 4;
+    static constexpr unsigned isoMask_ = 0x3;
+    static constexpr unsigned isoShift_ = 0;
+    static constexpr unsigned dxyMask_ = 0x3;
+    static constexpr unsigned dxyShift_ = 30;
+    static constexpr unsigned ptUnconstrainedMask_ = 0xFF;
+    static constexpr unsigned ptUnconstrainedShift_ = 21;
+    static constexpr unsigned ptUnconstrainedIntermedidateShift_ = 0;
+    static constexpr unsigned absEtaMu1Shift_ = 12;   // For Run-3
+    static constexpr unsigned etaMu1SignShift_ = 20;  // For Run-3
+    static constexpr unsigned absEtaMu2Shift_ = 21;   // For Run-3
+    static constexpr unsigned etaMu2SignShift_ = 29;  // For Run-3
   };
 }  // namespace l1t
 
