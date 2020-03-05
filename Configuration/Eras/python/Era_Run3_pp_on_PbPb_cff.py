d57669695c83bfe071e1d424c5fb2d43f1ddf690
Matthew <Matthew.Nguyen@cern.ch>
2020-03-05 22:43:40 +0100

    Changed era name to pp_on_PbPb

--- Configuration/Eras/python/Era_Run3_pp_on_PbPb_cff.py
+++ Configuration/Eras/python/Era_Run3_pp_on_PbPb_cff.py
@@ -2,6 +2,6 @@ import FWCore.ParameterSet.Config as cms
 
 from Configuration.Eras.Era_Run3_cff import Run3
 from Configuration.Eras.Modifier_pp_on_AA_2018_cff import pp_on_AA_2018
-from Configuration.Eras.Modifier_pbpb_run3_cff import pbpb_run3
+from Configuration.Eras.Modifier_pp_on_PbPb_run3_cff import pp_on_PbPb_run3
 
-Run3_PbPb = cms.ModifierChain(Run3, pp_on_AA_2018, pbpb_run3)
+Run3_pp_on_PbPb = cms.ModifierChain(Run3, pp_on_AA_2018, pp_on_PbPb_run3)
