912d2151c3f7a29e19ce2645e66bf7699bc8a4c0
Kevin Pedro <kpedro88@gmail.com>
2020-03-06 16:16:33 -0600

    code format

--- HeterogeneousCore/SonicCore/interface/SonicClientTypes.h
+++ HeterogeneousCore/SonicCore/interface/SonicClientTypes.h
@@ -4,24 +4,24 @@
 #include "FWCore/ParameterSet/interface/ParameterSet.h"
 
 //this base class exists to limit the impact of dependent scope in derived classes
-template <typename InputT, typename OutputT=InputT>
+template <typename InputT, typename OutputT = InputT>
 class SonicClientTypes {
-	public:
-		//typedefs for outside accessibility
-		typedef InputT Input;
-		typedef OutputT Output;
-		//destructor
-		virtual ~SonicClientTypes() {}
+public:
+  //typedefs for outside accessibility
+  typedef InputT Input;
+  typedef OutputT Output;
+  //destructor
+  virtual ~SonicClientTypes() {}
 
-		//accessors
-		Input& input() { return input_; }
-		const Input& input() const { return input_; }
-		void setInput(const Input& inp) { input_ = inp; }
-		const Output& output() const { return output_; }
+  //accessors
+  Input& input() { return input_; }
+  const Input& input() const { return input_; }
+  void setInput(const Input& inp) { input_ = inp; }
+  const Output& output() const { return output_; }
 
-	protected:
-		Input input_;
-		Output output_;
+protected:
+  Input input_;
+  Output output_;
 };
 
 #endif
