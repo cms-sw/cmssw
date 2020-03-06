d856212b71cd77520da9337b6b0af8a8cb57bf83
Laurent Forthomme <laurent.forthomme@cern.ch>
2020-03-06 12:21:29 +0100

    DF: More members to the digi definition

--- DataFormats/TotemReco/interface/TotemT2Digi.h
+++ DataFormats/TotemReco/interface/TotemT2Digi.h
@@ -13,11 +13,28 @@ class TotemT2Digi {
 public:
   explicit TotemT2Digi() = default;
 
+  void setLeadingEdge(unsigned int le) { lead_edge_ = le; }
+  unsigned int leadingEdge() const { return lead_edge_; }
+  void setTrailingEdge(unsigned int te) { trail_edge_ = te; }
+  unsigned int trailingEdge() const { return trail_edge_; }
+
 private:
+  /// Leading edge time
+  unsigned int lead_edge_;
+  /// Trailing edge time
+  unsigned int trail_edge_;
 };
 
-bool operator<(const TotemT2Digi&, const TotemT2Digi&) {
-  return true;
+bool operator<(const TotemT2Digi& lhs, const TotemT2Digi& rhs) {
+  if (lhs.leadingEdge() < rhs.leadingEdge())
+    return true;
+  if (lhs.leadingEdge() > rhs.leadingEdge())
+    return false;
+  if (lhs.trailingEdge() < rhs.trailingEdge())
+    return true;
+  if (lhs.trailingEdge() > rhs.trailingEdge())
+    return false;
+  return false;
 }
 
 #endif
