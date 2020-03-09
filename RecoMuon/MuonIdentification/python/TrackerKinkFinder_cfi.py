06e72424bfeae9f090690bdc34e95a0997746979
mmusich <marco.musich@cern.ch>
2020-03-09 11:54:59 +0100

    Revert "introduce run4_PixelCPEGeneric modifier and use it where appropriate"
    This reverts commit 50230fccb39ef043752300f68e4538131df63ba7.

--- RecoMuon/MuonIdentification/python/TrackerKinkFinder_cfi.py
+++ RecoMuon/MuonIdentification/python/TrackerKinkFinder_cfi.py
@@ -19,5 +19,3 @@ TrackerKinkFinderParametersBlock = cms.PSet(
     )
 )
 
-from Configuration.ProcessModifiers.run4_PixelCPEGeneric_cff import run4_PixelCPEGeneric
-run4_PixelCPEGeneric.toModify(TrackerKinkFinderParametersBlock, TrackerKinkFinderParameters = dict(TrackerRecHitBuilder = 'WithTrackAngle'))
