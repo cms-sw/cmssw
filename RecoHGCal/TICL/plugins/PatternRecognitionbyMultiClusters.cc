60794a3dc01e993c84da4bc95d816b8d350cebca
Felice <felice.pantaleo@cern.ch>
2020-03-10 19:23:46 +0100

    Use unordered_map instead of map for seedToTracksterAssociation

--- RecoHGCal/TICL/plugins/PatternRecognitionbyMultiClusters.cc
+++ RecoHGCal/TICL/plugins/PatternRecognitionbyMultiClusters.cc
@@ -2,6 +2,6 @@
 #include "FWCore/MessageLogger/interface/MessageLogger.h"
 
 void ticl::PatternRecognitionbyMultiClusters::makeTracksters(const PatternRecognitionAlgoBase::Inputs& input,
-                                                             std::vector<Trackster>& result,  std::map<int, std::vector<int>>& seedToTracksterAssociation) {
+                                                             std::vector<Trackster>& result,  std::unordered_map<int, std::vector<int>>& seedToTracksterAssociation) {
   LogDebug("HGCPatterRecoTrackster") << "making Tracksters" << std::endl;
 }
