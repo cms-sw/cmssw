0400cce58cd428090539a8cd7879019670a5d1c4
Ian J. Watson <ian.james.watson@cern.ch>
2020-03-11 16:15:06 +0900

    Update Run3 GEM to use the eMap from the DB

--- EventFilter/GEMRawToDigi/python/muonGEMDigis_cfi.py
+++ EventFilter/GEMRawToDigi/python/muonGEMDigis_cfi.py
@@ -3,4 +3,7 @@ from EventFilter.GEMRawToDigi.muonGEMDigisDefault_cfi import muonGEMDigisDefault
 muonGEMDigis = _muonGEMDigisDefault.clone()
 
 from Configuration.Eras.Modifier_run2_GEM_2017_cff import run2_GEM_2017
+from Configuration.Eras.Modifier_run3_GEM_cff import run3_GEM
+
 run2_GEM_2017.toModify(muonGEMDigis, useDBEMap = True)
+run3_GEM.toModify(muonGEMDigis, useDBEMap = True)
