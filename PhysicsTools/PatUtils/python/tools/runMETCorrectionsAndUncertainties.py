30a1dacc9faed3cda91ee5d30e14a3895771c7dd
Andreas Hinzmann <hinzmann@cern.ch>
2020-03-12 22:44:43 +0100

    python3

--- PhysicsTools/PatUtils/python/tools/runMETCorrectionsAndUncertainties.py
+++ PhysicsTools/PatUtils/python/tools/runMETCorrectionsAndUncertainties.py
@@ -1336,8 +1336,8 @@ class RunMETCorrectionsAndUncertainties(ConfigToolBase):
             genJetsCollection=cms.InputTag("slimmedGenJets")
 
         if self._parameters["Puppi"].value:
-	    getattr(process, "patSmearedJets"+postfix).algo = 'AK4PFPuppi'
-	    getattr(process, "patSmearedJets"+postfix).algopt = 'AK4PFPuppi_pt'
+            getattr(process, "patSmearedJets"+postfix).algo = 'AK4PFPuppi'
+            getattr(process, "patSmearedJets"+postfix).algopt = 'AK4PFPuppi_pt'
 
         if "PF" == self._parameters["metType"].value:
             smearedJetModule = getattr(process, "patSmearedJets"+postfix).clone(
