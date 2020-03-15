b6bb1f53cf509fc7fe19e2e017de89ec3cca4437
Ianna Osborne <ianna.osborne@cern.ch>
2020-03-15 18:29:07 +0100

    Add more debug info and speed it up a bit

--- DetectorDescription/DDCMS/test/Filter.cppunit.cc
+++ DetectorDescription/DDCMS/test/Filter.cppunit.cc
@@ -44,7 +44,7 @@ void testFilter::setUp() {
     vector<string_view> toks = split(i, "/");
     unique_ptr<Filter> f = nullptr;
     auto const& filter = find_if(begin(filters_), end(filters_), [&](auto const& f) {
-      auto const& k = find(begin(f->keys), end(f->keys), toks.front());
+      auto const& k = find_if(begin(f->keys), end(f->keys), [&](auto const& p) { return p.first == toks.front(); });
       if (k != end(f->keys)) {
         currentFilter = f.get();
         return true;
@@ -52,19 +52,27 @@ void testFilter::setUp() {
       return false;
     });
     if (filter == end(filters_)) {
-      filters_.emplace_back(unique_ptr<Filter>(new Filter{{toks.front()}, nullptr, nullptr}));
+      filters_.emplace_back(unique_ptr<Filter>(
+           new Filter{{std::pair<std::string_view, std::regex>(
+                          toks.front(), regex(std::string(toks.front().data(), toks.front().size())))},
+                      nullptr,
+                      nullptr}));
       currentFilter = filters_.back().get();
     }
     // all next levels
     for (size_t pos = 1; pos < toks.size(); ++pos) {
       if (currentFilter->next != nullptr) {
         currentFilter = currentFilter->next.get();
-        auto const& l = find(begin(currentFilter->keys), end(currentFilter->keys), toks[pos]);
+	auto const& l = find_if(
+             begin(currentFilter->keys), end(currentFilter->keys), [&](auto const& p) { return p.first == toks[pos]; });
         if (l == end(currentFilter->keys)) {
-          currentFilter->keys.emplace_back(toks[pos]);
+	  currentFilter->keys.emplace_back(toks[pos], regex(std::string(toks.front().data(), toks.front().size())));
         }
       } else {
-        currentFilter->next.reset(new Filter{{toks[pos]}, nullptr, currentFilter});
+	currentFilter->next.reset(new Filter{
+             {std::pair<std::string_view, std::regex>(toks[pos], regex({toks[pos].data(), toks[pos].size()}))},
+             nullptr,
+             currentFilter});
       }
     }
   }
@@ -72,7 +80,7 @@ void testFilter::setUp() {
 
 void testFilter::checkFilter() {
   string_view name = "MB2P.*"sv;
-  CPPUNIT_ASSERT(filters_.front()->keys.front() == name);
+  CPPUNIT_ASSERT(filters_.front()->keys.front().first == name);
   CPPUNIT_ASSERT(filters_.front()->up == nullptr);
   CPPUNIT_ASSERT(filters_.front()->next != nullptr);
   CPPUNIT_ASSERT(filters_.size() == 1);
@@ -90,7 +98,7 @@ void testFilter::checkFilter() {
 
 void testFilter::print(Filter* filter) {
   for (auto const& it : filter->keys) {
-    cout << it << " ";
+    cout << it.first << " ";
   }
   cout << "\n";
 }
