2b4136d7a8a9accad2713a296ab2f903c4a3a319
Fabio Cossutti <fabio.cossutti@ts.infn.it>
2020-03-19 19:35:06 +0100

    Add a new topology mode for TDR ETL

--- Geometry/MTDCommonData/interface/MTDTopologyMode.h
+++ Geometry/MTDCommonData/interface/MTDTopologyMode.h
@@ -8,7 +8,7 @@
 
 namespace MTDTopologyMode {
 
-  enum class Mode { undefined = 0, tile = 1, bar = 2, barzflat = 3, barphiflat = 4 };
+  enum class Mode { undefined = 0, tile = 1, bar = 2, barzflat = 3, barphiflat = 4, btlv1etlv4 = 5 };
 
   Mode MTDStringToEnumParser(const std::string&);
 
