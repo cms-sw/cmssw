b5ec5eab82160fc61519901d181d711e6752742d
Mircho Rodozov <mrodozov@cern.ch>
2020-03-19 18:03:20 +0200

    remove "the program result" from the program :/
    I thought that was part of the example

--- PhysicsTools/MXNet/test/test_mxnet.py
+++ PhysicsTools/MXNet/test/test_mxnet.py
@@ -1,8 +1,5 @@
 #!/usr/bin/env python
 import mxnet as mx
-import numpy
 a = mx.nd.ones((2, 3))
 b = a * 2 + 1
 b.asnumpy()
-numpy.array([[ 3.,  3.,  3.],
-       [ 3.,  3.,  3.]], dtype=numpy.float32)
