cebba8dd7930f91f3b23ccc795d264368f61f514
Jia Fu Low <jia.fu.low@cern.ch>
2020-03-20 11:57:13 -0400

    [EMTF] Update EMTFSubsystemTag & EMTFSubsystemCollector 1/2

--- L1Trigger/L1TMuonEndCap/interface/Common.h
+++ L1Trigger/L1TMuonEndCap/interface/Common.h
@@ -1,7 +1,10 @@
 #ifndef L1TMuonEndCap_Common_h
 #define L1TMuonEndCap_Common_h
 
-#include "DataFormats/L1TMuon/interface/CPPFDigi.h"
+#include <array>
+
+#include "FWCore/MessageLogger/interface/MessageLogger.h"
+
 #include "DataFormats/L1TMuon/interface/EMTFHit.h"
 #include "DataFormats/L1TMuon/interface/EMTFRoad.h"
 #include "DataFormats/L1TMuon/interface/EMTFTrack.h"
@@ -11,10 +14,6 @@
 #include "L1Trigger/L1TMuon/interface/MuonTriggerPrimitiveFwd.h"
 
 #include "L1Trigger/L1TMuonEndCap/interface/EMTFSubsystemTag.h"
-
-#include "FWCore/MessageLogger/interface/MessageLogger.h"
-
-#include <array>
 
 // Resolve namespaces
 
@@ -30,16 +29,11 @@ typedef L1TMuon::GeometryTranslator GeometryTranslator;
 typedef L1TMuon::TriggerPrimitive TriggerPrimitive;
 typedef L1TMuon::TriggerPrimitiveCollection TriggerPrimitiveCollection;
 
+typedef TriggerPrimitive::DTData DTData;
 typedef TriggerPrimitive::CSCData CSCData;
 typedef TriggerPrimitive::RPCData RPCData;
 typedef TriggerPrimitive::GEMData GEMData;
-
-typedef emtf::CSCTag CSCTag;
-typedef emtf::RPCTag RPCTag;
-typedef emtf::CPPFTag CPPFTag;
-typedef emtf::GEMTag GEMTag;
-typedef emtf::IRPCTag IRPCTag;
-typedef emtf::ME0Tag ME0Tag;
+typedef TriggerPrimitive::ME0Data ME0Data;
 
 namespace emtf {
 
--- L1Trigger/L1TMuonEndCap/interface/EMTFSubsystemTag.h
+++ L1Trigger/L1TMuonEndCap/interface/EMTFSubsystemTag.h
@@ -1,10 +1,18 @@
 #ifndef L1TMuonEndCap_EMTFSubsystemTag_h
 #define L1TMuonEndCap_EMTFSubsystemTag_h
 
+#include "DataFormats/L1DTTrackFinder/interface/L1MuDTChambPhDigi.h"
+#include "DataFormats/L1DTTrackFinder/interface/L1MuDTChambPhContainer.h"
+#include "DataFormats/L1DTTrackFinder/interface/L1MuDTChambThDigi.h"
+#include "DataFormats/L1DTTrackFinder/interface/L1MuDTChambThContainer.h"
 #include "DataFormats/CSCDigi/interface/CSCCorrelatedLCTDigi.h"
 #include "DataFormats/CSCDigi/interface/CSCCorrelatedLCTDigiCollection.h"
+#include "DataFormats/CSCDigi/interface/CSCComparatorDigi.h"
+#include "DataFormats/CSCDigi/interface/CSCComparatorDigiCollection.h"
 #include "DataFormats/RPCDigi/interface/RPCDigi.h"
 #include "DataFormats/RPCDigi/interface/RPCDigiCollection.h"
+#include "DataFormats/RPCRecHit/interface/RPCRecHit.h"
+#include "DataFormats/RPCRecHit/interface/RPCRecHitCollection.h"
 #include "DataFormats/L1TMuon/interface/CPPFDigi.h"
 #include "DataFormats/GEMDigi/interface/GEMPadDigiCluster.h"
 #include "DataFormats/GEMDigi/interface/GEMPadDigiClusterCollection.h"
@@ -13,14 +21,32 @@
 
 namespace emtf {
 
+  struct DTTag {
+    typedef L1MuDTChambPhDigi digi_type;
+    typedef L1MuDTChambPhContainer digi_collection;
+    typedef L1MuDTChambThDigi theta_digi_type;
+    typedef L1MuDTChambThContainer theta_digi_collection;
+  };
+
   struct CSCTag {
     typedef CSCCorrelatedLCTDigi digi_type;
     typedef CSCCorrelatedLCTDigiCollection digi_collection;
+    typedef CSCComparatorDigi comparator_digi_type;
+    typedef CSCComparatorDigiCollection comparator_digi_collection;
   };
 
   struct RPCTag {
     typedef RPCDigi digi_type;
     typedef RPCDigiCollection digi_collection;
+    typedef RPCRecHit rechit_type;
+    typedef RPCRecHitCollection rechit_collection;
+  };
+
+  struct IRPCTag {
+    typedef RPCDigi digi_type;
+    typedef RPCDigiCollection digi_collection;
+    typedef RPCRecHit rechit_type;
+    typedef RPCRecHitCollection rechit_collection;
   };
 
   struct CPPFTag {
@@ -31,11 +57,6 @@ namespace emtf {
   struct GEMTag {
     typedef GEMPadDigiCluster digi_type;
     typedef GEMPadDigiClusterCollection digi_collection;
-  };
-
-  struct IRPCTag {
-    typedef RPCDigi digi_type;
-    typedef RPCDigiCollection digi_collection;
   };
 
   struct ME0Tag {
