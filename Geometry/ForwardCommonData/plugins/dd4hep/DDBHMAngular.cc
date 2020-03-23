3a8ede84fbab592880bbcd69b70603b6852ab14d
Sunanda <sunanda.banerjee@cern.ch>
2020-03-23 19:02:40 +0100

    Take Carl's comments

--- Geometry/ForwardCommonData/plugins/dd4hep/DDBHMAngular.cc
+++ Geometry/ForwardCommonData/plugins/dd4hep/DDBHMAngular.cc
@@ -34,28 +34,26 @@ static long algorithm(dd4hep::Detector& /* description */,
   if (rotstr != "NULL") {
     rot = ns.rotation(rotMat);
   }
-
+  static const double fac1 = 0.5;
+  static const double fac2 = 1.5;
+  static const double fac3 = 14.5;
+  static const double fac4 = 15.5;
   for (int jj = 0; jj < units; jj++) {
     double driverX(0), driverY(0), driverZ(0);
     if (jj < 16) {
-      driverX = rr * cos((jj + 0.5) * dphi);
+      driverX = rr * cos((jj + fac1) * dphi);
       driverY = sqrt(rr * rr - driverX * driverX);
     } else if (jj == 16) {
-      driverX = rr * cos(15.5 * dphi);
+      driverX = rr * cos(fac4 * dphi);
       driverY = -sqrt(rr * rr - driverX * driverX);
     } else if (jj == 17) {
-      driverX = rr * cos(14.5 * dphi);
+      driverX = rr * cos(fac3 * dphi);
       driverY = -sqrt(rr * rr - driverX * driverX);
     } else if (jj == 18) {
-      driverX = rr * cos(0.5 * dphi);
-    } else if (jj == 17) {
-      driverX = rr * cos(14.5 * dphi);
-      driverY = -sqrt(rr * rr - driverX * driverX);
-    } else if (jj == 18) {
-      driverX = rr * cos(0.5 * dphi);
+      driverX = rr * cos(fac1 * dphi);
       driverY = -sqrt(rr * rr - driverX * driverX);
     } else if (jj == 19) {
-      driverX = rr * cos(1.5 * dphi);
+      driverX = rr * cos(fac2 * dphi);
       driverY = -sqrt(rr * rr - driverX * driverX);
     }
     dd4hep::Position tran(driverX, driverY, driverZ);
