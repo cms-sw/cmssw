30c20a0bd4836bc55cd5f471785d6d22f9d99be6
Ian Tomalin <ian.tomalin@stfc.ac.uk>
2020-03-23 18:36:37 +0000

    Bug fix hopefully identical to master

--- L1Trigger/TrackFindingTracklet/interface/HybridFit.h
+++ L1Trigger/TrackFindingTracklet/interface/HybridFit.h
@@ -34,9 +34,9 @@ class HybridFit{
     void Fit(Tracklet* tracklet, std::vector<std::pair<Stub*,L1TStub*>> &trackstublist){
       if (fakefit){
         tracklet->setFitPars(tracklet->rinvapprox(),tracklet->phi0approx(),tracklet->d0approx(),tracklet->tapprox(),
-        tracklet->z0approx(),0.,tracklet->rinv(),tracklet->phi0(),tracklet->d0(),tracklet->t(),tracklet->z0(),0.,
+			     tracklet->z0approx(),0.,0.,tracklet->rinv(),tracklet->phi0(),tracklet->d0(),tracklet->t(),tracklet->z0(),0.,0.,
         tracklet->fpgarinv().value(),tracklet->fpgaphi0().value(),tracklet->fpgad0().value(),tracklet->fpgat().value(),
-        tracklet->fpgaz0().value(),0);
+			     tracklet->fpgaz0().value(),0,0,0);
         return;
       }
 
