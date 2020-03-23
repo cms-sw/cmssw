0d64e8a04cae21d644c8d6f9e921de75fbf8baf0
Paul Lujan <paul.lujan@cern.ch>
2020-03-23 11:56:40 +0100

    send messages properly to MessageLogger

--- RecoLuminosity/LumiProducer/plugins/LumiProducerFromBrilcalc.cc
+++ RecoLuminosity/LumiProducer/plugins/LumiProducerFromBrilcalc.cc
@@ -2,7 +2,7 @@
 //
 // Package:    RecoLuminosity/LumiProducer
 // Class:      LumiProducerFromBrilcalc
-// 
+//
 /**\class LumiProducerFromBrilcalc LumiProducerFromBrilcalc.cc RecoLuminosity/LumiProducer/plugins/LumiProducerFromBrilcalc.cc
 
    Description: Takes a csv file with luminosity information produced by brilcalc and reads it into the event.
@@ -16,12 +16,10 @@
 //
 //
 
-
 // system include files
 #include <memory>
 #include <sstream>
 #include <fstream>
-#include <iostream>
 
 // user include files
 #include "FWCore/Framework/interface/Frameworkfwd.h"
@@ -32,6 +30,8 @@
 
 #include "FWCore/ParameterSet/interface/ParameterSet.h"
 #include "FWCore/Utilities/interface/StreamID.h"
+
+#include "FWCore/MessageLogger/interface/MessageLogger.h"
 
 #include "DataFormats/Luminosity/interface/LumiInfo.h"
 
@@ -66,7 +66,6 @@ private:
 // constants, enums and typedefs
 //
 
-
 //
 // static data member definitions
 //
@@ -74,47 +73,51 @@ private:
 //
 // constructors and destructor
 //
-LumiProducerFromBrilcalc::LumiProducerFromBrilcalc(const edm::ParameterSet& iConfig) :
-  lumiFile_(iConfig.getUntrackedParameter<std::string>("lumiFile")),
-  throwIfNotFound_(iConfig.getUntrackedParameter<bool>("throwIfNotFound", false)),
-  doBunchByBunch_(iConfig.getUntrackedParameter<bool>("doBunchByBunch", false)) {
+LumiProducerFromBrilcalc::LumiProducerFromBrilcalc(const edm::ParameterSet& iConfig)
+    : lumiFile_(iConfig.getUntrackedParameter<std::string>("lumiFile")),
+      throwIfNotFound_(iConfig.getUntrackedParameter<bool>("throwIfNotFound", false)),
+      doBunchByBunch_(iConfig.getUntrackedParameter<bool>("doBunchByBunch", false)) {
   //register your products
   produces<LumiInfo>("brilcalc");
 
   //now do what ever other initialization is needed
   if (doBunchByBunch_) {
-    throw cms::Exception("LumiProducerFromBrilcalc") << "Sorry, bunch-by-bunch luminosity is not yet supported. Please bug your friendly lumi expert!";
+    throw cms::Exception("LumiProducerFromBrilcalc")
+        << "Sorry, bunch-by-bunch luminosity is not yet supported. Please bug your friendly lumi expert!";
   }
 
   // Read luminosity data and store it in lumiData_.
-  std::cout << "Reading luminosity data from " << lumiFile_ << "...one moment..." << std::endl;
+  edm::LogInfo("LumiProducerFromBrilcalc") << "Reading luminosity data from " << lumiFile_ << "...one moment...";
   std::ifstream lumiFile(lumiFile_);
   if (!lumiFile.is_open()) {
     throw cms::Exception("LumiProducerFromBrilcalc") << "Failed to open input luminosity file " << lumiFile_;
   }
 
-  int nLS=0;
+  int nLS = 0;
   std::string line;
   while (1) {
     std::getline(lumiFile, line);
-    if (lumiFile.eof() || lumiFile.fail()) break;
-    if (line.empty()) continue; // skip blank lines
-    if (line.at(0) == '#') continue; // skip comment lines
-    
+    if (lumiFile.eof() || lumiFile.fail())
+      break;
+    if (line.empty())
+      continue;  // skip blank lines
+    if (line.at(0) == '#')
+      continue;  // skip comment lines
+
     // Break into fields. These should be, in order: run:fill, brills:cmsls, time, beam status, beam energy,
     // delivered lumi, recorded lumi, average pileup, source.
     std::stringstream ss(line);
     std::string field;
     std::vector<std::string> fields;
-    
+
     while (std::getline(ss, field, ','))
       fields.push_back(field);
-    
+
     if (fields.size() != 9) {
-      std::cout << "Malformed line in csv file: " << line << std::endl;
+      edm::LogWarning("LumiProducerFromBrilcalc") << "Malformed line in csv file: " << line;
       continue;
     }
-    
+
     // Extract the run number from fields[0] and the lumisection number from fields[1]. Fortunately since the
     // thing we want is before the colon, we don't have to split them again.
     int run, ls;
@@ -122,7 +125,7 @@ LumiProducerFromBrilcalc::LumiProducerFromBrilcalc(const edm::ParameterSet& iCon
     runfill >> run;
     std::stringstream lsls(fields[1]);
     lsls >> ls;
-    
+
     // Extract the delivered and recorded lumi from fields[5] and fields[6].
     float lumiDel, lumiRec, dtFrac;
     std::stringstream lumiDelString(fields[5]);
@@ -131,25 +134,20 @@ LumiProducerFromBrilcalc::LumiProducerFromBrilcalc(const edm::ParameterSet& iCon
     lumiRecString >> lumiRec;
 
     // Calculate the deadtime fraction
-    dtFrac = 1.0-lumiRec/lumiDel;
-
-    if (nLS < 20 || run==323511)
-      std::cout << run << " " << ls << " del " << lumiDel << " rec " << lumiRec << " dt " << dtFrac << std::endl;
+    dtFrac = 1.0 - lumiRec / lumiDel;
 
     // Finally, store it all.
     lumiData_[std::make_pair(run, ls)] = std::make_pair(lumiDel, dtFrac);
     nLS++;
   }
-  std::cout << "Read " << nLS << " lumisections from " << lumiFile_ << std::endl;
+  edm::LogInfo("LumiProducerFromBrilcalc") << "Read " << nLS << " lumisections from " << lumiFile_;
   lumiFile.close();
 }
-
 
 LumiProducerFromBrilcalc::~LumiProducerFromBrilcalc() {
   // do anything here that needs to be done at destruction time
   // (e.g. close files, deallocate resources etc.)
 }
-
 
 //
 // member functions
@@ -161,54 +159,48 @@ void LumiProducerFromBrilcalc::produce(edm::Event& iEvent, const edm::EventSetup
   std::pair<int, int> runls = std::make_pair(iEvent.run(), iEvent.luminosityBlock());
   if (lumiData_.count(runls) == 1) {
     // if we have data for this run/LS, put it in the event
-    std::cout << "filling for run " << runls.first << " ls " << runls.second << " with " << lumiData_[runls].second << " dt " << lumiData_[runls].first << std::endl;
+    LogDebug("LumiProducerFromBrilcalc") << "Filling for run " << runls.first << " ls " << runls.second << " with delivered "
+					 << lumiData_[runls].first << " dt " << lumiData_[runls].second;
     iEvent.put(std::make_unique<LumiInfo>(lumiData_[runls].second, bxlumi, lumiData_[runls].first), "brilcalc");
   } else {
     if (throwIfNotFound_) {
-      throw cms::Exception("LumiProducerFromBrilcalc") << "Failed to find luminosity for run " << runls.first << " LS " << runls.second;
+      throw cms::Exception("LumiProducerFromBrilcalc")
+          << "Failed to find luminosity for run " << runls.first << " LS " << runls.second;
     } else {
       // just put in zeroes
-      std::cout << "warning: failed to find luminosity for run " << runls.first << " ls " << runls.second << std::endl;
+      edm::LogWarning("LumiProducerFromBrilcalc") << "Failed to find luminosity for run " << runls.first << " ls " << runls.second;
       iEvent.put(std::make_unique<LumiInfo>(0, bxlumi, 0), "brilcalc");
     }
   }
 }
 
 // ------------ method called once each stream before processing any runs, lumis or events  ------------
-void
-LumiProducerFromBrilcalc::beginStream(edm::StreamID)
-{
-}
+void LumiProducerFromBrilcalc::beginStream(edm::StreamID) {}
 
 // ------------ method called once each stream after processing all runs, lumis and events  ------------
-void
-LumiProducerFromBrilcalc::endStream() {
-}
+void LumiProducerFromBrilcalc::endStream() {}
 
 // ------------ method called when starting to processes a run  ------------
-void
-LumiProducerFromBrilcalc::beginRun(edm::Run const&, edm::EventSetup const&)
-{
-}
- 
+void LumiProducerFromBrilcalc::beginRun(edm::Run const&, edm::EventSetup const&) {}
+
 // ------------ method called when ending the processing of a run  ------------
 /*
   void LumiProducerFromBrilcalc::endRun(edm::Run const&, edm::EventSetup const&) {
   }
 */
- 
+
 // ------------ method called when starting to processes a luminosity block  ------------
 /*
   void LumiProducerFromBrilcalc::beginLuminosityBlock(edm::LuminosityBlock const&, edm::EventSetup const&) {
   }
 */
- 
+
 // ------------ method called when ending the processing of a luminosity block  ------------
 /*
   void LumiProducerFromBrilcalc::endLuminosityBlock(edm::LuminosityBlock const&, edm::EventSetup const&) {
   }
 */
- 
+
 // ------------ method fills 'descriptions' with the allowed parameters for the module  ------------
 void LumiProducerFromBrilcalc::fillDescriptions(edm::ConfigurationDescriptions& descriptions) {
   // Allowed parameters
--- RecoLuminosity/LumiProducer/test/TestLumiProducerFromBrilcalc_cfg.py
+++ RecoLuminosity/LumiProducer/test/TestLumiProducerFromBrilcalc_cfg.py
@@ -2,19 +2,29 @@ import FWCore.ParameterSet.Config as cms
 
 process = cms.Process("TestLumiProducerFromBrilcalc")
 
+process.load("FWCore.MessageLogger.MessageLogger_cfi")
+process.MessageLogger = cms.Service("MessageLogger",
+                                    destinations   = cms.untracked.vstring("cout"),
+                                    categories     = cms.untracked.vstring("LumiProducerFromBrilcalc"),
+                                    debugModules   = cms.untracked.vstring("LumiInfo"),
+                                    cout           = cms.untracked.PSet(
+                                        threshold  = cms.untracked.string('DEBUG')
+                                    )
+)
+
 # just use a random relval which has meaningless run/LS numbers, and then a corresponding test file
 process.source = cms.Source("PoolSource",
                             fileNames = cms.untracked.vstring("/store/relval/CMSSW_10_2_7/RelValMinBias_13/GEN-SIM/102X_mc2017_realistic_v4_AC_v01-v1/20000/BB654FAE-5375-164F-BBFE-B330713759C6.root")
 )
 process.maxEvents = cms.untracked.PSet(input = cms.untracked.int32(10))
 
-process.LumiInfoFromBrilcalc = cms.EDProducer('LumiProducerFromBrilcalc',
-                                              lumiFile = cms.untracked.string("./testLumiFile.csv"),
-                                              throwIfNotFound = cms.untracked.bool(False),
-                                              doBunchByBunch = cms.untracked.bool(False))
+process.LumiInfo = cms.EDProducer('LumiProducerFromBrilcalc',
+                                  lumiFile = cms.untracked.string("./testLumiFile.csv"),
+                                  throwIfNotFound = cms.untracked.bool(False),
+                                  doBunchByBunch = cms.untracked.bool(False))
 
 process.test = cms.EDAnalyzer('TestLumiProducerFromBrilcalc',
-                              inputTag = cms.untracked.InputTag("LumiInfoFromBrilcalc", "brilcalc"))
+                              inputTag = cms.untracked.InputTag("LumiInfo", "brilcalc"))
 
-process.p = cms.Path(process.LumiInfoFromBrilcalc*process.test)
+process.p = cms.Path(process.LumiInfo*process.test)
 
