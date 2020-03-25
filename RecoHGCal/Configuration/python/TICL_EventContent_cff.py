17de4e6368e7392f67c0b4d43da911ab918e407c
Marco Rovere <marco.rovere@cern.ch>
2020-03-25 14:08:11 +0100

    Remove wildcards from Event-Content definition

--- RecoHGCal/Configuration/python/TICL_EventContent_cff.py
+++ RecoHGCal/Configuration/python/TICL_EventContent_cff.py
@@ -1,29 +1,35 @@
 import FWCore.ParameterSet.Config as cms
 
 
-# FEVT Content
-TICL_FEVT = cms.PSet(
-    outputCommands = cms.untracked.vstring(
-      'keep *_tracksters*_*_*',
-      'keep *_multiClustersFromTracksters*_*_*',
-      'keep *_ticlCandidateFromTrackstersProducer*_*_*',
-      'keep *_pfTICLProducer*_*_*'
-      )
-    )
-#RECO content
-TICL_RECO = cms.PSet(
-    outputCommands = cms.untracked.vstring(
-      'keep *_tracksters*_*_*',
-      'keep *_multiClustersFromTracksters*_*_*',
-      'keep *_ticlCandidateFromTrackstersProducer*_*_*',
-      'keep *_pfTICLProducer*_*_*'
-      )
-    )
-
 #AOD content
 TICL_AOD = cms.PSet(
     outputCommands = cms.untracked.vstring(
-      'keep *_multiClustersFromTracksters*_*_*'
+      'keep *_multiClustersFromTrackstersEM_*_*',
+      'keep *_multiClustersFromTrackstersHAD_*_*',
+      'keep *_multiClustersFromTrackstersTrk_*_*',
+      'keep *_multiClustersFromTrackstersMIP_*_*',
+      'keep *_multiClustersFromTrackstersMerge_*_*',
       )
     )
+
+#RECO content
+TICL_RECO = cms.PSet(
+    outputCommands = cms.untracked.vstring(
+      'keep *_trackstersEM_*_*',
+      'keep *_trackstersHAD_*_*',
+      'keep *_trackstersTrk_*_*',
+      'keep *_trackstersMIP_*_*',
+      'keep *_trackstersMerge_*_*',
+      'keep *_ticlCandidateFromTrackstersProducer_*_*',
+      'keep *_pfTICLProducer_*_*'
+      )
+    )
+TICL_RECO.outputCommands.extend(TICL_AOD.outputCommands)
+
+# FEVT Content
+TICL_FEVT = cms.PSet(
+    outputCommands = cms.untracked.vstring(
+      )
+    )
+TICL_FEVT.outputCommands.extend(TICL_RECO.outputCommands)
 
