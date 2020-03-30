3a0b17296c60a792e0d56736c8adff5288447bd6
Sunanda <sunanda.banerjee@cern.ch>
2020-03-30 19:53:27 +0200

    Update some of the scripts for H2 TB simularion

--- SimG4CMS/HcalTestBeam/test/python/run2006_37_cfg.py
+++ SimG4CMS/HcalTestBeam/test/python/run2006_37_cfg.py
@@ -133,6 +133,7 @@ process.g4SimHits.CaloSD = cms.PSet(
     HCNames        = cms.vstring('EcalHitsEB','EcalHitsEE','EcalHitsES','HcalHits'),
     UseResponseTables = cms.vint32(0,0,0,0),
     SuppressHeavy  = cms.bool(False),
+    UseFineCaloID  = cms.bool(False),
     CheckHits      = cms.untracked.int32(25),
     UseMap         = cms.untracked.bool(True),
     Verbosity      = cms.untracked.int32(0),
