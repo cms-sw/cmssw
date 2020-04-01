65f7b1ca41f55902c6b4a722919c2fd70c98fe56
Josh <Josh.Bendavid@cern.ch>
2020-04-01 18:25:48 +0200

    don't propagate cluster times to PFCandidates in SimPFProducer since this information is not currently consistent and makes downstream usage more error prone

--- RecoParticleFlow/PFSimProducer/plugins/SimPFProducer.cc
+++ RecoParticleFlow/PFSimProducer/plugins/SimPFProducer.cc
@@ -415,7 +415,6 @@ void SimPFProducer::produce(edm::StreamID, edm::Event& evt, const edm::EventSetu
         candidates->emplace_back(0, clu_p4, part_type);
         auto& candidate = candidates->back();
         candidate.addElementInBlock(blref, elem.index());
-        candidate.setTime(ref->time(), ref->timeError());
       }
     }
   }
