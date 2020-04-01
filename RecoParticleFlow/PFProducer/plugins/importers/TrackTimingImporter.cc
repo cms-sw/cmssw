01650f5007b251d48a8c1191e8b603779b56d3b1
Josh <Josh.Bendavid@cern.ch>
2020-04-01 11:44:24 +0200

    apply MTD-track association quality threshold in PFProducer

--- RecoParticleFlow/PFProducer/plugins/importers/TrackTimingImporter.cc
+++ RecoParticleFlow/PFProducer/plugins/importers/TrackTimingImporter.cc
@@ -17,16 +17,24 @@ class TrackTimingImporter : public BlockElementImporterBase {
 public:
   TrackTimingImporter(const edm::ParameterSet& conf, edm::ConsumesCollector& sumes)
       : BlockElementImporterBase(conf, sumes),
+        useTimeQuality_(conf.existsAs<edm::InputTag>("timeQualityMap")),
+        timeQualityThreshold_(useTimeQuality_ ? conf.getParameter<double>("timeQualityThreshold") : -99.),
         srcTime_(sumes.consumes<edm::ValueMap<float> >(conf.getParameter<edm::InputTag>("timeValueMap"))),
         srcTimeError_(sumes.consumes<edm::ValueMap<float> >(conf.getParameter<edm::InputTag>("timeErrorMap"))),
+        srcTimeQuality_(useTimeQuality_ ? sumes.consumes<edm::ValueMap<float> >(conf.getParameter<edm::InputTag>("timeQualityMap"))
+                          : edm::EDGetTokenT<edm::ValueMap<float>>()),
         srcTimeGsf_(sumes.consumes<edm::ValueMap<float> >(conf.getParameter<edm::InputTag>("timeValueMapGsf"))),
         srcTimeErrorGsf_(sumes.consumes<edm::ValueMap<float> >(conf.getParameter<edm::InputTag>("timeErrorMapGsf"))),
+        srcTimeQualityGsf_(useTimeQuality_ ? sumes.consumes<edm::ValueMap<float> >(conf.getParameter<edm::InputTag>("timeQualityMapGsf"))
+                          : edm::EDGetTokenT<edm::ValueMap<float>>()),
         debug_(conf.getUntrackedParameter<bool>("debug", false)) {}
 
   void importToBlock(const edm::Event&, ElementList&) const override;
 
 private:
-  edm::EDGetTokenT<edm::ValueMap<float> > srcTime_, srcTimeError_, srcTimeGsf_, srcTimeErrorGsf_;
+  const bool useTimeQuality_;
+  const double timeQualityThreshold_;
+  edm::EDGetTokenT<edm::ValueMap<float> > srcTime_, srcTimeError_, srcTimeQuality_, srcTimeGsf_, srcTimeErrorGsf_, srcTimeQualityGsf_;
   const bool debug_;
 };
 
@@ -39,12 +47,24 @@ void TrackTimingImporter::importToBlock(const edm::Event& e, BlockElementImporte
   auto const& timeErr = e.get(srcTimeError_);
   auto const& timeGsf = e.get(srcTimeGsf_);
   auto const& timeErrGsf = e.get(srcTimeErrorGsf_);
+  
+  edm::Handle<edm::ValueMap<float>> timeQualH, timeQualGsfH;
+  if (useTimeQuality_) {
+    e.getByToken(srcTimeQuality_, timeQualH);
+    e.getByToken(srcTimeQualityGsf_, timeQualGsfH);
+  }
 
   for (auto& elem : elems) {
     if (reco::PFBlockElement::TRACK == elem->type()) {
       const auto& ref = elem->trackRef();
       if (time.contains(ref.id())) {
-        elem->setTime(time[ref], timeErr[ref]);
+        const bool assocQuality = useTimeQuality_ ? (*timeQualH)[ref] > timeQualityThreshold_ : true;
+        if (assocQuality) {
+          elem->setTime(time[ref], timeErr[ref]);
+        }
+        else {
+          elem->setTime(0., -1.);
+        }
       }
       if (debug_) {
         edm::LogInfo("TrackTimingImporter")
@@ -54,7 +74,13 @@ void TrackTimingImporter::importToBlock(const edm::Event& e, BlockElementImporte
     } else if (reco::PFBlockElement::GSF == elem->type()) {
       const auto& ref = static_cast<const reco::PFBlockElementGsfTrack*>(elem.get())->GsftrackRef();
       if (timeGsf.contains(ref.id())) {
-        elem->setTime(timeGsf[ref], timeErrGsf[ref]);
+        const bool assocQuality = useTimeQuality_ ? (*timeQualGsfH)[ref] > timeQualityThreshold_ : true;
+        if (assocQuality) {
+          elem->setTime(timeGsf[ref], timeErrGsf[ref]);
+        }
+        else {
+          elem->setTime(0., -1.);
+        }
       }
       if (debug_) {
         edm::LogInfo("TrackTimingImporter")
