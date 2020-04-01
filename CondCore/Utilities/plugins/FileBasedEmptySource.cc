688a02c650c5f85b9d66722c38a33a1af391eb8d
Giacomo Govi <giacomo.govi@cern.ch>
2020-04-01 13:45:57 +0200

    Code-format repeated ( first time did not apply anything...)

--- CondCore/Utilities/plugins/FileBasedEmptySource.cc
+++ CondCore/Utilities/plugins/FileBasedEmptySource.cc
@@ -11,10 +11,14 @@ namespace cond {
     FileBasedEmptySource(edm::ParameterSet const&, edm::InputSourceDescription const&);
     ~FileBasedEmptySource() override;
     static void fillDescriptions(edm::ConfigurationDescriptions& descriptions);
+
   private:
-    void produce(edm::Event & e) override;
-    bool setRunAndEventInfo(edm::EventID& id, edm::TimeValue_t& time, edm::EventAuxiliary::ExperimentType& eType) override;
+    void produce(edm::Event& e) override;
+    bool setRunAndEventInfo(edm::EventID& id,
+                            edm::TimeValue_t& time,
+                            edm::EventAuxiliary::ExperimentType& eType) override;
     void initialize(edm::EventID& id, edm::TimeValue_t& time, edm::TimeValue_t& interval) override;
+
   private:
     unsigned int m_interval;
     unsigned long long m_eventId;
@@ -24,54 +28,52 @@ namespace cond {
     unsigned int m_currentLumi;
     boost::posix_time::ptime m_currentLumiTime;
   };
-}
+}  // namespace cond
 
 #include "FWCore/Utilities/interface/EDMException.h"
 #include "FWCore/ParameterSet/interface/ParameterSet.h"
 #include "FWCore/Framework/interface/IOVSyncValue.h"
 //#include "DataFormats/Provenance/interface/EventID.h"
 
-namespace cond{
-  //allowed parameters: firstRun, firstTime, lastRun, lastTime, 
+namespace cond {
+  //allowed parameters: firstRun, firstTime, lastRun, lastTime,
   //common paras: timetype,interval
-  FileBasedEmptySource::FileBasedEmptySource(edm::ParameterSet const& pset,
-					     edm::InputSourceDescription const& desc):
-    edm::ProducerSourceBase(pset,desc,true),
-    m_interval(  pset.getParameter<unsigned int>("interval") ),
-    m_eventId( 0 ),
-    m_eventsPerLumi( pset.getUntrackedParameter<unsigned int>("numberEventsInLuminosityBlock" ) ),
-    m_pathForLastLumiFile( pset.getParameter<std::string>("pathForLastLumiFile") ),
-    m_currentRun( 0 ),
-    m_currentLumi( 0 ),
-    m_currentLumiTime(){
-  }
+  FileBasedEmptySource::FileBasedEmptySource(edm::ParameterSet const& pset, edm::InputSourceDescription const& desc)
+      : edm::ProducerSourceBase(pset, desc, true),
+        m_interval(pset.getParameter<unsigned int>("interval")),
+        m_eventId(0),
+        m_eventsPerLumi(pset.getUntrackedParameter<unsigned int>("numberEventsInLuminosityBlock")),
+        m_pathForLastLumiFile(pset.getParameter<std::string>("pathForLastLumiFile")),
+        m_currentRun(0),
+        m_currentLumi(0),
+        m_currentLumiTime() {}
 
-  FileBasedEmptySource::~FileBasedEmptySource() {
-  }
+  FileBasedEmptySource::~FileBasedEmptySource() {}
 
-  void FileBasedEmptySource::produce( edm::Event & ) {
-  }  
+  void FileBasedEmptySource::produce(edm::Event&) {}
 
-  bool FileBasedEmptySource::setRunAndEventInfo(edm::EventID& id, edm::TimeValue_t& time, edm::EventAuxiliary::ExperimentType&){
+  bool FileBasedEmptySource::setRunAndEventInfo(edm::EventID& id,
+                                                edm::TimeValue_t& time,
+                                                edm::EventAuxiliary::ExperimentType&) {
     cond::Time_t lastLumi = cond::time::MIN_VAL;
     {
-      std::ifstream lastLumiFile( m_pathForLastLumiFile );
-      if( lastLumiFile) { 
-	lastLumiFile >> lastLumi;
+      std::ifstream lastLumiFile(m_pathForLastLumiFile);
+      if (lastLumiFile) {
+        lastLumiFile >> lastLumi;
       } else {
-	std::cout <<"Error: last lumi file can't be read."<<std::endl;
-	return false;
-      } 
+        std::cout << "Error: last lumi file can't be read." << std::endl;
+        return false;
+      }
     }
-    auto t = cond::time::unpack( lastLumi );
+    auto t = cond::time::unpack(lastLumi);
     unsigned int runId = t.first;
     unsigned int lumiId = t.second;
     //std::cout <<"###### setRunAndEventInfo Run: "<<runId<<" lumi: "<<lumiId<<std::endl;
     boost::posix_time::ptime now = boost::posix_time::microsec_clock::local_time();
-    if( runId == m_currentRun && lumiId == m_currentLumi ){
+    if (runId == m_currentRun && lumiId == m_currentLumi) {
       m_eventId += 1;
-      if( m_eventId >= m_eventsPerLumi ){
-	return false;
+      if (m_eventId >= m_eventsPerLumi) {
+        return false;
       }
     } else {
       m_currentRun = runId;
@@ -79,29 +81,30 @@ namespace cond{
       m_currentLumiTime = now;
       m_eventId = 1;
     }
-    std::cout <<"###### setRunAndEventInfo Run: "<<runId<<" lumi: "<<lumiId<<" event id: "<<m_eventId<<" time:"<<boost::posix_time::to_simple_string(now)<<std::endl;
+    std::cout << "###### setRunAndEventInfo Run: " << runId << " lumi: " << lumiId << " event id: " << m_eventId
+              << " time:" << boost::posix_time::to_simple_string(now) << std::endl;
     time = cond::time::from_boost(now);
     id = edm::EventID(runId, lumiId, m_eventId);
     usleep(20000);
     return true;
   }
 
-  void FileBasedEmptySource::initialize(edm::EventID& id, edm::TimeValue_t& time, edm::TimeValue_t& interval){
+  void FileBasedEmptySource::initialize(edm::EventID& id, edm::TimeValue_t& time, edm::TimeValue_t& interval) {
     cond::Time_t lastLumi = cond::time::MIN_VAL;
     {
-      std::ifstream lastLumiFile( m_pathForLastLumiFile );
-      if( lastLumiFile) { 
-	lastLumiFile >> lastLumi;
+      std::ifstream lastLumiFile(m_pathForLastLumiFile);
+      if (lastLumiFile) {
+        lastLumiFile >> lastLumi;
       } else {
-	std::cout <<"Error: last lumi file can't be read."<<std::endl;
-	return;
-      } 
+        std::cout << "Error: last lumi file can't be read." << std::endl;
+        return;
+      }
     }
     m_eventId = 0;
-    auto t = cond::time::unpack( lastLumi );
+    auto t = cond::time::unpack(lastLumi);
     unsigned int runId = t.first;
     unsigned int lumiId = t.second;
-    std::cout <<"###### initialize Run: "<<runId<<" lumi: "<<lumiId<<std::endl;
+    std::cout << "###### initialize Run: " << runId << " lumi: " << lumiId << std::endl;
     m_currentRun = runId;
     m_currentLumi = lumiId;
     boost::posix_time::ptime now = boost::posix_time::microsec_clock::local_time();
@@ -111,8 +114,7 @@ namespace cond{
     interval = m_interval;
   }
 
-  void
-  FileBasedEmptySource::fillDescriptions(edm::ConfigurationDescriptions& descriptions) {
+  void FileBasedEmptySource::fillDescriptions(edm::ConfigurationDescriptions& descriptions) {
     edm::ParameterSetDescription desc;
     desc.setComment("Creates runs, lumis and events containing no products.");
     ProducerSourceBase::fillDescription(desc);
@@ -130,8 +132,7 @@ namespace cond{
     descriptions.add("source", desc);
   }
 
-
-}//ns cond
+}  // namespace cond
 
 #include "FWCore/Framework/interface/InputSourceMacros.h"
 using cond::FileBasedEmptySource;
