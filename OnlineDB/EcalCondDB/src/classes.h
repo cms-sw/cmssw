617e8b140c19d41f31b07889e82ca7eea3f8fe8c
Giovanni Organtini <giovanni.organtini@roma1.infn.it>
2020-04-03 10:09:53 +0200

    removed the definition of the class from classes.h, following a prescription from Matti Kortelainen

--- OnlineDB/EcalCondDB/src/classes.h
+++ OnlineDB/EcalCondDB/src/classes.h
@@ -2,10 +2,3 @@
 #include "OnlineDB/EcalCondDB/interface/RunTag.h"
 #include "OnlineDB/EcalCondDB/interface/RunIOV.h"
 
-namespace OnlineDB_EcalCondDB {
-  struct dictionnary {
-    RunTag r1;
-    RunIOV r2;
-    EcalCondDBInterface e1;
-  };
-}  // namespace OnlineDB_EcalCondDB
