cbe170ca7e85f7fff81b379955e92226f4a0b10b
Marco Rovere <marco.rovere@cern.ch>
2020-04-03 22:42:24 +0200

    Remove Producer from python labels

--- RecoHGCal/TICL/python/iterativeTICL_cff.py
+++ RecoHGCal/TICL/python/iterativeTICL_cff.py
@@ -6,7 +6,7 @@ from RecoHGCal.TICL.EMStep_cff import *
 from RecoHGCal.TICL.HADStep_cff import *
 from RecoHGCal.TICL.ticlLayerTileProducer_cfi import ticlLayerTileProducer
 from RecoHGCal.TICL.ticlCandidateFromTrackstersProducer_cfi import ticlCandidateFromTrackstersProducer as _ticlCandidateFromTrackstersProducer
-from RecoHGCal.TICL.pfTICLProducer_cfi import pfTICLProducer
+from RecoHGCal.TICL.pfTICL_cfi import pfTICL
 from RecoHGCal.TICL.trackstersMergeProducer_cfi import trackstersMergeProducer as _trackstersMergeProducer
 from RecoHGCal.TICL.multiClustersFromTrackstersProducer_cfi import multiClustersFromTrackstersProducer as _multiClustersFromTrackstersProducer
 
@@ -19,12 +19,12 @@ ticlMultiClustersFromTrackstersMerge = _multiClustersFromTrackstersProducer.clon
 )
 ticlTracksterMergeTask = cms.Task(ticlTrackstersMerge, ticlMultiClustersFromTrackstersMerge)
 
-ticlCandidateFromTrackstersProducer = _ticlCandidateFromTrackstersProducer.clone(
+ticlCandidateFromTracksters = _ticlCandidateFromTrackstersProducer.clone(
       tracksterCollections = ["ticlTrackstersMerge"],
       # A possible alternative for momentum computation:
       # momentumPlugin = dict(plugin="TracksterP4FromTrackAndPCA")
     )
-ticlPFTask = cms.Task(ticlCandidateFromTrackstersProducer, pfTICLProducer)
+ticlPFTask = cms.Task(ticlCandidateFromTracksters, pfTICL)
 
 iterTICLTask = cms.Task(ticlLayerTileTask
     ,ticlMIPStepTask
@@ -37,6 +37,6 @@ iterTICLTask = cms.Task(ticlLayerTileTask
 
 def injectTICLintoPF(process):
     if getattr(process,'particleFlowTmp', None):
-      process.particleFlowTmp.src = ['particleFlowTmpBarrel', 'pfTICLProducer']
+      process.particleFlowTmp.src = ['particleFlowTmpBarrel', 'pfTICL']
 
     return process
