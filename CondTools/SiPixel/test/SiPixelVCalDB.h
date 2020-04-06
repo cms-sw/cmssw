3fa1042e4f1bccbf8a6669cb50b91424ac34de19
ineuteli <izaak.neutelings@cern.ch>
2020-04-07 00:22:28 +0200

    add detIdToPixelId help function

--- CondTools/SiPixel/test/SiPixelVCalDB.h
+++ CondTools/SiPixel/test/SiPixelVCalDB.h
@@ -13,16 +13,20 @@
 #include "FWCore/MessageLogger/interface/MessageLogger.h"
 #include "CondCore/DBOutputService/interface/PoolDBOutputService.h"
 #include "FWCore/ServiceRegistry/interface/Service.h"
+#include "DataFormats/DetId/interface/DetId.h"
+#include "DataFormats/TrackerCommon/interface/TrackerTopology.h"
+#include "DataFormats/SiPixelDetId/interface/PixelEndcapName.h"
+#include "DataFormats/SiPixelDetId/interface/PixelBarrelName.h"
 #include "CondFormats/SiPixelObjects/interface/SiPixelVCal.h"
 #include "CondTools/SiPixel/test/SiPixelVCalDB.h"
 
 class SiPixelVCalDB : public edm::EDAnalyzer {
 public:
 
-  // "PixelID"
+  // "PixelId"
   // BPix: 1000*(subdetId=1) + 100*(layer=1,2,3,4)
   // FPix: 1000*(subdetId=2) + 100*(side=1,2) + 10*(disk=1,2,3) + 1*(ring=1,2)
-  enum PixelID {
+  enum PixelId {
     L1=1100, L2=1200, L3=1300, L4=1400, // BPix
     Rm1l=2111, Rm1u=2112, Rm2l=2121, Rm2u=2122, Rm3l=2131, Rm3u=2132, // FPix minus
     Rp1l=2211, Rp1u=2212, Rp2l=2221, Rp2u=2222, Rp3l=2231, Rp3u=2232, // FPix plus
@@ -34,9 +38,9 @@ public:
   virtual void endJob();
   virtual void analyze(const edm::Event& e, const edm::EventSetup& c);
 
-  //const PixelID detIdToPixelID(const unsigned int detid); //const uint32_t&
-  static const PixelID calculateBPixID(const unsigned int layer);
-  static const PixelID calculateFPixID(const unsigned int side, const unsigned int disk, const unsigned int ring);
+  static const PixelId detIdToPixelId(const unsigned int detid, const TrackerTopology* trackTopo, const bool phase1=true); //const uint32_t&
+  static const PixelId calculateBPixID(const unsigned int layer);
+  static const PixelId calculateFPixID(const unsigned int side, const unsigned int disk, const unsigned int ring);
   static const int getPixelSubDetector(const unsigned int pixid);
   
 private:
