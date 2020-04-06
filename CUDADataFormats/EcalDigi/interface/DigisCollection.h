8f8b0e3efca9ecc63711d4f7257d614cd6b24751
Andrea Bocci <andrea.bocci@cern.ch>
2020-04-06 16:04:52 +0200

    Apply code formatting fixes from upstream integration (cms-patatrack#445)

--- CUDADataFormats/EcalDigi/interface/DigisCollection.h
+++ CUDADataFormats/EcalDigi/interface/DigisCollection.h
@@ -3,27 +3,25 @@
 
 namespace ecal {
 
-//
-// this is basically a view 
-// it does not own the actual memory -> does not reclaim
-//
-struct DigisCollection {
+  //
+  // this is basically a view
+  // it does not own the actual memory -> does not reclaim
+  //
+  struct DigisCollection {
     DigisCollection() = default;
-    DigisCollection(uint32_t *ids, uint16_t *data, uint32_t ndigis)
-        : ids{ids}, data{data}, ndigis{ndigis}
-    {}
-    DigisCollection(DigisCollection const&) = default;
-    DigisCollection& operator=(DigisCollection const&) = default;
+    DigisCollection(uint32_t *ids, uint16_t *data, uint32_t ndigis) : ids{ids}, data{data}, ndigis{ndigis} {}
+    DigisCollection(DigisCollection const &) = default;
+    DigisCollection &operator=(DigisCollection const &) = default;
 
-    DigisCollection(DigisCollection&&) = default;
-    DigisCollection& operator=(DigisCollection&&) = default;
+    DigisCollection(DigisCollection &&) = default;
+    DigisCollection &operator=(DigisCollection &&) = default;
 
     // stride is statically known
-    uint32_t *ids=nullptr;
-    uint16_t *data=nullptr;
+    uint32_t *ids = nullptr;
+    uint16_t *data = nullptr;
     uint32_t ndigis;
-};
+  };
 
-}
+}  // namespace ecal
 
-#endif // CUDADataFormats_EcalDigi_interface_DigisCollection_h
+#endif  // CUDADataFormats_EcalDigi_interface_DigisCollection_h
