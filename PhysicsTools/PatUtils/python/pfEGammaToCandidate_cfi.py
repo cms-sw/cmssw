5d1a2153504c2f715139d649f5f946195b76811a
Andreas Hinzmann <hinzmann@cern.ch>
2020-04-09 23:46:05 +0200

    comments

--- PhysicsTools/PatUtils/python/pfEGammaToCandidate_cfi.py
+++
@@ -1,8 +0,0 @@
-import FWCore.ParameterSet.Config as cms
-
-pfEGammaToCandidate = cms.EDProducer("PFEGammaToCandidate",
-    electrons = cms.InputTag("selectedPatElectrons"),
-    photons = cms.InputTag("selectedPatPhotons"),
-    electron2pf = cms.InputTag("particleBasedIsolation","gedGsfElectrons"),
-    photon2pf = cms.InputTag("particleBasedIsolation","gedPhotons")
-)
