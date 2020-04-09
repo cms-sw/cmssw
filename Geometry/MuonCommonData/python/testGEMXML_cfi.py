191fd533b12cefc1c6075ecfc844858dd5a3882f
Sunanda <sunanda.banerjee@cern.ch>
2020-04-09 15:58:33 +0200

    Add a GE21 version with 16 eta partitions

---
+++ Geometry/MuonCommonData/python/testGEMXML_cfi.py
@@ -0,0 +1,33 @@
+import FWCore.ParameterSet.Config as cms
+
+# This config was generated automatically using generate2026Geometry.py
+# If you notice a mistake, please update the generating script, not just this config
+
+XMLIdealGeometryESSource = cms.ESSource("XMLIdealGeometryESSource",
+    geomXMLFiles = cms.vstring(
+        'Geometry/CMSCommonData/data/materials.xml',
+        'Geometry/CMSCommonData/data/rotations.xml',
+        'Geometry/CMSCommonData/data/extend/v2/cmsextent.xml',
+        'Geometry/CMSCommonData/data/cmsMother.xml',
+        'Geometry/CMSCommonData/data/eta3/etaMax.xml',
+        'Geometry/CMSCommonData/data/cmsCalo.xml',
+        'Geometry/CMSCommonData/data/cmsMuon.xml',
+        'Geometry/CMSCommonData/data/mgnt.xml',
+        'Geometry/CMSCommonData/data/beampipe/2026/v1/beampipe.xml',
+        'Geometry/CMSCommonData/data/cmsBeam/2026/v1/cmsBeam.xml',
+        'Geometry/CMSCommonData/data/muonMB.xml',
+        'Geometry/CMSCommonData/data/muonMagnet.xml',
+        'Geometry/CMSCommonData/data/cavernData/2021/v1/cavernData.xml',
+        'Geometry/CMSCommonData/data/cms/2026/v3/cms.xml',
+        'Geometry/CMSCommonData/data/caloBase/2026/v2/caloBase.xml',
+        'Geometry/CMSCommonData/data/muonBase/2026/v3/muonBase.xml',
+        'Geometry/MuonCommonData/data/mbCommon/2021/v1/mbCommon.xml',
+        'Geometry/MuonCommonData/data/mf/2026/v2/mf.xml',
+        'Geometry/MuonCommonData/data/gemf/TDR_BaseLine/gemf.xml',
+        'Geometry/MuonCommonData/data/gem11/TDR_BaseLine/gem11.xml',
+        'Geometry/MuonCommonData/data/gem21/TDR_Eta16/gem21.xml',
+        'Geometry/MuonCommonData/data/me0/TDR_Dev/v2/me0.xml',
+        'Geometry/CMSCommonData/data/FieldParameters.xml',
+    ),
+    rootNodeName = cms.string('cms:OCMS')
+)
