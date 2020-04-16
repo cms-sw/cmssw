4811af95eb46b994302eb43434b3c84462db4b1b
Attila Radl <arttila.radl@cern.ch>
2020-04-17 01:01:31 +0200

    code style

--- Calibration/LumiAlCaRecoProducers/plugins/AlcaPCCIntegrator.cc
+++ Calibration/LumiAlCaRecoProducers/plugins/AlcaPCCIntegrator.cc
@@ -53,10 +53,8 @@ private:
 
 //--------------------------------------------------------------------------------------------------
 AlcaPCCIntegrator::AlcaPCCIntegrator(const edm::ParameterSet& iConfig) {
-  pccSource_ = iConfig.getParameter<edm::ParameterSet>("AlcaPCCIntegratorParameters")
-                           .getParameter<std::string>("inputPccLabel");
-  auto trigstring_ = iConfig.getParameter<edm::ParameterSet>("AlcaPCCIntegratorParameters")
-                    .getUntrackedParameter<std::string>("trigstring","alcaPCC");
+  pccSource_ = iConfig.getParameter<edm::ParameterSet>("AlcaPCCIntegratorParameters").getParameter<std::string>("inputPccLabel");
+  auto trigstring_ = iConfig.getParameter<edm::ParameterSet>("AlcaPCCIntegratorParameters").getUntrackedParameter<std::string>("trigstring","alcaPCC");
  
   prodInst_ = iConfig.getParameter<edm::ParameterSet>("AlcaPCCIntegratorParameters").getParameter<std::string>("ProdInst");
 	 
