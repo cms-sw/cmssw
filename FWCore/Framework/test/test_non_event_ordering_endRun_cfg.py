c8191de8e593d7bfc72ed4d827c722345cf5a05c
Matti Kortelainen <matti.kortelainen@cern.ch>
2020-04-20 01:46:55 +0200

    Keep modules in non-event ordering unit tests running by moving the last one in the chain to a Path
    Without a scheduled module consuming their output all modules would be deleted.

--- FWCore/Framework/test/test_non_event_ordering_endRun_cfg.py
+++ FWCore/Framework/test/test_non_event_ordering_endRun_cfg.py
@@ -10,4 +10,5 @@ process.a = cms.EDProducer("NonEventIntProducer", ivalue = cms.int32(1))
 process.b = cms.EDProducer("NonEventIntProducer", ivalue = cms.int32(2), consumesEndRun = cms.InputTag("c","endRun"), expectEndRun = cms.untracked.int32(3) )
 process.c = cms.EDProducer("NonEventIntProducer", ivalue = cms.int32(3), consumesEndRun = cms.InputTag("a", "endRun"), expectEndRun = cms.untracked.int32(1) )
 
-process.schedule = cms.Schedule(tasks=cms.Task(process.a,process.b,process.c))
+process.t = cms.Task(process.a, process.c)
+process.p = cms.Path(process.b, process.t)
