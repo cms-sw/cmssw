2717794437f4a7f9f7540cee90af60125f296981
Sanghyun Ko <sang.hyun.ko@cern.ch>
2020-04-20 12:11:39 +0900

    update GEN Relvals to CP5 (1)

--- Configuration/Generator/python/SoftQCDinelastic_13TeV_pythia8_cff.py
+++ Configuration/Generator/python/SoftQCDinelastic_13TeV_pythia8_cff.py
@@ -1,7 +1,7 @@
 import FWCore.ParameterSet.Config as cms
 
 from Configuration.Generator.Pythia8CommonSettings_cfi import *
-from Configuration.Generator.Pythia8CUEP8M1Settings_cfi import *
+from Configuration.Generator.MCTunes2017.PythiaCP5Settings_cfi import *
 
 
 
@@ -14,11 +14,11 @@ generator = cms.EDFilter("Pythia8GeneratorFilter",
                          comEnergy = cms.double(13000.0),
     PythiaParameters = cms.PSet(
         pythia8CommonSettingsBlock,
-        pythia8CUEP8M1SettingsBlock,
+        pythia8CP5SettingsBlock,
         processParameters = cms.vstring(
             'SoftQCD:inelastic = on'),
         parameterSets = cms.vstring('pythia8CommonSettings',
-                                    'pythia8CUEP8M1Settings',
+                                    'pythia8CP5Settings',
                                     'processParameters',
                                     )
         )
