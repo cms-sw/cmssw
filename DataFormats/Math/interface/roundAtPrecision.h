2de1f31036f0a28d75bae6ef5f37256ebb76ef98
Vincenzo Innocente <vincenzo.innocente@cern.ch>
2020-04-20 14:35:16 +0200

    add a fast and simple rounding function

---
+++ DataFormats/Math/interface/roundAtPrecision.h
@@ -0,0 +1,34 @@
+#ifndef DataFormatsMathroundAtPrecision_H
+#define DataFormatsMathroundAtPrecision_H
+
+#include "FWCore/Utilities/interface/isFinite.h"
+#include<cstring>
+#include<cstdint>
+
+// round with N significant bits
+template<int N>
+float roundAtPrecision(float x) {
+
+  if (edm::isNotFinite(x)) return x;
+
+  static_assert(N<23);
+  constexpr auto shift = 23-N;
+  constexpr uint32_t mask = 1<<(shift-1);
+
+  uint32_t i; memcpy(&i, &x, sizeof(x));
+
+  i += mask;
+  i>>=shift; i<<=shift;
+  memcpy(&x, &i, sizeof(x));
+
+  return x;
+
+}
+
+
+template<>
+float roundAtPrecision<23>(float x) { return x;}
+
+
+
+#endif  // DataFormatsMathroundAtPrecision_H
