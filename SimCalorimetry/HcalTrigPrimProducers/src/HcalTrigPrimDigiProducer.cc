741dd14fb8bb897b4cdf223dbe162e278e54a8c0
Josh Hiltbrand <hiltbran@umn.edu>
2020-04-21 11:55:36 -0500

    Initial commit of implementing weights method for OOT PU subtraction in HCAL

--- SimCalorimetry/HcalTrigPrimProducers/src/HcalTrigPrimDigiProducer.cc
+++ SimCalorimetry/HcalTrigPrimProducers/src/HcalTrigPrimDigiProducer.cc
@@ -43,6 +43,7 @@ HcalTrigPrimDigiProducer::HcalTrigPrimDigiProducer(const edm::ParameterSet& ps)
     theAlgo_.overrideParameters(pset);
   }
   theAlgo_.setUpgradeFlags(upgrades[0], upgrades[1], upgrades[2]);
+  theAlgo_.setWeightsQIE11(ps.getUntrackedParameter<edm::ParameterSet>("weightsQIE11"));
 
   HFEMB_ = false;
   if (ps.exists("LSConfig")) {
