471e64ab8ac3e1b36da2f1b8245f9793d7cd6ba8
calderon <calderon@cern.ch>
2020-04-22 16:51:00 +0200

    clean up muon dqm sequence

--- DQMOffline/Muon/python/muonCosmicMonitors_cff.py
+++ DQMOffline/Muon/python/muonCosmicMonitors_cff.py
@@ -7,16 +7,13 @@ from DQM.TrackingMonitor.MonitorTrackGLBCosmicMuons_cfi import *
 from DQM.TrackingMonitor.MonitorTrackTKCosmicMuons_cfi import *
 from DQM.TrackingMonitor.MonitorTrackEfficiencySTACosmicMuons_cff import *
 from DQM.TrackingMonitor.MonitorTrackEfficiencyTkTracks_cff import *
-from DQMOffline.Muon.dtSegmTask_cfi import *
+
 
 #dedicated analyzers for offline dqm
 from RecoMuon.TrackingTools.MuonServiceProxy_cff import *
 from DQMOffline.Muon.muonCosmicAnalyzer_cff import *
 from DQMOffline.Muon.CSCMonitor_cfi import *
 from DQMOffline.Muon.muonIdDQM_cff import *
-
-#dedicated clients for offline dqm
-from DQMOffline.Muon.muonQualityTests_cff import *
 
 
 from DQMServices.Core.DQMEDAnalyzer import DQMEDAnalyzer
@@ -34,7 +31,6 @@ muonTrackCosmicAnalyzersHLT = cms.Sequence(MonitorTrackSTACosmicMuonsHLTDT*
                                            MonitorTrackSTACosmicMuonsHLTCSC)
 
 muonCosmicMonitors = cms.Sequence(muonTrackCosmicAnalyzers*
-                                  dtSegmentsMonitor*
                                   cscMonitor*
                                   muonCosmicAnalyzer*
                                   muonIdDQM*
@@ -43,13 +39,11 @@ muonCosmicMonitors = cms.Sequence(muonTrackCosmicAnalyzers*
 ##muonCosmicMonitors = cms.Sequence(muonTrackCosmicAnalyzers*dtSegmentsMonitor*cscMonitor*muonCosmicAnalyzer)
 
 muonCosmicMonitors_woCSC = cms.Sequence(cms.SequencePlaceholder("muonTrackAnalyzers")*
-                                        dtSegmentsMonitor*
                                         muonCosmicMonitors)
 
 muonStandAloneCosmicMonitors = cms.Sequence(MonitorTrackSTACosmicMuons*
-                                            dtSegmentsMonitor*
                                             cscMonitor*
                                             muonSACosmicAnalyzer)
 
-muonCosmicMonitorsAndQualityTests = cms.Sequence(muonCosmicMonitors*muonQualityTests)
+muonCosmicMonitorsAndQualityTests = cms.Sequence(muonCosmicMonitors)
 
