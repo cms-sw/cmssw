785ef3607efdbe5701181b0d964aaee396427b1f
Lauren Hay <lmhay@buffalo.edu>
2020-04-23 21:27:46 +0200

    Code checks.

--- CommonTools/CandAlgos/plugins/CandFwdPtrMerger.cc
+++ CommonTools/CandAlgos/plugins/CandFwdPtrMerger.cc
@@ -12,12 +12,10 @@
 #include "DataFormats/PatCandidates/interface/PackedGenParticle.h"
 #include "DataFormats/Common/interface/FwdPtr.h"
 
-
 typedef Merger<std::vector<edm::FwdPtr<reco::Candidate>>> CandFwdPtrMerger;
 typedef Merger<std::vector<edm::FwdPtr<pat::PackedCandidate>>> PackedCandidateFwdPtrMerger;
 typedef Merger<std::vector<edm::FwdPtr<pat::PackedGenParticle>>> PackedGenParticleFwdPtrMerger;
 
-
-DEFINE_FWK_MODULE( CandFwdPtrMerger );
-DEFINE_FWK_MODULE( PackedCandidateFwdPtrMerger );
-DEFINE_FWK_MODULE( PackedGenParticleFwdPtrMerger );
+DEFINE_FWK_MODULE(CandFwdPtrMerger);
+DEFINE_FWK_MODULE(PackedCandidateFwdPtrMerger);
+DEFINE_FWK_MODULE(PackedGenParticleFwdPtrMerger);
