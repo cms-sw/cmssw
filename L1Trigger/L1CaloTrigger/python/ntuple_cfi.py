db2059a6097d679b4bc56093e868de80cf5119ff
Gianluca <gianluca.cerminara@cern.ch>
2020-04-23 12:19:44 +0200

    restructure EG ntuple code

--- L1Trigger/L1CaloTrigger/python/ntuple_cfi.py
+++ L1Trigger/L1CaloTrigger/python/ntuple_cfi.py
@@ -1,16 +1,19 @@
 import FWCore.ParameterSet.Config as cms
 
 ntuple_egammaEE = cms.PSet(
-    NtupleName = cms.string('L1TriggerNtupleEgammaEE'),
-    EgammaEE = cms.InputTag('l1EGammaEEProducer:L1EGammaCollectionBXVWithCuts')
+    NtupleName = cms.string('L1TriggerNtupleEgamma'),
+    Egamma = cms.InputTag('l1EGammaEEProducer:L1EGammaCollectionBXVWithCuts'),
+    BranchNamePrefix = cms.untracked.string("egammaEE")
 )
 
 ntuple_TTTracks = cms.PSet(
     NtupleName = cms.string('L1TriggerNtupleTrackTrigger'),
-    TTTracks = cms.InputTag("TTTracksFromTrackletEmulation", "Level1TTTracks")
+    TTTracks = cms.InputTag("TTTracksFromTrackletEmulation", "Level1TTTracks"),
+    BranchNamePrefix = cms.untracked.string("l1Trk")
 )
 
 ntuple_tkEle = cms.PSet(
     NtupleName = cms.string('L1TriggerNtupleTkElectrons'),
-    TkElectrons = cms.InputTag("L1TkElectrons","EG")
+    TkElectrons = cms.InputTag("L1TkElectrons","EG"),
+    BranchNamePrefix = cms.untracked.string("tkEle")
 )
