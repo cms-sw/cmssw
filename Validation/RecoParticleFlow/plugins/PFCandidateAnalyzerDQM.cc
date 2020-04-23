26d7019ccb4399c55dbb8c14834640200d6bcb3e
chosila <si_sutantawibul1@baylor.edu>
2020-04-22 20:30:33 -0500

    minor histogram name change, code checks

--- Validation/RecoParticleFlow/plugins/PFCandidateAnalyzerDQM.cc
+++ Validation/RecoParticleFlow/plugins/PFCandidateAnalyzerDQM.cc
@@ -1,3 +1,9 @@
+// Producer for particle flow candidates. Plots Eta, Phi, Charge, Pt (log freq, bin)
+// for different types of particles described in python/defaults_cfi.py
+// note: for pt, log freq is done in this producer, but log freq is done by running
+// compare.py
+// author: Chosila Sutantawibul, April 23, 2020
+
 #include "FWCore/MessageLogger/interface/MessageLogger.h"
 #include "FWCore/ParameterSet/interface/ParameterSet.h"
 #include "FWCore/ServiceRegistry/interface/Service.h"
@@ -24,7 +30,6 @@
 #include <map>
 #include <string>
 #include <cstring>
-#include <typeinfo>
 
 class PFCandidateAnalyzerDQM : public DQMEDAnalyzer {
 public:
@@ -59,7 +64,6 @@ PFCandidateAnalyzerDQM::PFCandidateAnalyzerDQM(const edm::ParameterSet& iConfig)
 
 void PFCandidateAnalyzerDQM::bookHistograms(DQMStore::IBooker& booker, edm::Run const&, edm::EventSetup const&) {
   // all candidate
-
   booker.setCurrentFolder("ParticleFlow/PackedCandidates/AllCandidate");
 
   // for eta binning
@@ -72,7 +76,7 @@ void PFCandidateAnalyzerDQM::bookHistograms(DQMStore::IBooker& booker, edm::Run 
   me["AllCandidateEta"] = booker.book1D("AllCandidateEta", etaHist);
 
   me["AllCandidatePt(log GeV)"] = booker.book1D("AllCandidatePt(log GeV)", "AllCandidatePt(log GeV)", 140, -2, 4);
-  me["AllCandidatePt(log freq)"] = booker.book1D("AllCandidatePt(log freq)", "AllCandidatePt(log freq)", 500, 0, 1000);
+  me["AllCandidatePt(log y)"] = booker.book1D("AllCandidatePt(log y)", "AllCandidatePt(log y)", 500, 0, 1000);
   me["AllCandidatePhi"] = booker.book1D("AllCandidatePhi", "AllCandidatePhi", 72, -M_PI, M_PI);
   me["AllCandidateCharge"] = booker.book1D("AllCandidateCharge", "AllCandidateCharge", 3, -1.5, 1.5);
 
@@ -84,8 +88,9 @@ void PFCandidateAnalyzerDQM::bookHistograms(DQMStore::IBooker& booker, edm::Run 
     TH1F* etaHist = new TH1F(etaHistName, etaHistName, n, etabinArray);
     me[pair.second + "Eta"] = booker.book1D(pair.second + "Eta", etaHist);
 
-    me[pair.second + "Pt(log GeV)"] = booker.book1D(pair.second + "Pt(log GeV)", pair.second + "Pt(log GeV)", 140, -2, 4);
-    me[pair.second + "Pt(log freq)"] = booker.book1D(pair.second + "Pt(log freq)", pair.second + "Pt(log freq)", 500, 0, 1000);
+    me[pair.second + "Pt(log GeV)"] =
+        booker.book1D(pair.second + "Pt(log GeV)", pair.second + "Pt(log GeV)", 140, -2, 4);
+    me[pair.second + "Pt(log y)"] = booker.book1D(pair.second + "Pt(log y)", pair.second + "Pt(log y)", 500, 0, 1000);
     me[pair.second + "Phi"] = booker.book1D(pair.second + "Phi", pair.second + "Phi", 72, -M_PI, M_PI);
     me[pair.second + "Charge"] = booker.book1D(pair.second + "Charge", pair.second + "Charge", 3, -1.5, 1.5);
   }
@@ -108,15 +113,15 @@ void PFCandidateAnalyzerDQM::analyze(const edm::Event& iEvent, const edm::EventS
       // Fill Histograms for Candidate Methods
       // all candidates
       me["AllCandidatePt(log GeV)"]->Fill(log10(pfHandle->at(i).pt()));
-      me["AllCandidatePt(log freq)"]->Fill(pfHandle->at(i).pt());
+      me["AllCandidatePt(log y)"]->Fill(pfHandle->at(i).pt());
       me["AllCandidateEta"]->Fill(pfHandle->at(i).eta());
       me["AllCandidatePhi"]->Fill(pfHandle->at(i).phi());
       me["AllCandidateCharge"]->Fill(pfHandle->at(i).charge());
       int pdgId = abs(pfHandle->at(i).pdgId());
       if (pdgMap.find(pdgId) != pdgMap.end()) {
         me[pdgMap[pdgId] + "Pt(log GeV)"]->Fill(log10(pfHandle->at(i).pt()));
-        me[pdgMap[pdgId] + "Pt(log freq)"]->Fill(pfHandle->at(i).pt());
-	me[pdgMap[pdgId] + "Eta"]->Fill(pfHandle->at(i).eta());
+        me[pdgMap[pdgId] + "Pt(log y)"]->Fill(pfHandle->at(i).pt());
+        me[pdgMap[pdgId] + "Eta"]->Fill(pfHandle->at(i).eta());
         me[pdgMap[pdgId] + "Phi"]->Fill(pfHandle->at(i).phi());
         me[pdgMap[pdgId] + "Charge"]->Fill(pfHandle->at(i).charge());
       }
