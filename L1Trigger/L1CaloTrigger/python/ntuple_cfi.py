05efb0d1da617bbbba3d6b481fa80535f37b7f8c
Gianluca <gianluca.cerminara@cern.ch>
2020-04-24 15:31:17 +0200

    streamline EG ntuple configuration

--- L1Trigger/L1CaloTrigger/python/ntuple_cfi.py
+++ L1Trigger/L1CaloTrigger/python/ntuple_cfi.py
@@ -6,14 +6,50 @@ ntuple_egammaEE = cms.PSet(
     BranchNamePrefix = cms.untracked.string("egammaEE")
 )
 
+ntuple_egammaEB = cms.PSet(
+    NtupleName = cms.string('L1TriggerNtupleEgamma'),
+    Egamma = cms.InputTag("L1EGammaClusterEmuProducer","L1EGammaCollectionBXVEmulator"),
+    BranchNamePrefix = cms.untracked.string("egammaEB")
+)
+
 ntuple_TTTracks = cms.PSet(
     NtupleName = cms.string('L1TriggerNtupleTrackTrigger'),
     TTTracks = cms.InputTag("TTTracksFromTrackletEmulation", "Level1TTTracks"),
     BranchNamePrefix = cms.untracked.string("l1Trk")
 )
 
-ntuple_tkEle = cms.PSet(
+ntuple_tkEleEE = cms.PSet(
     NtupleName = cms.string('L1TriggerNtupleTkElectrons'),
-    TkElectrons = cms.InputTag("L1TkElectrons","EG"),
-    BranchNamePrefix = cms.untracked.string("tkEle")
+    TkElectrons = cms.InputTag("L1TkElectronsHGC","EG"),
+    BranchNamePrefix = cms.untracked.string("tkEleEE")
+)
+
+ntuple_tkEleEB = cms.PSet(
+    NtupleName = cms.string('L1TriggerNtupleTkElectrons'),
+    TkElectrons = cms.InputTag("L1TkElectronsCrystal","EG"),
+    BranchNamePrefix = cms.untracked.string("tkEleEB")
+)
+
+ntuple_tkEleEllEE = cms.PSet(
+    NtupleName = cms.string('L1TriggerNtupleTkElectrons'),
+    TkElectrons = cms.InputTag("L1TkElectronsEllipticMatchHGC","EG"),
+    BranchNamePrefix = cms.untracked.string("tkEleEllEE")
+)
+
+ntuple_tkEleEllEB = cms.PSet(
+    NtupleName = cms.string('L1TriggerNtupleTkElectrons'),
+    TkElectrons = cms.InputTag("L1TkElectronsEllipticMatchCrystal","EG"),
+    BranchNamePrefix = cms.untracked.string("tkEleEllEB")
+)
+
+ntuple_tkIsoEleEE = cms.PSet(
+    NtupleName = cms.string('L1TriggerNtupleTkElectrons'),
+    TkElectrons = cms.InputTag("L1TkIsoElectronsHGC","EG"),
+    BranchNamePrefix = cms.untracked.string("tkIsoEleEE")
+)
+
+ntuple_tkIsoEleEB = cms.PSet(
+    NtupleName = cms.string('L1TriggerNtupleTkElectrons'),
+    TkElectrons = cms.InputTag("L1TkIsoElectronsCrystal","EG"),
+    BranchNamePrefix = cms.untracked.string("tkIsoEleEB")
 )
