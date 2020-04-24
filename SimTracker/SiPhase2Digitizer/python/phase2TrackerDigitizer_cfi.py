f313e7dc7b1f4bb86178170c8ee62a4b505c57f3
mmusich <marco.musich@cern.ch>
2020-04-24 22:39:25 +0200

    fix phase-2 Inner Tracker digitization for premixing

--- SimTracker/SiPhase2Digitizer/python/phase2TrackerDigitizer_cfi.py
+++ SimTracker/SiPhase2Digitizer/python/phase2TrackerDigitizer_cfi.py
@@ -221,9 +221,6 @@ phase2TrackerDigitizer = cms.PSet(
 # - do not apply inefficiency (to be done in stage2)
 # - disable threshold smearing
 #
-# For inner pixel
-# - extend the dynamic range of ADCs
-#
 # For outer tracker
 # - force analog readout to get the ADCs
 #
@@ -235,8 +232,6 @@ _premixStage1ModifyDict = dict(
         AddNoisyPixels = False,
         AddInefficiency = False,
         AddThresholdSmearing = False,
-        ElectronPerAdc = phase2TrackerDigitizer.PSPDigitizerAlgorithm.ElectronPerAdc.value(),
-        AdcFullScale = phase2TrackerDigitizer.PSPDigitizerAlgorithm.AdcFullScale.value(),
     ),
     PSPDigitizerAlgorithm = dict(
         AddNoisyPixels = False,
