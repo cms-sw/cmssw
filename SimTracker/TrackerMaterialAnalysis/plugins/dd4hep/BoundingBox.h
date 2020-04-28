8bc27cd0d2db3bf71ac6ca17dcb2cb8e90d50c2d
Andres Vargas <vargasa@cua.edu>
2020-04-28 10:56:01 -0500

    scram build code-format

--- SimTracker/TrackerMaterialAnalysis/plugins/dd4hep/BoundingBox.h
+++ SimTracker/TrackerMaterialAnalysis/plugins/dd4hep/BoundingBox.h
@@ -4,21 +4,18 @@
 #include <utility>
 
 class BoundingBox {
-
- private:
-
+private:
   double r_min;
   double r_max;
   double z_min;
   double z_max;
 
- public:
+public:
+  BoundingBox() : r_min(0.), r_max(0.), z_min(0.), z_max(0.) {}
 
- BoundingBox() : r_min(0.), r_max(0.), z_min(0.), z_max(0.) {}
+  BoundingBox(double min_r, double max_r, double min_z, double max_z)
+      : r_min(min_r), r_max(max_r), z_min(min_z), z_max(max_z) {}
 
- BoundingBox(double min_r, double max_r, double min_z, double max_z)
-   : r_min(min_r), r_max(max_r), z_min(min_z), z_max(max_z) {}
-  
   void grow(double r, double z);
 
   void grow(double skin);
@@ -28,7 +25,6 @@ class BoundingBox {
   std::pair<double, double> range_r() const { return std::make_pair(r_min, r_max); }
 
   std::pair<double, double> range_z() const { return std::make_pair(z_min, z_max); }
-
 };
 
 #endif
