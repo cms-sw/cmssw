59983704525fed3aa1151e28577b9f0b8a06b61f
simone gennai <simone.gennai@cern.ch>
2020-04-28 13:39:28 +0200

    added a plugin to read OnlineBeamSpotFromDB

---
+++ RecoVertex/BeamSpotProducer/plugins/OnlineBeamSpotFromDB.cc
@@ -0,0 +1,49 @@
+/**_________________________________________________________________
+   class:   OnlineBeamSpotFromDB.cc
+   package: RecoVertex/BeamSpotProducer
+   
+
+
+ author: Francisco Yumiceva, Fermilab (yumiceva@fnal.gov)
+
+
+________________________________________________________________**/
+
+// C++ standard
+#include <string>
+// CMS
+#include "DataFormats/BeamSpot/interface/BeamSpot.h"
+#include "RecoVertex/BeamSpotProducer/interface/OnlineBeamSpotFromDB.h"
+
+#include "FWCore/MessageLogger/interface/MessageLogger.h"
+
+#include "FWCore/Framework/interface/MakerMacros.h"
+#include "FWCore/ServiceRegistry/interface/Service.h"
+
+#include "FWCore/Framework/interface/ESHandle.h"
+#include "FWCore/Framework/interface/EventSetup.h"
+#include "FWCore/Framework/interface/IOVSyncValue.h"
+#include "CondFormats/DataRecord/interface/BeamSpotTransientObjectsRcd.h"
+#include "CondFormats/BeamSpotObjects/interface/BeamSpotOnlineObjects.h"
+
+OnlineBeamSpotFromDB::OnlineBeamSpotFromDB(const edm::ParameterSet& iConfig) {}
+
+OnlineBeamSpotFromDB::~OnlineBeamSpotFromDB() {}
+
+void OnlineBeamSpotFromDB::analyze(const edm::Event& iEvent, const edm::EventSetup& iSetup) {
+  edm::ESHandle<BeamSpotOnlineObjects> beamhandle;
+  iSetup.get<BeamSpotTransientObjectsRcd>().get(beamhandle);
+  const BeamSpotOnlineObjects* mybeamspot = beamhandle.product();
+
+  std::cout << " for runs: " << iEvent.id().run() << " - " << iEvent.id().run() << std::endl;
+  //std::cout << iEvent.getRun().beginTime().value() << std::endl;
+  //std::cout << iEvent.time().value() << std::endl;
+  std::cout << *mybeamspot << std::endl;
+}
+
+void OnlineBeamSpotFromDB::beginJob() {}
+
+void OnlineBeamSpotFromDB::endJob() {}
+
+//define this as a plug-in
+DEFINE_FWK_MODULE(OnlineBeamSpotFromDB);
