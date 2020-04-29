fdf6f3f6c0448127a9604ee129be231d37e01bc7
Sunanda <sunanda.banerjee@cern.ch>
2020-04-29 15:26:37 +0200

    Code check

--- Geometry/HGCalCommonData/plugins/DDHGCalEEAlgo.cc
+++ Geometry/HGCalCommonData/plugins/DDHGCalEEAlgo.cc
@@ -277,9 +277,11 @@ void DDHGCalEEAlgo::constructLayers(const DDLogicalPart& module, DDCompactView& 
                                  pgonRin,
                                  pgonRout);
 #ifdef EDM_ML_DEBUG
-	  edm::LogVerbatim("HGCalGeom") << "DDHGCalEEAlgo: z " << (zz - hthick) << ":" << (zz + hthick) << " with " << pgonZ.size() << " palnes";
-	  for (unsigned int isec = 0; isec < pgonZ.size(); ++isec) 
-	    edm::LogVerbatim("HGCalGeom") << "[" << isec << "] z " << pgonZ[isec] << " R " << pgonRin[isec] << ":" << pgonRout[isec];
+          edm::LogVerbatim("HGCalGeom") << "DDHGCalEEAlgo: z " << (zz - hthick) << ":" << (zz + hthick) << " with "
+                                        << pgonZ.size() << " palnes";
+          for (unsigned int isec = 0; isec < pgonZ.size(); ++isec)
+            edm::LogVerbatim("HGCalGeom")
+                << "[" << isec << "] z " << pgonZ[isec] << " R " << pgonRin[isec] << ":" << pgonRout[isec];
 #endif
           for (unsigned int isec = 0; isec < pgonZ.size(); ++isec) {
             pgonZ[isec] -= zz;
--- Geometry/HGCalCommonData/plugins/dd4hep/DDHGCalEEAlgo.cc
+++ Geometry/HGCalCommonData/plugins/dd4hep/DDHGCalEEAlgo.cc
@@ -252,9 +252,11 @@ struct HGCalEEAlgo {
                                    pgonRin,
                                    pgonRout);
 #ifdef EDM_ML_DEBUG
-	    edm::LogVerbatim("HGCalGeom") << "DDHGCalEEAlgo: z " << (zz - hthick) << ":" << (zz + hthick) << " with " << pgonZ.size() << " palnes";
-	    for (unsigned int isec = 0; isec < pgonZ.size(); ++isec) 
-	      edm::LogVerbatim("HGCalGeom") << "[" << isec << "] z " << pgonZ[isec] << " R " << pgonRin[isec] << ":" << pgonRout[isec];
+            edm::LogVerbatim("HGCalGeom") << "DDHGCalEEAlgo: z " << (zz - hthick) << ":" << (zz + hthick) << " with "
+                                          << pgonZ.size() << " palnes";
+            for (unsigned int isec = 0; isec < pgonZ.size(); ++isec)
+              edm::LogVerbatim("HGCalGeom")
+                  << "[" << isec << "] z " << pgonZ[isec] << " R " << pgonRin[isec] << ":" << pgonRout[isec];
 #endif
             for (unsigned int isec = 0; isec < pgonZ.size(); ++isec) {
               pgonZ[isec] -= zz;
