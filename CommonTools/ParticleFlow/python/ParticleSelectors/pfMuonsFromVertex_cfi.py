406b75a825116f1a1b60593fccf8198d2ee76cb8
Devin Taylor <dntaylor@ucdavis.edu>
2020-04-30 05:15:03 +0200

    fix missing parameters in modules

--- CommonTools/ParticleFlow/python/ParticleSelectors/pfMuonsFromVertex_cfi.py
+++ CommonTools/ParticleFlow/python/ParticleSelectors/pfMuonsFromVertex_cfi.py
@@ -6,6 +6,8 @@ pfMuonsFromVertex = cms.EDFilter(
     vertices = cms.InputTag("offlinePrimaryVertices"),  # vertices source
     d0Cut = cms.double(0.2),  # transverse IP
     dzCut = cms.double(0.5),  # longitudinal IP
+    dtCut = cms.double(-1.0), # time
     d0SigCut = cms.double(99.),  # transverse IP significance
     dzSigCut = cms.double(99.),  # longitudinal IP significance
+    dtSigCut = cms.double(-1.0), # time significance
     )
