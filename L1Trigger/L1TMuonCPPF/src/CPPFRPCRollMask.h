ff02482371ecb8cc195bf24a17a43cd26dc712f8
Roumyana <roumyana.mileva.hadjiiska@cern.ch>
2020-04-30 16:33:50 +0200

    CPPF emulator refactoring

---
+++ L1Trigger/L1TMuonCPPF/src/CPPFRPCRollMask.h
@@ -0,0 +1,9 @@
+#ifndef L1Trigger_CPPFRPCRollMask_h
+#define L1Trigger_CPPFRPCRollMask_h
+
+#include <bitset>
+
+const int maskCPPFSIZE = 192;
+typedef std::bitset<maskCPPFSIZE> CPPFRollMask;
+
+#endif
