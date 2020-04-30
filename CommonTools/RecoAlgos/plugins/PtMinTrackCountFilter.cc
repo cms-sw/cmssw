403853cde302ba35e9cb2f6d03c89110b1524666
Antonio Vagnerini <antonio.vagnerini@desy.de>
2020-04-30 18:16:02 +0200

    add track filter for N tracks with minimum pt

---
+++ CommonTools/RecoAlgos/plugins/PtMinTrackCountFilter.cc
@@ -0,0 +1,25 @@
+/* \class PtMinTrackCountFilter
+ *
+ * Filters events if at least N tracks above 
+ * a pt cut are present.
+ *
+ * \author: Antonio Vagnerini, DESY
+ *
+ */
+#include "FWCore/Framework/interface/MakerMacros.h"
+#include "DataFormats/TrackReco/interface/Track.h"
+#include "DataFormats/TrackReco/interface/TrackFwd.h"
+#include "CommonTools/UtilAlgos/interface/ObjectCountFilter.h"
+#include "CommonTools/UtilAlgos/interface/PtMinSelector.h"
+
+typedef ObjectCountFilter<reco::TrackCollection, PtMinSelector>::type PtMinTrackCountFilter;
+
+template<>
+void PtMinTrackCountFilter::fillDescriptions(edm::ConfigurationDescriptions& descriptions) {
+  edm::ParameterSetDescription desc;
+  desc.add<edm::InputTag>("src", edm::InputTag("tracks"));
+  desc.add<double>("ptMin", 0.);
+  descriptions.add("ptMinTrackCountFilter", desc);
+}
+
+DEFINE_FWK_MODULE(PtMinTrackCountFilter);
