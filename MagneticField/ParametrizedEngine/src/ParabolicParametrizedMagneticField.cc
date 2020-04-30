8850003c2cdefa358ac096684ee9029f4639c4e7
Nicola Amapane <nicola.amapane@cern.ch>
2020-04-30 22:04:20 +0200

    Silence warning for calls outside OAEParametrizedMagneticField's validity

--- MagneticField/ParametrizedEngine/src/ParabolicParametrizedMagneticField.cc
+++ MagneticField/ParametrizedEngine/src/ParabolicParametrizedMagneticField.cc
@@ -22,8 +22,8 @@ GlobalVector ParabolicParametrizedMagneticField::inTesla(const GlobalPoint& gp) 
   if (isDefined(gp)) {
     return inTeslaUnchecked(gp);
   } else {
-    edm::LogWarning("MagneticField") << " Point " << gp
-                                     << " is outside the validity region of ParabolicParametrizedMagneticField";
+    LogDebug("MagneticField|FieldOutsideValidity")
+        << " Point " << gp << " is outside the validity region of ParabolicParametrizedMagneticField";
     return GlobalVector();
   }
 }
