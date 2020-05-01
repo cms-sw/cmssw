ffe184277c60982f57acd34ec3300d39aff84fa3
Abhishek Das <adas@nd.edu>
2020-05-01 00:56:01 -0400

    HGCalTimingPremix-Commit1-05_01_2020

--- SimGeneral/MixingModule/python/aliases_cfi.py
+++ SimGeneral/MixingModule/python/aliases_cfi.py
@@ -79,21 +79,13 @@ from Configuration.Eras.Modifier_phase2_hgcal_cff import phase2_hgcal
 
 from Configuration.ProcessModifiers.premix_stage1_cff import premix_stage1
 
-(premix_stage1 & phase2_hgcal).toModify(simHGCalUnsuppressedDigis,
-    mix = {
-        0 : dict(type = "PHGCSimAccumulator"),
-        1 : dict(type = "PHGCSimAccumulator"),
-        2 : dict(type = "PHGCSimAccumulator"),
-    }
-)
-
-(premix_stage1 & phase2_hgcal).toModify(simHGCalUnsuppressedDigis,
-                                        mix = simHGCalUnsuppressedDigis.mix + {
-                                            simHGCalUnsuppressedDigis.mix[0].clone(type = "PreMixHGCSimAccumulator"),
-                                            simHGCalUnsuppressedDigis.mix[1].clone(type = "PreMixHGCSimAccumulator"),
-                                            simHGCalUnsuppressedDigis.mix[2].clone(type = "PreMixHGCSimAccumulator"),
-                                        }
-)
+(premix_stage1 & phase2_hgcal).toModify(simHGCalUnsuppressedDigis,                                                                
+    mix = {                                                                                                                       
+        0 : dict(type = "PreMixHGCSimAccumulator"),                                                                               
+        1 : dict(type = "PreMixHGCSimAccumulator"),                                                                               
+        2 : dict(type = "PreMixHGCSimAccumulator"),                                                                               
+    }                                                                                                                             
+) 
 
 from Configuration.Eras.Modifier_phase2_hfnose_cff import phase2_hfnose
 (~phase2_hfnose).toModify(simHFNoseUnsuppressedDigis, mix = None)
