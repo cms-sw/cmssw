c94e88c619cf8357782e1c1c27e1402e22ddd1b1
Marco Rovere <marco.rovere@cern.ch>
2020-05-01 09:11:35 +0200

    First implementation of a TICL debugger module

---
+++ RecoHGCal/TICL/test/ticlDebugger_cfg.py
@@ -0,0 +1,23 @@
+import FWCore.ParameterSet.Config as cms
+
+process = cms.Process("TICLDEBUG")
+
+process.load("FWCore.MessageService.MessageLogger_cfi")
+process.load('Configuration.Geometry.GeometryExtended2026D49Reco_cff')
+process.load('Configuration.StandardSequences.FrontierConditions_GlobalTag_cff')
+from Configuration.AlCa.GlobalTag import GlobalTag
+process.GlobalTag = GlobalTag(process.GlobalTag, 'auto:phase2_realistic_T15', '')
+
+process.maxEvents = cms.untracked.PSet( input = cms.untracked.int32(-1) )
+
+process.source = cms.Source("PoolSource",
+    # replace 'myfile.root' with the source file you want to use
+    fileNames = cms.untracked.vstring(
+        'file:step3.root'
+    )
+)
+
+process.load("RecoHGCal.TICL.ticlDebugger_cfi")
+
+process.p = cms.Path(process.ticlDebugger)
+
