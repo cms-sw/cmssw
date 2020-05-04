af888c62e0d541f180863a7461336a6641d1f58e
Andrea Bocci <andrea.bocci@cern.ch>
2020-05-05 00:56:25 +0200

    Add ECAL-only PF modules to the ECAL-only reconstruction sequence

--- Configuration/StandardSequences/python/Reconstruction_cff.py
+++ Configuration/StandardSequences/python/Reconstruction_cff.py
@@ -219,8 +219,12 @@ reconstruction_pixelTrackingOnly = cms.Sequence(reconstruction_pixelTrackingOnly
 
 reconstruction_ecalOnlyTask = cms.Task(
     bunchSpacingProducer,
+    offlineBeamSpot,
     ecalLocalRecoTask,
-    ecalClustersNoPFBoxTask
+    ecalClustersNoPFBoxTask,
+    pfClusteringPSTask,
+    pfClusteringECALTask,
+    particleFlowSuperClusterECALHLT
 )
 reconstruction_ecalOnly = cms.Sequence(reconstruction_ecalOnlyTask)
 
