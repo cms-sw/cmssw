0ffbb8b6c2e33397263df1089a1efaa2fedce340
bfontana <bruno.alves@cern.ch>
2020-05-06 15:43:17 +0200

    code working version prior ESProduct usage for Geometry and Topology

--- CUDADataFormats/HGCal/interface/HGCRecHitSoA.h
+++ CUDADataFormats/HGCal/interface/HGCRecHitSoA.h
@@ -1,15 +1,15 @@
-#ifndef CUDADATAFORMATS_HGCRECHITSOA_H
-#define CUDADATAFORMATS_HGCRECHITSOA_H 1
+#ifndef CudaDataFormats_HGCal_HGCRecHitSoA_h
+#define CudaDataFormats_HGCal_HGCRecHitSoA_h
 
 class HGCRecHitSoA {
  public:
-  float *energy;
-  float *time;
-  float *timeError;
-  uint32_t *id;
-  uint32_t *flagBits;
-  uint8_t *son;
-  int nbytes;
+  float *energy_; //calibrated energy of the rechit
+  float *time_; //time jitter of the UncalibRecHit
+  float *timeError_; //time resolution
+  uint32_t *id_; //rechit detId
+  uint32_t *flagBits_; //rechit flags describing its status (DataFormats/HGCRecHit/interface/HGCRecHit.h)
+  uint8_t *son_; //signal over noise
+  int nbytes_; //number of bytes of the SoA
 };
 
-#endif //CUDADATAFORMATS_HGCRECHITSOA_H
+#endif
