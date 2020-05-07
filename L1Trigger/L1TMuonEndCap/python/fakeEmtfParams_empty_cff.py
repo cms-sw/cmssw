891165134241a7d51913abf216a77dcaca009c10
Jia Fu Low <jia.fu.low@cern.ch>
2020-05-07 06:37:19 -0400

    [EMTF] Update python/fakeEmtfParams_*_cff.py

--- L1Trigger/L1TMuonEndCap/python/fakeEmtfParams_empty_cff.py
+++ L1Trigger/L1TMuonEndCap/python/fakeEmtfParams_empty_cff.py
@@ -29,7 +29,7 @@ emtfParams = cms.ESProducer(
     )
 
 
-## Fills pT LUT XMLs ("forests") from the database
+## Fills pT LUT XMLs ("forests") manually
 emtfForestsSource = cms.ESSource(
     "EmptyESSource",
     recordName = cms.string('L1TMuonEndCapForestRcd'),
@@ -37,27 +37,9 @@ emtfForestsSource = cms.ESSource(
     firstValid = cms.vuint32(1)
     )
 
-#emtfForestsDB = cms.ESSource(
-#    "PoolDBESSource",
-#    CondDB,
-#    toGet   = cms.VPSet(
-#        cms.PSet(
-#            ## https://cms-conddb.cern.ch/cmsDbBrowser/search/Prod/L1TMuonEndCapForest
-#            record = cms.string("L1TMuonEndCapForestRcd"),
-#
-#            # ## v5 EMTF pT LUTs from ~August 2016
-#            # tag = cms.string("L1TMuonEndCapForest_static_2016_mc")
-#            # ## v6 EMTF pT LUTs from May 24, 2017
-#            # tag = cms.string("L1TMuonEndCapForest_static_Sq_20170523_mc")
-#            ## v7 EMTF pT LUTs from June 7, 2017 - AWB 07.06.17
-#            tag = cms.string("L1TMuonEndCapForest_static_Sq_20170613_v7_mc")
-#            )
-#        )
-#    )
-
-# ## EMTF ESProducer. Fills CondFormats from local XML files instead of database.
-# emtfForests = cms.ESProducer(
-#     "L1TMuonEndCapForestESProducer",
-#     PtAssignVersion = cms.int32(7),
-#     bdtXMLDir = cms.string("2017_v7")
-#     )
+## Fills CondFormats from local XML files instead of database.
+emtfForests = cms.ESProducer(
+    "L1TMuonEndCapForestESProducer",
+    PtAssignVersion = cms.int32(7),
+    bdtXMLDir = cms.string("2017_v7")
+    )
