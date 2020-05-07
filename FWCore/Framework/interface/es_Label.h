ca51706b183f740b9b4bfa3ab430b255dbf0b806
Chris Jones <chrisdjones15@gmail.com>
2020-05-06 19:35:08 -0500

    Add const access

--- FWCore/Framework/interface/es_Label.h
+++ FWCore/Framework/interface/es_Label.h
@@ -43,6 +43,8 @@ namespace edm::es {
 
     T& operator*() { return *product_; }
     T* operator->() { return product_.get(); }
+    T const& operator*() const { return *product_; }
+    T const* operator->() const { return product_.get(); }
     mutable std::shared_ptr<T> product_{nullptr};
   };
 
