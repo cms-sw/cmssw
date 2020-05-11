adfb42a21a15b2ee2d4a94ce9dac10c9df1b80ac
Lauren Hay <lmhay@buffalo.edu>
2020-05-11 20:34:39 +0200

    Changed name of SetMerger to Unique Merger and added comments.

--- CommonTools/CandAlgos/plugins/CandPtrMerger.cc
+++ CommonTools/CandAlgos/plugins/CandPtrMerger.cc
@@ -6,15 +6,15 @@
  *
  */
 #include "FWCore/Framework/interface/MakerMacros.h"
-#include "CommonTools/UtilAlgos/interface/SetMerger.h"
+#include "CommonTools/UtilAlgos/interface/UniqueMerger.h"
 #include "DataFormats/Candidate/interface/Candidate.h"
 #include "DataFormats/PatCandidates/interface/PackedCandidate.h"
 #include "DataFormats/PatCandidates/interface/PackedGenParticle.h"
 #include "DataFormats/Common/interface/Ptr.h"
 
-typedef SetMerger<std::vector<edm::Ptr<reco::Candidate>>> CandPtrMerger;
-typedef SetMerger<std::vector<edm::Ptr<pat::PackedCandidate>>> PackedCandidatePtrMerger;
-typedef SetMerger<std::vector<edm::Ptr<pat::PackedGenParticle>>> PackedGenParticlePtrMerger;
+typedef UniqueMerger<std::vector<edm::Ptr<reco::Candidate>>> CandPtrMerger;
+typedef UniqueMerger<std::vector<edm::Ptr<pat::PackedCandidate>>> PackedCandidatePtrMerger;
+typedef UniqueMerger<std::vector<edm::Ptr<pat::PackedGenParticle>>> PackedGenParticlePtrMerger;
 
 DEFINE_FWK_MODULE(CandPtrMerger);
 DEFINE_FWK_MODULE(PackedCandidatePtrMerger);
