b426a895db17855d3190a5ccd6eaf7d6f2939cc5
Andrea Bocci <andrea.bocci@cern.ch>
2020-05-11 14:49:56 +0200

    Synchronise with CMSSW_11_1_0_pre7

--- Configuration/StandardSequences/python/RawToDigi_cff.py
+++ Configuration/StandardSequences/python/RawToDigi_cff.py
@@ -67,6 +67,9 @@ RawToDigi_noTk = cms.Sequence(RawToDigiTask_noTk)
 RawToDigiTask_pixelOnly = cms.Task(siPixelDigisTask, scalersRawToDigi)
 RawToDigi_pixelOnly = cms.Sequence(RawToDigiTask_pixelOnly)
 
+RawToDigiTask_ecalOnly = cms.Task(ecalDigis, ecalPreshowerDigis, scalersRawToDigi)
+RawToDigi_ecalOnly = cms.Sequence(RawToDigiTask_ecalOnly)
+
 scalersRawToDigi.scalersInputTag = 'rawDataCollector'
 siPixelDigis.cpu.InputLabel = 'rawDataCollector'
 #false by default anyways ecalDigis.DoRegional = False
