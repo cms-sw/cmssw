ec24b98fcf43fbdeb679ad4c1fcca536298d456b
andrius <andrius.kirilovas@cern.ch>
2020-05-11 17:50:59 +0200

    New version of frontend supporting new layouts

---
+++ DQMServices/DQMGUI/data/_next/static/vKKCqxI2Wc-FZWraIvd2w/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST = (function(a){return {"/":[a],"/index":[a]}}("static\u002Fchunks\u002F29107295.3cb75105cf6d50f14f0a.js"));self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
