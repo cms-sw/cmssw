3fa9d0a2af689f2b60b6f5f428904ea7da10b8af
Jeongeun Lee <jeongeun.lee@cern.ch>
2020-05-12 22:56:23 +0200

    update eventcontent structure and relax wildcards

--- RecoHI/HiJetAlgos/python/RecoHiJets_EventContent_cff.py
+++ RecoHI/HiJetAlgos/python/RecoHiJets_EventContent_cff.py
@@ -4,14 +4,19 @@ import FWCore.ParameterSet.Config as cms
 RecoHiJetsAOD = cms.PSet(
     outputCommands = cms.untracked.vstring(
         'keep *_ak4CaloJets_*_*',
+        'keep *_akPu3CaloJets_*_*', 
         'keep *_akPu4CaloJets_*_*', 
+        'keep *_akPu5CaloJets_*_*', 
+        'keep *_iterativeConePu5CaloJets_*_*', 
         'keep *_ak4PFJets_*_*',
         'keep *_akPu3PFJets_*_*',
         'keep *_akPu4PFJets_*_*',
+        'keep *_akPu5PFJets_*_*',
         'keep *_akCs3PFJets_*_*',
         'keep *_akCs4PFJets_*_*',
-        'keep *_PFTowers_*_*',
-        'keep *_hiFJRhoProducer_*_*',
+        'keep *_*HiGenJets_*_*',
+        'keep *_*PFTowers_*_*',
+        'keep *_*hiFJRhoProducer_*_*',
         'keep CaloTowersSorted_towerMaker_*_*',
         'keep recoPFCandidates_particleFlowTmp_*_*')
 )
@@ -19,11 +24,6 @@ RecoHiJetsAOD = cms.PSet(
 # RECO content
 RecoHiJetsRECO = cms.PSet(
     outputCommands = cms.untracked.vstring( 
-        'keep *_akPu3CaloJets_*_*', 
-        'keep *_akPu5CaloJets_*_*', 
-        'keep *_iterativeConePu5CaloJets_*_*', 
-        'keep *_akPu5PFJets_*_*',
-        'keep recoCandidatesOwned_caloTowers_*_*',
         'keep *_kt4PFJetsForRho_*_*')
 )
 RecoHiJetsRECO.outputCommands.extend(RecoHiJetsAOD.outputCommands)
