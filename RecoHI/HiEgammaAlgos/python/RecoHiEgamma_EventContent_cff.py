5aa000b7ac62e4c600892c61ff574c7dbcc7cc14
Jeongeun Lee <jeongeun.lee@cern.ch>
2020-05-12 22:39:30 +0200

    update eventcontent structure end, remove unsed drop

--- RecoHI/HiEgammaAlgos/python/RecoHiEgamma_EventContent_cff.py
+++ RecoHI/HiEgammaAlgos/python/RecoHiEgamma_EventContent_cff.py
@@ -17,17 +17,23 @@ RecoHiEgammaAOD = cms.PSet(
 # RECO content
 RecoHiEgammaRECO = cms.PSet(
     outputCommands = cms.untracked.vstring(
-        'keep recoSuperClusters_correctedHybridSuperClusters_*_*',        
+        'keep recoSuperClusters_cleanedHybridSuperClusters_*_*',
+        'keep recoSuperClusters_correctedEndcapSuperClustersWithPreshower_*_*', 
+        'keep recoSuperClusters_correctedHybridSuperClusters_*_*',
         'keep recoSuperClusters_correctedMulti5x5SuperClustersWithPreshower_*_*',
-        'keep recoSuperClusters_hfEMClusters_*_*',
         'keep recoSuperClusters_hybridSuperClusters_*_*',
+        'keep recoSuperClusters_islandSuperClusters_*_*',
+        'keep recoSuperClusters_mergedSuperClusters_*_*',
         'keep recoSuperClusters_multi5x5SuperClusters_*_*',
+        'keep recoSuperClusters_multi5x5SuperClustersCleaned_*_*',
+        'keep recoSuperClusters_multi5x5SuperClustersUncleaned_*_*',
         'keep recoSuperClusters_multi5x5SuperClustersWithPreshower_*_*',
         'keep recoSuperClusters_particleFlowEGamma_*_*',
         'keep recoSuperClusters_particleFlowSuperClusterECAL_*_*',
-        'keep recoSuperClusters_particleFlowSuperClusterOOTECAL_*_*',
-        'keep recoSuperClusters_lowPtGsfElectronSuperClusters_*_*',
-        'keep recoCaloClusters_hfEMClusters_*_*',
+        'keep recoSuperClusters_uncleanedHybridSuperClusters_*_*',
+        'keep recoSuperClusters_uncleanedOnlyCorrectedHybridSuperClusters_*_*',
+        'keep recoSuperClusters_uncleanedOnlyCorrectedMulti5x5SuperClustersWithPreshower_*_*',
+        'keep recoSuperClusters_uncleanedOnlyMulti5x5SuperClustersWithPreshower_*_*',
         'keep recoCaloClusters_particleFlowEGamma_*_*',
         'keep recoCaloClusters_cleanedHybridSuperClusters_*_*',
         'keep recoCaloClusters_hybridSuperClusters_*_*',
@@ -37,31 +43,17 @@ RecoHiEgammaRECO = cms.PSet(
         'keep recoCaloClusters_multi5x5BasicClustersUncleaned_*_*',
         'keep recoCaloClusters_multi5x5SuperClusters_*_*',
         'keep recoCaloClusters_particleFlowSuperClusterECAL_*_*',
-        'keep recoCaloClusters_particleFlowSuperClusterOOTECAL_*_*',
-        'keep recoCaloClusters_lowPtGsfElectronSuperClusters_*_*',
-        'keep recoCaloClusters_pfElectronTranslator_*_*',
-        'keep recoCaloClusters_pfPhotonTranslator_*_*',
+        'keep recoCaloClusters_multi5x5SuperClusters_*_*',
         'keep EcalRecHitsSorted_ecalRecHit_*_*',
         'keep EcalRecHitsSorted_ecalPreshowerRecHit_*_*',
         'keep EBSrFlagsSorted_ecalDigis__*',
         'keep EESrFlagsSorted_ecalDigis__*',
-        'keep floatedmValueMap_hiDetachedTripletStepQual_MVAVals_*',        
-        'keep floatedmValueMap_hiDetachedTripletStepSelector_MVAVals_*',    
-        'keep floatedmValueMap_hiGeneralTracks_MVAVals_*',                  
-        'keep floatedmValueMap_hiInitialStepSelector_MVAVals_*',            
-        'keep floatedmValueMap_hiLowPtTripletStepQual_MVAVals_*',           
-        'keep floatedmValueMap_hiLowPtTripletStepSelector_MVAVals_*', 
-        'keep floatedmValueMap_hiPixelPairStepSelector_MVAVals_*',          
-        'keep floatedmValueMap_hiRegitMuInitialStepSelector_MVAVals_*', 
-        'keep floatedmValueMap_hiRegitMuMixedTripletStepSelector_MVAVals_*',
-        'keep floatedmValueMap_hiRegitMuPixelLessStepSelector_MVAVals_*', 
-        'keep floatedmValueMap_hiRegitMuPixelPairStepSelector_MVAVals_*', 
         'keep recoPFCandidates_particleFlowEGamma_*_*',
         'keep recoPFCandidates_particleFlowTmp_*_*',
-        "keep recoGsfElectrons_ecalDrivenGsfElectrons_*_*",
-        "keep recoGsfElectrons_electronsWithPresel_*_*",
-        "keep recoGsfElectrons_gedGsfElectronsTmp_*_*",
-        "keep recoGsfElectrons_mvaElectrons_*_*")
+        'keep recoGsfElectrons_ecalDrivenGsfElectrons_*_*',
+        'keep recoGsfElectrons_electronsWithPresel_*_*',
+        'keep recoGsfElectrons_gedGsfElectronsTmp_*_*',
+        'keep recoGsfElectrons_mvaElectrons_*_*')
 )
 RecoHiEgammaRECO.outputCommands.extend(RecoHiEgammaAOD.outputCommands)
 
@@ -71,9 +63,8 @@ RecoHiEgammaFEVT = cms.PSet(
         'keep recoSuperClusters_*_*_*',
         'keep recoCaloClusters_*_*_*',
         'keep EcalRecHitsSorted_*_*_*',
-        'keep floatedmValueMap_*_*_*',
         'keep recoPFCandidates_*_*_*',
-        "keep recoElectronSeeds_*_*_*",
-        "keep recoGsfElectrons_*_*_*")
+        'keep recoElectronSeeds_*_*_*',
+        'keep recoGsfElectrons_*_*_*')
 )
 RecoHiEgammaFEVT.outputCommands.extend(RecoHiEgammaRECO.outputCommands)
