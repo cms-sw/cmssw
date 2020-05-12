33f70c3b6af3c576083ba01a1b1f1650f2ebe574
Matti Kortelainen <matti.kortelainen@cern.ch>
2020-05-12 17:01:46 +0200

    Move DT calibration DQM modules to Tasks to preserve the current behavior, to be reverted later

--- DQM/DTMonitorModule/python/ALCARECODTCalibSynchCosmicsDQM_cff.py
+++ DQM/DTMonitorModule/python/ALCARECODTCalibSynchCosmicsDQM_cff.py
@@ -31,6 +31,8 @@ from CalibMuon.DTCalibration.ALCARECODtCalibCosmics_cff import ALCARECODtCalibCo
 #                                               dtAlcaResolutionMonitorCosmics + 
 #                                               ALCARECODtCalibCosmicsHLTDQM )
 
-ALCARECODTCalibSynchCosmicsDQM = cms.Sequence( dtPreCalibrationTaskAlcaCosmics +
-                                               dtAlcaResolutionMonitorCosmics )
+# Short-term workaround to preserve the "run for every event" while removing the use of convertToUnscheduled()
+# To be reverted in a subsequent PR
+ALCARECODTCalibSynchCosmicsDQMTask = cms.Task(dtPreCalibrationTaskAlcaCosmics, dtAlcaResolutionMonitorCosmics)
+ALCARECODTCalibSynchCosmicsDQM = cms.Sequence(ALCARECODTCalibSynchCosmicsDQMTask)
 
