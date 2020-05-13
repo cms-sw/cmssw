3371411d28cc10ebbbfbc8f327b89f429ff4eb8e
Jan Kaspar <jan.kaspar@cern.ch>
2020-05-13 17:06:04 +0200

    Updated near-far association cuts.

--- RecoPPS/ProtonReconstruction/python/ctppsProtons_cff.py
+++ RecoPPS/ProtonReconstruction/python/ctppsProtons_cff.py
@@ -42,7 +42,7 @@ def apply2017Settings(ctppsProtons):
 
   ctppsProtons.association_cuts_45.xi_cut_apply = True
   ctppsProtons.association_cuts_45.xi_cut_value = 5. * 0.00121
-  ctppsProtons.association_cuts_45.xi_cut_mean = -6.0695e-5
+  ctppsProtons.association_cuts_45.xi_cut_mean = +6.0695e-5
 
   ctppsProtons.association_cuts_45.th_y_cut_apply = False
 
@@ -50,11 +50,11 @@ def apply2017Settings(ctppsProtons):
 
   ctppsProtons.association_cuts_56.y_cut_apply = True
   ctppsProtons.association_cuts_56.y_cut_value = 5. * 0.14777
-  ctppsProtons.association_cuts_56.y_cut_mean = 0.022612
+  ctppsProtons.association_cuts_56.y_cut_mean = -0.022612
 
   ctppsProtons.association_cuts_56.xi_cut_apply = True
   ctppsProtons.association_cuts_56.xi_cut_value = 5. * 0.0020627
-  ctppsProtons.association_cuts_56.xi_cut_mean = -8.012857e-5
+  ctppsProtons.association_cuts_56.xi_cut_mean = +8.012857e-5
 
   ctppsProtons.association_cuts_56.th_y_cut_apply = False
 
@@ -62,30 +62,30 @@ ctpps_2017.toModify(ctppsProtons, apply2017Settings)
 
 def apply2018Settings(ctppsProtons):
   ctppsProtons.association_cuts_45.x_cut_apply = True
-  ctppsProtons.association_cuts_45.x_cut_value = 5. * 0.16008188
-  ctppsProtons.association_cuts_45.x_cut_mean = 0.065194856
+  ctppsProtons.association_cuts_45.x_cut_value = 4. * 0.16008188
+  ctppsProtons.association_cuts_45.x_cut_mean = -0.065194856
 
   ctppsProtons.association_cuts_45.y_cut_apply = True
-  ctppsProtons.association_cuts_45.y_cut_value = 5. * 0.1407986
-  ctppsProtons.association_cuts_45.y_cut_mean = -0.10973631
+  ctppsProtons.association_cuts_45.y_cut_value = 4. * 0.1407986
+  ctppsProtons.association_cuts_45.y_cut_mean = +0.10973631
 
   ctppsProtons.association_cuts_45.xi_cut_apply = True
-  ctppsProtons.association_cuts_45.xi_cut_value = 5. * 0.0012403586
-  ctppsProtons.association_cuts_45.xi_cut_mean = -3.113062e-5
+  ctppsProtons.association_cuts_45.xi_cut_value = 4. * 0.0012403586
+  ctppsProtons.association_cuts_45.xi_cut_mean = +3.113062e-5
 
   ctppsProtons.association_cuts_45.th_y_cut_apply = False
 
   ctppsProtons.association_cuts_56.x_cut_apply = True
   ctppsProtons.association_cuts_56.x_cut_value = 5. * 0.18126434
-  ctppsProtons.association_cuts_56.x_cut_mean = -0.073016431
+  ctppsProtons.association_cuts_56.x_cut_mean = +0.073016431
 
   ctppsProtons.association_cuts_56.y_cut_apply = True
   ctppsProtons.association_cuts_56.y_cut_value = 5. * 0.14990802
-  ctppsProtons.association_cuts_56.y_cut_mean = -0.064261029
+  ctppsProtons.association_cuts_56.y_cut_mean = +0.064261029
 
   ctppsProtons.association_cuts_56.xi_cut_apply = True
   ctppsProtons.association_cuts_56.xi_cut_value = 5. * 0.002046409
-  ctppsProtons.association_cuts_56.xi_cut_mean = 1.1852528e-5
+  ctppsProtons.association_cuts_56.xi_cut_mean = -1.1852528e-5
 
   ctppsProtons.association_cuts_56.th_y_cut_apply = False
 
