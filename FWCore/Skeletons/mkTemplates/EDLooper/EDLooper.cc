44cd581d2ec960ffbac106b0873861632860bfa9
Matti Kortelainen <matti.kortelainen@cern.ch>
2020-05-13 03:47:21 +0200

    Update EDLooper template to conform clang-format

--- FWCore/Skeletons/mkTemplates/EDLooper/EDLooper.cc
+++ FWCore/Skeletons/mkTemplates/EDLooper/EDLooper.cc
@@ -2,7 +2,7 @@
 //
 // Package:    __subsys__/__pkgname__
 // Class:      __class__
-// 
+//
 /**\class __class__ __class__.cc __subsys__/__pkgname__/plugins/__class__.cc
 
  Description: [one line class summary]
@@ -16,7 +16,6 @@
 //
 //
 
-
 // system include files
 #include <memory>
 
@@ -28,35 +27,35 @@
 
 #include "FWCore/Framework/interface/ESProducts.h"
 
-
 //
 // class declaration
 //
 
 class __class__ : public edm::ESProducerLooper {
-   public:
-      __class__(const edm::ParameterSet&);
-      ~__class__();
+public:
+  __class__(const edm::ParameterSet&);
+  ~__class__();
 
 #python_begin
     if  len(__datatypes__) > 1:
         datatypes = []
         for dtype in __datatypes__:
             datatypes.append("std::unique_ptr<%s>" % dtype)
-        print "      typedef edm::ESProducts<%s> ReturnType;" % ','.join(datatypes)
+        print("  using ReturnType = edm::ESProducts<%s>;" % ','.join(datatypes))
     elif len(__datatypes__) == 1:
-        print "      typedef std::shared_ptr<%s> ReturnType;" % __datatypes__[0]
+        print("  using ReturnType = std::shared_ptr<%s>;" % __datatypes__[0])
 #python_end
 
-      ReturnType produce(const recordname&);
+  ReturnType produce(const __record__&);
 
-      virtual void beginOfJob(); 
-      virtual void startingNewLoop(unsigned int ) ; 
-      virtual Status duringLoop(const edm::Event&, const edm::EventSetup&) ; 
-      virtual Status endOfLoop(const edm::EventSetup&); 
-      virtual void endOfJob();
+  void beginOfJob() override;
+  void startingNewLoop(unsigned int) override;
+  Status duringLoop(const edm::Event&, const edm::EventSetup&) override;
+  Status endOfLoop(const edm::EventSetup&) override;
+  void endOfJob() override;
+
 private:
-      // ----------member data ---------------------------
+  // ----------member data ---------------------------
 };
 
 //
@@ -70,79 +69,60 @@ private:
 //
 // constructors and destructor
 //
-__class__::__class__(const edm::ParameterSet& iConfig)
-{
-   //the following line is needed to tell the framework what
-   // data is being produced
-   setWhatProduced(this);
+__class__::__class__(const edm::ParameterSet& iConfig) {
+  //the following line is needed to tell the framework what
+  // data is being produced
+  setWhatProduced(this);
 
-   //now do what ever other initialization is needed
+  //now do what ever other initialization is needed
 }
 
-
-__class__::~__class__()
-{
- 
-   // do anything here that needs to be done at desctruction time
-   // (e.g. close files, deallocate resources etc.)
-
+__class__::~__class__() {
+  // do anything here that needs to be done at desctruction time
+  // (e.g. close files, deallocate resources etc.)
 }
-
 
 //
 // member functions
 //
 
 // ------------ method called to produce the data  ------------
-__class__::ReturnType
-__class__::produce(const recordname& iRecord)
-{
-   using namespace edm::es;
+__class__::ReturnType __class__::produce(const __record__& iRecord) {
+  using namespace edm::es;
 #python_begin
     out1 = []
     out2 = []
     for dtype in __datatypes__:
-        out1.append("   std::unique_ptr<%s> p%s;" % (dtype, dtype))
+        out1.append("  std::unique_ptr<%s> p%s;" % (dtype, dtype))
         out2.append("p%s" % dtype)
     output  = '\n'.join(out1)
-    output += "\n   return products(%s);\n" % ','.join(out2)
-    print output
+    output += "\n  return products(%s);" % ','.join(out2)
+    print(output)
 #python_end
 }
 
-
 // ------------ method called once per job just before starting to loop over events  ------------
-void 
-__class__::beginOfJob(const edm::EventSetup&)
-{
+void __class__::beginOfJob(const edm::EventSetup&) {
 }
 
 // ------------ method called at the beginning of a new loop over the event  ------------
 // ------------ the argument starts at 0 and increments for each loop        ------------
-void 
-__class__::startingNewLoop(unsigned int iIteration)
-{
+void __class__::startingNewLoop(unsigned int iIteration) {
 }
 
 // ------------ called for each event in the loop.  The present event loop can be stopped by return kStop ------------
-__class__::Status 
-__class__::duringLoop(const edm::Event&, const edm::EventSetup&)
-{
+__class__::Status __class__::duringLoop(const edm::Event&, const edm::EventSetup&) {
   return kContinue;
 }
 
 
 // ------------ called at the end of each event loop. A new loop will occur if you return kContinue ------------
-__class__::Status 
-__class__::endOfLoop(const edm::EventSetup&, unsigned int)
-{
+__class__::Status __class__::endOfLoop(const edm::EventSetup&, unsigned int) {
   return kStop;
 }
 
 // ------------ called once each job just before the job ends ------------
-void 
-__class__::endOfJob()
-{
+void __class__::endOfJob() {
 }
 
 //define this as a plug-in
