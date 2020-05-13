c3e6c08a73a8a40a891f66eb09c8cfb4a86f2e79
Javier <Javier.Fernandez@cern.ch>
2020-05-13 10:13:35 +0200

    Removed unneeded import

--- DQM/DTMonitorClient/python/dtDQMOfflineCertification_cff.py
+++ DQM/DTMonitorClient/python/dtDQMOfflineCertification_cff.py
@@ -1,7 +1,6 @@
 import FWCore.ParameterSet.Config as cms
 
 from DQM.DTMonitorClient.dtDAQInfo_cfi import *
-from DQM.DTMonitorClient.dtDCSByLumiSummary_cfi import *
 from DQM.DTMonitorClient.dtCertificationSummary_cfi import *
 
 dtCertification = cms.Sequence(dtDAQInfo + dtCertificationSummary)
