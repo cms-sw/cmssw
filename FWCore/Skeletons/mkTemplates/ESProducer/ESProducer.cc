dd4d70e915ce6f4d535feeb1eb52cacba0fc30d1
Matti Kortelainen <matti.kortelainen@cern.ch>
2020-05-13 21:29:13 +0200

    Update ESProducer template to conform clang-format as close as feasible

--- FWCore/Skeletons/mkTemplates/ESProducer/ESProducer.cc
+++ FWCore/Skeletons/mkTemplates/ESProducer/ESProducer.cc
@@ -2,7 +2,7 @@
 //
 // Package:    __subsys__/__pkgname__
 // Class:      __class__
-// 
+//
 /**\class __class__
 
  Description: [one line class summary]
@@ -42,23 +42,24 @@
 //
 
 class __class__ : public edm::ESProducer {
-   public:
-      __class__(const edm::ParameterSet&);
-      ~__class__();
+public:
+  __class__(const edm::ParameterSet&);
+  ~__class__();
 
 #python_begin
     if  len(__datatypes__) > 1:
         datatypes = []
         for dtype in __datatypes__:
             datatypes.append("std::unique_ptr<%s>" % dtype)
-        print("      using ReturnType = edm::ESProducts<%s>;" % ','.join(datatypes))
+        print("  using ReturnType = edm::ESProducts<%s>;" % ', '.join(datatypes))
     elif len(__datatypes__) == 1:
-        print("      using ReturnType = std::unique_ptr<%s>;" % __datatypes__[0])
+        print("  using ReturnType = std::unique_ptr<%s>;" % __datatypes__[0])
 #python_end
 
-      ReturnType produce(const __record__&);
-   private:
-      // ----------member data ---------------------------
+  ReturnType produce(const __record__&);
+
+private:
+  // ----------member data ---------------------------
 };
 
 //
@@ -72,22 +73,17 @@ class __class__ : public edm::ESProducer {
 //
 // constructors and destructor
 //
-__class__::__class__(const edm::ParameterSet& iConfig)
-{
-   //the following line is needed to tell the framework what
-   // data is being produced
-   setWhatProduced(this);
+__class__::__class__(const edm::ParameterSet& iConfig) {
+  //the following line is needed to tell the framework what
+  // data is being produced
+  setWhatProduced(this);
 
-   //now do what ever other initialization is needed
+  //now do what ever other initialization is needed
 }
 
-
-__class__::~__class__()
-{
- 
-   // do anything here that needs to be done at destruction time
-   // (e.g. close files, deallocate resources etc.)
-
+__class__::~__class__() {
+  // do anything here that needs to be done at destruction time
+  // (e.g. close files, deallocate resources etc.)
 }
 
 //
@@ -95,30 +91,28 @@ __class__::~__class__()
 //
 
 // ------------ method called to produce the data  ------------
-__class__::ReturnType
-__class__::produce(const __record__& iRecord)
-{
-   // You can add arguments to the make_unique function call
-   // and they will be forwarded to the constructor of the
-   // data object. Also you can call functions that modify
-   // the data object after creating it. Often, before this
-   // you will retrieve data from the EventSetup through the
-   // record.
+__class__::ReturnType __class__::produce(const __record__& iRecord) {
+  // You can add arguments to the make_unique function call
+  // and they will be forwarded to the constructor of the
+  // data object. Also you can call functions that modify
+  // the data object after creating it. Often, before this
+  // you will retrieve data from the EventSetup through the
+  // record.
 #python_begin
     if  len(__datatypes__) > 1:
         out1 = []
         out2 = []
         i = 1
         for dtype in __datatypes__:
-            out1.append("   auto p%s = std::make_unique<%s>();" % (i, dtype))
+            out1.append("  auto p%s = std::make_unique<%s>();" % (i, dtype))
             out2.append("std::move(p%s)" % i)
             i = i + 1
         output  = '\n'.join(out1)
-        output += "\n   return edm::es::products(%s);" % ','.join(out2)
+        output += "\n  return edm::es::products(%s);" % ', '.join(out2)
         print(output)
     elif len(__datatypes__) == 1:
-        print("   auto product = std::make_unique<%s>();" % __datatypes__[0])
-        print("   return product;")
+        print("  auto product = std::make_unique<%s>();" % __datatypes__[0])
+        print("  return product;")
 #python_end
 }
 
