5e14cca14e06f529a0c50b58e4f126f0405e6e59
Luiz Mundim <luiz.mundim@cern.ch>
2020-05-14 07:19:14 +0200

    New transport method for pps proton transport

---
+++ SimTransport/PPSProtonTransport/python/PPSTransportESSources_cfi.py
@@ -0,0 +1,52 @@
+import FWCore.ParameterSet.Config as cms
+
+# beam optics
+#from CalibPPS.ESProducers.ctppsBeamParametersFromLHCInfoESSource_cfi import *
+from CalibPPS.ESProducers.ctppsOpticalFunctionsESSource_cfi import *
+from CalibPPS.ESProducers.ctppsBeamParametersESSource_cfi import *
+
+from CalibPPS.ESProducers.ctppsInterpolatedOpticalFunctionsESSource_cfi import *
+ctppsInterpolatedOpticalFunctionsESSource.lhcInfoLabel = ""
+
+from SimTransport.PPSProtonTransport.OpticalFunctionsConfig_cfi import *
+
+from CondCore.CondDB.CondDB_cfi import *
+ppsDBESSource = cms.ESSource("PoolDBESSource",
+    label = cms.string(""),
+    validityRange = cms.EventRange("0:min - 999999:max"),
+    beamEnergy = cms.double(6500),
+    xangle = cms.double(-1),
+    timetype = cms.untracked.string('runnumber'),
+    DumpStat=cms.untracked.bool(False),
+    toGet = cms.VPSet(
+                cms.PSet(
+                    record = cms.string('LHCInfoRcd'),
+                    tag = cms.string("LHCInfoEndFill_prompt_v2"),  #  FrontierProd
+                    connect = cms.string("frontier://FrontierProd/CMS_CONDITIONS")
+                ),
+                cms.PSet(
+                    record = cms.string('CTPPSOpticsRcd'),
+                    tag = cms.string("PPSOpticalFunctions_offline_v6"),
+                    connect = cms.string("frontier://FrontierProd/CMS_CONDITIONS"),
+                )
+            )
+)
+es_prefer_ppsDBESSource = cms.ESPrefer("PoolDBESSource","ppsDBESSource")
+
+_opticsConfig = opticalfunctionsTransportSetup_2018.opticalFunctionConfig # use 2018 as default
+
+from Configuration.Eras.Modifier_ctpps_2016_cff import ctpps_2016
+ctpps_2016.toReplaceWith(_opticsConfig, opticalfunctionsTransportSetup_2016.opticalFunctionConfig)
+
+from Configuration.Eras.Modifier_ctpps_2017_cff import ctpps_2017
+ctpps_2017.toReplaceWith(_opticsConfig, opticalfunctionsTransportSetup_2017.opticalFunctionConfig)
+
+from Configuration.Eras.Modifier_ctpps_2018_cff import ctpps_2018
+ctpps_2018.toReplaceWith(_opticsConfig, opticalfunctionsTransportSetup_2018.opticalFunctionConfig)
+
+from Configuration.Eras.Modifier_ctpps_2021_cff import ctpps_2021
+ctpps_2021.toReplaceWith(_opticsConfig, opticalfunctionsTransportSetup_2018.opticalFunctionConfig)
+
+ctppsBeamParametersESSource.halfXangleX45 = _opticsConfig.defaultCrossingAngle
+ctppsBeamParametersESSource.halfXangleX56 = _opticsConfig.defaultCrossingAngle
+ctppsOpticalFunctionsESSource.configuration.append(_opticsConfig.es_source)
--- SimTransport/PPSProtonTransport/src/TotemTransport.cc
+++ SimTransport/PPSProtonTransport/src/TotemTransport.cc
@@ -7,80 +7,88 @@
 
 #include <cmath>
 
-TotemTransport::TotemTransport() : ProtonTransport() { MODE = TransportMode::TOTEM; };
-TotemTransport::~TotemTransport() { this->clear(); }
-TotemTransport::TotemTransport(const edm::ParameterSet& iConfig, bool verbosity)
-    : ProtonTransport(),
-      m_parameters(iConfig.getParameter<edm::ParameterSet>("BeamProtTransportSetup")),
-      m_verbosity(iConfig.getParameter<bool>("Verbosity")),
-      m_model_root_file_r(m_parameters.getParameter<std::string>("ModelRootFile_R")),
-      m_model_root_file_l(m_parameters.getParameter<std::string>("ModelRootFile_L")),
-      m_model_ip_150_r_name(m_parameters.getParameter<std::string>("Model_IP_150_R_Name")),
-      m_model_ip_150_l_name(m_parameters.getParameter<std::string>("Model_IP_150_L_Name")),
-      m_model_ip_150_r_zmin(m_parameters.getParameter<double>("Model_IP_150_R_Zmin")),
-      m_model_ip_150_r_zmax(m_parameters.getParameter<double>("Model_IP_150_R_Zmax")),
-      m_model_ip_150_l_zmin(m_parameters.getParameter<double>("Model_IP_150_L_Zmin")),
-      m_model_ip_150_l_zmax(m_parameters.getParameter<double>("Model_IP_150_L_Zmax")),
-      m_beampipe_aperture_radius(m_parameters.getParameter<double>("BeampipeApertureRadius")) {
-  fBeamEnergy = m_parameters.getParameter<double>("sqrtS");
-  m_sigmaSTX = m_parameters.getParameter<double>("beamDivergenceX");
-  m_sigmaSTY = m_parameters.getParameter<double>("beamDivergenceY");
-  m_sig_E = m_parameters.getParameter<double>("beamEnergyDispersion");
-  fCrossingAngle_45 = m_parameters.getParameter<double>("halfCrossingAngleSector45");
-  fCrossingAngle_56 = m_parameters.getParameter<double>("halfCrossingAngleSector56");
+TotemTransport::TotemTransport(const edm::ParameterSet& iConfig)
+    : BaseProtonTransport(iConfig),
+      m_model_ip_150_r_name(iConfig.getParameter<std::string>("Model_IP_150_R_Name")),
+      m_model_ip_150_l_name(iConfig.getParameter<std::string>("Model_IP_150_L_Name")),
+      m_beampipe_aperture_radius(iConfig.getParameter<double>("BeampipeApertureRadius")) {
+  MODE = TransportMode::TOTEM;
+  beam1Filename_ = iConfig.getParameter<std::string>("Beam1Filename");
+  beam2Filename_ = iConfig.getParameter<std::string>("Beam2Filename");
+  fCrossingAngle_45 = iConfig.getParameter<double>("halfCrossingAngleSector45");
+  fCrossingAngle_56 = iConfig.getParameter<double>("halfCrossingAngleSector56");
+  beamEnergy_ = iConfig.getParameter<double>("BeamEnergy");
   fVtxMeanX = iConfig.getParameter<double>("VtxMeanX");
   fVtxMeanY = iConfig.getParameter<double>("VtxMeanY");
   fVtxMeanZ = iConfig.getParameter<double>("VtxMeanZ");
-  fBeamXatIP = m_parameters.getUntrackedParameter<double>("BeamXatIP", fVtxMeanX);
-  fBeamYatIP = m_parameters.getUntrackedParameter<double>("BeamYatIP", fVtxMeanY);
-  bApplyZShift = m_parameters.getParameter<bool>("ApplyZShift");
+  m_sigmaSTX = iConfig.getParameter<double>("BeamDivergenceX");
+  m_sigmaSTY = iConfig.getParameter<double>("BeamDivergenceY");
+  m_sigmaSX = iConfig.getParameter<double>("BeamSigmaX");
+  m_sigmaSY = iConfig.getParameter<double>("BeamSigmaY");
+  m_sig_E = iConfig.getParameter<double>("BeamEnergyDispersion");
+  fBeamXatIP = iConfig.getUntrackedParameter<double>("BeamXatIP", fVtxMeanX);
+  fBeamYatIP = iConfig.getUntrackedParameter<double>("BeamYatIP", fVtxMeanY);
 
-  MODE = TransportMode::TOTEM;
-  edm::LogInfo("ProtonTransport") << "=============================================================================\n"
-                                  << "             Bulding LHC Proton transporter based on TOTEM model\n"
-                                  << "=============================================================================\n";
+  if (fPPSRegionStart_56 > 0)
+    fPPSRegionStart_56 *= -1;  // make sure sector 56 has negative position, as TOTEM convention
 
-  fBeamMomentum = sqrt(fBeamEnergy * fBeamEnergy - ProtonMassSQ);
+  edm::LogInfo("TotemTransport") << "=============================================================================\n"
+                                 << "             Bulding LHC Proton transporter based on TOTEM model\n"
+                                 << "=============================================================================\n";
 
-  fPPSRegionStart_56 = m_model_ip_150_r_zmax;
-  fPPSRegionStart_45 = m_model_ip_150_l_zmax;
-
-  m_aprox_ip_150_r = ReadParameterization(m_model_ip_150_r_name, m_model_root_file_r);
-  m_aprox_ip_150_l = ReadParameterization(m_model_ip_150_l_name, m_model_root_file_l);
+  m_aprox_ip_150_r = ReadParameterization(m_model_ip_150_r_name, beam1Filename_);
+  m_aprox_ip_150_l = ReadParameterization(m_model_ip_150_l_name, beam2Filename_);
 
   if (m_aprox_ip_150_r == nullptr || m_aprox_ip_150_l == nullptr) {
-    edm::LogError("ProtonTransport") << "Parameterisation " << m_model_ip_150_r_name << " or " << m_model_ip_150_l_name
-                                     << " missing in file. Cannot proceed. ";
+    edm::LogError("TotemTransport") << "Parameterisation " << m_model_ip_150_r_name << " or " << m_model_ip_150_l_name
+                                    << " missing in file. Cannot proceed. ";
     exit(1);
   }
-  edm::LogInfo("TotemRPProtonTransportSetup")
-      << "Parameterizations read from file, pointers:" << m_aprox_ip_150_r << " " << m_aprox_ip_150_l << " ";
+  edm::LogInfo("TotemTransport") << "Parameterizations read from file, pointers:" << m_aprox_ip_150_r << " "
+                                 << m_aprox_ip_150_l << " ";
 }
+//
+// this method is the same for all propagator, but since transportProton is different for each derived class
+// it needes to be overriden
+//
 void TotemTransport::process(const HepMC::GenEvent* evt,
                              const edm::EventSetup& iSetup,
                              CLHEP::HepRandomEngine* _engine) {
-  engine = _engine;
+  this->clear();
+
+  engine_ = _engine;  // the engine needs to be updated for each event
 
   for (HepMC::GenEvent::particle_const_iterator eventParticle = evt->particles_begin();
        eventParticle != evt->particles_end();
        ++eventParticle) {
     if (!((*eventParticle)->status() == 1 && (*eventParticle)->pdg_id() == 2212))
       continue;
+
+    if (!(fabs((*eventParticle)->momentum().eta()) > etaCut_ && fabs((*eventParticle)->momentum().pz()) > momentumCut_))
+      continue;  // discard protons outside kinematic acceptance
+
     unsigned int line = (*eventParticle)->barcode();
     HepMC::GenParticle* gpart = (*eventParticle);
     if (gpart->pdg_id() != 2212)
       continue;  // only transport stable protons
-    if (gpart->status() != 1 /*&& gpart->status()<83 */)
+    if (gpart->status() != 1)
       continue;
-    if (m_beamPart.find(line) != m_beamPart.end())
+    if (m_beamPart.find(line) != m_beamPart.end())  // assures this protons has not been already propagated
       continue;
 
     transportProton(gpart);
   }
-  addPartToHepMC(const_cast<HepMC::GenEvent*>(evt));
 }
+//
+//
+// here comes the real thing
+//
+//
 bool TotemTransport::transportProton(const HepMC::GenParticle* in_trk) {
-  edm::LogInfo("ProtonTransport") << "Starting proton transport using TOTEM method\n";
+  //
+  //
+  edm::LogInfo("TotemTransport") << "Starting proton transport using TOTEM method\n";
+  //
   ApplyBeamCorrection(const_cast<HepMC::GenParticle*>(in_trk));
 
   const HepMC::GenVertex* in_pos = in_trk->production_vertex();
@@ -103,20 +111,21 @@ bool TotemTransport::transportProton(const HepMC::GenParticle* in_trk) {
   double in_momentum[3] = {in_mom.x(), in_mom.y(), in_mom.z()};
   double out_position[3];
   double out_momentum[3];
-  edm::LogInfo("ProtonTransport") << "before transport ->"
-                                  << " position: " << in_position[0] << ", " << in_position[1] << ", " << in_position[2]
-                                  << " momentum: " << in_momentum[0] << ", " << in_momentum[1] << ", "
-                                  << in_momentum[2];
+  edm::LogInfo("TotemTransport") << "before transport ->"
+                                 << " position: " << in_position[0] << ", " << in_position[1] << ", " << in_position[2]
+                                 << " momentum: " << in_momentum[0] << ", " << in_momentum[1] << ", " << in_momentum[2];
 
   LHCOpticsApproximator* approximator_ = nullptr;
+  double m_Zin_;
+  double m_Zout_;
   if (in_mom.z() > 0) {
     approximator_ = m_aprox_ip_150_l;
-    m_Zin_ = m_model_ip_150_l_zmin;
-    m_Zout_ = m_model_ip_150_l_zmax;
+    m_Zin_ = 0.0;  // Totem propagations assumes the starting point at 0 (zero)
+    m_Zout_ = fPPSRegionStart_45;
   } else {
     approximator_ = m_aprox_ip_150_r;
-    m_Zin_ = m_model_ip_150_r_zmin;
-    m_Zout_ = m_model_ip_150_r_zmax;
+    m_Zin_ = 0.0;  // Totem propagations assumes the starting point at 0 (zero)
+    m_Zout_ = fPPSRegionStart_56;
   }
 
   bool invert_beam_coord_system =
@@ -128,54 +137,59 @@ bool TotemTransport::transportProton(const HepMC::GenParticle* in_trk) {
   if (!tracked)
     return false;
 
-  edm::LogInfo("ProtonTransport") << "after transport -> "
-                                  << "position: " << out_position[0] << ", " << out_position[1] << ", "
-                                  << out_position[2] << "momentum: " << out_momentum[0] << ", " << out_momentum[1]
-                                  << ", " << out_momentum[2];
+  edm::LogInfo("TotemTransport") << "after transport -> "
+                                 << "position: " << out_position[0] << ", " << out_position[1] << ", "
+                                 << out_position[2] << "momentum: " << out_momentum[0] << ", " << out_momentum[1]
+                                 << ", " << out_momentum[2];
 
   if (out_position[0] * out_position[0] + out_position[1] * out_position[1] >
       m_beampipe_aperture_radius * m_beampipe_aperture_radius) {
-    edm::LogInfo("ProtonTransport") << "Proton ouside beampipe";
-    edm::LogInfo("ProtonTransport") << "===== END Transport "
-                                    << "====================";
+    edm::LogInfo("TotemTransport") << "Proton ouside beampipe";
+    edm::LogInfo("TotemTransport") << "===== END Transport "
+                                   << "====================";
     return false;
   }
 
   TVector3 out_pos(out_position[0] * meter, out_position[1] * meter, out_position[2] * meter);
   TVector3 out_mom(out_momentum[0], out_momentum[1], out_momentum[2]);
-  edm::LogInfo("TotemRPProtonTransportModel") << "output -> "
-                                              << "position: ";
-  out_pos.Print();
-  edm::LogInfo("TotemRPProtonTransportModel") << " momentum: ";
-  out_mom.Print();
-  double px = -out_momentum[0];
-  double py = out_momentum[1];  // this need to be checked again, since it seems an invertion is occuring in  the prop.
-  double pz = out_momentum[2];
+
+  if (verbosity_) {
+    LogDebug("TotemTransport") << "output -> position: ";
+    out_pos.Print();
+    LogDebug("TotemTransport") << " momentum: ";
+    out_mom.Print();
+  }
+
+  double px = -out_momentum[0];  // tote calculates px by means of TH_X, which is in the LHC ref. frame.
+  double py = out_momentum[1];   // this need to be checked again, since it seems an invertion is occuring in  the prop.
+  double pz =
+      out_momentum[2];  // totem calculates output pz already in the CMS ref. frame, it doesn't need to be converted
   double e = sqrt(px * px + py * py + pz * pz + ProtonMassSQ);
   TLorentzVector p_out(px, py, pz, e);
   double x1_ctpps = -out_position[0] * meter;  // Totem parameterization uses meter, one need it in millimeter
-  double y1_ctpps = -out_position[1] * meter;
+  double y1_ctpps = out_position[1] * meter;
 
   unsigned int line = in_trk->barcode();
 
-  if (m_verbosity)
-    LogDebug("ProtonTransportEventProcessing")
-        << "ProtonTransport:filterPPS: barcode = " << line << " x=  " << x1_ctpps << " y= " << y1_ctpps;
+  if (verbosity_)
+    LogDebug("TotemTransport") << "TotemTransport:filterPPS: barcode = " << line << " x=  " << x1_ctpps
+                               << " y= " << y1_ctpps;
 
   m_beamPart[line] = p_out;
   m_xAtTrPoint[line] = x1_ctpps;
   m_yAtTrPoint[line] = y1_ctpps;
   return true;
 }
+//
 LHCOpticsApproximator* TotemTransport::ReadParameterization(const std::string& m_model_name,
                                                             const std::string& rootfile) {
   edm::FileInPath fileName(rootfile.c_str());
   TFile* f = TFile::Open(fileName.fullPath().c_str(), "read");
   if (!f) {
-    edm::LogError("TotemRPProtonTransportSetup") << "File " << fileName << " not found. Exiting.";
+    edm::LogError("TotemTransport") << "File " << fileName << " not found. Exiting.";
     return nullptr;
   }
-  edm::LogInfo("TotemRPProtonTransportSetup") << "Root file opened, pointer:" << f;
+  edm::LogInfo("TotemTransport") << "Root file opened, pointer:" << f;
 
   // read parametrization
   LHCOpticsApproximator* aprox = (LHCOpticsApproximator*)f->Get(m_model_name.c_str());
