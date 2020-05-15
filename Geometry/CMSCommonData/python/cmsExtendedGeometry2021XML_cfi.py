8d897e8b69d5ec1f78e8845ea272d0eac39e3985
Carl Vuosalo <cvuosalo@cern.ch>
2020-05-15 22:39:49 +0200

    Revise vales for chemical elements to match Geant4

--- Geometry/CMSCommonData/python/cmsExtendedGeometry2021XML_cfi.py
+++ Geometry/CMSCommonData/python/cmsExtendedGeometry2021XML_cfi.py
@@ -4,7 +4,7 @@ import FWCore.ParameterSet.Config as cms
 
 XMLIdealGeometryESSource = cms.ESSource("XMLIdealGeometryESSource",
     geomXMLFiles = cms.vstring(
-        'Geometry/CMSCommonData/data/materials.xml',
+        'Geometry/CMSCommonData/data/materials/2021/v1/materials.xml',
         'Geometry/CMSCommonData/data/rotations.xml',
         'Geometry/CMSCommonData/data/extend/v2/cmsextent.xml',
         'Geometry/CMSCommonData/data/cms/2021/v2/cms.xml',
