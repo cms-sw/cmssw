fe1c773f862d7e810f927951b50db49f7cf054e1
Dinyar Rabady <dinyar@cern.ch>
2020-05-18 12:16:57 +0200

    Regional muon packer adapted for Kalman muons from BMTF Includes configuration of packer by Era. Modifications still needed for the BMTF setup to make use of this.

--- EventFilter/L1TRawToDigi/plugins/implementations_stage2/BMTFPackerOutput.cc
+++ EventFilter/L1TRawToDigi/plugins/implementations_stage2/BMTFPackerOutput.cc
@@ -21,7 +21,7 @@ namespace l1t {
       for (auto imu = muons->begin(); imu != muons->end(); imu++) {
         if (imu->processor() + 1 == board_id) {
           uint32_t firstWord(0), lastWord(0);
-          RegionalMuonRawDigiTranslator::generatePackedDataWords(*imu, firstWord, lastWord);
+          RegionalMuonRawDigiTranslator::generatePackedDataWords(*imu, firstWord, lastWord, isKalman_);
           payloadMap_[bmtfBlockID].push_back(firstWord);  //imu->link()*2+1
           payloadMap_[bmtfBlockID].push_back(lastWord);   //imu->link()*2+1
         }
@@ -51,14 +51,14 @@ namespace l1t {
       blocks.push_back(block);
 
       /*
-	  //debug from here
-	  std::cout << "block id : " << block.header().getID() << std::endl;
+      //debug from here
+      std::cout << "block id : " << block.header().getID() << std::endl;
 
-	  std::cout << "payload created : " << std::endl;
-	  for (auto &word : block.payload())  
-	    std::cout << std::bitset<32>(word).to_string() << std::endl;
-	  //debug up to here
-	*/
+      std::cout << "payload created : " << std::endl;
+      for (auto &word : block.payload())
+        std::cout << std::bitset<32>(word).to_string() << std::endl;
+      //debug up to here
+    */
 
       return blocks;
     }
