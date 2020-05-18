25e1def3a4db3019bf5174bb10c69901068cb09b
Chris Jones <chrisdjones15@gmail.com>
2020-05-18 15:50:48 -0500

    Use different task_arenas only in multi-thread running
    We need to use the same task_arena for single thread running as the tbb::task::wait_for_all that happens in the main thread will not run any tasks placed in another task_arena. That behavior would keep ES data products from being prefetched.

--- FWCore/Framework/src/esTaskArenas.cc
+++ FWCore/Framework/src/esTaskArenas.cc
@@ -1,11 +1,27 @@
 #include "esTaskArenas.h"
 
+namespace {
+  bool& switchForDifferentArenas() {
+    static bool s = false;
+    return s;
+  }
+
+  tbb::task_arena makeESTaskArena() {
+    if (switchForDifferentArenas()) {
+      return tbb::task_arena{tbb::this_task_arena::max_concurrency()};
+    }
+    return tbb::task_arena{tbb::task_arena::attach()};
+  }
+}  // namespace
+
+void edm::implementation_detail::useDifferentArenas() { switchForDifferentArenas() = true; }
+
 tbb::task_arena& edm::mainTaskArena() {
   static tbb::task_arena s_arena{tbb::task_arena::attach()};
   return s_arena;
 }
 
 tbb::task_arena& edm::esTaskArena() {
-  static tbb::task_arena s_arena{tbb::this_task_arena::max_concurrency()};
+  static tbb::task_arena s_arena{makeESTaskArena()};
   return s_arena;
 }
