28e696b7c89682a7c1d19f38144a08e9cf9bfb87
Leonardo Cristella <leonardo.cristella@cern.ch>
2020-05-19 14:06:38 +0200

    Polish code

--- RecoLocalCalo/HGCalRecProducers/plugins/HGCalRecHitMapProducer.cc
+++ RecoLocalCalo/HGCalRecProducers/plugins/HGCalRecHitMapProducer.cc
@@ -1,5 +1,3 @@
-#define __RecoLocalCalo_HGCRecProducers_HGCalRecHitMapProducer_H__
-
 // user include files
 #include <map>
 
@@ -18,53 +16,48 @@
 class HGCalRecHitMapProducer : public edm::global::EDProducer<> {
 public:
   HGCalRecHitMapProducer(const edm::ParameterSet&);
-  ~HGCalRecHitMapProducer() override {}
   static void fillDescriptions(edm::ConfigurationDescriptions& descriptions);
 
   void produce(edm::StreamID, edm::Event&, const edm::EventSetup&) const override;
 
 private:
-  edm::EDGetTokenT<HGCRecHitCollection> hits_ee_token;
-  edm::EDGetTokenT<HGCRecHitCollection> hits_fh_token;
-  edm::EDGetTokenT<HGCRecHitCollection> hits_bh_token;
+  edm::EDGetTokenT<HGCRecHitCollection> hits_ee_token_;
+  edm::EDGetTokenT<HGCRecHitCollection> hits_fh_token_;
+  edm::EDGetTokenT<HGCRecHitCollection> hits_bh_token_;
 };
 
 DEFINE_FWK_MODULE(HGCalRecHitMapProducer);
 
 HGCalRecHitMapProducer::HGCalRecHitMapProducer(const edm::ParameterSet& ps) {
-  hits_ee_token = consumes<HGCRecHitCollection>(ps.getParameter<edm::InputTag>("HGCEEInput"));
-  hits_fh_token = consumes<HGCRecHitCollection>(ps.getParameter<edm::InputTag>("HGCFHInput"));
-  hits_bh_token = consumes<HGCRecHitCollection>(ps.getParameter<edm::InputTag>("HGCBHInput"));
+  hits_ee_token_ = consumes<HGCRecHitCollection>(ps.getParameter<edm::InputTag>("EEInput"));
+  hits_fh_token_ = consumes<HGCRecHitCollection>(ps.getParameter<edm::InputTag>("FHInput"));
+  hits_bh_token_ = consumes<HGCRecHitCollection>(ps.getParameter<edm::InputTag>("BHInput"));
   produces<std::map<DetId, const HGCRecHit*>>();
 }
 
 void HGCalRecHitMapProducer::fillDescriptions(edm::ConfigurationDescriptions& descriptions) {
   edm::ParameterSetDescription desc;
-
-  desc.add<edm::InputTag>("HGCEEInput", edm::InputTag("HGCalRecHit", "HGCEERecHits"));
-  desc.add<edm::InputTag>("HGCFHInput", edm::InputTag("HGCalRecHit", "HGCHEFRecHits"));
-  desc.add<edm::InputTag>("HGCBHInput", edm::InputTag("HGCalRecHit", "HGCHEBRecHits"));
+  desc.add<edm::InputTag>("EEInput", {"HGCalRecHit", "HGCEERecHits"});
+  desc.add<edm::InputTag>("FHInput", {"HGCalRecHit", "HGCHEFRecHits"});
+  desc.add<edm::InputTag>("BHInput", {"HGCalRecHit", "HGCHEBRecHits"});
   descriptions.add("hgcalRecHitMapProducer", desc);
 }
 
 void HGCalRecHitMapProducer::produce(edm::StreamID, edm::Event& evt, const edm::EventSetup& es) const {
   std::unique_ptr<std::map<DetId, const HGCRecHit*>> hitMap(new std::map<DetId, const HGCRecHit*>);
-  edm::Handle<HGCRecHitCollection> ee_hits;
-  edm::Handle<HGCRecHitCollection> fh_hits;
-  edm::Handle<HGCRecHitCollection> bh_hits;
+  const auto& ee_hits = evt.get(hits_ee_token_);
+  const auto& fh_hits = evt.get(hits_fh_token_);
+  const auto& bh_hits = evt.get(hits_bh_token_);
 
-  evt.getByToken(hits_ee_token, ee_hits);
-  evt.getByToken(hits_fh_token, fh_hits);
-  evt.getByToken(hits_bh_token, bh_hits);
-  for (const auto& hit : *ee_hits.product()) {
+  for (const auto& hit : ee_hits) {
     hitMap->emplace(hit.detid(), &hit);
   }
 
-  for (const auto& hit : *fh_hits.product()) {
+  for (const auto& hit : fh_hits) {
     hitMap->emplace(hit.detid(), &hit);
   }
 
-  for (const auto& hit : *bh_hits.product()) {
+  for (const auto& hit : bh_hits) {
     hitMap->emplace(hit.detid(), &hit);
   }
   evt.put(std::move(hitMap));
--- RecoLocalCalo/HGCalRecProducers/src/classes.h
+++
@@ -1,4 +0,0 @@
-#include "DataFormats/Common/interface/Wrapper.h"
-
-// Add includes for your classes here
-#include "DataFormats/HGCRecHit/interface/HGCRecHitCollections.h"
