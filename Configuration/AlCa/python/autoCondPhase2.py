70a620a5ec7f742e0c0fbf71fe88579ec663f82e
mmusich <marco.musich@cern.ch>
2020-05-19 17:14:24 +0200

    update autoCondPhase2 with LA for Outer Tracker

--- Configuration/AlCa/python/autoCondPhase2.py
+++ Configuration/AlCa/python/autoCondPhase2.py
@@ -7,6 +7,8 @@ SiPixelSimLARecord        =   "SiPixelLorentzAngleSimRcd"
 SiPixelGenErrorRecord     =   "SiPixelGenErrorDBObjectRcd"       
 SiPixelTemplatesRecord    =   "SiPixelTemplateDBObjectRcd"       
 SiPixel2DTemplatesRecord  =   "SiPixel2DTemplateDBObjectRcd"     
+TrackerLARecord           =   "SiPhase2OuterTrackerLorentzAngleRcd"
+TrackerSimLARecord        =   "SiPhase2OuterTrackerLorentzAngleSimRcd"
 
 ##
 ## Active geometries: https://github.com/cms-sw/cmssw/blob/master/Configuration/Geometry/README.md
@@ -78,6 +80,26 @@ allTags["Template"] = {
 }
 
 ##
+## Outer Tracker records
+##
+
+tempConnectionString="sqlite_file:/tmp/musich/CMSSW_11_1_X_2020-05-17-2300/src/CondTools/SiPhase2Tracker/test/OTLorentzAngles.db"
+
+allTags["OTLA"] = {
+    'T15' : ( ','.join( [ 'SiPhase2OuterTrackerLorentzAngle_T15_v0' ,TrackerLARecord,tempConnectionString, "", "2020-05-19 14:00:00.000"] ), ),  #uH = 0.07/T
+    'T17' : ( ','.join( [ 'SiPhase2OuterTrackerLorentzAngle_T15_v0' ,TrackerLARecord,tempConnectionString, "", "2020-05-19 14:00:00.000"] ), ),  #uH = 0.07/T
+    'T19' : ( ','.join( [ 'SiPhase2OuterTrackerLorentzAngle_T15_v0' ,TrackerLARecord,tempConnectionString, "", "2020-05-19 14:00:00.000"] ), ),  #uH = 0.07/T
+    'T20' : ( ','.join( [ 'SiPhase2OuterTrackerLorentzAngle_T15_v0' ,TrackerLARecord,tempConnectionString, "", "2020-05-19 14:00:00.000"] ), ),  #uH = 0.07/T
+}
+
+allTags["SimOTLA"] = {
+    'T15' : ( ','.join( [ 'SiPhase2OuterTrackerLorentzAngle_T15_v0' ,TrackerSimLARecord,tempConnectionString, "", "2020-05-19 14:00:00.000"] ), ),  #uH = 0.07/T
+    'T17' : ( ','.join( [ 'SiPhase2OuterTrackerLorentzAngle_T15_v0' ,TrackerSimLARecord,tempConnectionString, "", "2020-05-19 14:00:00.000"] ), ),  #uH = 0.07/T
+    'T19' : ( ','.join( [ 'SiPhase2OuterTrackerLorentzAngle_T15_v0' ,TrackerSimLARecord,tempConnectionString, "", "2020-05-19 14:00:00.000"] ), ),  #uH = 0.07/T
+    'T20' : ( ','.join( [ 'SiPhase2OuterTrackerLorentzAngle_T15_v0' ,TrackerSimLARecord,tempConnectionString, "", "2020-05-19 14:00:00.000"] ), ),  #uH = 0.07/T
+}
+
+##
 ## All of the following conditions are not yet in active use, but will be activated in GT along the way
 ##
 
@@ -90,7 +112,7 @@ allTags["Template2Dden"] = {
 }
 
 # list of active tags to be replaced
-activeKeys = ["LA","LAWidth","SimLA","LAfromAlignment","GenError","Template"]
+activeKeys = ["LA","LAWidth","SimLA","LAfromAlignment","GenError","Template","SimOTLA","OTLA"]
 
 # list of geometries supported
 activeDets = ["T15","T17","T19","T20","T21","T22","T23"]
