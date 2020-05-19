b38549c27668c0b05b6d72a55364980fd4c19693
Mia <mia.tosi@cern.ch>
2020-05-19 18:27:07 +0200

    add xml file and python customizer for D49 reco tracker material

---
+++ SLHCUpgradeSimulations/Configuration/python/customise_TkRecoMaterial.py
@@ -0,0 +1,20 @@
+'''Customization functions for cmsDriver to change the phase-2 tracker reco material for geometry D49'''
+import FWCore.ParameterSet.Config as cms
+def customizeRecoMaterial(process):
+    
+    ''' will replace one tracker reco material file with another one for geometry D49
+    syntax: --customise SLHCUpgradeSimulations/Configuration/customise_TkRecoMaterial.customizeRecoMaterial
+    '''
+    
+    if hasattr(process,'XMLIdealGeometryESSource') and hasattr(process.XMLIdealGeometryESSource,'geomXMLFiles'):
+        
+        print("customizing Tracker Reco Material")
+        process.XMLIdealGeometryESSource.geomXMLFiles.remove(
+            'Geometry/TrackerRecoData/data/PhaseII/TiltedTracker613_MB_2019_04/trackerRecoMaterial.xml'
+        )
+        
+        process.XMLIdealGeometryESSource.geomXMLFiles.append(
+            'Geometry/TrackerRecoData/data/PhaseII/TiltedTracker613_MB_2019_04/v2_ITonly/trackerRecoMaterial.xml'
+        )
+        
+    return process
