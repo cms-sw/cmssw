0c7abec7e0b4a4e4727830c8ade16862842522c2
mmusich <marco.musich@cern.ch>
2020-05-19 17:13:54 +0200

    improve SiPhase2OuterTrackerLorentzAngleWriter

--- CondTools/SiPhase2Tracker/test/SiPhase2OuterTrackerLorentzAngleWriter_cfg.py
+++ CondTools/SiPhase2Tracker/test/SiPhase2OuterTrackerLorentzAngleWriter_cfg.py
@@ -1,21 +1,33 @@
+#! /usr/bin/env cmsRun
+# Author: Marco Musich (May 2020)
+from __future__ import print_function
+#import os
+import os, shlex, shutil, getpass
+#import subprocess
+
 import FWCore.ParameterSet.Config as cms
 
 process = cms.Process("TEST")
 
-tag = 'SiPhase2OuterTrackerLorentzAngle'
+tag = 'SiPhase2OuterTrackerLorentzAngle_T6'
 suffix = 'v0'
+outfile = tag+'_'+suffix+'.db'
+outdb = 'sqlite_file:'+outfile
 
-outdb = 'sqlite_file:'+tag+'_'+suffix+'.db'
+if os.path.exists(outfile):
+  oldfile = outfile.replace(".db","_old.db")
+  print(">>> Backing up locally existing '%s' -> '%s'"%(outfile,oldfile))
+  shutil.move(outfile,oldfile)
 
 process.load("CondCore.CondDB.CondDB_cfi")
 process.CondDB.connect = cms.string(outdb)
 
-process.load('Configuration.Geometry.GeometryExtended2026D49Reco_cff')
-process.load('Configuration.Geometry.GeometryExtended2026D49_cff')
+process.load('Configuration.Geometry.GeometryExtended2026D35Reco_cff')
+process.load('Configuration.Geometry.GeometryExtended2026D35_cff')
 
 process.load('Configuration.StandardSequences.FrontierConditions_GlobalTag_cff')
 from Configuration.AlCa.GlobalTag import GlobalTag
-process.GlobalTag = GlobalTag(process.GlobalTag, 'auto:phase2_realistic_T15')
+process.GlobalTag = GlobalTag(process.GlobalTag, 'auto:phase2_realistic_T6')
 process.maxEvents = cms.untracked.PSet( input = cms.untracked.int32(1) )
 
 process.source = cms.Source("EmptyIOVSource",
