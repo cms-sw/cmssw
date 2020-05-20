f026ac0426d2ab04a6edf4aadce7b694d53df1cd
Mia <mia.tosi@cern.ch>
2020-05-20 09:51:59 +0200

    make more pythonic

--- SLHCUpgradeSimulations/Configuration/python/customise_TkRecoMaterial.py
+++ SLHCUpgradeSimulations/Configuration/python/customise_TkRecoMaterial.py
@@ -1,21 +1,21 @@
 '''Customization functions for cmsDriver to change the phase-2 tracker reco material for geometry D49'''
 import FWCore.ParameterSet.Config as cms
-def customizeRecoMaterial(process):
+def customizeRecoMaterialD49(process):
     
     ''' will replace one tracker reco material file with another one for geometry D49
-    syntax: --customise SLHCUpgradeSimulations/Configuration/customise_TkRecoMaterial.customizeRecoMaterial
+    syntax: --customise SLHCUpgradeSimulations/Configuration/customise_TkRecoMaterial.customizeRecoMaterialD49
     '''
     
     if hasattr(process,'XMLIdealGeometryESSource') and hasattr(process.XMLIdealGeometryESSource,'geomXMLFiles'):
         
-
-        if 'Geometry/TrackerRecoData/data/PhaseII/TiltedTracker613_MB_2019_04/trackerRecoMaterial.xml' in process.XMLIdealGeometryESSource.geomXMLFiles :
+        try:
             process.XMLIdealGeometryESSource.geomXMLFiles.remove(
                 'Geometry/TrackerRecoData/data/PhaseII/TiltedTracker613_MB_2019_04/trackerRecoMaterial.xml'
             )
-        
             process.XMLIdealGeometryESSource.geomXMLFiles.append(
                 'Geometry/TrackerRecoData/data/PhaseII/TiltedTracker613_MB_2019_04/v2_ITonly/trackerRecoMaterial.xml'
             )
-        
+        except ValueError:
+            raise SystemExit("\n\n ERROR! Could not replace trackerRecoMaterial.xml file, please check if D49 geometry is being used \n\n")
+            
     return process
