2151b7d16d265451ac11e7c2ac6ce538503a410e
Sunanda <sunanda.banerjee@cern.ch>
2020-05-21 20:21:07 +0200

    Correct ME0GeometryBuilder to take care of DDD and DD4HEP

--- Geometry/GEMGeometryBuilder/plugins/ME0GeometryESModule.cc
+++ Geometry/GEMGeometryBuilder/plugins/ME0GeometryESModule.cc
@@ -14,9 +14,10 @@
 #include "Geometry/Records/interface/IdealGeometryRecord.h"
 #include "Geometry/MuonNumbering/interface/MuonGeometryConstants.h"
 #include "DetectorDescription/Core/interface/DDCompactView.h"
+#include "DetectorDescription/DDCMS/interface/DDCompactView.h"
 
-#include "Geometry/MuonNumbering/interface/DD4hep_MuonNumbering.h"
-#include <DetectorDescription/DDCMS/interface/DDCompactView.h>
+#include "Geometry/MuonNumbering/interface/MuonDDDNumbering.h"
+#include "Geometry/MuonNumbering/interface/MuonBaseNumber.h"
 
 #include "Geometry/Records/interface/ME0RecoGeometryRcd.h"
 #include "Geometry/Records/interface/MuonGeometryRecord.h"
@@ -32,8 +33,6 @@
 
 #include <memory>
 
-using namespace edm;
-
 class ME0GeometryESModule : public edm::ESProducer {
 public:
   ME0GeometryESModule(const edm::ParameterSet& p);
@@ -45,7 +44,6 @@ private:
   edm::ESGetToken<DDCompactView, IdealGeometryRecord> cpvToken_;
   edm::ESGetToken<MuonGeometryConstants, IdealGeometryRecord> mdcToken_;
   edm::ESGetToken<cms::DDCompactView, IdealGeometryRecord> dd4hepcpvToken_;
-  edm::ESGetToken<cms::MuonNumbering, MuonNumberingRecord> dd4hepmdcToken_;
   edm::ESGetToken<RecoIdealGeometry, ME0RecoGeometryRcd> rigme0Token_;
   // use the DDD or DD4hep as Geometry source
   bool useDDD_;
@@ -56,10 +54,8 @@ ME0GeometryESModule::ME0GeometryESModule(const edm::ParameterSet& p) {
   useDDD_ = p.getParameter<bool>("useDDD");
   useDD4hep_ = p.getUntrackedParameter<bool>("useDD4hep", false);
   auto cc = setWhatProduced(this);
-  if (useDDD_) {
+  if (useDDD_ || useDD4hep_) {
     cc.setConsumes(cpvToken_).setConsumes(mdcToken_);
-  } else if (useDD4hep_) {
-    cc.setConsumes(dd4hepcpvToken_).setConsumes(dd4hepmdcToken_);
   } else {
     cc.setConsumes(rigme0Token_);
   }
@@ -68,21 +64,21 @@ ME0GeometryESModule::ME0GeometryESModule(const edm::ParameterSet& p) {
 ME0GeometryESModule::~ME0GeometryESModule() {}
 
 std::unique_ptr<ME0Geometry> ME0GeometryESModule::produce(const MuonGeometryRecord& record) {
-  LogTrace("ME0GeometryESModule") << "ME0GeometryESModule::produce with useDDD = " << useDDD_;
+  edm::LogVerbatim("ME0GeometryESModule") << "ME0GeometryESModule::produce with useDDD = " << useDDD_;
   if (useDDD_) {
-    LogTrace("ME0GeometryESModule") << "ME0GeometryESModule::produce :: ME0GeometryBuilder builder";
+    edm::LogVerbatim("ME0GeometryESModule") << "ME0GeometryESModule::produce :: ME0GeometryBuilder builder";
     auto cpv = record.getTransientHandle(cpvToken_);
     const auto& mdc = record.get(mdcToken_);
     ME0GeometryBuilder builder;
     return std::unique_ptr<ME0Geometry>(builder.build(cpv.product(), mdc));
   } else if (useDD4hep_) {
-    LogTrace("ME0GeometryESModule") << "ME0GeometryESModule::produce :: ME0GeometryBuilder builder DD4hep";
+    edm::LogVerbatim("ME0GeometryESModule") << "ME0GeometryESModule::produce :: ME0GeometryBuilder builder DD4hep";
     auto cpv = record.getTransientHandle(dd4hepcpvToken_);
-    const auto& mdc = record.get(dd4hepmdcToken_);
+    const auto& mdc = record.get(mdcToken_);
     ME0GeometryBuilder builder;
     return std::unique_ptr<ME0Geometry>(builder.build(cpv.product(), mdc));
   } else {
-    LogTrace("ME0GeometryESModule") << "ME0GeometryESModule::produce :: ME0GeometryBuilderFromCondDB builder";
+    edm::LogVerbatim("ME0GeometryESModule") << "ME0GeometryESModule::produce :: ME0GeometryBuilderFromCondDB builder";
     const auto& rigme0 = record.get(rigme0Token_);
     ME0GeometryBuilderFromCondDB builder;
     return std::unique_ptr<ME0Geometry>(builder.build(rigme0));
