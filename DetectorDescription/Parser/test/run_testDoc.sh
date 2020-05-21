3985c5049c84d32436e2d8cbc1ccf277446f4842
Carl Vuosalo <cvuosalo@cern.ch>
2020-05-22 00:55:14 +0200

    Try another environment variable to set PATH

--- DetectorDescription/Parser/test/run_testDoc.sh
+++ DetectorDescription/Parser/test/run_testDoc.sh
@@ -3,7 +3,7 @@ test=testtestDoc
 function die { echo Failure $1: status $2 ; exit $2 ; }
 pushd ${LOCAL_TMP_DIR}
   export mecpath=${PATH}
-  export PATH=${CMSSW_RELEASE_BASE}/test/${SCRAM_ARCH}/:${LOCAL_TOP_DIR}/test/${SCRAM_ARCH}/:${PATH}
+  export PATH=${RELEASETOP}/test/${SCRAM_ARCH}/:${LOCAL_TOP_DIR}/test/${SCRAM_ARCH}/:${PATH}
   cd ${LOCAL_TEST_DIR}
 #  cp ${LOCAL_TOP_DIR}/test/${SCRAM_ARCH}/testDoc .
   echo ${test}testDoc ------------------------------------------------------------
