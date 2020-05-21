60430451f72cff02d45b63c14ea60597ca049c4b
Carl Vuosalo <cvuosalo@cern.ch>
2020-05-21 22:29:39 +0200

    Fix DetectorDescription Parser TE unit test so it runs

--- DetectorDescription/Parser/test/run_testDoc.sh
+++ DetectorDescription/Parser/test/run_testDoc.sh
@@ -3,11 +3,11 @@ test=testtestDoc
 function die { echo Failure $1: status $2 ; exit $2 ; }
 pushd ${LOCAL_TMP_DIR}
   export mecpath=${PATH}
-  export PATH=${LOCAL_TOP_DIR}/test/${SCRAM_ARCH}/:${PATH}
+  export PATH=${CMSSW_RELEASE_BASE}/test/${SCRAM_ARCH}/:${LOCAL_TOP_DIR}/test/${SCRAM_ARCH}/:${PATH}
   cd ${LOCAL_TEST_DIR}
 #  cp ${LOCAL_TOP_DIR}/test/${SCRAM_ARCH}/testDoc .
   echo ${test}testDoc ------------------------------------------------------------
-  testDoc testConfiguration.xml|| die "testDoc" $?
+  testDoc testConfiguration.xml || die "testDoc" $?
   export PATH=${mecpath}
   popd
 exit 0
