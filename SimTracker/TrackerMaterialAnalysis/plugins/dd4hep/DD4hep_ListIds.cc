1a7bee90351e3b50e92c7350fe283ecb894c4ddb
Andres Vargas <vargasa@cua.edu>
2020-05-22 11:50:32 -0500

    scram build code-format

--- SimTracker/TrackerMaterialAnalysis/plugins/dd4hep/DD4hep_ListIds.cc
+++ SimTracker/TrackerMaterialAnalysis/plugins/dd4hep/DD4hep_ListIds.cc
@@ -28,7 +28,7 @@ class DD4hep_ListIds : public edm::one::EDAnalyzer<> {
 public:
   DD4hep_ListIds(const edm::ParameterSet &);
   ~DD4hep_ListIds() override;
-  
+
 private:
   void analyze(const edm::Event &, const edm::EventSetup &) override;
   void beginJob() override {}
@@ -49,9 +49,8 @@ DD4hep_ListIds::DD4hep_ListIds(const edm::ParameterSet &pset)
 DD4hep_ListIds::~DD4hep_ListIds() {}
 
 void DD4hep_ListIds::analyze(const edm::Event &evt, const edm::EventSetup &setup) {
-
   edm::ESTransientHandle<cms::DDCompactView> cpv;
-  setup.get<IdealGeometryRecord>().get("CMS",cpv);
+  setup.get<IdealGeometryRecord>().get("CMS", cpv);
 
   std::string attribute = "TkDDDStructure";
   cms::DDFilter filter(attribute, "");
@@ -64,20 +63,22 @@ void DD4hep_ListIds::analyze(const edm::Event &evt, const edm::EventSetup &setup
   }
 
   for (const auto &i : tkdss) {
-
     edm::LogVerbatim("ListIds") << "\nFiltering " << i;
     cms::DDFilter filter1(attribute, {i.data(), i.size()});
     cms::DDFilteredView fv1(*cpv, filter1);
     fv1.firstChild();
 
-    std::vector<const cms::Node*> nodes ;
-    while(fv1.firstChild()){
+    std::vector<const cms::Node *> nodes;
+    while (fv1.firstChild()) {
       nodes = fv1.geoHistory();
-      if(std::find(materials_.begin(),materials_.end(),nodes[nodes.size()-1]->GetVolume()->GetMaterial()->GetName())!=materials_.end()){
-	for(const auto& n : nodes) {
-	  edm::LogVerbatim("ListIds") << n->GetVolume()->GetName() << "[" << n->GetNumber() << "]/";
-	}
-	edm::LogVerbatim("ListIds") << "Material:|" << nodes[nodes.size()-1]->GetVolume()->GetMaterial()->GetName() << "|\n";
+      if (std::find(materials_.begin(),
+                    materials_.end(),
+                    nodes[nodes.size() - 1]->GetVolume()->GetMaterial()->GetName()) != materials_.end()) {
+        for (const auto &n : nodes) {
+          edm::LogVerbatim("ListIds") << n->GetVolume()->GetName() << "[" << n->GetNumber() << "]/";
+        }
+        edm::LogVerbatim("ListIds") << "Material:|" << nodes[nodes.size() - 1]->GetVolume()->GetMaterial()->GetName()
+                                    << "|\n";
       }
     }
   }
