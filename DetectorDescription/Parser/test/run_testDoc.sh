e54fa8c72cc027222ec53965720f5407da5d19de
Carl Vuosalo <cvuosalo@cern.ch>
2020-05-22 21:12:29 +0200

    Try again in case something changed with Jenkins

--- DetectorDescription/Parser/test/run_testDoc.sh
+++ DetectorDescription/Parser/test/run_testDoc.sh
@@ -3,11 +3,10 @@ test=testtestDoc
 function die { echo Failure $1: status $2 ; exit $2 ; }
 pushd ${LOCAL_TMP_DIR}
   export mecpath=${PATH}
-  export PATH=${RELEASETOP}/test/${SCRAM_ARCH}/:${LOCAL_TOP_DIR}/test/${SCRAM_ARCH}/:${PATH}
+  test -z "${CMSSW_RELEASE_BASE}" && echo CMSSW_RELEASE_BASE is not set.
+  export PATH=${CMSSW_RELEASE_BASE}/test/${SCRAM_ARCH}/:${LOCAL_TOP_DIR}/test/${SCRAM_ARCH}/:${PATH}
   cd ${LOCAL_TEST_DIR}
-#  cp ${LOCAL_TOP_DIR}/test/${SCRAM_ARCH}/testDoc .
-  echo ${test}testDoc ------------------------------------------------------------
-  env
+  echo ${test} testDoc ------------------------------------------------------------
   testDoc testConfiguration.xml || die "testDoc" $?
   export PATH=${mecpath}
   popd
