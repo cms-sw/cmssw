7f3ef461fe03ee4517c63fed8dd846b1099f67ae
Pierre Depasse <pierre.depasse@cern.ch>
2020-05-23 19:25:08 +0200

    Tools to include EcalPFRecHitThresholds in Cond DB, bug correction

--- CondTools/Ecal/interface/EcalPFRecHitThresholdsHandler.h
+++ CondTools/Ecal/interface/EcalPFRecHitThresholdsHandler.h
@@ -11,7 +11,6 @@
 #include "CondCore/PopCon/interface/PopConSourceHandler.h"
 #include "FWCore/ParameterSet/interface/ParameterSetfwd.h"
 
-
 #include "FWCore/ServiceRegistry/interface/Service.h"
 #include "CondCore/DBOutputService/interface/PoolDBOutputService.h"
 #include "FWCore/Framework/interface/ESHandle.h"
@@ -22,8 +21,6 @@
 #include "DataFormats/Common/interface/Handle.h"
 #include "FWCore/Framework/interface/EventSetup.h"
 #include "FWCore/Framework/interface/EventSetupRecordKey.h"
-
-
 
 #include "CondFormats/EcalObjects/interface/EcalPFRecHitThresholds.h"
 #include "CondFormats/DataRecord/interface/EcalPFRecHitThresholdsRcd.h"
@@ -36,38 +33,34 @@
 #include "DataFormats/EcalDetId/interface/EBDetId.h"
 #include "DataFormats/Provenance/interface/Timestamp.h"
 
-
-
 #include "CondTools/Ecal/interface/EcalCondHeader.h"
 #include <string>
-
 
 namespace edm {
   class ParameterSet;
   class Event;
   class EventSetup;
-}
+}  // namespace edm
 
 namespace popcon {
   class EcalPFRecHitThresholdsHandler : public popcon::PopConSourceHandler<EcalPFRecHitThresholds> {
   public:
-    EcalPFRecHitThresholdsHandler(edm::ParameterSet const & );
-    ~EcalPFRecHitThresholdsHandler() override; 
-			
+    EcalPFRecHitThresholdsHandler(edm::ParameterSet const&);
+    ~EcalPFRecHitThresholdsHandler() override;
+
     void getNewObjects() override;
     void readXML(const std::string& filename, EcalFloatCondObjectContainer& record);
     void readTXT(const std::string& filename, EcalFloatCondObjectContainer& record);
 
-    std::string id() const override { return m_name;}
+    std::string id() const override { return m_name; }
     EcalCondDBInterface* econn;
 
   private:
-    const EcalPFRecHitThresholds * myPFRecHitThresholds;
+    const EcalPFRecHitThresholds* myPFRecHitThresholds;
     std::string m_name;
-    unsigned int m_firstRun ;		
+    unsigned int m_firstRun;
     std::string m_file_name;
     std::string m_file_type;
   };
-}
+}  // namespace popcon
 #endif
-
