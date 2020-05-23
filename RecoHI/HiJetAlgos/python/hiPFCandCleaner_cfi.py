273940ea72f0e42a3578cf285a9b7a5ac417f6ce
Stepan <stepobr@cern.ch>
2020-05-24 00:57:10 +0200

    drop HiPFCandCleaner for a different PR

--- RecoHI/HiJetAlgos/python/hiPFCandCleaner_cfi.py
+++
@@ -1,8 +0,0 @@
-import FWCore.ParameterSet.Config as cms
-
-hiPFCandCleaner = cms.EDProducer(
-    "HiPFCandCleaner",
-    ptMin = cms.double(4.),
-    absEtaMax = cms.double(2.),
-    candidatesSrc = cms.InputTag("particleFlow")
-    )
