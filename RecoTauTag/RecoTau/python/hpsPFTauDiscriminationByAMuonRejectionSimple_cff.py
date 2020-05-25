1330f7888e1244c9332ae4a412d6cc60dd119762
Michal Bluj <michal.bluj@cern.ch>
2020-05-25 18:46:19 +0200

    Switch to corrected energy fraction and enable calo veto in simple anti-mu tauID

--- RecoTauTag/RecoTau/python/hpsPFTauDiscriminationByAMuonRejectionSimple_cff.py
+++ RecoTauTag/RecoTau/python/hpsPFTauDiscriminationByAMuonRejectionSimple_cff.py
@@ -3,12 +3,12 @@ import FWCore.ParameterSet.Config as cms
 from RecoTauTag.Configuration.HPSPFTaus_cff import hpsPFTauDiscriminationByMuonRejection3
 
 hpsPFTauDiscriminationByLooseMuonRejectionSimple = hpsPFTauDiscriminationByMuonRejection3.clone(
-    PFTauProducer=cms.InputTag("hpsPFTauProducer"),
-    HoPMin=cms.double(0.1), #use smaller value than with AOD as raw energy is used
-    doCaloMuonVeto=cms.bool(False), #do not use it until tuned
-    srcPatMuons=cms.InputTag("slimmedMuons"),
-    maxNumberOfSTAMuons=cms.int32(-1),
-    maxNumberOfRPCMuons=cms.int32(-1),
+    PFTauProducer = cms.InputTag("hpsPFTauProducer"),
+    HoPMin = hpsPFTauDiscriminationByMuonRejection3.IDWPdefinitions[0].HoPMin,
+    doCaloMuonVeto = cms.bool(True),
+    srcPatMuons = cms.InputTag("slimmedMuons"),
+    maxNumberOfSTAMuons = cms.int32(-1),
+    maxNumberOfRPCMuons = cms.int32(-1),
     maxNumberOfMatches = hpsPFTauDiscriminationByMuonRejection3.IDWPdefinitions[0].maxNumberOfMatches,
     maxNumberOfHitsLast2Stations = hpsPFTauDiscriminationByMuonRejection3.IDWPdefinitions[0].maxNumberOfHitsLast2Stations
     )
@@ -20,5 +20,5 @@ for attr in ['IDWPdefinitions', 'srcMuons']:
 
 
 hpsPFTauDiscriminationByTightMuonRejectionSimple = hpsPFTauDiscriminationByLooseMuonRejectionSimple.clone(
-    maxNumberOfHitsLast2Stations=hpsPFTauDiscriminationByMuonRejection3.IDWPdefinitions[1].maxNumberOfHitsLast2Stations
+    maxNumberOfHitsLast2Stations = hpsPFTauDiscriminationByMuonRejection3.IDWPdefinitions[1].maxNumberOfHitsLast2Stations
 )
