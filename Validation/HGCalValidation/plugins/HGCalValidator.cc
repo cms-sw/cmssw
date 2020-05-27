dc1b5fe8af290041c3bd8e3ca3bddafac7e0fd5b
Leonardo Cristella <leonardo.cristella@cern.ch>
2020-05-27 11:01:18 +0200

    Align to lowercase module instance

--- Validation/HGCalValidation/plugins/HGCalValidator.cc
+++ Validation/HGCalValidation/plugins/HGCalValidator.cc
@@ -23,7 +23,7 @@ HGCalValidator::HGCalValidator(const edm::ParameterSet& pset)
 
   simVertices_ = consumes<std::vector<SimVertex>>(pset.getParameter<edm::InputTag>("simVertices"));
 
-  hitMap_ = consumes<std::unordered_map<DetId, const HGCRecHit *>>(edm::InputTag("HGCRecHitMapProducer"));
+  hitMap_ = consumes<std::unordered_map<DetId, const HGCRecHit *>>(edm::InputTag("hgcRecHitMapProducer"));
 
   density_ = consumes<Density>(edm::InputTag("hgcalLayerClusters"));
 
