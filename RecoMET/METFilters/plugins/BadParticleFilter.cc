88b24304dec56e611ea40789fb193d51b7f59e56
amkalsi <amkalsi@cern.ch>
2020-05-28 15:24:53 +0200

    new met filter

--- RecoMET/METFilters/plugins/BadParticleFilter.cc
+++ RecoMET/METFilters/plugins/BadParticleFilter.cc
@@ -19,182 +19,220 @@
 #include "DataFormats/TrackReco/interface/TrackFwd.h"
 #include "DataFormats/ParticleFlowCandidate/interface/PFCandidate.h"
 #include "DataFormats/ParticleFlowCandidate/interface/PFCandidateFwd.h"
+#include "DataFormats/VertexReco/interface/Vertex.h"
 
 //
 // class declaration
 //
 
 class BadParticleFilter : public edm::global::EDFilter<> {
-public:
-  explicit BadParticleFilter(const edm::ParameterSet&);
-  ~BadParticleFilter() override;
+	public:
+		explicit BadParticleFilter(const edm::ParameterSet&);
+		~BadParticleFilter() override;
 
-private:
-  bool filter(edm::StreamID iID, edm::Event&, const edm::EventSetup&) const override;
+	private:
+		bool filter(edm::StreamID iID, edm::Event&, const edm::EventSetup&) const override;
 
-  // ----------member data ---------------------------
+		// ----------member data ---------------------------
 
-  edm::EDGetTokenT<edm::View<reco::Candidate> > tokenPFCandidates_;
-  edm::EDGetTokenT<edm::View<reco::Muon> > tokenMuons_;
+		edm::EDGetTokenT<edm::View<reco::Candidate> >   tokenPFCandidates_;
+		edm::EDGetTokenT<edm::View<reco::Muon> >   tokenMuons_;
+		edm::EDGetTokenT<std::vector<reco::Vertex>> vtx_;
 
-  const bool taggingMode_;
-  int algo_;
-  const double maxDR_;
-  const double minPtDiffRel_;
-  const double minMuonTrackRelErr_;
-  const double innerTrackRelErr_;
-  const double minMuPt_;
-  const double segmentCompatibility_;
+		const bool taggingMode_;
+		int             algo_;
+		const double          maxDR_;
+		const double          minPtDiffRel_;
+		const double          minMuonTrackRelErr_;
+		const double          innerTrackRelErr_;
+		const double          minDxyBestTrack;
+		const double          minDzBestTrack;
+		const double          minMuPt_;
+		const double          segmentCompatibility_;
 
-  double maxDR2_;
+		double maxDR2_;
 
-  int filterType_;
-  enum { kBadPFMuon = 0, kBadPFMuonSummer16, kBadChargedCandidate, kBadChargedCandidateSummer16 };
+		int filterType_;
+		enum {kBadPFMuon=0,kBadPFMuonSummer16,kBadChargedCandidate,kBadChargedCandidateSummer16,kBadPFMuonDxyDz,kBadPFMuonDz};
 };
 
 //
 // constructors and destructor
 //
 BadParticleFilter::BadParticleFilter(const edm::ParameterSet& iConfig)
-    : tokenPFCandidates_(consumes<edm::View<reco::Candidate> >(iConfig.getParameter<edm::InputTag>("PFCandidates"))),
-      tokenMuons_(consumes<edm::View<reco::Muon> >(iConfig.getParameter<edm::InputTag>("muons"))),
-      taggingMode_(iConfig.getParameter<bool>("taggingMode")),
-      maxDR_(iConfig.getParameter<double>("maxDR")),
-      minPtDiffRel_(iConfig.getParameter<double>("minPtDiffRel")),
-      minMuonTrackRelErr_(iConfig.getParameter<double>("minMuonTrackRelErr")),
-      innerTrackRelErr_(iConfig.getParameter<double>("innerTrackRelErr")),
-      minMuPt_(iConfig.getParameter<double>("minMuonPt")),
-      segmentCompatibility_(iConfig.getParameter<double>("segmentCompatibility")) {
-  maxDR2_ = maxDR_ * maxDR_;
+	: tokenPFCandidates_ ( consumes<edm::View<reco::Candidate> >(iConfig.getParameter<edm::InputTag> ("PFCandidates")  ))
+	  , tokenMuons_ ( consumes<edm::View<reco::Muon> >(iConfig.getParameter<edm::InputTag> ("muons")  ))
+	  , vtx_(consumes<std::vector<reco::Vertex>>(iConfig.getParameter<edm::InputTag>("vtx")))
+	  , taggingMode_          ( iConfig.getParameter<bool>    ("taggingMode") )
+	  , maxDR_                ( iConfig.getParameter<double>  ("maxDR") )
+	  , minPtDiffRel_         ( iConfig.getParameter<double>  ("minPtDiffRel") )
+	  , minMuonTrackRelErr_   ( iConfig.getParameter<double>  ("minMuonTrackRelErr") )
+	  , innerTrackRelErr_     ( iConfig.getParameter<double>  ("innerTrackRelErr") )
+	  , minDxyBestTrack       ( iConfig.getParameter<double>  ("minDxyBestTrack") )
+	  , minDzBestTrack        ( iConfig.getParameter<double>  ("minDzBestTrack") )
+	  , minMuPt_            ( iConfig.getParameter<double>  ("minMuonPt") )
+	  , segmentCompatibility_ ( iConfig.getParameter<double>  ("segmentCompatibility") )
 
-  std::string filterName = iConfig.getParameter<std::string>("filterType");
-  if (filterName == "BadPFMuon")
-    filterType_ = kBadPFMuon;
-  else if (filterName == "BadPFMuonSummer16")
-    filterType_ = kBadPFMuonSummer16;
-  else if (filterName == "BadChargedCandidate")
-    filterType_ = kBadChargedCandidate;
-  else if (filterName == "BadChargedCandidateSummer16")
-    filterType_ = kBadChargedCandidateSummer16;
-  else {
-    throw cms::Exception("BadParticleFilter") << " Filter " << filterName << " is not available, please check name \n";
-  }
+{
+	maxDR2_=maxDR_*maxDR_;
 
-  algo_ = 0;
-  if (filterType_ == kBadPFMuon) {
-    algo_ = iConfig.getParameter<int>("algo");
-  }
+	std::string filterName=iConfig.getParameter<std::string>("filterType");
+	if(filterName=="BadPFMuon") filterType_=kBadPFMuon;
+	else if(filterName=="BadPFMuonSummer16") filterType_=kBadPFMuonSummer16;
+	else if(filterName=="BadChargedCandidate") filterType_=kBadChargedCandidate;
+	else if(filterName=="BadChargedCandidateSummer16") filterType_=kBadChargedCandidateSummer16;
+	else if(filterName=="BadPFMuonDxyDz") filterType_=kBadPFMuonDxyDz;
+	else if(filterName=="BadPFMuonDz") filterType_=kBadPFMuonDz;
+	else {
+		throw cms::Exception("BadParticleFilter")<<" Filter "<<filterName<<" is not available, please check name \n"; 
+	}
 
-  produces<bool>();
+	algo_=0;
+	if(filterType_==kBadPFMuon || filterType_==kBadPFMuonDxyDz || filterType_==kBadPFMuonDz) {
+		algo_=iConfig.getParameter<int>("algo");
+	}
+
+	produces<bool>();
 }
 
-BadParticleFilter::~BadParticleFilter() {}
+BadParticleFilter::~BadParticleFilter() { }
+
 
 //
 // member functions
 //
 
 // ------------ method called on each new Event  ------------
-bool BadParticleFilter::filter(edm::StreamID iID, edm::Event& iEvent, const edm::EventSetup& iSetup) const {
-  using namespace std;
-  using namespace edm;
+bool
+BadParticleFilter::filter(edm::StreamID iID, edm::Event& iEvent, const edm::EventSetup& iSetup) const
+{
+	using namespace std;
+	using namespace edm;
 
-  typedef View<reco::Candidate> CandidateView;
-  Handle<CandidateView> pfCandidates;
-  iEvent.getByToken(tokenPFCandidates_, pfCandidates);
+	typedef View<reco::Candidate> CandidateView;
+	Handle<CandidateView> pfCandidates;
+	iEvent.getByToken(tokenPFCandidates_,pfCandidates);
 
-  typedef View<reco::Muon> MuonView;
-  Handle<MuonView> muons;
-  iEvent.getByToken(tokenMuons_, muons);
+	typedef View<reco::Muon> MuonView;
+	Handle<MuonView> muons;
+	iEvent.getByToken(tokenMuons_,muons);
 
-  bool foundBadCandidate = false;
+	Handle<std::vector<reco::Vertex>> vtx;
+	iEvent.getByToken(vtx_,  vtx);
+	assert(!vtx->empty());
+	const auto &PV = vtx->front().position();
 
-  for (unsigned i = 0; i < muons->size(); ++i) {  // loop over all muons
 
-    const reco::Muon& muon = (*muons)[i];
+	bool foundBadCandidate = false;
 
-    reco::TrackRef innerMuonTrack = muon.innerTrack();
-    reco::TrackRef bestMuonTrack = muon.muonBestTrack();
+	for ( unsigned i=0; i < muons->size(); ++i ) { // loop over all muons
 
-    if (innerMuonTrack.isNull()) {
-      continue;
-    }
+		const reco::Muon & muon = (*muons)[i];
 
-    if (filterType_ == kBadChargedCandidate || filterType_ == kBadPFMuon) {
-      if (muon.pt() < minMuPt_ && innerMuonTrack->pt() < minMuPt_)
-        continue;
-    }
-    if (filterType_ == kBadChargedCandidateSummer16) {
-      if (muon.pt() < minMuPt_)
-        continue;
-    }
-    if (filterType_ == kBadPFMuonSummer16) {
-      if (innerMuonTrack->pt() < minMuPt_)
-        continue;
-    }
+		reco::TrackRef innerMuonTrack = muon.innerTrack();
+		reco::TrackRef bestMuonTrack = muon.muonBestTrack();
 
-    // Consider only Global Muons
-    if (filterType_ == kBadChargedCandidate || filterType_ == kBadPFMuon) {
-      if (muon.isGlobalMuon() == 0)
-        continue;
-    }
+		if(innerMuonTrack.isNull() ) { 
+			continue; 
+		}
 
-    if (filterType_ == kBadPFMuon || filterType_ == kBadPFMuonSummer16) {
-      if (!(innerMuonTrack->originalAlgo() == algo_ && innerMuonTrack->algo() == algo_))
-        continue;
-    }
+		if(filterType_==kBadChargedCandidate || filterType_==kBadPFMuon || filterType_==kBadPFMuonDxyDz ||  filterType_==kBadPFMuonDz) {
+			if(muon.pt()<minMuPt_ && innerMuonTrack->pt() < minMuPt_) continue;
+		}
+		if(filterType_==kBadChargedCandidateSummer16) {
+			if(muon.pt()<minMuPt_) continue;
+		}
+		if(filterType_==kBadPFMuonSummer16) {
+			if(innerMuonTrack->pt() < minMuPt_) continue;
+		}
 
-    if (filterType_ == kBadChargedCandidate || filterType_ == kBadPFMuon) {
-      if (muon::segmentCompatibility(muon) > segmentCompatibility_ &&
-          bestMuonTrack->ptError() / bestMuonTrack->pt() < minMuonTrackRelErr_ &&
-          innerMuonTrack->ptError() / innerMuonTrack->pt() < innerTrackRelErr_) {
-        continue;
-      }
-    }
-    if (filterType_ == kBadChargedCandidateSummer16 || filterType_ == kBadPFMuonSummer16) {
-      if (innerMuonTrack->quality(reco::TrackBase::highPurity))
-        continue;
-      if (!(innerMuonTrack->ptError() / innerMuonTrack->pt() > minMuonTrackRelErr_))
-        continue;
-    }
+		// Consider only Global Muons  	
+		if(filterType_==kBadChargedCandidate || filterType_==kBadPFMuon || filterType_==kBadPFMuonDxyDz ||  filterType_==kBadPFMuonDz) {
+			if(muon.isGlobalMuon() == 0) continue;
+		}
 
-    for (unsigned j = 0; j < pfCandidates->size(); ++j) {
-      const reco::Candidate& pfCandidate = (*pfCandidates)[j];
 
-      float dr2 = 1000;
-      if (filterType_ == kBadChargedCandidate || filterType_ == kBadChargedCandidateSummer16) {
-        if (!(std::abs(pfCandidate.pdgId()) == 211))
-          continue;
-        dr2 = deltaR2(innerMuonTrack->eta(), innerMuonTrack->phi(), pfCandidate.eta(), pfCandidate.phi());
-        float dpt = (pfCandidate.pt() - innerMuonTrack->pt()) / (0.5 * (innerMuonTrack->pt() + pfCandidate.pt()));
-        if ((dr2 < maxDR2_) && (std::abs(dpt) < minPtDiffRel_) &&
-            (filterType_ == kBadChargedCandidateSummer16 || muon.isPFMuon() == 0)) {
-          foundBadCandidate = true;
-          break;
-        }
-      }
+		if(filterType_==kBadPFMuon || filterType_==kBadPFMuonSummer16 || filterType_==kBadPFMuonDxyDz || filterType_==kBadPFMuonDz) {
+			if(! (innerMuonTrack->originalAlgo() == algo_  &&
+						innerMuonTrack->algo() == algo_ ) ) continue;
+		}
 
-      if (filterType_ == kBadPFMuon || filterType_ == kBadPFMuonSummer16) {
-        if (!((std::abs(pfCandidate.pdgId()) == 13) && (pfCandidate.pt() > minMuPt_)))
-          continue;
-        dr2 = deltaR2(muon.eta(), muon.phi(), pfCandidate.eta(), pfCandidate.phi());
-        if (dr2 < maxDR2_) {
-          foundBadCandidate = true;
-          break;
-        }
-      }
+		if(filterType_==kBadChargedCandidate || filterType_==kBadPFMuon) {
+			if(muon::segmentCompatibility(muon) > segmentCompatibility_ &&
+					bestMuonTrack->ptError()/bestMuonTrack->pt() < minMuonTrackRelErr_ &&
+					innerMuonTrack->ptError()/innerMuonTrack->pt() < innerTrackRelErr_ ) {
+				continue;
+			}
+		}
 
-      if (foundBadCandidate)
-        break;
-    }
-  }  // end loop over muonss
 
-  bool pass = !foundBadCandidate;
+		if(filterType_==kBadPFMuonDxyDz) {
+			if(muon::segmentCompatibility(muon) > segmentCompatibility_ &&
+					bestMuonTrack->ptError()/bestMuonTrack->pt() < minMuonTrackRelErr_ &&
+					innerMuonTrack->ptError()/innerMuonTrack->pt() < innerTrackRelErr_ &&
+					fabs(bestMuonTrack->dxy(PV)) < minDxyBestTrack &&
+					fabs(bestMuonTrack->dz(PV)) < minDzBestTrack    ) {
+				continue;
+			}
+		}
 
-  iEvent.put(std::unique_ptr<bool>(new bool(pass)));
+		if(filterType_==kBadPFMuonDz) {
 
-  return taggingMode_ || pass;
+			if(muon::segmentCompatibility(muon) > segmentCompatibility_ &&
+					bestMuonTrack->ptError()/bestMuonTrack->pt() < minMuonTrackRelErr_ &&
+					innerMuonTrack->ptError()/innerMuonTrack->pt() < innerTrackRelErr_ &&
+					fabs(bestMuonTrack->dz(PV)) < minDzBestTrack    ) {
+				continue;
+			}
+		}
+
+		if(filterType_==kBadChargedCandidateSummer16 || filterType_==kBadPFMuonSummer16) {
+			if(innerMuonTrack->quality(reco::TrackBase::highPurity)) continue;
+			if(!(innerMuonTrack->ptError()/innerMuonTrack->pt() > minMuonTrackRelErr_) ) continue;
+		}
+
+
+		for(unsigned j=0;j<pfCandidates->size();++j ) {
+			const reco::Candidate & pfCandidate = (*pfCandidates)[j];
+
+			float dr2=1000;
+			if(filterType_==kBadChargedCandidate || filterType_==kBadChargedCandidateSummer16) {
+				if(!(std::abs(pfCandidate.pdgId()) == 211) ) continue;
+				dr2 = deltaR2( innerMuonTrack->eta(), innerMuonTrack->phi(), pfCandidate.eta(), pfCandidate.phi() );
+				float dpt = ( pfCandidate.pt() - innerMuonTrack->pt())/(0.5*(innerMuonTrack->pt() + pfCandidate.pt()));
+				if( (dr2<maxDR2_) && (std::abs(dpt)<minPtDiffRel_) &&
+						(filterType_==kBadChargedCandidateSummer16 || muon.isPFMuon()==0) ) {
+					foundBadCandidate = true;
+					break;
+				}
+
+			}
+
+			if(filterType_==kBadPFMuon || filterType_==kBadPFMuonSummer16 || filterType_==kBadPFMuonDxyDz || filterType_==kBadPFMuonDz) {
+				if(!((std::abs(pfCandidate.pdgId()) == 13) && (pfCandidate.pt() > minMuPt_) ) ) continue;
+				dr2 = deltaR2( muon.eta(), muon.phi(), pfCandidate.eta(), pfCandidate.phi() );
+				if (dr2 < maxDR2_) {
+					foundBadCandidate=true;
+					break;
+				}
+			}
+
+			if(foundBadCandidate) break;
+
+		}
+	} // end loop over muonss
+
+	bool pass = !foundBadCandidate;
+
+	iEvent.put( std::unique_ptr<bool>(new bool(pass)) );
+
+	return taggingMode_ || pass;
+
+
 }
+
+
+
 
 //define this as a plug-in
 DEFINE_FWK_MODULE(BadParticleFilter);
--- RecoMET/METFilters/python/BadPFMuonSummer16Filter_cfi.py
+++ RecoMET/METFilters/python/BadPFMuonSummer16Filter_cfi.py
@@ -4,6 +4,7 @@ BadPFMuonSummer16Filter = cms.EDFilter(
     "BadParticleFilter",
     PFCandidates  = cms.InputTag("particleFlow"),   # Collection to test
     muons  = cms.InputTag("muons"),   # Collection to test 
+    vtx = cms.InputTag("offlinePrimaryVertices"),
     taggingMode   = cms.bool(False),
     filterType  =cms.string("BadPFMuonSummer16"),
     maxDR         = cms.double(0.001),              # Maximum DR between reco::muon->innerTrack and pfCandidate
@@ -11,6 +12,8 @@ BadPFMuonSummer16Filter = cms.EDFilter(
     algo          = cms.int32(14),
     innerTrackRelErr  = cms.double(0.5),               # threshold on inner track pt Error
     minMuonTrackRelErr = cms.double(2.0),          # minimum ptError/pt on muon best track
+    minDxyBestTrack = cms.double(-1.0),
+    minDzBestTrack = cms.double(-1.0),
     minMuonPt       = cms.double(100),               # pt threshold on PF muons 
     segmentCompatibility = cms.double(0.3),        # compatibility between the inner track and the segments in the muon spectrometer
     )
