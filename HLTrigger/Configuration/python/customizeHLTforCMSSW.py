e81cfce2a4c6dcb704fd160ea3396eb6305fe4f4
Igor <i.volobouev@ttu.edu>
2020-05-30 19:47:29 +0200

    HLT customization

--- HLTrigger/Configuration/python/customizeHLTforCMSSW.py
+++ HLTrigger/Configuration/python/customizeHLTforCMSSW.py
@@ -18,7 +18,7 @@ from HLTrigger.Configuration.common import *
 #     return process
 
 # Hcal calibration lookup using linear indexing
-def customiseForXXXXX(process):
+def customiseFor30060(process):
     process.hcalChannelPropertiesESProd = cms.ESProducer('HcalChannelPropertiesEP')
     return process
 
@@ -180,5 +180,5 @@ def customizeHLTforCMSSW(process, menuType="GRun"):
 
     # add call to action function in proper order: newest last!
     # process = customiseFor12718(process)
-    process = customiseForXXXXX(process)
+    process = customiseFor30060(process)
     return process
