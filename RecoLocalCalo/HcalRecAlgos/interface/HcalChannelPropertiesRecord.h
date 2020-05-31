75a58b4100ca1e776032b6af6220d08b3d314f29
Igor <i.volobouev@ttu.edu>
2020-05-31 08:08:42 +0200

    explicit inclusion of the header for boost::mpl::vector

--- RecoLocalCalo/HcalRecAlgos/interface/HcalChannelPropertiesRecord.h
+++ RecoLocalCalo/HcalRecAlgos/interface/HcalChannelPropertiesRecord.h
@@ -1,6 +1,7 @@
 #ifndef RecoLocalCalo_HcalRecAlgos_HcalChannelPropertiesRecord_h_
 #define RecoLocalCalo_HcalRecAlgos_HcalChannelPropertiesRecord_h_
 
+#include "boost/mpl/vector.hpp"
 #include "FWCore/Framework/interface/DependentRecordImplementation.h"
 
 class HcalDbRecord;
