10c2f56e2d96633ac4ad20123553c362d1932918
Slava Krutelyov <slava77@cern.ch>
2020-06-02 01:05:17 +0200

    fixup fillDescriptions for TPPFTausOnPFJetsDeltaR; use auto-generated tppfJetsOnPFCandidates and tppfTausOnPFJetsDeltaR

--- CommonTools/ParticleFlow/python/TopProjectors/pfNoJet_cfi.py
+++ CommonTools/ParticleFlow/python/TopProjectors/pfNoJet_cfi.py
@@ -1,10 +1,9 @@
 import FWCore.ParameterSet.Config as cms
+from CommonTools.ParticleFlow.tppfJetsOnPFCandidates_cfi import tppfJetsOnPFCandidates
 
-pfNoJet = cms.EDProducer(
-    "TPPFJetsOnPFCandidates",
-    enable =  cms.bool( True ),
-    verbose = cms.untracked.bool( False ),
-    name = cms.untracked.string("noJet"),
-    topCollection = cms.InputTag("pfJetsPtrs"),
-    bottomCollection = cms.InputTag("pfNoElectronJME"),
+pfNoJet = tppfJetsOnPFCandidates.clone(
+    enable = True,
+    name = "noJet",
+    topCollection = "pfJetsPtrs",
+    bottomCollection = "pfNoElectronJME",
 )
