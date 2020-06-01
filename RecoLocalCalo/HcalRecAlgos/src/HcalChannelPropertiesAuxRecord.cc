6b6ecdf409a114fbab36b227a9aa007c6a3702ab
Igor <i.volobouev@ttu.edu>
2020-06-01 23:57:32 +0200

    removing a private member of HcalChannelPropertiesEP

---
+++ RecoLocalCalo/HcalRecAlgos/src/HcalChannelPropertiesAuxRecord.cc
@@ -0,0 +1,7 @@
+#include "Geometry/Records/interface/HcalRecNumberingRecord.h"
+#include "CondFormats/DataRecord/interface/HcalRecoParamsRcd.h"
+#include "RecoLocalCalo/HcalRecAlgos/interface/HcalChannelPropertiesAuxRecord.h"
+
+#include "FWCore/Framework/interface/eventsetuprecord_registration_macro.h"
+
+EVENTSETUP_RECORD_REG(HcalChannelPropertiesAuxRecord);
--- RecoLocalCalo/HcalRecAlgos/src/HcalChannelPropertiesRecord.cc
+++ RecoLocalCalo/HcalRecAlgos/src/HcalChannelPropertiesRecord.cc
@@ -1,6 +1,7 @@
 #include "CalibFormats/HcalObjects/interface/HcalDbRecord.h"
 #include "Geometry/Records/interface/CaloGeometryRecord.h"
 #include "RecoLocalCalo/HcalRecAlgos/interface/HcalSeverityLevelComputerRcd.h"
+#include "RecoLocalCalo/HcalRecAlgos/interface/HcalChannelPropertiesAuxRecord.h"
 #include "RecoLocalCalo/HcalRecAlgos/interface/HcalChannelPropertiesRecord.h"
 
 #include "FWCore/Framework/interface/eventsetuprecord_registration_macro.h"
