85b70fa44f4b8cc7b4aad324e92ae01dc37289ae
Ianna Osborne <ianna.osborne@cern.ch>
2020-06-03 10:05:29 +0200

    Cleanup DD4hep tests: move them to appropriate subsystems

--- Geometry/TrackerCommonData/test/python/testDDTIDAxialCableAlgorithm.py
+++ Geometry/TrackerCommonData/test/python/testDDTIDAxialCableAlgorithm.py
@@ -1,48 +1,51 @@
 import FWCore.ParameterSet.Config as cms
 
-process = cms.Process("DDCMSDetectorTest")
+process = cms.Process("DDTIDAxialCableAlgoTest")
 
 process.source = cms.Source("EmptySource")
+
 process.maxEvents = cms.untracked.PSet( input = cms.untracked.int32(1) )
 
 process.MessageLogger = cms.Service(
     "MessageLogger",
-    statistics = cms.untracked.vstring('cout', 'testDDPixPhase1FwdDiskAlgo'),
-    categories = cms.untracked.vstring('TrackerGeom'),
+    statistics = cms.untracked.vstring('cout', 'testTIDAxialCableAlgo'),
+    categories = cms.untracked.vstring('TIDGeom'),
     cout = cms.untracked.PSet(
         threshold = cms.untracked.string('WARNING'),
         noLineBreaks = cms.untracked.bool(True)
-    ),
-    testDDPixPhase1FwdDiskAlgo = cms.untracked.PSet(
+        ),
+    testTIDAxialCableAlgo = cms.untracked.PSet(
         INFO = cms.untracked.PSet(
             limit = cms.untracked.int32(0)
-        ),
+            ),
         noLineBreaks = cms.untracked.bool(True),
         DEBUG = cms.untracked.PSet(
             limit = cms.untracked.int32(0)
-        ),
+            ),
         WARNING = cms.untracked.PSet(
             limit = cms.untracked.int32(0)
-        ),
+            ),
         ERROR = cms.untracked.PSet(
             limit = cms.untracked.int32(0)
-        ),
+            ),
         threshold = cms.untracked.string('INFO'),
-        TrackerGeom = cms.untracked.PSet(
+        TIDGeom = cms.untracked.PSet(
             limit = cms.untracked.int32(-1)
-        )
-    ),
+            )
+        ),
     destinations = cms.untracked.vstring('cout',
-                                         'testDDPixPhase1FwdDiskAlgo')
-)
+                                         'testTIDAxialCableAlgo')
+    )
 
 process.DDDetectorESProducer = cms.ESSource("DDDetectorESProducer",
-                                            confGeomXMLFiles = cms.FileInPath('DetectorDescription/DDCMS/data/cms-test-ddpixphase1fwddisk-algo.xml'),
-                                            appendToDataLabel = cms.string('testDDPixPhase1FwdDiskAlgo')
+    confGeomXMLFiles = cms.FileInPath(
+        'Geometry/TrackerCommonData/data/cms-test-ddtidaxialcable-algorithm.xml'
+    ),
+    appendToDataLabel = cms.string('TestTIDAxialCable')
 )
 
 process.testDump = cms.EDAnalyzer("DDTestDumpFile",
-    DDDetector = cms.ESInputTag('','testDDPixPhase1FwdDiskAlgo')
+    DDDetector = cms.ESInputTag('','TestTIDAxialCable')
 )
 
 process.p = cms.Path(process.testDump)
