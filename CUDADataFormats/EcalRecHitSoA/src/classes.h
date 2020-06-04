3360c674854ace212a11958f6b6ab26cf41c8eea
Andrea Massironi <massironi.andrea@gmail.com>
2020-06-04 22:32:11 +0200

    Implement ECAL rechits on GPU (cms-patatrack#462)
    Include standalone executables for the validation of ECAL uncalibrated
    and calibrated rechits.

--- CUDADataFormats/EcalRecHitSoA/src/classes.h
+++ CUDADataFormats/EcalRecHitSoA/src/classes.h
@@ -1,3 +1,4 @@
 #include "DataFormats/Common/interface/Wrapper.h"
 #include "CUDADataFormats/Common/interface/Product.h"
 #include "CUDADataFormats/EcalRecHitSoA/interface/EcalUncalibratedRecHit_soa.h"
+#include "CUDADataFormats/EcalRecHitSoA/interface/EcalRecHit_soa.h"
