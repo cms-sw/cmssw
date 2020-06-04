f8192d36a32e5d495e912e46faf0e9d5f36086cf
Sven Dildick <sven.dildick@cern.ch>
2020-06-03 22:19:56 -0500

    Recast ALCT patterns

--- L1Trigger/CSCTriggerPrimitives/interface/CSCPatternBank.h
+++ L1Trigger/CSCTriggerPrimitives/interface/CSCPatternBank.h
@@ -10,26 +10,21 @@
 
 class CSCPatternBank {
 public:
-  typedef std::vector<std::vector<std::vector<int> > > CLCTPatterns;
-
-  typedef int ALCTPatterns[CSCConstants::NUM_ALCT_PATTERNS][CSCConstants::MAX_WIRES_IN_PATTERN];
+  // typedef used for both ALCT and CLCT
+  typedef std::vector<std::vector<std::vector<int> > > LCTPatterns;
 
   /** Pre-defined ALCT patterns. */
 
-  // This is the pattern envelope, which is used to define the collision
-  // patterns A and B.
-  static const int alct_pattern_envelope[CSCConstants::MAX_WIRES_IN_PATTERN];
-
   // key wire offsets for ME1 and ME2 are the same
   // offsets for ME3 and ME4 are the same
-  static const int alct_keywire_offset[2][CSCConstants::MAX_WIRES_IN_PATTERN];
+  static const int alct_keywire_offset_[2][CSCConstants::ALCT_PATTERN_WIDTH];
 
   // Since the test beams in 2003, both collision patterns are "completely
   // open".  This is our current default.
-  static const ALCTPatterns alct_pattern_mask_open;
+  static const LCTPatterns alct_pattern_legacy_;
 
   // Special option for narrow pattern for ring 1 stations
-  static const ALCTPatterns alct_pattern_mask_r1;
+  static const LCTPatterns alct_pattern_r1_;
 
   /** Pre-defined CLCT patterns. */
 
@@ -39,10 +34,10 @@ public:
   // Bend of 0 is right/straight and bend of 1 is left.
   // The pattern maximum width is the last number in the the 6th layer
   // Use during Run-1 and Run-2
-  static const CLCTPatterns clct_pattern_legacy_;
+  static const LCTPatterns clct_pattern_legacy_;
 
   // New patterns for Run-3
-  static const CLCTPatterns clct_pattern_run3_;
+  static const LCTPatterns clct_pattern_run3_;
 
   // half strip offsets per layer for each half strip in the pattern envelope
   static const int clct_pattern_offset_[CSCConstants::CLCT_PATTERN_WIDTH];
