ebe34dbdca9ff2849917fcec8e3dd344218b1687
Vladimir <Vladimir.Ivantchenko@cern.ch>
2020-06-07 13:31:40 +0200

    code format

--- SimG4Core/PhysicsLists/interface/CMSHyperonFTFPBuilder.h
+++ SimG4Core/PhysicsLists/interface/CMSHyperonFTFPBuilder.h
@@ -1,11 +1,11 @@
 //---------------------------------------------------------------------------
 // Author: Vladimir Ivanchenko
-// Date:   June 2020                                                                                                              
+// Date:   June 2020
 //
 // Hyperon physics for the new CMS physics list FTFP_BERT_EMM
 // The hadron physics of FTFP_BERT has the transition between Bertini
 // (BERT) intra-nuclear cascade model and Fritiof (FTF) string model in the
-// energy region defined by hadronic parametes                                                              
+// energy region defined by hadronic parametes
 //---------------------------------------------------------------------------
 
 #ifndef SimG4Core_PhysicsLists_CMSHyperonFTFPBuilder_h
@@ -14,14 +14,12 @@
 #include "G4PhysicsBuilderInterface.hh"
 #include "globals.hh"
 
-class CMSHyperonFTFPBuilder : public G4PhysicsBuilderInterface
-{
-public: 
+class CMSHyperonFTFPBuilder : public G4PhysicsBuilderInterface {
+public:
   CMSHyperonFTFPBuilder();
   ~CMSHyperonFTFPBuilder() override;
 
-  void Build() final override;
+  void Build() final;
 };
 
 #endif
-
