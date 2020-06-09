e4f9d2c592db5e05d8877d4cc6a3444fe2eeb75a
Luiz Mundim <luiz.mundim@cern.ch>
2020-06-10 00:31:14 +0200

    bringing a file compliant with code-format option

--- SimTransport/PPSProtonTransport/src/OpticalFunctionsTransport.cc
+++ SimTransport/PPSProtonTransport/src/OpticalFunctionsTransport.cc
@@ -26,20 +26,24 @@ void OpticalFunctionsTransport::process(const HepMC::GenEvent* evt,
   iSetup.get<CTPPSBeamParametersRcd>().get(beamParameters_);
   iSetup.get<CTPPSInterpolatedOpticsRcd>().get(opticsLabel_, opticalFunctions_);
 
-// Choose the optical function corresponding to the first station ono each side (it is in lhc ref. frame)
+  // Choose the optical function corresponding to the first station ono each side (it is in lhc ref. frame)
   optFunctionId45_ = 0;
   optFunctionId56_ = 0;
-  for (const auto& ofp:(*opticalFunctions_)) {
-      if (ofp.second.getScoringPlaneZ()<0) {
-         if (optFunctionId45_==0) optFunctionId45_=ofp.first;
-         if (opticalFunctions_->at(optFunctionId45_).getScoringPlaneZ()<ofp.second.getScoringPlaneZ()) optFunctionId45_=ofp.first;
-      }
-      if (ofp.second.getScoringPlaneZ()>0) {
-         if (optFunctionId56_==0) optFunctionId56_= ofp.first;
-         if (opticalFunctions_->at(optFunctionId56_).getScoringPlaneZ()>ofp.second.getScoringPlaneZ()) optFunctionId56_=ofp.first;
-      }
+  for (const auto& ofp : (*opticalFunctions_)) {
+    if (ofp.second.getScoringPlaneZ() < 0) {
+      if (optFunctionId45_ == 0)
+        optFunctionId45_ = ofp.first;
+      if (opticalFunctions_->at(optFunctionId45_).getScoringPlaneZ() < ofp.second.getScoringPlaneZ())
+        optFunctionId45_ = ofp.first;
+    }
+    if (ofp.second.getScoringPlaneZ() > 0) {
+      if (optFunctionId56_ == 0)
+        optFunctionId56_ = ofp.first;
+      if (opticalFunctions_->at(optFunctionId56_).getScoringPlaneZ() > ofp.second.getScoringPlaneZ())
+        optFunctionId56_ = ofp.first;
+    }
   }
-//
+  //
   engine_ = _engine;  // the engine needs to be updated for each event
 
   for (HepMC::GenEvent::particle_const_iterator eventParticle = evt->particles_begin();
@@ -98,7 +102,8 @@ bool OpticalFunctionsTransport::transportProton(const HepMC::GenParticle* in_trk
     empiricalAperture_a_int = empiricalAperture56_a_int_;
     empiricalAperture_a_slp = empiricalAperture56_a_slp_;
   }
-  if (xangle>1.0) xangle*=urad;
+  if (xangle > 1.0)
+    xangle *= urad;
   // calculate kinematics for optics parametrisation
   const double p = mom_lhc.rho();
   const double xi = 1. - p / beamMomentum;
@@ -108,14 +113,15 @@ bool OpticalFunctionsTransport::transportProton(const HepMC::GenParticle* in_trk
   const double vtx_lhc_eff_y = vtx_lhc.y() - vtx_lhc.z() * (mom_lhc.y() / mom_lhc.z());
 
   if (verbosity_) {
-      LogDebug("OpticalFunctionsTransport")
+    LogDebug("OpticalFunctionsTransport")
         << "simu: xi = " << xi << ", th_x_phys = " << th_x_phys << ", th_y_phys = " << th_y_phys
         << ", vtx_lhc_eff_x = " << vtx_lhc_eff_x << ", vtx_lhc_eff_y = " << vtx_lhc_eff_y << std::endl;
   }
 
   // check empirical aperture
   if (useEmpiricalApertures_) {
-    const auto& xangle = (lhcInfo_->crossingAngle()>1.0)?lhcInfo_->crossingAngle()*urad:lhcInfo_->crossingAngle();
+    const auto& xangle =
+        (lhcInfo_->crossingAngle() > 1.0) ? lhcInfo_->crossingAngle() * urad : lhcInfo_->crossingAngle();
     const double xi_th = (empiricalAperture_xi0_int + xangle * empiricalAperture_xi0_slp) +
                          (empiricalAperture_a_int + xangle * empiricalAperture_a_slp) * th_x_phys;
 
@@ -127,45 +133,44 @@ bool OpticalFunctionsTransport::transportProton(const HepMC::GenParticle* in_trk
     }
   }
 
-
   // transport the proton into  pot/scoring plane
   auto ofp = opticalFunctions_->at(optFunctionId_);
   CTPPSDetId rpId(optFunctionId_);
   const unsigned int rpDecId = rpId.arm() * 100 + rpId.station() * 10 + rpId.rp();
 
   if (verbosity_)
-      LogDebug("OpticalFunctionsTransport") << "  RP " << rpDecId << std::endl;
+    LogDebug("OpticalFunctionsTransport") << "  RP " << rpDecId << std::endl;
 
-    // transport proton
+  // transport proton
   LHCInterpolatedOpticalFunctionsSet::Kinematics k_in = {
-        vtx_lhc_eff_x * 1E-1, th_x_phys, vtx_lhc_eff_y * 1E-1, th_y_phys, xi};  // conversions: mm -> cm
+      vtx_lhc_eff_x * 1E-1, th_x_phys, vtx_lhc_eff_y * 1E-1, th_y_phys, xi};  // conversions: mm -> cm
 
   LHCInterpolatedOpticalFunctionsSet::Kinematics k_out;
   ofp.transport(k_in, k_out, true);
 
-// Original code uses mm, but CMS uses cm, so keep it in cm
-  double b_x = k_out.x * 1E1,    b_y = k_out.y * 1E1;  // conversions: cm -> mm
+  // Original code uses mm, but CMS uses cm, so keep it in cm
+  double b_x = k_out.x * 1E1, b_y = k_out.y * 1E1;  // conversions: cm -> mm
   double a_x = k_out.th_x, a_y = k_out.th_y;
 
   // if needed, subtract beam position and angle
   if (produceHitsRelativeToBeam_) {
-      // determine beam position
-      LHCInterpolatedOpticalFunctionsSet::Kinematics k_be_in = {0., 0., 0., 0., 0.};
-      LHCInterpolatedOpticalFunctionsSet::Kinematics k_be_out;
-      ofp.transport(k_be_in, k_be_out, true);
+    // determine beam position
+    LHCInterpolatedOpticalFunctionsSet::Kinematics k_be_in = {0., 0., 0., 0., 0.};
+    LHCInterpolatedOpticalFunctionsSet::Kinematics k_be_out;
+    ofp.transport(k_be_in, k_be_out, true);
 
-      a_x -= k_be_out.th_x;
-      a_y -= k_be_out.th_y;
-      b_x -= k_be_out.x * 1E1;  
-      b_y -= k_be_out.y * 1E1;  // conversions: cm -> mm
+    a_x -= k_be_out.th_x;
+    a_y -= k_be_out.th_y;
+    b_x -= k_be_out.x * 1E1;
+    b_y -= k_be_out.y * 1E1;  // conversions: cm -> mm
   }
 
   const double z_scoringPlane = ofp.getScoringPlaneZ() * 1E1;  // conversion: cm --> mm
 
   if (verbosity_) {
-      LogDebug("OpticalFunctionsTransport")
-          << "    proton transported: a_x = " << a_x << " rad, a_y = " << a_y << " rad, b_x = " << b_x
-          << " mm, b_y = " << b_y << " mm, z = " << z_scoringPlane << " mm" << std::endl;
+    LogDebug("OpticalFunctionsTransport")
+        << "    proton transported: a_x = " << a_x << " rad, a_y = " << a_y << " rad, b_x = " << b_x
+        << " mm, b_y = " << b_y << " mm, z = " << z_scoringPlane << " mm" << std::endl;
   }
 
   unsigned int line = in_trk->barcode();
