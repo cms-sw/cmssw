5ce8d45849aeeed5ee3a5dbb69d6f50f4083c623
amkalsi <amkalsi@cern.ch>
2020-06-10 01:03:37 +0200

    Name of EDFilter

--- RecoMET/METFilters/python/BadPFMuonFilter_Dz_cfi.py
+++ RecoMET/METFilters/python/BadPFMuonFilter_Dz_cfi.py
@@ -1,6 +1,6 @@
 import FWCore.ParameterSet.Config as cms
 
-BadPFMuonFilter = cms.EDFilter(
+BadPFMuonFilterDz = cms.EDFilter(
     "BadParticleFilter",
     PFCandidates  = cms.InputTag("particleFlow"),   # Collection to test
     muons  = cms.InputTag("muons"),   # Collection to test 
@@ -13,7 +13,7 @@ BadPFMuonFilter = cms.EDFilter(
     minMuonTrackRelErr    = cms.double(2.0),               # threshold on best track RelptError
     innerTrackRelErr = cms.double(1.0),            # threshold on innerTrack relPtErr
     minDxyBestTrack = cms.double(-1.0),
-    minDzBestTrack = cms.double(-1.0),
+    minDzBestTrack = cms.double(0.5),
     minMuonPt       = cms.double(100),               # pt threshold on PF muons 
     segmentCompatibility = cms.double(0.3),        # compatibility between the inner track and the segments in the muon spectrometer
 )
