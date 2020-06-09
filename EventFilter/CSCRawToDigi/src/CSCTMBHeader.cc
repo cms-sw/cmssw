71085f3abbe15d0aeeef425b625a4b5637d70ce0
Sven Dildick <sven.dildick@cern.ch>
2020-06-09 13:35:37 -0500

    Rename CSCCLCTData to CSCComparatorData

--- EventFilter/CSCRawToDigi/src/CSCTMBHeader.cc
+++ EventFilter/CSCRawToDigi/src/CSCTMBHeader.cc
@@ -25,7 +25,7 @@ CSCTMBHeader::CSCTMBHeader(int firmwareVersion, int firmwareRevision)
     theHeaderFormat = std::shared_ptr<CSCVTMBHeaderFormat>(new CSCTMBHeader2006());
   } else if (firmwareVersion == 2007) {
     /* Checks for TMB2007 firmware revisions ranges to detect data format
-       * rev.0x50c3 - first revision with changed format 
+       * rev.0x50c3 - first revision with changed format
        * rev.0x42D5 - oldest known from 06/21/2007
        * There is 4-bits year value rollover in revision number (0 in 2016)
        */
@@ -55,7 +55,7 @@ CSCTMBHeader::CSCTMBHeader(const unsigned short *buf) : theHeaderFormat() {
     theFirmwareVersion = 2007;
     theHeaderFormat = std::shared_ptr<CSCVTMBHeaderFormat>(new CSCTMBHeader2007(buf));
     /* Checks for TMB2007 firmware revisions ranges to detect data format
-       * rev.0x50c3 - first revision with changed format 
+       * rev.0x50c3 - first revision with changed format
        * rev.0x42D5 - oldest known from 06/21/2007
        * There is 4-bits year value rollover in revision number (0 in 2016)
        */
