c92e3d67c080316a77d90bdd2e237355f0bbc997
Sunanda <sunanda.banerjee@cern.ch>
2020-06-11 00:23:48 +0200

    Code check

--- Geometry/HGCalCommonData/src/HGCalTileIndex.cc
+++ Geometry/HGCalCommonData/src/HGCalTileIndex.cc
@@ -9,16 +9,13 @@ const int32_t kHGCalRingMask = 0x1FF;
 
 int32_t HGCalTileIndex::tileIndex(const int32_t& layer, const int32_t& ring, const int32_t& phi) {
   int32_t id(0);
-  id |= (((phi & kHGCalPhiMask) << kHGCalPhiOffset) |
-	 ((ring & kHGCalRingMask) << kHGCalRingOffset) |
-	 ((layer & kHGCalLayerMask) << kHGCalLayerOffset));
+  id |= (((phi & kHGCalPhiMask) << kHGCalPhiOffset) | ((ring & kHGCalRingMask) << kHGCalRingOffset) |
+         ((layer & kHGCalLayerMask) << kHGCalLayerOffset));
   return id;
 }
 
 int32_t HGCalTileIndex::tileLayer(const int32_t& id) { return ((id >> kHGCalLayerOffset) & kHGCalLayerMask); }
 
-int32_t HGCalTileIndex::tileRing(const int32_t& id) { return ((id >> kHGCalRingOffset) & kHGCalRingMask);
-}
+int32_t HGCalTileIndex::tileRing(const int32_t& id) { return ((id >> kHGCalRingOffset) & kHGCalRingMask); }
 
-int32_t HGCalTileIndex::tilePhi(const int32_t& id) { return ((id >> kHGCalPhiOffset) & kHGCalPhiMask);
-}
+int32_t HGCalTileIndex::tilePhi(const int32_t& id) { return ((id >> kHGCalPhiOffset) & kHGCalPhiMask); }
