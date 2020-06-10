9f6a79b44a332bbca10463cc2a5ede8e6000323b
Sunanda <sunanda.banerjee@cern.ch>
2020-06-10 23:27:51 +0200

    Debug the partial wafer through masking

--- Geometry/HGCalCommonData/test/HGCalNumberingTester.cc
+++ Geometry/HGCalCommonData/test/HGCalNumberingTester.cc
@@ -42,7 +42,6 @@
 class HGCalNumberingTester : public edm::one::EDAnalyzer<> {
 public:
   explicit HGCalNumberingTester(const edm::ParameterSet&);
-  ~HGCalNumberingTester() override;
 
   void beginJob() override {}
   void analyze(edm::Event const& iEvent, edm::EventSetup const&) override;
@@ -80,16 +79,11 @@ HGCalNumberingTester::HGCalNumberingTester(const edm::ParameterSet& iC) {
       positionY_[k] /= CLHEP::mm;
     }
   }
-  std::cout << "Test numbering for " << nameDetector_ << " using constants of " << nameSense_ << " at "
-            << positionX_.size() << " local positions "
-            << "for every " << increment_ << " layers for DetType " << detType_ << " and  RecoFlag " << reco_
-            << std::endl;
+  std::cout << "Test numbering for " << nameDetector_ << " using constants of " << nameSense_ << " at " << positionX_.size() << " local positions " << "for every " << increment_ << " layers for DetType " << detType_ << " and  RecoFlag " << reco_ << std::endl;
   for (unsigned int k = 0; k < positionX_.size(); ++k)
     std::cout << "Position[" << k << "] " << positionX_[k] << " " << unit << ", " << positionY_[k] << " " << unit
               << std::endl;
 }
-
-HGCalNumberingTester::~HGCalNumberingTester() {}
 
 // ------------ method called to produce the data  ------------
 void HGCalNumberingTester::analyze(const edm::Event& iEvent, const edm::EventSetup& iSetup) {
@@ -99,9 +93,11 @@ void HGCalNumberingTester::analyze(const edm::Event& iEvent, const edm::EventSet
   if (detType_ != 0) {
     std::cout << "Minimum Wafer # " << hgdc.waferMin() << " Mamximum Wafer # " << hgdc.waferMax() << " Wafer counts "
               << hgdc.waferCount(0) << ":" << hgdc.waferCount(1) << std::endl;
-    for (unsigned int i = 0; i < hgdc.layers(true); ++i)
-      std::cout << "Layer " << i + 1 << " Wafers " << hgdc.wafers(i + 1, 0) << ":" << hgdc.wafers(i + 1, 1) << ":"
-                << hgdc.wafers(i + 1, 2) << std::endl;
+    for (unsigned int i = 0; i < hgdc.layers(true); ++i) {
+      int lay = i + 1;
+      double z = hgdc.waferZ(lay, reco_);
+      std::cout << "Layer " << lay << " Wafers " << hgdc.wafers(lay, 0) << ":" << hgdc.wafers(lay, 1) << ":" << hgdc.wafers(lay, 2) << " Z " << z << " R " << hgdc.rangeR(z, reco_).first << ":" << hgdc.rangeR(z, reco_).second << std::endl;
+    }
   }
   std::cout << std::endl << std::endl;
   std::pair<float, float> xy;
