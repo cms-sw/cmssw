e06ba5b06e1417dfe95829e1f7b0b4d39bfacd06
Stepan <stepobr@cern.ch>
2020-06-10 16:54:02 +0200

    fix code check warnings

--- RecoHI/HiJetAlgos/plugins/ParticleTowerProducer.cc
+++ RecoHI/HiJetAlgos/plugins/ParticleTowerProducer.cc
@@ -62,10 +62,10 @@ private:
 
   // tower edges from fast sim, used starting at index 30 for the HF
   static constexpr int ietaMax = 42;
-  static constexpr double etaedge[ietaMax] = {0.000, 0.087, 0.174, 0.261, 0.348, 0.435, 0.522, 0.609, 0.696, 0.783, 0.870,
-                                         0.957, 1.044, 1.131, 1.218, 1.305, 1.392, 1.479, 1.566, 1.653, 1.740, 1.830,
-                                         1.930, 2.043, 2.172, 2.322, 2.500, 2.650, 2.853, 3.000, 3.139, 3.314, 3.489,
-                                         3.664, 3.839, 4.013, 4.191, 4.363, 4.538, 4.716, 4.889, 5.191};
+  static constexpr double etaedge[ietaMax] = {
+      0.000, 0.087, 0.174, 0.261, 0.348, 0.435, 0.522, 0.609, 0.696, 0.783, 0.870, 0.957, 1.044, 1.131,
+      1.218, 1.305, 1.392, 1.479, 1.566, 1.653, 1.740, 1.830, 1.930, 2.043, 2.172, 2.322, 2.500, 2.650,
+      2.853, 3.000, 3.139, 3.314, 3.489, 3.664, 3.839, 4.013, 4.191, 4.363, 4.538, 4.716, 4.889, 5.191};
 };
 //
 // constructors and destructor
@@ -83,7 +83,7 @@ ParticleTowerProducer::ParticleTowerProducer(const edm::ParameterSet& iConfig)
 // ------------ method called to produce the data  ------------
 void ParticleTowerProducer::produce(edm::Event& iEvent, const edm::EventSetup& iSetup) {
   using namespace edm;
-  
+
   typedef std::pair<int, int> EtaPhi;
   typedef std::map<EtaPhi, double> EtaPhiMap;
   EtaPhiMap towers_;
