b186e5a2a0377aebb52e9a1a85197bc4cb98ae54
Olga Kodolova <Olga.Kodolova@cern.ch>
2020-06-10 16:06:58 +0200

    Applied patch https://cmssdt.cern.ch/SDT/code-checks/cms-sw-PR-30160/15988/code-format.patch e.g. curl https://cmssdt.cern.ch/SDT/code-checks/cms-sw-PR-30160/15988/code-format.patch | patch -p1 You can also run scram build code-format Provide some additional clean-up: introduce variable dRcone instead of 0.4

--- RecoJets/JetPlusTracks/plugins/TrackFromPFProducer.cc
+++ RecoJets/JetPlusTracks/plugins/TrackFromPFProducer.cc
@@ -16,47 +16,46 @@
 #include "Geometry/Records/interface/TrackerTopologyRcd.h"
 #include "DataFormats/PatCandidates/interface/PackedCandidate.h"
 
-TrackFromPFProducer::TrackFromPFProducer(const edm::ParameterSet& iConfig)
-{
- tokenPFCandidates_=consumes<pat::PackedCandidateCollection>(iConfig.getParameter<edm::InputTag>( "PFCandidates" )); 
- tokenPFCandidatesLostTracks_=consumes<pat::PackedCandidateCollection>(iConfig.getParameter<edm::InputTag>( "PFCandidatesLostTracks" ));
- produces<reco::TrackCollection>("tracksFromPF");
+TrackFromPFProducer::TrackFromPFProducer(const edm::ParameterSet& iConfig) {
+  tokenPFCandidates_ = consumes<pat::PackedCandidateCollection>(iConfig.getParameter<edm::InputTag>("PFCandidates"));
+  tokenPFCandidatesLostTracks_ =
+      consumes<pat::PackedCandidateCollection>(iConfig.getParameter<edm::InputTag>("PFCandidatesLostTracks"));
+  produces<reco::TrackCollection>("tracksFromPF");
 }
 
-
-void TrackFromPFProducer::produce(edm::Event& theEvent, const edm::EventSetup& setup)
-{
+void TrackFromPFProducer::produce(edm::Event& theEvent, const edm::EventSetup& setup) {
   //
   // create empty output collections
   //
   auto outputTColl = std::make_unique<reco::TrackCollection>();
 
-    // std::cout<<" TrackFromPFProducer::produce "<<std::endl;
-    edm::Handle<pat::PackedCandidateCollection> pfCandidates;
-    theEvent.getByToken( tokenPFCandidates_, pfCandidates);
+  // std::cout<<" TrackFromPFProducer::produce "<<std::endl;
+  edm::Handle<pat::PackedCandidateCollection> pfCandidates;
+  theEvent.getByToken(tokenPFCandidates_, pfCandidates);
 
-    for(unsigned int i = 0, n = pfCandidates->size(); i < n; ++i) {
-        const pat::PackedCandidate &pf = (*pfCandidates)[i];
-        if(pf.hasTrackDetails()){
-          const reco::Track &mytrack = pf.pseudoTrack();
+  for (unsigned int i = 0, n = pfCandidates->size(); i < n; ++i) {
+    const pat::PackedCandidate& pf = (*pfCandidates)[i];
+    if (pf.hasTrackDetails()) {
+      const reco::Track& mytrack = pf.pseudoTrack();
 
-         // std::cout<<" Track "<<std::isfinite(mytrack.pt())<<" "<<
-           //    isnan(mytrack.eta())<<" "<<pf.ptTrk()<<" "<<pf.etaAtVtx()<<" "<<
-             // mytrack.found()<<" "<<mytrack.pt()<<" "<<mytrack.eta()<<" "<<mytrack.phi()<<" "<<
-               //                 mytrack.vx()<<" "<<mytrack.vy()<<" "<<
-                 //               mytrack.vz()<<std::endl;
-           if(isnan(mytrack.pt())||isnan(mytrack.eta())||isnan(mytrack.phi())) continue;
-           if(!std::isfinite(mytrack.pt()) || !std::isfinite(mytrack.eta()) || !std::isfinite(mytrack.phi())) continue;
-           // std::cout<<" Accept "<<std::endl;
-           outputTColl->push_back(mytrack);          
-          }  
-           //else {
-           //  if(pf.charge()>0) std::cout<<pf.vx()<<" "<<pf.pt() <<std::endl;
-           // }
+      // std::cout<<" Track "<<std::isfinite(mytrack.pt())<<" "<<
+      //    isnan(mytrack.eta())<<" "<<pf.ptTrk()<<" "<<pf.etaAtVtx()<<" "<<
+      // mytrack.found()<<" "<<mytrack.pt()<<" "<<mytrack.eta()<<" "<<mytrack.phi()<<" "<<
+      //                 mytrack.vx()<<" "<<mytrack.vy()<<" "<<
+      //               mytrack.vz()<<std::endl;
+      if (isnan(mytrack.pt()) || isnan(mytrack.eta()) || isnan(mytrack.phi()))
+        continue;
+      if (!std::isfinite(mytrack.pt()) || !std::isfinite(mytrack.eta()) || !std::isfinite(mytrack.phi()))
+        continue;
+      // std::cout<<" Accept "<<std::endl;
+      outputTColl->push_back(mytrack);
     }
+    //else {
+    //  if(pf.charge()>0) std::cout<<pf.vx()<<" "<<pf.pt() <<std::endl;
+    // }
+  }
 
   //put everything in the event
-  theEvent.put(std::move(outputTColl),"tracksFromPF");
-
+  theEvent.put(std::move(outputTColl), "tracksFromPF");
 }
 DEFINE_FWK_MODULE(TrackFromPFProducer);
