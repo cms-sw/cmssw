cc06c3696a778f6072ba5df42985e244075f1e38
Sven Dildick <sven.dildick@cern.ch>
2020-06-12 12:04:48 -0500

    Merge header and source files

--- EventFilter/CSCRawToDigi/plugins/module.cc
+++
@@ -1,7 +0,0 @@
-#include "EventFilter/CSCRawToDigi/interface/DigiAnalyzer.h"
-#include "EventFilter/CSCRawToDigi/src/CSCDigiToPattern.h"
-#include "EventFilter/CSCRawToDigi/interface/CSCViewDigi.h"
-
-DEFINE_FWK_MODULE(DigiAnalyzer);
-DEFINE_FWK_MODULE(CSCDigiToPattern);
-DEFINE_FWK_MODULE(CSCViewDigi);
