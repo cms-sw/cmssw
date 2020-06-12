89cfddd62aaacc9c2a24ed2c2b9e60b2a053912b
Sunanda <sunanda.banerjee@cern.ch>
2020-06-12 18:33:49 +0200

    One more try

--- Geometry/CSCGeometryBuilder/python/cscGeometryDB_cfi.py
+++ Geometry/CSCGeometryBuilder/python/cscGeometryDB_cfi.py
@@ -1,14 +1,26 @@
-from Geometry.CSCGeometryBuilder.CSCGeometryESModule_cfi import CSCGeometryESModule
+import FWCore.ParameterSet.Config as cms
+
+#
+# This cfi should be included to build the CSC geometry model.
+#
+# modelling flags (for completeness - internal defaults are already sane)
+# GF would like to have a shorter name (e.g. CSCGeometry), but since originally
+# there was no name, replace statements in other configs would not work anymore...
+CSCGeometryESModule = cms.ESProducer("CSCGeometryESModule",
+  useDDD = cms.bool(False),
+  useDD4hep = cms.bool(False),
+  alignmentsLabel = cms.string(''),
+  appendToDataLabel = cms.string(''),
+  useRealWireGeometry = cms.bool(True),
+  useOnlyWiresInME1a = cms.bool(False),
+  useGangedStripsInME1a = cms.bool(True),
+  useCentreTIOffsets = cms.bool(False),
+  applyAlignment = cms.bool(True),  ## GF: to be abandoned
+  debugV = cms.untracked.bool(False)
+)
 
 #
 # Modify for running in run 2
 #
 from Configuration.Eras.Modifier_run2_common_cff import run2_common
-run2_common.toModify( CSCGeometryESModule, useGangedStripsInME1a = False )
-
-#
-# Modify for running with no ddd/dd4hep
-#
-
-CSCGeometryESModule.useDDD = False
-CSCGeometryESModule.useDD4Hep = False
+run2_common.toModify( CSCGeometryESModule, useGangedStripsInME1a=False )
