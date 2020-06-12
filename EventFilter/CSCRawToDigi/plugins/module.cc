14675388a6ccb8b49f9293d0e20992a0ce3ade10
Sven Dildick <sven.dildick@cern.ch>
2020-06-12 11:52:27 -0500

    Merge header and source files

--- EventFilter/CSCRawToDigi/plugins/module.cc
+++ EventFilter/CSCRawToDigi/plugins/module.cc
@@ -1,12 +1,7 @@
-#include "FWCore/Framework/interface/MakerMacros.h"
-#include <EventFilter/CSCRawToDigi/plugins/CSCDCCUnpacker.h>
-#include <EventFilter/CSCRawToDigi/interface/DigiAnalyzer.h>
-#include <EventFilter/CSCRawToDigi/src/CSCDigiToPattern.h>
-#include <EventFilter/CSCRawToDigi/interface/CSCViewDigi.h>
-#include <EventFilter/CSCRawToDigi/plugins/CSCDigiValidator.h>
+#include "EventFilter/CSCRawToDigi/interface/DigiAnalyzer.h"
+#include "EventFilter/CSCRawToDigi/src/CSCDigiToPattern.h"
+#include "EventFilter/CSCRawToDigi/interface/CSCViewDigi.h"
 
-DEFINE_FWK_MODULE(CSCDCCUnpacker);
 DEFINE_FWK_MODULE(DigiAnalyzer);
 DEFINE_FWK_MODULE(CSCDigiToPattern);
 DEFINE_FWK_MODULE(CSCViewDigi);
-DEFINE_FWK_MODULE(CSCDigiValidator);
