a66a2cfcaa314f9baf4976dbdbc9736a964abc41
Sunanda <sunanda.banerjee@cern.ch>
2020-06-12 03:29:17 +0200

    Take Matti's comments

--- Geometry/HGCalCommonData/src/HGCalTileIndex.cc
+++ Geometry/HGCalCommonData/src/HGCalTileIndex.cc
@@ -1,12 +1,5 @@
 #include "Geometry/HGCalCommonData/interface/HGCalTileIndex.h"
 
-const int32_t kHGCalLayerOffset = 18;
-const int32_t kHGCalLayerMask = 0x1F;
-const int32_t kHGCalPhiOffset = 0;
-const int32_t kHGCalPhiMask = 0x1FF;
-const int32_t kHGCalRingOffset = 9;
-const int32_t kHGCalRingMask = 0x1FF;
-
 int32_t HGCalTileIndex::tileIndex(const int32_t& layer, const int32_t& ring, const int32_t& phi) {
   int32_t id(0);
   id |= (((phi & kHGCalPhiMask) << kHGCalPhiOffset) | ((ring & kHGCalRingMask) << kHGCalRingOffset) |
