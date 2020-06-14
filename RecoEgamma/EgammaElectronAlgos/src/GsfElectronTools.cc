3e1de56008e4e8f9334caada386a8ff4aabcdbc7
Jonas Rembser <jonas.rembser@cern.ch>
2020-06-14 16:08:14 +0200

    avoid remaining math operations in inner loop of getClosestCtfToGsf

--- RecoEgamma/EgammaElectronAlgos/src/GsfElectronTools.cc
+++ RecoEgamma/EgammaElectronAlgos/src/GsfElectronTools.cc
@@ -1,8 +1,6 @@
-#include "DataFormats/EgammaCandidates/interface/GsfElectronCoreFwd.h"
 #include "DataFormats/EgammaCandidates/interface/GsfElectronCore.h"
 #include "DataFormats/ParticleFlowReco/interface/GsfPFRecTrack.h"
 #include "DataFormats/GsfTrackReco/interface/GsfTrack.h"
-#include "DataFormats/EgammaReco/interface/ElectronSeedFwd.h"
 #include "DataFormats/EgammaReco/interface/ElectronSeed.h"
 #include "RecoEgamma/EgammaElectronAlgos/interface/GsfElectronTools.h"
 
@@ -30,20 +28,22 @@ namespace egamma {
     TrackRef ctfTrackRef = TrackRef();
     const TrackCollection* ctfTrackCollection = ctfTracksH.product();
 
-    // get the Hit Pattern for the gsfTrack
+    double gsfEta = gsfTrackRef->eta();
+    double gsfPhi = gsfTrackRef->phi();
     const HitPattern& gsfHitPattern = gsfTrackRef->hitPattern();
+
+    constexpr double dR2 = 0.3 * 0.3;
 
     unsigned int counter = 0;
     for (auto ctfTkIter = ctfTrackCollection->begin(); ctfTkIter != ctfTrackCollection->end(); ctfTkIter++, counter++) {
       auto const& trackVariables = ctfTrackVariables[counter];
-      double dEta = gsfTrackRef->eta() - trackVariables.eta;
-      double dPhi = gsfTrackRef->phi() - trackVariables.phi;
-      double pi = acos(-1.);
-      if (std::abs(dPhi) > pi)
-        dPhi = 2 * pi - std::abs(dPhi);
+      double dEta = gsfEta - trackVariables.eta;
+      double dPhi = gsfPhi - trackVariables.phi;
+      if (std::abs(dPhi) > M_PI)
+        dPhi = 2 * M_PI - std::abs(dPhi);
 
       // dont want to look at every single track in the event!
-      if (sqrt(dEta * dEta + dPhi * dPhi) > 0.3)
+      if (dEta * dEta + dPhi * dPhi > dR2)
         continue;
 
       unsigned int shared = 0;
@@ -106,7 +106,7 @@ namespace egamma {
 
     }  //ctfTrack iterator
 
-    return make_pair(ctfTrackRef, maxFracShared);
+    return {ctfTrackRef, maxFracShared};
   }
 
 }  // namespace egamma
