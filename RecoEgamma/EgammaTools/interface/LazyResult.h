35394cf86db6ced3bff9ccd48c8cb32bee02cd74
Jonas Rembser <jonas.rembser@cern.ch>
2020-06-15 11:47:41 +0200

    add more comments to LazyResult

--- RecoEgamma/EgammaTools/interface/LazyResult.h
+++ RecoEgamma/EgammaTools/interface/LazyResult.h
@@ -1,3 +1,44 @@
+#ifndef RecoEgamma_EgammaTools_LazyResult_h
+#define RecoEgamma_EgammaTools_LazyResult_h
+// -*- C++ -*-
+//
+// Package:     RecoEgamma/EgammaTools
+// Class  :     TableTest
+//
+/**\class LazyResult LazyResult.h "RecoEgamma/EgammaTools/interface/LazyResult.h"
+ Description: Wrapper around a function call for lazy execution.
+
+ Usage:
+    // example: lazy addition
+    auto result = LazyResult(std::plus<int>, x, x);
+    std::cout << result.value() << std::endl;
+
+Notes:
+  * The arguments for the delayed call are stored by reference (watch their
+    lifetime).
+  * The overhead in memory compared to just storing the result is small: one
+    reference per argument, one bool flag and a function pointer (on my system:
+    1 byte for lambda function, 8 bytes for global function and 16 bytes for
+    member function due to possible index to virtual table).
+
+Implementation:
+
+  * For the Args... we explicitly add const& (also in the the args_ tuple).
+    Otherwise, the arguments will be stored by value which comes with too much
+    overhead. This implies that the lifetime of the arguments passed to
+    LazyResult neet to live longer than the Lazy instance. Function pointers
+    are small, so no need for const& to the Func.
+  * An alternative to using a ::value() member function to get the result could
+    be a cast operator: operator Result const &(). This might be pretty because
+    the result is automatically evaluated the first time you try to bind it to
+    a Result const &. I think this is however a bit too implicit and dangerous.
+
+*/
+//
+// Original Author:  Jonas Rembser
+//         Created:  Mon, 14 Jun 2020 00:00:00 GMT
+//
+
 #include <tuple>
 #include <type_traits>
 
@@ -6,32 +47,30 @@ class LazyResult {
 public:
   using Result = typename std::invoke_result<Func, Args...>::type;
 
-  // For the Args... we explicitly add const& (also in the the args_ tuple).
-  // Otherwise, the arguments will be stored by value which comes with too
-  // much overhead.  This implies that the lifetime of the arguments passed
-  // to Lazy neet to live longer than the Lazy instance. Function pointers
-  // are small, so no need for const& to the Func.
   LazyResult(Func func, Args const&... args) : func_(func), args_(args...) {}
 
-  // Get the actual result. An alternative to using a ::value() member
-  // function could be a cast operator: operator Result const &().
-  // This might be pretty because the result is automatically evaluated the
-  // first time you try to bind it to a Result const &. I think this is
-  // however a bit too implicit and dangerous.
   Result const& value() {
     if (!evaluated_) {
-      result_ = func_(std::get<Args const&>(args_)...);
-      evaluated_ = true;
+      evaluate();
     }
     return result_;
   }
 
 private:
-  // The evaluated_ and func_ member might both be just one byte (if the
-  // fuction is a lambda function for example), so by having them together we
-  // save alignment bits if this is the case.
+  void evaluate() { evaluateImpl(std::make_index_sequence<sizeof...(Args)>{}); }
+
+  template <std::size_t... ArgIndices>
+  void evaluateImpl(std::index_sequence<ArgIndices...>) {
+    result_ = func_(std::get<ArgIndices>(args_)...);
+    evaluated_ = true;
+  }
+
+  // having evaluated_ and the potentially small func_ together potentially
+  // saves alignemnt bits (e.g. a lambda function is just 1 byte)
   bool evaluated_ = false;
   Func func_;
   std::tuple<Args const&...> args_;
   Result result_;
 };
+
+#endif
