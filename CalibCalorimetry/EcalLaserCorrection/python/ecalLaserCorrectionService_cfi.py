9e34140476cf636f256a024fdf9820dc3e5d5baf
Fabrice Couderc <fabrice.couderc@cern.ch>
2020-06-16 11:49:05 +0200

    put back default deltat to zero

--- CalibCalorimetry/EcalLaserCorrection/python/ecalLaserCorrectionService_cfi.py
+++ CalibCalorimetry/EcalLaserCorrection/python/ecalLaserCorrectionService_cfi.py
@@ -3,7 +3,7 @@ import FWCore.ParameterSet.Config as cms
 EcalLaserCorrectionService = cms.ESProducer("EcalLaserCorrectionService", 
                                         ### delta t is the safety margin before stopping
                                         ### the laser correction extrapolation (this is in seconds)
-                                        maxExtrapolationTimeInSec = cms.uint32( 5  ) )
+                                        maxExtrapolationTimeInSec = cms.uint32( 0 ) )
 
 
 
