46902157a7e9f08498772b716b141e262e3257da
Jason Lee <jason.lee@cern.ch>
2020-06-17 00:52:27 +0900

    using default constructor and assign range by reference

--- EventFilter/GEMRawToDigi/interface/GEMRawToDigi.h
+++ EventFilter/GEMRawToDigi/interface/GEMRawToDigi.h
@@ -1,6 +1,5 @@
 #ifndef EventFilter_GEMRawToDigi_GEMRawToDigi_h
 #define EventFilter_GEMRawToDigi_GEMRawToDigi_h
-
 /** \class GEMRawToDigi
  *  \author J. Lee, Yechan Kang - UoS
  */
@@ -8,12 +7,9 @@
 #include "EventFilter/GEMRawToDigi/interface/AMC13Event.h"
 
 class GEMRawToDigi {
-public:
-  /// Constructor
-  GEMRawToDigi(){};
-
+ public:
   std::unique_ptr<gem::AMC13Event> convertWordToAMC13Event(const uint64_t* word);
 
-private:
+ private:
 };
 #endif
