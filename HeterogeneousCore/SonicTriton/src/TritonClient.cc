ca83125faa8cbbb3145d6e000dd3e0adf126c007
Kevin Pedro <kpedro88@gmail.com>
2020-06-19 17:01:27 -0500

    remove severity mechanism to simplify

--- HeterogeneousCore/SonicTriton/src/TritonClient.cc
+++ HeterogeneousCore/SonicTriton/src/TritonClient.cc
@@ -27,63 +27,51 @@ TritonClient<Client>::TritonClient(const edm::ParameterSet& params)
       nInput_(params.getParameter<unsigned>("nInput")),
       nOutput_(params.getParameter<unsigned>("nOutput")),
       verbose_(params.getParameter<bool>("verbose")),
-      minSeverityExit_(static_cast<Severity>(params.getParameter<int>("minSeverityExit"))),
       allowedTries_(params.getParameter<unsigned>("allowedTries")) {
   this->clientName_ = "TritonClient";
 }
 
 template <typename Client>
-std::exception_ptr TritonClient<Client>::wrap(const nic::Error& err, const std::string& msg, Severity sev) const {
-  std::exception_ptr eptr;
+bool TritonClient<Client>::wrap(const nic::Error& err, const std::string& msg) const {
   if (!err.IsOk()) {
-    if (sev > minSeverityExit_) {
-      cms::Exception ex("TritonServerFailure");
-      ex << msg << ": " << err;
-      eptr = make_exception_ptr(ex);
-    } else {
-      //just emit warning
-      edm::LogWarning("TritonServerWarning") << msg << ": " << err;
-    }
+    //emit warning
+    edm::LogWarning("TritonServerWarning") << msg << ": " << err;
   }
-  return eptr;
+  return err.IsOk();
 }
 
 template <typename Client>
-std::exception_ptr TritonClient<Client>::setup() {
-  std::exception_ptr exptr{};
+bool TritonClient<Client>::setup() {
+  bool status = true;
 
-  exptr = wrap(nic::InferGrpcContext::Create(&context_, url_, modelName_, modelVersion_, false),
-               "setup(): unable to create inference context",
-               Severity::High);
-  if (exptr)
-    return exptr;
+  status = wrap(nic::InferGrpcContext::Create(&context_, url_, modelName_, modelVersion_, false),
+                "setup(): unable to create inference context");
+  if (!status)
+    return status;
 
   //only used for monitoring
   bool has_server = false;
   if (verbose_) {
-    auto server_err = nic::ServerStatusGrpcContext::Create(&serverCtx_, url_, false);
-    wrap(server_err, "setup(): unable to create server context", Severity::None);
-    has_server = server_err.IsOk();
+    bool has_server = wrap(nic::ServerStatusGrpcContext::Create(&serverCtx_, url_, false),
+                           "setup(): unable to create server context");
   }
   if (!has_server)
     serverCtx_ = nullptr;
 
   std::unique_ptr<nic::InferContext::Options> options;
-  exptr = wrap(nic::InferContext::Options::Create(&options),
-               "setup(): unable to create inference context options",
-               Severity::Low);
-  if (exptr)
-    return exptr;
+  status = wrap(nic::InferContext::Options::Create(&options), "setup(): unable to create inference context options");
+  if (!status)
+    return status;
 
   options->SetBatchSize(batchSize_);
   for (const auto& output : context_->Outputs()) {
-    exptr = wrap(options->AddRawResult(output), "setup(): unable to add raw result", Severity::Low);
-    if (exptr)
-      return exptr;
+    status = wrap(options->AddRawResult(output), "setup(): unable to add raw result");
+    if (!status)
+      return status;
   }
-  exptr = wrap(context_->SetRunOptions(*options), "setup(): unable to set run options", Severity::Low);
-  if (exptr)
-    return exptr;
+  status = wrap(context_->SetRunOptions(*options), "setup(): unable to set run options");
+  if (!status)
+    return status;
 
   const auto& nicInputs = context_->Inputs();
   nicInput_ = nicInputs[0];
@@ -93,35 +81,33 @@ std::exception_ptr TritonClient<Client>::setup() {
   std::vector<int64_t> input_shape;
   for (unsigned i0 = 0; i0 < batchSize_; i0++) {
     float* arr = &(this->input_.data()[i0 * nInput_]);
-    exptr = wrap(nicInput_->SetRaw(reinterpret_cast<const uint8_t*>(arr), nInput_ * sizeof(float)),
-                 "setup(): unable to set data for batch entry " + std::to_string(i0),
-                 Severity::High);
-    if (exptr)
-      return exptr;
+    status = wrap(nicInput_->SetRaw(reinterpret_cast<const uint8_t*>(arr), nInput_ * sizeof(float)),
+                  "setup(): unable to set data for batch entry " + std::to_string(i0));
+    if (!status)
+      return status;
   }
   auto t2 = std::chrono::high_resolution_clock::now();
   if (!this->debugName_.empty())
     edm::LogInfo(this->fullDebugName_) << "Input array time: "
                                        << std::chrono::duration_cast<std::chrono::microseconds>(t2 - t1).count();
 
-  //should be empty
-  return exptr;
+  //should be true
+  return status;
 }
 
 template <typename Client>
-std::exception_ptr TritonClient<Client>::getResults(const std::unique_ptr<nic::InferContext::Result>& result) {
-  std::exception_ptr exptr{};
+bool TritonClient<Client>::getResults(const std::unique_ptr<nic::InferContext::Result>& result) {
+  bool status = true;
 
   auto t1 = std::chrono::high_resolution_clock::now();
   this->output_.resize(nOutput_ * batchSize_, 0.f);
   for (unsigned i0 = 0; i0 < batchSize_; i0++) {
     const uint8_t* r0;
     size_t content_byte_size;
-    exptr = wrap(result->GetRaw(i0, &r0, &content_byte_size),
-                 "getResults(): unable to get raw for entry " + std::to_string(i0),
-                 Severity::High);
-    if (exptr)
-      return exptr;
+    status = wrap(result->GetRaw(i0, &r0, &content_byte_size),
+                  "getResults(): unable to get raw for entry " + std::to_string(i0));
+    if (!status)
+      return status;
     const float* lVal = reinterpret_cast<const float*>(r0);
     std::memcpy(&this->output_[i0 * nOutput_], lVal, nOutput_ * sizeof(float));
   }
@@ -130,8 +116,8 @@ std::exception_ptr TritonClient<Client>::getResults(const std::unique_ptr<nic::I
     edm::LogInfo(this->fullDebugName_) << "Output array time: "
                                        << std::chrono::duration_cast<std::chrono::microseconds>(t2 - t1).count();
 
-  //should be empty
-  return exptr;
+  //should be true
+  return status;
 }
 
 //default case for sync and pseudo async
@@ -144,9 +130,9 @@ void TritonClient<Client>::evaluate() {
   }
 
   //common operations first
-  auto exptr = setup();
-  if (exptr) {
-    this->finish(false, exptr);
+  bool status = setup();
+  if (!status) {
+    this->finish(false);
     return;
   }
 
@@ -156,10 +142,10 @@ void TritonClient<Client>::evaluate() {
   //blocking call
   auto t1 = std::chrono::high_resolution_clock::now();
   std::map<std::string, std::unique_ptr<nic::InferContext::Result>> results;
-  exptr = wrap(context_->Run(&results), "evaluate(): unable to run and/or get result", Severity::High);
-  if (exptr) {
+  status = wrap(context_->Run(&results), "evaluate(): unable to run and/or get result");
+  if (!status) {
     this->output_.resize(nOutput_ * batchSize_, 0.f);
-    this->finish(false, exptr);
+    this->finish(false);
     return;
   }
 
@@ -175,9 +161,9 @@ void TritonClient<Client>::evaluate() {
     this->reportServerSideStats(stats);
   }
 
-  exptr = getResults(results.begin()->second);
+  status = getResults(results.begin()->second);
 
-  this->finish(exptr == nullptr, exptr);
+  this->finish(status);
 }
 
 //specialization for true async
@@ -190,9 +176,9 @@ void TritonClientAsync::evaluate() {
   }
 
   //common operations first
-  auto exptr = setup();
-  if (exptr) {
-    this->finish(false, exptr);
+  bool status = setup();
+  if (!status) {
+    this->finish(false);
     return;
   }
 
@@ -201,42 +187,42 @@ void TritonClientAsync::evaluate() {
 
   //non-blocking call
   auto t1 = std::chrono::high_resolution_clock::now();
-  exptr = wrap(context_->AsyncRun([t1, start_status, this](nic::InferContext* ctx,
-                                                           const std::shared_ptr<nic::InferContext::Request>& request) {
-    //get results
-    std::map<std::string, std::unique_ptr<nic::InferContext::Result>> results;
-    auto exptr =
-        this->wrap(ctx->GetAsyncRunResults(request, &results), "evaluate(): unable to get result", Severity::High);
-    if (exptr) {
-      this->output_.resize(nOutput_ * batchSize_, 0.f);
-      this->finish(false, exptr);
-      return;
-    }
-    auto t2 = std::chrono::high_resolution_clock::now();
+  status =
+      wrap(context_->AsyncRun([t1, start_status, this](nic::InferContext* ctx,
+                                                       const std::shared_ptr<nic::InferContext::Request>& request) {
+        //get results
+        bool status = true;
+        std::map<std::string, std::unique_ptr<nic::InferContext::Result>> results;
+        status = this->wrap(ctx->GetAsyncRunResults(request, &results), "evaluate(): unable to get result");
+        if (!status) {
+          this->output_.resize(nOutput_ * batchSize_, 0.f);
+          this->finish(false);
+          return;
+        }
+        auto t2 = std::chrono::high_resolution_clock::now();
 
-    if (!this->debugName_.empty())
-      edm::LogInfo(this->fullDebugName_) << "Remote time: "
-                                         << std::chrono::duration_cast<std::chrono::microseconds>(t2 - t1).count();
+        if (!this->debugName_.empty())
+          edm::LogInfo(this->fullDebugName_)
+              << "Remote time: " << std::chrono::duration_cast<std::chrono::microseconds>(t2 - t1).count();
 
-    const auto& end_status = this->getServerSideStatus();
+        const auto& end_status = this->getServerSideStatus();
 
-    if (this->verbose()) {
-      const auto& stats = this->summarizeServerStats(start_status, end_status);
-      this->reportServerSideStats(stats);
-    }
+        if (this->verbose()) {
+          const auto& stats = this->summarizeServerStats(start_status, end_status);
+          this->reportServerSideStats(stats);
+        }
 
-    //check result
-    exptr = this->getResults(results.begin()->second);
+        //check result
+        status = this->getResults(results.begin()->second);
 
-    //finish
-    this->finish(exptr == nullptr, exptr);
-  }),
-               "evaluate(): unable to launch async run",
-               Severity::High);
+        //finish
+        this->finish(status);
+      }),
+           "evaluate(): unable to launch async run");
 
   //if AsyncRun failed, finish() wasn't called
-  if (exptr)
-    this->finish(false, exptr);
+  if (!status)
+    this->finish(false);
 }
 
 template <typename Client>
