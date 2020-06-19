2e866f958b5d5e661e73d99ed61f230477108715
Lauren Hay <lmhay@buffalo.edu>
2020-06-19 20:30:47 +0200

    Fixing included header files.

--- CommonTools/UtilAlgos/interface/UniqueMerger.h
+++ CommonTools/UtilAlgos/interface/UniqueMerger.h
@@ -20,14 +20,13 @@
  * 
  */
 
-#include <typeinfo>
-#include <iostream>
 #include "FWCore/Framework/interface/global/EDProducer.h"
 #include "FWCore/Framework/interface/Event.h"
 #include "FWCore/ParameterSet/interface/ParameterSet.h"
 #include "FWCore/Utilities/interface/transform.h"
 #include "FWCore/Utilities/interface/InputTag.h"
 #include "DataFormats/Common/interface/CloneTrait.h"
+#include "FWCore/MessageLogger/interface/MessageLogger.h"
 #include <vector>
 
 template <typename InputCollection,
