2b5ab253e755d8a4e44dc17446a9e52ce4c7fed9
wouf <wouf@users.noreply.github.com>
2020-06-19 13:58:16 +0300

    Update GeneratorEmbMix_cff.py

--- Configuration/StandardSequences/python/GeneratorEmbMix_cff.py
+++ Configuration/StandardSequences/python/GeneratorEmbMix_cff.py
@@ -1,14 +1,4 @@
 import FWCore.ParameterSet.Config as cms
+from Configuration.StandardSequences.GeneratorMix_cff import *
 
-# Produce GenParticles of the two HepMCProducts
-from Configuration.StandardSequences.Generator_cff import *
-from GeneratorInterface.Core.generatorSmeared_cfi import *
-genParticles.mix = cms.string("mix")
-genParticles.doSubEvent = cms.untracked.bool(True)
-genParticles.useCrossingFrame = cms.untracked.bool(True)
-genParticles.saveBarCodes = cms.untracked.bool(True)
-genParticles.abortOnUnknownPDGCode = cms.untracked.bool(False)
-
-GenSmeared = cms.Sequence(generatorSmeared)
-hiGenJets = cms.Sequence(genParticlesForJets*hiRecoGenJets)
 pgen = cms.Sequence(cms.SequencePlaceholder("mix")+cms.SequencePlaceholder("generator")+cms.SequencePlaceholder("randomEngineStateProducer")+VertexSmearing+GenSmeared+genParticles+hiGenJets)
