7af821c9afe9537ac8afa5e05e432404c98416f5
Gabrielle Hugo <gabrielle.hugo83@gmail.com>
2020-06-22 15:04:28 +0200

    Port COCOA to DD4hep + Regenerate COCOA input files + Add documentation + 3 bug fixes in legacy COCOA, significantly affecting optical alignment results (Inverse rotation in IdealGeometry from XML + Bug in extra alignment parameters + Conversion error in results written to DB).

--- Alignment/CocoaApplication/interface/CocoaAnalyzer.h
+++ Alignment/CocoaApplication/interface/CocoaAnalyzer.h
@@ -1,78 +1,43 @@
-#ifndef CocoaAnalyser_HH
-#define CocoaAnalyser_HH
-//-*- C++ -*-
-//
-// Package:    Alignment/CocoaApplication
-// Class:      CocoaAnalyzer
-// 
-/*
+#ifndef ALIGNMENT_COCOA_APPLICATION_COCOA_ANALYZER_H
+#define ALIGNMENT_COCOA_APPLICATION_COCOA_ANALYZER_H
 
- Description: test access to the OpticalAlignMeasurements via OpticalAlignMeasurementsGeneratedSource
-    This also should demonstrate access to a geometry via the XMLIdealGeometryESSource
-    for use in THE COCOA analyzer.
-
- Implementation:
-     Iterate over retrieved alignments.
-*/
-//
-
-#include <stdexcept>
-#include <string>
-#include <iostream>
-#include <map>
 #include "FWCore/Framework/interface/one/EDAnalyzer.h"
-#include "DetectorDescription/Core/interface/DDPosData.h" 
-
-class Event;
-class EventSetup;
-class Entry;
-//#include "FWCore/Framework/interface/EventSetup.h"
-
+#include "DetectorDescription/DDCMS/interface/DDSpecParRegistry.h"
 #include "CondFormats/OptAlignObjects/interface/OpticalAlignments.h"
-#include "CondFormats/OptAlignObjects/interface/OpticalAlignInfo.h"
 #include "CondFormats/OptAlignObjects/interface/OpticalAlignMeasurements.h"
 
-class DDFilteredView;
-class DDCompactView;
-class DDSpecifics;
-class OpticalObject;
+class CocoaAnalyzer : public edm::one::EDAnalyzer<edm::one::SharedResources> {
+public:
+  explicit CocoaAnalyzer(edm::ParameterSet const& p);
+  explicit CocoaAnalyzer(int i) {}
+  ~CocoaAnalyzer() override {}
 
+  void beginJob() override;
+  void analyze(const edm::Event& e, const edm::EventSetup& c) override;
 
-class CocoaAnalyzer : public edm::one::EDAnalyzer<edm::one::SharedResources>
-{
- public:
-  
-  explicit  CocoaAnalyzer(edm::ParameterSet const& p);
-  explicit  CocoaAnalyzer(int i) { }
-  virtual ~ CocoaAnalyzer() { }
-  
-  virtual void beginJob() override;
-  virtual void analyze(const edm::Event& e, const edm::EventSetup& c) override;
-  // see note on endJob() at the bottom of the file.
-  // virtual void endJob() ;
+private:
+  void readXMLFile(const edm::EventSetup& evts);
 
- private:
-  void ReadXMLFile( const edm::EventSetup& evts );
-  std::vector<OpticalAlignInfo> ReadCalibrationDB( const edm::EventSetup& evts );
+  std::vector<OpticalAlignInfo> readCalibrationDB(const edm::EventSetup& evts);
+  void correctAllOpticalAlignments(std::vector<OpticalAlignInfo>& allDBOpticalAlignments);
+  void correctOpticalAlignmentParameter(OpticalAlignParam& myXMLParam, const OpticalAlignParam& myDBParam);
 
-  void CorrectOptAlignments( std::vector<OpticalAlignInfo>& oaListCalib );
-  OpticalAlignInfo* FindOpticalAlignInfoXML( const OpticalAlignInfo& oaInfo );
-  bool CorrectOaParam( OpticalAlignParam* oaParamXML, const OpticalAlignParam& oaParamDB );
+  void runCocoa();
 
-  void RunCocoa();
+  template <typename T>
+  std::vector<T> getAllParameterValuesFromSpecParSections(const cms::DDSpecParRegistry& allSpecParSections,
+                                                          const std::string& nodePath,
+                                                          const std::string& parameterName);
+  template <typename T>
+  T getParameterValueFromSpecParSections(const cms::DDSpecParRegistry& allSpecParSections,
+                                         const std::string& nodePath,
+                                         const std::string& parameterName,
+                                         const unsigned int parameterValueIndex);
 
-  OpticalAlignInfo GetOptAlignInfoFromOptO( OpticalObject* opto );
-  double myFetchDbl(const DDsvalues_type& dvst, 
-	        		      const std::string& spName,
-				    const size_t& vecInd );
-  std::string myFetchString(const DDsvalues_type& dvst, 
-				      const std::string& spName,
-				    const size_t& vecInd );
-
- private:
+private:
   OpticalAlignments oaList_;
   OpticalAlignMeasurements measList_;
-  std::string theCocoaDaqRootFileName;
+  std::string theCocoaDaqRootFileName_;
 };
 
 #endif
