23bc366a7d080088dd71d3de9702e3f38993d34e
Maria Giulia Ratti <maria.giulia.ratti@cern.ch>
2020-06-24 11:43:17 +0200

    commented all couts

--- GeneratorInterface/Pythia8Interface/plugins/Pythia8Hadronizer.cc
+++ GeneratorInterface/Pythia8Interface/plugins/Pythia8Hadronizer.cc
@@ -487,11 +487,12 @@ bool Pythia8Hadronizer::initializeForInternalPartons()
   // init decayer
   fDecayer->settings.flag("ProcessLevel:all", false ); // trick
   fDecayer->settings.flag("ProcessLevel:resonanceDecays", true );
+  //MG = couts and other tests when decay of HNL was performed by Pythia
   //fDecayer->readString("9900015:m0 = 2.0");
-  //fDecayer->readString("9900015:tau0 = 5");
-  std::cout << "BEFORE LISTING of HNL PROPERTIES" << std::endl;
-  fDecayer->particleData.list(9900015);
-  std::cout << "AFTER LISTING of HNL PROPERTIES" << std::endl;
+  //fDecayer->readString("9900015:tau0 = 1");
+  //std::cout << "BEFORE LISTING of HNL PROPERTIES" << std::endl;
+  //fDecayer->particleData.list(9900015);
+  //std::cout << "AFTER LISTING of HNL PROPERTIES" << std::endl;
   //antiName spinType chargeType colType m0 mWidth mMin mMax tau0");
   //fDecayer->settings.flag("HNLDecays:mode = 3"); // copied from Sonia
   //fDecayer->settings.flag("HNLDecays:HNLPolarization = 1."); // copied from Sonia
@@ -876,7 +877,7 @@ bool Pythia8Hadronizer::residualDecay()
   bool result = true;
 
   //MG
-  std::cout   << setw(8)<<  "PdgId" 
+  /*std::cout   << setw(8)<<  "PdgId" 
               << setw(8) << "Status" 
               << setw(8) << "GenMass" 
               << setw(8) << "Name" 
@@ -890,7 +891,7 @@ bool Pythia8Hadronizer::residualDecay()
               << setw(8) << "mayDec" 
               << setw(8) << "canDec" 
               << setw(8) << "doExt" 
-              << setw(8) << "isVis"  << std::endl;
+              << setw(8) << "isVis"  << std::endl;*/
 
   for ( int ipart=NPartsAfterDecays; ipart>NPartsBeforeDecays; ipart-- )
   {
@@ -898,7 +899,7 @@ bool Pythia8Hadronizer::residualDecay()
     // Take the paricle from the HepMC gen event
     HepMC::GenParticle* part = event().get()->barcode_to_particle( ipart );
     // MG
-    std::cout << setw(8) << part->pdg_id()
+    /*std::cout << setw(8) << part->pdg_id()
               << setw(8) << part->status() 
               << setw(8) << part->generated_mass() 
               << setw(8) << (fDecayer->particleData).name(part->pdg_id()) 
@@ -914,7 +915,7 @@ bool Pythia8Hadronizer::residualDecay()
               << setw(8) << (fDecayer->particleData).doExternalDecay(part->pdg_id())
               << setw(8) << (fDecayer->particleData).isVisible(part->pdg_id())
               << std::endl;
-
+    */
 
     if ( part->status() == 1 && (fDecayer->particleData).canDecay(part->pdg_id()) )
     {
@@ -929,10 +930,10 @@ bool Pythia8Hadronizer::residualDecay()
       HepMC::GenVertex* ProdVtx = part->production_vertex();
       py8part.vProd( ProdVtx->position().x(), ProdVtx->position().y(),
                      ProdVtx->position().z(), ProdVtx->position().t() );
-      py8part.tau( (fDecayer->particleData).tau0( part->pdg_id() ) ); // despite the name tau, this is actually the tau0
+      //py8part.tau( (fDecayer->particleData).tau0( part->pdg_id() ) ); // this is the proper lifetime -> this is wrong, because it will be set to a fixed value...!
       fDecayer->event.append( py8part ); // append the particle to the current event, should be the only one
       int nentries = fDecayer->event.size();
-      std::cout << "check nPart before decay=" << nentries << std::endl;
+/*      std::cout << "check nPart before decay=" << nentries << std::endl;
       std::cout << "p8 particle properties" << std::endl;
       std::cout << "prodV x=" << py8part.xProd() << " decV x=" << py8part.xDec() << std::endl;
       std::cout << "prodV y=" << py8part.yProd() << " decV x=" << py8part.yDec() << std::endl;
@@ -946,14 +947,15 @@ bool Pythia8Hadronizer::residualDecay()
       std::cout << "3-mom=" << std::sqrt(py8part.px()*py8part.px() + py8part.py()*py8part.py() + py8part.pz()*py8part.pz()) << std::endl;
       std::cout << "tau0=" << py8part.tau0() << std::endl;
       std::cout << "tau=" <<  py8part.tau() << std::endl;
+*/      
       if ( !fDecayer->event[nentries-1].mayDecay() ) continue;
       fDecayer->next(); // generate, i.e. only decay the only particle existing
       int nentries1 = fDecayer->event.size();
-      std::cout << "check nPart after decay=" << nentries1 << std::endl;
+ //     std::cout << "check nPart after decay=" << nentries1 << std::endl;
       if ( nentries1 <= nentries ) continue; //same number of particles, no decays...
 
       part->set_status(2); // this is the HNL that ends in the final record
-      std::cout << "PARTICLE DECAYED" << std::endl;
+ //     std::cout << "PARTICLE DECAYED" << std::endl;
       result = toHepMC.fill_next_event( *(fDecayer.get()), event().get(), -1, true, part);
 
     }
