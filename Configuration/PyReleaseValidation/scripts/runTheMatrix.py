c21694c9330bbb50b1b9aa32465e62925977b9de
Slava Krutelyov <slava77@cern.ch>
2020-06-24 02:54:14 +0200

    keep reminis grouped

--- Configuration/PyReleaseValidation/scripts/runTheMatrix.py
+++ Configuration/PyReleaseValidation/scripts/runTheMatrix.py
@@ -61,9 +61,9 @@ if __name__ == '__main__':
                      136.731, #2016B Photon data
                      136.7611, #2016E JetHT reMINIAOD from 80X legacy
                      136.8311, #2017F JetHT reMINIAOD from 94X reprocessing
+                     136.88811,#2018D JetHT reMINIAOD from UL processing
                      136.788, #2017B Photon data
                      136.85, #2018A Egamma data
-                     136.88811,#2018D JetHT reMINIAOD from UL processing
                      140.53, #2011 HI data
                      140.56, #2018 HI data
                      158.0, #2018 HI MC with pp-like reco
