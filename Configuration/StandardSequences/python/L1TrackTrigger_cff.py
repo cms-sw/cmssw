6b9ccb57dee572818384626c2db944f300b1fd15
Vladimir <vladimir.rekovic@cern.ch>
2020-06-25 09:24:22 +0200

    Turn track trigger on.

--- Configuration/StandardSequences/python/L1TrackTrigger_cff.py
+++ Configuration/StandardSequences/python/L1TrackTrigger_cff.py
@@ -4,7 +4,7 @@ from L1Trigger.TrackTrigger.TrackTrigger_cff import *
 from SimTracker.TrackTriggerAssociation.TrackTriggerAssociator_cff import *
 from L1Trigger.TrackerDTC.ProducerED_cff import *
 
-#L1TrackTrigger=cms.Sequence(TrackTriggerClustersStubs*TrackTriggerAssociatorClustersStubs*TrackTriggerTTTracks*TrackTriggerAssociatorTracks)
-L1TrackTrigger=cms.Sequence(TrackTriggerClustersStubs*TrackTriggerAssociatorClustersStubs*TrackerDTCProducer)
+L1TrackTrigger=cms.Sequence(TrackTriggerClustersStubs*TrackTriggerAssociatorClustersStubs*TrackerDTCProducer*TrackTriggerTTTracks*TrackTriggerAssociatorTracks)
+#L1TrackTrigger=cms.Sequence(TrackTriggerClustersStubs*TrackTriggerAssociatorClustersStubs*TrackerDTCProducer)
 
 TTStubAlgorithm_official_Phase2TrackerDigi_.zMatchingPS = cms.bool(True)
