1df78d32d5d2c19729d732d4973114a0733667fd
Sunanda <sunanda.banerjee@cern.ch>
2020-06-25 21:33:57 +0200

    Code check

--- Geometry/HGCalGeometry/test/HGCalWaferInFileCheck.cc
+++ Geometry/HGCalGeometry/test/HGCalWaferInFileCheck.cc
@@ -74,8 +74,8 @@ void HGCalWaferInFileCheck::analyze(const edm::Event& iEvent, const edm::EventSe
   const HGCalGeometry* geom = &geomR;
   const auto& hgdc = geom->topology().dddConstants();
 
-  std::cout << nameDetector_ << "\nCheck Wafers in file are all valid for "<< nameDetector_ << std::endl  << std::endl;
-  if ((hgdc.geomMode() == HGCalGeometryMode::Hexagon8) || (hgdc.geomMode() == HGCalGeometryMode::Hexagon8Full)) { 
+  std::cout << nameDetector_ << "\nCheck Wafers in file are all valid for " << nameDetector_ << std::endl << std::endl;
+  if ((hgdc.geomMode() == HGCalGeometryMode::Hexagon8) || (hgdc.geomMode() == HGCalGeometryMode::Hexagon8Full)) {
     DetId::Detector det = (nameSense_ == "HGCalHESiliconSensitive") ? DetId::HGCalHSi : DetId::HGCalEE;
     static std::vector<std::string> types = {"F", "b", "g", "gm", "a", "d", "dm", "c", "X"};
     // See if all entries in the file are valid
@@ -88,14 +88,18 @@ void HGCalWaferInFileCheck::analyze(const edm::Event& iEvent, const edm::EventSe
       int type = std::get<0>(hgdc.waferFileInfo(k));
       HGCSiliconDetId id(det, 1, type, layer, waferU, waferV, 0, 0);
       if (!geom->topology().validModule(id, 3)) {
-	int part = std::get<1>(hgdc.waferFileInfoFromIndex(indx));
-	std::string typex = (part < static_cast<int>(types.size())) ? types[part] : "X";
-	const auto& xy = hgdc.waferPosition(layer, waferU, waferV, true, false);
-	std::cout << "ID[" << k << "]: (" << (hgdc.getLayerOffset() + layer) << ", " << waferU << ", " << waferV << ", " << typex << ") at (" << std::setprecision(4) << xy.first << ", " << xy.second << ", " << hgdc.waferZ(layer, true) << ") not valid" << std::endl;
-	++bad1;
+        int part = std::get<1>(hgdc.waferFileInfoFromIndex(indx));
+        std::string typex = (part < static_cast<int>(types.size())) ? types[part] : "X";
+        const auto& xy = hgdc.waferPosition(layer, waferU, waferV, true, false);
+        std::cout << "ID[" << k << "]: (" << (hgdc.getLayerOffset() + layer) << ", " << waferU << ", " << waferV << ", "
+                  << typex << ") at (" << std::setprecision(4) << xy.first << ", " << xy.second << ", "
+                  << hgdc.waferZ(layer, true) << ") not valid" << std::endl;
+        ++bad1;
       }
     }
-    std::cout << "\n\nFinds " << bad1 << " invalid wafers among " << hgdc.waferFileSize() << " wafers in the list" << std::endl << std::endl;
+    std::cout << "\n\nFinds " << bad1 << " invalid wafers among " << hgdc.waferFileSize() << " wafers in the list"
+              << std::endl
+              << std::endl;
 
     // See if some of the valid wafers are missing
     auto const& ids = geom->getValidGeomDetIds();
@@ -103,22 +107,26 @@ void HGCalWaferInFileCheck::analyze(const edm::Event& iEvent, const edm::EventSe
     for (unsigned int k = 0; k < ids.size(); ++k) {
       HGCSiliconDetId id(ids[k]);
       if (id.zside() == 1) {
-	++all;
-	int indx = HGCalWaferIndex::waferIndex(id.layer(), id.waferU(), id.waferV());
-	if (!hgdc.waferFileInfoExist(indx)) {
-	  int part = hgdc.waferTypeRotation(id.layer(), id.waferU(), id.waferV(), false, false).first;
-	  if (part != HGCalTypes::WaferOut) {
-	    std::string typex = (part < static_cast<int>(types.size())) ? types[part] : "X";
-	    const auto& xy = hgdc.waferPosition(id.layer(), id.waferU(), id.waferV(), true, false);
-	    std::cout << "ID[" << k << "]: (" << (hgdc.getLayerOffset() + id.layer()) << ", " << id.waferU() << ", " << id.waferV() << ", " << typex << ")  at (" << std::setprecision(4) << xy.first << ", " << xy.second << ", " << hgdc.waferZ(id.layer(), true) << ") not in wafer-list" << std::endl;
-	    ++bad2;
-	  } else {
-	    ++xtra;
-	  }
-	}
+        ++all;
+        int indx = HGCalWaferIndex::waferIndex(id.layer(), id.waferU(), id.waferV());
+        if (!hgdc.waferFileInfoExist(indx)) {
+          int part = hgdc.waferTypeRotation(id.layer(), id.waferU(), id.waferV(), false, false).first;
+          if (part != HGCalTypes::WaferOut) {
+            std::string typex = (part < static_cast<int>(types.size())) ? types[part] : "X";
+            const auto& xy = hgdc.waferPosition(id.layer(), id.waferU(), id.waferV(), true, false);
+            std::cout << "ID[" << k << "]: (" << (hgdc.getLayerOffset() + id.layer()) << ", " << id.waferU() << ", "
+                      << id.waferV() << ", " << typex << ")  at (" << std::setprecision(4) << xy.first << ", "
+                      << xy.second << ", " << hgdc.waferZ(id.layer(), true) << ") not in wafer-list" << std::endl;
+            ++bad2;
+          } else {
+            ++xtra;
+          }
+        }
       }
     }
-    std::cout << "\n\nFinds " << bad2 << " missing wafers among " << all << " valid wafers and " << xtra << " extra ones" << std::endl << std::endl;
+    std::cout << "\n\nFinds " << bad2 << " missing wafers among " << all << " valid wafers and " << xtra
+              << " extra ones" << std::endl
+              << std::endl;
 
     // Now cross check the content
     int allG(0), badT(0), badP(0), badR(0), badG(0), badT1(0), badT2(0);
@@ -133,29 +141,39 @@ void HGCalWaferInFileCheck::analyze(const edm::Event& iEvent, const edm::EventSe
       int type2 = hgdc.waferType(layer, waferU, waferV, false);
       HGCSiliconDetId id(det, 1, type2, layer, waferU, waferV, 0, 0);
       if (geom->topology().validModule(id, 3)) {
-	++allG;
-	int part2 = hgdc.waferTypeRotation(id.layer(), id.waferU(), id.waferV(), false, false).first;
-	int rotn2 = hgdc.waferTypeRotation(id.layer(), id.waferU(), id.waferV(), false, false).second;
-	bool typeOK = (type1 == type2);
-	bool partOK = ((part1 == part2) || ((part1 == HGCalTypes::WaferFull) && (part2 == HGCalTypes::WaferOut)));
-	bool rotnOK = ((rotn1 == rotn2) || (part1 == HGCalTypes::WaferFull) || (part2 == HGCalTypes::WaferFull));
-	if (!typeOK) {
-	  ++badT;
-	  if (type1 == 0) ++badT1;
-	  else if (type2 == 0) ++badT2;
-	}
-	if (!partOK) ++badP;
-	if (!rotnOK) ++badR;
-	if ((!typeOK) || (!partOK) || (!rotnOK)) {
-	  ++badG;
-	  std::string partx1 = (part1 < static_cast<int>(types.size())) ? types[part1] : "X";
-	  std::string partx2 = (part2 < static_cast<int>(types.size())) ? types[part2] : "X";
-	  const auto& xy = hgdc.waferPosition(layer, waferU, waferV, true, false);
-	  std::cout << "ID[" << k << "]: (" << (hgdc.getLayerOffset() + layer) << ", " << waferU << ", " << waferV << ", " << type1 << ":" << type2 << ", " << partx1 << ":" << partx2 << ", " << rotn1 << ":" << rotn2 << ") at (" << std::setprecision(4) << xy.first << ", " << xy.second << ", " << hgdc.waferZ(layer, true) << ") failure flag " << typeOK << ":" << partOK << ":" << rotnOK << std::endl;
-	}
+        ++allG;
+        int part2 = hgdc.waferTypeRotation(id.layer(), id.waferU(), id.waferV(), false, false).first;
+        int rotn2 = hgdc.waferTypeRotation(id.layer(), id.waferU(), id.waferV(), false, false).second;
+        bool typeOK = (type1 == type2);
+        bool partOK = ((part1 == part2) || ((part1 == HGCalTypes::WaferFull) && (part2 == HGCalTypes::WaferOut)));
+        bool rotnOK = ((rotn1 == rotn2) || (part1 == HGCalTypes::WaferFull) || (part2 == HGCalTypes::WaferFull));
+        if (!typeOK) {
+          ++badT;
+          if (type1 == 0)
+            ++badT1;
+          else if (type2 == 0)
+            ++badT2;
+        }
+        if (!partOK)
+          ++badP;
+        if (!rotnOK)
+          ++badR;
+        if ((!typeOK) || (!partOK) || (!rotnOK)) {
+          ++badG;
+          std::string partx1 = (part1 < static_cast<int>(types.size())) ? types[part1] : "X";
+          std::string partx2 = (part2 < static_cast<int>(types.size())) ? types[part2] : "X";
+          const auto& xy = hgdc.waferPosition(layer, waferU, waferV, true, false);
+          std::cout << "ID[" << k << "]: (" << (hgdc.getLayerOffset() + layer) << ", " << waferU << ", " << waferV
+                    << ", " << type1 << ":" << type2 << ", " << partx1 << ":" << partx2 << ", " << rotn1 << ":" << rotn2
+                    << ") at (" << std::setprecision(4) << xy.first << ", " << xy.second << ", "
+                    << hgdc.waferZ(layer, true) << ") failure flag " << typeOK << ":" << partOK << ":" << rotnOK
+                    << std::endl;
+        }
       }
     }
-    std::cout << "\n\nFinds " << badG << " (" << badT << "[" << badT1 << ":" << badT2 << "]:" << badP << ":" << badR << ") mismatch among " << allG << " wafers with the same indices" << std::endl << std::endl;
+    std::cout << "\n\nFinds " << badG << " (" << badT << "[" << badT1 << ":" << badT2 << "]:" << badP << ":" << badR
+              << ") mismatch among " << allG << " wafers with the same indices" << std::endl
+              << std::endl;
   }
 }
 
