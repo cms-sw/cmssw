2e633c1d7abd0ff55ba7387594d2fb0ef434e8b2
Vladimir <vladimir.rekovic@cern.ch>
2020-06-25 11:24:58 +0200

    Use PromptExtendedHybridTracks and Associator.

--- Configuration/StandardSequences/python/L1TrackTrigger_cff.py
+++ Configuration/StandardSequences/python/L1TrackTrigger_cff.py
@@ -3,8 +3,10 @@ import FWCore.ParameterSet.Config as cms
 from L1Trigger.TrackTrigger.TrackTrigger_cff import *
 from SimTracker.TrackTriggerAssociation.TrackTriggerAssociator_cff import *
 from L1Trigger.TrackerDTC.ProducerED_cff import *
+from L1Trigger.TrackFindingTracklet.L1HybridEmulationTracks_cff import *
 
-L1TrackTrigger=cms.Sequence(TrackTriggerClustersStubs*TrackTriggerAssociatorClustersStubs*TrackerDTCProducer*TrackTriggerTTTracks*TrackTriggerAssociatorTracks)
-#L1TrackTrigger=cms.Sequence(TrackTriggerClustersStubs*TrackTriggerAssociatorClustersStubs*TrackerDTCProducer)
+L1TrackTrigger=cms.Sequence(TrackTriggerClustersStubs*TrackTriggerAssociatorClustersStubs*TrackerDTCProducer*L1PromptExtendedHybridTracksWithAssociators)
+#L1TrackTrigger=cms.Sequence(TrackTriggerClustersStubs*TrackTriggerAssociatorClustersStubs*TrackerDTCProducer*L1PromptExtendedHybridTracks)
+
 
 TTStubAlgorithm_official_Phase2TrackerDigi_.zMatchingPS = cms.bool(True)
