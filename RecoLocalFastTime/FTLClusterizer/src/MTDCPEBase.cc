b1b10f9d048d7ee6d3da721436551d1c924a92d7
Ang Li <al8fm@virginia.edu>
2020-06-25 15:00:18 -0500

    reversed MTDCPEBase

--- RecoLocalFastTime/FTLClusterizer/src/MTDCPEBase.cc
+++ RecoLocalFastTime/FTLClusterizer/src/MTDCPEBase.cc
@@ -1,10 +1,8 @@
 #include "Geometry/MTDGeometryBuilder/interface/MTDGeomDetUnit.h"
 #include "Geometry/MTDGeometryBuilder/interface/RectangularMTDTopology.h"
 #include "Geometry/MTDGeometryBuilder/interface/ProxyMTDTopology.h"
-#include "Geometry/CommonDetUnit/interface/GeomDetEnumerators.h"
 
 #include "RecoLocalFastTime/FTLClusterizer/interface/MTDCPEBase.h"
-#include "RecoLocalFastTime/FTLClusterizer/interface/BTLRecHitsErrorEstimatorIM.h"
 
 // MessageLogger
 #include "FWCore/MessageLogger/interface/MessageLogger.h"
@@ -78,14 +76,7 @@ LocalError MTDCPEBase::localError(DetParam const& dp, ClusterParam& cp) const {
   constexpr double one_over_twelve = 1. / 12.;
   MeasurementPoint pos(cp.theCluster->x(), cp.theCluster->y());
   MeasurementError simpleRect(one_over_twelve, 0, one_over_twelve);
-  if (GeomDetEnumerators::isBarrel(dp.thePart)) {
-    LocalPoint lp = localPosition(dp, cp);
-    BTLRecHitsErrorEstimatorIM btlHitErr(dp.theDet, lp);
-    LocalError err_improved = btlHitErr.localError();
-    return err_improved;
-  } else {
-    return dp.theTopol->localError(pos, simpleRect);
-  }
+  return dp.theTopol->localError(pos, simpleRect);
 }
 
 MTDCPEBase::TimeValue MTDCPEBase::clusterTime(DetParam const& dp, ClusterParam& cp) const {
