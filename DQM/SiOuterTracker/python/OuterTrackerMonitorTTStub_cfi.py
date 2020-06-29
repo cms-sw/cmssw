4acd3cadb1dd70e30db01dc55d37c43d3b7eee40
Emily <macdonald.e17@gmail.com>
2020-06-29 16:48:06 -0500

    Updated so code actually worked with master branch: removed interface, and added class to each plugin

--- DQM/SiOuterTracker/python/OuterTrackerMonitorTTStub_cfi.py
+++ DQM/SiOuterTracker/python/OuterTrackerMonitorTTStub_cfi.py
@@ -1,5 +1,4 @@
 import FWCore.ParameterSet.Config as cms
-# import math
 
 from DQMServices.Core.DQMEDAnalyzer import DQMEDAnalyzer
 OuterTrackerMonitorTTStub = DQMEDAnalyzer('OuterTrackerMonitorTTStub',
