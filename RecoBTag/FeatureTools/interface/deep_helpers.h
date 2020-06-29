32ee277699263cef804906afeeba7ac323073d3c
Javier Duarte <jduarte@ucsd.edu>
2020-06-29 12:55:26 -0700

    factorize PreprocessParams

--- RecoBTag/FeatureTools/interface/deep_helpers.h
+++ RecoBTag/FeatureTools/interface/deep_helpers.h
@@ -74,5 +74,38 @@ namespace btagbtvdeep {
   float vtx_ass_from_pfcand(const reco::PFCandidate &pfcand, int pv_ass_quality, const reco::VertexRef &pv);
   float quality_from_pfcand(const reco::PFCandidate &pfcand);
   float lost_inner_hits_from_pfcand(const reco::PFCandidate &pfcand);
+
+  // struct to hold preprocessing parameters
+  struct PreprocessParams {
+    struct VarInfo {
+      VarInfo() {}
+      VarInfo(float median, float norm_factor, float replace_value, float lower_bound, float upper_bound)
+          : center(median),
+            norm_factor(norm_factor),
+            replace_value(replace_value),
+            lower_bound(lower_bound),
+            upper_bound(upper_bound) {}
+      float center = 0;
+      float norm_factor = 1;
+      float replace_value = 0;
+      float lower_bound = -5;
+      float upper_bound = 5;
+    };
+
+    unsigned var_length = 0;
+    float pad = 0;
+    std::vector<std::string> var_names;
+    std::unordered_map<std::string, VarInfo> var_info_map;
+
+    VarInfo get_info(const std::string &name) const {
+      auto item = var_info_map.find(name);
+      if (item != var_info_map.end()) {
+        return item->second;
+      } else {
+        throw cms::Exception("InvalidArgument") << "Cannot find variable info for " << name;
+      }
+    }
+  };
+
 }  // namespace btagbtvdeep
 #endif  //RecoBTag_FeatureTools_deep_helpers_h
