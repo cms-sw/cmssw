deebf6766c730a8c97ac3f3fe90b065b24d5e70d
Cesar Bernardes <cesar.augusto.bernardes@cern.ch>
2020-06-29 05:31:33 +0200

    * Move and rename .cc module to PhysicsTools/PatAlgos * Create cfi file using fillDescription method * Rename cfi file hiPixelTracks and clone the module created by * fillDescription method * Add parameter ptMin and also a flag resetHP to avoid reseting HP flag * in all cases

---
+++ PhysicsTools/PatAlgos/python/patTracksToPackedCandidates_cfi.py
@@ -0,0 +1,17 @@
+import FWCore.ParameterSet.Config as cms
+
+patTracksToPackedCandidates = cms.EDProducer('PATTracksToPackedCandidates',
+  srcTracks = cms.InputTag('hiConformalPixelTracks'),
+  srcVertices = cms.InputTag('offlinePrimaryVertices'),
+  srcPrimaryVertices = cms.InputTag('offlineSlimmedPrimaryVertices'),
+  dzSigCut = cms.double(10),
+  dxySigCut = cms.double(25),
+  dzSigHP = cms.double(7), #to set HighPurity flag for pixel tracks
+  dxySigHP = cms.double(20), #to set HighPurity flag for pixel tracks
+  ptMax = cms.double(1), #max pT for pixel tracks - above this use general tracks instead
+  ptMin = cms.double(0.3), #min pT for pixel tracks
+  resetHP = cms.bool(True), #pixel tracks do not have HP flag set. Use False if does not want to reset HP flag
+  covarianceVersion = cms.int32(0),
+  covarianceSchema = cms.int32(520),
+  mightGet = cms.optional.untracked.vstring
+)
