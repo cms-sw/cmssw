1a6636334a928c6a09566ce7b30be5b88dfdfd22
Mia <mia.tosi@cern.ch>
2020-06-29 19:00:29 +0200

    add modifier for handling the pixel CPE for HLT phase2
    fix
    
    fix

--- RecoTracker/TransientTrackingRecHit/python/TTRHBuilderWithTemplate_cfi.py
+++ RecoTracker/TransientTrackingRecHit/python/TTRHBuilderWithTemplate_cfi.py
@@ -18,3 +18,7 @@ phase1Pixel.toModify(TTRHBuilderAngleAndTemplate, PixelCPE = 'PixelCPEClusterRep
 # Turn off template reco for phase 2 (when not supported)
 from Configuration.ProcessModifiers.phase2_PixelCPEGeneric_cff import phase2_PixelCPEGeneric
 phase2_PixelCPEGeneric.toModify(TTRHBuilderAngleAndTemplate, PixelCPE = 'PixelCPEGeneric')
+
+# Turn off template reco for phase 2 HLT
+from Configuration.ProcessModifiers.phase2_PixelCPEGeneric4HLT_cff import phase2_PixelCPEGeneric4HLT
+phase2_PixelCPEGeneric4HLT.toModify(TTRHBuilderAngleAndTemplate, PixelCPE = 'PixelCPEGeneric')
