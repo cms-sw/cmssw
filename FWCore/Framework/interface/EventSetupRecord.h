67d33e49c2bfc8af98fb85306ea40fb3aafe6c6a
Matti Kortelainen <matti.kortelainen@cern.ch>
2020-07-02 15:39:55 +0200

    Code format

--- FWCore/Framework/interface/EventSetupRecord.h
+++ FWCore/Framework/interface/EventSetupRecord.h
@@ -111,10 +111,9 @@ namespace edm {
 
       template <typename HolderT>
       bool get(char const* iName, HolderT& iHolder) const {
-        if
-          UNLIKELY(requireTokens_) {
-            throwCalledGetWithoutToken(heterocontainer::className<typename HolderT::value_type>(), iName);
-          }
+        if UNLIKELY (requireTokens_) {
+          throwCalledGetWithoutToken(heterocontainer::className<typename HolderT::value_type>(), iName);
+        }
         typename HolderT::value_type const* value = nullptr;
         ComponentDescription const* desc = nullptr;
         std::shared_ptr<ESHandleExceptionFactory> whyFailedFactory;
@@ -136,10 +135,9 @@ namespace edm {
 
       template <typename HolderT>
       bool get(ESInputTag const& iTag, HolderT& iHolder) const {
-        if
-          UNLIKELY(requireTokens_) {
-            throwCalledGetWithoutToken(heterocontainer::className<typename HolderT::value_type>(), iTag.data().c_str());
-          }
+        if UNLIKELY (requireTokens_) {
+          throwCalledGetWithoutToken(heterocontainer::className<typename HolderT::value_type>(), iTag.data().c_str());
+        }
         typename HolderT::value_type const* value = nullptr;
         ComponentDescription const* desc = nullptr;
         std::shared_ptr<ESHandleExceptionFactory> whyFailedFactory;
@@ -207,17 +205,20 @@ namespace edm {
     protected:
       template <template <typename> typename H, typename T, typename R>
       H<T> getHandleImpl(ESGetToken<T, R> const& iToken) const {
-        if
-          UNLIKELY(iToken.transitionID() != transitionID()) { throwWrongTransitionID(); }
+        if UNLIKELY (iToken.transitionID() != transitionID()) {
+          throwWrongTransitionID();
+        }
         assert(iToken.isInitialized());
         assert(getTokenIndices_);
         //need to check token has valid index
-        if
-          UNLIKELY(not iToken.hasValidIndex()) { return invalidTokenHandle<H>(iToken); }
+        if UNLIKELY (not iToken.hasValidIndex()) {
+          return invalidTokenHandle<H>(iToken);
+        }
 
         auto proxyIndex = getTokenIndices_[iToken.index().value()];
-        if
-          UNLIKELY(proxyIndex.value() == std::numeric_limits<int>::max()) { return noProxyHandle<H>(iToken); }
+        if UNLIKELY (proxyIndex.value() == std::numeric_limits<int>::max()) {
+          return noProxyHandle<H>(iToken);
+        }
 
         T const* value = nullptr;
         ComponentDescription const* desc = nullptr;
@@ -225,8 +226,9 @@ namespace edm {
 
         impl_->getImplementation(value, proxyIndex, H<T>::transientAccessOnly, desc, whyFailedFactory, eventSetupImpl_);
 
-        if
-          UNLIKELY(not value) { return H<T>(std::move(whyFailedFactory)); }
+        if UNLIKELY (not value) {
+          return H<T>(std::move(whyFailedFactory));
+        }
         return H<T>(value, desc);
       }
 
