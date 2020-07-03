89b100cf3be9410805bfb382759f2c4a7b6eef3d
silviodonato <silvio.donato@cern.ch>
2020-07-03 17:32:54 +0200

    bug-fix

--- Configuration/StandardSequences/python/L1TrackTrigger_cff.py
+++ Configuration/StandardSequences/python/L1TrackTrigger_cff.py
@@ -2,9 +2,9 @@ import FWCore.ParameterSet.Config as cms
 
 from L1Trigger.TrackTrigger.TrackTrigger_cff import *
 from SimTracker.TrackTriggerAssociation.TrackTriggerAssociator_cff import *
-from L1Trigger.TrackerDTC.Producer_cff import *
+from L1Trigger.TrackerDTC.ProducerED_cff import *
+from L1Trigger.TrackFindingTracklet.L1HybridEmulationTracks_cff import * 
 
-#L1TrackTrigger=cms.Sequence(TrackTriggerClustersStubs*TrackTriggerAssociatorClustersStubs*TrackTriggerTTTracks*TrackTriggerAssociatorTracks)
 L1TrackTrigger=cms.Sequence(TrackTriggerClustersStubs*TrackTriggerAssociatorClustersStubs*TrackerDTCProducer)
 
 # Customisation to enable TTTracks in geometry D41 and later (corresponding to phase2_trackerV14 or later). Includes the HGCAL L1 trigger
