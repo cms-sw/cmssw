16d6769d7b78c609799bf9f7262dd06762e00c8b
Sunanda <sunanda.banerjee@cern.ch>
2020-07-03 21:28:03 +0200

    Code check

--- Geometry/HGCalCommonData/plugins/DDHGCalEEFileAlgo.cc
+++ Geometry/HGCalCommonData/plugins/DDHGCalEEFileAlgo.cc
@@ -331,9 +331,11 @@ void DDHGCalEEFileAlgo::constructLayers(const DDLogicalPart& module, DDCompactVi
     // Make consistency check of all the partitions of the block
     if (std::abs(thickTot - layerThick_[i]) >= tol2_) {
       if (thickTot > layerThick_[i]) {
-	edm::LogError("HGCalGeom") << "Thickness of the partition " << layerThick_[i] << " is smaller than " << thickTot << ": thickness of all its components **** ERROR ****";
+        edm::LogError("HGCalGeom") << "Thickness of the partition " << layerThick_[i] << " is smaller than " << thickTot
+                                   << ": thickness of all its components **** ERROR ****";
       } else {
-	edm::LogWarning("HGCalGeom") << "Thickness of the partition " << layerThick_[i] << " does not match with " << thickTot << " of the components";
+        edm::LogWarning("HGCalGeom") << "Thickness of the partition " << layerThick_[i] << " does not match with "
+                                     << thickTot << " of the components";
       }
     }
   }  // End of loop over blocks
