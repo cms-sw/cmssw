749b94af0a922fd239c00f14b3f504470ff635b6
simone gennai <simone.gennai@cern.ch>
2020-07-07 16:55:56 +0200

    trying to sync with 11_1_x

--- CondFormats/BeamSpotObjects/interface/BeamSpotOnlineObjects.h
+++ CondFormats/BeamSpotObjects/interface/BeamSpotOnlineObjects.h
@@ -13,12 +13,14 @@
  */
 
 #include "CondFormats/Serialization/interface/Serializable.h"
-
+#include "CondFormats/Common/interface/Time.h"
 #include "CondFormats/BeamSpotObjects/interface/BeamSpotObjects.h"
 
 #include <cmath>
 #include <sstream>
 #include <cstring>
+#include <vector>
+#include <string>
 
 class BeamSpotOnlineObjects : public BeamSpotObjects {
 public:
@@ -27,10 +29,15 @@ public:
     lastAnalyzedLumi_ = 0;
     lastAnalyzedRun_ = 0;
     lastAnalyzedFill_ = 0;
+    intParams_.resize(ISIZE, std::vector<int>(1, 0));
+    timeParams_.resize(TSIZE, std::vector<unsigned long long>(1, 0ULL));
   }
 
-  virtual ~BeamSpotOnlineObjects() {}
+  ~BeamSpotOnlineObjects() override {}
 
+  /// Enums
+  enum IntParamIndex { NUM_TRACKS = 0, NUM_PVS = 1, ISIZE = 2 };
+  enum TimeParamIndex { CREATE_TIME = 0, TSIZE = 1 };
 
   /// Setters Methods
   // set lastAnalyzedLumi_, last analyzed lumisection
@@ -42,6 +49,14 @@ public:
   // set lastAnalyzedFill_, fill of the last analyzed lumisection
   void SetLastAnalyzedFill(int val) { lastAnalyzedFill_ = val; }
 
+  // set number of tracks used in the BeamSpot fit
+  void SetNumTracks(int val);
+
+  // set number of Primary Vertices used in the BeamSpot fit
+  void SetNumPVs(int val);
+
+  // set creation time of the payload
+  void SetCreationTime(cond::Time_t val);
 
   /// Getters Methods
   // get lastAnalyzedLumi_, last analyzed lumisection
@@ -53,15 +68,26 @@ public:
   // get lastAnalyzedFill_, fill of the last analyzed lumisection
   int GetLastAnalyzedFill() const { return lastAnalyzedFill_; }
 
+  // get number of tracks used in the BeamSpot fit
+  int GetNumTracks() const;
+
+  // get number of Primary Vertices used in the BeamSpot fit
+  int GetNumPVs() const;
+
+  // get creation time of the payload
+  cond::Time_t GetCreationTime() const;
 
   /// Print BeamSpotOnline parameters
   void print(std::stringstream& ss) const;
-
 
 private:
   int lastAnalyzedLumi_;
   int lastAnalyzedRun_;
   int lastAnalyzedFill_;
+  std::vector<std::vector<int> > intParams_;
+  std::vector<std::vector<float> > floatParams_;
+  std::vector<std::vector<std::string> > stringParams_;
+  std::vector<std::vector<unsigned long long> > timeParams_;
 
   COND_SERIALIZABLE;
 };
--- CondFormats/BeamSpotObjects/src/BeamSpotOnlineObjects.cc
+++ CondFormats/BeamSpotObjects/src/BeamSpotOnlineObjects.cc
@@ -2,6 +2,71 @@
 
 #include <iostream>
 
+namespace BeamSpotOnlineObjectsImpl {
+  template <typename T>
+  const T& getParams(const std::vector<T>& params, size_t index) {
+    if (index >= params.size())
+      throw std::out_of_range("Parameter with index " + std::to_string(index) + " is out of range.");
+    return params[index];
+  }
+
+  template <typename T>
+  T& accessParams(std::vector<T>& params, size_t index) {
+    if (index >= params.size())
+      throw std::out_of_range("Parameter with index " + std::to_string(index) + " is out of range.");
+    return params[index];
+  }
+
+  template <typename T>
+  const T& getOneParam(const std::vector<std::vector<T> >& params, size_t index) {
+    if (index >= params.size())
+      throw std::out_of_range("Parameter with index " + std::to_string(index) + " is out of range.");
+    const std::vector<T>& inner = params[index];
+    if (inner.empty())
+      throw std::out_of_range("Parameter with index " + std::to_string(index) + " type=" + typeid(T).name() +
+                              " has no value stored.");
+    return inner[0];
+  }
+
+  template <typename T>
+  void setOneParam(std::vector<std::vector<T> >& params, size_t index, const T& value) {
+    if (index >= params.size())
+      throw std::out_of_range("Parameter with index " + std::to_string(index) + " is out of range.");
+    params[index] = std::vector<T>(1, value);
+  }
+
+  template <typename T>
+  void setParams(std::vector<T>& params, size_t index, const T& value) {
+    if (index >= params.size())
+      throw std::out_of_range("Parameter with index " + std::to_string(index) + " is out of range.");
+    params[index] = value;
+  }
+
+}  //namespace BeamSpotOnlineObjectsImpl
+
+// getters
+int BeamSpotOnlineObjects::GetNumTracks() const {
+  return BeamSpotOnlineObjectsImpl::getOneParam(intParams_, NUM_TRACKS);
+}
+
+int BeamSpotOnlineObjects::GetNumPVs() const { return BeamSpotOnlineObjectsImpl::getOneParam(intParams_, NUM_PVS); }
+
+cond::Time_t BeamSpotOnlineObjects::GetCreationTime() const {
+  return BeamSpotOnlineObjectsImpl::getOneParam(timeParams_, CREATE_TIME);
+}
+
+// setters
+void BeamSpotOnlineObjects::SetNumTracks(int nTracks) {
+  BeamSpotOnlineObjectsImpl::setOneParam(intParams_, NUM_TRACKS, nTracks);
+}
+
+void BeamSpotOnlineObjects::SetNumPVs(int nPVs) { BeamSpotOnlineObjectsImpl::setOneParam(intParams_, NUM_PVS, nPVs); }
+
+void BeamSpotOnlineObjects::SetCreationTime(cond::Time_t createTime) {
+  BeamSpotOnlineObjectsImpl::setOneParam(timeParams_, CREATE_TIME, createTime);
+}
+
+// printers
 void BeamSpotOnlineObjects::print(std::stringstream& ss) const {
   ss << "-----------------------------------------------------\n"
      << "              BeamSpotOnline Data\n\n"
