9e763317ebec49c90871b307d85aab40b3de58e4
Tamas <vami.tamas@wigner.mta.hu>
2020-07-07 23:02:40 +0200

    add unit test
    test edits
    
    extra enter

--- CondTools/SiPixel/test/run_0T_BoR3.sh
+++ CondTools/SiPixel/test/run_0T_BoR3.sh
@@ -1,2 +1,7 @@
-cmsRun SiPixelTemplateDBObjectUploader_cfg.py Fullname=SiPixelTemplateDBObject_phase1_0T_mc_BoR3_v1_bugfix Map=../data/SiPixelTemplateDBObject_0T_phase1_BoR3_v1/mapping.csv TemplateFilePath=CondTools/SiPixel/data/SiPixelTemplateDBObject_0T_phase1_BoR3_v1
-cmsRun SiPixelGenErrorDBObjectUploader_cfg.py Fullname=SiPixelGenErrorDBObject_phase1_0T_mc_BoR3_v1_bugfix Map=../data/SiPixelTemplateDBObject_0T_phase1_BoR3_v1/mapping.csv GenErrFilePath=CondTools/SiPixel/data/SiPixelTemplateDBObject_0T_phase1_BoR3_v1
+#!/bin/sh
+
+function die { echo $1: status $2 ; exit $2; }
+
+echo "TESTING Pixel CPE DB codes ..."
+cmsRun ${LOCAL_TEST_DIR}/SiPixelTemplateDBObjectUploader_cfg.py Fullname=SiPixelTemplateDBObject_phase1_0T_mc_BoR3_v1_bugfix Map=${LOCAL_TEST_DIR}/../data/phaseI_mapping.csv TemplateFilePath=CalibTracker/SiPixelESProducers/data/SiPixelTemplateDBObject_0T_phase1_BoR3_v1 || die "Failure running SiPixelTemplateDBObjectUploader_cfg.py" $? 
+cmsRun ${LOCAL_TEST_DIR}/SiPixelGenErrorDBObjectUploader_cfg.py Fullname=SiPixelGenErrorDBObject_phase1_0T_mc_BoR3_v1_bugfix Map=${LOCAL_TEST_DIR}/../data/phaseI_mapping.csv GenErrFilePath=CalibTracker/SiPixelESProducers/data/SiPixelTemplateDBObject_0T_phase1_BoR3_v1 || die "Failure running SiPixelGenErrorDBObjectUploader_cfg.py" $? 
