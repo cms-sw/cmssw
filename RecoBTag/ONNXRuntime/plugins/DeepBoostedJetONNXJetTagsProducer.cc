18da0a3fb70ba24effd53f70ed2b090cca61ffa9
Huilin Qu <huilin.qu@cern.ch>
2020-07-08 16:59:33 +0200

    Support ONNX models w/ dynamic axes.

--- RecoBTag/ONNXRuntime/plugins/DeepBoostedJetONNXJetTagsProducer.cc
+++ RecoBTag/ONNXRuntime/plugins/DeepBoostedJetONNXJetTagsProducer.cc
@@ -44,7 +44,8 @@ private:
   std::vector<float> center_norm_pad(const std::vector<float> &input,
                                      float center,
                                      float scale,
-                                     unsigned target_length,
+                                     unsigned min_length,
+                                     unsigned max_length,
                                      float pad_value = 0,
                                      float replace_inf_value = 0,
                                      float min = 0,
@@ -52,9 +53,10 @@ private:
   void make_inputs(const reco::DeepBoostedJetTagInfo &taginfo);
 
   const edm::EDGetTokenT<TagInfoCollection> src_;
-  std::vector<std::string> flav_names_;                  // names of the output scores
-  std::vector<std::string> input_names_;                 // names of each input group - the ordering is important!
-  std::vector<std::vector<unsigned int>> input_shapes_;  // shapes of each input group
+  std::vector<std::string> flav_names_;             // names of the output scores
+  std::vector<std::string> input_names_;            // names of each input group - the ordering is important!
+  std::vector<std::vector<int64_t>> input_shapes_;  // shapes of each input group (-1 for dynamic axis)
+  std::vector<unsigned> input_sizes_;               // total length of each input vector
   std::unordered_map<std::string, PreprocessParams> prep_info_map_;  // preprocessing info for each input group
 
   FloatArrays data_;
@@ -72,34 +74,43 @@ DeepBoostedJetONNXJetTagsProducer::DeepBoostedJetONNXJetTagsProducer(const edm::
   input_names_ = prep_pset.getParameter<std::vector<std::string>>("input_names");
   for (const auto &group_name : input_names_) {
     const auto &group_pset = prep_pset.getParameterSet(group_name);
-    input_shapes_.push_back(group_pset.getParameter<std::vector<unsigned>>("input_shape"));
     auto &prep_params = prep_info_map_[group_name];
-    prep_params.var_length = group_pset.getParameter<unsigned>("var_length");
     prep_params.var_names = group_pset.getParameter<std::vector<std::string>>("var_names");
-    prep_params.pad = group_pset.getParameter<double>("pad");
+    if (group_pset.existsAs<unsigned>("var_length")) {
+      prep_params.min_length = group_pset.getParameter<unsigned>("var_length");
+      prep_params.max_length = prep_params.min_length;
+    } else {
+      prep_params.min_length = group_pset.getParameter<unsigned>("min_length");
+      prep_params.max_length = group_pset.getParameter<unsigned>("max_length");
+      input_shapes_.push_back({1, (int64_t)prep_params.var_names.size(), -1});
+    }
     const auto &var_info_pset = group_pset.getParameterSet("var_infos");
     for (const auto &var_name : prep_params.var_names) {
       const auto &var_pset = var_info_pset.getParameterSet(var_name);
       double median = var_pset.getParameter<double>("median");
       double norm_factor = var_pset.getParameter<double>("norm_factor");
-      double replace_inf_value = var_pset.getParameter<double>("replace_inf_value");
-      double lower_bound = var_pset.getParameter<double>("lower_bound");
-      double upper_bound = var_pset.getParameter<double>("upper_bound");
+      double replace_inf_value =
+          var_pset.exists("replace_inf_value") ? var_pset.getParameter<double>("replace_inf_value") : 0;
+      double lower_bound = var_pset.exists("lower_bound") ? var_pset.getParameter<double>("lower_bound") : -5;
+      double upper_bound = var_pset.exists("upper_bound") ? var_pset.getParameter<double>("upper_bound") : 5;
+      double pad = var_pset.exists("pad") ? var_pset.getParameter<double>("pad") : 0;
       prep_params.var_info_map[var_name] =
-          PreprocessParams::VarInfo(median, norm_factor, replace_inf_value, lower_bound, upper_bound);
+          PreprocessParams::VarInfo(median, norm_factor, replace_inf_value, lower_bound, upper_bound, pad);
     }
 
-    // create data storage with a fixed size vector initilized w/ pad
-    unsigned len = prep_params.var_length * prep_params.var_names.size();
-    data_.emplace_back(len, prep_params.pad);
+    // create data storage with a fixed size vector initiliazed w/ 0
+    const auto &len = input_sizes_.emplace_back(prep_params.max_length * prep_params.var_names.size());
+    data_.emplace_back(len, 0);
   }
 
   if (debug_) {
     for (unsigned i = 0; i < input_names_.size(); ++i) {
       const auto &group_name = input_names_.at(i);
-      std::cout << group_name << "\nshapes: ";
-      for (const auto &x : input_shapes_.at(i)) {
-        std::cout << x << ", ";
+      if (!input_shapes_.empty()) {
+        std::cout << group_name << "\nshapes: ";
+        for (const auto &x : input_shapes_.at(i)) {
+          std::cout << x << ", ";
+        }
       }
       std::cout << "\nvariables: ";
       for (const auto &x : prep_info_map_.at(group_name).var_names) {
@@ -183,7 +194,7 @@ void DeepBoostedJetONNXJetTagsProducer::produce(edm::Event &iEvent, const edm::E
       // convert inputs
       make_inputs(taginfo);
       // run prediction and get outputs
-      outputs = globalCache()->run(input_names_, data_)[0];
+      outputs = globalCache()->run(input_names_, data_, input_shapes_)[0];
       assert(outputs.size() == flav_names_.size());
     }
 
@@ -215,7 +226,8 @@ void DeepBoostedJetONNXJetTagsProducer::produce(edm::Event &iEvent, const edm::E
 std::vector<float> DeepBoostedJetONNXJetTagsProducer::center_norm_pad(const std::vector<float> &input,
                                                                       float center,
                                                                       float norm_factor,
-                                                                      unsigned target_length,
+                                                                      unsigned min_length,
+                                                                      unsigned max_length,
                                                                       float pad_value,
                                                                       float replace_inf_value,
                                                                       float min,
@@ -223,7 +235,9 @@ std::vector<float> DeepBoostedJetONNXJetTagsProducer::center_norm_pad(const std:
   // do variable shifting/scaling/padding/clipping in one go
 
   assert(min <= pad_value && pad_value <= max);
+  assert(min_length <= max_length);
 
+  unsigned target_length = std::clamp((unsigned)input.size(), min_length, max_length);
   std::vector<float> out(target_length, pad_value);
   for (unsigned i = 0; i < input.size() && i < target_length; ++i) {
     out[i] = std::clamp((catch_infs(input[i], replace_inf_value) - center) * norm_factor, min, max);
@@ -234,34 +248,42 @@ std::vector<float> DeepBoostedJetONNXJetTagsProducer::center_norm_pad(const std:
 void DeepBoostedJetONNXJetTagsProducer::make_inputs(const reco::DeepBoostedJetTagInfo &taginfo) {
   for (unsigned igroup = 0; igroup < input_names_.size(); ++igroup) {
     const auto &group_name = input_names_[igroup];
+    const auto &prep_params = prep_info_map_.at(group_name);
     auto &group_values = data_[igroup];
-    const auto &prep_params = prep_info_map_[group_name];
-    // first reset group_values to pad
-    std::fill(group_values.begin(), group_values.end(), prep_params.pad);
+    group_values.resize(input_sizes_[igroup]);
+    // first reset group_values to 0
+    std::fill(group_values.begin(), group_values.end(), 0);
     unsigned curr_pos = 0;
     // transform/pad
-    for (const auto &varname : prep_params.var_names) {
+    for (unsigned i = 0; i < prep_params.var_names.size(); ++i) {
+      const auto &varname = prep_params.var_names[i];
       const auto &raw_value = taginfo.features().get(varname);
       const auto &info = prep_params.info(varname);
       auto val = center_norm_pad(raw_value,
                                  info.center,
                                  info.norm_factor,
-                                 prep_params.var_length,
-                                 prep_params.pad,
+                                 prep_params.min_length,
+                                 prep_params.max_length,
+                                 info.pad,
                                  info.replace_inf_value,
                                  info.lower_bound,
                                  info.upper_bound);
       std::copy(val.begin(), val.end(), group_values.begin() + curr_pos);
-      curr_pos += prep_params.var_length;
+      curr_pos += val.size();
+      if (i == 0 && (!input_shapes_.empty())) {
+        input_shapes_[igroup][2] = val.size();
+      }
 
       if (debug_) {
         std::cout << " -- var=" << varname << ", center=" << info.center << ", scale=" << info.norm_factor
-                  << ", replace=" << info.replace_inf_value << ", pad=" << prep_params.pad << std::endl;
-        std::cout << "values (first 5 and last 3): " << val.at(0) << ", " << val.at(1) << ", " << val.at(2) << ", "
-                  << val.at(3) << ", " << val.at(4) << "..." << val.at(prep_params.var_length - 3) << ", "
-                  << val.at(prep_params.var_length - 2) << ", " << val.at(prep_params.var_length - 1) << std::endl;
+                  << ", replace=" << info.replace_inf_value << ", pad=" << info.pad << std::endl;
+        for (const auto &v : val) {
+          std::cout << v << ",";
+        }
+        std::cout << std::endl;
       }
     }
+    group_values.resize(curr_pos);
   }
 }
 
