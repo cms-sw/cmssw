a5e4e406bb06f6b226d61e99e0b5e3f58af07ac3
Kevin Pedro <kpedro88@gmail.com>
2020-07-10 14:24:48 -0500

    remove unneeded eras

--- Configuration/Eras/python/Era_Phase2C4_dd4hep_cff.py
+++
@@ -1,6 +0,0 @@
-import FWCore.ParameterSet.Config as cms
-
-from Configuration.Eras.Era_Phase2C4_cff import Phase2C4
-from Configuration.ProcessModifiers.dd4hep_cff import dd4hep
-
-Phase2C4_dd4hep = cms.ModifierChain(Phase2C4, dd4hep)
