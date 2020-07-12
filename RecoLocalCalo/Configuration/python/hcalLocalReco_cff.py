f95560a5c33bf663ea9d0a76e33f7d57782ea264
Andrea Bocci <andrea.bocci@cern.ch>
2020-07-12 23:24:43 +0200

    Synchronise with CMSSW_11_2_0_pre2

--- RecoLocalCalo/Configuration/python/hcalLocalReco_cff.py
+++ RecoLocalCalo/Configuration/python/hcalLocalReco_cff.py
@@ -1,33 +1,32 @@
 import FWCore.ParameterSet.Config as cms
 
 from RecoLocalCalo.HcalRecAlgos.hcalRecAlgoESProd_cfi import *
+from RecoLocalCalo.HcalRecAlgos.hcalChannelPropertiesESProd_cfi import *
 hcalOOTPileupESProducer = cms.ESProducer('OOTPileupDBCompatibilityESProducer')
 
 from RecoLocalCalo.HcalRecProducers.HBHEPhase1Reconstructor_cfi import hbheprereco as _phase1_hbheprereco
 hbheprereco = _phase1_hbheprereco.clone(
-    processQIE11 = cms.bool(False),
-    tsFromDB = cms.bool(True),
+    processQIE11 = False,
+    tsFromDB = True,
     pulseShapeParametersQIE8 = dict(
-        TrianglePeakTS = cms.uint32(4),
+        TrianglePeakTS = 4,
     )
 )
 
 from RecoLocalCalo.HcalRecProducers.HcalHitReconstructor_ho_cfi import *
 from RecoLocalCalo.HcalRecProducers.HcalHitReconstructor_hf_cfi import *
 from RecoLocalCalo.HcalRecProducers.HcalHitReconstructor_zdc_cfi import *
-hcalLocalRecoTask = cms.Task(hbheprereco,hfreco,horeco,zdcreco)
+hcalLocalRecoTask = cms.Task(hbheprereco, hfreco, horeco, zdcreco)
 hcalLocalRecoSequence = cms.Sequence(hcalLocalRecoTask)
 
 from RecoLocalCalo.HcalRecProducers.hfprereco_cfi import hfprereco
 from RecoLocalCalo.HcalRecProducers.HFPhase1Reconstructor_cfi import hfreco as _phase1_hfreco
 from RecoLocalCalo.HcalRecProducers.hbheplan1_cfi import hbheplan1
 
-#--- for HCALonly wf
-hcalOnlyLocalRecoTask = cms.Task(hbheprereco,hfprereco,hfreco,horeco)
-
-# copy for cosmics
+#--- for cosmics
 _default_hfreco = hfreco.clone()
 
+#--- for Phase 1
 _phase1_hcalLocalRecoTask = hcalLocalRecoTask.copy()
 _phase1_hcalLocalRecoTask.add(hfprereco)
 
@@ -35,7 +34,7 @@ from Configuration.Eras.Modifier_run2_HF_2017_cff import run2_HF_2017
 run2_HF_2017.toReplaceWith( hcalLocalRecoTask, _phase1_hcalLocalRecoTask )
 run2_HF_2017.toReplaceWith( hfreco, _phase1_hfreco )
 from Configuration.Eras.Modifier_run2_HCAL_2017_cff import run2_HCAL_2017
-run2_HCAL_2017.toReplaceWith( hbheprereco, _phase1_hbheprereco )
+run2_HCAL_2017.toReplaceWith(hbheprereco, _phase1_hbheprereco)
 
 _plan1_hcalLocalRecoTask = _phase1_hcalLocalRecoTask.copy()
 _plan1_hcalLocalRecoTask.add(hbheplan1)
@@ -48,17 +47,30 @@ _collapse_hcalLocalRecoTask.add(hbhecollapse)
 from Configuration.ProcessModifiers.run2_HECollapse_2018_cff import run2_HECollapse_2018
 run2_HECollapse_2018.toReplaceWith(hcalLocalRecoTask, _collapse_hcalLocalRecoTask)
 
-# Run 3 HCAL workflow on GPU
+#--- for Run 3 and later
+_run3_hcalLocalRecoTask = _phase1_hcalLocalRecoTask.copy()
+_run3_hcalLocalRecoTask.remove(hbheprereco)
+from Configuration.Eras.Modifier_run3_HB_cff import run3_HB
+run3_HB.toReplaceWith(hcalLocalRecoTask, _run3_hcalLocalRecoTask)
+
+#--- for Run 3 on GPU
 from Configuration.ProcessModifiers.gpu_cff import gpu
 
 from RecoLocalCalo.HcalRecProducers.hbheRecHitProducerGPUTask_cff import *
-_hcalLocalRecoTaskGPU = hcalLocalRecoTask.copy()
-_hcalLocalRecoTaskGPU.add(hbheRecHitProducerGPUTask)
-gpu.toReplaceWith(hcalLocalRecoTask, _hcalLocalRecoTaskGPU)
+_run3_hcalLocalRecoGPUTask = _run3_hcalLocalRecoTask.copy()
+_run3_hcalLocalRecoGPUTask.add(hbheRecHitProducerGPUTask)
+gpu.toReplaceWith(hcalLocalRecoTask, _run3_hcalLocalRecoGPUTask)
 
-_hcalOnlyLocalRecoTaskGPU = hcalOnlyLocalRecoTask.copy()
-_hcalOnlyLocalRecoTaskGPU.add(hbheRecHitProducerGPUTask)
-gpu.toReplaceWith(hcalOnlyLocalRecoTask, _hcalOnlyLocalRecoTaskGPU)
+#--- HCAL-only workflow for Run 3
+# FIXME rename `hbheprereco` to `hbhereco` and use it from hcalGlobalRecoTask
+hcalOnlyLocalRecoTask = cms.Task(hbheprereco, hfprereco, hfreco, horeco)
+
+#--- HCAL-only workflow for Run 3 on GPU
+from Configuration.ProcessModifiers.gpu_cff import gpu
+
+_hcalOnlyLocalRecoGPUTask = hcalOnlyLocalRecoTask.copy()
+_hcalOnlyLocalRecoGPUTask.add(hbheRecHitProducerGPUTask)
+gpu.toReplaceWith(hcalOnlyLocalRecoTask, _hcalOnlyLocalRecoGPUTask)
 
 from RecoLocalCalo.HcalRecProducers.hcalCPURecHitsProducer_cfi import hcalCPURecHitsProducer as _hcalCPURecHitsProducer
 gpu.toReplaceWith(hbheprereco, _hcalCPURecHitsProducer.clone(
@@ -66,15 +78,8 @@ gpu.toReplaceWith(hbheprereco, _hcalCPURecHitsProducer.clone(
     recHitsM0LabelOut = "",
     recHitsLegacyLabelOut = ""
 ))
-#---
 
-_phase2_hcalLocalRecoTask = hcalLocalRecoTask.copy()
-_phase2_hcalLocalRecoTask.remove(hbheprereco)
-
-from Configuration.Eras.Modifier_phase2_hcal_cff import phase2_hcal
-phase2_hcal.toReplaceWith( hcalLocalRecoTask, _phase2_hcalLocalRecoTask )
-
-
+#--- for FastSim
 _fastSim_hcalLocalRecoTask = hcalLocalRecoTask.copyAndExclude([zdcreco])
 from Configuration.Eras.Modifier_fastSim_cff import fastSim
 fastSim.toReplaceWith( hcalLocalRecoTask, _fastSim_hcalLocalRecoTask )
