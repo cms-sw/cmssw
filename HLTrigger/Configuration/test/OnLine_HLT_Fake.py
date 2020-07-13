301c5399735e95aaae2dc01a95860f20c200a0a3
Martin Grunewald <Martin.Grunewald@cern.ch>
2020-07-13 14:03:07 +0200

    HLT Menu Migration to 11_2_0_pre2 Template

--- HLTrigger/Configuration/test/OnLine_HLT_Fake.py
+++ HLTrigger/Configuration/test/OnLine_HLT_Fake.py
@@ -1,13 +1,13 @@
-# hltGetConfiguration --full --data /dev/CMSSW_11_1_0/Fake --type Fake --unprescale --process HLTFake --globaltag auto:run1_hlt_Fake --input file:RelVal_Raw_Fake_DATA.root
+# hltGetConfiguration --full --data /dev/CMSSW_11_2_0/Fake --type Fake --unprescale --process HLTFake --globaltag auto:run1_hlt_Fake --input file:RelVal_Raw_Fake_DATA.root
 
-# /dev/CMSSW_11_1_0/Fake/V9 (CMSSW_11_1_0)
+# /dev/CMSSW_11_2_0/Fake/V3 (CMSSW_11_2_0_pre2)
 
 import FWCore.ParameterSet.Config as cms
 
 process = cms.Process( "HLTFake" )
 
 process.HLTConfigVersion = cms.PSet(
-  tableName = cms.string('/dev/CMSSW_11_1_0/Fake/V9')
+  tableName = cms.string('/dev/CMSSW_11_2_0/Fake/V3')
 )
 
 process.streams = cms.PSet(  A = cms.vstring( 'InitialPD' ) )
