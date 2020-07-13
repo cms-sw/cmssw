173bd213b7da5edad7e7e2555ac5b9a57f480e09
Gabrielle Hugo <gabrielle.hugo83@gmail.com>
2020-07-13 18:37:17 +0200

    moved dd4hep xml files

--- CondTools/Geometry/test/ppsgeometrywriter.py
+++ CondTools/Geometry/test/ppsgeometrywriter.py
@@ -18,39 +18,18 @@ process.PoolDBOutputService = cms.Service("PoolDBOutputService",
     ))
 )
 
-# minimum of logs
-#process.MessageLogger = cms.Service("MessageLogger",
-#    statistics = cms.untracked.vstring(),
-#    destinations = cms.untracked.vstring('cout'),
-#    cout = cms.untracked.PSet(
-#        threshold = cms.untracked.string('INFO')
-#    )
-#)
-
 process.load("FWCore.MessageService.MessageLogger_cfi")
 process.MessageLogger.debugModules = cms.untracked.vstring('ppsGeometryBuilder')
 process.MessageLogger.cout = cms.untracked.PSet(
     threshold = cms.untracked.string('DEBUG'))
 
-
 # geometry
-#from Geometry.VeryForwardGeometry.geometryIdealPPSFromDD_2017_cfi import allFiles
+from Geometry.VeryForwardGeometry.geometryIdealPPSFromDD_2017_cfi import allFiles
 
-#process.XMLIdealGeometryESSource_CTPPS = cms.ESSource("XMLIdealGeometryESSource",
-#    geomXMLFiles = allFiles,
-#    rootNodeName = cms.string('cms:CMSE')
-#)
-
-# dd4hep-based geometry
-process.DDDetectorESProducer = cms.ESSource("DDDetectorESProducer",
-                                            confGeomXMLFiles = cms.FileInPath('Geometry/VeryForwardGeometry/test/geometryIdealPPSFromDD_2017_ddhep.xml'),
-                                            appendToDataLabel = cms.string('XMLIdealGeometryESSource_CTPPS')
+process.XMLIdealGeometryESSource_CTPPS = cms.ESSource("XMLIdealGeometryESSource",
+    geomXMLFiles = allFiles,
+    rootNodeName = cms.string('cms:CMSE')
 )
-
-process.DDCompactViewESProducer = cms.ESProducer("DDCompactViewESProducer",
-                                                 appendToDataLabel = cms.string('XMLIdealGeometryESSource_CTPPS')
-)
-
 
 # no events to process
 process.source = cms.Source("EmptySource",
--- CondTools/Geometry/test/ppsgeometrywriter_dd4hep.py
+++ CondTools/Geometry/test/ppsgeometrywriter_dd4hep.py
@@ -18,33 +18,16 @@ process.PoolDBOutputService = cms.Service("PoolDBOutputService",
     ))
 )
 
-# minimum of logs
-#process.MessageLogger = cms.Service("MessageLogger",
-#    statistics = cms.untracked.vstring(),
-#    destinations = cms.untracked.vstring('cout'),
-#    cout = cms.untracked.PSet(
-#        threshold = cms.untracked.string('INFO')
-#    )
-#)
-
+# MessageLogger
 process.load("FWCore.MessageService.MessageLogger_cfi")
 process.MessageLogger.debugModules = cms.untracked.vstring('ppsGeometryBuilder')
 process.MessageLogger.cout = cms.untracked.PSet(
     threshold = cms.untracked.string('DEBUG'))
 
 
-# geometry
-#from Geometry.VeryForwardGeometry.geometryIdealPPSFromDD_2017_cfi import allFiles
-
-#process.XMLIdealGeometryESSource_CTPPS = cms.ESSource("XMLIdealGeometryESSource",
-#    geomXMLFiles = allFiles,
-#    rootNodeName = cms.string('cms:CMSE')
-#)
-
 # dd4hep-based geometry
 process.DDDetectorESProducer = cms.ESSource("DDDetectorESProducer",
-                                            confGeomXMLFiles = cms.FileInPath('Geometry/VeryForwardGeometry/test/geometryIdealPPSFromDD_2018_ddhep.xml'),
-                                            #confGeomXMLFiles = cms.FileInPath('Geometry/VeryForwardGeometry/data/dd4hep/cms-pps-reco-geometry-2018.xml'),
+                                            confGeomXMLFiles = cms.FileInPath('Geometry/VeryForwardGeometry/data/dd4hep/dd4hep_geometryIdealPPS_2018.xml'),
                                             appendToDataLabel = cms.string('XMLIdealGeometryESSource_CTPPS')
 )
 
