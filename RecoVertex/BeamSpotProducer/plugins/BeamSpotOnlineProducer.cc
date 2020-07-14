dfd109aa8537fcba8d8c76971ba8fe5516ccb163
Jonas Rembser <jonas.rembser@cern.ch>
2020-07-14 12:31:55 +0200

    clean BuildFiles in and after Reco area

--- RecoVertex/BeamSpotProducer/plugins/BeamSpotOnlineProducer.cc
+++ RecoVertex/BeamSpotProducer/plugins/BeamSpotOnlineProducer.cc
@@ -18,7 +18,6 @@ ________________________________________________________________**/
 #include "FWCore/Framework/interface/ESHandle.h"
 #include "FWCore/Framework/interface/Event.h"
 #include "FWCore/Framework/interface/EventSetup.h"
-//#include "FWCore/Framework/interface/IOVSyncValue.h"
 #include "FWCore/Framework/interface/stream/EDProducer.h"
 #include "FWCore/MessageLogger/interface/MessageLogger.h"
 #include "FWCore/ParameterSet/interface/ParameterSet.h"
