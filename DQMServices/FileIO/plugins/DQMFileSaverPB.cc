262f5c4fea990f844eca5fd22b2f9f00a21be683
andrius <andrius.kirilovas@cern.ch>
2020-07-15 15:09:31 +0200

    No more lumi in a PB file name
    This ensures that files get overwritten to save space

--- DQMServices/FileIO/plugins/DQMFileSaverPB.cc
+++ DQMServices/FileIO/plugins/DQMFileSaverPB.cc
@@ -78,7 +78,7 @@ void DQMFileSaverPB::saveLumi(const FileParameters& fp) const {
       std::filesystem::create_directories(runDir);
     }
     else {
-      baseName = fmt::sprintf("%s/%s/run%06d_ls%04d_%s_%s", runDir, tag_, fp.run_, fp.lumi_, tag_, streamLabel_);
+      baseName = fmt::sprintf("%s/%s/run%06d_%s_%s", runDir, tag_, fp.run_, tag_, streamLabel_);
       std::filesystem::create_directories(runDir + "/" + tag_);
     }
 
