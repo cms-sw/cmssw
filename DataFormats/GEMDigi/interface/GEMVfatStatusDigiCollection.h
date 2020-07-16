2b6858f48a68d318000f600a25c8c7d2e676569c
Ian J. Watson <ian.james.watson@cern.ch>
2020-07-16 14:28:21 +0900

    Move dictionary generating classes from EventFilter/GEMRawToDigi to DataFormats/GEMDigi and update references

--- DataFormats/GEMDigi/interface/GEMVfatStatusDigiCollection.h
+++ DataFormats/GEMDigi/interface/GEMVfatStatusDigiCollection.h
@@ -1,7 +1,7 @@
 #ifndef DataFormats_GEMDigi_GEMVfatStatusDigiCollection_h
 #define DataFormats_GEMDigi_GEMVfatStatusDigiCollection_h
 
-#include "EventFilter/GEMRawToDigi/interface/GEMVfatStatusDigi.h"
+#include "DataFormats/GEMDigi/interface/GEMVfatStatusDigi.h"
 #include "DataFormats/MuonDetId/interface/GEMDetId.h"
 #include "DataFormats/MuonData/interface/MuonDigiCollection.h"
 
