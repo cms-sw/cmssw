30c3202db45744e764d6f21fc041b7f2182bd84f
Huilin Qu <huilin.qu@cern.ch>
2020-07-16 16:06:25 +0200

    Improve parameter handling.

--- RecoBTag/ONNXRuntime/plugins/BoostedJetONNXJetTagsProducer.cc
+++ RecoBTag/ONNXRuntime/plugins/BoostedJetONNXJetTagsProducer.cc
@@ -74,10 +74,10 @@ BoostedJetONNXJetTagsProducer::BoostedJetONNXJetTagsProducer(const edm::Paramete
   if (!json_path.empty()) {
     // use preprocessing json file if available
     std::ifstream ifs(edm::FileInPath(json_path).fullPath());
-    nlohmann::json j = nlohmann::json::parse(ifs);
-    j.at("input_names").get_to(input_names_);
+    nlohmann::json js = nlohmann::json::parse(ifs);
+    js.at("input_names").get_to(input_names_);
     for (const auto &group_name : input_names_) {
-      const auto &group_pset = j.at(group_name);
+      const auto &group_pset = js.at(group_name);
       auto &prep_params = prep_info_map_[group_name];
       group_pset.at("var_names").get_to(prep_params.var_names);
       if (group_pset.contains("var_length")) {
@@ -93,10 +93,9 @@ BoostedJetONNXJetTagsProducer::BoostedJetONNXJetTagsProducer(const edm::Paramete
         const auto &var_pset = var_info_pset.at(var_name);
         double median = var_pset.at("median");
         double norm_factor = var_pset.at("norm_factor");
-        double replace_inf_value =
-            var_pset.contains("replace_inf_value") ? double(var_pset.at("replace_inf_value")) : 0;
-        double lower_bound = var_pset.contains("lower_bound") ? double(var_pset.at("lower_bound")) : -5;
-        double upper_bound = var_pset.contains("upper_bound") ? double(var_pset.at("upper_bound")) : 5;
+        double replace_inf_value = var_pset.at("replace_inf_value");
+        double lower_bound = var_pset.at("lower_bound");
+        double upper_bound = var_pset.at("upper_bound");
         double pad = var_pset.contains("pad") ? double(var_pset.at("pad")) : 0;
         prep_params.var_info_map[var_name] =
             PreprocessParams::VarInfo(median, norm_factor, replace_inf_value, lower_bound, upper_bound, pad);
@@ -114,26 +113,18 @@ BoostedJetONNXJetTagsProducer::BoostedJetONNXJetTagsProducer(const edm::Paramete
       const auto &group_pset = prep_pset.getParameterSet(group_name);
       auto &prep_params = prep_info_map_[group_name];
       prep_params.var_names = group_pset.getParameter<std::vector<std::string>>("var_names");
-      if (group_pset.existsAs<unsigned>("var_length")) {
-        prep_params.min_length = group_pset.getParameter<unsigned>("var_length");
-        prep_params.max_length = prep_params.min_length;
-      } else {
-        prep_params.min_length = group_pset.getParameter<unsigned>("min_length");
-        prep_params.max_length = group_pset.getParameter<unsigned>("max_length");
-        input_shapes_.push_back({1, (int64_t)prep_params.var_names.size(), -1});
-      }
+      prep_params.min_length = group_pset.getParameter<unsigned>("var_length");
+      prep_params.max_length = prep_params.min_length;
       const auto &var_info_pset = group_pset.getParameterSet("var_infos");
       for (const auto &var_name : prep_params.var_names) {
         const auto &var_pset = var_info_pset.getParameterSet(var_name);
         double median = var_pset.getParameter<double>("median");
         double norm_factor = var_pset.getParameter<double>("norm_factor");
-        double replace_inf_value =
-            var_pset.exists("replace_inf_value") ? var_pset.getParameter<double>("replace_inf_value") : 0;
-        double lower_bound = var_pset.exists("lower_bound") ? var_pset.getParameter<double>("lower_bound") : -5;
-        double upper_bound = var_pset.exists("upper_bound") ? var_pset.getParameter<double>("upper_bound") : 5;
-        double pad = var_pset.exists("pad") ? var_pset.getParameter<double>("pad") : 0;
+        double replace_inf_value = var_pset.getParameter<double>("replace_inf_value");
+        double lower_bound = var_pset.getParameter<double>("lower_bound");
+        double upper_bound = var_pset.getParameter<double>("upper_bound");
         prep_params.var_info_map[var_name] =
-            PreprocessParams::VarInfo(median, norm_factor, replace_inf_value, lower_bound, upper_bound, pad);
+            PreprocessParams::VarInfo(median, norm_factor, replace_inf_value, lower_bound, upper_bound, 0);
       }
 
       // create data storage with a fixed size vector initiliazed w/ 0
@@ -180,6 +171,7 @@ void BoostedJetONNXJetTagsProducer::fillDescriptions(edm::ConfigurationDescripti
   // `preprocessParams` is deprecated -- use the preprocessing json file instead
   edm::ParameterSetDescription preprocessParams;
   preprocessParams.setAllowAnything();
+  preprocessParams.setComment("`preprocessParams` is deprecated, please use `preprocess_json` instead.");
   desc.addOptional<edm::ParameterSetDescription>("preprocessParams", preprocessParams);
   desc.add<edm::FileInPath>("model_path",
                             edm::FileInPath("RecoBTag/Combined/data/DeepBoostedJet/V02/full/resnet.onnx"));
