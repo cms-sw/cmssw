6bf3145d8bd955fc889962745acebc7aaf18c481
Mircho Rodozov <mrodozov@cern.ch>
2020-07-17 17:41:36 +0200

    clang format

--- DQMOffline/L1Trigger/src/HistDefinition.cc
+++ DQMOffline/L1Trigger/src/HistDefinition.cc
@@ -33,14 +33,14 @@ namespace dqmoffline {
       std::vector<unsigned int> map_values;
 
       map_values.reserve(mapping.size());
- for (auto const &imap : mapping) {
+      for (auto const &imap : mapping) {
         map_values.push_back(imap.second);
       }
       unsigned int max_size = *std::max_element(map_values.begin(), map_values.end());
       max_size = std::max(max_size, (unsigned int)mapping.size());
       definitions.resize(max_size);
 
-      for (const auto& name : names) {
+      for (const auto &name : names) {
         if (mapping.find(name) != mapping.end()) {
           const edm::ParameterSet &hd(ps.getParameter<edm::ParameterSet>(name));
           definitions[mapping.at(name)] = HistDefinition(hd);
