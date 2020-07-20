7637833a71048e0473dd1b9c176e4b31537476b9
Martin Grunewald <Martin.Grunewald@cern.ch>
2020-07-20 09:47:57 +0200

    HLT menu development

--- HLTrigger/Configuration/test/OnLine_HLT_PIon.py
+++ HLTrigger/Configuration/test/OnLine_HLT_PIon.py
@@ -1,13 +1,13 @@
 # hltGetConfiguration --full --data /dev/CMSSW_11_2_0/PIon --type PIon --unprescale --process HLTPIon --globaltag auto:run3_hlt_PIon --input file:RelVal_Raw_PIon_DATA.root
 
-# /dev/CMSSW_11_2_0/PIon/V3 (CMSSW_11_2_0_pre2)
+# /dev/CMSSW_11_2_0/PIon/V4 (CMSSW_11_2_0_pre2)
 
 import FWCore.ParameterSet.Config as cms
 
 process = cms.Process( "HLTPIon" )
 
 process.HLTConfigVersion = cms.PSet(
-  tableName = cms.string('/dev/CMSSW_11_2_0/PIon/V3')
+  tableName = cms.string('/dev/CMSSW_11_2_0/PIon/V4')
 )
 
 process.transferSystem = cms.PSet( 
