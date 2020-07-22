47421b82dfc66a58d6562b3ec1260696948eca63
Slava Krutelyov <slava77@gmail.com>
2020-07-22 09:18:20 -0700

    code-format

--- CondFormats/JetMETObjects/test/TestCorrections.C
+++ CondFormats/JetMETObjects/test/TestCorrections.C
@@ -4,9 +4,9 @@ void TestCorrections(double rawPt, double rawEta, double rawPhi, double rawE, do
   gSystem->Load("libFWCoreFWLite.so");
   FWLiteEnabler::enable();
   ////////////// Construct the JetCorrectorParameters objects ///////////////////////
-  string L1Tag    = "../data/Jec11V0_L1Offset_AK5JPT.txt"; 
-  string L1JPTTag = "../data/Jec11V0_L1JPTOffset_AK5JPT.txt"; 
-  JetCorrectorParameters *L1Par    = new JetCorrectorParameters(L1Tag);
+  string L1Tag = "../data/Jec11V0_L1Offset_AK5JPT.txt";
+  string L1JPTTag = "../data/Jec11V0_L1JPTOffset_AK5JPT.txt";
+  JetCorrectorParameters *L1Par = new JetCorrectorParameters(L1Tag);
   JetCorrectorParameters *L1JPTPar = new JetCorrectorParameters(L1JPTTag);
   vector<JetCorrectorParameters> vPar;
   vPar.push_back(*L1Par);
@@ -15,12 +15,12 @@ void TestCorrections(double rawPt, double rawEta, double rawPhi, double rawE, do
   FactorizedJetCorrector *JetCorrector = new FactorizedJetCorrector(vPar);
   ////////////// Loop over jets //////////////////////
   TLorentzVector rawJetP4;
-  rawJetP4.SetPtEtaPhiE(rawPt,rawEta,rawPhi,rawE);
-  JetCorrector->setJPTrawP4(rawJetP4); 
+  rawJetP4.SetPtEtaPhiE(rawPt, rawEta, rawPhi, rawE);
+  JetCorrector->setJPTrawP4(rawJetP4);
   JetCorrector->setJetE(JPTE);
   JetCorrector->setNPV(NPV);
   vector<float> vcor;
   vcor = JetCorrector->getSubCorrections();
-  cout<<"Correction applied to JPT jet after L1Offset = "<<vcor[0]<<endl;
-  cout<<"Correction applied to JPT jet after L1JPTOffset = "<<vcor[1]<<endl;
+  cout << "Correction applied to JPT jet after L1Offset = " << vcor[0] << endl;
+  cout << "Correction applied to JPT jet after L1JPTOffset = " << vcor[1] << endl;
 }
