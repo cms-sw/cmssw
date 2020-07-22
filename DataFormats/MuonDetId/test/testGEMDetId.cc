f7a460e8c054f6d491bcf1f35f4ed79a4e1a6ad0
Sunanda <sunanda.banerjee@cern.ch>
2020-07-22 22:08:43 +0200

    Add a test code which investigates ID issue for GEM

---
+++ DataFormats/MuonDetId/test/testGEMDetId.cc
@@ -0,0 +1,48 @@
+#include "DataFormats/MuonDetId/interface/GEMDetId.h"
+#include "DataFormats/DetId/interface/DetId.h"
+
+#include <iostream>
+
+void testCell() {
+  for (int re = GEMDetId::minRegionId; re <= GEMDetId::maxRegionId; ++re) {
+    if (re != 0) {
+      for (int ri = GEMDetId::minRingId; ri <= GEMDetId::maxRingId; ++ri) {
+	for (int st = GEMDetId::minStationId; st <= GEMDetId::maxStationId; ++st) {
+	  for (int la = GEMDetId::minLayerId; la <= GEMDetId::maxLayerId; ++la) {
+	    for (int ch = 1+GEMDetId::minChamberId; ch <= GEMDetId::maxChamberId; ++ch) {
+	      for (int ro = GEMDetId::minRollId; ro <= GEMDetId::maxRollId; ++ro) {
+		GEMDetId id(re, ri, st, la, ch, ro);
+		if ((id.region() != re) || (id.ring() != ri) ||
+		    (id.station() != st) || (id.layer() != la) ||
+		    (id.chamber() != ch) || (id.roll() != ro)) {
+		  std::cout << id << " should have been (" << re << ", " << ri
+			    << ", " << st << ", " << la << ", " << ch << ", "
+			    << ro << ") " << std::hex << id.rawId() << std::dec
+			    << " ***** ERROR *****" << std::endl;
+		}
+	      }
+	    }
+	  }
+	}
+      }
+    }
+  }
+}
+
+void testFail() {
+  unsigned int raw1(671189248), raw2(688425248);
+  GEMDetId id0(-1, 1, 1, 2, 10, 1);
+  GEMDetId id1(raw1);
+  GEMDetId id2(raw2);
+  std::cout << " ID0: " << std::hex << id0.rawId() << std::dec << ":" 
+	    << id0.rawId() << " " << id0 << "\n ID1: " << std::hex 
+	    << id1.rawId() << ":" << raw1 << std::dec << ":" << raw1 << " "
+	    << id1 << "\n ID2: " << std::hex << id2.rawId() << ":" 
+	    << raw2 << std::dec << ":" << raw2 << " " << id2 << std::endl;
+}
+
+int main() {
+  testFail();
+  testCell();
+  return 0;
+}
