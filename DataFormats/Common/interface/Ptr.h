65e1360e32760cde32a9a13484afc004d13a4f26
Matti Kortelainen <matti.kortelainen@cern.ch>
2020-07-25 03:34:55 +0200

    Work around clang5 in rootcling

--- DataFormats/Common/interface/Ptr.h
+++ DataFormats/Common/interface/Ptr.h
@@ -182,15 +182,15 @@ namespace edm {
         WrapperBase const* prod = getter->getIt(core_.id());
         unsigned int iKey = key_;
         if (prod == nullptr) {
-          std::optional optProd = getter->getThinnedProduct(core_.id(), key_);
-          if (not optProd.has_value()) {
+          auto optionalProd = getter->getThinnedProduct(core_.id(), key_);
+          if (not optionalProd.has_value()) {
             if (throwIfNotFound) {
               core_.productNotFoundException(typeid(T));
             } else {
               return;
             }
           }
-          std::tie(prod, iKey) = *optProd;
+          std::tie(prod, iKey) = *optionalProd;
         }
         void const* ad = nullptr;
         prod->setPtr(typeid(T), iKey, ad);
