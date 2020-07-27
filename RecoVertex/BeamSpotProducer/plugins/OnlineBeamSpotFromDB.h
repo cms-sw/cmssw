1e9ed8c3d156b887baba0a92f40733d0df144b99
simone gennai <simone.gennai@cern.ch>
2020-07-27 11:14:41 +0200

    added LogInfo and fillDescriptions to the OnlieBeamSpotFromDB

--- RecoVertex/BeamSpotProducer/plugins/OnlineBeamSpotFromDB.h
+++ RecoVertex/BeamSpotProducer/plugins/OnlineBeamSpotFromDB.h
@@ -16,15 +16,18 @@ ________________________________________________________________**/
 #include <string>
 // CMS
 #include "FWCore/Framework/interface/Frameworkfwd.h"
-#include "FWCore/Framework/interface/EDAnalyzer.h"
+#include "FWCore/Framework/interface/one/EDAnalyzer.h"
 #include "FWCore/Framework/interface/Event.h"
 #include "FWCore/ParameterSet/interface/ParameterSet.h"
+namespace edm{
+  class ParameterSetDescription;
 
-class OnlineBeamSpotFromDB : public edm::EDAnalyzer {
+}
+class OnlineBeamSpotFromDB : public edm::one::EDAnalyzer<> {
 public:
   explicit OnlineBeamSpotFromDB(const edm::ParameterSet&);
   ~OnlineBeamSpotFromDB() override;
-
+ static void fillDescription(edm::ParameterSetDescription& desc);
 private:
   void beginJob() override;
   void analyze(const edm::Event&, const edm::EventSetup&) override;
