dff13c0e80a2b7cca1c8b9022d504ffe64f29356
simone gennai <simone.gennai@cern.ch>
2020-07-27 11:39:58 +0200

    modified the read test python

--- RecoVertex/BeamSpotProducer/test/readOnlineBeamSpotFromDB.py
+++ RecoVertex/BeamSpotProducer/test/readOnlineBeamSpotFromDB.py
@@ -7,7 +7,7 @@ process.load("FWCore.MessageLogger.MessageLogger_cfi")
 process.load("CondCore.DBCommon.CondDBSetup_cfi")
 
 
-""" 
+
 process.BeamSpotDBSource = cms.ESSource("PoolDBESSource",
                                         process.CondDBSetup,
                                         toGet = cms.VPSet(
@@ -15,35 +15,36 @@ process.BeamSpotDBSource = cms.ESSource("PoolDBESSource",
 
 cms.PSet(
     record = cms.string('BeamSpotOnlineLegacyObjectsRcd'),
-    tag = cms.string("BSOnLineLegacy_tag"),
+    tag = cms.string("BeamSpotOnlineTestLegacy"),
     refreshTime = cms.uint64(1)
 
     ),
-cms.PSet(
-    record = cms.string('BeamSpotOnlineHLTObjectsRcd'),
-    tag = cms.string('BSOnLineHLT_tag'),
-    refreshTime = cms.uint64(1)
-    )
+#cms.PSet(
+#    record = cms.string('BeamSpotOnlineHLTObjectsRcd'),
+#    tag = cms.string('BSOnLineHLT_tag'),
+#    refreshTime = cms.uint64(1)
+#    )
 
 ),
-                                        connect = cms.string('sqlite_file:BeamSpotOnlineLegacy_JetHT.db')
-                                        #connect = cms.string('oracle://cms_orcoff_prod/CMS_COND_31X_BEAMSPOT')
+#                                        connect = cms.string('sqlite_file:BeamSpotOnlineLegacy_JetHT.db')
+                                        connect = cms.string('oracle://cms_orcon_prod/CMS_CONDITIONS')
                                         #connect = cms.string('frontier://PromptProd/CMS_COND_31X_BEAMSPOT')
                                         )
 
-"""
-from Configuration.AlCa.GlobalTag import GlobalTag as customiseGlobalTag
-process.GlobalTag = customiseGlobalTag(globaltag = "auto:run3_hlt_GRun")
-#process.BeamSpotESProducer = cms.ESProducer("OnlineBeamSpotESProducer")
-process.BeamSpotESProducer = cms.ESProducer("OfflineToTransientBeamSpotESProducer")
+#from Configuration.AlCa.GlobalTag import GlobalTag as customiseGlobalTag
+#process.GlobalTag = customiseGlobalTag(globaltag = "auto:run3_hlt_GRun")
+process.BeamSpotESProducer = cms.ESProducer("OnlineBeamSpotESProducer")
+#process.BeamSpotESProducer = cms.ESProducer("OfflineToTransientBeamSpotESProducer")
+process.MessageLogger.destinations = cms.untracked.vstring('detailedInfo')
+process.MessageLogger.detailedInfo   = cms.untracked.PSet(threshold = cms.untracked.string('INFO'))
 
 process.source = cms.Source("EmptySource")
-process.source.numberEventsInRun=cms.untracked.uint32(1)
-process.source.firstRun = cms.untracked.uint32(123741)
-process.source.firstLuminosityBlock = cms.untracked.uint32(1)
-process.source.numberEventsInLuminosityBlock = cms.untracked.uint32(2)
+process.source.numberEventsInRun=cms.untracked.uint32(10)
+process.source.firstRun = cms.untracked.uint32(336365)
+process.source.firstLuminosityBlock = cms.untracked.uint32(13)
+process.source.numberEventsInLuminosityBlock = cms.untracked.uint32(1)
 process.maxEvents = cms.untracked.PSet(
-            input = cms.untracked.int32(7)
+            input = cms.untracked.int32(200)
                     )
 process.beamspot = cms.EDAnalyzer("OnlineBeamSpotFromDB")
 
