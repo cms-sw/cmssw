44f7abfbe5dd8320d8b4eac4c2799c95a15d3e5d
simone gennai <simone.gennai@cern.ch>
2020-07-28 16:07:03 +0200

    addressing first set of comments from Matti

--- RecoVertex/BeamSpotProducer/plugins/OfflineToTransientBeamSpotESProducer.cc
+++ RecoVertex/BeamSpotProducer/plugins/OfflineToTransientBeamSpotESProducer.cc
@@ -1,4 +1,12 @@
-#include "RecoVertex/BeamSpotProducer/plugins/OfflineToTransientBeamSpotESProducer.h"
+#include "CondFormats/DataRecord/interface/BeamSpotObjectsRcd.h"
+#include "CondFormats/BeamSpotObjects/interface/BeamSpotObjects.h"
+#include "CondFormats/DataRecord/interface/BeamSpotTransientObjectsRcd.h"
+#include "FWCore/ParameterSet/interface/ConfigurationDescriptions.h"
+#include "FWCore/Framework/interface/ESProductHost.h"
+#include "FWCore/Utilities/interface/ReusableObjectHolder.h"
+#include "FWCore/Framework/interface/ESProducer.h"
+#include "FWCore/ParameterSet/interface/ParameterSet.h"
+
 #include "FWCore/Framework/interface/EventSetup.h"
 #include "FWCore/Framework/interface/ESHandle.h"
 #include "FWCore/Framework/interface/ModuleFactory.h"
@@ -11,6 +19,21 @@
 #include <string>
 
 using namespace edm;
+class OfflineToTransientBeamSpotESProducer : public edm::ESProducer {
+public:
+  OfflineToTransientBeamSpotESProducer(const edm::ParameterSet &p);
+  ~OfflineToTransientBeamSpotESProducer() override;
+  std::shared_ptr<const BeamSpotObjects> produce(const BeamSpotTransientObjectsRcd &);
+  static void fillDescriptions(edm::ConfigurationDescriptions &desc);
+
+private:
+  //const BeamSpotObjects* theOfflineBS_;
+  //const BeamSpotObjects *transientBS_ = nullptr;
+  const BeamSpotObjects dummyBS_;
+  edm::ESGetToken<BeamSpotObjects, BeamSpotTransientObjectsRcd> const bsToken_;
+  edm::ESGetToken<BeamSpotObjects, BeamSpotObjectsRcd> bsOfflineToken_;
+  
+};
 
 OfflineToTransientBeamSpotESProducer::OfflineToTransientBeamSpotESProducer(const edm::ParameterSet& p) {
   auto cc = setWhatProduced(this);
@@ -21,7 +44,7 @@ OfflineToTransientBeamSpotESProducer::OfflineToTransientBeamSpotESProducer(const
 OfflineToTransientBeamSpotESProducer::~OfflineToTransientBeamSpotESProducer() {
   //delete theOfflineBS_;
 }
-void OfflineToTransientBeamSpotESProducer::fillDescription(edm::ConfigurationDescriptions& desc) {
+void OfflineToTransientBeamSpotESProducer::fillDescriptions(edm::ConfigurationDescriptions& desc) {
   edm::ParameterSetDescription dsc;
   desc.addWithDefaultLabel(dsc);
 }
