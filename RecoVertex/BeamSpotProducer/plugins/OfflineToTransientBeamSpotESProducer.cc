ade1e1691a3e4f7a9d72bd4ee493ea23cca98a43
simone gennai <simone.gennai@cern.ch>
2020-07-28 09:58:54 +0200

    modified the OfflineToTransient ESProducer following Matti's suggestions

--- RecoVertex/BeamSpotProducer/plugins/OfflineToTransientBeamSpotESProducer.cc
+++ RecoVertex/BeamSpotProducer/plugins/OfflineToTransientBeamSpotESProducer.cc
@@ -1,46 +1,59 @@
-#include "RecoVertex/BeamSpotProducer/plugins/OfflineToTransientBeamSpotESProducer.h"
+#include "CondFormats/DataRecord/interface/BeamSpotObjectsRcd.h"
+#include "CondFormats/BeamSpotObjects/interface/BeamSpotObjects.h"
+#include "CondFormats/DataRecord/interface/BeamSpotTransientObjectsRcd.h"
+#include "FWCore/ParameterSet/interface/ConfigurationDescriptions.h"
+#include "FWCore/Framework/interface/ESProductHost.h"
+#include "FWCore/Utilities/interface/ReusableObjectHolder.h"
+#include "FWCore/Framework/interface/ESProducer.h"
+#include "FWCore/ParameterSet/interface/ParameterSet.h"
+
 #include "FWCore/Framework/interface/EventSetup.h"
 #include "FWCore/Framework/interface/ESHandle.h"
 #include "FWCore/Framework/interface/ModuleFactory.h"
 #include "FWCore/Framework/interface/ESProducer.h"
 #include "FWCore/Utilities/interface/do_nothing_deleter.h"
+#include "FWCore/ParameterSet/interface/ParameterSetDescription.h"
 
 #include <iostream>
 #include <memory>
 #include <string>
 
 using namespace edm;
+class OfflineToTransientBeamSpotESProducer : public edm::ESProducer {
+public:
+  OfflineToTransientBeamSpotESProducer(const edm::ParameterSet& p);
+  ~OfflineToTransientBeamSpotESProducer() override;
+  std::shared_ptr<const BeamSpotObjects> produce(const BeamSpotTransientObjectsRcd&);
+  static void fillDescriptions(edm::ConfigurationDescriptions& desc);
+
+private:
+  //const BeamSpotObjects* theOfflineBS_;
+  //const BeamSpotObjects *transientBS_ = nullptr;
+  const BeamSpotObjects dummyBS_;
+  edm::ESGetToken<BeamSpotObjects, BeamSpotTransientObjectsRcd> const bsToken_;
+  edm::ESGetToken<BeamSpotObjects, BeamSpotObjectsRcd> bsOfflineToken_;
+};
 
 OfflineToTransientBeamSpotESProducer::OfflineToTransientBeamSpotESProducer(const edm::ParameterSet& p) {
   auto cc = setWhatProduced(this);
 
-  transientBS_ = new BeamSpotObjects;
-  //theOfflineBS_ = new BeamSpotObjects;
   bsOfflineToken_ = cc.consumesFrom<BeamSpotObjects, BeamSpotObjectsRcd>();
 }
 
 OfflineToTransientBeamSpotESProducer::~OfflineToTransientBeamSpotESProducer() {
-  delete transientBS_;
   //delete theOfflineBS_;
 }
-
+void OfflineToTransientBeamSpotESProducer::fillDescriptions(edm::ConfigurationDescriptions& desc) {
+  edm::ParameterSetDescription dsc;
+  desc.addWithDefaultLabel(dsc);
+}
 std::shared_ptr<const BeamSpotObjects> OfflineToTransientBeamSpotESProducer::produce(
     const BeamSpotTransientObjectsRcd& iRecord) {
-  if (!(iRecord.tryToGetRecord<BeamSpotObjectsRcd>())) {
-    //Missing offline record????
-
-    return std::shared_ptr<const BeamSpotObjects>(&(*transientBS_), edm::do_nothing_deleter());
+  auto optionalRec = iRecord.tryToGetRecord<BeamSpotObjectsRcd>();
+  if (not optionalRec) {
+    return std::shared_ptr<const BeamSpotObjects>(&dummyBS_, edm::do_nothing_deleter());
   }
-
-  auto host = holder_.makeOrGet([]() { return new HostType; });
-
-  if (iRecord.tryToGetRecord<BeamSpotObjectsRcd>()) {
-    host->ifRecordChanges<BeamSpotObjectsRcd>(
-        iRecord, [this, h = host.get()](auto const& rec) { transientBS_ = &rec.get(bsOfflineToken_); });
-  }
-
-
-  return std::shared_ptr<const BeamSpotObjects>(&(*transientBS_), edm::do_nothing_deleter());
+  return std::shared_ptr<const BeamSpotObjects>(&optionalRec->get(bsOfflineToken_), edm::do_nothing_deleter());
 };
 
 DEFINE_FWK_EVENTSETUP_MODULE(OfflineToTransientBeamSpotESProducer);
