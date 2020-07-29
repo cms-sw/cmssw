2adff6a6064b34dfdd62d347dad09b8ee1f10787
simone gennai <simone.gennai@cern.ch>
2020-07-29 11:55:30 +0200

    resolve conflict

--- RecoVertex/BeamSpotProducer/plugins/OfflineToTransientBeamSpotESProducer.h
+++ RecoVertex/BeamSpotProducer/plugins/OfflineToTransientBeamSpotESProducer.h
@@ -5,7 +5,6 @@
 #include "CondFormats/BeamSpotObjects/interface/BeamSpotObjects.h"
 #include "CondFormats/DataRecord/interface/BeamSpotTransientObjectsRcd.h"
 
-
 #include "FWCore/Framework/interface/ESProductHost.h"
 #include "FWCore/Utilities/interface/ReusableObjectHolder.h"
 #include "FWCore/Framework/interface/ESProducer.h"
@@ -17,20 +16,15 @@ public:
   OfflineToTransientBeamSpotESProducer(const edm::ParameterSet &p);
   ~OfflineToTransientBeamSpotESProducer() override;
   std::shared_ptr<const BeamSpotObjects> produce(const BeamSpotTransientObjectsRcd &);
- 
-  
 
 private:
-//const BeamSpotObjects* theOfflineBS_;
-  const BeamSpotObjects* transientBS_;
+  //const BeamSpotObjects* theOfflineBS_;
+  const BeamSpotObjects *transientBS_;
   edm::ESGetToken<BeamSpotObjects, BeamSpotTransientObjectsRcd> const bsToken_;
   edm::ESGetToken<BeamSpotObjects, BeamSpotObjectsRcd> bsOfflineToken_;
-  using HostType = edm::ESProductHost<BeamSpotObjects,BeamSpotObjectsRcd>;                                        
+  using HostType = edm::ESProductHost<BeamSpotObjects, BeamSpotObjectsRcd>;
 
   edm::ReusableObjectHolder<HostType> holder_;
-
-  
-
 };
 
 #endif
