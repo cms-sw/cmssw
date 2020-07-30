ce4572f4b90ef7f7d9c4eb63d811f0b0f64ccb74
Vladimir <Vladimir.Ivantchenko@cern.ch>
2020-07-30 13:04:55 +0200

    gflash update

--- SimG4CMS/Calo/interface/ECalSD.h
+++ SimG4CMS/Calo/interface/ECalSD.h
@@ -16,7 +16,7 @@
 #include "DataFormats/DetId/interface/DetId.h"
 #include "FWCore/MessageLogger/interface/MessageLogger.h"
 
-#include "G4String.hh"
+//#include "G4String.hh"
 #ifdef plotDebug
 #include <TH2F.h>
 #endif
@@ -42,6 +42,7 @@ protected:
   double getEnergyDeposit(const G4Step *) override;
   int getTrackID(const G4Track *) override;
   uint16_t getDepth(const G4Step *) override;
+  double EnergyCorrected(const G4Step&, const G4Track*) override;
 
 private:
   void initMap();
