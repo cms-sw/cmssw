7185c701f373fafce7fbc72a058e230d11f463d1
peruzzim <peruzzim@users.noreply.github.com>
2020-07-31 16:27:39 +0200

    Forced update of files, cumulative pr 491 505 506

--- DataFormats/NanoAOD/interface/MergeableCounterTable.h
+++ DataFormats/NanoAOD/interface/MergeableCounterTable.h
@@ -4,115 +4,190 @@
 #include "FWCore/Utilities/interface/Exception.h"
 #include <vector>
 #include <string>
+#include <algorithm>
 
 namespace nanoaod {
 
-class MergeableCounterTable {
-    public:
-        MergeableCounterTable() {}
-        typedef long long int_accumulator; // we accumulate in long long int, to avoid overflow
-        typedef double float_accumulator; // we accumulate in double, to preserve precision
+  class MergeableCounterTable {
+  public:
+    MergeableCounterTable() {}
+    typedef long long int_accumulator;  // we accumulate in long long int, to avoid overflow
+    typedef double float_accumulator;   // we accumulate in double, to preserve precision
 
-        template<typename T>
-        struct SingleColumn {
-            typedef T value_type;
-            SingleColumn() {}
-            SingleColumn(const std::string & aname, const std::string & adoc, T avalue = T()) : name(aname), doc(adoc), value(avalue) {}
-            std::string name, doc;
-            T value;
-            void operator+=(const SingleColumn<T> & other) {
-                //// if one arrives here from tryMerge the checks are already done in the compatible() function before.
-                //// you may however want to enable these and remove the 'return false' in tryMerge in order to see what's incompatible between the tables.
-                //if (name != other.name) throw cms::Exception("LogicError", "Trying to merge "+name+" with "+other.name+"\n");
-                value += other.value;
-            }
-            bool compatible(const SingleColumn<T> & other) {
-                return name == other.name;  // we don't check the doc, not needed
-            }
-        };
-        typedef SingleColumn<float_accumulator> FloatColumn; 
-        typedef SingleColumn<int_accumulator> IntColumn;  
-       
-        template<typename T>
-        struct VectorColumn {
-            typedef T element_type;
-            VectorColumn() {}
-            VectorColumn(const std::string & aname, const std::string & adoc, unsigned int size) : name(aname), doc(adoc), values(size, T()) {}
-            VectorColumn(const std::string & aname, const std::string & adoc, const std::vector<T> & somevalues) : name(aname), doc(adoc), values(somevalues) {}
-            std::string name, doc;
-            std::vector<T> values;
-            void operator+=(const VectorColumn<T> & other) {
-                //// if one arrives here from tryMerge the checks are already done in the compatible() function before.
-                //// you may however want to enable these and remove the 'return false' in tryMerge in order to see what's incompatible between the tables.
-                //if (name != other.name) throw cms::Exception("LogicError", "Trying to merge "+name+" with "+other.name+"\n");
-                //if (values.size() != other.values.size()) throw cms::Exception("LogicError", "Trying to merge "+name+" with different number of values!\n");
-                for (unsigned int i = 0, n = values.size(); i < n; ++i) {
-                    values[i] += other.values[i];
-                }
-            }
-            bool compatible(const VectorColumn<T> & other) {
-                return name == other.name && values.size() == other.values.size(); // we don't check the doc, not needed
-            }
-        };
-        typedef VectorColumn<float_accumulator> VFloatColumn; 
-        typedef VectorColumn<int_accumulator> VIntColumn;
- 
-        const std::vector<FloatColumn> & floatCols() const { return floatCols_; }
-        const std::vector<VFloatColumn> & vfloatCols() const { return vfloatCols_; }
-        const std::vector<IntColumn> & intCols() const { return intCols_; }
-        const std::vector<VIntColumn> & vintCols() const { return vintCols_; }
-       
-        template<typename F>
-        void addFloat(const std::string & name, const std::string & doc, F value) { floatCols_.push_back(FloatColumn(name, doc, value)); } 
+    template <typename T>
+    struct SingleColumn {
+      typedef T value_type;
+      SingleColumn() {}
+      SingleColumn(const std::string& aname, const std::string& adoc, T avalue = T())
+          : name(aname), doc(adoc), value(avalue) {}
+      std::string name, doc;
+      T value;
+      void operator+=(const SingleColumn<T>& other) {
+        if (!compatible(other))
+          throw cms::Exception("LogicError",
+                               "Trying to merge " + name + " with " + other.name + " failed compatibility test.\n");
+        value += other.value;
+      }
+      bool compatible(const SingleColumn<T>& other) {
+        return name == other.name;  // we don't check the doc, not needed
+      }
+    };
+    typedef SingleColumn<float_accumulator> FloatColumn;
+    typedef SingleColumn<int_accumulator> IntColumn;
 
-        template<typename I>
-        void addInt(const std::string & name, const std::string & doc, I value) { intCols_.push_back(IntColumn(name, doc, value)); } 
+    template <typename T>
+    struct SingleWithNormColumn : SingleColumn<T> {
+      SingleWithNormColumn() { norm = 0; }
+      SingleWithNormColumn(const std::string& aname, const std::string& adoc, T avalue = T(), const double anorm = 0)
+          : SingleColumn<T>(aname, adoc, avalue), norm(anorm) {}
+      double norm;
+      void operator+=(const SingleWithNormColumn<T>& other) {
+        if (!this->compatible(other))
+          throw cms::Exception(
+              "LogicError", "Trying to merge " + this->name + " with " + other.name + " failed compatibility test.\n");
+        auto newNorm = norm + other.norm;
+        this->value = (newNorm != 0) ? (this->value * norm + other.value * other.norm) / newNorm : 0;
+        norm = newNorm;
+      }
+    };
+    typedef SingleWithNormColumn<float_accumulator> FloatWithNormColumn;
 
-        template<typename F>
-        void addVFloat(const std::string & name, const std::string & doc, const std::vector<F> values) { 
-            vfloatCols_.push_back(VFloatColumn(name, doc, values.size())); 
-            std::copy(values.begin(), values.end(), vfloatCols_.back().values.begin());
-        } 
-
-        template<typename I>
-        void addVInt(const std::string & name, const std::string & doc, const std::vector<I> values) { 
-            vintCols_.push_back(VIntColumn(name, doc, values.size())); 
-            std::copy(values.begin(), values.end(), vintCols_.back().values.begin());
-        } 
-
-
-        bool mergeProduct(const MergeableCounterTable & other) {
-            if (!tryMerge(intCols_, other.intCols_)) return false;
-            if (!tryMerge(vintCols_, other.vintCols_)) return false;
-            if (!tryMerge(floatCols_, other.floatCols_)) return false;
-            if (!tryMerge(vfloatCols_, other.vfloatCols_)) return false;
-            return true; 
+    template <typename T>
+    struct VectorColumn {
+      typedef T element_type;
+      VectorColumn() {}
+      VectorColumn(const std::string& aname, const std::string& adoc, unsigned int size)
+          : name(aname), doc(adoc), values(size, T()) {}
+      VectorColumn(const std::string& aname, const std::string& adoc, const std::vector<T>& somevalues)
+          : name(aname), doc(adoc), values(somevalues) {}
+      std::string name, doc;
+      std::vector<T> values;
+      void operator+=(const VectorColumn<T>& other) {
+        if (!compatible(other))
+          throw cms::Exception("LogicError",
+                               "Trying to merge " + name + " with " + other.name + " failed compatibility test.\n");
+        for (unsigned int i = 0, n = values.size(); i < n; ++i) {
+          values[i] += other.values[i];
         }
+      }
+      bool compatible(const VectorColumn<T>& other) {
+        return name == other.name && values.size() == other.values.size();  // we don't check the doc, not needed
+      }
+    };
+    typedef VectorColumn<float_accumulator> VFloatColumn;
+    typedef VectorColumn<int_accumulator> VIntColumn;
 
-        void swap(MergeableCounterTable& iOther) {
-          floatCols_.swap(iOther.floatCols_);
-          vfloatCols_.swap(iOther.vfloatCols_);
-          intCols_.swap(iOther.intCols_);
-          vintCols_.swap(iOther.vintCols_);
+    template <typename T>
+    struct VectorWithNormColumn : VectorColumn<T> {
+      double norm;
+      VectorWithNormColumn() { norm = 0; }
+      VectorWithNormColumn(const std::string& aname, const std::string& adoc, unsigned int size, double anorm = 0)
+          : VectorColumn<T>(aname, adoc, size), norm(anorm) {}
+      VectorWithNormColumn(const std::string& aname,
+                           const std::string& adoc,
+                           const std::vector<T>& somevalues,
+                           double anorm = 0)
+          : VectorColumn<T>(aname, adoc, somevalues), norm(anorm) {}
+      void operator+=(const VectorWithNormColumn<T>& other) {
+        if (!this->compatible(other))
+          throw cms::Exception(
+              "LogicError", "Trying to merge " + this->name + " with " + other.name + " failed compatibility test.\n");
+        auto newNorm = norm + other.norm;
+        for (unsigned int i = 0, n = this->values.size(); i < n; ++i) {
+          this->values[i] =
+              (newNorm != 0) ? (this->values[i] * norm + other.values[i] * other.norm) / (norm + other.norm) : 0;
         }
+        norm = newNorm;
+      }
+    };
+    typedef VectorWithNormColumn<float_accumulator> VFloatWithNormColumn;
 
-   private:
-        std::vector<FloatColumn> floatCols_;
-        std::vector<VFloatColumn> vfloatCols_;
-        std::vector<IntColumn> intCols_;
-        std::vector<VIntColumn> vintCols_;
+    const std::vector<FloatColumn>& floatCols() const { return floatCols_; }
+    const std::vector<VFloatColumn>& vfloatCols() const { return vfloatCols_; }
+    const std::vector<FloatWithNormColumn>& floatWithNormCols() const { return floatWithNormCols_; }
+    const std::vector<VFloatWithNormColumn>& vfloatWithNormCols() const { return vfloatWithNormCols_; }
+    const std::vector<IntColumn>& intCols() const { return intCols_; }
+    const std::vector<VIntColumn>& vintCols() const { return vintCols_; }
 
-        template<typename T>
-        bool tryMerge(std::vector<T> & one, const std::vector<T> & two) {
-            if (one.size() != two.size()) return false;
-            for (unsigned int i = 0, n = one.size(); i < n; ++i) {
-                if (!one[i].compatible(two[i])) return false;
-                one[i] += two[i];
-            }
-            return true;
-        }
-};
+    template <typename F>
+    void addFloat(const std::string& name, const std::string& doc, F value) {
+      floatCols_.push_back(FloatColumn(name, doc, value));
+    }
 
-} // namespace nanoaod
+    template <typename F>
+    void addFloatWithNorm(const std::string& name, const std::string& doc, F value, double norm) {
+      floatWithNormCols_.push_back(FloatWithNormColumn(name, doc, value, norm));
+    }
+
+    template <typename I>
+    void addInt(const std::string& name, const std::string& doc, I value) {
+      intCols_.push_back(IntColumn(name, doc, value));
+    }
+
+    template <typename F>
+    void addVFloat(const std::string& name, const std::string& doc, const std::vector<F> values) {
+      vfloatCols_.push_back(VFloatColumn(name, doc, values.size()));
+      std::copy(values.begin(), values.end(), vfloatCols_.back().values.begin());
+    }
+
+    template <typename F>
+    void addVFloatWithNorm(const std::string& name, const std::string& doc, const std::vector<F> values, double norm) {
+      vfloatWithNormCols_.push_back(VFloatWithNormColumn(name, doc, values.size(), norm));
+      std::copy(values.begin(), values.end(), vfloatWithNormCols_.back().values.begin());
+    }
+
+    template <typename I>
+    void addVInt(const std::string& name, const std::string& doc, const std::vector<I> values) {
+      vintCols_.push_back(VIntColumn(name, doc, values.size()));
+      std::copy(values.begin(), values.end(), vintCols_.back().values.begin());
+    }
+
+    bool mergeProduct(const MergeableCounterTable& other) {
+      if (!tryMerge(intCols_, other.intCols_))
+        return false;
+      if (!tryMerge(vintCols_, other.vintCols_))
+        return false;
+      if (!tryMerge(floatCols_, other.floatCols_))
+        return false;
+      if (!tryMerge(vfloatCols_, other.vfloatCols_))
+        return false;
+      if (!tryMerge(floatWithNormCols_, other.floatWithNormCols_))
+        return false;
+      if (!tryMerge(vfloatWithNormCols_, other.vfloatWithNormCols_))
+        return false;
+      return true;
+    }
+
+    void swap(MergeableCounterTable& iOther) {
+      floatCols_.swap(iOther.floatCols_);
+      vfloatCols_.swap(iOther.vfloatCols_);
+      floatWithNormCols_.swap(iOther.floatWithNormCols_);
+      vfloatWithNormCols_.swap(iOther.vfloatWithNormCols_);
+      intCols_.swap(iOther.intCols_);
+      vintCols_.swap(iOther.vintCols_);
+    }
+
+  private:
+    std::vector<FloatColumn> floatCols_;
+    std::vector<VFloatColumn> vfloatCols_;
+    std::vector<FloatWithNormColumn> floatWithNormCols_;
+    std::vector<VFloatWithNormColumn> vfloatWithNormCols_;
+    std::vector<IntColumn> intCols_;
+    std::vector<VIntColumn> vintCols_;
+
+    template <typename T>
+    bool tryMerge(std::vector<T>& one, const std::vector<T>& two) {
+      for (auto y : two) {
+        auto x = std::find_if(one.begin(), one.end(), [&y](const T& x) { return x.name == y.name; });
+        if (x == one.end())
+          one.push_back(y);
+        else
+          (*x) += y;
+      }
+      return true;
+    }
+  };
+
+}  // namespace nanoaod
 
 #endif
--- PhysicsTools/NanoAOD/plugins/SummaryTableOutputBranches.h
+++ PhysicsTools/NanoAOD/plugins/SummaryTableOutputBranches.h
@@ -10,44 +10,54 @@
 #include "FWCore/Utilities/interface/EDGetToken.h"
 
 class SummaryTableOutputBranches {
- public:
-    SummaryTableOutputBranches(const edm::BranchDescription *desc, const edm::EDGetToken & token ) :
-        m_token(token), m_branchesBooked(false)
-    {
-        if (desc->className() != "nanoaod::MergeableCounterTable") throw cms::Exception("Configuration", "NanoAODOutputModule can only write out MergableCounterTable objects");
-    }
+public:
+  SummaryTableOutputBranches(const edm::BranchDescription *desc, const edm::EDGetToken &token)
+      : m_token(token), m_fills(0) {
+    if (desc->className() != "nanoaod::MergeableCounterTable")
+      throw cms::Exception("Configuration", "NanoAODOutputModule can only write out MergableCounterTable objects");
+  }
 
-    void fill(const edm::OccurrenceForOutput &iWhatever, TTree & tree) ;
+  void fill(const edm::OccurrenceForOutput &iWhatever, TTree &tree);
 
- private:
-    edm::EDGetToken m_token;
+private:
+  edm::EDGetToken m_token;
 
-    struct NamedBranchPtr {
-        std::string name;
-        TBranch * branch;
-        NamedBranchPtr(const std::string & aname, TBranch *branchptr = nullptr) : 
-            name(aname), branch(branchptr) {}
-    };
-    std::vector<NamedBranchPtr> m_intBranches, m_floatBranches;
+  struct NamedBranchPtr {
+    std::string name;
+    TBranch *branch;
+    NamedBranchPtr(const std::string &aname, TBranch *branchptr = nullptr) : name(aname), branch(branchptr) {}
+  };
+  std::vector<NamedBranchPtr> m_intBranches, m_floatBranches, m_floatWithNormBranches;
 
-    struct NamedVectorBranchPtr : public NamedBranchPtr {
-        UInt_t count;
-        TBranch * counterBranch;
-        NamedVectorBranchPtr(const std::string & aname, TBranch *counterBranchptr = nullptr, TBranch *valueBranchptr = nullptr) : 
-            NamedBranchPtr(aname,valueBranchptr), counterBranch(counterBranchptr) {}
-    };
-    std::vector<NamedVectorBranchPtr> m_vintBranches, m_vfloatBranches;
+  struct NamedVectorBranchPtr : public NamedBranchPtr {
+    UInt_t count;
+    TBranch *counterBranch;
+    NamedVectorBranchPtr(const std::string &aname,
+                         TBranch *counterBranchptr = nullptr,
+                         TBranch *valueBranchptr = nullptr)
+        : NamedBranchPtr(aname, valueBranchptr), counterBranch(counterBranchptr) {}
+  };
+  std::vector<NamedVectorBranchPtr> m_vintBranches, m_vfloatBranches, m_vfloatWithNormBranches;
 
-    bool m_branchesBooked;
+  unsigned long m_fills;
 
-    void defineBranchesFromFirstEvent(const nanoaod::MergeableCounterTable & tab, TTree & tree) ;
+  void updateBranches(const nanoaod::MergeableCounterTable &tab, TTree &tree);
 
-    template<typename Col> void makeScalarBranches(const std::vector<Col> & tabcols, TTree & tree, const std::string & rootType, std::vector<NamedBranchPtr> & branches);
-    template<typename Col> void makeVectorBranches(const std::vector<Col> & tabcols, TTree & tree, const std::string & rootType, std::vector<NamedVectorBranchPtr> & branches );
+  template <typename T, typename Col>
+  void makeScalarBranches(const std::vector<Col> &tabcols,
+                          TTree &tree,
+                          const std::string &rootType,
+                          std::vector<NamedBranchPtr> &branches);
+  template <typename Col>
+  void makeVectorBranches(const std::vector<Col> &tabcols,
+                          TTree &tree,
+                          const std::string &rootType,
+                          std::vector<NamedVectorBranchPtr> &branches);
 
-    template<typename Col> void fillScalarBranches(const std::vector<Col> & tabcols, std::vector<NamedBranchPtr> & branches);
-    template<typename Col> void fillVectorBranches(const std::vector<Col> & tabcols, std::vector<NamedVectorBranchPtr> & branches );
+  template <typename Col>
+  void fillScalarBranches(const std::vector<Col> &tabcols, std::vector<NamedBranchPtr> &branches);
+  template <typename Col>
+  void fillVectorBranches(const std::vector<Col> &tabcols, std::vector<NamedVectorBranchPtr> &branches);
 };
 
 #endif
-
