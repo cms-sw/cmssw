eaf90d1fca00645d89de8c94acc62ff571c40689
Matti Kortelainen <matti.kortelainen@cern.ch>
2020-07-31 17:25:44 +0200

    Remove empty destructor

--- SimMuon/MCTruth/plugins/MuonTrackProducer.cc
+++ SimMuon/MCTruth/plugins/MuonTrackProducer.cc
@@ -27,7 +27,6 @@
 class MuonTrackProducer : public edm::global::EDProducer<> {
 public:
   explicit MuonTrackProducer(const edm::ParameterSet &);
-  ~MuonTrackProducer() override;
 
 private:
   void produce(edm::StreamID, edm::Event &, const edm::EventSetup &) const override;
@@ -57,8 +56,6 @@ MuonTrackProducer::MuonTrackProducer(const edm::ParameterSet &parset)
   produces<reco::TrackExtraCollection>();
   produces<TrackingRecHitCollection>();
 }
-
-MuonTrackProducer::~MuonTrackProducer() {}
 
 void MuonTrackProducer::produce(edm::StreamID, edm::Event &iEvent, const edm::EventSetup &iSetup) const {
   edm::Handle<reco::MuonCollection> muonCollectionH = iEvent.getHandle(muonsToken);
