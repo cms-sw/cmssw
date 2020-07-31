cad33ece1be7cfda429687d2edfb544a34cbf422
Giacomo Govi <giacomo.govi@cern.ch>
2020-07-31 14:39:02 +0200

    Code-checks + code-format on a new file ( not sure how to have this executed at the same time as the existing files...

--- CondCore/CondDB/plugins/CondDBPyBind11Wrappers.cc
+++ CondCore/CondDB/plugins/CondDBPyBind11Wrappers.cc
@@ -5,18 +5,21 @@
 
 namespace cond {
 
-  std::pair<std::string, std::string> getDbCredentials(const std::string& connectionString, bool updateMode, const std::string& authPath){
+  std::pair<std::string, std::string> getDbCredentials(const std::string& connectionString,
+                                                       bool updateMode,
+                                                       const std::string& authPath) {
     std::string ap = authPath;
-    if(ap==""){
+    if (ap.empty()) {
       ap = std::string(std::getenv(cond::auth::COND_AUTH_PATH));
     }
-    auto ret = std::make_pair(std::string(""),std::string(""));
-    if(!ap.empty()){ 
+    auto ret = std::make_pair(std::string(""), std::string(""));
+    if (!ap.empty()) {
       CredentialStore credDb;
-      credDb.setUpForConnectionString( connectionString, ap );
+      credDb.setUpForConnectionString(connectionString, ap);
       std::string role(cond::auth::COND_READER_ROLE);
-      if( updateMode ) role = cond::auth::COND_WRITER_ROLE;
-      ret = credDb.getUserCredentials( connectionString, role );
+      if (updateMode)
+        role = cond::auth::COND_WRITER_ROLE;
+      ret = credDb.getUserCredentials(connectionString, role);
     }
     return ret;
   }
@@ -30,4 +33,3 @@ namespace py = pybind11;
 PYBIND11_MODULE(pluginCondDBPyBind11Interface, m) {
   m.def("get_db_credentials", &cond::getDbCredentials, "Get db credentials for a connection string");
 }
-
