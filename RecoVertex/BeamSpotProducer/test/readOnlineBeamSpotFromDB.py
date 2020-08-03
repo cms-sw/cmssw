851cd6257c627871d4899fc29220b543661ab7af
simone gennai <simone.gennai@cern.ch>
2020-08-03 12:31:36 +0200

    removing un-needed commented lines

--- RecoVertex/BeamSpotProducer/test/readOnlineBeamSpotFromDB.py
+++ RecoVertex/BeamSpotProducer/test/readOnlineBeamSpotFromDB.py
@@ -7,34 +7,34 @@ process.load("FWCore.MessageLogger.MessageLogger_cfi")
 process.load("CondCore.DBCommon.CondDBSetup_cfi")
 
 
-"""
-process.BeamSpotDBSource = cms.ESSource("PoolDBESSource",
+tesOnLineESProducer = True
+
+if tesOnLineESProducer:
+    process.BeamSpotDBSource = cms.ESSource("PoolDBESSource",
                                         process.CondDBSetup,
                                         toGet = cms.VPSet(
+                                            cms.PSet(
+                                                record = cms.string('BeamSpotOnlineLegacyObjectsRcd'),
+                                                tag = cms.string("BeamSpotOnlineTestLegacy"),
+                                                refreshTime = cms.uint64(1)
 
+                                            ),
+                                            #cms.PSet(
+                                            #    record = cms.string('BeamSpotOnlineHLTObjectsRcd'),
+                                            #    tag = cms.string('BSOnLineHLT_tag'),
+                                            #    refreshTime = cms.uint64(1)
+                                            #    )
 
-cms.PSet(
-    record = cms.string('BeamSpotOnlineLegacyObjectsRcd'),
-    tag = cms.string("BeamSpotOnlineTestLegacy"),
-    refreshTime = cms.uint64(1)
-
-    ),
-#cms.PSet(
-#    record = cms.string('BeamSpotOnlineHLTObjectsRcd'),
-#    tag = cms.string('BSOnLineHLT_tag'),
-#    refreshTime = cms.uint64(1)
-#    )
-
-),
-#                                        connect = cms.string('sqlite_file:BeamSpotOnlineLegacy_JetHT.db')
+                                ),
                                         connect = cms.string('oracle://cms_orcon_prod/CMS_CONDITIONS')
-                                        #connect = cms.string('frontier://PromptProd/CMS_COND_31X_BEAMSPOT')
+
                                         )
-process.BeamSpotESProducer = cms.ESProducer("OnlineBeamSpotESProducer")
-"""
-from Configuration.AlCa.GlobalTag import GlobalTag as customiseGlobalTag
-process.GlobalTag = customiseGlobalTag(globaltag = "auto:run3_hlt_GRun")
-process.BeamSpotESProducer = cms.ESProducer("OfflineToTransientBeamSpotESProducer")
+    process.BeamSpotESProducer = cms.ESProducer("OnlineBeamSpotESProducer")
+
+else:
+    from Configuration.AlCa.GlobalTag import GlobalTag as customiseGlobalTag
+    process.GlobalTag = customiseGlobalTag(globaltag = "auto:run3_hlt_GRun")
+    process.BeamSpotESProducer = cms.ESProducer("OfflineToTransientBeamSpotESProducer")
 
 
 process.MessageLogger.destinations = cms.untracked.vstring('detailedInfo')
