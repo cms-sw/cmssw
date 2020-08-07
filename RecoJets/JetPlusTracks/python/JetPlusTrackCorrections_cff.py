9db69c5bf3b4c547d69b82e91e2f403c11ae99e6
kodolova <Olga.Kodolova@cern.ch>
2020-08-07 11:15:02 +0300

    Trackjets as seeds of JetPlusTrack at RECO and MINIAOD (#30430)
    * TrackJet seeds adding
    
    * TrackJet seeds adding
    
    * TrackJet seeds adding
    
    * TrackJet seeds adding
    
    * TrackJet seeding
    
    * Make cleaning, i.e. change comaprison of collection size with 0 to empty()
    clean Buildfile.xml
    
    * Applied patch https://cmssdt.cern.ch/SDT/code-checks/cms-sw-PR-30160/15988/code-format.patch
    e.g. curl https://cmssdt.cern.ch/SDT/code-checks/cms-sw-PR-30160/15988/code-format.patch | patch -p1
    You can also run scram build code-format
    Provide some additional clean-up: introduce variable dRcone instead of 0.4
    
    * Add missed flag in configurator
    
    * Remove *.orig *.rej
    
    * Add deltaR method, change hardcoded cuts to configurable
    
    * Changes proposed by code-format
    
    * Chnage to deltaR and deltaPhi
    
    * Code-formatting after changes
    
    * Changes according new rules
    
    * Removing RecoJets/JetPlusTracks/plugins/TrackFromPFProducer
    
    * code-format
    
    * Change to auto for variables, code format, changes in python
    
    * Add PAT libraries
    
    * Make changes in PATJetPlusTrackCorrections_cff.py and additional cleaning
    
    * Code-formatting
    
    * Format changes
    
    * Adding missing configurator
    
    * Add test configurator
    
    * Change output root file
    
    * Change in PAT JetPlusTrackProducer python

--- RecoJets/JetPlusTracks/python/JetPlusTrackCorrections_cff.py
+++ RecoJets/JetPlusTracks/python/JetPlusTrackCorrections_cff.py
@@ -11,15 +11,22 @@ ak4JetTracksAssociatorAtVertexJPT.pvSrc = cms.InputTag("offlinePrimaryVertices")
 from RecoEgamma.ElectronIdentification.electronIdSequence_cff import eidTight
 JPTeidTight = eidTight.clone()
 
+# ---------- Seeds from TrackJets
+
+from RecoJets.JetPlusTracks.jetPlusTrackAddonSeedProducer_cfi import *
+
+JetPlusTrackAddonSeedReco = jetPlusTrackAddonSeedProducer.clone()
 
 # ---------- Module definition
 from RecoJets.JetPlusTracks.JetPlusTrackCorrections_cfi import *
-
 
 JetPlusTrackZSPCorJetAntiKt4 = cms.EDProducer(
     "JetPlusTrackProducer",
     cms.PSet(JPTZSPCorrectorAntiKt4),
     src = cms.InputTag("ak4CaloJets"),
+    srcTrackJets = cms.InputTag("ak4TrackJets"),
+    srcAddCaloJets = cms.InputTag('JetPlusTrackAddonSeedReco'),
+    extrapolations = cms.InputTag("trackExtrapolator"),
     tagName = cms.vstring('ZSP_CMSSW390_Akt_05_PU0'),
     tagNameOffset = cms.vstring(),
     PU = cms.int32(-1),
@@ -27,7 +34,8 @@ JetPlusTrackZSPCorJetAntiKt4 = cms.EDProducer(
     UseZSP = cms.bool(False),
     srcPVs = cms.InputTag('offlinePrimaryVertices'),    
     alias = cms.untracked.string('JetPlusTrackZSPCorJetAntiKt4'),
-    ptCUT = cms.double(15.)
+    ptCUT = cms.double(15.),
+    dRcone = cms.double(0.4)
     )
 
 JetPlusTrackZSPCorJetAntiKt4.JetTracksAssociationAtVertex = cms.InputTag("ak4JetTracksAssociatorAtVertexJPT")
@@ -40,6 +48,7 @@ JetPlusTrackZSPCorJetAntiKt4.JetSplitMerge = cms.int32(2)
 
 JetPlusTrackCorrectionsAntiKt4Task = cms.Task(
     JPTeidTight,
+    JetPlusTrackAddonSeedReco,
     ak4JetTracksAssociatorAtVertexJPT,
     ak4JetTracksAssociatorAtCaloFace,
     JetPlusTrackZSPCorJetAntiKt4
