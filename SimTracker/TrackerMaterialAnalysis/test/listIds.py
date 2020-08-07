ae6a1967ebdc05e5627a45b533693193027f2425
Andres Vargas <vargasa@cua.edu>
2020-08-07 15:16:49 -0500

    See https://github.com/cms-sw/cmssw/pull/31083#discussion_r466881634

---
+++ SimTracker/TrackerMaterialAnalysis/test/listIds.py
@@ -0,0 +1,25 @@
+#! /usr/bin/env cmsRun
+
+import FWCore.ParameterSet.Config as cms
+
+process = cms.Process("MaterialAnalyser")
+process.load('Configuration.Geometry.GeometryDB_cff')
+process.load('Configuration.StandardSequences.FrontierConditions_GlobalTag_cff')
+from Configuration.AlCa.GlobalTag import GlobalTag
+process.GlobalTag = GlobalTag(process.GlobalTag, 'auto:run2_mc', '')
+process.load("Configuration.StandardSequences.MagneticField_cff")
+
+process.MessageLogger = cms.Service("MessageLogger",
+    destinations = cms.untracked.vstring('cout')
+)
+
+process.source = cms.Source("EmptySource")
+process.maxEvents = cms.untracked.PSet(
+    input = cms.untracked.int32(1)
+)
+
+process.listIds = cms.EDAnalyzer("ListIds",
+                                materials = cms.untracked.vstring("materials:Silicon" , "tracker:SenSi"),
+                                printMaterial = cms.untracked.bool(False)
+                              )
+process.path = cms.Path(process.listIds)
