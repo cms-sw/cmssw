96c3294e4972abc21ca063d4f3f2e8566f1f72df
Andrea Bocci <andrea.bocci@cern.ch>
2020-08-07 20:13:26 +0200

    Update the BeamSpotCUDA class (#530)
    Make the BeamSpotCUDA movable and explicitly non-copiable (as was
    already the case due to the device::unique_ptr data member).
    
    Remove the cudaMemcpyAsync from the BeamSpotCUDA data format, and move
    it to the BeamSpotToCUDA producer.

--- CUDADataFormats/BeamSpot/interface/BeamSpotCUDA.h
+++ CUDADataFormats/BeamSpot/interface/BeamSpotCUDA.h
@@ -1,15 +1,14 @@
 #ifndef CUDADataFormats_BeamSpot_interface_BeamSpotCUDA_h
 #define CUDADataFormats_BeamSpot_interface_BeamSpotCUDA_h
 
-#include "HeterogeneousCore/CUDAUtilities/interface/device_unique_ptr.h"
-
 #include <cuda_runtime.h>
+
+#include "HeterogeneousCore/CUDAUtilities/interface/device_unique_ptr.h"
 
 class BeamSpotCUDA {
 public:
-  // alignas(128) doesn't really make sense as there is only one
-  // beamspot per event?
-  struct Data {
+  // align to the CUDA L1 cache line size
+  struct alignas(128) Data {
     float x, y, z;  // position
     // TODO: add covariance matrix
 
@@ -20,13 +19,26 @@ public:
     float betaStar;
   };
 
+  // default constructor, required by cms::cuda::Product<BeamSpotCUDA>
   BeamSpotCUDA() = default;
-  BeamSpotCUDA(Data const* data_h, cudaStream_t stream);
 
+  // constructor that allocates cached device memory on the given CUDA stream
+  BeamSpotCUDA(cudaStream_t stream) { data_d_ = cms::cuda::make_device_unique<Data>(stream); }
+
+  // movable, non-copiable
+  BeamSpotCUDA(BeamSpotCUDA const&) = delete;
+  BeamSpotCUDA(BeamSpotCUDA&&) = default;
+  BeamSpotCUDA& operator=(BeamSpotCUDA const&) = delete;
+  BeamSpotCUDA& operator=(BeamSpotCUDA&&) = default;
+
+  Data* data() { return data_d_.get(); }
   Data const* data() const { return data_d_.get(); }
+
+  cms::cuda::device::unique_ptr<Data>& ptr() { return data_d_; }
+  cms::cuda::device::unique_ptr<Data> const& ptr() const { return data_d_; }
 
 private:
   cms::cuda::device::unique_ptr<Data> data_d_;
 };
 
-#endif
+#endif  // CUDADataFormats_BeamSpot_interface_BeamSpotCUDA_h
