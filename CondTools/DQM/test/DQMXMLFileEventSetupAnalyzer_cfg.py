ca1b34d3d895f93a4c0283419d98978fe6af4699
mmusich <marco.musich@cern.ch>
2020-08-08 16:50:33 +0200

    start clean-up of CondToos/DQM

--- CondTools/DQM/test/DQMXMLFileEventSetupAnalyzer_cfg.py
+++ CondTools/DQM/test/DQMXMLFileEventSetupAnalyzer_cfg.py
@@ -2,11 +2,13 @@ import FWCore.ParameterSet.Config as cms
 
 process = cms.Process('XMLFILERETRIEVER')
 
-# setting the Global Tag
+####################################################################
+# Get the GlogalTag
+####################################################################
 process.load("Configuration.StandardSequences.FrontierConditions_GlobalTag_cff")
-process.GlobalTag.connect = "sqlite_file:./tagDB.db" 
-process.GlobalTag.globaltag = 'MY_GT::All' 
-process.GlobalTag.DumpStat = cms.untracked.bool(True)
+from Configuration.AlCa.GlobalTag import GlobalTag
+process.GlobalTag = GlobalTag(process.GlobalTag, 'auto:run3_data_promptlike', '')
+#process.GlobalTag.DumpStat = cms.untracked.bool(True)  # optional if you want it to be verbose
 
 # import of standard configurations
 process.load("DQMServices.Core.DQMStore_cfg")
@@ -16,29 +18,15 @@ process.DQMStore.verboseQT = cms.untracked.int32(0)
 process.maxEvents = cms.untracked.PSet(input = cms.untracked.int32(-1))
 
 # Input source
-from CondCore.DBCommon.CondDBSetup_cfi import *
-
 process.source = cms.Source("EmptyIOVSource",
                             timetype = cms.string('runnumber'),
                             firstValue = cms.uint64(1),
                             lastValue = cms.uint64(1),
                             interval = cms.uint64(1)
                             )
-                                      
-#process.XmlRetrieval_2 = cms.ESSource("PoolDBESSource",
-#                                      CondDBSetup,
-#                                      connect = cms.string('sqlite_file:./testXML.db'),
-#                                      BlobStreamerName = cms.untracked.string('TBufferBlobStreamingService'),
-#                                      messageLevel = cms.untracked.int32(1),
-#                                      timetype = cms.string('runnumber'),
-#                                      toGet = cms.VPSet(cms.PSet(record = cms.string('DQMXMLFileRcd'),
-#                                                                 tag = cms.string('XML_pixels_1'),
-#                                                                 label=cms.untracked.string('XML2_mio')
-#                                                                 )
-#                                                        )
-#                                      )
 
-#process.XmlRetrieval_1 = cms.ESSource("PoolDBESSource",
+# from CondCore.CondDB.CondDB_cfi import *
+# process.XmlRetrieval_1 = cms.ESSource("PoolDBESSource",
 #                                      CondDBSetup,
 #                                      connect = cms.string('sqlite_file:./testXML.db'),
 #                                      BlobStreamerName = cms.untracked.string('TBufferBlobStreamingService'),
@@ -51,10 +39,28 @@ process.source = cms.Source("EmptyIOVSource",
 #                                                        )
 #                                      )
 
+# process.ReferenceRetrieval = cms.ESSource("PoolDBESSource",
+#                                   CondDBSetup,
+#                                   connect = cms.string('sqlite_file:DQMReferenceHistogramTest.db'),
+#                                   BlobStreamerName = cms.untracked.string('TBufferBlobStreamingService'),
+#                                   messageLevel = cms.untracked.int32(1), #3 for high verbosity
+#                                   timetype = cms.string('runnumber'),
+#                                   toGet = cms.VPSet(cms.PSet(record = cms.string('DQMReferenceHistogramRootFileRcd'),
+#                                                              tag = cms.string('ROOTFILE_DQM')
+#                                                              )
+#                                                     )
+#                                   )
+#
 
-process.load('CondTools/DQM/DQMXMLFileEventSetupAnalyzer_cfi')
-process.dqmXMLFileGetter.labelToGet = cms.string('fuffa')
-
+# process.RecordDataGetter = cms.EDAnalyzer("EventSetupRecordDataGetter",
+#                                   toGet = cms.VPSet(cms.PSet(record = cms.string('DQMReferenceHistogramRootFileRcd'),
+#                                                          data = cms.vstring('ROOTFILE_DQM_Test10')
+#                                                          )
+#                                                 ),
+#                                   verbose = cms.untracked.bool(False)
+#                                   )
+                                      
+process.load('CondTools.DQM.DQMXMLFileEventSetupAnalyzer_cfi')
+process.dqmXMLFileGetter.labelToGet = cms.string('SiPixelDQMQTests')
 process.path = cms.Path(process.dqmXMLFileGetter)
 
-#process.path = cms.Path()
