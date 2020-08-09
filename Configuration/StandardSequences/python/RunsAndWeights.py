9083b6425211528bac7cf80e98d01f6e59166768
Norraphat <srimanob@mail.cern.ch>
2020-08-09 18:09:38 +0200

    fix name

--- Configuration/StandardSequences/python/RunsAndWeights.py
+++ Configuration/StandardSequences/python/RunsAndWeights.py
@@ -1,4 +1,4 @@
 RunsAndWeights = {
     'Run2012_AB_C_D_oneRunPerEra' : 'SimGeneral.Configuration.RunsAndWeights_Run2012_AB_C_D_oneRunPerEra',
-    'RunsAndWeights_Run2018_ABCD' : 'SimGeneral.Configuration.RunsAndWeights_Run2018_ABCD'
+    'Run2018_ABCD' : 'SimGeneral.Configuration.RunsAndWeights_Run2018_ABCD'
     }
