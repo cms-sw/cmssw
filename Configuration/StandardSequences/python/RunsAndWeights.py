7d1d0b8ee30cc1ea614e566d4432e5340c3cb2ce
Norraphat <srimanob@mail.cern.ch>
2020-08-09 18:07:08 +0200

    Update RunsAndWeights configuration

--- Configuration/StandardSequences/python/RunsAndWeights.py
+++ Configuration/StandardSequences/python/RunsAndWeights.py
@@ -1,3 +1,4 @@
 RunsAndWeights = {
-    'Run2012_AB_C_D_oneRunPerEra' : 'SimGeneral.Configuration.RunsAndWeights_Run2012_AB_C_D_oneRunPerEra'
+    'Run2012_AB_C_D_oneRunPerEra' : 'SimGeneral.Configuration.RunsAndWeights_Run2012_AB_C_D_oneRunPerEra',
+    'RunsAndWeights_Run2018_ABCD' : 'SimGeneral.Configuration.RunsAndWeights_Run2018_ABCD'
     }
