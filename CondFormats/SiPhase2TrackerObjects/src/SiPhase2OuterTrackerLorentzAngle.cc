738a467e65493dc2405f234973a64215e5890c35
mmusich <marco.musich@cern.ch>
2020-08-10 19:23:06 +0200

    Updated SiPhase2OuterTrackerLorentzAngle: - correct typo - throw when necessary - use range-based loop - remove redundant methods to retrieve LA for a certain module type

--- CondFormats/SiPhase2TrackerObjects/src/SiPhase2OuterTrackerLorentzAngle.cc
+++ CondFormats/SiPhase2TrackerObjects/src/SiPhase2OuterTrackerLorentzAngle.cc
@@ -5,7 +5,7 @@ bool SiPhase2OuterTrackerLorentzAngle::putLorentzAngle(const uint32_t& detid, fl
   std::unordered_map<unsigned int, float>::const_iterator id = m_LA.find(detid);
   if (id != m_LA.end()) {
     edm::LogError("SiPhase2OuterTrackerLorentzAngle") << "SiPhase2OuterTrackerLorentzAngle for DetID " << detid
-                                                      << " is already stored. Skippig this put" << std::endl;
+                                                      << " is already stored. Skipping this put" << std::endl;
     return false;
   } else
     m_LA[detid] = value;
@@ -17,47 +17,26 @@ float SiPhase2OuterTrackerLorentzAngle::getLorentzAngle(const uint32_t& detid) c
   if (id != m_LA.end())
     return id->second;
   else {
-    edm::LogError("SiPhase2OuterTrackerLorentzAngle")
+    throw cms::Exception("SiPhase2OuterTrackerLorentzAngle")
         << "SiPhase2OuterTrackerLorentzAngle for DetID " << detid << " is not stored" << std::endl;
   }
-  return 0;
 }
 
-void SiPhase2OuterTrackerLorentzAngle::getLorentzAngles_PSP(const TrackerGeometry* trackerGeometry,
-                                                            std::unordered_map<unsigned int, float>& out) const {
+void SiPhase2OuterTrackerLorentzAngle::getLorentzAnglesByModuleType(const TrackerGeometry* trackerGeometry,
+                                                                    const TrackerGeometry::ModuleType& theType,
+                                                                    std::unordered_map<unsigned int, float>& out) const {
   for (const auto& [det, LA] : m_LA) {
-    if (trackerGeometry->getDetectorType(det) == TrackerGeometry::ModuleType::Ph2PSP) {
-      out[det] = LA;
-    }
-  }
-}
-
-void SiPhase2OuterTrackerLorentzAngle::getLorentzAngles_PSS(const TrackerGeometry* trackerGeometry,
-                                                            std::unordered_map<unsigned int, float>& out) const {
-  for (const auto& [det, LA] : m_LA) {
-    if (trackerGeometry->getDetectorType(det) == TrackerGeometry::ModuleType::Ph2PSS) {
-      out[det] = LA;
-    }
-  }
-}
-
-void SiPhase2OuterTrackerLorentzAngle::getLorentzAngles_2S(const TrackerGeometry* trackerGeometry,
-                                                           std::unordered_map<unsigned int, float>& out) const {
-  for (const auto& [det, LA] : m_LA) {
-    if (trackerGeometry->getDetectorType(det) == TrackerGeometry::ModuleType::Ph2SS) {
+    if (trackerGeometry->getDetectorType(det) == theType) {
       out[det] = LA;
     }
   }
 }
 
 void SiPhase2OuterTrackerLorentzAngle::printDebug(std::stringstream& ss, const TrackerTopology* /*trackerTopo*/) const {
-  std::unordered_map<unsigned int, float> detid_la = getLorentzAngles();
-  std::unordered_map<unsigned int, float>::const_iterator it;
-  size_t count = 0;
+  const std::unordered_map<unsigned int, float>& detid_la = getLorentzAngles();
   ss << "SiPhase2OuterTrackerLorentzAngleReader:" << std::endl;
   ss << "detid \t Lorentz angle" << std::endl;
-  for (it = detid_la.begin(); it != detid_la.end(); ++it) {
-    ss << it->first << "\t" << it->second << std::endl;
-    ++count;
+  for (const auto& it : detid_la) {
+    ss << it.first << "\t" << it.second << std::endl;
   }
 }
