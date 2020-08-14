c739bea5c347511f6390c8c0e1733a50524f99e4
Justinas Rumsevicius <justinas.rumsevicius@gmail.com>
2020-08-14 12:36:06 +0200

    Move "wmsplit" dict build to a static method, remove circular import

--- Configuration/PyReleaseValidation/python/MatrixInjector.py
+++ Configuration/PyReleaseValidation/python/MatrixInjector.py
@@ -167,14 +167,19 @@ class MatrixInjector(object):
         self.chainDicts={}
 
 
-    def prepare(self,mReader, directories, mode='init'):
+    @staticmethod
+    def get_wmsplit():
+        """
+        Return a "wmsplit" dictionary that contain non-default LumisPerJob values
+        """
+        wmsplit = {}
         try:
             #from Configuration.PyReleaseValidation.relval_steps import wmsplit
             wmsplit = {}
             wmsplit['DIGIHI']=5
             wmsplit['RECOHI']=5
             wmsplit['HLTD']=5
-            wmsplit['RECODreHLT']=2  
+            wmsplit['RECODreHLT']=2
             wmsplit['DIGIPU']=4
             wmsplit['DIGIPU1']=4
             wmsplit['RECOPU1']=1
@@ -254,8 +259,8 @@ class MatrixInjector(object):
             wmsplit['HLTDR2_2018']=1
             wmsplit['HLTDR2_2018_BadHcalMitig']=1
             wmsplit['Hadronizer']=1
-            wmsplit['DIGIUP15']=1 
-            wmsplit['RECOUP15']=1 
+            wmsplit['DIGIUP15']=1
+            wmsplit['RECOUP15']=1
             wmsplit['RECOAODUP15']=5
             wmsplit['DBLMINIAODMCUP15NODQM']=5
             wmsplit['DigiFull']=5
@@ -263,7 +268,7 @@ class MatrixInjector(object):
             wmsplit['DigiFullPU']=1
             wmsplit['RecoFullPU']=1
             wmsplit['RECOHID11']=1
-            wmsplit['DigiFullTriggerPU_2026D17PU'] = 1 
+            wmsplit['DigiFullTriggerPU_2026D17PU'] = 1
             wmsplit['RecoFullGlobalPU_2026D17PU']=1
             wmsplit['DIGIUP17']=1
             wmsplit['RECOUP17']=1
@@ -303,19 +308,20 @@ class MatrixInjector(object):
             wmsplit['RecoFullGlobalPU_2026D50PU']=1
             wmsplit['DigiFullTriggerPU_2026D51PU'] = 1
             wmsplit['RecoFullGlobalPU_2026D51PU']=1
-            wmsplit['DigiFullTriggerPU_2026D52PU'] = 1	
-            wmsplit['RecoFullGlobalPU_2026D52PU']=1           
-            wmsplit['DigiFullTriggerPU_2026D57PU'] = 1	
-            wmsplit['RecoFullGlobalPU_2026D57PU']=1           
-            wmsplit['DigiFullTriggerPU_2026D58PU'] = 1	
-            wmsplit['RecoFullGlobalPU_2026D58PU']=1           
-                         
-            #import pprint
-            #pprint.pprint(wmsplit)            
-        except:
-            print("Not set up for step splitting")
-            wmsplit={}
+            wmsplit['DigiFullTriggerPU_2026D52PU'] = 1
+            wmsplit['RecoFullGlobalPU_2026D52PU']=1
+            wmsplit['DigiFullTriggerPU_2026D57PU'] = 1
+            wmsplit['RecoFullGlobalPU_2026D57PU']=1
+            wmsplit['DigiFullTriggerPU_2026D58PU'] = 1
+            wmsplit['RecoFullGlobalPU_2026D58PU']=1
+        except Exception as ex:
+            print('Exception while building a wmsplit dictionary: %s' % (str(ex)))
+            return {}
 
+        return wmsplit
+
+    def prepare(self,mReader, directories, mode='init'):
+        wmsplit = MatrixInjector.get_wmsplit()
         acqEra=False
         for (n,dir) in directories.items():
             chainDict=copy.deepcopy(self.defaultChain)
