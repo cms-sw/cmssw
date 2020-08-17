e9e5e705d299d0fab2e99c18afad3a0cd83843d6
Sunanda <sunanda.banerjee@cern.ch>
2020-08-17 19:25:43 +0200

    Remove unwanted DDD reference fro the code and test the used scripts

--- Geometry/HcalCommonData/test/python/runHcalRecNumberingDDDTester_cfg.py
+++ Geometry/HcalCommonData/test/python/runHcalRecNumberingDDDTester_cfg.py
@@ -1,9 +1,10 @@
 import FWCore.ParameterSet.Config as cms
-process = cms.Process("HcalSimNumberingTest")
+from Configuration.Eras.Era_Run3_cff import Run3
 
-#process.load('Geometry.HcalCommonData.testPhase1GeometryXML_cfi')
-#process.load('Geometry.HcalCommonData.hcalSimulationParameters_cff')
-process.load('Configuration.Geometry.GeometryExtended2026D41_cff')
+process = cms.Process("HcalRecNumberingTest",Run3)
+
+process.load('Configuration.Geometry.GeometryExtended2021Reco_cff')
+#process.load('Configuration.Geometry.GeometryExtended2026D41Reco_cff')
 process.load('FWCore.MessageService.MessageLogger_cfi')
 
 if 'MessageLogger' in process.__dict__:
@@ -14,7 +15,7 @@ process.maxEvents = cms.untracked.PSet(
     input = cms.untracked.int32(1)
     )
 
-process.hpa = cms.EDAnalyzer("HcalSimNumberingTester")
+process.hpa = cms.EDAnalyzer("HcalRecNumberingTester")
 
 process.Timing = cms.Service("Timing")
 process.SimpleMemoryCheck = cms.Service("SimpleMemoryCheck")
