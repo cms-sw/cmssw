e3de31094b50394ecab680dfb0c5a6aaef1380b0
Jeongeun Lee <jeongeun.lee@cern.ch>
2020-08-18 12:02:09 +0200

    drop type specs where original parameter exist (RecoJets)

--- RecoJets/JetProducers/python/ak8GenJets_cfi.py
+++ RecoJets/JetProducers/python/ak8GenJets_cfi.py
@@ -3,8 +3,8 @@ import FWCore.ParameterSet.Config as cms
 from RecoJets.JetProducers.ak4GenJets_cfi import ak4GenJets
 
 ak8GenJets = ak4GenJets.clone(
-    rParam       = cms.double(0.8)
-    )
+    rParam       = 0.8
+)
 
 ak8GenJetsSoftDrop = ak8GenJets.clone(
     useSoftDrop = cms.bool(True),
@@ -13,9 +13,9 @@ ak8GenJetsSoftDrop = ak8GenJets.clone(
     R0   = cms.double(0.8),
     useExplicitGhosts = cms.bool(True),
     writeCompound = cms.bool(True),
-    jetCollInstanceName=cms.string("SubJets"),
+    jetCollInstanceName = cms.string("SubJets"),
     jetPtMin = 100.0
-    )
+)
 
 ak8GenJetsConstituents = cms.EDProducer("GenJetConstituentSelector",
                                           src = cms.InputTag("ak8GenJets"),
