7e590f24be89895283f128d5c53b56994c40db5c
Marco Rovere <marco.rovere@cern.ch>
2020-08-18 10:04:14 +0200

    Add angles plots to TICL Trackster validation

--- Validation/HGCalValidation/plugins/TICLTrackstersValidation.cc
+++ Validation/HGCalValidation/plugins/TICLTrackstersValidation.cc
@@ -2,6 +2,7 @@
 #include <unordered_map>
 
 // user include files
+#include "DataFormats/Math/interface/Point3D.h"
 #include "FWCore/Framework/interface/Frameworkfwd.h"
 #include "DQMServices/Core/interface/DQMGlobalEDAnalyzer.h"
 
@@ -12,6 +13,7 @@
 
 #include "FWCore/Utilities/interface/transform.h"
 #include "DataFormats/HGCalReco/interface/Trackster.h"
+#include "DataFormats/HGCalReco/interface/TICLSeedingRegion.h"
 
 #include "DataFormats/CaloRecHit/interface/CaloCluster.h"
 #include "RecoLocalCalo/HGCalRecAlgos/interface/RecHitTools.h"
@@ -23,6 +25,10 @@ struct Histogram_TICLTrackstersValidation {
   dqm::reco::MonitorElement* delta_energy_;
   dqm::reco::MonitorElement* delta_energy_vs_energy_;
   dqm::reco::MonitorElement* delta_layer_;
+  // For the definition of the angles, read http://hgcal.web.cern.ch/hgcal/Reconstruction/Tutorial/
+  dqm::reco::MonitorElement* angle_alpha_;
+  dqm::reco::MonitorElement* angle_alpha_alternative_;
+  dqm::reco::MonitorElement* angle_beta_;
 };
 
 using Histograms_TICLTrackstersValidation = std::unordered_map<unsigned int, Histogram_TICLTrackstersValidation>;
@@ -47,6 +53,8 @@ private:
   std::vector<std::string> trackstersCollectionsNames_;
   std::vector<edm::EDGetTokenT<std::vector<Trackster>>> tracksterTokens_;
   edm::EDGetTokenT<std::vector<reco::CaloCluster>> layerClustersToken_;
+  edm::EDGetTokenT<std::vector<TICLSeedingRegion>> ticlSeedingGlobalToken_;
+  edm::EDGetTokenT<std::vector<TICLSeedingRegion>> ticlSeedingTrkToken_;
   hgcal::RecHitTools rhtools_;
 };
 
@@ -57,8 +65,9 @@ TICLTrackstersValidation::TICLTrackstersValidation(const edm::ParameterSet& iCon
                                              trackstersCollectionsNames_.emplace_back(tag.label());
                                              return consumes<std::vector<Trackster>>(tag);
                                            });
-
   layerClustersToken_ = consumes<std::vector<reco::CaloCluster>>(iConfig.getParameter<edm::InputTag>("layerClusters"));
+  ticlSeedingGlobalToken_ = consumes<std::vector<TICLSeedingRegion>>(iConfig.getParameter<edm::InputTag>("ticlSeedingGlobal"));
+  ticlSeedingTrkToken_ = consumes<std::vector<TICLSeedingRegion>>(iConfig.getParameter<edm::InputTag>("ticlSeedingTrk"));
 }
 
 TICLTrackstersValidation::~TICLTrackstersValidation() {}
@@ -69,6 +78,15 @@ void TICLTrackstersValidation::dqmAnalyze(edm::Event const& iEvent,
   edm::Handle<std::vector<reco::CaloCluster>> layerClustersH;
   iEvent.getByToken(layerClustersToken_, layerClustersH);
   auto const& layerClusters = *layerClustersH.product();
+
+  edm::Handle<std::vector<TICLSeedingRegion>> ticlSeedingGlobalH;
+  iEvent.getByToken(ticlSeedingGlobalToken_, ticlSeedingGlobalH);
+  auto const& ticlSeedingGlobal = *ticlSeedingGlobalH.product();
+
+  edm::Handle<std::vector<TICLSeedingRegion>> ticlSeedingTrkH;
+  iEvent.getByToken(ticlSeedingTrkToken_, ticlSeedingTrkH);
+  auto const& ticlSeedingTrk = *ticlSeedingTrkH.product();
+
   int layers = rhtools_.lastLayer();
   for (const auto& trackster_token : tracksterTokens_) {
     edm::Handle<std::vector<Trackster>> trackster_h;
@@ -89,6 +107,50 @@ void TICLTrackstersValidation::dqmAnalyze(edm::Event const& iEvent,
         histo.delta_energy_->Fill(oc.energy() - ic.energy());
         histo.delta_energy_vs_energy_->Fill(ic.energy(), oc.energy() - ic.energy());
         histo.delta_layer_->Fill(layer_out - layer_in);
+
+        // Alpha angles
+        const auto & outer_outer_pos = oc.position();
+        const auto & outer_inner_pos = ic.position();
+        const auto & seed = thisTrackster.seedIndex();
+        auto seedGlobalPos = math::XYZPoint(ticlSeedingGlobal[0].origin.x(),
+            ticlSeedingGlobal[0].origin.y(), ticlSeedingGlobal[0].origin.z());
+        auto seedDirectionPos = math::XYZPoint(ticlSeedingGlobal[0].directionAtOrigin.x(),
+            ticlSeedingGlobal[0].directionAtOrigin.y(), ticlSeedingGlobal[0].directionAtOrigin.z());
+        if (thisTrackster.seedID().id() != 0) {
+          // Seed to trackster association is, at present, rather convoluted.
+          for (auto const &s : ticlSeedingTrk) {
+            if (s.index == seed) {
+              seedGlobalPos = math::XYZPoint(s.origin.x(),
+                  s.origin.y(), s.origin.z());
+              seedDirectionPos = math::XYZPoint(s.directionAtOrigin.x(),
+                  s.directionAtOrigin.y(), s.directionAtOrigin.z());
+              break;
+            }
+          }
+        }
+
+        auto alpha = (outer_inner_pos-seedGlobalPos).Dot(outer_outer_pos - outer_inner_pos)/
+          sqrt((outer_inner_pos-seedGlobalPos).Mag2()*(outer_outer_pos-outer_inner_pos).Mag2());
+        auto alpha_alternative = (outer_outer_pos - seedGlobalPos).Dot(seedDirectionPos) /
+          sqrt((outer_outer_pos - seedGlobalPos).Mag2()*seedDirectionPos.Mag2());
+        histo.angle_alpha_->Fill(alpha);
+        histo.angle_alpha_alternative_->Fill(alpha_alternative);
+
+        // Beta angle is usually computed using 2 edges. Another inner loop
+        // is therefore needed.
+        std::vector<std::array<unsigned int, 2>> innerDoublets;
+        for ( const auto & otherEdge : thisTrackster.edges()) {
+          if (otherEdge[1] == edge[0]) {
+            innerDoublets.push_back(otherEdge);
+          }
+        }
+        for (const auto & inner : innerDoublets) {
+          const auto & inner_ic = layerClusters[inner[0]];
+          const auto & inner_inner_pos = inner_ic.position();
+          auto beta = (outer_inner_pos-inner_inner_pos).Dot(outer_outer_pos-inner_inner_pos)/
+            sqrt((outer_inner_pos-inner_inner_pos).Mag2()*(outer_outer_pos-inner_inner_pos).Mag2());
+          histo.angle_beta_->Fill(beta);
+        }
       }
     }
   }
@@ -106,6 +168,9 @@ void TICLTrackstersValidation::bookHistograms(DQMStore::IBooker& ibook,
     histo.delta_energy_ = ibook.book1D("Delta energy", "Delta Energy (O-I)", 800, -20., 20.);
     histo.delta_energy_vs_energy_ = ibook.book2D("Delta Energy vs Energy", "Delta Energy (O-I) vs Energy (I)", 200, 0., 20., 800, -20., 20.);
     histo.delta_layer_ = ibook.book1D("Delta Layer", "Delta Layer", 10, 0., 10.);
+    histo.angle_alpha_ = ibook.book1D("cosAngle Alpha", "cosAngle Alpha", 200, -1., 1.);
+    histo.angle_beta_ = ibook.book1D("cosAngle Beta", "cosAngle Beta", 200, -1., 1.);
+    histo.angle_alpha_alternative_ = ibook.book1D("cosAngle Alpha Alternative", "Angle Alpha Alternative", 200, -1., 1.);
 
     labelIndex++;
   }
@@ -126,6 +191,8 @@ void TICLTrackstersValidation::fillDescriptions(edm::ConfigurationDescriptions& 
                                            edm::InputTag("ticlTrackstersMerge")};
   desc.add<std::vector<edm::InputTag>>("tracksterCollections", source_vector);
   desc.add<edm::InputTag>("layerClusters", edm::InputTag("hgcalLayerClusters"));
+  desc.add<edm::InputTag>("ticlSeedingGlobal", edm::InputTag("ticlSeedingGlobal"));
+  desc.add<edm::InputTag>("ticlSeedingTrk", edm::InputTag("ticlSeedingTrk"));
   desc.add<std::string>("folder", "HGCAL/");
   descriptions.add("ticlTrackstersValidationDefault", desc);
 }
