84e9f99f803e6fd133e3181b7bac0758bc263fd9
Remi <remigius.mommsen@cern.ch>
2020-08-19 15:22:00 +0200

    Add GEM plus/minus HV states and provide an accessor to get the magnetic field strength

--- DataFormats/OnlineMetaData/src/DCSRecord.cc
+++ DataFormats/OnlineMetaData/src/DCSRecord.cc
@@ -6,7 +6,8 @@
 
 const DCSRecord::ParitionNames DCSRecord::partitionNames_ = {
     {"EBp",  "EBm",  "EEp",    "EEm", "HBHEa",  "HBHEb", "HBHEc", "HF",   "HO",   "RPC",  "DT0", "DTp", "DTm",
-     "CSCp", "CSCm", "CASTOR", "ZDC", "TIBTID", "TOB",   "TECp",  "TECm", "BPIX", "FPIX", "ESp", "ESm"}};
+     "CSCp", "CSCm", "CASTOR", "ZDC", "TIBTID", "TOB",   "TECp",  "TECm", "BPIX", "FPIX", "ESp", "ESm",
+     "GEMm", "GEMp"}};
 
 DCSRecord::DCSRecord() : timestamp_(edm::Timestamp::invalidTimestamp()), magnetCurrent_(-1) {}
 
