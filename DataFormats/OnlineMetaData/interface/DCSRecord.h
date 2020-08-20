3332882f23c6596015f7532dfc0a1d247b78f4e5
Remi <remigius.mommsen@cern.ch>
2020-08-20 08:23:51 +0200

    code-format fix

--- DataFormats/OnlineMetaData/interface/DCSRecord.h
+++ DataFormats/OnlineMetaData/interface/DCSRecord.h
@@ -20,7 +20,7 @@
 class DCSRecord {
 public:
   // Adding new partitions requires to add a new bitset definition with
-  // the correct dimension to DataFormats/StdDictionaries/src/classes_def_others.xml 
+  // the correct dimension to DataFormats/StdDictionaries/src/classes_def_others.xml
   enum Partition {
     EBp,
     EBm,
