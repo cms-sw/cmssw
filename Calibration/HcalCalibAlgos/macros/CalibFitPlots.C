f28a9c4132dd5726765373d8d81a59385d4a47b4
Sunanda <sunanda.banerjee@cern.ch>
2020-08-20 19:04:27 +0200

    Code check

--- Calibration/HcalCalibAlgos/macros/CalibFitPlots.C
+++ Calibration/HcalCalibAlgos/macros/CalibFitPlots.C
@@ -935,28 +935,37 @@ void PlotHist(const char* infile,
   std::string name1[5] = {"Z0", "Z1", "Z2", "Z3", "Z4"};
   std::string name2[5] = {"L0", "L1", "L2", "L3", "L4"};
   std::string name3[5] = {"V0", "V1", "V2", "V3", "V4"};
-  std::string name4[12] = {"etaB31", "etaB32", "etaB33", "etaB34",
-			   "etaB11", "etaB12", "etaB13", "etaB14",
-			   "etaB01", "etaB02", "etaB03", "etaB04"};
+  std::string name4[12] = {"etaB31",
+                           "etaB32",
+                           "etaB33",
+                           "etaB34",
+                           "etaB11",
+                           "etaB12",
+                           "etaB13",
+                           "etaB14",
+                           "etaB01",
+                           "etaB02",
+                           "etaB03",
+                           "etaB04"};
   std::string name5[5] = {"W0", "W1", "W2", "W3", "W4"};
   std::string title[6] = {"Tracks with p = 10:20 GeV",
-			  "Tracks with p = 20:30 GeV",
+                          "Tracks with p = 20:30 GeV",
                           "Tracks with p = 30:40 GeV",
                           "Tracks with p = 40:60 GeV",
                           "Tracks with p = 60:100 GeV",
                           "Tracks with p = 20:100 GeV"};
   std::string title1[12] = {"Tracks with p = 40:60 GeV (Barrel)",
-			    "Tracks with p = 40:60 GeV (Transition)",
-			    "Tracks with p = 40:60 GeV (Endcap)",
-			    "Tracks with p = 40:60 GeV",
-			    "Tracks with p = 20:30 GeV (Barrel)",
-			    "Tracks with p = 20:30 GeV (Transition)",
-			    "Tracks with p = 20:30 GeV (Endcap)",
-			    "Tracks with p = 20:30 GeV",
-			    "Tracks with p = 10:20 GeV (Barrel)",
-			    "Tracks with p = 10:20 GeV (Transition)",
-			    "Tracks with p = 10:20 GeV (Endcap)",
-			    "Tracks with p = 10:20 GeV"};
+                            "Tracks with p = 40:60 GeV (Transition)",
+                            "Tracks with p = 40:60 GeV (Endcap)",
+                            "Tracks with p = 40:60 GeV",
+                            "Tracks with p = 20:30 GeV (Barrel)",
+                            "Tracks with p = 20:30 GeV (Transition)",
+                            "Tracks with p = 20:30 GeV (Endcap)",
+                            "Tracks with p = 20:30 GeV",
+                            "Tracks with p = 10:20 GeV (Barrel)",
+                            "Tracks with p = 10:20 GeV (Transition)",
+                            "Tracks with p = 10:20 GeV (Endcap)",
+                            "Tracks with p = 10:20 GeV"};
   std::string xtitl[5] = {"E_{HCAL}/(p-E_{ECAL})", "i#eta", "d_{L1}", "# Vertex", "E_{HCAL}/(p-E_{ECAL})"};
   std::string ytitl[5] = {
       "Tracks", "MPV(E_{HCAL}/(p-E_{ECAL}))", "MPV(E_{HCAL}/(p-E_{ECAL}))", "MPV(E_{HCAL}/(p-E_{ECAL}))", "Tracks"};
@@ -1727,14 +1736,14 @@ void PlotHistCorrResults(std::string infile, std::string text, std::string prefi
   }
 }
 
-void PlotHistCorrFactor(char* infile, 
-                        std::string text, 
-			std::string prefixF = "", 
-			double scale = 1.0, 
-			int nmin = 100, 
-			bool dataMC = false,
-			bool drawStatBox = true,
-			bool save = false) {
+void PlotHistCorrFactor(char* infile,
+                        std::string text,
+                        std::string prefixF = "",
+                        double scale = 1.0,
+                        int nmin = 100,
+                        bool dataMC = false,
+                        bool drawStatBox = true,
+                        bool save = false) {
   std::map<int, cfactors> cfacs;
   int etamin(100), etamax(-100), maxdepth(0);
   readCorrFactors(infile, scale, cfacs, etamin, etamax, maxdepth);
@@ -1798,7 +1807,7 @@ void PlotHistCorrFactor(char* infile,
   pad->SetRightMargin(0.10);
   pad->SetTopMargin(0.10);
   double yh = 0.90;
-// double yl = yh - 0.025 * hists.size() - dy - 0.01;
+  // double yl = yh - 0.025 * hists.size() - dy - 0.01;
   double yl = 0.15;
   TLegend* legend = new TLegend(0.35, yl, 0.65, yl + 0.04 * hists.size());
   legend->SetFillColor(kWhite);
