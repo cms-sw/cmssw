28a5deaca973d49e86d19d636bd5fe660fe7f08f
David <David.Lange@cern.ch>
2020-08-20 10:03:51 +0200

    code checks

--- RecoTracker/MeasurementDet/interface/MeasurementTracker.h
+++ RecoTracker/MeasurementDet/interface/MeasurementTracker.h
@@ -4,7 +4,6 @@
 #include "TrackingTools/MeasurementDet/interface/MeasurementDetSystem.h"
 #include "DataFormats/DetId/interface/DetId.h"
 
-
 #include "RecoTracker/TkDetLayers/interface/GeometricSearchTracker.h"
 
 #include "FWCore/Framework/interface/Event.h"
