d2dfd25f882cc4a1ab25c2bcfbb35363b2964ef9
Andrea Bocci <andrea.bocci@cern.ch>
2020-08-21 09:23:22 +0200

    Move the underlying data type BeamSpotCUDA::Data to a separate transient struct BeamSpotPOD

--- CUDADataFormats/BeamSpot/interface/BeamSpotCUDA.h
+++ CUDADataFormats/BeamSpot/interface/BeamSpotCUDA.h
@@ -3,27 +3,16 @@
 
 #include <cuda_runtime.h>
 
+#include "DataFormats/BeamSpot/interface/BeamSpotPOD.h"
 #include "HeterogeneousCore/CUDAUtilities/interface/device_unique_ptr.h"
 
 class BeamSpotCUDA {
 public:
-  // align to the CUDA L1 cache line size
-  struct alignas(128) Data {
-    float x, y, z;  // position
-    // TODO: add covariance matrix
-
-    float sigmaZ;
-    float beamWidthX, beamWidthY;
-    float dxdz, dydz;
-    float emittanceX, emittanceY;
-    float betaStar;
-  };
-
   // default constructor, required by cms::cuda::Product<BeamSpotCUDA>
   BeamSpotCUDA() = default;
 
   // constructor that allocates cached device memory on the given CUDA stream
-  BeamSpotCUDA(cudaStream_t stream) { data_d_ = cms::cuda::make_device_unique<Data>(stream); }
+  BeamSpotCUDA(cudaStream_t stream) { data_d_ = cms::cuda::make_device_unique<BeamSpotPOD>(stream); }
 
   // movable, non-copiable
   BeamSpotCUDA(BeamSpotCUDA const&) = delete;
@@ -31,14 +20,14 @@ public:
   BeamSpotCUDA& operator=(BeamSpotCUDA const&) = delete;
   BeamSpotCUDA& operator=(BeamSpotCUDA&&) = default;
 
-  Data* data() { return data_d_.get(); }
-  Data const* data() const { return data_d_.get(); }
+  BeamSpotPOD* data() { return data_d_.get(); }
+  BeamSpotPOD const* data() const { return data_d_.get(); }
 
-  cms::cuda::device::unique_ptr<Data>& ptr() { return data_d_; }
-  cms::cuda::device::unique_ptr<Data> const& ptr() const { return data_d_; }
+  cms::cuda::device::unique_ptr<BeamSpotPOD>& ptr() { return data_d_; }
+  cms::cuda::device::unique_ptr<BeamSpotPOD> const& ptr() const { return data_d_; }
 
 private:
-  cms::cuda::device::unique_ptr<Data> data_d_;
+  cms::cuda::device::unique_ptr<BeamSpotPOD> data_d_;
 };
 
 #endif  // CUDADataFormats_BeamSpot_interface_BeamSpotCUDA_h
