b7acf630cb51434ef985bbb2f591ca38884199a3
Christopher Jones <chrisdjones15@gmail.com>
2020-08-25 09:09:09 -0500

    Special operator= to avoid accidently using templated version

--- FWCore/Utilities/interface/EDGetToken.h
+++ FWCore/Utilities/interface/EDGetToken.h
@@ -89,6 +89,10 @@ namespace edm {
     //Needed to avoid EDGetTokenT(ADAPTER&&) from being called instead
     // when we can use C++20 concepts we can avoid the problem using a constraint
     constexpr EDGetTokenT(EDGetTokenT& iOther) noexcept : m_value{iOther.m_value} {}
+
+    constexpr EDGetTokenT& operator=(EDGetTokenT<T>& iOther) {
+      return (*this = const_cast<const EDGetTokenT<T>&>(iOther));
+    }
     // ---------- const member functions ---------------------
     constexpr unsigned int index() const noexcept { return m_value; }
     constexpr bool isUninitialized() const noexcept { return m_value == s_uninitializedValue; }
