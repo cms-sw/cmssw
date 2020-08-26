5dfa4d6845fc4629b0c48c1955bcab7d649a120b
Michal Bluj <michal.bluj@cern.ch>
2020-08-26 10:47:35 +0200

    Propagate to HGCal instead of EndcapECal for phase2

--- RecoTauTag/RecoTau/interface/PositionAtECalEntranceComputer.h
+++ RecoTauTag/RecoTau/interface/PositionAtECalEntranceComputer.h
@@ -15,6 +15,9 @@
 
 #include "FWCore/Framework/interface/EventSetup.h"
 #include "DataFormats/Candidate/interface/Candidate.h"
+#include "RecoLocalCalo/HGCalRecAlgos/interface/RecHitTools.h"
+
+class MagneticField;
 
 class PositionAtECalEntranceComputer {
 public:
@@ -24,10 +27,14 @@ public:
   void beginEvent(const edm::EventSetup&);
 
   //To do: it seems to more practical to put this to the ES
-  reco::Candidate::Point operator()(const reco::Candidate* particle, bool& success) const;
+  reco::Candidate::Point operator()(const reco::Candidate* particle, bool& success, bool isPhase2 = false) const;
 
 private:
   double bField_z_;
+  MagneticField const* bField_;
+  hgcal::RecHitTools recHitTools_;
+  float hgcalFace_z_;
+  static constexpr float ecalBarrelEndcapEtaBorder_ = 1.479;
 };
 
 #endif  // RecoTauTag_RecoTau_PositionAtECalEntranceComputer_h
