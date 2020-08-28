7a560790a902b6a07a4d1ff66dd8c5eeeb8c1309
andrius <andrius.kirilovas@cern.ch>
2020-08-28 18:44:20 +0200

    Added new DQM GUI support to the rest of the clients

--- DQM/Integration/python/clients/beamhltfake_dqm_sourceclient-live_cfg.py
+++ DQM/Integration/python/clients/beamhltfake_dqm_sourceclient-live_cfg.py
@@ -66,6 +66,9 @@ process.hltTriggerTypeFilter = cms.EDFilter("HLTTriggerTypeFilter",
 process.load("DQM.Integration.config.environment_cfi")
 process.dqmEnv.subSystemFolder = 'FakeBeamMonitor'
 process.dqmSaver.tag           = 'FakeBeamMonitor'
+process.dqmSaver.runNumber     = options.runNumber
+process.dqmSaverPB.tag         = 'FakeBeamMonitor'
+process.dqmSaverPB.runNumber   = options.runNumber
 
 #-----------------------------
 # BeamMonitor
@@ -94,7 +97,7 @@ process.load("DQM.Integration.config.FrontierCondition_GT_cfi")
 #  process.dqmBeamMonitor.BeamFitter.DIPFileName = '/nfshome0/dqmdev/BeamMonitorDQM/BeamFitResults.txt'
 
 process.dqmcommon = cms.Sequence(process.dqmEnv
-                               * process.dqmSaver)
+                               * process.dqmSaver*process.dqmSaverPB)
 
 process.monitor = cms.Sequence(process.dqmBeamMonitor)
 
