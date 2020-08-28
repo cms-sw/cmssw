86f8d65e10255efa89b92fb6997cf4640c774dbb
Martin Grunewald <Martin.Grunewald@cern.ch>
2020-08-28 12:40:07 +0200

    Migration of HLT menus to 1120pre5 template

--- HLTrigger/Configuration/python/HLT_PIon_cff.py
+++ HLTrigger/Configuration/python/HLT_PIon_cff.py
@@ -1,13 +1,13 @@
 # hltGetConfiguration --cff --data /dev/CMSSW_11_2_0/PIon --type PIon
 
-# /dev/CMSSW_11_2_0/PIon/V7 (CMSSW_11_2_0_pre5)
+# /dev/CMSSW_11_2_0/PIon/V8 (CMSSW_11_2_0_pre5)
 
 import FWCore.ParameterSet.Config as cms
 
 fragment = cms.ProcessFragment( "HLT" )
 
 fragment.HLTConfigVersion = cms.PSet(
-  tableName = cms.string('/dev/CMSSW_11_2_0/PIon/V7')
+  tableName = cms.string('/dev/CMSSW_11_2_0/PIon/V8')
 )
 
 fragment.transferSystem = cms.PSet( 
