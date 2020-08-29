1ce36ed28dd88ae618aeaf85326f1aab66b37d68
mmusich <marco.musich@cern.ch>
2020-08-29 16:29:19 +0200

    remove PixelCPEInitial_cfi and PixelCPEParmError_cfi and all their occurences in CMSSW

--- RecoTracker/TrackProducer/python/CTFPixelLess_cff.py
+++ RecoTracker/TrackProducer/python/CTFPixelLess_cff.py
@@ -7,8 +7,6 @@ from TrackingTools.KalmanUpdators.Chi2MeasurementEstimator_cfi import *
 # stripCPE
 from RecoLocalTracker.SiStripRecHitConverter.StripCPEfromTrackAngle_cfi import *
 from RecoLocalTracker.SiStripRecHitConverter.SiStripRecHitMatcher_cfi import *
-# pixelCPE
-from RecoLocalTracker.SiPixelRecHits.PixelCPEParmError_cfi import *
 #TransientTrackingBuilder
 from RecoTracker.TransientTrackingRecHit.TransientTrackingRecHitBuilder_cfi import *
 
