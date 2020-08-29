b763eb5053158be3f7b043077b87f923d8fb094c
Luiz Mundim <luiz.mundim@cern.ch>
2020-08-29 04:32:03 +0200

    PPS sim turned ON by LHCTransport boolean flag; PPS Digi has been removed while geometry is not in DB

--- SimPPS/Configuration/test/pg_step1_GEN_SIM_2021.py
+++ SimPPS/Configuration/test/pg_step1_GEN_SIM_2021.py
@@ -24,6 +24,9 @@ process.load('Configuration.Geometry.GeometryExtended2021_cff')
 
 process.RandomNumberGeneratorService.generator.initialSeed = cms.untracked.uint32(random.randint(0,900000000))
 
+process.load('SimG4Core.Application.g4SimHits_cfi')
+process.g4SimHits.LHCTransport = cms.bool(True)
+
 nEvent_ = 1000
 process.maxEvents = cms.untracked.PSet(
         input = cms.untracked.int32(nEvent_)
