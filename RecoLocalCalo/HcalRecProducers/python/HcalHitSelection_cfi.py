2f922520c52408644e84c46604ebd8f5e75ab17e
Salavat <abdullin@mail.cern.ch>
2020-09-03 16:28:53 +0200

    slimmedHcalRecHits for MINIAOD

--- RecoLocalCalo/HcalRecProducers/python/HcalHitSelection_cfi.py
+++ RecoLocalCalo/HcalRecProducers/python/HcalHitSelection_cfi.py
@@ -11,5 +11,12 @@ reducedHcalRecHits = cms.EDProducer("HcalHitSelection",
                                          )
                                     )
 
+slimmedHcalRecHits = reducedHcalRecHits.clone(
+          hbheTag = cms.InputTag("reducedHcalRecHits","hbhereco"),
+          hfTag   = cms.InputTag("reducedHcalRecHits","hfreco"),
+          hoTag   = cms.InputTag(""),
+          interestingDetIds = cms.VInputTag()
+       )
+
 from Configuration.Eras.Modifier_pp_on_AA_2018_cff import pp_on_AA_2018
 pp_on_AA_2018.toModify(reducedHcalRecHits.interestingDetIds, func = lambda list: list.remove(cms.InputTag("interestingOotEgammaIsoHCALDetId")) )
