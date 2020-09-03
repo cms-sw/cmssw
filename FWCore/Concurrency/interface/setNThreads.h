06cbb7e59768d94879d24008b62be7eb054d4990
Christopher Jones <chrisdjones15@gmail.com>
2020-09-03 11:37:44 -0500

    Added comment about ThreadsController lifetime requirements

--- FWCore/Concurrency/interface/setNThreads.h
+++ FWCore/Concurrency/interface/setNThreads.h
@@ -10,6 +10,8 @@
 #include "FWCore/Concurrency/interface/ThreadsController.h"
 
 namespace edm {
+  //This guarantees that the previous ThreadsController is destroyed before a new one starts
+  // At one time certain TBB control elements required such behavior.
   unsigned int setNThreads(unsigned int iNThreads, unsigned int iStackSize, std::unique_ptr<ThreadsController>& oPtr);
-}
+}  // namespace edm
 #endif /* FWCore_Concurrency_setNThreads_h */
