f8309ae0d826f0a55775715abcec696b9fa642da
Gabrielle Hugo <gabrielle.hugo83@gmail.com>
2020-09-04 19:44:13 +0200

    Restore old DD scenarios + move DD4hep scenarios to dedicated repo.

---
+++ Geometry/VeryForwardGeometry/python/dd4hep/geometryRPFromDD_2018_cfi.py
@@ -0,0 +1,16 @@
+import FWCore.ParameterSet.Config as cms
+
+DDDetectorESProducer = cms.ESSource("DDDetectorESProducer",
+                                            confGeomXMLFiles = cms.FileInPath('Geometry/VeryForwardGeometry/data/dd4hep/geometryRPFromDD_2018.xml'),
+                                            appendToDataLabel = cms.string('XMLIdealGeometryESSource_CTPPS')
+)
+
+DDCompactViewESProducer = cms.ESProducer("DDCompactViewESProducer",
+                                            appendToDataLabel = cms.string('XMLIdealGeometryESSource_CTPPS')
+)
+
+ctppsGeometryESModule = cms.ESProducer("PPSGeometryESProducer",
+    verbosity = cms.untracked.uint32(1),
+    detectorTag = cms.string("XMLIdealGeometryESSource_CTPPS")
+)
+
