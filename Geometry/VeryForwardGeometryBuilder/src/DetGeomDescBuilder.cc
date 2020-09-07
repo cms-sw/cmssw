4e4c6be2a3ad7c382f977fabad418f521f91dbe1
Gabrielle Hugo <gabrielle.hugo83@gmail.com>
2020-09-07 16:15:41 +0200

    scram build code-format

--- Geometry/VeryForwardGeometryBuilder/src/DetGeomDescBuilder.cc
+++ Geometry/VeryForwardGeometryBuilder/src/DetGeomDescBuilder.cc
@@ -5,48 +5,45 @@
 #include "DetectorDescription/DDCMS/interface/DDDetector.h"
 #include "FWCore/MessageLogger/interface/MessageLogger.h"
 
-
 /*
  * Generic function to build geo (tree of DetGeomDesc) from old DD compact view.
  */
 std::unique_ptr<DetGeomDesc> detgeomdescbuilder::buildDetGeomDescFromCompactView(const DDCompactView& myCompactView) {
-// Create DDFilteredView (no filter!!)
-DDPassAllFilter filter;
-DDFilteredView fv(myCompactView, filter);
+  // Create DDFilteredView (no filter!!)
+  DDPassAllFilter filter;
+  DDFilteredView fv(myCompactView, filter);
 
-// Geo info: root node.
-auto geoInfoRoot = std::make_unique<DetGeomDesc>(fv);
+  // Geo info: root node.
+  auto geoInfoRoot = std::make_unique<DetGeomDesc>(fv);
 
-// Construct the tree of children geo info (DetGeomDesc).
-detgeomdescbuilder::buildDetGeomDescDescendants(fv, geoInfoRoot.get());
+  // Construct the tree of children geo info (DetGeomDesc).
+  detgeomdescbuilder::buildDetGeomDescDescendants(fv, geoInfoRoot.get());
 
-edm::LogInfo("PPSGeometryESProducer") << "Successfully built geometry.";
+  edm::LogInfo("PPSGeometryESProducer") << "Successfully built geometry.";
 
-return geoInfoRoot;
+  return geoInfoRoot;
 }
-
 
 /*
  * Depth-first search recursion.
  * Construct the tree of children geo info (DetGeomDesc) (old DD).
  */
 void detgeomdescbuilder::buildDetGeomDescDescendants(DDFilteredView& fv, DetGeomDesc* geoInfo) {
-// Leaf
-if (!fv.firstChild())
-  return;
+  // Leaf
+  if (!fv.firstChild())
+    return;
 
-do {
-// Create node, and add it to the geoInfoParent's list.
-DetGeomDesc* child = new DetGeomDesc(fv);
-geoInfo->addComponent(child);
+  do {
+    // Create node, and add it to the geoInfoParent's list.
+    DetGeomDesc* child = new DetGeomDesc(fv);
+    geoInfo->addComponent(child);
 
-// Recursion
-buildDetGeomDescDescendants(fv, child);
-} while (fv.nextSibling());
+    // Recursion
+    buildDetGeomDescDescendants(fv, child);
+  } while (fv.nextSibling());
 
-fv.parent();
+  fv.parent();
 }
-
 
 /*
  * Generic function to build geo (tree of DetGeomDesc) from DD4hep compact view.
@@ -72,8 +69,8 @@ std::unique_ptr<DetGeomDesc> detgeomdescbuilder::buildDetGeomDescFromCompactView
     geoInfoRoot->addComponent(child);
   } while (fv.next(0));
 
-  edm::LogInfo("PPSGeometryESProducer") << "Successfully built geometry, it has "
-                                        << (geoInfoRoot->components()).size() << " DetGeomDesc nodes.";
+  edm::LogInfo("PPSGeometryESProducer") << "Successfully built geometry, it has " << (geoInfoRoot->components()).size()
+                                        << " DetGeomDesc nodes.";
 
   return geoInfoRoot;
 }
