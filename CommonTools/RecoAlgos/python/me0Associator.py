86fc6ae9e627962ab1829d5d248d48ae3b7662b9
Andrea <perrotta@cern.ch>
2020-09-08 12:33:52 +0200

    clean up

--- CommonTools/RecoAlgos/python/me0Associator.py
+++ CommonTools/RecoAlgos/python/me0Associator.py
@@ -4,7 +4,6 @@ import FWCore.ParameterSet.Config as cms
 #----------ME0Muon Collection Production for association by chi2
 me0muon = cms.EDProducer("ME0MuonTrackCollProducer",
                          me0MuonTag = cms.InputTag("me0SegmentMatching"),
-                         selectionTags = cms.vstring('All'),
                          )
 #--------------------
 me0muonColl_seq = cms.Sequence(
