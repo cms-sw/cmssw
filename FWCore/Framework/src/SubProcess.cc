e4b855f0d66a82645f0325f3cf6a0875d56db568
Christopher Jones <chrisdjones15@gmail.com>
2020-09-08 13:03:38 -0500

    Move non public API headers from FWCore/Framework/interface
    The headers moved were not used by any other header and were only
    used in the src/ or test/ sub-directories of FWCore/Framework

--- FWCore/Framework/src/SubProcess.cc
+++ FWCore/Framework/src/SubProcess.cc
@@ -1,4 +1,4 @@
-#include "FWCore/Framework/interface/SubProcess.h"
+#include "FWCore/Framework/src/SubProcess.h"
 
 #include "DataFormats/Common/interface/ThinnedAssociation.h"
 #include "DataFormats/Provenance/interface/BranchIDListHelper.h"
@@ -18,7 +18,7 @@
 #include "FWCore/Framework/interface/LuminosityBlockPrincipal.h"
 #include "FWCore/Framework/interface/ProcessBlockPrincipal.h"
 #include "FWCore/Framework/interface/OccurrenceTraits.h"
-#include "FWCore/Framework/interface/OutputModuleDescription.h"
+#include "FWCore/Framework/src/OutputModuleDescription.h"
 #include "FWCore/Framework/interface/RunPrincipal.h"
 #include "FWCore/Framework/interface/getAllTriggerNames.h"
 #include "FWCore/Framework/interface/TriggerNamesService.h"
