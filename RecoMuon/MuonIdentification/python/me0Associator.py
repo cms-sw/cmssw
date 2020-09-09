d0e079e62efb9d60629be8572f681f7e14987f5f
Andrea <perrotta@cern.ch>
2020-09-09 14:52:20 +0200

    Move ME0MuonTrackCollProducer in RecoMuon/MuonIdentification

---
+++ RecoMuon/MuonIdentification/python/me0Associator.py
@@ -0,0 +1,11 @@
+import FWCore.ParameterSet.Config as cms
+
+
+#----------ME0Muon Collection Production for association by chi2
+me0muon = cms.EDProducer("ME0MuonTrackCollProducer",
+                         me0MuonTag = cms.InputTag("me0SegmentMatching"),
+                         )
+#--------------------
+me0muonColl_seq = cms.Sequence(
+                             me0muon
+                             )
