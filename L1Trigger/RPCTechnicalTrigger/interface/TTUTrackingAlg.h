32a3501afc62f6ba43ef919b919f83d0cb831252
Matti Kortelainen <matti.kortelainen@cern.ch>
2020-09-09 21:31:59 +0200

    Sort comparison needs to be 'less than'

--- L1Trigger/RPCTechnicalTrigger/interface/TTUTrackingAlg.h
+++ L1Trigger/RPCTechnicalTrigger/interface/TTUTrackingAlg.h
@@ -134,11 +134,11 @@ private:
   };
 
   struct SortBySector {
-    bool operator()(const Seed* a, const Seed* b) { return ((*a).m_sectorId <= (*b).m_sectorId); }
+    bool operator()(const Seed* a, const Seed* b) { return ((*a).m_sectorId < (*b).m_sectorId); }
   };
 
   struct SortByLayer {
-    bool operator()(const Seed* a, const Seed* b) { return ((*a).m_stationId <= (*b).m_stationId); }
+    bool operator()(const Seed* a, const Seed* b) { return ((*a).m_stationId < (*b).m_stationId); }
   };
 
   inline void print(const std::vector<Seed*>& seeds) {
