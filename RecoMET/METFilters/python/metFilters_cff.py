8cdb9158e9850bccbe3111f7f9f089a0265651b9
Laurent <laurent.thomas@cern.ch>
2020-09-09 03:16:52 +0200

    HF Noise Event Filter 106X backport

--- RecoMET/METFilters/python/metFilters_cff.py
+++ RecoMET/METFilters/python/metFilters_cff.py
@@ -91,6 +91,9 @@ from RecoMET.METFilters.BadPFMuonFilter_cfi import *
 ## The muon bad track filter with Dz cut (2020) _____________________________________||
 from RecoMET.METFilters.BadPFMuonDzFilter_cfi import *
 
+from RecoMET.METFilters.hfNoisyHitsFilter_cfi import *
+
+
 metFilters = cms.Sequence(
    HBHENoiseFilterResultProducer *
    HBHENoiseFilter *
@@ -115,6 +118,7 @@ metFilters = cms.Sequence(
    chargedHadronTrackResolutionFilter *
    BadChargedCandidateFilter*
    BadPFMuonFilter *
+   hfNoisyHitsFilter *
    BadChargedCandidateSummer16Filter*
    BadPFMuonSummer16Filter *
    muonBadTrackFilter
