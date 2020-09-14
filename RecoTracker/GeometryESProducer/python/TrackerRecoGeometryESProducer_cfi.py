999d80bc6e94b3d37ae27a6d7e3c635a5b2e88b8
JanFSchulte <jfschulte@hotmail.com>
2020-09-14 13:30:14 -0500

    add process modifier to switch on Vector Hits

--- RecoTracker/GeometryESProducer/python/TrackerRecoGeometryESProducer_cfi.py
+++ RecoTracker/GeometryESProducer/python/TrackerRecoGeometryESProducer_cfi.py
@@ -4,6 +4,6 @@ TrackerRecoGeometryESProducer = cms.ESProducer("TrackerRecoGeometryESProducer",
   usePhase2Stacks = cms.bool(False)
 )
 
-from Configuration.Eras.Modifier_trackingPhase2PU140_cff import trackingPhase2PU140
-trackingPhase2PU140.toModify(TrackerRecoGeometryESProducer, usePhase2Stacks = False)
+from Configuration.ProcessModifiers.vectorHits_cff import vectorHits
+vectorHits.toModify(TrackerRecoGeometryESProducer, usePhase2Stacks = True)
 
