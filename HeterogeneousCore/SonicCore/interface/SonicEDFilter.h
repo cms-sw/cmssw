d6a32155319b9603f4aacc656f535528e6597a47
Kevin Pedro <kpedro88@gmail.com>
2020-09-15 15:23:33 -0500

    add filter and analyzer modules for sonic

--- HeterogeneousCore/SonicCore/interface/SonicEDFilter.h
+++ HeterogeneousCore/SonicCore/interface/SonicEDFilter.h
@@ -1,8 +1,8 @@
-#ifndef HeterogeneousCore_SonicCore_SonicEDProducer
-#define HeterogeneousCore_SonicCore_SonicEDProducer
+#ifndef HeterogeneousCore_SonicCore_SonicEDFilter
+#define HeterogeneousCore_SonicCore_SonicEDFilter
 
 #include "FWCore/Framework/interface/Event.h"
-#include "FWCore/Framework/interface/stream/EDProducer.h"
+#include "FWCore/Framework/interface/stream/EDFilter.h"
 #include "FWCore/ParameterSet/interface/ParameterSet.h"
 #include "FWCore/Framework/interface/EventSetup.h"
 #include "FWCore/Concurrency/interface/WaitingTaskWithArenaHolder.h"
@@ -12,18 +12,18 @@
 #include <string>
 #include <chrono>
 
-//this is a stream producer because client operations are not multithread-safe in general
+//this is a stream filter because client operations are not multithread-safe in general
 //it is designed such that the user never has to interact with the client or the acquire() callback directly
 template <typename Client, typename... Capabilities>
-class SonicEDProducer : public edm::stream::EDProducer<edm::ExternalWork, Capabilities...> {
+class SonicEDFilter : public edm::stream::EDFilter<edm::ExternalWork, Capabilities...> {
 public:
   //typedefs to simplify usage
   typedef typename Client::Input Input;
   typedef typename Client::Output Output;
   //constructor
-  SonicEDProducer(edm::ParameterSet const& cfg) : client_(cfg.getParameter<edm::ParameterSet>("Client")) {}
+  SonicEDFilter(edm::ParameterSet const& cfg) : client_(cfg.getParameter<edm::ParameterSet>("Client")) {}
   //destructor
-  ~SonicEDProducer() override = default;
+  ~SonicEDFilter() override = default;
 
   //derived classes use a dedicated acquire() interface that incorporates client_.input()
   //(no need to interact with callback holder)
@@ -36,18 +36,20 @@ public:
   }
   virtual void acquire(edm::Event const& iEvent, edm::EventSetup const& iSetup, Input& iInput) = 0;
   //derived classes use a dedicated produce() interface that incorporates client_.output()
-  void produce(edm::Event& iEvent, edm::EventSetup const& iSetup) final {
+  bool filter(edm::Event& iEvent, edm::EventSetup const& iSetup) final {
     //measure time between acquire and produce
     sonic_utils::printDebugTime(client_.debugName(), "dispatch() time: ", t_dispatch_);
 
     auto t0 = std::chrono::high_resolution_clock::now();
-    produce(iEvent, iSetup, client_.output());
+    bool result = produce(iEvent, iSetup, client_.output());
     sonic_utils::printDebugTime(client_.debugName(), "produce() time: ", t0);
 
     //reset client data
     client_.reset();
+
+    return result;
   }
-  virtual void produce(edm::Event& iEvent, edm::EventSetup const& iSetup, Output const& iOutput) = 0;
+  virtual bool filter(edm::Event& iEvent, edm::EventSetup const& iSetup, Output const& iOutput) = 0;
 
 protected:
   //for debugging
