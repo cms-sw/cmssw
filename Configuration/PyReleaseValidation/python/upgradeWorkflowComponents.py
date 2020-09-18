c19293d6ecac917b19fb3a3e7d386bf96e3db401
jfernan2 <Javier.Fernandez@cern.ch>
2020-09-18 16:54:43 +0200

    Update upgradeWorkflowComponents.py

--- Configuration/PyReleaseValidation/python/upgradeWorkflowComponents.py
+++ Configuration/PyReleaseValidation/python/upgradeWorkflowComponents.py
@@ -926,7 +926,7 @@ upgradeProperties[2017] = {
 for key in list(upgradeProperties[2017].keys()):
     upgradeProperties[2017][key+'PU'] = deepcopy(upgradeProperties[2017][key])
     upgradeProperties[2017][key+'PU']['ScenToRun'] = ['GenSim','DigiPU'] + \
-                                                     (['RecoPU','HARVESTPU'] if '202' in key else ['RecoFakeHLTPU','HARVESTFakeHLTPU']) + \ #2021, 2023 and 2023 still run not FakeHLT Menus
+                                                     (['RecoPU','HARVESTPU'] if '202' in key else ['RecoFakeHLTPU','HARVESTFakeHLTPU']) + \
                                                      (['Nano'] if 'Design' not in key else [])
 
 upgradeProperties[2026] = {
