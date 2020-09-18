fded037073c34a8e411e179de670c974ec4fa4cc
Salavat <abdullin@mail.cern.ch>
2020-09-18 17:51:14 +0200

    slimmedHcalRecHits for 106X

--- RecoLocalCalo/HcalRecProducers/python/HcalHitSelection_cfi.py
+++ RecoLocalCalo/HcalRecProducers/python/HcalHitSelection_cfi.py
@@ -11,5 +11,12 @@ reducedHcalRecHits = cms.EDProducer("HcalHitSelection",
                                          )
                                     )
 
+slimmedHcalRecHits = reducedHcalRecHits.clone(
+          hbheTag = "reducedHcalRecHits:hbhereco",
+          hfTag   = "reducedHcalRecHits:hfreco",
+          hoTag   = "reducedHcalRecHits:horeco",
+          interestingDetIds = ()
+       )
+
 from Configuration.Eras.Modifier_pp_on_AA_2018_cff import pp_on_AA_2018
 pp_on_AA_2018.toModify(reducedHcalRecHits.interestingDetIds, func = lambda list: list.remove(cms.InputTag("interestingOotEgammaIsoHCALDetId")) )
