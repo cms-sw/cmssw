b0ab1f1dc0501fab0b0651999111ea2f0f56bef2
Sunanda <sunanda.banerjee@cern.ch>
2020-09-20 22:08:06 +0200

    Bug fix for the v14 geometry of HGCal

--- Geometry/HGCalCommonData/plugins/DDHGCalHEFileAlgo.cc
+++ Geometry/HGCalCommonData/plugins/DDHGCalHEFileAlgo.cc
@@ -558,7 +558,7 @@ void DDHGCalHEFileAlgo::positionSensitive(
 #ifdef EDM_ML_DEBUG
       ++ntot;
 #endif
-      int type = HGCalWaferType::getType(HGCalWaferIndex::waferIndex(layer, u, v, false), waferIndex_, waferTypes_);
+      int type = HGCalWaferType::getType(HGCalWaferIndex::waferIndex((layer + 1), u, v, false), waferIndex_, waferTypes_);
       if (corner.first > 0 && type >= 0) {
         int copy = HGCalTypes::packTypeUV(type, u, v);
 #ifdef EDM_ML_DEBUG
