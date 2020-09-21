9792469ab6d47805012d154409a101dbe6bf37b5
Justin Williams <justinwilliams@ku.edu>
2020-09-21 14:18:53 -0500

    updates SimpleFlatTableProducerPlugins to work with newly added proton tables

--- PhysicsTools/NanoAOD/plugins/SimpleFlatTableProducerPlugins.cc
+++ PhysicsTools/NanoAOD/plugins/SimpleFlatTableProducerPlugins.cc
@@ -9,7 +9,15 @@ typedef EventSingletonSimpleFlatTableProducer<GenEventInfoProduct> SimpleGenEven
 #include "SimDataFormats/HTXS/interface/HiggsTemplateCrossSections.h"
 typedef EventSingletonSimpleFlatTableProducer<HTXS::HiggsClassification> SimpleHTXSFlatTableProducer;
 
+#include "DataFormats/ProtonReco/interface/ForwardProton.h"
+typedef SimpleFlatTableProducer<reco::ForwardProton> SimpleProtonTrackFlatTableProducer;
+
+#include "DataFormats/CTPPSReco/interface/CTPPSLocalTrackLite.h"
+typedef SimpleFlatTableProducer<CTPPSLocalTrackLite> SimpleLocalTrackFlatTableProducer;
+
 #include "FWCore/Framework/interface/MakerMacros.h"
 DEFINE_FWK_MODULE(SimpleCandidateFlatTableProducer);
 DEFINE_FWK_MODULE(SimpleGenEventFlatTableProducer);
 DEFINE_FWK_MODULE(SimpleHTXSFlatTableProducer);
+DEFINE_FWK_MODULE(SimpleProtonTrackFlatTableProducer);
+DEFINE_FWK_MODULE(SimpleLocalTrackFlatTableProducer);
