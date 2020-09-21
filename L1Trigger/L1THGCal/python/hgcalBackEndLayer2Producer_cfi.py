55cf0941e336ede23e6d1fbc4a30a2cef0945f09
sudha ahuja <sudha.ahuja@cern.ch>
2020-09-21 14:00:37 +0200

    seeding algorithm updates for phi smoothing
    minor change

--- L1Trigger/L1THGCal/python/hgcalBackEndLayer2Producer_cfi.py
+++ L1Trigger/L1THGCal/python/hgcalBackEndLayer2Producer_cfi.py
@@ -105,7 +105,7 @@ histoMax_C3d_clustering_params = cms.PSet(dR_multicluster=cms.double(0.03),
 # (see https://indico.cern.ch/event/806845/contributions/3359859/attachments/1815187/2966402/19-03-20_EGPerf_HGCBE.pdf
 # for more details)
 phase2_hgcalV10.toModify(histoMax_C3d_seeding_params,
-                        threshold_histo_multicluster=7.5,  # MipT
+                        threshold_histo_multicluster=8.5,  # MipT
                         )
 
 
