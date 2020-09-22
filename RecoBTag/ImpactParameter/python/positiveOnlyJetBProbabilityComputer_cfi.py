27bbeba405fafcfb5a9e878760e2d9083fd02846
Jeongeun Lee <jeongeun.lee@cern.ch>
2020-09-22 15:28:36 +0200

    drop type specs in RecoBTag

--- RecoBTag/ImpactParameter/python/positiveOnlyJetBProbabilityComputer_cfi.py
+++ RecoBTag/ImpactParameter/python/positiveOnlyJetBProbabilityComputer_cfi.py
@@ -4,5 +4,5 @@ from RecoBTag.ImpactParameter.jetBProbabilityComputer_cfi import *
 
 # positiveOnlyJetBProbability btag computer
 positiveOnlyJetBProbabilityComputer = jetBProbabilityComputer.clone(
-    trackIpSign = cms.int32(1) ## 0 = use both, 1 = positive only, -1 = negative only
+    trackIpSign = 1 ## 0 = use both, 1 = positive only, -1 = negative only
 )
--- RecoBTag/SecondaryVertex/python/pfGhostTrackVertexTagInfos_cfi.py
+++ RecoBTag/SecondaryVertex/python/pfGhostTrackVertexTagInfos_cfi.py
@@ -3,6 +3,7 @@ import FWCore.ParameterSet.Config as cms
 from RecoBTag.SecondaryVertex.pfSecondaryVertexTagInfos_cfi import *
 from RecoBTag.SecondaryVertex.ghostTrackVertexReco_cff import *
 
-pfGhostTrackVertexTagInfos = pfSecondaryVertexTagInfos.clone()
-pfGhostTrackVertexTagInfos.vertexReco = ghostTrackVertexRecoBlock.vertexReco
-pfGhostTrackVertexTagInfos.vertexCuts.multiplicityMin = 1
+pfGhostTrackVertexTagInfos = pfSecondaryVertexTagInfos.clone(
+    vertexReco = ghostTrackVertexRecoBlock.vertexReco,
+    vertexCuts = dict(multiplicityMin = 1)
+)
