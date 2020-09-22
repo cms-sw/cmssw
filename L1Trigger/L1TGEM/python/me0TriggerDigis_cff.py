fa722f2cc1ad60067ecb1fe34821d338c4e41c84
Sven Dildick <sven.dildick@cern.ch>
2020-09-22 13:16:33 -0500

    GE0 era for trigger and digitizer

--- L1Trigger/L1TGEM/python/me0TriggerDigis_cff.py
+++ L1Trigger/L1TGEM/python/me0TriggerDigis_cff.py
@@ -1,4 +1,5 @@
 import FWCore.ParameterSet.Config as cms
+from Configuration.Eras.Modifier_phase2_GE0_cff import phase2_GE0
 
 from L1Trigger.L1TGEM.simMuonME0PadDigis_cfi import *
 from L1Trigger.L1TGEM.me0TriggerDigis_cfi import *
@@ -6,3 +7,6 @@ from L1Trigger.L1TGEM.me0TriggerPseudoDigis_cff import *
 
 me0TriggerRealDigiTask = cms.Task(simMuonME0PadDigis, me0TriggerDigis)
 me0TriggerAllDigiTask = cms.Task(me0TriggerRealDigiTask, me0TriggerPseudoDigiTask)
+
+## in scenarios with GE0, remove the pseudo digis
+phase2_GE0.toReplaceWith(me0TriggerAllDigiTask, me0TriggerAllDigiTask.copyAndExclude([me0TriggerPseudoDigiTask]))
