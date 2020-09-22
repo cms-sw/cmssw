e133a6368e785888475b17ffeedae40b817d34c5
Gabrielle Hugo <gabrielle.hugo83@gmail.com>
2020-09-22 17:52:42 +0200

    VERY IMPORTANT: Fix navigation in Tracker. This allows to have a 1:1 navigation between old DD and DD4hep. The key idea is that one should not use while (fv.firstChild()) in the CmsTrackerLevelBuilder, as these calls are already intricated in a stack of calls along the volume hierarchy. This would make the volume processing advance too much, while the volumes of interest have actually not been processed yet. The solution is to TAKE ADVANTAGE OF THE STACK CALLS TO NAVIGATE DOWN THE VOLUME HIERARCHY, SWITCH TO THE NEXT VOLUME IN CmsDetConstruction ONLY (to which all calls converged to!), AND THEN WISELY USE THE CALL STACK TO GO BACK UP. In practice, it is a bit more complicated, as in addition, one need to be able to dig a bit further done in the volume hierarchy, within CmsDetConstruction, when the module has 2 sensors. This solution is elegant, simple, and fully works. All other errors are due to independent issues. For example, Phase 1 disk builer segfaults when used with DD4hep, because of hardcoded radius (this segfault did not appear before, because navigation was not ok, hence Phase 1 disk builer was never called (!!!). NB: Calls to firstChild() require an anormalous amount of time before navigation fix, and obviously still do after navigation fix, this is an independent issue.

--- Geometry/TrackerNumberingBuilder/plugins/CmsDetConstruction.cc
+++ Geometry/TrackerNumberingBuilder/plugins/CmsDetConstruction.cc
@@ -46,21 +46,21 @@ void CmsDetConstruction<FilteredView>::buildSmallDetsforStack(FilteredView& fv,
   mother->addComponent(det);
 }
 
-template <class FilteredView>
-void CmsDetConstruction<FilteredView>::buildComponent(FilteredView& fv,
-                                                      GeometricDet* mother,
-                                                      const std::string& attribute) {
+template <>
+void CmsDetConstruction<DDFilteredView>::buildComponent(DDFilteredView& fv,
+                                                        GeometricDet* mother,
+                                                        const std::string& attribute) {
   //
   // at this level I check whether it is a merged detector or not
   //
 
   GeometricDet* det = new GeometricDet(&fv,
-                                       CmsTrackerLevelBuilder<FilteredView>::theCmsTrackerStringToEnum.type(
-                                           ExtractStringFromDDD<FilteredView>::getString(attribute, &fv)));
+                                       CmsTrackerLevelBuilder<DDFilteredView>::theCmsTrackerStringToEnum.type(
+                                           ExtractStringFromDDD<DDFilteredView>::getString(attribute, &fv)));
 
   //Phase1 mergedDet: searching for sensors
-  if (CmsTrackerLevelBuilder<FilteredView>::theCmsTrackerStringToEnum.type(
-          ExtractStringFromDDD<FilteredView>::getString(attribute, &fv)) == GeometricDet::mergedDet) {
+  if (CmsTrackerLevelBuilder<DDFilteredView>::theCmsTrackerStringToEnum.type(
+          ExtractStringFromDDD<DDFilteredView>::getString(attribute, &fv)) == GeometricDet::mergedDet) {
     // I have to go one step lower ...
     bool dodets = fv.firstChild();  // descend to the first Layer
     while (dodets) {
@@ -75,8 +75,8 @@ void CmsDetConstruction<FilteredView>::buildComponent(FilteredView& fv,
   }
 
   //Phase2 stackDet: same procedure, different nomenclature
-  else if (CmsTrackerLevelBuilder<FilteredView>::theCmsTrackerStringToEnum.type(
-               ExtractStringFromDDD<FilteredView>::getString(attribute, &fv)) == GeometricDet::OTPhase2Stack) {
+  else if (CmsTrackerLevelBuilder<DDFilteredView>::theCmsTrackerStringToEnum.type(
+               ExtractStringFromDDD<DDFilteredView>::getString(attribute, &fv)) == GeometricDet::OTPhase2Stack) {
     bool dodets = fv.firstChild();
     while (dodets) {
       buildSmallDetsforStack(fv, det, attribute);
@@ -88,5 +88,70 @@ void CmsDetConstruction<FilteredView>::buildComponent(FilteredView& fv,
   mother->addComponent(det);
 }
 
-template class CmsDetConstruction<DDFilteredView>;
-template class CmsDetConstruction<cms::DDFilteredView>;
+template <>
+void CmsDetConstruction<cms::DDFilteredView>::buildComponent(cms::DDFilteredView& fv,
+                                                             GeometricDet* mother,
+                                                             const std::string& attribute) {
+  //
+  // at this level I check whether it is a merged detector or not
+  //
+
+  GeometricDet* det = new GeometricDet(&fv,
+                                       CmsTrackerLevelBuilder<cms::DDFilteredView>::theCmsTrackerStringToEnum.type(
+                                           ExtractStringFromDDD<cms::DDFilteredView>::getString(attribute, &fv)));
+
+  // PHASE 1 (MERGEDDET)
+  if (CmsTrackerLevelBuilder<cms::DDFilteredView>::theCmsTrackerStringToEnum.type(
+          ExtractStringFromDDD<cms::DDFilteredView>::getString(attribute, &fv)) == GeometricDet::mergedDet) {
+
+    // Go down in hierarchy: from module to sensor
+    if (!fv.firstChild()) {
+      std::cout << "CmsDetConstruction::buildComponent  !fv.firstChild()" << std::endl;
+      return;
+    }
+
+    // This is the sensor hierarchy level
+    const int sensorHierarchyLevel = fv.level(); 
+
+    // Loop on all siblings (ie, on all sensors)
+    while (fv.level() == sensorHierarchyLevel) {
+      buildSmallDetsforGlued(fv, det, attribute);
+
+      // Go to the next volume in FilteredView.
+      // NB: If this volume is another sensor of the same module, let's stay in the loop.
+      // Otherwise, loop is exited.
+      fv.firstChild();
+    }
+  }
+
+  // PHASE 2 (STACKDET)
+  else if (CmsTrackerLevelBuilder<cms::DDFilteredView>::theCmsTrackerStringToEnum.type(
+               ExtractStringFromDDD<cms::DDFilteredView>::getString(attribute, &fv)) == GeometricDet::OTPhase2Stack) {
+   
+    // Go down in hierarchy: from module to sensor
+    if (!fv.firstChild()) {
+      std::cout << "CmsDetConstruction::buildComponent  !fv.firstChild()" << std::endl;
+      return;
+    }
+
+    // This is the sensor hierarchy level
+    const int sensorHierarchyLevel = fv.level(); 
+
+    // Loop on all siblings (ie, on all sensors)
+    while (fv.level() == sensorHierarchyLevel) {
+      buildSmallDetsforStack(fv, det, attribute);
+
+      // Go to the next volume in FilteredView.
+      // NB: If this volume is another sensor of the same module, let's stay in the loop.
+      // Otherwise, loop is exited.
+      fv.firstChild();
+    }
+  }
+
+  else {
+    // Go to the next volume in FilteredView.
+    fv.firstChild();
+  }
+
+  mother->addComponent(det);
+}
