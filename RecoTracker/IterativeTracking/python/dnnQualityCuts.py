d907338ae870daace2e735c8a28ccbd69ab94e17
hajohajo@helsinki.fi <hajohajo@helsinki.fi>
2020-09-22 16:09:45 +0200

    Add track classifier that uses tensorflow.

---
+++ RecoTracker/IterativeTracking/python/dnnQualityCuts.py
@@ -0,0 +1,13 @@
+qualityCutDictionary = {
+   'InitialStep':                  [-0.2, 0.15, 0.40],
+   'LowPtQuadStep':                [-0.2, 0.10, 0.45],
+   'HighPtTripletStep':            [0.55, 0.60, 0.65],
+   'LowPtTripletStep':             [-0.2, -0.1, 0.10],
+   'DetachedQuadStep':             [-0.3, 0.20, 0.55],
+   'DetachedTripletStep':          [-0.5, -0.1, 0.40],
+   'PixelPairStep':                [-0.4, -0.35, -0.3],
+   'MixedTripletStep':             [-0.7, -0.5, -0.3],
+   'PixelLessStep':                [-0.8, -0.7, -0.6],
+   'TobTecStep':                   [-0.6, -0.55, -0.5],
+   'JetCoreRegionalStep':          [0.20, 0.30, 0.50]
+}
