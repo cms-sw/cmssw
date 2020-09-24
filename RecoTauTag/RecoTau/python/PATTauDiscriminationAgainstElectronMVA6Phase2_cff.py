0694b6f26862ba86e6b9e31372e293229ed48b2d
Michal Bluj <michal.bluj@cern.ch>
2020-09-24 16:42:50 +0200

    Define common setings for anti-e phase2 in one cff

--- RecoTauTag/RecoTau/python/PATTauDiscriminationAgainstElectronMVA6Phase2_cff.py
+++ RecoTauTag/RecoTau/python/PATTauDiscriminationAgainstElectronMVA6Phase2_cff.py
@@ -5,6 +5,7 @@ mergedSlimmedElectronsForTauId = cms.EDProducer('PATElectronCollectionMerger',
     src = cms.VInputTag('slimmedElectrons', 'slimmedElectronsFromMultiCl')
 )
 # anti-e phase-2 tauID (Raw)
+from RecoTauTag.RecoTau.tauDiscriminationAgainstElectronMVA6Phase2_mvaDefs_cff import mvaNames_phase2, mapping_phase2, workingPoints_phase2
 from RecoTauTag.RecoTau.TauDiscriminatorTools import noPrediscriminants
 from RecoTauTag.RecoTau.patTauDiscriminationAgainstElectronMVA6_cfi import *
 patTauDiscriminationAgainstElectronMVA6Phase2Raw = patTauDiscriminationAgainstElectronMVA6.clone(
@@ -13,94 +14,27 @@ patTauDiscriminationAgainstElectronMVA6Phase2Raw = patTauDiscriminationAgainstEl
     srcElectrons = "mergedSlimmedElectronsForTauId",
     isPhase2 = True,
     vetoEcalCracks = False,
-    mvaName_NoEleMatch_woGwoGSF_BL = "RecoTauTag_antiElectronPhase2MVA6v1_gbr_NoEleMatch_woGwoGSF_BL",
-    mvaName_NoEleMatch_wGwoGSF_BL = "RecoTauTag_antiElectronPhase2MVA6v1_gbr_NoEleMatch_wGwoGSF_BL",
-    mvaName_woGwGSF_BL = "RecoTauTag_antiElectronPhase2MVA6v1_gbr_woGwGSF_BL",
-    mvaName_wGwGSF_BL = "RecoTauTag_antiElectronPhase2MVA6v1_gbr_wGwGSF_BL",
-    mvaName_NoEleMatch_woGwoGSF_EC = "RecoTauTag_antiElectronPhase2MVA6v1_gbr_NoEleMatch_woGwoGSF_FWEC",
-    mvaName_NoEleMatch_wGwoGSF_EC = "RecoTauTag_antiElectronPhase2MVA6v1_gbr_NoEleMatch_wGwoGSF_FWEC",
-    mvaName_woGwGSF_EC = "RecoTauTag_antiElectronPhase2MVA6v1_gbr_woGwGSF_FWEC",
-    mvaName_wGwGSF_EC = "RecoTauTag_antiElectronPhase2MVA6v1_gbr_wGwGSF_FWEC",
-    mvaName_NoEleMatch_woGwoGSF_VFEC = "RecoTauTag_antiElectronPhase2MVA6v1_gbr_NoEleMatch_woGwoGSF_VFWEC",
-    mvaName_NoEleMatch_wGwoGSF_VFEC = "RecoTauTag_antiElectronPhase2MVA6v1_gbr_NoEleMatch_wGwoGSF_VFWEC",
-    mvaName_woGwGSF_VFEC = "RecoTauTag_antiElectronPhase2MVA6v1_gbr_woGwGSF_VFWEC",
-    mvaName_wGwGSF_VFEC = "RecoTauTag_antiElectronPhase2MVA6v1_gbr_wGwGSF_VFWEC"
+    mvaName_NoEleMatch_woGwoGSF_BL = mvaNames_phase2.mvaName_NoEleMatch_woGwoGSF_BL,
+    mvaName_NoEleMatch_wGwoGSF_BL = mvaNames_phase2.mvaName_NoEleMatch_wGwoGSF_BL,
+    mvaName_woGwGSF_BL = mvaNames_phase2.mvaName_woGwGSF_BL,
+    mvaName_wGwGSF_BL = mvaNames_phase2.mvaName_wGwGSF_BL,
+    mvaName_NoEleMatch_woGwoGSF_EC = mvaNames_phase2.mvaName_NoEleMatch_woGwoGSF_EC,
+    mvaName_NoEleMatch_wGwoGSF_EC = mvaNames_phase2.mvaName_NoEleMatch_wGwoGSF_EC,
+    mvaName_woGwGSF_EC = mvaNames_phase2.mvaName_woGwGSF_EC,
+    mvaName_wGwGSF_EC = mvaNames_phase2.mvaName_wGwGSF_EC,
+    mvaName_NoEleMatch_woGwoGSF_VFEC = mvaNames_phase2.mvaName_NoEleMatch_woGwoGSF_VFEC,
+    mvaName_NoEleMatch_wGwoGSF_VFEC = mvaNames_phase2.mvaName_NoEleMatch_wGwoGSF_VFEC,
+    mvaName_woGwGSF_VFEC = mvaNames_phase2.mvaName_woGwGSF_VFEC,
+    mvaName_wGwGSF_VFEC = mvaNames_phase2.mvaName_wGwGSF_VFEC
 )
 # anti-e phase-2 tauID (WPs)
 from RecoTauTag.RecoTau.patTauDiscriminantCutMultiplexerDefault_cfi import patTauDiscriminantCutMultiplexerDefault
 patTauDiscriminationAgainstElectronMVA6Phase2 = patTauDiscriminantCutMultiplexerDefault.clone(
     #Note: PFTauProducer and Prediscriminants have to be set in the final cfg
     toMultiplex = 'patTauDiscriminationAgainstElectronMVA6Phase2Raw',
-    mapping = [
-        cms.PSet(
-            category = cms.uint32(0), # minMVANoEleMatchWOgWOgsfBL
-            cut = cms.string("RecoTauTag_antiElectronPhase2MVA6v1_gbr_NoEleMatch_woGwoGSF_BL"),
-            variable = cms.string("pt")
-        ),
-        cms.PSet(
-            category = cms.uint32(2), # minMVANoEleMatchWgWOgsfBL
-            cut = cms.string("RecoTauTag_antiElectronPhase2MVA6v1_gbr_NoEleMatch_wGwoGSF_BL"),
-            variable = cms.string("pt")
-        ),
-        cms.PSet(
-            category = cms.uint32(5), # minMVAWOgWgsfBL
-            cut = cms.string("RecoTauTag_antiElectronPhase2MVA6v1_gbr_woGwGSF_BL"),
-            variable = cms.string("pt")
-        ),
-        cms.PSet(
-            category = cms.uint32(7), # minMVAWgWgsfBL
-            cut = cms.string("RecoTauTag_antiElectronPhase2MVA6v1_gbr_wGwGSF_BL"),
-            variable = cms.string("pt")
-        ),
-        cms.PSet(
-            category = cms.uint32(8), # minMVANoEleMatchWOgWOgsfEC
-            cut = cms.string("RecoTauTag_antiElectronPhase2MVA6v1_gbr_NoEleMatch_woGwoGSF_FWEC"),
-            variable = cms.string("pt")
-        ),
-        cms.PSet(
-            category = cms.uint32(9), # minMVANoEleMatchWOgWOgsfVFEC
-            cut = cms.string("RecoTauTag_antiElectronPhase2MVA6v1_gbr_NoEleMatch_woGwoGSF_VFWEC"),
-            variable = cms.string("pt")
-        ),
-        cms.PSet(
-            category = cms.uint32(10), # minMVANoEleMatchWgWOgsfEC
-            cut = cms.string("RecoTauTag_antiElectronPhase2MVA6v1_gbr_NoEleMatch_wGwoGSF_FWEC"),
-            variable = cms.string("pt")
-        ),
-        cms.PSet(
-            category = cms.uint32(11), # minMVANoEleMatchWgWOgsfVFEC
-            cut = cms.string("RecoTauTag_antiElectronPhase2MVA6v1_gbr_NoEleMatch_wGwoGSF_VFWEC"),
-            variable = cms.string("pt")
-        ),
-        cms.PSet(
-            category = cms.uint32(13), # minMVAWOgWgsfEC
-            cut = cms.string("RecoTauTag_antiElectronPhase2MVA6v1_gbr_woGwGSF_FWEC"),
-            variable = cms.string("pt")
-        ),
-        cms.PSet(
-            category = cms.uint32(14), # minMVAWOgWgsfVFEC
-            cut = cms.string("RecoTauTag_antiElectronPhase2MVA6v1_gbr_woGwGSF_VFWEC"),
-            variable = cms.string("pt")
-        ),
-        cms.PSet(
-            category = cms.uint32(15), # minMVAWgWgsfEC
-            cut = cms.string("RecoTauTag_antiElectronPhase2MVA6v1_gbr_wGwGSF_FWEC"),
-            variable = cms.string("pt")
-        ),
-        cms.PSet(
-            category = cms.uint32(16), # minMVAWgWgsfVFEC
-            cut = cms.string("RecoTauTag_antiElectronPhase2MVA6v1_gbr_wGwGSF_VFWEC"),
-            variable = cms.string("pt")
-        )
-    ],
+    mapping = mapping_phase2,
     rawValues = ["discriminator", "category"],
-    workingPoints = cms.vstring(
-        "_WPEff98",
-        "_WPEff90",
-        "_WPEff80",
-        "_WPEff70",
-        "_WPEff60"
-    )
+    workingPoints = workingPoints_phase2
 )
 
 patTauDiscriminationAgainstElectronMVA6Phase2Task = cms.Task(
--- RecoTauTag/RecoTau/python/PFRecoTauDiscriminationAgainstElectronMVA6Phase2_cff.py
+++ RecoTauTag/RecoTau/python/PFRecoTauDiscriminationAgainstElectronMVA6Phase2_cff.py
@@ -19,6 +19,7 @@ hgcElectronIdForTauId = hgcalElectronIDValueMap.clone(
     electrons = "mergedGsfElectronsForTauId"
 )
 # anti-e phase-2 tauID (raw)
+from RecoTauTag.RecoTau.tauDiscriminationAgainstElectronMVA6Phase2_mvaDefs_cff import mvaNames_phase2, mapping_phase2, workingPoints_phase2
 from RecoTauTag.RecoTau.pfRecoTauDiscriminationAgainstElectronMVA6_cfi import *
 pfRecoTauDiscriminationAgainstElectronMVA6Phase2Raw = pfRecoTauDiscriminationAgainstElectronMVA6.clone(
     #Note: PFTauProducer and Prediscriminants have to be set in the final cfg
@@ -26,94 +27,27 @@ pfRecoTauDiscriminationAgainstElectronMVA6Phase2Raw = pfRecoTauDiscriminationAga
     isPhase2 = True,
     vetoEcalCracks = False,
     hgcalElectronIDs = [cms.InputTag("hgcElectronIdForTauId", key) for key in hgcElectronIdForTauId.variables],
-    mvaName_NoEleMatch_woGwoGSF_BL = "RecoTauTag_antiElectronPhase2MVA6v1_gbr_NoEleMatch_woGwoGSF_BL",
-    mvaName_NoEleMatch_wGwoGSF_BL = "RecoTauTag_antiElectronPhase2MVA6v1_gbr_NoEleMatch_wGwoGSF_BL",
-    mvaName_woGwGSF_BL = "RecoTauTag_antiElectronPhase2MVA6v1_gbr_woGwGSF_BL",
-    mvaName_wGwGSF_BL = "RecoTauTag_antiElectronPhase2MVA6v1_gbr_wGwGSF_BL",
-    mvaName_NoEleMatch_woGwoGSF_EC = "RecoTauTag_antiElectronPhase2MVA6v1_gbr_NoEleMatch_woGwoGSF_FWEC",
-    mvaName_NoEleMatch_wGwoGSF_EC = "RecoTauTag_antiElectronPhase2MVA6v1_gbr_NoEleMatch_wGwoGSF_FWEC",
-    mvaName_woGwGSF_EC = "RecoTauTag_antiElectronPhase2MVA6v1_gbr_woGwGSF_FWEC",
-    mvaName_wGwGSF_EC = "RecoTauTag_antiElectronPhase2MVA6v1_gbr_wGwGSF_FWEC",
-    mvaName_NoEleMatch_woGwoGSF_VFEC = "RecoTauTag_antiElectronPhase2MVA6v1_gbr_NoEleMatch_woGwoGSF_VFWEC",
-    mvaName_NoEleMatch_wGwoGSF_VFEC = "RecoTauTag_antiElectronPhase2MVA6v1_gbr_NoEleMatch_wGwoGSF_VFWEC",
-    mvaName_woGwGSF_VFEC = "RecoTauTag_antiElectronPhase2MVA6v1_gbr_woGwGSF_VFWEC",
-    mvaName_wGwGSF_VFEC = "RecoTauTag_antiElectronPhase2MVA6v1_gbr_wGwGSF_VFWEC"
+    mvaName_NoEleMatch_woGwoGSF_BL = mvaNames_phase2.mvaName_NoEleMatch_woGwoGSF_BL,
+    mvaName_NoEleMatch_wGwoGSF_BL = mvaNames_phase2.mvaName_NoEleMatch_wGwoGSF_BL,
+    mvaName_woGwGSF_BL = mvaNames_phase2.mvaName_woGwGSF_BL,
+    mvaName_wGwGSF_BL = mvaNames_phase2.mvaName_wGwGSF_BL,
+    mvaName_NoEleMatch_woGwoGSF_EC = mvaNames_phase2.mvaName_NoEleMatch_woGwoGSF_EC,
+    mvaName_NoEleMatch_wGwoGSF_EC = mvaNames_phase2.mvaName_NoEleMatch_wGwoGSF_EC,
+    mvaName_woGwGSF_EC = mvaNames_phase2.mvaName_woGwGSF_EC,
+    mvaName_wGwGSF_EC = mvaNames_phase2.mvaName_wGwGSF_EC,
+    mvaName_NoEleMatch_woGwoGSF_VFEC = mvaNames_phase2.mvaName_NoEleMatch_woGwoGSF_VFEC,
+    mvaName_NoEleMatch_wGwoGSF_VFEC = mvaNames_phase2.mvaName_NoEleMatch_wGwoGSF_VFEC,
+    mvaName_woGwGSF_VFEC = mvaNames_phase2.mvaName_woGwGSF_VFEC,
+    mvaName_wGwGSF_VFEC = mvaNames_phase2.mvaName_wGwGSF_VFEC
 )
 # anti-e phase-2 tauID (WPs)
 from RecoTauTag.RecoTau.recoTauDiscriminantCutMultiplexerDefault_cfi import recoTauDiscriminantCutMultiplexerDefault
 pfRecoTauDiscriminationAgainstElectronMVA6Phase2 = recoTauDiscriminantCutMultiplexerDefault.clone(
     #Note: PFTauProducer and Prediscriminants have to be set in the final cfg
     toMultiplex = "pfRecoTauDiscriminationAgainstElectronMVA6Phase2Raw",
-    mapping = [
-        cms.PSet(
-            category = cms.uint32(0), # minMVANoEleMatchWOgWOgsfBL
-            cut = cms.string("RecoTauTag_antiElectronPhase2MVA6v1_gbr_NoEleMatch_woGwoGSF_BL"),
-            variable = cms.string("pt")
-        ),
-        cms.PSet(
-            category = cms.uint32(2), # minMVANoEleMatchWgWOgsfBL
-            cut = cms.string("RecoTauTag_antiElectronPhase2MVA6v1_gbr_NoEleMatch_wGwoGSF_BL"),
-            variable = cms.string("pt")
-        ),
-        cms.PSet(
-            category = cms.uint32(5), # minMVAWOgWgsfBL
-            cut = cms.string("RecoTauTag_antiElectronPhase2MVA6v1_gbr_woGwGSF_BL"),
-            variable = cms.string("pt")
-        ),
-        cms.PSet(
-            category = cms.uint32(7), # minMVAWgWgsfBL
-            cut = cms.string("RecoTauTag_antiElectronPhase2MVA6v1_gbr_wGwGSF_BL"),
-            variable = cms.string("pt")
-        ),
-        cms.PSet(
-            category = cms.uint32(8), # minMVANoEleMatchWOgWOgsfEC
-            cut = cms.string("RecoTauTag_antiElectronPhase2MVA6v1_gbr_NoEleMatch_woGwoGSF_FWEC"),
-            variable = cms.string("pt")
-        ),
-        cms.PSet(
-            category = cms.uint32(9), # minMVANoEleMatchWOgWOgsfVFEC
-            cut = cms.string("RecoTauTag_antiElectronPhase2MVA6v1_gbr_NoEleMatch_woGwoGSF_VFWEC"),
-            variable = cms.string("pt")
-        ),
-        cms.PSet(
-            category = cms.uint32(10), # minMVANoEleMatchWgWOgsfEC
-            cut = cms.string("RecoTauTag_antiElectronPhase2MVA6v1_gbr_NoEleMatch_wGwoGSF_FWEC"),
-            variable = cms.string("pt")
-        ),
-        cms.PSet(
-            category = cms.uint32(11), # minMVANoEleMatchWgWOgsfVFEC
-            cut = cms.string("RecoTauTag_antiElectronPhase2MVA6v1_gbr_NoEleMatch_wGwoGSF_VFWEC"),
-            variable = cms.string("pt")
-        ),
-        cms.PSet(
-            category = cms.uint32(13), # minMVAWOgWgsfEC
-            cut = cms.string("RecoTauTag_antiElectronPhase2MVA6v1_gbr_woGwGSF_FWEC"),
-            variable = cms.string("pt")
-        ),
-        cms.PSet(
-            category = cms.uint32(14), # minMVAWOgWgsfVFEC
-            cut = cms.string("RecoTauTag_antiElectronPhase2MVA6v1_gbr_woGwGSF_VFWEC"),
-            variable = cms.string("pt")
-        ),
-        cms.PSet(
-            category = cms.uint32(15), # minMVAWgWgsfEC
-            cut = cms.string("RecoTauTag_antiElectronPhase2MVA6v1_gbr_wGwGSF_FWEC"),
-            variable = cms.string("pt")
-        ),
-        cms.PSet(
-            category = cms.uint32(16), # minMVAWgWgsfVFEC
-            cut = cms.string("RecoTauTag_antiElectronPhase2MVA6v1_gbr_wGwGSF_VFWEC"),
-            variable = cms.string("pt")
-        )
-    ],
+    mapping = mapping_phase2,
     rawValues = ["discriminator", "category"],
-    workingPoints = cms.vstring(
-        "_WPEff98",
-        "_WPEff90",
-        "_WPEff80",
-        "_WPEff70",
-        "_WPEff60"
-    )
+    workingPoints = workingPoints_phase2
 )
 
 electronsForTauDiscriminationAgainstElectronMVA6Phase2Task = cms.Task(
