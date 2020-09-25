e12a82c6ef25c4d9ac556d2779ecc66080885e08
Gabrielle Hugo <gabrielle.hugo83@gmail.com>
2020-09-25 13:11:35 +0200

    Make CmsDetConstruction understandable.

--- Geometry/TrackerNumberingBuilder/plugins/CmsDetConstruction.cc
+++ Geometry/TrackerNumberingBuilder/plugins/CmsDetConstruction.cc
@@ -43,55 +43,82 @@ void CmsDetConstruction<FilteredView>::buildSmallDetsforStack(FilteredView& fv,
   mother->addComponent(det);
 }
 
+/*
+ * OLD DD.
+ * Module with 2 sensors: calculate the sensor local ID, and add the sensor to its mother volume (module).
+ * Module with 1 sensor: just add the sensor to to its mother volume (ladder).
+ */
 template <>
 void CmsDetConstruction<DDFilteredView>::buildComponent(DDFilteredView& fv,
                                                         GeometricDet* mother,
                                                         const std::string& attribute) {
 
-  GeometricDet* det = new GeometricDet(&fv,
-                                       CmsTrackerLevelBuilder<DDFilteredView>::theCmsTrackerStringToEnum.type(
-                                           ExtractStringFromDDD<DDFilteredView>::getString(attribute, &fv)));
+  // Mother volume
+  // Module with 2 sensors: the mother volume is the module volume.
+  // Module with 1 sensor: the mother volume is the ladder volume.
+  const std::string& myTopologicalNameInXMLs = ExtractStringFromDDD<DDFilteredView>::getString(attribute, &fv);
+  const GeometricDet::GDEnumType& myTopologicalType = CmsTrackerLevelBuilder<DDFilteredView>::theCmsTrackerStringToEnum.type(myTopologicalNameInXMLs);
 
-  //Phase1 mergedDet: searching for sensors
-  if (CmsTrackerLevelBuilder<DDFilteredView>::theCmsTrackerStringToEnum.type(
-          ExtractStringFromDDD<DDFilteredView>::getString(attribute, &fv)) == GeometricDet::mergedDet) {
-    // I have to go one step lower ...
-    bool dodets = fv.firstChild();  // descend to the first Layer
+  GeometricDet* det = new GeometricDet(&fv, myTopologicalType);
+
+  const bool isPhase1ModuleWith2Sensors = (myTopologicalType == GeometricDet::mergedDet);
+  const bool isPhase2ModuleWith2Sensors = (myTopologicalType == GeometricDet::OTPhase2Stack);
+
+  // CASE A: MODULE HAS 2 SENSORS
+  if (isPhase1ModuleWith2Sensors || isPhase2ModuleWith2Sensors) {
+  
+    // Go down in hierarchy: from module to sensor
+    bool dodets = fv.firstChild(); // very important
     while (dodets) {
-      buildSmallDetsforGlued(fv, det, attribute);
-      dodets = setNext(fv);
-    }
-    fv.parent();
 
+      // PHASE 1 (MERGEDDET)
+      if (isPhase1ModuleWith2Sensors) {
+	buildSmallDetsforGlued(fv, det, attribute);
+      }
+      // PHASE 2 (STACKDET)
+      else if (isPhase2ModuleWith2Sensors) {
+	buildSmallDetsforStack(fv, det, attribute);
+      }
+
+      dodets = fv.nextSibling();
+    }
+
+    fv.parent();
   }
 
-  //Phase2 stackDet: same procedure, different nomenclature
-  else if (CmsTrackerLevelBuilder<DDFilteredView>::theCmsTrackerStringToEnum.type(
-               ExtractStringFromDDD<DDFilteredView>::getString(attribute, &fv)) == GeometricDet::OTPhase2Stack) {
-    bool dodets = fv.firstChild();
-    while (dodets) {
-      buildSmallDetsforStack(fv, det, attribute);
-      dodets = setNext(fv);
-    }
-    fv.parent();
-  }
+  // CASE B: MODULE HAS 1 SENSOR: NOTHING SPECIFIC TO DO
+  // Indeed, we are not going to sort sensors within module, if there is only 1 sensor!
 
+  // ALL CASES: add sensor to its mother volume (module or ladder).
   mother->addComponent(det);
 }
 
+
+/*
+ * DD4hep.
+ * Module with 2 sensors: calculate the sensor local ID, and add the sensor to its mother volume (module).
+ * Module with 1 sensor: just add the sensor to its mother volume (ladder).
+ */
 template <>
 void CmsDetConstruction<cms::DDFilteredView>::buildComponent(cms::DDFilteredView& fv,
                                                              GeometricDet* mother,
                                                              const std::string& attribute) {
-  GeometricDet* det = new GeometricDet(&fv,
-                                       CmsTrackerLevelBuilder<cms::DDFilteredView>::theCmsTrackerStringToEnum.type(
-                                           ExtractStringFromDDD<cms::DDFilteredView>::getString(attribute, &fv)));
 
-  // PHASE 1 (MERGEDDET)
-  if (CmsTrackerLevelBuilder<cms::DDFilteredView>::theCmsTrackerStringToEnum.type(
-          ExtractStringFromDDD<cms::DDFilteredView>::getString(attribute, &fv)) == GeometricDet::mergedDet) {
+  // Mother volume
+  // Module with 2 sensors: the mother volume is the module volume.
+  // Module with 1 sensor: the mother volume is the ladder volume.
+  const std::string& myTopologicalNameInXMLs = ExtractStringFromDDD<cms::DDFilteredView>::getString(attribute, &fv);
+  const GeometricDet::GDEnumType& myTopologicalType = CmsTrackerLevelBuilder<cms::DDFilteredView>::theCmsTrackerStringToEnum.type(myTopologicalNameInXMLs);
+  GeometricDet* det = new GeometricDet(&fv, myTopologicalType);
+
+  const bool isPhase1ModuleWith2Sensors = (myTopologicalType == GeometricDet::mergedDet);
+  const bool isPhase2ModuleWith2Sensors = (myTopologicalType == GeometricDet::OTPhase2Stack);
+
+  // CASE A: MODULE HAS 2 SENSORS
+  if (isPhase1ModuleWith2Sensors || isPhase2ModuleWith2Sensors) {
+
     // Go down in hierarchy: from module to sensor
-    if (!fv.firstChild()) {
+    if (!fv.firstChild()) { // very important
       edm::LogError("CmsDetConstruction::buildComponent. Cannot go down to sensor volume.");
       return;
     }
@@ -101,40 +128,30 @@ void CmsDetConstruction<cms::DDFilteredView>::buildComponent(cms::DDFilteredView
 
     // Loop on all siblings (ie, on all sensors)
     while (fv.level() == sensorHierarchyLevel) {
-      buildSmallDetsforGlued(fv, det, attribute);
+
+      // PHASE 1 (MERGEDDET)
+      if (isPhase1ModuleWith2Sensors) {
+	buildSmallDetsforGlued(fv, det, attribute);
+      }
+      // PHASE 2 (STACKDET)
+      else if (isPhase2ModuleWith2Sensors) {
+	buildSmallDetsforStack(fv, det, attribute);
+      }
 
       // Go to the next volume in FilteredView.
       // NB: If this volume is another sensor of the same module, will stay in the loop.
+      // Otherwise, it is very important to access the next volume to be treated anyway.
       fv.firstChild();
     }
   }
 
-  // PHASE 2 (STACKDET)
-  else if (CmsTrackerLevelBuilder<cms::DDFilteredView>::theCmsTrackerStringToEnum.type(
-               ExtractStringFromDDD<cms::DDFilteredView>::getString(attribute, &fv)) == GeometricDet::OTPhase2Stack) {
-    // Go down in hierarchy: from module to sensor
-    if (!fv.firstChild()) {
-      edm::LogError("CmsDetConstruction::buildComponent. Cannot go down to sensor volume.");
-      return;
-    }
-
-    // This is the sensor hierarchy level
-    const int sensorHierarchyLevel = fv.level();
-
-    // Loop on all siblings (ie, on all sensors)
-    while (fv.level() == sensorHierarchyLevel) {
-      buildSmallDetsforStack(fv, det, attribute);
-
-      // Go to the next volume in FilteredView.
-      // NB: If this volume is another sensor of the same module, will stay in the loop.
-      fv.firstChild();
-    }
-  }
-
+  // CASE B: MODULE HAS 1 SENSOR: NOTHING SPECIFIC TO DO
+  // Indeed, we are not going to sort sensors within module, if there is only 1 sensor!
   else {
     // Go to the next volume in FilteredView.
     fv.firstChild();
   }
 
+  // ALL CASES: add sensor to its mother volume (module or ladder).
   mother->addComponent(det);
 }
