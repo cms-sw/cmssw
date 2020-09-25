aded05193bbcfda9dd7acda3f33ae138bb7cb405
Gabrielle Hugo <gabrielle.hugo83@gmail.com>
2020-09-25 11:01:22 +0200

    Remove aweful style of having starting underscore for GeometricDet data members by trailing underscores.

--- Geometry/TrackerNumberingBuilder/src/GeometricDet.cc
+++ Geometry/TrackerNumberingBuilder/src/GeometricDet.cc
@@ -69,62 +69,62 @@ GeometricDet::~GeometricDet() { deleteComponents(); }
   Constructor from old DD Filtered view.
  */
 GeometricDet::GeometricDet(DDFilteredView* fv, GeometricEnumType type)
-    : _trans(fv->translation()),
-      _phi(_trans.Phi()),
-      _rho(_trans.Rho()),
-      _rot(fv->rotation()),
-      _shape(cms::dd::name_from_value(cms::LegacySolidShapeMap, fv->shape())),
-      _ddname(fv->name()),
-      _type(type),
-      _params(fv->parameters()),
-      _radLength(getDouble("TrackerRadLength", *fv)),
-      _xi(getDouble("TrackerXi", *fv)),
-      _pixROCRows(getDouble("PixelROCRows", *fv)),
-      _pixROCCols(getDouble("PixelROCCols", *fv)),
-      _pixROCx(getDouble("PixelROC_X", *fv)),
-      _pixROCy(getDouble("PixelROC_Y", *fv)),
-      _stereo(getString("TrackerStereoDetectors", *fv) == strue),
-      _isLowerSensor(getString("TrackerLowerDetectors", *fv) == strue),
-      _isUpperSensor(getString("TrackerUpperDetectors", *fv) == strue),
-      _siliconAPVNum(getDouble("SiliconAPVNumber", *fv)),
-      _isFromDD4hep(false) {
-  //  workaround instead of this at initialization _ddd(fv->navPos().begin(),fv->navPos().end()),
+    : trans_(fv->translation()),
+      phi_(trans_.Phi()),
+      rho_(trans_.Rho()),
+      rot_(fv->rotation()),
+      shape_(cms::dd::name_from_value(cms::LegacySolidShapeMap, fv->shape())),
+      ddname_(fv->name()),
+      type_(type),
+      params_(fv->parameters()),
+      radLength_(getDouble("TrackerRadLength", *fv)),
+      xi_(getDouble("TrackerXi", *fv)),
+      pixROCRows_(getDouble("PixelROCRows", *fv)),
+      pixROCCols_(getDouble("PixelROCCols", *fv)),
+      pixROCx_(getDouble("PixelROCX_", *fv)),
+      pixROCy_(getDouble("PixelROCY_", *fv)),
+      stereo_(getString("TrackerStereoDetectors", *fv) == strue),
+      isLowerSensor_(getString("TrackerLowerDetectors", *fv) == strue),
+      isUpperSensor_(getString("TrackerUpperDetectors", *fv) == strue),
+      siliconAPVNum_(getDouble("SiliconAPVNumber", *fv)),
+      isFromDD4hep_(false) {
+  //  workaround instead of this at initialization ddd_(fv->navPos().begin(),fv->navPos().end()),
   const DDFilteredView::nav_type& nt = fv->navPos();
-  _ddd = nav_type(nt.begin(), nt.end());
+  ddd_ = nav_type(nt.begin(), nt.end());
 }
 
 /*
   Constructor from DD4HEP Filtered view.
  */
 GeometricDet::GeometricDet(cms::DDFilteredView* fv, GeometricEnumType type)
-    : _trans(geant_units::operators::convertCmToMm(fv->translation())),
-      _phi(_trans.Phi()),
-      _rho(_trans.Rho()),
-      _rot(fv->rotation()),
-      _shape(fv->shape()),
-      _ddd(fv->navPos()),  // To be studied
-      _ddname(dd4hep::dd::noNamespace(fv->name())),
-      _type(type),
-      _params(computeLegacyShapeParameters(_shape, fv->solid())),
-      _radLength(fv->get<double>("TrackerRadLength")),  // To be studied
-      _xi(fv->get<double>("TrackerXi")),                // To be studied
-      _pixROCRows(fv->get<double>("PixelROCRows")),
-      _pixROCCols(fv->get<double>("PixelROCCols")),
-      _pixROCx(fv->get<double>("PixelROC_X")),
-      _pixROCy(fv->get<double>("PixelROC_Y")),
-      _stereo(fv->get<std::string_view>("TrackerStereoDetectors") == strue),
-      _isLowerSensor(fv->get<std::string_view>("TrackerLowerDetectors") == strue),
-      _isUpperSensor(fv->get<std::string_view>("TrackerUpperDetectors") == strue),
-      _siliconAPVNum(fv->get<double>("SiliconAPVNumber")),
-      _isFromDD4hep(true) {}
+    : trans_(geant_units::operators::convertCmToMm(fv->translation())),
+      phi_(trans_.Phi()),
+      rho_(trans_.Rho()),
+      rot_(fv->rotation()),
+      shape_(fv->shape()),
+      ddd_(fv->navPos()),  // To be studied
+      ddname_(dd4hep::dd::noNamespace(fv->name())),
+      type_(type),
+      params_(computeLegacyShapeParameters(shape_, fv->solid())),
+      radLength_(fv->get<double>("TrackerRadLength")),  // To be studied
+      xi_(fv->get<double>("TrackerXi")),                // To be studied
+      pixROCRows_(fv->get<double>("PixelROCRows")),
+      pixROCCols_(fv->get<double>("PixelROCCols")),
+      pixROCx_(fv->get<double>("PixelROCX_")),
+      pixROCy_(fv->get<double>("PixelROCY_")),
+      stereo_(fv->get<std::string_view>("TrackerStereoDetectors") == strue),
+      isLowerSensor_(fv->get<std::string_view>("TrackerLowerDetectors") == strue),
+      isUpperSensor_(fv->get<std::string_view>("TrackerUpperDetectors") == strue),
+      siliconAPVNum_(fv->get<double>("SiliconAPVNumber")),
+      isFromDD4hep_(true) {}
 
 // PGeometricDet is persistent version... make it... then come back here and make the
 // constructor.
 GeometricDet::GeometricDet(const PGeometricDet::Item& onePGD, GeometricEnumType type)
-    : _trans(onePGD._x, onePGD._y, onePGD._z),
-      _phi(onePGD._phi),  //_trans.Phi()),
-      _rho(onePGD._rho),  //_trans.Rho()),
-      _rot(onePGD._a11,
+    : trans_(onePGD._x, onePGD._y, onePGD._z),
+      phi_(onePGD._phi),  //trans_.Phi()),
+      rho_(onePGD._rho),  //trans_.Rho()),
+      rot_(onePGD._a11,
            onePGD._a12,
            onePGD._a13,
            onePGD._a21,
@@ -133,54 +133,54 @@ GeometricDet::GeometricDet(const PGeometricDet::Item& onePGD, GeometricEnumType 
            onePGD._a31,
            onePGD._a32,
            onePGD._a33),
-      _shape(cms::dd::name_from_value(cms::LegacySolidShapeMap, static_cast<LegacySolidShape>(onePGD._shape))),
-      _ddd(),
-      _ddname(onePGD._name),
-      _type(type),
-      _params(),
-      _geographicalID(onePGD._geographicalID),
-      _radLength(onePGD._radLength),
-      _xi(onePGD._xi),
-      _pixROCRows(onePGD._pixROCRows),
-      _pixROCCols(onePGD._pixROCCols),
-      _pixROCx(onePGD._pixROCx),
-      _pixROCy(onePGD._pixROCy),
-      _stereo(onePGD._stereo),
-      _siliconAPVNum(onePGD._siliconAPVNum) {
+      shape_(cms::dd::name_from_value(cms::LegacySolidShapeMap, static_cast<LegacySolidShape>(onePGD._shape))),
+      ddd_(),
+      ddname_(onePGD._name),
+      type_(type),
+      params_(),
+      geographicalID_(onePGD._geographicalID),
+      radLength_(onePGD._radLength),
+      xi_(onePGD._xi),
+      pixROCRows_(onePGD._pixROCRows),
+      pixROCCols_(onePGD._pixROCCols),
+      pixROCx_(onePGD._pixROCx),
+      pixROCy_(onePGD._pixROCy),
+      stereo_(onePGD._stereo),
+      siliconAPVNum_(onePGD._siliconAPVNum) {
   if (onePGD._shape == 1 || onePGD._shape == 3) {  //The parms vector is neede only in the case of box or trap shape
-    _params.reserve(11);
-    _params.emplace_back(onePGD._params0);
-    _params.emplace_back(onePGD._params1);
-    _params.emplace_back(onePGD._params2);
-    _params.emplace_back(onePGD._params3);
-    _params.emplace_back(onePGD._params4);
-    _params.emplace_back(onePGD._params5);
-    _params.emplace_back(onePGD._params6);
-    _params.emplace_back(onePGD._params7);
-    _params.emplace_back(onePGD._params8);
-    _params.emplace_back(onePGD._params9);
-    _params.emplace_back(onePGD._params10);
+    params_.reserve(11);
+    params_.emplace_back(onePGD._params0);
+    params_.emplace_back(onePGD._params1);
+    params_.emplace_back(onePGD._params2);
+    params_.emplace_back(onePGD._params3);
+    params_.emplace_back(onePGD._params4);
+    params_.emplace_back(onePGD._params5);
+    params_.emplace_back(onePGD._params6);
+    params_.emplace_back(onePGD._params7);
+    params_.emplace_back(onePGD._params8);
+    params_.emplace_back(onePGD._params9);
+    params_.emplace_back(onePGD._params10);
   }
 
-  _ddd.reserve(onePGD._numnt);
-  _ddd.emplace_back(onePGD._nt0);
-  _ddd.emplace_back(onePGD._nt1);
-  _ddd.emplace_back(onePGD._nt2);
-  _ddd.emplace_back(onePGD._nt3);
+  ddd_.reserve(onePGD._numnt);
+  ddd_.emplace_back(onePGD._nt0);
+  ddd_.emplace_back(onePGD._nt1);
+  ddd_.emplace_back(onePGD._nt2);
+  ddd_.emplace_back(onePGD._nt3);
   if (onePGD._numnt > 4) {
-    _ddd.emplace_back(onePGD._nt4);
+    ddd_.emplace_back(onePGD._nt4);
     if (onePGD._numnt > 5) {
-      _ddd.emplace_back(onePGD._nt5);
+      ddd_.emplace_back(onePGD._nt5);
       if (onePGD._numnt > 6) {
-        _ddd.emplace_back(onePGD._nt6);
+        ddd_.emplace_back(onePGD._nt6);
         if (onePGD._numnt > 7) {
-          _ddd.emplace_back(onePGD._nt7);
+          ddd_.emplace_back(onePGD._nt7);
           if (onePGD._numnt > 8) {
-            _ddd.emplace_back(onePGD._nt8);
+            ddd_.emplace_back(onePGD._nt8);
             if (onePGD._numnt > 9) {
-              _ddd.emplace_back(onePGD._nt9);
+              ddd_.emplace_back(onePGD._nt9);
               if (onePGD._numnt > 10) {
-                _ddd.emplace_back(onePGD._nt10);
+                ddd_.emplace_back(onePGD._nt10);
               }
             }
           }
@@ -194,29 +194,29 @@ GeometricDet::ConstGeometricDetContainer GeometricDet::deepComponents() const {
   //
   // iterate on all the components ;)
   //
-  ConstGeometricDetContainer _temp;
-  deepComponents(_temp);
-  return _temp;
+  ConstGeometricDetContainer temp_;
+  deepComponents(temp_);
+  return temp_;
 }
 
 void GeometricDet::deepComponents(ConstGeometricDetContainer& cont) const {
   if (isLeaf())
     cont.emplace_back(this);
   else
-    std::for_each(_container.begin(), _container.end(), [&](const GeometricDet* iDet) { iDet->deepComponents(cont); });
+    std::for_each(container_.begin(), container_.end(), [&](const GeometricDet* iDet) { iDet->deepComponents(cont); });
 }
 
 void GeometricDet::addComponents(GeometricDetContainer const& cont) {
-  _container.reserve(_container.size() + cont.size());
-  std::copy(cont.begin(), cont.end(), back_inserter(_container));
+  container_.reserve(container_.size() + cont.size());
+  std::copy(cont.begin(), cont.end(), back_inserter(container_));
 }
 
 void GeometricDet::addComponents(ConstGeometricDetContainer const& cont) {
-  _container.reserve(_container.size() + cont.size());
-  std::copy(cont.begin(), cont.end(), back_inserter(_container));
+  container_.reserve(container_.size() + cont.size());
+  std::copy(cont.begin(), cont.end(), back_inserter(container_));
 }
 
-void GeometricDet::addComponent(GeometricDet* det) { _container.emplace_back(det); }
+void GeometricDet::addComponent(GeometricDet* det) { container_.emplace_back(det); }
 
 namespace {
   struct Deleter {
@@ -225,19 +225,19 @@ namespace {
 }  // namespace
 
 void GeometricDet::deleteComponents() {
-  std::for_each(_container.begin(), _container.end(), Deleter());
-  _container.clear();
+  std::for_each(container_.begin(), container_.end(), Deleter());
+  container_.clear();
 }
 
 GeometricDet::Position GeometricDet::positionBounds() const {
-  Position _pos(float(_trans.x() / cm), float(_trans.y() / cm), float(_trans.z() / cm));
-  return _pos;
+  Position pos_(float(trans_.x() / cm), float(trans_.y() / cm), float(trans_.z() / cm));
+  return pos_;
 }
 
 GeometricDet::Rotation GeometricDet::rotationBounds() const {
   Translation x, y, z;
-  _rot.GetComponents(x, y, z);
-  Rotation _rotation(float(x.X()),
+  rot_.GetComponents(x, y, z);
+  Rotation rotation_(float(x.X()),
                      float(x.Y()),
                      float(x.Z()),
                      float(y.X()),
@@ -246,13 +246,13 @@ GeometricDet::Rotation GeometricDet::rotationBounds() const {
                      float(z.X()),
                      float(z.Y()),
                      float(z.Z()));
-  return _rotation;
+  return rotation_;
 }
 
 std::unique_ptr<Bounds> GeometricDet::bounds() const {
-  const std::vector<double>& par = _params;
+  const std::vector<double>& par = params_;
   TrackerShapeToBounds shapeToBounds;
-  return std::unique_ptr<Bounds>(shapeToBounds.buildBounds(_shape, par));
+  return std::unique_ptr<Bounds>(shapeToBounds.buildBounds(shape_, par));
 }
 
 /*
