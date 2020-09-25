85a5445ba2d60bb5cd565816d922a3b75eed6a6c
Kevin Pedro <kpedro88@gmail.com>
2020-09-25 15:07:34 -0500

    simplify and fix tests

--- HeterogeneousCore/SonicTriton/test/fetch_model.sh
+++ HeterogeneousCore/SonicTriton/test/fetch_model.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 
-# borrowed from https://github.com/NVIDIA/triton-inference-server/tree/master/docs/examples
+# borrowed from https://github.com/triton-inference-server/server/tree/master/docs/examples
 
-TRITON_VERSION=$(scram tool info triton-inference-server | grep "Version : " | cut -d' ' -f3)
+TRITON_REPO="https://github.com/triton-inference-server/server"
+TRITON_VERSION=$(scram tool info triton-inference-server | grep "Version : " | cut -d' ' -f3 | cut -d'-' -f1)
 
 TEST_DIR=`pwd`
 
@@ -11,28 +12,20 @@ cd $TEST_DIR
 mkdir -p $MODEL_DIR
 cd $MODEL_DIR
 
-curl -O -L https://github.com/NVIDIA/triton-inference-server/raw/v${TRITON_VERSION}/docs/examples/model_repository/resnet50_netdef/config.pbtxt
-curl -O -L https://github.com/NVIDIA/triton-inference-server/raw/v${TRITON_VERSION}/docs/examples/model_repository/resnet50_netdef/resnet50_labels.txt
+curl -O -L ${TRITON_REPO}/raw/v${TRITON_VERSION}/docs/examples/model_repository/resnet50_netdef/config.pbtxt
+curl -O -L ${TRITON_REPO}/raw/v${TRITON_VERSION}/docs/examples/model_repository/resnet50_netdef/resnet50_labels.txt
 
 mkdir -p 1
 
 curl -o 1/model.netdef http://download.caffe2.ai.s3.amazonaws.com/models/resnet50/predict_net.pb
 curl -o 1/init_model.netdef http://download.caffe2.ai.s3.amazonaws.com/models/resnet50/init_net.pb
 
+GAT_REPO="https://github.com/lgray/triton-torchgeo-gat-example/raw/cmssw_20.06-v1-py3"
 GAT_DIR=${TEST_DIR}/../data/models/gat_test
 cd $TEST_DIR
 mkdir -p $GAT_DIR
 cd $GAT_DIR
 
-curl -O -L https://github.com/lgray/triton-torchgeo-gat-example/raw/cmssw_20.06-v1-py3/artifacts/models/gat_test/config.pbtxt
+curl -O -L ${GAT_REPO}/artifacts/models/gat_test/config.pbtxt
 mkdir -p 1
-curl -o 1/model.pt -L https://github.com/lgray/triton-torchgeo-gat-example/raw/cmssw_20.06-v1-py3/artifacts/models/gat_test/1/model.pt
-
-TORCH_DIR=${TEST_DIR}/../data/lib/
-cd $TEST_DIR
-mkdir -p $TORCH_DIR
-cd $TORCH_DIR
-
-for lib in libtorchcluster.so libtorchscatter.so libtorchsparse.so libtorchsplineconv.so; do
-  curl -O -L https://github.com/lgray/triton-torchgeo-gat-example/raw/cmssw_20.06-v1-py3/artifacts/lib/$lib
-done
+curl -o 1/model.pt -L ${GAT_REPO}/artifacts/models/gat_test/1/model.pt
