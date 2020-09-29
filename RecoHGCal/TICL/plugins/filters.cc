e46b82f35107f23df17be0a4d6a3e0cd01948a57
Felice <felice.pantaleo@cern.ch>
2020-09-29 18:23:47 +0200

    Created a ClusterFilter by Algo, Size and Layer Range

--- RecoHGCal/TICL/plugins/filters.cc
+++ RecoHGCal/TICL/plugins/filters.cc
@@ -7,9 +7,13 @@
 #include "ClusterFilterByAlgo.h"
 #include "ClusterFilterByAlgoAndSize.h"
 #include "ClusterFilterBySize.h"
+#include "ClusterFilterByAlgoAndSizeAndLayerRange.h"
 
 using namespace ticl;
 
 DEFINE_EDM_PLUGIN(ClusterFilterFactory, ClusterFilterByAlgo, "ClusterFilterByAlgo");
 DEFINE_EDM_PLUGIN(ClusterFilterFactory, ClusterFilterByAlgoAndSize, "ClusterFilterByAlgoAndSize");
 DEFINE_EDM_PLUGIN(ClusterFilterFactory, ClusterFilterBySize, "ClusterFilterBySize");
+DEFINE_EDM_PLUGIN(ClusterFilterFactory,
+                  ClusterFilterByAlgoAndSizeAndLayerRange,
+                  "ClusterFilterByAlgoAndSizeAndLayerRange");
