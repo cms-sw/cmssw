47747108aaf3e6d7a7e6c71dbe124b179608bf31
Jan-Frederik <jschulte@cern.ch>
2020-09-30 01:35:37 +0200

    slightly reduce diff

--- RecoTracker/MeasurementDet/plugins/MeasurementTrackerImpl.cc
+++ RecoTracker/MeasurementDet/plugins/MeasurementTrackerImpl.cc
@@ -283,7 +283,7 @@ void MeasurementTrackerImpl::addGluedDet(const GluedGeomDet* gd) {
 
 void MeasurementTrackerImpl::addStackDet(const StackGeomDet* gd) {
   //since the Stack will be composed by PS or 2S,
-  //  //both cluster parameter estimators are needed? - right now just the thePixelCPE is used.
+  //both cluster parameter estimators are needed? - right now just the thePixelCPE is used.
   theStackDets.push_back(TkStackMeasurementDet(gd, thePxDetConditions.pixelCPE()));
 }
 
