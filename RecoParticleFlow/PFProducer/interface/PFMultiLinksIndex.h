7ee55496792ecc84f2daffb853f3487ef888560d
Joosep Pata <joosep.pata@gmail.com>
2020-09-29 11:31:00 +0300

    refactor PFMultiLinksIndex

--- RecoParticleFlow/PFProducer/interface/PFMultiLinksIndex.h
+++ RecoParticleFlow/PFProducer/interface/PFMultiLinksIndex.h
@@ -9,31 +9,21 @@
 
 namespace reco {
 
-  struct pair_hash {
-    template <class T1, class T2>
-    std::size_t operator()(const std::pair<T1, T2>& p) const {
-      auto h1 = std::hash<T1>{}(p.first);
-      auto h2 = std::hash<T2>{}(p.second);
-      return h1 ^ h2;
-    }
-  };
+  //For each element, store the indices of the linked elements
+  using ElementLinks = std::vector<std::set<size_t>>;
 
   //stores the links between PFElements that are found by KDTrees.
   class PFMultiLinksIndex {
   public:
-    PFMultiLinksIndex(size_t nelements) {
-      _linkdata[std::make_pair(reco::PFBlockElement::ECAL, reco::PFBlockElement::TRACK)].resize(nelements,
-                                                                                                std::set<size_t>());
-      _linkdata[std::make_pair(reco::PFBlockElement::PS2, reco::PFBlockElement::ECAL)].resize(nelements,
-                                                                                              std::set<size_t>());
-      _linkdata[std::make_pair(reco::PFBlockElement::PS1, reco::PFBlockElement::ECAL)].resize(nelements,
-                                                                                              std::set<size_t>());
-      _linkdata[std::make_pair(reco::PFBlockElement::TRACK, reco::PFBlockElement::HCAL)].resize(nelements,
-                                                                                                std::set<size_t>());
-      _linkdata[std::make_pair(reco::PFBlockElement::TRACK, reco::PFBlockElement::HFHAD)].resize(nelements,
-                                                                                                 std::set<size_t>());
-      _linkdata[std::make_pair(reco::PFBlockElement::TRACK, reco::PFBlockElement::HFEM)].resize(nelements,
-                                                                                                std::set<size_t>());
+    PFMultiLinksIndex(size_t nelements) : _linkdata({{
+        ElementLinks(nelements, std::set<size_t>()),
+        ElementLinks(nelements, std::set<size_t>()),
+        ElementLinks(nelements, std::set<size_t>()),
+        ElementLinks(nelements, std::set<size_t>()),
+        ElementLinks(nelements, std::set<size_t>()),
+        ElementLinks(nelements, std::set<size_t>())}})
+    {
+      
     };
 
     void addLink(size_t ielem1,
@@ -42,7 +32,7 @@ namespace reco {
                  reco::PFBlockElement::Type dst_type) {
       LogDebug("PFMultiLinksIndex") << "adding link src=" << src_type << " dst=" << dst_type << " ielem1=" << ielem1
                                     << " ielem2=" << ielem2;
-      _linkdata.at(std::make_pair(src_type, dst_type))[ielem1].insert(ielem2);
+      _linkdata[getLinkIndex(src_type, dst_type)][ielem1].insert(ielem2);
     }
 
     bool isValid(size_t ielem1, reco::PFBlockElement::Type src_type, reco::PFBlockElement::Type dst_type) const {
@@ -50,22 +40,40 @@ namespace reco {
     }
 
     size_t getNumLinks(size_t ielem1, reco::PFBlockElement::Type src_type, reco::PFBlockElement::Type dst_type) const {
-      return _linkdata.at(std::make_pair(src_type, dst_type))[ielem1].size();
+      return _linkdata[getLinkIndex(src_type, dst_type)][ielem1].size();
     }
 
     bool isLinked(size_t ielem1,
                   size_t ielem2,
                   reco::PFBlockElement::Type src_type,
                   reco::PFBlockElement::Type dst_type) const {
-      const auto& ml = _linkdata.at(std::make_pair(src_type, dst_type))[ielem1];
+      const auto& ml = _linkdata[getLinkIndex(src_type, dst_type)][ielem1];
       return ml.find(ielem2) != ml.end();
     }
 
   private:
-    std::unordered_map<std::pair<reco::PFBlockElement::Type, reco::PFBlockElement::Type>,
-                       std::vector<std::set<size_t>>,
-                       pair_hash>
-        _linkdata;
+    //For each link type, store the per-element links (indices of linked elements)
+    std::array<ElementLinks, 6> _linkdata;
+
+    size_t getLinkIndex(reco::PFBlockElement::Type src_type,
+                 reco::PFBlockElement::Type dst_type) const {
+      if (src_type == reco::PFBlockElement::ECAL && dst_type == reco::PFBlockElement::TRACK) {
+        return 0;
+      }
+      else if (src_type == reco::PFBlockElement::PS2 && dst_type == reco::PFBlockElement::ECAL) {
+        return 1;
+      } else if (src_type == reco::PFBlockElement::PS1 && dst_type == reco::PFBlockElement::ECAL) {
+        return 2;
+      } else if (src_type == reco::PFBlockElement::TRACK && dst_type == reco::PFBlockElement::HCAL) {
+        return 3;
+      } else if (src_type == reco::PFBlockElement::TRACK && dst_type == reco::PFBlockElement::HFHAD) {
+        return 4;
+      } else if (src_type == reco::PFBlockElement::TRACK && dst_type == reco::PFBlockElement::HFEM) {
+        return 5;
+      }
+
+      throw cms::Exception("LogicError") << "Unhandled types in PFMultiLinksIndex::getLinkIndex";
+    }
   };
 
 }  // namespace reco
