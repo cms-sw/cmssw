f39f3a981c20781d1c82f1b843c3053f61491179
Felice <felice.pantaleo@cern.ch>
2020-09-30 14:39:54 +0200

    include an Electromagnetic TrackSeeded iteration as first iteration

---
+++ RecoHGCal/TICL/python/TrkEMStep_cff.py
@@ -0,0 +1,47 @@
+import FWCore.ParameterSet.Config as cms
+
+from RecoHGCal.TICL.TICLSeedingRegions_cff import ticlSeedingTrk
+from RecoHGCal.TICL.trackstersProducer_cfi import trackstersProducer as _trackstersProducer
+from RecoHGCal.TICL.filteredLayerClustersProducer_cfi import filteredLayerClustersProducer as _filteredLayerClustersProducer
+from RecoHGCal.TICL.multiClustersFromTrackstersProducer_cfi import multiClustersFromTrackstersProducer as _multiClustersFromTrackstersProducer
+
+# CLUSTER FILTERING/MASKING
+
+filteredLayerClustersTrkEM = _filteredLayerClustersProducer.clone(
+    clusterFilter = "ClusterFilterByAlgoAndSizeAndLayerRange",
+    min_cluster_size = 2, # inclusive
+    min_layerId = 3, # inclusive
+    max_layerId = 30, # inclusive
+    algo_number = 8,
+    iteration_label = "TrkEM"
+)
+
+# CA - PATTERN RECOGNITION
+
+ticlTrackstersTrkEM = _trackstersProducer.clone(
+    filtered_mask = cms.InputTag("filteredLayerClustersTrkEM", "TrkEM"),
+    seeding_regions = "ticlSeedingTrk",
+    filter_on_categories = [0, 1],
+    pid_threshold = 0.5,
+    max_out_in_hops = 4,
+    missing_layers = 1,
+    min_clusters_per_ntuplet = 10,
+    min_cos_theta = 0.978,  # ~12 degrees
+    min_cos_pointing = 0.9, # ~25 degrees
+    max_delta_time = 3.,
+    itername = "TrkEM",
+    algo_verbosity = 0,
+)
+
+
+# MULTICLUSTERS
+
+ticlMultiClustersFromTrackstersTrkEM = _multiClustersFromTrackstersProducer.clone(
+    Tracksters = "ticlTrackstersTrkEM"
+)
+
+ticlTrkEMStepTask = cms.Task(ticlSeedingTrk
+    ,filteredLayerClustersTrkEM
+    ,ticlTrackstersTrkEM
+    ,ticlMultiClustersFromTrackstersTrkEM)
+
