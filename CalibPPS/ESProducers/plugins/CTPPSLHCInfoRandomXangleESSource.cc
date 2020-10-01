21b3b22ad1c812c1764dbccf6ee1699439e99f77
Jan Kaspar <jan.kaspar@cern.ch>
2020-10-01 11:49:03 +0200

    Use FileInPath, clean up.

--- CalibPPS/ESProducers/plugins/CTPPSLHCInfoRandomXangleESSource.cc
+++ CalibPPS/ESProducers/plugins/CTPPSLHCInfoRandomXangleESSource.cc
@@ -39,17 +39,15 @@ private:
   unsigned int m_generateEveryNEvents;
 
   double m_beamEnergy;
-  double m_betaStar;
 
   std::unique_ptr<CLHEP::HepRandomEngine> m_engine;
 
-  template <typename T>
   struct BinData {
     double min, max;
-    T profile;
+    double xangle, betaStar;
   };
 
-  std::vector<BinData<std::pair<double,double>>> xangleBetaStarBins;
+  std::vector<BinData> xangleBetaStarBins;
 };
 
 //----------------------------------------------------------------------------------------------------
@@ -63,36 +61,40 @@ CTPPSLHCInfoRandomXangleESSource::CTPPSLHCInfoRandomXangleESSource(const edm::Pa
       m_beamEnergy(conf.getParameter<double>("beamEnergy")),
 
       m_engine(new CLHEP::HepJamesRandom(conf.getParameter<unsigned int>("seed"))) {
+  // get input beta* vs. xangle histogram
   const auto &xangleBetaStarHistogramFile = conf.getParameter<std::string>("xangleBetaStarHistogramFile");
   const auto &xangleBetaStarHistogramObject = conf.getParameter<std::string>("xangleBetaStarHistogramObject");
 
-  TFile *f_in = TFile::Open(xangleBetaStarHistogramFile.c_str());
+  edm::FileInPath fip(xangleBetaStarHistogramFile);
+  std::unique_ptr<TFile> f_in(TFile::Open(fip.fullPath().c_str()));
   if (!f_in)
     throw cms::Exception("PPS") << "Cannot open input file '" << xangleBetaStarHistogramFile << "'.";
 
   TH2D *h_xangle_beta_star = (TH2D *)f_in->Get(xangleBetaStarHistogramObject.c_str());
   if (!h_xangle_beta_star)
     throw cms::Exception("PPS") << "Cannot load input object '" << xangleBetaStarHistogramObject << "'.";
-  h_xangle_beta_star->SetDirectory(0);
-  delete f_in;
 
+  // parse histogram
   double sum = 0.;
-  for (int bi = 1; bi <= h_xangle_beta_star->GetNcells(); ++bi){
-    double val=h_xangle_beta_star->GetBinContent(bi);
-    sum+=val;
+  for (int x = 1; x <= h_xangle_beta_star->GetNbinsX(); ++x) {
+    for (int y = 1; y <= h_xangle_beta_star->GetNbinsY(); ++y)
+      sum += h_xangle_beta_star->GetBinContent(x, y);
   }
 
-  double cw=0;
-  for (int x = 1; x <= h_xangle_beta_star->GetNbinsX(); ++x) 
+  double cw = 0;
+  for (int x = 1; x <= h_xangle_beta_star->GetNbinsX(); ++x) {
     for (int y = 1; y <= h_xangle_beta_star->GetNbinsY(); ++y) {
-      double sample=h_xangle_beta_star->GetBinContent(h_xangle_beta_star->GetBin(x,y));
-      if(sample>=1){
-        sample/=sum;
-        xangleBetaStarBins.push_back({cw,cw+sample,std::pair<double,double>(h_xangle_beta_star->GetXaxis()->GetBinCenter(x),h_xangle_beta_star->GetYaxis()->GetBinCenter(y))});
-        cw+=sample;
+      const double c = h_xangle_beta_star->GetBinContent(x, y);
+      const double xangle = h_xangle_beta_star->GetXaxis()->GetBinCenter(x);
+      const double betaStar = h_xangle_beta_star->GetYaxis()->GetBinCenter(y);
+
+      if (c > 0.) {
+        const double rc = c / sum;
+        xangleBetaStarBins.push_back({cw, cw + rc, xangle, betaStar});
+        cw += rc;
       }
     }
-  delete h_xangle_beta_star;
+  }
 
   setWhatProduced(this, m_label);
   findingRecord<LHCInfoRcd>();
@@ -132,20 +134,19 @@ void CTPPSLHCInfoRandomXangleESSource::setIntervalFor(const edm::eventsetup::Eve
 edm::ESProducts<std::unique_ptr<LHCInfo>> CTPPSLHCInfoRandomXangleESSource::produce(const LHCInfoRcd &) {
   auto output = std::make_unique<LHCInfo>();
 
-  double xangle=0, beta = 0.;
+  double xangle = 0., betaStar = 0.;
   const double u = CLHEP::RandFlat::shoot(m_engine.get(), 0., 1.);
   for (const auto &d : xangleBetaStarBins) {
     if (d.min <= u && u <= d.max) {
-      xangle = d.profile.first;
-      beta=d.profile.second;
+      xangle = d.xangle;
+      betaStar = d.betaStar;
       break;
     }
   }
 
   output->setEnergy(m_beamEnergy);
   output->setCrossingAngle(xangle);
-  output->setBetaStar(beta);
-
+  output->setBetaStar(betaStar);
 
   return edm::es::products(std::move(output));
 }
