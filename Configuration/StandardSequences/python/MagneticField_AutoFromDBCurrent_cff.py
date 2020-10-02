825e3ace622ec4dfe3e3e25f7524ba978c0d425b
Nicola Amapane <nicola.amapane@cern.ch>
2020-10-02 15:55:06 +0200

    cleanup obsolete, unused standard sequences

--- Configuration/StandardSequences/python/MagneticField_AutoFromDBCurrent_cff.py
+++
@@ -1,6 +0,0 @@
-# This cff is obsolete and will eventually be deprecated.
-# Please replace it with 
-# Configuration.StandardSequences.MagneticField_cff.py
-# in your setup.
-
-from Configuration.StandardSequences.MagneticField_cff import *
