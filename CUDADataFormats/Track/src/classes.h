f50f7a1d4ec83000a7ada2fa352ac80aec08b803
Andrea Bocci <andrea.bocci@cern.ch>
2020-10-05 17:35:45 +0200

    Further clean up after merging CMSSW_11_2_0_pre7 (#556)
    Minor bug fixes:
      - fix a typo in EventFilter/EcalRawToDigi/plugins/BuildFile.xml .
    
    Clean up:
      - remove obsolete ArrayShadow class;
      - remove obsolete profiling functions.

--- CUDADataFormats/Track/src/classes.h
+++ CUDADataFormats/Track/src/classes.h
@@ -4,7 +4,6 @@
 #include "CUDADataFormats/Common/interface/Product.h"
 #include "CUDADataFormats/Common/interface/HostProduct.h"
 #include "CUDADataFormats/Track/interface/PixelTrackHeterogeneous.h"
-#include "CUDADataFormats/Common/interface/ArrayShadow.h"
 #include "DataFormats/Common/interface/Wrapper.h"
 
 #endif  // CUDADataFormats_Track_src_classes_h
