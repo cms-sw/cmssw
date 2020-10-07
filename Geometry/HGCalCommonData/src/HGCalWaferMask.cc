0f965ddd5cb72886641d1b3633b3ea82c1b008de
Sunanda <sunanda.banerjee@cern.ch>
2020-10-07 15:49:24 +0200

    Take suggestions

--- Geometry/HGCalCommonData/src/HGCalWaferMask.cc
+++ Geometry/HGCalCommonData/src/HGCalWaferMask.cc
@@ -334,6 +334,7 @@ bool HGCalWaferMask::goodCell(int u, int v, int n, int type, int rotn) {
 }
 
 int HGCalWaferMask::getRotation(int zside, int type, int rotn) {
+  if (rotn >= HGCalTypes::WaferCornerMax) rotn = HGCalTypes::WaferCorner0;
   int newrotn(rotn);
   if ((zside < 0) && (type != HGCalTypes::WaferFull)) {
     if (type == HGCalTypes::WaferFive) {  //WaferFive
