98e7321b4052633dafea8531162122f8ad60800e
hajohajo@helsinki.fi <hajohajo@helsinki.fi>
2020-10-09 15:09:00 +0200

    Improved naming scheme for TrackTfClassifier weights

--- RecoTracker/FinalTrackSelectors/python/trackSelectionTf_cfi.py
+++ RecoTracker/FinalTrackSelectors/python/trackSelectionTf_cfi.py
@@ -1,5 +1,5 @@
 from DataFormats.TrackTfGraph.tfGraphDefProducer_cfi import tfGraphDefProducer as _tfGraphDefProducer
 trackSelectionTf = _tfGraphDefProducer.clone(
     ComponentName = "trackSelectionTf",
-    FileName = "RecoTracker/FinalTrackSelectors/data/frozen_graph.pb"
+    FileName = "RecoTracker/FinalTrackSelectors/data/QCDFlatPU_QCDHighPt_ZEE_DisplacedSUSY_2020.pb"
 )
