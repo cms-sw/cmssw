47d5b27b1e04d76a0c20bbe28cdd5df8a99dff2b
Dario <dario.soldi@cern.ch>
2020-10-09 10:38:09 +0200

    Modify from Era_Phase2C10_Ecal_Devel to Era_Phase2C11_Ecal_Devel

--- Configuration/Eras/python/Era_Phase2C10_Ecal_Devel_cff.py
+++
@@ -1,8 +0,0 @@
-import FWCore.ParameterSet.Config as cms
-
-from Configuration.Eras.Era_Phase2C10_cff import Phase2C10
-from Configuration.Eras.Modifier_phase2_ecal_devel_cff import phase2_ecal_devel
-from Configuration.Eras.Modifier_phase2_hfnose_cff import phase2_hfnose
-
-Phase2C10_Ecal_Devel = cms.ModifierChain(Phase2C10,phase2_hfnose,phase2_ecal_devel)
-
