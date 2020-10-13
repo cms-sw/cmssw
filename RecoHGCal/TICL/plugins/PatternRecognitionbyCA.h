86ecf556e823ab6314ff603fb694a33e96d02589
Felice <felice.pantaleo@cern.ch>
2020-10-13 21:21:08 +0200

    Add a new max_longitudinal_sigmaPCA cut for EM iterations, with an initial value of 10

--- RecoHGCal/TICL/plugins/PatternRecognitionbyCA.h
+++ RecoHGCal/TICL/plugins/PatternRecognitionbyCA.h
@@ -46,6 +46,7 @@ namespace ticl {
     const std::vector<int> filter_on_categories_;
     const double pid_threshold_;
     const double energy_em_over_total_threshold_;
+    const double max_longitudinal_sigmaPCA_;
     const int min_clusters_per_ntuplet_;
     const float max_delta_time_;
     const std::string eidInputName_;
