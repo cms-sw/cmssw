0ca3101182fe68fc344039dc5297f94afee9057b
Jeongeun Lee <jeongeun.lee@cern.ch>
2020-10-14 11:50:18 +0200

    drop type specs in RecoPixelVertexing

--- RecoPixelVertexing/PixelLowPtUtilities/python/MinBiasCkfTrajectoryFilter_cfi.py
+++ RecoPixelVertexing/PixelLowPtUtilities/python/MinBiasCkfTrajectoryFilter_cfi.py
@@ -2,11 +2,10 @@ import FWCore.ParameterSet.Config as cms
 
 # Trajectory filter for min bias
 import TrackingTools.TrajectoryFiltering.TrajectoryFilter_cff
-ckfBaseTrajectoryFilterForMinBias = TrackingTools.TrajectoryFiltering.TrajectoryFilter_cff.CkfBaseTrajectoryFilter_block.clone()
-
-ckfBaseTrajectoryFilterForMinBias.minimumNumberOfHits = 3
-ckfBaseTrajectoryFilterForMinBias.minPt               = 0.075
-
+ckfBaseTrajectoryFilterForMinBias = TrackingTools.TrajectoryFiltering.TrajectoryFilter_cff.CkfBaseTrajectoryFilter_block.clone(
+    minimumNumberOfHits = 3,
+    minPt               = 0.075
+)
 from RecoPixelVertexing.PixelLowPtUtilities.ClusterShapeHitFilterESProducer_cfi import *
 from RecoPixelVertexing.PixelLowPtUtilities.ClusterShapeTrajectoryFilter_cfi import *
 
@@ -14,5 +13,5 @@ from RecoPixelVertexing.PixelLowPtUtilities.ClusterShapeTrajectoryFilter_cfi imp
 MinBiasCkfTrajectoryFilter = TrackingTools.TrajectoryFiltering.TrajectoryFilter_cff.CompositeTrajectoryFilter_block.clone(
     filters   = [cms.PSet(refToPSet_ = cms.string('ckfBaseTrajectoryFilterForMinBias')),
                  cms.PSet(refToPSet_ = cms.string('ClusterShapeTrajectoryFilter'))]
-    )
+)
 
