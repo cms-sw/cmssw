01b45105e07e8c04270943d3e10ef9beae159843
Kevin Pedro <kpedro88@gmail.com>
2020-10-15 14:44:13 -0500

    modifier for offline test

--- RecoLocalCalo/Configuration/python/hcalLocalReco_cff.py
+++ RecoLocalCalo/Configuration/python/hcalLocalReco_cff.py
@@ -39,6 +39,30 @@ run2_HF_2017.toReplaceWith( hfreco, _phase1_hfreco )
 from Configuration.Eras.Modifier_run2_HCAL_2017_cff import run2_HCAL_2017
 run2_HCAL_2017.toReplaceWith( hbheprereco, _phase1_hbheprereco )
 
+#--- ML-based reco using SONIC+Triton
+from Configuration.ProcessModifiers.enableSonicTriton_cff import enableSonicTriton
+hbhechannelinfo = _phase1_hbheprereco.clone(
+    makeRecHits = cms.bool(False),
+    saveInfos = cms.bool(True),
+)
+_sonic_hbheprereco = cms.EDProducer("FacileHcalReconstructor",
+    Client = cms.PSet(
+        batchSize = cms.untracked.uint32(16000),
+        address = cms.untracked.string("0.0.0.0"),
+        port = cms.untracked.uint32(8001),
+        timeout = cms.untracked.uint32(300),
+        modelName = cms.string("facile_all_v2"),
+        mode = cms.string("Async"),
+        modelVersion = cms.string(""),
+        verbose = cms.untracked.bool(False),
+        allowedTries = cms.untracked.uint32(5),
+        outputs = cms.untracked.vstring("output/BiasAdd"),
+    ),
+    ChannelInfoName = cms.InputTag("hbhechannelinfo")
+)
+(enableSonicTriton & run2_HCAL_2017).toReplaceWith(hbheprereco, _sonic_hbheprereco)
+(enableSonicTriton & run2_HCAL_2017).toModify(hcalLocalRecoTask, lambda x: x.add(hbhechannelinfo))
+
 _plan1_hcalLocalRecoTask = _phase1_hcalLocalRecoTask.copy()
 _plan1_hcalLocalRecoTask.add(hbheplan1)
 from Configuration.Eras.Modifier_run2_HEPlan1_2017_cff import run2_HEPlan1_2017
