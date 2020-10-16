ca51ce26a6379ea06fa082e44a835221f67b502b
Loukas Gouskos <Loukas.Gouskos@cern.ch>
2020-10-16 14:49:27 +0200

    add sv ntracks

--- PhysicsTools/NanoAOD/python/vertices_cff.py
+++ PhysicsTools/NanoAOD/python/vertices_cff.py
@@ -31,6 +31,7 @@ svCandidateTable =  cms.EDProducer("SimpleCandidateFlatTableProducer",
         z   = Var("position().z()", float, doc = "secondary vertex Z position, in cm",precision=14),
         ndof   = Var("vertexNdof()", float, doc = "number of degrees of freedom",precision=8),
         chi2   = Var("vertexNormalizedChi2()", float, doc = "reduced chi2, i.e. chi/ndof",precision=8),
+        ntracks = Var("numberOfDaughters()", "uint8", doc = "number of tracks"),
     ),
 )
 svCandidateTable.variables.pt.precision=10
