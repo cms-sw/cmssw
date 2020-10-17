e7ea7d6b6b8ae73a4bd6a9caad592690e5455ee8
Sunanda <sunanda.banerjee@cern.ch>
2020-10-17 20:10:22 +0200

    Code check

--- SimG4CMS/Muon/test/MuonSimHitDump.cc
+++ SimG4CMS/Muon/test/MuonSimHitDump.cc
@@ -32,28 +32,26 @@ protected:
 private:
   const std::string g4Label_;
   const std::vector<std::string> hitLab_;
-  const std::vector<edm::EDGetTokenT<edm::PSimHitContainer> > toksMuon_;
+  const std::vector<edm::EDGetTokenT<edm::PSimHitContainer>> toksMuon_;
   const std::vector<int> types_;
   const int maxEvent_;
   int kount_;
 };
 
-MuonSimHitDump::MuonSimHitDump(const edm::ParameterSet& ps) :
-  g4Label_(ps.getParameter<std::string>("ModuleLabel")),
-  hitLab_(ps.getParameter<std::vector<std::string>>("HitCollections")),
-  toksMuon_{
-  edm::vector_transform(hitLab_,
-			[this](const std::string& name) {
-			  return consumes<edm::PSimHitContainer>(
-								 edm::InputTag{g4Label_, name});
-			    })},
-  types_(ps.getParameter<std::vector<int>>("CollectionTypes")),
-  maxEvent_(ps.getParameter<int>("MaxEvent")),
-  kount_(0) {
-
-    edm::LogVerbatim("HitStudy") << "Module Label: " << g4Label_ << "   with " << hitLab_.size() << " collections and maxEvent = " << maxEvent_;
-    for (unsigned int k = 0; k < hitLab_.size(); ++k)
-      edm::LogVerbatim("HitStudy") << "[" << k << "] Type " << types_[k] << " Label " << hitLab_[k];
+MuonSimHitDump::MuonSimHitDump(const edm::ParameterSet& ps)
+    : g4Label_(ps.getParameter<std::string>("ModuleLabel")),
+      hitLab_(ps.getParameter<std::vector<std::string>>("HitCollections")),
+      toksMuon_{edm::vector_transform(hitLab_,
+                                      [this](const std::string& name) {
+                                        return consumes<edm::PSimHitContainer>(edm::InputTag{g4Label_, name});
+                                      })},
+      types_(ps.getParameter<std::vector<int>>("CollectionTypes")),
+      maxEvent_(ps.getParameter<int>("MaxEvent")),
+      kount_(0) {
+  edm::LogVerbatim("HitStudy") << "Module Label: " << g4Label_ << "   with " << hitLab_.size()
+                               << " collections and maxEvent = " << maxEvent_;
+  for (unsigned int k = 0; k < hitLab_.size(); ++k)
+    edm::LogVerbatim("HitStudy") << "[" << k << "] Type " << types_[k] << " Label " << hitLab_[k];
 }
 
 void MuonSimHitDump::fillDescriptions(edm::ConfigurationDescriptions& descriptions) {
@@ -61,8 +59,8 @@ void MuonSimHitDump::fillDescriptions(edm::ConfigurationDescriptions& descriptio
   std::vector<std::string> coll = {"MuonDTHits", "MuonCSCHits", "MuonRPCHits", "MuonGEMHits"};
   std::vector<int> type = {0, 1, 2, 3};
   desc.add<std::string>("ModuleLabel", "g4SimHits");
-  desc.add<std::vector<std::string> >("HitCollections", coll);
-  desc.add<std::vector<int> >("CollectionTypes", type);
+  desc.add<std::vector<std::string>>("HitCollections", coll);
+  desc.add<std::vector<int>>("CollectionTypes", type);
   desc.add<int>("MaxEvent", 10);
   descriptions.add("muonSimHitDump", desc);
 }
@@ -76,27 +74,33 @@ void MuonSimHitDump::analyze(const edm::Event& e, const edm::EventSetup&) {
       edm::Handle<edm::PSimHitContainer> hitsMuon;
       e.getByToken(toksMuon_[k], hitsMuon);
       if (hitsMuon.isValid())
-	edm::LogVerbatim("HitStudy") << "MuonSimHitDump: Input " << hitsMuon->size() << " hits of type " << types_[k] << " (" << dets[k] << ")";
+        edm::LogVerbatim("HitStudy") << "MuonSimHitDump: Input " << hitsMuon->size() << " hits of type " << types_[k]
+                                     << " (" << dets[k] << ")";
       unsigned int i(0);
       for (auto const& hit : *hitsMuon) {
-	auto entry = hit.entryPoint();
-	auto exit = hit.exitPoint();
-	auto p = hit.pabs();
-	auto tof = hit.tof();
-	auto edep = hit.energyLoss();
-	unsigned int track = hit.trackId();
-	unsigned int id = hit.detUnitId();
-	if (types_[k] == 0)
-	  edm::LogVerbatim("HitStudy") << "[" << i << "] " << DTWireId(id) << " Trk " << track << " p " << p << " dE " << edep << " T " << tof << " Enetry " << entry << " Exit " << exit;
-	else if (types_[k] == 1)
-	  edm::LogVerbatim("HitStudy") << "[" << i << "] " << CSCDetId(id) << " Trk " << track << " p " << p << " dE " << edep << " T " << tof << " Enetry " << entry << " Exit " << exit;
-	else if (types_[k] == 2)
-	  edm::LogVerbatim("HitStudy") << "[" << i << "] " << RPCDetId(id) << " Trk " << track << " p " << p << " dE " << edep << " T " << tof << " Enetry " << entry << " Exit " << exit;
-	else if (types_[k] == 3)
-	  edm::LogVerbatim("HitStudy") << "[" << i << "] " << GEMDetId(id) << " Trk " << track << " p " << p << " dE " << edep << " T " << tof << " Enetry " << entry << " Exit " << exit;
-	else 
-	  edm::LogVerbatim("HitStudy") << "[" << i << "] " << ME0DetId(id) << " Trk " << track << " p " << p << " dE " << edep << " T " << tof << " Enetry " << entry << " Exit " << exit;
-	++i;
+        auto entry = hit.entryPoint();
+        auto exit = hit.exitPoint();
+        auto p = hit.pabs();
+        auto tof = hit.tof();
+        auto edep = hit.energyLoss();
+        unsigned int track = hit.trackId();
+        unsigned int id = hit.detUnitId();
+        if (types_[k] == 0)
+          edm::LogVerbatim("HitStudy") << "[" << i << "] " << DTWireId(id) << " Trk " << track << " p " << p << " dE "
+                                       << edep << " T " << tof << " Enetry " << entry << " Exit " << exit;
+        else if (types_[k] == 1)
+          edm::LogVerbatim("HitStudy") << "[" << i << "] " << CSCDetId(id) << " Trk " << track << " p " << p << " dE "
+                                       << edep << " T " << tof << " Enetry " << entry << " Exit " << exit;
+        else if (types_[k] == 2)
+          edm::LogVerbatim("HitStudy") << "[" << i << "] " << RPCDetId(id) << " Trk " << track << " p " << p << " dE "
+                                       << edep << " T " << tof << " Enetry " << entry << " Exit " << exit;
+        else if (types_[k] == 3)
+          edm::LogVerbatim("HitStudy") << "[" << i << "] " << GEMDetId(id) << " Trk " << track << " p " << p << " dE "
+                                       << edep << " T " << tof << " Enetry " << entry << " Exit " << exit;
+        else
+          edm::LogVerbatim("HitStudy") << "[" << i << "] " << ME0DetId(id) << " Trk " << track << " p " << p << " dE "
+                                       << edep << " T " << tof << " Enetry " << entry << " Exit " << exit;
+        ++i;
       }
     }
   }
