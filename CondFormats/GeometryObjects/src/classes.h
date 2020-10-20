b2c46fba67080dca423bb114e4af58080a2cb5c8
Fabio Cossutti <fabio.cossutti@ts.infn.it>
2020-10-20 18:42:39 +0200

    Cleaning of PGeometricTimingDetExtra

--- CondFormats/GeometryObjects/src/classes.h
+++ CondFormats/GeometryObjects/src/classes.h
@@ -1,6 +1,5 @@
 #include "CondFormats/GeometryObjects/interface/PGeometricDet.h"
 #include "CondFormats/GeometryObjects/interface/PGeometricTimingDet.h"
-#include "CondFormats/GeometryObjects/interface/PGeometricTimingDetExtra.h"
 #include "CondFormats/GeometryObjects/interface/PCaloGeometry.h"
 #include "CondFormats/GeometryObjects/interface/RecoIdealGeometry.h"
 #include "CondFormats/GeometryObjects/interface/CSCRecoDigiParameters.h"
--- Geometry/Records/interface/IdealGeometryRecord.h
+++ Geometry/Records/interface/IdealGeometryRecord.h
@@ -20,11 +20,10 @@
 
 #include "FWCore/Framework/interface/DependentRecordImplementation.h"
 #include "Geometry/Records/interface/GeometryFileRcd.h"
-#include "Geometry/Records/interface/PGeometricTimingDetExtraRcd.h"
 #include "FWCore/Utilities/interface/mplVector.h"
 
 class IdealGeometryRecord : public edm::eventsetup::DependentRecordImplementation<
                                 IdealGeometryRecord,
-                                edm::mpl::Vector<GeometryFileRcd, PGeometricTimingDetExtraRcd> > {};
+                                edm::mpl::Vector<GeometryFileRcd> > {};
 
 #endif /* RECORDS_IDEALGEOMETRYRECORD_H */
