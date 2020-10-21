d15c243505089cd22af11c6c16402834cd648bca
Fabio Cossutti <fabio.cossutti@ts.infn.it>
2020-10-21 19:24:55 +0200

    Move scenarios D72 and D73 to Phase2C11_etlV4 era

--- Configuration/Geometry/python/dict2026Geometry.py
+++ Configuration/Geometry/python/dict2026Geometry.py
@@ -1186,7 +1186,7 @@ timingDict = {
             'from Geometry.MTDGeometryBuilder.idealForDigiMTDGeometry_cff import *',
             'mtdGeometry.applyAlignment = cms.bool(False)'
         ],
-        "era" : "phase2_timing, phase2_timing_layer",
+        "era" : "phase2_timing, phase2_timing_layer, phase2_etlV4",
     },
     "I13" : {
         1 : [
@@ -1214,7 +1214,7 @@ timingDict = {
            'from Geometry.MTDGeometryBuilder.idealForDigiMTDGeometry_cff import *',
            'mtdGeometry.applyAlignment = cms.bool(False)'
            ],
-       "era" : "phase2_timing, phase2_timing_layer",
+       "era" : "phase2_timing, phase2_timing_layer, phase2_etlV4",
     },
 }
 
