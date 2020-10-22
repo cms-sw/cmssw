cf66a408a03e4e66d4d158a32669a5139c633448
Jonas Rembser <jonas.rembser@cern.ch>
2020-10-23 00:40:12 +0200

    move the CSCCondDB plugins in the plugin directory

--- OnlineDB/CSCCondDB/plugins/plugins.cc
+++ OnlineDB/CSCCondDB/plugins/plugins.cc
@@ -1,15 +1,10 @@
-#include "FWCore/Framework/interface/MakerMacros.h"
 #include "FWCore/Framework/interface/SourceFactory.h"
 #include "OnlineDB/CSCCondDB/interface/CSCChamberMapValues.h"
 #include "OnlineDB/CSCCondDB/interface/CSCChamberIndexValues.h"
 #include "OnlineDB/CSCCondDB/interface/CSCCrateMapValues.h"
 #include "OnlineDB/CSCCondDB/interface/CSCChamberTimeCorrectionsValues.h"
 #include "OnlineDB/CSCCondDB/interface/CSCDDUMapValues.h"
-#include "OnlineDB/CSCCondDB/interface/CSCMap1Read.h"
-#include "OnlineDB/CSCCondDB/interface/CSCChamberTimeCorrectionsReadTest.h"
 
-DEFINE_FWK_MODULE(CSCMap1Read);
-DEFINE_FWK_MODULE(CSCChamberTimeCorrectionsReadTest);
 DEFINE_FWK_EVENTSETUP_SOURCE(CSCChamberMapValues);
 DEFINE_FWK_EVENTSETUP_SOURCE(CSCChamberIndexValues);
 DEFINE_FWK_EVENTSETUP_SOURCE(CSCCrateMapValues);
