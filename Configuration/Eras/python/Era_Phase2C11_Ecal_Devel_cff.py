71913b7a87f5e0f29e1328874062c7cee199f213
Kevin Pedro <kpedro88@gmail.com>
2020-10-22 12:43:11 -0500

    remove unneeded Era

--- Configuration/Eras/python/Era_Phase2C11_Ecal_Devel_cff.py
+++
@@ -1,7 +0,0 @@
-import FWCore.ParameterSet.Config as cms
-
-from Configuration.Eras.Era_Phase2C11_cff import Phase2C11
-from Configuration.Eras.Modifier_phase2_ecal_devel_cff import phase2_ecal_devel
-
-Phase2C11_Ecal_Devel = cms.ModifierChain(Phase2C11,phase2_ecal_devel)
-
