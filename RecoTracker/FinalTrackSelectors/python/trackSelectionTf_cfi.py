f9d694705dfa167c9ebe89b30774eea0a42a3916
[Minxi <[yhzz598@gmail.com]>
2020-10-23 13:19:39 -0400

    test

--- RecoTracker/FinalTrackSelectors/python/trackSelectionTf_cfi.py
+++ RecoTracker/FinalTrackSelectors/python/trackSelectionTf_cfi.py
@@ -1,4 +1,4 @@
-from DataFormats.TrackTfGraph.tfGraphDefProducer_cfi import tfGraphDefProducer as _tfGraphDefProducer
+from RecoTracker.FinalTrackSelectors.tfGraphDefProducer_cfi import tfGraphDefProducer as _tfGraphDefProducer
 trackSelectionTf = _tfGraphDefProducer.clone(
     ComponentName = "trackSelectionTf",
     FileName = "RecoTracker/FinalTrackSelectors/data/QCDFlatPU_QCDHighPt_ZEE_DisplacedSUSY_2020.pb"
