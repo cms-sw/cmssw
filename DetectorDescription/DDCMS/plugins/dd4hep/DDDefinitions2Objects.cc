9882910c1824ef1872cec26fa7eee12b2cc373d3
Ianna Osborne <ianna.osborne@cern.ch>
2020-10-28 16:48:47 +0100

    after using TGeo reflection factory titles of reflected volumes are not valid

--- DetectorDescription/DDCMS/plugins/dd4hep/DDDefinitions2Objects.cc
+++ DetectorDescription/DDCMS/plugins/dd4hep/DDDefinitions2Objects.cc
@@ -2058,7 +2058,7 @@ static long load_dddefinition(Detector& det, xml_h element) {
 
     string fname = xml::DocumentHandler::system_path(element);
     bool open_geometry = dddef.hasChild(DD_CMU(open_geometry)) ? dddef.child(DD_CMU(open_geometry)) : true;
-    bool close_geometry = dddef.hasChild(DD_CMU(close_geometry)) ? dddef.hasChild(DD_CMU(close_geometry)) : true;
+    bool close_geometry = dddef.hasChild(DD_CMU(close_geometry)) ? dddef.hasChild(DD_CMU(close_geometry)) : false;
 
     xml_coll_t(dddef, _U(debug)).for_each(Converter<debug>(det, &context));
 
