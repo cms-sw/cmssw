ed7afcb2e402a5ad2de8472ae11fee244a243522
Justin Williams <justinwilliams@ku.edu>
2020-10-28 10:51:12 -0500

    Added proton filter and removed excess proton variables

--- PhysicsTools/NanoAOD/python/protons_cff.py
+++ PhysicsTools/NanoAOD/python/protons_cff.py
@@ -6,14 +6,16 @@ from Configuration.Eras.Modifier_run2_nanoAOD_94XMiniAODv2_cff import run2_nanoA
 from Configuration.Eras.Modifier_run2_nanoAOD_94X2016_cff import run2_nanoAOD_94X2016
 from Configuration.Eras.Modifier_run2_nanoAOD_102Xv1_cff import run2_nanoAOD_102Xv1
 
+filteredProtons = cms.EDProducer("PPSProtonFilter")
+
 protonTable = cms.EDProducer("ProtonProducer",
-                             tagRecoProtonsSingle = cms.InputTag("ctppsProtons", "singleRP"),
-                             tagRecoProtonsMulti  = cms.InputTag("ctppsProtons", "multiRP"),
+                             tagRecoProtonsSingle = cms.InputTag("filteredProtons", "singleRP"),
+                             tagRecoProtonsMulti  = cms.InputTag("filteredProtons", "multiRP"),
                              tagTrackLite         = cms.InputTag("ctppsLocalTrackLiteProducer")
 )
 
 singleRPTable = cms.EDProducer("SimpleProtonTrackFlatTableProducer",
-    src = cms.InputTag("ctppsProtons","singleRP"),
+    src = cms.InputTag("filteredProtons","singleRP"),
     cut = cms.string(""),
     name = cms.string("Proton_singleRP"),
     doc  = cms.string("bon"),
@@ -22,7 +24,7 @@ singleRPTable = cms.EDProducer("SimpleProtonTrackFlatTableProducer",
     skipNonExistingSrc = cms.bool(True),
     variables = cms.PSet(
         xi = Var("xi",float,doc="xi or dp/p",precision=12),
-        thetaY = Var("thetaY",float,doc="th y",precision=13),
+        thetaY = Var("thetaY",float,doc="th y",precision=10),
     ),
     externalVariables = cms.PSet(
         decRPId = ExtVar("protonTable:protonRPId",int,doc="Detector ID",precision=8), 
@@ -30,7 +32,7 @@ singleRPTable = cms.EDProducer("SimpleProtonTrackFlatTableProducer",
 )
 
 multiRPTable = cms.EDProducer("SimpleProtonTrackFlatTableProducer",
-    src = cms.InputTag("ctppsProtons","multiRP"),
+    src = cms.InputTag("filteredProtons","multiRP"),
     cut = cms.string(""),
     name = cms.string("Proton_multiRP"),
     doc  = cms.string("bon"),
@@ -42,7 +44,7 @@ multiRPTable = cms.EDProducer("SimpleProtonTrackFlatTableProducer",
         thetaX = Var("thetaX",float,doc="theta x",precision=13),
         thetaY = Var("thetaY",float,doc="theta y",precision=13),
         t = Var("t",float,doc="Mandelstam variable t",precision=13),
-        validFit = Var("validFit && (chi2 < 0.1)",bool,doc="valid Fit && chi2 < 0.1"),
+        validFit = Var("validFit && (chi2 < 0.1)",bool,doc="valid Fit"),
         time = Var("time()",float,doc="time",precision=16),
         timeUnc = Var("timeError",float,doc="time uncertainty",precision=13),
     ),
@@ -52,10 +54,11 @@ multiRPTable = cms.EDProducer("SimpleProtonTrackFlatTableProducer",
 )
 
 protonTables = cms.Sequence(    
-    protonTable
+    filteredProtons
+    +protonTable
     +singleRPTable
     +multiRPTable
 )
 
-for modifier in run2_miniAOD_80XLegacy, run2_nanoAOD_94XMiniAODv1, run2_nanoAOD_94XMiniAODv2, run2_nanoAOD_94X2016, run2_nanoAOD_102Xv1:
+for modifier in run2_miniAOD_80XLegacy, run2_nanoAOD_94XMiniAODv1, run2_nanoAOD_94XMiniAODv2, run2_nanoAOD_94X2016:
     modifier.toReplaceWith(protonTables, cms.Sequence())
