a381138b8eb0defd43a2e543c6756997d3a3bac2
Jan Kaspar <jan.kaspar@cern.ch>
2020-10-28 18:41:31 +0100

    Removed obsolete code.

--- CalibPPS/AlignmentRelative/src/classes.h
+++ CalibPPS/AlignmentRelative/src/classes.h
@@ -8,36 +8,3 @@
 #include <TVectorD.h>
 #include <TGraph.h>
 #include <TH1D.h>
-
-namespace {
-  namespace {
-    LocalTrackFit ltf;
-
-    AlignmentTask at;
-
-    AlignmentConstraint ac;
-    std::map<unsigned int, TVectorD> muitvd;
-
-    std::vector<AlignmentConstraint> vac;
-
-    DetGeometry dg;
-    AlignmentGeometry ag;
-
-    std::map<unsigned int, DetGeometry> muidg;
-    std::set<unsigned int> sui;
-
-    SingularMode sm;
-    std::vector<SingularMode> vsm;
-
-    JanAlignmentAlgorithm jaa;
-    JanAlignmentAlgorithm::ScatterPlot jaasp;
-    JanAlignmentAlgorithm::DetStat jaads;
-    std::map<unsigned int, JanAlignmentAlgorithm::DetStat> muids;
-    std::vector<TH1D*> vth;
-    std::vector<TGraph*> vtg;
-    std::map<std::set<unsigned int>, JanAlignmentAlgorithm::ScatterPlot> msuisp;
-
-    AlignmentResult ar;
-    std::map<unsigned int, AlignmentResult> muiar;
-  }  // namespace
-}  // namespace
