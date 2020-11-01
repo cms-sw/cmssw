cbf3e5b389251c99b3f0389c1705115179055051
Dinyar Rabady <dinyar@cern.ch>
2020-11-01 22:01:15 +0100

    clang format

--- DQM/L1TMonitor/interface/L1TStage2MuonComp.h
+++ DQM/L1TMonitor/interface/L1TStage2MuonComp.h
@@ -60,7 +60,8 @@ private:
     RPTUNCONSTR,
     RDXY
   };
-  int numErrBins_ { RIDX }; // In Run-2 we didn't have the last two bins. This is incremented in source file if we configure for Run-3.
+  int numErrBins_{
+      RIDX};  // In Run-2 we didn't have the last two bins. This is incremented in source file if we configure for Run-3.
   bool incBin[RDXY + 1];
 
   edm::EDGetTokenT<l1t::MuonBxCollection> muonToken1;
