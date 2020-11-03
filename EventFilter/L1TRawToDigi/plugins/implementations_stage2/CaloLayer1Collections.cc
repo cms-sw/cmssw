7043036d60755d819f29d3cd9ccd759c12e9f0f8
Alexander <asavin@mail.cern.ch>
2020-11-03 15:02:29 +0100

    L1TCaloLayer1 unpacker is modified to deal wirh 5BX readout for ECAL TPs + code check modificaations

--- EventFilter/L1TRawToDigi/plugins/implementations_stage2/CaloLayer1Collections.cc
+++ EventFilter/L1TRawToDigi/plugins/implementations_stage2/CaloLayer1Collections.cc
@@ -9,7 +9,7 @@ namespace l1t {
       event_.put(std::move(hcalDigis_));
       event_.put(std::move(caloRegions_));
 
-      for(int i=0; i<5;++i){
+      for (int i = 0; i < 5; ++i) {
         event_.put(std::move(ecalDigisBx_[i]), "EcalDigisBx" + std::to_string(i + 1));
       }
     }
