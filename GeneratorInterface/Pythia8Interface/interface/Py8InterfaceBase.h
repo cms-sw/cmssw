46256402294b596a83d20e17ea2f8b1489e59c54
Kevin Pedro <kpedro88@gmail.com>
2020-11-03 16:11:31 -0600

    Read SLHA from TTree

--- GeneratorInterface/Pythia8Interface/interface/Py8InterfaceBase.h
+++ GeneratorInterface/Pythia8Interface/interface/Py8InterfaceBase.h
@@ -32,6 +32,7 @@ namespace gen {
          virtual bool generatePartonsAndHadronize() = 0;
          bool decay() { return true; } // NOT used - let's call it "design imperfection"
          bool readSettings( int ); // common func
+         void makeTmpSLHA(const std::string&); //helper for above
          virtual bool initializeForInternalPartons() = 0;
          bool declareStableParticles( const std::vector<int>& ); // common func
          bool declareSpecialSettings( const std::vector<std::string>& ); // common func
