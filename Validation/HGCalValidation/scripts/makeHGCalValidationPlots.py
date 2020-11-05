ce6bd57bf244c5d59adb8fefc6d33d839e3c0e50
Erica Brondolin <erica.brondolin@cern.ch>
2020-11-05 16:12:04 +0100

    Add part of the Hits

--- Validation/HGCalValidation/scripts/makeHGCalValidationPlots.py
+++ Validation/HGCalValidation/scripts/makeHGCalValidationPlots.py
@@ -3,6 +3,7 @@
 from __future__ import print_function
 import os
 import argparse
+from time import time
 
 from Validation.RecoTrack.plotting.validation import SimpleValidation, SimpleSample
 import Validation.HGCalValidation.hgcalPlots as hgcalPlots
@@ -55,49 +56,38 @@ def main(opts):
         hgchitcalib = [hgcalPlots.hgcalHitCalibPlotter]
         val.doPlots(hgchitcalib, plotterDrawArgs=drawArgs)
     else :
+        #hits
+    	hgchit = [hgcalPlots.hgcalHitPlotter]
+        hgcalPlots.append_hgcalHitsPlots('HGCalSimHitsV', "Simulated Hits")
+        hgcalPlots.append_hgcalHitsPlots('HGCalRecHitsV', "Reconstruced Hits")
+        hgcalPlots.append_hgcalDigisPlots('HGCalDigisV', "Digis")
+    	val.doPlots(hgchit, plotterDrawArgs=drawArgs)   
+
+        #calib
+        hgchitcalib = [hgcalPlots.hgcalHitCalibPlotter]
+        val.doPlots(hgchitcalib, plotterDrawArgs=drawArgs)
+
         #multiclusters
         hgcmulticlus = [hgcalPlots.hgcalMultiClustersPlotter]
         for i_iter in trackstersIters :
             tracksterCollection = i_iter.replace("ticlMultiClustersFromTracksters","ticlTracksters")
-            print(i_iter)
             hgcalPlots.append_hgcalMultiClustersPlots(i_iter, tracksterCollection)
         val.doPlots(hgcmulticlus, plotterDrawArgs=drawArgs)
 
 	#hgclayclus = [hgcalPlots.hgcalLayerClustersPlotter]
 	#val.doPlots(hgclayclus, plotterDrawArgs=drawArgs)
 
+
 #        #In case of all you have to keep a specific order in one to one 
 #        #correspondance between subdirprefix and collections and validation names
 #	#layer clusters 
 #	hgclayclus = [hgcalPlots.hgcalLayerClustersPlotter]
 #	val.doPlots(hgclayclus, plotterDrawArgs=drawArgs)
-#        #multiclusters
-#        sample = SimpleSample(opts.subdirprefix[1], opts.html_sample, filenames)
-#        val = SimpleValidation([sample], opts.outputDir[1])
-#        htmlReport_2 = val.createHtmlReport(validationName=opts.html_validation_name[1])
-#        hgcmulticlus = [hgcalPlots.hgcalMultiClustersPlotter]
-#        val.doPlots(hgcmulticlus, plotterDrawArgs=drawArgs)
-#        #hits
-#	sample = SimpleSample(opts.subdirprefix[2], opts.html_sample, filenames)
-#	val = SimpleValidation([sample], opts.outputDir[2])
-#	htmlReport_3 = val.createHtmlReport(validationName=opts.html_validation_name[2])
-#	hgchit = [hgcalPlots.hgcalHitPlotter]
-#        val.doPlots(hgchit, plotterDrawArgs=drawArgs)
-#        #calib
-#        sample = SimpleSample(opts.subdirprefix[3], opts.html_sample, filenames)
-#        val = SimpleValidation([sample], opts.outputDir[3])
-#        htmlReport_4 = val.createHtmlReport(validationName=opts.html_validation_name[3])
-#        hgchitcalib = [hgcalPlots.hgcalHitCalibPlotter]
-#        val.doPlots(hgchitcalib, plotterDrawArgs=drawArgs)
 
     if opts.no_html:
         print("Plots created into directory '%s'." % opts.outputDir)
     else:
         htmlReport.write()
-#	if(opts.collection=="all"):
-#		htmlReport_2.write()
-#                htmlReport_3.write()
-#		htmlReport_4.write()
 
         print("Plots and HTML report created into directory '%s'. You can just move it to some www area and access the pages via web browser" % (','.join(opts.outputDir)))
 
