7d64f803c33a10cc8c35a2e8b36f4313771c34af
Andreas Psallidas <andreas.psallidas@cern.ch>
2020-11-05 11:07:53 +0100

    droping type specifications

--- RecoLocalCalo/HGCalRecProducers/python/hgcalLayerClusters_cff.py
+++ RecoLocalCalo/HGCalRecProducers/python/hgcalLayerClusters_cff.py
@@ -11,29 +11,29 @@ from SimCalorimetry.HGCalSimProducers.hgcalDigitizer_cfi import fC_per_ele, HGCA
 hgcalLayerClusters = hgcalLayerClusters_.clone()
 
 hgcalLayerClusters.timeOffset = hgceeDigitizer.tofDelay
-hgcalLayerClusters.plugin.dEdXweights = cms.vdouble(dEdX.weights)
+hgcalLayerClusters.plugin.dEdXweights = dEdX.weights
 #With the introduction of 7 regional factors (6 for silicon plus 1 for scintillator),
 #we extend fcPerMip (along with noises below) so that it is guaranteed that they have 6 entries.
-hgcalLayerClusters.plugin.fcPerMip = cms.vdouble(HGCalUncalibRecHit.HGCEEConfig.fCPerMIP + HGCalUncalibRecHit.HGCHEFConfig.fCPerMIP)
-hgcalLayerClusters.plugin.thicknessCorrection = cms.vdouble(HGCalRecHit.thicknessCorrection)
+hgcalLayerClusters.plugin.fcPerMip = HGCalUncalibRecHit.HGCEEConfig.fCPerMIP + HGCalUncalibRecHit.HGCHEFConfig.fCPerMIP
+hgcalLayerClusters.plugin.thicknessCorrection = HGCalRecHit.thicknessCorrection
 hgcalLayerClusters.plugin.sciThicknessCorrection = HGCalRecHit.sciThicknessCorrection
 hgcalLayerClusters.plugin.deltasi_index_regemfac = HGCalRecHit.deltasi_index_regemfac
-hgcalLayerClusters.plugin.fcPerEle = cms.double(fC_per_ele)
+hgcalLayerClusters.plugin.fcPerEle = fC_per_ele
 #Extending noises as fcPerMip, see comment above.
-hgcalLayerClusters.plugin.noises = cms.vdouble(HGCAL_noises.values + HGCAL_noises.values)
+hgcalLayerClusters.plugin.noises = HGCAL_noises.values + HGCAL_noises.values
 hgcalLayerClusters.plugin.noiseMip = hgchebackDigitizer.digiCfg.noise
 
 hgcalLayerClustersHFNose = hgcalLayerClusters_.clone(
     detector = 'HFNose',
     timeOffset = hfnoseDigitizer.tofDelay,
-    nHitsTime = cms.uint32(3),
+    nHitsTime = 3,
     plugin = dict(
         dEdXweights = dEdX.weightsNose,
-        maxNumberOfThickIndices = cms.uint32(3),
+        maxNumberOfThickIndices = 3,
         fcPerMip = HGCalUncalibRecHit.HGCHFNoseConfig.fCPerMIP,
         thicknessCorrection = HGCalRecHit.thicknessNoseCorrection,
         fcPerEle = fC_per_ele,
-        noises = cms.vdouble(HGCAL_noises.values),
+        noises = HGCAL_noises.values,
         noiseMip = hgchebackDigitizer.digiCfg.noise
     )
 )
