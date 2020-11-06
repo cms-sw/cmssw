fd0666271979b8bafb6959a0f6bbf6625a1b1ad5
Andreas Psallidas <andreas.psallidas@cern.ch>
2020-11-06 11:59:15 +0100

    assign value of the object avoiding bindings

--- RecoLocalCalo/HGCalRecProducers/python/hgcalLayerClusters_cff.py
+++ RecoLocalCalo/HGCalRecProducers/python/hgcalLayerClusters_cff.py
@@ -11,31 +11,31 @@ from SimCalorimetry.HGCalSimProducers.hgcalDigitizer_cfi import fC_per_ele, HGCA
 hgcalLayerClusters = hgcalLayerClusters_.clone(
     timeOffset = hgceeDigitizer.tofDelay,
     plugin = dict(
-        dEdXweights = dEdX.weights,
+        dEdXweights = dEdX.weights.value(),
         #With the introduction of 7 regional factors (6 for silicon plus 1 for scintillator),
         #we extend fcPerMip (along with noises below) so that it is guaranteed that they have 6 entries.
-        fcPerMip = HGCalUncalibRecHit.HGCEEConfig.fCPerMIP + HGCalUncalibRecHit.HGCHEFConfig.fCPerMIP,
-        thicknessCorrection = HGCalRecHit.thicknessCorrection,
-        sciThicknessCorrection = HGCalRecHit.sciThicknessCorrection,
-        deltasi_index_regemfac = HGCalRecHit.deltasi_index_regemfac,
+        fcPerMip = HGCalUncalibRecHit.HGCEEConfig.fCPerMIP.value() + HGCalUncalibRecHit.HGCHEFConfig.fCPerMIP.value(),
+        thicknessCorrection = HGCalRecHit.thicknessCorrection.value(),
+        sciThicknessCorrection = HGCalRecHit.sciThicknessCorrection.value(),
+        deltasi_index_regemfac = HGCalRecHit.deltasi_index_regemfac.value(),
         fcPerEle = fC_per_ele,
         #Extending noises as fcPerMip, see comment above.
-        noises = HGCAL_noises.values + HGCAL_noises.values,
-        noiseMip = hgchebackDigitizer.digiCfg.noise
+        noises = HGCAL_noises.values.value() + HGCAL_noises.values.value(),
+        noiseMip = hgchebackDigitizer.digiCfg.noise.value()
     )
 )
 
 hgcalLayerClustersHFNose = hgcalLayerClusters_.clone(
     detector = 'HFNose',
-    timeOffset = hfnoseDigitizer.tofDelay,
+    timeOffset = hfnoseDigitizer.tofDelay.value(),
     nHitsTime = 3,
     plugin = dict(
-        dEdXweights = dEdX.weightsNose,
+        dEdXweights = dEdX.weightsNose.value(),
         maxNumberOfThickIndices = 3,
-        fcPerMip = HGCalUncalibRecHit.HGCHFNoseConfig.fCPerMIP,
-        thicknessCorrection = HGCalRecHit.thicknessNoseCorrection,
+        fcPerMip = HGCalUncalibRecHit.HGCHFNoseConfig.fCPerMIP.value(),
+        thicknessCorrection = HGCalRecHit.thicknessNoseCorrection.value(),
         fcPerEle = fC_per_ele,
-        noises = HGCAL_noises.values,
-        noiseMip = hgchebackDigitizer.digiCfg.noise
+        noises = HGCAL_noises.values.value(),
+        noiseMip = hgchebackDigitizer.digiCfg.noise.value()
     )
 )
