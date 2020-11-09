db1a1123f0ac9a7d048a287870f8ab76c473af9b
Malik Shahzad Muzaffar <shahzad.malik.muzaffar@cern.ch>
2020-11-09 15:58:18 +0100

    Update RunIndex.cc

--- FWCore/Utilities/src/RunIndex.cc
+++ FWCore/Utilities/src/RunIndex.cc
@@ -3,7 +3,7 @@
 #include <limits>
 
 namespace edm {
- 
+
   const unsigned int RunIndex::invalidValue_ = std::numeric_limits<unsigned int>::max();
 
   RunIndex RunIndex::invalidRunIndex() { return RunIndex(invalidValue_); }
