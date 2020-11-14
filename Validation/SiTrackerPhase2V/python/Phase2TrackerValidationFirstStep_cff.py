d9820ab67134b631a2428c959e9a660940074a0a
Suvankar Roy Chowdhury <suvankar.roy.chowdhury@cern.ch>
2020-11-14 21:52:30 +0100

    adding DQM modules for Phase2 cluster

--- Validation/SiTrackerPhase2V/python/Phase2TrackerValidationFirstStep_cff.py
+++ Validation/SiTrackerPhase2V/python/Phase2TrackerValidationFirstStep_cff.py
@@ -1,7 +1,10 @@
 import FWCore.ParameterSet.Config as cms
 from Validation.SiTrackerPhase2V.Phase2TrackerValidateDigi_cff import *
 from Validation.SiTrackerPhase2V.Phase2ITValidateRecHit_cff import *
+from Validation.SiTrackerPhase2V.Phase2OTValidateCluster_cff import *
+
 trackerphase2ValidationSource = cms.Sequence(pixDigiValid  
                                              + otDigiValid 
                                              + rechitValidIT
+                                             + clusterValidOT
 )
