547b4802aee4c7e0c66c6e658f4874cf7e934044
Justin Williams <justinwilliams@ku.edu>
2020-11-16 12:07:49 -0600

    removed singleRP protons and only storing contributing tracks for multiRP protons

--- PhysicsTools/NanoAOD/plugins/ProtonProducer.cc
+++ PhysicsTools/NanoAOD/plugins/ProtonProducer.cc
@@ -44,12 +44,8 @@
 class ProtonProducer : public edm::global::EDProducer<> {
 public:
   ProtonProducer(edm::ParameterSet const &ps)
-      : tokenRecoProtonsSingleRP_(
-            mayConsume<reco::ForwardProtonCollection>(ps.getParameter<edm::InputTag>("tagRecoProtonsSingle"))),
-        tokenRecoProtonsMultiRP_(
-            mayConsume<reco::ForwardProtonCollection>(ps.getParameter<edm::InputTag>("tagRecoProtonsMulti"))),
-        tokenTracksLite_(mayConsume<std::vector<CTPPSLocalTrackLite>>(ps.getParameter<edm::InputTag>("tagTrackLite"))) {
-    produces<edm::ValueMap<int>>("protonRPId");
+      : tokenRecoProtonsMultiRP_(
+	    mayConsume<reco::ForwardProtonCollection>(ps.getParameter<edm::InputTag>("tagRecoProtonsMulti"))) {
     produces<edm::ValueMap<int>>("arm");
     produces<nanoaod::FlatTable>("ppsTrackTable");
   }
@@ -57,97 +53,35 @@ public:
 
   // ------------ method called to produce the data  ------------
   void produce(edm::StreamID id, edm::Event &iEvent, const edm::EventSetup &iSetup) const override {
-    // Get Forward Proton handles
-    edm::Handle<reco::ForwardProtonCollection> hRecoProtonsSingleRP;
-    iEvent.getByToken(tokenRecoProtonsSingleRP_, hRecoProtonsSingleRP);
-
+    // Get Forward Proton handle
     edm::Handle<reco::ForwardProtonCollection> hRecoProtonsMultiRP;
     iEvent.getByToken(tokenRecoProtonsMultiRP_, hRecoProtonsMultiRP);
 
-    // Get PPS Local Track handle
-    edm::Handle<std::vector<CTPPSLocalTrackLite>> ppsTracksLite;
-    iEvent.getByToken(tokenTracksLite_, ppsTracksLite);
-
     // book output variables for protons
-    std::vector<int> singleRP_RPId;
     std::vector<int> multiRP_arm;
 
     // book output variables for tracks
-    std::vector<float> trackX, trackY, trackTime, trackTimeUnc, localSlopeX, localSlopeY, normalizedChi2;
-    std::vector<int> singleRPProtonIdx, multiRPProtonIdx, decRPId, numFitPoints, pixelRecoInfo, rpType;
+    std::vector<float> trackX, trackY, trackTime, trackTimeUnc;
+    std::vector<int> multiRPProtonIdx, decRPId, rpType;
 
-    // process single-RP protons
-    {
-      const auto &num_proton = hRecoProtonsSingleRP->size();
-      singleRP_RPId.reserve(num_proton);
+    for (unsigned int p_idx = 0; p_idx < hRecoProtonsMultiRP->size(); ++p_idx) {
+      const auto &proton = hRecoProtonsMultiRP->at(p_idx);
+      multiRP_arm.push_back((proton.pz() < 0.) ? 1 : 0);
 
-      for (const auto &proton : *hRecoProtonsSingleRP) {
-        CTPPSDetId rpId((*proton.contributingLocalTracks().begin())->rpId());
-        unsigned int rpDecId = rpId.arm() * 100 + rpId.station() * 10 + rpId.rp();
-        singleRP_RPId.push_back(rpDecId);
+      for (const auto &ref : proton.contributingLocalTracks()) {
+	multiRPProtonIdx.push_back(p_idx);
+	CTPPSDetId rpId(ref->rpId());
+	unsigned int rpDecId = rpId.arm() * 100 + rpId.station() * 10 + rpId.rp();
+	decRPId.push_back(rpDecId);
+	rpType.push_back(rpId.subdetId());
+	trackX.push_back(ref->x());
+	trackY.push_back(ref->y());
+	trackTime.push_back(ref->time());
+	trackTimeUnc.push_back(ref->timeUnc());
       }
     }
-
-    // process multi-RP protons
-    {
-      const auto &num_proton = hRecoProtonsMultiRP->size();
-      multiRP_arm.reserve(num_proton);
-
-      for (const auto &proton : *hRecoProtonsMultiRP) {
-        multiRP_arm.push_back((proton.pz() < 0.) ? 1 : 0);
-      }
-    }
-
-    // process local tracks
-    for (unsigned int tr_idx = 0; tr_idx < ppsTracksLite->size(); ++tr_idx) {
-      const auto &tr = ppsTracksLite->at(tr_idx);
-
-      CTPPSDetId rpId(tr.rpId());
-      unsigned int rpDecId = rpId.arm() * 100 + rpId.station() * 10 + rpId.rp();
-
-      decRPId.push_back(rpDecId);
-      rpType.push_back(rpId.subdetId());
-
-      trackX.push_back(tr.x());
-      trackY.push_back(tr.y());
-      trackTime.push_back(tr.time());
-      trackTimeUnc.push_back(tr.timeUnc());
-      numFitPoints.push_back(tr.numberOfPointsUsedForFit());
-      pixelRecoInfo.push_back(static_cast<int>(tr.pixelTrackRecoInfo()));
-      normalizedChi2.push_back(tr.chiSquaredOverNDF());
-      localSlopeX.push_back(tr.tx());
-      localSlopeY.push_back(tr.ty());
-
-      signed int singleRP_idx = -1;
-      for (unsigned int p_idx = 0; p_idx < hRecoProtonsSingleRP->size(); ++p_idx) {
-        const auto &proton = hRecoProtonsSingleRP->at(p_idx);
-
-        for (const auto &ref : proton.contributingLocalTracks()) {
-          if (ref.key() == tr_idx)
-            singleRP_idx = p_idx;
-        }
-      }
-      singleRPProtonIdx.push_back(singleRP_idx);
-
-      signed int multiRP_idx = -1;
-      for (unsigned int p_idx = 0; p_idx < hRecoProtonsMultiRP->size(); ++p_idx) {
-        const auto &proton = hRecoProtonsMultiRP->at(p_idx);
-
-        for (const auto &ref : proton.contributingLocalTracks()) {
-          if (ref.key() == tr_idx)
-            multiRP_idx = p_idx;
-        }
-      }
-      multiRPProtonIdx.push_back(multiRP_idx);
-    }
-
+  
     // update proton tables
-    std::unique_ptr<edm::ValueMap<int>> protonRPIdV(new edm::ValueMap<int>());
-    edm::ValueMap<int>::Filler fillerID(*protonRPIdV);
-    fillerID.insert(hRecoProtonsSingleRP, singleRP_RPId.begin(), singleRP_RPId.end());
-    fillerID.fill();
-
-    // Testing
     std::unique_ptr<edm::ValueMap<int>> multiRP_armV(new edm::ValueMap<int>());
     edm::ValueMap<int>::Filler fillermultiArm(*multiRP_armV);
     fillermultiArm.insert(hRecoProtonsMultiRP, multiRP_arm.begin(), multiRP_arm.end());
@@ -155,7 +89,6 @@ public:
 
     // build track table
     auto ppsTab = std::make_unique<nanoaod::FlatTable>(trackX.size(), "PPSLocalTrack", false);
-    ppsTab->addColumn<int>("singleRPProtonIdx", singleRPProtonIdx, "local track - proton correspondence");
     ppsTab->addColumn<int>("multiRPProtonIdx", multiRPProtonIdx, "local track - proton correspondence");
     ppsTab->addColumn<float>("x", trackX, "local track x", 16);
     ppsTab->addColumn<float>("y", trackY, "local track y", 13);
@@ -166,7 +99,6 @@ public:
     ppsTab->setDoc("ppsLocalTrack variables");
 
     // save output
-    iEvent.put(std::move(protonRPIdV), "protonRPId");
     iEvent.put(std::move(multiRP_armV), "arm");
     iEvent.put(std::move(ppsTab), "ppsTrackTable");
   }
@@ -174,16 +106,12 @@ public:
   // ------------ method fills 'descriptions' with the allowed parameters for the module  ------------
   static void fillDescriptions(edm::ConfigurationDescriptions &descriptions) {
     edm::ParameterSetDescription desc;
-    desc.add<edm::InputTag>("tagRecoProtonsSingle")->setComment("singleRP proton collection");
     desc.add<edm::InputTag>("tagRecoProtonsMulti")->setComment("multiRP proton collection");
-    desc.add<edm::InputTag>("tagTrackLite")->setComment("pps local tracks lite collection");
     descriptions.add("ProtonProducer", desc);
   }
 
 protected:
-  const edm::EDGetTokenT<reco::ForwardProtonCollection> tokenRecoProtonsSingleRP_;
   const edm::EDGetTokenT<reco::ForwardProtonCollection> tokenRecoProtonsMultiRP_;
-  const edm::EDGetTokenT<std::vector<CTPPSLocalTrackLite>> tokenTracksLite_;
 };
 
 DEFINE_FWK_MODULE(ProtonProducer);
