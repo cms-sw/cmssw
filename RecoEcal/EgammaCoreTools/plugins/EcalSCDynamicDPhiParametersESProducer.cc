636fca30430c08ada3068ce2f408fd4d69533100
Thomas Reis <thomas.reis@cern.ch>
2020-11-18 17:26:16 +0100

    Add fillDescriptions functions.

--- RecoEcal/EgammaCoreTools/plugins/EcalSCDynamicDPhiParametersESProducer.cc
+++ RecoEcal/EgammaCoreTools/plugins/EcalSCDynamicDPhiParametersESProducer.cc
@@ -22,6 +22,8 @@
 // user include files
 #include "FWCore/Framework/interface/ModuleFactory.h"
 #include "FWCore/Framework/interface/ESProducer.h"
+#include <FWCore/ParameterSet/interface/ConfigurationDescriptions.h>
+#include <FWCore/ParameterSet/interface/ParameterSetDescription.h>
 
 #include "CondFormats/DataRecord/interface/EcalSCDynamicDPhiParametersRcd.h"
 #include "RecoEcal/EgammaCoreTools/interface/SCDynamicDPhiParametersHelper.h"
@@ -34,6 +36,8 @@ class EcalSCDynamicDPhiParametersESProducer : public edm::ESProducer {
 public:
   EcalSCDynamicDPhiParametersESProducer(const edm::ParameterSet&);
   ~EcalSCDynamicDPhiParametersESProducer() override;
+
+  static void fillDescriptions(edm::ConfigurationDescriptions& descriptions);
 
   using ReturnType = std::unique_ptr<EcalSCDynamicDPhiParameters>;
 
@@ -64,6 +68,37 @@ EcalSCDynamicDPhiParametersESProducer::ReturnType EcalSCDynamicDPhiParametersESP
     const EcalSCDynamicDPhiParametersRcd& iRecord) {
   auto product = std::make_unique<EcalSCDynamicDPhiParameters>(params_);
   return product;
+}
+
+void EcalSCDynamicDPhiParametersESProducer::fillDescriptions(edm::ConfigurationDescriptions& descriptions) {
+  edm::ParameterSetDescription desc;
+  {
+    edm::ParameterSetDescription vpsd1;
+    vpsd1.add<double>("eMin");
+    vpsd1.add<double>("etaMin");
+    vpsd1.add<double>("yoffset");
+    vpsd1.add<double>("scale");
+    vpsd1.add<double>("xoffset");
+    vpsd1.add<double>("width");
+    vpsd1.add<double>("saturation");
+    vpsd1.add<double>("cutoff");
+    std::vector<edm::ParameterSet> temp1;
+    temp1.reserve(1);
+    {
+      edm::ParameterSet temp2;
+      temp2.addParameter<double>("eMin", 0.);
+      temp2.addParameter<double>("etaMin", 0.);
+      temp2.addParameter<double>("yoffset", 0.0280506);
+      temp2.addParameter<double>("scale", 0.946048);
+      temp2.addParameter<double>("xoffset", -0.101172);
+      temp2.addParameter<double>("width", 0.432767);
+      temp2.addParameter<double>("saturation", 0.14);
+      temp2.addParameter<double>("cutoff", 0.6);
+      temp1.push_back(temp2);
+    }
+    desc.addVPSet("dynamicDPhiParameterSets", vpsd1, temp1);
+  }
+  descriptions.add("ecalSCDynamicDPhiParametersESProducer", desc);
 }
 
 //define this as a plug-in
