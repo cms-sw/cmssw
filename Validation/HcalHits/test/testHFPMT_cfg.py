603c2e7ae5860447276820e54c84f1f414a15929
Christopher Jones <chrisdjones15@gmail.com>
2020-11-19 15:05:42 -0600

    Updated configurations in Validation to new MessageLogger syntax

--- Validation/HcalHits/test/testHFPMT_cfg.py
+++ Validation/HcalHits/test/testHFPMT_cfg.py
@@ -16,20 +16,21 @@ process.load("Configuration.EventContent.EventContent_cff")
 process.load("SimG4Core.Application.g4SimHits_cfi")
 
 process.MessageLogger = cms.Service("MessageLogger",
+    cerr = cms.untracked.PSet(
+        enable = cms.untracked.bool(False)
+    ),
     cout = cms.untracked.PSet(
-        default = cms.untracked.PSet(
-            limit = cms.untracked.int32(0)
-        ),
         HcalSim = cms.untracked.PSet(
             limit = cms.untracked.int32(-1)
         ),
         ValidHcal = cms.untracked.PSet(
             limit = cms.untracked.int32(-1)
-        )
-    ),
-    categories = cms.untracked.vstring('HcalSim', 
-        'ValidHcal'),
-    destinations = cms.untracked.vstring('cout')
+        ),
+        default = cms.untracked.PSet(
+            limit = cms.untracked.int32(0)
+        ),
+        enable = cms.untracked.bool(True)
+    )
 )
 
 process.maxEvents = cms.untracked.PSet(
