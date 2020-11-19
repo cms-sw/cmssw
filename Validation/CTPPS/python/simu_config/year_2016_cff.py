e08ee4cd47fedc1ccc85c3f0810944c0be09a0e5
Jan Kaspar <jan.kaspar@cern.ch>
2020-11-19 17:53:54 +0100

    remove unused code

--- Validation/CTPPS/python/simu_config/year_2016_cff.py
+++ Validation/CTPPS/python/simu_config/year_2016_cff.py
@@ -2,22 +2,16 @@ import FWCore.ParameterSet.Config as cms
 
 from Validation.CTPPS.simu_config.base_cff import *
 
-# FIXME: move to the right place
-from Geometry.VeryForwardGeometry.geometryRPFromDD_2017_cfi import * # using 2017 here is OK
-del ctppsGeometryESModule
-
 # base profile settings for 2016
 profile_base_2016 = profile_base.clone(
   ctppsLHCInfo = dict(
     beamEnergy = 6500
-  ),
-
-  xmlIdealGeometry = dict(
-    geomXMLFiles = totemGeomXMLFiles + ctppsDiamondGeomXMLFiles + ctppsUFSDGeomXMLFiles + ctppsPixelGeomXMLFiles +
-      cms.vstring("Geometry/VeryForwardData/data/2016_ctpps_15sigma_margin0/RP_Dist_Beam_Cent.xml"),
-    rootNodeName = cms.string('cms:CMSE')
   )
 )
+
+# geometry
+from Geometry.VeryForwardGeometry.geometryRPFromDD_2017_cfi import * # using 2017 here is OK
+del ctppsGeometryESModule # this functionality is replaced by the composite ES source
 
 # local reconstruction
 ctppsLocalTrackLiteProducer.includeStrips = True
