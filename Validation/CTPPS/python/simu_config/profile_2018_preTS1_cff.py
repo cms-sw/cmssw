53861cfcc28d4add03da92a21fa2c69b9bfab183
Jan Kaspar <jan.kaspar@cern.ch>
2020-11-19 10:56:54 +0100

    add default values of L_int

--- Validation/CTPPS/python/simu_config/profile_2018_preTS1_cff.py
+++ Validation/CTPPS/python/simu_config/profile_2018_preTS1_cff.py
@@ -5,6 +5,8 @@ from Validation.CTPPS.simu_config.year_2018_cff import *
 alignmentFile = "Validation/CTPPS/alignment/2018_preTS1.xml"
 
 profile_2018_preTS1 = profile_base_2018.clone(
+  L_int = 18.488297964,
+
   ctppsLHCInfo = dict(
     xangleBetaStarHistogramObject = "2018_preTS1/h2_betaStar_vs_xangle"
   ),
