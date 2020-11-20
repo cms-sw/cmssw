8077558a33ee27ffb2f35787b3d549d82daed9df
Thiago <Thiago.Tomei@cern.ch>
2020-11-20 20:04:17 +0100

    Support for L1 HPS PF Taus (from Talinn group)

--- DataFormats/L1TParticleFlow/src/classes.h
+++ DataFormats/L1TParticleFlow/src/classes.h
@@ -6,3 +6,15 @@
 #include "DataFormats/L1TParticleFlow/interface/PFCandidate.h"
 #include "DataFormats/L1TParticleFlow/interface/PFJet.h"
 #include "DataFormats/L1TParticleFlow/interface/PFTau.h"
+#include "DataFormats/L1TParticleFlow/interface/HPSPFTau.h"
+#include "DataFormats/L1TParticleFlow/interface/HPSPFTauFwd.h"
+
+namespace DataFormats_L1TParticleFlow {
+  struct dictionary {
+    l1t::HPSPFTau hpspftau;
+    l1t::HPSPFTauCollection hpspftauCollection;
+    edm::Wrapper<l1t::HPSPFTauCollection> hpspftauCWrapper;
+    l1t::HPSPFTauRefVector hpspftaureRefVector;
+    l1t::HPSPFTauVectorRef hpspftauVectorRef;
+  };
+}  // namespace DataFormats_L1TParticleFlow
