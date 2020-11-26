73ddfe93176b3037e38cac7552ac10a8ce2b5b1f
Alexx Perloff <Alexx.Perloff@Colorado.edu>
2020-11-26 01:20:33 -0600

    Apply the changes recommended by clang-format.
    (cherry picked from commit 0ca56cc77dd7c31b87cdc76713a91083e122ec4a)

--- L1Trigger/VertexFinder/interface/selection.h
+++ L1Trigger/VertexFinder/interface/selection.h
@@ -7,14 +7,16 @@
 
 namespace l1tVertexFinder {
 
-//! Returns primary vertex based on default criterion (max sum pT from all constituent tracks); throws if given empty collection.
-const l1t::Vertex& getPrimaryVertex(const std::vector<l1t::Vertex>& aVertexCollection);
+  //! Returns primary vertex based on default criterion (max sum pT from all constituent tracks); throws if given empty collection.
+  const l1t::Vertex& getPrimaryVertex(const std::vector<l1t::Vertex>& aVertexCollection);
 
-//! Returns vertex for which parameter 'aFunction' returns the highest value; throws if given empty collection.
-const l1t::Vertex& getPrimaryVertex(const std::vector<l1t::Vertex>& aVertexCollection, const std::function<float(const std::vector<edm::Ptr<l1t::Vertex::Track_t>>&)>& aFunction);
+  //! Returns vertex for which parameter 'aFunction' returns the highest value; throws if given empty collection.
+  const l1t::Vertex& getPrimaryVertex(
+      const std::vector<l1t::Vertex>& aVertexCollection,
+      const std::function<float(const std::vector<edm::Ptr<l1t::Vertex::Track_t>>&)>& aFunction);
 
-// const l1t::Vertex& getPrimaryVertex(const std::vector<l1t::Vertex>& aVertexCollection, const std::function<float (const l1t::Vertex::Track_t&)> aFunction);
+  // const l1t::Vertex& getPrimaryVertex(const std::vector<l1t::Vertex>& aVertexCollection, const std::function<float (const l1t::Vertex::Track_t&)> aFunction);
 
-} // end namespace l1tVertexFinder
+}  // end namespace l1tVertexFinder
 
 #endif
