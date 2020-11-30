d6b916758cb956bfe361102d0d38e93f376b679f
Thiago Tomei <Thiago.Tomei@cern.ch>
2020-11-30 17:21:36 -0300

    Update DataFormats/L1TParticleFlow/src/classes.h
    Removed the additional dictionary of collections, Refs, etc. for HPSPFTau
    
    Co-authored-by: Andrea Bocci <andrea.bocci@cern.ch>

--- DataFormats/L1TParticleFlow/src/classes.h
+++ DataFormats/L1TParticleFlow/src/classes.h
@@ -8,13 +8,3 @@
 #include "DataFormats/L1TParticleFlow/interface/PFTau.h"
 #include "DataFormats/L1TParticleFlow/interface/HPSPFTau.h"
 #include "DataFormats/L1TParticleFlow/interface/HPSPFTauFwd.h"
-
-namespace DataFormats_L1TParticleFlow {
-  struct dictionary {
-    l1t::HPSPFTau hpspftau;
-    l1t::HPSPFTauCollection hpspftauCollection;
-    edm::Wrapper<l1t::HPSPFTauCollection> hpspftauCWrapper;
-    l1t::HPSPFTauRefVector hpspftaureRefVector;
-    l1t::HPSPFTauVectorRef hpspftauVectorRef;
-  };
-}  // namespace DataFormats_L1TParticleFlow
