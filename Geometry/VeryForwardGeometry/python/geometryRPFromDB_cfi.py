9b5a389cd3c5d23ab0e70a555cff2841b0cbab03
Laurent Forthomme <laurent.forthomme@cern.ch>
2020-12-01 13:17:41 +0100

    Specifying the isRun2 flag in ctppsGeometryESModule base object definition

--- Geometry/VeryForwardGeometry/python/geometryRPFromDB_cfi.py
+++ Geometry/VeryForwardGeometry/python/geometryRPFromDB_cfi.py
@@ -8,14 +8,15 @@ XMLIdealGeometryESSource_CTPPS = cms.ESProducer("XMLIdealGeometryESProducer",
 
 ctppsGeometryESModule = cms.ESProducer("CTPPSGeometryESModule",
     verbosity = cms.untracked.uint32(1),
+    isRun2 = cms.bool(False),
     compactViewTag = cms.string('XMLIdealGeometryESSource_CTPPS')
 )
 
 from Configuration.Eras.Modifier_ctpps_2016_cff import ctpps_2016
-ctpps_2016.toModify(ctppsGeometryESModule, isRun2=True)
+ctpps_2016.toModify(ctppsGeometryESModule, isRun2=cms.bool(True))
 
 from Configuration.Eras.Modifier_ctpps_2017_cff import ctpps_2017
-ctpps_2017.toModify(ctppsGeometryESModule, isRun2=True)
+ctpps_2017.toModify(ctppsGeometryESModule, isRun2=cms.bool(True))
 
 from Configuration.Eras.Modifier_ctpps_2018_cff import ctpps_2018
-ctpps_2018.toModify(ctppsGeometryESModule, isRun2=True)
+ctpps_2018.toModify(ctppsGeometryESModule, isRun2=cms.bool(True))
