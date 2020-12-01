08a4ebec226ec4db39fd4bd454e59592d58bea80
Sunanda <sunanda.banerjee@cern.ch>
2020-12-01 20:14:47 +0100

    Make all 2017 modified MB cff's to 2017Plan1

--- Configuration/Geometry/python/GeometryExtended2017Plan1FlatPlus10Percent_cff.py
+++ Configuration/Geometry/python/GeometryExtended2017Plan1FlatPlus10Percent_cff.py
@@ -1,9 +1,9 @@
 import FWCore.ParameterSet.Config as cms
 
 #
-# 2017 Geometry configuration with tracker material modified by +5%
+# 2017 Geometry configuration with tracker material modified by +10%
 #
-from Geometry.CMSCommonData.cmsExtendedGeometry2017FlatPlus05PercentXML_cfi import *
+from Geometry.CMSCommonData.cmsExtendedGeometry2017Plan1FlatPlus10PercentXML_cfi import *
 from Geometry.TrackerNumberingBuilder.trackerNumberingGeometry_cfi import *
 from Geometry.EcalCommonData.ecalSimulationParameters_cff import *
 from Geometry.HcalCommonData.hcalDDDSimConstants_cff import *
