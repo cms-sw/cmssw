7d4826ec5c0856cc1b921b716c5294ab3b76606c
Sergio Lo Meo <sergio.lomeo@cern.ch>
2020-12-03 09:08:02 +0100

    restored DDD units

--- Geometry/RPCGeometryBuilder/src/RPCGeometryParsFromDD.cc
+++ Geometry/RPCGeometryBuilder/src/RPCGeometryParsFromDD.cc
@@ -22,7 +22,7 @@
 #include <DetectorDescription/DDCMS/interface/DDFilteredView.h>
 #include <DetectorDescription/DDCMS/interface/DDCompactView.h>
 #include "DetectorDescription/DDCMS/interface/DDSpecParRegistry.h"
-#include "DataFormats/Math/interface/GeantUnits.h"
+//#include "DataFormats/Math/interface/GeantUnits.h"
 #include "FWCore/MessageLogger/interface/MessageLogger.h"
 
 RPCGeometryParsFromDD::RPCGeometryParsFromDD() {}
@@ -99,28 +99,28 @@ void RPCGeometryParsFromDD::buildGeometry(DDFilteredView& fview,
     rota.GetComponents(x, y, z);
     std::vector<double> pars;
     if (dpar.size() == 3) {
-      const double width = geant_units::operators::convertMmToCm(dpar[0]);
-      const double length = geant_units::operators::convertMmToCm(dpar[1]);
-      const double thickness = geant_units::operators::convertMmToCm(dpar[2]);
+      const double width = dpar[0];
+      const double length = dpar[1];
+      const double thickness = dpar[2];
       edm::LogVerbatim("RPCGeometryParsFromDD")
           << " (2) dpar.size() == 3, width: " << width << " length: " << length << " thickness: " << thickness;
       pars = {width, length, thickness, numbOfStrips.doubles()[0]};
     } else {
-      const double dpar4 = geant_units::operators::convertMmToCm(dpar[4]);
-      const double dpar8 = geant_units::operators::convertMmToCm(dpar[8]);
-      const double dpar0 = geant_units::operators::convertMmToCm(dpar[0]);
+      const double dpar4 = dpar[4];
+      const double dpar8 = dpar[8];
+      const double dpar0 = dpar[0];
       pars = {dpar4, dpar8, dpar0, 0.4, numbOfStrips.doubles()[0]};
       edm::LogVerbatim("RPCGeometryParsFromDD")
           << " (3), else, dpar[4]: " << dpar4 << " dpar[8]: " << dpar8 << " dpar[0]: " << dpar0;
     }
 
-    const std::vector<double> vtra = {geant_units::operators::convertMmToCm(tran.x()),
-                                      geant_units::operators::convertMmToCm(tran.y()),
-                                      geant_units::operators::convertMmToCm(tran.z())};
+    const std::vector<double> vtra = {tran.x(),
+                                      tran.y(),
+                                      tran.z()};
 
-    edm::LogVerbatim("RPCGeometryParsFromDD") << " (4), tran.x() " << geant_units::operators::convertMmToCm(tran.x())
-                                              << " tran.y(): " << geant_units::operators::convertMmToCm(tran.y())
-                                              << " tran.z(): " << geant_units::operators::convertMmToCm(tran.z());
+    edm::LogVerbatim("RPCGeometryParsFromDD") << " (4), tran.x() " << tran.x()
+                                              << " tran.y(): " << tran.y()
+                                              << " tran.z(): " << tran.z();
 
     const std::vector<double> vrot = {x.X(), x.Y(), x.Z(), y.X(), y.Y(), y.Z(), z.X(), z.Y(), z.Z()};
     edm::LogVerbatim("RPCGeometryParsFromDD")
@@ -153,9 +153,9 @@ void RPCGeometryParsFromDD::buildGeometry(cms::DDFilteredView& fview,
     const std::vector<std::string> strpars = {std::string(name)};
 
     std::vector<double> tran(3);
-    tran[0] = static_cast<double>(fview.translation().X());
-    tran[1] = static_cast<double>(fview.translation().Y());
-    tran[2] = static_cast<double>(fview.translation().Z());
+    tran[0] = static_cast<double>(fview.translation().X()) / dd4hep::mm;
+    tran[1] = static_cast<double>(fview.translation().Y()) / dd4hep::mm;
+    tran[2] = static_cast<double>(fview.translation().Z()) / dd4hep::mm;
 
     DDRotationMatrix rota;
     fview.rot(rota);
@@ -164,16 +164,16 @@ void RPCGeometryParsFromDD::buildGeometry(cms::DDFilteredView& fview,
     const std::vector<double> rot = {x.X(), x.Y(), x.Z(), y.X(), y.Y(), y.Z(), z.X(), z.Y(), z.Z()};
 
     if (dd4hep::isA<dd4hep::Box>(fview.solid())) {
-      const std::vector<double> pars = {dpar[0], dpar[1], dpar[2], double(nStrips)};
+      const std::vector<double> pars = {dpar[0] / dd4hep::mm, dpar[1] / dd4hep::mm, dpar[2] / dd4hep::mm, double(nStrips)};
       edm::LogVerbatim("RPCGeometryParsFromDD")
-          << " (2), dd4hep::Box, width: " << dpar[0] << " length: " << dpar[1] << " thickness: " << dpar[2];
+	<< " (2), dd4hep::Box, width: " << dpar[0] / dd4hep::mm<< " length: " << dpar[1] / dd4hep::mm<< " thickness: " << dpar[2] / dd4hep::mm;
       rgeo.insert(rpcid, tran, rot, pars, strpars);
     } else {
       const double ti = 0.4;
-      const std::vector<double> pars = {dpar[0], dpar[1], dpar[3], ti, double(nStrips)};
+      const std::vector<double> pars = {dpar[0] / dd4hep::mm, dpar[1] / dd4hep::mm, dpar[3] / dd4hep::mm, ti, double(nStrips)};
       edm::LogVerbatim("RPCGeometryParsFromDD")
-          << " (3), else, dpar[0] (i.e. dpar[4] for DD): " << dpar[0] << " dpar[1] (i.e. dpar[8] for DD): " << dpar[1]
-          << " dpar[3] (i.e. dpar[0] for DD): " << dpar[3];
+	<< " (3), else, dpar[0] (i.e. dpar[4] for DD): " << dpar[0] / dd4hep::mm<< " dpar[1] (i.e. dpar[8] for DD): " << dpar[1] / dd4hep::mm
+	<< " dpar[3] (i.e. dpar[0] for DD): " << dpar[3] / dd4hep::mm;
       rgeo.insert(rpcid, tran, rot, pars, strpars);
     }
     edm::LogVerbatim("RPCGeometryParsFromDD")
