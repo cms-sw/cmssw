127c2f95d87c0d7ef55a9eb09f0c9f4b4900f29f
Leonardo Cristella <leonardo.cristella@cern.ch>
2020-12-09 12:23:59 +0100

    Modifier for premix_stage2

--- SimDataFormats/Associations/python/LCToCPAsssociation_cfi.py
+++ SimDataFormats/Associations/python/LCToCPAsssociation_cfi.py
@@ -5,3 +5,8 @@ trackingParticleRecoTrackAsssociation = cms.EDProducer("LCToCPAssociatorEDProduc
     label_cp = cms.InputTag("mix","MergedCaloTruth"),
     label_lc = cms.InputTag("hgcalLayerClusters")
 )
+
+from Configuration.ProcessModifiers.premix_stage2_cff import premix_stage2
+premix_stage2.toModify(trackingParticleRecoTrackAsssociation,
+    label_cp = "mixData:MergedCaloTruth"
+)
