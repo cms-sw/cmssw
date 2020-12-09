cc64919c8dff48d87e5a9f95fc5d92f2236fb1cc
Laurent Forthomme <laurent.forthomme@cern.ch>
2020-12-09 14:38:04 +0100

    Picking up isRun2 flag from base ESModule

--- Validation/CTPPS/python/simu_config/year_2016_cff.py
+++ Validation/CTPPS/python/simu_config/year_2016_cff.py
@@ -12,7 +12,7 @@ profile_base_2016 = profile_base.clone(
 # geometry
 from Geometry.VeryForwardGeometry.geometryRPFromDD_2017_cfi import * # using 2017 here is OK
 ctppsCompositeESSource.compactViewTag = ctppsGeometryESModule.compactViewTag
-ctppsCompositeESSource.isRun2 = True
+ctppsCompositeESSource.isRun2 = ctppsGeometryESModule.isRun2
 del ctppsGeometryESModule # this functionality is replaced by the composite ES source
 
 # local reconstruction
