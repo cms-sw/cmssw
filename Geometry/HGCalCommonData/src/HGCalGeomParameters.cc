a0737f68cd6f04593f1c9eb5c78f8bfaa8ded632
Sunanda <sunanda.banerjee@cern.ch>
2020-12-09 19:47:26 +0100

    Undo the changes in HGCalGeomParameters

--- Geometry/HGCalCommonData/src/HGCalGeomParameters.cc
+++ Geometry/HGCalCommonData/src/HGCalGeomParameters.cc
@@ -12,7 +12,6 @@
 #include "DetectorDescription/Core/interface/DDValue.h"
 #include "DetectorDescription/Core/interface/DDutils.h"
 #include "DetectorDescription/RegressionTest/interface/DDErrorDetection.h"
-#include "Geometry/HGCalCommonData/interface/HGCalTileIndex.h"
 #include "Geometry/HGCalCommonData/interface/HGCalTypes.h"
 #include "Geometry/HGCalCommonData/interface/HGCalWaferIndex.h"
 #include "Geometry/HGCalCommonData/interface/HGCalWaferMask.h"
@@ -1405,7 +1404,7 @@ void HGCalGeomParameters::loadSpecParsTrapezoid(HGCalParameters& php,
     php.tileInfoMap_[tileIndx[k]] =
         HGCalParameters::tileInfo(tileType[k], tileSiPM[k], tileHEX1[k], tileHEX2[k], tileHEX3[k], tileHEX4[k]);
 #ifdef EDM_ML_DEBUG
-    edm::LogVerbatim("HGCalGeom") << "Tile[" << k << ":" << tileIndx[k] << ":" << std::hex << tileIndx[k] << std::dec << ":" << HGCalTileIndex::tileLayer(tileIndx[k]) << ":" << HGCalTileIndex::tileRing(tileIndx[k]) << ":" << HGCalTileIndex::tilePhi(tileIndx[k])<< "] "
+    edm::LogVerbatim("HGCalGeom") << "Tile[" << k << ":" << tileIndx[k] << "] "
                                   << " Type " << tileType[k] << " SiPM " << tileSiPM[k] << " HEX " << std::hex
                                   << tileHEX1[k] << ":" << tileHEX2[k] << ":" << tileHEX3[k] << ":" << tileHEX4[k]
                                   << std::dec;
