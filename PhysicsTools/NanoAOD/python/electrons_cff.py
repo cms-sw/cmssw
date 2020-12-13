00f7b92417733424660a97202ff50ee247a2b4f3
maria <dalfonso@cern.ch>
2020-12-13 22:21:16 +0100

    centralize modifiers in one place

--- PhysicsTools/NanoAOD/python/electrons_cff.py
+++ PhysicsTools/NanoAOD/python/electrons_cff.py
@@ -1,14 +1,6 @@
 import FWCore.ParameterSet.Config as cms
-from Configuration.Eras.Modifier_run2_egamma_2016_cff import run2_egamma_2016
-from Configuration.Eras.Modifier_run2_miniAOD_80XLegacy_cff import run2_miniAOD_80XLegacy
-from Configuration.Eras.Modifier_run2_nanoAOD_94XMiniAODv1_cff import run2_nanoAOD_94XMiniAODv1
-from Configuration.Eras.Modifier_run2_nanoAOD_94XMiniAODv2_cff import run2_nanoAOD_94XMiniAODv2
-from Configuration.Eras.Modifier_run2_nanoAOD_94X2016_cff import run2_nanoAOD_94X2016
-from Configuration.Eras.Modifier_run2_nanoAOD_102Xv1_cff import run2_nanoAOD_102Xv1
-from Configuration.Eras.Modifier_run2_nanoAOD_106Xv1_cff import run2_nanoAOD_106Xv1
-from Configuration.Eras.Modifier_run2_egamma_2017_cff import run2_egamma_2017
-from Configuration.Eras.Modifier_run2_egamma_2018_cff import run2_egamma_2018
 
+from PhysicsTools.NanoAOD.nano_eras_cff import *
 from PhysicsTools.NanoAOD.common_cff import *
 import PhysicsTools.PatAlgos.producersLayer1.electronProducer_cfi
 from math import ceil,log
