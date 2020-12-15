eef856168d80a53709f7726ab4a6056f3b94f4b2
Marco Rovere <marco.rovere@cern.ch>
2020-12-15 13:28:25 +0100

    Merge header and implementation file for digitizer plugins

--- SimCalorimetry/HGCalSimProducers/plugins/HGCEEDigitizer.cc
+++ SimCalorimetry/HGCalSimProducers/plugins/HGCEEDigitizer.cc
@@ -1,4 +1,20 @@
-#include "HGCEEDigitizer.h"
+#include "SimCalorimetry/HGCalSimProducers/interface/HGCDigitizerBase.h"
+#include "DataFormats/HGCDigi/interface/HGCDigiCollections.h"
+#include "FWCore/PluginManager/interface/PluginFactory.h"
+#include "SimCalorimetry/HGCalSimProducers/interface/HGCDigitizerPluginFactory.h"
+
+class HGCEEDigitizer : public HGCDigitizerBase {
+public:
+  HGCEEDigitizer(const edm::ParameterSet& ps);
+  void runDigitizer(std::unique_ptr<HGCalDigiCollection>& digiColl,
+                    hgc::HGCSimHitDataAccumulator& simData,
+                    const CaloSubdetectorGeometry* theGeom,
+                    const std::unordered_set<DetId>& validIds,
+                    CLHEP::HepRandomEngine* engine) override;
+  ~HGCEEDigitizer() override;
+
+private:
+};
 
 using namespace hgc_digi;
 
@@ -14,3 +30,5 @@ void HGCEEDigitizer::runDigitizer(std::unique_ptr<HGCalDigiCollection>& digiColl
 
 //
 HGCEEDigitizer::~HGCEEDigitizer() {}
+
+DEFINE_EDM_PLUGIN(HGCDigitizerPluginFactory, HGCEEDigitizer, "HGCEEDigitizer");
