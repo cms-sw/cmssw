ebfd5f0b00e3722faca3c68609f9975c6cb98bdd
JeongEun Lee <jeongeun.lee@cern.ch>
2020-12-16 14:08:39 +0900

    Update HIBestVertexSequences_cff.py

--- RecoHI/HiTracking/python/HIBestVertexSequences_cff.py
+++ RecoHI/HiTracking/python/HIBestVertexSequences_cff.py
@@ -21,16 +21,16 @@ from RecoHI.HiTracking.HIPixelAdaptiveVertex_cfi import *
 hiOfflinePrimaryVertices=hiPixelAdaptiveVertex.clone( # vertexing run AFTER tracking
     TrackLabel = "hiGeneralTracks",
                                        
-    TkFilterParameters = dict(
-        algorithm = 'filterWithThreshold',
-        maxNormalizedChi2 = 5.0,
-        minPixelLayersWithHits = 3,   #0 missing pix hit (Run 1 pixels)
-        minSiliconLayersWithHits = 5, #at least 8 (3pix+5strip) hits total
-        maxD0Significance = 3.0,      #default is 5.0 in pp; 3.0 here suppresses split vtxs
-        minPt  = 0.0,
-        maxEta = 100.,               
-        trackQuality = "any",
-        numTracksThreshold = 2
+    TkFilterParameters = cms.PSet(
+	algorithm                = cms.string('filterWithThreshold'),
+        maxNormalizedChi2        = cms.double(5.0),
+        minPixelLayersWithHits   = cms.int32(3),   #0 missing pix hit (Run 1 pixels)
+        minSiliconLayersWithHits = cms.int32(5), #at least 8 (3pix+5strip) hits total
+        maxD0Significance        = cms.double(3.0),      #default is 5.0 in pp; 3.0 here suppresses split vtxs
+        minPt                    = cms.double(0.0),
+        maxEta                   = cms.double(100.),               
+        trackQuality             = cms.string("any"),
+        numTracksThreshold       = cms.int32(2)
     )
 )
 from Configuration.Eras.Modifier_trackingPhase1_cff import trackingPhase1
