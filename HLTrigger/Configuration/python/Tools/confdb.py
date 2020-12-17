bdf1e0117698f54c035ccbaa63a7bcd290c91b0c
Martin Grunewald <Martin.Grunewald@cern.ch>
2020-12-17 17:40:42 +0100

    Fix MessageLogger -2nd round

--- HLTrigger/Configuration/python/Tools/confdb.py
+++ HLTrigger/Configuration/python/Tools/confdb.py
@@ -541,11 +541,11 @@ if 'PrescaleService' in process.__dict__:
     # request summary informations from the MessageLogger
     self.data += """
 if 'MessageLogger' in %(dict)s:
-    %(process)s.MessageLogger.TriggerSummaryProducerAOD=dict()
-    %(process)s.MessageLogger.L1GtTrigReport=dict()
-    %(process)s.MessageLogger.L1TGlobalSummary=dict()
-    %(process)s.MessageLogger.HLTrigReport=dict()
-    %(process)s.MessageLogger.FastReport=dict()
+    %(process)s.MessageLogger.TriggerSummaryProducerAOD=cms.untracked.PSet()
+    %(process)s.MessageLogger.L1GtTrigReport=cms.untracked.PSet()
+    %(process)s.MessageLogger.L1TGlobalSummary=cms.untracked.PSet()
+    %(process)s.MessageLogger.HLTrigReport=cms.untracked.PSet()
+    %(process)s.MessageLogger.FastReport=cms.untracked.PSet()
 """
 
 
--- HLTrigger/Configuration/test/OnLine_HLT_Fake.py
+++ HLTrigger/Configuration/test/OnLine_HLT_Fake.py
@@ -358,11 +358,11 @@ if 'GlobalTag' in process.__dict__:
     process.GlobalTag = customiseGlobalTag(process.GlobalTag, globaltag = 'auto:run1_hlt_Fake')
 
 if 'MessageLogger' in process.__dict__:
-    process.MessageLogger.TriggerSummaryProducerAOD=dict()
-    process.MessageLogger.L1GtTrigReport=dict()
-    process.MessageLogger.L1TGlobalSummary=dict()
-    process.MessageLogger.HLTrigReport=dict()
-    process.MessageLogger.FastReport=dict()
+    process.MessageLogger.TriggerSummaryProducerAOD=cms.untracked.PSet()
+    process.MessageLogger.L1GtTrigReport=cms.untracked.PSet()
+    process.MessageLogger.L1TGlobalSummary=cms.untracked.PSet()
+    process.MessageLogger.HLTrigReport=cms.untracked.PSet()
+    process.MessageLogger.FastReport=cms.untracked.PSet()
 
 # add specific customizations
 _customInfo = {}
