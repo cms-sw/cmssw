86e88096dbf4e99b4ad7c523e2ad0054ec43e02c
Sunanda <sunanda.banerjee@cern.ch>
2020-12-17 22:57:21 +0100

    Enable the showerlibrary producer to work in the new CMSSW version

--- SimG4CMS/ShowerLibraryProducer/python/hcalforwardshower_cfi.py
+++ SimG4CMS/ShowerLibraryProducer/python/hcalforwardshower_cfi.py
@@ -3,7 +3,11 @@ import FWCore.ParameterSet.Config as cms
 XMLIdealGeometryESSource = cms.ESSource("XMLIdealGeometryESSource",
     geomXMLFiles = cms.vstring('Geometry/CMSCommonData/data/materials.xml', 
         'Geometry/HcalCommonData/data/hcalforwardmaterial.xml', 
-        'Geometry/HcalCommonData/data/hcalforwardshower.xml'),
+        'Geometry/HcalCommonData/data/hcalforwardshower/v1/hcalforwardshower.xml',
+        'Geometry/HcalCommonData/data/hcalSimNumbering.xml',
+        'Geometry/HcalSimData/data/hf.xml',
+        'Geometry/HcalSimData/data/hfpmt.xml',
+        'Geometry/HcalSimData/data/hffibrebundle.xml'),
     rootNodeName = cms.string('hcalforwardshower:HFWorld')
 )
 
