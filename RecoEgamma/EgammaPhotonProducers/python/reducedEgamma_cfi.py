e299916c70169fb6d0e336d3e3a98841306e118d
Jonas Rembser <jonas.rembser@cern.ch>
2020-12-18 14:05:02 +0100

    implement review comments to reducedEgamma_cfi.py

--- RecoEgamma/EgammaPhotonProducers/python/reducedEgamma_cfi.py
+++ RecoEgamma/EgammaPhotonProducers/python/reducedEgamma_cfi.py
@@ -54,14 +54,6 @@ phase2_common.toModify(reducedEgamma,
         preshowerEcalHits = cms.InputTag(""),
 )
 
-from Configuration.Eras.Modifier_pp_on_AA_2018_cff import pp_on_AA_2018
-from Configuration.Eras.Modifier_pp_on_PbPb_run3_cff import pp_on_PbPb_run3
-(pp_on_AA_2018 | pp_on_PbPb_run3).toModify(
-    reducedEgamma,
-    hiPhotonIsolationMapInput = cms.InputTag("photonIsolationHIProducerppGED"),
-    hiPhotonIsolationMapOutput = cms.string("photonIsolationHIProducerppGED")
-)
-
 from Configuration.Eras.Modifier_run2_miniAOD_80XLegacy_cff import run2_miniAOD_80XLegacy
 run2_miniAOD_80XLegacy.toModify(
     reducedEgamma, 
@@ -126,9 +118,13 @@ modifyReducedEGammaRun2MiniAOD = (
     run2_miniAOD_94XFall17 | run2_miniAOD_80XLegacy | run2_miniAOD_UL).makeProcessModifier(calibrateReducedEgamma)
 
 from Configuration.ProcessModifiers.pp_on_AA_cff import pp_on_AA
-pp_on_AA.toModify(reducedEgamma,
-    ootPhotons = cms.InputTag(""),
+
+pp_on_AA.toModify(
+    reducedEgamma,
+    ootPhotons = "",
     keepPhotons = "pt>15 && abs(eta)<2.5",
     slimRelinkPhotons = "pt>15 && abs(eta)<2.5",
-    relinkPhotons = "pt>15 && abs(eta)<2.5"
-    )
+    relinkPhotons = "pt>15 && abs(eta)<2.5",
+    hiPhotonIsolationMapInput = "photonIsolationHIProducerppGED",
+    hiPhotonIsolationMapOutput = "photonIsolationHIProducerppGED"
+)
