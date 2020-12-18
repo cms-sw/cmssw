92b32a55770e3adaa33397dd8d38f42e7153714d
Sunanda <sunanda.banerjee@cern.ch>
2020-12-18 23:58:46 +0100

    Compress the property information of Wafers and tiles to reduce space requirement

---
+++ Geometry/HGCalCommonData/src/HGCalProperty.cc
@@ -0,0 +1,26 @@
+#include "Geometry/HGCalCommonData/interface/HGCalProperty.h"
+
+const int32_t kHGCalFactor = 10;
+const int32_t kHGCalOffsetThick = 1;
+const int32_t kHGCalOffsetPartial = 10;
+const int32_t kHGCalOffsetOrient = 100;
+const int32_t kHGCalOffsetType = 1;
+const int32_t kHGCalOffsetSiPM = 10;
+
+int32_t HGCalProperty::waferProperty(const int32_t thick, const int32_t part, const int32_t orient) {
+  return (((thick % kHGCalFactor) * kHGCalOffsetThick) + ((part % kHGCalFactor) * kHGCalOffsetPartial) + ((orient % kHGCalFactor) * kHGCalOffsetOrient));
+}
+
+int32_t HGCalProperty::waferThick(const int32_t property) { return ((property / kHGCalOffsetThick) % kHGCalFactor); }
+
+int32_t HGCalProperty::waferPartial(const int32_t property) { return ((property / kHGCalOffsetPartial) % kHGCalFactor); }
+
+int32_t HGCalProperty::waferOrient(const int32_t property) { return  ((property / kHGCalOffsetOrient) % kHGCalFactor); }
+
+int32_t HGCalProperty::tileProperty(const int32_t type, const int32_t sipm) {
+  return (((type % kHGCalFactor) * kHGCalOffsetType) + ((sipm % kHGCalFactor) * kHGCalOffsetSiPM));
+}
+
+int32_t HGCalProperty::tileType(const int32_t property) { return ((property / kHGCalOffsetType) % kHGCalFactor); }
+
+int32_t HGCalProperty::tileSiPM(const int32_t property) { return ((property / kHGCalOffsetSiPM) % kHGCalFactor); }
