babaaf980155bae7abe3d3abc8058693d902cf6f
Gabrielle Hugo <gabrielle.hugo83@gmail.com>
2020-12-21 22:50:44 +0100

    Update comments regarding future change of dd4hep unit

--- Geometry/VeryForwardGeometryBuilder/src/DetGeomDesc.cc
+++ Geometry/VeryForwardGeometryBuilder/src/DetGeomDesc.cc
@@ -47,14 +47,14 @@ DetGeomDesc::DetGeomDesc(const cms::DDFilteredView& fv, const bool isRun2)
     : m_name(computeNameWithNoNamespace(fv.name())),
       m_copy(fv.copyNum()),
       m_isDD4hep(true),
-      m_trans(fv.translation() / dd4hep::mm),  // converted from cm (DD4hep) to mm
+      m_trans(fv.translation() / dd4hep::mm),  // converted from DD4hep unit to mm
       m_rot(fv.rotation()),
-      m_params(computeParameters(fv)),  // default unit from DD4hep (cm)
+      m_params(computeParameters(fv)),  // default unit from DD4hep
       m_isABox(dd4hep::isA<dd4hep::Box>(fv.solid())),
-      m_diamondBoxParams(computeDiamondDimensions(m_isABox, m_isDD4hep, m_params)),  // converted from cm (DD4hep) to mm
+      m_diamondBoxParams(computeDiamondDimensions(m_isABox, m_isDD4hep, m_params)),  // converted from DD4hep unit to mm
       m_sensorType(computeSensorType(fv.name())),
       m_geographicalID(computeDetIDFromDD4hep(m_name, fv.copyNos(), fv.copyNum(), isRun2)),
-      m_z(fv.translation().z() / dd4hep::mm)  // converted from cm (DD4hep) to mm
+      m_z(fv.translation().z() / dd4hep::mm)  // converted from DD4hep unit to mm
 {}
 
 DetGeomDesc::DetGeomDesc(const DetGeomDesc& ref, CopyMode cm) {
@@ -140,7 +140,7 @@ std::vector<double> DetGeomDesc::computeParameters(const cms::DDFilteredView& fv
  * Compute diamond dimensions.
  * The diamond sensors are represented by the Box shape parameters.
  * oldDD: params are already in mm.
- * DD4hep: convert params from cm (DD4hep) to mm (legacy expected by PPS reco software).
+ * DD4hep: convert params from DD4hep unit to mm (legacy expected by PPS reco software).
  */
 DiamondDimensions DetGeomDesc::computeDiamondDimensions(const bool isABox,
                                                         const bool isDD4hep,
@@ -151,7 +151,7 @@ DiamondDimensions DetGeomDesc::computeDiamondDimensions(const bool isABox,
       // mm (legacy)
       boxShapeParameters = {params.at(0), params.at(1), params.at(2)};
     } else {
-      // convert cm (DD4hep) to mm (legacy expected by PPS reco software)
+      // convert from DD4hep unit to mm (legacy expected by PPS reco software)
       boxShapeParameters = {params.at(0) / dd4hep::mm, params.at(1) / dd4hep::mm, params.at(2) / dd4hep::mm};
     }
   }
