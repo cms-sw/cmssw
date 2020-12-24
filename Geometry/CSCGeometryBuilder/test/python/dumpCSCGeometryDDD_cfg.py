8d3ef40b6d1a11fbe482b329c8d5e4b3892d693f
Sergio Lo Meo <sergio.lomeo@cern.ch>
2020-12-24 09:01:09 +0100

    enable debug message in dump scripts

--- Geometry/CSCGeometryBuilder/test/python/dumpCSCGeometryDDD_cfg.py
+++ Geometry/CSCGeometryBuilder/test/python/dumpCSCGeometryDDD_cfg.py
@@ -15,6 +15,7 @@ process.load("Geometry.MuonNumbering.muonNumberingInitialization_cfi")
 process.load("Geometry.CSCGeometryBuilder.cscGeometry_cfi")
 process.load("Geometry.CSCGeometryBuilder.cscGeometryDump_cfi")
 
+
 process.CSCGeometryESModule.applyAlignment = False
 if 'MessageLogger' in process.__dict__:
     process.MessageLogger.Geometry=dict()
@@ -26,6 +27,18 @@ process.source = cms.Source('EmptySource')
 process.maxEvents = cms.untracked.PSet(
     input = cms.untracked.int32(1)
 )
+
+process.MessageLogger = cms.Service(
+        "MessageLogger",
+  destinations = cms.untracked.vstring(
+                'cout'
+        ),
+        cout = cms.untracked.PSet(
+                threshold = cms.untracked.string('DEBUG')
+        ),
+        debugModules = cms.untracked.vstring('*')
+)
+
 
 process.cscGeometryDump.verbose = True
 
