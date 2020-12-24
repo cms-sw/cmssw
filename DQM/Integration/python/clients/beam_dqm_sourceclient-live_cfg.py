0f21001718d3913e78368038221026c14c467095
Andrea Bocci <andrea.bocci@cern.ch>
2020-12-24 16:05:33 +0100

    Synchronise with CMSSW_11_3_X_2020-12-24-2300

--- DQM/Integration/python/clients/beam_dqm_sourceclient-live_cfg.py
+++ DQM/Integration/python/clients/beam_dqm_sourceclient-live_cfg.py
@@ -4,12 +4,19 @@ import FWCore.ParameterSet.Config as cms
 # Define here the BeamSpotOnline record name,
 # it will be used both in BeamMonitor setup and in payload creation/upload
 BSOnlineRecordName = 'BeamSpotOnlineLegacyObjectsRcd'
+BSOnlineTag = 'BeamSpotOnlineTestLegacy'
+BSOnlineJobName = 'BeamSpotOnlineTestLegacy'
 
 #from Configuration.Eras.Era_Run2_2018_cff import Run2_2018
 #process = cms.Process("BeamMonitor", Run2_2018) FIXME
 import sys
 from Configuration.Eras.Era_Run2_2018_pp_on_AA_cff import Run2_2018_pp_on_AA
 process = cms.Process("BeamMonitor", Run2_2018_pp_on_AA)
+
+# Configure tag and jobName if running Playback system
+if "dqm_cmssw/playback" in str(sys.argv[1]):
+    BSOnlineTag = BSOnlineTag + 'Playback'
+    BSOnlineJobName = BSOnlineJobName + 'Playback'
 
 #
 process.MessageLogger = cms.Service("MessageLogger",
@@ -27,12 +34,6 @@ unitTest = False
 if 'unitTest=True' in sys.argv:
     live=False
     unitTest=True
-
-# Switch to veto the upload of the BeamSpot conditions to the DB
-# when False it performs the upload
-noDB = True
-if 'noDB=False' in sys.argv:
-    noDB=False
 
 #---------------
 # Input sources
@@ -68,10 +69,12 @@ process.dqmEnvPixelLess.subSystemFolder = 'BeamMonitor_PixelLess'
 # Conditions
 if (live):
     process.load("DQM.Integration.config.FrontierCondition_GT_cfi")
+    process.GlobalTag.DBParameters.authenticationPath = cms.untracked.string('.')
 else:
     process.load('Configuration.StandardSequences.FrontierConditions_GlobalTag_cff')
     from Configuration.AlCa.GlobalTag import GlobalTag as gtCustomise
     process.GlobalTag = gtCustomise(process.GlobalTag, 'auto:run2_data', '')
+    process.GlobalTag.DBParameters.authenticationPath = cms.untracked.string('.')
     # you may need to set manually the GT in the line below
     #process.GlobalTag.globaltag = '100X_upgrade2018_realistic_v10'
 
@@ -352,37 +355,58 @@ process.dqmBeamMonitor.hltResults = cms.InputTag("TriggerResults","","HLT")
 
 #---------
 # Upload BeamSpotOnlineObject (LegacyRcd) to CondDB
-process.OnlineDBOutputService = cms.Service("OnlineDBOutputService",
+if unitTest == False:
+    process.OnlineDBOutputService = cms.Service("OnlineDBOutputService",
 
-    DBParameters = cms.PSet(
-                            messageLevel = cms.untracked.int32(0),
-                            authenticationPath = cms.untracked.string('.')
-                           ),
+        DBParameters = cms.PSet(
+                                messageLevel = cms.untracked.int32(0),
+                                authenticationPath = cms.untracked.string('.')
+                            ),
 
-    # Upload to CondDB
-    connect = cms.string('oracle://cms_orcoff_prep/CMS_CONDITIONS'),
-    preLoadConnectionString = cms.untracked.string('frontier://FrontierPrep/CMS_CONDITIONS'),
+        # Upload to CondDB
+        connect = cms.string('oracle://cms_orcon_prod/CMS_CONDITIONS'),
+        preLoadConnectionString = cms.untracked.string('frontier://FrontierProd/CMS_CONDITIONS'),
 
-    runNumber = cms.untracked.uint64(options.runNumber),
-    lastLumiFile = cms.untracked.string(''),
-    writeTransactionDelay = cms.untracked.uint32(options.transDelay),
-    latency = cms.untracked.uint32(2),
-    autoCommit = cms.untracked.bool(True),
-    saveLogsOnDB = cms.untracked.bool(True),
-    jobName = cms.untracked.string("BeamSpotOnlineLegacyTest"), # name of the DB log record
-    toPut = cms.VPSet(cms.PSet(
-        record = cms.string(BSOnlineRecordName),
-        tag = cms.string('BSOnlineLegacy_tag'),
-        timetype = cms.untracked.string('Lumi'),
-        onlyAppendUpdatePolicy = cms.untracked.bool(True)
-    ))
-)
+        runNumber = cms.untracked.uint64(options.runNumber),
+        #lastLumiFile = cms.untracked.string('last_lumi.txt'),
+        #lastLumiUrl = cms.untracked.string('http://ru-c2e14-11-01.cms:11100/urn:xdaq-application:lid=52/getLatestLumiSection'),
+        omsServiceUrl = cms.untracked.string('http://cmsoms-services.cms:9949/urn:xdaq-application:lid=100/getRunAndLumiSection'),
+        writeTransactionDelay = cms.untracked.uint32(options.transDelay),
+        latency = cms.untracked.uint32(2),
+        autoCommit = cms.untracked.bool(True),
+        saveLogsOnDB = cms.untracked.bool(True),
+        jobName = cms.untracked.string(BSOnlineJobName), # name of the DB log record
+        toPut = cms.VPSet(cms.PSet(
+            record = cms.string(BSOnlineRecordName),
+            tag = cms.string(BSOnlineTag),
+            timetype = cms.untracked.string('Lumi'),
+            onlyAppendUpdatePolicy = cms.untracked.bool(True)
+        ))
+    )
+else:
+    process.OnlineDBOutputService = cms.Service("OnlineDBOutputService",
 
-# If not live or noDB: produce a (local) SQLITE file
-if not live or noDB:
-    process.OnlineDBOutputService.connect = cms.string('sqlite_file:BeamSpotOnlineLegacy.db')
-    process.OnlineDBOutputService.preLoadConnectionString = cms.untracked.string('sqlite_file:BeamSpotOnlineLegacy.db')
-    process.OnlineDBOutputService.saveLogsOnDB = cms.untracked.bool(False)
+        DBParameters = cms.PSet(
+                                messageLevel = cms.untracked.int32(0),
+                                authenticationPath = cms.untracked.string('.')
+                            ),
+
+        # Upload to CondDB
+        connect = cms.string('sqlite_file:BeamSpotOnlineLegacy.db'),
+        preLoadConnectionString = cms.untracked.string('sqlite_file:BeamSpotOnlineLegacy.db'),
+        runNumber = cms.untracked.uint64(options.runNumber),
+        lastLumiFile = cms.untracked.string('last_lumi.txt'),
+        #lastLumiUrl = cms.untracked.string('http://ru-c2e14-11-01.cms:11100/urn:xdaq-application:lid=52/getLatestLumiSection'),
+        writeTransactionDelay = cms.untracked.uint32(options.transDelay),
+        latency = cms.untracked.uint32(2),
+        autoCommit = cms.untracked.bool(True),
+        toPut = cms.VPSet(cms.PSet(
+            record = cms.string(BSOnlineRecordName),
+            tag = cms.string(BSOnlineTag),
+            timetype = cms.untracked.string('Lumi'),
+            onlyAppendUpdatePolicy = cms.untracked.bool(True)
+        ))
+    )
 
 #---------
 # Final path
