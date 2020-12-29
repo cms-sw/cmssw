c39d35a918f22796c6a9700c1ac24564da372f01
maria <dalfonso@cern.ch>
2020-12-29 20:20:32 +0100

    initial version

--- RecoHGCal/TICL/python/TrkEMStep_cff.py
+++ RecoHGCal/TICL/python/TrkEMStep_cff.py
@@ -1,6 +1,6 @@
 import FWCore.ParameterSet.Config as cms
 
-from RecoHGCal.TICL.TICLSeedingRegions_cff import ticlSeedingTrk
+from RecoHGCal.TICL.TICLSeedingRegions_cff import ticlSeedingTrk, ticlSeedingTrkHFNose
 from RecoHGCal.TICL.trackstersProducer_cfi import trackstersProducer as _trackstersProducer
 from RecoHGCal.TICL.filteredLayerClustersProducer_cfi import filteredLayerClustersProducer as _filteredLayerClustersProducer
 from RecoHGCal.TICL.multiClustersFromTrackstersProducer_cfi import multiClustersFromTrackstersProducer as _multiClustersFromTrackstersProducer
@@ -48,4 +48,42 @@ ticlTrkEMStepTask = cms.Task(ticlSeedingTrk
     ,filteredLayerClustersTrkEM
     ,ticlTrackstersTrkEM
     ,ticlMultiClustersFromTrackstersTrkEM)
+
+filteredLayerClustersHFNoseTrkEM = filteredLayerClustersTrkEM.clone(
+    LayerClusters = 'hgcalLayerClustersHFNose',
+    LayerClustersInputMask = cms.InputTag("hgcalLayerClustersHFNose","InitialLayerClustersMask"),
+    #    min_cluster_size = 3, # inclusive
+    #    max_layerId = 30, # inclusive
+    algo_number = 9,
+    iteration_label = "TrkEMn"
+)
+
+ticlTrackstersHFNoseTrkEM = ticlTrackstersTrkEM.clone(
+    detector = "HFNose",
+    layer_clusters = "hgcalLayerClustersHFNose",
+    layer_clusters_hfnose_tiles = "ticlLayerTileHFNose",
+    original_mask = cms.InputTag("hgcalLayerClustersHFNose","InitialLayerClustersMask"),
+    filtered_mask = cms.InputTag("filteredLayerClustersHFNoseTrkEM", "TrkEMn"),
+    seeding_regions = "ticlSeedingTrkHFNose",
+    filter_on_categories = [0, 1],
+    pid_threshold = 0.,
+#    energy_em_over_total_threshold = 0.9,
+#    max_longitudinal_sigmaPCA = 10,
+    shower_start_max_layer = 5, #inclusive
+#    max_out_in_hops = 1,
+#    max_missing_layers_in_trackster = 2,
+#    skip_layers = 2,
+    min_layers_per_trackster = 6,
+#    min_cos_theta = 0.97,  # ~14 degrees
+#    min_cos_pointing = 0.94, # ~20 degrees
+#    root_doublet_max_distance_from_seed_squared = 2.5e-3, # dR=0.05
+#    max_delta_time = 3.,
+    itername = "TrkEMn",
+#    algo_verbosity = 0,
+    time_layerclusters = "hgcalLayerClustersHFNose:timeLayerCluster",
+)
+
+ticlHFNoseTrkEMStepTask = cms.Task(ticlSeedingTrkHFNose
+    ,filteredLayerClustersHFNoseTrkEM
+    ,ticlTrackstersHFNoseTrkEM)
 
