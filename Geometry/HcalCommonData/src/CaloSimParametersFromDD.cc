e6b77f913aeea628aa7889bf1e71b4a5a491272c
Sunanda <sunanda.banerjee@cern.ch>
2021-01-01 19:10:38 +0100

    Make the Cherenkov example working for DDD

--- Geometry/HcalCommonData/src/CaloSimParametersFromDD.cc
+++ Geometry/HcalCommonData/src/CaloSimParametersFromDD.cc
@@ -8,7 +8,7 @@
 #include <iostream>
 #include <iomanip>
 
-//#define EDM_ML_DEBUG
+#define EDM_ML_DEBUG
 
 template <typename T>
 void myPrint(std::string value, const std::vector<T>& vec) {
@@ -32,6 +32,7 @@ bool CaloSimParametersFromDD::build(const DDCompactView* cpv, CaloSimulationPara
   DDFilteredView fv(*cpv, filter);
   fv.firstChild();
   DDsvalues_type sv(fv.mergedSpecifics());
+  edm::LogVerbatim("HCalGeom") << "Filtered view " << &fv << " after filter with " << sv.size() << " contents";
 
   php.caloNames_ = getNames("Calorimeter", sv, false);
   php.levels_ = getNumbers("Levels", sv, false);
@@ -98,7 +99,7 @@ std::vector<std::string> CaloSimParametersFromDD::getNames(const std::string& st
     int nval = fvec.size();
     if ((nval < 1) && (!ignore)) {
       edm::LogError("HCalGeom") << "CaloSimParametersFromDD: # of " << str << " bins " << nval << " < 1 ==> illegal ";
-      throw cms::Exception("Unknown", "CaloSimParametersFromDD") << "nval < 2 for array " << str << "\n";
+      throw cms::Exception("Unknown", "CaloSimParametersFromDD") << "nval < 1 for array " << str << "\n";
     }
 
     return fvec;
