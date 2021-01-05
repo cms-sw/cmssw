fe8ca65b73f28de47696bc431a9bebcd3daa7dbc
Ian Tomalin <ian.tomalin@stfc.ac.uk>
2021-01-05 11:53:06 +0000

    TMTT review comments (#62)

--- L1Trigger/TrackFindingTMTT/interface/Array2D.h
+++ L1Trigger/TrackFindingTMTT/interface/Array2D.h
@@ -2,6 +2,7 @@
 #define L1Trigger_TrackFindingTMTT_Array2D_h
 
 #include <vector>
+#include <stdexcept>
 
 //=== Generic 2D array class.
 
@@ -14,20 +15,30 @@ namespace tmtt {
 
   template <typename T>
   class Array2D {
+
   public:
     //for a mxn matrix - row major
-    Array2D(unsigned int m, unsigned int n) : _n{n}, _m{m} { array2D_.resize(m * n); }
-    const T& operator()(unsigned int i, unsigned int j) const { return array2D_.at(i * _n + j); }
-    T& operator()(unsigned int i, unsigned int j) {
-      if (i >= _m || j >= _n)
-        throw std::out_of_range("matrix access out of bounds");
+    Array2D(unsigned int m, unsigned int n) : array2D_(m*n), m_{m}, n_{n} {} 
 
-      return array2D_[i * _n + j];
+    const T& operator()(unsigned int i, unsigned int j) const { 
+      checkBounds(i, j);
+      return array2D_.at(i * n_ + j); 
+    }
+
+    T& operator()(unsigned int i, unsigned int j) {
+      checkBounds(i, j);
+      return array2D_[i * n_ + j];
+    }
+
+  private:
+    void checkBounds(unsigned int i, unsigned int j) const {
+      if (i >= m_ || j >= n_)
+        throw std::out_of_range("matrix access out of bounds");
     }
 
   private:
     std::vector<T> array2D_;
-    unsigned int _n, _m;
+    unsigned int m_, n_;
   };
 }  // namespace tmtt
 
