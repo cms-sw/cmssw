ec3fe45837edd9e4bcb6db891b165ca2b7c411f0
Douja Darej <douja.darej@gmail.com>
2021-01-07 10:53:50 +0100

    DisplacedGeneralStep in one commit

--- RecoTracker/IterativeTracking/python/iterativeTkConfig.py
+++ RecoTracker/IterativeTracking/python/iterativeTkConfig.py
@@ -46,8 +46,13 @@ _iterations_trackingPhase1 = [
     "MixedTripletStep",
     "PixelLessStep",
     "TobTecStep",
-    "JetCoreRegionalStep",
 ]
+
+from Configuration.ProcessModifiers.displacedTracking_cff import displacedTracking
+displacedTracking.toModify(_iterations_trackingPhase1, func=lambda x: x.append('DisplacedGeneralStep'))
+
+_iterations_trackingPhase1.append('JetCoreRegionalStep')
+
 _iterations_trackingPhase2PU140 = [
     "InitialStep",
     "HighPtTripletStep",
