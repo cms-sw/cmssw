f1c2cddf5581c3b1a6c4b8ed7cb765b70050d786
Gabrielle Hugo <gabrielle.hugo83@gmail.com>
2021-01-07 14:23:29 +0100

    Cannot compare with namespace on one side and no namespace on the other side. In practice, for all CMSSW sens and ProdCuts XMLs and all scenarios, it is sufficient to compare with no namespace.

--- SimG4Core/Geometry/src/DD4hep_DDG4Builder.cc
+++ SimG4Core/Geometry/src/DD4hep_DDG4Builder.cc
@@ -48,7 +48,8 @@ G4VPhysicalVolume *DDG4Builder::BuildGeometry(SensitiveDetectorCatalog &catalog)
   for (auto const &it : map_) {
     for (auto const &fit : specs) {
       for (auto const &pit : fit.second->paths) {
-        if (dd4hep::dd::compareEqualName(dd4hep::dd::realTopName(pit), dd4hep::dd::noNamespace(it.first.name()))) {
+        if (dd4hep::dd::compareEqualName(dd4hep::dd::noNamespace(dd4hep::dd::realTopName(pit)),
+                                         dd4hep::dd::noNamespace(it.first.name()))) {
           dd4hepVec.emplace_back(&*it.second, &*fit.second);
         }
       }
