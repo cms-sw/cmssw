e09fb01a2cc2388a66d80bd2a9f02f22ae66bab8
Jeongeun Lee <jeongeun.lee@cern.ch>
2021-01-10 14:13:05 +0100

    drop type spec in RecoCaloTracker

--- RecoLocalTracker/Phase2TrackerRecHits/python/Phase2StripCPEGeometricESProducer_cfi.py
+++ RecoLocalTracker/Phase2TrackerRecHits/python/Phase2StripCPEGeometricESProducer_cfi.py
@@ -2,7 +2,8 @@ import FWCore.ParameterSet.Config as cms
 
 from RecoLocalTracker.Phase2TrackerRecHits.Phase2StripCPEESProducer_cfi import *
 
-phase2StripCPEGeometricESProducer = phase2StripCPEESProducer.clone()
-#phase2StripCPEGeometricESProducer.ComponentName = cms.string('Phase2StripCPEGeometric')
-phase2StripCPEGeometricESProducer.ComponentType = 'Phase2StripCPEGeometric'
-phase2StripCPEGeometricESProducer.parameters    = cms.PSet()
+phase2StripCPEGeometricESProducer = phase2StripCPEESProducer.clone(
+    #ComponentName = 'Phase2StripCPEGeometric',
+    ComponentType = 'Phase2StripCPEGeometric',
+    parameters    = cms.PSet()
+)
