4f504b75ebca1098e6d405984365684ecb3a47de
Leonardo Cristella <leonardo.cristella@cern.ch>
2021-01-12 10:51:56 +0100

    Use associator

--- SimDataFormats/Associations/python/LCToCPAssociation_cfi.py
+++ SimDataFormats/Associations/python/LCToCPAssociation_cfi.py
@@ -1,12 +1,12 @@
 import FWCore.ParameterSet.Config as cms
 
-trackingParticleRecoTrackAsssociation = cms.EDProducer("LCToCPAssociatorEDProducer",
+layerClusterCaloParticleAssociation = cms.EDProducer("LCToCPAssociatorEDProducer",
     associator = cms.InputTag('lcAssocByEnergyScoreProducer'),
     label_cp = cms.InputTag("mix","MergedCaloTruth"),
     label_lc = cms.InputTag("hgcalLayerClusters")
 )
 
 from Configuration.ProcessModifiers.premix_stage2_cff import premix_stage2
-premix_stage2.toModify(trackingParticleRecoTrackAsssociation,
+premix_stage2.toModify(layerClusterCaloParticleAssociation,
     label_cp = "mixData:MergedCaloTruth"
 )
