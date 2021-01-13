93b167922b2133aa17b75e3a737c66dad920739e
JeongEun Lee <jeongeun.lee@cern.ch>
2021-01-13 20:48:41 +0900

    remove commented out line

--- RecoLocalTracker/Phase2TrackerRecHits/python/Phase2StripCPEGeometricESProducer_cfi.py
+++ RecoLocalTracker/Phase2TrackerRecHits/python/Phase2StripCPEGeometricESProducer_cfi.py
@@ -3,7 +3,6 @@ import FWCore.ParameterSet.Config as cms
 from RecoLocalTracker.Phase2TrackerRecHits.Phase2StripCPEESProducer_cfi import *
 
 phase2StripCPEGeometricESProducer = phase2StripCPEESProducer.clone(
-    #ComponentName = 'Phase2StripCPEGeometric',
     ComponentType = 'Phase2StripCPEGeometric',
     parameters    = cms.PSet()
 )
