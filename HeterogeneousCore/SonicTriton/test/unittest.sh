1a3abbc6122375f939e007198a17cdfcb8e2d6ab
Kevin Pedro <kpedro88@gmail.com>
2021-01-13 09:40:15 -0600

    assume gat model always available

--- HeterogeneousCore/SonicTriton/test/unittest.sh
+++ HeterogeneousCore/SonicTriton/test/unittest.sh
@@ -4,21 +4,13 @@ LOCALTOP=$1
 
 # the test is not possible if:
 # 1. avx instructions not supported (needed for singularity on CPU)
-# 2. the model files cannot be found
-# 3. singularity not found or not usable
+# 2. singularity not found or not usable
 # so just return true in those cases
 
 if grep -q avx /proc/cpuinfo; then
 	echo "has avx"
 else
 	echo "missing avx"
-	exit 0
-fi
-
-if [ -n "$(find $(echo $CMSSW_SEARCH_PATH | tr ':' ' ') -path "*/HeterogeneousCore/SonicTriton/data/models/gat_test/config.pbtxt" 2>/dev/null)" ]; then
-	echo "has model file"
-else
-	echo "missing model file"
 	exit 0
 fi
 
