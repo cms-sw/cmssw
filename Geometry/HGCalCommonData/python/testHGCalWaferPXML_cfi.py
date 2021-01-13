9cc21f657806d8812dd9408a5fc1f123e88ffbf9
Sunanda <sunanda.banerjee@cern.ch>
2021-01-13 20:45:07 +0100

    Algorithm to make partial silicon modules

--- Geometry/HGCalCommonData/python/testHGCalWaferPXML_cfi.py
+++ Geometry/HGCalCommonData/python/testHGCalWaferPXML_cfi.py
@@ -3,11 +3,11 @@ import FWCore.ParameterSet.Config as cms
 XMLIdealGeometryESSource = cms.ESSource("XMLIdealGeometryESSource",
     geomXMLFiles = cms.vstring('Geometry/CMSCommonData/data/materials.xml',
         'Geometry/CMSCommonData/data/rotations.xml',
-        'Geometry/HGCalCommonData/test/cms.xml',
+        'Geometry/HGCalCommonData/data/hgcalwafer/cms.xml',
         'Geometry/HGCalCommonData/data/hgcalMaterial/v1/hgcalMaterial.xml',
-        'Geometry/HGCalCommonData/data/hgcalwafer/v15/hgcal.xml',
-        'Geometry/HGCalCommonData/data/hgcalwafer/v15/hgcalpos.xml',
-        'Geometry/HGCalCommonData/data/hgcalwafer/v15/hgcalwafer.xml',
+        'Geometry/HGCalCommonData/data/hgcalwafer/v15p/hgcal.xml',
+        'Geometry/HGCalCommonData/data/hgcalwafer/v15p/hgcalpos.xml',
+        'Geometry/HGCalCommonData/data/hgcalwafer/v15p/hgcalwafer.xml',
         'Geometry/HGCalCommonData/data/hgcalcell/v15/hgcalcell.xml'),
     rootNodeName = cms.string('cms:OCMS')
 )
