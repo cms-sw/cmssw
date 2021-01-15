abe6ecb0f00f615c5908153898210c4c06f24602
tocheng <tongguang.cheng@cern.ch>
2021-01-15 02:59:39 +0100

    Remove unnessasery header file

--- Alignment/OfflineValidation/plugins/PixelBaryCentreAnalyzer.cc
+++ Alignment/OfflineValidation/plugins/PixelBaryCentreAnalyzer.cc
@@ -17,8 +17,6 @@
 #include "FWCore/Framework/interface/one/EDAnalyzer.h"
 #include "FWCore/Framework/interface/Event.h"
 #include "FWCore/Framework/interface/MakerMacros.h"
-#include "FWCore/Framework/interface/EventSetup.h"
-#include "FWCore/Framework/interface/ESHandle.h"
 #include "FWCore/Framework/interface/ESWatcher.h"
 #include "FWCore/ParameterSet/interface/ParameterSet.h"
 #include "FWCore/Utilities/interface/ESGetToken.h"
