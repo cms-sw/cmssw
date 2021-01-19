3db7d13655ee718dd4cc80b20c0a855c1e974b5b
Vladimir <vladimir.rekovic@cern.ch>
2021-01-19 13:08:31 +0100

    emulate and cuts unconstrained pt and impact param - uGT - Rick Cavanaugh

--- L1Trigger/L1TGlobal/src/MuCondition.cc
+++ L1Trigger/L1TGlobal/src/MuCondition.cc
@@ -382,6 +382,12 @@ const bool l1t::MuCondition::checkObjectParameter(const int iCondition,
                         << "\n\t hwQual     = 0x " << cand.hwQual() << "\n\t hwIso      = 0x " << cand.hwIso()
                         << std::dec << std::endl;
 
+  if (!checkThreshold(objPar.unconstrainedPtLow, objPar.unconstrainedPtHigh, cand.hwPtUnconstrained(), m_gtMuonTemplate->condGEq())) 
+    {
+      LogDebug("L1TGlobal") << "\t\t Muon Failed unconstrainedPt checkThreshold " << std::endl;
+      return false;
+    }
+
   if (!checkThreshold(objPar.ptLowThreshold, objPar.ptHighThreshold, cand.hwPt(), m_gtMuonTemplate->condGEq())) {
     LogDebug("L1TGlobal") << "\t\t Muon Failed checkThreshold " << std::endl;
     return false;
@@ -443,6 +449,18 @@ const bool l1t::MuCondition::checkObjectParameter(const int iCondition,
   bool passIsoLUT = ((objPar.isolationLUT >> cand.hwIso()) & 1);
   if (!passIsoLUT) {
     LogDebug("L1TGlobal") << "\t\t l1t::Candidate failed isolation requirement" << std::endl;
+    return false;
+  }
+
+  // check impact parameter ( bit check ) with impact parameter LUT
+  // sanity check on candidate impact parameter
+  if (cand.hwDXY() > 3) {
+    LogDebug("L1TGlobal") << "\t\t l1t::Candidate has out of range hwDXY = " << cand.hwDXY() << std::endl;
+    return false;
+  }
+  bool passImpactParameterLUT = ((objPar.impactParameterLUT >> cand.hwDXY()) & 1);
+  if (!passImpactParameterLUT) {
+    LogDebug("L1TGlobal") << "\t\t l1t::Candidate failed impact parameter requirement" << std::endl;
     return false;
   }
 
