e21ccc163ace4c4eda6d4c8d66d58d98fb57fd57
yeckang <yechan.kang@cern.ch>
2021-01-20 23:47:53 +0100

    turn off detail plot in GEMValidation

--- Validation/MuonGEMRecHits/python/MuonGEMRecHits_cff.py
+++ Validation/MuonGEMRecHits/python/MuonGEMRecHits_cff.py
@@ -8,7 +8,6 @@ gemRecHitsValidation = DQMEDAnalyzer('GEMRecHitValidation',
     GEMValidationCommonParameters,
     gemSimHit = muonSimHitMatcherPSet.gemSimHit,
     gemRecHit = gemRecHit,
-    detailPlot = cms.bool(True),
 )
 
 gemLocalRecoValidation = cms.Sequence(gemRecHitsValidation)
