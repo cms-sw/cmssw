d8257cbe70de8034afe7f3f36bb97c27f91d2ad5
Jeongeun Lee <jeongeun.lee@cern.ch>
2021-01-20 12:58:59 +0100

    move parameter inside the clone/modifier

--- RecoEcal/EgammaClusterProducers/python/particleFlowSuperClusterECAL_cfi.py
+++ RecoEcal/EgammaClusterProducers/python/particleFlowSuperClusterECAL_cfi.py
@@ -11,9 +11,9 @@ from RecoEcal.EgammaCoreTools.EcalSCDynamicDPhiParametersESProducer_cff import *
 particleFlowSuperClusterECAL = _particleFlowSuperClusterECALMustache.clone()
 
 from Configuration.ProcessModifiers.pp_on_AA_cff import pp_on_AA
-pp_on_AA.toModify(particleFlowSuperClusterECAL, useDynamicDPhiWindow = False)
-pp_on_AA.toModify(particleFlowSuperClusterECAL, phiwidth_SuperClusterBarrel = 0.20)
-pp_on_AA.toModify(particleFlowSuperClusterECAL, phiwidth_SuperClusterEndcap = 0.20)
+pp_on_AA.toModify(particleFlowSuperClusterECAL, useDynamicDPhiWindow = False,
+                                                phiwidth_SuperClusterBarrel = 0.20,
+                                                phiwidth_SuperClusterEndcap = 0.20)
 
 from Configuration.ProcessModifiers.egamma_lowPt_exclusive_cff import egamma_lowPt_exclusive
 egamma_lowPt_exclusive.toModify(particleFlowSuperClusterECAL,
