95adf124dd1b92fa0773cd6fdbc57d3fa804df28
Jeongeun Lee <jeongeun.lee@cern.ch>
2021-01-20 12:33:43 +0100

    recheck_drop type specs in RecoTracker

--- RecoTracker/IterativeTracking/python/MixedTripletStep_cff.py
+++ RecoTracker/IterativeTracking/python/MixedTripletStep_cff.py
@@ -78,7 +78,6 @@ highBetaStar_2018.toModify(mixedTripletStepSeedLayersA,
     ]
 )
 
-
 # TrackingRegion
 from RecoTracker.TkTrackingRegions.globalTrackingRegionFromBeamSpotFixedZ_cfi import globalTrackingRegionFromBeamSpotFixedZ as _globalTrackingRegionFromBeamSpotFixedZ
 _mixedTripletStepTrackingRegionsCommon = _globalTrackingRegionFromBeamSpotFixedZ.clone(RegionPSet = dict(
