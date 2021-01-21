c58912fa31ad684ae157aae5433b1edfb8e293e0
yeckang <yechan.kang@cern.ch>
2021-01-21 11:09:59 +0100

    add layer numbers in GE0

--- Validation/MuonGEMDigis/python/PostProcessor_cff.py
+++ Validation/MuonGEMDigis/python/PostProcessor_cff.py
@@ -4,6 +4,6 @@ from DQMServices.Core.DQMEDHarvester import DQMEDHarvester
 gemDigiHarvesting = DQMEDHarvester("MuonGEMDigisHarvestor",
     regionIds = cms.untracked.vint32(-1, 1),
     stationIds = cms.untracked.vint32(1, 2),
-    layerIds = cms.untracked.vint32(1, 2),
+    layerIds = cms.untracked.vint32(1, 2, 3, 4, 5, 6),
 )
 MuonGEMDigisPostProcessors = cms.Sequence(gemDigiHarvesting)
--- Validation/MuonGEMRecHits/python/PostProcessor_cff.py
+++ Validation/MuonGEMRecHits/python/PostProcessor_cff.py
@@ -4,7 +4,7 @@ from DQMServices.Core.DQMEDHarvester import DQMEDHarvester
 gemRecHitHarvesting = DQMEDHarvester("MuonGEMRecHitsHarvestor",
     regionIds = cms.untracked.vint32(-1, 1),
     stationIds = cms.untracked.vint32(1, 2),
-    layerIds = cms.untracked.vint32(1, 2),
+    layerIds = cms.untracked.vint32(1, 2, 3, 4, 5, 6),
 )
 
 
