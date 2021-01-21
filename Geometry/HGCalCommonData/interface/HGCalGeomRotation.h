0fc26ea37b5ddff630bb6937c92523165fe279a5
Samuel Webb <samuel.webb@cern.ch>
2021-01-21 10:09:27 +0100

    Tidy up and make code more symmetric for 60 and 120 degrees

--- Geometry/HGCalCommonData/interface/HGCalGeomRotation.h
+++ Geometry/HGCalCommonData/interface/HGCalGeomRotation.h
@@ -3,27 +3,28 @@
 
 class HGCalGeomRotation {
 public:
-
   enum SectorType { Sector120Degrees, Sector60Degrees };
   enum WaferCentring { WaferCentred, CornerCentredY, CornerCentredMercedes };
 
-  HGCalGeomRotation(SectorType sectorType){_sectorType = sectorType;};
+  HGCalGeomRotation(SectorType sectorType) { _sectorType = sectorType; };
   ~HGCalGeomRotation() {}
 
   void uvMappingFromSector0(WaferCentring waferCentring, int& moduleU, int& moduleV, unsigned sector) const;
   unsigned uvMappingToSector0(WaferCentring waferCentring, int& moduleU, int& moduleV) const;
 
 private:
-
-  void RotateModule60DegreesAnticlockwise(int& moduleU, int& moduleV) const;
-  void RotateModule60DegreesClockwise(int& moduleU, int& moduleV) const;
+  void uvMappingFrom120DegreeSector0(WaferCentring waferCentring, int& moduleU, int& moduleV, unsigned sector) const;
+  void uvMappingFrom60DegreeSector0(WaferCentring waferCentring, int& moduleU, int& moduleV, unsigned sector) const;
 
   unsigned uvMappingTo120DegreeSector0(WaferCentring waferCentring, int& moduleU, int& moduleV) const;
   unsigned uvMappingTo60DegreeSector0(WaferCentring waferCentring, int& moduleU, int& moduleV) const;
 
+  void RotateModule60DegreesAnticlockwise(int& moduleU, int& moduleV) const;
+  void RotateModule60DegreesClockwise(int& moduleU, int& moduleV) const;
+  void RotateModule120DegreesAnticlockwise(int& moduleU, int& moduleV, int offset) const;
+  void RotateModule120DegreesClockwise(int& moduleU, int& moduleV, int offset) const;
+
   SectorType _sectorType;
-
-
 };
 
 #endif
