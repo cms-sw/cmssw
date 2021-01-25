03e4c1f49bd95d076d031763673ac985e8d8d0f8
Max Orok <maxwellorok@gmail.com>
2021-01-25 12:39:23 -0500

    Try writing an empty RNTuple

--- PhysicsTools/NanoAOD/plugins/NanoAODRNTupleOutputModule.cc
+++ PhysicsTools/NanoAOD/plugins/NanoAODRNTupleOutputModule.cc
@@ -12,6 +12,7 @@
 
 #include <string>
 
+#include <ROOT/RNTuple.hxx>
 #include <ROOT/RNTupleModel.hxx>
 
 #include "FWCore/Framework/interface/one/OutputModule.h"
@@ -33,33 +34,39 @@ public:
   static void fillDescriptions(edm::ConfigurationDescriptions& descriptions);
 
 private:
+  void openFile(edm::FileBlock const&) override;
+  bool isFileOpen() const override;
   void write(edm::EventForOutput const& e) override;
   void writeLuminosityBlock(edm::LuminosityBlockForOutput const&) override;
   void writeRun(edm::RunForOutput const&) override;
-  bool isFileOpen() const override;
-  void openFile(edm::FileBlock const&) override;
   void reallyCloseFile() override;
 
   std::string m_fileName;
   std::string m_logicalFileName;
   edm::JobReport::Token m_jrToken;
+
+  std::unique_ptr<ROOT::Experimental::RNTupleModel> m_model;
 };
 
 NanoAODRNTupleOutputModule::NanoAODRNTupleOutputModule(edm::ParameterSet const& pset)
     : edm::one::OutputModuleBase::OutputModuleBase(pset),
       edm::one::OutputModule<>(pset),
       m_fileName(pset.getUntrackedParameter<std::string>("fileName")),
-      m_logicalFileName(pset.getUntrackedParameter<std::string>("logicalFileName")) {}
+      m_logicalFileName(pset.getUntrackedParameter<std::string>("logicalFileName")),
+      m_model(ROOT::Experimental::RNTupleModel::Create()) {}
 
 NanoAODRNTupleOutputModule::~NanoAODRNTupleOutputModule() {}
 
-void NanoAODRNTupleOutputModule::write(edm::EventForOutput const& iEvent) {}
 void NanoAODRNTupleOutputModule::writeLuminosityBlock(edm::LuminosityBlockForOutput const& iLumi) {}
 void NanoAODRNTupleOutputModule::writeRun(edm::RunForOutput const& iRun) {}
-bool NanoAODRNTupleOutputModule::isFileOpen() const { return false; }
+
+bool NanoAODRNTupleOutputModule::isFileOpen() const {
+  return nullptr != m_model.get();
+}
 
 void NanoAODRNTupleOutputModule::openFile(edm::FileBlock const&) {
-  // TODO open RNTuple
+  // m_model->MakeField<std::vector<float>>("floats");
+
   edm::Service<edm::JobReport> jr;
   cms::Digest branchHash;
   m_jrToken = jr->outputFileOpened(m_fileName,
@@ -75,7 +82,16 @@ void NanoAODRNTupleOutputModule::openFile(edm::FileBlock const&) {
                                    std::vector<std::string>());
 }
 
+void NanoAODRNTupleOutputModule::write(edm::EventForOutput const& iEvent) {
+
+}
+
 void NanoAODRNTupleOutputModule::reallyCloseFile() {
+  {
+    auto ntuple = ROOT::Experimental::RNTupleWriter::Recreate(std::move(m_model), "ntuple", m_fileName);
+    ntuple->Fill();
+  }
+
   edm::Service<edm::JobReport> jr;
   jr->outputFileClosed(m_jrToken);
 }
@@ -85,6 +101,14 @@ void NanoAODRNTupleOutputModule::fillDescriptions(edm::ConfigurationDescriptions
 
   desc.addUntracked<std::string>("fileName");
   desc.addUntracked<std::string>("logicalFileName", "");
+
+  const std::vector<std::string> keep = {"drop *",
+                                         "keep nanoaodFlatTable_*Table_*_*",
+                                         "keep edmTriggerResults_*_*_*",
+                                         "keep String_*_genModel_*",
+                                         "keep nanoaodMergeableCounterTable_*Table_*_*",
+                                         "keep nanoaodUniqueString_nanoMetadata_*_*"};
+  edm::one::OutputModule<>::fillDescription(desc, keep);
 
   descriptions.addDefault(desc);
 }
