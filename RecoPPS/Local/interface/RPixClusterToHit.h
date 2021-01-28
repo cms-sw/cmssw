53e33c55a293d9a050986fd5e3d481efda063165
Fabrizio <Fabrizio.Ferro@ge.infn.it>
2021-01-28 12:38:56 +0100

    pps pixel topology for RECO

--- RecoPPS/Local/interface/RPixClusterToHit.h
+++ RecoPPS/Local/interface/RPixClusterToHit.h
@@ -10,19 +10,21 @@
 #include "DataFormats/Common/interface/DetSetVector.h"
 #include "DataFormats/CTPPSReco/interface/CTPPSPixelCluster.h"
 #include "DataFormats/CTPPSReco/interface/CTPPSPixelRecHit.h"
-#include "Geometry/VeryForwardGeometry/interface/CTPPSPixelSimTopology.h"
+//#include "Geometry/VeryForwardGeometry/interface/CTPPSPixelSimTopology.h"
+#include "CondFormats/PPSObjects/interface/PPSPixelTopology.h"
 
 class RPixClusterToHit {
-public:
+ public:
   RPixClusterToHit(edm::ParameterSet const &conf);
 
   void buildHits(unsigned int detId,
                  const std::vector<CTPPSPixelCluster> &clusters,
-                 std::vector<CTPPSPixelRecHit> &hits);
-  void make_hit(CTPPSPixelCluster aCluster, std::vector<CTPPSPixelRecHit> &hits);
+                 std::vector<CTPPSPixelRecHit> &hits,
+		 const PPSPixelTopology &ppt);
+  void make_hit(CTPPSPixelCluster aCluster, std::vector<CTPPSPixelRecHit> &hits, const PPSPixelTopology &ppt);
   ~RPixClusterToHit();
 
-private:
+ private:
   int verbosity_;
 };
 
