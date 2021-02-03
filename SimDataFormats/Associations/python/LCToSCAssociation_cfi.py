a2affb5be7e1e10399e95809fbabe34ab4cf889c
Leonardo Cristella <leonardo.cristella@cern.ch>
2021-02-03 21:43:09 +0100

    Reverse dependency

--- SimDataFormats/Associations/python/LCToSCAssociation_cfi.py
+++
@@ -1,8 +0,0 @@
-import FWCore.ParameterSet.Config as cms
-from Validation.HGCalValidation.HGCalValidator_cfi import hgcalValidator
-
-layerClusterSimClusterAssociation = cms.EDProducer("LCToSCAssociatorEDProducer",
-    associator = cms.InputTag('scAssocByEnergyScoreProducer'),
-    label_scl = hgcalValidator.label_scl,
-    label_lcl = hgcalValidator.label_lcl
-)
