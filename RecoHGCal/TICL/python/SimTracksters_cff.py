e57b82499089b5b5138ead063e5e68c74315ec02
Felice <felice.pantaleo@cern.ch>
2021-02-12 18:28:42 +0100

    add multiclusters from simtrackstrs

--- RecoHGCal/TICL/python/SimTracksters_cff.py
+++ RecoHGCal/TICL/python/SimTracksters_cff.py
@@ -1,6 +1,7 @@
 import FWCore.ParameterSet.Config as cms
 
 from RecoHGCal.TICL.trackstersFromSimClustersProducer_cfi import trackstersFromSimClustersProducer as _trackstersFromSimClustersProducer
+from RecoHGCal.TICL.multiClustersFromTrackstersProducer_cfi import multiClustersFromTrackstersProducer as _multiClustersFromTrackstersProducer
 
 
 # CA - PATTERN RECOGNITION
@@ -8,6 +9,9 @@ from RecoHGCal.TICL.trackstersFromSimClustersProducer_cfi import trackstersFromS
 ticlSimTracksters = _trackstersFromSimClustersProducer.clone(
 )
 
+ticlMultiClustersFromSimTracksters = _multiClustersFromTrackstersProducer.clone(
+    Tracksters = "ticlSimTracksters"
+)
 
-ticlSimTrackstersTask = cms.Task(ticlSimTracksters,)
+ticlSimTrackstersTask = cms.Task(ticlSimTracksters, ticlMultiClustersFromSimTracksters)
 
