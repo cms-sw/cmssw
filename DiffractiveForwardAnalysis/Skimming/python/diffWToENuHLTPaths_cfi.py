98c8e957a885ba7a8637999d95f63a8da5a78e17
Silvio <silvio.donato@cern.ch>
2021-02-15 12:32:31 +0100

    Cleaning of some Analysis packages

--- DiffractiveForwardAnalysis/Skimming/python/diffWToENuHLTPaths_cfi.py
+++
@@ -1,8 +0,0 @@
-import FWCore.ParameterSet.Config as cms
-
-import copy
-from HLTrigger.HLTfilters.hltHighLevel_cfi import *
-diffWToENuHLTFilter = copy.deepcopy(hltHighLevel)
-diffWToENuHLTFilter.TriggerResultsTag = cms.InputTag("TriggerResults","","HLT")
-diffWToENuHLTFilter.HLTPaths = ['HLT1Electron']
-
--- HiggsAnalysis/Skimming/python/higgsToInvisible_HLTPaths_cfi.py
+++
@@ -1,7 +0,0 @@
-import FWCore.ParameterSet.Config as cms
-
-import copy
-from HLTrigger.HLTfilters.hltHighLevel_cfi import *
-higgsToInvisibleHLTFilter = copy.deepcopy(hltHighLevel)
-higgsToInvisibleHLTFilter.HLTPaths = ['HLT_DoubleFwdJet40_MET60']
-
