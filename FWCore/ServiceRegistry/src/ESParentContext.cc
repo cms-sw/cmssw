1d7770518c5bb40903e1e7cacd2638ae1916b6a5
Chris Jones <chrisdjones15@gmail.com>
2021-02-16 16:34:27 -0600

    code formatting

--- FWCore/ServiceRegistry/src/ESParentContext.cc
+++ FWCore/ServiceRegistry/src/ESParentContext.cc
@@ -10,20 +10,26 @@ namespace edm {
 
   ESParentContext::ESParentContext() : type_(Type::kInvalid) { parent_.esmodule = nullptr; }
 
-  ESParentContext::ESParentContext(ModuleCallingContext const* module) noexcept : type_(Type::kModule) { parent_.module = module; }
+  ESParentContext::ESParentContext(ModuleCallingContext const* module) noexcept : type_(Type::kModule) {
+    parent_.module = module;
+  }
 
-  ESParentContext::ESParentContext(ESModuleCallingContext const* module) noexcept : type_(Type::kESModule) { parent_.esmodule = module; }
+  ESParentContext::ESParentContext(ESModuleCallingContext const* module) noexcept : type_(Type::kESModule) {
+    parent_.esmodule = module;
+  }
 
   ModuleCallingContext const* ESParentContext::moduleCallingContext() const {
     if (type_ != Type::kModule) {
-      throw Exception(errors::LogicError) << "ESParentContext::moduleCallingContext called for incorrect type of context";
+      throw Exception(errors::LogicError)
+          << "ESParentContext::moduleCallingContext called for incorrect type of context";
     }
     return parent_.module;
   }
 
   ESModuleCallingContext const* ESParentContext::esmoduleCallingContext() const {
     if (type_ != Type::kESModule) {
-      throw Exception(errors::LogicError) << "ESParentContext::esmoduleCallingContext called for incorrect type of context";
+      throw Exception(errors::LogicError)
+          << "ESParentContext::esmoduleCallingContext called for incorrect type of context";
     }
     return parent_.esmodule;
   }
