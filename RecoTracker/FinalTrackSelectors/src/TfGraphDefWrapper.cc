20d4948a187b778956ccbc0a4ef4d3d239b0bf1e
Joosep Pata <joosep.pata@gmail.com>
2021-02-18 13:03:47 +0200

    fix tf graph mem leak

--- RecoTracker/FinalTrackSelectors/src/TfGraphDefWrapper.cc
+++ RecoTracker/FinalTrackSelectors/src/TfGraphDefWrapper.cc
@@ -1,6 +1,12 @@
 #include "RecoTracker/FinalTrackSelectors/interface/TfGraphDefWrapper.h"
 
-TfGraphDefWrapper::TfGraphDefWrapper(tensorflow::Session* session) : session_(session) {}
+TfGraphDefWrapper::TfGraphDefWrapper(tensorflow::Session* session, tensorflow::GraphDef* graph)
+    : session_(session), graph_(graph) {}
 const tensorflow::Session* TfGraphDefWrapper::getSession() const {
   return const_cast<const tensorflow::Session*>(session_);
 }
+
+TfGraphDefWrapper::~TfGraphDefWrapper() {
+  tensorflow::closeSession(session_);
+  delete graph_;
+};
