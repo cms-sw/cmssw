1067aabe9695511599eaebf6ddd52234ebd26aad
Laurent Forthomme <laurent.forthomme@cern.ch>
2021-02-23 13:20:38 +0100

    Copying instead of cloning

--- Geometry/VeryForwardGeometry/python/commons_cff.py
+++ Geometry/VeryForwardGeometry/python/commons_cff.py
@@ -1,12 +1,13 @@
 import FWCore.ParameterSet.Config as cms
 from importlib import import_module
+from copy import copy
 
 def cloneGeometry(mod_path):
     _geom = import_module(mod_path)
-    totemGeomXMLFiles = _geom.totemGeomXMLFiles.clone()
-    ctppsDiamondGeomXMLFiles = _geom.ctppsDiamondGeomXMLFiles.clone()
-    ctppsUFSDGeomXMLFiles = _geom.ctppsUFSDGeomXMLFiles.clone()
-    ctppsPixelGeomXMLFiles = _geom.ctppsPixelGeomXMLFiles.clone()
+    totemGeomXMLFiles = copy(_geom.totemGeomXMLFiles)
+    ctppsDiamondGeomXMLFiles = copy(_geom.ctppsDiamondGeomXMLFiles)
+    ctppsUFSDGeomXMLFiles = copy(_geom.ctppsUFSDGeomXMLFiles)
+    ctppsPixelGeomXMLFiles = copy(_geom.ctppsPixelGeomXMLFiles)
     XMLIdealGeometryESSource_CTPPS = _geom.XMLIdealGeometryESSource_CTPPS.clone(
         geomXMLFiles = totemGeomXMLFiles + ctppsDiamondGeomXMLFiles + ctppsUFSDGeomXMLFiles + ctppsPixelGeomXMLFiles
     )
