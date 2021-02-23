bc5ee2d0b86ac7d276f0d11a355d86b65fd9c09e
Richard <Richard.J.Cavanaugh@gmail.com>
2021-02-23 09:05:21 -0600

    Bugfix to UPT and DXY cuts following February MWGR

--- L1Trigger/L1TGlobal/src/MuCondition.cc
+++ L1Trigger/L1TGlobal/src/MuCondition.cc
@@ -382,15 +382,32 @@ const bool l1t::MuCondition::checkObjectParameter(const int iCondition,
                         << "\n\t hwQual     = 0x " << cand.hwQual() << "\n\t hwIso      = 0x " << cand.hwIso()
                         << std::dec << std::endl;
 
-  if (objPar.unconstrainedPtHigh > 0)  // Check if unconstrained pT cut-window is valid
-  {
-    if (!checkUnconstrainedPt(objPar.unconstrainedPtLow,
-                              objPar.unconstrainedPtHigh,
-                              cand.hwPtUnconstrained(),
-                              m_gtMuonTemplate->condGEq())) {
-      LogDebug("L1TGlobal") << "\t\t Muon Failed unconstrainedPt checkThreshold; iCondition = " << iCondition
-                            << std::endl;
-      return false;
+  if( objPar.unconstrainedPtHigh > 0 )  // Rick Cavanaugh:  Check if unconstrained pT cut-window is valid
+    {
+      if( !checkUnconstrainedPt(objPar.unconstrainedPtLow,
+				objPar.unconstrainedPtHigh,
+				cand.hwPtUnconstrained(),
+				m_gtMuonTemplate->condGEq()) ) 
+	{
+	  LogDebug("L1TGlobal") << "\t\t Muon Failed unconstrainedPt checkThreshold; iCondition = " << iCondition
+				<< std::endl;
+	  return false;
+	}
+    }
+  if( objPar.impactParameterLUT != 0 ) // Rick Cavanaugh:  Check if impact parameter LUT is valid.  0xF is default; 0x0 is invalid
+    { 
+      // check impact parameter ( bit check ) with impact parameter LUT
+      // sanity check on candidate impact parameter
+      if (cand.hwDXY() > 3) {
+	LogDebug("L1TGlobal") << "\t\t l1t::Candidate has out of range hwDXY = " << cand.hwDXY() << std::endl;
+	return false;
+      }
+      bool passImpactParameterLUT = ((objPar.impactParameterLUT >> cand.hwDXY()) & 1);
+      if (!passImpactParameterLUT) 
+	{
+	  LogDebug("L1TGlobal") << "\t\t l1t::Candidate failed impact parameter requirement" << std::endl;
+	  return false;
+	}
     }
   }
 
