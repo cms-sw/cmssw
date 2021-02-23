f316363d4a31badd620ea61eca5e5f65801fe503
Felice <felice.pantaleo@cern.ch>
2021-02-23 15:22:13 +0100

    make enabling simclusters validation plots

--- RecoHGCal/TICL/python/SimTracksters_cff.py
+++ RecoHGCal/TICL/python/SimTracksters_cff.py
@@ -2,16 +2,26 @@ import FWCore.ParameterSet.Config as cms
 
 from RecoHGCal.TICL.trackstersFromSimClustersProducer_cfi import trackstersFromSimClustersProducer as _trackstersFromSimClustersProducer
 from RecoHGCal.TICL.multiClustersFromTrackstersProducer_cfi import multiClustersFromTrackstersProducer as _multiClustersFromTrackstersProducer
+from RecoHGCal.TICL.filteredLayerClustersProducer_cfi import filteredLayerClustersProducer as _filteredLayerClustersProducer
 
 
 # CA - PATTERN RECOGNITION
 
+
+filteredLayerClustersSimTracksters = _filteredLayerClustersProducer.clone(
+    clusterFilter = "ClusterFilterByAlgoAndSize",
+    algo_number = 8,
+    min_cluster_size = 0, # inclusive
+    iteration_label = "ticlSimTracksters"
+)
+
 ticlSimTracksters = _trackstersFromSimClustersProducer.clone(
+    # filtered_mask = cms.InputTag("filteredLayerClustersSimTracksters", "ticlSimTracksters"),
 )
 
 ticlMultiClustersFromSimTracksters = _multiClustersFromTrackstersProducer.clone(
     Tracksters = "ticlSimTracksters"
 )
 
-ticlSimTrackstersTask = cms.Task(ticlSimTracksters, ticlMultiClustersFromSimTracksters)
+ticlSimTrackstersTask = cms.Task(filteredLayerClustersSimTracksters, ticlSimTracksters, ticlMultiClustersFromSimTracksters)
 
