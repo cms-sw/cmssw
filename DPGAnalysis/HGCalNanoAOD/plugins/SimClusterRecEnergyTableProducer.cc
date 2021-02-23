b4d99ee0f68e81628000efdff4550adb6594129b
Kenneth Long <kdlong@hep.wisc.edu>
2021-02-24 00:05:53 +0100

    Restore missing includes

--- DPGAnalysis/HGCalNanoAOD/plugins/SimClusterRecEnergyTableProducer.cc
+++ DPGAnalysis/HGCalNanoAOD/plugins/SimClusterRecEnergyTableProducer.cc
@@ -1,4 +1,6 @@
 #include "PhysicsTools/NanoAOD/interface/ObjectPropertyFromIndexMapTableProducer.h"
+#include "SimDataFormats/CaloAnalysis/interface/SimCluster.h"
+#include "SimDataFormats/CaloAnalysis/interface/SimClusterFwd.h"
 #include "FWCore/Framework/interface/MakerMacros.h"
 
 typedef ObjectPropertyFromIndexMapTableProducer<SimClusterCollection, float> SimClusterRecEnergyTableProducer;
