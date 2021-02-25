b48c908af4f4bcf60f2ff14ae2b6d2b56b51f478
Giovanni <gpetruc@gmail.com>
2021-02-25 15:10:57 +0100

    Fix HGCal sector definition
    (cherry picked from commit 648a0a47dd9bbf7d021ff8fea03e7f991564c3a4)

--- L1Trigger/Phase2L1ParticleFlow/python/l1ctLayer1_cff.py
+++ L1Trigger/Phase2L1ParticleFlow/python/l1ctLayer1_cff.py
@@ -90,6 +90,17 @@ l1ctLayer1Barrel = cms.EDProducer("L1TCorrelatorLayer1Producer",
 
 )
 
+_hgcalSectors = cms.VPSet(
+    cms.PSet( 
+        etaBoundaries = cms.vdouble(-3.0, -1.5),
+        phiSlices     = cms.uint32(3)
+    ),
+    cms.PSet( 
+        etaBoundaries = cms.vdouble(+1.5, +3.0),
+        phiSlices     = cms.uint32(3)
+    )
+
+)
 
 l1ctLayer1HGCal = cms.EDProducer("L1TCorrelatorLayer1Producer",
     tracks = cms.InputTag('pfTracksFromL1Tracks'),
@@ -159,16 +170,7 @@ l1ctLayer1HGCal = cms.EDProducer("L1TCorrelatorLayer1Producer",
         trkQualityPtMin=cms.double(10.0),
     ),
     writeEGSta = cms.bool(True),    
-    caloSectors = cms.VPSet(
-        cms.PSet( 
-            etaBoundaries = cms.vdouble(-2.5, -1.5),
-            phiSlices     = cms.uint32(3)
-        ),
-        cms.PSet( 
-            etaBoundaries = cms.vdouble(+1.5, +2.5),
-            phiSlices     = cms.uint32(3)
-        )
-    ),
+    caloSectors = _hgcalSectors,
     regions = cms.VPSet(
         cms.PSet( 
             etaBoundaries = cms.vdouble(-2.5, -1.5),
@@ -244,16 +246,7 @@ l1ctLayer1HGCalNoTK = cms.EDProducer("L1TCorrelatorLayer1Producer",
         trkQualityPtMin=cms.double(10.0),
     ),
     writeEGSta = cms.bool(True),    
-    caloSectors = cms.VPSet(
-        cms.PSet( 
-            etaBoundaries = cms.vdouble(-3.0, -2.5),
-            phiSlices     = cms.uint32(3)
-        ),
-        cms.PSet( 
-            etaBoundaries = cms.vdouble(+2.5, +3.0),
-            phiSlices     = cms.uint32(3)
-        )
-    ),
+    caloSectors = _hgcalSectors,
     regions = cms.VPSet(
         cms.PSet( 
             etaBoundaries = cms.vdouble(-3.0, -2.5),
