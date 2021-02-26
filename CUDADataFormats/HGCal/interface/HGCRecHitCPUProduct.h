ba8c7ff52f1e31a9300a6398f7e18074cc08f3b0
Bruno Alves <balves16@hotmail.com>
2021-02-26 19:13:36 +0100

    produce HGCRecHitSoAs on the fly

--- CUDADataFormats/HGCal/interface/HGCRecHitCPUProduct.h
+++ CUDADataFormats/HGCal/interface/HGCRecHitCPUProduct.h
@@ -12,7 +12,17 @@
 class HGCRecHitCPUProduct {
 public:
   HGCRecHitCPUProduct() = default;
-  explicit HGCRecHitCPUProduct(uint32_t nhits, const cudaStream_t &stream);
+  HGCRecHitCPUProduct(uint32_t nhits, const cudaStream_t& stream) : nhits_(nhits) {
+    constexpr std::array<int,memory::npointers::ntypes_hgcrechits_soa> sizes =
+      {{ memory::npointers::float_hgcrechits_soa * sizeof(float),
+	 memory::npointers::uint32_hgcrechits_soa * sizeof(uint32_t),
+	 memory::npointers::uint8_hgcrechits_soa * sizeof(uint8_t) }};
+    size_tot_ = std::accumulate(sizes.begin(), sizes.end(), 0);
+    pad_ = ((nhits - 1) / 32 + 1) * 32;  //align to warp boundary (assumption: warpSize = 32)
+    //mem_ = cms::cuda::make_host_unique<std::byte[]>(pad_ * size_tot_, stream);
+    mem_ = std::make_unique<std::byte[]>(pad_ * size_tot_);
+  }
+  //explicit HGCRecHitCPUProduct(uint32_t nhits, const cudaStream_t &stream);
   ~HGCRecHitCPUProduct() = default;
 
   HGCRecHitCPUProduct(const HGCRecHitCPUProduct &) = delete;
@@ -20,9 +30,19 @@ public:
   HGCRecHitCPUProduct(HGCRecHitCPUProduct &&) = default;
   HGCRecHitCPUProduct &operator=(HGCRecHitCPUProduct &&) = default;
 
-  void defineSoAMemoryLayout_();
-  void copySoAMemoryLayoutToConst_();
-  HGCRecHitSoA get() { return soa_; }
+  HGCRecHitSoA get() {
+    HGCRecHitSoA soa;
+    soa.energy_    = reinterpret_cast<float*>(mem_.get());
+    soa.time_      = soa.energy_ + pad_;
+    soa.timeError_ = soa.time_ + pad_;
+    soa.id_        = reinterpret_cast<uint32_t*>(soa.timeError_ + pad_);
+    soa.flagBits_  = soa.id_ + pad_;
+    soa.son_       = reinterpret_cast<uint8_t*>(soa.flagBits_ + pad_);
+    soa.nbytes_    = size_tot_;
+    soa.nhits_     = nhits_;
+    soa.pad_       = pad_;
+    return soa;
+  }
 
   ConstHGCRecHitSoA get() const {
     ConstHGCRecHitSoA soa;
@@ -41,7 +61,6 @@ public:
 private:
   //cms::cuda::host::unique_ptr<std::byte[]> mem_;
   std::unique_ptr<std::byte[]> mem_;
-  HGCRecHitSoA soa_;
   uint32_t pad_;
   uint32_t nhits_;
   uint32_t size_tot_;
