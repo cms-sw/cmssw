6dbc573c87d4adf7b62ec5ee3d48640ceb41e759
Ernesta <ernesta.petraityte@yahoo.com>
2021-02-26 12:12:05 +0100

    front-end update

--- DQMServices/DQMGUI/frontend/_next/static/chunks/webpack.js
+++ DQMServices/DQMGUI/frontend/_next/static/chunks/webpack.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "cd77f75f3023cc90d12d";
+/******/ 	var hotCurrentHash = "8f0351c60b1f295ca460";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
