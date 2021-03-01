cf2976b38f5a5359cdca125fbc0a8d0f78052157
Thomas Reis <thomas.reis@cern.ch>
2021-03-01 13:26:40 +0100

    Move EcalIntercalibConstantsGPU from RecoLocalCalo/EcalRecAlgos to CondFormats/EcalObjects.

--- CondFormats/EcalObjects/interface/EcalIntercalibConstantsGPU.h
+++ CondFormats/EcalObjects/interface/EcalIntercalibConstantsGPU.h
@@ -1,5 +1,5 @@
-#ifndef RecoLocalCalo_EcalRecAlgos_interface_EcalIntercalibConstantsGPU_h
-#define RecoLocalCalo_EcalRecAlgos_interface_EcalIntercalibConstantsGPU_h
+#ifndef CondFormats_EcalObjects_interface_EcalIntercalibConstantsGPU_h
+#define CondFormats_EcalObjects_interface_EcalIntercalibConstantsGPU_h
 
 #include "CondFormats/EcalObjects/interface/EcalIntercalibConstants.h"
 
@@ -40,4 +40,4 @@ private:
 #endif  // __CUDACC__
 };
 
-#endif  // RecoLocalCalo_EcalRecAlgos_interface_EcalIntercalibConstantsGPU_h
+#endif  // CondFormats_EcalObjects_interface_EcalIntercalibConstantsGPU_h
