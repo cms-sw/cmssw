c1b9f5f01aed8cfabacc6c0d73ded71df1820108
Laurent Forthomme <laurent.forthomme@cern.ch>
2021-03-01 19:02:19 +0100

    DF+Reco: Added a few bits to the reco algorithm logic, TotemT2RecHitProducerAlgorithm now inherits from timing base one

---
+++ RecoPPS/Local/src/TotemT2RecHitProducerAlgorithm.cc
@@ -0,0 +1,35 @@
+/****************************************************************************
+*
+* This is a part of PPS offline software.
+* Authors:
+*   Laurent Forthomme (laurent.forthomme@cern.ch)
+*
+****************************************************************************/
+
+#include "RecoPPS/Local/interface/TotemT2RecHitProducerAlgorithm.h"
+
+#include "DataFormats/CTPPSDetId/interface/TotemT2DetId.h"
+
+void TotemT2RecHitProducerAlgorithm::build(const CTPPSGeometry& geom,
+                                           const edm::DetSetVector<TotemT2Digi>& input,
+                                           edm::DetSetVector<TotemT2RecHit>& output) {
+  for (const auto& vec : input) {
+    const TotemT2DetId detid(vec.detId());
+    const int sector = detid.arm(), plane = detid.plane(), channel = detid.channel();
+
+    // retrieve the timing calibration part for this channel
+    const auto& ch_params = (apply_calib_) ? calib_.parameters(sector, 0, plane, channel) : std::vector<double>{};
+    // default values for offset + time precision if calibration object not found
+    const double ch_t_offset = (apply_calib_) ? calib_.timeOffset(sector, 0, plane, channel) : 0.;
+    const double ch_t_precis = (apply_calib_) ? calib_.timePrecision(sector, 0, plane, channel) : 0.;
+
+    // prepare the output collection
+    edm::DetSet<TotemT2RecHit>& rec_hits = output.find_or_insert(detid);
+
+    // retrieve the geometry element associated to this DetID
+    const DetGeomDesc* det = geom.sensor(detid);
+
+    const float x_pos = det->translation().x(), y_pos = det->translation().y();
+    float z_pos = det->parentZPosition();  // retrieve the plane position;
+  }
+}
