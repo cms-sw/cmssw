c020313af106d52986119e7e2e62a7eba91421c0
Gianluca <gianluca.cerminara@cern.ch>
2021-03-02 16:03:48 +0100

    code-format
    (cherry picked from commit ced3a0ee52aef357035d77f987d78e7aaabecf8b)

--- L1Trigger/Phase2L1ParticleFlow/plugins/L1TEGMultiMerger.cc
+++ L1Trigger/Phase2L1ParticleFlow/plugins/L1TEGMultiMerger.cc
@@ -20,35 +20,35 @@ public:
 private:
   void produce(edm::StreamID, edm::Event&, const edm::EventSetup&) const override;
 
-  
   template <class T>
   class InstanceMerger {
   public:
-    InstanceMerger(L1TEGMultiMerger *prod, const edm::ParameterSet& conf) : instanceLabel_(conf.getParameter<std::string>("instance")) {
-      for (const auto& producer_tag: conf.getParameter<std::vector<edm::InputTag>>("pfProducers")) {
+    InstanceMerger(L1TEGMultiMerger* prod, const edm::ParameterSet& conf)
+        : instanceLabel_(conf.getParameter<std::string>("instance")) {
+      for (const auto& producer_tag : conf.getParameter<std::vector<edm::InputTag>>("pfProducers")) {
         tokens_.push_back(
             prod->consumes<T>(edm::InputTag(producer_tag.label(), producer_tag.instance(), producer_tag.process())));
       }
-      prod->produces<T>(instanceLabel_);      
-    }    
-    
+      prod->produces<T>(instanceLabel_);
+    }
+
     void produce(edm::Event& iEvent) const {
       edm::Handle<T> handle;
       auto out = std::make_unique<T>();
-      for(const auto& token: tokens_) {
+      for (const auto& token : tokens_) {
         iEvent.getByToken(token, handle);
         populate(out, *handle);
       }
       iEvent.put(std::move(out), instanceLabel_);
     }
+
   private:
-    
-    template<class TT>
-    void populate(std::unique_ptr<TT> &out, const TT& in) const {
+    template <class TT>
+    void populate(std::unique_ptr<TT>& out, const TT& in) const {
       out->insert(out->end(), in.begin(), in.end());
     }
 
-    void populate(std::unique_ptr<BXVector<l1t::EGamma>> &out, const BXVector<l1t::EGamma>& in) const {
+    void populate(std::unique_ptr<BXVector<l1t::EGamma>>& out, const BXVector<l1t::EGamma>& in) const {
       for (int bx = in.getFirstBX(); bx <= in.getLastBX(); bx++) {
         for (auto egee_itr = in.begin(bx); egee_itr != in.end(bx); egee_itr++) {
           out->push_back(bx, *egee_itr);
@@ -56,28 +56,23 @@ private:
       }
     }
 
-    
     std::vector<edm::EDGetTokenT<T>> tokens_;
     std::string instanceLabel_;
   };
-  
 
   std::vector<InstanceMerger<l1t::TkElectronCollection>> tkEleMerger;
   std::vector<InstanceMerger<l1t::TkEmCollection>> tkEmMerger;
   std::vector<InstanceMerger<BXVector<l1t::EGamma>>> tkEGMerger;
-
 };
 
-
-
 L1TEGMultiMerger::L1TEGMultiMerger(const edm::ParameterSet& conf) {
-  for (const auto& config: conf.getParameter<std::vector<edm::ParameterSet>>("tkElectrons")) {
+  for (const auto& config : conf.getParameter<std::vector<edm::ParameterSet>>("tkElectrons")) {
     tkEleMerger.push_back(InstanceMerger<l1t::TkElectronCollection>(this, config));
   }
-  for (const auto& config: conf.getParameter<std::vector<edm::ParameterSet>>("tkEms")) {
+  for (const auto& config : conf.getParameter<std::vector<edm::ParameterSet>>("tkEms")) {
     tkEmMerger.push_back(InstanceMerger<l1t::TkEmCollection>(this, config));
   }
-  for (const auto& config: conf.getParameter<std::vector<edm::ParameterSet>>("tkEgs")) {
+  for (const auto& config : conf.getParameter<std::vector<edm::ParameterSet>>("tkEgs")) {
     tkEGMerger.push_back(InstanceMerger<BXVector<l1t::EGamma>>(this, config));
   }
 }
@@ -85,11 +80,11 @@ L1TEGMultiMerger::L1TEGMultiMerger(const edm::ParameterSet& conf) {
 L1TEGMultiMerger::~L1TEGMultiMerger() {}
 
 void L1TEGMultiMerger::produce(edm::StreamID, edm::Event& iEvent, const edm::EventSetup&) const {
-  for (const auto& eleMerger: tkEleMerger)
+  for (const auto& eleMerger : tkEleMerger)
     eleMerger.produce(iEvent);
-  for (const auto& emMerger: tkEmMerger)
+  for (const auto& emMerger : tkEmMerger)
     emMerger.produce(iEvent);
-  for (const auto& egMerger: tkEGMerger)
+  for (const auto& egMerger : tkEGMerger)
     egMerger.produce(iEvent);
 }
 
