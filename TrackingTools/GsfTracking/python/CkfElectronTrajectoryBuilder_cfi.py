c7ddc908f4d110b853c9f54f7efd4ab28bd38517
Jeongeun Lee <jeongeun.lee@cern.ch>
2021-03-03 09:47:10 +0100

    minor fix delete copy

--- TrackingTools/GsfTracking/python/CkfElectronTrajectoryBuilder_cfi.py
+++ TrackingTools/GsfTracking/python/CkfElectronTrajectoryBuilder_cfi.py
@@ -1,6 +1,5 @@
 import FWCore.ParameterSet.Config as cms
 
-#import copy
 from RecoTracker.CkfPattern.CkfTrajectoryBuilder_cfi import *
 CkfElectronTrajectoryBuilder = CkfTrajectoryBuilder.clone(
     propagatorAlong = 'fwdElectronPropagator',
