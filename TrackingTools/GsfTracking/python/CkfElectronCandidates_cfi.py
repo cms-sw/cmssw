63415df5f759241633fddf29227afe1decafce8d
Jeongeun Lee <jeongeun.lee@cern.ch>
2021-03-03 09:47:10 +0100

    minor fix delete copy

--- TrackingTools/GsfTracking/python/CkfElectronCandidates_cfi.py
+++ TrackingTools/GsfTracking/python/CkfElectronCandidates_cfi.py
@@ -1,6 +1,5 @@
 import FWCore.ParameterSet.Config as cms
 
-#import copy
 from RecoTracker.CkfPattern.CkfTrackCandidates_cfi import *
 CkfElectronCandidates = ckfTrackCandidates.clone(
     TrajectoryBuilderPSet = dict(
