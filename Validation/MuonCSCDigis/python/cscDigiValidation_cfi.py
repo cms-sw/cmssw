6f3240d3f62ed6c0a44512cf02886b60d73c7db6
Sven Dildick <sven.dildick@cern.ch>
2021-03-06 00:59:07 -0600

    TP efficiencies and validation cleanup

--- Validation/MuonCSCDigis/python/cscDigiValidation_cfi.py
+++ Validation/MuonCSCDigis/python/cscDigiValidation_cfi.py
@@ -1,15 +1,22 @@
 import FWCore.ParameterSet.Config as cms
+from Validation.MuonHits.muonSimHitMatcherPSet import *
+from Validation.MuonGEMDigis.muonGEMDigiPSet import *
+from Validation.MuonCSCDigis.muonCSCDigiPSet import *
+from Validation.MuonCSCDigis.muonCSCStubPSet import *
 
 from DQMServices.Core.DQMEDAnalyzer import DQMEDAnalyzer
-cscDigiValidation = DQMEDAnalyzer('CSCDigiValidation',
+cscDigiValidation = DQMEDAnalyzer(
+    'CSCDigiValidation',
+    muonSimHitMatcherPSet,
+    muonGEMDigiPSet,
+    muonCSCDigiPSet,
+    muonCSCStubPSet,
     simHitsTag = cms.InputTag("mix", "g4SimHitsMuonCSCHits"),
-    wireDigiTag = cms.InputTag("simMuonCSCDigis","MuonCSCWireDigi"),
     outputFile = cms.string(''),
-    stripDigiTag = cms.InputTag("simMuonCSCDigis","MuonCSCStripDigi"),
-    comparatorDigiTag = cms.InputTag("simMuonCSCDigis","MuonCSCComparatorDigi"),
-    alctDigiTag = cms.InputTag("simCscTriggerPrimitiveDigis"),
-    clctDigiTag = cms.InputTag("simCscTriggerPrimitiveDigis"),
-    doSim = cms.bool(False)
+    doSim = cms.bool(True),
+    ## numbering follows the chamberType in CSCDetId
+    etaMins = cms.vdouble(2.0, 1.6, 1.2, 0.9, 1.6, 1.0, 1.7, 1.1, 1.8, 1.2),
+    etaMaxs = cms.vdouble(2.4, 2.2, 1.7, 1.1, 2.4, 1.6, 2.4, 1.7, 2.4, 1.8)
 )
 
 from Configuration.Eras.Modifier_fastSim_cff import fastSim
