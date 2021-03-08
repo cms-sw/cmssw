afb4bd3b01a9ea09767bd92ae98c485c5505d2b4
Seo Hyeon An <klar.wind0425@gmail.com>
2021-03-08 08:45:29 +0100

    RPCDQM DCSInfo resize nLS checking bin contents

--- DQM/RPCMonitorClient/interface/RPCDcsInfoClient.h
+++ DQM/RPCMonitorClient/interface/RPCDcsInfoClient.h
@@ -8,18 +8,16 @@
 class RPCDcsInfoClient : public DQMEDHarvester {
 public:
   RPCDcsInfoClient(const edm::ParameterSet &ps);
-  ~RPCDcsInfoClient() override;
+  ~RPCDcsInfoClient() override = default;
 
 protected:
-  void beginJob() override;
   void dqmEndJob(DQMStore::IBooker &, DQMStore::IGetter &) override;  //performed in the endJob
 
 private:
-  std::string dcsinfofolder_;
-  std::string eventinfofolder_;
-  std::string dqmprovinfofolder_;
+  const std::string dcsinfofolder_;
+  const std::string eventinfofolder_;
+  const std::string dqmprovinfofolder_;
 
-  std::vector<int> DCS;
 };
 
 #endif
