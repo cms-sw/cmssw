7befc4e005267df5c1565b39687f58631b6a3ca0
Erica Brondolin <erica.brondolin@cern.ch>
2021-03-09 11:26:00 +0100

    Better printout

--- RecoHGCal/TICL/test/ticlDebugger_cfg.py
+++ RecoHGCal/TICL/test/ticlDebugger_cfg.py
@@ -19,8 +19,6 @@ process.source = cms.Source("PoolSource",
 
 process.load("RecoHGCal.TICL.ticlDebugger_cfi")
 process.load("SimGeneral.Debugging.caloParticleDebugger_cfi")
-process.caloParticleDebugger.printTrackingP = True
-process.caloParticleDebugger.printSimTracks = True
 
 process.p = cms.Path(process.ticlDebugger+process.caloParticleDebugger)
 
