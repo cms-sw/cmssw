bc079d168144ced6884bd8c06030591bd87723e2
Patrick Gartung <gartung@fnal.gov>
2021-03-09 14:57:57 -0600

    Utilities/StaticAnalyzers: group callstacks in EventSetupRecord::get() report by package directory and producer

--- Utilities/StaticAnalyzers/scripts/callgraph.py
+++ Utilities/StaticAnalyzers/scripts/callgraph.py
@@ -1,6 +1,8 @@
 #! /usr/bin/env python
 from __future__ import print_function
 import re
+from collections import defaultdict
+
 topfunc = re.compile(r"::(accumulate|acquire|startingNewLoop|duringLoop|endOfLoop|beginOfJob|endOfJob|produce|analyze|filter|beginLuminosityBlock|beginRun|beginStream|streamBeginRun|streamBeginLuminosityBlock|streamEndRun|streamEndLuminosityBlock|globalBeginRun|globalEndRun|globalBeginLuminosityBlock|globalEndLuminosityBlock|endRun|endLuminosityBlock)\(")
 
 baseclass = re.compile(r"edm::(one::|stream::|global::)?(ED(Producer|Filter|Analyzer|(IterateNTimes|NavigateEvents)?Looper)(Base)?|impl::(ExternalWork|Accumulator))")
@@ -13,6 +15,15 @@ epfuncs=set()
 
 import networkx as nx
 G=nx.DiGraph()
+
+g = open('module_to_package.txt')
+
+module2package=defaultdict(list)
+for line in g:
+    fields = line.strip().split(';')
+    if len(fields) <2:
+        continue
+    module2package[fields[1]].append(fields[0])
 
 f = open('function-calls-db.txt')
 
@@ -36,13 +47,13 @@ for line in f :
 				if epfunc.search(fields[1]) : epfuncs.add(fields[1])
 f.close()
 
+    
+
 #for epfunc in sorted(epfuncs): print(epfunc)
-print("Callstacks for top level functions calling EventSetupRecord::get<>()")
-print()
 
 callstacks=set()
-for tfunc in sorted(toplevelfuncs):
-        for epfunc in sorted(epfuncs):
+for tfunc in toplevelfuncs:
+        for epfunc in epfuncs:
 		if G.has_node(tfunc) and G.has_node(epfunc) and nx.has_path(G,tfunc,epfunc) : 
 			path = nx.shortest_path(G,tfunc,epfunc)
 			cs=""
@@ -55,4 +66,14 @@ for tfunc in sorted(toplevelfuncs):
                         callstacks.add(cs)
                         break
 
-for cs in sorted(callstacks): print(cs)
+print("Callstacks for top level functions calling EventSetupRecord::get<>() sorted by package directory and producer name")
+print()
+
+for key in sorted(module2package.keys()):
+   print("package directory %s\n" % key)
+   for value in sorted(module2package[key]):
+      print("\tproducer name %s\n" % value)
+      vre=re.compile(value)
+      for cs in sorted(callstacks):
+           if vre.search(cs):
+               print("\t\tcall stack %s\n" % cs)
