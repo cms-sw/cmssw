53a3d97163bebbfbd3e9d08c4855c4965dcf92ac
Matti Kortelainen <matti.kortelainen@cern.ch>
2021-03-08 17:56:13 -0800

    Add support for cluster charge cut per iteration

--- RecoTracker/MkFit/plugins/MkFitHitConverter.cc
+++ RecoTracker/MkFit/plugins/MkFitHitConverter.cc
@@ -5,6 +5,7 @@
 #include "FWCore/ParameterSet/interface/ParameterSet.h"
 #include "FWCore/Utilities/interface/Likely.h"
 
+#include "DataFormats/Provenance/interface/ProductID.h"
 #include "DataFormats/SiStripCluster/interface/SiStripClusterTools.h"
 #include "DataFormats/TrajectorySeed/interface/TrajectorySeed.h"
 #include "DataFormats/TrackerRecHit2D/interface/SiPixelRecHitCollection.h"
@@ -45,13 +46,14 @@ private:
   void produce(edm::StreamID, edm::Event& iEvent, const edm::EventSetup& iSetup) const override;
 
   template <typename HitCollection>
-  void convertHits(const HitCollection& hits,
-                   mkfit::EventOfHits& mkFitEventOfHits,
-                   mkfit::HitVec& mkFitHits,
-                   std::vector<TrackingRecHit const*>& clusterIndexToHit,
-                   const TrackerTopology& ttopo,
-                   const TransientTrackingRecHitBuilder& ttrhBuilder,
-                   const MkFitGeometry& mkFitGeom) const;
+  edm::ProductID convertHits(const HitCollection& hits,
+                             mkfit::EventOfHits& mkFitEventOfHits,
+                             mkfit::HitVec& mkFitHits,
+                             std::vector<TrackingRecHit const*>& clusterIndexToHit,
+                             std::vector<float>& clusterChargeVec,
+                             const TrackerTopology& ttopo,
+                             const TransientTrackingRecHitBuilder& ttrhBuilder,
+                             const MkFitGeometry& mkFitGeom) const;
 
   float clusterCharge(const SiStripRecHit2D& hit, DetId hitId) const;
   std::nullptr_t clusterCharge(const SiPixelRecHit& hit, DetId hitId) const;
@@ -118,12 +120,33 @@ void MkFitHitConverter::produce(edm::StreamID iID, edm::Event& iEvent, const edm
 
   MkFitClusterIndexToHit clusterIndexToHit;
 
-  auto convert = [&](auto& hits, auto& mkFitHits, auto& clusterIndexToHit) {
-    convertHits(hits, hitWrapper.eventOfHits(), mkFitHits, clusterIndexToHit, ttopo, ttrhBuilder, mkFitGeom);
+  auto convert = [&](auto& hits, auto& mkFitHits, auto& clusterIndexToHit, auto& clusterCharge) {
+    return convertHits(
+        hits, hitWrapper.eventOfHits(), mkFitHits, clusterIndexToHit, clusterCharge, ttopo, ttrhBuilder, mkFitGeom);
   };
-  convert(iEvent.get(pixelRecHitToken_), hitWrapper.pixelHits(), clusterIndexToHit.pixelHits());
-  convert(iEvent.get(stripRphiRecHitToken_), hitWrapper.outerHits(), clusterIndexToHit.outerHits());
-  convert(iEvent.get(stripStereoRecHitToken_), hitWrapper.outerHits(), clusterIndexToHit.outerHits());
+  std::vector<float> dummy;
+  auto pixelClusterID =
+      convert(iEvent.get(pixelRecHitToken_), hitWrapper.pixelHits(), clusterIndexToHit.pixelHits(), dummy);
+
+  edm::ProductID stripClusterID;
+  const auto& stripRphiHits = iEvent.get(stripRphiRecHitToken_);
+  const auto& stripStereoHits = iEvent.get(stripStereoRecHitToken_);
+  if (not stripRphiHits.empty()) {
+    stripClusterID =
+        convert(stripRphiHits, hitWrapper.outerHits(), clusterIndexToHit.outerHits(), hitWrapper.stripClusterCharge());
+  }
+  if (not stripStereoHits.empty()) {
+    auto stripStereoClusterID = convert(
+        stripStereoHits, hitWrapper.outerHits(), clusterIndexToHit.outerHits(), hitWrapper.stripClusterCharge());
+    if (stripRphiHits.empty()) {
+      stripClusterID = stripStereoClusterID;
+    } else if (stripClusterID != stripStereoClusterID) {
+      throw cms::Exception("LogicError") << "Encountered different cluster ProductIDs for strip RPhi hits ("
+                                         << stripClusterID << ") and stereo (" << stripStereoClusterID << ")";
+    }
+  }
+  hitWrapper.setPixelClustersID(pixelClusterID);
+  hitWrapper.setOuterClustersID(stripClusterID);
 
   mkfit::StdSeq::Cmssw_LoadHits_End(hitWrapper.eventOfHits());
 
@@ -149,25 +172,33 @@ void MkFitHitConverter::setDetails(mkfit::Hit& mhit, const SiStripCluster& clust
 }
 
 template <typename HitCollection>
-void MkFitHitConverter::convertHits(const HitCollection& hits,
-                                    mkfit::EventOfHits& mkFitEventOfHits,
-                                    mkfit::HitVec& mkFitHits,
-                                    std::vector<TrackingRecHit const*>& clusterIndexToHit,
-                                    const TrackerTopology& ttopo,
-                                    const TransientTrackingRecHitBuilder& ttrhBuilder,
-                                    const MkFitGeometry& mkFitGeom) const {
+edm::ProductID MkFitHitConverter::convertHits(const HitCollection& hits,
+                                              mkfit::EventOfHits& mkFitEventOfHits,
+                                              mkfit::HitVec& mkFitHits,
+                                              std::vector<TrackingRecHit const*>& clusterIndexToHit,
+                                              std::vector<float>& clusterChargeVec,
+                                              const TrackerTopology& ttopo,
+                                              const TransientTrackingRecHitBuilder& ttrhBuilder,
+                                              const MkFitGeometry& mkFitGeom) const {
   if (hits.empty())
-    return;
+    return edm::ProductID{};
   auto isPlusSide = [&ttopo](const DetId& detid) {
     return ttopo.side(detid) == static_cast<unsigned>(TrackerDetSide::PosEndcap);
   };
 
+  constexpr bool applyCCC = std::is_same_v<typename HitCollection::data_type, SiStripRecHit2D>;
+
+  edm::ProductID clusterID;
   {
     const auto& lastClusterRef = hits.data().back().firstClusterRef();
+    clusterID = lastClusterRef.id();
     if (lastClusterRef.index() >= mkFitHits.size()) {
       auto const size = lastClusterRef.index();
       mkFitHits.resize(size);
       clusterIndexToHit.resize(size, nullptr);
+      if constexpr (applyCCC) {
+        clusterChargeVec.resize(size, -1.f);
+      }
     }
   }
 
@@ -195,7 +226,13 @@ void MkFitHitConverter::convertHits(const HitCollection& hits,
       err.At(0, 2) = gerr.czx();
       err.At(1, 2) = gerr.czy();
 
-      const auto clusterIndex = hit.firstClusterRef().index();
+      auto clusterRef = hit.firstClusterRef();
+      if UNLIKELY (clusterRef.id() != clusterID) {
+        throw cms::Exception("LogicError")
+            << "Input hit collection has Refs to many cluster collections. Last hit had Ref to product " << clusterID
+            << ", but encountered Ref to product " << clusterRef.id() << " on detid " << detid.rawId();
+      }
+      const auto clusterIndex = clusterRef.index();
       LogTrace("MkFitHitConverter") << "Adding hit detid " << detid.rawId() << " subdet " << subdet << " layer "
                                     << layer << " isStereo " << isStereo << " zplus " << isPlusSide(detid) << " index "
                                     << clusterIndex << " ilay " << ilay;
@@ -203,15 +240,23 @@ void MkFitHitConverter::convertHits(const HitCollection& hits,
       if UNLIKELY (clusterIndex >= mkFitHits.size()) {
         mkFitHits.resize(clusterIndex + 1);
         clusterIndexToHit.resize(clusterIndex + 1, nullptr);
+        if constexpr (applyCCC) {
+          clusterChargeVec.resize(clusterIndex + 1, -1.f);
+        }
       }
       mkFitHits[clusterIndex] = mkfit::Hit(pos, err);
       clusterIndexToHit[clusterIndex] = &hit;
+      if constexpr (applyCCC) {
+        clusterChargeVec[clusterIndex] = charge;
+      }
+
       const auto uniqueIdInLayer = mkFitGeom.uniqueIdInLayer(ilay, detid.rawId());
       setDetails(mkFitHits[clusterIndex], *(hit.cluster()), uniqueIdInLayer, charge);
 
       mkFitEventOfHits[ilay].RegisterHit(clusterIndex);
     }
   }
+  return clusterID;
 }
 
 DEFINE_FWK_MODULE(MkFitHitConverter);
--- RecoTracker/MkFit/python/customizeHLTIter0ToMkFit.py
+++ RecoTracker/MkFit/python/customizeHLTIter0ToMkFit.py
@@ -36,6 +36,7 @@ def customizeHLTIter0ToMkFit(process):
     process.hltIter0PFlowCkfTrackCandidatesMkFit = mkFitProducer_cfi.mkFitProducer.clone(
         hits = "hltIter0PFlowCkfTrackCandidatesMkFitHits",
         seeds = "hltIter0PFlowCkfTrackCandidatesMkFitSeeds",
+        minGoodStripCharge = dict(refToPSet_ = 'HLTSiStripClusterChargeCutLoose'),
     )
     process.hltIter0PFlowCkfTrackCandidates = mkFitOutputConverter_cfi.mkFitOutputConverter.clone(
         seeds = "hltIter0PFLowPixelSeedsFromPixelTracks",
