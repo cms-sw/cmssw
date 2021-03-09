40ea29901b451f2e5c87487ccbf9a190a7f692ab
Sven Dildick <sven.dildick@cern.ch>
2021-03-09 12:39:03 -0600

    Make run for all eras

--- Validation/MuonCSCDigis/python/cscDigiValidation_cfi.py
+++ Validation/MuonCSCDigis/python/cscDigiValidation_cfi.py
@@ -12,12 +12,16 @@ cscDigiValidation = DQMEDAnalyzer(
     muonCSCDigiPSet,
     muonCSCStubPSet,
     simHitsTag = cms.InputTag("mix", "g4SimHitsMuonCSCHits"),
-    outputFile = cms.string(''),
     doSim = cms.bool(True),
+    useGEMs = cms.bool(False),
     ## numbering follows the chamberType in CSCDetId
     etaMins = cms.vdouble(2.0, 1.6, 1.2, 0.9, 1.6, 1.0, 1.7, 1.1, 1.8, 1.2),
     etaMaxs = cms.vdouble(2.4, 2.2, 1.7, 1.1, 2.4, 1.6, 2.4, 1.7, 2.4, 1.8)
 )
 
+from Configuration.Eras.Modifier_run3_GEM_cff import run3_GEM
+run3_GEM.toModify(cscDigiValidation, useGEMs = True)
+
+## do not run GEMs in fastsim sequences
 from Configuration.Eras.Modifier_fastSim_cff import fastSim
 fastSim.toModify(cscDigiValidation, simHitsTag = "mix:MuonSimHitsMuonCSCHits")
