56673e0efbbbb6dee9debb3ebca33a2b542c5885
Sven Dildick <sven.dildick@cern.ch>
2021-03-09 12:39:03 -0600

    Make run for all eras

--- Validation/CSCRecHits/python/cscRecHitValidation_cfi.py
+++ Validation/CSCRecHits/python/cscRecHitValidation_cfi.py
@@ -6,8 +6,12 @@ cscRecHitValidation = DQMEDAnalyzer(
     'CSCRecHitValidation',
     cscRecHitPSet,
     doSim = cms.bool(True),
+    useGEMs = cms.bool(False),
     simHitsTag = cms.InputTag("mix","g4SimHitsMuonCSCHits")
 )
+
+from Configuration.Eras.Modifier_run3_GEM_cff import run3_GEM
+run3_GEM.toModify(cscRecHitValidation, useGEMs = True)
 
 from Configuration.Eras.Modifier_fastSim_cff import fastSim
 fastSim.toModify(cscRecHitValidation, simHitsTag = "mix:MuonSimHitsMuonCSCHits")
