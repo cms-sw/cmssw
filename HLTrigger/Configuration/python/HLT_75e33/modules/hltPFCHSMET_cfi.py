e55f5bb8cf44a5c703ab97ed90ba72b83b8a840d
Marino Missiroli <m.missiroli@cern.ch>
2021-03-09 17:49:16 +0100

    Update the Jets/MET configuration to match the AR and draft TDR
    Update the Jets/MET configuration:
      - replace PFPuppiMET with PFPuppiMETTypeOne
      - update the Jets/MET thresholds to match the AR and draft TDR
      - remove the PF CHS reconstruction

--- HLTrigger/Configuration/python/HLT_75e33/modules/hltPFCHSMET_cfi.py
+++
@@ -1,7 +0,0 @@
-import FWCore.ParameterSet.Config as cms
-
-hltPFCHSMET = cms.EDProducer("PFMETProducer",
-    calculateSignificance = cms.bool(False),
-    globalThreshold = cms.double(0.0),
-    src = cms.InputTag("hltParticleFlowCHS")
-)
--- HLTrigger/Configuration/python/HLT_75e33/sequences/HLTPFCHSMETReconstruction_cfi.py
+++
@@ -1,6 +0,0 @@
-import FWCore.ParameterSet.Config as cms
-
-from ..modules.hltParticleFlowCHS_cfi import *
-from ..modules.hltPFCHSMET_cfi import *
-
-HLTPFCHSMETReconstruction = cms.Sequence(hltParticleFlowCHS+hltPFCHSMET)
