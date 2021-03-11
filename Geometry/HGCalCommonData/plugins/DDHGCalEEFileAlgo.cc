ce0991a5fef9954e451900ec02bade600641fd92
Sunanda <sunanda.banerjee@cern.ch>
2021-03-11 17:57:57 +0100

    Correct material budget plot for V14

--- Geometry/HGCalCommonData/plugins/DDHGCalEEFileAlgo.cc
+++ Geometry/HGCalCommonData/plugins/DDHGCalEEFileAlgo.cc
@@ -250,39 +250,26 @@ void DDHGCalEEFileAlgo::constructLayers(const DDLogicalPart& module, DDCompactVi
       DDLogicalPart glog;
       if (layerSense_[ly] < 1) {
         std::vector<double> pgonZ, pgonRin, pgonRout;
-        if (layerSense_[ly] == 0 || absorbMode_ == 0) {
-          double rmax = routF * cosAlpha_ - tol1_;
-          pgonZ.emplace_back(-hthick);
-          pgonZ.emplace_back(hthick);
-          pgonRin.emplace_back(rinB);
-          pgonRin.emplace_back(rinB);
-          pgonRout.emplace_back(rmax);
-          pgonRout.emplace_back(rmax);
-        } else {
-          HGCalGeomTools::radius(zz - hthick,
-                                 zz + hthick,
-                                 zFrontB_,
-                                 rMinFront_,
-                                 slopeB_,
-                                 zFrontT_,
-                                 rMaxFront_,
-                                 slopeT_,
-                                 -layerSense_[ly],
-                                 pgonZ,
-                                 pgonRin,
-                                 pgonRout);
-#ifdef EDM_ML_DEBUG
-          edm::LogVerbatim("HGCalGeom") << "DDHGCalEEFileAlgo: z " << (zz - hthick) << ":" << (zz + hthick) << " with "
-                                        << pgonZ.size() << " palnes";
-          for (unsigned int isec = 0; isec < pgonZ.size(); ++isec)
-            edm::LogVerbatim("HGCalGeom")
-                << "[" << isec << "] z " << pgonZ[isec] << " R " << pgonRin[isec] << ":" << pgonRout[isec];
-#endif
-          for (unsigned int isec = 0; isec < pgonZ.size(); ++isec) {
-            pgonZ[isec] -= zz;
+	double rmax = routF * cosAlpha_ - tol1_;
+	HGCalGeomTools::radius(zz - hthick,
+			       zz + hthick,
+			       zFrontB_,
+			       rMinFront_,
+			       slopeB_,
+			       zFrontT_,
+			       rMaxFront_,
+			       slopeT_,
+			       -layerSense_[ly],
+			       pgonZ,
+			       pgonRin,
+			       pgonRout);
+	for (unsigned int isec = 0; isec < pgonZ.size(); ++isec) {
+	  pgonZ[isec] -= zz;
+	  if (layerSense_[ly] == 0 || absorbMode_ == 0)
+            pgonRout[isec] = rmax;
+	  else
             pgonRout[isec] = pgonRout[isec] * cosAlpha_ - tol1_;
-          }
-        }
+	}
         DDSolid solid =
             DDSolidFactory::polyhedra(DDName(name, nameSpace_), sectors_, -alpha_, 2._pi, pgonZ, pgonRin, pgonRout);
         glog = DDLogicalPart(solid.ddname(), matter, solid);
@@ -290,7 +277,7 @@ void DDHGCalEEFileAlgo::constructLayers(const DDLogicalPart& module, DDCompactVi
         edm::LogVerbatim("HGCalGeom") << "DDHGCalEEFileAlgo: " << solid.name() << " polyhedra of " << sectors_
                                       << " sectors covering " << convertRadToDeg(-alpha_) << ":"
                                       << convertRadToDeg(-alpha_ + 2._pi) << " with " << pgonZ.size()
-                                      << " sections and filled with " << matName << ":" << &matter;
+                                      << " sections and filled with " << matName;
         for (unsigned int k = 0; k < pgonZ.size(); ++k)
           edm::LogVerbatim("HGCalGeom") << "[" << k << "] z " << pgonZ[k] << " R " << pgonRin[k] << ":" << pgonRout[k];
 #endif
