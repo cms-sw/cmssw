774e425bc2ead63ee487621eba0ec59850b2518d
Sunanda <sunanda.banerjee@cern.ch>
2021-03-11 17:57:57 +0100

    Correct material budget plot for V14

--- Geometry/HGCalCommonData/plugins/DDHGCalHEFileAlgo.cc
+++ Geometry/HGCalCommonData/plugins/DDHGCalHEFileAlgo.cc
@@ -304,33 +304,26 @@ void DDHGCalHEFileAlgo::constructLayers(const DDLogicalPart& module, DDCompactVi
       DDLogicalPart glog;
       if (layerSense_[ly] < 1) {
         std::vector<double> pgonZ, pgonRin, pgonRout;
-        if (layerSense_[ly] == 0 || absorbMode_ == 0) {
-          double rmax =
-              (std::min(routF, HGCalGeomTools::radius(zz + hthick, zFrontT_, rMaxFront_, slopeT_)) * cosAlpha_) - tol1_;
-          pgonZ.emplace_back(-hthick);
-          pgonZ.emplace_back(hthick);
-          pgonRin.emplace_back(rinB);
-          pgonRin.emplace_back(rinB);
-          pgonRout.emplace_back(rmax);
-          pgonRout.emplace_back(rmax);
-        } else {
-          HGCalGeomTools::radius(zz - hthick,
-                                 zz + hthick,
-                                 zFrontB_,
-                                 rMinFront_,
-                                 slopeB_,
-                                 zFrontT_,
-                                 rMaxFront_,
-                                 slopeT_,
-                                 -layerSense_[ly],
-                                 pgonZ,
-                                 pgonRin,
-                                 pgonRout);
-          for (unsigned int isec = 0; isec < pgonZ.size(); ++isec) {
-            pgonZ[isec] -= zz;
-            pgonRout[isec] = pgonRout[isec] * cosAlpha_ - tol1_;
-          }
-        }
+	double rmax = (std::min(routF, HGCalGeomTools::radius(zz + hthick, zFrontT_, rMaxFront_, slopeT_)) * cosAlpha_) - tol1_;
+	HGCalGeomTools::radius(zz - hthick,
+			       zz + hthick,
+			       zFrontB_,
+			       rMinFront_,
+			       slopeB_,
+			       zFrontT_,
+			       rMaxFront_,
+			       slopeT_,
+			       -layerSense_[ly],
+			       pgonZ,
+			       pgonRin,
+			       pgonRout);
+	for (unsigned int isec = 0; isec < pgonZ.size(); ++isec) {
+	  pgonZ[isec] -= zz;
+	  if (layerSense_[ly] == 0 || absorbMode_ == 0)
+	    pgonRout[isec] = rmax;
+	  else
+	    pgonRout[isec] = pgonRout[isec] * cosAlpha_ - tol1_;
+	}
         DDSolid solid =
             DDSolidFactory::polyhedra(DDName(name, nameSpace_), sectors_, -alpha_, 2._pi, pgonZ, pgonRin, pgonRout);
         glog = DDLogicalPart(solid.ddname(), matter, solid);
