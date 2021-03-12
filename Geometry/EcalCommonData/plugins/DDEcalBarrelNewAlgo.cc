7be372c11bae18c22cbd405d50db5114ef6215aa
Sunanda <sunanda.banerjee@cern.ch>
2021-03-12 16:07:15 +0100

    Code check

--- Geometry/EcalCommonData/plugins/DDEcalBarrelNewAlgo.cc
+++ Geometry/EcalCommonData/plugins/DDEcalBarrelNewAlgo.cc
@@ -1924,7 +1924,7 @@ void DDEcalBarrelNewAlgo::execute(DDCompactView& cpv) {
                                 : myrot(ilyPipeLog[type].name().name() + "_rot" + std::to_string(copyNum[type]),
                                         Rota(Vec3(xx, yy, 0), 90 * deg))));
 #ifdef EDM_ML_DEBUG
-	      std::string rrr = (9 > type) ? " with no rotation" : " with rotation";
+              std::string rrr = (9 > type) ? " with no rotation" : " with rotation";
               edm::LogVerbatim("EBGeomX") << ilyPipeLog[type].name() << ":" << copyNum[type] << " positioned in "
                                           << xilyLog.name() << " at" << DDTranslation(xx, yy, zz) << rrr;
 #endif
