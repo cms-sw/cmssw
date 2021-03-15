90b03f3ab12952294c5f49e0e9fcfb29daa4cf01
Ernesta <ernesta.petraityte@yahoo.com>
2021-03-15 16:25:33 +0100

    front-end update for ppd workshop

--- DQMServices/DQMGUI/frontend/_next/static/chunks/webpack.js
+++ DQMServices/DQMGUI/frontend/_next/static/chunks/webpack.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "0d54d866349a2a30edcb";
+/******/ 	var hotCurrentHash = "a39f25daf2cc888ea6b3";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
