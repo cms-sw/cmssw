6608bedbaa59d2c30586e6183b66eff7358cb05c
Theodor Christian Herwig <2671-therwig@users.noreply.gitlab.cern.ch>
2021-03-16 04:26:29 +0100

    first complete version of the cmssw emulator
    (cherry picked from commit 3a9823f06b0b3b22a675106ba793629d1cff74b6)

--- L1Trigger/Phase2L1ParticleFlow/python/L1MetPfProducer_cfi.py
+++ L1Trigger/Phase2L1ParticleFlow/python/L1MetPfProducer_cfi.py
@@ -1,6 +1,8 @@
 import FWCore.ParameterSet.Config as cms
 
-# L1MetPfProducer = cms.EDProducer("L1MetPfProducer",
-L1MetPfProducer = cms.EDProducer("L1MetPfProducerHLS",
+L1MetPfProducer = cms.EDProducer("L1MetPfProducer",
+                                 L1PFObjects = cms.InputTag("L1PFProducer","l1pfCandidates")
+)
+L1MetPfProducerHLS = cms.EDProducer("L1MetPfProducerHLS",
                                  L1PFObjects = cms.InputTag("L1PFProducer","l1pfCandidates")
 )
