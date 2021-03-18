fc5aca2c4905d4f1e3192c6dd8530a7569d06821
Adinda de Wit <adinda.maite.de.wit@cern.ch>
2021-03-18 18:45:06 +0100

    Update era modifiers for T25/T26

--- Configuration/PyReleaseValidation/python/upgradeWorkflowComponents.py
+++ Configuration/PyReleaseValidation/python/upgradeWorkflowComponents.py
@@ -1291,16 +1291,14 @@ upgradeProperties[2026] = {
         'Geom' : 'Extended2026D80', # N.B.: Geometry with 3D pixels in the Inner Tracker.
         'HLTmenu': '@fake2',
         'GT' : 'auto:phase2_realistic_T25',
-        #'ProcessModifier': 'PixelCPEGeneric',   # This swaps template reco CPE for generic reco CPE
-        'Era' : 'Phase2C11T23M9', # customized for 3D Pixels and Muon M9
+        'Era' : 'Phase2C11I13M9', 
         'ScenToRun' : ['GenSimHLBeamSpot','DigiTrigger','RecoGlobal', 'HARVESTGlobal'],
     },
     '2026D81' : {
         'Geom' : 'Extended2026D81', # N.B.: Geometry with 3D pixels and square 50x50 um2 pixels in the Inner Tracker.
         'HLTmenu': '@fake2',
         'GT' : 'auto:phase2_realistic_T26',
-        'ProcessModifier': 'PixelCPEGeneric',   # This swaps template reco CPE for generic reco CPE
-        'Era' : 'Phase2C11T23M9', # customizes for 3D Pixels and Muon M9
+        'Era' : 'Phase2C11I13T26M9', # customized for square pixels and Muon M9
         'ScenToRun' : ['GenSimHLBeamSpot','DigiTrigger','RecoGlobal', 'HARVESTGlobal'],
     },
 }
