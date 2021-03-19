1e3ea731a712223784dd899d9c9d6206ef6bb6ca
Davide Valsecchi <davide.valsecchi@cern.ch>
2021-03-19 11:33:06 +0100

    Added record for ECAL trigger double weights mechanism

---
+++ CondFormats/DataRecord/interface/EcalTPGTPModeRcd.h
@@ -0,0 +1,14 @@
+#ifndef EcalTPGTPMode_RCD_h
+#define EcalTPGTPMode_RCD_h
+
+
+//
+// Author:      Davide Valsecchi
+// Created:     Thu, 11 Feb 2021 08:50:49 GMT
+//
+
+#include "FWCore/Framework/interface/EventSetupRecordImplementation.h"
+
+class EcalTPGTPModeRcd : public edm::eventsetup::EventSetupRecordImplementation<EcalTPGTPModeRcd> {};
+
+#endif
