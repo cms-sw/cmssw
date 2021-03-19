7eb0d71a66c97359af60ae52ab4ba83ebad1d44d
Patrick Gartung <gartung@fnal.gov>
2021-03-19 18:29:33 +0100

    Print the number of reports per file in parens in the scan-build report

--- Utilities/StaticAnalyzers/scripts/postprocess-scan-build.py
+++ Utilities/StaticAnalyzers/scripts/postprocess-scan-build.py
@@ -6,13 +6,20 @@ url=os.path.abspath(sys.argv[1])
 report_dir = os.path.dirname(url)
 page=open(url)
 soup=BeautifulSoup(page.read(),features="lxml")
-seen=set()
+seen=dict()
 tables=soup.find_all('table',recursive=True)
+
+header=tables[2].findAll('thead')
+rowheader=header[0].findAll('tr')
+rowheaders=rowheader[0].findAll('td')
+rowheaders[2].append(' (# reports)')
+
 rows = tables[2].findChildren('tr')
 for row in rows:
     cells=row.findChildren('td')
     key=str(cells[2])+str(cells[4])+str(cells[5])
-    if key in seen:
+    if key in seen.keys():
+        seen[key]=seen[key]+1
         href = cells[6].find('a',href=True)
         if href:
           report = href['href'].split("#")[0]
@@ -21,5 +28,13 @@ for row in rows:
             os.remove(report_file)
         row.decompose()
     else:
-        seen.add(key)
-print soup.prettify("latin1")
+        seen[key]=1
+
+
+rows = tables[2].find('tbody').findChildren('tr')
+for row in rows:
+    cells=row.findChildren('td')
+    key=str(cells[2])+str(cells[4])+str(cells[5])
+    if not key==rowheaders[2]:
+        cells[2].append(' ({})'.format(seen[key]))
+print(soup.prettify("latin1"))
