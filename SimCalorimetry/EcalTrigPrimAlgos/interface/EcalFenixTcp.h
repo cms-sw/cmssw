791a5f95df2f8539fac636796a4f9752eaf47749
Davide Valsecchi <davide.valsecchi@cern.ch>
2021-03-26 14:37:46 +0100

    Double weights ECAL TPG mechanism
    Implemented the double weights mechanism in the emulator.
    The ECAL Fenix strip now applies two filters and two peak finders.
    The Formatter classes are adapted to produce the correct output based on the EcalTPGTPMode record.

--- SimCalorimetry/EcalTrigPrimAlgos/interface/EcalFenixTcp.h
+++ SimCalorimetry/EcalTrigPrimAlgos/interface/EcalFenixTcp.h
@@ -6,7 +6,8 @@
 #include <SimCalorimetry/EcalTrigPrimAlgos/interface/EcalFenixFgvbEB.h>
 #include <SimCalorimetry/EcalTrigPrimAlgos/interface/EcalFenixMaxof2.h>
 #include <SimCalorimetry/EcalTrigPrimAlgos/interface/EcalFenixTcpFgvbEE.h>
-#include <SimCalorimetry/EcalTrigPrimAlgos/interface/EcalFenixTcpFormat.h>
+#include <SimCalorimetry/EcalTrigPrimAlgos/interface/EcalFenixTcpFormatEE.h>
+#include <SimCalorimetry/EcalTrigPrimAlgos/interface/EcalFenixTcpFormatEB.h>
 #include <SimCalorimetry/EcalTrigPrimAlgos/interface/EcalFenixTcpsFgvbEB.h>
 
 #include <DataFormats/EcalDigi/interface/EBDataFrame.h>
@@ -23,6 +24,7 @@ class EcalTPGFineGrainEBIdMap;
 class EcalTPGFineGrainTowerEE;
 class EcalTrigTowerDetId;
 class EcalTPGTowerStatus;
+class EcalTPGTPMode;
 
 /**
     \class EcalFenixTcp
@@ -31,8 +33,8 @@ class EcalTPGTowerStatus;
 class EcalFenixTcp {
 private:
   bool debug_;
-
   int nbMaxStrips_;
+  bool TPinfoPrintout_;
 
   EcalFenixMaxof2 *maxOf2_;
   std::vector<EcalFenixBypassLin *> bypasslin_;
@@ -41,11 +43,15 @@ private:
   EcalFenixTcpFgvbEE *fgvbEE_;
   EcalFenixTcpsFgvbEB *sfgvbEB_;
 
-  EcalFenixTcpFormat *formatter_;
+  EcalFenixTcpFormatEB *formatter_EB_;
+  EcalFenixTcpFormatEE *formatter_EE_;
+
+
 
   // permanent data structures
   std::vector<std::vector<int>> bypasslin_out_;
-  std::vector<int> adder_out_;
+  std::vector<int> adder_even_out_;
+  std::vector<int> adder_odd_out_;
   std::vector<int> maxOf2_out_;
   std::vector<int> fgvb_out_;
   std::vector<int> strip_fgvb_out_;
@@ -58,7 +64,8 @@ public:
                    const EcalTPGFineGrainEBIdMap *ecaltpgFineGrainEB,
                    const EcalTPGFineGrainTowerEE *ecaltpgFineGrainTowerEE,
                    const EcalTPGTowerStatus *ecaltpgBadTT,
-                   const EcalTPGSpike *ecaltpgSpike) {
+                   const EcalTPGSpike *ecaltpgSpike,
+                   const EcalTPGTPMode* ecaltpgTPMode) {
     ecaltpgFgEBGroup_ = ecaltpgFgEBGroup;
     ecaltpgLutGroup_ = ecaltpgLutGroup;
     ecaltpgLut_ = ecaltpgLut;
@@ -66,10 +73,11 @@ public:
     ecaltpgFineGrainTowerEE_ = ecaltpgFineGrainTowerEE;
     ecaltpgBadTT_ = ecaltpgBadTT;
     ecaltpgSpike_ = ecaltpgSpike;
+    ecaltpgTPMode_ = ecaltpgTPMode;
   }
   // end temporary, for timing tests
 
-  EcalFenixTcp(bool tcpFormat, bool debug, bool famos, int binOfMax, int maxNrSamples, int nbMaxStrips);
+  EcalFenixTcp(bool tcpFormat, bool debug, bool famos, int binOfMax, int maxNrSamples, int nbMaxStrips,bool TPinfoPrintout);
   virtual ~EcalFenixTcp();
 
   void process(std::vector<EBDataFrame> &bid,  // dummy argument for template call
@@ -87,11 +95,12 @@ public:
                bool isInInnerRings,
                EcalTrigTowerDetId thisTower);
 
-  void process_part1(std::vector<std::vector<int>> &tpframetow, int nStr, int bitMask);
+  void process_part1(std::vector<std::vector<int>> &tpframetow, int nStr, int bitMask, int bitOddEven);
 
   void process_part2_barrel(std::vector<std::vector<int>> &,
                             int nStr,
                             int bitMask,
+                            int bitOddEven,
                             const EcalTPGFineGrainEBGroup *ecaltpgFgEBGroup,
                             const EcalTPGLutGroup *ecaltpgLutGroup,
                             const EcalTPGLutIdMap *ecaltpgLut,
@@ -105,6 +114,7 @@ public:
   void process_part2_endcap(std::vector<std::vector<int>> &,
                             int nStr,
                             int bitMask,
+                            int bitOddEven,
                             const EcalTPGLutGroup *ecaltpgLutGroup,
                             const EcalTPGLutIdMap *ecaltpgLut,
                             const EcalTPGFineGrainTowerEE *ecaltpgFineGrainTowerEE,
@@ -117,7 +127,8 @@ public:
   EcalFenixBypassLin *getBypasslin(int i) const { return bypasslin_[i]; }
   EcalFenixEtTot *getAdder() const { return adder_; }
   EcalFenixMaxof2 *getMaxOf2() const { return maxOf2_; }
-  EcalFenixTcpFormat *getFormatter() const { return formatter_; }
+  EcalFenixTcpFormatEB *getFormatterEB() const { return formatter_EB_; }
+  EcalFenixTcpFormatEE *getFormatterEE() const { return formatter_EE_; }
   EcalFenixFgvbEB *getFGVBEB() const { return fgvbEB_; }
   EcalFenixTcpFgvbEE *getFGVBEE() const { return fgvbEE_; }
   EcalFenixTcpsFgvbEB *getsFGVBEB() const { return sfgvbEB_; }
@@ -129,6 +140,7 @@ public:
   const EcalTPGFineGrainTowerEE *ecaltpgFineGrainTowerEE_;
   const EcalTPGTowerStatus *ecaltpgBadTT_;
   const EcalTPGSpike *ecaltpgSpike_;
+  const EcalTPGTPMode * ecaltpgTPMode_;
 };
 
 #endif
--- SimCalorimetry/EcalTrigPrimAlgos/src/EcalFenixOddAmplitudeFilter.cc
+++ SimCalorimetry/EcalTrigPrimAlgos/src/EcalFenixOddAmplitudeFilter.cc
@@ -1,90 +1,82 @@
 #include "CondFormats/EcalObjects/interface/EcalTPGGroups.h"
-#include "CondFormats/EcalObjects/interface/EcalTPGWeightGroup.h"
-#include "CondFormats/EcalObjects/interface/EcalTPGWeightIdMap.h"
+#include "CondFormats/EcalObjects/interface/EcalTPGOddWeightGroup.h"
+#include "CondFormats/EcalObjects/interface/EcalTPGOddWeightIdMap.h"
 #include "FWCore/MessageLogger/interface/MessageLogger.h"
-#include <SimCalorimetry/EcalTrigPrimAlgos/interface/EcalFenixAmplitudeFilter.h>
+#include <SimCalorimetry/EcalTrigPrimAlgos/interface/EcalFenixOddAmplitudeFilter.h>
 #include <iostream>
+#include <string>
+#include <fstream>
 
-EcalFenixAmplitudeFilter::EcalFenixAmplitudeFilter(bool TPinfoPrintout) : inputsAlreadyIn_(0), stripid_{0}, shift_(6), TPinfoPrintout_(TPinfoPrintout) {}
+EcalFenixOddAmplitudeFilter::EcalFenixOddAmplitudeFilter(bool TPinfoPrintout)
+    : inputsAlreadyIn_(0), stripid_{0}, shift_(6), TPinfoPrintout_(TPinfoPrintout) {}
 
 
-EcalFenixAmplitudeFilter::~EcalFenixAmplitudeFilter() {}
+EcalFenixOddAmplitudeFilter::~EcalFenixOddAmplitudeFilter() {}
 
-int EcalFenixAmplitudeFilter::setInput(int input, int fgvb) {
+int EcalFenixOddAmplitudeFilter::setInput(int input) {
   if (input > 0X3FFFF) {
     std::cout << "ERROR IN INPUT OF AMPLITUDE FILTER" << std::endl;
     return -1;
   }
   if (inputsAlreadyIn_ < 5) {
     buffer_[inputsAlreadyIn_] = input;
-    fgvbBuffer_[inputsAlreadyIn_] = fgvb;
     inputsAlreadyIn_++;
   } else {
     for (int i = 0; i < 4; i++) {
       buffer_[i] = buffer_[i + 1];
-      fgvbBuffer_[i] = fgvbBuffer_[i + 1];
     }
     buffer_[4] = input;
-    fgvbBuffer_[4] = fgvb;
   }
   return 1;
 }
 
-void EcalFenixAmplitudeFilter::process(std::vector<int> &addout,
-                                       std::vector<int> &output,
-                                       std::vector<int> &fgvbIn,
-                                       std::vector<int> &fgvbOut) {
+void EcalFenixOddAmplitudeFilter::process(std::vector<int> &addout,
+                                       std::vector<int> &output) {
   // test
   inputsAlreadyIn_ = 0;
   for (unsigned int i = 0; i < 5; i++) {
     buffer_[i] = 0;  // FIXME: 5
-    fgvbBuffer_[i] = 0;
   }
   // test end
 
   for (unsigned int i = 0; i < addout.size(); i++) {
+
     // Only save TP info for Clock i >= 4 (from 0-9) because first 5 digis required to produce first ET value
     if (i>=4){
       if(TPinfoPrintout_) std::cout<<i<<std::dec;
     }
-    setInput(addout[i], fgvbIn[i]);
-    process();
+    setInput(addout[i]);
+    process(); // This should probably be renamed to something meaningful and not the same as the very function it's being called in...
     output[i] = processedOutput_;
-    fgvbOut[i] = processedFgvbOutput_;
   }
   // shift the result by 1!
   for (unsigned int i = 0; i < (output.size()); i++) {
     if (i != output.size() - 1) {
       output[i] = output[i + 1];
-      fgvbOut[i] = fgvbOut[i + 1];
     } else {
       output[i] = 0;
-      fgvbOut[i] = 0;
     }
   }
   return;
 }
 
-void EcalFenixAmplitudeFilter::process() {
+void EcalFenixOddAmplitudeFilter::process() {
   // UB FIXME: 5
   processedOutput_ = 0;
-  processedFgvbOutput_ = 0;
   if (inputsAlreadyIn_ < 5)
     return;
   int output = 0;
-  int fgvbInt = 0;
+
   for (int i = 0; i < 5; i++) {
     output += (weights_[i] * buffer_[i]) >> shift_;
-    if ((fgvbBuffer_[i] == 1 && i == 3) || fgvbInt == 1) {
-      fgvbInt = 1;
-    }
   }
+
   if (output < 0)
     output = 0;
   if (output > 0X3FFFF)
     output = 0X3FFFF;
   processedOutput_ = output;
-  processedFgvbOutput_ = fgvbInt;
+
   // by RK
   if(TPinfoPrintout_){
     std::cout<<" "<<stripid_;
@@ -93,32 +85,35 @@ void EcalFenixAmplitudeFilter::process() {
     for (int i = 0; i < 5; i++) {
       std::cout<<" "<<weights_[i]/64.0<<std::dec;}
     for (int i = 0; i < 5; i++) {
-      std::cout<<" "<<buffer_[i]<<std::dec;} // digis
+      std::cout<<" "<<buffer_[i]<<std::dec;} // Digis
     std::cout << " --> output: " << output;
-    std::cout << " EVEN";
+    std::cout << " ODD";
     std::cout<<std::endl;
   }
 }
 
-void EcalFenixAmplitudeFilter::setParameters(uint32_t raw,
-                                             const EcalTPGWeightIdMap *ecaltpgWeightMap,
-                                             const EcalTPGWeightGroup *ecaltpgWeightGroup) {
+void EcalFenixOddAmplitudeFilter::setParameters(uint32_t raw,
+                                             const EcalTPGOddWeightIdMap *ecaltpgOddWeightMap,
+                                             const EcalTPGOddWeightGroup *ecaltpgOddWeightGroup) {
+
   stripid_ = raw;    // by RK
   uint32_t params_[5];
-  const EcalTPGGroups::EcalTPGGroupsMap &groupmap = ecaltpgWeightGroup->getMap();
+  const EcalTPGGroups::EcalTPGGroupsMap &groupmap = ecaltpgOddWeightGroup->getMap();
   EcalTPGGroups::EcalTPGGroupsMapItr it = groupmap.find(raw);
   if (it != groupmap.end()) {
     uint32_t weightid = (*it).second;
-    const EcalTPGWeightIdMap::EcalTPGWeightMap &weightmap = ecaltpgWeightMap->getMap();
-    EcalTPGWeightIdMap::EcalTPGWeightMapItr itw = weightmap.find(weightid);
+    const EcalTPGOddWeightIdMap::EcalTPGWeightMap &weightmap = ecaltpgOddWeightMap->getMap();
+    EcalTPGOddWeightIdMap::EcalTPGWeightMapItr itw = weightmap.find(weightid);
     (*itw).second.getValues(params_[0], params_[1], params_[2], params_[3], params_[4]);
 
     // we have to transform negative coded in 7 bits into negative coded in 32
     // bits maybe this should go into the getValue method??
     // std::cout << "peak flag settings" << std::endl;
-    for (int i = 0; i < 5; ++i) {
-      weights_[i] = (params_[i] & 0x40) ? (int)(params_[i] | 0xffffffc0) : (int)(params_[i]);
 
+    for (int i = 0; i < 5; ++i) {
+
+      weights_[i] = (params_[i] & 0x40) ? (int)(params_[i] | 0xffffffc0) : (int)(params_[i]);
+      //std::cout << "ODD weight: "<<  std::dec << params_[i] << " --> " << std::dec << weights_[i] << std::endl;
       // Construct the peakFlag for sFGVB processing
       // peakFlag_[i] = ((params_[i] & 0x80) > 0x0) ? 1 : 0;
       // std::cout << " " << params_[i] << std::endl;
@@ -127,4 +122,5 @@ void EcalFenixAmplitudeFilter::setParameters(uint32_t raw,
     // std::cout << std::endl;
   } else
     edm::LogWarning("EcalTPG") << " could not find EcalTPGGroupsMap entry for " << raw;
+
 }
