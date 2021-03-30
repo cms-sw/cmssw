d7f5183e6cf75217f947b6c2a0af8c872e327cb9
Sunanda <sunanda.banerjee@cern.ch>
2021-03-30 15:39:06 +0200

    Code check

--- Geometry/HGCalCommonData/test/HGCalValidHexTester.cc
+++ Geometry/HGCalCommonData/test/HGCalValidHexTester.cc
@@ -53,19 +53,20 @@ private:
   edm::ESGetToken<HGCalDDDConstants, IdealGeometryRecord> dddToken_;
 };
 
-HGCalValidHexTester::HGCalValidHexTester(const edm::ParameterSet& iC) :
-  nameDetector_(iC.getParameter<std::string>("NameDevice")),
-  nameSense_(iC.getParameter<std::string>("NameSense")),
-  layers_(iC.getParameter<std::vector<int> >("Layers")),
-  moduleU_(iC.getParameter<std::vector<int> >("ModuleU")),
-  moduleV_(iC.getParameter<std::vector<int> >("ModuleV")),
-  types_(iC.getParameter<std::vector<int> >("Types")) {
-
+HGCalValidHexTester::HGCalValidHexTester(const edm::ParameterSet& iC)
+    : nameDetector_(iC.getParameter<std::string>("NameDevice")),
+      nameSense_(iC.getParameter<std::string>("NameSense")),
+      layers_(iC.getParameter<std::vector<int> >("Layers")),
+      moduleU_(iC.getParameter<std::vector<int> >("ModuleU")),
+      moduleV_(iC.getParameter<std::vector<int> >("ModuleV")),
+      types_(iC.getParameter<std::vector<int> >("Types")) {
   dddToken_ = esConsumes<HGCalDDDConstants, IdealGeometryRecord>(edm::ESInputTag{"", nameSense_});
 
-  edm::LogVerbatim("HGCalGeom") << "Test valid cells for " << nameDetector_ << " using constants of " << nameSense_ << " for " << layers_.size() << " modules";
+  edm::LogVerbatim("HGCalGeom") << "Test valid cells for " << nameDetector_ << " using constants of " << nameSense_
+                                << " for " << layers_.size() << " modules";
   for (unsigned int k = 0; k < layers_.size(); ++k)
-    edm::LogVerbatim("HGCalGeom") << "Wafer[" << k << "] Layer " << layers_[k] << " Type " << types_[k] << " U:V " << moduleU_[k] << ":" << moduleV_[k];
+    edm::LogVerbatim("HGCalGeom") << "Wafer[" << k << "] Layer " << layers_[k] << " Type " << types_[k] << " U:V "
+                                  << moduleU_[k] << ":" << moduleV_[k];
 }
 
 void HGCalValidHexTester::fillDescriptions(edm::ConfigurationDescriptions& descriptions) {
@@ -87,7 +88,8 @@ void HGCalValidHexTester::fillDescriptions(edm::ConfigurationDescriptions& descr
 void HGCalValidHexTester::analyze(const edm::Event& iEvent, const edm::EventSetup& iSetup) {
   const DetId::Detector det = (nameSense_ == "HGCalEESensitive") ? DetId::HGCalEE : DetId::HGCalHSi;
   const HGCalDDDConstants& hgdc = iSetup.getData(dddToken_);
-  edm::LogVerbatim("HGCalGeom") << nameDetector_ << " Layers = " << hgdc.layers(true) << " Sectors = " << hgdc.sectors() << "\n";
+  edm::LogVerbatim("HGCalGeom") << nameDetector_ << " Layers = " << hgdc.layers(true) << " Sectors = " << hgdc.sectors()
+                                << "\n";
   for (unsigned int k = 0; k < layers_.size(); ++k) {
     int nCells = (types_[k] == 0) ? HGCSiliconDetId::HGCalFineN : HGCSiliconDetId::HGCalCoarseN;
     int ncell(0);
@@ -95,7 +97,10 @@ void HGCalValidHexTester::analyze(const edm::Event& iEvent, const edm::EventSetu
       for (int v = 0; v < 2 * nCells; ++v) {
         if (((v - u) < nCells) && (u - v) <= nCells) {
           std::string state = hgdc.isValidHex8(layers_[k], moduleU_[k], moduleV_[k], u, v) ? "within" : "outside of";
-          edm::LogVerbatim("HGCalGeom") << "Cell[" << k << "," << ncell << "] " << HGCSiliconDetId(det, 1, types_[k], layers_[k], moduleU_[k], moduleV_[k], u, v) << " is " << state << " fiducial volume";
+          edm::LogVerbatim("HGCalGeom") << "Cell[" << k << "," << ncell << "] "
+                                        << HGCSiliconDetId(
+                                               det, 1, types_[k], layers_[k], moduleU_[k], moduleV_[k], u, v)
+                                        << " is " << state << " fiducial volume";
           ++ncell;
         }
       }
