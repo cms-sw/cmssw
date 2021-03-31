aabc33b90e8435c9742ff4bb206ed7bffa31b262
Jeongeun Lee <jeongeun.lee@cern.ch>
2021-03-31 09:08:40 +0200

    replace EDProducer with clone from the default cfipython

--- PhysicsTools/PatAlgos/python/patMuonMerger_cfi.py
+++ PhysicsTools/PatAlgos/python/patMuonMerger_cfi.py
@@ -1,10 +1,10 @@
 import FWCore.ParameterSet.Config as cms
-
-mergedMuons = cms.EDProducer("PATMuonMerger",
-                             muons     = cms.InputTag("slimmedMuons"), 
-                             pfCandidates=cms.InputTag("packedPFCandidates"),
-                             otherTracks = cms.InputTag("lostTracks"),
-                             muonCut = cms.string("pt>15 && abs(eta)<2.4"),
-                             pfCandidatesCut = cms.string("pt>15 && abs(eta)<2.4"),
-                             lostTrackCut = cms.string("pt>15 && abs(eta)<2.4")
+import PhysicsTools.PatAlgos.mergedMuonsNoCuts_cfi as _mod
+mergedMuons = _mod.mergedMuonsNoCuts.clone(
+                             muons           = "slimmedMuons", 
+                             pfCandidates    = "packedPFCandidates",
+                             otherTracks     = "lostTracks",
+                             muonCut         = "pt>15 && abs(eta)<2.4",
+                             pfCandidatesCut = "pt>15 && abs(eta)<2.4",
+                             lostTrackCut    = "pt>15 && abs(eta)<2.4"
                          )
