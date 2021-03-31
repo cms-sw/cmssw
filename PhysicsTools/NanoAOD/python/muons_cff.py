3990ede82e5c53d56744f9a67dd2b90a329c35e6
mdunser <marc.dunser@cern.ch>
2021-03-31 15:36:27 +0200

    backport safety for pT passthrough

--- PhysicsTools/NanoAOD/python/muons_cff.py
+++ PhysicsTools/NanoAOD/python/muons_cff.py
@@ -48,6 +48,7 @@ finalMuons = cms.EDFilter("PATMuonRefSelector",
     src = cms.InputTag("slimmedMuonsWithUserData"),
     cut = cms.string("pt > 15 || (pt > 3 && (passed('CutBasedIdLoose') || passed('SoftCutBasedId') || passed('SoftMvaId') || passed('CutBasedIdGlobalHighPt') || passed('CutBasedIdTrkHighPt')))")
 )
+(run2_nanoAOD_106Xv1 & ~run2_nanoAOD_devel).toModify(finalMuons, cut = "(pt > 3 && (passed('CutBasedIdLoose') || passed('SoftCutBasedId') || passed('SoftMvaId') || passed('CutBasedIdGlobalHighPt') || passed('CutBasedIdTrkHighPt')))")
 
 finalLooseMuons = cms.EDFilter("PATMuonRefSelector", # for isotrack cleaning
     src = cms.InputTag("slimmedMuonsWithUserData"),
