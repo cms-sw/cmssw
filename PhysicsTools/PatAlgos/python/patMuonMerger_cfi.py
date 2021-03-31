ad5ca357f4048ca80a7a3ca1534d48aefe4d7887
Jeongeun Lee <jeongeun.lee@cern.ch>
2021-03-31 09:14:46 +0200

    drop parameters with identical values in the reference cfipy

--- PhysicsTools/PatAlgos/python/patMuonMerger_cfi.py
+++ PhysicsTools/PatAlgos/python/patMuonMerger_cfi.py
@@ -1,9 +1,6 @@
 import FWCore.ParameterSet.Config as cms
 import PhysicsTools.PatAlgos.mergedMuonsNoCuts_cfi as _mod
 mergedMuons = _mod.mergedMuonsNoCuts.clone(
-                             muons           = "slimmedMuons", 
-                             pfCandidates    = "packedPFCandidates",
-                             otherTracks     = "lostTracks",
                              muonCut         = "pt>15 && abs(eta)<2.4",
                              pfCandidatesCut = "pt>15 && abs(eta)<2.4",
                              lostTrackCut    = "pt>15 && abs(eta)<2.4"
