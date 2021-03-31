c52783c0dd70bbbce70ed2c574dcb6b6260c35c4
Felice <felice.pantaleo@cern.ch>
2021-03-31 23:56:57 +0200

    Fix for premix wf

--- RecoHGCal/TICL/python/SimTracksters_cff.py
+++ RecoHGCal/TICL/python/SimTracksters_cff.py
@@ -18,6 +18,11 @@ filteredLayerClustersSimTracksters = _filteredLayerClustersProducer.clone(
 ticlSimTracksters = _trackstersFromSimClustersProducer.clone(
 )
 
+from Configuration.ProcessModifiers.premix_stage2_cff import premix_stage2
+premix_stage2.toModify(ticlSimTracksters,
+    simclusters = "mixData:MergedCaloTruth"
+)
+
 ticlMultiClustersFromSimTracksters = _multiClustersFromTrackstersProducer.clone(
     Tracksters = "ticlSimTracksters"
 )
