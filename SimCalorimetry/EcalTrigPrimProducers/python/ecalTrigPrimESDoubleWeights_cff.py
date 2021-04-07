25fe611ea61c5d8ee60bc2ebb43ff92fe4adabb7
Davide Valsecchi <davide.valsecchi@cern.ch>
2021-04-07 17:42:51 +0200

    Added ConDB connection

--- SimCalorimetry/EcalTrigPrimProducers/python/ecalTrigPrimESDoubleWeights_cff.py
+++ SimCalorimetry/EcalTrigPrimProducers/python/ecalTrigPrimESDoubleWeights_cff.py
@@ -1,6 +1,8 @@
 import FWCore.ParameterSet.Config as cms
 from CondCore.CondDB.CondDB_cfi import *
 
+CondDB.connect = cms.string('frontier://FrontierProd/CMS_CONDITIONS')
+
 EcalTPGDoubleWeightRecords = cms.ESSource("PoolDBESSource",CondDB,
                                 toGet = cms.VPSet(
                                             cms.PSet(record = cms.string("EcalTPGOddWeightGroupRcd"),
