7e03851a390458893b66a7404a7dcf3c51615306
Jeongeun Lee <jeongeun.lee@cern.ch>
2021-04-08 06:08:23 +0200

    clone from default hfsimplereco instead of default hosimplereco

--- RecoLocalCalo/HcalRecProducers/python/hfQIE10Reco_cfi.py
+++ RecoLocalCalo/HcalRecProducers/python/hfQIE10Reco_cfi.py
@@ -1,12 +1,9 @@
 import FWCore.ParameterSet.Config as cms
-import RecoLocalCalo.HcalRecProducers.hosimplereco_cfi as _mod
+import RecoLocalCalo.HcalRecProducers.hfsimplereco_cfi as _mod
 
-hfQIE10Reco = _mod.hosimplereco.clone(
-    correctionPhaseNS = 0.0,
+hfQIE10Reco = _mod.hfsimplereco.clone(
     digiLabel = "simHcalUnsuppressedDigis:HFQIE10DigiCollection",
     Subdetector = 'HFQIE10',
-    correctForPhaseContainment = False,
-    correctForTimeslew = False,
     firstSample = 2,
     samplesToAdd = 1
 )
