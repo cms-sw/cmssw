9ef498ab472e429b75cd2e7a7945d5c3fb731be2
Theodor Christian Herwig <2671-therwig@users.noreply.gitlab.cern.ch>
2021-04-12 21:15:53 +0200

    remove old version
    (cherry picked from commit ab239bfa9824678e9a4a13975bf0dfe472d6ced5)

--- L1Trigger/Phase2L1ParticleFlow/python/L1MetPfProducer_cfi.py
+++ L1Trigger/Phase2L1ParticleFlow/python/L1MetPfProducer_cfi.py
@@ -1,12 +1,6 @@
 import FWCore.ParameterSet.Config as cms
 
-L1MetPfProducerTDR = cms.EDProducer("L1MetPfProducerTDR",
-                                    L1PFObjects = cms.InputTag("L1PFProducer","l1pfCandidates"),
-                                    sinCosTableBits = cms.uint32(8),
-                                    inverseDropBits = cms.uint32(2),
-                                    arcTanTableBits = cms.uint32(7)
-)
-
 L1MetPfProducer = cms.EDProducer("L1MetPfProducer",
                                  L1PFObjects = cms.InputTag("L1PFProducer","l1pfCandidates"),
+                                 maxCandidates = cms.int32(128),
 )
