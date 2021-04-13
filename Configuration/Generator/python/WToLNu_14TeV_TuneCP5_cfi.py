066fa3ea6081ab64abde92919bd2fb808935726b
Mia <mia.tosi@cern.ch>
2021-04-13 13:33:19 +0200

    add TuneCP5 ones
    fix

--- Configuration/Generator/python/WToLNu_14TeV_TuneCP5_cfi.py
+++ Configuration/Generator/python/WToLNu_14TeV_TuneCP5_cfi.py
@@ -1,6 +1,6 @@
 import FWCore.ParameterSet.Config as cms
 from Configuration.Generator.Pythia8CommonSettings_cfi import *
-from Configuration.Generator.Pythia8CUEP8M1Settings_cfi import *
+from Configuration.Generator.MCTunes2017.PythiaCP5Settings_cfi import *
 
 generator = cms.EDFilter("Pythia8GeneratorFilter",
                          pythiaPylistVerbosity = cms.untracked.int32(0),
@@ -11,7 +11,7 @@ generator = cms.EDFilter("Pythia8GeneratorFilter",
                          maxEventsToPrint = cms.untracked.int32(0),
                          PythiaParameters = cms.PSet(
         pythia8CommonSettingsBlock,
-        pythia8CUEP8M1SettingsBlock,
+        pythia8CP5SettingsBlock,
         processParameters = cms.vstring(
             'WeakSingleBoson:ffbar2W = on',
             '24:onMode = off',
@@ -20,7 +20,7 @@ generator = cms.EDFilter("Pythia8GeneratorFilter",
             '24:onIfAny = 15 16',
             ),
         parameterSets = cms.vstring('pythia8CommonSettings',
-                                    'pythia8CUEP8M1Settings',
+                                    'pythia8CP5Settings',
                                     'processParameters',
                                 )
                          )
