b629e7634fa4e30234b6bd8735faa5f10bf7f4a4
Jeongeun Lee <jeongeun.lee@cern.ch>
2021-04-13 15:34:37 +0200

    retain the comments and structure

--- RecoLocalMuon/CSCRecHitD/python/cscRecHitD_cfi.py
+++ RecoLocalMuon/CSCRecHitD/python/cscRecHitD_cfi.py
@@ -7,16 +7,66 @@ from RecoLocalMuon.CSCRecHitD.cscRecHitD_cff import *
 import RecoLocalMuon.CSCRecHitD.configWireTimeWindow_cfi as _mod
 
 csc2DRecHits = _mod.configWireTimeWindow.clone(
+    #
+    #    Parameters for coordinate and uncertainty calculations
+    #    Data and MC parameters are (still) different
+    #    Needs tuning
+    #
+    #cscRecHitDParameters, # drop this due to identical values in cfipython _mod
+    #
+    #    Parameters for strip hits
+    #
+    CSCStripPeakThreshold = 10.0,
+    CSCStripClusterChargeCut = 25.0,
+    CSCStripxtalksOffset = 0.03,
+    #
+    #    How to find SCA peak time?
+    #                              
+    UseAverageTime = False,
+    UseParabolaFit = False,
+    UseFivePoleFit = True,                       
+    #
+    #    Parameters for wire hits
+    CSCWireClusterDeltaT = 1,
+    #
     #    wire time window used for reconstruction
     CSCUseReducedWireTimeWindow = True,
     CSCWireTimeWindowLow = 5,
-    CSCWireTimeWindowHigh = 11
+    CSCWireTimeWindowHigh = 11,
+    #
+    #    Calibration info:
+    CSCUseCalibrations = True,
+    #    Pedestal treatment
+    CSCUseStaticPedestals = False,
+    CSCNoOfTimeBinsForDynamicPedestal = 2,
     #
     #    Which digis:
     #
+    #  When using data from unpacker
+    wireDigiTag  = "muonCSCDigis:MuonCSCWireDigi",
+    stripDigiTag = "muonCSCDigis:MuonCSCStripDigi",
     #  When using data from simulation
     #    wireDigiTag  = "simMuonCSCDigis:MuonCSCWireDigi",
     #    stripDigiTag = "simMuonCSCDigis:MuonCSCStripDigi",
+    #
+    # Use dead channels information 
+    readBadChannels = True,
+    readBadChambers = True,
+    #                          
+    # Do we use the chip and chamber and L1A phase corrections when filling the recHit time?
+    #
+    CSCUseTimingCorrections = True,
+    #
+    # Do we correct the energy deposited for gas gains?
+    CSCUseGasGainCorrections = True,
+    #
+    #    Parameters which are not used currently
+    #
+    CSCDebug = False,
+    #  To be set once wire digis have proper timing info:
+    CSCstripWireDeltaTime = 8,
+    # to be deleted
+    CSCStripClusterSize = 3
 )
 
 ##
