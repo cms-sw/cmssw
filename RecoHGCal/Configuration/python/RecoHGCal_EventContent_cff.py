26316a4c0105e2c30351a754659b50ff4160c0b7
Felice <felice.pantaleo@cern.ch>
2021-04-13 15:43:15 +0200

    add comment to deprecate Multiclusters

--- RecoHGCal/Configuration/python/RecoHGCal_EventContent_cff.py
+++ RecoHGCal/Configuration/python/RecoHGCal_EventContent_cff.py
@@ -3,6 +3,7 @@ import FWCore.ParameterSet.Config as cms
 
 #AOD content
 TICL_AOD = cms.PSet(
+  # MultiClusters will be deprecated soon
     outputCommands = cms.untracked.vstring(
       'keep *_ticlMultiClustersFromTrackstersEM_*_*',
       'keep *_ticlMultiClustersFromTrackstersHAD_*_*',
