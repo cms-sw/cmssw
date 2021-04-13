cd20582c3eea71360e1348d92889a4f783483515
Andrea Bocci <andrea.bocci@cern.ch>
2021-04-13 23:42:40 +0200

    Make the Pixel 'gpu' workflow use a GPU if available, and fall-back to CPU otherwise

--- RecoPixelVertexing/PixelVertexFinding/python/PixelVertexes_cff.py
+++ RecoPixelVertexing/PixelVertexFinding/python/PixelVertexes_cff.py
@@ -1,4 +1,7 @@
 import FWCore.ParameterSet.Config as cms
+from HeterogeneousCore.CUDACore.SwitchProducerCUDA import SwitchProducerCUDA
 
-from RecoPixelVertexing.PixelVertexFinding.PixelVertexes_cfi import *
-
+from RecoPixelVertexing.PixelVertexFinding.PixelVertexes_cfi import pvClusterComparer, pixelVertices as _pixelVertices
+pixelVertices = SwitchProducerCUDA(
+    cpu = _pixelVertices.clone()
+)
