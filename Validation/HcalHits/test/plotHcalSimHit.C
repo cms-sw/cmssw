f86ef73ef98ced5938c72118c0c61e4781710d3e
Sunanda <sunanda.banerjee@cern.ch>
2021-04-20 21:05:39 +0200

    Code check

--- Validation/HcalHits/test/plotHcalSimHit.C
+++ Validation/HcalHits/test/plotHcalSimHit.C
@@ -36,14 +36,14 @@
 #include <string>
 
 void setTDRStyle() {
-  TStyle *tdrStyle = new TStyle("tdrStyle","Style for P-TDR");
+  TStyle *tdrStyle = new TStyle("tdrStyle", "Style for P-TDR");
 
   // For the canvas:
   tdrStyle->SetCanvasBorderMode(0);
   tdrStyle->SetCanvasColor(kWhite);
-  tdrStyle->SetCanvasDefH(600); //Height of canvas
-  tdrStyle->SetCanvasDefW(600); //Width of canvas
-  tdrStyle->SetCanvasDefX(0);   //POsition on screen
+  tdrStyle->SetCanvasDefH(600);  //Height of canvas
+  tdrStyle->SetCanvasDefW(600);  //Width of canvas
+  tdrStyle->SetCanvasDefX(0);    //POsition on screen
   tdrStyle->SetCanvasDefY(0);
 
   // For the Pad:
@@ -108,17 +108,15 @@ void setTDRStyle() {
   tdrStyle->SetPadTickY(1);
 
   tdrStyle->cd();
-
 }
-
 
 void plotCompare(std::string infile1, std::string infile2, std::string text1, std::string text2, int type, bool save) {
   const int ndets = 4;
   const int nhist = 5;
-  std::string names[ndets][nhist] = {{"Hit05", "Hit17", "Hit21", "Hit25", "Hit29"}, 
-				     {"Hit06", "Hit18", "Hit22", "Hit26", "Hit30"},
-				     {"Hit07", "Hit19", "Hit23", "Hit27", "Hit31"},
-				     {"Hit08", "Hit20", "Hit24", "Hit28", "Hit32"}};
+  std::string names[ndets][nhist] = {{"Hit05", "Hit17", "Hit21", "Hit25", "Hit29"},
+                                     {"Hit06", "Hit18", "Hit22", "Hit26", "Hit30"},
+                                     {"Hit07", "Hit19", "Hit23", "Hit27", "Hit31"},
+                                     {"Hit08", "Hit20", "Hit24", "Hit28", "Hit32"}};
   std::string xtitl[nhist] = {"Hits", "Depth", "i#eta", "i#phi", "Energy (MeV)"};
   std::string dets[ndets] = {"HB", "HE", "HO", "HF"};
   int nbins[nhist] = {10, 1, 1, 1, 1};
@@ -127,69 +125,72 @@ void plotCompare(std::string infile1, std::string infile2, std::string text1, st
   int colors[2] = {2, 4};
   int style[2] = {1, 2};
 
-  if (type < 0 || type >= ndets) type = 0;
+  if (type < 0 || type >= ndets)
+    type = 0;
   setTDRStyle();
   TFile *file1 = new TFile(infile1.c_str());
   TFile *file2 = new TFile(infile2.c_str());
   if ((file1 != nullptr) && (file2 != nullptr)) {
     char name[20], cname[50];
     for (int k = 0; k < nhist; ++k) {
-      sprintf (name, "%s", names[type][k].c_str());
-      sprintf (cname, "c_%s", names[type][k].c_str());
-      TH1D* hist[2];
-      hist[0] = (TH1D*)(file1->FindObjectAny(name));
-      hist[1] = (TH1D*)(file2->FindObjectAny(name));
+      sprintf(name, "%s", names[type][k].c_str());
+      sprintf(cname, "c_%s", names[type][k].c_str());
+      TH1D *hist[2];
+      hist[0] = (TH1D *)(file1->FindObjectAny(name));
+      hist[1] = (TH1D *)(file2->FindObjectAny(name));
       if ((hist[0] != nullptr) && (hist[1] != nullptr)) {
-	TCanvas *pad = new TCanvas(cname, cname, 800, 600);
-	pad->SetRightMargin(0.10);
-	pad->SetTopMargin(0.10);
-	if (logs[k] > 0) pad->SetLogy();
-	TLegend *leg = new TLegend(0.65,0.69,0.89,0.77);
-	leg->SetFillColor(kWhite);
-	double ymax(0.90);
-	for (int i = 0; i < 2; ++i) {
-	  hist[i]->SetLineStyle(style[i]);
-	  hist[i]->SetLineColor(colors[i]);
-	  hist[i]->SetLineWidth(2);
-	  hist[i]->Rebin(nbins[k]);
-	  hist[i]->GetXaxis()->SetTitle(xtitl[k].c_str());
-	  if (xmax[k] > 0)
-	    hist[i]->GetXaxis()->SetRangeUser(0, xmax[k]);
-	  if (i == 0)
-	    hist[i]->Draw("HIST");
-	  else
-	    hist[i]->Draw("HIST sames");
-	  if (i == 0) 
-	    sprintf(name, "%s", text1.c_str());
-	  else
-	    sprintf(name, "%s", text2.c_str());
-	  leg->AddEntry(hist[i], name, "lp");
-	  pad->Update();
-	  TPaveStats* st1 = (TPaveStats*)hist[i]->GetListOfFunctions()->FindObject("stats");
-	  if (st1 != nullptr) {
-	    double ymin = ymax - 0.06;
-	    st1->SetFillColor(kWhite);
-	    st1->SetLineColor(colors[i]);
-	    st1->SetTextColor(colors[i]);
-	    st1->SetY1NDC(ymin); st1->SetY2NDC(ymax);
-	    st1->SetX1NDC(0.70); st1->SetX2NDC(0.90);
-	    ymax = ymin;
-	  }
-	}
-	leg->Draw("same");
-	pad->Update();
-	TPaveText *txt1 = new TPaveText(0.65,0.63,0.89,0.685,"blNDC");
-	txt1->SetFillColor(kWhite);
-	sprintf(name, "%s", dets[type].c_str());
-	txt1->AddText(name);
-	txt1->Draw("same");
-	pad->Update();
-	if (save) {
-	  sprintf (cname, "%s.pdf", pad->GetName());
-	  pad->Print(cname);
-	}
+        TCanvas *pad = new TCanvas(cname, cname, 800, 600);
+        pad->SetRightMargin(0.10);
+        pad->SetTopMargin(0.10);
+        if (logs[k] > 0)
+          pad->SetLogy();
+        TLegend *leg = new TLegend(0.65, 0.69, 0.89, 0.77);
+        leg->SetFillColor(kWhite);
+        double ymax(0.90);
+        for (int i = 0; i < 2; ++i) {
+          hist[i]->SetLineStyle(style[i]);
+          hist[i]->SetLineColor(colors[i]);
+          hist[i]->SetLineWidth(2);
+          hist[i]->Rebin(nbins[k]);
+          hist[i]->GetXaxis()->SetTitle(xtitl[k].c_str());
+          if (xmax[k] > 0)
+            hist[i]->GetXaxis()->SetRangeUser(0, xmax[k]);
+          if (i == 0)
+            hist[i]->Draw("HIST");
+          else
+            hist[i]->Draw("HIST sames");
+          if (i == 0)
+            sprintf(name, "%s", text1.c_str());
+          else
+            sprintf(name, "%s", text2.c_str());
+          leg->AddEntry(hist[i], name, "lp");
+          pad->Update();
+          TPaveStats *st1 = (TPaveStats *)hist[i]->GetListOfFunctions()->FindObject("stats");
+          if (st1 != nullptr) {
+            double ymin = ymax - 0.06;
+            st1->SetFillColor(kWhite);
+            st1->SetLineColor(colors[i]);
+            st1->SetTextColor(colors[i]);
+            st1->SetY1NDC(ymin);
+            st1->SetY2NDC(ymax);
+            st1->SetX1NDC(0.70);
+            st1->SetX2NDC(0.90);
+            ymax = ymin;
+          }
+        }
+        leg->Draw("same");
+        pad->Update();
+        TPaveText *txt1 = new TPaveText(0.65, 0.63, 0.89, 0.685, "blNDC");
+        txt1->SetFillColor(kWhite);
+        sprintf(name, "%s", dets[type].c_str());
+        txt1->AddText(name);
+        txt1->Draw("same");
+        pad->Update();
+        if (save) {
+          sprintf(cname, "%s.pdf", pad->GetName());
+          pad->Print(cname);
+        }
       }
     }
   }
 }
-
