8aabe24354049168baf33c2bd5129127b2d417d7
namppl <kyungwook.nam@cern.ch>
2021-04-20 12:59:33 +0200

    uncommented the verbosity line and changed the names

--- Configuration/Generator/python/ZpToEE_m6000_TuneCP5_14TeV-pythia8_cfi.py
+++ Configuration/Generator/python/ZpToEE_m6000_TuneCP5_14TeV-pythia8_cfi.py
@@ -3,7 +3,7 @@ from Configuration.Generator.Pythia8CommonSettings_cfi import *
 from Configuration.Generator.MCTunes2017.PythiaCP5Settings_cfi import *
 
 generator = cms.EDFilter("Pythia8GeneratorFilter",
-                         #pythiaHepMCVerbosity = cms.untracked.bool(False),
+                         pythiaHepMCVerbosity = cms.untracked.bool(False),
                          comEnergy = cms.double(14000.0),
                          maxEventsToPrint = cms.untracked.int32(0),
                          pythiaPylistVerbosity = cms.untracked.int32(1),
