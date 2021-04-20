e3e594b765b539dfc1f1919580e43fd0b0c35a8a
Marco Rovere <marco.rovere@cern.ch>
2021-04-20 18:20:48 +0200

    Adapt TICL PR configuration to validated plugins

--- RecoHGCal/TICL/python/CLUE3DStep_cff.py
+++ RecoHGCal/TICL/python/CLUE3DStep_cff.py
@@ -19,21 +19,8 @@ filteredLayerClustersCLUE3D = _filteredLayerClustersProducer.clone(
 ticlTrackstersCLUE3D = _trackstersProducer.clone(
     filtered_mask = "filteredLayerClustersCLUE3D:CLUE3D",
     seeding_regions = "ticlSeedingGlobal",
-    filter_on_categories = [0, 1],
-    pid_threshold = 0.5,
-    energy_em_over_total_threshold = 0.9,
-    max_longitudinal_sigmaPCA = 10,
-    shower_start_max_layer = 5, #inclusive
-    max_out_in_hops = 1,
-    skip_layers = 2,
-    max_missing_layers_in_trackster = 1,
-    min_layers_per_trackster = 10,
-    min_cos_theta = 0.97,  # ~14 degrees
-    min_cos_pointing = 0.9, # ~25 degrees
-    max_delta_time = 3.,
     itername = "CLUE3D",
-    algo_verbosity = 0,
-    patternRecognitionAlgo = "CLUE3D"
+    patternRecognitionBy = "CLUE3D"
 )
 
 # MULTICLUSTERS
