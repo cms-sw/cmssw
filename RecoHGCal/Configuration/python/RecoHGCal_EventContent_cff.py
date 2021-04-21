2a1396070abbd18bf070b6b5b96dc0f459748cfe
Leonardo Cristella <leonardo.cristella@cern.ch>
2021-04-21 12:58:19 +0200

    Update TICL event content

--- RecoHGCal/Configuration/python/RecoHGCal_EventContent_cff.py
+++ RecoHGCal/Configuration/python/RecoHGCal_EventContent_cff.py
@@ -1,38 +1,18 @@
 import FWCore.ParameterSet.Config as cms
 
+from RecoHGCal.TICL.iterativeTICL_cff import ticlIterLabelsMerge
 
-#AOD content
-TICL_AOD = cms.PSet(
-  # 13/04/2021 Felice: MultiClusters will be deprecated soon
-    outputCommands = cms.untracked.vstring(
-      'keep *_ticlMultiClustersFromTrackstersEM_*_*',
-      'keep *_ticlMultiClustersFromTrackstersHAD_*_*',
-      'keep *_ticlMultiClustersFromTrackstersTrk_*_*',
-      'keep *_ticlMultiClustersFromTrackstersTrkEM_*_*',
-      'keep *_ticlMultiClustersFromTrackstersMIP_*_*',
-      'keep *_ticlMultiClustersFromTrackstersMerge_*_*',
-      'keep *_ticlMultiClustersFromSimTracksters_*_*',
-      )
-    )
+trackstersIters = ['keep *_ticlTracksters'+iteration+'_*_*' for iteration in ticlIterLabelsMerge]
+trackstersHFNoseIters = ['keep *_ticlTrackstersHFNose'+iteration+'_*_*' for iteration in ticlIterLabelsMerge]
 
 #RECO content
 TICL_RECO = cms.PSet(
     outputCommands = cms.untracked.vstring(
-      'keep *_ticlTrackstersTrkEM_*_*',
-      'keep *_ticlTrackstersEM_*_*',
-      'keep *_ticlTrackstersHAD_*_*',
-      'keep *_ticlTrackstersTrk_*_*',
-      'keep *_ticlTrackstersMIP_*_*',
-      'keep *_ticlTrackstersMerge_*_*',
-      'keep *_ticlTrackstersHFNoseTrkEM_*_*',
-      'keep *_ticlTrackstersHFNoseEM_*_*',
-      'keep *_ticlTrackstersHFNoseMIP_*_*',
-      'keep *_ticlTrackstersHFNoseHAD_*_*',
-      'keep *_ticlTrackstersHFNoseMerge_*_*',
-      'keep *_pfTICL_*_*'
+      trackstersIters +
+      trackstersHFNoseIters +
+      ['keep *_pfTICL_*_*']
       )
     )
-TICL_RECO.outputCommands.extend(TICL_AOD.outputCommands)
 
 # FEVT Content
 TICL_FEVT = cms.PSet(
@@ -41,4 +21,3 @@ TICL_FEVT = cms.PSet(
       )
     )
 TICL_FEVT.outputCommands.extend(TICL_RECO.outputCommands)
-
