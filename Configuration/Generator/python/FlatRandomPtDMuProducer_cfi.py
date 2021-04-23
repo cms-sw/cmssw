9ba1f5bce23dcd8cb85b97e1a8f905bac8ebe8b2
reza goldouzian <reza.goldouzian@cern.ch>
2021-04-23 09:42:06 +0200

    Displaced muon with new parameters for the Phase2 L1 tracker studies - Variables are ordered

--- Configuration/Generator/python/FlatRandomPtDMuProducer_cfi.py
+++ Configuration/Generator/python/FlatRandomPtDMuProducer_cfi.py
@@ -2,23 +2,23 @@ import FWCore.ParameterSet.Config as cms
 
 generator = cms.EDProducer("FlatRandomPtAndDxyGunProducer",
     PGunParameters = cms.PSet(
-        ConeH = cms.double(3000.0),
-        ConeRadius = cms.double(1000.0),
-        DistanceToAPEX = cms.double(100.0),
-        LxyMax = cms.double(300.0),
-        LzMax = cms.double(300.0),
-        MaxEta = cms.double(2.6),
-        MaxPhi = cms.double(3.14159265359),
+        PartID = cms.vint32(-13),
+        MinPt = cms.double(1.5),
         MaxPt = cms.double(8.0),
         MinEta = cms.double(-2.6),
+        MaxEta = cms.double(2.6),
         MinPhi = cms.double(-3.14159265359),
-        MinPt = cms.double(1.5),
-        PartID = cms.vint32(-13),
+        MaxPhi = cms.double(3.14159265359),
+        LxyMax = cms.double(300.0),
+        LzMax = cms.double(300.0),
+        ConeRadius = cms.double(1000.0),
+        ConeH = cms.double(3000.0),
+        DistanceToAPEX = cms.double(100.0),
         dxyMax = cms.double(100.0),
         dxyMin = cms.double(0.0)
     ),
     Verbosity = cms.untracked.int32(0),
-    firstRun = cms.untracked.uint32(1),
+    psethack = cms.string('displaced muon'),
     AddAntiParticle = cms.bool(True),
-    psethack = cms.string('displaced muon')
+    firstRun = cms.untracked.uint32(1)
 )
