1b8cdb6420b356e7ad56b23eb161a2e9fc9a0ee1
Sven Dildick <sven.dildick@cern.ch>
2021-04-23 20:29:49 -0500

    Comments by Slava

--- EventFilter/CSCRawToDigi/python/cscPacker_cfi.py
+++ EventFilter/CSCRawToDigi/python/cscPacker_cfi.py
@@ -9,11 +9,13 @@ cscpacker = cscPackerDef.clone()
 ##
 # packer - simply get rid of it
 from Configuration.Eras.Modifier_run2_common_cff import run2_common
-run2_common.toModify( cscpacker, useFormatVersion = cms.uint32(2013) )
-run2_common.toModify( cscpacker, usePreTriggers = cms.bool(False) )
-run2_common.toModify( cscpacker, packEverything = cms.bool(True) )
+run2_common.toModify( cscpacker,
+                      useFormatVersion = 2013,
+                      usePreTriggers = False,
+                      packEverything = True)
 
 ## in Run-3 include GEMs
 from Configuration.Eras.Modifier_run3_GEM_cff import run3_GEM
-run3_GEM.toModify( cscpacker, padDigiClusterTag = cms.InputTag("simMuonGEMPadDigiClusters") )
-run3_GEM.toModify( cscpacker, useGEMs = cms.bool(False) )
+run3_GEM.toModify( cscpacker,
+                   padDigiClusterTag = "simMuonGEMPadDigiClusters",
+                   useGEMs = False)
