194c06abb1e19035671ea8e9690f0a221b1dd974
Fabio Cossutti <fabio.cossutti@ts.infn.it>
2021-04-26 16:16:04 +0200

    horizontal and vertical shift functions

--- Geometry/MTDNumberingBuilder/interface/MTDTopology.h
+++ Geometry/MTDNumberingBuilder/interface/MTDTopology.h
@@ -13,8 +13,8 @@
 class MTDTopology {
 public:
   struct ETLfaceLayout {
-    int idDiscSide_;  // disc face identifier
-    int idDetType1_;  // module type id identifier for first row
+    uint32_t idDiscSide_;  // disc face identifier
+    uint32_t idDetType1_;  // module type id identifier for first row
 
     std::array<std::vector<int>, 2> start_copy_;  // start copy per row, first of type idDetType1_
     std::array<std::vector<int>, 2> offset_;      // offset per row, first of type idDetType1_
@@ -33,8 +33,8 @@ public:
   // navigation methods in ETL topology, provide the index of the det next to DetId for
   // horizontal and vertical shifts in both directions, assuming the predefined order in a sector
 
-  //size_t hshiftETL(const uint32_t detid, const int horizontalShift) const;
-  //size_t vshiftETL(const uint32_t detid, const int verticalShift) const;
+  size_t hshiftETL(const uint32_t detid, const int horizontalShift) const;
+  size_t vshiftETL(const uint32_t detid, const int verticalShift) const;
 
 private:
   const int mtdTopologyMode_;
