d96955f6906045f30ac74794927378fb8dce4cee
Fabio Cossutti <fabio.cossutti@ts.infn.it>
2021-04-26 11:55:49 +0200

    Add ETL structure information to MTDTopology

--- Geometry/MTDNumberingBuilder/interface/MTDTopology.h
+++ Geometry/MTDNumberingBuilder/interface/MTDTopology.h
@@ -4,18 +4,33 @@
 #include "DataFormats/DetId/interface/DetId.h"
 #include "DataFormats/ForwardDetId/interface/ForwardSubdetector.h"
 #include "DataFormats/ForwardDetId/interface/MTDDetId.h"
+#include "DataFormats/ForwardDetId/interface/ETLDetId.h"
 
 #include <vector>
 #include <string>
 
 class MTDTopology {
 public:
-  MTDTopology(const int &topologyMode);
+  struct ETLfaceLayout {
+    int idDiscSide_;
+    int idDetType1_;
+
+    std::vector<int> start_copy_1_;
+    std::vector<int> start_copy_2_;
+    std::vector<int> offset_1_;
+    std::vector<int> offset_2_;
+  };
+
+  using ETLValues = std::vector<ETLfaceLayout>;
+
+  MTDTopology(const int& topologyMode, const ETLValues& etl);
 
   int getMTDTopologyMode() const { return mtdTopologyMode_; }
 
 private:
   const int mtdTopologyMode_;
+
+  const ETLValues etlVals_;
 };
 
 #endif
