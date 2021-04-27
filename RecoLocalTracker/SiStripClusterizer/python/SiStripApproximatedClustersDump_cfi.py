0ceb15c3294751ec5b602be93132617ab1e072d2
Ivan <icali@cern.ch>
2021-04-27 18:12:45 +0200

    fising cluster dump config

--- RecoLocalTracker/SiStripClusterizer/python/SiStripApproximatedClustersDump_cfi.py
+++ RecoLocalTracker/SiStripClusterizer/python/SiStripApproximatedClustersDump_cfi.py
@@ -1,5 +1,5 @@
 import FWCore.ParameterSet.Config as cms
 
 SiStripApproximatedClustersDump = cms.EDAnalyzer("SiStripApproximatedClustersDump",
-    approximatedClustersTag = cms.InputTag("SiStripClusters2ApproxClustersv3")
+    approximatedClustersTag = cms.InputTag("SiStripClusters2ApproxClustersv1")
 )
