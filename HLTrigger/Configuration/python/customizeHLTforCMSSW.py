b144ade45aed1a26ff4b07b4ec23a68de3436b9c
Jeongeun Lee <jeongeun.lee@cern.ch>
2021-04-28 12:06:58 +0200

    fix customization

--- HLTrigger/Configuration/python/customizeHLTforCMSSW.py
+++ HLTrigger/Configuration/python/customizeHLTforCMSSW.py
@@ -142,5 +142,6 @@ def customizeHLTforCMSSW(process, menuType="GRun"):
 
     # add call to action function in proper order: newest last!
     # process = customiseFor12718(process)
+    process = customizeFor33543(process)
 
     return process
