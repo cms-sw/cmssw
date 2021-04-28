d37f54dcca341596429429b1fa5e049062e1b2d1
Mia <mia.tosi@cern.ch>
2021-04-28 09:45:42 +0200

    remove old W and W' CUETP8M1 tune configs

--- Configuration/Generator/python/WM_14TeV_TuneCP5_cfi.py
+++ Configuration/Generator/python/WM_14TeV_TuneCP5_cfi.py
@@ -1,24 +1,21 @@
 import FWCore.ParameterSet.Config as cms
 from Configuration.Generator.Pythia8CommonSettings_cfi import *
-from Configuration.Generator.Pythia8CUEP8M1Settings_cfi import *
+from Configuration.Generator.MCTunes2017.PythiaCP5Settings_cfi import *
 
 generator = cms.EDFilter("Pythia8GeneratorFilter",
+                         pythiaPylistVerbosity = cms.untracked.int32(0),
+                         filterEfficiency = cms.untracked.double(1.0),
                          pythiaHepMCVerbosity = cms.untracked.bool(False),
-                         maxEventsToPrint = cms.untracked.int32(0),
-                         pythiaPylistVerbosity = cms.untracked.int32(1),
-                         filterEfficiency = cms.untracked.double(1.),
-                         crossSection = cms.untracked.double(1.),
+                         crossSection = cms.untracked.double(17120.0),
                          comEnergy = cms.double(14000.0),
+                         maxEventsToPrint = cms.untracked.int32(0),
                          PythiaParameters = cms.PSet(
         pythia8CommonSettingsBlock,
-        pythia8CUEP8M1SettingsBlock,
+        pythia8CP5SettingsBlock,
         processParameters = cms.vstring(
-            'NewGaugeBoson:ffbar2Wprime = on',
-            '34:m0 = 2000 ',
-            '34:onMode = off',
-            '34:onIfAny = 11 12',
-            '34:onIfAny = 13 14',
-            '34:onIfAny = 15 16',
+            'WeakSingleBoson:ffbar2W = on',
+            '24:onMode = off',
+            '24:onIfAny = 13 14',
             ),
         parameterSets = cms.vstring('pythia8CommonSettings',
                                     'pythia8CUEP8M1Settings',
