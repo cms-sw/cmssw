de7bf73928c97c01936a751fb9369d05dffcb1cf
Fabio Cossutti <fabio.cossutti@ts.infn.it>
2021-04-29 17:19:17 +0200

    Update for missing vshift, provide and use ifno about closest module in row if present

--- RecoMTD/DetLayers/test/TestETLNavigation.cc
+++ RecoMTD/DetLayers/test/TestETLNavigation.cc
@@ -84,10 +84,20 @@ void TestETLNavigation::analyze(const Event& ev, const EventSetup& es) {
           }
         }
         for (int iside = -1; iside <= 1; iside += 2) {
-          size_t idetNew = isector->vshift(modId, iside);
+          size_t closest(isector->basicComponents().size());
+          size_t idetNew = isector->vshift(modId, iside, closest);
           if (idetNew >= isector->basicComponents().size()) {
             LogVerbatim("MTDLayerDump") << "...............vshift= " << std::fixed << std::setw(2) << iside
                                         << " out of range";
+            if (closest < isector->basicComponents().size()) {
+              ETLDetId newId(isector->basicComponents()[closest]->geographicalId().rawId());
+              LogVerbatim("MTDLayerDump")
+                  << std::fixed << ".......closest.vshift= " << std::setw(2) << iside << " side = " << std::setw(4)
+                  << newId.mtdSide() << " Disc/Side/Sector = " << std::setw(4) << newId.nDisc() << " " << std::setw(4)
+                  << newId.discSide() << " " << std::setw(4) << newId.sector() << " mod/type = " << std::setw(4)
+                  << newId.module() << " " << std::setw(4) << newId.modType()
+                  << " pos = " << isector->basicComponents()[closest]->position();
+            }
           } else {
             ETLDetId newId(isector->basicComponents()[idetNew]->geographicalId().rawId());
             LogVerbatim("MTDLayerDump") << std::fixed << "...............vshift= " << std::setw(2) << iside
