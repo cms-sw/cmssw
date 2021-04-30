3341c97f759905c4e696a59d12c0e934ae5246b8
Giulia Sorrentino <giulia.sorrentino@cern.ch>
2021-04-30 11:54:08 +0200

    Initializing parameters in the plugin

--- GeneratorInterface/GenFilters/python/LHEmttFilter_cfi.py
+++ GeneratorInterface/GenFilters/python/LHEmttFilter_cfi.py
@@ -2,7 +2,7 @@ import FWCore.ParameterSet.Config as cms
 
 lhemttFilter = cms.EDFilter("LHEmttFilter",
     src = cms.InputTag("source"),
-    MinInvMass = cms.double(-1),
+    MinInvMass = cms.double(1000),
     MaxInvMass = cms.double(-1),
     ptMin = cms.double(0)
 )
