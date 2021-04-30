f602bdd526d4a6e8d63474d136711e2bafc3efc8
reza goldouzian <reza.goldouzian@cern.ch>
2021-04-30 12:14:59 +0200

    Single -> Double Names are updated

--- Configuration/Generator/python/DoubleElectronFlatPt1p5To8_cfi.py
+++ Configuration/Generator/python/DoubleElectronFlatPt1p5To8_cfi.py
@@ -4,15 +4,16 @@ generator = cms.EDFilter("Pythia8PtGun",
                          PGunParameters = cms.PSet(
         MaxPt = cms.double(8.0),
         MinPt = cms.double(1.5),
-        ParticleID = cms.vint32(-13),
+        ParticleID = cms.vint32(11),
         AddAntiParticle = cms.bool(True),
-        MaxEta = cms.double(2.85),
+        MaxEta = cms.double(3.1),
         MaxPhi = cms.double(3.14159265359),
-        MinEta = cms.double(-2.85),
+        MinEta = cms.double(-3.1),
         MinPhi = cms.double(-3.14159265359) ## in radians
         ),
                          Verbosity = cms.untracked.int32(0), ## set to 1 (or greater)  for printouts
-                         psethack = cms.string('single mu pt 1.5 to 8'),
+                         psethack = cms.string('double electron pt 1.5 to 8'),
                          firstRun = cms.untracked.uint32(1),
                          PythiaParameters = cms.PSet(parameterSets = cms.vstring())
                          )
+
