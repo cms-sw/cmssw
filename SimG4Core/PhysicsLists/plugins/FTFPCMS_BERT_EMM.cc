fe490ee09c6c4a374a5abb1e7c13ee42642a41b0
Vladimir <Vladimir.Ivantchenko@cern.ch>
2021-05-05 12:35:09 +0200

    code format

--- SimG4Core/PhysicsLists/plugins/FTFPCMS_BERT_EMM.cc
+++ SimG4Core/PhysicsLists/plugins/FTFPCMS_BERT_EMM.cc
@@ -17,8 +17,7 @@ FTFPCMS_BERT_EMM::FTFPCMS_BERT_EMM(const edm::ParameterSet& p) : PhysicsList(p) 
   double maxBERT = p.getParameter<double>("EmaxBERT") * CLHEP::GeV;
   double maxBERTpi = p.getParameter<double>("EmaxBERTpi") * CLHEP::GeV;
   edm::LogVerbatim("PhysicsList") << "CMS Physics List FTFP_BERT_EMM: "
-				  << "\n Flags for EM Physics: " << emPhys 
-                                  << "; Hadronic Physics: " << hadPhys 
+                                  << "\n Flags for EM Physics: " << emPhys << "; Hadronic Physics: " << hadPhys
                                   << "\n Transition energy Bertini/FTFP from " << minFTFP / CLHEP::GeV << " to "
                                   << maxBERT / CLHEP::GeV << "; for pions to " << maxBERTpi / CLHEP::GeV << " GeV";
 
