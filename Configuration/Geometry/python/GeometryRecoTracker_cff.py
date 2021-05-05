70dabfa37761236585e20f0ce1456d7f11242d6c
Adinda de Wit <adinda.maite.de.wit@cern.ch>
2021-05-05 12:02:06 +0200

    Adding T27 and support for brickedness + new record to store this information

--- Configuration/Geometry/python/GeometryRecoTracker_cff.py
+++ Configuration/Geometry/python/GeometryRecoTracker_cff.py
@@ -6,6 +6,7 @@ from Geometry.CommonTopologies.globalTrackingGeometry_cfi import *
 
 # Tracker
 from RecoTracker.GeometryESProducer.TrackerRecoGeometryESProducer_cfi import *
+from Geometry.TrackerGeometryBuilder.TrackerAdditionalParametersPerDet_cfi import *
 
 # Alignment
 from Geometry.TrackerGeometryBuilder.idealForDigiTrackerGeometry_cff import *
