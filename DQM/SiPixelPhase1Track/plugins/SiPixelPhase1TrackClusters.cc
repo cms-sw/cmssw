0f6c4da312f2b31db1910ca53406324899acc2cc
Alessandro Rossi <alessandro.rossi@cern.ch>
2021-05-06 10:59:39 +0200

    code-format patch

--- DQM/SiPixelPhase1Track/plugins/SiPixelPhase1TrackClusters.cc
+++ DQM/SiPixelPhase1Track/plugins/SiPixelPhase1TrackClusters.cc
@@ -113,7 +113,8 @@ namespace {
     trackerGeomToken_ = esConsumes<TrackerGeometry, TrackerDigiGeometryRecord>();
     clusterShapeHitFilterToken_ =
         esConsumes<ClusterShapeHitFilter, CkfComponentsRecord>(edm::ESInputTag("", "ClusterShapeHitFilter"));
-    templateDBobjectToken_ = esConsumes<SiPixelTemplateDBObject, SiPixelTemplateDBObjectESProducerRcd,edm::Transition::BeginRun>();
+    templateDBobjectToken_ =
+        esConsumes<SiPixelTemplateDBObject, SiPixelTemplateDBObjectESProducerRcd, edm::Transition::BeginRun>();
   }
 
   void SiPixelPhase1TrackClusters::dqmBeginRun(const edm::Run& iRun, const edm::EventSetup& iSetup) {
