ff24f236b73b65e4236773a631c8949744ca5bc2
Fabio Cossutti <fabio.cossutti@ts.infn.it>
2021-05-07 18:07:06 +0200

    Technical fixes to DataFormats/FTLRecHit and RecoLocalFastTime

--- RecoLocalFastTime/FTLClusterizer/plugins/MTDClusterProducer.cc
+++ RecoLocalFastTime/FTLClusterizer/plugins/MTDClusterProducer.cc
@@ -74,8 +74,7 @@ MTDClusterProducer::MTDClusterProducer(edm::ParameterSet const& conf)
       etlHits_(consumes<FTLRecHitCollection>(conf.getParameter<edm::InputTag>("srcEndcap"))),
       ftlbInstance_(conf.getParameter<std::string>("BarrelClusterName")),
       ftleInstance_(conf.getParameter<std::string>("EndcapClusterName")),
-      clusterMode_(conf.getParameter<std::string>("ClusterMode")),
-      clusterizer_(nullptr)  // the default, in case we fail to make one
+      clusterMode_(conf.getParameter<std::string>("ClusterMode"))
 {
   //--- Declare to the EDM what kind of collections we will be making.
   produces<FTLClusterCollection>(ftlbInstance_);
