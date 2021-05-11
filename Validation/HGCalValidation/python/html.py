c9a4d02f0d223ee6fadc3647077be165e68831e8
Andreas Psallidas <andreas.psallidas@cern.ch>
2021-05-11 13:31:30 +0200

    update scripts

--- Validation/HGCalValidation/python/html.py
+++ Validation/HGCalValidation/python/html.py
@@ -98,7 +98,7 @@ _pageNameMap = {
     "hitCalibration": "Reconstructed hits calibration",
     "hitValidation" : "Simulated hits, digis, reconstructed hits validation" , 
     "hgcalLayerClusters": "Layer clusters",
-    "allTiclMultiClusters":"TICL multiclusters", 
+    "tracksters":"Tracksters", 
     "ticlMultiClustersFromTrackstersEM": "Electromagnetic multiclusters",
     "ticlMultiClustersFromTrackstersHAD": "Hadronic multiclusters",
     "hgcalMultiClusters" : "Old multiclusters",
@@ -114,7 +114,7 @@ _sectionNameMapOrder = collections.OrderedDict([
     ("ticlMultiClustersFromTrackstersEM","Electromagnetic multiclusters"),
     # ticlMultiClustersFromTrackstersHAD
     ("ticlMultiClustersFromTrackstersHAD","Hadronic multiclusters"),
-    ("allTiclMultiClusters","TICL multiclusters"),
+    ("tracksters","Tracksters"),
     # hgcalMultiClusters
     ("hgcalMultiClusters","Old multiclusters"),
 ])
@@ -124,7 +124,7 @@ _summary = {}
 
 #Objects to keep in summary
 #_summobj = ['hitCalibration','hitValidation', 'hgcalLayerClusters','ticlMultiClustersFromTrackstersEM','ticlMultiClustersFromTrackstersHAD']
-_summobj = ['hitCalibration','hitValidation', 'hgcalLayerClusters','allTiclMultiClusters']
+_summobj = ['hitCalibration','hitValidation', 'hgcalLayerClusters','tracksters']
 #_summobj = ['hitCalibration','hitValidation', 'hgcalLayerClusters']
 
 #Plots to keep in summary from hitCalibration
