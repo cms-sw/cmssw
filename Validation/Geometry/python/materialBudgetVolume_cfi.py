e274db54558573dcbfee674f8e5abbfd92d44b2b
Sunanda <sunanda.banerjee@cern.ch>
2021-05-12 13:51:57 +0200

    Bug fix for the material budget code and add a macro for getting the plots

--- Validation/Geometry/python/materialBudgetVolume_cfi.py
+++ Validation/Geometry/python/materialBudgetVolume_cfi.py
@@ -3,8 +3,8 @@ from SimG4Core.Configuration.SimG4Core_cff import *
 
 g4SimHits.Watchers = cms.VPSet(cms.PSet(
     MaterialBudgetVolume = cms.PSet(
-        lvNames = cms.vstring('BEAM', 'BEAM1', 'BEAM2', 'BEAM3', 'BEAM4', 'Tracker', 'ECAL', 'HCal', 'VCAL', 'MUON', 'HGCAL', 'MGNT', 'OQUA'),
-        lvLevels = cms.vint32(3, 3, 3, 3, 3, 3, 4, 4, 3, 3, 8, 3, 3),
+        lvNames = cms.vstring('BEAM', 'BEAM1', 'BEAM2', 'BEAM3', 'BEAM4', 'Tracker', 'ECAL', 'HCal', 'VCAL', 'MGNT', 'MUON', 'OQUA', 'CALOEC'),
+        lvLevels = cms.vint32(3, 3, 3, 3, 3, 3, 4, 4, 3, 4, 3, 3, 4),
         useDD4Hep = cms.bool(False),
     ),
     type = cms.string('MaterialBudgetVolume')
