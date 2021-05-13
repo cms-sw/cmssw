a3612b9f645f090936f5e19db877781d8ec0ec1b
ccaillol <ccaillol@cern.ch>
2021-05-13 10:18:22 +0200

    remove useless cfi

--- L1Trigger/Phase2L1ParticleFlow/python/L1METPFProducer_cfi.py
+++
@@ -1,6 +0,0 @@
-import FWCore.ParameterSet.Config as cms
-
-L1METPFProducer = cms.EDProducer("L1METPFProducer",
-                                 L1PFObjects = cms.InputTag("L1PFProducer","l1pfCandidates"),
-                                 maxCandidates = cms.int32(128),
-)
