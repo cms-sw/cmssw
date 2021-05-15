5ee4b99e08d9aecbb6aa99e44489f73a59679b40
Cms Build <cmsbuild@cern.ch>
2021-05-15 17:16:43 +0200

    remove redundant variable and init an array

--- L1Trigger/L1TTrackMatch/interface/L1TrackJetProducer.h
+++ L1Trigger/L1TTrackMatch/interface/L1TrackJetProducer.h
@@ -8,21 +8,21 @@
 //Each individual box in the eta and phi dimension.
 //  Also used to store final cluster data for each zbin.
 struct EtaPhiBin {
-  float pTtot = 0;
-  int numtracks = 0;
-  int numttrks = 0;
-  int numtdtrks = 0;
-  int numttdtrks = 0;
-  bool used = true;
-  float phi = 0;  //average phi value (halfway b/t min and max)
-  float eta = 0;  //average eta value
+  float pTtot;
+  int numtracks;
+  int numttrks;
+  int numtdtrks;
+  int numttdtrks;
+  bool used;
+  float phi;  //average phi value (halfway b/t min and max)
+  float eta;  //average eta value
 };
 
 //store important information for plots
 struct MaxZBin {
-  int znum = 0;    //Numbered from 0 to nzbins (16, 32, or 64) in order
-  int nclust = 0;  //number of clusters in this bin
-  float zbincenter = 0;
-  EtaPhiBin *clusters = nullptr;  //list of all the clusters in this bin
-  float ht = 0;                   //sum of all cluster pTs--only the zbin with the maximum ht is stored
+  int znum;    //Numbered from 0 to nzbins (16, 32, or 64) in order
+  int nclust;  //number of clusters in this bin
+  float zbincenter;
+  EtaPhiBin *clusters;  //list of all the clusters in this bin
+  float ht;             //sum of all cluster pTs--only the zbin with the maximum ht is stored
 };
