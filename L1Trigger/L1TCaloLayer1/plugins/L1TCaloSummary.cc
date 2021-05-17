6ec182398ccc9ae82c0cc4b2a2261a33cc46db2d
pallabidas <pallabi91@gmail.com>
2021-05-17 21:42:17 +0200

    attempting to read regions from layer1
    (cherry picked from commit 885606b4fc8e5895d72cb7ba983bb14673a1019e)

--- L1Trigger/L1TCaloLayer1/plugins/L1TCaloSummary.cc
+++ L1Trigger/L1TCaloLayer1/plugins/L1TCaloSummary.cc
@@ -85,29 +85,29 @@ private:
 
   // ----------member data ---------------------------
 
-  edm::EDGetTokenT<EcalTrigPrimDigiCollection> ecalTPSource;
-  std::string ecalTPSourceLabel;
-  edm::EDGetTokenT<HcalTrigPrimDigiCollection> hcalTPSource;
-  std::string hcalTPSourceLabel;
-
-  std::vector<std::array<std::array<std::array<uint32_t, nEtBins>, nCalSideBins>, nCalEtaBins> > ecalLUT;
-  std::vector<std::array<std::array<std::array<uint32_t, nEtBins>, nCalSideBins>, nCalEtaBins> > hcalLUT;
-  std::vector<std::array<std::array<uint32_t, nEtBins>, nHfEtaBins> > hfLUT;
-  std::vector<unsigned int> ePhiMap;
-  std::vector<unsigned int> hPhiMap;
-  std::vector<unsigned int> hfPhiMap;
+//  edm::EDGetTokenT<EcalTrigPrimDigiCollection> ecalTPSource;
+//  std::string ecalTPSourceLabel;
+//  edm::EDGetTokenT<HcalTrigPrimDigiCollection> hcalTPSource;
+//  std::string hcalTPSourceLabel;
+//
+//  std::vector<std::array<std::array<std::array<uint32_t, nEtBins>, nCalSideBins>, nCalEtaBins> > ecalLUT;
+//  std::vector<std::array<std::array<std::array<uint32_t, nEtBins>, nCalSideBins>, nCalEtaBins> > hcalLUT;
+//  std::vector<std::array<std::array<uint32_t, nEtBins>, nHfEtaBins> > hfLUT;
+//  std::vector<unsigned int> ePhiMap;
+//  std::vector<unsigned int> hPhiMap;
+//  std::vector<unsigned int> hfPhiMap;
 
   uint32_t nPumBins;
 
   std::vector< std::vector< std::vector < uint32_t > > > pumLUT;
 
-  std::vector< UCTTower* > twrList;
-
-  bool useLSB;
-  bool useCalib;
-  bool useECALLUT;
-  bool useHCALLUT;
-  bool useHFLUT;
+//  std::vector< UCTTower* > twrList;
+//
+//  bool useLSB;
+//  bool useCalib;
+//  bool useECALLUT;
+//  bool useHCALLUT;
+//  bool useHFLUT;
 
   double caloScaleFactor;
 
@@ -120,6 +120,8 @@ private:
 
   bool verbose;
   int fwVersion;
+
+  edm::EDGetTokenT<L1CaloRegionCollection> regionToken;
 
   UCTLayer1 *layer1;
   UCTSummaryCard *summaryCard;
@@ -138,20 +140,20 @@ private:
 // constructors and destructor
 //
 L1TCaloSummary::L1TCaloSummary(const edm::ParameterSet& iConfig) :
-  ecalTPSource(consumes<EcalTrigPrimDigiCollection>(iConfig.getParameter<edm::InputTag>("ecalToken"))),
-  ecalTPSourceLabel(iConfig.getParameter<edm::InputTag>("ecalToken").label()),
-  hcalTPSource(consumes<HcalTrigPrimDigiCollection>(iConfig.getParameter<edm::InputTag>("hcalToken"))),
-  hcalTPSourceLabel(iConfig.getParameter<edm::InputTag>("hcalToken").label()),
-  ePhiMap(72 * 2, 0),
-  hPhiMap(72 * 2, 0),
-  hfPhiMap(72 * 2, 0),
+//  ecalTPSource(consumes<EcalTrigPrimDigiCollection>(iConfig.getParameter<edm::InputTag>("ecalToken"))),
+//  ecalTPSourceLabel(iConfig.getParameter<edm::InputTag>("ecalToken").label()),
+//  hcalTPSource(consumes<HcalTrigPrimDigiCollection>(iConfig.getParameter<edm::InputTag>("hcalToken"))),
+//  hcalTPSourceLabel(iConfig.getParameter<edm::InputTag>("hcalToken").label()),
+//  ePhiMap(72 * 2, 0),
+//  hPhiMap(72 * 2, 0),
+//  hfPhiMap(72 * 2, 0),
   nPumBins(iConfig.getParameter<unsigned int>("nPumBins")),
   pumLUT(nPumBins, std::vector< std::vector<uint32_t> >(2, std::vector<uint32_t>(13))),
-  useLSB(iConfig.getParameter<bool>("useLSB")),
-  useCalib(iConfig.getParameter<bool>("useCalib")),
-  useECALLUT(iConfig.getParameter<bool>("useECALLUT")),
-  useHCALLUT(iConfig.getParameter<bool>("useHCALLUT")),
-  useHFLUT(iConfig.getParameter<bool>("useHFLUT")),
+//  useLSB(iConfig.getParameter<bool>("useLSB")),
+//  useCalib(iConfig.getParameter<bool>("useCalib")),
+//  useECALLUT(iConfig.getParameter<bool>("useECALLUT")),
+//  useHCALLUT(iConfig.getParameter<bool>("useHCALLUT")),
+//  useHFLUT(iConfig.getParameter<bool>("useHFLUT")),
   caloScaleFactor(iConfig.getParameter<double>("caloScaleFactor")),
   jetSeed(iConfig.getParameter<unsigned int>("jetSeed")),
   tauSeed(iConfig.getParameter<unsigned int>("tauSeed")),
@@ -160,7 +162,8 @@ L1TCaloSummary::L1TCaloSummary(const edm::ParameterSet& iConfig) :
   eGammaIsolationFactor(iConfig.getParameter<double>("eGammaIsolationFactor")),
   boostedJetPtFactor(iConfig.getParameter<double>("boostedJetPtFactor")),
   verbose(iConfig.getParameter<bool>("verbose")),
-  fwVersion(iConfig.getParameter<int>("firmwareVersion"))
+  fwVersion(iConfig.getParameter<int>("firmwareVersion")),
+  regionToken(consumes<L1CaloRegionCollection>( edm::InputTag("l1tCaloLayer1Digis","","L1TCaloSummaryTest") ))
 {
   std::vector<double> pumLUTData;
   char pumLUTString[10];
@@ -179,25 +182,26 @@ L1TCaloSummary::L1TCaloSummary(const edm::ParameterSet& iConfig) :
     }
   }
   produces< L1JetParticleCollection >( "Boosted" ) ;
-  layer1 = new UCTLayer1(fwVersion);
-  summaryCard = new UCTSummaryCard(layer1, &pumLUT, jetSeed, tauSeed, tauIsolationFactor, eGammaSeed, eGammaIsolationFactor);
-  vector<UCTCrate*> crates = layer1->getCrates();
-  for(uint32_t crt = 0; crt < crates.size(); crt++) {
-    vector<UCTCard*> cards = crates[crt]->getCards();
-    for(uint32_t crd = 0; crd < cards.size(); crd++) {
-      vector<UCTRegion*> regions = cards[crd]->getRegions();
-      for(uint32_t rgn = 0; rgn < regions.size(); rgn++) {
-	vector<UCTTower*> towers = regions[rgn]->getTowers();
-	for(uint32_t twr = 0; twr < towers.size(); twr++) {
-	  twrList.push_back(towers[twr]);
-	}
-      }
-    }
-  }
+  summaryCard = new UCTSummaryCard(&pumLUT, jetSeed, tauSeed, tauIsolationFactor, eGammaSeed, eGammaIsolationFactor);
+//  layer1 = new UCTLayer1(fwVersion);
+//  summaryCard = new UCTSummaryCard(layer1, &pumLUT, jetSeed, tauSeed, tauIsolationFactor, eGammaSeed, eGammaIsolationFactor);
+//  vector<UCTCrate*> crates = layer1->getCrates();
+//  for(uint32_t crt = 0; crt < crates.size(); crt++) {
+//    vector<UCTCard*> cards = crates[crt]->getCards();
+//    for(uint32_t crd = 0; crd < cards.size(); crd++) {
+//      vector<UCTRegion*> regions = cards[crd]->getRegions();
+//      for(uint32_t rgn = 0; rgn < regions.size(); rgn++) {
+//	vector<UCTTower*> towers = regions[rgn]->getTowers();
+//	for(uint32_t twr = 0; twr < towers.size(); twr++) {
+//	  twrList.push_back(towers[twr]);
+//	}
+//      }
+//    }
+//  }
 }
 
 L1TCaloSummary::~L1TCaloSummary() {
-  if(layer1 != 0) delete layer1;
+//  if(layer1 != 0) delete layer1;
   if(summaryCard != 0) delete summaryCard;
 }
 
@@ -211,91 +215,123 @@ L1TCaloSummary::produce(edm::Event& iEvent, const edm::EventSetup& iSetup)
 {
   using namespace edm;
 
-  edm::Handle<EcalTrigPrimDigiCollection> ecalTPs;
-  iEvent.getByToken(ecalTPSource, ecalTPs);
-  edm::Handle<HcalTrigPrimDigiCollection> hcalTPs;
-  iEvent.getByToken(hcalTPSource, hcalTPs);
+//  edm::Handle<EcalTrigPrimDigiCollection> ecalTPs;
+//  iEvent.getByToken(ecalTPSource, ecalTPs);
+//  edm::Handle<HcalTrigPrimDigiCollection> hcalTPs;
+//  iEvent.getByToken(hcalTPSource, hcalTPs);
 
   std::unique_ptr<L1JetParticleCollection> bJetCands(new L1JetParticleCollection);
 
-  uint32_t expectedTotalET = 0;
+//  uint32_t expectedTotalET = 0;
+//
+//  if(!layer1->clearEvent()) {
+//    std::cerr << "UCT: Failed to clear event" << std::endl;
+//    exit(1);
+//  }
+//
+//  for ( const auto& ecalTp : *ecalTPs ) {
+//    int caloEta = ecalTp.id().ieta();
+//    int caloPhi = ecalTp.id().iphi();
+//    int et = ecalTp.compressedEt();
+//    bool fgVeto = ecalTp.fineGrain();
+//    if(et != 0) {
+//      UCTTowerIndex t = UCTTowerIndex(caloEta, caloPhi);
+//      if(!layer1->setECALData(t,fgVeto,et)) {
+//	std::cerr << "UCT: Failed loading an ECAL tower" << std::endl;
+//	return;
+//      }
+//      expectedTotalET += et;
+//    }
+//  }
+//
+//  for ( const auto& hcalTp : *hcalTPs ) {
+//    int caloEta = hcalTp.id().ieta();
+//    uint32_t absCaloEta = abs(caloEta);
+//    // Tower 29 is not used by Layer-1
+//    if(absCaloEta == 29) {
+//      continue;
+//    }
+//    // Prevent usage of HF TPs with Layer-1 emulator if HCAL TPs are old style
+//    else if(hcalTp.id().version() == 0 && absCaloEta > 29) {
+//      continue;
+//    }
+//    else if(absCaloEta <= 41) {
+//      int caloPhi = hcalTp.id().iphi();
+//      if(caloPhi <= 72) {
+//	int et = hcalTp.SOI_compressedEt();
+//	bool fg = hcalTp.SOI_fineGrain();
+//	if(et != 0) {
+//	  UCTTowerIndex t = UCTTowerIndex(caloEta, caloPhi);
+//	  uint32_t featureBits = 0;
+//	  if(fg) featureBits = 0x1F; // Set all five feature bits for the moment - they are not defined in HW / FW yet!
+//	  if(!layer1->setHCALData(t, featureBits, et)) {
+//	    std::cerr << "caloEta = " << caloEta << "; caloPhi =" << caloPhi << std::endl;
+//	    std::cerr << "UCT: Failed loading an HCAL tower" << std::endl;
+//	    return;
+//	    
+//	  }
+//	  expectedTotalET += et;
+//	}
+//      }
+//      else {
+//	std::cerr << "Illegal Tower: caloEta = " << caloEta << "; caloPhi =" << caloPhi << std::endl;	
+//      }
+//    }
+//    else {
+//      std::cerr << "Illegal Tower: caloEta = " << caloEta << std::endl;
+//    }
+//  }
+//
+//  if(!layer1->process()) {
+//    std::cerr << "UCT: Failed to process layer 1" << std::endl;
+//    exit(1);
+//  }
+//
+//  // Crude check if total ET is approximately OK!
+//  // We can't expect exact match as there is region level saturation to 10-bits
+//  // 1% is good enough
+//  int diff = abs((int)layer1->et() - (int)expectedTotalET);
+//  if(verbose && diff > 0.01 * expectedTotalET ) {
+//    print();
+//    std::cout << "Expected " 
+//	      << std::showbase << std::internal << std::setfill('0') << std::setw(10) << std::hex
+//	      << expectedTotalET << std::dec << std::endl;
+//  }
 
-  if(!layer1->clearEvent()) {
-    std::cerr << "UCT: Failed to clear event" << std::endl;
-    exit(1);
-  }
-
-  for ( const auto& ecalTp : *ecalTPs ) {
-    int caloEta = ecalTp.id().ieta();
-    int caloPhi = ecalTp.id().iphi();
-    int et = ecalTp.compressedEt();
-    bool fgVeto = ecalTp.fineGrain();
-    if(et != 0) {
-      UCTTowerIndex t = UCTTowerIndex(caloEta, caloPhi);
-      if(!layer1->setECALData(t,fgVeto,et)) {
-	std::cerr << "UCT: Failed loading an ECAL tower" << std::endl;
-	return;
-      }
-      expectedTotalET += et;
-    }
-  }
-
-  for ( const auto& hcalTp : *hcalTPs ) {
-    int caloEta = hcalTp.id().ieta();
-    uint32_t absCaloEta = abs(caloEta);
-    // Tower 29 is not used by Layer-1
-    if(absCaloEta == 29) {
-      continue;
-    }
-    // Prevent usage of HF TPs with Layer-1 emulator if HCAL TPs are old style
-    else if(hcalTp.id().version() == 0 && absCaloEta > 29) {
-      continue;
-    }
-    else if(absCaloEta <= 41) {
-      int caloPhi = hcalTp.id().iphi();
-      if(caloPhi <= 72) {
-	int et = hcalTp.SOI_compressedEt();
-	bool fg = hcalTp.SOI_fineGrain();
-	if(et != 0) {
-	  UCTTowerIndex t = UCTTowerIndex(caloEta, caloPhi);
-	  uint32_t featureBits = 0;
-	  if(fg) featureBits = 0x1F; // Set all five feature bits for the moment - they are not defined in HW / FW yet!
-	  if(!layer1->setHCALData(t, featureBits, et)) {
-	    std::cerr << "caloEta = " << caloEta << "; caloPhi =" << caloPhi << std::endl;
-	    std::cerr << "UCT: Failed loading an HCAL tower" << std::endl;
-	    return;
-	    
-	  }
-	  expectedTotalET += et;
-	}
-      }
-      else {
-	std::cerr << "Illegal Tower: caloEta = " << caloEta << "; caloPhi =" << caloPhi << std::endl;	
-      }
-    }
-    else {
-      std::cerr << "Illegal Tower: caloEta = " << caloEta << std::endl;
-    }
-  }
-
-  if(!layer1->process()) {
-    std::cerr << "UCT: Failed to process layer 1" << std::endl;
-    exit(1);
-  }
-
-  // Crude check if total ET is approximately OK!
-  // We can't expect exact match as there is region level saturation to 10-bits
-  // 1% is good enough
-  int diff = abs((int)layer1->et() - (int)expectedTotalET);
-  if(verbose && diff > 0.01 * expectedTotalET ) {
-    print();
-    std::cout << "Expected " 
-	      << std::showbase << std::internal << std::setfill('0') << std::setw(10) << std::hex
-	      << expectedTotalET << std::dec << std::endl;
-  }
- 
   UCTGeometry g;
 
+  summaryCard->clearRegions();
+  std::vector<UCTRegion*> inputRegions;
+  inputRegions.clear();
+  //edm::Handle<L1CaloRegionCollection> regionCollection;
+  edm::Handle<std::vector<L1CaloRegion>> regionCollection;
+  if(!iEvent.getByToken(regionToken, regionCollection)) std::cerr << "UCT: Failed to get regions!";
+  iEvent.getByToken(regionToken, regionCollection);
+  //const std::vector<L1CaloRegion> &caloRegion = *regionCollection;
+  //for(auto i : caloRegion) {
+  for (const L1CaloRegion &i : *regionCollection) {
+    cout<<"here L1TCaloSummary: "<<hex<<i.raw()<<endl;
+    uint32_t uctEta = 99;
+    if(i.gctEta() < 11) { uctEta = i.gctEta() - 11; }
+    else if(i.gctEta() > 10) { uctEta = i.gctEta() - 10; }
+    //if(uctEta != 99) summaryCard->setRegionData(i.raw(), uctEta, i.gctPhi(), fwVersion);
+    UCTRegionIndex r = UCTRegionIndex(uctEta, i.gctPhi());
+    UCTTowerIndex t = g.getUCTTowerIndex(r);
+    uint32_t absCaloEta = std::abs(t.first);
+    uint32_t absCaloPhi = std::abs(t.second);
+    bool negativeEta = false;
+    if (t.first < 0)
+      negativeEta = true;
+    uint32_t crate = g.getCrate(t.first, t.second);
+    uint32_t card = g.getCard(t.first, t.second);
+    uint32_t region = g.getRegion(absCaloEta, absCaloPhi) * 2;
+    UCTRegion* test = new UCTRegion(crate, card, negativeEta, region, fwVersion);
+    test->setRegionSummary(i.raw());
+    cout<<"here L1TCaloSummary test: "<<hex<<test->compressedData()<<endl;
+    inputRegions.push_back(test);
+  }
+  summaryCard->setRegionData(inputRegions);
+ 
   if(!summaryCard->process()) {
     std::cerr << "UCT: Failed to process summary card" << std::endl;
     exit(1);      
@@ -337,30 +373,30 @@ L1TCaloSummary::produce(edm::Event& iEvent, const edm::EventSetup& iSetup)
 }
 
 void L1TCaloSummary::print() {
-  vector<UCTCrate*> crates = layer1->getCrates();
-  for(uint32_t crt = 0; crt < crates.size(); crt++) {
-    vector<UCTCard*> cards = crates[crt]->getCards();
-    for(uint32_t crd = 0; crd < cards.size(); crd++) {
-      vector<UCTRegion*> regions = cards[crd]->getRegions();
-      for(uint32_t rgn = 0; rgn < regions.size(); rgn++) {
-	if(regions[rgn]->et() > 10) {
-	  int hitEta = regions[rgn]->hitCaloEta();
-	  int hitPhi = regions[rgn]->hitCaloPhi();
-	  vector<UCTTower*> towers = regions[rgn]->getTowers();
-	  for(uint32_t twr = 0; twr < towers.size(); twr++) {
-	    if(towers[twr]->caloPhi() == hitPhi && towers[twr]->caloEta() == hitEta) {
-	      std::cout << "*";
-	    }
-	    if(towers[twr]->et() > 10) std::cout << *towers[twr];
-	  }
-	  std::cout << *regions[rgn];
-	}
-      }
-      std::cout << *cards[crd];
-    }
-    std::cout << *crates[crt];
-  }
-  std::cout << *layer1;
+//  vector<UCTCrate*> crates = layer1->getCrates();
+//  for(uint32_t crt = 0; crt < crates.size(); crt++) {
+//    vector<UCTCard*> cards = crates[crt]->getCards();
+//    for(uint32_t crd = 0; crd < cards.size(); crd++) {
+//      vector<UCTRegion*> regions = cards[crd]->getRegions();
+//      for(uint32_t rgn = 0; rgn < regions.size(); rgn++) {
+//	if(regions[rgn]->et() > 10) {
+//	  int hitEta = regions[rgn]->hitCaloEta();
+//	  int hitPhi = regions[rgn]->hitCaloPhi();
+//	  vector<UCTTower*> towers = regions[rgn]->getTowers();
+//	  for(uint32_t twr = 0; twr < towers.size(); twr++) {
+//	    if(towers[twr]->caloPhi() == hitPhi && towers[twr]->caloEta() == hitEta) {
+//	      std::cout << "*";
+//	    }
+//	    if(towers[twr]->et() > 10) std::cout << *towers[twr];
+//	  }
+//	  std::cout << *regions[rgn];
+//	}
+//      }
+//      std::cout << *cards[crd];
+//    }
+//    std::cout << *crates[crt];
+//  }
+//  std::cout << *layer1;
 }
 
 // ------------ method called once each job just before starting event loop  ------------
@@ -378,39 +414,39 @@ L1TCaloSummary::endJob() {
 
 void
 L1TCaloSummary::beginRun(edm::Run const& iRun, edm::EventSetup const& iSetup) {
-  if (!L1TCaloLayer1FetchLUTs(iSetup,
-                              ecalLUT,
-                              hcalLUT,
-                              hfLUT,
-                              ePhiMap,
-                              hPhiMap,
-                              hfPhiMap,
-                              useLSB,
-                              useCalib,
-                              useECALLUT,
-                              useHCALLUT,
-                              useHFLUT,
-                              fwVersion)) {
-    LOG_ERROR << "L1TCaloLayer1::beginRun: failed to fetch LUTS - using unity" << std::endl;
-    std::array<std::array<std::array<uint32_t, nEtBins>, nCalSideBins>, nCalEtaBins> eCalLayer1EtaSideEtArray;
-    std::array<std::array<std::array<uint32_t, nEtBins>, nCalSideBins>, nCalEtaBins> hCalLayer1EtaSideEtArray;
-    std::array<std::array<uint32_t, nEtBins>, nHfEtaBins> hfLayer1EtaEtArray;
-    ecalLUT.push_back(eCalLayer1EtaSideEtArray);
-    hcalLUT.push_back(hCalLayer1EtaSideEtArray);
-    hfLUT.push_back(hfLayer1EtaEtArray);
-  }
-  for (uint32_t twr = 0; twr < twrList.size(); twr++) {
-    int iphi = twrList[twr]->caloPhi();
-    int ieta = twrList[twr]->caloEta();
-    if (ieta < 0) {
-      iphi -= 1;
-    } else {
-      iphi += 71;
-    }
-    twrList[twr]->setECALLUT(&ecalLUT[ePhiMap[iphi]]);
-    twrList[twr]->setHCALLUT(&hcalLUT[hPhiMap[iphi]]);
-    twrList[twr]->setHFLUT(&hfLUT[hfPhiMap[iphi]]);
-  }
+//  if (!L1TCaloLayer1FetchLUTs(iSetup,
+//                              ecalLUT,
+//                              hcalLUT,
+//                              hfLUT,
+//                              ePhiMap,
+//                              hPhiMap,
+//                              hfPhiMap,
+//                              useLSB,
+//                              useCalib,
+//                              useECALLUT,
+//                              useHCALLUT,
+//                              useHFLUT,
+//                              fwVersion)) {
+//    LOG_ERROR << "L1TCaloLayer1::beginRun: failed to fetch LUTS - using unity" << std::endl;
+//    std::array<std::array<std::array<uint32_t, nEtBins>, nCalSideBins>, nCalEtaBins> eCalLayer1EtaSideEtArray;
+//    std::array<std::array<std::array<uint32_t, nEtBins>, nCalSideBins>, nCalEtaBins> hCalLayer1EtaSideEtArray;
+//    std::array<std::array<uint32_t, nEtBins>, nHfEtaBins> hfLayer1EtaEtArray;
+//    ecalLUT.push_back(eCalLayer1EtaSideEtArray);
+//    hcalLUT.push_back(hCalLayer1EtaSideEtArray);
+//    hfLUT.push_back(hfLayer1EtaEtArray);
+//  }
+//  for (uint32_t twr = 0; twr < twrList.size(); twr++) {
+//    int iphi = twrList[twr]->caloPhi();
+//    int ieta = twrList[twr]->caloEta();
+//    if (ieta < 0) {
+//      iphi -= 1;
+//    } else {
+//      iphi += 71;
+//    }
+//    twrList[twr]->setECALLUT(&ecalLUT[ePhiMap[iphi]]);
+//    twrList[twr]->setHCALLUT(&hcalLUT[hPhiMap[iphi]]);
+//    twrList[twr]->setHFLUT(&hfLUT[hfPhiMap[iphi]]);
+//  }
 }
  
 // ------------ method called when ending the processing of a run  ------------
