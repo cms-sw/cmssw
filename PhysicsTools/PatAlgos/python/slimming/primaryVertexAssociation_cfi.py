ef453f7ab50f1293658f851673eb200a649c2f48
AdrianoDee <adriano.diflorio@ba.infn.it>
2021-05-19 12:39:47 +0200

    Adding offlineVerticesWithBS to miniAOD

--- PhysicsTools/PatAlgos/python/slimming/primaryVertexAssociation_cfi.py
+++ PhysicsTools/PatAlgos/python/slimming/primaryVertexAssociation_cfi.py
@@ -4,10 +4,17 @@ from CommonTools.RecoAlgos.sortedPFPrimaryVertices_cfi import sortedPFPrimaryVer
 primaryVertexAssociation = sortedPFPrimaryVertices.clone(
   qualityForPrimary = cms.int32(2),
   produceSortedVertices = cms.bool(False),
-  producePileUpCollection  = cms.bool(False),  
+  producePileUpCollection  = cms.bool(False),
   produceNoPileUpCollection = cms.bool(False)
+)
+
+primaryVertexWithBSAssociation = primaryVertexAssociation.clone(
+  vertices = cms.InputTag("offlinePrimaryVerticesWithBS")
 )
 
 from Configuration.ProcessModifiers.run2_miniAOD_pp_on_AA_103X_cff import run2_miniAOD_pp_on_AA_103X
 run2_miniAOD_pp_on_AA_103X.toModify(primaryVertexAssociation,particles = "cleanedParticleFlow")
 primaryVertexAssociationCleaned = primaryVertexAssociation.clone(particles = "cleanedParticleFlow:removed")
+
+run2_miniAOD_pp_on_AA_103X.toModify(primaryVertexWithBSAssociation,particles = "cleanedParticleFlow")
+primaryVertexWithBSAssociationCleaned = primaryVertexWithBSAssociation.clone(particles = "cleanedParticleFlow:removed")
