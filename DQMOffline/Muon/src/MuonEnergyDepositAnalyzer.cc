b80f763c0740ea7529334a42a84f6a94e0ee39c5
calderon <calderon@cern.ch>
2021-05-20 16:15:42 +0200

    migrate esConsumes

--- DQMOffline/Muon/src/MuonEnergyDepositAnalyzer.cc
+++ DQMOffline/Muon/src/MuonEnergyDepositAnalyzer.cc
@@ -22,8 +22,8 @@ using namespace std;
 using namespace edm;
 
 MuonEnergyDepositAnalyzer::MuonEnergyDepositAnalyzer(const edm::ParameterSet& pSet)
-  : trasientTrackToken_(esConsumes<TransientTrackBuilder, TransientTrackRecord>(edm::ESInputTag("", "TransientTrackBuilder"))) {
-
+    : trasientTrackToken_(
+          esConsumes<TransientTrackBuilder, TransientTrackRecord>(edm::ESInputTag("", "TransientTrackBuilder"))) {
   parameters = pSet;
 
   theMuonCollectionLabel_ = consumes<reco::MuonCollection>(parameters.getParameter<InputTag>("MuonCollection"));
