43ca7e0f94b6f84e2f31c8f50fe6177826f56dfa
AdrianoDee <adriano.diflorio@ba.infn.it>
2021-05-22 10:37:38 +0200

    Fixing naming for primaryVertexWithBSAssociation

--- PhysicsTools/PatAlgos/python/slimming/offlineSlimmedPrimaryVerticesWithBS_cfi.py
+++ PhysicsTools/PatAlgos/python/slimming/offlineSlimmedPrimaryVerticesWithBS_cfi.py
@@ -3,7 +3,7 @@ import FWCore.ParameterSet.Config as cms
 offlineSlimmedPrimaryVerticesWithBS = cms.EDProducer("PATVertexSlimmer",
     src = cms.InputTag("offlinePrimaryVerticesWithBS"),
 #this works also on input file produced with CMSSW<=740pre8, needed for testing or for 72X AOD processing with 74X
-    score = cms.InputTag("primaryVertexAssociationWithBS","original"),
+    score = cms.InputTag("primaryVertexWithBSAssociation","original"),
 # this need new input file with scores made at RECO time (PR #8102, #8101), enable before MINIAOD prod in 74x
 #   score = cms.InputTag("offlinePrimaryVertices"),
 )
