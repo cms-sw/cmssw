93b3993bb0330503082475f2723674bd3d0893b0
Leonardo Cristella <leonardo.cristella@cern.ch>
2021-05-26 19:40:57 +0200

    Fix typos

--- SimCalorimetry/HGCalAssociatorProducers/python/TSToSimTSAssociation_cfi.py
+++ SimCalorimetry/HGCalAssociatorProducers/python/TSToSimTSAssociation_cfi.py
@@ -1,6 +1,6 @@
 import FWCore.ParameterSet.Config as cms
 
-tracksterSimClusterAssociation = cms.EDProducer("TSToSimTSAssociatorEDProducer",
+tracksterSimTracksterAssociation = cms.EDProducer("TSToSimTSAssociatorEDProducer",
     associator = cms.InputTag('simTsAssocByEnergyScoreProducer'),
     label_tst = cms.InputTag("ticlTrackstersMerge"),
     label_simTst = cms.InputTag("ticlSimTracksters"),
