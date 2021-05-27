6b6213dd14967c52395bff93599666f1757d506b
Christopher Jones <chrisdjones15@gmail.com>
2021-05-27 16:03:00 -0500

    Removed *Rcd.h headers from CondFormats/L1TObjects
    The classes are already declared in CondFormats/Records so these
    were a 'one definition rule' violation.

--- CondFormats/L1TObjects/src/L1TUtmCutValueRcd.h
+++
@@ -1,5 +0,0 @@
-#ifndef CondFormatsDataRecord_L1TUtmCutValueRcd_h
-#define CondFormatsDataRecord_L1TUtmCutValueRcd_h
-#include "FWCore/Framework/interface/EventSetupRecordImplementation.h
-class L1TUtmCutValueRcd : public edm::eventsetup::EventSetupRecordImplementation<L1TUtmCutValueRcd> {};
-#endif
