838f9f155ecc6aa0d0ef5f54a1f0f95ab9060f93
Silvio <silvio.donato@cern.ch>
2021-05-27 12:27:15 +0000

    backport of 28802 to 94X

--- SimMuon/MCTruth/plugins/MuonSimClassifier.cc
+++ SimMuon/MCTruth/plugins/MuonSimClassifier.cc
@@ -302,6 +302,11 @@ MuonSimClassifier::produce(edm::Event& iEvent, const edm::EventSetup& iSetup)
 		  if (mMom->numberOfMothers() > 0) {
 		    mMom = mMom->motherRef();
 		  }
+              else {
+                LogTrace("MuonSimClassifier") << "\t No Mother is found ";
+                break;
+              }
+              
 		  LogTrace("MuonSimClassifier") 
 		    << "\t\t backtracking mother "<<jm<<", pdgId = "<<mMom->pdgId()<<", status= " <<mMom->status();
 		}
