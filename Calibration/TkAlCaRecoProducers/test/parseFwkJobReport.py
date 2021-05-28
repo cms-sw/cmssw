61f3cb073cfaae2f3f5a98fb8bf3e80a5df63b16
mmusich <marco.musich@cern.ch>
2021-05-28 21:19:19 +0200

    improve scripts and actually return failure if does not pass checks

--- Calibration/TkAlCaRecoProducers/test/parseFwkJobReport.py
+++ Calibration/TkAlCaRecoProducers/test/parseFwkJobReport.py
@@ -1,12 +1,18 @@
 from __future__ import print_function
 import xml.etree.ElementTree as ET
-  
+import sys
+
 ## declare all constants here
 TARGET_LIST_OF_TAGS=['SiPixelQualityFromDbRcd_other', 'SiPixelQualityFromDbRcd_prompt', 'SiPixelQualityFromDbRcd_stuckTBM', 
-                     'SiStripApvGain_pcl', 'SiStripApvGainAAG_pcl', 'SiStripBadStrip_pcl', 'SiPixelAli_pcl']
+                     'SiStripApvGain_pcl', 'SiStripApvGainAAG_pcl',
+                     'SiStripBadStrip_pcl', 'SiPixelAli_pcl']
 TARGET_DQM_FILES=1
+TARGET_DQM_FILENAME='./DQM_V0001_R000325022__Express__PCLTest__ALCAPROMPT.root'
 TARGET_DB_FILES=7
+TARGET_DB_FILENAME='sqlite_file:promptCalibConditions.db'
+TOTAL_TARGET_FILES=TARGET_DQM_FILES+TARGET_DB_FILES
 
+#_____________________________________________________
 def parseXML(xmlfile):
   
     # create element tree object
@@ -15,22 +21,25 @@ def parseXML(xmlfile):
     # get root element
     root = tree.getroot()
 
-    if( len(root.findall('AnalysisFile'))!=8):
-        print("ERROR: not found enough AnalysisFile entries in the FrameworkJobReport.xml")
+    totAnaEntries=len(root.findall('AnalysisFile'))
+
+    if(totAnaEntries!=TOTAL_TARGET_FILES):
+        print("ERROR: found a not expected number (",totAnaEntries,") of AnalysisFile entries in the FrameworkJobReport.xml")
         return -1
 
     listOfInputTags=[]
 
     countDBfiles=0
     countDQMfiles=0
+
     # iterate news items
     for item in root.findall('AnalysisFile'):
         # iterate child elements of item
         for child in item:
             if(child.tag == 'FileName'):
-                if(child.text=='sqlite_file:promptCalibConditions.db'):
+                if(child.text==TARGET_DB_FILENAME):
                     countDBfiles+=1
-                elif(child.text=='./DQM_V0001_R000325022__Express__PCLTest__ALCAPROMPT.root'):
+                elif(child.text==TARGET_DQM_FILENAME):
                     countDQMfiles+=1
                 else:
                     pass
@@ -38,7 +47,7 @@ def parseXML(xmlfile):
                 listOfInputTags.append(child.attrib['Value'])
 
     if(countDBfiles!=TARGET_DB_FILES):
-        print("ERROR! Found a not expected number DB files,",countDBfiles)
+        print("ERROR! Found a not expected number of DB files",countDBfiles)
         return -1
 
     if(countDQMfiles!=TARGET_DQM_FILES):
@@ -52,22 +61,24 @@ def parseXML(xmlfile):
     
     return 0
 
+#_____________________________________________________
 def main():
     try:
         f = open("FrameworkJobReport.xml")
-        # Do something with the file
     except IOError:
         print("File not accessible")
+        sys.exit(1)
 
     # parse xml file
     result = parseXML('FrameworkJobReport.xml')
     if(result==0):
         print("All is fine with the world!")
+        sys.exit(0)
     else:
         print("Parsing the FwkJobReport results in failure!")
+        sys.exit(1)
 
-    return result
-
+#_____________________________________________________
 if __name__ == "__main__":
   
     # calling main function
