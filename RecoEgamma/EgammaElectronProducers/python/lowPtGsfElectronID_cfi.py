647898b8a8d40280227754f2f958481d68cb2df8
bainbrid <bainbrid@users.noreply.github.com>
2021-05-31 22:23:53 +0200

    slava review: module modified in same cfi

--- RecoEgamma/EgammaElectronProducers/python/lowPtGsfElectronID_cfi.py
+++ RecoEgamma/EgammaElectronProducers/python/lowPtGsfElectronID_cfi.py
@@ -1,6 +1,7 @@
 import FWCore.ParameterSet.Config as cms
 
-from RecoEgamma.EgammaElectronProducers.lowPtGsfElectronID_cfi import lowPtGsfElectronID
+from RecoEgamma.EgammaElectronProducers.defaultLowPtGsfElectronID_cfi import defaultLowPtGsfElectronID
+lowPtGsfElectronID = defaultLowPtGsfElectronID.clone()
 
 from Configuration.Eras.Modifier_bParking_cff import bParking
 bParking.toModify(
