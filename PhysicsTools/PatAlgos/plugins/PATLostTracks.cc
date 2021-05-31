608b91c9526f6fcbbd60ca00a54c7a9c3bdbb1c0
AdrianoDee <adriano.diflorio@ba.infn.it>
2021-05-31 14:05:17 +0200

    Moving the NOTUSED cut above

--- PhysicsTools/PatAlgos/plugins/PATLostTracks.cc
+++ PhysicsTools/PatAlgos/plugins/PATLostTracks.cc
@@ -205,17 +205,16 @@ void pat::PATLostTracks::produce(edm::StreamID, edm::Event& iEvent, const edm::E
       // selecting potential Xi- -> Lambda pi candidates
       math::XYZTLorentzVector p4Lambda(v0.px(), v0.py(), v0.pz(), sqrt(v0.momentum().mag2() + v0.mass() * v0.mass()));
 
-      for (unsigned int trkIndx = 0; trkIndx < tracks->size(); trkIndx++) {
+      for (unsigned int trkIndx = 0; trkIndx < tracks->size(); trkIndx++)
+      {
         reco::TrackRef trk(tracks, trkIndx);
-        if ((*trk).charge() * protonCharge < 0)
+        if ((*trk).charge() * protonCharge < 0 || trkStatus[trkIndx] != TrkStatus::NOTUSED)
           continue;
 
         math::XYZTLorentzVector p4pi(
             trk->px(), trk->py(), trk->pz(), sqrt(trk->momentum().mag2() + 0.01947995518));  // pion mass ^2
-        if ((p4Lambda + p4pi).M() < xiMassCut_) {  // selecting potential Xi- candidates
-          if (trkStatus[trkIndx] == TrkStatus::NOTUSED)
-            trkStatus[trkIndx] = TrkStatus::VTX;
-        }
+        if ((p4Lambda + p4pi).M() < xiMassCut_)
+            trkStatus[trkIndx] = TrkStatus::VTX; // selecting potential Xi- candidates
       }
     }
   }
