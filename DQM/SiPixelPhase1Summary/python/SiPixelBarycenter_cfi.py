ec0979cdfce459406dab4e85ee08d68b81eb0074
Danilo Meuser <danilo.meuser@rwth-aachen.de>
2021-06-02 09:40:29 +0200

    Fixed namin of inclusion guards an removed Phase1 from names

--- DQM/SiPixelPhase1Summary/python/SiPixelBarycenter_cfi.py
+++ DQM/SiPixelPhase1Summary/python/SiPixelBarycenter_cfi.py
@@ -5,4 +5,4 @@ from DQMServices.Core.DQMEDHarvester import DQMEDHarvester
 # This object is used to make changes for different running scenarios
 #
 
-SiPixelPhase1BarycenterOffline = DQMEDHarvester("SiPixelPhase1Barycenter")
+SiPixelBarycenterOffline = DQMEDHarvester("SiPixelBarycenter")
--- DQM/SiPixelPhase1Summary/src/SiPixelBarycenter.cc
+++ DQM/SiPixelPhase1Summary/src/SiPixelBarycenter.cc
@@ -1,11 +1,11 @@
 // -*- C++ -*-
 //
-// Package:    SiPixelPhase1Barycenter
-// Class:      SiPixelPhase1Barycenter
+// Package:    SiPixelPhase1Summary
+// Class:      SiPixelBarycenter
 //
 /**\class 
 
- Description: Create the Phase 1 pixel barycenter plots
+ Description: Create the pixel barycenter plots
 
  Implementation:
      <Notes on implementation>
@@ -15,9 +15,9 @@
 //         Created:  26th May 2021
 //
 //
-#include "DQM/SiPixelPhase1Summary/interface/SiPixelPhase1Barycenter.h"
+#include "DQM/SiPixelPhase1Summary/interface/SiPixelBarycenter.h"
 #include "FWCore/MessageLogger/interface/MessageLogger.h"
-#include "DQM/SiPixelPhase1Summary/interface/SiPixelPhase1BarycenterHelper.h"
+#include "DQM/SiPixelPhase1Summary/interface/SiPixelBarycenterHelper.h"
 #include "CalibTracker/StandaloneTrackerTopology/interface/StandaloneTrackerTopology.h"
 
 #include <string>
@@ -26,23 +26,23 @@
 using namespace std;
 using namespace edm;
 
-SiPixelPhase1Barycenter::SiPixelPhase1Barycenter(const edm::ParameterSet& iConfig)
+SiPixelBarycenter::SiPixelBarycenter(const edm::ParameterSet& iConfig)
     : DQMEDHarvester(iConfig) {
-  LogInfo("PixelDQM") << "SiPixelPhase1Barycenter::SiPixelPhase1Barycenter: Got DQM BackEnd interface" << endl;
+  LogInfo("PixelDQM") << "SiPixelBarycenter::SiPixelBarycenter: Got DQM BackEnd interface" << endl;
 
 }
 
-SiPixelPhase1Barycenter::~SiPixelPhase1Barycenter() {
+SiPixelBarycenter::~SiPixelBarycenter() {
   // do anything here that needs to be done at desctruction time
   // (e.g. close files, deallocate resources etc.)
-  LogInfo("PixelDQM") << "SiPixelPhase1Barycenter::~SiPixelPhase1Barycenter: Destructor" << endl;
+  LogInfo("PixelDQM") << "SiPixelBarycenter::~SiPixelBarycenter: Destructor" << endl;
 }
 
-void SiPixelPhase1Barycenter::beginRun(edm::Run const& run, edm::EventSetup const& eSetup) {}
+void SiPixelBarycenter::beginRun(edm::Run const& run, edm::EventSetup const& eSetup) {}
 
-void SiPixelPhase1Barycenter::dqmEndJob(DQMStore::IBooker& iBooker, DQMStore::IGetter& iGetter) {}
+void SiPixelBarycenter::dqmEndJob(DQMStore::IBooker& iBooker, DQMStore::IGetter& iGetter) {}
 
-void SiPixelPhase1Barycenter::dqmEndLuminosityBlock(DQMStore::IBooker& iBooker,
+void SiPixelBarycenter::dqmEndLuminosityBlock(DQMStore::IBooker& iBooker,
                                                  DQMStore::IGetter& iGetter,
                                                  const edm::LuminosityBlock& lumiSeg,
                                                  edm::EventSetup const& c) {
@@ -62,7 +62,7 @@ void SiPixelPhase1Barycenter::dqmEndLuminosityBlock(DQMStore::IBooker& iBooker,
 //------------------------------------------------------------------
 // Used to book the barycenter histograms
 //------------------------------------------------------------------
-void SiPixelPhase1Barycenter::bookBarycenterHistograms(DQMStore::IBooker& iBooker) {
+void SiPixelBarycenter::bookBarycenterHistograms(DQMStore::IBooker& iBooker) {
   iBooker.cd();
 
   iBooker.setCurrentFolder("PixelPhase1/Barycenter");
@@ -86,7 +86,7 @@ void SiPixelPhase1Barycenter::bookBarycenterHistograms(DQMStore::IBooker& iBooke
 //------------------------------------------------------------------
 // Fill the Barycenter histograms
 //------------------------------------------------------------------
-void SiPixelPhase1Barycenter::fillBarycenterHistograms(DQMStore::IBooker& iBooker, DQMStore::IGetter& iGetter, const std::vector<AlignTransform>& input, const std::vector<AlignTransform>& GPR) {
+void SiPixelBarycenter::fillBarycenterHistograms(DQMStore::IBooker& iBooker, DQMStore::IGetter& iGetter, const std::vector<AlignTransform>& input, const std::vector<AlignTransform>& GPR) {
   
   const auto GPR_translation_pixel = GPR[0].translation();
   const std::map<DQMBarycenter::coordinate, float> GPR_pixel = {{DQMBarycenter::t_x, GPR_translation_pixel.x()}, {DQMBarycenter::t_y, GPR_translation_pixel.y()}, {DQMBarycenter::t_z, GPR_translation_pixel.z()}};
@@ -109,4 +109,4 @@ void SiPixelPhase1Barycenter::fillBarycenterHistograms(DQMStore::IBooker& iBooke
 }
 
 //define this as a plug-in
-DEFINE_FWK_MODULE(SiPixelPhase1Barycenter);
+DEFINE_FWK_MODULE(SiPixelBarycenter);
