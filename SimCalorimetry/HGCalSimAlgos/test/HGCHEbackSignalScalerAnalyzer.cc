e959d00e403a7ac1eb80e427beda867a96c005de
Pedro <psilva@cern.ch>
2021-06-03 18:16:27 +0200

    updating test analyzer to scan different options

--- SimCalorimetry/HGCalSimAlgos/test/HGCHEbackSignalScalerAnalyzer.cc
+++ SimCalorimetry/HGCalSimAlgos/test/HGCHEbackSignalScalerAnalyzer.cc
@@ -59,14 +59,13 @@ private:
   std::string sipmMap_;
   uint32_t nPEperMIP_;
   double refIdark_;
-
-  std::set<int> allLayers_, allIeta_;  //allIphi_
+  std::set<int> allLayers_,allIphi_,allIeta_;
 
   const HGCalGeometry* gHGCal_;
   const HGCalDDDConstants* hgcCons_;
 
-  std::map<int, std::map<TString, TH1F*> > layerHistos_;
-  std::map<TString, TH2F*> histos_;
+  std::map<int, std::map<TString, TH1F *> > layerHistos_;
+  std::map<TString, TH2F *> histos_;
 };
 
 //
@@ -77,7 +76,7 @@ HGCHEbackSignalScalerAnalyzer::HGCHEbackSignalScalerAnalyzer(const edm::Paramete
       doseMapAlgo_(iConfig.getParameter<uint32_t>("doseMapAlgo")),      
       sipmMap_(iConfig.getParameter<std::string>("sipmMap")),
       nPEperMIP_(iConfig.getParameter<uint32_t>("nPEperMIP")),
-      refIdark_(iConfig.getParameter<uint32_t>("referenceIdark"))
+      refIdark_(iConfig.getParameter<double>("referenceIdark"))
 {
   usesResource("TFileService");
   fs->file().cd();
@@ -91,6 +90,7 @@ HGCHEbackSignalScalerAnalyzer::~HGCHEbackSignalScalerAnalyzer() {}
 
 // ------------ method called on each new Event  ------------
 void HGCHEbackSignalScalerAnalyzer::analyze(const edm::Event& iEvent, const edm::EventSetup& iSetup) {
+
   //get geometry
   edm::ESHandle<HGCalGeometry> geomhandle;
   iSetup.get<IdealGeometryRecord>().get("HGCalHEScintillatorSensitive", geomhandle);
@@ -113,42 +113,26 @@ void HGCHEbackSignalScalerAnalyzer::analyze(const edm::Event& iEvent, const edm:
 
   //setup maps
   createBinning(detIdVec);
-  int minLayer(*std::min_element(allLayers_.begin(), allLayers_.end()));
-  int maxLayer(*std::max_element(allLayers_.begin(), allLayers_.end()));
-  int nLayers(maxLayer - minLayer + 1);
-  int minIeta(*std::min_element(allIeta_.begin(), allIeta_.end()));
-  int maxIeta(*std::max_element(allIeta_.begin(), allIeta_.end()));
-  int nIeta(maxIeta - minIeta + 1);
-  //int minIphi( *std::min_element(allIphi_.begin(),allIphi_.end()) );
-  //int maxIphi( *std::max_element(allIphi_.begin(),allIphi_.end()) );
-  //int nIphi(maxIphi - minIphi + 1);
 
-  TString hnames[] = {"count", "radius", "dose", "fluence", "s", "n", "sn", "lysf"};
-  TString htitles[] = {"tiles",
-                       "#rho [cm]",
-                       "<Dose> [krad]",
-                       "<Fluence> [1 MeV n_{eq}/cm^{2}]",
-                       "<S> [pe]",
-                       "<#sigma_{N}> [pe]",
-                       "<S/#sigma_{N}>",
-                       "LY SF"};
-  for (size_t i = 0; i < sizeof(hnames) / sizeof(TString); i++) {
-    histos_[hnames[i] + "_ieta"] = fs->make<TH2F>(
-        hnames[i] + "_ieta", ";Layer;i-#eta;" + htitles[i], nLayers, minLayer, maxLayer + 1, nIeta, minIeta, maxIeta);
-    //histos_[hnames[i] + "_iphi"] = fs->make<TH2F>(
-    //    hnames[i] + "_iphi", ";Layer;i-#phi;" + htitles[i], nLayers, minLayer, maxLayer + 1, nIphi, minIphi, maxIphi);
+  int minLayer( *allLayers_.begin() ), maxLayer( *allLayers_.end() ),  nLayers(maxLayer-minLayer+1);
+  int minIeta( *allIeta_.begin() ),    maxIeta( *allIeta_.end() ),     nIeta(maxIeta-minIeta+1);
+  int minIphi( *allIphi_.begin() ),    maxIphi( *allIphi_.end() ),     nIphi(maxIphi-minIphi+1);
+
+  TString hnames[]={"count","radius","dose","fluence","s","n","sn"};
+  TString htitles[]={"tiles","#rho [cm]","<Dose> [krad]","<Fluence> [1 MeV n_{eq}/cm^{2}]","<S> [pe]","<N> [pe]","<S/N>"};
+  for(size_t i=0; i<sizeof(hnames)/sizeof(TString); i++){
+    histos_[hnames[i]+"_ieta"] = fs->make<TH2F>(hnames[i]+"_ieta",";Layer;i-#eta;"+htitles[i],nLayers,minLayer,maxLayer+1, nIeta,minIeta,maxIeta);
+    histos_[hnames[i]+"_iphi"] = fs->make<TH2F>(hnames[i]+"_iphi",";Layer;i-#phi;"+htitles[i],nLayers,minLayer,maxLayer+1, nIphi,minIphi,maxIphi);
 
     //add per-layer profiles
-    for (int ilay = minLayer; ilay <= maxLayer; ilay++) {
-      if (layerHistos_.count(ilay) == 0) {
-        std::map<TString, TH1F*> templ_map;
-        layerHistos_[ilay] = templ_map;
+    for(int ilay=minLayer; ilay<=maxLayer; ilay++) {
+      if(layerHistos_.count(ilay)==0) {
+        std::map<TString, TH1F *> templ_map;
+        layerHistos_[ilay]=templ_map;
       }
-      TString hnameLay(Form("%s_lay%d_", hnames[i].Data(), ilay));
-      layerHistos_[ilay][hnames[i] + "_ieta"] =
-          fs->make<TH1F>(hnameLay + "ieta", ";i-#eta;" + htitles[i], nIeta, minIeta, maxIeta);
-      //layerHistos_[ilay][hnames[i] + "_iphi"] =
-      //fs->make<TH1F>(hnameLay + "iphi", ";i-#phi;" + htitles[i], nIphi, minIphi, maxIphi);
+      TString hnameLay( Form("%s_lay%d_",hnames[i].Data(),ilay) );
+      layerHistos_[ilay][hnames[i]+"_ieta"] = fs->make<TH1F>(hnameLay+"ieta",";i-#eta;"+htitles[i],nIeta,minIeta,maxIeta);
+      layerHistos_[ilay][hnames[i]+"_iphi"] = fs->make<TH1F>(hnameLay+"iphi",";i-#phi;"+htitles[i],nIphi,minIphi,maxIphi);
     }
   }
 
@@ -160,79 +144,76 @@ void HGCHEbackSignalScalerAnalyzer::analyze(const edm::Event& iEvent, const edm:
   scal.setGeometry(gHGCal_);
 
   //loop over valid detId from the HGCHEback
-  for (auto myId : detIdVec) {
+  for(auto myId : detIdVec ) {
+
     HGCScintillatorDetId scId(myId.rawId());
 
     int layer = std::abs(scId.layer());
-    std::pair<int, int> ietaphi = scId.ietaphi();
+    std::pair<int, int> ietaphi= scId.ietaphi();
     int ieta = std::abs(ietaphi.first);
-    //int iphi = std::abs(ietaphi.second);
+    int iphi = std::abs(ietaphi.second);
 
     //characteristics of this tile
     GlobalPoint global = gHGCal_->getPosition(scId);
-    double radius = std::sqrt(std::pow(global.x(), 2) + std::pow(global.y(), 2));
-    double dose = scal.getDoseValue(DetId::HGCalHSc, layer, radius);
-    double fluence = scal.getFluenceValue(DetId::HGCalHSc, layer, radius);
+    double radius      = std::sqrt(std::pow(global.x(), 2) + std::pow(global.y(), 2));
+    double dose        = scal.getDoseValue(DetId::HGCalHSc, layer, radius);
+    double fluence     = scal.getFluenceValue(DetId::HGCalHSc, layer, radius);
+    auto dosePair      = scal.scaleByDose(scId, radius);
+    double s           = nPEperMIP_*dosePair.first;
+    double n           = dosePair.second;
+    double sn          = n>0 ? s/n : -1.;
 
-    auto dosePair = scal.scaleByDose(scId, radius);
-    float scaleFactorBySipmArea = scal.scaleBySipmArea(scId, radius);
-    float scaleFactorByTileArea = scal.scaleByTileArea(scId, radius);
-    float scaleFactorByDose = dosePair.first;
-    float noiseByFluence = dosePair.second;
+    histos_["count_ieta"]->Fill(layer,ieta,1);
+    histos_["count_iphi"]->Fill(layer,iphi,1);
+    histos_["radius_ieta"]->Fill(layer,ieta,radius);
+    histos_["radius_iphi"]->Fill(layer,iphi,radius);
+    histos_["dose_ieta"]->Fill(layer,ieta,dose);
+    histos_["dose_iphi"]->Fill(layer,iphi,dose);
+    histos_["fluence_ieta"]->Fill(layer,ieta,fluence);
+    histos_["fluence_iphi"]->Fill(layer,iphi,fluence);
+    histos_["s_ieta"]->Fill(layer,ieta,s);
+    histos_["s_iphi"]->Fill(layer,iphi,s);
+    histos_["n_ieta"]->Fill(layer,ieta,n);
+    histos_["n_iphi"]->Fill(layer,iphi,n);
+    histos_["sn_ieta"]->Fill(layer,ieta,sn);
+    histos_["sn_iphi"]->Fill(layer,iphi,sn);
 
-    float lysf = scaleFactorByTileArea * scaleFactorBySipmArea * scaleFactorByDose;
-    float s = nPEperMIP_ * lysf;
-    float n = noiseByFluence * sqrt(scaleFactorBySipmArea);
-    float sn = s / n;
-
-    histos_["count_ieta"]->Fill(layer, ieta, 1);
-    //histos_["count_iphi"]->Fill(layer, iphi, 1);
-    histos_["radius_ieta"]->Fill(layer, ieta, radius);
-    //histos_["radius_iphi"]->Fill(layer, iphi, radius);
-    histos_["dose_ieta"]->Fill(layer, ieta, dose);
-    //histos_["dose_iphi"]->Fill(layer, iphi, dose);
-    histos_["fluence_ieta"]->Fill(layer, ieta, fluence);
-    //histos_["fluence_iphi"]->Fill(layer, iphi, fluence);
-    histos_["s_ieta"]->Fill(layer, ieta, s);
-    //histos_["s_iphi"]->Fill(layer, iphi, s);
-    histos_["n_ieta"]->Fill(layer, ieta, n);
-    //histos_["n_iphi"]->Fill(layer, iphi, n);
-    histos_["sn_ieta"]->Fill(layer, ieta, sn);
-    //histos_["sn_iphi"]->Fill(layer, iphi, sn);
-    histos_["lysf_ieta"]->Fill(layer, ieta, lysf);
-    //histos_["lysf_iphi"]->Fill(layer, iphi, lysf);
-
-    if (layerHistos_.count(layer) == 0) {
+    if(layerHistos_.count(layer)==0) {
       continue;
     }
 
     //per layer
-    layerHistos_[layer]["count_ieta"]->Fill(ieta, 1);
-    //layerHistos_[layer]["count_iphi"]->Fill(iphi, 1);
-    layerHistos_[layer]["radius_ieta"]->Fill(ieta, radius);
-    //layerHistos_[layer]["radius_iphi"]->Fill(iphi, radius);
-    layerHistos_[layer]["dose_ieta"]->Fill(ieta, dose);
-    //layerHistos_[layer]["dose_iphi"]->Fill(iphi, dose);
-    layerHistos_[layer]["fluence_ieta"]->Fill(ieta, fluence);
-    //layerHistos_[layer]["fluence_iphi"]->Fill(iphi, fluence);
-    layerHistos_[layer]["s_ieta"]->Fill(ieta, s);
-    //layerHistos_[layer]["s_iphi"]->Fill(iphi, s);
-    layerHistos_[layer]["n_ieta"]->Fill(ieta, n);
-    //layerHistos_[layer]["n_iphi"]->Fill(iphi, n);
-    layerHistos_[layer]["sn_ieta"]->Fill(ieta, sn);
-    //layerHistos_[layer]["sn_iphi"]->Fill(iphi, sn);
-    layerHistos_[layer]["lysf_ieta"]->Fill(ieta, lysf);
-    //layerHistos_[layer]["lysf_iphi"]->Fill(iphi, lysf);
+    layerHistos_[layer]["count_ieta"]->Fill(ieta,1);
+    layerHistos_[layer]["count_iphi"]->Fill(iphi,1);
+    layerHistos_[layer]["radius_ieta"]->Fill(ieta,radius);
+    layerHistos_[layer]["radius_iphi"]->Fill(iphi,radius);
+    layerHistos_[layer]["dose_ieta"]->Fill(ieta,dose);
+    layerHistos_[layer]["dose_iphi"]->Fill(iphi,dose);
+    layerHistos_[layer]["fluence_ieta"]->Fill(ieta,fluence);
+    layerHistos_[layer]["fluence_iphi"]->Fill(iphi,fluence);
+    layerHistos_[layer]["s_ieta"]->Fill(ieta,s);
+    layerHistos_[layer]["s_iphi"]->Fill(iphi,s);
+    layerHistos_[layer]["n_ieta"]->Fill(ieta,n);
+    layerHistos_[layer]["n_iphi"]->Fill(iphi,n);
+    layerHistos_[layer]["sn_ieta"]->Fill(ieta,sn);
+    layerHistos_[layer]["sn_iphi"]->Fill(iphi,sn);
   }
 
-  //for all (except count histograms) normalize by the number of sensors
-  for (auto& lit : layerHistos_) {
-    for (auto& hit : lit.second) {
-      TString name(hit.first);
-      if (name.Contains("count_"))
-        continue;
-      TString count_name(name.Contains("_ieta") ? "count_ieta" : "count_iphi");
-      hit.second->Divide(lit.second[count_name]);
+  //normalize by the number of tiles to have the mean stored
+  for(auto h : histos_) {
+    TString name(h.first);
+    if(name.Contains("count_") ) continue;
+    TString pfix( name.Contains("ieta") ? "ieta" : "iphi" );
+    h.second->Divide( histos_["count_"+pfix] );
+  }
+
+  for(auto h : layerHistos_) {
+    int lay(h.first);
+    for(auto hh : layerHistos_[lay]) {
+      TString name(hh.first);
+      if(name.Contains("count_") ) continue;
+      TString pfix( name.Contains("ieta") ? "ieta" : "iphi" );
+      hh.second->Divide( layerHistos_[lay]["count_"+pfix] );
     }
   }
 
@@ -247,16 +228,19 @@ void HGCHEbackSignalScalerAnalyzer::analyze(const edm::Event& iEvent, const edm:
 
 //
 void HGCHEbackSignalScalerAnalyzer::createBinning(const std::vector<DetId>& detIdVec) {
+
   //loop over detids to find possible values
   for (auto myId : detIdVec) {
+
     HGCScintillatorDetId scId(myId.rawId());
     int layer = std::abs(scId.layer());
-    std::pair<int, int> ietaphi = scId.ietaphi();
-
+    std::pair<int, int> ietaphi= scId.ietaphi();
+  
     allLayers_.insert(std::abs(layer));
     allIeta_.insert(std::abs(ietaphi.first));
-    //allIphi_.insert(std::abs(ietaphi.second));
+    allIphi_.insert(std::abs(ietaphi.second));
   }
+
 }
 
 //define this as a plug-in
--- SimCalorimetry/HGCalSimAlgos/test/hgchebacksignalscaler_cfg.py
+++ SimCalorimetry/HGCalSimAlgos/test/hgchebacksignalscaler_cfg.py
@@ -1,36 +1,47 @@
-#run with: cmsRun hgchebacksignalscaler_cfg.py doseMap=SimCalorimetry/HGCalSimProducers/data/doseParams_3000fb_fluka-3.5.15.9.txt sipmMap=SimCalorimetry/HGCalSimProducers/data/sipmParams_geom-10.txt nPEperMIP=21 doseMapAlgo=0 referenceIdark=0.5
+#run with: cmsRun hgchebacksignalscaler_cfg.py doseMap=SimCalorimetry/HGCalSimProducers/data/doseParams_3000fb_fluka-3.7.20.txt nPEperMIP=21
 
 import FWCore.ParameterSet.Config as cms
 from FWCore.ParameterSet.VarParsing import VarParsing
 from Configuration.StandardSequences.Eras import eras
 
 options = VarParsing()
-options.register ("doseMap", "",  VarParsing.multiplicity.singleton, VarParsing.varType.string)
-options.register ("doseMapAlgo", 0,  VarParsing.multiplicity.singleton, VarParsing.varType.int)
-options.register ("sipmMap", "",  VarParsing.multiplicity.singleton, VarParsing.varType.string)
-options.register ("nPEperMIP", "",  VarParsing.multiplicity.singleton, VarParsing.varType.int)
-options.register ("referenceIdark", 0.5,  VarParsing.multiplicity.singleton, VarParsing.varType.double)
+options.register("doseMap",   "",  VarParsing.multiplicity.singleton, VarParsing.varType.string)
+options.register("nPEperMIP", 21,  VarParsing.multiplicity.singleton, VarParsing.varType.int)
+options.register("geom", "GeometryExtended2026D49Reco",  VarParsing.multiplicity.singleton, VarParsing.varType.string)
 options.parseArguments()
 
-process = cms.Process("demo",eras.Phase2C9)
-
+from Configuration.Eras.Era_Phase2C10_cff import Phase2C10
+process = cms.Process('demo',Phase2C10)
 process.load("Configuration.StandardSequences.FrontierConditions_GlobalTag_cff")
-process.load('Configuration.Geometry.{}_cff'.format(options.geometry))
+process.load('Configuration.Geometry.{}_cff'.format(options.geom))
+
 from Configuration.AlCa.GlobalTag import GlobalTag
 process.GlobalTag = GlobalTag(process.GlobalTag, 'auto:phase2_realistic', '')
 
 process.maxEvents = cms.untracked.PSet( input = cms.untracked.int32(1) )
 process.source = cms.Source("EmptySource")
 
-process.plotter = cms.EDAnalyzer("HGCHEbackSignalScalerAnalyzer",
-                                 doseMap  = cms.string( options.doseMap ),
-                                 doseMapAlgo = cms.uint32(options.doseMapAlgo),
-                                 sipmMap  = cms.string( options.sipmMap ),
-                                 nPEperMIP = cms.uint32( options.nPEperMIP ),
-                                 referenceIdark = cms.double( options.referenceIdark ) )
+process.aged_2020     = cms.EDAnalyzer("HGCHEbackSignalScalerAnalyzer",
+                                       doseMap  = cms.string( options.doseMap ),
+                                       doseMapAlgo = cms.uint32( 2 ),
+                                       sipmMap  = cms.string( 'SimCalorimetry/HGCalSimProducers/data/sipmParams_geom-10.txt' ),
+                                       nPEperMIP = cms.uint32( options.nPEperMIP ),
+                                       referenceIdark = cms.double( -1 ) )
+process.aged_2020_noAreaScaling = process.aged_2020.clone( doseMapAlgo = cms.uint32(2+1+4) )
+process.aged_2020_noRadScaling  = process.aged_2020.clone( doseMapAlgo = cms.uint32(2+8+16) )
+process.aged_2020_noNoise       = process.aged_2020.clone( doseMapAlgo = cms.uint32(2+32) )
+process.aged_2021               = process.aged_2020.clone( doseMapAlgo = cms.uint32(2),
+                                                           referenceIdark = cms.double(0.5) )
 
+#add tfile service
 process.TFileService = cms.Service("TFileService",
-                                   fileName = cms.string("sipmontile_dosemap_output.root")
-                               )
+                                   fileName = cms.string("sipmontile_dosemap_output.root"))
 
-process.p = cms.Path(process.plotter)
+process.p = cms.Path( 
+    process.aged_2020
+    *process.aged_2020_noAreaScaling
+    *process.aged_2020_noRadScaling
+    *process.aged_2020_noNoise
+    *process.aged_2021
+)
+
