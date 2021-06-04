e8a060179e77153cb193dab3f13fba0ba9bd48dc
Christopher Jones <chrisdjones15@gmail.com>
2021-06-04 17:11:21 -0500

    Moved framework dependencies to MuonSensitiveDetectorBuilder
    The sensitive detector now does not depend on any classes from the
    framework. This will allow OscarMTProducer to only ask the builder
    about what it consumes and its needed config parameters.

--- SimG4CMS/Muon/src/MuonSensitiveDetectorBuilder.cc
+++ SimG4CMS/Muon/src/MuonSensitiveDetectorBuilder.cc
@@ -17,9 +17,15 @@
 #include "SimG4Core/Notification/interface/SimActivityRegistryEnroller.h"
 #include "SimG4Core/SensitiveDetector/interface/SensitiveDetectorPluginFactory.h"
 
+#include "Geometry/MuonNumbering/interface/MuonGeometryConstants.h"
+#include "Geometry/Records/interface/IdealGeometryRecord.h"
+
 #include "SimG4CMS/Muon/interface/MuonSensitiveDetector.h"
 
+#include "FWCore/Framework/interface/ESHandle.h"
+#include "FWCore/Framework/interface/EventSetup.h"
 #include "FWCore/PluginManager/interface/ModuleDef.h"
+#include "FWCore/ParameterSet/interface/ParameterSet.h"
 
 class MuonSensitiveDetectorBuilder : public SensitiveDetectorMakerBase {
   SensitiveDetector* make(const std::string& iname,
@@ -28,7 +34,23 @@ class MuonSensitiveDetectorBuilder : public SensitiveDetectorMakerBase {
                           const edm::ParameterSet& p,
                           const SimTrackManager* man,
                           SimActivityRegistry& reg) const override {
-    MuonSensitiveDetector* sd = new MuonSensitiveDetector(iname, es, clg, p, man);
+    edm::ESHandle<MuonOffsetMap> mom;
+    es.get<IdealGeometryRecord>().get(mom);
+    const MuonOffsetMap* offmap = (mom.isValid()) ? mom.product() : nullptr;
+    edm::LogVerbatim("MuonSim") << "Finds the offset map at " << offmap;
+    edm::ESHandle<MuonGeometryConstants> mdc;
+    es.get<IdealGeometryRecord>().get(mdc);
+
+    edm::ParameterSet m_MuonSD = p.getParameter<edm::ParameterSet>("MuonSD");
+    auto ePersistentCutGeV =
+        m_MuonSD.getParameter<double>("EnergyThresholdForPersistency") / CLHEP::GeV;  //Default 1. GeV
+    auto allMuonsPersistent = m_MuonSD.getParameter<bool>("AllMuonsPersistent");
+    auto printHits = m_MuonSD.getParameter<bool>("PrintHits");
+    bool dd4hep = p.getParameter<bool>("g4GeometryDD4hepSource");
+    //
+
+    MuonSensitiveDetector* sd = new MuonSensitiveDetector(
+        iname, offmap, *mdc, clg, ePersistentCutGeV, allMuonsPersistent, printHits, dd4hep, man);
     SimActivityRegistryEnroller::enroll(reg, sd);
     return sd;
   }
