f066ebbf18b31e3d105a6042404beb02f2d7d9f6
Shahzad Malik Muzaffar <Shahzad.Malik.Muzaffar@cern.ch>
2021-06-08 11:47:48 +0200

    [HLT] Apply code-format-all

--- HLTrigger/HLTcore/interface/HLTPrescaler.h
+++ HLTrigger/HLTcore/interface/HLTPrescaler.h
@@ -16,7 +16,7 @@
 #include <memory>
 
 #include "FWCore/Framework/interface/Event.h"
-        #include "FWCore/Framework/interface/Frameworkfwd.h"
+#include "FWCore/Framework/interface/Frameworkfwd.h"
 #include "FWCore/Framework/interface/stream/EDFilter.h"
 #include "FWCore/ParameterSet/interface/ParameterSet.h"
 #include "FWCore/PrescaleService/interface/PrescaleService.h"
