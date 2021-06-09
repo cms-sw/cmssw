2e1919a1950d61c512dc23d99be4557a7a02f929
mmusich <marco.musich@cern.ch>
2021-06-09 10:57:59 +0200

    update architecture in tests and add one more test in testLorentzAngleMap

--- CondCore/SiPixelPlugins/test/testLorentzAngleMap.sh
+++ CondCore/SiPixelPlugins/test/testLorentzAngleMap.sh
@@ -2,7 +2,7 @@
 # Save current working dir so img can be outputted there later
 W_DIR=$(pwd);
 # Set SCRAM architecture var
-SCRAM_ARCH=slc6_amd64_gcc630;
+SCRAM_ARCH=slc7_amd64_gcc900;
 export SCRAM_ARCH;
 source /afs/cern.ch/cms/cmsset_default.sh;
 eval `scram run -sh`;
@@ -36,6 +36,17 @@ getPayloadData.py \
     --test ;
 
 mv *.png $W_DIR/plots_LAMap/FPixPixelLAMap.png
+
+getPayloadData.py \
+    --plugin pluginSiPixelLorentzAngle_PayloadInspector \
+    --plot plot_SiPixelFullLorentzAngleMapByROC \
+    --tag SiPixelLorentzAngle_v11_offline \
+    --time_type Run \
+    --iovs '{"start_iov": "324245", "end_iov": "324245"}' \
+    --db Prod \
+    --test ;
+
+mv *.png $W_DIR/plots_LAMap/PixelLAMap.png
 
 getPayloadData.py \
     --plugin pluginSiPixelLorentzAngle_PayloadInspector \
