c87a1c8954545da0fd12d6657a6f38426d43b18b
Suvankar Roy Chowdhury <suvankar.roy.chowdhury@cern.ch>
2021-06-11 15:41:47 +0200

    bugfix to config and remove unecessary line

--- Validation/SiTrackerPhase2V/python/Phase2OTValidateTrackingRecHit_cff.py
+++ Validation/SiTrackerPhase2V/python/Phase2OTValidateTrackingRecHit_cff.py
@@ -6,5 +6,5 @@ trackingRechitValidOT = Phase2OTValidateTrackingRecHit.clone()
 
 from Configuration.ProcessModifiers.premix_stage2_cff import premix_stage2
 premix_stage2.toModify(trackingRechitValidOT,
-    pixelSimLinkSrc = "mixData:PixelDigiSimLink",
+    phase2TrackerSimLinkSrc = "mixData:Phase2OTDigiSimLink",
 )
