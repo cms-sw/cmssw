e6ac6b4eeed1e514ba44f25f9135c7decd483e81
Sven Dildick <sven.dildick@cern.ch>
2021-06-14 12:50:12 -0500

    Fix minor bug in slopeList

--- L1Trigger/CSCTriggerPrimitives/src/ComparatorCodeLUT.cc
+++ L1Trigger/CSCTriggerPrimitives/src/ComparatorCodeLUT.cc
@@ -146,8 +146,8 @@ int ComparatorCodeLUT::calculateComparatorCode(const pattern& halfStripPattern) 
 }
 
 unsigned ComparatorCodeLUT::convertSlopeToRun2Pattern(const unsigned slope) const {
-  const unsigned slopeList[32] = {10, 10, 10, 8, 8, 8, 6, 6, 6, 6, 4, 4, 2, 2, 2, 2,
-                                  10, 10, 10, 9, 9, 9, 7, 7, 7, 7, 5, 5, 3, 3, 3, 3};
+  const unsigned slopeList[32] = {10, 10, 10, 8, 8, 8, 6, 6, 6, 4, 4, 4, 2, 2, 2, 2,
+                                  10, 10, 10, 9, 9, 9, 7, 7, 7, 5, 5, 5, 3, 3, 3, 3};
   return slopeList[slope];
 }
 
