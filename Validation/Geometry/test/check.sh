bc81d5951663d7d69d5e46a64e5200e61e62c23f
David <David.Lange@cern.ch>
2021-06-16 10:39:02 +0200

    all unit tests to discover the appropriate python version to use (based on what the fwk is using)

--- Validation/Geometry/test/check.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-{
-python3 -c "from FWCore.PythonFramework.CmsRun import CmsRun" 2>/dev/null &&echo "using python3"
-}||
-{
-python -c "from FWCore.PythonFramework.CmsRun import CmsRun" 2>/dev/null && echo "using python2"
-}
