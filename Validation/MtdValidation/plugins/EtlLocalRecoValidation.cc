aea561fc46b0f450d2704024fe66064b963f294d
Massimo Casarsa <massimo.casarsa@ts.infn.it>
2021-06-17 16:49:23 +0200

    Added validation plots with the SIM clusters

--- Validation/MtdValidation/plugins/EtlLocalRecoValidation.cc
+++ Validation/MtdValidation/plugins/EtlLocalRecoValidation.cc
@@ -117,6 +117,11 @@ private:
   MonitorElement* meCluEnergyRes_[2];
   MonitorElement* meCluTPullvsE_[2];
   MonitorElement* meCluTPullvsEta_[2];
+  MonitorElement* meCluXRes_[2];
+  MonitorElement* meCluYRes_[2];
+  MonitorElement* meCluZRes_[2];
+  MonitorElement* meCluYXLocal_[2];
+  MonitorElement* meCluYXLocalSim_[2];
 
   // --- UncalibratedRecHits histograms
 
@@ -440,12 +445,21 @@ void EtlLocalRecoValidation::analyze(const edm::Event& iEvent, const edm::EventS
 
         float time_res = cluster.time() - cluTimeSIM;
         float energy_res = cluster.energy() - cluEneSIM;
+        float x_res = global_point.x() - cluGlobalPosSIM.x();
+        float y_res = global_point.y() - cluGlobalPosSIM.y();
+        float z_res = global_point.z() - cluGlobalPosSIM.z();
 
         meCluTimeRes_[iside]->Fill(time_res);
         meCluEnergyRes_[iside]->Fill(energy_res);
+        meCluXRes_[iside]->Fill(x_res);
+        meCluYRes_[iside]->Fill(y_res);
+        meCluZRes_[iside]->Fill(z_res);
 
-        meCluTPullvsEta_[iside]->Fill(std::abs(cluGlobalPosSIM.eta()), time_res / cluster.timeError());
+        meCluTPullvsEta_[iside]->Fill(cluGlobalPosSIM.eta(), time_res / cluster.timeError());
         meCluTPullvsE_[iside]->Fill(cluEneSIM, time_res / cluster.timeError());
+
+        meCluYXLocal_[iside]->Fill(local_point.x(), local_point.y());
+        meCluYXLocalSim_[iside]->Fill(cluLocalPosSIM.x(), cluLocalPosSIM.y());
 
       }  // if ( cluTimeSIM > 0. &&  cluEneSIM > 0. )
 
@@ -795,9 +809,9 @@ void EtlLocalRecoValidation::bookHistograms(DQMStore::IBooker& ibook,
                                       0.,
                                       100.);
   meTPullvsE_ = ibook.bookProfile(
-      "EtlTPullvsE", "ETL time pull vs E;E_{SIM} [MeV];T_{RECO}-T_{SIM}/#sigma_{T_{RECO}}", 20, 0., 2., -5., 5., "S");
+      "EtlTPullvsE", "ETL time pull vs E;E_{SIM} [MeV];(T_{RECO}-T_{SIM})/#sigma_{T_{RECO}}", 20, 0., 2., -5., 5., "S");
   meTPullvsEta_ = ibook.bookProfile("EtlTPullvsEta",
-                                    "ETL time pull vs #eta;|#eta_{RECO}|;T_{RECO}-T_{SIM}/#sigma_{T_{RECO}}",
+                                    "ETL time pull vs #eta;|#eta_{RECO}|;(T_{RECO}-T_{SIM})/#sigma_{T_{RECO}}",
                                     26,
                                     1.65,
                                     3.0,
@@ -900,40 +914,80 @@ void EtlLocalRecoValidation::bookHistograms(DQMStore::IBooker& ibook,
 
   meCluTPullvsE_[0] =
       ibook.bookProfile("EtlCluTPullvsEZneg",
-                        "ETL cluster time pull vs E (-Z);E_{SIM} [MeV];T_{RECO}-T_{SIM}/#sigma_{T_{RECO}}",
-                        20,
+                        "ETL cluster time pull vs E (-Z);E_{SIM} [MeV];(T_{RECO}-T_{SIM})/#sigma_{T_{RECO}}",
+                        25,
                         0.,
-                        20.,
+                        0.5,
                         -5.,
                         5.,
                         "S");
   meCluTPullvsE_[1] =
       ibook.bookProfile("EtlCluTPullvsEZpos",
-                        "ETL cluster time pull vs E (+Z);E_{SIM} [MeV];T_{RECO}-T_{SIM}/#sigma_{T_{RECO}}",
-                        20,
+                        "ETL cluster time pull vs E (+Z);E_{SIM} [MeV];(T_{RECO}-T_{SIM})/#sigma_{T_{RECO}}",
+                        25,
                         0.,
-                        20.,
+                        0.5,
                         -5.,
                         5.,
                         "S");
   meCluTPullvsEta_[0] =
       ibook.bookProfile("EtlCluTPullvsEtaZneg",
-                        "ETL cluster time pull vs #eta (-Z);|#eta_{RECO}|;T_{RECO}-T_{SIM}/#sigma_{T_{RECO}}",
-                        32,
-                        0,
-                        1.55,
+                        "ETL cluster time pull vs #eta (-Z);|#eta_{RECO}|;(T_{RECO}-T_{SIM})/#sigma_{T_{RECO}}",
+                        30,
+                        -3.,
+                        -1.65,
                         -5.,
                         5.,
                         "S");
   meCluTPullvsEta_[1] =
       ibook.bookProfile("EtlCluTPullvsEtaZpos",
-                        "ETL cluster time pull vs #eta (+Z);|#eta_{RECO}|;T_{RECO}-T_{SIM}/#sigma_{T_{RECO}}",
-                        32,
-                        0,
-                        1.55,
+                        "ETL cluster time pull vs #eta (+Z);|#eta_{RECO}|;(T_{RECO}-T_{SIM})/#sigma_{T_{RECO}}",
+                        30,
+                        1.65,
+                        3.,
                         -5.,
                         5.,
                         "S");
+  meCluXRes_[0] = ibook.book1D("EtlCluXResZneg", "ETL cluster X resolution (-Z);X_{RECO}-X_{SIM} [cm]", 100, -0.1, 0.1);
+  meCluXRes_[1] = ibook.book1D("EtlCluXResZpos", "ETL cluster X resolution (+Z);X_{RECO}-X_{SIM} [cm]", 100, -0.1, 0.1);
+  meCluYRes_[0] = ibook.book1D("EtlCluYResZneg", "ETL cluster Y resolution (-Z);Y_{RECO}-Y_{SIM} [cm]", 100, -0.1, 0.1);
+  meCluYRes_[1] = ibook.book1D("EtlCluYResZpos", "ETL cluster Y resolution (+Z);Y_{RECO}-Y_{SIM} [cm]", 100, -0.1, 0.1);
+  meCluZRes_[0] =
+      ibook.book1D("EtlCluZResZneg", "ETL cluster Z resolution (-Z);Z_{RECO}-Z_{SIM} [cm]", 100, -0.003, 0.003);
+  meCluZRes_[1] =
+      ibook.book1D("EtlCluZResZpos", "ETL cluster Z resolution (+Z);Z_{RECO}-Z_{SIM} [cm]", 100, -0.003, 0.003);
+  meCluYXLocal_[0] = ibook.book2D("EtlCluYXLocalZneg",
+                                  "ETL cluster local Y vs X (-Z);X^{local}_{RECO} [cm];Y^{local}_{RECO} [cm]",
+                                  100,
+                                  -2.2,
+                                  2.2,
+                                  100,
+                                  -1.1,
+                                  1.1);
+  meCluYXLocal_[1] = ibook.book2D("EtlCluYXLocalZpos",
+                                  "ETL cluster local Y vs X (+Z);X^{local}_{RECO} [cm];Y^{local}_{RECO} [cm]",
+                                  100,
+                                  -2.2,
+                                  2.2,
+                                  100,
+                                  -1.1,
+                                  1.1);
+  meCluYXLocalSim_[0] = ibook.book2D("EtlCluYXLocalSimZneg",
+                                     "ETL cluster local Y vs X (-Z);X^{local}_{SIM} [cm];Y^{local}_{SIM} [cm]",
+                                     200,
+                                     -2.2,
+                                     2.2,
+                                     200,
+                                     -1.1,
+                                     1.1);
+  meCluYXLocalSim_[1] = ibook.book2D("EtlCluYXLocalSimZpos",
+                                     "ETL cluster local Y vs X (+Z);X^{local}_{SIM} [cm];Y^{local}_{SIM} [cm]",
+                                     200,
+                                     -2.2,
+                                     2.2,
+                                     200,
+                                     -1.1,
+                                     1.1);
 
   // --- UncalibratedRecHits histograms
 
