90c52de5129f0298359c89ef82453b5208323ea9
jfernan2 <Javier.Fernandez@cern.ch>
2021-06-21 09:24:42 +0200

    Remove obsolete lines

--- DQM/Integration/python/clients/es_dqm_sourceclient-live_cfg.py
+++ DQM/Integration/python/clients/es_dqm_sourceclient-live_cfg.py
@@ -36,9 +36,6 @@ process.esRawToDigi.sourceTag = 'source'
 process.esRawToDigi.debugMode = False
 
 process.load('RecoLocalCalo/EcalRecProducers/ecalPreshowerRecHit_cfi')
-#process.ecalPreshowerRecHit.ESGain = 2
-#process.ecalPreshowerRecHit.ESBaseline = 0
-#process.ecalPreshowerRecHit.ESMIPADC = 50
 process.ecalPreshowerRecHit.ESdigiCollection = "esRawToDigi"
 process.ecalPreshowerRecHit.ESRecoAlgo = 0
 
