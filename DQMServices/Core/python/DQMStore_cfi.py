809e48bce766a5b2fee7c4f673fd365ef2d7dbfa
Javier <Javier.Fernandez@cern.ch>
2021-06-23 10:21:48 +0200

    First prototype of nanoDQMIO with single LS granularity

--- DQMServices/Core/python/DQMStore_cfi.py
+++ DQMServices/Core/python/DQMStore_cfi.py
@@ -4,8 +4,9 @@ DQMStore = cms.Service("DQMStore",
     verbose = cms.untracked.int32(0),
     # similar to LSBasedMode but for offline. Explicitly sets LumiFLag on all
     # MEs/modules that allow it (canSaveByLumi)
-    saveByLumi = cms.untracked.bool(False),
-    trackME = cms.untracked.string(""),
+    saveByLumi = cms.untracked.bool(True),
+    MEsToSave = cms.untracked.vstring('Muons/MuonRecoAnalyzer/','Muons/MuonIdDQM/GlobalMuons/hDT1Pullx'),
+    trackME = cms.untracked.string("Muons/MuonRecoAnalyzer/Res_TkSta_theta"),
 
     # UNUSED: historical HLT configs expect this option to be present, so it
     # remains here, even though the DQMStore does not use it any more.
