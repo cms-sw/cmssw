3092f70e382efde9aa4736ab3e06563393a43dbd
Javier <Javier.Fernandez@cern.ch>
2021-06-24 16:52:03 +0200

    Fix for defaults

--- DQMServices/Core/python/DQMStore_cfi.py
+++ DQMServices/Core/python/DQMStore_cfi.py
@@ -5,10 +5,10 @@ DQMStore = cms.Service("DQMStore",
     verbose = cms.untracked.int32(0),
     # similar to LSBasedMode but for offline. Explicitly sets LumiFLag on all
     # MEs/modules that allow it (canSaveByLumi)
-    saveByLumi = cms.untracked.bool(True),
+    saveByLumi = cms.untracked.bool(False),
     #Following list has no effect if saveByLumi is False
     MEsToSave = cms.untracked.vstring(nanoDQMIO_perLSoutput.MEsToSave),  
-    trackME = cms.untracked.string("Muons/MuonRecoAnalyzer/Res_TkSta_theta"),
+    trackME = cms.untracked.string(""),
 
     # UNUSED: historical HLT configs expect this option to be present, so it
     # remains here, even though the DQMStore does not use it any more.
