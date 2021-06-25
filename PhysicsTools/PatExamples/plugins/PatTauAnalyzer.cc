de74f55d5ebbbf645918b876d5995721b50004bb
Jonas Rembser <jonas.rembser@cern.ch>
2021-06-25 19:00:00 +0200

    Merge .h and .cc files of plugins in several PhysicsTools packages

--- PhysicsTools/PatExamples/plugins/PatTauAnalyzer.cc
+++ PhysicsTools/PatExamples/plugins/PatTauAnalyzer.cc
@@ -1,11 +1,74 @@
-#include "PhysicsTools/PatExamples/plugins/PatTauAnalyzer.h"
-
+#include "CommonTools/UtilAlgos/interface/TFileService.h"
+#include "DataFormats/PatCandidates/interface/Tau.h"
+#include "FWCore/Framework/interface/EDAnalyzer.h"
+#include "FWCore/Framework/interface/Event.h"
+#include "FWCore/Framework/interface/EventSetup.h"
 #include "FWCore/Framework/interface/Frameworkfwd.h"
 #include "FWCore/MessageLogger/interface/MessageLogger.h"
+#include "FWCore/ParameterSet/interface/ParameterSet.h"
 #include "FWCore/ServiceRegistry/interface/Service.h"
-#include "CommonTools/UtilAlgos/interface/TFileService.h"
+#include "FWCore/Utilities/interface/InputTag.h"
 
+#include <TH1.h>
 #include <TMath.h>
+
+#include <string>
+
+class PatTauAnalyzer : public edm::EDAnalyzer {
+public:
+  explicit PatTauAnalyzer(const edm::ParameterSet&);
+  ~PatTauAnalyzer() override;
+
+  //--- methods inherited from EDAnalyzer base-class
+  void beginJob() override;
+  void analyze(const edm::Event&, const edm::EventSetup&) override;
+  void endJob() override;
+
+private:
+  //--- configuration parameters
+  edm::InputTag src_;
+  edm::EDGetTokenT<pat::TauCollection> srcToken_;
+
+  bool requireGenTauMatch_;
+
+  std::string discrByLeadTrack_;
+  std::string discrByIso_;
+  std::string discrByTaNC_;
+
+  //--- generator level histograms
+  TH1* hGenTauEnergy_;
+  TH1* hGenTauPt_;
+  TH1* hGenTauEta_;
+  TH1* hGenTauPhi_;
+
+  //--- reconstruction level histograms
+  TH1* hTauJetEnergy_;
+  TH1* hTauJetPt_;
+  TH1* hTauJetEta_;
+  TH1* hTauJetPhi_;
+
+  TH1* hNumTauJets_;
+
+  TH1* hTauLeadTrackPt_;
+
+  TH1* hTauNumSigConeTracks_;
+  TH1* hTauNumIsoConeTracks_;
+
+  TH1* hTauDiscrByIso_;
+  TH1* hTauDiscrByTaNC_;
+  TH1* hTauDiscrAgainstElectrons_;
+  TH1* hTauDiscrAgainstMuons_;
+
+  TH1* hTauJetEnergyIsoPassed_;
+  TH1* hTauJetPtIsoPassed_;
+  TH1* hTauJetEtaIsoPassed_;
+  TH1* hTauJetPhiIsoPassed_;
+
+  TH1* hTauJetEnergyTaNCpassed_;
+  TH1* hTauJetPtTaNCpassed_;
+  TH1* hTauJetEtaTaNCpassed_;
+  TH1* hTauJetPhiTaNCpassed_;
+};
 
 const reco::GenParticle* getGenTau(const pat::Tau& patTau) {
   std::vector<reco::GenParticleRef> associatedGenParticles = patTau.genParticleRefs();
