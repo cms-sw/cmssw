d8fed33ca489b90b6e0e0286f81bbf73c10dfdc4
W. David Dagenhart <wdd@fnal.gov>
2021-06-28 23:08:02 +0200

    Use catch2 STATIC_REQUIRE instead of REQUIRE

--- FWCore/Framework/test/test_catch2notTP_InputProcessBlock.cc
+++ FWCore/Framework/test/test_catch2notTP_InputProcessBlock.cc
@@ -31,38 +31,38 @@ namespace edmtest {
 
 TEST_CASE("test InputProcessBlock", "[InputProcessBlock]") {
   SECTION("test HasAbility") {
-    REQUIRE(edmtest::TestAnalyzerWithInputProcessBlockAbility::HasAbility::kInputProcessBlockCache);
-    REQUIRE(!edmtest::TestAnalyzerWithoutInputProcessBlockAbility::HasAbility::kInputProcessBlockCache);
+    STATIC_REQUIRE(edmtest::TestAnalyzerWithInputProcessBlockAbility::HasAbility::kInputProcessBlockCache);
+    STATIC_REQUIRE(!edmtest::TestAnalyzerWithoutInputProcessBlockAbility::HasAbility::kInputProcessBlockCache);
   }
   SECTION("test type aliases") {
-    REQUIRE(std::is_same<edmtest::TestAnalyzerWithInputProcessBlockAbility::InputProcessBlockCache,
-                         edm::impl::InputProcessBlockCacheImpl<int, unsigned int, int>>());
-    REQUIRE(std::is_same<edmtest::TestAnalyzerWithoutInputProcessBlockAbility::InputProcessBlockCache, void>());
-    REQUIRE(std::is_same<edm::stream::impl::choose_unique_ptr<
-                             edmtest::TestAnalyzerWithInputProcessBlockAbility::InputProcessBlockCache>::type,
-                         std::unique_ptr<edm::impl::InputProcessBlockCacheImpl<int, unsigned int, int>>>());
-    REQUIRE(std::is_same<edm::stream::impl::choose_unique_ptr<
-                             edmtest::TestAnalyzerWithoutInputProcessBlockAbility::InputProcessBlockCache>::type,
-                         edm::stream::impl::dummy_ptr>());
+    STATIC_REQUIRE(std::is_same<edmtest::TestAnalyzerWithInputProcessBlockAbility::InputProcessBlockCache,
+                                edm::impl::InputProcessBlockCacheImpl<int, unsigned int, int>>());
+    STATIC_REQUIRE(std::is_same<edmtest::TestAnalyzerWithoutInputProcessBlockAbility::InputProcessBlockCache, void>());
+    STATIC_REQUIRE(std::is_same<edm::stream::impl::choose_unique_ptr<
+                                    edmtest::TestAnalyzerWithInputProcessBlockAbility::InputProcessBlockCache>::type,
+                                std::unique_ptr<edm::impl::InputProcessBlockCacheImpl<int, unsigned int, int>>>());
+    STATIC_REQUIRE(std::is_same<edm::stream::impl::choose_unique_ptr<
+                                    edmtest::TestAnalyzerWithoutInputProcessBlockAbility::InputProcessBlockCache>::type,
+                                edm::stream::impl::dummy_ptr>());
 
-    REQUIRE(std::is_same<edmtest::TestFilterWithInputProcessBlockAbility::InputProcessBlockCache,
-                         edm::impl::InputProcessBlockCacheImpl<int, unsigned int, int>>());
-    REQUIRE(std::is_same<edmtest::TestFilterWithoutInputProcessBlockAbility::InputProcessBlockCache, void>());
-    REQUIRE(std::is_same<edm::stream::impl::choose_unique_ptr<
-                             edmtest::TestFilterWithInputProcessBlockAbility::InputProcessBlockCache>::type,
-                         std::unique_ptr<edm::impl::InputProcessBlockCacheImpl<int, unsigned int, int>>>());
-    REQUIRE(std::is_same<edm::stream::impl::choose_unique_ptr<
-                             edmtest::TestFilterWithoutInputProcessBlockAbility::InputProcessBlockCache>::type,
-                         edm::stream::impl::dummy_ptr>());
+    STATIC_REQUIRE(std::is_same<edmtest::TestFilterWithInputProcessBlockAbility::InputProcessBlockCache,
+                                edm::impl::InputProcessBlockCacheImpl<int, unsigned int, int>>());
+    STATIC_REQUIRE(std::is_same<edmtest::TestFilterWithoutInputProcessBlockAbility::InputProcessBlockCache, void>());
+    STATIC_REQUIRE(std::is_same<edm::stream::impl::choose_unique_ptr<
+                                    edmtest::TestFilterWithInputProcessBlockAbility::InputProcessBlockCache>::type,
+                                std::unique_ptr<edm::impl::InputProcessBlockCacheImpl<int, unsigned int, int>>>());
+    STATIC_REQUIRE(std::is_same<edm::stream::impl::choose_unique_ptr<
+                                    edmtest::TestFilterWithoutInputProcessBlockAbility::InputProcessBlockCache>::type,
+                                edm::stream::impl::dummy_ptr>());
 
-    REQUIRE(std::is_same<edmtest::TestProducerWithInputProcessBlockAbility::InputProcessBlockCache,
-                         edm::impl::InputProcessBlockCacheImpl<int, unsigned int, int>>());
-    REQUIRE(std::is_same<edmtest::TestProducerWithoutInputProcessBlockAbility::InputProcessBlockCache, void>());
-    REQUIRE(std::is_same<edm::stream::impl::choose_unique_ptr<
-                             edmtest::TestProducerWithInputProcessBlockAbility::InputProcessBlockCache>::type,
-                         std::unique_ptr<edm::impl::InputProcessBlockCacheImpl<int, unsigned int, int>>>());
-    REQUIRE(std::is_same<edm::stream::impl::choose_unique_ptr<
-                             edmtest::TestProducerWithoutInputProcessBlockAbility::InputProcessBlockCache>::type,
-                         edm::stream::impl::dummy_ptr>());
+    STATIC_REQUIRE(std::is_same<edmtest::TestProducerWithInputProcessBlockAbility::InputProcessBlockCache,
+                                edm::impl::InputProcessBlockCacheImpl<int, unsigned int, int>>());
+    STATIC_REQUIRE(std::is_same<edmtest::TestProducerWithoutInputProcessBlockAbility::InputProcessBlockCache, void>());
+    STATIC_REQUIRE(std::is_same<edm::stream::impl::choose_unique_ptr<
+                                    edmtest::TestProducerWithInputProcessBlockAbility::InputProcessBlockCache>::type,
+                                std::unique_ptr<edm::impl::InputProcessBlockCacheImpl<int, unsigned int, int>>>());
+    STATIC_REQUIRE(std::is_same<edm::stream::impl::choose_unique_ptr<
+                                    edmtest::TestProducerWithoutInputProcessBlockAbility::InputProcessBlockCache>::type,
+                                edm::stream::impl::dummy_ptr>());
   }
 }
