9181fe9604cdd3befdd2c9b6f5733fe6067ef20f
Sunanda Banerjee <sunanda@cmsdev34.cern.ch>
2021-06-28 15:42:24 +0200

    Remove all future conflicts

--- Configuration/Geometry/python/GeometrySimDB_cff.py
+++ Configuration/Geometry/python/GeometrySimDB_cff.py
@@ -8,5 +8,8 @@ from GeometryReaders.XMLIdealGeometryESSource.cmsGeometryDB_cff import *
 from Geometry.MuonNumbering.muonNumberingInitialization_cfi import *
 from Geometry.TrackerNumberingBuilder.trackerNumberingGeometryDB_cfi import *
 from Geometry.EcalCommonData.ecalSimulationParameters_cff   import *
-from Geometry.HcalCommonData.hcalSimDBConstants_cff         import *
+from Geometry.HcalCommonData.hcalSimulationParameters_cfi   import *
+from Geometry.HcalCommonData.hcalDDDSimConstants_cfi        import *
+from Geometry.HcalCommonData.hcalSimulationConstants_cfi    import *
+from Geometry.HcalCommonData.caloSimulationParameters_cff   import *
 from Geometry.MuonNumbering.muonGeometryConstants_cff       import *
