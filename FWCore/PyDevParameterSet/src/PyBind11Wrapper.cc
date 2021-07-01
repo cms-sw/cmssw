fe08cadf7e50d8d4cf791eb3bd6b7cc9a97b9e6e
David <David.Lange@cern.ch>
2021-07-01 17:15:43 +0200

    change default parameter set to python3. Remove now obsolete PyDevParameterSet

--- FWCore/PyDevParameterSet/src/PyBind11Wrapper.cc
+++
@@ -1,9 +0,0 @@
-#include "FWCore/PyDevParameterSet/interface/PyBind11Wrapper.h"
-#include "FWCore/Utilities/interface/Exception.h"
-//#include <iostream>
-namespace cmspython3 {
-
-  void pythonToCppException(const std::string& iType, const std::string& error) {
-    throw cms::Exception(iType) << " unknown python problem occurred.\n" << error << std::endl;
-  }
-}  // namespace cmspython3
