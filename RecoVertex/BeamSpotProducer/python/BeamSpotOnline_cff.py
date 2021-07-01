df4ecf239917c9d83b877ea58196758fd8769d8e
gennai <simone.gennai@cern.ch>
2021-07-01 11:01:01 +0200

    modified the various (ES)producers for proper express stream and HLT beamspot use for Run3

--- RecoVertex/BeamSpotProducer/python/BeamSpotOnline_cff.py
+++ RecoVertex/BeamSpotProducer/python/BeamSpotOnline_cff.py
@@ -3,6 +3,7 @@ import FWCore.ParameterSet.Config as cms
 from RecoVertex.BeamSpotProducer.BeamSpotOnline_cfi import *
 
 #scalers = cms.EDProducer('ScalersRawToDigi')
+BeamSpotESProducer = cms.ESProducer("OnlineBeamSpotESProducer")
 
 onlineBeamSpot = cms.Sequence( onlineBeamSpotProducer )
 
