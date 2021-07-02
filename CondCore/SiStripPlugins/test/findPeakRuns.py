d263edd7e81d8e6d31d73eb3b7a900064be178bb
mmusich <marco.musich@cern.ch>
2021-07-02 16:25:31 +0200

    update findPeakRuns script to include runs in which tracker is in DAQ

--- CondCore/SiStripPlugins/test/findPeakRuns.py
+++ CondCore/SiStripPlugins/test/findPeakRuns.py
@@ -4,13 +4,17 @@ Script used to find all PEAK runs
 Usage:
 
 create the list of SiStripLatency::singleReadOutMode() IOVs / values with:
-getPayloadData.py --plugin pluginSiStripLatency_PayloadInspector --plot plot_SiStripIsPeakModeHistory --tag SiStripLatency_GR10_v2_hlt --time_type Run --iovs '{"start_iov": "1", "end_iov": "400000"}' --db Prod --test > & log.json 
+
+getPayloadData.py --plugin pluginSiStripLatency_PayloadInspector --plot plot_SiStripIsPeakModeHistory --tag SiStripLatency_GR10_v2_hlt --time_type Run --iovs '{"start_iov": "1", "end_iov": "400000"}' --db Prod --test > & log.json
+
+create the list of runs with the Strip Tracker in global DAQ
+
+getPayloadData.py --plugin pluginRunInfo_PayloadInspector --plot plot_RunInfoTrackerHistory --tag runinfo_31X_hlt --time_type Run --iovs '{"start_iov": "1", "end_iov": "400000"}' --db Prod --test > & isTrackerin.json
 
 followed by:
 
-findPeakRuns.py -f log.json > allPeakRuns.log
+python findPeakRuns.py -f log.json -r isTrackerIn.json > & allTrackerRuns.csv &
 '''
-
 
 from __future__ import print_function
 import json
@@ -54,13 +58,15 @@ def findPeakIOVs(values):
 parser = OptionParser()
 parser.add_option("-f", "--file", dest="filename",
                   help="open FILE and extract info", metavar="FILE")
+parser.add_option("-r", "--runfile", dest="runfilename",
+                  help="open RUNFILE and extract info", metavar="RUNFILE")
 parser.add_option("-v", "--verbose",
                   action="store_true", dest="verbose", default=False,
                   help="Print PEAK/DECO dictionary")
 
 (options, args) = parser.parse_args()
 
-with open(options.filename) as data_file:    
+with open(options.filename) as data_file:
     data = json.load(data_file)
     values = data["data"]
     
@@ -68,16 +74,42 @@ IOVs = findPeakIOVs(values)
 if(options.verbose):
     pprint(IOVs)
 
-con = conddb.connect(url = conddb.make_url("pro"))
-session = con.session()
-RUNINFO = session.get_dbtype(conddb.RunInfo)
+with open(options.runfilename) as rundata_file:
+    data = json.load(rundata_file)
+    runvalues = data["data"]
+
+isTrackerIn = {}
+for entry in runvalues:
+    isTrackerIn[entry['x']]=entry['y']
+
+# for value in runvalues:
+#     isTrackerIn = bool(value['y'])
+#     runnumber = int(value['x'])
+#     if(not isTrackerIn):
+#         # there was no Tracker in this run
+#         continue
+#     else:
+#         for key, value in IOVs.items():
+#             if(key[0]<runnumber and key[1]>runnumber):
+#                 print(runnumber,",",key[0],"-",key[1],",",value
 
 ####################################
 # Get the whole list of runs
 ####################################
-AllRuns = session.query(RUNINFO.run_number, RUNINFO.start_time, RUNINFO.end_time).all()
-for run in AllRuns:
-    for key, value in IOVs.items():
-        if(key[0]<run[0] and key[1]>run[0]):
-            if(value=='PEAK'):
-                print(run[0],"(",run[1],") IOV [",key[0],"-",key[1],") which is ",value," mode")
+con = conddb.connect(url = conddb.make_url("pro"))
+session = con.session()
+RUNINFO = session.get_dbtype(conddb.RunInfo)
+allRuns = session.query(RUNINFO.run_number, RUNINFO.start_time, RUNINFO.end_time).all()
+for run in allRuns:
+    if run[0] not in isTrackerIn:
+        continue
+    if(isTrackerIn[run[0]]):
+        #print(run[0],"Tracker In")
+        for key, value in IOVs.items():
+            if(key[0]<run[0] and key[1]>run[0]):
+             #if(value=='PEAK'):
+             #    print(run[0],"(",run[1].strftime('%Y-%m-%d'),") [",key[0],"-",key[1],") which is ",value," mode")
+                print(run[0],",",run[1].strftime('%Y-%m-%d'),",",key[0],"-",key[1],",",value)
+    else:
+        pass
+        #print(run[0],"Tracker Out")
