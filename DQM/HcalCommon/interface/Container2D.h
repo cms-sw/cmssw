77c2fa179b2028aa55ab3062a53af4e1d72a7a93
Long <long.wang@cern.ch>
2021-07-04 16:33:24 +0200

    Revert "Adding tp data and emulation plots for all time slices and split based on slot"
    This reverts commit 2c67a1ae4f06e2c3682f772a13498aa1b9e4db20.

--- DQM/HcalCommon/interface/Container2D.h
+++ DQM/HcalCommon/interface/Container2D.h
@@ -126,8 +126,6 @@ namespace hcaldqm {
     void fill(HcalTrigTowerDetId const &, int, double) override;
     void fill(HcalTrigTowerDetId const &, double, double) override;
 
-    void fill(HcalTrigTowerDetId const &, HcalElectronicsId const &, int, int);
-
     double getBinEntries(HcalTrigTowerDetId const &) override;
     double getBinEntries(HcalTrigTowerDetId const &, int) override;
     double getBinEntries(HcalTrigTowerDetId const &, double) override;
