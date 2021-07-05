a37402f6d454fdb2af06231d17add2bb1fa676c2
Angela Czirkos <czirkosangela@gmail.com>
2021-07-05 10:39:15 +0200

    code-checks

--- CUDADataFormats/TrackingRecHit/interface/SiPixelHitStatus.h
+++ CUDADataFormats/TrackingRecHit/interface/SiPixelHitStatus.h
@@ -5,11 +5,11 @@
 
 // more information on bit fields : https://en.cppreference.com/w/cpp/language/bit_field
 struct SiPixelHitStatus {
-  bool isBigX : 1;    //  ∈[0,1]
-  bool isOneX : 1;    //  ∈[0,1]
-  bool isBigY : 1;    //  ∈[0,1]
-  bool isOneY : 1;    //  ∈[0,1]
-  uint8_t qBin : 3;   //  ∈[0,1,...,7]
+  bool isBigX : 1;   //  ∈[0,1]
+  bool isOneX : 1;   //  ∈[0,1]
+  bool isBigY : 1;   //  ∈[0,1]
+  bool isOneY : 1;   //  ∈[0,1]
+  uint8_t qBin : 3;  //  ∈[0,1,...,7]
 };
 
 #endif
