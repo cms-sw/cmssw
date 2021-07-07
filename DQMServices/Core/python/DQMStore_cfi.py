f189ed961bb96da515a3bacbd23ed042ae207737
jfernan2 <Javier.Fernandez@cern.ch>
2021-07-07 13:20:51 +0200

    Update DQMStore_cfi.py
    Fix mistake

--- DQMServices/Core/python/DQMStore_cfi.py
+++ DQMServices/Core/python/DQMStore_cfi.py
@@ -5,7 +5,7 @@ DQMStore = cms.Service("DQMStore",
     verbose = cms.untracked.int32(0),
     # similar to LSBasedMode but for offline. Explicitly sets LumiFLag on all
     # MEs/modules that allow it (canSaveByLumi)
-    saveByLumi = cms.untracked.bool(True),
+    saveByLumi = cms.untracked.bool(False),
     #Following list has no effect if saveByLumi is False
     MEsToSave = cms.untracked.vstring(nanoDQMIO_perLSoutput.MEsToSave),  
     trackME = cms.untracked.string(""),
