053a393c5ab6308f936131f837b6649fb138e98e
kpanos <panos.katsoulis@cern.ch>
2021-07-07 12:28:41 +0200

    changes in the L1Config utilities, adapt Writer and upload config

--- L1TriggerConfig/Utilities/src/L1TMuonBarrelParamsWriter.cc
+++ L1TriggerConfig/Utilities/src/L1TMuonBarrelParamsWriter.cc
@@ -9,8 +9,9 @@
 #include "FWCore/Framework/interface/ESHandle.h"
 
 #include "CondFormats/DataRecord/interface/L1TMuonBarrelParamsO2ORcd.h"
-#include "CondFormats/DataRecord/interface/L1TMuonBarrelParamsRcd.h"
+#include "CondFormats/DataRecord/interface/L1TMuonBarrelParamsRcd.h" // includes both Old-Phase1 and Kalman Rcds
 #include "CondFormats/L1TObjects/interface/L1TMuonBarrelParams.h"
+#include "CondFormats/L1TObjects/interface/L1TMuonBarrelKalmanParams.h"
 
 #include "FWCore/ServiceRegistry/interface/Service.h"
 #include "CondCore/DBOutputService/interface/PoolDBOutputService.h"
@@ -30,19 +31,24 @@ public:
 
 void L1TMuonBarrelParamsWriter::analyze(const edm::Event& iEvent, const edm::EventSetup& evSetup) {
   edm::ESHandle<L1TMuonBarrelParams> handle1;
+  edm::ESHandle<L1TMuonBarrelKalmanParams> handle2;
 
   if (isO2Opayload)
     evSetup.get<L1TMuonBarrelParamsO2ORcd>().get(handle1);
-  else
+  else {
     evSetup.get<L1TMuonBarrelParamsRcd>().get(handle1);
+    evSetup.get<L1TMuonBarrelKalmanParamsRcd>().get(handle2);
+  }
 
   std::shared_ptr<L1TMuonBarrelParams> ptr1(new L1TMuonBarrelParams(*(handle1.product())));
+  std::shared_ptr<L1TMuonBarrelKalmanParams> ptr2(new L1TMuonBarrelKalmanParams(*(handle2.product())));
 
   edm::Service<cond::service::PoolDBOutputService> poolDb;
   if (poolDb.isAvailable()) {
     cond::Time_t firstSinceTime = poolDb->beginOfTime();
-    poolDb->writeOne(
-        ptr1.get(), firstSinceTime, (isO2Opayload ? "L1TMuonBarrelParamsO2ORcd" : "L1TMuonBarrelParamsRcd"));
+    poolDb->writeOne( ptr1.get(), firstSinceTime, (isO2Opayload ? "L1TMuonBarrelParamsO2ORcd" : "L1TMuonBarrelParamsRcd") );
+    if (not isO2Opayload)
+      poolDb->writeOne( ptr2.get(), firstSinceTime, ("L1TMuonBarrelKalmanParamsRcd") );
   }
 }
 
