c2eba25c4efad5c984fc1cf40a064d2d9cea8031
Sven Dildick <sven.dildick@cern.ch>
2021-07-08 15:52:56 -0500

    Move CCLUT (again) so that it is done each time a single CLCT is found. This is the most accurate representation of CCLUT in the firmware.

--- L1Trigger/CSCTriggerPrimitives/interface/CSCUpgradeCathodeLCTProcessor.h
+++ L1Trigger/CSCTriggerPrimitives/interface/CSCUpgradeCathodeLCTProcessor.h
@@ -31,31 +31,34 @@ public:
                                 unsigned chamber,
                                 const edm::ParameterSet& conf);
 
-protected:
-  /* is a given half-strip in a pretrigger dead zone */
-  bool busyMap[CSCConstants::MAX_NUM_HALF_STRIPS_RUN2_TRIGGER][CSCConstants::MAX_CLCT_TBINS];
-
 private:
-  /** VK: use of localized dead-time zones */
-  bool use_dead_time_zoning;
-
-  /* +/- half-strips around a keystrip that are dead
-     for a few BX after a CLCT was found*/
-  unsigned int clct_state_machine_zone;
-
-  /** VK: allow triggers only in +-pretrig_trig_zone around pretriggers */
-  unsigned int pretrig_trig_zone;
-
-  /** VK: whether to use corrected_bx instead of pretrigger BX */
-  bool use_corrected_bx;
+  /* Phase2 version. Check all half-strip pattern envelopes simultaneously,
+     on every clock cycle, for a matching pattern */
+  bool preTrigger(const int start_bx, int& first_bx) override;
 
   /* Phase-2 version of the CLCT finder function */
   std::vector<CSCCLCTDigi> findLCTs(
       const std::vector<int> halfstrip[CSCConstants::NUM_LAYERS][CSCConstants::MAX_NUM_HALF_STRIPS_RUN2_TRIGGER])
       override;
 
-  /* Phase2 version,  Check all half-strip pattern envelopes simultaneously, on every clock cycle, for a matching pattern */
-  bool preTrigger(const PulseArray pulse, const int start_bx, int& first_bx) override;
+  // mark half-strip zones around pretriggers that happened at the current first_bx
+  void markPreTriggerZone(bool pretrig_zone[CSCConstants::MAX_NUM_HALF_STRIPS_RUN2_TRIGGER]) const;
+
+  // update the deadzone after finding a pretrigger
+  void markBusyZone(const int bx);
+
+  /* is a given half-strip in a pretrigger dead zone */
+  bool busyMap_[CSCConstants::MAX_NUM_HALF_STRIPS_RUN2_TRIGGER][CSCConstants::MAX_CLCT_TBINS];
+
+  /** VK: use of localized dead-time zones */
+  bool use_dead_time_zoning_;
+
+  /* +/- half-strips around a keystrip that are dead
+     for a few BX after a CLCT was found*/
+  unsigned int clct_state_machine_zone_;
+
+  /** VK: allow triggers only in +-pretrig_trig_zone around pretriggers */
+  unsigned int pretrig_trig_zone_;
 };
 
 #endif
