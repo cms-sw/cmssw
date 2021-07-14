20af301a4c8eea9d6e2f7f6c1566bac80efaf876
Adinda de Wit <adinda.maite.de.wit@cern.ch>
2021-07-14 18:00:42 +0200

    Adding comments on the presence of PTrackerAdditionalParametersPerDet

--- Configuration/Geometry/python/GeometryRecoTracker_cff.py
+++ Configuration/Geometry/python/GeometryRecoTracker_cff.py
@@ -6,6 +6,7 @@ from Geometry.CommonTopologies.globalTrackingGeometry_cfi import *
 
 # Tracker
 from RecoTracker.GeometryESProducer.TrackerRecoGeometryESProducer_cfi import *
+# TrackerAdditionalParametersPerDet contains only default values, needed for consistency with Phase 2
 from Geometry.TrackerGeometryBuilder.TrackerAdditionalParametersPerDet_cfi import *
 
 # Alignment
