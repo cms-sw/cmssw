fe72ac6fdc1a007fe43ce66a4c457d6db51093fe
Jason Lee <jason.lee@cern.ch>
2021-07-15 06:00:47 +0900

    added status collections added GEMPackingTester

---
+++ EventFilter/GEMRawToDigi/plugins/GEMPackingTester.cc
@@ -0,0 +1,129 @@
+#include <memory>
+#include <iostream>
+#include <TTree.h>
+
+#include "FWCore/Framework/interface/Frameworkfwd.h"
+#include "FWCore/Framework/interface/one/EDAnalyzer.h"
+#include "FWCore/ServiceRegistry/interface/Service.h"
+#include "CommonTools/UtilAlgos/interface/TFileService.h"
+
+#include "FWCore/Framework/interface/Event.h"
+#include "FWCore/Framework/interface/MakerMacros.h"
+
+#include "FWCore/ParameterSet/interface/ParameterSet.h"
+#include "FWCore/Utilities/interface/InputTag.h"
+#include "DataFormats/FEDRawData/interface/FEDNumbering.h"
+#include "DataFormats/FEDRawData/interface/FEDRawDataCollection.h"
+#include "DataFormats/FEDRawData/interface/FEDTrailer.h"
+#include "DataFormats/GEMDigi/interface/GEMDigiCollection.h"
+
+using namespace std;
+class GEMPackingTester : public edm::one::EDAnalyzer<edm::one::SharedResources> {
+public:
+  explicit GEMPackingTester(const edm::ParameterSet&);
+  ~GEMPackingTester();
+
+  static void fillDescriptions(edm::ConfigurationDescriptions& descriptions);
+
+private:
+  void beginJob() override;
+  void analyze(const edm::Event&, const edm::EventSetup&) override;
+  void endJob() override;
+
+  edm::EDGetTokenT<FEDRawDataCollection> fedToken_;
+  edm::EDGetTokenT<GEMDigiCollection> gemDigiToken_;
+  edm::EDGetTokenT<GEMDigiCollection> gemSimDigiToken_;
+
+  TTree* tree_;
+  int b_ge0, b_ge1, b_ge2;
+};
+
+GEMPackingTester::GEMPackingTester(const edm::ParameterSet& iConfig)
+    : fedToken_(consumes<FEDRawDataCollection>(iConfig.getParameter<edm::InputTag>("fed"))),
+      gemDigiToken_(consumes<GEMDigiCollection>(iConfig.getParameter<edm::InputTag>("gemDigi"))),
+      gemSimDigiToken_(consumes<GEMDigiCollection>(iConfig.getParameter<edm::InputTag>("gemSimDigi"))) {
+  usesResource("TFileService");
+  edm::Service<TFileService> fs;
+  tree_ = fs->make<TTree>("fed", "fed");
+  tree_->Branch("ge0", &b_ge0, "ge0/I");
+  tree_->Branch("ge1", &b_ge1, "ge1/I");
+  tree_->Branch("ge2", &b_ge2, "ge2/I");
+}
+
+GEMPackingTester::~GEMPackingTester() {}
+
+void GEMPackingTester::analyze(const edm::Event& iEvent, const edm::EventSetup& iSetup) {
+  using namespace edm;
+
+  b_ge0 = 0;
+  b_ge1 = 0;
+  b_ge2 = 0;
+
+  edm::Handle<FEDRawDataCollection> fed_buffers;
+  iEvent.getByToken(fedToken_, fed_buffers);
+  for (unsigned int fedId = FEDNumbering::MINGEMFEDID; fedId <= FEDNumbering::MAXGEMFEDID; ++fedId) {
+    const FEDRawData& fedData = fed_buffers->FEDData(fedId);
+
+    //int nWords = fedData.size() / sizeof(uint64_t);
+    // std::cout << " fedId:" << fedId
+    //           << " fed Size:" << fedData.size()
+    //           << " words:" << nWords
+    //           << std::endl;
+
+    if (fedId == 1473 or fedId == 1474)
+      b_ge0 += fedData.size();
+    if (fedId == 1467 or fedId == 1468)
+      b_ge1 += fedData.size();
+    if (fedId == 1469 or fedId == 1470)
+              b_ge2 += fedData.size();
+  }
+  // std::cout << " ge0:" << b_ge0
+  //           << " ge1:" << b_ge1
+  //           << " ge2:" << b_ge2
+  //           << std::endl;
+
+  edm::Handle<GEMDigiCollection> gemDigis;
+  iEvent.getByToken(gemDigiToken_, gemDigis);
+  edm::Handle<GEMDigiCollection> gemSimDigis;
+  iEvent.getByToken(gemSimDigiToken_, gemSimDigis);
+
+  for (auto const& simDigi : *gemSimDigis) {
+    const GEMDetId& gemId = simDigi.first;
+    const GEMDigiCollection::Range& sim = simDigi.second;
+    const GEMDigiCollection::Range& packed = gemDigis->get(gemId);
+
+    int nsims = 0;
+    for (auto digi = sim.first; digi != sim.second; ++digi) {
+      nsims++;
+    }
+    int npacked = 0;
+    for (auto digi = packed.first; digi != packed.second; ++digi) {
+      npacked++;
+    }
+    if (nsims != npacked) {
+      cout << gemId << endl;
+      for (auto digi = sim.first; digi != sim.second; ++digi) {
+        cout << "sim " << digi->strip() << " " << digi->bx() << endl;
+      }
+      for (auto digi = packed.first; digi != packed.second; ++digi) {
+        cout << "rec " << digi->strip() << " " << digi->bx() << endl;
+      }
+    }
+  }
+
+  tree_->Fill();
+}
+
+void GEMPackingTester::beginJob() {}
+
+void GEMPackingTester::endJob() {}
+
+void GEMPackingTester::fillDescriptions(edm::ConfigurationDescriptions& descriptions) {
+  edm::ParameterSetDescription desc;
+  desc.add<edm::InputTag>("fed", edm::InputTag("rawDataCollector"));
+  desc.add<edm::InputTag>("gemDigi", edm::InputTag("muonGEMDigis"));
+  desc.add<edm::InputTag>("gemSimDigi", edm::InputTag("simMuonGEMDigis"));
+  descriptions.add("GEMPackingTester", desc);
+}
+
+DEFINE_FWK_MODULE(GEMPackingTester);
---
+++ EventFilter/GEMRawToDigi/test/gemUnPackerTester.py
@@ -0,0 +1,39 @@
+import FWCore.ParameterSet.Config as cms
+from Configuration.Eras.Era_Phase2C11M9_cff import Phase2C11M9
+
+process = cms.Process('gemTester',Phase2C11M9)
+
+process.load('FWCore.MessageService.MessageLogger_cfi')
+process.load('Configuration.StandardSequences.DigiToRaw_cff')
+process.load('Configuration.StandardSequences.RawToDigi_cff')
+process.load('EventFilter.GEMRawToDigi.GEMPackingTester_cfi')
+process.load('Configuration.EventContent.EventContent_cff')
+
+process.GEMPackingTester.gemDigi = cms.InputTag("muonGEMDigis",'','gemTester')
+process.muonGEMDigis.keepDAQStatus = True
+process.maxEvents = cms.untracked.PSet(input = cms.untracked.int32(1))
+
+process.source = cms.Source("PoolSource",                           
+  fileNames = cms.untracked.vstring('file:/store/relval/CMSSW_11_3_0_pre4/RelValZMM_14/GEN-SIM-RECO/PU_113X_mcRun4_realistic_v4_2026D76PU200-v1/00000/028001e8-5c24-48e7-8162-5da736ad7d38.root'),
+)
+
+process.FEVTDEBUGHLToutput = cms.OutputModule("PoolOutputModule",
+    dataset = cms.untracked.PSet(
+        dataTier = cms.untracked.string('GEN-SIM-RECO'),
+        filterName = cms.untracked.string('')
+    ),
+    fileName = cms.untracked.string('file:muonGEMDigis.root'),
+    outputCommands = cms.untracked.vstring( ('drop *', 'keep *_muonGEMDigis_*_*')),
+    splitLevel = cms.untracked.int32(0)
+)
+
+process.TFileService = cms.Service('TFileService', fileName = cms.string('gemTester.root') )
+process.rawDataCollector.RawCollectionList = cms.VInputTag(cms.InputTag("gemPacker",'','gemTester'))
+process.MessageLogger.cerr.threshold = "DEBUG"
+#process.MessageLogger.debugModules = ["gemPacker", "muonGEMDigis"]
+process.MessageLogger.debugModules = ["muonGEMDigis"]
+
+process.FEVTDEBUGHLToutput_step = cms.EndPath(process.FEVTDEBUGHLToutput)
+
+process.p = cms.Path(process.gemPacker+process.rawDataCollector+process.muonGEMDigis+process.GEMPackingTester)
+
