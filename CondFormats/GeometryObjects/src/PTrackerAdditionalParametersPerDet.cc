f4c881f33695310190d898b0bfe794b4c76753d5
Adinda de Wit <adinda.maite.de.wit@cern.ch>
2021-07-15 12:57:08 +0200

    PTrackerAdditionalParametersPerDet: add getter/setter functions to unnamed namespace

--- CondFormats/GeometryObjects/src/PTrackerAdditionalParametersPerDet.cc
+++ CondFormats/GeometryObjects/src/PTrackerAdditionalParametersPerDet.cc
@@ -1,28 +1,31 @@
 #include "CondFormats/GeometryObjects/interface/PTrackerAdditionalParametersPerDet.h"
 #include <iostream>
 
-template <typename T>
-const T getThisParam(const std::vector<std::vector<T>>& params, size_t index_outer, size_t index_inner) {
-  if (index_outer >= params.size())
-    throw std::out_of_range("Parameter with index " + std::to_string(index_outer) + " is out of range.");
-  if (index_inner >= params[index_outer].size())
-    throw std::out_of_range("Parameter with index " + std::to_string(index_inner) + " is out of range.");
-  return params[index_outer][index_inner];
-}
+namespace {
+  template <typename T>
+  const T getThisParam(const std::vector<std::vector<T>>& params, size_t index_outer, size_t index_inner) {
+    if (index_outer >= params.size())
+      throw std::out_of_range("Parameter with index " + std::to_string(index_outer) + " is out of range.");
+    if (index_inner >= params[index_outer].size())
+      throw std::out_of_range("Parameter with index " + std::to_string(index_inner) + " is out of range.");
+    return params[index_outer][index_inner];
+  }
 
-template <typename T>
-void setThisParam(std::vector<std::vector<T>>& params, size_t index_outer, const T& value) {
-  if (index_outer >= params.size())
-    throw std::out_of_range("Parameter with index " + std::to_string(index_outer) + " is out of range.");
-  params[index_outer].push_back(value);
-}
+  template <typename T>
+  void setThisParam(std::vector<std::vector<T>>& params, size_t index_outer, const T& value) {
+    if (index_outer >= params.size())
+      throw std::out_of_range("Parameter with index " + std::to_string(index_outer) + " is out of range.");
+    params[index_outer].push_back(value);
+  }
 
-template <typename T>
-const std::vector<T>& getAllParams(const std::vector<std::vector<T>>& params, size_t index_outer) {
-  if (index_outer >= params.size())
-    throw std::out_of_range("Parameter with index " + std::to_string(index_outer) + " is out of range.");
-  return params[index_outer];
-}
+  template <typename T>
+  const std::vector<T>& getAllParams(const std::vector<std::vector<T>>& params, size_t index_outer) {
+    if (index_outer >= params.size())
+      throw std::out_of_range("Parameter with index " + std::to_string(index_outer) + " is out of range.");
+    return params[index_outer];
+  }
+
+}  // namespace
 
 int PTrackerAdditionalParametersPerDet::getGeographicalId(int theIndex) const {
   return getThisParam(intParams_, GEOGRAPHICAL_ID, theIndex);
