116f2812331017e38c22f3763951a084a5bb3f59
Christopher <krzysztof.misan@cern.ch>
2021-07-16 12:46:42 +0200

    restructure configs, fix recHitProducerAlgorithm, add digi converter

---
+++ RecoPPS/Local/plugins/DiamondSampicDigiProducer.cc
@@ -0,0 +1,193 @@
+// -*- C++ -*-
+//
+// Package:    SampicDigi/DiamondSampicDigiProducer
+// Class:      DiamondSampicDigiProducer
+//
+/**\class DiamondSampicDigiProducer DiamondSampicDigiProducer.cc SampicDigi/DiamondSampicDigiProducer/plugins/DiamondSampicDigiProducer.cc
+
+ Description: This plugin takes sampic digi as input and converts them to TotemTimingDigi
+
+ Implementation:
+     [Notes on implementation]
+*/
+//
+// Original Author:  Krzysztof Misan
+//         Created:  Sun, 07 Mar 2021 14:42:52 GMT
+//
+//
+
+// system include files
+#include <memory>
+#include <string>
+
+// user include files
+#include "FWCore/Framework/interface/Frameworkfwd.h"
+#include "FWCore/Framework/interface/stream/EDProducer.h"
+
+#include "FWCore/Framework/interface/Event.h"
+#include "FWCore/Framework/interface/MakerMacros.h"
+
+#include "FWCore/ParameterSet/interface/ParameterSet.h"
+#include "FWCore/Utilities/interface/StreamID.h"
+
+#include "DataFormats/CTPPSDigi/interface/TotemTimingDigi.h"
+#include "DataFormats/CTPPSDetId/interface/TotemTimingDetId.h"
+#include "DataFormats/Common/interface/DetSetVector.h"
+#include "DataFormats/CTPPSDetId/interface/CTPPSDiamondDetId.h"
+
+#include "TFile.h"
+#include "TTree.h"
+#include "TChain.h"
+
+const int MAX_SAMPIC_CHANNELS=32;  
+const int SAMPIC_SAMPLES=64;
+
+//
+// class declaration
+//
+
+class DiamondSampicDigiProducer : public edm::stream::EDProducer<> {
+public:
+  explicit DiamondSampicDigiProducer(const edm::ParameterSet&);
+  ~DiamondSampicDigiProducer();
+
+  static void fillDescriptions(edm::ConfigurationDescriptions& descriptions);
+
+private:
+  void produce(edm::Event&, const edm::EventSetup&) override;
+
+  // ----------member data ---------------------------
+  std::vector<std::string> sampicFilesVec;
+  std::unordered_map<unsigned int, std::vector<unsigned int>> detid_vs_chid_;
+  TChain *inputTree;
+  std::stringstream ssLog;
+};
+
+//
+// constructors and destructor
+//
+using namespace edm;
+using namespace std;
+
+DiamondSampicDigiProducer::DiamondSampicDigiProducer(const edm::ParameterSet& iConfig)
+: sampicFilesVec(iConfig.getParameter<std::vector<std::string>>("sampicFilesVec"))
+{
+
+  for (const auto& id_map : iConfig.getParameter<std::vector<edm::ParameterSet>>("idsMapping"))
+    detid_vs_chid_[id_map.getParameter<unsigned int>("treeChId")] =id_map.getParameter<vector<unsigned int>>("detId");
+
+  inputTree = new TChain("desy");
+	for ( uint i = 0; i < sampicFilesVec.size() ; i++)
+		inputTree->Add(sampicFilesVec[i].c_str());
+
+
+  produces<DetSetVector<TotemTimingDigi>>("TotemTiming");
+}
+
+DiamondSampicDigiProducer::~DiamondSampicDigiProducer() {
+  delete inputTree;
+}
+
+//
+// member functions
+//
+
+// ------------ method called to produce the data  ------------
+void DiamondSampicDigiProducer::produce(edm::Event& iEvent, const edm::EventSetup& iSetup) {
+
+  int eventNum=iEvent.id().event();
+  auto digi = std::make_unique<edm::DetSetVector<TotemTimingDigi>>();
+
+  uint num_samples;
+  double trigger_time;
+  int sample_channel[MAX_SAMPIC_CHANNELS];
+  int sample_cellInfoForOrderedData[MAX_SAMPIC_CHANNELS];
+  uint sample_timestampA[MAX_SAMPIC_CHANNELS];
+  uint sample_timestampB[MAX_SAMPIC_CHANNELS];
+  ULong64_t sample_timestampFPGA[MAX_SAMPIC_CHANNELS];
+  double sample_amp[MAX_SAMPIC_CHANNELS][SAMPIC_SAMPLES];
+
+  inputTree->SetBranchAddress("num_samples", &num_samples);
+  inputTree->SetBranchAddress("trigger_time", &trigger_time);
+  inputTree->SetBranchAddress("sample_channel", sample_channel);
+  inputTree->SetBranchAddress("sample_timestampFPGA", sample_timestampFPGA);
+  inputTree->SetBranchAddress("sample_timestampA", sample_timestampA);
+  inputTree->SetBranchAddress("sample_timestampB", sample_timestampB);
+  inputTree->SetBranchAddress("sample_cellInfoForOrderedData", sample_cellInfoForOrderedData);
+  inputTree->SetBranchAddress("sample_ampl", sample_amp);
+
+  inputTree->GetEntry(eventNum);
+      
+  int bunchNumber = ((int) trigger_time/25 ) % 3564;
+  int orbitNumber = (int) (trigger_time / 88924.45);
+
+
+  
+  if(num_samples==0){
+    iEvent.put(std::move(digi),"TotemTiming");
+    return;
+  }
+  TotemTimingEventInfo eventInfoTmp(0,
+                                                  sample_timestampFPGA[0],//l1ATimestamp
+                                                  bunchNumber,
+                                                  orbitNumber,
+                                                  eventNum,
+                                                  1,//
+                                                  -1900,//l1ALatency
+                                                  64,//
+                                                  0,//
+                                                  1);
+
+  for(uint i=0;i<num_samples;i++){
+
+    unsigned short ch_id=sample_channel[i];
+
+    std::vector<uint8_t> samples;
+
+    for(int y=0;y<SAMPIC_SAMPLES;y++){
+      samples.push_back((int)(sample_amp[i][y]/1.2*256));
+      ssLog<<(int)(sample_amp[i][y]/1.2*256)<<endl;}
+        
+
+    if(ch_id<8){
+      continue;
+    }
+
+    TotemTimingDigi digiTmp(2*32+ch_id,
+                                        sample_timestampFPGA[i],
+                                        sample_timestampA[i],
+                                        sample_timestampB[i],
+                                        sample_cellInfoForOrderedData[i],
+                                        samples,
+                                        eventInfoTmp);
+        
+
+    auto vec=detid_vs_chid_.at(ch_id);
+    for(auto id:vec){
+      CTPPSDiamondDetId detId(id);
+      edm::DetSet<TotemTimingDigi>& digis_for_detid = digi->find_or_insert(detId);
+      digis_for_detid.push_back(digiTmp);
+    }
+
+  }
+  iEvent.put(std::move(digi),"TotemTiming");
+  
+}
+
+// ------------ method fills 'descriptions' with the allowed parameters for the module  ------------
+void DiamondSampicDigiProducer::fillDescriptions(edm::ConfigurationDescriptions& descriptions) {
+
+  edm::ParameterSetDescription desc;
+  desc.add<vector<std::string>>("sampicFilesVec")->setComment("path to sampic root data");
+
+  edm::ParameterSetDescription idmap_valid;
+  idmap_valid.add<unsigned int>("treeChId", 0)->setComment("HW id as retrieved from tree");
+  idmap_valid.add<vector<unsigned int>>("detId")->setComment("mapped CTPPSDiamondDetId's for this channel");
+
+  desc.addVPSet("idsMapping",idmap_valid);
+
+  descriptions.add("DiamondSampicDigiProducer",desc);
+}
+
+//define this as a plug-in
+DEFINE_FWK_MODULE(DiamondSampicDigiProducer);
