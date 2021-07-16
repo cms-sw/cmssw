2107fdff00b3163bc3493afcd7cbaac155f079ab
Sunanda <sunanda.banerjee@cern.ch>
2021-07-16 20:31:06 +0200

    Convert one more SD (Hcal) to new format

--- SimG4CMS/Calo/interface/HcalTestNS.h
+++ SimG4CMS/Calo/interface/HcalTestNS.h
@@ -1,13 +1,11 @@
 #ifndef SimG4CMS_HcalTestNS_h
 #define SimG4CMS_HcalTestNS_h
 
-#include "FWCore/Framework/interface/EventSetup.h"
-#include "Geometry/HcalCommonData/interface/HcalNumberingFromDDD.h"
 #include "Geometry/HcalCommonData/interface/HcalDDDRecConstants.h"
 
 class HcalTestNS {
 public:
-  HcalTestNS(const edm::EventSetup*);
+  HcalTestNS(const HcalDDDRecConstants*);
   virtual ~HcalTestNS();
 
   bool compare(HcalNumberingFromDDD::HcalID const&, uint32_t const&);
