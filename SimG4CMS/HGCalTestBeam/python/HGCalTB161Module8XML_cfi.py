0bd5fd098db0d96152cc871f98385b55deb79642
Sunanda <sunanda.banerjee@cern.ch>
2021-07-18 01:51:25 +0200

    Update SD class initializations in SimG4CMS/HGCalTestBeam

--- SimG4CMS/HGCalTestBeam/python/HGCalTB161Module8XML_cfi.py
+++ SimG4CMS/HGCalTestBeam/python/HGCalTB161Module8XML_cfi.py
@@ -10,8 +10,8 @@ XMLIdealGeometryESSource = cms.ESSource("XMLIdealGeometryESSource",
         'Geometry/HGCalCommonData/data/TB161/hgcalBeam.xml',
         'Geometry/HGCalCommonData/data/TB161/hgcalsense.xml',
         'Geometry/HGCalCommonData/data/TB161/hgcProdCuts.xml',
-        'Geometry/HGCalCommonData/data/TB161/8Module/hgcalCons.xml'
-                               ),
+        'Geometry/HGCalCommonData/data/TB161/8Module/hgcalCons.xml',
+        'SimG4CMS/HGCalTestBeam/data/CaloUtil.xml'),
     rootNodeName = cms.string('cms:OCMS')
 )
 
