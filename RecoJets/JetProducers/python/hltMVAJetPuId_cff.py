f5ca4f0b9b66cbd875d8eb61b11292886dcf2d19
Jeongeun Lee <jeongeun.lee@cern.ch>
2021-07-19 05:34:40 +0200

    replace EDProducer with clone cfipy, drop type spec, and duplicated param

--- RecoJets/JetProducers/python/hltMVAJetPuId_cff.py
+++ RecoJets/JetProducers/python/hltMVAJetPuId_cff.py
@@ -1,15 +1,11 @@
 from RecoJets.JetProducers.hltPUIdAlgo_cff import *
+import RecoJets.JetProducers.MVAJetPuIdProducer_cfi as _mod
 
-hltMVAJetPuIdCalculator = cms.EDProducer('MVAJetPuIdProducer',
-                                      produceJetIds = cms.bool(False),
-                                      jetids = cms.InputTag(""),
-                                      runMvas = cms.bool(True),
-                                      algos = cms.VPSet(cms.VPSet(full_74x)),
-				      jets     = cms.InputTag('hltAK4PFJetsCorrected'),
-				      rho      = cms.InputTag('hltFixedGridRhoFastjetAll'),
-				      vertexes = cms.InputTag('hltPixelVertices'),
-				      jec     = cms.string("AK4PFchs"),
-				      applyJec = cms.bool(False),
-				      inputIsCorrected = cms.bool(True),
-		)
-hltMVAJetPuIdEvaluator = hltMVAJetPuIdCalculator.clone( jetids = "pileupJetIdCalculator" )
+hltMVAJetPuIdCalculator = _mod.MVAJetPuIdProducer.clone(
+    produceJetIds = False,
+    algos   = cms.VPSet(cms.VPSet(full_74x)),
+    jec     = "AK4PFchs"
+)
+hltMVAJetPuIdEvaluator = hltMVAJetPuIdCalculator.clone(
+    jetids = "pileupJetIdCalculator" 
+)
