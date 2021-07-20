32eec56aae3bee58898939f81be3c643880f53f7
Sunanda <sunanda.banerjee@cern.ch>
2021-07-20 20:10:24 +0200

    Add a few cfi's to help processing with DDD or DD4Hep

--- Geometry/HcalCommonData/python/hcalDDFromDBConstants_cff.py
+++ Geometry/HcalCommonData/python/hcalDDFromDBConstants_cff.py
@@ -1,6 +1,5 @@
 import FWCore.ParameterSet.Config as cms
 
-from Geometry.HcalCommonData.hcalParameters_cff             import *
 from Geometry.HcalCommonData.hcalSimulationParameters_cff   import *
 from Geometry.HcalCommonData.hcalSimulationConstants_cfi    import *
 from Geometry.HcalCommonData.caloSimulationParameters_cff   import *
