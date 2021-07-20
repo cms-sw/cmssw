1b0a2013a46fbb94d61599a794ee856fc52ec7e1
Sunanda <sunanda.banerjee@cern.ch>
2021-07-20 21:30:40 +0200

    Correction

--- Geometry/HcalCommonData/python/hcalDDFromDBConstants_cff.py
+++ Geometry/HcalCommonData/python/hcalDDFromDBConstants_cff.py
@@ -3,4 +3,4 @@ import FWCore.ParameterSet.Config as cms
 from Geometry.HcalCommonData.hcalSimulationParameters_cff   import *
 from Geometry.HcalCommonData.hcalSimulationConstants_cfi    import *
 from Geometry.HcalCommonData.caloSimulationParameters_cff   import *
-from Geometry.HcalCommonData.hcalhcalDBConstants_cff        import *
+from Geometry.HcalCommonData.hcalDBConstants_cff            import *
