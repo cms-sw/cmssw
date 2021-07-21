a149813d2db4a2e553e32b3834fbe453d6c739be
Christopher Jones <chrisdjones15@gmail.com>
2021-07-21 16:46:05 -0500

    Remove EventSetup use from PdtEntry
    Users of the method must now pass the ParticleDataTable they
    obtained from the EventSetup. All such cases were migrated to use
    esConsumes.

--- CommonTools/RecoAlgos/src/MassiveCandidateConverter.cc
+++ CommonTools/RecoAlgos/src/MassiveCandidateConverter.cc
@@ -2,16 +2,21 @@
 #include "FWCore/ParameterSet/interface/ParameterSet.h"
 #include "HepPDT/ParticleData.hh"
 #include "FWCore/Framework/interface/ESHandle.h"
+#include "FWCore/Framework/interface/ConsumesCollector.h"
+#include "FWCore/Framework/interface/EventSetup.h"
 #include <algorithm>
 using namespace edm;
 using namespace std;
 using namespace converter;
 
-MassiveCandidateConverter::MassiveCandidateConverter(const edm::ParameterSet& cfg)
-    : massSqr_(0), particle_(cfg.getParameter<PdtEntry>("particleType")) {}
+MassiveCandidateConverter::MassiveCandidateConverter(const edm::ParameterSet& cfg, edm::ConsumesCollector iC)
+    : massSqr_(0),
+      particle_(cfg.getParameter<PdtEntry>("particleType")),
+      tableToken_(iC.esConsumes<edm::Transition::BeginRun>()) {}
 
 void MassiveCandidateConverter::beginFirstRun(const EventSetup& es) {
-  particle_.setup(es);
+  auto const& pdt = es.getData(tableToken_);
+  particle_.setup(pdt);
   massSqr_ = particle_.data().mass();
   massSqr_ *= massSqr_;
 }
