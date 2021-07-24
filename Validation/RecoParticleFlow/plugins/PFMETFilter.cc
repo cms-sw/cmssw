8befbcf1924cc33a1485dc0cdffb990173d0de22
Jonas Rembser <jonas.rembser@cern.ch>
2021-07-24 11:40:37 +0200

    Merge .h with .cc files for Validation/RecoParticleFlow plugins

--- Validation/RecoParticleFlow/plugins/PFMETFilter.cc
+++ Validation/RecoParticleFlow/plugins/PFMETFilter.cc
@@ -1,13 +1,47 @@
-#include "Validation/RecoParticleFlow/plugins/PFMETFilter.h"
+// author: Florent Lacroix (UIC)
+// date: 07/14/2009
 
 #include "DataFormats/Candidate/interface/Candidate.h"
 #include "DataFormats/Candidate/interface/CandidateFwd.h"
 #include "DataFormats/Common/interface/Handle.h"
 #include "DataFormats/METReco/interface/MET.h"
+#include "FWCore/Framework/interface/EDFilter.h"
 #include "FWCore/Framework/interface/ESHandle.h"
 #include "FWCore/Framework/interface/Event.h"
+#include "FWCore/Framework/interface/Frameworkfwd.h"
 #include "FWCore/ParameterSet/interface/ParameterSet.h"
 #include "FWCore/Utilities/interface/InputTag.h"
+
+class PFMETFilter : public edm::EDFilter {
+public:
+  explicit PFMETFilter(const edm::ParameterSet &);
+  ~PFMETFilter() override;
+
+  bool filter(edm::Event &, const edm::EventSetup &) override;
+  void beginJob() override;
+  void endJob() override;
+  bool checkInput();
+
+private:
+  std::vector<std::string> collections_;
+  std::vector<std::string> variables_;
+  std::vector<double> min_;
+  std::vector<double> max_;
+  std::vector<int> doMin_;
+  std::vector<int> doMax_;
+  // parameters for the cut:
+  // sqrt(DeltaMEX**2+DeltaMEY**2)>DeltaMEXsigma*sigma
+  // with sigma=sigma_a+sigma_b*sqrt(SET)+sigma_c*SET
+  std::string TrueMET_;
+  double DeltaMEXsigma_;
+  double sigma_a_;
+  double sigma_b_;
+  double sigma_c_;
+  bool verbose_;
+};
+
+#include "FWCore/Framework/interface/MakerMacros.h"
+DEFINE_FWK_MODULE(PFMETFilter);
 
 PFMETFilter::PFMETFilter(const edm::ParameterSet &iConfig) {
   collections_ = iConfig.getParameter<std::vector<std::string>>("Collections");
