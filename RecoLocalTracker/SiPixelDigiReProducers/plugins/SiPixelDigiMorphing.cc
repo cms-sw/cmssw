76e2d97329048c72ef140b5bf8def7069f1ad327
Viktor Veszpremi <veszpv@cern.ch>
2021-07-27 00:04:20 +0200

    Moved class definition, changed references to new package name, readjusted python configuration as requested

--- RecoLocalTracker/SiPixelDigiReProducers/plugins/SiPixelDigiMorphing.cc
+++ RecoLocalTracker/SiPixelDigiReProducers/plugins/SiPixelDigiMorphing.cc
@@ -1,9 +1,52 @@
-#include "FWCore/PluginManager/interface/ModuleDef.h"
+#ifndef RecoLocalTracker_SiPixelDigiMorphing_SiPixelDigiMorphing_h
+#define RecoLocalTracker_SiPixelDigiMorphing_SiPixelDigiMorphing_h
+
 #include "FWCore/Framework/interface/MakerMacros.h"
+#include "FWCore/Framework/interface/Frameworkfwd.h"
+#include "FWCore/Framework/interface/stream/EDProducer.h"
 #include "FWCore/MessageLogger/interface/MessageLogger.h"
+#include "FWCore/ParameterSet/interface/ParameterSet.h"
+#include "FWCore/ParameterSet/interface/ConfigurationDescriptions.h"
+#include "FWCore/PluginManager/interface/ModuleDef.h"
+#include "DataFormats/Common/interface/DetSetVector.h"
+#include "DataFormats/Common/interface/DetSetVectorNew.h"
 #include "DataFormats/DetId/interface/DetId.h"
-#include "SiPixelDigiMorphing.h"
+#include "DataFormats/SiPixelDigi/interface/PixelDigi.h"
+
 #include <bitset>
+
+
+class SiPixelDigiMorphing : public edm::stream::EDProducer<> {
+public:
+  explicit SiPixelDigiMorphing(const edm::ParameterSet& conf);
+  ~SiPixelDigiMorphing() override;
+
+  static void fillDescriptions(edm::ConfigurationDescriptions& descriptions);
+  void produce(edm::Event& e, const edm::EventSetup& c) override;
+
+private:
+  edm::EDGetTokenT<edm::DetSetVector<PixelDigi>> tPixelDigi;
+  edm::EDPutTokenT<edm::DetSetVector<PixelDigi>> tPutPixelDigi;
+
+  int32_t nrows_;
+  int32_t ncols_;
+  int32_t nrocs_;  // in Phase 1, this is ROCs, but could be any subset of a pixel row
+  int32_t ncols_r_;
+
+  int32_t iters_;
+  int32_t ksize_;
+
+  std::vector<uint64_t> kernel1_;
+  std::vector<uint64_t> kernel2_;
+  uint64_t mask_;
+
+  enum MorphOption { Dilate, Erode };
+
+  uint32_t fakeAdc_;
+
+  void morph(uint64_t* imap, uint64_t* omap, uint64_t* kernel, MorphOption op);
+};
+
 
 SiPixelDigiMorphing::SiPixelDigiMorphing(edm::ParameterSet const& conf)
     : tPutPixelDigi(produces<edm::DetSetVector<PixelDigi>>()),
@@ -66,7 +109,7 @@ void SiPixelDigiMorphing::fillDescriptions(edm::ConfigurationDescriptions& descr
   desc.add<std::vector<int32_t>>("kernel2", {2, 7, 2});
   desc.add<uint32_t>("fakeAdc", 100);
 
-  descriptions.add("SiPixelDigiMorphing", desc);
+  descriptions.addWithDefaultLabel(desc);
 }
 
 void SiPixelDigiMorphing::produce(edm::Event& e, const edm::EventSetup& es) {
@@ -170,3 +213,5 @@ void SiPixelDigiMorphing::morph(uint64_t* imap, uint64_t* omap, uint64_t* kernel
 }
 
 DEFINE_FWK_MODULE(SiPixelDigiMorphing);
+
+#endif
--- RecoLocalTracker/SiPixelDigiReProducers/python/SiPixelDigiMorphing_cfi.py
+++ RecoLocalTracker/SiPixelDigiReProducers/python/SiPixelDigiMorphing_cfi.py
@@ -1,15 +1,5 @@
 import FWCore.ParameterSet.Config as cms
 
-siPixelDigisMorphed = cms.EDProducer(
-    "SiPixelDigiMorphing",
-    src = cms.InputTag('siPixelDigis'),
-    nrows = cms.int32(160),
-    ncols = cms.int32(416),
-    nrocs = cms.int32(8),
-    iters = cms.int32(1),
-    kernel1 = cms.vint32(7, 7, 7),
-    kernel2 = cms.vint32(2, 7, 2),
-    fakeAdc = cms.uint32(100) # note, unit is 10e-
-)
+from RecoLocalTracker.SiPixelDigiReProducers.siPixelDigiMorphing_cfi import siPixelDigiMorphing as _siPixelDigiMorphing
 
-
+siPixelDigisMorphed = _siPixelDigiMorphing.clone()
