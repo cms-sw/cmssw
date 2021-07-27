1a32b1c121f44d624b9d42015a6d648db878a0d1
Sunanda <sunanda.banerjee@cern.ch>
2021-07-27 18:19:02 +0200

    Fix the special scenarios for testing particle flux in forward regions

--- Geometry/ForwardCommonData/python/totemTest2015_cff.py
+++ Geometry/ForwardCommonData/python/totemTest2015_cff.py
@@ -4,4 +4,6 @@ import FWCore.ParameterSet.Config as cms
 from Geometry.ForwardCommonData.totemTest2015XML_cfi import *
 from Geometry.TrackerNumberingBuilder.trackerNumberingGeometry_cfi import *
 from Geometry.EcalCommonData.ecalSimulationParameters_cff import *
-from Geometry.HcalCommonData.hcalDDDSimConstants_cff import *
+from Geometry.HcalCommonData.hcalDDConstants_cff import *
+from Geometry.MuonNumbering.muonGeometryConstants_cff import *
+from Geometry.MuonNumbering.muonOffsetESProducer_cff import *
