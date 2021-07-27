e7da8e21f4f12aa1d0f6068c693ac7e85ab2e752
Benedikt Maier <benedikt.maier@cern.ch>
2021-07-27 07:56:58 -0400

    ensure no GEN-to-SIM vertex smearing for CloseByParticleGun workflows

--- IOMC/EventVertexGenerators/python/VtxSmearedHGCALCloseBy_cfi.py
+++ IOMC/EventVertexGenerators/python/VtxSmearedHGCALCloseBy_cfi.py
@@ -1,7 +1,7 @@
 import FWCore.ParameterSet.Config as cms
 
 from IOMC.EventVertexGenerators.VtxSmearedParameters_cfi import *
-VtxSmeared = cms.EDProducer("HLLHCEvtVtxGenerator",
+VtxSmeared = cms.EDProducer("GaussEvtVtxGenerator",
     GaussVtxSmearingParameters,
     VtxSmearedCommon
 )
