f055e954bc1ba201298432988127d6c4e679dd9e
Abhirami Harilal <abhi.harilal@gmail.com>
2021-07-27 17:37:18 +0200

    Drop type specs from Ecal DQM python config files

--- DQM/EcalCommon/python/CommonParams_cfi.py
+++ DQM/EcalCommon/python/CommonParams_cfi.py
@@ -4,9 +4,13 @@ ecalCommonParams = cms.untracked.PSet(
     onlineMode = cms.untracked.bool(False),
     willConvertToEDM = cms.untracked.bool(True)
 )
+<<<<<<< HEAD
+=======
 
-ecaldqmLaserWavelengths = cms.untracked.vint32(1, 2, 3)
-ecaldqmLedWavelengths = cms.untracked.vint32(1, 2)
-ecaldqmMGPAGains = cms.untracked.vint32(12)
-ecaldqmMGPAGainsPN = cms.untracked.vint32(16)
+#ecaldqmLaserWavelengths = cms.untracked.vint32(1, 2, 3)
+>>>>>>> af90af1e2e742d36094250a06c7d8522d5d7a9c8
+ecaldqmLaserWavelengths = 1, 2, 3
+ecaldqmLedWavelengths = 1, 2
+ecaldqmMGPAGains = 12
+ecaldqmMGPAGainsPN = 16
 
