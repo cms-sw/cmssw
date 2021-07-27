b187411430658f73423e880713c5c75071c390c9
Josh Bendavid <Josh.Bendavid@cern.ch>
2021-07-27 15:27:55 +0200

    code formatting

--- RecoTracker/TrackProducer/plugins/TrackExtraRekeyer.cc
+++ RecoTracker/TrackProducer/plugins/TrackExtraRekeyer.cc
@@ -5,47 +5,43 @@
 #include "DataFormats/TrackReco/interface/TrackFwd.h"
 #include "DataFormats/TrackReco/interface/TrackExtraFwd.h"
 
-class TrackExtraRekeyer : public edm::stream::EDProducer<>
-{
+class TrackExtraRekeyer : public edm::stream::EDProducer<> {
 public:
   explicit TrackExtraRekeyer(const edm::ParameterSet &);
   ~TrackExtraRekeyer() {}
 
-//   static void fillDescriptions(edm::ConfigurationDescriptions &descriptions);
+  //   static void fillDescriptions(edm::ConfigurationDescriptions &descriptions);
 
 private:
-  
   virtual void produce(edm::Event &, const edm::EventSetup &) override;
-  
+
   edm::EDGetTokenT<reco::TrackCollection> inputTrack_;
   edm::EDGetTokenT<edm::Association<reco::TrackExtraCollection>> inputAssoc_;
   edm::EDPutTokenT<reco::TrackCollection> outputTrack_;
-
 };
-
 
 TrackExtraRekeyer::TrackExtraRekeyer(const edm::ParameterSet &iConfig)
 
 {
   inputTrack_ = consumes<reco::TrackCollection>(iConfig.getParameter<edm::InputTag>("src"));
-  inputAssoc_ = consumes<edm::Association<reco::TrackExtraCollection>>(iConfig.getParameter<edm::InputTag>("association"));
-  
+  inputAssoc_ =
+      consumes<edm::Association<reco::TrackExtraCollection>>(iConfig.getParameter<edm::InputTag>("association"));
+
   outputTrack_ = produces<reco::TrackCollection>();
 }
 
 // ------------ method called for each event  ------------
-void TrackExtraRekeyer::produce(edm::Event &iEvent, const edm::EventSetup &iSetup)
-{
+void TrackExtraRekeyer::produce(edm::Event &iEvent, const edm::EventSetup &iSetup) {
   using namespace edm;
-  
+
   Handle<reco::TrackCollection> tracks;
   iEvent.getByToken(inputTrack_, tracks);
-  
+
   Handle<edm::Association<reco::TrackExtraCollection>> assoc;
   iEvent.getByToken(inputAssoc_, assoc);
 
   reco::TrackCollection tracksOut;
-  
+
   for (auto const &track : *tracks) {
     if (!assoc->contains(track.extra().id())) {
       continue;
@@ -56,9 +52,8 @@ void TrackExtraRekeyer::produce(edm::Event &iEvent, const edm::EventSetup &iSetu
       trackout.setExtra(trackextraref);
     }
   }
-  
+
   iEvent.emplace(outputTrack_, std::move(tracksOut));
-  
 }
 
 DEFINE_FWK_MODULE(TrackExtraRekeyer);
--- RecoTracker/TrackProducer/plugins/TrackProducerFromPatMuons.cc
+++ RecoTracker/TrackProducer/plugins/TrackProducerFromPatMuons.cc
@@ -6,24 +6,20 @@
 #include "DataFormats/TrackReco/interface/TrackExtraFwd.h"
 #include "DataFormats/PatCandidates/interface/Muon.h"
 
-class TrackProducerFromPatMuons : public edm::stream::EDProducer<>
-{
+class TrackProducerFromPatMuons : public edm::stream::EDProducer<> {
 public:
   explicit TrackProducerFromPatMuons(const edm::ParameterSet &);
   ~TrackProducerFromPatMuons() {}
 
-//   static void fillDescriptions(edm::ConfigurationDescriptions &descriptions);
+  //   static void fillDescriptions(edm::ConfigurationDescriptions &descriptions);
 
 private:
-  
   virtual void produce(edm::Event &, const edm::EventSetup &) override;
-  
+
   edm::EDGetTokenT<std::vector<pat::Muon>> inputMuons_;
   edm::EDPutTokenT<reco::TrackCollection> outputTrack_;
   bool innerTrackOnly_;
-
 };
-
 
 TrackProducerFromPatMuons::TrackProducerFromPatMuons(const edm::ParameterSet &iConfig)
 
@@ -34,24 +30,22 @@ TrackProducerFromPatMuons::TrackProducerFromPatMuons(const edm::ParameterSet &iC
 }
 
 // ------------ method called for each event  ------------
-void TrackProducerFromPatMuons::produce(edm::Event &iEvent, const edm::EventSetup &iSetup)
-{
+void TrackProducerFromPatMuons::produce(edm::Event &iEvent, const edm::EventSetup &iSetup) {
   using namespace edm;
-  
+
   Handle<std::vector<pat::Muon>> muons;
   iEvent.getByToken(inputMuons_, muons);
 
   reco::TrackCollection tracksOut;
-  
+
   for (auto const &muon : *muons) {
     const reco::TrackRef trackRef = innerTrackOnly_ ? muon.innerTrack() : muon.muonBestTrack();
     if (trackRef.isNonnull() && trackRef->extra().isAvailable()) {
       tracksOut.emplace_back(*trackRef);
     }
   }
-  
+
   iEvent.emplace(outputTrack_, std::move(tracksOut));
-  
 }
 
 DEFINE_FWK_MODULE(TrackProducerFromPatMuons);
