d5bd28cc5e69fb4bb9a9cdbad637ce5c43aca232
Abhirami Harilal <abhi.harilal@gmail.com>
2021-07-27 17:40:15 +0200

    Clean up

--- DQM/EcalCommon/python/CommonParams_cfi.py
+++ DQM/EcalCommon/python/CommonParams_cfi.py
@@ -4,11 +4,6 @@ ecalCommonParams = cms.untracked.PSet(
     onlineMode = cms.untracked.bool(False),
     willConvertToEDM = cms.untracked.bool(True)
 )
-<<<<<<< HEAD
-=======
-
-#ecaldqmLaserWavelengths = cms.untracked.vint32(1, 2, 3)
->>>>>>> af90af1e2e742d36094250a06c7d8522d5d7a9c8
 ecaldqmLaserWavelengths = 1, 2, 3
 ecaldqmLedWavelengths = 1, 2
 ecaldqmMGPAGains = 12
