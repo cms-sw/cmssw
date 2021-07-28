7be998cafd127d362e174170d6d2de8fac91a198
Sunanda <sunanda.banerjee@cern.ch>
2021-07-28 17:11:03 +0200

    Update some of the cfg's in Validation/HcalHits in view of changes to SD classes

--- Validation/HcalHits/test/run_HF_cfg.py
+++ Validation/HcalHits/test/run_HF_cfg.py
@@ -4,10 +4,13 @@ process = cms.Process("CaloTest")
 process.load("SimGeneral.HepPDTESSource.pythiapdt_cfi")
 
 process.load("IOMC.EventVertexGenerators.VtxSmearedGauss_cfi")
+process.load('Configuration.StandardSequences.Generator_cff')
 
 process.load("Configuration.StandardSequences.MagneticField_cff")
 
 process.load("Geometry.CMSCommonData.ecalhcalGeometryXML_cfi")
+process.load("Geometry.EcalCommonData.ecalSimulationParameters_cff")
+process.load("Geometry.HcalCommonData.hcalDDConstants_cff")
 
 process.load("SimG4Core.Application.g4SimHits_cfi")
 
@@ -40,24 +43,22 @@ process.source = cms.Source("PoolSource",
 
 process.Timing = cms.Service("Timing")
 
-process.RandomNumberGeneratorService = cms.Service("RandomNumberGeneratorService",
-    moduleSeeds = cms.PSet(
-        g4SimHits = cms.untracked.uint32(9876),
-        VtxSmeared = cms.untracked.uint32(123456789)
-    ),
-    sourceSeed = cms.untracked.uint32(135799753)
-)
+process.load("IOMC.RandomEngine.IOMC_cff")
+process.RandomNumberGeneratorService.g4SimHits.initialSeed = 9876
+process.RandomNumberGeneratorService.VtxSmeared.initialSeed = 123456789
+process.rndmStore = cms.EDProducer("RandomEngineStateProducer")
 
 process.USER = cms.OutputModule("PoolOutputModule",
     fileName = cms.untracked.string('simevent_HF.root')
 )
 
-process.p1 = cms.Path(process.VtxSmeared*process.g4SimHits*process.hcalHitValid)
+process.p1 = cms.Path(process.VtxSmeared*process.generatorSmeared*process.g4SimHits*process.hcalHitValid)
 process.outpath = cms.EndPath(process.USER)
 process.VtxSmeared.SigmaX = 0.00001
 process.VtxSmeared.SigmaY = 0.00001
 process.VtxSmeared.SigmaZ = 0.00001
 process.g4SimHits.UseMagneticField = False
+process.g4SimHits.OnlySDs = ['EcalSensitiveDetector', 'CaloTrkProcessing', 'HcalSensitiveDetector']
 process.g4SimHits.Watchers = cms.VPSet(cms.PSet(
     SimG4HcalValidation = cms.PSet(
         TimeLowLimit = cms.double(0.0),
@@ -81,7 +82,7 @@ process.g4SimHits.Watchers = cms.VPSet(cms.PSet(
     ),
     type = cms.string('SimG4HcalValidation')
 ))
-process.DQM.collectorHost = ''
+#process.DQM.collectorHost = ''
 process.hcalHitValid.outputFile = 'valid_HF.root'
 
 
