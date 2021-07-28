f964ad0456ee536ea00211e77ffdeed6eaaab74f
Shahzad Malik Muzaffar <Shahzad.Malik.Muzaffar@cern.ch>
2021-07-28 11:35:17 +0200

    code-format

--- Fireworks/Tracks/plugins/FWTracksRecHitsProxyBuilder.cc
+++ Fireworks/Tracks/plugins/FWTracksRecHitsProxyBuilder.cc
@@ -27,6 +27,7 @@ public:
 private:
   using FWSimpleProxyBuilderTemplate<reco::Track>::build;
   void build(const reco::Track& iData, unsigned int iIndex, TEveElement& oItemHolder, const FWViewContext*) override;
+
 public:
   FWTracksRecHitsProxyBuilder(const FWTracksRecHitsProxyBuilder&) = delete;                   // stop default
   const FWTracksRecHitsProxyBuilder& operator=(const FWTracksRecHitsProxyBuilder&) = delete;  // stop default
