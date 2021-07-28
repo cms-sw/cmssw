3b6d32afc2c3b8d0fadd9bd5f7e29357c3108f05
Kevin <kyoon@mit.edu>
2021-07-28 04:27:20 +0200

    removed unnecessary comments

---
+++ RecoHGCal/TICL/python/SimTracksters_cff.py
@@ -0,0 +1,26 @@
+import FWCore.ParameterSet.Config as cms
+
+from RecoHGCal.TICL.trackstersFromSimClustersProducer_cfi import trackstersFromSimClustersProducer as _trackstersFromSimClustersProducer
+from RecoHGCal.TICL.filteredLayerClustersProducer_cfi import filteredLayerClustersProducer as _filteredLayerClustersProducer
+
+
+# CA - PATTERN RECOGNITION
+
+
+filteredLayerClustersSimTracksters = _filteredLayerClustersProducer.clone(
+    clusterFilter = "ClusterFilterByAlgoAndSize",
+    algo_number = 8,
+    min_cluster_size = 0, # inclusive
+    iteration_label = "ticlSimTracksters"
+)
+
+ticlSimTracksters = _trackstersFromSimClustersProducer.clone(
+)
+
+from Configuration.ProcessModifiers.premix_stage2_cff import premix_stage2
+premix_stage2.toModify(ticlSimTracksters,
+    simclusters = "mixData:MergedCaloTruth",
+    caloparticles = "mixData:MergedCaloTruth",
+)
+
+ticlSimTrackstersTask = cms.Task(filteredLayerClustersSimTracksters, ticlSimTracksters)
