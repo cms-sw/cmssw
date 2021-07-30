31e63c657cc1817890190afab2198ab7bc7966b9
lgray <lindsey.gray@gmail.com>
2021-07-30 11:20:27 -0500

    add missing vars

--- PhysicsTools/NanoAOD/python/globals_cff.py
+++ PhysicsTools/NanoAOD/python/globals_cff.py
@@ -18,6 +18,7 @@ beamSpotTable = cms.EDProducer("SimpleBeamspotFlatTableProducer",
 
 rhoTable = cms.EDProducer("GlobalVariablesTableProducer",
     variables = cms.PSet(
+        fixedGridRhoAll = ExtVar( cms.InputTag("fixedGridRhoAll"), "double", doc = "rho from all PF Candidates, no foreground removal (for isolation of prompt photons)" ),
         fixedGridRhoFastjetAll = ExtVar( cms.InputTag("fixedGridRhoFastjetAll"), "double", doc = "rho from all PF Candidates, used e.g. for JECs" ),
         fixedGridRhoFastjetCentralNeutral = ExtVar( cms.InputTag("fixedGridRhoFastjetCentralNeutral"), "double", doc = "rho from neutral PF Candidates with |eta| < 2.5, used e.g. for rho corrections of some lepton isolations" ),
         fixedGridRhoFastjetCentralCalo = ExtVar( cms.InputTag("fixedGridRhoFastjetCentralCalo"), "double", doc = "rho from calo towers with |eta| < 2.5, used e.g. egamma PFCluster isolation" ),
