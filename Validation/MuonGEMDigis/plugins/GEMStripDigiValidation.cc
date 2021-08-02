6d60106b3b1c2225518afd7f5fb0276490389403
yeckang <yechan.kang@cern.ch>
2021-08-02 09:30:55 +0200

    reduce the size of gem validation plots (remove duplicated plots with dqm plots)

--- Validation/MuonGEMDigis/plugins/GEMStripDigiValidation.cc
+++ Validation/MuonGEMDigis/plugins/GEMStripDigiValidation.cc
@@ -30,45 +30,48 @@ void GEMStripDigiValidation::bookHistograms(DQMStore::IBooker& booker,
   // NOTE Bunch Crossing
   booker.setCurrentFolder("GEM/Digis");
 
-  me_bx_ = booker.book1D("strip_bx", "Strip Digi Bunch Crossing", 5, -2.5, 2.5);
+  if (detail_plot_) {
+    me_detail_bx_ = booker.book1D("strip_bx", "Strip Digi Bunch Crossing", 5, -2.5, 2.5);
 
-  for (const auto& region : gem->regions()) {
-    if (region == nullptr) {
-      edm::LogError(kLogCategory_) << "Null region";
-      continue;
-    }
-    Int_t region_id = region->region();
-    for (const auto& station : region->stations()) {
-      if (station == nullptr) {
-        edm::LogError(kLogCategory_) << "Null station for region = " << region_id;
+    for (const auto& region : gem->regions()) {
+      if (region == nullptr) {
+        edm::LogError(kLogCategory_) << "Null region";
         continue;
       }
-      Int_t station_id = station->station();
+      Int_t region_id = region->region();
+      for (const auto& station : region->stations()) {
+        if (station == nullptr) {
+          edm::LogError(kLogCategory_) << "Null station for region = " << region_id;
+          continue;
+        }
+        Int_t station_id = station->station();
 
-      const auto& superChamberVec = station->superChambers();
-      if (superChamberVec.empty()) {
-        edm::LogError(kLogCategory_) << "Super chambers missing for region = " << region_id
-                                     << " and station = " << station_id;
-        continue;
-      }
-      const GEMSuperChamber* super_chamber = superChamberVec.front();
-      if (super_chamber == nullptr) {
-        edm::LogError(kLogCategory_) << "Failed to find super chamber for region = " << region_id
-                                     << " and station = " << station_id;
-        continue;
-      }
-      for (const auto& chamber : super_chamber->chambers()) {
-        Int_t layer_id = chamber->id().layer();
-        ME3IdsKey key3(region_id, station_id, layer_id);
+        const auto& superChamberVec = station->superChambers();
+        if (superChamberVec.empty()) {
+          edm::LogError(kLogCategory_) << "Super chambers missing for region = " << region_id
+                                       << " and station = " << station_id;
+          continue;
+        }
+        const GEMSuperChamber* super_chamber = superChamberVec.front();
+        if (super_chamber == nullptr) {
+          edm::LogError(kLogCategory_) << "Failed to find super chamber for region = " << region_id
+                                       << " and station = " << station_id;
+          continue;
+        }
+        for (const auto& chamber : super_chamber->chambers()) {
+          Int_t layer_id = chamber->id().layer();
+          ME3IdsKey key3(region_id, station_id, layer_id);
 
-        me_bx_layer_[key3] =
-            bookHist1D(booker, key3, "strip_bx", "Strip Digi Bunch Crossing", 5, -2.5, 2.5, "Bunch crossing");
-      }  // chamber loop
-    }    // station loop
-  }      // region loop
+          me_detail_bx_layer_[key3] =
+              bookHist1D(booker, key3, "strip_bx", "Strip Digi Bunch Crossing", 5, -2.5, 2.5, "Bunch crossing");
+        }  // chamber loop
+      }    // station loop
+    }      // region loop
+  }
 
   // NOTE Occupancy
-  me_total_strip_ = booker.book1D("total_strips_per_event", "Number of strip digi per event", 50, -0.5, 395.5);
+  if (detail_plot_) 
+    me_detail_total_strip_all_ = booker.book1D("total_strips_per_event", "Number of strip digi per event", 50, -0.5, 395.5);
 
   for (const auto& region : gem->regions()) {
     Int_t region_id = region->region();
@@ -79,6 +82,8 @@ void GEMStripDigiValidation::bookHistograms(DQMStore::IBooker& booker,
     for (const auto& station : region->stations()) {
       Int_t station_id = station->station();
       ME2IdsKey key2{region_id, station_id};
+
+      me_occ_pid_[key2] = bookPIDHist(booker, key2, "strip_occ_pid", "Number of entreis for each particle");
 
       if (detail_plot_) {
         me_detail_total_strip_[key2] =
@@ -95,6 +100,11 @@ void GEMStripDigiValidation::bookHistograms(DQMStore::IBooker& booker,
         edm::LogError(kLogCategory_) << "Super chambers missing or null for region = " << region_id
                                      << " and station = " << station_id;
       } else {
+        for (const auto& etaPart : superChamberVec[0]->chambers()[0]->etaPartitions()) {
+          Int_t ieta = etaPart->id().ieta();
+          ME3IdsKey key{region_id, station_id, ieta};
+          me_occ_pid_eta_[key] = bookPIDHist(booker, key2, ieta, "strip_occ_pid", "Number of entries for each particles");
+        }
         for (const auto& chamber : superChamberVec[0]->chambers()) {
           if (chamber == nullptr) {
             edm::LogError(kLogCategory_) << "Null chamber for region, station, super chamber = (" << region_id << ", "
@@ -111,8 +121,6 @@ void GEMStripDigiValidation::bookHistograms(DQMStore::IBooker& booker,
                 << station_id << ", " << superChamberVec[0]->id() << ", " << chamber->id() << ")";
             continue;
           }
-
-          me_occ_pid_[key3] = bookPIDHist(booker, key3, "strip_occ_pid", "Number of entreis for each particle");
 
           if (detail_plot_) {
             Int_t num_strips = etaPartitionsVec.front()->nstrips();
@@ -221,10 +229,10 @@ void GEMStripDigiValidation::analyze(const edm::Event& event, const edm::EventSe
       Float_t digi_g_r = strip_global_pos.perp();
       Float_t digi_g_abs_z = std::abs(strip_global_pos.z());
 
-      me_bx_->Fill(bx);
-      me_bx_layer_[key3]->Fill(bx);
-
       if (detail_plot_) {
+        me_detail_bx_->Fill(bx);
+        me_detail_bx_layer_[key3]->Fill(bx);
+
         me_detail_occ_zr_[region_id]->Fill(digi_g_abs_z, digi_g_r);
         me_detail_occ_det_[key2]->Fill(bin_x, ieta);
         me_detail_occ_xy_[key3]->Fill(digi_g_x, digi_g_y);
@@ -232,8 +240,8 @@ void GEMStripDigiValidation::analyze(const edm::Event& event, const edm::EventSe
       }
     }
   }  // range loop
-  me_total_strip_->Fill(total_strip);
   if (detail_plot_) {
+    me_detail_total_strip_all_->Fill(total_strip);
     for (const auto& region : gem->regions()) {
       Int_t region_id = region->region();
       for (const auto& station : region->stations()) {
@@ -260,6 +268,7 @@ void GEMStripDigiValidation::analyze(const edm::Event& event, const edm::EventSe
     Int_t ieta = simhit_gemid.ieta();
     Int_t num_layers = simhit_gemid.nlayers();
 
+    ME3IdsKey key{region_id, station_id, ieta};
     ME2IdsKey key2{region_id, station_id};
     ME3IdsKey key3{region_id, station_id, layer_id};
 
@@ -284,7 +293,8 @@ void GEMStripDigiValidation::analyze(const edm::Event& event, const edm::EventSe
         Int_t pid = simhit.particleType();
         Int_t pid_idx = getPidIdx(pid);
 
-        me_occ_pid_[key3]->Fill(pid_idx);
+        me_occ_pid_[key2]->Fill(pid_idx);
+        me_occ_pid_eta_[key3]->Fill(pid_idx);
 
         if (detail_plot_) {
           if (isMuonSimHit(simhit)) {
