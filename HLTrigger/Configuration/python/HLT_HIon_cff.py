4fd04d924abd91e6d42ddbc3410012730c6ad73e
Martin Grunewald <Martin.Grunewald@cern.ch>
2021-08-03 14:13:22 +0200

    HLT menu migration to CMSSW 12_0_0_pre5

--- HLTrigger/Configuration/python/HLT_HIon_cff.py
+++ HLTrigger/Configuration/python/HLT_HIon_cff.py
@@ -1,13 +1,13 @@
 # hltGetConfiguration --cff --data /dev/CMSSW_12_0_0/HIon --type HIon
 
-# /dev/CMSSW_12_0_0/HIon/V3 (CMSSW_12_0_0_pre4)
+# /dev/CMSSW_12_0_0/HIon/V5 (CMSSW_12_0_0_pre5)
 
 import FWCore.ParameterSet.Config as cms
 
 fragment = cms.ProcessFragment( "HLT" )
 
 fragment.HLTConfigVersion = cms.PSet(
-  tableName = cms.string('/dev/CMSSW_12_0_0/HIon/V3')
+  tableName = cms.string('/dev/CMSSW_12_0_0/HIon/V5')
 )
 
 fragment.transferSystem = cms.PSet( 
@@ -4378,6 +4378,9 @@ fragment.SteppingHelixPropagatorAny = cms.ESProducer( "SteppingHelixPropagatorES
   ApplyRadX0Correction = cms.bool( True ),
   useMagVolumes = cms.bool( True ),
   returnTangentPlane = cms.bool( True )
+)
+fragment.TrackerAdditionalParametersPerDetESModule = cms.ESProducer( "TrackerAdditionalParametersPerDetESModule",
+  appendToDataLabel = cms.string( "" )
 )
 fragment.TransientTrackBuilderESProducer = cms.ESProducer( "TransientTrackBuilderESProducer",
   ComponentName = cms.string( "TransientTrackBuilder" )
