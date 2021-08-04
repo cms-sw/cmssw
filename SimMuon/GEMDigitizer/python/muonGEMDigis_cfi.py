6559ac2877a66ee68621dddd80f44d3f7d2f5f81
yeckang <yechan.kang@cern.ch>
2021-08-04 18:08:31 +0200

    Added bx0filter for Run3 condition

--- SimMuon/GEMDigitizer/python/muonGEMDigis_cfi.py
+++ SimMuon/GEMDigitizer/python/muonGEMDigis_cfi.py
@@ -7,10 +7,10 @@ from Configuration.ProcessModifiers.premix_stage2_cff import premix_stage2
 premix_stage2.toModify(simMuonGEMDigis, mixLabel = "mixData")
 
 from Configuration.Eras.Modifier_run2_common_cff import run2_common
-run2_common.toModify( simMuonGEMDigis, instLumi = 1.5)
+run2_common.toModify( simMuonGEMDigis, instLumi = 1.5, bx0filter = True)
 
 from Configuration.Eras.Modifier_run3_common_cff import run3_common
-run3_common.toModify( simMuonGEMDigis, instLumi = 2.0)
+run3_common.toModify( simMuonGEMDigis, instLumi = 2.0, bx0filter = True)
 
 from Configuration.Eras.Modifier_phase2_common_cff import phase2_common
-phase2_common.toModify( simMuonGEMDigis, instLumi = 5)
+phase2_common.toModify( simMuonGEMDigis, instLumi = 5, bx0filter = False)
