84e801b08cab78fd55d6270a73ce65ae848c1063
Thomas Reis <thomas.reis@cern.ch>
2021-08-04 15:11:50 +0200

    Fix crash of ECAL GPU reco when ECAL is out of the run.

--- EventFilter/EcalRawToDigi/plugins/EcalRawToDigiGPU.cc
+++ EventFilter/EcalRawToDigi/plugins/EcalRawToDigiGPU.cc
@@ -138,6 +138,10 @@ void EcalRawToDigiGPU::acquire(edm::Event const& event,
   if (counter > 0) {
     ecal::raw::entryPoint(
         inputCPU, inputGPU, outputGPU_, scratchGPU, outputCPU_, conditions, ctx.stream(), counter, currentCummOffset);
+  } else {
+    // reset the number of channels
+    outputCPU_.nchannels[0] = 0;
+    outputCPU_.nchannels[1] = 0;
   }
 }
 
