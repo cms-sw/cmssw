7787a9bfb54795575bd207028142e77cc0401658
Sven Dildick <sven.dildick@cern.ch>
2021-08-04 13:21:47 -0500

    Put CSC and GEM-CSC local trigger lookup tables in ES objects

---
+++ CondFormats/DataRecord/interface/CSCL1TPLookupTableCCLUTRcd.h
@@ -0,0 +1,6 @@
+#ifndef CondFormats_DataRecord_CSCL1TPLookupTableCCLUTRcd
+#define CondFormats_DataRecord_CSCL1TPLookupTableCCLUTRcd
+#include "FWCore/Framework/interface/EventSetupRecordImplementation.h"
+class CSCL1TPLookupTableCCLUTRcd : public edm::eventsetup::EventSetupRecordImplementation<CSCL1TPLookupTableCCLUTRcd> {
+};
+#endif
