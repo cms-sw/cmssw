8a26d827aa41266899daa998cc171ac2f0de7e99
Sunanda <sunanda.banerjee@cern.ch>
2021-08-05 23:21:18 +0200

    Put HGCPassive in src

---
+++ SimG4CMS/HGCalTestBeam/plugins/module.cc
@@ -0,0 +1,5 @@
+#include "SimG4CMS/HGCalTestBeam/interface/HGCPassive.h"
+#include "SimG4Core/Watcher/interface/SimWatcherFactory.h"
+#include "FWCore/PluginManager/interface/ModuleDef.h"
+
+DEFINE_SIMWATCHER(HGCPassive);
