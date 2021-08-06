598f03236d24fd5c6f8fc2f80d947732b91c8bd7
Malik Shahzad Muzaffar <shahzad.malik.muzaffar@cern.ch>
2021-08-06 09:43:14 +0200

    Update CondCore/CondDB/src/ConnectionPool.cc
    Co-authored-by: Andrea Perrotta <perrotta@cern.ch>

--- CondCore/CondDB/src/ConnectionPool.cc
+++ CondCore/CondDB/src/ConnectionPool.cc
@@ -2,7 +2,7 @@
 #include "DbConnectionString.h"
 #include "IDbAuthentication.h"
 #include "SessionImpl.h"
-#include "../interface/IOVSchema.h"
+#include "CondCore/CondDB/interface/IOVSchema.h"
 #include "CoralMsgReporter.h"
 //
 #include "CondCore/CondDB/interface/CoralServiceManager.h"
