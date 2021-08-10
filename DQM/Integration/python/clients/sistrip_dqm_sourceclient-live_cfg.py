ad40012bb2b99ca39995e1ac262f164aa364f56d
mmusich <marco.musich@cern.ch>
2021-08-10 10:57:54 +0200

    add commissioning_run as runType and use it in SiStrip online DQM client

--- DQM/Integration/python/clients/sistrip_dqm_sourceclient-live_cfg.py
+++ DQM/Integration/python/clients/sistrip_dqm_sourceclient-live_cfg.py
@@ -288,6 +288,17 @@ if (process.runType.getRunType() == process.runType.cosmic_run or process.runTyp
                          )
 
 
+### COMMISSIONING RUN SETTINGS
+if (process.runType.getRunType() == process.runType.commissioning_run):
+    #event selection for commissioning runs
+    if ((process.runType.getRunType() == process.runType.commissioning_run) and live):
+        process.source.SelectEvents = ['HLT_*']
+
+    process.SiStripFedMonitor = cms.Sequence(process.siStripFEDMonitor)
+    process.p = cms.Path(
+        process.siStripFEDCheck *
+        process.SiStripFedMonitor
+    )
 
 #else :
 ### pp COLLISION SETTING
