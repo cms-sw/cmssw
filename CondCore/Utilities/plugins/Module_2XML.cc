05aca1d20b4735c23e2bc8f502484aa10a35ea15
sgrzegorz <grzegorzsroka2@gmail.com>
2021-08-13 11:26:18 +0200

    Database connection

--- CondCore/Utilities/plugins/Module_2XML.cc
+++ CondCore/Utilities/plugins/Module_2XML.cc
@@ -32,6 +32,7 @@ PAYLOAD_2XML_MODULE(pluginUtilities_payload2xml) {
   PAYLOAD_2XML_CLASS(CTPPSPixelGainCalibrations);
   PAYLOAD_2XML_CLASS(PPSAlignmentConfig)
   PAYLOAD_2XML_CLASS(PPSAlignmentConfiguration)
+  PAYLOAD_2XML_CLASS(PPSAssociationCuts)
   PAYLOAD_2XML_CLASS(CastorChannelQuality);
   PAYLOAD_2XML_CLASS(CastorElectronicsMap);
   PAYLOAD_2XML_CLASS(CastorGainWidths);
