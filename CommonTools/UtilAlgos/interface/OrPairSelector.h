1369632d221d4d37e1448e5d4e555f8488b7d7f0
yuanchao <yuanchao@cern.ch>
2021-08-20 20:23:59 +0800

    fixed code check errors on altered files

--- CommonTools/UtilAlgos/interface/OrPairSelector.h
+++ CommonTools/UtilAlgos/interface/OrPairSelector.h
@@ -5,17 +5,16 @@
 
 namespace reco {
   namespace modules {
-    
-    template<typename S1, typename S2>
+
+    template <typename S1, typename S2>
     struct ParameterAdapter<OrPairSelector<S1, S2> > {
-      static OrPairSelector<S1, S2> make(const edm::ParameterSet & cfg) {
-	return OrPairSelector<S1, S2>(modules::make<S1>(cfg.getParameter<edm::ParameterSet>("cut1")), 
-				      modules::make<S2>(cfg.getParameter<edm::ParameterSet>("cut2"))); 
+      static OrPairSelector<S1, S2> make(const edm::ParameterSet& cfg) {
+        return OrPairSelector<S1, S2>(modules::make<S1>(cfg.getParameter<edm::ParameterSet>("cut1")),
+                                      modules::make<S2>(cfg.getParameter<edm::ParameterSet>("cut2")));
       }
     };
 
-  }
-}
+  }  // namespace modules
+}  // namespace reco
 
 #endif
-
