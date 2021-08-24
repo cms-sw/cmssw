d52c7b37e7984621739919c08686d675c8c13faf
Angela Czirkos <czirkosangela@gmail.com>
2021-08-24 09:21:22 +0200

    apply coding rules

--- RecoLocalTracker/SiPixelRecHits/interface/pixelCPEforGPU.h
+++ RecoLocalTracker/SiPixelRecHits/interface/pixelCPEforGPU.h
@@ -16,12 +16,10 @@ namespace CPEFastParametrisation {
   // From https://cmssdt.cern.ch/dxr/CMSSW/source/CondFormats/SiPixelTransient/src/SiPixelGenError.cc#485-486
   // qbin: int (0-4) describing the charge of the cluster
   // [0: 1.5<Q/Qavg, 1: 1<Q/Qavg<1.5, 2: 0.85<Q/Qavg<1, 3: 0.95Qmin<Q<0.85Qavg, 4: Q<0.95Qmin]
-  constexpr int GenErrorQBins = 5;
+  constexpr int kGenErrorQBins = 5;
   // arbitrary number of bins for sampling errors
-  constexpr int NumErrorBins = 16;
+  constexpr int kNumErrorBins = 16;
 }  // namespace CPEFastParametrisation
-
-using namespace CPEFastParametrisation;
 
 namespace pixelCPEforGPU {
 
@@ -54,9 +52,10 @@ namespace pixelCPEforGPU {
 
     float apeXX, apeYY;  // ape^2
     uint8_t sx2, sy1, sy2;
-    uint8_t sigmax[NumErrorBins], sigmax1[NumErrorBins], sigmay[NumErrorBins];  // in micron
-    float xfact[GenErrorQBins], yfact[GenErrorQBins];
-    int minCh[GenErrorQBins];
+    uint8_t sigmax[CPEFastParametrisation::kNumErrorBins], sigmax1[CPEFastParametrisation::kNumErrorBins],
+        sigmay[CPEFastParametrisation::kNumErrorBins];  // in micron
+    float xfact[CPEFastParametrisation::kGenErrorQBins], yfact[CPEFastParametrisation::kGenErrorQBins];
+    int minCh[CPEFastParametrisation::kGenErrorQBins];
 
     Frame frame;
   };
@@ -353,15 +352,15 @@ namespace pixelCPEforGPU {
 
     auto ch = cp.charge[ic];
     auto bin = 0;
-    for (; bin < GenErrorQBins - 1; ++bin)
+    for (; bin < CPEFastParametrisation::kGenErrorQBins - 1; ++bin)
       // find first bin which minimum charge exceeds cluster charge
       if (ch < detParams.minCh[bin + 1])
         break;
 
     // in detParams qBins are reversed bin0 -> smallest charge, bin4-> largest charge
     // whereas in CondFormats/SiPixelTransient/src/SiPixelGenError.cc it is the opposite
-    // so we reverse the bin here -> GenErrorQBins - 1 - bin
-    cp.status[ic].qBin = GenErrorQBins - 1 - bin;
+    // so we reverse the bin here -> kGenErrorQBins - 1 - bin
+    cp.status[ic].qBin = CPEFastParametrisation::kGenErrorQBins - 1 - bin;
     cp.status[ic].isOneX = isOneX;
     cp.status[ic].isBigX = (isOneX & isBigX) | isEdgeX;
     cp.status[ic].isOneY = isOneY;
@@ -369,8 +368,8 @@ namespace pixelCPEforGPU {
 
     auto xoff = -float(phase1PixelTopology::xOffset) * comParams.thePitchX;
     int low_value = 0;
-    int high_value = NumErrorBins - 1;
-    int bin_value = float(NumErrorBins) * (cp.xpos[ic] + xoff) / (2 * xoff);
+    int high_value = CPEFastParametrisation::kNumErrorBins - 1;
+    int bin_value = float(CPEFastParametrisation::kNumErrorBins) * (cp.xpos[ic] + xoff) / (2 * xoff);
     // return estimated bin value truncated to [0, 15]
     int jx = std::clamp(bin_value, low_value, high_value);
 
