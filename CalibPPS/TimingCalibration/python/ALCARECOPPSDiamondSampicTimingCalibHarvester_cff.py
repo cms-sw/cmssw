6e29cd397ac0ded29170c317b02e7c14fff94204
Christopher <krzysztof.misan@cern.ch>
2021-08-26 22:20:02 +0200

    fix typo in harvester cff

--- CalibPPS/TimingCalibration/python/ALCARECOPPSDiamondSampicTimingCalibHarvester_cff.py
+++ CalibPPS/TimingCalibration/python/ALCARECOPPSDiamondSampicTimingCalibHarvester_cff.py
@@ -1,5 +1,5 @@
 import FWCore.ParameterSet.Config as cms
 
 from Geometry.VeryForwardGeometry.geometryRPFromDB_cfi import *
-from CalibPPS.TimingCalibration.ppsDiamondSampicTimingCalibrationPCLHarvester_cfi import *
+from CalibPPS.TimingCalibration.PPSDiamondSampicTimingCalibrationPCLHarvester_cfi import *
 
