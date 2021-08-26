7d4a23c41aeb0306c5b083c8870e33a7af3abb9d
Christopher Jones <chrisdjones15@gmail.com>
2021-08-25 19:10:47 -0500

    Use EDPutTokenT to avoid making a temporary container on the heap
    Some code-formatting also snuck in.

--- RecoEgamma/EgammaElectronProducers/plugins/LowPtGsfElectronIDProducer.cc
+++ RecoEgamma/EgammaElectronProducers/plugins/LowPtGsfElectronIDProducer.cc
@@ -42,14 +42,15 @@ private:
   edm::EDGetTokenT<reco::GsfElectronCollection> electrons_;
   edm::EDGetTokenT<pat::ElectronCollection> patElectrons_;
   const edm::EDGetTokenT<double> rho_;
-  edm::EDGetTokenT<edm::ValueMap<float> > unbiased_;
+  edm::EDGetTokenT<edm::ValueMap<float>> unbiased_;
   const edm::ESGetToken<MagneticField, IdealMagneticFieldRecord> fieldToken_;
 
   const std::vector<std::string> names_;
+  std::vector<edm::EDPutTokenT<edm::ValueMap<float>>> putTokens_;
   const bool passThrough_;
   const double minPtThreshold_;
   const double maxPtThreshold_;
-  std::vector<std::unique_ptr<const GBRForest> > models_;
+  std::vector<std::unique_ptr<const GBRForest>> models_;
   const std::vector<double> thresholds_;
   const std::string versionName_;
   enum class Version { V0, V1 };
@@ -65,19 +66,19 @@ LowPtGsfElectronIDProducer::LowPtGsfElectronIDProducer(const edm::ParameterSet& 
       rho_(consumes<double>(conf.getParameter<edm::InputTag>("rho"))),
       unbiased_(),
       fieldToken_(esConsumes()),
-      names_(conf.getParameter<std::vector<std::string> >("ModelNames")),
+      names_(conf.getParameter<std::vector<std::string>>("ModelNames")),
       passThrough_(conf.getParameter<bool>("PassThrough")),
       minPtThreshold_(conf.getParameter<double>("MinPtThreshold")),
       maxPtThreshold_(conf.getParameter<double>("MaxPtThreshold")),
-      thresholds_(conf.getParameter<std::vector<double> >("ModelThresholds")),
+      thresholds_(conf.getParameter<std::vector<double>>("ModelThresholds")),
       versionName_(conf.getParameter<std::string>("Version")) {
   if (usePAT_) {
     patElectrons_ = consumes<pat::ElectronCollection>(conf.getParameter<edm::InputTag>("electrons"));
   } else {
     electrons_ = consumes<reco::GsfElectronCollection>(conf.getParameter<edm::InputTag>("electrons"));
-    unbiased_ = consumes<edm::ValueMap<float> >(conf.getParameter<edm::InputTag>("unbiased"));
+    unbiased_ = consumes<edm::ValueMap<float>>(conf.getParameter<edm::InputTag>("unbiased"));
   }
-  for (auto& weights : conf.getParameter<std::vector<std::string> >("ModelWeights")) {
+  for (auto& weights : conf.getParameter<std::vector<std::string>>("ModelWeights")) {
     models_.push_back(createGBRForest(edm::FileInPath(weights)));
   }
   if (names_.size() != models_.size()) {
@@ -95,8 +96,9 @@ LowPtGsfElectronIDProducer::LowPtGsfElectronIDProducer(const edm::ParameterSet& 
   } else {
     throw cms::Exception("Incorrect configuration") << "Unknown Version: " << versionName_ << "\n";
   }
+  putTokens_.reserve(names_.size());
   for (const auto& name : names_) {
-    produces<edm::ValueMap<float> >(name);
+    putTokens_.emplace_back(produces<edm::ValueMap<float>>(name));
   }
 }
 
@@ -175,11 +177,11 @@ void LowPtGsfElectronIDProducer::doWork(
         output[iele] = -999.;
       }
     }
-    auto ptr = std::make_unique<edm::ValueMap<float> >(edm::ValueMap<float>());
-    edm::ValueMap<float>::Filler filler(*ptr);
+    edm::ValueMap<float> valueMap;
+    edm::ValueMap<float>::Filler filler(valueMap);
     filler.insert(electrons, output.begin(), output.end());
     filler.fill();
-    event.put(std::move(ptr), names_[index]);
+    event.emplace(putTokens_[index], std::move(valueMap));
   }
 }
 
@@ -204,10 +206,10 @@ void LowPtGsfElectronIDProducer::fillDescriptions(edm::ConfigurationDescriptions
   desc.add<edm::InputTag>("electrons", edm::InputTag("lowPtGsfElectrons"));
   desc.addOptional<edm::InputTag>("unbiased", edm::InputTag("lowPtGsfElectronSeedValueMaps:unbiased"));
   desc.add<edm::InputTag>("rho", edm::InputTag("fixedGridRhoFastjetAll"));
-  desc.add<std::vector<std::string> >("ModelNames", {""});
-  desc.add<std::vector<std::string> >(
+  desc.add<std::vector<std::string>>("ModelNames", {""});
+  desc.add<std::vector<std::string>>(
       "ModelWeights", {"RecoEgamma/ElectronIdentification/data/LowPtElectrons/LowPtElectrons_ID_2020Nov28.root"});
-  desc.add<std::vector<double> >("ModelThresholds", {-99.});
+  desc.add<std::vector<double>>("ModelThresholds", {-99.});
   desc.add<bool>("PassThrough", false);
   desc.add<double>("MinPtThreshold", 0.5);
   desc.add<double>("MaxPtThreshold", 15.);
