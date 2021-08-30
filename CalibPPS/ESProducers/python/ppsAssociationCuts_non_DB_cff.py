7bfc85520fa38e5a96540cac2e4c1a3bd5591885
Jan Kaspar <jan.kaspar@cern.ch>
2021-08-30 14:05:30 +0200

    update association-cut model for 2021

--- CalibPPS/ESProducers/python/ppsAssociationCuts_non_DB_cff.py
+++ CalibPPS/ESProducers/python/ppsAssociationCuts_non_DB_cff.py
@@ -65,30 +65,28 @@ p2018 = cms.PSet(
 )
 ppsAssociationCutsESSource.configuration.append(p2018)
 
-pFuture = cms.PSet(
+p2021 = cms.PSet(
     validityRange=cms.EventRange("343890:min - 999999:max"),
     association_cuts_45=cms.PSet(
-        x_cut_mean=cms.string("-0.065194856"),
-        x_cut_threshold=cms.string("4. * 0.16008188"),
-        y_cut_mean=cms.string("+0.10973631"),
-        y_cut_threshold=cms.string("4. * 0.1407986"),
-        xi_cut_mean=cms.string("+3.113062e-5"),
-        xi_cut_threshold=cms.string("4. * 0.0012403586"),
+        x_cut_mean = cms.string("- ( (-0.530895+0.112595*[x_near]+-0.006785*[x_near]*[x_near]) - (0.046487+0.179333/[x_near])*abs([y_near]) )"),
+        x_cut_threshold = cms.string("4 * ( (0.091692+0.009316*[x_near]) + (-0.000727)*[y_near]*[y_near] )"),
+        y_cut_mean = cms.string("- ( (0.672525+-0.619398/[x_near])*TMath::Erf([y_near]/(0.968499+0.597136*[x_near])) )"),
+        y_cut_threshold = cms.string("4 * ( (0.015202+0.962944/[x_near]/[x_near]) + (0.009804+-7.710241/pow([x_near],5))*abs([y_near]) )"),
+
         ti_tr_min=cms.double(-1.5),
         ti_tr_max=cms.double(2.0)
     ),
     association_cuts_56=cms.PSet(
-        x_cut_mean=cms.string("+0.073016431"),
-        x_cut_threshold=cms.string("5. * 0.18126434"),
-        y_cut_mean=cms.string("+0.064261029"),
-        y_cut_threshold=cms.string("5. * 0.14990802"),
-        xi_cut_mean=cms.string("-1.1852528e-5"),
-        xi_cut_threshold=cms.string("5. * 0.002046409"),
+        x_cut_mean = cms.string("- ( (-0.530895+0.112595*[x_near]+-0.006785*[x_near]*[x_near]) - (0.046487+0.179333/[x_near])*abs([y_near]) )"),
+        x_cut_threshold = cms.string("4 * ( (0.091692+0.009316*[x_near]) + (-0.000727)*[y_near]*[y_near] )"),
+        y_cut_mean = cms.string("- ( (0.672525+-0.619398/[x_near])*TMath::Erf([y_near]/(0.968499+0.597136*[x_near])) )"),
+        y_cut_threshold = cms.string("4 * ( (0.015202+0.962944/[x_near]/[x_near]) + (0.009804+-7.710241/pow([x_near],5))*abs([y_near]) )"),
+
         ti_tr_min=cms.double(-1.5),
         ti_tr_max=cms.double(2.0)
     ),
 )
-ppsAssociationCutsESSource.configuration.append(pFuture)
+ppsAssociationCutsESSource.configuration.append(p2021)
 
 def use_single_infinite_iov_entry(ppsAssociationCutsESSource, iov):
     ppsAssociationCutsESSource.configuration = cms.VPSet()
