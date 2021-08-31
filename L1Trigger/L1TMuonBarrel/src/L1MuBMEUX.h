3306d8ea8de310a477b431779e79b72abf97303f
Christopher Jones <chrisdjones15@gmail.com>
2021-08-31 12:26:41 -0500

    Added esConsumes to helper classes in L1TMuonBarrel

--- L1Trigger/L1TMuonBarrel/src/L1MuBMEUX.h
+++ L1Trigger/L1TMuonBarrel/src/L1MuBMEUX.h
@@ -34,7 +34,6 @@
 // Collaborating Class Declarations --
 //------------------------------------
 
-#include <FWCore/Framework/interface/ESHandle.h>
 #include "CondFormats/L1TObjects/interface/L1TMuonBarrelParams.h"
 #include "CondFormats/DataRecord/interface/L1TMuonBarrelParamsRcd.h"
 
@@ -60,7 +59,7 @@ public:
   bool operator==(const L1MuBMEUX&) const;
 
   /// run Extrapolator
-  void run(const edm::EventSetup& c);
+  void run(const L1TMuonBarrelParams& c);
 
   /// reset Extrapolator
   void reset();
@@ -117,15 +116,11 @@ private:
   const L1MuBMTrackSegPhi* m_start;   // start track segment
   const L1MuBMTrackSegPhi* m_target;  // target track segment
 
-  edm::ESHandle<L1TMuonBarrelParams> bmtfParamsHandle;
-
-  //edm::ESHandle< L1MuDTExtLut > theExtLUTs;  // extrapolation look-up tables
   L1MuBMLUTHandler* theExtLUTs;
   int const theExtFilter = 1;           // extrapolation quality filter
   unsigned short const nbit_phi = 12;   // number of bits used for phi
   unsigned short const nbit_phib = 10;  // number of bits used for phib
 
-  //edm::ESHandle< L1MuDTTFParameters > pars;
   L1MuDTTFParameters pars;
 };
 
