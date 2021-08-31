70cde0adf6d5b67c8b3abdf6eb145cc3a698fae2
Jan Kaspar <jan.kaspar@cern.ch>
2021-08-31 13:04:59 +0200

    add 2022 scenario for direct simulation

---
+++ Validation/CTPPS/python/simu_config/year_2022_cff.py
@@ -0,0 +1,66 @@
+import FWCore.ParameterSet.Config as cms
+
+from Validation.CTPPS.simu_config.base_cff import *
+
+# base profile settings for 2022
+profile_base_2022 = profile_base.clone(
+  ctppsLHCInfo = dict(
+    beamEnergy = 7000
+  ),
+
+  ctppsOpticalFunctions = dict(
+    opticalFunctions = cms.VPSet(
+      cms.PSet( xangle = cms.double(144.974), fileName = cms.FileInPath("CalibPPS/ESProducers/data/optical_functions/2022/version_pre1/144.974urad.root") ),
+      cms.PSet( xangle = cms.double(160.000), fileName = cms.FileInPath("CalibPPS/ESProducers/data/optical_functions/2022/version_pre1/160.000urad.root") )
+    ),
+
+    scoringPlanes = cms.VPSet(
+      # z in cm
+      cms.PSet( rpId = cms.uint32(2014838784), dirName = cms.string("XRPH_D6L5_B2"), z = cms.double(-21255.0) ),  # RP 003, pixel
+      cms.PSet( rpId = cms.uint32(2056257536), dirName = cms.string("XRPH_A6L5_B2"), z = cms.double(-21507.8) ),  # RP 022, diamond
+      cms.PSet( rpId = cms.uint32(2054160384), dirName = cms.string("XRPH_E6L5_B2"), z = cms.double(-21570.0) ),  # RP 016, diamond
+      cms.PSet( rpId = cms.uint32(2023227392), dirName = cms.string("XRPH_B6L5_B2"), z = cms.double(-21955.0) ),  # RP 023, pixel
+
+      cms.PSet( rpId = cms.uint32(2031616000), dirName = cms.string("XRPH_D6R5_B1"), z = cms.double(+21255.0) ),  # RP 103, pixel
+      cms.PSet( rpId = cms.uint32(2073034752), dirName = cms.string("XRPH_A6R5_B1"), z = cms.double(+21507.8) ),  # RP 122, diamond
+      cms.PSet( rpId = cms.uint32(2070937600), dirName = cms.string("XRPH_E6R5_B1"), z = cms.double(+21570.0) ),  # RP 116, diamond
+      cms.PSet( rpId = cms.uint32(2040004608), dirName = cms.string("XRPH_B6R5_B1"), z = cms.double(+21955.0) ),  # RP 123, pixel
+    )
+  ),
+ 
+  ctppsDirectSimuData = dict(
+    empiricalAperture45 = cms.string("1E3*([xi] - 0.20)"),
+    empiricalAperture56 = cms.string("1E3*([xi] - 0.20)")
+  )
+)
+
+# adjust basic settings
+generator.energy = profile_base_2022.ctppsLHCInfo.beamEnergy
+
+# geometry (NB: until a dedicated 2022 geometry is issued, it is OK to use 2021 one here)
+from Geometry.VeryForwardGeometry.geometryRPFromDD_2021_cfi import *
+ctppsCompositeESSource.compactViewTag = ctppsGeometryESModule.compactViewTag
+del ctppsGeometryESModule # this functionality is replaced by the composite ES source
+
+# local reconstruction
+ctppsLocalTrackLiteProducer.includeStrips = False
+ctppsLocalTrackLiteProducer.includePixels = True
+ctppsLocalTrackLiteProducer.includeDiamonds = True
+
+reco_local = cms.Sequence(
+  ctppsPixelLocalTracks
+  * ctppsDiamondLocalReconstruction
+  * ctppsLocalTrackLiteProducer
+)
+
+# RP ids
+rpIds = cms.PSet(
+  rp_45_F = cms.uint32(23),
+  rp_45_N = cms.uint32(3),
+  rp_56_N = cms.uint32(103),
+  rp_56_F = cms.uint32(123)
+)
+
+# default list of profiles
+from Validation.CTPPS.simu_config.profile_2022_default_cff import profile_2022_default
+ctppsCompositeESSource.periods = [profile_2022_default]
