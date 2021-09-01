c8d0f3a7d4829be581fa7fbda7cca2008922b5b9
Sunanda <sunanda.banerjee@cern.ch>
2021-09-01 20:01:58 +0200

    Clean up a few cfi's for HCAL and SimDB geometry

--- Configuration/Geometry/python/GeometryDDDSimDB_cff.py
+++ Configuration/Geometry/python/GeometryDDDSimDB_cff.py
@@ -8,8 +8,6 @@ from GeometryReaders.XMLIdealGeometryESSource.cmsGeometryDB_cff import *
 from Geometry.MuonNumbering.muonNumberingInitialization_cfi import *
 from Geometry.TrackerNumberingBuilder.trackerNumberingGeometryDB_cfi import *
 from Geometry.EcalCommonData.ecalSimulationParameters_cff   import *
-from Geometry.HcalCommonData.hcalSimulationParameters_cff   import *
-from Geometry.HcalCommonData.hcalDDDSimConstants_cfi        import *
-from Geometry.HcalCommonData.hcalSimulationConstants_cfi    import *
-from Geometry.HcalCommonData.caloSimulationParameters_cff   import *
+from Geometry.HcalCommonData.hcalSimDBConstants_cff         import *
 from Geometry.MuonNumbering.muonGeometryConstants_cff       import *
+from Geometry.MuonNumbering.muonOffsetESProducer_cff        import *
