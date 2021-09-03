22291fce5051acdde5cb51ea5af002390a0db1a9
Sunanda <sunanda.banerjee@cern.ch>
2021-09-03 16:15:26 +0200

    Code check

--- Calibration/HcalIsolatedTrackReco/plugins/HITRegionalPixelSeedGenerator.cc
+++ Calibration/HcalIsolatedTrackReco/plugins/HITRegionalPixelSeedGenerator.cc
@@ -168,8 +168,8 @@ public:
       e.getByToken(token_vertex, vertices);
       const reco::VertexCollection vertCollection = *(vertices.product());
       if (!vertCollection.empty()) {
-//      reco::VertexCollection::const_iterator ci = vertCollection.begin();
-//      originz = ci->z();
+        //      reco::VertexCollection::const_iterator ci = vertCollection.begin();
+        //      originz = ci->z();
       } else {
         deltaZVertex = 15.;
       }
