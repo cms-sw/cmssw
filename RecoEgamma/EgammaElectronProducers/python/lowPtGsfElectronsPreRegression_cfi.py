f661be29c5afea269b968b530cc5b60e6524964c
bainbrid <bainbrid@users.noreply.github.com>
2021-09-07 17:25:49 +0200

    do NOT take 'closest' track from generalTracksBeforeMixing for FastSim

--- RecoEgamma/EgammaElectronProducers/python/lowPtGsfElectronsPreRegression_cfi.py
+++ RecoEgamma/EgammaElectronProducers/python/lowPtGsfElectronsPreRegression_cfi.py
@@ -1,6 +1,3 @@
 from RecoEgamma.EgammaElectronProducers.gsfElectrons_cfi import ecalDrivenGsfElectrons
 
 lowPtGsfElectronsPreRegression = ecalDrivenGsfElectrons.clone(gsfElectronCoresTag = "lowPtGsfElectronCores")
-
-from Configuration.Eras.Modifier_fastSim_cff import fastSim
-fastSim.toModify(lowPtGsfElectronsPreRegression,ctfTracksTag = "generalTracksBeforeMixing")
