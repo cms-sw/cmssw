107d3e7b4073197a243794028976d584318117d1
W. David Dagenhart <wdd@fnal.gov>
2021-09-08 19:18:48 +0200

    Delete unused EventSetup get call

--- RecoTracker/TkSeedGenerator/plugins/SeedGeneratorFromProtoTracksEDProducer.h
+++ RecoTracker/TkSeedGenerator/plugins/SeedGeneratorFromProtoTracksEDProducer.h
@@ -3,16 +3,12 @@
 #include "FWCore/Utilities/interface/Visibility.h"
 
 #include "FWCore/Framework/interface/stream/EDProducer.h"
+#include "FWCore/Framework/interface/FrameworkfwdMostUsed.h"
 #include "DataFormats/TrackReco/interface/TrackFwd.h"
 #include "DataFormats/VertexReco/interface/VertexFwd.h"
 #include "SeedFromConsecutiveHitsCreator.h"
 
 #include <string>
-
-namespace edm {
-  class Event;
-  class EventSetup;
-}  // namespace edm
 
 class dso_hidden SeedGeneratorFromProtoTracksEDProducer : public edm::stream::EDProducer<> {
 public:
@@ -26,7 +22,6 @@ private:
   const double originRadius;
   const bool useProtoTrackKinematics;
   const bool useEventsWithNoVertex;
-  const std::string builderName;
   const bool usePV_;
   const bool includeFourthHit_;
   const edm::EDGetTokenT<reco::TrackCollection> theInputCollectionTag;
