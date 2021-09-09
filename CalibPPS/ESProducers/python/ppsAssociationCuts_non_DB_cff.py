db0a07d333d7aeb3379301bf49139be5a5d63a3d
Grzegorz Sroka <grzegorzsroka2@gmail.com>
2021-09-09 20:26:30 +0200

    add 2022 config and remove 2021 config from the default list

--- CalibPPS/ESProducers/python/ppsAssociationCuts_non_DB_cff.py
+++ CalibPPS/ESProducers/python/ppsAssociationCuts_non_DB_cff.py
@@ -66,6 +66,30 @@ p2018 = cms.PSet(
 ppsAssociationCutsESSource.configuration.append(p2018)
 
 p2021 = cms.PSet(
+    validityRange=cms.EventRange("1234:1 - 1234:max"), # NB: a fake IOV, there are no LHC data from 2021 - keep it just for MC
+    association_cuts_45=cms.PSet(
+        x_cut_mean = cms.string("- ( (-0.530895+0.112595*[x_near]+-0.006785*[x_near]*[x_near]) - (0.046487+0.179333/[x_near])*abs([y_near]) )"),
+        x_cut_threshold = cms.string("4 * ( (0.091692+0.009316*[x_near]) + (-0.000727)*[y_near]*[y_near] )"),
+        y_cut_mean = cms.string("- ( (0.672525+-0.619398/[x_near])*TMath::Erf([y_near]/(0.968499+0.597136*[x_near])) )"),
+        y_cut_threshold = cms.string("4 * ( (0.015202+0.962944/[x_near]/[x_near]) + (0.009804+-7.710241/pow([x_near],5))*abs([y_near]) )"),
+
+        ti_tr_min=cms.double(-1.5),
+        ti_tr_max=cms.double(2.0)
+    ),
+    association_cuts_56=cms.PSet(
+        x_cut_mean = cms.string("- ( (-0.530895+0.112595*[x_near]+-0.006785*[x_near]*[x_near]) - (0.046487+0.179333/[x_near])*abs([y_near]) )"),
+        x_cut_threshold = cms.string("4 * ( (0.091692+0.009316*[x_near]) + (-0.000727)*[y_near]*[y_near] )"),
+        y_cut_mean = cms.string("- ( (0.672525+-0.619398/[x_near])*TMath::Erf([y_near]/(0.968499+0.597136*[x_near])) )"),
+        y_cut_threshold = cms.string("4 * ( (0.015202+0.962944/[x_near]/[x_near]) + (0.009804+-7.710241/pow([x_near],5))*abs([y_near]) )"),
+
+        ti_tr_min=cms.double(-1.5),
+        ti_tr_max=cms.double(2.0)
+    ),
+)
+
+# NB: do not append the 2021 config - not used for any LHC data
+
+p2022 = cms.PSet(
     validityRange=cms.EventRange("343890:min - 999999:max"),
     association_cuts_45=cms.PSet(
         x_cut_mean = cms.string("- ( (-0.530895+0.112595*[x_near]+-0.006785*[x_near]*[x_near]) - (0.046487+0.179333/[x_near])*abs([y_near]) )"),
@@ -86,7 +110,7 @@ p2021 = cms.PSet(
         ti_tr_max=cms.double(2.0)
     ),
 )
-ppsAssociationCutsESSource.configuration.append(p2021)
+ppsAssociationCutsESSource.configuration.append(p2022)
 
 def use_single_infinite_iov_entry(ppsAssociationCutsESSource, iov):
     ppsAssociationCutsESSource.configuration = cms.VPSet()
