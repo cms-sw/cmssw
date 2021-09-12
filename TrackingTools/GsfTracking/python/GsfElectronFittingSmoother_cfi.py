23bc20c22efc71af6b494ef940ddc028796f6485
swmukher <s.mukherjee@cern.ch>
2021-09-12 12:58:52 +0200

    TrackingTools/GsfTracking

--- TrackingTools/GsfTracking/python/GsfElectronFittingSmoother_cfi.py
+++ TrackingTools/GsfTracking/python/GsfElectronFittingSmoother_cfi.py
@@ -4,5 +4,7 @@ import TrackingTools.TrackFitters.KFFittingSmoother_cfi
 GsfElectronFittingSmoother = TrackingTools.TrackFitters.KFFittingSmoother_cfi.KFFittingSmoother.clone(
     ComponentName = 'GsfElectronFittingSmoother',
     Fitter        = 'GsfTrajectoryFitter',
-    Smoother      = 'GsfTrajectorySmoother'
+    Smoother      = 'GsfTrajectorySmoother',
+    MinNumberOfHitsHighEta = 3,
+    HighEtaSwitch = 2.5
 )
