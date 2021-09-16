4017ad352e523a694a8e6525da5cb3f6808a2c6c
Javier <Javier.Fernandez@cern.ch>
2021-09-16 17:56:55 +0200

    Remove long from DQMServices

--- DQMServices/StreamerIO/scripts/esMonitoring.py
+++ DQMServices/StreamerIO/scripts/esMonitoring.py
@@ -52,7 +52,7 @@ class ElasticReport(object):
 
             pr = l.split("=")
             if len(pr) > 1 and pr[0] == "runNumber" and pr[1].isdigit():
-                run = long(pr[1])
+                run = int(pr[1])
                 self.doc["run"] = run
 
         self.make_id()
