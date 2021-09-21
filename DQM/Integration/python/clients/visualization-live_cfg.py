292e6ce4b6864ac1943e5442ee5a0b33089013f5
jfernan2 <Javier.Fernandez@cern.ch>
2021-09-21 12:08:43 +0200

    Update visualization-live_cfg.py

--- DQM/Integration/python/clients/visualization-live_cfg.py
+++ DQM/Integration/python/clients/visualization-live_cfg.py
@@ -61,7 +61,7 @@ if unitTest:
 process.source = source
 
 if not unitTest:
-    process.source.inputFileTransitionsEachEvent = cms.untracked.bool(True)
+    process.source.inputFileTransitionsEachEvent = True
     process.source.skipFirstLumis                = True
     process.source.minEventsPerLumi              = 0
     process.source.nextLumiTimeoutMillis         = 10000
