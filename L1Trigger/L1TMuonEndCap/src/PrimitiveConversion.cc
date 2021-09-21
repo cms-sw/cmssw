3d7374e639cc219b48996a34f3e3061c806d71ef
jrotter2 <67421720+jrotter2@users.noreply.github.com>
2021-09-21 14:57:00 -0500

    Clumped Code, Updated isOTMB and isTMB names.
    +Moved pattern calculation above the set parameters section in the code. 
    +Added pattern variable which gets passed to conv_hit.set_pattern(pattern) on line 190.
    
    +Changed isME11 to isOTMB and (isMEX2 or isME13) to isTMB in both lines 135-139 and 327-330.

--- L1Trigger/L1TMuonEndCap/src/PrimitiveConversion.cc
+++ L1Trigger/L1TMuonEndCap/src/PrimitiveConversion.cc
@@ -128,6 +128,29 @@ void PrimitiveConversion::convert_csc(int pc_sector,
       emtf_assert(tp_subsector == 2);
     }
   }
+  
+  // Calculate Pattern
+  unsigned pattern = tp_data.pattern;
+  const auto& detid(conv_hit.CreateCSCDetId());
+  const bool isOTMB(detid.isME11() or detid.isME21() or detid.isME31() or detid.isME41());
+  const bool isTMB((detid.isME12() or detid.isME22() or detid.isME32() or detid.isME42()) or (detid.isME13()));
+
+  // check if CCLUT is on for this CSC TP
+  const bool useRun3CCLUT((useRun3CCLUT_OTMB_ and isOTMB) or (useRun3CCLUT_TMB_ and isTMB));
+  if (useRun3CCLUT) {
+    // convert slope to Run-2 pattern for CSC TPs coming from MEX/1 chambers
+    // where the CCLUT algorithm is enabled
+    const unsigned slopeList[32] = {10, 10, 10, 8, 8, 8, 6, 6, 6, 4, 4, 4, 2, 2, 2, 2,
+                                    10, 10, 10, 9, 9, 9, 7, 7, 7, 5, 5, 5, 3, 3, 3, 3};
+
+    // this LUT follows the same convention as in CSCPatternBank.cc
+    unsigned slope_and_sign(tp_data.slope);
+    if (tp_data.bend == 0) {
+      slope_and_sign += 16;
+    }
+    unsigned run2_converted_PID = slopeList[slope_and_sign];
+    pattern = run2_converted_PID;
+  }
 
   // Set properties
   conv_hit.SetCSCDetId(tp_detId);
@@ -164,30 +187,8 @@ void PrimitiveConversion::convert_csc(int pc_sector,
   conv_hit.set_alct_quality(tp_data.alct_quality);
   conv_hit.set_clct_quality(tp_data.clct_quality);
 
-  const auto& detid(conv_hit.CreateCSCDetId());
-  const bool isMEX1(detid.isME11() or detid.isME21() or detid.isME31() or detid.isME41());
-  const bool isMEX2(detid.isME12() or detid.isME22() or detid.isME32() or detid.isME42());
-  const bool isME13(detid.isME13());
-
-  // check if CCLUT is on for this CSC TP
-  const bool useRun3CCLUT((useRun3CCLUT_OTMB_ and isMEX1) or (useRun3CCLUT_TMB_ and (isMEX2 or isME13)));
-  if (useRun3CCLUT) {
-    // convert slope to Run-2 pattern for CSC TPs coming from MEX/1 chambers
-    // where the CCLUT algorithm is enabled
-    const unsigned slopeList[32] = {10, 10, 10, 8, 8, 8, 6, 6, 6, 4, 4, 4, 2, 2, 2, 2,
-                                    10, 10, 10, 9, 9, 9, 7, 7, 7, 5, 5, 5, 3, 3, 3, 3};
-
-    // this LUT follows the same convention as in CSCPatternBank.cc
-    unsigned slope_and_sign(tp_data.slope);
-    if (tp_data.bend == 0) {
-      slope_and_sign += 16;
-    }
-    unsigned run2_converted_PID = slopeList[slope_and_sign];
-    conv_hit.set_pattern(run2_converted_PID);
-  } else {
-    conv_hit.set_pattern(tp_data.pattern);
-  }
-
+  conv_hit.set_pattern(pattern);
+  
   // Run-3
   conv_hit.set_strip_quart(tp_data.strip_quart);
   conv_hit.set_strip_eighth(tp_data.strip_eighth);
@@ -323,11 +324,10 @@ void PrimitiveConversion::convert_csc_details(EMTFHit& conv_hit) const {
 
   // check if the CCLUT algorithm is on in this chamber
   const auto& detid(conv_hit.CreateCSCDetId());
-  const bool isMEX1(detid.isME11() or detid.isME21() or detid.isME31() or detid.isME41());
-  const bool isMEX2(detid.isME12() or detid.isME22() or detid.isME32() or detid.isME42());
-  const bool isME13(detid.isME13());
+  const bool isOTMB(detid.isME11() or detid.isME21() or detid.isME31() or detid.isME41());
+  const bool isTMB((detid.isME12() or detid.isME22() or detid.isME32() or detid.isME42()) or (detid.isME13()));
 
-  const bool useRun3CCLUT((useRun3CCLUT_OTMB_ and isMEX1) or (useRun3CCLUT_TMB_ and (isMEX2 or isME13)));
+  const bool useRun3CCLUT((useRun3CCLUT_OTMB_ and isOTMB) or (useRun3CCLUT_TMB_ and isTMB));
 
   if (is_10degree) {
     eighth_strip = fw_strip << 2;  // full precision, uses only 2 bits of pattern correction
