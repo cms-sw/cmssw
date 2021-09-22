8ca9cf0370b8222b94c2468fddd0882aa538d4ad
mmusich <marco.musich@cern.ch>
2021-09-22 20:05:29 +0200

    fix offline BS swap for express Run3

--- RecoVertex/BeamSpotProducer/python/BeamSpotOnline_cfi.py
+++ RecoVertex/BeamSpotProducer/python/BeamSpotOnline_cfi.py
@@ -10,3 +10,5 @@ onlineBeamSpotProducer = cms.EDProducer('BeamSpotOnlineProducer',
                                         useTransientRecord = cms.bool(False)
 )
 
+from Configuration.Eras.Modifier_run3_common_cff import run3_common
+run3_common.toModify(onlineBeamSpotProducer, useTransientRecord = True)
