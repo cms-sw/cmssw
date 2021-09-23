2f3ae1c01220adbf1595de80644e486139731349
Dinyar Rabady <dinyar@cern.ch>
2021-09-23 13:35:29 +0200

    Add tokens and collections for unpacked muon showers

--- EventFilter/L1TRawToDigi/plugins/implementations_stage2/GMTCollections.cc
+++ EventFilter/L1TRawToDigi/plugins/implementations_stage2/GMTCollections.cc
@@ -19,6 +19,12 @@ namespace l1t {
       event_.put(std::move(imdMuonsEMTFPos_), "imdMuonsEMTFPos");
       event_.put(std::move(imdMuonsOMTFNeg_), "imdMuonsOMTFNeg");
       event_.put(std::move(imdMuonsOMTFPos_), "imdMuonsOMTFPos");
+
+      event_.put(std::move(regionalMuonShowersEMTF_), "EMTF");
+      event_.put(std::move(muonShowers_[0]), "MuonShower");
+      for (int i = 1; i < 6; ++i) {
+        event_.put(std::move(muonShowers_[i]), "MuonShowerCopy" + std::to_string(i));
+      }
     }
   }  // namespace stage2
 }  // namespace l1t
