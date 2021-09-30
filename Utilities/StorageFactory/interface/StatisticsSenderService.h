539b60fd2c5094c58b954af69850090cc7053bf0
Chris Jones <chrisdjones15@gmail.com>
2021-09-30 11:25:22 -0500

    StatisticsSenderService now requires LFN or URL
    When sending information to the StatisticsSenderService, the file LFN or URL must be supplied.

--- Utilities/StorageFactory/interface/StatisticsSenderService.h
+++ Utilities/StorageFactory/interface/StatisticsSenderService.h
@@ -6,6 +6,7 @@
 #include <sstream>
 #include <atomic>
 #include <mutex>
+#include <tbb/concurrent_unordered_map.h>
 
 namespace edm {
 
@@ -16,19 +17,24 @@ namespace edm {
 
     class StatisticsSenderService {
     public:
-      StatisticsSenderService(edm::ParameterSet const &pset, edm::ActivityRegistry &ar);
+      StatisticsSenderService(edm::ParameterSet const& pset, edm::ActivityRegistry& ar);
 
-      void setSize(size_t size);
-      void setCurrentServer(const std::string &servername);
-      void filePreCloseEvent(std::string const &lfn, bool usedFallback);
-      static const char *getJobID();
-      static bool getX509Subject(std::string &);
+      void setSize(const std::string& urlOrLfn, size_t size);
+      void setCurrentServer(const std::string& urlOrLfn, const std::string& servername);
+      static const char* getJobID();
+      static bool getX509Subject(std::string&);
+
+      void openingFile(std::string const& lfn, size_t size = -1);
+      void closedFile(std::string const& lfn, bool usedFallback);
 
     private:
+      void filePreCloseEvent(std::string const& lfn, bool usedFallback);
+
+      std::string const* matchedLfn(std::string const& iURL);  //updates its internal cache
       class FileStatistics {
       public:
         FileStatistics();
-        void fillUDP(std::ostringstream &os);
+        void fillUDP(std::ostringstream& os);
 
       private:
         ssize_t m_read_single_operations;
@@ -42,19 +48,30 @@ namespace edm {
         time_t m_start_time;
       };
 
-      void determineHostnames(void);
-      void fillUDP(const std::string &, bool, std::string &);
+      struct FileInfo {
+        explicit FileInfo(std::string const& iLFN);
+        std::string m_filelfn;
+        std::string m_serverhost;
+        std::string m_serverdomain;
+        ssize_t m_size;
+        size_t m_id;  //from m_counter
+        std::atomic<int> m_openCount;
+      };
+
+      void determineHostnames();
+      void fillUDP(const std::string& site, const FileInfo& fileinfo, bool, std::string&);
+      void cleanupOldFiles();
+
       std::string m_clienthost;
       std::string m_clientdomain;
-      std::string m_serverhost;
-      std::string m_serverdomain;
-      std::string m_filelfn;
+      tbb::concurrent_unordered_map<std::string, FileInfo> m_lfnToFileInfo;
+      tbb::concurrent_unordered_map<std::string, std::string> m_urlToLfn;
       FileStatistics m_filestats;
       std::string m_guid;
       size_t m_counter;
-      std::atomic<ssize_t> m_size;
       std::string m_userdn;
       std::mutex m_servermutex;
+      const bool m_debug;
     };
 
   }  // namespace storage
