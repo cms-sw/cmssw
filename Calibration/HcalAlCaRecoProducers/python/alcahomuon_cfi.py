3003219a8a248b9a7fae5375a7d10bee277774e5
Sunanda <sunanda.banerjee@cern.ch>
2021-10-02 02:06:27 +0200

    Modernize the remaining HCAL AlCaReco producers

--- Calibration/HcalAlCaRecoProducers/python/alcahomuon_cfi.py
+++ Calibration/HcalAlCaRecoProducers/python/alcahomuon_cfi.py
@@ -6,24 +6,8 @@ import FWCore.ParameterSet.Config as cms
 #process.load('Configuration.StandardSequences.Reconstruction_Data_cff')
 #from Configuration.StandardSequences.Reconstruction_Data_cff import *
 from RecoLocalCalo.HcalRecAlgos.hcalRecAlgoESProd_cfi import *
-hoCalibProducer = cms.EDProducer("AlCaHOCalibProducer",
-    hbheInput = cms.InputTag("hbhereco"),
-    hotime = cms.untracked.bool(False),
-    hbinfo = cms.untracked.bool(False),
-    sigma = cms.untracked.double(1.0),
-    plotOccupancy = cms.untracked.bool(False),
-    CosmicData = cms.untracked.bool(False),
-    hoInput = cms.InputTag("horeco"),
-    towerInput = cms.InputTag("towerMaker"),
-    RootFileName = cms.untracked.string('test.root'),
-    m_scale = cms.untracked.double(4.0),
-    debug = cms.untracked.bool(False),
-    muons = cms.untracked.InputTag("muons"),
-    #muons = cms.untracked.InputTag("cosmicMuons"),
-    #muons = cms.untracked.InputTag("standAloneMuons"),
-    vertexTags = cms.InputTag("offlinePrimaryVertices"),
-    lumiTags = cms.InputTag("scalersRawToDigi")
-    #lumiTags = cms.InputTag("lumiProducer")
-)
+
+import Calibration.HcalAlCaRecoProducers.alcaHOCalibProducer_cfi
+hoCalibProducer = Calibration.HcalAlCaRecoProducers.alcaHOCalibProducer_cfi.alcaHOCalibProducer.clone()
 
 
