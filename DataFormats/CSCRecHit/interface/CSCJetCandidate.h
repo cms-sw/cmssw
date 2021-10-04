130b698642441551c00842920477db7567d65ee5
Martin <ka_hei_martin_kwok@brown.edu>
2021-10-04 15:01:10 -0500

    add rechit variables

--- DataFormats/CSCRecHit/interface/CSCJetCandidate.h
+++ DataFormats/CSCRecHit/interface/CSCJetCandidate.h
@@ -15,25 +15,42 @@ namespace reco {
   public:
 
     //default constructor
-    CSCJetCandidate(): x_(0.),y_(0.),z_(0.),tPeak_(-999.),tWire_(-999.) {}
+    CSCJetCandidate(): x_(0.),y_(0.),z_(0.),tPeak_(-999.),tWire_(-999.),quality_(0),chamber_(0), station_(0), nStrips_(0), hitWire_(0), wgroupsBX_(0), nWireGroups_(0){}
     
-    CSCJetCandidate(const double phi,const double eta,const double x, const double y,const double z, const double tPeak, const double tWire);
+    CSCJetCandidate(const double phi,const double eta,const float x, const float y,const float z, const float tPeak, const float tWire,
+                    const int quality,const int chamber,const int station,const int nStrips,const int hitWire,const int wgroupsBX,const int nWireGroups
+                );
 
     //destructor
     ~CSCJetCandidate() override;
 
-    double xPos() const {return x_;}
-    double yPos() const {return y_;}
-    double zPos() const {return z_;}
-    double tPeak() const {return tPeak_;}
-    double tWire() const {return tWire_;}
+    float xPos() const {return x_;}
+    float yPos() const {return y_;}
+    float zPos() const {return z_;}
+    float tPeak() const {return tPeak_;}
+    float tWire() const {return tWire_;}
+    int quality() const {return quality_;}
+    int chamber() const {return chamber_;}
+    int station() const {return station_;}
+    int nStrips() const {return nStrips_;}
+    int hitWire() const {return hitWire_;}
+    int wgroupsBX() const {return wgroupsBX_;}
+    int nWireGroups() const {return nWireGroups_;}
 
    private:
-    double x_;      
-    double y_;
-    double z_;      
-    double tPeak_;      
-    double tWire_;      
+    float x_;      
+    float y_;
+    float z_;      
+    float tPeak_;      
+    float tWire_;    
+    int   quality_; 
+    int   chamber_; 
+    int   station_; 
+    int   nStrips_;
+    int   hitWire_;
+    int   wgroupsBX_; 
+    int   nWireGroups_;
+ 
   };
 
    typedef std::vector<CSCJetCandidate> CSCJetCandidateCollection;
--- DataFormats/CSCRecHit/src/CSCJetCandidate.cc
+++ DataFormats/CSCRecHit/src/CSCJetCandidate.cc
@@ -2,11 +2,12 @@
 
 /*
  * Create LeafCandidate with (eta,phi) only as CSC rechits has no energy/momentum measurement
- * Energy is set to 1.0 as a place holder, mass is set at 0.
+ * Pt is set to 1.0 as a place holder, mass is set at 0.
  * Vertex associated with the CSC rechit is set to the origin.
  *
  */
-reco::CSCJetCandidate::CSCJetCandidate(double phi, double eta, double x,double y, double z,double tPeak, double tWire):
+reco::CSCJetCandidate::CSCJetCandidate(double phi, double eta, float x,float y, float z,float tPeak, float tWire,
+                                       int quality, int chamber, int station, int nStrips, int hitWire, int wgroupsBX, int nWireGroups):
              LeafCandidate(0,LorentzVector(math::PtEtaPhiMLorentzVector(1.0,eta,phi,0)),Point(0,0,0)) {
     
     x_ = x;
@@ -14,6 +15,14 @@ reco::CSCJetCandidate::CSCJetCandidate(double phi, double eta, double x,double y
     z_ = z;
     tPeak_ = tPeak;
     tWire_ = tWire;
+    quality_      =    quality;     
+    chamber_      =    chamber; 
+    station_      =    station; 
+    nStrips_      =    nStrips;
+    hitWire_      =    hitWire;
+    wgroupsBX_    =    wgroupsBX; 
+    nWireGroups_  =    nWireGroups;
+
 }
 
 reco::CSCJetCandidate::~CSCJetCandidate() {
