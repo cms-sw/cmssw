8a4c9c862ac83fc34baf995a3f2d55bd72cc55b5
Marco Link <m.link@cern.ch>
2021-10-07 13:01:41 +0200

    add Tagger Analyzer/Harvester for DQM@MiniAOD

---
+++ DQMOffline/RecoB/plugins/MiniAODTaggerAnalyzer.h
@@ -0,0 +1,51 @@
+#ifndef MiniAODTaggerAnalyzer_H
+#define MiniAODTaggerAnalyzer_H
+
+#include "FWCore/Framework/interface/Frameworkfwd.h"
+#include "DQMServices/Core/interface/DQMEDAnalyzer.h"
+#include "FWCore/Framework/interface/stream/EDProducer.h"
+#include "DataFormats/PatCandidates/interface/Jet.h"
+#include "DQMOffline/RecoB/interface/JetTagPlotter.h"
+
+/** \class MiniAODTaggerAnalyzer
+ *
+ *  Tagger analyzer to run on MiniAOD
+ *
+ */
+
+class MiniAODTaggerAnalyzer : public DQMEDAnalyzer {
+public:
+    explicit MiniAODTaggerAnalyzer(const edm::ParameterSet& pSet);
+    ~MiniAODTaggerAnalyzer() override;
+
+    void analyze(const edm::Event& iEvent, const edm::EventSetup& iSetup) override;
+
+private:
+    void bookHistograms(DQMStore::IBooker&, edm::Run const&, edm::EventSetup const&) override;
+    typedef std::vector<std::string> vstring;
+
+    // using JetTagPlotter object for all the hard work ;)
+    std::unique_ptr<JetTagPlotter> jetTagPlotter_;
+
+
+    edm::EDGetTokenT<std::vector <pat::Jet> > jetToken_;
+    edm::ParameterSet disrParameters_;
+
+    std::string folder_;
+    vstring discrNumerator_;
+    vstring discrDenominator_;
+
+
+    bool doCTagPlots_;
+    bool dodifferentialPlots_;
+    double discrCut_;
+
+    bool etaActive_;
+    double etaMin_;
+    double etaMax_;
+    bool ptActive_;
+    double ptMin_;
+    double ptMax_;
+};
+
+#endif
---
+++ DQMOffline/RecoB/plugins/MiniAODTaggerHarvester.cc
@@ -0,0 +1,48 @@
+#include "DQMOffline/RecoB/plugins/MiniAODTaggerHarvester.h"
+#include "FWCore/Framework/interface/MakerMacros.h"
+#include "FWCore/Framework/interface/Event.h"
+
+
+
+MiniAODTaggerHarvester::MiniAODTaggerHarvester(const edm::ParameterSet& pSet)
+: folder_(pSet.getParameter<std::string>("folder")),
+disrParameters_(pSet.getParameter<edm::ParameterSet>("parameters")),
+
+doCTagPlots_(pSet.getParameter<bool>("CTagPlots")),
+dodifferentialPlots_(pSet.getParameter<bool>("differentialPlots")),
+discrCut_(pSet.getParameter<double>("discrCut")),
+
+etaActive_(pSet.getParameter<bool>("etaActive")),
+etaMin_(pSet.getParameter<double>("etaMin")),
+etaMax_(pSet.getParameter<double>("etaMax")),
+ptActive_(pSet.getParameter<bool>("ptActive")),
+ptMin_(pSet.getParameter<double>("ptMin")),
+ptMax_(pSet.getParameter<double>("ptMax"))
+
+{
+
+}
+
+
+MiniAODTaggerHarvester::~MiniAODTaggerHarvester() { }
+
+
+void MiniAODTaggerHarvester::dqmEndJob(DQMStore::IBooker& ibook, DQMStore::IGetter& iget)
+{
+    jetTagPlotter_ = std::make_unique<JetTagPlotter>(folder_,
+                                                     EtaPtBin(etaActive_, etaMin_, etaMax_, ptActive_, ptMin_, ptMax_),
+                                                     disrParameters_,
+                                                     0, //TODO MC
+                                                     true,
+                                                     ibook,
+                                                     doCTagPlots_,
+                                                     dodifferentialPlots_,
+                                                     discrCut_
+                                                    );
+
+    jetTagPlotter_->finalize(ibook, iget);
+}
+
+
+//define this as a plug-in
+DEFINE_FWK_MODULE(MiniAODTaggerHarvester);
