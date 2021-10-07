8203dd11fdab07e2333014f7a2135678eca4ae27
Christopher Jones <chrisdjones15@gmail.com>
2021-10-07 11:00:36 -0500

    ObjectShallowCloneSelector uses ObjectSelector
    The only user of ObjectShallowCloneSelector can safely be used in a thread-friendly manner.

--- CommonTools/CandAlgos/interface/ObjectShallowCloneSelector.h
+++ CommonTools/CandAlgos/interface/ObjectShallowCloneSelector.h
@@ -5,17 +5,17 @@
  * \author Luca Lista, INFN
  *
  */
-#include "CommonTools/UtilAlgos/interface/ObjectSelectorLegacy.h"
+#include "CommonTools/UtilAlgos/interface/ObjectSelector.h"
 #include "DataFormats/Candidate/interface/ShallowCloneCandidate.h"
 #include "DataFormats/Common/interface/RefVector.h"
 
 template <typename Selector,
           typename SizeSelector = NonNullNumberSelector,
           typename PostProcessor = helper::NullPostProcessor<reco::CandidateCollection> >
-class ObjectShallowCloneSelector : public ObjectSelectorLegacy<Selector, reco::CandidateCollection, SizeSelector> {
+class ObjectShallowCloneSelector : public ObjectSelector<Selector, reco::CandidateCollection, SizeSelector> {
 public:
   explicit ObjectShallowCloneSelector(const edm::ParameterSet& cfg)
-      : ObjectSelectorLegacy<Selector, reco::CandidateCollection, SizeSelector, PostProcessor>(cfg) {}
+      : ObjectSelector<Selector, reco::CandidateCollection, SizeSelector, PostProcessor>(cfg) {}
 };
 
 #endif
