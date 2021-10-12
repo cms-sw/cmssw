650c9710a475839205e920229459d0f0db0e9911
Vincenzo Innocente <vincenzo.innocente@cern.ch>
2021-10-12 12:28:47 +0200

    add cooperative groups

--- HeterogeneousCore/CUDAUtilities/interface/cudaCompat.h
+++ HeterogeneousCore/CUDAUtilities/interface/cudaCompat.h
@@ -21,6 +21,9 @@
 #undef __forceinline__
 #define __forceinline__ inline __attribute__((always_inline))
 
+#undef __launch_bounds__
+#define __launch_bounds__(...)
+
 namespace cms {
   namespace cudacompat {
 
@@ -106,13 +109,124 @@ namespace cms {
     }
 
     inline void __syncthreads() {}
-    inline void __threadfence() {}
     inline bool __syncthreads_or(bool x) { return x; }
     inline bool __syncthreads_and(bool x) { return x; }
+
+    inline void __trap() { abort(); }
+    inline void __threadfence() {}
+
     template <typename T>
     inline T __ldg(T const* x) {
       return *x;
     }
+
+    namespace cooperative_groups {
+
+      // This class represents the thread block
+      class thread_block {
+      private:
+        thread_block() = default;
+
+        friend thread_block this_thread_block();
+
+      public:
+        // Synchronize the threads named in the group.
+        // On the serial CPU implementation, do nothing.
+        static void sync() {}
+
+        // Total number of threads in the group.
+        // On the serial CPU implementation, always 1.
+        static unsigned long long size() { return 1; }
+
+        // Rank of the calling thread within [0, size-1].
+        // On the serial CPU implementation, always 0.
+        static unsigned long long thread_rank() { return 0; }
+
+        // 3-Dimensional index of the block within the launched grid.
+        // On the serial CPU implementation, always {0, 0, 0}.
+        static dim3 group_index() { return blockIdx; }
+
+        // 3-Dimensional index of the thread within the launched block
+        // On the serial CPU implementation, always {0, 0, 0}.
+        static dim3 thread_index() { return threadIdx; }
+
+        // Dimensions of the launched block.
+        // On the serial CPU implementation, always {1, 1, 1}.
+        static dim3 group_dim() { return blockDim; }
+      };
+
+      // Return the current thread block
+      inline thread_block this_thread_block() { return thread_block{}; }
+
+      // Represent a tiled group of threads, with compile-time fixed size.
+      // On the serial CPU implementation, the only valid Size is 1
+      template <unsigned int Size, typename ParentT>
+      class thread_block_tile {
+      private:
+        static_assert(
+            Size == 1,
+            "The cudaCompat Cooperative Groups implementation supports only tiled groups of a single thread.");
+
+        thread_block_tile() = default;
+
+        friend thread_block_tile<Size, ParentT> tiled_partition(const ParentT& g);
+
+      public:
+        // Synchronize the threads named in the group.
+        // On the serial CPU implementation, do nothing.
+        void sync() const {}
+
+        // Total number of threads in the group.
+        // On the serial CPU implementation, always 1.
+        unsigned long long size() const { return 1; }
+
+        // Rank of the calling thread within [0, size-1].
+        // On the serial CPU implementation, always 0.
+        unsigned long long thread_rank() const { return 0; }
+
+        // Returns the number of groups created when the parent group was partitioned.
+        // On the serial CPU implementation, always 1.
+        unsigned long long meta_group_size() const { return 1; }
+
+        // Linear rank of the group within the set of tiles partitioned from a parent group (bounded by meta_group_size).
+        // On the serial CPU implementation, always 0.
+        unsigned long long meta_group_rank() const { return 0; }
+
+        // Not implemented - Refer to Warp Shuffle Functions
+        template <typename T>
+        T shfl(T var, unsigned int src_rank) const;
+        template <typename T>
+        T shfl_up(T var, int delta) const;
+        template <typename T>
+        T shfl_down(T var, int delta) const;
+        template <typename T>
+        T shfl_xor(T var, int delta) const;
+
+        // Not implemented - Refer to Warp Vote Functions
+        template <typename T>
+        T any(int predicate) const;
+        template <typename T>
+        T all(int predicate) const;
+        template <typename T>
+        T ballot(int predicate) const;
+
+        // Not implemented - Refer to Warp Match Functions
+        template <typename T>
+        T match_any(T val) const;
+        template <typename T>
+        T match_all(T val, int& pred) const;
+      };
+
+      template <unsigned int Size, typename ParentT>
+      inline thread_block_tile<Size, ParentT> tiled_partition(const ParentT& g) {
+        static_assert(
+            Size == 1,
+            "The cudaCompat Cooperative Groups implementation supports only tiled groups of a single thread.");
+
+        return thread_block_tile<Size, ParentT>{};
+      }
+
+    }  // namespace cooperative_groups
 
   }  // namespace cudacompat
 }  // namespace cms
