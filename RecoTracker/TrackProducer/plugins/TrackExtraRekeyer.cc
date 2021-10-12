03101ed4f1a43be3798376a4cb45e653ca419a5d
mmusich <marco.musich@cern.ch>
2021-10-12 11:22:21 +0200

    modernize TrackExtraRekeyer

--- RecoTracker/TrackProducer/plugins/TrackExtraRekeyer.cc
+++ RecoTracker/TrackProducer/plugins/TrackExtraRekeyer.cc
@@ -1,59 +1,61 @@
-#include "FWCore/Framework/interface/stream/EDProducer.h"
+#include "FWCore/Framework/interface/Event.h"
+#include "FWCore/Framework/interface/global/EDProducer.h"
 #include "FWCore/Framework/interface/MakerMacros.h"
 
 #include "DataFormats/TrackReco/interface/Track.h"
 #include "DataFormats/TrackReco/interface/TrackFwd.h"
 #include "DataFormats/TrackReco/interface/TrackExtraFwd.h"
 
-class TrackExtraRekeyer : public edm::stream::EDProducer<> {
+class TrackExtraRekeyer : public edm::global::EDProducer<> {
 public:
   explicit TrackExtraRekeyer(const edm::ParameterSet &);
-  ~TrackExtraRekeyer() override {}
+  ~TrackExtraRekeyer() override = default;
 
-  //   static void fillDescriptions(edm::ConfigurationDescriptions &descriptions);
+  static void fillDescriptions(edm::ConfigurationDescriptions &descriptions);
 
 private:
-  void produce(edm::Event &, const edm::EventSetup &) override;
+  void produce(edm::StreamID, edm::Event &, const edm::EventSetup &) const override;
 
-  edm::EDGetTokenT<reco::TrackCollection> inputTrack_;
-  edm::EDGetTokenT<edm::Association<reco::TrackExtraCollection>> inputAssoc_;
-  edm::EDPutTokenT<reco::TrackCollection> outputTrack_;
+  // memeber data
+  const edm::EDGetTokenT<reco::TrackCollection> inputTrack_;
+  const edm::EDGetTokenT<edm::Association<reco::TrackExtraCollection>> inputAssoc_;
+  const edm::EDPutTokenT<reco::TrackCollection> outputTrack_;
 };
 
 TrackExtraRekeyer::TrackExtraRekeyer(const edm::ParameterSet &iConfig)
-
-{
-  inputTrack_ = consumes<reco::TrackCollection>(iConfig.getParameter<edm::InputTag>("src"));
-  inputAssoc_ =
-      consumes<edm::Association<reco::TrackExtraCollection>>(iConfig.getParameter<edm::InputTag>("association"));
-
-  outputTrack_ = produces<reco::TrackCollection>();
-}
+    : inputTrack_(consumes(iConfig.getParameter<edm::InputTag>("src"))),
+      inputAssoc_(consumes(iConfig.getParameter<edm::InputTag>("association"))),
+      outputTrack_(produces<reco::TrackCollection>()) {}
 
 // ------------ method called for each event  ------------
-void TrackExtraRekeyer::produce(edm::Event &iEvent, const edm::EventSetup &iSetup) {
+void TrackExtraRekeyer::produce(edm::StreamID, edm::Event &iEvent, const edm::EventSetup &iSetup) const {
   using namespace edm;
 
-  Handle<reco::TrackCollection> tracks;
-  iEvent.getByToken(inputTrack_, tracks);
-
-  Handle<edm::Association<reco::TrackExtraCollection>> assoc;
-  iEvent.getByToken(inputAssoc_, assoc);
+  auto const &tracks = iEvent.get(inputTrack_);
+  auto const &assoc = iEvent.get(inputAssoc_);
 
   reco::TrackCollection tracksOut;
 
-  for (auto const &track : *tracks) {
-    if (!assoc->contains(track.extra().id())) {
+  for (auto const &track : tracks) {
+    if (!assoc.contains(track.extra().id())) {
       continue;
     }
-    const reco::TrackExtraRef &trackextraref = (*assoc)[track.extra()];
+    const reco::TrackExtraRef &trackextraref = assoc[track.extra()];
     if (trackextraref.isNonnull()) {
       auto &trackout = tracksOut.emplace_back(track);
       trackout.setExtra(trackextraref);
     }
   }
-
   iEvent.emplace(outputTrack_, std::move(tracksOut));
+}
+
+void TrackExtraRekeyer::fillDescriptions(edm::ConfigurationDescriptions &descriptions) {
+  edm::ParameterSetDescription desc;
+  desc.setComment("Simple prooducer to re-key muon tracks for refit");
+  desc.add<edm::InputTag>("src", edm::InputTag("generalTracks"))->setComment("input track collections");
+  desc.add<edm::InputTag>("association", edm::InputTag("muonReducedTrackExtras"))
+      ->setComment("input track association collection");
+  descriptions.addWithDefaultLabel(desc);
 }
 
 DEFINE_FWK_MODULE(TrackExtraRekeyer);
