d62d2d9989f19c1939d7ba137e12f35faced3a54
Tamas Vami <tamas.almos.vami@cern.ch>
2021-10-12 22:24:21 -0400

    Include realistic CSCBadChambers in Run-3 MC GTs

--- Configuration/AlCa/python/autoCond.py
+++ Configuration/AlCa/python/autoCond.py
@@ -68,15 +68,15 @@ autoCond = {
     # GlobalTag for MC production with perfectly aligned and calibrated detector for Phase1 2021
     'phase1_2021_design'           : '121X_mcRun3_2021_design_v10',
     # GlobalTag for MC production with realistic conditions for Phase1 2021
-    'phase1_2021_realistic'        : '121X_mcRun3_2021_realistic_v11',
+    'phase1_2021_realistic'        : '121X_mcRun3_2021_realistic_v12',
     # GlobalTag for MC production (cosmics) with realistic conditions for Phase1 2021,  Strip tracker in DECO mode
-    'phase1_2021_cosmics'          : '121X_mcRun3_2021cosmics_realistic_deco_v11',
+    'phase1_2021_cosmics'          : '121X_mcRun3_2021cosmics_realistic_deco_v12',
     # GlobalTag for MC production with realistic conditions for Phase1 2021 detector for Heavy Ion
-    'phase1_2021_realistic_hi'     : '121X_mcRun3_2021_realistic_HI_v11',
+    'phase1_2021_realistic_hi'     : '121X_mcRun3_2021_realistic_HI_v12',
     # GlobalTag for MC production with realistic conditions for Phase1 2023
-    'phase1_2023_realistic'        : '121X_mcRun3_2023_realistic_v10',
+    'phase1_2023_realistic'        : '121X_mcRun3_2023_realistic_v11',
     # GlobalTag for MC production with realistic conditions for Phase1 2024
-    'phase1_2024_realistic'        : '121X_mcRun3_2024_realistic_v10',
+    'phase1_2024_realistic'        : '121X_mcRun3_2024_realistic_v11',
     # GlobalTag for MC production with realistic conditions for Phase2
     'phase2_realistic'             : '121X_mcRun4_realistic_v6'
 }
