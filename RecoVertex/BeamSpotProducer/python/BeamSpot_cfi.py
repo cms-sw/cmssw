7527bdc30f6206d6267703f5004a3452bb65e5d7
mmusich <marco.musich@cern.ch>
2021-10-13 10:43:37 +0200

    add onlineBeamSpotESProducer to BeamSpot_cfi: fix general Online BS swap case

--- RecoVertex/BeamSpotProducer/python/BeamSpot_cfi.py
+++ RecoVertex/BeamSpotProducer/python/BeamSpot_cfi.py
@@ -4,6 +4,11 @@ import RecoVertex.BeamSpotProducer.Modifiers as mods
 
 offlineBeamSpot = cms.EDProducer("BeamSpotProducer")
 
+import RecoVertex.BeamSpotProducer.onlineBeamSpotESProducer_cfi as _mod
+BeamSpotESProducer = _mod.onlineBeamSpotESProducer.clone(
+    timeThreshold = 999999 # for express allow >48h old payloads for replays. DO NOT CHANGE
+)
+
 import RecoVertex.BeamSpotProducer.BeamSpotOnline_cfi
 _onlineBeamSpotProducer = RecoVertex.BeamSpotProducer.BeamSpotOnline_cfi.onlineBeamSpotProducer.clone()
 mods.offlineToOnlineBeamSpotSwap.toReplaceWith(offlineBeamSpot, _onlineBeamSpotProducer)
