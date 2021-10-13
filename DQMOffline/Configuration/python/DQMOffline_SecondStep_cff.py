a69111847f8c6c9e28739bc244d778430706a573
Marco Link <m.link@cern.ch>
2021-10-13 13:21:07 +0200

    renaming BTV DQM@MiniAOD sequences

--- DQMOffline/Configuration/python/DQMOffline_SecondStep_cff.py
+++ DQMOffline/Configuration/python/DQMOffline_SecondStep_cff.py
@@ -220,8 +220,8 @@ DQMHarvestBTag = cms.Sequence( bTagCollectorSequenceDATA )
 
 from PhysicsTools.NanoAOD.nanoDQM_cff import *
 from Validation.RecoParticleFlow.DQMForPF_MiniAOD_cff import *
-from DQMOffline.RecoB.dqmMiniAnalyzer_cff import *
+from DQMOffline.RecoB.bTagMiniDQM_cff import *
 
-DQMHarvestMiniAOD = cms.Sequence( dataCertificationJetMETSequence * muonQualityTests_miniAOD * DQMHarvestPF * bTagHarvester_mini)
+DQMHarvestMiniAOD = cms.Sequence( dataCertificationJetMETSequence * muonQualityTests_miniAOD * DQMHarvestPF * bTagMiniDQMHarvesting)
 DQMHarvestNanoAOD = cms.Sequence( nanoHarvest )
 
