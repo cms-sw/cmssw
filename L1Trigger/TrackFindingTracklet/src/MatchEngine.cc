972011bc5439e5db05f1bfcd1b6e330aeb64fde1
Anders Ryd <aryd@cern.ch>
2021-10-15 13:03:39 -0400

    Determine the PS vs 2S in disks same way as we do in HLS

--- L1Trigger/TrackFindingTracklet/src/MatchEngine.cc
+++ L1Trigger/TrackFindingTracklet/src/MatchEngine.cc
@@ -222,9 +222,21 @@ void MatchEngine::execute(unsigned int iSector) {
       //Read vmstub memory and extract data fields
       const VMStubME& vmstub = vmstubs_->getVMStubMEBin(rzbin, istubtmp);
 
-      bool isPSmodule = vmstub.isPSmodule();
-
       int stubfinerz = vmstub.finerz().value();
+
+      bool isPSmodule=false;
+
+      if (barrel_) {
+	isPSmodule = layerdisk_ < N_PSLAYER;
+      } else {
+	if (layerdisk_<N_LAYER+2) {
+	  isPSmodule = ((rzbin&7)<3) || ((rzbin&7)==3 && stubfinerz<=3);
+	} else {
+	  isPSmodule = ((rzbin&7)<3) || ((rzbin&7)==3 && stubfinerz<=2);
+	}
+      }
+      
+      assert(isPSmodule == vmstub.isPSmodule());
 
       int nbits = isPSmodule ? N_BENDBITS_PS : N_BENDBITS_2S;
 
@@ -233,7 +245,7 @@ void MatchEngine::execute(unsigned int iSector) {
       constexpr int mindeltaphicut = 3;
       constexpr int maxdeltaphicut = 5;
       bool passphi = (std::abs(deltaphi) < mindeltaphicut) || (std::abs(deltaphi) > maxdeltaphicut);
-
+      
       unsigned int index = (projrinv << nbits) + vmstub.bend().value();
       if (!barrel_ && isPSmodule) {
         index += (1 << (nrinv_ + N_BENDBITS_2S));
