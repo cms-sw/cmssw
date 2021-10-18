57c6fdf3e84116e1121012f90dd6532834bd348a
mmusich <marco.musich@cern.ch>
2021-10-18 16:52:18 +0200

    add DropBoxMetadata_Display

--- CondCore/PhysicsToolsPlugins/plugins/DropBoxMetadata_PayloadInspector.cc
+++ CondCore/PhysicsToolsPlugins/plugins/DropBoxMetadata_PayloadInspector.cc
@@ -18,6 +18,7 @@
 #include <memory>
 #include <sstream>
 #include <iostream>
+#include <boost/algorithm/string/replace.hpp>
 
 // include ROOT
 #include "TProfile.h"
@@ -50,19 +51,80 @@ namespace {
         if (payload.get()) {
           std::vector<std::string> records = payload->getAllRecords();
           for (const auto& record : records) {
-            std::cout << "record: " << record << std::endl;
+            edm::LogPrint("DropBoxMetadata_PayloadInspector") << "record: " << record << std::endl;
             const auto& parameters = payload->getRecordParameters(record);
             const auto& recordParams = parameters.getParameterMap();
             for (const auto& [key, val] : recordParams) {
-              std::cout << key << " : "
-                        << " value: " << val << std::endl;
+              if (val.find("&quot;") != std::string::npos) {
+                const auto& replaced = replaceAll(val, std::string("&quot;"), std::string("'"));
+                edm::LogPrint("DropBoxMetadata_PayloadInspector") << key << " : " << replaced << std::endl;
+              } else {
+                edm::LogPrint("DropBoxMetadata_PayloadInspector") << key << " : " << val << std::endl;
+              }
             }
           }
         }
       }
       return true;
     }
+
+  private:
+    std::string replaceAll(std::string str, const std::string& from, const std::string& to) {
+      size_t start_pos = 0;
+      while ((start_pos = str.find(from, start_pos)) != std::string::npos) {
+        str.replace(start_pos, from.length(), to);
+        start_pos += to.length();  // Handles case where 'to' is a substring of 'from'
+      }
+      return str;
+    }
+  };
+
+  /************************************************
+     DropBoxMetadata Payload Inspector of 1 IOV 
+  *************************************************/
+  class DropBoxMetadata_Display : public PlotImage<DropBoxMetadata, SINGLE_IOV> {
+  public:
+    DropBoxMetadata_Display() : PlotImage<DropBoxMetadata, SINGLE_IOV>("DropBoxMetadata Display of contents") {}
+
+    bool fill() override {
+      auto tag = PlotBase::getTag<0>();
+      auto iov = tag.iovs.front();
+      std::shared_ptr<DropBoxMetadata> payload = fetchPayload(std::get<1>(iov));
+
+      std::vector<std::string> records = payload->getAllRecords();
+      TCanvas canvas("Canv", "Canv", 1200, 100 * records.size());
+
+      for (const auto& record : records) {
+        edm::LogPrint("DropBoxMetadata_PayloadInspector") << "record: " << record << std::endl;
+        const auto& parameters = payload->getRecordParameters(record);
+        const auto& recordParams = parameters.getParameterMap();
+        for (const auto& [key, val] : recordParams) {
+          if (val.find("&quot;") != std::string::npos) {
+            const auto& replaced = replaceAll(val, std::string("&quot;"), std::string("'"));
+            edm::LogPrint("DropBoxMetadata_PayloadInspector") << key << " : " << replaced << std::endl;
+          } else {
+            edm::LogPrint("DropBoxMetadata_PayloadInspector") << key << " : " << val << std::endl;
+          }
+        }
+      }
+      std::string fileName(m_imageFileName);
+      canvas.SaveAs(fileName.c_str());
+      return true;
+    }
+
+  private:
+    std::string replaceAll(std::string str, const std::string& from, const std::string& to) {
+      size_t start_pos = 0;
+      while ((start_pos = str.find(from, start_pos)) != std::string::npos) {
+        str.replace(start_pos, from.length(), to);
+        start_pos += to.length();  // Handles case where 'to' is a substring of 'from'
+      }
+      return str;
+    }
   };
 }  // namespace
 
-PAYLOAD_INSPECTOR_MODULE(DropBoxMetadata) { PAYLOAD_INSPECTOR_CLASS(DropBoxMetadataTest); }
+PAYLOAD_INSPECTOR_MODULE(DropBoxMetadata) {
+  PAYLOAD_INSPECTOR_CLASS(DropBoxMetadataTest);
+  PAYLOAD_INSPECTOR_CLASS(DropBoxMetadata_Display);
+}
