de4bbbcbfc8a4fc3a0cac46603dc1aa9a02581c8
Sunanda <sunanda.banerjee@cern.ch>
2021-10-19 20:01:49 +0200

    Use SimProducer for TB material budget studies

---
+++ SimG4CMS/HGCalTestBeam/python/hgcalTBMBAnalyzerCERNBeam_cfi.py
@@ -0,0 +1,16 @@
+import FWCore.ParameterSet.Config as cms
+from SimG4CMS.HGCalTestBeam.hgcalTBMBAnalyzer_cfi import *
+
+hgcalTBMBAnalyzerCERNBeam = hgcalTBMBAnalyzer.clone(
+    detectorNames = cms.vstring(
+        'HGCalBeamWChamb',  
+        'HGCalBeamS1',
+        'HGCalBeamS2',
+        'HGCalBeamS3',
+        'HGCalBeamS4',
+        'HGCalBeamS5',
+        'HGCalBeamS6',
+        'HGCalBeamCK3',
+        'HGCalBeamHaloCounter',
+        'HGCalBeamMuonCounter'
+    ))
