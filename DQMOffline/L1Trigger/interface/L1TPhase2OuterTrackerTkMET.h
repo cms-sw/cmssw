c243b201da1c36c06c027189fe6ef437487f31cd
Dylan <drankin@bu.edu>
2021-10-20 08:07:41 -0500

    code checks fixes

--- DQMOffline/L1Trigger/interface/L1TPhase2OuterTrackerTkMET.h
+++ DQMOffline/L1Trigger/interface/L1TPhase2OuterTrackerTkMET.h
@@ -24,38 +24,37 @@ public:
   explicit L1TPhase2OuterTrackerTkMET(const edm::ParameterSet&);
   ~L1TPhase2OuterTrackerTkMET() override;
   void analyze(const edm::Event&, const edm::EventSetup&) override;
-  void bookHistograms(DQMStore::IBooker &, edm::Run const &, edm::EventSetup const &) override;
+  void bookHistograms(DQMStore::IBooker&, edm::Run const&, edm::EventSetup const&) override;
 
-  MonitorElement* Track_N = nullptr; // Number of tracks per event
-  MonitorElement* Track_Pt = nullptr; // pT distrubtion for tracks
-  MonitorElement* Track_Eta = nullptr; // eta distrubtion for tracks
-  MonitorElement* Track_VtxZ = nullptr; // z0 distrubtion for tracks
-  MonitorElement* Track_Chi2Dof = nullptr;  // chi2 distrubtion for tracks
+  MonitorElement* Track_N = nullptr;         // Number of tracks per event
+  MonitorElement* Track_Pt = nullptr;        // pT distrubtion for tracks
+  MonitorElement* Track_Eta = nullptr;       // eta distrubtion for tracks
+  MonitorElement* Track_VtxZ = nullptr;      // z0 distrubtion for tracks
+  MonitorElement* Track_Chi2Dof = nullptr;   // chi2 distrubtion for tracks
   MonitorElement* Track_BendChi2 = nullptr;  // bend chi2 distrubtion for tracks
-  MonitorElement* Track_NStubs = nullptr;  // nstubs distrubtion for tracks
+  MonitorElement* Track_NStubs = nullptr;    // nstubs distrubtion for tracks
   MonitorElement* Track_NPSstubs = nullptr;  // nPS stubs distrubtion for tracks
 
-  MonitorElement* TkMET_QualityCuts = nullptr; //Matches the quality cuts in the producer
+  MonitorElement* TkMET_QualityCuts = nullptr;  //Matches the quality cuts in the producer
   MonitorElement* TkMET_PU = nullptr;
 
- private:
+private:
   edm::ParameterSet conf_;
   // edm::EDGetTokenT< l1t::VertexCollection > pvToken;
-  edm::EDGetTokenT< l1t::TkPrimaryVertexCollection > pvToken;
-  edm::EDGetTokenT<std::vector< TTTrack< Ref_Phase2TrackerDigi_ > > >  ttTrackToken_;
+  edm::EDGetTokenT<l1t::TkPrimaryVertexCollection> pvToken;
+  edm::EDGetTokenT<std::vector<TTTrack<Ref_Phase2TrackerDigi_> > > ttTrackToken_;
 
-  float maxZ0;	    // in cm
-  float DeltaZ;	    // in cm
+  float maxZ0;   // in cm
+  float DeltaZ;  // in cm
   float maxEta;
   float chi2dofMax;
   float bendchi2Max;
-  float minPt;	    // in GeV
+  float minPt;  // in GeV
   int nStubsmin;
-  int nStubsPSmin;  // minimum number of stubs in PS modules
-  float maxPt;	    // in GeV
-  int HighPtTracks; // saturate or truncate
+  int nStubsPSmin;   // minimum number of stubs in PS modules
+  float maxPt;       // in GeV
+  int HighPtTracks;  // saturate or truncate
 
   std::string topFolderName_;
 };
 #endif
-
