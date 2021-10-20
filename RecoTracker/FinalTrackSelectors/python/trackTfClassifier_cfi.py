cce8ca6b8382a278fb2c4e145dc0c2be9ce1d900
minxi <minxi@lxplus.cern.ch>
2021-10-20 19:21:26 +0200

    fix the name of the _mod

--- RecoTracker/FinalTrackSelectors/python/trackTfClassifier_cfi.py
+++ RecoTracker/FinalTrackSelectors/python/trackTfClassifier_cfi.py
@@ -1,5 +1,5 @@
 from Configuration.ProcessModifiers.trackdnn_CKF_cff import trackdnn_CKF
-import RecoTracker.FinalTrackSelectors.TrackTfClassifier_cfi as _mod
+import RecoTracker.FinalTrackSelectors.trackTfClassifierDefault_cfi as _mod
 
-trackTfClassifier = _mod.TrackTfClassifier.clone()
+trackTfClassifier = _mod.trackTfClassifierDefault.clone()
 trackdnn_CKF.toModify(trackTfClassifier.mva, tfDnnLabel = 'trackSelectionTf_CKF')
