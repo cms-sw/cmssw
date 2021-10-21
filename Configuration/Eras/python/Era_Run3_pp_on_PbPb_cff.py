ee617ef46b43f748c4cd92b3137012944f14ca44
minxi <minxi@lxplus.cern.ch>
2021-10-21 22:40:58 +0200

    solve conflict

--- Configuration/Eras/python/Era_Run3_pp_on_PbPb_cff.py
+++ Configuration/Eras/python/Era_Run3_pp_on_PbPb_cff.py
@@ -6,8 +6,5 @@ from Configuration.ProcessModifiers.pp_on_AA_cff import pp_on_AA
 from Configuration.Eras.Modifier_pp_on_PbPb_run3_cff import pp_on_PbPb_run3
 from Configuration.ProcessModifiers.trackingNoLoopers_cff import trackingNoLoopers
 
-<<<<<<< HEAD
-Run3_pp_on_PbPb = cms.ModifierChain(Run3_noMkFit.copyAndExclude([trackingNoLoopers]), pp_on_AA, pp_on_PbPb_run3)
-=======
-Run3_pp_on_PbPb = cms.ModifierChain(Run3_noMkFit.copyAndExclude([trackdnn, trackdnn_CKF]), pp_on_AA, pp_on_PbPb_run3)
->>>>>>> remove dnn in the heavy ion track selection
+Run3_pp_on_PbPb = cms.ModifierChain(Run3_noMkFit.copyAndExclude([trackdnn, trackdnn_CKF, trackingNoLoopers]), pp_on_AA, pp_on_PbPb_run3)
+
