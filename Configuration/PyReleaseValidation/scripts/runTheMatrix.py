ee40013f4426454ede4d61aa44feb27899a7c638
Norraphat <srimanob@mail.cern.ch>
2021-10-26 01:10:01 +0200

    update GT

--- Configuration/PyReleaseValidation/scripts/runTheMatrix.py
+++ Configuration/PyReleaseValidation/scripts/runTheMatrix.py
@@ -97,6 +97,7 @@ if __name__ == '__main__':
                      28234.0, #2026D60 (exercise HF nose)
                      34634.0, #2026D76 ttbar (2021 new baseline)
                      34834.999, #2026D76 ttbar premixing stage1+stage2, PU50
+                     38634.0, #2026D86 ttbar
                      25202.0, #2016 ttbar UP15 PU
                      250202.181, #2018 ttbar stage1 + stage2 premix
                      ],
