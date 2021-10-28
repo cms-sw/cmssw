28c4eb846efe22f2ff627173762d5f3f7024d23d
Fabio Cossutti <fabio.cossutti@ts.infn.it>
2021-10-28 11:10:50 +0200

    Add harvesting of 4D vertex analysis efficiencies

--- Validation/MtdValidation/python/MtdPostProcessor_cff.py
+++ Validation/MtdValidation/python/MtdPostProcessor_cff.py
@@ -2,5 +2,6 @@ import FWCore.ParameterSet.Config as cms
 
 from Validation.MtdValidation.btlSimHitsPostProcessor_cfi import btlSimHitsPostProcessor
 from Validation.MtdValidation.MtdTracksPostProcessor_cfi import MtdTracksPostProcessor
+from Validation.MtdValidation.Primary4DVertexPostProcessor_cfi import Primary4DVertexPostProcessor
 
-mtdValidationPostProcessor = cms.Sequence(btlSimHitsPostProcessor + MtdTracksPostProcessor)
+mtdValidationPostProcessor = cms.Sequence(btlSimHitsPostProcessor + MtdTracksPostProcessor + Primary4DVertexPostProcessor)
