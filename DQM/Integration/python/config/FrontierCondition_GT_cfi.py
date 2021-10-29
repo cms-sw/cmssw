58bd87e6099e2ef42449218cb5d73ef2af5c05f3
mmusich <marco.musich@cern.ch>
2021-10-29 11:11:32 +0200

    customize OnlineBeamSpot ESProducer inputs to be refreshed at each LS

--- DQM/Integration/python/config/FrontierCondition_GT_cfi.py
+++ DQM/Integration/python/config/FrontierCondition_GT_cfi.py
@@ -2,3 +2,21 @@ import FWCore.ParameterSet.Config as cms
 from Configuration.StandardSequences.FrontierConditions_GlobalTag_cff import *
 from Configuration.AlCa.autoCond import autoCond
 GlobalTag.globaltag = autoCond['run3_hlt']
+
+#############################################
+#
+#              DO NOT REMOVE
+#
+# This GlobalTag customization is necessary to 
+# refresh the online BeamSpot ESProducer inputs
+# used by the online DQM clients at every LS
+# (as it done in the HLT menu).
+##############################################
+GlobalTag.toGet = cms.VPSet(
+    cms.PSet( record = cms.string( "BeamSpotOnlineLegacyObjectsRcd" ),
+              refreshTime = cms.uint64( 1 ),
+            ),
+    cms.PSet( record = cms.string( "BeamSpotOnlineHLTObjectsRcd" ),
+              refreshTime = cms.uint64( 1 )
+            )
+)
