07240c722fcb8eb76c2c6d0abe98846eb535e38b
Mauro <mauro.dinardo@gmail.com>
2021-10-29 15:03:07 +0200

    Added modification to cfg file for BeamPixel monitor

--- DQM/Integration/python/clients/beampixel_dqm_sourceclient-live_cfg.py
+++ DQM/Integration/python/clients/beampixel_dqm_sourceclient-live_cfg.py
@@ -2,8 +2,8 @@ from __future__ import print_function
 import FWCore.ParameterSet.Config as cms
 
 import sys
-from Configuration.Eras.Era_Run2_2018_cff import Run2_2018
-process = cms.Process("BeamPixel", Run2_2018)
+from Configuration.Eras.Era_Run3_cff import Run3
+process = cms.Process("BeamMonitor", Run3)
 
 unitTest = False
 if 'unitTest=True' in sys.argv:
