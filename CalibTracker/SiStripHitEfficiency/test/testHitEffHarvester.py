681813a7ed3606184edc322154150b1870f5630a
Pieter David <pieter.david@cern.ch>
2021-11-01 15:56:01 +0100

    Some fixes and updates

---
+++ CalibTracker/SiStripHitEfficiency/test/testHitEffHarvester.py
@@ -0,0 +1,67 @@
+import FWCore.ParameterSet.Config as cms
+
+process = cms.Process("HitEffHarvest")
+process.load("Configuration/StandardSequences/MagneticField_cff")
+process.load("Configuration.StandardSequences.GeometryRecoDB_cff")
+process.load('Configuration.StandardSequences.FrontierConditions_GlobalTag_cff')
+process.load('Configuration.StandardSequences.DQMSaverAtRunEnd_cff')
+process.load('FWCore.MessageService.MessageLogger_cfi')
+
+from Configuration.AlCa.GlobalTag import GlobalTag
+process.GlobalTag = GlobalTag(process.GlobalTag, 'auto:run2_data', '')  
+
+process.source = cms.Source("DQMRootSource",
+    fileNames = cms.untracked.vstring('file:DQM.root')
+)
+
+runNumber = 325172
+
+process.maxEvents = cms.untracked.PSet(input = cms.untracked.int32(10000))
+
+process.hiteffharvest = cms.EDProducer("SiStripHitEfficiencyHarvester",
+    Threshold           = cms.double(0.1),
+    nModsMin            = cms.int32(5),
+    AutoIneffModTagging = cms.untracked.bool(True),  # default true, automatic limit for each layer to identify inefficient modules
+    Record              = cms.string('SiStripBadStrip'),
+    doStoreOnDB         = cms.bool(True),
+    ShowRings           = cms.untracked.bool(False),  # default False
+    TkMapMin            = cms.untracked.double(0.90), # default 0.90
+    EffPlotMin          = cms.untracked.double(0.90), # default 0.90
+    Title               = cms.string(' Hit Efficiency - run {0:d}'.format(runNumber))
+    )
+
+process.load("DQM.SiStripCommon.TkHistoMap_cff")
+
+process.allPath = cms.Path(process.hiteffharvest*process.DQMSaver)
+
+process.MessageLogger = cms.Service(
+    "MessageLogger",
+    destinations = cms.untracked.vstring(
+        "log_hiteffharvest"
+        ),
+    log_hiteffharvest = cms.untracked.PSet(
+        threshold = cms.untracked.string("DEBUG"),
+        default = cms.untracked.PSet(
+        limit = cms.untracked.int32(-1)
+        )
+    ),
+    debugModules = cms.untracked.vstring("hiteffharvest"),
+    categories=cms.untracked.vstring("TkHistoMap", "SiStripHitEfficiency:HitEff", "SiStripHitEfficiency")
+)
+
+process.TFileService = cms.Service("TFileService",
+        fileName = cms.string('SiStripHitEffHistos_run{0:d}_NEW.root'.format(runNumber))
+)
+
+process.PoolDBOutputService = cms.Service("PoolDBOutputService",
+    BlobStreamerName = cms.untracked.string('TBufferBlobStreamingService'),
+    DBParameters = cms.PSet(
+        authenticationPath = cms.untracked.string('/afs/cern.ch/cms/DB/conddb')
+    ),
+    timetype = cms.untracked.string('runnumber'),
+    connect = cms.string('sqlite_file:dbfile_NEW.db'),
+    toPut = cms.VPSet(cms.PSet(
+        record = cms.string('SiStripBadStrip'),
+        tag = cms.string('SiStripHitEffBadModules')
+    ))
+)
