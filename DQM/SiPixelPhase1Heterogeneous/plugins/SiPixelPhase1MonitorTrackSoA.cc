fb564bafdf09045a0c824909654ddf8bed1ed523
Suvankar Roy Chowdhury <suvankar.roy.chowdhury@cern.ch>
2021-11-03 10:15:32 +0100

    add to the pixeOnly dqm and harvesting step
    address review comments
    
    add plot for ntracks associated
    
    simplify ntracks

--- DQM/SiPixelPhase1Heterogeneous/plugins/SiPixelPhase1MonitorTrackSoA.cc
+++ DQM/SiPixelPhase1Heterogeneous/plugins/SiPixelPhase1MonitorTrackSoA.cc
@@ -34,6 +34,8 @@ public:
 private:
   edm::EDGetTokenT<PixelTrackHeterogeneous> tokenSoATrack_;
   std::string topFolderName_;
+  bool useQualityCut_;
+  pixelTrack::Quality minQuality_;
   MonitorElement* hnTracks;
   MonitorElement* hnHits;
   MonitorElement* hchi2;
@@ -41,6 +43,7 @@ private:
   MonitorElement* heta;
   MonitorElement* hphi;
   MonitorElement* hz;
+  MonitorElement* htip;
 };
 
 //
@@ -50,12 +53,13 @@ private:
 SiPixelPhase1MonitorTrackSoA::SiPixelPhase1MonitorTrackSoA(const edm::ParameterSet& iConfig) {
   tokenSoATrack_ = consumes<PixelTrackHeterogeneous>(iConfig.getParameter<edm::InputTag>("pixelTrackSrc"));
   topFolderName_ = iConfig.getParameter<std::string>("TopFolderName");  //"SiPixelHeterogeneous/PixelTrackSoA";
+  useQualityCut_ = iConfig.getParameter<bool>("useQualityCut");
+  minQuality_ = pixelTrack::qualityByName(iConfig.getParameter<std::string>("minQuality"));
 }
 
 SiPixelPhase1MonitorTrackSoA::~SiPixelPhase1MonitorTrackSoA() {
   // do anything here that needs to be done at desctruction time
   // (e.g. close files, deallocate resources etc.)
-  edm::LogInfo("SiPixelPhase1MonitorTrackSoA") << ">>> Destroy SiPixelPhase1MonitorTrackSoA ";
 }
 
 // -- Analyze
@@ -63,7 +67,7 @@ SiPixelPhase1MonitorTrackSoA::~SiPixelPhase1MonitorTrackSoA() {
 void SiPixelPhase1MonitorTrackSoA::analyze(const edm::Event& iEvent, const edm::EventSetup& iSetup) {
   auto const& tsoa = *iEvent.get(tokenSoATrack_);
   auto maxTracks = tsoa.stride();
-
+  auto const* quality = tsoa.qualityData();
   int32_t nTracks = 0;
   for (int32_t it = 0; it < maxTracks; ++it) {
     auto nHits = tsoa.nHits(it);
@@ -72,16 +76,20 @@ void SiPixelPhase1MonitorTrackSoA::analyze(const edm::Event& iEvent, const edm::
     float pt = tsoa.pt(it);
     if (!(pt > 0.))
       continue;
+    if (useQualityCut_ && quality[it] < minQuality_)
+      continue;
     float chi2 = tsoa.chi2(it);
     float phi = tsoa.phi(it);
     float zip = tsoa.zip(it);
     float eta = tsoa.eta(it);
+    float tip = tsoa.tip(it);
     hchi2->Fill(chi2);
     hnHits->Fill(nHits);
     hpt->Fill(pt);
     heta->Fill(eta);
     hphi->Fill(phi);
     hz->Fill(zip);
+    htip->Fill(tip);
     nTracks++;
   }
   hnTracks->Fill(nTracks);
@@ -93,7 +101,6 @@ void SiPixelPhase1MonitorTrackSoA::analyze(const edm::Event& iEvent, const edm::
 void SiPixelPhase1MonitorTrackSoA::bookHistograms(DQMStore::IBooker& ibooker,
                                                   edm::Run const& iRun,
                                                   edm::EventSetup const& iSetup) {
-  //std::string top_folder = ""//
   ibooker.cd();
   ibooker.setCurrentFolder(topFolderName_);
   hnTracks = ibooker.book1D("nTracks", ";Number of tracks per event;#entries", 1001, -0.5, 1000.5);
@@ -103,6 +110,7 @@ void SiPixelPhase1MonitorTrackSoA::bookHistograms(DQMStore::IBooker& ibooker,
   heta = ibooker.book1D("eta", ";Track #eta;#entries", 30, -3., 3.);
   hphi = ibooker.book1D("phi", ";Track #phi;#entries", 30, -M_PI, M_PI);
   hz = ibooker.book1D("z", ";Track z;#entries", 30, -30., 30.);
+  htip = ibooker.book1D("tip", "", 100, -0.5, 0.5);
 }
 
 void SiPixelPhase1MonitorTrackSoA::fillDescriptions(edm::ConfigurationDescriptions& descriptions) {
@@ -110,8 +118,8 @@ void SiPixelPhase1MonitorTrackSoA::fillDescriptions(edm::ConfigurationDescriptio
   edm::ParameterSetDescription desc;
   desc.add<edm::InputTag>("pixelTrackSrc", edm::InputTag("pixelTracksSoA"));
   desc.add<std::string>("TopFolderName", "SiPixelHeterogeneous/PixelTrackSoA");
-  descriptions.add("monitorpixelTrackSoA", desc);
-  // or use the following to generate the label from the module's C++ type
-  //descriptions.addWithDefaultLabel(desc);
+  desc.add<bool>("useQualityCut", false);
+  desc.add<std::string>("minQuality", "loose");
+  descriptions.addWithDefaultLabel(desc);
 }
 DEFINE_FWK_MODULE(SiPixelPhase1MonitorTrackSoA);
