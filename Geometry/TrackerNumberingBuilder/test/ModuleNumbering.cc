e9ba33c8b462179a0f6d2990e43c2e9ce4e2f797
Sunanda <sunanda.banerjee@cern.ch>
2021-11-05 21:03:32 +0100

    Taking care of ANdrea's comments

--- Geometry/TrackerNumberingBuilder/test/ModuleNumbering.cc
+++ Geometry/TrackerNumberingBuilder/test/ModuleNumbering.cc
@@ -22,6 +22,7 @@
 
 #include "FWCore/Framework/interface/Event.h"
 #include "FWCore/Framework/interface/EventSetup.h"
+#include "FWCore/Framework/interface/ESHandle.h"
 #include "FWCore/Framework/interface/MakerMacros.h"
 
 #include "FWCore/ParameterSet/interface/ParameterSet.h"
