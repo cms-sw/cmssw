e642b395fd76aa2c890a8382b12424000920d86c
Tamas <Tamas.Almos.Vami@cern.ch>
2021-11-05 01:16:27 +0100

    Add producers/configs to refit muon tracks with standard TrackRefitter
    Code checks and code format

---
+++ RecoTracker/TrackProducer/plugins/TrackExtraRekeyer.cc
@@ -0,0 +1,62 @@
+#include "FWCore/Framework/interface/stream/EDProducer.h"
+#include "FWCore/Framework/interface/MakerMacros.h"
+
+#include "DataFormats/TrackReco/interface/Track.h"
+#include "DataFormats/TrackReco/interface/TrackFwd.h"
+#include "DataFormats/TrackReco/interface/TrackExtraFwd.h"
+
+class TrackExtraRekeyer : public edm::stream::EDProducer<> {
+public:
+  explicit TrackExtraRekeyer(const edm::ParameterSet &);
+  ~TrackExtraRekeyer() override {}
+
+  static void fillDescriptions(edm::ConfigurationDescriptions &descriptions);
+
+private:
+  void produce(edm::Event &, const edm::EventSetup &) override;
+
+  edm::EDGetTokenT<reco::TrackCollection> inputTrack_;
+  edm::EDGetTokenT<edm::Association<reco::TrackExtraCollection>> inputAssoc_;
+  edm::EDPutTokenT<reco::TrackCollection> outputTrack_;
+};
+
+TrackExtraRekeyer::TrackExtraRekeyer(const edm::ParameterSet &iConfig) {
+  inputTrack_ = consumes<reco::TrackCollection>(iConfig.getParameter<edm::InputTag>("src"));
+  inputAssoc_ =
+      consumes<edm::Association<reco::TrackExtraCollection>>(iConfig.getParameter<edm::InputTag>("association"));
+  outputTrack_ = produces<reco::TrackCollection>();
+}
+
+// ------------ method called for each event  ------------
+void TrackExtraRekeyer::produce(edm::Event &iEvent, const edm::EventSetup &iSetup) {
+  using namespace edm;
+
+  auto const &tracks = iEvent.get(inputTrack_);
+  auto const &assoc = iEvent.get(inputAssoc_);
+
+  reco::TrackCollection tracksOut;
+
+  for (auto const &track : tracks) {
+    if (!assoc.contains(track.extra().id())) {
+      continue;
+    }
+    const reco::TrackExtraRef &trackextraref = assoc[track.extra()];
+    if (trackextraref.isNonnull()) {
+      auto &trackout = tracksOut.emplace_back(track);
+      trackout.setExtra(trackextraref);
+    }
+  }
+
+  iEvent.emplace(outputTrack_, std::move(tracksOut));
+}
+
+void TrackExtraRekeyer::fillDescriptions(edm::ConfigurationDescriptions &descriptions) {
+  edm::ParameterSetDescription desc;
+  desc.setComment("Simple prooducer to re-key muon tracks for refit");
+  desc.add<edm::InputTag>("src", edm::InputTag("generalTracks"))->setComment("input track collections");
+  desc.add<edm::InputTag>("association", edm::InputTag("muonReducedTrackExtras"))
+      ->setComment("input track association collection");
+  descriptions.addWithDefaultLabel(desc);
+}
+
+DEFINE_FWK_MODULE(TrackExtraRekeyer);
