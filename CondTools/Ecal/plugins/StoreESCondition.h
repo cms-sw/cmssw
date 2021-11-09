322f695b376ecf21e8257a08324391357fcd8cc9
Thomas Reis <thomas.reis@cern.ch>
2021-11-09 16:12:51 +0100

    Replace bare pointers with shared_ptrs and couts with LogInfos.

--- CondTools/Ecal/plugins/StoreESCondition.h
+++ CondTools/Ecal/plugins/StoreESCondition.h
@@ -5,7 +5,7 @@
 
 #include <string>
 #include <map>
-#include <iostream>
+#include <memory>
 #include <fstream>
 #include <cstdio>
 #include <typeinfo>
@@ -30,15 +30,15 @@ namespace edm {
 
 class StoreESCondition : public edm::one::EDAnalyzer<> {
 public:
-  ESThresholds* readESThresholdsFromFile(const char*);
-  ESPedestals* readESPedestalsFromFile(const char*);
-  ESRecHitRatioCuts* readESRecHitRatioCutsFromFile(const char*);
-  ESGain* readESGainFromFile(const char*);
-  ESTimeSampleWeights* readESTimeSampleWeightsFromFile(const char*);
-  ESChannelStatus* readESChannelStatusFromFile(const char*);
-  ESIntercalibConstants* readESIntercalibConstantsFromFile(const char*);
-  ESMissingEnergyCalibration* readESMissingEnergyFromFile(const char*);
-  ESEEIntercalibConstants* readESEEIntercalibConstantsFromFile(const char*);
+  std::shared_ptr<ESThresholds> readESThresholdsFromFile(const char*);
+  std::shared_ptr<ESPedestals> readESPedestalsFromFile(const char*);
+  std::shared_ptr<ESRecHitRatioCuts> readESRecHitRatioCutsFromFile(const char*);
+  std::shared_ptr<ESGain> readESGainFromFile(const char*);
+  std::shared_ptr<ESTimeSampleWeights> readESTimeSampleWeightsFromFile(const char*);
+  std::shared_ptr<ESChannelStatus> readESChannelStatusFromFile(const char*);
+  std::shared_ptr<ESIntercalibConstants> readESIntercalibConstantsFromFile(const char*);
+  std::shared_ptr<ESMissingEnergyCalibration> readESMissingEnergyFromFile(const char*);
+  std::shared_ptr<ESEEIntercalibConstants> readESEEIntercalibConstantsFromFile(const char*);
   void writeToLogFile(std::string, std::string, unsigned long long);
   void writeToLogFileResults(char*);
 
