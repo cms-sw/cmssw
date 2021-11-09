26bc4b3249711eef8c3fc9e1a96d49432de02a78
Sunanda <sunanda.banerjee@cern.ch>
2021-11-09 19:38:22 +0100

    Small fix

--- Geometry/HGCalGeometry/src/HGCalGeometryLoader.cc
+++ Geometry/HGCalGeometry/src/HGCalGeometryLoader.cc
@@ -29,9 +29,9 @@ HGCalGeometry* HGCalGeometryLoader::build(const HGCalTopology& topology) {
                                                   : (int)HGCalGeometry::k_NumberOfParametersPerHex);
   uint32_t numberOfShapes =
       (topology.tileTrapezoid() ? HGCalGeometry::k_NumberOfShapesTrd : HGCalGeometry::k_NumberOfShapes);
-  HGCalGeometryMode::GeometryMode mode = topology.geomMode();
-  bool test = (mode == HGCalGeometryMode::TrapezoidModule);
+  bool test = topology.tileTrapezoid();
 #ifdef EDM_ML_DEBUG
+  HGCalGeometryMode::GeometryMode mode = topology.geomMode();
   edm::LogVerbatim("HGCalGeom") << "Number of Cells " << numberOfCells << ":" << numberExpected << " for sub-detector "
                                 << topology.subDetector() << " Shapes " << numberOfShapes << ":" << parametersPerShape_
                                 << " mode " << mode;
