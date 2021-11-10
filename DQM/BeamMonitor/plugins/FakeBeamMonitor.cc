96153556ec0cadcf81d13c2789e134e265c9bf59
francescobrivio <francesco.brivio@cern.ch>
2021-11-10 18:17:02 +0100

    Migrate BeamSpot codes to new `PoolDBOutputService` methods

--- DQM/BeamMonitor/plugins/FakeBeamMonitor.cc
+++ DQM/BeamMonitor/plugins/FakeBeamMonitor.cc
@@ -1384,51 +1384,48 @@ void FakeBeamMonitor::FitAndFill(const LuminosityBlock& lumiSeg, int& lastlumi, 
   //     }
 
   // Create the BeamSpotOnlineObjects object
-  BeamSpotOnlineObjects* BSOnline = new BeamSpotOnlineObjects();
-  BSOnline->SetLastAnalyzedLumi(LSRange.second);
-  BSOnline->SetLastAnalyzedRun(frun);
-  BSOnline->SetLastAnalyzedFill(0);  // To be updated with correct LHC Fill number
-  BSOnline->SetPosition(bs.x0(), bs.y0(), bs.z0());
-  BSOnline->SetSigmaZ(bs.sigmaZ());
-  BSOnline->SetBeamWidthX(bs.BeamWidthX());
-  BSOnline->SetBeamWidthY(bs.BeamWidthY());
-  BSOnline->SetBeamWidthXError(bs.BeamWidthXError());
-  BSOnline->SetBeamWidthYError(bs.BeamWidthYError());
-  BSOnline->Setdxdz(bs.dxdz());
-  BSOnline->Setdydz(bs.dydz());
-  BSOnline->SetType(bs.type());
-  BSOnline->SetEmittanceX(bs.emittanceX());
-  BSOnline->SetEmittanceY(bs.emittanceY());
-  BSOnline->SetBetaStar(bs.betaStar());
+  BeamSpotOnlineObjects BSOnline;
+  BSOnline.SetLastAnalyzedLumi(LSRange.second);
+  BSOnline.SetLastAnalyzedRun(frun);
+  BSOnline.SetLastAnalyzedFill(0);  // To be updated with correct LHC Fill number
+  BSOnline.SetPosition(bs.x0(), bs.y0(), bs.z0());
+  BSOnline.SetSigmaZ(bs.sigmaZ());
+  BSOnline.SetBeamWidthX(bs.BeamWidthX());
+  BSOnline.SetBeamWidthY(bs.BeamWidthY());
+  BSOnline.SetBeamWidthXError(bs.BeamWidthXError());
+  BSOnline.SetBeamWidthYError(bs.BeamWidthYError());
+  BSOnline.Setdxdz(bs.dxdz());
+  BSOnline.Setdydz(bs.dydz());
+  BSOnline.SetType(bs.type());
+  BSOnline.SetEmittanceX(bs.emittanceX());
+  BSOnline.SetEmittanceY(bs.emittanceY());
+  BSOnline.SetBetaStar(bs.betaStar());
   for (int i = 0; i < 7; ++i) {
     for (int j = 0; j < 7; ++j) {
-      BSOnline->SetCovariance(i, j, bs.covariance(i, j));
+      BSOnline.SetCovariance(i, j, bs.covariance(i, j));
     }
   }
-  //      BSOnline->SetNumTracks(theBeamFitter->getNTracks());
-  //      BSOnline->SetNumPVs(theBeamFitter->getNPVs());
-  BSOnline->SetNumTracks(50);
-  BSOnline->SetNumPVs(10);
-  BSOnline->SetUsedEvents((int)DipPVInfo_[0]);
-  BSOnline->SetMeanPV(DipPVInfo_[1]);
-  BSOnline->SetMeanErrorPV(DipPVInfo_[2]);
-  BSOnline->SetRmsPV(DipPVInfo_[3]);
-  BSOnline->SetRmsErrorPV(DipPVInfo_[4]);
-  BSOnline->SetMaxPVs((int)DipPVInfo_[5]);
+  BSOnline.SetNumTracks(50);
+  BSOnline.SetNumPVs(10);
+  BSOnline.SetUsedEvents((int)DipPVInfo_[0]);
+  BSOnline.SetMeanPV(DipPVInfo_[1]);
+  BSOnline.SetMeanErrorPV(DipPVInfo_[2]);
+  BSOnline.SetRmsPV(DipPVInfo_[3]);
+  BSOnline.SetRmsErrorPV(DipPVInfo_[4]);
+  BSOnline.SetMaxPVs((int)DipPVInfo_[5]);
   auto creationTime =
       std::chrono::duration_cast<std::chrono::microseconds>(std::chrono::system_clock::now().time_since_epoch()).count();
-  BSOnline->SetCreationTime(creationTime);
+  BSOnline.SetCreationTime(creationTime);
 
   // use fake timestamps from 1970.01.01 00:00:00 to 1970.01.01 00:00:01 GMT
   std::pair<time_t, time_t> timeForDIP = std::make_pair(0, 1);
-  BSOnline->SetStartTimeStamp(timeForDIP.first);
-  BSOnline->SetStartTime(getGMTstring(timeForDIP.first));
-  BSOnline->SetEndTimeStamp(timeForDIP.second);
-  BSOnline->SetEndTime(getGMTstring(timeForDIP.second));
+  BSOnline.SetStartTimeStamp(timeForDIP.first);
+  BSOnline.SetStartTime(getGMTstring(timeForDIP.first));
+  BSOnline.SetEndTimeStamp(timeForDIP.second);
+  BSOnline.SetEndTime(getGMTstring(timeForDIP.second));
 
   edm::LogInfo("FakeBeamMonitor") << "FitAndFill::[PayloadCreation] BeamSpotOnline object created: \n" << std::endl;
-  edm::LogInfo("FakeBeamMonitor") << *BSOnline << std::endl;
-  //std::cout << "------------------> fitted BS: " << *BSOnline << std::endl;
+  edm::LogInfo("FakeBeamMonitor") << BSOnline << std::endl;
 
   // Create the payload for BeamSpotOnlineObjects object
   if (onlineDbService_.isAvailable()) {
@@ -1444,25 +1441,25 @@ void FakeBeamMonitor::FitAndFill(const LuminosityBlock& lumiSeg, int& lastlumi, 
     onlineDbService_->logger().logInfo() << "\n" << bs;
     onlineDbService_->logger().logInfo()
         << "FakeBeamMonitor::FitAndFill - [PayloadCreation] BeamSpotOnline object created:";
-    onlineDbService_->logger().logInfo() << "\n" << *BSOnline;
+    onlineDbService_->logger().logInfo() << "\n" << BSOnline;
     onlineDbService_->logger().logInfo() << "FakeBeamMonitor - Additional parameters for DIP:";
-    onlineDbService_->logger().logInfo() << "Events used in the fit: " << BSOnline->GetUsedEvents();
-    onlineDbService_->logger().logInfo() << "Mean PV               : " << BSOnline->GetMeanPV();
-    onlineDbService_->logger().logInfo() << "Mean PV Error         : " << BSOnline->GetMeanErrorPV();
-    onlineDbService_->logger().logInfo() << "Rms PV                : " << BSOnline->GetRmsPV();
-    onlineDbService_->logger().logInfo() << "Rms PV Error          : " << BSOnline->GetRmsErrorPV();
-    onlineDbService_->logger().logInfo() << "Max PVs               : " << BSOnline->GetMaxPVs();
-    onlineDbService_->logger().logInfo() << "StartTime             : " << BSOnline->GetStartTime();
-    onlineDbService_->logger().logInfo() << "StartTimeStamp        : " << BSOnline->GetStartTimeStamp();
-    onlineDbService_->logger().logInfo() << "EndTime               : " << BSOnline->GetEndTime();
-    onlineDbService_->logger().logInfo() << "EndTimeStamp          : " << BSOnline->GetEndTimeStamp();
+    onlineDbService_->logger().logInfo() << "Events used in the fit: " << BSOnline.GetUsedEvents();
+    onlineDbService_->logger().logInfo() << "Mean PV               : " << BSOnline.GetMeanPV();
+    onlineDbService_->logger().logInfo() << "Mean PV Error         : " << BSOnline.GetMeanErrorPV();
+    onlineDbService_->logger().logInfo() << "Rms PV                : " << BSOnline.GetRmsPV();
+    onlineDbService_->logger().logInfo() << "Rms PV Error          : " << BSOnline.GetRmsErrorPV();
+    onlineDbService_->logger().logInfo() << "Max PVs               : " << BSOnline.GetMaxPVs();
+    onlineDbService_->logger().logInfo() << "StartTime             : " << BSOnline.GetStartTime();
+    onlineDbService_->logger().logInfo() << "StartTimeStamp        : " << BSOnline.GetStartTimeStamp();
+    onlineDbService_->logger().logInfo() << "EndTime               : " << BSOnline.GetEndTime();
+    onlineDbService_->logger().logInfo() << "EndTimeStamp          : " << BSOnline.GetEndTimeStamp();
     onlineDbService_->logger().logInfo() << "FakeBeamMonitor::FitAndFill - [PayloadCreation] onlineDbService available";
     onlineDbService_->logger().logInfo() << "FakeBeamMonitor::FitAndFill - [PayloadCreation] SetCreationTime: "
                                          << creationTime << " [epoch in microseconds]";
     try {
       onlineDbService_->writeIOVForNextLumisection(BSOnline, recordName_);
       onlineDbService_->logger().logInfo()
-          << "FakeBeamMonitor::FitAndFill - [PayloadCreation] writeForNextLumisection executed correctly";
+          << "FakeBeamMonitor::FitAndFill - [PayloadCreation] writeIOVForNextLumisection executed correctly";
     } catch (const std::exception& e) {
       onlineDbService_->logger().logError() << "FakeBeamMonitor - Error writing record: " << recordName_
                                             << " for Run: " << frun << " - Lumi: " << LSRange.second;
