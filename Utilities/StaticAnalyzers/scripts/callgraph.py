6ebb5270587fb55388af00b708ce44aa5f430ac1
Patrick Gartung <gartung@fnal.gov>
2021-11-13 01:47:49 +0100

    remove commented out code

--- Utilities/StaticAnalyzers/scripts/callgraph.py
+++ Utilities/StaticAnalyzers/scripts/callgraph.py
@@ -57,17 +57,6 @@ assert(m)
 m2=vre.search(function2)
 assert(m2)
 
-#modules = list()
-#with open('modules_in_ib.txt', 'r') as m:
-#    for line in m:
-#        modules.append(line.strip())
-#mods='|'.join(modules)
-#mods_regex=r'\b(' + mods+ r'QQQQQQQQ)\b'
-#comp_mods_regex=re.compile(mods_regex)
-#n=comp_mods_regex.search(function)
-#assert(n)
-#n2=comp_mods_regex.search(function2)
-#assert(n2)
 
 module2package = dict()
 with open('module_to_package.yaml') as m:
@@ -84,36 +73,6 @@ for k,v in p.items():
 
 for k in module2package.keys():
     module2package[k]=sorted(set(module2package[k]))
-
-
-#class2base = dict()
-#mbcl = re.compile("(base|data) class")
-#with open('classes.txt.dumperall') as f:
-#    for line in f:
-#        if mbcl.search(line):
-#            fields = line.split("'")
-#            if fields[2] == ' base class ' and not baseclassre.search(fields[3]):
-#                class2base.setdefault(fields[1], []).append(fields[3])
-#
-#
-#bmodules=[]
-#for package, modules in module2package.items():
-#    for module in modules:
-#        for cl in class2base.keys():
-#            clname=cl.split('::')[-1]
-#            if module == clname:
-#                for basecl in class2base[cl]:
-#                    if not basecl in set(module2package[package]):
-#                        module2package[package].append(basecl)
-#                    if not basecl in set(bmodules):
-#                        bmodules.append(basecl)
-#
-#
-#bmods='|'.join(bmodules)
-#bmods_regex=r'\b(' + bmods+ r'QQQQQQQQ)\b'
-#comp_bmods_regex=re.compile(bmods_regex)
-#n=comp_bmods_regex.search("function 'edm::BaseFlatGunProducer::beginRun(const edm::Run &, const class edm::EventSetup &)' calls function 'edm::EventSetup::getData<class edm::ESHandle<class HepPDT::ParticleDataTable>>(class edm::ESHandle<class HepPDT::ParticleDataTable> &) const'")
-#assert(n)
 
 G = nx.DiGraph()
 with open('function-calls-db.txt') as f:
