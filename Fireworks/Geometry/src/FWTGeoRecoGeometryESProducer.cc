5773465e226a0b2e790037e5e3ddcaa461798615
Jason Lee <jason.lee@cern.ch>
2021-11-17 04:43:56 +0900

    disable gem by default

--- Fireworks/Geometry/src/FWTGeoRecoGeometryESProducer.cc
+++ Fireworks/Geometry/src/FWTGeoRecoGeometryESProducer.cc
@@ -63,8 +63,11 @@ namespace {
 FWTGeoRecoGeometryESProducer::FWTGeoRecoGeometryESProducer(const edm::ParameterSet& pset) : m_dummyMedium(nullptr) {
   m_tracker = pset.getUntrackedParameter<bool>("Tracker", true);
   m_muon = pset.getUntrackedParameter<bool>("Muon", true);
-  m_gem = pset.getUntrackedParameter<bool>("GEM", true);
+  m_gem = pset.getUntrackedParameter<bool>("GEM", false);
   m_calo = pset.getUntrackedParameter<bool>("Calo", true);
+
+  if (m_muon)
+    m_gem = true;
 
   auto cc = setWhatProduced(this);
   if (m_tracker || m_muon || m_gem) {
