fa041b27fc28b97d3440ef6c6e6fca8976ea116f
Laurent Forthomme <laurent.forthomme@cern.ch>
2021-11-18 12:05:09 +0100

    Removing ctppsGeometryESModule from run3 recipes (loaded by default by the recoCTPPS sequence container)

--- SimPPS/DirectSimProducer/python/simPPS2018_cfi.py
+++ SimPPS/DirectSimProducer/python/simPPS2018_cfi.py
@@ -4,6 +4,7 @@ from CalibPPS.ESProducers.ppsAssociationCuts_non_DB_cff import use_single_infini
 from CalibPPS.ESProducers.ppsAssociationCuts_non_DB_cff import ppsAssociationCutsESSource as _esAssCuts
 from Geometry.VeryForwardGeometry.commons_cff import cloneGeometry
 from SimPPS.DirectSimProducer.profiles_2018_cff import profile_2018_preTS1, profile_2018_TS1_TS2, profile_2018_postTS2
+from SimPPS.DirectSimProducer.simPPS2017_cfi import rpIds
 
 ppsAssociationCutsESSource = _esAssCuts.clone()
 use_single_infinite_iov_entry(ppsAssociationCutsESSource, p2018)
@@ -14,12 +15,4 @@ ctppsCompositeESSource = _esComp.clone(
     periods = [profile_2018_preTS1, profile_2018_TS1_TS2, profile_2018_postTS2],
     compactViewTag = ctppsGeometryESModule.compactViewTag,
     isRun2 = ctppsGeometryESModule.isRun2
-)
-
-# RP ids
-rpIds = cms.PSet(
-  rp_45_F = cms.uint32(23),
-  rp_45_N = cms.uint32(3),
-  rp_56_N = cms.uint32(103),
-  rp_56_F = cms.uint32(123)
 )
