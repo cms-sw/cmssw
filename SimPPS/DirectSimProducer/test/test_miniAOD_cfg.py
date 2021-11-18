264ffc4d000dd1cff386d6ddc4c991d375fbc11e
Laurent Forthomme <laurent.forthomme@cern.ch>
2021-11-19 00:47:54 +0100

    New dedicated sequence for direct simulation reconstruction

--- SimPPS/DirectSimProducer/test/test_miniAOD_cfg.py
+++ SimPPS/DirectSimProducer/test/test_miniAOD_cfg.py
@@ -9,6 +9,9 @@ process.load('Configuration.StandardSequences.GeometryRecoDB_cff')
 process.load('Configuration.StandardSequences.MagneticField_cff')
 process.load('Configuration.StandardSequences.EndOfProcess_cff')
 process.load('Configuration.StandardSequences.FrontierConditions_GlobalTag_cff')
+# PPS simulation and reconstruction chains with standard settings
+process.load('RecoPPS.Configuration.recoCTPPS_cff')
+process.load('SimPPS.Configuration.directSimPPS_cff')
 
 # minimum of logs
 process.load('FWCore.MessageService.MessageLogger_cfi')
@@ -24,23 +27,23 @@ process.source = cms.Source("PoolSource",
     fileNames = cms.untracked.vstring(
         '/store/mc/RunIISummer20UL16MiniAODAPVv2/GGToMuMu_Pt-25_Elastic_13TeV-lpair/MINIAODSIM/106X_mcRun2_asymptotic_preVFP_v11-v1/280000/3870E880-4A47-7440-B122-C76062D2290F.root',
     ),
-    #inputCommands = cms.untracked.vstring(
-    #    'drop *',
-    #    'keep FEDRawDataCollection_*_*_*'
-    #)
 )
 
 process.maxEvents = cms.untracked.PSet(
     input = cms.untracked.int32(1000)
 )
 
-# PPS simulation and reconstruction chains with standard settings
-process.load('SimPPS.Configuration.ppsDirectSim_cff')
-process.load('RecoPPS.Configuration.recoCTPPS_cff')
+process.RandomNumberGeneratorService = cms.Service("RandomNumberGeneratorService",
+    beamDivergenceVtxGenerator = cms.PSet(initialSeed = cms.untracked.uint32(3849)),
+    ppsDirectProtonSimulation = cms.PSet(initialSeed = cms.untracked.uint32(4981))
+)
+
+from SimPPS.DirectSimProducer.profile_base_cff import matchDirectSimOutputs
+matchDirectSimOutputs(process, miniAOD=True)
 
 process.p = cms.Path(
-    process.ppsDirectSim
-    * process.ctppsProtons
+    process.directSimPPS
+    * process.recoDirectSimPPS
 )
 
 # output configuration
