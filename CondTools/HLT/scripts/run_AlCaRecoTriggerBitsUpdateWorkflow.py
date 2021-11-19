f93a4cb82e9901f1cdce54446035d8614365abcd
mmusich <marco.musich@cern.ch>
2021-11-19 23:51:36 +0100

    make update triggerbits wf script use multi-processing

--- CondTools/HLT/scripts/run_AlCaRecoTriggerBitsUpdateWorkflow.py
+++ CondTools/HLT/scripts/run_AlCaRecoTriggerBitsUpdateWorkflow.py
@@ -8,15 +8,17 @@ import os
 import sys
 import ast
 import optparse
-import hashlib
-import tarfile
-import netrc
-import getpass
-import errno
-import sqlite3
-import json
-import tempfile
-import CondCore.Utilities.conddblib as conddb 
+import multiprocessing
+import CondCore.Utilities.conddblib as conddb
+
+officialdbs = {
+    # frontier connections
+    'frontier://PromptProd/CMS_CONDITIONS'   :'pro',
+    'frontier://FrontierProd/CMS_CONDITIONS' :'pro',
+    'frontier://FrontierArc/CMS_CONDITIONS'  :'arc',
+    'frontier://FrontierInt/CMS_CONDITIONS'  :'int',
+    'frontier://FrontierPrep/CMS_CONDITIONS' :'dev'
+}
 
 ##############################################
 def getCommandOutput(command):
@@ -45,16 +47,6 @@ def get_iovs(db, tag):
 
     ### unfortunately some gymnastics is needed here
     ### to make use of the conddb library
-
-    officialdbs = { 
-        # frontier connections
-        'frontier://PromptProd/CMS_CONDITIONS'   :'pro',  
-        'frontier://FrontierProd/CMS_CONDITIONS' :'pro', 
-        'frontier://FrontierArc/CMS_CONDITIONS'  :'arc',            
-        'frontier://FrontierInt/CMS_CONDITIONS'  :'int',            
-        'frontier://FrontierPrep/CMS_CONDITIONS' :'dev', 
-        }
-    
     if db in officialdbs.keys():
         db = officialdbs[db]
 
@@ -74,89 +66,104 @@ def get_iovs(db, tag):
 
     return sorted([int(item[0]) for item in iovs])
 
+##############################################
+def updateBits(blob):
+##############################################
+    if(blob[0]<blob[1]):
+        command = 'conddb --yes --db %s copy %s --destdb %s --from %s --to %s' % (blob[4],
+                                                                                  blob[5],
+                                                                                  blob[5]+"_IOV_"+str(blob[2])+".db",
+                                                                                  str(blob[2]) ,
+                                                                                  str(blob[3]))
+        getCommandOutput(command)
+    else:
+        # last IOV needs special command
+        command = 'conddb --yes --db %s copy %s --destdb %s --from %s' % (blob[4],
+                                                                          blob[5],
+                                                                          blob[5]+"_IOV_"+str(blob[2])+".db",
+                                                                          str(blob[2]))
+        getCommandOutput(command)
+
+    # update the trigger bits
+    cmsRunCommand='cmsRun $CMSSW_BASE/src/CondTools/HLT/test/AlCaRecoTriggerBitsRcdUpdate_TEMPL_cfg.py \
+    inputDB=%s inputTag=%s outputDB=%s outputTag=%s firstRun=%s' % ("sqlite_file:"+blob[5]+"_IOV_"+str(blob[2])+".db",
+                                                                    blob[5],
+                                                                    "sqlite_file:"+blob[5]+"_IOV_"+str(blob[2])+"_updated.db",
+                                                                    blob[6],
+                                                                    str(blob[2]))
+    getCommandOutput(cmsRunCommand)
 
 ##############################################
 def main():
 ##############################################
      
-     defaultDB     = 'sqlite_file:mySqlite.db'
-     defaultInTag  = 'myInTag'
-     defaultOutTag = 'myOutTag'
+    defaultDB     = 'sqlite_file:mySqlite.db'
+    defaultInTag  = 'myInTag'
+    defaultOutTag = 'myOutTag'
+    defaultProc   = 20
 
-     parser = optparse.OptionParser(usage = 'Usage: %prog [options] <file> [<file> ...]\n')
-     
-     parser.add_option('-f', '--inputDB',
-                       dest = 'inputDB',
-                       default = defaultDB,
-                       help = 'file to inspect',
-                       )
-     
-     parser.add_option('-i', '--inputTag',
-                       dest = 'InputTag',
-                       default = defaultInTag,
-                       help = 'tag to be inspected',
-                       )
+    parser = optparse.OptionParser(usage = 'Usage: %prog [options] <file> [<file> ...]\n')
+    parser.add_option('-f', '--inputDB',
+                      dest = 'inputDB',
+                      default = defaultDB,
+                      help = 'file to inspect')
+    parser.add_option('-i', '--inputTag',
+                      dest = 'InputTag',
+                      default = defaultInTag,
+                      help = 'tag to be inspected')
+    parser.add_option('-d', '--destTag',
+                      dest = 'destTag',
+                      default = defaultOutTag,
+                      help = 'tag to be written')
+    parser.add_option('-p', '--processes',
+                      dest = 'nproc',
+                      default = defaultProc,
+                      help = 'multiprocesses to run')
+    parser.add_option("-C", '--clean',
+                      dest="doTheCleaning",
+                      action="store_true",
+                      default = True,
+                      help = 'if true remove the transient files')
 
-     parser.add_option('-d', '--destTag',
-                       dest = 'destTag',
-                       default = defaultOutTag,
-                       help = 'tag to be written',
-                       )
+    (options, arguments) = parser.parse_args()
 
-     parser.add_option("-C", '--clean', 
-                       dest="doTheCleaning",
-                       action="store_true",
-                       default = True,
-                       help = 'if true remove the transient files',
-                       )
+    db_url = options.inputDB
+    if db_url in officialdbs.keys():
+        db_url = officialdbs[db_url]
 
-     (options, arguments) = parser.parse_args()
+    ## allow to use input sqlite files as well
+    db_url = db_url.replace("sqlite_file:", "").replace("sqlite:", "")
 
-     sqlite_db_url = options.inputDB
-     if(".db" in sqlite_db_url):
-         sqlite_db_url = "sqlite_file:{0}".format(sqlite_db_url)
+    sinces = get_iovs(options.inputDB,options.InputTag)
 
-     ##########
-     # code to get it working on sqlite files without CMSSW
+    print("List of sinces: %s" % sinces)
 
-     # db = sqlite3.connect(sqlite_db_url)
-     # cursor = db.cursor()
-     # cursor.execute("SELECT * FROM IOV;")
-     # IOVs = cursor.fetchall()
-     # sinces = []
-     # for element in IOVs:
-     #    sinces.append(element[1])
+    myInputTuple=[]
 
-     sinces = get_iovs(options.inputDB,options.InputTag)
+    for i,since in enumerate(sinces):
+        if(i<len(sinces)-1):
+            #                   0             1        2               3      4                5               6
+            myInputTuple.append((i,len(sinces)-1,sinces[i],sinces[i+1]-1,db_url,options.InputTag,options.destTag))
+        else:
+            myInputTuple.append((i,len(sinces)-1,sinces[i],-1,db_url,options.InputTag,options.destTag))
 
-     print(sinces)
+    pool = multiprocessing.Pool(processes=options.nproc)  # start nproc worker processes
+    count = pool.map(updateBits,myInputTuple)
 
-     for i,since in enumerate(sinces):
-          #print i,since
-         
-         print("============================================================")
-         if(i<len(sinces)-1):
-             command = 'conddb_import -c sqlite_file:'+options.InputTag+"_IOV_"+str(sinces[i])+'.db -f '+sqlite_db_url+" -i "+options.InputTag+" -t "+options.InputTag+" -b "+str(sinces[i])+" -e "+str(sinces[i+1]-1)
-             getCommandOutput(command)
-         else:
-             command = 'conddb_import -c sqlite_file:'+options.InputTag+"_IOV_"+str(sinces[i])+'.db -f '+sqlite_db_url+" -i "+options.InputTag+" -t "+options.InputTag+" -b "+str(sinces[i])
-             getCommandOutput(command)
-             
-         # update the trigger bits             
-         cmsRunCommand="cmsRun $CMSSW_BASE/src/CondTools/HLT/test/AlCaRecoTriggerBitsRcdUpdate_TEMPL_cfg.py inputDB=sqlite_file:"+options.InputTag+"_IOV_"+str(sinces[i])+".db inputTag="+options.InputTag+" outputDB=sqlite_file:"+options.InputTag+"_IOV_"+str(sinces[i])+"_updated.db outputTag="+options.destTag+" firstRun="+str(sinces[i])
-         getCommandOutput(cmsRunCommand)
-     
-         # merge the output
-         mergeCommand = 'conddb_import -f sqlite_file:'+options.InputTag+"_IOV_"+str(sinces[i])+'_updated.db -c sqlite_file:'+options.destTag+".db -i "+options.destTag+" -t "+options.destTag+" -b "+str(sinces[i])
-         getCommandOutput(mergeCommand)
+    # merge the output
+    for i,since in enumerate(sinces):
+        mergeCommand='conddb --yes --db %s copy %s --destdb %s --from %s' % (options.InputTag+"_IOV_"+str(sinces[i])+"_updated.db",
+                                                                             options.destTag,
+                                                                             options.destTag+".db",
+                                                                             str(sinces[i]))
+        getCommandOutput(mergeCommand)
 
-         # clean the house
-         
-         if(options.doTheCleaning):
-             cleanCommand = 'rm -fr *updated*.db *IOV_*.db'
-             getCommandOutput(cleanCommand)
-         else:
-             print("======> keeping the transient files")
+    # clean the house (after all is done)
+    if(options.doTheCleaning):
+        cleanCommand = 'rm -fr *updated*.db *IOV_*.db'
+        getCommandOutput(cleanCommand)
+    else:
+        print("======> keeping the transient files")
           
 if __name__ == "__main__":        
      main()
