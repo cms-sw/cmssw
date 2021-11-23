dfad877b086dddffdb63f8350e82ffe080680876
Louis Portales <72452043+portalesHEP@users.noreply.github.com>
2021-11-23 16:28:18 +0100

    standalone version of backend stage1 truncation

--- L1Trigger/L1THGCal/plugins/backend/HGCalBackendStage1Processor.cc
+++ L1Trigger/L1THGCal/plugins/backend/HGCalBackendStage1Processor.cc
@@ -1,18 +1,27 @@
 #include "L1Trigger/L1THGCal/interface/backend/HGCalBackendStage1Processor.h"
+#include "DataFormats/ForwardDetId/interface/HGCalTriggerBackendDetId.h"
 
 DEFINE_EDM_PLUGIN(HGCalBackendStage1Factory, HGCalBackendStage1Processor, "HGCalBackendStage1Processor");
 
 HGCalBackendStage1Processor::HGCalBackendStage1Processor(const edm::ParameterSet& conf)
-    : HGCalBackendStage1ProcessorBase(conf) {
-  truncation_ = std::make_unique<HGCalStage1TruncationImpl>(conf.getParameterSet("truncation_parameters"));
+    : HGCalBackendStage1ProcessorBase(conf), conf_(conf) {
+  const edm::ParameterSet& truncationParamConfig = conf.getParameterSet("truncation_parameters");
+  const std::string& truncationWrapperName = truncationParamConfig.getParameter<std::string>("AlgoName");
+
+  truncationWrapper_ = std::unique_ptr<HGCalStage1TruncationWrapperBase>{
+      HGCalStage1TruncationWrapperBaseFactory::get()->create(truncationWrapperName, truncationParamConfig)};
 }
 
 void HGCalBackendStage1Processor::run(
     const std::pair<uint32_t, std::vector<edm::Ptr<l1t::HGCalTriggerCell>>>& fpga_id_tcs,
     std::vector<edm::Ptr<l1t::HGCalTriggerCell>>& truncated_tcs,
     const edm::EventSetup& es) {
-  if (truncation_)
-    truncation_->eventSetup(es);
+  const unsigned sector120 = HGCalTriggerBackendDetId(fpga_id_tcs.first).sector();
+  const uint32_t fpga_id = fpga_id_tcs.first;
 
-  truncation_->run(fpga_id_tcs.first, fpga_id_tcs.second, truncated_tcs);
+  // Configuration
+  const std::tuple<const edm::EventSetup&, unsigned, uint32_t> configuration{es, sector120, fpga_id};
+  truncationWrapper_->configure(configuration);
+
+  truncationWrapper_->process(fpga_id_tcs.second, truncated_tcs);
 }
