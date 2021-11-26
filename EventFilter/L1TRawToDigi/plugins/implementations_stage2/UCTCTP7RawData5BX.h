1b7e65b00cab808e585792fc47dbb94c56217e9c
Ho-Fung Tsoi <hftsoi0411@gmail.com>
2021-11-26 16:31:42 +0100

    Modified Calo-L1 unpacker to read 5BX payload format

--- EventFilter/L1TRawToDigi/plugins/implementations_stage2/UCTCTP7RawData5BX.h
+++ EventFilter/L1TRawToDigi/plugins/implementations_stage2/UCTCTP7RawData5BX.h
@@ -1,31 +1,31 @@
-#ifndef UCTCTP7RawData_hh
-#define UCTCTP7RawData_hh
+#ifndef UCTCTP7RawData5BX_hh
+#define UCTCTP7RawData5BX_hh
 
 #include "FWCore/MessageLogger/interface/MessageLogger.h"
 #include "FWCore/MessageLogger/interface/MessageDrop.h"
 
-class UCTCTP7RawData {
+class UCTCTP7RawData5BX {
 public:
   enum CaloType { EBEE = 0, HBHE, HF };
 
   // read-only constructor
-  UCTCTP7RawData(const uint32_t* d) : myDataPtr(d) {
+  UCTCTP7RawData5BX(const uint32_t* d) : myDataPtr(d) {
     if (myDataPtr == nullptr) {
-      edm::LogError("UCTCTP7RawData") << "You gave me a nullptr :<";
+      edm::LogError("UCTCTP7RawData5BX") << "You gave me a nullptr :<";
     }
   }
-  // read-write constructor, caller must allocate 192*sizeof(uint32_t) bytes
-  UCTCTP7RawData(uint32_t* d) : myDataPtr(d), myDataWritePtr(d) {
+  // read-write constructor, caller must allocate 192*5*sizeof(uint32_t) bytes
+  UCTCTP7RawData5BX(uint32_t* d) : myDataPtr(d), myDataWritePtr(d) {
     if (myDataPtr == nullptr) {
-      edm::LogError("UCTCTP7RawData") << "You gave me a nullptr :<";
+      edm::LogError("UCTCTP7RawData5BX") << "You gave me a nullptr :<";
     }
   }
 
   // No copy constructor and equality operator are needed
-  UCTCTP7RawData(const UCTCTP7RawData&) = delete;
-  const UCTCTP7RawData& operator=(const UCTCTP7RawData& i) = delete;
+  UCTCTP7RawData5BX(const UCTCTP7RawData5BX&) = delete;
+  const UCTCTP7RawData5BX& operator=(const UCTCTP7RawData5BX& i) = delete;
 
-  virtual ~UCTCTP7RawData() { ; }
+  virtual ~UCTCTP7RawData5BX() { ; }
 
   // Access functions for convenience
 
@@ -33,48 +33,50 @@ public:
 
   uint32_t sof() { return myDataPtr[0]; }
 
-  size_t getIndex(CaloType cType, bool negativeEta, uint32_t cEta, uint32_t iPhi) {
+  size_t getIndex(CaloType cType, bool negativeEta, uint32_t cEta, uint32_t iPhi, uint32_t BX_n) {
     size_t index = 0;
     if (cType == EBEE || cType == HBHE) {
       if (iPhi > 3) {
-        edm::LogError("UCTCTP7RawData") << "Incorrect iPhi; iPhi = " << iPhi << "; should be in [0,3]";
+        edm::LogError("UCTCTP7RawData5BX") << "Incorrect iPhi; iPhi = " << iPhi << "; should be in [0,3]";
         return index;
       }
       if (cEta < 1 || cEta > 28) {
-        edm::LogError("UCTCTP7RawData") << "Incorrect caloEta; cEta = " << cEta << "; should be in [1-28]";
+        edm::LogError("UCTCTP7RawData5BX") << "Incorrect caloEta; cEta = " << cEta << "; should be in [1-28]";
         return index;
       }
       // ECAL/HB+HE fragment size is 3 32-bit words
       // Each fragment covers 2 eta and 4 phi towers
       // All four phi towers are in one 32-bit word
       // Even and odd eta are in neighboring 32-bit words
-      index = (((cEta - 1) / 2) * (3 + 3) + ((cEta - 1) % 2));
+      // Now each fragment contains 5 BX instead of just 1
+      // Here BX_n = 0, 1, 2, 3, 4, where 2 is nominal
+      index = (((cEta - 1) / 2) * (3 + 3) * 5 + ((cEta - 1) % 2)) + 3 * BX_n;
       // But, towers are arranged in a peculiar order for firmware
       // convenience - the index needs to be computing with these
       // if statements.  This is brittle code that one should be
       // very careful with.
       if (negativeEta) {
         // Add offset for 6 ECAL and 6 HCAL fragments
-        index += (6 * (3 + 3));
+        index += (6 * (3 + 3)) * 5;
       } else {
         if (cEta > 12) {
           // Add offset for 14 ECAL, 14 HB+HE and 2 HF fragments
           // Note that first six are included in the definition of
           // the variable - index
           // Note also that HF fragments are larger at 4 32-bit words
-          index += ((14 * (3 + 3) + (2 * 4)));
+          index += ((14 * (3 + 3) + (2 * 4))) * 5;
         }
       }
       // Data starts with ECAL towers so offset by 3 additional 32-bit words
       if (cType == HBHE)
-        index += 3;
+        index += 3 * 5;
     } else if (cType == HF) {
       if (iPhi > 1) {
-        edm::LogError("UCTCTP7RawData") << "HF iPhi should be 0 or 1 (for a , b) - invalid iPhi  = " << iPhi;
+        edm::LogError("UCTCTP7RawData5BX") << "HF iPhi should be 0 or 1 (for a , b) - invalid iPhi  = " << iPhi;
         return index;
       }
       if (cEta < 30 || cEta > 41) {
-        edm::LogError("UCTCTP7RawData") << "HF cEta should be between 30 and 41 - invalid cEta = " << cEta;
+        edm::LogError("UCTCTP7RawData5BX") << "HF cEta should be between 30 and 41 - invalid cEta = " << cEta;
         return index;
       }
       if (negativeEta) {
@@ -82,33 +84,33 @@ public:
           // Offset by 6 positive eta and 14 negative eta EBEE/HBHE fragments (each 3 32-bit words)
           // There are four HF cEta towers packed in each 32-bit word
           // Add additional offset of 1 for (34-37) and 2 for (38-41)
-          index = 20 * (3 + 3) + ((cEta - 30) / 4);
+          index = 20 * (3 + 3) * 5 + ((cEta - 30) / 4) + 4 * BX_n;
         } else {
           // Additional HF a fragment offset for HF b channel
-          index = 20 * (3 + 3) + 1 * 4 + ((cEta - 30) / 4);
+          index = 20 * (3 + 3) * 5 + 1 * 4 * 5 + ((cEta - 30) / 4) + 4 * BX_n;
         }
       } else {
         if (iPhi == 0) {
           // Offset by all EBEE/HBHE and two HF fragments (4 32-bit words)
-          index = 2 * 14 * (3 + 3) + 2 * 4 + ((cEta - 30) / 4);
+          index = 2 * 14 * (3 + 3) * 5 + 2 * 4 * 5 + ((cEta - 30) / 4) + 4 * BX_n;
         } else {
           // Additional HF a fragment offset for HF b channel
-          index = 2 * 14 * (3 + 3) + 3 * 4 + ((cEta - 30) / 4);
+          index = 2 * 14 * (3 + 3) * 5 + 3 * 4 * 5 + ((cEta - 30) / 4) + 4 * BX_n;
         }
       }
     } else {
-      edm::LogError("UCTCTP7RawData") << "Unknown CaloType " << cType;
+      edm::LogError("UCTCTP7RawData5BX") << "Unknown CaloType " << cType;
       return index;
     }
-    if (index >= 192) {
-      edm::LogError("UCTCTP7RawData") << "Managed to calculate an out-of-bounds index, buyer beware";
+    if (index >= 192 * 5) {
+      edm::LogError("UCTCTP7RawData5BX") << "Managed to calculate an out-of-bounds index, buyer beware";
     }
     return index;
   }
 
-  size_t getFeatureIndex(CaloType cType, bool negativeEta, uint32_t cEta, uint32_t iPhi) {
+  size_t getFeatureIndex(CaloType cType, bool negativeEta, uint32_t cEta, uint32_t iPhi, uint32_t BX_n) {
     // Get index into the data words for the tower
-    size_t index = getIndex(cType, negativeEta, cEta, iPhi);
+    size_t index = getIndex(cType, negativeEta, cEta, iPhi, BX_n);
     if (cType == EBEE || cType == HBHE) {
       // Two 32-bit words contain ET, so we should offset the index to
       // to the feature and link status bits
@@ -134,24 +136,24 @@ public:
       index += (3 - ((cEta - 30) / 4));
       if (index == 0) {
         // Since we sticth index-1, zero is also illegal
-        edm::LogError("UCTCTP7RawData") << "Managed to calculate an out-of-bounds index, buyer beware";
+        edm::LogError("UCTCTP7RawData5BX") << "Managed to calculate an out-of-bounds index, buyer beware";
       }
     } else {
       // Unknown calotype error already generated in getIndex()
       return 0;
     }
-    if (index >= 192) {
-      edm::LogError("UCTCTP7RawData") << "Managed to calculate an out-of-bounds index, buyer beware";
+    if (index >= 192 * 5) {
+      edm::LogError("UCTCTP7RawData5BX") << "Managed to calculate an out-of-bounds index, buyer beware";
     }
     return index;
   }
 
-  void setET(CaloType cType, bool negativeEta, uint32_t cEta, uint32_t iPhi, uint32_t et) {
+  void setET(CaloType cType, bool negativeEta, uint32_t cEta, uint32_t iPhi, uint32_t et, uint32_t BX_n) {
     if (myDataWritePtr == nullptr) {
-      edm::LogError("UCTCTP7RawData") << "I was made in read-only mode";
+      edm::LogError("UCTCTP7RawData5BX") << "I was made in read-only mode";
       return;
     }
-    size_t index = getIndex(cType, negativeEta, cEta, iPhi);
+    size_t index = getIndex(cType, negativeEta, cEta, iPhi, BX_n);
     uint32_t& data = myDataWritePtr[index];
     if (cType == HF) {
       // Pick out the correct 8-bits for the iEta chosen
@@ -167,8 +169,8 @@ public:
     }
   }
 
-  uint32_t getET(CaloType cType, bool negativeEta, uint32_t cEta, uint32_t iPhi) {
-    size_t index = getIndex(cType, negativeEta, cEta, iPhi);
+  uint32_t getET(CaloType cType, bool negativeEta, uint32_t cEta, uint32_t iPhi, uint32_t BX_n) {
+    size_t index = getIndex(cType, negativeEta, cEta, iPhi, BX_n);
     const uint32_t data = myDataPtr[index];
     uint32_t et = 0xDEADBEEF;
     if (cType == HF) {
@@ -185,15 +187,15 @@ public:
     return et;
   }
 
-  void setFB(CaloType cType, bool negativeEta, uint32_t cEta, uint32_t iPhi, uint32_t fb) {
+  void setFB(CaloType cType, bool negativeEta, uint32_t cEta, uint32_t iPhi, uint32_t fb, uint32_t BX_n) {
     if (myDataWritePtr == nullptr) {
-      edm::LogError("UCTCTP7RawData") << "I was made in read-only mode";
+      edm::LogError("UCTCTP7RawData5BX") << "I was made in read-only mode";
       return;
     }
     if (cType == HF) {
-      setHFFeatureBits(negativeEta, cEta, iPhi, fb);
+      setHFFeatureBits(negativeEta, cEta, iPhi, fb, BX_n);
     } else {
-      size_t index = getFeatureIndex(cType, negativeEta, cEta, iPhi);
+      size_t index = getFeatureIndex(cType, negativeEta, cEta, iPhi, BX_n);
       uint32_t& data = myDataWritePtr[index];
 
       uint32_t tower = iPhi;
@@ -204,12 +206,12 @@ public:
     }
   }
 
-  uint32_t getFB(CaloType cType, bool negativeEta, uint32_t cEta, uint32_t iPhi) {
-    size_t index = getFeatureIndex(cType, negativeEta, cEta, iPhi);
+  uint32_t getFB(CaloType cType, bool negativeEta, uint32_t cEta, uint32_t iPhi, uint32_t BX_n) {
+    size_t index = getFeatureIndex(cType, negativeEta, cEta, iPhi, BX_n);
     const uint32_t data = myDataPtr[index];
     uint32_t fb = 0;
     if (cType == HF) {
-      fb = getHFFeatureBits(negativeEta, cEta, iPhi);
+      fb = getHFFeatureBits(negativeEta, cEta, iPhi, BX_n);
     } else {
       // Pick out the correct bit for the tower chosen
       uint32_t tower = iPhi;
@@ -221,12 +223,12 @@ public:
     return fb;
   }
 
-  void setHFFeatureBits(bool negativeEta, uint32_t cEta, uint32_t iPhi, uint32_t fb) {
+  void setHFFeatureBits(bool negativeEta, uint32_t cEta, uint32_t iPhi, uint32_t fb, uint32_t BX_n) {
     if (myDataWritePtr == nullptr) {
-      edm::LogError("UCTCTP7RawData") << "I was made in read-only mode";
+      edm::LogError("UCTCTP7RawData5BX") << "I was made in read-only mode";
       return;
     }
-    size_t index = getFeatureIndex(HF, negativeEta, cEta, iPhi);
+    size_t index = getFeatureIndex(HF, negativeEta, cEta, iPhi, BX_n);
     uint32_t shift = (cEta - 30) * 2;
     if (cEta == 41)
       shift = 20;  // 41 occurs on b-fiber but shares the position of 40
@@ -239,8 +241,8 @@ public:
     }
   }
 
-  uint32_t getHFFeatureBits(bool negativeEta, uint32_t cEta, uint32_t iPhi) {
-    size_t index = getFeatureIndex(HF, negativeEta, cEta, iPhi);
+  uint32_t getHFFeatureBits(bool negativeEta, uint32_t cEta, uint32_t iPhi, uint32_t BX_n) {
+    size_t index = getFeatureIndex(HF, negativeEta, cEta, iPhi, BX_n);
     // Stitch together the top 8 bits from previous 32-bit word and bottom 14 bits from this word
     const uint32_t data = ((myDataPtr[index] & 0x3FFF) << 8) + (myDataPtr[index - 1] >> 24);
     uint32_t shift = (cEta - 30) * 2;
@@ -249,50 +251,56 @@ public:
     return ((data >> shift) & 0x3);
   }
 
-  uint32_t getLinkStatus(CaloType cType, bool negativeEta, uint32_t cEta, uint32_t iPhi) {
-    size_t index = getFeatureIndex(cType, negativeEta, cEta, iPhi);
+  uint32_t getLinkStatus(CaloType cType, bool negativeEta, uint32_t cEta, uint32_t iPhi, uint32_t BX_n) {
+    size_t index = getFeatureIndex(cType, negativeEta, cEta, iPhi, BX_n);
     const uint32_t data = myDataPtr[index];
     return (data >> 16);
   }
 
-  size_t getSummaryIndex(bool negativeEta, uint32_t region) {
-    size_t index = 2 * 14 * (3 + 3) + 4 * 4 + (region / 2);
+  size_t getSummaryIndex(bool negativeEta, uint32_t region, uint32_t BX_n) {
+    size_t index = 2 * 14 * (3 + 3) * 5 + 4 * 4 * 5 + (region / 2) + 4 * BX_n;
     if (negativeEta)
-      index += 4;
-    if (index >= 192) {
-      edm::LogError("UCTCTP7RawData") << "Managed to calculate an out-of-bounds index, buyer beware";
+      index += 4 * 5;
+    if (index >= 192 * 5) {
+      edm::LogError("UCTCTP7RawData5BX") << "Managed to calculate an out-of-bounds index, buyer beware";
     }
     return index;
   }
 
-  void setRegionSummary(bool negativeEta, uint32_t region, uint32_t regionData) {
+  void setRegionSummary(bool negativeEta, uint32_t region, uint32_t regionData, uint32_t BX_n) {
     if (myDataWritePtr == nullptr) {
-      edm::LogError("UCTCTP7RawData") << "I was made in read-only mode";
+      edm::LogError("UCTCTP7RawData5BX") << "I was made in read-only mode";
       return;
     }
-    size_t index = getSummaryIndex(negativeEta, region);
+    size_t index = getSummaryIndex(negativeEta, region, BX_n);
     uint32_t& data = myDataWritePtr[index];
     data |= (regionData & 0xFFFF) << (16 * (region % 2));
   }
 
-  uint32_t getRegionSummary(bool negativeEta, uint32_t region) {
-    size_t index = getSummaryIndex(negativeEta, region);
+  uint32_t getRegionSummary(bool negativeEta, uint32_t region, uint32_t BX_n) {
+    size_t index = getSummaryIndex(negativeEta, region, BX_n);
     const uint32_t data = myDataPtr[index];
     return ((data >> (16 * (region % 2))) & 0xFFFF);
   }
 
-  uint32_t getRegionET(bool negativeEta, uint32_t region) { return (getRegionSummary(negativeEta, region) & 0x3FF); }
-
-  bool getRegionEGVeto(bool negativeEta, uint32_t region) { return (getRegionSummary(negativeEta, region) & 0x0400); }
-
-  bool getRegionTauVeto(bool negativeEta, uint32_t region) { return (getRegionSummary(negativeEta, region) & 0x0800); }
-
-  uint32_t getRegionHitLocation(bool negativeEta, uint32_t region) {
-    return ((getRegionSummary(negativeEta, region) & 0xF000) >> 12);
+  uint32_t getRegionET(bool negativeEta, uint32_t region, uint32_t BX_n) {
+    return (getRegionSummary(negativeEta, region, BX_n) & 0x3FF);
   }
 
-  bool isTowerMasked(CaloType cType, bool negativeEta, uint32_t cEta, uint32_t iPhi) {
-    uint32_t linkStatus = getLinkStatus(cType, negativeEta, cEta, iPhi);
+  bool getRegionEGVeto(bool negativeEta, uint32_t region, uint32_t BX_n) {
+    return (getRegionSummary(negativeEta, region, BX_n) & 0x0400);
+  }
+
+  bool getRegionTauVeto(bool negativeEta, uint32_t region, uint32_t BX_n) {
+    return (getRegionSummary(negativeEta, region, BX_n) & 0x0800);
+  }
+
+  uint32_t getRegionHitLocation(bool negativeEta, uint32_t region, uint32_t BX_n) {
+    return ((getRegionSummary(negativeEta, region, BX_n) & 0xF000) >> 12);
+  }
+
+  bool isTowerMasked(CaloType cType, bool negativeEta, uint32_t cEta, uint32_t iPhi, uint32_t BX_n) {
+    uint32_t linkStatus = getLinkStatus(cType, negativeEta, cEta, iPhi, BX_n);
     uint32_t tower = iPhi;
     if ((cEta % 2) == 0)
       tower += 4;
@@ -304,32 +312,32 @@ public:
     return ((linkStatus & (0x1 << tower)) != 0);
   }
 
-  bool isLinkMisaligned(CaloType cType, bool negativeEta, uint32_t cEta, uint32_t iPhi) {
-    uint32_t linkStatus = getLinkStatus(cType, negativeEta, cEta, iPhi);
+  bool isLinkMisaligned(CaloType cType, bool negativeEta, uint32_t cEta, uint32_t iPhi, uint32_t BX_n) {
+    uint32_t linkStatus = getLinkStatus(cType, negativeEta, cEta, iPhi, BX_n);
     if (cType == EBEE && (cEta == 17 || cEta == 21)) {
       return ((linkStatus & 0x00000100) != 0);
     }
     return ((linkStatus & 0x00001000) != 0);
   }
 
-  bool isLinkInError(CaloType cType, bool negativeEta, uint32_t cEta, uint32_t iPhi) {
-    uint32_t linkStatus = getLinkStatus(cType, negativeEta, cEta, iPhi);
+  bool isLinkInError(CaloType cType, bool negativeEta, uint32_t cEta, uint32_t iPhi, uint32_t BX_n) {
+    uint32_t linkStatus = getLinkStatus(cType, negativeEta, cEta, iPhi, BX_n);
     if (cType == EBEE && (cEta == 17 || cEta == 21)) {
       return ((linkStatus & 0x00000200) != 0);
     }
     return ((linkStatus & 0x00002000) != 0);
   }
 
-  bool isLinkDown(CaloType cType, bool negativeEta, uint32_t cEta, uint32_t iPhi) {
-    uint32_t linkStatus = getLinkStatus(cType, negativeEta, cEta, iPhi);
+  bool isLinkDown(CaloType cType, bool negativeEta, uint32_t cEta, uint32_t iPhi, uint32_t BX_n) {
+    uint32_t linkStatus = getLinkStatus(cType, negativeEta, cEta, iPhi, BX_n);
     if (cType == EBEE && (cEta == 17 || cEta == 21)) {
       return ((linkStatus & 0x00000400) != 0);
     }
     return ((linkStatus & 0x00004000) != 0);
   }
 
-  bool isLinkMasked(CaloType cType, bool negativeEta, uint32_t cEta, uint32_t iPhi) {
-    uint32_t linkStatus = getLinkStatus(cType, negativeEta, cEta, iPhi);
+  bool isLinkMasked(CaloType cType, bool negativeEta, uint32_t cEta, uint32_t iPhi, uint32_t BX_n) {
+    uint32_t linkStatus = getLinkStatus(cType, negativeEta, cEta, iPhi, BX_n);
     if (cType == EBEE && (cEta == 17 || cEta == 21)) {
       return ((linkStatus & 0x00000800) != 0);
     }
@@ -337,7 +345,7 @@ public:
   }
 
 private:
-  // Pointer to contiguous array of 192 values
+  // Pointer to contiguous array of 192*5 values
   // We assume instantiator of this class will gurantee that fact
   const uint32_t* myDataPtr;
   // == myDataPtr unless read-only
