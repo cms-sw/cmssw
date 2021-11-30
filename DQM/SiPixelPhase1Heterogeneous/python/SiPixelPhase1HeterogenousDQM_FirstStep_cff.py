50dd6b98089807265372d5abc285484333682321
Tommaso Tedeschi <tedesk95@gmail.com>
2021-11-30 11:34:07 +0100

    Update SiPixelPhase1HeterogenousDQM_FirstStep_cff.py

--- DQM/SiPixelPhase1Heterogeneous/python/SiPixelPhase1HeterogenousDQM_FirstStep_cff.py
+++ DQM/SiPixelPhase1Heterogeneous/python/SiPixelPhase1HeterogenousDQM_FirstStep_cff.py
@@ -1,5 +1,6 @@
 import FWCore.ParameterSet.Config as cms
+from DQM.SiPixelPhase1Heterogeneous.siPixelPhase1MonitorClusterSoA_cfi import *
 from DQM.SiPixelPhase1Heterogeneous.siPixelPhase1MonitorTrackSoA_cfi import *
 from DQM.SiPixelPhase1Heterogeneous.siPixelPhase1MonitorVertexSoA_cfi import *
 
-monitorpixelSoASource = cms.Sequence(siPixelPhase1MonitorTrackSoA * siPixelPhase1MonitorVertexSoA)
+monitorpixelSoASource = cms.Sequence(siPixelPhase1MonitorClusterSoA * siPixelPhase1MonitorTrackSoA * siPixelPhase1MonitorVertexSoA)
