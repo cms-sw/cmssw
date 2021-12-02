501f1735e8f24df4767663b5ad3852d669e80346
Olga Kodolova <Olga.Kodolova@cern.ch>
2021-12-02 15:25:12 +0100

    Correction for 2 code-lines rejected by code-check.

--- PhysicsTools/PatAlgos/plugins/JPTJetSlimmer.cc
+++ PhysicsTools/PatAlgos/plugins/JPTJetSlimmer.cc
@@ -21,9 +21,9 @@ public:
         srcCaloToken_(consumes<edm::View<reco::CaloJet> >(params.getParameter<edm::InputTag>("srcCalo"))),
         cut_(params.getParameter<std::string>("cut")),
         selector_(cut_) {
-         produces<reco::JPTJetCollection>();
-         produces<reco::CaloJetCollection>();
-        }
+    produces<reco::JPTJetCollection>();
+    produces<reco::CaloJetCollection>();
+  }
 
   ~JPTJetSlimmer() override {}
 
@@ -35,37 +35,38 @@ public:
     edm::Ref<reco::CaloJetCollection>::key_type idxCaloJet = 0;
 
     edm::Handle<edm::View<reco::CaloJet> > h_calojets;
-    iEvent.getByToken(srcCaloToken_, h_calojets); 
+    iEvent.getByToken(srcCaloToken_, h_calojets);
 
     edm::Handle<edm::View<reco::JPTJet> > h_jets;
     iEvent.getByToken(srcToken_, h_jets);
-     
+
     for (auto const& ijet : *h_jets) {
       if (selector_(ijet)) {
-// Add specific : only reference to CaloJet collection. It is necessary for 
-// recalibration JPTJet at MiniAod.
-          edm::RefToBase<reco::Jet> jptjetRef = ijet.getCaloJetRef();
-          reco::CaloJet const * rawcalojet = dynamic_cast<reco::CaloJet const *>( &* jptjetRef);
-        int icalo = -1; 
+        // Add specific : only reference to CaloJet collection. It is necessary for
+        // recalibration JPTJet at MiniAod.
+        const edm::RefToBase<reco::Jet>& jptjetRef = ijet.getCaloJetRef();
+        reco::CaloJet const * rawcalojet = dynamic_cast<reco::CaloJet const *>( &* jptjetRef);
+        int icalo = -1;
         int i = 0;
-        for(auto const& icjet : *h_calojets) {
-            double dr2 = deltaR2(icjet, *rawcalojet);
-            if (dr2 <= 0.001) {
-               icalo = i;
-            }
+        for (auto const& icjet : *h_calojets) {
+          double dr2 = deltaR2(icjet, *rawcalojet);
+          if (dr2 <= 0.001) {
+            icalo = i;
+          }
           i++;
         }
         reco::JPTJet::Specific tmp_specific;
-        if(icalo < 0) {
-// Add reference to the created CaloJet collection
+        if (icalo < 0) {
+          // Add reference to the created CaloJet collection          
           reco::CaloJetRef myjet(pOut1RefProd, idxCaloJet++);
           tmp_specific.theCaloJetRef = edm::RefToBase<reco::Jet>(myjet);
           caloJets->push_back(*rawcalojet);
         } else {
-//  Add reference to existing slimmedCaloJet Collection to JPTJet
-          tmp_specific.theCaloJetRef = edm::RefToBase<reco::Jet>(h_calojets->refAt(icalo));;
+          //  Add reference to existing slimmedCaloJet Collection to JPTJet
+          tmp_specific.theCaloJetRef = edm::RefToBase<reco::Jet>(h_calojets->refAt(icalo));
+          ;
         }
-        const reco::Candidate::Point orivtx = ijet.vertex();
+        const reco::Candidate::Point& orivtx = ijet.vertex();
         reco::JPTJet newJPTJet(ijet.p4(), orivtx, tmp_specific, ijet.getJetConstituents());
         float jetArea = ijet.jetArea();
         newJPTJet.setJetArea(fabs(jetArea));
