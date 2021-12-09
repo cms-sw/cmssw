948625394fba348eba0201f85eb85e577a7f01fb
francescobrivio <francesco.brivio@cern.ch>
2021-12-09 16:12:56 +0100

    migrate legacy EDModules in `CondCore/ESSources/test` and change cout to LogPrint

--- CondCore/ESSources/test/stubs/EfficiencyByLabelAnalyzer.cc
+++ CondCore/ESSources/test/stubs/EfficiencyByLabelAnalyzer.cc
@@ -13,7 +13,7 @@ Toy EDProducers and EDProducts for testing purposes only.
 #include "CondFormats/Calibration/interface/Efficiency.h"
 #include "CondFormats/DataRecord/interface/ExEfficiency.h"
 
-#include "FWCore/Framework/interface/EDAnalyzer.h"
+#include "FWCore/Framework/interface/one/EDAnalyzer.h"
 #include "FWCore/Framework/interface/Event.h"
 #include "FWCore/Framework/interface/EventSetup.h"
 #include "FWCore/Framework/interface/MakerMacros.h"
@@ -21,14 +21,18 @@ Toy EDProducers and EDProducts for testing purposes only.
 using namespace std;
 
 namespace edmtest {
-  class EfficiencyByLabelAnalyzer : public edm::EDAnalyzer {
+  class EfficiencyByLabelAnalyzer : public edm::one::EDAnalyzer<> {
   public:
     explicit EfficiencyByLabelAnalyzer(edm::ParameterSet const& p)
         : theEffToken1_(esConsumes()), theEffToken2_(esConsumes()) {
-      std::cout << "EfficiencyByLabelAnalyzer" << std::endl;
+      edm::LogPrint("EfficiencyByLabelAnalyzer") << "EfficiencyByLabelAnalyzer";
     }
-    explicit EfficiencyByLabelAnalyzer(int i) { std::cout << "EfficiencyByLabelAnalyzer " << i << std::endl; }
-    virtual ~EfficiencyByLabelAnalyzer() { std::cout << "~EfficiencyByLabelAnalyzer " << std::endl; }
+    explicit EfficiencyByLabelAnalyzer(int i) {
+      edm::LogPrint("EfficiencyByLabelAnalyzer") << "EfficiencyByLabelAnalyzer " << i;
+    }
+    virtual ~EfficiencyByLabelAnalyzer() {
+      edm::LogPrint("EfficiencyByLabelAnalyzer") << "~EfficiencyByLabelAnalyzer ";
+    }
     virtual void analyze(const edm::Event& e, const edm::EventSetup& c);
 
   private:
@@ -38,28 +42,28 @@ namespace edmtest {
   void EfficiencyByLabelAnalyzer::analyze(const edm::Event& e, const edm::EventSetup& context) {
     using namespace edm::eventsetup;
     // Context is not used.
-    std::cout << " I AM IN RUN NUMBER " << e.id().run() << std::endl;
-    std::cout << " ---EVENT NUMBER " << e.id().event() << std::endl;
+    edm::LogPrint("EfficiencyByLabelAnalyzer") << " I AM IN RUN NUMBER " << e.id().run();
+    edm::LogPrint("EfficiencyByLabelAnalyzer") << " ---EVENT NUMBER " << e.id().event();
     edm::eventsetup::EventSetupRecordKey recordKey(
         edm::eventsetup::EventSetupRecordKey::TypeTag::findType("ExEfficiencyRcd"));
     if (recordKey.type() == edm::eventsetup::EventSetupRecordKey::TypeTag()) {
       //record not found
-      std::cout << "Record \"ExEfficiencyRcd"
-                << "\" does not exist " << std::endl;
+      edm::LogPrint("EfficiencyByLabelAnalyzer") << "Record \"ExEfficiencyRcd\" does not exist";
     }
-    std::cout << "got context" << std::endl;
+    edm::LogPrint("EfficiencyByLabelAnalyzer") << "got context";
     {
       condex::Efficiency const& eff = context.getData(theEffToken2_);
-      std::cout << "Efficiency*, type (2) " << (void*)(&eff) << " " << typeid(eff).name() << std::endl;
+      edm::LogPrint("EfficiencyByLabelAnalyzer")
+          << "Efficiency*, type (2) " << (void*)(&eff) << " " << typeid(eff).name();
     }
     condex::Efficiency const& eff = context.getData(theEffToken1_);
-    std::cout << "Efficiency*, type " << (void*)(&eff) << " " << typeid(eff).name() << std::endl;
+    edm::LogPrint("EfficiencyByLabelAnalyzer") << "Efficiency*, type " << (void*)(&eff) << " " << typeid(eff).name();
     for (float pt = 0; pt < 10; pt += 2) {
-      std::cout << "\npt=" << pt << "    :";
+      edm::LogPrint("EfficiencyByLabelAnalyzer") << "\npt=" << pt << "    :";
       for (float eta = -3; eta < 3; eta += 1)
-        std::cout << eff(pt, eta) << " ";
+        edm::LogPrint("EfficiencyByLabelAnalyzer") << eff(pt, eta) << " ";
     }
-    std::cout << std::endl;
+    edm::LogPrint("EfficiencyByLabelAnalyzer");
   }
 
   DEFINE_FWK_MODULE(EfficiencyByLabelAnalyzer);
--- CondCore/ESSources/test/stubs/PedestalsByLabelAnalyzer.cc
+++ CondCore/ESSources/test/stubs/PedestalsByLabelAnalyzer.cc
@@ -12,7 +12,7 @@ Toy EDProducers and EDProducts for testing purposes only.
 #include "CondFormats/Calibration/interface/Pedestals.h"
 #include "CondFormats/DataRecord/interface/PedestalsRcd.h"
 
-#include "FWCore/Framework/interface/EDAnalyzer.h"
+#include "FWCore/Framework/interface/one/EDAnalyzer.h"
 #include "FWCore/Framework/interface/Event.h"
 #include "FWCore/Framework/interface/EventSetup.h"
 #include "FWCore/Framework/interface/MakerMacros.h"
@@ -20,14 +20,16 @@ Toy EDProducers and EDProducts for testing purposes only.
 using namespace std;
 
 namespace edmtest {
-  class PedestalsByLabelAnalyzer : public edm::EDAnalyzer {
+  class PedestalsByLabelAnalyzer : public edm::one::EDAnalyzer<> {
   public:
     explicit PedestalsByLabelAnalyzer(edm::ParameterSet const& p)
         : thePedestalToken_(esConsumes(edm::ESInputTag("", "lab3d"))) {
-      std::cout << "PedestalsByLabelAnalyzer" << std::endl;
+      edm::LogPrint("PedestalsByLabelAnalyzer") << "PedestalsByLabelAnalyzer";
     }
-    explicit PedestalsByLabelAnalyzer(int i) { std::cout << "PedestalsByLabelAnalyzer " << i << std::endl; }
-    virtual ~PedestalsByLabelAnalyzer() { std::cout << "~PedestalsByLabelAnalyzer " << std::endl; }
+    explicit PedestalsByLabelAnalyzer(int i) {
+      edm::LogPrint("PedestalsByLabelAnalyzer") << "PedestalsByLabelAnalyzer " << i;
+    }
+    virtual ~PedestalsByLabelAnalyzer() { edm::LogPrint("PedestalsByLabelAnalyzer") << "~PedestalsByLabelAnalyzer "; }
     virtual void analyze(const edm::Event& e, const edm::EventSetup& c);
 
   private:
@@ -37,22 +39,21 @@ namespace edmtest {
   void PedestalsByLabelAnalyzer::analyze(const edm::Event& e, const edm::EventSetup& context) {
     using namespace edm::eventsetup;
     // Context is not used.
-    std::cout << " I AM IN RUN NUMBER " << e.id().run() << std::endl;
-    std::cout << " ---EVENT NUMBER " << e.id().event() << std::endl;
+    edm::LogPrint("PedestalsByLabelAnalyzer") << " I AM IN RUN NUMBER " << e.id().run();
+    edm::LogPrint("PedestalsByLabelAnalyzer") << " ---EVENT NUMBER " << e.id().event();
     edm::eventsetup::EventSetupRecordKey recordKey(
         edm::eventsetup::EventSetupRecordKey::TypeTag::findType("PedestalsRcd"));
     if (recordKey.type() == edm::eventsetup::EventSetupRecordKey::TypeTag()) {
       //record not found
-      std::cout << "Record \"PedestalsRcd"
-                << "\" does not exist " << std::endl;
+      edm::LogPrint("PedestalsByLabelAnalyzer") << "Record \"PedestalsRcd\" does not exist";
     }
-    std::cout << "got context" << std::endl;
+    edm::LogPrint("PedestalsByLabelAnalyzer") << "got context";
     auto const& myped = &context.getData(thePedestalToken_);
-    std::cout << "Pedestals* " << myped << std::endl;
+    edm::LogPrint("PedestalsByLabelAnalyzer") << "Pedestals* " << myped;
     for (std::vector<Pedestals::Item>::const_iterator it = myped->m_pedestals.begin(); it != myped->m_pedestals.end();
          ++it)
-      std::cout << " mean: " << it->m_mean << " variance: " << it->m_variance;
-    std::cout << std::endl;
+      edm::LogPrint("PedestalsByLabelAnalyzer") << " mean: " << it->m_mean << " variance: " << it->m_variance;
+    edm::LogPrint("PedestalsByLabelAnalyzer") << std::endl;
   }
   DEFINE_FWK_MODULE(PedestalsByLabelAnalyzer);
 }  // namespace edmtest
