0463d97bffdb7d20499d9171a905e794404f5b32
Michael-Krohn <krohn045@umn.edu>
2021-12-10 09:04:10 -0600

    Only apply TP modification to Run3

--- CalibCalorimetry/HcalTPGAlgos/src/HcaluLUTTPGCoder.cc
+++ CalibCalorimetry/HcalTPGAlgos/src/HcaluLUTTPGCoder.cc
@@ -545,8 +545,8 @@ void HcaluLUTTPGCoder::update(const HcalDbService& conditions) {
               lut[adc] |= QIE11_LUT_MSB1;
           }
 
-	  //Zeroing the 4th depth in the trigger towers where |ieta| = 16 to match the behavior in the uHTR firmware, where the 4th depth is not included in the sum over depths when constructing the TP energy for this tower.
-	  if (abs(cell.ieta()) == 16 && cell.depth() == 4) {
+	  //Zeroing the 4th depth in the trigger towers where |ieta| = 16 to match the behavior in the uHTR firmware in Run3, where the 4th depth is not included in the sum over depths when constructing the TP energy for this tower.
+	  if (abs(cell.ieta()) == 16 && cell.depth() == 4 && topo_->triggerMode() >= HcalTopologyMode::TriggerMode_2021) {
             lut[adc] = 0;
           }
         }
