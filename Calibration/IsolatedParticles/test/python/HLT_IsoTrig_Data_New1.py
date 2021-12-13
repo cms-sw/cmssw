628bd4f8e9f923db16bb90aa1f01e12577fbf703
Javier <Javier.Fernandez@cern.ch>
2021-12-13 19:51:49 +0100

    Drop EI from test file

--- Calibration/IsolatedParticles/test/python/HLT_IsoTrig_Data_New1.py
+++ Calibration/IsolatedParticles/test/python/HLT_IsoTrig_Data_New1.py
@@ -2,7 +2,7 @@
 # using: 
 # Revision: 1.19 
 # Source: /local/reps/CMSSW/CMSSW/Configuration/Applications/python/ConfigBuilder.py,v 
-# with command line options: step3 --conditions auto:run2_mc_GRun -n 10 --eventcontent FEVTDEBUGHLT,DQM -s RAW2DIGI,L1Reco,RECO,EI,VALIDATION,DQM --datatier GEN-SIM-RECO,DQM --customise SLHCUpgradeSimulations/Configuration/postLS1Customs.customisePostLS1 --geometry Extended2015 --magField 38T_PostLS1 --conditions auto:run2_mc_GRun --no_exec --filein file:step2.root --fileout file:step3.root
+# with command line options: step3 --conditions auto:run2_mc_GRun -n 10 --eventcontent FEVTDEBUGHLT,DQM -s RAW2DIGI,L1Reco,RECO,VALIDATION,DQM --datatier GEN-SIM-RECO,DQM --customise SLHCUpgradeSimulations/Configuration/postLS1Customs.customisePostLS1 --geometry Extended2015 --magField 38T_PostLS1 --conditions auto:run2_mc_GRun --no_exec --filein file:step2.root --fileout file:step3.root
 import FWCore.ParameterSet.Config as cms
 
 process = cms.Process('HLTNew1')
@@ -22,7 +22,6 @@ process.load('HLTrigger.Configuration.HLT_GRun_cff')
 process.load('Configuration.StandardSequences.RawToDigi_cff')
 process.load('Configuration.StandardSequences.L1Reco_cff')
 process.load('Configuration.StandardSequences.Reconstruction_cff')
-process.load('CommonTools.ParticleFlow.EITopPAG_cff')
 process.load('Configuration.StandardSequences.Validation_cff')
 process.load('DQMOffline.Configuration.DQMOfflineMC_cff')
 process.load('Configuration.StandardSequences.EndOfProcess_cff')
@@ -211,7 +210,6 @@ process.FastTimerOutput = cms.EndPath( process.dqmOutput )
 process.raw2digi_step = cms.Path(process.RawToDigi)
 process.L1Reco_step = cms.Path(process.L1Reco)
 process.reconstruction_step = cms.Path(process.reconstruction)
-process.eventinterpretaion_step = cms.Path(process.EIsequence)
 process.prevalidation_step = cms.Path(process.prevalidation)
 process.dqmoffline_step = cms.Path(process.DQMOffline)
 process.validation_step = cms.EndPath(process.validation)
