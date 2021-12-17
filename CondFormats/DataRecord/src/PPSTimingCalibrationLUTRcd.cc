57c876e13360b8e792c752677dedd2b6d667ee3a
Christopher <krzysztof.misan@cern.ch>
2021-12-17 14:43:31 +0100

    Calibration record for HPTDC nonlinearities compensation

---
+++ CondFormats/DataRecord/src/PPSTimingCalibrationLUTRcd.cc
@@ -0,0 +1,15 @@
+// -*- C++ -*-
+//
+// Package:     CondFormats/DataRecord
+// Class  :     PPSTimingCalibrationLUTRcd
+//
+// Implementation:
+//     [Notes on implementation]
+//
+// Author:      Krzysztof Misan
+// Created:     Sun, 12 Dec 2021 18:10:14 GMT
+
+#include "CondFormats/DataRecord/interface/PPSTimingCalibrationLUTRcd.h"
+#include "FWCore/Framework/interface/eventsetuprecord_registration_macro.h"
+
+EVENTSETUP_RECORD_REG(PPSTimingCalibrationLUTRcd);
