f61a5b0b64c0d06a7981dff2b089d99c5c75651e
Nihar Ranjan Saha <56932008+NiharSaha@users.noreply.github.com>
2021-12-18 20:56:15 +0530

    Update DQM/HLTEvF/python/HLTTrackingMonitoring_cff.py
    Co-authored-by: Marino Missiroli <m.missiroli@cern.ch>

--- DQM/HLTEvF/python/HLTTrackingMonitoring_cff.py
+++ DQM/HLTEvF/python/HLTTrackingMonitoring_cff.py
@@ -2,9 +2,9 @@ import FWCore.ParameterSet.Config as cms
 
 # tracking monitor
 from DQMOffline.Trigger.TrackingMonitoring_cff import *
-doProfilesVsLS   = True
-beamSpot = "hltOnlineBeamSpot"
-beamSpot = "hltOnlineBeamSpot"
+iterHLTTracksMonitoringHLT.doProfilesVsLS = True
+iterHLTTracksMonitoringHLT.beamSpot = "hltOnlineBeamSpot"
+pixelTracksMonitoringHLT.beamSpot = "hltOnlineBeamSpot"
 
 from TrackingTools.TrackFitters.KFTrajectoryFitter_cfi import *
 from TrackingTools.TransientTrack.TransientTrackBuilder_cfi import *
