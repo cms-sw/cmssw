f3b565c2a7c013d9bd77963c986fe85350f44936
mmusich <marco.musich@cern.ch>
2021-12-22 19:15:20 +0100

    take parameters from fillDescriptions method

--- CalibTracker/SiPixelLorentzAngle/python/SiPixelLorentzAnglePCLWorker_cfi.py
+++ CalibTracker/SiPixelLorentzAngle/python/SiPixelLorentzAnglePCLWorker_cfi.py
@@ -1,23 +1,7 @@
 import FWCore.ParameterSet.Config as cms
 
-from DQMServices.Core.DQMEDAnalyzer import DQMEDAnalyzer
-SiPixelLorentzAnglePCLWorker = DQMEDAnalyzer(
-    "SiPixelLorentzAnglePCLWorker",
-    folder = cms.string('AlCaReco/SiPixelLorentzAngle'),
-    notInPCL = cms.bool(False),
-    fileName = cms.string('testrun.root'),
-    newmodulelist = cms.vstring(),
-    src = cms.InputTag("TrackRefitter"),
-    binsDepth    = cms.int32(50),
-    binsDrift =    cms.int32(200),
-    ptMin = cms.double(3),
-    normChi2Max = cms.double(2),
-    clustSizeYMin = cms.int32(4),
-    clustSizeYMinL4 = cms.int32(3),
-    clustSizeXMax = cms.int32(5),
-    residualMax = cms.double(0.005),
-    clustChargeMaxPerLength = cms.double(50000)
-)
+from CalibTracker.SiPixelLorentzAngle.siPixelLorentzAnglePCLWorker_cfi import siPixelLorentzAnglePCLWorker as _defaultWorker
+SiPixelLorentzAnglePCLWorker = _defaultWorker.clone()
 
 ## modules replaced over 2017/2018 EOY shutdown, "new" in 2018
 from Configuration.Eras.Modifier_run2_SiPixel_2018_cff import run2_SiPixel_2018
