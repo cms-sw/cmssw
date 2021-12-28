719126db1638f3bbb5db71290cc68720accbefe0
Alexx Perloff <Alexx.Perloff@Colorado.edu>
2021-12-28 11:01:44 -0600

    Changes to L1TrackerEtMissProducer if the L1TrackSelectionProducer is run first.
    (cherry picked from commit e1dda8deb549973d6e32de0367142625ddbc091e)

--- L1Trigger/L1TTrackMatch/plugins/L1TrackerEtMissProducer.cc
+++ L1Trigger/L1TTrackMatch/plugins/L1TrackerEtMissProducer.cc
@@ -34,6 +34,8 @@ class L1TrackerEtMissProducer : public edm::stream::EDProducer<> {
 public:
   typedef TTTrack<Ref_Phase2TrackerDigi_> L1TTTrackType;
   typedef std::vector<L1TTTrackType> L1TTTrackCollectionType;
+  typedef edm::Ref<L1TTTrackCollectionType> L1TTTrackRefType;
+  typedef std::vector<L1TTTrackRefType> L1TTTrackRefCollectionType;
 
   typedef Vertex L1VertexType;
   typedef VertexCollection L1VertexCollectionType;
@@ -47,18 +49,11 @@ private:
   virtual void endJob();
 
   // ----------member data ---------------------------
-  const float maxZ0_;  // in cm
-  float deltaZ_;       // in cm
-  const float Chi2RphidofMax_;
-  const float Chi2RzdofMax_;
-  const float bendChi2Max_;
-  const float minPt_;  // in GeV
-  const int nStubsmin_;
-  const int nPSStubsMin_;  // minimum number of stubs in PS modules
-  const float maxPt_;
-  const float maxEta_;
-  const int highPtTracks_;       // saturate or truncate
-  const bool displaced_;         // prompt/displaced tracks
+  float deltaZ_;  // in cm
+  float maxPt_;       // in GeV
+  int highPtTracks_;  // saturate or truncate
+  bool displaced_;    // prompt/displaced tracks
+
   vector<double> z0Thresholds_;  // Threshold for track to vertex association
   vector<double> etaRegions_;    // Eta bins for choosing deltaZ threshold
   bool debug_;
@@ -67,30 +62,22 @@ private:
   std::string L1ExtendedMetCollectionName;
 
   const edm::EDGetTokenT<VertexCollection> pvToken_;
-  const edm::EDGetTokenT<L1TTTrackCollectionType> trackToken_;
-  edm::ESGetToken<TrackerTopology, TrackerTopologyRcd> tTopoToken_;
+  const edm::EDGetTokenT<L1TTTrackRefCollectionType> trackToken_;
 };
 
 // constructor//
 L1TrackerEtMissProducer::L1TrackerEtMissProducer(const edm::ParameterSet& iConfig)
-    : maxZ0_((float)iConfig.getParameter<double>("maxZ0")),
-      deltaZ_((float)iConfig.getParameter<double>("deltaZ")),
-      Chi2RphidofMax_((float)iConfig.getParameter<double>("chi2rphidofMax")),
-      Chi2RzdofMax_((float)iConfig.getParameter<double>("chi2rzdofMax")),
-      bendChi2Max_((float)iConfig.getParameter<double>("bendChi2Max")),
-      minPt_((float)iConfig.getParameter<double>("minPt")),
-      nStubsmin_(iConfig.getParameter<int>("nStubsmin")),
-      nPSStubsMin_(iConfig.getParameter<int>("nPSStubsMin")),
-      maxPt_((float)iConfig.getParameter<double>("maxPt")),
-      maxEta_((float)iConfig.getParameter<double>("maxEta")),
-      highPtTracks_(iConfig.getParameter<int>("highPtTracks")),
-      displaced_(iConfig.getParameter<bool>("displaced")),
-      z0Thresholds_(iConfig.getParameter<std::vector<double>>("z0Thresholds")),
-      etaRegions_(iConfig.getParameter<std::vector<double>>("etaRegions")),
-      debug_(iConfig.getParameter<bool>("debug")),
-      pvToken_(consumes<L1VertexCollectionType>(iConfig.getParameter<edm::InputTag>("L1VertexInputTag"))),
-      trackToken_(consumes<L1TTTrackCollectionType>(iConfig.getParameter<edm::InputTag>("L1TrackInputTag"))),
-      tTopoToken_(esConsumes<TrackerTopology, TrackerTopologyRcd>(edm::ESInputTag("", ""))) {
+    : pvToken_(consumes<L1VertexCollectionType>(iConfig.getParameter<edm::InputTag>("L1VertexInputTag"))),
+      trackToken_(consumes<L1TTTrackRefCollectionType>(iConfig.getParameter<edm::InputTag>("L1TrackInputTag"))) {
+  deltaZ_ = (float)iConfig.getParameter<double>("deltaZ");
+  maxPt_ = (float)iConfig.getParameter<double>("maxPt");
+  highPtTracks_ = iConfig.getParameter<int>("highPtTracks");
+  displaced_ = iConfig.getParameter<bool>("displaced");
+  z0Thresholds_ = iConfig.getParameter<std::vector<double>>("z0Thresholds");
+  etaRegions_ = iConfig.getParameter<std::vector<double>>("etaRegions");
+
+  debug_ = iConfig.getParameter<bool>("debug");
+
   L1MetCollectionName = (std::string)iConfig.getParameter<std::string>("L1MetCollectionName");
 
   if (displaced_) {
@@ -113,9 +100,8 @@ void L1TrackerEtMissProducer::produce(edm::Event& iEvent, const edm::EventSetup&
   edm::Handle<L1VertexCollectionType> L1VertexHandle;
   iEvent.getByToken(pvToken_, L1VertexHandle);
 
-  edm::Handle<L1TTTrackCollectionType> L1TTTrackHandle;
+  edm::Handle<L1TTTrackRefCollectionType> L1TTTrackHandle;
   iEvent.getByToken(trackToken_, L1TTTrackHandle);
-  L1TTTrackCollectionType::const_iterator trackIter;
 
   if (!L1VertexHandle.isValid()) {
     LogError("L1TrackerEtMissProducer") << "\nWarning: VertexCollection not found in the event. Exit\n";
@@ -135,36 +121,14 @@ void L1TrackerEtMissProducer::produce(edm::Event& iEvent, const edm::EventSetup&
   double etTot_PU = 0;
   float zVTX = L1VertexHandle->begin()->z0();
 
-  int numtracks = 0;
   int numqualitytracks = 0;
   int numassoctracks = 0;
 
-  for (trackIter = L1TTTrackHandle->begin(); trackIter != L1TTTrackHandle->end(); ++trackIter) {
-    numtracks++;
-    float pt = trackIter->momentum().perp();
-    float phi = trackIter->momentum().phi();
-    float eta = trackIter->momentum().eta();
-    float chi2rphidof = trackIter->chi2XYRed();
-    float chi2rzdof = trackIter->chi2ZRed();
-    float bendChi2 = trackIter->stubPtConsistency();
-    float z0 = trackIter->z0();
-    //unsigned int Sector = trackIter->phiSector();
-    std::vector<edm::Ref<edmNew::DetSetVector<TTStub<Ref_Phase2TrackerDigi_>>, TTStub<Ref_Phase2TrackerDigi_>>>
-        theStubs = trackIter->getStubRefs();
-    int nstubs = (int)theStubs.size();
-
-    if (pt < minPt_)
-      continue;
-    if (std::abs(z0) > maxZ0_)
-      continue;
-    if (std::abs(eta) > maxEta_)
-      continue;
-    if (chi2rphidof > Chi2RphidofMax_)
-      continue;
-    if (chi2rzdof > Chi2RzdofMax_)
-      continue;
-    if (bendChi2 > bendChi2Max_)
-      continue;
+  for (const auto & track : *L1TTTrackHandle) {
+    float pt = track->momentum().perp();
+    float phi = track->momentum().phi();
+    float eta = track->momentum().eta();
+    float z0 = track->z0();
 
     if (maxPt_ > 0 && pt > maxPt_) {
       if (highPtTracks_ == 0)
@@ -173,27 +137,11 @@ void L1TrackerEtMissProducer::produce(edm::Event& iEvent, const edm::EventSetup&
         pt = maxPt_;  // saturate
     }
 
-    int nPS = 0;  // number of stubs in PS modules
-    // loop over the stubs
-    for (unsigned int istub = 0; istub < (unsigned int)theStubs.size(); istub++) {
-      DetId detId(theStubs.at(istub)->getDetId());
-      if (detId.det() == DetId::Detector::Tracker) {
-        if ((detId.subdetId() == StripSubdetector::TOB && tTopo.tobLayer(detId) <= 3) ||
-            (detId.subdetId() == StripSubdetector::TID && tTopo.tidRing(detId) <= 9))
-          nPS++;
-      }
-    }
-
-    if (nstubs < nStubsmin_)
-      continue;
-    if (nPS < nPSStubsMin_)
-      continue;
-
     numqualitytracks++;
 
     if (!displaced_) {  // if displaced, deltaZ = 3.0 cm, very loose
       // construct deltaZ cut to be based on track eta
-      for (unsigned int reg = 0; reg < etaRegions_.size() - 1; reg++) {
+      for (unsigned int reg = 0; reg < etaRegions_.size(); reg++) {
         if (std::abs(eta) >= etaRegions_[reg] && std::abs(eta) < etaRegions_[reg + 1]) {
           deltaZ_ = z0Thresholds_[reg];
           break;
@@ -230,8 +178,8 @@ void L1TrackerEtMissProducer::produce(edm::Event& iEvent, const edm::EventSetup&
                                                 << "\n"
                                                 << "MET: " << et << "| Phi: " << etphi << "\n"
 
-                                                << "# Intial Tracks: " << numtracks << "\n"
-                                                << "# Tracks after Quality Cuts: " << numqualitytracks << "\n"
+                                                << "# Tracks after Quality Cuts: " << L1TTTrackHandle->size() << "\n"
+                                                << "# Tacks after additional highPt Cuts: " << numqualitytracks << "\n"
                                                 << "# Tracks Associated to Vertex: " << numassoctracks << "\n"
                                                 << "========================================================"
                                                 << "\n";
