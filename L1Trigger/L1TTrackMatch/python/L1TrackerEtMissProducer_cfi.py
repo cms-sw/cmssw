6b334bc57bd71e323752aefc622670fe6fb4aa97
Alexx Perloff <Alexx.Perloff@Colorado.edu>
2021-12-28 11:01:44 -0600

    Changes to L1TrackerEtMissProducer if the L1TrackSelectionProducer is run first.
    (cherry picked from commit e1dda8deb549973d6e32de0367142625ddbc091e)

--- L1Trigger/L1TTrackMatch/python/L1TrackerEtMissProducer_cfi.py
+++ L1Trigger/L1TTrackMatch/python/L1TrackerEtMissProducer_cfi.py
@@ -1,19 +1,12 @@
 import FWCore.ParameterSet.Config as cms
 from L1Trigger.VertexFinder.VertexProducer_cff import VertexProducer
+from L1Trigger.L1TTrackMatch.L1TrackSelectionProducer_cfi import L1TrackSelectionProducer, L1TrackSelectionProducerExtended
 
 L1TrackerEtMiss = cms.EDProducer('L1TrackerEtMissProducer',
-    L1TrackInputTag = cms.InputTag("TTTracksFromTrackletEmulation", "Level1TTTracks"),
+    L1TrackInputTag = cms.InputTag("L1TrackSelectionProducer", L1TrackSelectionProducer.outputCollectionName.value()),
     L1VertexInputTag = cms.InputTag("VertexProducer", VertexProducer.l1VertexCollectionName.value()),
     L1MetCollectionName = cms.string("L1TrackerEtMiss"),
-    maxZ0 = cms.double ( 15. ) ,    # in cm
-    maxEta = cms.double ( 2.4 ) ,   # max eta allowed for chosen tracks
-    chi2rzdofMax = cms.double( 5. ), # max chi2rz/dof allowed for chosen tracks
-    chi2rphidofMax = cms.double( 20. ), # max chi2rphi/dof allowed for chosen tracks
-    bendChi2Max = cms.double( 2.25 ),# max bendchi2 allowed for chosen tracks
-    minPt = cms.double( 2. ),       # in GeV
     deltaZ = cms.double( 3. ),      # in cm
-    nStubsmin = cms.int32( 4 ),     # min number of stubs for the tracks
-    nPSStubsMin = cms.int32( -1 ),  # min number of stubs in the PS Modules
     maxPt = cms.double( 200. ),	    # in GeV. When maxPt > 0, tracks with PT above maxPt are considered as
                                     # mismeasured and are treated according to highPtTracks below.
                                     # When maxPt < 0, no special treatment is done for high PT tracks.
@@ -27,19 +20,11 @@ L1TrackerEtMiss = cms.EDProducer('L1TrackerEtMissProducer',
 )
 
 L1TrackerEtMissExtended = cms.EDProducer('L1TrackerEtMissProducer', #NOT OPTIMIZED, STUDIED, OR USED
-    L1TrackInputTag = cms.InputTag("TTTracksFromExtendedTrackletEmulation", "Level1TTTracks"),
+    L1TrackInputTag = cms.InputTag("L1TrackSelectionProducerExtended", L1TrackSelectionProducerExtended.outputCollectionName.value()),
     L1VertexInputTag = cms.InputTag("VertexProducer", VertexProducer.l1VertexCollectionName.value()),
     L1MetCollectionName = cms.string("L1TrackerEtMiss"),
     L1MetExtendedCollectionName = cms.string("L1TrackerExtendedEtMiss"),
-    maxZ0 = cms.double ( 15. ) ,    # in cm
-    maxEta = cms.double ( 2.4 ) ,   # max eta allowed for chosen tracks
-    chi2rzdofMax = cms.double( 10. ), # max chi2rz/dof allowed
-    chi2rphidofMax = cms.double( 40. ), # max chi2rphi/dof allowed
-    bendChi2Max = cms.double( 2.4 ),# max bendchi2 allowed for chosen tracks
-    minPt = cms.double( 3. ),       # in GeV
     deltaZ = cms.double( 3.0 ),     # in cm
-    nStubsmin = cms.int32( 4 ),     # min number of stubs for the tracks
-    nPSStubsMin = cms.int32( -1 ),  # min number of stubs in the PS Modules
     maxPt = cms.double( 200. ),	    # in GeV. When maxPt > 0, tracks with PT above maxPt are considered as
                                     # mismeasured and are treated according to highPtTracks below.
                                     # When maxPt < 0, no special treatment is done for high PT tracks.
