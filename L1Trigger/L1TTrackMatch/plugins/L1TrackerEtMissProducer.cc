dab299e637a03cdf04f5762ecb4b219aaac3c913
Alexx Perloff <Alexx.Perloff@Colorado.edu>
2021-12-28 13:29:57 -0600

    Rely on the L1TrackSelectionProducer for all of the track quality cuts and the track-vertex association.
    (cherry picked from commit 970dc1b45d1cf975a9bc745abd94a99fd6d19955)
    (cherry picked from commit 2ad014bdb390db024952314f932d67bdb85cf4d0)

--- L1Trigger/L1TTrackMatch/plugins/L1TrackerEtMissProducer.cc
+++ L1Trigger/L1TTrackMatch/plugins/L1TrackerEtMissProducer.cc
@@ -4,6 +4,7 @@
 // Modified by Christopher Brown 27 March 2021
 
 // system include files
+#include <algorithm>
 #include <memory>
 
 // user include files
@@ -13,103 +14,69 @@
 #include "DataFormats/L1TCorrelator/interface/TkEtMiss.h"
 #include "DataFormats/L1TCorrelator/interface/TkEtMissFwd.h"
 #include "DataFormats/L1TrackTrigger/interface/TTTypes.h"
-#include "DataFormats/L1Trigger/interface/Vertex.h"
 #include "DataFormats/Math/interface/LorentzVector.h"
 #include "FWCore/Framework/interface/Event.h"
 #include "FWCore/Framework/interface/EventSetup.h"
 #include "FWCore/Framework/interface/Frameworkfwd.h"
 #include "FWCore/Framework/interface/MakerMacros.h"
-#include "FWCore/Framework/interface/stream/EDProducer.h"
+#include "FWCore/Framework/interface/global/EDProducer.h"
 #include "FWCore/ParameterSet/interface/ParameterSet.h"
-
-// detector geometry
-#include "DataFormats/TrackerCommon/interface/TrackerTopology.h"
-#include "Geometry/Records/interface/TrackerTopologyRcd.h"
-#include "MagneticField/Engine/interface/MagneticField.h"
-#include "MagneticField/Records/interface/IdealMagneticFieldRecord.h"
 
 using namespace l1t;
 
-class L1TrackerEtMissProducer : public edm::stream::EDProducer<> {
+class L1TrackerEtMissProducer : public edm::global::EDProducer<> {
 public:
   typedef TTTrack<Ref_Phase2TrackerDigi_> L1TTTrackType;
   typedef std::vector<L1TTTrackType> L1TTTrackCollectionType;
   typedef edm::Ref<L1TTTrackCollectionType> L1TTTrackRefType;
   typedef std::vector<L1TTTrackRefType> L1TTTrackRefCollectionType;
 
-  typedef Vertex L1VertexType;
-  typedef VertexCollection L1VertexCollectionType;
-
   explicit L1TrackerEtMissProducer(const edm::ParameterSet&);
   ~L1TrackerEtMissProducer() override;
 
 private:
-  virtual void beginJob();
-  void produce(edm::Event&, const edm::EventSetup&) override;
-  virtual void endJob();
+  void produce(edm::StreamID, edm::Event&, const edm::EventSetup&) const override;
 
   // ----------member data ---------------------------
-  float deltaZ_;  // in cm
+  const edm::EDGetTokenT<L1TTTrackRefCollectionType> trackToken_;
+  const edm::EDGetTokenT<L1TTTrackRefCollectionType> vtxAssocTrackToken_;
+  std::string L1MetCollectionName;
   float maxPt_;       // in GeV
   int highPtTracks_;  // saturate or truncate
-  bool displaced_;    // prompt/displaced tracks
-
-  vector<double> z0Thresholds_;  // Threshold for track to vertex association
-  vector<double> etaRegions_;    // Eta bins for choosing deltaZ threshold
   bool debug_;
-
-  std::string L1MetCollectionName;
-  std::string L1ExtendedMetCollectionName;
-
-  const edm::EDGetTokenT<VertexCollection> pvToken_;
-  const edm::EDGetTokenT<L1TTTrackRefCollectionType> trackToken_;
 };
 
-// constructor//
+// constructor
 L1TrackerEtMissProducer::L1TrackerEtMissProducer(const edm::ParameterSet& iConfig)
-    : pvToken_(consumes<L1VertexCollectionType>(iConfig.getParameter<edm::InputTag>("L1VertexInputTag"))),
-      trackToken_(consumes<L1TTTrackRefCollectionType>(iConfig.getParameter<edm::InputTag>("L1TrackInputTag"))) {
-  deltaZ_ = (float)iConfig.getParameter<double>("deltaZ");
-  maxPt_ = (float)iConfig.getParameter<double>("maxPt");
-  highPtTracks_ = iConfig.getParameter<int>("highPtTracks");
-  displaced_ = iConfig.getParameter<bool>("displaced");
-  z0Thresholds_ = iConfig.getParameter<std::vector<double>>("z0Thresholds");
-  etaRegions_ = iConfig.getParameter<std::vector<double>>("etaRegions");
-
-  debug_ = iConfig.getParameter<bool>("debug");
-
-  L1MetCollectionName = (std::string)iConfig.getParameter<std::string>("L1MetCollectionName");
-
-  if (displaced_) {
-    L1ExtendedMetCollectionName = (std::string)iConfig.getParameter<std::string>("L1MetExtendedCollectionName");
-    produces<TkEtMissCollection>(L1ExtendedMetCollectionName);
-  } else
-    produces<TkEtMissCollection>(L1MetCollectionName);
+    : trackToken_(consumes<L1TTTrackRefCollectionType>(iConfig.getParameter<edm::InputTag>("L1TrackInputTag"))),
+      vtxAssocTrackToken_(consumes<L1TTTrackRefCollectionType>(iConfig.getParameter<edm::InputTag>("L1TrackAssociatedInputTag"))),
+      L1MetCollectionName(iConfig.getParameter<std::string>("L1MetCollectionName")),
+      maxPt_(iConfig.getParameter<double>("maxPt")),
+      highPtTracks_(iConfig.getParameter<int>("highPtTracks")),
+      debug_(iConfig.getParameter<bool>("debug")) {
+  produces<TkEtMissCollection>(L1MetCollectionName);
 }
 
 L1TrackerEtMissProducer::~L1TrackerEtMissProducer() {}
 
-void L1TrackerEtMissProducer::produce(edm::Event& iEvent, const edm::EventSetup& iSetup) {
+void L1TrackerEtMissProducer::produce(edm::StreamID, edm::Event& iEvent, const edm::EventSetup& iSetup) const {
   using namespace edm;
 
   std::unique_ptr<TkEtMissCollection> METCollection(new TkEtMissCollection);
 
-  // Tracker Topology
-  const TrackerTopology& tTopo = iSetup.getData(tTopoToken_);
-
-  edm::Handle<L1VertexCollectionType> L1VertexHandle;
-  iEvent.getByToken(pvToken_, L1VertexHandle);
-
   edm::Handle<L1TTTrackRefCollectionType> L1TTTrackHandle;
   iEvent.getByToken(trackToken_, L1TTTrackHandle);
 
-  if (!L1VertexHandle.isValid()) {
-    LogError("L1TrackerEtMissProducer") << "\nWarning: VertexCollection not found in the event. Exit\n";
-    return;
-  }
+  edm::Handle<L1TTTrackRefCollectionType> L1TTTrackAssociatedHandle;
+  iEvent.getByToken(vtxAssocTrackToken_, L1TTTrackAssociatedHandle);
 
   if (!L1TTTrackHandle.isValid()) {
     LogError("L1TrackerEtMissProducer") << "\nWarning: L1TTTrackCollection not found in the event. Exit\n";
+    return;
+  }
+
+  if (!L1TTTrackAssociatedHandle.isValid()) {
+    LogError("L1TrackerEtMissProducer") << "\nWarning: L1TTTrackAssociatedCollection not found in the event. Exit\n";
     return;
   }
 
@@ -119,7 +86,6 @@ void L1TrackerEtMissProducer::produce(edm::Event& iEvent, const edm::EventSetup&
   double sumPx_PU = 0;
   double sumPy_PU = 0;
   double etTot_PU = 0;
-  float zVTX = L1VertexHandle->begin()->z0();
 
   int numqualitytracks = 0;
   int numassoctracks = 0;
@@ -127,8 +93,6 @@ void L1TrackerEtMissProducer::produce(edm::Event& iEvent, const edm::EventSetup&
   for (const auto & track : *L1TTTrackHandle) {
     float pt = track->momentum().perp();
     float phi = track->momentum().phi();
-    float eta = track->momentum().eta();
-    float z0 = track->z0();
 
     if (maxPt_ > 0 && pt > maxPt_) {
       if (highPtTracks_ == 0)
@@ -139,21 +103,7 @@ void L1TrackerEtMissProducer::produce(edm::Event& iEvent, const edm::EventSetup&
 
     numqualitytracks++;
 
-    if (!displaced_) {  // if displaced, deltaZ = 3.0 cm, very loose
-      // construct deltaZ cut to be based on track eta
-      for (unsigned int reg = 0; reg < etaRegions_.size(); reg++) {
-        if (std::abs(eta) >= etaRegions_[reg] && std::abs(eta) < etaRegions_[reg + 1]) {
-          deltaZ_ = z0Thresholds_[reg];
-          break;
-        }
-      }
-      if (std::abs(eta) >= etaRegions_[etaRegions_.size() - 1]) {
-        deltaZ_ = z0Thresholds_[etaRegions_.size() - 1];
-        break;
-      }
-    }
-
-    if (std::abs(z0 - zVTX) <= deltaZ_) {
+    if (std::find(L1TTTrackAssociatedHandle->begin(), L1TTTrackAssociatedHandle->end(), track) != L1TTTrackAssociatedHandle->end()) {
       numassoctracks++;
       sumPx += pt * cos(phi);
       sumPy += pt * sin(phi);
@@ -178,9 +128,9 @@ void L1TrackerEtMissProducer::produce(edm::Event& iEvent, const edm::EventSetup&
                                                 << "\n"
                                                 << "MET: " << et << "| Phi: " << etphi << "\n"
 
-                                                << "# Tracks after Quality Cuts: " << L1TTTrackHandle->size() << "\n"
+                                                << "# Tracks after quality cuts: " << L1TTTrackHandle->size() << "\n"
                                                 << "# Tacks after additional highPt Cuts: " << numqualitytracks << "\n"
-                                                << "# Tracks Associated to Vertex: " << numassoctracks << "\n"
+                                                << "# Tracks associated to vertex: " << numassoctracks << "\n"
                                                 << "========================================================"
                                                 << "\n";
   }
@@ -188,14 +138,7 @@ void L1TrackerEtMissProducer::produce(edm::Event& iEvent, const edm::EventSetup&
   int ibx = 0;
   METCollection->push_back(TkEtMiss(missingEt, TkEtMiss::kMET, etphi, numassoctracks, ibx));
 
-  if (displaced_)
-    iEvent.put(std::move(METCollection), L1ExtendedMetCollectionName);
-  else
-    iEvent.put(std::move(METCollection), L1MetCollectionName);
+  iEvent.put(std::move(METCollection), L1MetCollectionName);
 }  // end producer
-
-void L1TrackerEtMissProducer::beginJob() {}
-
-void L1TrackerEtMissProducer::endJob() {}
 
 DEFINE_FWK_MODULE(L1TrackerEtMissProducer);
--- L1Trigger/L1TTrackMatch/python/L1TrackerEtMissProducer_cfi.py
+++ L1Trigger/L1TTrackMatch/python/L1TrackerEtMissProducer_cfi.py
@@ -4,35 +4,28 @@ from L1Trigger.L1TTrackMatch.L1TrackSelectionProducer_cfi import L1TrackSelectio
 
 L1TrackerEtMiss = cms.EDProducer('L1TrackerEtMissProducer',
     L1TrackInputTag = cms.InputTag("L1TrackSelectionProducer", L1TrackSelectionProducer.outputCollectionName.value()),
+    L1TrackAssociatedInputTag = cms.InputTag("L1TrackSelectionProducer", L1TrackSelectionProducer.outputCollectionName.value() + "Associated"),
     L1VertexInputTag = cms.InputTag("VertexProducer", VertexProducer.l1VertexCollectionName.value()),
     L1MetCollectionName = cms.string("L1TrackerEtMiss"),
-    deltaZ = cms.double( 3. ),      # in cm
     maxPt = cms.double( 200. ),	    # in GeV. When maxPt > 0, tracks with PT above maxPt are considered as
                                     # mismeasured and are treated according to highPtTracks below.
                                     # When maxPt < 0, no special treatment is done for high PT tracks.
     highPtTracks = cms.int32( 1 ),  # when = 0 : truncation. Tracks with PT above maxPt are ignored
                                     # when = 1 : saturation. Tracks with PT above maxPt are set to PT=maxPt.
                                     # When maxPt < 0, no special treatment is done for high PT tracks.
-    displaced = cms.bool(False),     # Use promt/displaced tracks
-    z0Thresholds = cms.vdouble( 0.37, 0.5, 0.6, 0.75, 1.0, 1.6 ), # Threshold for track to vertex association.
-    etaRegions = cms.vdouble( 0, 0.7, 1.0, 1.2, 1.6, 2.0, 2.4 ), # Eta bins for choosing deltaZ threshold.
     debug     = cms.bool(False)
 )
 
 L1TrackerEtMissExtended = cms.EDProducer('L1TrackerEtMissProducer', #NOT OPTIMIZED, STUDIED, OR USED
     L1TrackInputTag = cms.InputTag("L1TrackSelectionProducerExtended", L1TrackSelectionProducerExtended.outputCollectionName.value()),
+    L1TrackAssociatedInputTag = cms.InputTag("L1TrackSelectionProducer", L1TrackSelectionProducer.outputCollectionName.value() + "Associated"),
     L1VertexInputTag = cms.InputTag("VertexProducer", VertexProducer.l1VertexCollectionName.value()),
-    L1MetCollectionName = cms.string("L1TrackerEtMiss"),
-    L1MetExtendedCollectionName = cms.string("L1TrackerExtendedEtMiss"),
-    deltaZ = cms.double( 3.0 ),     # in cm
+    L1MetCollectionName = cms.string("L1TrackerExtendedEtMiss"),
     maxPt = cms.double( 200. ),	    # in GeV. When maxPt > 0, tracks with PT above maxPt are considered as
                                     # mismeasured and are treated according to highPtTracks below.
                                     # When maxPt < 0, no special treatment is done for high PT tracks.
     highPtTracks = cms.int32( 1 ),  # when = 0 : truncation. Tracks with PT above maxPt are ignored
                                     # when = 1 : saturation. Tracks with PT above maxPt are set to PT=maxPt.
                                     # When maxPt < 0, no special treatment is done for high PT tracks.
-    displaced = cms.bool(True),      # Use promt/displaced tracks
-    z0Thresholds = cms.vdouble( 3.0, 3.0, 3.0, 3.0, 3.0, 3.0 ), # Threshold for track to vertex association.
-    etaRegions = cms.vdouble( 0, 0.7, 1.0, 1.2, 1.6, 2.0, 2.4 ), # Eta bins for choosing deltaZ threshold.
     debug     = cms.bool(False)
 )
