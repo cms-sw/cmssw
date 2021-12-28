43afc3b480eee8b8a865058b4c422b26ceaf86e3
Vladimir <Vladimir.Ivantchenko@cern.ch>
2021-12-28 09:32:30 +0100

    removed commented lines

--- SimTransport/PPSProtonTransport/interface/HectorTransport.h
+++ SimTransport/PPSProtonTransport/interface/HectorTransport.h
@@ -8,7 +8,6 @@
 
 #include "FWCore/Framework/interface/EventSetup.h"
 #include "FWCore/Utilities/interface/EDGetToken.h"
-//#include "FWCore/Framework/interface/ESHandle.h"
 #include "FWCore/Framework/interface/ConsumesCollector.h"
 #include "FWCore/MessageLogger/interface/MessageLogger.h"
 #include "FWCore/ParameterSet/interface/ParameterSet.h"
