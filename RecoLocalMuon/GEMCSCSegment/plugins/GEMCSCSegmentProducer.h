17782ecb5f178b7645e3feebc3761f9fe8466424
Seungjin Yang <seungjin.yang@cern.ch>
2021-12-30 01:09:09 +0900

    Use tokens to retrieve GEMGeometry and CSCGeometry from the EventSetup because EventSetupRecord::get has been deprecated.

--- RecoLocalMuon/GEMCSCSegment/plugins/GEMCSCSegmentProducer.h
+++ RecoLocalMuon/GEMCSCSegment/plugins/GEMCSCSegmentProducer.h
@@ -12,11 +12,16 @@
 #include "FWCore/Framework/interface/Frameworkfwd.h"
 #include "FWCore/Framework/interface/stream/EDProducer.h"
 #include "FWCore/Framework/interface/Event.h"
+#include "FWCore/Framework/interface/ESHandle.h"
 #include "FWCore/ParameterSet/interface/ParameterSet.h"
 #include "FWCore/Utilities/interface/InputTag.h"
 
 #include "DataFormats/CSCRecHit/interface/CSCSegmentCollection.h"
 #include "DataFormats/GEMRecHit/interface/GEMRecHitCollection.h"
+
+#include <Geometry/Records/interface/MuonGeometryRecord.h>
+#include "Geometry/CSCGeometry/interface/CSCGeometry.h"
+#include "Geometry/GEMGeometry/interface/GEMGeometry.h"
 
 class GEMCSCSegmentBuilder;
 
@@ -30,10 +35,13 @@ public:
   void produce(edm::Event&, const edm::EventSetup&) override;
 
 private:
+  const edm::ESGetToken<CSCGeometry, MuonGeometryRecord> kCSCGeometryToken_;
+  const edm::ESGetToken<GEMGeometry, MuonGeometryRecord> kGEMGeometryToken_;
+  const edm::EDGetTokenT<CSCSegmentCollection> kCSCSegmentCollectionToken_;
+  const edm::EDGetTokenT<GEMRecHitCollection> kGEMRecHitCollectionToken_;
+
   int iev;  // events through
   GEMCSCSegmentBuilder* segmentBuilder_;
-  edm::EDGetTokenT<CSCSegmentCollection> csc_token;
-  edm::EDGetTokenT<GEMRecHitCollection> gem_token;
 };
 
 #endif
