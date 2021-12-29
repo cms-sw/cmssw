8b0430523f9d01089727444767a96964b3dbc047
Sunanda <sunanda.banerjee@cern.ch>
2021-12-29 16:22:35 +0100

    Take care of comments

--- SimTracker/SiPixelDigitizer/test/PixelSimHitsTest.cc
+++ SimTracker/SiPixelDigitizer/test/PixelSimHitsTest.cc
@@ -70,7 +70,7 @@ using namespace edm;
 
 //#define CHECK_GEOM
 
-class PixelSimHitsTest : public edm::one::EDAnalyzer<> {
+class PixelSimHitsTest : public edm::one::EDAnalyzer<edm::one::SharedResources> {
 public:
   explicit PixelSimHitsTest(const edm::ParameterSet &);
   ~PixelSimHitsTest() override;
@@ -117,6 +117,8 @@ private:
 };
 //
 PixelSimHitsTest::PixelSimHitsTest(const edm::ParameterSet &iConfig) : conf_(iConfig) {
+  usesResource(TFileService::kSharedResource);
+
   std::string src_, list_;
   src_ = iConfig.getParameter<std::string>("src");
   list_ = iConfig.getParameter<std::string>("list");
