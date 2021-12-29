9faaf5943218833de3fa7d314876e1c09985e504
Sunanda <sunanda.banerjee@cern.ch>
2021-12-29 16:22:35 +0100

    Take care of comments

--- SimTracker/SiPixelDigitizer/test/PixelDigisTest.cc
+++ SimTracker/SiPixelDigitizer/test/PixelDigisTest.cc
@@ -103,7 +103,7 @@ using namespace std;
 // class decleration
 //
 
-class PixelDigisTest : public edm::one::EDAnalyzer<> {
+class PixelDigisTest : public edm::one::EDAnalyzer<edm::one::SharedResources> {
 public:
   explicit PixelDigisTest(const edm::ParameterSet &);
   ~PixelDigisTest() override;
@@ -169,6 +169,8 @@ private:
 // constructors and destructor
 //
 PixelDigisTest::PixelDigisTest(const edm::ParameterSet &iConfig) {
+  usesResource(TFileService::kSharedResource);
+
   //We put this here for the moment since there is no better place
   //edm::Service<MonitorDaemon> daemon;
   //daemon.operator->();
