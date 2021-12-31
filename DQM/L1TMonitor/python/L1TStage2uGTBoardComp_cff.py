d618fc5f37fa29006f3869d674fb8a7124f9ccdc
Nihar Ranjan Saha <56932008+NiharSaha@users.noreply.github.com>
2021-12-31 16:21:59 +0530

    Update L1TStage2uGTBoardComp_cff.py

--- DQM/L1TMonitor/python/L1TStage2uGTBoardComp_cff.py
+++ DQM/L1TMonitor/python/L1TStage2uGTBoardComp_cff.py
@@ -16,28 +16,28 @@ l1tStage2uGTMuon1vsMuon2 = DQMEDAnalyzer(
 )
 
 l1tStage2uGTMuon1vsMuon3 = l1tStage2uGTMuon1vsMuon2.clone(
-muonCollection2 = "gtStage2Digis:Muon3",
-muonCollection2Title = "Muons uGT Board 3",
-summaryTitle = "Summary of Comparison between Muons from uGT Board 1 and uGT Board 3",
-monitorDir = "L1T/L1TStage2uGT/uGTBoardComparisons/Board1vsBoard3/Muons"
+    muonCollection2 = "gtStage2Digis:Muon3",
+    muonCollection2Title = "Muons uGT Board 3",
+    summaryTitle = "Summary of Comparison between Muons from uGT Board 1 and uGT Board 3",
+    monitorDir = "L1T/L1TStage2uGT/uGTBoardComparisons/Board1vsBoard3/Muons"
 )
 l1tStage2uGTMuon1vsMuon4 = l1tStage2uGTMuon1vsMuon2.clone(
-muonCollection2 = "gtStage2Digis:Muon4",
-muonCollection2Title = "Muons uGT Board 4",
-summaryTitle = "Summary of Comparison between Muons from uGT Board 1 and uGT Board 4",
-monitorDir = "L1T/L1TStage2uGT/uGTBoardComparisons/Board1vsBoard4/Muons"
+    muonCollection2 = "gtStage2Digis:Muon4",
+    muonCollection2Title = "Muons uGT Board 4",
+    summaryTitle = "Summary of Comparison between Muons from uGT Board 1 and uGT Board 4",
+    monitorDir = "L1T/L1TStage2uGT/uGTBoardComparisons/Board1vsBoard4/Muons"
 )
 l1tStage2uGTMuon1vsMuon5 = l1tStage2uGTMuon1vsMuon2.clone(
-muonCollection2 = "gtStage2Digis:Muon5",
-muonCollection2Title = "Muons uGT Board 5",
-summaryTitle = "Summary of Comparison between Muons from uGT Board 1 and uGT Board 5",
-monitorDir = "L1T/L1TStage2uGT/uGTBoardComparisons/Board1vsBoard5/Muons"
+    muonCollection2 = "gtStage2Digis:Muon5",
+    muonCollection2Title = "Muons uGT Board 5",
+    summaryTitle = "Summary of Comparison between Muons from uGT Board 1 and uGT Board 5",
+    monitorDir = "L1T/L1TStage2uGT/uGTBoardComparisons/Board1vsBoard5/Muons"
 )
 l1tStage2uGTMuon1vsMuon6 = l1tStage2uGTMuon1vsMuon2.clone(
-muonCollection2 = "gtStage2Digis:Muon6",
-muonCollection2Title = "Muons uGT Board 6",
-summaryTitle = "Summary of Comparison between Muons from uGT Board 1 and uGT Board 6",
-monitorDir = "L1T/L1TStage2uGT/uGTBoardComparisons/Board1vsBoard6/Muons"
+    muonCollection2 = "gtStage2Digis:Muon6",
+    muonCollection2Title = "Muons uGT Board 6",
+    summaryTitle = "Summary of Comparison between Muons from uGT Board 1 and uGT Board 6",
+    monitorDir = "L1T/L1TStage2uGT/uGTBoardComparisons/Board1vsBoard6/Muons"
 )
 l1tStage2uGTBoardCompMuonsSeq = cms.Sequence(
     l1tStage2uGTMuon1vsMuon2 +
@@ -65,36 +65,36 @@ l1tStage2uGTCalo1vsCalo2 = DQMEDAnalyzer(
 )
 
 l1tStage2uGTCalo1vsCalo3 = l1tStage2uGTCalo1vsCalo2.clone(
-collection2Title  = "uGT Board 3", 
-JetCollection2    = "gtStage2Digis:Jet3",
-EGammaCollection2 = "gtStage2Digis:EGamma3",
-TauCollection2    = "gtStage2Digis:Tau3",
-EtSumCollection2  = "gtStage2Digis:EtSum3",
-monitorDir = "L1T/L1TStage2uGT/uGTBoardComparisons/Board1vsBoard3/CaloLayer2"
+    collection2Title  = "uGT Board 3", 
+    JetCollection2    = "gtStage2Digis:Jet3",
+    EGammaCollection2 = "gtStage2Digis:EGamma3",
+    TauCollection2    = "gtStage2Digis:Tau3",
+    EtSumCollection2  = "gtStage2Digis:EtSum3",
+    monitorDir = "L1T/L1TStage2uGT/uGTBoardComparisons/Board1vsBoard3/CaloLayer2"
 )
 l1tStage2uGTCalo1vsCalo4 = l1tStage2uGTCalo1vsCalo2.clone(
-collection2Title  = "uGT Board 4", 
-JetCollection2    = "gtStage2Digis:Jet4",
-EGammaCollection2 = "gtStage2Digis:EGamma4",
-TauCollection2    = "gtStage2Digis:Tau4",
-EtSumCollection2  = "gtStage2Digis:EtSum4",
-monitorDir = "L1T/L1TStage2uGT/uGTBoardComparisons/Board1vsBoard4/CaloLayer2"
+    collection2Title  = "uGT Board 4", 
+    JetCollection2    = "gtStage2Digis:Jet4",
+    EGammaCollection2 = "gtStage2Digis:EGamma4",
+    TauCollection2    = "gtStage2Digis:Tau4",
+    EtSumCollection2  = "gtStage2Digis:EtSum4",
+    monitorDir = "L1T/L1TStage2uGT/uGTBoardComparisons/Board1vsBoard4/CaloLayer2"
 )
 l1tStage2uGTCalo1vsCalo5 = l1tStage2uGTCalo1vsCalo2.clone(
-collection2Title  = "uGT Board 5", 
-JetCollection2    = "gtStage2Digis:Jet5",
-EGammaCollection2 = "gtStage2Digis:EGamma5",
-TauCollection2    = "gtStage2Digis:Tau5",
-EtSumCollection2  = "gtStage2Digis:EtSum5",
-monitorDir = "L1T/L1TStage2uGT/uGTBoardComparisons/Board1vsBoard5/CaloLayer2"
+    collection2Title  = "uGT Board 5", 
+    JetCollection2    = "gtStage2Digis:Jet5",
+    EGammaCollection2 = "gtStage2Digis:EGamma5",
+    TauCollection2    = "gtStage2Digis:Tau5",
+    EtSumCollection2  = "gtStage2Digis:EtSum5",
+    monitorDir = "L1T/L1TStage2uGT/uGTBoardComparisons/Board1vsBoard5/CaloLayer2"
 )
 l1tStage2uGTCalo1vsCalo6 = l1tStage2uGTCalo1vsCalo2.clone(
-collection2Title  = "uGT Board 6", 
-JetCollection2    = "gtStage2Digis:Jet6",
-EGammaCollection2 = "gtStage2Digis:EGamma6",
-TauCollection2    = "gtStage2Digis:Tau6",
-EtSumCollection2  = "gtStage2Digis:EtSum6",
-monitorDir = "L1T/L1TStage2uGT/uGTBoardComparisons/Board1vsBoard6/CaloLayer2"
+    collection2Title  = "uGT Board 6", 
+    JetCollection2    = "gtStage2Digis:Jet6",
+    EGammaCollection2 = "gtStage2Digis:EGamma6",
+    TauCollection2    = "gtStage2Digis:Tau6",
+    EtSumCollection2  = "gtStage2Digis:EtSum6",
+    monitorDir = "L1T/L1TStage2uGT/uGTBoardComparisons/Board1vsBoard6/CaloLayer2"
 )
 l1tStage2uGTBoardCompCaloLayer2Seq = cms.Sequence(
     l1tStage2uGTCalo1vsCalo2 +
