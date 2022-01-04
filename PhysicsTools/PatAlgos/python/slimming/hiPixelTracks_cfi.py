f3d9ca78f2748d6108378188f1a7b6c80ed2d5dd
Cesar Bernardes <cesar.augusto.bernardes@cern.ch>
2022-01-04 01:20:09 +0100

    update parameters during cloning

--- PhysicsTools/PatAlgos/python/slimming/hiPixelTracks_cfi.py
+++ PhysicsTools/PatAlgos/python/slimming/hiPixelTracks_cfi.py
@@ -2,12 +2,13 @@ import FWCore.ParameterSet.Config as cms
 
 from PhysicsTools.PatAlgos.patTracksToPackedCandidates_cfi import patTracksToPackedCandidates
 
-hiPixelTracks = patTracksToPackedCandidates.clone()
-hiPixelTracks.dxySigCut = 3.
-hiPixelTracks.dzSigCut = 3.
-hiPixelTracks.dxySigHP = 2.
-hiPixelTracks.dzSigHP = 2.
-hiPixelTracks.ptMin = 0.2
+hiPixelTracks = patTracksToPackedCandidates.clone(
+  dxySigCut = 3.0,
+  dzSigCut = 3.0,
+  dxySigHP = 2.0,
+  dzSigHP = 2.0,
+  ptMin = 0.2
+)
 
 from Configuration.Eras.Modifier_phase1Pixel_cff import phase1Pixel
 phase1Pixel.toModify(hiPixelTracks, covarianceVersion=1)
