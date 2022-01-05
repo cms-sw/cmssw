e5882fe56c59e1dc4555290dcdf209974c44483d
NiharSaha <nihar333saha000@gmail.com>
2022-01-05 17:47:40 +0100

    Drop type specs for Alignment and EGamma

--- DQMOffline/EGamma/python/electronOfflineClientSequence_cff.py
+++ DQMOffline/EGamma/python/electronOfflineClientSequence_cff.py
@@ -2,31 +2,31 @@ import FWCore.ParameterSet.Config as cms
 
 from DQMOffline.EGamma.electronOfflineClient_cfi import *
 
-dqmElectronClientAllElectrons = dqmElectronOfflineClient.clone() ;
-dqmElectronClientAllElectrons.InputFolderName = cms.string("Egamma/Electrons/Ele2_All") ;
-dqmElectronClientAllElectrons.OutputFolderName = cms.string("Egamma/Electrons/Ele2_All") ;
-
-dqmElectronClientAllElectronsHGC = dqmElectronOfflineClient.clone() ;
-dqmElectronClientAllElectronsHGC.InputFolderName = cms.string("Egamma/Electrons/Ele2HGC_All") ;
-dqmElectronClientAllElectronsHGC.OutputFolderName = cms.string("Egamma/Electrons/Ele2HGC_All") ;
-
-dqmElectronClientSelectionEt = dqmElectronOfflineClient.clone() ;
-dqmElectronClientSelectionEt.InputFolderName = cms.string("Egamma/Electrons/Ele3_Et10") ;
-dqmElectronClientSelectionEt.OutputFolderName = cms.string("Egamma/Electrons/Ele3_Et10") ;
-
-dqmElectronClientSelectionEtIso = dqmElectronOfflineClient.clone() ;
-dqmElectronClientSelectionEtIso.InputFolderName = cms.string("Egamma/Electrons/Ele4_Et10TkIso1") ;
-dqmElectronClientSelectionEtIso.OutputFolderName = cms.string("Egamma/Electrons/Ele4_Et10TkIso1") ;
-
-#dqmElectronClientSelectionEtIsoElID = dqmElectronOfflineClient.clone() ;
-#dqmElectronClientSelectionEtIsoElID.InputFolderName = cms.string("Egamma/Electrons/Ele4_Et10TkIso1ElID") ;
-#dqmElectronClientSelectionEtIsoElID.OutputFolderName = cms.string("Egamma/Electrons/Ele4_Et10TkIso1ElID") ;
-
-dqmElectronClientTagAndProbe = dqmElectronOfflineClient.clone() ;
-dqmElectronClientTagAndProbe.InputFolderName = cms.string("Egamma/Electrons/Ele5_TagAndProbe") ;
-dqmElectronClientTagAndProbe.OutputFolderName = cms.string("Egamma/Electrons/Ele5_TagAndProbe") ;
-dqmElectronClientTagAndProbe.EffHistoTitle = cms.string("")
-
+dqmElectronClientAllElectrons = dqmElectronOfflineClient.clone(
+    InputFolderName = "Egamma/Electrons/Ele2_All",
+    OutputFolderName = "Egamma/Electrons/Ele2_All"
+)
+dqmElectronClientAllElectronsHGC = dqmElectronOfflineClient.clone(
+    InputFolderName = "Egamma/Electrons/Ele2HGC_All",
+    OutputFolderName = "Egamma/Electrons/Ele2HGC_All"
+)
+dqmElectronClientSelectionEt = dqmElectronOfflineClient.clone(
+    InputFolderName = "Egamma/Electrons/Ele3_Et10",
+    OutputFolderName = "Egamma/Electrons/Ele3_Et10"
+)
+dqmElectronClientSelectionEtIso = dqmElectronOfflineClient.clone(
+    InputFolderName = "Egamma/Electrons/Ele4_Et10TkIso1",
+    OutputFolderName = "Egamma/Electrons/Ele4_Et10TkIso1"
+)
+#dqmElectronClientSelectionEtIsoElID = dqmElectronOfflineClient.clone(
+#InputFolderName = "Egamma/Electrons/Ele4_Et10TkIso1ElID",
+#OutputFolderName = "Egamma/Electrons/Ele4_Et10TkIso1ElID"
+#)
+dqmElectronClientTagAndProbe = dqmElectronOfflineClient.clone(
+    InputFolderName = "Egamma/Electrons/Ele5_TagAndProbe",
+    OutputFolderName = "Egamma/Electrons/Ele5_TagAndProbe",
+    EffHistoTitle = ""
+)
 electronOfflineClientSequence = cms.Sequence(
    dqmElectronClientAllElectrons
  * dqmElectronClientSelectionEt
