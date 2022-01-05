612de057f253933b2d479749346d691273a93aa0
Sunanda <sunanda.banerjee@cern.ch>
2022-01-05 17:34:40 +0100

    Try to avoid compilation warnings in some Validation codes in RecoPixelVertexing, RecoVertex, TrackerDigis

--- Validation/RecoVertex/interface/PrimaryVertexAnalyzer4PUSlimmed.h
+++ Validation/RecoVertex/interface/PrimaryVertexAnalyzer4PUSlimmed.h
@@ -26,7 +26,6 @@
 
 // user include files
 #include "FWCore/Framework/interface/Frameworkfwd.h"
-#include "FWCore/Framework/interface/EDAnalyzer.h"
 #include "FWCore/Framework/interface/ESHandle.h"
 
 // math
