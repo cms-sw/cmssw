2eb3268a022bcedb686e1d4ede9dc608485ddf97
Sunanda <sunanda.banerjee@cern.ch>
2022-01-08 20:55:54 +0100

    Take Andrea's suggestions

--- Calibration/HcalAlCaRecoProducers/python/alcaHcalIsotrkProducer_cff.py
+++ Calibration/HcalAlCaRecoProducers/python/alcaHcalIsotrkProducer_cff.py
@@ -5,23 +5,23 @@ from Calibration.HcalAlCaRecoProducers.alcaHcalIsotrkProducer_cfi import *
 from Configuration.Eras.Modifier_run2_ECAL_2017_cff import run2_ECAL_2017
 
 run2_ECAL_2017.toModify(alcaHcalIsotrkProducer,
-  EBHitEnergyThreshold    = cms.double(0.18),
-  EEHitEnergyThreshold0   = cms.double(-206.074),
-  EEHitEnergyThreshold1   = cms.double(357.671),
-  EEHitEnergyThreshold2   = cms.double(-204.978),
-  EEHitEnergyThreshold3   = cms.double(39.033),
-  EEHitEnergyThresholdLow = cms.double(1.25),
-  EEHitEnergyThresholdHigh= cms.double(10.0),
+  EBHitEnergyThreshold    = 0.18,
+  EEHitEnergyThreshold0   = -206.074,
+  EEHitEnergyThreshold1   = 357.671,
+  EEHitEnergyThreshold2   = -204.978,
+  EEHitEnergyThreshold3   = 39.033,
+  EEHitEnergyThresholdLow = 1.25,
+  EEHitEnergyThresholdHigh= 10.0
 )
 
 from Configuration.Eras.Modifier_run2_HCAL_2018_cff import run2_HCAL_2018
 
 run2_HCAL_2018.toModify(alcaHcalIsotrkProducer,
-  EBHitEnergyThreshold    = cms.double(0.10),
-  EEHitEnergyThreshold0   = cms.double(-41.0664),
-  EEHitEnergyThreshold1   = cms.double(68.795),
-  EEHitEnergyThreshold2   = cms.double(-38.1483),
-  EEHitEnergyThreshold3   = cms.double(7.04303),
-  EEHitEnergyThresholdLow = cms.double(0.11),
-  EEHitEnergyThresholdHigh= cms.double(15.4),
+  EBHitEnergyThreshold    = 0.10,
+  EEHitEnergyThreshold0   = -41.0664,
+  EEHitEnergyThreshold1   = 68.795,
+  EEHitEnergyThreshold2   = -38.1483,
+  EEHitEnergyThreshold3   = 7.04303,
+  EEHitEnergyThresholdLow = 0.11,
+  EEHitEnergyThresholdHigh= 15.4
 )
