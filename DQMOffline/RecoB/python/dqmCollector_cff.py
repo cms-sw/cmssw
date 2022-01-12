4eba5e4fc0e763844d1ab0725342c7aadcccd0d4
Nihar Ranjan Saha <56932008+NiharSaha@users.noreply.github.com>
2022-01-12 16:01:28 +0530

    Update dqmCollector_cff.py

--- DQMOffline/RecoB/python/dqmCollector_cff.py
+++ DQMOffline/RecoB/python/dqmCollector_cff.py
@@ -3,15 +3,15 @@ import FWCore.ParameterSet.Config as cms
 from DQMOffline.RecoB.dqmAnalyzer_cff import *
 
 #pf DATA collector
-bTagCollectorDATA = bTagHarvest.clone(ptRanges = (0.0,))
+bTagCollectorDATA = bTagHarvest.clone(ptRanges = [0.0])
 # module execution
 bTagCollectorSequenceDATA = cms.Sequence(bTagCollectorDATA)
 
 #pf MC collector
 bTagCollectorMC = bTagHarvestMC.clone(
     flavPlots = "allbcl", #harvest all, b, c, dusg and ni histos 
-    ptRanges = (0.0,),
-    etaRanges = (0.0,),
+    ptRanges = [0.0],
+    etaRanges = [0.0],
 )
 # module execution
 bTagCollectorSequenceMC = cms.Sequence(bTagCollectorMC)
