b19b29dd17e32b0fd76e865d9934d56fc88457d3
Vladimir <Vladimir.Ivantchenko@cern.ch>
2022-01-16 16:27:10 +0100

    Enable PPS transport

--- SimG4Core/Application/python/g4SimHits_cfi.py
+++ SimG4Core/Application/python/g4SimHits_cfi.py
@@ -632,7 +632,7 @@ run2_HCAL_2017.toModify( g4SimHits, HCalSD = dict( TestNumberingScheme = True ) 
 ##
 from Configuration.Eras.Modifier_run3_common_cff import run3_common
 run3_common.toModify( g4SimHits, CastorSD = dict( useShowerLibrary = False ) ) 
-run3_common.toModify( g4SimHits, LHCTransport = False )
+run3_common.toModify( g4SimHits, LHCTransport = True )
 
 ##
 ## Disable PPS from Run 3 PbPb runs
