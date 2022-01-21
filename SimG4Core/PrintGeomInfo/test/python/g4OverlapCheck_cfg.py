26dd1fa61b88a6de7c03a52dafdff94acf0d2037
Sunanda <sunanda.banerjee@cern.ch>
2022-01-22 00:11:01 +0100

    Modify the cfg's in SimG4Core/PrintGeomInfo for an esay operation

--- SimG4Core/PrintGeomInfo/test/python/g4OverlapCheck_cfg.py
+++ SimG4Core/PrintGeomInfo/test/python/g4OverlapCheck_cfg.py
@@ -1,30 +1,59 @@
-# cmsRun g4OverlapCheck_cfg.py tol=0.01
+###############################################################################
+# Way to use this:
+#   cmsRun g4OverlapCheck_cfg.py geometry=2021 tol=0.1
+#
+#   Options for geometry 2016, 2018, 2021
+#
+###############################################################################
 import FWCore.ParameterSet.Config as cms
+import os, sys, imp, re
 import FWCore.ParameterSet.VarParsing as VarParsing
 
-#from Configuration.Eras.Era_Run2_cff import Run2
-#process = cms.Process('SIM',Run2)
-#process.load('Configuration.Geometry.GeometryExtended2015Reco_cff')
-#process.load('Configuration.Geometry.GeometryExtended2017Reco_cff')
+####################################################################
+### SETUP OPTIONS
+options = VarParsing.VarParsing('standard')
+options.register('geometry',
+                 "2021",
+                  VarParsing.VarParsing.multiplicity.singleton,
+                  VarParsing.VarParsing.varType.string,
+                  "geometry of operations: 2016, 2018, 2021")
+options.register('tol',
+                 0.1,
+                 VarParsing.VarParsing.multiplicity.singleton,
+                 VarParsing.VarParsing.varType.float,
+                 "Tolerance for checking overlaps: 0.0, 0.01, 0.1, 1.0"
+)
 
-from Configuration.Eras.Era_Run3_DDD_cff import Run3_DDD
-process = cms.Process('SIM',Run3_DDD)
-process.load('Configuration.Geometry.GeometryExtended2021Reco_cff')
+### get and parse the command line arguments
+options.parseArguments()
+
+print(options)
+
+####################################################################
+# Use the options
+
+if (options.geometry == "2016"):
+    from Configuration.Eras.Era_Run2_2016_cff import Run2_2016
+    process = cms.Process('G4PrintGeometry',Run2_2016)
+    process.load('Configuration.Geometry.GeometryExtended2016Reco_cff')
+    baseName = 'cms2016'
+elif (options.geometry == "2018"):
+    from Configuration.Eras.Era_Run2_2018_cff import Run2_2018
+    process = cms.Process('G4PrintGeometry',Run2_2018)
+    process.load('Configuration.Geometry.GeometryExtended2018Reco_cff')
+    baseName = 'cms2018'
+else:
+    from Configuration.Eras.Era_Run3_DDD_cff import Run3_DDD
+    process = cms.Process('G4PrintGeometry',Run3_DDD)
+    process.load('Configuration.Geometry.GeometryExtended2021Reco_cff')
+    baseName = 'cms2021'
+
+print("Base file Name: ", baseName)
 
 process.load('FWCore.MessageService.MessageLogger_cfi')
 
 #if hasattr(process,'MessageLogger'):
 #    process.MessageLogger.HCalGeom=dict()
-
-options = VarParsing.VarParsing('standard')
-options.register('tol',
-                 0.1,
-                 VarParsing.VarParsing.multiplicity.singleton,
-                 VarParsing.VarParsing.varType.float,
-                 "Tolerance for checking overlaps: 0.01, 0.1, 1.0"
-)
-options.parseArguments()
-print(options)
 
 from SimG4Core.PrintGeomInfo.g4TestGeometry_cfi import *
 process = checkOverlap(process)
@@ -33,7 +62,7 @@ process = checkOverlap(process)
 process.g4SimHits.CheckGeometry = True
 
 # Geant4 geometry check 
-process.g4SimHits.G4CheckOverlap.OutputBaseName = cms.string("cms2021")
+process.g4SimHits.G4CheckOverlap.OutputBaseName = cms.string(baseName)
 process.g4SimHits.G4CheckOverlap.OverlapFlag = cms.bool(True)
 process.g4SimHits.G4CheckOverlap.Tolerance  = cms.double(options.tol)
 process.g4SimHits.G4CheckOverlap.Resolution = cms.int32(10000)
