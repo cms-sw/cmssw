7fc097bed1de780c10ae3edf032379a0443b8850
Nihar Ranjan Saha <56932008+NiharSaha@users.noreply.github.com>
2022-01-24 20:12:55 +0530

    Update DiDispStaMuonMonitor_cff.py

--- DQMOffline/Trigger/python/DiDispStaMuonMonitor_cff.py
+++ DQMOffline/Trigger/python/DiDispStaMuonMonitor_cff.py
@@ -4,40 +4,40 @@ from DQMOffline.Trigger.DiDispStaMuonMonitor_cfi import hltDiDispStaMuonMonitori
 
 
 hltDiDispStaMuonCosmicMonitoring = hltDiDispStaMuonMonitoring.clone(
-    FolderName = 'HLT/EXO/DiDispStaMuon/DoubleL2Mu23NoVtx_2Cha_CosmicSeed/'
+    FolderName = 'HLT/EXO/DiDispStaMuon/DoubleL2Mu23NoVtx_2Cha_CosmicSeed/',
+    numGenericTriggerEventPSet = dict(hltPaths = ["HLT_DoubleL2Mu23NoVtx_2Cha_CosmicSeed_v*"]) #HLT_ZeroBias_v*
 )
-hltDiDispStaMuonCosmicMonitoring.numGenericTriggerEventPSet.hltPaths = cms.vstring("HLT_DoubleL2Mu23NoVtx_2Cha_CosmicSeed_v*") #HLT_ZeroBias_v*
 ## Efficiency trigger
 hltDispStaMuon23Monitoring = hltDiDispStaMuonMonitoring.clone(
-    FolderName = 'HLT/EXO/DiDispStaMuon/L2Mu23NoVtx_2Cha/'
+    FolderName = 'HLT/EXO/DiDispStaMuon/L2Mu23NoVtx_2Cha/',
+    numGenericTriggerEventPSet = dict(hltPaths = ["HLT_L2Mu23NoVtx_2Cha_v*"]) #HLT_ZeroBias_v*
 )
-hltDispStaMuon23Monitoring.numGenericTriggerEventPSet.hltPaths = cms.vstring("HLT_L2Mu23NoVtx_2Cha_v*") #HLT_ZeroBias_v*
 
 hltDispStaMuon23CosmicMonitoring = hltDiDispStaMuonMonitoring.clone(
-    FolderName = 'HLT/EXO/DiDispStaMuon/L2Mu23NoVtx_2Cha_CosmicSeed/'
+    FolderName = 'HLT/EXO/DiDispStaMuon/L2Mu23NoVtx_2Cha_CosmicSeed/',
+    numGenericTriggerEventPSet = dict(hltPaths = ["HLT_L2Mu23NoVtx_2Cha_CosmicSeed_v*"]) #HLT_ZeroBias_v*
 )
-hltDispStaMuon23CosmicMonitoring.numGenericTriggerEventPSet.hltPaths = cms.vstring("HLT_L2Mu23NoVtx_2Cha_CosmicSeed_v*") #HLT_ZeroBias_v*
 
 ## Backup trigger
 hltDiDispStaMuon25Monitoring = hltDiDispStaMuonMonitoring.clone(
-    FolderName = 'HLT/EXO/DiDispStaMuon/DoubleL2Mu25NoVtx_2Cha/'
+    FolderName = 'HLT/EXO/DiDispStaMuon/DoubleL2Mu25NoVtx_2Cha/',
+    numGenericTriggerEventPSet = dict(hltPaths = ["HLT_DoubleL2Mu25NoVtx_2Cha_v*"]) #HLT_ZeroBias_v*
 )
-hltDiDispStaMuon25Monitoring.numGenericTriggerEventPSet.hltPaths = cms.vstring("HLT_DoubleL2Mu25NoVtx_2Cha_v*") #HLT_ZeroBias_v*
 
 hltDiDispStaMuon25CosmicMonitoring = hltDiDispStaMuonMonitoring.clone(
-    FolderName = 'HLT/EXO/DiDispStaMuon/DoubleL2Mu25NoVtx_2Cha_CosmicSeed/'
+    FolderName = 'HLT/EXO/DiDispStaMuon/DoubleL2Mu25NoVtx_2Cha_CosmicSeed/',
+    numGenericTriggerEventPSet = dict(hltPaths = ["HLT_DoubleL2Mu25NoVtx_2Cha_CosmicSeed_v*"]) #HLT_ZeroBias_v*
 )
-hltDiDispStaMuon25CosmicMonitoring.numGenericTriggerEventPSet.hltPaths = cms.vstring("HLT_DoubleL2Mu25NoVtx_2Cha_CosmicSeed_v*") #HLT_ZeroBias_v*
 
 hltDiDispStaMuon30Monitoring = hltDiDispStaMuonMonitoring.clone(
-    FolderName = 'HLT/EXO/DiDispStaMuon/DoubleL2Mu30NoVtx_2Cha_Eta2p4/'
+    FolderName = 'HLT/EXO/DiDispStaMuon/DoubleL2Mu30NoVtx_2Cha_Eta2p4/',
+    numGenericTriggerEventPSet = dict(hltPaths = ["HLT_DoubleL2Mu30NoVtx_2Cha_Eta2p4_v*"]) #HLT_ZeroBias_v*
 )
-hltDiDispStaMuon30Monitoring.numGenericTriggerEventPSet.hltPaths = cms.vstring("HLT_DoubleL2Mu30NoVtx_2Cha_Eta2p4_v*") #HLT_ZeroBias_v*
 
 hltDiDispStaMuon30CosmicMonitoring = hltDiDispStaMuonMonitoring.clone(
-    FolderName = 'HLT/EXO/DiDispStaMuon/DoubleL2Mu30NoVtx_2Cha_CosmicSeed_Eta2p4/'
+    FolderName = 'HLT/EXO/DiDispStaMuon/DoubleL2Mu30NoVtx_2Cha_CosmicSeed_Eta2p4/',
+    numGenericTriggerEventPSet = dict(hltPaths = ["HLT_DoubleL2Mu30NoVtx_2Cha_CosmicSeed_Eta2p4_v*"]) #HLT_ZeroBias_v*
 )
-hltDiDispStaMuon30CosmicMonitoring.numGenericTriggerEventPSet.hltPaths = cms.vstring("HLT_DoubleL2Mu30NoVtx_2Cha_CosmicSeed_Eta2p4_v*") #HLT_ZeroBias_v*
 
 
 exoHLTdispStaMuonMonitoring = cms.Sequence(
