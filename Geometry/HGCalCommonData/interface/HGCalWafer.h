f9de3484222588153e15720cad44f8808329447c
Sunanda <sunanda.banerjee@cern.ch>
2022-01-25 23:22:40 +0100

    Try to match coding convention

--- Geometry/HGCalCommonData/interface/HGCalWafer.h
+++ Geometry/HGCalCommonData/interface/HGCalWafer.h
@@ -8,33 +8,33 @@ class HGCalWafer {
 public:
   HGCalWafer(double waferSize, int32_t nFine, int32_t nCoarse);
 
-  static constexpr int32_t WaferOrient0 = 0;
-  static constexpr int32_t WaferOrient1 = 1;
-  static constexpr int32_t WaferOrient2 = 2;
-  static constexpr int32_t WaferOrient3 = 3;
-  static constexpr int32_t WaferOrient4 = 4;
-  static constexpr int32_t WaferOrient5 = 5;
+  static constexpr int32_t waferOrient0 = 0;
+  static constexpr int32_t waferOrient1 = 1;
+  static constexpr int32_t waferOrient2 = 2;
+  static constexpr int32_t waferOrient3 = 3;
+  static constexpr int32_t waferOrient4 = 4;
+  static constexpr int32_t waferOrient5 = 5;
 
-  static constexpr int32_t WaferPlacementIndex0 = 0;
-  static constexpr int32_t WaferPlacementIndex1 = 1;
-  static constexpr int32_t WaferPlacementIndex2 = 2;
-  static constexpr int32_t WaferPlacementIndex3 = 3;
-  static constexpr int32_t WaferPlacementIndex4 = 4;
-  static constexpr int32_t WaferPlacementIndex5 = 5;
-  static constexpr int32_t WaferPlacementIndex6 = 6;
-  static constexpr int32_t WaferPlacementIndex7 = 7;
-  static constexpr int32_t WaferPlacementIndex8 = 8;
-  static constexpr int32_t WaferPlacementIndex9 = 9;
-  static constexpr int32_t WaferPlacementIndex10 = 10;
-  static constexpr int32_t WaferPlacementIndex11 = 11;
+  static constexpr int32_t waferPlacementIndex0 = 0;
+  static constexpr int32_t waferPlacementIndex1 = 1;
+  static constexpr int32_t waferPlacementIndex2 = 2;
+  static constexpr int32_t waferPlacementIndex3 = 3;
+  static constexpr int32_t waferPlacementIndex4 = 4;
+  static constexpr int32_t waferPlacementIndex5 = 5;
+  static constexpr int32_t waferPlacementIndex6 = 6;
+  static constexpr int32_t waferPlacementIndex7 = 7;
+  static constexpr int32_t waferPlacementIndex8 = 8;
+  static constexpr int32_t waferPlacementIndex9 = 9;
+  static constexpr int32_t waferPlacementIndex10 = 10;
+  static constexpr int32_t waferPlacementIndex11 = 11;
 
-  static constexpr int32_t WaferPlacementExtra = 6;
-  static constexpr int32_t WaferPlacementOld = 7;
-  static constexpr int32_t WaferPlacementTotal = 12;
+  static constexpr int32_t waferPlacementExtra = 6;
+  static constexpr int32_t waferPlacementOld = 7;
+  static constexpr int32_t waferPlacementTotal = 12;
 
-  static constexpr int32_t CornerCell = 0;
-  static constexpr int32_t TruncatedCell = 1;
-  static constexpr int32_t ExtendedCell = 2;
+  static constexpr int32_t cornerCell = 0;
+  static constexpr int32_t truncatedCell = 1;
+  static constexpr int32_t extendedCell = 2;
 
   std::pair<double, double> HGCalWaferUV2XY1(int32_t u, int32_t v, int32_t placementIndex, int32_t type);
   std::pair<double, double> HGCalWaferUV2XY2(int32_t u, int32_t v, int32_t placementIndex, int32_t type);
@@ -43,8 +43,8 @@ public:
 
 private:
   const double sqrt3By2_ = (0.5 * std::sqrt(3.0));
-  int32_t N_[2];
-  double R_[2], r_[2];
+  int32_t ncell_[2];
+  double cellX_[2], cellY_[2];
 };
 
 #endif
