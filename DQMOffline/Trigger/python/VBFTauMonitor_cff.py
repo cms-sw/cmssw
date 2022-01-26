7ebb94330d9f5595189266befddc15531b69a355
NiharSaha <nihar333saha000@gmail.com>
2022-01-26 07:54:50 +0100

    Update on drop type specs for Trigger

--- DQMOffline/Trigger/python/VBFTauMonitor_cff.py
+++ DQMOffline/Trigger/python/VBFTauMonitor_cff.py
@@ -3,24 +3,20 @@ import FWCore.ParameterSet.Config as cms
 from DQMOffline.Trigger.ObjMonitor_cfi import hltobjmonitoring
 
 # HLT_
-VBFtaumonitoring = hltobjmonitoring.clone()
+VBFtaumonitoring = hltobjmonitoring.clone(
 
-#VBFtaumonitoring.FolderName = cms.string('HLT/Higgs/VBFTau')
-VBFtaumonitoring.FolderName = cms.string('HLT/HIG/VBFTau')
-VBFtaumonitoring.numGenericTriggerEventPSet.hltInputTag   = cms.InputTag( "TriggerResults","","HLT" )
-VBFtaumonitoring.numGenericTriggerEventPSet.hltPaths = cms.vstring(
-  "HLT_VBF_DoubleLooseChargedIsoPFTau20_Trk1_eta2p1_Reg_v*",
-  "HLT_VBF_DoubleMediumChargedIsoPFTau20_Trk1_eta2p1_Reg_v*",
-  "HLT_VBF_DoubleTightChargedIsoPFTau20_Trk1_eta2p1_Reg_v*"
+    #FolderName = 'HLT/Higgs/VBFTau',
+    FolderName = 'HLT/HIG/VBFTau',
+    numGenericTriggerEventPSet = dict(hltInputTag   = "TriggerResults::HLT",
+                                    hltPaths = ["HLT_VBF_DoubleLooseChargedIsoPFTau20_Trk1_eta2p1_Reg_v*",
+                                                "HLT_VBF_DoubleMediumChargedIsoPFTau20_Trk1_eta2p1_Reg_v*",
+                                                "HLT_VBF_DoubleTightChargedIsoPFTau20_Trk1_eta2p1_Reg_v*"]),
+    jetSelection = "pt>40 & abs(eta)<4.7",
+    jetId = "loose",
+    njets = 2,
+
+    denGenericTriggerEventPSet = dict(hltPaths = ["HLT_DoubleMediumChargedIsoPFTau35_Trk1_eta2p1_Reg_v*"])
 )
-VBFtaumonitoring.jetSelection = cms.string("pt>40 & abs(eta)<4.7")
-VBFtaumonitoring.jetId = cms.string("loose")
-VBFtaumonitoring.njets = cms.int32(2)
-
-VBFtaumonitoring.denGenericTriggerEventPSet.hltPaths = cms.vstring(
-  "HLT_DoubleMediumChargedIsoPFTau35_Trk1_eta2p1_Reg_v*"
-)
-
 higgstautauHLTVBFmonitoring = cms.Sequence(
   VBFtaumonitoring
 )
