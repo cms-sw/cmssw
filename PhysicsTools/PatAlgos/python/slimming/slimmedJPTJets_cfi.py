354f5648abe544287e79f7f2cb7c98542fa4589d
Olga Kodolova <Olga.Kodolova@cern.ch>
2022-01-26 11:42:31 +0100

    Add |eta|<2.5 in slimmedJPTJets.py

--- PhysicsTools/PatAlgos/python/slimming/slimmedJPTJets_cfi.py
+++ PhysicsTools/PatAlgos/python/slimming/slimmedJPTJets_cfi.py
@@ -3,5 +3,5 @@ import FWCore.ParameterSet.Config as cms
 slimmedJPTJets = cms.EDProducer("JPTJetSlimmer",
     src = cms.InputTag("JetPlusTrackZSPCorJetAntiKt4"),
     srcCalo = cms.InputTag("slimmedCaloJets"),
-    cut = cms.string("pt>20")
+    cut = cms.string("pt>25 && abs(eta) < 2.5")
 )
