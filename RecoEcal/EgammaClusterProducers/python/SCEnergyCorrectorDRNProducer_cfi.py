99ce2ba039697be23eccf803c0a096250be31c07
Simon Rothman <srothman@mit.edu>
2022-01-28 10:37:04 -0600

    SonicTriton default params

--- RecoEcal/EgammaClusterProducers/python/SCEnergyCorrectorDRNProducer_cfi.py
+++ RecoEcal/EgammaClusterProducers/python/SCEnergyCorrectorDRNProducer_cfi.py
@@ -4,15 +4,10 @@ DRNProducerEB = cms.EDProducer('SCEnergyCorrectorDRNProducer',
    inputSCs = cms.InputTag('particleFlowSuperClusterECAL','particleFlowSuperClusterECALBarrel'),
    Client = cms.PSet(
         mode = cms.string("Async"),
-        preferredServer = cms.untracked.string(""),
-        timeout = cms.untracked.uint32(10),
         modelName = cms.string("MustacheEB"),
-        modelVersion = cms.string(""),
         modelConfigPath = cms.FileInPath("RecoEcal/EgammaClusterProducers/data/models/MustacheEB/config.pbtxt"),
-        verbose = cms.untracked.bool(False),
         allowedTries = cms.untracked.uint32(1),
-        useSharedMemory = cms.untracked.bool(True),
-        compression = cms.untracked.string(""),
+        timeout = cms.untracked.uint32(10),
     ),
  )
 
@@ -21,15 +16,10 @@ DRNProducerEE = cms.EDProducer('SCEnergyCorrectorDRNProducer',
    inputSCs = cms.InputTag('particleFlowSuperClusterECAL','particleFlowSuperClusterECALEndcapWithPreshower'),
    Client = cms.PSet(
         mode = cms.string("Async"),
-        preferredServer = cms.untracked.string(""),
-        timeout = cms.untracked.uint32(10),
         modelName = cms.string('MustacheEE'),
-        modelVersion = cms.string(""),
         modelConfigPath = cms.FileInPath("RecoEcal/EgammaClusterProducers/data/models/MustacheEE/config.pbtxt"),
-        verbose = cms.untracked.bool(False),
         allowedTries = cms.untracked.uint32(1),
-        useSharedMemory = cms.untracked.bool(True),
-        compression = cms.untracked.string(""),
+        timeout = cms.untracked.uint32(10),
     ),
  )
 
