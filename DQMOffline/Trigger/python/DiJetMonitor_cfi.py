b66bf15c536a8f69ecf7109c6033b5bb619768e8
Nihar Ranjan Saha <56932008+NiharSaha@users.noreply.github.com>
2022-01-29 18:18:29 +0530

    Update DiJetMonitor_cfi.py

--- DQMOffline/Trigger/python/DiJetMonitor_cfi.py
+++ DQMOffline/Trigger/python/DiJetMonitor_cfi.py
@@ -8,31 +8,31 @@ DiPFjetAve40_Prommonitoring = dijetMonitoring.clone(
     dijetSrc  = "ak4PFJets", # ak4PFJets, ak4PFJetsCHS
     electrons = "gedGsfElectrons", # while pfIsolatedElectronsEI are reco::PFCandidate !
     muons     = "muons", # while pfIsolatedMuonsEI are reco::PFCandidate !
-    ptcut     = 20 # while pfIsolatedMuonsEI are reco::PFCandidate !
-)
-DiPFjetAve40_Prommonitoring.histoPSet.dijetPSet = dict(
-  nbins = 200 ,
-  xmin  =  0,
-  xmax  = 1000.,
-)
-DiPFjetAve40_Prommonitoring.histoPSet.dijetPtThrPSet = dict(
-  nbins = 50 ,
-  xmin  =  0.,
-  xmax  = 100.,
-)
+    ptcut     = 20, # while pfIsolatedMuonsEI are reco::PFCandidate !
 
-DiPFjetAve40_Prommonitoring.numGenericTriggerEventPSet.andOr         = cms.bool( False )
-DiPFjetAve40_Prommonitoring.numGenericTriggerEventPSet.dbLabel       = cms.string("JetMETDQMTrigger") # it does not exist yet, we should consider the possibility of using the DB, but as it is now it will need a label per path !
-DiPFjetAve40_Prommonitoring.numGenericTriggerEventPSet.andOrHlt      = cms.bool(True)# True:=OR; False:=AND
-DiPFjetAve40_Prommonitoring.numGenericTriggerEventPSet.hltInputTag   = cms.InputTag( "TriggerResults::HLT" )
-DiPFjetAve40_Prommonitoring.numGenericTriggerEventPSet.hltPaths      = cms.vstring("HLT_DiPFJetAve40_v*") # HLT_ZeroBias_v*
-DiPFjetAve40_Prommonitoring.numGenericTriggerEventPSet.errorReplyHlt = cms.bool( False )
-DiPFjetAve40_Prommonitoring.numGenericTriggerEventPSet.verbosityLevel = cms.uint32(1)
+    histoPSet = dict(dijetPSet = dict(
+            nbins = 200 ,
+            xmin  =  0,
+            xmax  = 1000.),
+                     dijetPtThrPSet = dict(
+                             nbins = 50 ,
+                             xmin  =  0.,
+                             xmax  = 100.)),
 
-DiPFjetAve40_Prommonitoring.denGenericTriggerEventPSet.andOr         = cms.bool( False )
-DiPFjetAve40_Prommonitoring.denGenericTriggerEventPSet.dcsInputTag   = cms.InputTag( "scalersRawToDigi" )
-DiPFjetAve40_Prommonitoring.denGenericTriggerEventPSet.dcsPartitions = cms.vint32 ( 24, 25, 26, 27, 28, 29 ) # 24-27: strip, 28-29: pixel, we should add all other detectors !
-DiPFjetAve40_Prommonitoring.denGenericTriggerEventPSet.andOrDcs      = cms.bool( False )
-DiPFjetAve40_Prommonitoring.denGenericTriggerEventPSet.errorReplyDcs = cms.bool( True )
-DiPFjetAve40_Prommonitoring.denGenericTriggerEventPSet.verbosityLevel = cms.uint32(1)
+    numGenericTriggerEventPSet = dict(andOr = False,
+                                      dbLabel = "JetMETDQMTrigger", # it does not exist yet, we should consider the possibility of using the DB, but as it is now it will ne\
+ed a label per path !                                                                                                                                                        
+                                      andOrHlt      = True, # True:=OR; False:=AND                                                                                           
+                                      hltInputTag   = "TriggerResults::HLT" ,
+                                      hltPaths      = ["HLT_DiPFJetAve40_v*"], # HLT_ZeroBias_v*                                                                             
+                                      errorReplyHlt =  False,
+                                      verbosityLevel = 1),
 
+    denGenericTriggerEventPSet = dict(andOr = False,
+                                      dcsInputTag   = "scalersRawToDigi",
+                                      dcsPartitions = [24, 25, 26, 27, 28, 29], # 24-27: strip, 28-29: pixel, we should add all other detectors !                            
+                                      andOrDcs      = False,
+                                      errorReplyDcs = True,
+                                      verbosityLevel = 1)
+
+)
