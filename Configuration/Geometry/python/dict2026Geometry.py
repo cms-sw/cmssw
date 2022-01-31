2ddddb6fbf01cd557652a6c6979311ef1b1f2fa0
Adinda de Wit <adinda.maite.de.wit@cern.ch>
2022-01-31 19:27:04 +0100

    Renaming TX1->T30; add dd4hep TrackerIrregularRingAlgo

--- Configuration/Geometry/python/dict2026Geometry.py
+++ Configuration/Geometry/python/dict2026Geometry.py
@@ -526,7 +526,7 @@ trackerDict = {
         ],
         "era" : "phase2_tracker, phase2_brickedPixels, phase2_squarePixels, trackingPhase2PU140",
     },
-       "TX1" : {
+       "T30" : {
         1 : [
             'Geometry/TrackerCommonData/data/PhaseII/TFPXTEPXReordered/trackerParameters.xml',
             'Geometry/TrackerCommonData/data/pixfwdCommon.xml',
@@ -1747,7 +1747,7 @@ detectorVersionDict = {
     ("O9","T24","C17","M10","F6","I15") : "D88",
     ("O7","T28","C14","M9","F8","I13") : "D89",
     ("O7","T29","C14","M9","F8","I13") : "D90",
-    ("O9","TX1","C17","M10","F6","I15") : "D91",
+    ("O9","T30","C17","M10","F6","I15") : "D91",
  }
 
 deprecatedDets = set([ "D1", "D2", "D3", "D5", "D6" , "D7", "D4", "D8" , "D9", "D12", "D13", "D15", "D10", "D11", "D14", "D16", "D17", "D18", "D19", "D20", "D21", "D22", "D23", "D24", "D25", "D26", "D27", "D28", "D29", "D30", "D31", "D32", "D33", "D34", "D36", "D37", "D38", "D39", "D40", "D42", "D35", "D41", "D43", "D44", "D45", "D46", "D48", "D47", "D50", "D51", "D52", "D53", "D54", "D55", "D56", "D57", "D58", "D59", "D61", "D62", "D63", "D64", "D65", "D66", "D67", "D69", "D71", "D72", "D73", "D74", "D75"])
