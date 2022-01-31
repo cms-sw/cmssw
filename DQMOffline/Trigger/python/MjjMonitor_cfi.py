0a9e8206301e85eb5332870f11f849efbc88c45b
Nihar Ranjan Saha <56932008+NiharSaha@users.noreply.github.com>
2022-01-31 23:14:15 +0530

    Update MjjMonitor_cfi.py

--- DQMOffline/Trigger/python/MjjMonitor_cfi.py
+++ DQMOffline/Trigger/python/MjjMonitor_cfi.py
@@ -13,26 +13,27 @@ hltMjjmonitoring = htMonitoring.clone(
     met       = "pfMetEI",
     jets      = "ak8PFJetsPuppi",
     electrons = "gedGsfElectrons",
-    muons     = "muons"
+    muons     = "muons",
+    
+    histoPSet = dict(htPSet = dict(
+            nbins =  200,
+            xmin  =  -0.5,
+            xmax  = 19999.5)),
+
+    numGenericTriggerEventPSet = dict(
+        andOr         =  False,
+        andOrHlt      = True, # True:=OR; False:=AND
+        hltInputTag   =  "TriggerResults::HLT",
+        hltPaths      = ["HLT_PFMETNoMu120_PFMHTNoMu120_IDTight_v*"],
+        errorReplyHlt = False,
+        verbosityLevel = 0),
+
+    denGenericTriggerEventPSet = dict(
+        andOr         =  False,
+        dcsInputTag   =  "scalersRawToDigi",
+        dcsPartitions = [ 24, 25, 26, 27, 28, 29], # 24-27: strip, 28-29
+        andOrDcs      =  False,
+        errorReplyDcs = True,
+        verbosityLevel = 0,
+        hltPaths      = ["HLT_IsoMu27_v*"])
 )
-hltMjjmonitoring.histoPSet.htPSet = cms.PSet(
-  nbins = cms.uint32 (  200  ),
-  xmin  = cms.double(   -0.5),
-  xmax  = cms.double(19999.5),
-)
-
-hltMjjmonitoring.numGenericTriggerEventPSet.andOr         = cms.bool( False )
-
-hltMjjmonitoring.numGenericTriggerEventPSet.andOrHlt      = cms.bool(True)# True:=OR; False:=AND
-hltMjjmonitoring.numGenericTriggerEventPSet.hltInputTag   = cms.InputTag( "TriggerResults::HLT" )
-hltMjjmonitoring.numGenericTriggerEventPSet.hltPaths      = cms.vstring("HLT_PFMETNoMu120_PFMHTNoMu120_IDTight_v*")
-hltMjjmonitoring.numGenericTriggerEventPSet.errorReplyHlt = cms.bool( False )
-hltMjjmonitoring.numGenericTriggerEventPSet.verbosityLevel = cms.uint32(0)
-
-hltMjjmonitoring.denGenericTriggerEventPSet.andOr         = cms.bool( False )
-hltMjjmonitoring.denGenericTriggerEventPSet.dcsInputTag   = cms.InputTag( "scalersRawToDigi" )
-hltMjjmonitoring.denGenericTriggerEventPSet.dcsPartitions = cms.vint32 ( 24, 25, 26, 27, 28, 29 ) # 24-27: strip, 28-29
-hltMjjmonitoring.denGenericTriggerEventPSet.andOrDcs      = cms.bool( False )
-hltMjjmonitoring.denGenericTriggerEventPSet.errorReplyDcs = cms.bool( True )
-hltMjjmonitoring.denGenericTriggerEventPSet.verbosityLevel = cms.uint32(0)
-hltMjjmonitoring.denGenericTriggerEventPSet.hltPaths      = cms.vstring("HLT_IsoMu27_v*")
