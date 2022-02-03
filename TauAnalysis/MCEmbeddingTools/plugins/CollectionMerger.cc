ff041baa287bbf6df5f268c0a7d012473021c0d3
Artur Gottmann <artur.gottmann@desy.de>
2022-02-03 15:04:39 +0100

    MCEmbeddingTools: update CollectionMerger

--- TauAnalysis/MCEmbeddingTools/plugins/CollectionMerger.cc
+++ TauAnalysis/MCEmbeddingTools/plugins/CollectionMerger.cc
@@ -7,6 +7,14 @@
 #include "DataFormats/EcalRecHit/interface/EcalRecHit.h"
 #include "DataFormats/Common/interface/SortedCollection.h"
 
+#include "DataFormats/EgammaReco/interface/SuperClusterFwd.h"
+#include "DataFormats/EgammaReco/interface/SuperCluster.h"
+
+#include "DataFormats/ParticleFlowReco/interface/PFClusterFwd.h"
+#include "DataFormats/ParticleFlowReco/interface/PFCluster.h"
+
+#include "DataFormats/CaloRecHit/interface/CaloCluster.h"
+
 #include "DataFormats/MuonDetId/interface/DTLayerId.h"
 #include "DataFormats/DTRecHit/interface/DTSLRecCluster.h"
 #include "DataFormats/DTRecHit/interface/DTRecHit1DPair.h"
@@ -17,12 +25,28 @@
 #include "DataFormats/RPCRecHit/interface/RPCRecHit.h"
 #include "DataFormats/HcalRecHit/interface/ZDCRecHit.h"
 
+#include "DataFormats/TrackReco/interface/Track.h"
+#include "DataFormats/TrackReco/interface/TrackFwd.h"
+#include "DataFormats/TrackReco/interface/TrackToTrackMap.h"
+
+#include "DataFormats/EgammaReco/interface/ElectronSeed.h"
+#include "DataFormats/ParticleFlowReco/interface/PreId.h"
+
+#include "DataFormats/TrajectorySeed/interface/TrajectorySeed.h"
+
+#include "DataFormats/EgammaCandidates/interface/Conversion.h"
+
+#include "DataFormats/VertexReco/interface/Vertex.h"
+#include "DataFormats/Candidate/interface/VertexCompositeCandidate.h"
+
 #include "DataFormats/Common/interface/RangeMap.h"
 #include "DataFormats/Common/interface/OwnVector.h"
 
 typedef CollectionMerger<edmNew::DetSetVector<SiPixelCluster>, SiPixelCluster> PixelColMerger;
 typedef CollectionMerger<edmNew::DetSetVector<SiStripCluster>, SiStripCluster> StripColMerger;
 
+typedef CollectionMerger<std::vector<reco::ElectronSeed>, reco::ElectronSeed> ElectronSeedColMerger;
+typedef CollectionMerger<std::vector<reco::ElectronSeed>, reco::ElectronSeed> EcalDrivenElectronSeedColMerger;
 typedef CollectionMerger<edm::SortedCollection<EcalRecHit>, EcalRecHit> EcalRecHitColMerger;
 typedef CollectionMerger<edm::SortedCollection<HBHERecHit>, HBHERecHit> HBHERecHitColMerger;
 typedef CollectionMerger<edm::SortedCollection<HFRecHit>, HFRecHit> HFRecHitColMerger;
@@ -33,6 +57,18 @@ typedef CollectionMerger<edm::SortedCollection<ZDCRecHit>, ZDCRecHit> ZDCRecHitC
 typedef CollectionMerger<edm::RangeMap<DTLayerId, edm::OwnVector<DTRecHit1DPair> >, DTRecHit1DPair> DTRecHitColMerger;
 typedef CollectionMerger<edm::RangeMap<CSCDetId, edm::OwnVector<CSCRecHit2D> >, CSCRecHit2D> CSCRecHitColMerger;
 typedef CollectionMerger<edm::RangeMap<RPCDetId, edm::OwnVector<RPCRecHit> >, RPCRecHit> RPCRecHitColMerger;
+
+template <typename T1, typename T2>
+void  CollectionMerger<T1,T2>::willconsume(const edm::ParameterSet& iConfig)
+{
+
+}
+
+template <typename T1, typename T2>
+void  CollectionMerger<T1,T2>::willproduce(std::string instance, std::string alias)
+{
+  produces<MergeCollection>(instance);
+}
 
 // -------- Here Tracker Merger -----------
 template <typename T1, typename T2>
@@ -111,9 +147,110 @@ void CollectionMerger<T1, T2>::fill_output_obj_muonchamber(
   }
 }
 
+
+// -------- Here Electron Seed Merger -----------
+//TODO: move seed merger to TrackMerger class
+template <>
+void  CollectionMerger<std::vector<reco::ElectronSeed>,  reco::ElectronSeed>::willconsume(const edm::ParameterSet& iConfig)
+{
+    //track refs for trackerdriven seeds
+    inputs_fixtrackrefs_ = consumes<TrackToTrackMapnew >( edm::InputTag("generalTracks") );
+    inputs_fixtrackcol_ = consumes<reco::TrackCollection >( edm::InputTag("generalTracks") );
+
+    //sc refs for ecaldriven seeds
+    inputs_scEB_ = consumes<reco::SuperClusterCollection >(edm::InputTag("particleFlowSuperClusterECAL:particleFlowSuperClusterECALBarrel") );
+    inputs_scEE_ = consumes<reco::SuperClusterCollection >(edm::InputTag("particleFlowSuperClusterECAL:particleFlowSuperClusterECALEndcapWithPreshower") );
+}
+
+template <>
+void  CollectionMerger<std::vector<reco::ElectronSeed>,  reco::ElectronSeed>::willproduce(std::string instance, std::string alias)
+{
+    produces<MergeCollection>(instance);
+}
+
+template <typename T1, typename T2>
+void  CollectionMerger<T1,T2>::fill_output_obj_seed(edm::Event& iEvent, std::unique_ptr<MergeCollection > & output, std::vector<edm::Handle<MergeCollection> > &inputCollections)
+{
+  //track to track map for trackerdriven seed fix
+  edm::Handle<TrackToTrackMapnew> track_ref_map;
+  iEvent.getByToken(inputs_fixtrackrefs_, track_ref_map);
+
+  edm::Handle<reco::TrackCollection> track_new_col;
+  iEvent.getByToken(inputs_fixtrackcol_, track_new_col);
+  std::map<reco::TrackRef , reco::TrackRef > simple_track_to_track_map; //I didn't find a more elegant way, so just build a good old fassion std::map
+   for (unsigned abc =0;  abc < track_new_col->size();  ++abc) {
+    reco::TrackRef trackRef(track_new_col, abc);
+    simple_track_to_track_map[((*track_ref_map)[trackRef])[0]] = trackRef;
+  }
+
+  //ECAL barrel supercluster collection
+  edm::Handle<reco::SuperClusterCollection> scEB;
+  iEvent.getByToken(inputs_scEB_,scEB);
+  auto bScEB = scEB->cbegin();
+  auto eScEB = scEB->cend();
+
+  //ECAL endcap supercluster collection
+  edm::Handle<reco::SuperClusterCollection> scEE;
+  iEvent.getByToken(inputs_scEE_,scEE);
+  auto bScEE = scEE->cbegin();
+  auto eScEE = scEE->cend();
+
+  // First merge the two collections again
+  for (auto const & inputCollection : inputCollections){
+    for ( typename MergeCollection::const_iterator seed = inputCollection->begin(); seed!= inputCollection->end(); ++seed ) {
+      T2 newSeed(*seed);
+
+      //fix reference to supercluster if seed is ecal driven
+      if (seed->isEcalDriven()) {
+        const reco::ElectronSeed::CaloClusterRef & seedCcRef(seed->caloCluster());
+        if( seedCcRef.isNonnull() ) {
+
+          //find corresponding supercluster in ECAL barrel and endcap
+          float dx, dy, dz, dr;
+          float drMin = std::numeric_limits<float>::infinity();
+          reco::ElectronSeed::CaloClusterRef ccRefMin;
+          for( auto sc = bScEB; sc != eScEB; ++sc ) {
+            const reco::SuperClusterRef & scRef(reco::SuperClusterRef(scEB,std::distance(bScEB,sc)));
+            dx = fabs(scRef->x()-seedCcRef->x());
+            dy = fabs(scRef->y()-seedCcRef->y());
+            dz = fabs(scRef->z()-seedCcRef->z());
+            dr = sqrt(dx*dx+dy*dy+dz*dz);
+            if ( dr < drMin ) {
+              drMin = dr;
+              ccRefMin = reco::ElectronSeed::CaloClusterRef(scRef);
+            }
+          }
+          for( auto sc = bScEE; sc != eScEE; ++sc ) {
+            const reco::SuperClusterRef & scRef(reco::SuperClusterRef(scEE,std::distance(bScEE,sc)));
+            dx = fabs(scRef->x()-seedCcRef->x());
+            dy = fabs(scRef->y()-seedCcRef->y());
+            dz = fabs(scRef->z()-seedCcRef->z());
+            dr = sqrt(dx*dx+dy*dy+dz*dz);
+            if ( dr < drMin ) {
+              drMin = dr;
+              ccRefMin = reco::ElectronSeed::CaloClusterRef(scRef);
+            }
+          }
+          //set new calocluster if new ref was found
+          if (drMin < 10.0) {
+            newSeed.setCaloCluster(ccRefMin);
+          }
+        }
+      }
+      if (seed->isTrackerDriven()) {
+        const reco::ElectronSeed::CtfTrackRef  & ctfTrackRef(seed->ctfTrack());
+        if ( ctfTrackRef.isNonnull() ){
+          newSeed.setCtfTrack(simple_track_to_track_map[ctfTrackRef]);
+        }
+      }
+      output->push_back(newSeed);
+    }
+  }
+}
+
 // Here some overloaded functions, which are needed such that the right merger function is called for the indivudal Collections
 template <typename T1, typename T2>
-void CollectionMerger<T1, T2>::fill_output_obj(std::unique_ptr<MergeCollection> &output,
+void CollectionMerger<T1, T2>::fill_output_obj(edm::Event& iEvent, std::unique_ptr<MergeCollection> &output,
                                                std::vector<edm::Handle<MergeCollection> > &inputCollections) {
   assert(0);  // CV: make sure general function never gets called;
               //     always use template specializations
@@ -121,75 +258,87 @@ void CollectionMerger<T1, T2>::fill_output_obj(std::unique_ptr<MergeCollection> 
 
 // Start with the Tracker collections
 template <>
-void CollectionMerger<edmNew::DetSetVector<SiPixelCluster>, SiPixelCluster>::fill_output_obj(
+void CollectionMerger<edmNew::DetSetVector<SiPixelCluster>, SiPixelCluster>::fill_output_obj(edm::Event& iEvent,
     std::unique_ptr<MergeCollection> &output, std::vector<edm::Handle<MergeCollection> > &inputCollections) {
   fill_output_obj_tracker(output, inputCollections, true);
 }
 
 template <>
-void CollectionMerger<edmNew::DetSetVector<SiStripCluster>, SiStripCluster>::fill_output_obj(
+void CollectionMerger<edmNew::DetSetVector<SiStripCluster>, SiStripCluster>::fill_output_obj(edm::Event& iEvent,
     std::unique_ptr<MergeCollection> &output, std::vector<edm::Handle<MergeCollection> > &inputCollections) {
   fill_output_obj_tracker(output, inputCollections);
 }
 
 // Next are the Calo entries
 template <>
-void CollectionMerger<edm::SortedCollection<EcalRecHit>, EcalRecHit>::fill_output_obj(
+void CollectionMerger<edm::SortedCollection<EcalRecHit>, EcalRecHit>::fill_output_obj(edm::Event& iEvent,
     std::unique_ptr<MergeCollection> &output, std::vector<edm::Handle<MergeCollection> > &inputCollections) {
   fill_output_obj_calo(output, inputCollections);
 }
 
 template <>
-void CollectionMerger<edm::SortedCollection<HBHERecHit>, HBHERecHit>::fill_output_obj(
+void CollectionMerger<edm::SortedCollection<HBHERecHit>, HBHERecHit>::fill_output_obj(edm::Event& iEvent,
     std::unique_ptr<MergeCollection> &output, std::vector<edm::Handle<MergeCollection> > &inputCollections) {
   fill_output_obj_calo(output, inputCollections);
 }
 
 template <>
-void CollectionMerger<edm::SortedCollection<HFRecHit>, HFRecHit>::fill_output_obj(
+void CollectionMerger<edm::SortedCollection<HFRecHit>, HFRecHit>::fill_output_obj(edm::Event& iEvent,
     std::unique_ptr<MergeCollection> &output, std::vector<edm::Handle<MergeCollection> > &inputCollections) {
   fill_output_obj_calo(output, inputCollections);
 }
 
 template <>
-void CollectionMerger<edm::SortedCollection<HORecHit>, HORecHit>::fill_output_obj(
+void CollectionMerger<edm::SortedCollection<HORecHit>, HORecHit>::fill_output_obj(edm::Event& iEvent,
     std::unique_ptr<MergeCollection> &output, std::vector<edm::Handle<MergeCollection> > &inputCollections) {
   fill_output_obj_calo(output, inputCollections);
 }
 
 template <>
-void CollectionMerger<edm::SortedCollection<CastorRecHit>, CastorRecHit>::fill_output_obj(
+void CollectionMerger<edm::SortedCollection<CastorRecHit>, CastorRecHit>::fill_output_obj(edm::Event& iEvent,
     std::unique_ptr<MergeCollection> &output, std::vector<edm::Handle<MergeCollection> > &inputCollections) {
   fill_output_obj_calo(output, inputCollections);
 }
 
 template <>
-void CollectionMerger<edm::SortedCollection<ZDCRecHit>, ZDCRecHit>::fill_output_obj(
+void CollectionMerger<edm::SortedCollection<ZDCRecHit>, ZDCRecHit>::fill_output_obj(edm::Event& iEvent,
     std::unique_ptr<MergeCollection> &output, std::vector<edm::Handle<MergeCollection> > &inputCollections) {
   fill_output_obj_calo(output, inputCollections);
 }
 
 // Here the Muon Chamber
 template <>
-void CollectionMerger<edm::RangeMap<DTLayerId, edm::OwnVector<DTRecHit1DPair> >, DTRecHit1DPair>::fill_output_obj(
+void CollectionMerger<edm::RangeMap<DTLayerId, edm::OwnVector<DTRecHit1DPair> >, DTRecHit1DPair>::fill_output_obj(edm::Event& iEvent,
     std::unique_ptr<MergeCollection> &output, std::vector<edm::Handle<MergeCollection> > &inputCollections) {
   fill_output_obj_muonchamber(output, inputCollections);
 }
 
 template <>
-void CollectionMerger<edm::RangeMap<CSCDetId, edm::OwnVector<CSCRecHit2D> >, CSCRecHit2D>::fill_output_obj(
+void CollectionMerger<edm::RangeMap<CSCDetId, edm::OwnVector<CSCRecHit2D> >, CSCRecHit2D>::fill_output_obj(edm::Event& iEvent,
     std::unique_ptr<MergeCollection> &output, std::vector<edm::Handle<MergeCollection> > &inputCollections) {
   fill_output_obj_muonchamber(output, inputCollections);
 }
 
 template <>
-void CollectionMerger<edm::RangeMap<RPCDetId, edm::OwnVector<RPCRecHit> >, RPCRecHit>::fill_output_obj(
+void CollectionMerger<edm::RangeMap<RPCDetId, edm::OwnVector<RPCRecHit> >, RPCRecHit>::fill_output_obj(edm::Event& iEvent,
     std::unique_ptr<MergeCollection> &output, std::vector<edm::Handle<MergeCollection> > &inputCollections) {
   fill_output_obj_muonchamber(output, inputCollections);
 }
+
+// Here Electron Seeds
+template <>
+void  CollectionMerger<std::vector<reco::ElectronSeed>,  reco::ElectronSeed>::fill_output_obj(edm::Event& iEvent,std::unique_ptr<MergeCollection > & output, std::vector<edm::Handle<MergeCollection> > &inputCollections)
+{
+  fill_output_obj_seed(iEvent, output,inputCollections);
+}
+
+
 
 DEFINE_FWK_MODULE(PixelColMerger);
 DEFINE_FWK_MODULE(StripColMerger);
+
+DEFINE_FWK_MODULE(ElectronSeedColMerger);
+DEFINE_FWK_MODULE(EcalDrivenElectronSeedColMerger);
 
 DEFINE_FWK_MODULE(EcalRecHitColMerger);
 DEFINE_FWK_MODULE(HBHERecHitColMerger);
--- TauAnalysis/MCEmbeddingTools/plugins/CollectionMerger.h
+++ TauAnalysis/MCEmbeddingTools/plugins/CollectionMerger.h
@@ -1,12 +1,12 @@
 /** \class CollectionMerger
  *
- * 
+ *
  * \author Stefan Wayand;
  *         Christian Veelken, LLR
  *
- * 
  *
- * 
+ *
+ *
  *
  */
 #ifndef TauAnalysis_MCEmbeddingTools_CollectionMerger_H
@@ -19,6 +19,16 @@
 #include "DataFormats/PatCandidates/interface/Muon.h"
 #include "DataFormats/MuonReco/interface/MuonEnergy.h"
 #include "FWCore/Framework/interface/MakerMacros.h"
+
+#include "DataFormats/EgammaReco/interface/SuperClusterFwd.h"
+#include "DataFormats/EgammaReco/interface/SuperCluster.h"
+
+#include "DataFormats/ParticleFlowReco/interface/PFClusterFwd.h"
+#include "DataFormats/ParticleFlowReco/interface/PFCluster.h"
+
+#include "DataFormats/TrackReco/interface/Track.h"
+#include "DataFormats/TrackReco/interface/TrackFwd.h"
+#include "DataFormats/TrackReco/interface/TrackToTrackMap.h"
 
 #include "TrackingTools/TrackAssociator/interface/TrackAssociatorParameters.h"
 #include "TrackingTools/TrackAssociator/interface/TrackDetectorAssociator.h"
@@ -48,7 +58,13 @@ private:
   typedef T2 BaseHit;
   std::map<std::string, std::vector<edm::EDGetTokenT<MergeCollection> > > inputs_;
 
-  void fill_output_obj(std::unique_ptr<MergeCollection> &output,
+  edm::EDGetTokenT<reco::SuperClusterCollection> inputs_scEB_,inputs_scEE_, inputs_SC_;
+
+  typedef edm::ValueMap<reco::TrackRefVector> TrackToTrackMapnew;
+  edm::EDGetTokenT<TrackToTrackMapnew > inputs_fixtrackrefs_;
+  edm::EDGetTokenT<reco::TrackCollection > inputs_fixtrackcol_;
+
+  void fill_output_obj(edm::Event&, std::unique_ptr<MergeCollection> &output,
                        std::vector<edm::Handle<MergeCollection> > &inputCollections);
   void fill_output_obj_tracker(std::unique_ptr<MergeCollection> &output,
                                std::vector<edm::Handle<MergeCollection> > &inputCollections,
@@ -57,6 +73,12 @@ private:
                             std::vector<edm::Handle<MergeCollection> > &inputCollections);
   void fill_output_obj_muonchamber(std::unique_ptr<MergeCollection> &output,
                                    std::vector<edm::Handle<MergeCollection> > &inputCollections);
+  //seed merger
+  void fill_output_obj_seed(edm::Event&, std::unique_ptr<MergeCollection > & output, std::vector<edm::Handle<MergeCollection> > &inputCollections);
+
+  void willproduce(std::string instance, std::string alias);
+  void willconsume(const edm::ParameterSet& iConfig);
+
 };
 
 template <typename T1, typename T2>
@@ -65,9 +87,11 @@ CollectionMerger<T1, T2>::CollectionMerger(const edm::ParameterSet &iConfig) {
   for (auto const &inCollection : inCollections) {
     inputs_[inCollection.instance()].push_back(consumes<MergeCollection>(inCollection));
   }
+  willconsume(iConfig);
   for (const auto &toproduce : inputs_) {
+    std::string alias( iConfig.getParameter<std::string>( "@module_label" ) );
     //  std::cout<<toproduce.first<<"\t"<<toproduce.second.size()<<std::endl;
-    produces<MergeCollection>(toproduce.first);
+    willproduce(toproduce.first,alias);
   }
 }
 
@@ -85,7 +109,7 @@ void CollectionMerger<T1, T2>::produce(edm::Event &iEvent, const edm::EventSetup
     for (unsigned id = 0; id < input_.second.size(); id++) {
       iEvent.getByToken(input_.second[id], inputCollections[id]);
     }
-    fill_output_obj(output, inputCollections);
+    fill_output_obj(iEvent, output, inputCollections);
     iEvent.put(std::move(output), input_.first);
   }
 }
