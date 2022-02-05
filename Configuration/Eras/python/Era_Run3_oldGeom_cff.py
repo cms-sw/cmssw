bcdd16b141b2a5f30ea5b251ba99ed1059cc5866
Sunanda <sunanda.banerjee@cern.ch>
2022-02-05 22:00:26 +0100

    Correct the ERA definition

--- Configuration/Eras/python/Era_Run3_oldGeom_cff.py
+++ Configuration/Eras/python/Era_Run3_oldGeom_cff.py
@@ -3,4 +3,4 @@ import FWCore.ParameterSet.Config as cms
 from Configuration.Eras.Era_Run3_cff import Run3
 from Configuration.Eras.Modifier_run3_geomOld_cff import run3_geomOld
 
-Phase2C11 = cms.ModifierChain(Run3, run3_geomOld)
+Run3_oldGeom = cms.ModifierChain(Run3, run3_geomOld)
