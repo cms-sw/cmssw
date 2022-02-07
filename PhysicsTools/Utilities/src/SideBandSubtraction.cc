50c4148bd21c4acc0f8406eb6052f1905c206317
Shahzad Malik Muzaffar <Shahzad.Malik.Muzaffar@cern.ch>
2022-02-07 13:44:20 +0100

    [ROOT6] Fix needed for root master to explicitly pass RooCmdArg

--- PhysicsTools/Utilities/src/SideBandSubtraction.cc
+++ PhysicsTools/Utilities/src/SideBandSubtraction.cc
@@ -412,7 +412,7 @@ int SideBandSubtract::doGlobalFit() {
     cout << "Beginning SideBand Subtraction\n";
 
   if (ModelPDF != nullptr && Data != nullptr && SeparationVariable != nullptr) {
-    fit_result = ModelPDF->fitTo(*Data, "r");
+    fit_result = ModelPDF->fitTo(*Data, RooCmdArg("r", 0));
   } else {
     cerr << "ERROR: doGobalFit, no ModelPDF, SeparationVariable or Data specified\n";
     return -1;
