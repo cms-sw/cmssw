3b754c9e2496583ef3cc2800aae88ec57e635c0f
Alejandro Sanchez <alejandro.sanchez@cern.ch>
2022-02-15 23:26:59 +0100

    add new modifier specific to gpu validation

---
+++ Configuration/ProcessModifiers/python/gpuValidation_cff.py
@@ -0,0 +1,5 @@
+import FWCore.ParameterSet.Config as cms
+
+# This modifier is for turning on gpu validation DQM modules
+
+gpuValidation =  cms.Modifier()
--- DQM/EcalMonitorTasks/python/EcalMonitorTask_cff.py
+++ DQM/EcalMonitorTasks/python/EcalMonitorTask_cff.py
@@ -3,9 +3,9 @@ import FWCore.ParameterSet.Config as cms
 from DQM.EcalMonitorTasks.EcalMonitorTask_cfi import *
 
 # Customization to run the CPU vs GPU comparison task if the job runs on a GPU enabled machine
-from Configuration.ProcessModifiers.gpu_cff import gpu
+from Configuration.ProcessModifiers.gpuValidation_cff import gpuValidation
 from DQM.EcalMonitorTasks.GpuTask_cfi import ecalGpuTask
 
-gpu.toModify(ecalGpuTask.params, runGpuTask = cms.untracked.bool(True))
-gpu.toModify(ecalMonitorTask.workers, func = lambda workers: workers.append("GpuTask"))
-gpu.toModify(ecalMonitorTask, workerParameters = dict(GpuTask = ecalGpuTask))
+gpuValidation.toModify(ecalGpuTask.params, runGpuTask = cms.untracked.bool(True))
+gpuValidation.toModify(ecalMonitorTask.workers, func = lambda workers: workers.append("GpuTask"))
+gpuValidation.toModify(ecalMonitorTask, workerParameters = dict(GpuTask = ecalGpuTask))
