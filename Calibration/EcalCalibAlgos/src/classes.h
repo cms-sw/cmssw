d28ffa75da60a59b39e44d914de16ef5029ef774
Simone Pigazzi <simone.pigazzini@live.it>
2022-02-17 00:07:54 +0100

    Add EcalPhiSym workflow for Run3

---
+++ Calibration/EcalCalibAlgos/src/classes.h
@@ -0,0 +1,18 @@
+#include "DataFormats/Common/interface/Wrapper.h"
+
+#include "Calibration/EcalCalibAlgos/interface/EcalPhiSymRecHit.h"
+#include "Calibration/EcalCalibAlgos/interface/EcalPhiSymInfo.h"
+
+namespace
+{
+    struct dictionary
+    {
+        EcalPhiSymRecHit dummy11;
+        std::vector<EcalPhiSymRecHit> dummy12;
+        edm::Wrapper<EcalPhiSymRecHitCollection> dummy13;
+
+        EcalPhiSymInfo dummy21;
+        std::vector<EcalPhiSymInfo> dummy22;
+        edm::Wrapper<EcalPhiSymInfo> dummy23;
+    };
+}
