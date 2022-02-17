67017b4fec9bd1a44aa6d98cc79b436734a5c7db
Alejandro Sanchez <alejandro.sanchez@cern.ch>
2022-02-17 16:43:50 +0100

    replace gpuValidation modifier with modifier chain and create ECAL specific modifier

--- DQM/EcalMonitorTasks/python/EcalMonitorTask_cff.py
+++ DQM/EcalMonitorTasks/python/EcalMonitorTask_cff.py
@@ -3,9 +3,9 @@ import FWCore.ParameterSet.Config as cms
 from DQM.EcalMonitorTasks.EcalMonitorTask_cfi import *
 
 # Customization to run the CPU vs GPU comparison task if the job runs on a GPU enabled machine
-from Configuration.ProcessModifiers.gpuValidation_cff import gpuValidation
+from Configuration.ProcessModifiers.gpuValidationEcal_cff import gpuValidationEcal
 from DQM.EcalMonitorTasks.GpuTask_cfi import ecalGpuTask
 
-gpuValidation.toModify(ecalGpuTask.params, runGpuTask = cms.untracked.bool(True))
-gpuValidation.toModify(ecalMonitorTask.workers, func = lambda workers: workers.append("GpuTask"))
-gpuValidation.toModify(ecalMonitorTask, workerParameters = dict(GpuTask = ecalGpuTask))
+gpuValidationEcal.toModify(ecalGpuTask.params, runGpuTask = cms.untracked.bool(True))
+gpuValidationEcal.toModify(ecalMonitorTask.workers, func = lambda workers: workers.append("GpuTask"))
+gpuValidationEcal.toModify(ecalMonitorTask, workerParameters = dict(GpuTask = ecalGpuTask))
