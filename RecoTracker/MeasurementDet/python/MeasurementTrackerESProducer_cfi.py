bee6bf9e54985257a3a7ee193cc62f33f9c2fbc8
mmusich <marco.musich@cern.ch>
2022-02-17 14:24:08 +0100

    avoid explicit python types in the configuration when using toModify()

--- RecoTracker/MeasurementDet/python/MeasurementTrackerESProducer_cfi.py
+++ RecoTracker/MeasurementDet/python/MeasurementTrackerESProducer_cfi.py
@@ -6,14 +6,13 @@ MeasurementTracker = _MeasurementTrackerESProducer_default.clone()
 
 from Configuration.Eras.Modifier_trackingPhase2PU140_cff import trackingPhase2PU140
 trackingPhase2PU140.toModify(MeasurementTracker, 
-                             Phase2StripCPE = cms.string('Phase2StripCPE'),
-                             #StripCPE = cms.string(''),
-                             UseStripModuleQualityDB = cms.bool(False),
-                             UseStripAPVFiberQualityDB = cms.bool(False),
-                             MaskBadAPVFibers = cms.bool(False),
-                             UseStripStripQualityDB = cms.bool(False),
-                             )
+                             Phase2StripCPE = 'Phase2StripCPE',
+                             #StripCPE = '',
+                             UseStripModuleQualityDB = False,
+                             UseStripAPVFiberQualityDB = False,
+                             MaskBadAPVFibers = False,
+                             UseStripStripQualityDB = False)
 
 from Configuration.Eras.Modifier_phase2_brickedPixels_cff import phase2_brickedPixels
-phase2_brickedPixels.toModify(MeasurementTracker, PixelCPE = cms.string('PixelCPEGenericForBricked'))
+phase2_brickedPixels.toModify(MeasurementTracker, PixelCPE = 'PixelCPEGenericForBricked')
 
--- RecoTracker/TransientTrackingRecHit/python/TransientTrackingRecHitBuilder_cfi.py
+++ RecoTracker/TransientTrackingRecHit/python/TransientTrackingRecHitBuilder_cfi.py
@@ -2,6 +2,7 @@ import FWCore.ParameterSet.Config as cms
 
 ttrhbwr = cms.ESProducer("TkTransientTrackingRecHitBuilderESProducer",
     StripCPE = cms.string('StripCPEfromTrackAngle'),
+    Phase2StripCPE = cms.string(''),
     ComponentName = cms.string('WithTrackAngle'),
     PixelCPE = cms.string('PixelCPEGeneric'),
     Matcher = cms.string('StandardMatcher'),
@@ -10,10 +11,9 @@ ttrhbwr = cms.ESProducer("TkTransientTrackingRecHitBuilderESProducer",
 
 from Configuration.Eras.Modifier_trackingPhase2PU140_cff import trackingPhase2PU140
 trackingPhase2PU140.toModify(ttrhbwr, 
-                             Phase2StripCPE = cms.string('Phase2StripCPE'),
-                             StripCPE = cms.string('FakeStripCPE')
-                             )
+                             Phase2StripCPE = 'Phase2StripCPE',
+                             StripCPE = 'FakeStripCPE')
 
 from Configuration.Eras.Modifier_phase2_brickedPixels_cff import phase2_brickedPixels
-phase2_brickedPixels.toModify(ttrhbwr, PixelCPE = cms.string('PixelCPEGenericForBricked'))
+phase2_brickedPixels.toModify(ttrhbwr, PixelCPE = 'PixelCPEGenericForBricked')
 
