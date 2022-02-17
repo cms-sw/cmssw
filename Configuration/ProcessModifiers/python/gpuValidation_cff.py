4823c3577b464b49c0bed9ccb51f6e749dceebe1
Long <long.wang@cern.ch>
2022-02-17 10:59:39 +0100

    HcalDQM: GPU vs CPU comparison task
    scram build code-checks
    
    Using gpu modifier as a switch to turn on GPU monitoring task
    
    Conditioning the denominator equals case for depth plots
    
    fixing all denominator equals zero cases
    
    Add gpuValidationHcal modifier
    
    naming convention, fillDescriptions(), and header cleanup

--- Configuration/ProcessModifiers/python/gpuValidation_cff.py
+++ Configuration/ProcessModifiers/python/gpuValidation_cff.py
@@ -2,10 +2,12 @@ import FWCore.ParameterSet.Config as cms
 
 from Configuration.ProcessModifiers.gpu_cff import *
 from Configuration.ProcessModifiers.gpuValidationEcal_cff import *
+from Configuration.ProcessModifiers.gpuValidationHcal_cff import *
 
 # This modifier chain is for turning on DQM modules used for gpu validation
 
 gpuValidation =  cms.ModifierChain(
     gpu,
-    gpuValidationEcal
+    gpuValidationEcal,
+    gpuValidationHcal
 )
