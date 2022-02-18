e0e95a05b58cd6974642886eac68f5c16c4496a1
Martin Grunewald <Martin.Grunewald@cern.ch>
2022-02-18 15:42:38 +0100

    HLT menu migration to CMSSW_12_3_0_pre5 template

--- HLTrigger/Configuration/test/OnLine_HLT_Fake1.py
+++ HLTrigger/Configuration/test/OnLine_HLT_Fake1.py
@@ -1,13 +1,13 @@
 # hltGetConfiguration --full --data /dev/CMSSW_12_3_0/Fake1 --type Fake1 --unprescale --process HLTFake1 --globaltag auto:run2_hlt_Fake1 --input file:RelVal_Raw_Fake1_DATA.root
 
-# /dev/CMSSW_12_3_0/Fake1/V5 (CMSSW_12_3_0_pre4)
+# /dev/CMSSW_12_3_0/Fake1/V6 (CMSSW_12_3_0_pre5)
 
 import FWCore.ParameterSet.Config as cms
 
 process = cms.Process( "HLTFake1" )
 
 process.HLTConfigVersion = cms.PSet(
-  tableName = cms.string('/dev/CMSSW_12_3_0/Fake1/V5')
+  tableName = cms.string('/dev/CMSSW_12_3_0/Fake1/V6')
 )
 
 process.streams = cms.PSet(  A = cms.vstring( 'InitialPD' ) )
@@ -336,7 +336,7 @@ process.dqmOutput = cms.OutputModule("DQMRootOutputModule",
     fileName = cms.untracked.string("DQMIO.root")
 )
 
-process.DQMOutput = cms.EndPath( process.dqmOutput )
+process.DQMOutput = cms.FinalPath( process.dqmOutput )
 
 
 
