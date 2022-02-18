0c8775a60a3dab626db6e21cc4cf609129cdfca8
Sanghyun Ko <sanghyun.ko@cern.ch>
2022-02-19 07:09:50 +0900

    improve code formats for private members

--- Validation/EventGenerator/plugins/LheWeightValidation.cc
+++ Validation/EventGenerator/plugins/LheWeightValidation.cc
@@ -8,32 +8,32 @@ using namespace edm;
 
 LheWeightValidation::LheWeightValidation(const edm::ParameterSet& iPSet) {
   lheLabel_ = iPSet.getParameter<edm::InputTag>("lheProduct"),
-  genParticleToken = consumes<reco::GenParticleCollection>(iPSet.getParameter<edm::InputTag>("genParticles"));
-  lheEvtToken = consumes<LHEEventProduct>(lheLabel_);
-  lheRunToken = consumes<LHERunInfoProduct, edm::InRun>(lheLabel_);
-  genJetToken = consumes<reco::GenJetCollection>(iPSet.getParameter<edm::InputTag>("genJets"));
-  dumpLHEheader = iPSet.getParameter<bool>("dumpLHEheader");
-  nScaleVar = iPSet.getParameter<int>("nScaleVar");
-  idxPdfStart = iPSet.getParameter<int>("idxPdfStart");
-  idxPdfEnd = iPSet.getParameter<int>("idxPdfEnd");
-  leadLepPtRange = iPSet.getParameter<double>("leadLepPtRange");
-  leadLepPtNbin = iPSet.getParameter<int>("leadLepPtNbin");
-  leadLepPtCut = iPSet.getParameter<double>("leadLepPtCut");
-  lepEtaCut = iPSet.getParameter<double>("lepEtaCut");
-  rapidityRange = iPSet.getParameter<double>("rapidityRange");
-  rapidityNbin = iPSet.getParameter<int>("rapidityNbin");
-  jetPtCut = iPSet.getParameter<double>("jetPtCut");
-  jetEtaCut = iPSet.getParameter<double>("jetEtaCut");
-  nJetsNbin = iPSet.getParameter<int>("nJetsNbin");
-  jetPtRange = iPSet.getParameter<double>("jetPtRange");
-  jetPtNbin = iPSet.getParameter<int>("jetPtNbin");
+  genParticleToken_ = consumes<reco::GenParticleCollection>(iPSet.getParameter<edm::InputTag>("genParticles"));
+  lheEvtToken_ = consumes<LHEEventProduct>(lheLabel_);
+  lheRunToken_ = consumes<LHERunInfoProduct, edm::InRun>(lheLabel_);
+  genJetToken_ = consumes<reco::GenJetCollection>(iPSet.getParameter<edm::InputTag>("genJets"));
+  dumpLHEheader_ = iPSet.getParameter<bool>("dumpLHEheader");
+  nScaleVar_ = iPSet.getParameter<int>("nScaleVar");
+  idxPdfStart_ = iPSet.getParameter<int>("idxPdfStart");
+  idxPdfEnd_ = iPSet.getParameter<int>("idxPdfEnd");
+  leadLepPtRange_ = iPSet.getParameter<double>("leadLepPtRange");
+  leadLepPtNbin_ = iPSet.getParameter<int>("leadLepPtNbin");
+  leadLepPtCut_ = iPSet.getParameter<double>("leadLepPtCut");
+  lepEtaCut_ = iPSet.getParameter<double>("lepEtaCut");
+  rapidityRange_ = iPSet.getParameter<double>("rapidityRange");
+  rapidityNbin_ = iPSet.getParameter<int>("rapidityNbin");
+  jetPtCut_ = iPSet.getParameter<double>("jetPtCut");
+  jetEtaCut_ = iPSet.getParameter<double>("jetEtaCut");
+  nJetsNbin_ = iPSet.getParameter<int>("nJetsNbin");
+  jetPtRange_ = iPSet.getParameter<double>("jetPtRange");
+  jetPtNbin_ = iPSet.getParameter<int>("jetPtNbin");
 
-  nPdfVar = idxPdfEnd - idxPdfStart + 1;
+  nPdfVar_ = idxPdfEnd_ - idxPdfStart_ + 1;
 }
 
 LheWeightValidation::~LheWeightValidation() {}
 
-void LheWeightValidation::bookHistograms(DQMStore::IBooker& i, edm::Run const& iRun, edm::EventSetup const&) {
+void LheWeightValidation::bookHistograms(DQMStore::IBooker& iBook, edm::Run const& iRun, edm::EventSetup const&) {
   // check LHE product exists
   edm::Handle<LHERunInfoProduct> lheInfo;
   iRun.getByLabel(lheLabel_, lheInfo);
@@ -43,62 +43,62 @@ void LheWeightValidation::bookHistograms(DQMStore::IBooker& i, edm::Run const& i
 
   ///Setting the DQM top directories
   std::string folderName = "Generator/LHEWeight";
-  dqm = new DQMHelper(&i);
-  i.setCurrentFolder(folderName);
+  dqm_ = new DQMHelper(&iBook);
+  iBook.setCurrentFolder(folderName);
 
   // Number of analyzed events
-  nEvt = dqm->book1dHisto("nEvt", "n analyzed Events", 1, 0., 1., "bin", "Number of Events");
-  nlogWgt = dqm->book1dHisto("nlogWgt", "Log10(n weights)", 100, 0., 5., "log_{10}(nWgts)", "Number of Events");
-  wgtVal = dqm->book1dHisto("wgtVal", "weights", 100, -1.5, 3., "weight", "Number of Weights");
+  nEvt_ = dqm_->book1dHisto("nEvt", "n analyzed Events", 1, 0., 1., "bin", "Number of Events");
+  nlogWgt_ = dqm_->book1dHisto("nlogWgt", "Log10(n weights)", 100, 0., 5., "log_{10}(nWgts)", "Number of Events");
+  wgtVal_ = dqm_->book1dHisto("wgtVal", "weights", 100, -1.5, 3., "weight", "Number of Weights");
 
-  bookTemplates(leadLepPtScaleVar,
-                leadLepPtPdfVar,
-                leadLepPtTemp,
+  bookTemplates(leadLepPtScaleVar_,
+                leadLepPtPdfVar_,
+                leadLepPtTemp_,
                 "leadLepPt",
                 "leading lepton Pt",
-                leadLepPtNbin,
+                leadLepPtNbin_,
                 0.,
-                leadLepPtRange,
+                leadLepPtRange_,
                 "Pt_{l} (GeV)",
                 "Number of Events");
-  bookTemplates(leadLepEtaScaleVar,
-                leadLepEtaPdfVar,
-                leadLepEtaTemp,
+  bookTemplates(leadLepEtaScaleVar_,
+                leadLepEtaPdfVar_,
+                leadLepEtaTemp_,
                 "leadLepEta",
                 "leading lepton #eta",
-                rapidityNbin,
-                -rapidityRange,
-                rapidityRange,
+                rapidityNbin_,
+                -rapidityRange_,
+                rapidityRange_,
                 "#eta_{l}",
                 "Number of Events");
-  bookTemplates(jetMultScaleVar,
-                jetMultPdfVar,
-                jetMultTemp,
+  bookTemplates(jetMultScaleVar_,
+                jetMultPdfVar_,
+                jetMultTemp_,
                 "JetMultiplicity",
                 "Gen jet multiplicity",
-                nJetsNbin,
+                nJetsNbin_,
                 0,
-                nJetsNbin,
+                nJetsNbin_,
                 "n",
                 "Number of Events");
-  bookTemplates(leadJetPtScaleVar,
-                leadJetPtPdfVar,
-                leadJetPtTemp,
+  bookTemplates(leadJetPtScaleVar_,
+                leadJetPtPdfVar_,
+                leadJetPtTemp_,
                 "leadJetPt",
                 "leading Gen jet Pt",
-                jetPtNbin,
+                jetPtNbin_,
                 0.,
-                jetPtRange,
+                jetPtRange_,
                 "Pt_{j} (GeV)",
                 "Number of Events");
-  bookTemplates(leadJetEtaScaleVar,
-                leadJetEtaPdfVar,
-                leadJetEtaTemp,
+  bookTemplates(leadJetEtaScaleVar_,
+                leadJetEtaPdfVar_,
+                leadJetEtaTemp_,
                 "leadJetEta",
                 "leading Gen jet #eta",
-                rapidityNbin,
-                -rapidityRange,
-                rapidityRange,
+                rapidityNbin_,
+                -rapidityRange_,
+                rapidityRange_,
                 "#eta_{j}",
                 "Number of Events");
 
@@ -115,29 +115,29 @@ void LheWeightValidation::bookTemplates(std::vector<std::unique_ptr<TH1F>>& scal
                                         float high,
                                         std::string xtitle,
                                         std::string ytitle) {
-  tmps.push_back(dqm->book1dHisto(name, title, nbin, low, high, xtitle, ytitle));
-  tmps.push_back(dqm->book1dHisto(name + "ScaleUp", title + " scale up", nbin, low, high, xtitle, ytitle));
+  tmps.push_back(dqm_->book1dHisto(name, title, nbin, low, high, xtitle, ytitle));
+  tmps.push_back(dqm_->book1dHisto(name + "ScaleUp", title + " scale up", nbin, low, high, xtitle, ytitle));
   tmps.at(1)->getTH1()->Sumw2(false);
-  tmps.push_back(dqm->book1dHisto(name + "ScaleDn", title + " scale down", nbin, low, high, xtitle, ytitle));
+  tmps.push_back(dqm_->book1dHisto(name + "ScaleDn", title + " scale down", nbin, low, high, xtitle, ytitle));
   tmps.at(2)->getTH1()->Sumw2(false);
-  tmps.push_back(dqm->book1dHisto(
+  tmps.push_back(dqm_->book1dHisto(
       name + "ScaleUp_ratio", "Ratio of " + title + " scale upper envelop / Nominal", nbin, low, high, xtitle, ytitle));
   tmps.at(3)->setEfficiencyFlag();
-  tmps.push_back(dqm->book1dHisto(
+  tmps.push_back(dqm_->book1dHisto(
       name + "ScaleDn_ratio", "Ratio of " + title + " scale lower envelop / Nominal", nbin, low, high, xtitle, ytitle));
   tmps.at(4)->setEfficiencyFlag();
-  tmps.push_back(dqm->book1dHisto(name + "PdfUp", title + " PDF upper RMS", nbin, low, high, xtitle, ytitle));
+  tmps.push_back(dqm_->book1dHisto(name + "PdfUp", title + " PDF upper RMS", nbin, low, high, xtitle, ytitle));
   tmps.at(5)->getTH1()->Sumw2(false);
-  tmps.push_back(dqm->book1dHisto(name + "PdfDn", title + " PDF lower RMS", nbin, low, high, xtitle, ytitle));
+  tmps.push_back(dqm_->book1dHisto(name + "PdfDn", title + " PDF lower RMS", nbin, low, high, xtitle, ytitle));
   tmps.at(6)->getTH1()->Sumw2(false);
-  tmps.push_back(dqm->book1dHisto(
+  tmps.push_back(dqm_->book1dHisto(
       name + "PdfUp_ratio", "Ratio of " + title + " PDF upper RMS / Nominal", nbin, low, high, xtitle, ytitle));
   tmps.at(7)->setEfficiencyFlag();
-  tmps.push_back(dqm->book1dHisto(
+  tmps.push_back(dqm_->book1dHisto(
       name + "PdfDn_ratio", "Ratio of " + title + " PDF lower RMS / Nominal", nbin, low, high, xtitle, ytitle));
   tmps.at(8)->setEfficiencyFlag();
 
-  for (int idx = 0; idx < nScaleVar; idx++) {
+  for (int idx = 0; idx < nScaleVar_; idx++) {
     scaleVar.push_back(
         std::make_unique<TH1F>(std::string(name + "Scale" + std::to_string(idx)).c_str(),
                                std::string(";" + std::string(xtitle) + ";" + std::string(ytitle)).c_str(),
@@ -147,7 +147,7 @@ void LheWeightValidation::bookTemplates(std::vector<std::unique_ptr<TH1F>>& scal
     scaleVar.at(idx)->Sumw2();
   }
 
-  for (int idx = 0; idx < nPdfVar; idx++) {
+  for (int idx = 0; idx < nPdfVar_; idx++) {
     pdfVar.push_back(std::make_unique<TH1F>(std::string(name + "Pdf" + std::to_string(idx)).c_str(),
                                             std::string(";" + std::string(xtitle) + ";" + std::string(ytitle)).c_str(),
                                             nbin,
@@ -157,38 +157,38 @@ void LheWeightValidation::bookTemplates(std::vector<std::unique_ptr<TH1F>>& scal
   }
 }  // to get ratio plots correctly - need to modify PostProcessor_cff.py as well!
 
-void LheWeightValidation::dqmBeginRun(const edm::Run& r, const edm::EventSetup& c) {}
+void LheWeightValidation::dqmBeginRun(const edm::Run&, const edm::EventSetup&) {}
 
 void LheWeightValidation::analyze(const edm::Event& iEvent, const edm::EventSetup& iSetup) {
   edm::Handle<LHEEventProduct> lheEvt;
 
-  if (!lheEvtToken.isUninitialized())
-    iEvent.getByToken(lheEvtToken, lheEvt);
+  if (!lheEvtToken_.isUninitialized())
+    iEvent.getByToken(lheEvtToken_, lheEvt);
 
   if (!lheEvt.isValid())
     return;  // do nothing if there is no LHE product
 
-  orgWgt = lheEvt->originalXWGTUP();
-  weight = orgWgt / std::abs(orgWgt);
-  weights = lheEvt->weights();
+  orgWgt_ = lheEvt->originalXWGTUP();
+  weight_ = orgWgt_ / std::abs(orgWgt_);
+  weights_ = lheEvt->weights();
 
-  nEvt->Fill(0.5, weight);
-  nlogWgt->Fill(std::log10(lheEvt->weights().size()), weight);
+  nEvt_->Fill(0.5, weight_);
+  nlogWgt_->Fill(std::log10(lheEvt->weights().size()), weight_);
 
   for (unsigned idx = 0; idx < lheEvt->weights().size(); idx++)
-    wgtVal->Fill(weights[idx].wgt / orgWgt);
+    wgtVal_->Fill(weights_[idx].wgt / orgWgt_);
 
   edm::Handle<reco::GenParticleCollection> ptcls;
-  iEvent.getByToken(genParticleToken, ptcls);
+  iEvent.getByToken(genParticleToken_, ptcls);
   edm::Handle<reco::GenJetCollection> genjets;
-  iEvent.getByToken(genJetToken, genjets);
+  iEvent.getByToken(genJetToken_, genjets);
 
   std::vector<reco::GenParticleRef> leptons;
 
   for (unsigned iptc = 0; iptc < ptcls->size(); iptc++) {
     reco::GenParticleRef ptc(ptcls, iptc);
     if (ptc->status() == 1 && (std::abs(ptc->pdgId()) == 11 || std::abs(ptc->pdgId()) == 13)) {
-      if (ptc->pt() > leadLepPtCut && std::abs(ptc->eta()) < lepEtaCut)
+      if (ptc->pt() > leadLepPtCut_ && std::abs(ptc->eta()) < lepEtaCut_)
         leptons.push_back(ptc);
     }
   }
@@ -197,8 +197,8 @@ void LheWeightValidation::analyze(const edm::Event& iEvent, const edm::EventSetu
 
   if (!leptons.empty()) {
     reco::GenParticleRef leadLep = leptons.at(0);
-    fillTemplates(leadLepPtScaleVar, leadLepPtPdfVar, leadLepPtTemp, leadLep->pt());
-    fillTemplates(leadLepEtaScaleVar, leadLepEtaPdfVar, leadLepEtaTemp, leadLep->eta());
+    fillTemplates(leadLepPtScaleVar_, leadLepPtPdfVar_, leadLepPtTemp_, leadLep->pt());
+    fillTemplates(leadLepEtaScaleVar_, leadLepEtaPdfVar_, leadLepEtaTemp_, leadLep->eta());
   }
 
   std::vector<reco::GenJetRef> genjetVec;
@@ -206,18 +206,18 @@ void LheWeightValidation::analyze(const edm::Event& iEvent, const edm::EventSetu
   for (unsigned igj = 0; igj < genjets->size(); igj++) {
     reco::GenJetRef genjet(genjets, igj);
 
-    if (genjet->pt() > jetPtCut && std::abs(genjet->eta()) < jetEtaCut)
+    if (genjet->pt() > jetPtCut_ && std::abs(genjet->eta()) < jetEtaCut_)
       genjetVec.push_back(genjet);
   }
 
-  fillTemplates(jetMultScaleVar, jetMultPdfVar, jetMultTemp, (float)genjetVec.size());
+  fillTemplates(jetMultScaleVar_, jetMultPdfVar_, jetMultTemp_, (float)genjetVec.size());
 
   if (!genjetVec.empty()) {
     std::sort(genjetVec.begin(), genjetVec.end(), HepMCValidationHelper::sortByPtRef<reco::GenJetRef>);
 
     auto leadJet = genjetVec.at(0);
-    fillTemplates(leadJetPtScaleVar, leadJetPtPdfVar, leadJetPtTemp, leadJet->pt());
-    fillTemplates(leadJetEtaScaleVar, leadJetEtaPdfVar, leadJetEtaTemp, leadJet->eta());
+    fillTemplates(leadJetPtScaleVar_, leadJetPtPdfVar_, leadJetPtTemp_, leadJet->pt());
+    fillTemplates(leadJetEtaScaleVar_, leadJetEtaPdfVar_, leadJetEtaTemp_, leadJet->eta());
   }
 }  //analyze
 
@@ -225,41 +225,41 @@ void LheWeightValidation::fillTemplates(std::vector<std::unique_ptr<TH1F>>& scal
                                         std::vector<std::unique_ptr<TH1F>>& pdfVar,
                                         std::vector<MonitorElement*>& tmps,
                                         float obs) {
-  tmps.at(0)->Fill(obs, weight);
+  tmps.at(0)->Fill(obs, weight_);
 
-  if (static_cast<int>(weights.size()) >= nScaleVar) {
-    for (int iWgt = 0; iWgt < nScaleVar; iWgt++)
-      scaleVar.at(iWgt)->Fill(obs, weights[iWgt].wgt / orgWgt);
+  if (static_cast<int>(weights_.size()) >= nScaleVar_) {
+    for (int iWgt = 0; iWgt < nScaleVar_; iWgt++)
+      scaleVar.at(iWgt)->Fill(obs, weights_[iWgt].wgt / orgWgt_);
   }
 
-  if (static_cast<int>(weights.size()) >= idxPdfEnd) {
-    for (int iWgt = 0; iWgt < nPdfVar; iWgt++)
-      pdfVar.at(iWgt)->Fill(obs, weights[idxPdfStart + iWgt].wgt / orgWgt);
+  if (static_cast<int>(weights_.size()) >= idxPdfEnd_) {
+    for (int iWgt = 0; iWgt < nPdfVar_; iWgt++)
+      pdfVar.at(iWgt)->Fill(obs, weights_[idxPdfStart_ + iWgt].wgt / orgWgt_);
   }
 }
 
-void LheWeightValidation::dqmEndRun(const edm::Run& iRun, const edm::EventSetup& c) {
-  if (lheRunToken.isUninitialized())
+void LheWeightValidation::dqmEndRun(const edm::Run& iRun, const edm::EventSetup&) {
+  if (lheRunToken_.isUninitialized())
     return;
 
   edm::Handle<LHERunInfoProduct> lheInfo;
-  iRun.getByToken(lheRunToken, lheInfo);
+  iRun.getByToken(lheRunToken_, lheInfo);
 
   if (!lheInfo.isValid())
     return;
 
-  envelop(leadLepPtScaleVar, leadLepPtTemp);
-  pdfRMS(leadLepPtPdfVar, leadLepPtTemp);
-  envelop(leadLepEtaScaleVar, leadLepEtaTemp);
-  pdfRMS(leadLepEtaPdfVar, leadLepEtaTemp);
-  envelop(jetMultScaleVar, jetMultTemp);
-  pdfRMS(jetMultPdfVar, jetMultTemp);
-  envelop(leadJetPtScaleVar, leadJetPtTemp);
-  pdfRMS(leadJetPtPdfVar, leadJetPtTemp);
-  envelop(leadJetEtaScaleVar, leadJetEtaTemp);
-  pdfRMS(leadJetEtaPdfVar, leadJetEtaTemp);
+  envelop(leadLepPtScaleVar_, leadLepPtTemp_);
+  pdfRMS(leadLepPtPdfVar_, leadLepPtTemp_);
+  envelop(leadLepEtaScaleVar_, leadLepEtaTemp_);
+  pdfRMS(leadLepEtaPdfVar_, leadLepEtaTemp_);
+  envelop(jetMultScaleVar_, jetMultTemp_);
+  pdfRMS(jetMultPdfVar_, jetMultTemp_);
+  envelop(leadJetPtScaleVar_, leadJetPtTemp_);
+  pdfRMS(leadJetPtPdfVar_, leadJetPtTemp_);
+  envelop(leadJetEtaScaleVar_, leadJetEtaTemp_);
+  pdfRMS(leadJetEtaPdfVar_, leadJetEtaTemp_);
 
-  if (dumpLHEheader) {
+  if (dumpLHEheader_) {
     for (auto it = lheInfo->headers_begin(); it != lheInfo->headers_end(); it++) {
       std::cout << "Header start" << std::endl;
       std::cout << "Tag: " << it->tag() << std::endl;
