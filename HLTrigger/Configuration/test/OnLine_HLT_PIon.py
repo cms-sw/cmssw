eb27ad7e4176ec2f18820712ab6df02807497926
Marino Missiroli <m.missiroli@cern.ch>
2022-02-21 12:32:19 +0100

    HLT menu development for 12_3_X

--- HLTrigger/Configuration/test/OnLine_HLT_PIon.py
+++ HLTrigger/Configuration/test/OnLine_HLT_PIon.py
@@ -1,6 +1,6 @@
 # hltGetConfiguration --full --data /dev/CMSSW_12_3_0/PIon --type PIon --unprescale --process HLTPIon --globaltag auto:run3_hlt_PIon --input file:RelVal_Raw_PIon_DATA.root
 
-# /dev/CMSSW_12_3_0/PIon/V30 (CMSSW_12_3_0_pre5)
+# /dev/CMSSW_12_3_0/PIon/V36 (CMSSW_12_3_0_pre5)
 
 import FWCore.ParameterSet.Config as cms
 
@@ -9,7 +9,7 @@ from HeterogeneousCore.CUDACore.SwitchProducerCUDA import SwitchProducerCUDA
 process = cms.Process( "HLTPIon" )
 
 process.HLTConfigVersion = cms.PSet(
-  tableName = cms.string('/dev/CMSSW_12_3_0/PIon/V30')
+  tableName = cms.string('/dev/CMSSW_12_3_0/PIon/V36')
 )
 
 process.transferSystem = cms.PSet( 
