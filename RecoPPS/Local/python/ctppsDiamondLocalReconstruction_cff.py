692903e31359508a83292ee87bba62fa9b3bda11
Helena <helena.malbouisson@gmail.com>
2022-02-22 19:08:58 +0100

    add PPS and HCAL tags in GTs

--- RecoPPS/Local/python/ctppsDiamondLocalReconstruction_cff.py
+++ RecoPPS/Local/python/ctppsDiamondLocalReconstruction_cff.py
@@ -1,13 +1,5 @@
 import FWCore.ParameterSet.Config as cms
 
-# Temporary customization of GT - to be removed once the tag is included in the GTs
-from Configuration.StandardSequences.FrontierConditions_GlobalTag_cff import GlobalTag
-GlobalTag.toGet.append(
-  cms.PSet(record = cms.string('PPSTimingCalibrationLUTRcd'),
-           tag = cms.string('PPSDiamondTimingCalibrationLUT_test')
-  )
-)
-
 # reco hit production
 from RecoPPS.Local.ctppsDiamondRecHits_cfi import ctppsDiamondRecHits
 
