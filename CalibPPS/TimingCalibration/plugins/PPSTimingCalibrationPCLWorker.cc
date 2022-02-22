8a9606ead385a2111640eaada57bb2b551a11360
Mateusz <mateuszkocot99@gmail.com>
2022-02-22 16:32:14 +0100

    code-format

--- CalibPPS/TimingCalibration/plugins/PPSTimingCalibrationPCLWorker.cc
+++ CalibPPS/TimingCalibration/plugins/PPSTimingCalibrationPCLWorker.cc
@@ -41,9 +41,11 @@ private:
                       const edm::EventSetup&,
                       TimingCalibrationHistograms&) const override;
 
-  template<typename T>
-  bool searchForProduct(edm::Event const& iEvent, const std::vector<edm::EDGetTokenT<T>>& tokens, 
-                    const std::vector<edm::InputTag>& tags, edm::Handle<T>& handle) const;
+  template <typename T>
+  bool searchForProduct(edm::Event const& iEvent,
+                        const std::vector<edm::EDGetTokenT<T>>& tokens,
+                        const std::vector<edm::InputTag>& tags,
+                        edm::Handle<T>& handle) const;
 
   std::vector<edm::InputTag> RecHitTags_;
   std::vector<edm::EDGetTokenT<edm::DetSetVector<CTPPSDiamondRecHit>>> diamondRecHitTokens_;
@@ -58,11 +60,8 @@ PPSTimingCalibrationPCLWorker::PPSTimingCalibrationPCLWorker(const edm::Paramete
     : RecHitTags_(iConfig.getParameter<std::vector<edm::InputTag>>("diamondRecHitTags")),
       geomEsToken_(esConsumes<edm::Transition::BeginRun>()),
       dqmDir_(iConfig.getParameter<std::string>("dqmDir")) {
-
-
-
-      for (auto& tag : RecHitTags_)
-        diamondRecHitTokens_.push_back(consumes<edm::DetSetVector<CTPPSDiamondRecHit>>(tag));
+  for (auto& tag : RecHitTags_)
+    diamondRecHitTokens_.push_back(consumes<edm::DetSetVector<CTPPSDiamondRecHit>>(tag));
 }
 
 //------------------------------------------------------------------------------
@@ -95,7 +94,6 @@ void PPSTimingCalibrationPCLWorker::bookHistograms(DQMStore::IBooker& iBooker,
 void PPSTimingCalibrationPCLWorker::dqmAnalyze(const edm::Event& iEvent,
                                                const edm::EventSetup& iSetup,
                                                const TimingCalibrationHistograms& iHists) const {
-
   edm::Handle<edm::DetSetVector<CTPPSDiamondRecHit>> dsv_rechits;
   // then extract the rechits information for later processing
   searchForProduct(iEvent, diamondRecHitTokens_, RecHitTags_, dsv_rechits);
@@ -135,23 +133,24 @@ void PPSTimingCalibrationPCLWorker::fillDescriptions(edm::ConfigurationDescripti
   descriptions.addWithDefaultLabel(desc);
 }
 
-template<typename T>
-bool PPSTimingCalibrationPCLWorker::searchForProduct(edm::Event const& iEvent, const std::vector<edm::EDGetTokenT<T>>& tokens, 
-                                                                  const std::vector<edm::InputTag>& tags, edm::Handle<T>& handle) const{
+template <typename T>
+bool PPSTimingCalibrationPCLWorker::searchForProduct(edm::Event const& iEvent,
+                                                     const std::vector<edm::EDGetTokenT<T>>& tokens,
+                                                     const std::vector<edm::InputTag>& tags,
+                                                     edm::Handle<T>& handle) const {
   bool foundProduct = false;
   for (unsigned int i = 0; i < tokens.size(); i++)
     if (auto h = iEvent.getHandle(tokens[i])) {
-      handle=h;
+      handle = h;
       foundProduct = true;
       edm::LogInfo("searchForProduct") << "Found a product with " << tags[i];
       break;
     }
-  
+
   if (!foundProduct)
     throw edm::Exception(edm::errors::ProductNotFound) << "Could not find a product with any of the selected labels.";
 
   return foundProduct;
-  
 }
 
 DEFINE_FWK_MODULE(PPSTimingCalibrationPCLWorker);
