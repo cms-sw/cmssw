8635e1300a2b255e28b698052cd48ebe2aee615f
Kevin Pedro <kpedro88@gmail.com>
2022-02-22 16:47:58 -0600

    code format

--- HeterogeneousCore/SonicCore/interface/SonicAcquirer.h
+++ HeterogeneousCore/SonicCore/interface/SonicAcquirer.h
@@ -18,7 +18,9 @@ public:
   typedef typename Client::Input Input;
   //constructor
   SonicAcquirer(edm::ParameterSet const& cfg)
-      : clientPset_(cfg.getParameterSet("Client")), debugName_(cfg.getParameter<std::string>("@module_label")), verbose_(clientPset_.getUntrackedParameter<bool>("verbose")) {}
+      : clientPset_(cfg.getParameterSet("Client")),
+        debugName_(cfg.getParameter<std::string>("@module_label")),
+        verbose_(clientPset_.getUntrackedParameter<bool>("verbose")) {}
   //destructor
   ~SonicAcquirer() override = default;
 
