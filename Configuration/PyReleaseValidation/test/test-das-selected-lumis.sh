c4ef3f9f25e43f0588a9d069994d78c68c8672e7
Malik Shahzad Muzaffar <shahzad.malik.muzaffar@cern.ch>
2022-02-26 00:24:54 +0100

    Update test-das-selected-lumis.sh

--- Configuration/PyReleaseValidation/test/test-das-selected-lumis.sh
+++ Configuration/PyReleaseValidation/test/test-das-selected-lumis.sh
@@ -1,8 +1,8 @@
 #!/bin/bash -ex
 export CMS_BOT_USE_DASGOCLIENT=true
 QUERY="lumi,file dataset=/HIHardProbes/HIRun2018A-v1/RAW run=326479"
-dasgoclient --limit 0 --query "${QUERY}" --format json | das-selected-lumis.py 1,23 | sort > original.txt
-export CMS_BOT_USE_DASGOCLIENT=false
-dasgoclient --limit 0 --query "${QUERY}" --format json | das-selected-lumis.py 1,23 | sort > cached.txt
-diff -u original.txt cached.txt
-
+dasgoclient --limit 0 --query "${QUERY}" --format json | das-selected-lumis.py 1,23 | grep '^/store/' > original.txt
+cat original.txt
+if [ $(cat original.txt | wc -l) -eq 0 ] ; then
+  exit 1
+fi
