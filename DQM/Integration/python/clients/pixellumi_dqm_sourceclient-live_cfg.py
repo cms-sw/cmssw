0c7099b9adbd8c9c4fcedd2f4ac784edaf86b5c6
pmandrik <petr.mandrik@cern.ch>
2022-02-27 17:17:47 +0100

    migrate processes to Run3

--- DQM/Integration/python/clients/pixellumi_dqm_sourceclient-live_cfg.py
+++ DQM/Integration/python/clients/pixellumi_dqm_sourceclient-live_cfg.py
@@ -2,7 +2,8 @@ from __future__ import print_function
 import FWCore.ParameterSet.Config as cms
 import sys
 
-process = cms.Process("PixelLumiDQM")
+from Configuration.Eras.Era_Run3_cff import Run3
+process = cms.Process("PixelLumiDQM", Run3)
 
 unitTest=False
 if 'unitTest=True' in sys.argv:
