9f77e8a9e54e9ed27bbb9bf0fb729d9ef2d0dccf
namppl <kyungwook.nam@cern.ch>
2022-02-28 10:41:04 +0100

    implemented Michal's comments

--- RecoTauTag/HLTProducers/interface/PFDiJetCorrCheckerWithDiTau.h
+++ RecoTauTag/HLTProducers/interface/PFDiJetCorrCheckerWithDiTau.h
@@ -1,23 +1,18 @@
-#ifndef RecoTauTag_HLTProducers_PFJetsTauCorrelationCondition_H
-#define RecoTauTag_HLTProducers_PFJetsTauCorrelationCondition_H
+#ifndef RecoTauTag_HLTProducers_PFDiJetCorrCheckerWithDiTau_H
+#define RecoTauTag_HLTProducers_PFDiJetCorrCheckerWithDiTau_H
 
 // user include files
 #include "FWCore/Framework/interface/Frameworkfwd.h"
-#include "FWCore/Framework/interface/global/EDProducer.h"
+#include "FWCore/Framework/interface/stream/EDProducer.h"
 #include "FWCore/ParameterSet/interface/ConfigurationDescriptions.h"
 #include "FWCore/ParameterSet/interface/ParameterSetDescription.h"
 #include "FWCore/Framework/interface/Event.h"
 #include "FWCore/ParameterSet/interface/ParameterSet.h"
 #include "FWCore/Utilities/interface/InputTag.h"
 #include "DataFormats/Common/interface/Handle.h"
-#include "DataFormats/L1Trigger/interface/Tau.h"
-#include "DataFormats/JetReco/interface/CaloJetCollection.h"
-#include "DataFormats/TauReco/interface/PFTauFwd.h"
 #include "DataFormats/HLTReco/interface/TriggerFilterObjectWithRefs.h"
-#include "DataFormats/HLTReco/interface/TriggerObject.h"
-#include "DataFormats/HLTReco/interface/TriggerEvent.h"
 
-class PFJetsTauCorrelationCondition : public edm::global::EDProducer<> {
+class PFDiJetCorrCheckerWithDiTau : public edm::stream::EDProducer<> {
 private:
   const edm::EDGetTokenT<trigger::TriggerFilterObjectWithRefs> tauSrc_;
   const edm::EDGetTokenT<reco::PFJetCollection> pfJetSrc_;
@@ -26,9 +21,9 @@ private:
   const double matchingR2_;
 
 public:
-  explicit PFJetsTauCorrelationCondition(const edm::ParameterSet&);
-  ~PFJetsTauCorrelationCondition() override;
-  void produce(edm::StreamID, edm::Event&, const edm::EventSetup&) const override;
+  explicit PFDiJetCorrCheckerWithDiTau(const edm::ParameterSet&);
+  ~PFDiJetCorrCheckerWithDiTau() override;
+  void produce(edm::Event&, const edm::EventSetup&) override;
   static void fillDescriptions(edm::ConfigurationDescriptions& descriptions);
 };
 #endif
