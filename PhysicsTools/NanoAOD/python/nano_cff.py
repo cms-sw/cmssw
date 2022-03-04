0e58eec4bde995370d993aa8db0f31853025859a
Michal Bluj <michal.bluj@cern.ch>
2022-03-04 23:13:11 +0100

    adjust era treatment for boosted taus

--- PhysicsTools/NanoAOD/python/nano_cff.py
+++ PhysicsTools/NanoAOD/python/nano_cff.py
@@ -366,7 +366,8 @@ def nanoAOD_customizeCommon(process):
         idsToAdd = cms.vstring()
     )
     run2_nanoAOD_106Xv2.toModify(nanoAOD_boostedTau_switch, idsToAdd = ["2017v2", "dR0p32017v2", "newDM2017v2","againstEle2018"])
-    (~(run2_nanoAOD_94XMiniAODv1 | run2_nanoAOD_94X2016 | run2_nanoAOD_94XMiniAODv2 | run2_nanoAOD_102Xv1 | run2_nanoAOD_106Xv1)).toModify(process, lambda p : nanoAOD_addBoostedTauIds(p, nanoAOD_boostedTau_switch.idsToAdd.value()))
+    run2_nanoAOD_106Xv2.toModify(process, lambda p : nanoAOD_addBoostedTauIds(p, nanoAOD_boostedTau_switch.idsToAdd.value()))
+
     return process
 
 def nanoAOD_customizeData(process):
