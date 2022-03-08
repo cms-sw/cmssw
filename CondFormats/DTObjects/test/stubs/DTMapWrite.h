b1b71147e9f3f8c6218421cf322276248658f896
yuanchao <yuanchao@cern.ch>
2022-03-08 10:18:08 +0800

    migrate deprecated EDModule for CondFormats/DTObjects

--- CondFormats/DTObjects/test/stubs/DTMapWrite.h
+++ CondFormats/DTObjects/test/stubs/DTMapWrite.h
@@ -5,20 +5,20 @@ Toy EDAnalyzer for testing purposes only.
 
 ----------------------------------------------------------------------*/
 
-#include "FWCore/Framework/interface/EDAnalyzer.h"
+#include "FWCore/Framework/interface/one/EDAnalyzer.h"
 #include "FWCore/Framework/interface/Event.h"
 
 #include "FWCore/Framework/interface/EventSetup.h"
 #include "FWCore/ParameterSet/interface/ParameterSet.h"
 
 namespace edmtest {
-  class DTMapWrite : public edm::EDAnalyzer {
+  class DTMapWrite : public edm::one::EDAnalyzer<> {
   public:
     explicit DTMapWrite(edm::ParameterSet const& p);
     explicit DTMapWrite(int i);
-    virtual ~DTMapWrite();
-    virtual void analyze(const edm::Event& e, const edm::EventSetup& c);
-    virtual void endJob();
+    ~DTMapWrite() override = default;
+    void analyze(const edm::Event& e, const edm::EventSetup& c) override;
+    void endJob() override;
 
   private:
   };
--- CondFormats/DTObjects/test/stubs/DTRangeT0Write.h
+++ CondFormats/DTObjects/test/stubs/DTRangeT0Write.h
@@ -5,20 +5,20 @@ Toy EDAnalyzer for testing purposes only.
 
 ----------------------------------------------------------------------*/
 
-#include "FWCore/Framework/interface/EDAnalyzer.h"
+#include "FWCore/Framework/interface/one/EDAnalyzer.h"
 #include "FWCore/Framework/interface/Event.h"
 
 #include "FWCore/Framework/interface/EventSetup.h"
 #include "FWCore/ParameterSet/interface/ParameterSet.h"
 
 namespace edmtest {
-  class DTRangeT0Write : public edm::EDAnalyzer {
+  class DTRangeT0Write : public edm::one::EDAnalyzer<> {
   public:
     explicit DTRangeT0Write(edm::ParameterSet const& p);
     explicit DTRangeT0Write(int i);
-    virtual ~DTRangeT0Write();
-    virtual void analyze(const edm::Event& e, const edm::EventSetup& c);
-    virtual void endJob();
+    ~DTRangeT0Write() override = default;
+    void analyze(const edm::Event& e, const edm::EventSetup& c) override;
+    void endJob() override;
 
   private:
   };
