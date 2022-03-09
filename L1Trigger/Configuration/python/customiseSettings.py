0e299bba39b1d17bedf5089b1b5f8b475e79ac7b
Aaron Bundock <aaron.bundock@cern.ch>
2022-03-09 18:04:21 +0100

    L1T: add new caloparams using Layer 1 12_0 ECAL SF and HCAL SF = 1

--- L1Trigger/Configuration/python/customiseSettings.py
+++ L1Trigger/Configuration/python/customiseSettings.py
@@ -2,6 +2,10 @@ from __future__ import print_function
 import os.path
 import FWCore.ParameterSet.Config as cms
 
+def L1TSettingsToCaloParams_2022_v0_1(process):
+    process.load("L1Trigger.L1TCalorimeter.caloParams_2022_v0_1_cfi")
+    return process
+
 def L1TSettingsToCaloParams_2021_v0_2(process):
     process.load("L1Trigger.L1TCalorimeter.caloParams_2021_v0_2_cfi")
     return process
