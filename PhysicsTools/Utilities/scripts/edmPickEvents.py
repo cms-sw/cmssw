67382f8b2c743447cabb2f7bc65b8f9530ebd156
iarspider <iarspider@gmail.com>
2022-03-11 11:12:23 +0100

    edmPickEvents improvements
    dasgoclient doesn't print all errors to stderr, some go to stdout. Since the errors are not proper JSON, edmPickEvents script will raise JSONDecodeError, but will not show the actual error. The following changes were added to get a better error message:
    1) An early detection of failure (test if exit_code != 0) 
    2) A failsafe (catching JSONDecodeError)

--- PhysicsTools/Utilities/scripts/edmPickEvents.py
+++ PhysicsTools/Utilities/scripts/edmPickEvents.py
@@ -124,15 +124,31 @@ def getFileNames_dasgoclient(event):
     cmd = ['dasgoclient', '-query', query, '-json']
     proc = subprocess.Popen(cmd, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
     files = []
-    err = proc.stderr.read()
-    if  err:
-        print("DAS error: %s" % err)
+    try:
+        out, err = proc.communicate(timeout=15)
+    except TimeoutExpired:
+        proc.kill()
+        out, err = proc.communicate()
+    exit_code = proc.returncode
+    if err or (exit_code != 0):
+        print("DAS query error: return code\n--- Standard output---".format(exit_code))
+        print(out)
+        print("--- Standard error---")
+        print(err)
+        print("--- End --")
+        exit(1)
     else:
-        for row in json.load(proc.stdout):
-            for rec in row.get('file', []):
-                fname = rec.get('name', '')
-                if fname:
-                    files.append(fname)
+        try:
+            for row in json.loads(out):
+                for rec in row.get('file', []):
+                    fname = rec.get('name', '')
+                    if fname:
+                        files.append(fname)
+        except json.decoder.JSONDecodeError:
+            print("dasgoclient returned invalid JSON:")
+            print(out)
+            print("-- End --")
+            exit(1)
     return files
 
 def fullCPMpath():
