2f80e2ffe7c5e8a63d4c3da78f909dce7d23a769
Simon Rothman <srothman@mit.edu>
2022-03-11 10:08:10 -0600

    Removed test

--- PhysicsTools/PatAlgos/test/runtests.sh
+++ PhysicsTools/PatAlgos/test/runtests.sh
@@ -53,5 +53,3 @@ cmsRun ${LOCAL_TEST_DIR}/patTuple_updateJets_fromMiniAOD_cfg.py || die 'Failure 
 # cmsRun ${LOCAL_TEST_DIR}/patTuple_onlyMuons_cfg.py || die 'Failure using patTuple_onlyMuons_cfg.py' $?
 # cmsRun ${LOCAL_TEST_DIR}/patTuple_onlyPhotons_cfg.py || die 'Failure using patTuple_onlyPhotons_cfg.py' $?
 # cmsRun ${LOCAL_TEST_DIR}/patTuple_onlyTaus_cfg.py || die 'Failure using patTuple_onlyTaus_cfg.py' $?
-
-cmsRun ${LOCAL_TEST_DIR}/DRNTest_cfg.py || die "Failure using DRNtest" $?
