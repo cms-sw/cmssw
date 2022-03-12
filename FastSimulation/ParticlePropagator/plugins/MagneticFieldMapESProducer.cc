cd2ef2bdb3dc31c021d557de1b7a0550793dbaf8
Sunanda <sunanda.banerjee@cern.ch>
2022-03-12 03:10:27 +0100

    Take care of deprecated code in FastSimulation/ParticlePropagator

--- FastSimulation/ParticlePropagator/plugins/MagneticFieldMapESProducer.cc
+++ FastSimulation/ParticlePropagator/plugins/MagneticFieldMapESProducer.cc
@@ -1,30 +1,29 @@
 #include "FastSimulation/ParticlePropagator/plugins/MagneticFieldMapESProducer.h"
-#include "FastSimulation/TrackerSetup/interface/TrackerInteractionGeometryRecord.h"
-#include "MagneticField/Records/interface/IdealMagneticFieldRecord.h"
 
 #include "FWCore/Framework/interface/ESHandle.h"
+#include "FWCore/Framework/interface/ConsumesCollector.h"
 #include "FWCore/Framework/interface/ModuleFactory.h"
 #include "FWCore/ParameterSet/interface/ParameterSet.h"
 
 #include <memory>
 
-MagneticFieldMapESProducer::MagneticFieldMapESProducer(const edm::ParameterSet& p) {
-  setWhatProduced(this);
-  _label = p.getUntrackedParameter<std::string>("trackerGeometryLabel", "");
-
+MagneticFieldMapESProducer::MagneticFieldMapESProducer(const edm::ParameterSet& p) : _label(p.getUntrackedParameter<std::string>("trackerGeometryLabel", "")) {
   //    theTrackerMaterial = p.getParameter<edm::ParameterSet>("TrackerMaterial");
+
+  auto cc = setWhatProduced(this);
+  tokenGeom_ = cc.consumes(edm::ESInputTag("", _label));
+  tokenBField_ = cc.consumes();
+
+  setWhatProduced(this);
 }
 
 MagneticFieldMapESProducer::~MagneticFieldMapESProducer() {}
 
 std::unique_ptr<MagneticFieldMap> MagneticFieldMapESProducer::produce(const MagneticFieldMapRecord& iRecord) {
-  edm::ESHandle<TrackerInteractionGeometry> theInteractionGeometry;
-  edm::ESHandle<MagneticField> theMagneticField;
+  const edm::ESHandle<TrackerInteractionGeometry> & theInteractionGeometry = iRecord.getRecord<TrackerInteractionGeometryRecord>().getHandle(tokenGeom_);
+  const edm::ESHandle<MagneticField> & theMagneticField = iRecord.getRecord<IdealMagneticFieldRecord>().getHandle(tokenBField_);
 
-  iRecord.getRecord<TrackerInteractionGeometryRecord>().get(_label, theInteractionGeometry);
-  iRecord.getRecord<IdealMagneticFieldRecord>().get(theMagneticField);
-
-  return std::make_unique<MagneticFieldMap>(&(*theMagneticField), &(*theInteractionGeometry));
+  return std::make_unique<MagneticFieldMap>(theMagneticField.product(), theInteractionGeometry.product());
 }
 
 DEFINE_FWK_EVENTSETUP_MODULE(MagneticFieldMapESProducer);
--- FastSimulation/ParticlePropagator/plugins/MagneticFieldMapESProducer.h
+++ FastSimulation/ParticlePropagator/plugins/MagneticFieldMapESProducer.h
@@ -2,8 +2,11 @@
 #define FastSimulation_ParticlePropagator_MagneticFieldMapESProducer_H
 
 #include "FWCore/Framework/interface/ESProducer.h"
+#include "FWCore/Utilities/interface/ESGetToken.h"
 #include "FastSimulation/ParticlePropagator/interface/MagneticFieldMapRecord.h"
 #include "FastSimulation/ParticlePropagator/interface/MagneticFieldMap.h"
+#include "FastSimulation/TrackerSetup/interface/TrackerInteractionGeometryRecord.h"
+#include "MagneticField/Records/interface/IdealMagneticFieldRecord.h"
 #include <memory>
 #include <string>
 
@@ -14,7 +17,10 @@ public:
   std::unique_ptr<MagneticFieldMap> produce(const MagneticFieldMapRecord &);
 
 private:
-  std::string _label;
+  const std::string _label;
+  edm::ESGetToken<TrackerInteractionGeometry, TrackerInteractionGeometryRecord> tokenGeom_;
+  edm::ESGetToken<MagneticField, IdealMagneticFieldRecord> tokenBField_;
+
 };
 
 #endif
