d4de0cf19401ae0b0671f13f839db2ad7bd39fdc
ccaillol <ccaillol@cern.ch>
2022-03-15 14:49:44 +0100

    cleaning

--- L1Trigger/L1TMuon/interface/RegionalMuonRawDigiTranslator.h
+++ L1Trigger/L1TMuon/interface/RegionalMuonRawDigiTranslator.h
@@ -27,7 +27,7 @@ namespace l1t {
                                         uint32_t& raw_data_32_63,
                                         bool isKbmtf,
                                         bool useEmtfDisplacementInfo);
-    void generatePackedShowerPayload(const RegionalMuonShower& shower,
+    static void generatePackedShowerPayload(const RegionalMuonShower& shower,
                                             std::array<uint32_t, 6>& payload,
                                             bool useEmtfNominalTightShowers,
                                             bool useEmtfLooseShowers);
