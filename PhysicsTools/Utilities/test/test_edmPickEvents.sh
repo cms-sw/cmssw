5325724158b62b85b8cb1a328aefac335388a1ee
Shahzad Malik Muzaffar <shahzad.malik.muzaffar@cern.ch>
2022-03-17 17:35:43 +0100

    edmPickEvent: print the actual das output instead of json parsing error

--- PhysicsTools/Utilities/test/test_edmPickEvents.sh
+++ PhysicsTools/Utilities/test/test_edmPickEvents.sh
@@ -1,9 +1,4 @@
 #!/bin/bash -ex
 #Dataset, Run, Lumi and Events are copied from Workflows 4.22
 
-if ! edmPickEvents.py "/JetHT/Run2018A-PromptReco-v1/MINIAOD" 315489:31:19015199,315489:31:19098714,315489:31:18897114  > run_edmCopyPickMerge.sh ; then
-  cat run_edmCopyPickMerge.sh
-  exit 1
-fi
-chmod +x run_edmCopyPickMerge.sh
-./run_edmCopyPickMerge.sh
+edmPickEvents.py --runInteractive "/JetHT/Run2018A-PromptReco-v1/MINIAOD" 315489:31:19015199,315489:31:19098714,315489:31:18897114
