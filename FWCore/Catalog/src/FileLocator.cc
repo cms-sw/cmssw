5b1f4e8d01e7f763af971c98accbea2ec73b1106
Duong <nhduongvn@gmail.com>
2022-03-18 16:25:11 -0500

    adapt the data access to use new storage description defined in storage.json (transition to Rucio)

--- FWCore/Catalog/src/FileLocator.cc
+++ FWCore/Catalog/src/FileLocator.cc
@@ -1,15 +1,17 @@
 #include "FWCore/Catalog/interface/FileLocator.h"
-#include "FWCore/Catalog/interface/SiteLocalConfig.h"
 #include "FWCore/ServiceRegistry/interface/Service.h"
 
 #include <boost/algorithm/string.hpp>
 #include <boost/algorithm/string/replace.hpp>
+#include <boost/property_tree/json_parser.hpp>
 
+#include <filesystem>
 #include <cstdlib>
 #include <stdexcept>
 #include <fstream>
-#include <iostream>
 #include <sstream>
+
+namespace pt = boost::property_tree;
 
 namespace {
 
@@ -17,20 +19,18 @@ namespace {
     std::string result = outputFormat;
     std::stringstream str;
 
-    // std::cerr << "Output format: "<< outputFormat << std::endl;
     for (size_t i = 1; i < matches.size(); ++i) {
       str.str("");
       str << "$" << i;
-      // std::cerr << "Current match: " << matches[i] << std::endl;
       std::string const matchedString(matches[i].first, matches[i].second);
       if (!matchedString.empty())
         boost::algorithm::replace_all(result, str.str(), matchedString);
     }
-    // std::cerr << "Final string: " << result << std::endl;
     return result;
   }
 
   constexpr char const* const kEmptyString = "";
+  constexpr char const* const kLFNPrefix = "/store/";
 
   const char* safe(const char* iCheck) {
     if (iCheck == nullptr) {
@@ -41,32 +41,52 @@ namespace {
 
 }  // namespace
 
-namespace edm {
-  FileLocator::FileLocator(std::string const& catUrl, unsigned iCatalog) : m_destination("any") {
-    init(catUrl, iCatalog);
+namespace pt = boost::property_tree;
 
-    // std::cout << m_protocols.size() << " protocols" << std::endl;
-    // std::cout << m_directRules[m_protocols[0]].size() << " rules" << std::endl;
+namespace edm {
+
+  FileLocator::FileLocator(std::string const& catUrl, unsigned iCatalog) : m_destination("any") {
+    init_trivialCatalog(catUrl, iCatalog);
+  }
+
+  FileLocator::FileLocator(edm::CatalogAttributes const& catAttr,
+                           unsigned iCatalog,
+                           std::string const& storageDescriptionPath)
+      : m_destination("any") {
+    init(catAttr, iCatalog, storageDescriptionPath);
   }
 
   FileLocator::~FileLocator() {}
 
-  std::string FileLocator::pfn(std::string const& ilfn) const { return convert(ilfn, m_directRules, true); }
-
-  std::string FileLocator::lfn(std::string const& ipfn) const { return convert(ipfn, m_inverseRules, false); }
+  std::string FileLocator::pfn(std::string const& ilfn, edm::CatalogType catType) const {
+    if (catType == edm::CatalogType::TrivialCatalog)
+      return convert(ilfn, m_directRules_trivialCatalog, true);
+    return convert(ilfn, m_directRules, true);
+  }
 
   std::string FileLocator::convert(std::string const& input, ProtocolRules const& rules, bool direct) const {
     std::string out = "";
-
+    //check if input is an authentic LFN
+    if (input.compare(0, 7, kLFNPrefix) != 0)
+      return out;
+    //use prefix in the protocol
+    if (!m_prefix.empty()) {
+      out = m_prefix + "/" + input;
+      if (input[0] == '/')
+        out = m_prefix + input;
+      return out;
+    }
+    //no prefix in the protocol, use rule
     for (size_t pi = 0, pe = m_protocols.size(); pi != pe; ++pi) {
       out = applyRules(rules, m_protocols[pi], m_destination, direct, input);
-      if (!out.empty())
+      if (!out.empty()) {
         return out;
+      }
     }
     return out;
   }
 
-  void FileLocator::parseRule(tinyxml2::XMLElement* ruleElement, ProtocolRules& rules) {
+  void FileLocator::parseRuleTrivialCatalog(tinyxml2::XMLElement* ruleElement, ProtocolRules& rules) {
     if (!ruleElement) {
       throw cms::Exception("TrivialFileCatalog", std::string("TrivialFileCatalog::connect: Malformed trivial catalog"));
     }
@@ -89,27 +109,42 @@ namespace edm {
     rules[protocol].emplace_back(std::move(rule));
   }
 
-  void FileLocator::init(std::string const& catUrl, unsigned iCatalog) {
-    std::string m_url = catUrl;
+  void FileLocator::parseRule(pt::ptree::value_type const& storageRule,
+                              std::string const& protocol,
+                              ProtocolRules& rules) {
+    if (storageRule.second.empty()) {
+      throw cms::Exception("RucioFileCatalog", "edm::FileLocator::parseRule Malformed storage rule");
+    }
+    auto const pathMatchRegexp = storageRule.second.get<std::string>("lfn");
+    auto const result = storageRule.second.get<std::string>("pfn");
+    Rule rule;
+    rule.pathMatch.assign(pathMatchRegexp);
+    rule.destinationMatch.assign(".*");
+    rule.result = result;
+    rule.chain = "";
+    rules[protocol].emplace_back(std::move(rule));
+  }
 
-    if (m_url.empty()) {
+  void FileLocator::init_trivialCatalog(std::string const& catUrl, unsigned iCatalog) {
+    std::string url = catUrl;
+    if (url.empty()) {
       Service<SiteLocalConfig> localconfservice;
       if (!localconfservice.isAvailable())
         throw cms::Exception("TrivialFileCatalog", "edm::SiteLocalConfigService is not available");
-      if (iCatalog >= localconfservice->dataCatalogs().size())
+      if (iCatalog >= localconfservice->trivialDataCatalogs().size())
         throw cms::Exception("TrivialFileCatalog", "edm::FileLocator: Request nonexistence data catalog");
-      m_url = localconfservice->dataCatalogs()[iCatalog];
+      url = localconfservice->trivialDataCatalogs()[iCatalog];
     }
 
-    if (m_url.find("file:") == std::string::npos) {
+    if (url.find("file:") == std::string::npos) {
       throw cms::Exception("TrivialFileCatalog",
                            "TrivialFileCatalog::connect: Malformed url for file catalog configuration");
     }
 
-    m_url = m_url.erase(0, m_url.find(':') + 1);
+    url = url.erase(0, url.find(':') + 1);
 
     std::vector<std::string> tokens;
-    boost::algorithm::split(tokens, m_url, boost::is_any_of(std::string("?")));
+    boost::algorithm::split(tokens, url, boost::is_any_of(std::string("?")));
     m_filename = tokens[0];
 
     if (tokens.size() == 2) {
@@ -146,9 +181,6 @@ namespace edm {
     std::ifstream configFile;
     configFile.open(m_filename.c_str());
 
-    //
-    // std::cout << "Using catalog configuration " << m_filename << std::endl;
-
     if (!configFile.good() || !configFile.is_open()) {
       throw cms::Exception("TrivialFileCatalog",
                            "TrivialFileCatalog::connect: Unable to open trivial file catalog " + m_filename);
@@ -177,13 +209,96 @@ namespace edm {
     /*first of all do the lfn-to-pfn bit*/
     for (auto el = rootElement->FirstChildElement("lfn-to-pfn"); el != nullptr;
          el = el->NextSiblingElement("lfn-to-pfn")) {
-      parseRule(el, m_directRules);
+      parseRuleTrivialCatalog(el, m_directRules_trivialCatalog);
     }
 
     /*Then we handle the pfn-to-lfn bit*/
     for (auto el = rootElement->FirstChildElement("pfn-to-lfn"); el != nullptr;
          el = el->NextSiblingElement("pfn-to-lfn")) {
-      parseRule(el, m_inverseRules);
+      parseRuleTrivialCatalog(el, m_inverseRules);
+    }
+  }
+
+  void FileLocator::init(edm::CatalogAttributes const& input_dataCatalog,
+                         unsigned iCatalog,
+                         std::string const& storageDescriptionPath) {
+    Service<SiteLocalConfig> localconfservice;
+    edm::CatalogAttributes aCatalog = input_dataCatalog;
+    if (input_dataCatalog.empty()) {
+      if (!localconfservice.isAvailable()) {
+        cms::Exception ex("FileCatalog");
+        ex << "edm::SiteLocalConfigService is not available";
+        ex.addContext("Calling edm::FileLocator::init()");
+        throw ex;
+      }
+      if (iCatalog >= localconfservice->dataCatalogs().size()) {
+        cms::Exception ex("FileCatalog");
+        ex << "Request nonexistence data catalog";
+        ex.addContext("Calling edm::FileLocator::init()");
+        throw ex;
+      }
+      aCatalog = localconfservice->dataCatalogs()[iCatalog];
+    }
+
+    std::filesystem::path filename_storage = localconfservice->storageDescriptionPath(aCatalog);
+
+    //use path to storage description from input parameter
+    if (!storageDescriptionPath.empty())
+      filename_storage = storageDescriptionPath;
+
+    //now read json
+    pt::ptree json;
+    try {
+      boost::property_tree::read_json(filename_storage.string(), json);
+    } catch (std::exception& e) {
+      cms::Exception ex("FileCatalog");
+      ex << "Can not open storage.json (" << filename_storage.string()
+         << "). Check SITECONFIG_PATH and site-local-config.xml <data-access>";
+      ex.addContext("edm::FileLocator:init()");
+      throw ex;
+    }
+    auto found_site = std::find_if(json.begin(), json.end(), [&](pt::ptree::value_type const& site) {
+      //get site name
+      std::string siteName = site.second.get("site", kEmptyString);
+      //get volume name
+      std::string volName = site.second.get("volume", kEmptyString);
+      return aCatalog.storageSite == siteName && aCatalog.volume == volName;
+    });
+
+    //let enforce that site-local-config.xml and storage.json contains valid catalogs in <data-access>, in which site defined in site-local-config.xml <data-access> should be found in storage.json
+    if (found_site == json.end()) {
+      cms::Exception ex("FileCatalog");
+      ex << "Can not find site and volume " << aCatalog.site << ", " << aCatalog.volume
+         << " in storage.json. Check site-local-config.xml <data-access> and storage.json";
+      ex.addContext("edm::FileLocator:init()");
+      throw ex;
+    }
+
+    const pt::ptree& protocols = found_site->second.find("protocols")->second;
+    auto found_protocol = std::find_if(protocols.begin(), protocols.end(), [&](pt::ptree::value_type const& protocol) {
+      std::string protName = protocol.second.get("protocol", kEmptyString);
+      return aCatalog.protocol == protName;
+    });
+
+    //let enforce that site-local-config.xml and storage.json contains valid catalogs, in which protocol defined in site-local-config.xml <data-access> should be found in storage.json
+    if (found_protocol == protocols.end()) {
+      cms::Exception ex("FileCatalog");
+      ex << "Can not find protocol " << aCatalog.protocol
+         << " in storage.json. Check site-local-config.xml <data-access> and storage.json";
+      ex.addContext("edm::FileLocator:init()");
+      throw ex;
+    }
+
+    std::string protName = found_protocol->second.get("protocol", kEmptyString);
+    m_protocols.push_back(protName);
+    m_prefix = found_protocol->second.get("prefix", kEmptyString);
+    if (m_prefix == kEmptyString) {
+      //get rules
+      const pt::ptree& rules = found_protocol->second.find("rules")->second;
+      //loop over rules
+      for (pt::ptree::value_type const& storageRule : rules) {
+        parseRule(storageRule, protName, m_directRules);
+      }
     }
   }
 
@@ -192,8 +307,6 @@ namespace edm {
                                       std::string const& destination,
                                       bool direct,
                                       std::string name) const {
-    // std::cerr << "Calling apply rules with protocol: " << protocol << "\n destination: " << destination << "\n " << " on name " << name << std::endl;
-
     ProtocolRules::const_iterator const rulesIterator = protocolRules.find(protocol);
     if (rulesIterator == protocolRules.end()) {
       return "";
--- FWCore/Services/test/test_sitelocalconfig.sh
+++ FWCore/Services/test/test_sitelocalconfig.sh
@@ -6,10 +6,11 @@ function die { echo $1: status $2 ;  exit $2; }
 mkdir -p ${CMSSW_BASE}/test/SITECONF
 mkdir -p ${CMSSW_BASE}/test/SITECONF/local
 mkdir -p ${CMSSW_BASE}/test/SITECONF/local/JobConfig
-cp ${LOCAL_TEST_DIR}/no-source-site-local-config.testfile ${CMSSW_BASE}/test/SITECONF/local/JobConfig/site-local-config.xml
 
+export SITECONFIG_PATH=${CMSSW_BASE}/test/SITECONF/local
+
+cp ${LOCAL_TEST_DIR}/no-source-site-local-config.testfile ${CMSSW_BASE}/test/SITECONF/local/JobConfig/site-local-config.xml
 F1=${LOCAL_TEST_DIR}/test_sitelocalconfig_no_source_cfg.py
-export CMS_PATH=${CMSSW_BASE}/test
 (cmsRun $F1 ) || die "Failure using $F1" $?
 
 cp ${LOCAL_TEST_DIR}/source-site-local-config.testfile ${CMSSW_BASE}/test/SITECONF/local/JobConfig/site-local-config.xml
