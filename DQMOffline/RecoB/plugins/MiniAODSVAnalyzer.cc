580376283f37c3fd3c303636d2a44f57f47b5d95
Andrea <perrotta@cern.ch>
2022-03-18 06:44:48 +0100

    Cleaning code recently added in DQMOffline/RecoB

--- DQMOffline/RecoB/plugins/MiniAODSVAnalyzer.cc
+++ DQMOffline/RecoB/plugins/MiniAODSVAnalyzer.cc
@@ -1,16 +1,57 @@
-#include "DQMOffline/RecoB/plugins/MiniAODSVAnalyzer.h"
+
+#include "DQMServices/Core/interface/DQMEDAnalyzer.h"
+#include "DataFormats/PatCandidates/interface/Jet.h"
+#include "DataFormats/BTauReco/interface/SecondaryVertexTagInfo.h"
+#include "CommonTools/Statistics/interface/ChiSquaredProbability.h"
+#include "FWCore/Framework/interface/Frameworkfwd.h"
 #include "FWCore/Framework/interface/MakerMacros.h"
 #include "FWCore/Framework/interface/Event.h"
+
+/** \class MiniAODSVAnalyzer
+ *
+ *  Secondary Vertex Analyzer to run on MiniAOD
+ *
+ */
+
+class MiniAODSVAnalyzer : public DQMEDAnalyzer {
+public:
+  explicit MiniAODSVAnalyzer(const edm::ParameterSet& pSet);
+  ~MiniAODSVAnalyzer() override = default;
+
+  void analyze(const edm::Event& iEvent, const edm::EventSetup& iSetup) override;
+
+private:
+  void bookHistograms(DQMStore::IBooker&, edm::Run const&, edm::EventSetup const&) override;
+
+  const edm::EDGetTokenT<std::vector<pat::Jet>> jetToken_;
+  const std::string svTagInfo_;
+  const double ptMin_;
+  const double etaMax_;
+
+  MonitorElement* n_sv_;
+
+  MonitorElement* sv_mass_;
+  MonitorElement* sv_pt_;
+  MonitorElement* sv_ntracks_;
+  MonitorElement* sv_chi2norm_;
+  MonitorElement* sv_chi2prob_;
+
+  // relation to jet
+  MonitorElement* sv_ptrel_;
+  MonitorElement* sv_energyratio_;
+  MonitorElement* sv_deltaR_;
+
+  MonitorElement* sv_dxy_;
+  MonitorElement* sv_dxysig_;
+  MonitorElement* sv_d3d_;
+  MonitorElement* sv_d3dsig_;
+};
 
 MiniAODSVAnalyzer::MiniAODSVAnalyzer(const edm::ParameterSet& pSet)
     : jetToken_(consumes<std::vector<pat::Jet>>(pSet.getParameter<edm::InputTag>("JetTag"))),
       svTagInfo_(pSet.getParameter<std::string>("svTagInfo")),
       ptMin_(pSet.getParameter<double>("ptMin")),
-      etaMax_(pSet.getParameter<double>("etaMax"))
-
-{}
-
-MiniAODSVAnalyzer::~MiniAODSVAnalyzer() {}
+      etaMax_(pSet.getParameter<double>("etaMax")) {}
 
 void MiniAODSVAnalyzer::bookHistograms(DQMStore::IBooker& ibook, edm::Run const& run, edm::EventSetup const& es) {
   ibook.setCurrentFolder("Btag/SV");
