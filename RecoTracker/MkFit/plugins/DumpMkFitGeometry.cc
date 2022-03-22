6c7afd3c6a746f80a1ec4fedc26a97042a4cc3fe
Matevz Tadel <mtadel@ucsd.edu>
2022-03-22 04:33:22 -0700

    Implement writing and reading of binary TrackerInfo file.
    Cleanup standalone Makefiles for external Math/.

--- RecoTracker/MkFit/plugins/DumpMkFitGeometry.cc
+++ RecoTracker/MkFit/plugins/DumpMkFitGeometry.cc
@@ -38,32 +38,16 @@ void DumpMkFitGeometry::analyze(const edm::Event& iEvent, const edm::EventSetup&
   const auto& mkfg = iSetup.getData(m_mkfGeoToken);
   const mkfit::TrackerInfo& ti = mkfg.trackerInfo();
 
-  printf("DumpMkFitGeometry geom_ptr=%p, n_layers = %d\n", &mkfg, ti.n_layers());
-  if (m_level > 0) {
-    int n_modules = 0;
-    for (int i = 0; i < ti.n_layers(); ++i) {
-      const mkfit::LayerInfo& li = ti.layer(i);
-      li.print_layer();
-      n_modules += li.n_modules();
-      if (m_level > 1) {
-        printf("  Detailed module list N=%d\n", li.n_modules());
-        for (int j = 0; j < li.n_modules(); ++j) {
-          const mkfit::ModuleInfo& mi = li.module_info(j);
-          auto* p = mi.m_pos.Array();
-          auto* z = mi.m_zdir.Array();
-          auto* x = mi.m_xdir.Array();
-          // clang-format off
-          printf("Layer %d, mid=%u; detid=0x%x pos=%.3f,%.3f,%.3f, "
-                 "norm=%.3f,%.3f,%.3f, phi=%.3f,%.3f,%.3f\n",
-                 i, j, mi.m_detid, p[0], p[1], p[2],
-                 z[0], z[1], z[2], x[0], x[1], x[2]);
-          // clang-format on
-        }
-        printf("\n");
-      }
-    }
-    printf("DumpMkFitGeometry finished, n_modules=%d\n", n_modules);
+  printf("DumpMkFitGeometry geom_ptr=%p, n_layers = %d ==> ", &mkfg, ti.n_layers());
+  if (m_outputFileName.empty()) {
+    printf("no file-name specified, not dumping binary file\n");
+  } else {
+    printf("binary file = '%s'\n", m_outputFileName.c_str());
+    ti.write_bin_file(m_outputFileName);
   }
+
+  ti.print_tracker(m_level);
+  printf("DumpMkFitGeometry finished, n_modules=%d\n", ti.n_total_modules());
 }
 
 void DumpMkFitGeometry::beginJob(void) {}
--- RecoTracker/MkFitCore/standalone/ConfigStandalone.cc
+++ RecoTracker/MkFitCore/standalone/ConfigStandalone.cc
@@ -90,8 +90,8 @@ namespace mkfit {
   }  // namespace
 
   void execTrackerInfoCreatorPlugin(const std::string &base, TrackerInfo &ti, IterationsInfo &ii, bool verbose) {
-    std::string soname = base + ".so";
-
+    const std::string soname = base + ".so";
+    const std::string binname = base + ".bin";
     struct stat st;
 
     int si = 0;
@@ -103,20 +103,28 @@ namespace mkfit {
         path += "/";
       }
       path += search_path[si];
-      path += soname;
-      if (stat(path.c_str(), &st) == 0) {
-        printf("mkfit::execTrackerInfoCreatorPlugin processing '%s'\n", path.c_str());
+      std::string sopath = path + soname;
+      if (stat(sopath.c_str(), &st) == 0) {
+        printf("mkfit::execTrackerInfoCreatorPlugin processing '%s'\n", sopath.c_str());
 
-        void *h = dlopen(path.c_str(), RTLD_LAZY);
+        void *h = dlopen(sopath.c_str(), RTLD_LAZY);
         if (!h) {
           perror("dlopen failed");
           exit(2);
         }
 
-        long long *p2f = (long long *)dlsym(h, "TrackerInfoCrator_ptr");
+        long long *p2f = (long long *)dlsym(h, "TrackerInfoCreator_ptr");
         if (!p2f) {
           perror("dlsym failed");
           exit(2);
+        }
+
+        std::string binpath = path + binname;
+        if (stat(binpath.c_str(), &st) == 0) {
+          printf("mkfit::execTrackerInfoCreatorPlugin found TrackerInfo binary file '%s'\n", binpath.c_str());
+          ti.read_bin_file(binpath);
+        } else {
+          printf("mkfit::execTrackerInfoCreatorPlugin has NOT found TrackerInfo binary file '%s'\n", binpath.c_str());
         }
 
         TrackerInfoCreator_foo foo = (TrackerInfoCreator_foo)(*p2f);
