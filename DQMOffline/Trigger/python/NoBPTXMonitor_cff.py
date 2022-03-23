e08de56783431a05342b3401d305eb9e3a57f3d2
Juliette Alimena <juliette.alimena@cern.ch>
2022-03-23 11:07:41 +0100

    remove monitoring of backup NoBPTX paths

--- DQMOffline/Trigger/python/NoBPTXMonitor_cff.py
+++ DQMOffline/Trigger/python/NoBPTXMonitor_cff.py
@@ -2,24 +2,12 @@ import FWCore.ParameterSet.Config as cms
 
 from DQMOffline.Trigger.NoBPTXMonitor_cfi import hltNoBPTXmonitoring
 
-hltNoBPTXJetE70Monitoring = hltNoBPTXmonitoring.clone(
-    FolderName = 'HLT/EXO/NoBPTX/JetE70/',
-    numGenericTriggerEventPSet = dict(hltPaths = ["HLT_UncorrectedJetE70_NoBPTX3BX_v*"]) #HLT_ZeroBias_v*
-)
-
 hltNoBPTXL2Mu40Monitoring = hltNoBPTXmonitoring.clone(
     FolderName = 'HLT/EXO/NoBPTX/L2Mu40/',
     numGenericTriggerEventPSet = dict(hltPaths = ["HLT_L2Mu40_NoVertex_3Sta_NoBPTX3BX_v*"]) #HLT_ZeroBias_v*
 )
 
-hltNoBPTXL2Mu45Monitoring = hltNoBPTXmonitoring.clone(
-    FolderName = 'HLT/EXO/NoBPTX/L2Mu45/',
-    numGenericTriggerEventPSet = dict(hltPaths = ["HLT_L2Mu45_NoVertex_3Sta_NoBPTX3BX_v*"]) #HLT_ZeroBias_v*
-)
-
 exoHLTNoBPTXmonitoring = cms.Sequence(
     hltNoBPTXmonitoring
-    + hltNoBPTXJetE70Monitoring
     + hltNoBPTXL2Mu40Monitoring
-    + hltNoBPTXL2Mu45Monitoring
 )
