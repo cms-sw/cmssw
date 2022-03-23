b63ef880185946daf05cdfaef26239daf8a56532
Juliette Alimena <juliette.alimena@cern.ch>
2022-03-23 11:10:28 +0100

    remove monitoring of backup DoubleL2Muon, backup DoubleL3Muon, and backup Mu+photon paths. add monitoring of new (displaced) muon paths

--- DQMOffline/Trigger/python/DiDispStaMuonMonitor_cff.py
+++ DQMOffline/Trigger/python/DiDispStaMuonMonitor_cff.py
@@ -7,46 +7,14 @@ hltDiDispStaMuonCosmicMonitoring = hltDiDispStaMuonMonitoring.clone(
     FolderName = 'HLT/EXO/DiDispStaMuon/DoubleL2Mu23NoVtx_2Cha_CosmicSeed/',
     numGenericTriggerEventPSet = dict(hltPaths = ["HLT_DoubleL2Mu23NoVtx_2Cha_CosmicSeed_v*"]) #HLT_ZeroBias_v*
 )
-## Efficiency trigger
-hltDispStaMuon23Monitoring = hltDiDispStaMuonMonitoring.clone(
-    FolderName = 'HLT/EXO/DiDispStaMuon/L2Mu23NoVtx_2Cha/',
-    numGenericTriggerEventPSet = dict(hltPaths = ["HLT_L2Mu23NoVtx_2Cha_v*"]) #HLT_ZeroBias_v*
-)
 
-hltDispStaMuon23CosmicMonitoring = hltDiDispStaMuonMonitoring.clone(
-    FolderName = 'HLT/EXO/DiDispStaMuon/L2Mu23NoVtx_2Cha_CosmicSeed/',
-    numGenericTriggerEventPSet = dict(hltPaths = ["HLT_L2Mu23NoVtx_2Cha_CosmicSeed_v*"]) #HLT_ZeroBias_v*
+hltDiDispStaMuon10PromptL3Mu0VetoMonitoring = hltDiDispStaMuonMonitoring.clone(
+    FolderName = 'HLT/EXO/DiDispStaMuon/DoubleL2Mu10NoVtx_2Cha_PromptL3Mu0Veto/',
+    numGenericTriggerEventPSet = dict(hltPaths = ["HLT_DoubleL2Mu10NoVtx_2Cha_PromptL3Mu0Veto_v*"]) #HLT_ZeroBias_v*
 )
-
-## Backup trigger
-hltDiDispStaMuon25Monitoring = hltDiDispStaMuonMonitoring.clone(
-    FolderName = 'HLT/EXO/DiDispStaMuon/DoubleL2Mu25NoVtx_2Cha/',
-    numGenericTriggerEventPSet = dict(hltPaths = ["HLT_DoubleL2Mu25NoVtx_2Cha_v*"]) #HLT_ZeroBias_v*
-)
-
-hltDiDispStaMuon25CosmicMonitoring = hltDiDispStaMuonMonitoring.clone(
-    FolderName = 'HLT/EXO/DiDispStaMuon/DoubleL2Mu25NoVtx_2Cha_CosmicSeed/',
-    numGenericTriggerEventPSet = dict(hltPaths = ["HLT_DoubleL2Mu25NoVtx_2Cha_CosmicSeed_v*"]) #HLT_ZeroBias_v*
-)
-
-hltDiDispStaMuon30Monitoring = hltDiDispStaMuonMonitoring.clone(
-    FolderName = 'HLT/EXO/DiDispStaMuon/DoubleL2Mu30NoVtx_2Cha_Eta2p4/',
-    numGenericTriggerEventPSet = dict(hltPaths = ["HLT_DoubleL2Mu30NoVtx_2Cha_Eta2p4_v*"]) #HLT_ZeroBias_v*
-)
-
-hltDiDispStaMuon30CosmicMonitoring = hltDiDispStaMuonMonitoring.clone(
-    FolderName = 'HLT/EXO/DiDispStaMuon/DoubleL2Mu30NoVtx_2Cha_CosmicSeed_Eta2p4/',
-    numGenericTriggerEventPSet = dict(hltPaths = ["HLT_DoubleL2Mu30NoVtx_2Cha_CosmicSeed_Eta2p4_v*"]) #HLT_ZeroBias_v*
-)
-
 
 exoHLTdispStaMuonMonitoring = cms.Sequence(
     hltDiDispStaMuonMonitoring
     + hltDiDispStaMuonCosmicMonitoring
-    + hltDispStaMuon23Monitoring
-    + hltDispStaMuon23CosmicMonitoring
-    + hltDiDispStaMuon25Monitoring
-    + hltDiDispStaMuon25CosmicMonitoring
-    + hltDiDispStaMuon30Monitoring
-    + hltDiDispStaMuon30CosmicMonitoring
+    + hltDiDispStaMuon10PromptL3Mu0VetoMonitoring
 )
