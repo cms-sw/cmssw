960912a0ad87c017ceceedd252925a27fa84ded2
Marco Rovere <marco.rovere@cern.ch>
2022-03-24 19:12:17 +0100

    Add TICL from Reco customise

---
+++ RecoHGCal/TICL/python/customiseTICLFromReco.py
@@ -0,0 +1,46 @@
+# Reconstruction
+from RecoHGCal.TICL.iterativeTICL_cff import *
+# Validation
+from Validation.HGCalValidation.HGCalValidator_cfi import *
+from RecoLocalCalo.HGCalRecProducers.hgcalRecHitMapProducer_cfi import hgcalRecHitMapProducer
+
+# Load DNN ESSource
+from RecoTracker.IterativeTracking.iterativeTk_cff import trackdnn_source
+
+# Automatic addition of the customisation function from RecoHGCal.Configuration.RecoHGCal_EventContent_cff
+from RecoHGCal.Configuration.RecoHGCal_EventContent_cff import customiseHGCalOnlyEventContent
+
+
+
+def customiseTICLFromReco(process):
+# TensorFlow ESSource
+    process.TFESSource = cms.Task(process.trackdnn_source)
+# Reconstruction
+    process.TICL = cms.Path(process.TFESSource,
+                            process.ticlLayerTileTask,
+                            process.ticlIterationsTask,
+                            process.ticlTracksterMergeTask)
+# Validation
+    process.TICL_ValidationProducers = cms.Task(process.hgcalRecHitMapProducer,
+                                                process.lcAssocByEnergyScoreProducer,
+                                                process.layerClusterCaloParticleAssociationProducer,
+                                                process.scAssocByEnergyScoreProducer,
+                                                process.layerClusterSimClusterAssociationProducer,
+                                               )
+    process.TICL_Validator = cms.Task(process.hgcalValidator)
+    process.TICL_Validation = cms.Path(process.TICL_ValidationProducers,
+                                       process.TICL_Validator
+                                      )
+# Path and EndPath definitions
+    process.FEVTDEBUGHLToutput_step = cms.EndPath(process.FEVTDEBUGHLToutput)
+    process.DQMoutput_step = cms.EndPath(process.DQMoutput)
+
+# Schedule definition
+    process.schedule = cms.Schedule(process.TICL,
+                                    process.TICL_Validation,
+                                    process.FEVTDEBUGHLToutput_step,
+                                    process.DQMoutput_step)
+#call to customisation function customiseHGCalOnlyEventContent imported from RecoHGCal.Configuration.RecoHGCal_EventContent_cff
+    process = customiseHGCalOnlyEventContent(process)
+
+    return process
