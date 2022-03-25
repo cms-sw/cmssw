32e691bc76c40a23a32d59f0e5bddebfa85ccb90
Juliette Alimena <juliette.alimena@cern.ch>
2022-03-25 09:01:42 +0100

    remove hltMETClean requirements and histograms, since these filters have been removed from the HLT menu

--- DQMOffline/Trigger/python/METplusTrackMonitor_cff.py
+++ DQMOffline/Trigger/python/METplusTrackMonitor_cff.py
@@ -6,7 +6,6 @@ from DQMOffline.Trigger.METplusTrackMonitor_cfi import hltMETplusTrackMonitoring
 MET105_IsoTrk50monitoring = hltMETplusTrackMonitoring.clone(
     FolderName = 'HLT/EXO/MET/MET105_IsoTrk50/',
     hltMetFilter = 'hltMET105::HLT',
-    hltMetCleanFilter = 'hltMETClean65::HLT',
     met       = "caloMet",
 )
 MET105_IsoTrk50monitoring.numGenericTriggerEventPSet.hltPaths = cms.vstring("HLT_MET105_IsoTrk50_v*")
@@ -15,7 +14,6 @@ MET105_IsoTrk50monitoring.numGenericTriggerEventPSet.hltPaths = cms.vstring("HLT
 PFMET105_IsoTrk50monitoring = hltMETplusTrackMonitoring.clone(
     FolderName = 'HLT/EXO/MET/PFMET105_IsoTrk50/',
     hltMetFilter = 'hltPFMET105::HLT',
-    hltMetCleanFilter = 'hltMETClean65::HLT',
     met       = "caloMet",
 )
 PFMET105_IsoTrk50monitoring.numGenericTriggerEventPSet.hltPaths = cms.vstring("HLT_PFMET105_IsoTrk50_v*")
@@ -24,7 +22,6 @@ PFMET105_IsoTrk50monitoring.numGenericTriggerEventPSet.hltPaths = cms.vstring("H
 PFMET110_PFJet100monitoring = hltMETplusTrackMonitoring.clone(
     FolderName = 'HLT/EXO/MET/PFMET110_PFJet100/',
     hltMetFilter = 'hltPFMET110::HLT',
-    hltMetCleanFilter = 'hltMETClean65::HLT', #NEED TO CHECK
     met       = "caloMet",
 )
 PFMET110_PFJet100monitoring.numGenericTriggerEventPSet.hltPaths = cms.vstring("HLT_PFMET110_PFJet100_v*")
