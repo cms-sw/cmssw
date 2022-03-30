53d7f695fc3b13deafcdd887f0a39df22a8eadb5
ccaillol <ccaillol@cern.ch>
2022-03-30 09:59:42 +0200

    update L1TParticleFlow dataformats for phase-2

--- DataFormats/L1TParticleFlow/src/PFCluster.cc
+++ DataFormats/L1TParticleFlow/src/PFCluster.cc
@@ -5,7 +5,7 @@ void l1t::PFCluster::calibratePt(float newpt, float preserveEmEt) {
   ptError_ *= newpt / pt();
   setP4(PolarLorentzVector(newpt, eta(), phi(), mass()));
   if (preserveEmEt) {
-    float hNew = std::max<float>(pt() - currEmEt, 0);
+    float hNew = pt() - currEmEt;
     hOverE_ = (currEmEt > 0 ? hNew / currEmEt : -1);
   }
 }
