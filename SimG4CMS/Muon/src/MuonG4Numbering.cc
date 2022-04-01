3edeabb6158d5da6eeed706d955a23ebd983982b
Sunanda <sunanda.banerjee@cern.ch>
2022-04-01 04:00:19 +0200

    Code check

--- SimG4CMS/Muon/src/MuonG4Numbering.cc
+++ SimG4CMS/Muon/src/MuonG4Numbering.cc
@@ -32,12 +32,15 @@ MuonG4Numbering::MuonG4Numbering(const MuonGeometryConstants& muonConstants, con
     edm::LogVerbatim("MuonSim") << "MuonGeometryNumbering finds unusual level constant:" << theLevelPart;
   }
   if ((theStartCopyNo != 0) && (theStartCopyNo != 1)) {
-    edm::LogVerbatim("MuonSim") << "MuonGeometryNumbering finds unusual start value for copy numbers:" << theStartCopyNo;
+    edm::LogVerbatim("MuonSim") << "MuonGeometryNumbering finds unusual start value for copy numbers:"
+                                << theStartCopyNo;
   }
 
 #ifdef EDM_ML_DEBUG
   edm::LogVerbatim("MuonSim") << "StartCopyNo = " << theStartCopyNo;
-  edm::LogVerbatim("MuonSim") << "MuonG4Numbering configured with Level = " << theLevelPart << " Super = " << theSuperPart << " Base = " << theBasePart << " StartCopyNo = " << theStartCopyNo;
+  edm::LogVerbatim("MuonSim") << "MuonG4Numbering configured with Level = " << theLevelPart
+                              << " Super = " << theSuperPart << " Base = " << theBasePart
+                              << " StartCopyNo = " << theStartCopyNo;
   edm::LogVerbatim("MuonSim") << "dd4hep flag set to " << dd4hep_ << " and offsetmap at " << offMap_;
 #endif
 }
