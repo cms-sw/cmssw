9b76bfbb1f85e0088d4317943e009c97e2ef518d
Sunanda <sunanda.banerjee@cern.ch>
2022-04-05 09:34:58 +0200

    Take care of Andrea's comments

--- Geometry/HGCalGeometry/interface/HGCalGeometry.h
+++ Geometry/HGCalGeometry/interface/HGCalGeometry.h
@@ -50,7 +50,7 @@ public:
 
   HGCalGeometry(const HGCalTopology& topology);
 
-  ~HGCalGeometry() override;
+  ~HGCalGeometry() override = default;
 
   void localCorners(Pt3DVec& lc, const CCGFloat* pv, unsigned int i, Pt3D& ref);
 
@@ -70,8 +70,7 @@ public:
                   CaloSubdetectorGeometry::DimVec& dimVector,
                   CaloSubdetectorGeometry::IVec& dinsVector) const override;
 
-  GlobalPoint getPosition(const DetId& id) const;
-  GlobalPoint getPosition(const DetId& id, bool debug) const;
+  GlobalPoint getPosition(const DetId& id, bool debug = false) const;
   GlobalPoint getWaferPosition(const DetId& id) const;
 
   /// Returns area of a cell
