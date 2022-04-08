f115d16bc88bd74724e4ea200fbde2006f059268
Steve Lantz <steve.lantz@cornell.edu>
2022-04-07 19:01:40 -0700

    [mkFit] Fix standalone build scripts for gcc-10, add new README

--- RecoTracker/MkFitCore/standalone/xeon_scripts/init-gcc10-env.sh
+++ RecoTracker/MkFitCore/standalone/xeon_scripts/init-gcc10-env.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
-source /cvmfs/cms.cern.ch/slc7_amd64_gcc10/lcg/root/6.20.06-cms/etc/profile.d/init.sh
-export TBB_GCC=/cvmfs/cms.cern.ch/slc7_amd64_gcc10/external/tbb/2020_U2
+source /cvmfs/cms.cern.ch/slc7_amd64_gcc10/lcg/root/6.24.07-f52350f4e0b802edeb9a2551a7d00b92/etc/profile.d/init.sh
+export TBB_GCC=/cvmfs/cms.cern.ch/slc7_amd64_gcc10/external/tbb/v2021.4.0-d0152ca29055e3a1bbf629673f6e97c4
 # workaround for https://github.com/cms-sw/cmsdist/issues/5574
 # remove when we switch to a ROOT build where that issues is fixed
-export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:$LIBJPEG_TURBO_ROOT/lib64
+#export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:$LIBJPEG_TURBO_ROOT/lib64
 ### source /opt/intel/bin/compilervars.sh intel64
