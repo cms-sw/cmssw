c8951fbbf5fe22da0e483b6b1e6242e2f2f38506
Roberval Walsh <roberval.walsh@gmail.com>
2022-04-09 16:16:00 +0200

    updates for run3 mssm Hbb trigger dqm

--- DQMOffline/Trigger/python/MssmHbbBtagTriggerMonitor_Client_cfi.py
+++ DQMOffline/Trigger/python/MssmHbbBtagTriggerMonitor_Client_cfi.py
@@ -3,7 +3,12 @@ import FWCore.ParameterSet.Config as cms
 from DQMServices.Core.DQMEDHarvester import DQMEDHarvester
 
 mssmHbbBtag = DQMEDHarvester("DQMGenericClient",
-    subDirs        = cms.untracked.vstring("HLT/HIG/MssmHbb/semileptonic/BtagTrigger/*"),
+    subDirs        = cms.untracked.vstring(
+        "HLT/HIG/MssmHbb/fullhadronic/DeepCSVBtagTrigger/*",
+        "HLT/HIG/MssmHbb/semileptonic/DeepCSVBtagTrigger/*",
+        "HLT/HIG/MssmHbb/fullhadronic/DeepJetBtagTrigger/*",
+        "HLT/HIG/MssmHbb/semileptonic/DeepJetBtagTrigger/*",
+    ),
     verbose        = cms.untracked.uint32(0), # Set to 2 for all messages
     resolution     = cms.vstring(),
     efficiency     = cms.vstring(
@@ -13,40 +18,6 @@ mssmHbbBtag = DQMEDHarvester("DQMGenericClient",
     ),
 )
 
-mssmHbbBtagSL40noMu = mssmHbbBtag.clone(
-    subDirs = ["HLT/HIG/MssmHbb/semileptonic/BtagTrigger/pt40_noMuon"]
-)
-mssmHbbBtagSL40 = mssmHbbBtag.clone(
-    subDirs = ["HLT/HIG/MssmHbb/semileptonic/BtagTrigger/pt40/"]
-)
-mssmHbbBtagSL100 = mssmHbbBtag.clone(
-    subDirs = ["HLT/HIG/MssmHbb/semileptonic/BtagTrigger/pt100/"]
-)
-mssmHbbBtagSL200 = mssmHbbBtag.clone(
-    subDirs = ["HLT/HIG/MssmHbb/semileptonic/BtagTrigger/pt200/"]
-)
-mssmHbbBtagSL350 = mssmHbbBtag.clone(
-    subDirs = ["HLT/HIG/MssmHbb/semileptonic/BtagTrigger/pt350/"]
-)
-mssmHbbBtagAH100 = mssmHbbBtag.clone(
-    subDirs = ["HLT/HIG/MssmHbb/fullhadronic/BtagTrigger/pt100/"]
-)
-mssmHbbBtagAH200 = mssmHbbBtag.clone(
-    subDirs = ["HLT/HIG/MssmHbb/fullhadronic/BtagTrigger/pt200/"]
-)
-mssmHbbBtagAH350 = mssmHbbBtag.clone(
-    subDirs = ["HLT/HIG/MssmHbb/fullhadronic/BtagTrigger/pt350/"]
-)
-
-
 mssmHbbBtagTriggerEfficiency = cms.Sequence(
-   mssmHbbBtag
-#   mssmHbbBtagSL40noMu
-# + mssmHbbBtagSL40
-# + mssmHbbBtagSL100
-# + mssmHbbBtagSL200
-# + mssmHbbBtagSL350
-# + mssmHbbBtagAH100
-# + mssmHbbBtagAH200
-# + mssmHbbBtagAH350
+   mssmHbbBtag 
 )
