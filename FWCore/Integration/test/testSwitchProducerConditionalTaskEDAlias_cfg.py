242576c3998b1694d24ef8da43b9fbe18a6715d2
Matti Kortelainen <matti.kortelainen@cern.ch>
2022-04-12 18:34:13 +0200

    Fix EDAlias treatment for ConditionalTask

--- FWCore/Integration/test/testSwitchProducerConditionalTaskEDAlias_cfg.py
+++ FWCore/Integration/test/testSwitchProducerConditionalTaskEDAlias_cfg.py
@@ -23,15 +23,20 @@ process.out = cms.OutputModule("PoolOutputModule",
     outputCommands = cms.untracked.vstring(
         'keep *_intProducerAlias_*_*',
         'keep *_intProducerDep_*_*',
+        'keep *_intProducerDepAliasDep_*_*',
     )
 )
 
 process.intProducer1 = cms.EDProducer("ManyIntProducer", ivalue = cms.int32(1))
 process.intProducer2 = cms.EDProducer("ManyIntProducer", ivalue = cms.int32(2), values = cms.VPSet(cms.PSet(instance=cms.string("foo"),value=cms.int32(2))))
+process.intProducer3 = cms.EDProducer("ManyIntProducer", ivalue = cms.int32(3))
+process.intProducer4 = cms.EDProducer("ManyIntProducer", ivalue = cms.int32(4))
 if enableTest2:
     process.intProducer1.throw = cms.untracked.bool(True)
 else:
     process.intProducer2.throw = cms.untracked.bool(True)
+    process.intProducer3.throw = cms.untracked.bool(True)
+    process.intProducer4.throw = cms.untracked.bool(True)
 
 
 process.intProducerAlias = SwitchProducerTest(
@@ -44,6 +49,19 @@ process.intProducerDep = cms.EDProducer("AddIntsProducer", labels = cms.VInputTa
 if not enableTest2:
     process.intProducerDep.labels = ["intProducer1"]
 
-process.ct = cms.ConditionalTask(process.intProducer1, process.intProducer2)
-process.p = cms.Path(process.intProducerAlias+process.intProducerDep,
+process.intProducerDepAlias = SwitchProducerTest(
+    test1 = cms.EDProducer("AddIntsProducer", labels = cms.VInputTag("intProducerDep", "intProducerAlias")),
+    test2 = cms.EDAlias(intProducer3 = cms.VPSet(cms.PSet(type = cms.string("*"), fromProductInstance = cms.string(""), toProductInstance = cms.string(""))),
+                        intProducer4 = cms.VPSet(cms.PSet(type = cms.string("*"), fromProductInstance = cms.string(""), toProductInstance = cms.string("other"))))
+)
+process.intProducerDepAliasDep = cms.EDProducer("AddIntsProducer", labels = cms.VInputTag("intProducer3", "intProducer4"))
+if not enableTest2:
+    process.intProducerDepAliasDep.labels = ["intProducerAlias"]
+
+process.ct = cms.ConditionalTask(process.intProducer1, process.intProducer2, process.intProducer3, process.intProducer4)
+process.p = cms.Path(process.intProducerAlias+process.intProducerDep +
+                     process.intProducerDepAlias + process.intProducerDepAliasDep,
                      process.ct)
+
+process.ct2 = cms.ConditionalTask(process.intProducerAlias, process.intProducerDep, process.intProducerDepAlias, process.ct)
+process.p2 = cms.Path(process.intProducerDepAliasDep, process.ct2)
