3d267d580010dae88b6a90ac61fda3032d38b9ee
Alp <aakpinar@bu.edu>
2022-04-12 15:28:35 +0200

    Add HLTJetHFCleaner module

--- HLTrigger/JetMET/plugins/SealModule.cc
+++ HLTrigger/JetMET/plugins/SealModule.cc
@@ -23,6 +23,8 @@
 #include "HLTrigger/JetMET/interface/HLTPFJetIDProducer.h"
 #include "HLTrigger/JetMET/interface/HLTMETCleanerUsingJetID.h"
 
+#include "HLTrigger/JetMET/plugins/HLTJetHFCleaner.h"
+
 //Work with all jet collections without changing the module name
 #include "HLTrigger/JetMET/interface/HLTHtMhtProducer.h"
 #include "HLTrigger/JetMET/interface/HLTCaloTowerHtMhtProducer.h"
@@ -256,3 +258,9 @@ DEFINE_FWK_MODULE(HLTPFJetsMatchedToFilteredCaloJetsProducer);
 
 typedef HLTPFJetsMatchedToFilteredJetsProducer<reco::PFJetRef> HLTPFJetsMatchedToFilteredPFJetsProducer;
 DEFINE_FWK_MODULE(HLTPFJetsMatchedToFilteredPFJetsProducer);
+
+typedef HLTJetHFCleaner<reco::CaloJet> HLTCaloJetHFCleaner;
+DEFINE_FWK_MODULE(HLTCaloJetHFCleaner);
+
+typedef HLTJetHFCleaner<reco::PFJet> HLTPFJetHFCleaner;
+DEFINE_FWK_MODULE(HLTPFJetHFCleaner);
