268687a2ce4673fdf3c3fbad07b61e0f9a895031
Simon Rothman <srothman@mit.edu>
2022-04-14 09:04:25 -0500

    Same for electrons

--- PhysicsTools/PatAlgos/python/slimming/patElectronDRNCorrector_cfi.py
+++ PhysicsTools/PatAlgos/python/slimming/patElectronDRNCorrector_cfi.py
@@ -1,15 +1,15 @@
 import FWCore.ParameterSet.Config as cms
 
-patElectronsDRN = cms.EDProducer("PatElectronDRNCorrectionProducer",
-    particleSource = cms.InputTag("selectedPatElectrons"),
-    rhoName = cms.InputTag("fixedGridRhoFastjetAll"),
+from RecoEgamma.EgammaTools.patElectronDRNCorrectionProducer_cfi import patElectronDRNCorrectionProducer
 
-    Client = cms.PSet(
-      mode = cms.string("Async"),
-      modelName = cms.string("electronObjectEnsemble"),
-      modelConfigPath = cms.FileInPath("RecoEgamma/EgammaElectronProducers/data/models/electronObjectEnsemble/config.pbtxt"),
-      allowedTries = cms.untracked.uint32(1),
-      timeout = cms.untracked.uint32(10),
-      useSharedMemory = cms.untracked.bool(True),
+patElectronsDRN = patElectronDRNCorrectionProducer.clone(
+                            particleSource = 'selectedPatElectrons',
+                            rhoName = 'fixedGridRhoFastjetAll',
+                            Client = patElectronDRNCorrectionProducer.Client.clone(
+                              mode = 'Async',
+                              allowedTries = 1,
+                              modelName = 'electronObjectEnsemble',
+                              modelConfigPath = 'RecoEgamma/EgammaElectronProducers/data/models/electronObjectEnsemble/config.pbtxt',
+                              timeout = 10
+                            )
     )
-)
