f9e967441f122320c58dc2c1e9f5070e0599205d
Shahzad Malik Muzaffar <shahzad.malik.muzaffar@cern.ch>
2022-04-15 05:30:45 +0200

    use OPENSSL_VERSION_NUMBER instead of OPENSSL_API_COMPAT

--- Utilities/OpenSSL/interface/openssl_init.h
+++ Utilities/OpenSSL/interface/openssl_init.h
@@ -2,7 +2,7 @@
 #define UTILITIES_OPENSSL_OPENSSL_INIT_H
 #include <openssl/evp.h>
 
-#if OPENSSL_API_COMPAT < 0x10100000L
+#if OPENSSL_VERSION_NUMBER < 0x10100000L
 #define EVP_MD_CTX_new EVP_MD_CTX_create
 #define EVP_MD_CTX_free EVP_MD_CTX_destroy
 #endif
