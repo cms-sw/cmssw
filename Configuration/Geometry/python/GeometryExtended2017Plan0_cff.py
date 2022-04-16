7d10ec1e283e3310ed7c073ee14df5eeec826565
Sunanda <sunanda.banerjee@cern.ch>
2022-04-16 06:15:36 +0200

    Rearrange 2017 scenarios in view of updates foreseen

--- Configuration/Geometry/python/GeometryExtended2017Plan0_cff.py
+++ Configuration/Geometry/python/GeometryExtended2017Plan0_cff.py
@@ -4,7 +4,7 @@ import FWCore.ParameterSet.Config as cms
 # Geometry master configuration
 #
 # Ideal geometry, needed for simulation
-from Geometry.CMSCommonData.cmsExtendedGeometry2017XML_cfi import *
+from Geometry.CMSCommonData.cmsExtendedGeometry2017Plan0XML_cfi import *
 from Geometry.TrackerNumberingBuilder.trackerNumberingGeometry_cfi import *
 from Geometry.EcalCommonData.ecalSimulationParameters_cff import *
 from Geometry.HcalCommonData.hcalDDDSimConstants_cff import *
