25094bb12dfc383496108f93e13ed945549af76f
Kevin Pedro <kpedro88@gmail.com>
2022-04-19 17:33:50 -0500

    add a test for ragged inputs

---
+++ HeterogeneousCore/SonicTriton/test/TritonIdentityProducer.cc
@@ -0,0 +1,66 @@
+#include "HeterogeneousCore/SonicTriton/interface/TritonEDProducer.h"
+
+#include "FWCore/ParameterSet/interface/FileInPath.h"
+#include "FWCore/ParameterSet/interface/ConfigurationDescriptions.h"
+#include "FWCore/ParameterSet/interface/ParameterSetDescription.h"
+#include "FWCore/Framework/interface/MakerMacros.h"
+
+#include <sstream>
+#include <string>
+#include <vector>
+#include <map>
+#include <cmath>
+
+class TritonIdentityProducer : public TritonEDProducer<> {
+public:
+  explicit TritonIdentityProducer(edm::ParameterSet const& cfg)
+      : TritonEDProducer<>(cfg),
+        batchSize_(4) { }
+  void acquire(edm::Event const& iEvent, edm::EventSetup const& iSetup, Input& iInput) override {
+    //follow Triton QA tests for ragged input
+    std::vector<std::vector<float>> value_lists{
+      {2,2},
+      {4,4,4,4},
+      {1},
+      {3,3,3}
+    };
+
+    client_->setBatchSize(batchSize_);
+    auto& input1 = iInput.at("RAGGED_INPUT");
+    auto data1 = input1.allocate<float>();
+    for (int i = 0; i < batchSize_; ++i) {
+      (*data1)[i] = value_lists[i];
+      input1.setShape(0, (*data1)[i].size(), i);
+    }
+
+    // convert to server format
+    input1.toServer(data1);
+  }
+  void produce(edm::Event& iEvent, edm::EventSetup const& iSetup, Output const& iOutput) override {
+    // check the results
+    const auto& output1 = iOutput.at("RAGGED_OUTPUT");
+    // convert from server format
+    const auto& tmp = output1.fromServer<float>();
+    edm::LogInfo msg(debugName_);
+    for (int i = 0; i < batchSize_; ++i){
+        msg << "output " << i << ": ";
+        for(int j = 0; j < output1.shape()[0]; ++j){
+            msg << tmp[i][j] << " ";
+        }
+        msg << "\n";
+    }
+  }
+  ~TritonIdentityProducer() override = default;
+
+  static void fillDescriptions(edm::ConfigurationDescriptions& descriptions) {
+    edm::ParameterSetDescription desc;
+    TritonClient::fillPSetDescription(desc);
+    //to ensure distinct cfi names
+    descriptions.addWithDefaultLabel(desc);
+  }
+
+private:
+  int batchSize_;
+};
+
+DEFINE_FWK_MODULE(TritonIdentityProducer);
