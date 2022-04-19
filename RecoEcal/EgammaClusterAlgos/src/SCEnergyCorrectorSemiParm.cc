d2d14c95882834f3162ef894928f83f19a101f00
Tamas <Tamas.Almos.Vami@cern.ch>
2022-04-19 18:42:26 +0200

    Change regTrainedWithPS to true and update EGM HLT supercluster regression tags for Run 3 in MC

--- RecoEcal/EgammaClusterAlgos/src/SCEnergyCorrectorSemiParm.cc
+++ RecoEcal/EgammaClusterAlgos/src/SCEnergyCorrectorSemiParm.cc
@@ -51,7 +51,7 @@ SCEnergyCorrectorSemiParm::SCEnergyCorrectorSemiParm()
       caloGeom_(nullptr),
       isHLT_(false),
       isPhaseII_(false),
-      regTrainedWithPS_(false),
+      regTrainedWithPS_(true),
       applySigmaIetaIphiBug_(false),
       nHitsAboveThresholdEB_(0),
       nHitsAboveThresholdEE_(0),
@@ -67,7 +67,7 @@ SCEnergyCorrectorSemiParm::SCEnergyCorrectorSemiParm(const edm::ParameterSet& iC
 void SCEnergyCorrectorSemiParm::fillPSetDescription(edm::ParameterSetDescription& desc) {
   desc.add<bool>("isHLT", false);
   desc.add<bool>("isPhaseII", false);
-  desc.add<bool>("regTrainedWithPS", false);
+  desc.add<bool>("regTrainedWithPS", true);
   desc.add<bool>("applySigmaIetaIphiBug", false);
   desc.add<edm::InputTag>("ecalRecHitsEE", edm::InputTag("ecalRecHit", "EcalRecHitsEE"));
   desc.add<edm::InputTag>("ecalRecHitsEB", edm::InputTag("ecalRecHit", "EcalRecHitsEB"));
