32ecdf76503498abfbbe4d7cf61f9f649b5b207f
mmusich <marco.musich@cern.ch>
2022-04-20 22:20:29 +0200

    protect fetching of input histograms in SiPixelPhase1TrackComparisonHarvester and run it only if the gpuValidationPixel modifier is active

--- DQM/SiPixelPhase1Heterogeneous/python/SiPixelPhase1HeterogenousDQMHarvesting_cff.py
+++ DQM/SiPixelPhase1Heterogeneous/python/SiPixelPhase1HeterogenousDQMHarvesting_cff.py
@@ -1,4 +1,11 @@
 import FWCore.ParameterSet.Config as cms
-from DQM.SiPixelPhase1Heterogeneous.siPixelPhase1TrackComparisonHarvester_cfi import *
+siPixelPhase1HeterogenousDQMHarvesting = cms.Sequence() # empty sequence if not both CPU and GPU recos are run
 
-siPixelPhase1HeterogenousDQMHarvesting = cms.Sequence(siPixelPhase1TrackComparisonHarvester)
+from DQM.SiPixelPhase1Heterogeneous.siPixelPhase1TrackComparisonHarvester_cfi import *
+siPixelPhase1HeterogenousDQMComparisonHarvesting = cms.Sequence(siPixelPhase1TrackComparisonHarvester)
+
+# add the harvester in case of the validation modifier is active
+from Configuration.ProcessModifiers.gpuValidationPixel_cff import gpuValidationPixel
+gpuValidationPixel.toReplaceWith(siPixelPhase1HeterogenousDQMHarvesting,siPixelPhase1HeterogenousDQMComparisonHarvesting)
+
+
