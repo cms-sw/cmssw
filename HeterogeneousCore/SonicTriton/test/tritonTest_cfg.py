36a7ae9f3d3c13b5e0a5ff0e3baefbde58091acb
Kevin Pedro <kpedro88@gmail.com>
2022-04-20 14:47:26 -0500

    use simpler example, fix output printing

--- HeterogeneousCore/SonicTriton/test/tritonTest_cfg.py
+++ HeterogeneousCore/SonicTriton/test/tritonTest_cfg.py
@@ -8,7 +8,7 @@ models = {
     "TritonGraphProducer": ["gat_test"],
     "TritonGraphFilter": ["gat_test"],
     "TritonGraphAnalyzer": ["gat_test"],
-    "TritonIdentityProducer": ["ragged_acc_shape"],
+    "TritonIdentityProducer": ["ragged_io"],
 }
 
 # other choices
