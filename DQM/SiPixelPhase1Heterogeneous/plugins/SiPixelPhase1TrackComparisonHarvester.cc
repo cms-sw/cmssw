1465cf714595fc4b5f3be8f2996f5afac3f2804e
mmusich <marco.musich@cern.ch>
2022-04-20 18:29:59 +0200

    clean-up harvester code

--- DQM/SiPixelPhase1Heterogeneous/plugins/SiPixelPhase1TrackComparisonHarvester.cc
+++ DQM/SiPixelPhase1Heterogeneous/plugins/SiPixelPhase1TrackComparisonHarvester.cc
@@ -10,22 +10,18 @@
 class SiPixelPhase1TrackComparisonHarvester : public DQMEDHarvester {
 public:
   explicit SiPixelPhase1TrackComparisonHarvester(const edm::ParameterSet&);
-  ~SiPixelPhase1TrackComparisonHarvester() override;
+  ~SiPixelPhase1TrackComparisonHarvester() override = default;
   void dqmEndJob(DQMStore::IBooker& ibooker, DQMStore::IGetter& igetter) override;
 
   static void fillDescriptions(edm::ConfigurationDescriptions& descriptions);
 
 private:
-
   // ----------member data ---------------------------
   const std::string topFolder_;
 };
 
 SiPixelPhase1TrackComparisonHarvester::SiPixelPhase1TrackComparisonHarvester(const edm::ParameterSet& iConfig)
-  : topFolder_(iConfig.getParameter<std::string>("topFolderName")) {
-}
-
-SiPixelPhase1TrackComparisonHarvester::~SiPixelPhase1TrackComparisonHarvester() {}
+    : topFolder_(iConfig.getParameter<std::string>("topFolderName")) {}
 
 void SiPixelPhase1TrackComparisonHarvester::dqmEndJob(DQMStore::IBooker& ibooker, DQMStore::IGetter& igetter) {
   MonitorElement* hpt_eta_tkAllCPU = igetter.get(topFolder_ + "/ptetatrkAllCPU");
@@ -35,13 +31,14 @@ void SiPixelPhase1TrackComparisonHarvester::dqmEndJob(DQMStore::IBooker& ibooker
 
   ibooker.cd();
   ibooker.setCurrentFolder(topFolder_);
-  MonitorElement* hpt_eta_matchRatio = ibooker.book2D("matchingeff_pt_eta", "Efficiency of track matching; #eta; p_{T} [GeV];", 30, -M_PI, M_PI, 200, 0., 200.);
-  MonitorElement* hphi_z_matchRatio = ibooker.book2D("matchingeff_phi_z", "Efficiency of track matching; #phi; z [cm];", 30, -30., 30., 60, -M_PI, M_PI);
+  MonitorElement* hpt_eta_matchRatio = ibooker.book2D(
+      "matchingeff_pt_eta", "Efficiency of track matching; #eta; p_{T} [GeV];", 30, -M_PI, M_PI, 200, 0., 200.);
+  MonitorElement* hphi_z_matchRatio = ibooker.book2D(
+      "matchingeff_phi_z", "Efficiency of track matching; #phi; z [cm];", 30, -30., 30., 60, -M_PI, M_PI);
 
   hpt_eta_matchRatio->divide(hpt_eta_tkAllCPUmatched, hpt_eta_tkAllCPU, 1., 1., "B");
   hphi_z_matchRatio->divide(hphi_z_tkAllCPUmatched_, hphi_z_tkAllCPU_, 1., 1., "B");
 }
-
 
 #include "FWCore/ParameterSet/interface/ConfigurationDescriptions.h"
 #include "FWCore/ParameterSet/interface/ParameterSetDescription.h"
@@ -49,8 +46,6 @@ void SiPixelPhase1TrackComparisonHarvester::fillDescriptions(edm::ConfigurationD
   edm::ParameterSetDescription desc;
   desc.add<std::string>("topFolderName", "SiPixelHeterogeneous/PixelTrackCompareGPUvsCPU/");
   descriptions.add("siPixelPhase1TrackComparisonHarvester", desc);
-  // or use the following to generate the label from the module's C++ type
-  //descriptions.addWithDefaultLabel(desc);
 }
 
 //define this as a plug-in
