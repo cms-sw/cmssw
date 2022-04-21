e332b5f0f75f1c6784274f9df51d0d088e20bbb1
Andrea Trapote <andreatrapote21@gmail.com>
2022-04-21 18:14:02 +0200

    Updating Muon MVA ID working point tight

--- PhysicsTools/PatAlgos/plugins/PATMuonProducer.cc
+++ PhysicsTools/PatAlgos/plugins/PATMuonProducer.cc
@@ -1051,7 +1051,7 @@ void PATMuonProducer::produce(edm::Event& iEvent, const edm::EventSetup& iSetup)
     float mvaID = 0.0;
     constexpr int MVAsentinelValue = -99;
     constexpr float mvaIDmediumCut = 0.08;
-    constexpr float mvaIDtightCut = 0.49;
+    constexpr float mvaIDtightCut = 0.12;
     if (computeMuonIDMVA_) {
       if (muon.isLooseMuon()) {
         mvaID = globalCache()->muonMvaIDEstimator().computeMVAID(muon)[1];
@@ -1060,7 +1060,7 @@ void PATMuonProducer::produce(edm::Event& iEvent, const edm::EventSetup& iSetup)
       }
       muon.setMvaIDValue(mvaID);
       muon.setSelector(reco::Muon::MvaIDwpMedium, muon.mvaIDValue() > mvaIDmediumCut);
-      muon.setSelector(reco::Muon::MvaIDwpTight, muon.mvaIDValue() > mvaIDtightCut);
+      muon.setSelector(reco::Muon::MvaIDwpTight, muon.mvaIDValue() > mvaIDtightCut and dz < 0.5 and dxy < 0.2);
     }
 
     //SOFT MVA
