19365c48f0a340b86ac65ece1c5f877c0db33e0c
kpanos <panos.katsoulis@cern.ch>
2022-04-21 15:54:00 +0200

    new L1O2O unit tests and first config changes for L1CondDBPayloadWriter

--- CondTools/L1TriggerExt/python/L1CondDBPayloadWriterExt_cfi.py
+++ CondTools/L1TriggerExt/python/L1CondDBPayloadWriterExt_cfi.py
@@ -8,15 +8,16 @@ L1CondDBPayloadWriterExt = cms.EDAnalyzer(
     logTransactions = cms.bool(False),
     newL1TriggerKeyListExt = cms.bool(False),
     sysWriters = cms.vstring(
-       "L1TCaloParamsO2ORcd@CaloParams",
-       "L1TGlobalPrescalesVetosFractO2ORcd@L1TGlobalPrescalesVetosFract",
-       "L1TMuonBarrelParamsO2ORcd@L1TMuonBarrelParams",
-       "L1TMuonEndCapForestO2ORcd@L1TMuonEndCapForest",
-       "L1TMuonEndCapParamsO2ORcd@L1TMuonEndCapParams",
-       "L1TMuonGlobalParamsO2ORcd@L1TMuonGlobalParams",
-       "L1TMuonOverlapFwVersionO2ORcd@L1TMuonOverlapFwVersion",
-       "L1TMuonOverlapParamsO2ORcd@L1TMuonOverlapParams",
-       "L1TUtmTriggerMenuO2ORcd@L1TUtmTriggerMenu"
+        "L1TriggerKeyExtRcd@L1TriggerKeyExt",
+        "L1TCaloParamsO2ORcd@CaloParams",
+        "L1TGlobalPrescalesVetosFractO2ORcd@L1TGlobalPrescalesVetosFract",
+        "L1TMuonBarrelParamsO2ORcd@L1TMuonBarrelParams",
+#        "L1TMuonEndCapForestO2ORcd@L1TMuonEndCapForest",
+        "L1TMuonEndCapParamsO2ORcd@L1TMuonEndCapParams",
+        "L1TMuonGlobalParamsO2ORcd@L1TMuonGlobalParams",
+        "L1TMuonOverlapFwVersionO2ORcd@L1TMuonOverlapFwVersion",
+#        "L1TMuonOverlapParamsO2ORcd@L1TMuonOverlapParams",
+        "L1TUtmTriggerMenuO2ORcd@L1TUtmTriggerMenu"
     )
 )
 
