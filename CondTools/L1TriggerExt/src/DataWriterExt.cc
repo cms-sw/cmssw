4273e63970023f0bd8853b9c0b646e7a076e8123
kpanos <panos.katsoulis@cern.ch>
2022-04-22 20:15:06 +0200

    L1CondDBPayloadWriterExt light re-desine and config to run like before ClangBuild fix

--- CondTools/L1TriggerExt/src/DataWriterExt.cc
+++ CondTools/L1TriggerExt/src/DataWriterExt.cc
@@ -18,17 +18,18 @@ namespace l1t {
   // they need to be constructed at "construction time" to register what they produce
   DataWriterExt::DataWriterExt(const std::string& recordType) {
     WriterFactory* factory = WriterFactory::get();
-    writer = factory->create(recordType + "@Writer");
-    if (writer.get() == nullptr) {
+    writer_ = factory->create(recordType + "@Writer");
+    if (writer_.get() == nullptr) {
       throw cond::Exception("DataWriter: could not create WriterProxy with name " + recordType + "@Writer");
     }
     edm::LogVerbatim("L1-O2O DataWriterExt::DataWriterExt")
-        << "Created new " << typeid(writer).name() << "  | address " << writer.get() << "  | rcd " << recordType;
+        << "Created new " << typeid(writer_).name() << "  | address " << writer_.get() << "  | rcd " << recordType;
   }
+
   DataWriterExt::~DataWriterExt() {}
 
   std::string DataWriterExt::writePayload(const edm::EventSetup& setup) {
-    edm::LogVerbatim("L1-O2O DataWriterExt::writePayload") << "Will use stored writer at " << writer.get();
+    edm::LogVerbatim("L1-O2O DataWriterExt::writePayload") << "Will use stored writer at " << writer_.get();
 
     edm::Service<cond::service::PoolDBOutputService> poolDb;
     if (!poolDb.isAvailable()) {
@@ -38,19 +39,20 @@ namespace l1t {
     // 2010-02-16: Move session and transaction to WriterProxy::save().  Otherwise, if another transaction is
     // started while WriterProxy::save() is called (e.g. in a ESProducer like L1ConfigOnlineProdBase), the
     // transaction here will become read-only.
-    std::string payloadToken = writer->save(setup);
-    edm::LogVerbatim("L1-O2O") << typeid(*writer).name() << " PAYLOAD TOKEN " << payloadToken;
+    std::string payloadToken = writer_->save(setup);
+    auto& writerRef = *writer_.get();
+    edm::LogVerbatim("L1-O2O") << typeid(writerRef).name() << " PAYLOAD TOKEN " << payloadToken;
 
     return payloadToken;
   }
 
   std::string DataWriterExt::writePayload(const edm::EventSetup& setup, const std::string& recordType) {
     WriterFactory* factory = WriterFactory::get();
-    WriterProxyPtr writer_(factory->create(recordType + "@Writer"));
-    if (writer_.get() == nullptr) {
+    WriterProxyPtr writer(factory->create(recordType + "@Writer"));
+    if (writer.get() == nullptr) {
       throw cond::Exception("DataWriter: could not create WriterProxy with name " + recordType + "@Writer");
     }
-    writer = std::move(writer_);
+    writer_ = std::move(writer);
 
     edm::Service<cond::service::PoolDBOutputService> poolDb;
     if (!poolDb.isAvailable()) {
@@ -69,9 +71,9 @@ namespace l1t {
 
     // update key to have new payload registered for record-type pair.
     //  std::string payloadToken = writer->save( setup, session ) ;
-    std::string payloadToken = writer->save(setup);
-
-    edm::LogVerbatim("L1-O2O") << typeid(*writer).name() << " PAYLOAD TOKEN " << payloadToken;
+    std::string payloadToken = writer_->save(setup);
+    auto& writerRef = *writer_.get();
+    edm::LogVerbatim("L1-O2O") << typeid(writerRef).name() << " PAYLOAD TOKEN " << payloadToken;
 
     ////  tr.close();
     //   tr.commit ();
