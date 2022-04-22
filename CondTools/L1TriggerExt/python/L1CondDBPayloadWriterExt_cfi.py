8c2a86cd80285a4164b475bd2f4a5925c781cd4e
kpanos <panos.katsoulis@cern.ch>
2022-04-22 20:15:06 +0200

    L1CondDBPayloadWriterExt light re-desine and config to run like before ClangBuild fix

--- CondTools/L1TriggerExt/python/L1CondDBPayloadWriterExt_cfi.py
+++ CondTools/L1TriggerExt/python/L1CondDBPayloadWriterExt_cfi.py
@@ -12,11 +12,11 @@ L1CondDBPayloadWriterExt = cms.EDAnalyzer(
         "L1TCaloParamsO2ORcd@CaloParams",
         "L1TGlobalPrescalesVetosFractO2ORcd@L1TGlobalPrescalesVetosFract",
         "L1TMuonBarrelParamsO2ORcd@L1TMuonBarrelParams",
-#        "L1TMuonEndCapForestO2ORcd@L1TMuonEndCapForest",
+        "L1TMuonEndCapForestO2ORcd@L1TMuonEndCapForest",
         "L1TMuonEndCapParamsO2ORcd@L1TMuonEndCapParams",
         "L1TMuonGlobalParamsO2ORcd@L1TMuonGlobalParams",
         "L1TMuonOverlapFwVersionO2ORcd@L1TMuonOverlapFwVersion",
-#        "L1TMuonOverlapParamsO2ORcd@L1TMuonOverlapParams",
+        "L1TMuonOverlapParamsO2ORcd@L1TMuonOverlapParams",
         "L1TUtmTriggerMenuO2ORcd@L1TUtmTriggerMenu"
     )
 )
