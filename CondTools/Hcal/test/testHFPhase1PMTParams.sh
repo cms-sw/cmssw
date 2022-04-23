6338ad940fcae6a97397628dd010819b51856a12
Igor <i.volobouev@ttu.edu>
2022-04-23 18:21:51 +0200

    Running write_HFPhase1PMTParam executable without explicit path

--- CondTools/Hcal/test/testHFPhase1PMTParams.sh
+++ CondTools/Hcal/test/testHFPhase1PMTParams.sh
@@ -3,7 +3,7 @@
 function die { echo $1: status $2 ; exit $2; }
 
 echo "TESTING HFPhase1PMTParams generation code ..."
-${CMSSW_BASE}/test/${SCRAM_ARCH}/write_HFPhase1PMTParams 1 HFPhase1PMTParams_V00_mc.bbin || die "Failure running write_HFPhase1PMTParams" $?
+write_HFPhase1PMTParams 1 HFPhase1PMTParams_V00_mc.bbin || die "Failure running write_HFPhase1PMTParams" $?
 
 echo "TESTING HFPhase1PMTParams database creation code ..."
 cmsRun ${LOCAL_TEST_DIR}/HFPhase1PMTParamsDBWriter_cfg.py || die "Failure running HFPhase1PMTParamsDBWriter_cfg.py" $?
