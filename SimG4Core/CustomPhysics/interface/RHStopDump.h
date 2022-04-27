49e9e1f3ac423e8647e5ab91292ff346db3a1c89
Sunanda <sunanda.banerjee@cern.ch>
2022-04-27 12:07:00 +0200

    Replace getByLabel with getHandle in SimG4Core/CustomPhysics

--- SimG4Core/CustomPhysics/interface/RHStopDump.h
+++ SimG4Core/CustomPhysics/interface/RHStopDump.h
@@ -12,12 +12,20 @@
 class RHStopDump : public edm::one::EDAnalyzer<edm::one::SharedResources> {
 public:
   explicit RHStopDump(const edm::ParameterSet&);
-  ~RHStopDump() override{};
+  ~RHStopDump() override = default;
   void analyze(const edm::Event&, const edm::EventSetup&) override;
 
 private:
   std::ofstream mStream;
-  std::string mProducer;
+  const std::string mProducer;
+  const edm::EDGetTokenT<std::vector<std::string> > tokNames_;
+  const edm::EDGetTokenT<std::vector<float> > tokenXs_;
+  const edm::EDGetTokenT<std::vector<float> > tokenYs_;
+  const edm::EDGetTokenT<std::vector<float> > tokenZs_;
+  const edm::EDGetTokenT<std::vector<float> > tokenTs_;
+  const edm::EDGetTokenT<std::vector<int> > tokenIds_;
+  const edm::EDGetTokenT<std::vector<float> > tokenMasses_;
+  const edm::EDGetTokenT<std::vector<float> > tokenCharges_;
 };
 
 #endif
