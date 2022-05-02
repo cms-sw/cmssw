7ab5decca825532dceaa4b20bc360c050140e9f9
Chris Jones <chrisdjones15@gmail.com>
2022-05-02 17:34:00 -0400

    Added IOV synchronization following to Tracer

--- FWCore/Services/plugins/Tracer.cc
+++ FWCore/Services/plugins/Tracer.cc
@@ -13,6 +13,7 @@
 #include "FWCore/Framework/interface/ComponentDescription.h"
 #include "FWCore/Framework/interface/DataKey.h"
 #include "FWCore/Framework/interface/EventSetupRecordKey.h"
+#include "FWCore/Framework/interface/IOVSyncValue.h"
 
 #include "FWCore/ServiceRegistry/interface/ServiceMaker.h"
 
@@ -437,6 +438,19 @@ Tracer::Tracer(ParameterSet const& iPS, ActivityRegistry& iRegistry)
               << " lumi = " << iContext.luminosityBlockID().luminosityBlock();
         }
         out << " : time = " << iContext.timestamp().value();
+      });
+          
+  iRegistry.esSyncIOVQueuingSignal_.connect(
+      [this](edm::IOVSyncValue const& iSync) {
+        LogAbsolute("Tracer") <<TimeStamper(printTimestamps_) <<indention_ << indention_ <<" queuing: EventSetup synchronization "<<iSync.eventID();
+      });
+  iRegistry.preESSyncIOVSignal_.connect(
+      [this](edm::IOVSyncValue const& iSync) {
+        LogAbsolute("Tracer") <<TimeStamper(printTimestamps_) <<indention_ <<indention_ <<" pre: EventSetup synchronizing "<<iSync.eventID();
+      });
+  iRegistry.postESSyncIOVSignal_.connect(
+      [this](edm::IOVSyncValue const& iSync) {
+        LogAbsolute("Tracer") <<TimeStamper(printTimestamps_) <<indention_ << indention_ <<" post: EventSetup synchronizing "<<iSync.eventID();
       });
 }
 
