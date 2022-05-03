d125ff729a754d62b16b10dd8ad1d31c05223446
Georgios Karathanasis <georgios.karathanasis@cern.ch>
2022-05-03 15:01:58 +0200

    applying comments #1
    (cherry picked from commit 66b19f9179d6bba6b3590957b5a32a67689c65f5)
    (cherry picked from commit 3be1082753e23ff4b93ffd4ef4738991cf5f3c91)

--- L1Trigger/L1TTrackMatch/python/L1TrackJetProducer_cfi.py
+++ L1Trigger/L1TTrackMatch/python/L1TrackJetProducer_cfi.py
@@ -4,9 +4,7 @@ from L1Trigger.VertexFinder.VertexProducer_cff import VertexProducer
 #prompt jet selection
 L1TrackJets = cms.EDProducer('L1TrackJetProducer',
 	L1TrackInputTag= cms.InputTag("TTTracksFromTrackletEmulation", "Level1TTTracks"),
-#        L1TrackInputTag= cms.InputTag("TTTracksFromTrackletEmulation","Level1TTTracks","IN"),
         L1PVertexCollection = cms.InputTag("VertexProducer", VertexProducer.l1VertexCollectionName.value()),
-#        L1PVertexCollection = cms.InputTag("L1TkPrimaryVertex"),
         MaxDzTrackPV = cms.double( 1.0 ), #max distance from PV;negative=no cut
 	trk_zMax = cms.double (15.) ,    # maximum track z
 	trk_ptMax = cms.double(200.),    # maximumum track pT before saturation [GeV]
@@ -23,8 +21,10 @@ L1TrackJets = cms.EDProducer('L1TrackJetProducer',
 	zBins=cms.int32(1),
 	d0_cutNStubs4=cms.double(-1),
 	d0_cutNStubs5=cms.double(-1),
-	lowpTJetMinTrackMultiplicity=cms.int32(2),
-	highpTJetMinTrackMultiplicity=cms.int32(3),
+	lowpTJetMinTrackMultiplicity=cms.int32(2),#used only for more than 1 z-bins (ie not *prompt*)
+        lowpTJetThreshold=cms.double(50.),#used only for more than 1 z-bins (ie not *prompt*)
+	highpTJetMinTrackMultiplicity=cms.int32(3),#used only for more than 1 z-bins (ie not *prompt*)
+        highpTJetThreshold=cms.double(100.),#used only for more than 1 z-bins (ie not *prompt*)
 	displaced=cms.bool(False), #Flag for displaced tracks
 	nStubs4DisplacedChi2=cms.double(5.0), #Displaced track quality flags for loose/tight
 	nStubs4DisplacedBend=cms.double(1.7),
@@ -51,10 +51,12 @@ L1TrackJetsExtended = cms.EDProducer('L1TrackJetProducer',
 	etaBins=cms.int32(24), #number of eta bins
 	phiBins=cms.int32(27), #number of phi bins
 	zBins=cms.int32(1),    #number of z bins
-	d0_cutNStubs4=cms.double(-1),    # -1 excludes nstub=4 from disp tag
-	d0_cutNStubs5=cms.double(0.22),  # -1 excludes nstub>4 from disp tag
-	lowpTJetMinTrackMultiplicity=cms.int32(2),  #used only on zbin finding
-	highpTJetMinTrackMultiplicity=cms.int32(3), #used only on zbin finding
+	d0_cutNStubs4=cms.double(-1),    # -1 excludes nstub=4 from disp tag process
+	d0_cutNStubs5=cms.double(0.22),  # -1 excludes nstub>4 from disp tag process
+	lowpTJetMinTrackMultiplicity=cms.int32(2),  #relevant only when N of z-bins >1; excludes from the HT calculation jets with low number of very energetic tracks; this cut selects the threshold on number of tracks
+        lowpTJetThreshold=cms.double(50.), # this threshold controls the pT of the jet
+	highpTJetMinTrackMultiplicity=cms.int32(3), #same as above for a different WP of tracks / pT
+        highpTJetThreshold=cms.double(100.),
 	displaced=cms.bool(True), #Flag for displaced tracks
 	nStubs4DisplacedChi2=cms.double(3.3), #Disp tracks selection [trk<cut]
 	nStubs4DisplacedBend=cms.double(2.3),
