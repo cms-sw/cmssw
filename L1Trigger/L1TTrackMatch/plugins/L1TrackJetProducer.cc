9c6ad087c835665bbaa9c45b50e87dfcfb1a4660
Georgios Karathanasis <georgios.karathanasis@cern.ch>
2022-05-03 15:01:58 +0200

    rebasing update track jets
    (cherry picked from commit 66b19f9179d6bba6b3590957b5a32a67689c65f5)
    (cherry picked from commit 4e8b4b038c29c5de21f9b465090e9766d03504a8)

--- L1Trigger/L1TTrackMatch/plugins/L1TrackJetProducer.cc
+++ L1Trigger/L1TTrackMatch/plugins/L1TrackJetProducer.cc
@@ -29,7 +29,6 @@
 #include "Geometry/CommonTopologies/interface/PixelGeomDetType.h"
 #include "Geometry/TrackerGeometryBuilder/interface/TrackerGeometry.h"
 
-#include "DataFormats/L1TCorrelator/interface/TkPrimaryVertex.h"
 #include "DataFormats/L1Trigger/interface/Vertex.h"
 
 #include "L1TrackJetProducer.h"
@@ -47,7 +46,6 @@ using namespace std;
 using namespace edm;
 using namespace l1t;
 
-
 class L1TrackJetProducer : public stream::EDProducer<> {
 public:
   explicit L1TrackJetProducer(const ParameterSet &);
@@ -58,7 +56,6 @@ public:
   static void fillDescriptions(ConfigurationDescriptions &descriptions);
   bool trackQualityCuts(int trk_nstub, float trk_chi2, float trk_bendchi2);
 
-
 private:
   void beginStream(StreamID) override;
   void produce(Event &, const EventSetup &) override;
@@ -66,9 +63,9 @@ private:
 
   // ----------member data ---------------------------
 
-  const EDGetTokenT<vector<TTTrack<Ref_Phase2TrackerDigi_> > > trackToken_;
-  const edm::EDGetTokenT<std::vector<l1t::Vertex> > PVtxToken_;
-  vector<Ptr<L1TTTrackType> > L1TrkPtrs_;
+  const EDGetTokenT<vector<TTTrack<Ref_Phase2TrackerDigi_>>> trackToken_;
+  const edm::EDGetTokenT<std::vector<l1t::Vertex>> PVtxToken_;
+  vector<Ptr<L1TTTrackType>> L1TrkPtrs_;
   vector<int> tdtrk_;
   float trkZMax_;
   float trkPtMax_;
@@ -80,7 +77,9 @@ private:
   float nStubs5PromptBend_;
   int trkNPSStubMin_;
   int lowpTJetMinTrackMultiplicity_;
+  float lowpTJetThreshold_;
   int highpTJetMinTrackMultiplicity_;
+  float highpTJetThreshold_;
   int zBins_;
   int etaBins_;
   int phiBins_;
@@ -100,9 +99,8 @@ private:
 };
 
 L1TrackJetProducer::L1TrackJetProducer(const ParameterSet &iConfig)
-    : trackToken_( consumes<vector<TTTrack<Ref_Phase2TrackerDigi_> > >(iConfig.getParameter<InputTag>("L1TrackInputTag")) ),
-      PVtxToken_( consumes< vector<l1t::Vertex> >(iConfig.getParameter<InputTag>("L1PVertexCollection")) )
-  {
+    : trackToken_(consumes<vector<TTTrack<Ref_Phase2TrackerDigi_>>>(iConfig.getParameter<InputTag>("L1TrackInputTag"))),
+      PVtxToken_(consumes<vector<l1t::Vertex>>(iConfig.getParameter<InputTag>("L1PVertexCollection"))) {
   trkZMax_ = (float)iConfig.getParameter<double>("trk_zMax");
   trkPtMax_ = (float)iConfig.getParameter<double>("trk_ptMax");
   trkPtMin_ = (float)iConfig.getParameter<double>("trk_ptMin");
@@ -119,7 +117,9 @@ L1TrackJetProducer::L1TrackJetProducer(const ParameterSet &iConfig)
   d0CutNStubs4_ = (float)iConfig.getParameter<double>("d0_cutNStubs4");
   d0CutNStubs5_ = (float)iConfig.getParameter<double>("d0_cutNStubs5");
   lowpTJetMinTrackMultiplicity_ = (int)iConfig.getParameter<int>("lowpTJetMinTrackMultiplicity");
+  lowpTJetThreshold_ = (float)iConfig.getParameter<double>("lowpTJetThreshold");
   highpTJetMinTrackMultiplicity_ = (int)iConfig.getParameter<int>("highpTJetMinTrackMultiplicity");
+  highpTJetThreshold_ = (float)iConfig.getParameter<double>("highpTJetThreshold");
   displaced_ = iConfig.getParameter<bool>("displaced");
   nStubs4DisplacedChi2_ = (float)iConfig.getParameter<double>("nStubs4DisplacedChi2");
   nStubs5DisplacedChi2_ = (float)iConfig.getParameter<double>("nStubs5DisplacedChi2");
@@ -128,7 +128,7 @@ L1TrackJetProducer::L1TrackJetProducer(const ParameterSet &iConfig)
   nDisplacedTracks_ = (int)iConfig.getParameter<int>("nDisplacedTracks");
   dzPVTrk_ = (float)iConfig.getParameter<double>("MaxDzTrackPV");
 
-  zStep_ = 2.0 * trkZMax_ / (zBins_+1);    // added +1 in denom
+  zStep_ = 2.0 * trkZMax_ / (zBins_ + 1);  // added +1 in denom
   etaStep_ = 2.0 * trkEtaMax_ / etaBins_;  //etaStep is the width of an etabin
   phiStep_ = 2 * M_PI / phiBins_;          ////phiStep is the width of a phibin
 
@@ -150,17 +150,17 @@ void L1TrackJetProducer::produce(Event &iEvent, const EventSetup &iSetup) {
   iSetup.get<TrackerDigiGeometryRecord>().get(tGeomHandle);
   const TrackerTopology *const tTopo = tTopoHandle.product();
 
-  edm::Handle<vector<TTTrack<Ref_Phase2TrackerDigi_> > > TTTrackHandle;
+  edm::Handle<vector<TTTrack<Ref_Phase2TrackerDigi_>>> TTTrackHandle;
   iEvent.getByToken(trackToken_, TTTrackHandle);
 
-  edm::Handle< std::vector<l1t::Vertex> > PVtx;
+  edm::Handle<std::vector<l1t::Vertex>> PVtx;
   iEvent.getByToken(PVtxToken_, PVtx);
-  float PVz = (PVtx->at(0)).z0(); 
+  float PVz = (PVtx->at(0)).z0();
 
   L1TrkPtrs_.clear();
   tdtrk_.clear();
 
-  for (unsigned int this_l1track=0; this_l1track<TTTrackHandle->size(); this_l1track++){
+  for (unsigned int this_l1track = 0; this_l1track < TTTrackHandle->size(); this_l1track++) {
     edm::Ptr<L1TTTrackType> trkPtr(TTTrackHandle, this_l1track);
     float trk_pt = trkPtr->momentum().perp();
     int trk_nstubs = (int)trkPtr->getStubRefs().size();
@@ -168,7 +168,7 @@ void L1TrackJetProducer::produce(Event &iEvent, const EventSetup &iSetup) {
     float trk_d0 = trkPtr->d0();
     float trk_bendchi2 = trkPtr->stubPtConsistency();
 
-    int trk_nPS = 0; 
+    int trk_nPS = 0;
     for (int istub = 0; istub < trk_nstubs; istub++) {  // loop over the stubs
       DetId detId(trkPtr->getStubRefs().at(istub)->getDetId());
       if (detId.det() == DetId::Detector::Tracker) {
@@ -179,145 +179,145 @@ void L1TrackJetProducer::produce(Event &iEvent, const EventSetup &iSetup) {
     }
     // select tracks
     if (trk_nPS < trkNPSStubMin_)
-      continue; 
+      continue;
     if (!trackQualityCuts(trk_nstubs, trk_chi2dof, trk_bendchi2))
       continue;
-    if ( fabs(PVz-trkPtr->z0()) > dzPVTrk_ && dzPVTrk_>0 )
+    if (std::abs(PVz - trkPtr->z0()) > dzPVTrk_ && dzPVTrk_ > 0)
       continue;
-    if (fabs(trkPtr->z0()) > trkZMax_)
+    if (std::abs(trkPtr->z0()) > trkZMax_)
       continue;
-    if ( fabs(trkPtr->momentum().eta())>trkEtaMax_)
+    if (std::abs(trkPtr->momentum().eta()) > trkEtaMax_)
       continue;
     if (trk_pt < trkPtMin_)
       continue;
     L1TrkPtrs_.push_back(trkPtr);
 
-    if ((fabs(trk_d0) > d0CutNStubs5_ && trk_nstubs >= 5 && d0CutNStubs5_>=0) || (trk_nstubs == 4 && fabs(trk_d0) > d0CutNStubs4_ && d0CutNStubs4_>=0))
-      tdtrk_.push_back(1); //displaced track
+    if ((std::abs(trk_d0) > d0CutNStubs5_ && trk_nstubs >= 5 && d0CutNStubs5_ >= 0) ||
+        (trk_nstubs == 4 && std::abs(trk_d0) > d0CutNStubs4_ && d0CutNStubs4_ >= 0))
+      tdtrk_.push_back(1);  //displaced track
     else
       tdtrk_.push_back(0);  // not displaced track
   }
 
   if (!L1TrkPtrs_.empty()) {
-   MaxZBin mzb;
-   mzb.znum=-1;
-   mzb.nclust=-1;
-   mzb.ht=-1;
-   EtaPhiBin epbins_default[phiBins_][etaBins_];  // create grid of phiBins
+    MaxZBin mzb;
+    mzb.znum = -1;
+    mzb.nclust = -1;
+    mzb.ht = -1;
+    EtaPhiBin epbins_default[phiBins_][etaBins_];  // create grid of phiBins
 
-   float phi = -1.0 * M_PI;
-   float eta = -1.0 * trkEtaMax_;
-   for (int i = 0; i < phiBins_; ++i) {
-     eta = -1.0 * trkEtaMax_;
-     for (int j = 0; j < etaBins_; ++j) {
-       epbins_default[i][j].phi = (phi + (phi + phiStep_) ) / 2.0; // phimin=phi; phimax= phimin+step
-       epbins_default[i][j].eta = (eta + (eta + etaStep_) ) / 2.0; // phimin=phi; phimax phimin+step
-       eta += etaStep_;
-     }  // for each etabin
-     phi += phiStep_;
-   }  // for each phibin (finished creating epbins) 
+    float phi = -1.0 * M_PI;
+    float eta = -1.0 * trkEtaMax_;
+    for (int i = 0; i < phiBins_; ++i) {
+      eta = -1.0 * trkEtaMax_;
+      for (int j = 0; j < etaBins_; ++j) {
+        epbins_default[i][j].phi = (phi + (phi + phiStep_)) / 2.0;  // phimin=phi; phimax= phimin+step
+        epbins_default[i][j].eta = (eta + (eta + etaStep_)) / 2.0;  // phimin=phi; phimax phimin+step
+        eta += etaStep_;
+      }  // for each etabin
+      phi += phiStep_;
+    }  // for each phibin (finished creating epbins)
 
-  std::vector<float> zmins,zmaxs;
-  for (int zbin = 0; zbin < zBins_; zbin++) {
-    zmins.push_back(-1.0 * trkZMax_ + zStep_*zbin);
-    zmaxs.push_back(-1.0 * trkZMax_ + zStep_*zbin + zStep_*2);
-  }
-
-  // create vectors that hold data
-  std::vector<std::vector<EtaPhiBin>> L1clusters;
-  L1clusters.reserve(phiBins_);
-  std::vector<EtaPhiBin> L2clusters;
-
-  for (int i = 0; i < phiBins_; ++i) { 
-   for (int j = 0; j < etaBins_; ++j) {
-      epbins_default[i][j].pTtot = 0;
-      epbins_default[i][j].used = false;
-      epbins_default[i][j].numtracks = 0;
-      epbins_default[i][j].numttrks = 0;
-      epbins_default[i][j].numtdtrks = 0;
-      epbins_default[i][j].numttdtrks = 0;
-      epbins_default[i][j].trackidx.clear();
+    std::vector<float> zmins, zmaxs;
+    for (int zbin = 0; zbin < zBins_; zbin++) {
+      zmins.push_back(-1.0 * trkZMax_ + zStep_ * zbin);
+      zmaxs.push_back(-1.0 * trkZMax_ + zStep_ * zbin + zStep_ * 2);
     }
-  } 
 
-  for (unsigned int zbin = 0; zbin < zmins.size(); ++zbin) {
-     // initialize matrices 
-     float zmin = zmins[zbin];
-     float zmax = zmaxs[zbin];
-     EtaPhiBin epbins[phiBins_][etaBins_];
-     std::copy(&epbins_default[0][0], &epbins_default[0][0]+phiBins_*etaBins_,&epbins[0][0]); 
+    // create vectors that hold data
+    std::vector<std::vector<EtaPhiBin>> L1clusters;
+    L1clusters.reserve(phiBins_);
+    std::vector<EtaPhiBin> L2clusters;
 
-     //clear containers
-     L1clusters.clear();
-     L2clusters.clear();
+    for (int i = 0; i < phiBins_; ++i) {
+      for (int j = 0; j < etaBins_; ++j) {
+        epbins_default[i][j].pTtot = 0;
+        epbins_default[i][j].used = false;
+        epbins_default[i][j].numtracks = 0;
+        epbins_default[i][j].numttrks = 0;
+        epbins_default[i][j].numtdtrks = 0;
+        epbins_default[i][j].numttdtrks = 0;
+        epbins_default[i][j].trackidx.clear();
+      }
+    }
 
-     // fill grid
-     for (unsigned int k = 0; k < L1TrkPtrs_.size(); ++k) {
-       float trkpt = L1TrkPtrs_[k]->momentum().perp();
-       float trketa = L1TrkPtrs_[k]->momentum().eta();
-       float trkphi = L1TrkPtrs_[k]->momentum().phi();
-       float trkZ = L1TrkPtrs_[k]->z0();
-       if ( zmax < trkZ)
+    for (unsigned int zbin = 0; zbin < zmins.size(); ++zbin) {
+      // initialize matrices
+      float zmin = zmins[zbin];
+      float zmax = zmaxs[zbin];
+      EtaPhiBin epbins[phiBins_][etaBins_];
+      std::copy(&epbins_default[0][0], &epbins_default[0][0] + phiBins_ * etaBins_, &epbins[0][0]);
+
+      //clear containers
+      L1clusters.clear();
+      L2clusters.clear();
+
+      // fill grid
+      for (unsigned int k = 0; k < L1TrkPtrs_.size(); ++k) {
+        float trkpt = L1TrkPtrs_[k]->momentum().perp();
+        float trketa = L1TrkPtrs_[k]->momentum().eta();
+        float trkphi = L1TrkPtrs_[k]->momentum().phi();
+        float trkZ = L1TrkPtrs_[k]->z0();
+        if (zmax < trkZ)
           continue;
-       if (zbin==0){
-         if ( zmin > trkZ ) 
+        if (zbin == 0) {
+          if (zmin > trkZ)
             continue;
-       } else{
-         if ( zmin > trkZ || zmin ==trkZ )
+        } else {
+          if (zmin >= trkZ)
             continue;
-       }
-       for (int i = 0; i < phiBins_; ++i) {
-         for (int j = 0; j < etaBins_; ++j) {
-           float eta_min=epbins[i][j].eta - etaStep_ / 2.0; //eta min
-           float eta_max=epbins[i][j].eta + etaStep_ / 2.0; //eta max
-           float phi_min=epbins[i][j].phi - phiStep_ / 2.0; //phi min
-           float phi_max=epbins[i][j].phi + phiStep_ / 2.0; //phi max         
+        }
+        for (int i = 0; i < phiBins_; ++i) {
+          for (int j = 0; j < etaBins_; ++j) {
+            float eta_min = epbins[i][j].eta - etaStep_ / 2.0;  //eta min
+            float eta_max = epbins[i][j].eta + etaStep_ / 2.0;  //eta max
+            float phi_min = epbins[i][j].phi - phiStep_ / 2.0;  //phi min
+            float phi_max = epbins[i][j].phi + phiStep_ / 2.0;  //phi max
 
-           if ( ( trketa < eta_min ) || ( trketa > eta_max ) ||
-              ( trkphi < phi_min ) || ( trkphi > phi_max )   )
+            if ((trketa < eta_min) || (trketa > eta_max) || (trkphi < phi_min) || (trkphi > phi_max))
               continue;
 
-           if (trkpt < trkPtMax_)
-             epbins[i][j].pTtot += trkpt;
-           else
-             epbins[i][j].pTtot += trkPtMax_;
-           epbins[i][j].numtdtrks += tdtrk_[k];
-           epbins[i][j].trackidx.push_back(k);
-           ++epbins[i][j].numtracks;
-        } // for each phibin
-      } // for each phibin
-   } //end loop over tracks
+            if (trkpt < trkPtMax_)
+              epbins[i][j].pTtot += trkpt;
+            else
+              epbins[i][j].pTtot += trkPtMax_;
+            epbins[i][j].numtdtrks += tdtrk_[k];
+            epbins[i][j].trackidx.push_back(k);
+            ++epbins[i][j].numtracks;
+          }  // for each phibin
+        }    // for each phibin
+      }      //end loop over tracks
 
-   // first layer clustering - in eta for all phi bins
-   for (int phibin = 0; phibin < phiBins_; ++phibin) {
-     L1clusters.push_back( L1_clustering(epbins[phibin],etaBins_,etaStep_) );
-   }
+      // first layer clustering - in eta for all phi bins
+      for (int phibin = 0; phibin < phiBins_; ++phibin) {
+        L1clusters.push_back(L1_clustering(epbins[phibin], etaBins_, etaStep_));
+      }
 
-   //second layer clustering in phi for all eta clusters  
-   L2clusters= L2_clustering(L1clusters, phiBins_, phiStep_, etaStep_);
+      //second layer clustering in phi for all eta clusters
+      L2clusters = L2_clustering(L1clusters, phiBins_, phiStep_, etaStep_);
 
- // sum all cluster pTs in this zbin to find max
-   float sum_pt = 0;
-   for (unsigned int k = 0; k < L2clusters.size(); ++k) {
-     if (L2clusters[k].pTtot > 50 && L2clusters[k].numtracks < lowpTJetMinTrackMultiplicity_)
+      // sum all cluster pTs in this zbin to find max
+      float sum_pt = 0;
+      for (unsigned int k = 0; k < L2clusters.size(); ++k) {
+        if (L2clusters[k].pTtot > lowpTJetThreshold_ && L2clusters[k].numtracks < lowpTJetMinTrackMultiplicity_)
+          continue;
+        if (L2clusters[k].pTtot > highpTJetThreshold_ && L2clusters[k].numtracks < highpTJetMinTrackMultiplicity_)
+          continue;
+        if (L2clusters[k].pTtot > minTrkJetpT_)
+          sum_pt += L2clusters[k].pTtot;
+      }
+
+      if (sum_pt < mzb.ht)
         continue;
-     if (L2clusters[k].pTtot > 100 && L2clusters[k].numtracks < highpTJetMinTrackMultiplicity_)
-        continue;
-     if (L2clusters[k].pTtot > minTrkJetpT_)
-        sum_pt += L2clusters[k].pTtot;
-   }
+      // if ht is larger than previous max, this is the new vertex zbin
+      mzb.ht = sum_pt;
+      mzb.znum = zbin;
+      mzb.clusters = L2clusters;
+      mzb.nclust = L2clusters.size();
+      mzb.zbincenter = (zmin + zmax) / 2.0;
+    }  //zbin loop
 
-   if (sum_pt < mzb.ht) continue;
-   // if ht is larger than previous max, this is the new vertex zbin
-   mzb.ht=sum_pt;
-   mzb.znum = zbin;
-   mzb.clusters=L2clusters;
-   mzb.nclust = L2clusters.size();
-   mzb.zbincenter = (zmin + zmax) / 2.0;
-  }//zbin loop
-   
- 
-    vector<Ptr<L1TTTrackType> > L1TrackAssocJet;
+    vector<Ptr<L1TTTrackType>> L1TrackAssocJet;
     for (unsigned int j = 0; j < mzb.clusters.size(); ++j) {
       float jetEta = mzb.clusters[j].eta;
       float jetPhi = mzb.clusters[j].phi;
@@ -327,22 +327,20 @@ void L1TrackJetProducer::produce(Event &iEvent, const EventSetup &iSetup) {
       float jetPz = jetPt * sinh(jetEta);
       float jetP = jetPt * cosh(jetEta);
       int totalDisptrk = mzb.clusters[j].numtdtrks;
-      bool isDispJet=false;
-      if (totalDisptrk>nDisplacedTracks_ || totalDisptrk==nDisplacedTracks_)
-         isDispJet=true;
+      bool isDispJet = false;
+      if (totalDisptrk > nDisplacedTracks_ || totalDisptrk == nDisplacedTracks_)
+        isDispJet = true;
 
       math::XYZTLorentzVector jetP4(jetPx, jetPy, jetPz, jetP);
       L1TrackAssocJet.clear();
       for (unsigned int itrk = 0; itrk < mzb.clusters[j].trackidx.size(); itrk++)
-         L1TrackAssocJet.push_back(L1TrkPtrs_[mzb.clusters[j].trackidx[itrk]]);
+        L1TrackAssocJet.push_back(L1TrkPtrs_[mzb.clusters[j].trackidx[itrk]]);
 
-      TkJet trkJet(jetP4, L1TrackAssocJet, mzb.zbincenter, 
-                   mzb.clusters[j].numtracks, 0, totalDisptrk, 0, isDispJet);
+      TkJet trkJet(jetP4, L1TrackAssocJet, mzb.zbincenter, mzb.clusters[j].numtracks, 0, totalDisptrk, 0, isDispJet);
 
       if (!L1TrackAssocJet.empty())
         L1L1TrackJetProducer->push_back(trkJet);
     }
- 
 
     if (displaced_)
       iEvent.put(std::move(L1L1TrackJetProducer), "L1TrackJetsExtended");
@@ -355,23 +353,18 @@ void L1TrackJetProducer::beginStream(StreamID) {}
 
 void L1TrackJetProducer::endStream() {}
 
-bool L1TrackJetProducer::trackQualityCuts(
-    int trk_nstub, float trk_chi2, float trk_bendchi2) {
+bool L1TrackJetProducer::trackQualityCuts(int trk_nstub, float trk_chi2, float trk_bendchi2) {
   bool PassQuality = false;
-  if (!displaced_){
-     if ( trk_nstub == 4 && trk_bendchi2 < nStubs4PromptBend_ &&
-          trk_chi2 < nStubs4PromptChi2_)
-        PassQuality = true;
-     if ( trk_nstub > 4 && trk_bendchi2 < nStubs5PromptBend_  && 
-          trk_chi2 < nStubs5PromptChi2_)
-        PassQuality = true;     
-  } else{
-     if (trk_nstub == 4 && trk_bendchi2 < nStubs4DisplacedBend_ && 
-         trk_chi2 < nStubs4DisplacedChi2_ )
-        PassQuality = true;
-     if (trk_nstub >4 && trk_bendchi2 < nStubs5DisplacedBend_ && 
-         trk_chi2 < nStubs5DisplacedChi2_ )
-        PassQuality = true;
+  if (!displaced_) {
+    if (trk_nstub == 4 && trk_bendchi2 < nStubs4PromptBend_ && trk_chi2 < nStubs4PromptChi2_) // 4 stubs are the lowest track quality and have different cuts
+      PassQuality = true;
+    if (trk_nstub > 4 && trk_bendchi2 < nStubs5PromptBend_ && trk_chi2 < nStubs5PromptChi2_) // above 4 stubs diffent selection imposed (genrally looser)
+      PassQuality = true;
+  } else {
+    if (trk_nstub == 4 && trk_bendchi2 < nStubs4DisplacedBend_ && trk_chi2 < nStubs4DisplacedChi2_) // 4 stubs are the lowest track quality and have different cuts
+      PassQuality = true;
+    if (trk_nstub > 4 && trk_bendchi2 < nStubs5DisplacedBend_ && trk_chi2 < nStubs5DisplacedChi2_) // above 4 stubs diffent selection imposed (genrally looser)
+      PassQuality = true;
   }
   return PassQuality;
 }
