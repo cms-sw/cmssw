b3c3d70e7083f10140ddd88af5635cb9c087b8a2
Laurent Forthomme <laurent.forthomme@cern.ch>
2022-05-04 11:06:14 +0200

    Sim: added an ESPrefer rule for LHCInfo and association cuts

--- SimPPS/DirectSimProducer/python/matching_cff.py
+++ SimPPS/DirectSimProducer/python/matching_cff.py
@@ -1,10 +1,14 @@
 import FWCore.ParameterSet.Config as cms
 
 def matchDirectSimOutputs(process, AOD=False, miniAOD=False):
+    # match sources of rechits with direct simulation outputs
     process.totemRPUVPatternFinder.tagRecHit = cms.InputTag('ppsDirectProtonSimulation')
     process.ctppsPixelLocalTracks.tag = cms.InputTag('ppsDirectProtonSimulation')
     process.ctppsDiamondLocalTracks.recHitsTag = cms.InputTag('ppsDirectProtonSimulation')
+    # handle clashes between simulation and GT conditions
     process.es_prefer_geometry = cms.ESPrefer('CTPPSCompositeESSource', 'ctppsCompositeESSource')
+    process.es_prefer_lhcinfo = cms.ESPrefer('CTPPSBeamParametersFromLHCInfoESSource', 'ctppsBeamParametersFromLHCInfoESSource')
+    process.es_prefer_assocuts = cms.ESPrefer('PPSAssociationCutsESSource', 'ppsAssociationCutsESSource')
     if AOD:
         process.beamDivergenceVtxGenerator.src = cms.InputTag('')
         process.beamDivergenceVtxGenerator.srcGenParticle = cms.VInputTag(
@@ -17,3 +21,4 @@ def matchDirectSimOutputs(process, AOD=False, miniAOD=False):
             cms.InputTag('genPUProtons', 'genPUProtons'),
             cms.InputTag('prunedGenParticles')
         )
+    return process
