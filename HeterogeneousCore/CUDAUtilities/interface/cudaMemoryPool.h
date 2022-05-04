26a67001f0ee4157081e10478b523d1474c5d409
Vincenzo Innocente <vincenzo.innocente@cern.ch>
2022-05-04 18:02:29 +0200

    clean up

--- HeterogeneousCore/CUDAUtilities/interface/cudaMemoryPool.h
+++ HeterogeneousCore/CUDAUtilities/interface/cudaMemoryPool.h
@@ -2,85 +2,69 @@
 #include "memoryPool.h"
 #include <vector>
 
-// only for cudaStream_t 
-#include<cuda.h>
-
+// only for cudaStream_t
+#include <cuda.h>
 
 namespace memoryPool {
   namespace cuda {
 
     void dumpStat();
 
-    SimplePoolAllocator * getPool(Where where);
+    SimplePoolAllocator *getPool(Where where);
 
     // allocate either on current device or on host
-    std::pair<void *,int> alloc(uint64_t size, SimplePoolAllocator & pool);
+    std::pair<void *, int> alloc(uint64_t size, SimplePoolAllocator &pool);
 
     // schedule free
-    void free(cudaStream_t stream, std::vector<int> buckets, SimplePoolAllocator & pool);
+    void free(cudaStream_t stream, std::vector<int> buckets, SimplePoolAllocator &pool);
 
+    struct CudaDeleterBase : public DeleterBase {
+      CudaDeleterBase(cudaStream_t const &stream, Where where) : DeleterBase(getPool(where)), m_stream(stream) {}
 
-    struct CudaDeleterBase  : public DeleterBase {
-
-     CudaDeleterBase(cudaStream_t const & stream, Where where) : DeleterBase(getPool(where)),
-           m_stream(stream) {}
-
-      CudaDeleterBase(cudaStream_t const & stream, SimplePoolAllocator * pool) : DeleterBase(pool),
-           m_stream(stream) {}
+      CudaDeleterBase(cudaStream_t const &stream, SimplePoolAllocator *pool) : DeleterBase(pool), m_stream(stream) {}
 
       ~CudaDeleterBase() override = default;
 
-      
       cudaStream_t m_stream;
-
     };
-    
+
     struct DeleteOne final : public CudaDeleterBase {
-
       using CudaDeleterBase::CudaDeleterBase;
-    
-      ~DeleteOne() override = default;
-      void operator()(int bucket) override {
-          free(m_stream, std::vector<int>(1,bucket), *pool());
-      }
 
+      ~DeleteOne() override = default;
+      void operator()(int bucket) override { free(m_stream, std::vector<int>(1, bucket), *pool()); }
     };
 
     struct BundleDelete final : public CudaDeleterBase {
-
       using CudaDeleterBase::CudaDeleterBase;
 
-      ~BundleDelete() override {
-         free(m_stream, std::move(m_buckets), *pool());
-      }
+      ~BundleDelete() override { free(m_stream, std::move(m_buckets), *pool()); }
 
-      void operator()(int bucket) override {
-         m_buckets.push_back(bucket);
-      }
+      void operator()(int bucket) override { m_buckets.push_back(bucket); }
 
       std::vector<int> m_buckets;
-
     };
 
-     template<typename T>
-      unique_ptr<T> make_unique(uint64_t size, Deleter del) {
-        auto ret = alloc(sizeof(T)*size,*del.pool());
-        if (ret.second<0) throw std::bad_alloc();
-        del.setBucket(ret.second);
-        return unique_ptr<T>((T*)(ret.first),del);
-      }
+    template <typename T>
+    unique_ptr<T> make_unique(uint64_t size, Deleter del) {
+      auto ret = alloc(sizeof(T) * size, *del.pool());
+      if (ret.second < 0)
+        throw std::bad_alloc();
+      del.setBucket(ret.second);
+      return unique_ptr<T>((T *)(ret.first), del);
+    }
 
-      template<typename T>
-      unique_ptr<T> make_unique(uint64_t size, cudaStream_t const & stream, Where where) {
-         return make_unique<T>(sizeof(T)*size,Deleter(std::make_shared<DeleteOne>(stream,getPool(where))));
-      }
+    template <typename T>
+    unique_ptr<T> make_unique(uint64_t size, cudaStream_t const &stream, Where where) {
+      return make_unique<T>(sizeof(T) * size, Deleter(std::make_shared<DeleteOne>(stream, getPool(where))));
+    }
 
-/*
+    /*
       template< class T, class... Args >
       memoryPool::unique_ptr<T> make_unique( Args&&... args );
       template< class T, class... Args >
       memoryPool::unique_ptr<T> make_unique(Deleter del, Args&&... args );
 */
 
-  } // cuda
-} // memoryPool
+  }  // namespace cuda
+}  // namespace memoryPool
--- HeterogeneousCore/CUDAUtilities/interface/memoryPool.h
+++ HeterogeneousCore/CUDAUtilities/interface/memoryPool.h
@@ -1,50 +1,47 @@
 #pragma once
-#include<memory>
-#include<new>
-
+#include <memory>
+#include <new>
 
 class SimplePoolAllocator;
 
 namespace memoryPool {
 
-  enum Where {onCPU,onDevice,onHost, unified};
+  enum Where { onCPU, onDevice, onHost, unified };
 
   class DeleterBase {
-    public: 
-
-    explicit DeleterBase(SimplePoolAllocator * pool) : m_pool(pool){}
+  public:
+    explicit DeleterBase(SimplePoolAllocator* pool) : m_pool(pool) {}
     virtual ~DeleterBase() = default;
-    virtual void operator()(int bucket) =0;
+    virtual void operator()(int bucket) = 0;
 
-    SimplePoolAllocator * pool() const { return m_pool;}
+    SimplePoolAllocator* pool() const { return m_pool; }
 
-    protected:
-     SimplePoolAllocator * m_pool;
+  protected:
+    SimplePoolAllocator* m_pool;
   };
 
   class Deleter {
   public:
-    explicit Deleter(int bucket=-1) : m_bucket(bucket) {}
-    Deleter(std::shared_ptr<DeleterBase> del, int bucket=-1) : me(del), m_bucket(bucket) {}
+    explicit Deleter(int bucket = -1) : m_bucket(bucket) {}
+    Deleter(std::shared_ptr<DeleterBase> del, int bucket = -1) : me(del), m_bucket(bucket) {}
 
-    void set(std::shared_ptr<DeleterBase> del) { me=del;}
-    void setBucket(int bucket) { m_bucket = bucket;}
+    void set(std::shared_ptr<DeleterBase> del) { me = del; }
+    void setBucket(int bucket) { m_bucket = bucket; }
 
-    void operator()(void * p) {
-      if (!me) throw std::bad_alloc(); 
+    void operator()(void* p) {
+      if (!me)
+        throw std::bad_alloc();
       (*me)(m_bucket);
     }
 
-    SimplePoolAllocator * pool() const { return me->pool();}
+    SimplePoolAllocator* pool() const { return me->pool(); }
 
   private:
     std::shared_ptr<DeleterBase> me;
     int m_bucket;
   };
 
-
   template <typename T>
-  using unique_ptr = std::unique_ptr<T,Deleter>;
+  using unique_ptr = std::unique_ptr<T, Deleter>;
 
-
-}
+}  // namespace memoryPool
