7776bc38b4ea80747fb0561d14725196d660fbbe
mmusich <marco.musich@cern.ch>
2022-05-04 11:55:03 +0200

    follow-up to code review

--- Calibration/TkAlCaRecoProducers/python/AlcaSiStripHitEfficiencyHarvester_cfi.py
+++ Calibration/TkAlCaRecoProducers/python/AlcaSiStripHitEfficiencyHarvester_cfi.py
@@ -3,13 +3,12 @@ import FWCore.ParameterSet.Config as cms
 from DQM.SiStripCommon.TkHistoMap_cff import *
 from CalibTracker.SiStripHitEfficiency.siStripHitEfficiencyHarvester_cfi import siStripHitEfficiencyHarvester
 alcasiStripHitEfficiencyHarvester =  siStripHitEfficiencyHarvester.clone(
-    isAtPCL             = cms.bool(True),
-    Threshold           = cms.double(0.1),
-    nModsMin            = cms.int32(5),
-    AutoIneffModTagging = cms.untracked.bool(True),  # default true, automatic limit for each layer to identify inefficient modules
-    Record              = cms.string('SiStripBadStripFromHitEffRcd'),
-    doStoreOnDB         = cms.bool(True),
-    ShowRings           = cms.untracked.bool(False),  # default False
-    TkMapMin            = cms.untracked.double(0.90), # default 0.90
-    #EffPlotMin          = cms.untracked.double(0.90), # default 0.90
-    Title               = cms.string('SiStrip Hit Efficiency Map'))
+    isAtPCL             = True,
+    Threshold           = 0.1,
+    nModsMin            = 5,
+    AutoIneffModTagging = True,  # default true, automatic limit for each layer to identify inefficient modules
+    Record              = 'SiStripBadStripFromHitEffRcd',
+    doStoreOnDB         = True,
+    ShowRings           = False,  # default False
+    TkMapMin            = 0.90, # default 0.90
+    Title               = 'SiStrip Hit Efficiency Map')
