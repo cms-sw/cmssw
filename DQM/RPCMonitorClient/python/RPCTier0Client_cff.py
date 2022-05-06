c14e4e7a64d57a55c2eeec6d96f7eff61c176b19
Jiwon <minerva1993@gmail.com>
2022-05-06 19:34:28 +0200

    RPC DQM updates

--- DQM/RPCMonitorClient/python/RPCTier0Client_cff.py
+++ DQM/RPCMonitorClient/python/RPCTier0Client_cff.py
@@ -10,8 +10,6 @@ from DQM.RPCMonitorClient.RPCDqmClient_cfi import *
 
 from DQM.RPCMonitorClient.RPCRecHitProbabilityClient_cfi import *
 
-from  DQM.RPCMonitorClient.RPCChamberQuality_cfi import *
-
 from DQMOffline.MuonDPG.rpcTnPEfficiencyClient_cfi import *
 
 
@@ -26,7 +24,7 @@ qTesterRPC = DQMQualityTester(
 from DQM.RPCMonitorClient.RPCDcsInfoClient_cfi import *
 
 if (muonFlag):
-    rpcTier0Client = cms.Sequence(qTesterRPC*rpcdqmclient*rpcdqmMuonclient*rpcrechitprobabilityclient*rpcChamberQuality*rpcMuonChamberQuality*rpcDcsInfoClient*rpcTnPEfficiencyClient*rpcEventSummary)
+    rpcTier0Client = cms.Sequence(qTesterRPC*rpcdqmclient*rpcdqmMuonclient*rpcrechitprobabilityclient*rpcDcsInfoClient*rpcTnPEfficiencyClient*rpcEventSummary)
 else:
-    rpcTier0Client = cms.Sequence(qTesterRPC*rpcdqmclient*rpcChamberQuality*rpcDcsInfoClient*rpcEventSummary)
+    rpcTier0Client = cms.Sequence(qTesterRPC*rpcdqmclient*rpcDcsInfoClient*rpcEventSummary)
 
