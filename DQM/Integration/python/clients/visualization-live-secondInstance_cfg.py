96117a0a2c8f769157794398622b5481f8bbe451
pmandrik <petr.mandrik@cern.ch>
2022-05-09 11:08:04 +0200

    move reco options into dqm clients

--- DQM/Integration/python/clients/visualization-live-secondInstance_cfg.py
+++ DQM/Integration/python/clients/visualization-live-secondInstance_cfg.py
@@ -48,7 +48,7 @@ from DQM.Integration.config.FrontierCondition_GT_autoExpress_cfi import GlobalTa
 kwds = {
    'globalTag': GlobalTag.globaltag.value(),
    'globalTagConnect': GlobalTag.connect.value(),
-   'beamSplashRun' : options.BeamSplashRun,
+   'beamSplashRun' : ":localreco+hcalOnlyGlobalRecoSequence+caloTowersRec" if options.BeamSplashRun else "",
 }
 
 # explicitly select the input collection, since we get multiple in online
