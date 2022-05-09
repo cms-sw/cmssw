4c0381badd190a8b8671428b20918bcef4d0a20c
leonardo <leonardo.giannini@cern.ch>
2022-05-09 14:10:24 -0700

    DNN selection for tk candidates + application to pixelLess and highPtTriplet

--- RecoTracker/IterativeTracking/python/HighPtTripletStep_cff.py
+++ RecoTracker/IterativeTracking/python/HighPtTripletStep_cff.py
@@ -241,6 +241,8 @@ trackingMkFitHighPtTripletStep.toReplaceWith(highPtTripletStepTrackCandidates, m
     seeds = 'highPtTripletStepSeeds',
     mkFitSeeds = 'highPtTripletStepTrackCandidatesMkFitSeeds',
     tracks = 'highPtTripletStepTrackCandidatesMkFit',
+    candMVASel = True,
+    candWP = -0.3,
 ))
 (pp_on_XeXe_2017 | pp_on_AA).toModify(highPtTripletStepTrackCandidatesMkFitConfig, minPt=0.7)
 
