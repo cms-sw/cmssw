535df2346c85e0527941483252b1cc76f4ad491a
AdrianoDee <adriano.diflorio@ba.infn.it>
2022-05-12 19:37:01 +0200

    Backport of PR 37617

--- RecoLocalTracker/SiPixelRecHits/python/SiPixelRecHits_cfi.py
+++ RecoLocalTracker/SiPixelRecHits/python/SiPixelRecHits_cfi.py
@@ -23,15 +23,18 @@ siPixelRecHitsPreSplitting = SwitchProducerCUDA(
 
 # convert the pixel rechits from legacy to SoA format
 from RecoLocalTracker.SiPixelRecHits.siPixelRecHitSoAFromLegacy_cfi import siPixelRecHitSoAFromLegacy as _siPixelRecHitsPreSplittingSoA
-siPixelRecHitsPreSplittingSoA = _siPixelRecHitsPreSplittingSoA.clone(convertToLegacy=True)
+from RecoLocalTracker.SiPixelRecHits.siPixelRecHitSoAFromCUDA_cfi import siPixelRecHitSoAFromCUDA as _siPixelRecHitSoAFromCUDA
+
+siPixelRecHitsPreSplittingCPU = _siPixelRecHitsPreSplittingSoA.clone(convertToLegacy=True)
+
 # phase 2 tracker modifier
 from Configuration.Eras.Modifier_phase2_tracker_cff import phase2_tracker
-phase2_tracker.toModify(siPixelRecHitsPreSplittingSoA,
+phase2_tracker.toModify(siPixelRecHitsPreSplittingCPU,
     isPhase2 = True)
 # modifier used to prompt patatrack pixel tracks reconstruction on cpu
 from Configuration.ProcessModifiers.pixelNtupletFit_cff import pixelNtupletFit
 pixelNtupletFit.toModify(siPixelRecHitsPreSplitting,
-    cpu = siPixelRecHitsPreSplittingSoA.clone()
+    cpu = _siPixelRecHitsPreSplittingSoA.clone(convertToLegacy=True)
 )
 
 siPixelRecHitsPreSplittingTask = cms.Task(
@@ -48,9 +51,19 @@ siPixelRecHitsPreSplittingCUDA = _siPixelRecHitCUDA.clone(
 # transfer the pixel rechits to the host and convert them from SoA
 from RecoLocalTracker.SiPixelRecHits.siPixelRecHitFromCUDA_cfi import siPixelRecHitFromCUDA as _siPixelRecHitFromCUDA
 
+#this is an alias for the SoA on GPU or CPU to be used for DQM
+siPixelRecHitsPreSplittingSoA = SwitchProducerCUDA(
+    cpu = cms.EDAlias(
+            siPixelRecHitsPreSplittingCPU = cms.VPSet(
+                 cms.PSet(type = cms.string("cmscudacompatCPUTraitsTrackingRecHit2DHeterogeneous")),
+                 cms.PSet(type = cms.string("uintAsHostProduct"))
+             )),
+    cuda = _siPixelRecHitSoAFromCUDA.clone()
+)
+
 (gpu & pixelNtupletFit).toModify(siPixelRecHitsPreSplitting,
     cpu = cms.EDAlias(
-            siPixelRecHitsPreSplittingSoA = cms.VPSet(
+            siPixelRecHitsPreSplittingCPU = cms.VPSet(
                  cms.PSet(type = cms.string("SiPixelRecHitedmNewDetSetVector")),
                  cms.PSet(type = cms.string("uintAsHostProduct"))
              )
@@ -58,10 +71,12 @@ from RecoLocalTracker.SiPixelRecHits.siPixelRecHitFromCUDA_cfi import siPixelRec
     cuda = _siPixelRecHitFromCUDA.clone())
 
 (gpu & pixelNtupletFit).toReplaceWith(siPixelRecHitsPreSplittingTask, cms.Task(
-    # reconstruct the pixel rechits on the gpu
+    # reconstruct the pixel rechits on the gpu or on the cpu
+    # (normally only one of the two is run because only one is consumed from later stages)
     siPixelRecHitsPreSplittingCUDA,
-    # producing and converting on cpu
-    siPixelRecHitsPreSplittingSoA,
+    siPixelRecHitsPreSplittingCPU,
     # SwitchProducer wrapping an EDAlias on cpu or the converter from SoA to legacy on gpu
-    siPixelRecHitsPreSplittingTask.copy()
+    siPixelRecHitsPreSplittingTask.copy(),
+    # producing and converting on cpu (if needed)
+    siPixelRecHitsPreSplittingSoA
 ))
