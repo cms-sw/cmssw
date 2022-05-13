77b7b51eb61fa9c58d3688f44d22ab4f83108f84
CeliaFernandez <celia.fernandez.madrazo@cern.ch>
2022-05-13 19:53:52 +0200

    Apply comments and clean the code

--- PhysicsTools/PatAlgos/python/producersLayer1/displacedMuonProducer_cff.py
+++ PhysicsTools/PatAlgos/python/producersLayer1/displacedMuonProducer_cff.py
@@ -6,83 +6,82 @@ from PhysicsTools.PatAlgos.producersLayer1.muonProducer_cfi import *
 from PhysicsTools.PatAlgos.recoLayer0.filteredDisplacedMuons_cfi import *
 filteredDisplacedMuonsTask = cms.Task(filteredDisplacedMuons)
 
-sourceMuons = cms.InputTag("filteredDisplacedMuons")
 
 patDisplacedMuons = patMuons.clone(
 
     # Input collection
-    muonSource = sourceMuons,
+    muonSource = "filteredDisplacedMuons",
 
     # embedding objects
-    embedMuonBestTrack      = cms.bool(True),  ## embed in AOD externally stored muon best track from global pflow
-    embedTunePMuonBestTrack = cms.bool(True),  ## embed in AOD externally stored muon best track from muon only
-    forceBestTrackEmbedding = cms.bool(False), ## force embedding separately the best tracks even if they're already embedded e.g. as tracker or global tracks
-    embedTrack          = cms.bool(True), ## embed in AOD externally stored tracker track
-    embedCombinedMuon   = cms.bool(True),  ## embed in AOD externally stored combined muon track
-    embedStandAloneMuon = cms.bool(True),  ## embed in AOD externally stored standalone muon track
-    embedPickyMuon      = cms.bool(False),  ## embed in AOD externally stored TeV-refit picky muon track
-    embedTpfmsMuon      = cms.bool(False),  ## embed in AOD externally stored TeV-refit TPFMS muon track
-    embedDytMuon        = cms.bool(False),  ## embed in AOD externally stored TeV-refit DYT muon track
-    embedPFCandidate    = cms.bool(False),  ## embed in AOD externally stored particle flow candidate
+    embedMuonBestTrack      = True,  ## embed in AOD externally stored muon best track from global pflow
+    embedTunePMuonBestTrack = True,  ## embed in AOD externally stored muon best track from muon only
+    forceBestTrackEmbedding = False, ## force embedding separately the best tracks even if they're already embedded e.g. as tracker or global tracks
+    embedTrack          = True, ## embed in AOD externally stored tracker track
+    embedCombinedMuon   = True,  ## embed in AOD externally stored combined muon track
+    embedStandAloneMuon = True,  ## embed in AOD externally stored standalone muon track
+    embedPickyMuon      = False,  ## embed in AOD externally stored TeV-refit picky muon track
+    embedTpfmsMuon      = False,  ## embed in AOD externally stored TeV-refit TPFMS muon track
+    embedDytMuon        = False,  ## embed in AOD externally stored TeV-refit DYT muon track
+    embedPFCandidate    = False,  ## embed in AOD externally stored particle flow candidate
 
     # embedding of muon MET corrections for caloMET
-    embedCaloMETMuonCorrs = cms.bool(False),
+    embedCaloMETMuonCorrs = False,
     # embedding of muon MET corrections for tcMET
-    embedTcMETMuonCorrs   = cms.bool(False), # removed from RECO/AOD!
+    embedTcMETMuonCorrs   = False, # removed from RECO/AOD!
 
     # Read and store combined inverse beta
-    addInverseBeta    = cms.bool(True),
-    sourceMuonTimeExtra = cms.InputTag("filteredDisplacedMuons","combined"), 
+    addInverseBeta    = True,
+    sourceMuonTimeExtra = ["filteredDisplacedMuons","combined"], 
 
     # mc matching (deactivated)
-    addGenMatch   = cms.bool(False),
-    embedGenMatch = cms.bool(False),
+    addGenMatch   = False,
+    embedGenMatch = False,
     genParticleMatch = "displacedMuonMatch", # deactivated
 
     # high level selections
-    embedHighLevelSelection = cms.bool(True),
-    beamLineSrc             = cms.InputTag("offlineBeamSpot"),
-    pvSrc                   = cms.InputTag("offlinePrimaryVertices"),
+    embedHighLevelSelection = True,
+    beamLineSrc             = "offlineBeamSpot",
+    pvSrc                   = "offlinePrimaryVertices",
 
     # ecal PF energy
-    embedPfEcalEnergy = cms.bool(False),
-    addPuppiIsolation = cms.bool(False),
+    embedPfEcalEnergy = False,
+    addPuppiIsolation = False,
 
     # Compute and store Mini-Isolation.
     # Implemention and a description of parameters can be found in:
     # PhysicsTools/PatUtils/src/PFIsolation.cc
     # only works in miniaod, so set to True in miniAOD_tools.py
-    computeMiniIso = cms.bool(False),
-    effectiveAreaVec = cms.vdouble(0.0566, 0.0562, 0.0363, 0.0119, 0.0064),
-    pfCandsForMiniIso = cms.InputTag("packedPFCandidates"),
-    miniIsoParams = cms.vdouble(0.05, 0.2, 10.0, 0.5, 0.0001, 0.01, 0.01, 0.01, 0.0),
+    computeMiniIso = False,
+    effectiveAreaVec = [0.0566, 0.0562, 0.0363, 0.0119, 0.0064],
+    pfCandsForMiniIso = "packedPFCandidates",
+    miniIsoParams = [0.05, 0.2, 10.0, 0.5, 0.0001, 0.01, 0.01, 0.01, 0.0],
 
-    computePuppiCombinedIso = cms.bool(False),
+    computePuppiCombinedIso = False,
     # Standard Muon Selectors and Jet-related observables
     # Depends on MiniIsolation, so only works in miniaod
     # Don't forget to set flags properly in miniAOD_tools.py                      
-    computeMuonMVA = cms.bool(False),
-    mvaTrainingFile      = cms.FileInPath("RecoMuon/MuonIdentification/data/mu_2017_BDTG.weights.xml"),
-    lowPtmvaTrainingFile = cms.FileInPath("RecoMuon/MuonIdentification/data/mu_lowpt_BDTG.weights.xml"),
-    recomputeBasicSelectors = cms.bool(False),
-    mvaUseJec = cms.bool(False),
-    mvaDrMax = cms.double(0.4),
-    mvaJetTag = cms.InputTag("pfCombinedInclusiveSecondaryVertexV2BJetTags"),
-    mvaL1Corrector = cms.InputTag("ak4PFCHSL1FastjetCorrector"),
-    mvaL1L2L3ResCorrector = cms.InputTag("ak4PFCHSL1FastL2L3Corrector"),
-    rho = cms.InputTag("fixedGridRhoFastjetCentralNeutral"),
+    computeMuonMVA = False,
+    mvaTrainingFile      = "RecoMuon/MuonIdentification/data/mu_2017_BDTG.weights.xml",
+    lowPtmvaTrainingFile = "RecoMuon/MuonIdentification/data/mu_lowpt_BDTG.weights.xml",
+    recomputeBasicSelectors = False,
+    mvaUseJec = False,
+    mvaDrMax = 0.4,
+    mvaJetTag = "pfCombinedInclusiveSecondaryVertexV2BJetTags",
+    mvaL1Corrector = "ak4PFCHSL1FastjetCorrector",
+    mvaL1L2L3ResCorrector = "ak4PFCHSL1FastL2L3Corrector",
+    rho = "fixedGridRhoFastjetCentralNeutral",
 
-    computeSoftMuonMVA = cms.bool(False),
-    softMvaTrainingFile = cms.FileInPath("RecoMuon/MuonIdentification/data/TMVA-muonid-bmm4-B-25.weights.xml"),
+    computeSoftMuonMVA = False,
+    softMvaTrainingFile = "RecoMuon/MuonIdentification/data/TMVA-muonid-bmm4-B-25.weights.xml",
 
     # MC Info
-    muonSimInfo = cms.InputTag("displacedMuonSimClassifier"), # This module does not exists but producer checks existence by itself
+    muonSimInfo = "displacedMuonSimClassifier", # This module does not exists but producer checks existence by itself
 
     # Trigger Info 
-    addTriggerMatching = cms.bool(False),
-    triggerObjects = cms.InputTag("slimmedPatTrigger"),
-    triggerResults = cms.InputTag("TriggerResults","","HLT"),
-    hltCollectionFilters = cms.vstring('*')
+    addTriggerMatching = False,
+    triggerObjects = "slimmedPatTrigger",
+    triggerResults = ["TriggerResults","","HLT"],
+    hltCollectionFilters = ['*']
 
 )
 
