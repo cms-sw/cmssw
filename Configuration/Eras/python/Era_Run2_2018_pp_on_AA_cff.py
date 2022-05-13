6ff6b585aaaa593f0de614477f991f847836abed
milanche85@gmail.com <milanche85@gmail.com>
2022-05-12 22:36:21 -0400

    removing mkFit from exlude in Eras for PbPb collisions

--- Configuration/Eras/python/Era_Run2_2018_pp_on_AA_cff.py
+++ Configuration/Eras/python/Era_Run2_2018_pp_on_AA_cff.py
@@ -6,5 +6,5 @@ from Configuration.Eras.Modifier_pp_on_AA_2018_cff import pp_on_AA_2018
 from Configuration.Eras.ModifierChain_trackingMkFitProd_cff import trackingMkFitProd
 from Configuration.ProcessModifiers.trackingNoLoopers_cff import trackingNoLoopers
 
-Run2_2018_pp_on_AA = cms.ModifierChain(Run2_2018.copyAndExclude([trackingMkFitProd, trackdnn, trackingNoLoopers]), pp_on_AA, pp_on_AA_2018)
+Run2_2018_pp_on_AA = cms.ModifierChain(Run2_2018.copyAndExclude([trackdnn, trackingNoLoopers]), pp_on_AA, pp_on_AA_2018)
 
--- Configuration/Eras/python/Era_Run3_pp_on_PbPb_cff.py
+++ Configuration/Eras/python/Era_Run3_pp_on_PbPb_cff.py
@@ -1,10 +1,10 @@
 import FWCore.ParameterSet.Config as cms
 from Configuration.ProcessModifiers.trackdnn_CKF_cff import trackdnn_CKF
 from Configuration.ProcessModifiers.trackdnn_cff import trackdnn
-from Configuration.Eras.Era_Run3_noMkFit_cff import Run3_noMkFit
+from Configuration.Eras.Era_Run3_cff import Run3
 from Configuration.ProcessModifiers.pp_on_AA_cff import pp_on_AA
 from Configuration.Eras.Modifier_pp_on_PbPb_run3_cff import pp_on_PbPb_run3
 from Configuration.ProcessModifiers.trackingNoLoopers_cff import trackingNoLoopers
 
-Run3_pp_on_PbPb = cms.ModifierChain(Run3_noMkFit.copyAndExclude([trackdnn, trackdnn_CKF, trackingNoLoopers]), pp_on_AA, pp_on_PbPb_run3)
+Run3_pp_on_PbPb = cms.ModifierChain(Run3.copyAndExclude([trackdnn, trackdnn_CKF, trackingNoLoopers]), pp_on_AA, pp_on_PbPb_run3)
 
