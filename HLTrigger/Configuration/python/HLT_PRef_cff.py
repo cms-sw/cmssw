bf9021e98d56c94d7e4fd1d66b97d6958d948aae
Marino Missiroli <m.missiroli@cern.ch>
2022-05-16 18:07:45 +0200

    HLT menu development for 12_3_X (branch: 12_5_X)

--- HLTrigger/Configuration/python/HLT_PRef_cff.py
+++ HLTrigger/Configuration/python/HLT_PRef_cff.py
@@ -1,6 +1,6 @@
 # hltGetConfiguration --cff --data /dev/CMSSW_12_3_0/PRef --type PRef
 
-# /dev/CMSSW_12_3_0/PRef/V79 (CMSSW_12_3_0)
+# /dev/CMSSW_12_3_0/PRef/V84 (CMSSW_12_3_0)
 
 import FWCore.ParameterSet.Config as cms
 
@@ -12,7 +12,7 @@ fragment = cms.ProcessFragment( "HLT" )
 fragment.ProcessAcceleratorCUDA = ProcessAcceleratorCUDA()
 
 fragment.HLTConfigVersion = cms.PSet(
-  tableName = cms.string('/dev/CMSSW_12_3_0/PRef/V79')
+  tableName = cms.string('/dev/CMSSW_12_3_0/PRef/V84')
 )
 
 fragment.transferSystem = cms.PSet( 
@@ -4652,6 +4652,11 @@ fragment.hltESPTrajectorySmootherRK = cms.ESProducer( "KFTrajectorySmootherESPro
   RecoGeometry = cms.string( "hltESPDummyDetLayerGeometry" ),
   errorRescaling = cms.double( 100.0 ),
   minHits = cms.int32( 3 ),
+  appendToDataLabel = cms.string( "" )
+)
+fragment.hltOnlineBeamSpotESProducer = cms.ESProducer( "OnlineBeamSpotESProducer",
+  timeThreshold = cms.int32( 48 ),
+  sigmaZThreshold = cms.double( 2.0 ),
   appendToDataLabel = cms.string( "" )
 )
 fragment.hltPixelTracksCleanerBySharedHits = cms.ESProducer( "PixelTrackCleanerBySharedHitsESProducer",
