1510384e2d8f32fa805659411cc01f3d335954f5
Thomas Reis <thomas.reis@cern.ch>
2022-05-18 14:30:00 +0200

    Remove obsolete ECAL test beam code.

--- SimCalorimetry/EcalTestBeam/src/SealModule.cc
+++
@@ -1,5 +0,0 @@
-#include "FWCore/Framework/interface/MakerMacros.h"
-#include "SimCalorimetry/EcalTestBeam/interface/EcalTBDigiProducer.h"
-#include "SimGeneral/MixingModule/interface/DigiAccumulatorMixModFactory.h"
-
-DEFINE_DIGI_ACCUMULATOR(EcalTBDigiProducer);
