581fd27703af68aff3be5fe0fde104659a957b93
Martin Grunewald <Martin.Grunewald@cern.ch>
2022-05-19 15:34:35 +0200

    HLT menu development plus V1.1 frozen menu

--- HLTrigger/Configuration/python/HLT_Fake1_cff.py
+++ HLTrigger/Configuration/python/HLT_Fake1_cff.py
@@ -1,13 +1,14 @@
-# hltGetConfiguration --cff --data /dev/CMSSW_12_3_0/Fake1 --type Fake1
+# hltGetConfiguration --cff --data /dev/CMSSW_12_4_0/Fake1 --type Fake1
 
-# /dev/CMSSW_12_3_0/Fake1/V8 (CMSSW_12_3_0)
+# /dev/CMSSW_12_4_0/Fake1/V7 (CMSSW_12_4_0_pre4)
 
 import FWCore.ParameterSet.Config as cms
 
 fragment = cms.ProcessFragment( "HLT" )
 
+
 fragment.HLTConfigVersion = cms.PSet(
-  tableName = cms.string('/dev/CMSSW_12_3_0/Fake1/V8')
+  tableName = cms.string('/dev/CMSSW_12_4_0/Fake1/V7')
 )
 
 fragment.streams = cms.PSet(  A = cms.vstring( 'InitialPD' ) )
--- HLTrigger/Configuration/python/HLTrigger_Datasets_PIon_cff.py
+++ HLTrigger/Configuration/python/HLTrigger_Datasets_PIon_cff.py
@@ -1,4 +1,4 @@
-# /dev/CMSSW_12_3_0/PIon
+# /dev/CMSSW_12_4_0/PIon
 
 import FWCore.ParameterSet.Config as cms
 
