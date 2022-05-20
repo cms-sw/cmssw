f868397c300ab1f370b80b965356f1da7ae62e60
swagata87 <physics.swagata@gmail.com>
2022-05-20 14:24:02 +0200

    remove RecoEgamma/Examples

--- RecoEgamma/Examples/plugins/plugins.cc
+++
@@ -1,7 +0,0 @@
-#include "CommonTools/UtilAlgos/interface/Merger.h"
-#include "DataFormats/EgammaReco/interface/SuperCluster.h"
-#include "DataFormats/EgammaReco/interface/SuperClusterFwd.h"
-#include "FWCore/Framework/interface/MakerMacros.h"
-
-typedef Merger<reco::SuperClusterCollection> EgammaSuperClusterMerger;
-DEFINE_FWK_MODULE(EgammaSuperClusterMerger);
