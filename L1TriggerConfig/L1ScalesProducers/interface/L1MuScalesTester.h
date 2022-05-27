1240aa5eada5a19209622e4800ea8f08e9622e77
Christopher Jones <chrisdjones15@gmail.com>
2022-05-27 12:08:46 -0500

    Fix CMS deprecation warnings in L1TriggerConfig/L1ScalesProducers
    - changed to thread-friendly module types
    - use esConsumes

--- L1TriggerConfig/L1ScalesProducers/interface/L1MuScalesTester.h
+++ L1TriggerConfig/L1ScalesProducers/interface/L1MuScalesTester.h
@@ -5,19 +5,25 @@
 //
 
 // user include files
-#include "FWCore/Framework/interface/EDAnalyzer.h"
+#include "FWCore/Framework/interface/one/EDAnalyzer.h"
 #include "FWCore/Framework/interface/Event.h"
 #include "FWCore/ParameterSet/interface/ParameterSet.h"
+
+#include "CondFormats/L1TObjects/interface/L1MuTriggerScales.h"
+#include "CondFormats/DataRecord/interface/L1MuTriggerScalesRcd.h"
+#include "CondFormats/L1TObjects/interface/L1MuTriggerPtScale.h"
+#include "CondFormats/DataRecord/interface/L1MuTriggerPtScaleRcd.h"
+#include "CondFormats/L1TObjects/interface/L1MuGMTScales.h"
+#include "CondFormats/DataRecord/interface/L1MuGMTScalesRcd.h"
 
 class L1MuScale;
 //
 // class decleration
 //
 
-class L1MuScalesTester : public edm::EDAnalyzer {
+class L1MuScalesTester : public edm::one::EDAnalyzer<> {
 public:
   explicit L1MuScalesTester(const edm::ParameterSet&);
-  ~L1MuScalesTester() override;
 
   void analyze(const edm::Event&, const edm::EventSetup&) override;
 
@@ -25,4 +31,7 @@ public:
 
 private:
   // ----------member data ---------------------------
+  edm::ESGetToken<L1MuTriggerScales, L1MuTriggerScalesRcd> l1muscalesToken_;
+  edm::ESGetToken<L1MuTriggerPtScale, L1MuTriggerPtScaleRcd> l1muptscaleToken_;
+  edm::ESGetToken<L1MuGMTScales, L1MuGMTScalesRcd> l1gmtscalesToken_;
 };
