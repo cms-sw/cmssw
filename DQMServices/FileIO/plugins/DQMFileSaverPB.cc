2a6e560231d78beaa5f9680363c3694accfd1257
Sam Harper <sam.j.harper@gmail.com>
2022-05-28 20:30:49 +0200

    swapping to more modern conventions

--- DQMServices/FileIO/plugins/DQMFileSaverPB.cc
+++ DQMServices/FileIO/plugins/DQMFileSaverPB.cc
@@ -260,8 +260,8 @@ void DQMFileSaverPB::savePB(DQMStore* store, std::string const& filename, int ru
       // Compress ME blob with zlib
       int maxOutputSize = this->getMaxCompressedSize(buffer.Length());
       std::vector<char> compression_output(maxOutputSize);
-      uLong total_out = this->compressME(buffer, maxOutputSize, &compression_output[0]);
-      histo.set_streamed_histo(&compression_output[0], total_out);
+      uLong total_out = this->compressME(buffer, maxOutputSize, compression_output.data());
+      histo.set_streamed_histo(compression_output.data(), total_out);
     }
 
     // Save quality reports
