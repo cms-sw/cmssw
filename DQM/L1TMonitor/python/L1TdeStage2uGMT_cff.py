e3f3bd231e8db4a1976dd1074420f8db8b3f91c4
Dinyar Rabady <dinyar@cern.ch>
2022-05-28 17:45:52 +0200

    uGMT DQM changes following removal of ZS
    The following changes were done to cope with the uGMT readout menu
    following the removal of the zero suppression of the uGMT:
    - Removed zero suppression folder from uGMT DQM
    - Ignore BX range mismatches in uGMT muon copy comparisons as we read
    out only the central BX for the copies but +/-2 for the main outputs.
    - Enable uGMT muon copy comparisons for all events (not just the
    validation events).
    - Ignore BX range mismathes for the data-emulator comparisons for the
    same reason.
    - Move the main uGMT DQM plots behind the fat events filter as otherwise
    the BX distribution plots would be heavily biased towards the central
    BX. (Because only the central BX is read out in standard events for the
    inputs.)

--- DQM/L1TMonitor/python/L1TdeStage2uGMT_cff.py
+++ DQM/L1TMonitor/python/L1TdeStage2uGMT_cff.py
@@ -11,6 +11,7 @@ ugmtEmuImdMuDqmDir = ugmtEmuDqmDir+"/intermediate_muons"
 
 # List of bins to ignore
 ignoreBins = [7, 8, 12, 13]
+ignoreBinsRun3 = [1, 7, 8, 12, 13]
 
 # fills histograms with all uGMT emulated muons
 # uGMT input muon histograms from track finders are not filled since they are identical to the data DQM plots
@@ -87,9 +88,10 @@ l1tdeStage2uGMT = DQMEDAnalyzer(
     ignoreBin = cms.untracked.vint32(),
 )
 
-## Era: Run3_2021; Displaced muons from BMTF used in uGMT from Run-3
+## Era: Run3_2021; Displaced muons used in uGMT from Run-3
+ # Additionally: Ignore BX range mismatches. This is necessary because we only read out the central BX for the inputs, so that is what the emulator has to work on.
 from Configuration.Eras.Modifier_stage2L1Trigger_2021_cff import stage2L1Trigger_2021
-stage2L1Trigger_2021.toModify(l1tdeStage2uGMT, displacedQuantities = cms.untracked.bool(True))
+stage2L1Trigger_2021.toModify(l1tdeStage2uGMT, displacedQuantities = cms.untracked.bool(True), ignoreBin = cms.untracked.vint32(1))
 
 # compares the unpacked uGMT muon shower collection to the emulated uGMT muon shower collection
 # only showers that do not match are filled in the histograms
@@ -102,7 +104,7 @@ l1tdeStage2uGMTShowers = DQMEDAnalyzer(
     muonShowerCollection2Title = cms.untracked.string("uGMT emulator"),
     summaryTitle = cms.untracked.string("Summary of comparison between uGMT showers and uGMT emulator showers"),
     verbose = cms.untracked.bool(False),
-    ignoreBin = cms.untracked.vint32(),
+    ignoreBin = cms.untracked.vint32(1), # Ignore BX range mismatches. This is necessary because we only read out the central BX for the inputs, so that is what the emulator has to work on.
 )
 
 # compares the unpacked uGMT intermediate muon collection to the emulated uGMT intermediate muon collection
@@ -114,13 +116,16 @@ l1tdeStage2uGMTIntermediateBMTF = l1tdeStage2uGMT.clone(
     summaryTitle = "Summary of uGMT intermediate muon from BMTF comparison between unpacked and emulated",
     ignoreBin = ignoreBins
 )
+## Era: Run3_2021; Ignore BX range mismatches. This is necessary because we only read out the central BX for the inputs, so that is what the emulator has to work on.
+from Configuration.Eras.Modifier_stage2L1Trigger_2021_cff import stage2L1Trigger_2021
+stage2L1Trigger_2021.toModify(l1tdeStage2uGMTIntermediateBMTF, ignoreBin = ignoreBinsRun3)
+
 l1tdeStage2uGMTIntermediateOMTFNeg = l1tdeStage2uGMTIntermediateBMTF.clone(
     displacedQuantities = False,
     muonCollection1 = (unpackerModule, "imdMuonsOMTFNeg"),
     muonCollection2 = (emulatorModule, "imdMuonsOMTFNeg"),
     monitorDir = (ugmtEmuImdMuDqmDir+"/OMTF_neg/data_vs_emulator_comparison"),
     summaryTitle = ("Summary of uGMT intermediate muon from OMTF- comparison between unpacked and emulated"),
-    ignoreBin = ignoreBins
 )
 l1tdeStage2uGMTIntermediateOMTFPos = l1tdeStage2uGMTIntermediateBMTF.clone(
     displacedQuantities = False,
@@ -128,7 +133,6 @@ l1tdeStage2uGMTIntermediateOMTFPos = l1tdeStage2uGMTIntermediateBMTF.clone(
     muonCollection2 = (emulatorModule, "imdMuonsOMTFPos"),
     monitorDir = (ugmtEmuImdMuDqmDir+"/OMTF_pos/data_vs_emulator_comparison"),
     summaryTitle = "Summary of uGMT intermediate muon from OMTF+ comparison between unpacked and emulated",
-    ignoreBin = ignoreBins
 )
 l1tdeStage2uGMTIntermediateEMTFNeg = l1tdeStage2uGMTIntermediateBMTF.clone(
     displacedQuantities = False,
@@ -136,7 +140,6 @@ l1tdeStage2uGMTIntermediateEMTFNeg = l1tdeStage2uGMTIntermediateBMTF.clone(
     muonCollection2 = (emulatorModule, "imdMuonsEMTFNeg"),
     monitorDir = (ugmtEmuImdMuDqmDir+"/EMTF_neg/data_vs_emulator_comparison"),
     summaryTitle = "Summary of uGMT intermediate muon from EMTF- comparison between unpacked and emulated",
-    ignoreBin = ignoreBins
 )
 l1tdeStage2uGMTIntermediateEMTFPos = l1tdeStage2uGMTIntermediateBMTF.clone(
     displacedQuantities = False,
@@ -144,7 +147,6 @@ l1tdeStage2uGMTIntermediateEMTFPos = l1tdeStage2uGMTIntermediateBMTF.clone(
     muonCollection2 = (emulatorModule, "imdMuonsEMTFPos"),
     monitorDir = (ugmtEmuImdMuDqmDir+"/EMTF_pos/data_vs_emulator_comparison"),
     summaryTitle = "Summary of uGMT intermediate muon from EMTF+ comparison between unpacked and emulated",
-    ignoreBin = ignoreBins
 )
 # sequences
 l1tStage2uGMTEmulatorOnlineDQMSeq = cms.Sequence(
