30b34a46de5eace4405482817f121e55897622e1
Dinyar Rabady <dinyar@cern.ch>
2022-05-28 20:28:26 +0200

    Add uGMT muon showers to DQM quality tests

--- DQM/L1TMonitorClient/python/L1TStage2uGMTClient_cff.py
+++ DQM/L1TMonitorClient/python/L1TStage2uGMTClient_cff.py
@@ -48,6 +48,44 @@ l1tStage2uGMTMuonVsuGMTMuonCopy5RatioClient = l1tStage2uGMTMuonVsuGMTMuonCopy1Ra
     inputDen = ugmtMuCpyDqmDir+'/uGMTMuonCopy5/'+errHistDenStr,
     ratioTitle = 'Summary of mismatch rates between uGMT muons and uGMT muon copy 5'
 )
+
+# Showers
+l1tStage2uGMTShowerVsuGMTShowerCopy1RatioClient = l1tStage2uGMTMuonVsuGMTMuonCopy1RatioClient.clone(
+    monitorDir = ugmtMuCpyDqmDir+'/uGMTMuonShoweruGMTMuonShowerCopy2',
+    inputNum = ugmtMuCpyDqmDir+'/uGMTMuonShowerCopies/uGMTMuonShowerCopy1/'+errHistNumStr,
+    inputDen = ugmtMuCpyDqmDir+'/uGMTMuonShowerCopies/uGMTMuonShowerCopy1/'+errHistDenStr,
+    ratioTitle = 'Summary of mismatch rates between uGMT showers and uGMT shower copy 1',
+    ignoreBin = cms.untracked.vint32(ignoreBins['OutputCopies'])
+)
+l1tStage2uGMTShowerVsuGMTShowerCopy2RatioClient = l1tStage2uGMTMuonVsuGMTMuonCopy1RatioClient.clone(
+    monitorDir = ugmtMuCpyDqmDir+'/uGMTMuonShoweruGMTMuonShowerCopy2',
+    inputNum = ugmtMuCpyDqmDir+'/uGMTMuonShowerCopies/uGMTMuonShowerCopy2/'+errHistNumStr,
+    inputDen = ugmtMuCpyDqmDir+'/uGMTMuonShowerCopies/uGMTMuonShowerCopy2/'+errHistDenStr,
+    ratioTitle = 'Summary of mismatch rates between uGMT showers and uGMT shower copy 2',
+    ignoreBin = cms.untracked.vint32(ignoreBins['OutputCopies'])
+)
+l1tStage2uGMTShowerVsuGMTShowerCopy3RatioClient = l1tStage2uGMTMuonVsuGMTMuonCopy1RatioClient.clone(
+    monitorDir = ugmtMuCpyDqmDir+'/uGMTMuonShoweruGMTMuonShowerCopy3',
+    inputNum = ugmtMuCpyDqmDir+'/uGMTMuonShowerCopies/uGMTMuonShowerCopy3/'+errHistNumStr,
+    inputDen = ugmtMuCpyDqmDir+'/uGMTMuonShowerCopies/uGMTMuonShowerCopy3/'+errHistDenStr,
+    ratioTitle = 'Summary of mismatch rates between uGMT showers and uGMT shower copy 3',
+    ignoreBin = cms.untracked.vint32(ignoreBins['OutputCopies'])
+)
+l1tStage2uGMTShowerVsuGMTShowerCopy4RatioClient = l1tStage2uGMTMuonVsuGMTMuonCopy1RatioClient.clone(
+    monitorDir = ugmtMuCpyDqmDir+'/uGMTMuonShoweruGMTMuonShowerCopy4',
+    inputNum = ugmtMuCpyDqmDir+'/uGMTMuonShowerCopies/uGMTMuonShowerCopy4/'+errHistNumStr,
+    inputDen = ugmtMuCpyDqmDir+'/uGMTMuonShowerCopies/uGMTMuonShowerCopy4/'+errHistDenStr,
+    ratioTitle = 'Summary of mismatch rates between uGMT showers and uGMT shower copy 4',
+    ignoreBin = cms.untracked.vint32(ignoreBins['OutputCopies'])
+)
+l1tStage2uGMTShowerVsuGMTShowerCopy5RatioClient = l1tStage2uGMTMuonVsuGMTMuonCopy1RatioClient.clone(
+    monitorDir = ugmtMuCpyDqmDir+'/uGMTMuonShoweruGMTMuonShowerCopy5',
+    inputNum = ugmtMuCpyDqmDir+'/uGMTMuonShowerCopies/uGMTMuonShowerCopy5/'+errHistNumStr,
+    inputDen = ugmtMuCpyDqmDir+'/uGMTMuonShowerCopies/uGMTMuonShowerCopy5/'+errHistDenStr,
+    ratioTitle = 'Summary of mismatch rates between uGMT showers and uGMT shower copy 5',
+    ignoreBin = cms.untracked.vint32(ignoreBins['OutputCopies'])
+)
+
 # RegionalMuonCands
 l1tStage2BmtfOutVsuGMTInRatioClient = l1tStage2uGMTMuonVsuGMTMuonCopy1RatioClient.clone(
     monitorDir = ugmtDqmDir+'/BMTFoutput_vs_uGMTinput',
@@ -71,6 +109,15 @@ l1tStage2EmtfOutVsuGMTInRatioClient = l1tStage2uGMTMuonVsuGMTMuonCopy1RatioClien
     ignoreBin = ignoreBins['Emtf']
 )
 
+# RegionalShowerCands
+l1tStage2EmtfOutVsuGMTInShowerRatioClient = l1tStage2uGMTMuonVsuGMTMuonCopy1RatioClient.clone(
+    monitorDir = ugmtDqmDir+'/EMTFoutput_vs_uGMTinput/Muon Showers',
+    inputNum = ugmtDqmDir+'/EMTFoutput_vs_uGMTinput/Muon Showers/'+errHistNumStr,
+    inputDen = ugmtDqmDir+'/EMTFoutput_vs_uGMTinput/Muon Showers/'+errHistDenStr,
+    ratioTitle = 'Summary of mismatch rates between EMTF output showers and uGMT input showers from EMTF',
+    ignoreBin = ignoreBins['EmtfShowers']
+)
+
 # sequences
 l1tStage2uGMTMuonCompClient = cms.Sequence(
     l1tStage2uGMTMuonVsuGMTMuonCopy1RatioClient
@@ -79,12 +126,25 @@ l1tStage2uGMTMuonCompClient = cms.Sequence(
   + l1tStage2uGMTMuonVsuGMTMuonCopy4RatioClient
   + l1tStage2uGMTMuonVsuGMTMuonCopy5RatioClient
 )
+# Add shower copy tests for Run-3
+_run3_l1tStage2uGMTMuonCompClient = cms.Sequence(
+    l1tStage2uGMTMuonCompClient.copy()
+  + l1tStage2uGMTShowerVsuGMTShowerCopy1RatioClient
+  + l1tStage2uGMTShowerVsuGMTShowerCopy2RatioClient
+  + l1tStage2uGMTShowerVsuGMTShowerCopy3RatioClient
+  + l1tStage2uGMTShowerVsuGMTShowerCopy4RatioClient
+  + l1tStage2uGMTShowerVsuGMTShowerCopy5RatioClient
+)
+stage2L1Trigger_2021.toReplaceWith(l1tStage2uGMTMuonCompClient, _run3_l1tStage2uGMTMuonCompClient)
 
 l1tStage2uGMTRegionalMuonCandCompClient = cms.Sequence(
     l1tStage2BmtfOutVsuGMTInRatioClient
   + l1tStage2OmtfOutVsuGMTInRatioClient
   + l1tStage2EmtfOutVsuGMTInRatioClient
 )
+# Add input vs. output shower tests in Run-3.
+_run3_l1tStage2uGMTRegionalMuonCandCompClient = cms.Sequence(l1tStage2uGMTRegionalMuonCandCompClient.copy() + l1tStage2EmtfOutVsuGMTInShowerRatioClient)
+stage2L1Trigger_2021.toReplaceWith(l1tStage2uGMTRegionalMuonCandCompClient, _run3_l1tStage2uGMTRegionalMuonCandCompClient)
 
 l1tStage2uGMTClient = cms.Sequence(
     l1tStage2uGMTMuonCompClient
