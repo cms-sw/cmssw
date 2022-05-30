f989fdfdc44254bb9886d1e4a1f742e40e24914c
Vladimir <Vladimir.Ivantchenko@cern.ch>
2022-05-30 20:10:38 +0200

    combined commit

--- SimG4Core/Notification/interface/G4SimVertex.h
+++ SimG4Core/Notification/interface/G4SimVertex.h
@@ -3,14 +3,13 @@
 
 #include "DataFormats/Math/interface/Vector3D.h"
 #include "DataFormats/Math/interface/LorentzVector.h"
-#include <vector>
 #include <cmath>
 
 class G4SimVertex {
 public:
-  G4SimVertex() {}
   G4SimVertex(const math::XYZVectorD& ip, double it, int iv, unsigned int typ = 0)
       : ilv_(ip), itime_(it), itrack_(iv), procType_(typ) {}
+  ~G4SimVertex() = default;
   /// index of the parent (-1 if no parent)
   const math::XYZVectorD& vertexPosition() const { return ilv_; }
   double vertexGlobalTime() const { return itime_; }
