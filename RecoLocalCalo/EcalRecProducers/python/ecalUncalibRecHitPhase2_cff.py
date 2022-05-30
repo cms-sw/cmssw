b1ba0842a97ee0add91c7130315ef94a7bb9292b
Christopher Rhys Sandever <89704887+ChrisSandever@users.noreply.github.com>
2022-05-30 23:22:15 +0100

    Chanhed Ph2 to Phase2 and removes EE objects

--- RecoLocalCalo/EcalRecProducers/python/ecalUncalibRecHitPhase2_cff.py
+++ RecoLocalCalo/EcalRecProducers/python/ecalUncalibRecHitPhase2_cff.py
@@ -14,39 +14,42 @@ ecalUncalibRecHitPhase2Task = cms.Task(ecalUncalibRecHitPhase2)
 # conditions used on gpu
 
 
-from RecoLocalCalo.EcalRecProducers.ecalPh2DigiToGPUProducer_cfi import ecalPh2DigiToGPUProducer as _ecalPh2DigiToGPUProducer
-ecalPh2DigiToGPUProducer = _ecalPh2DigiToGPUProducer.clone()
+from RecoLocalCalo.EcalRecProducers.ecalPhase2DigiToGPUProducer_cfi import ecalPhase2DigiToGPUProducer as _ecalPhase2DigiToGPUProducer
+ecalPhase2DigiToGPUProducer = _ecalPhase2DigiToGPUProducer.clone()
 
 # gpu weights
 from RecoLocalCalo.EcalRecProducers.ecalUncalibRecHitPhase2GPU_cfi import ecalUncalibRecHitPhase2GPU as _ecalUncalibRecHitPhase2GPU
 ecalUncalibRecHitPhase2GPU = _ecalUncalibRecHitPhase2GPU.clone(
-  digisLabelEB = cms.InputTag('ecalPh2DigiToGPUProducer', 'ebDigis')
+  digisLabelEB = ('ecalPhase2DigiToGPUProducer', 'ebDigis')
 )
 
 # copy the uncalibrated rechits from GPU to CPU
 from RecoLocalCalo.EcalRecProducers.ecalCPUUncalibRecHitProducer_cfi import ecalCPUUncalibRecHitProducer as _ecalCPUUncalibRecHitProducer
-ecalMultiFitUncalibRecHitSoAnew = _ecalCPUUncalibRecHitProducer.clone(
-  recHitsInLabelEB = cms.InputTag('ecalUncalibRecHitPhase2GPU', 'EcalUncalibRecHitsEB'),
-  isPhase2 = cms.bool(True)
+ecalUncalibRecHitSoA = _ecalCPUUncalibRecHitProducer.clone(
+  recHitsInLabelEB = ('ecalUncalibRecHitPhase2GPU', 'EcalUncalibRecHitsEB'),
+  isPhase2 = True,
+  recHitsInLabelEE = None,  # remove unneeded Phase1 parameters
+  recHitsOutLabelEE = None
 )
 
 
-# convert the uncalibrated rechits from SoA to legacy format
 from RecoLocalCalo.EcalRecProducers.ecalUncalibRecHitConvertGPU2CPUFormat_cfi import ecalUncalibRecHitConvertGPU2CPUFormat as _ecalUncalibRecHitConvertGPU2CPUFormat
 gpu.toModify(ecalUncalibRecHitPhase2,
-  cuda = _ecalUncalibRecHitConvertGPU2CPUFormat.clone(
-  recHitsLabelGPUEB = cms.InputTag('ecalMultiFitUncalibRecHitSoAnew', 'EcalUncalibRecHitsEB'),
-  isPhase2 = cms.bool(True)
+    cuda = _ecalUncalibRecHitConvertGPU2CPUFormat.clone(
+        isPhase2 = cms.bool(True),
+        recHitsLabelGPUEB = cms.InputTag('ecalUncalibRecHitSoA', 'EcalUncalibRecHitsEB'),
+        recHitsLabelGPUEE = None,  # remove unneeded Phase1 parameters
+        recHitsLabelCPUEE = None
     )
 )
 
 gpu.toReplaceWith(ecalUncalibRecHitPhase2Task, cms.Task(
   # convert phase2 digis to GPU SoA
-  ecalPh2DigiToGPUProducer, 
+  ecalPhase2DigiToGPUProducer, 
   # ECAL weights running on GPU
   ecalUncalibRecHitPhase2GPU,
   # copy the uncalibrated rechits from GPU to CPU
-  ecalMultiFitUncalibRecHitSoAnew,
+  ecalUncalibRecHitSoA,
   # ECAL multifit running on CPU, or convert the uncalibrated rechits from SoA to legacy format
   ecalUncalibRecHitPhase2,
 ))
