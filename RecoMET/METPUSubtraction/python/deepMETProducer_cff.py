622f66830c50a48db773d54e9789aed62ba4b064
William McCormack <wmccorma@mit.edu>
2022-05-31 11:10:17 -0500

    adjusting deepmet configs to account for new phase 2 model

--- RecoMET/METPUSubtraction/python/deepMETProducer_cff.py
+++ RecoMET/METPUSubtraction/python/deepMETProducer_cff.py
@@ -13,12 +13,12 @@ from Configuration.Eras.Modifier_phase2_common_cff import phase2_common
 phase2_common.toModify(
     deepMETsResolutionTune,
     max_n_pf=12500,
-    graph_path="RecoMET/METPUSubtraction/data/models/deepmet/deepmet_v1_phase2/model.graphdef"
+    graph_path="RecoMET/METPUSubtraction/data/models/deepmet_phase2/deepmet_v1_phase2/model.graphdef"
 )
 phase2_common.toModify(
     deepMETsResponseTune,
     max_n_pf=12500,
-    graph_path="RecoMET/METPUSubtraction/data/models/deepmet/deepmet_resp_v1_phase2/model.graphdef"
+    graph_path="RecoMET/METPUSubtraction/data/models/deepmet_phase2/deepmet_resp_v1_phase2/model.graphdef"
 )
 
 from Configuration.Eras.Modifier_run2_jme_2016_cff import run2_jme_2016
@@ -34,6 +34,8 @@ from Configuration.ProcessModifiers.deepMETSonicTriton_cff import deepMETSonicTr
 _deepMETsResolutionTuneSonic = _deepMETSonicProducer.clone(
     max_n_pf = deepMETsResolutionTune.max_n_pf,
     Client = dict(
+        modelName = deepMETsResolutionTune.graph_path.value().split('/')[-3],
+        modelConfigPath = '/'.join(deepMETsResolutionTune.graph_path.value().split('/')[:-2])+'/config.pbtxt',
         modelVersion = os.path.realpath(pfnInPath(deepMETsResolutionTune.graph_path.value()).split(':')[-1]).split('/')[-2], #model "1"
     ),
 )
@@ -42,6 +44,8 @@ deepMETSonicTriton.toReplaceWith(deepMETsResolutionTune, _deepMETsResolutionTune
 _deepMETsResponseTuneSonic = _deepMETSonicProducer.clone(
     max_n_pf = deepMETsResponseTune.max_n_pf,
     Client = dict(
+        modelName = deepMETsResponseTune.graph_path.value().split('/')[-3],
+        modelConfigPath = '/'.join(deepMETsResponseTune.graph_path.value().split('/')[:-2])+'/config.pbtxt',
         modelVersion = os.path.realpath(pfnInPath(deepMETsResponseTune.graph_path.value()).split(':')[-1]).split('/')[-2], #model "2"
     ),
 )
