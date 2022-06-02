fd82c9f9fc830b6c36d8df1fd288c2bc0ce9431d
mmusich <marco.musich@cern.ch>
2022-06-02 15:51:33 +0200

    rename also the Output commands

--- Calibration/TkAlCaRecoProducers/python/ALCARECOPromptCalibProdSiStripHitEfficiency_Output_cff.py
+++ Calibration/TkAlCaRecoProducers/python/ALCARECOPromptCalibProdSiStripHitEfficiency_Output_cff.py
@@ -5,5 +5,5 @@ OutALCARECOPromptCalibProdSiStripHitEff_noDrop = cms.PSet(
         SelectEvents = cms.vstring('pathALCARECOPromptCalibProdSiStripHitEfficiency')),
     outputCommands = cms.untracked.vstring('keep *_MEtoEDMConvertSiStripHitEff_*_*'))
 
-OutALCARECOPromptCalibProdSiStripHitEfficiency = OutALCARECOPromptCalibProdSiStripHitEff_noDrop.clone()
-OutALCARECOPromptCalibProdSiStripHitEfficiency.outputCommands.insert(0, "drop *")
+OutALCARECOPromptCalibProdSiStripHitEff = OutALCARECOPromptCalibProdSiStripHitEff_noDrop.clone()
+OutALCARECOPromptCalibProdSiStripHitEff.outputCommands.insert(0, "drop *")
