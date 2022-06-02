a5d5ae67cc91b5011f8ca543944e6bba5b76fbb8
Qiang Li <qiang.li@cern.ch>
2022-06-02 15:31:29 +0800

    Revert "[L1T] fix negative shift"

--- L1Trigger/TrackFindingTracklet/interface/imath.h
+++ L1Trigger/TrackFindingTracklet/interface/imath.h
@@ -1014,7 +1014,7 @@ namespace trklet {
         case mode::pos:
           return addr << shift_;
         case mode::neg:
-          return (Nelements_ - addr) << shift_;
+          return (addr - Nelements_) << shift_;
         case mode::both:
           return (addr << ashift_) >> (ashift_ - shift_);
       }
