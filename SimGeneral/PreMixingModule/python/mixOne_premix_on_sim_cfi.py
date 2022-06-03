70eb7083d1dab2eba1e418bb47d6def8e1e8ea84
mmusich <marco.musich@cern.ch>
2022-06-03 16:49:16 +0200

    run the pixel rundependent modifier before the fast-sim modifications

--- SimGeneral/PreMixingModule/python/mixOne_premix_on_sim_cfi.py
+++ SimGeneral/PreMixingModule/python/mixOne_premix_on_sim_cfi.py
@@ -153,6 +153,14 @@ mixData = cms.EDProducer("PreMixingModule",
     ),
 )
 
+# pixel run-dependent
+from Configuration.ProcessModifiers.runDependentForPixel_cff import runDependentForPixel
+from Configuration.ProcessModifiers.premix_stage2_cff import premix_stage2
+(runDependentForPixel & premix_stage2).toModify(mixData.workers.pixel,
+         UseReweighting=False,
+         applyLateReweighting=True,
+         store_SimHitEntryExitPoints=False
+)
 
 from Configuration.Eras.Modifier_fastSim_cff import fastSim
 from FastSimulation.Tracking.recoTrackAccumulator_cfi import recoTrackAccumulator as _recoTrackAccumulator
@@ -319,10 +327,3 @@ phase2_hfnose.toModify(mixData,
 # Run-dependent MC
 from Configuration.ProcessModifiers.runDependent_cff import runDependent
 runDependent.toModify(mixData.workers.ecal, timeDependent=True)
-from Configuration.ProcessModifiers.runDependentForPixel_cff import runDependentForPixel  
-from Configuration.ProcessModifiers.premix_stage2_cff import premix_stage2
-(runDependentForPixel & premix_stage2).toModify(mixData.workers.pixel,
-         UseReweighting=False,
-         applyLateReweighting=True,
-         store_SimHitEntryExitPoints=False
-)
