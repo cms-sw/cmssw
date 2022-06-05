70101c34542577e0eb2a78c380c5cf13def9eb26
Sunanda <sunanda.banerjee@cern.ch>
2022-06-05 08:27:25 +0200

    Update a few cfg's

--- Validation/HGCalValidation/test/python/runHGCalDigiStudy_cfg.py
+++ Validation/HGCalValidation/test/python/runHGCalDigiStudy_cfg.py
@@ -31,26 +31,31 @@ if (options.geometry == "D83"):
     process = cms.Process('PROD',Phase2C11M9)
     process.load('Configuration.Geometry.GeometryExtended2026D83_cff')
     process.load('Configuration.Geometry.GeometryExtended2026D83Reco_cff')
+    fileInput = 'file:step2D83tt.root'
     fileName = 'hgcDigiD83tt.root'
 elif (options.geometry == "D88"):
     from Configuration.Eras.Era_Phase2C11M9_cff import Phase2C11M9
     process = cms.Process('PROD',Phase2C11M9)
     process.load('Configuration.Geometry.GeometryExtended2026D88_cff')
     process.load('Configuration.Geometry.GeometryExtended2026D88Reco_cff')
+    fileInput = 'file:step2D88tt.root'
     fileName = 'hgcDigiD88tt.root'
 elif (options.geometry == "D92"):
     from Configuration.Eras.Era_Phase2C11M9_cff import Phase2C11M9
     process = cms.Process('PROD',Phase2C11M9)
     process.load('Configuration.Geometry.GeometryExtended2026D92_cff')
     process.load('Configuration.Geometry.GeometryExtended2026D92Reco_cff')
+    fileInput = 'file:step2D92tt.root'
     fileName = 'hgcDigiD92tt.root'
 else:
     from Configuration.Eras.Era_Phase2C11_cff import Phase2C11
     process = cms.Process('PROD',Phase2C11)
     process.load('Configuration.Geometry.GeometryExtended2026D77_cff')
     process.load('Configuration.Geometry.GeometryExtended2026D77Reco_cff')
+    fileInput = 'file:step2D77tt.root'
     fileName = 'hgcDigiD77tt.root'
 
+print("Input file: ", fileInput)
 print("Output file: ", fileName)
 
 process.load("SimGeneral.HepPDTESSource.pythiapdt_cfi")
@@ -63,14 +68,9 @@ from Configuration.AlCa.GlobalTag import GlobalTag
 process.GlobalTag = GlobalTag(process.GlobalTag, 'auto:phase2_realistic_T21', '')
 
 process.source = cms.Source("PoolSource",
-                            fileNames = cms.untracked.vstring(
-        'file:step2.root',
-        )
-                            )
+                            fileNames = cms.untracked.vstring(fileInput) )
 
-process.maxEvents = cms.untracked.PSet(
-    input = cms.untracked.int32(-1)
-)
+process.maxEvents = cms.untracked.PSet( input = cms.untracked.int32(-1) )
 
 process.TFileService = cms.Service("TFileService",
                                    fileName = cms.string(fileName),
--- Validation/HGCalValidation/test/python/runHGCalRecHitStudy_cfg.py
+++ Validation/HGCalValidation/test/python/runHGCalRecHitStudy_cfg.py
@@ -31,26 +31,31 @@ if (options.geometry == "D83"):
     process = cms.Process('PROD',Phase2C11M9)
     process.load('Configuration.Geometry.GeometryExtended2026D83_cff')
     process.load('Configuration.Geometry.GeometryExtended2026D83Reco_cff')
+    fileInput = 'file:step3D83tt.root'
     fileName = 'hgcRecHitD83tt.root'
 elif (options.geometry == "D88"):
     from Configuration.Eras.Era_Phase2C11M9_cff import Phase2C11M9
     process = cms.Process('PROD',Phase2C11M9)
     process.load('Configuration.Geometry.GeometryExtended2026D88_cff')
     process.load('Configuration.Geometry.GeometryExtended2026D88Reco_cff')
+    fileInput = 'file:step3D88tt.root'
     fileName = 'hgcRecHitD88tt.root'
 elif (options.geometry == "D92"):
     from Configuration.Eras.Era_Phase2C11M9_cff import Phase2C11M9
     process = cms.Process('PROD',Phase2C11M9)
     process.load('Configuration.Geometry.GeometryExtended2026D92_cff')
     process.load('Configuration.Geometry.GeometryExtended2026D92Reco_cff')
+    fileInput = 'file:step3D92tt.root'
     fileName = 'hgcRecHitD92tt.root'
 else:
     from Configuration.Eras.Era_Phase2C11_cff import Phase2C11
     process = cms.Process('PROD',Phase2C11)
     process.load('Configuration.Geometry.GeometryExtended2026D77_cff')
     process.load('Configuration.Geometry.GeometryExtended2026D77Reco_cff')
+    fileInput = 'file:step3D77tt.root'
     fileName = 'hgcRecHitD77tt.root'
 
+print("Input file: ", fileInput)
 print("Output file: ", fileName)
 
 process.load("SimGeneral.HepPDTESSource.pythiapdt_cfi")
@@ -65,12 +70,9 @@ process.GlobalTag = GlobalTag(process.GlobalTag, 'auto:phase2_realistic_T21', ''
 process.MessageLogger.cerr.FwkReport.reportEvery = 2
 
 process.source = cms.Source("PoolSource",
-                            fileNames = cms.untracked.vstring('file:step3.root')
-)
+                            fileNames = cms.untracked.vstring(fileInput) )
 
-process.maxEvents = cms.untracked.PSet(
-    input = cms.untracked.int32(-1)
-)
+process.maxEvents = cms.untracked.PSet( input = cms.untracked.int32(-1) )
 
 process.load('Validation.HGCalValidation.hgcalRecHitStudy_cff')
 
