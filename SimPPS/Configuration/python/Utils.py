7e60a2efa1783e5fdb47ea1de502bcde309753ac
Laurent Forthomme <laurent.forthomme@cern.ch>
2022-06-07 18:09:21 +0200

    Config: adding rng service only if not found in process attributes ; added a base setupPPSDirectSim customiser to avoid code duplication

--- SimPPS/Configuration/python/Utils.py
+++ SimPPS/Configuration/python/Utils.py
@@ -1,27 +1,29 @@
 import FWCore.ParameterSet.Config as cms
 
-def setupPPSDirectSimAOD(process):
+def setupPPSDirectSim(process):
     process.load('SimPPS.Configuration.directSimPPS_cff')
     process.load('RecoPPS.Configuration.recoCTPPS_cff')
+    if not hasattr(process, 'RandomNumberGeneratorService'):
+        process.RandomNumberGeneratorService = cms.Service("RandomNumberGeneratorService")
+    if not hasattr(process.RandomNumberGeneratorService, 'beamDivergenceVtxGenerator'):
+        process.RandomNumberGeneratorService.beamDivergenceVtxGenerator = cms.PSet(initialSeed = cms.untracked.uint32(3849))
+    if not hasattr(process.RandomNumberGeneratorService, 'ppsDirectProtonSimulation'):
+        process.RandomNumberGeneratorService.ppsDirectProtonSimulation = cms.PSet(initialSeed = cms.untracked.uint32(4981))
+    process.ppsDirectSim = cms.Path(process.directSimPPS * process.recoDirectSimPPS)
+    process.schedule.append(process.ppsDirectSim)
+
+    from SimPPS.DirectSimProducer.matching_cff import matchDirectSimOutputs
+    matchDirectSimOutputs(process)
+    return process
+
+def setupPPSDirectSimAOD(process):
+    setupPPSDirectSim(process)
     from SimPPS.DirectSimProducer.matching_cff import matchDirectSimOutputsAOD
     matchDirectSimOutputsAOD(process)
-    process.RandomNumberGeneratorService = cms.Service("RandomNumberGeneratorService",
-        beamDivergenceVtxGenerator = cms.PSet(initialSeed = cms.untracked.uint32(3849)),
-        ppsDirectProtonSimulation = cms.PSet(initialSeed = cms.untracked.uint32(4981))
-    )
-    process.directSim = cms.Path(process.directSimPPS * process.recoDirectSimPPS)
-    process.schedule.append(process.directSim)
     return process
 
 def setupPPSDirectSimMiniAOD(process):
-    process.load('SimPPS.Configuration.directSimPPS_cff')
-    process.load('RecoPPS.Configuration.recoCTPPS_cff')
+    setupPPSDirectSim(process)
     from SimPPS.DirectSimProducer.matching_cff import matchDirectSimOutputsMiniAOD
     matchDirectSimOutputsMiniAOD(process)
-    process.RandomNumberGeneratorService = cms.Service("RandomNumberGeneratorService",
-        beamDivergenceVtxGenerator = cms.PSet(initialSeed = cms.untracked.uint32(3849)),
-        ppsDirectProtonSimulation = cms.PSet(initialSeed = cms.untracked.uint32(4981))
-    )
-    process.directSim = cms.Path(process.directSimPPS * process.recoDirectSimPPS)
-    process.schedule.append(process.directSim)
     return process
