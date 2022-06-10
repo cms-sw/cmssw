c924fa1317fa6914692051430823ebbb398f081b
Tamas <Tamas.Almos.Vami@cern.ch>
2022-06-10 20:53:31 +0200

    Updating EGM regression tags and conditions for the L1 EMTF and CALO tags for Run-3 MCs

--- Configuration/AlCa/python/autoCond.py
+++ Configuration/AlCa/python/autoCond.py
@@ -68,21 +68,21 @@ autoCond = {
     # GlobalTag for MC production (cosmics) with realistic conditions for full Phase1 2018 detector,  Strip tracker in PEAK mode
     'phase1_2018_cosmics_peak'     : '123X_upgrade2018cosmics_realistic_peak_v2',
     # GlobalTag for MC production with perfectly aligned and calibrated detector for Phase1 2022
-    'phase1_2022_design'           : '124X_mcRun3_2022_design_v4',
+    'phase1_2022_design'           : '124X_mcRun3_2022_design_v5',
     # GlobalTag for MC production with realistic conditions for Phase1 2022
-    'phase1_2022_realistic'        : '124X_mcRun3_2022_realistic_v4',
+    'phase1_2022_realistic'        : '124X_mcRun3_2022_realistic_v5',
     # GlobalTag for MC production (cosmics) with realistic conditions for Phase1 2022,  Strip tracker in DECO mode
-    'phase1_2022_cosmics'          : '124X_mcRun3_2022cosmics_realistic_deco_v5',
+    'phase1_2022_cosmics'          : '124X_mcRun3_2022cosmics_realistic_deco_v6',
     # GlobalTag for MC production (cosmics) with perfectly aligned and calibrated detector for Phase1 2022, Strip tracker in DECO mode
-    'phase1_2022_cosmics_design'   : '124X_mcRun3_2022cosmics_design_deco_v4',
+    'phase1_2022_cosmics_design'   : '124X_mcRun3_2022cosmics_design_deco_v5',
     # GlobalTag for MC production with realistic conditions for Phase1 2022 detector for Heavy Ion
-    'phase1_2022_realistic_hi'     : '124X_mcRun3_2022_realistic_HI_v4',
+    'phase1_2022_realistic_hi'     : '124X_mcRun3_2022_realistic_HI_v5',
     # GlobalTag for MC production with realistic conditions for Phase1 2023
-    'phase1_2023_realistic'        : '124X_mcRun3_2023_realistic_v4',
+    'phase1_2023_realistic'        : '124X_mcRun3_2023_realistic_v5',
     # GlobalTag for MC production with realistic conditions for Phase1 2024
-    'phase1_2024_realistic'        : '124X_mcRun3_2024_realistic_v4',
+    'phase1_2024_realistic'        : '124X_mcRun3_2024_realistic_v5',
     # GlobalTag for MC production with realistic conditions for Phase2
-    'phase2_realistic'             : '124X_mcRun4_realistic_v5'
+    'phase2_realistic'             : '124X_mcRun4_realistic_v6'
 }
 
 aliases = {
