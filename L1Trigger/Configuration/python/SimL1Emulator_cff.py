1a4dd10032a3ca6b1ccfc561d1dd9b9892ffa198
ccaillol <ccaillol@cern.ch>
2022-06-10 12:45:36 +0200

    remove l1pfjetmet sequence

--- L1Trigger/Configuration/python/SimL1Emulator_cff.py
+++ L1Trigger/Configuration/python/SimL1Emulator_cff.py
@@ -214,8 +214,8 @@ _phase2_siml1emulator.add(l1PFJetsPhase1Task_9x9)
 
 # PF MET
 # ########################################################################
-from L1Trigger.Phase2L1ParticleFlow.l1pfJetMet_cff import *
-_phase2_siml1emulator.add(l1PFJetsTask)
+#from L1Trigger.Phase2L1ParticleFlow.l1pfJetMet_cff import *
+#_phase2_siml1emulator.add(l1PFJetsTask)
 
 from L1Trigger.Phase2L1ParticleFlow.L1MetPfProducer_cfi import *
 _phase2_siml1emulator.add(L1MetPfProducer)
