c34e04c4e7ff065fadf52c12923ad28b59ccf1f2
Gianluca <gianluca.cerminara@cern.ch>
2022-06-13 14:26:45 +0200

    remove obsolete e/g plugins
    (cherry picked from commit 31600b06867c4713f1fcf701c9a69d09fbb6536e)

--- L1Trigger/L1CaloTrigger/python/l1EGammaEEProducer_cfi.py
+++
@@ -1,5 +0,0 @@
-import FWCore.ParameterSet.Config as cms
-
-l1EGammaEEProducer = cms.EDProducer("L1EGammaEEProducer",
-                                    calibrationConfig = cms.PSet(calibrationFile = cms.FileInPath('L1Trigger/L1TCalorimeter/data/calib_ee_v1.json')),
-                                    Multiclusters=cms.InputTag('hgcalBackEndLayer2Producer:HGCalBackendLayer2Processor3DClustering'))
