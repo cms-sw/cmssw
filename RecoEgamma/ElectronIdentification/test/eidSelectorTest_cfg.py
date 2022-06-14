99ed83e1c8d3ee0474b5d347ea519ea34e22a1b2
JeongEun Lee <jeongeun.lee@cern.ch>
2022-06-15 00:20:28 +0900

    minor fix 3

--- RecoEgamma/ElectronIdentification/test/eidSelectorTest_cfg.py
+++ RecoEgamma/ElectronIdentification/test/eidSelectorTest_cfg.py
@@ -3,7 +3,7 @@ from FWCore.ParameterSet.Config import *
 process = cms.Process("runElectronID")
 
 process.load('FWCore.MessageService.MessageLogger_cfi')
-process.load('Configuration/StandardSequences/Services_cff')
+process.load('Configuration.StandardSequences.Services_cff')
 process.load('Configuration.StandardSequences.GeometryDB_cff')
 process.load("Configuration.StandardSequences.MagneticField_cff")
 
