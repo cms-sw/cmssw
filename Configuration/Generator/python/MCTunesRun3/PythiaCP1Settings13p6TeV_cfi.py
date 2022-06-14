89c96f1b5843111b3bb7e7d3ed8977364f7b7106
Sanghyun Ko <sang.hyun.ko@cern.ch>
2022-06-14 12:55:07 +0200

    add 13.6 TeV CPX settings seperately

---
+++ Configuration/Generator/python/MCTunesRun3/PythiaCP1Settings13p6TeV_cfi.py
@@ -0,0 +1,20 @@
+import FWCore.ParameterSet.Config as cms
+
+pythia8CP1Settings13p6TeVBlock = cms.PSet(
+    pythia8CP1Settings13p6TeV = cms.vstring(
+        'Tune:pp 14',
+	'Tune:ee 7',
+	'MultipartonInteractions:bProfile=2',
+	'MultipartonInteractions:ecmPow=0.1543',
+	'MultipartonInteractions:pT0Ref=2.40',
+	'MultipartonInteractions:coreRadius=0.5436',
+	'MultipartonInteractions:coreFraction=0.6836',
+	'ColourReconnection:range=2.633',
+	'SigmaTotal:zeroAXB=off',
+	'SpaceShower:rapidityOrder=off',
+        'SigmaTotal:mode = 0',
+        'SigmaTotal:sigmaEl = 22.08',
+        'SigmaTotal:sigmaTot = 101.037',
+        'PDF:pSet=LHAPDF6:NNPDF31_lo_as_0130',
+	)
+)
---
+++ Configuration/Generator/python/MCTunesRun3/PythiaCP2Settings13p6TeV_cfi.py
@@ -0,0 +1,23 @@
+import FWCore.ParameterSet.Config as cms
+
+pythia8CP2Settings13p6TeVBlock = cms.PSet(
+    pythia8CP2Settings13p6TeV = cms.vstring(
+	'Tune:pp 14',
+	'Tune:ee 7',
+	'MultipartonInteractions:ecmPow=0.1391',
+	'MultipartonInteractions:bProfile=2',
+	'MultipartonInteractions:pT0Ref=2.306',
+	'MultipartonInteractions:coreRadius=0.3755',
+	'MultipartonInteractions:coreFraction=0.3269',
+	'ColourReconnection:range=2.323',
+	'SigmaTotal:zeroAXB=off', 
+	'SpaceShower:rapidityOrder=off',
+	'SpaceShower:alphaSvalue=0.13',
+        'TimeShower:alphaSvalue=0.13',
+        'SigmaTotal:mode = 0',
+        'SigmaTotal:sigmaEl = 22.08',
+        'SigmaTotal:sigmaTot = 101.037',
+        'PDF:pSet=LHAPDF6:NNPDF31_lo_as_0130',
+	)
+)
+
