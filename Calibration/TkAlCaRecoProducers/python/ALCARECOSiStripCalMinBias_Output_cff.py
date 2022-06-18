328b58bd680eadbf4de3a50e2fcca39e4340457e
mmusich <marco.musich@cern.ch>
2022-06-18 12:01:35 +0200

    update the SiStrip AlCaReco event contents to cope with no SCAL in Run3

--- Calibration/TkAlCaRecoProducers/python/ALCARECOSiStripCalMinBias_Output_cff.py
+++ Calibration/TkAlCaRecoProducers/python/ALCARECOSiStripCalMinBias_Output_cff.py
@@ -21,3 +21,14 @@ OutALCARECOSiStripCalMinBias_noDrop = cms.PSet(
 import copy
 OutALCARECOSiStripCalMinBias=copy.deepcopy(OutALCARECOSiStripCalMinBias_noDrop)
 OutALCARECOSiStripCalMinBias.outputCommands.insert(0,"drop *")
+
+# in Run3, SCAL digis replaced by onlineMetaDataDigis
+_run3_common_removedCommands = OutALCARECOSiStripCalMinBias.outputCommands
+_run3_common_removedCommands.remove('keep LumiScalerss_scalersRawToDigi_*_*')
+_run3_common_removedCommands.remove('keep DcsStatuss_scalersRawToDigi_*_*')
+
+_run3_common_extraCommands = ['keep DCSRecord_onlineMetaDataDigis_*_*',
+                              'keep OnlineLuminosityRecord_onlineMetaDataDigis_*_*']
+
+from Configuration.Eras.Modifier_run3_common_cff import run3_common
+run3_common.toModify(OutALCARECOSiStripCalMinBias, outputCommands = _run3_common_removedCommands + _run3_common_extraCommands)
