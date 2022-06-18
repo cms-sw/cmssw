8fb670c1e800970d554dbba2031f387704cd797f
mmusich <marco.musich@cern.ch>
2022-06-18 12:01:48 +0200

    update the Tracker Alignment AlCaReco event contents to cope with no SCAL in Run3

--- Alignment/CommonAlignmentProducer/python/ALCARECOTkAlMuonIsolated_Output_cff.py
+++ Alignment/CommonAlignmentProducer/python/ALCARECOTkAlMuonIsolated_Output_cff.py
@@ -17,3 +17,12 @@ OutALCARECOTkAlMuonIsolated_noDrop = cms.PSet(
 import copy
 OutALCARECOTkAlMuonIsolated = copy.deepcopy(OutALCARECOTkAlMuonIsolated_noDrop)
 OutALCARECOTkAlMuonIsolated.outputCommands.insert(0, "drop *")
+
+# in Run3, SCAL digis replaced by onlineMetaDataDigis
+_run3_common_removedCommands = OutALCARECOTkAlMuonIsolated.outputCommands
+_run3_common_removedCommands.remove('keep DcsStatuss_scalersRawToDigi_*_*')
+
+_run3_common_extraCommands = ['keep DCSRecord_onlineMetaDataDigis_*_*']
+
+from Configuration.Eras.Modifier_run3_common_cff import run3_common
+run3_common.toModify(OutALCARECOTkAlMuonIsolated, outputCommands = _run3_common_removedCommands + _run3_common_extraCommands)
