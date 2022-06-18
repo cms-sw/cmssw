734e92d4ff3c2163975991fca3d0635725965eb1
mmusich <marco.musich@cern.ch>
2022-06-18 12:01:48 +0200

    update the Tracker Alignment AlCaReco event contents to cope with no SCAL in Run3

--- Alignment/CommonAlignmentProducer/python/ALCARECOTkAlJpsiMuMuHI_Output_cff.py
+++ Alignment/CommonAlignmentProducer/python/ALCARECOTkAlJpsiMuMuHI_Output_cff.py
@@ -17,3 +17,12 @@ OutALCARECOTkAlJpsiMuMuHI_noDrop = cms.PSet(
 import copy
 OutALCARECOTkAlJpsiMuMuHI = copy.deepcopy(OutALCARECOTkAlJpsiMuMuHI_noDrop)
 OutALCARECOTkAlJpsiMuMuHI.outputCommands.insert(0, "drop *")
+
+# in Run3, SCAL digis replaced by onlineMetaDataDigis
+_run3_common_removedCommands = OutALCARECOTkAlJpsiMuMuHI.outputCommands
+_run3_common_removedCommands.remove('keep DcsStatuss_scalersRawToDigi_*_*')
+
+_run3_common_extraCommands = ['keep DCSRecord_onlineMetaDataDigis_*_*']
+
+from Configuration.Eras.Modifier_run3_common_cff import run3_common
+run3_common.toModify(OutALCARECOTkAlJpsiMuMuHI, outputCommands = _run3_common_removedCommands + _run3_common_extraCommands)
--- Alignment/CommonAlignmentProducer/python/ALCARECOTkAlJpsiMuMu_Output_cff.py
+++ Alignment/CommonAlignmentProducer/python/ALCARECOTkAlJpsiMuMu_Output_cff.py
@@ -17,3 +17,12 @@ OutALCARECOTkAlJpsiMuMu_noDrop = cms.PSet(
 import copy
 OutALCARECOTkAlJpsiMuMu = copy.deepcopy(OutALCARECOTkAlJpsiMuMu_noDrop)
 OutALCARECOTkAlJpsiMuMu.outputCommands.insert(0, "drop *")
+
+# in Run3, SCAL digis replaced by onlineMetaDataDigis
+_run3_common_removedCommands = OutALCARECOTkAlJpsiMuMu.outputCommands
+_run3_common_removedCommands.remove('keep DcsStatuss_scalersRawToDigi_*_*')
+
+_run3_common_extraCommands = ['keep DCSRecord_onlineMetaDataDigis_*_*']
+
+from Configuration.Eras.Modifier_run3_common_cff import run3_common
+run3_common.toModify(OutALCARECOTkAlJpsiMuMu, outputCommands = _run3_common_removedCommands + _run3_common_extraCommands)
