b9d1e7c0844d032cef889d26e5cba64761379fbc
Alejandro Sanchez <alejandro.sanchez@cern.ch>
2022-06-24 00:06:31 +0200

    Revert "Enable ecalGpuTask in Online DQM using HLT collections"
    This reverts commit 02367cc82d932536f5698899860be0f2de535bc3.

--- DQM/EcalMonitorTasks/python/EcalMonitorTask_cff.py
+++ DQM/EcalMonitorTasks/python/EcalMonitorTask_cff.py
@@ -6,16 +6,6 @@ from DQM.EcalMonitorTasks.EcalMonitorTask_cfi import *
 from Configuration.ProcessModifiers.gpuValidationEcal_cff import gpuValidationEcal
 from DQM.EcalMonitorTasks.ecalGpuTask_cfi import ecalGpuTask
 
-# Input tags used for offline DQM RECO, enables SwitchProducerCUDA to generate these collections
-gpuValidationEcal.toModify(ecalDQMCollectionTags, EBCpuDigi = cms.untracked.InputTag("ecalDigis@cpu", "ebDigis"))
-gpuValidationEcal.toModify(ecalDQMCollectionTags, EECpuDigi = cms.untracked.InputTag("ecalDigis@cpu", "eeDigis"))
-gpuValidationEcal.toModify(ecalDQMCollectionTags, EBGpuDigi = cms.untracked.InputTag("ecalDigis@cuda", "ebDigis"))
-gpuValidationEcal.toModify(ecalDQMCollectionTags, EEGpuDigi = cms.untracked.InputTag("ecalDigis@cuda", "eeDigis"))
-gpuValidationEcal.toModify(ecalDQMCollectionTags, EBCpuUncalibRecHit = cms.untracked.InputTag("ecalMultiFitUncalibRecHit@cpu", "EcalUncalibRecHitsEB"))
-gpuValidationEcal.toModify(ecalDQMCollectionTags, EECpuUncalibRecHit = cms.untracked.InputTag("ecalMultiFitUncalibRecHit@cpu", "EcalUncalibRecHitsEE"))
-gpuValidationEcal.toModify(ecalDQMCollectionTags, EBGpuUncalibRecHit = cms.untracked.InputTag("ecalMultiFitUncalibRecHit@cuda", "EcalUncalibRecHitsEB"))
-gpuValidationEcal.toModify(ecalDQMCollectionTags, EEGpuUncalibRecHit = cms.untracked.InputTag("ecalMultiFitUncalibRecHit@cuda", "EcalUncalibRecHitsEE"))
-gpuValidationEcal.toModify(ecalDQMCollectionTags, EBCpuRecHit = cms.untracked.InputTag("ecalRecHit@cpu", "EcalRecHitsEB"))
-gpuValidationEcal.toModify(ecalDQMCollectionTags, EECpuRecHit = cms.untracked.InputTag("ecalRecHit@cpu", "EcalRecHitsEE"))
-gpuValidationEcal.toModify(ecalDQMCollectionTags, EBGpuRecHit = cms.untracked.InputTag("ecalRecHit@cuda", "EcalRecHitsEB"))
-gpuValidationEcal.toModify(ecalDQMCollectionTags, EEGpuRecHit = cms.untracked.InputTag("ecalRecHit@cuda", "EcalRecHitsEE"))
+gpuValidationEcal.toModify(ecalGpuTask.params, runGpuTask = True)
+gpuValidationEcal.toModify(ecalMonitorTask.workers, func = lambda workers: workers.append("GpuTask"))
+gpuValidationEcal.toModify(ecalMonitorTask, workerParameters = dict(GpuTask = ecalGpuTask))
