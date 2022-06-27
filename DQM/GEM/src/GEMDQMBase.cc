daabd124b1fb0355625d0b34f93716b68e9b9cdc
Ivan Razumov <iarspider@gmail.com>
2022-06-27 10:32:09 +0200

    Update GEMDQMBase.cc

--- DQM/GEM/src/GEMDQMBase.cc
+++ DQM/GEM/src/GEMDQMBase.cc
@@ -108,9 +108,7 @@ int GEMDQMBase::loadChambers() {
         int layer_number = pchamber->id().layer();
         ME3IdsKey key3(region_number, station_number, layer_number);
         mapStationInfo_[key3] =
-            MEStationInfo(region_number, station_number, layer_number, num_superchambers, num_etas, num_vfat, num_digi);
-        mapStationInfo_[key3].nMinIdxChamber_ = nMinIdxChamber;
-        mapStationInfo_[key3].nMaxIdxChamber_ = nMaxIdxChamber;
+            MEStationInfo(region_number, station_number, layer_number, num_superchambers, num_etas, num_vfat, num_digi, nMinIdxChamber, nMaxIdxChamber);
         readGeometryRadiusInfoChamber(station, mapStationInfo_[key3]);
         readGeometryPhiInfoChamber(station, mapStationInfo_[key3]);
       }
