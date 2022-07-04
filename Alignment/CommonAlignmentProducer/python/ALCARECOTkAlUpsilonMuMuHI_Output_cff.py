9b9d61e3da4386deb9ad52a121681d295698abb9
mmusich <marco.musich@cern.ch>
2022-07-04 15:04:50 +0200

    fix the TkAlignment AlCaReco event contents to cope with no SCAL in Run3

--- Alignment/CommonAlignmentProducer/python/ALCARECOTkAlUpsilonMuMuHI_Output_cff.py
+++ Alignment/CommonAlignmentProducer/python/ALCARECOTkAlUpsilonMuMuHI_Output_cff.py
@@ -14,15 +14,15 @@ OutALCARECOTkAlUpsilonMuMuHI_noDrop = cms.PSet(
 	'keep *_hiSelectedVertex_*_*')
 )
 
-import copy
-OutALCARECOTkAlUpsilonMuMuHI = copy.deepcopy(OutALCARECOTkAlUpsilonMuMuHI_noDrop)
-OutALCARECOTkAlUpsilonMuMuHI.outputCommands.insert(0, "drop *")
-
 # in Run3, SCAL digis replaced by onlineMetaDataDigis
-_run3_common_removedCommands = OutALCARECOTkAlUpsilonMuMuHI.outputCommands
+import copy
+_run3_common_removedCommands = OutALCARECOTkAlUpsilonMuMuHI_noDrop.outputCommands.copy()
 _run3_common_removedCommands.remove('keep DcsStatuss_scalersRawToDigi_*_*')
 
 _run3_common_extraCommands = ['keep DCSRecord_onlineMetaDataDigis_*_*']
 
 from Configuration.Eras.Modifier_run3_common_cff import run3_common
-run3_common.toModify(OutALCARECOTkAlUpsilonMuMuHI, outputCommands = _run3_common_removedCommands + _run3_common_extraCommands)
+run3_common.toModify(OutALCARECOTkAlUpsilonMuMuHI_noDrop, outputCommands = _run3_common_removedCommands + _run3_common_extraCommands)
+
+OutALCARECOTkAlUpsilonMuMuHI = OutALCARECOTkAlUpsilonMuMuHI_noDrop.clone()
+OutALCARECOTkAlUpsilonMuMuHI.outputCommands.insert(0, "drop *")
