155ea4bf0cdda51084562aaf40737ec2df8f6861
Sunanda <sunanda.banerjee@cern.ch>
2022-07-05 13:09:52 +0200

    Modify the cff as suggested

--- SimG4CMS/Calo/python/hgcalHitPartial_cff.py
+++ SimG4CMS/Calo/python/hgcalHitPartial_cff.py
@@ -3,5 +3,6 @@ import FWCore.ParameterSet.Config as cms
 from SimG4CMS.Calo.hgcalHitPartialEE_cfi import *
 
 hgcalHitPartialHE = hgcalHitPartialEE.clone(
-    nameSense  = cms.string("HGCalHESiliconSensitive"),
-    caloHitSource = cms.string("HGCHitsHEfront"))
+    nameSense  = "HGCalHESiliconSensitive",
+    caloHitSource = "HGCHitsHEfront"
+)
