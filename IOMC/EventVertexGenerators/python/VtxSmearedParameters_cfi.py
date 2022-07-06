acee088d9e98d7421a09f044e5e2e67d1546e34d
francescobrivio <francesco.brivio@cern.ch>
2022-07-06 15:58:04 +0200

    Add VtxSmearing scenario for Run 3 early data at 13.6 TeV

--- IOMC/EventVertexGenerators/python/VtxSmearedParameters_cfi.py
+++ IOMC/EventVertexGenerators/python/VtxSmearedParameters_cfi.py
@@ -780,6 +780,38 @@ Realistic25ns900GeV2021CollisionVtxSmearingParameters = cms.PSet(
     Z0 = cms.double(0.423776)
 )
 
+# From first Run 3 data at 13.6 TeV and 3.8T
+# BS parameters extracted from run 355100, fill 7920:
+# X0         =  0.172394 [cm]
+# Y0         = -0.180946 [cm]
+# Z0         =  0.94181  [cm]
+# sigmaZ0    =  3.81941  [cm]
+# BeamWidthX = 0.0008772 [cm]
+# BeamWidthY = 0.0010078 [cm]
+#
+# set SigmaZ0 = 3.8 [cm]
+# set BeamWidthX = BeamWidthY = 10.0 [um]
+# set beta* = 30 cm
+# energy = 13.6 TeV
+# From LHC calculator, emittance is 6.621e-8 cm
+# https://lpc.web.cern.ch/lumiCalc.html
+#
+# BPIX absolute position (https://twiki.cern.ch/twiki/bin/view/CMS/TkAlignmentPixelPosition?rev=42#2022):
+# X = -0.01955 cm
+# Y = -0.1583  cm
+# Z = -0.2626  cm
+Realistic25ns13p6TeVEarly2022CollisionVtxSmearingParameters = cms.PSet(
+    Phi = cms.double(0.0),
+    BetaStar = cms.double(30.0),
+    Emittance = cms.double(6.621e-8),
+    Alpha = cms.double(0.0),
+    SigmaZ = cms.double(3.8),
+    TimeOffset = cms.double(0.0),
+    X0 = cms.double(0.191944),
+    Y0 = cms.double(-0.022646),
+    Z0 = cms.double(1.20441)
+)
+
 # Test HF offset
 ShiftedCollision2015VtxSmearingParameters = cms.PSet(
     Phi = cms.double(0.0),
