1fa1b71a229aa98736e5f3fa820327c1e9cdf6a2
mmusich <marco.musich@cern.ch>
2022-07-08 12:44:10 +0200

    fix issue with ODR of stAPVGain struct

--- CalibTracker/SiStripChannelGain/src/SiStripGainsPCLHarvester.cc
+++ CalibTracker/SiStripChannelGain/src/SiStripGainsPCLHarvester.cc
@@ -712,6 +712,7 @@ void SiStripGainsPCLHarvester::checkBookAPVColls(const edm::EventSetup& es) {
           APV->Index = Index;
           APV->Bin = -1;
           APV->DetId = Detid.rawId();
+          APV->Side = 0;
           APV->APVId = j;
           APV->SubDet = SubDet;
           APV->FitMPV = -1;
@@ -761,6 +762,7 @@ void SiStripGainsPCLHarvester::checkBookAPVColls(const edm::EventSetup& es) {
             APV->Index = Index;
             APV->Bin = -1;
             APV->DetId = Detid.rawId();
+            APV->Side = 0;
             APV->APVId = (j << 3 | i);
             APV->SubDet = SubDet;
             APV->FitMPV = -1;
