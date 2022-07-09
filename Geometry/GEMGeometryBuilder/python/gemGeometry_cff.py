502aabe2553ecd9c666e0dcc3485a646891be42e
hyunyong <antilogy33@gmail.com>
2022-07-09 10:52:44 +0200

    Run3 GEMGeometryBuilder alignment modifier

--- Geometry/GEMGeometryBuilder/python/gemGeometry_cff.py
+++ Geometry/GEMGeometryBuilder/python/gemGeometry_cff.py
@@ -2,4 +2,8 @@ from Geometry.GEMGeometryBuilder.gemGeometry_cfi import gemGeometry
 
 from Configuration.ProcessModifiers.dd4hep_cff import dd4hep
 
+from Configuration.Eras.Modifier_run3_GEM_cff import run3_GEM
+
 dd4hep.toModify(gemGeometry, fromDDD = False, fromDD4hep = True)
+
+run3_GEM.toModify(gemGeometry, applyAlignment = True)
