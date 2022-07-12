ea2d63a0639cd0306f557dd1eea83bafa35ac077
Norraphat <srimanob@mail.cern.ch>
2022-07-12 11:38:59 +0200

    clean up

--- Configuration/PyReleaseValidation/python/relval_2026.py
+++ Configuration/PyReleaseValidation/python/relval_2026.py
@@ -14,20 +14,6 @@ from Configuration.PyReleaseValidation.relval_upgrade import workflows as _upgra
 
 #2026 WFs to run in IB (TTbar)
 numWFIB = []
-numWFIB.extend([23234.0]) #2026D49
-numWFIB.extend([23434.99,23434.999]) #2026D49 premixing combined stage1+stage2 (ttbar+PU200, ttbar+PU50 for PR test)
-numWFIB.extend([23234.21,23434.21,23434.9921]) #2026D49 prodlike, prodlike PU, prodlike premix stage1+stage2
-numWFIB.extend([28234.0]) #2026D60
-numWFIB.extend([31434.0]) #2026D68
-numWFIB.extend([32234.0]) #2026D70
-numWFIB.extend([34634.0]) #2026D76
-numWFIB.extend([35034.0]) #2026D77
-numWFIB.extend([36234.0]) #2026D80
-numWFIB.extend([36634.0]) #2026D81
-numWFIB.extend([37034.0]) #2026D82
-numWFIB.extend([37434.0]) #2026D83
-numWFIB.extend([37834.0]) #2026D84
-numWFIB.extend([38234.0]) #2026D85
 numWFIB.extend([38634.0]) #2026D86
 numWFIB.extend([39434.0,39434.911,39434.103]) #2026D88 DDD XML, DD4hep XML, aging
 numWFIB.extend([39434.75]) #2026D88 with HLT75e33
