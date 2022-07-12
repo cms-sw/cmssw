e13043199730da3da9719f42024c02294842624d
hyunyong <antilogy33@gmail.com>
2022-07-12 14:08:54 +0200

    applyAlignment = False for phase2

--- Geometry/GEMGeometryBuilder/python/gemGeometryDB_cfi.py
+++ Geometry/GEMGeometryBuilder/python/gemGeometryDB_cfi.py
@@ -12,5 +12,7 @@ GEMGeometryESModule = cms.ESProducer("GEMGeometryESModule",
 )
 
 from Configuration.Eras.Modifier_run3_GEM_cff import run3_GEM
+from Configuration.Eras.Modifier_phase2_GEM_cff import phase2_GEM
 
 run3_GEM.toModify(GEMGeometryESModule, applyAlignment = True)
+phase2_GEM.toModify(GEMGeometryESModule, applyAlignment = False)
