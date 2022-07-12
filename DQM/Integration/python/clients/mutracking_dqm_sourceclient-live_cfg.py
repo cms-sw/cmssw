817fbc4694f0a5c63dd17eeea8d8c18d3e3bcfb1
francescobrivio <francesco.brivio@cern.ch>
2022-07-12 23:07:35 +0200

    Update GT for DQM clients unitTests and input file used for unitTest

--- DQM/Integration/python/clients/mutracking_dqm_sourceclient-live_cfg.py
+++ DQM/Integration/python/clients/mutracking_dqm_sourceclient-live_cfg.py
@@ -99,7 +99,7 @@ if (live):
 elif(offlineTesting):
     process.load('Configuration.StandardSequences.FrontierConditions_GlobalTag_cff')
     from Configuration.AlCa.GlobalTag import GlobalTag as gtCustomise
-    process.GlobalTag = gtCustomise(process.GlobalTag, 'auto:run2_data', '')
+    process.GlobalTag = gtCustomise(process.GlobalTag, 'auto:run3_data', '')
 
 
 
