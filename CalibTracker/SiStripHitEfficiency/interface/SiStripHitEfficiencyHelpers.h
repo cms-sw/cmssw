f558ce9b70ebe1a63f94257c960289625a401ea9
mmusich <marco.musich@cern.ch>
2022-07-21 17:57:16 +0200

    SiStripHitEfficiencyWorker: sanitize histogram names to avoid DQM Offline GUI indexing problem with ALCAPROMPT datasets

--- CalibTracker/SiStripHitEfficiency/interface/SiStripHitEfficiencyHelpers.h
+++ CalibTracker/SiStripHitEfficiency/interface/SiStripHitEfficiencyHelpers.h
@@ -23,6 +23,16 @@ namespace {
     k_END_OF_LAYS_AND_RINGS = 35
   };
 
+  inline void replaceInString(std::string& str, const std::string& from, const std::string& to) {
+    if (from.empty())
+      return;
+    size_t start_pos = 0;
+    while ((start_pos = str.find(from, start_pos)) != std::string::npos) {
+      str.replace(start_pos, from.length(), to);
+      start_pos += to.length();  // In case 'to' contains 'from', like replacing 'x' with 'yx'
+    }
+  }
+
   inline unsigned int checkLayer(unsigned int iidd, const TrackerTopology* tTopo) {
     switch (DetId(iidd).subdetId()) {
       case SiStripSubdetector::TIB:
