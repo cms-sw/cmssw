fbdaf3fab15e40e5a06768312d1f5d79085f92b3
Juliette Alimena <juliette.alimena@cern.ch>
2022-07-21 13:14:59 +0200

    update displaced muon path monitoring

--- HLTriggerOffline/Exotica/python/analyses/hltExoticaDisplacedL2Dimuon_cff.py
+++ HLTriggerOffline/Exotica/python/analyses/hltExoticaDisplacedL2Dimuon_cff.py
@@ -4,7 +4,9 @@ DisplacedL2DimuonPSet = cms.PSet(
     hltPathsToCheck = cms.vstring(
         "HLT_DoubleL2Mu23NoVtx_2Cha_CosmicSeed_v", #Claimed for Run 3
         "HLT_DoubleL2Mu23NoVtx_2Cha_v", #Claimed for Run 3
-        "HLT_DoubleL2Mu10NoVtx_2Cha_PromptL3Mu0Veto_v", #New for Run 3
+        "HLT_DoubleL2Mu10NoVtx_2Cha_VetoL3Mu0DxyMax1cm_v", #New for Run 3 (introduced in HLT V1.3)
+        "HLT_DoubleL2Mu_L3Mu16NoVtx_VetoL3Mu0DxyMax0p1cm_v", #New for Run 3 (introduced in HLT V1.3)
+        "HLT_DoubleL2Mu10NoVtx_2Cha_CosmicSeed_VetoL3Mu0DxyMax1cm_v", #New for Run 3 (introduced in HLT V1.3)
         ),
     recMuonLabel  = cms.InputTag("muons"),
     recMuonTrkLabel  = cms.InputTag("displacedStandAloneMuons"),
