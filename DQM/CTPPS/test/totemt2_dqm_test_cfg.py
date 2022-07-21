e4f1ec05197440fb20e08331cd8041ba95f15fc9
Arkadiusz Cwikla <arkadiusz.cwikla98@gmail.com>
2022-07-21 14:16:54 +0200

    DQM: added plots to nT2 DQM component

--- DQM/CTPPS/test/totemt2_dqm_test_cfg.py
+++ DQM/CTPPS/test/totemt2_dqm_test_cfg.py
@@ -3,7 +3,7 @@ import string
 
 process = cms.Process('RECODQM')
 
-process.maxEvents = cms.untracked.PSet( input = cms.untracked.int32(1000) )
+process.maxEvents = cms.untracked.PSet( input = cms.untracked.int32(6000) )
 process.verbosity = cms.untracked.PSet( input = cms.untracked.int32(-1) )
 
 # minimum of logs
