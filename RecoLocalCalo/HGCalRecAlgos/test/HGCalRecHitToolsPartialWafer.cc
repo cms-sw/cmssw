60c473da196d1b056364a6ac05a36700d3fdc1b4
Sunanda <sunanda.banerjee@cern.ch>
2022-07-21 03:56:05 +0200

    Code check

--- RecoLocalCalo/HGCalRecAlgos/test/HGCalRecHitToolsPartialWafer.cc
+++ RecoLocalCalo/HGCalRecAlgos/test/HGCalRecHitToolsPartialWafer.cc
@@ -60,7 +60,8 @@ HGCalRecHitToolsPartialWafer::HGCalRecHitToolsPartialWafer(const edm::ParameterS
       digiToken_(consumes<HGCalDigiCollection>(source_)),
       recHitToken_(consumes<HGCRecHitCollection>(source_)),
       geomToken_(esConsumes<CaloGeometry, CaloGeometryRecord>()) {
-  edm::LogVerbatim("HGCalSim") << "Test Hit ID using Digi(1)/RecHit(0): " << checkDigi_ << " for " << nameSense_ << " with module Label: " << source_;
+  edm::LogVerbatim("HGCalSim") << "Test Hit ID using Digi(1)/RecHit(0): " << checkDigi_ << " for " << nameSense_
+                               << " with module Label: " << source_;
 }
 
 void HGCalRecHitToolsPartialWafer::fillDescriptions(edm::ConfigurationDescriptions& descriptions) {
@@ -74,7 +75,11 @@ void HGCalRecHitToolsPartialWafer::fillDescriptions(edm::ConfigurationDescriptio
 void HGCalRecHitToolsPartialWafer::analyze(const edm::Event& e, const edm::EventSetup& iS) {
   // get HGCal geometry constant
   const CaloGeometry geo = iS.getData(geomToken_);
-  const HGCalGeometry* geom = (nameSense_ == "HGCalEESensitive") ? static_cast<const HGCalGeometry*>(geo.getSubdetectorGeometry(DetId::HGCalEE, ForwardSubdetector::ForwardEmpty)) : static_cast<const HGCalGeometry*>(geo.getSubdetectorGeometry(DetId::HGCalHSi, ForwardSubdetector::ForwardEmpty));
+  const HGCalGeometry* geom = (nameSense_ == "HGCalEESensitive")
+                                  ? static_cast<const HGCalGeometry*>(
+                                        geo.getSubdetectorGeometry(DetId::HGCalEE, ForwardSubdetector::ForwardEmpty))
+                                  : static_cast<const HGCalGeometry*>(
+                                        geo.getSubdetectorGeometry(DetId::HGCalHSi, ForwardSubdetector::ForwardEmpty));
   //Setup the tool
   tool_.setGeometry(geo);
 
@@ -82,7 +87,8 @@ void HGCalRecHitToolsPartialWafer::analyze(const edm::Event& e, const edm::Event
   if (checkDigi_) {
     const auto& collection = e.getHandle(digiToken_);
     if (collection.isValid()) {
-      edm::LogVerbatim("HGCalSim") << "HGCalRecHitToolsPartialWafer: Finds Digi Collection for " << nameSense_ << " with " << collection->size() << " hits";
+      edm::LogVerbatim("HGCalSim") << "HGCalRecHitToolsPartialWafer: Finds Digi Collection for " << nameSense_
+                                   << " with " << collection->size() << " hits";
       analyze(*(collection.product()), geom);
     } else {
       edm::LogVerbatim("HGCalSim") << "HGCalRecHitToolsPartialWafer: Cannot find Digi collection for " << nameSense_;
@@ -90,7 +96,8 @@ void HGCalRecHitToolsPartialWafer::analyze(const edm::Event& e, const edm::Event
   } else {
     const auto& collection = e.getHandle(recHitToken_);
     if (collection.isValid()) {
-      edm::LogVerbatim("HGCalSim") << "HGCalRecHitToolsPartialWafer: Finds RecHit Collection for " << nameSense_ << " with " << collection->size() << " hits";
+      edm::LogVerbatim("HGCalSim") << "HGCalRecHitToolsPartialWafer: Finds RecHit Collection for " << nameSense_
+                                   << " with " << collection->size() << " hits";
       analyze(*(collection.product()), geom);
     } else {
       edm::LogVerbatim("HGCalSim") << "HGCalRecHitToolsPartialWafer: Cannot find RecHit collection for " << nameSense_;
@@ -114,14 +121,14 @@ void HGCalRecHitToolsPartialWafer::analyze(const T& collection, const HGCalGeome
       const auto& info = hgc.waferInfo(hid.layer(), hid.waferU(), hid.waferV());
       // Only partial wafers
       if (info.part != HGCalTypes::WaferFull) {
-	++good;
-	GlobalPoint pos1 = geom->getPosition(id);
-	GlobalPoint pos2 = tool_.getPosition(id);
-	edm::LogVerbatim("HGCalSim") << "Hit[" << all << ":" << allSi << ":" << good << "]" << HGCSiliconDetId(id)
-				     << " Wafer Type:Part:Orient:Cassette " << info.type << ":" << info.part << ":"
-				     << info.orient << ":" << info.cassette << " at (" << pos1.x() << ", "
-				     << pos1.y() << ", " << pos1.z() << ") or (" << pos2.x() << ", " << pos2.y()
-				     << ", " << pos2.z() << ")";
+        ++good;
+        GlobalPoint pos1 = geom->getPosition(id);
+        GlobalPoint pos2 = tool_.getPosition(id);
+        edm::LogVerbatim("HGCalSim") << "Hit[" << all << ":" << allSi << ":" << good << "]" << HGCSiliconDetId(id)
+                                     << " Wafer Type:Part:Orient:Cassette " << info.type << ":" << info.part << ":"
+                                     << info.orient << ":" << info.cassette << " at (" << pos1.x() << ", " << pos1.y()
+                                     << ", " << pos1.z() << ") or (" << pos2.x() << ", " << pos2.y() << ", " << pos2.z()
+                                     << ")";
       }
     }
   }
