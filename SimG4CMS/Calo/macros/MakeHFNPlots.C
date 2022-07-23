7ee672565d4de83c0c8392557767c7e28e55b8d1
Sunanda <sunanda.banerjee@cern.ch>
2022-07-23 15:00:24 +0200

    Update the validation scripts for HFNose in Validation/HGCalValidation and macros in SimG4CMS/Calo

--- SimG4CMS/Calo/macros/MakeHFNPlots.C
+++ SimG4CMS/Calo/macros/MakeHFNPlots.C
@@ -3,19 +3,23 @@
 // Usage:
 // .L MakeHFNPlots.C+g
 //
-//   To make plot of layers from a file created using .SimHit/Digi/RecHitStudy
+//   To make plot of layers from a file created using SimHit/Digi/RecHit
+//   using the scripts runHFNoseSimHitStudy_cfg.py, runHFNoseDigiStudy_cfg.py,
+//   runHFNoseRecHitStudy_cfg.py in Validation/HGCalValidation
+//
 //     makeLayerPlots(fname, type, todomin, todomax, tag, text, save)
 //
 //   where
-//     fname   std::string   Name of the ROOT file (hfnSimHitD44tt.root)
-//     type    int           File type (0:SimHit; 2:Digi; 3:RecHit)
-//     todomin int           Range of plots to make (minimum: -1
-//     todomax int           maximum: 8)
-//     tag     std::string   To be added to the name of the canvas ("")
-//     text    std::string   To be added to the title of the histogram ("")
-//     save    bool          If the canvas is to be saved as jpg file (false)
+//     fname   std::string   Name of the ROOT file [hfnSimHitD94tt.root]
+//     type    int           File type (0:SimHit; 2:Digi; 3:RecHit) [0]
+//     todomin int           Range of plots to make    [0]
+//     todomax int           (minimum: -1; maximum: 8) [7]
+//     tag     std::string   To be added to the name of the canvas [""]
+//     text    std::string   To be added to the title of the histogram [""]
+//     save    bool          If the canvas is to be saved as jpg file [false]
 //
 //////////////////////////////////////////////////////////////////////////////
+
 #include <TCanvas.h>
 #include <TChain.h>
 #include <TFile.h>
@@ -37,7 +41,7 @@
 #include <string>
 #include <vector>
 
-void makeLayerPlots(std::string fname="hfnSimHitD44tt.root", int type=0,
+void makeLayerPlots(std::string fname="hfnSimHitD94tt.root", int type=0,
 		    int todomin=0, int todomax=7, std::string tag="", 
 		    std::string text="", bool save=false) {
 
