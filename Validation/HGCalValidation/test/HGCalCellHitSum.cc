e034bfbffeb77ab2008bed28f73c592207e13067
Sunanda <sunanda.banerjee@cern.ch>
2022-07-25 12:48:09 +0200

    Code check a

--- Validation/HGCalValidation/test/HGCalCellHitSum.cc
+++ Validation/HGCalValidation/test/HGCalCellHitSum.cc
@@ -320,8 +320,8 @@ HGCalCellHitSum::HGCalCellHitSum(const edm::ParameterSet &iConfig)
   //now do what ever initialization is needed
   usesResource(TFileService::kSharedResource);
   edm::LogVerbatim("ValidHGCal") << "HGCalCellHitSum::Initialize for " << name_ << " using " << geometryFileName_
-                                 << " and collections " << iConfig.getParameter<edm::InputTag>("simtrack") << " and "
-                                 << iConfig.getParameter<edm::InputTag>("simhits");
+                                 << " and collections for simTrack:" << iConfig.getParameter<edm::InputTag>("simtrack") 
+                                 << "; and for hits " << iConfig.getParameter<edm::InputTag>("simhits");
   edm::Service<TFileService> fs;
 
   hCharge = fs->make<TH1D>("charge", "Charges", 200, -20, 20);
