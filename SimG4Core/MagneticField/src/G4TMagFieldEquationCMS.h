2f590c47477e5c3b862f0a5cea39fda0777268f9
Vincenzo Innocente <vincenzo.innocente@cern.ch>
2022-07-25 11:29:23 +0200

    inline field, cleanup

--- SimG4Core/MagneticField/src/G4TMagFieldEquationCMS.h
+++ SimG4Core/MagneticField/src/G4TMagFieldEquationCMS.h
@@ -35,66 +35,54 @@
 // --------------------------------------------------------------------
 // Created: Josh Xie  (Google Summer of Code 2014 )
 // Adapted from G4Mag_UsualEqRhs
-// 
-// #include "G4ChargeState.hh"
+//
+// Modified by Vincenzo innocente 7/2022 to adapt to specific CMS field
+//
+
 #include "G4Mag_UsualEqRhs.hh"
+#include <cassert>
 
-template 
-<class T_Field>
-class G4TMagFieldEquationCMS final : public G4Mag_UsualEqRhs
-{
-  public:
+template <class T_Field>
+class G4TMagFieldEquationCMS final : public G4Mag_UsualEqRhs {
+public:
+  G4TMagFieldEquationCMS(T_Field* f) : G4Mag_UsualEqRhs(f) {
+    assert(f);
+    itsField = f;
+  }
 
-    G4TMagFieldEquationCMS(T_Field* f)
-       : G4Mag_UsualEqRhs(f)
-    {
-            itsField = f;
-    }
+  virtual ~G4TMagFieldEquationCMS() { ; }
 
-    virtual ~G4TMagFieldEquationCMS(){;}
+  inline void GetFieldValueCMS(const G4double Point[], G4double Field[]) const {
+    itsField->GetFieldValue(Point, Field);
+  }
 
-    inline void GetFieldValueCMS(const G4double Point[4],
-                              G4double Field[]) const
-    {
-      itsField->T_Field::GetFieldValue(Point, Field);
-    }
+  inline void TEvaluateRhsGivenB(const G4double y[],
+                                 G4double inv_momentum_magnitude,
+                                 const G4double B[3],
+                                 G4double dydx[]) const {
+    G4double cof = FCof() * inv_momentum_magnitude;
 
-    inline void TEvaluateRhsGivenB( const G4double y[], G4double invMon,
-                                    const G4double B[3],
-                                    G4double dydx[] ) const
-    {
-      G4double inv_momentum_magnitude = invMon;
-      G4double cof = FCof()*inv_momentum_magnitude;
-      
-      dydx[0] = y[3]*inv_momentum_magnitude;       //  (d/ds)x = Vx/V
-      dydx[1] = y[4]*inv_momentum_magnitude;       //  (d/ds)y = Vy/V
-      dydx[2] = y[5]*inv_momentum_magnitude;       //  (d/ds)z = Vz/V
-      
-      dydx[3] = cof*(y[4]*B[2] - y[5]*B[1]) ;  // Ax = a*(Vy*Bz - Vz*By)
-      dydx[4] = cof*(y[5]*B[0] - y[3]*B[2]) ;  // Ay = a*(Vz*Bx - Vx*Bz)
-      dydx[5] = cof*(y[3]*B[1] - y[4]*B[0]) ;  // Az = a*(Vx*By - Vy*Bx)
-      
-      return ;
-    }
+    dydx[0] = y[3] * inv_momentum_magnitude;  //  (d/ds)x = Vx/V
+    dydx[1] = y[4] * inv_momentum_magnitude;  //  (d/ds)y = Vy/V
+    dydx[2] = y[5] * inv_momentum_magnitude;  //  (d/ds)z = Vz/V
 
-    __attribute__((always_inline)) 
-    void TRightHandSide(const G4double y[], G4double invMon, G4double dydx[] )
-    	const
-    {
-      G4double Field[G4maximum_number_of_field_components]; 
-      G4double  PositionAndTime[4];
-      PositionAndTime[0] = y[0];
-      PositionAndTime[1] = y[1];
-      PositionAndTime[2] = y[2];
-      PositionAndTime[3] = y[7];   
-      GetFieldValue(PositionAndTime, Field) ;
-      TEvaluateRhsGivenB(y, invMon, Field, dydx);
-    }
-   
+    dydx[3] = cof * (y[4] * B[2] - y[5] * B[1]);  // Ax = a*(Vy*Bz - Vz*By)
+    dydx[4] = cof * (y[5] * B[0] - y[3] * B[2]);  // Ay = a*(Vz*Bx - Vx*Bz)
+    dydx[5] = cof * (y[3] * B[1] - y[4] * B[0]);  // Az = a*(Vx*By - Vy*Bx)
+
+    return;
+  }
+
+  __attribute__((always_inline)) void TRightHandSide(const G4double y[],
+                                                     G4double inv_momentum_magnitude,
+                                                     G4double dydx[]) const {
+    // CMS field  is three dimentional
+    G4double Field[3];
+    GetFieldValueCMS(y, Field);
+    TEvaluateRhsGivenB(y, inv_momentum_magnitude, Field, dydx);
+  }
+
 private:
-  enum { G4maximum_number_of_field_components = 24 };
-
   // Dependent objects
-  T_Field *itsField;
+  T_Field* itsField;
 };
-
