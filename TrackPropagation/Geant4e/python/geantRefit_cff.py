126863879169bbf6bd1612c4d31061cb9fc49eec
Vincenzo Innocente <vincenzo.innocente@cern.ch>
2022-07-27 17:52:45 +0200

    import MagneticField from g4SimHits

--- TrackPropagation/Geant4e/python/geantRefit_cff.py
+++ TrackPropagation/Geant4e/python/geantRefit_cff.py
@@ -6,48 +6,20 @@ from TrackPropagation.Geant4e.Geant4ePropagator_cfi import *
 
 from TrackingTools.TrackRefitter.TracksToTrajectories_cff import *
 
+from SimG4Core.Application.g4SimHits_cfi import g4SimHits as _g4SimHits
+
+
 ## Set up geometry
 geopro = cms.EDProducer("GeometryProducer",
-     GeoFromDD4hep = cms.bool(True),
+     GeoFromDD4hep = cms.bool(False),
      UseMagneticField = cms.bool(True),
      UseSensitiveDetectors = cms.bool(False),
-     MagneticField = cms.PSet(
-        UseLocalMagFieldManager = cms.bool(False),
-        Verbosity = cms.bool(False),
-        ConfGlobalMFM = cms.PSet(
-            Volume = cms.string('OCMS'),
-            OCMS = cms.PSet(
-                Stepper = cms.string('G4TDormandPrince45'),
-                Type = cms.string('CMSIMField'),
-                StepperParam = cms.PSet(
-                    VacRegions = cms.vstring(),
-#                   VacRegions = cms.vstring('DefaultRegionForTheWorld','BeamPipeVacuum','BeamPipeOutside'),
-                    EnergyThTracker = cms.double(0.2),     ## in GeV
-                    RmaxTracker = cms.double(8000),       ## in mm
-                    ZmaxTracker = cms.double(11000),       ## in mm
-                    MaximumEpsilonStep = cms.untracked.double(0.01),
-                    DeltaOneStep = cms.double(0.001),      ## in mm
-                    DeltaOneStepTracker = cms.double(1e-4),## in mm
-                    MaximumLoopCounts = cms.untracked.double(1000.0),
-                    DeltaChord = cms.double(0.002),       ## in mm
-                    DeltaChordTracker = cms.double(0.001), ## in mm
-                    MinStep = cms.double(0.1),             ## in mm
-                    DeltaIntersectionAndOneStep = cms.untracked.double(-1.0),
-                    DeltaIntersection = cms.double(0.0001),     ## in mm
-                    DeltaIntersectionTracker = cms.double(1e-6),## in mm
-                    MaxStep = cms.double(150.),            ## in cm
-                    MinimumEpsilonStep = cms.untracked.double(1e-05),
-                    EnergyThSimple = cms.double(0.015),    ## in GeV
-                    DeltaChordSimple = cms.double(0.1),    ## in mm
-                    DeltaOneStepSimple = cms.double(0.1),  ## in mm
-                    DeltaIntersectionSimple = cms.double(0.01), ## in mm
-                    MaxStepSimple = cms.double(50.),       ## in cm
-                )
-            )
-    ),
-    delta = cms.double(1.0)
-    ),
-   )
+     MagneticField =  _g4SimHits.MagneticField.clone()
+)
+
+from Configuration.ProcessModifiers.dd4hep_cff import dd4hep
+dd4hep.toModify(geopro, GeoFromDD4hep = True )
+
 
 # load this to do a track refit
 from RecoTracker.TrackProducer.TrackRefitters_cff import *
