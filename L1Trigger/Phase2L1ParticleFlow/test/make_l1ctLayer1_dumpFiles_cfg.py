277eca7f3c9eefddac1b28580ff1476935bd48d7
ccaillol <ccaillol@cern.ch>
2022-07-28 15:06:11 +0200

    L1T correlator developments phase-2
    (cherry picked from commit f51be456f21461c0a6f4d4c4365bf830e154ebfd)

--- L1Trigger/Phase2L1ParticleFlow/test/make_l1ctLayer1_dumpFiles_cfg.py
+++ L1Trigger/Phase2L1ParticleFlow/test/make_l1ctLayer1_dumpFiles_cfg.py
@@ -1,8 +1,6 @@
 import FWCore.ParameterSet.Config as cms
 from Configuration.StandardSequences.Eras import eras
 
-produceEGStage2Pattern = False
-
 process = cms.Process("RESP", eras.Phase2C9)
 
 process.load('Configuration.StandardSequences.Services_cff')
@@ -17,7 +15,8 @@ process.source = cms.Source("PoolSource",
     inputCommands = cms.untracked.vstring("keep *", 
             "drop l1tPFClusters_*_*_*",
             "drop l1tPFTracks_*_*_*",
-            "drop l1tPFCandidates_*_*_*")
+            "drop l1tPFCandidates_*_*_*",
+            "drop l1tTkPrimaryVertexs_*_*_*")
 )
 
 process.load('Configuration.Geometry.GeometryExtended2026D49Reco_cff')
@@ -29,7 +28,6 @@ process.load('Configuration.StandardSequences.FrontierConditions_GlobalTag_cff')
 from Configuration.AlCa.GlobalTag import GlobalTag
 process.GlobalTag = GlobalTag(process.GlobalTag, '123X_mcRun4_realistic_v3', '')
 
-process.load("L1Trigger.Phase2L1ParticleFlow.l1ParticleFlow_cff")
 process.load('L1Trigger.Phase2L1ParticleFlow.l1ctLayer1_cff')
 process.load('L1Trigger.Phase2L1ParticleFlow.l1ctLayer2EG_cff')
 process.load('L1Trigger.L1TTrackMatch.l1tGTTInputProducer_cfi')
@@ -41,39 +39,33 @@ from L1Trigger.Phase2L1GMT.gmt_cfi import l1tStandaloneMuons
 process.l1tSAMuonsGmt = l1tStandaloneMuons.clone()
 
 process.l1tLayer1Barrel9 = process.l1tLayer1Barrel.clone()
+process.l1tLayer1Barrel9.puAlgo.nFinalSort = 32
 process.l1tLayer1Barrel9.regions[0].etaBoundaries = [ -1.5, -0.5, 0.5, 1.5 ] 
 process.l1tLayer1Barrel9.boards=cms.VPSet(
         cms.PSet(
-            regions=cms.vuint32(list(range(0, 3)) + [x+9 for x in range(0, 3)] + [x+18 for x in range(0, 3)])),
+            regions=cms.vuint32(*[0+9*ie+i for ie in range(3) for i in range(3)])),
         cms.PSet(
-            regions=cms.vuint32(list(range(3, 6)) + [x+9 for x in range(3, 6)] + [x+18 for x in range(3, 6)])),
+            regions=cms.vuint32(*[3+9*ie+i for ie in range(3) for i in range(3)])),
         cms.PSet(
-            regions=cms.vuint32(list(range(6, 9)) + [x+9 for x in range(6, 9)] + [x+18 for x in range(6, 9)])),
+            regions=cms.vuint32(*[6+9*ie+i for ie in range(3) for i in range(3)])),
     )
 
 process.runPF = cms.Path( 
         process.l1tSAMuonsGmt +
         process.l1tGTTInputProducer +
         process.l1tVertexFinderEmulator +
-        process.l1tPFTracksFromL1Tracks +
-        process.l1tParticleFlow_calo +
         process.l1tLayer1Barrel +
         process.l1tLayer1Barrel9 +
         process.l1tLayer1HGCal +
         process.l1tLayer1HGCalNoTK +
-        process.l1tLayer1HF +
-        process.l1tLayer1 +
-        process.l1tLayer2EG
-    )
-
-if produceEGStage2Pattern:
-    process.l1tLayer2EG.writeInPattern = True
-    process.l1tLayer2EG.writeOutPattern = True
-
-process.source.fileNames  = [ '/store/cmst3/group/l1tr/gpetrucc/11_1_0/NewInputs110X/110121.done/TTbar_PU200/inputs110X_%d.root' % i for i in (1,3,7,8,9) ]
-process.l1tPFClustersFromCombinedCaloHCal.phase2barrelCaloTowers = [cms.InputTag("l1tEGammaClusterEmuProducer",)]
+        process.l1tLayer1HF
+)
+process.runPF.associate(process.l1tLayer1TaskInputsTask)
 
 
 for det in "Barrel", "Barrel9", "HGCal", "HGCalNoTK", "HF":
-    l1pf = getattr(process, 'l1tLayer1'+det)
-    l1pf.dumpFileName = cms.untracked.string("TTbar_PU200_110X_"+det+".dump")
+    l1pf = getattr(process, 'l1ctLayer1'+det)
+    l1pf.dumpFileName = cms.untracked.string("TTbar_PU200_"+det+".dump")
+
+process.source.fileNames  = [ '/store/cmst3/group/l1tr/gpetrucc/11_1_0/NewInputs110X/110121.done/TTbar_PU200/inputs110X_%d.root' % i for i in (1,3,7,8,9) ]
+process.l1tPFClustersFromCombinedCaloHCal.phase2barrelCaloTowers = [cms.InputTag("l1tEGammaClusterEmuProducer",)]
