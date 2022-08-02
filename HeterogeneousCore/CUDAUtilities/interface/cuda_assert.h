12f084c06440b97baae71de558eb0528950dee0d
Andrea Bocci <andrea.bocci@cern.ch>
2022-08-02 23:10:18 +0200

    Use __trap() instead of assert() in production
    In GPU code in production builds (when GPU_DEBUG is not defined) compile
    a call to assert(condition) as __trap(not condition) instead of disabling
    it completely.

--- HeterogeneousCore/CUDAUtilities/interface/cuda_assert.h
+++ HeterogeneousCore/CUDAUtilities/interface/cuda_assert.h
@@ -3,16 +3,35 @@
 
 #ifdef __CUDA_ARCH__
 #ifndef GPU_DEBUG
+
 // disable asserts
 #ifndef NDEBUG
 #define NDEBUG
 #endif
-#else
+
+#else  // GPU_DEBUG
+
 // enable asserts
 #ifdef NDEBUG
 #undef NDEBUG
 #endif
-#endif
+
+#endif  // GPU_DEBUG
 #endif  // __CUDA_ARCH__
 
 #include <cassert>
+
+#ifdef __CUDA_ARCH__
+#ifndef GPU_DEBUG
+
+// replace the no-op assert() with a check and a __trap() instruction
+#undef assert
+#define assert(expr) \
+  do                 \
+    if (not(expr)) { \
+      __trap();      \
+    }                \
+  while (false)
+
+#endif  // GPU_DEBUG
+#endif  // __CUDA_ARCH__
