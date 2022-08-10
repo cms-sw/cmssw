f01494ea86da3563c3cb1ec3d84d6687698ae016
Aaron Bundock <aaron.bundock@cern.ch>
2022-08-10 14:36:04 +0200

    L1T: Add v0_4 with updated JEC, fix to LUT conversion script

--- L1Trigger/Configuration/python/customiseSettings.py
+++ L1Trigger/Configuration/python/customiseSettings.py
@@ -2,6 +2,14 @@ from __future__ import print_function
 import os.path
 import FWCore.ParameterSet.Config as cms
 
+def L1TSettingsToCaloParamsHI_2022_v0_4(process):
+    process.load("L1Trigger.L1TCalorimeter.caloParamsHI_2022_v0_4_cfi")
+    return process
+
+def L1TSettingsToCaloParams_2022_v0_4(process):
+    process.load("L1Trigger.L1TCalorimeter.caloParams_2022_v0_4_cfi")
+    return process
+
 def L1TSettingsToCaloParamsHI_2022_v0_3(process):
     process.load("L1Trigger.L1TCalorimeter.caloParamsHI_2022_v0_3_cfi")
     return process
