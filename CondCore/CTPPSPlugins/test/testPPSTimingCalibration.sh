1f09d43ffe080ead48ef6a976dd7f527420d7555
Aleksandra Krzeminska <olakrzemka@gmail.com>
2022-08-10 18:19:14 +0200

    Add remaining plots for PPSTimingCalibration for Payload Inspector

--- CondCore/CTPPSPlugins/test/testPPSTimingCalibration.sh
+++ CondCore/CTPPSPlugins/test/testPPSTimingCalibration.sh
@@ -10,16 +10,25 @@ then
 
     echo "Testing history plots"
 
-    for param in 0 1 2 3
+    for db in 0 1
     do
-        getPayloadData.py \
-            --plugin pluginPPSTimingCalibration_PayloadInspector \
-            --plot plot_PPSTimingCalibration_history_htdc_calibration_param$param \
-            --tag PPSDiamondTimingCalibration_v1 \
-            --time_type Run \
-            --iovs '{"start_iov": "294645", "end_iov": "325176"}' \
-            --db Prod \
-            --test 2> CondCore/CTPPSPlugins/test/results/data_history_$param.json
+        for pl in 0 1 2 3
+        do
+            for ch in 0 1 2 3 4 5 6 7 8 9 10 11
+            do
+                for param in 0 1 2 3
+                do
+                    python3 CondCore/Utilities/scripts/getPayloadData.py \
+                            --plugin pluginPPSTimingCalibration_PayloadInspector \
+                            --plot plot_PPSTimingCalibration_history_htdc_calibration_db${db}pl${pl}ch${ch}_param${param} \
+                            --tag CTPPPSTimingCalibration_HPTDC_byPCL_v0_prompt \
+                            --time_type Run \
+                            --iovs '{"start_iov": "355892", "end_iov": "357079"}' \
+                            --db Prod \
+                            --test 2> CondCore/CTPPSPlugins/test/results/data_history__db${db}pl${pl}ch${ch}_param${param}.json
+                done
+            done
+        done                    
     done 
 
 
@@ -33,12 +42,12 @@ then
             then
                 getPayloadData.py \
                     --plugin pluginPPSTimingCalibration_PayloadInspector \
-                    --plot  plot_PPSTimingCalibration_htdc_calibration_params$param1$param2 \
-                    --tag PPSDiamondTimingCalibration_v1 \
+                    --plot  plot_PPSTimingCalibration_htdc_calibration_params${param1}${param2} \
+                    --tag CTPPPSTimingCalibration_HPTDC_byPCL_v0_prompt \
                     --time_type Run \
-                    --iovs '{"start_iov": "294645", "end_iov": "325176"}' \
+                    --iovs '{"start_iov": "355892", "end_iov": "357079"}' \
                     --db Prod \
-                    --test 2> CondCore/CTPPSPlugins/test/results/data_params_$param1$param2.json 
+                    --test 2> CondCore/CTPPSPlugins/test/results/data_params_${param1}${param2}.json 
             fi  
         done
     done    
@@ -47,16 +56,26 @@ then
 
     echo "Testing channel plots"
 
-    getPayloadData.py \
-        --plugin pluginPPSTimingCalibration_PayloadInspector \
-        --plot plot_PPSTimingCalibration_htdc_calibration_param3 \
-        --tag PPSDiamondTimingCalibration_v1 \
-        --time_type Run \
-        --iovs '{"start_iov": "294645", "end_iov": "325176"}' \
-        --db Prod \
-        --test
 
-    mv *.png CondCore/CTPPSPlugins/test/results/plot_PPSTimingCalibration_ppc.png
+    for db in 0 1
+    do
+        for pl in 0 1 2 3
+        do
+            for param in 0 1 2 3
+            do
+                getPayloadData.py \
+                    --plugin pluginPPSTimingCalibration_PayloadInspector \
+                    --plot plot_PPSTimingCalibration_htdc_calibration_db${db}pl${pl}param${param}_per_channels \
+                    --tag CTPPPSTimingCalibration_HPTDC_byPCL_v0_prompt \
+                    --time_type Run \
+                    --iovs '{"start_iov": "356489", "end_iov": "356489"}' \
+                    --db Prod \
+                    --test
+                mv *.png CondCore/CTPPSPlugins/test/results/plot_PPSTimingCalibration_db${db}pl${pl}param${param}_per_channels.png    
+            done
+        done                    
+    done 
+
 
 elif [ "$1" == "clear" ]
 then
