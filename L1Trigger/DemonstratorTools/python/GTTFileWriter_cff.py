4535bd793e9f23ba6bdd1c11c4f5eb5b7122d0a5
Alexx Perloff <Alexx.Perloff@Colorado.edu>
2022-08-14 02:05:24 -0500

    Create output files for the links from the GTT to the GT. These files will have 4 links, one each for jets/sums, taus, light mesons, and isolated tracks/vertices.
    (cherry picked from commit 0b4991f8a68581e1a598c1383e03692eef55a484)

--- L1Trigger/DemonstratorTools/python/GTTFileWriter_cff.py
+++ L1Trigger/DemonstratorTools/python/GTTFileWriter_cff.py
@@ -2,10 +2,14 @@ import FWCore.ParameterSet.Config as cms
 
 GTTFileWriter = cms.EDAnalyzer('GTTFileWriter',
   tracks = cms.untracked.InputTag("l1tTTTracksFromTrackletEmulation", "Level1TTTracks"),
-  convertedTracks = cms.untracked.InputTag("l1tGTTInputProducer","Level1TTTracksConverted"),
+  convertedTracks = cms.untracked.InputTag("l1tGTTInputProducer", "Level1TTTracksConverted"),
   vertices = cms.untracked.InputTag("l1tVertexProducer", "l1verticesEmulation"),
+  jets = cms.untracked.InputTag("l1tTrackJetsEmulation","L1TrackJets"),
+  htmiss = cms.untracked.InputTag("l1tTrackerEmuHTMiss", "L1TrackerEmuHTMiss"),
+  etmiss = cms.untracked.InputTag("l1tTrackerEmuEtMiss", "L1TrackerEmuEtMiss"),
   inputFilename = cms.untracked.string("L1GTTInputFile"),
   inputConvertedFilename = cms.untracked.string("L1GTTInputConvertedFile"),
-  outputFilename = cms.untracked.string("L1GTTOutputToCorrelatorFile"),
+  outputCorrelatorFilename = cms.untracked.string("L1GTTOutputToCorrelatorFile"),
+  outputGlobalTriggerFilename = cms.untracked.string("L1GTTOutputToGlobalTriggerFile"),
   format = cms.untracked.string("APx")
 )
