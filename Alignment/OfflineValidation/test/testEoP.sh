0ea34e981c79f86e775bc0b10821048188a420da
mmusich <marco.musich@cern.ch>
2022-08-15 15:58:40 +0200

    add G4e in alignment refitting test

--- Alignment/OfflineValidation/test/testEoP.sh
+++ Alignment/OfflineValidation/test/testEoP.sh
@@ -1,12 +1,6 @@
 #! /bin/bash
 function die { echo $1: status $2 ; exit $2; }
 
-echo "TESTING DiMuonVertexValidation ..."
-cmsRun ${LOCAL_TEST_DIR}/DiMuonVertexValidation_cfg.py maxEvents=10 || die "Failure running DiMuonVertexValidation_cfg.py" $?
-
-echo "TESTING inspectData ..."
-cmsRun ${LOCAL_TEST_DIR}/inspectData_cfg.py unitTest=True || die "Failure running inspectData_cfg.py" $?
-
 echo "TESTING eopTreeWriter (Pion Analysis) ..."
 cmsRun ${LOCAL_TEST_DIR}/eopTreeWriter_cfg.py unitTest=True maxEvents=10 || die "Failure running eopTreeWriter" $?
 
