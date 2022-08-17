6340733ec53f32ac93e107a2f331c305fd15570b
mmusich <marco.musich@cern.ch>
2022-08-17 21:41:07 +0200

    introduce DiMuonMassBiasClient

---
+++ DQMOffline/Alignment/interface/DiMuonMassBiasClient.h
@@ -0,0 +1,98 @@
+#ifndef DQMOffline_Alignment_DiMuonMassBiasClient_h
+#define DQMOffline_Alignment_DiMuonMassBiasClient_h
+// -*- C++ -*-
+//
+// Package:    DQMOffline/Alignment
+// Class  :    DiMuonMassBiasClient
+//
+// DQM class to plot di-muon mass bias in different kinematics bins
+
+// system includes
+#include <string>
+
+// user includes
+#include "DQMServices/Core/interface/DQMEDHarvester.h"
+#include "DQMServices/Core/interface/DQMStore.h"
+#include "DataFormats/GeometryCommonDetAlgo/interface/Measurement1D.h"
+#include "FWCore/Framework/interface/ESHandle.h"
+#include "FWCore/Framework/interface/EventSetup.h"
+#include "FWCore/Framework/interface/LuminosityBlock.h"
+#include "FWCore/Framework/interface/MakerMacros.h"
+#include "FWCore/Framework/interface/Run.h"
+#include "FWCore/ParameterSet/interface/ParameterSet.h"
+
+namespace diMuonMassBias {
+
+  struct fitOutputs {
+  public:
+    fitOutputs(const Measurement1D& bias, const Measurement1D& width) : m_bias(bias), m_width(width) {}
+
+    // getters
+    const Measurement1D getBias() { return m_bias; }
+    const Measurement1D getWidth() { return m_width; }
+
+  private:
+    Measurement1D m_bias;
+    Measurement1D m_width;
+  };
+
+  // helper functions to fill arrays from vectors
+  inline void fillArrayF(float* x, const edm::ParameterSet& cfg, const char* name) {
+    auto v = cfg.getParameter<std::vector<double>>(name);
+    assert(v.size() == 3);
+    std::copy(std::begin(v), std::end(v), x);
+  }
+
+  inline void fillArrayI(int* x, const edm::ParameterSet& cfg, const char* name) {
+    auto v = cfg.getParameter<std::vector<int>>(name);
+    assert(v.size() == 3);
+    std::copy(std::begin(v), std::end(v), x);
+  }
+}  // namespace diMuonMassBias
+
+class DiMuonMassBiasClient : public DQMEDHarvester {
+public:
+  /// Constructor
+  DiMuonMassBiasClient(const edm::ParameterSet& ps);
+
+  /// Destructor
+  ~DiMuonMassBiasClient() override;
+
+  static void fillDescriptions(edm::ConfigurationDescriptions& descriptions);
+
+protected:
+  /// BeginJob
+  void beginJob(void) override;
+
+  /// BeginRun
+  void beginRun(edm::Run const& run, edm::EventSetup const& eSetup) override;
+
+  /// EndJob
+  void dqmEndJob(DQMStore::IBooker& ibooker_, DQMStore::IGetter& igetter_) override;
+
+private:
+  /// book MEs
+  void bookMEs(DQMStore::IBooker& ibooker);
+  void getMEsToHarvest(DQMStore::IGetter& igetter);
+  diMuonMassBias::fitOutputs fitVoigt(TH1* hist, const bool& fitBackground = false) const;
+
+  // data members
+  const std::string TopFolder_;
+  const bool fitBackground_;
+  const bool debugMode_;
+
+  float meanConfig_[3];  /* parmaeters for the fit: mean */
+  float widthConfig_[3]; /* parameters for the fit: width */
+  float sigmaConfig_[3]; /* parmaeters for the fit: sigma */
+
+  // list of histograms to harvest
+  std::vector<std::string> MEtoHarvest_;
+
+  // the histograms to be filled
+  std::map<std::string, MonitorElement*> meanProfiles_;
+  std::map<std::string, MonitorElement*> widthProfiles_;
+
+  // the histograms than need to be fit and displays
+  std::map<std::string, MonitorElement*> harvestTargets_;
+};
+#endif
