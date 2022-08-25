0e3e268a52636360d11704f62a21cb4c5ee8fcc9
abaty <abaty@rice.edu>
2022-08-25 18:03:53 +0200

    approximation of barycenter in approx clusters to a 10th of a strip

--- DataFormats/SiStripCluster/interface/SiStripApproximateCluster.h
+++ DataFormats/SiStripCluster/interface/SiStripApproximateCluster.h
@@ -8,7 +8,10 @@ class SiStripApproximateCluster {
 public:
   SiStripApproximateCluster() {}
 
-  explicit SiStripApproximateCluster(float barycenter, uint8_t width, float avgCharge, bool isSaturated) {
+  explicit SiStripApproximateCluster(cms_uint16_t barycenter,
+                                     cms_uint8_t width,
+                                     cms_uint8_t avgCharge,
+                                     bool isSaturated) {
     barycenter_ = barycenter;
     width_ = width;
     avgCharge_ = avgCharge;
@@ -17,15 +20,15 @@ public:
 
   explicit SiStripApproximateCluster(const SiStripCluster& cluster, unsigned int maxNSat);
 
-  float barycenter() const { return barycenter_; }
-  uint8_t width() const { return width_; }
-  float avgCharge() const { return avgCharge_; }
+  cms_uint16_t barycenter() const { return barycenter_; }
+  cms_uint8_t width() const { return width_; }
+  cms_uint8_t avgCharge() const { return avgCharge_; }
   bool isSaturated() const { return isSaturated_; }
 
 private:
-  float barycenter_ = 0;
-  uint8_t width_ = 0;
-  float avgCharge_ = 0;
+  cms_uint16_t barycenter_ = 0;
+  cms_uint8_t width_ = 0;
+  cms_uint8_t avgCharge_ = 0;
   bool isSaturated_ = false;
 };
 #endif  // DataFormats_SiStripCluster_SiStripApproximateCluster_h
