9d3f0c13f596b432b0f056e367c629da76d84b7b
Marino Missiroli <marino.missiroli@cern.ch>
2022-08-26 18:16:00 +0200

    deprecate integer types for L1T and HLT prescales in HLTConfigProvider and its clients

--- DataFormats/PatCandidates/src/TriggerPath.cc
+++ DataFormats/PatCandidates/src/TriggerPath.cc
@@ -1,35 +1,30 @@
-//
-//
-
 #include "DataFormats/PatCandidates/interface/TriggerPath.h"
-
-using namespace pat;
 
 // Constructors and Destructor
 
 // Default constructor
-TriggerPath::TriggerPath()
+pat::TriggerPath::TriggerPath()
     : name_(), index_(), prescale_(), run_(), accept_(), error_(), lastActiveFilterSlot_(), l3Filters_(0) {
   modules_.clear();
   filterIndices_.clear();
 }
 
 // Constructor from path name only
-TriggerPath::TriggerPath(const std::string& name)
+pat::TriggerPath::TriggerPath(const std::string& name)
     : name_(name), index_(), prescale_(), run_(), accept_(), error_(), lastActiveFilterSlot_(), l3Filters_(0) {
   modules_.clear();
   filterIndices_.clear();
 }
 
 // Constructor from values
-TriggerPath::TriggerPath(const std::string& name,
-                         unsigned index,
-                         unsigned prescale,
-                         bool run,
-                         bool accept,
-                         bool error,
-                         unsigned lastActiveFilterSlot,
-                         unsigned l3Filters)
+pat::TriggerPath::TriggerPath(const std::string& name,
+                              unsigned index,
+                              double prescale,
+                              bool run,
+                              bool accept,
+                              bool error,
+                              unsigned lastActiveFilterSlot,
+                              unsigned l3Filters)
     : name_(name),
       index_(index),
       prescale_(prescale),
@@ -45,14 +40,14 @@ TriggerPath::TriggerPath(const std::string& name,
 // Methods
 
 // Get the index of a certain module
-int TriggerPath::indexModule(const std::string& name) const {
+int pat::TriggerPath::indexModule(const std::string& name) const {
   if (modules_.begin() == modules_.end())
     return -1;
   return (std::find(modules_.begin(), modules_.end(), name) - modules_.begin());
 }
 
 // Get names of all L1 seeds with a certain decision
-std::vector<std::string> TriggerPath::l1Seeds(const bool decision) const {
+std::vector<std::string> pat::TriggerPath::l1Seeds(const bool decision) const {
   std::vector<std::string> seeds;
   for (L1SeedCollection::const_iterator iSeed = l1Seeds().begin(); iSeed != l1Seeds().end(); ++iSeed) {
     if (iSeed->first == decision)
