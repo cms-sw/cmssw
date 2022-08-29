9d604bad8ec39c2722d7f8473f84d5c7c5c1f5d9
4quarks <47900195+4quarks@users.noreply.github.com>
2022-08-29 17:33:45 +0200

    Anonymous namespace and string to unique_ptr

--- Utilities/XrdAdaptor/src/XrdRequestManager.cc
+++ Utilities/XrdAdaptor/src/XrdRequestManager.cc
@@ -121,48 +121,45 @@ static void SendMonitoringInfo(XrdCl::File &file) {
   }
 }
 
-std::string *GetQueryTransport(const XrdCl::URL &url, uint16_t query) {
-  XrdCl::AnyObject result;
-  XrdCl::DefaultEnv::GetPostMaster()->QueryTransport(url, query, result);
-  std::string *method;
-  result.Get(method);
-  return method;
-}
+namespace{ 
+    std::unique_ptr<std::string> getQueryTransport(const XrdCl::URL &url, uint16_t query) {
+      XrdCl::AnyObject result;
+      XrdCl::DefaultEnv::GetPostMaster()->QueryTransport(url, query, result);
+      std::unique_ptr<std::string>method;
+      result.Get(method.get());
+      return method;
+    }
 
-static void TracerouteRedirections(const XrdCl::HostList *hostList) {
-  int idx_redirection = 1;
-  std::string traceroute_str = "-------------------------------\nTraceroute:\n";
-  for (auto itr = hostList->begin(); itr != hostList->end(); ++itr) {
-    // Query info
-    std::string *stack_ip_method = GetQueryTransport(itr->url, XrdCl::StreamQuery::IpStack);
-    std::string *ip_method = GetQueryTransport(itr->url, XrdCl::StreamQuery::IpAddr);
-    std::string *auth_method = GetQueryTransport(itr->url, XrdCl::TransportQuery::Auth);
-    std::string *hostname_method = GetQueryTransport(itr->url, XrdCl::StreamQuery::HostName);
+  void tracerouteRedirections(const XrdCl::HostList *hostList) {
+    edm::LogInfo("XrdAdaptorLvl2").log([hostList](auto& li) {
+      int idx_redirection = 1;
+      std::string all_redirections;
+      for (auto const& host : *hostList) {
+        // Query info
+        std::unique_ptr<std::string> stack_ip_method = getQueryTransport(host.url, XrdCl::StreamQuery::IpStack);
+        std::unique_ptr<std::string> ip_method = getQueryTransport(host.url, XrdCl::StreamQuery::IpAddr);
+        std::unique_ptr<std::string> auth_method = getQueryTransport(host.url, XrdCl::TransportQuery::Auth);
+        std::unique_ptr<std::string> hostname_method = getQueryTransport(host.url, XrdCl::StreamQuery::HostName);
+        std::string type_resource = "endpoint";
+        std::string authentication;
+        // Organize redirection info
+        if (!auth_method->empty()) {
+          authentication = *auth_method;
+        } else {
+          authentication = "no auth";
+        };
+        if (host.loadBalancer == 1) {
+          type_resource = "load balancer";
+        };
+        std::string redirection = fmt::format("{}. || {} / {} / {} / {} / {} / {} ||\n", idx_redirection, *hostname_method, 
+          *stack_ip_method, *ip_method, host.url.GetPort(), authentication, type_resource);
 
-    // Organize redirection info
-    std::string redirection = std::to_string(idx_redirection) + ". || ";
-    redirection = redirection + *hostname_method + " / " + *stack_ip_method + " / " + *ip_method + " / " +
-                  std::to_string(itr->url.GetPort());
-    if (!auth_method->empty()) {
-      redirection = redirection + " / " + *auth_method;
-    } else {
-      redirection = redirection + " / " + "no auth";
-    };
-    if (itr->loadBalancer == 1) {
-      redirection = redirection + " / load balancer";
-    } else {
-      redirection = redirection + " / " + "endpoint";
-    };
-    redirection = redirection + " ||" + " \n";
-    traceroute_str = traceroute_str + redirection;
-    delete stack_ip_method;
-    delete ip_method;
-    delete auth_method;
-    delete hostname_method;
-    ++idx_redirection;
+        all_redirections = all_redirections + redirection;
+        ++idx_redirection;
+      }
+      li.format("-------------------------------\nTraceroute:\n{}-------------------------------", all_redirections);
+    });
   }
-  traceroute_str = traceroute_str + "-------------------------------";
-  edm::LogInfo("XrdAdaptorLvl2") << traceroute_str;
 }
 
 RequestManager::RequestManager(const std::string &filename, XrdCl::OpenFlags::Flags flags, XrdCl::Access::Mode perms)
@@ -222,7 +219,7 @@ void RequestManager::initialize(std::weak_ptr<RequestManager> self) {
     handler.WaitForResponse();
     std::unique_ptr<XrdCl::XRootDStatus> status = handler.GetStatus();
     std::unique_ptr<XrdCl::HostList> hostList = handler.GetHosts();
-    TracerouteRedirections(hostList.get());
+    tracerouteRedirections(hostList.get());
     Source::determineHostExcludeString(*file, hostList.get(), excludeString);
     assert(status);
     if (status->IsOK()) {
@@ -341,22 +338,29 @@ void RequestManager::reportSiteChange(std::vector<std::shared_ptr<Source>> const
                                       std::vector<std::shared_ptr<Source>> const &iNew,
                                       std::string orig_site) const {
   auto siteList = formatSites(iNew);
-  if (!(!orig_site.empty() && (orig_site != siteList))) {
+  if (orig_site.empty() || (orig_site == siteList)) {
     auto oldSites = formatSites(iOld);
   }
-  std::string all_active_endpoints = "";
-  std::string all_active_endpoints_quality = "";
-  int size_active_sources = iNew.size();
-  for (int i = 0; i < size_active_sources; i++) {
-    std::string quality = std::to_string(iNew[i]->getQuality());
-    std::string hostname_a;
-    Source::getHostname(iNew[i]->PrettyID(), hostname_a);
 
-    all_active_endpoints.append("   [" + std::to_string(i + 1) + "] " + hostname_a);
-    all_active_endpoints_quality.append("   [" + std::to_string(i + 1) + "] " + quality + " for " + hostname_a);
-  }
-  edm::LogInfo("XrdAdaptorLvl1") << "Serving data from: " << all_active_endpoints;
-  edm::LogInfo("XrdAdaptorLvl3") << "The quality of the active sources is: " << all_active_endpoints_quality;
+  edm::LogInfo("XrdAdaptorLvl1").log([&](auto& li) {
+    li << "Serving data from: ";
+    int size_active_sources = iNew.size();
+    for (int i = 0; i < size_active_sources; ++i) {
+      std::string hostname_a;
+      Source::getHostname(iNew[i]->PrettyID(), hostname_a);
+      li.format("   [{}] {}", i+1, hostname_a);
+    }
+  });
+
+  edm::LogInfo("XrdAdaptorLvl3").log([&](auto& li) {
+    li << "The quality of the active sources is: ";
+    int size_active_sources = iNew.size();
+    for (int i = 0; i < size_active_sources; ++i) {
+      std::string hostname_a;
+      std::string quality = std::to_string(iNew[i]->getQuality());
+      li.format("   [{}] {} for {}", i+1, quality, hostname_a);
+    }
+  });
 }
 
 void RequestManager::checkSources(timespec &now,
@@ -390,16 +394,17 @@ bool RequestManager::compareSources(const timespec &now,
   std::string hostname_b;
   unsigned quality_a = activeSources[a]->getQuality();
   unsigned quality_b = activeSources[b]->getQuality();
-  Source::getHostname(activeSources[a]->ID(), hostname_a);
-  Source::getHostname(activeSources[b]->ID(), hostname_b);
+  
 
   bool findNewSource = false;
   if ((quality_a > 5130) || ((quality_a > 260) && (quality_b * 4 < quality_a))) {
+    Source::getHostname(activeSources[a]->ID(), hostname_a);
     if (quality_a > 5130) {
       edm::LogWarning("XrdAdaptorLvl3") << "Deactivating " << hostname_a << " from active sources because the quality ("
                                         << quality_a << ") is above 5130 and it is not the only active server";
     }
     if ((quality_a > 260) && (quality_b * 4 < quality_a)) {
+      Source::getHostname(activeSources[b]->ID(), hostname_b);
       edm::LogWarning("XrdAdaptorLvl3") << "Deactivating " << hostname_a << " from active sources because its quality ("
                                         << quality_a
                                         << ") is higher than 260 and 4 times larger than the other active server "
@@ -1078,7 +1083,7 @@ void XrdAdaptor::RequestManager::OpenHandler::HandleResponseWithHosts(XrdCl::XRo
   std::shared_ptr<Source> source;
   std::unique_ptr<XrdCl::XRootDStatus> status(status_ptr);
   std::unique_ptr<XrdCl::HostList> hostList(hostList_ptr);
-  TracerouteRedirections(hostList.get());
+  tracerouteRedirections(hostList.get());
   auto manager = m_manager.lock();
   // Manager object has already been deleted.  Cleanup the
   // response objects, remove our self-reference, and ignore the response.
