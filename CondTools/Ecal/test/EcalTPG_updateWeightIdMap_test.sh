bb5580cbcf99bb3887417d42f63a01d44275aba7
Davide Valsecchi <davide.valsecchi@cern.ch>
2022-08-29 15:20:09 +0200

    Fixed misnamed files in EcalTPG unittest script

--- CondTools/Ecal/test/EcalTPG_updateWeightIdMap_test.sh
+++ CondTools/Ecal/test/EcalTPG_updateWeightIdMap_test.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-cmsRun ./src/CondTools/Ecal/python/updateTPGWeightIdMap.py output=EcalTPGWeightGroup_TEST.db input=./src/CondTools/Ecal/data/EcalTPGWeightIdMap_perstrip_test.txt filetype=txt outputtag=unittest
+cmsRun ./src/CondTools/Ecal/python/updateTPGWeightIdMap.py output=EcalTPGWeightIdMap_TEST.db input=./src/CondTools/Ecal/data/EcalTPGWeightIdMap_perstrip_test.txt filetype=txt outputtag=unittest
 ret=$?
 echo "return code is $ret"
 exit $ret
