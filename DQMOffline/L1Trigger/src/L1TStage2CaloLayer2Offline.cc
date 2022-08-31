4fa7e0bfb40c1780363276e26c7d33cf68eb7dff
ccaillol <ccaillol@cern.ch>
2022-08-31 10:54:44 +0200

    L1T DQM bug fix

--- DQMOffline/L1Trigger/src/L1TStage2CaloLayer2Offline.cc
+++ DQMOffline/L1Trigger/src/L1TStage2CaloLayer2Offline.cc
@@ -422,7 +422,6 @@ void L1TStage2CaloLayer2Offline::fillJets(edm::Event const& e, const unsigned in
       minDeltaR = currentDeltaR;
       closestL1Jet = *jet;
       foundMatch = true;
-      break;
     }
   }
   //	}
