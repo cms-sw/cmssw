1bed46deca1cd8036d4a9ca8ed0417876f642e6b
Arnaud <arnaud.chiron@cern.ch>
2022-09-15 16:52:37 +0200

    PFClusterIso V1 : test part

--- Validation/RecoEgamma/test/ElectronMcSignalValidation_gedGsfElectrons_cfg.py
+++ Validation/RecoEgamma/test/ElectronMcSignalValidation_gedGsfElectrons_cfg.py
@@ -52,21 +52,10 @@ process.source = cms.Source("PoolSource", fileNames=cms.untracked.vstring(*flist
 #    fileNames = cms.untracked.vstring(
 #    [
 # 'file:/eos/user/a/archiron/HGCal_Shares/step3_A8F750A4-6D87-E711-A476-0CC47A4D7600.root',
-# 'file:/eos/user/a/archiron/HGCal_Shares/step3_AE79E794-7287-E711-9D2A-0CC47A78A3EE.root',
-# 'file:/eos/user/a/archiron/HGCal_Shares/step3_D801BDAF-7087-E711-AEF8-0CC47A7C354A.root',
 
 # 'file:/eos/user/r/rovere/www/shared/step3.root',
 
 # 'root://cms-xrd-global.cern.ch//store/relval/CMSSW_9_3_2/RelValQCD_Pt-15To7000_Flat_14TeV/GEN-SIM-RECO/93X_upgrade2023_realistic_v2_2023D17noPU-v1/10000/00FF6760-F8A6-E711-AA68-0025905A60D6.root',
-# 'root://cms-xrd-global.cern.ch//store/relval/CMSSW_11_3_0_pre4/RelValZEE_14/GEN-SIM-RECO/113X_mcRun4_realistic_v4_2026D76noPU-v1/00000/0d87e1b9-1398-4a32-9775-e2d170c00df3.root',
-# 'root://cms-xrd-global.cern.ch//store/relval/CMSSW_11_3_0_pre4/RelValZEE_14/GEN-SIM-RECO/113X_mcRun4_realistic_v4_2026D76noPU-v1/00000/22bff740-c71a-4527-a6ac-58360438960b.root',
-# 'root://cms-xrd-global.cern.ch//store/relval/CMSSW_11_3_0_pre4/RelValZEE_14/GEN-SIM-RECO/113X_mcRun4_realistic_v4_2026D76noPU-v1/00000/268ccf98-9216-493b-a1ff-403db31fbfd2.root',
-# 'root://cms-xrd-global.cern.ch//store/relval/CMSSW_11_3_0_pre4/RelValZEE_14/GEN-SIM-RECO/113X_mcRun4_realistic_v4_2026D76noPU-v1/00000/515c0b30-2803-490e-a1bf-06722f934a2f.root',
-# 'root://cms-xrd-global.cern.ch//store/relval/CMSSW_11_3_0_pre4/RelValZEE_14/GEN-SIM-RECO/113X_mcRun4_realistic_v4_2026D76noPU-v1/00000/7f266520-2cf7-4245-859c-3cb82b4e4cea.root',
-# 'root://cms-xrd-global.cern.ch//store/relval/CMSSW_11_3_0_pre4/RelValZEE_14/GEN-SIM-RECO/113X_mcRun4_realistic_v4_2026D76noPU-v1/00000/9c7caca7-9179-4d91-a4fa-2cc37cef73a1.root',
-# 'root://cms-xrd-global.cern.ch//store/relval/CMSSW_11_3_0_pre4/RelValZEE_14/GEN-SIM-RECO/113X_mcRun4_realistic_v4_2026D76noPU-v1/00000/a75081f2-16e5-499e-ac41-6b98afddbf28.root',
-# 'root://cms-xrd-global.cern.ch//store/relval/CMSSW_11_3_0_pre4/RelValZEE_14/GEN-SIM-RECO/113X_mcRun4_realistic_v4_2026D76noPU-v1/00000/b714a540-be23-423c-9b71-b92b07235dde.root',
-# 'root://cms-xrd-global.cern.ch//store/relval/CMSSW_11_3_0_pre4/RelValZEE_14/GEN-SIM-RECO/113X_mcRun4_realistic_v4_2026D76noPU-v1/00000/cf9fb9fc-8450-4d7f-a285-41007aed4158.root',
 
 # ]
 # )
@@ -89,7 +78,8 @@ from Configuration.AlCa.autoCond import autoCond
 
 # process.GlobalTag.globaltag = os.environ['TEST_GLOBAL_TAG'] + '::All'
 #process.GlobalTag.globaltag = '120X_mcRun3_2021_realistic_v1'
-process.GlobalTag.globaltag = '123X_mcRun3_2021_realistic_v14'
+#process.GlobalTag.globaltag = '125X_mcRun3_2022_realistic_v3'
+#process.GlobalTag.globaltag = '125X_mcRun4_realistic_v2_2026D88noPU' # no more needed
 # process.GlobalTag.globaltag = '113X_mcRun3_2021_realistic_v4'
 # process.GlobalTag.globaltag = '93X_mc2017_realistic_v1'
 # process.GlobalTag.globaltag = '92X_upgrade2017_realistic_v10'
