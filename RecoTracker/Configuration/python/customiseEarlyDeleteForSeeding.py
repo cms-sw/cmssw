435da92f78da929ab38d33c68585ef7860365994
Christopher Jones <chrisdjones15@gmail.com>
2022-09-16 09:55:21 -0500

    Simplified customiseEarlyDeleteForSeeding implementation

--- RecoTracker/Configuration/python/customiseEarlyDeleteForSeeding.py
+++ RecoTracker/Configuration/python/customiseEarlyDeleteForSeeding.py
@@ -17,18 +17,17 @@ def customiseEarlyDeleteForSeeding(process, products):
             if module.produceIntermediateHitDoublets:
                 products[name].append(_branchName("IntermediateHitDoublets", name))
         elif cppType in ["PixelTripletHLTEDProducer", "PixelTripletLargeTipEDProducer"]:
-            dependencies = []
-            if module.produceSeedingHitSets:
-                products[name].append(_branchName("RegionsSeedingHitSets", name))
-                dependencies.append(_branchName("RegionsSeedingHitSets", name))
-            if module.produceIntermediateHitTriplets:
-                products[name].append(_branchName("IntermediateHitTriplets", name))
-                dependencies.append(_branchName("IntermediateHitTriplets", name))
             # LayerHitMapCache of the doublets is forwarded to both
             # products, hence the dependency
             b = _branchName('IntermediateHitDoublets', module.doublets.getModuleLabel())
-            for d in dependencies:
-                references[d] = [b]
+            if module.produceSeedingHitSets:
+                pBranch = _branchName("RegionsSeedingHitSets", name)
+                products[name].append(pBranch)
+                references[pBranch]=[b]
+            if module.produceIntermediateHitTriplets:
+                pBranch = _branchName("IntermediateHitTriplets", name)
+                products[name].append(pBranch)
+                references[pBranch]=[b]
         elif cppType in ["MultiHitFromChi2EDProducer"]:
             products[name].extend([
                 _branchName("RegionsSeedingHitSets", name),
