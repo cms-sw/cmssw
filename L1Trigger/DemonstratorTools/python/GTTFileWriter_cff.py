150665b1927b9f37283b259ad4e0a21ea0bfae49
ccaillol <ccaillol@cern.ch>
2022-09-16 15:28:26 +0200

    minor fixes

--- L1Trigger/DemonstratorTools/python/GTTFileWriter_cff.py
+++ L1Trigger/DemonstratorTools/python/GTTFileWriter_cff.py
@@ -2,7 +2,7 @@ import FWCore.ParameterSet.Config as cms
 
 GTTFileWriter = cms.EDAnalyzer('GTTFileWriter',
   tracks = cms.untracked.InputTag("l1tTTTracksFromTrackletEmulation", "Level1TTTracks"),
-  convertedTracks = cms.untracked.InputTag("l1tL1GTTInputProducer", "Level1TTTracksConverted"),
+  convertedTracks = cms.untracked.InputTag("l1tGTTInputProducer", "Level1TTTracksConverted"),
   vertices = cms.untracked.InputTag("l1tVertexProducer", "l1verticesEmulation"),
   selectedTracks = cms.untracked.InputTag("l1tTrackSelectionProducer", "Level1TTTracksSelectedEmulation"),
   vertexAssociatedTracks = cms.untracked.InputTag("l1tTrackSelectionProducer", "Level1TTTracksSelectedAssociatedEmulation"),
