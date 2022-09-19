ef42a40e782fde003e82414349ca31304cdb50a6
Norraphat <srimanob@mail.cern.ch>
2022-09-19 20:42:03 +0200

    update HGCAL pset for HLT

--- HLTrigger/Configuration/python/HLT_75e33/psets/HGCAL_noise_fC_cfi.py
+++ HLTrigger/Configuration/python/HLT_75e33/psets/HGCAL_noise_fC_cfi.py
@@ -4,5 +4,6 @@ HGCAL_noise_fC = cms.PSet(
     doseMap = cms.string(''),
     scaleByDose = cms.bool(False),
     scaleByDoseAlgo = cms.uint32(0),
+    scaleByDoseFactor = cms.double(1),
     values = cms.vdouble(0.32041012, 0.384492144, 0.32041012)
 )
--- HLTrigger/Configuration/python/HLT_75e33/psets/HGCAL_noise_heback_cfi.py
+++ HLTrigger/Configuration/python/HLT_75e33/psets/HGCAL_noise_heback_cfi.py
@@ -4,5 +4,9 @@ HGCAL_noise_heback = cms.PSet(
     doseMap = cms.string(''),
     noise_MIP = cms.double(0.01),
     scaleByDose = cms.bool(False),
-    scaleByDoseAlgo = cms.uint32(0)
+    scaleByDoseAlgo = cms.uint32(0),
+    scaleByDoseFactor = cms.double(1),
+    sipmMap = cms.string(""),
+    referenceIdark = cms.double(-1),
+    referenceXtalk = cms.double(-1)
 )
