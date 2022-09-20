616d539d1c5d323f79a632cc672b5044174d91ac
Norraphat <srimanob@mail.cern.ch>
2022-09-20 20:46:22 +0200

    Update HLT siPhase2Clusters

--- HLTrigger/Configuration/python/HLT_75e33/modules/siPhase2Clusters_cfi.py
+++ HLTrigger/Configuration/python/HLT_75e33/modules/siPhase2Clusters_cfi.py
@@ -5,3 +5,6 @@ siPhase2Clusters = cms.EDProducer("Phase2TrackerClusterizer",
     maxNumberClusters = cms.uint32(0),
     src = cms.InputTag("mix","Tracker")
 )
+
+from Configuration.ProcessModifiers.premix_stage2_cff import premix_stage2
+premix_stage2.toModify(siPhase2Clusters, src = "mixData:Tracker")
