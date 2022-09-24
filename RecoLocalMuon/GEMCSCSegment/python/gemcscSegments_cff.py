c8830e2f983d723842e0a3f57d68285204c62029
Seungjin Yang <slowmoyang@gmail.com>
2022-09-24 08:59:49 +0200

    replace Run3_GEM with gemcscSegmentsTesting
    Turn on GE21-ME21 segment reconstruction only in relval workflows.

--- RecoLocalMuon/GEMCSCSegment/python/gemcscSegments_cff.py
+++ RecoLocalMuon/GEMCSCSegment/python/gemcscSegments_cff.py
@@ -2,5 +2,5 @@ import FWCore.ParameterSet.Config as cms
 
 from RecoLocalMuon.GEMCSCSegment.gemcscSegments_cfi import gemcscSegments
 
-from Configuration.Eras.Modifier_run3_GEM_cff import run3_GEM
-run3_GEM.toModify(gemcscSegments, enableME21GE21=True)
+from Configuration.ProcessModifiers.gemcscSegmentsTesting_cff import gemcscSegmentsTesting
+gemcscSegmentsTesting.toModify(gemcscSegments, enableME21GE21=True)
