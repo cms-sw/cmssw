7cae10bb78ea9ffbd9ab746d68e37deca4f9aa5c
Andrea Bocci <andrea.bocci@cern.ch>
2022-09-28 16:06:58 +0200

    Make the CUDA and Alpaka write/read tests fail on errors

--- HeterogeneousCore/CUDATest/test/testHeterogeneousCoreCUDATestWriteRead.sh
+++ HeterogeneousCore/CUDATest/test/testHeterogeneousCoreCUDATestWriteRead.sh
@@ -11,14 +11,14 @@ rm -f test.root
 echo "--------------------------------------------------------------------------------"
 echo "$ cmsRun ${LOCALTOP}/src/HeterogeneousCore/CUDATest/test/writer.py"
 echo
-cmsRun ${LOCALTOP}/src/HeterogeneousCore/CUDATest/test/writer.py
+cmsRun ${LOCALTOP}/src/HeterogeneousCore/CUDATest/test/writer.py || exit $?
 echo
 echo "--------------------------------------------------------------------------------"
 echo "$ cmsRun ${LOCALTOP}/src/HeterogeneousCore/CUDATest/test/reader.py"
 echo
-cmsRun ${LOCALTOP}/src/HeterogeneousCore/CUDATest/test/reader.py
+cmsRun ${LOCALTOP}/src/HeterogeneousCore/CUDATest/test/reader.py || exit $?
 echo
 echo "--------------------------------------------------------------------------------"
 echo "$ edmDumpEventContent test.root"
 echo
-edmDumpEventContent test.root
+edmDumpEventContent test.root || exit $?
