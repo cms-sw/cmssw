f829daece1e5d346ca6b7f09a13cec99688bd715
Shahzad Malik Muzaffar <shahzad.malik.muzaffar@cern.ch>
2022-09-28 12:38:10 +0200

    [DAQ] [GCC12] Fix build warnings

--- EventFilter/Utilities/plugins/ExceptionGenerator.cc
+++ EventFilter/Utilities/plugins/ExceptionGenerator.cc
@@ -214,7 +214,10 @@ namespace evf {
         } break;
         case 13: {
           void *vp = malloc(1024);
+#pragma GCC diagnostic push
+#pragma GCC diagnostic ignored "-Wstringop-overflow"
           memset((char *)vp - 32, 0, 1024);
+#pragma GCC diagnostic pop
           free(vp);
         } break;
         case 14: {
