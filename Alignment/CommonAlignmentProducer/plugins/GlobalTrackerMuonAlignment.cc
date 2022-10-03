35bee0863d1a99718bc145c98d856792bcbcee75
Christopher Jones <chrisdjones15@gmail.com>
2022-10-03 10:05:28 -0500

    Removed unnecessary header in GlobalTrackerMuonAlignment
    This removes a commented out inclusion of a deprecated header.
    Removing this makes it easier to grep CMSSW to find the includes.

--- Alignment/CommonAlignmentProducer/plugins/GlobalTrackerMuonAlignment.cc
+++ Alignment/CommonAlignmentProducer/plugins/GlobalTrackerMuonAlignment.cc
@@ -34,7 +34,6 @@
 #include "FWCore/Framework/interface/one/EDAnalyzer.h"
 #include "FWCore/Framework/interface/ESWatcher.h"
 #include "FWCore/Framework/interface/Frameworkfwd.h"
-//#include "FWCore/Framework/interface/EDProducer.h"
 #include "FWCore/Framework/interface/Event.h"
 #include "FWCore/Framework/interface/EventSetup.h"
 #include "FWCore/Framework/interface/MakerMacros.h"
