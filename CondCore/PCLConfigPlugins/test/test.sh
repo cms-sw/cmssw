cd1f3a1dba946cb4b665983282f237250f85a0d1
mmusich <marco.musich@cern.ch>
2022-10-03 19:10:17 +0200

    add PayloadInspector for the high granularity payload

--- CondCore/PCLConfigPlugins/test/test.sh
+++ CondCore/PCLConfigPlugins/test/test.sh
@@ -1,17 +1,16 @@
 #!/bin/bash
 # Save current working dir so img can be outputted there later
 W_DIR=$(pwd);
-# Set SCRAM architecture var
-SCRAM_ARCH=slc6_amd64_gcc630;
-export SCRAM_ARCH;
 source /afs/cern.ch/cms/cmsset_default.sh;
 eval `scram run -sh`;
 # Go back to original working directory
 cd $W_DIR;
 # Run get payload data script
 
+mkdir $W_DIR/results/
+
 ####################
-# Test Gains
+# Test Display
 ####################
 getPayloadData.py \
     --plugin pluginAlignPCLThresholds_PayloadInspector \
@@ -21,3 +20,49 @@ getPayloadData.py \
     --iovs '{"start_iov": "1", "end_iov": "1"}' \
     --db Prod \
     --test;
+
+mv *.png $W_DIR/results/LG_display.png
+
+####################
+# Test Compare
+####################
+getPayloadData.py \
+    --plugin pluginAlignPCLThresholds_PayloadInspector \
+    --plot plot_AlignPCLThresholds_CompareTwoTags \
+    --tag SiPixelAliThresholds_offline_v0 \
+    --tagtwo SiPixelAliThresholds_express_v0 \
+    --time_type Run \
+    --iovs '{"start_iov": "1", "end_iov": "1"}' \
+    --iovstwo '{"start_iov": "1", "end_iov": "1"}' \
+    --db Prod \
+    --test;
+
+mv *.png $W_DIR/results/LG_compare.png
+
+####################
+# Test Display HG
+####################
+getPayloadData.py \
+    --plugin pluginAlignPCLThresholdsHG_PayloadInspector \
+    --plot plot_AlignPCLThresholdsHG_Display \
+    --tag SiPixelAliThresholdsHG_express_v0 \
+    --time_type Run \
+    --iovs '{"start_iov": "1", "end_iov": "1"}' \
+    --db Prod \
+    --test;
+
+mv *.png $W_DIR/results/HG_display.png
+
+####################
+# Test Compare HG
+####################
+getPayloadData.py \
+    --plugin pluginAlignPCLThresholdsHG_PayloadInspector \
+    --plot plot_AlignPCLThresholdsHG_Compare \
+    --tag SiPixelAliThresholdsHG_express_v0 \
+    --time_type Run \
+    --iovs '{"start_iov": "1", "end_iov": "359659"}' \
+    --db Prod \
+    --test;
+
+mv *.png $W_DIR/results/HG_compare.png
