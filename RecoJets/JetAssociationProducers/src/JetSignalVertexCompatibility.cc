f26dbe6e06e851f5dd4f457a01b48a35955860b9
Christopher Jones <chrisdjones15@gmail.com>
2022-10-06 11:22:00 -0500

    Convert RecoJets/JetAssociationProducers to thread friendly types
    This fixes the CMS deprecation warnings.

--- RecoJets/JetAssociationProducers/src/JetSignalVertexCompatibility.cc
+++ RecoJets/JetAssociationProducers/src/JetSignalVertexCompatibility.cc
@@ -26,16 +26,14 @@ JetSignalVertexCompatibility::JetSignalVertexCompatibility(const edm::ParameterS
     : algo(params.getParameter<double>("cut"), params.getParameter<double>("temperature")) {
   jetTracksAssocToken = consumes<JetTracksAssociationCollection>(params.getParameter<edm::InputTag>("jetTracksAssoc"));
   primaryVerticesToken = consumes<VertexCollection>(params.getParameter<edm::InputTag>("primaryVertices"));
+  builderToken = esConsumes(edm::ESInputTag("", "TransientTrackBuilder"));
   produces<JetFloatAssociation::Container>();
 }
 
 JetSignalVertexCompatibility::~JetSignalVertexCompatibility() {}
 
 void JetSignalVertexCompatibility::produce(edm::Event &event, const edm::EventSetup &es) {
-  edm::ESHandle<TransientTrackBuilder> trackBuilder;
-  es.get<TransientTrackRecord>().get("TransientTrackBuilder", trackBuilder);
-
-  algo.resetEvent(trackBuilder.product());
+  algo.resetEvent(&es.getData(builderToken));
 
   edm::Handle<JetTracksAssociationCollection> jetTracksAssoc;
   event.getByToken(jetTracksAssocToken, jetTracksAssoc);
