882d28f40b504f5d27352908aca6b4f32239f7b3
leonardo <leonardo.giannini@cern.ch>
2022-10-11 15:48:46 -0700

    12_5_0_pre5 run3 retraining and WPs

--- RecoTracker/FinalTrackSelectors/python/trackSelectionTf_cfi.py
+++ RecoTracker/FinalTrackSelectors/python/trackSelectionTf_cfi.py
@@ -1,7 +1,7 @@
 from PhysicsTools.TensorFlow.tfGraphDefProducer_cfi import tfGraphDefProducer as _tfGraphDefProducer
 trackSelectionTf = _tfGraphDefProducer.clone(
     ComponentName = "trackSelectionTf",
-    FileName = "RecoTracker/FinalTrackSelectors/data/TrackTfClassifier/MkFit4plus3_2021Run3.pb"
+    FileName = "RecoTracker/FinalTrackSelectors/data/TrackTfClassifier/MkFit_Run3_12_5_0_pre5.pb"
 )
 
 
