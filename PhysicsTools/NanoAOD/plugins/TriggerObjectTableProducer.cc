218e1fd1c7140ed6aa6752e26c120e27ee767e28
Sebastien Wertz <sebastien.wertz@cern.ch>
2022-10-11 13:32:50 +0200

    fix loops over BXVector

--- PhysicsTools/NanoAOD/plugins/TriggerObjectTableProducer.cc
+++ PhysicsTools/NanoAOD/plugins/TriggerObjectTableProducer.cc
@@ -150,19 +150,20 @@ void TriggerObjectTableProducer::produce(edm::Event &iEvent, const edm::EventSet
   const auto &l1Tau = iEvent.get(l1Tau_);
 
   std::vector<pair<pat::TriggerObjectStandAlone, int>> l1Objects;
-  l1Objects.reserve(l1EG.size() + l1Sum.size() + l1Jet.size() + l1Muon.size() + l1Tau.size());
+  l1Objects.reserve(l1EG.size(0) + l1Sum.size(0) + l1Jet.size(0) + l1Muon.size(0) + l1Tau.size(0));
 
-  for (const auto &obj : l1EG) {
-    pat::TriggerObjectStandAlone l1obj(obj.p4());
+  // no range-based for because we want bx=0 only
+  for (l1t::EGammaBxCollection::const_iterator it = l1EG.begin(0); it != l1EG.end(0); it++) {
+    pat::TriggerObjectStandAlone l1obj(it->p4());
     l1obj.setCollection("L1EG");
     l1obj.addTriggerObjectType(trigger::TriggerL1EG);
-    l1Objects.emplace_back(l1obj, obj.hwIso());
+    l1Objects.emplace_back(l1obj, it->hwIso());
   }
 
-  for (const auto &obj : l1Sum) {
-    pat::TriggerObjectStandAlone l1obj(obj.p4());
+  for (l1t::EtSumBxCollection::const_iterator it = l1Sum.begin(0); it != l1Sum.end(0); it++) {
+    pat::TriggerObjectStandAlone l1obj(it->p4());
 
-    switch (obj.getType()) {
+    switch (it->getType()) {
       case l1t::EtSum::EtSumType::kMissingEt:
         l1obj.addTriggerObjectType(trigger::TriggerL1ETM);
         l1obj.setCollection("L1ETM");
@@ -207,29 +208,29 @@ void TriggerObjectTableProducer::produce(edm::Event &iEvent, const edm::EventSet
         continue;
     }
 
-    l1Objects.emplace_back(l1obj, obj.hwIso());
+    l1Objects.emplace_back(l1obj, it->hwIso());
   }
 
-  for (const auto &obj : l1Jet) {
-    pat::TriggerObjectStandAlone l1obj(obj.p4());
+  for (l1t::JetBxCollection::const_iterator it = l1Jet.begin(0); it != l1Jet.end(0); it++) {
+    pat::TriggerObjectStandAlone l1obj(it->p4());
     l1obj.setCollection("L1Jet");
     l1obj.addTriggerObjectType(trigger::TriggerL1Jet);
-    l1Objects.emplace_back(l1obj, obj.hwIso());
+    l1Objects.emplace_back(l1obj, it->hwIso());
   }
 
-  for (const auto &obj : l1Muon) {
-    pat::TriggerObjectStandAlone l1obj(obj.p4());
+  for (l1t::MuonBxCollection::const_iterator it = l1Muon.begin(0); it != l1Muon.end(0); it++) {
+    pat::TriggerObjectStandAlone l1obj(it->p4());
     l1obj.setCollection("L1Mu");
     l1obj.addTriggerObjectType(trigger::TriggerL1Mu);
-    l1obj.setCharge(obj.charge());
-    l1Objects.emplace_back(l1obj, obj.hwIso());
+    l1obj.setCharge(it->charge());
+    l1Objects.emplace_back(l1obj, it->hwIso());
   }
 
-  for (const auto &obj : l1Tau) {
-    pat::TriggerObjectStandAlone l1obj(obj.p4());
+  for (l1t::TauBxCollection::const_iterator it = l1Tau.begin(0); it != l1Tau.end(0); it++) {
+    pat::TriggerObjectStandAlone l1obj(it->p4());
     l1obj.setCollection("L1Tau");
     l1obj.addTriggerObjectType(trigger::TriggerL1Tau);
-    l1Objects.emplace_back(l1obj, obj.hwIso());
+    l1Objects.emplace_back(l1obj, it->hwIso());
   }
 
   unsigned int nobj = selected.size();
