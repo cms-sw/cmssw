ef9dc33e330ff0c15efea7af56647a4b24bd637a
ccaillol <ccaillol@cern.ch>
2022-10-13 14:38:09 +0200

    fix cfi

--- L1Trigger/L1TTrackMatch/python/l1tTrackerEmuEtMiss_cfi.py
+++ L1Trigger/L1TTrackMatch/python/l1tTrackerEmuEtMiss_cfi.py
@@ -10,8 +10,8 @@ l1tTrackerEmuEtMiss = cms.EDProducer('L1TrackerEtMissEmulatorProducer',
     # set useVertexEmulator to true
     L1MetCollectionName = cms.string("L1TrackerEmuEtMiss"),
     
-    nCordicSteps = cms.int32( 8 ), #Number of steps for cordic sqrt and phi computation
-    debug        = cms.int32( 0 ),  #0 - No Debug, 1 - LUT debug, 2 - Phi Debug, 3 - Z debug, 4 - Et Debug, 5 - Cordic Debug, 6 - Output, 7 - Every Selected Track
+    nCordicSteps = cms.int32( 13 ), #Number of steps for cordic sqrt and phi computation
+    debug        = cms.int32( 6 ),  #0 - No Debug, 1 - LUT debug, 2 - Phi Debug, 3 - Z debug, 4 - Et Debug, 5 - Cordic Debug, 6 - Output, 7 - Every Selected Track
     useGTTinput  = cms.bool( True ),
 
 )
