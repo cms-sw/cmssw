189c362ac2ded660a7714d737aeab8511ff23027
Jonas Hahnfeld <jonas.hahnfeld@cern.ch>
2022-10-14 17:39:26 +0200

    Add G4HepEm to CMSSW
    The new EMH physics list replaces the EM processes for electrons,
    positrons, and gammas with G4HepEm. Note that gamma-lepto-nuclear
    interactions are NOT implemented yet, and disabled for this physics
    list (by removing G4EmExtraPhysics).

---
+++ SimG4Core/PhysicsLists/plugins/FTFPCMS_BERT_EMH.cc
@@ -0,0 +1,44 @@
+#include "FTFPCMS_BERT_EMH.h"
+#include "SimG4Core/PhysicsLists/interface/CMSEmStandardPhysicsEMH.h"
+#include "SimG4Core/PhysicsLists/interface/CMSHadronPhysicsFTFP_BERT.h"
+#include "FWCore/MessageLogger/interface/MessageLogger.h"
+
+#include "G4DecayPhysics.hh"
+#include "G4IonPhysics.hh"
+#include "G4StoppingPhysics.hh"
+#include "G4HadronElasticPhysics.hh"
+
+FTFPCMS_BERT_EMH::FTFPCMS_BERT_EMH(const edm::ParameterSet& p) : PhysicsList(p) {
+  int ver = p.getUntrackedParameter<int>("Verbosity", 0);
+  bool emPhys = p.getUntrackedParameter<bool>("EMPhysics", true);
+  bool hadPhys = p.getUntrackedParameter<bool>("HadPhysics", true);
+  double minFTFP = p.getParameter<double>("EminFTFP") * CLHEP::GeV;
+  double maxBERT = p.getParameter<double>("EmaxBERT") * CLHEP::GeV;
+  double maxBERTpi = p.getParameter<double>("EmaxBERTpi") * CLHEP::GeV;
+  edm::LogVerbatim("PhysicsList") << "CMS Physics List FTFP_BERT_EMH: "
+                                  << "\n Flags for EM Physics: " << emPhys << "; Hadronic Physics: " << hadPhys
+                                  << "\n Transition energy Bertini/FTFP from " << minFTFP / CLHEP::GeV << " to "
+                                  << maxBERT / CLHEP::GeV << "; for pions to " << maxBERTpi / CLHEP::GeV << " GeV";
+
+  if (emPhys) {
+    // EM Physics
+    RegisterPhysics(new CMSEmStandardPhysicsEMH(ver, p));
+  }
+
+  // Decays
+  this->RegisterPhysics(new G4DecayPhysics(ver));
+
+  if (hadPhys) {
+    // Hadron Elastic scattering
+    RegisterPhysics(new G4HadronElasticPhysics(ver));
+
+    // Hadron Physics
+    RegisterPhysics(new CMSHadronPhysicsFTFP_BERT(minFTFP, maxBERT, maxBERTpi, minFTFP, maxBERT));
+
+    // Stopping Physics
+    RegisterPhysics(new G4StoppingPhysics(ver));
+
+    // Ion Physics
+    RegisterPhysics(new G4IonPhysics(ver));
+  }
+}
