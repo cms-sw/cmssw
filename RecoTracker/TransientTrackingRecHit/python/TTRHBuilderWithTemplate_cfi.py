f6282afb9a0ec58304c33968f40fce8b5185f6c9
Mario Masciovecchio <mario.masciovecchio@cern.ch>
2022-10-17 11:30:25 -0700

    Use SiPixel CPE (cluster repair) with speed=0; i.e., without ProbQ computation

--- RecoTracker/TransientTrackingRecHit/python/TTRHBuilderWithTemplate_cfi.py
+++ RecoTracker/TransientTrackingRecHit/python/TTRHBuilderWithTemplate_cfi.py
@@ -8,6 +8,8 @@ TTRHBuilderAngleAndTemplate = tkTransientTrackingRecHitBuilderESProducer.clone(S
                                                                                Matcher = 'StandardMatcher',
                                                                                ComputeCoarseLocalPositionFromDisk = False)
 
+TTRHBuilderAngleAndTemplateWithoutProbQ = TTRHBuilderAngleAndTemplate.clone(ComponentName = 'WithAngleAndTemplateWithoutProbQ')
+
 from Configuration.Eras.Modifier_trackingPhase2PU140_cff import trackingPhase2PU140
 trackingPhase2PU140.toModify(TTRHBuilderAngleAndTemplate, 
                              Phase2StripCPE = 'Phase2StripCPE',
@@ -16,6 +18,7 @@ trackingPhase2PU140.toModify(TTRHBuilderAngleAndTemplate,
 # uncomment these two lines to turn on Cluster Repair CPE
 from Configuration.Eras.Modifier_phase1Pixel_cff import phase1Pixel
 phase1Pixel.toModify(TTRHBuilderAngleAndTemplate, PixelCPE = 'PixelCPEClusterRepair')
+phase1Pixel.toModify(TTRHBuilderAngleAndTemplateWithoutProbQ, PixelCPE = 'PixelCPEClusterRepairWithoutProbQ')
 
 # Turn off template reco for phase 2 (when not supported)
 from Configuration.ProcessModifiers.PixelCPEGeneric_cff import PixelCPEGeneric
