caf14246cbfc5e7ff11899fe91ce08471638ff39
leonardo <leonardo.giannini@cern.ch>
2022-10-25 08:50:03 -0700

    pixelLess DNN test

--- RecoTracker/FinalTrackSelectors/python/trackSelectionTf_cfi.py
+++ RecoTracker/FinalTrackSelectors/python/trackSelectionTf_cfi.py
@@ -3,5 +3,9 @@ trackSelectionTf = _tfGraphDefProducer.clone(
     ComponentName = "trackSelectionTf",
     FileName = "RecoTracker/FinalTrackSelectors/data/TrackTfClassifier/MkFit_Run3_12_5_0_pre5.pb"
 )
+trackSelectionTfPLess = _tfGraphDefProducer.clone(
+    ComponentName = "trackSelectionTfPLess",
+    FileName = "RecoTracker/FinalTrackSelectors/data/TrackTfClassifier/MkFitPixelLessOnly_Run3_12_5_0_pre5.pb"
+)
 
 
