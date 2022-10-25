41f568805e076e3cc7fbbf55d2a6f533d0cc0f9f
jean-roch <vlimant@cern.ch>
2022-10-25 20:32:09 +0200

    unused parameter ; from configuration validation

--- PhysicsTools/NanoAOD/python/genVertex_cff.py
+++ PhysicsTools/NanoAOD/python/genVertex_cff.py
@@ -3,10 +3,8 @@ from PhysicsTools.NanoAOD.common_cff import Var,ExtVar
 
 genVertexTable = cms.EDProducer("SimpleXYZPointFlatTableProducer",
     src = cms.InputTag("genParticles:xyz0"),
-    cut = cms.string(""),
     name= cms.string("GenVtx"),
     doc = cms.string("Gen vertex"),
-    singleton = cms.bool(True),
     extension = cms.bool(False),
     variables = cms.PSet(
          x = Var("X", float, doc="gen vertex x", precision=10),
