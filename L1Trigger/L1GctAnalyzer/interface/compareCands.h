f2bd151f70f52269b520fbf6aec6c483688f4bfa
Christopher Jones <chrisdjones15@gmail.com>
2022-10-28 11:06:58 -0500

    Removed unused EDAnalyzer.h from L1Trigger*
    The header is deprecated.

--- L1Trigger/L1GctAnalyzer/interface/compareCands.h
+++ L1Trigger/L1GctAnalyzer/interface/compareCands.h
@@ -3,7 +3,6 @@
 
 #include "FWCore/ParameterSet/interface/ParameterSet.h"
 #include "FWCore/Framework/interface/Frameworkfwd.h"
-#include "FWCore/Framework/interface/EDAnalyzer.h"
 #include "FWCore/Framework/interface/Event.h"
 #include "FWCore/Framework/interface/MakerMacros.h"
 #include "FWCore/ParameterSet/interface/ParameterSet.h"
