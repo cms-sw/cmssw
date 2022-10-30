5e6af5695a221ab1c5d7069fe05409d44013f520
yechanKang <yck.kang@gmail.com>
2022-10-30 18:45:44 +0900

    update GEMOHStatus to check GE21 module type

--- EventFilter/GEMRawToDigi/plugins/GEMRawToDigiModule.cc
+++ EventFilter/GEMRawToDigi/plugins/GEMRawToDigiModule.cc
@@ -172,7 +172,7 @@ void GEMRawToDigiModule::produce(edm::StreamID iID, edm::Event& iEvent, edm::Eve
         GEMDetId cId(geb_dc.detId);
         int chamberType = geb_dc.chamberType;
 
-        GEMOHStatus st_oh(optoHybrid);
+        GEMOHStatus st_oh(optoHybrid, chamberType);
         if (st_oh.isBad()) {
           LogDebug("GEMRawToDigiModule") << st_oh;
           if (keepDAQStatus_) {
