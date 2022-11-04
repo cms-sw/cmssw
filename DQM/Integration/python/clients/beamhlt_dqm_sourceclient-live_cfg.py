8420561923404ad06113de685a8104f40f3acfa1
francescobrivio <francesco.brivio@cern.ch>
2022-11-04 16:05:08 +0100

    Use Era Run3_pp_on_PbPb_approxSiStripClusters in DQM clients with tracking

--- DQM/Integration/python/clients/beamhlt_dqm_sourceclient-live_cfg.py
+++ DQM/Integration/python/clients/beamhlt_dqm_sourceclient-live_cfg.py
@@ -10,8 +10,13 @@ BSOnlineOmsServiceUrl = 'http://cmsoms-services.cms:9949/urn:xdaq-application:li
 useLockRecords = True
 
 import sys
-from Configuration.Eras.Era_Run3_cff import Run3
-process = cms.Process("BeamMonitor", Run3)
+if 'runkey=hi_run' in sys.argv:
+  from Configuration.Eras.Era_Run3_pp_on_PbPb_approxSiStripClusters_cff import Run3_pp_on_PbPb_approxSiStripClusters
+  process = cms.Process("BeamMonitor", Run3_pp_on_PbPb_approxSiStripClusters)
+else:
+  from Configuration.Eras.Era_Run3_cff import Run3
+  process = cms.Process("BeamMonitor", Run3)
+
 
 # Message logger
 #process.load("FWCore.MessageLogger.MessageLogger_cfi")
