aa8675e3bdece620efb73b447f9191fffbcb6ee6
Abhirami Harilal <abhi.harilal@gmail.com>
2022-11-15 15:53:41 +0100

    Add TTID error plots

--- DQM/EcalMonitorClient/python/IntegrityClient_cfi.py
+++ DQM/EcalMonitorClient/python/IntegrityClient_cfi.py
@@ -18,7 +18,8 @@ ecalIntegrityClient = cms.untracked.PSet(
         ChId = ecalIntegrityTask.MEs.ChId,
         TowerId = ecalIntegrityTask.MEs.TowerId,
         BXSRP = ecalRawDataTask.MEs.BXSRP,
-        BXTCC = ecalRawDataTask.MEs.BXTCC
+        BXTCC = ecalRawDataTask.MEs.BXTCC,
+	NumEvents = ecalOccupancyTask.MEs.NEvents
     ),
     MEs = cms.untracked.PSet(
         QualitySummary = cms.untracked.PSet(
@@ -41,6 +42,13 @@ ecalIntegrityClient = cms.untracked.PSet(
             otype = cms.untracked.string('Ecal3P'),
             btype = cms.untracked.string('Crystal'),
             description = cms.untracked.string('Map of channel status as given by the Ecal Channel Status Record. LEGEND:<br/>0: Channel ok,<br/>1: DAC settings problem, pedestal not in the design range,<br/>2: Channel with no laser, ok elsewhere,<br/>3: Noisy,<br/>4: Very noisy,<br/>5-7: Reserved for more categories of noisy channels,<br/>8: Channel at fixed gain 6 (or 6 and 1),<br/>9: Channel at fixed gain 1,<br/>10: Channel at fixed gain 0 (dead of type this),<br/>11: Non-responding isolated channel (dead of type other),<br/>12: Channel and one or more neigbors not responding (e.g.: in a dead VFE 5x1 channel),<br/>13: Channel in TT with no data link, TP data ok,<br/>14: Channel in TT with no data link and no TP data.')
-        )
+       ),
+	TowerIdNormalized = cms.untracked.PSet(
+            path = cms.untracked.string('%(subdet)s/%(prefix)sIntegrityTask/TTId/%(prefix)sIT TTId Normalized %(sm)s'),
+            kind = cms.untracked.string('TH2F'),
+            otype = cms.untracked.string('SM'),
+            btype = cms.untracked.string('SuperCrystal'),
+            description = cms.untracked.string('TTID errors normalized by total no.of processed events per run.')
+       )
     )
 )
