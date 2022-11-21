8f3f2655c7814ba6b6cc9841dcbe94f73be81e79
Vincenzo Mastrapasqua <vin.mas21@gmail.com>
2022-11-21 17:08:59 +0100

    restore a eff vs rapidity plot

--- HLTriggerOffline/HeavyFlavor/python/heavyFlavorValidationHarvesting_cfi.py
+++ HLTriggerOffline/HeavyFlavor/python/heavyFlavorValidationHarvesting_cfi.py
@@ -12,22 +12,23 @@ heavyFlavorValidationHarvesting = DQMEDHarvester("HeavyFlavorHarvesting",
 
 #################  DOUBLE  ETA  PT  ######################
     cms.untracked.PSet( NumDenEffMEnames = cms.untracked.vstring("globDimuon_genEtaPt","genDimuon_genEtaPt","effGlobDigenAND_genEtaPt") ),
+    cms.untracked.PSet( NumDenEffMEnames = cms.untracked.vstring("pathDimuon_recoEtaPt","globDimuon_recoEtaPt","effPathDiglobOR_recoEtaPt") ),
     cms.untracked.PSet( NumDenEffMEnames = cms.untracked.vstring("diPathDimuon_recoEtaPt","globDimuon_recoEtaPt","effPathDiglobAND_recoEtaPt") ),
 
 #################  DOUBLE  RAPIDITY  PT  ######################
-#    cms.untracked.PSet( NumDenEffMEnames = cms.untracked.vstring("globDimuon_genRapPt","genDimuon_genRapPt","effGlobDigenAND_genRapPt") ),
-#    cms.untracked.PSet( NumDenEffMEnames = cms.untracked.vstring("pathDimuon_recoRapPt","globDimuon_recoRapPt","effPathDiglobOR_recoRapPt") ),
-#    cms.untracked.PSet( NumDenEffMEnames = cms.untracked.vstring("diPathDimuon_recoRapPt","globDimuon_recoRapPt","effPathDiglobAND_recoRapPt") ),
+    cms.untracked.PSet( NumDenEffMEnames = cms.untracked.vstring("globDimuon_genRapPt","genDimuon_genRapPt","effGlobDigenAND_genRapPt") ),
+    cms.untracked.PSet( NumDenEffMEnames = cms.untracked.vstring("pathDimuon_recoRapPt","globDimuon_recoRapPt","effPathDiglobOR_recoRapPt") ),
+    cms.untracked.PSet( NumDenEffMEnames = cms.untracked.vstring("diPathDimuon_recoRapPt","globDimuon_recoRapPt","effPathDiglobAND_recoRapPt") ),
     
-    #################  DOUBLE  PT  DR  ######################
-#     cms.untracked.PSet( NumDenEffMEnames = cms.untracked.vstring("globDimuon_genPtDR","genDimuon_genPtDR","effGlobDigenAND_genPtDR") ),
-#     cms.untracked.PSet( NumDenEffMEnames = cms.untracked.vstring("pathDimuon_recoPtDR","globDimuon_recoPtDR","effPathDiglobOR_recoPtDR") ),
-#     cms.untracked.PSet( NumDenEffMEnames = cms.untracked.vstring("diPathDimuon_recoPtDR","globDimuon_recoPtDR","effPathDiglobAND_recoPtDR") ),
+#################  DOUBLE  PT  DR  ######################
+    cms.untracked.PSet( NumDenEffMEnames = cms.untracked.vstring("globDimuon_genPtDR","genDimuon_genPtDR","effGlobDigenAND_genPtDR") ),
+    cms.untracked.PSet( NumDenEffMEnames = cms.untracked.vstring("pathDimuon_recoPtDR","globDimuon_recoPtDR","effPathDiglobOR_recoPtDR") ),
+    cms.untracked.PSet( NumDenEffMEnames = cms.untracked.vstring("diPathDimuon_recoPtDR","globDimuon_recoPtDR","effPathDiglobAND_recoPtDR") ),
 
-    #################  DOUBLE  ETA  PT  ######################
-#     cms.untracked.PSet( NumDenEffMEnames = cms.untracked.vstring("globDimuon_genPtDRpos","genDimuon_genPtDRpos","effGlobDigenAND_genPtDRpos") ),
-#     cms.untracked.PSet( NumDenEffMEnames = cms.untracked.vstring("pathDimuon_recoPtDRpos","globDimuon_recoPtDRpos","effPathDiglobOR_recoPtDRpos") ),
-#     cms.untracked.PSet( NumDenEffMEnames = cms.untracked.vstring("diPathDimuon_recoPtDRpos","globDimuon_recoPtDRpos","effPathDiglobAND_recoPtDRpos") ),
+#################  DOUBLE  ETA  PT  ######################
+#    cms.untracked.PSet( NumDenEffMEnames = cms.untracked.vstring("globDimuon_genPtDRpos","genDimuon_genPtDRpos","effGlobDigenAND_genPtDRpos") ),
+#    cms.untracked.PSet( NumDenEffMEnames = cms.untracked.vstring("pathDimuon_recoPtDRpos","globDimuon_recoPtDRpos","effPathDiglobOR_recoPtDRpos") ),
+#    cms.untracked.PSet( NumDenEffMEnames = cms.untracked.vstring("diPathDimuon_recoPtDRpos","globDimuon_recoPtDRpos","effPathDiglobAND_recoPtDRpos") ),
 
     cms.untracked.PSet( NumDenEffMEnames = cms.untracked.vstring("resultDimuon_recoEtaPt","globDimuon_recoEtaPt","effResultDiglob_recoEtaPt") ),
     cms.untracked.PSet( NumDenEffMEnames = cms.untracked.vstring("resultDimuon_recoRapPt","globDimuon_recoRapPt","effResultDiglob_recoRapPt") )
