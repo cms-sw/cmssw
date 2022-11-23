4d69ebe5b29d7047429a459085c6e50bb318ae6b
W. David Dagenhart <wdd@fnal.gov>
2022-11-23 23:33:27 +0100

    JetCorrector migration in DiJetAnalyzer, GammaJetAnalysis

--- Calibration/HcalCalibAlgos/test/python/gammaJetAnalysis_cfg.py
+++ Calibration/HcalCalibAlgos/test/python/gammaJetAnalysis_cfg.py
@@ -1,3 +1,19 @@
+# This was migrated to use reco::JetCorrector in Nov 2022.
+# This was not fully tested because this configuration
+# before the migration already failed with multiple errors
+# unrelated to JetCorrectors. At the least:
+#
+# Something unknown but unrelated to JetCorrectors in these
+# three lines in gammaJetAnalysis_cfi.py causes this file to
+# be unparseable by Python:
+#
+#   from RecoJets.Configuration.RecoJets_cff import *
+#   from RecoJets.Configuration.RecoPFJets_cff import *
+#   from CommonTools.ParticleFlow.pfNoPileUp_cff import *
+#
+# The input file does not exist in a publicly available
+# space. There may be other problems.
+
 import FWCore.ParameterSet.Config as cms
 process = cms.Process('ANALYSIS')
 
@@ -16,7 +32,7 @@ process.MessageLogger.cerr.FwkReport.reportEvery=cms.untracked.int32(1000)
 #load the gammaJet analyzer
 process.load('Calibration.HcalCalibAlgos.gammaJetAnalysis_cfi')
 #  needed for nonCHS
-process.load('JetMETCorrections.Configuration.JetCorrectionProducers_cff')
+process.load('JetMETCorrections.Configuration.CorrectedJetProducers_cff')
 
 # run over files
 process.GammaJetAnalysis.rootHistFilename = cms.string('PhoJet_tree_CHS.root')
@@ -46,7 +62,7 @@ process.GammaJetAnalysis_noCHS= process.GammaJetAnalysis.clone()
 process.GammaJetAnalysis_noCHS.rootHistFilename = cms.string('PhoJet_tree_nonCHS.root')
 # for 7XY use ak4* instead of ak5
 process.GammaJetAnalysis_noCHS.pfJetCollName = cms.string('ak4PFJets')
-process.GammaJetAnalysis_noCHS.pfJetCorrName = cms.string('ak4PFL2L3')
+process.GammaJetAnalysis_noCHS.JetCorrections = cms.InputTag("ak4PFL2L3Corrector")
 
 process.source = cms.Source("PoolSource", 
                             fileNames = cms.untracked.vstring(
@@ -69,5 +85,5 @@ process.GammaJetAnalysis.doGenJets = cms.bool(False)
 process.GammaJetAnalysis.debug     = cms.untracked.int32(0)
 
 process.p = cms.Path(
-    process.GammaJetAnalysis
+    process.GammaJetAnalysis, process.ak4PFL2L3CorrectorTask, process.ak4PFCHSL2L3CorrectorTask
 )
