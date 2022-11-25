acaaf7f3a1ed59c4c25cf199b6cfbd16e3f2518f
Ian Tomalin <ian.tomalin@stfc.ac.uk>
2022-11-25 00:41:39 +0000

    updated to geometry D88

--- L1Trigger/TrackFindingTracklet/test/skimForCI_cfg.py
+++ L1Trigger/TrackFindingTracklet/test/skimForCI_cfg.py
@@ -32,8 +32,8 @@ process.load('Configuration.EventContent.EventContent_cff')
 process.load('Configuration.StandardSequences.MagneticField_cff')
 
 # D49 geometry (T15 tracker)
-process.load('Configuration.Geometry.GeometryExtended2026D76Reco_cff')
-process.load('Configuration.Geometry.GeometryExtended2026D76_cff')
+process.load('Configuration.Geometry.GeometryExtended2026DD88Reco_cff')
+process.load('Configuration.Geometry.GeometryExtended2026DD88_cff')
 
 process.load('Configuration.StandardSequences.EndOfProcess_cff')
 
@@ -46,8 +46,11 @@ process.maxEvents = cms.untracked.PSet(input = cms.untracked.int32(1000))
 from MCsamples.Scripts.getCMSdata_cfi import *
 
 # Read data from card files (defines getCMSdataFromCards()):
-from MCsamples.RelVal_1130_D76.PU0_TTbar_14TeV_cfi import *
-inputMC = getCMSdataFromCards()
+#from MCsamples.RelVal_1130_DD88.PU0_TTbar_14TeV_cfi import *
+#inputMC = getCMSdataFromCards()
+
+dataName="/RelValTTbar_14TeV/CMSSW_12_6_0_pre4-125X_mcRun4_realistic_v2_2026D88noPU-v1/GEN-SIM-DIGI-RAW"
+inputMC=getCMSdata(dataName)
 
 process.source = cms.Source("PoolSource", fileNames = cms.untracked.vstring(*inputMC))
 
