badbd8bc0b8dabfb3da2d837d756b2d9326bf674
Sunanda <sunanda.banerjee@cern.ch>
2022-12-06 14:09:16 +0100

    Modify the test code to spot the issue for V17

--- Geometry/HGCalCommonData/test/HGCalWaferIDTester.cc
+++ Geometry/HGCalCommonData/test/HGCalWaferIDTester.cc
@@ -13,7 +13,7 @@
 */
 //
 // Original Author:  Sunanda Banerjee
-//         Created:  Mon 2019/07/15
+//         Created:  Mon 2022/12/01
 //
 //
 
@@ -51,6 +51,7 @@ public:
 
 private:
   const std::string nameSense_, errorFile_;
+  const int mode_;
   const edm::ESGetToken<HGCalDDDConstants, IdealGeometryRecord> dddToken_;
   std::vector<HGCSiliconDetId> detIds_;
   std::vector<std::pair<double, double>> posXY_;
@@ -59,9 +60,10 @@ private:
 HGCalWaferIDTester::HGCalWaferIDTester(const edm::ParameterSet& iC)
     : nameSense_(iC.getParameter<std::string>("nameSense")),
       errorFile_(iC.getParameter<std::string>("fileName")),
+      mode_(iC.getParameter<int>("mode")),
       dddToken_(esConsumes<HGCalDDDConstants, IdealGeometryRecord>(edm::ESInputTag{"", nameSense_})) {
   edm::LogVerbatim("HGCalGeom") << "Test HGCSilicon DetID for " << nameSense_ << " of positions from the file "
-                                << errorFile_;
+                                << errorFile_ << " for the mode " << mode_;
 
   edm::FileInPath filetmp("Geometry/HGCalCommonData/data/" + errorFile_);
   std::string fileName = filetmp.fullPath();
@@ -83,8 +85,8 @@ HGCalWaferIDTester::HGCalWaferIDTester(const edm::ParameterSet& iC)
         int waferV = std::atoi(items[4].c_str());
         int cellU = std::atoi(items[5].c_str());
         int cellV = std::atoi(items[6].c_str());
-        double xx = std::atof(items[7].c_str());
-        double yy = std::atof(items[8].c_str());
+        double xx = std::atof(items[7].c_str()) * CLHEP::cm;
+        double yy = std::atof(items[8].c_str()) * CLHEP::cm;
         HGCSiliconDetId id(det, zp, type, layer, waferU, waferV, cellU, cellV);
         detIds_.emplace_back(id);
         posXY_.emplace_back(std::make_pair(xx, yy));
@@ -101,23 +103,25 @@ HGCalWaferIDTester::HGCalWaferIDTester(const edm::ParameterSet& iC)
 
 void HGCalWaferIDTester::fillDescriptions(edm::ConfigurationDescriptions& descriptions) {
   edm::ParameterSetDescription desc;
-  desc.add<std::string>("nameSense", "HGCalEESensitive");
-  desc.add<std::string>("fileName", "cellIDEE.txt");
-  descriptions.add("hgcalWaferIDTesterEE", desc);
+  desc.add<std::string>("nameSense", "HGCalHESiliconSensitive");
+  desc.add<std::string>("fileName", "cellIDHEF.txt");
+  desc.add<int>("mode", 1);
+  descriptions.add("hgcalWaferIDTesterHEF", desc);
 }
 
 // ------------ method called to produce the data  ------------
 void HGCalWaferIDTester::analyze(const edm::Event& iEvent, const edm::EventSetup& iSetup) {
   const HGCalDDDConstants& hgdc = iSetup.getData(dddToken_);
-  edm::LogVerbatim("HGCalGeom") << "\nStart testing " << nameSense_ << std::endl;
+  edm::LogVerbatim("HGCalGeom") << "\nStart testing " << nameSense_ << " for mode " << mode_ << std::endl;
 
+  bool debug = (mode_ > 0) ? true : false;
   for (unsigned int k = 0; k < detIds_.size(); ++k) {
     int cellU(0), cellV(0), waferType(-1), waferU(0), waferV(0);
     double wt(0);
     double xx = posXY_[k].first;
     double yy = posXY_[k].second;
     int layer = detIds_[k].layer();
-    hgdc.waferFromPosition(xx, yy, layer, waferU, waferV, cellU, cellV, waferType, wt, false, false);
+    hgdc.waferFromPosition(xx, yy, layer, waferU, waferV, cellU, cellV, waferType, wt, false, debug);
     HGCSiliconDetId id(detIds_[k].det(), detIds_[k].zside(), waferType, layer, waferU, waferV, cellU, cellV);
     if (id.rawId() != detIds_[k].rawId())
       edm::LogVerbatim("HGCalGeom") << "Non-matching DetId for [" << k << "] Original " << detIds_[k] << " New " << id;
