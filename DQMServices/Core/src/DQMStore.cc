f271956f8f09b8764a6ff1f9ad6567a171a0dd5f
Luka <Luka.Lambrecht@UGent.be>
2022-12-08 14:33:17 +0100

    change nanoDQMIO ME selection to exact matching

--- DQMServices/Core/src/DQMStore.cc
+++ DQMServices/Core/src/DQMStore.cc
@@ -657,22 +657,26 @@ namespace dqm::implementation {
     auto const& meset = store_->globalMEs_[edm::LuminosityBlockID(runNumber, lumi)];
     auto it = meset.lower_bound(path);
 
-    // decide if the ME should be save din DQMIO based on the list provided
-    bool saveIt = true;
-
-    // rfind can be used as a prefix match.
+    // decide if the ME should be saved in DQMIO based on the list provided
+    bool saveIt = false;
     while (it != meset.end() && (*it)->getFullname().rfind(path_str, 0) == 0) {
       if (store_->doSaveByLumi_ && not store_->MEsToSave_.empty()) {
+        std::string name = (*it)->getFullname();
+        saveIt = false;
         for (std::vector<std::string>::const_iterator ipath = store_->MEsToSave_.begin();
              ipath != store_->MEsToSave_.end();
              ++ipath) {
-          std::string name = (*it)->getFullname();
-          if (name.find(*ipath) != std::string::npos) {
+          std::string nameToSave = *ipath;
+          // option 1 (used in the past): inclusive selection
+          // (store all MEs that contain any of the requested patterns)
+          // if (name.find(nameToSave) != std::string::npos) {
+          // option 2 (current criterion): exact selection
+          // (store only MEs that exactly match a requested pattern)
+          if( name==nameToSave ){
             saveIt = true;
-            //std::cout<<name<<" compared to"<<ipath->data()<<std::endl;
+            // std::cout<<name<<" compared to"<<ipath->data()<<std::endl;
             break;
           }
-          saveIt = false;
         }
       }
 
