10623a96a80580423f0d91a46729bbbda4b1ebd0
Sebastien Wertz <sebastien.wertz@cern.ch>
2022-12-08 11:37:45 +0100

    remove frozen nanoV10 config

--- Configuration/PyReleaseValidation/python/relval_nano.py
+++ Configuration/PyReleaseValidation/python/relval_nano.py
@@ -89,10 +89,6 @@ steps['NANO_data10.6ul18v2']=merge([{'--era':'Run2_2018,run2_nanoAOD_106Xv2',
 ##12.2 INPUT (mc only)
 steps['TTbarMINIAOD12.2'] = {'INPUT':InputInfo(location='STD',
                                                dataSet='/TTToSemiLeptonic_TuneCP5_13p6TeV-powheg-pythia8/Run3Winter22MiniAOD-FlatPU0to70_122X_mcRun3_2021_realistic_v9-v2/MINIAODSIM')}
-steps['NANO_mc12.2_v10']=merge([{'--era':'Run3,run3_nanoAOD_122',
-                                 '--conditions':'auto:phase1_2022_realistic',
-                                 '-s':'NANO:PhysicsTools/NanoAOD/V10/nano_cff,DQM:@nanoAODDQM'},
-                                _NANO_mc])
 steps['NANO_mc12.2']=merge([{'--era':'Run3,run3_nanoAOD_122',
                              '--conditions':'auto:phase1_2022_realistic'},
                             _NANO_mc])
@@ -101,32 +97,20 @@ steps['NANO_mc12.2']=merge([{'--era':'Run3,run3_nanoAOD_122',
 steps['TTbarMINIAOD12.4'] = {'INPUT':InputInfo(location='STD',
                                                ## to be updated as soon as some TTbar appears in a 12.4 campaign
                                                dataSet='/RelValTTbar_14TeV/CMSSW_12_4_9_patch1-124X_mcRun3_2022_realistic_v10_BS2022-v1/MINIAODSIM')}
-steps['NANO_mc12.4_v10']=merge([{'--era':'Run3,run3_nanoAOD_124',
-                                 '--conditions':'auto:phase1_2022_realistic',
-                                 '-s':'NANO:PhysicsTools/NanoAOD/V10/nano_cff,DQM:@nanoAODDQM'},
-                                _NANO_mc])
 steps['NANO_mc12.4']=merge([{'--era':'Run3,run3_nanoAOD_124',
                              '--conditions':'auto:phase1_2022_realistic'},
                             _NANO_mc])
 
 steps['MuonEG2022MINIAOD12.4'] = {'INPUT':InputInfo(location='STD',ls=run3_lumis,
                                                     dataSet='/MuonEG/Run2022D-PromptReco-v2/MINIAOD')}
-steps['NANO_data12.4_v10']=merge([{'--era':'Run3,run3_nanoAOD_124',
-                                   '--conditions':'auto:run3_data',
-                                   '-s':'NANO:PhysicsTools/NanoAOD/V10/nano_cff,DQM:@nanoAODDQM'},
-                                  _NANO_data])
 steps['NANO_data12.4']=merge([{'--era':'Run3,run3_nanoAOD_124',
                                '--conditions':'auto:run3_data'},
                               _NANO_data])
 
-##12.6 workflows ("from scratch")
+##12.6 workflows
 steps['TTBarMINIAOD12.6'] = {'INPUT':InputInfo(location='STD',ls=run3_lumis,
                                                ## this is a dataset from the last pre-release: to be updated much too often IMO
                                                dataSet='/RelValTTbar_14TeV/CMSSW_12_6_0_pre4-PU_125X_mcRun3_2022_realistic_v4-v1/MINIAODSIM')}
-steps['NANO_mc12.6_v10']=merge([{'--era':'Run3',
-                                 '--conditions':'auto:phase1_2022_realistic',
-                                 '-s':'NANO:PhysicsTools/NanoAOD/V10/nano_cff,DQM:@nanoAODDQM'},
-                                _NANO_mc])
 steps['NANO_mc12.6']=merge([{'--era':'Run3',
                              '--conditions':'auto:phase1_2022_realistic'},
                             _NANO_mc])
@@ -144,22 +128,16 @@ workflows[2500.332] = ['NANOdata106Xul18v2', ['MuonEG2018MINIAOD10.6v2', 'NANO_d
 
 ################
 #12.2 input
-workflows[2500.4  ] = ['NANOmc122Xrun3_v10', ['TTbarMINIAOD12.2','NANO_mc12.2_v10', 'HRV_NANO_mc']]
 workflows[2500.401] = ['NANOmc122Xrun3', ['TTbarMINIAOD12.2','NANO_mc12.2', 'HRV_NANO_mc']]
 
 ################
 #12.4 input
-## these are borken because of tau configuration in NANO ATM: they should be re-enabled when a fix gets in
-workflows[2500.5  ] = ['NANOmc124Xrun3_v10', ['TTbarMINIAOD12.4','NANO_mc12.4_v10', 'HRV_NANO_mc']]
 workflows[2500.501] = ['NANOmc124Xrun3', ['TTbarMINIAOD12.4','NANO_mc12.4', 'HRV_NANO_mc']]
-
-workflows[2500.51 ] = ['NANOdata124Xrun3_v10', ['MuonEG2022MINIAOD12.4','NANO_data12.4_v10', 'HRV_NANO_data']]
 workflows[2500.511] = ['NANOdata124Xrun3', ['MuonEG2022MINIAOD12.4','NANO_data12.4', 'HRV_NANO_data']]
 
 ################
 #12.6 workflows
 ## these two workflows should be creating a sample "from scratch" instead of using a pre-release sample as input
-workflows[2500.6  ] = ['NANOmc126X_v10', ['TTBarMINIAOD12.6','NANO_mc12.6_v10', 'HRV_NANO_mc']]
 workflows[2500.601] = ['NANOmc126X', ['TTBarMINIAOD12.6','NANO_mc12.6', 'HRV_NANO_mc']]
 
 ################
