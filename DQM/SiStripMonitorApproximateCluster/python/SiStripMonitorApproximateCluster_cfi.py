0787a7e3db222873ea9ec0b154bec9f5b6dc32f1
mmusich <marco.musich@cern.ch>
2022-12-12 16:26:33 +0100

    add comparisons between Approximated and Standard SiStrip clusters

--- DQM/SiStripMonitorApproximateCluster/python/SiStripMonitorApproximateCluster_cfi.py
+++ DQM/SiStripMonitorApproximateCluster/python/SiStripMonitorApproximateCluster_cfi.py
@@ -3,5 +3,7 @@ import FWCore.ParameterSet.Config as cms
 # SiStripMonitorCluster
 from DQMServices.Core.DQMEDAnalyzer import DQMEDAnalyzer
 SiStripMonitorApproximateCluster = DQMEDAnalyzer("SiStripMonitorApproximateCluster",
-                                                 ClusterProducerStrip = cms.InputTag('hltSiStripClusters2ApproxClusters'),
+                                                 compareClusters = cms.bool(False),
+                                                 ApproxClustersProducer = cms.InputTag('hltSiStripClusters2ApproxClusters'),
+                                                 ClustersProducer = cms.InputTag('siStripClusters'),
                                                  folder = cms.string('SiStripApproximateClusters'))
