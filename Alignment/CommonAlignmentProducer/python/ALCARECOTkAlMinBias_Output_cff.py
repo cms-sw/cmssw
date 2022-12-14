a8ed0404e21ef47a7d8ce1bc350013e84d44ff33
mmusich <marco.musich@cern.ch>
2022-12-14 16:47:58 +0100

    change output commands of Tracker ALCARECOs to take care of phase-2 OT clusters

--- Alignment/CommonAlignmentProducer/python/ALCARECOTkAlMinBias_Output_cff.py
+++ Alignment/CommonAlignmentProducer/python/ALCARECOTkAlMinBias_Output_cff.py
@@ -6,7 +6,11 @@ OutALCARECOTkAlMinBias_noDrop = cms.PSet(
         SelectEvents = cms.vstring('pathALCARECOTkAlMinBias')
     ),
     outputCommands = cms.untracked.vstring(
-        'keep *_ALCARECOTkAlMinBias_*_*', 
+        'keep recoTracks_ALCARECOTkAlMinBias_*_*',
+        'keep recoTrackExtras_ALCARECOTkAlMinBias_*_*',
+        'keep TrackingRecHitsOwned_ALCARECOTkAlMinBias_*_*',
+        'keep SiPixelClusteredmNewDetSetVector_ALCARECOTkAlMinBias_*_*',
+        'keep SiStripClusteredmNewDetSetVector_ALCARECOTkAlMinBias_*_*',
         'keep L1AcceptBunchCrossings_*_*_*',
         'keep L1GlobalTriggerReadoutRecord_gtDigis_*_*',
         'keep *_TriggerResults_*_*',
@@ -25,6 +29,15 @@ _run3_common_extraCommands = ['keep DCSRecord_onlineMetaDataDigis_*_*',
 
 from Configuration.Eras.Modifier_run3_common_cff import run3_common
 run3_common.toModify(OutALCARECOTkAlMinBias_noDrop, outputCommands = _run3_common_removedCommands + _run3_common_extraCommands)
+
+# in Phase2, remove the SiStrip clusters and keep the OT ones instead
+_phase2_common_removedCommands = OutALCARECOTkAlMinBias_noDrop.outputCommands.copy()
+_phase2_common_removedCommands.remove('keep SiStripClusteredmNewDetSetVector_ALCARECOTkAlMinBias_*_*')
+
+_phase2_common_extraCommands = ['keep Phase2TrackerCluster1DedmNewDetSetVector_ALCARECOTkAlMinBias_*_*']
+
+from Configuration.Eras.Modifier_phase2_common_cff import phase2_common
+phase2_common.toModify(OutALCARECOTkAlMinBias_noDrop, outputCommands = _phase2_common_removedCommands + _phase2_common_extraCommands )
 
 OutALCARECOTkAlMinBias = OutALCARECOTkAlMinBias_noDrop.clone()
 OutALCARECOTkAlMinBias.outputCommands.insert(0, "drop *")
