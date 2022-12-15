e93bf4314aa9f8d664277a3d5f40e04be29f62c0
Marino Missiroli <m.missiroli@cern.ch>
2022-12-15 18:14:14 +0100

    added "fillEveryLumiSection" parameter to ThroughputServiceClient

--- HLTrigger/Timer/plugins/ThroughputServiceClient.cc
+++ HLTrigger/Timer/plugins/ThroughputServiceClient.cc
@@ -32,6 +32,7 @@ public:
 private:
   const std::string m_dqm_path;
   const bool m_dqm_merge;
+  const bool m_fillEveryLumiSection;
 
   void dqmEndLuminosityBlock(DQMStore::IBooker &booker,
                              DQMStore::IGetter &getter,
@@ -45,7 +46,8 @@ private:
 
 ThroughputServiceClient::ThroughputServiceClient(edm::ParameterSet const &config)
     : m_dqm_path(config.getUntrackedParameter<std::string>("dqmPath")),
-      m_dqm_merge(config.getUntrackedParameter<bool>("createSummary")) {}
+      m_dqm_merge(config.getUntrackedParameter<bool>("createSummary")),
+      m_fillEveryLumiSection(config.getParameter<bool>("fillEveryLumiSection")) {}
 
 void ThroughputServiceClient::dqmEndJob(DQMStore::IBooker &booker, DQMStore::IGetter &getter) {
   fillSummaryPlots(booker, getter);
@@ -55,7 +57,9 @@ void ThroughputServiceClient::dqmEndLuminosityBlock(DQMStore::IBooker &booker,
                                                     DQMStore::IGetter &getter,
                                                     edm::LuminosityBlock const &,
                                                     edm::EventSetup const &) {
-  fillSummaryPlots(booker, getter);
+  if (m_fillEveryLumiSection) {
+    fillSummaryPlots(booker, getter);
+  }
 }
 
 void ThroughputServiceClient::fillSummaryPlots(DQMStore::IBooker &booker, DQMStore::IGetter &getter) {
@@ -70,9 +74,10 @@ void ThroughputServiceClient::fillSummaryPlots(DQMStore::IBooker &booker, DQMSto
     std::vector<std::string> subdirs = getter.getSubdirs();
     for (auto const &subdir : subdirs) {
       if (boost::regex_match(subdir, running_n_processes)) {
-        if (getter.get(subdir + "/throughput_sourced"))
+        if (getter.get(subdir + "/throughput_sourced")) {
           // the plots are in a per-number-of-processes subfolder
           folders.push_back(subdir);
+        }
       }
     }
   }
@@ -146,6 +151,7 @@ void ThroughputServiceClient::fillDescriptions(edm::ConfigurationDescriptions &d
   edm::ParameterSetDescription desc;
   desc.addUntracked<std::string>("dqmPath", "HLT/Throughput");
   desc.addUntracked<bool>("createSummary", true);
+  desc.add<bool>("fillEveryLumiSection", true);
   descriptions.add("throughputServiceClient", desc);
 }
 
