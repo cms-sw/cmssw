0c7b6d0c8eeafc809aff2366b28321dbc52d9a9c
Andrea Valenzuela <aand.valenzuela@gmail.com>
2022-12-15 14:56:46 +0100

    Factor out singularity check
    Factor out singularity check
    
    Factor out singularity check
    
    run singularity check from bin path
    
    Skip test if nested containers not available
    
    Apply code review

--- RecoEcal/EgammaClusterProducers/test/runtests.sh
+++ RecoEcal/EgammaClusterProducers/test/runtests.sh
@@ -2,4 +2,9 @@
 
 function die { echo $1: status $2 ;  exit $2; }
 
+if ! singularity-check.sh; then
+  echo "missing singularity or missing unprivileged user namespace support"
+  exit 0
+fi
+
 cmsRun ${LOCAL_TEST_DIR}/DRNTest_cfg.py || die 'Failure using cmsRun' $?
