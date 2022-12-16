78e4bc9ba36e2d997dab95c5bb1d3c445782e616
Marino Missiroli <m.missiroli@cern.ch>
2022-12-16 08:34:50 +0100

    HLT menu development for 12_6_X (branch: 13_0_X)

--- HLTrigger/Configuration/test/OnLine_HLT_HIon.py
+++ HLTrigger/Configuration/test/OnLine_HLT_HIon.py
@@ -1,6 +1,6 @@
 # hltGetConfiguration /dev/CMSSW_12_6_0/HIon --full --data --type HIon --unprescale --process HLTHIon --globaltag auto:run3_hlt_HIon --input file:RelVal_Raw_HIon_DATA.root
 
-# /dev/CMSSW_12_6_0/HIon/V11 (CMSSW_12_6_0_pre5)
+# /dev/CMSSW_12_6_0/HIon/V16 (CMSSW_12_6_0)
 
 import FWCore.ParameterSet.Config as cms
 
@@ -12,7 +12,7 @@ process = cms.Process( "HLTHIon" )
 process.ProcessAcceleratorCUDA = ProcessAcceleratorCUDA()
 
 process.HLTConfigVersion = cms.PSet(
-  tableName = cms.string('/dev/CMSSW_12_6_0/HIon/V11')
+  tableName = cms.string('/dev/CMSSW_12_6_0/HIon/V16')
 )
 
 process.transferSystem = cms.PSet( 
