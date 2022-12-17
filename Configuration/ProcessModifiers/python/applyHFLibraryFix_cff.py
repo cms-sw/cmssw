a73c51c0e11df10e44620fde5c51269059768fea
Salavat <abdullin@mail.cern.ch>
2022-12-17 16:07:15 +0100

    Option to use Run3 HFShowerLibrary file for Run2 with Modifier applyHFLibraryFix

---
+++ Configuration/ProcessModifiers/python/applyHFLibraryFix_cff.py
@@ -0,0 +1,7 @@
+import FWCore.ParameterSet.Config as cms
+
+#--- For using Run3 HFSHowerLibrary for Run2 simulation
+#--- instead of the default one with a problem (deficit of entries)
+ 
+applyHFLibraryFix = cms.Modifier()
+
