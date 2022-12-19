f6467a7c752fbd39bde0f820826f2e96e2706d04
W. David Dagenhart <wdd@fnal.gov>
2022-12-19 16:30:40 +0100

    Delete more unneeded comment lines

--- L1Trigger/L1TNtuples/test/test_JetRecoProducer.py
+++ L1Trigger/L1TNtuples/test/test_JetRecoProducer.py
@@ -42,7 +42,6 @@ process.TFileService = cms.Service("TFileService",
 # producer under test
 process.load("L1Trigger.L1TNtuples.l1JetRecoTreeProducer_cfi")
 
-# get corrected jets
 #process.l1RecoTreeProducer.jetTag = cms.untracked.InputTag("ak4PFCHSJetsL2")
 
 process.p = cms.Path(
