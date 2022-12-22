a522c803bf38977ceb346018b882b560ffe3900e
Matti Kortelainen <matti.kortelainen@cern.ch>
2022-12-22 22:14:58 +0100

    Add test for configuration hash staying unchanged

--- FWCore/Integration/test/ProcessAccelerator_t.cpp
+++ FWCore/Integration/test/ProcessAccelerator_t.cpp
@@ -14,10 +14,10 @@
 static constexpr auto s_tag = "[ProcessAccelerator]";
 
 namespace {
-  std::string makeConfig(bool test2Enabled,
-                         std::string_view test1,
-                         std::string_view test2,
-                         std::string_view accelerator) {
+  std::string makeSwitchConfig(bool test2Enabled,
+                               std::string_view test1,
+                               std::string_view test2,
+                               std::string_view accelerator) {
     const std::string appendTest2 = test2Enabled ? "self._enabled.append('test2')" : "";
     return fmt::format(
         R"_(from FWCore.TestProcessor.TestProcess import *
@@ -58,18 +58,74 @@ process.moduleToTest(process.s)
         test1,
         test2);
   }
+
+  std::string makeResolverConfig(bool test2Enabled, std::string_view accelerator, std::string_view variant) {
+    const std::string appendTest2 = test2Enabled ? "self._enabled.append('test2')" : "";
+    const std::string explicitVariant =
+        variant.empty() ? std::string(variant) : fmt::format(", variant=cms.untracked.string('{}')", variant);
+    return fmt::format(
+        R"_(from FWCore.TestProcessor.TestProcess import *
+import FWCore.ParameterSet.Config as cms
+
+class ModuleTypeResolverTest:
+    def __init__(self, accelerators):
+        self._variants = []
+        if "test2" in accelerators:
+            self._variants.append("test2")
+        if "test1" in accelerators:
+            self._variants.append("test1")
+        if len(self._variants) == 0:
+            raise cms.EDMException(cms.edm.errors.UnavailableAccelerator, "No 'test1' or 'test2' accelerator available")
+
+    def plugin(self):
+        return "TestTypeResolverMaker"
+
+    def setModuleVariant(self, module):
+        if "generic::" in module.type_():
+            if hasattr(module, "variant"):
+                if module.variant.value() not in self._variants:
+                    raise cms.EDMException(cms.edm.errors.UnavailableAccelerator, "Module {{}} has the Test variant set explicitly to {{}}, but its accelerator is not available for the job".format(module.label_(), module.variant.value()))
+            else:
+                module.variant = cms.untracked.string(self._variants[0])
+
+class ProcessAcceleratorTest(cms.ProcessAccelerator):
+    def __init__(self):
+        super(ProcessAcceleratorTest,self).__init__()
+        self._labels = ["test1", "test2"]
+        self._enabled = ["test1"]
+        {}
+    def labels(self):
+        return self._labels
+    def enabledLabels(self):
+        return self._enabled
+    def moduleTypeResolver(self, accelerators):
+        return ModuleTypeResolverTest(accelerators)
+
+process = TestProcess()
+process.options.accelerators = ["{}"]
+process.ProcessAcceleratorTest = ProcessAcceleratorTest()
+process.m = cms.EDProducer("generic::IntProducer",
+    valueCpu = cms.int32(1), valueOther = cms.int32(2)
+    {}
+)
+process.moduleToTest(process.m)
+)_",
+        appendTest2,
+        accelerator,
+        explicitVariant);
+  }
 }  // namespace
 
-TEST_CASE("Configuration", s_tag) {
+TEST_CASE("Configuration with SwitchProducer", s_tag) {
   const std::string test1{"cms.EDProducer('IntProducer', ivalue = cms.int32(1))"};
   const std::string test2{"cms.EDProducer('ManyIntProducer', ivalue = cms.int32(2), values = cms.VPSet())"};
 
-  const std::string baseConfig_auto = makeConfig(true, test1, test2, "*");
-  const std::string baseConfig_test1 = makeConfig(true, test1, test2, "test1");
-  const std::string baseConfig_test2 = makeConfig(true, test1, test2, "test2");
-  const std::string baseConfigTest2Disabled_auto = makeConfig(false, test1, test2, "*");
-  const std::string baseConfigTest2Disabled_test1 = makeConfig(false, test1, test2, "test1");
-  const std::string baseConfigTest2Disabled_test2 = makeConfig(false, test1, test2, "test2");
+  const std::string baseConfig_auto = makeSwitchConfig(true, test1, test2, "*");
+  const std::string baseConfig_test1 = makeSwitchConfig(true, test1, test2, "test1");
+  const std::string baseConfig_test2 = makeSwitchConfig(true, test1, test2, "test2");
+  const std::string baseConfigTest2Disabled_auto = makeSwitchConfig(false, test1, test2, "*");
+  const std::string baseConfigTest2Disabled_test1 = makeSwitchConfig(false, test1, test2, "test1");
+  const std::string baseConfigTest2Disabled_test2 = makeSwitchConfig(false, test1, test2, "test2");
 
   SECTION("Configuration hash is not changed") {
     auto pset_auto = edm::readConfig(baseConfig_auto);
@@ -154,5 +210,32 @@ TEST_CASE("Configuration", s_tag) {
     REQUIRE_THROWS_WITH(
         edm::test::TestProcessor(configTest2Disabled_test2),
         Catch::Contains("The system has no compute accelerators that match the patterns") && Catch::Contains("test1"));
+  }
+}
+
+TEST_CASE("Configuration with ModuleTypeResolver", s_tag) {
+  SECTION("Configuration hash is not changed") {
+    auto pset_auto = edm::readConfig(makeResolverConfig(true, "*", ""));
+    auto pset_test1 = edm::readConfig(makeResolverConfig(true, "test1", ""));
+    auto pset_test2 = edm::readConfig(makeResolverConfig(true, "test2", ""));
+    auto pset_test1explicit = edm::readConfig(makeResolverConfig(true, "*", "test1"));
+    auto pset_test2explicit = edm::readConfig(makeResolverConfig(true, "*", "test2"));
+    auto psetTest2Disabled_auto = edm::readConfig(makeResolverConfig(false, "*", ""));
+    auto psetTest2Disabled_test1 = edm::readConfig(makeResolverConfig(false, "test1", ""));
+    REQUIRE_THROWS_WITH(edm::readConfig(makeResolverConfig(false, "test2", "")),
+                        Catch::Contains("UnavailableAccelerator"));
+    pset_auto->registerIt();
+    pset_test1->registerIt();
+    pset_test2->registerIt();
+    pset_test1explicit->registerIt();
+    pset_test2explicit->registerIt();
+    psetTest2Disabled_auto->registerIt();
+    psetTest2Disabled_test1->registerIt();
+    REQUIRE(pset_auto->id() == pset_test1->id());
+    REQUIRE(pset_auto->id() == pset_test2->id());
+    REQUIRE(pset_auto->id() == pset_test1explicit->id());
+    REQUIRE(pset_auto->id() == pset_test2explicit->id());
+    REQUIRE(pset_auto->id() == psetTest2Disabled_auto->id());
+    REQUIRE(pset_auto->id() == psetTest2Disabled_test1->id());
   }
 }
