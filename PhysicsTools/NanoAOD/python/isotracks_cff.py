f97f58f657dd0ee3ae04055575215ad1ae25dfbf
Sebastien Wertz <sebastien.wertz@cern.ch>
2023-01-11 15:40:58 +0100

    use 8 or 16 bit integers where relevant

--- PhysicsTools/NanoAOD/python/isotracks_cff.py
+++ PhysicsTools/NanoAOD/python/isotracks_cff.py
@@ -33,10 +33,10 @@ isoTrackTable = simpleCandidateFlatTableProducer.clone(
         pfRelIso03_chg = Var("pfIsolationDR03().chargedHadronIso/pt",float,doc="PF relative isolation dR=0.3, charged component",precision=10),
         pfRelIso03_all = Var("(pfIsolationDR03().chargedHadronIso + max(pfIsolationDR03().neutralHadronIso + pfIsolationDR03().photonIso - pfIsolationDR03().puChargedHadronIso/2,0.0))/pt",float,doc="PF relative isolation dR=0.3, total (deltaBeta corrections)",precision=10),
         isPFcand = Var("packedCandRef().isNonnull()",bool,doc="if isolated track is a PF candidate"),
-        fromPV = Var("fromPV", int, doc="isolated track comes from PV"),
+        fromPV = Var("fromPV", "int16", doc="isolated track comes from PV"),
         pdgId = Var("pdgId",int,doc="PDG id of PF cand"),
         isHighPurityTrack = Var("isHighPurityTrack",bool,doc="track is high purity"),
-        charge = Var("charge", int, doc="electric charge"),
+        charge = Var("charge", "int16", doc="electric charge"),
     ),
     externalVariables = cms.PSet(
         miniPFRelIso_chg = ExtVar("isoForIsoTk:miniIsoChg",float,doc="mini PF relative isolation, charged component",precision=10),
