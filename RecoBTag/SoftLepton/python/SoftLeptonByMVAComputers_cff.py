a4cd41dcdd64513d82ade16f5ddd3a57064209bd
francescobrivio <francesco.brivio@cern.ch>
2023-01-16 15:18:05 +0100

    modify Run3 PSets for SoftLeptonByMVAComputers instead of individual producers

--- RecoBTag/SoftLepton/python/SoftLeptonByMVAComputers_cff.py
+++ RecoBTag/SoftLepton/python/SoftLeptonByMVAComputers_cff.py
@@ -16,6 +16,15 @@ softPFMuonCommon = cms.PSet(
     useAdaBoost = cms.bool(True)
 )
 
+from Configuration.Eras.Modifier_run3_common_cff import run3_common
+run3_common.toModify(
+    softPFElectronCommon,
+    useCondDB = cms.bool(False),
+).toModify(
+    softPFMuonCommon,
+    useCondDB = cms.bool(False),
+)
+
 softPFElectronComputer = cms.ESProducer("ElectronTaggerESProducer",
     softPFElectronCommon,
     ipSign = cms.string("any"),
@@ -44,12 +53,4 @@ negativeSoftPFMuonComputer = cms.ESProducer("MuonTaggerESProducer",
 positiveSoftPFMuonComputer = cms.ESProducer("MuonTaggerESProducer",
     softPFMuonCommon,
     ipSign = cms.string("positive")
-)
-
-from Configuration.Eras.Modifier_run3_common_cff import run3_common
-run3_common.toModify(softPFElectronComputer,
-                     useCondDB = cms.bool(False),
-)
-run3_common.toModify(softPFMuonComputer,
-                     useCondDB = cms.bool(False),
 )
