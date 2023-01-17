a9304bcab7c5dc4907c80f6328448847680909bc
Kenichi Hatakeyama <kenichi.hatakeyama@cern.ch>
2023-01-16 21:28:33 -0600

    Some renaming. Also use cms::cuda::device::unique_ptr.

--- RecoParticleFlow/PFClusterProducer/src/PFHBHERecHitParamsGPU.cc
+++ RecoParticleFlow/PFClusterProducer/src/PFHBHERecHitParamsGPU.cc
@@ -2,60 +2,74 @@
 
 #include "FWCore/Utilities/interface/typelookup.h"
 #include "HeterogeneousCore/CUDAUtilities/interface/cudaCheck.h"
+#include "HeterogeneousCore/CUDAUtilities/interface/copyAsync.h"
 
 PFHBHERecHitParamsGPU::PFHBHERecHitParamsGPU(edm::ParameterSet const& ps) {
-  auto const& valuesdepthHB = ps.getParameter<std::vector<int>>("depthHB");
-  auto const& valuesdepthHE = ps.getParameter<std::vector<int>>("depthHE");
-  auto const& valuesthresholdE_HB = ps.getParameter<std::vector<double>>("thresholdE_HB");
-  auto const& valuesthresholdE_HE = ps.getParameter<std::vector<double>>("thresholdE_HE");
-  valuesdepthHB_.resize(valuesdepthHB.size());
-  valuesdepthHE_.resize(valuesdepthHE.size());
-  valuesthresholdE_HB_.resize(valuesthresholdE_HB.size());
-  valuesthresholdE_HE_.resize(valuesthresholdE_HE.size());
-  std::copy(valuesdepthHB.begin(), valuesdepthHB.end(), valuesdepthHB_.begin());
-  std::copy(valuesdepthHE.begin(), valuesdepthHE.end(), valuesdepthHE_.begin());
-  std::copy(valuesthresholdE_HB.begin(), valuesthresholdE_HB.end(), valuesthresholdE_HB_.begin());
-  std::copy(valuesthresholdE_HE.begin(), valuesthresholdE_HE.end(), valuesthresholdE_HE_.begin());
+  auto const& valuesDepthHB = ps.getParameter<std::vector<int>>("depthHB");
+  auto const& valuesDepthHE = ps.getParameter<std::vector<int>>("depthHE");
+  auto const& valuesThresholdE_HB = ps.getParameter<std::vector<double>>("thresholdE_HB");
+  auto const& valuesThresholdE_HE = ps.getParameter<std::vector<double>>("thresholdE_HE");
+  depthHB_.resize(valuesDepthHB.size());
+  depthHE_.resize(valuesDepthHE.size());
+  thresholdE_HB_.resize(valuesThresholdE_HB.size());
+  thresholdE_HE_.resize(valuesThresholdE_HE.size());
+  std::copy(valuesDepthHB.begin(), valuesDepthHB.end(), depthHB_.begin());
+  std::copy(valuesDepthHE.begin(), valuesDepthHE.end(), depthHE_.begin());
+  std::copy(valuesThresholdE_HB.begin(), valuesThresholdE_HB.end(), thresholdE_HB_.begin());
+  std::copy(valuesThresholdE_HE.begin(), valuesThresholdE_HE.end(), thresholdE_HE_.begin());
 }
 
 PFHBHERecHitParamsGPU::Product::~Product() {
   // deallocation
-  cudaCheck(cudaFree(valuesdepthHB));
-  cudaCheck(cudaFree(valuesdepthHE));
-  cudaCheck(cudaFree(valuesthresholdE_HB));
-  cudaCheck(cudaFree(valuesthresholdE_HE));
+  cudaCheck(cudaFree(depthHB));
+  cudaCheck(cudaFree(depthHE));
+  cudaCheck(cudaFree(thresholdE_HB));
+  cudaCheck(cudaFree(thresholdE_HE));
 }
 
 PFHBHERecHitParamsGPU::Product const& PFHBHERecHitParamsGPU::getProduct(cudaStream_t cudaStream) const {
   auto const& product = product_.dataForCurrentDeviceAsync(
       cudaStream, [this](PFHBHERecHitParamsGPU::Product& product, cudaStream_t cudaStream) {
+	//
+	product.depthHB = cms::cuda::make_device_unique<int[]>(depthHB_.size(), cudaStream);
+	product.depthHE = cms::cuda::make_device_unique<int[]>(depthHE_.size(), cudaStream);
+	product.thresholdE_HB = cms::cuda::make_device_unique<float[]>(thresholdE_HB_.size(), cudaStream);
+	product.thresholdE_HE = cms::cuda::make_device_unique<float[]>(thresholdE_HE_.size(), cudaStream);
+	//
         // malloc
-        cudaCheck(cudaMalloc((void**)&product.valuesdepthHB, this->valuesdepthHB_.size() * sizeof(int)));
-        cudaCheck(cudaMalloc((void**)&product.valuesdepthHE, this->valuesdepthHE_.size() * sizeof(int)));
-        cudaCheck(cudaMalloc((void**)&product.valuesthresholdE_HB, this->valuesthresholdE_HB_.size() * sizeof(float)));
-        cudaCheck(cudaMalloc((void**)&product.valuesthresholdE_HE, this->valuesthresholdE_HE_.size() * sizeof(float)));
+        //cudaCheck(cudaMalloc((void**)&product.valuesdepthHB, this->valuesdepthHB_.size() * sizeof(int)));
+        //cudaCheck(cudaMalloc((void**)&product.depthHE, this->depthHE_.size() * sizeof(int)));
+        //cudaCheck(cudaMalloc((void**)&product.thresholdE_HB, this->thresholdE_HB_.size() * sizeof(float)));
+        //cudaCheck(cudaMalloc((void**)&product.thresholdE_HE, this->thresholdE_HE_.size() * sizeof(float)));
 
         // transfer
+	cms::cuda::copyAsync(product.depthHB, depthHB_, cudaStream);
+	cms::cuda::copyAsync(product.depthHE, depthHE_, cudaStream);
+	cms::cuda::copyAsync(product.thresholdE_HB, thresholdE_HB_, cudaStream);
+	cms::cuda::copyAsync(product.thresholdE_HE, thresholdE_HE_, cudaStream);
+	//
+	/*
         cudaCheck(cudaMemcpyAsync(product.valuesdepthHB,
                                   this->valuesdepthHB_.data(),
                                   this->valuesdepthHB_.size() * sizeof(int),
                                   cudaMemcpyHostToDevice,
                                   cudaStream));
-        cudaCheck(cudaMemcpyAsync(product.valuesdepthHE,
-                                  this->valuesdepthHE_.data(),
-                                  this->valuesdepthHE_.size() * sizeof(int),
+        cudaCheck(cudaMemcpyAsync(product.depthHB,
+                                  this->depthHE_.data(),
+                                  this->depthHE_.size() * sizeof(int),
                                   cudaMemcpyHostToDevice,
                                   cudaStream));
-        cudaCheck(cudaMemcpyAsync(product.valuesthresholdE_HB,
-                                  this->valuesthresholdE_HB_.data(),
-                                  this->valuesthresholdE_HB_.size() * sizeof(float),
+        cudaCheck(cudaMemcpyAsync(product.thresholdE_HB,
+                                  this->thresholdE_HB_.data(),
+                                  this->thresholdE_HB_.size() * sizeof(float),
                                   cudaMemcpyHostToDevice,
                                   cudaStream));
-        cudaCheck(cudaMemcpyAsync(product.valuesthresholdE_HE,
-                                  this->valuesthresholdE_HE_.data(),
-                                  this->valuesthresholdE_HE_.size() * sizeof(float),
+        cudaCheck(cudaMemcpyAsync(product.thresholdE_HE,
+                                  this->thresholdE_HE_.data(),
+                                  this->thresholdE_HE_.size() * sizeof(float),
                                   cudaMemcpyHostToDevice,
                                   cudaStream));
+	*/
       });
 
   return product;
