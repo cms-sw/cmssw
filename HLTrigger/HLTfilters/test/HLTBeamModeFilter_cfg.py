2cbb4b7340c57d9142dec1997c45952fe6aecadb
Marino Missiroli <marino.missiroli@cern.ch>
2023-01-19 13:29:38 +0100

    remove mentions of Geometry_cff in HLT packages

--- HLTrigger/HLTfilters/test/HLTBeamModeFilter_cfg.py
+++ HLTrigger/HLTfilters/test/HLTBeamModeFilter_cfg.py
@@ -1,101 +1,66 @@
-from __future__ import print_function
 #
 # cfg file to test HLTBeamModeFilter
 # it requires as input:
 #   a RAW data file (with FED 812 included) or 
 #   a digi data file, with L1GlobalTriggerEvmReadoutRecord product valid
 #
-# the input sample is chosen in L1Trigger.GlobalTriggerAnalyzer.UserOptions_cff
-#
 # V.M. Ghete 2010-05-31
 #
-
 import FWCore.ParameterSet.Config as cms
 import sys
 
+useMC = False
+
 process = cms.Process('TestHLTBeamModeFilter')
 
-print('\n')
-from L1Trigger.GlobalTriggerAnalyzer.UserOptions_cff import *
-if errorUserOptions == True :
-    print('\nError returned by UserOptions_cff\n')
-    sys.exit()
+process.source = cms.Source('PoolSource', fileNames = cms.untracked.vstring())
 
+if useMC:
+    gtName = 'auto:run2_mc_l1stage1'
+    process.source.fileNames = ['/store/relval/CMSSW_5_3_6-START53_V14/RelValProdMinBias/GEN-SIM-RAW/v2/00000/52000D8A-032A-E211-BC94-00304867BFA8.root']
+else:
+    gtName = 'auto:run2_data'
+    process.source.fileNames = ['/store/data/Run2012A/MuEG/RAW/v1/000/191/718/14932935-E289-E111-830C-5404A6388697.root']
 
-# source according to data type
-if dataType == 'StreamFile' :
-    process.source = cms.Source("NewEventStreamFileReader", fileNames=readFiles)
-else :        
-    process.source = cms.Source ('PoolSource', fileNames=readFiles, secondaryFileNames=secFiles)
-
+# load and configure modules via GlobalTag
+# https://twiki.cern.ch/twiki/bin/view/CMS/SWGuideFrontierConditions
+process.load("Configuration.StandardSequences.FrontierConditions_GlobalTag_cff")
+from Configuration.AlCa.GlobalTag import GlobalTag
+process.GlobalTag = GlobalTag(process.GlobalTag, gtName, '')
 
 # number of events to be processed and source file
-process.maxEvents = cms.untracked.PSet(
-    input=cms.untracked.int32(maxNumberEvents)
+process.maxEvents.input = 100
+
+from EventFilter.L1GlobalTriggerRawToDigi.l1GtEvmUnpack_cfi import l1GtEvmUnpack as _l1GtEvmUnpack
+process.gtEvmDigis = _l1GtEvmUnpack.clone(
+    EvmGtInputTag = 'rawDataCollector',
+    UnpackBxInEvent = 1,
+    Verbosity = cms.untracked.int32(1) # set EVM unpacker to verbose
 )
 
-#
-# load and configure modules via Global Tag
-
-process.load('Configuration.StandardSequences.Geometry_cff')
-
-# https://twiki.cern.ch/twiki/bin/view/CMS/SWGuideFrontierConditions
-process.load('Configuration.StandardSequences.FrontierConditions_GlobalTag_cff')
-
-process.GlobalTag.globaltag = useGlobalTag+'::All'
-
-
-
-
-
-# this module
 process.load('HLTrigger.HLTfilters.hltBeamModeFilter_cfi')
- 
 # replacing arguments for hltBeamModeFilter
-
-    # InputTag for the L1 Global Trigger EVM readout record
-    #   gtDigis        GT Emulator
-    #   l1GtEvmUnpack  GT EVM Unpacker (default module name) 
-    #   gtEvmDigis     GT EVM Unpacker in RawToDigi standard sequence  
-    #
-    #   cloned GT unpacker in HLT = gtEvmDigis
-process.hltBeamModeFilter.L1GtEvmReadoutRecordTag = cms.InputTag("gtEvmDigis")
-
+#  InputTag for the L1 Global Trigger EVM readout record
+#   gtDigis        GT Emulator
+#   l1GtEvmUnpack  GT EVM Unpacker (default module name)
+#   gtEvmDigis     GT EVM Unpacker in RawToDigi standard sequence
+#
+#   cloned GT unpacker in HLT = gtEvmDigis
+process.hltBeamModeFilter.L1GtEvmReadoutRecordTag = 'gtEvmDigis'
 # vector of allowed beam modes (see enumeration in header file for implemented values)
 # default value: 11 (STABLE)
-#process.hltBeamModeFilter.AllowedBeamMode = cms.vuint32(11)
-process.hltBeamModeFilter.AllowedBeamMode = cms.vuint32(9, 10, 11)
+#process.hltBeamModeFilter.AllowedBeamMode = [11]
+process.hltBeamModeFilter.AllowedBeamMode = [9, 10, 11]
 
-
-# path to be run
-# for RAW data, run first the RAWTODIGI 
-if (dataType == 'RAW') and not (useRelValSample) :
-    process.load('Configuration/StandardSequences/RawToDigi_Data_cff')
-    process.gtEvmDigis.UnpackBxInEvent = cms.int32(1)
-    # set EVM unpacker to verbose
-    process.gtEvmDigis.Verbosity = cms.untracked.int32(1)
-
-    process.p = cms.Path(process.RawToDigi * process.hltBeamModeFilter)
-
-elif (dataType == 'RAW') and (useRelValSample) :
-    process.load('Configuration/StandardSequences/RawToDigi_cff')
-    process.gtEvmDigis.UnpackBxInEvent = cms.int32(1)
-    # set EVM unpacker to verbose
-    process.gtEvmDigis.Verbosity = cms.untracked.int32(1)
-
-    process.p = cms.Path(process.RawToDigi * process.hltBeamModeFilter)
-    
-else :        
-    # it does not run on standard RECO, but I let the path here (exit in the filter) 
-    process.p = cms.Path(process.hltBeamModeFilter)
+process.p = cms.Path(process.gtEvmDigis * process.hltBeamModeFilter)
 
 # services
 
 # Message Logger
 process.load('FWCore.MessageService.MessageLogger_cfi')
 process.MessageLogger.debugModules = ['gtEvmDigis', 'hltBeamModeFilter']
-process.MessageLogger.L1GlobalTriggerEvmRawToDigi=dict()
-process.MessageLogger.HLTBeamModeFilter=dict()
+process.MessageLogger.L1GlobalTriggerEvmRawToDigi = dict()
+process.MessageLogger.HLTBeamModeFilter = dict()
 
 process.MessageLogger.cerr.threshold = 'DEBUG'
 #process.MessageLogger.cerr.threshold = 'INFO'
@@ -109,5 +74,3 @@ process.MessageLogger.cerr.ERROR = cms.untracked.PSet( limit = cms.untracked.int
 
 process.MessageLogger.cerr.L1GlobalTriggerEvmRawToDigi = cms.untracked.PSet( limit = cms.untracked.int32(-1) )
 process.MessageLogger.cerr.HLTBeamModeFilter = cms.untracked.PSet( limit = cms.untracked.int32(-1) )
-
-
