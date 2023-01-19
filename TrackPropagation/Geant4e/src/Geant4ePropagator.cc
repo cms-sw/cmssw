396444d0289f9bbfaa941c1cd4602a125f423b42
Vladimir <Vladimir.Ivantchenko@cern.ch>
2023-01-19 22:02:29 +0100

    make update on top of master

--- TrackPropagation/Geant4e/src/Geant4ePropagator.cc
+++ TrackPropagation/Geant4e/src/Geant4ePropagator.cc
@@ -28,9 +28,10 @@
 #include "G4GeometryTolerance.hh"
 #include "G4SteppingControl.hh"
 #include "G4TransportationManager.hh"
-#include "G4RunManagerKernel.hh"
 #include "G4Tubs.hh"
 #include "G4UImanager.hh"
+#include "G4ErrorPropagationNavigator.hh"
+#include "G4RunManagerKernel.hh"
 
 // CLHEP
 #include "CLHEP/Units/GlobalSystemOfUnits.h"
@@ -74,10 +75,11 @@ Geant4ePropagator::~Geant4ePropagator() {
 
 void Geant4ePropagator::ensureGeant4eIsInitilized(bool forceInit) const {
   LogDebug("Geant4e") << "ensureGeant4eIsInitilized called" << std::endl;
-  if (forceInit) {
-    LogDebug("Geant4e") << "Initializing G4 propagator";
+  if ( forceInit ) {
+    LogDebug("Geant4e") << "Initializing G4 propagator" << std::endl;
 
     //G4UImanager::GetUIpointer()->ApplyCommand("/exerror/setField -10. kilogauss");
+
     auto man = G4RunManagerKernel::GetRunManagerKernel();
     man->SetVerboseLevel(0);
     theG4eManager->InitGeant4e();
@@ -310,10 +312,17 @@ std::pair<TrajectoryStateOnSurface, double> Geant4ePropagator::propagateGeneric(
 
   theG4eManager->InitTrackPropagation();
 
+  // re-initialize navigator to avoid mismatches and/or segfaults
+  theG4eManager->GetErrorPropagationNavigator()->LocateGlobalPointAndSetup(
+      g4InitPos, &g4InitMom, /*pRelativeSearch = */ false, /*ignoreDirection = */ false);
+
   bool continuePropagation = true;
   while (continuePropagation) {
     iterations++;
     LogDebug("Geant4e") << std::endl << "step count " << iterations << " step length " << finalPathLength;
+
+    // re-initialize navigator to avoid mismatches and/or segfaults
+    theG4eManager->GetErrorPropagationNavigator()->LocateGlobalPointWithinVolume(g4eTrajState.GetPosition());
 
     const int ierr = theG4eManager->PropagateOneStep(&g4eTrajState, mode);
 
