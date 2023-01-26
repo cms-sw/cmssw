841ce01a3990f73c530a1f1862adde0dda87ef78
Patrick Gartung <gartung@fnal.gov>
2023-01-26 22:50:04 +0100

    Jemalloc heap profiling output name does not use pipes

--- Validation/Performance/python/JeProfInfo.py
+++ Validation/Performance/python/JeProfInfo.py
@@ -5,7 +5,7 @@ def customise(process):
     process.JeProfService = cms.Service("JeProfService",
         reportFirstEvent            = cms.untracked.int32(1),  #Dump first event for baseline studies
         reportEventInterval         = cms.untracked.int32( ((process.maxEvents.input.value()-1)//2) ), # dump in the middle of the run
-        reportToFileAtPostEvent     = cms.untracked.string("| gzip -c > Jeprof.heap.%I.gz")
+        reportToFileAtPostEvent     = cms.untracked.string("jeprof.%I.heap")
         )
     
     return(process)
