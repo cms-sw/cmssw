9592516aa9c4f08ad2c5d72913e18ccfc27a2200
Brent R. Yates <brentRyates@gmail.com>
2023-01-29 15:33:32 -0500

    MP fix disk deltar (#200)
    * Fixed ideltar calcualtion to match HLS
    
    * Minor fixes
    
    * Ran code-format, replaced numerical constant
    
    ---------
    
    Co-authored-by: bryates <brent.yates@email.ucr.edu>

--- L1Trigger/TrackFindingTracklet/src/MatchProcessor.cc
+++ L1Trigger/TrackFindingTracklet/src/MatchProcessor.cc
@@ -352,7 +352,7 @@ void MatchProcessor::execute(unsigned int iSector, double phimin) {
 
         unsigned int iphi = (fpgaphi.value() >> (fpgaphi.nbits() - nvmbits_)) & (nvmbins_ - 1);
 
-        int nextrabits = 2;
+        constexpr int nextrabits = 2;
         int overlapbits = nvmbits_ + nextrabits;
 
         unsigned int extrabits = fpgaphi.bits(fpgaphi.nbits() - overlapbits - nextrabits, nextrabits);
@@ -435,7 +435,7 @@ void MatchProcessor::execute(unsigned int iSector, double phimin) {
         VMStubsMEMemory* stubmem = vmstubs_[0];
         bool usefirstMinus = stubmem->nStubsBin(ivmMinus * nbins + slot) != 0;
         bool usesecondMinus = (second && (stubmem->nStubsBin(ivmMinus * nbins + slot + 1) != 0));
-        bool usefirstPlus = ivmPlus != ivmMinus && stubmem->nStubsBin(ivmPlus * nbins + slot) != 0;
+        bool usefirstPlus = ivmPlus != ivmMinus && (stubmem->nStubsBin(ivmPlus * nbins + slot) != 0);
         bool usesecondPlus = ivmPlus != ivmMinus && (second && (stubmem->nStubsBin(ivmPlus * nbins + slot + 1) != 0));
 
         good_ = usefirstPlus || usesecondPlus || usefirstMinus || usesecondMinus;
@@ -583,7 +583,7 @@ bool MatchProcessor::matchCalculator(Tracklet* tracklet, const Stub* fpgastub, b
     // Update the "best" values
     if (imatch) {
       best_ideltaphi_barrel = std::abs(ideltaphi);
-      best_ideltaz_barrel = std::abs(ideltaz);
+      best_ideltaz_barrel = std::abs(ideltaz << dzshift_);
     }
 
     if (settings_.debugTracklet()) {
@@ -671,7 +671,8 @@ bool MatchProcessor::matchCalculator(Tracklet* tracklet, const Stub* fpgastub, b
       }
     }
 
-    int ideltar = (irstub * settings_.kr()) / settings_.krprojshiftdisk() - ir;
+    constexpr int diff_bits = 1;
+    int ideltar = (irstub >> diff_bits) - ir;
 
     if (!stub->isPSmodule()) {
       int ialpha = fpgastub->alpha().value();
