c53ae814dc92bcf865d4229f9814dd058d38b07a
Sunanda <sunanda.banerjee@cern.ch>
2023-01-30 09:15:21 +0100

    Test the TB topology for HGCal

---
+++ Geometry/CaloTopology/test/testHGCalTBTopology_cfg.py
@@ -0,0 +1,40 @@
+import FWCore.ParameterSet.Config as cms
+
+process = cms.Process("PROD")
+process.load("SimGeneral.HepPDTESSource.pdt_cfi")
+process.load("Geometry.HGCalTBCommonData.testTB181V1XML_cfi")
+process.load("Geometry.HGCalTBCommonData.hgcalTBParametersInitialization_cfi")
+process.load("Geometry.HGCalTBCommonData.hgcalTBNumberingInitialization_cfi")
+process.load("Geometry.CaloEventSetup.HGCalTBTopology_cfi")
+process.load("FWCore.MessageService.MessageLogger_cfi")
+process.load("Geometry.CaloTopology.hgcalTBTopologyTester_cfi")
+
+if 'MessageLogger' in process.__dict__:
+    process.MessageLogger.HGCalGeom=dict()
+
+process.load("IOMC.RandomEngine.IOMC_cff")
+process.RandomNumberGeneratorService.generator.initialSeed = 456789
+
+process.source = cms.Source("EmptySource")
+
+process.generator = cms.EDProducer("FlatRandomEGunProducer",
+    PGunParameters = cms.PSet(
+        PartID = cms.vint32(14),
+        MinEta = cms.double(-3.5),
+        MaxEta = cms.double(3.5),
+        MinPhi = cms.double(-3.14159265359),
+        MaxPhi = cms.double(3.14159265359),
+        MinE   = cms.double(9.99),
+        MaxE   = cms.double(10.01)
+    ),
+    AddAntiParticle = cms.bool(False),
+    Verbosity       = cms.untracked.int32(0),
+    firstRun        = cms.untracked.uint32(1)
+)
+
+process.maxEvents = cms.untracked.PSet(
+    input = cms.untracked.int32(1)
+)
+
+process.p1 = cms.Path(process.generator*
+                      process.hgcalTBTopologyTesterEE)
