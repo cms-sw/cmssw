9d605458d635a2acf1ce4fb49826000767326bae
Sunanda <sunanda.banerjee@cern.ch>
2023-01-31 09:46:22 +0100

    Changes made as suggested by Andrea

--- Geometry/CaloTopology/python/hgcalTBTopologyTester_cfi.py
+++ Geometry/CaloTopology/python/hgcalTBTopologyTester_cfi.py
@@ -3,8 +3,8 @@ import FWCore.ParameterSet.Config as cms
 from Geometry.CaloTopology.hgcalTBTopologyTesterEE_cfi import *
 
 hgcalTBTopologyTesterHEF = hgcalTBTopologyTesterEE.clone(
-    detectorName = cms.string("HGCalHESiliconSensitive"),
-    types        = cms.vint32(0,0,1,1,1,1,2,2,2),
-    layers       = cms.vint32(1,2,9,3,4,5,6,7,8),
-    sector       = cms.vint32(2,3,3,4,5,6,7,8,9),
-    cells        = cms.vint32(0,10,15,8,8,10,10,15,15))
+    detectorName = "HGCalHESiliconSensitive",
+    types        = [0, 0, 1, 1, 1, 1, 2, 2, 2],
+    layers       = [1, 2, 9, 3, 4, 5, 6, 7, 8],
+    sector       = [2, 3, 3, 4, 5, 6, 7, 8, 9],
+    cells        = [0,10,15, 8, 8,10,10,15,15] )
