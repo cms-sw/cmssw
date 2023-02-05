c101a4e9726228a33fe96411676d739d69eb28b6
mmusich <marco.musich@cern.ch>
2023-02-05 16:27:26 +0100

    improve fillDescriptions of EopTreeWriter

--- Alignment/OfflineValidation/python/eopTreeWriter_cfi.py
+++ Alignment/OfflineValidation/python/eopTreeWriter_cfi.py
@@ -1,5 +1,6 @@
 import FWCore.ParameterSet.Config as cms
 
-energyOverMomentumTree = cms.EDAnalyzer('EOP',
-             src = cms.InputTag('TrackRefitter')
+from Alignment.OfflineValidation.eopTreeWriter_cfi import eopTreeWriter as _eopTreeWriter
+energyOverMomentumTree = _eopTreeWriter.clone(
+    src = 'TrackRefitter'
 )
