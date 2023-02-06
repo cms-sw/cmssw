145017e1f66ab34af22350b6734c6d42b59de3eb
Emyr Clement <emyr.clement@bristol.ac.uk>
2023-02-06 21:14:15 +0100

    Fix name of extended tracks input.

--- L1Trigger/Phase2L1ParticleFlow/python/l1tPFTracksFromL1Tracks_cfi.py
+++ L1Trigger/Phase2L1ParticleFlow/python/l1tPFTracksFromL1Tracks_cfi.py
@@ -26,7 +26,7 @@ l1tPFTracksFromL1Tracks = cms.EDProducer("PFTrackProducerFromL1Tracks",
 )
 
 l1tPFTracksFromL1TracksExtended = l1tPFTracksFromL1Tracks.clone(
-    L1TrackTag = cms.InputTag("TTTracksFromExtendedTrackletEmulation", "Level1TTTracks"),
+    L1TrackTag = cms.InputTag("l1tTTTracksFromExtendedTrackletEmulation", "Level1TTTracks"),
     nParam = 5,
     qualityBits = cms.vstring( 
         "momentum.perp > 2 && getStubRefs.size >= 4 && chi2Red < 15 && POCA.x < 1.0 && POCA.x > -1.0 && POCA.y < 1.0 && POCA.y > -1.0",
