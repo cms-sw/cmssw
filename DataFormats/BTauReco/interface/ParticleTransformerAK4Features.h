d2e3ef24401101a808dc9da7f6d121756ef5e4d3
Annika Stein <annika-stein@t-online.de>
2023-02-07 19:40:19 +0100

    fix: remove commented-out lines and simplify classes_def.xml without intermediate versions

--- DataFormats/BTauReco/interface/ParticleTransformerAK4Features.h
+++ DataFormats/BTauReco/interface/ParticleTransformerAK4Features.h
@@ -1,7 +1,6 @@
 #ifndef DataFormats_BTauReco_ParticleTransformerAK4Features_h
 #define DataFormats_BTauReco_ParticleTransformerAK4Features_h
 
-//#include "DataFormats/BTauReco/interface/JetFeatures.h"
 #include "DataFormats/BTauReco/interface/SecondaryVertexFeatures.h"
 #include "DataFormats/BTauReco/interface/NeutralCandidateFeatures.h"
 #include "DataFormats/BTauReco/interface/ChargedCandidateFeatures.h"
@@ -12,14 +11,12 @@ namespace btagbtvdeep {
 
   class ParticleTransformerAK4Features {
   public:
-    //JetFeatures jet_features;
 
     std::vector<SecondaryVertexFeatures> sv_features;
 
     std::vector<NeutralCandidateFeatures> n_pf_features;
     std::vector<ChargedCandidateFeatures> c_pf_features;
 
-    //std::size_t npv;  // used by deep flavour
   };
 
 }  // namespace btagbtvdeep
