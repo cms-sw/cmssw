856aa8c54fa3b3e0a6f291aa565c75b825b46d41
Sunanda <sunanda.banerjee@cern.ch>
2023-02-10 12:56:46 +0100

    Use old default SL

--- Geometry/HcalSimData/python/HFParameters_cff.py
+++ Geometry/HcalSimData/python/HFParameters_cff.py
@@ -41,5 +41,5 @@ run2_common.toModify( HFShowerBlock, ProbMax = 0.5 )
 ## Change for the latest HFShowerLibrary file for Run 3
 ##
 from Configuration.Eras.Modifier_run3_HFSL_cff import run3_HFSL
-run3_HFSL.toModify( HFLibraryFileBlock, FileName = 'SimG4CMS/Calo/data/HFShowerLibrary_run3_v7.root', FileVersion = 3 )
+run3_HFSL.toModify( HFLibraryFileBlock, FileName = 'SimG4CMS/Calo/data/HFShowerLibrary_run3_v6.root', FileVersion = 2 )
 run3_HFSL.toModify( HFShowerBlock, EqualizeTimeShift = True )
