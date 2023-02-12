74b0b8303b0955ea2d1ceffdc7693bfae5ad7356
Jihoon Shin <sjhsjs987@gmail.com>
2023-02-13 00:29:49 +0900

    Fix PUID Variable bug and implement PUPPI Weight using a Value Map

--- PhysicsTools/NanoAOD/python/custom_jme_cff.py
+++ PhysicsTools/NanoAOD/python/custom_jme_cff.py
@@ -274,7 +274,7 @@ def AddPileUpJetIDVars(proc, jetName="", jetSrc="", jetTableName="", jetTaskName
       vertexes  = "offlineSlimmedPrimaryVertices",
       inputIsCorrected = True,
       applyJec  = False,
-      usePuppi = True if "PUPPI" in jetName.upper() else False
+      srcConstituentWeights = "packedPFCandidatespuppi" if "PUPPI" in jetName.upper() else ""
     )
   )
   getattr(proc,jetTaskName).add(getattr(proc, puJetIdVarsCalculator))
