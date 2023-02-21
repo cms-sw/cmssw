f732d4ff385477fc08ec4de29c695e1034e3dd39
mmusich <marco.musich@cern.ch>
2023-02-21 12:00:20 +0100

    add a fillDescriptions method for TrackerTrackHitFilter

--- RecoTracker/FinalTrackSelectors/python/TrackerTrackHitFilter_cff.py
+++ RecoTracker/FinalTrackSelectors/python/TrackerTrackHitFilter_cff.py
@@ -1,43 +1,46 @@
 import FWCore.ParameterSet.Config as cms
 
-TrackerTrackHitFilter = cms.EDProducer("TrackerTrackHitFilter",
-                              src = cms.InputTag("generalTracks"),
-                              minimumHits =cms.uint32(3), ##min number of hits for refit
-                              ## # layers to remove
-                              commands = cms.vstring(
-                                         "drop PXB",  "drop PXE"   ### same works for TIB, TID, TOB, TEC,
-                                        #"drop TIB 3",  ## you can also drop specific layers/wheel/disks
-                                        #"keep PXB 3",  ## you can also 'keep' some layer after
-                                                        ##having dropped the whole structure
-                               ),
-                              
-                              ###list of individual detids to turn off, in addition to the structures above
-                              detsToIgnore = cms.vuint32( ),
-                              
-                              ### what to do with invalid hits
-                              replaceWithInactiveHits =cms.bool(False), ## instead of removing hits replace
-                                                                        ## them with inactive hits, so you still
-                                                                        ## consider the multiple scattering
-                              stripFrontInvalidHits   =cms.bool(False),   ## strip invalid & inactive hits from
-                              stripBackInvalidHits    =cms.bool(False),   ## any end of the track
-                              
-                              stripAllInvalidHits = cms.bool(False), ##not sure if it's better 'true' or 'false'
-                                                                     ## might be dangerous to turn on
-                                                                     ## as you will forget about MS
+from RecoTracker.FinalTrackSelectors.trackerTrackHitFilter_cfi import trackerTrackHitFilter as _trackerTrackHitFilter
+TrackerTrackHitFilter = _trackerTrackHitFilter.clone(
+    src = "generalTracks",
+    minimumHits = 3, ##min number of hits for refit
+    ## # layers to remove
+    commands = ["drop PXB",  "drop PXE"],   ### same works for TIB, TID, TOB, TEC,
+                                            # "drop TIB 3",  ## you can also drop specific layers/wheel/disks
+                                            # "keep PXB 3",  ## you can also 'keep' some layer after
+                                            # having dropped the whole structure
 
-                              ### hit quality cuts
-                              rejectBadStoNHits = cms.bool(False),
-                              CMNSubtractionMode = cms.string("Median"), ## "TT6"
-                              StoNcommands = cms.vstring(
-                                                         "TIB 1.0 ", "TOB 1.0 999.0"
-                                                        ),
-                              useTrajectories=cms.bool(False),
-                              rejectLowAngleHits=cms.bool(False),
-                              TrackAngleCut=cms.double(0.25),       ## in radians
-                              tagOverlaps=cms.bool(False),
-                              usePixelQualityFlag=cms.bool(False),
-                              PxlTemplateProbXYCut=cms.double(0.000125), #recommended by experts
-                              PxlTemplateProbXYChargeCut=cms.double(-99.), #recommended by experts
-                              PxlTemplateqBinCut =cms.vint32(0, 3),       #recommended by experts
-                              PxlCorrClusterChargeCut = cms.double(-999.0)   
-                             )####end of module 
+    ###list of individual detids to turn off, in addition to the structures above
+    detsToIgnore = [],
+
+    ### what to do with invalid hits
+    replaceWithInactiveHits = False, ## instead of removing hits replace
+                                     ## them with inactive hits, so you still
+                                     ## consider the multiple scattering
+
+    stripFrontInvalidHits = False,   ## strip invalid & inactive hits from
+    stripBackInvalidHits = False,    ## any end of the track
+
+    stripAllInvalidHits = False, ## not sure if it's better 'true' or 'false'
+                                 ## might be dangerous to turn on
+                                 ## as you will forget about MS
+
+    ### hit quality cuts
+    isPhase2 = False,
+    rejectBadStoNHits = False,
+    CMNSubtractionMode = "Median", ## "TT6"
+    StoNcommands = ["TIB 1.0 ", "TOB 1.0 999.0"],
+    useTrajectories = False,
+    rejectLowAngleHits = False,
+    TrackAngleCut = 0.25,       ## in radians
+    tagOverlaps= False,
+    usePixelQualityFlag = False,
+    PxlTemplateProbXYCut = 0.000125,   # recommended by experts
+    PxlTemplateProbXYChargeCut = -99., # recommended by experts
+    PxlTemplateqBinCut = [0, 3],       # recommended by experts
+    PxlCorrClusterChargeCut = -999.0
+) #### end of module
+
+from Configuration.Eras.Modifier_phase2_tracker_cff import phase2_tracker
+phase2_tracker.toModify(TrackerTrackHitFilter,
+                        isPhase2 = True)
