89c892f560e3a7ff725dabef8b0f2ae064b70018
Florian Lorkowski <florian.lorkowski@desy.de>
2023-02-23 12:00:45 +0100

    Make PFRecHit CPU/Alpaka comparion more rigorous

--- RecoParticleFlow/PFRecHitProducer/plugins/PFRecHitProducerTest.cc
+++ RecoParticleFlow/PFRecHitProducer/plugins/PFRecHitProducerTest.cc
@@ -11,7 +11,6 @@
 #include "FWCore/Framework/interface/EventSetup.h"
 #include "FWCore/Framework/interface/Frameworkfwd.h"
 #include "FWCore/ParameterSet/interface/ParameterSet.h"
-#include "FWCore/PluginManager/interface/ModuleDef.h"
 #include "FWCore/ParameterSet/interface/ConfigurationDescriptions.h"
 #include "DataFormats/ParticleFlowReco/interface/PFRecHitHostCollection.h"
 
@@ -23,15 +22,18 @@
 class PFRecHitProducerTest : public DQMEDAnalyzer {
 public:
   PFRecHitProducerTest(edm::ParameterSet const& conf);
-  ~PFRecHitProducerTest() override = default;
+  ~PFRecHitProducerTest() override;
   void analyze(edm::Event const& e, edm::EventSetup const& c) override;
   void bookHistograms(DQMStore::IBooker&, edm::Run const&, edm::EventSetup const&) override {};
   //static void fillDescriptions(edm::ConfigurationDescriptions& descriptions);
 
 private:
+  void DumpEvent(const reco::PFRecHitCollection& pfRecHitsCPU, const PFRecHitHostCollection::ConstView& pfRecHitsAlpaka);
+
   edm::EDGetTokenT<edm::SortedCollection<HBHERecHit>> recHitsToken;
   edm::EDGetTokenT<reco::PFRecHitCollection> pfRecHitsTokenCPU;
   edm::EDGetTokenT<PFRecHitHostCollection> pfRecHitsTokenAlpaka;
+  int32_t num_events = 0, num_errors = 0;
 };
 
 PFRecHitProducerTest::PFRecHitProducerTest(const edm::ParameterSet& conf)
@@ -43,11 +45,11 @@ PFRecHitProducerTest::PFRecHitProducerTest(const edm::ParameterSet& conf)
           consumes<PFRecHitHostCollection>(conf.getUntrackedParameter<edm::InputTag>("pfRecHitsSourceAlpaka")))
      {}
 
-void PFRecHitProducerTest::analyze(edm::Event const& event, edm::EventSetup const& c) {
-  static int cnt = 0;
-  if (cnt++ >= 1)
-    return;
+PFRecHitProducerTest::~PFRecHitProducerTest() {
+  printf("PFRecHitProducerTest has compared %u events and found %u problems\n", num_events, num_errors);
+}
 
+void PFRecHitProducerTest::analyze(edm::Event const& event, edm::EventSetup const& c) {
   // Rec Hits
   //edm::Handle<edm::SortedCollection<HBHERecHit>> recHits;
   //event.getByToken(recHitsToken, recHits);
@@ -57,27 +59,63 @@ void PFRecHitProducerTest::analyze(edm::Event const& event, edm::EventSetup cons
   //  printf("recHit %4lu %u\n", i, recHits->operator[](i).id().rawId());
 
   // PF Rec Hits
-  // paste <(grep "^CPU" validation.log | sort -nk3) <(grep "^GPU" validation.log | sort -nk3) | awk '$3!=$13 || $4!=$14 || $5!=$15 || $6!=$16 || $9!=$19 {print}' | head
-  edm::Handle<reco::PFRecHitCollection> pfRecHitsCPU;
+  edm::Handle<reco::PFRecHitCollection> pfRecHitsCPUlegacy;
   edm::Handle<PFRecHitHostCollection> pfRecHitsAlpakaSoA;
-  event.getByToken(pfRecHitsTokenCPU, pfRecHitsCPU);
+  event.getByToken(pfRecHitsTokenCPU, pfRecHitsCPUlegacy);
   event.getByToken(pfRecHitsTokenAlpaka, pfRecHitsAlpakaSoA);
-  auto& pfRecHitsAlpaka = pfRecHitsAlpakaSoA->view();
+  
+  const reco::PFRecHitCollection& pfRecHitsCPU = *pfRecHitsCPUlegacy;
+  const PFRecHitHostCollection::ConstView& pfRecHitsAlpaka = pfRecHitsAlpakaSoA->const_view();
 
-  fprintf(stdout, "Found %zd/%d pfRecHits with CPU/Alpaka\n", pfRecHitsCPU->size(), pfRecHitsAlpaka.size());
-  fprintf(stderr, "Found %zd/%d pfRecHits with CPU/Alpaka\n", pfRecHitsCPU->size(), pfRecHitsAlpaka.size());
-  for (size_t i = 0; i < pfRecHitsCPU->size(); i++)
+  bool error = false;
+  if(pfRecHitsCPU.size() != pfRecHitsAlpaka.size())
+    error = true;
+  else
+  {
+    for (size_t i = 0; i < pfRecHitsCPU.size(); i++)
+    {
+      const uint32_t detId = pfRecHitsCPU[i].detId();
+      bool detId_found = false;
+      for (size_t j = 0; j < pfRecHitsAlpaka.size(); j++)
+        if(detId == pfRecHitsAlpaka[j].detId())
+        {
+          if(detId_found)
+            error = true;
+          detId_found = true;
+          if(pfRecHitsCPU[i].depth() != pfRecHitsAlpaka[j].depth()
+            || pfRecHitsCPU[i].layer() != pfRecHitsAlpaka[j].layer()
+            || pfRecHitsCPU[i].time() != pfRecHitsAlpaka[j].time()
+            || pfRecHitsCPU[i].energy() != pfRecHitsAlpaka[j].energy())
+            error = true;
+        }
+        if(!detId_found)
+          error = true;
+    }
+  }
+
+  if(error)
+  {
+    //if(num_errors == 0)
+    //  DumpEvent(pfRecHitsCPU, pfRecHitsAlpaka);
+    num_errors++;
+  }
+  num_events++;
+}
+
+void PFRecHitProducerTest::DumpEvent(const reco::PFRecHitCollection& pfRecHitsCPU, const PFRecHitHostCollection::ConstView& pfRecHitsAlpaka) {
+  printf("Found %zd/%d pfRecHits with CPU/Alpaka\n", pfRecHitsCPU.size(), pfRecHitsAlpaka.size());
+  for (size_t i = 0; i < pfRecHitsCPU.size(); i++)
     printf("CPU %4lu %u %d %d %u : %f %f (%f,%f,%f)\n",
            i,
-           pfRecHitsCPU->at(i).detId(),
-           pfRecHitsCPU->at(i).depth(),
-           pfRecHitsCPU->at(i).layer(),
-           pfRecHitsCPU->at(i).neighbours().size(),
-           pfRecHitsCPU->at(i).time(),
-           pfRecHitsCPU->at(i).energy(),
-           0.,  //pfRecHitsCPU->at(i).position().x(),
-           0.,  //pfRecHitsCPU->at(i).position().y(),
-           0.   //pfRecHitsCPU->at(i).position().z()
+           pfRecHitsCPU[i].detId(),
+           pfRecHitsCPU[i].depth(),
+           pfRecHitsCPU[i].layer(),
+           pfRecHitsCPU[i].neighbours().size(),
+           pfRecHitsCPU[i].time(),
+           pfRecHitsCPU[i].energy(),
+           0.,  //pfRecHitsCPU[i].position().x(),
+           0.,  //pfRecHitsCPU[i].position().y(),
+           0.   //pfRecHitsCPU[i].position().z()
     );
   for (size_t i = 0; i < pfRecHitsAlpaka.size(); i++)
     printf("Alpaka %4lu %u %d %d %u : %f %f (%f,%f,%f)\n",
@@ -93,6 +131,7 @@ void PFRecHitProducerTest::analyze(edm::Event const& event, edm::EventSetup cons
            0.   //pfRecHitsGPU->at(i).position().z()
     );
 }
+
 
 // void PFRecHitProducerTest::fillDescriptions(edm::ConfigurationDescriptions& descriptions) {
 //   edm::ParameterSetDescription desc;
