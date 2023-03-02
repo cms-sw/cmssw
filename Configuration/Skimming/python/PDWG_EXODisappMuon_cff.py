244395bb6a38a568509219452d18499331090797
Michael Revering <michael.martin.r@cern.ch>
2023-03-02 11:56:23 -0600

    Added horeco and CSC2DrecHits to disappearing muon skim event data

--- Configuration/Skimming/python/PDWG_EXODisappMuon_cff.py
+++ Configuration/Skimming/python/PDWG_EXODisappMuon_cff.py
@@ -5,6 +5,8 @@ import HLTrigger.HLTfilters.hltHighLevel_cfi as hlt
 from Configuration.EventContent.EventContent_cff import AODSIMEventContent
 EXODisappMuonSkimContent = AODSIMEventContent.clone()
 EXODisappMuonSkimContent.outputCommands.append('keep *_hbhereco_*_*')
+EXODisappMuonSkimContent.outputCommands.append('keep *_horeco_*_*')
+EXODisappMuonSkimContent.outputCommands.append('keep *_csc2DRecHits_*_*')
 
 exoDisappMuonsHLT = hlt.hltHighLevel.clone(
    throw = False,
