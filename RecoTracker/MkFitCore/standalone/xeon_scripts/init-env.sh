c5a6071ad96eb526d98c06d88d76732911e788a9
Steve Lantz <steve.lantz@cornell.edu>
2023-03-03 16:56:05 -0800

    mkFit: technical fixes for standalone build only

--- RecoTracker/MkFitCore/standalone/xeon_scripts/init-env.sh
+++ RecoTracker/MkFitCore/standalone/xeon_scripts/init-env.sh
@@ -1,9 +1,16 @@
 #!/bin/bash
-if [[ `lsb_release -si` == "Fedora" ]]
+if [[ `[ -f /usr/bin/lsb_release ] && lsb_release -si` == \
+        "Fedora" ||
+      `[ -f /etc/redhat-release ] && awk '{print $1}' /etc/redhat-release` == \
+        "AlmaLinux" ]]
 then
-  source /cvmfs/cms.cern.ch/el8_amd64_gcc10/lcg/root/6.24.07-da610b2b7ed663a0a05d3605f3d83ceb/etc/profile.d/init.sh
-  export TBB_GCC=/cvmfs/cms.cern.ch/el8_amd64_gcc10/external/tbb/v2021.5.0-e966a5acb1e4d5fd7605074bafbb079c/
+  source /cvmfs/cms.cern.ch/el8_amd64_gcc11/lcg/root/6.26.07-e76d33b57cbf5a1c12b870df2a6ebb79/etc/profile.d/init.sh
+  export TBB_GCC=/cvmfs/cms.cern.ch/el8_amd64_gcc11/external/tbb/v2021.8.0-791ebc4967ab49af60ca9ad2aa021259
+#  source /cvmfs/cms.cern.ch/el8_amd64_gcc10/lcg/root/6.24.07-3ea108fb48fc7b8bb2960269f724c0d6/etc/profile.d/init.sh
+#  export TBB_GCC=/cvmfs/cms.cern.ch/el8_amd64_gcc10/external/tbb/v2021.5.0-36aff7df349e0716374b1668ccd18e17
 else
-  source /cvmfs/cms.cern.ch/slc7_amd64_gcc10/lcg/root/6.24.07-f52350f4e0b802edeb9a2551a7d00b92/etc/profile.d/init.sh
-  export TBB_GCC=/cvmfs/cms.cern.ch/slc7_amd64_gcc10/external/tbb/v2021.4.0-d0152ca29055e3a1bbf629673f6e97c4
+  source /cvmfs/cms.cern.ch/slc7_amd64_gcc11/lcg/root/6.26.07-e76d33b57cbf5a1c12b870df2a6ebb79/etc/profile.d/init.sh
+  export TBB_GCC=/cvmfs/cms.cern.ch/slc7_amd64_gcc11/external/tbb/v2021.8.0-791ebc4967ab49af60ca9ad2aa021259
+#  source /cvmfs/cms.cern.ch/slc7_amd64_gcc10/lcg/root/6.24.07-3ea108fb48fc7b8bb2960269f724c0d6/etc/profile.d/init.sh
+#  export TBB_GCC=/cvmfs/cms.cern.ch/slc7_amd64_gcc10/external/tbb/v2021.5.0-36aff7df349e0716374b1668ccd18e17
 fi
