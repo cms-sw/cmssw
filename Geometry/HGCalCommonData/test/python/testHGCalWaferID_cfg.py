999970942d20c1ef985df8a7c11f1620b35dead1
Sunanda <sunanda.banerjee@cern.ch>
2023-03-06 10:33:51 +0100

    Correct the nonzzero cassette shift scenario for HGCal

--- Geometry/HGCalCommonData/test/python/testHGCalWaferID_cfg.py
+++ Geometry/HGCalCommonData/test/python/testHGCalWaferID_cfg.py
@@ -43,6 +43,7 @@ process.load('FWCore.MessageService.MessageLogger_cfi')
 
 if hasattr(process,'MessageLogger'):
     process.MessageLogger.HGCalGeomW=dict()
+    process.MessageLogger.HGCalGeom=dict()
 
 process.load("IOMC.RandomEngine.IOMC_cff")
 process.RandomNumberGeneratorService.generator.initialSeed = 456789
