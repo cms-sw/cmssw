16945882d3dfa734937e34cdee653db000b315aa
Shahzad Malik Muzaffar <shahzad.malik.muzaffar@cern.ch>
2023-03-07 08:22:28 +0100

    [DB] Various fixes/improvements for unit test

--- CondTools/Ecal/test/EcalDCS_O2O_test.sh
+++ CondTools/Ecal/test/EcalDCS_O2O_test.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 conddb --yes copy EcalDCSTowerStatus_online --destdb EcalDCSTowerStatus_online_O2OTEST.db --o2oTest
-popconRun ./src/CondTools/Ecal/python/EcalDCS_popcon.py -d sqlite_file:EcalDCSTowerStatus_online_O2OTEST.db -t EcalDCSTowerStatus_online -c
+popconRun $CMSSW_BASE/src/CondTools/Ecal/python/EcalDCS_popcon.py -d sqlite_file:EcalDCSTowerStatus_online_O2OTEST.db -t EcalDCSTowerStatus_online -c
 ret=$?
 conddb --db EcalDCSTowerStatus_online_O2OTEST.db list EcalDCSTowerStatus_online
 echo "return code is $ret"
--- CondTools/Ecal/test/EcalTPGLutGroup_O2O_test.sh
+++ CondTools/Ecal/test/EcalTPGLutGroup_O2O_test.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 conddb --yes copy EcalTPGLutGroup_v2_hlt --destdb EcalTPGLutGroup_v2_hlt_O2OTEST.db --o2oTest
-cmsRun ./src/CondTools/Ecal/python/copyLutGroup_cfg.py destinationDatabase=sqlite_file:EcalTPGLutGroup_v2_hlt_O2OTEST.db destinationTag=EcalTPGLutGroup_v2_hlt
+cmsRun $CMSSW_BASE/src/CondTools/Ecal/python/copyLutGroup_cfg.py destinationDatabase=sqlite_file:EcalTPGLutGroup_v2_hlt_O2OTEST.db destinationTag=EcalTPGLutGroup_v2_hlt
 ret=$?
 conddb --db EcalTPGLutGroup_v2_hlt_O2OTEST.db list EcalTPGLutGroup_v2_hlt
 echo "return code is $ret"
