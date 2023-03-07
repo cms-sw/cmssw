a0ac26b7ed9b6ac0a8f688713d1960eb40ad5eec
Shahzad Malik Muzaffar <shahzad.malik.muzaffar@cern.ch>
2023-03-07 08:23:29 +0100

    [ALCA] Various fixes/improvements for unit test

--- Alignment/OfflineValidation/test/testingScripts/test_unitSubmitPVsplit.sh
+++ Alignment/OfflineValidation/test/testingScripts/test_unitSubmitPVsplit.sh
@@ -3,4 +3,5 @@
 function die { echo $1: status $2 ; exit $2; }
 
 echo " TESTING Split Vertex Validation submission ..."
-submitPVResolutionJobs.py -j UNIT_TEST -D /JetHT/Run2018C-TkAlMinBias-12Nov2019_UL2018-v2/ALCARECO -i ${LOCAL_TEST_DIR}/PVResolutionExample.ini --unitTest || die "Failure running Split Vertex Validation submission" $?
+submitPVResolutionJobs.py -j UNIT_TEST -D /JetHT/Run2018C-TkAlMinBias-12Nov2019_UL2018-v2/ALCARECO \
+  -i ${CMSSW_BASE}/src/Alignment/OfflineValidation/test/PVResolutionExample.ini --unitTest || die "Failure running Split Vertex Validation submission" $?
--- Alignment/OfflineValidation/test/testingScripts/validateAlignments.sh
+++ Alignment/OfflineValidation/test/testingScripts/validateAlignments.sh
@@ -1,12 +1,10 @@
 #!/bin/zsh
 
-cd $CMSSW_BASE/src/Alignment/OfflineValidation/test
-
 echo "Printing help"
 validateAlignments.py -h
 
 echo "Running over YAML"
-validateAlignments.py -v -f -d unit_test.yaml
+validateAlignments.py -v -f -d ${CMSSW_BASE}/src/Alignment/OfflineValidation/test/unit_test.yaml
 
 echo "Running over JSON"
-validateAlignments.py -v -d -f unit_test.json
+validateAlignments.py -v -d -f ${CMSSW_BASE}/src/Alignment/OfflineValidation/test/unit_test.json
