eddd2b92b6f1f1c3ff769edda4979ebfa89e2339
Steve Lantz <steve.lantz@cornell.edu>
2023-03-06 19:18:07 -0800

    mkFit: simplified init-env.sh for standalone build

--- RecoTracker/MkFitCore/standalone/xeon_scripts/init-env.sh
+++ RecoTracker/MkFitCore/standalone/xeon_scripts/init-env.sh
@@ -4,13 +4,9 @@ if [[ `[ -f /usr/bin/lsb_release ] && lsb_release -si` == \
       `[ -f /etc/redhat-release ] && awk '{print $1}' /etc/redhat-release` == \
         "AlmaLinux" ]]
 then
-  source /cvmfs/cms.cern.ch/el8_amd64_gcc11/lcg/root/6.26.07-e76d33b57cbf5a1c12b870df2a6ebb79/etc/profile.d/init.sh
-  export TBB_GCC=/cvmfs/cms.cern.ch/el8_amd64_gcc11/external/tbb/v2021.8.0-791ebc4967ab49af60ca9ad2aa021259
-#  source /cvmfs/cms.cern.ch/el8_amd64_gcc10/lcg/root/6.24.07-3ea108fb48fc7b8bb2960269f724c0d6/etc/profile.d/init.sh
-#  export TBB_GCC=/cvmfs/cms.cern.ch/el8_amd64_gcc10/external/tbb/v2021.5.0-36aff7df349e0716374b1668ccd18e17
+  os_arch_comp="el8_amd64_gcc11"
 else
-  source /cvmfs/cms.cern.ch/slc7_amd64_gcc11/lcg/root/6.26.07-e76d33b57cbf5a1c12b870df2a6ebb79/etc/profile.d/init.sh
-  export TBB_GCC=/cvmfs/cms.cern.ch/slc7_amd64_gcc11/external/tbb/v2021.8.0-791ebc4967ab49af60ca9ad2aa021259
-#  source /cvmfs/cms.cern.ch/slc7_amd64_gcc10/lcg/root/6.24.07-3ea108fb48fc7b8bb2960269f724c0d6/etc/profile.d/init.sh
-#  export TBB_GCC=/cvmfs/cms.cern.ch/slc7_amd64_gcc10/external/tbb/v2021.5.0-36aff7df349e0716374b1668ccd18e17
+  os_arch_comp="slc7_amd64_gcc11"
 fi
+  source /cvmfs/cms.cern.ch/${os_arch_comp}/lcg/root/6.26.07-e76d33b57cbf5a1c12b870df2a6ebb79/etc/profile.d/init.sh
+  export TBB_GCC=/cvmfs/cms.cern.ch/${os_arch_comp}/external/tbb/v2021.8.0-791ebc4967ab49af60ca9ad2aa021259
