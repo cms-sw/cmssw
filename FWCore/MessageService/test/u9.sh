1a96da3e6f1a96926453222fe1124d5d95a82081
Shahzad Malik Muzaffar <shahzad.malik.muzaffar@cern.ch>
2023-03-08 17:51:07 +0100

    [CORE] Various fixes/improvements for unit test

--- FWCore/MessageService/test/u9.sh
+++ FWCore/MessageService/test/u9.sh
@@ -2,25 +2,21 @@
 
 case `uname` in Darwin) SED_OPT="-i '' -E";;*) SED_OPT="-i -r";; esac ;
 
-pushd $LOCAL_TMP_DIR
-
 status=0
   
 rm -f warnings.log infos.log job_report.xml 
 
-cmsRun -j job_report.xml -p $LOCAL_TEST_DIR/u9_cfg.py || exit $?
+cmsRun -j job_report.xml -p ${SCRAM_TEST_PATH}/u9_cfg.py || exit $?
  
 for file in warnings.log infos.log job_report.xml   
 do
-  sed $SED_OPT -f $LOCAL_TEST_DIR/filter-timestamps.sed $file
-  diff $LOCAL_TEST_DIR/unit_test_outputs/$file $LOCAL_TMP_DIR/$file  
+  sed $SED_OPT -f ${SCRAM_TEST_PATH}/filter-timestamps.sed $file
+  diff ${SCRAM_TEST_PATH}/unit_test_outputs/$file ./$file
   if [ $? -ne 0 ]  
   then
     echo The above discrepancies concern $file 
     status=1
   fi
 done
-
-popd
 
 exit $status
