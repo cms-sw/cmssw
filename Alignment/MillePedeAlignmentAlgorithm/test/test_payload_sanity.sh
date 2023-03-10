ba966fea116707d82f89268fc0c18ba07e958ac3
Shahzad Malik Muzaffar <shahzad.malik.muzaffar@cern.ch>
2023-03-10 19:11:28 +0100

    [ALCA] Various fixes/improvements for unit test

--- Alignment/MillePedeAlignmentAlgorithm/test/test_payload_sanity.sh
+++ Alignment/MillePedeAlignmentAlgorithm/test/test_payload_sanity.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 function die { echo $1: status $2; exit $2; }
 
-INPUTFILE=${LOCAL_TEST_DIR}/alignments_MP.db
-(cmsRun ${LOCAL_TEST_DIR}/AlignmentRcdChecker_cfg.py inputSqliteFile=${INPUTFILE}) || die 'failed running AlignmentRcdChecker'
+INPUTFILE=${SCRAM_TEST_PATH}/alignments_MP.db
+(cmsRun ${SCRAM_TEST_PATH}/AlignmentRcdChecker_cfg.py inputSqliteFile=${INPUTFILE}) || die 'failed running AlignmentRcdChecker'
 rm $INPUTFILE
