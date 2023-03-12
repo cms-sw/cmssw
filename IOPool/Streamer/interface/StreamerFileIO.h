24d45adfc1bd516b2fdfc7913c0c011166f3151c
Srecko Morovic <srecko.morovic@cern.ch>
2023-03-12 23:08:33 +0100

    allow padding between event headers and ignore another INIT block which could be there due to concenating two files with a header file new streamer unit test for padded files write out files with padding. Used for unit test checking that such files can be opened and read address comments improved unit tests
    Note: fixed conflict with the unit test due to improved merged into
    master
    
    Conflicts:
    	IOPool/Streamer/test/RunSimple_NewStreamer.sh

--- IOPool/Streamer/interface/StreamerFileIO.h
+++ IOPool/Streamer/interface/StreamerFileIO.h
@@ -20,13 +20,14 @@ namespace edm::streamer {
   */
   {
   public:
-    explicit OutputFile(const std::string& name);
+    explicit OutputFile(const std::string& name, uint32 padding = 0);
     /**
       CTOR, takes file path name as argument
      */
     ~OutputFile();
 
-    bool write(const char* ptr, size_t n);
+    bool write(const char* ptr, size_t n, bool doPadding = false);
+    bool writePadding();
 
     std::string fileName() const { return filename_; }
     uint64 current_offset() const { return current_offset_; }
@@ -42,9 +43,11 @@ namespace edm::streamer {
     bool do_adler_;
     uint32 adlera_;
     uint32 adlerb_;
+    uint32 padding_;
 
     edm::propagate_const<std::shared_ptr<std::ofstream>> ost_;
     std::string filename_;
+    std::unique_ptr<char[]> paddingBuf_;
   };
 }  // namespace edm::streamer
 #endif
