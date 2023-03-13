3d5d59b2133aa1097f2703a70bbbef53dd25e25f
Andrea Bocci <andrea.bocci@cern.ch>
2023-03-13 19:21:54 +0100

    Remove L1 modules

--- HLTrigger/Configuration/python/HLT_75e33/modules/simMuonGEMPadDigis_cfi.py
+++
@@ -1,6 +0,0 @@
-import FWCore.ParameterSet.Config as cms
-
-simMuonGEMPadDigis = cms.EDProducer("GEMPadDigiProducer",
-    InputCollection = cms.InputTag("simMuonGEMDigis"),
-    mightGet = cms.optional.untracked.vstring
-)
--- HLTrigger/Configuration/python/HLT_75e33/paths/l1tReconstructionPath_cfi.py
+++
@@ -1,7 +0,0 @@
-import FWCore.ParameterSet.Config as cms
-
-from ..tasks.l1tReconstructionTask_cfi import *
-
-L1TReconstructionPath = cms.Path(
-    L1TReconstructionTask
-)
