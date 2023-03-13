fe72e551921ff8a0de1e125894b2a3bbf579a006
stephanie <stephanie.beauceron@cern.ch>
2023-03-13 18:25:28 +0100

    Following Marino comments

--- HLTrigger/Configuration/python/HLT_75e33/tasks/RawToDigiTask_cfi.py
+++ HLTrigger/Configuration/python/HLT_75e33/tasks/RawToDigiTask_cfi.py
@@ -6,7 +6,7 @@ from ..modules.hgcalDigis_cfi import *
 from ..modules.muonCSCDigis_cfi import *
 from ..modules.muonDTDigis_cfi import *
 from ..modules.muonGEMDigis_cfi import *
-from ..tasks.ecalDigisTask_cfi import *
+from ..tasks.hltEcalDigisTask_cfi import *
 
 RawToDigiTask = cms.Task(
     hltEcalDigisTask,
