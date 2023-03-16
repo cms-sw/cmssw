e3aef2fbca54e7e91bb538d8b11d46399594efad
Ian Tomalin <ian.tomalin@stfc.ac.uk>
2023-03-16 15:19:07 +0000

    Update MC datasets in L1 track CMSSW jobs. (#214)
    * Added missing includes
    
    * Updated MC samples

--- L1Trigger/TrackFindingTracklet/python/ProducerHPH_cff.py
+++ L1Trigger/TrackFindingTracklet/python/ProducerHPH_cff.py
@@ -1,5 +1,7 @@
 import FWCore.ParameterSet.Config as cms
 
+from L1Trigger.TrackerTFP.ProducerES_cff import TrackTriggerDataFormats
+from L1Trigger.TrackerTFP.ProducerLayerEncoding_cff import TrackTriggerLayerEncoding
 from L1Trigger.TrackFindingTracklet.ProducerHPH_cfi import HitPatternHelper_params
 
 HitPatternHelperSetup = cms.ESProducer("hph::ProducerHPH", HitPatternHelper_params)
