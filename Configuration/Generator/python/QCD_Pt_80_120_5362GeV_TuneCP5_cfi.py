4164d0357589bf92ddf87ba59ee766e9fff7755a
Matthew <Matthew.Nguyen@cern.ch>
2023-03-18 10:02:09 +0100

    HI 2023 relval wfs + realistic 2022 vtx smearing

---
+++ Configuration/Generator/python/QCD_Pt_80_120_5362GeV_TuneCP5_cfi.py
@@ -0,0 +1,25 @@
+import FWCore.ParameterSet.Config as cms
+from Configuration.Generator.Pythia8CommonSettings_cfi import *
+from Configuration.Generator.MCTunes2017.PythiaCP5Settings_cfi import *
+
+generator = cms.EDFilter("Pythia8ConcurrentGeneratorFilter",
+                         pythiaPylistVerbosity = cms.untracked.int32(0),
+                         filterEfficiency = cms.untracked.double(1.0),
+                         pythiaHepMCVerbosity = cms.untracked.bool(False),
+                         comEnergy = cms.double(5362.0),
+                         maxEventsToPrint = cms.untracked.int32(0),
+                         PythiaParameters = cms.PSet(
+        pythia8CommonSettingsBlock,
+        pythia8CP5SettingsBlock,
+        processParameters = cms.vstring(
+            'HardQCD:all = on',
+            'PhaseSpace:pTHatMin = 80.',
+            'PhaseSpace:pTHatMax = 120.'
+            ),
+        parameterSets = cms.vstring('pythia8CommonSettings',
+                                    'pythia8CP5Settings',
+                                    'processParameters',
+                                    )
+        )
+                         )
+
