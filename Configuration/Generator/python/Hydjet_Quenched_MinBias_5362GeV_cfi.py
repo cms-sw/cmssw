a5577b01111e6df0f9cfcc593d0ae0b9ebcaefa3
Matthew <Matthew.Nguyen@cern.ch>
2023-03-18 10:02:09 +0100

    HI 2023 relval wfs + realistic 2022 vtx smearing

---
+++ Configuration/Generator/python/Hydjet_Quenched_MinBias_5362GeV_cfi.py
@@ -0,0 +1,28 @@
+import FWCore.ParameterSet.Config as cms
+
+from Configuration.Generator.Pyquen2015Settings_cff import *
+
+generator = cms.EDFilter("HydjetGeneratorFilter",
+                         collisionParameters5362GeV,
+                         qgpParameters2023,
+                         hydjetParameters2023,
+                         hydjetMode = cms.string('kHydroQJets'),
+                         PythiaParameters = cms.PSet(pyquenPythiaDefaultBlock,
+                                                     # Quarkonia and Weak Bosons added back upon dilepton group's request.
+                                                     parameterSets = cms.vstring('pythiaUESettings',
+                                                                                 'hydjetPythiaDefault',
+                                                                                 'myParameters',
+                                                                                 'pythiaJets',
+                                                                                 'pythiaPromptPhotons',
+                                                                                 'pythiaZjets',
+                                                                                 'pythiaBottomoniumNRQCD',
+                                                                                 'pythiaCharmoniumNRQCD',
+                                                                                 'pythiaQuarkoniaSettings',
+                                                                                 'pythiaWeakBosons'
+                                                                                 )
+                                                     ),
+                         cFlag = cms.int32(1),
+                         bMin = cms.double(0),
+                         bMax = cms.double(30),
+                         bFixed = cms.double(0)
+                         )
