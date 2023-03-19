a9cb93db1cd0ae88bde5b70bbd12863d9d4d0535
swagata87 <physics.swagata@gmail.com>
2023-03-19 13:27:39 +0100

    remove unnecessary open paths

--- HLTrigger/Configuration/python/HLT_75e33/modules/hltPrePhoton100EBTightIDTightIsoOpenUnseeded_cfi.py
+++
@@ -1,6 +0,0 @@
-import FWCore.ParameterSet.Config as cms
-
-hltPrePhoton100EBTightIDTightIsoOpenUnseeded = cms.EDFilter("HLTPrescaler",
-    L1GtReadoutRecordTag = cms.InputTag("hltGtStage2Digis"),
-    offset = cms.uint32(0)
-)
