e5e9db4d8dcd7746dc0c2a6c936868327f83ae2d
Shahzad Malik Muzaffar <shahzad.malik.muzaffar@cern.ch>
2023-03-20 11:12:47 +0100

    match _DBG_X IBs

--- FWCore/MessageService/test/u1.sh
+++ FWCore/MessageService/test/u1.sh
@@ -13,7 +13,7 @@ for file in u1_errors.log u1_warnings.log u1_infos.log u1_debugs.log u1_default.
 do
   sed $SED_OPT -f ${SCRAM_TEST_PATH}/filter-timestamps.sed $file
   ref_log=${SCRAM_TEST_PATH}/unit_test_outputs/$file
-  [[ " ${CMSSW_VERSION}" == *"_DBG"* ]] && [ -e ${SCRAM_TEST_PATH}/unit_test_outputs/DBG/$file ] && ref_log=${SCRAM_TEST_PATH}/unit_test_outputs/DBG/$file
+  [[ " ${CMSSW_VERSION}" == *"_DBG_X"* ]] && [ -e ${SCRAM_TEST_PATH}/unit_test_outputs/DBG/$file ] && ref_log=${SCRAM_TEST_PATH}/unit_test_outputs/DBG/$file
   diff ${ref_log} ./$file
   if [ $? -ne 0 ]  
   then
--- FWCore/MessageService/test/u19.sh
+++ FWCore/MessageService/test/u19.sh
@@ -13,7 +13,7 @@ for file in u19_infos.log u19_debugs.log
 do
   sed $SED_OPT -f ${SCRAM_TEST_PATH}/filter-timestamps.sed $file
   ref_log=${SCRAM_TEST_PATH}/unit_test_outputs/$file
-  [[ " ${CMSSW_VERSION}" == *"_DBG"* ]] && [ -e ${SCRAM_TEST_PATH}/unit_test_outputs/DBG/$file ] && ref_log=${SCRAM_TEST_PATH}/unit_test_outputs/DBG/$file
+  [[ " ${CMSSW_VERSION}" == *"_DBG_X"* ]] && [ -e ${SCRAM_TEST_PATH}/unit_test_outputs/DBG/$file ] && ref_log=${SCRAM_TEST_PATH}/unit_test_outputs/DBG/$file
   diff ${ref_log} ./$file
   if [ $? -ne 0 ]  
   then
