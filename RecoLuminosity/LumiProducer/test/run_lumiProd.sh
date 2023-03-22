e5aec88bbfdf59a47d6b75c9611c919eae4016c1
Shahzad Malik Muzaffar <shahzad.malik.muzaffar@cern.ch>
2023-03-22 18:13:57 +0100

    [RECONSTRUCTION] Various fixes/improvements for unit test

--- RecoLuminosity/LumiProducer/test/run_lumiProd.sh
+++ RecoLuminosity/LumiProducer/test/run_lumiProd.sh
@@ -2,10 +2,4 @@
 
 function die { echo Failure $1: status $2 ; exit $2 ; }
 
-pushd ${LOCAL_TMP_DIR}
-
-  cmsRun -p ${LOCAL_TEST_DIR}/testLumiProd_cfg.py || die "cmsRun testLumiProd_cfg.py" $?
-
-popd
-
-exit 0
+cmsRun -p ${SCRAM_TEST_PATH}/testLumiProd_cfg.py || die "cmsRun testLumiProd_cfg.py" $?
