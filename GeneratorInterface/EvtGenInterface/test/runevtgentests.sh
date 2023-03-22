3b0da0d277bb4247bd925f61639e598f08f5b9bb
Shahzad Malik Muzaffar <shahzad.malik.muzaffar@cern.ch>
2023-03-22 18:13:39 +0100

    [GENERATORS] Various fixes/improvements for unit test

--- GeneratorInterface/EvtGenInterface/test/runevtgentests.sh
+++ GeneratorInterface/EvtGenInterface/test/runevtgentests.sh
@@ -3,7 +3,7 @@
 function die { echo $1: status $2 ;  exit $2; }
 
 # list basic test
-cmsRun ${LOCAL_TEST_DIR}/Py8_bplus_evtgen1_cfg.py || die 'Failure using Py8_bplus_evtgen1_cfg.py' $?
+cmsRun ${SCRAM_TEST_PATH}/Py8_bplus_evtgen1_cfg.py || die 'Failure using Py8_bplus_evtgen1_cfg.py' $?
 
 # extra test
-cmsRun ${LOCAL_TEST_DIR}/../plugins/test/Py8_lambadb_evtgen1_Lb2plnuLCSR_cfg.py || die 'Py8_lambadb_evtgen1_Lb2plnuLCSR_cfg.py' $?
+cmsRun ${SCRAM_TEST_PATH}/../plugins/test/Py8_lambadb_evtgen1_Lb2plnuLCSR_cfg.py || die 'Py8_lambadb_evtgen1_Lb2plnuLCSR_cfg.py' $?
