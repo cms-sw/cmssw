b9fa4db1c3f7e4849752a669542b7effbbbb9928
Shahzad Malik Muzaffar <shahzad.malik.muzaffar@cern.ch>
2023-03-22 18:12:12 +0100

    [ALCA-DB] Various fixes/improvements for unit test

--- CondCore/ESSources/test/TestConcurrentIOVsCondCore.sh
+++ CondCore/ESSources/test/TestConcurrentIOVsCondCore.sh
@@ -2,11 +2,7 @@
 
 function die { echo $1: status $2 ;  exit $2; }
 
-pushd ${LOCAL_TMP_DIR}
-
 echo cmsRun TestConcurrentIOVsCondCore_cfg.py
-cmsRun --parameter-set ${LOCAL_TEST_DIR}/TestConcurrentIOVsCondCore_cfg.py || die 'Failed in TestConcurrentIOVsCondCore_cfg.py' $?
+cmsRun --parameter-set ${SCRAM_TEST_PATH}/TestConcurrentIOVsCondCore_cfg.py || die 'Failed in TestConcurrentIOVsCondCore_cfg.py' $?
 grep "TestConcurrentIOVsCondCore: " TestConcurrentIOVsCondCoreCout.log > TestConcurrentIOVsCondCore.log
-diff ${LOCAL_TEST_DIR}/unit_test_outputs/TestConcurrentIOVsCondCore.log TestConcurrentIOVsCondCore.log || die "comparing TestConcurrentIOVsCondCore.log" $?
-
-popd
+diff ${SCRAM_TEST_PATH}/unit_test_outputs/TestConcurrentIOVsCondCore.log TestConcurrentIOVsCondCore.log || die "comparing TestConcurrentIOVsCondCore.log" $?
