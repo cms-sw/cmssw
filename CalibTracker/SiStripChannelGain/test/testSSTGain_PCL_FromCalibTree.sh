0870dab5eb0a13c2aeae4ccea6cbebf80dd9ae23
Shahzad Malik Muzaffar <shahzad.malik.muzaffar@cern.ch>
2023-03-23 12:58:48 +0100

    [ALCA] Various fixes/improvements for unit test

--- CalibTracker/SiStripChannelGain/test/testSSTGain_PCL_FromCalibTree.sh
+++ CalibTracker/SiStripChannelGain/test/testSSTGain_PCL_FromCalibTree.sh
@@ -8,8 +8,8 @@ COMMMAND=`xrdfs root://eoscms.cern.ch// locate ${REMOTE}/${FILE}`
 STATUS=$?
 echo "xrdfs command status = "$STATUS
 if [ $STATUS -eq 0 ]; then
-    (cmsRun ${LOCAL_TEST_DIR}/Gains_CT_step1.py firstRun=${RUN} lastRun=${RUN} inputFiles=root://eoscms.cern.ch//$REMOTE/$FILE outputFile=PromptCalibProdSiStripGainsFromTree.root) || die 'Failure running cmsRun Gains_CT_step1.py firstRun=${RUN} lastRun=${RUN} inputFiles=root://eoscms.cern.ch//$REMOTE/$FILE outputFile=PromptCalibProdSiStripGainsFromTree.root' $?
-    (cmsRun ${LOCAL_TEST_DIR}/Gains_CT_step2.py inputFiles=file:PromptCalibProdSiStripGainsFromTree.root DQMOutput=True) || die 'Failure running cmsRun Gains_CT_step2.py inputFiles=file:PromptCalibProdSiStripGainsFromTree.root DQMOutput=True' $?
+    (cmsRun ${SCRAM_TEST_PATH}/Gains_CT_step1.py firstRun=${RUN} lastRun=${RUN} inputFiles=root://eoscms.cern.ch//$REMOTE/$FILE outputFile=PromptCalibProdSiStripGainsFromTree.root) || die 'Failure running cmsRun Gains_CT_step1.py firstRun=${RUN} lastRun=${RUN} inputFiles=root://eoscms.cern.ch//$REMOTE/$FILE outputFile=PromptCalibProdSiStripGainsFromTree.root' $?
+    (cmsRun ${SCRAM_TEST_PATH}/Gains_CT_step2.py inputFiles=file:PromptCalibProdSiStripGainsFromTree.root DQMOutput=True) || die 'Failure running cmsRun Gains_CT_step2.py inputFiles=file:PromptCalibProdSiStripGainsFromTree.root DQMOutput=True' $?
 else
   die "SKIPPING test, file ${FILE} not found" 0
 fi
