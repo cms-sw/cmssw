2b54203f150b1ee162d96c3392c00613deb3e3ac
Srecko Morovic <srecko.morovic@cern.ch>
2023-03-23 20:25:00 +0100

    Add padded streamer format check for DQM streamer source
    Conflicts:
    	DQMServices/StreamerIO/test/RunStreamer.sh

--- DQMServices/StreamerIO/test/RunStreamer.sh
+++ DQMServices/StreamerIO/test/RunStreamer.sh
@@ -23,7 +23,20 @@ mkdir ${OUTDIR}
 cp *_cfg.py ${OUTDIR}
 cd ${OUTDIR}
 
+rm -rf run000001
 mkdir run000001
+
+#test checksum with padded format
+echo "{\"data\" :[10,10, \"teststreamfile.dat\"]}" >  run000001/run1_ls1_test.jsn
+cmsRun streamOutPadding_cfg.py > outp 2>&1 || die "cmsRun streamOutPadding_cfg.py" $?
+mv teststreamfile.dat run000001/teststreamfile.dat
+cmsRun streamOutAlt_cfg.py  > outAlt 2>&1 || die "cmsRun streamOutAlt_cfg.py" $?
+cmsRun streamOutExt_cfg.py  > outExt 2>&1 || die "cmsRun streamOutExt_cfg.py" $?
+timeout --signal SIGTERM 180 cmsRun streamIn_cfg.py  > inp  2>&1 || die "cmsRun streamIn_cfg.py" $?
+
+rm -rf run000001
+mkdir run000001
+
 #the initial json file to read
 echo "{\"data\" :[10,10, \"teststreamfile.dat\"]}" >  run000001/run1_ls1_test.jsn
 cmsRun streamOut_cfg.py > out 2>&1 || die "cmsRun streamOut_cfg.py" $?
@@ -42,31 +55,19 @@ rm run000001/run000001_ls0000_EoR.jsn
 mv teststreamfile_ext.dat run000001/teststreamfile_ext.dat
 timeout --signal SIGTERM 180 cmsRun streamInExt_cfg.py  > ext  2>&1 || die "cmsRun streamInExt_cfg.py" $?
 
-echo "{\"data\" :[10,10, \"teststreamfile.dat\"]}" >  run000001/run1_ls1_test.jsn
-cmsRun streamOutPadding_cfg.py > outPadding 2>&1 || die "cmsRun streamOutPadding_cfg.py" $?
-mv teststreamfile_padding.dat run000001/teststreamfile.dat
-timeout --signal SIGTERM 180 cmsRun streamIn_cfg.py  > inPadding  2>&1 || die "cmsRun streamIn_cfg.py" $?
-
-
 # echo "CHECKSUM = 1" > out
 # echo "CHECKSUM = 1" > in
 
 ANS_OUT_SIZE=`grep -c CHECKSUM out`
-ANS_OUT_PADD_SIZE=`grep -c CHECKSUM outPadding`
 ANS_OUT=`grep CHECKSUM out`
-ANS_OUT_PADD=`grep CHECKSUM outPadding`
 ANS_IN=`grep CHECKSUM in`
-ANS_IN_PADD=`grep CHECKSUM inPadding`
+ANS_OUTP_SIZE=`grep -c CHECKSUM outp`
+ANS_OUTP=`grep CHECKSUM outp`
+ANS_INP=`grep CHECKSUM inp`
 
 if [ "${ANS_OUT_SIZE}" == "0" ]
 then
     echo "New Stream Test Failed (out was not created)"
-    RC=1
-fi
-
-if [ "${ANS_OUT_PADD_SIZE}" == "0" ]
-then
-    echo "New Padded Stream Test Failed (out was not created)"
     RC=1
 fi
 
@@ -76,9 +77,15 @@ then
     RC=1
 fi
 
-if [ "${ANS_OUT_PADD}" != "${ANS_IN_PADD}" ]
+if [ "${ANS_OUTP_SIZE}" == "0" ]
 then
-    echo "New Padded Stream Test Failed (out!=in)"
+    echo "New Stream Padded Test Failed (out was not created)"
+    RC=1
+fi
+
+if [ "${ANS_OUTP}" != "${ANS_INP}" ]
+then
+    echo "New Stream Padded Test Failed (out!=in)"
     RC=1
 fi
 
