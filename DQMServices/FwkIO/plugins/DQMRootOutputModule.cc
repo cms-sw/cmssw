c3833ea4475c9d04487a9b5e8bbb1336e603245a
W. David Dagenhart <wdd@fnal.gov>
2023-03-23 22:05:41 +0100

    Remove unnecessary call to consumesMany

--- DQMServices/FwkIO/plugins/DQMRootOutputModule.cc
+++ DQMServices/FwkIO/plugins/DQMRootOutputModule.cc
@@ -293,15 +293,7 @@ DQMRootOutputModule::DQMRootOutputModule(edm::ParameterSet const& pset)
       m_presentHistoryIndex(0),
       m_filterOnRun(pset.getUntrackedParameter<unsigned int>("filterOnRun")),
       m_fullNameBufferPtr(&m_fullNameBuffer),
-      m_indicesTree(nullptr) {
-  // Declare dependencies for all Lumi and Run tokens here. In
-  // principle could use the keep statements, but then DQMToken would
-  // have to be made persistent (transient products are ignored),
-  // which would lead to a need to (finally) remove underscores from
-  // DQM module labels.
-  consumesMany<DQMToken, edm::InLumi>();
-  consumesMany<DQMToken, edm::InRun>();
-}
+      m_indicesTree(nullptr) {}
 
 // DQMRootOutputModule::DQMRootOutputModule(const DQMRootOutputModule& rhs)
 // {
