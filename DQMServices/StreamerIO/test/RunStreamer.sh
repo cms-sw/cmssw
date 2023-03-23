442af98674526921f2f97fdd061d863d2117a8db
Srecko Morovic <srecko.morovic@cern.ch>
2023-03-23 18:49:26 +0100

    add padded streamer file test to DQM

--- DQMServices/StreamerIO/test/RunStreamer.sh
+++ DQMServices/StreamerIO/test/RunStreamer.sh
@@ -28,16 +28,31 @@ rm run000001/run000001_ls0000_EoR.jsn
 mv teststreamfile_ext.dat run000001/teststreamfile_ext.dat
 timeout --signal SIGTERM 180 cmsRun ${SCRAM_TEST_PATH}/streamInExt_cfg.py  > ext  2>&1 || die "cmsRun streamInExt_cfg.py" $?
 
+echo "{\"data\" :[10,10, \"teststreamfile.dat\"]}" >  run000001/run1_ls1_test.jsn
+cmsRun ${SCRAM_TEST_PATH}/streamOutPadding_cfg.py > outPadding 2>&1 || die "cmsRun streamOutPadding_cfg.py" $?
+mv teststreamfile_padding.dat run000001/teststreamfile.dat
+timeout --signal SIGTERM 180 cmsRun ${SCRAM_TEST_PATH}/streamIn_cfg.py  > inPadding  2>&1 || die "cmsRun streamIn_cfg.py" $?
+
+
 # echo "CHECKSUM = 1" > out
 # echo "CHECKSUM = 1" > in
 
 ANS_OUT_SIZE=`grep -c CHECKSUM out`
+ANS_OUT_PADD_SIZE=`grep -c CHECKSUM outPadding`
 ANS_OUT=`grep CHECKSUM out`
+ANS_OUT_PADD=`grep CHECKSUM outPadding`
 ANS_IN=`grep CHECKSUM in`
+ANS_IN_PADD=`grep CHECKSUM inPadding`
 
 if [ "${ANS_OUT_SIZE}" == "0" ]
 then
     echo "New Stream Test Failed (out was not created)"
+    RC=1
+fi
+
+if [ "${ANS_OUT_PADD_SIZE}" == "0" ]
+then
+    echo "New Padded Stream Test Failed (out was not created)"
     RC=1
 fi
 
@@ -46,5 +61,12 @@ then
     echo "New Stream Test Failed (out!=in)"
     RC=1
 fi
+
+if [ "${ANS_OUT_PADD}" != "${ANS_IN_PADD}" ]
+then
+    echo "New Padded Stream Test Failed (out!=in)"
+    RC=1
+fi
+
 
 exit ${RC}
