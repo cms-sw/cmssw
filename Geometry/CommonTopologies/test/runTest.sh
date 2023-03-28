2d37d7444f59cbf2361f38b220dcb1aee7abd726
Sunanda <sunanda.banerjee@cern.ch>
2023-03-28 16:06:18 +0200

    Restore runTest

--- Geometry/CommonTopologies/test/runTest.sh
+++ Geometry/CommonTopologies/test/runTest.sh
@@ -2,13 +2,13 @@
 echo " testing Geometry/CommonTopologies"
 
 export tmpdir=${LOCAL_TMP_DIR:-/tmp}
-cmsRun ${LOCAL_TEST_DIR}/python/ValidateRadial_cfg.py |& grep "ALIVE" > /dev/null 
+cmsRun ${LOCAL_TEST_DIR}/python/ValidateRadial_cfg.py | grep "ALIVE" > /dev/null 
 if [[ $? -ne 0 ]]
 then
  echo "Vin test tricks do not work"
  exit 20
 fi
-cmsRun ${LOCAL_TEST_DIR}/python/ValidateRadial_cfg.py |& grep "BOGUS" > /dev/null 
+cmsRun ${LOCAL_TEST_DIR}/python/ValidateRadial_cfg.py | grep "BOGUS" > /dev/null 
 if [[ $? -eq 0 ]]
 then
  echo "Vin test tricks do not work"
@@ -16,7 +16,7 @@ then
 fi
 
 rm -f failureLimits.root
-cmsRun ${LOCAL_TEST_DIR}/python/ValidateRadial_cfg.py |& grep "FAILED" > /dev/null 
+cmsRun ${LOCAL_TEST_DIR}/python/ValidateRadial_cfg.py | grep "FAILED" > /dev/null 
 if [[ $? -eq 0 ]]
 then
  echo "ValidateRadial failed"
