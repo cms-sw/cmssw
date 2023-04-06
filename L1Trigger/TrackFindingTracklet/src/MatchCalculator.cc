760d5d6676afa9829e98e43a46c0ab8f69b9447b
bryates <brent.yates@email.ucr.edu>
2023-04-06 17:17:08 +0200

    Fxied crash by just using `idrcut`

--- L1Trigger/TrackFindingTracklet/src/MatchCalculator.cc
+++ L1Trigger/TrackFindingTracklet/src/MatchCalculator.cc
@@ -420,11 +420,12 @@ void MatchCalculator::execute(unsigned int iSector, double phioffset) {
       next_projid = projindex;
       // Do we have a new tracklet?
       bool newtracklet = (j == 0 || projindex != curr_projid);
+      if (j == 0)
+        best_ideltar_disk = (1 << (fpgastub->r().nbits() - 1));  // Set to the maximum possible
       // If so, replace the "best" values with the cut tables
       if (newtracklet) {
         best_ideltaphi_disk = idrphicut;
-        // Set to the maximum possible if j==0
-        best_ideltar_disk = (j == 0)? (1 << (fpgastub->r().nbits() - 1)) : idrcut;
+        best_ideltar_disk = idrcut;
       }
 
       // Update the cut vales (cut table if new tracklet, otherwise current best)
