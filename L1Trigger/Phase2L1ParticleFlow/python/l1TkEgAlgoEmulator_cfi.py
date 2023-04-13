454105dd138883e66addb9d1d79b5ce938af4015
Gianluca <gianluca.cerminara@cern.ch>
2023-04-13 16:41:23 +0200

    use the 0.955 score for the loose WP

--- L1Trigger/Phase2L1ParticleFlow/python/l1TkEgAlgoEmulator_cfi.py
+++ L1Trigger/Phase2L1ParticleFlow/python/l1TkEgAlgoEmulator_cfi.py
@@ -54,7 +54,7 @@ tkEgAlgoParameters = cms.PSet(
     nCompCandPerCluster=cms.uint32(3),
     compositeParametersTkEle=cms.PSet(
         # the working points are cuts on BDT output logits log(p/1-p)
-        loose_wp=cms.double(-4.),
+        loose_wp=cms.double(-0.732422),
         tight_wp=cms.double(0.214844),
         model=cms.string("L1Trigger/Phase2L1ParticleFlow/data/compositeID.json")
     ),
