204b800664abca1c61d96a3217085f6642c897d2
Andrea Bocci <andrea.bocci@cern.ch>
2023-04-14 02:51:26 +0200

    Drop support for the "old" accelerator framework

--- DataFormats/PortableTestObjects/src/alpaka/classes_cuda.h
+++ DataFormats/PortableTestObjects/src/alpaka/classes_cuda.h
@@ -1,5 +1,4 @@
 #include "DataFormats/Common/interface/DeviceProduct.h"
 #include "DataFormats/Common/interface/Wrapper.h"
-#include "DataFormats/Portable/interface/Product.h"
 #include "DataFormats/PortableTestObjects/interface/TestSoA.h"
 #include "DataFormats/PortableTestObjects/interface/alpaka/TestDeviceCollection.h"
