8cbe321173ddc0a87804e12a4cf697d6e4b10f97
Slava Krutelyov <slava77@gmail.com>
2023-04-25 09:51:25 -0700

    spurious change from auto-fwd port

--- PhysicsTools/PatAlgos/plugins/PATMuonProducer.cc
+++ PhysicsTools/PatAlgos/plugins/PATMuonProducer.cc
@@ -309,8 +309,6 @@ void pat::PATMuonProducer::readIsolationLabels(const edm::ParameterSet& iConfig,
       for (; it != ed; ++it, ++key) {
         labels.push_back(std::make_pair(pat::IsolationKeys(key), *it));
       }
-      tokens = edm::vector_transform(
-          labels, [this](IsolationLabel const& label) { return consumes<edm::ValueMap<T>>(label.second); });
     }
   }
   tokens = edm::vector_transform(labels, [this](pat::PATMuonProducer::IsolationLabel const& label) {
