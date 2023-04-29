69c657f7b92aa6fd55a2798ddee41cc749052bcd
Marino Missiroli <m.missiroli@cern.ch>
2023-04-29 20:01:55 +0200

    use FakeHLT sequences for DQM and Validation in pre-2023 wfs

--- Configuration/PyReleaseValidation/python/relval_standard.py
+++ Configuration/PyReleaseValidation/python/relval_standard.py
@@ -785,4 +785,3 @@ workflows[280]= ['',['AMPT_PPb_5020GeV_MinimumBias','DIGI','RECO','HARVEST']]
 
 ### pPb Run2 ###
 workflows[281]= ['',['EPOS_PPb_8160GeV_MinimumBias','DIGIUP15_PPb','RECOUP15_PPb','HARVESTUP15_PPb']]
-
