8f23ac09ac9644ce53394debb2f75d1ad212bde9
francescobrivio <francesco.brivio@cern.ch>
2023-05-02 15:22:15 +0200

    Update L1T Menu tag for 2023

--- Configuration/AlCa/python/autoCond.py
+++ Configuration/AlCa/python/autoCond.py
@@ -74,19 +74,19 @@ autoCond = {
     # GlobalTag for MC production with realistic conditions for Phase1 2022 detector for Heavy Ion
     'phase1_2022_realistic_hi'     : '131X_mcRun3_2022_realistic_HI_v7',
     # GlobalTag for MC production with perfectly aligned and calibrated detector for Phase1 2023
-    'phase1_2023_design'           : '131X_mcRun3_2023_design_v4',
+    'phase1_2023_design'           : '131X_mcRun3_2023_design_v5',
     # GlobalTag for MC production with realistic conditions for Phase1 2023
-    'phase1_2023_realistic'        : '131X_mcRun3_2023_realistic_v4',
+    'phase1_2023_realistic'        : '131X_mcRun3_2023_realistic_v5',
     # GlobalTag for MC production (cosmics) with realistic conditions for Phase1 2023,  Strip tracker in DECO mode
-    'phase1_2023_cosmics'          : '131X_mcRun3_2023cosmics_realistic_deco_v4',
+    'phase1_2023_cosmics'          : '131X_mcRun3_2023cosmics_realistic_deco_v5',
     # GlobalTag for MC production (cosmics) with perfectly aligned and calibrated detector for Phase1 2023, Strip tracker in DECO mode
-    'phase1_2023_cosmics_design'   : '131X_mcRun3_2023cosmics_design_deco_v4',
+    'phase1_2023_cosmics_design'   : '131X_mcRun3_2023cosmics_design_deco_v5',
     # GlobalTag for MC production with realistic conditions for Phase1 2023 detector for Heavy Ion
-    'phase1_2023_realistic_hi'     : '131X_mcRun3_2023_realistic_HI_v9',
+    'phase1_2023_realistic_hi'     : '131X_mcRun3_2023_realistic_HI_v10',
     # GlobalTag for MC production with realistic conditions for Phase1 2024
-    'phase1_2024_realistic'        : '131X_mcRun3_2024_realistic_v2',
+    'phase1_2024_realistic'        : '131X_mcRun3_2024_realistic_v3',
     # GlobalTag for MC production with realistic conditions for Phase2
-    'phase2_realistic'             : '131X_mcRun4_realistic_v3'
+    'phase2_realistic'             : '131X_mcRun4_realistic_v4'
 }
 
 aliases = {
