11a947790eeb23620deb5906a83b76a87e05f564
Dmytro Kovalskyi <dmytro@cern.ch>
2023-05-10 14:08:54 +0200

    formatting

--- GeneratorInterface/GenFilters/plugins/MCMultiSignatureFilter.cc
+++ GeneratorInterface/GenFilters/plugins/MCMultiSignatureFilter.cc
@@ -42,10 +42,10 @@ private:
   const edm::EDGetTokenT<edm::HepMCProduct> token_;
   const bool selectAny_;               // if true select events that satistify any signature, otherwise all
   const std::vector<int> particleID_;  // particle IDs to look for
-  std::vector<double> ptMin_;   // minimum Pt of particles
-  std::vector<double> etaMax_;  // maximum fabs(eta) of particles
-  std::vector<int> status_;     // status of particles
-  std::vector<int> minN_;       // minimum number of particles
+  std::vector<double> ptMin_;          // minimum Pt of particles
+  std::vector<double> etaMax_;         // maximum fabs(eta) of particles
+  std::vector<int> status_;            // status of particles
+  std::vector<int> minN_;              // minimum number of particles
 };
 
 MCMultiSignatureFilter::MCMultiSignatureFilter(const edm::ParameterSet& iConfig)
@@ -56,14 +56,10 @@ MCMultiSignatureFilter::MCMultiSignatureFilter(const edm::ParameterSet& iConfig)
       ptMin_(iConfig.getParameter<std::vector<double> >("PtMin")),
       etaMax_(iConfig.getParameter<std::vector<double> >("EtaMax")),
       status_(iConfig.getParameter<std::vector<int> >("Status")),
-      minN_(iConfig.getParameter<std::vector<int> >("MinN"))
-{
-  if ( particleID_.size() != ptMin_.size() ||
-       particleID_.size() != etaMax_.size() ||
-       particleID_.size() != status_.size() ||
-       particleID_.size() != minN_.size() )
-    throw cms::Exception("BadConfig") << 
-      "MCMultiSignatureFilter: parameter vectors must have the same length";
+      minN_(iConfig.getParameter<std::vector<int> >("MinN")) {
+  if (particleID_.size() != ptMin_.size() || particleID_.size() != etaMax_.size() ||
+      particleID_.size() != status_.size() || particleID_.size() != minN_.size())
+    throw cms::Exception("BadConfig") << "MCMultiSignatureFilter: parameter vectors must have the same length";
 }
 
 // ------------ method called to skim the data  ------------
@@ -76,28 +72,25 @@ bool MCMultiSignatureFilter::filter(edm::StreamID, edm::Event& iEvent, const edm
 
   const HepMC::GenEvent* genEvent = evt->GetEvent();
 
-  for (HepMC::GenEvent::particle_const_iterator p = genEvent->particles_begin(); 
-       p != genEvent->particles_end(); ++p) {
+  for (HepMC::GenEvent::particle_const_iterator p = genEvent->particles_begin(); p != genEvent->particles_end(); ++p) {
     auto genParticle = *p;
-    for (unsigned int i = 0; i < nSignatures; ++i){
-      if ( abs(genParticle->pdg_id()) != abs(particleID_[i]) or
-	   genParticle->momentum().perp() < ptMin_[i] or
-	   fabs(genParticle->momentum().eta()) > etaMax_[i] or
-	   genParticle->status() != status_[i]) 
-	continue;
+    for (unsigned int i = 0; i < nSignatures; ++i) {
+      if (abs(genParticle->pdg_id()) != abs(particleID_[i]) or genParticle->momentum().perp() < ptMin_[i] or
+          fabs(genParticle->momentum().eta()) > etaMax_[i] or genParticle->status() != status_[i])
+        continue;
 
       searchResults[i] += 1;
     }
   }
-  
+
   bool passedAll(true);
   bool passedAny(false);
-  for (unsigned int i = 0; i < nSignatures; ++i){
+  for (unsigned int i = 0; i < nSignatures; ++i) {
     bool passed = searchResults[i] >= minN_[i];
     passedAll = passedAll and passed;
     passedAny = passedAny or passed;
   }
-  
+
   if (selectAny_)
     return passedAny;
   else
