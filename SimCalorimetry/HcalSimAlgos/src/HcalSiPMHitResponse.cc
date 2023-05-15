96bcc1e175845a06c384b253eb706bc460aa0163
Vincenzo Innocente <vincenzo.innocente@cern.ch>
2023-05-15 10:01:24 +0200

    reduce timing for 10x

--- SimCalorimetry/HcalSimAlgos/src/HcalSiPMHitResponse.cc
+++ SimCalorimetry/HcalSimAlgos/src/HcalSiPMHitResponse.cc
@@ -86,14 +86,18 @@ void HcalSiPMHitResponse::add(const CaloSamples& signal) {
     return;
   }
   DetId id(signal.id());
+#ifdef EDM_ML_DEBUG 
   int photonTimeHistSize = nbins * getReadoutFrameSize(id);
   assert(photonTimeHistSize == signal.size());
-  if (precisionTimedPhotons.find(id) == precisionTimedPhotons.end()) {
-    precisionTimedPhotons.insert(std::pair<DetId, photonTimeHist>(id, photonTimeHist(photonTimeHistSize, 0)));
+#endif
+  auto photI = precisionTimedPhotons.find(id);
+  if (photI == precisionTimedPhotons.end()) {
+    photI = precisionTimedPhotons.emplace(id, photonTimeHist(signal.size(), 0)).first;
   }
+  auto & phot = photI->second;
   for (int i = 0; i < signal.size(); ++i) {
     unsigned int photons(signal[i] + 0.5);
-    precisionTimedPhotons[id][i] += photons;
+    phot[i] += photons;
   }
 }
 
