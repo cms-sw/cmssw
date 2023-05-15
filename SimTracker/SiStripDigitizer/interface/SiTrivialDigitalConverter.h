8f51858010a735720e2c148c1817f1ed6842b875
Vincenzo Innocente <vincenzo.innocente@cern.ch>
2023-05-15 16:19:02 +0200

    cleaner, not much faster (cannot vectorize)

--- SimTracker/SiStripDigitizer/interface/SiTrivialDigitalConverter.h
+++ SimTracker/SiStripDigitizer/interface/SiTrivialDigitalConverter.h
@@ -13,10 +13,6 @@ public:
   DigitalRawVecType const& convertRaw(const std::vector<float>&, const SiStripGain*, unsigned int detid) override;
 
 private:
-  int convert(float in) const { return truncate(in * ADCperElectron); }
-  int convertRaw(float in) const { return truncateRaw(in * ADCperElectron); }
-  int truncate(float in_adc) const;
-  int truncateRaw(float in_adc) const;
 
   const float ADCperElectron;
   SiDigitalConverter::DigitalVecType _temp;
