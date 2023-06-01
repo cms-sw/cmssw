3d1f89a22dccefec1c361b3932ae3a05b1939e52
Meng_Lu <1501110102@pku.edu.cn>
2023-06-01 15:45:58 +0200

    Update ReweightUserHooks.h, make it to be able to handle run2 and run3

--- GeneratorInterface/Pythia8Interface/plugins/ReweightUserHooks.h
+++ GeneratorInterface/Pythia8Interface/plugins/ReweightUserHooks.h
@@ -27,7 +27,7 @@ private:
 class PtHatEmpReweightUserHook : public Pythia8::UserHooks {
 public:
   PtHatEmpReweightUserHook(const std::string& tuneName = "") {
-    if (tuneName == "CP5")
+    if (tuneName == "CP5" || tuneName == "CP5Run3")
       p = {7377.94700788, 8.38168461349, -4.70983112392, -0.0310148108446, -0.028798537937, 925.335472326};
     //Default reweighting - works good for tune CUEPT8M1
     else
@@ -37,10 +37,15 @@ public:
            -5.1575514014931e-01,
            5.5951279807561e-02,
            3.5e+02};
-    sigma = [this](double x) -> double {
-      return (p[0] * pow(x, p[2] + p[3] * log(0.01 * x) + p[4] * pow(log(0.01 * x), 2)) *
-              pow(1 - 2 * x / (13600. + p[5]), p[1])) *
-             x;
+    if (tuneName == "CP5Run3")
+      sigma = [this](double x) -> double {
+        return (p[0] * pow(x, p[2] + p[3] * log(0.01 * x) + p[4] * pow(log(0.01 * x), 2)) *
+              pow(1 - 2 * x / (13600. + p[5]), p[1])) * x;
+    else
+      sigma = [this](double x) -> double {
+        return (p[0] * pow(x, p[2] + p[3] * log(0.01 * x) + p[4] * pow(log(0.01 * x), 2)) *
+              pow(1 - 2 * x / (13000. + p[5]), p[1])) * x;
+      
     };
   }
   ~PtHatEmpReweightUserHook() override {}
