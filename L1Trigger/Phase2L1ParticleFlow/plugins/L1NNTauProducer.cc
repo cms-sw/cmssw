70daec1708cdabc874b4934620d15b25c13b8b41
Duchstf <minhduc8199@gmail.com>
2023-06-07 03:18:26 -0500

    code formatting

--- L1Trigger/Phase2L1ParticleFlow/plugins/L1NNTauProducer.cc
+++ L1Trigger/Phase2L1ParticleFlow/plugins/L1NNTauProducer.cc
@@ -265,8 +265,8 @@ void L1NNTauProducer::makeTau_HW(const l1t::PFCandidate& seed,
 
   //Firmware Tau
   l1ct::Tau l1ctTau;
-  l1ctTau.hwPt = l1ct::pt_t(pt);  //l1gt is <16,11> and currently <16,14>
-  l1ctTau.hwEta = l1ct::Scales::makeGlbEta(seed.eta()); // seed.eta() and seed.phi() are in physical coordinates
+  l1ctTau.hwPt = l1ct::pt_t(pt);                         //l1gt is <16,11> and currently <16,14>
+  l1ctTau.hwEta = l1ct::Scales::makeGlbEta(seed.eta());  // seed.eta() and seed.phi() are in physical coordinates
   l1ctTau.hwPhi = l1ct::Scales::makeGlbPhi(seed.phi());
 
   l1ctTau.hwSeedPt = seed.pt();
