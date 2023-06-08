492128bc1ceebd009ae1c1de9440b8a9cc7a6c63
Felice Pantaleo <felice.pantaleo@cern.ch>
2023-06-08 16:20:06 +0200

    fix read out of bounds in CLUE3D

--- RecoHGCal/TICL/plugins/PatternRecognitionbyCLUE3D.cc
+++ RecoHGCal/TICL/plugins/PatternRecognitionbyCLUE3D.cc
@@ -686,7 +686,7 @@ void PatternRecognitionbyCLUE3D<TILES>::calculateDistanceToHigher(
       int etaWindow = 1;
       int phiWindow = 1;
       int etaBinMin = std::max(tileOnLayer.etaBin(clustersOnLayer.eta[i]) - etaWindow, 0);
-      int etaBinMax = std::min(tileOnLayer.etaBin(clustersOnLayer.eta[i]) + etaWindow, nEtaBin);
+      int etaBinMax = std::min(tileOnLayer.etaBin(clustersOnLayer.eta[i]) + etaWindow, nEtaBin - 1);
       int phiBinMin = tileOnLayer.phiBin(clustersOnLayer.phi[i]) - phiWindow;
       int phiBinMax = tileOnLayer.phiBin(clustersOnLayer.phi[i]) + phiWindow;
       for (int ieta = etaBinMin; ieta <= etaBinMax; ++ieta) {
