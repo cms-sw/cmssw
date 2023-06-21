b4e5059eccc1e3e5bcf43b899e26615b589f1c1d
Aaron Bundock <aaron.bundock@cern.ch>
2023-06-21 15:23:10 +0200

    Update customise settings

--- L1Trigger/Configuration/python/customiseSettings.py
+++ L1Trigger/Configuration/python/customiseSettings.py
@@ -2,12 +2,28 @@ from __future__ import print_function
 import os.path
 import FWCore.ParameterSet.Config as cms
 
+def L1TSettingsToCaloParams_2023_v0_3_ppRef(process):
+    process.load("L1Trigger.L1TCalorimeter.caloParams_2023_v0_3_ppRef_cfi")
+    return process
+
+def L1TSettingsToCaloParamsHI_2023_v0_3(process):
+    process.load("L1Trigger.L1TCalorimeter.caloParamsHI_2023_v0_3_cfi")
+    return process
+
+def L1TSettingsToCaloParams_2023_v0_3(process):
+    process.load("L1Trigger.L1TCalorimeter.caloParams_2023_v0_3_cfi")
+    return process
+
 def L1TSettingsToCaloParams_2023_v0_2(process):
     process.load("L1Trigger.L1TCalorimeter.caloParams_2023_v0_2_cfi")
     return process
 
 def L1TSettingsToCaloParams_2023_v0_1(process):
     process.load("L1Trigger.L1TCalorimeter.caloParams_2023_v0_1_cfi")
+    return process
+
+def L1TSettingsToCaloParamsHI_2023_v0_0(process):
+    process.load("L1Trigger.L1TCalorimeter.caloParamsHI_2023_v0_0_cfi")
     return process
 
 def L1TSettingsToCaloParamsHI_2022_v0_6(process):
