7351d1468b9957d9e8fc0172bb63816370f82ebe
Artur Gottmann <artur.gottmann@desy.de>
2023-06-23 11:59:32 +0200

    Add 3rd HLT path to fix behaviour for 2016 samples

--- TauAnalysis/MCEmbeddingTools/python/SelectingProcedure_cff.py
+++ TauAnalysis/MCEmbeddingTools/python/SelectingProcedure_cff.py
@@ -10,7 +10,7 @@ doubleMuonHLTTrigger = cms.EDFilter("TriggerResultsFilter",
     hltResults = cms.InputTag("TriggerResults","","HLT"),
     l1tResults = cms.InputTag(""),
     throw = cms.bool(False),
-    triggerConditions = cms.vstring("HLT_Mu17_TrkIsoVVL_Mu8_TrkIsoVVL_DZ_v* OR HLT_Mu17_TrkIsoVVL_Mu8_TrkIsoVVL_DZ_Mass8_v*")
+    triggerConditions = cms.vstring("HLT_Mu17_TrkIsoVVL_Mu8_TrkIsoVVL_DZ_v* OR HLT_Mu17_TrkIsoVVL_Mu8_TrkIsoVVL_DZ_Mass8_v* OR HLT_Mu17_TrkIsoVVL_TkMu8_TrkIsoVVL_DZ_v*")
 )
 
 
