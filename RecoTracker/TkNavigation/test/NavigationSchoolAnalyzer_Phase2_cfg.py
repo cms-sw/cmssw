724877aa739bfce6965f3d6eafc02d9e149c01b8
mmusich <marco.musich@cern.ch>
2023-06-30 11:10:36 +0200

    separate phase-1 from phase-2 NavigationSchoolAnalyzer configurations and add a unit test

--- RecoTracker/TkNavigation/test/NavigationSchoolAnalyzer_Phase2_cfg.py
+++ RecoTracker/TkNavigation/test/NavigationSchoolAnalyzer_Phase2_cfg.py
@@ -3,12 +3,16 @@ import FWCore.ParameterSet.Config as cms
 # set the geometry and the GlobalTag
 
 process = cms.Process("NavigationSchoolAnalyzer")
-process.load("Configuration.StandardSequences.GeometryDB_cff")
-process.load('Configuration.StandardSequences.GeometryRecoDB_cff')
+
+# the following lines are kept for when the phase-2 geometry wil be moved to DB
+#process.load("Configuration.StandardSequences.GeometryDB_cff")
+#process.load('Configuration.StandardSequences.GeometryRecoDB_cff')
+
+process.load('Configuration.Geometry.GeometryExtended2026D98Reco_cff')
 process.load("Configuration.StandardSequences.MagneticField_cff")
 process.load('Configuration.StandardSequences.FrontierConditions_GlobalTag_cff')
 from Configuration.AlCa.GlobalTag import GlobalTag
-process.GlobalTag = GlobalTag(process.GlobalTag, 'auto:phase1_2022_realistic', '')
+process.GlobalTag = GlobalTag(process.GlobalTag, 'auto:phase2_realistic_T25', '')
 process.load("RecoTracker.TkNavigation.NavigationSchoolESProducer_cff")
 
 #process.MessageLogger = cms.Service("MessageLogger",
@@ -21,7 +25,9 @@ process.load("RecoTracker.TkNavigation.NavigationSchoolESProducer_cff")
 
 
 #This has to be modified in order to read the tracker + MTD structure
-process.TrackerRecoGeometryESProducer = cms.ESProducer("TrackerRecoGeometryESProducer")
+process.TrackerRecoGeometryESProducer = cms.ESProducer("TrackerMTDRecoGeometryESProducer",
+    usePhase2Stacks = cms.bool(False)
+)
 
 process.maxEvents = cms.untracked.PSet(
     input = cms.untracked.int32(1)
@@ -37,3 +43,4 @@ process.navigationSchoolAnalyzer = cms.EDAnalyzer("NavigationSchoolAnalyzer",
 process.muonNavigationTest = cms.EDAnalyzer("MuonNavigationTest")
 
 process.p = cms.Path(process.navigationSchoolAnalyzer+process.muonNavigationTest)
+
