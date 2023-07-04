e5d2c09d277246d003b3d02115f62be3171ab6c9
Saumya Phor <saumya.phor4252@gmail.com>
2023-07-04 10:05:19 +0200

    Revert the 2024 GT back since not necessary

--- Configuration/AlCa/python/autoCond.py
+++ Configuration/AlCa/python/autoCond.py
@@ -84,7 +84,7 @@ autoCond = {
     # GlobalTag for MC production with realistic conditions for Phase1 2023 detector for Heavy Ion
     'phase1_2023_realistic_hi'     : '131X_mcRun3_2023_realistic_HI_v12',
     # GlobalTag for MC production with realistic conditions for Phase1 2024
-    'phase1_2024_realistic'        : '131X_mcRun3_2024_realistic_v5',
+    'phase1_2024_realistic'        : '131X_mcRun3_2024_realistic_v4',
     # GlobalTag for MC production with realistic conditions for Phase2
     'phase2_realistic'             : '131X_mcRun4_realistic_v6'
 }
