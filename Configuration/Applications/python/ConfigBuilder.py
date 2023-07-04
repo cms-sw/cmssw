c23b5b8c77d4c9510739eef9b09f3d263c9dc20f
Simone Pigazzi <simone.pigazzini@live.it>
2023-07-04 15:42:13 +0200

    add autoNANO mapping and modify prepare_NANO accordingly

--- Configuration/Applications/python/ConfigBuilder.py
+++ Configuration/Applications/python/ConfigBuilder.py
@@ -1073,6 +1073,7 @@ class ConfigBuilder(object):
         #TODO: Check based of file input
         self.NANOGENDefaultSeq='nanogenSequence'
         self.NANODefaultSeq='nanoSequence'
+        self.NANODefaultCustom='nanoAOD_customizeCommon'
 
         self.EVTCONTDefaultCFF="Configuration/EventContent/EventContent_cff"
 
@@ -1776,9 +1777,32 @@ class ConfigBuilder(object):
         print(f"in prepare_nano {stepSpec}")
         ''' Enrich the schedule with NANO '''
         _,_nanoSeq,_nanoCff = self.loadDefaultOrSpecifiedCFF(stepSpec,self.NANODefaultCFF,self.NANODefaultSeq)
-        self.scheduleSequence(_nanoSeq,'nanoAOD_step')
-        custom = "nanoAOD_customizeCommon"
-        self._options.customisation_file.insert(0,'.'.join([_nanoCff,custom]))
+        
+        # create full specified sequence using autoNANO 
+        from PhysicsTools.NanoAOD.autoNANO import autoNANO, expandNanoMapping
+        # if not a autoNANO mapping, load an empty customization, which later will be converted into the default.
+        _nanoCustoms = _nanoSeq.split('+') if '@' in stepSpec else [''] 
+        _nanoSeq = _nanoSeq.split('+')
+        expandNanoMapping(_nanoSeq, autoNANO, 'sequence')
+        expandNanoMapping(_nanoCustoms, autoNANO, 'customize')
+        # make sure there are no duplicates while preserving the ordering
+        _nanoSeq = list(sorted(set(_nanoSeq), key=_nanoSeq.index))
+        _nanoCustoms = list(sorted(set(_nanoCustoms), key=_nanoCustoms.index))
+        # replace empty sequence with default
+        _nanoSeq = [seq if seq!='' else self.NANODefaultSeq for seq in _nanoSeq]    
+        _nanoCustoms = [cust if cust!='' else self.NANODefaultCustom for cust in _nanoCustoms]   
+        # build and inject the sequence
+        print('NANO sequence:', _nanoSeq)
+        print('NANO customizations:', _nanoCustoms)
+        if len(_nanoSeq) < 1 and '@' in stepSpec:
+            raise Exception(f'The specified mapping: {stepSpec} generates an empty NANO sequence. Please provide a valid mappign')
+        self.scheduleSequence('+'.join(_nanoSeq), 'nanoAOD_step')
+        
+        # add the customisations
+        for custom in _nanoCustoms:
+            custom_path = custom if '.' in custom else '.'.join([_nanoCff,custom]) 
+            # customization order can be important for NANO, here later specified customise take precedence
+            self._options.customisation_file.append(custom_path)
         if self._options.hltProcess:
             if len(self._options.customise_commands) > 1:
                 self._options.customise_commands = self._options.customise_commands + " \n"
