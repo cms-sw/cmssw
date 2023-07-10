c9eb70ee7e3e06e36f73cc4665e3b74937cb08cf
Luca Guzzi <luca.guzzi@mib.infn.it>
2023-07-10 12:02:17 +0200

    remove fully duplicated pixel modules from modules and paths

--- HLTrigger/Configuration/python/HLT_75e33/modules/hltPhase2L3MuonPixelTracks_cfi.py
+++ HLTrigger/Configuration/python/HLT_75e33/modules/hltPhase2L3MuonPixelTracks_cfi.py
@@ -2,8 +2,8 @@ import FWCore.ParameterSet.Config as cms
 
 hltPhase2L3MuonPixelTracks = cms.EDProducer("PixelTrackProducer",
     Cleaner = cms.string('hltPhase2L3MuonPixelTrackCleanerBySharedHits'),
-    Filter = cms.InputTag("hltPhase2L3MuonPixelTrackFilterByKinematics"),
-    Fitter = cms.InputTag("hltPhase2L3MuonPixelFitterByHelixProjections"),
+    Filter = cms.InputTag("pixelTrackFilterByKinematics"),
+    Fitter = cms.InputTag("pixelFitterByHelixProjections"),
     SeedingHitSets = cms.InputTag("hltPhase2L3MuonPixelTracksHitQuadruplets"),
     mightGet = cms.optional.untracked.vstring,
     passLabel = cms.string('hltPhase2L3MuonPixelTracks')
--- HLTrigger/Configuration/python/HLT_75e33/paths/HLT_TriMu_10_5_5_DZ_FromL1TkMuon_cfi.py
+++ HLTrigger/Configuration/python/HLT_75e33/paths/HLT_TriMu_10_5_5_DZ_FromL1TkMuon_cfi.py
@@ -48,8 +48,8 @@ from ..modules.hltPhase2L3FromL1TkMuonTrimmedPixelVertices_cfi import *
 from ..modules.hltPhase2L3GlbMuon_cfi import *
 from ..modules.hltPhase2L3MuonCandidates_cfi import *
 from ..modules.hltPhase2L3MuonMerged_cfi import *
-from ..modules.hltPhase2L3MuonPixelTracksFilter_cfi import *
-from ..modules.hltPhase2L3MuonPixelTracksFitter_cfi import *
+from ..modules.pixelTrackFilterByKinematics_cfi import *
+from ..modules.pixelFitterByHelixProjections_cfi import *
 from ..modules.hltPhase2L3Muons_cfi import *
 from ..modules.hltPhase2L3MuonsNoID_cfi import *
 from ..modules.hltPhase2L3MuonTracks_cfi import *
@@ -123,8 +123,8 @@ HLT_TriMu_10_5_5_DZ_FromL1TkMuon = cms.Path(
         hltPhase2L3GlbMuon,
         hltPhase2L3MuonCandidates,
         hltPhase2L3MuonMerged,
-        hltPhase2L3MuonPixelTracksFilter,
-        hltPhase2L3MuonPixelTracksFitter,
+        pixelTrackFilterByKinematics,
+        pixelFitterByHelixProjections,
         hltPhase2L3MuonTracks,
         hltPhase2L3Muons,
         hltPhase2L3MuonsNoID,
