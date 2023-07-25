c16502b1aa1d73b63535eec9c12d43ba9122848d
Fabio Cossutti <fabio.cossutti@ts.infn.it>
2023-07-25 14:21:56 +0200

    Clean Geometry/MTD* for obsolete ETL ring scenario

--- Geometry/MTDCommonData/src/ETLNumberingScheme.cc
+++ Geometry/MTDCommonData/src/ETLNumberingScheme.cc
@@ -32,10 +32,6 @@ uint32_t ETLNumberingScheme::getUnitID(const MTDBaseNumber& baseNumber) const {
     return 0;
   }
 
-  // Discriminate pre-TDR and TDR scenarios
-
-  const bool preTDR = (baseNumber.getLevelName(3).find("Ring") != std::string::npos);
-
   const uint32_t modCopy(baseNumber.getCopyNumber(2));
 
   const std::string_view& ringName(baseNumber.getLevelName(3));  // name of ring volume
@@ -44,23 +40,20 @@ uint32_t ETLNumberingScheme::getUnitID(const MTDBaseNumber& baseNumber) const {
   int ringCopy(::atoi(baseName.data() + 4));
 
   uint32_t discN, sectorS, sectorN;
-  if (!preTDR) {
-    discN = (baseNumber.getLevelName(4).find("Disc1") != std::string::npos) ? 0 : 1;
-    sectorS = (baseNumber.getLevelName(3).find("Front") != std::string::npos) ? 0 : 1;
-    sectorN = baseNumber.getCopyNumber(3);
+  discN = (baseNumber.getLevelName(4).find("Disc1") != std::string::npos) ? 0 : 1;
+  sectorS = (baseNumber.getLevelName(3).find("Front") != std::string::npos) ? 0 : 1;
+  sectorN = baseNumber.getCopyNumber(3);
 
-    ETLDetId tmpId;
-    ringCopy = static_cast<int>(tmpId.encodeSector(discN, sectorS, sectorN));
+  ETLDetId tmpId;
+  ringCopy = static_cast<int>(tmpId.encodeSector(discN, sectorS, sectorN));
 
-    modtyp = (baseNumber.getLevelName(2).find("_Left") != std::string::npos) ? 1 : 2;
-  }
+  modtyp = (baseNumber.getLevelName(2).find("_Left") != std::string::npos) ? 1 : 2;
 
-  // Side choice: up to scenario D38 is given by level 7 (HGCal v9)
-  int nSide(7);
+  int nSide(8);
   const std::string_view& sideName(baseNumber.getLevelName(nSide));
   // Side choice: from scenario D41 is given by level 8 (HGCal v10)
-  if (sideName.find("CALOECTSFront") != std::string::npos) {
-    nSide = 8;
+  if (sideName.find("CALOECTSFront") == std::string::npos) {
+    edm::LogError("MTDGeom") << "ETLNumberingScheme: incorrect volume stack found!";
   }
   const uint32_t sideCopy(baseNumber.getCopyNumber(nSide));
   const uint32_t zside(sideCopy == 1 ? 1 : 0);
@@ -76,24 +69,21 @@ uint32_t ETLNumberingScheme::getUnitID(const MTDBaseNumber& baseNumber) const {
 
   // error checking
 
-  if ((modtyp != 0 && preTDR) || (modtyp == 0 && !preTDR)) {
+  if (modtyp == 0) {
     edm::LogWarning("MTDGeom") << "ETLNumberingScheme::getUnitID(): "
                                << "****************** Bad module name = " << modtyp
                                << ", Volume Name = " << baseNumber.getLevelName(4);
     return 0;
   }
 
-  if ((preTDR && (1 > modCopy || ETLDetId::kETLv1maxModule < modCopy)) ||
-      (!preTDR && (1 > modCopy ||
-                   static_cast<unsigned>(std::max(ETLDetId::kETLv4maxModule, ETLDetId::kETLv5maxModule)) < modCopy))) {
+  if (1 > modCopy || static_cast<unsigned>(std::max(ETLDetId::kETLv4maxModule, ETLDetId::kETLv5maxModule)) < modCopy) {
     edm::LogWarning("MTDGeom") << "ETLNumberingScheme::getUnitID(): "
                                << "****************** Bad module copy = " << modCopy
                                << ", Volume Number = " << baseNumber.getCopyNumber(4);
     return 0;
   }
 
-  if ((preTDR && (1 > ringCopy || ETLDetId::kETLv1maxRing < ringCopy)) ||
-      (!preTDR && (1 > ringCopy || ETLDetId::kETLv4maxRing < ringCopy))) {
+  if (1 > ringCopy || ETLDetId::kETLv4maxRing < ringCopy) {
     edm::LogWarning("MTDGeom") << "ETLNumberingScheme::getUnitID(): "
                                << "****************** Bad ring copy = " << ringCopy
                                << ", Volume Number = " << baseNumber.getCopyNumber(3);
@@ -110,14 +100,12 @@ uint32_t ETLNumberingScheme::getUnitID(const MTDBaseNumber& baseNumber) const {
                           << " ring = " << ringCopy << " zside = " << zside << " module = " << modCopy
                           << " modtyp = " << modtyp << " Raw Id = " << intindex << thisETLdetid;
 #endif
-  if (!preTDR) {
-    ETLDetId altETLdetid(zside, discN, sectorS, sectorN, modCopy, modtyp);
-    const uint32_t altintindex = altETLdetid.rawId();
-    if (intindex != altintindex) {
-      edm::LogWarning("MTDGeom") << "Incorrect alternative construction \n"
-                                 << "disc = " << discN << " disc side = " << sectorS << " sector = " << sectorN << "\n"
-                                 << altETLdetid;
-    }
+  ETLDetId altETLdetid(zside, discN, sectorS, sectorN, modCopy, modtyp);
+  const uint32_t altintindex = altETLdetid.rawId();
+  if (intindex != altintindex) {
+    edm::LogWarning("MTDGeom") << "Incorrect alternative construction \n"
+                               << "disc = " << discN << " disc side = " << sectorS << " sector = " << sectorN << "\n"
+                               << altETLdetid;
   }
 
   return intindex;
