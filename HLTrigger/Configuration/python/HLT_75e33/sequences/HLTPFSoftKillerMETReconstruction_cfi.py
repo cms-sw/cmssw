32c42d421964797b77a38b9450e352f612d87103
Theo Chatzistavrou <teophysix@gmail.com>
2023-07-25 09:19:25 +0200

    remove_unnecessary_modules

--- HLTrigger/Configuration/python/HLT_75e33/sequences/HLTPFSoftKillerMETReconstruction_cfi.py
+++
@@ -1,9 +0,0 @@
-import FWCore.ParameterSet.Config as cms
-
-from ..modules.hltParticleFlowSoftKiller_cfi import *
-from ..modules.hltPFSoftKillerMET_cfi import *
-
-HLTPFSoftKillerMETReconstruction = cms.Sequence(
-    hltParticleFlowSoftKiller +
-    hltPFSoftKillerMET
-)
