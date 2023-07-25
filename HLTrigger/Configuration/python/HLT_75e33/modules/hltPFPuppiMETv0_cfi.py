c93dd3bca6847ff21eacabf1ac7019c129ba0851
Theo Chatzistavrou <teophysix@gmail.com>
2023-07-25 09:19:25 +0200

    remove_unnecessary_modules

--- HLTrigger/Configuration/python/HLT_75e33/modules/hltPFPuppiMETv0_cfi.py
+++
@@ -1,9 +0,0 @@
-import FWCore.ParameterSet.Config as cms
-
-hltPFPuppiMETv0 = cms.EDProducer("PFMETProducer",
-    applyWeight = cms.bool(True),
-    calculateSignificance = cms.bool(False),
-    globalThreshold = cms.double(0.0),
-    src = cms.InputTag("particleFlowTmp"),
-    srcWeights = cms.InputTag("hltPFPuppi")
-)
