cf21c1163c94f8631f44e0e58f676c8e0cdfb0b7
ppsgit <ppsgit@cern.ch>
2023-07-26 10:50:22 +0200

    Change number of events to process

--- RecoPPS/RPixEfficiencyTools/python/PPSAlcaReco_fromDIGI_cfg.py
+++ RecoPPS/RPixEfficiencyTools/python/PPSAlcaReco_fromDIGI_cfg.py
@@ -63,7 +63,7 @@ process.MessageLogger = cms.Service("MessageLogger",
 
 
 #CONFIGURE PROCESS
-process.maxEvents = cms.untracked.PSet( input = cms.untracked.int32(1000) )
+process.maxEvents = cms.untracked.PSet( input = cms.untracked.int32(-1) )
 
 if options.useJsonFile == True:
     import FWCore.PythonUtilities.LumiList as LumiList
