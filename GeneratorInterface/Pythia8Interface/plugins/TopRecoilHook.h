84d8e12c74fb1dfab8fd5bc9ce7e11114002312b
vslokenb <108902248+vslokenb@users.noreply.github.com>
2023-07-27 15:19:21 +0200

    Update TopRecoilHook.h
    Follow fix suggested by PR 42313 to remove useless check.

--- GeneratorInterface/Pythia8Interface/plugins/TopRecoilHook.h
+++ GeneratorInterface/Pythia8Interface/plugins/TopRecoilHook.h
@@ -33,11 +33,7 @@ namespace Pythia8 {
     }
 
     // Destructor prints histogram.
-    ~TopRecoilHook() override {
-      if (doTopRecoil)
-        cout << *wtCorr;
-      delete wtCorr;
-    }
+     ~TopRecoilHook() override { delete wtCorr; }
 
     // Initialise. Only use hook for simple showers with recoilToColoured = off.
     bool initAfterBeams() override {
