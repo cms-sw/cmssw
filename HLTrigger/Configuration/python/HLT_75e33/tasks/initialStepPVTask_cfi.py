dee997c4295049b8a1ed7400c1f4d495b9dd4db6
Luca Guzzi <luca.guzzi@mib.infn.it>
2023-07-31 18:46:46 +0200

    remove caloTowerForTrk duplicated module in favour of hltTowerMakerForAll

--- HLTrigger/Configuration/python/HLT_75e33/tasks/initialStepPVTask_cfi.py
+++ HLTrigger/Configuration/python/HLT_75e33/tasks/initialStepPVTask_cfi.py
@@ -1,11 +1,11 @@
 import FWCore.ParameterSet.Config as cms
 
 from ..modules.ak4CaloJetsForTrk_cfi import *
-from ..modules.caloTowerForTrk_cfi import *
+from ..modules.hltTowerMakerForAll_cfi import *
 from ..modules.firstStepPrimaryVerticesUnsorted_cfi import *
 
 initialStepPVTask = cms.Task(
     ak4CaloJetsForTrk,
-    caloTowerForTrk,
+    hltTowerMakerForAll,
     firstStepPrimaryVerticesUnsorted,
 )
