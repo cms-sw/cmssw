bcd98709db3c8577a8aca3c5548edb6eb165ec8d
Luca Guzzi <luca.guzzi@mib.infn.it>
2023-08-02 18:04:08 +0200

    rename pixelTracks->hltPhase2PixelTracks

--- HLTrigger/Configuration/python/HLT_75e33/tasks/pixelTracksTask_cfi.py
+++ HLTrigger/Configuration/python/HLT_75e33/tasks/pixelTracksTask_cfi.py
@@ -2,7 +2,7 @@ import FWCore.ParameterSet.Config as cms
 
 from ..modules.hltPhase2PixelFitterByHelixProjections_cfi import *
 from ..modules.hltPhase2PixelTrackFilterByKinematics_cfi import *
-from ..modules.pixelTracks_cfi import *
+from ..modules.hltPhase2PixelTracks_cfi import *
 from ..modules.hltPhase2PixelTracksHitDoublets_cfi import *
 from ..modules.hltPhase2PixelTracksHitSeeds_cfi import *
 from ..modules.hltPhase2PixelTracksSeedLayers_cfi import *
@@ -11,7 +11,7 @@ from ..modules.hltPhase2PixelTracksAndHighPtStepTrackingRegions_cfi import *
 pixelTracksTask = cms.Task(
     hltPhase2PixelFitterByHelixProjections,
     hltPhase2PixelTrackFilterByKinematics,
-    pixelTracks,
+    hltPhase2PixelTracks,
     hltPhase2PixelTracksHitDoublets,
     hltPhase2PixelTracksHitSeeds,
     hltPhase2PixelTracksSeedLayers,
