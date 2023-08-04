81dde92486885f44f184837655d7740fa27ae199
vslokenb <108902248+vslokenb@users.noreply.github.com>
2023-08-04 13:40:26 +0200

    Update GeneratorInterface/Pythia8Interface/plugins/TopRecoilHook.h
    Co-authored-by: Andrea Perrotta <perrotta@cern.ch>

--- GeneratorInterface/Pythia8Interface/plugins/TopRecoilHook.h
+++ GeneratorInterface/Pythia8Interface/plugins/TopRecoilHook.h
@@ -33,7 +33,7 @@ namespace Pythia8 {
     }
 
     // Destructor prints histogram.
-     ~TopRecoilHook() override { delete wtCorr; }
+    ~TopRecoilHook() override { delete wtCorr; }
 
     // Initialise. Only use hook for simple showers with recoilToColoured = off.
     bool initAfterBeams() override {
