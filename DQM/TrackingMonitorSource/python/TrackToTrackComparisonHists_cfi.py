e98b350a3b1fd7e6807f1e4b54934731f82d189a
acmbulla <a.bulla@campus.unimib.it>
2023-08-11 15:04:12 +0200

    little fix to previous commit

--- DQM/TrackingMonitorSource/python/TrackToTrackComparisonHists_cfi.py
+++ DQM/TrackingMonitorSource/python/TrackToTrackComparisonHists_cfi.py
@@ -2,6 +2,9 @@ import FWCore.ParameterSet.Config as cms
 
 from DQM.TrackingMonitorSource.trackToTrackComparisonHists_cfi import trackToTrackComparisonHists
 from Configuration.Eras.Modifier_run3_common_cff import run3_common
+from Configuration.Eras.Modifier_phase2_common_cff import phase2_common
 
-run3_common.toModify(trackToTrackComparisonHists.histoPSet, onlinelumi_nbin=375, onlinelumi_rangeMin=200., onlinelumi_rangeMax=25000.)
-run3_common.toModify(trackToTrackComparisonHists.histoPSet, PU_nbin=200, PU_rangeMin=0., PU_rangeMax=200.)
+TrackToTrackComparisonHists = trackToTrackComparisonHists.clone()
+
+run3_common.toModify(TrackToTrackComparisonHists.histoPSet, onlinelumi_nbin=375, onlinelumi_rangeMin=200., onlinelumi_rangeMax=25000.)
+phase2_common.toModify(TrackToTrackComparisonHists.histoPSet, PU_nbin=200, PU_rangeMin=0., PU_rangeMax=200.)
