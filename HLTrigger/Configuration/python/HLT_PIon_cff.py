0f24a54e2048a65e96f9e8d9339d98d53d1b2382
Marino Missiroli <m.missiroli@cern.ch>
2023-08-14 18:27:43 +0200

    HLT menu development for 13_0_X (branch: 13_3_X)
    Development of HLT menus for `CMSSW_13_0_X`.
    
    This update includes the integration of the following JIRA ticket.
    
     - [CMSHLT-2889](https://its.cern.ch/jira/browse/CMSHLT-2889): [TRK] add doublet-recovery tracks to EventContent of `HLTMonitor` stream

--- HLTrigger/Configuration/python/HLT_PIon_cff.py
+++ HLTrigger/Configuration/python/HLT_PIon_cff.py
@@ -1,6 +1,6 @@
 # hltGetConfiguration /dev/CMSSW_13_0_0/PIon --cff --data --type PIon
 
-# /dev/CMSSW_13_0_0/PIon/V152 (CMSSW_13_0_2)
+# /dev/CMSSW_13_0_0/PIon/V153 (CMSSW_13_0_2)
 
 import FWCore.ParameterSet.Config as cms
 
@@ -12,7 +12,7 @@ fragment = cms.ProcessFragment( "HLT" )
 fragment.ProcessAcceleratorCUDA = ProcessAcceleratorCUDA()
 
 fragment.HLTConfigVersion = cms.PSet(
-  tableName = cms.string('/dev/CMSSW_13_0_0/PIon/V152')
+  tableName = cms.string('/dev/CMSSW_13_0_0/PIon/V153')
 )
 
 fragment.HLTIter0PSetTrajectoryBuilderIT = cms.PSet( 
