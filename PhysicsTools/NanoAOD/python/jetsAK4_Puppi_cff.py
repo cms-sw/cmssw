31a828f4b8890e504f88dc6fd9c1b3b2d6cdb89a
Nurfikri Norjoharuddeen <nurfikri89@gmail.com>
2023-08-18 19:17:50 +0200

    Fix typo. Add labelName.

--- PhysicsTools/NanoAOD/python/jetsAK4_Puppi_cff.py
+++ PhysicsTools/NanoAOD/python/jetsAK4_Puppi_cff.py
@@ -160,11 +160,12 @@ def nanoAOD_addDeepInfoAK4(process,addParticleNet,addRobustParTAK4=False):
         jetSource = cms.InputTag('slimmedJetsPuppi'),
         jetCorrections = ('AK4PFPuppi', cms.vstring(['L2Relative', 'L3Absolute']), 'None'),
         btagDiscriminators = _btagDiscriminators,
+        labelName = "Puppi",
         postfix = 'WithDeepInfo',
     )
     process.load("Configuration.StandardSequences.MagneticField_cff")
-    process.jetPuppiCorrFactorsNano.src="selectedUpdatedPatJetsWithDeepInfo"
-    process.updatedJets.jetSource="selectedUpdatedPatJetsWithDeepInfo"
+    process.jetPuppiCorrFactorsNano.src="selectedUpdatedPatJetsPuppiWithDeepInfo"
+    process.updatedJetsPuppi.jetSource="selectedUpdatedPatJetsPuppiWithDeepInfo"
     return process
 
 nanoAOD_addDeepInfoAK4_switch = cms.PSet(
