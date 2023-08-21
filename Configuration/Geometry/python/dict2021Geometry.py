97e82ae707f5fbcd4edfc4a8fea467b41ee6856c
Sunanda <sunanda.banerjee@cern.ch>
2023-08-21 18:48:01 +0200

    Backport PRs #42463, #42496, #42549, #42550, #42557, #42578 to fix run3 scenario in CMSSW version 13_1_X due to issues in HCAL

--- Configuration/Geometry/python/dict2021Geometry.py
+++ Configuration/Geometry/python/dict2021Geometry.py
@@ -1339,11 +1339,11 @@ caloDict = {
             'Geometry/HcalCommonData/data/hcalforwardalgo.xml',
             'Geometry/HcalCommonData/data/average/hcalforwardmaterial.xml',
             'Geometry/HcalCommonData/data/hcalSimNumbering/2021/v1/hcalSimNumbering.xml', 
-            'Geometry/HcalCommonData/data/hcalRecNumbering/2021/hcalRecNumbering.xml',
+            'Geometry/HcalCommonData/data/hcalRecNumbering/2021/v1/hcalRecNumbering.xml',
         ],
         3 : [
             'Geometry/EcalSimData/data/ecalsens.xml',
-            'Geometry/HcalCommonData/data/hcalsens/2021/v1/hcalsenspmf.xml',
+            'Geometry/HcalCommonData/data/hcalsens/2021/v2/hcalsenspmf.xml',
             'Geometry/HcalSimData/data/hf.xml',
             'Geometry/HcalSimData/data/hfpmt.xml',
             'Geometry/HcalSimData/data/hffibrebundle.xml',
