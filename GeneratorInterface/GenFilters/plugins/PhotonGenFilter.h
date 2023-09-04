fc7175cc6267da349d16b161b2bfb9e86fdd740a
Michele Mormile <michele.mormile@cern.ch>
2023-09-04 20:20:31 +0200

    Added GenFilter to select only events with a photon with specified characteristics

---
+++ GeneratorInterface/GenFilters/plugins/PhotonGenFilter.h
@@ -0,0 +1,40 @@
+#ifndef PhotonGenFilter_h
+#define PhotonGenFilter_h
+
+#include <memory>
+#include <iostream>
+
+#include "FWCore/Framework/interface/Frameworkfwd.h"
+#include "FWCore/Framework/interface/global/EDFilter.h"
+
+#include "FWCore/Framework/interface/Event.h"
+#include "FWCore/Framework/interface/MakerMacros.h"
+
+#include "FWCore/ParameterSet/interface/ParameterSet.h"
+
+
+namespace edm {
+  class HepMCProduct;
+}
+
+class PhotonGenFilter : public edm::global::EDFilter<> {
+public:
+    explicit PhotonGenFilter(const edm::ParameterSet&);
+    ~PhotonGenFilter() override;
+    
+    bool filter(edm::StreamID, edm::Event&, const edm::EventSetup&) const override;
+private:
+    // Private member variables and functions
+
+  const edm::EDGetTokenT<edm::HepMCProduct> token_;
+  std::vector<int> particleID;
+  std::vector<double> ptMin;
+  std::vector<double> etaMin;
+  std::vector<double> etaMax;
+  std::vector<double> drMin;
+  std::vector<int> status;
+  const double betaBoost;
+
+};
+
+#endif
