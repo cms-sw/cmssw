fa291dd4479d5d47f32504b593165e81eba210bb
mmusich <marco.musich@cern.ch>
2023-09-14 11:00:21 +0200

    substitute the Phase-1 pixel HLT offline monitoring with the Phase-2 one in the case of the phase-2 HLT monitoring

--- DQMOffline/Trigger/python/SiPixel_OfflineMonitoring_cff.py
+++ DQMOffline/Trigger/python/SiPixel_OfflineMonitoring_cff.py
@@ -7,13 +7,8 @@ from DQM.SiPixelMonitorTrack.RefitterForPixelDQM import *
 from RecoLocalTracker.SiPixelRecHits.SiPixelTemplateStoreESProducer_cfi import *
 
 hltSiPixelClusterShapeCache = siPixelClusterShapeCache.clone(src = 'hltSiPixelClusters')
-from Configuration.Eras.Modifier_phase2_tracker_cff import phase2_tracker
-phase2_tracker.toModify(hltSiPixelClusterShapeCache, src = cms.InputTag("siPixelClusters","","HLT"))
-
 hltrefittedForPixelDQM = refittedForPixelDQM.clone(src ='hltMergedTracks',
                                                    TTRHBuilder = 'WithTrackAngle') # no templates at HLT
-phase2_tracker.toModify(hltrefittedForPixelDQM, src = cms.InputTag("generalTracks","","HLT"))
-
 sipixelMonitorHLTsequence = cms.Sequence(
     hltSiPixelClusterShapeCache
     + hltSiPixelPhase1ClustersAnalyzer
