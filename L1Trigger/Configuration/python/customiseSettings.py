dc8b838e0f13cccd9104df88b09a15c17937489b
mitaylor <mitay@mit.edu>
2023-09-18 14:48:15 +0200

    backport of PR 42697

--- L1Trigger/Configuration/python/customiseSettings.py
+++ L1Trigger/Configuration/python/customiseSettings.py
@@ -2,6 +2,10 @@ from __future__ import print_function
 import os.path
 import FWCore.ParameterSet.Config as cms
 
+def L1TSettingsToCaloParamsHI_2023_v0_4_1(process):
+    process.load("L1Trigger.L1TCalorimeter.caloParamsHI_2023_v0_4_1_cfi")
+    return process
+
 def L1TSettingsToCaloParams_2023_v0_4(process):
     process.load("L1Trigger.L1TCalorimeter.caloParams_2023_v0_4_cfi")
     return process
