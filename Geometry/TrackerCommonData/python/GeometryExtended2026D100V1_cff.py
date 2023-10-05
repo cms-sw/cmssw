924e18e1fecd4145a3d6ef33f5f5a26a240c0312
Sunanda <sunanda.banerjee@cern.ch>
2023-10-05 10:14:50 +0200

    Possible overlap removal in teh T33 version of tracker geometry

---
+++ Geometry/TrackerCommonData/python/GeometryExtended2026D100V1_cff.py
@@ -0,0 +1,12 @@
+import FWCore.ParameterSet.Config as cms
+
+from Geometry.TrackerCommonData.cmsExtendedGeometry2026D100V1XML_cfi import *
+from Geometry.TrackerNumberingBuilder.trackerNumberingGeometry_cff import *
+from SLHCUpgradeSimulations.Geometry.fakePhase2OuterTrackerConditions_cff import *
+from Geometry.EcalCommonData.ecalSimulationParameters_cff import *
+from Geometry.HcalCommonData.hcalDDDSimConstants_cff import *
+from Geometry.HGCalCommonData.hgcalParametersInitialization_cfi import *
+from Geometry.HGCalCommonData.hgcalNumberingInitialization_cfi import *
+from Geometry.MuonNumbering.muonGeometryConstants_cff import *
+from Geometry.MuonNumbering.muonOffsetESProducer_cff import *
+from Geometry.MTDNumberingBuilder.mtdNumberingGeometry_cff import *
