586783fa486aa082062664c9bd8787968bc6f21b
mmusich <marco.musich@cern.ch>
2023-10-11 21:40:50 +0200

    add 'beam' (i.e. AlCaBeamMonitor) sequence for Phase-2

--- DQMOffline/Configuration/python/autoDQM.py
+++ DQMOffline/Configuration/python/autoDQM.py
@@ -249,7 +249,7 @@ autoDQM = { 'DQMMessageLogger': ['DQMMessageLoggerSeq',
                      'DQMNone'],
             }
 
-_phase2_allowed = ['trackingOnlyDQM','outerTracker', 'trackerPhase2', 'muon','hcal','hcal2','egamma','L1TMonPhase2','HLTMon']
+_phase2_allowed = ['beam','trackingOnlyDQM','outerTracker', 'trackerPhase2', 'muon','hcal','hcal2','egamma','L1TMonPhase2','HLTMon']
 autoDQM['phase2'] = ['','','']
 for i in [0,2]:
     autoDQM['phase2'][i] = '+'.join([autoDQM[m][i] for m in _phase2_allowed])
