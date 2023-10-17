7f96e8660be47ecfb3ad6610635e6b3c8bb1940b
Sunanda <sunanda.banerjee@cern.ch>
2023-10-17 16:30:13 +0200

    Modify the tester for parameter and a few scripts to test HGCal geometry

--- Geometry/HGCalCommonData/test/python/testHGCalPartialIDTester_cfg.py
+++ Geometry/HGCalCommonData/test/python/testHGCalPartialIDTester_cfg.py
@@ -7,7 +7,7 @@
 #
 ###############################################################################
 import FWCore.ParameterSet.Config as cms
-import os, sys, imp, re, random
+import os, sys, importlib, re, random
 import FWCore.ParameterSet.VarParsing as VarParsing
 
 ####################################################################
