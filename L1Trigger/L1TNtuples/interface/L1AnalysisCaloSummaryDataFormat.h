0fad9ab2c171a94c9980403089d8062cc1836aee
Andrew Loeliger <loeliger@wisc.edu>
2023-10-25 10:13:57 -0500

    code format

--- L1Trigger/L1TNtuples/interface/L1AnalysisCaloSummaryDataFormat.h
+++ L1Trigger/L1TNtuples/interface/L1AnalysisCaloSummaryDataFormat.h
@@ -2,21 +2,21 @@
 #define L1Trigger_L1TNtuples_L1AnalysisCaloSummaryDataFormat_h
 
 namespace L1Analysis {
-    struct L1AnalysisCaloSummaryDataFormat {
-        L1AnalysisCaloSummaryDataFormat() { Reset(); }
-        ~L1AnalysisCaloSummaryDataFormat() {};
+  struct L1AnalysisCaloSummaryDataFormat {
+    L1AnalysisCaloSummaryDataFormat() { Reset(); }
+    ~L1AnalysisCaloSummaryDataFormat(){};
 
-        void Reset() {
-            CICADAScore = 0;
-            for (short iPhi = 0; iPhi < 18; ++iPhi)
-                for (short iEta = 0; iEta < 14; ++iEta)
-                    modelInput[iPhi][iEta] = 0;
-        }
-        void Init() {}
+    void Reset() {
+      CICADAScore = 0;
+      for (short iPhi = 0; iPhi < 18; ++iPhi)
+        for (short iEta = 0; iEta < 14; ++iEta)
+          modelInput[iPhi][iEta] = 0;
+    }
+    void Init() {}
 
-        float CICADAScore;
-        unsigned short int modelInput[18][14]; //Stored in indices of [iPhi][iEta]
-    };
-}
+    float CICADAScore;
+    unsigned short int modelInput[18][14];  //Stored in indices of [iPhi][iEta]
+  };
+}  // namespace L1Analysis
 
 #endif
--- L1Trigger/L1TNtuples/plugins/L1CaloSummaryTreeProducer.cc
+++ L1Trigger/L1TNtuples/plugins/L1CaloSummaryTreeProducer.cc
@@ -1,6 +1,6 @@
 // Producer for the CaloSummary cards emulator
 // Author: Andrew Loeliger
-// Presumably the L1TNtuple Format is going to last very long with the advent of L1NanoAOD, 
+// Presumably the L1TNtuple Format is going to last very long with the advent of L1NanoAOD,
 // But as of now, this is the only way to get CICADA into official menu studying tools
 
 #include <memory>
@@ -22,62 +22,61 @@
 #include "DataFormats/L1CaloTrigger/interface/L1CaloCollections.h"
 #include "DataFormats/L1CaloTrigger/interface/L1CaloRegion.h"
 
-
 class L1CaloSummaryTreeProducer : public edm::one::EDAnalyzer<edm::one::SharedResources> {
-    public:
-        explicit L1CaloSummaryTreeProducer (const edm::ParameterSet&);
-        ~L1CaloSummaryTreeProducer() override;
+public:
+  explicit L1CaloSummaryTreeProducer(const edm::ParameterSet&);
+  ~L1CaloSummaryTreeProducer() override;
 
-    private:
-        void beginJob() override {};
-        void analyze(const edm::Event&, const edm::EventSetup&) override;
-        void endJob() override {};
-    
-    public:
-        L1Analysis::L1AnalysisCaloSummaryDataFormat* caloSummaryData_;
-    
-    private:
-        const edm::EDGetTokenT<float> scoreToken_;
-        const edm::EDGetTokenT<L1CaloRegionCollection> regionToken_;
-        edm::Service<TFileService> fs_;
-        TTree* tree_;
+private:
+  void beginJob() override{};
+  void analyze(const edm::Event&, const edm::EventSetup&) override;
+  void endJob() override{};
+
+public:
+  L1Analysis::L1AnalysisCaloSummaryDataFormat* caloSummaryData_;
+
+private:
+  const edm::EDGetTokenT<float> scoreToken_;
+  const edm::EDGetTokenT<L1CaloRegionCollection> regionToken_;
+  edm::Service<TFileService> fs_;
+  TTree* tree_;
 };
 
-L1CaloSummaryTreeProducer::L1CaloSummaryTreeProducer(const edm::ParameterSet& iConfig):
-    scoreToken_(consumes<float>(iConfig.getUntrackedParameter<edm::InputTag>("scoreToken"))),
-    regionToken_(consumes<L1CaloRegionCollection>(iConfig.getUntrackedParameter<edm::InputTag>("regionToken"))) {
-        usesResource(TFileService::kSharedResource);
-        tree_ = fs_->make<TTree>("L1CaloSummaryTree", "L1CaloSummaryTree");
-        tree_->Branch("CaloSummary","L1Analysis::L1AnalysisCaloSummaryDataFormat", &caloSummaryData_, 32000, 3);
+L1CaloSummaryTreeProducer::L1CaloSummaryTreeProducer(const edm::ParameterSet& iConfig)
+    : scoreToken_(consumes<float>(iConfig.getUntrackedParameter<edm::InputTag>("scoreToken"))),
+      regionToken_(consumes<L1CaloRegionCollection>(iConfig.getUntrackedParameter<edm::InputTag>("regionToken"))) {
+  usesResource(TFileService::kSharedResource);
+  tree_ = fs_->make<TTree>("L1CaloSummaryTree", "L1CaloSummaryTree");
+  tree_->Branch("CaloSummary", "L1Analysis::L1AnalysisCaloSummaryDataFormat", &caloSummaryData_, 32000, 3);
 
-        caloSummaryData_ = new L1Analysis::L1AnalysisCaloSummaryDataFormat();       
+  caloSummaryData_ = new L1Analysis::L1AnalysisCaloSummaryDataFormat();
 }
 
 void L1CaloSummaryTreeProducer::analyze(const edm::Event& iEvent, const edm::EventSetup& iSetup) {
-    caloSummaryData_->Reset();
+  caloSummaryData_->Reset();
 
-    edm::Handle<L1CaloRegionCollection> regions;
-    iEvent.getByToken(regionToken_, regions);
+  edm::Handle<L1CaloRegionCollection> regions;
+  iEvent.getByToken(regionToken_, regions);
 
-    if(regions.isValid()) {
-        for (auto itr: *regions) {
-            caloSummaryData_->modelInput[itr.gctPhi()][itr.gctEta()-4] = itr.et(); //4 is subtracted off of the Eta to account for the 4+4 forward/backward HF regions that are not used in CICADA. These take offset the iEta by 4
-        }
+  if (regions.isValid()) {
+    for (auto itr : *regions) {
+      caloSummaryData_->modelInput[itr.gctPhi()][itr.gctEta() - 4] =
+          itr.et();  //4 is subtracted off of the Eta to account for the 4+4 forward/backward HF regions that are not used in CICADA. These take offset the iEta by 4
     }
-    else {
-        edm::LogWarning("L1Ntuple") << "Could not find region regions. CICADA model input will not be filled";
-    }
+  } else {
+    edm::LogWarning("L1Ntuple") << "Could not find region regions. CICADA model input will not be filled";
+  }
 
-    edm::Handle<float> score;
-    iEvent.getByToken(scoreToken_, score);
-    if (score.isValid()) caloSummaryData_->CICADAScore = *score;
-    else edm::LogWarning("L1Ntuple") << "Could not find a proper CICADA score. CICADA score will not be filled.";
+  edm::Handle<float> score;
+  iEvent.getByToken(scoreToken_, score);
+  if (score.isValid())
+    caloSummaryData_->CICADAScore = *score;
+  else
+    edm::LogWarning("L1Ntuple") << "Could not find a proper CICADA score. CICADA score will not be filled.";
 
-    tree_->Fill();
+  tree_->Fill();
 }
 
-L1CaloSummaryTreeProducer::~L1CaloSummaryTreeProducer() {
-    delete caloSummaryData_;
-}
+L1CaloSummaryTreeProducer::~L1CaloSummaryTreeProducer() { delete caloSummaryData_; }
 
 DEFINE_FWK_MODULE(L1CaloSummaryTreeProducer);
