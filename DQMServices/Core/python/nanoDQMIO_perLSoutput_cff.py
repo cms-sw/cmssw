c5c5735fdfec1d702931c0905df71dc6e564ceb9
alpapana <94459575+alpapana@users.noreply.github.com>
2023-10-26 15:05:44 +0200

    Add per-LS MEs

--- DQMServices/Core/python/nanoDQMIO_perLSoutput_cff.py
+++ DQMServices/Core/python/nanoDQMIO_perLSoutput_cff.py
@@ -205,5 +205,7 @@ nanoDQMIO_perLSoutput = cms.PSet(
     "JetMET/Jet/Cleanedak4PFJetsCHS/Pt",
     "JetMET/MET/pfMETT1/Cleaned/METSig",
     "JetMET/vertices",     
+    "JetMET/HIJetValidation/akCs4PFJets/SumPFPt",
+    "JetMET/HIJetValidation/akPu4CaloJets/SumPFPt"
   ) )
 )
