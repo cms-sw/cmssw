2628b6443b4eb6550b4aed90f95107d3e6de9e53
Matti Kortelainen <matti.kortelainen@cern.ch>
2023-10-27 15:56:06 -0500

    Clean up some unnecessary transition function calls/comments in DQM

--- DQMOffline/Trigger/plugins/FSQDiJetAve.cc
+++ DQMOffline/Trigger/plugins/FSQDiJetAve.cc
@@ -852,28 +852,6 @@ void FSQDiJetAve::bookHistograms(DQMStore::IBooker& booker, edm::Run const& run,
   }
 }
 //*/
-// ------------ method called when ending the processing of a run  ------------
-/*
-void 
-FSQDiJetAve::endRun(edm::Run const&, edm::EventSetup const&)
-{
-}
-*/
-
-// ------------ method called when starting to processes a luminosity block  ------------
-/*
-void 
-FSQDiJetAve::beginLuminosityBlock(edm::LuminosityBlock const&, edm::EventSetup const&)
-{
-}
-*/
-
-// ------------ method called when ending the processing of a luminosity block  ------------
-/*
-void 
-FSQDiJetAve::endLuminosityBlock(edm::LuminosityBlock const&, edm::EventSetup const&)
-{}
-// */
 
 // ------------ method fills 'descriptions' with the allowed parameters for the module  ------------
 void FSQDiJetAve::fillDescriptions(edm::ConfigurationDescriptions& descriptions) {
