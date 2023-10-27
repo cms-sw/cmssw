49f9941a974a69ffca2d4d22bf0ae038dac24f24
mmusich <marco.musich@cern.ch>
2023-10-27 16:07:37 +0200

    migrate JetMETDQMDCSFilter to use DSCRecord when DCSStatus is not available (>= Run3 case)

--- DQMOffline/JetMET/interface/JetMETDQMDCSFilter.h
+++ DQMOffline/JetMET/interface/JetMETDQMDCSFilter.h
@@ -1,24 +1,19 @@
-#ifndef JetMETDQMDCSFilter_H
-#define JetMETDQMDCSFilter_H
+#ifndef DQMOffline_JetMET_JetMETDQMDCSFilter_H
+#define DQMOffline_JetMET_JetMETDQMDCSFilter_H
 
-#include "FWCore/ParameterSet/interface/ParameterSet.h"
-#include "FWCore/Framework/interface/Frameworkfwd.h"
-#include "FWCore/Framework/interface/Event.h"
-#include "FWCore/Framework/interface/MakerMacros.h"
-#include "FWCore/ServiceRegistry/interface/Service.h"
-#include "FWCore/PluginManager/interface/ModuleDef.h"
-#include "DataFormats/Scalers/interface/DcsStatus.h"
-#include "FWCore/Framework/interface/ConsumesCollector.h"
 #include "DataFormats/Common/interface/Handle.h"
-#include "DQMServices/Core/interface/DQMStore.h"
-#include "DataFormats/Scalers/interface/DcsStatus.h"
-
 #include "DataFormats/JetReco/interface/CaloJetCollection.h"
 #include "DataFormats/JetReco/interface/PFJetCollection.h"
-
+#include "DataFormats/OnlineMetaData/interface/DCSRecord.h"
 #include "DataFormats/Scalers/interface/DcsStatus.h"
-#include "FWCore/Utilities/interface/EDGetToken.h"
+#include "FWCore/Framework/interface/ConsumesCollector.h"
 #include "FWCore/Framework/interface/EDConsumerBase.h"
+#include "FWCore/Framework/interface/Event.h"
+#include "FWCore/Framework/interface/Frameworkfwd.h"
+#include "FWCore/Framework/interface/MakerMacros.h"
+#include "FWCore/ParameterSet/interface/ParameterSet.h"
+#include "FWCore/PluginManager/interface/ModuleDef.h"
+#include "FWCore/Utilities/interface/EDGetToken.h"
 
 class JetMETDQMDCSFilter {
 public:
@@ -30,13 +25,19 @@ public:
   ~JetMETDQMDCSFilter();
   bool filter(const edm::Event& evt, const edm::EventSetup& es);
   bool passPIX, passSiStrip, passECAL, passHBHE, passHF, passHO, passES, passMuon;
-  edm::EDGetTokenT<DcsStatusCollection> scalarsToken;
+  edm::EDGetTokenT<DcsStatusCollection> scalarsToken_;
+  edm::EDGetTokenT<DCSRecord> dcsRecordToken_;
 
 private:
+  template <typename T>
+  void checkDCSInfoPerPartition(const T& DCS);
+  void initializeVars();
   bool verbose_;
   bool filter_;
   bool detectorOn_;
   std::string detectorTypes_;
+  std::map<std::string, std::vector<int>> associationMap_;
+  std::map<std::string, bool> passPerDet_;
 };
 
 #endif
