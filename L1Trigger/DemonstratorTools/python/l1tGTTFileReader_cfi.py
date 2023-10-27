8bb6254841058e532918a923086574be67d5e4be
Nick Manganelli <nicholas.james.manganelli@cern.ch>
2023-10-27 07:44:19 +0200

    Update GTTFileReader to load and produce collection of fixed Vertices

--- L1Trigger/DemonstratorTools/python/l1tGTTFileReader_cfi.py
+++ L1Trigger/DemonstratorTools/python/l1tGTTFileReader_cfi.py
@@ -2,5 +2,6 @@ import FWCore.ParameterSet.Config as cms
 
 l1tGTTFileReader = cms.EDProducer('GTTFileReader',
   files = cms.vstring("gttOutput_0.txt"), #, "gttOutput_1.txt"),
+  l1VertexCollectionName = cms.string("L1VerticesFirmware"),
   format = cms.untracked.string("APx")
 )
