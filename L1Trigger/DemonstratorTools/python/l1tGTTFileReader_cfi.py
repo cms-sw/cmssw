61072133dffbbaf789353946a0b53e31a1644a63
Nick Manganelli <nicholas.james.manganelli@cern.ch>
2023-10-27 21:22:05 +0200

    progress towards loading tracks through another reader

--- L1Trigger/DemonstratorTools/python/l1tGTTFileReader_cfi.py
+++ L1Trigger/DemonstratorTools/python/l1tGTTFileReader_cfi.py
@@ -1,7 +1,8 @@
 import FWCore.ParameterSet.Config as cms
 
 l1tGTTFileReader = cms.EDProducer('GTTFileReader',
-  files = cms.vstring("gttOutput_0.txt"), #, "gttOutput_1.txt"),
+  files = cms.vstring("L1GTTOutputToCorrelatorFile_0.txt"),
+  filesInputTracks = cms.vstring("L1GTTInputFile_0.txt"),
   l1VertexCollectionName = cms.string("L1VerticesFirmware"),
   format = cms.untracked.string("APx")
 )
