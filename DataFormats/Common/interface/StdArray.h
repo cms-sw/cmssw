07d9aba8452517828a872ea3ae8573852d3f0e70
iarspider <iarspider@gmail.com>
2023-10-31 10:41:39 +0100

    Update StdArray.h

--- DataFormats/Common/interface/StdArray.h
+++ DataFormats/Common/interface/StdArray.h
@@ -52,6 +52,8 @@ namespace edm {
     using reverse_iterator = std::reverse_iterator<iterator>;
     using const_reverse_iterator = std::reverse_iterator<const_iterator>;
 
+    // Assignment operators
+
     // copy assignment operator
     constexpr StdArray& operator=(StdArray<T, N> const& init) = default;
 
