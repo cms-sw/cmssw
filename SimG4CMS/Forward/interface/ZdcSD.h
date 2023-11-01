b57a647aea5b3a91a229afcc2b25d25b65906602
Sunanda <sunanda.banerjee@cern.ch>
2023-11-01 10:55:09 +0100

    Merging the changes of ZDC code from Anna which was used by the ZDC collaboration for the Run2 setup

--- SimG4CMS/Forward/interface/ZdcSD.h
+++ SimG4CMS/Forward/interface/ZdcSD.h
@@ -1,37 +1,52 @@
+#ifndef SimG4CMSForwardZdcSD_h
+#define SimG4CMSForwardZdcSD_h
 ///////////////////////////////////////////////////////////////////////////////
 // File: ZdcSD.h
 // Date: 02.04
 // Description: Stores hits of Zdc in appropriate  container
 //
 ///////////////////////////////////////////////////////////////////////////////
-#ifndef ZdcSD_h
-#define ZdcSD_h
 #include "SimG4CMS/Calo/interface/CaloSD.h"
 #include "SimG4CMS/Forward/interface/ZdcShowerLibrary.h"
-#include "FWCore/Framework/interface/Frameworkfwd.h"
-#include "FWCore/ParameterSet/interface/ParameterSetfwd.h"
+#include "SimG4CMS/Forward/interface/ZdcNumberingScheme.h"
 
 class ZdcSD : public CaloSD {
-public:
+
+public:    
   ZdcSD(const std::string &, const SensitiveDetectorCatalog &, edm::ParameterSet const &, const SimTrackManager *);
-
+ 
   ~ZdcSD() override = default;
-
+  bool ProcessHits(G4Step * step,G4TouchableHistory * tHistory) override;
   uint32_t setDetUnitId(const G4Step *step) override;
+ 
+  //  bool getFromLibrary(const G4Step * step) override;
+
+  double calculateCherenkovDeposit(G4Step*);
+  double calculateMeanNumberOfPhotons(int,double,double); 
+  double photonEnergyDist(int,double,double);
+  double generatePhotonEnergy(int,double,double);
+  double pmtEfficiency(double);
+  double convertEnergyToWavelength(double);
+
+  double calculateN2InvIntegral(double);
+  double evaluateFunction(const std::vector<double>, const std::vector<double>, double);
+  double linearInterpolation(double,double,double,double,double);
+ 
 
 protected:
-  double getEnergyDeposit(const G4Step *) override;
-  bool getFromLibrary(const G4Step *) override;
   void initRun() override;
+private:    
 
-private:
   int verbosity;
-  bool useShowerLibrary, useShowerHits;
+  bool  useShowerLibrary,useShowerHits; 
+  int   setTrackID(G4Step * step);
   double thFibDir;
   double zdcHitEnergyCut;
+  std::unique_ptr<ZdcShowerLibrary>   showerLibrary;
+  std::unique_ptr<ZdcNumberingScheme> numberingScheme;
 
-  std::unique_ptr<ZdcShowerLibrary> showerLibrary;
   std::vector<ZdcShowerLibrary::Hit> hits;
+
 };
 
-#endif  // ZdcSD_h
+#endif // ZdcSD_h
--- SimTransport/HectorProducer/interface/Hector.h
+++ SimTransport/HectorProducer/interface/Hector.h
@@ -2,15 +2,15 @@
 #define SimTransport_Hector_h
 
 // user include files
-#include "FWCore/Framework/interface/ESHandle.h"
 #include "FWCore/Framework/interface/EventSetup.h"
+#include "FWCore/Framework/interface/ESHandle.h"
 
 // HepMC headers
 #include "SimGeneral/HepPDTRecord/interface/ParticleDataTable.h"
 
 #include "HepMC/GenEvent.h"
-#include "HepMC/GenParticle.h"
 #include "HepMC/GenVertex.h"
+#include "HepMC/GenParticle.h"
 #include "SimDataFormats/GeneratorProducts/interface/HepMCProduct.h"
 
 #include "SimDataFormats/Forward/interface/LHCTransportLink.h"
@@ -19,16 +19,17 @@
 // SimpleConfigurable replacement
 #include "FWCore/ParameterSet/interface/ParameterSet.h"
 
-// Hector headers
+//Hector headers
 #include "H_BeamLine.h"
-#include "H_BeamParticle.h"
 #include "H_RecRPObject.h"
-#include <map>
+#include "H_BeamParticle.h"
 #include <string>
+#include <map>
 
 class TRandom3;
 
 class Hector {
+  
 public:
   //  Hector(const edm::ParameterSet & ps);
   Hector(const edm::ParameterSet &ps,
@@ -38,73 +39,69 @@ public:
          bool ZDCTransport);
   //  Hector();
   virtual ~Hector();
-
+  
   /*!Clears ApertureFlags, prepares Hector for a next event*/
   void clearApertureFlags();
-  /*!Clears BeamParticle, prepares Hector for a next Aperture check or/and a
-   * next event*/
+  /*!Clears BeamParticle, prepares Hector for a next Aperture check or/and a next event*/
   void clear();
   /*!Adds the stable protons from the event \a ev to a beamline*/
-  void add(const HepMC::GenEvent *ev, const edm::EventSetup &es);
+  void add( const HepMC::GenEvent * ev , const edm::EventSetup & es);
   /*!propagate the particles through a beamline to FP420*/
-  void filterFP420(TRandom3 *);
+  void filterFP420(TRandom3*);
   /*!propagate the particles through a beamline to ZDC*/
-  void filterZDC(TRandom3 *);
+  void filterZDC(TRandom3*);
   /*!propagate the particles through a beamline to ZDC*/
-  void filterD1(TRandom3 *);
-
-  int getDirect(unsigned int part_n) const;
+  void filterD1(TRandom3*);
+  
+  int getDirect( unsigned int part_n ) const;
 
   /*!Prints properties of all particles in a beamline*/
   void print() const;
-  /*!Return vector of the particle lines (HepMC::GenParticle::barcode()) in a
-   * beamline*/
+  /*!Return vector of the particle lines (HepMC::GenParticle::barcode()) in a beamline*/
   // std::vector<unsigned int> part_list() const;
-
+    
   //    bool isCharged(const HepMC::GenParticle * p);
+  
+  HepMC::GenEvent * addPartToHepMC( HepMC::GenEvent * event );
 
-  HepMC::GenEvent *addPartToHepMC(HepMC::GenEvent *event);
-
-  std::vector<LHCTransportLink> &getCorrespondenceMap() { return theCorrespondenceMap; }
-
-  /*
-      private:
-      //  edm::ParameterSet m_pBeamLine;
-      */
+  std::vector<LHCTransportLink> & getCorrespondenceMap() { return theCorrespondenceMap; }
+    
 private:
+    
   const edm::ESGetToken<HepPDT::ParticleDataTable, PDTRecord> tok_pdt_;
+    
   // Defaults
-  double lengthfp420;
-  double lengthzdc;
-  double lengthd1;
-
+  double lengthfp420 ;
+  double lengthzdc ;
+  double lengthd1 ;
+    
   double etacut;
   bool m_smearAng;
   double m_sig_e;
   bool m_smearE;
   double m_sigmaSTX;
   double m_sigmaSTY;
-
+   
   float m_rpp420_f;
   float m_rpp420_b;
   float m_rppzdc;
   float m_rppd1;
-
-  edm::ESHandle<ParticleDataTable> pdt;
-
+    
+  edm::ESHandle < ParticleDataTable > pdt;
+    
   // Hector
-  H_BeamLine *m_beamlineFP4201;
-  H_BeamLine *m_beamlineFP4202;
-  H_BeamLine *m_beamlineZDC1;
-  H_BeamLine *m_beamlineZDC2;
-  H_BeamLine *m_beamlineD11;
-  H_BeamLine *m_beamlineD12;
+  H_BeamLine * m_beamlineFP4201;
+  H_BeamLine * m_beamlineFP4202;
+  H_BeamLine * m_beamlineZDC1;
+  H_BeamLine * m_beamlineZDC2;
+  H_BeamLine * m_beamlineD11;
+  H_BeamLine * m_beamlineD12;
   //
-
-  H_RecRPObject *m_rp420_f;
-  H_RecRPObject *m_rp420_b;
-
-  std::map<unsigned int, H_BeamParticle *> m_beamPart;
+    
+  H_RecRPObject * m_rp420_f;
+  H_RecRPObject * m_rp420_b;
+  
+  std::map<unsigned int, H_BeamParticle*> m_beamPart;
   std::map<unsigned int, int> m_direct;
   std::map<unsigned int, bool> m_isStoppedfp420;
   std::map<unsigned int, bool> m_isStoppedzdc;
@@ -114,15 +111,15 @@ private:
   std::map<unsigned int, double> m_TxAtTrPoint;
   std::map<unsigned int, double> m_TyAtTrPoint;
   std::map<unsigned int, double> m_eAtTrPoint;
-
+    
   std::map<unsigned int, double> m_eta;
   std::map<unsigned int, int> m_pdg;
   std::map<unsigned int, double> m_pz;
   std::map<unsigned int, bool> m_isCharged;
-
-  string beam1filename;
-  string beam2filename;
-
+    
+  std::string beam1filename;
+  std::string beam2filename;
+    
   bool m_verbosity;
   bool m_FP420Transport;
   bool m_ZDCTransport;
