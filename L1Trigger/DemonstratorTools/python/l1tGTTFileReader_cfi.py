8951ec09e7504b2b3dada94eb930f8cac0fb4797
Nick Manganelli <nicholas.james.manganelli@cern.ch>
2023-11-02 21:08:17 +0100

    Add optional input capability parameters for the GTTFileReader into the cfi file.

--- L1Trigger/DemonstratorTools/python/l1tGTTFileReader_cfi.py
+++ L1Trigger/DemonstratorTools/python/l1tGTTFileReader_cfi.py
@@ -1,8 +1,15 @@
 import FWCore.ParameterSet.Config as cms
 
 l1tGTTFileReader = cms.EDProducer('GTTFileReader',
-  files = cms.vstring("L1GTTOutputToCorrelatorFile_0.txt"),
+  processOutputToCorrelator = cms.bool(True),
+  processInputTracks = cms.bool(False),
+  processOutputToGlobalTrigger = cms.bool(False), #Not fully implemented yet, partial skeleton added
+  kEmptyFramesOutputToCorrelator = cms.untracked.uint32(0),
+  kEmptyFramesInputTracks = cms.untracked.uint32(0),
+  kEmptyFramesOutputToGlobalTrigger = cms.untracked.uint32(0),
+  filesOutputToCorrelator = cms.vstring("L1GTTOutputToCorrelatorFile_0.txt"),
   filesInputTracks = cms.vstring("L1GTTInputFile_0.txt"),
+  filesOutputToGlobalTrigger = cms.vstring("L1GTTOutputToGlobalTriggerFile_0.txt"),
   l1VertexCollectionName = cms.string("L1VerticesFirmware"),
   l1TrackCollectionName = cms.string("Level1TTTracks"),
   format = cms.untracked.string("APx")
