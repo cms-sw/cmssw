85d3cc460f47f92886380846d2e2f21616bbefd9
Javier <Javier.Fernandez@cern.ch>
2023-11-08 17:37:24 +0100

    Remove commented code

--- L1Trigger/DTTriggerPhase2/src/lat_code_maker.py
+++ L1Trigger/DTTriggerPhase2/src/lat_code_maker.py
@@ -5,10 +5,5 @@ with open(filename) as f:
 
 for iline, line in enumerate(lines):
     line = line.strip().split("/")
-    # text = "if      " if iline == 0 else "else if "
-    # text += "(inMPath->missingLayer() == %s" % line[0]
-    # text += " && are_equal(inMPath->cellLayout(), (int[4])(%s))) " % line[1]
-    # text += "lateralities.push_back(%s);" % line[2]
-    # print(text)
     text = "lat_combinations.push_back({ %s, %s, %s });" % (line[0], line[1], line[2])
     print(text)
