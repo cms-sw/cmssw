ceab451440eb27ae2539e8cf745529416118f7e6
Andrea Bocci <andrea.bocci@cern.ch>
2023-11-09 14:19:52 +0100

    Adopt the c++ ROOT read rules for TestHostCollection

---
+++ DataFormats/PortableTestObjects/src/classes.cc
@@ -0,0 +1,4 @@
+#include "DataFormats/Portable/interface/PortableHostCollectionReadRules.h"
+#include "DataFormats/PortableTestObjects/interface/TestHostCollection.h"
+
+SET_PORTABLEHOSTCOLLECTION_READ_RULES(portabletest::TestHostCollection);
