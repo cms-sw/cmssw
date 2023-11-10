8067ab53a8ec6d0e008e344c51dbe5ccc364d7f9
matteo <matteo.migliorini95@gmail.com>
2023-11-10 16:24:08 +0100

    New DAQ source for L1Trigger scouting
    Implemented a new DAQSourceModel to read and merge Run3 L1 scouting data.
    - Similar to the existing FRDStriped input source
    - Modified EvFDaqDirector to enable the possibility of having multiple data sources per ramdisk
    - Added DataFormats/L1Scouting containing Scouting Raw Data Collection
      - Vector of FEDRawData
      - Included optional argument wordSize in the FEDRawData resize methods: 8 bytes by default, can be set to 4 bytes for scouting
      - Not a real FED, using a SRDCollection since L1scouting data will never be mixed with event data
    - Removed old scouting files that were used as an example
    - Test script for the ScoutingRun3 daq source

--- DataFormats/FEDRawData/src/FEDRawData.cc
+++ DataFormats/FEDRawData/src/FEDRawData.cc
@@ -13,9 +13,10 @@ using namespace std;
 
 FEDRawData::FEDRawData() {}
 
-FEDRawData::FEDRawData(size_t newsize) : data_(newsize) {
-  if (newsize % 8 != 0)
-    throw cms::Exception("DataCorrupt") << "FEDRawData::resize: " << newsize << " is not a multiple of 8 bytes."
+FEDRawData::FEDRawData(size_t newsize, size_t wordsize) : data_(newsize) {
+  if (newsize % wordsize != 0)
+    throw cms::Exception("DataCorrupt") << "FEDRawData::resize: " << newsize << " is not a multiple of "
+                                        << wordsize << " bytes."
                                         << endl;
 }
 
@@ -25,13 +26,14 @@ const unsigned char *FEDRawData::data() const { return data_.data(); }
 
 unsigned char *FEDRawData::data() { return data_.data(); }
 
-void FEDRawData::resize(size_t newsize) {
+void FEDRawData::resize(size_t newsize, size_t wordsize) {
   if (size() == newsize)
     return;
 
   data_.resize(newsize);
 
-  if (newsize % 8 != 0)
-    throw cms::Exception("DataCorrupt") << "FEDRawData::resize: " << newsize << " is not a multiple of 8 bytes."
+  if (newsize % wordsize != 0)
+    throw cms::Exception("DataCorrupt") << "FEDRawData::resize: " << newsize << " is not a multiple of "
+                                        << wordsize << " bytes."
                                         << endl;
 }
--- EventFilter/Utilities/test/testScoutingRun3_daqsource.py
+++ EventFilter/Utilities/test/testScoutingRun3_daqsource.py
@@ -1,65 +1,72 @@
 from __future__ import print_function
 import FWCore.ParameterSet.Config as cms
 import FWCore.ParameterSet.VarParsing as VarParsing
-import os
+import os, sys
 
-options = VarParsing.VarParsing ('analysis')
+options = VarParsing.VarParsing ("analysis")
 
-options.register ('runNumber',
-#                  325175, # default value
-                  325172, # default value
+options.register ("runNumber",
+                  368636,
                   VarParsing.VarParsing.multiplicity.singleton,
-                  VarParsing.VarParsing.varType.int,          # string, int, or float
+                  VarParsing.VarParsing.varType.int,
                   "Run Number")
 
-options.register ('buBaseDir',
-                  'ramdisk', # default value
+options.register ("daqSourceMode",
+                  "ScoutingRun3",
                   VarParsing.VarParsing.multiplicity.singleton,
-                  VarParsing.VarParsing.varType.string,          # string, int, or float
+                  VarParsing.VarParsing.varType.string,
+                  "DAQ source data mode")
+
+options.register ("buBaseDir",
+                  "/dev/shm",
+                  VarParsing.VarParsing.multiplicity.singleton,
+                  VarParsing.VarParsing.varType.string,
                   "BU base directory")
 
-options.register ('fuBaseDir',
-                  'data', # default value
+options.register ("fuBaseDir",
+                  "/tmp/",
                   VarParsing.VarParsing.multiplicity.singleton,
-                  VarParsing.VarParsing.varType.string,          # string, int, or float
+                  VarParsing.VarParsing.varType.string,
                   "BU base directory")
 
-options.register ('fffBaseDir',
-                  '.', # default value
+options.register ("fffBaseDir",
+                  "/dev/shm",
                   VarParsing.VarParsing.multiplicity.singleton,
-                  VarParsing.VarParsing.varType.string,          # string, int, or float
+                  VarParsing.VarParsing.varType.string,
                   "FFF base directory")
 
-options.register ('numThreads',
-                  1, # default value
+options.register ("numThreads",
+                  2,
                   VarParsing.VarParsing.multiplicity.singleton,
-                  VarParsing.VarParsing.varType.int,          # string, int, or float
+                  VarParsing.VarParsing.varType.int,
                   "Number of CMSSW threads")
 
-options.register ('numFwkStreams',
-                  1, # default value
+options.register ("numFwkStreams",
+                  2,
                   VarParsing.VarParsing.multiplicity.singleton,
-                  VarParsing.VarParsing.varType.int,          # string, int, or float
+                  VarParsing.VarParsing.varType.int,
                   "Number of CMSSW streams")
-
 
 options.parseArguments()
 
 cmsswbase = os.path.expandvars("$CMSSW_BASE/")
 
-process = cms.Process("TESTFU")
+process = cms.Process("SCPU")
 process.maxEvents = cms.untracked.PSet(
     input = cms.untracked.int32(-1)
 )
 
 process.options = cms.untracked.PSet(
+    wantSummary = cms.untracked.bool(True),
     numberOfThreads = cms.untracked.uint32(options.numThreads),
     numberOfStreams = cms.untracked.uint32(options.numFwkStreams),
-    numberOfConcurrentLuminosityBlocks = cms.untracked.uint32(1) # ShmStreamConsumer requires synchronization at LuminosityBlock boundaries
+    numberOfConcurrentLuminosityBlocks = cms.untracked.uint32(1)
 )
 process.MessageLogger = cms.Service("MessageLogger",
-    cout = cms.untracked.PSet(threshold = cms.untracked.string( "INFO" )),
-    destinations = cms.untracked.vstring( 'cout' )
+    cout = cms.untracked.PSet(
+        threshold = cms.untracked.string( "INFO" )
+    ),
+    destinations = cms.untracked.vstring( "cout" ),
 )
 
 process.FastMonitoringService = cms.Service("FastMonitoringService",
@@ -68,6 +75,12 @@ process.FastMonitoringService = cms.Service("FastMonitoringService",
 
 process.EvFDaqDirector = cms.Service("EvFDaqDirector",
     useFileBroker = cms.untracked.bool(False),
+    buBaseDirsAll = cms.untracked.vstring(
+        options.buBaseDir
+    ),
+    buBaseDirsNumStreams = cms.untracked.vint32(
+        2
+    ),
     fileBrokerHostFromCfg = cms.untracked.bool(True),
     fileBrokerHost = cms.untracked.string("htcp40.cern.ch"),
     runNumber = cms.untracked.uint32(options.runNumber),
@@ -83,35 +96,33 @@ except Exception as ex:
   pass
 
 ram_dir_path=options.buBaseDir+"/run"+str(options.runNumber).zfill(6)+"/"
+flist = [
+   ram_dir_path + "run" + str(options.runNumber) + "_ls0340_index000001.raw"
+]
 
 process.source = cms.Source("DAQSource",
-#    dataMode = cms.untracked.string("ScoutingRun2Muon"),
-    dataMode = cms.untracked.string("ScoutingRun2Multi"),
-    verifyChecksum = cms.untracked.bool(True),
+    testing = cms.untracked.bool(True),
+    dataMode = cms.untracked.string(options.daqSourceMode),
+    verifyChecksum = cms.untracked.bool(False),
     useL1EventID = cms.untracked.bool(False),
-    eventChunkSize = cms.untracked.uint32(8),
-    eventChunkBlock = cms.untracked.uint32(8),
+    eventChunkBlock = cms.untracked.uint32(64),
+    eventChunkSize = cms.untracked.uint32(128),
+    maxChunkSize = cms.untracked.uint32(256),
     numBuffers = cms.untracked.uint32(2),
     maxBufferedFiles = cms.untracked.uint32(2),
     fileListMode = cms.untracked.bool(True),
-    fileNames = cms.untracked.vstring(
-#        ram_dir_path+"run325175_ls0001_index000001.raw"
-        ram_dir_path+"run325172_ls0010_index000000.raw",
-        ram_dir_path+"run325172_ls0380_index000000.raw"
-    )
+    fileNames = cms.untracked.vstring(*flist)
 
 )
 
-process.output = cms.OutputModule("PoolOutputModule",
+process.outputZB = cms.OutputModule("PoolOutputModule",
     fileName = cms.untracked.string('file:PoolOutputTest.root'),
-    outputCommands = cms.untracked.vstring("drop *","keep *_rawDataCollector_*_*")
+    outputCommands = cms.untracked.vstring(
+        "drop *",
+        "keep *_rawDataCollector_*_*"
     )
-
+)
 
 process.ep = cms.EndPath(
-#  process.streamA
-#  + process.streamB
-#  + process.streamC
-# + process.streamD
-  process.output
+    process.outputZB
 )
