775939b902f1412262cccc2ad7bea7751ee2c845
Sunanda <sunanda.banerjee@cern.ch>
2023-11-12 11:53:19 +0100

    Modify the material budget scipt to enable obtaining material budget plots for HFNose

--- Validation/Geometry/python/materialBudgetVolume_cfi.py
+++ Validation/Geometry/python/materialBudgetVolume_cfi.py
@@ -3,9 +3,11 @@ from SimG4Core.Configuration.SimG4Core_cff import *
 
 g4SimHits.Watchers = cms.VPSet(cms.PSet(
     MaterialBudgetVolume = cms.PSet(
-        lvNames = cms.vstring('BEAM', 'BEAM1', 'BEAM2', 'BEAM3', 'BEAM4', 'Tracker', 'ECAL', 'HCal', 'VCAL', 'MGNT', 'MUON', 'OQUA', 'CALOEC'),
-        lvLevels = cms.vint32(3, 3, 3, 3, 3, 3, 4, 4, 3, 4, 3, 3, 4),
+        lvNames = cms.vstring('BEAM', 'BEAM1', 'BEAM2', 'BEAM3', 'BEAM4', 'Tracker', 'ECAL', 'HCal', 'VCAL', 'MGNT', 'MUON', 'OQUA', 'CALOEC', 'HFNoseVol'),
+        lvLevels = cms.vint32(3, 3, 3, 3, 3, 3, 4, 4, 3, 4, 3, 3, 4, 3),
         useDD4hep = cms.bool(False),
+        rMax = cms.double(-1.),
+        zMax = cms.double(-1.),
     ),
     type = cms.string('MaterialBudgetVolume'),
 ))
