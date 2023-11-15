91235b583453bc4646556474e68de3d27e6a8319
adriano <adriano.di.florio@cern.ch>
2023-11-15 18:23:52 +0100

    Implement PortableObject types
    PortableHostObject<T> is a class template that wraps a struct type T and an
    alpaka host buffer, that owns the memory where the struct is allocated. The
    content of the struct is persistent, while the buffer itself is transient.
    Specialisations of this template can be persisted, but requre special ROOT read
    rules to read the data into an alpaka memory buffer.
    
    PortableDeviceObject<T, TDev> is a class template that wraps a struct type T and
    an alpaka device buffer, that owns the memory where the struct is allocated.
    To avoid confusion and ODR-violations, the PortableDeviceObject<T, TDev>
    template cannot be used with the Host device type.
    Specialisations of this template are transient and cannot be persisted.
    
    Add TestHostObject and TestDeviceObject based on a TestStruct.
    
    Co-authored-by: Adriano Di Florio <adriano.diflorio@ba.infn.it>
    Co-authored-by: Breno Orzari <breno.orzari@hotmail.com>
    Co-authored-by: Dimitris Papagiannis <nothingface0@gmail.com>

---
+++ DataFormats/Portable/interface/PortableDeviceObject.h
@@ -0,0 +1,72 @@
+#ifndef DataFormats_Portable_interface_PortableDeviceObject_h
+#define DataFormats_Portable_interface_PortableDeviceObject_h
+
+#include <cassert>
+#include <optional>
+#include <type_traits>
+
+#include <alpaka/alpaka.hpp>
+
+#include "HeterogeneousCore/AlpakaInterface/interface/config.h"
+#include "HeterogeneousCore/AlpakaInterface/interface/memory.h"
+
+// generic object in device memory
+template <typename T, typename TDev, typename = std::enable_if_t<alpaka::isDevice<TDev>>>
+class PortableDeviceObject {
+  static_assert(not std::is_same_v<TDev, alpaka_common::DevHost>,
+                "Use PortableHostObject<T> instead of PortableDeviceObject<T, DevHost>");
+
+public:
+  using Product = T;
+  using Buffer = cms::alpakatools::device_buffer<TDev, Product>;
+  using ConstBuffer = cms::alpakatools::const_device_buffer<TDev, Product>;
+
+  PortableDeviceObject() = default;
+
+  PortableDeviceObject(TDev const& device)
+      // allocate global device memory
+      : buffer_{cms::alpakatools::make_device_buffer<Product>(device)} {
+    assert(reinterpret_cast<uintptr_t>(buffer_->data()) % alignof(Product) == 0);
+  }
+
+  template <typename TQueue, typename = std::enable_if_t<alpaka::isQueue<TQueue>>>
+  PortableDeviceObject(TQueue const& queue)
+      // allocate global device memory with queue-ordered semantic
+      : buffer_{cms::alpakatools::make_device_buffer<Product>(queue)} {
+    assert(reinterpret_cast<uintptr_t>(buffer_->data()) % alignof(Product) == 0);
+  }
+
+  // non-copyable
+  PortableDeviceObject(PortableDeviceObject const&) = delete;
+  PortableDeviceObject& operator=(PortableDeviceObject const&) = delete;
+
+  // movable
+  PortableDeviceObject(PortableDeviceObject&&) = default;
+  PortableDeviceObject& operator=(PortableDeviceObject&&) = default;
+
+  // default destructor
+  ~PortableDeviceObject() = default;
+
+  // access the product
+  Product& value() { return *buffer_->data(); }
+  Product const& value() const { return *buffer_->data(); }
+
+  Product* data() { return buffer_->data(); }
+  Product const* data() const { return buffer_->data(); }
+
+  Product& operator*() { return *buffer_->data(); }
+  Product const& operator*() const { return *buffer_->data(); }
+
+  Product* operator->() { return buffer_->data(); }
+  Product const* operator->() const { return buffer_->data(); }
+
+  // access the buffer
+  Buffer buffer() { return *buffer_; }
+  ConstBuffer buffer() const { return *buffer_; }
+  ConstBuffer const_buffer() const { return *buffer_; }
+
+private:
+  std::optional<Buffer> buffer_;
+};
+
+#endif  // DataFormats_Portable_interface_PortableDeviceObject_h
--- DataFormats/PortableTestObjects/src/alpaka/classes_rocm.h
+++ DataFormats/PortableTestObjects/src/alpaka/classes_rocm.h
@@ -1,4 +1,6 @@
 #include "DataFormats/Common/interface/DeviceProduct.h"
 #include "DataFormats/Common/interface/Wrapper.h"
 #include "DataFormats/PortableTestObjects/interface/TestSoA.h"
+#include "DataFormats/PortableTestObjects/interface/TestStruct.h"
 #include "DataFormats/PortableTestObjects/interface/alpaka/TestDeviceCollection.h"
+#include "DataFormats/PortableTestObjects/interface/alpaka/TestDeviceObject.h"
