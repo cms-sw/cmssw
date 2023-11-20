8dc0006718dd15195c3cf2983fe20838b14844b1
Sunanda <sunanda.banerjee@cern.ch>
2023-11-20 13:06:53 +0100

    Extend one more momentum bin to be studied in the calibration using isolated charged hadrons

--- Calibration/HcalCalibAlgos/macros/CalibMonitor.C
+++ Calibration/HcalCalibAlgos/macros/CalibMonitor.C
@@ -293,7 +293,7 @@ public:
   void correctEnergy(double &ener, const Long64_t &entry);
 
 private:
-  static const unsigned int npbin = 6, kp50 = 3;
+  static const unsigned int npbin = 7, kp50 = 3;
   CalibCorrFactor *corrFactor_;
   CalibCorr *cFactor_;
   CalibSelectRBX *cSelect_;
@@ -593,7 +593,7 @@ void CalibMonitor::Init(TChain *tree, const char *comFileName, const char *outFi
       ++nbins;
     }
   }
-  int ipbin[npbin] = {10, 20, 30, 40, 60, 100};
+  int ipbin[npbin] = {10, 20, 30, 40, 60, 100, 500};
   for (unsigned int i = 0; i < npbin; ++i)
     ps_.push_back((double)(ipbin[i]));
   int npvtx[6] = {0, 7, 10, 13, 16, 100};
