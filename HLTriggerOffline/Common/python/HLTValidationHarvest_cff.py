e57c10a521b4425028087f9ddc75084318d842a7
mmusich <marco.musich@cern.ch>
2023-11-20 10:36:36 +0100

    follow-up to #43094, remove HLT and vertexing validation harvesting from phase-2 modifier

--- HLTriggerOffline/Common/python/HLTValidationHarvest_cff.py
+++ HLTriggerOffline/Common/python/HLTValidationHarvest_cff.py
@@ -42,9 +42,7 @@ from Configuration.Eras.Modifier_phase1Pixel_cff import phase1Pixel
 # Temporary Phase-2 configuration
 # Exclude everything except JetMET for now
 from Configuration.Eras.Modifier_phase2_common_cff import phase2_common
-phase2_common.toReplaceWith(hltpostvalidation, hltpostvalidation.copyAndExclude([postProcessorHLTtrackingSequence,
-                                                                                 postProcessorHLTvertexing,
-                                                                                 HLTMuonPostVal,
+phase2_common.toReplaceWith(hltpostvalidation, hltpostvalidation.copyAndExclude([HLTMuonPostVal,
                                                                                  HLTTauPostVal,
                                                                                  EgammaPostVal,
                                                                                  postProcessorHLTgsfTrackingSequence,
