c264171fd11ad715ad8baf8ba4e332c8790a5620
Matti Kortelainen <matti.kortelainen@cern.ch>
2023-11-22 18:51:36 +0100

    Add CopyToDeviceCache class template

--- HeterogeneousCore/AlpakaTest/test/testAlpakaModules_cfg.py
+++ HeterogeneousCore/AlpakaTest/test/testAlpakaModules_cfg.py
@@ -73,6 +73,12 @@ process.alpakaGlobalProducer = testAlpakaGlobalProducer.clone(
 process.alpakaGlobalProducerE = cms.EDProducer("TestAlpakaGlobalProducerE@alpaka",
     source = cms.InputTag("alpakaGlobalProducer")
 )
+process.alpakaGlobalProducerCopyToDeviceCache = cms.EDProducer("TestAlpakaGlobalProducerCopyToDeviceCache@alpaka",
+    source = cms.InputTag("alpakaGlobalProducer"),
+    x = cms.int32(3),
+    y = cms.int32(4),
+    z = cms.int32(5),
+)
 process.alpakaStreamProducer = cms.EDProducer("TestAlpakaStreamProducer@alpaka",
     source = cms.InputTag("intProduct"),
     eventSetupSource = cms.ESInputTag("alpakaESProducerB", "explicitLabel"),
@@ -107,6 +113,10 @@ process.alpakaGlobalConsumerE = process.alpakaGlobalConsumer.clone(
     source = "alpakaGlobalProducerE",
     expectXvalues = cms.vdouble([(i%2)*10+1 + abs(27)+i*2 for i in range(0,5)] + [0]*5)
 )
+process.alpakaGlobalConsumerCopyToDeviceCache = process.alpakaGlobalConsumer.clone(
+    source = "alpakaGlobalProducerCopyToDeviceCache",
+    expectXvalues = cms.vdouble([3]*10)
+)
 process.alpakaStreamConsumer = cms.EDAnalyzer("TestAlpakaAnalyzer",
     source = cms.InputTag("alpakaStreamProducer"),
     expectSize = cms.int32(5),
@@ -131,7 +141,7 @@ if args.processAcceleratorBackend != "":
 if args.moduleBackend != "":
     for name in ["ESProducerA", "ESProducerB", "ESProducerC", "ESProducerD", "ESProducerE",
                  "ESProducerNull",
-                 "GlobalProducer", "GlobalProducerE",
+                 "GlobalProducer", "GlobalProducerE", "GlobalProducerCopyToDeviceCache",
                  "StreamProducer", "StreamInstanceProducer", "StreamSynchronizingProducer",
                  "NullESConsumer"]:
         mod = getattr(process, "alpaka"+name)
@@ -143,6 +153,8 @@ if args.expectBackend == "cuda_async":
     setExpect(process.alpakaGlobalConsumer, size=20)
     setExpect(process.alpakaGlobalConsumerE, size=20)
     process.alpakaGlobalConsumerE.expectXvalues.extend([0]*(20-10))
+    setExpect(process.alpakaGlobalConsumerCopyToDeviceCache, size=20)
+    process.alpakaGlobalConsumerCopyToDeviceCache.expectXvalues = [3]*20
     setExpect(process.alpakaStreamConsumer, size=25)
     setExpect(process.alpakaStreamInstanceConsumer, size=36)
     setExpect(process.alpakaStreamSynchronizingConsumer, size=20)
@@ -153,6 +165,8 @@ elif args.expectBackend == "rocm_async":
     setExpect(process.alpakaGlobalConsumer, size = 30)
     setExpect(process.alpakaGlobalConsumerE, size = 30)
     process.alpakaGlobalConsumerE.expectXvalues.extend([0]*(30-10))
+    setExpect(process.alpakaGlobalConsumerCopyToDeviceCache, size = 30)
+    process.alpakaGlobalConsumerCopyToDeviceCache.expectXvalues = [3]*30
     setExpect(process.alpakaStreamConsumer, size = 125)
     setExpect(process.alpakaStreamInstanceConsumer, size = 216)
     setExpect(process.alpakaStreamSynchronizingConsumer, size = 30)
@@ -171,6 +185,7 @@ process.t = cms.Task(
     process.intProduct,
     process.alpakaGlobalProducer,
     process.alpakaGlobalProducerE,
+    process.alpakaGlobalProducerCopyToDeviceCache,
     process.alpakaStreamProducer,
     process.alpakaStreamInstanceProducer,
     process.alpakaStreamSynchronizingProducer
@@ -178,6 +193,7 @@ process.t = cms.Task(
 process.p = cms.Path(
     process.alpakaGlobalConsumer+
     process.alpakaGlobalConsumerE+
+    process.alpakaGlobalConsumerCopyToDeviceCache+
     process.alpakaStreamConsumer+
     process.alpakaStreamInstanceConsumer+
     process.alpakaStreamSynchronizingConsumer+
