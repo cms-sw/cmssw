b2bdbef6284897f11f90256b7526319ce3234b96
Srecko <srecko.morovic@cern.ch>
2023-12-05 11:54:03 +0100

    * e2x2 produced by the EGammaClusterShapeProducer * Photon XGBoost MVA estimator and producer. Implemented using native XGBoost C API for photon MVA score Currently diphoton score filter is not yet submitted.
    Model files will be added to the cms-data
    
    XGBoost Double photon combination filter
    
    fix module name
    
    code-format
    
    fix comments from maintainers
    
    Update HLTrigger/Egamma/plugins/HLTEgammaDoubleXGBoostCombFilter.cc
    
    Co-authored-by: Marco Musich <marco.musich@cern.ch>
    
    Update HLTrigger/Egamma/plugins/HLTEgammaDoubleXGBoostCombFilter.cc
    
    Co-authored-by: Marco Musich <marco.musich@cern.ch>
    
    Update HLTrigger/Egamma/plugins/HLTEgammaDoubleXGBoostCombFilter.cc
    
    Co-authored-by: Marco Musich <marco.musich@cern.ch>
    
    Update HLTrigger/Egamma/plugins/HLTEgammaDoubleXGBoostCombFilter.cc
    
    Co-authored-by: Marco Musich <marco.musich@cern.ch>
    
    Update HLTrigger/Egamma/plugins/HLTEgammaDoubleXGBoostCombFilter.cc
    
    Co-authored-by: Marco Musich <marco.musich@cern.ch>
    
    Update HLTrigger/Egamma/plugins/HLTEgammaDoubleXGBoostCombFilter.cc
    
    Co-authored-by: Marco Musich <marco.musich@cern.ch>
    
    Update RecoEgamma/PhotonIdentification/plugins/PhotonXGBoostProducer.cc
    
    Co-authored-by: Marco Musich <marco.musich@cern.ch>
    
    Update RecoEgamma/PhotonIdentification/plugins/PhotonXGBoostProducer.cc
    
    Co-authored-by: Marco Musich <marco.musich@cern.ch>
    
    Update RecoEgamma/PhotonIdentification/plugins/PhotonXGBoostProducer.cc
    
    Co-authored-by: Marco Musich <marco.musich@cern.ch>
    
    Update RecoEgamma/PhotonIdentification/plugins/PhotonXGBoostProducer.cc
    
    Co-authored-by: Marco Musich <marco.musich@cern.ch>
    
    Update RecoEgamma/PhotonIdentification/plugins/PhotonXGBoostProducer.cc
    
    Co-authored-by: Marco Musich <marco.musich@cern.ch>
    
    tidy up and sort headers
    
    code-format
    
    Update RecoEgamma/PhotonIdentification/plugins/PhotonXGBoostProducer.cc
    
    Co-authored-by: Marco Musich <marco.musich@cern.ch>

---
+++ RecoEgamma/PhotonIdentification/src/PhotonXGBoostEstimator.cc
@@ -0,0 +1,60 @@
+#include "RecoEgamma/PhotonIdentification/interface/PhotonXGBoostEstimator.h"
+#include <sstream>
+
+PhotonXGBoostEstimator::PhotonXGBoostEstimator(const edm::FileInPath& weightsFile, int best_ntree_limit) {
+  XGBoosterCreate(NULL, 0, &booster_);
+  XGBoosterLoadModel(booster_, weightsFile.fullPath().c_str());
+  best_ntree_limit_ = best_ntree_limit;
+
+  std::stringstream config;
+  config << "{\"training\": false, \"type\": 0, \"iteration_begin\": 0, \"iteration_end\": " << best_ntree_limit_
+         << ", \"strict_shape\": false}";
+  config_ = config.str();
+}
+
+PhotonXGBoostEstimator::~PhotonXGBoostEstimator() { XGBoosterFree(booster_); }
+
+namespace {
+  enum inputIndexes {
+    rawEnergy = 0,      // 0
+    r9 = 1,             // 1
+    sigmaIEtaIEta = 2,  // 2
+    etaWidth = 3,       // 3
+    phiWidth = 4,       // 4
+    e2x2 = 5,           // 5
+    eta = 6,            // 6
+    hOvrE = 7,          // 7
+    ecalPFIso = 8,      // 8
+  };
+}  // namespace
+
+float PhotonXGBoostEstimator::computeMva(float rawEnergyIn,
+                                         float r9In,
+                                         float sigmaIEtaIEtaIn,
+                                         float etaWidthIn,
+                                         float phiWidthIn,
+                                         float e2x2In,
+                                         float etaIn,
+                                         float hOvrEIn,
+                                         float ecalPFIsoIn) const {
+  float var[9];
+  var[rawEnergy] = rawEnergyIn;
+  var[r9] = r9In;
+  var[sigmaIEtaIEta] = sigmaIEtaIEtaIn;
+  var[etaWidth] = etaWidthIn;
+  var[phiWidth] = phiWidthIn;
+  var[e2x2] = e2x2In;
+  var[eta] = etaIn;
+  var[hOvrE] = hOvrEIn;
+  var[ecalPFIso] = ecalPFIsoIn;
+
+  DMatrixHandle dmat;
+  XGDMatrixCreateFromMat(var, 1, 9, -999.9f, &dmat);
+  uint64_t const* out_shape;
+  uint64_t out_dim;
+  const float* out_result = NULL;
+  XGBoosterPredictFromDMatrix(booster_, dmat, config_.c_str(), &out_shape, &out_dim, &out_result);
+  float ret = out_result[0];
+  XGDMatrixFree(dmat);
+  return ret;
+}
