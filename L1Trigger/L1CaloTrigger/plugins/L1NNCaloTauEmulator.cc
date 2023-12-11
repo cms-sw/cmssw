3671cb3959fe10a6afaac6e252ef7732c7faf289
Jona Motta <jona.motta@polytechnique.edu>
2023-12-11 17:59:31 +0100

    fix unused variable warning

--- L1Trigger/L1CaloTrigger/plugins/L1NNCaloTauEmulator.cc
+++ L1Trigger/L1CaloTrigger/plugins/L1NNCaloTauEmulator.cc
@@ -529,6 +529,7 @@ void l1tNNCaloTauEmulator::produce(edm::Event& iEvent, const edm::EventSetup& eS
     if (!VsPuId.method().empty()) {
       int id = VsPuId.passID(*cl3dIt, cluster);
       idScore = cluster.egVsPUMVAOut();
+      ++id;
     }
 
     float eta_hgcalCoord = correctInputEtaCl3d(cl3d.eta());
