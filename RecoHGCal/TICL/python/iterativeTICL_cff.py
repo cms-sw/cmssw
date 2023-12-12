d8d8faee9cb6970fe66b4647568d0cee2331412c
Felice Pantaleo <felice.pantaleo@cern.ch>
2023-12-12 18:26:02 +0100

    include ticlv5 in iterativeTICL

--- RecoHGCal/TICL/python/iterativeTICL_cff.py
+++ RecoHGCal/TICL/python/iterativeTICL_cff.py
@@ -7,20 +7,28 @@ from RecoHGCal.TICL.TrkEMStep_cff import *
 from RecoHGCal.TICL.TrkStep_cff import *
 from RecoHGCal.TICL.EMStep_cff import *
 from RecoHGCal.TICL.HADStep_cff import *
+from RecoHGCal.TICL.CLUE3DEM_cff import *
+from RecoHGCal.TICL.CLUE3DHAD_cff import *
 
 from RecoHGCal.TICL.ticlLayerTileProducer_cfi import ticlLayerTileProducer
 from RecoHGCal.TICL.pfTICLProducer_cfi import pfTICLProducer as _pfTICLProducer
 from RecoHGCal.TICL.trackstersMergeProducer_cfi import trackstersMergeProducer as _trackstersMergeProducer
 from RecoHGCal.TICL.tracksterSelectionTf_cfi import *
 
+from RecoHGCal.TICL.tracksterLinksProducer_cfi import tracksterLinksProducer as _tracksterLinksProducer
+
+
 ticlLayerTileTask = cms.Task(ticlLayerTileProducer)
 
 ticlTrackstersMerge = _trackstersMergeProducer.clone()
+ticlTracksterLinks = _tracksterLinksProducer.clone()
 
 pfTICL = _pfTICLProducer.clone()
 ticlPFTask = cms.Task(pfTICL)
 
 ticlIterationsTask = cms.Task(
+    ticlCLUE3DEMStepTask,
+    ticlCLUE3DHADStepTask,
     ticlCLUE3DHighStepTask
 )
 
@@ -29,14 +37,16 @@ from Configuration.ProcessModifiers.fastJetTICL_cff import fastJetTICL
 fastJetTICL.toModify(ticlIterationsTask, func=lambda x : x.add(ticlFastJetStepTask))
 
 
-ticlIterLabels = [_step.itername.value() for _iteration in ticlIterationsTask for _step in _iteration if (_step._TypedParameterizable__type == "TrackstersProducer")]
+ticlIterLabels = ["CLUE3DEM", "CLUE3DHAD", "CLUE3DHigh"]
 
 ticlTracksterMergeTask = cms.Task(ticlTrackstersMerge)
+ticlTracksterLinksTask = cms.Task(ticlTracksterLinks)
 
 
 mergeTICLTask = cms.Task(ticlLayerTileTask
     ,ticlIterationsTask
     ,ticlTracksterMergeTask
+    ,ticlTracksterLinksTask
 )
 
 ticlIterLabelsMerge = ticlIterLabels + ["Merge"]
