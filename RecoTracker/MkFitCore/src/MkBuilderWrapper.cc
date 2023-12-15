306b6634d5795fa5997263c72d2c8486c290629d
Dan Riley <Daniel.Riley@cornell.edu>
2023-12-15 12:12:57 -0500

    fixup! more valgrind, set loop indices to vector width, cleanup builder pools in producer destructor

--- RecoTracker/MkFitCore/src/MkBuilderWrapper.cc
+++ RecoTracker/MkFitCore/src/MkBuilderWrapper.cc
@@ -2,7 +2,7 @@
 #include "RecoTracker/MkFitCore/interface/MkBuilder.h"
 
 namespace mkfit {
-  MkBuilderWrapper::MkBuilderWrapper(bool silent) : builder_(MkBuilder::make_builder(silent)) = default;
+  MkBuilderWrapper::MkBuilderWrapper(bool silent) : builder_(MkBuilder::make_builder(silent)) {}
   MkBuilderWrapper::~MkBuilderWrapper() = default;
 
   void MkBuilderWrapper::populate() { MkBuilder::populate(); }
