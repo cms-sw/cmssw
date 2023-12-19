2050f1f4be1ceaf578ef8be79af0daa57bf0132f
swagata87 <physics.swagata@gmail.com>
2023-12-19 19:21:48 +0100

    phase out calotowers in PF

--- HLTrigger/Configuration/python/HLT_75e33/modules/particleFlowBlock_cfi.py
+++ HLTrigger/Configuration/python/HLT_75e33/modules/particleFlowBlock_cfi.py
@@ -11,6 +11,9 @@ particleFlowBlock = cms.EDProducer("PFBlockProducer",
             source_eb = cms.InputTag("particleFlowSuperClusterECAL","particleFlowSuperClusterECALBarrel"),
             source_ee = cms.InputTag("particleFlowSuperClusterECAL","particleFlowSuperClusterECALEndcapWithPreshower"),
             source_towers = cms.InputTag("towerMaker"),
+            hbheRecHitsTag = cms.InputTag("hltHbhereco"),
+            maxSeverityHB = cms.int32(9),
+            maxSeverityHE = cms.int32(9),
             superClustersArePF = cms.bool(True)
         ),
         cms.PSet(
