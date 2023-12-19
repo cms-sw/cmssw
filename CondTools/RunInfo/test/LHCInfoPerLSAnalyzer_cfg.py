648b92051c9a6b84307d4af89dd8454a5a1ffca5
Jan Chyczynski <jasiek.ch16@gmail.com>
2023-12-19 22:58:09 +0100

    LHCInfo* analyzers and writers

--- CondTools/RunInfo/test/LHCInfoPerLSAnalyzer_cfg.py
+++ CondTools/RunInfo/test/LHCInfoPerLSAnalyzer_cfg.py
@@ -1,6 +1,47 @@
 import FWCore.ParameterSet.Config as cms
 
+import FWCore.ParameterSet.VarParsing as VarParsing
 process = cms.Process('test')
+
+options = VarParsing.VarParsing()
+options.register( 'db'
+                , 'sqlite_file:lhcinfoperls_pop_test.db' #default value
+                , VarParsing.VarParsing.multiplicity.singleton
+                , VarParsing.VarParsing.varType.string
+                , "Connection string to the DB where payloads are going to be read from"
+                  )
+options.register( 'tag'
+                , 'LHCInfoPerLS_PopCon_test'
+                , VarParsing.VarParsing.multiplicity.singleton
+                , VarParsing.VarParsing.varType.string
+                , "Tag to read from in source db"
+                  )
+options.register( 'timestamp'
+                , 1
+                , VarParsing.VarParsing.multiplicity.singleton
+                , VarParsing.VarParsing.varType.int
+                , "Timestamp to which payload with relavant IOV will be read"
+                  )
+options.register( 'csv'
+                , False
+                , VarParsing.VarParsing.multiplicity.singleton
+                , VarParsing.VarParsing.varType.bool
+                , "Weather or not to print the values in csv format"
+                  )
+options.register( 'header'
+                , False
+                , VarParsing.VarParsing.multiplicity.singleton
+                , VarParsing.VarParsing.varType.bool
+                , "Weather or not to print header for the csv, works only in when csv=True"
+                  )
+options.register( 'separator'
+                , ','
+                , VarParsing.VarParsing.multiplicity.singleton
+                , VarParsing.VarParsing.varType.string
+                , "separator for the csv format, works only in when csv=True"
+                  )
+options.parseArguments()
+
 
 # minimum logging
 process.MessageLogger = cms.Service("MessageLogger",
@@ -9,20 +50,20 @@ process.MessageLogger = cms.Service("MessageLogger",
     ),
     cout = cms.untracked.PSet(
         enable = cms.untracked.bool(True),
-        threshold = cms.untracked.string('INFO')
+        threshold = cms.untracked.string('ERROR')
     )
 )
 
 process.source = cms.Source('EmptyIOVSource',
-    timetype = cms.string('runnumber'),
-    firstValue = cms.uint64(1),
-    lastValue = cms.uint64(1),
+    timetype = cms.string('timestamp'),
+    firstValue = cms.uint64(options.timestamp),
+    lastValue = cms.uint64(options.timestamp),
     interval = cms.uint64(1)
 )
 
 # load info from database
 process.load('CondCore.CondDB.CondDB_cfi')
-process.CondDB.connect = 'sqlite_file:LHCInfoPerLS.sqlite' # SQLite input
+process.CondDB.connect = options.db # SQLite input
 
 process.PoolDBESSource = cms.ESSource('PoolDBESSource',
     process.CondDB,
@@ -30,12 +71,18 @@ process.PoolDBESSource = cms.ESSource('PoolDBESSource',
     toGet = cms.VPSet(
         cms.PSet(
             record = cms.string('LHCInfoPerLSRcd'),
-            tag = cms.string('LHCInfoPerLSFake')
+            tag = cms.string(options.tag)
         )
     )
 )
 
-process.LHCInfoPerLSAnalyzer = cms.EDAnalyzer('LHCInfoPerLSAnalyzer')
+process.LHCInfoPerLSAnalyzer = cms.EDAnalyzer('LHCInfoPerLSAnalyzer',
+                                              csvFormat = cms.untracked.bool(options.csv),
+                                              header = cms.untracked.bool(options.header),
+                                              iov = cms.untracked.uint64(options.timestamp),
+                                              separator = cms.untracked.string(options.separator),
+                                              
+)
 
 process.path = cms.Path(
     process.LHCInfoPerLSAnalyzer
