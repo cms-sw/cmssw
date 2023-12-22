3fa895ec3772b34a78d21bf736d59c39afd5947f
Breno Orzari <breno.orzari@hotmail.com>
2023-12-22 16:30:37 +0100

    Renaming and fix for phase2

--- DQM/TrackingMonitorSource/python/shortTrackResolution_cff.py
+++ DQM/TrackingMonitorSource/python/shortTrackResolution_cff.py
@@ -4,39 +4,43 @@ from DQMServices.Core.DQMEDAnalyzer import DQMEDAnalyzer
 from RecoTracker.FinalTrackSelectors.SingleLongTrackProducer_cfi import *
 
 from RecoTracker.FinalTrackSelectors.trackerTrackHitFilter_cfi import trackerTrackHitFilter as _trackerTrackHitFilter
-TrackerTrackHitFilter = _trackerTrackHitFilter.clone(src = "SingleLongTrackProducer",
+ShortTrackCandidates = _trackerTrackHitFilter.clone(src = "SingleLongTrackProducer",
                                                      truncateTracks = True,
                                                      replaceWithInactiveHits = True,
                                                      rejectBadStoNHits = True,
                                                      usePixelQualityFlag = True)
 
-TrackerTrackHitFilter3 = TrackerTrackHitFilter.clone(minimumHits = 3,
+from Configuration.Eras.Modifier_phase2_tracker_cff import phase2_tracker
+phase2_tracker.toModify(ShortTrackCandidates,
+                        isPhase2 = True)
+
+ShortTrackCandidates3 = ShortTrackCandidates.clone(minimumHits = 3,
                                                      layersRemaining = 3)
 
-TrackerTrackHitFilter4 = TrackerTrackHitFilter.clone(minimumHits = 4,
+ShortTrackCandidates4 = ShortTrackCandidates.clone(minimumHits = 4,
                                                      layersRemaining = 4)
 
-TrackerTrackHitFilter5 = TrackerTrackHitFilter.clone(minimumHits = 5,
+ShortTrackCandidates5 = ShortTrackCandidates.clone(minimumHits = 5,
                                                      layersRemaining = 5)
 
-TrackerTrackHitFilter6 = TrackerTrackHitFilter.clone(minimumHits = 6,
+ShortTrackCandidates6 = ShortTrackCandidates.clone(minimumHits = 6,
                                                      layersRemaining = 6)
 
-TrackerTrackHitFilter7 = TrackerTrackHitFilter.clone(minimumHits = 7,
+ShortTrackCandidates7 = ShortTrackCandidates.clone(minimumHits = 7,
                                                      layersRemaining = 7)
 
-TrackerTrackHitFilter8 = TrackerTrackHitFilter.clone(minimumHits = 8,
+ShortTrackCandidates8 = ShortTrackCandidates.clone(minimumHits = 8,
                                                      layersRemaining = 8)
 
 import RecoTracker.TrackProducer.CTFFinalFitWithMaterial_cff
-HitFilteredTracks = RecoTracker.TrackProducer.CTFFinalFitWithMaterial_cff.ctfWithMaterialTracks.clone(src = 'TrackerTrackHitFilter')
+RefittedShortTracks = RecoTracker.TrackProducer.CTFFinalFitWithMaterial_cff.ctfWithMaterialTracks.clone(src = 'ShortTrackCandidates')
 
-HitFilteredTracks3 = HitFilteredTracks.clone(src = 'TrackerTrackHitFilter3')
-HitFilteredTracks4 = HitFilteredTracks.clone(src = 'TrackerTrackHitFilter4')
-HitFilteredTracks5 = HitFilteredTracks.clone(src = 'TrackerTrackHitFilter5')
-HitFilteredTracks6 = HitFilteredTracks.clone(src = 'TrackerTrackHitFilter6')
-HitFilteredTracks7 = HitFilteredTracks.clone(src = 'TrackerTrackHitFilter7')
-HitFilteredTracks8 = HitFilteredTracks.clone(src = 'TrackerTrackHitFilter8')
+RefittedShortTracks3 = RefittedShortTracks.clone(src = 'ShortTrackCandidates3')
+RefittedShortTracks4 = RefittedShortTracks.clone(src = 'ShortTrackCandidates4')
+RefittedShortTracks5 = RefittedShortTracks.clone(src = 'ShortTrackCandidates5')
+RefittedShortTracks6 = RefittedShortTracks.clone(src = 'ShortTrackCandidates6')
+RefittedShortTracks7 = RefittedShortTracks.clone(src = 'ShortTrackCandidates7')
+RefittedShortTracks8 = RefittedShortTracks.clone(src = 'ShortTrackCandidates8')
 
 from DQM.TrackingMonitorSource.shortenedTrackResolution_cfi import shortenedTrackResolution as _shortenedTrackResolution
 trackingResolution = _shortenedTrackResolution.clone(folderName           = "Tracking/ShortTrackResolution",
@@ -47,19 +51,19 @@ trackingResolution = _shortenedTrackResolution.clone(folderName           = "Tra
                                                      maxTracksPtInput     = 99999.9,
                                                      maxDrInput           = 0.01,
                                                      tracksInputTag       = "SingleLongTrackProducer",
-                                                     tracksRerecoInputTag = ["HitFilteredTracks3","HitFilteredTracks4","HitFilteredTracks5","HitFilteredTracks6","HitFilteredTracks7","HitFilteredTracks8"])
+                                                     tracksRerecoInputTag = ["RefittedShortTracks3","RefittedShortTracks4","RefittedShortTracks5","RefittedShortTracks6","RefittedShortTracks7","RefittedShortTracks8"])
                                                      
 shortTrackResolution3to8 = cms.Sequence(SingleLongTrackProducer *
-                                        TrackerTrackHitFilter3 *
-                                        TrackerTrackHitFilter4 *
-                                        TrackerTrackHitFilter5 *
-                                        TrackerTrackHitFilter6 *
-                                        TrackerTrackHitFilter7 *
-                                        TrackerTrackHitFilter8 *
-                                        HitFilteredTracks3 *
-                                        HitFilteredTracks4 *
-                                        HitFilteredTracks5 *
-                                        HitFilteredTracks6 *
-                                        HitFilteredTracks7 *
-                                        HitFilteredTracks8 *
+                                        ShortTrackCandidates3 *
+                                        ShortTrackCandidates4 *
+                                        ShortTrackCandidates5 *
+                                        ShortTrackCandidates6 *
+                                        ShortTrackCandidates7 *
+                                        ShortTrackCandidates8 *
+                                        RefittedShortTracks3 *
+                                        RefittedShortTracks4 *
+                                        RefittedShortTracks5 *
+                                        RefittedShortTracks6 *
+                                        RefittedShortTracks7 *
+                                        RefittedShortTracks8 *
                                         trackingResolution)
