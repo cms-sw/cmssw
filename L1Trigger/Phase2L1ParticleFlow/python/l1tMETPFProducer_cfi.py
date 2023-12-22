8ff050c403cab407c98479609f8de4f334076508
Javier Duarte <jduarte@ucsd.edu>
2023-12-21 22:19:24 -0800

    first commit

--- L1Trigger/Phase2L1ParticleFlow/python/l1tMETPFProducer_cfi.py
+++ L1Trigger/Phase2L1ParticleFlow/python/l1tMETPFProducer_cfi.py
@@ -2,5 +2,6 @@ import FWCore.ParameterSet.Config as cms
 
 l1tMETPFProducer = cms.EDProducer("L1MetPfProducer",
                                  L1PFObjects = cms.InputTag("l1tLayer1","Puppi"),
-                                 maxCands = cms.int32(128),
+                                 maxCands = cms.int32(100),
+                                 modelVersion = cms.string("L1METML_v1"),
 )
