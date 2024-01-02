5adaab33a32e7ebdbafa2d15b8db588cd3c2cc26
Christopher Jones <chrisdjones15@gmail.com>
2024-01-02 10:22:26 -0600

    Modernize Phase2TrackerDigiProducer
    - changed to a global module
    - improved memory handling
    - avoid unnecessary recalculations

--- EventFilter/Phase2TrackerRawToDigi/plugins/Phase2TrackerDigiProducer.cc
+++ EventFilter/Phase2TrackerRawToDigi/plugins/Phase2TrackerDigiProducer.cc
@@ -13,7 +13,7 @@
 #include "EventFilter/Phase2TrackerRawToDigi/interface/Phase2TrackerFEDRawChannelUnpacker.h"
 #include "EventFilter/Phase2TrackerRawToDigi/interface/Phase2TrackerFEDZSChannelUnpacker.h"
 #include "EventFilter/Phase2TrackerRawToDigi/interface/utils.h"
-#include "FWCore/Framework/interface/stream/EDProducer.h"
+#include "FWCore/Framework/interface/global/EDProducer.h"
 #include "FWCore/Framework/interface/Event.h"
 #include "FWCore/Framework/interface/EventSetup.h"
 #include "FWCore/MessageLogger/interface/MessageLogger.h"
@@ -22,23 +22,19 @@
 
 namespace Phase2Tracker {
 
-  class Phase2TrackerDigiProducer : public edm::stream::EDProducer<> {
+  class Phase2TrackerDigiProducer : public edm::global::EDProducer<> {
   public:
     /// constructor
     Phase2TrackerDigiProducer(const edm::ParameterSet& pset);
     /// default constructor
     ~Phase2TrackerDigiProducer() override = default;
-    void beginRun(edm::Run const&, edm::EventSetup const&) override;
-    void produce(edm::Event&, const edm::EventSetup&) override;
+    void produce(edm::StreamID, edm::Event&, const edm::EventSetup&) const override;
 
   private:
     const edm::ESGetToken<Phase2TrackerCabling, Phase2TrackerCablingRcd> ph2CablingESToken_;
 
-    unsigned int runNumber_;
-    edm::EDGetTokenT<FEDRawDataCollection> token_;
-    const Phase2TrackerCabling* cabling_;
-    uint32_t cacheId_;
-    DetIdCollection detids_;
+    const edm::EDGetTokenT<FEDRawDataCollection> token_;
+    const edm::EDPutTokenT<edm::DetSetVector<Phase2TrackerDigi>> putToken_;
     class Registry {
     public:
       /// constructor
@@ -54,8 +50,6 @@ namespace Phase2Tracker {
       size_t index;
       uint16_t length;
     };
-    std::vector<Registry> proc_work_registry_;
-    std::vector<Phase2TrackerDigi> proc_work_digis_;
   };
 }  // namespace Phase2Tracker
 
@@ -68,41 +62,58 @@ using namespace std;
 namespace Phase2Tracker {
 
   Phase2TrackerDigiProducer::Phase2TrackerDigiProducer(const edm::ParameterSet& pset)
-      : ph2CablingESToken_(esConsumes<edm::Transition::BeginRun>()), runNumber_(0), cabling_(nullptr), cacheId_(0) {
-    // define product
-    produces<edm::DetSetVector<Phase2TrackerDigi>>("ProcessedRaw");
-    token_ = consumes<FEDRawDataCollection>(pset.getParameter<edm::InputTag>("ProductLabel"));
-  }
+      : ph2CablingESToken_(esConsumes()),
+        token_(consumes<FEDRawDataCollection>(pset.getParameter<edm::InputTag>("ProductLabel"))),
+        putToken_(produces<edm::DetSetVector<Phase2TrackerDigi>>("ProcessedRaw")) {}
 
-  void Phase2TrackerDigiProducer::beginRun(edm::Run const& run, edm::EventSetup const& es) {
+  void Phase2TrackerDigiProducer::produce(edm::StreamID, edm::Event& event, const edm::EventSetup& es) const {
     // fetch cabling from event setup
-    cabling_ = &es.getData(ph2CablingESToken_);
-  }
-
-  void Phase2TrackerDigiProducer::produce(edm::Event& event, const edm::EventSetup& es) {
-    // empty vectors for the next event
-    proc_work_registry_.clear();
-    proc_work_digis_.clear();
+    auto const& cabling = es.getData(ph2CablingESToken_);
 
     // Retrieve FEDRawData collection
     edm::Handle<FEDRawDataCollection> buffers;
     event.getByToken(token_, buffers);
 
+    //reserve enough working memory
+    std::vector<Registry> proc_work_registry;
+    std::vector<Phase2TrackerDigi> proc_work_digis;
+    {
+      size_t reserve_count = 0;
+      size_t reserve_digis = 0;
+      for (size_t fedIndex = Phase2Tracker::FED_ID_MIN; fedIndex <= Phase2Tracker::CMS_FED_ID_MAX; ++fedIndex) {
+        const FEDRawData& fed = buffers->FEDData(fedIndex);
+        if (fed.size() != 0) {
+          // construct buffer
+          Phase2Tracker::Phase2TrackerFEDBuffer buffer(fed.data(), fed.size());
+          int ichan = 0;
+          for (int ife = 0; ife < MAX_FE_PER_FED; ife++) {
+            for (int icbc = 0; icbc < MAX_CBC_PER_FE; icbc++) {
+              if (buffer.channel(ichan).length() > 0) {
+                ++reserve_count;
+                //calculation from Phase2TrackerFEDRawChannelUnpacker.h
+                reserve_digis += buffer.channel(ichan).length() * 8 - STRIPS_PADDING;
+                ++ichan;
+              }
+            }
+          }
+        }
+      }
+      proc_work_registry.reserve(2 * reserve_count);
+      proc_work_digis.reserve(reserve_digis);
+    }
     // Analyze strip tracker FED buffers in data
-    size_t fedIndex;
-    for (fedIndex = Phase2Tracker::FED_ID_MIN; fedIndex <= Phase2Tracker::CMS_FED_ID_MAX; ++fedIndex) {
+    for (size_t fedIndex = Phase2Tracker::FED_ID_MIN; fedIndex <= Phase2Tracker::CMS_FED_ID_MAX; ++fedIndex) {
       const FEDRawData& fed = buffers->FEDData(fedIndex);
       if (fed.size() != 0) {
         // construct buffer
-        Phase2Tracker::Phase2TrackerFEDBuffer* buffer = nullptr;
-        buffer = new Phase2Tracker::Phase2TrackerFEDBuffer(fed.data(), fed.size());
+        Phase2Tracker::Phase2TrackerFEDBuffer buffer(fed.data(), fed.size());
 
 #ifdef EDM_ML_DEBUG
         std::ostringstream ss;
         ss << " -------------------------------------------- " << endl;
         ss << " buffer debug ------------------------------- " << endl;
         ss << " -------------------------------------------- " << endl;
-        ss << " buffer size : " << buffer->bufferSize() << endl;
+        ss << " buffer size : " << buffer.bufferSize() << endl;
         ss << " fed id      : " << fedIndex << endl;
         ss << " -------------------------------------------- " << endl;
         ss << " tracker header debug ------------------------" << endl;
@@ -111,7 +122,7 @@ namespace Phase2Tracker {
         ss.clear();
         ss.str("");
 
-        Phase2TrackerFEDHeader tr_header = buffer->trackerHeader();
+        Phase2TrackerFEDHeader tr_header = buffer.trackerHeader();
         ss << " Version  : " << hex << setw(2) << (int)tr_header.getDataFormatVersion() << endl;
         ss << " Mode     : " << hex << setw(2) << tr_header.getDebugMode() << endl;
         ss << " Type     : " << hex << setw(2) << (int)tr_header.getEventType() << endl;
@@ -146,10 +157,10 @@ namespace Phase2Tracker {
         int ichan = 0;
         for (int ife = 0; ife < MAX_FE_PER_FED; ife++) {
           for (int icbc = 0; icbc < MAX_CBC_PER_FE; icbc++) {
-            const Phase2TrackerFEDChannel& channel = buffer->channel(ichan);
+            const Phase2TrackerFEDChannel& channel = buffer.channel(ichan);
             if (channel.length() > 0) {
               // get fedid from cabling
-              const Phase2TrackerModule mod = cabling_->findFedCh(std::make_pair(fedIndex, ife));
+              const Phase2TrackerModule mod = cabling.findFedCh(std::make_pair(fedIndex, ife));
               uint32_t detid = mod.getDetid();
 #ifdef EDM_ML_DEBUG
               ss << dec << " id from cabling : " << detid << endl;
@@ -193,46 +204,40 @@ namespace Phase2Tracker {
 
               // store beginning and end of this digis for this detid and add this registry to the list
               // and store data
-              Registry regItemTop(detid + 1, STRIPS_PER_CBC * icbc / 2, proc_work_digis_.size(), stripsTop.size());
-              proc_work_registry_.push_back(regItemTop);
-              proc_work_digis_.insert(proc_work_digis_.end(), stripsTop.begin(), stripsTop.end());
-              Registry regItemBottom(
-                  detid + 2, STRIPS_PER_CBC * icbc / 2, proc_work_digis_.size(), stripsBottom.size());
-              proc_work_registry_.push_back(regItemBottom);
-              proc_work_digis_.insert(proc_work_digis_.end(), stripsBottom.begin(), stripsBottom.end());
+              proc_work_registry.emplace_back(
+                  detid + 1, STRIPS_PER_CBC * icbc / 2, proc_work_digis.size(), stripsTop.size());
+              proc_work_digis.insert(proc_work_digis.end(), stripsTop.begin(), stripsTop.end());
+              proc_work_registry.emplace_back(
+                  detid + 2, STRIPS_PER_CBC * icbc / 2, proc_work_digis.size(), stripsBottom.size());
+              proc_work_digis.insert(proc_work_digis.end(), stripsBottom.begin(), stripsBottom.end());
             }
             ichan++;
           }
         }  // end loop on channels
-        // store digis in edm collections
-        std::sort(proc_work_registry_.begin(), proc_work_registry_.end());
-        std::vector<edm::DetSet<Phase2TrackerDigi>> sorted_and_merged;
-
-        edm::DetSetVector<Phase2TrackerDigi>* pr = new edm::DetSetVector<Phase2TrackerDigi>();
-
-        std::vector<Registry>::iterator it = proc_work_registry_.begin(), it2 = it + 1, end = proc_work_registry_.end();
-        while (it < end) {
-          sorted_and_merged.push_back(edm::DetSet<Phase2TrackerDigi>(it->detid));
-          std::vector<Phase2TrackerDigi>& digis = sorted_and_merged.back().data;
-          // first count how many digis we have
-          size_t len = it->length;
-          for (it2 = it + 1; (it2 != end) && (it2->detid == it->detid); ++it2) {
-            len += it2->length;
-          }
-          // reserve memory
-          digis.reserve(len);
-          // push them in
-          for (it2 = it + 0; (it2 != end) && (it2->detid == it->detid); ++it2) {
-            digis.insert(digis.end(), &proc_work_digis_[it2->index], &proc_work_digis_[it2->index + it2->length]);
-          }
-          it = it2;
-        }
-
-        edm::DetSetVector<Phase2TrackerDigi> proc_raw_dsv(sorted_and_merged, true);
-        pr->swap(proc_raw_dsv);
-        event.put(std::unique_ptr<edm::DetSetVector<Phase2TrackerDigi>>(pr), "ProcessedRaw");
-        delete buffer;
       }
     }
+
+    // store digis in edm collections
+    std::sort(proc_work_registry.begin(), proc_work_registry.end());
+    std::vector<edm::DetSet<Phase2TrackerDigi>> sorted_and_merged;
+
+    std::vector<Registry>::iterator it = proc_work_registry.begin(), it2 = it + 1, end = proc_work_registry.end();
+    while (it < end) {
+      sorted_and_merged.push_back(edm::DetSet<Phase2TrackerDigi>(it->detid));
+      std::vector<Phase2TrackerDigi>& digis = sorted_and_merged.back().data;
+      // first count how many digis we have
+      size_t len = it->length;
+      for (it2 = it + 1; (it2 != end) && (it2->detid == it->detid); ++it2) {
+        len += it2->length;
+      }
+      // reserve memory
+      digis.reserve(len);
+      // push them in
+      for (it2 = it + 0; (it2 != end) && (it2->detid == it->detid); ++it2) {
+        digis.insert(digis.end(), &proc_work_digis[it2->index], &proc_work_digis[it2->index + it2->length]);
+      }
+      it = it2;
+    }
+    event.emplace(putToken_, sorted_and_merged, true);
   }
 }  // namespace Phase2Tracker
