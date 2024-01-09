e64b9eee83d6f1fa51ace332e7cb3b97a0cbb0d2
Michal Bluj <michal.bluj@cern.ch>
2024-01-09 21:12:49 +0100

    OMTF emulator for phase2

---
+++ L1Trigger/L1TMuonOverlapPhase2/test/expert/runMuonOverlapPatternGeneratorClassProb_phase2.py
@@ -0,0 +1,196 @@
+# -*- coding: utf-8 -*-
+import FWCore.ParameterSet.Config as cms
+process = cms.Process("L1TMuonEmulation")
+import os
+import sys
+import re
+from os import listdir
+from os.path import isfile, join
+
+process.load("FWCore.MessageLogger.MessageLogger_cfi")
+
+verbose = True
+
+versionIn = "ExtraplMB1nadMB2DTQualAndEtaFixedP_ValueP1Scale_t20_v1_SingleMu_iPt_and_OneOverPt"
+#versionIn = "ExtraplMB1nadMB2DTQualAndEtaFixedP_ValueP1Scale_t20_SingleMu_mcWaw2023_OneOverPt"
+#versionIn = "ExtraplMB1nadMB2DTQualAndEtaValueP1Scale_t18"
+#versionIn = "0x00011_oldSample_3_30Files"
+
+versionOut =  versionIn + "_classProb17_recalib2_minDP0"
+
+if verbose: 
+    process.MessageLogger = cms.Service("MessageLogger",
+       #suppressInfo       = cms.untracked.vstring('AfterSource', 'PostModule'),
+       destinations   = cms.untracked.vstring(
+                                               #'detailedInfo',
+                                               #'critical',
+                                               #'cout',
+                                               #'cerr',
+                                               'omtfEventPrint'
+                    ),
+       categories        = cms.untracked.vstring('l1tOmtfEventPrint', 'OMTFReconstruction'),
+       omtfEventPrint = cms.untracked.PSet(    
+                         filename  = cms.untracked.string('Patterns_' + versionOut),
+                         extension = cms.untracked.string('.txt'),                
+                         threshold = cms.untracked.string('DEBUG'),
+                         default = cms.untracked.PSet( limit = cms.untracked.int32(0) ), 
+                         #INFO   =  cms.untracked.int32(0),
+                         #DEBUG   = cms.untracked.int32(0),
+                         l1tOmtfEventPrint = cms.untracked.PSet( limit = cms.untracked.int32(1000000000) ),
+                         OMTFReconstruction = cms.untracked.PSet( limit = cms.untracked.int32(1000000000) )
+                       ),
+       debugModules = cms.untracked.vstring('simOmtfDigis') 
+       #debugModules = cms.untracked.vstring('*')
+    )
+
+    #process.MessageLogger.cerr.FwkReport.reportEvery = cms.untracked.int32(100)
+if not verbose:
+    process.MessageLogger.cerr.FwkReport.reportEvery = cms.untracked.int32(-1)
+    process.options = cms.untracked.PSet(wantSummary = cms.untracked.bool(False), 
+                                         #SkipEvent = cms.untracked.vstring('ProductNotFound') 
+                                     )
+    
+# PostLS1 geometry used
+process.load('Configuration.Geometry.GeometryExtended2015Reco_cff')
+process.load('Configuration.Geometry.GeometryExtended2015_cff')    
+    
+# import of standard configurations
+process.load('Configuration.StandardSequences.Services_cff')
+process.load('SimGeneral.HepPDTESSource.pythiapdt_cfi')
+process.load('Configuration.EventContent.EventContent_cff')
+process.load('SimGeneral.MixingModule.mixNoPU_cfi')
+#process.load('Configuration.Geometry.GeometryExtended2026D41Reco_cff')
+#process.load('Configuration.Geometry.GeometryExtended2026D41_cff')
+process.load('Configuration.StandardSequences.MagneticField_cff')
+#process.load('Configuration.StandardSequences.SimL1Emulator_cff')
+process.load('Configuration.StandardSequences.EndOfProcess_cff')
+process.load('Configuration.StandardSequences.FrontierConditions_GlobalTag_cff')
+
+from Configuration.AlCa.GlobalTag import GlobalTag
+#process.GlobalTag = GlobalTag(process.GlobalTag, 'auto:upgradePLS3', '')
+process.GlobalTag = GlobalTag(process.GlobalTag, '103X_upgrade2023_realistic_v2', '') 
+
+path = '/eos/user/k/kbunkow/cms_data/SingleMuFullEta/721_FullEta_v4/' #old sample, but very big
+#path = '/eos/user/a/akalinow/Data/SingleMu/9_3_14_FullEta_v2/' #new sample, but small and more noisy
+#path = '/eos/user/a/akalinow/Data/SingleMu/9_3_14_FullEta_v1/'
+
+#path = '/afs/cern.ch/work/a/akalinow/public/MuCorrelator/Data/SingleMu/9_3_14_FullEta_v1/'
+#path = '/afs/cern.ch/work/k/kbunkow/public/data/SingleMuFullEta/721_FullEta_v4/'
+
+chosenFiles = []
+
+#chosenFiles = ['file://' + path + f for f in onlyfiles if (('_p_10_' in f) or ('_m_10_' in f))]
+chosenFiles.append('file://' + path + "SingleMu_18_p_1_1_rD8.root")
+
+
+print("chosenFiles")
+for chFile in chosenFiles:
+    print(chFile)
+
+if len(chosenFiles) == 0 :
+    print("no files selected!!!!!!!!!!!!!!!")
+    exit
+
+firstEv = 0#40000
+#nEvents = 1000
+
+# input files (up to 255 files accepted)
+process.source = cms.Source('PoolSource',
+fileNames = cms.untracked.vstring( 
+    #'file:/eos/user/k/kbunkow/cms_data/SingleMuFullEta/721_FullEta_v4/SingleMu_16_p_1_1_xTE.root',
+    #'file:/afs/cern.ch/user/k/kpijanow/Neutrino_Pt-2to20_gun_50.root',
+    list(chosenFiles), ),
+    skipEvents =  cms.untracked.uint32(0),
+    inputCommands=cms.untracked.vstring(
+        'keep *',
+        'drop l1tEMTFHit2016Extras_simEmtfDigis_CSC_HLT',
+        'drop l1tEMTFHit2016Extras_simEmtfDigis_RPC_HLT',
+        'drop l1tEMTFHit2016s_simEmtfDigis__HLT',
+        'drop l1tEMTFTrack2016Extras_simEmtfDigis__HLT',
+        'drop l1tEMTFTrack2016s_simEmtfDigis__HLT')
+)
+	                    
+process.maxEvents = cms.untracked.PSet( input = cms.untracked.int32(1))
+
+
+####Event Setup Producer
+process.load('L1Trigger.L1TMuonOverlapPhase1.fakeOmtfParams_cff')
+process.omtfParams.configXMLFile = cms.FileInPath("L1Trigger/L1TMuon/data/omtf_config/hwToLogicLayer_0x0009_patGen.xml")
+process.omtfParams.patternsXMLFiles = cms.VPSet(
+        cms.PSet(patternsXMLFile = cms.FileInPath("L1Trigger/L1TMuonOverlapPhase1/test/expert/omtf/Patterns_template.xml")), )
+
+process.esProd = cms.EDAnalyzer("EventSetupRecordDataGetter",
+   toGet = cms.VPSet(
+      cms.PSet(record = cms.string('L1TMuonOverlapParamsRcd'),
+               data = cms.vstring('L1TMuonOverlapParams'))
+                   ),
+   verbose = cms.untracked.bool(False)
+)
+
+#process.TFileService = cms.Service("TFileService", fileName = cms.string('omtfAnalysis1_1.root'), closeFileFast = cms.untracked.bool(True) )
+                                   
+####OMTF Emulator
+process.load('L1Trigger.L1TMuonOverlapPhase1.simOmtfDigis_cfi')
+
+process.simOmtfDigis.bxMin = cms.int32(0)
+process.simOmtfDigis.bxMax = cms.int32(0)
+
+process.simOmtfDigis.dumpResultToXML = cms.bool(False)
+process.simOmtfDigis.eventCaptureDebug = cms.bool(False)
+
+process.simOmtfDigis.patternsXMLFile = cms.FileInPath("L1Trigger/L1TMuonOverlapPhase1/test/expert/omtf/Patterns_template.xml")
+#process.simOmtfDigis.patternsXMLFile = cms.FileInPath("L1Trigger/L1TMuonOverlapPhase1/test/expert/omtf/Patterns_0x00012_oldSample_3_30Files_grouped1_classProb1_recalib.xml")
+#process.simOmtfDigis.patternsXMLFile = cms.FileInPath("L1Trigger/L1TMuonOverlapPhase1/test/expert/omtf/Patterns_0x00012_oldSample_3_30Files_grouped1_classProb11_recalib2.xml")
+#process.simOmtfDigis.patternsXMLFile = cms.FileInPath("L1Trigger/L1TMuon/data/omtf_config/Patterns_0x0009_oldSample_3_10Files_classProb2.xml")
+#process.simOmtfDigis.patternsXMLFiles = cms.VPSet(cms.PSet(patternsXMLFile = cms.FileInPath("L1Trigger/L1TMuon/data/omtf_config/GPs_parametrised_plus_v1.xml")),
+#                                                       cms.PSet(patternsXMLFile = cms.FileInPath("L1Trigger/L1TMuon/data/omtf_config/GPs_parametrised_minus_v1.xml"))  )
+
+#process.simOmtfDigis.patternsXMLFiles = cms.VPSet(#cms.PSet(patternsXMLFile = cms.FileInPath("L1Trigger/L1TMuonOverlapPhase1/test/expert/omtf/PatternsDisplaced_0x0007_minus.xml")),
+#                                                        cms.PSet(patternsXMLFile = cms.FileInPath("L1Trigger/L1TMuonOverlapPhase1/test/expert/omtf/PatternsDisplaced_0x0007_plus.xml"))  
+#                                                        )
+
+
+#process.simOmtfDigis.patternGenerator = cms.string("modifyClassProb")
+#process.simOmtfDigis.patternGenerator = cms.string("groupPatterns")
+process.simOmtfDigis.patternGenerator = cms.string("patternGenFromStat")
+#process.simOmtfDigis.patternGenerator = cms.string("") #does nothing except storing the patterns in the root file
+#process.simOmtfDigis.patternsROOTFile = cms.FileInPath("L1Trigger/L1TMuonOverlapPhase1/test/expert/omtf/Patterns_0x00011_oldSample_3_30Files_layerStat.root")
+#process.simOmtfDigis.patternsROOTFile = cms.FileInPath("L1Trigger/L1TMuonOverlapPhase1/test/expert/omtf/Patterns_layerStat_ExtraplMB1nadMB2_t14.root")
+#process.simOmtfDigis.patternsROOTFile = cms.FileInPath("L1Trigger/L1TMuonOverlapPhase1/test/expert/omtf/Patterns_layerStat_ExtraplMB1nadMB2FullAlgo_t16.root")
+process.simOmtfDigis.patternsROOTFile = cms.FileInPath("L1Trigger/L1TMuonOverlapPhase2/test/expert/Patterns_layerStat/Patterns_layerStat_" + versionIn + ".root")
+
+process.simOmtfDigis.patternType = cms.string("GoldenPatternWithStat")
+process.simOmtfDigis.generatePatterns = cms.bool(True)
+#process.simOmtfDigis.optimisedPatsXmlFile = cms.string("Patterns_0x0009_oldSample_3_10Files_classProb3.xml")
+#process.simOmtfDigis.optimisedPatsXmlFile = cms.string("Patterns_0x00012_oldSample_3_30Files_grouped1_classProb17_recalib2.xml")
+#process.simOmtfDigis.optimisedPatsXmlFile = cms.string("Patterns_ExtraplMB1nadMB2FullAlgo_t16_classProb17_recalib2.xml")
+process.simOmtfDigis.optimisedPatsXmlFile = cms.string("Patterns_" + versionOut + ".xml")
+#process.simOmtfDigis.optimisedPatsXmlFile = cms.string("PatternsDisplaced_0x0007_p.xml")
+
+process.simOmtfDigis.rpcMaxClusterSize = cms.int32(3)
+process.simOmtfDigis.rpcMaxClusterCnt = cms.int32(2)
+process.simOmtfDigis.rpcDropAllClustersIfMoreThanMax = cms.bool(True)
+
+process.simOmtfDigis.goldenPatternResultFinalizeFunction = cms.int32(3) #valid values are 0, 1, 2, 3, 5
+process.simOmtfDigis.lctCentralBx = cms.int32(6);#<<<<<<<<<<<<<<<<!!!!!!!!!!!!!!!!!!!!TODO this was changed in CMSSW 10(?) to 8. if the data were generated with the previous CMSSW then you have to use 6
+
+process.simOmtfDigis.simTracksTag = cms.InputTag('g4SimHits')
+
+#process.dumpED = cms.EDAnalyzer("EventContentAnalyzer")
+#process.dumpES = cms.EDAnalyzer("PrintEventSetupContent")
+
+process.L1TMuonSeq = cms.Sequence( process.esProd          
+                                   + process.simOmtfDigis 
+                                   #+ process.dumpED
+                                   #+ process.dumpES
+)
+
+process.L1TMuonPath = cms.Path(process.L1TMuonSeq)
+
+#process.out = cms.OutputModule("PoolOutputModule", 
+#   fileName = cms.untracked.string("l1tomtf_superprimitives1.root")
+#)
+
+#process.output_step = cms.EndPath(process.out)
+#process.schedule = cms.Schedule(process.L1TMuonPath)
+#process.schedule.extend([process.output_step])
---
+++ L1Trigger/L1TMuonOverlapPhase2/test/runMuonOverlap.py
@@ -0,0 +1,137 @@
+# -*- coding: utf-8 -*-
+import FWCore.ParameterSet.Config as cms
+process = cms.Process("L1TMuonEmulation")
+import os
+import sys
+
+process.load("FWCore.MessageLogger.MessageLogger_cfi")
+
+verbose = True
+
+if verbose: 
+    process.MessageLogger = cms.Service("MessageLogger",
+       #suppressInfo       = cms.untracked.vstring('AfterSource', 'PostModule'),
+       destinations   = cms.untracked.vstring(
+                                               #'detailedInfo',
+                                               #'critical',
+                                               #'cout',
+                                               #'cerr',
+                                               'omtfEventPrint'
+                    ),
+       categories        = cms.untracked.vstring('l1tOmtfEventPrint', 'OMTFReconstruction'),
+       omtfEventPrint = cms.untracked.PSet(    
+                         filename  = cms.untracked.string('log_MuonOverlap_v0209'),
+                         extension = cms.untracked.string('.txt'),                
+                         threshold = cms.untracked.string('DEBUG'),
+                         default = cms.untracked.PSet( limit = cms.untracked.int32(0) ), 
+                         #INFO   =  cms.untracked.int32(0),
+                         #DEBUG   = cms.untracked.int32(0),
+                         l1tOmtfEventPrint = cms.untracked.PSet( limit = cms.untracked.int32(1000000000) ),
+                         OMTFReconstruction = cms.untracked.PSet( limit = cms.untracked.int32(1000000000) )
+                       ),
+       debugModules = cms.untracked.vstring('L1MuonAnalyzerOmtf', 'simOmtfPhase2Digis') 
+       #debugModules = cms.untracked.vstring('*')
+    )
+
+    #process.MessageLogger.cerr.FwkReport.reportEvery = cms.untracked.int32(100)
+if not verbose:
+    process.MessageLogger.cerr.FwkReport.reportEvery = cms.untracked.int32(1000)
+    process.options = cms.untracked.PSet(wantSummary = cms.untracked.bool(False), 
+										 #SkipEvent = cms.untracked.vstring('ProductNotFound') 
+									 )                                         #SkipEvent = cms.untracked.vstring('ProductNotFound') 
+
+process.source = cms.Source('PoolSource',
+ #fileNames = cms.untracked.vstring('file:/afs/cern.ch/work/g/gflouris/public/SingleMuPt6180_noanti_10k_eta1.root')
+ #fileNames = cms.untracked.vstring('file:///afs/cern.ch/work/k/kbunkow/private/omtf_data/SingleMu_15_p_1_1_qtl.root')    
+ #fileNames = cms.untracked.vstring('file:///eos/user/k/kbunkow/cms_data/mc/PhaseIIFall17D/SingleMu_PU200_32DF01CC-A342-E811-9FE7-48D539F3863E_dump500Events.root')
+ #fileNames = cms.untracked.vstring("file:///eos/user/k/kbunkow/cms_data/mc/PhaseIITDRSpring19DR/PhaseIITDRSpring19DR_Mu_FlatPt2to100_noPU_v31_E0D5C6A5-B855-D14F-9124-0B2C9B28D0EA_dump4000Ev.root")
+ fileNames = cms.untracked.vstring('file:///eos/user/k/kbunkow/cms_data/mc/mcWaw2022/DoubleMuPt1to100Eta24_1kevents.root')                      
+ )
+	                    
+process.maxEvents = cms.untracked.PSet( input = cms.untracked.int32(-1))
+
+process.load('Configuration.Geometry.GeometryExtended2026D86Reco_cff')
+process.load('Configuration.Geometry.GeometryExtended2026D86_cff')  
+############################
+#process.load('Configuration.StandardSequences.FrontierConditions_GlobalTag_condDBv2_cff')
+process.load('Configuration.StandardSequences.FrontierConditions_GlobalTag_cff')
+from Configuration.AlCa.GlobalTag import GlobalTag
+process.GlobalTag = GlobalTag(process.GlobalTag, '103X_upgrade2023_realistic_v2', '') 
+
+
+####Event Setup Producer
+process.load('L1Trigger.L1TMuonOverlapPhase1.fakeOmtfParams_cff')
+process.omtfParams.configXMLFile = cms.FileInPath("L1Trigger/L1TMuon/data/omtf_config/hwToLogicLayer_0x0209.xml")
+
+process.esProd = cms.EDAnalyzer("EventSetupRecordDataGetter",
+   toGet = cms.VPSet(
+      cms.PSet(record = cms.string('L1TMuonOverlapParamsRcd'),
+               data = cms.vstring('L1TMuonOverlapParams'))
+                   ),
+   verbose = cms.untracked.bool(False)
+)
+
+process.TFileService = cms.Service("TFileService", fileName = cms.string('omtfAnalysis1.root'), closeFileFast = cms.untracked.bool(True) )
+		
+#TODO
+#process.load("L1Trigger.DTTriggerPhase2.dtTriggerPhase2PrimitiveDigis_cfi")
+#process.load("Configuration.StandardSequences.FrontierConditions_GlobalTag_cff")
+#process.load("Configuration.StandardSequences.MagneticField_AutoFromDBCurrent_cff")
+#process.GlobalTag.globaltag = "90X_dataRun2_Express_v2"
+#process.GlobalTag.globaltag = "80X_dataRun2_2016SeptRepro_v7"
+#process.GlobalTag.globaltag = "100X_upgrade2018_realistic_v10"
+
+#Calibrate Digis
+process.load("L1Trigger.DTTriggerPhase2.CalibratedDigis_cfi")
+#process.CalibratedDigis.flat_calib = 325 #turn to 0 to use the DB  , 325 for JM and Jorge benchmark
+process.CalibratedDigis.dtDigiTag = "simMuonDTDigis" #turn to 0 to use the DB  , 325 for JM and Jorge benchmark
+process.CalibratedDigis.scenario = 0 # 0 for mc, 1 for data, 2 for slice test
+
+#DTTriggerPhase2
+process.load("L1Trigger.DTTriggerPhase2.dtTriggerPhase2PrimitiveDigis_cfi")
+#process.dtTriggerPhase2PrimitiveDigis.trigger_with_sl = 3  #4 means SL 1 and 3
+#for the moment the part working in phase2 format is the slice test
+#process.dtTriggerPhase2PrimitiveDigis.p2_df = True
+#process.dtTriggerPhase2PrimitiveDigis.filter_primos = True
+#for debugging
+#process.dtTriggerPhase2PrimitiveDigis.pinta = True
+#process.dtTriggerPhase2PrimitiveDigis.min_phinhits_match_segment = 4
+#process.dtTriggerPhase2PrimitiveDigis.debug = True
+process.dtTriggerPhase2PrimitiveDigis.scenario = 0
+process.dtTriggerPhase2PrimitiveDigis.dump = True
+	
+								
+####OMTF Emulator
+process.load('L1Trigger.L1TMuonOverlapPhase2.simOmtfPhase2Digis_cfi')
+
+process.simOmtfPhase2Digis.dumpResultToXML = cms.bool(True)
+process.simOmtfPhase2Digis.eventCaptureDebug = cms.bool(True)
+
+process.simOmtfPhase2Digis.rpcMaxClusterSize = cms.int32(3)
+process.simOmtfPhase2Digis.rpcMaxClusterCnt = cms.int32(2)
+process.simOmtfPhase2Digis.rpcDropAllClustersIfMoreThanMax = cms.bool(False)
+
+process.simOmtfPhase2Digis.lctCentralBx = cms.int32(8);#<<<<<<<<<<<<<<<<!!!!!!!!!!!!!!!!!!!!TODO this was changed in CMSSW 10(?) to 8. if the data were generated with the previous CMSSW then you have to use 6
+
+process.simOmtfPhase2Digis.dropDTPrimitives = cms.bool(True)
+process.simOmtfPhase2Digis.usePhase2DTPrimitives = cms.bool(True)
+
+process.dumpED = cms.EDAnalyzer("EventContentAnalyzer")
+process.dumpES = cms.EDAnalyzer("PrintEventSetupContent")
+
+process.L1TMuonSeq = cms.Sequence( process.esProd          
+                                   + process.simOmtfPhase2Digis 
+                                   #+ process.dumpED
+                                   #+ process.dumpES
+)
+
+process.L1TMuonPath = cms.Path(process.CalibratedDigis * 
+							process.dtTriggerPhase2PrimitiveDigis * process.L1TMuonSeq)
+
+process.out = cms.OutputModule("PoolOutputModule", 
+   fileName = cms.untracked.string("l1tomtf_superprimitives1.root")
+)
+
+#process.output_step = cms.EndPath(process.out)
+#process.schedule = cms.Schedule(process.L1TMuonPath)
+#process.schedule.extend([process.output_step])
