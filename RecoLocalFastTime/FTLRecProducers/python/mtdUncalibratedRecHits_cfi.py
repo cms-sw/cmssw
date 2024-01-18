3f54e8ee02110956aa0628e8c15ab1b5f0234e66
parbol <pablom@cern.ch>
2024-01-18 22:54:59 +0100

    Fix parameters in uncalibrated rechits

--- RecoLocalFastTime/FTLRecProducers/python/mtdUncalibratedRecHits_cfi.py
+++ RecoLocalFastTime/FTLRecProducers/python/mtdUncalibratedRecHits_cfi.py
@@ -22,7 +22,11 @@ _endcapAlgo = cms.PSet(
     adcSaturation = mtdDigitizer.endcapDigitizer.ElectronicsSimulation.adcSaturation_MIP,
     toaLSB_ns     = mtdDigitizer.endcapDigitizer.ElectronicsSimulation.toaLSB_ns,
     tofDelay      = mtdDigitizer.endcapDigitizer.DeviceSimulation.tofDelay,
-    timeResolutionInNs = cms.string("0.039") # [ns]
+    timeResolutionInNs = cms.string("0.039"), # [ns]
+    timeCorr_p0 = cms.double(0.667116),
+    timeCorr_p1 = cms.double(-0.500665),
+    timeCorr_p2 = cms.double(0.141136),
+    timeCorr_p3 = cms.double(-0.0169169)
 )
 
 
@@ -33,11 +37,7 @@ mtdUncalibratedRecHits = cms.EDProducer(
     barrelDigis = cms.InputTag('mix:FTLBarrel'),
     endcapDigis = cms.InputTag('mix:FTLEndcap'),
     BarrelHitsName = cms.string('FTLBarrel'),
-    EndcapHitsName = cms.string('FTLEndcap'),
-    timeCorr_p0 = cms.double(0.667116),
-    timeCorr_p1 = cms.double(-0.500665),
-    timeCorr_p2 = cms.double(0.141136),
-    timeCorr_p3 = cms.double(-0.0169169)
+    EndcapHitsName = cms.string('FTLEndcap')
 )
 
 from Configuration.ProcessModifiers.premix_stage2_cff import premix_stage2
