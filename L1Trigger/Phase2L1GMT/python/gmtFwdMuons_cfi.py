ae4cff609d81d81654e0fa4ceb6842868f24d065
Zhenbin Wu <zhenbin.wu@gmail.com>
2024-01-22 11:51:42 -0600

    Add python files

---
+++ L1Trigger/Phase2L1GMT/python/gmtFwdMuons_cfi.py
@@ -0,0 +1,7 @@
+import FWCore.ParameterSet.Config as cms
+
+gmtFwdMuons = cms.EDProducer('Phase2L1TGMTFwdMuonTranslator',
+                             muons      = cms.InputTag('simGmtStage2Digis'),
+                             stubs      = cms.InputTag('gmtStubs','tps')
+
+)
