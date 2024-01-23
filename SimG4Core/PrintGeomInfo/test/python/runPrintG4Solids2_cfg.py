0084c011159859b0e7ee6135fb31f126b5040083
Sunanda <sunanda.banerjee@cern.ch>
2024-01-23 05:10:24 +0100

    Add possibilities of more scenarios in some of the checks of G4 Components

--- SimG4Core/PrintGeomInfo/test/python/runPrintG4Solids2_cfg.py
+++ SimG4Core/PrintGeomInfo/test/python/runPrintG4Solids2_cfg.py
@@ -3,7 +3,7 @@
 #   cmsRun grunPrintG4Solids_cfg.py geometry=D98 dd4hep=False
 #
 #   Options for geometry D88, D91, D92, D93, D94, D95, D96, D98, D99, D100,
-#                        D101, D102, D103
+#                        D101, D102, D103, D104,D105, D106
 #   Options for type DDD, DD4hep
 #
 ###############################################################################
@@ -18,7 +18,7 @@ options.register('geometry',
                  "D88",
                   VarParsing.VarParsing.multiplicity.singleton,
                   VarParsing.VarParsing.varType.string,
-                  "geometry of operations: D88, D91, D92, D93, D94, D95, D96, D98, D99, D100, D101, D102, D103")
+                  "geometry of operations: D88, D91, D92, D93, D94, D95, D96, D98, D99, D100, D101, D102, D103, D104,D105, D106")
 options.register('type',
                  "DDD",
                   VarParsing.VarParsing.multiplicity.singleton,
