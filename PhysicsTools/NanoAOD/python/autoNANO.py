09b3d39446d3005610d75a39a628668f052692f6
Rajarshi Bhattacharya <rajarshi.bhattacharya@cern.ch>
2024-01-24 10:43:24 +0100

    Fix in autoNano for the Muon POG custom Nano sequence

--- PhysicsTools/NanoAOD/python/autoNANO.py
+++ PhysicsTools/NanoAOD/python/autoNANO.py
@@ -28,13 +28,13 @@ autoNANO = {
     # L1 flavours: add tables through customize, supposed to be combined with PHYS
     'L1' : {'customize': 'nanoL1TrigObjCustomize'},
     'L1FULL' : {'customize': 'nanoL1TrigObjCustomizeFull'},
-    # MUDPG flavours: use their own sequence
-    'MUDPG' : {'sequence': 'DPGAnalysis/MuonTools/muNtupleProducer_cff.muDPGNanoProducer',
-               'customize': 'DPGAnalysis/MuonTools/muNtupleProducer_cff.muDPGNanoCustomize'},
-    'MUDPGBKG' : {'sequence': 'DPGAnalysis/MuonTools/muNtupleProducerBkg_cff.muDPGNanoProducerBkg',
-                  'customize': 'DPGAnalysis/MuonTools/muNtupleProducerBkg_cff.muDPGNanoBkgCustomize'},
     # Muon POG flavours : add tables through customize, supposed to be combined with PHYS
-    'MUPOG' : {'customize' : 'PrepMuonCustomNanoAOD'},
+    'MUPOG' : {'customize' : 'PhysicsTools/NanoAOD/custom_muon_cff.PrepMuonCustomNanoAOD'},
+    # MUDPG flavours: use their own sequence 
+    'MUDPG' : {'sequence': 'muDPGNanoProducer',
+               'customize': 'muDPGNanoCustomize'},
+    'MUDPGBKG' : {'sequence': 'muDPGNanoProducerBkg',
+                  'customize': 'muDPGNanoBkgCustomize'},
     # PromptReco config: PHYS+L1
     'Prompt' : {'sequence': '@PHYS',
                 'customize': '@PHYS+@L1'}
