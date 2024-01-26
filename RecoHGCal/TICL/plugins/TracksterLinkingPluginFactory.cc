0fc40551b99da4694c2363cfe65ae7ca5c312dd1
Felice Pantaleo <felice.pantaleo@cern.ch>
2024-01-26 15:34:22 +0100

    ticlv5: added a new Passthrough Trackster Linking plugin

--- RecoHGCal/TICL/plugins/TracksterLinkingPluginFactory.cc
+++ RecoHGCal/TICL/plugins/TracksterLinkingPluginFactory.cc
@@ -3,9 +3,11 @@
 #include "FWCore/ParameterSet/interface/ValidatedPluginMacros.h"
 #include "TracksterLinkingbyFastJet.h"
 #include "TracksterLinkingbySkeletons.h"
+#include "TracksterLinkingPassthrough.h"
 #include "RecoHGCal/TICL/plugins/TracksterLinkingPluginFactory.h"
 
 EDM_REGISTER_VALIDATED_PLUGINFACTORY(TracksterLinkingPluginFactory, "TracksterLinkingPluginFactory");
 DEFINE_EDM_VALIDATED_PLUGIN(TracksterLinkingPluginFactory, ticl::TracksterLinkingbySkeletons, "Skeletons");
 // DEFINE_EDM_VALIDATED_PLUGIN(TracksterLinkingPluginFactory, ticl::TracksterLinkingbySuperClustering, "SuperClustering");
 DEFINE_EDM_VALIDATED_PLUGIN(TracksterLinkingPluginFactory, ticl::TracksterLinkingbyFastJet, "FastJet");
+DEFINE_EDM_VALIDATED_PLUGIN(TracksterLinkingPluginFactory, ticl::TracksterLinkingPassthrough, "Passthrough");
