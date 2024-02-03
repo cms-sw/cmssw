a2c7a2708ec0416d007db9f1e983ca4a9be06075
Andrea Bocci <andrea.bocci@cern.ch>
2024-02-03 01:22:26 +0100

    Clean up includes and dependencies in pixel CondFormats

--- CondFormats/SiPixelObjects/interface/alpaka/SiPixelMappingDevice.h
+++ CondFormats/SiPixelObjects/interface/alpaka/SiPixelMappingDevice.h
@@ -2,11 +2,10 @@
 #define CondFormats_SiPixelObjects_interface_alpaka_SiPixelMappingDevice_h
 
 #include <cstdint>
-#include <alpaka/alpaka.hpp>
+
 #include "DataFormats/Portable/interface/alpaka/PortableCollection.h"
 #include "CondFormats/SiPixelObjects/interface/SiPixelMappingLayout.h"
-#include "HeterogeneousCore/AlpakaCore/interface/alpaka/ESProducer.h"
-#include "DataFormats/Portable/interface/PortableHostCollection.h"
+#include "HeterogeneousCore/AlpakaInterface/interface/config.h"
 
 namespace ALPAKA_ACCELERATOR_NAMESPACE {
 
