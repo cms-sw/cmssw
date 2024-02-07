0f1dbf46286ce0dd9c6ba2d60bde295228a7f351
Dinyar Rabady <dinyar@cern.ch>
2024-02-07 14:35:27 +0100

    Sync matching windows in uGMT fakeParams with what we have in firmware
    This is purely for bookkeeping, as the fakeParams aren't used normally.

--- L1Trigger/L1TMuon/python/fakeGmtParams_cff.py
+++ L1Trigger/L1TMuon/python/fakeGmtParams_cff.py
@@ -98,15 +98,15 @@ gmtParams = cms.ESProducer('L1TMuonGlobalParamsESProducer',
     BONegMatchQualLUTfEtaCoarse   = cms.double(1),
     BONegMatchQualLUTfPhi         = cms.double(6),
 
-    FOPosMatchQualLUTMaxDR        = cms.double(0.075),
+    FOPosMatchQualLUTMaxDR        = cms.double(0.2),
     FOPosMatchQualLUTfEta         = cms.double(1),
     FOPosMatchQualLUTfEtaCoarse   = cms.double(1),
-    FOPosMatchQualLUTfPhi         = cms.double(3),
+    FOPosMatchQualLUTfPhi         = cms.double(2),
 
-    FONegMatchQualLUTMaxDR        = cms.double(0.075),
+    FONegMatchQualLUTMaxDR        = cms.double(0.2),
     FONegMatchQualLUTfEta         = cms.double(1),
     FONegMatchQualLUTfEtaCoarse   = cms.double(1),
-    FONegMatchQualLUTfPhi         = cms.double(3),
+    FONegMatchQualLUTfPhi         = cms.double(2),
 
     # parameters for SortRank LUT if no path to an external LUT is defined
     # If a path to an external LUT is defined these parameters have no effect
