c8fc98a757c849016c5cda716e6b30dab09ed3c6
Sunanda <sunanda.banerjee@cern.ch>
2024-02-10 10:52:00 +0100

    Modify some of the test scripts in Validation/HGCalValidation for each of calorimeter scenarios

--- Validation/HGCalValidation/test/python/protoHGCalSimWatcher_cfg.py
+++ Validation/HGCalValidation/test/python/protoHGCalSimWatcher_cfg.py
@@ -1,22 +1,22 @@
 ###############################################################################
 # Way to use this:
-#   cmsRun protoHGCalSimWatcher_cfg.py geometry=D88
+#   cmsRun protoHGCalSimWatcher_cfg.py geometry=D99
 #
-#   Options for geometry D88, D92, D93
+#   Options for geometry D98, D99, D108, D94, D103, D104, D106, D109
 #
 ###############################################################################
 import FWCore.ParameterSet.Config as cms
-import os, sys, imp, re
+import os, sys, importlib, re
 import FWCore.ParameterSet.VarParsing as VarParsing
 
 ####################################################################
 ### SETUP OPTIONS
 options = VarParsing.VarParsing('standard')
 options.register('geometry',
-                 "D88",
+                 "D99",
                   VarParsing.VarParsing.multiplicity.singleton,
                   VarParsing.VarParsing.varType.string,
-                  "geometry of operations: D88, D92, D93")
+                  "geometry of operations: D98, D99, D108, D94, D103, D104, D106, D109")
 
 ### get and parse the command line arguments
 options.parseArguments()
@@ -26,8 +26,21 @@ print(options)
 ####################################################################
 # Use the options
 
-from Configuration.Eras.Era_Phase2C17I13M9_cff import Phase2C17I13M9
-process = cms.Process('PROD',Phase2C17I13M9)
+if (options.geometry == "D94"):
+    from Configuration.Eras.Era_Phase2C20I13M9_cff import Phase2C20I13M9
+    process = cms.Process('PROD',Phase2C20I13M9)
+elif (options.geometry == "D104"):
+    from Configuration.Eras.Era_Phase2C22I13M9_cff import Phase2C22I13M9
+    process = cms.Process('PROD',PhaseC22I13M9)
+elif (options.geometry == "D106"):
+    from Configuration.Eras.Era_Phase2C22I13M9_cff import Phase2C22I13M9
+    process = cms.Process('PROD',PhaseC22I13M9)
+elif (options.geometry == "D109"):
+    from Configuration.Eras.Era_Phase2C22I13M9_cff import Phase2C22I13M9
+    process = cms.Process('PROD',PhaseC22I13M9)
+else:
+    from Configuration.Eras.Era_Phase2C17I13M9_cff import Phase2C17I13M9
+    process = cms.Process('PROD',Phase2C17I13M9)
 
 geomFile = "Configuration.Geometry.GeometryExtended2026" + options.geometry + "Reco_cff"
 fileCheck = "testHGCalSimWatcher2026" + options.geometry + ".root"
@@ -149,7 +162,7 @@ else:
 # Other statements
 process.genstepfilter.triggerConditions=cms.vstring("generation_step")
 from Configuration.AlCa.GlobalTag import GlobalTag
-process.GlobalTag = GlobalTag(process.GlobalTag, 'auto:phase2_realistic', '')
+process.GlobalTag = GlobalTag(process.GlobalTag, 'auto:phase2_realistic_T21', '')
 
 process.generator = cms.EDProducer("FlatRandomPtGunProducer",
     PGunParameters = cms.PSet(
