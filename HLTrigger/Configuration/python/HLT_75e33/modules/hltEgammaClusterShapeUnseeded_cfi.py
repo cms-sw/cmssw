77e55cfac97882d221cb775e6f563211b774f7a4
swagata87 <physics.swagata@gmail.com>
2024-02-11 15:33:11 +0100

    adapt phase 2 cfgs

--- HLTrigger/Configuration/python/HLT_75e33/modules/hltEgammaClusterShapeUnseeded_cfi.py
+++ HLTrigger/Configuration/python/HLT_75e33/modules/hltEgammaClusterShapeUnseeded_cfi.py
@@ -3,6 +3,5 @@ import FWCore.ParameterSet.Config as cms
 hltEgammaClusterShapeUnseeded = cms.EDProducer("EgammaHLTClusterShapeProducer",
     ecalRechitEB = cms.InputTag("hltEcalRecHit","EcalRecHitsEB"),
     ecalRechitEE = cms.InputTag("hltEcalRecHit","EcalRecHitsEE"),
-    isIeta = cms.bool(True),
     recoEcalCandidateProducer = cms.InputTag("hltEgammaCandidatesUnseeded")
 )
