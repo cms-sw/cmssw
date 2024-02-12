b44e74c84eaf35e2d5eaa5e76067e9cf9b93881b
kerstinlovisa <lovisa.rygaard@cern.ch>
2024-02-12 14:59:08 +0100

    Re-introducing headers.h file needed for serialization classes

--- CondFormats/HGCalObjects/src/classes.h
+++ CondFormats/HGCalObjects/src/classes.h
@@ -1,3 +1,1 @@
-#include "CondFormats/HGCalObjects/interface/HGCalDenseIndexerBase.h"
-#include "CondFormats/HGCalObjects/interface/HGCalMappingCellIndexer.h"
-#include "CondFormats/HGCalObjects/interface/HGCalMappingModuleIndexer.h"
+#include "CondFormats/HGCalObjects/src/headers.h"
