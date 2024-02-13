acefa3368343229f94d50271c2ec146f12994734
Chris Jones <chrisdjones15@gmail.com>
2024-02-13 14:43:20 -0600

    Alternative python module configuration syntax
    Generates a new python file from the C++ configuration description
    which describes the default. This can be imported to allow a
    more simplified module configuration syntax.

--- FWCore/Integration/test/check_empty_event_cfg.py
+++ FWCore/Integration/test/check_empty_event_cfg.py
@@ -1,14 +1,15 @@
 import FWCore.ParameterSet.Config as cms
+from FWCore.Modules.modules import EventContentAnalyzer, EmptySource
 
 process = cms.Process("EMPTY")
 
-process.test = cms.EDAnalyzer("EventContentAnalyzer", listPathStatus = cms.untracked.bool(True))
+process.test = EventContentAnalyzer(listPathStatus = True)
 
 process.e = cms.EndPath(process.test)
 
 #process.out = cms.OutputModule("AsciiOutputModule")
 #process.e2 = cms.EndPath(process.out)
 
-process.source = cms.Source("EmptySource")
+process.source = EmptySource()
 
 process.maxEvents.input = 1
--- FWCore/Integration/test/delayedreader_throw_cfg.py
+++ FWCore/Integration/test/delayedreader_throw_cfg.py
@@ -1,27 +1,31 @@
 import FWCore.ParameterSet.Config as cms
+from FWCore.Framework.modules import AddIntsProducer, IntProductFilter
+from FWCore.Modules.modules import AsciiOutputModule
+from FWCore.Integration.modules import DelayedReaderThrowingSource
 
 process = cms.Process("TEST")
 
-process.source = cms.Source("DelayedReaderThrowingSource", labels = cms.untracked.vstring("test", "test2", "test3"))
+process.source = DelayedReaderThrowingSource( labels = ["test", "test2", "test3"])
 
-process.getter = cms.EDProducer("AddIntsProducer", labels = cms.VInputTag(cms.InputTag("test","","INPUTTEST")))
-process.onPath = cms.EDProducer("AddIntsProducer", labels = cms.VInputTag(cms.InputTag("test2", "", "INPUTTEST"), cms.InputTag("getter", "other")))
-process.f1 = cms.EDFilter("IntProductFilter", label = cms.InputTag("onPath"), shouldProduce = cms.bool(True))
-process.f2 = cms.EDFilter("IntProductFilter", label = cms.InputTag("onPath"), shouldProduce = cms.bool(True))
-process.inFront = cms.EDFilter("IntProductFilter", label = cms.InputTag("test3"))
+process.getter = AddIntsProducer(labels = [("test","","INPUTTEST")])
+process.onPath = AddIntsProducer(labels = [("test2", "", "INPUTTEST"), ("getter", "other")])
+process.f1 = IntProductFilter( label = "onPath", shouldProduce = True)
+process.f2 = IntProductFilter( label = "onPath", shouldProduce = True)
+process.inFront = IntProductFilter( label = "test3")
 
 process.p1 = cms.Path(process.inFront+process.onPath+process.f1+process.f2)
 process.p3 = cms.Path(process.onPath+process.f1, cms.Task(process.getter))
 
 process.p2 = cms.Path(process.onPath+process.f2)
 
-
-#process.dump = cms.EDAnalyzer("EventContentAnalyzer")
+#from FWCore.Modules.modules import EventContentAnalyzer import *
+#process.dump = EventContentAnalyzer()
 #process.p = cms.Path(process.dump)
 
-process.out = cms.OutputModule("AsciiOutputModule")
+process.out = AsciiOutputModule()
 process.e = cms.EndPath(process.out, cms.Task(process.getter))
 
 process.maxEvents.input = 1
 
-#process.add_(cms.Service("Tracer"))
+#from FWCore.Services.modules import Tracer
+#process.add_(Tracer())
