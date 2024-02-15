30317a2ab837179aab320b621ebc29c4f92944ad
mmusich <marco.musich@cern.ch>
2024-02-15 09:44:25 +0100

    Integrate CMSHLT-3023

--- HLTrigger/Configuration/test/OnLine_HLT_PIon.py
+++ HLTrigger/Configuration/test/OnLine_HLT_PIon.py
@@ -1,6 +1,6 @@
 # hltGetConfiguration /dev/CMSSW_14_0_0/PIon --full --data --type PIon --unprescale --process HLTPIon --globaltag auto:run3_hlt_PIon --input file:RelVal_Raw_PIon_DATA.root
 
-# /dev/CMSSW_14_0_0/PIon/V12 (CMSSW_14_0_0_pre3)
+# /dev/CMSSW_14_0_0/PIon/V13 (CMSSW_14_0_0_pre3)
 
 import FWCore.ParameterSet.Config as cms
 
@@ -12,7 +12,7 @@ process = cms.Process( "HLTPIon" )
 process.ProcessAcceleratorCUDA = ProcessAcceleratorCUDA()
 
 process.HLTConfigVersion = cms.PSet(
-  tableName = cms.string('/dev/CMSSW_14_0_0/PIon/V12')
+  tableName = cms.string('/dev/CMSSW_14_0_0/PIon/V13')
 )
 
 process.HLTIter0PSetTrajectoryBuilderIT = cms.PSet( 
