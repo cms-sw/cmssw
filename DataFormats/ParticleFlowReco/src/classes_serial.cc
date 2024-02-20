23506e80c7b2b701e0e6edebcad9ccc9e7660022
Andrea Bocci <andrea.bocci@cern.ch>
2024-02-20 09:32:44 +0100

    Move PF rechit read rules to classes_serial.cc
    Move the ROOT read rules for the PF rechits SoA host collections from
    classes_serial_def.xml to classes_serial.cc.

---
+++ DataFormats/ParticleFlowReco/src/classes_serial.cc
@@ -0,0 +1,10 @@
+#include "DataFormats/ParticleFlowReco/interface/CaloRecHitHostCollection.h"
+#include "DataFormats/ParticleFlowReco/interface/PFClusterHostCollection.h"
+#include "DataFormats/ParticleFlowReco/interface/PFRecHitFractionHostCollection.h"
+#include "DataFormats/ParticleFlowReco/interface/PFRecHitHostCollection.h"
+#include "DataFormats/Portable/interface/PortableHostCollectionReadRules.h"
+
+SET_PORTABLEHOSTCOLLECTION_READ_RULES(reco::CaloRecHitHostCollection);
+SET_PORTABLEHOSTCOLLECTION_READ_RULES(reco::PFClusterHostCollection);
+SET_PORTABLEHOSTCOLLECTION_READ_RULES(reco::PFRecHitFractionHostCollection);
+SET_PORTABLEHOSTCOLLECTION_READ_RULES(reco::PFRecHitHostCollection);
