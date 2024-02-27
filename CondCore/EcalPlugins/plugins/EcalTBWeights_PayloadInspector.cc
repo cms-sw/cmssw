b17c176d942c46082e9cc60020eb67774dc33ae8
Andrea Bocci <andrea.bocci@cern.ch>
2024-02-27 14:20:39 +0100

    Add missing newline at the end of file

--- CondCore/EcalPlugins/plugins/EcalTBWeights_PayloadInspector.cc
+++ CondCore/EcalPlugins/plugins/EcalTBWeights_PayloadInspector.cc
@@ -8,11 +8,11 @@
 // the data format of the condition to be inspected
 #include "CondFormats/EcalObjects/interface/EcalTBWeights.h"
 
-#include "TH2F.h" // a 2-D histogram with four bytes per cell (float)
+#include "TH2F.h"  // a 2-D histogram with four bytes per cell (float)
 #include "TCanvas.h"
 #include "TLine.h"
 #include "TStyle.h"
-#include "TLatex.h"//write mathematical equations.
+#include "TLatex.h"  //write mathematical equations.
 #include "TPave.h"
 #include "TPaveStats.h"
 #include <string>
@@ -20,92 +20,92 @@
 
 namespace {
 
-/*******************************************************
+  /*******************************************************
  2d plot of Ecal TBWeights of 1 IOV
  *******************************************************/
-class EcalTBWeightsPlot: public cond::payloadInspector::PlotImage<EcalTBWeights>{
-	public:
-		EcalTBWeightsPlot():
-			cond::payloadInspector::PlotImage<EcalTBWeights>("Ecal TBWeights - map "){
-				setSingleIov(true);
-		}
+  class EcalTBWeightsPlot : public cond::payloadInspector::PlotImage<EcalTBWeights> {
+  public:
+    EcalTBWeightsPlot() : cond::payloadInspector::PlotImage<EcalTBWeights>("Ecal TBWeights - map ") {
+      setSingleIov(true);
+    }
 
-	bool fill(const std::vector<std::tuple<cond::Time_t, cond::Hash> >& iovs)override {
-		auto iov=iovs.front();
-		std::shared_ptr <EcalTBWeights> payload = fetchPayload(std::get<1> (iov));
-		unsigned int run=std::get<0> (iov);
-		TH2F* align;
-		int NbRows;
+    bool fill(const std::vector<std::tuple<cond::Time_t, cond::Hash> >& iovs) override {
+      auto iov = iovs.front();
+      std::shared_ptr<EcalTBWeights> payload = fetchPayload(std::get<1>(iov));
+      unsigned int run = std::get<0>(iov);
+      TH2F* align;
+      int NbRows;
 
-		if(payload.get()){
-			EcalTBWeights::EcalTBWeightMap map=(*payload).getMap();
-			NbRows=map.size();
-			align=new TH2F("Ecal PTM Temperatures","EcalXtalGroupId          EcalTDCId          EcalWeightSet",
-				3,0,3,NbRows,0,NbRows);
-		
-			double row = NbRows-0.5;
+      if (payload.get()) {
+        EcalTBWeights::EcalTBWeightMap map = (*payload).getMap();
+        NbRows = map.size();
+        align = new TH2F("Ecal PTM Temperatures",
+                         "EcalXtalGroupId          EcalTDCId          EcalWeightSet",
+                         3,
+                         0,
+                         3,
+                         NbRows,
+                         0,
+                         NbRows);
 
-			for (EcalTBWeights::EcalPTMTemperatureMap::const_iterator it =map.begin(); 
-				it != map.end();it++) {
-				
-				//uint32_t mapKey=it->first;
-				//float val=it->second;
+        double row = NbRows - 0.5;
 
-				//align->Fill(0.5,row,mapKey);
-				//align->Fill(1.5,row,val);
+        for (EcalTBWeights::EcalPTMTemperatureMap::const_iterator it = map.begin(); it != map.end(); it++) {
+          //uint32_t mapKey=it->first;
+          //float val=it->second;
 
-				row--;
-			}
-			
-		}else
-			return false;
+          //align->Fill(0.5,row,mapKey);
+          //align->Fill(1.5,row,val);
 
-		gStyle->SetPalette(1);
-		gStyle->SetOptStat(0);
-		TCanvas canvas("CC map", "CC map", 1000, 1000);
-		TLatex t1;
-		t1.SetNDC();
-		t1.SetTextAlign(26);
-		t1.SetTextSize(0.05);
-		t1.SetTextColor(2);
-		t1.DrawLatex(0.5, 0.96,Form("Ecal TBWeights, IOV %i", run));
+          row--;
+        }
 
+      } else
+        return false;
 
-		TPad* pad = new TPad("pad", "pad", 0.0, 0.0, 1.0, 0.94);
-		pad->Draw();
-		pad->cd();
-		align->Draw("TEXT");
-		TLine* l = new TLine;
-		l->SetLineWidth(1);
+      gStyle->SetPalette(1);
+      gStyle->SetOptStat(0);
+      TCanvas canvas("CC map", "CC map", 1000, 1000);
+      TLatex t1;
+      t1.SetNDC();
+      t1.SetTextAlign(26);
+      t1.SetTextSize(0.05);
+      t1.SetTextColor(2);
+      t1.DrawLatex(0.5, 0.96, Form("Ecal TBWeights, IOV %i", run));
 
-		for (int i = 1; i < NbRows; i++) {
-		  double y = (double) i;
-		  l = new TLine(0., y, 3., y);
-		  l->Draw();
-		}
+      TPad* pad = new TPad("pad", "pad", 0.0, 0.0, 1.0, 0.94);
+      pad->Draw();
+      pad->cd();
+      align->Draw("TEXT");
+      TLine* l = new TLine;
+      l->SetLineWidth(1);
 
-		for (int i = 1; i < 3; i++) {
-		  double x = (double) i;
-		  double y = (double) NbRows;
-		  l = new TLine(x, 0., x, y);
-		  l->Draw();
-		}
+      for (int i = 1; i < NbRows; i++) {
+        double y = (double)i;
+        l = new TLine(0., y, 3., y);
+        l->Draw();
+      }
 
-		align->GetXaxis()->SetTickLength(0.);
-		align->GetXaxis()->SetLabelSize(0.);
-		align->GetYaxis()->SetTickLength(0.);
-		align->GetYaxis()->SetLabelSize(0.);
+      for (int i = 1; i < 3; i++) {
+        double x = (double)i;
+        double y = (double)NbRows;
+        l = new TLine(x, 0., x, y);
+        l->Draw();
+      }
 
-		std::string ImageName(m_imageFileName);
-		canvas.SaveAs(ImageName.c_str());
+      align->GetXaxis()->SetTickLength(0.);
+      align->GetXaxis()->SetLabelSize(0.);
+      align->GetYaxis()->SetTickLength(0.);
+      align->GetYaxis()->SetLabelSize(0.);
 
-		return true;
-	}
-};
+      std::string ImageName(m_imageFileName);
+      canvas.SaveAs(ImageName.c_str());
 
-} //close namespace
+      return true;
+    }
+  };
+
+}  // namespace
 
 // Register the classes as boost python plugin
-PAYLOAD_INSPECTOR_MODULE(EcalTBWeights) {
-  PAYLOAD_INSPECTOR_CLASS(EcalTBWeightsPlot);
-}
+PAYLOAD_INSPECTOR_MODULE(EcalTBWeights) { PAYLOAD_INSPECTOR_CLASS(EcalTBWeightsPlot); }
