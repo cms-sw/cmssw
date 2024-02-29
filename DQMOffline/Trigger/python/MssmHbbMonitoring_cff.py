e2051255bff64ce6c58c2ab1e71bc9649cdbc562
Roberval Walsh <roberval.walsh@gmail.com>
2024-02-29 19:33:07 +0100

    improved muon pt binning

--- DQMOffline/Trigger/python/MssmHbbMonitoring_cff.py
+++ DQMOffline/Trigger/python/MssmHbbMonitoring_cff.py
@@ -2,6 +2,8 @@ import FWCore.ParameterSet.Config as cms
 
 from DQMOffline.Trigger.MssmHbbMonitoring_cfi import mssmHbbMonitoring
 
+MUON_PT_BINNING = [0,7,11,12,13,15,20,30,40,50]
+
 #Define MssmHbb specific cuts 
 hltMssmHbbmonitoring =  mssmHbbMonitoring.clone(
     btagAlgos = ["pfParticleNetAK4DiscriminatorsJetTagsForRECO:BvsAll"],
@@ -36,7 +38,7 @@ hltMssmHbbMonitoringSL40 = hltMssmHbbmonitoring.clone(
     muoSelection = 'pt>12 & abs(eta)<2.2 & isPFMuon & isGlobalMuon  & innerTrack.hitPattern.trackerLayersWithMeasurement>5 & innerTrack.hitPattern.numberOfValidPixelHits>0 & globalTrack.hitPattern.numberOfValidMuonHits>0 & globalTrack.normalizedChi2<10',
     numGenericTriggerEventPSet = dict(hltPaths = ['HLT_Mu12_DoublePFJets40MaxDeta1p6_PNet2BTag_0p11_v*']),
     histoPSet = dict(jetPtBinning = [0,40,60,80,120,160,250,300,350,400,500,1000,1500],
-                     muPtBinning = [0,7,11,12,13,15,20,30,40,50,70,100,150,200,400,700])
+                     muPtBinning = MUON_PT_BINNING)
 
 )
 
@@ -48,7 +50,7 @@ hltMssmHbbMonitoringSL54 = hltMssmHbbmonitoring.clone(
     muoSelection = 'pt>12 & abs(eta)<2.2 & isPFMuon & isGlobalMuon  & innerTrack.hitPattern.trackerLayersWithMeasurement>5 & innerTrack.hitPattern.numberOfValidPixelHits>0 & globalTrack.hitPattern.numberOfValidMuonHits>0 & globalTrack.normalizedChi2<10',
     numGenericTriggerEventPSet = dict(hltPaths = ['HLT_Mu12_DoublePFJets54MaxDeta1p6_PNet2BTag_0p11_v*']),
     histoPSet = dict(jetPtBinning = [0,40,60,80,120,160,250,300,350,400,500,1000,1500],
-                     muPtBinning = [0,7,11,12,13,15,20,30,40,50,70,100,150,200,400,700])
+                     muPtBinning = MUON_PT_BINNING)
 )
 
 
@@ -60,7 +62,7 @@ hltMssmHbbMonitoringMu12 = hltMssmHbbmonitoring.clone(
     njets = 0,
     muoSelection = 'pt>12 & abs(eta)<2.2 & isPFMuon & isGlobalMuon  & innerTrack.hitPattern.trackerLayersWithMeasurement>5 & innerTrack.hitPattern.numberOfValidPixelHits>0 & globalTrack.hitPattern.numberOfValidMuonHits>0 & globalTrack.normalizedChi2<10',
     numGenericTriggerEventPSet = dict(hltPaths = ['HLT_Mu12eta2p3_v*']),
-    histoPSet = dict(muPtBinning = [0,7,11,12,13,15,20,30,40,50,70,100,150,200,400,700])
+    histoPSet = dict(muPtBinning = MUON_PT_BINNING)
 )
 
 hltMssmHbbMonitoringMu12Jet40 = hltMssmHbbmonitoring.clone(
@@ -72,7 +74,7 @@ hltMssmHbbMonitoringMu12Jet40 = hltMssmHbbmonitoring.clone(
     jetSelection = 'pt>40 & abs(eta)<2.2',
     numGenericTriggerEventPSet = dict(hltPaths = ['HLT_Mu12eta2p3_PFJet40_v*']),
     histoPSet = dict(jetPtBinning = [0,40,60,80,120,160,250,300,350,400,500,1000,1500],
-                     muPtBinning = [0,7,11,12,13,15,20,30,40,50,70,100,150,200,400,700])
+                     muPtBinning = MUON_PT_BINNING)
 )
 
 
