09033127eec99fc0a6159b8a3af858cae9c95720
mmusich <marco.musich@cern.ch>
2024-03-04 12:32:48 +0100

    This update includes the integration of the following JIRA tickets: - [CMSHLT-3067](https://its.cern.ch/jira/browse/CMSHLT-3067): [Scouting] Adding new L1 seeds to the DoubleMuon Scouting path - [CMSHLT-3089](https://its.cern.ch/jira/browse/CMSHLT-3089): [MUO/Scouting] Add hltScoutingMuonPackerVtx to Scouting EventContent

--- HLTrigger/Configuration/test/OnLine_HLT_HIon.py
+++ HLTrigger/Configuration/test/OnLine_HLT_HIon.py
@@ -1,6 +1,6 @@
 # hltGetConfiguration /dev/CMSSW_14_0_0/HIon --full --data --type HIon --unprescale --process HLTHIon --globaltag auto:run3_hlt_HIon --input file:RelVal_Raw_HIon_DATA.root
 
-# /dev/CMSSW_14_0_0/HIon/V61 (CMSSW_14_0_1_HLT1)
+# /dev/CMSSW_14_0_0/HIon/V64 (CMSSW_14_0_1_HLT2)
 
 import FWCore.ParameterSet.Config as cms
 
@@ -11,7 +11,7 @@ process = cms.Process( "HLTHIon" )
 process.load("Configuration.StandardSequences.Accelerators_cff")
 
 process.HLTConfigVersion = cms.PSet(
-  tableName = cms.string("/dev/CMSSW_14_0_0/HIon/V61")
+  tableName = cms.string("/dev/CMSSW_14_0_0/HIon/V64")
 )
 
 process.HLTIter0PSetTrajectoryBuilderIT = cms.PSet( 
