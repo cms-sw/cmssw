a8f0abb8a7097decf1a8632acb806b2bcd5ec808
Matevz Tadel <mtadel@ucsd.edu>
2024-03-04 22:15:24 -0800

    Shell and (semi)-debug printout improvements.
    - Makefile -- add ShellDict.h to wrap all shell dict includes
    
    - Shell dictionary - add dicts for event, hit, track
    
    - Shell.cc - add printouts to make clear what seed-cleaning is doing
    
    - Event.cc
      - better hit printout
      - clear seed lookup after setting new external seed vector
    
    - buildtestMPlex.cc - do not enforce simlabel lokup table creation,
      let it be generated on demand.

--- RecoTracker/MkFitCMS/standalone/Shell.h
+++ RecoTracker/MkFitCMS/standalone/Shell.h
@@ -40,6 +40,9 @@ namespace mkfit {
     EventOfHits *eoh() { return m_eoh; }
     MkBuilder *builder() { return m_builder; }
 
+    const TrackVec& seeds() const { return m_seeds; }
+    const TrackVec& tracks() const { return m_tracks; }
+
     // --------------------------------------------------------
     // Analysis helpers
 
