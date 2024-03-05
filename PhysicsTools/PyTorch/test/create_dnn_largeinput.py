c5ab8b49e888333805e1bbdb7f0d085bc8c9139e
Davide Valsecchi <davide.valsecchi@cern.ch>
2024-03-05 10:36:17 +0100

    added script for alpaka model testing

---
+++ PhysicsTools/PyTorch/test/create_dnn_largeinput.py
@@ -0,0 +1,29 @@
+import sys
+import os
+import torch
+
+# prepare the datadir
+if len(sys.argv) >= 2:
+    datadir = sys.argv[1]
+else:
+    thisdir = os.path.dirname(os.path.abspath(__file__))
+    datadir = os.path.join(os.path.dirname(thisdir), "bin", "data")
+
+os.makedirs(datadir, exist_ok=True)
+
+class MyModuleSum(torch.nn.Module):
+    def __init__(self):
+        super(MyModuleSum, self).__init__()
+        
+    def forward(self, A, B):
+          return A+B
+
+      
+module = MyModuleSum()
+model(torch.ones(1<<16), torch.ones(1<<16))
+
+tm = torch.jit.trace(model.eval(), [torch.ones(1<<16), torch.ones(1<<16)])
+
+tm.save(f"{datadir}/simple_dnn_largeinput.pt")
+
+print("simple_dnn_largeinput.pt created successfully!")
