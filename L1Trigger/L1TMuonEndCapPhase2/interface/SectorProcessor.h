685390350a7ab11bda1080dbf6a17e1814cddb68
Osvaldo Miguel Colin <osvaldomc2009@gmail.com>
2024-03-05 23:37:24 +0100

    Switched to unsigned indexes for iteration, and applied naming conventions for L1TMuonEndcapPhase2

--- L1Trigger/L1TMuonEndCapPhase2/interface/SectorProcessor.h
+++ L1Trigger/L1TMuonEndCapPhase2/interface/SectorProcessor.h
@@ -12,9 +12,9 @@ namespace emtf::phase2 {
 
     ~SectorProcessor();
 
-    void configure_event(const edm::Event&);
+    void configureEvent(const edm::Event&);
 
-    void configure_bx(const int&);
+    void configureBx(const int&);
 
     void select(const TriggerPrimitive&, const TPInfo&);
 
@@ -36,13 +36,13 @@ namespace emtf::phase2 {
     std::map<SubsystemType, ILinkTPCMap> bx_ilink_tpc_maps_;
 
     // Helper functions
-    void copy_tp(const ILinkTPCMap& source, ILinkTPCMap& target) const;
+    void copyTP(const ILinkTPCMap& source, ILinkTPCMap& target) const;
 
-    void convert_tp(const int&, const ILinkTPCMap&, EMTFHitCollection&);
+    void convertTP(const int&, const ILinkTPCMap&, EMTFHitCollection&);
 
-    void populate_segments(const std::vector<EMTFHitCollection>&, std::map<int, int>&, segment_collection_t&);
+    void populateSegments(const std::vector<EMTFHitCollection>&, std::map<int, int>&, segment_collection_t&);
 
-    void build_tracks(const std::map<int, int>&, const segment_collection_t&, const bool&, EMTFTrackCollection&);
+    void buildTracks(const std::map<int, int>&, const segment_collection_t&, const bool&, EMTFTrackCollection&);
   };
 
 }  // namespace emtf::phase2
--- L1Trigger/L1TMuonEndCapPhase2/src/TrackFinder.cc
+++ L1Trigger/L1TMuonEndCapPhase2/src/TrackFinder.cc
@@ -168,7 +168,7 @@ void TrackFinder::process(
 
   // Before event
   for (auto& sector_processor : sector_processors_) {
-    sector_processor->configure_event(i_event);
+    sector_processor->configureEvent(i_event);
   }
 
   // Orderly loop BX
@@ -186,7 +186,7 @@ void TrackFinder::process(
     // Loop over all sector processors
     for (auto& sector_processor : sector_processors_) {
       // Before BX
-      sector_processor->configure_bx(bx);
+      sector_processor->configureBx(bx);
 
       // Select trigger primitives in BX
       if (bx_tpc_ptr != nullptr) {
@@ -212,10 +212,10 @@ void TrackFinder::process(
   bx_tpc_map.clear();
 }
 
-void TrackFinder::on_job_begin() {
+void TrackFinder::onJobBegin() {
   // Do Nothing
 }
 
-void TrackFinder::on_job_end() {
+void TrackFinder::onJobEnd() {
   // Do Nothing
 }
