6336590b5abe04b9aab94a8dd55a4fdad14750d8
Marco Rovere <marco.rovere@cern.ch>
2024-03-07 19:12:56 +0100

    Fix muon paths filters.

--- HLTrigger/Configuration/python/HLT_75e33/paths/HLT_Mu37_Mu27_FromL1TkMuon_cfi.py
+++ HLTrigger/Configuration/python/HLT_75e33/paths/HLT_Mu37_Mu27_FromL1TkMuon_cfi.py
@@ -1,8 +1,6 @@
 import FWCore.ParameterSet.Config as cms
 
 from ..modules.hltDoubleMuon7DZ1p0_cfi import *
-from ..modules.hltL1TkDoubleMuFiltered7_cfi import *
-from ..modules.hltL1TkSingleMuFiltered15_cfi import *
 from ..modules.hltL3fL1DoubleMu155fFiltered37_cfi import *
 from ..modules.hltL3fL1DoubleMu155fPreFiltered27_cfi import *
 from ..modules.hltPhase2L3MuonCandidates_cfi import *
@@ -16,4 +14,4 @@ from ..sequences.itLocalRecoSequence_cfi import *
 from ..sequences.muonlocalrecoSequence_cfi import *
 from ..sequences.otLocalRecoSequence_cfi import *
 
-HLT_Mu37_Mu27_FromL1TkMuon = cms.Path(HLTBeginSequence+hltL1TkDoubleMuFiltered7+hltL1TkSingleMuFiltered15+hltDoubleMuon7DZ1p0+muonlocalrecoSequence+itLocalRecoSequence+otLocalRecoSequence+hltPhase2PixelFitterByHelixProjections+hltPhase2PixelTrackFilterByKinematics+HLTTrackingV61Sequence+HLTMuonsSequence+hltPhase2L3MuonCandidates+hltL3fL1DoubleMu155fPreFiltered27+hltL3fL1DoubleMu155fFiltered37+HLTEndSequence)
+HLT_Mu37_Mu27_FromL1TkMuon = cms.Path(HLTBeginSequence+hltDoubleMuon7DZ1p0+muonlocalrecoSequence+itLocalRecoSequence+otLocalRecoSequence+hltPhase2PixelFitterByHelixProjections+hltPhase2PixelTrackFilterByKinematics+HLTTrackingV61Sequence+HLTMuonsSequence+hltPhase2L3MuonCandidates+hltL3fL1DoubleMu155fPreFiltered27+hltL3fL1DoubleMu155fFiltered37+HLTEndSequence)
--- HLTrigger/Configuration/python/HLT_75e33/paths/HLT_Mu50_FromL1TkMuon_cfi.py
+++ HLTrigger/Configuration/python/HLT_75e33/paths/HLT_Mu50_FromL1TkMuon_cfi.py
@@ -1,6 +1,5 @@
 import FWCore.ParameterSet.Config as cms
 
-from ..modules.hltL1TkSingleMuFiltered22_cfi import *
 from ..modules.hltL3fL1TkSingleMu22L3Filtered50Q_cfi import *
 from ..modules.hltPhase2L3MuonCandidates_cfi import *
 from ..modules.hltPhase2PixelFitterByHelixProjections_cfi import *
@@ -13,4 +12,4 @@ from ..sequences.itLocalRecoSequence_cfi import *
 from ..sequences.muonlocalrecoSequence_cfi import *
 from ..sequences.otLocalRecoSequence_cfi import *
 
-HLT_Mu50_FromL1TkMuon = cms.Path(HLTBeginSequence+hltL1TkSingleMuFiltered22+muonlocalrecoSequence+itLocalRecoSequence+otLocalRecoSequence+hltPhase2PixelFitterByHelixProjections+hltPhase2PixelTrackFilterByKinematics+HLTTrackingV61Sequence+HLTMuonsSequence+hltPhase2L3MuonCandidates+hltL3fL1TkSingleMu22L3Filtered50Q+HLTEndSequence)
+HLT_Mu50_FromL1TkMuon = cms.Path(HLTBeginSequence+muonlocalrecoSequence+itLocalRecoSequence+otLocalRecoSequence+hltPhase2PixelFitterByHelixProjections+hltPhase2PixelTrackFilterByKinematics+HLTTrackingV61Sequence+HLTMuonsSequence+hltPhase2L3MuonCandidates+hltL3fL1TkSingleMu22L3Filtered50Q+HLTEndSequence)
