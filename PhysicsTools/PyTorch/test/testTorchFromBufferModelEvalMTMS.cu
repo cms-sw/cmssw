5542a9aabdcf2266f5a779f45b0bbd26f0787607
Eric Cano <eric.cano@cern.ch>
2024-03-08 15:51:43 +0100

    Debuging multistreaming of pyTorch: moving to one model instance per thread.
    Works around a mishandling or default device id in scteam setting

--- PhysicsTools/PyTorch/test/testTorchFromBufferModelEvalMTMS.cu
+++ PhysicsTools/PyTorch/test/testTorchFromBufferModelEvalMTMS.cu
@@ -82,13 +82,13 @@ void vector_add(int* a, int* b, int* c, int N, int cuda_grid_size, int cuda_bloc
 // bool ENABLE_ERROR = true;
 // We take the model as non consr as the forward function is a non const one.
 void testTorchFromBufferModelEvalSinglePass(torch::jit::script::Module& model, const int * a_cpu,  const int *b_cpu, int * c_cpu, 
-        const int N, const size_t bytes) {
+        const int N, const size_t bytes, const size_t thread, const size_t iteration) {
   // Declare GPU memory pointers
   int *a_gpu, *b_gpu, *c_gpu;
 
   NVTXScopedRange allocRange("GPU memory allocation");
   // Allocate memory on the device
-  cout << "Allocating memory for vectors on GPU" << endl;
+  cout << "T" << thread << " I" << iteration << " Allocating memory for vectors on GPU" << endl;
   cudaMallocAsync(&a_gpu, bytes, c10::cuda::getCurrentCUDAStream().stream());
   cudaMallocAsync(&b_gpu, bytes, c10::cuda::getCurrentCUDAStream().stream());
   cudaMallocAsync(&c_gpu, bytes, c10::cuda::getCurrentCUDAStream().stream());
@@ -97,7 +97,7 @@ void testTorchFromBufferModelEvalSinglePass(torch::jit::script::Module& model, c
   
   NVTXScopedRange memcpyRange("Memcpy host to dev");
   // Copy data from the host to the device (CPU -> GPU)
-  cout << "Transfering vectors from CPU to GPU" << endl;
+  cout << "T" << thread << " I" << iteration << " Transfering vectors from CPU to GPU" << endl;
   cudaMemcpyAsync(a_gpu, a_cpu, bytes, cudaMemcpyHostToDevice, c10::cuda::getCurrentCUDAStream().stream());
   cudaMemcpyAsync(b_gpu, b_cpu, bytes, cudaMemcpyHostToDevice, c10::cuda::getCurrentCUDAStream().stream());
   memcpyRange.end();
@@ -116,11 +116,11 @@ void testTorchFromBufferModelEvalSinglePass(torch::jit::script::Module& model, c
   try {
     // Convert pinned memory on GPU to Torch tensor on GPU
     auto options = torch::TensorOptions().dtype(torch::kInt).device(torch::kCUDA, 0).pinned_memory(true);
-    cout << "Converting vectors and result to Torch tensors on GPU" << endl;
+    cout << "T" << thread << " I" << iteration << " Converting vectors and result to Torch tensors on GPU" << endl;
     torch::Tensor a_gpu_tensor = torch::from_blob(a_gpu, {N}, options);
     torch::Tensor b_gpu_tensor = torch::from_blob(b_gpu, {N}, options);
 
-    cout << "Running torch inference" << endl;
+    cout << "T" << thread << " I" << iteration << " Running torch inference" << endl;
     std::vector<torch::jit::IValue> inputs{a_gpu_tensor, b_gpu_tensor};
     // Not fully understood but std::move() is needed
     // https://stackoverflow.com/questions/71790378/assign-memory-blob-to-py-torch-output-tensor-c-api 
@@ -140,16 +140,17 @@ void testTorchFromBufferModelEvalSinglePass(torch::jit::script::Module& model, c
 
   NVTXScopedRange memcpyBackRange("Memcpy dev to host");
   // Copy memory from device and also synchronize (implicitly)
-  cout << "Synchronizing CPU and GPU. Copying result from GPU to CPU" << endl;
+  cout << "T" << thread << " I" << iteration << " Synchronizing CPU and GPU. Copying result from GPU to CPU" << endl;
   cudaMemcpyAsync(c_cpu, c_gpu, bytes, cudaMemcpyDeviceToHost, c10::cuda::getCurrentCUDAStream().stream());
   memcpyBackRange.end();
   
   if constexpr (doValidation) {
     NVTXScopedRange validationRange("Validation");
     // Verify the result on the CPU
-    cout << "Verifying result on CPU" << endl;
+    cudaStreamSynchronize(c10::cuda::getCurrentCUDAStream().stream());
+    cout << "T" << thread << " I" << iteration << " Verifying result on CPU" << endl;
     for (int i = 0; i < N; ++i) {
-      CPPUNIT_ASSERT(c_cpu[i] == a_cpu[i] + b_cpu[i]);
+      CPPUNIT_ASSERT_MESSAGE("ERROR: Mismatch in verification", c_cpu[i] == a_cpu[i] + b_cpu[i]);
     }
     validationRange.end();
   }
@@ -167,17 +168,20 @@ void testTorchFromBufferModelEval::test() {
     // Load the TorchScript model
   std::string model_path = dataPath_ + "/simple_dnn_largeinput.pt";
 
-  cout << "Loading model..." << endl;
-  torch::jit::script::Module model;
-  torch::Device device(torch::kCUDA);
-  try {
-    // Deserialize the ScriptModule from a file using torch::jit::load().
-    model = torch::jit::load(model_path);
-    model.to(device);
-
-  } catch (const c10::Error& e) {
-    std::cerr << "error loading the model\n" << e.what() << std::endl;
-  }
+//  cout << "Loading model..." << endl;
+//  torch::jit::script::Module model;
+//  // We need to set the device index to 0 (or a valid value) as leaving it to default (-1) leads to a 
+//  // bug when setting the cuda stream (-1 is used as an array index without resolving back to 
+//  // real index (probably).
+//  torch::Device device(torch::kCUDA, 0);
+//  try {
+//    // Deserialize the ScriptModule from a file using torch::jit::load().
+//    model = torch::jit::load(model_path);
+//    model.to(device);
+//
+//  } catch (const c10::Error& e) {
+//    std::cerr << "error loading the model\n" << e.what() << std::endl;
+//  }
   
   // Setup array, here 2^16 = 65536 items
   const int N = 1 << 20;
@@ -201,9 +205,18 @@ void testTorchFromBufferModelEval::test() {
     b_cpu[i] = rand() % 100;
   }
   
+  size_t threadCount = 10;
+//  std::vector<torch::jit::script::Module> perThreadModels;
+//  for (size_t t=0, t<threadCount; ++t) {
+//    
+//  }
   
+  // We need to set the device index to 0 (or a valid value) as leaving it to default (-1) leads to a 
+  // bug when setting the cuda stream (-1 is used as an array index without resolving back to 
+  // real index (probably).
+  torch::Device device(torch::kCUDA, 0);
   std::vector<std::thread> threads;
-  for (size_t t=0; t<1; ++t) {
+  for (size_t t=0; t<threadCount; ++t) {
     threads.emplace_back([&, t]{
       char threadName[15];
       snprintf(threadName, 15, "test::%ld", t);
@@ -212,15 +225,29 @@ void testTorchFromBufferModelEval::test() {
       cout << "Thread " << t << ": allocating CUDA stream and result buffer" << endl;
       cudaStream_t cudaStream;
       cudaStreamCreate(&cudaStream);
-      auto torchStream = c10::cuda::getStreamFromExternal(cudaStream, device.index());
+      c10::cuda::CUDAStream torchStream = c10::cuda::getStreamFromExternal(cudaStream, device.index());
       c10::cuda::setCurrentCUDAStream(torchStream);
+      
+      cout << "Thread " << t << ": loading model..." << endl;
+      torch::jit::script::Module model;
+
+      try {
+        // Deserialize the ScriptModule from a file using torch::jit::load().
+        model = torch::jit::load(model_path);
+        model.to(device, true /* async */);
+
+      } catch (const c10::Error& e) {
+        std::cerr << "error loading the model\n" << e.what() << std::endl;
+      }
+      
       int * c_cpu;
       cudaMallocHost(&c_cpu, bytes);
       // Get a pyTorch style cuda stream, device is captured from above.
       for (size_t i=0; i<10; ++i)
-        testTorchFromBufferModelEvalSinglePass(model, a_cpu, b_cpu, c_cpu, N, bytes);
+        testTorchFromBufferModelEvalSinglePass(model, a_cpu, b_cpu, c_cpu, N, bytes, t, i);
+      cudaStreamSynchronize(cudaStream);
       cout << "Thread " << t << " Test loop complete." << endl;
-      cudaFreeHost(c_cpu);
+      //cudaFreeHost(c_cpu);
       cout << "Thread " << t << " Result buffer freeed." << endl;
       c10::cuda::setCurrentCUDAStream(c10::cuda::getDefaultCUDAStream());
       cout << "Thread " << t << " Stream reset." << endl;
@@ -228,7 +255,7 @@ void testTorchFromBufferModelEval::test() {
   }
   for (auto &t: threads) t.join();
   cout << "Threads done." << endl;
-  cudaFreeHost(a_cpu);
-  cudaFreeHost(b_cpu);
+//  cudaFreeHost(a_cpu);
+//  cudaFreeHost(b_cpu);
   // Fixme: free mempory in case of exceptions...
 }
