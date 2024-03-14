d5d7ed2f1c116ca41bf2aac26ea42fd327549f1f
Emyr Clement <emyr.clement@bristol.ac.uk>
2024-03-14 12:48:10 +0000

    Produce and use custom HGCal towers for baseline GCT calo jets and taus.

--- L1Trigger/Configuration/python/SimL1Emulator_cff.py
+++ L1Trigger/Configuration/python/SimL1Emulator_cff.py
@@ -121,12 +121,12 @@ from L1Trigger.L1CaloTrigger.l1tNNCaloTauEmulator_cfi import *
 _phase2_siml1emulator.add(l1tNNCaloTauEmulator)
 
 # ---- Produce the emulated CaloJets and Taus
-from L1Trigger.L1CaloTrigger.l1tPhase2CaloJetEmulator_cfi import *
+from L1Trigger.L1CaloTrigger.l1tPhase2CaloJetEmulator_cff import *
 
 _phase2_siml1emulator.add(l1tTowerCalibration)
 _phase2_siml1emulator.add(l1tCaloJet)
 _phase2_siml1emulator.add(l1tCaloJetHTT)
-_phase2_siml1emulator.add(l1tPhase2CaloJetEmulator)
+_phase2_siml1emulator.add(L1TCaloJetsTausTask)
 
 # Overlap and EndCap Muon Track Finder
 # ########################################################################
---
+++ L1Trigger/L1CaloTrigger/python/l1tPhase2CaloJetEmulator_cff.py
@@ -0,0 +1,21 @@
+from L1Trigger.L1CaloTrigger.l1tPhase2CaloJetEmulator_cfi import *
+
+from L1Trigger.L1THGCal.l1tHGCalTowerMapProducer_cfi import *
+from L1Trigger.L1THGCal.l1tHGCalTowerMapProducer_cfi import L1TTriggerTowerConfig_energySplit
+from L1Trigger.L1THGCal.l1tHGCalTowerProducer_cfi import *
+
+# Add HGCal tower producers for energy split towers
+# Based on custom_towers_energySplit in L1Trigger/L1THGCal/python/customTowers.py
+parameters_towers_2d = L1TTriggerTowerConfig_energySplit.clone()
+l1tHGCalEnergySplitTowerMapProducer = l1tHGCalTowerMapProducer.clone()
+l1tHGCalEnergySplitTowerMapProducer.ProcessorParameters.towermap_parameters.L1TTriggerTowerConfig = parameters_towers_2d
+l1tHGCalEnergySplitTowerProducer = l1tHGCalTowerProducer.clone( InputTowerMaps = ("l1tHGCalEnergySplitTowerMapProducer","HGCalTowerMapProcessor") )
+L1THGCalEnergySplitTowersTask = cms.Task(l1tHGCalEnergySplitTowerMapProducer, l1tHGCalEnergySplitTowerProducer)
+
+# Use energy split towers in calo jet/tau emulator
+l1tPhase2CaloJetEmulator.hgcalTowers.hgcalTowers = ("l1tHGCalEnergySplitTowerProducer","HGCalTowerProcessor")
+
+L1TCaloJetsTausTask = cms.Task(
+    L1THGCalEnergySplitTowersTask,
+    l1tPhase2CaloJetEmulator
+)
