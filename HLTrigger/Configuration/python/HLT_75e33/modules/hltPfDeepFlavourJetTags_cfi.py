54150776327886f5670aa3c9d166b98658a084b1
hhsia <hsin-wei.hsia@cern.ch>
2024-03-15 14:13:35 -0500

    modified for Phase2 DeepJet retraining model

--- HLTrigger/Configuration/python/HLT_75e33/modules/hltPfDeepFlavourJetTags_cfi.py
+++ HLTrigger/Configuration/python/HLT_75e33/modules/hltPfDeepFlavourJetTags_cfi.py
@@ -17,7 +17,7 @@ hltPfDeepFlavourJetTags = cms.EDProducer("DeepFlavourONNXJetTagsProducer",
         'input_5'
     ),
     mightGet = cms.optional.untracked.vstring,
-    model_path = cms.FileInPath('RecoBTag/Combined/data/DeepFlavourV01_PhaseII/model.onnx'),
+    model_path = cms.FileInPath('RecoBTag/Combined/data/DDeepFlavourV02_PhaseII/DeepJet_retraining_phase2_new_inputs.onnx'),
     output_names = cms.vstring(),
     src = cms.InputTag("hltPfDeepFlavourTagInfos")
 )
