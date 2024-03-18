24386319ee8b90fabb5080a14c86f089b9c89720
lathomasexport <laurent.thomas@cern.ch>
2024-03-18 15:08:40 +0100

    Protection against cases where reemulation is called twice in the process

--- DPGAnalysis/L1TNanoAOD/python/l1tNano_cff.py
+++ DPGAnalysis/L1TNanoAOD/python/l1tNano_cff.py
@@ -14,52 +14,52 @@ l1tNanoTask = cms.Task(nanoMetadata,l1TablesTask)
 l1tNanoSequence = cms.Sequence(l1tNanoTask)
 
 def addEmulObjects(process):
-    
+
     process.l1tNanoTask.add(l1EmulObjTablesTask)
     
     return process
 
 
 def addUnpackedCaloTPs(process):
-    
-    process.l1tNanoTask.add(process.l1CaloTPsNanoTask)
 
+    process.l1tNanoTask.add(process.l1CaloTPsNanoTask)
+    
     return process
 
 def addEmulCaloTPs(process):
-    
+
     process.l1tNanoTask.add(process.l1EmulCaloTPsNanoTask)
 
     return process
 
 def addUnpackedCaloLayer1(process):
-    
+
     process.l1tNanoTask.add(process.l1CaloLayer1NanoTask)
 
     return process
 
 def addEmulCaloLayer1(process):
-    
-    process.l1tNanoTask.add(process.l1EmulCaloLayer1NanoTask)
 
+    process.l1tNanoTask.add(process.l1EmulCaloLayer1NanoTask)
+         
     return process
 
 def addUnpackedCaloTPsandLayer1(process):
-    
+
     addUnpackedCaloTPs(process)
     addUnpackedCaloLayer1(process)
 
     return process
 
 def addEmulCaloTPsandLayer1(process):
-    
+
     addEmulCaloTPs(process)
     addEmulCaloLayer1(process)
 
     return process
 
 def addCaloFull(process):
-    
+
     addEmulCaloTPsandLayer1(process)
     addUnpackedCaloTPsandLayer1(process)
     addEmulObjects(process)
