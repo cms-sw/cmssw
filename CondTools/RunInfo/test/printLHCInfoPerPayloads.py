411d90532c39ed8a1c3f217e2161e15c24ac9381
Jan Chyczynski <jasiek.ch16@gmail.com>
2024-03-21 20:30:32 +0100

    renaming/moving files, printLHCInfoPerPayloads argument validation

--- CondTools/RunInfo/test/printLHCInfoPerPayloads.py
+++ CondTools/RunInfo/test/printLHCInfoPerPayloads.py
@@ -10,12 +10,14 @@ process = cms.Process('dumpLHCInfoPerPyalods')
 
 TEST_DIR = os.environ['CMSSW_BASE'] + "/src/CondTools/RunInfo/test"
 
+
 options = VarParsing.VarParsing()
+supported_records = {"LHCInfoPerLS", "LHCInfoPerFill"}
 options.register( 'record'
-                , '' #default value
+                , None #default value is None because this argument is required
                 , VarParsing.VarParsing.multiplicity.singleton
                 , VarParsing.VarParsing.varType.string
-                , "the class to analyze, accepted values LHCInfoPerLS, LHCInfoPerFill"
+                , f"the class to analyze, accepted values: {supported_records}"
                   )
 options.register( 'db'
                 , 'frontier://FrontierProd/CMS_CONDITIONS' #default value
@@ -24,7 +26,7 @@ options.register( 'db'
                 , "Connection string to the DB where payloads are going to be read from"
                   )
 options.register( 'tag'
-                , 'LHCInfo_PopCon_test'
+                , None #default value is None because this argument is required
                 , VarParsing.VarParsing.multiplicity.singleton
                 , VarParsing.VarParsing.varType.string
                 , "Tag to read from in source"
@@ -49,12 +51,20 @@ options.register( 'separator'
                   )
 options.parseArguments()
 
+if options.record is None:
+  print(f"Please specify the record name argument (accepted values: {supported_records})", file=sys.stderr)
+  exit(1)
+if options.record not in supported_records:
+  print(f"Provided record name '{options.record}' is not supported (accepted values: {supported_records})", file=sys.stderr)
+  exit(1)
 
+if options.tag is None:
+  print(f"Please specify the tag by adding to the command: tag=<tag to be printed>", file=sys.stderr)
+  exit(1)
 
 for line in map(str.rstrip, sys.stdin):
     for iov in line.split():
         if options.record == "LHCInfoPerLS":
-            # print(iov, end = ";")
             os.system(f"cmsRun {TEST_DIR}/LHCInfoPerLSAnalyzer_cfg.py tag={options.tag} \
             db={options.db} csv={options.csv} header={options.header} separator={options.separator} timestamp={iov}")
         elif options.record == "LHCInfoPerFill":
