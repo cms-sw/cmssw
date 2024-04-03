6eedba8b6ed1ce3488182b8a2539b1eb53227cbc
Marco Rovere <marco.rovere@cern.ch>
2024-04-03 21:00:17 +0200

    Move Muon Paths to use sequence in HLT Phase2 Menu.

---
+++ HLTrigger/Configuration/python/HLT_75e33/sequences/HLTPhase2L3MuonGeneralTracksSequence_cfi.py
@@ -0,0 +1,52 @@
+import FWCore.ParameterSet.Config as cms
+
+from ..modules.trackerClusterCheck_cfi import *
+from ..modules.hltPhase2L3MuonPixelTracksTrackingRegions_cfi import *
+from ..modules.hltPhase2L3MuonPixelTracksSeedLayers_cfi import *
+from ..modules.hltPhase2L3MuonPixelTracksHitDoublets_cfi import *
+from ..modules.hltPhase2L3MuonPixelTracksHitQuadruplets_cfi import *
+from ..modules.hltPhase2L3MuonPixelTracks_cfi import *
+from ..modules.hltPhase2L3MuonPixelVertices_cfi import *
+from ..modules.hltPhase2L3MuonInitialStepSeeds_cfi import *
+from ..modules.hltPhase2L3MuonInitialStepTrackCandidates_cfi import *
+from ..modules.hltPhase2L3MuonInitialStepTracks_cfi import *
+from ..modules.hltPhase2L3MuonInitialStepTrackCutClassifier_cfi import *
+from ..modules.hltPhase2L3MuonInitialStepTracksSelectionHighPurity_cfi import *
+from ..modules.hltPhase2L3MuonHighPtTripletStepClusters_cfi import *
+from ..modules.hltPhase2L3MuonHighPtTripletStepTrackingRegions_cfi import *
+from ..modules.hltPhase2L3MuonHighPtTripletStepSeedLayers_cfi import *
+from ..modules.hltPhase2L3MuonHighPtTripletStepHitDoublets_cfi import *
+from ..modules.hltPhase2L3MuonHighPtTripletStepHitTriplets_cfi import *
+from ..modules.hltPhase2L3MuonHighPtTripletStepSeeds_cfi import *
+from ..modules.hltPhase2L3MuonHighPtTripletStepTrackCandidates_cfi import *
+from ..modules.hltPhase2L3MuonHighPtTripletStepTracks_cfi import *
+from ..modules.hltPhase2L3MuonHighPtTripletStepTracksSelectionHighPurity_cfi import *
+from ..modules.hltPhase2L3MuonHighPtTripletStepTrackCutClassifier_cfi import *
+from ..modules.hltPhase2L3MuonGeneralTracks_cfi import *
+
+
+HLTPhase2L3MuonGeneralTracksSequence = cms.Sequence(
+    trackerClusterCheck
+    +hltPhase2L3MuonPixelTracksTrackingRegions
+    +hltPhase2L3MuonPixelTracksSeedLayers
+    +hltPhase2L3MuonPixelTracksHitDoublets
+    +hltPhase2L3MuonPixelTracksHitQuadruplets
+    +hltPhase2L3MuonPixelTracks
+    +hltPhase2L3MuonPixelVertices
+    +hltPhase2L3MuonInitialStepSeeds
+    +hltPhase2L3MuonInitialStepTrackCandidates
+    +hltPhase2L3MuonInitialStepTracks
+    +hltPhase2L3MuonInitialStepTrackCutClassifier
+    +hltPhase2L3MuonInitialStepTracksSelectionHighPurity
+    +hltPhase2L3MuonHighPtTripletStepClusters
+    +hltPhase2L3MuonHighPtTripletStepTrackingRegions
+    +hltPhase2L3MuonHighPtTripletStepSeedLayers
+    +hltPhase2L3MuonHighPtTripletStepHitDoublets
+    +hltPhase2L3MuonHighPtTripletStepHitTriplets
+    +hltPhase2L3MuonHighPtTripletStepSeeds
+    +hltPhase2L3MuonHighPtTripletStepTrackCandidates
+    +hltPhase2L3MuonHighPtTripletStepTracks
+    +hltPhase2L3MuonHighPtTripletStepTrackCutClassifier
+    +hltPhase2L3MuonHighPtTripletStepTracksSelectionHighPurity
+    +hltPhase2L3MuonGeneralTracks
+    )
