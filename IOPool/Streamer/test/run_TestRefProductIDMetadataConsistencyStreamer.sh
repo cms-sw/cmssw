c21309dee1267af8902a97e7ea4ddca2fd15ce9d
Matti Kortelainen <matti.kortelainen@cern.ch>
2024-04-08 23:54:43 +0200

    Add tests for ProductID metadata consistency for ROOT and streamer formats

---
+++ IOPool/Streamer/test/run_TestRefProductIDMetadataConsistencyStreamer.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+function die { echo Failure $1: status $2 ; exit $2 ; }
+function runSuccess {
+    echo "cmsRun $@"
+    cmsRun $@ || die "cmsRun $*" $?
+    echo
+}
+function runFailure {
+    echo "cmsRun $@ (exepcted to fail)"
+    cmsRun $@ && die "cmsRun $*" 1
+    echo
+}
+
+# Produce two files with different ProductID metadata
+runSuccess ${SCRAM_TEST_PATH}/testRefProductIDMetadataConsistencyStreamer_cfg.py
+runSuccess ${SCRAM_TEST_PATH}/testRefProductIDMetadataConsistencyStreamer_cfg.py --enableOther
+
+# Processing the two files together works
+runSuccess ${SCRAM_TEST_PATH}/testRefProductIDMetadataConsistencyStreamerTest_cfg.py --input refconsistency_1.dat --input refconsistency_10.dat
+
+# Concatenating the two files by keeping the Init message of only first file ...
+echo "Concatenating streamer files"
+CatStreamerFiles refconsistency_cat.dat refconsistency_1.dat refconsistency_10.dat
+echo
+
+# ... fails
+runFailure ${SCRAM_TEST_PATH}/testModuleTypeResolverRefTest_cfg.py --input moduletyperesolver_ref_cat.dat
