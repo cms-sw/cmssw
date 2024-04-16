989900e5eb2a5260f308b0b1818139704e5888ae
Ivan Razumov <iarspider@gmail.com>
2024-04-16 12:45:06 +0200

    Dummy commit

--- Alignment/CommonAlignmentProducer/python/ALCARECOTkAlDiMuonAndVertex_Output_cff.py
+++ Alignment/CommonAlignmentProducer/python/ALCARECOTkAlDiMuonAndVertex_Output_cff.py
@@ -36,3 +36,4 @@ phase2_common.toModify(OutALCARECOTkAlDiMuonAndVertex_noDrop, outputCommands = _
 
 OutALCARECOTkAlDiMuonAndVertex = OutALCARECOTkAlDiMuonAndVertex_noDrop.clone()
 OutALCARECOTkAlDiMuonAndVertex.outputCommands.insert(0, "drop *")
+# dummy dummy
