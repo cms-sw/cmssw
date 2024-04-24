32f057c123535f8b27752131c9e76c6e2f41372a
Jakub Gajownik <jakub.gajownik@cern.ch>
2024-04-24 15:44:09 +0200

    Reco running only on CPU bug

--- RecoLocalCalo/EcalRecProducers/python/ecalUncalibRecHitPhase2_cff.py
+++ RecoLocalCalo/EcalRecProducers/python/ecalUncalibRecHitPhase2_cff.py
@@ -1,17 +1,37 @@
 import FWCore.ParameterSet.Config as cms
+from HeterogeneousCore.CUDACore.SwitchProducerCUDA import SwitchProducerCUDA
+
+# ECAL Phase 2 weights running on CPU
+from RecoLocalCalo.EcalRecProducers.ecalUncalibRecHitPhase2_cfi import ecalUncalibRecHitPhase2 as _ecalUncalibRecHitPhase2
+ecalUncalibRecHitPhase2CPU = _ecalUncalibRecHitPhase2.clone() 
+ecalUncalibRecHitPhase2 = SwitchProducerCUDA(
+  cpu = ecalUncalibRecHitPhase2CPU
+)
+
+ecalUncalibRecHitPhase2Task = cms.Task(
+        # ECAL weights running on CPU
+        ecalUncalibRecHitPhase2
+)
+
+
 from Configuration.StandardSequences.Accelerators_cff import *
 
+# process modifier to run alpaka implementation
+from Configuration.ProcessModifiers.alpaka_cff import alpaka
+
+#ECAL Phase 2 Digis Producer running on the accelerator
 from RecoLocalCalo.EcalRecProducers.ecalPhase2DigiToPortableProducer_cfi import ecalPhase2DigiToPortableProducer as _ecalPhase2DigiToPortableProducer
 ecalPhase2DigiToPortableProducer = _ecalPhase2DigiToPortableProducer.clone()
 
-# portable weights
+# ECAL Phase 2 weights portable running
 from RecoLocalCalo.EcalRecProducers.ecalUncalibRecHitPhase2Portable_cfi import ecalUncalibRecHitPhase2Portable as _ecalUncalibRecHitPhase2Portable
 ecalUncalibRecHitPhase2Portable = _ecalUncalibRecHitPhase2Portable.clone(
         digisLabelEB = 'ecalPhase2DigiToPortableProducer:ebDigis'
 )
 
 from RecoLocalCalo.EcalRecProducers.ecalUncalibRecHitSoAToLegacy_cfi import ecalUncalibRecHitSoAToLegacy as _ecalUncalibRecHitSoAToLegacy
-ecalUncalibRecHitPhase2 = _ecalUncalibRecHitSoAToLegacy.clone(
+alpaka.toModify(ecalUncalibRecHitPhase2,
+    cpu = _ecalUncalibRecHitSoAToLegacy.clone(),
     isPhase2 = True,
     uncalibRecHitsPortableEB = 'ecalUncalibRecHitPhase2Portable:EcalUncalibRecHitsEB',
     uncalibRecHitsPortableEE = None,
@@ -19,11 +39,11 @@ ecalUncalibRecHitPhase2 = _ecalUncalibRecHitSoAToLegacy.clone(
 )
 
 
-ecalUncalibRecHitPhase2Task = cms.Task(
+alpaka.toReplaceWith(ecalUncalibRecHitPhase2Task, cms.Task(
   # convert phase2 digis to Portable Collection
   ecalPhase2DigiToPortableProducer, 
   # ECAL weights running on Portable
   ecalUncalibRecHitPhase2Portable,
   # Convert the uncalibrated rechits from Portable Collection to legacy format
   ecalUncalibRecHitPhase2
-)
+))
