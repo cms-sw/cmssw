8e07918f40de3332f3e277e69f7267ae2c857da9
Andrea Bocci <andrea.bocci@cern.ch>
2024-04-29 23:37:16 +0200

    Reorder includes

--- RecoParticleFlow/PFRecHitProducer/plugins/alpaka/CalorimeterDefinitions.h
+++ RecoParticleFlow/PFRecHitProducer/plugins/alpaka/CalorimeterDefinitions.h
@@ -13,10 +13,11 @@
 #include "RecoParticleFlow/PFRecHitProducer/interface/alpaka/PFRecHitParamsDeviceCollection.h"
 #include "RecoParticleFlow/PFRecHitProducer/interface/alpaka/PFRecHitTopologyDeviceCollection.h"
 
-class PFRecHitHCALTopologyRecord;
-class PFRecHitECALTopologyRecord;
+// Forward declaration of EventSetup records, to avoid propagating the dependency on framework headers to device code
 class PFRecHitHCALParamsRecord;
+class PFRecHitHCALTopologyRecord;
 class EcalPFRecHitThresholdsRcd;
+class PFRecHitECALTopologyRecord;
 
 // This file defines two structs:
 // 1) ALPAKA_ACCELERATOR_NAMESPACE::particleFlowRecHitProducer::HCAL
