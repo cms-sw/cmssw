a165757edd36f12f5167dac3ace282124d4eddd8
Fabio Cossutti <fabio.cossutti@ts.infn.it>
2024-04-29 15:22:37 +0200

    Add Mtd association maps also for PREMIX

--- SimFastTiming/Configuration/python/SimFastTiming_EventContent_cff.py
+++ SimFastTiming/Configuration/python/SimFastTiming_EventContent_cff.py
@@ -27,7 +27,7 @@ _phase2_timing_extraCommands = cms.PSet( # using PSet in order to customize with
 # Unsure what to do with InitialVertices, they don't seem to be consumed downstream?
 from Configuration.ProcessModifiers.premix_stage2_cff import premix_stage2
 premix_stage2.toModify(_phase2_timing_extraCommands,
-    value = [ 'keep *_mixData_FTLBarrel_*','keep *_mixData_FTLEndcap_*','keep *_mix_InitialVertices_*' ]
+    value = [ 'keep *_mixData_FTLBarrel_*','keep *_mixData_FTLEndcap_*','keep *_mix_InitialVertices_*', 'keep *_mtdRecoClusterToSimLayerClusterAssociation_*_*', 'keep *_mtdSimLayerClusterToTPAssociation_*_*' ]
 )
 from Configuration.Eras.Modifier_phase2_timing_cff import phase2_timing
 phase2_timing.toModify( SimFastTimingRAW, outputCommands = SimFastTimingRAW.outputCommands + _phase2_timing_extraCommands.value )
