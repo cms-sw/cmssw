6ee9a72503a707d2a35a5f77b6f57c85cf91ec20
Ryan McCarthy <ryan.edward.mccarthy@cern.ch>
2024-05-13 14:49:15 +0200

    Reverting changes to prompt track quality BDT, adding fix to MVA score, and updating displaced vertex BDT

--- L1Trigger/TrackTrigger/python/TrackQualityParams_cfi.py
+++ L1Trigger/TrackTrigger/python/TrackQualityParams_cfi.py
@@ -4,7 +4,7 @@ TrackQualityParams = cms.PSet(# This emulation GBDT is optimised for the HYBRID_
                               # It is compatible with the HYBRID simulation and will give equivilant performance with this workflow
                               model = cms.FileInPath("L1Trigger/TrackTrigger/data/L1_TrackQuality_GBDT_emulation_digitized.json"),
                               #Vector of strings of training features, in the order that the model was trained with
-                              featureNames = cms.vstring(["phi", "eta", "z0", "bendchi2_bin", "nstub", 
+                              featureNames = cms.vstring(["tanl", "z0_scaled", "bendchi2_bin", "nstub",
                                                           "nlaymiss_interior", "chi2rphi_bin", "chi2rz_bin"]),
                               tqemu_TanlScale = cms.double( 128.0),
                               tqemu_Z0Scale = cms.double( 64.0 ),
