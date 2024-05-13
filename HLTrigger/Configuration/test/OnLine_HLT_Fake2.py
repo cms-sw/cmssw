24bd4ad2c8d52c9c9bd710be6a377cfc88429d9d
mmusich <marco.musich@cern.ch>
2024-05-13 09:00:15 +0200

    HLT menu development for 13_0_X
    This version of the GRun Frozen HLT menu corresponds to an update to the "V1.2" menu for the 2023 pp data-taking period.
    This update includes the integration of the following JIRA tickets:
    - [CMSHLT-3192](https://its.cern.ch/jira/browse/CMSHLT-3192) : Add HLT_QuadPFJet70_50_40_35_PNet2BTagMean0p65 to 2023 MC campaign

--- HLTrigger/Configuration/test/OnLine_HLT_Fake2.py
+++ HLTrigger/Configuration/test/OnLine_HLT_Fake2.py
@@ -6,9 +6,8 @@ import FWCore.ParameterSet.Config as cms
 
 process = cms.Process( "HLTFake2" )
 
-
 process.HLTConfigVersion = cms.PSet(
-  tableName = cms.string('/dev/CMSSW_13_0_0/Fake2/V9')
+  tableName = cms.string("/dev/CMSSW_13_0_0/Fake2/V9")
 )
 
 process.streams = cms.PSet(  A = cms.vstring( 'InitialPD' ) )
