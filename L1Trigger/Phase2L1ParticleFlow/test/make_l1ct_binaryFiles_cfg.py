28b00418eb208c2987e42ebbe45a3cdd25b9cef7
Giovanni <gpetruc@gmail.com>
2024-05-13 12:23:53 +0200

    Drop on input old l1tKMTFTracks

--- L1Trigger/Phase2L1ParticleFlow/test/make_l1ct_binaryFiles_cfg.py
+++ L1Trigger/Phase2L1ParticleFlow/test/make_l1ct_binaryFiles_cfg.py
@@ -38,7 +38,8 @@ process.source = cms.Source("PoolSource",
             "drop l1tPFClusters_*_*_*",
             "drop l1tPFTracks_*_*_*",
             "drop l1tPFCandidates_*_*_*",
-            "drop l1tTkPrimaryVertexs_*_*_*"),
+            "drop l1tTkPrimaryVertexs_*_*_*",
+            "drop l1tKMTFTracks_*_*_*"),
     skipEvents = cms.untracked.uint32(0),
 )
 
