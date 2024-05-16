3e6604d7c0c556ffab498f224d09839aefcd4905
mmusich <marco.musich@cern.ch>
2024-05-16 17:13:55 +0200

    use 3D histograms to derive 2D maps of bias

--- Alignment/OfflineValidation/macros/DiMuonMassProfiles.C
+++ Alignment/OfflineValidation/macros/DiMuonMassProfiles.C
@@ -14,6 +14,7 @@
 #include "TGaxis.h"
 #include "TH1.h"
 #include "TH2.h"
+#include "TH3.h"
 #include "TKey.h"
 #include "TLegend.h"
 #include "TObjString.h"
@@ -47,6 +48,21 @@ Int_t def_markers[9] = {kFullSquare,
 Int_t def_colors[9] = {kBlack, kRed, kBlue, kMagenta, kGreen, kCyan, kViolet, kOrange, kGreen + 2};
 
 /*--------------------------------------------------------------------*/
+std::pair<int, int> getClosestFactors(int input)
+/*--------------------------------------------------------------------*/
+{
+  if ((input % 2 != 0) && input > 1) {
+    input += 1;
+  }
+
+  int testNum = (int)sqrt(input);
+  while (input % testNum != 0) {
+    testNum--;
+  }
+  return std::make_pair(testNum, input / testNum);
+}
+
+/*--------------------------------------------------------------------*/
 template <typename T>
 void MakeNicePlotStyle(T* hist)
 /*--------------------------------------------------------------------*/
@@ -60,8 +76,18 @@ void MakeNicePlotStyle(T* hist)
   hist->GetYaxis()->SetTitleFont(42);
   hist->GetXaxis()->SetTitleSize(0.05);
   hist->GetYaxis()->SetTitleSize(0.05);
-  hist->GetXaxis()->SetTitleOffset(0.9);
-  hist->GetYaxis()->SetTitleOffset(1.7);
+  if (((TObject*)hist)->IsA()->InheritsFrom("TH2")) {
+    hist->GetZaxis()->CenterTitle(true);
+    hist->GetZaxis()->SetTitleSize(0.04);
+    hist->GetZaxis()->SetLabelFont(42);
+    hist->GetYaxis()->SetLabelSize(.05);
+    hist->GetXaxis()->SetTitleOffset(1.0);
+    hist->GetYaxis()->SetTitleOffset(1.0);
+    hist->GetZaxis()->SetTitleOffset(1.15);
+  } else {
+    hist->GetXaxis()->SetTitleOffset(0.9);
+    hist->GetYaxis()->SetTitleOffset(1.7);
+  }
   hist->GetXaxis()->SetLabelFont(42);
   hist->GetYaxis()->SetLabelFont(42);
   if (((TObject*)hist)->IsA()->InheritsFrom("TGraph")) {
@@ -95,6 +121,7 @@ namespace diMuonMassBias {
 
   using histoMap = std::map<std::string, TH1F*>;
   using histo2DMap = std::map<std::string, TH2F*>;
+  using histo3DMap = std::map<std::string, TH3F*>;
 
 }  // namespace diMuonMassBias
 
@@ -234,6 +261,146 @@ void fitAndFillHisto(std::pair<std::string, TH2F*> toHarvest,
 }
 
 //-----------------------------------------------------------------------------------
+void bookMaps(const diMuonMassBias::histo3DMap& harvestTargets_,
+              diMuonMassBias::histo2DMap& meanMaps_,
+              diMuonMassBias::histo2DMap& widthMaps_,
+              const unsigned int counter)
+//-----------------------------------------------------------------------------------
+{
+  for (const auto& [key, ME] : harvestTargets_) {
+    if (ME == nullptr) {
+      std::cout << "could not find MonitorElement for key: " << key << std::endl;
+      continue;
+    }
+
+    const auto& title = ME->GetTitle();
+    const auto& xtitle = ME->GetYaxis()->GetTitle();
+    const auto& ytitle = ME->GetZaxis()->GetTitle();
+
+    const auto& nxbins = ME->GetNbinsY();
+    const auto& xmin = ME->GetYaxis()->GetXmin();
+    const auto& xmax = ME->GetYaxis()->GetXmax();
+
+    const auto& nybins = ME->GetNbinsZ();
+    const auto& ymin = ME->GetZaxis()->GetXmin();
+    const auto& ymax = ME->GetZaxis()->GetXmax();
+
+    if (debugMode_) {
+      std::cout << "Booking " << key << std::endl;
+    }
+
+    TH2F* meanToBook =
+        new TH2F(fmt::format("Mean_{}_{}", counter, key).c_str(),
+                 fmt::format("{};{};{};#LT M_{{#mu^{{-}}#mu^{{+}}}} #GT [GeV]", title, xtitle, ytitle).c_str(),
+                 nxbins,
+                 xmin,
+                 xmax,
+                 nybins,
+                 ymin,
+                 ymax);
+
+    if (debugMode_) {
+      std::cout << "after creating mean" << key << std::endl;
+    }
+
+    meanMaps_.insert({key, meanToBook});
+
+    if (debugMode_) {
+      std::cout << "after inserting mean" << key << std::endl;
+    }
+
+    TH2F* sigmaToBook =
+        new TH2F(fmt::format("Sigma_{}_{}", counter, key).c_str(),
+                 fmt::format("{};{};#sigma of  M_{{#mu^{{-}}#mu^{{+}}}} [GeV]", title, xtitle, ytitle).c_str(),
+                 nxbins,
+                 xmin,
+                 xmax,
+                 nybins,
+                 ymin,
+                 ymax);
+
+    if (debugMode_) {
+      std::cout << "after creating sigma" << key << std::endl;
+    }
+
+    widthMaps_.insert({key, sigmaToBook});
+
+    if (debugMode_) {
+      std::cout << "after inserting sigma" << key << std::endl;
+    }
+  }
+}
+
+//-----------------------------------------------------------------------------------
+void fitAndFillMap(std::pair<std::string, TH3F*> toHarvest,
+                   diMuonMassBias::histo2DMap& meanMaps_,
+                   diMuonMassBias::histo2DMap& widthMaps_)
+//-----------------------------------------------------------------------------------
+{
+  const auto& key = toHarvest.first;
+  const auto& ME = toHarvest.second;
+
+  if (debugMode_)
+    std::cout << "dealing with key: " << key << std::endl;
+
+  if (ME == nullptr) {
+    std::cout << "could not find MonitorElement for key: " << key << std::endl;
+    return;
+  }
+
+  for (int binY = 1; binY <= ME->GetNbinsY(); binY++) {
+    const auto& yaxis = ME->GetYaxis();
+    const auto& y_low_edge = yaxis->GetBinLowEdge(binY);
+    const auto& y_high_edge = yaxis->GetBinUpEdge(binY);
+
+    for (int binZ = 1; binZ <= ME->GetNbinsZ(); binZ++) {
+      const auto& zaxis = ME->GetZaxis();
+      const auto& z_low_edge = zaxis->GetBinLowEdge(binZ);
+      const auto& z_high_edge = zaxis->GetBinUpEdge(binZ);
+
+      if (debugMode_) {
+        std::cout << "dealing with y bin: " << binY << " range: (" << y_low_edge << "," << y_high_edge << ")";
+        std::cout << " dealing with z bin: " << binZ << " range: (" << z_low_edge << "," << z_high_edge << ")"
+                  << std::endl;
+      }
+
+      const auto& ProjYZ = ME->ProjectionX(Form("%s_proj_%i_proj_%i", key.c_str(), binY, binZ), binY, binY, binZ, binZ);
+      ProjYZ->SetTitle(Form("%s #in (%.2f,%.2f) - (%.2f,%.2f), bin: %i-%i ",
+                            ProjYZ->GetTitle(),
+                            y_low_edge,
+                            y_high_edge,
+                            z_low_edge,
+                            z_high_edge,
+                            binY,
+                            binZ));
+
+      diMuonMassBias::fitOutputs results = fitBWTimesCB(ProjYZ);
+
+      if (results.isInvalid()) {
+        std::cout << "the current bin has invalid data" << std::endl;
+        continue;
+      }
+
+      // fill the mean profiles
+      const Measurement1D& bias = results.getBias();
+      meanMaps_[key]->SetBinContent(binY, binZ, bias.value());
+      meanMaps_[key]->SetBinError(binY, binZ, bias.error());
+
+      // fill the width profiles
+      const Measurement1D& width = results.getWidth();
+      widthMaps_[key]->SetBinContent(binY, binZ, width.value());
+      widthMaps_[key]->SetBinError(binY, binZ, width.error());
+
+      if (debugMode_) {
+        std::cout << "key: " << key << " bin: (" << binY << "," << binZ << ")  bias: " << bias.value()
+                  << " (in histo: " << meanMaps_[key]->GetBinContent(binY, binZ) << ") width: " << width.value()
+                  << " (in histo: " << widthMaps_[key]->GetBinContent(binY, binZ) << ")" << std::endl;
+      }
+    }
+  }
+}
+
+//-----------------------------------------------------------------------------------
 void bookHistos(const diMuonMassBias::histo2DMap& harvestTargets_,
                 diMuonMassBias::histoMap& meanHistos_,
                 diMuonMassBias::histoMap& widthHistos_,
@@ -293,11 +460,14 @@ void bookHistos(const diMuonMassBias::histo2DMap& harvestTargets_,
 }
 
 //-----------------------------------------------------------------------------------
-void getMEsToHarvest(diMuonMassBias::histo2DMap& harvestTargets_, TFile* file)
+void getMEsToHarvest(diMuonMassBias::histo2DMap& harvestTargets_,
+                     diMuonMassBias::histo3DMap& harvestTargets3D_,
+                     TFile* file)
 //-----------------------------------------------------------------------------------
 {
   std::string inFolder = "DiMuonMassValidation";
 
+  // list of 2D histograms to flatten
   std::vector<std::string> MEtoHarvest_ = {"th2d_mass_CosThetaCS",
                                            "th2d_mass_DeltaEta",
                                            "th2d_mass_EtaMinus",
@@ -328,6 +498,20 @@ void getMEsToHarvest(diMuonMassBias::histo2DMap& harvestTargets_, TFile* file)
       continue;
     }
     harvestTargets_.insert({hname, toHarvest});
+  }
+
+  // list of 3D histograms to flatten
+  std::vector<std::string> MEtoHarvest3D_ = {"th3d_mass_vs_eta_phi_plus", "th3d_mass_vs_eta_phi_minus"};
+
+  for (const auto& hname : MEtoHarvest3D_) {
+    std::cout << "trying to get: " << hname << std::endl;
+    TH3F* toHarvest = (TH3F*)file->Get((inFolder + "/" + hname).c_str());
+
+    if (toHarvest == nullptr) {
+      std::cout << "could not find input MonitorElement: " << inFolder + "/" + hname << std::endl;
+      continue;
+    }
+    harvestTargets3D_.insert({hname, toHarvest});
   }
 }
 
@@ -433,6 +617,105 @@ void producePlots(const std::vector<diMuonMassBias::histoMap>& inputMap,
 }
 
 /************************************************/
+void produceMaps(const std::vector<diMuonMassBias::histo2DMap>& inputMap,
+                 const std::vector<std::string>& MEtoHarvest,
+                 const std::vector<TString>& labels,
+                 const TString& Rlabel,
+                 const bool isWidth)
+/************************************************/
+{
+  const auto& sides = getClosestFactors(labels.size());
+
+  if (debugMode_) {
+    std::cout << "SIDES:" << sides.second << " :" << sides.first << std::endl;
+  }
+
+  int W = 800 * sides.second;
+  int H = 600 * sides.first;
+  // references for T, B, L, R
+  float T = 0.08 * H;
+  float B = 0.12 * H;
+  float L = 0.12 * W;
+  float R = 0.04 * W;
+
+  // Draw the legend
+  TLegend* infoBox = new TLegend(0.65, 0.75, 0.95, 0.90, "");
+  infoBox->SetShadowColor(0);  // 0 = transparent
+  infoBox->SetFillColor(kWhite);
+  infoBox->SetTextSize(0.035);
+
+  for (const auto& var : MEtoHarvest) {
+    TCanvas* c = new TCanvas(
+        ((isWidth ? "width_" : "mean_") + var).c_str(), ((isWidth ? "width_" : "mean_") + var).c_str(), W, H);
+
+    c->SetFillColor(0);
+    c->SetBorderMode(0);
+    c->SetFrameFillStyle(0);
+    c->SetFrameBorderMode(0);
+    c->SetLeftMargin(L / W);
+    c->SetRightMargin(R / W);
+    c->SetTopMargin(T / H);
+    c->SetBottomMargin(B / H);
+    c->SetTickx(0);
+    c->SetTicky(0);
+
+    c->Divide(sides.second, sides.first);
+
+    unsigned int count{0};
+    for (const auto& histoMap : inputMap) {
+      if (debugMode_) {
+        std::cout << var << "  n.bins: " << histoMap.at(var)->GetNbinsX()
+                  << " entries: " << histoMap.at(var)->GetEntries() << "title: " << histoMap.at(var)->GetTitle()
+                  << " x-axis title: " << histoMap.at(var)->GetXaxis()->GetTitle() << std::endl;
+      }
+
+      if (debugMode_) {
+        for (int bin = 1; bin <= histoMap.at(var)->GetNbinsX(); bin++) {
+          std::cout << var << " bin " << bin << " : " << histoMap.at(var)->GetBinContent(bin) << " +/-"
+                    << histoMap.at(var)->GetBinError(bin) << std::endl;
+        }
+      }
+
+      if (isWidth) {
+        // for width resolution between 0.5 and 2.8
+        histoMap.at(var)->GetZaxis()->SetRangeUser(0.5, 2.85);
+      } else {
+        // for mass between 90.5 and 91.5
+        histoMap.at(var)->GetZaxis()->SetRangeUser(90., 92.);
+      }
+
+      MakeNicePlotStyle<TH2>(histoMap.at(var));
+
+      c->cd(count + 1);
+      c->cd(count + 1)->SetRightMargin(0.15);
+      histoMap.at(var)->Draw("colz");
+
+      // fill the legend only if that's the first element in the vector of variables
+      if (var == MEtoHarvest[0]) {
+        infoBox->AddEntry(histoMap.at(var), labels[count], "LP");
+      }
+      //infoBox->Draw("same");
+
+      CMS_lumi(dynamic_cast<TPad*>(gPad), 0, 3, labels[count]);
+      count++;
+    }
+
+    // Find the position of the first '/'
+    size_t pos = var.find('/');
+    std::string outputName{var};
+
+    // Check if '/' is found
+    if (pos != std::string::npos) {
+      // Erase the substring before the '/' (including the '/')
+      outputName.erase(0, pos + 1);
+    }
+
+    c->SaveAs(((isWidth ? "width_" : "mean_") + outputName + ".png").c_str());
+    c->SaveAs(((isWidth ? "width_" : "mean_") + outputName + ".pdf").c_str());
+  }
+}
+
+/************************************************/
 void DiMuonMassProfiles(TString namesandlabels, const TString& Rlabel = "")
 /************************************************/
 {
@@ -457,18 +740,30 @@ void DiMuonMassProfiles(TString namesandlabels, const TString& Rlabel = "")
     }
   }
 
+  // for the bias plots
   std::vector<diMuonMassBias::histoMap> v_meanHistos;
   std::vector<diMuonMassBias::histoMap> v_widthHistos;
+
+  // for the maps
+  std::vector<diMuonMassBias::histo2DMap> v_meanMaps;
+  std::vector<diMuonMassBias::histo2DMap> v_widthMaps;
 
   unsigned int countFiles{0};
   for (const auto& file : sourceFiles) {
     diMuonMassBias::histo2DMap harvestTargets;
-    getMEsToHarvest(harvestTargets, file);
+    diMuonMassBias::histo3DMap harvestTargets3D;
+
+    getMEsToHarvest(harvestTargets, harvestTargets3D, file);
 
     diMuonMassBias::histoMap meanHistos;
     diMuonMassBias::histoMap widthHistos;
 
     bookHistos(harvestTargets, meanHistos, widthHistos, countFiles);
+
+    diMuonMassBias::histo2DMap meanMaps;
+    diMuonMassBias::histo2DMap widthMaps;
+
+    bookMaps(harvestTargets3D, meanMaps, widthMaps, countFiles);
 
     for (const auto& element : harvestTargets) {
       fitAndFillHisto(element, meanHistos, widthHistos);
@@ -476,6 +771,13 @@ void DiMuonMassProfiles(TString namesandlabels, const TString& Rlabel = "")
 
     v_meanHistos.push_back(meanHistos);
     v_widthHistos.push_back(widthHistos);
+
+    for (const auto& element : harvestTargets3D) {
+      fitAndFillMap(element, meanMaps, widthMaps);
+    }
+
+    v_meanMaps.push_back(meanMaps);
+    v_widthMaps.push_back(widthMaps);
 
     countFiles++;
   }
@@ -502,6 +804,11 @@ void DiMuonMassProfiles(TString namesandlabels, const TString& Rlabel = "")
 
   producePlots(v_meanHistos, MEtoHarvest, labels, Rlabel, false);
   producePlots(v_widthHistos, MEtoHarvest, labels, Rlabel, true);
+
+  std::vector<std::string> MEtoHarvest3D = {"th3d_mass_vs_eta_phi_plus", "th3d_mass_vs_eta_phi_minus"};
+
+  produceMaps(v_meanMaps, MEtoHarvest3D, labels, Rlabel, false);
+  produceMaps(v_widthMaps, MEtoHarvest3D, labels, Rlabel, true);
 
   // finally close the file
   for (const auto& file : sourceFiles) {
