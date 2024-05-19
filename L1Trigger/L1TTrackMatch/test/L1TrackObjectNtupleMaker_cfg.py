080dafa17ad1fabeee8dfd2efd8b0245fc824a36
Ryan McCarthy <ryan.edward.mccarthy@cern.ch>
2024-05-19 14:02:43 +0200

    Adding option to run displaced vertexing in ntupler maker, reverting changes to ntuple maker params, removing BDT requirement on displaced track quality

--- L1Trigger/L1TTrackMatch/test/L1TrackObjectNtupleMaker_cfg.py
+++ L1Trigger/L1TTrackMatch/test/L1TrackObjectNtupleMaker_cfg.py
@@ -20,6 +20,7 @@ DISPLACED = ''
 
 
 runVtxNN = True
+runDispVert = False
 ############################################################
 # import standard configurations
 ############################################################
@@ -41,7 +42,6 @@ process.MessageLogger.cerr.INFO.limit = cms.untracked.int32(0) # default: 0
 ############################################################
 # input and output
 ############################################################
-
 
 process.maxEvents = cms.untracked.PSet(input = cms.untracked.int32(20))
 
@@ -170,7 +170,7 @@ elif (L1TRKALGO == 'HYBRID_DISPLACED'):
     process.pTkMET = cms.Path(process.l1tTrackerEtMissExtended)
     process.pTkMHT = cms.Path(process.l1tTrackerHTMissExtended)
     process.pTkMHTEmulator = cms.Path(process.l1tTrackerEmuHTMissExtended)
-    process.DispVert = cms.Path(process.DisplacedVertexProducer)
+    if(runDispVert): process.DispVert = cms.Path(process.DisplacedVertexProducer)
     DISPLACED = 'Displaced'#
 
 # HYBRID: extended tracking
@@ -192,7 +192,7 @@ elif (L1TRKALGO == 'HYBRID_PROMPTANDDISP'):
     process.pTkMHT = cms.Path(process.l1tTrackerHTMiss*process.l1tTrackerHTMissExtended)
     process.pTkMHTEmulator = cms.Path(process.l1tTrackerEmuHTMiss*process.l1tTrackerEmuHTMissExtended)
     process.pL1TrackTripletEmulator = cms.Path(process.l1tTrackTripletEmulation)
-    process.DispVert = cms.Path(process.DisplacedVertexProducer)
+    if(runDispVert): process.DispVert = cms.Path(process.DisplacedVertexProducer)
     DISPLACED = 'Both'
 
 
@@ -284,6 +284,7 @@ process.L1TrackNtuple = cms.EDAnalyzer('L1TrackObjectNtupleMaker',
         RecoVertexInputTag=cms.InputTag("l1tVertexFinder", "L1Vertices"),
         RecoVertexEmuInputTag=cms.InputTag("l1tVertexFinderEmulator", "L1VerticesEmulation"),
         DisplacedVertexInputTag = cms.InputTag("DisplacedVertexProducer","dispVertices"),
+        runDispVert = cms.bool(runDispVert)
 )
 
 process.ntuple = cms.Path(process.L1TrackNtuple)
@@ -304,4 +305,7 @@ process.pOut = cms.EndPath(process.out)
 # use this if cluster/stub associators not available
 # process.schedule = cms.Schedule(process.TTClusterStubTruth,process.TTTracksEmuWithTruth,process.ntuple)
 
-process.schedule = cms.Schedule(process.TTClusterStub, process.TTClusterStubTruth, process.dtc, process.TTTracksEmuWithTruth, process.pL1GTTInput, process.pL1TrackSelection, process.pPV, process.pPVemu,process.pL1TrackVertexAssociation, process.pL1TrackJets, process.pL1TrackJetsEmu,process.pL1TrackFastJets, process.pTkMET, process.pTkMETEmu, process.pTkMHT, process.pTkMHTEmulator,process.pL1TrackTripletEmulator, process.DispVert, process.ntuple)
+if(runDispVert):
+    process.schedule = cms.Schedule(process.TTClusterStub, process.TTClusterStubTruth, process.dtc, process.TTTracksEmuWithTruth, process.pL1GTTInput, process.pL1TrackSelection, process.pPV, process.pPVemu,process.pL1TrackVertexAssociation, process.pL1TrackJets, process.pL1TrackJetsEmu,process.pL1TrackFastJets, process.pTkMET, process.pTkMETEmu, process.pTkMHT, process.pTkMHTEmulator,process.pL1TrackTripletEmulator, process.DispVert, process.ntuple)
+else:
+    process.schedule = cms.Schedule(process.TTClusterStub, process.TTClusterStubTruth, process.dtc, process.TTTracksEmuWithTruth, process.pL1GTTInput, process.pL1TrackSelection, process.pPV, process.pPVemu,process.pL1TrackVertexAssociation, process.pL1TrackJets, process.pL1TrackJetsEmu,process.pL1TrackFastJets, process.pTkMET, process.pTkMETEmu, process.pTkMHT, process.pTkMHTEmulator,process.pL1TrackTripletEmulator,process.ntuple)
