01ec63f71dc3f8eae7868a1e9e31c1a44b6f37b8
Ivan Razumov <iarspider@gmail.com>
2024-05-28 12:29:17 +0200

    Fix typo & code-format

--- CondCore/EcalPlugins/plugins/EcalDQMChannelStatus_PayloadInspector.cc
+++ CondCore/EcalPlugins/plugins/EcalDQMChannelStatus_PayloadInspector.cc
@@ -18,8 +18,9 @@
 
 namespace {
   constexpr int kEBChannels = 61200, kEEChannels = 14648, NRGBs = 5, NCont = 255;
-  constexpr int MIN_IETA = 1, MIN_IPHI = 1, MAX_IETA = 85, MAX_IPHI = 360;  // barrel lower and upper bounds on eta and phi
-  constexpr int IX_MIN = 1, IY_MIN = 1, IX_MAX = 100, IY_MAX = 100;         // endcaps lower and upper bounds on x and y
+  constexpr int MIN_IETA = 1, MIN_IPHI = 1, MAX_IETA = 85,
+                MAX_IPHI = 360;                                      // barrel lower and upper bounds on eta and phi
+  constexpr int IX_MIN = 1, IY_MIN = 1, IX_MAX = 100, IY_MAX = 100;  // endcaps lower and upper bounds on x and y
 
   /*******************************************************
      2d plot of ECAL barrel DQM channel status of 1 IOV
