c619df66693bc02f0b1086e86e68180f34c7b5f4
Andrea Bocci <andrea.bocci@cern.ch>
2024-06-07 11:15:29 +0200

    Update edm::bit_cast
    Use std::bit_cast in C++20 mode, or __builtin_bit_cast if available, otherwise
    fall back to memcpy.

--- FWCore/Utilities/interface/bit_cast.h
+++ FWCore/Utilities/interface/bit_cast.h
@@ -18,19 +18,43 @@
 //         Created:  Wed, 01 Sep 2021 19:11:41 GMT
 //
 
+// for compilers that do not support __has_builtin
+#ifndef __has_builtin
+#define __has_builtin(x) 0
+#endif
+
 // system include files
 #include <cstring>
+#include <type_traits>
 
-// user include files
+#if __cplusplus >= 202002L
+
+// in C++20 we can use std::bit_cast
+
+#include <bit>
 
 namespace edm {
-  //in C++20 we can use std::bit_cast which is constexpr
-  template <class To, class From>
-  inline To bit_cast(const From &src) noexcept {
+  using std::bit_cast;
+}  // namespace edm
+
+#elif __has_builtin(__builtin_bit_cast)
+
+// before C++20 we can use __builtin_bit_cast, if supported
+
+namespace edm {
+  template <typename To, typename From>
+  constexpr inline To bit_cast(const From &src) noexcept {
+    static_assert(std::is_trivially_copyable_v<From>);
+    static_assert(std::is_trivially_copyable_v<To>);
     static_assert(sizeof(To) == sizeof(From), "incompatible types");
-    To dst;
-    std::memcpy(&dst, &src, sizeof(To));
-    return dst;
+    return __builtin_bit_cast(To, src);
   }
 }  // namespace edm
-#endif
+
+#else
+
+#error constexpr edm::bit_cast is not supported by the compiler
+
+#endif  // __cplusplus >= 202002L
+
+#endif  // FWCore_Utilities_bit_cast_h
