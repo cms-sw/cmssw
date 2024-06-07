49712ce3912e6343e734d10b1007c770962a35d3
Sunanda <sunanda.banerjee@cern.ch>
2024-06-07 09:30:53 +0200

    Code check

--- SimG4CMS/Calo/src/HGCalSD.cc
+++ SimG4CMS/Calo/src/HGCalSD.cc
@@ -191,7 +191,8 @@ uint32_t HGCalSD::setDetUnitId(const G4Step* aStep) {
 
   uint32_t id = setDetUnitId(layer, module, cell, iz, hitPoint);
 #ifdef EDM_ML_DEBUG
-  edm::LogVerbatim("HGCSim") << "ID Layer " << layer << " Module " << module << " Cell " << cell << " " << std::hex << id << std::dec << " " << HGCSiliconDetId(id);
+  edm::LogVerbatim("HGCSim") << "ID Layer " << layer << " Module " << module << " Cell " << cell << " " << std::hex
+                             << id << std::dec << " " << HGCSiliconDetId(id);
 #endif
   if ((rejectMB_ || fiducialCut_) && id != 0) {
     auto uv = HGCSiliconDetId(id).waferUV();
