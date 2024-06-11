c73ad3480e24c297dcc16c0df89fb74339f301fc
Martin <ka_hei_martin_kwok@brown.edu>
2024-06-11 22:27:27 +0200

    HcalRecHit SoA dataFormat

---
+++ DataFormats/HcalRecHit/interface/HcalRecHitSoA.h
@@ -0,0 +1,19 @@
+#ifndef DataFormats_HcalRecHit_HcalRecHitSoA_h
+#define DataFormats_HcalRecHit_HcalRecHitSoA_h
+
+#include "DataFormats/SoATemplate/interface/SoALayout.h"
+
+namespace hcal {
+
+  GENERATE_SOA_LAYOUT(HcalRecHitSoALayout,
+                      SOA_SCALAR(uint32_t, size),
+                      SOA_COLUMN(uint32_t, detId),
+                      SOA_COLUMN(float, energy),
+                      SOA_COLUMN(float, chi2),
+                      SOA_COLUMN(float, energyM0),
+                      SOA_COLUMN(float, timeM0))
+
+  using HcalRecHitSoA = HcalRecHitSoALayout<>;
+}  // namespace hcal
+
+#endif
