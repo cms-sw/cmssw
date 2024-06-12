d5376d6015a1d59bc313db761901a53f3aaaecd9
Patin Inkaew <earthpatin@gmail.com>
2024-06-13 00:39:29 +0200

    add run3_scouting_nanoAOD_post2023 modifier to Era_Run3_2024

--- Configuration/Eras/python/Era_Run3_2024_cff.py
+++ Configuration/Eras/python/Era_Run3_2024_cff.py
@@ -2,5 +2,6 @@ import FWCore.ParameterSet.Config as cms
 
 from Configuration.Eras.Era_Run3_cff import Run3
 from Configuration.Eras.Modifier_run3_2024_L1T_cff import run3_2024_L1T
+from Configuration.Eras.Modifier_run3_scouting_nanoAOD_post2023_cff import run3_scouting_nanoAOD_post2023
 
-Run3_2024 = cms.ModifierChain(Run3, run3_2024_L1T)
+Run3_2024 = cms.ModifierChain(Run3, run3_2024_L1T, run3_scouting_nanoAOD_post2023)
