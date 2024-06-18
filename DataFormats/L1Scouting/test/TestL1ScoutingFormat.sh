73d8ad20713b9fdfb42d7e76841ad4f058002053
Matteo Migliorini <matteo.migliorini95@gmail.com>
2024-06-18 16:59:07 +0200

    Added in the test script new testing file

--- DataFormats/L1Scouting/test/TestL1ScoutingFormat.sh
+++ DataFormats/L1Scouting/test/TestL1ScoutingFormat.sh
@@ -10,8 +10,14 @@ file=testL1Scouting.root
 
 cmsRun ${LOCAL_TEST_DIR}/read_L1Scouting_cfg.py --inputFile "$file" || die "Failure using read_L1Scouting_cfg.py $file" $?
 
+# test file for muon, jet, e/gamma and energy sums data formats
 oldFile="testL1Scouting_v3_v3_v3_v3_v3_13_3_0_pre5.root"
 inputfile=$(edmFileInPath DataFormats/L1Scouting/data/$oldFile) || die "Failure edmFileInPath DataFormats/L1Scouting/data/$oldFile" $?
 cmsRun ${LOCAL_TEST_DIR}/read_L1Scouting_cfg.py --inputFile "$inputfile" --bmtfStubVersion 0 || die "Failed to read old file $oldFile" $?
+
+# added BMTF input stubs data format
+oldFile="testL1Scouting_v3_v3_v3_v3_v3_v3_14_1_0_pre4.root"
+inputfile=$(edmFileInPath DataFormats/L1Scouting/data/$oldFile) || die "Failure edmFileInPath DataFormats/L1Scouting/data/$oldFile" $?
+cmsRun ${LOCAL_TEST_DIR}/read_L1Scouting_cfg.py --inputFile "$inputfile" --bmtfStubVersion 3 || die "Failed to read old file $oldFile" $?
 
 exit 0
