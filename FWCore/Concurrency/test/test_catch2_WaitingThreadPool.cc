bb85be9bf2caa38eb1f8f997d7827b42c0594615
iarspider <iarspider@gmail.com>
2024-06-18 10:41:14 +0200

    Remove unused lambda captures in test_catch2_WaitingThreadPool.cc

--- FWCore/Concurrency/test/test_catch2_WaitingThreadPool.cc
+++ FWCore/Concurrency/test/test_catch2_WaitingThreadPool.cc
@@ -123,7 +123,7 @@ TEST_CASE("Test WaitingThreadPool", "[edm::WaitingThreadPool") {
 
       {
         using namespace edm::waiting_task::chain;
-        auto h = first([&pool, &count](edm::WaitingTaskHolder h) {
+        auto h = first([&pool](edm::WaitingTaskHolder h) {
                    edm::WaitingTaskWithArenaHolder h2(std::move(h));
                    pool.runAsync(
                        std::move(h2), []() { throw std::runtime_error("error"); }, errorContext);
