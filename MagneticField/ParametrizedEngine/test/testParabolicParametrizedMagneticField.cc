bec7c6e0269ccfc0e0be7693a272b009964174e7
Leonardo Cristella <leonardo.cristella@cern.ch>
2024-06-19 15:13:50 +0200

    Add BuildFile for test

--- MagneticField/ParametrizedEngine/test/testParabolicParametrizedMagneticField.cc
+++ MagneticField/ParametrizedEngine/test/testParabolicParametrizedMagneticField.cc
@@ -2,9 +2,12 @@
 #include <Eigen/Core>
 #include "MagneticField/ParametrizedEngine/interface/alpaka/ParabolicParametrizedMagneticField.h"
 
-using Vector3f = Eigen::Matrix<float, 3, 1>;
-using namespace MagneticFieldParabolicPortable;
+int main() {
+  using namespace ALPAKA_ACCELERATOR_NAMESPACE::MagneticFieldParabolicPortable;
+  using Vector3f = Eigen::Matrix<float, 3, 1>;
 
-Vector3f position{1, 1, 1};
+  Vector3f position{1, 1, 1};
+  assert(MagneticFieldAtPoint(position) == B0Z(position) * Kr(position));
 
-assert(MagneticFieldAtPoint(position) == B0Z(position) * Kr(position));
+  return 0;
+}
