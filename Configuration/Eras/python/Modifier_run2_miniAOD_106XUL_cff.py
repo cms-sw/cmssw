b5c3be5818740319218f0ad7bdddd11bfdfba7b4
jean-roch <vlimant@cern.ch>
2024-06-24 10:41:59 +0200

    roll back adding run2_miniAOD_106XUL

--- Configuration/Eras/python/Modifier_run2_miniAOD_106XUL_cff.py
+++
@@ -1,6 +0,0 @@
-import FWCore.ParameterSet.Config as cms
-
-## this modidier is meant for processing 10.6.X AOD/AODSIM with the current release,
-## therefore adapting the PAT configuration to whatever changes happened at the AOD level, to produce a consistent MINI
-
-run2_miniAOD_106XUL = cms.Modifier()
