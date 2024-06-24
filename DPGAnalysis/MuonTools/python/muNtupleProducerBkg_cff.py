f355f18c1328ea636535f1d5220cd704d20d363f
Carlo Battilana <carlo.battilana@gmail.com>
2024-06-24 17:48:55 +0200

    Update table convention naming to deal with NANOEDMAODoutput

--- DPGAnalysis/MuonTools/python/muNtupleProducerBkg_cff.py
+++ DPGAnalysis/MuonTools/python/muNtupleProducerBkg_cff.py
@@ -5,9 +5,8 @@ from PhysicsTools.NanoAOD.common_cff import *
 from DPGAnalysis.MuonTools.nano_mu_global_cff import *
 from DPGAnalysis.MuonTools.nano_mu_digi_cff import *
 
-muDPGNanoProducerBkg = cms.Sequence(lhcInfoTableProducer
-                                   + lumiTableProducer
-                                   + muDigiProducersBkg)
+muDPGNanoProducerBkg = cms.Sequence(globalTables
+                                   + muDigiTablesBkg)
 
 def muDPGNanoBkgCustomize(process) :
 
