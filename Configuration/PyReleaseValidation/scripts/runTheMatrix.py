905735877c0eac8fd750fded8d7c5d39ed258b59
Norraphat <srimanob@mail.cern.ch>
2024-07-03 19:31:30 +0200

    clean up

--- Configuration/PyReleaseValidation/scripts/runTheMatrix.py
+++ Configuration/PyReleaseValidation/scripts/runTheMatrix.py
@@ -95,7 +95,7 @@ if __name__ == '__main__':
                     29834.999,  # RelValTTbar_14TeV (PREMIX)            phase2_realistic_T33        Extended2026D110         AVE_50_BX_25ns_m3p3     
                     29696.0,    # RelValCloseByPGun_CE_E_Front_120um    phase2_realistic_T33        Extended2026D110
                     29700.0,    # RelValCloseByPGun_CE_H_Coarse_Scint   phase2_realistic_T33        Extended2026D110  
-                    23234.0,    # TTbar_14TeV_TuneCP5                   phase2_realistic_T21        Extended2026D94         (exercise with HFNose) 
+                    #23234.0,   # Need new workflow with HFNose
                     
 
                     ###### pp Data
