9bfcc61dca9fb8088afa12484b5c408da4a273b2
Leonardo Cristella <leonardo.cristella@cern.ch>
2024-07-04 17:27:59 +0200

    Add helper vectors

--- MagneticField/ParametrizedEngine/test/testParabolicParametrizedMagneticField.cc
+++ MagneticField/ParametrizedEngine/test/testParabolicParametrizedMagneticField.cc
@@ -21,14 +21,12 @@ int main() {
   edm::FileInPath mydata("MagneticField/Engine/data/Regression/referenceField_160812_RII_3_8T.bin");
   file.open(mydata.fullPath().c_str(), ios::binary);
 
-  float resolution = 0.2;
-  float maxdelta = 0.;
-  int fail = 0;
   int count = 0;
 
   float px, py, pz;
   float bx, by, bz;
-  GlobalPoint gp;
+  vector<Vector3f> points;
+  vector<GlobalVector> referenceB_vec;
 
   int numberOfPoints = 100;
   do {
@@ -36,19 +34,31 @@ int main() {
           file.read((char*)&pz, sizeof(float)) && file.read((char*)&bx, sizeof(float)) &&
           file.read((char*)&by, sizeof(float)) && file.read((char*)&bz, sizeof(float))))
       break;
-    gp = GlobalPoint(px, py, pz);
 
+    const auto gp = GlobalPoint(px, py, pz);
     if (gp.perp2() > Parameters::max_radius2 || fabs(gp.z()) > Parameters::max_z)
       continue;
 
-    GlobalVector referenceB(bx, by, bz);
-    GlobalVector deviceB(0, 0, magneticFieldAtPoint(Vector3f(px, py, pz)));
+    points.push_back(Vector3f(px, py, pz));
+    referenceB_vec.push_back(GlobalVector(bx, by, bz));
+    count++;
+  } while (count < numberOfPoints);
+
+  float resolution = 0.2;
+  float maxdelta = 0.;
+  int fail = 0;
+
+  for (uint i=0; i<points.size(); i++) {
+    const auto point = points[i];
+    const auto referenceB = referenceB_vec[i];
+    GlobalVector deviceB(0, 0, magneticFieldAtPoint(point));
     if ((referenceB - deviceB).mag() > resolution) {
       ++fail;
       float delta = (referenceB - deviceB).mag();
       if (delta > maxdelta)
         maxdelta = delta;
       if (fail < 10) {
+        const GlobalPoint gp(point(0), point(1), point(2));
         cout << " Discrepancy at point  # " << count + 1 << ": " << gp << ", R " << gp.perp() << ", Phi " << gp.phi()
              << ", delta: " << referenceB - deviceB << " " << delta << endl;
         cout << " Old: " << referenceB << ", New: " << deviceB << endl;
@@ -56,8 +66,7 @@ int main() {
         cout << "..." << endl;
       }
     }
-    count++;
-  } while (count < numberOfPoints);
+  }
 
   if (fail != 0)
     throw cms::Exception("RegressionFailure") << "MF regression found: at least " << fail << " failures";
