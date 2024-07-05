2590fe79942855c20c6636eee4be3325de2540fc
Sunanda <sunanda.banerjee@cern.ch>
2024-07-05 08:13:40 +0200

    Update the DetId for HGCal scintillator in view of changes for the V19 version of HGCal geometry

--- DataFormats/ForwardDetId/src/HGCScintillatorDetId.cc
+++ DataFormats/ForwardDetId/src/HGCScintillatorDetId.cc
@@ -21,7 +21,5 @@ std::vector<HGCScintillatorDetId> HGCScintillatorDetId::detectorCells() const {
 }
 
 std::ostream& operator<<(std::ostream& s, const HGCScintillatorDetId& id) {
-  return s << " HGCScintillatorDetId::EE:HE= " << id.isEE() << ":" << id.isHE() << " trigger= " << id.trigger()
-           << " type= " << id.type() << " SiPM= " << id.sipm() << " layer= " << id.layer() << " ring= " << id.iradius()
-           << ":" << id.iradiusTrigger() << " phi= " << id.iphi() << ":" << id.iphiTrigger();
+  return s << " HGCScintillatorDetId::EE:HE= " << id.isEE() << ":" << id.isHE() << " trigger= " << id.trigger() << " granularity= " << id.granularity()<< " type= " << id.type() << " SiPM= " << id.sipm() << " layer= " << id.layer() << " ring= " << id.iradius() << ":" << id.iradiusTrigger() << " phi= " << id.iphi() << ":" << id.iphiTrigger();
 }
