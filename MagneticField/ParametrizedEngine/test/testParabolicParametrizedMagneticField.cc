05933ccae9d54ebc5c7bb8a674204fb6102f4252
Leonardo Cristella <leonardo.cristella@cern.ch>
2024-07-08 11:12:41 +0200

    Run test on device

--- MagneticField/ParametrizedEngine/test/testParabolicParametrizedMagneticField.cc
+++ MagneticField/ParametrizedEngine/test/testParabolicParametrizedMagneticField.cc
@@ -1,3 +1,5 @@
+#include <alpaka/alpaka.hpp>
+
 #include "FWCore/ParameterSet/interface/ParameterSet.h"
 #include "FWCore/ParameterSet/interface/FileInPath.h"
 #include "DataFormats/GeometryVector/interface/GlobalPoint.h"
@@ -7,14 +9,33 @@
 
 #include "MagneticField/ParametrizedEngine/interface/alpaka/ParabolicParametrizedMagneticField.h"
 
+#include "FWCore/Utilities/interface/stringize.h"
+#include "HeterogeneousCore/AlpakaInterface/interface/config.h"
+#include "HeterogeneousCore/AlpakaInterface/interface/memory.h"
+#include "HeterogeneousCore/AlpakaInterface/interface/workdivision.h"
+
 #include <iostream>
 #include <fstream>
 #include <Eigen/Core>
 
 using namespace edm;
 using namespace std;
+using namespace alpaka;
+using namespace ALPAKA_ACCELERATOR_NAMESPACE;
 using namespace ALPAKA_ACCELERATOR_NAMESPACE::magneticFieldParabolicPortable;
 using Vector3f = Eigen::Matrix<float, 3, 1>;
+
+struct MagneticFieldKernel {
+  template <typename TAcc, typename T>
+  ALPAKA_FN_ACC void operator()(
+      TAcc const& acc, T const* __restrict__ in, T* __restrict__ out, size_t size) const {
+    for (auto index : cms::alpakatools::uniform_elements(acc, size)) {
+      out[index][0] = 0;
+      out[index][1] = 0;
+      out[index][2] = magneticFieldAtPoint(in[index]);
+    }
+  }
+};
 
 int main() {
   ifstream file;
@@ -22,7 +43,6 @@ int main() {
   file.open(mydata.fullPath().c_str(), ios::binary);
 
   int count = 0;
-
   float px, py, pz;
   float bx, by, bz;
   vector<Vector3f> points;
@@ -44,30 +64,64 @@ int main() {
     count++;
   } while (count < numberOfPoints);
 
+  const size_t size = points.size();
+  // allocate the input and output host buffer in pinned memory accessible by the Platform devices
+  auto in_host = cms::alpakatools::make_host_buffer<float[], Platform>(size);
+  auto out_host = cms::alpakatools::make_host_buffer<float[], Platform>(size);
+  // fill the input buffers, and the output buffer with zeros
+  for (size_t i = 0; i < size; ++i) {
+    in_host[i] = points[i];
+    out_host[i] = {0.};
+  }
+
   float resolution = 0.2;
   float maxdelta = 0.;
   int fail = 0;
 
-  for (uint i=0; i<points.size(); i++) {
-    const auto point = points[i];
-    const auto referenceB = referenceB_vec[i];
-    GlobalVector deviceB(0, 0, magneticFieldAtPoint(point));
-    if ((referenceB - deviceB).mag() > resolution) {
-      ++fail;
-      float delta = (referenceB - deviceB).mag();
-      if (delta > maxdelta)
-        maxdelta = delta;
-      if (fail < 10) {
-        const GlobalPoint gp(point(0), point(1), point(2));
-        cout << " Discrepancy at point  # " << count + 1 << ": " << gp << ", R " << gp.perp() << ", Phi " << gp.phi()
-             << ", delta: " << referenceB - deviceB << " " << delta << endl;
-        cout << " Old: " << referenceB << ", New: " << deviceB << endl;
-      } else if (fail == 10) {
-        cout << "..." << endl;
+  // run the test on each device
+  for (auto const& device : cms::alpakatools::devices<Platform>()) {
+    auto queue = Queue(device);
+    // allocate input and output buffers on the device
+    auto points_dev = cms::alpakatools::make_device_buffer<float[]>(queue, size);
+    auto deviceB = cms::alpakatools::make_device_buffer<float[]>(queue, size);
+
+    // copy the input data to the device; the size is known from the buffer objects
+    alpaka::memcpy(queue, points_dev, in_host);
+    // fill the output buffer with zeros; the size is known from the buffer objects
+    alpaka::memset(queue, deviceB, 0.);
+
+    auto workDiv = cms::alpakatools::make_workdiv<Acc1D>(1, size);
+    alpaka::exec<Acc1D>(queue, workDiv, MagneticFieldKernel, points_dev.data(), deviceB.data(), size);
+
+    // copy the results from the device to the host
+    alpaka::memcpy(queue, out_host, deviceB);
+
+    // wait for the kernel and the potential copy to complete
+    alpaka::wait(queue);
+
+    // check the results
+    for (uint i=0; i<points.size(); i++) {
+      const auto point = points[i];
+      const auto referenceB = referenceB_vec[i];
+      const auto out_host_element = out_host[i];
+      GlobalVector parametricB(out_host_element[0], out_host_element[1], out_host_element[2]);
+      if ((referenceB - parametricB).mag() > resolution) {
+        ++fail;
+        float delta = (referenceB - parametricB).mag();
+        if (delta > maxdelta)
+          maxdelta = delta;
+        if (fail < 10) {
+          const GlobalPoint gp(point(0), point(1), point(2));
+          cout << " Discrepancy at point  # " << count + 1 << ": " << gp << ", R " << gp.perp() << ", Phi " << gp.phi()
+               << ", delta: " << referenceB - parametricB << " " << delta << endl;
+          cout << " Old: " << referenceB << ", New: " << parametricB << endl;
+        } else if (fail == 10) {
+          cout << "..." << endl;
+        }
       }
     }
-  }
 
-  if (fail != 0)
-    throw cms::Exception("RegressionFailure") << "MF regression found: at least " << fail << " failures";
+    if (fail != 0)
+      throw cms::Exception("RegressionFailure") << "MF regression found: at least " << fail << " failures";
+  }
 }
