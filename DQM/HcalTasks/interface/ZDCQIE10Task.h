97debf47613d4324816dbe56dc7f57936ded3264
Joshua Bistany-Riebman <joshua.bistany.riebman@cern.ch>
2024-07-24 18:07:09 +0200

    code formatting

--- DQM/HcalTasks/interface/ZDCQIE10Task.h
+++ DQM/HcalTasks/interface/ZDCQIE10Task.h
@@ -43,62 +43,55 @@
 #include "CondFormats/HcalObjects/interface/HcalLongRecoParams.h"
 #include "CondFormats/HcalObjects/interface/HcalLongRecoParam.h"
 
-
-
-
 class ZDCQIE10Task : public hcaldqm::DQTask {
 public:
-    ZDCQIE10Task(edm::ParameterSet const &);
-    ~ZDCQIE10Task() override {}
-    
-    void bookHistograms(DQMStore::IBooker &, edm::Run const &, edm::EventSetup const &) override;
-    //    std::shared_ptr<hcaldqm::Cache> globalBeginLuminosityBlock(edm::LuminosityBlock const &,
-    //							       edm::EventSetup const &) const override;
-    //void globalEndLuminosityBlock(edm::LuminosityBlock const &, edm::EventSetup const &) override;
+  ZDCQIE10Task(edm::ParameterSet const &);
+  ~ZDCQIE10Task() override {}
 
+  void bookHistograms(DQMStore::IBooker &, edm::Run const &, edm::EventSetup const &) override;
+  //    std::shared_ptr<hcaldqm::Cache> globalBeginLuminosityBlock(edm::LuminosityBlock const &,
+  //							       edm::EventSetup const &) const override;
+  //void globalEndLuminosityBlock(edm::LuminosityBlock const &, edm::EventSetup const &) override;
 
-    
 protected:
-    void _process(edm::Event const&, edm::EventSetup const&) override;
-    
-    //	tags
-    edm::InputTag _tagQIE10;
-    edm::InputTag sumTag;
-    edm::EDGetTokenT<QIE10DigiCollection> _tokQIE10;
-    edm::ESGetToken<HcalDbService, HcalDbRecord> hcalDbServiceToken_;
-    edm::EDGetToken sumToken_;
-    edm::ESGetToken<HcalTopology, HcalRecNumberingRecord> htopoToken_;
-    edm::ESGetToken<HcalLongRecoParams, HcalLongRecoParamsRcd> paramsToken_;
-    //edm::EDGetTokenT<l1t::EtSumBxCollection> sumZDCToken_;
-    
-    //emap
-    hcaldqm::electronicsmap::ElectronicsMap _ehashmap;  // online only
-        
-    //	hcaldqm::Containers
-    std::map<uint32_t, MonitorElement*> _cADC_EChannel;
-    std::map<uint32_t, MonitorElement*> _cADC_vs_TS_EChannel;
-    std::map<uint32_t, MonitorElement*> _cDigiSize_Crate;
-    std::map<uint32_t, MonitorElement*>_cDigiSize_FED;
-    std::map<uint32_t, MonitorElement*> _cADC_PM;    
-    std::map<uint32_t, MonitorElement*> _cADC_vs_TS_PM;
-    std::map<uint32_t, MonitorElement*> _cOccupancy_FEDuTCA;
-    std::map<uint32_t, MonitorElement*> _cOccupancy_ElectronicsuTCA;
-    std::map<uint32_t, MonitorElement*> _cOccupancy_Crate;
-    std::map<uint32_t, MonitorElement*> _cOccupancy_CrateSlot;
-    std::map<uint32_t, MonitorElement*> _cZDC_SUMS;
-    std::map<uint32_t, MonitorElement*> _cZDC_BXSUMS;
-    std::map<uint32_t, MonitorElement*> _cZDC_BX_EmuSUMS;
-    std::map<uint32_t, MonitorElement*> _cZDC_SumComp;
-    std::map<uint32_t, MonitorElement*> _cZDC_CapIDS;
-    std::map<uint32_t, MonitorElement*> _cfC_EChannel;
-    std::map<uint32_t, MonitorElement*> _cTDC_EChannel;
-    std::map<uint32_t, MonitorElement*> _cfC_vs_TS_EChannel;
-    std::map<uint32_t, MonitorElement*> _cZDC_HAD_TM;
-    std::map<uint32_t, MonitorElement*> _cZDC_EM_TM;
+  void _process(edm::Event const &, edm::EventSetup const &) override;
 
-    std::unique_ptr<HcalLongRecoParams> longRecoParams_;
-    
+  //	tags
+  edm::InputTag _tagQIE10;
+  edm::InputTag sumTag;
+  edm::EDGetTokenT<QIE10DigiCollection> _tokQIE10;
+  edm::ESGetToken<HcalDbService, HcalDbRecord> hcalDbServiceToken_;
+  edm::EDGetToken sumToken_;
+  edm::ESGetToken<HcalTopology, HcalRecNumberingRecord> htopoToken_;
+  edm::ESGetToken<HcalLongRecoParams, HcalLongRecoParamsRcd> paramsToken_;
+  //edm::EDGetTokenT<l1t::EtSumBxCollection> sumZDCToken_;
 
-    };
+  //emap
+  hcaldqm::electronicsmap::ElectronicsMap _ehashmap;  // online only
+
+  //	hcaldqm::Containers
+  std::map<uint32_t, MonitorElement *> _cADC_EChannel;
+  std::map<uint32_t, MonitorElement *> _cADC_vs_TS_EChannel;
+  std::map<uint32_t, MonitorElement *> _cDigiSize_Crate;
+  std::map<uint32_t, MonitorElement *> _cDigiSize_FED;
+  std::map<uint32_t, MonitorElement *> _cADC_PM;
+  std::map<uint32_t, MonitorElement *> _cADC_vs_TS_PM;
+  std::map<uint32_t, MonitorElement *> _cOccupancy_FEDuTCA;
+  std::map<uint32_t, MonitorElement *> _cOccupancy_ElectronicsuTCA;
+  std::map<uint32_t, MonitorElement *> _cOccupancy_Crate;
+  std::map<uint32_t, MonitorElement *> _cOccupancy_CrateSlot;
+  std::map<uint32_t, MonitorElement *> _cZDC_SUMS;
+  std::map<uint32_t, MonitorElement *> _cZDC_BXSUMS;
+  std::map<uint32_t, MonitorElement *> _cZDC_BX_EmuSUMS;
+  std::map<uint32_t, MonitorElement *> _cZDC_SumComp;
+  std::map<uint32_t, MonitorElement *> _cZDC_CapIDS;
+  std::map<uint32_t, MonitorElement *> _cfC_EChannel;
+  std::map<uint32_t, MonitorElement *> _cTDC_EChannel;
+  std::map<uint32_t, MonitorElement *> _cfC_vs_TS_EChannel;
+  std::map<uint32_t, MonitorElement *> _cZDC_HAD_TM;
+  std::map<uint32_t, MonitorElement *> _cZDC_EM_TM;
+
+  std::unique_ptr<HcalLongRecoParams> longRecoParams_;
+};
 
 #endif
