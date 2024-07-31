df499eb0a500fc0aa8cfc94a841881fed075b000
Ryan McCarthy <ryan.edward.mccarthy@cern.ch>
2024-07-31 17:44:03 +0200

    Removing track selection module and moving cuts to python config. Fixed bug in track truth association which affected BDT training boolean

--- L1Trigger/L1TTrackMatch/python/DisplacedVertexProducer_cfi.py
+++ L1Trigger/L1TTrackMatch/python/DisplacedVertexProducer_cfi.py
@@ -1,11 +1,27 @@
 import FWCore.ParameterSet.Config as cms
 
 DisplacedVertexProducer = cms.EDProducer('DisplacedVertexProducer',
-  l1TracksInputTag = cms.InputTag("l1tTrackSelectionProducerExtendedForDispVert", "Level1TTTracksSelected"),
+  l1TracksInputTag = cms.InputTag("l1tTTTracksFromExtendedTrackletEmulation", "Level1TTTracks"),
   l1TrackVertexCollectionName = cms.string("dispVertices"),
   mcTruthTrackInputTag = cms.InputTag("TTTrackAssociatorFromPixelDigisExtended", "Level1TTTracks"),
   ONNXmodel = cms.string("/afs/cern.ch/user/r/rmccarth/private/dispVert/l1tOfflinePR/CMSSW_14_0_0_pre3/src/L1Trigger/L1TTrackMatch/test/dispVertSlim_model.onnx"),
-  ONNXInputName = cms.string("feature_input")
+  ONNXInputName = cms.string("feature_input"),
+  cutSet = cms.PSet(
+      chi2rzMax = cms.double(3.0), # chi2rz value for all tracks must be less than this
+      dispMVAMin = cms.double(0.2), # displaced track quality MVA score for all tracks must be greater than this
+      promptMVAMin = cms.double(0.2), # prompt track quality MVA score for all tracks must be greater than this
+      ptMin = cms.double(3.0), # pt value for all tracks must be greater than this [GeV]
+      etaMax = cms.double(2.4), # eta value for all tracks must be less than this
+      dispD0Min = cms.double(1.0), # d0 value for tracks to be considered for displaced track cuts must be greater than this [cm]
+      promptMVADispTrackMin = cms.double(0.5), # prompt track quality MVA score for tracks with |d0|>dispD0Min must be greater than this
+      overlapEtaMin = cms.double(1.1), # eta value for tracks to be considered for overlap track cuts must be greater than this
+      overlapEtaMax = cms.double(1.7), # eta value for tracks to be considered for overlap track cuts must be less than this
+      overlapNStubsMin = cms.int32(4), # number of stubs for tracks with overlapEtaMin<|eta|<overlapEtaMax must be greater than this
+      diskEtaMin = cms.double(0.95), # eta value for tracks to be considered for disk track cuts must be greater than this
+      diskD0Min = cms.double(0.08), # abs d0 value for tracks with |eta|>diskEtaMin must be greater than this [cm]
+      barrelD0Min = cms.double(0.06) # abs d0 value for tracks with |eta|<=diskEtaMin must be greater than this [cm]
+      
+  ) 
 )
 
 '''
