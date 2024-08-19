6d2d558f2e69086e16e5ae9423cf5c90ac853f7a
Benedikt Tor Kristinsson <bkristinsson@benediktkristinssonsummerstudent.dyndns.cern.ch>
2024-08-19 11:11:49 +0200

    Test of readout of linting

--- FWCore/Framework/python/test/cmsExceptionsFatal_cff.py
+++ FWCore/Framework/python/test/cmsExceptionsFatal_cff.py
@@ -3,5 +3,9 @@ import FWCore.ParameterSet.Config as cms
 import FWCore.Framework.test.cmsExceptionsFatalOption_cff
 
 options = cms.untracked.PSet(
-  Rethrow = FWCore.Framework.test.cmsExceptionsFatalOption_cff.Rethrow
+    Rethrow=FWCore.Framework.test.cmsExceptionsFatalOption_cff.Rethrow
 )
+#test
+y =10
+x=2
+print(x)
