9d571ef9c585487e04a8d31056663d283f6123b3
mmusich <marco.musich@cern.ch>
2024-08-25 15:07:29 +0200

    support value plotting from input file

--- DQM/TrackerRemapper/bin/printPixelTrackerMap.cc
+++ DQM/TrackerRemapper/bin/printPixelTrackerMap.cc
@@ -1,50 +1,69 @@
 #include "DQM/TrackerRemapper/interface/Phase1PixelSummaryMap.h"
+#include <cstdlib>
+#include <fstream>
 #include <iostream>
 #include <numeric>  // std::accumulate
-#include <iostream>
-#include <vector>
 #include <sstream>
 #include <string>
+#include <vector>
 
 #include "TCanvas.h"
 #include "TStyle.h"
 
 int main(int argc, char* argv[]) {
-  gStyle->SetPalette(1);
-  // Check if detids are provided as command line arguments
-  if (argc < 2) {
-    std::cerr << "Usage: " << argv[0] << " detid1 detid2 detid3 ..." << std::endl;
-    return 1;
+  std::string inputFile;
+  std::vector<std::pair<uint32_t, float>> detidValues;
+
+  // Parse command line arguments
+  for (int i = 1; i < argc; ++i) {
+    if (std::string(argv[i]) == "--input-file" && i + 1 < argc) {
+      gStyle->SetPalette(kRainbow);
+      gStyle->SetNumberContours(256);
+      inputFile = argv[++i];
+    } else {
+      gStyle->SetPalette(1);
+      // Treat as DetId list if no --input-file is provided
+      uint32_t detid = std::stoul(argv[i]);
+      detidValues.emplace_back(detid, 1.0);  // Default value is 1.0
+    }
   }
 
-  // Convert command line arguments to a vector of uint32_t detids
-  std::vector<uint32_t> detids;
-  for (int i = 1; i < argc; ++i) {
-    uint32_t detid;
-    std::stringstream ss(argv[i]);
-    if (!(ss >> detid)) {
-      std::cerr << "Error: Invalid detid: " << argv[i] << std::endl;
+  // If --input-file is provided, read from file
+  if (!inputFile.empty()) {
+    std::ifstream file(inputFile);
+    if (!file) {
+      std::cerr << "Error: Unable to open input file " << inputFile << std::endl;
       return 1;
     }
-    detids.push_back(detid);
+
+    std::string line;
+    while (std::getline(file, line)) {
+      std::istringstream iss(line);
+      uint32_t detid;
+      float value = 1.0;  // Default value
+
+      iss >> detid;
+      if (iss >> value) {  // If a second column exists, read it as value
+        detidValues.emplace_back(detid, value);
+      } else {
+        detidValues.emplace_back(detid, 1.0);
+      }
+    }
   }
 
-  // Create the Phase1PixelSummaryMap object
-  Phase1PixelSummaryMap theMap("colz", "Marked Pixel Modules", "");
-  
-  // Create the tracker base map
+  // Create the map and fill it
+  Phase1PixelSummaryMap theMap("colz", "Marked Pixel Modules", "input values");
   theMap.createTrackerBaseMap();
 
-  // Fill the tracker map with provided detids
-  for (const auto& detid : detids) {
-    theMap.fillTrackerMap(detid, 1.0);
+  for (const auto& [detid, value] : detidValues) {
+    theMap.fillTrackerMap(detid, value);
   }
 
   TCanvas c = TCanvas("c", "c", 3000, 2000);
   theMap.printTrackerMap(c);
   c.SaveAs("Phase1PixelSummaryMap.png");
 
-  std::cout << "Filled tracker map with " << detids.size() << " detids." << std::endl;
+  std::cout << "Filled tracker map with " << detidValues.size() << " detids." << std::endl;
 
   return 0;
 }
