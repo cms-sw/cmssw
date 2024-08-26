a6f4580d0612bc24bcc37c33ab501b9d0a20cb5f
mmusich <marco.musich@cern.ch>
2024-08-26 08:43:07 +0200

    Update Pixel GPU DQM online client

--- DQM/SiPixelHeterogeneous/plugins/SiPixelCompareTracks.cc
+++ DQM/SiPixelHeterogeneous/plugins/SiPixelCompareTracks.cc
@@ -11,6 +11,7 @@
 //
 
 // for string manipulations
+#include <algorithm>
 #include <fmt/printf.h>
 #include "DataFormats/Common/interface/Handle.h"
 #include "DataFormats/Math/interface/deltaR.h"
@@ -94,6 +95,9 @@ private:
   MonitorElement* hnTracks_;
   MonitorElement* hnLooseAndAboveTracks_;
   MonitorElement* hnLooseAndAboveTracks_matched_;
+  MonitorElement* hDeltaNTracks_;
+  MonitorElement* hDeltaNLooseAndAboveTracks_;
+  MonitorElement* hDeltaNLooseAndAboveTracks_matched_;
   MonitorElement* hnHits_;
   MonitorElement* hnHitsVsPhi_;
   MonitorElement* hnHitsVsEta_;
@@ -105,6 +109,7 @@ private:
   MonitorElement* hChi2VsPhi_;
   MonitorElement* hChi2VsEta_;
   MonitorElement* hpt_;
+  MonitorElement* hCurvature_;
   MonitorElement* hptLogLog_;
   MonitorElement* heta_;
   MonitorElement* hphi_;
@@ -200,6 +205,7 @@ void SiPixelCompareTracks<T>::analyzeSeparate(U tokenRef, V tokenTar, const edm:
     float phiRef = reco::phi(tsoaRef.view(), it);
     float zipRef = reco::zip(tsoaRef.view(), it);
     float tipRef = reco::tip(tsoaRef.view(), it);
+    auto qRef = reco::charge(tsoaRef.view(), it);
 
     if (!(ptRef > 0.))
       continue;
@@ -231,17 +237,18 @@ void SiPixelCompareTracks<T>::analyzeSeparate(U tokenRef, V tokenTar, const edm:
     nLooseAndAboveTracksRef_matchedTar++;
 
     hchi2_->Fill(tsoaRef.view()[it].chi2(), tsoaTar.view()[closestTkidx].chi2());
-    hCharge_->Fill(reco::charge(tsoaRef.view(), it), reco::charge(tsoaTar.view(), closestTkidx));
+    hCharge_->Fill(qRef, reco::charge(tsoaTar.view(), closestTkidx));
     hnHits_->Fill(helper::nHits(tsoaRef.view(), it), helper::nHits(tsoaTar.view(), closestTkidx));
     hnLayers_->Fill(tsoaRef.view()[it].nLayers(), tsoaTar.view()[closestTkidx].nLayers());
     hpt_->Fill(ptRef, tsoaTar.view()[closestTkidx].pt());
+    hCurvature_->Fill(qRef / ptRef, reco::charge(tsoaTar.view(), closestTkidx) / tsoaTar.view()[closestTkidx].pt());
     hptLogLog_->Fill(ptRef, tsoaTar.view()[closestTkidx].pt());
     heta_->Fill(etaRef, tsoaTar.view()[closestTkidx].eta());
     hphi_->Fill(phiRef, reco::phi(tsoaTar.view(), closestTkidx));
     hz_->Fill(zipRef, reco::zip(tsoaTar.view(), closestTkidx));
     htip_->Fill(tipRef, reco::tip(tsoaTar.view(), closestTkidx));
     hptdiffMatched_->Fill(ptRef - tsoaTar.view()[closestTkidx].pt());
-    hCurvdiffMatched_->Fill((reco::charge(tsoaRef.view(), it) / tsoaRef.view()[it].pt()) -
+    hCurvdiffMatched_->Fill(qRef / ptRef -
                             (reco::charge(tsoaTar.view(), closestTkidx) / tsoaTar.view()[closestTkidx].pt()));
     hetadiffMatched_->Fill(etaRef - tsoaTar.view()[closestTkidx].eta());
     hphidiffMatched_->Fill(reco::deltaPhi(phiRef, reco::phi(tsoaTar.view(), closestTkidx)));
@@ -250,9 +257,23 @@ void SiPixelCompareTracks<T>::analyzeSeparate(U tokenRef, V tokenTar, const edm:
     hpt_eta_tkAllRefMatched_->Fill(etaRef, tsoaRef.view()[it].pt());  //matched to gpu
     hphi_z_tkAllRefMatched_->Fill(etaRef, zipRef);
   }
-  hnTracks_->Fill(nTracksRef, nTracksTar);
-  hnLooseAndAboveTracks_->Fill(nLooseAndAboveTracksRef, nLooseAndAboveTracksTar);
-  hnLooseAndAboveTracks_matched_->Fill(nLooseAndAboveTracksRef, nLooseAndAboveTracksRef_matchedTar);
+
+  // Define a lambda function for filling the histograms
+  auto fillHistogram = [](auto& histogram, auto xValue, auto yValue) { histogram->Fill(xValue, yValue); };
+
+  // Define a lambda for filling delta histograms
+  auto fillDeltaHistogram = [](auto& histogram, int cpuValue, int gpuValue) {
+    histogram->Fill(std::min(cpuValue, 1000), std::clamp(gpuValue - cpuValue, -100, 100));
+  };
+
+  // Fill the histograms
+  fillHistogram(hnTracks_, nTracksRef, nTracksTar);
+  fillHistogram(hnLooseAndAboveTracks_, nLooseAndAboveTracksRef, nLooseAndAboveTracksTar);
+  fillHistogram(hnLooseAndAboveTracks_matched_, nLooseAndAboveTracksRef, nLooseAndAboveTracksRef_matchedTar);
+
+  fillDeltaHistogram(hDeltaNTracks_, nTracksRef, nTracksTar);
+  fillDeltaHistogram(hDeltaNLooseAndAboveTracks_, nLooseAndAboveTracksRef, nLooseAndAboveTracksTar);
+  fillDeltaHistogram(hDeltaNLooseAndAboveTracks_matched_, nLooseAndAboveTracksRef, nLooseAndAboveTracksRef_matchedTar);
 }
 
 //
@@ -275,13 +296,44 @@ void SiPixelCompareTracks<T>::bookHistograms(DQMStore::IBooker& iBook,
   iBook.cd();
   iBook.setCurrentFolder(topFolderName_);
 
-  // clang-format off
+  // Define a helper function for booking histograms
   std::string toRep = "Number of tracks";
+  auto bookTracksTH2I = [&](const std::string& name,
+                            const std::string& title,
+                            int xBins,
+                            double xMin,
+                            double xMax,
+                            int yBins,
+                            double yMin,
+                            double yMax) {
+    return iBook.book2I(name, fmt::sprintf(title, toRep), xBins, xMin, xMax, yBins, yMin, yMax);
+  };
+
+  // Define common parameters for different histogram types
+  constexpr int xBins = 501;
+  constexpr double xMin = -0.5;
+  constexpr double xMax = 1001.5;
+
+  constexpr int dXBins = 1001;
+  constexpr double dXMin = -0.5;
+  constexpr double dXMax = 1000.5;
+
+  constexpr int dYBins = 201;
+  constexpr double dYMin = -100.5;
+  constexpr double dYMax = 100.5;
+
   // FIXME: all the 2D correlation plots are quite heavy in terms of memory consumption, so a as soon as DQM supports THnSparse
   // these should be moved to a less resource consuming format
-  hnTracks_ = iBook.book2I("nTracks", fmt::sprintf("%s per event; Reference; Target",toRep), 501, -0.5, 500.5, 501, -0.5, 500.5);
-  hnLooseAndAboveTracks_ = iBook.book2I("nLooseAndAboveTracks", fmt::sprintf("%s (quality #geq loose) per event; Reference; Target",toRep), 501, -0.5, 500.5, 501, -0.5, 500.5);
-  hnLooseAndAboveTracks_matched_ = iBook.book2I("nLooseAndAboveTracks_matched", fmt::sprintf("%s (quality #geq loose) per event; Reference; Target",toRep), 501, -0.5, 500.5, 501, -0.5, 500.5);
+
+  // Book histograms using the helper function
+  // clang-format off
+  hnTracks_ = bookTracksTH2I("nTracks", "%s per event; Reference; Target", xBins, xMin, xMax, xBins, xMin, xMax);
+  hnLooseAndAboveTracks_ = bookTracksTH2I("nLooseAndAboveTracks", "%s (quality #geq loose) per event; Reference; Target", xBins, xMin, xMax, xBins, xMin, xMax);
+  hnLooseAndAboveTracks_matched_ = bookTracksTH2I("nLooseAndAboveTracks_matched", "%s (quality #geq loose) per event; Reference; Target", xBins, xMin, xMax, xBins, xMin, xMax);
+
+  hDeltaNTracks_ = bookTracksTH2I("deltaNTracks", "%s per event; Reference; Target - Reference", dXBins, dXMin, dXMax, dYBins, dYMin, dYMax);
+  hDeltaNLooseAndAboveTracks_ = bookTracksTH2I("deltaNLooseAndAboveTracks", "%s (quality #geq loose) per event; Reference; Target - Reference", dXBins, dXMin, dXMax, dYBins, dYMin, dYMax);
+  hDeltaNLooseAndAboveTracks_matched_ = bookTracksTH2I("deltaNLooseAndAboveTracks_matched", "%s (quality #geq loose) per event; Reference; Target - Reference", dXBins, dXMin, dXMax, dYBins, dYMin, dYMax);
 
   toRep = "Number of all RecHits per track (quality #geq loose)";
   hnHits_ = iBook.book2I("nRecHits", fmt::sprintf("%s;Reference;Target",toRep), 15, -0.5, 14.5, 15, -0.5, 14.5);
@@ -296,18 +348,20 @@ void SiPixelCompareTracks<T>::bookHistograms(DQMStore::IBooker& iBook,
   hCharge_ = iBook.book2I("charge",fmt::sprintf("%s;Reference;Target",toRep),3, -1.5, 1.5, 3, -1.5, 1.5);
 
   hpt_ = iBook.book2I("pt", "Track (quality #geq loose) p_{T} [GeV];Reference;Target", 200, 0., 200., 200, 0., 200.);
+  hCurvature_ = iBook.book2I("curvature", "Track (quality #geq loose) q/p_{T} [GeV^{-1}];Reference;Target",  60,- 3., 3., 60, -3., 3. );
   hptLogLog_ = make2DIfLog(iBook, true, true, "ptLogLog", "Track (quality #geq loose) p_{T} [GeV];Reference;Target", 200, log10(0.5), log10(200.), 200, log10(0.5), log10(200.));
   heta_ = iBook.book2I("eta", "Track (quality #geq loose) #eta;Reference;Target", 30, -3., 3., 30, -3., 3.);
   hphi_ = iBook.book2I("phi", "Track (quality #geq loose) #phi;Reference;Target", 30, -M_PI, M_PI, 30, -M_PI, M_PI);
   hz_ = iBook.book2I("z", "Track (quality #geq loose) z [cm];Reference;Target", 30, -30., 30., 30, -30., 30.);
   htip_ = iBook.book2I("tip", "Track (quality #geq loose) TIP [cm];Reference;Target", 100, -0.5, 0.5, 100, -0.5, 0.5);
+
   //1D difference plots
-  hptdiffMatched_ = iBook.book1D("ptdiffmatched", " p_{T} diff [GeV] between matched tracks; #Delta p_{T} [GeV]", 60, -30., 30.);
-  hCurvdiffMatched_ = iBook.book1D("curvdiffmatched", "q/p_{T} diff [GeV] between matched tracks; #Delta q/p_{T} [GeV]", 60, -30., 30.);
-  hetadiffMatched_ = iBook.book1D("etadiffmatched", " #eta diff between matched tracks; #Delta #eta", 160, -0.04 ,0.04);
-  hphidiffMatched_ = iBook.book1D("phidiffmatched", " #phi diff between matched tracks; #Delta #phi",  160, -0.04 ,0.04);
-  hzdiffMatched_ = iBook.book1D("zdiffmatched", " z diff between matched tracks; #Delta z [cm]", 300, -1.5, 1.5);
-  htipdiffMatched_ = iBook.book1D("tipdiffmatched", " TIP diff between matched tracks; #Delta TIP [cm]", 300, -1.5, 1.5);
+  hptdiffMatched_ = iBook.book1D("ptdiffmatched", " p_{T} diff [GeV] between matched tracks; #Delta p_{T} [GeV]", 61, -30.5, 30.5);
+  hCurvdiffMatched_ = iBook.book1D("curvdiffmatched", "q/p_{T} diff [GeV^{-1}] between matched tracks; #Delta q/p_{T} [GeV^{-1}]", 61, -3.05, 3.05);
+  hetadiffMatched_ = iBook.book1D("etadiffmatched", " #eta diff between matched tracks; #Delta #eta", 161, -0.045 ,0.045);
+  hphidiffMatched_ = iBook.book1D("phidiffmatched", " #phi diff between matched tracks; #Delta #phi",  161, -0.045 ,0.045);
+  hzdiffMatched_ = iBook.book1D("zdiffmatched", " z diff between matched tracks; #Delta z [cm]", 301, -1.55, 1.55);
+  htipdiffMatched_ = iBook.book1D("tipdiffmatched", " TIP diff between matched tracks; #Delta TIP [cm]", 301, -1.55, 1.55);
   //2D plots for eff
   hpt_eta_tkAllRef_ = iBook.book2I("ptetatrkAllReference", "Track (quality #geq loose) on Reference; #eta; p_{T} [GeV];", 30, -M_PI, M_PI, 200, 0., 200.);
   hpt_eta_tkAllRefMatched_ = iBook.book2I("ptetatrkAllReferencematched", "Track (quality #geq loose) on Reference matched to Target track; #eta; p_{T} [GeV];", 30, -M_PI, M_PI, 200, 0., 200.);
@@ -326,7 +380,7 @@ void SiPixelCompareTracks<T>::fillDescriptions(edm::ConfigurationDescriptions& d
   desc.add<std::string>("topFolderName", "SiPixelHeterogeneous/PixelTrackCompareDeviceVSHost");
   desc.add<bool>("useQualityCut", true);
   desc.add<std::string>("minQuality", "loose");
-  desc.add<double>("deltaR2cut", 0.04);
+  desc.add<double>("deltaR2cut", 0.02 * 0.02)->setComment("deltaR2 cut between track on device and host");
   descriptions.addWithDefaultLabel(desc);
 }
 
