eb18cb3dfcdec93bb3bb2a35edabdb83704c6533
Andrea Bocci <andrea.bocci@cern.ch>
2024-09-02 18:37:44 +0200

    Remove legacy modules for CUDA pixel cluster reconstruction

--- CalibTracker/SiPixelESProducers/src/ES_SiPixelROCsStatusAndMappingWrapper.cc
+++
@@ -1,4 +0,0 @@
-#include "CalibTracker/SiPixelESProducers/interface/SiPixelROCsStatusAndMappingWrapper.h"
-#include "FWCore/Utilities/interface/typelookup.h"
-
-TYPELOOKUP_DATA_REG(SiPixelROCsStatusAndMappingWrapper);
--- RecoLocalTracker/SiPixelClusterizer/test/cpuClustering_t.cpp
+++
@@ -1,1 +0,0 @@
-#include "gpuClustering_t.h"
