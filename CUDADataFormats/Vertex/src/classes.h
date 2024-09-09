96c0f21c437463bc6b81e9b807b03c3bed8a3e6a
Andrea Bocci <andrea.bocci@cern.ch>
2024-09-09 23:42:16 +0200

    Remove legacy modules for CUDA pixel vertex reconstruction and DQM

--- CUDADataFormats/Vertex/src/classes.h
+++
@@ -1,9 +0,0 @@
-#ifndef CUDADataFormats_Vertex_src_classes_h
-#define CUDADataFormats_Vertex_src_classes_h
-
-#include "CUDADataFormats/Vertex/interface/ZVertexSoAHeterogeneousDevice.h"
-#include "CUDADataFormats/Vertex/interface/ZVertexSoAHeterogeneousHost.h"
-#include "CUDADataFormats/Common/interface/Product.h"
-#include "DataFormats/Common/interface/Wrapper.h"
-
-#endif  // CUDADataFormats_Vertex_src_classes_h
