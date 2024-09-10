e211eb80a6a9d5de8f84419a0ed86bd855866a4e
Andrea Bocci <andrea.bocci@cern.ch>
2024-09-10 12:33:17 +0200

    Remove legacy modules for CUDA pixel error unpacking

--- EventFilter/SiPixelRawToDigi/python/siPixelDigis_cff.py
+++ EventFilter/SiPixelRawToDigi/python/siPixelDigis_cff.py
@@ -3,11 +3,9 @@ import FWCore.ParameterSet.Config as cms
 from EventFilter.SiPixelRawToDigi.SiPixelRawToDigi_cfi import siPixelDigis
 
 siPixelDigisTask = cms.Task(
-    # SwitchProducer wrapping the legacy pixel digis producer or an alias combining the pixel digis information converted from SoA
     siPixelDigis
 )
 
-# Phase 2 Tracker Modifier
+# FIXME remove siPixelDigis until we have Phase 2 pixel digis
 from Configuration.Eras.Modifier_phase2_tracker_cff import phase2_tracker
-# Remove siPixelDigis until we have phase2 pixel digis
-phase2_tracker.toReplaceWith(siPixelDigisTask, cms.Task()) #FIXME
+phase2_tracker.toReplaceWith(siPixelDigisTask, cms.Task())
