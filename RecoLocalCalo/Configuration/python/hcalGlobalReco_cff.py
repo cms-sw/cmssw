e5e64379870587593d0b2b50e45a6de3b97cd0c8
Andrea Bocci <andrea.bocci@cern.ch>
2024-09-14 00:57:05 +0200

    Remove legacy CUDA modules for HCAL local reconstruction

--- RecoLocalCalo/Configuration/python/hcalGlobalReco_cff.py
+++ RecoLocalCalo/Configuration/python/hcalGlobalReco_cff.py
@@ -1,13 +1,9 @@
 import FWCore.ParameterSet.Config as cms
-from HeterogeneousCore.CUDACore.SwitchProducerCUDA import SwitchProducerCUDA
 
 #--- for Run 1 and Run 2
 from RecoLocalCalo.HcalRecProducers.HBHEIsolatedNoiseReflagger_cfi import hbhereco as _phase0_hbhereco
-hbhereco = SwitchProducerCUDA(
-    cpu = _phase0_hbhereco.clone()
-)
+hbhereco = _phase0_hbhereco.clone()
 hbherecoLegacy = _phase0_hbhereco.clone()
-
 
 hcalGlobalRecoTask = cms.Task(hbhereco)
 hcalGlobalRecoSequence = cms.Sequence(hcalGlobalRecoTask)
@@ -22,26 +18,17 @@ hcalOnlyLegacyGlobalRecoTask = cms.Task()
 from Configuration.Eras.Modifier_run3_HB_cff import run3_HB
 
 from RecoLocalCalo.HcalRecProducers.HBHEPhase1Reconstructor_cfi import hbheprereco as _phase1_hbheprereco
-run3_HB.toReplaceWith(hbhereco.cpu, _phase1_hbheprereco)
+run3_HB.toReplaceWith(hbhereco, _phase1_hbheprereco)
 run3_HB.toReplaceWith(hcalOnlyGlobalRecoTask, cms.Task(hbhereco))
 run3_HB.toReplaceWith(hbherecoLegacy, _phase1_hbheprereco)
 run3_HB.toReplaceWith(hcalOnlyLegacyGlobalRecoTask, cms.Task(hbherecoLegacy))
 
-
 #--- for Run 3 on GPU
-from Configuration.ProcessModifiers.gpu_cff import gpu
 from Configuration.ProcessModifiers.alpaka_cff import alpaka
 
-from RecoLocalCalo.HcalRecProducers.hcalCPURecHitsProducer_cfi import hcalCPURecHitsProducer as _hbherecoFromCUDA
-(run3_HB & gpu).toModify(hbhereco,
-    cuda = _hbherecoFromCUDA.clone(
-        produceSoA = False
-    )
-)
-
 from RecoLocalCalo.HcalRecProducers.hcalRecHitSoAToLegacy_cfi import  hcalRecHitSoAToLegacy 
-(alpaka & run3_HB).toModify(hbhereco,
-    cpu = hcalRecHitSoAToLegacy.clone(
+(alpaka & run3_HB).toReplaceWith(hbhereco,
+    hcalRecHitSoAToLegacy.clone(
         src = ("hbheRecHitProducerPortable","")
     )
 )
