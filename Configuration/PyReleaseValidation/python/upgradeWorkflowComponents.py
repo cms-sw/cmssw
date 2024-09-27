8cc135f41d3b9cc2929309dc472a6d943595e213
Kevin Pedro <kpedro88@gmail.com>
2024-09-27 13:52:00 -0500

    fix mistake in regex

--- Configuration/PyReleaseValidation/python/upgradeWorkflowComponents.py
+++ Configuration/PyReleaseValidation/python/upgradeWorkflowComponents.py
@@ -2511,9 +2511,9 @@ class UpgradeWorkflowPremix(UpgradeWorkflow):
                 d = merge([stepDict[self.getStepName(step)][k]])
                 if "_PMXS1S2" in self.suffix and "--filein" in d:
                     filein = d["--filein"]
-                    m = re.search("step(?P<ind>\\d+)_", filein)
+                    m = re.search("step(?P<ind>\\d+)", filein)
                     if m:
-                        d["--filein"] = filein.replace(m.group(), "step%d_"%(int(m.group("ind"))+1))
+                        d["--filein"] = filein.replace(m.group(), "step%d"%(int(m.group("ind"))+1))
                 stepDict[stepName][k] = d
                 # run2/3 WFs use Nano (not NanoPU) in PU WF
                 stepDict[self.getStepName(step)][k] = merge([d])
