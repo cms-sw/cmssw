069ca5905e53a0576b8a2183221b05db8c27abfa
Ivan Razumov <iarspider@gmail.com>
2024-10-01 10:48:10 +0200

    [L1] Fix syntax in python scripts

--- L1Trigger/L1TMuonEndCap/test/tools/make_coordlut_MC.py
+++ L1Trigger/L1TMuonEndCap/test/tools/make_coordlut_MC.py
@@ -8,7 +8,7 @@ process.load("Configuration.StandardSequences.FrontierConditions_GlobalTag_cff")
 
 from Configuration.AlCa.GlobalTag import GlobalTag
 process.GlobalTag = GlobalTag(process.GlobalTag, 'auto:run2_mc', '')
-print "Using GlobalTag: %s" % process.GlobalTag.globaltag.value()
+print("Using GlobalTag: %s" % process.GlobalTag.globaltag.value())
 
 # Fake alignment is/should be ideal geometry
 # ==========================================
