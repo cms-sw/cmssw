34390264db24ed83415f0677cd71eb2226cb2099
Ivan Razumov <iarspider@gmail.com>
2024-10-01 10:12:50 +0200

    [DQM] Fix syntax in python scripts

--- Validation/HcalHits/test/ZdcSimHitStudy_cfg.py
+++ Validation/HcalHits/test/ZdcSimHitStudy_cfg.py
@@ -45,7 +45,7 @@ process.load("DQMServices.Components.DQMEnvironment_cfi")
 process.dqmsave_step = cms.Path(process.DQMSaver)
 
 
-print process.ZDCDigiStudy.Verbose
+print(process.ZDCDigiStudy.Verbose)
 process.p1 = cms.Path(
     process.ZDCDigiStudy
     *process.zdcSimHitStudy)
