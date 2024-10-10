dab7cb3a31c012bd20276083c060e9e48f22b037
mmusich <marco.musich@cern.ch>
2024-10-10 14:45:00 +0200

    add DQM / Validation monitoring for hltInitialStep and  hltHighPtTripletTracks in the Phase2 HLT menu

--- Configuration/EventContent/python/EventContent_cff.py
+++ Configuration/EventContent/python/EventContent_cff.py
@@ -682,6 +682,8 @@ phase2_tracker.toModify(FEVTDEBUGHLTEventContent,
                             'keep *_hltPhase2PixelTracks_*_*',
                             'keep *_hltPhase2PixelVertices_*_*',
                             'keep *_hltGeneralTracks_*_*',
+                            'keep *_hltInitialStepTrackSelectionHighPurity_*_*',
+                            'keep *_hltHighPtTripletStepTrackSelectionHighPurity_*_*',
                             'keep *_hltOfflinePrimaryVertices_*_*',
                             'keep *_hltHGCalRecHit_*_*'
                         ])
