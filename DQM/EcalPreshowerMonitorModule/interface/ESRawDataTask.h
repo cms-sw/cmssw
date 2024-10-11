9ba611b7ba7113d25e2d1c955463cde4847ba762
Kyungmin Park <kyungmin.park@cern.ch>
2024-10-11 02:27:53 +0200

    Add ByLS DQM plots for ES

--- DQM/EcalPreshowerMonitorModule/interface/ESRawDataTask.h
+++ DQM/EcalPreshowerMonitorModule/interface/ESRawDataTask.h
@@ -9,8 +9,13 @@
 
 #include "DQMServices/Core/interface/DQMEDAnalyzer.h"
 #include "DQMServices/Core/interface/DQMStore.h"
+#include "DQMServices/Core/interface/DQMOneEDAnalyzer.h"
 
-class ESRawDataTask : public DQMEDAnalyzer {
+struct ESRawLSCache {
+  int ievtLS_;
+};
+
+class ESRawDataTask : public DQMOneEDAnalyzer<edm::LuminosityBlockCache<ESRawLSCache>> {
 public:
   ESRawDataTask(const edm::ParameterSet& ps);
   ~ESRawDataTask() override {}
@@ -22,6 +27,12 @@ protected:
   /// Setup
   void bookHistograms(DQMStore::IBooker&, edm::Run const&, edm::EventSetup const&) override;
 
+  /// Begin Lumi
+  std::shared_ptr<ESRawLSCache> globalBeginLuminosityBlock(const edm::LuminosityBlock& lumi,
+                                                           const edm::EventSetup& c) const override;
+  /// End Lumi
+  void globalEndLuminosityBlock(const edm::LuminosityBlock& lumi, const edm::EventSetup& c) override;
+
 private:
   int ievt_;
 
@@ -30,10 +41,12 @@ private:
   edm::EDGetTokenT<ESRawDataCollection> dccCollections_;
   edm::EDGetTokenT<FEDRawDataCollection> FEDRawDataCollection_;
 
-  //MonitorElement* meRunNumberErrors_;
   MonitorElement* meL1ADCCErrors_;
+  MonitorElement* meL1ADCCErrorsByLS_;
   MonitorElement* meBXDCCErrors_;
+  MonitorElement* meBXDCCErrorsByLS_;
   MonitorElement* meOrbitNumberDCCErrors_;
+  MonitorElement* meOrbitNumberDCCErrorsByLS_;
   MonitorElement* meL1ADiff_;
   MonitorElement* meBXDiff_;
   MonitorElement* meOrbitNumberDiff_;
