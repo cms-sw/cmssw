d8702a4c2d29d0f32e1a3a8f142759dc4d3c8234
mmusich <marco.musich@cern.ch>
2024-10-12 11:57:50 +0200

    improve the testCompare script, rename SiStripApvGainCompareByDiff plots

--- CondCore/SiStripPlugins/scripts/testCompare.sh
+++ CondCore/SiStripPlugins/scripts/testCompare.sh
@@ -1,4 +1,5 @@
-#!/bin/bash
+#!/bin/bash -e
+
 display_usage() { 
     echo "This script must be run giving the following arguments." 
     echo -e "./testCompare.sh <name of tag> <first IOV> <last IOV> <sqlite file> \n\n"
@@ -6,75 +7,99 @@ display_usage() {
 } 
 
 # if less than two arguments supplied, display usage 
-if [  $# -le 3 ]
-then
+if [  $# -le 3 ]; then
     display_usage
     exit 1
 fi
  
 # check whether user had supplied -h or --help . If yes display usage 
-if [[ ( $# == "--help") ||  $# == "-h" ]]
-then
+if [[ ( "$1" == "--help" ) ||  "$1" == "-h" ]]; then
     display_usage
     exit 0
 fi
 
 # Save current working dir so img can be outputted there later
-W_DIR=$(pwd);
+W_DIR=$(pwd)
 
 STARTIOV=$2
 ENDIOV=$3
 
-source /afs/cern.ch/cms/cmsset_default.sh;
-eval `scram run -sh`;
+source /cvmfs/cms.cern.ch/cmsset_default.sh
+eval "$(scram run -sh)"
+
 # Go back to original working directory
-cd $W_DIR;
+cd "$W_DIR"
 
-plotTypes=(SiStripApvGainsComparatorSingleTag SiStripApvGainsValuesComparatorSingleTag SiStripApvGainsComparatorByRegionSingleTag SiStripApvGainsRatioComparatorByRegionSingleTag SiStripApvGainByPartition)
+plotTypes=(
+    SiStripApvGainsComparatorSingleTag
+    SiStripApvGainsValuesComparatorSingleTag
+    SiStripApvGainsComparatorByRegionSingleTag
+    SiStripApvGainsRatioComparatorByRegionSingleTag
+    SiStripApvGainByPartition
+)
 
-mkdir -p $W_DIR/results_$2-$3
+mkdir -p "$W_DIR/results_$2-$3"
 
-if [ -f *.png ]; then    
+# Remove any existing PNG files
+if ls *.png 1> /dev/null 2>&1; then
     rm *.png
 fi
 
-for i in "${plotTypes[@]}" 
-do
-echo "Making plot ${i}"
-# Run get payload data script
-    getPayloadData.py \
-	--plugin pluginSiStripApvGain_PayloadInspector \
-	--plot plot_${i} \
-	--tag $1 \
-	--time_type Run \
-	--iovs  '{"start_iov": "'$STARTIOV'", "end_iov": "'$ENDIOV'"}' \
-	--db sqlite_file:$4 \
-	--test;
+for i in "${plotTypes[@]}"; do
+    echo "Making plot ${i}"
 
-    mv *.png $W_DIR/results_$2-$3/${i}_$1_$2-$3.png
+    # Run get payload data script
+    getPayloadData.py \
+        --plugin pluginSiStripApvGain_PayloadInspector \
+        --plot plot_${i} \
+        --tag "$1" \
+        --time_type Run \
+        --iovs '{"start_iov": "'$STARTIOV'", "end_iov": "'$ENDIOV'"}' \
+        --db sqlite_file:"$4" \
+        --test
+
+    # Check if the command failed
+    if [ $? -ne 0 ]; then
+        echo "Error in getPayloadData for plot ${i}, exiting..."
+        exit 1
+    fi
+
+    mv *.png "$W_DIR/results_$2-$3/${i}_$1_$2-$3.png"
 done
 
-plotTypes2=(SiStripApvGainCompareByPartition SiStripApvGainDiffByPartition)
+plotTypes2=(
+    SiStripApvGainCompareByPartitionTwoTags
+    SiStripApvGainDiffByPartitionTwoTags
+)
 
-for i in "${plotTypes2[@]}" 
-do
-echo "Making plot ${i}"
-# Run get payload data script
+for i in "${plotTypes2[@]}"; do
+    echo "Making plot ${i}"
+
+    # Run get payload data script
     getPayloadData.py \
-	--plugin pluginSiStripApvGain_PayloadInspector \
-	--plot plot_${i} \
-	--tag $1 \
-	--tagtwo $1 \
-	--time_type Run \
-	--iovs '{"start_iov": "'$STARTIOV'", "end_iov": "'$STARTIOV'"}' \
-	--iovstwo '{"start_iov": "'$ENDIOV'", "end_iov": "'$ENDIOV'"}' \
-	--db sqlite_file:$4 \
-	--test;
+        --plugin pluginSiStripApvGain_PayloadInspector \
+        --plot plot_${i} \
+        --tag "$1" \
+        --tagtwo "$1" \
+        --time_type Run \
+        --iovs '{"start_iov": "'$STARTIOV'", "end_iov": "'$STARTIOV'"}' \
+        --iovstwo '{"start_iov": "'$ENDIOV'", "end_iov": "'$ENDIOV'"}' \
+        --db sqlite_file:"$4" \
+        --test
 
-    mv *.png $W_DIR/results_$2-$3/${i}_$1_$2-$3.png
+    # Check if the command failed
+    if [ $? -ne 0 ]; then
+        echo "Error in getPayloadData for plot ${i}, exiting..."
+        exit 1
+    fi
+
+    mv *.png "$W_DIR/results_$2-$3/${i}_$1_$2-$3.png"
 done
 
-plotTypes3=(SiStripApvGainsAvgDeviationRatioWithPreviousIOVTrackerMap SiStripApvGainsMaxDeviationRatioWithPreviousIOVTrackerMap)
+plotTypes3=(
+    SiStripApvGainsAvgDeviationRatioWithPreviousIOVTrackerMap
+    SiStripApvGainsMaxDeviationRatioWithPreviousIOVTrackerMap
+)
 
 for i in "${plotTypes3[@]}"
 do
@@ -90,8 +115,14 @@ do
 	    --iovs  '{"start_iov": "'$STARTIOV'", "end_iov": "'$ENDIOV'"}' \
 	    --input_params '{"nsigma":"'${j}'"}' \
 	    --db sqlite_file:$4 \
-	    --test;
+	    --test
 
-	mv *.png $W_DIR/results_$2-$3/${i}_${j}sigmas_$1_$2-$3.png
+	# Check if the command failed
+	if [ $? -ne 0 ]; then
+            echo "Error in getPayloadData for plot ${i}, exiting..."
+            exit 1
+	fi
+
+	mv *.png "$W_DIR/results_$2-$3/${i}_${j}sigmas_$1_$2-$3.png"
     done
 done
