923ba3a41febb8c4231824af5ab9606ecabb0fbe
mmusich <marco.musich@cern.ch>
2024-10-18 11:47:11 +0200

    fixReading_12_4_X_Files when reconstructing data from 12_4_X_HLT

--- Configuration/PyReleaseValidation/test/test_mc_setup/test_MC_setup_reco.sh
+++ Configuration/PyReleaseValidation/test/test_mc_setup/test_MC_setup_reco.sh
@@ -9,10 +9,18 @@ era=$3
 
 #export SCRAM_ARCH=$scram_arch
 
-echo '> Running RAW2DIGI,L1Reco,RECO,RECOSIM + PAT steps in ' $release
+echo '> Running RAW2DIGI,L1Reco,RECO,RECOSIM + PAT steps reading input generated in ' $release
+
+# Check if release matches the pattern "CMSSW_12_4_*_HLT"
+if [[ $release == CMSSW_12_4_*_HLT ]]; then
+    echo "Release matches CMSSW_12_4_*_HLT, adding customisation for fixReading_12_4_X_Files"
+    customise_flag="IOPool/Input/fixReading_12_4_X_Files.fixReading_12_4_X_Files, Configuration/DataProcessing/Utils.addMonitoring"
+else
+    customise_flag="Configuration/DataProcessing/Utils.addMonitoring"
+fi
 
 cmsDriver.py  --python_filename reco.py --eventcontent AODSIM \
---customise Configuration/DataProcessing/Utils.addMonitoring \
+--customise $customise_flag \
 --datatier AODSIM --fileout file:step4.root \
 --conditions $conditions --step RAW2DIGI,L1Reco,RECO,RECOSIM \
 --geometry DB:Extended --filein file:step3.root --era $era \
