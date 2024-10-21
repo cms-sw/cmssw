0ed8a2f897004f643de03ddb7b0e2b38b4045d9b
brusale <brusale11@gmail.com>
2024-10-21 14:43:26 +0200

    implement CLUE algorithm in the barrel
    Adds CLUE algorithm implementation in the barrel.
    Also the respective producer and plugin factory are introduced, along
    with the validation chain.

--- SimCalorimetry/HGCalAssociatorProducers/plugins/LCToSCAssociatorByEnergyScoreProducer.cc
+++ SimCalorimetry/HGCalAssociatorProducers/plugins/LCToSCAssociatorByEnergyScoreProducer.cc
@@ -85,10 +85,8 @@ void LCToSCAssociatorByEnergyScoreProducer<HIT>::fillDescriptions(edm::Configura
                                           edm::InputTag("HGCalRecHit", "HGCHEBRecHits")});
   } else {
     desc.add<edm::InputTag>("hitMapTag", edm::InputTag("recHitMapProducer", "barrelRecHitMap"));
-    desc.add<std::vector<edm::InputTag>>("hits",
-                                         {edm::InputTag("particleFlowRecHitECAL", ""),
-                                          edm::InputTag("particleFlowRecHitHBHE", ""),
-                                          edm::InputTag("particleFlowRecHitHO", "")});
+    desc.add<std::vector<edm::InputTag>>(
+        "hits", {edm::InputTag("particleFlowRecHitECAL", ""), edm::InputTag("particleFlowRecHitHBHE", "")});
   }
   cfg.addWithDefaultLabel(desc);
 }
