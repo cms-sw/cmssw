4c0ba5d1d6dd8e76ff3bcdb3c5e9dab4afa2b4de
Andrew Loeliger <loeliger@wisc.edu>
2024-10-23 04:06:15 -0500

    Change CICADA DQM sequences to be copies of the original

--- DQM/L1TMonitor/python/L1TCaloLayer1Summary_cff.py
+++ DQM/L1TMonitor/python/L1TCaloLayer1Summary_cff.py
@@ -5,8 +5,12 @@ from L1Trigger.L1TCaloLayer1.simCaloStage2Layer1Summary_cfi import *
 from L1Trigger.L1TCaloLayer1.simCaloStage2Layer1Digis_cfi import *
 from DQM.L1TMonitor.L1TCaloLayer1Summary_cfi import *
 
-simCaloStage2Layer1Summary.caloLayer1Regions = cms.InputTag("caloLayer1Digis", "")
-simCaloStage2Layer1Digis.ecalToken = cms.InputTag("ecalDigis", "EcalTriggerPrimitives")
-simCaloStage2Layer1Digis.hcalToken = cms.InputTag("hcalDigis", "")
+dqmSimCaloStage2Layer1Summary = simCaloStage2Layer1Summary.clone(
+    caloLayer1Regions = cms.InputTag("caloLayer1Digis", ""),
+)
+dqmSimCaloStage2Layer1Digis = simCaloStage2Layer1Digis.clone(
+    ecalToken = cms.InputTag("ecalDigis", "EcalTriggerPrimitives"),
+    hcalToken = cms.InputTag("hcalDigis", "")
+)
 
-l1tCaloLayer1SummarySeq = cms.Sequence(simCaloStage2Layer1Digis * simCaloStage2Layer1Summary * l1tCaloLayer1Summary)
+l1tCaloLayer1SummarySeq = cms.Sequence(dqmSimCaloStage2Layer1Digis * dqmSimCaloStage2Layer1Summary * l1tCaloLayer1Summary)
