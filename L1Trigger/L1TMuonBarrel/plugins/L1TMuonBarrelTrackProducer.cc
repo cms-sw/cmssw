d69ff3305db8cc013714d55a68ff5055344e0688
Christopher Jones <chrisdjones15@gmail.com>
2024-11-08 14:24:17 -0600

    Improved memory handling in L1TMuonBarrelTrackProducer
    Moved producer header info into source.

--- L1Trigger/L1TMuonBarrel/plugins/L1TMuonBarrelTrackProducer.cc
+++ L1Trigger/L1TMuonBarrel/plugins/L1TMuonBarrelTrackProducer.cc
@@ -12,28 +12,40 @@
 //   G. Flouris               U Ioannina
 //--------------------------------------------------
 
-#include "L1TMuonBarrelTrackProducer.h"
-
 #include "DataFormats/Common/interface/Handle.h"
 #include "FWCore/Framework/interface/Event.h"
+#include "FWCore/Framework/interface/one/EDProducer.h"
+#include "FWCore/ParameterSet/interface/ParameterSet.h"
 
 #include "DataFormats/L1DTTrackFinder/interface/L1MuDTChambPhContainer.h"
 #include "DataFormats/L1DTTrackFinder/interface/L1MuDTChambThContainer.h"
 #include "FWCore/Utilities/interface/EDGetToken.h"
 #include "FWCore/Utilities/interface/InputTag.h"
 #include "DataFormats/L1CSCTrackFinder/interface/TrackStub.h"
-#include <FWCore/Framework/interface/ConsumesCollector.h>
+#include "FWCore/Framework/interface/ConsumesCollector.h"
 
 #include "L1Trigger/L1TMuonBarrel/interface/L1MuBMTFConfig.h"
-#include "L1Trigger/L1TMuonBarrel/interface/L1MuBMTFSetup.h"
 #include "L1Trigger/L1TMuonBarrel/interface/L1MuBMTrackFinder.h"
 
 #include <iostream>
 #include <iomanip>
 
+class L1TMuonBarrelTrackProducer : public edm::one::EDProducer<edm::one::SharedResources> {
+public:
+  /// Constructor
+  L1TMuonBarrelTrackProducer(const edm::ParameterSet& pset);
+
+  /// Produce digis out of raw data
+  void produce(edm::Event& e, const edm::EventSetup& c) override;
+
+private:
+  L1MuBMTrackFinder dtbx_;
+};
+
 using namespace std;
 
-L1TMuonBarrelTrackProducer::L1TMuonBarrelTrackProducer(const edm::ParameterSet& pset) {
+L1TMuonBarrelTrackProducer::L1TMuonBarrelTrackProducer(const edm::ParameterSet& pset)
+    : dtbx_(pset, consumesCollector()) {
   produces<l1t::RegionalMuonCandBxCollection>("BMTF");
   produces<l1t::RegionalMuonCandBxCollection>("UnsortedBMTF");
   produces<vector<L1MuBMTrack> >("BMTF");
@@ -41,24 +53,20 @@ L1TMuonBarrelTrackProducer::L1TMuonBarrelTrackProducer(const edm::ParameterSet& 
   produces<vector<L1MuBMTrackSegEta> >("BMTF");
 
   usesResource("L1TMuonBarrelTrackProducer");
-  setup1 = std::make_unique<L1MuBMTFSetup>(pset, consumesCollector());
 }
 
-L1TMuonBarrelTrackProducer::~L1TMuonBarrelTrackProducer() {}
-
 void L1TMuonBarrelTrackProducer::produce(edm::Event& e, const edm::EventSetup& c) {
-  L1MuBMTrackFinder* dtbx = setup1->TrackFinder();
-  dtbx->clear();
+  dtbx_.clear();
 
-  if (dtbx->config().Debug(1))
+  if (dtbx_.config().Debug(1))
     cout << endl;
-  if (dtbx->config().Debug(1))
+  if (dtbx_.config().Debug(1))
     cout << "**** L1MuonBMTFTrigger processing event  ****" << endl;
 
-  dtbx->run(e, c);
+  dtbx_.run(e, c);
 
-  int ndt = dtbx->numberOfTracks();
-  if (dtbx->config().Debug(1))
+  int ndt = dtbx_.numberOfTracks();
+  if (dtbx_.config().Debug(1))
     cout << "Number of muons found by the L1 BBMX TRIGGER : " << ndt << endl;
 
   std::unique_ptr<l1t::RegionalMuonCandBxCollection> tra_product(new l1t::RegionalMuonCandBxCollection);
@@ -68,16 +76,16 @@ void L1TMuonBarrelTrackProducer::produce(edm::Event& e, const edm::EventSetup& c
   unique_ptr<vector<L1MuBMTrackSegEta> > vec_L1MuBMTrackSegEta(new vector<L1MuBMTrackSegEta>);
 
   ///Muons before muon sorter
-  l1t::RegionalMuonCandBxCollection dtTracks = dtbx->getcache0();
+  l1t::RegionalMuonCandBxCollection dtTracks = dtbx_.getcache0();
   *tra_product = dtTracks;
 
   ///Muons after muon sorter, for uGMT
-  l1t::RegionalMuonCandBxCollection BMTracks = dtbx->getcache();
+  l1t::RegionalMuonCandBxCollection BMTracks = dtbx_.getcache();
   *vec_product = BMTracks;
 
-  *vec_L1MuBMTrack = dtbx->getcache1();
-  *vec_L1MuBMTrackSegPhi = dtbx->getcache2();
-  *vec_L1MuBMTrackSegEta = dtbx->getcache3();
+  *vec_L1MuBMTrack = dtbx_.getcache1();
+  *vec_L1MuBMTrackSegPhi = dtbx_.getcache2();
+  *vec_L1MuBMTrackSegEta = dtbx_.getcache3();
 
   //for (int ibx = BMTracks.getFirstBX(); ibx  <= BMTracks.getLastBX(); ibx++){
   //cout << "DEBUG:  BMTF size at bx " << ibx << " " << BMTracks.size(ibx) << "\n";
