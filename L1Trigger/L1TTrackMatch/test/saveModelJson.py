a3964e08c1638bb365e4b9e1e7c81ad66ec58a62
Ryan McCarthy <ryan.edward.mccarthy@cern.ch>
2024-11-14 17:22:13 +0100

    Deleting unused models and scripts

--- L1Trigger/L1TTrackMatch/test/saveModelJson.py
+++
@@ -1,5 +0,0 @@
-import conifer
-import joblib
-xgb = joblib.load("/nfs/data41/rmccarth/conifer/vertexing/dispVertTaggerEmulationFixedPoint.pkl")
-model = conifer.converters.convert_from_xgboost(xgb)
-model.save('dispVertTaggerEmulationFixedPoint.json')
