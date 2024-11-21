8864274426361a54dd7b13f4edcf3585bc55fd03
Shahzad Malik Muzaffar <shahzad.malik.muzaffar@cern.ch>
2024-11-21 16:50:50 +0100

    Avoid using past module for python2/3 compatibility

--- FWCore/ParameterSet/python/Types.py
+++ FWCore/ParameterSet/python/Types.py
@@ -3,12 +3,12 @@ from .Mixins import PrintOptions, _SimpleParameterTypeBase, _ParameterTypeBase, 
 from .Mixins import _ValidatingParameterListBase, specialImportRegistry
 from .Mixins import saveOrigin
 from .ExceptionHandling import format_typename, format_outerframe
-from past.builtins import long
 import codecs
 import copy
 import math
 import builtins
 
+long = int
 _builtin_bool = bool
 
 class _Untracked(object):
