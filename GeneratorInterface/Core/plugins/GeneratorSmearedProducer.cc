5f4fe988c79dbc802a70e4062e4bab6c943d83ae
Mikhail <Mikhail.Kirsanov@cern.ch>
2024-11-25 18:14:09 +0100

    add handling of HepMC3Product

--- GeneratorInterface/Core/plugins/GeneratorSmearedProducer.cc
+++ GeneratorInterface/Core/plugins/GeneratorSmearedProducer.cc
@@ -5,6 +5,7 @@
 #include "FWCore/ParameterSet/interface/ParameterSet.h"
 
 #include "SimDataFormats/GeneratorProducts/interface/HepMCProduct.h"
+#include "SimDataFormats/GeneratorProducts/interface/HepMC3Product.h"
 #include "FWCore/Framework/interface/global/EDProducer.h"
 #include "FWCore/Utilities/interface/EDGetToken.h"
 #include "FWCore/Framework/interface/MakerMacros.h"
@@ -16,7 +17,6 @@ namespace edm {
   class ConfigurationDescriptions;
   class Event;
   class EventSetup;
-  class HepMCProduct;
 }  // namespace edm
 
 class GeneratorSmearedProducer : public edm::global::EDProducer<> {
@@ -29,14 +29,19 @@ public:
 private:
   const edm::EDGetTokenT<edm::HepMCProduct> newToken_;
   const edm::EDGetTokenT<edm::HepMCProduct> oldToken_;
+  const edm::EDGetTokenT<edm::HepMC3Product> Token3_;
 };
 
 GeneratorSmearedProducer::GeneratorSmearedProducer(edm::ParameterSet const& ps)
     : newToken_(consumes<edm::HepMCProduct>(ps.getUntrackedParameter<edm::InputTag>("currentTag"))),
-      oldToken_(consumes<edm::HepMCProduct>(ps.getUntrackedParameter<edm::InputTag>("previousTag"))) {
+      oldToken_(consumes<edm::HepMCProduct>(ps.getUntrackedParameter<edm::InputTag>("previousTag"))),
+      Token3_(consumes<edm::HepMC3Product>(ps.getUntrackedParameter<edm::InputTag>("currentTag"))) {
   // This producer produces a HepMCProduct, a copy of the original one
-  // It is used for backwaerd compatibility
+  // It is used for backward compatibility
+  // If HepMC3Product exists, it produces its copy
+  // It adds "generatorSmeared" to description, which is needed for further processing
   produces<edm::HepMCProduct>();
+  produces<edm::HepMC3Product>();
 }
 
 void GeneratorSmearedProducer::produce(edm::StreamID, edm::Event& iEvent, const edm::EventSetup& es) const {
@@ -48,6 +53,13 @@ void GeneratorSmearedProducer::produce(edm::StreamID, edm::Event& iEvent, const 
   if (found) {
     std::unique_ptr<edm::HepMCProduct> theCopy(new edm::HepMCProduct(*theHepMCProduct));
     iEvent.put(std::move(theCopy));
+  }
+
+  edm::Handle<edm::HepMC3Product> theHepMC3Product;
+  found = iEvent.getByToken(Token3_, theHepMC3Product);
+  if (found) {
+    std::unique_ptr<edm::HepMC3Product> theCopy3(new edm::HepMC3Product(*theHepMC3Product));
+    iEvent.put(std::move(theCopy3));
   }
 }
 
