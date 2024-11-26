7bc4ae8d39fda93d5265b856ea15b71bcc511f8a
Mikhail Kirsanov <mkirsano@cmsdev42.cern.ch>
2024-11-26 16:42:56 +0100

    Add possibility to work with HepMC3Product

--- IOMC/EventVertexGenerators/src/BaseEvtVtxGenerator.cc
+++ IOMC/EventVertexGenerators/src/BaseEvtVtxGenerator.cc
@@ -8,7 +8,7 @@
 #include "FWCore/ParameterSet/interface/ParameterSet.h"
 
 #include "SimDataFormats/GeneratorProducts/interface/HepMCProduct.h"
-#include "SimDataFormats/GeneratorProducts/interface/HepMCProduct.h"
+#include "SimDataFormats/GeneratorProducts/interface/HepMC3Product.h"
 
 #include "FWCore/ServiceRegistry/interface/Service.h"
 #include "FWCore/Utilities/interface/RandomNumberGenerator.h"
@@ -35,7 +35,9 @@ BaseEvtVtxGenerator::BaseEvtVtxGenerator(const ParameterSet& pset) {
                                              "in the configuration file or remove the modules that require it.";
   }
 
+  sourceToken3 = consumes<edm::HepMC3Product>(pset.getParameter<edm::InputTag>("src"));
   sourceToken = consumes<edm::HepMCProduct>(pset.getParameter<edm::InputTag>("src"));
+  produces<edm::HepMC3Product>();
   produces<edm::HepMCProduct>();
 }
 
@@ -47,20 +49,38 @@ void BaseEvtVtxGenerator::produce(Event& evt, const EventSetup&) {
 
   Handle<HepMCProduct> HepUnsmearedMCEvt;
 
-  evt.getByToken(sourceToken, HepUnsmearedMCEvt);
+  bool found = evt.getByToken(sourceToken, HepUnsmearedMCEvt);
 
-  // Copy the HepMC::GenEvent
-  HepMC::GenEvent* genevt = new HepMC::GenEvent(*HepUnsmearedMCEvt->GetEvent());
-  std::unique_ptr<edm::HepMCProduct> HepMCEvt(new edm::HepMCProduct(genevt));
-  // generate new vertex & apply the shift
-  //
-  HepMCEvt->applyVtxGen(newVertex(engine));
+  if (found) { // HepMC event exists
 
-  //HepMCEvt->LorentzBoost( 0., 142.e-6 );
-  HepMCEvt->boostToLab(GetInvLorentzBoost(), "vertex");
-  HepMCEvt->boostToLab(GetInvLorentzBoost(), "momentum");
+    // Make a copy
+    HepMC::GenEvent* genevt = new HepMC::GenEvent(*HepUnsmearedMCEvt->GetEvent());
 
-  evt.put(std::move(HepMCEvt));
+    std::unique_ptr<edm::HepMCProduct> HepMCEvt(new edm::HepMCProduct(genevt));
+    // generate new vertex & apply the shift
+    //
+    HepMCEvt->applyVtxGen(newVertex(engine));
+
+    //HepMCEvt->LorentzBoost( 0., 142.e-6 );
+    HepMCEvt->boostToLab(GetInvLorentzBoost(), "vertex");
+    HepMCEvt->boostToLab(GetInvLorentzBoost(), "momentum");
+
+    evt.put(std::move(HepMCEvt));
+
+  } else { // no HepMC event, try to get HepMC3 event
+
+    Handle<HepMC3Product> HepUnsmearedMCEvt3;
+    found = evt.getByToken(sourceToken3, HepUnsmearedMCEvt3);
+
+    if(!found) throw cms::Exception("ProductAbsent") << "No HepMCProduct, tried to get HepMC3Product, but it is also absent " << std::endl;
+
+    HepMC3::GenEvent* genevt3 = new HepMC3::GenEvent();
+    genevt3->read_data(*HepUnsmearedMCEvt3->GetEvent());
+    HepMC3Product* productcopy3 = new HepMC3Product(genevt3);    // For the moment do not really smear HepMC3
+    std::unique_ptr<edm::HepMC3Product> HepMC3Evt(productcopy3);
+    evt.put(std::move(HepMC3Evt)); 
+
+  }
 
   return;
 }
