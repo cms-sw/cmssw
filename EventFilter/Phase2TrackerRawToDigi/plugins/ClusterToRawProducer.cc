20d09a0d2dea76d1781bbca117566013a00d0395
am2023 <a.mastronikolis23@imperial.ac.uk>
2024-12-01 21:22:00 +0000

    for loop fix

--- EventFilter/Phase2TrackerRawToDigi/plugins/ClusterToRawProducer.cc
+++ EventFilter/Phase2TrackerRawToDigi/plugins/ClusterToRawProducer.cc
@@ -82,10 +82,9 @@ void ClusterToRawProducer::produce(edm::Event& iEvent, const edm::EventSetup& iS
     using namespace Phase2TrackerSpecifications;
     using namespace Phase2DAQFormatSpecification;
 
-    for (int dtc_id = 1; dtc_id == 1; dtc_id++)
-    // for (int dtc_id = MIN_DTC_ID; dtc_id < MAX_DTC_ID; dtc_id++)
+    for (int dtc_id = MIN_DTC_ID; dtc_id < MAX_DTC_ID; dtc_id++)
     {
-        for (int slink_id = 0; slink_id == 0; slink_id++)
+        for (int slink_id = 0; slink_id < MAX_SLINK_ID + 1; slink_id++)
         {
             int index_first = slink_id * MODULES_PER_SLINK;
             int index_last = (slink_id + 1) * MODULES_PER_SLINK;
