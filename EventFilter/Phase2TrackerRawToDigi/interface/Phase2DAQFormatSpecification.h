ea37131d0da7c5dc5d8784e4825c32a98fbb31c1
am2023 <a.mastronikolis23@imperial.ac.uk>
2024-12-01 21:28:56 +0000

    rmed comment

--- EventFilter/Phase2TrackerRawToDigi/interface/Phase2DAQFormatSpecification.h
+++ EventFilter/Phase2TrackerRawToDigi/interface/Phase2DAQFormatSpecification.h
@@ -24,9 +24,9 @@ namespace Phase2DAQFormatSpecification
     static const int SCLUSTER_ADDRESS_BITS_PS = 7;
 
     static const int WIDTH_MAX_VALUE = 0x7;
-    static const int WIDTH_BITS = 3; // Added: Number of bits for Width
+    static const int WIDTH_BITS = 3;
 
-    static const int MIP_BITS = 1; // Added: Number of bits for MIP
+    static const int MIP_BITS = 1;
 
     static const int SS_CLUSTER_BITS = 14;
     static const int PX_CLUSTER_BITS = 17;
