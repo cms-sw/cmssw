1b2edb77378e9f7e5334426110cc2d7ec2e476dc
mmusich <marco.musich@cern.ch>
2024-12-09 19:08:57 +0100

    fix clang linking issue

--- SimMuon/MCTruth/plugins/SealModule.cc
+++ SimMuon/MCTruth/plugins/SealModule.cc
@@ -2,10 +2,6 @@
 #include "FWCore/PluginManager/interface/ModuleDef.h"
 #include "SimMuon/MCTruth/interface/CSCTruthTest.h"
 #include "SimMuon/MCTruth/plugins/MuonAssociatorEDProducer.h"
-#include "SimMuon/MCTruth/plugins/SeedToTrackProducer.h"
-#include "SimMuon/MCTruth/plugins/Phase2SeedToTrackProducer.h"
 
 DEFINE_FWK_MODULE(MuonAssociatorEDProducer);
 DEFINE_FWK_MODULE(CSCTruthTest);
-DEFINE_FWK_MODULE(SeedToTrackProducer);
-DEFINE_FWK_MODULE(Phase2SeedToTrackProducer);
