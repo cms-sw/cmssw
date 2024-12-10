dd23ea9120921e09615c92ea08bd5de0c36b457b
mmusich <marco.musich@cern.ch>
2024-12-10 08:57:53 +0100

    fetch HLT ESproducers directly from the HLT menu

---
+++ Alignment/CommonAlignmentProducer/python/HLTModulesIncludes_cff.py
@@ -0,0 +1,5 @@
+from HLTrigger.Configuration.HLT_FULL_cff import fragment
+
+hltESPStripCPEfromTrackAngle = fragment.hltESPStripCPEfromTrackAngle
+hltESPPixelCPEGeneric = fragment.hltESPPixelCPEGeneric
+hltESPTTRHBWithTrackAngle = fragment.hltESPTTRHBWithTrackAngle
