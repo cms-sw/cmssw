b54f67663a66620d2812f2958b21bf527c5f12c2
mmusich <marco.musich@cern.ch>
2024-12-11 16:21:49 +0100

    add SiPixelAli HG Combined workflows to testPCLAlCaHarvesting

--- Calibration/TkAlCaRecoProducers/test/parseFwkJobReport.py
+++ Calibration/TkAlCaRecoProducers/test/parseFwkJobReport.py
@@ -18,7 +18,9 @@ TARGET_LIST_OF_TAGS=['BeamSpotObject_ByLumi',           # beamspot
                      'SiStripLA_pcl',
                      'SiPixelAli_pcl',                  # Alignment
                      'SiPixelAliHG_pcl',
-                     'SiPixelAliHGCombined_pcl']
+                     'SiPixelAliHGCombined_pcl',
+                     'SiPixelAliHLTHGCombined_pcl'
+                     ]
 TARGET_DQM_FILES=1
 TARGET_DQM_FILENAME='./DQM_V0001_R000325022__Express__PCLTest__ALCAPROMPT.root'
 TARGET_DB_FILES=len(TARGET_LIST_OF_TAGS)
