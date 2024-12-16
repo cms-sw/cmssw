db750b01df372d0ebae9283ad13e9a47234ded69
Malik Shahzad Muzaffar <shahzad.malik.muzaffar@cern.ch>
2024-12-16 15:35:32 +0100

    Update test-clang-tidy.sh

--- Utilities/ReleaseScripts/test/test-clang-tidy.sh
+++ Utilities/ReleaseScripts/test/test-clang-tidy.sh
@@ -2,6 +2,8 @@
 #this test should use scram build code-checks
 
 
+
+
 clang-tidy -export-fixes $CMSSW_BASE/test-clang-tidy.cc.yaml -header-filter "$CMSSW_BASE/src/.*" $CMSSW_BASE/src/Utilities/ReleaseScripts/test/test-clang-tidy.cc
 sed -i -e "s|$CMSSW_BASE/src/||" $CMSSW_BASE/test-clang-tidy.cc.yaml
 sed -i -e '/^\s\s*BuildDirectory/d;/^\s\s*Level:/d' $CMSSW_BASE/test-clang-tidy.cc.yaml
