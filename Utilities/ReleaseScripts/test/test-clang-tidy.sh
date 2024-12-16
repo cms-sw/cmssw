43cac4d974977886a57b635b7c809418cd099f6a
Malik Shahzad Muzaffar <shahzad.malik.muzaffar@cern.ch>
2024-12-16 16:13:49 +0100

    [Do not merge][14.2] Testing draft PR for production release cycle

--- Utilities/ReleaseScripts/test/test-clang-tidy.sh
+++ Utilities/ReleaseScripts/test/test-clang-tidy.sh
@@ -1,4 +1,6 @@
 #!/bin/bash -ex
+
+
 clang-tidy -export-fixes $CMSSW_BASE/test-clang-tidy.cc.yaml -header-filter "$CMSSW_BASE/src/.*" $CMSSW_BASE/src/Utilities/ReleaseScripts/test/test-clang-tidy.cc
 sed -i -e "s|$CMSSW_BASE/src/||" $CMSSW_BASE/test-clang-tidy.cc.yaml
 sed -i -e '/^\s\s*BuildDirectory/d;/^\s\s*Level:/d' $CMSSW_BASE/test-clang-tidy.cc.yaml
