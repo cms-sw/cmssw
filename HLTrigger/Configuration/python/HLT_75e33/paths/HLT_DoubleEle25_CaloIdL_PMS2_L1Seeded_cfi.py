aa7f6e1dfdd1969086cecd2b6c7e759041947210
mmusich <marco.musich@cern.ch>
2024-12-17 15:08:59 +0100

    clean-up unused inclusions in HLT paths in Phase2 HLT menu

--- HLTrigger/Configuration/python/HLT_75e33/paths/HLT_DoubleEle25_CaloIdL_PMS2_L1Seeded_cfi.py
+++ HLTrigger/Configuration/python/HLT_75e33/paths/HLT_DoubleEle25_CaloIdL_PMS2_L1Seeded_cfi.py
@@ -1,6 +1,5 @@
 import FWCore.ParameterSet.Config as cms
 
-from ..modules.hltPreDoubleEle25CaloIdLPMS2L1Seeded_cfi import *
 from ..sequences.HLTBeginSequence_cfi import *
 from ..sequences.HLTDoubleEle25CaloIdLPMS2L1SeededSequence_cfi import *
 from ..sequences.HLTEndSequence_cfi import *
