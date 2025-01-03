d60583885d2712153339e515d57368600187c708
mmusich <marco.musich@cern.ch>
2025-01-03 16:40:08 +0100

    add fillDescriptions to JetTracksAssociatorAtVertex

--- RecoJets/JetAssociationProducers/src/JetTracksAssociatorAtVertex.h
+++ RecoJets/JetAssociationProducers/src/JetTracksAssociatorAtVertex.h
@@ -8,29 +8,30 @@
 #ifndef JetTracksAssociatorAtVertex_h
 #define JetTracksAssociatorAtVertex_h
 
-#include "FWCore/Framework/interface/stream/EDProducer.h"
 #include "DataFormats/Common/interface/EDProductfwd.h"
-#include "FWCore/Utilities/interface/InputTag.h"
+#include "FWCore/Framework/interface/stream/EDProducer.h"
+#include "FWCore/ParameterSet/interface/ConfigurationDescriptions.h"
 #include "FWCore/ParameterSet/interface/ParameterSet.h"
-
+#include "FWCore/ParameterSet/interface/ParameterSetDescription.h"
+#include "FWCore/Utilities/interface/InputTag.h"
 #include "RecoJets/JetAssociationAlgorithms/interface/JetTracksAssociationDRVertex.h"
 #include "RecoJets/JetAssociationAlgorithms/interface/JetTracksAssociationDRVertexAssigned.h"
 
 class JetTracksAssociatorAtVertex : public edm::stream::EDProducer<> {
 public:
   JetTracksAssociatorAtVertex(const edm::ParameterSet&);
-  ~JetTracksAssociatorAtVertex() override;
+  ~JetTracksAssociatorAtVertex() override = default;
 
+  static void fillDescriptions(edm::ConfigurationDescriptions& descriptions);
   void produce(edm::Event&, const edm::EventSetup&) override;
 
 private:
-  edm::EDGetTokenT<edm::View<reco::Jet>> mJets;
-  edm::EDGetTokenT<reco::TrackCollection> mTracks;
+  const edm::EDGetTokenT<edm::View<reco::Jet>> mJets;
+  const edm::EDGetTokenT<reco::TrackCollection> mTracks;
 
-  int mTrackQuality;
-  JetTracksAssociationDRVertex mAssociator;
-  JetTracksAssociationDRVertexAssigned mAssociatorAssigned;
-  bool useAssigned;  /// if true, use the track/jet association with vertex assignment to tracks
+  const JetTracksAssociationDRVertex mAssociator;
+  const JetTracksAssociationDRVertexAssigned mAssociatorAssigned;
+  const bool useAssigned;  /// if true, use the track/jet association with vertex assignment to tracks
   edm::EDGetTokenT<reco::VertexCollection> pvSrc;  /// if useAssigned, will read this PV collection.
 };
 
