edcb5a5a38feb6dbb20879a41c7018ca68f46883
mmusich <marco.musich@cern.ch>
2025-01-03 11:39:07 +0100

    add fillDescriptions to MuonLinksProducer{ForHLT}

--- RecoMuon/MuonIdentification/plugins/MuonLinksProducerForHLT.h
+++ RecoMuon/MuonIdentification/plugins/MuonLinksProducerForHLT.h
@@ -11,31 +11,33 @@
  */
 
 // user include files
+#include "DataFormats/MuonReco/interface/MuonFwd.h"
+#include "DataFormats/MuonReco/interface/MuonTrackLinks.h"
+#include "DataFormats/TrackReco/interface/Track.h"
+#include "DataFormats/TrackReco/interface/TrackFwd.h"
+#include "FWCore/Framework/interface/Event.h"
 #include "FWCore/Framework/interface/Frameworkfwd.h"
 #include "FWCore/Framework/interface/global/EDProducer.h"
-#include "FWCore/Framework/interface/Event.h"
+#include "FWCore/ParameterSet/interface/ConfigurationDescriptions.h"
 #include "FWCore/ParameterSet/interface/ParameterSet.h"
-#include "DataFormats/TrackReco/interface/TrackFwd.h"
-#include "DataFormats/TrackReco/interface/Track.h"
-#include "DataFormats/MuonReco/interface/MuonFwd.h"
-//#include "DataFormats/MuonReco/interface/Muon.h"
-#include "DataFormats/MuonReco/interface/MuonTrackLinks.h"
+#include "FWCore/ParameterSet/interface/ParameterSetDescription.h"
 
 class MuonLinksProducerForHLT : public edm::global::EDProducer<> {
 public:
   explicit MuonLinksProducerForHLT(const edm::ParameterSet&);
-
-  ~MuonLinksProducerForHLT() override;
+  ~MuonLinksProducerForHLT() override = default;
 
   void produce(edm::StreamID, edm::Event&, const edm::EventSetup&) const override;
 
+  static void fillDescriptions(edm::ConfigurationDescriptions& descriptions);
+
 private:
-  edm::InputTag theLinkCollectionInInput;
-  edm::InputTag theInclusiveTrackCollectionInInput;
-  edm::EDGetTokenT<reco::MuonTrackLinksCollection> linkToken_;
-  edm::EDGetTokenT<reco::TrackCollection> trackToken_;
-  double ptMin;
-  double pMin;
-  double shareHitFraction;
+  const edm::InputTag theLinkCollectionInInput_;
+  const edm::InputTag theInclusiveTrackCollectionInInput_;
+  const edm::EDGetTokenT<reco::MuonTrackLinksCollection> linkToken_;
+  const edm::EDGetTokenT<reco::TrackCollection> trackToken_;
+  const double ptMin_;
+  const double pMin_;
+  const double shareHitFraction_;
 };
 #endif
