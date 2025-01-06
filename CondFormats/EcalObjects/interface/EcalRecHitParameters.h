793e61553d003963861c3253192062b64d40d364
Thomas <thomas.reis@cern.ch>
2025-01-06 12:23:16 +0100

    Fix compilation issue with gcc13.

--- CondFormats/EcalObjects/interface/EcalRecHitParameters.h
+++ CondFormats/EcalObjects/interface/EcalRecHitParameters.h
@@ -1,12 +1,13 @@
 #ifndef CondFormats_EcalObjects_EcalRecHitParameters_h
 #define CondFormats_EcalObjects_EcalRecHitParameters_h
 
-#include <bitset>
 #include <array>
+#include <bitset>
+#include <cstdint>
 
 constexpr size_t kNEcalChannelStatusCodes = 16;  // The HW supports 16 channel status codes
 using RecoFlagBitsArray =
-    std::array<uint32_t, kNEcalChannelStatusCodes>;  // associate recoFlagBits to all channel status codes
+    std::array<std::uint32_t, kNEcalChannelStatusCodes>;  // associate recoFlagBits to all channel status codes
 
 struct EcalRecHitParameters {
   RecoFlagBitsArray recoFlagBits;
