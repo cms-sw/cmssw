5c2080c0304b7dcd782c7992371784e76df84487
Luca Ferragina <ferragina.luca@gmail.com>
2025-01-07 13:34:40 +0100

    Fix double inclusion of Phase2HLTMuonSelectorForL3 in some cases

--- RecoMuon/L3TrackFinder/plugins/SealModules.cc
+++ RecoMuon/L3TrackFinder/plugins/SealModules.cc
@@ -4,6 +4,8 @@
 #include "RecoMuon/L3TrackFinder/interface/MuonCkfTrajectoryBuilder.h"
 #include "RecoTracker/CkfPattern/interface/BaseCkfTrajectoryBuilderFactory.h"
 #include "RecoMuon/L3TrackFinder/interface/HLTMuonL2SelectorForL3IO.h"
+#include "RecoMuon/L3TrackFinder/interface/Phase2HLTMuonSelectorForL3.h"
 
 DEFINE_EDM_VALIDATED_PLUGIN(BaseCkfTrajectoryBuilderFactory, MuonCkfTrajectoryBuilder, "MuonCkfTrajectoryBuilder");
 DEFINE_FWK_MODULE(HLTMuonL2SelectorForL3IO);
+DEFINE_FWK_MODULE(Phase2HLTMuonSelectorForL3);
