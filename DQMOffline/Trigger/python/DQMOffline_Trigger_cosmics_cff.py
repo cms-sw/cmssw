0fa4a402865873fae3f2b3be011efee66c609adb
mmusich <marco.musich@cern.ch>
2025-01-07 16:52:42 +0100

    add DQM infrastructure to monitoring tracking at HLT in cosmics

--- DQMOffline/Trigger/python/DQMOffline_Trigger_cosmics_cff.py
+++ DQMOffline/Trigger/python/DQMOffline_Trigger_cosmics_cff.py
@@ -47,12 +47,18 @@ from DQMOffline.Trigger.HLTTauDQMOffline_cff import *
 # JetMET
 from DQMOffline.Trigger.JetMETHLTOfflineSource_cfi import *
 
+# Tracks
+from DQMOffline.Trigger.TrackToTrackMonitoringCosmics_cff import *
+from DQMOffline.Trigger.TrackingMonitoringCosmics_cff import *
+
 import DQMServices.Components.DQMEnvironment_cfi
 dqmEnvHLT= DQMServices.Components.DQMEnvironment_cfi.dqmEnv.clone(
     subSystemFolder = 'HLT'
 )
 
 offlineHLTSource = cms.Sequence(
+    cosmicTrackingMonitorHLT *
+    hltToOfflineCosmicsTrackValidatorSequence *
     dqmHLTFiltersDQMonitor *
     egHLTOffDQMSource *
     hltMuonOfflineAnalyzers *
