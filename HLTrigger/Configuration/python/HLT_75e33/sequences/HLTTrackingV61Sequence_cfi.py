fd4a8d9a85239484dc930f2fbb715be00becf91e
Luca Guzzi <luca.guzzi@mib.infn.it>
2025-01-16 17:53:26 +0100

    add trimming modules for the phase2 hlt menus

--- HLTrigger/Configuration/python/HLT_75e33/sequences/HLTTrackingV61Sequence_cfi.py
+++ HLTrigger/Configuration/python/HLT_75e33/sequences/HLTTrackingV61Sequence_cfi.py
@@ -2,6 +2,7 @@ import FWCore.ParameterSet.Config as cms
 
 from ..modules.hltGeneralTracks_cfi import *
 from ..modules.hltPhase2PixelVertices_cfi import *
+from ..modules.hltPhase2TrimmedPixelVertices_cfi import *
 from ..modules.hltTrackerClusterCheck_cfi import *
 from ..sequences.HLTHighPtTripletStepSequence_cfi import *
 from ..sequences.HLTPhase2PixelTracksSequence_cfi import *
@@ -13,3 +14,8 @@ HLTTrackingV61Sequence = cms.Sequence((HLTItLocalRecoSequence+HLTOtLocalRecoSequ
 
 from Configuration.ProcessModifiers.singleIterPatatrack_cff import singleIterPatatrack
 singleIterPatatrack.toReplaceWith(HLTTrackingV61Sequence, HLTTrackingV61Sequence.copyAndExclude([HLTHighPtTripletStepSequence]))
+
+from Configuration.ProcessModifiers.phase2_hlt_vertexTrimming_cff import phase2_hlt_vertexTrimming
+_HLTTrackingV61SequenceTrimming = HLTTrackingV61Sequence.copy()
+_HLTTrackingV61SequenceTrimming.insert(_HLTTrackingV61SequenceTrimming.index(hltPhase2PixelVertices)+1, hltPhase2TrimmedPixelVertices)
+phase2_hlt_vertexTrimming.toReplaceWith(HLTTrackingV61Sequence, _HLTTrackingV61SequenceTrimming)
