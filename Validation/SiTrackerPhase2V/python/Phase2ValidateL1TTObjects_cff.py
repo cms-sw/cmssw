74cd17a04349c968b195fd6f4cb03492163d0e83
Brandi Skipworth <bskipwo1@vols.utk.edu>
2025-01-31 19:21:13 +0100

    restore Phase2OTHarvestTracks and Phase2OTValidateTracks, delete Reconstruction versions

--- Validation/SiTrackerPhase2V/python/Phase2ValidateL1TTObjects_cff.py
+++ Validation/SiTrackerPhase2V/python/Phase2ValidateL1TTObjects_cff.py
@@ -5,6 +5,6 @@ from Validation.SiTrackerPhase2V.Phase2OTValidateTTStub_cfi import *
 
 trackingParticleValidOT = Phase2OTValidateReconstruction.clone()
 
-trackingParticleExtendedValidOT = Phase2OTValidateTrackingParticles.clone()
+trackingParticleExtendedValidOT = Phase2OTValidateReconstruction.clone()
 
 stubValidOT = Phase2OTValidateTTStub.clone()
