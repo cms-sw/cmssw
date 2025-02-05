58db531ff3c7ead709b0a7ebbb71da98f37c84db
Dinko Ferencek <Dinko.Ferencek@cern.ch>
2025-02-05 10:03:15 +0100

    lowered cluster charge threshold for pixel L1

--- Configuration/Eras/python/Era_Run3_2025_cff.py
+++ Configuration/Eras/python/Era_Run3_2025_cff.py
@@ -2,5 +2,6 @@ import FWCore.ParameterSet.Config as cms
 
 from Configuration.Eras.Era_Run3_2024_cff import Run3_2024
 from Configuration.Eras.Modifier_run3_GEM_2025_cff import run3_GEM_2025
+from Configuration.Eras.Modifier_run3_SiPixel_2025_cff import run3_SiPixel_2025
 
-Run3_2025 = cms.ModifierChain(Run3_2024, run3_GEM_2025)
+Run3_2025 = cms.ModifierChain(Run3_2024, run3_GEM_2025, run3_SiPixel_2025)
