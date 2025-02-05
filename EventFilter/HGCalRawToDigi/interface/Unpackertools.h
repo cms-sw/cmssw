3bcf4345a8b61da8e9e1a45438fd1a489730da27
cramonal <clara.ramon.alvarez@cern.ch>
2025-02-05 20:01:50 +0100

    fix format

--- EventFilter/HGCalRawToDigi/interface/Unpackertools.h
+++ EventFilter/HGCalRawToDigi/interface/Unpackertools.h
@@ -9,9 +9,8 @@ namespace hgcal {
      @short this function re-computes the CRC of the ECON-D packet and compares it with the trailer of the packet
      false is returned if there is a mismatch
    */
-  bool econdCRCAnalysis(const uint64_t *header, uint32_t pos, uint32_t payloadLength, uint64_t crcpol=0x4c11db7);
+  bool econdCRCAnalysis(const uint64_t *header, uint32_t pos, uint32_t payloadLength, uint64_t crcpol = 0x4c11db7);
 
-
-} // namespace hgcal
+}  // namespace hgcal
 
 #endif
