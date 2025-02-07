b849e995e7be72f69fd82cb5b93a6fc2c40781a5
cquarant <claudio.quaranta@cern.ch>
2025-02-07 11:22:57 +0100

    code formatting + BTLElectronicsMapping

--- DataFormats/ForwardDetId/interface/BTLDetId.h
+++ DataFormats/ForwardDetId/interface/BTLDetId.h
@@ -8,22 +8,22 @@
 #include <bitset>
 
 /** 
-    @class BTLDetId
-    @brief Detector identifier class for the Barrel Timing Layer.
-    The crystal count must start from 0, copy number must be scaled by 1 unit.
+  @class BTLDetId
+  @brief Detector identifier class for the Barrel Timing Layer.
+  The crystal count must start from 0, copy number must be scaled by 1 unit.
 
-    // Geometry v2,v3 BTLDetID
-    bit 15-10: module sequential number
-    bit 9-8  : crystal type (1 - 3)
-    bit 7-6  : readout unit sequential number within a type ( 1 - 2 )
-    bit 5-0  : crystal sequential number within a module ( 0 - 15 )
+  // Geometry v2,v3 BTLDetID
+  bit 15-10: module sequential number
+  bit 9-8  : crystal type (1 - 3)
+  bit 7-6  : readout unit sequential number within a type ( 1 - 2 )
+  bit 5-0  : crystal sequential number within a module ( 0 - 15 )
 
-    // Geometry v2 new DetID (all type 1 modules)
-    bit 15: kBTLNewFormat (0 - old BTLDetID, 1 - new BTLDetID)
-    bit 12-10: Readout unit number ( 1 - 6 )
-    bit 9-6  : Detector Module ( 1 - 12 )
-    bit  5   : Sensor Module inside DM ( 0 - 1 )
-    bit 4-0  : Crystal number in a SM ( 1 - 16 )
+  // Geometry v2 new DetID (all type 1 modules)
+  bit 15: kBTLNewFormat (0 - old BTLDetID, 1 - new BTLDetID)
+  bit 12-10: Readout unit number ( 1 - 6 )
+  bit 9-6  : Detector Module ( 1 - 12 )
+  bit  5   : Sensor Module inside DM ( 0 - 1 )
+  bit 4-0  : Crystal number in a SM ( 1 - 16 )
 */
 
 class BTLDetId : public MTDDetId {
@@ -116,27 +116,30 @@ public:
 
   /** Returns BTL crystal number. */
   inline int crystal() const {
-    if (id_ & kBTLNewFormat)
+    if (id_ & kBTLNewFormat) {
       return ((id_ >> kBTLCrystalOffset) & kBTLCrystalMask);
-    else
+    } else {
       return ((id_ >> kBTLoldCrystalOffset) & kBTLoldCrystalMask) + 1;
+    }
   }
 
   /** Returns BTL crystal number in construction database. */
   inline int crystalConsDB() const {
-    if (((id_ >> kBTLCrystalOffset) & kBTLCrystalMask) == kCrystalsPerModuleV2)
+    if (((id_ >> kBTLCrystalOffset) & kBTLCrystalMask) == kCrystalsPerModuleV2) {
       return -1;
-    if (smodule() == 0)
+    }
+    if (smodule() == 0) {
       return kCrystalsPerModuleV2 - 1 - ((id_ >> kBTLCrystalOffset) & kBTLCrystalMask);
-    else
+    } else {
       return ((id_ >> kBTLCrystalOffset) & kBTLCrystalMask);
+    }
   }
 
   /** Returns BTL detector module number. */
   inline int dmodule() const {
-    if (id_ & kBTLNewFormat)
+    if (id_ & kBTLNewFormat) {
       return ((id_ >> kBTLdetectorModOffset) & kBTLdetectorModMask);
-    else {
+    } else {
       uint32_t oldModule = (id_ >> kBTLoldModuleOffset) & kBTLoldModuleMask;
       uint32_t detModule =
           int((oldModule - 1) % (kDModulesInRUCol)) * kDModulesInRURow +
@@ -147,9 +150,9 @@ public:
 
   /** Returns BTL sensor module number. */
   inline int smodule() const {
-    if (id_ & kBTLNewFormat)
+    if (id_ & kBTLNewFormat) {
       return ((id_ >> kBTLsensorModOffset) & kBTLsensorModMask);
-    else {
+    } else {
       uint32_t oldModule = (id_ >> kBTLoldModuleOffset) & kBTLoldModuleMask;
       uint32_t senModule =
           int((oldModule - 1) / kDModulesInRUCol) % kSModulesInDM;  // in old scenario module number starts from 1
@@ -159,52 +162,58 @@ public:
 
   /** Returns BTL module number [1-24] (OLD BTL NUMBERING). */
   inline int module() const {
-    if (id_ & kBTLNewFormat)
+    if (id_ & kBTLNewFormat) {
       return ((dmodule() % kDModulesInRURow) * (kSModulesInDM * kDModulesInRUCol) + int(dmodule() / kDModulesInRURow) +
               kDModulesInRUCol * smodule()) +
              1;
-    else
+    } else {
       return (id_ >> kBTLoldModuleOffset) & kBTLoldModuleMask;
+    }
   }
 
   /** Returns BTL crystal type number [1-3] (OLD BTL NUMBERING). */
   inline int modType() const {
-    if (id_ & kBTLNewFormat)
+    if (id_ & kBTLNewFormat) {
       return int(runit() / kRUPerTypeV2 + 1);
-    else
+    } else {
       return (id_ >> kBTLoldModTypeOffset) & kBTLoldModTypeMask;
+    }
   }
 
   /** Returns BTL global readout unit number. */
   inline int runit() const {
-    if (id_ & kBTLNewFormat)
+    if (id_ & kBTLNewFormat) {
       return ((id_ >> kBTLRUOffset) & kBTLRUMask);
-    else
+    } else {
       return (modType() - 1) * kRUPerTypeV2 + int((id_ >> kBTLoldRUOffset) & kBTLoldRUMask);
+    }
   }
 
   /** Returns BTL readout unit number per type [1-2], from Global RU number [1-6]. */
   inline int runitByType() const {
-    if (id_ & kBTLNewFormat)
+    if (id_ & kBTLNewFormat) {
       return ((runit() % kRUPerTypeV2) + 1);
-    else
+    } else {
       return (((runit() - 1) % kRUPerTypeV2) + 1);
+    }
   }
 
   /** return the row in GeomDet language **/
   inline int row(unsigned nrows = kCrystalsPerModuleV2) const {
-    if (id_ & kBTLNewFormat)
+    if (id_ & kBTLNewFormat) {
       return crystal() % nrows;
-    else
+    } else {
       return (crystal() - 1) % nrows;
+    }
   }
 
   /** return the column in GeomDetLanguage **/
   inline int column(unsigned nrows = kCrystalsPerModuleV2) const {
-    if (id_ & kBTLNewFormat)
+    if (id_ & kBTLNewFormat) {
       return crystal() / nrows;
-    else
+    } else {
       return (crystal() - 1) / nrows;
+    }
   }
 
   /** create a Geographical DetId for Tracking **/
