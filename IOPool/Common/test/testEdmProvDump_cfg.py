9455de16f1013d2550c968fca73be250eeeae63b
Matti Kortelainen <matti.kortelainen@cern.ch>
2025-02-14 15:51:00 +0100

    Overhaul process information printing and extend testing

--- IOPool/Common/test/testEdmProvDump_cfg.py
+++ IOPool/Common/test/testEdmProvDump_cfg.py
@@ -1,18 +1,48 @@
 import FWCore.ParameterSet.Config as cms
+import argparse
+
+parser = argparse.ArgumentParser(description='Test edmProvDump')
+parser.add_argument("--lumi", type=int, default=1, help="LuminosityBlock number (default 1)")
+parser.add_argument("--ivalue", type=int, default=11, help="Value for one tracked parameter (default 11)")
+parser.add_argument("--output", default="testEdmProvDump.root", help="Output file name")
+parser.add_argument("--accelerators", type=str, nargs='+', help="Propagated to process.options.accelerators (default is unset)")
+parser.add_argument("--version", type=str, help="CMSSW version to be used in the ProcessHistory (default is unset")
+args = parser.parse_args()
+
+class ProcessAcceleratorTest(cms.ProcessAccelerator):
+    def __init__(self):
+        super(ProcessAcceleratorTest,self).__init__()
+        self._labels = ["test-one", "test-two"]
+    def labels(self):
+        return self._labels
+    def enabledLabels(self):
+        return self._labels
 
 process = cms.Process("PROD1")
+if args.version:
+    process._specialOverrideReleaseVersionOnlyForTesting(args.version)
+if args.accelerators:
+    process.add_(ProcessAcceleratorTest())
+    process.options.accelerators = args.accelerators
 
-process.source = cms.Source("IntSource")
-process.maxEvents = cms.untracked.PSet(
-    input = cms.untracked.int32(3)
+process.source = cms.Source("IntSource",
+    firstLuminosityBlock = cms.untracked.uint32(args.lumi)
 )
+process.maxEvents.input = 3
 
 process.out = cms.OutputModule("PoolOutputModule",
-    fileName = cms.untracked.string('testEdmProvDump.root'),
+    fileName = cms.untracked.string(args.output),
     outputCommands = cms.untracked.vstring(
         'keep *', 
         'drop *_intProducerA_*_*'
     )
+)
+
+process.WhatsItESProducer = cms.ESProducer("WhatsItESProducer")
+process.DoodadESSource = cms.ESSource("DoodadESSource")
+
+process.topLevelPSet = cms.PSet(
+    someInformation = cms.string("foobar")
 )
 
 process.a1 = cms.EDAnalyzer("TestFindProduct",
@@ -50,7 +80,7 @@ process.aliasForInt = cms.EDAlias(
 
 process.intVectorProducer = cms.EDProducer("IntVectorProducer",
   count = cms.int32(9),
-  ivalue = cms.int32(11)
+  ivalue = cms.int32(args.ivalue)
 )
 
 process.t = cms.Task(process.intProducerU, process.intProducerA, process.intVectorProducer)
