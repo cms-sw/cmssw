c997efb6564b450898ff6784ecda0e6e674245c5
Andrea Bocci <andrea.bocci@cern.ch>
2025-02-16 15:25:27 +0100

    Implement an integration test for post_insert()

---
+++ DataFormats/TestObjects/interface/ThingWithDoNotSort.h
@@ -0,0 +1,30 @@
+#ifndef DataFormats_TestObjects_interface_ThingWithDoNotSort_h
+#define DataFormats_TestObjects_interface_ThingWithDoNotSort_h
+
+#include <stdexcept>
+#include <cstdint>
+
+#include "DataFormats/Common/interface/traits.h"
+#include "FWCore/Utilities/interface/typedefs.h"
+
+namespace edmtest {
+
+  class ThingWithDoNotSort : public edm::DoNotSortUponInsertion {
+  public:
+    ThingWithDoNotSort() : value_{0} {};
+    explicit ThingWithDoNotSort(cms_int32_t v) : value_{v} {}
+
+    void post_insert() {
+      throw std::logic_error(
+          "post_insert() called for ThingWithDoNotSort that inherits from edm::DoNotSortUponInsertion");
+    }
+
+    int32_t value() const { return value_; }
+
+  private:
+    cms_int32_t value_;
+  };
+
+}  // namespace edmtest
+
+#endif  // DataFormats_TestObjects_interface_ThingWithDoNotSort_h
