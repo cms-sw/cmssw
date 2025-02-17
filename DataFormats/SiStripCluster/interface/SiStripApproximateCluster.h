2bf3498e83b753d2cdc7e02f663e5bfd89dccc17
Vinaya <vinaykrishnanmb@gmail.com>
2025-02-17 14:39:21 +0100

    fix on code-check and code-format

--- DataFormats/SiStripCluster/interface/SiStripApproximateCluster.h
+++ DataFormats/SiStripCluster/interface/SiStripApproximateCluster.h
@@ -28,9 +28,10 @@ public:
                                      bool peakFilter);
 
   float barycenter() const {
-    float _barycenter = compBarycenter_ * maxBarycenter_/maxRange_;
+    float _barycenter = compBarycenter_ * maxBarycenter_ / maxRange_;
     assert(_barycenter <= maxBarycenter_ && "Returning barycenter > maxBarycenter");
-    return _barycenter; }
+    return _barycenter;
+  }
   cms_uint8_t width() const { return width_; }
   cms_uint8_t avgCharge() const { return avgCharge_; }
   bool filter() const { return filter_; }
@@ -44,7 +45,7 @@ private:
   bool filter_ = false;
   bool isSaturated_ = false;
   bool peakFilter_ = false;
-  static constexpr double maxRange_ = 65535.; //16 bit
+  static constexpr double maxRange_ = 65535.;  //16 bit
   static constexpr double maxBarycenter_ = 770.;
   static constexpr double trimMaxADC_ = 30.;
   static constexpr double trimMaxFracTotal_ = .15;
--- DataFormats/SiStripCluster/src/SiStripApproximateCluster.cc
+++ DataFormats/SiStripCluster/src/SiStripApproximateCluster.cc
@@ -1,18 +1,18 @@
 #include "DataFormats/SiStripCluster/interface/SiStripApproximateCluster.h"
 #include "DataFormats/SiStripCluster/interface/SiStripCluster.h"
 #include <algorithm>
+#include <cassert>
 #include <cmath>
-#include <assert.h>
 
 SiStripApproximateCluster::SiStripApproximateCluster(const SiStripCluster& cluster,
                                                      unsigned int maxNSat,
                                                      float hitPredPos,
                                                      bool peakFilter) {
-  compBarycenter_ = std::round(cluster.barycenter() * maxRange_/maxBarycenter_);
+  compBarycenter_ = std::round(cluster.barycenter() * maxRange_ / maxBarycenter_);
   assert(cluster.barycenter() <= maxBarycenter_ && "Got a barycenter > maxBarycenter");
   assert(compBarycenter_ <= maxRange_ && "Filling compBarycenter > maxRange");
   width_ = cluster.size();
-  avgCharge_ = (cluster.charge() + cluster.size()/2) / cluster.size();
+  avgCharge_ = (cluster.charge() + cluster.size() / 2) / cluster.size();
   filter_ = false;
   isSaturated_ = false;
   peakFilter_ = peakFilter;
