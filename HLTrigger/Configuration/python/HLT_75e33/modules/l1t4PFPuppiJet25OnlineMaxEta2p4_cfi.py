dbdb6b37bcc367455a6ecf720b272c0f8f30a85e
Marco Rovere <marco.rovere@cern.ch>
2025-02-17 14:40:44 +0100

    Remove unused L1T paths from the past.
    Remove unused L1T paths from the past.

--- HLTrigger/Configuration/python/HLT_75e33/modules/l1t4PFPuppiJet25OnlineMaxEta2p4_cfi.py
+++
@@ -1,9 +0,0 @@
-import FWCore.ParameterSet.Config as cms
-
-l1t4PFPuppiJet25OnlineMaxEta2p4 = cms.EDFilter("L1TJetFilter",
-    MaxEta = cms.double(2.4),
-    MinEta = cms.double(-2.4),
-    MinN = cms.int32(4),
-    MinPt = cms.double(25.0),
-    inputTag = cms.InputTag("l1tPhase1JetCalibrator9x9trimmed","Phase1L1TJetFromPfCandidates")
-)
