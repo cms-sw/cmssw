56f14563f3e14c583437ba65e7d4976d3d2b6eeb
Sunanda <sunanda.banerjee@cern.ch>
2025-02-18 03:08:07 +0100

    Update the remaining scriptts in Configuration/Geometry for Calorimeter and Muon systems

--- Configuration/Geometry/python/GeometrySimIdealGFlash_cff.py
+++ Configuration/Geometry/python/GeometrySimIdealGFlash_cff.py
@@ -2,8 +2,10 @@ import FWCore.ParameterSet.Config as cms
 
 # Ideal geometry, needed for simulation
 from Geometry.CMSCommonData.cmsIdealGeometryGFlashXML_cfi import *
-from Geometry.TrackerNumberingBuilder.trackerNumberingGeometry_cfi import *
+from Geometry.TrackerNumberingBuilder.trackerNumberingGeometry_cff import *
 from Geometry.EcalCommonData.ecalSimulationParameters_cff import *
 from Geometry.HcalCommonData.hcalDDDSimConstants_cff import *
+from Geometry.HcalCommonData.hcalDDDSimConstants_cff import *
 from Geometry.MuonNumbering.muonGeometryConstants_cff import *
+from Geometry.MuonNumbering.muonOffsetESProducer_cff import *
 
