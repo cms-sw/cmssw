bec516a41f1665d2693725299198a1b9236eff68
Vinaya <vinaykrishnanmb@gmail.com>
2025-02-18 10:54:32 +0100

    renamed the config file

--- Configuration/Eras/python/Era_Run2024_approxSiStripCluster.py
+++ Configuration/Eras/python/Era_Run2024_approxSiStripCluster.py
@@ -4,4 +4,4 @@ from Configuration.Eras.Era_Run3_2024_cff import Run3_2024
 from Configuration.ProcessModifiers.approxSiStripClusters_cff import approxSiStripClusters
 from Configuration.ProcessModifiers.trackingNoLoopers_cff import trackingNoLoopers
 
-Run3_pp_on_PbPb_approxSiStripClusters_2024 = cms.ModifierChain(Run3_2024.copyAndExclude([trackingNoLoopers]),  approxSiStripClusters) 
+Run3_2024_approxSiStripClusters = cms.ModifierChain(Run3_2024.copyAndExclude([trackingNoLoopers]),  approxSiStripClusters) 
