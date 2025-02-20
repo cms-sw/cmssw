6f0d9c0b5b8e4310dba373fab02414ea72b8d9c8
Woohyeon Heo <gjdngus9809@gmail.com>
2025-02-20 14:36:46 +0900

    add description comments

--- L1Trigger/L1TGEM/interface/ME0StubAlgoPatUnit.h
+++ L1Trigger/L1TGEM/interface/ME0StubAlgoPatUnit.h
@@ -18,20 +18,19 @@ namespace l1t {
     std::vector<int> calculate_cluster_size(const std::vector<uint64_t>& data);
     std::vector<int> calculate_hits(const std::vector<uint64_t>& data);
 
-    ME0StubPrimitive pat_unit(
-        const std::vector<uint64_t>& data,
-        const std::vector<std::vector<int>>& bx_data,
-        int strip = 0,
-        int partition = -1,
-        std::vector<int> ly_thresh_patid =
-            {7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 5, 5, 4, 4, 4, 4, 4},  // layer count threshold for 17 pattern ids
-        std::vector<int> ly_thresh_eta = 
-            {4, 5, 4, 5, 4, 5, 4, 5, 4, 5, 4, 5, 4, 5, 4},  // layer count threshold for 8 eta partitions + 7 "virtual" eta partitions
-        int input_max_span = 37,
-        bool skip_centroids = true,
-        int num_or = 2,
-        bool light_hit_count = true,
-        bool verbose = false);
+    ME0StubPrimitive pat_unit(const std::vector<uint64_t>& data,
+                              const std::vector<std::vector<int>>& bx_data,
+                              int strip = 0,
+                              int partition = -1,
+                              std::vector<int> ly_thresh_patid = {7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 5, 5, 4, 4, 4, 4, 4},
+                              // layer count threshold for 17 pattern ids
+                              std::vector<int> ly_thresh_eta = {4, 5, 4, 5, 4, 5, 4, 5, 4, 5, 4, 5, 4, 5, 4},
+                              // layer count threshold for 8 eta partitions + 7 "virtual" eta partitions
+                              int input_max_span = 37,
+                              bool skip_centroids = true,
+                              int num_or = 2,
+                              bool light_hit_count = true,
+                              bool verbose = false);
   }  // namespace me0
 }  // namespace l1t
 #endif
