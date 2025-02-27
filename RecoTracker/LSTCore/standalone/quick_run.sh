77f00806f8e79e5a82b41828e677ad490eacd69d
kk829 <kk829@cornell.edu>
2025-02-27 13:49:45 -0500

    Edited quick_run.sh

--- RecoTracker/LSTCore/standalone/quick_run.sh
+++ RecoTracker/LSTCore/standalone/quick_run.sh
@@ -5,5 +5,5 @@ rm -r LST*.root
 lst_make_tracklooper -mcC;
 lst -i PU200 -l -o LSTNtuple.root;
 createPerfNumDenHists -i LSTNtuple.root -o LSTNumDen.root;
-python3 efficiency/python/lst_plot_performance.py --individual --pt_cut 50 LSTNumDen.root -t "mywork021z";
-cp /mnt/data1/kk829/cmssw/RecoTracker/LSTCore/standalone/performance/mywork021z_a3119eD-PU200/mtv/var/TC_base_0_0_eff_rjet.png ~/public_html/all_rjet_1_zoom/TC_base_0_pT50_jet-pT50_etacut.png
+python3 efficiency/python/lst_plot_performance.py --individual --pt_cut 50 LSTNumDen.root -t "mywork022z";
+# cp /mnt/data1/kk829/cmssw/RecoTracker/LSTCore/standalone/performance/mywork022z_a3119eD-PU200/mtv/var/TC_base_0_0_eff_rjet.png ~/public_html/all_rjet_1_zoom/TC_base_0_pT50_jet-pT100_etacut.png
