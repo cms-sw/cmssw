b72541213bee8780d014ad991d03dd9e4aaeaf58
Luca Guzzi <luca.guzzi@cern.ch>
2025-02-27 16:58:13 +0100

    add missing configuration to the alca beam monitor

--- RecoVertex/BeamSpotProducer/python/BeamSpotOnline_cfi.py
+++ RecoVertex/BeamSpotProducer/python/BeamSpotOnline_cfi.py
@@ -3,5 +3,9 @@ import RecoVertex.BeamSpotProducer.beamSpotOnlineProducer_cfi as _mod
 onlineBeamSpotProducer = _mod.beamSpotOnlineProducer.clone(
                                         src = 'scalersRawToDigi',
                                         setSigmaZ = -1, #negative value disables it.
-                                        gtEvmLabel = 'gtEvmDigis'
+                                        gtEvmLabel = 'gtEvmDigis',
+                                        useBSOnlineRecords = True,
+                                        timeThreshold = 48,
+                                        sigmaZThreshold = 2,
+                                        sigmaXYThreshold = 4
 )
