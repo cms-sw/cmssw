a4e259a3931cf0ae9368f43bdb4ef6d5d19c95b4
Luca Guzzi <luca.guzzi@cern.ch>
2025-02-27 17:39:21 +0100

    fix missing modifier

--- RecoVertex/BeamSpotProducer/python/BeamSpotOnline_cfi.py
+++ RecoVertex/BeamSpotProducer/python/BeamSpotOnline_cfi.py
@@ -3,9 +3,8 @@ import RecoVertex.BeamSpotProducer.beamSpotOnlineProducer_cfi as _mod
 onlineBeamSpotProducer = _mod.beamSpotOnlineProducer.clone(
                                         src = 'scalersRawToDigi',
                                         setSigmaZ = -1, #negative value disables it.
-                                        gtEvmLabel = 'gtEvmDigis',
-                                        useBSOnlineRecords = True,
-                                        timeThreshold = 48,
-                                        sigmaZThreshold = 2,
-                                        sigmaXYThreshold = 4
+                                        gtEvmLabel = 'gtEvmDigis'
 )
+
+from Configuration.Eras.Modifier_run3_common_cff import run3_common
+run3_common.toModify(onlineBeamSpotProducer, useBSOnlineRecords = True, timeThreshold = 48, sigmaZThreshold = 2, sigmaXYThreshold = 4)
