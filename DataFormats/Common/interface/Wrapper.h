1d1bf8629cd3ee807bff0496cc4486caf935e37d
Andrea Bocci <andrea.bocci@cern.ch>
2025-03-04 13:47:46 +0100

    Introduce the TrivialCopyTraits

--- DataFormats/Common/interface/Wrapper.h
+++ DataFormats/Common/interface/Wrapper.h
@@ -7,18 +7,23 @@ Wrapper: A template wrapper around EDProducts to hold the product ID.
 
 ----------------------------------------------------------------------*/
 
-#include "DataFormats/Common/interface/Uninitialized.h"
 #include "DataFormats/Common/interface/CMS_CLASS_VERSION.h"
+#include "DataFormats/Common/interface/TrivialCopyTraits.h"
+#include "DataFormats/Common/interface/Uninitialized.h"
 #include "DataFormats/Common/interface/WrapperBase.h"
 #include "DataFormats/Common/interface/WrapperDetail.h"
 #include "DataFormats/Provenance/interface/ProductID.h"
+#include "FWCore/Utilities/interface/EDMException.h"
+#include "FWCore/Utilities/interface/TypeDemangler.h"
 #include "FWCore/Utilities/interface/Visibility.h"
 
 #include <algorithm>
 #include <cassert>
 #include <memory>
+#include <span>
 #include <string>
 #include <typeinfo>
+#include <vector>
 
 namespace edm {
   template <typename T>
@@ -59,6 +64,7 @@ namespace edm {
     }
 
     bool isPresent_() const override { return present; }
+    void markAsPresent_() override { present = true; }
     std::type_info const& dynamicTypeInfo_() const override { return typeid(T); }
     std::type_info const& wrappedTypeInfo_() const override { return typeid(Wrapper<T>); }
 
@@ -80,6 +86,14 @@ namespace edm {
                           std::vector<void const*>& oPtr) const override;
 
     std::shared_ptr<soa::TableExaminerBase> tableExaminer_() const override;
+
+    bool hasTrivialCopyTraits_() const override;
+    bool hasTrivialCopyProperties_() const override;
+    void trivialCopyInitialize_(edm::AnyBuffer const& args) override;
+    edm::AnyBuffer trivialCopyParameters_() const override;
+    std::vector<std::span<const std::byte>> trivialCopyRegions_() const override;
+    std::vector<std::span<std::byte>> trivialCopyRegions_() override;
+    void trivialCopyFinalize_() override;
 
   private:
     T obj;
@@ -176,13 +190,109 @@ namespace edm {
       }
     };
   }  // namespace soa
+
   template <typename T>
   inline std::shared_ptr<edm::soa::TableExaminerBase> Wrapper<T>::tableExaminer_() const {
     return soa::MakeTableExaminer<T>::make(&obj);
+  }
+
+  template <typename T>
+  inline bool Wrapper<T>::hasTrivialCopyTraits_() const {
+    if constexpr (requires(T& t) { edm::TrivialCopyTraits<T>::regions(t); }) {
+      return true;
+    }
+    return false;
+  }
+
+  template <typename T>
+  inline bool Wrapper<T>::hasTrivialCopyProperties_() const {
+    if constexpr (requires { typename edm::TrivialCopyTraits<T>::Properties; }) {
+      return true;
+    }
+    return false;
+  }
+
+  template <typename T>
+  void Wrapper<T>::trivialCopyInitialize_([[maybe_unused]] edm::AnyBuffer const& args) {
+    if (not present) {
+      throw edm::Exception(edm::errors::LogicError) << "Attempt to access an empty Wrapper";
+    }
+    // if edm::TrivialCopyTraits<T>::Properties is not defined, do not call initialize()
+    if constexpr (not requires { typename edm::TrivialCopyTraits<T>::Properties; }) {
+      return;
+    } else
+      // if edm::TrivialCopyTraits<T>::Properties is void, call initialize() without any additional arguments
+      if constexpr (std::is_same_v<typename edm::TrivialCopyTraits<T>::Properties, void>) {
+        edm::TrivialCopyTraits<T>::initialize(obj);
+      } else
+      // if edm::TrivialCopyTraits<T>::Properties is not void, cast args to Properties and pass it as an additional argument to initialize()
+      {
+        edm::TrivialCopyTraits<T>::initialize(obj, args.cast_to<typename edm::TrivialCopyTraits<T>::Properties>());
+      }
+  }
+
+  template <typename T>
+  inline edm::AnyBuffer Wrapper<T>::trivialCopyParameters_() const {
+    if (not present) {
+      throw edm::Exception(edm::errors::LogicError) << "Attempt to access an empty Wrapper";
+    }
+    // if edm::TrivialCopyTraits<T>::Properties is not defined, do not call properties()
+    if constexpr (not requires { typename edm::TrivialCopyTraits<T>::Properties; }) {
+      return {};
+    } else
+      // if edm::TrivialCopyTraits<T>::Properties is void, do not call properties()
+      if constexpr (std::is_same_v<typename edm::TrivialCopyTraits<T>::Properties, void>) {
+        return {};
+      } else
+      // if edm::TrivialCopyTraits<T>::Properties is not void, call properties() and wrap the result in an edm::AnyBuffer
+      {
+        typename edm::TrivialCopyTraits<T>::Properties p = edm::TrivialCopyTraits<T>::properties(obj);
+        return edm::AnyBuffer(p);
+      }
+  }
+
+  template <typename T>
+  inline std::vector<std::span<const std::byte>> Wrapper<T>::trivialCopyRegions_() const {
+    if (not present) {
+      throw edm::Exception(edm::errors::LogicError) << "Attempt to access an empty Wrapper";
+    }
+    if constexpr (requires(T const& t) { edm::TrivialCopyTraits<T>::regions(t); }) {
+      return edm::TrivialCopyTraits<T>::regions(obj);
+    } else {
+      throw edm::Exception(edm::errors::LogicError)
+          << "edm::TrivialCopyTraits<T>::regions(const T&) is not defined for type "
+          << edm::typeDemangle(typeid(T).name());
+      return {};
+    }
+  }
+
+  template <typename T>
+  inline std::vector<std::span<std::byte>> Wrapper<T>::trivialCopyRegions_() {
+    if (not present) {
+      throw edm::Exception(edm::errors::LogicError) << "Attempt to access an empty Wrapper";
+    }
+    if constexpr (requires(T& t) { edm::TrivialCopyTraits<T>::regions(t); }) {
+      return edm::TrivialCopyTraits<T>::regions(obj);
+    } else {
+      throw edm::Exception(edm::errors::LogicError)
+          << "edm::TrivialCopyTraits<T>::regions(const T&) is not defined for type "
+          << edm::typeDemangle(typeid(T).name());
+      return {};
+    }
+  }
+
+  template <typename T>
+  inline void Wrapper<T>::trivialCopyFinalize_() {
+    if (not present) {
+      throw edm::Exception(edm::errors::LogicError) << "Attempt to access an empty Wrapper";
+    }
+    if constexpr (requires(T& t) { edm::TrivialCopyTraits<T>::finalize(t); }) {
+      edm::TrivialCopyTraits<T>::finalize(obj);
+    }
   }
 
 }  // namespace edm
 
 #include "DataFormats/Common/interface/WrapperView.icc"
 
-#endif
+#endif  // DataFormats_Common_Wrapper_h
