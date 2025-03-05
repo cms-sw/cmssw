d6eb08b9ecff25803765f3020a220512ac4a8b6e
mmusich <marco.musich@cern.ch>
2025-03-05 09:38:00 +0100

    HLT menu development for 15_0_X (branch for 15_1_X)
    These updates correspond to the changes accumulated in the 15_0_X release cycle before the "V1.0" GRun menu for the 2025 pp data-taking period was released.
    This update includes the integration of the following JIRA tickets:
    
    - [CMSHLT-3427](https://its.cern.ch/jira/browse/CMSHLT-3427) [TSG/FOG] Adding MinBias Paths to "FirstCollisions" menu (Run2025A)
    - [CMSHLT-3434](https://its.cern.ch/jira/browse/CMSHLT-3434) [TSG] Move OutputModules from FinalPaths to EndPaths

--- HLTrigger/Configuration/python/HLT_Fake_cff.py
+++ HLTrigger/Configuration/python/HLT_Fake_cff.py
@@ -1,13 +1,13 @@
 # hltGetConfiguration /dev/CMSSW_15_0_0/Fake --cff --data --type Fake
 
-# /dev/CMSSW_15_0_0/Fake/V5 (CMSSW_15_0_0)
+# /dev/CMSSW_15_0_0/Fake/V6 (CMSSW_15_0_0)
 
 import FWCore.ParameterSet.Config as cms
 
 fragment = cms.ProcessFragment( "HLT" )
 
 fragment.HLTConfigVersion = cms.PSet(
-  tableName = cms.string("/dev/CMSSW_15_0_0/Fake/V5")
+  tableName = cms.string("/dev/CMSSW_15_0_0/Fake/V6")
 )
 
 fragment.streams = cms.PSet(  A = cms.vstring( 'InitialPD' ) )
