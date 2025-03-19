e9ef704874849e68bd5a8eacd21d7a4b89ee8462
Andres Rios Tascon <ariostas@gmail.com>
2025-03-19 09:26:20 -0400

    Fix class dict

--- RecoTracker/LST/src/classes.h
+++ RecoTracker/LST/src/classes.h
@@ -2,8 +2,7 @@
 #define RecoTracker_LST_classes_h
 
 #include "DataFormats/Common/interface/Wrapper.h"
-#include "RecoTracker/LST/interface/LSTPixelSeedInput.h"
-#include "RecoTracker/LST/interface/LSTPhase2OTHitsInput.h"
+#include "RecoTracker/LSTCore/interface/LSTInputHostCollection.h"
 #include "RecoTracker/LST/interface/LSTOutput.h"
 
 #endif
