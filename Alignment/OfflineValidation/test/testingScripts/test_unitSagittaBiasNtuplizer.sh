51d399292cf1cbaacebb670eee6a59472eaf6365
mmusich <marco.musich@cern.ch>
2025-03-21 16:38:24 +0100

    add unit test for plotting utility analyzeDiMuonBiases.C

--- Alignment/OfflineValidation/test/testingScripts/test_unitSagittaBiasNtuplizer.sh
+++ Alignment/OfflineValidation/test/testingScripts/test_unitSagittaBiasNtuplizer.sh
@@ -6,5 +6,3 @@ cmsRun ${CMSSW_BASE}/src/Alignment/OfflineValidation/test/SagittaBiasNtuplizer_c
 
 echo "TESTING SagittaBiasNtuplizer Analyser with ALCARECO input..."
 cmsRun ${CMSSW_BASE}/src/Alignment/OfflineValidation/test/SagittaBiasNtuplizer_cfg.py globalTag=142X_mcRun3_2025_realistic_v7 fromRECO=False  myfile=/store/relval/CMSSW_15_1_0_pre1/RelValZMM_14/ALCARECO/TkAlDiMuonAndVertex-142X_mcRun3_2025_realistic_v7_STD_RegeneratedGS_2025_noPU-v1/2580000/b5e3fc09-7b77-42b8-9d69-d37e6fcfb5b8.root || die "Failure running SagittaBiasNtuplizer_cfg.py (with ALCARECO input)" $?
-
-
