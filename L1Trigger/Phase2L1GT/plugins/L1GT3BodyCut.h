fdd1d13cca227c1161c96cabba2ed24e63a16e27
Benjamin Huber <benjamin.huber@cern.ch>
2025-03-23 23:22:14 +0100

    L1GT3BodyCut: Improve readability of dynamic resolution reduction Also use `floor` +1 instead to get the bit beyond the most significant set bit

--- L1Trigger/Phase2L1GT/plugins/L1GT3BodyCut.h
+++ L1Trigger/Phase2L1GT/plugins/L1GT3BodyCut.h
@@ -51,19 +51,23 @@ namespace l1t {
               })),
           scaleNormalShift_(std::round(std::log2(std::ceil(coshEtaLUT_.output_scale() / coshEtaLUT2_.output_scale())))),
           invMassResolutionReduceShift_([&]() {
+            // Computation of the dynamic input two-body mass resolution w.r.t. the cut value.
+            // The result is a resolution of inputs between 2^-15 to 2^-16 of the cut value.
             if (minInvMassSqrDiv2_) {
-              return std::max<int>(std::ceil(std::log2(minInvMassSqrDiv2_.value() + 1.0)) - 16, 0);
+              return std::max<int>(std::floor(std::log2(minInvMassSqrDiv2_.value())) + 1 - CALC_BITS, 0);
             } else if (maxInvMassSqrDiv2_) {
-              return std::max<int>(std::ceil(std::log2(maxInvMassSqrDiv2_.value())) - 16, 0);
+              return std::max<int>(std::floor(std::log2(maxInvMassSqrDiv2_.value())) + 1 - CALC_BITS, 0);
             } else {
               return 0;
             }
           }()),
           transMassResolutionReduceShift_([&]() {
+            // Computation of the dynamic input two-body mass resolution w.r.t. the cut value.
+            // The result is a resolution of inputs between 2^-15 to 2^-16 of the cut value.
             if (minTransMassSqrDiv2_) {
-              return std::max<int>(std::ceil(std::log2(minTransMassSqrDiv2_.value() + 1.0)) - 16, 0);
+              return std::max<int>(std::floor(std::log2(minTransMassSqrDiv2_.value())) + 1 - CALC_BITS, 0);
             } else if (maxTransMassSqrDiv2_) {
-              return std::max<int>(std::ceil(std::log2(maxTransMassSqrDiv2_.value())) - 16, 0);
+              return std::max<int>(std::floor(std::log2(maxTransMassSqrDiv2_.value())) + 1 - CALC_BITS, 0);
             } else {
               return 0;
             }
@@ -108,6 +112,7 @@ namespace l1t {
 
   private:
     static constexpr int HW_PI = 1 << (P2GTCandidate::hwPhi_t::width - 1);  // assumes phi in [-pi, pi)
+    static constexpr int CALC_BITS = 16;                                    // Allocate 16 bits to the calculation
 
     int64_t calc2BodyInvMass(const P2GTCandidate& obj1,
                              const P2GTCandidate& obj2,
