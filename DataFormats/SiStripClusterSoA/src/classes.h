de84aceb90c1279754e9060c1ff4f0d6b8e6fcf2
Pietro Grutta <pietrogrutta@gmail.com>
2025-03-24 22:15:37 +0100

    Moved StdArray to DataFormats/Common, according to @makortel comment (discussion_r2010918025)

--- DataFormats/SiStripClusterSoA/src/classes.h
+++ DataFormats/SiStripClusterSoA/src/classes.h
@@ -1,7 +1,6 @@
 #ifndef DataFormats_SiStripClusterSoA_src_classes_h
 #define DataFormats_SiStripClusterSoA_src_classes_h
 
-#include "DataFormats/Common/interface/StdArray.h"
 #include "DataFormats/Common/interface/Wrapper.h"
 #include "DataFormats/SiStripClusterSoA/interface/SiStripClustersHost.h"
 #include "DataFormats/SiStripClusterSoA/interface/SiStripClustersSoA.h"
