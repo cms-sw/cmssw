dabfc4d1c1528b093d1c827e1507827f34a18a47
Pietro Grutta <pietrogrutta@gmail.com>
2025-03-24 22:17:11 +0100

    Removed useless check according to @makortel comment (discussion_r2010820275)

--- RecoLocalTracker/SiStripClusterizer/plugins/alpaka/SiStripRawToClusterAlgo.dev.cc
+++ RecoLocalTracker/SiStripClusterizer/plugins/alpaka/SiStripRawToClusterAlgo.dev.cc
@@ -1,8 +1,3 @@
-// Check that ALPAKA_HOST_ONLY is not defined during device compilation:
-#ifdef ALPAKA_HOST_ONLY
-#error ALPAKA_HOST_ONLY defined in device compilation
-#endif
-
 #include <alpaka/alpaka.hpp>
 
 #include "HeterogeneousCore/AlpakaInterface/interface/config.h"
