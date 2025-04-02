3d1b919f3dff8e2cffb064f5b99ff7c1091f1f00
brusale <brusale11@gmail.com>
2025-04-02 23:01:53 +0200

    add BarrelValidation plots

--- Validation/Configuration/python/globalValidation_cff.py
+++ Validation/Configuration/python/globalValidation_cff.py
@@ -46,7 +46,6 @@ from Validation.Configuration.hgcalSimValid_cff import *
 from Validation.Configuration.barrelSimValid_cff import *
 from Validation.Configuration.mtdSimValid_cff import *
 from Validation.Configuration.ecalSimValid_cff import *
-from Validation.Configuration.barrelSimValid_cff import *
 from Validation.SiTrackerPhase2V.Phase2TrackerValidationFirstStep_cff import *
 
 # filter/producer "pre-" sequence for globalValidation
@@ -202,15 +201,17 @@ globalPrevalidationHGCal = cms.Sequence(hgcalAssociators, ticlSimTrackstersTask)
 
 globalValidationHGCal = cms.Sequence(hgcalValidation)
 
-_ticlBarrel_globalPrevalidation = globalPrevalidationHGCal.copy()
-_ticlBarrel_globalPrevalidation += cms.Sequence(barrelAssociators)
+globalPrevalidationBarrel = cms.Sequence()
+_globalPrevalidationBarrel = globalPrevalidationBarrel.copy()
+_globalPrevalidationBarrel += cms.Sequence(barrelAssociators)
 
-_ticlBarrel_globalValidation = globalValidationHGCal.copy()
-_ticlBarrel_globalValidation += barrelValidation
+globalValidationBarrel = cms.Sequence()
+_globalValidationBarrel = globalValidationBarrel.copy()
+_globalValidationBarrel += barrelValidation
 
 from Configuration.ProcessModifiers.ticl_barrel_cff import ticl_barrel
-ticl_barrel.toReplaceWith(globalPrevalidationHGCal, _ticlBarrel_globalPrevalidation)
-ticl_barrel.toReplaceWith(globalValidationHGCal, _ticlBarrel_globalValidation)
+ticl_barrel.toReplaceWith(globalPrevalidationBarrel, _globalPrevalidationBarrel)
+ticl_barrel.toReplaceWith(globalValidationBarrel, _globalValidationBarrel)
 
 globalValidationMTD = cms.Sequence()
 
