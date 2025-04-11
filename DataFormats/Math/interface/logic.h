57f5037a39a3dedfec11288cd508df828c4f7922
Kevin Pedro <kpedro88@gmail.com>
2025-04-11 09:06:47 -0500

    introduce function to perform branchless 'and' operation

---
+++ DataFormats/Math/interface/logic.h
@@ -0,0 +1,13 @@
+#ifndef DataFormats_Math_logic_h
+#define DataFormats_Math_logic_h
+
+namespace reco {
+  // this function can be called with any boolean expressions as the parameters
+  // this forces the evaluation of both expressions (faster if the expressions are simple)
+  // and applying && to two bools avoids branching (jump instruction)
+  // whereas applying && to the two original expressions may cause branching
+  // this is an alternative to using the bitwise and operator (&), which never short-circuits
+  bool branchless_and(bool a, bool b) { return a && b; }
+}  // namespace reco
+
+#endif
