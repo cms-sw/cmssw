6f5d87d343faa5b27e9243aefa26319d8918e9cd
Christian Winter <c.winter@kit.edu>
2025-04-13 21:02:06 +0200

    add tau embedding process modifiers

---
+++ Configuration/ProcessModifiers/python/tau_embedding_emu_cff.py
@@ -0,0 +1,4 @@
+import FWCore.ParameterSet.Config as cms
+
+# This modifier sets the correct cuts for the taus decaying into one electron and one muon. More info can be found in TauAnalysis/MCEmbeddingTools.
+tau_embedding_emu = cms.Modifier()
