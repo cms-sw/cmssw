71e07a4fa29fc913b86a5d1d35622a4d2fd31910
Marco Musich <marco.musich@cern.ch>
2025-04-13 15:04:21 +0200

    change era to Run3_2025 in a selected online DQM clients

--- DQM/Integration/python/clients/beam_dqm_sourceclient-live_cfg.py
+++ DQM/Integration/python/clients/beam_dqm_sourceclient-live_cfg.py
@@ -13,8 +13,8 @@ if 'runkey=hi_run' in sys.argv:
   from Configuration.Eras.Era_Run3_pp_on_PbPb_approxSiStripClusters_cff import Run3_pp_on_PbPb_approxSiStripClusters
   process = cms.Process("BeamMonitorLegacy", Run3_pp_on_PbPb_approxSiStripClusters)
 else:
-  from Configuration.Eras.Era_Run3_cff import Run3
-  process = cms.Process("BeamMonitorLegacy", Run3)
+  from Configuration.Eras.Era_Run3_2025_cff import Run3_2025
+  process = cms.Process("BeamMonitorLegacy", Run3_2025)
 
 process.load('FWCore.MessageService.MessageLogger_cfi')
 process.MessageLogger.debugModules = cms.untracked.vstring('*')
