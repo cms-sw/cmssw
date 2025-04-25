1175c8be4f9050bf92ddb4b877362844fb25e814
Joshua Hiltbrand <joshua.hiltbrand@gmail.com>
2025-04-25 12:18:56 +0200

    adjust hardcode HCAL TP channel params (as used for phase 2)

--- CalibCalorimetry/HcalPlugins/python/Hcal_PCCUpdate_cff.py
+++ CalibCalorimetry/HcalPlugins/python/Hcal_PCCUpdate_cff.py
@@ -7,3 +7,6 @@ PCCUpdate = cms.PSet(
 
 from Configuration.Eras.Modifier_run3_common_cff import run3_common
 run3_common.toModify(PCCUpdate, applyFixPCC = True)
+
+from Configuration.Eras.Modifier_phase2_common_cff import phase2_common
+phase2_common.toModify(PCCUpdate, applyFixPCC = True)
