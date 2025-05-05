6d7875b88de165fdd0d5ee7227735b951214e0bc
Huilin Qu <huilin.qu@cern.ch>
2025-05-05 16:59:54 +0200

    Lower the muon pt threshold for nanoAOD.

--- Configuration/Eras/python/Era_Run3_2025_cff.py
+++ Configuration/Eras/python/Era_Run3_2025_cff.py
@@ -5,6 +5,7 @@ from Configuration.Eras.Modifier_run3_GEM_2025_cff import run3_GEM_2025
 from Configuration.Eras.Modifier_run3_CSC_2025_cff import run3_CSC_2025
 from Configuration.Eras.Modifier_stage2L1Trigger_2025_cff import stage2L1Trigger_2025
 from Configuration.Eras.Modifier_run3_SiPixel_2025_cff import run3_SiPixel_2025
+from Configuration.Eras.Modifier_run3_nanoAOD_2025_cff import run3_nanoAOD_2025
 from Configuration.ProcessModifiers.ecal_cctiming_cff import ecal_cctiming
 
-Run3_2025 = cms.ModifierChain(Run3_2024, run3_GEM_2025, stage2L1Trigger_2025, run3_SiPixel_2025, run3_CSC_2025, ecal_cctiming)
+Run3_2025 = cms.ModifierChain(Run3_2024, run3_GEM_2025, stage2L1Trigger_2025, run3_SiPixel_2025, run3_CSC_2025, run3_nanoAOD_2025, ecal_cctiming)
---
+++ Configuration/Eras/python/Modifier_run3_nanoAOD_2025_cff.py
@@ -0,0 +1,4 @@
+import FWCore.ParameterSet.Config as cms
+
+# for 2025 data-taking (and possibly also 2026)
+run3_nanoAOD_2025 = cms.Modifier()
