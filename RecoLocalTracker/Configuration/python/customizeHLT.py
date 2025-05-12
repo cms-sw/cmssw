9d233f49a7dd1847a8e9aeb58d77b6a851057a59
Matej Roguljic <matej.roguljic@gmail.com>
2025-05-12 08:09:27 +0200

    add goodEdgeAlgo wfs and hlt customisation

---
+++ RecoLocalTracker/Configuration/python/customizeHLT.py
@@ -0,0 +1,15 @@
+import FWCore.ParameterSet.Config as cms
+
+# helper functions
+from HLTrigger.Configuration.common import *
+
+
+# Enable good edge CPE algorithm in pixel local reconstruction
+def customiseFor47966(process):
+    for prod in esproducers_by_type(process, 'PixelCPEGenericESProducer', 'PixelCPEFastParamsESProducerAlpakaPhase1@alpaka'):
+        if not hasattr(prod, 'GoodEdgeAlgo'):
+            prod.GoodEdgeAlgo = cms.bool(True)
+        else:
+            prod.GoodEdgeAlgo = True
+
+    return process 
