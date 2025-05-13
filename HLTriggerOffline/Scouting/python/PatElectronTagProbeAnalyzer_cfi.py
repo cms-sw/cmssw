f16489095b3c25087fe48228a1efa658b5ea2070
AlbertHsuNTUphys <b07202037@ntu.edu.tw>
2025-05-13 18:53:15 +0200

    [L1 seed] replace for 2025 data taking

--- HLTriggerOffline/Scouting/python/PatElectronTagProbeAnalyzer_cfi.py
+++ HLTriggerOffline/Scouting/python/PatElectronTagProbeAnalyzer_cfi.py
@@ -5,7 +5,10 @@ DoubleEGL1 = [
   "L1_DoubleEG_LooseIso16_LooseIso12_er1p5",
   "L1_DoubleEG_LooseIso18_LooseIso12_er1p5",
   "L1_DoubleEG_LooseIso20_LooseIso12_er1p5",
-  "L1_DoubleEG_LooseIso22_LooseIso12_er1p5"
+  "L1_DoubleEG_LooseIso22_LooseIso12_er1p5",
+  "L1_DoubleEG15_11_er1p2_dR_Max0p6",
+  "L1_DoubleEG16_11_er1p2_dR_Max0p6",
+  "L1_DoubleEG17_11_er1p2_dR_Max0p6"
 ]
 
 SinglePhotonL1 = [
