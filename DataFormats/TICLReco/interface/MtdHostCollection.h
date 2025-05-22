b391a0b6a887a134e7f3524a1bf4f479cf25f8c4
valsdav <davide.valsecchi@cern.ch>
2025-05-22 10:53:00 +0200

    Renaming HGCALReco TICL classes to TICLReco

--- DataFormats/TICLReco/interface/MtdHostCollection.h
+++ DataFormats/TICLReco/interface/MtdHostCollection.h
@@ -1,8 +1,8 @@
-#ifndef DataFormats_HGCalReco_MtdHostCollection_h
-#define DataFormats_HGCalReco_MtdHostCollection_h
+#ifndef DataFormats_TICLReco_MtdHostCollection_h
+#define DataFormats_TICLReco_MtdHostCollection_h
 
 #include "DataFormats/Portable/interface/PortableHostCollection.h"
-#include "DataFormats/HGCalReco/interface/MtdSoA.h"
+#include "DataFormats/TICLReco/interface/MtdSoA.h"
 
 // MtdSoA in host memory
 using MtdHostCollection = PortableHostCollection<MtdSoA>;
--- DataFormats/TICLReco/src/classes.cc
+++ DataFormats/TICLReco/src/classes.cc
@@ -1,5 +1,5 @@
 #include "DataFormats/Portable/interface/PortableHostCollectionReadRules.h"
 #include "DataFormats/Portable/interface/PortableHostObjectReadRules.h"
-#include "DataFormats/HGCalReco/interface/MtdHostCollection.h"
+#include "DataFormats/TICLReco/interface/MtdHostCollection.h"
 
 SET_PORTABLEHOSTCOLLECTION_READ_RULES(MtdHostCollection);
