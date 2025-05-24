e98c85ae163b92354949da15eb1ad9c77e825fc0
Marco Rovere <marco.rovere@cern.ch>
2025-05-24 14:51:03 +0200

    Fix size of allocated buffer.

--- RecoTracker/PixelSeeding/plugins/alpaka/CAHitNtupletGeneratorKernels.dev.cc
+++ RecoTracker/PixelSeeding/plugins/alpaka/CAHitNtupletGeneratorKernels.dev.cc
@@ -54,6 +54,69 @@ namespace ALPAKA_ACCELERATOR_NAMESPACE {
     int nCellsToCells = std::max(int(maxDoublets * algoParams.avgCellsPerCell_), 1);
     int nCellsToTracks = std::max(int(maxDoublets * algoParams.avgTracksPerCell_), 1);
 
+    // Cell -> Neighbor Cells
+    device_cellToNeighbors_{cms::alpakatools::make_device_buffer<GenericContainer>(queue)},
+      device_cellToNeighborsStorage_{cms::alpakatools::make_device_buffer<GenericContainerStorage[]>(
+            queue, int(maxDoublets * m_params.algoParams_.avgCellsPerCell_) + 1)},
+      device_cellToNeighborsOffsets_{
+      cms::alpakatools::make_device_buffer<GenericContainerOffsets[]>(queue, maxDoublets + 1)},
+      device_cellToNeighborsView_{device_cellToNeighbors_.data(),
+	device_cellToNeighborsOffsets_.data(),
+	device_cellToNeighborsStorage_.data(),
+	int(maxDoublets + 1),
+	int(maxDoublets * m_params.algoParams_.avgCellsPerCell_)},
+      
+      // Cell -> Tracks
+      device_cellToTracks_{cms::alpakatools::make_device_buffer<GenericContainer>(queue)},
+      device_cellToTracksStorage_{cms::alpakatools::make_device_buffer<GenericContainerStorage[]>(
+            queue, int(maxDoublets * m_params.algoParams_.avgTracksPerCell_) + 1)},
+      device_cellToTracksOffsets_{
+      cms::alpakatools::make_device_buffer<GenericContainerOffsets[]>(queue, maxDoublets + 1)},
+      device_cellToTracksView_{device_cellToTracks_.data(),
+	device_cellToTracksOffsets_.data(),
+	device_cellToTracksStorage_.data(),
+	int(maxDoublets + 1),
+	int(maxDoublets * m_params.algoParams_.avgTracksPerCell_) + 1},
+      
+      // Tracks -> Hits
+      device_hitContainer_{cms::alpakatools::make_device_buffer<SequentialContainer>(queue)},
+      device_hitContainerStorage_{cms::alpakatools::make_device_buffer<SequentialContainerStorage[]>(
+            queue, int(m_params.algoParams_.avgHitsPerTrack_ * maxTuples) + 1)},
+      device_hitContainerOffsets_{
+      cms::alpakatools::make_device_buffer<SequentialContainerOffsets[]>(queue, maxTuples + 1)},
+      device_hitContainerView_{device_hitContainer_.data(),
+	device_hitContainerOffsets_.data(),
+	device_hitContainerStorage_.data(),
+	int(maxTuples + 1),
+	int(m_params.algoParams_.avgHitsPerTrack_ * maxTuples) + 1},
+      
+      // No.Hits -> Track (Multiplicity)
+      device_tupleMultiplicity_{cms::alpakatools::make_device_buffer<GenericContainer>(queue)},
+      device_tupleMultiplicityStorage_{
+      cms::alpakatools::make_device_buffer<GenericContainerStorage[]>(queue, maxTuples)},
+      device_tupleMultiplicityOffsets_{
+      cms::alpakatools::make_device_buffer<GenericContainerOffsets[]>(queue, TrackerTraits::maxHitsOnTrack + 1)},
+      device_tupleMultiplicityView_{device_tupleMultiplicity_.data(),
+	device_tupleMultiplicityOffsets_.data(),
+	device_tupleMultiplicityStorage_.data(),
+	int(TrackerTraits::maxHitsOnTrack + 1),
+	int(maxTuples)},
+      
+      // Structures and Counters Storage
+      device_simpleCells_{cms::alpakatools::make_device_buffer<SimpleCell[]>(queue, maxDoublets)},
+      device_extraStorage_{
+      cms::alpakatools::make_device_buffer<cms::alpakatools::AtomicPairCounter::DoubleWord[]>(queue, 5u)},
+      device_hitTuple_apc_{reinterpret_cast<cms::alpakatools::AtomicPairCounter *>(device_extraStorage_.data())},
+      device_nCells_{
+      cms::alpakatools::make_device_view(queue, *reinterpret_cast<uint32_t *>(device_extraStorage_.data() + 2))},
+      device_nTriplets_{
+      cms::alpakatools::make_device_view(queue, *reinterpret_cast<uint32_t *>(device_extraStorage_.data() + 3))},
+      device_nCellTracks_{
+            cms::alpakatools::make_device_view(queue, *reinterpret_cast<uint32_t *>(device_extraStorage_.data() + 4))},
+      deviceTriplets_{CACoupleSoACollection(maxDoublets * m_params.algoParams_.avgCellsPerCell_, queue)},
+      deviceTracksCells_{
+      CACoupleSoACollection(int(maxDoublets * m_params.algoParams_.avgTracksPerCell_) + 1, queue)} {
+      
 #ifdef GPU_DEBUG
     std::cout << "Allocation for tuple building with: " << std::endl;
     std::cout << "- nHits          = " << nHits << std::endl;
