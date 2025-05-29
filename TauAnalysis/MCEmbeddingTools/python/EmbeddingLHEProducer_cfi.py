f4fcef85fe374ec927d3cfbb81e92bef6ee1f4c9
Christian Winter <c.winter@kit.edu>
2025-05-29 19:56:26 +0200

    first version without customize, but not HLT step
    (cherry picked from commit 030bd7cc9959b709bf34be53bc163ff4733e24d6)

--- TauAnalysis/MCEmbeddingTools/python/EmbeddingLHEProducer_cfi.py
+++ TauAnalysis/MCEmbeddingTools/python/EmbeddingLHEProducer_cfi.py
@@ -1,7 +1,11 @@
 import FWCore.ParameterSet.Config as cms
 from Configuration.ProcessModifiers.tau_embedding_mu_to_mu_cff import tau_embedding_mu_to_mu
 from Configuration.ProcessModifiers.tau_embedding_mu_to_e_cff import tau_embedding_mu_to_e
+from Configuration.Eras.Modifier_run2_common_cff import run2_common
+from Configuration.Eras.Modifier_run3_common_cff import run3_common
+from RecoLuminosity.LumiProducer.bunchSpacingProducer_cfi import bunchSpacingProducer
 
+generalModifier = run2_common | run3_common
 
 externalLHEProducer = cms.EDProducer("EmbeddingLHEProducer",
     src = cms.InputTag("selectedMuonsForEmbedding","",""),
@@ -14,7 +18,10 @@ externalLHEProducer = cms.EDProducer("EmbeddingLHEProducer",
 )
 # if running mu->mu embedding simulate muon (pid=13) instead of a tau (pid=15)
 tau_embedding_mu_to_mu.toModify(externalLHEProducer, particleToEmbed = cms.int32(13))
-# if running mu->e embedding simulate muon (pid=11) instead of a tau (pid=15)
+# if running mu->e embedding simulate electron (pid=11) instead of a tau (pid=15)
 tau_embedding_mu_to_e.toModify(externalLHEProducer, particleToEmbed = cms.int32(11))
 
-makeexternalLHEProducer = cms.Sequence(externalLHEProducer)
+generalModifier.toModify(bunchSpacingProducer, bunchSpacingOverride = cms.uint32(25), overrideBunchSpacing = cms.bool(True))
+
+
+embeddingLHEProducerTask = cms.Sequence(externalLHEProducer)
