3cc0d53e3cf359c5eeeec343ffb9896cfe5fd797
Carlo Battilana <carlo.battilana@gmail.com>
2025-06-02 15:42:14 +0200

    Add further information for GEM and RPC MUODPG BKG ntuples and for DT ZMu ntuples

--- Configuration/PyReleaseValidation/python/relval_nano.py
+++ Configuration/PyReleaseValidation/python/relval_nano.py
@@ -265,7 +265,7 @@ steps['scoutingNANO_withPrompt_data14.0'] = merge([{'-s': 'NANO:@Prompt+@ScoutMo
 steps['muDPGNANO_data14.0'] = merge([{'-s': 'RAW2DIGI,NANO:@MUDPG', '-n': '100'},
                                      steps['NANO_data14.0']])
 
-steps['muDPGNANOBkg_data14.0'] = merge([{'-s': 'RAW2DIGI,NANO:@MUDPGBKG', '-n': '100'},
+steps['muDPGNANOBkg_data14.0'] = merge([{'-s': 'RAW2DIGI,RECO:localreco,NANO:@MUDPGBKG', '-n': '100'},
                                         steps['NANO_data14.0']])
 
 steps['hcalDPGNANO_data14.0'] = merge([{'-s': 'RAW2DIGI,RECO,NANO:@HCAL', '-n': '100',
