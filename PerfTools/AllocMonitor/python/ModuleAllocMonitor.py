b1cc982379b44092c44be3c3c6dfe0ded1f4bf61
Patrick Gartung <gartung@fnal.gov>
2025-06-06 20:46:10 +0200

    Use full module name

--- PerfTools/AllocMonitor/python/ModuleAllocMonitor.py
+++ PerfTools/AllocMonitor/python/ModuleAllocMonitor.py
@@ -1,6 +1,6 @@
 import FWCore.ParameterSet.Config as cms
 def customise(process):
-    process.AllocMonitor = cms.Service("ModuleAllocMonitor",
+    process.ModuleAllocMonitor = cms.Service("ModuleAllocMonitor",
                                    fileName=cms.untracked.string("moduleAllocMonitor.log")
                                    )
     return(process)
