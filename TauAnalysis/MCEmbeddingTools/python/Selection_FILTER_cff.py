f4d649cb6d67529897b68eab630e4ffc243d70a7
Christian Winter <c.winter@kit.edu>
2025-06-06 17:24:55 +0200

    add comments to filter python config fragment
    (cherry picked from commit c5586ea1c03cb9804511b5355ec091899ae04ae7)

--- TauAnalysis/MCEmbeddingTools/python/Selection_FILTER_cff.py
+++ TauAnalysis/MCEmbeddingTools/python/Selection_FILTER_cff.py
@@ -1,3 +1,25 @@
+"""
+This config fragment filters for a muon pair which can be used for tau embedding.
+It selects muons based on trigger conditions, kinematic cuts, and identification criteria.
+It then produces a collection of muons suitable for embedding (selectedMuonsForEmbedding), including their kinematic properties.
+This is then later used in the LHE step to simulate two taus with slightly adjusted kinematics.
+To use this config fragment, a cmsDriver command like the following can be used:
+```
+cmsDriver.py SELECT \
+    --step RAW2DIGI,L1Reco,RECO,PAT,FILTER:TauAnalysis/MCEmbeddingTools/Selection_FILTER_cff.makePatMuonsZmumuSelection \
+    --processName SELECT \
+    --data \
+    --scenario pp \
+    --eventcontent RAWRECO \
+    --datatier RAWRECO \
+    --outputCommands 'keep *_selectedMuonsForEmbedding_*_SELECT','keep *_patMuonsAfterID_*_SELECT','keep *_slimmedMuons_*_SELECT','keep *_slimmedMuonTrackExtras_*_SELECT','keep recoVertexs_offlineSlimmedPrimaryVertices_*_SELECT','keep *_firstStepPrimaryVertices_*_SELECT','keep *_ecalDrivenElectronSeeds_*_SELECT' \
+    --era ... \
+    --conditions ... \
+    --filein ... \
+    --fileout ...
+```
+"""
+
 import FWCore.ParameterSet.Config as cms
 from Configuration.Eras.Modifier_run2_HLTconditions_2016_cff import (
     run2_HLTconditions_2016,
@@ -20,7 +42,7 @@ doubleMuonHLTTrigger = cms.EDFilter("TriggerResultsFilter",
     triggerConditions = cms.vstring("HLT_Mu17_TrkIsoVVL_Mu8_TrkIsoVVL_DZ_Mass3p8_v*") # unprescaled trigger for 2018,22,23,24 (see https://twiki.cern.ch/twiki/bin/view/CMS/MuonHLT2018, https://twiki.cern.ch/twiki/bin/view/CMS/MuonHLT2022, https://twiki.cern.ch/twiki/bin/view/CMS/MuonHLT2023, https://twiki.cern.ch/twiki/bin/view/CMS/MuonHLT2024)
 )
 
-#### change the used triggers for run2 ####
+# change the used triggers for run2
 # Use two different triggers as the Mass8 one has a higer luminosity in 2017 according to https://cmshltinfo.app.cern.ch/summary?search=HLT_Mu17_TrkIsoVVL_Mu8_TrkIsoVVL_DZ_Mass&year=2017&paths=true&prescaled=true&stream-types=Physics
 # probably because he was already active in earlier runs than the Mass3p8 trigger
 # Both are unprescaled
@@ -38,38 +60,14 @@ patMuonsAfterKinCuts = cms.EDFilter("PATMuonSelector",
     filter = cms.bool(True)
 )
 
-
-# For impact parameter (w.r.t. to PV) requirements, a vector collection is needed, therefore only dB < 0.2 required.
-# The default requirements (in C++):
-# 1) fabs(recoMu.muonBestTrack()->dxy(vertex->position())) < 0.2 ----> similar to dB < 0.2
-# 2) fabs(recoMu.muonBestTrack()->dz(vertex->position())) < 0.5
-patMuonsAfterTightID = cms.EDFilter("PATMuonSelector",
-    src = cms.InputTag("patMuonsAfterKinCuts"),
-    cut = cms.string(
-    "isPFMuon && isGlobalMuon"
-    " && muonID('GlobalMuonPromptTight')"
-    " && numberOfMatchedStations > 1"
-    " && innerTrack.hitPattern.trackerLayersWithMeasurement > 5"
-    " && innerTrack.hitPattern.numberOfValidPixelHits > 0"
-    " && dB < 0.2"
-    ),
-    filter = cms.bool(True)
-)
-
-patMuonsAfterMediumID = cms.EDFilter("PATMuonSelector",
-    src = cms.InputTag("patMuonsAfterKinCuts"),
-    cut = cms.string("isMediumMuon"),
-    filter = cms.bool(True)
-)
-
-patMuonsAfterLooseID = cms.EDFilter("PATMuonSelector",
+## require loose muon ID
+patMuonsAfterID = cms.EDFilter("PATMuonSelector",
     src = cms.InputTag("patMuonsAfterKinCuts"),
     cut = cms.string("isLooseMuon"),
     filter = cms.bool(True)
 )
 
-patMuonsAfterID = patMuonsAfterLooseID.clone()
-
+## create Z->mumu candidates
 ZmumuCandidates = cms.EDProducer("CandViewShallowCloneCombiner",
     checkCharge = cms.bool(True),
     # require one of the muons with pT > 17 GeV, and an invariant mass > 20 GeV
@@ -77,13 +75,16 @@ ZmumuCandidates = cms.EDProducer("CandViewShallowCloneCombiner",
     decay = cms.string("patMuonsAfterID@+ patMuonsAfterID@-")
 )
 
-
+## require at least one Z->mumu candidate
 ZmumuCandidatesFilter = cms.EDFilter("CandViewCountFilter",
     src = cms.InputTag("ZmumuCandidates"),
     minNumber = cms.uint32(1)
     # filter = cms.bool(True)
 )
 
+## Create a collection of muons suitable for embedding
+# The MuMuForEmbeddingSelector therefore selects the muons from the Z->mumu candidates,
+# where the Z->mumu candidate mass is the highest.
 selectedMuonsForEmbedding = cms.EDProducer("MuMuForEmbeddingSelector",
     ZmumuCandidatesCollection = cms.InputTag("ZmumuCandidates"),
     inputTagVertex = cms.InputTag("offlinePrimaryVertices"),
@@ -100,7 +101,3 @@ makePatMuonsZmumuSelection = cms.Sequence(
     + ZmumuCandidatesFilter
     + selectedMuonsForEmbedding
 )
-
-# (run2_common & ~run3_common).toModify(bunchSpacingProducer, bunchSpacingOverride = cms.uint32(25), overrideBunchSpacing = cms.bool(True))
-# does not work...
-# bunchSpacingProducer, overrideBunchSpacing = cms.bool(True)
