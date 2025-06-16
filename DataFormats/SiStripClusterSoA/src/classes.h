4e7aec1182050ea0eb25b796e7bcdfa83bee695f
Pietro Grutta <pietrogrutta@gmail.com>
2025-06-16 19:07:55 +0200

    Renamed SiStripClustersSoA to SiStripClusterSoA Cleaned README. Removed commented tests (do not detached checkout here)

--- DataFormats/SiStripClusterSoA/src/classes.h
+++ DataFormats/SiStripClusterSoA/src/classes.h
@@ -2,7 +2,7 @@
 #define DataFormats_SiStripClusterSoA_src_classes_h
 
 #include "DataFormats/Common/interface/Wrapper.h"
-#include "DataFormats/SiStripClusterSoA/interface/SiStripClustersHost.h"
-#include "DataFormats/SiStripClusterSoA/interface/SiStripClustersSoA.h"
+#include "DataFormats/SiStripClusterSoA/interface/SiStripClusterHost.h"
+// #include "DataFormats/SiStripClusterSoA/interface/SiStripClusterSoA.h"
 
 #endif  // DataFormats_SiStripClusterSoA_src_classes_h
