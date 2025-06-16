aee6f6c315138765ddeed4972a40119c53ccab7b
Fabio Cossutti <fabio.cossutti@ts.infn.it>
2025-06-16 13:17:20 +0200

    Move baseline MTD test geometry version to D121

--- Geometry/MTDCommonData/python/defaultMTDConditionsEra_cff.py
+++ Geometry/MTDCommonData/python/defaultMTDConditionsEra_cff.py
@@ -1,6 +1,6 @@
 import Configuration.Geometry.defaultPhase2ConditionsEra_cff as _centralgeo
 
-MTD_DEFAULT_VERSION = "Run4D110"
+MTD_DEFAULT_VERSION = "Run4D121"
 
 def check_mtdgeo():
     if MTD_DEFAULT_VERSION != _centralgeo.DEFAULT_VERSION:
