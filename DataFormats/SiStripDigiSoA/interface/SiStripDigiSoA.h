0d5fd599c2ef6c1b76489f69e5b9941fde68902c
Pietro Grutta <pietrogrutta@gmail.com>
2025-06-16 19:17:41 +0200

    Implemented SiStripDigi dataformat

--- DataFormats/SiStripDigiSoA/interface/SiStripDigiSoA.h
+++ DataFormats/SiStripDigiSoA/interface/SiStripDigiSoA.h
@@ -5,18 +5,17 @@
 #include "DataFormats/SoATemplate/interface/SoALayout.h"
 
 namespace sistrip {
-  GENERATE_SOA_LAYOUT(
-    SiStripDigiSoALayout,
-    SOA_COLUMN(uint8_t, adc),
-    SOA_COLUMN(uint16_t, channel),
-    SOA_COLUMN(uint16_t, stripId),
-    //
-    SOA_SCALAR(uint32_t, nbGoodCandidates)
-  )
+  GENERATE_SOA_LAYOUT(SiStripDigiSoALayout,
+                      SOA_COLUMN(uint8_t, adc),
+                      SOA_COLUMN(uint16_t, channel),
+                      SOA_COLUMN(uint16_t, stripId),
+                      //
+                      SOA_SCALAR(uint32_t, nbGoodCandidates),
+                      SOA_SCALAR(uint32_t, nbCandidates))
 
   using SiStripDigiSoA = SiStripDigiSoALayout<>;
   using SiStripDigiView = SiStripDigiSoA::View;
   using SiStripDigiConstView = SiStripDigiSoA::ConstView;
-}
+}  // namespace sistrip
 
 #endif
