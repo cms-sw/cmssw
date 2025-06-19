d41dc2bff711653e4fa09ccddb00f7b128670100
Mijail Tokarev <mijail.tokarev@ug.uchile.cl>
2025-06-19 12:45:45 +0200

    Add Econ-T indexer
    Adding Trigger Cell and Module Indexers
    
    Add new ESProducer for Trigger ModuleMapping
    
    Created copy files to edit for testing porpouses
    
    Renaming of the TriggerESProfucer file
    
    Change geometry module to make it work in the current branch
    
    Firsts edit to Trigger Tester
    
    Change class and Module name
    
    Add new classes and modules to BuildFiles
    
    Corrected a miss naming
    
    Adding the files to EDM type lookup
    
    Small correctiond to Trigger Tester
    
    First debug of EDM
    
    EDM error fixed
    
    Testing Trigger producer
    
    Corrected a bug in the calculation of NTrLinks per for a given module type
    
    Changing some comments
    
    Change of function name to reflect it being Trigger variant
    
    Added a getter that takes a dense TCidx
    
    Corrected Typo
    
    Added new trig_fed column to module locator and updated Trigger ESProducer to account for that
    
    Added HGCalDigiTrigger from hgcal-comm
    
    (Buggy) Added new SoA for Trigger Cells
    
    Change order of loading of ESProducers
    
    Small change in ModuleIndexerTrigger.h
    
    Comments and spacing
    
    Fix Triger Link Indexing for TM + minor bug fixes
    
    (Fixed) Added new SoA for Trigger Modules; Module done, Dense Index to do
    
    Module Trigger SoA producer tested
    
    Delete unused cff file
    
    Add Dense Index Trigger Info
    
    Change to the hgcalmapping_cff to allow running of just DAQ module locators.
    
    Small change to default module locator trigger for tensting porpuses
    
    Add regex for SiPM Tiles in Trigger Index ESProducer
    
    Changing comments
    
    Code formatting
    
    Delete test data
    
    Delete references to fantom directories
    
    Undo tester changes

--- Geometry/HGCalMapping/python/hgcalmapping_cff.py
+++ Geometry/HGCalMapping/python/hgcalmapping_cff.py
@@ -4,7 +4,8 @@ def customise_hgcalmapper(process,
                           modules = 'Geometry/HGCalMapping/data/ModuleMaps/modulelocator_test.txt',
                           sicells = 'Geometry/HGCalMapping/data/CellMaps/WaferCellMapTraces.txt',
                           sipmcells = 'Geometry/HGCalMapping/data/CellMaps/channels_sipmontile.hgcal.txt',
-                          offsetfile = 'Geometry/HGCalMapping/data/CellMaps/calibration_to_surrounding_offsetMap.txt'):
+                          offsetfile = 'Geometry/HGCalMapping/data/CellMaps/calibration_to_surrounding_offsetMap.txt',
+                          trigmodule = 'Geometry/HGCalMapping/data/ModuleMaps/modulelocator_trigger_test.txt'):
     """the following function configures the mapping producers
     NOTE: for production-targetted configs should be avoided as it checks if the process as 
     already the Accelerators sequence loaded, if not it loads it to the process"""
@@ -13,6 +14,11 @@ def customise_hgcalmapper(process,
     process.hgCalMappingESProducer.modules = cms.FileInPath(modules)
     process.hgCalMappingESProducer.si = cms.FileInPath(sicells)
     process.hgCalMappingESProducer.sipm = cms.FileInPath(sipmcells)
+
+    process.load('Geometry.HGCalMapping.hgCalMappingTriggerESProducer_cfi')
+    process.hgCalMappingTriggerESProducer.modules = cms.FileInPath(trigmodule)
+    process.hgCalMappingTriggerESProducer.si = cms.FileInPath(sicells)
+    process.hgCalMappingTriggerESProducer.sipm = cms.FileInPath(sipmcells)
 
     if not hasattr(process, 'ProcessAcceleratorCUDA'):
         process.load('Configuration.StandardSequences.Accelerators_cff')
@@ -26,5 +32,11 @@ def customise_hgcalmapper(process,
                                                           moduleindexer=cms.ESInputTag(''))
     process.hgCalDenseIndexInfoESProducer = cms.ESProducer('hgcal::HGCalDenseIndexInfoESProducer@alpaka',
                                                            moduleindexer=cms.ESInputTag('') )
-    
+
+    process.hgCalMappingModuleTriggerESProducer = cms.ESProducer('hgcal::HGCalMappingTriggerModuleESProducer@alpaka',
+                                                                filename=cms.FileInPath(trigmodule),
+                                                                moduleindexer=cms.ESInputTag(''))
+    process.hgCalDenseIndexTriggerInfoESProducer = cms.ESProducer('hgcal::HGCalDenseIndexTriggerInfoESProducer@alpaka',
+                                                           moduleindexer=cms.ESInputTag('') )
+
     return process
