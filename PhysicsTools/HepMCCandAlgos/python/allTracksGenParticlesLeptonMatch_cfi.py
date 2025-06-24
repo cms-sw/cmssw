a04220a64c74261022151de4607b5c07fd544a40
Tamas Almos Vami <Tamas.Almos.Vami@cern.ch>
2025-06-23 20:23:22 -0700

    Remove deprecated genParticleCandidates

--- PhysicsTools/HepMCCandAlgos/python/allTracksGenParticlesLeptonMatch_cfi.py
+++ PhysicsTools/HepMCCandAlgos/python/allTracksGenParticlesLeptonMatch_cfi.py
@@ -4,7 +4,7 @@ allTracksGenParticlesLeptonMatch = cms.EDFilter("MCTruthDeltaRMatcher",
     src = cms.InputTag("allTracks"),
     distMin = cms.double(0.15),
     matchPDGId = cms.vint32(11, 13),
-    matched = cms.InputTag("genParticleCandidates")
+    matched = cms.InputTag("genParticles")
 )
 
 
