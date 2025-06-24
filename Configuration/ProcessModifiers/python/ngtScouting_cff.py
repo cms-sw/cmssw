9a497645ae100193e4d73ff83697afc847ccf148
Marco Musich <marco.musich@cern.ch>
2025-06-24 15:37:01 +0200

    Change meaning of ngtScouting modifier in combination with trackingLST
    - ngtScouting without trackingLST means generalTracks==pixelTracksHP
    - ngtScouting with trackingLST means generalTracks==(pixelTracksHP + T5s)
    
    Introduce MVA-based high purity selection for extended pixelTracks
    Enable OT-trimmed seeding for LST when trackingLST and CAExtension are used
    Optimise CA memory usage
    
    Co-Authored-By: Jan Schulz <jan.gerrit.schulz@cern.ch>
    Co-Authored-By: Marco Rovere <marco.rovere@cern.ch>
    Co-Authored-By: Luca Ferragina <ferragina.luca@gmail.com>

--- Configuration/ProcessModifiers/python/ngtScouting_cff.py
+++ Configuration/ProcessModifiers/python/ngtScouting_cff.py
@@ -1,5 +1,4 @@
 import FWCore.ParameterSet.Config as cms
 
-# This modifier is for running ngt scouting menu validation
-
+# This modifier is for running ngt scouting menu
 ngtScouting = cms.Modifier()
