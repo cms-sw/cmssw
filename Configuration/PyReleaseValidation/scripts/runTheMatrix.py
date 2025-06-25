68ae5ff1c08ecff2bbb2f2ce757b964b29da3d64
Marco Musich <marco.musich@cern.ch>
2025-06-25 22:02:57 +0200

    introduce .772 (NGT scouting with Nano and Validation)
    - fix missing ngtScouting modifier

--- Configuration/PyReleaseValidation/scripts/runTheMatrix.py
+++ Configuration/PyReleaseValidation/scripts/runTheMatrix.py
@@ -161,10 +161,11 @@ if __name__ == '__main__':
                      29634.756,   # HLT phase-2 timing menu trimmed tracking
                      29634.7561,  # HLT phase-2 timing menu Alpaka, trimmed tracking
                      29634.7562,  # HLT phase-2 timing menu Alpaka, trimmed tracking, single tracking iteration variant
-                     29634.759,   # HLT phase-2 timing menu Alpaka, single tracking iteration, LST seeding + CKF building variant
-                     29634.757,   # HLT phase-2 timing menu, with NANO:@Phase2HLT
+                     29634.757,   # HLT phase-2 timing menu Alpaka, single tracking iteration, LST seeding + CKF building variant
+                     29634.759,   # HLT phase-2 timing menu, with NANO:@Phase2HLT
                      29634.77,    # HLT phase-2 NGT Scouting menu
-                     29634.771]   # HLT phase-2 NGT Scouting menu, with NANO:@NGTScouting
+                     29634.771,   # HLT phase-2 NGT Scouting menu, with NANO:@NGTScouting
+                     29634.772]   # HLT phase-2 NGT Scouting menu, with NANO:@NGTScoutingVal
     }
 
     predefinedSet['limited'] = (
