03b6e5211ee41b5e04fbda8d83396f4806ad41a6
Marco Musich <marco.musich@cern.ch>
2025-06-26 22:23:49 +0200

    resolve the missing DCSRecords printout in JME DQM for HLT monitoring

--- DQM/HLTEvF/python/ScoutingJetMonitoring_cff.py
+++ DQM/HLTEvF/python/ScoutingJetMonitoring_cff.py
@@ -4,12 +4,25 @@ import FWCore.ParameterSet.Config as cms
 
 from DQMOffline.JetMET.jetMETDQMOfflineSource_cff import *
 
-
-jetDQMOnlineAnalyzerAk4ScoutingCleaned = jetDQMAnalyzerAk4ScoutingCleaned.clone()
-jetDQMOnlineAnalyzerAk4ScoutingUncleaned = jetDQMAnalyzerAk4ScoutingUncleaned.clone()
+jetDQMOnlineAnalyzerAk4ScoutingCleaned = jetDQMAnalyzerAk4ScoutingCleaned.clone(
+    DCSFilterForJetMonitoring = cms.PSet(
+        DetectorTypes = cms.untracked.string("ecal:hbhe:hf:pixel:sistrip:es:muon"),
+        onlineMetaDataDigisSrc =  cms.untracked.InputTag("hltOnlineMetaDataDigis"),
+        DebugOn = cms.untracked.bool(False),
+        alwaysPass = cms.untracked.bool(False)
+    )
+)
+jetDQMOnlineAnalyzerAk4ScoutingUncleaned = jetDQMAnalyzerAk4ScoutingUncleaned.clone(
+    DCSFilterForJetMonitoring = cms.PSet(
+      DetectorTypes = cms.untracked.string("ecal:hbhe:hf:pixel:sistrip:es:muon"),
+        onlineMetaDataDigisSrc =  cms.untracked.InputTag("hltOnlineMetaDataDigis"),
+        DebugOn = cms.untracked.bool(False),
+        alwaysPass = cms.untracked.bool(False)
+    )
+)
 
 jetDQMOnlineAnalyzerSequenceScouting = cms.Sequence(jetDQMOnlineAnalyzerAk4ScoutingUncleaned*jetDQMOnlineAnalyzerAk4ScoutingCleaned)
 
 ScoutingJetMonitoring = cms.Sequence(jetPreDQMSeqScouting*
-                                      dqmAk4PFScoutingL1FastL2L3ResidualCorrectorChain*
-                                      jetDQMOnlineAnalyzerSequenceScouting)
+                                     dqmAk4PFScoutingL1FastL2L3ResidualCorrectorChain*
+                                     jetDQMOnlineAnalyzerSequenceScouting)
