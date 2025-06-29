905bc1706686d05faf94b9c1a7bda43f8665b40e
Marco Musich <marco.musich@cern.ch>
2025-06-29 19:30:01 +0200

    apply stage2 L1T customizations to TrackToTrackComparisonHists

--- DQM/TrackingMonitorSource/python/TrackToTrackComparisonHists_cfi.py
+++ DQM/TrackingMonitorSource/python/TrackToTrackComparisonHists_cfi.py
@@ -6,6 +6,14 @@ from Configuration.Eras.Modifier_phase2_common_cff import phase2_common
 
 TrackToTrackComparisonHists = trackToTrackComparisonHists.clone()
 
+from Configuration.Eras.Modifier_stage2L1Trigger_cff import stage2L1Trigger
+stage2L1Trigger.toModify(TrackToTrackComparisonHists,
+                         genericTriggerEventPSet = dict(stage2 = cms.bool(True),
+                                                        l1tAlgBlkInputTag = cms.InputTag("gtStage2Digis"),
+                                                        l1tExtBlkInputTag = cms.InputTag("gtStage2Digis"),
+                                                        ReadPrescalesFromFile = cms.bool(False))
+                         )
+
 run3_common.toModify(TrackToTrackComparisonHists.histoPSet, Eta_rangeMin=-3.,Eta_rangeMax =3.)
 run3_common.toModify(TrackToTrackComparisonHists.histoPSet, onlinelumi_nbin=375, onlinelumi_rangeMin=200., onlinelumi_rangeMax=25000.)
 phase2_common.toModify(TrackToTrackComparisonHists.histoPSet, Eta_rangeMin=-4.,Eta_rangeMax =4.)
