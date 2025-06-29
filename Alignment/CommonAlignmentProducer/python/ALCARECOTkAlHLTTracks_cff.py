09c3b41529b45b3925ecdb2ad484a5f0f6088063
Marco Musich <marco.musich@cern.ch>
2025-06-29 14:12:21 +0200

    re-organize ALCARECOTkAlHLTTracks producers

--- Alignment/CommonAlignmentProducer/python/ALCARECOTkAlHLTTracks_cff.py
+++ Alignment/CommonAlignmentProducer/python/ALCARECOTkAlHLTTracks_cff.py
@@ -40,15 +40,4 @@ ALCARECOTkAlHLTTracks.TwoBodyDecaySelector.applyMassrangeFilter = False
 ALCARECOTkAlHLTTracks.TwoBodyDecaySelector.applyChargeFilter = False
 ALCARECOTkAlHLTTracks.TwoBodyDecaySelector.applyAcoplanarityFilter = False
 
-##################################################################
-# Tracks from the selected vertex
-#################################################################
-import Alignment.CommonAlignmentProducer.AlignmentTracksFromVertexSelector_cfi as TracksFromPixelVertex
-ALCARECOTkAlHLTPixelVertexTracks = TracksFromPixelVertex.AlignmentTracksFromVertexSelector.clone(
-    src = 'hltPixelTracks',
-    vertices = 'hltPixelVertices',
-    useClosestVertexToDilepton = False,
-    vertexIndex = 0,
-)
-
-seqALCARECOTkAlHLTTracks = cms.Sequence(ALCARECOTkAlHLTTracksHLT+ALCARECOTkAlHLTTracksDCSFilter+ALCARECOTkAlHLTTracks+ALCARECOTkAlHLTPixelVertexTracks)
+seqALCARECOTkAlHLTTracks = cms.Sequence(ALCARECOTkAlHLTTracksHLT+ALCARECOTkAlHLTTracksDCSFilter+ALCARECOTkAlHLTTracks)
