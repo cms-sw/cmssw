ccf70b7606ec17c1e61710ffd72dcf19eb273db6
iarspider <iarspider@gmail.com>
2025-07-09 17:24:30 +0200

    Update EcalUncalibRecHitPhase2WeightsAlgoPortable.dev.cc

--- RecoLocalCalo/EcalRecProducers/plugins/alpaka/EcalUncalibRecHitPhase2WeightsAlgoPortable.dev.cc
+++ RecoLocalCalo/EcalRecProducers/plugins/alpaka/EcalUncalibRecHitPhase2WeightsAlgoPortable.dev.cc
@@ -39,7 +39,7 @@ namespace ALPAKA_ACCELERATOR_NAMESPACE::ecal::weights {
 #pragma GCC diagnostic ignored "-Wdangling-reference"
         const auto& digi = digisDev[tx].data();
 #pragma GCC diagnostic pop
-        // rebuild 4
+        // rebuild 5
         auto recHit = uncalibratedRecHitsDev[tx];
         recHit.amplitude() = 0;
         recHit.jitter() = 0;
