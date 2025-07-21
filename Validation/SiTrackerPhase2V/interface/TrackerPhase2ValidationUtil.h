43976584da70ec807f243beb549ec75ac8eb59d9
Brandi Skipworth <bskipwo1@vols.utk.edu>
2025-07-21 20:17:29 +0200

    fixed magic numbers and cleanup

--- Validation/SiTrackerPhase2V/interface/TrackerPhase2ValidationUtil.h
+++ Validation/SiTrackerPhase2V/interface/TrackerPhase2ValidationUtil.h
@@ -1,9 +1,12 @@
+
 #ifndef _Validation_SiTrackerPhase2V_TrackerPhase2ValidationUtil_h
 #define _Validation_SiTrackerPhase2V_TrackerPhase2ValidationUtil_h
 
 #include "SimDataFormats/TrackingHit/interface/PSimHit.h"
 #include "SimDataFormats/Track/interface/SimTrack.h"
 #include "SimDataFormats/TrackingAnalysis/interface/TrackingParticle.h"
+#include "L1Trigger/TrackFindingTracklet/interface/Settings.h"
+#include "DataFormats/Math/interface/deltaPhi.h"
 #include <cmath>
 #include <tuple>  // for std::tuple
 
@@ -12,26 +15,25 @@ namespace phase2tkutil {
   bool isPrimary(const SimTrack& simTrk, const PSimHit* simHit);
   static constexpr float cmtomicron = 1e4;
 
-  inline std::tuple<float, float, float> computeZ0LxyD0(const TrackingParticle& tp, float pt, float eta, float phi) {
+  inline std::tuple<float, float, float> computeZ0LxyD0(const TrackingParticle& tp) {
+    trklet::Settings settings;
+    double bfield_ = settings.bfield();
+    double c_ = settings.c();
+
     float vz = tp.vz();
     float vx = tp.vx();
     float vy = tp.vy();
     float charge = tp.charge();
+    float pt = tp.pt();
+    float eta = tp.eta();
+    float phi = tp.phi();
 
     float t = std::tan(2.0 * std::atan(1.0) - 2.0 * std::atan(std::exp(-eta)));
-    float delx = -vx;
-    float dely = -vy;
-    float K = 0.01 * 0.5696 / pt * charge;  // curvature correction
+    float K = 0.01 * bfield_ * c_ / 2 / pt * charge;  // curvature correction
     float A = 1. / (2. * K);
-    float x0p = delx - A * std::sin(phi);
-    float y0p = dely + A * std::cos(phi);
-    const float pi = 4.0 * std::atan(1.0);
-    float delphi = phi - std::atan2(-K * x0p, K * y0p);
-
-    if (delphi < -pi)
-      delphi += 2.0 * pi;
-    if (delphi > pi)
-      delphi -= 2.0 * pi;
+    float x0p = -vx - A * std::sin(phi);
+    float y0p = -vy + A * std::cos(phi);
+    float delphi = reco::deltaPhi(phi, std::atan2(-K * x0p, K * y0p));
 
     float z0 = vz + t * delphi / (2.0 * K);
     float Lxy = std::sqrt(vx * vx + vy * vy);
