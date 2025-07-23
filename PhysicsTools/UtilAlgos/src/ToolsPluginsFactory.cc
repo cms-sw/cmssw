f3ac57983aba64a427fce8a43dcf6e783ff1dab6
Tamas <Tamas.Almos.Vami@cern.ch>
2025-07-23 23:31:58 +0200

    Deprecate VariableHelperService

--- PhysicsTools/UtilAlgos/src/ToolsPluginsFactory.cc
+++ PhysicsTools/UtilAlgos/src/ToolsPluginsFactory.cc
@@ -5,9 +5,6 @@ EDM_REGISTER_PLUGINFACTORY(EventSelectorFactoryFromHelper, "EventSelectorFactory
 #include "PhysicsTools/UtilAlgos/interface/NTupler.h"
 EDM_REGISTER_PLUGINFACTORY(NTuplerFactory, "NTuplerFactory");
 
-#include "PhysicsTools/UtilAlgos/interface/Plotter.h"
-EDM_REGISTER_PLUGINFACTORY(PlotterFactory, "PlotterFactory");
-
 #include "PhysicsTools/UtilAlgos/interface/CachingVariable.h"
 EDM_REGISTER_PLUGINFACTORY(CachingVariableFactory, "CachingVariableFactory");
 EDM_REGISTER_PLUGINFACTORY(VariableComputerFactory, "VariableComputerFactory");
