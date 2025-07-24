1922428f8be25205858460b878052319a7e600d1
Pietro Grutta <pietrogrutta@gmail.com>
2025-07-24 11:22:36 +0200

    Dataformats SiStripClusterSoA, SiStripDigiSoA

---
+++ DataFormats/SiStripClusterSoA/src/classes.h
@@ -0,0 +1,8 @@
+#ifndef DataFormats_SiStripClusterSoA_src_classes_h
+#define DataFormats_SiStripClusterSoA_src_classes_h
+
+#include "DataFormats/Common/interface/Wrapper.h"
+#include "DataFormats/SiStripClusterSoA/interface/SiStripClusterHost.h"
+// #include "DataFormats/SiStripClusterSoA/interface/SiStripClusterSoA.h"
+
+#endif  // DataFormats_SiStripClusterSoA_src_classes_h
