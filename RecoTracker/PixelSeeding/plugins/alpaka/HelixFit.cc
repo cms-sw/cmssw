e7bd8923d90c5b1ba958831c12c7c1f2f35204f0
leonardo beltrame <leonardo.beltrame@cern.ch>
2025-07-25 13:00:22 +0200

    Rebased successfully

--- RecoTracker/PixelSeeding/plugins/alpaka/HelixFit.cc
+++ RecoTracker/PixelSeeding/plugins/alpaka/HelixFit.cc
@@ -12,7 +12,7 @@ namespace ALPAKA_ACCELERATOR_NAMESPACE {
 
     ALPAKA_ASSERT_ACC(tuples_);
     ALPAKA_ASSERT_ACC(tupleMultiplicity_);
-    ALPAKA_ASSERT_ACC(helix_fit_results.pt());
+    ALPAKA_ASSERT_ACC(helix_fit_results.pt().data());
   }
 
   template <typename TrackerTraits>
