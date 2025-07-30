c2919512437e519c620f62d8e6b23da0fb799aa7
Giovanni <gpetruc@gmail.com>
2025-07-30 18:02:47 +0200

    code improvements related to L1-Scouting CaloTowers

--- EventFilter/L1ScoutingRawToDigi/plugins/ScCaloTowerRawToDigi.cc
+++ EventFilter/L1ScoutingRawToDigi/plugins/ScCaloTowerRawToDigi.cc
@@ -1,39 +1,83 @@
-#include "EventFilter/L1ScoutingRawToDigi/plugins/ScCaloTowerRawToDigi.h"
+#include <ios>
+#include <memory>
+#include <utility>
+#include <vector>
 
-ScCaloTowerRawToDigi::ScCaloTowerRawToDigi(const edm::ParameterSet& iConfig) {
-  srcInputTag_ = iConfig.getParameter<edm::InputTag>("srcInputTag");
-  sourceIdList_ = iConfig.getParameter<std::vector<int>>("sourceIdList");
+#include "DataFormats/FEDRawData/interface/FEDRawData.h"
+#include "DataFormats/L1Scouting/interface/L1ScoutingCaloTower.h"
+#include "DataFormats/L1Scouting/interface/OrbitCollection.h"
+#include "DataFormats/L1ScoutingRawData/interface/SDSNumbering.h"
+#include "DataFormats/L1ScoutingRawData/interface/SDSRawDataCollection.h"
+#include "EventFilter/L1ScoutingRawToDigi/interface/blocks.h"
+#include "EventFilter/L1ScoutingRawToDigi/interface/masks.h"
+#include "EventFilter/L1ScoutingRawToDigi/interface/shifts.h"
+#include "FWCore/Framework/interface/Event.h"
+#include "FWCore/Framework/interface/stream/EDProducer.h"
+#include "FWCore/MessageLogger/interface/MessageLogger.h"
+#include "FWCore/ParameterSet/interface/ConfigurationDescriptions.h"
+#include "FWCore/ParameterSet/interface/ParameterSetDescription.h"
 
-  // initialize orbit buffer for BX 1->3564;
-  orbitBuffer_ = std::vector<std::vector<l1ScoutingRun3::CaloTower>>(3565);
+class ScCaloTowerRawToDigi : public edm::stream::EDProducer<> {
+public:
+  explicit ScCaloTowerRawToDigi(const edm::ParameterSet&);
+  ~ScCaloTowerRawToDigi() override = default;
+
+  static void fillDescriptions(edm::ConfigurationDescriptions& descriptions);
+
+private:
+  // BX per orbit
+  static constexpr unsigned int NBX = 3564;
+
+  void produce(edm::Event&, const edm::EventSetup&) override;
+
+  void unpackOrbit(const unsigned char* buf, size_t len, int sdsId);
+
+  edm::EDGetTokenT<SDSRawDataCollection> const rawToken_;
+  std::vector<int> const sourceIdList_;
+  bool const debug_;
+
+  // vector holding data for every bunch crossing
+  // before filling the orbit collection
+  std::vector<std::vector<l1ScoutingRun3::CaloTower>> orbitBuffer_;
+
+  int nCaloTowersOrbit_;
+};
+
+ScCaloTowerRawToDigi::ScCaloTowerRawToDigi(const edm::ParameterSet& iConfig)
+    : rawToken_(consumes(iConfig.getParameter<edm::InputTag>("srcInputTag"))),
+      sourceIdList_(iConfig.getParameter<std::vector<int>>("sourceIdList")),
+      debug_(iConfig.getUntrackedParameter<bool>("debug")),
+      orbitBuffer_(NBX + 1),
+      nCaloTowersOrbit_(0) {
   for (auto& bxVec : orbitBuffer_) {
+    // reasonable upper estimate
     bxVec.reserve(4096);
   }
-  nCaloTowersOrbit_ = 0;
+
+  for (auto const& sdsId : sourceIdList_) {
+    if (sdsId < SDSNumbering::CaloTowerMinSDSID or sdsId > SDSNumbering::CaloTowerMaxSDSID) {
+      edm::LogError("ScCaloTowerRawToDigi")
+          << "Provided a source ID outside the expected range: " << sdsId << ", expected range ["
+          << SDSNumbering::CaloTowerMinSDSID << ", " << SDSNumbering::CaloTowerMaxSDSID << "]";
+    }
+  }
 
   produces<l1ScoutingRun3::CaloTowerOrbitCollection>("CaloTower").setBranchAlias("CaloTowerOrbitCollection");
-  rawToken_ = consumes<SDSRawDataCollection>(srcInputTag_);
 }
 
-ScCaloTowerRawToDigi::~ScCaloTowerRawToDigi() {}
+void ScCaloTowerRawToDigi::produce(edm::Event& iEvent, const edm::EventSetup&) {
+  auto const& scoutingRawDataCollection = iEvent.get(rawToken_);
 
-void ScCaloTowerRawToDigi::produce(edm::Event& iEvent, const edm::EventSetup& iSetup) {
-  edm::Handle<SDSRawDataCollection> ScoutingRawDataCollection;
-  iEvent.getByToken(rawToken_, ScoutingRawDataCollection);
+  auto unpackedCaloTowers = std::make_unique<l1ScoutingRun3::CaloTowerOrbitCollection>();
 
-  std::unique_ptr<l1ScoutingRun3::CaloTowerOrbitCollection> unpackedCaloTowers(
-      new l1ScoutingRun3::CaloTowerOrbitCollection);
+  nCaloTowersOrbit_ = 0;
 
   for (const auto& sdsId : sourceIdList_) {
-    if ((sdsId < SDSNumbering::CaloTowerMinSDSID) || (sdsId > SDSNumbering::CaloTowerMaxSDSID))
-      edm::LogError("ScCaloTowerRawToDigi::produce")
-          << "Provided a source ID outside the expected range: " << sdsId << ", expected range ["
-          << SDSNumbering::CaloTowerMinSDSID << ", " << SDSNumbering::CaloTowerMaxSDSID;
-    const FEDRawData& sourceRawData = ScoutingRawDataCollection->FEDData(sdsId);
+    const FEDRawData& sourceRawData = scoutingRawDataCollection.FEDData(sdsId);
     size_t orbitSize = sourceRawData.size();
 
-    if (sourceRawData.size() == 0) {
-      LogDebug("L1Scout") << "No raw data for CaloTower FED " << sdsId;
+    if (debug_ && (orbitSize == 0)) {
+      edm::LogWarning("ScCaloTowerRawToDigi") << "No raw data for CaloTower FED " << sdsId;
     }
 
     // unpack current orbit and store data into the orbitBufferr
@@ -48,62 +92,91 @@ void ScCaloTowerRawToDigi::produce(edm::Event& iEvent, const edm::EventSetup& iS
 }
 
 void ScCaloTowerRawToDigi::unpackOrbit(const unsigned char* buf, size_t len, int sdsId) {
-  // reset counters
-  nCaloTowersOrbit_ = 0;
-
   size_t pos = 0;
+  const size_t blockHeaderSize = 3 * sizeof(uint32_t);
 
   while (pos < len) {
-    assert(pos + 4 <= len);
+    if (pos + blockHeaderSize > len) {
+      edm::LogError("ScCaloTowerRawToDigi") << "Corrupt data in sourceId " << sdsId << ", incomplete header";
+      break;  // no sense trying to unpack further
+    }
+    const l1ScoutingRun3::calol1::block* bl = reinterpret_cast<const l1ScoutingRun3::calol1::block*>(buf + pos);
 
-    l1ScoutingRun3::calol1::block* bl = (l1ScoutingRun3::calol1::block*)(buf + pos);
+    unsigned int bx = bl->bx;
+    unsigned int orbit = (bl->orbit) & 0x7FFFFFFF;
+    unsigned int ctCount = bl->header;
 
-    unsigned bx = bl->bx;
-    unsigned orbit = (bl->orbit) & 0x7FFFFFFF;
-    unsigned ctCount = bl->header;
+    pos += blockHeaderSize;
 
-    size_t pos_increment = 12 + ctCount * 4;
+    if ((pos + 4 * ctCount) > len) {
+      edm::LogError("ScCaloTowerRawToDigi")
+          << "Corrupt data in sourceId " << sdsId << ", orbit " << orbit << ", BX " << bx << ": expecting " << ctCount
+          << " towers but only " << (len - pos) << " bytes left in the block.";
+      break;  // no sense trying to unpack further
+    }
 
-    assert(pos_increment <= len);
+    if (bx > NBX) {  // need this check as otherwise the code will crash later accessing orbitBuffer_
+      edm::LogError("ScCaloTowerRawToDigi")
+          << "Corrupt data in sourceId " << sdsId << ", orbit " << orbit << ", invalid BX " << bx;
+      break;  // we could go to the next block, but if the data is corrupted it probably doesn't help
+    }
 
-    pos += 12;  // header
-
-    LogDebug("L1Scout") << " CaloTower #" << sdsId << " Orbit " << orbit << ", BX -> " << bx << ", nCaloTowers -> "
-                        << ctCount;
+    if (debug_) {
+      edm::LogPrint("ScCaloTowerRawToDigi")
+          << " CaloTower #" << sdsId << " Orbit " << orbit << ", BX -> " << bx << ", nCaloTowers -> " << ctCount;
+    }
 
     // Unpack calo towers
+    auto& bufferThisBX = orbitBuffer_[bx];
     int16_t ET, erBits, miscBits, eta, phi;
 
-    for (unsigned int i = 0; i < ctCount; i++) {
-      uint64_t ct_raw = *(uint32_t*)(buf + pos);
-      pos += 4;
+    const uint32_t* towerPtr = reinterpret_cast<const uint32_t*>(buf + pos);
+
+    for (unsigned int i = 0; i < ctCount; ++i, ++towerPtr) {
+      uint32_t ct_raw = *towerPtr;
 
       ET = ((ct_raw >> l1ScoutingRun3::calol1::shiftsCaloTowers::ET) & l1ScoutingRun3::calol1::masksCaloTowers::ET);
+
       erBits = ((ct_raw >> l1ScoutingRun3::calol1::shiftsCaloTowers::erBits) &
                 l1ScoutingRun3::calol1::masksCaloTowers::erBits);
+
       miscBits = ((ct_raw >> l1ScoutingRun3::calol1::shiftsCaloTowers::miscBits) &
                   l1ScoutingRun3::calol1::masksCaloTowers::miscBits);
+
       phi = ((ct_raw >> l1ScoutingRun3::calol1::shiftsCaloTowers::phi) & l1ScoutingRun3::calol1::masksCaloTowers::phi);
 
       eta = ((ct_raw >> l1ScoutingRun3::calol1::shiftsCaloTowers::eta) & l1ScoutingRun3::calol1::masksCaloTowers::eta);
       eta = eta >= 128 ? eta - 256 : eta;
 
-      l1ScoutingRun3::CaloTower ct(ET, erBits, miscBits, eta, phi);
-      orbitBuffer_[bx].push_back(ct);
-      nCaloTowersOrbit_++;
+      bufferThisBX.emplace_back(ET, erBits, miscBits, eta, phi);
 
-      LogDebug("L1Scout") << "Calo Tower " << i << ", raw: 0x" << std::hex << ct_raw << std::dec << "\tET: " << ET
-                          << "\tER bits: " << erBits << "\tMisc bits: " << miscBits << "\tEta: " << eta
-                          << "\tPhi: " << phi;
+      if (debug_) {
+        edm::LogPrint("ScCaloTowerRawToDigi")
+            << "Calo Tower " << i << ", raw: 0x" << std::hex << ct_raw << std::dec << "\n\tET: " << ET
+            << "\n\tER bits: " << erBits << "\n\tMisc bits: " << miscBits << "\n\tEta: " << eta << "\n\tPhi: " << phi;
+      }
     }
 
+    pos += 4 * ctCount;
+    nCaloTowersOrbit_ += ctCount;
   }  // end orbit while loop
 }
 
 void ScCaloTowerRawToDigi::fillDescriptions(edm::ConfigurationDescriptions& descriptions) {
   edm::ParameterSetDescription desc;
-  desc.setUnknown();
+
+  desc.add<edm::InputTag>("srcInputTag", edm::InputTag("rawDataCollector"));
+
+  std::vector<int> sourceIds;
+  for (int id = SDSNumbering::CaloTowerMinSDSID; id <= SDSNumbering::CaloTowerMaxSDSID; ++id) {
+    sourceIds.emplace_back(id);
+  }
+  desc.add<std::vector<int>>("sourceIdList", sourceIds);
+
+  desc.addUntracked<bool>("debug", false);
+
   descriptions.addDefault(desc);
 }
 
+#include "FWCore/Framework/interface/MakerMacros.h"
 DEFINE_FWK_MODULE(ScCaloTowerRawToDigi);
--- L1TriggerScouting/OnlineProcessing/plugins/ScoutingJetProducer.cc
+++ L1TriggerScouting/OnlineProcessing/plugins/ScoutingJetProducer.cc
@@ -1,84 +1,73 @@
-#include "FWCore/Framework/interface/Event.h"
-#include "FWCore/Framework/interface/Frameworkfwd.h"
-#include "FWCore/Framework/interface/stream/EDProducer.h"
-#include "FWCore/Framework/interface/MakerMacros.h"
-#include "FWCore/ParameterSet/interface/ConfigurationDescriptions.h"
-#include "FWCore/ParameterSet/interface/ParameterSet.h"
-#include "FWCore/ParameterSet/interface/ParameterSetDescription.h"
-#include "FWCore/Utilities/interface/EDGetToken.h"
-#include "FWCore/Utilities/interface/EDPutToken.h"
-#include "FWCore/Utilities/interface/Exception.h"
-#include "FWCore/Utilities/interface/InputTag.h"
-#include "FWCore/Utilities/interface/StreamID.h"
+#include <memory>
+#include <utility>
+#include <vector>
 
-#include "DataFormats/L1Scouting/interface/OrbitCollection.h"
 #include "DataFormats/L1Scouting/interface/L1ScoutingCaloTower.h"
 #include "DataFormats/L1Scouting/interface/L1ScoutingFastJet.h"
-
+#include "FWCore/Framework/interface/Event.h"
+#include "FWCore/Framework/interface/global/EDProducer.h"
+#include "FWCore/ParameterSet/interface/ConfigurationDescriptions.h"
+#include "FWCore/ParameterSet/interface/ParameterSetDescription.h"
 #include "L1TriggerScouting/Utilities/interface/conversion.h"
 
-// root libraries
+// ROOT libraries
 #include "Math/Vector4D.h"
 #include "Math/VectorUtil.h"
 
 // fastjet libraries
 #include "fastjet/ClusterSequence.hh"
 
-#include <memory>
-#include <utility>
-#include <vector>
-
-using namespace l1ScoutingRun3;
-
-class ScoutingJetProducer : public edm::stream::EDProducer<> {
+class ScoutingJetProducer : public edm::global::EDProducer<> {
 public:
   explicit ScoutingJetProducer(const edm::ParameterSet&);
-  ~ScoutingJetProducer() override {}
+  ~ScoutingJetProducer() override = default;
 
   static void fillDescriptions(edm::ConfigurationDescriptions& descriptions);
 
 private:
-  void produce(edm::Event&, const edm::EventSetup&) override;
+  void produce(edm::StreamID, edm::Event&, const edm::EventSetup&) const override;
 
-  // tokens for scouting data
-  edm::EDGetTokenT<OrbitCollection<l1ScoutingRun3::CaloTower>> src_;
-  double akR_;
-  double ptMin_;
-  bool debug_;
+  edm::EDGetTokenT<l1ScoutingRun3::CaloTowerOrbitCollection> const src_;
+  double const akR_;
+  double const ptMin_;
+  bool const debug_;
 };
 
 ScoutingJetProducer::ScoutingJetProducer(const edm::ParameterSet& iPSet)
     : src_(consumes(iPSet.getParameter<edm::InputTag>("src"))),
       akR_(iPSet.getParameter<double>("akR")),
       ptMin_(iPSet.getParameter<double>("ptMin")),
-      debug_(iPSet.getParameter<bool>("debug")) {
-  produces<OrbitCollection<l1ScoutingRun3::FastJet>>("FastJet").setBranchAlias("FastJetOrbitCollection");
+      debug_(iPSet.getUntrackedParameter<bool>("debug")) {
+  produces<l1ScoutingRun3::FastJetOrbitCollection>("FastJet").setBranchAlias("FastJetOrbitCollection");
 }
 
 // ------------ method called for each ORBIT  ------------
-void ScoutingJetProducer::produce(edm::Event& iEvent, const edm::EventSetup& iSetup) {
-  edm::Handle<OrbitCollection<l1ScoutingRun3::CaloTower>> caloTowerCollection;
-  iEvent.getByToken(src_, caloTowerCollection);
+void ScoutingJetProducer::produce(edm::StreamID, edm::Event& iEvent, const edm::EventSetup&) const {
+  auto const& caloTowerCollection = iEvent.get(src_);
 
-  std::unique_ptr<OrbitCollection<l1ScoutingRun3::FastJet>> fastJetCollection(new FastJetOrbitCollection);
-  std::vector<std::vector<l1ScoutingRun3::FastJet>> fastJetBuffer(3565);
-  unsigned nFastJet = 0;
+  auto fastJetCollection = std::make_unique<l1ScoutingRun3::FastJetOrbitCollection>();
+  std::vector<std::vector<l1ScoutingRun3::FastJet>> fastJetBuffer(3565);  // range of BX values
+  unsigned int nFastJet = 0;
 
   // define fastjet algorithm
   fastjet::JetDefinition jetDef(fastjet::antikt_algorithm, akR_);
 
-  // loop over valid bunch crossings
-  for (const unsigned& bx : caloTowerCollection->getFilledBxs()) {
-    const auto& cts = caloTowerCollection->bxIterator(bx);
+  // create pseudojet vector to be filled
+  std::vector<fastjet::PseudoJet> pjCTs;
 
-    // create pseudojet vector to be filled
-    std::vector<fastjet::PseudoJet> pjCTs;
-    pjCTs.reserve(cts.size());
+  // loop over valid bunch crossings
+  for (auto const bx : caloTowerCollection.getFilledBxs()) {
+    const auto& cts = caloTowerCollection.bxIterator(bx);
 
     // prepare pseudojets to give in input to fastjet
+    pjCTs.clear();
+    pjCTs.reserve(cts.size());
     for (const auto& ct : cts) {
-      ROOT::Math::PtEtaPhiMVector ctLV(calol1::fEt(ct.hwEt()), calol1::fEta(ct.hwEta()), calol1::fPhi(ct.hwPhi()), 0);
-      pjCTs.push_back(fastjet::PseudoJet(ctLV.px(), ctLV.py(), ctLV.pz(), ctLV.E()));
+      ROOT::Math::PtEtaPhiMVector ctLV(l1ScoutingRun3::calol1::fEt(ct.hwEt()),
+                                       l1ScoutingRun3::calol1::fEta(ct.hwEta()),
+                                       l1ScoutingRun3::calol1::fPhi(ct.hwPhi()),
+                                       0);
+      pjCTs.emplace_back(ctLV.px(), ctLV.py(), ctLV.pz(), ctLV.E());
     }
 
     // run the jet clustering with the given jet definition
@@ -88,14 +77,12 @@ void ScoutingJetProducer::produce(edm::Event& iEvent, const edm::EventSetup& iSe
     std::vector<fastjet::PseudoJet> incJets = fastjet::sorted_by_pt(clustSeq.inclusive_jets(ptMin_));
 
     // fill fast jet objects buffer
-    fastJetBuffer[bx].reserve(incJets.size());
-    int nConst = 0;
-    double area = 0.;
+    auto& bufferThisBX = fastJetBuffer[bx];
+    bufferThisBX.reserve(incJets.size());
     for (const auto& incJet : incJets) {
-      nConst = incJet.has_constituents() ? incJet.constituents().size() : 0;
-      area = incJet.has_area() ? incJet.area() : -1.0;
-      FastJet fj = FastJet(incJet.Et(), incJet.eta(), incJet.phi(), incJet.m(), nConst, area);
-      fastJetBuffer[bx].push_back(fj);
+      int nConst = incJet.has_constituents() ? incJet.constituents().size() : 0;
+      float area = incJet.has_area() ? incJet.area() : -1.0f;
+      bufferThisBX.emplace_back(incJet.Et(), incJet.eta(), incJet.phi(), incJet.m(), nConst, area);
       nFastJet++;
     }
   }
@@ -107,8 +94,12 @@ void ScoutingJetProducer::produce(edm::Event& iEvent, const edm::EventSetup& iSe
 
 void ScoutingJetProducer::fillDescriptions(edm::ConfigurationDescriptions& descriptions) {
   edm::ParameterSetDescription desc;
-  desc.setUnknown();
+  desc.add<edm::InputTag>("src");
+  desc.add<double>("akR");
+  desc.add<double>("ptMin");
+  desc.addUntracked<bool>("debug", false);
   descriptions.addDefault(desc);
 }
 
+#include "FWCore/Framework/interface/MakerMacros.h"
 DEFINE_FWK_MODULE(ScoutingJetProducer);
