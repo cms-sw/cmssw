236406c9bfd5a76c78bbac9c7ff333483cd2db5d
Kirutheeka Namasivayam <kirutheekan23@gmail.com>
2025-08-07 14:41:55 +0200

    add hgcal digis

--- FastSimulation/Configuration/python/DigiAliases_cff.py
+++ FastSimulation/Configuration/python/DigiAliases_cff.py
@@ -77,7 +77,7 @@ def loadDigiAliases(process, premix=False):
                 )
            }
           )
-
+    
     process.hcalDigis = cms.EDAlias(
         **{"simHcalDigis" if nopremix else "DMHcalDigis" :
             cms.VPSet(
@@ -98,6 +98,28 @@ def loadDigiAliases(process, premix=False):
            }
           )
 
+
+    process.hgcalDigis = cms.EDAlias(
+        mix = cms.VPSet(
+            cms.PSet(
+                fromProductInstance = cms.string('HGCDigisEE'),
+                toProductInstance = cms.string('EE'),
+                type = cms.string('DetIdHGCSampleHGCDataFramesSorted')
+            ),
+            cms.PSet(
+                fromProductInstance = cms.string('HGCDigisHEfront'),
+                toProductInstance = cms.string('HEfront'),
+                type = cms.string('DetIdHGCSampleHGCDataFramesSorted')
+            ),
+            cms.PSet(
+                fromProductInstance = cms.string('HGCDigisHEback'),
+                toProductInstance = cms.string('HEback'),
+                type = cms.string('DetIdHGCSampleHGCDataFramesSorted')
+            )
+        )
+    )
+
+    
     process.muonDTDigis = cms.EDAlias(
         simMuonDTDigis = cms.VPSet(
                 cms.PSet(
