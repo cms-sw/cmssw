2cec6f9c4a702f6fc1ce251bd6b918d0c805dbeb
Slava Krutelyov <slava77@cern.ch>
2025-08-08 14:08:15 -0700

    cleanup of unneeded fragments and unused paths

--- HLTrigger/Configuration/python/HLT_75e33_trackingOnly_cff.py
+++ HLTrigger/Configuration/python/HLT_75e33_trackingOnly_cff.py
@@ -2,7 +2,12 @@ import FWCore.ParameterSet.Config as cms
 
 from .HLT_75e33_cff import fragment
 
-fragment.load("HLTrigger/Configuration/HLT_75e33/paths/MC_TRK_cfi")
+
+for p in dir(fragment):
+    att = getattr(fragment, p)
+    if isinstance(att, cms.Path) and p not in ["MC_TRK", "HLTriggerFinalPath", "HLTAnalyzerEndpath"]:
+        delattr(fragment, p)
+    del att
 
 fragment.schedule = cms.Schedule(*[
     fragment.MC_TRK,
