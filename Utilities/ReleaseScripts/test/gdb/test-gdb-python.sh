a6e18806dcdcdc326f2b60a6a5047170fab3e64d
Shahzad Malik Muzaffar <shahzad.malik.muzaffar@cern.ch>
2025-08-11 15:41:47 +0200

    get the realpath of python: fixes unit test during PR tests

--- Utilities/ReleaseScripts/test/gdb/test-gdb-python.sh
+++ Utilities/ReleaseScripts/test/gdb/test-gdb-python.sh
@@ -1,6 +1,7 @@
 #!/bin/bash -ex
 gdb_python=$(gdb -q -ex 'python exec("import sys;print(sys.executable)")' -ex quit)
-cmssw_python=$(scram tool tag python3 PYTHON3_BASE)/bin/python
+cmssw_python=$(scram tool tag python3 PYTHON3_BASE)/bin
+cmssw_python=$(realpath ${cmssw_python})/python
 if [ "${gdb_python}" != "${cmssw_python}" ] ; then
   echo "python used by GDB and CMSSW are not same"
   exit 1
