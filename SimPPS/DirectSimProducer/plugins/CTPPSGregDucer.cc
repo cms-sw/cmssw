28f086a0ea6a120b3904d2732e068bfdd66e1738
Grzegorz <g.jedrzejows@student.uw.edu.pl>
2025-08-14 19:05:37 +0200

    interpolation is implemented but is likely wrong

--- SimPPS/DirectSimProducer/plugins/CTPPSGregDucer.cc
+++ SimPPS/DirectSimProducer/plugins/CTPPSGregDucer.cc
@@ -83,8 +83,17 @@ private:
 
   void getCut();
 
+  void interpolate(double xi,
+                   double phi);
+
+  double interpolate_step(double exp_value,
+                          double low_bound,
+                          double up_bound,
+                          double theta_l,
+                          double theta_u) const;
+
   bool applyCut(const HepMC::GenParticle *part,
-                const CTPPSBeamParameters &beamParameters) const;
+                const CTPPSBeamParameters &beamParameters);
 
 
 
@@ -96,6 +105,8 @@ private:
   bool debug = 0;
   std::string filename1_;
   std::map<double, std::map<double, std::pair<double, double>>> cutMap1_;
+  double theta_min = 0;
+  double theta_max = 0;
 };
 //----------------------------------------------------------------------------------------------------
 
@@ -211,17 +222,13 @@ void CTPPSGregDucer::produce(edm::Event &event, const edm::EventSetup &iSetup) {
 //----------------------------------------------------------------------------------------------------
 
 bool CTPPSGregDucer::applyCut(const HepMC::GenParticle *part,
-                              const CTPPSBeamParameters &beamParameters) const {
+                              const CTPPSBeamParameters &beamParameters) {
     if(debug) if (part->pdg_id() != 2212) std::cout << "non proton" << std::endl;
-    // double xi = 0;
-    // xi = 1.- part->momentum().rho()/ beamParameters.getBeamMom56();
-    double xi_cutTest, phi_cutTest;
-    xi_cutTest =  0.039000;
-    phi_cutTest = 2.356194;
-    std::pair<double, double> thetas_cutTest = cutMap1_.at(xi_cutTest).at(phi_cutTest);   
-    double theta_min = thetas_cutTest.first;
-    double theta_max = thetas_cutTest.second;
 
+    double xi, phi;
+    xi = 1.- part->momentum().rho()/ beamParameters.getBeamMom56();
+    phi = part->momentum().phi();
+    interpolate(xi, phi);
 
     // accept only stable protons
     if (part->pdg_id() == 2212){
@@ -236,5 +243,118 @@ bool CTPPSGregDucer::applyCut(const HepMC::GenParticle *part,
 }
 
 //----------------------------------------------------------------------------------------------------
+void CTPPSGregDucer::interpolate(double xi,
+                                 double phi){
+
+
+    //transforming phi to (0;2pi) convention
+
+    if(phi < 0) {
+      phi += 2.0 * M_PI;
+    }
+
+
+    auto it_xi_upper = cutMap1_.upper_bound(xi);
+    double xi1, xi2;
+
+    if (it_xi_upper == cutMap1_.end()) {
+    // xi is greater than all keys in the map.
+    // Set both xi1 and xi2 to the largest key.
+    auto it_xi_lower = std::prev(it_xi_upper);
+    xi1 = it_xi_lower->first;
+    xi2 = it_xi_lower->first;
+    } else if (it_xi_upper == cutMap1_.begin()) {
+        // xi is less than all keys in the map.
+        // Set both xi1 and xi2 to the smallest key.
+        xi1 = it_xi_upper->first;
+        xi2 = it_xi_upper->first;
+    } else {
+        // xi is either a key or is between two keys.
+        // The key before it_xi_upper is the smaller one.
+        auto it_xi_lower = std::prev(it_xi_upper);
+        xi1 = it_xi_lower->first;
+        xi2 = it_xi_upper->first;
+    }
+    if(debug) std::cout << xi1 << " " << xi2 << std::endl; //the xi1 is always smaller than xi2 
+
+
+    auto& innerMap = cutMap1_[xi1];
+    auto it_phi_upper = innerMap.upper_bound(phi);
+    double phi1, phi2;
+
+   if (it_phi_upper == innerMap.begin()) {
+    // phi is less than all keys in the map.
+    // Set both phi1 and phi2 to the smallest key.
+    phi1 = it_phi_upper->first;
+    phi2 = it_phi_upper->first;
+    } else if (it_phi_upper == innerMap.end()) {
+        // phi is greater than all keys in the map.
+        // Set both phi1 and phi2 to the largest key.
+        auto it_phi_lower = std::prev(it_phi_upper);
+        phi1 = it_phi_lower->first;
+        phi2 = it_phi_lower->first;
+    } else {
+        // phi is either a key or is between two keys.
+        // The key before it_phi_upper is the smaller one.
+        auto it_phi_lower = std::prev(it_phi_upper);
+        phi1 = it_phi_lower->first;
+        phi2 = it_phi_upper->first;
+    }
+
+    if(debug) std::cout << phi1 << " " << phi2 << std::endl; // phi1 not always smaller than phi2
+
+    // Access the pair for (xi1, phi1) and assign its components
+    std::pair<double, double> thetaPair11 = cutMap1_[xi1][phi1];
+    double theta_m11 = thetaPair11.first;
+    double theta_p11 = thetaPair11.second;
+
+    // Access the pair for (xi1, phi2) and assign its components
+    std::pair<double, double> thetaPair12 = cutMap1_[xi1][phi2];
+    double theta_m12 = thetaPair12.first;
+    double theta_p12 = thetaPair12.second;
+
+    // Access the pair for (xi2, phi1) and assign its components
+    std::pair<double, double> thetaPair21 = cutMap1_[xi2][phi1];
+    double theta_m21 = thetaPair21.first;
+    double theta_p21 = thetaPair21.second;
+
+    // Access the pair for (xi2, phi2) and assign its components
+    std::pair<double, double> thetaPair22 = cutMap1_[xi2][phi2];
+    double theta_m22 = thetaPair22.first;
+    double theta_p22 = thetaPair22.second;
+
+    double theta_interm11, theta_interm12, theta_interm21, theta_interm22;
+
+    // xi1 
+    theta_interm11 = interpolate_step(phi, phi1, phi2, theta_m11, theta_m12);
+    theta_interm12 = interpolate_step(phi, phi1, phi2, theta_p11, theta_p12);
+
+    // xi2
+    theta_interm21 = interpolate_step(phi, phi1, phi2, theta_m21, theta_m22);
+    theta_interm22 = interpolate_step(phi, phi1, phi2, theta_p21, theta_p22);
+
+    // Result
+    theta_min = interpolate_step(xi, xi1, xi2, theta_interm11, theta_interm12);
+    theta_max = interpolate_step(xi, xi1, xi2, theta_interm21, theta_interm22);
+
+    if(debug) std::cout << "Xi: " << xi << " Phi:" << phi << " Theta_min thata_max: " << theta_min << " " << theta_max << std::endl;
+    //I think there is an error in theta interpolation execution not logic
+
+}
+
+
+//----------------------------------------------------------------------------------------------------
+
+  double CTPPSGregDucer::interpolate_step(double exp_value,
+                          double low_bound,
+                          double up_bound,
+                          double theta_l,
+                          double theta_u) const{
+    double theta = 0;
+
+    theta = (exp_value - low_bound) * (theta_u - theta_l) / (up_bound - low_bound) + theta_l;
+
+    return theta;                     
+  }
 
 DEFINE_FWK_MODULE(CTPPSGregDucer);
