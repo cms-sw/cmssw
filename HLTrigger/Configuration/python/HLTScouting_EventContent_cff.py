6f6a06bc45d1726bbd7ff197c163bf1f0c489484
Marco Musich <marco.musich@cern.ch>
2025-08-15 10:59:14 +0200

    introduce HLTScoutingExtra and use it for HLTrigger* event contents

--- HLTrigger/Configuration/python/HLTScouting_EventContent_cff.py
+++ HLTrigger/Configuration/python/HLTScouting_EventContent_cff.py
@@ -3,11 +3,9 @@ import FWCore.ParameterSet.Config as cms
 # EventContent for HLT Scouting products.
 
 # This file keep track of all the scouting objects used since Run-2 
-# HLTScoutingAll is used in Configuration/EventContent to keep all scouting objects available in all AOD/MINIAOD[SIM]
-
 HLTScoutingAll = cms.PSet(
     outputCommands = cms.vstring( *(
-        ## Current scouting objects (from 2024)
+        ## Current scouting objects (from 2025, V1.3, same content as HLTriggerMINIAOD)
         'keep *_hltFEDSelectorL1_*_*',  # PR#20739
         'keep *_hltScoutingEgammaPacker_*_*', # PR#20739
         'keep *_hltScoutingMuonPackerNoVtx_*_*', # PR#44302
@@ -15,7 +13,21 @@ HLTScoutingAll = cms.PSet(
         'keep *_hltScoutingPFPacker_*_*', # PR#20739
         'keep *_hltScoutingPrimaryVertexPacker_*_*',  # PR#20739
         'keep *_hltScoutingTrackPacker_*_*', # PR #23077
+        'keep *_hltScoutingRecHitPacker_*_*', # PR #48613
+        ## Previous scouting objects
+        # Run3 (2022-23)
+        'keep *_hltScoutingMuonPacker_*_*', # added w/ PR#20739, removed w/ PR#44302
 
+        # Run2 only
+        'keep *_hltScoutingCaloPacker_*_*',  # added  w/ PR#20739, removed w/ PR#37114
+        'keep *_hltScoutingMuonPackerCalo_*_*', # added  w/ PR#20739, removed w/  PR#37114
+        'keep *_hltScoutingPrimaryVertexPackerCaloMuon_*_*', # added  w/ PR#20739, removed w/  PR#37114
+    ) )
+)
+
+# HLTScoutingExtra is used in Configuration/EventContent to keep all scouting objects available in all AOD/MINIAOD[SIM]
+HLTScoutingExtra = cms.PSet(
+    outputCommands = cms.vstring( *(
         ## Previous scouting objects
         # Run3 (2022-23)
         'keep *_hltScoutingMuonPacker_*_*', # added w/ PR#20739, removed w/ PR#44302
