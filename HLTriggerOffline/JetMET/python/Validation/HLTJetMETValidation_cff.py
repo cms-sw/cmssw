9549c1ee2ac70897ca2e48cca39db6f2acf179db
Bruno Alves <balves16@hotmail.com>
2025-08-15 10:14:49 +0200

    Add import with MET validation modules.

--- HLTriggerOffline/JetMET/python/Validation/HLTJetMETValidation_cff.py
+++ HLTriggerOffline/JetMET/python/Validation/HLTJetMETValidation_cff.py
@@ -2,6 +2,7 @@ import FWCore.ParameterSet.Config as cms
 
 from HLTriggerOffline.JetMET.Validation.SingleJetValidation_cfi import *
 from Validation.RecoJets.hltJetValidation_cff import *
+from Validation.RecoJets.hltMETValidation_cff import *
 
 ##please do NOT include paths here!
 HLTJetMETValSeq = cms.Sequence(
