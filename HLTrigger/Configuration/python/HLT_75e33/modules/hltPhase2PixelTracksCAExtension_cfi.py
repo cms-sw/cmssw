39851905d001ce797aac2c71244a64be7128b93e
Jan Schulz <jan.gerrit.schulz@cern.ch>
2025-09-04 14:51:21 +0200

    Restructure HLT_75e33 Phase2 PixelTracking configuration
    - introduce PixelVertexing sequence
    - include this PixelVertexing seuquence in combined PixelTracksAndVertices sequence
    - make high purity selected PixelTracks the final PixelTracks for CAExtension

---
+++ HLTrigger/Configuration/python/HLT_75e33/modules/hltPhase2PixelTracksCAExtension_cfi.py
@@ -0,0 +1,12 @@
+import FWCore.ParameterSet.Config as cms
+
+hltPhase2PixelTracksCAExtension = cms.EDProducer("PixelTrackProducerFromSoAAlpakaPhase2",
+    beamSpot = cms.InputTag("hltOnlineBeamSpot"),
+    minNumberOfHits = cms.int32(0),
+    minQuality = cms.string('tight'),
+    pixelRecHitLegacySrc = cms.InputTag("hltSiPixelRecHits"),
+    trackSrc = cms.InputTag("hltPhase2PixelTracksSoA"),
+    outerTrackerRecHitSrc = cms.InputTag("hltSiPhase2RecHits"),
+    useOTExtension = cms.bool(True),
+    requireQuadsFromConsecutiveLayers = cms.bool(True)
+)
--- HLTrigger/Configuration/python/HLT_75e33/sequences/HLTTrackingSequence_cfi.py
+++ HLTrigger/Configuration/python/HLT_75e33/sequences/HLTTrackingSequence_cfi.py
@@ -1,24 +1,17 @@
 import FWCore.ParameterSet.Config as cms
 
 from ..modules.hltGeneralTracks_cfi import *
-from ..modules.hltPhase2PixelVertices_cfi import *
-from ..modules.hltPhase2TrimmedPixelVertices_cfi import *
 from ..modules.hltTrackerClusterCheck_cfi import *
 from ..sequences.HLTHighPtTripletStepSequence_cfi import *
-from ..sequences.HLTPhase2PixelTracksSequence_cfi import *
+from ..sequences.HLTPhase2PixelTracksAndVerticesSequence_cfi import *
 from ..sequences.HLTInitialStepSequence_cfi import *
 from ..sequences.HLTItLocalRecoSequence_cfi import *
 from ..sequences.HLTOtLocalRecoSequence_cfi import *
-from ..modules.hltPhase2PixelTracksCutClassifier_cfi import hltPhase2PixelTracksCutClassifier
-from ..modules.hltPhase2PixelTracksSelectionHighPurity_cfi import hltPhase2PixelTracksSelectionHighPurity
 
 HLTTrackingSequence = cms.Sequence(HLTItLocalRecoSequence+
                                    HLTOtLocalRecoSequence+
                                    hltTrackerClusterCheck+
-                                   HLTPhase2PixelTracksSequence+
-                                   hltPhase2PixelVertices+
-                                   hltPhase2PixelTracksCutClassifier+
-                                   hltPhase2PixelTracksSelectionHighPurity+
+                                   HLTPhase2PixelTracksAndVerticesSequence+
                                    HLTInitialStepSequence+
                                    HLTHighPtTripletStepSequence+
                                    hltGeneralTracks)
@@ -30,8 +23,3 @@ from Configuration.ProcessModifiers.ngtScouting_cff import ngtScouting
 from Configuration.ProcessModifiers.trackingLST_cff import trackingLST
 (ngtScouting & trackingLST).toReplaceWith(HLTTrackingSequence, HLTTrackingSequence.copyAndExclude([HLTHighPtTripletStepSequence]))
 (ngtScouting & ~trackingLST).toReplaceWith(HLTTrackingSequence, HLTTrackingSequence.copyAndExclude([HLTInitialStepSequence,HLTHighPtTripletStepSequence]))
-
-from Configuration.ProcessModifiers.phase2_hlt_vertexTrimming_cff import phase2_hlt_vertexTrimming
-_HLTTrackingSequenceTrimming = HLTTrackingSequence.copy()
-_HLTTrackingSequenceTrimming.insert(_HLTTrackingSequenceTrimming.index(hltPhase2PixelVertices)+1, hltPhase2TrimmedPixelVertices)
-phase2_hlt_vertexTrimming.toReplaceWith(HLTTrackingSequence, _HLTTrackingSequenceTrimming)
