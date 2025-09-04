806ab7415d0dfeced189a1f20238f87ef32e2731
Jan Schulz <jan.gerrit.schulz@cern.ch>
2025-09-04 18:05:55 +0200

    Make Patatrack Pixel Tracking the default for Phase2

--- HLTrigger/Configuration/python/HLT_75e33/modules/hltPhase2PixelTracks_cfi.py
+++ HLTrigger/Configuration/python/HLT_75e33/modules/hltPhase2PixelTracks_cfi.py
@@ -1,16 +1,6 @@
 import FWCore.ParameterSet.Config as cms
 
-hltPhase2PixelTracks = cms.EDProducer("PixelTrackProducer",
-    Cleaner = cms.string('pixelTrackCleanerBySharedHits'),
-    Filter = cms.InputTag("hltPhase2PixelTrackFilterByKinematics"),
-    Fitter = cms.InputTag("hltPhase2PixelFitterByHelixProjections"),
-    SeedingHitSets = cms.InputTag("hltPhase2PixelTracksHitSeeds"),
-    mightGet = cms.optional.untracked.vstring,
-    passLabel = cms.string('hltPhase2PixelTracks')
-)
-
-from Configuration.ProcessModifiers.alpaka_cff import alpaka
-_hltPhase2PixelTracks = cms.EDProducer("PixelTrackProducerFromSoAAlpakaPhase2",
+hltPhase2PixelTracks = cms.EDProducer("PixelTrackProducerFromSoAAlpakaPhase2",
     beamSpot = cms.InputTag("hltOnlineBeamSpot"),
     minNumberOfHits = cms.int32(0),
     minQuality = cms.string('tight'),
@@ -20,7 +10,6 @@ _hltPhase2PixelTracks = cms.EDProducer("PixelTrackProducerFromSoAAlpakaPhase2",
     useOTExtension = cms.bool(False),
     requireQuadsFromConsecutiveLayers = cms.bool(False)
 )
-alpaka.toReplaceWith(hltPhase2PixelTracks, _hltPhase2PixelTracks)
 
 from Configuration.ProcessModifiers.phase2CAExtension_cff import phase2CAExtension
 _hltPhase2PixelTracksCAExtensionSelectionHighPurity = cms.EDProducer("TrackCollectionFilterCloner",
