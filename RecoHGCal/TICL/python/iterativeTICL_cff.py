9a4e5cf7231b53ebbc5d64f9f5da97503c3e5fdd
Wahid Redjeb <wahid.redjeb.wr@gmail.com>
2025-09-04 15:34:46 +0200

    move linking energy regression to TICLCandidateLevel, remove Track only candidates

--- RecoHGCal/TICL/python/iterativeTICL_cff.py
+++ RecoHGCal/TICL/python/iterativeTICL_cff.py
@@ -84,7 +84,7 @@ ticlTracksterLinks = _tracksterLinksProducer.clone(
       type = cms.string('Skeletons')
     
     ),  
-    regressionAndPid = cms.bool(True),
+    regressionAndPid = cms.bool(False),
     inferenceAlgo = cms.string('TracksterInferenceByPFN'),
     pluginInferenceAlgoTracksterInferenceByDNN = cms.PSet(
         algo_verbosity = cms.int32(0),
@@ -115,7 +115,31 @@ ticlTracksterLinks = _tracksterLinksProducer.clone(
         type = cms.string('TracksterInferenceByPFN')
     )
 )
-ticlCandidate = _ticlCandidateProducer.clone()
+
+ticlCandidate = _ticlCandidateProducer.clone(
+    inferenceAlgo=cms.string('TracksterInferenceByPFN'),
+    regressionAndPid = cms.bool(True),
+    pluginInferenceAlgoTracksterInferenceByPFN=cms.PSet(
+        algo_verbosity=cms.int32(0),
+        onnxPIDModelPath=cms.FileInPath(
+            'RecoHGCal/TICL/data/ticlv5/onnx_models/PFN/linking/id_v0.onnx'),
+        onnxEnergyModelPath=cms.FileInPath(
+            'RecoHGCal/TICL/data/ticlv5/onnx_models/PFN/linking/energy_v0.onnx'),
+        inputNames=cms.vstring(
+            'input',
+            'input_tr_features'
+        ),
+        output_en=cms.vstring('enreg_output'),
+        output_id=cms.vstring('pid_output'),
+        eid_min_cluster_energy=cms.double(1),
+        eid_n_layers=cms.int32(50),
+        eid_n_clusters=cms.int32(10),
+        doPID=cms.int32(1),
+        doRegression=cms.int32(1),
+        type=cms.string('TracksterInferenceByPFN')
+    )
+)
+
 mtdSoA = _mtdSoAProducer.clone()
 
 pfTICL = _pfTICLProducer.clone()
