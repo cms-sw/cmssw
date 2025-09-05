1db267ffe451203a8fac37659e8e70447145cdef
Luca Ferragina <ferragina.luca@gmail.com>
2025-09-05 11:10:10 +0200

    Add CA Extension workflows to ph2_hlt short matrix and IB matrix

--- Configuration/PyReleaseValidation/python/relval_Run4.py
+++ Configuration/PyReleaseValidation/python/relval_Run4.py
@@ -70,6 +70,7 @@ numWFIB.extend([prefixDet+234.703])  #LST tracking on CPU (initialStep+HighPtTri
 # Phase-2 HLT tests
 numWFIB.extend([prefixDet+34.7501])# HLTTrackingOnly75e33
 numWFIB.extend([prefixDet+34.751]) # HLTTiming75e33, alpaka
+numWFIB.extend([prefixDet+34.7511])# HLTTiming75e33, phase2CAExtension
 numWFIB.extend([prefixDet+34.752]) # HLTTiming75e33, ticl_v5
 numWFIB.extend([prefixDet+34.753]) # HLTTiming75e33, alpaka,singleIterPatatrack
 numWFIB.extend([prefixDet+34.754]) # HLTTiming75e33, alpaka,singleIterPatatrack,trackingLST
@@ -84,6 +85,8 @@ numWFIB.extend([prefixDet+34.77])  # NGTScouting
 numWFIB.extend([prefixDet+34.771]) # NGTScouting + alpaka + TICL-v5 + TICL-Barrel
 numWFIB.extend([prefixDet+34.772]) # NGTScouting + NANO
 numWFIB.extend([prefixDet+34.773]) # NGTScouting + NANO (including validation)
+numWFIB.extend([prefixDet+34.774])  # NGTScouting + Phase2CAExtension as GeneneralTracks
+numWFIB.extend([prefixDet+34.775])  # NGTScouting + Phase2CAExtension&LSTT5 as GeneralTracks
 
 for numWF in numWFIB:
     workflows[numWF] = _upgrade_workflows[numWF]
