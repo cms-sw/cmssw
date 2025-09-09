50e8b14435da31c1a18d27669079a263c9b1fff5
Gianluca <gianluca.cerminara@cern.ch>
2025-09-09 14:02:50 +0200

    TMUX18 barrel regionizer can now convert TMUX6 input sectors to the expected format

--- L1Trigger/Phase2L1ParticleFlow/interface/regionizer/middle_buffer_multififo_regionizer_ref.h
+++ L1Trigger/Phase2L1ParticleFlow/interface/regionizer/middle_buffer_multififo_regionizer_ref.h
@@ -22,7 +22,8 @@ namespace l1ct {
                                             bool streaming,
                                             unsigned int outii,
                                             unsigned int pauseii,
-                                            bool useAlsoVtxCoords);
+                                            bool useAlsoVtxCoords,
+                                            bool tmux6GCTinput = false);
     // note: this one will work only in CMSSW
     MiddleBufferMultififoRegionizerEmulator(const edm::ParameterSet& iConfig);
 
@@ -88,6 +89,9 @@ namespace l1ct {
     static void encode(const l1ct::EmCaloObjEmu& from, l1ct::HadCaloObjEmu& to);
     static void encode(const l1ct::HadCaloObjEmu& from, l1ct::HadCaloObjEmu& to);
     static void decode(l1ct::HadCaloObjEmu& had, l1ct::EmCaloObjEmu& em);
+    void convert_GCTinput_tmux(const RegionizerDecodedInputs& in_tm6, RegionizerDecodedInputs& in_tm18) const;
+    void init_GCT_tmux18sectors(std::vector<l1ct::DetectorSector<l1ct::HadCaloObjEmu>>& gct_tmux18_hadcalo,
+                                std::vector<l1ct::DetectorSector<l1ct::EmCaloObjEmu>>& gct_tmux18_emcalo) const;
 
   protected:
     const unsigned int NTK_SECTORS, NCALO_SECTORS;
@@ -97,6 +101,7 @@ namespace l1ct {
     bool streaming_;
     bool init_;
     unsigned int iclock_;
+    bool tmux6GCTinput_;
     std::vector<l1ct::PFRegionEmu> mergedRegions_, outputRegions_;
     multififo_regionizer::Regionizer<l1ct::TkObjEmu> tkRegionizerPre_, tkRegionizerPost_;
     multififo_regionizer::Regionizer<l1ct::HadCaloObjEmu> hadCaloRegionizerPre_, hadCaloRegionizerPost_;
@@ -107,6 +112,9 @@ namespace l1ct {
     std::vector<l1ct::multififo_regionizer::EtaPhiBuffer<l1ct::HadCaloObjEmu>> hadCaloBuffers_;
     std::vector<l1ct::multififo_regionizer::EtaPhiBuffer<l1ct::EmCaloObjEmu>> emCaloBuffers_;
     std::vector<l1ct::multififo_regionizer::EtaPhiBuffer<l1ct::MuObjEmu>> muBuffers_;
+    std::vector<PFRegionEmu> gct_slr_regions_;
+    std::vector<l1ct::DetectorSector<l1ct::HadCaloObjEmu>> gct_tmux18_hadcalo_;
+    std::vector<l1ct::DetectorSector<l1ct::EmCaloObjEmu>> gct_tmux18_emcalo_;
 
     template <typename T>
     void fillCaloLinks_(unsigned int iclock,
@@ -119,6 +127,33 @@ namespace l1ct {
                              const std::vector<DetectorSector<l1ct::HadCaloObjEmu>>& had_in,
                              std::vector<l1ct::HadCaloObjEmu>& links,
                              std::vector<bool>& valid);
+
+    void run_worker(const RegionizerDecodedInputs& in, std::vector<PFInputRegion>& out);
+    void init_GCT_slrs(std::vector<l1ct::PFRegionEmu>& gct_slr_regions) const;
+
+    template <typename T>
+    void convert_GCTsector_tmux(const std::vector<l1ct::DetectorSector<T>>& tm6_sectors,
+                                std::vector<l1ct::DetectorSector<T>>& tm18_sectors) const {
+      std::vector<unsigned int> gct_slr_tmux18sector_mapping = {0, 0, 1, 1, 2, 2, 0, 0, 1, 1, 2, 2};
+      for (auto& sec : tm6_sectors) {
+        // std::cout << "Processing TMUX6 hadcalo sector: " << isec << " #cl: " << sec.obj.size() << std::endl;
+        for (auto cl : sec.obj) {
+          if (cl.hwPt == 0)
+            continue;  // skip empty objects
+          glbeta_t gl_hweta = sec.region.hwGlbEta(cl.hwEta);
+          glbphi_t gl_hwphi = sec.region.hwGlbPhi(cl.hwPhi);
+          for (unsigned int islr = 0; islr < gct_slr_regions_.size(); ++islr) {
+            if (gct_slr_regions_[islr].containsHw(gl_hweta, gl_hwphi)) {
+              auto itmux18 = gct_slr_tmux18sector_mapping[islr];
+              cl.hwEta = l1ct::Scales::makeEta(tm18_sectors[itmux18].region.localEta(sec.region.floatGlbEtaOf(cl)));
+              cl.hwPhi = l1ct::Scales::makePhi(tm18_sectors[itmux18].region.localPhi(sec.region.floatGlbPhiOf(cl)));
+              tm18_sectors[itmux18].obj.push_back(cl);
+              break;
+            }
+          }
+        }
+      }
+    };
   };
 }  // namespace l1ct
 
