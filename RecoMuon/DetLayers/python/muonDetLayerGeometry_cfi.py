77e477f0e23f2201b990ec66c41406a3acba7422
Jongwon Shin <eigen1907@gmail.com>
2025-09-11 17:29:16 +0200

    Apply suggestion from code review by perrotta

--- RecoMuon/DetLayers/python/muonDetLayerGeometry_cfi.py
+++ RecoMuon/DetLayers/python/muonDetLayerGeometry_cfi.py
@@ -4,7 +4,7 @@ import FWCore.ParameterSet.Config as cms
 # This cfi should be included to build the muon DetLayers.
 #
 
-MuonDetLayerGeometryESProducer = cms.ESProducer("MuonDetLayerGeometryESProducer", useUpdatedRPCIsFront=False)
+MuonDetLayerGeometryESProducer = cms.ESProducer("MuonDetLayerGeometryESProducer", useUpdatedRPCIsFront=cms.bool(False))
 
 from Configuration.Eras.Modifier_run3_RPC_2025_cff import run3_RPC_2025
 from Configuration.Eras.Modifier_phase2_RPC_cff import phase2_RPC
