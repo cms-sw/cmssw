a8762150cdd72714237c1c4011d587a97b66538a
Chris Jones <chrisdjones15@gmail.com>
2025-09-12 14:50:45 -0500

    Properly find python file for test

--- IOPool/Tests/test/unscheduled_fail_on_output_no_dependency_TryToContinue_cfg.py
+++ IOPool/Tests/test/unscheduled_fail_on_output_no_dependency_TryToContinue_cfg.py
@@ -1,6 +1,6 @@
 import FWCore.ParameterSet.Config as cms
 
-from FWCore.Integration.test.unscheduled_fail_on_output_cfg import process
+from unscheduled_fail_on_output_cfg import process
 process.options.TryToContinue = ['NotFound']
 
 process.out.outputCommands = cms.untracked.vstring('keep *', 'drop *_failing_*_*')
