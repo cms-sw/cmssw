810ed2d0bce75d3f399497d6ef6d125aa06da194
Marco Musich <marco.musich@cern.ch>
2025-09-14 16:17:13 +0200

    restructure HLTPhase2PixelTracksAndVerticesSequence for vertex trimming process modifier

--- HLTrigger/Configuration/python/HLT_75e33/sequences/HLTPhase2PixelVertexingSequence_cfi.py
+++ HLTrigger/Configuration/python/HLT_75e33/sequences/HLTPhase2PixelVertexingSequence_cfi.py
@@ -5,12 +5,3 @@ from ..modules.hltPhase2PixelVertices_cfi import *
 HLTPhase2PixelVertexingSequence = cms.Sequence(
     hltPhase2PixelVertices
 )
-
-from Configuration.ProcessModifiers.phase2_hlt_vertexTrimming_cff import phase2_hlt_vertexTrimming
-from ..modules.hltPhase2TrimmedPixelVertices_cfi import *
-_HLTPhase2PixelVertexingSequenceTrimming = HLTPhase2PixelVertexingSequence.copy()
-_HLTPhase2PixelVertexingSequenceTrimming.insert(
-    _HLTPhase2PixelVertexingSequenceTrimming.index(hltPhase2PixelVertices)+1, 
-    hltPhase2TrimmedPixelVertices
-    )
-phase2_hlt_vertexTrimming.toReplaceWith(HLTPhase2PixelVertexingSequence, _HLTPhase2PixelVertexingSequenceTrimming)
