042f541947b4bd1211cf788d3dfcba6aaae27d77
Patrick Gartung <gartung@fnal.gov>
2025-09-16 11:30:28 -0500

    Add additional metrics. Add options for construction and events transitions

--- PerfTools/AllocMonitor/scripts/edmModuleAllocJsonToCircles.py
+++ PerfTools/AllocMonitor/scripts/edmModuleAllocJsonToCircles.py
@@ -1,51 +1,75 @@
 #!/usr/bin/env python3
 import json
 
-def processModule(moduleLabel, moduleType, moduleInfo, construction):
-    construction[moduleLabel] = {"cpptype": moduleType, "alloc": {}}
+def processModuleTransition(moduleLabel, moduleType, moduleInfo, moduleTransition):
+    moduleTransition[moduleLabel] = {"cpptype": moduleType, "alloc": {}}
     for entry in moduleInfo:
         if entry["transition"] == "construction":
-            construction[moduleLabel]["alloc"] = entry.get("alloc",{})
+            moduleTransition[moduleLabel]["alloc"] = entry.get("alloc",{})
 
-def formatToCircles(construction):
+def formatToCircles(moduleTransition):
     nevents = 1
     doc = {
-        "modules": [],
-        "resources": [
-            {
-                "name": "nAlloc",
-                "description": "num allocs",
-                "title": "Number of allocations",
-                "unit": "B"
-            },
-            {
-                "name": "added",
-                "description": "added memory",
-                "title": "Amount of memory added to the process at the end of the transition",
-                "unit": "B"
-            },
-            {
-                "name": "maxTemp",
-                "description": "peak temporary memory",
-                "title": "Peak temporary memory held during the transition",
-                "unit": "B"
-            }
-        ],
-        "total": {
-            "events": nevents, # TODO
-            "label": "FOO", # TODO
-            "nAlloc": 1, # TODO
-            "added": 1, # TODO
-            "maxTemp": 1, # TODO
-            "type": "Job"
-        }
+       "modules": [],
+       "resources": [
+           {
+               "name": "added",
+               "description": "added memory",
+               "title": "Amount of memory added to the process at the end of the transition",
+               "unit": "Bytes"
+           },
+           {
+               "name": "nAlloc",
+               "description": "num allocs",
+               "title": "Number of allocations",
+               "unit": "count"
+           },
+           {
+               "name": "max1Alloc",
+               "description": "Maximum temporary allocations",
+               "title": "Maximum temporary allocations held during the transition",
+               "unit": "count"
+           },
+           {
+               "name": "nDealloc",
+               "description": "num deallocs",
+               "title": "Number of deallocations",
+               "unit": "count"
+           },
+           {
+               "name": "minTemp",
+               "description": "minimum temporary memory",
+               "title": "Minimum temporary memory held during the transition",
+               "unit": "Bytes"
+           },
+           {
+               "name": "maxTemp",
+               "description": "peak temporary memory",
+               "title": "Maximum temporary memory held during the transition",
+               "unit": "Bytes"
+           }
+       ],
+       "total": {
+           "events": 0,
+           "label": "Job",
+           "nAlloc": 0,
+           "nDealloc": 0,
+           "minTemp": 0,
+           "max1Alloc": 0,
+           "added": 0,
+           "maxTemp": 0,
+           "type": "Job"
+       }
     }
-    for label, info in construction.items():
+    for label, info in moduleTransition.items():
         alloc=info.get("alloc", {})
         if alloc:
             added = alloc.get("added", 0)
             nAlloc = alloc.get("nAlloc", 0)
+            nDealloc = alloc.get("nDealloc", 0)
+            minTemp = alloc.get("minTemp", 0)
             maxTemp = alloc.get("maxTemp", 0)
+            max1Alloc = alloc.get("max1Alloc", 0)
         doc["modules"].append({
             "events" : nevents,
             "label": label,
@@ -53,21 +77,27 @@ def formatToCircles(construction):
             "nAlloc": nAlloc,
             "added": added,
             "maxTemp": maxTemp,
+            "nDealloc": nDealloc,
+            "minTemp": minTemp,
+            "max1Alloc": max1Alloc
         })
     return doc
             
 def main(args):
+    import sys
     doc = json.load(args.filename)
     moduleTypes = doc['cpptypes']
-
-    construction = dict()
-
-    processModule("source", "sourceType", doc["source"],construction)
-    for moduleLabel, moduleInfo in doc["modules"].items():
-        processModule(moduleLabel, moduleTypes[moduleLabel], moduleInfo, construction)
-
-    import sys
-    json.dump(formatToCircles(construction), sys.stdout, indent=2)
+    if args.construction:
+        moduleTransition = dict()
+        processModuleTransition("source", "PoolSource", doc["source"], moduleTransition)
+        for moduleLabel, moduleInfo in doc["modules"].items():
+            processModuleTransition(moduleLabel, moduleTypes[moduleLabel], moduleInfo, moduleTransition)
+        json.dump(formatToCircles(moduleTransition), sys.stdout, indent=2)
+    if args.events:
+        moduleTransition = dict()
+        for moduleLabel, moduleInfo in doc["modules"].items():
+            processModuleTransition(moduleLabel, moduleTypes[moduleLabel], moduleInfo, moduleTransition)
+        json.dump(formatToCircles(moduleTransition), sys.stdout, indent=2)
 
 if __name__ == "__main__":
     import argparse
@@ -76,6 +106,12 @@ if __name__ == "__main__":
     parser.add_argument('filename',
                         type=argparse.FileType('r'), # open file
                         help='file to process')
+    parser.add_argument('--construction',
+                        action='store_true',
+                        help='show construction transition memory use')
+    parser.add_argument('--events',
+                        action='store_true',
+                        help='show event transition memory use')
 
     args = parser.parse_args()
     main(args)
