daf6d621ffe78b0e4754a5aa0358bcc4950b2b38
brusale <brusale11@gmail.com>
2025-09-17 18:35:08 +0200

    SimTracksters and CLUE3DBarrel with ticl_barrel
    Produce SimTracksters and run CLUE3D in the barrel when
    the ticl_barrel procModifier is enabled

--- RecoHGCal/TICL/python/CLUE3DBarrel_cff.py
+++ RecoHGCal/TICL/python/CLUE3DBarrel_cff.py
@@ -1,5 +1,6 @@
 import FWCore.ParameterSet.Config as cms
 
+from RecoHGCal.TICL.TICLSeedingRegions_cff import ticlSeedingGlobal
 from RecoHGCal.TICL.filteredLayerClustersProducer_cfi import filteredLayerClustersProducer as _filteredLayerClustersProducer
 from RecoHGCal.TICL.trackstersProducer_cfi import trackstersProducer as _trackstersProducer
 
@@ -7,16 +8,19 @@ filteredLayerClustersCLUE3DBarrel = _filteredLayerClustersProducer.clone(
     clusterFilter = "ClusterFilterByAlgo",
     algo_number = [10, 11],
     iteration_label = "CLUE3DBarrel",
-    max_layerid = 5
+    max_layerId = 5
 )
 
 # PATTERN RECOGNITION
-ticlTrackstersCLUE3DBarrel = _tracksters.clone(
+ticlTrackstersCLUE3DBarrel = _trackstersProducer.clone(
     filtered_mask = "filteredLayerClustersCLUE3DBarrel:CLUE3DBarrel",
-    seedins_regions = "ticlSeedingGlobal",
-    itername = "Barrel",
+    seeding_regions = "ticlSeedingGlobal",
+    itername = "CLUE3DBarrel",
+    detector = "Barrel",
     patternRecognitionBy = "CLUE3D",
+    layer_clusters_barrel_tiles = "ticlLayerTileBarrel:ticlLayerTilesBarrel",
     pluginPatternRecognitionByCLUE3D = dict(
+        algo_verbosity = 9999,
         criticalDensity = [0.5, 0.5, 0.5],
         criticalSelfDensity = [0., 0., 0.],
         criticalEtaPhiDistance = [3 * 0.0175, 3 * 0.087, 3 * 0.087, 3 * 0.087],
@@ -24,6 +28,10 @@ ticlTrackstersCLUE3DBarrel = _tracksters.clone(
         densityOnSameLayer = False,
         minNumLayerCluster = [1, 1, 1],
         useAbsoluteProjectiveScale = False,
-        densitySiblingLayers = [4, 4, 4]
+        densitySiblingLayers = [2, 4, 4]
     )
-)  
+)
+ 
+ticlCLUE3DBarrelTask = cms.Task(ticlSeedingGlobal
+    ,ticlTrackstersCLUE3DBarrel
+) 
