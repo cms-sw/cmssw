463184a42e9bef0befdeed078fe63d6fa9162833
Matti Kortelainen <matti.kortelainen@cern.ch>
2025-09-18 11:20:04 -0500

    Refactor commonalities between setenv and putenv tests

--- Utilities/ReleaseScripts/test/gdb/test-cmsTraceFunction-setenv.sh
+++ Utilities/ReleaseScripts/test/gdb/test-cmsTraceFunction-setenv.sh
@@ -2,44 +2,35 @@
 
 TRACE="cmsTraceFunction --startAfterFunction ScheduleItems::initMisc setenv -f putenv --abort"
 
+check_func() {
+  local func_name="$1"
+  local src_name="$2"
+  local exe_name="test-cmsTraceFunction-${func_name}"
+  local raw_log="${func_name}_raw.log"
+  local log="${func_name}.log"
+
+  g++ -o "$exe_name" "$(dirname $0)/$src_name"
+  set +e
+  $TRACE ./$exe_name 2>&1 > "$raw_log"
+  local ret=$?
+  set -e
+  grep "$func_name" "$raw_log" > "$log"
+
+  if [ ${ret} = 0 ]; then
+    echo "cmsTraceFunction exited with exit code 0, expected non-zero exit code"
+    exit 1
+  fi
+
+  local call_count=$(grep -c "^${func_name}() called" "$log")
+  local break_count=$(grep -c "Breakpoint .* in ${func_name} ()" "$log")
+  if [ ${call_count} != 1 ] || [ ${break_count} != 1 ] ; then
+    echo "Unexpected number of ${func_name} calls ${call_count} or breakpoints ${break_count}; expecting both to be 1"
+    exit 1
+  fi
+}
+
 # Check setenv
-g++ -o test-cmsTraceFunction-setenv $(dirname $0)/test-cmsTraceFunction-setenv.cpp
-set +e
-$TRACE ./test-cmsTraceFunction-setenv 2>&1 > setenv_raw.log
-ret_setenv=$?
-set -e
-grep setenv setenv_raw.log > setenv.log
-#rm -f test-cmsTraceFunction-setenv
-
-if [ ${ret_setenv} = 0 ]; then
-  echo "cmsTraceFunction exited with exit code 0, expected non-zero exit code"
-  exit 1
-fi
-
-setenv_count=$(grep -c '^setenv() called' setenv.log)
-break_setenv=$(grep -c 'Breakpoint .* in setenv ()' setenv.log)
-if [ ${setenv_count} != 1 ] || [ ${break_setenv} != 1 ] ; then
-  echo "Unexpected number of setenv calls ${setenv_count} or breakpoints ${break_setenv}; expecting both to be 1"
-  exit 1
-fi
+check_func "setenv" "test-cmsTraceFunction-setenv.cpp"
 
 # Check putenv
-g++ -o test-cmsTraceFunction-putenv $(dirname $0)/test-cmsTraceFunction-putenv.cpp
-set +e
-$TRACE ./test-cmsTraceFunction-putenv 2>&1 > putenv_raw.log
-ret_putenv=$?
-set -e
-grep putenv putenv_raw.log > putenv.log
-rm -f test-cmsTraceFunction-putenv
-
-if [ ${ret_puttenv} = 0 ]; then
-  echo "cmsTraceFunction exited with exit code 0, expected non-zero exit code"
-  exit 1
-fi
-
-putenv_count=$(grep -c '^putenv() called' putenv.log)
-break_putenv=$(grep -c 'Breakpoint .* in putenv ()' putenv.log)
-if [ ${putenv_count} != 1 ] || [ ${break_tenv} != 1 ] ; then
-  echo "Unexpected number of putenv calls ${putenv_count} or breakpoints ${break_putenv}; expecting both to be 1"
-  exit 1
-fi
+check_func "putenv" "test-cmsTraceFunction-putenv.cpp"
