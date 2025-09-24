6042d6a95497eeb2e021c140ea309e44d7793293
Patrick Gartung <gartung@fnal.gov>
2025-09-24 14:35:38 -0500

    Changes suggested by code review.

--- PerfTools/AllocMonitor/scripts/edmModuleAllocJsonToCircles.py
+++ PerfTools/AllocMonitor/scripts/edmModuleAllocJsonToCircles.py
@@ -1,10 +1,15 @@
 #!/usr/bin/env python3
 import json
-transitionTypes = ["construction", "event",
-                        "begin job", "begin stream",
-                        "global begin run", "global begin luminosity block",
-                        "stream begin run", "stream begin luminosity block",
-                        ]
+transitionTypes = [
+    "construction",
+    "begin job",
+    "begin stream",
+    "global begin run",
+    "stream begin run",
+    "global begin luminosity block",
+    "stream begin luminosity block",
+    "event",
+]
 allocTypes = ["added", "nAlloc", "nDealloc", "maxTemp", "max1Alloc"]
 
 def processModuleTransition(moduleLabel, moduleType, moduleInfo, transitionType, moduleTransition):
@@ -24,44 +29,44 @@ def formatToCircles(moduleTransitions):
     for transitionType in transitionTypes:
         doc["resources"] += [
             {
-               "name": "added %s" % transitionType,
-               "description": "added memory per %s transition" % transitionType,
-               "title": "Amount of memory added to the process at the end of the %s transition" % transitionType,
+               "name": f"added {transitionType}",
+               "description": f"{transitionType}: added memory (average)",
+               "title": f"Amount of memory added to the process at the end of the {transitionType} transition",
                "unit": "kB"
             },
             {
 
-               "name": "nAlloc %s" % transitionType,
-               "description": "num allocs per %s transition" % transitionType,
-               "title": "Number of allocations during the %s transition" % transitionType,
+               "name": f"nAlloc {transitionType}",
+               "description": f"{transitionType}: num allocs (average)",
+               "title": f"Number of allocations during the {transitionType} transition",
                "unit": ""
             },
             {
-               "name": "max1Alloc %s" % transitionType,
-               "description": "maximum one time allocation per %s transition" % transitionType,
-               "title": "Maximum one time allocation held during the %s transition" % transitionType,
-               "unit": "kB"
-           },
-           {
-               "name": "nDealloc %s" % transitionType,
-               "description": "num deallocs for %s transition" % transitionType,
-               "title": "Number of deallocations during the %s transition" % transitionType,
+               "name": f"nDealloc {transitionType}",
+               "description": f"{transitionType}: num deallocs (average)",
+               "title": f"Number of deallocations during the {transitionType} transition",
                "unit": ""
-           },
-           {
-               "name": "maxTemp %s" % transitionType,
-               "description": "maximum temporary memory per %s transition" % transitionType,
-               "title": "Maximum temporary memory held during the %s transition" % transitionType,
+            },
+            {
+               "name": f"maxTemp {transitionType}",
+               "description": f"{transitionType}: maximum temporary memory (average)",
+               "title": f"Maximum temporary memory held during the {transitionType} transition",
                "unit": "kB"
-           },
+            },
+            {
+               "name": f"max1Alloc {transitionType}",
+               "description": f"{transitionType}: largest single allocation (average)",
+               "title": f"Largest single allocation held during the {transitionType} transition",
+               "unit": "kB"
+            },
         ]
-# The circles code uses the "events" field to normalize the values between files with different number of events
-# Here we set it to 1 for the total events because the total is already normalized per transition
+    # The circles code uses the "events" field to normalize the values between files with different number of events
+    # Here we set it to 1 for the total events because the total is already normalized per transition
         doc["total"]["events"] = 1
         doc["total"]["label"] = "Job"
         doc["total"]["type"] = "Job"
         for allocType in allocTypes:
-            doc["total"]["%s %s" % (allocType, transitionType)] = 0
+            doc["total"][f"{allocType} {transitionType}"] = 0
 
     for transitionType, moduleTransition in moduleTransitions.items():
         for label, info in moduleTransition.items():
@@ -84,22 +89,22 @@ def formatToCircles(moduleTransitions):
                 max1Alloc += alloc.get("max1Alloc", 0)
             ntransitions = moduleTransitions[transitionType][label]["nTransitions"]
             if ntransitions > 0:
-                modules_dict[label]["nAlloc %s" % transitionType] = nAlloc/ntransitions
-                modules_dict[label]["added %s" % transitionType] = (added/ntransitions)/1024
-                modules_dict[label]["maxTemp %s" % transitionType] = (maxTemp/ntransitions)/1024
-                modules_dict[label]["nDealloc %s" % transitionType] = nDealloc/ntransitions
-                modules_dict[label]["max1Alloc %s" % transitionType] = (max1Alloc/ntransitions)/1024
+                modules_dict[label][f"nAlloc {transitionType}"] = nAlloc/ntransitions
+                modules_dict[label][f"added {transitionType}"] = (added/ntransitions)/1024
+                modules_dict[label][f"maxTemp {transitionType}"] = (maxTemp/ntransitions)/1024
+                modules_dict[label][f"nDealloc {transitionType}"] = nDealloc/ntransitions
+                modules_dict[label][f"max1Alloc {transitionType}"] = (max1Alloc/ntransitions)/1024
             else:
-                modules_dict[label]["nAlloc %s" % transitionType] = nAlloc
-                modules_dict[label]["added %s" % transitionType] = (added)/1024
-                modules_dict[label]["maxTemp %s" % transitionType] = (maxTemp)/1024
-                modules_dict[label]["nDealloc %s" % transitionType] = nDealloc
-                modules_dict[label]["max1Alloc %s" % transitionType] = max1Alloc/1024
-            doc["total"]["nAlloc %s" % transitionType] += modules_dict[label]["nAlloc %s" % transitionType] 
-            doc["total"]["nDealloc %s" % transitionType] += modules_dict[label]["nDealloc %s" % transitionType]
-            doc["total"]["maxTemp %s" % transitionType] += modules_dict[label]["maxTemp %s" % transitionType]
-            doc["total"]["added %s" % transitionType] += modules_dict[label]["added %s" % transitionType]
-            doc["total"]["max1Alloc %s" % transitionType] += modules_dict[label]["max1Alloc %s" % transitionType]
+                modules_dict[label][f"nAlloc {transitionType}"] = nAlloc
+                modules_dict[label][f"added {transitionType}"] = (added)/1024
+                modules_dict[label][f"maxTemp {transitionType}"] = (maxTemp)/1024
+                modules_dict[label][f"nDealloc {transitionType}"] = nDealloc
+                modules_dict[label][f"max1Alloc {transitionType}"] = max1Alloc/1024
+            doc["total"][f"nAlloc {transitionType}"] += modules_dict[label][f"nAlloc {transitionType}"]
+            doc["total"][f"nDealloc {transitionType}"] += modules_dict[label][f"nDealloc {transitionType}"]
+            doc["total"][f"maxTemp {transitionType}"] += modules_dict[label][f"maxTemp {transitionType}"]
+            doc["total"][f"added {transitionType}"] += modules_dict[label][f"added {transitionType}"]
+            doc["total"][f"max1Alloc {transitionType}"] += modules_dict[label][f"max1Alloc {transitionType}"]
 
     for key in sorted(modules_dict.keys()):
         module = modules_dict[key]
