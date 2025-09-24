09a572ca8e8e9c02e14a1c65b9e8ecbc4382ee3b
Martin Grunewald <Martin.Grunewald@cern.ch>
2025-09-24 09:28:51 +0200

    HLT menu development for 15_0_X
    These updates correspond to further changes accumulated after the "V1.3" (last) GRun menu was delivered.
    This update includes the integration of the following JIRA tickets:
    - [CMSHLT-3629](https://its.cern.ch/jira/browse/CMSHLT-3629) [BPH] Low-PU 2025 BPH DiMuon path with pT=0 and OS+SS muons
    - [CMSHLT-3640](https://its.cern.ch/jira/browse/CMSHLT-3640) [HIN] Silence framework related warnings in HIon menu

--- HLTrigger/Configuration/python/HLT_PRef_cff.py
+++ HLTrigger/Configuration/python/HLT_PRef_cff.py
@@ -1,6 +1,6 @@
 # hltGetConfiguration /dev/CMSSW_15_0_0/PRef --cff --data --type PRef
 
-# /dev/CMSSW_15_0_0/PRef/V114 (CMSSW_15_0_11)
+# /dev/CMSSW_15_0_0/PRef/V117 (CMSSW_15_0_11)
 
 import FWCore.ParameterSet.Config as cms
 
@@ -9,7 +9,7 @@ fragment = cms.ProcessFragment( "HLT" )
 fragment.load("Configuration.StandardSequences.Accelerators_cff")
 
 fragment.HLTConfigVersion = cms.PSet(
-  tableName = cms.string("/dev/CMSSW_15_0_0/PRef/V114")
+  tableName = cms.string("/dev/CMSSW_15_0_0/PRef/V117")
 )
 
 fragment.HLTGroupedCkfTrajectoryBuilderP5 = cms.PSet( 
