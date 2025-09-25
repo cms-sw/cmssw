aec0366f386e3bdb79707683187eb7c85d6ecbc0
Bruno Alves <balves16@hotmail.com>
2025-09-25 10:41:25 +0200

    Support multiple MET collections.

--- Validation/RecoMET/scripts/makeHLTMETValidationPlots.py
+++ Validation/RecoMET/scripts/makeHLTMETValidationPlots.py
@@ -19,8 +19,17 @@ class dotdict(dict):
     __getattr__ = dict.get
     __setattr__ = dict.__setitem__
     __delattr__ = dict.__delitem__
-    
-def CheckRootFile(hname, rebin=None):
+
+def createDir(adir):
+    if not os.path.exists(adir):
+        os.makedirs(adir)
+    return adir
+
+def checkRootDir(afile, adir):
+    if not afile.Get(adir):
+        raise RuntimeError(f"Directory '{adir}' not found in {afile}")
+
+def checkRootFile(hname, rebin=None):
     hist_orig = file.Get(hname)
     if not hist_orig:
         raise RuntimeError(f"WARNING: Histogram {hname} not found.")
@@ -98,153 +107,125 @@ class Plotter:
             plt.savefig(name + '.' + ext)
         plt.close()
 
+def plot1Dvars(adir, avars, outdir, metType, top_text=False):
+    """
+    Plots 1D distributions.
+    The `avars` variables is a dictionary whose values are (xlabel, ylabel, rebin).
+    """
+    for var, (xlabel, ylabel, rebin) in avars.items():
+        plotter = Plotter(args.sample_label)
+        root_hist = checkRootFile(f"{adir}/{var}", rebin=rebin)
+        nbins, bin_edges, bin_centers, bin_widths = define_bins(root_hist)
+        values, errors = histo_values_errors(root_hist)
+        
+        plotter.ax.errorbar(bin_centers, values, xerr=None, yerr=errors,
+                            fmt='s', color='black', label=xlabel, **errorbar_kwargs)
+        # plotter.ax.step(bin_edges[:-1], values, where="post", color='black')
+        plotter.ax.stairs(values, bin_edges, color='black', linewidth=2)
+        plotter.ax.text(0.03, 0.97, metType, transform=plotter.ax.transAxes, fontsize=fontsize,
+                        verticalalignment='top', horizontalalignment='left')
+
+        diff_step = 0.05 * abs(max(values)-min(values))
+        plotter.limits(y=(min(values) - diff_step, max(values) + 2*diff_step), logY=False)
+        plotter.labels(x=xlabel, y=ylabel)
+
+        if top_text:
+            plotter.ax.text(0.97, 0.97, root_hist.GetTitle().replace('ET', r'$E_T$'), transform=plotter.ax.transAxes, fontsize=fontsize,
+                            verticalalignment='top', horizontalalignment='right')
+
+        plotter.save( os.path.join(outdir, var) )
+
+def plot1Dtrigger(adir, avars, metType, outdir):
+    avarsDict = {}
+    for var in avars:
+        root_hist = checkRootFile(f"{adir}/{var}", rebin=None)
+
+        xlabel = root_hist.GetXaxis().GetTitle().replace('ET', r'$E_T$ ')
+        ylabel = root_hist.GetYaxis().GetTitle()
+
+        avarsDict[var] = (xlabel, ylabel, None)
+
+    plot1Dvars(adir, avarsDict, outdir, metType=metType, top_text=True)
+
+    
 if __name__ == '__main__':
     full_command = 'for amet in "hltPFMET" "hltPFPuppiMET" "hltPFPuppiMETTypeOne"; do python3 Validation/RecoMET/scripts/makeHLTMETValidationPlots.py --file Run/DQM_1000_Wprime.root --odir /eos/user/b/bfontana/www/MET_Valid/Wprime -l Wprime --met ${amet}; done'
     parser = argparse.ArgumentParser(description='Make HLT MET validation plots. \nRun all MET paths with\n' + full_command)
     parser.add_argument('-f', '--file', required=True, help='Paths to the DQM ROOT file.')
-    parser.add_argument('-m', '--met', default='hltPFPuppiMET', help='Name of the met collection')
+    parser.add_argument('-m', '--met',  nargs='+', default='hltPFPuppiMET', required=False, help='Name of the met collection(s).')
     parser.add_argument('-o', '--odir', default="HLTMETValidationPlots", required=False, help='Path to the output directory.')
     parser.add_argument('-l', '--sample_label', default="QCD (200 PU)", required=False,  help='Sample label for plotting.')
     args = parser.parse_args()
 
-    if not os.path.exists(args.odir):
-        os.makedirs(args.odir)
-
-    outdir = os.path.join(args.odir, args.met)
-    if not os.path.exists(outdir):
-        os.makedirs(outdir)
+    createDir(args.odir)
+    for metType in args.met:
+        outdir = createDir(os.path.join(args.odir, metType))
     
     file = ROOT.TFile.Open(args.file)
-    dqm_dir = f"DQMData/Run 1/HLT/Run summary/JetMET/METValidation/{args.met}"
-    if not file.Get(dqm_dir):
-        raise RuntimeError(f"Directory '{dqm_dir}' not found in {args.file}")
-
-    fontsize = 16    
-
+    fontsize = 16
     tprofile_rebinning = {'B': (30, 40, 50, 80, 100, 120, 140, 160, 200, 250, 300, 350, 400, 500, 600), #barrel
                           'E': (30, 40, 50, 80, 100, 120, 140, 160, 200, 250, 300, 350, 400, 500, 600), # endcap
                           'F': (30, 40, 50, 80, 120, 240, 600)} # forward
 
     METType = {'hltPFMET': "PF MET",
                'hltPFPuppiMET': "PF PUPPI MET",
-               'hltPFPuppiMETTypeOne': "PF Type-1 PUPPI MET"}.get(args.met, args.met)
+               'hltPFPuppiMETTypeOne': "PF Type-1 PUPPI MET"}
     
     colors = hep.style.CMS['axes.prop_cycle'].by_key()['color']
     markers = ('o', 's', 'd')
     errorbar_kwargs = dict(capsize=3, elinewidth=0.8, capthick=2, linewidth=2, linestyle='')
 
-    #####################################
-    # Plot 1D variables from METTester
-    #####################################
-    Var1DList = {
-        'HFEMEt'                  : (r'HF EM $E_T$', None),
-        'HFEMEtFraction'          : (r'HF EM $E_T$ fraction', None),
-        'HFHadronEt'              : (r'HF Hadron $E_T$', 2),
-        'HFHadronEtFraction'      : ('HF Hadron $E_T$ fraction', 2),
-        'MET'                     : ('MET', 2),
-        'MEx'                     : ('MET x', 4),
-        'MEy'                     : ('MET y', 4),
-        'METPhi'                  : (r'MET $\phi$', 2),
-        'METDeltaPhi_GenMETCalo'  : (r'Calo MET $\Delta\phi$', 2),
-        'METDeltaPhi_GenMETTrue'  : (r'True MET $\Delta\phi$', 2),
-        'METDiff_GenMETCalo'      : (r'MET - gen MET$_{Calo}$', 10),
-        'METDiff_GenMETTrue'      : (r'MET - gen MET$_{True}$', 10),
-        'METSignPseudo'           : ('MET Significance (Event-by-event)', None), # Et / std: (: (sqrt(sumEt)
-        'METSignReal'             : ('MET Significance (Likelihood)', None), # covariance matrix missing
-        'MET_Nvtx'                : ('Number of vertices (MET-weighted)', 6),
-        'Nvertex'                 : ('Number of vertices', 6),
-        'SumET'                   : (r'$\sum E_T$', 4),
-        'chargedHadronEt'         : (r'Charged Hadron $E_T$', 2),
-        'chargedHadronEtFraction' : (r'Charged Hadron $E_T$ fraction', 2),
-        'neutralHadronEt'         : (r'Neutral Hadron $E_T$', 2),
-        'neutralHadronEtFraction' : (r'Neutral Hadron $E_T$ fraction', 2),
-        'photonEt'                : (r'Photon $E_T$', 2),
-        'photonEtFraction'        : (r'Photon $E_T$ fraction', 2),
-        'muonEt'                  : (r'Muon $E_T$', None),
-        'muonEtFraction'          : (r'Muon $E_T$ fraction', None),
-        'electronEt'              : (r'Electron $E_T$', None),
-        'electronEtFraction'      : (r'Electron $E_T$ fraction', None),
+    nEventsLabel = '# Events'
+    vars1D = {
+        'HFEMEt'                  : (r'HF EM $E_T$', nEventsLabel, None),
+        'HFEMEtFraction'          : (r'HF EM $E_T$ fraction', nEventsLabel, None),
+        'HFHadronEt'              : (r'HF Hadron $E_T$', nEventsLabel, 2),
+        'HFHadronEtFraction'      : ('HF Hadron $E_T$ fraction', nEventsLabel, 2),
+        'MET'                     : ('MET', nEventsLabel, 2),
+        'MEx'                     : ('MET x', nEventsLabel, 4),
+        'MEy'                     : ('MET y', nEventsLabel, 4),
+        'METPhi'                  : (r'MET $\phi$', nEventsLabel, 2),
+        'METDeltaPhi_GenMETCalo'  : (r'Calo MET $\Delta\phi$', nEventsLabel, 2),
+        'METDeltaPhi_GenMETTrue'  : (r'True MET $\Delta\phi$', nEventsLabel, 2),
+        'METDiff_GenMETCalo'      : (r'MET - gen MET$_{Calo}$', nEventsLabel, 10),
+        'METDiff_GenMETTrue'      : (r'MET - gen MET$_{True}$', nEventsLabel, 10),
+        'METSignPseudo'           : ('MET Significance (Event-by-event)', nEventsLabel, None), # Et / std: (: (sqrt(sumEt)
+        'METSignReal'             : ('MET Significance (Likelihood)', nEventsLabel, None), # covariance matrix missing
+        'MET_Nvtx'                : ('Number of vertices (MET-weighted)', nEventsLabel, 6),
+        'Nvertex'                 : ('Number of vertices', nEventsLabel, 6),
+        'SumET'                   : (r'$\sum E_T$', nEventsLabel, 4),
+        'chargedHadronEt'         : (r'Charged Hadron $E_T$', nEventsLabel, 2),
+        'chargedHadronEtFraction' : (r'Charged Hadron $E_T$ fraction', nEventsLabel, 2),
+        'neutralHadronEt'         : (r'Neutral Hadron $E_T$', nEventsLabel, 2),
+        'neutralHadronEtFraction' : (r'Neutral Hadron $E_T$ fraction', nEventsLabel, 2),
+        'photonEt'                : (r'Photon $E_T$', nEventsLabel, 2),
+        'photonEtFraction'        : (r'Photon $E_T$ fraction', nEventsLabel, 2),
+        'muonEt'                  : (r'Muon $E_T$', nEventsLabel, None),
+        'muonEtFraction'          : (r'Muon $E_T$ fraction', nEventsLabel, None),
+        'electronEt'              : (r'Electron $E_T$', nEventsLabel, None),
+        'electronEtFraction'      : (r'Electron $E_T$ fraction', nEventsLabel, None),
+        # MET post-processing
+        'METDiffAggr_MET': ('MET', 'MET Mean Difference', None),
+        'METDiffAggr_Phi': (r'$\phi$', 'MET Mean Difference', None),
+        'METResolAggr_MET': ('MET', 'MET Resolution', None),
+        'METResolAggr_Phi': (r'$\phi$', 'MET Resolution', None),
+        'METRespAggr_MET': ('MET', 'MET Response', None),
+        'METRespAggr_Phi': (r'$\phi$', 'MET Response', None),
+        'METSignAggr_MET': ('MET', 'MET Significance', None),
+        'METSignAggr_Phi': (r'$\phi$', 'MET Significance', None)
     }
-    
-    for var, (xlabel, rebin) in Var1DList.items():
-        plotter = Plotter(args.sample_label)
-        root_hist = CheckRootFile(f"{dqm_dir}/{var}", rebin=rebin)
-        nbins, bin_edges, bin_centers, bin_widths = define_bins(root_hist)
-        values, errors = histo_values_errors(root_hist)
 
-        plotter.ax.errorbar(bin_centers, values, xerr=None, yerr=errors,
-                     fmt='s', color='black', label=xlabel, **errorbar_kwargs)
-        plotter.ax.step(bin_edges[:-1], values, where="post", color='black')
-        plotter.ax.text(0.03, 0.97, METType, transform=plotter.ax.transAxes, fontsize=fontsize,
-                        verticalalignment='top', horizontalalignment='left')
+    # Plot 1D MET variables
+    # for metType in args.met:
+    #     dqm_dir = f"DQMData/Run 1/HLT/Run summary/JetMET/METValidation/{metType}"
+    #     checkRootDir(file, dqm_dir)
+    #     plot1Dvars(dqm_dir, vars1D, METType[metType], outdir=os.path.join(args.odir, metType))
 
-        diff_step = 0.05 * abs(max(values)-min(values))
-        plotter.limits(y=(min(values) - diff_step, max(values) + 2*diff_step), logY=False)
-        plotter.labels(x=xlabel, y='# Events')
-
-        plotter.save( os.path.join(outdir, var) )
-
-    ################################################
-    # Plot 1D variables from METTesterPostProcessor
-    ################################################
-    var1dNames = {'METDiffAggr_MET': ('MET', 'MET Mean Difference'),
-                  'METDiffAggr_Phi': (r'$\phi$', 'MET Mean Difference'),
-                  'METResolAggr_MET': ('MET', 'MET Resolution'),
-                  'METResolAggr_Phi': (r'$\phi$', 'MET Resolution'),
-                  'METRespAggr_MET': ('MET', 'MET Response'),
-                  'METRespAggr_Phi': (r'$\phi$', 'MET Response'),
-                  'METSignAggr_MET': ('MET', 'MET Significance'),
-                  'METSignAggr_Phi': (r'$\phi$', 'MET Significance')}
-
-    for var, (xlabel, ylabel) in var1dNames.items():
-        plotter = Plotter(args.sample_label)
-        root_hist = CheckRootFile(f"{dqm_dir}/{var}", rebin=None)
-        nbins, bin_edges, bin_centers, bin_widths = define_bins(root_hist)
-        values, errors = histo_values_errors(root_hist)
+    # Plot MET turn-on curves
+    trigger = 'HLT_PFPuppiMETTypeOne140_PFPuppiMHT140'
+    turnon_dir = f"DQMData/Run 1/HLT/Run summary/JetMET/TurnOnValidation/{trigger}"
+    checkRootDir(file, turnon_dir)
+    vars1Dtrigger = ('TurnOngMET', 'TurnOngMETLow', 'TurnOnhMET', 'TurnOnhMETLow')
+    outdir = createDir(os.path.join(args.odir, trigger))
+    plot1Dtrigger(turnon_dir, vars1Dtrigger, METType[metType], outdir=outdir)
         
-        plotter.ax.errorbar(bin_centers, values, xerr=None, yerr=errors,
-                             fmt='s', color='black', label=xlabel, **errorbar_kwargs)
-        plotter.ax.stairs(values, bin_edges, color='black', linewidth=2)
-        plotter.ax.text(0.03, 0.97, METType, transform=plotter.ax.transAxes, fontsize=fontsize,
-                        verticalalignment='top', horizontalalignment='left')
-
-        diff_step = 0.05 * abs(max(values)-min(values))
-        plotter.limits(y=(min(values) - diff_step, max(values) + 2*diff_step), logY=False)
-        plotter.labels(x=xlabel, y=ylabel)
-
-        plotter.save( os.path.join(outdir, var) )
-
-    ######################
-    # Plot turn-on curves
-    ######################
-    turnon_dir = f"DQMData/Run 1/HLT/Run summary/JetMET/TurnOnValidation/HLT_PFPuppiMETTypeOne140_PFPuppiMHT140"
-    if not file.Get(dqm_dir):
-        raise RuntimeError(f"Directory '{turnon_dir}' not found in {args.file}")
-
-    var1dNames = ('TurnOngMET', 'TurnOngMETLow', 'TurnOnhMET', 'TurnOnhMETLow')
-
-    for var in var1dNames:
-        plotter = Plotter(args.sample_label)
-        root_hist = CheckRootFile(f"{turnon_dir}/{var}", rebin=None)
-        nbins, bin_edges, bin_centers, bin_widths = define_bins(root_hist)
-        values, errors = histo_values_errors(root_hist)
-
-        xlabel = root_hist.GetXaxis().GetTitle().replace('ET', r'$E_T$ ')
-        ylabel = root_hist.GetYaxis().GetTitle()
-        title = root_hist.GetTitle()
-
-        plotter.ax.errorbar(bin_centers, values, xerr=None, yerr=errors,
-                             fmt='s', color='black', label=xlabel, **errorbar_kwargs)
-        plotter.ax.stairs(values, bin_edges, color='black', linewidth=2)
-        plotter.ax.text(0.03, 0.97, METType, transform=plotter.ax.transAxes, fontsize=fontsize,
-                        verticalalignment='top', horizontalalignment='left')
-
-        diff_step = 0.05 * (max(values)-min(values))
-        plotter.limits(y=(min(values) - diff_step, 1.2*max(values)), logY=False)
-        plotter.labels(x=xlabel, y=ylabel)
-
-        plotter.ax.text(0.97, 0.97, title.replace('ET', r'$E_T$'), transform=plotter.ax.transAxes, fontsize=fontsize,
-                        verticalalignment='top', horizontalalignment='right')
-
-        newvar = var.replace('_', '')
-        plotter.save( os.path.join(outdir, newvar) )
