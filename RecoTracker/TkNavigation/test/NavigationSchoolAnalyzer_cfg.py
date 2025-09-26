cd14369eb55a933aeb6b2fe087d1b27bb2d034d5
Fabio Cossutti <fabio.cossutti@ts.infn.it>
2025-09-26 17:42:41 +0200

    Fix NavigationSchoolAnalyzer cfgs

--- RecoTracker/TkNavigation/test/NavigationSchoolAnalyzer_cfg.py
+++ RecoTracker/TkNavigation/test/NavigationSchoolAnalyzer_cfg.py
@@ -20,6 +20,9 @@ process.load("RecoTracker.TkNavigation.NavigationSchoolESProducer_cff")
 #)
 
 
+#This has to be modified in order to read the tracker + MTD structure
+process.TrackerRecoGeometryESProducer = cms.ESProducer("TrackerRecoGeometryESProducer")
+
 process.maxEvents = cms.untracked.PSet(
     input = cms.untracked.int32(1)
 )
