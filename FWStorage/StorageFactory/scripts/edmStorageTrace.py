87d4978374ee7aa73738d2beff988b9efaac2f73
Matti Kortelainen <matti.kortelainen@cern.ch>
2025-09-30 15:40:59 -0500

    Use raw string to fix a warning about invalid escape sequences

--- FWStorage/StorageFactory/scripts/edmStorageTrace.py
+++ FWStorage/StorageFactory/scripts/edmStorageTrace.py
@@ -334,7 +334,7 @@ def printMapReadRanges(logEntries, mapFileName):
         # Extract the offset (At:...), size (N=...), type, and content
         # (with name and possibly title) of an element in
         # TFile::Map("extended") printout
-        line_re = re.compile("At:(?P<offset>\d+)\s*N=(?P<size>\d+)\s*(?P<type>\w+).*(?P<content>name:.*$)")
+        line_re = re.compile(r"At:(?P<offset>\d+)\s*N=(?P<size>\d+)\s*(?P<type>\w+).*(?P<content>name:.*$)")
         for line in f:
             m = line_re.search(line)
             if m:
