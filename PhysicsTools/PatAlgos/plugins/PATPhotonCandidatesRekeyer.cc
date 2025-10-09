52b2549377d6b25700c83b2694f20cba85d435d1
jean-roch <vlimant@cern.ch>
2025-10-09 13:01:23 +0200

    use range whereever possible

--- PhysicsTools/PatAlgos/plugins/PATPhotonCandidatesRekeyer.cc
+++ PhysicsTools/PatAlgos/plugins/PATPhotonCandidatesRekeyer.cc
@@ -53,9 +53,9 @@ void PATPhotonCandidatesRekeyer::produce(edm::Event &iEvent, edm::EventSetup con
   auto outPtrP = std::make_unique<std::vector<pat::Photon>>();
   outPtrP->reserve(src->size());
 
-  for (size_t i = 0; i < src->size(); ++i) {
+  for (const auto & obj : *src){
     // copy original pat object and append to vector
-    outPtrP->emplace_back((*src)[i]);
+    outPtrP->emplace_back(obj);
 
     std::vector<unsigned int> keys;
     for (const edm::Ref<pat::PackedCandidateCollection> &ref : outPtrP->back().associatedPackedPFCandidates()) {
