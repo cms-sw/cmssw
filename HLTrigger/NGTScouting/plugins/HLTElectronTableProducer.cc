8040d7cacd8ffa626001beeb5c9952d350639098
Marco Musich <marco.musich@cern.ch>
2025-10-09 15:16:05 +0200

    clean-up of plugins in NGTScouting

--- HLTrigger/NGTScouting/plugins/HLTElectronTableProducer.cc
+++ HLTrigger/NGTScouting/plugins/HLTElectronTableProducer.cc
@@ -1,17 +1,7 @@
-#include <memory>
-
-// user include files
-#include "CommonTools/Utils/interface/StringCutObjectSelector.h"
-#include "DataFormats/NanoAOD/interface/FlatTable.h"
-#include "DataFormats/Scouting/interface/Run3ScoutingElectron.h"
-#include "FWCore/Framework/interface/Event.h"
-#include "FWCore/Framework/interface/Frameworkfwd.h"
-#include "FWCore/Framework/interface/MakerMacros.h"
-#include "FWCore/Framework/interface/stream/EDProducer.h"
-#include "FWCore/ParameterSet/interface/ParameterSet.h"
-#include "FWCore/Utilities/interface/StreamID.h"
 #include "PhysicsTools/NanoAOD/interface/SimpleFlatTableProducer.h"
 
+// user include files
+#include "DataFormats/Scouting/interface/Run3ScoutingElectron.h"
 typedef SimpleFlatTableProducer<Run3ScoutingElectron> HLTElectronTableProducer;
 
 #include "FWCore/Framework/interface/MakerMacros.h"
