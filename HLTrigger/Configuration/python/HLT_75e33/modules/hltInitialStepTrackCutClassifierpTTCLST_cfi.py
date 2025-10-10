4f2c71ec6483e154826d8ac181b62b6e93ad20f3
Manos Vourliotis <emmanouil.vourliotis@gmail.com>
2025-10-10 10:56:47 -0700

    Update high purity ID and simplify HLT LST configurations

--- HLTrigger/Configuration/python/HLT_75e33/modules/hltInitialStepTrackCutClassifierpTTCLST_cfi.py
+++
@@ -1,4 +0,0 @@
-import FWCore.ParameterSet.Config as cms
-
-from ..modules.hltInitialStepTrackCutClassifier_cfi import hltInitialStepTrackCutClassifier as _hltInitialStepTrackCutClassifier
-hltInitialStepTrackCutClassifierpTTCLST = _hltInitialStepTrackCutClassifier.clone( src = "hltInitialStepTrackspTTCLST" )
