eae91a8b0751c68e5824e644e2963a1d4cbbaa67
Mario Gonzalez <mario.gonzalez.carpintero@cern.ch>
2025-10-16 15:39:42 +0200

    minor changes

--- TrivialSerialisation/Common/interface/TrivialSerialiserSourceBase.h
+++ TrivialSerialisation/Common/interface/TrivialSerialiserSourceBase.h
@@ -7,7 +7,7 @@
 namespace ngt {
   class TrivialSerialiserSourceBase {
   public:
-    TrivialSerialiserSourceBase() {};
+    TrivialSerialiserSourceBase() = default;
 
     virtual std::unique_ptr<TrivialSerialiserBase> initialize(edm::WrapperBase& wrapper) = 0;
     virtual std::unique_ptr<const TrivialSerialiserBase> initialize(const edm::WrapperBase& wrapper) = 0;
