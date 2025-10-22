9a3ef9ae14f452aa15d3f868b97c63f6b996b1dc
adriano <adriano.di.florio@cern.ch>
2025-10-22 18:56:38 +0200

    Decorating the tests

--- DataFormats/TrackSoA/test/TestReadHostTrackSoA.cc
+++ DataFormats/TrackSoA/test/TestReadHostTrackSoA.cc
@@ -23,16 +23,20 @@ namespace edmtest {
 
   private:
     edm::EDGetTokenT<::reco::TracksHost> getToken_;
+    const unsigned int trackSize_;
   };
 
   TestReadHostTrackSoA::TestReadHostTrackSoA(edm::ParameterSet const& iPSet)
-      : getToken_(consumes(iPSet.getParameter<edm::InputTag>("input"))) {}
+      : getToken_(consumes(iPSet.getParameter<edm::InputTag>("input"))),
+        trackSize_(iPSet.getParameter<unsigned int>("trackSize")) {}
 
   void TestReadHostTrackSoA::analyze(edm::StreamID, edm::Event const& iEvent, edm::EventSetup const&) const {
     auto const& tracks = iEvent.get(getToken_);
     auto tracksView = tracks.view();
 
-    std::cout << "tracksView.metadata().size() = " << tracksView.metadata().size() << std::endl;
+    assert(tracksView.metadata().size() == int(trackSize_));
+    assert(tracksView.nTracks() == int(trackSize_));
+
     for (int i = 0; i < tracksView.metadata().size(); ++i) {
       if (tracksView[i].eta() != float(i)) {
         throw cms::Exception("TestReadHostTrackSoA Failure") << "TestReadHostTrackSoA::analyze, entry. i = " << i;
@@ -43,6 +47,7 @@ namespace edmtest {
   void TestReadHostTrackSoA::fillDescriptions(edm::ConfigurationDescriptions& descriptions) {
     edm::ParameterSetDescription desc;
     desc.add<edm::InputTag>("input");
+    desc.add<unsigned int>("trackSize", 1000);
     descriptions.addDefault(desc);
   }
 }  // namespace edmtest
