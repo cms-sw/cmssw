d6b11f4c685b1649a53b0d77f2d49cf1c0227f99
Andrea Bocci <andrea.bocci@cern.ch>
2025-10-28 10:34:38 +0100

    Explicitly export CUDA kernels in shared libraries
    Mark CUDA kernels in shared libraries as having default visibility, so
    they can be used by other executables, libraries or plugins.
    
    The default linkage of CUDA kernels changed from public to private in
    CUDA 13.0.

--- HeterogeneousTest/CUDAKernel/src/DeviceAdditionKernel.cu
+++ HeterogeneousTest/CUDAKernel/src/DeviceAdditionKernel.cu
@@ -7,17 +7,17 @@
 
 namespace cms::cudatest {
 
-  __global__ void kernel_add_vectors_f(const float* __restrict__ in1,
-                                       const float* __restrict__ in2,
-                                       float* __restrict__ out,
-                                       size_t size) {
+  __global__ __attribute__((visibility("default"))) void kernel_add_vectors_f(const float* __restrict__ in1,
+                                                                              const float* __restrict__ in2,
+                                                                              float* __restrict__ out,
+                                                                              size_t size) {
     add_vectors_f(in1, in2, out, size);
   }
 
-  __global__ void kernel_add_vectors_d(const double* __restrict__ in1,
-                                       const double* __restrict__ in2,
-                                       double* __restrict__ out,
-                                       size_t size) {
+  __global__ __attribute__((visibility("default"))) void kernel_add_vectors_d(const double* __restrict__ in1,
+                                                                              const double* __restrict__ in2,
+                                                                              double* __restrict__ out,
+                                                                              size_t size) {
     add_vectors_d(in1, in2, out, size);
   }
 
