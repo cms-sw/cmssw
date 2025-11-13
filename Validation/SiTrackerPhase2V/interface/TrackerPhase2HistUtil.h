5cdca81e90570bcc42c0afce75080191067a8fc7
Brandi Skipworth <bskipwo1@vols.utk.edu>
2025-11-13 20:23:06 +0100

    added helper functions for booking histograms

--- Validation/SiTrackerPhase2V/interface/TrackerPhase2HistUtil.h
+++ Validation/SiTrackerPhase2V/interface/TrackerPhase2HistUtil.h
@@ -11,11 +11,15 @@
 #define Validation_SiTrackerPhase2V_TrackerPhase2HistUtil_h
 
 #include <cmath>
+#include <string>
+#include <vector>
 
+#include "FWCore/ParameterSet/interface/ParameterSet.h"
 #include "DQMServices/Core/interface/DQMStore.h"
 #include "TH1.h"
 
 namespace phase2tkutil {
+
   inline void makeEfficiencyME(TH1* numerator,
                                TH1* denominator,
                                dqm::legacy::MonitorElement* me,
@@ -29,5 +33,44 @@ namespace phase2tkutil {
     efficiency->GetYaxis()->SetTitle("Efficiency");
   }
 
+  using ME = dqm::legacy::MonitorElement;
+
+  inline ME* book1DFromPS(DQMStore::IBooker& iBooker,
+                          const std::string& name,
+                          const edm::ParameterSet& ps,
+                          const char* xaxis,
+                          const char* yaxis) {
+    auto h = iBooker.book1D(name, name,
+                            ps.getParameter<int32_t>("Nbinsx"),
+                            ps.getParameter<double>("xmin"),
+                            ps.getParameter<double>("xmax"));
+    h->setAxisTitle(xaxis, 1);
+    h->setAxisTitle(yaxis, 2);
+    return h;
+  }
+
+  inline void bookDenNum(DQMStore::IBooker& iBooker,
+                         const std::string& base,
+                         ME*& den, ME*& num,
+                         const edm::ParameterSet& ps,
+                         const char* xaxis,
+                         const char* denY = "# tracking particles",
+                         const char* numY = "# matched tracking particles",
+                         const std::string& denPrefix = "tp_",
+                         const std::string& numPrefix = "match_tp_") {
+    den = book1DFromPS(iBooker, denPrefix + base, ps, xaxis, denY);
+    num = book1DFromPS(iBooker, numPrefix + base, ps, xaxis, numY);
+  }
+
+  inline void bookIntoVec(DQMStore::IBooker& iBooker,
+                          std::vector<ME*>& vec,
+                          int i,
+                          const std::string& name,
+                          const edm::ParameterSet& ps,
+                          const char* xaxis,
+                          const char* yaxis = "# tracking particles") {
+    if ((int)vec.size() <= i) vec.resize(i + 1, nullptr);
+    vec[i] = book1DFromPS(iBooker, name, ps, xaxis, yaxis);
+  }
 }  // namespace phase2tkutil
 #endif
