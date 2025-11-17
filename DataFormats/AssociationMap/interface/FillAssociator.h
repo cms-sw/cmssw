51cfa2b8e50d4b8880d88c15b9a31824291ef2c8
Simone <simone.balducci00@gmail.com>
2025-11-17 19:52:15 +0100

    Implement heterogeneous Association Map

---
+++ DataFormats/AssociationMap/interface/FillAssociator.h
@@ -0,0 +1,25 @@
+
+#pragma once
+
+#include "DataFormats/AssociationMap/interface/AssociationMap.h"
+#include "DataFormats/AssociationMap/interface/detail/FillAssociator.h"
+#include <alpaka/alpaka.hpp>
+#include <span>
+
+namespace ALPAKA_ACCELERATOR_NAMESPACE {
+  namespace associator {
+
+    template <typename TAcc,
+              typename TQueue,
+              ticl::concepts::trivially_copyable TMapped = uint32_t,
+              std::integral TKey = uint32_t>
+      requires alpaka::isQueue<TQueue> && alpaka::isAccelerator<TAcc>
+    ALPAKA_FN_HOST auto fill_associator(TQueue& queue,
+                                        ticl::AssociationMapView<TMapped, TKey>& map,
+                                        std::span<const TKey> keys,
+                                        std::span<const TMapped> values) {
+      detail::fill_associator<TAcc>(queue, map, keys, values);
+    }
+
+  }  // namespace associator
+}  // namespace ALPAKA_ACCELERATOR_NAMESPACE
