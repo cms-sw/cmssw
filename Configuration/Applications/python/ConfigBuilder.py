764dc62a20e21ef54fc56bf74eabd65a63f899ba
Silvio Donato <silvio.donato@cern.ch>
2025-11-17 10:20:09 +0100

    use PathStatus in genstepfilter

--- Configuration/Applications/python/ConfigBuilder.py
+++ Configuration/Applications/python/ConfigBuilder.py
@@ -1524,7 +1524,11 @@ class ConfigBuilder(object):
         self.schedule.append(self.process.generation_step)
 
         #register to the genstepfilter the name of the path (static right now, but might evolve)
-        self.executeAndRemember('process.genstepfilter.triggerConditions=cms.vstring("generation_step")')
+        self.executeAndRemember(
+'''process.genstepfilter.triggerConditions=cms.vstring("generation_step")
+process.genstepfilter.usePathStatus = True
+process.genstepfilter.hltResults = ""
+''')
 
         if 'reGEN' in self.stepMap or stepSpec == 'pgen_smear':
             #stop here
