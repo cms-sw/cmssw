af6ea3568e08cd39e3e45edc4c40eb82a5353862
Alexander Albert <aa41ma@gmail.com>
2025-11-19 14:32:49 -0600

    update Run3_2025 flag + import

--- Configuration/Eras/python/Era_Run3_2025_cff.py
+++ Configuration/Eras/python/Era_Run3_2025_cff.py
@@ -2,7 +2,7 @@ import FWCore.ParameterSet.Config as cms
 
 from Configuration.Eras.Era_Run3_2024_cff import Run3_2024
 from Configuration.Eras.Modifier_run3_GEM_2025_cff import run3_GEM_2025
-from Configuration.Eras.Modifier_run3_CSC_2025_cff import run3_CSC_2025_AtoE
+from Configuration.Eras.Modifier_run3_CSC_2025_cff import run3_CSC_2025_FtoG
 from Configuration.Eras.Modifier_stage2L1Trigger_2025_cff import stage2L1Trigger_2025
 from Configuration.Eras.Modifier_run3_SiPixel_2025_cff import run3_SiPixel_2025
 from Configuration.Eras.Modifier_run3_nanoAOD_2025_cff import run3_nanoAOD_2025
