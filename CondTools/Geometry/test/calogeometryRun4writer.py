783293f01d46343180c80d447c7c68b839320a60
Kevin Pedro <kpedro88@gmail.com>
2025-11-21 11:43:16 -0600

    update CondTools/Geometry to use D121

--- CondTools/Geometry/test/calogeometryRun4writer.py
+++ CondTools/Geometry/test/calogeometryRun4writer.py
@@ -3,7 +3,7 @@ import FWCore.ParameterSet.Config as cms
 from Configuration.Eras.Era_Phase2C17I13M9_cff import Phase2C17I13M9
 process = cms.Process("CaloGeometryWriter",Phase2C17I13M9)
 process.load('CondCore.CondDB.CondDB_cfi')
-process.load('Configuration.Geometry.GeometryExtendedRun4D110_cff')
+process.load('Configuration.Geometry.GeometryExtendedRun4D121_cff')
 process.load('Geometry.CaloEventSetup.CaloGeometryRun4DBWriter_cfi')
 process.load('CondTools.Geometry.HcalParametersWriter_cff')
 
