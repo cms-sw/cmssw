314b7b73f7c6ff2e960f79062f92661fe5e3e62c
Mario Gonzalez <mario.gonzalez.carpintero@cern.ch>
2025-11-24 19:13:31 +0100

    Implement serialisation plugins for SoA products
    Add a test for the serialisation using the GenericCloner.
    
    Co-authored-by: Andrea Bocci <andrea.bocci@cern.ch>

--- DataFormats/ParticleFlowReco/plugins/TrivialSerialisation.cc
+++ DataFormats/ParticleFlowReco/plugins/TrivialSerialisation.cc
@@ -1,12 +1,13 @@
 // Include the Eigen core library before including the SoA definitions
 #include <Eigen/Core>
 
-#include "DataFormats/Common/interface/Wrapper.h"
 #include "DataFormats/ParticleFlowReco/interface/CaloRecHitHostCollection.h"
-#include "DataFormats/ParticleFlowReco/interface/CaloRecHitSoA.h"
 #include "DataFormats/ParticleFlowReco/interface/PFClusterHostCollection.h"
-#include "DataFormats/ParticleFlowReco/interface/PFClusterSoA.h"
 #include "DataFormats/ParticleFlowReco/interface/PFRecHitFractionHostCollection.h"
-#include "DataFormats/ParticleFlowReco/interface/PFRecHitFractionSoA.h"
 #include "DataFormats/ParticleFlowReco/interface/PFRecHitHostCollection.h"
-#include "DataFormats/ParticleFlowReco/interface/PFRecHitSoA.h"
+#include "HeterogeneousCore/TrivialSerialisation/interface/SerialiserFactory.h"
+
+DEFINE_TRIVIAL_SERIALISER_PLUGIN(reco::CaloRecHitHostCollection);
+DEFINE_TRIVIAL_SERIALISER_PLUGIN(reco::PFClusterHostCollection);
+DEFINE_TRIVIAL_SERIALISER_PLUGIN(reco::PFRecHitFractionHostCollection);
+DEFINE_TRIVIAL_SERIALISER_PLUGIN(reco::PFRecHitHostCollection);
