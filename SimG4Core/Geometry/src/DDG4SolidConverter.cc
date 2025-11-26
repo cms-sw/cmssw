9591585e34fc2e798e8d9291c695078ee4468216
Shahzad Malik Muzaffar <shahzad.malik.muzaffar@cern.ch>
2025-11-26 15:10:08 +0100

    [SIMULATION] [LLVM21] Apply clang-tidy changes

--- SimG4Core/Geometry/src/DDG4SolidConverter.cc
+++ SimG4Core/Geometry/src/DDG4SolidConverter.cc
@@ -249,8 +249,10 @@ G4VSolid *DDG4SolidConverter::extrudedpolygon(const DDSolid &solid) {
 
   std::vector<G4TwoVector> polygon;
   std::vector<G4ExtrudedSolid::ZSection> zsections;
+  polygon.reserve(x.size());
   for (unsigned int it = 0; it < x.size(); ++it)
     polygon.emplace_back(x[it], y[it]);
+  zsections.reserve(z.size());
   for (unsigned int it = 0; it < z.size(); ++it)
     zsections.emplace_back(z[it], G4TwoVector(zx[it], zy[it]), zs[it]);
   return new G4ExtrudedSolid(solid.name().name(), polygon, zsections);
