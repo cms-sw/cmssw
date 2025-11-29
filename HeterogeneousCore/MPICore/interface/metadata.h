bb50fbd64f96bea589bdac6966b15393cc051ea1
Anna Polova <anna.polova@cern.ch>
2025-11-29 10:51:40 +0100

    Extend CMSSW to a distributed application over MPI
    Let multiple CMSSW processes on the same or different machines coordinate event
    processing and transfer data products over MPI.
    
    The implementation is based on four CMSSW modules. Two are responsible for
    setting up the communication channels and coordinate the event processing:
      - a "remote controller" called MPIController;
      - a "remote source" called MPISource;
    and two are responsible for the transfer of data products:
      - a "sender" called MPISender;
      - a "receiver" called MPIReceiver.
    
    Data products can be serialised and transferred using the trivial serialisation
    from HeterogeneousCore/TrivialSerialisation - if available - or the ROOT-based
    serialisation.
    
    Various tests are used to validate the implementation: matching the local and
    remote event id, transferring SoA products with trivial serialisation, and
    transferring legacy product with ROOT serialisation.
    
    Co-authored-by: Andrea Bocci <andrea.bocci@cern.ch>
    Co-authored-by: Anna Polova <anna.polova@cern.ch>
    Co-authored-by: Mario Gonzalez <mario.gonzalez.carpintero@cern.ch>

---
+++ HeterogeneousCore/MPICore/interface/metadata.h
@@ -0,0 +1,114 @@
+#ifndef HeterogeneousCore_MPICore_interface_metadata_h
+#define HeterogeneousCore_MPICore_interface_metadata_h
+
+// C++ standard library headers
+#include <cassert>
+#include <cstdint>
+#include <cstdlib>
+#include <cstring>
+#include <iostream>
+#include <span>
+#include <stdexcept>
+#include <variant>
+#include <vector>
+
+// MPI headers
+#include <mpi.h>
+
+enum ProductFlags : uint8_t {
+  HasMissing = 1 << 0,
+  HasSerialized = 1 << 1,
+  HasTrivialCopy = 1 << 2,
+};
+
+struct ProductMetadata {
+  enum class Kind : uint8_t { Missing = 0, Serialized = 1, TrivialCopy = 2 };
+
+  Kind kind;
+  size_t sizeMeta = 0;
+  const uint8_t* trivialCopyOffset = nullptr;  // Only valid if kind == TrivialCopy
+};
+
+class ProductMetadataBuilder {
+public:
+  ProductMetadataBuilder();
+  explicit ProductMetadataBuilder(size_t productCount);
+  ~ProductMetadataBuilder();
+
+  // No copy
+  ProductMetadataBuilder(const ProductMetadataBuilder&) = delete;
+  ProductMetadataBuilder& operator=(const ProductMetadataBuilder&) = delete;
+
+  // Move
+  ProductMetadataBuilder(ProductMetadataBuilder&& other) noexcept;
+  ProductMetadataBuilder& operator=(ProductMetadataBuilder&& other) noexcept;
+
+  // Sender-side: pre-allocate
+  void reserve(size_t bytes);
+
+  // set or reset number of products. will fail if not set called before sending
+  void setProductCount(size_t prod_num) { productCount_ = prod_num; }
+  void setHeader();
+
+  // Sender API
+  void addMissing();
+  void addSerialized(size_t size);
+  void addTrivialCopy(const std::byte* buffer, size_t size);
+
+  const uint8_t* data() const;
+  uint8_t* data();
+  size_t size() const;
+  std::span<const uint8_t> buffer() const;
+
+  // Receiver-side
+  void receiveMetadata(int src, int tag, MPI_Comm comm);
+
+  // Not memory safe for trivial copy products.
+  // Please make sure that ProductMetadataBuilder lives longer than returned ProductMetadata
+  ProductMetadata getNext();
+
+  int64_t productCount() const { return productCount_; }
+  int64_t serializedBufferSize() const { return serializedBufferSize_; }
+  bool hasMissing() const { return productFlags_ & HasMissing; }
+  bool hasSerialized() const { return productFlags_ & HasSerialized; }
+  bool hasTrivialCopy() const { return productFlags_ & HasTrivialCopy; }
+
+  void debugPrintMetadataSummary() const;
+
+private:
+  uint8_t* buffer_;
+  size_t capacity_;
+  size_t size_;
+  size_t readOffset_;
+  const size_t headerSize_ = 13;  // header is always present in the metadata object do describe products in general
+  const size_t maxMetadataSize_ = 1024;  // default size for buffer initialization. must fit any metadata
+  int serializedBufferSize_ = 0;
+  uint8_t productFlags_ = 0;
+  int64_t productCount_ = 0;
+
+  void resizeBuffer(size_t newCap);
+  void ensureCapacity(size_t needed);
+
+  void appendBytes(const std::byte* src, size_t size);
+
+  template <typename T>
+  void append(T value) {
+    static_assert(std::is_trivially_copyable_v<T>);
+    ensureCapacity(sizeof(T));
+    std::memcpy(buffer_ + size_, &value, sizeof(T));
+    size_ += sizeof(T);
+  }
+
+  template <typename T>
+  T consume() {
+    static_assert(std::is_trivially_copyable_v<T>);
+    if (readOffset_ + sizeof(T) > size_)
+      throw std::runtime_error("Buffer underflow");
+    T val;
+    std::memcpy(&val, buffer_ + readOffset_, sizeof(T));
+    readOffset_ += sizeof(T);
+    return val;
+  }
+};
+
+#endif  // HeterogeneousCore_MPICore_interface_metadata_h
