7720fbe89f4b7257baa95a4f1bb058b448dfb2d6
Igor <i.volobouev@ttu.edu>
2025-12-05 12:01:58 +0100

    fix parameter type

--- RecoLocalCalo/HcalRecAlgos/python/hcalPulseShapesESProd_cfi.py
+++ RecoLocalCalo/HcalRecAlgos/python/hcalPulseShapesESProd_cfi.py
@@ -3,7 +3,7 @@ import FWCore.ParameterSet.Config as cms
 hcalPulseShapesESProd = cms.ESProducer('HcalPulseShapesEP',
     productLabel = cms.string('HcalDataShapes'),
     pulseShapeLength = cms.uint32(250),
-    globalTimeShift = cms.int32(0),
+    globalTimeShift = cms.double(0.0),
     pulseDumpFile = cms.untracked.string(''),
     dumpPrecision = cms.untracked.uint32(0)
 )
