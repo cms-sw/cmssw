e30eeb08d04791a398d94e0c15f6c78afbfc33da
Bruno Alves <balves16@hotmail.com>
2025-12-09 10:21:10 +0100

    Fix CloseBy configuration.

--- Configuration/Generator/python/CloseByPGun_Barrel_Front_cfi.py
+++ Configuration/Generator/python/CloseByPGun_Barrel_Front_cfi.py
@@ -3,11 +3,11 @@ from .CE_E_Front_300um_cfi import generator
 
 _pgunPSet = generator.PGunParameters
 
-_pgunPSet.ControlledByEta = cms.bool(True)
+_pgunPSet.ControlledByREta = cms.bool(True)
 _pgunPSet.MinEta = cms.double(-1.479)
 _pgunPSet.MaxEta = cms.double(1.479)
-_pgunPSet.RMin = cms.double(128.5)
-_pgunPSet.RMax = cms.double(128.4)
+_pgunPSet.RMin = cms.double(128.4)
+_pgunPSet.RMax = cms.double(128.5)
 _pgunPSet.ZMin = cms.double(-230)
 _pgunPSet.ZMax = cms.double(230)
 
