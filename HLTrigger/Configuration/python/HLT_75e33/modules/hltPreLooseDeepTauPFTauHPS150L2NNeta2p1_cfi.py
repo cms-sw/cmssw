cfc92f414de30a562d6c3b35743daeb726588fb9
chihwan an <an.chihwan@cern.ch>
2025-12-11 15:46:55 +0900

    v1 for tautrigger,bug fixed

---
+++ HLTrigger/Configuration/python/HLT_75e33/modules/hltPreLooseDeepTauPFTauHPS150L2NNeta2p1_cfi.py
@@ -0,0 +1,6 @@
+import FWCore.ParameterSet.Config as cms
+
+hltPreLooseDeepTauPFTauHPS150L2NNeta2p1 = cms.EDFilter("HLTPrescaler",
+    L1GtReadoutRecordTag = cms.InputTag("hltGtStage2Digis"),
+    offset = cms.uint32(0)
+)
