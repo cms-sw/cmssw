e9fddec315f56954db6edd3860bec1a2a4f5c6ec
Andre Govinda Stahl Leiton <andre.govinda.stahl.leiton@cern.ch>
2025-12-20 17:58:35 +0100

    Fix use of UPC era modifiers

--- Configuration/Eras/python/Era_Run3_2023_UPC_cff.py
+++ Configuration/Eras/python/Era_Run3_2023_UPC_cff.py
@@ -4,5 +4,6 @@ from Configuration.Eras.Era_Run3_2023_cff import Run3_2023
 from Configuration.ProcessModifiers.egamma_lowPt_exclusive_cff import egamma_lowPt_exclusive
 from Configuration.Eras.Modifier_highBetaStar_cff import highBetaStar
 from Configuration.Eras.Modifier_run3_upc_cff import run3_upc
+from Configuration.Eras.Modifier_run3_upc_2023_cff import run3_upc_2023
 
-Run3_2023_UPC = cms.ModifierChain(Run3_2023, egamma_lowPt_exclusive, highBetaStar, run3_upc)
+Run3_2023_UPC = cms.ModifierChain(Run3_2023, egamma_lowPt_exclusive, highBetaStar, run3_upc, run3_upc_2023)
