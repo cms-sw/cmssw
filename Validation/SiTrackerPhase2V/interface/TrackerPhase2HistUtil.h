0f11897938e058fc73c84a12809354eeeb03f183
Brandi Skipworth <bskipwo1@vols.utk.edu>
2026-01-06 21:40:41 +0100

    add helper function for resolution hists

--- Validation/SiTrackerPhase2V/interface/TrackerPhase2HistUtil.h
+++ Validation/SiTrackerPhase2V/interface/TrackerPhase2HistUtil.h
@@ -13,10 +13,12 @@
 #include <cmath>
 #include <string>
 #include <vector>
+#include <algorithm>
 
 #include "DQMServices/Core/interface/DQMStore.h"
 #include "DQMServices/Core/interface/MonitorElement.h"
 #include "FWCore/ParameterSet/interface/ParameterSet.h"
+#include "FWCore/MessageLogger/interface/MessageLogger.h"
 #include "TH1.h"
 
 namespace phase2tkutil {
@@ -32,6 +34,30 @@ namespace phase2tkutil {
     efficiency->SetStats(false);
     efficiency->GetXaxis()->SetTitle(xAxisTitle.c_str());
     efficiency->GetYaxis()->SetTitle("Efficiency");
+  }
+
+  inline void fillResolutionFromVec(const std::vector<dqm::legacy::MonitorElement*>& srcVec,
+                                    dqm::legacy::MonitorElement* destME,
+                                    const std::string& yAxisTitle) {
+    if (!destME) return;
+    
+    // Check if any source elements are missing
+    if (std::find(srcVec.begin(), srcVec.end(), nullptr) != srcVec.end()) {
+        edm::LogWarning("TrackerPhase2HistUtil") << "Missing source ME for resolution: " << destME->getName();
+        return;
+    }
+
+    TH1* hDest = destME->getTH1();
+    hDest->SetMinimum(0.0);
+    hDest->SetStats(false);
+    hDest->GetYaxis()->SetTitle(yAxisTitle.c_str());
+
+    for (size_t i = 0; i < srcVec.size(); ++i) {
+        TH1* hSrc = srcVec[i]->getTH1();
+        // Bin 1 in destination corresponds to index 0 in vector
+        hDest->SetBinContent(i + 1, hSrc->GetStdDev());
+        hDest->SetBinError(i + 1, hSrc->GetStdDevError());
+    }
   }
 
   inline dqm::legacy::MonitorElement* book1DFromPS(
