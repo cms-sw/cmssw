686d49387a09e12105b9b3e2add9f7a2ce0cd3f2
Bruno Alves <balves16@hotmail.com>
2026-01-14 11:39:27 +0100

    Unit test for Jet validation.

---
+++ Validation/RecoJets/test/test_makeJetValidationPlots.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+function die { echo $1: status $2; exit $2; }
+LOCAL_TEST_DIR="$PWD"
+REMOTE="/store/group/phys_jetmet/cmssw_unittests/"
+REDIRECTOR="root://eoscms.cern.ch/" # root://cms-xrd-global.cern.ch
+DQMFILE="DQM_RelValQCD_FlatPt_15_3000HS_14_CMSSW_16_0_0_pre4.root"
+OUTDIR="."
+SCRIPT="makeJetValidationPlots.py"
+
+COMMMAND=`xrdfs ${REDIRECTOR} locate ${REMOTE}${DQMFILE}`
+STATUS=$?
+echo "xrdfs command status = "$STATUS
+
+if [ $STATUS -eq 0 ]; then
+    printf "Using file ${DQMFILE}.\nRunning in ${LOCAL_TEST_DIR}.\n"
+    xrdcp ${REDIRECTOR}/${REMOTE}${DQMFILE} .
+    (${SCRIPT} --file ./${DQMFILE} --odir ./${OUTDIR} --jet hltAK4PFPuppiJets) || die 'failed running ${SCRIPT} ${DQMFILE}' $?
+    rm -fr ./${DQMFILE}
+else 
+	die "SKIPPING test, file ${DQMFILE} not found" 0
+fi
