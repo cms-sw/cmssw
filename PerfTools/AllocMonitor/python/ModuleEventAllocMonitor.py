5345c62685881f4278807c523d52175ea55e3f2a
Patrick Gartung <gartung@fnal.gov>
2026-01-15 19:55:53 +0100

    PerfTools/AllocMonitor: Add customize function for EventAllocMonitor that defines the log

---
+++ PerfTools/AllocMonitor/python/ModuleEventAllocMonitor.py
@@ -0,0 +1,6 @@
+import FWCore.ParameterSet.Config as cms
+def customise(process):
+    process.ModuleEventAllocMonitor = cms.Service("ModuleEventAllocMonitor",
+                                   fileName=cms.untracked.string("moduleEventAllocMonitor.log")
+                                   )
+    return(process)
