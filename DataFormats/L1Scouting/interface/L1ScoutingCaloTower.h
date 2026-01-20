035eb2d51a01620d2ad67837be1f8dda6f8c9e78
Thomas Owen James <tom.james@cern.ch>
2026-01-20 17:58:44 +0100

    replaced all uint16_t with int16_t in L1ScoutingCaloTower

--- DataFormats/L1Scouting/interface/L1ScoutingCaloTower.h
+++ DataFormats/L1Scouting/interface/L1ScoutingCaloTower.h
@@ -12,24 +12,24 @@ namespace l1ScoutingRun3 {
     CaloTower(int16_t hwEt, int16_t erBits, int16_t miscBits, int16_t hwEta, int16_t hwPhi)
         : hwEt_(hwEt), erBits_(erBits), miscBits_(miscBits), hwEta_(hwEta), hwPhi_(hwPhi) {}
 
-    void setHwEt(uint16_t hwEt) { hwEt_ = hwEt; }
-    void setErBits(uint16_t erBits) { erBits_ = erBits; }
-    void setMiscBits(uint16_t miscBits) { miscBits_ = miscBits; }
-    void setHwEta(uint16_t hwEta) { hwEta_ = hwEta; }
-    void setHwPhi(uint16_t hwPhi) { hwPhi_ = hwPhi; }
+    void setHwEt(int16_t hwEt) { hwEt_ = hwEt; }
+    void setErBits(int16_t erBits) { erBits_ = erBits; }
+    void setMiscBits(int16_t miscBits) { miscBits_ = miscBits; }
+    void setHwEta(int16_t hwEta) { hwEta_ = hwEta; }
+    void setHwPhi(int16_t hwPhi) { hwPhi_ = hwPhi; }
 
-    uint16_t hwEt() const { return hwEt_; }
-    uint16_t erBits() const { return erBits_; }
-    uint16_t miscBits() const { return miscBits_; }
-    uint16_t hwEta() const { return hwEta_; }
-    uint16_t hwPhi() const { return hwPhi_; }
+    int16_t hwEt() const { return hwEt_; }
+    int16_t erBits() const { return erBits_; }
+    int16_t miscBits() const { return miscBits_; }
+    int16_t hwEta() const { return hwEta_; }
+    int16_t hwPhi() const { return hwPhi_; }
 
   private:
-    uint16_t hwEt_;
-    uint16_t erBits_;
-    uint16_t miscBits_;
-    uint16_t hwEta_;
-    uint16_t hwPhi_;
+    int16_t hwEt_;
+    int16_t erBits_;
+    int16_t miscBits_;
+    int16_t hwEta_;
+    int16_t hwPhi_;
   };
 
   typedef OrbitCollection<CaloTower> CaloTowerOrbitCollection;
