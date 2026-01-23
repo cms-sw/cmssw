e7dd95a53dae3d418b9c44fe0c8c5ceb39edb309
Mario Gonzalez <mario.gonzalez.carpintero@cern.ch>
2026-01-23 18:04:48 +0100

    Improved MPI tests

--- HeterogeneousCore/MPICore/test/MPITestWorker.h
+++ HeterogeneousCore/MPICore/test/MPITestWorker.h
@@ -1,28 +1,22 @@
-//initializing the libraries needed
-//WORKER//
-#include <iostream>
-#include <iomanip>
-#include <cstdlib>
-#include <string>
-#include <algorithm>
-#include <vector>
-#include <random>
+// MPI Test Worker process implementation (runs on rank != 0)
 #include <utility>
+#include <vector>
+
 #include <mpi.h>
-#include <unistd.h>
-#include <cmath>  // for abs() from <cmath>
-#include "processInterface.h"
+
+#include "MPITestBase.h"
 
 class MPI_Worker : public MPI_TEST {
 public:
   MPI_Worker() {
     MPI_Comm_size(MPI_COMM_WORLD, &size_);
     MPI_Comm_rank(MPI_COMM_WORLD, &rank_);
-    //assert(rank_ != 0  && size_ >= 2);
+    // assert(rank_ != 0 && size_ >= 2);
   }
 
+  // Beginning of blockingSend (Worker)
   std::pair<float, float> blockingSend() override {
-    //ROOT RANK IS ALWAYS ZERO
+    // Root rank is always zero
 
     MPI_Status status;
     MPI_Probe(0, 0, MPI_COMM_WORLD, &status);
@@ -35,14 +29,15 @@ public:
     MPI_Recv(&v1[0], messageSize, MPI_FLOAT, 0, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
     MPI_Recv(&v2[0], messageSize, MPI_FLOAT, 0, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
 
-    for (int i = 0; i < v1.size(); i++) {
+    for (size_t i = 0; i < v1.size(); i++) {
       v1[i] += v2[i];
     }
 
     MPI_Send(&v1[0], v1.size(), MPI_FLOAT, 0, 0, MPI_COMM_WORLD);
-  }
+    return std::pair<float, float>(0.0f, 0.0f);
+  }  // End of blockingSend
 
-  //beginning of nonBlockingSend
+  // Beginning of nonBlockingSend (Worker)
   std::pair<float, float> nonBlockingSend() override {
     MPI_Request requestSend;
     MPI_Request requestRecv[1];
@@ -65,10 +60,10 @@ public:
     }
 
     MPI_Issend(&v1[0], v1.size(), MPI_FLOAT, 0, 0, MPI_COMM_WORLD, &requestSend);
-  }
-  //end of nonBlockingSend
+    return std::pair<float, float>(0.0f, 0.0f);
+  }  // End of nonBlockingSend
 
-  //beginning of blockingScatter
+  // Beginning of blockingScatter (Worker)
   std::pair<float, float> blockingScatter() override {
     MPI_Recv(&messageSize, 1, MPI_INT, 0, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
     // Resize vectors
@@ -84,10 +79,10 @@ public:
     }
 
     MPI_Gatherv(&v1[0], v1.size(), MPI_FLOAT, NULL, NULL, NULL, MPI_FLOAT, 0, MPI_COMM_WORLD);
-  }
-  //end of blockingScatter
+    return std::pair<float, float>(0.0f, 0.0f);
+  }  // End of blockingScatter
 
-  //begging of nonBlockingScatter
+  // Beginning of nonBlockingScatter (Worker)
   std::pair<float, float> nonBlockingScatter() override {
     MPI_Recv(&messageSize, 1, MPI_INT, 0, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
 
@@ -109,12 +104,12 @@ public:
     }
 
     MPI_Igatherv(&v1[0], v1.size(), MPI_FLOAT, NULL, NULL, NULL, MPI_FLOAT, 0, MPI_COMM_WORLD, &requestGather);
-  }
-  //end of nonBlockingScatter
+    return std::pair<float, float>(0.0f, 0.0f);
+  }  // End of nonBlockingScatter
 
-  //beginning of blockingSendRecv WORKER
+  // Beginning of blockingSendRecv (Worker)
   std::pair<float, float> blockingSendRecv() override {
-    //ROOT RANK IS ALWAYS ZERO
+    // Root rank is always zero
 
     MPI_Status status;
     MPI_Probe(0, 0, MPI_COMM_WORLD, &status);
@@ -124,7 +119,7 @@ public:
     v1.resize(messageSize);
     v2.resize(messageSize);
 
-    //receiving v1
+    // Receiving v1
     MPI_Sendrecv(
         /*sending buf*/ NULL,
         /*send count*/ 0,
@@ -140,7 +135,7 @@ public:
         /*Comm.*/ MPI_COMM_WORLD,
         /*status*/ MPI_STATUS_IGNORE);
 
-    //receiving v2
+    // Receiving v2
     MPI_Sendrecv(
         /*sending buf*/ NULL,
         /*send count*/ 0,
@@ -158,12 +153,12 @@ public:
 
     );
 
-    //adding the two vectors and the result is in v1
-    for (int i = 0; i < v1.size(); i++) {
+    // Adding the two vectors and the result is in v1
+    for (size_t i = 0; i < v1.size(); i++) {
       v1[i] += v2[i];
     }
 
-    //sending back the result of the summation
+    // Sending back the result of the summation
     MPI_Sendrecv(
         /*sending buf*/ &v1[0],
         /*send count*/ v1.size(),
@@ -180,56 +175,59 @@ public:
         /*status*/ MPI_STATUS_IGNORE
 
     );
-  }
-  //end of blockingSend
+    return std::pair<float, float>(0.0f, 0.0f);
+  }  // End of blockingSendRecv
 
-  //beggining of one sided communincation
-  std::pair<float, float> oneSidedCommMaster() override {  //WORKER
-    //ROOT RANK IS ALWAYS ZERO
+  // Beginning of oneSidedCommMaster (Worker side)
+  std::pair<float, float> oneSidedCommMaster() override {
+    // Root rank is always zero
     MPI_Win win1, win2;
     int vec_size;
-    //receiving the size of vectors
+    // Receiving the size of vectors
     MPI_Recv(&vec_size, 1, MPI_INT, 0, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
 
     v1.resize(vec_size);
     v2.resize(vec_size);
 
-    //widow buffer
-    /////////////////////////// creating window //////////////////////////
+    // Window buffer
+    /////////////////////////// Creating window //////////////////////////
     MPI_Win_create(&v1[0], vec_size * sizeof(float), sizeof(float), MPI_INFO_NULL, MPI_COMM_WORLD, &win1);
     //fence 1
     MPI_Win_fence(0, win1);
 
     MPI_Win_create(&v2[0], vec_size * sizeof(float), sizeof(float), MPI_INFO_NULL, MPI_COMM_WORLD, &win2);
-    //fence 2
+    // Fence 2
     MPI_Win_fence(0, win2);
-    ////////////////////////// end of creating the window ///////////////
-    //////////////////////// getting the vector ////////////////
-    //fence 3
-    MPI_Win_fence(0, win1);  //put the double vector(v1 & v2) from root
-    //fence 4
-    MPI_Win_fence(0, win2);  //put the double vector(v1 & v2) from root
+    ////////////////////////// End of creating the window ///////////////
+    //////////////////////// Getting the vector ////////////////
+    // Fence 3
+    MPI_Win_fence(0, win1);  // Put the double vector (v1 & v2) from root
+    // Fence 4
+    MPI_Win_fence(0, win2);  // Put the double vector (v1 & v2) from root
 
-    //////////////// calculating the summation of 2 vectors //////////////////
+    //////////////// Calculating the summation of 2 vectors //////////////////
 
     for (int i = 0; i < vec_size; i++) {
       v1[i] += v2[i];
     }
-    /////////////////////// end of calculation ///////////////////////
-    //fence 5
-    MPI_Win_fence(0, win1);  //get from root: root is getting the data from workers window
+    /////////////////////// End of calculation ///////////////////////
+    // Fence 5
+    MPI_Win_fence(0, win1);  // Get from root: root is getting the data from worker's window
 
-    //fence 6
-    MPI_Win_fence(0, win1);  //get from root: root is getting the data from workers window
+    // Fence 6
+    MPI_Win_fence(0, win1);  // Get from root: root is getting the data from worker's window
 
-    //free the window
+    // Free the window
     MPI_Win_free(&win1);
     MPI_Win_free(&win2);
-  }
+    return std::pair<float, float>(0.0f, 0.0f);
+  }  // End of oneSidedCommMaster
+
+  // Beginning of oneSidedCommWorker (Worker side)
   std::pair<float, float> oneSidedCommWorker() override {
-    //ROOT RANK IS ALWAYS ZERO
+    // Root rank is always zero
     int vec_size;
-    //receiving the size of vectors
+    // Receiving the size of vectors
     MPI_Recv(&vec_size, 1, MPI_INT, 0, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
 
     int MPI_size;
@@ -240,57 +238,58 @@ public:
 
     int batchSize = vec_size / (MPI_size - 1) + (rem >= MPI_rank ? 1 : 0);
 
-    //widow buffer
+    // Window buffer
     std::vector<float> v1, v2;
     v1.resize(batchSize);
     v2.resize(batchSize);
 
-    /////////////////////////// creating window //////////////////////////
+    /////////////////////////// Creating window //////////////////////////
     MPI_Win win1;
     MPI_Win_create(&v1[0], batchSize * sizeof(float), sizeof(float), MPI_INFO_NULL, MPI_COMM_WORLD, &win1);
-    //fence 1
+    // Fence 1
     MPI_Win_fence(0, win1);
 
     MPI_Win win2;
     MPI_Win_create(&v2[0], batchSize * sizeof(float), sizeof(float), MPI_INFO_NULL, MPI_COMM_WORLD, &win2);
-    //fence 2
+    // Fence 2
     MPI_Win_fence(0, win2);
-    ////////////////////////// end of creating the window ///////////////
+    ////////////////////////// End of creating the window ///////////////
 
     int dp = batchSize * (MPI_rank - 1) + (rem >= MPI_rank ? 0 : rem);
 
-    ///////////////////////// getting the vector ////////////////
-    //getting v1 from root
+    ///////////////////////// Getting the vector ////////////////
+    // Getting v1 from root
     MPI_Get(&v1[0], batchSize, MPI_FLOAT, 0, dp, batchSize, MPI_FLOAT, win1);
-    //fence 3
-    MPI_Win_fence(0, win1);  //put the double vector(v1 & v2) from root
+    // Fence 3
+    MPI_Win_fence(0, win1);  // Put the double vector (v1 & v2) from root
 
-    //getting v2 from root
+    // Getting v2 from root
     MPI_Get(&v2[0], batchSize, MPI_FLOAT, 0, dp, batchSize, MPI_FLOAT, win2);
-    //fence 4
-    MPI_Win_fence(0, win2);  //put the double vector(v1 & v2) from root
+    // Fence 4
+    MPI_Win_fence(0, win2);  // Put the double vector (v1 & v2) from root
 
-    //////////////// calculating the summation of 2 vectors //////////////////
+    //////////////// Calculating the summation of 2 vectors //////////////////
     for (int i = 0; i < batchSize; i++) {
       v1[i] += v2[i];
     }
-    //fence 5
+    // Fence 5
     MPI_Win_fence(0, win1);
 
-    /////////////////////// end of calculation ///////////////////////
+    /////////////////////// End of calculation ///////////////////////
     MPI_Put(&v1[0], batchSize, MPI_FLOAT, 0, dp, batchSize, MPI_FLOAT, win1);
-    //fence 6
-    MPI_Win_fence(0, win1);  //get from root: root is getting the data from workers window
+    // Fence 6
+    MPI_Win_fence(0, win1);  // Get from root: root is getting the data from worker's window
 
-    //free the window
+    // Free the window
     MPI_Win_free(&win1);
     MPI_Win_free(&win2);
-  }
+    return std::pair<float, float>(0.0f, 0.0f);
+  }  // End of oneSidedCommWorker
 
 private:
   int size_;
   int rank_;
-  int messageSize;  //size for the worker vects
+  int messageSize;  // Size for the worker vectors
   std::vector<float> v1;
   std::vector<float> v2;
 };
