a28874af3f2d4912535782799b8f5f65dd04b6c6
Sunanda <sunanda.banerjee@cern.ch>
2026-01-28 05:10:58 +0100

    Code check

--- Geometry/CaloTopology/test/HGCalNeighbourTester.cc
+++ Geometry/CaloTopology/test/HGCalNeighbourTester.cc
@@ -93,12 +93,16 @@ void HGCalNeighbourTester::beginRun(edm::Run const &iRun, edm::EventSetup const 
     for (unsigned int k = 0; k < detIds_.size(); k += nskip_) {
       HGCSiliconDetId id(detIds_[k]);
       std::vector<unsigned int> ids = hgc_->nearestNeighboursOfDetId(detIds_[k].rawId());
-      edm::LogVerbatim("HGCGeom") << "[" << k << "] Layer " << id.layer() << " Wafer " << id.waferU() << ":" << id.waferV() << " Cell " << id.cellU() << ":" << id.cellV() << " has " << ids.size() << " neighbours:";
+      edm::LogVerbatim("HGCGeom") << "[" << k << "] Layer " << id.layer() << " Wafer " << id.waferU() << ":"
+                                  << id.waferV() << " Cell " << id.cellU() << ":" << id.cellV() << " has " << ids.size()
+                                  << " neighbours:";
       unsigned int k1(0);
-      for (auto const& idZ : ids) {
-	HGCSiliconDetId idx(idZ);
-	edm::LogVerbatim("HGCGeom") << "[" << k << "] Layer " << id.layer() << " Wafer " << id.waferU() << ":" << id.waferV() << " Cell " << id.cellU() << ":" << id.cellV() << " has " << ids.size() << " neighbours:";
-	++k1;
+      for (auto const &idZ : ids) {
+        HGCSiliconDetId idx(idZ);
+        edm::LogVerbatim("HGCGeom") << "[" << k << "] Layer " << id.layer() << " Wafer " << id.waferU() << ":"
+                                    << id.waferV() << " Cell " << id.cellU() << ":" << id.cellV() << " has "
+                                    << ids.size() << " neighbours:";
+        ++k1;
       }
     }
   } else {
