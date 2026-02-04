041f7eb7a243ac06ce997a5fe94cf19780585776
Dmytro Kovalskyi <dmytro@cern.ch>
2026-02-04 11:17:31 +0100

    use standard collection names

--- PhysicsTools/PatFromScouting/test/test_scoutingToMiniAOD_cfg.py
+++ PhysicsTools/PatFromScouting/test/test_scoutingToMiniAOD_cfg.py
@@ -1,4 +1,10 @@
 import FWCore.ParameterSet.Config as cms
+from FWCore.ParameterSet.VarParsing import VarParsing
+
+options = VarParsing('analysis')
+options.inputFiles = ['file:/data/dmytro/data/store+data+Run2024C+ScoutingPFRun3+HLTSCOUT+v1+000+380+197+00000+05943107-3d39-4fcd-bea9-2b71ca2c3890.root']
+options.maxEvents = 100
+options.parseArguments()
 
 process = cms.Process("TEST")
 
@@ -8,37 +14,51 @@ process.MessageLogger.cerr.FwkReport.reportEvery = 10
 # Load particle data table (needed for PF candidate producer)
 process.load("SimGeneral.HepPDTESSource.pdt_cfi")
 
-process.maxEvents = cms.untracked.PSet(input = cms.untracked.int32(100))
+# Load conditions for beam spot
+process.load('Configuration.StandardSequences.FrontierConditions_GlobalTag_cff')
+from Configuration.AlCa.GlobalTag import GlobalTag
+process.GlobalTag = GlobalTag(process.GlobalTag, 'auto:run3_data_prompt', '')
+
+process.maxEvents = cms.untracked.PSet(input = cms.untracked.int32(options.maxEvents))
 
 process.source = cms.Source("PoolSource",
-    fileNames = cms.untracked.vstring(
-        'file:store+data+Run2024C+ScoutingPFRun3+HLTSCOUT+v1+000+380+197+00000+05943107-3d39-4fcd-bea9-2b71ca2c3890.root'
-    )
+    fileNames = cms.untracked.vstring(options.inputFiles)
 )
 
 # Load the scouting to MiniAOD configuration
 from PhysicsTools.PatFromScouting.scoutingToMiniAOD_cff import *
 
-process.scoutingPFCandidates = scoutingPFCandidates
-process.scoutingVertices = scoutingVertices
-process.scoutingMuons = scoutingMuons.clone(
+# Use standard MiniAOD collection names
+process.packedPFCandidates = packedPFCandidates
+process.offlineSlimmedPrimaryVertices = offlineSlimmedPrimaryVertices
+process.slimmedMuons = slimmedMuons.clone(
     src = cms.InputTag("hltScoutingMuonPackerVtx")
 )
-process.scoutingElectrons = scoutingElectrons
-process.scoutingPhotons = scoutingPhotons
-process.scoutingJets = scoutingJets
-process.scoutingMET = scoutingMET
+process.slimmedElectrons = slimmedElectrons
+process.slimmedPhotons = slimmedPhotons
+process.slimmedJets = slimmedJets
+process.slimmedMETs = slimmedMETs
 process.scoutingTracks = scoutingTracks
+process.offlineBeamSpot = offlineBeamSpot
+process.fixedGridRhoFastjetAll = fixedGridRhoFastjetAll
+process.gtStage2Digis = gtStage2Digis
+process.gmtStage2Digis = gmtStage2Digis
+process.caloStage2Digis = caloStage2Digis
 
 process.p = cms.Path(
-    process.scoutingPFCandidates +
-    process.scoutingVertices +
-    process.scoutingMuons +
-    process.scoutingElectrons +
-    process.scoutingPhotons +
-    process.scoutingJets +
-    process.scoutingMET +
-    process.scoutingTracks
+    process.offlineSlimmedPrimaryVertices +
+    process.packedPFCandidates +
+    process.offlineBeamSpot +
+    process.slimmedMuons +
+    process.slimmedElectrons +
+    process.slimmedPhotons +
+    process.slimmedJets +
+    process.slimmedMETs +
+    process.scoutingTracks +
+    process.fixedGridRhoFastjetAll +
+    process.gtStage2Digis +
+    process.gmtStage2Digis +
+    process.caloStage2Digis
 )
 
 # Output
@@ -46,14 +66,20 @@ process.out = cms.OutputModule("PoolOutputModule",
     fileName = cms.untracked.string('scoutingToMiniAOD_test.root'),
     outputCommands = cms.untracked.vstring(
         'drop *',
-        'keep *_scoutingPFCandidates_*_*',
-        'keep *_scoutingVertices_*_*',
-        'keep *_scoutingMuons_*_*',
-        'keep *_scoutingElectrons_*_*',
-        'keep *_scoutingPhotons_*_*',
-        'keep *_scoutingJets_*_*',
-        'keep *_scoutingMET_*_*',
+        'keep *_packedPFCandidates_*_*',
+        'keep *_offlineSlimmedPrimaryVertices_*_*',
+        'keep *_slimmedMuons_*_*',
+        'keep *_slimmedElectrons_*_*',
+        'keep *_slimmedPhotons_*_*',
+        'keep *_slimmedJets_*_*',
+        'keep *_slimmedMETs_*_*',
         'keep *_scoutingTracks_*_*',
+        'keep *_TriggerResults_*_*',
+        'keep *_offlineBeamSpot_*_*',
+        'keep *_fixedGridRho*_*_*',
+        'keep *_gtStage2Digis_*_*',
+        'keep *_gmtStage2Digis_*_*',
+        'keep *_caloStage2Digis_*_*',
     )
 )
 
