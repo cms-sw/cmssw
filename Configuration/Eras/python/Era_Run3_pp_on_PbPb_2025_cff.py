c5828cb285d3e46a1e722aaaf5652194a8b516b2
Hannah <hannah.bossi@yale.edu>
2026-02-08 01:59:46 +0100

    Change implementation to process modifier.

--- Configuration/Eras/python/Era_Run3_pp_on_PbPb_2025_cff.py
+++ Configuration/Eras/python/Era_Run3_pp_on_PbPb_2025_cff.py
@@ -2,6 +2,7 @@ import FWCore.ParameterSet.Config as cms
 
 from Configuration.Eras.Era_Run3_2025_cff import Run3_2025
 from Configuration.ProcessModifiers.pp_on_AA_cff import pp_on_AA
+from Configuration.ProcessModifiers.rpdReco_cff import rpdReco
 from Configuration.Eras.Modifier_pp_on_PbPb_run3_cff import pp_on_PbPb_run3
 from Configuration.Eras.Modifier_pp_on_PbPb_run3_2025_cff import pp_on_PbPb_run3_2025
 from Configuration.Eras.Modifier_dedx_lfit_cff import dedx_lfit
--- Configuration/Eras/python/Era_Run3_pp_on_PbPb_2026_cff.py
+++ Configuration/Eras/python/Era_Run3_pp_on_PbPb_2026_cff.py
@@ -2,6 +2,7 @@ import FWCore.ParameterSet.Config as cms
 
 from Configuration.Eras.Era_Run3_2026_cff import Run3_2026
 from Configuration.ProcessModifiers.pp_on_AA_cff import pp_on_AA
+from Configuration.ProcessModifiers.rpdReco_cff import rpdReco
 from Configuration.Eras.Modifier_pp_on_PbPb_run3_cff import pp_on_PbPb_run3
 from Configuration.Eras.Modifier_pp_on_PbPb_run3_2026_cff import pp_on_PbPb_run3_2026
 from Configuration.Eras.Modifier_dedx_lfit_cff import dedx_lfit
