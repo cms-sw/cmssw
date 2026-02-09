fa81aaaaa45e9933584050da8263561bdbea24cb
Hannah <hannah.bossi@yale.edu>
2026-02-09 23:53:04 +0100

    Add rpd reco back in.

--- Configuration/Eras/python/Era_Run3_pp_on_PbPb_2025_cff.py
+++ Configuration/Eras/python/Era_Run3_pp_on_PbPb_2025_cff.py
@@ -7,4 +7,4 @@ from Configuration.Eras.Modifier_pp_on_PbPb_run3_cff import pp_on_PbPb_run3
 from Configuration.Eras.Modifier_pp_on_PbPb_run3_2025_cff import pp_on_PbPb_run3_2025
 from Configuration.Eras.Modifier_dedx_lfit_cff import dedx_lfit
 
-Run3_pp_on_PbPb_2025 = cms.ModifierChain(Run3_2025, dedx_lfit, pp_on_AA, pp_on_PbPb_run3, pp_on_PbPb_run3_2025)
+Run3_pp_on_PbPb_2025 = cms.ModifierChain(Run3_2025, dedx_lfit, pp_on_AA, rpdReco, pp_on_PbPb_run3, pp_on_PbPb_run3_2025)
--- Configuration/Eras/python/Era_Run3_pp_on_PbPb_2026_cff.py
+++ Configuration/Eras/python/Era_Run3_pp_on_PbPb_2026_cff.py
@@ -7,4 +7,4 @@ from Configuration.Eras.Modifier_pp_on_PbPb_run3_cff import pp_on_PbPb_run3
 from Configuration.Eras.Modifier_pp_on_PbPb_run3_2026_cff import pp_on_PbPb_run3_2026
 from Configuration.Eras.Modifier_dedx_lfit_cff import dedx_lfit
 
-Run3_pp_on_PbPb_2026 = cms.ModifierChain(Run3_2026, dedx_lfit, pp_on_AA, pp_on_PbPb_run3, pp_on_PbPb_run3_2026)
+Run3_pp_on_PbPb_2026 = cms.ModifierChain(Run3_2026, dedx_lfit, pp_on_AA, rpdReco, pp_on_PbPb_run3, pp_on_PbPb_run3_2026)
