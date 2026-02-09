df699bc5e08ce37aaddee57869a3fec42a5a2adb
Christopher Jones <chrisdjones15@gmail.com>
2026-02-09 15:39:25 -0600

    Apply code-format

--- L1Trigger/Phase2L1ParticleFlow/interface/corrector.h
+++ L1Trigger/Phase2L1ParticleFlow/interface/corrector.h
@@ -21,59 +21,59 @@ namespace l1tpf {
           emulate_(false),
           debug_(false),
           emulationMode_(l1tpf::corrector::EmulationMode::CorrectedPt) {}
-    corrector(const std::string &iFile,
+    corrector(const std::string& iFile,
               float emfMax = -1,
               bool debug = false,
               bool emulate = false,
               l1tpf::corrector::EmulationMode emulationMode = l1tpf::corrector::EmulationMode::CorrectedPt);
-    corrector(const std::string &iFile,
-              const std::string &directory,
+    corrector(const std::string& iFile,
+              const std::string& directory,
               float emfMax = -1,
               bool debug = false,
               bool emulate = false,
               l1tpf::corrector::EmulationMode emulationMode = l1tpf::corrector::EmulationMode::CorrectedPt);
-    corrector(TDirectory *src,
+    corrector(TDirectory* src,
               float emfMax = -1,
               bool debug = false,
               bool emulate = false,
               l1tpf::corrector::EmulationMode emulationMode = l1tpf::corrector::EmulationMode::CorrectedPt);
     // create an empty corrector (you'll need to fill the graphs later)
-    corrector(const TH1 *index, float emfMax = -1);
+    corrector(const TH1* index, float emfMax = -1);
     ~corrector();
 
     // no copy, but can move
-    corrector(const corrector &corr) = delete;
-    corrector &operator=(const corrector &corr) = delete;
-    corrector(corrector &&corr);
-    corrector &operator=(corrector &&corr);
+    corrector(const corrector& corr) = delete;
+    corrector& operator=(const corrector& corr) = delete;
+    corrector(corrector&& corr);
+    corrector& operator=(corrector&& corr);
 
     float correctedPt(float et, float emEt, float eta) const;
     float correctedPt(float et, float eta) const { return correctedPt(et, 0, eta); }
-    void correctPt(l1t::PFCluster &cluster, float preserveEmEt = true) const;
+    void correctPt(l1t::PFCluster& cluster, float preserveEmEt = true) const;
 
     bool valid() const { return (index_.get() != nullptr); }
 
     // set the graph (note: it is cloned, and the corrector owns the clone)
-    void setGraph(const TGraph &graph, int ieta, int iemf = 0);
+    void setGraph(const TGraph& graph, int ieta, int iemf = 0);
 
     bool is2d() const { return is2d_; }
     unsigned int neta() const { return neta_; }
     unsigned int nemf() const { return nemf_; }
     // access the index histogram
-    const TH1 &getIndex() const { return *index_; }
+    const TH1& getIndex() const { return *index_; }
     // access the graphs (owned by the corrector, may be null)
-    TGraph *getGraph(int ieta, int iemf = 0) { return corrections_[ieta * nemf_ + iemf]; }
-    const TGraph *getGraph(int ieta, int iemf = 0) const { return corrections_[ieta * nemf_ + iemf]; }
+    TGraph* getGraph(int ieta, int iemf = 0) { return corrections_[ieta * nemf_ + iemf]; }
+    const TGraph* getGraph(int ieta, int iemf = 0) const { return corrections_[ieta * nemf_ + iemf]; }
 
     // store the corrector
-    void writeToFile(const std::string &filename, const std::string &directory) const;
+    void writeToFile(const std::string& filename, const std::string& directory) const;
     // store the corrector
-    void writeToFile(TDirectory *dest) const;
+    void writeToFile(TDirectory* dest) const;
 
   private:
     std::unique_ptr<TH1> index_;
-    std::vector<TGraph *> corrections_;
-    std::vector<TH1 *> correctionsEmulated_;
+    std::vector<TGraph*> corrections_;
+    std::vector<TH1*> correctionsEmulated_;
     bool is2d_;
     unsigned int neta_, nemf_;
     float emfMax_;
@@ -81,10 +81,10 @@ namespace l1tpf {
     bool debug_;
     l1tpf::corrector::EmulationMode emulationMode_;
 
-    void init_(const std::string &iFile, const std::string &directory, bool debug, bool emulate);
-    void init_(TDirectory *src, bool debug);
-    void initGraphs_(TDirectory *src, bool debug);
-    void initEmulation_(TDirectory *src, bool debug);
+    void init_(const std::string& iFile, const std::string& directory, bool debug, bool emulate);
+    void init_(TDirectory* src, bool debug);
+    void initGraphs_(TDirectory* src, bool debug);
+    void initEmulation_(TDirectory* src, bool debug);
   };
 }  // namespace l1tpf
 #endif
