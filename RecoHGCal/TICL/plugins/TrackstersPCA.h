fa0921b062c86799ae0cef3fddf38edd16d19e64
Felice Pantaleo <felice.pantaleo@cern.ch>
2026-02-11 17:10:33 +0100

    TICL: avoiding copying all the hits of each layercluster to access the detId of its first hit

--- RecoHGCal/TICL/plugins/TrackstersPCA.h
+++ RecoHGCal/TICL/plugins/TrackstersPCA.h
@@ -38,9 +38,7 @@ namespace ticl {
                                                size_t N);
 
   inline unsigned getLayerFromLC(const reco::CaloCluster &LC, const hgcal::RecHitTools &rhtools) {
-    std::vector<std::pair<DetId, float>> thisclusterHits = LC.hitsAndFractions();
-    auto layer = rhtools.getLayerWithOffset(thisclusterHits[0].first);
-    return layer;
+    return rhtools.getLayerWithOffset(LC.hitsAndFractions()[0].first);
   }
 
   // Sort the layer clusters in the given trackster in bins of layer. Returns : vector[index=layer, value=vector[LC index]]]
