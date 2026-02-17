dec36708a20fd0eb93dc84b08478735c2acc66af
Annnnya <anna.polova@ucu.edu.ua>
2026-02-17 17:01:43 +0100

    formatted plugin to record cpp names

--- HeterogeneousCore/MPICore/plugins/MPIPrintCPPNames.cc
+++ HeterogeneousCore/MPICore/plugins/MPIPrintCPPNames.cc
@@ -10,26 +10,18 @@
 class PrintCPPNames : public edm::stream::EDProducer<> {
 public:
   explicit PrintCPPNames(edm::ParameterSet const&) {
-    callWhenNewProductsRegistered(
-        [](edm::ProductDescription const& product) {
-          static constexpr std::string_view kPathStatus("edm::PathStatus");
-          static constexpr std::string_view kEndPathStatus("edm::EndPathStatus");
+    callWhenNewProductsRegistered([](edm::ProductDescription const& product) {
+      static constexpr std::string_view kPathStatus("edm::PathStatus");
+      static constexpr std::string_view kEndPathStatus("edm::EndPathStatus");
 
-          // Skip framework-internal status products if desired
-          if (product.className() == kPathStatus ||
-              product.className() == kEndPathStatus)
-            return;
+      // Skip framework-internal status products if desired
+      if (product.className() == kPathStatus || product.className() == kEndPathStatus)
+        return;
 
-          std::cout
-              << "PrintCPPNames: considering product "
-              << product.friendlyClassName() << '_'
-              << product.moduleLabel() << '_'
-              << product.productInstanceName() << '_'
-              << product.processName()
-              << " of type " << product.unwrappedType().name()
-              << " branch type " << product.branchType()
-              << '\n';
-        });
+      std::cout << "PrintCPPNames: considering product " << product.friendlyClassName() << '_' << product.moduleLabel()
+                << '_' << product.productInstanceName() << '_' << product.processName() << " of type "
+                << product.unwrappedType().name() << " branch type " << product.branchType() << '\n';
+    });
   }
 
   void produce(edm::Event&, edm::EventSetup const&) override {
