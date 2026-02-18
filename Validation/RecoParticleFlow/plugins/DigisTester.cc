e73ad517ed3112b030fead6a08cfe4bbc9ac3cbb
Bruno Alves <balves16@hotmail.com>
2026-02-18 19:19:11 +0100

    Add RecHit and Digi tester.

---
+++ Validation/RecoParticleFlow/plugins/DigisTester.cc
@@ -0,0 +1,147 @@
+#include "FWCore/Framework/interface/MakerMacros.h"
+#include "DQMServices/Core/interface/DQMEDAnalyzer.h"
+#include "DQMServices/Core/interface/DQMStore.h"
+#include "FWCore/Framework/interface/Event.h"
+#include "FWCore/ParameterSet/interface/ParameterSet.h"
+#include "DataFormats/Common/interface/Handle.h"
+#include "FWCore/MessageLogger/interface/MessageLogger.h"
+
+#include "Geometry/CaloGeometry/interface/CaloGeometry.h"
+#include "Geometry/Records/interface/CaloGeometryRecord.h"
+#include "DataFormats/GeometryVector/interface/GlobalPoint.h"
+#include "Geometry/CaloTopology/interface/EcalBarrelTopology.h"
+#include "Geometry/CaloTopology/interface/EcalEndcapTopology.h"
+
+#include "DataFormats/EcalDigi/interface/EcalDigiCollections.h"
+#include "DataFormats/DetId/interface/DetId.h"
+#include "DataFormats/EcalDetId/interface/EBDetId.h"
+
+class DigisTester : public DQMEDAnalyzer {
+public:
+  explicit DigisTester(const edm::ParameterSet&);  
+
+protected:
+  void bookHistograms(DQMStore::IBooker&, edm::Run const&, edm::EventSetup const&) override;
+  void analyze(const edm::Event&, const edm::EventSetup&) override;
+  
+  edm::ESGetToken<CaloGeometry, CaloGeometryRecord> geometry_token_;
+  edm::EDGetTokenT<EBDigiCollection> ecalEBDigisToken_;
+  edm::EDGetTokenT<EEDigiCollection> ecalEEDigisToken_;
+
+  std::string outFolder_;
+
+  using U2Map = std::unordered_map<std::string, MonitorElement*>;
+  U2Map h2d_ebdigis_, h2d_eedigis_;
+};
+
+DigisTester::DigisTester(const edm::ParameterSet& iConfig)
+  : geometry_token_(esConsumes<CaloGeometry, CaloGeometryRecord>()),
+	ecalEBDigisToken_(consumes<EBDigiCollection>(iConfig.getParameter<edm::InputTag>("ecalEBDigis"))),
+	ecalEEDigisToken_(consumes<EEDigiCollection>(iConfig.getParameter<edm::InputTag>("ecalEEDigis"))),
+	outFolder_(iConfig.getParameter<std::string>("outFolder")) {}
+
+void DigisTester::bookHistograms(DQMStore::IBooker& ibook,
+								 edm::Run const&,
+								 edm::EventSetup const&) {
+  ibook.setCurrentFolder(outFolder_ + "/Digis");
+
+  unsigned mNBinsADC = 100;
+  float mADCMin = 180.;
+  float mADCMax = 500.;
+  unsigned mNBinsPhi = 100;
+  float mPhiMax = 3.2;
+  unsigned mNBinsEta = 100;
+  float mEtaMax = 3.3;
+
+  std::unordered_map<std::string, std::tuple<unsigned, float, float, unsigned, float, float>> ebVars = {
+	{"ADC_Eta", std::make_tuple(mNBinsADC, mADCMin, mADCMax, mNBinsEta, -mEtaMax, mEtaMax)},
+	{"ADC_Phi", std::make_tuple(mNBinsADC, mADCMin, mADCMax, mNBinsPhi, -mPhiMax, mPhiMax)},
+	{"Eta_Phi",   std::make_tuple(mNBinsEta, -mEtaMax, mEtaMax, mNBinsPhi, -mPhiMax, mPhiMax)},
+  };
+
+  for (auto& ebVar : ebVars) {
+    auto [nBinsX, hMinX, hMaxX, nBinsY, hMinY, hMaxY] = ebVar.second;
+    auto x_title = ebVar.first.substr(0, ebVar.first.find("_"));
+    auto y_title = ebVar.first.substr(ebVar.first.find("_") + 1);
+    h2d_ebdigis_[ebVar.first] = ibook.book2D("EcalEBDigis" + ebVar.first,
+											 "EcalEBDigis;" + x_title + ";" + y_title,
+											 nBinsX, hMinX, hMaxX,
+											 nBinsY, hMinY, hMaxY);
+  }
+
+  std::unordered_map<std::string, std::tuple<unsigned, float, float, unsigned, float, float>> eeVars = {
+	{"ADC_Eta",  std::make_tuple(mNBinsADC, mADCMin, mADCMax, mNBinsEta, -mEtaMax, mEtaMax)},
+	{"ADC_Phi",  std::make_tuple(mNBinsADC, mADCMin, mADCMax, mNBinsPhi, -mPhiMax, mPhiMax)},
+	{"Eta_Phi",  std::make_tuple(mNBinsEta, -mEtaMax, mEtaMax, mNBinsPhi, -mPhiMax, mPhiMax)},
+  };
+
+  for (auto& eeVar : eeVars) {
+    auto [nBinsX, hMinX, hMaxX, nBinsY, hMinY, hMaxY] = eeVar.second;
+    auto x_title = eeVar.first.substr(0, eeVar.first.find("_"));
+    auto y_title = eeVar.first.substr(eeVar.first.find("_") + 1);
+    h2d_eedigis_[eeVar.first] = ibook.book2D("EcalEEDigis" + eeVar.first,
+											 "EcalEEDigis;" + x_title + ";" + y_title,
+											 nBinsX, hMinX, hMaxX,
+											 nBinsY, hMinY, hMaxY);
+  }
+}
+
+void DigisTester::analyze(const edm::Event& iEvent, const edm::EventSetup& iSetup) {
+  const CaloGeometry& caloGeom = iSetup.getData(geometry_token_);
+  // const auto& barrelGeom = caloGeom.getSubdetectorGeometry(DetId::Ecal, EcalBarrel);
+  // const auto& endcapGeom = caloGeom.getSubdetectorGeometry(DetId::Ecal, EcalEndcap);
+  
+  edm::Handle<EBDigiCollection> ebDigisHandle;
+  iEvent.getByToken(ecalEBDigisToken_, ebDigisHandle);
+  if (!ebDigisHandle.isValid()) {
+    edm::LogPrint("DigisTester") << "Input EB Digis collection not found.";
+    return;
+  }
+
+  edm::Handle<EEDigiCollection> eeDigisHandle;
+  iEvent.getByToken(ecalEEDigisToken_, eeDigisHandle);
+  if (!eeDigisHandle.isValid()) {
+    edm::LogPrint("DigisTester") << "Input EE Digis collection not found.";
+    return;
+  }
+
+  for (const edm::DataFrame& digi : *ebDigisHandle) {
+	float eta = caloGeom.getPosition(digi.id()).eta();
+	float phi = caloGeom.getPosition(digi.id()).phi();
+	
+    // Get the number of ADC samples (usually 10)
+    int nADCSamples = digi.size();
+
+	float adcMax = 0;
+	for (int i=0; i<nADCSamples; ++i) {
+	  // compute max of adc counts of all channels
+	  float adcVal = static_cast<EBDataFrame>(digi).sample(i).adc();
+	  if (adcVal > adcMax) adcMax = adcVal;
+	}
+	  
+	h2d_ebdigis_["ADC_Eta"]->Fill(adcMax, eta);
+	h2d_ebdigis_["ADC_Phi"]->Fill(adcMax, phi);
+	h2d_ebdigis_["Eta_Phi"]->Fill(eta, phi);
+  }
+
+  for (const edm::DataFrame& digi : *eeDigisHandle) {
+	float eta = caloGeom.getPosition(digi.id()).eta();
+	float phi = caloGeom.getPosition(digi.id()).phi();
+
+	// Get the number of ADC samples (usually 10)
+    int nADCSamples = digi.size();
+
+	float adcMax = 0;
+	for (int i=0; i<nADCSamples; ++i) {
+	  // compute max of adc counts of all channels
+	  float adcVal = static_cast<EEDataFrame>(digi).sample(i).adc();
+	  if (adcVal > adcMax) adcMax = adcVal;
+	}
+
+	h2d_eedigis_["ADC_Eta"]->Fill(adcMax, eta);
+	h2d_eedigis_["ADC_Phi"]->Fill(adcMax, phi);
+	h2d_eedigis_["Eta_Phi"]->Fill(eta, phi);
+  }
+}
+
+DEFINE_FWK_MODULE(DigisTester);
---
+++ Validation/RecoParticleFlow/scripts/makeHLTHitValidationPlots.py
@@ -0,0 +1,137 @@
+#!/usr/bin/env python3
+
+import os
+import argparse
+import numpy as np
+import utils
+import matplotlib.pyplot as plt
+plt.style.use('tableau-colorblind10')
+
+import ROOT
+import mplhep as hep
+hep.style.use("CMS")
+
+from makeHLTPFValidationPlots import plot2D, debug
+
+if __name__ == '__main__':
+
+    def check_list_length(value):
+        if len(value) <= 1:
+            raise argparse.ArgumentTypeError("List must have more than one item")
+        return value
+
+    class DependencyAction(argparse.Action):
+        def __call__(self, parser, namespace, values, option_string=None):
+            if option_string == "--compare_files_labels" and not namespace.compare_files:
+                parser.error("`--compare_files_labels` requires `--compare_files` to be set")
+            if option_string == "--compare_files_labels" and not namespace.met:
+                parser.error("`--compare_files_labels` requires `--met` to be set")
+            if len(namespace.met) > 1:
+                parser.error("When comparing files only one MET collection can be used.")
+            if len(values) != len(namespace.compare_files):
+                parser.error("`--compare_files_labels` must have the same size as `--compare_files`")
+            setattr(namespace, self.dest, values)
+        
+    full_command = 'python3 Validation/RecoParticleFlow/scripts/makeHLTPFValidationPlots.py --odir <your_dir> -l TTbar -f <your_ROOT_file>'
+    parser = argparse.ArgumentParser(description='Make HLT PF validation plots. \nExample command:\n' + full_command)
+    parser.add_argument('-o', '--odir', default="HLTPFValidationPlots", help='Path to the output directory.')
+    parser.add_argument('-l', '--sample_label', default="", help='Sample label for plotting.')
+    parser.add_argument('-e', '--era', default="Phase2", help="Chose between ['Phase2', 'Run3'].")
+    parser.add_argument('--EnFracCut', default=0.01, help='Cut on the sim cluster energy fraction.')
+    parser.add_argument('--PtCut', default=0.1, help='Cut on the sim cluster energy fraction.')
+    parser.add_argument('--digis', action='store_true', help='Plot digis, otherwise plot hits.')
+
+    mutual_excl2 = parser.add_mutually_exclusive_group(required=True)
+    mutual_excl2.add_argument('-f', '--file', help='Paths to the DQM ROOT file.')
+    mutual_excl2.add_argument('-x', '--compare_files', nargs='+', type=check_list_length,
+                              help='Compare the same collection in different DQM files.', )
+    parser.add_argument('-y', '--compare_files_labels', nargs='+',
+                        action=DependencyAction, help='Compare the same collection in different DQM files.',)
+    
+    args = parser.parse_args()
+
+    utils.createDir(args.odir)
+    parentDir = os.path.dirname(args.odir)
+    if args.odir[-1] == '/':
+        parentDir = os.path.dirname(parentDir)
+    utils.createIndexPHP(src=parentDir, dest=args.odir)
+    
+    fontsize = 16    
+    colors = hep.style.CMS['axes.prop_cycle'].by_key()['color']
+    markers = ('o', 's', 'd')
+    errorbar_kwargs = dict(capsize=3, elinewidth=0.8, capthick=2, linewidth=2, linestyle='')
+
+    nSimClustersLabel = '# SimClusters'
+    nPFClustersLabel = '# PFClusters'
+
+    titles = {'responsePt': r"$p_{T}^{Reco}/p_{T}^{Sim}$", 
+              'response': r"$E^{Reco}/E^{Sim}$", 
+              'av_responsePt': r"$<p_{T}^{Reco}/p_{T}^{Sim}>$", 
+              'av_response': r"$<E^{Reco}/E^{Sim}>$", 
+              'resolutionPt': r"$\sigma(p_{T}^{Reco}/p_{T}^{Sim}) / <p_{T}^{Reco}/p_{T}^{Sim}>$", 
+              'resolution': r"$\sigma(E^{Reco}/E^{Sim}) / <E^{Reco}/E^{Sim}>$", 
+              'eff': 'Efficiency',
+              'fake': 'Fake Rate',
+              'split': 'Split Rate',
+              'merge': 'Merge Rate'}
+
+    dqm_dir = f'DQMData/Run 1/HLT/Run summary/ParticleFlow/' + ('Digis' if args.digis else 'Hits')
+    afile = ROOT.TFile.Open(args.file)
+    utils.checkRootDir(afile, dqm_dir)
+
+    debug('Start caching Hits histograms...')
+    subdirs = []
+    cached_histos = {}
+    directory = afile.GetDirectory(dqm_dir)
+    for key in directory.GetListOfKeys():
+        obj = key.ReadObj()
+        if isinstance(obj, ROOT.TDirectory):
+            subdirs.append(obj.GetName())
+            subdir = afile.GetDirectory(f"{dqm_dir}/{obj.GetName()}")
+            for subkey in subdir.GetListOfKeys():
+                name = subkey.GetName()
+                cached_histos[f"{obj.GetName()}/{name}"] = subkey.ReadObj()
+        else:
+            name = key.GetName()
+            cached_histos[name] = key.ReadObj()
+    debug(' ...done.')
+
+    if args.digis:
+        vars2D = {
+            'EcalEBDigisADC_Eta': dict(xtitle=r'$\max(ADC Counts)$', ytitle=r'$\eta$', var='# EB Digis'),
+            'EcalEBDigisADC_Phi': dict(xtitle=r'$\max(ADC Counts)$', ytitle=r'$\phi$', var='# EB Digis'),
+            'EcalEBDigisEta_Phi': dict(xtitle=r'$\eta$', ytitle=r'$\phi$', var='# EB Digis'),
+            'EcalEEDigisADC_Eta': dict(xtitle=r'$\max(ADC Counts)$', ytitle=r'$\eta$', var='# EE Digis'),
+            'EcalEEDigisADC_Phi': dict(xtitle=r'$\max(ADC Counts)$', ytitle=r'$\phi$', var='# EE Digis'),
+            'EcalEEDigisEta_Phi': dict(xtitle=r'$\eta$', ytitle=r'$\phi$', var='# EE Digis'),
+        }
+    else:
+        vars2D = {
+            **{f'{det}UncalibRecHitsEn_Eta': dict(ytitle=r'$\eta$', var=f'# Uncalibrated {det} Reconstructed Hits', xtitle='Amplitude [ADC Counts]', logz=True)
+               for det in ('EE', 'EB')},
+            **{f'{det}UncalibRecHitsEn_Phi': dict(ytitle=r'$\phi$', var=f'# Uncalibrated {det} Reconstructed Hits', xtitle='Amplitude [ADC Counts]', logz=True)
+               for det in ('EE', 'EB')},
+            **{f'{det}UncalibRecHitsEta_Phi': dict(ytitle=r'$\phi$', var=f'# Uncalibrated {det} Reconstructed Hits', xtitle=r'$\eta$', logz=True)
+           for det in ('EE', 'EB')},
+            **{f'{det}UncalibRecHitsEta_Phi': dict(ytitle=r'$\phi$', var=f'# Uncalibrated {det} Reconstructed Hits', xtitle=r'$\eta$', logz=True)
+           for det in ('EE', 'EB')},
+            **{f'{det}RecHitsEn_Eta': dict(ytitle=r'$\eta$', var=f'# Reconstructed {det} Hits', xtitle='Energy [GeV]', logz=True)
+           for det in ('EE', 'EB')},
+            **{f'{det}RecHitsEn_Phi': dict(ytitle=r'$\phi$', var=f'# Reconstructed {det} Hits', xtitle='Energy [GeV]', logz=True)
+           for det in ('EE', 'EB')},
+            **{f'{det}RecHitsEta_Phi': dict(ytitle=r'$\phi$', var=f'# Reconstructed {det} Hits', xtitle=r'$\eta$', logz=True)
+           for det in ('EE', 'EB')},
+            **{f'{det}SimHitsEn_Eta': dict(ytitle=r'$\eta$', var=f'# Simulated {det} Hits', xtitle='Energy [GeV]', logz=True)
+           for det in ('EE', 'EB')},
+            **{f'{det}SimHitsEn_Phi': dict(ytitle=r'$\phi$', var=f'# Simulated {det} Hits', xtitle='Energy [GeV]', logz=True)
+           for det in ('EE', 'EB')},
+            **{f'{det}SimHitsEta_Phi': dict(ytitle=r'$\phi$', var=f'# Simulated {det} Hits', xtitle=r'$\eta$', logz=True)
+           for det in ('EE', 'EB')},
+            'PFRecHitsEn_Eta': dict(ytitle=r'$\eta$', var='# PF Reconstructed Hits', xtitle='Energy [GeV]', logz=True),
+            'PFRecHitsEn_Phi': dict(ytitle=r'$\phi$', var='# PF Reconstructed Hits', xtitle='Energy [GeV]', logz=True),
+            'PFRecHitsEta_Phi': dict(ytitle=r'$\phi$', var='# PF Reconstructed Hits', xtitle=r'$\eta$', logz=True),
+        }
+
+    for name, props in vars2D.items():
+        root_hist = cached_histos[f"{name}"]
+        plot2D(root_hist, args.sample_label, args.era, props, outname=os.path.join(args.odir, name))
