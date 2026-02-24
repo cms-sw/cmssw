205deec9577a634af0d6a56e8d4a98decd0e87c4
cquarant <claudio.quaranta@cern.ch>
2026-02-24 17:26:14 +0100

    removed 'using namespace' from header file

--- DataFormats/FTLDigiSoA/interface/alpaka/BTLDigiDeviceCollection.h
+++ DataFormats/FTLDigiSoA/interface/alpaka/BTLDigiDeviceCollection.h
@@ -10,13 +10,11 @@ namespace ALPAKA_ACCELERATOR_NAMESPACE::btldigi {
 
   // Make the names from the top-level btldigi namespace visible for unqualified lookup
   // inside the ALPAKA_ACCELERATOR_NAMESPACE::btldigi namespace.
-  using namespace ::btldigi;
-
-  using BTLDigiDeviceCollection = PortableCollection<BTLDigiSoA>;
+  using BTLDigiDeviceCollection = PortableCollection<::btldigi::BTLDigiSoA>;
 
 }  // namespace ALPAKA_ACCELERATOR_NAMESPACE::btldigi
 
 // Check that the portable device collection for the host device is the same as the portable host collection.
 ASSERT_DEVICE_MATCHES_HOST_COLLECTION(btldigi::BTLDigiDeviceCollection, btldigi::BTLDigiHostCollection);
 
-#endif  // DataFormats_FTLDigi_interface_alpaka_BTLDigiDeviceCollection_h
+#endif  // DataFormats_FTLDigiSoA_interface_alpaka_BTLDigiDeviceCollection_h
