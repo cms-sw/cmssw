65e939a83df4002a5ff348fb5307ddec749aef37
Eftychia Tzovara <eftychia.tzovara@cern.ch>
2026-02-27 12:48:47 +0100

    ScoutingJetMonitoring_cff.py: fixing naming convention

--- DQM/HLTEvF/python/ScoutingJetMonitoring_cff.py
+++ DQM/HLTEvF/python/ScoutingJetMonitoring_cff.py
@@ -27,74 +27,74 @@ jetDQMOnlineAnalyzerSequenceScouting = cms.Sequence(jetDQMOnlineAnalyzerAk4Scout
 
 ######### Scouitng Trigger and L1 seeds #########
 #PFScoutingJetHT
-PFScoutingJetHT_Online_Prommonitoring = PFScoutingJetHT_Prommonitoring.clone(
+PFScoutingJetHT_Onlinemonitoring = PFScoutingJetHT_Prommonitoring.clone(
     FolderName = 'HLT/ScoutingOnline/Jet/PFScoutingJetHT/'
 )
 
 # L1_HTT200er
-L1HTT200_Online_Prommonitoring = L1HTT200_Prommonitoring.clone(
+L1HTT200_Onlinemonitoring = L1HTT200_Prommonitoring.clone(
     FolderName = 'HLT/ScoutingOnline/Jet/L1_HTT200er/'
 )
 
 
 # L1_HTT255er
-L1HTT255_Online_Prommonitoring = L1HTT255_Prommonitoring.clone(
+L1HTT255_Onlinemonitoring = L1HTT255_Prommonitoring.clone(
     FolderName = 'HLT/ScoutingOnline/Jet/L1_HTT255er/'
 )
 
 
 # L1_HTT280er
-L1HTT280_Online_Prommonitoring = L1HTT280_Prommonitoring.clone(
+L1HTT280_Onlinemonitoring = L1HTT280_Prommonitoring.clone(
     FolderName = 'HLT/ScoutingOnline/Jet/L1_HTT280er/'
 )
 
 
 #  L1_HTT320er
-L1HTT320_Online_Prommonitoring = L1HTT320_Prommonitoring.clone(
+L1HTT320_Onlinemonitoring = L1HTT320_Prommonitoring.clone(
     FolderName = 'HLT/ScoutingOnline/Jet/L1_HTT320er/'
 )
 
 
 # L1_HTT360er
-L1HTT360_Online_Prommonitoring = L1HTT360_Prommonitoring.clone(
+L1HTT360_Onlinemonitoring = L1HTT360_Prommonitoring.clone(
     FolderName = 'HLT/ScoutingOnline/Jet/L1_HTT360er/'
 )
 
 # L1_HTT400er
-L1HTT400_Online_Prommonitoring = L1HTT400_Prommonitoring.clone(
+L1HTT400_Onlinemonitoring = L1HTT400_Prommonitoring.clone(
     FolderName = 'HLT/ScoutingOnline/Jet/L1_HTT400er/'
 )
 
 
 # L1_HTT450er
-L1HTT450_Online_Prommonitoring = L1HTT450_Prommonitoring.clone(
+L1HTT450_Onlinemonitoring = L1HTT450_Prommonitoring.clone(
     FolderName = 'HLT/ScoutingOnline/Jet/L1_HTT450er/'
 )
 
 
 # L1_SingleJet180
-L1SingleJet180_Online_Prommonitoring = L1SingleJet180_Prommonitoring.clone(
+L1SingleJet180_Onlinemonitoring = L1SingleJet180_Prommonitoring.clone(
     FolderName = 'HLT/ScoutingOnline/Jet/L1_SingleJet180/'
 )
 
 
 # L1_SingleJet200
-L1SingleJet200_Online_Prommonitoring = L1SingleJet200_Prommonitoring.clone(
+L1SingleJet200_Onlinemonitoring = L1SingleJet200_Prommonitoring.clone(
     FolderName = 'HLT/ScoutingOnline/Jet/L1_SingleJet200/'
 )
 
 HLTScoutingJetOnlinemonitoring = cms.Sequence(
     ak4PFScoutL1FastL2L3ResidualCorrectorChain
-    *PFScoutingJetHT_Online_Prommonitoring
-    *L1HTT200_Online_Prommonitoring
-    *L1HTT255_Online_Prommonitoring
-    *L1HTT280_Online_Prommonitoring
-    *L1HTT320_Online_Prommonitoring
-    *L1HTT360_Online_Prommonitoring
-    *L1HTT400_Online_Prommonitoring
-    *L1HTT450_Online_Prommonitoring
-    *L1SingleJet180_Online_Prommonitoring
-    *L1SingleJet200_Online_Prommonitoring
+    *PFScoutingJetHT_Onlinemonitoring
+    *L1HTT200_Onlinemonitoring
+    *L1HTT255_Onlinemonitoring
+    *L1HTT280_Onlinemonitoring
+    *L1HTT320_Onlinemonitoring
+    *L1HTT360_Onlinemonitoring
+    *L1HTT400_Onlinemonitoring
+    *L1HTT450_Onlinemonitoring
+    *L1SingleJet180_Onlinemonitoring
+    *L1SingleJet200_Onlinemonitoring
 )
 
 jetmetScoutingOnlineMonitorHLT = cms.Sequence(
