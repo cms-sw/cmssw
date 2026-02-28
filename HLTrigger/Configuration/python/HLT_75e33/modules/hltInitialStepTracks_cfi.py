2417334837ff23e516a0f3695b4b5f691eab0b4d
leonardo <leonardo.giannini@cern.ch>
2026-02-27 16:45:09 -0800

    remove the params that are not default from hltInitialStepTracks_cfi

--- HLTrigger/Configuration/python/HLT_75e33/modules/hltInitialStepTracks_cfi.py
+++ HLTrigger/Configuration/python/HLT_75e33/modules/hltInitialStepTracks_cfi.py
@@ -31,46 +31,7 @@ _hltInitialStepTracksMkFitFit = cms.EDProducer("MkFitOutputTrackConverter",
     mkFitSeeds = cms.InputTag("hltInitialStepMkFitSeeds"),
     src = cms.InputTag("hltInitialStepTrackCandidatesMkFitFit"),
     seeds = cms.InputTag("hltInitialStepSeeds"),
-    ttrhBuilder = cms.ESInputTag('', 'WithTrackAngle'),
-    propagatorAlong = cms.ESInputTag('', 'PropagatorWithMaterial'),
-    propagatorOpposite = cms.ESInputTag('', 'PropagatorWithMaterialOpposite'),
-    qualityMaxInvPt = cms.double(100),
-    qualityMinTheta = cms.double(0.01),
-    qualityMaxR = cms.double(120),
-    qualityMaxZ = cms.double(280),
-    qualityMaxPosErr = cms.double(100),
-    qualitySignPt = cms.bool(True),
-    calibrate = cms.bool(True),
-    calibBinCenter = cms.vdouble(
-      0.1704,
-      0.6028,
-      1.0188,
-      1.2898,
-      1.439,
-      1.4908,
-      1.55
-    ),
-    calibBinCoeff = cms.vdouble(
-      1,
-      1.0004,
-      1.00014,
-      1.0027,
-      1.0029,
-      1.0009,
-      0.9999
-    ),
-    calibBinOffset = cms.vdouble(
-      0.0016,
-      0.0032,
-      0.0033,
-      0.0045,
-      0.0005,
-      0.0012,
-      0.0003
-    ),
-    NavigationSchool = cms.ESInputTag('', 'SimpleNavigationSchool'),
-    measurementTrackerEvent = cms.InputTag("hltMeasurementTrackerEvent"),
-    mightGet = cms.optional.untracked.vstring
+    measurementTrackerEvent = cms.InputTag("hltMeasurementTrackerEvent")
 )
 
 _hltInitialStepTracksMkFitFitLSTSeeds = _hltInitialStepTracksMkFitFit.clone(seeds = "hltInitialStepTrajectorySeedsLST")
