a8e7c9af817122e529f88911994800e6f9d3de35
Dmytro Kovalskyi <dmytro@cern.ch>
2026-03-04 06:38:09 +0100

    Added support for jet constituents. Not very useful for Run2024 data, since scouting jets don't have constituent indices filled. Still can be useful in the future.

--- PhysicsTools/PatFromScouting/plugins/PatFromScoutingJetProducer.cc
+++ PhysicsTools/PatFromScouting/plugins/PatFromScoutingJetProducer.cc
@@ -13,6 +13,9 @@
      - Jet area
      - PFJet::Specific (energy fractions, multiplicities)
      - B-tagging discriminators (CSV, DeepCSV)
+
+     Constituent indices from the scouting jet are resolved to
+     CandidatePtr daughters pointing into the PackedCandidateCollection.
 */
 //
 // Original Author:  Dmytro Kovalskyi
@@ -31,6 +34,7 @@
 #include "FWCore/ParameterSet/interface/ParameterSetDescription.h"
 
 #include "DataFormats/PatCandidates/interface/Jet.h"
+#include "DataFormats/PatCandidates/interface/PackedCandidate.h"
 #include "DataFormats/Scouting/interface/Run3ScoutingPFJet.h"
 
 class PatFromScoutingJetProducer : public edm::stream::EDProducer<> {
@@ -44,10 +48,12 @@ private:
   void produce(edm::Event&, const edm::EventSetup&) override;
 
   const edm::EDGetTokenT<Run3ScoutingPFJetCollection> jetToken_;
+  const edm::EDGetTokenT<pat::PackedCandidateCollection> pfCandToken_;
 };
 
 PatFromScoutingJetProducer::PatFromScoutingJetProducer(const edm::ParameterSet& iConfig)
-    : jetToken_(consumes<Run3ScoutingPFJetCollection>(iConfig.getParameter<edm::InputTag>("src"))) {
+    : jetToken_(consumes<Run3ScoutingPFJetCollection>(iConfig.getParameter<edm::InputTag>("src"))),
+      pfCandToken_(consumes<pat::PackedCandidateCollection>(iConfig.getParameter<edm::InputTag>("pfCandidates"))) {
   produces<pat::JetCollection>();
 }
 
@@ -55,10 +61,20 @@ void PatFromScoutingJetProducer::produce(edm::Event& iEvent, const edm::EventSet
   auto patJets = std::make_unique<pat::JetCollection>();
 
   const auto& scoutingJets = iEvent.get(jetToken_);
+  auto pfCandHandle = iEvent.getHandle(pfCandToken_);
 
   for (const auto& sJet : scoutingJets) {
-    // Constructor now handles kinematics, PFSpecific, and b-tagging
-    patJets->push_back(pat::Jet(sJet));
+    pat::Jet jet(sJet);
+
+    // Resolve constituent indices to CandidatePtr daughters
+    for (int idx : sJet.constituents()) {
+      if (idx >= 0 && idx < static_cast<int>(pfCandHandle->size())) {
+        reco::CandidatePtr ptr(pfCandHandle, idx);
+        jet.addDaughter(ptr);
+      }
+    }
+
+    patJets->push_back(jet);
   }
 
   iEvent.put(std::move(patJets));
@@ -67,6 +83,7 @@ void PatFromScoutingJetProducer::produce(edm::Event& iEvent, const edm::EventSet
 void PatFromScoutingJetProducer::fillDescriptions(edm::ConfigurationDescriptions& descriptions) {
   edm::ParameterSetDescription desc;
   desc.add<edm::InputTag>("src", edm::InputTag("hltScoutingPFPacker"));
+  desc.add<edm::InputTag>("pfCandidates", edm::InputTag("packedPFCandidates"));
   descriptions.addWithDefaultLabel(desc);
 }
 
