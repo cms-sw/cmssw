10178d8f9516c3b2af1571980599660b3dc3ab8a
Marino Missiroli <marino.missiroli@cern.ch>
2026-03-07 17:54:31 +0100

    L1S(Nano): updates for CaloTowers in Run3_2026 era
    Updates to add CaloTowers' information to the output
    of "L1ScoutingNano" for 2026 workflows.
    
    This is done by adding a modifier named "run3_l1scouting_2026",
    and including it in the Era "Run3_2026".
    
    Various updates are made to custom_l1scoutingrun3_cff, mainly
     - using a dedicated Sequence (rather than overriding the regular NANO sequence), and
     - making the customisation of the relevant output module(s) more robust
       (covering both the NANOEDMAOD and NANOAOD data tiers, and
       not relying solely on the label of the output module).
    
    Co-Authored-By: Rocco Ardino <rocco.ardino@cern.ch>

--- Configuration/Eras/python/Era_Run3_2026_cff.py
+++ Configuration/Eras/python/Era_Run3_2026_cff.py
@@ -2,4 +2,6 @@ import FWCore.ParameterSet.Config as cms
 
 from Configuration.Eras.Era_Run3_2025_cff import Run3_2025
 
-Run3_2026 = cms.ModifierChain(Run3_2025)
+from Configuration.Eras.Modifier_run3_l1scouting_2026_cff import run3_l1scouting_2026
+
+Run3_2026 = cms.ModifierChain(Run3_2025, run3_l1scouting_2026)
