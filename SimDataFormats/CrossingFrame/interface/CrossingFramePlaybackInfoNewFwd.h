89762ed9d2d897da782681c6a69ea844c1b6c0c3
Matti Kortelainen <matti.kortelainen@cern.ch>
2026-03-11 15:02:03 -0500

    Move stored products to io_v1 namespace

--- SimDataFormats/CrossingFrame/interface/CrossingFramePlaybackInfoNewFwd.h
+++ SimDataFormats/CrossingFrame/interface/CrossingFramePlaybackInfoNewFwd.h
@@ -1,6 +1,10 @@
 #ifndef SimDataFormats_CrossingFrame_CrossingFramePlaybackInfoNewFwd_h
 #define SimDataFormats_CrossingFrame_CrossingFramePlaybackInfoNewFwd_h
 
-class CrossingFramePlaybackInfoNew;
+namespace io_v1 {
+  class CrossingFramePlaybackInfoNew;
+}  // namespace io_v1
+
+using CrossingFramePlaybackInfoNew = io_v1::CrossingFramePlaybackInfoNew;
 
 #endif
--- SimDataFormats/CrossingFrame/src/CrossingFramePlaybackInfoNew.cc
+++ SimDataFormats/CrossingFrame/src/CrossingFramePlaybackInfoNew.cc
@@ -1,9 +1,13 @@
 #include "SimDataFormats/CrossingFrame/interface/CrossingFramePlaybackInfoNew.h"
 #include <utility>
 
-CrossingFramePlaybackInfoNew::CrossingFramePlaybackInfoNew(int minBunch, int maxBunch, unsigned int maxNbSources)
-    : maxNbSources_(maxNbSources),
-      nBcrossings_(maxBunch - minBunch + 1),
-      sizes_(maxNbSources_ * nBcrossings_, 0U),
-      eventInfo_(),
-      minBunch_(minBunch) {}
+namespace io_v1 {
+
+  CrossingFramePlaybackInfoNew::CrossingFramePlaybackInfoNew(int minBunch, int maxBunch, unsigned int maxNbSources)
+      : maxNbSources_(maxNbSources),
+        nBcrossings_(maxBunch - minBunch + 1),
+        sizes_(maxNbSources_ * nBcrossings_, 0U),
+        eventInfo_(),
+        minBunch_(minBunch) {}
+
+}  // namespace io_v1
