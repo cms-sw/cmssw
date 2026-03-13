ba84cc98d148bb05dba65a77dad2848066bcb642
Igor <i.volobouev@ttu.edu>
2026-03-13 14:58:13 +0100

    refactoring HcalPulseDelaysPopConAnalyzer

--- CondTools/Hcal/plugins/HcalPulseDelaysPopConAnalyzer.cc
+++ CondTools/Hcal/plugins/HcalPulseDelaysPopConAnalyzer.cc
@@ -1,38 +1,8 @@
-#include "CondCore/PopCon/interface/PopConAnalyzer.h"
 #include "CondFormats/DataRecord/interface/HcalPulseDelaysRcd.h"
+#include "CondTools/Hcal/interface/EventSetupPayloadPopConAnalyzer.h"
 #include "CondTools/Hcal/interface/HcalPulseDelaysHandler.h"
 #include "FWCore/Framework/interface/MakerMacros.h"
 
-class HcalPulseDelaysPopConAnalyzer : public popcon::PopConAnalyzer<HcalPulseDelaysHandler> {
-public:
-  typedef HcalPulseDelaysHandler SourceHandler;
-
-  HcalPulseDelaysPopConAnalyzer(const edm::ParameterSet& pset)
-      : popcon::PopConAnalyzer<HcalPulseDelaysHandler>(pset),
-        m_populator(pset),
-        m_source(pset.getParameter<edm::ParameterSet>("Source")),
-        m_tok(esConsumes<HcalPulseDelays, HcalPulseDelaysRcd>()) {}
-
-private:
-  void endJob() override {
-    m_source.initObject(myDBObject);
-    write();
-  }
-
-  void analyze(const edm::Event& ev, const edm::EventSetup& esetup) override {
-    //Using ES to get the data:
-
-    myDBObject = new HcalPulseDelays(esetup.getData(m_tok));
-  }
-
-  void write() { m_populator.write(m_source); }
-
-private:
-  popcon::PopCon m_populator;
-  SourceHandler m_source;
-  edm::ESGetToken<HcalPulseDelays, HcalPulseDelaysRcd> m_tok;
-
-  HcalPulseDelays* myDBObject;
-};
+typedef EventSetupPayloadPopConAnalyzer<HcalPulseDelaysHandler, HcalPulseDelaysRcd> HcalPulseDelaysPopConAnalyzer;
 
 DEFINE_FWK_MODULE(HcalPulseDelaysPopConAnalyzer);
