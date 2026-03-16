da9e6954b2829aabcffc91e0b99983c42e5e2f87
Christopher Jones <chrisdjones15@gmail.com>
2026-03-16 12:55:41 -0500

    Moved stored products in L1DTTrackFinder to versioned namespace

--- DataFormats/L1DTTrackFinder/interface/L1MuDTChambPhContainerFwd.h
+++ DataFormats/L1DTTrackFinder/interface/L1MuDTChambPhContainerFwd.h
@@ -1,6 +1,8 @@
 #ifndef DataFormats_L1DTTrackFinder_L1MuDTChambPhContainerFwd_h
 #define DataFormats_L1DTTrackFinder_L1MuDTChambPhContainerFwd_h
 
-class L1MuDTChambPhContainer;
-
+namespace io_v1 {
+  class L1MuDTChambPhContainer;
+}
+using L1MuDTChambPhContainer = io_v1::L1MuDTChambPhContainer;
 #endif
