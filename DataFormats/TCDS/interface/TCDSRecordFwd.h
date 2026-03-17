259f35e4a6eb412ae4a22eb09270c1c50223867d
Christopher Jones <chrisdjones15@gmail.com>
2026-03-17 14:28:39 -0500

    Moved TCDSRecord to versioned namespace

--- DataFormats/TCDS/interface/TCDSRecordFwd.h
+++ DataFormats/TCDS/interface/TCDSRecordFwd.h
@@ -1,6 +1,9 @@
 #ifndef DataFormats_TCDS_TCDSRecordFwd_h
 #define DataFormats_TCDS_TCDSRecordFwd_h
 
-class TCDSRecord;
+namespace io_v1 {
+  class TCDSRecord;
+}
+using TCDSRecord = io_v1::TCDSRecord;
 
 #endif
