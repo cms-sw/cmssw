ea3ef3424891ac23a51319b0d7c042ef994f9b44
brusale <brusale11@gmail.com>
2026-03-18 17:35:30 +0100

    add HLT modules and sequences tracksters in barrel
    Introducing modules and sequences that are necessary to run CLUE3D in the barrel.
    Only working when using the ticl_barrel process modifier.

---
+++ HLTrigger/Configuration/python/HLT_75e33/modules/hltFilteredLayerClustersCLUE3DBarrel_cfi.py
@@ -0,0 +1,14 @@
+import FWCore.ParameterSet.Config as cms
+
+hltFilteredLayerClustersCLUE3DBarrel = cms.EDProducer("FilteredLayerClustersProducer",
+    LayerClusters = cms.InputTag("hltMergeLayerClusters"),
+    LayerClustersInputMask = cms.InputTag("hltMergeLayerClusters", "InitialLayerClustersMask"),
+    clusterFilter = cms.string('ClusterFilterByAlgo'),
+    iteration_label = cms.string('CLUE3DBarrel'),
+    max_cluster_size = cms.int32(9999),
+    max_layerId = cms.int32(9999),
+    mightGet = cms.optional.untracked.vstring,
+    min_cluster_size = cms.int32(0),
+    min_layerId = cms.int32(0),
+    algo_number = cms.vint32(10, 11)
+)
