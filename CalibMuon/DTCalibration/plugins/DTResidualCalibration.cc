11d5e2aae4dc5e4ad68a79fa3e12385db600c713
Andrey Pozdnyakov <andreypz@gmail.com>
2026-03-19 16:47:40 +0100

    Run code format

--- CalibMuon/DTCalibration/plugins/DTResidualCalibration.cc
+++ CalibMuon/DTCalibration/plugins/DTResidualCalibration.cc
@@ -40,7 +40,6 @@ DTResidualCalibration::DTResidualCalibration(const edm::ParameterSet& pset)
   select_ = new DTSegmentSelector(pset, collector);
 
   LogDebug("Calibration") << "[DTResidualCalibration] Constructor called.";
-  
   std::string rootFileName = pset.getUntrackedParameter<std::string>("rootFileName", "residuals.root");
   rootFile_ = new TFile(rootFileName.c_str(), "RECREATE");
   rootFile_->cd();
@@ -61,8 +60,6 @@ DTResidualCalibration::~DTResidualCalibration() {
 void DTResidualCalibration::beginJob() { TH1::SetDefaultSumw2(true); }
 
 void DTResidualCalibration::beginRun(const edm::Run& run, const edm::EventSetup& setup) {
-
-  
   // get the geometry
   edm::ESHandle<DTGeometry> dtGeomH;
   dtGeomH = setup.getHandle(dtGeomToken_);
@@ -238,7 +235,7 @@ void DTResidualCalibration::bookHistos(DTSuperLayerId slId) {
                               -histRange_,
                               histRange_);
   histosTH1F->SetDirectory(sectorDir);
-  
+
   TH2F* histosTH2F = new TH2F(("hResDistVsDist" + slHistoName).c_str(),
                               "Residuals on the dist. (cm) from wire (rec_hit - segm_extr) vs dist. (cm)",
                               100,
@@ -312,7 +309,7 @@ void DTResidualCalibration::bookHistos(DTLayerId layerId) {
 
 // Fill a set of histograms for a given SL
 void DTResidualCalibration::fillHistos(DTSuperLayerId slId, float distance, float residualOnDistance) {
-  LogDebug("Calibration")<< "Filling the histogram "<< slId <<" with res=" <<residualOnDistance;
+  LogDebug("Calibration") << "Filling the histogram " << slId << " with res=" << residualOnDistance;
   histoMapTH1F_[slId]->Fill(residualOnDistance);
   histoMapTH2F_[slId]->Fill(distance, residualOnDistance);
 }
