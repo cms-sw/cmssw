39739f4507093a125ec57ea2292a297e968df703
ctdax <colbythomps@gmail.com>
2026-03-20 21:35:56 +0100

    Removed changes to MicroEventContent and SimG4Core_EventContent

--- SimG4Core/Configuration/python/SimG4Core_EventContent_cff.py
+++ SimG4Core/Configuration/python/SimG4Core_EventContent_cff.py
@@ -3,24 +3,21 @@ import FWCore.ParameterSet.Config as cms
 #RAW content 
 SimG4CoreRAW = cms.PSet(
     outputCommands = cms.untracked.vstring('keep *_g4SimHits_*_*', 
-        'keep edmHepMCProduct_source_*_*',
-        'keep recoGenParticles_RHDecayTracer_RHadronDecay_SIM')
+        'keep edmHepMCProduct_source_*_*')
 )
 #RECO content
 SimG4CoreRECO = cms.PSet(
     outputCommands = cms.untracked.vstring('keep edmHepMCProduct_source_*_*', 
         'keep SimTracks_g4SimHits_*_*', 
-        'keep SimVertexs_g4SimHits_*_*',
-        'keep recoGenParticles_RHDecayTracer_RHadronDecay_SIM')
+        'keep SimVertexs_g4SimHits_*_*')
 )
 #AOD content
 SimG4CoreAOD = cms.PSet(
-    outputCommands = cms.untracked.vstring('keep recoGenParticles_RHDecayTracer_RHadronDecay_SIM')
+    outputCommands = cms.untracked.vstring()
 )
 #HLTAODSIM content
 SimG4CoreHLTAODSIM = cms.PSet(
     outputCommands = cms.untracked.vstring(
-'keep SimVertexs_g4SimHits_*_*',
-'keep recoGenParticles_RHDecayTracer_RHadronDecay_SIM')
+'keep SimVertexs_g4SimHits_*_*')
 )
 
