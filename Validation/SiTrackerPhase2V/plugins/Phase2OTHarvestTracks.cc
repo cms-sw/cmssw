ee2022cb56e717296d2849c2bbac5859b33babb9
Brandi Skipworth <bskipwo1@vols.utk.edu>
2026-03-23 22:56:13 +0100

    updates from running code-format

--- Validation/SiTrackerPhase2V/plugins/Phase2OTHarvestTracks.cc
+++ Validation/SiTrackerPhase2V/plugins/Phase2OTHarvestTracks.cc
@@ -4,8 +4,10 @@
 /**
  * This class is part of the Phase 2 Tracker validation framework and performs
  * the harvesting step for L1 tracks validation. It processes histograms
- * created during the earlier validation steps to calculate efficiencies and
- * resolutions for tracking performance.
+ * created during the earlier validation steps to calculate resolutions for
+ * tracking performance. Specifically it extracts standard deviations from
+ * resolution ingredients for Nominal, Extended Prompt, and Extended Displaced
+ * L1 tracks.
  *
  * Usage:
  * To generate histograms from this code, run the test configuration files
@@ -13,7 +15,7 @@
  * can then be analyzed or visualized.
  */
 
-// Updated by: Brandi Skipworth, 2025
+// Updated by: Brandi Skipworth, 2026
 
 #include <algorithm>
 #include <vector>
@@ -51,75 +53,180 @@ void Phase2OTHarvestTracks::dqmEndJob(DQMStore::IBooker &ibooker, DQMStore::IGet
   float eta_bins[] = {0.0, 0.7, 1.0, 1.2, 1.6, 2.0, 2.4};
   int eta_binnum = 6;
   std::string eta_ranges[6] = {"eta0to0p7", "eta0p7to1", "eta1to1p2", "eta1p2to1p6", "eta1p6to2", "eta2to2p4"};
-  
+
   // nominal collection
   std::vector<MonitorElement *> respt_pt2to3(6, nullptr), respt_pt3to8(6, nullptr), respt_pt8toInf(6, nullptr);
-  std::vector<MonitorElement *> mereseta_vect(6, nullptr), meresphi_vect(6, nullptr), meresz0_vect(6, nullptr), meresd0_vect(6, nullptr);
+  std::vector<MonitorElement *> mereseta_vect(6, nullptr), meresphi_vect(6, nullptr), meresz0_vect(6, nullptr),
+      meresd0_vect(6, nullptr);
 
-  std::vector<MonitorElement *> prompt_respt_pt2to3(6, nullptr), prompt_respt_pt3to8(6, nullptr), prompt_respt_pt8toInf(6, nullptr);
-  std::vector<MonitorElement *> prompt_mereseta_vect(6, nullptr), prompt_meresphi_vect(6, nullptr), prompt_meresz0_vect(6, nullptr), prompt_meresd0_vect(6, nullptr);
+  std::vector<MonitorElement *> prompt_respt_pt2to3(6, nullptr), prompt_respt_pt3to8(6, nullptr),
+      prompt_respt_pt8toInf(6, nullptr);
+  std::vector<MonitorElement *> prompt_mereseta_vect(6, nullptr), prompt_meresphi_vect(6, nullptr),
+      prompt_meresz0_vect(6, nullptr), prompt_meresd0_vect(6, nullptr);
 
-  std::vector<MonitorElement *> displaced_respt_pt2to3(6, nullptr), displaced_respt_pt3to8(6, nullptr), displaced_respt_pt8toInf(6, nullptr);
-  std::vector<MonitorElement *> displaced_mereseta_vect(6, nullptr), displaced_meresphi_vect(6, nullptr), displaced_meresz0_vect(6, nullptr), displaced_meresd0_vect(6, nullptr);
+  std::vector<MonitorElement *> displaced_respt_pt2to3(6, nullptr), displaced_respt_pt3to8(6, nullptr),
+      displaced_respt_pt8toInf(6, nullptr);
+  std::vector<MonitorElement *> displaced_mereseta_vect(6, nullptr), displaced_meresphi_vect(6, nullptr),
+      displaced_meresz0_vect(6, nullptr), displaced_meresd0_vect(6, nullptr);
 
   for (int i = 0; i < 6; i++) {
     std::string resIng = topFolderName_ + "/Nominal_L1TF/ResolutionIngredients/";
-    respt_pt2to3[i]   = igetter.get(resIng + "respt_" + eta_ranges[i] + "_pt2to3");
-    respt_pt3to8[i]   = igetter.get(resIng + "respt_" + eta_ranges[i] + "_pt3to8");
+    respt_pt2to3[i] = igetter.get(resIng + "respt_" + eta_ranges[i] + "_pt2to3");
+    respt_pt3to8[i] = igetter.get(resIng + "respt_" + eta_ranges[i] + "_pt3to8");
     respt_pt8toInf[i] = igetter.get(resIng + "respt_" + eta_ranges[i] + "_pt8toInf");
-    mereseta_vect[i]  = igetter.get(resIng + "reseta_" + eta_ranges[i]);
-    meresphi_vect[i]  = igetter.get(resIng + "resphi_" + eta_ranges[i]);
-    meresz0_vect[i]   = igetter.get(resIng + "resz0_" + eta_ranges[i]);
-    meresd0_vect[i]   = igetter.get(resIng + "resd0_" + eta_ranges[i]);
+    mereseta_vect[i] = igetter.get(resIng + "reseta_" + eta_ranges[i]);
+    meresphi_vect[i] = igetter.get(resIng + "resphi_" + eta_ranges[i]);
+    meresz0_vect[i] = igetter.get(resIng + "resz0_" + eta_ranges[i]);
+    meresd0_vect[i] = igetter.get(resIng + "resd0_" + eta_ranges[i]);
 
     std::string promptIng = topFolderName_ + "/Extended_L1TF/Prompt/ResolutionIngredients/";
-    prompt_respt_pt2to3[i]   = igetter.get(promptIng + "respt_prompt_" + eta_ranges[i] + "_pt2to3");
-    prompt_respt_pt3to8[i]   = igetter.get(promptIng + "respt_prompt_" + eta_ranges[i] + "_pt3to8");
+    prompt_respt_pt2to3[i] = igetter.get(promptIng + "respt_prompt_" + eta_ranges[i] + "_pt2to3");
+    prompt_respt_pt3to8[i] = igetter.get(promptIng + "respt_prompt_" + eta_ranges[i] + "_pt3to8");
     prompt_respt_pt8toInf[i] = igetter.get(promptIng + "respt_prompt_" + eta_ranges[i] + "_pt8toInf");
-    prompt_mereseta_vect[i]  = igetter.get(promptIng + "reseta_prompt_" + eta_ranges[i]);
-    prompt_meresphi_vect[i]  = igetter.get(promptIng + "resphi_prompt_" + eta_ranges[i]);
-    prompt_meresz0_vect[i]   = igetter.get(promptIng + "resz0_prompt_" + eta_ranges[i]);
-    prompt_meresd0_vect[i]   = igetter.get(promptIng + "resd0_prompt_" + eta_ranges[i]);
+    prompt_mereseta_vect[i] = igetter.get(promptIng + "reseta_prompt_" + eta_ranges[i]);
+    prompt_meresphi_vect[i] = igetter.get(promptIng + "resphi_prompt_" + eta_ranges[i]);
+    prompt_meresz0_vect[i] = igetter.get(promptIng + "resz0_prompt_" + eta_ranges[i]);
+    prompt_meresd0_vect[i] = igetter.get(promptIng + "resd0_prompt_" + eta_ranges[i]);
 
     std::string dispIng = topFolderName_ + "/Extended_L1TF/Displaced/ResolutionIngredients/";
-    displaced_respt_pt2to3[i]   = igetter.get(dispIng + "respt_displaced_" + eta_ranges[i] + "_pt2to3");
-    displaced_respt_pt3to8[i]   = igetter.get(dispIng + "respt_displaced_" + eta_ranges[i] + "_pt3to8");
+    displaced_respt_pt2to3[i] = igetter.get(dispIng + "respt_displaced_" + eta_ranges[i] + "_pt2to3");
+    displaced_respt_pt3to8[i] = igetter.get(dispIng + "respt_displaced_" + eta_ranges[i] + "_pt3to8");
     displaced_respt_pt8toInf[i] = igetter.get(dispIng + "respt_displaced_" + eta_ranges[i] + "_pt8toInf");
-    displaced_mereseta_vect[i]  = igetter.get(dispIng + "reseta_displaced_" + eta_ranges[i]);
-    displaced_meresphi_vect[i]  = igetter.get(dispIng + "resphi_displaced_" + eta_ranges[i]);
-    displaced_meresz0_vect[i]   = igetter.get(dispIng + "resz0_displaced_" + eta_ranges[i]);
-    displaced_meresd0_vect[i]   = igetter.get(dispIng + "resd0_displaced_" + eta_ranges[i]);
+    displaced_mereseta_vect[i] = igetter.get(dispIng + "reseta_displaced_" + eta_ranges[i]);
+    displaced_meresphi_vect[i] = igetter.get(dispIng + "resphi_displaced_" + eta_ranges[i]);
+    displaced_meresz0_vect[i] = igetter.get(dispIng + "resz0_displaced_" + eta_ranges[i]);
+    displaced_meresd0_vect[i] = igetter.get(dispIng + "resd0_displaced_" + eta_ranges[i]);
   }
 
   // Final Resolutions - Nominal
   ibooker.setCurrentFolder(topFolderName_ + "/Nominal_L1TF/FinalResolution");
-  phase2tkutil::fillResolutionFromVec(respt_pt2to3,   ibooker.book1D("pTResVsEta_2-3",   ";|#eta|;#sigma(#Delta p_{T}/p_{T})", eta_binnum, eta_bins), "");
-  phase2tkutil::fillResolutionFromVec(respt_pt3to8,   ibooker.book1D("pTResVsEta_3-8",   ";|#eta|;#sigma(#Delta p_{T}/p_{T})", eta_binnum, eta_bins), "");
-  phase2tkutil::fillResolutionFromVec(respt_pt8toInf, ibooker.book1D("pTResVsEta_8-inf", ";|#eta|;#sigma(#Delta p_{T}/p_{T})", eta_binnum, eta_bins), "");
-  phase2tkutil::fillResolutionFromVec(mereseta_vect,  ibooker.book1D("EtaResolution",    ";|#eta|;#sigma(#Delta#eta)",         eta_binnum, eta_bins), "");
-  phase2tkutil::fillResolutionFromVec(meresphi_vect,  ibooker.book1D("PhiResolution",    ";|#eta|;#sigma(#Delta#phi)",         eta_binnum, eta_bins), "");
-  phase2tkutil::fillResolutionFromVec(meresz0_vect,   ibooker.book1D("z0Resolution",     ";|#eta|;#sigma(#Deltaz0) [cm]",      eta_binnum, eta_bins), "");
-  phase2tkutil::fillResolutionFromVec(meresd0_vect,   ibooker.book1D("d0Resolution",     ";|#eta|;#sigma(#Deltad_{0}) [cm]",   eta_binnum, eta_bins), "");
+  phase2tkutil::fillResolutionFromVec(
+      respt_pt2to3,
+      ibooker.book1D(
+          "pTResVsEta_2-3", "pT resolution vs |#eta| (2-3 GeV);|#eta|;#sigma(#Delta p_{T}/p_{T})", eta_binnum, eta_bins),
+      "");
+  phase2tkutil::fillResolutionFromVec(
+      respt_pt3to8,
+      ibooker.book1D(
+          "pTResVsEta_3-8", "pT resolution vs |#eta| (3-8 GeV);|#eta|;#sigma(#Delta p_{T}/p_{T})", eta_binnum, eta_bins),
+      "");
+  phase2tkutil::fillResolutionFromVec(
+      respt_pt8toInf,
+      ibooker.book1D("pTResVsEta_8-inf",
+                     "pT resolution vs |#eta| (8-inf GeV);|#eta|;#sigma(#Delta p_{T}/p_{T})",
+                     eta_binnum,
+                     eta_bins),
+      "");
+  phase2tkutil::fillResolutionFromVec(
+      mereseta_vect,
+      ibooker.book1D("EtaResolution", "#eta resolution vs |#eta|;|#eta|;#sigma(#Delta#eta)", eta_binnum, eta_bins),
+      "");
+  phase2tkutil::fillResolutionFromVec(
+      meresphi_vect,
+      ibooker.book1D("PhiResolution", "#phi resolution vs |#eta|;|#eta|;#sigma(#Delta#phi)", eta_binnum, eta_bins),
+      "");
+  phase2tkutil::fillResolutionFromVec(
+      meresz0_vect,
+      ibooker.book1D("z0Resolution", "z0 resolution vs |#eta|;|#eta|;#sigma(#Deltaz0) [cm]", eta_binnum, eta_bins),
+      "");
+  phase2tkutil::fillResolutionFromVec(
+      meresd0_vect,
+      ibooker.book1D("d0Resolution", "d0 resolution vs |#eta|;|#eta|;#sigma(#Deltad_{0}) [cm]", eta_binnum, eta_bins),
+      "");
 
   // Final Resolutions - Prompt
   ibooker.setCurrentFolder(topFolderName_ + "/Extended_L1TF/Prompt/FinalResolution");
-  phase2tkutil::fillResolutionFromVec(prompt_respt_pt2to3,   ibooker.book1D("pTResVsEta_2-3_prompt",   ";|#eta|;#sigma(#Delta p_{T}/p_{T})", eta_binnum, eta_bins), "");
-  phase2tkutil::fillResolutionFromVec(prompt_respt_pt3to8,   ibooker.book1D("pTResVsEta_3-8_prompt",   ";|#eta|;#sigma(#Delta p_{T}/p_{T})", eta_binnum, eta_bins), "");
-  phase2tkutil::fillResolutionFromVec(prompt_respt_pt8toInf, ibooker.book1D("pTResVsEta_8-inf_prompt", ";|#eta|;#sigma(#Delta p_{T}/p_{T})", eta_binnum, eta_bins), "");
-  phase2tkutil::fillResolutionFromVec(prompt_mereseta_vect,  ibooker.book1D("EtaResolution_prompt",    ";|#eta|;#sigma(#Delta#eta)",         eta_binnum, eta_bins), "");
-  phase2tkutil::fillResolutionFromVec(prompt_meresphi_vect,  ibooker.book1D("PhiResolution_prompt",    ";|#eta|;#sigma(#Delta#phi)",         eta_binnum, eta_bins), "");
-  phase2tkutil::fillResolutionFromVec(prompt_meresz0_vect,   ibooker.book1D("z0Resolution_prompt",     ";|#eta|;#sigma(#Deltaz0) [cm]",      eta_binnum, eta_bins), "");
-  phase2tkutil::fillResolutionFromVec(prompt_meresd0_vect,   ibooker.book1D("d0Resolution_prompt",     ";|#eta|;#sigma(#Deltad_{0}) [cm]",   eta_binnum, eta_bins), "");
+  phase2tkutil::fillResolutionFromVec(
+      prompt_respt_pt2to3,
+      ibooker.book1D("pTResVsEta_2-3_prompt",
+                     "Prompt pT resolution vs |#eta| (2-3 GeV);|#eta|;#sigma(#Delta p_{T}/p_{T})",
+                     eta_binnum,
+                     eta_bins),
+      "");
+  phase2tkutil::fillResolutionFromVec(
+      prompt_respt_pt3to8,
+      ibooker.book1D("pTResVsEta_3-8_prompt",
+                     "Prompt pT resolution vs |#eta| (3-8 GeV);|#eta|;#sigma(#Delta p_{T}/p_{T})",
+                     eta_binnum,
+                     eta_bins),
+      "");
+  phase2tkutil::fillResolutionFromVec(
+      prompt_respt_pt8toInf,
+      ibooker.book1D("pTResVsEta_8-inf_prompt",
+                     "Prompt pT resolution vs |#eta| (8-inf GeV);|#eta|;#sigma(#Delta p_{T}/p_{T})",
+                     eta_binnum,
+                     eta_bins),
+      "");
+  phase2tkutil::fillResolutionFromVec(
+      prompt_mereseta_vect,
+      ibooker.book1D(
+          "EtaResolution_prompt", "Prompt #eta resolution vs |#eta|;|#eta|;#sigma(#Delta#eta)", eta_binnum, eta_bins),
+      "");
+  phase2tkutil::fillResolutionFromVec(
+      prompt_meresphi_vect,
+      ibooker.book1D(
+          "PhiResolution_prompt", "Prompt #phi resolution vs |#eta|;|#eta|;#sigma(#Delta#phi)", eta_binnum, eta_bins),
+      "");
+  phase2tkutil::fillResolutionFromVec(
+      prompt_meresz0_vect,
+      ibooker.book1D(
+          "z0Resolution_prompt", "Prompt z0 resolution vs |#eta|;|#eta|;#sigma(#Deltaz0) [cm]", eta_binnum, eta_bins),
+      "");
+  phase2tkutil::fillResolutionFromVec(prompt_meresd0_vect,
+                                      ibooker.book1D("d0Resolution_prompt",
+                                                     "Prompt d0 resolution vs |#eta|;|#eta|;#sigma(#Deltad_{0}) [cm]",
+                                                     eta_binnum,
+                                                     eta_bins),
+                                      "");
 
   // Final Resolutions - Displaced
   ibooker.setCurrentFolder(topFolderName_ + "/Extended_L1TF/Displaced/FinalResolution");
-  phase2tkutil::fillResolutionFromVec(displaced_respt_pt2to3,   ibooker.book1D("pTResVsEta_2-3_displaced",   ";|#eta|;#sigma(#Delta p_{T}/p_{T})", eta_binnum, eta_bins), "");
-  phase2tkutil::fillResolutionFromVec(displaced_respt_pt3to8,   ibooker.book1D("pTResVsEta_3-8_displaced",   ";|#eta|;#sigma(#Delta p_{T}/p_{T})", eta_binnum, eta_bins), "");
-  phase2tkutil::fillResolutionFromVec(displaced_respt_pt8toInf, ibooker.book1D("pTResVsEta_8-inf_displaced", ";|#eta|;#sigma(#Delta p_{T}/p_{T})", eta_binnum, eta_bins), "");
-  phase2tkutil::fillResolutionFromVec(displaced_mereseta_vect,  ibooker.book1D("EtaResolution_displaced",    ";|#eta|;#sigma(#Delta#eta)",         eta_binnum, eta_bins), "");
-  phase2tkutil::fillResolutionFromVec(displaced_meresphi_vect,  ibooker.book1D("PhiResolution_displaced",    ";|#eta|;#sigma(#Delta#phi)",         eta_binnum, eta_bins), "");
-  phase2tkutil::fillResolutionFromVec(displaced_meresz0_vect,   ibooker.book1D("z0Resolution_displaced",     ";|#eta|;#sigma(#Deltaz0) [cm]",      eta_binnum, eta_bins), "");
-  phase2tkutil::fillResolutionFromVec(displaced_meresd0_vect,   ibooker.book1D("d0Resolution_displaced",     ";|#eta|;#sigma(#Deltad_{0}) [cm]",   eta_binnum, eta_bins), "");
+  phase2tkutil::fillResolutionFromVec(
+      displaced_respt_pt2to3,
+      ibooker.book1D("pTResVsEta_2-3_displaced",
+                     "Displaced pT resolution vs |#eta| (2-3 GeV);|#eta|;#sigma(#Delta p_{T}/p_{T})",
+                     eta_binnum,
+                     eta_bins),
+      "");
+  phase2tkutil::fillResolutionFromVec(
+      displaced_respt_pt3to8,
+      ibooker.book1D("pTResVsEta_3-8_displaced",
+                     "Displaced pT resolution vs |#eta| (3-8 GeV);|#eta|;#sigma(#Delta p_{T}/p_{T})",
+                     eta_binnum,
+                     eta_bins),
+      "");
+  phase2tkutil::fillResolutionFromVec(
+      displaced_respt_pt8toInf,
+      ibooker.book1D("pTResVsEta_8-inf_displaced",
+                     "Displaced pT resolution vs |#eta| (8-inf GeV);|#eta|;#sigma(#Delta p_{T}/p_{T})",
+                     eta_binnum,
+                     eta_bins),
+      "");
+  phase2tkutil::fillResolutionFromVec(displaced_mereseta_vect,
+                                      ibooker.book1D("EtaResolution_displaced",
+                                                     "Displaced #eta resolution vs |#eta|;|#eta|;#sigma(#Delta#eta)",
+                                                     eta_binnum,
+                                                     eta_bins),
+                                      "");
+  phase2tkutil::fillResolutionFromVec(displaced_meresphi_vect,
+                                      ibooker.book1D("PhiResolution_displaced",
+                                                     "Displaced #phi resolution vs |#eta|;|#eta|;#sigma(#Delta#phi)",
+                                                     eta_binnum,
+                                                     eta_bins),
+                                      "");
+  phase2tkutil::fillResolutionFromVec(displaced_meresz0_vect,
+                                      ibooker.book1D("z0Resolution_displaced",
+                                                     "Displaced z0 resolution vs |#eta|;|#eta|;#sigma(#Deltaz0) [cm]",
+                                                     eta_binnum,
+                                                     eta_bins),
+                                      "");
+  phase2tkutil::fillResolutionFromVec(
+      displaced_meresd0_vect,
+      ibooker.book1D("d0Resolution_displaced",
+                     "Displaced d0 resolution vs |#eta|;|#eta|;#sigma(#Deltad_{0}) [cm]",
+                     eta_binnum,
+                     eta_bins),
+      "");
 }  // end dqmEndJob
 
 #include "FWCore/ParameterSet/interface/ConfigurationDescriptions.h"
