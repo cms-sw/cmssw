e9effa9dea48120509bd29802ff55341a26f8b0d
Andrea Bocci <andrea.bocci@cern.ch>
2026-04-01 19:11:48 +0200

    Add an EDMetadataSentry constructor with an explicit queue

--- HeterogeneousCore/AlpakaCore/src/alpaka/EDMetadataSentry.cc
+++ HeterogeneousCore/AlpakaCore/src/alpaka/EDMetadataSentry.cc
@@ -1,9 +1,16 @@
+#include <alpaka/alpaka.hpp>
+
 #include "HeterogeneousCore/AlpakaCore/interface/alpaka/EDMetadataSentry.h"
 #include "HeterogeneousCore/AlpakaCore/interface/alpaka/chooseDevice.h"
 #include "HeterogeneousCore/AlpakaInterface/interface/QueueCache.h"
 
 namespace ALPAKA_ACCELERATOR_NAMESPACE {
   namespace detail {
+
+    EDMetadataSentry::EDMetadataSentry(std::shared_ptr<Queue> queue, bool synchronize) : synchronize_(synchronize) {
+      assert(queue);
+      metadata_ = std::make_shared<EDMetadata>(std::move(queue));
+    }
 
     EDMetadataSentry::EDMetadataSentry(edm::StreamID streamID, bool synchronize) : synchronize_(synchronize) {
       auto const& device = detail::chooseDevice(streamID);
