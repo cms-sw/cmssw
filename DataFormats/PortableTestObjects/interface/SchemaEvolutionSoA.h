dcb53215cc82037644fdd18001a98e8c72370ed8
markus <markus.holzer@cern.ch>
2026-04-02 12:58:11 +0200

    Store enum as integer in SoA backend

--- DataFormats/PortableTestObjects/interface/SchemaEvolutionSoA.h
+++ DataFormats/PortableTestObjects/interface/SchemaEvolutionSoA.h
@@ -11,24 +11,30 @@
 namespace portabletest {
 
   using SEArray = edm::StdArray<int, 3>;
-  using SEEigenVector = Eigen::Matrix<int64_t, 3, 3>;
-  /*struct SiPixelErrorCompact {
-  uint32_t rawId;
-  uint32_t word;
+  using SEEigenVector = Eigen::Matrix<float, 4, 1>;
+  enum class PixelType : int64_t { s1 = 0, s2 = 1, s3 = 2 };
+  template<typename T>
+  struct SiPixelErrorCompact {
+  T rawId;
+  T word;
   uint8_t errorType;
   double fedId;
-};*/
+};
+
+using SiPixelErrorCompact64 = SiPixelErrorCompact<uint32_t>;
 
   GENERATE_SOA_LAYOUT(SchemaEvolutionSoATemplate,
                       SOA_COLUMN(float, cOneFloat),
                       SOA_COLUMN(int, cTwoInt),
                       SOA_COLUMN(double, cThreeDouble),
-                      // SOA_COLUMN(SiPixelErrorCompact, pixelErrors),
                       SOA_COLUMN(SEArray, cFourArray),
+                      SOA_COLUMN(PixelType, cpixelType),
+                      SOA_COLUMN(SiPixelErrorCompact64, customStruct),
                       SOA_EIGEN_COLUMN(SEEigenVector, eOneVector3d),
                       SOA_SCALAR(int, sOneInt),
                       SOA_SCALAR(float, sTwoFloat),
-                      SOA_SCALAR(double, sThreeDouble));
+                      SOA_SCALAR(double, sThreeDouble),
+                      SOA_SCALAR(PixelType, sPixelType));
 
   using SchemaEvolutionSoA = SchemaEvolutionSoATemplate<>;
   using SchemaEvolutionSoAConstView = SchemaEvolutionSoA::ConstView;
--- DataFormats/PortableTestObjects/plugins/SchemaEvolutionProducer.cc
+++ DataFormats/PortableTestObjects/plugins/SchemaEvolutionProducer.cc
@@ -28,7 +28,7 @@ void SchemaEvolutionProducer::produce(edm::StreamID iID, edm::Event& event, cons
       std::make_unique<portabletest::SchemaEvolutionHostCollection>(cms::alpakatools::host(), elems);
   auto& view = SchemaEvolutionProduct->view();
 
-  portabletest::SEEigenVector::Scalar counter = 0;
+  // portabletest::SEEigenVector::Scalar counter = 0;
   for (int i = 0; i < view.metadata().size(); i++) {
     auto element = view[i];
     element.cOneFloat() = static_cast<float>(i) + 0.1f;
@@ -39,15 +39,20 @@ void SchemaEvolutionProducer::produce(edm::StreamID iID, edm::Event& event, cons
 
     for (int r = 0; r < portabletest::SEEigenVector::RowsAtCompileTime; ++r) {
       for (int c = 0; c < portabletest::SEEigenVector::ColsAtCompileTime; ++c) {
-        inputVec(r, c) = static_cast<portabletest::SEEigenVector::Scalar>(counter++);
+        inputVec(r, c) = static_cast<portabletest::SEEigenVector::Scalar>(r);
       }
     }
+    // element.compact() = portabletest::SiPixelErrorCompact64{1, 2, 3, 4.0};
     element.eOneVector3d() = inputVec;
     element.cFourArray() = portabletest::SEArray{{i, i + 1, i + 2}};
+    element.cpixelType() = portabletest::PixelType::s3;
   }
+  std::cout << "Sizeof SiPixelErrorCompact64 " << sizeof(portabletest::SiPixelErrorCompact64) << std::endl;
+
   view.sOneInt() = 42;
   view.sTwoFloat() = 1.0f / 3.0f;
   view.sThreeDouble() = 1.0 / 10.0;
+  view.sPixelType() = portabletest::PixelType::s2;
 
   event.put(std::move(SchemaEvolutionProduct), "SchemaEvolutionProduct");
 }
