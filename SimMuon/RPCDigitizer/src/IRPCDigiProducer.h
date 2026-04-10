a251a7ed936e0f13f97d27234bb64bfc8396e7e0
bapavlov <Borislav.Pavlov@cern.ch>
2026-04-10 18:53:14 +0300

    Update IRPCDigiProducer.h to include edm::stream::WatchRuns

--- SimMuon/RPCDigitizer/src/IRPCDigiProducer.h
+++ SimMuon/RPCDigitizer/src/IRPCDigiProducer.h
@@ -20,7 +20,7 @@ class RPCGeometry;
 class RPCSimSetUp;
 class RPCSynchronizer;
 
-class IRPCDigiProducer : public edm::stream::EDProducer<> {
+class IRPCDigiProducer : public edm::stream::EDProducer<edm::stream::WatchRuns> {
 public:
   //  typedef edm::DetSetVector<RPCDigiSimLink> RPCDigiSimLinks;
   typedef IRPCDigitizer::RPCDigiSimLinks IRPCDigitizerSimLinks;
