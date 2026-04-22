fe864bb81e362ba7872fd8c0cb968205df623452
Andrea Bocci <andrea.bocci@cern.ch>
2026-04-22 15:14:43 +0200

    Replace LogAbsolute with LogInfo
    Enable LogInfo MPI messages in the tests

--- HeterogeneousCore/MPICore/test/controller_soa_cfg.py
+++ HeterogeneousCore/MPICore/test/controller_soa_cfg.py
@@ -15,6 +15,12 @@ process.options.wantSummary = False
 process.source = cms.Source("EmptySource")
 process.maxEvents.input = 10
 
+process.load("FWCore.ParameterSet.MessageLogger")
+process.MessageLogger.cerr.MPI = cms.untracked.PSet(
+    reportEvery = cms.untracked.int32( 1 ),
+    limit = cms.untracked.int32( 10000000 )
+)
+
 process.load("HeterogeneousCore.MPIServices.MPIService_cfi")
 
 # produce and send a portable object, a portable collection, and some portable multicollections
