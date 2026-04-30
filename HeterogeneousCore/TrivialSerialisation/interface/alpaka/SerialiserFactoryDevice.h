9f93de60b579dcacd05af640312c367141b13bb8
Andrea Bocci <andrea.bocci@cern.ch>
2026-04-30 14:25:06 +0200

    Use __COUNTER__ instead of __LINE__ to declare unique variables

--- HeterogeneousCore/TrivialSerialisation/interface/alpaka/SerialiserFactoryDevice.h
+++ HeterogeneousCore/TrivialSerialisation/interface/alpaka/SerialiserFactoryDevice.h
@@ -19,12 +19,12 @@ namespace ALPAKA_ACCELERATOR_NAMESPACE::ngt {
 // to it (it is attached here). The plugin is registered under both the mangled
 // typeid name and EDM_STRINGIZE(TYPE). EDM_STRINGIZE(TYPE) is more
 // human-readable, and thus more suitable for Python configuration files.
-#define DEFINE_TRIVIAL_SERIALISER_PLUGIN_DEVICE(TYPE)                                                   \
-  DEFINE_EDM_PLUGIN(ALPAKA_ACCELERATOR_NAMESPACE::ngt::SerialiserFactoryDevice,                         \
-                    ALPAKA_ACCELERATOR_NAMESPACE::ngt::Serialiser<ALPAKA_ACCELERATOR_NAMESPACE::TYPE>,  \
-                    typeid(edm::DeviceProduct<ALPAKA_ACCELERATOR_NAMESPACE::TYPE>).name());             \
-  DEFINE_EDM_PLUGIN2(ALPAKA_ACCELERATOR_NAMESPACE::ngt::SerialiserFactoryDevice,                        \
-                     ALPAKA_ACCELERATOR_NAMESPACE::ngt::Serialiser<ALPAKA_ACCELERATOR_NAMESPACE::TYPE>, \
-                     EDM_STRINGIZE(TYPE))
+#define DEFINE_TRIVIAL_SERIALISER_PLUGIN_DEVICE(TYPE)                                                  \
+  DEFINE_EDM_PLUGIN(ALPAKA_ACCELERATOR_NAMESPACE::ngt::SerialiserFactoryDevice,                        \
+                    ALPAKA_ACCELERATOR_NAMESPACE::ngt::Serialiser<ALPAKA_ACCELERATOR_NAMESPACE::TYPE>, \
+                    typeid(edm::DeviceProduct<ALPAKA_ACCELERATOR_NAMESPACE::TYPE>).name());            \
+  DEFINE_EDM_PLUGIN(ALPAKA_ACCELERATOR_NAMESPACE::ngt::SerialiserFactoryDevice,                        \
+                    ALPAKA_ACCELERATOR_NAMESPACE::ngt::Serialiser<ALPAKA_ACCELERATOR_NAMESPACE::TYPE>, \
+                    EDM_STRINGIZE(TYPE))
 
 #endif  // HeterogeneousCore_TrivialSerialisation_interface_alpaka_SerialiserFactoryDevice_h
