9c08dd867b42ba073152adae5806f6c20f4fe6b5
markus <markus.holzer@cern.ch>
2026-05-05 22:16:49 +0200

    Add source location to SoA out of range error messages

--- DataFormats/SoATemplate/src/SoACommon.cc
+++ DataFormats/SoATemplate/src/SoACommon.cc
@@ -6,7 +6,12 @@
 namespace cms::soa::detail {
   [[noreturn]] void throwRuntimeError(const char* message) { throw std::runtime_error(message); }
 
-  [[noreturn]] void throwOutOfRangeError(const char* message, cms::soa::size_type index, cms::soa::size_type range) {
-    throw std::out_of_range(std::format("{}: index {} out of range {}", message, index, range));
+  [[noreturn]] void throwOutOfRangeError(const char* message, SourceIndex index, cms::soa::size_type range) {
+    throw std::out_of_range(std::format("{}: index {} out of range {} at file {} at line {} \n",
+                                        message,
+                                        index.value_,
+                                        range,
+                                        index.location_.file_name(),
+                                        index.location_.line()));
   }
 }  // namespace cms::soa::detail
