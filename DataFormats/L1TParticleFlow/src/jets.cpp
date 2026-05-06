77e08ae49fffd7edf30087d3a97874cc01d0308a
Matti Kortelainen <matti.kortelainen@cern.ch>
2026-05-06 18:44:12 -0500

    Move stored types having map data member to io_v1 namespace

--- DataFormats/L1TParticleFlow/src/jets.cpp
+++ DataFormats/L1TParticleFlow/src/jets.cpp
@@ -1,20 +1,21 @@
 #include "DataFormats/L1TParticleFlow/interface/jets.h"
 
-const std::unordered_map<std::string, l1ct::JetTagClass::JetTagClassValue> l1ct::JetTagClass::labels_ = {
-    {"b", l1ct::JetTagClass::JetTagClassValue::b},
-    {"c", l1ct::JetTagClass::JetTagClassValue::c},
-    {"uds", l1ct::JetTagClass::JetTagClassValue::uds},
-    {"g", l1ct::JetTagClass::JetTagClassValue::g},
-    {"tau_p", l1ct::JetTagClass::JetTagClassValue::tau_p},
-    {"tau_n", l1ct::JetTagClass::JetTagClassValue::tau_n},
-    {"mu", l1ct::JetTagClass::JetTagClassValue::mu},
-    {"e", l1ct::JetTagClass::JetTagClassValue::e}};
+const std::unordered_map<std::string, l1ct::io_v1::JetTagClass::JetTagClassValue> l1ct::io_v1::JetTagClass::labels_ = {
+    {"b", l1ct::io_v1::JetTagClass::JetTagClassValue::b},
+    {"c", l1ct::io_v1::JetTagClass::JetTagClassValue::c},
+    {"uds", l1ct::io_v1::JetTagClass::JetTagClassValue::uds},
+    {"g", l1ct::io_v1::JetTagClass::JetTagClassValue::g},
+    {"tau_p", l1ct::io_v1::JetTagClass::JetTagClassValue::tau_p},
+    {"tau_n", l1ct::io_v1::JetTagClass::JetTagClassValue::tau_n},
+    {"mu", l1ct::io_v1::JetTagClass::JetTagClassValue::mu},
+    {"e", l1ct::io_v1::JetTagClass::JetTagClassValue::e}};
 
-const l1ct::JetTagClass l1ct::JetTagClassHandler::tagClassesDefault_[NTagFields] = {l1ct::JetTagClass("b"),
-                                                                                    l1ct::JetTagClass("c"),
-                                                                                    l1ct::JetTagClass("uds"),
-                                                                                    l1ct::JetTagClass("g"),
-                                                                                    l1ct::JetTagClass("tau_p"),
-                                                                                    l1ct::JetTagClass("tau_n"),
-                                                                                    l1ct::JetTagClass("mu"),
-                                                                                    l1ct::JetTagClass("e")};
+const l1ct::io_v1::JetTagClass l1ct::io_v1::JetTagClassHandler::tagClassesDefault_[NTagFields] = {
+    l1ct::io_v1::JetTagClass("b"),
+    l1ct::io_v1::JetTagClass("c"),
+    l1ct::io_v1::JetTagClass("uds"),
+    l1ct::io_v1::JetTagClass("g"),
+    l1ct::io_v1::JetTagClass("tau_p"),
+    l1ct::io_v1::JetTagClass("tau_n"),
+    l1ct::io_v1::JetTagClass("mu"),
+    l1ct::io_v1::JetTagClass("e")};
