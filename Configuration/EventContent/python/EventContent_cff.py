60e32a36062de4004de0a29a17adb3cf8023492c
Elena Vernazza <elena.vernazza@cern.ch>
2026-05-26 17:54:34 +0200

    Persist MET collections for Phase-2 HLT validation

--- Configuration/EventContent/python/EventContent_cff.py
+++ Configuration/EventContent/python/EventContent_cff.py
@@ -736,6 +736,9 @@ phase2_common.toModify(FEVTDEBUGHLTEventContent,
                            'keep *_hltMergeLayerClusters_*_*',
                            'keep *_hltParticleFlowRecHit*_*_*',
                            'keep *_hltEgammaGsfTracksL1Seeded_*_*',
+                           'keep *_hltPFMET_*_*',
+                           'keep *_hltPFPuppiMET_*_*',
+                           'keep *_hltPFPuppiMETTypeOne_*_*',
                        ])
 
 phase2_muon.toModify(FEVTDEBUGHLTEventContent, 
