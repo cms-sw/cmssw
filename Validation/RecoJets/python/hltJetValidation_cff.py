ce72f60e5fbe480cdc9b5f07b3958187003375ac
Elena Vernazza <elena.vernazza@cern.ch>
2026-05-27 10:48:57 +0200

    Rerun (if needed) HLT Jet Correction during pre-validation

--- Validation/RecoJets/python/hltJetValidation_cff.py
+++ Validation/RecoJets/python/hltJetValidation_cff.py
@@ -3,12 +3,42 @@ from PhysicsTools.NanoAOD.genparticles_cff import *
 from RecoJets.Configuration.GenJetParticles_cff import *
 from RecoJets.Configuration.RecoGenJets_cff import *
 
+from HLTrigger.Configuration.HLT_75e33.modules.hltAK4PFPuppiJetCorrectorL1_cfi import *
+from HLTrigger.Configuration.HLT_75e33.modules.hltAK4PFPuppiJetCorrectorL2_cfi import *
+from HLTrigger.Configuration.HLT_75e33.modules.hltAK4PFPuppiJetCorrectorL3_cfi import *
+from HLTrigger.Configuration.HLT_75e33.modules.hltAK4PFPuppiJetCorrector_cfi import *
+from HLTrigger.Configuration.HLT_75e33.modules.hltAK4PFJetCorrectorL1_cfi import *
+from HLTrigger.Configuration.HLT_75e33.modules.hltAK4PFJetCorrectorL2_cfi import *
+from HLTrigger.Configuration.HLT_75e33.modules.hltAK4PFJetCorrectorL3_cfi import *
+from HLTrigger.Configuration.HLT_75e33.modules.hltAK4PFJetCorrector_cfi import *
+from HLTrigger.Configuration.HLT_75e33.modules.hltAK4PFCHSJetCorrectorL1_cfi import *
+from HLTrigger.Configuration.HLT_75e33.modules.hltAK4PFCHSJetCorrectorL2_cfi import *
+from HLTrigger.Configuration.HLT_75e33.modules.hltAK4PFCHSJetCorrectorL3_cfi import *
+from HLTrigger.Configuration.HLT_75e33.modules.hltAK4PFCHSJetCorrector_cfi import *
+
+# reproduce JetCorrector modules when not available
+hltJetCorrectionSequence = cms.Sequence(
+    hltAK4PFPuppiJetCorrectorL1
+    + hltAK4PFPuppiJetCorrectorL2
+    + hltAK4PFPuppiJetCorrectorL3
+    + hltAK4PFPuppiJetCorrector
+    + hltAK4PFJetCorrectorL1
+    + hltAK4PFJetCorrectorL2
+    + hltAK4PFJetCorrectorL3
+    + hltAK4PFJetCorrector
+    + hltAK4PFCHSJetCorrectorL1
+    + hltAK4PFCHSJetCorrectorL2
+    + hltAK4PFCHSJetCorrectorL3
+    + hltAK4PFCHSJetCorrector
+)
+
 hltJetPreValidSeq = cms.Sequence(
     prunedGenParticlesWithStatusOne
     + prunedGenParticles
     + finalGenParticles
     + genParticlesForJetsNoNu
     + ak4GenJetsNoNu
+    + hltJetCorrectionSequence
 )
 
 from Validation.RecoJets.jetTester_cfi import jetTester as _jetTester
