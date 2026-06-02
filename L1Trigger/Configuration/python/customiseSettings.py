a369e7fa80dbe3ad0dcfac1d7ac650f712f8f46a
[li-chenyan] <[chenyanli@mail.sdu.edu.cn]>
2026-06-02 08:07:31 +0200

    update LUTs in caloParamsHI2026

--- L1Trigger/Configuration/python/customiseSettings.py
+++ L1Trigger/Configuration/python/customiseSettings.py
@@ -1,6 +1,10 @@
 import os.path
 import FWCore.ParameterSet.Config as cms
 
+def L1TSettingsToCaloParamsHI_2026_v0_2(process):
+    process.load("L1Trigger.L1TCalorimeter.caloParamsHI_2026_v0_2_cfi")
+    return process
+
 def L1TSettingsToCaloParamsHI_2026_v0_1(process):
     process.load("L1Trigger.L1TCalorimeter.caloParamsHI_2026_v0_1_cfi")
     return process
