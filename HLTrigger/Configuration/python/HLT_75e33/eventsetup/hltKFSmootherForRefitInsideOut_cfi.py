925d04e05a576f69155a73208d53c5d33e17a4ce
Marco Musich <marco.musich@cern.ch>
2026-06-03 21:50:16 +0200

    introduce mtd_at_hlt_cff and use it for running fast timing reconstruction

--- HLTrigger/Configuration/python/HLT_75e33/eventsetup/hltKFSmootherForRefitInsideOut_cfi.py
+++ HLTrigger/Configuration/python/HLT_75e33/eventsetup/hltKFSmootherForRefitInsideOut_cfi.py
@@ -1,12 +1,15 @@
 import FWCore.ParameterSet.Config as cms
 
-hltKFSmootherForRefitInsideOut = cms.ESProducer('KFTrajectorySmootherESProducer',
-    ComponentName = cms.string('hltKFSmootherForRefitInsideOut'),
-    Propagator = cms.string('hltSmartPropagatorAnyRK'),
-    Updator = cms.string('hltESPKFUpdator'),
-    Estimator = cms.string('hltChi2EstimatorForRefit'),
-    RecoGeometry = cms.string('GlobalDetLayerGeometry'),
-    errorRescaling = cms.double(100),
-    minHits = cms.int32(3),
-    appendToDataLabel = cms.string('')
-)
+def _addProcessKFSmootherForRefitInsideOut(process):
+    process.hltKFSmootherForRefitInsideOut = cms.ESProducer('KFTrajectorySmootherESProducer',
+                                                            ComponentName = cms.string('hltKFSmootherForRefitInsideOut'),
+                                                            Propagator = cms.string('hltSmartPropagatorAnyRK'),
+                                                            Updator = cms.string('hltESPKFUpdator'),
+                                                            Estimator = cms.string('hltChi2EstimatorForRefit'),
+                                                            RecoGeometry = cms.string('GlobalDetLayerGeometry'),
+                                                            errorRescaling = cms.double(100),
+                                                            minHits = cms.int32(3),
+                                                            appendToDataLabel = cms.string(''))
+
+from Configuration.ProcessModifiers.mtd_at_hlt_cff import mtd_at_hlt
+modifyConfigurationForKFSmootherForRefitInsideOut_ = mtd_at_hlt.makeProcessModifier(_addProcessKFSmootherForRefitInsideOut)
--- HLTrigger/Configuration/python/HLT_75e33/eventsetup/hltMTDCPEESProducer_cfi.py
+++ HLTrigger/Configuration/python/HLT_75e33/eventsetup/hltMTDCPEESProducer_cfi.py
@@ -1,5 +1,8 @@
 import FWCore.ParameterSet.Config as cms
 
-hltMTDCPEESProducer = cms.ESProducer('MTDCPEESProducer',
-                                     appendToDataLabel = cms.string('')
-                                     )
+def _addProcessMTDCPEESProducer(process):
+    process.hltMTDCPEESProducer = cms.ESProducer('MTDCPEESProducer',
+                                                 appendToDataLabel = cms.string(''))
+
+from Configuration.ProcessModifiers.mtd_at_hlt_cff import mtd_at_hlt
+modifyConfigurationForMTDCPEESProducer_ = mtd_at_hlt.makeProcessModifier(_addProcessMTDCPEESProducer)
