65ffca71e51e861e788e68459348cc52afa5a1ee
Marco Musich <marco.musich@cern.ch>
2026-06-04 11:36:08 +0200

    add offline-like 4D vertexing reco at HLT via hlt_at_mtd modifier

---
+++ HLTrigger/Configuration/python/HLT_75e33/sequences/HLTVertex4DRecoSequence_cfi.py
@@ -0,0 +1,13 @@
+import FWCore.ParameterSet.Config as cms
+
+from ..modules.hltTofPID4DnoPID_cfi import *
+from ..modules.hltUnsortedOfflinePrimaryVertices4D_cfi import *
+from ..modules.hltTrackWithVertexRefSelectorBeforeSorting4D_cfi import *
+from ..modules.hltTrackRefsForJetsBeforeSorting4D_cfi import *
+from ..modules.hltOfflinePrimaryVertices4D_cfi import *
+
+HLTVertex4DRecoSequence = cms.Sequence(hltTofPID4DnoPID+
+                                       hltUnsortedOfflinePrimaryVertices4D+
+                                       hltTrackWithVertexRefSelectorBeforeSorting4D+
+                                       hltTrackRefsForJetsBeforeSorting4D+
+                                       hltOfflinePrimaryVertices4D)
