d878196bef19241c6ba66b88056e107859ad6349
Marco Musich <marco.musich@cern.ch>
2026-06-05 14:37:28 +0200

    run hltTofPID in hltTofPID in HLTVertex4DRecoSequence and use it in PFTICLProducer

--- HLTrigger/Configuration/python/HLT_75e33/sequences/HLTVertex4DRecoSequence_cfi.py
+++ HLTrigger/Configuration/python/HLT_75e33/sequences/HLTVertex4DRecoSequence_cfi.py
@@ -5,9 +5,11 @@ from ..modules.hltUnsortedOfflinePrimaryVertices4D_cfi import *
 from ..modules.hltTrackWithVertexRefSelectorBeforeSorting4D_cfi import *
 from ..modules.hltTrackRefsForJetsBeforeSorting4D_cfi import *
 from ..modules.hltOfflinePrimaryVertices4D_cfi import *
+from ..modules.hltTofPID_cfi import *
 
 HLTVertex4DRecoSequence = cms.Sequence(hltTofPID4DnoPID+
                                        hltUnsortedOfflinePrimaryVertices4D+
                                        hltTrackWithVertexRefSelectorBeforeSorting4D+
                                        hltTrackRefsForJetsBeforeSorting4D+
-                                       hltOfflinePrimaryVertices4D)
+                                       hltOfflinePrimaryVertices4D+
+                                       hltTofPID)
