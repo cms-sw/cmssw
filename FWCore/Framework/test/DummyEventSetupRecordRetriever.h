626bd32770ee4348961c55aa475a8bf87fcd5d8a
Chris Jones <chrisdjones15@gmail.com>
2026-06-18 09:23:47 -0500

    Optionally allow setting of start IOV for DummyEventSetupRecordRetriever

--- FWCore/Framework/test/DummyEventSetupRecordRetriever.h
+++ FWCore/Framework/test/DummyEventSetupRecordRetriever.h
@@ -21,17 +21,19 @@
 // system include files
 
 // user include files
-#include "FWCore/Framework/interface/EventSetupRecordInfiniteIntervalFinder.h"
+#include "FWCore/Framework/interface/EventSetupRecordIntervalFinder.h"
 #include "FWCore/Framework/interface/ESProducer.h"
+#include "FWCore/Framework/interface/IOVSyncValue.h"
 
 #include "FWCore/Framework/test/DummyEventSetupRecord.h"
 #include "FWCore/Framework/test/DummyEventSetupData.h"
 
 // forward declarations
 namespace edm {
-  class DummyEventSetupRecordRetriever : public EventSetupRecordInfiniteIntervalFinder, public ESProducer {
+  class DummyEventSetupRecordRetriever : public EventSetupRecordIntervalFinder, public ESProducer {
   public:
-    DummyEventSetupRecordRetriever() {
+    explicit DummyEventSetupRecordRetriever(edm::IOVSyncValue const& iStart = edm::IOVSyncValue::invalidIOVSyncValue())
+        : m_start(iStart) {
       this->findingRecord<DummyEventSetupRecord>();
       setWhatProduced(this);
     }
@@ -39,6 +41,32 @@ namespace edm {
     std::unique_ptr<DummyEventSetupData> produce(const DummyEventSetupRecord&) {
       return std::make_unique<DummyEventSetupData>(1);
     }
+
+  protected:
+    void setIntervalFor(const edm::eventsetup::EventSetupRecordKey&,
+                        const edm::IOVSyncValue& iTime,
+                        edm::ValidityInterval& iInterval) final {
+      if (m_start == edm::IOVSyncValue::invalidIOVSyncValue()) {
+        iInterval = edm::ValidityInterval(edm::IOVSyncValue::beginOfTime(), edm::IOVSyncValue::endOfTime());
+      } else {
+        edm::ValidityInterval temp(m_start, edm::IOVSyncValue::endOfTime());
+        if (temp.validFor(iTime)) {
+          iInterval = temp;
+        } else {
+          iInterval = edm::ValidityInterval();
+        }
+      }
+    }
+
+    bool isConcurrentFinder() const final { return true; }
+
+  private:
+    DummyEventSetupRecordRetriever(const DummyEventSetupRecordRetriever&) = delete;  // stop default
+
+    const DummyEventSetupRecordRetriever& operator=(const DummyEventSetupRecordRetriever&) = delete;  // stop default
+
+    // ---------- member data --------------------------------
+    edm::IOVSyncValue m_start;
   };
 }  // namespace edm
 #endif
