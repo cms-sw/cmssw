9785e8c7785488101cab3a7b8a56c4292ba0fc74
Jade Chismar <jchismar@ucsd.edu>
2026-06-24 10:50:44 -0700

    Add copyTrajectories option to trackTorchClassifier

--- HLTrigger/Configuration/python/HLT_75e33/modules/hltInitialStepTrackTorchClassifierOutput_cfi.py
+++ HLTrigger/Configuration/python/HLT_75e33/modules/hltInitialStepTrackTorchClassifierOutput_cfi.py
@@ -4,7 +4,11 @@ hltInitialStepTrackTorchClassifierOutput = cms.EDProducer("TrackTorchClassifierF
     src = cms.InputTag("hltInitialStepTracks"),
     scores = cms.InputTag("hltInitialStepTrackTorchClassifier"),
     features = cms.InputTag("hltInitialStepTrackFeatureExtractor"),
+    copyTrajectories = cms.bool(False),
     minScore = cms.double(0.377),
     dxyThreshold = cms.double(0.5),
     highDxyMinScore = cms.double(0.267)
 )
+
+from Configuration.ProcessModifiers.mtd_at_hlt_cff import mtd_at_hlt
+mtd_at_hlt.toModify(hltInitialStepTrackTorchClassifierOutput, copyTrajectories = True)
