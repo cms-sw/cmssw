0cd5e2a96f325810dbff962331591e24e5d91078
Luca Ferragina <ferragina.luca@gmail.com>
2026-06-25 16:48:11 +0200

    Add max resident threads information
    CUDA allocates global device memory to account for the kernel memory
    spilling from registers into thread-local stack. This allocation is
    directly managed by the driver and cached to the largest spill across
    all kernel launched by a process. The allocation is per is per context
    and it is sized based upon STACK x max_resident_threads (the worst
    possible case with full occupancy running the most "spilling kernel".
    The added information provides number to compute the automatic
    allocation, given a value of STACK for a spilling kernel

--- HeterogeneousCore/CUDAServices/plugins/CUDAService.cc
+++ HeterogeneousCore/CUDAServices/plugins/CUDAService.cc
@@ -264,8 +264,13 @@ CUDAService::CUDAService(edm::ParameterSet const& config) : verbose_(config.getU
     if (verbose_) {
       log << '\n';
       log << "  streaming multiprocessors: " << std::setw(13) << properties.multiProcessorCount << '\n';
+      log << "  max threads per multiprocessor: " << std::setw(8) << properties.maxThreadsPerMultiProcessor << '\n';
+      log << "  max resident threads: " << std::setw(18)
+          << properties.multiProcessorCount * properties.maxThreadsPerMultiProcessor << '\n';
       log << "  CUDA cores: " << std::setw(28)
           << properties.multiProcessorCount * getCudaCoresPerSM(properties.major, properties.minor) << '\n';
+      log << "  max resident threads per CUDA core: " << std::setw(4)
+          << properties.maxThreadsPerMultiProcessor / getCudaCoresPerSM(properties.major, properties.minor) << '\n';
       int singleToDoublePrecisionPerfRatio = 0;
 #if CUDART_VERSION < 13000
       singleToDoublePrecisionPerfRatio = properties.singleToDoublePrecisionPerfRatio;
