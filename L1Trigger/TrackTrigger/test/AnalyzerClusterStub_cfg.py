29a0f833f8e8921eb1e516afcd40f420b667e1ac
Ian Tomalin <ian.tomalin@stfc.ac.uk>
2026-07-03 18:53:45 +0200

    Add TTCluster EDAnalyzer (#374)
    * Add TTCluster EDAnalyzer
    
    * auto code format

--- L1Trigger/TrackTrigger/test/AnalyzerClusterStub_cfg.py
+++ L1Trigger/TrackTrigger/test/AnalyzerClusterStub_cfg.py
@@ -1,14 +1,9 @@
 ####################################################
-# SLHCUpgradeSimulations                           #
-# Configuration file for Full Workflow             #
-# Step 2 (again)                                   #
-# Validate the L1 track trigger inputs             #
-# TTCluster & TTStub.                              #
-####################################################
-# Author: Nicola Pozzobon                          #
-# CERN, August 2012                                #
-# Updated: Ian Tomalin                             #
-# RAL, May 2026                                    #
+# Analyze TTClusters & TTStubs                     #
+# (P2 objects used for L1 tracking)                #
+# using either AnalyzerClusterStub.cc,             #
+# or AnalyzerCluster.cc.                           #
+# (Edit this file to decide which to run)          #
 ####################################################
 
 ################################################################################
@@ -44,16 +39,18 @@ process.load('Configuration.StandardSequences.MagneticField_cff')
 process.source = cms.Source("PoolSource",
     #fileNames = cms.untracked.vstring('/store/relval/CMSSW_15_1_0_pre5/RelValDoubleMuFlatPt1p5To8/GEN-SIM-DIGI-RAW/150X_mcRun4_realistic_v1_RV269_Run4D110_noPU-v1/2590000/1172421f-823f-420f-8ec9-3de20dd6dda4.root')
     #fileNames = cms.untracked.vstring('/store/relval/CMSSW_15_1_0_pre5/RelValTTbar_14TeV_TuneCP5/GEN-SIM-DIGI-RAW/150X_mcRun4_realistic_v1_RV269_Run4D110_noPU-v1/2590000/02a7204c-3f1c-4858-9350-2f506d729bbc.root')
-    fileNames = cms.untracked.vstring('/store/relval/CMSSW_15_1_0_pre5/RelValTTbar_14TeV_TuneCP5/GEN-SIM-DIGI-RAW/PU_150X_mcRun4_realistic_v1_RV269_Run4D110_PU-v2/2590000/0f0bcfd3-dafe-4dda-8d39-9765f6eae68e.root')
+    #fileNames = cms.untracked.vstring('/store/relval/CMSSW_15_1_0_pre5/RelValTTbar_14TeV_TuneCP5/GEN-SIM-DIGI-RAW/PU_150X_mcRun4_realistic_v1_RV269_Run4D110_PU-v2/2590000/0f0bcfd3-dafe-4dda-8d39-9765f6eae68e.root')
+    fileNames = cms.untracked.vstring('file:/opt/ppd/data/cms/L1TrkMC/MCsamples1510_D110/RelVal/TTbar/PU0/02a7204c-3f1c-4858-9350-2f506d729bbc.root')
 )
 
 process.maxEvents = cms.untracked.PSet(
-  input = cms.untracked.int32(10)
+  input = cms.untracked.int32(100)
 )
 
 ################################################################################
-# load the analyzer
+# load the analyzers
 ################################################################################
+# Old analyzer for clusters & stubs (rates, origin of stubs etc.)
 process.AnalyzerClusterStub = cms.EDAnalyzer("AnalyzerClusterStub",
   DebugMode = cms.bool(True),
   TTClusterInputTag = cms.InputTag("TTClustersFromPhase2TrackerDigis", "ClusterInclusive"),
@@ -64,18 +61,38 @@ process.AnalyzerClusterStub = cms.EDAnalyzer("AnalyzerClusterStub",
   TrackingVertexInputTag = cms.InputTag("mix", "MergedTrackTruth"),   
 )
 
+# New analyzer for clusters (position resolution w.r.t. SimHits etc.)
+process.AnalyzerCluster = cms.EDAnalyzer("AnalyzerCluster",
+  trackingParticles= cms.InputTag("mix","MergedTrackTruth"),
+  ttclusters = cms.InputTag("TTClustersFromPhase2TrackerDigis", "ClusterInclusive"),
+  links = cms.InputTag("simSiPixelDigis", "Tracker"),
+  simhitsbarrel = cms.InputTag("g4SimHits", "TrackerHitsPixelBarrelLowTof"),
+  simhitsendcap = cms.InputTag("g4SimHits", "TrackerHitsPixelEndcapLowTof"),
+  simtracks = cms.InputTag("g4SimHits"),
+  ECasRings = cms.bool(True),
+  SimTrackMinPt = cms.double(2.)
+                                        
+)
+
 #################################################################################################
 # define output file and message logger
 #################################################################################################
 process.TFileService = cms.Service("TFileService",
-    fileName = cms.string('file:AnalyzerClusterStub.root'),
+    fileName = cms.string('file:analysis.root'),
     closeFileFast = cms.untracked.bool(True)
 )
 
 #################################################################################################
 # define the final path to be fed to cmsRun
 #################################################################################################
-process.p = cms.Path( process.AnalyzerClusterStub )
+
+#-- Choose which analyzer to run
+
+# This is the old one for both clusters & stubs.
+#process.p = cms.Path( process.AnalyzerClusterStub )
+
+# This is the new one for clusters alone (includes resolution plots unlike old one)
+process.p = cms.Path( process.AnalyzerCluster )
 
 
 
