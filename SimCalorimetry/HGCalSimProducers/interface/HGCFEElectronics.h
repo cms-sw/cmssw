c6ad57c23e2b1220da0e72d442f4813ced957d17
Felice Pantaleo <felice.pantaleo@cern.ch>
2026-07-22 04:18:14 +0200

    Harden ToA offset generation and fix HFNose aging customise target
    Bound the generateTimeOffset loop by both array sizes, apply the same
    process-attribute fix to the v19 toModify in HFNose_setRealisticNoiseSi
    that HGCal_setRealisticNoiseSi received, and correct stale thickness
    comments.

--- SimCalorimetry/HGCalSimProducers/interface/HGCFEElectronics.h
+++ SimCalorimetry/HGCalSimProducers/interface/HGCFEElectronics.h
@@ -89,7 +89,8 @@ public:
   };
 
   void generateTimeOffset(CLHEP::HepRandomEngine* engine) {
-    for (size_t i = 0; i < jitterConstant_ns_.size(); i++)
+    const size_t n = std::min(eventTimeOffset_ns_.size(), jitterConstant_ns_.size());
+    for (size_t i = 0; i < n; i++)
       eventTimeOffset_ns_[i] = CLHEP::RandGaussQ::shoot(engine, 0, jitterConstant_ns_[i]);
   };
 
