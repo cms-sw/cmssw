7b1adbb129f72ff8000c2c5c966f820a07be1cd1
Marco Musich <marco.musich@cern.ch>
2026-08-10 15:38:45 +0200

    fix DST_HeterogeneousReco path to include e-gamma alpaka portable modules

--- HLTrigger/Configuration/python/HLT_75e33/paths/DST_HeterogeneousReco_cfi.py
+++ HLTrigger/Configuration/python/HLT_75e33/paths/DST_HeterogeneousReco_cfi.py
@@ -24,6 +24,52 @@ from ..modules.hltSiPixelClusters_cfi import hltSiPixelClusters
 from ..modules.hltSiPixelRecHits_cfi import hltSiPixelRecHits
 from ..modules.hltSiPhase2Clusters_cfi import hltSiPhase2Clusters
 from ..modules.hltSiPhase2RecHits_cfi import hltSiPhase2RecHits
+from ..modules.hltEcalBarrelDigisInRegions_cfi import hltEcalBarrelDigisInRegions
+from ..modules.hltEcalDetIdToBeRecovered_cfi import hltEcalDetIdToBeRecovered
+from ..modules.hltEcalDigis_cfi import hltEcalDigis
+from ..modules.hltEcalMultiFitUncalibRecHit_cfi import hltEcalMultiFitUncalibRecHit
+from ..modules.hltElePixelHitTripletsClusterRemoverL1Seeded_cfi import hltElePixelHitTripletsClusterRemoverL1Seeded
+from ..modules.hltEleSeedsTrackingRegionsL1Seeded_cfi import hltEleSeedsTrackingRegionsL1Seeded
+from ..modules.hltFilteredLayerClustersCLUE3DHighL1Seeded_cfi import hltFilteredLayerClustersCLUE3DHighL1Seeded
+from ..modules.hltHcalDigis_cfi import hltHcalDigis
+from ..modules.hltHgcalDigisL1Seeded_cfi import hltHgcalDigisL1Seeded
+from ..modules.hltHgcalLayerClustersHSciL1Seeded_cfi import hltHgcalLayerClustersHSciL1Seeded
+from ..modules.hltHgcalLayerClustersHSiL1Seeded_cfi import hltHgcalLayerClustersHSiL1Seeded
+from ..modules.hltL1TEGammaFilteredCollectionProducer_cfi import hltL1TEGammaFilteredCollectionProducer
+from ..modules.hltL1TEGammaHGCFilteredCollectionProducer_cfi import hltL1TEGammaHGCFilteredCollectionProducer
+from ..modules.hltParticleFlowRecHitECALL1Seeded_cfi import hltParticleFlowRecHitECALL1Seeded
+from ..modules.hltPixelLayerTriplets_cfi import hltPixelLayerTriplets
+from ..modules.hltTiclLayerTileProducerL1Seeded_cfi import hltTiclLayerTileProducerL1Seeded
+from ..modules.hltTiclSeedingL1_cfi import hltTiclSeedingL1
+from ..modules.hltTiclTracksterLinksSuperclusteringDNNL1Seeded_cfi import hltTiclTracksterLinksSuperclusteringDNNL1Seeded
+from ..modules.hltEcalUncalibRecHitL1Seeded_cfi import hltEcalUncalibRecHitL1Seeded
+from ..modules.hltPixelLayerPairsL1Seeded_cfi import hltPixelLayerPairsL1Seeded
+from ..modules.hltHbhereco_cfi import hltHbhereco
+from ..modules.hltHGCalUncalibRecHitL1Seeded_cfi import hltHGCalUncalibRecHitL1Seeded
+from ..modules.hltRechitInRegionsECAL_cfi import hltRechitInRegionsECAL
+from ..modules.hltParticleFlowClusterECALUncorrectedL1Seeded_cfi import hltParticleFlowClusterECALUncorrectedL1Seeded
+from ..modules.hltElePixelHitDoubletsForTripletsL1Seeded_cfi import hltElePixelHitDoubletsForTripletsL1Seeded
+from ..modules.hltEcalRecHit_cfi import hltEcalRecHit
+from ..modules.hltTiclTrackstersCLUE3DHighL1Seeded_cfi import hltTiclTrackstersCLUE3DHighL1Seeded
+from ..modules.hltEcalRecHitL1Seeded_cfi import hltEcalRecHitL1Seeded
+from ..modules.hltElePixelHitDoubletsL1Seeded_cfi import hltElePixelHitDoubletsL1Seeded
+from ..modules.hltHGCalRecHitL1Seeded_cfi import hltHGCalRecHitL1Seeded
+from ..modules.hltElePixelHitTripletsL1Seeded_cfi import hltElePixelHitTripletsL1Seeded
+from ..modules.hltFixedGridRhoFastjetAllCaloForEGamma_cfi import hltFixedGridRhoFastjetAllCaloForEGamma
+from ..modules.hltParticleFlowClusterECALL1Seeded_cfi import hltParticleFlowClusterECALL1Seeded
+from ..modules.hltElePixelSeedsDoubletsL1Seeded_cfi import hltElePixelSeedsDoubletsL1Seeded
+from ..modules.hltRechitInRegionsHGCAL_cfi import hltRechitInRegionsHGCAL
+from ..modules.hltElePixelSeedsTripletsL1Seeded_cfi import hltElePixelSeedsTripletsL1Seeded
+from ..modules.hltEgammaHoverEL1Seeded_cfi import hltEgammaHoverEL1Seeded
+from ..modules.hltParticleFlowSuperClusterECALL1Seeded_cfi import hltParticleFlowSuperClusterECALL1Seeded
+from ..modules.hltElePixelSeedsCombinedL1Seeded_cfi import hltElePixelSeedsCombinedL1Seeded
+from ..modules.hltHgcalLayerClustersEEL1Seeded_cfi import hltHgcalLayerClustersEEL1Seeded
+from ..modules.hltMergeLayerClustersL1Seeded_cfi import hltMergeLayerClustersL1Seeded
+from ..modules.hltTiclEGammaSuperClusterProducerL1Seeded_cfi import hltTiclEGammaSuperClusterProducerL1Seeded
+from ..modules.hltEgammaCandidatesL1Seeded_cfi import hltEgammaCandidatesL1Seeded
+from ..modules.hltEgammaSuperClustersToPixelMatchL1Seeded_cfi import hltEgammaSuperClustersToPixelMatchL1Seeded
+from ..modules.hltEgammaElectronPixelSeedsPortable_cfi import hltEgammaElectronPixelSeedsPortable
+from ..modules.hltBunchSpacingProducer_cfi import hltBunchSpacingProducer
 from ..sequences.HLTBeginSequence_cfi import *
 from ..sequences.HLTEndSequence_cfi import *
 
@@ -62,6 +108,60 @@ HLTHeterogeneousHGCalRecoSequence = cms.Sequence(
     + hltHgcalSoALayerClustersProducer
 )
 
+HLTHeterogeneousEgammaRecoSequence = cms.Sequence(
+    hltL1TEGammaHGCFilteredCollectionProducer
+    + hltHgcalDigisL1Seeded
+    + hltHGCalUncalibRecHitL1Seeded
+    + hltHGCalRecHitL1Seeded
+    + hltRechitInRegionsHGCAL
+    + hltHgcalLayerClustersEEL1Seeded
+    + hltHgcalLayerClustersHSiL1Seeded
+    + hltHgcalLayerClustersHSciL1Seeded
+    + hltMergeLayerClustersL1Seeded
+    + hltTiclLayerTileProducerL1Seeded
+    + hltTiclSeedingL1
+    + hltFilteredLayerClustersCLUE3DHighL1Seeded
+    + hltTiclTrackstersCLUE3DHighL1Seeded
+    + hltTiclTracksterLinksSuperclusteringDNNL1Seeded
+    + hltTiclEGammaSuperClusterProducerL1Seeded
+
+    + hltEcalDigis
+    + hltL1TEGammaFilteredCollectionProducer
+    + hltEcalBarrelDigisInRegions
+    + hltBunchSpacingProducer
+    + hltEcalUncalibRecHitL1Seeded
+    + hltEcalDetIdToBeRecovered
+    + hltEcalRecHitL1Seeded
+    + hltRechitInRegionsECAL
+    + hltParticleFlowRecHitECALL1Seeded
+    + hltParticleFlowClusterECALUncorrectedL1Seeded
+    + hltParticleFlowClusterECALL1Seeded
+    + hltParticleFlowSuperClusterECALL1Seeded
+    + hltEgammaCandidatesL1Seeded
+
+    + hltHcalDigis
+    + hltHbhereco
+    + hltEcalMultiFitUncalibRecHit
+    + hltEcalRecHit
+    + hltFixedGridRhoFastjetAllCaloForEGamma
+    + hltEgammaHoverEL1Seeded
+    + hltEgammaSuperClustersToPixelMatchL1Seeded
+
+    + hltPixelLayerTriplets
+    + hltEleSeedsTrackingRegionsL1Seeded
+    + hltElePixelHitDoubletsForTripletsL1Seeded
+    + hltElePixelHitTripletsL1Seeded
+    + hltElePixelSeedsTripletsL1Seeded
+
+    + hltElePixelHitTripletsClusterRemoverL1Seeded
+    + hltPixelLayerPairsL1Seeded
+    + hltElePixelHitDoubletsL1Seeded
+    + hltElePixelSeedsDoubletsL1Seeded
+    + hltElePixelSeedsCombinedL1Seeded
+
+    + hltEgammaElectronPixelSeedsPortable
+)
+
 DST_HeterogeneousReco = cms.Path(
     HLTBeginSequence
     + hltL1GTAcceptFilter
@@ -69,5 +169,6 @@ DST_HeterogeneousReco = cms.Path(
     + HLTPixelTrackingSequence
     + HLTLSTSequence
     + HLTHeterogeneousHGCalRecoSequence
+    + HLTHeterogeneousEgammaRecoSequence
     + HLTEndSequence
 )
