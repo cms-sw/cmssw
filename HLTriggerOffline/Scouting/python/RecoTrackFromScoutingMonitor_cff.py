c4d5aae9524d36d523a956acac2f342f1185a1ef
HLT Optimal Calibrations <sakura@cern.ch>
2026-08-12 13:17:00 +0200

    do not throw on missing input in Run3ScoutingTrackToRecoTrackProducer and Run3ScoutingVertexToRecoVertexProducer

--- HLTriggerOffline/Scouting/python/RecoTrackFromScoutingMonitor_cff.py
+++ HLTriggerOffline/Scouting/python/RecoTrackFromScoutingMonitor_cff.py
@@ -2,12 +2,14 @@ import FWCore.ParameterSet.Config as cms
 
 from .RecoTrackFromScoutingMonitor_cfi import scoutingRecoTrackMonitor
 
-# Tracks
+# Scouting Tracks to reco::Track conversion
 recoTracksFromScouting = cms.EDProducer("Run3ScoutingTrackToRecoTrackProducer",
+                                        skipMissingProduct = cms.bool(True), # do not throw on missing input
 					src = cms.InputTag("hltScoutingTrackPacker"))
 
-# Vertices 
+# Scouting Vertices to reco::Vertex conversion
 recoVerticesFromScouting = cms.EDProducer("Run3ScoutingVertexToRecoVertexProducer",
+                                          skipMissingProduct = cms.bool(True), # do not throw on missing input
 					  src = cms.InputTag("hltScoutingPrimaryVertexPacker", "primaryVtx"))
 
 recoTrackFromScoutingMonitorSequence = cms.Sequence(recoTracksFromScouting +
