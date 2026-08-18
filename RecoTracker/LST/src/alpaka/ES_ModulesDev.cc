4ed1894528edfbc587d0323478d41ebea73c833a
Shahzad Malik Muzaffar <shahzad.malik.muzaffar@cern.ch>
2026-08-18 10:51:47 +0200

    added PhysicsTools/PyTorch/test/README.md

--- RecoTracker/LST/src/alpaka/ES_ModulesDev.cc
+++ RecoTracker/LST/src/alpaka/ES_ModulesDev.cc
@@ -1,4 +1,4 @@
 #include "RecoTracker/LSTCore/interface/LSTESData.h"
 #include "HeterogeneousCore/AlpakaCore/interface/alpaka/typelookup.h"
-// test commit
+
 TYPELOOKUP_ALPAKA_TEMPLATED_DATA_REG(lst::LSTESData);
