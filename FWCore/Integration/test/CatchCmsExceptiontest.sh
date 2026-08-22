56ad4633486cfe19ce9a7eab6721159eb6097ad0
Chris Jones <chrisdjones15@gmail.com>
2026-08-22 10:24:53 -0500

    Updated ThrowingSource and tests
    - ThrowingSource now inherits from a more primative base class
    - Tests using ThrowingSource now are run

--- FWCore/Integration/test/CatchCmsExceptiontest.sh
+++ FWCore/Integration/test/CatchCmsExceptiontest.sh
@@ -4,9 +4,21 @@ LOCAL_TEST_DIR=${SCRAM_TEST_PATH}
 # Pass in name and status
 function die { echo $1: status $2 ;  exit $2; }
 
+function test_failure { 
+    if [ "$2" != "65" ]
+     then
+       echo $1: status $2; exit $2;
+    fi
+}
+
+
 cmsRun ${LOCAL_TEST_DIR}/CatchCmsExceptiontest_cfg.py &> CatchCmsException.log && die 'Failed in using CatchCmsException_cfg.py' 1
 
 grep -q WhatsItESProducer CatchCmsException.log || die 'Failed to find Producers name' $?
 
-#echo running cmsRun testSkipEvent_cfg.py
-#cmsRun ${LOCAL_TEST_DIR}/testSkipEvent_cfg.py &> testSkipEvent.log || die 'Failed in using testSkipEvent_cfg.py' $?
+#This is the case where the exception is not thrown, so the exit code should be 0
+cmsRun ${LOCAL_TEST_DIR}/CatchCmsExceptionFromSource_cfg.py --whenToThrow=0 || die 'Failed in using CatchCmsExceptionFromSource_cfg.py' $?
+
+for whenToThrow in $(seq 1 11); do
+  cmsRun ${LOCAL_TEST_DIR}/CatchCmsExceptionFromSource_cfg.py --whenToThrow=${whenToThrow}; test_failure "Failed in using CatchCmsExceptionFromSource_cfg.py --whenToThrow=${whenToThrow}" $?
+done
