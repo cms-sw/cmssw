f0df50964cb8e2e1b7960b9da73554cd07964eb9
Joshua Hiltbrand <joshua.hiltbrand@proton.me>
2026-08-31 14:30:56 +0200

    cleaning up in HCAL TPG coder

--- CalibCalorimetry/HcalTPGAlgos/interface/HcaluLUTTPGCoder.h
+++ CalibCalorimetry/HcalTPGAlgos/interface/HcaluLUTTPGCoder.h
@@ -1,35 +1,42 @@
-#ifndef CALIBCALORIMETRY_HCALTPGALGOS_HCALNOMINALTPGCODER_H
-#define CALIBCALORIMETRY_HCALTPGALGOS_HCALNOMINALTPGCODER_H 1
+#ifndef CalibCalorimetry_HcalTPGAlgos_HcaluLUTTPGCoder_h
+#define CalibCalorimetry_HcalTPGAlgos_HcaluLUTTPGCoder_h
 
-#include "CalibFormats/HcalObjects/interface/HcalTPGCoder.h"
-#include "CalibFormats/HcalObjects/interface/HcalNominalCoder.h"
-#include "Geometry/CaloTopology/interface/HcalTopology.h"
-#include "DataFormats/HcalDetId/interface/HcalDetId.h"
+// -*- C++ -*-
+//
+// Package:     CalibCalorimetry/HcalTPGAlgos
+// Class  :     HcaluLUTTPGCoder
+//
+/**\class HcaluLUTTPGCoder HcaluLUTTPGCoder.h CalibCalorimetry/HcalTPGAlgos/interface/HcaluLUTTPGCoder.h
+
+ Description: Builds and maintains the HCAL input (linearization) look-up table
+
+ Usage:
+    <usage>
+
+*/
+//
+// Original Author:  M. Weinberger, T. Bose, G. Landsberg
+//
+
 #include "CalibCalorimetry/HcalAlgos/interface/HcalPulseContainmentManager.h"
+#include "CalibCalorimetry/HcalAlgos/interface/HcalTimeSlew.h"
+#include "CalibFormats/CaloObjects/interface/IntegerCaloSamples.h"
+#include "CalibFormats/HcalObjects/interface/HcalDbService.h"
+#include "CalibFormats/HcalObjects/interface/HcalNominalCoder.h"
+#include "CalibFormats/HcalObjects/interface/HcalTPGCoder.h"
 #include "CondFormats/HcalObjects/interface/HcalElectronicsMap.h"
+#include "DataFormats/HcalDetId/interface/HcalDetId.h"
+#include "DataFormats/HcalDetId/interface/HcalZDCDetId.h"
+#include "DataFormats/HcalDigi/interface/HBHEDataFrame.h"
+#include "DataFormats/HcalDigi/interface/HFDataFrame.h"
+#include "DataFormats/HcalDigi/interface/QIE10DataFrame.h"
+#include "DataFormats/HcalDigi/interface/QIE11DataFrame.h"
+#include "Geometry/CaloTopology/interface/HcalTopology.h"
 
 #include <bitset>
+#include <cstdint>
 #include <vector>
 
-class HcalDbService;
-
-/** \class HcaluLUTTPGCoder
-  *  
-  * The nominal coder uses a user-supplied table to linearize the ADC values.
-  *
-  * <pre>
-  * [number of ieta slices]
-  * [low tower 1] [low tower 2] ...
-  * [high tower 1] [ high tower 2] ...
-  * [LUT 1(0)] [LUT 2(0)] ...
-  * [LUT 1(1)] [LUT 2(1)] ...
-  * . . .
-  * [LUT 1(127)] [LUT 2(127)] ...
-  * </pre>
-  *
-  * \author M. Weinberger -- TAMU
-  * \author Tulika Bose and Greg Landsberg -- Brown
-  */
 class HcaluLUTTPGCoder : public HcalTPGCoder {
 public:
   static const float lsb_;
@@ -48,17 +55,13 @@ public:
   void compress(const IntegerCaloSamples& ics,
                 const std::vector<bool>& featureBits,
                 HcalTriggerPrimitiveDigi& tp) const override;
-  unsigned short adc2Linear(HcalQIESample sample, HcalDetId id) const override;
-  float getLUTPedestal(HcalDetId id) const override;
-  float getLUTGain(HcalDetId id) const override;
-  std::vector<unsigned short> getLinearizationLUT(HcalDetId id) const override;
-  std::vector<unsigned short> getLinearizationLUT(HcalZDCDetId id, bool ootpu_lut) const override;
+  unsigned short adc2Linear(const HcalQIESample& sample, const HcalDetId& id) const override;
+  std::vector<unsigned short> getLinearizationLUT(const HcalDetId& id) const override;
+  std::vector<unsigned short> getLinearizationLUT(const HcalZDCDetId& id, bool ootpu_lut) const override;
 
-  double cosh_ieta(int ieta, int depth, HcalSubdetector subdet);
+  double cosh_ieta(int ieta, int depth, const HcalSubdetector& subdet);
   void make_cosh_ieta_map(void);
   void update(const HcalDbService& conditions);
-  void update(const char* filename, bool appendMSB = false);
-  void updateXML(const char* filename);
   void setLUTGenerationMode(bool gen) { LUTGenerationMode_ = gen; };
   void setOverrideFGHF(bool overrideFGHF) { overrideFGHF_ = overrideFGHF; };
   void setFGHFthresholds(const std::array<uint32_t, 2>& fgthresholds) { FG_HF_thresholds_ = fgthresholds; };
@@ -90,7 +93,7 @@ public:
   void lookupMSB(const QIE10DataFrame& df, std::vector<std::bitset<2>>& msb) const;
   void lookupMSB(const QIE11DataFrame& df, std::vector<std::bitset<2>>& msb) const;
   bool getMSB(const HcalDetId& id, int adc) const;
-  int getLUTId(HcalSubdetector id, int ieta, int iphi, int depth) const;
+  int getLUTId(const HcalSubdetector& id, int ieta, int iphi, int depth) const;
   int getLUTId(uint32_t rawid) const;
   int getLUTId(const HcalDetId& detid) const;
   int getLUTId(const HcalZDCDetId& detid) const;
@@ -101,11 +104,9 @@ public:
   static const int QIE10_ZDC_LUT_BITMASK = 0x3FF;
 
 private:
-  // typedef
   typedef unsigned short LutElement;
   typedef std::vector<LutElement> Lut;
 
-  // constants
   static const size_t INPUT_LUT_SIZE = 128;
   static const size_t UPGRADE_LUT_SIZE = 256;
   static const int nFi_ = 72;
@@ -116,7 +117,6 @@ private:
   static const int QIE10_LUT_MSB0 = 0x1000;
   static const int QIE10_LUT_MSB1 = 0x2000;
 
-  // member variables
   const HcalTopology* topo_;
   const HcalElectronicsMap* emap_;
   const HcalTimeSlew* delay_;
@@ -131,8 +131,6 @@ private:
   int firstHFEta_, lastHFEta_, nHFEta_, maxDepthHF_, sizeHF_;
   int sizeZDC_;
   std::vector<Lut> inputLUT_;
-  std::vector<float> gain_;
-  std::vector<float> ped_;
   std::vector<double> cosh_ieta_;
   // edge cases not covered by the cosh_ieta_ map
   double cosh_ieta_28_HE_low_depths_, cosh_ieta_28_HE_high_depths_, cosh_ieta_29_HE_;
--- CalibFormats/CaloTPG/src/ES_HcalTPGScale.cc
+++
@@ -1,4 +0,0 @@
-#include "FWCore/Utilities/interface/typelookup.h"
-#include "CalibFormats/CaloTPG/interface/HcalTPGScale.h"
-
-TYPELOOKUP_DATA_REG(HcalTPGScale);
