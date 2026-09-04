7991b71acb9e1025dbbefe8f65d015b814557ef1
Andre Govinda Stahl Leiton <andre.govinda.stahl.leiton@cern.ch>
2026-09-04 14:53:40 +0200

    Remove extra ECAL SC cut for electrons in PbPb Run3 era

--- RecoEgamma/EgammaElectronProducers/python/ecalDrivenElectronSeeds_cff.py
+++ RecoEgamma/EgammaElectronProducers/python/ecalDrivenElectronSeeds_cff.py
@@ -11,7 +11,8 @@ from RecoTracker.IterativeTracking.ElectronSeeds_cff import newCombinedSeeds
 ecalDrivenElectronSeeds.initialSeedsVector = newCombinedSeeds.seedCollections
 
 from Configuration.ProcessModifiers.pp_on_AA_cff import pp_on_AA
-pp_on_AA.toModify(ecalDrivenElectronSeeds, SCEtCut = 15.0)
+from Configuration.Eras.Modifier_run3_common_cff import run3_common
+(pp_on_AA & ~run3_common).toModify(ecalDrivenElectronSeeds, SCEtCut = 15.0)
 pp_on_AA.toModify(ecalDrivenElectronSeeds, maxHOverEBarrel = 0.5, maxHOverEEndcaps = 0.5)
 
 from Configuration.Eras.Modifier_phase2_hgcal_cff import phase2_hgcal
