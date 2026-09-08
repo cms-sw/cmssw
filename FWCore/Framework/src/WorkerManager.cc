0111c5d3c52cab588ecd38de485f64e7eb6d749d
Chris Jones <chrisdjones15@gmail.com>
2026-09-08 10:43:36 -0500

    Specialize WorkerManager templates
    Break into global, stream and event versions as they are used differently.

--- FWCore/Framework/src/WorkerManager.cc
+++ FWCore/Framework/src/WorkerManager.cc
@@ -15,28 +15,58 @@
 namespace edm {
   // -----------------------------
   template <typename TI, typename TP>
-  WorkerManager<TI, TP>::WorkerManager(std::shared_ptr<ModuleRegistry> modReg,
-                                       std::shared_ptr<ActivityRegistry> areg,
-                                       ExceptionToActionTable const& actions)
-      : workerReg_(areg, modReg),
-        actionTable_(&actions),
-        allWorkers_(),
-        unscheduled_(*areg),
-        lastSetupEventPrincipal_(nullptr) {}  // WorkerManager::WorkerManager
+  WorkerManagerCore<TI, TP>::WorkerManagerCore(std::shared_ptr<ModuleRegistry> modReg,
+                                               std::shared_ptr<ActivityRegistry> areg,
+                                               ExceptionToActionTable const& actions)
+      : workerReg_(areg, modReg), actionTable_(&actions), allWorkers_(), lastSetupPrincipal_(nullptr) {}
+
+  template <typename TI>
+  WorkerManager<TI, TransitionPhaseGlobal>::WorkerManager(std::shared_ptr<ModuleRegistry> modReg,
+                                                          std::shared_ptr<ActivityRegistry> areg,
+                                                          ExceptionToActionTable const& actions)
+      : WorkerManagerCore<TI, TransitionPhaseGlobal>(modReg, areg, actions) {}
+
+  template <typename TI>
+  WorkerManager<TI, TransitionPhaseStream>::WorkerManager(std::shared_ptr<ModuleRegistry> modReg,
+                                                          std::shared_ptr<ActivityRegistry> areg,
+                                                          ExceptionToActionTable const& actions)
+      : WorkerManagerCore<TI, TransitionPhaseStream>(modReg, areg, actions) {}
+
+  WorkerManager<EventTransitionInfo, TransitionPhaseStream>::WorkerManager(std::shared_ptr<ModuleRegistry> modReg,
+                                                                           std::shared_ptr<ActivityRegistry> areg,
+                                                                           ExceptionToActionTable const& actions)
+      : WorkerManagerCore<EventTransitionInfo, TransitionPhaseStream>(modReg, areg, actions),
+        unscheduled_(*areg) {}  // WorkerManager::WorkerManager
 
   template <typename TI, typename TP>
-  void WorkerManager<TI, TP>::deleteModuleIfExists(std::string const& moduleLabel) {
+  Worker const* WorkerManagerCore<TI, TP>::deleteModuleIfExists(std::string const& moduleLabel) {
     auto worker = workerReg_.get(moduleLabel);
     if (worker != nullptr) {
       auto eraseBeg = std::remove(allWorkers_.begin(), allWorkers_.end(), worker);
       allWorkers_.erase(eraseBeg, allWorkers_.end());
-      unscheduled_.removeWorker(worker);
       workerReg_.deleteModule(moduleLabel);
+    }
+    return worker;
+  }
+
+  template <typename TI>
+  void WorkerManager<TI, TransitionPhaseStream>::deleteModuleIfExists(std::string const& moduleLabel) {
+    (void)WorkerManagerCore<TI, TransitionPhaseStream>::deleteModuleIfExists(moduleLabel);
+  }
+  template <typename TI>
+  void WorkerManager<TI, TransitionPhaseGlobal>::deleteModuleIfExists(std::string const& moduleLabel) {
+    (void)WorkerManagerCore<TI, TransitionPhaseGlobal>::deleteModuleIfExists(moduleLabel);
+  }
+
+  void WorkerManager<EventTransitionInfo, TransitionPhaseStream>::deleteModuleIfExists(std::string const& moduleLabel) {
+    auto worker = WorkerManagerCore<EventTransitionInfo, TransitionPhaseStream>::deleteModuleIfExists(moduleLabel);
+    if (worker != nullptr) {
+      unscheduled_.removeWorker(worker);
     }
   }
 
   template <typename TI, typename TP>
-  Worker* WorkerManager<TI, TP>::getWorkerForExistingModule(std::string const& label) {
+  Worker* WorkerManagerCore<TI, TP>::getWorkerForExistingModule(std::string const& label) {
     auto worker = workerReg_.getWorkerFromExistingModule(label, actionTable_);
     if (nullptr != worker) {
       addToAllWorkers(worker);
@@ -44,9 +74,9 @@ namespace edm {
     return worker;
   }
 
-  template <typename TI, typename TP>
-  void WorkerManager<TI, TP>::addToUnscheduledWorkers(ModuleDescription const& iDescription) {
-    auto newWorker = workerReg_.getWorkerFromExistingModule(iDescription.moduleLabel(), actionTable_);
+  void WorkerManager<EventTransitionInfo, TransitionPhaseStream>::addToUnscheduledWorkers(
+      ModuleDescription const& iDescription) {
+    auto newWorker = this->getWorkerForExistingModuleUnattached(iDescription.moduleLabel());
     assert(nullptr != newWorker);
     assert(newWorker->moduleType() == Worker::Types::kProducer || newWorker->moduleType() == Worker::Types::kFilter);
     unscheduled_.addWorker(newWorker);
@@ -55,31 +85,51 @@ namespace edm {
   }
 
   template <typename TI, typename TP>
-  void WorkerManager<TI, TP>::resetAll() {
+  void WorkerManagerCore<TI, TP>::resetAll() {
     for_all(allWorkers_, std::bind(&Worker::reset, std::placeholders::_1));
   }
 
   template <typename TI, typename TP>
-  void WorkerManager<TI, TP>::addToAllWorkers(Worker* w) {
+  void WorkerManagerCore<TI, TP>::addToAllWorkers(Worker* w) {
     if (!search_all(allWorkers_, w)) {
       allWorkers_.push_back(w);
     }
   }
 
   template <typename TI, typename TP>
-  void WorkerManager<TI, TP>::setupResolvers(Principal& ep) {
+  void WorkerManagerCore<TI, TP>::setupResolvers(Principal& ep, UnscheduledAuxiliary const* aux) {
     this->resetAll();
-    if (&ep != lastSetupEventPrincipal_) {
-      UnscheduledConfigurator config(allWorkers_.begin(), allWorkers_.end(), &(unscheduled_.auxiliary()));
+    if (&ep != lastSetupPrincipal_) {
+      UnscheduledConfigurator config(allWorkers().begin(), allWorkers().end(), aux);
       ep.setupUnscheduled(config);
-      lastSetupEventPrincipal_ = &ep;
+      lastSetupPrincipal_ = &ep;
     }
   }
 
-  template <typename TI, typename TP>
-  void WorkerManager<TI, TP>::setupOnDemandSystem(EventTransitionInfo const& info) {
+  template <typename TI>
+  void WorkerManager<TI, TransitionPhaseStream>::setupResolvers(Principal& ep) {
+    this->setupResolvers(ep, nullptr);
+  }
+  template <typename TI>
+  void WorkerManager<TI, TransitionPhaseGlobal>::setupResolvers(Principal& ep) {
+    this->setupResolvers(ep, nullptr);
+  }
+
+  void WorkerManager<EventTransitionInfo, TransitionPhaseStream>::setupResolvers(Principal& ep) {
+    this->setupResolvers(ep, &(unscheduled_.auxiliary()));
+  }
+
+  void WorkerManager<EventTransitionInfo, TransitionPhaseStream>::setupOnDemandSystem(EventTransitionInfo const& info) {
     unscheduled_.setEventTransitionInfo(info);
   }
+
+  template class WorkerManagerCore<RunTransitionInfo, TransitionPhaseGlobal>;
+  template class WorkerManagerCore<LumiTransitionInfo, TransitionPhaseGlobal>;
+  template class WorkerManagerCore<RunTransitionInfo, TransitionPhaseStream>;
+  template class WorkerManagerCore<LumiTransitionInfo, TransitionPhaseStream>;
+  template class WorkerManagerCore<EventTransitionInfo, TransitionPhaseStream>;
+  template class WorkerManagerCore<ProcessBlockTransitionInfo, TransitionPhaseGlobal>;
+  template class WorkerManagerCore<InputProcessBlockTransitionInfo, TransitionPhaseGlobal>;
 
   template class WorkerManager<RunTransitionInfo, TransitionPhaseGlobal>;
   template class WorkerManager<LumiTransitionInfo, TransitionPhaseGlobal>;
