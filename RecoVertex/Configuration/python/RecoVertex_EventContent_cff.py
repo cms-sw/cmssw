0d732382bca5342e4809898f2360e9be49c8da26
Marco Musich <marco.musich@cern.ch>
2026-09-10 14:08:50 +0200

    remove MC truth consuming modules from vertexrecoTask

--- RecoVertex/Configuration/python/RecoVertex_EventContent_cff.py
+++ RecoVertex/Configuration/python/RecoVertex_EventContent_cff.py
@@ -14,8 +14,7 @@ from Configuration.Eras.Modifier_phase2_timing_cff import phase2_timing
 from Configuration.Eras.Modifier_phase2_timing_layer_cff import phase2_timing_layer
 
 _phase2_tktiming_RecoVertexEventContent = [ 'keep *_offlinePrimaryVertices4D__*',
-                                            'keep *_offlinePrimaryVertices4DWithBS__*',
-                                            'keep *_trackTimeValueMapProducer_*_*' ]
+                                            'keep *_offlinePrimaryVertices4DWithBS__*' ]
 
 _phase2_tktiming_layer_RecoVertexEventContent = [ 'keep *_tofPID_*_*']
 phase2_timing.toModify( RecoVertexAOD,
