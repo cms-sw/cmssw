cdbaf012324a90d354672ad7db797bbb65f6090f
Jan Schulz <jan.gerrit.schulz@cern.ch>
2026-09-17 15:42:32 +0200

    Remove non-extended Patatrack configuration and hltPhase2LegacyTrackingPatatrackQuads procModifier

--- Configuration/PyReleaseValidation/python/relval_Run4.py
+++ Configuration/PyReleaseValidation/python/relval_Run4.py
@@ -69,7 +69,6 @@ numWFIB.extend([prefixDet+34.751])  # HLTTiming75e33, alpaka
 numWFIB.extend([prefixDet+34.7521]) # HLTTiming75e33, ticlv5TrackLinkingGNN
 numWFIB.extend([prefixDet+34.7522]) # HLTTiming75e33, mtd_at_hlt
 numWFIB.extend([prefixDet+34.753])  # HLTTiming75e33, legacy tracking
-numWFIB.extend([prefixDet+34.754])  # HLTTiming75e33, legacy tracking with Patatrack quads
 numWFIB.extend([prefixDet+34.755])  # HLTTiming75e33, trackingLST
 numWFIB.extend([prefixDet+34.757])  # HLTTiming75e33, MkFitFit
 numWFIB.extend([prefixDet+34.758])  # HLTTiming75e33, ticl_barrel
--- Configuration/PyReleaseValidation/scripts/runTheMatrix.py
+++ Configuration/PyReleaseValidation/scripts/runTheMatrix.py
@@ -159,7 +159,6 @@ if __name__ == '__main__':
                      prefixDet+34.7521,  # HLT phase-2 timing menu ticlv5TrackLinkGNN variant
                      prefixDet+34.7522,  # HLT phase-2 timing menu mtd_at_hlt variant
                      prefixDet+34.753,   # HLT phase-2 timing menu legacy tracking
-                     prefixDet+34.754,   # HLT phase-2 timing menu legacy tracking with Patatrack quads
                      prefixDet+34.755,   # HLT phase-2 timing menu LST building variant
                      prefixDet+34.757,   # HLT phase-2 timing menu mkFit fitting variant
                      prefixDet+34.758,   # HLT phase-2 timing menu ticl_barrel variant
