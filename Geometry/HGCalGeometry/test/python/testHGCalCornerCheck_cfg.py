15ba4b7a873034aa7977986915997e09ea4067b3
Sunanda <sunanda.banerjee@cern.ch>
2026-09-21 15:45:52 +0200

    Add 2 testing code to verif the getCorners methods of HGCalGeometry to test V19 ginst V17

---
+++ Geometry/HGCalGeometry/test/python/testHGCalCornerCheck_cfg.py
@@ -0,0 +1,93 @@
+###############################################################################
+# Way to use this:
+#   cmsRun testHGCalCornerCheck_cfg.py geometry=D121 debugFlag=0
+#
+#   Options for geometry D104, D110, D111, D112, D113, D114, D115, D120, D121
+#                        D122, D123, D124, D125, D126, D127, D128
+#   Options for debugFlg 0, 1
+#
+###############################################################################
+import FWCore.ParameterSet.Config as cms
+import os, sys, importlib, re
+import FWCore.ParameterSet.VarParsing as VarParsing
+
+####################################################################
+### SETUP OPTIONS
+options = VarParsing.VarParsing('standard')
+options.register('geometry',
+                 "D126",
+                  VarParsing.VarParsing.multiplicity.singleton,
+                  VarParsing.VarParsing.varType.string,
+                  "geometry of operations: D104, D110, D111, D112, D113, D114, D123, D124, D125, D126, D127, D128")
+options.register('debugFlag',
+                 1,
+                 VarParsing.VarParsing.multiplicity.singleton,
+                 VarParsing.VarParsing.varType.int,
+                 "geometry of operations: 0, 1")
+
+
+### get and parse the command line arguments
+options.parseArguments()
+print(options)
+
+####################################################################
+# Use the options
+
+geomName = "Run4" + options.geometry
+geomFile = "Configuration.Geometry.GeometryExtended" + geomName + "Reco_cff"
+import Configuration.Geometry.defaultPhase2ConditionsEra_cff as _settings
+GLOBAL_TAG, ERA = _settings.get_era_and_conditions(geomName)
+debugFlag = options.debugFlag
+
+print("Geometry Name:   ", geomName)
+print("Geom file Name:  ", geomFile)
+print("Global Tag Name: ", GLOBAL_TAG)
+print("Era Name:        ", ERA)
+print("DebugFlag:       ", debugFlag)
+
+process = cms.Process("HGCalCornerCheck",ERA)
+
+process.load(geomFile)
+process.load("SimGeneral.HepPDTESSource.pdt_cfi")
+process.load('Configuration.StandardSequences.MagneticField_cff')
+process.load('Configuration.StandardSequences.Services_cff')
+process.load('Geometry.HGCalGeometry.hgcalCornerCheck_cfi')
+process.load('FWCore.MessageService.MessageLogger_cfi')
+process.load('Configuration.EventContent.EventContent_cff')
+process.load('Configuration.StandardSequences.FrontierConditions_GlobalTag_cff')
+from Configuration.AlCa.GlobalTag import GlobalTag
+process.GlobalTag = GlobalTag(process.GlobalTag, GLOBAL_TAG, '')
+
+if hasattr(process,'MessageLogger'):
+    process.MessageLogger.HGCalGeom=dict()
+    process.MessageLogger.HGCalGeomX=dict()
+
+process.load("IOMC.RandomEngine.IOMC_cff")
+process.RandomNumberGeneratorService.generator.initialSeed = 456789
+
+process.source = cms.Source("EmptySource")
+
+process.generator = cms.EDProducer("FlatRandomEGunProducer",
+    PGunParameters = cms.PSet(
+        PartID = cms.vint32(14),
+        MinEta = cms.double(-3.5),
+        MaxEta = cms.double(3.5),
+        MinPhi = cms.double(-3.14159265359),
+        MaxPhi = cms.double(3.14159265359),
+        MinE   = cms.double(9.99),
+        MaxE   = cms.double(10.01)
+    ),
+    AddAntiParticle = cms.bool(False),
+    Verbosity       = cms.untracked.int32(0),
+    firstRun        = cms.untracked.uint32(1)
+)
+
+process.maxEvents = cms.untracked.PSet(
+    input = cms.untracked.int32(1)
+)
+
+process.hgcalCornerCheck.LayerFirst = 8
+process.hgcalCornerCheck.LayerLast  = 21
+process.hgcalCornerCheck.Debug      = debugFlag
+
+process.p1 = cms.Path(process.generator*process.hgcalCornerCheck)
