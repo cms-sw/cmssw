6524d3f09e58d36156ee844ce09ae4b37468e235
Akrit Kumar Behera <84931167+akritkbehera@users.noreply.github.com>
2026-09-22 07:47:32 +0200

    Fix C++20 module-declaration parse error in TkOffTreeVariables.h

--- Alignment/OfflineValidation/interface/TkOffTreeVariables.h
+++ Alignment/OfflineValidation/interface/TkOffTreeVariables.h
@@ -59,7 +59,7 @@ struct TkOffTreeVariables {
   UInt_t moduleId, subDetId,   //moduleId == detId
       layer, side, half, rod,  // half = TPB: halfBarrel, TPE: halfCylinder, TIB: halfShell
       ring, petal, blade, panel, outerInner,
-      module;  //orientation of modules in TIB:1/2= int/ext string, TID:1/2=back/front ring, TEC 1/2=back/front petal
+      UInt_t module;  //orientation of modules in TIB:1/2= int/ext string, TID:1/2=back/front ring, TEC 1/2=back/front petal
 
   Bool_t isDoubleSide;  // (!isDoubleSide) is a detUnit, (isDoubleSide) is a Det (glued Modules)
   Bool_t isStereo;      // (!isStereo) is a rPhi-module, (isStereo) is the stereo module from a Det
