267e87382d4233d3e500641e3d6cca3cbeeed067
soohwan <soohwan.lee@cern.ch>
2026-09-22 04:20:47 +0200

    new logic for HF threshold in conversion seed geneartor and modifier for Run3 PbPb

--- RecoTracker/ConversionSeedGenerators/plugins/PhotonConversionTrajectorySeedProducerFromSingleLeg.cc
+++ RecoTracker/ConversionSeedGenerators/plugins/PhotonConversionTrajectorySeedProducerFromSingleLeg.cc
@@ -11,6 +11,7 @@
 
 #include "FWCore/MessageLogger/interface/MessageLogger.h"
 #include "PhotonConversionTrajectorySeedProducerFromSingleLegAlgo.h"
+#include "DataFormats/CaloTowers/interface/CaloTowerCollection.h"
 //#include "UserUtilities/TimingPerformance/interface/TimeReport.h"
 
 class dso_hidden PhotonConversionTrajectorySeedProducerFromSingleLeg final : public edm::stream::EDProducer<> {
@@ -26,17 +27,35 @@ public:
 private:
   std::string _newSeedCandidates;
   PhotonConversionTrajectorySeedProducerFromSingleLegAlgo* _theFinder;
+  edm::EDGetTokenT<CaloTowerCollection> _towerToken;
+  double _maxHFTowerSum;
 };
 
 PhotonConversionTrajectorySeedProducerFromSingleLeg::PhotonConversionTrajectorySeedProducerFromSingleLeg(
     const edm::ParameterSet& conf)
-    : _newSeedCandidates(conf.getParameter<std::string>("newSeedCandidates")) {
+    : _newSeedCandidates(conf.getParameter<std::string>("newSeedCandidates")),
+      _towerToken(conf.existsAs<double>("maxHFTowerSum")
+                      ? consumes<CaloTowerCollection>(conf.getParameter<edm::InputTag>("towers"))
+                      : edm::EDGetTokenT<CaloTowerCollection>()),
+      _maxHFTowerSum(conf.existsAs<double>("maxHFTowerSum") ? conf.getParameter<double>("maxHFTowerSum") : 0.) {
   _theFinder = new PhotonConversionTrajectorySeedProducerFromSingleLegAlgo(conf, consumesCollector());
   produces<TrajectorySeedCollection>(_newSeedCandidates);
 }
 
 void PhotonConversionTrajectorySeedProducerFromSingleLeg::produce(edm::Event& ev, const edm::EventSetup& es) {
   auto result = std::make_unique<TrajectorySeedCollection>();
+
+  if (!_towerToken.isUninitialized()) {
+    double hfTowerSum = 0.;
+    for (const auto& tower : ev.get(_towerToken)) {
+      if (tower.ietaAbs() > 29)
+        hfTowerSum += tower.pt();
+    }
+    if (hfTowerSum >= _maxHFTowerSum) {
+      ev.put(std::move(result), _newSeedCandidates);
+      return;
+    }
+  }
 
   _theFinder->find(ev, es, *result);
   result->shrink_to_fit();
